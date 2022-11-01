97fa9aec14aba2ee91839625d78bf34ede89fef3
ym2629 <dydals2629@naver.com>
2022-11-02 08:58:00 +0900

    feat: output result of everything wrong

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -4,4 +4,8 @@ public class Output {
 	void oneGameResult(int ball, int strike) {
 		System.out.printf("%d볼 %d스트라이크",ball,strike);
 	}	
+	
+	void wrongEverything() {
+		System.out.println("낫싱");
+	}
 }
