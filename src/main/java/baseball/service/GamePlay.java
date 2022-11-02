910c8e7e917b489f1466e0ee2ab5cfedbb105f7b
정핸모 <gusah009@naver.com>
2022-11-02 23:39:07 +0900

    feat: 받아온 게임의 클래스로 해당 게임을 생성해주는 로직 구현

--- src/main/java/baseball/service/GamePlay.java
+++ src/main/java/baseball/service/GamePlay.java
@@ -1,5 +1,19 @@
 package baseball.service;
 
+import baseball.game.Game;
+
 public class GamePlay {
 
+  public static void start(Class<Game> gameClazz) {
+    Game game = getGameInstance(gameClazz);
+    game.play();
+  }
+
+  private static Game getGameInstance(Class<Game> gameClazz) {
+    try {
+      return gameClazz.getDeclaredConstructor().newInstance();
+    } catch (Exception e) {
+      throw new IllegalArgumentException("잘못된 게임 클래스입니다.");
+    }
+  }
 }
