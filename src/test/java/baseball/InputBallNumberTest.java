c1e876ee07fceb9b41449d664e8f53dbf02cf4ec
CMHyuk <alsgur9710@naver.com>
2022-11-02 23:24:41 +0900

    test: 중복된 숫자 테스트

--- src/test/java/baseball/InputBallNumberTest.java
+++ src/test/java/baseball/InputBallNumberTest.java
@@ -38,4 +38,38 @@ class InputBallNumberTest {
         String message = exception.getMessage();
         assertEquals("1~9까지의 숫자만 입력해주세요.", message);
     }
+
+    @Test
+    void 중복된_숫자_입력() {
+        String ballNumber1 = "112";
+        String ballNumber2 = "122";
+        String ballNumber3 = "221";
+        String ballNumber4 = "212";
+        String ballNumber5 = "111";
+
+        IllegalArgumentException exception1 = assertThrows(IllegalArgumentException.class,
+                () -> inputBallNumber.validation(ballNumber1));
+        String message1 = exception1.getMessage();
+        assertEquals("중복된 숫자는 입력할 수 없습니다.", message1);
+
+        IllegalArgumentException exception2 = assertThrows(IllegalArgumentException.class,
+                () -> inputBallNumber.validation(ballNumber2));
+        String message2 = exception2.getMessage();
+        assertEquals("중복된 숫자는 입력할 수 없습니다.", message2);
+
+        IllegalArgumentException exception3 = assertThrows(IllegalArgumentException.class,
+                () -> inputBallNumber.validation(ballNumber3));
+        String message3 = exception3.getMessage();
+        assertEquals("중복된 숫자는 입력할 수 없습니다.", message3);
+
+        IllegalArgumentException exception4 = assertThrows(IllegalArgumentException.class,
+                () -> inputBallNumber.validation(ballNumber4));
+        String message4 = exception4.getMessage();
+        assertEquals("중복된 숫자는 입력할 수 없습니다.", message4);
+
+        IllegalArgumentException exception5 = assertThrows(IllegalArgumentException.class,
+                () -> inputBallNumber.validation(ballNumber5));
+        String message5 = exception5.getMessage();
+        assertEquals("중복된 숫자는 입력할 수 없습니다.", message5);
+    }
 }
