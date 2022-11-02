a5483e70f02520da54bb5606b092fec143072611
panorama713 <yaiba713@gmail.com>
2022-11-02 17:58:34 +0900

    docs: fix list of feat

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,25 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //TODO: 프로그램 구현
+        //todo: 랜덤으로 3자리 숫자를 정하는 기능
+        //todo: 3자리 숫자를 입력받는 기능
+            //todo: 잘못된 값을 입력했을때 기능
+                //todo: 애플리케이션 종료(IllegalArgumentException)
+            //todo: 3자리 숫자가 입력되었을때 기능 실행
+                //todo: 입력한 숫자가 랜덤한 숫자와 같은지 확인하는 기능 실행
+        //todo: 스트라이크의 개수로 프로그램 종료 검증 기능
+            //todo: 3스트라이크
+    }
+    //todo: 입력한 숫자가 랜덤한 숫자와 같은지 확인하는 기능
+        //todo: 3스트라이크일 경우
+            //todo: 프로그램 종료
+        //todo: 아닐경우
+
+    public static int num_verification(int num){
+        //todo: 1개 이상의 숫자가 같고 자리가 같은 경우 = (맞은개수)스트라이크
+        //todo: 1개 이상의 숫자가 같고 자리가 다른 경우 = (맞은개수)볼
+        //todo: 입력 숫자가 모두 다른 경우 = 낫싱
+        return 0;
     }
 }
