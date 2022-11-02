1a093cf710e29f4f0e93b868e6b6650e1da7654d
shinD <huipulci1@naver.com>
2022-11-03 01:34:04 +0900

    feat: 숫자 입력 단계 실행 시 숫자 입력 문구 출력하는 기능 구현

--- src/main/java/baseball/domain/step/InputNumberStep.java
+++ src/main/java/baseball/domain/step/InputNumberStep.java
@@ -1,13 +1,19 @@
 package baseball.domain.step;
 
 import baseball.application.context.BaseBallGameContext;
+import baseball.application.io.Writer;
+import baseball.domain.number.BaseBallGameNumbers;
 
 public class InputNumberStep implements Step{
+
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
     private static final Step next = new GameProgressStep();
 
     @Override
     public void execute(BaseBallGameContext context) {
+        Writer writer = context.writer();
+        writer.println(INPUT_NUMBER_MESSAGE);
 
     }
 
