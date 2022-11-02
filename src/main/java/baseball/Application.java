d8168ef46ccfb888c3a1a9c724c2e10186358752
oyoungsun <plusmate@naver.com>
2022-11-02 21:49:03 +0900

    docs : 기능 리스트 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
 public class Application {
+    static String comNum = "719";
+    static String myNum = "123";
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 게임 시작
+        // TODO: 랜덤 수 생성
+        // TODO: 입력받기
+        // TODO: 숫자 비교
+        // TODO: 결과 출력
+        // TODO: 게임 종료, 재시작 or 종료
+
     }
 }
