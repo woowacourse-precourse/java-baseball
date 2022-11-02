c6f64f4e0bcf4183b23453c256c09a743407df94
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-02 21:23:48 +0900

    feat : 랜덤 숫자 생성 테스트 기능 구현

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,14 +1,60 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static camp.nextstep.edu.missionutils.test.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
+
+    @DisplayName("숫자 생성의 범위를 확인한다")
+    @Test
+    void testNumberCreationRange(){
+        Computer computer = new Computer();
+        int randomNumber = computer.createRandomNumber();
+
+        assertThat(randomNumber).isBetween(1,9);
+    }
+
+    @DisplayName("난수 생성을 확인한다")
+    @RepeatedTest(50)
+    void testRandomNumberGeneration(){
+
+        /*
+          랜덤으로 숫자가 만들어지는 것을 확인하기 위해서
+          일정 수준을 여러번 생성하는 시나리오를 가정한다.
+          즉,
+          9의 크기를 가진 배열에 모든 숫자를 계속 해서 채워 넣는다면
+          특정 반복 횟수를 지나
+          모든 공간이 채워진다면
+          랜덤 숫자가 만들어지는 것이라고 가정하고
+          이를 검증한다.
+
+          배열의 공간        0 1 2 3 4 5 6 7 8
+          랜덤 숫자          1 2 3 4 5 6 7 8 9
+          호출시 1로 초기화   1 1 1 1 1 1 1 1 1 => 합계 9
+         */
+
+        Computer computer = new Computer();
+        int[] counts = new int[10];
+        for (int counter=0; counter < 70; counter++){
+            int randomNumber = computer.createRandomNumber();
+            counts[randomNumber] = 1;
+        }
+        int sum = 0;
+        for (int counter =0; counter <10; counter++ ){
+            sum+= counts[counter];
+        }
+        assertEquals(9,sum);
+
+    }
+
+
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
