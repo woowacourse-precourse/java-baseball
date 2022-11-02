7d49103425e764ed08531f131c21bf987ea505a3
dev_kong <ahdjd5@gmail.com>
2022-11-02 17:26:08 +0900

    test(GameSetting): check randomly generate number list contain duplicated number

---
+++ src/test/java/baseball/gameSetting/RandomNumberTest.java
@@ -0,0 +1,28 @@
+package baseball.gameSetting;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import baseball.utils.RandomNumber;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+
+public class RandomNumberTest {
+
+    @Test
+    void check_exist_duplicated_number_in_list() {
+        boolean testResult = true;
+        boolean result = true;
+        int count = 0;
+        while (count < 1000) {
+            List<Integer> randomNumberList = RandomNumber.generateUnduplicatedThreeNumbers();
+            Set<Integer> numSet = new HashSet<>(randomNumberList);
+            if (numSet.size() != randomNumberList.size()) {
+                testResult = false;
+            }
+            count++;
+        }
+        assertThat(result).isEqualTo(testResult);
+    }
+}
