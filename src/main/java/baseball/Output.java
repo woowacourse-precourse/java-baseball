8c9eab26467903d6ba2ca0dd15c68423f0f1204e
ym2629 <dydals2629@naver.com>
2022-11-02 09:03:37 +0900

    feat: output beginning of game

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -1,6 +1,10 @@
 package baseball;
 
 public class Output {
+	void stratGame() {
+		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
+	
 	void oneGameResult(int ball, int strike) {
 		System.out.printf("%d볼 %d스트라이크",ball,strike);
 	}	
