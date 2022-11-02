6f26080cd602501503b43b61a71110bac511cabe
이한솔 <hansol8701@naver.com>
2022-11-03 03:23:07 +0900

    feat: 사용자 숫자 입력시 유효성 검증 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,30 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Application {
 
-    public static void startGame(){
+    public static void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static boolean validation(String input) {
+
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        if (input.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+        if (!input.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException();
+        }
+        if (!input.equals(input.chars().distinct().map(s -> s - 48).mapToObj(String::valueOf).collect(Collectors.joining()))) {
+            throw new IllegalArgumentException();
+        }
+
+        return true;
     }
 
     public static void main(String[] args) {
--- src/test/java/study/MyTest.java
+++ src/test/java/study/MyTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static baseball.Application.startGame;
+import static baseball.Application.validation;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -19,21 +21,32 @@ public class MyTest {
     }
 
     @Test
-    @DisplayName("사용자 숫자 입력")
+    @DisplayName("사용자 숫자 입력시 유효성 검증")
     void 사용자_숫자_입력시_유효성_검증() {
         String input = "123";
         String wrongInput1 = "1234";
         String wrongInput2 = "120";
-        String wrongInput3 = "12";
+        String wrongInput3 = "111";
         String wrongInput4 = "1.3";
         String wrongInput5 = "abc";
-        String wrongInput6 = "111";
 
 
         assertThat(validation(input)).isTrue();
-        assertThat(validation(wrongInput1)).isTrue();
         assertThatThrownBy(() -> {
             validation(wrongInput1);
         }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> {
+            validation(wrongInput2);
+        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> {
+            validation(wrongInput3);
+        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> {
+            validation(wrongInput4);
+        }).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> {
+            validation(wrongInput5);
+        }).isInstanceOf(IllegalArgumentException.class);
+
     }
 }
