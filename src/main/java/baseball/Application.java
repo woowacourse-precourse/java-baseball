c5585fc22c05da2799896745bdcd52503390d4b2
saturnone1 <saturnone1@naver.com>
2022-11-03 08:17:00 +0900

    feature(createNum): create Random Numbers
    3자리의 무작위 번호를 생성하는 기능을 추가하였습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,27 @@
 package baseball;
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
-    public static void main(String[] args){
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+    public List<Integer> numList = new ArrayList<>();
+    public void createNum(){
+        while (numList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+            if (!numList.contains(randomNumber)) {
+                numList.add(randomNumber);
             }
         }
+    }
+
+    public List<Integer> getNumList() {
+        return numList;
+    }
+
+    public static void main(String[] args){
+        Application app = new Application();
+
     }
 }
