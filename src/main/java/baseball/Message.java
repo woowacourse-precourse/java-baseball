cce8f85568b405cda78f52988b45856495bbca9a
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-02 22:25:55 +0900

    feat : 메시지 값을 가지고 있는 클래스 추가

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -1,2 +1,8 @@
-package baseball;public class Message {
+package baseball;
+
+public class Message {
+    public static String START_GAME = "숫자 야구 게임을 시작합니다.";
+    public static final String USER_WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String ASK_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String END_GAME = "숫자 야구 게임을 종료합니다.";
 }
