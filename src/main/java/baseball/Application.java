18c859d40994d91b6b75b4d1f7b228c2951d14dd
saturnone1 <saturnone1@naver.com>
2022-11-03 08:54:39 +0900

    feature($Application): start baseball game
    Couple of fixed:
    - createNum return type (List<> -> void) : save into class instance 'numList'
    - startGame Method: start,get input until 'answer' being true.
    - getInputNum Method: get input with Console.readline

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,10 +4,19 @@ import java.util.List;
 import java.util.ArrayList;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public List<Integer> numList = new ArrayList<>();
-    public void createNum(){
+    public boolean answer = false;
+    public String inputNum = "";
+
+    public Application(){
+        createNum();
+        startGame();
+    }
+
+    public void createNum() {
         while (numList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!numList.contains(randomNumber)) {
@@ -15,13 +24,23 @@ public class Application {
             }
         }
     }
+    public void startGame(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (!answer){
+            getInputNum();
+        }
+    }
+
+    public void getInputNum() {
+        System.out.print("숫자를 입력해주세요 : ");
+        inputNum = Console.readLine();
+    }
 
     public List<Integer> getNumList() {
         return numList;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Application app = new Application();
-
     }
 }
