5ba6681832643bc6c86ee4679848751a226c1698
dev_kong <ahdjd5@gmail.com>
2022-11-02 23:59:16 +0900

    feat(GameStart): add duplicated check method

--- src/main/java/baseball/utils/Validator.java
+++ src/main/java/baseball/utils/Validator.java
@@ -1,22 +1,34 @@
 package baseball.utils;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Validator {
     public static void checkUserAnswerInput(String userAnswer) {
         checkStringLength(userAnswer, 3);
         checkNumber(userAnswer);
+        checkDuplicated(userAnswer);
     }
 
     private static void checkStringLength(String string, int length) {
-        if(string.length() != length) {
+        if (string.length() != length) {
             throw new IllegalArgumentException();
         }
     }
 
     private static void checkNumber(String string) {
         boolean isNumber = Pattern.matches("^[0-9]*$", string);
-        if(!isNumber){
+        if (!isNumber) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkDuplicated(String string) {
+        String[] strings = string.split("");
+        Set<String> uniqueValues = new HashSet<>(List.of(strings));
+        if (strings.length != uniqueValues.size()) {
             throw new IllegalArgumentException();
         }
     }
