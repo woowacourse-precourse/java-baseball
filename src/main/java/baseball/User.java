43b713409445b04db424d5906e8126580fed7da1
HwangSeokJin <ghkd3703@gmail.com>
2022-11-02 22:52:43 +0900

    feat(User): 사용자가 입력한 값의 길이가 3이 아닐 때 IllegalArgumentException 발생

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -6,5 +6,17 @@ public class User {
 
     public void pickNumbers(){
         String numbers = Console.readLine();
+
+        validateInput(numbers);
+    }
+
+    private void validateInput(String input){
+        validateLength(input);
+    }
+
+    private void validateLength(String input){
+        if (input.length() != 3){
+            throw new IllegalArgumentException();
+        }
     }
 }
