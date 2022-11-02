96ccc3d5dbe4252ebb1050e13a04e95678d81b66
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-02 20:14:48 +0900

    feat(/getThreeRandomNumber()) : getThreeRandomNumber() 메서드 작성
    - 컴퓨터의 세 자리 수 생성하는 메서드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        for (int i = 0; i < 100; i++) {
+            System.out.println(Randoms.pickNumberInRange(1, 10));
+
+        }
+    }
+
+    private static List<Integer> getThreeRandomNumber() {
+        List<Integer> result = new ArrayList<>();
+
+        while (result.size() < 3) {
+            int tmpRandomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!result.contains(tmpRandomNumber)) {
+                result.add(tmpRandomNumber);
+            }
+        }
+
+        return result;
     }
 }
