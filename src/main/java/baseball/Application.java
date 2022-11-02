f45b7b773d90a00524eb16379ec74d674d88a714
Kwon-sang <kwonsg514@gmail.com>
2022-11-03 03:01:03 +0900

    3. User 입력기능을 하는 메서드와 올바른 입력인지 판단하는 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,8 +9,13 @@ public class Application {
     public static void main(String[] args) {
         initializerOfStrikeAndBallCounter();
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
         List<Integer> computer = createRandomThreeDigitNum();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해 주세요 : ");
+        List<Integer> user = userInput();
+
+
 
     }
 
@@ -25,11 +30,38 @@ public class Application {
 
         while(computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1,9);
-            if(!computer.contains(randomNumber)){
-                computer.add(randomNumber);
-            }
+            if(!computer.contains(randomNumber)) computer.add(randomNumber);
         }
 
         return computer;
     }
+
+    static List<Integer> userInput() {
+        List<Integer> user = new ArrayList<>();
+
+        String input = Console.readLine();
+        isCorrectedInput(input);
+
+        for(char number : input.toCharArray()) {
+            user.add(number-'0');
+        }
+
+        return user;
+    }
+
+    static boolean isCorrectedInput(String input) {
+        if (input.length() != 3) throw new IllegalArgumentException();
+
+        Set<Character> set = new HashSet<>();
+        for (char i : input.toCharArray()) {
+            if (!Character.isDigit(i) || i == '0') throw new IllegalArgumentException();
+
+            if(!set.contains(i)) set.add(i);
+            else throw new IllegalArgumentException();
+        }
+
+        return true;
+    }
+
+
 }
