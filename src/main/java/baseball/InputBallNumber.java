e7995410549336555be57c715e236862a22ffd81
CMHyuk <alsgur9710@naver.com>
2022-11-02 23:09:25 +0900

    feat: 입력 숫자 길이 검증 기능

--- src/main/java/baseball/InputBallNumber.java
+++ src/main/java/baseball/InputBallNumber.java
@@ -4,9 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputBallNumber {
 
+    private static final int BALL_NUMBER_LENGTH = 3;
+
     public String inputBallNum() {
         System.out.print("숫자를 입력해주세요 : ");
         String ballNumbers = Console.readLine();
+        validation(ballNumbers);
         return ballNumbers;
+    }
+
+    public void validation(String ballNumbers) {
+        checkLength(ballNumbers);
+    }
+
+    private void checkLength(String ballNumbers) {
+        if (ballNumbers.length() != BALL_NUMBER_LENGTH) {
+            throw new IllegalArgumentException("1~9까지의 숫자 3개를 입력해주세요.");
+        }
     }
 }
