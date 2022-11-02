5d6866b4fef098ba3ac0c55e40cd821a0e95772f
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-02 23:11:08 +0900

    feat : 입력받은 결과를 출력해주는 gameResult() 메서드

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -1,8 +1,22 @@
 package baseball;
 
+import static baseball.BaseballGame.*;
+
 public class Message {
     public static String START_GAME = "숫자 야구 게임을 시작합니다.";
     public static final String USER_WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String ASK_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String END_GAME = "숫자 야구 게임을 종료합니다.";
+
+    public static String gameResult() {
+        if (ball != 0 && strike != 0) {
+            return (ball + "볼" + " " + strike + "스트라이크");
+        } else if (ball == 0 && strike != 0) {
+            return (strike + "스트라이크");
+        } else if (ball != 0 && strike == 0) {
+            return (ball + "볼");
+        } else {
+            return ("낫싱");
+        }
+    }
 }
