72b805c3700f40dfe51f7e81278ffbab0043ac93
HwangSeokJin <ghkd3703@gmail.com>
2022-11-02 22:35:27 +0900

    feat: 사용자가 숫자를 입력하는 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,11 +2,21 @@ package baseball;
 
 import java.util.List;
 import java.util.ArrayList;
-import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseballGame {
 
-    public void play(){
+    private Computer computer;
+    private User user;
 
+    private BaseballGame(){
+        computer = new Computer();
+        user = new User();
+    }
+
+    public void play(){
+        System.out.println("숫자 야구 게임을 시작합니다 ");
+        computer.pickRandomNumbersFrom1To9();
+
+        user.pickNumbers();
     }
 }
