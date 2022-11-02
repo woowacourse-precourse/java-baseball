00f3c36fe9546b5940b523d7469f35aeab6fc93f
greeng00se <thegreengoose7@gmail.com>
2022-11-02 22:33:20 +0900

    feat(StartView): 게임 시작시 문자열을 출력하는 책임을 가진 StartView 클래스 추가

---
+++ src/main/java/baseball/view/StartView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class StartView {
+    private final static String START_BASEBALL_GAME = "숫자 야구 게임을 시작합니다.";
+
+    public static void startBaseballGame() {
+        System.out.println(START_BASEBALL_GAME);
+    }
+}
---
+++ src/test/java/baseball/view/StartViewTest.java
@@ -0,0 +1,31 @@
+package baseball.view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StartViewTest {
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setup() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @AfterEach
+    public void restore() {
+        System.setOut(System.out);
+        output.reset();
+    }
+
+    @Test
+    void 게임을_시작하면_숫자_야구_게임을_시작합니다_라고_화면에_출력한다() {
+        StartView.startBaseballGame();
+
+        assertThat(output.toString()).isEqualTo("숫자 야구 게임을 시작합니다.\n");
+    }
+}
