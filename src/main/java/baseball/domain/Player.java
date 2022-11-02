0b01194c35a949437cf9fa248d849699ba2b54bc
jinny-l <jinny.l.dev@gmail.com>
2022-11-03 01:36:39 +0900

    refactor(domain): Player 클래스의 getInput() 메소드의 리턴 타입 변경

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,11 +1,15 @@
 package baseball.domain;
 
+import baseball.util.StringToArrayList;
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
 
 public class Player {
 
-	public String getInput() {
-		return Console.readLine();
+	public List<Integer> getInput() {
+		String playerInput = Console.readLine();
+		return StringToArrayList.convert(playerInput);
 	}
 
 }
--- src/main/java/baseball/util/StringToArrayList.java
+++ src/main/java/baseball/util/StringToArrayList.java
@@ -8,9 +8,13 @@ public class StringToArrayList {
 	public static List<Integer> convert(String input) {
 		List<Integer> arrayList = new ArrayList<>();
 
-		for (int i = 0; i < input.length(); i++) {
-			char c = input.charAt(i);
-			arrayList.add(Character.getNumericValue(c));
+		try {
+			for (int i = 0; i < input.length(); i++) {
+				char c = input.charAt(i);
+				arrayList.add(Character.getNumericValue(c));
+			}
+		} catch (IllegalArgumentException ie) {
+			System.out.println(SystemMessage.printError());
 		}
 
 		return arrayList;
