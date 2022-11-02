8b3d40a1560e8d3d91b077adb0b5cbfa634d05a5
jhon3242 <rldotnr2@gmail.com>
2022-11-02 22:24:12 +0900

    feat(출력 처리): 정답 입력시 출력 처리
    OutputView
    - 게임 승리시 메시지 출력 메서드 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,6 +2,7 @@ package baseball.view;
 
 public class OutputView {
 	private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+	private static final String WIN_MESSAGE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
 	public static void gameStartView(){
 		System.out.println(START_MESSAGE);
@@ -17,5 +18,9 @@ public class OutputView {
 			strikeStr = String.valueOf(strike) + "볼";
 		}
 		System.out.println(String.join(ballStr, strikeStr));
+	}
+
+	public static void displayWin(){
+		System.out.println(WIN_MESSAGE);
 	}
 }
