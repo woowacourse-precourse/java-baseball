643bf4e252ee446678462aff1bb1ea5709a0958e
ywj9811 <ywj9811@naver.com>
2022-11-03 01:11:00 +0900

    [feat]Main 클래스 야구 게임 위한 랜덤 값을 위한 메소드 생성

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -1,10 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public void GameStart() {
         int Restart = 1;
         while (Restart == 1) {
-
+            List<Integer> RandomNumber = GetRandomNum();
+            List<Integer> GamerInput = new ArrayList<>();
+            System.out.println("숫자 야구 게임을 시작하겠습니다.");
         }
+    }
+
+    private List<Integer> GetRandomNum() {
+        List<Integer> GetNum = new ArrayList<>();
+        while (GetNum.size() < 3) {
+            int rdNum = Randoms.pickNumberInRange(1, 9);
+            if (!GetNum.contains(rdNum)) {
+                GetNum.add(rdNum);
+            }
+        }
+        return GetNum;
     }
 }
