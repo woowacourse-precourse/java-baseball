eb7b01b8974ab312e7ce006b92839da5bd1cf206
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-02 17:33:11 +0900

    feat : 컴퓨터가 랜덤으로 3자리 숫자를 만드는 기능 추가 각 자리수는 1-9 서로 다른 수로 이루어져있다. camp.nextstep.edu.missionutils에서 제공하는 Randoms을 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
+        List<Integer> computer = makeRandomNumber();
+    }
+
+    public static List<Integer> makeRandomNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) { //중복 방지
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
     }
 }
