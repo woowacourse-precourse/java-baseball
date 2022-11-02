e358a81b988f7db8c16f721862b536150af1f527
yj2980 <gum0945@naver.com>
2022-11-02 20:18:52 +0900

    feat : 사용자 입력값 예외 처리 기능 추가

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -3,13 +3,22 @@ package baseball.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import baseball.service.GameService;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class GameController {
 	final int min = 1;
 	final int max = 9;
 
-	public int createRandomNumber() {
-		return Randoms.pickNumberInRange(min, max);
+	public String gameStart() {
+		GameService gs = new GameService();
+
+		return gs.createRandomNumber();
+	}
+
+	public void saveUserNumber(String userNumber) {
+		GameService gs = new GameService();
+
+		gs.checkException(userNumber);
 	}
 }
--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,5 +1,7 @@
 package baseball.service;
 
+import org.mockito.internal.matchers.Null;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class GameService {
@@ -7,12 +9,53 @@ public class GameService {
 	final int max = 9;
 
 	public String createRandomNumber() {
-		String randomNumber = "";
+		String random = "";
 
-		for (int i = 0; i < 3; i++)
-			randomNumber += Randoms.pickNumberInRange(min, max);
+		for (int i = 0; i < 3; i++) {
+			int randomNumber = Randoms.pickNumberInRange(min, max);
+			while (checkEqualNumber(random, randomNumber)) {
+				randomNumber = Randoms.pickNumberInRange(min, max);
+			}
+			random += randomNumber;
+		}
 
-		return randomNumber;
+		return random;
 	}
 
+	public boolean checkEqualNumber(String number, int addedNumber) {
+		if (number.contains(String.valueOf(addedNumber)))
+			return true;
+		return false;
+	}
+
+	public void checkException(String number) {
+		checkNullException(number);
+		isSameWord(number);
+
+		for (int i = 0; i < number.length(); i++) {
+			checkUserNumber(Character.getNumericValue(number.charAt(i)));
+		}
+	}
+
+	public void isSameWord(String number) {
+		String tmp = "";
+
+		for (int i = 0; i < number.length(); i++) {
+			if (checkEqualNumber(tmp, Character.getNumericValue(number.charAt(i)))) {
+				throw new IllegalArgumentException("같은 숫자가 있습니다.");
+			}
+			tmp += number.charAt(i);
+		}
+	}
+	public void checkUserNumber(int userNumber) {
+		if (userNumber < 1 || userNumber > 9) {
+			throw new IllegalArgumentException("숫자 범위를 벗어났습니다.");
+		}
+	}
+	public void checkNullException(String number) {
+		if (number == null || number.isEmpty())
+			throw new IllegalArgumentException("NULL값 에러");
+		if (number.length() != 3)
+			throw new IllegalArgumentException("숫자 길이 에러");
+	}
 }
