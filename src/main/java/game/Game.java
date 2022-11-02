5274157167d058e6653ed1fdb5337abfb7f5b9a6
yg0585 <yg0585@naver.com>
2022-11-02 19:10:35 +0900

    feat: 랜덤한 숫자 3개 생성하고 중복되어 있는 숫자인지 검사

---
+++ src/main/java/game/Game.java
@@ -0,0 +1,27 @@
+package game;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+
+    private List<Integer> computer = new ArrayList<>();
+
+    public Game() {
+
+    }
+
+    private void generateNum() {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            extracted(computer, randomNumber);
+        }
+    }
+
+    private void extracted(List<Integer> computer, int randomNumber) {
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber);
+        }
+    }
+}
