99e74669345ec251497e76bb47e5ee9b4da4e599
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-02 18:04:49 +0900

    feat: 게임시작시 지정된 게임 시작 메시지를 출력한다.
    BaseballGame class를 통해 게임 시작시 "숫자 야구 게임을 시작합니다." 라는 메시지를 출력한다

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class BaseballGame {
+
+    public final static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public void start() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+
+
+}
