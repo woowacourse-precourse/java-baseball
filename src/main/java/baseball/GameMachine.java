0a7176724ae286685e6d139e4029749bc9f3c475
unit74 <u__nit@naver.com>
2022-11-02 22:23:41 +0900

    fix: 출력 형식 및 인덱스 수정

--- src/main/java/baseball/GameMachine.java
+++ src/main/java/baseball/GameMachine.java
@@ -90,17 +90,18 @@ public class GameMachine {
                 gamer = convertGamerNumber(input);
 
                 List<Integer> sb = strikeAndBall();
-                int strike = sb.get(0), ball = sb.get(0);
+                int strike = sb.get(0), ball = sb.get(1);
 
                 if(ball != 0) {
                     System.out.print(ball + "볼 ");
                 }
                 if(strike != 0) {
-                    System.out.println(strike + "스트라이크");
+                    System.out.print(strike + "스트라이크");
                 }
                 if(strike == 0 && ball == 0) {
-                    System.out.println("낫싱");
+                    System.out.print("낫싱");
                 }
+                System.out.println();
                 if(strike == 3) {
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     gaming = false;
