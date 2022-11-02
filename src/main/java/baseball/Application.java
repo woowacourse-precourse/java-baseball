d403e2fe45d2be0e8ab4c05a01dc02848d6c2905
JIWEON-JEONG <dlektl6@naver.com>
2022-11-02 20:11:51 +0900

    feat : 유저가 입력한 값 받아오기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,25 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
+
+    private final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Application application = new Application();
-        application.start();
+        application.run();
+
+
 
     }
 
-    public void start() {
+    public void run() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            System.out.println("숫자를 입력해주세요 : ");
+            String input = scanner.nextLine();
+        }
     }
 }
