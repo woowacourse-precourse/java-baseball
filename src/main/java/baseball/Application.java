643bd7627d2c4814e2c44e0dd2c523d6a04de743
HyeonjungYun <guswnd9971@gmail.com>
2022-11-02 19:01:40 +0900

    feat: out의 갯수를 판별하는 메서드 distinguishOut() 추가.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,20 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
+
+    public static int distinguishOut (List<Integer> inputNumberList, List<Integer> computerNumberList) {
+        int outCounting = 0;
+
+        for (int i = 0; i <3; i++) {
+            if (inputNumberList.get(i) == computerNumberList.get(i)) {
+                outCounting += 1;
+            }
+        }
+
+        return outCounting;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
