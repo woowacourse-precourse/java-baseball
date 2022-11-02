633eae33e86b41c79dc1bb75036da3f1a2977e50
tjwjdgus12 <01048733438a@gmail.com>
2022-11-02 23:02:03 +0900

    feat: 랜덤 숫자 생성 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public final static int digit = 3;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    public static List<Integer> generateNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < digit) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(number)) {
+                numbers.add(number);
+            }
+        }
+        return numbers;
     }
 }
