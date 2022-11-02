173a6620524e59c1326c5ba8d83a686bc6c2551e
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-02 18:00:08 +0900

    컴퓨터의 서로 다른 숫자 3개 저장

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        comNumber();
+    }
+
+    // 컴퓨터의 서로 다른 숫자 3개 저장
+    public static List comNumber() {
+        List<Integer> comList = new ArrayList<>();
+
+        for (int i=0; i<3; i++) {
+            int num = Randoms.pickNumberInRange(1, 9);
+
+            if(!comList.contains(num)) {
+                comList.add(num);
+            } else {
+                i--;
+            }
+        }
+
+        return comList;
     }
 }
