beba31de3e8b36f822c61b90fc9df13529920e68
making-a-scene <4936joo@naver.com>
2022-11-02 19:46:58 +0900

    generate method : getPlaceValuesFromNum()

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+   public static List<Integer> getPlaceValuesFromNum(int num)  {
+        int count = 0;
+        List<Integer> placeValuesFromNum = new ArrayList<>();
+        for(int temp = num; temp > 0; temp /= 10, count++) {
+            if(temp % 10 == 0)  throw new IllegalArgumentException();
+            placeValuesFromNum.add(temp % 10);
+        }
+        if(count == 3)  return placeValuesFromNum;
+        else throw new IllegalArgumentException("세 자릿수를 입력해주세요.");
     }
 }
