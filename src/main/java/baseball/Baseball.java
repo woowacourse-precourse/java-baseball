8cbe0561fa734471b90a9d8357fdfdfed0d6b9cd
Jimin <thswlals0219@gmail.com>
2022-11-03 00:15:42 +0900

    feat: 사용자의 수를 입력 받는 기능 추가

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,7 +1,11 @@
 package baseball;
 
 public class Baseball {
+    private User user = new User();
+
     public void playGame() {
-            System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        user.hitTheBall();
     }
 }
---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+    public void hitTheBall() {
+        System.out.print("숫자를 입력해주세요 : ");
+        Console.readLine();
+    }
+}
