67832f9bfe90fc03c2be5c85be015aa510a2b6d4
xi_jjun <rlawowns97@naver.com>
2022-11-03 05:59:23 +0900

    feat: 사용자와 상호작용하는 기능 추가

---
+++ src/main/java/baseball/views/View.java
@@ -0,0 +1,39 @@
+package baseball.views;
+
+import baseball.model.CompareResultDto;
+
+public class View {
+	public void startMessage() {
+		Printer.printGameStartMessage();
+	}
+
+	public String baseballInput() {
+		Printer.printPlzEnterTheBaseballNumberMessage();
+		String userBaseBallNumber = Reader.getUserBaseBallNumber();
+
+		return userBaseBallNumber;
+	}
+
+	public void hintMessage(CompareResultDto compareResult) {
+		int ballCnt = compareResult.getBallCnt();
+		int strikeCnt = compareResult.getStrikeCnt();
+
+		if (isNoting(ballCnt, strikeCnt)) {
+			Printer.printHintNothingMessage();
+			return;
+		}
+
+		Printer.printHintCountMessage(ballCnt, strikeCnt);
+	}
+
+	private boolean isNoting(int ballCnt, int strikeCnt) {
+		return ballCnt + strikeCnt == 0;
+	}
+
+	public int endMessage() {
+		Printer.printGameEndingMessage();
+		String proceed = Reader.getUserGameProceedNumber();
+
+		return Integer.parseInt(proceed);
+	}
+}
