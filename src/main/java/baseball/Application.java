ac390b20c42e1256692982af0aea9a5b2ed73fa0
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2022-11-02 23:19:17 +0900

    feat: inplement generate 3 digit numberImplement generating 3 digit number with no overlap using Randoms.pickNumberInRange

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
+    public static List<Integer> generateNumber(){
+        List<Integer> num = new ArrayList<>();
+        while (num.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!num.contains(randomNumber)) {
+                num.add(randomNumber);
+            }
+        }
+        return num;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> test = generateNumber();
+        System.out.print(test);
     }
 }
