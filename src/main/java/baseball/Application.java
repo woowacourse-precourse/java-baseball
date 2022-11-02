ad43e34e58143170023f5ac8543c51170fdac7ae
jeujeon <1132jjw@naver.com>
2022-11-03 01:50:52 +0900

    feat(BaseballGame): 게임 진행 흐름을 위한 코드 작성
    gameStart 메서드 안에 while(true)를 넣어 게임을 중단하기 전까지 재시작 하도록 하였다.
    game 메서드 안에도 while(true)를 넣어 정답을 맞추기 전까지는 중단하지 못하게 하였다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,5 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
+        BaseballGame game = new BaseballGame();
+        game.gameStart();
     }
 }
---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,18 @@
+package baseball;
+
+public class BaseballGame {
+    public BaseballGame() {
+    }
+
+    public void gameStart() {
+        while (true) {
+            game();
+        }
+    }
+
+    public void game() {
+        while (true) {
+
+        }
+    }
+}
