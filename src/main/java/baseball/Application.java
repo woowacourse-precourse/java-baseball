486644396ae7e1d383928a42c9df247d8338ad85
jmj005 <jmj00005@naver.com>
2022-11-02 22:42:21 +0900

    feat: 게임 시작 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,11 @@ import java.util.List;
 
 public class Application {
 
+    private static void printGameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
     private static void createComputerNumber(List<Integer> computerNumber) {
         int randomNumber = Randoms.pickNumberInRange(1, 9);
         if (!computerNumber.contains(randomNumber)) {
