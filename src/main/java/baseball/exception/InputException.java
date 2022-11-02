9fa021b53fcd8e4c9b26f14cc3858a43a3b83fca
sangil <gsl0505@naver.com>
2022-11-02 21:23:22 +0900

    feat(Exception): 입력받은 값이 0을 포함할 경우 예외처리 기능 구현

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -6,14 +6,23 @@ public class InputException {
 	private static final int NUMBER_COUNT = 3;
 	private static final String ONE_LETTER_DIVISION_REGEX = "";
 	private static final String NUMBER_REGEX = "^[1-9]*$";
+	private static final String ZERO_NUMBER = "0";
 
-	private void checkNumberCount(String input) {
+	public static String checkNumberForm(String input) {
+		checkNumberCount(input);
+		checkDuplicatedNumber(input);
+		isNumber(input);
+		containZeroNumber(input);
+		return input;
+	}
+
+	private static void checkNumberCount(String input) {
 		if (input.length() != NUMBER_COUNT) {
 			throw new IllegalArgumentException();
 		}
 	}
 
-	private void checkDuplicatedNumber(String input) {
+	private static void checkDuplicatedNumber(String input) {
 		int distinctNumberCount = (int)Arrays.stream(input.split(ONE_LETTER_DIVISION_REGEX))
 			.map(Integer::parseInt)
 			.distinct()
@@ -23,11 +32,15 @@ public class InputException {
 		}
 	}
 
-	private void checkNumber(String input) {
+	private static void isNumber(String input) {
 		if (!input.matches(NUMBER_REGEX)) {
 			throw new IllegalArgumentException();
 		}
 	}
 
-
+	private static void containZeroNumber(String input) {
+		if (input.contains(ZERO_NUMBER)) {
+			throw new IllegalArgumentException();
+		}
+	}
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.exception.InputException;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
@@ -8,7 +9,7 @@ public class InputView {
 
 	public static String userNumber() {
 		System.out.print(INPUT_USER_NUMBER_MESSAGE);
-		return Console.readLine();
+		return InputException.checkNumberForm(Console.readLine());
 	}
 
 	public static String restartOrExit() {
