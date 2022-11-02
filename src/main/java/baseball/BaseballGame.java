fd2c4b1bdbba3a46c931193bb79287d4dba6cdd7
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-03 02:59:58 +0900

    feat(BaseballGame): 사용자의 입력을 받아 결과를 출력한다

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,51 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballGame {
+    private Computer computer;
+
+    public BaseballGame init() {
+        this.computer = new Computer();
+        return this;
+    }
+
+    public void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        computer.putAnswer(List.of(1, 2, 3));
+        while (true) {
+            List<Integer> guessNumbers = inputNumbers();
+            List<Integer> results = computer.returnResult(guessNumbers);
+            Integer strikeCnt = results.get(0);
+            Integer ballCnt = results.get(1);
+            if(strikeCnt == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        if (input.equals("2")) {
+            return;
+        }
+        if (input.equals("1")) {
+            start();
+        }
+        throw new IllegalArgumentException("잘못된 입력입니다");
+        // 컴퓨터가 랜덤숫자를 만든다
+        // 세자리 숫자 입력받는다
+    }
+
+    private List<Integer> inputNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        List<Integer> guessNumbers = new ArrayList<>();
+        String input = Console.readLine();
+        for (int i = 0; i < input.length(); i++) {
+            guessNumbers.add(Character.getNumericValue(input.charAt(i)));
+        }
+        return guessNumbers;
+    }
+}
