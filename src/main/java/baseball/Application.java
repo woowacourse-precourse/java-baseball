e1f64d435b1169351083f03b999cd4c5230e3801
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-11-02 19:56:39 +0900

    userInput 기능 추가
    사용자로부터 입력을 받아 int형으로 리턴하는 기능 추가.
    input_String_to_int을 호출하는데 이 함수는 예외 처리하면서 Stirng을 int로 바꿔준다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int user_input=userInput();
+    }
+
+    private static int userInput() {
+        String input_String;
+        int input_int;
+        Scanner scanner=new Scanner(System.in);
+
+        input_String=scanner.next();
+        input_int=input_String_to_int(input_String);
+        return input_int;
     }
 }
