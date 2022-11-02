dcda9a4a9acee47b06375dd203235185fb269c00
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 21:56:00 +0900

    Feat: 사용자 숫자 입력과 유효성 검사 클래스/메서드 생성

---
+++ src/main/java/domain/User.java
@@ -0,0 +1,20 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Console;
+import constant.Messages;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    public Numbers input() {
+        String userInput = Console.readLine();
+        List<Number> list = new ArrayList<>();
+
+        for (int i = 0; i < userInput.length(); i++) {
+            list.add(new Number(userInput.charAt(i) - '0'));
+        }
+
+        return new Numbers(list);
+    }
+}
