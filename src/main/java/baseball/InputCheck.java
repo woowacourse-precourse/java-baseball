02709a1e88d5712f02d08817b5bc2cd43fef2fda
eyeofsol <kingjihong@naver.com>
2022-11-03 01:00:22 +0900

    feat(InputCheck): 입력이 올바른지 확인하는 기능 구현
    InputCheck에 3자리 숫자인지 판별하는 기능 구현

---
+++ src/main/java/baseball/InputCheck.java
@@ -0,0 +1,28 @@
+package baseball;
+
+public class InputCheck {
+
+    public static boolean isInputError(String input) {
+        char temp;
+
+        if (input.length() != 3) {
+            return true;
+        }
+        if (hasNonDigit(input)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean hasNonDigit(String input) {
+        char temp;
+
+        for (int i = 0; i < input.length(); i++) {
+            temp = input.charAt(i);
+            if (!Character.isDigit(temp)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
