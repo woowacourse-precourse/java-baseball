0ff5de4c932e67475649719ccfd9df06c65e749b
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-02 16:03:49 +0900

    feat : 게임 로직 기능 추가
    처음 실행될 때 게임이 반드시 실행된다.
    GameHandler 를 통해 게임이 시작되고
    게임이 종료될 때 사용자가 새로운 게임을 진행할지, 그만할지 정할 수 있다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	public static final int GAME_CONTINUE_SIGNAL = 1;
+
+	public static void main(String[] args) {
+		int repetition;
+		do {
+			repetition = GameHandler.startGame(RandomNumber.makeRandomNumber());
+		} while (repetition == GAME_CONTINUE_SIGNAL);
+	}
+
 }
