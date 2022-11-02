b03c9bec481c7a193c390b8df9f6ccfe7e84c0a3
정핸모 <gusah009@naver.com>
2022-11-02 23:33:17 +0900

    feat: 사용자에게서 입력을 받기 전 "숫자 야구 게임을 시작합니다." 문구를 띄움

--- src/main/java/baseball/game/Baseball.java
+++ src/main/java/baseball/game/Baseball.java
@@ -48,6 +48,7 @@ public class Baseball implements Game {
   }
 
   private void input() {
+    System.out.println("숫자 야구 게임을 시작합니다.\n");
     String playerInput = Console.readLine();
     if (isInvalid(playerInput)) {
       throw new IllegalArgumentException();
