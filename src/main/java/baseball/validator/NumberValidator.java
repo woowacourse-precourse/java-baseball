55b7af67fc21d183ca00ac0361b014675b07a80d
whdcks420 <whdcks420@gmail.com>
2022-11-02 22:01:14 +0900

    feat: 중복된 숫자를 입력했을때 예외처리 기능 구현

--- src/main/java/baseball/validator/NumberValidator.java
+++ src/main/java/baseball/validator/NumberValidator.java
@@ -1,14 +1,19 @@
 package baseball.validator;
 
+import java.util.stream.Collectors;
+
 public class NumberValidator {
 
     public static void validate(String userNumber) {
         if (!userNumber.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException();
         }
-
         if (userNumber.length() != 3) {
             throw new IllegalArgumentException();
         }
+        if (userNumber.chars().mapToObj(i -> (char) i).collect(Collectors.toSet()).size() != 3) {
+            throw new IllegalArgumentException();
+        }
+
     }
 }
