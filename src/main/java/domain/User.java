349b88008538f994b56e2c10446fe9834b08977b
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 22:04:41 +0900

    feat: 사용자 입력 숫자 유효성 검사 코드 추가

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -8,12 +8,16 @@ import java.util.List;
 
 public class User {
     public Numbers input() {
-        String userInput = Console.readLine();
+//        String userInput = Console.readLine();
+        String userInput = "1234";
         List<Number> list = new ArrayList<>();
 
         for (int i = 0; i < userInput.length(); i++) {
             list.add(new Number(userInput.charAt(i) - '0'));
         }
+
+        new Numbers(list).isValidSize(list);
+        new Numbers(list).isDuplicate(list);
 
         return new Numbers(list);
     }
