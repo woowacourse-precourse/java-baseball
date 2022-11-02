398d9240e915ce77e47a8951ec317db29d8a93ba
JJ <jeongjae1996@naver.com>
2022-11-03 03:08:10 +0900

    feat: 컴퓨터의 수를 랜덤으로 생성하기 위한 기능 구현
    createRandomNumber 메서드 내부에 서로 다른 3자리 수를 ArrayList로 만들도록 구현했습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static List<Integer> createRandomNumber() {
+        List<Integer> computer_num = new ArrayList<>();
+
+        while (computer_num.size() < 3) {
+            int random_number = Randoms.pickNumberInRange(1, 9);
+            if (!computer_num.contains(random_number)) {
+                computer_num.add(random_number);
+            }
+        }
+        return computer_num;
+    }
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         while (true) {
+
+            List<Integer> computer_num = createRandomNumber();
+            System.out.println(computer_num);
             System.out.print("숫자를 입력해주세요 : ");
             break; // 기능 완전 구현 전까지 반복문을 탈출하기 위함.
         }
