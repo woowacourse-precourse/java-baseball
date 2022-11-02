0942a5f9df7f9132da283f6ee675fa9fc337683e
shinD <huipulci1@naver.com>
2022-11-03 04:42:52 +0900

    feat: 게임 진행 단계 두 수 비교 기능 구현
    게임 진행 단계 실행 시 사용자와 컴퓨터의 두 수를 비교하는 기능 구현.
    비교 이후 비교 결과 출력 기능 구현.

--- src/main/java/baseball/domain/step/GameProgressStep.java
+++ src/main/java/baseball/domain/step/GameProgressStep.java
@@ -1,11 +1,22 @@
 package baseball.domain.step;
 
 import baseball.application.context.BaseBallGameContext;
+import baseball.application.io.Writer;
+import baseball.domain.comparator.Comparator;
+import baseball.domain.computer.Computer;
+import baseball.domain.player.Player;
+import baseball.domain.result.CompareResult;
 
 public class GameProgressStep implements Step {
 
     @Override
     public void execute(BaseBallGameContext context) {
+        Player player = context.player();
+        Computer computer = context.computer();
+        Comparator comparator = context.comparator();
+
+        CompareResult compareResult =
+                comparator.compare(player.baseBallGameNumbers(), computer.baseBallGameNumbers());
 
     }
 
