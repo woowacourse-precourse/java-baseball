84d78da4db528f3bccb6a713f33d45e5209fc062
kanghyki <kanghyki@gmail.com>
2022-11-02 22:47:13 +0900

    Test: 테스트 파일 추가

---
+++ src/test/java/baseball/OpponentTest.java
@@ -0,0 +1,15 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OpponentTest {
+    @Test
+    void 테스트_테스트() {
+        String input = "(1,2)";
+        String result = input.substring(1, 4);
+
+        assertThat(result).isEqualTo("1,2");
+    }
+}
