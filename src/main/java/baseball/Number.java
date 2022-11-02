10149461668720a9befce7f86499c20501c6c430
dooboocookie <bkh3030@gmail.com>
2022-11-02 22:16:34 +0900

    feat: Number클래스 추가, 검증 로직 추가

---
+++ src/main/java/baseball/Number.java
@@ -0,0 +1,20 @@
+package baseball;
+
+public class Number {
+
+    private final String ERROR_MESSAGE_SIZE = "숫자는 0~9 사이 값만 입력가능합니다.";
+
+    private int number;
+
+    public Number(int number) {
+        validateSize(number);
+        this.number = number;
+    }
+
+    private void validateSize(int number) {
+        if ((number < 1) || (number > 9)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_SIZE);
+        }
+    }
+
+}
