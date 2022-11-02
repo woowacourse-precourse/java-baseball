d76ce90597b529c735271c072cc3f1765e0fde9e
jhon3242 <rldotnr2@gmail.com>
2022-11-02 22:09:59 +0900

    feat(출력 처리): 게임 시작 메시지 문구 출력 구현
    OutputView
    - 출력 부분을 담당하는 OutputView 클래스 구현
    - 게임 시작시 메시지를 출력하는 메서드 구현

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -2,10 +2,12 @@ package baseball.controller;
 
 import baseball.model.Validator;
 import baseball.view.InputView;
+import baseball.view.OutputView;
 
 public class BaseballController {
 	private final int CORRECT_LENGTH = 3;
 	public void start(){
+		OutputView.gameStartView();
 		Validator tryValidator = new Validator(InputView.tryAnswerInput(), CORRECT_LENGTH);
 	}
 }
