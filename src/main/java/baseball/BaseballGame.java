6e540c1ecd1a17c43e0fa72ad2fd97f802eb6409
ym2629 <dydals2629@naver.com>
2022-11-02 11:02:44 +0900

    feat: check ball

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -8,5 +8,21 @@ public class BaseballGame {
 	int ball;
 	int strike;
 	int user;
+	Ball computer = new Ball();
 	
+	void checkBall(String userAnswer) {
+		int result = 0;
+		char[] usernumber = userAnswer.toCharArray();
+		char[] computernumber = Integer.toString(computer.number).toCharArray();
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < 3; j++) {
+				if(j == i) {
+					continue;
+				}else if(usernumber[i] == computernumber[j]) {
+					result += 1;
+				}
+			}
+		}
+		ball = result;
+	}
 }
