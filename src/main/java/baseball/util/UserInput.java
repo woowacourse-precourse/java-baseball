ab2aed059104ea630562ead5ee40cc01fd0d63eb
yg0585 <yg0585@naver.com>
2022-11-02 19:54:00 +0900

    fix : User Input method 반환 type 변경

--- src/main/java/baseball/util/UserInput.java
+++ src/main/java/baseball/util/UserInput.java
@@ -1,10 +1,19 @@
 package baseball.util;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserInput {
-    public String getInput() {
+    public List<Integer> getInput() {
         String input = Console.readLine();
-        return input;
+        List<Integer> inputList = new ArrayList<>();
+
+        for (int idx = 0; idx < input.length(); idx++) {
+            char c = input.charAt(idx);
+            inputList.add(Character.getNumericValue(c));
+        }
+
+        return inputList;
     }
 }
