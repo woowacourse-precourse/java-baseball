75c695a81e2e45849476d372811914ebdf4dd778
01192mg <01192mg@gmail.com>
2022-11-02 22:12:02 +0900

    feat: 1~9 범위의 임의의 수를 생성하는 기능 구현

--- src/main/java/baseball/RandomThreeDigitNumberGenerator.java
+++ src/main/java/baseball/RandomThreeDigitNumberGenerator.java
@@ -1,9 +1,22 @@
 package baseball;
 
+import static baseball.utils.Constants.*;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RandomThreeDigitNumberGenerator {
     public static List<Integer> generate() {
-        return List.of(1, 2, 3);
+        List<Integer> threeDigitNumber = new ArrayList<>();
+        while (threeDigitNumber.size() < RANDOM_NUMBER_LENGTH) {
+            int randomNumber = getRandomNumber();
+            threeDigitNumber.add(randomNumber);
+        }
+        return threeDigitNumber;
+    }
+
+    private static int getRandomNumber() {
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
