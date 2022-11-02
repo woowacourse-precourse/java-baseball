f3b74371c0380cc152c8bd56af3318bba5994866
shinD <huipulci1@naver.com>
2022-11-03 02:07:15 +0900

    test: Player 테스트코드 작성
    BaseBallGameNumber 를 교체하는 메서드 테스트.

---
+++ src/test/java/baseball/domain/player/PlayerTest.java
@@ -0,0 +1,30 @@
+package baseball.domain.player;
+
+import baseball.domain.number.BaseBallGameNumbers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Player 는 ")
+class PlayerTest {
+
+    @Test
+    @DisplayName("BaseBallGameNumber를 교체할 수 있다.")
+    public void playerCanChangeBaseBallGameNumbers() {
+        // given
+        String numbers = "123";
+        String changeNumbers = "321";
+        BaseBallGameNumbers baseBallGameNumbers = new BaseBallGameNumbers(numbers);
+        BaseBallGameNumbers changedNumbers = new BaseBallGameNumbers(changeNumbers);
+        Player player = new Player();
+
+        // when
+        player.changeNumbers(baseBallGameNumbers);
+        player.changeNumbers(changedNumbers);
+
+        // then
+        assertThat(player.baseBallGameNumbers()).isEqualTo(changedNumbers);
+        assertThat(player.baseBallGameNumbers()).isNotEqualTo(baseBallGameNumbers);
+    }
+}
