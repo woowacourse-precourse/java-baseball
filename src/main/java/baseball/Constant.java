57ac26e452c50c0c9a1a6fe45cdb307f58bf980d
SH <gram753@naver.com>
2022-11-02 22:17:24 +0900

    feat: NOTHING_MESSAGE 상수 추가

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -6,6 +6,7 @@ public class Constant {
 	public final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 	public final String STRIKE_CALL = "스트라이크";
 	public final String BALL_CALL = "볼";
+	public final String NOTHING_MESSAGE = "낫싱";
 	public final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 	public final String END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 	public final String RESTART_GAME = "1";
