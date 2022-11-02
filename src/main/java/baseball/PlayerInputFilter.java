15329c6cf956274fdd36156a73d0f636f265f6e8
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-02 21:03:04 +0900

    feat: 실패하는 테스트를 위한 클래스 PlayerInputFilter 생성

---
+++ src/main/java/baseball/PlayerInputFilter.java
@@ -0,0 +1,33 @@
+package baseball;
+
+public class PlayerInputFilter {
+    private int[] numbers;
+
+    public PlayerInputFilter(){
+        numbers = new int[3];
+    }
+
+    public void checkIfNotNumber(String numbers){
+
+    }
+
+    public void checkIfNotThreeDigit(String numbers){
+
+    }
+
+    public void checkIfContainsZero(String numbers){
+
+    }
+
+    public void checkIfContainsMultiNumbers(String numbers){
+
+    }
+
+    public void saveInput(String numbers){
+
+    }
+
+    public int[] getResultNumbersArray(){
+        return new int[]{};
+    }
+}
