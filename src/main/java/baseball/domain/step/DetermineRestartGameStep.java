d90225ec0326dfeabe91a442e2b56c26006ae2b4
shinD <huipulci1@naver.com>
2022-11-03 05:41:28 +0900

    feat: 게임 재실행 여부 단계 실행 시 문구 출력 기능 구현
    게임 재실행 여부 문구를 출력하도록 구현.

---
+++ src/main/java/baseball/domain/step/DetermineRestartGameStep.java
@@ -0,0 +1,25 @@
+package baseball.domain.step;
+
+import baseball.application.context.BaseBallGameContext;
+import baseball.application.io.Writer;
+
+public class DetermineRestartGameStep implements Step {
+
+    private static final String RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    @Override
+    public void execute(BaseBallGameContext context) {
+        Writer writer = context.writer();
+        writer.println(RESTART_GAME_MESSAGE);
+    }
+
+    @Override
+    public Step next() {
+        return null;
+    }
+
+    @Override
+    public boolean executable() {
+        return true;
+    }
+}
