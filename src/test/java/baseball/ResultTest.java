f019ed8722835364385646ab2d7ca318a502b84a
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 23:20:41 +0900

    Test: 볼 문구 테스트

--- src/test/java/baseball/ResultTest.java
+++ src/test/java/baseball/ResultTest.java
@@ -24,4 +24,26 @@ public class ResultTest{
         Result result = new Result(cNum, uNum);
         assertEquals(result.getResult(), Messages.successMessage);
     }
+
+    @Test
+    void 볼_문구_출력_확인() {
+        List<Number> list = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            list.add(new Number(i));
+        }
+        Numbers cNum = new Numbers(list);
+
+        list = new ArrayList<>();
+        list.add(new Number(2));
+        list.add(new Number(3));
+        list.add(new Number(1));
+
+        Numbers uNum = new Numbers(list);
+        Result result = new Result(cNum, uNum);
+
+        assertEquals(result.getResult(), "3볼");
+    }
+
+
+
 }
