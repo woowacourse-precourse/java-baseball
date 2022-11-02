a554d5ca4a0dab879247fff2c9b1da0302a34098
HyeonjungYun <guswnd9971@gmail.com>
2022-11-02 19:21:35 +0900

    feat: strike 횟수를 판별하는 메서드 distinguishStrike()메서드 추가.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
 
@@ -14,6 +16,18 @@ public class Application {
         }
 
         return outCounting;
+    }
+
+    public static int distinguishStrike (List<Integer> inputNumberList, List<Integer> computerNumberList, int outCounting) {
+        int strikeCounting = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (computerNumberList.contains(inputNumberList.get(i))) {
+                strikeCounting += 1;
+            }
+        }
+
+        return strikeCounting - outCounting;
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
