62a2925ae6ccddbad0f1e066a8e5e3b9ca25b1b5
tjwjdgus12 <01048733438a@gmail.com>
2022-11-02 23:14:24 +0900

    feat: 숫자 리스트 유효성 검사 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,19 +6,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public final static int digit = 3;
     public static void main(String[] args) {
 
     }
 
     public static List<Integer> generateNumbers() {
         List<Integer> numbers = new ArrayList<>();
-        while (numbers.size() < digit) {
+        while (numbers.size() < 3) {
             int number = Randoms.pickNumberInRange(1, 9);
             if (!numbers.contains(number)) {
                 numbers.add(number);
             }
         }
         return numbers;
+    }
+
+    public static boolean isValid(List<Integer> numbers) {
+        if (numbers.size() != 3) {
+            return false;
+        }
+        return !numbers.get(0).equals(numbers.get(1))
+                && !numbers.get(1).equals(numbers.get(2))
+                && !numbers.get(2).equals(numbers.get(0));
     }
 }
