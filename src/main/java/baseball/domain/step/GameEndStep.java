ba34059c9146f0fc1364950f25103faa37705303
shinD <huipulci1@naver.com>
2022-11-03 04:55:28 +0900

    feat: 게임 종료 단계 구현
    GameEndStep 가 다음 단계로 [게임 재실행 여부 판단 단계]를 갖도록 구현.
    GameEndStep 실행 시 게임 종료 메세지를 출력하도록 구현.

--- src/main/java/baseball/domain/step/GameEndStep.java
+++ src/main/java/baseball/domain/step/GameEndStep.java
@@ -1,21 +1,29 @@
 package baseball.domain.step;
 
 import baseball.application.context.BaseBallGameContext;
+import baseball.application.io.Writer;
+import baseball.application.random.Range;
 
 public class GameEndStep implements Step{
 
+    private static final String RIGHT_ANSWER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    private static final Step NEXT = new DetermineRestartGameStep();
+
     @Override
     public void execute(BaseBallGameContext context) {
+        Writer writer = context.writer();
 
+        writer.println(RIGHT_ANSWER_MESSAGE);
     }
 
     @Override
     public Step next() {
-        return null;
+        return NEXT;
     }
 
     @Override
     public boolean executable() {
-        return false;
+        return true;
     }
 }
