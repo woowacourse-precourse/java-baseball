d58c7ce38418128238171bca32a006d318551167
unit74 <u__nit@naver.com>
2022-11-02 22:09:03 +0900

    refactor: 사용하지 않는 import문 최적화

--- src/test/java/baseball/MethodTest.java
+++ src/test/java/baseball/MethodTest.java
@@ -2,14 +2,11 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
