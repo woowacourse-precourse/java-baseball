3df60d6bcec122363252c2f78efec5e8ee264c7b
이한솔 <hansol8701@naver.com>
2022-11-03 02:38:07 +0900

    test: 사용자 숫자 입력시 유효성 검증 코드 추가

--- src/test/java/study/MyTest.java
+++ src/test/java/study/MyTest.java
@@ -17,4 +17,23 @@ public class MyTest {
         startGame();
         assertThat(true).isTrue();
     }
+
+    @Test
+    @DisplayName("사용자 숫자 입력")
+    void 사용자_숫자_입력시_유효성_검증() {
+        String input = "123";
+        String wrongInput1 = "1234";
+        String wrongInput2 = "120";
+        String wrongInput3 = "12";
+        String wrongInput4 = "1.3";
+        String wrongInput5 = "abc";
+        String wrongInput6 = "111";
+
+
+        assertThat(validation(input)).isTrue();
+        assertThat(validation(wrongInput1)).isTrue();
+        assertThatThrownBy(() -> {
+            validation(wrongInput1);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 }
