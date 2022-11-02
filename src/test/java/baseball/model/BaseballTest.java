93535fa65d2a890ea72bf0f6dd71cb2d241c2946
whdcks420 <whdcks420@gmail.com>
2022-11-02 21:53:19 +0900

    test: 서로다른 세자리의 수 생성 테스트

--- src/test/java/baseball/model/BaseballTest.java
+++ src/test/java/baseball/model/BaseballTest.java
@@ -1,4 +1,28 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 class BaseballTest {
-  
+
+    @Test
+    void 서로_다른_세자리_숫자_생성_성공() {
+        String baseballNumber = Baseball.generateNumber();
+        Set<Character> list = baseballNumber.chars().mapToObj(i -> (char) i).collect(Collectors.toSet());
+
+        Assertions.assertEquals(3, list.size());
+        Assertions.assertEquals(3, baseballNumber.length());
+    }
+
+    @Test
+    void 서로_다른_세자리_숫자_생성_실패() {
+        String baseballNumber = Baseball.generateNumber();
+        Set<Character> list = baseballNumber.chars().mapToObj(i -> (char) i).collect(Collectors.toSet());
+
+        Assertions.assertNotEquals(4, list.size());
+        Assertions.assertNotEquals(4, baseballNumber.length());
+    }
 }
