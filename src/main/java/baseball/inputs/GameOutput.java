5e4a02d484ff92f50ac4c314f42dcbfe437cff30
WhalesBob <ndw8200@naver.com>
2022-11-02 22:21:40 +0900

    feat : implement printWhenStart, printWhenUserWin method

--- src/main/java/baseball/inputs/GameOutput.java
+++ src/main/java/baseball/inputs/GameOutput.java
@@ -1,6 +1,10 @@
 package baseball.inputs;
 
 public class GameOutput {
+    public void printWhenStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public void printPlayingCount(int strikeCount, int ballCount){
         boolean isStrikeExists = (strikeCount > 0);
         boolean isBallExists = (ballCount > 0);
@@ -18,5 +22,9 @@ public class GameOutput {
         }else{
             System.out.printf("%d볼 %d스트라이크\n",ballCount,strikeCount);
         }
+    }
+
+    public void printWhenUserWin(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
