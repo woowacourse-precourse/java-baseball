fc0004c246914f9574d303450c009b938579a726
jinny-l <jinny.l.dev@gmail.com>
2022-11-02 23:51:16 +0900

    refactor(domain): Computer 클래스에 Random 클래스 포함하는 것으로 수정

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,13 +1,36 @@
 package baseball.domain;
 
-import baseball.util.Random;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
-	public static List<Integer> getNumbers() {
-		return Random.getThreeUniqueNumbers();
+	private static final int SIZE = 3;
+	private static final int START_INCLUSIVE = 1;
+	private static final int END_INCLUSIVE = 9;
+
+	private static final List<Integer> threeUniqueNumbers = new ArrayList<>(SIZE);
+
+	public List<Integer> getNumbers() {
+		return getThreeUniqueNumbers();
+	}
+
+	private static List<Integer> getThreeUniqueNumbers() {
+		int randomNumber;
+
+		while (threeUniqueNumbers.size() <= SIZE) {
+			randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+
+			if (threeUniqueNumbers.contains(randomNumber)) {
+				continue;
+			}
+
+			threeUniqueNumbers.add(randomNumber);
+		}
+
+		return threeUniqueNumbers;
 	}
 
 }
