272da4e58cba8c11c49a710cbe4049658bda268c
jhon3242 <rldotnr2@gmail.com>
2022-11-02 17:08:36 +0900

    feat(입력값 처리): 숫자가 아닌 경우 예외 처리 구현
    Validator
    - 입력값이 숫자가 아닌 경우 예외를 내뿜는 메서드 구현

---
+++ src/main/java/model/Validator.java
@@ -0,0 +1,19 @@
+package model;
+
+public class Validator {
+	final String INPUT_VALUE;
+	final int CORRECT_LENGTH;
+
+	public Validator(String INPUT_VALUE, int CORRECT_LENGTH) {
+		this.INPUT_VALUE = INPUT_VALUE;
+		this.CORRECT_LENGTH = CORRECT_LENGTH;
+	}
+
+	private void isAllNum(){
+		for (int i = 0; i < INPUT_VALUE.length(); i++) {
+			if (!Character.isDigit(INPUT_VALUE.charAt(i))){
+				throw new IllegalArgumentException("Only digits are possible");
+			}
+		}
+	}
+}
