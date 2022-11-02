5381b2d3b25049bbe1e88a295a57ce611ea3bfc8
shinD <huipulci1@naver.com>
2022-11-03 05:45:26 +0900

    feat: 게임 재실행 여부 단계에서 사용자 입력에 따라 다음 단계 설정
    게임을 새로 시작하는 경우 `게임 초기화 단계` 로 설정.
    게임을 종료하는 경우 `애플리케이션을 종료 단계`로 설정.
    잘못된 값이라면 `IllegalArgumentException` 발생시키고 애플리케이션을 종료.

--- src/main/java/baseball/domain/step/DetermineRestartGameStep.java
+++ src/main/java/baseball/domain/step/DetermineRestartGameStep.java
@@ -7,7 +7,13 @@ import baseball.application.io.Writer;
 public class DetermineRestartGameStep implements Step {
 
     private static final String RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String RESTART_GAME_FLAG = "1";
+    private static final String APPLICATION_EXIT_FLAG = "0";
 
+    private static final Step RESTART_GAME_STEP = new GameInitStep();
+    private static final Step APPLICATION_EXIT_STEP = new ApplicationExitStep();
+
+    private Step next = APPLICATION_EXIT_STEP;
 
     @Override
     public void execute(BaseBallGameContext context) {
@@ -17,11 +23,25 @@ public class DetermineRestartGameStep implements Step {
         Reader reader = context.reader();
         String input = reader.readLine();
 
+        setNextStep(input);
+    }
+
+    private void setNextStep(String input) {
+        if (input.equals(RESTART_GAME_FLAG)) {
+            this.next = RESTART_GAME_STEP;
+            return;
+        }
+        if (input.equals(APPLICATION_EXIT_FLAG)) {
+            this.next = APPLICATION_EXIT_STEP;
+            return;
+        }
+
+        throw new IllegalArgumentException("0 또는 1의 값을 입력하지 않았습니다.");
     }
 
     @Override
     public Step next() {
-        return null;
+        return next;
     }
 
     @Override
