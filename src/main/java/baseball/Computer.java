a623531d3ef46921f6b113abb44f788766475740
kevstevie <jianna442@gmail.com>
2022-11-02 21:30:09 +0900

    feat: class Computer 숫자 조합 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private Computer() {}
+
+    public List<Integer> generateNumber() {
+        List list = new ArrayList();
+        while (list.size() < 3) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (!list.contains(number)) {
+                list.add(number);
+            }
+        }
+        return list;
+    }
+}
