a3c007d3d5e9184e1833091381635fdba47dc7a9
shinD <huipulci1@naver.com>
2022-11-03 03:57:58 +0900

    test: BaseBallGameNumbers 정수 리스트를 통한 생성 테스트 추가
    정수 리스트를 통해서도 BaseBallGameNumbers가 생성되는지 확인하는 테스트 추가.
    올바르지 않은 리스트에 대해 IllegalArgumentException 를 발생시키는지 확인하는 테스트 추가.

--- src/test/java/baseball/domain/number/BaseBallGameNumbersTest.java
+++ src/test/java/baseball/domain/number/BaseBallGameNumbersTest.java
@@ -27,6 +27,19 @@ class BaseBallGameNumbersTest {
     }
 
     @Test
+    @DisplayName("정수 리스트를 통해 생성된다.")
+    public void constructedByIntegerList() {
+        // given
+        List<Integer> integers = List.of(1, 2, 3);
+
+        // when
+        BaseBallGameNumbers ballGameNumbers = new BaseBallGameNumbers(integers);
+
+        // then
+        assertThat(ballGameNumbers.numbers().size()).isEqualTo(3);
+    }
+
+    @Test
     @DisplayName("입력된 순서가 유지된다.")
     public void maintainInputOrder() {
         // given
@@ -48,8 +61,8 @@ class BaseBallGameNumbersTest {
 
 
     @Test
-    @DisplayName("총 3개의 BaseBallGameNumber를 생성하지 못했다면 IllegalArgumentException을 발생시킨다.")
-    public void throwIllegalArgumentExceptionWhenConstructedByInvalidInput() {
+    @DisplayName("문자열을 통해 총 3개의 BaseBallGameNumber를 생성하지 못했다면 IllegalArgumentException을 발생시킨다.")
+    public void throwIllegalArgumentExceptionWhenConstructedByInvalidStringInput() {
         // given
         String underRangeInput = "102";
         String duplicatedIntegerInput = "998";
@@ -73,6 +86,33 @@ class BaseBallGameNumbersTest {
 
         assertThrows(IllegalArgumentException.class,
                 () -> new BaseBallGameNumbers(consistOfAllNonIntegerInput));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new BaseBallGameNumbers(emptyInput));
+    }
+
+    @Test
+    @DisplayName("정수 리스트를 통해 총 3개의 BaseBallGameNumber를 생성하지 못했다면 IllegalArgumentException을 발생시킨다.")
+    public void throwIllegalArgumentExceptionWhenConstructedByInvalidIntegerInput() {
+        // given
+        List<Integer> underRangeInput = List.of(1, 0, 2);
+        List<Integer> upperRangeInput = List.of(12, 3, 5);
+        List<Integer> duplicatedIntegerInput = List.of(9, 9, 8);
+        List<Integer> largerThenInputSizeInput = List.of(9,8,7,6,5,4,3,2,1);
+        List<Integer> emptyInput = List.of();
+
+        // when, then
+        assertThrows(IllegalArgumentException.class,
+                () -> new BaseBallGameNumbers(underRangeInput));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new BaseBallGameNumbers(upperRangeInput));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new BaseBallGameNumbers(duplicatedIntegerInput));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> new BaseBallGameNumbers(largerThenInputSizeInput));
 
         assertThrows(IllegalArgumentException.class,
                 () -> new BaseBallGameNumbers(emptyInput));
