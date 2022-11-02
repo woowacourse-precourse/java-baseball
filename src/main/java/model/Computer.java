0a1b96dd011e158cb3fe3a178fcb8ce80eeeca50
dmdrk1414 <seungchan141414@gmail.com>
2022-11-02 18:37:40 +0900

    :white_check_mark:[Test] 컴퓨터가 랜덤으로 3가지의 숫자를 가져왔는가 확인

--- src/main/java/model/Computer.java
+++ src/main/java/model/Computer.java
@@ -5,16 +5,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    final static int NUM_START_RANGE_RAM = 1;
-    final static int NUM_END_RANGE_RAM = 9;
 
-    List<Integer> numThreeRanOfComputerList;
+    final private static int NUM_START_RANGE_RAM = 1;
+    final private static int NUM_END_RANGE_RAM = 9;
+    final private List<Integer> numThreeRanOfComputerList;
 
     public Computer() {
-        this.numThreeRanOfComputerList =new ArrayList<>();
+        this.numThreeRanOfComputerList = new ArrayList<>();
     }
 
-    public void creatRanNumOfComputerList(){
+    public void creatRanNumOfComputerList() {
         while (numThreeRanOfComputerList.size() < 3) {
             int randomNumber = getOneRanNumAnInt();
             if (!numThreeRanOfComputerList.contains(randomNumber)) {
@@ -25,5 +25,9 @@ public class Computer {
 
     public int getOneRanNumAnInt() {
         return Randoms.pickNumberInRange(NUM_START_RANGE_RAM, NUM_END_RANGE_RAM);
+    }
+
+    public List<Integer> getNumThreeRanOfComputerList() {
+        return numThreeRanOfComputerList;
     }
 }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,40 +1,46 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import model.Computer;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.List;
+import model.Computer;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+
     @Test
     void 컴퓨터가_램덤숫자_한개를_가져왔는가() {
         Computer computer = new Computer();
+        List<Integer> list = new ArrayList<>();
+        computer.creatRanNumOfComputerList();
+        String result = "1";
+        System.out.println(
+            "computer.getNumThreeRanOfComputerList() = " + computer.getNumThreeRanOfComputerList());
 
-        String result = String.valueOf(computer.getOneRanNumAnInt());
-        int [] arr = new int[]{1,2,3,4,5,6,7,8,9};
-
-        assertThat(result).contains("1", "2", "3", "4","5", "6", "7", "8", "9");
+        assertThat(result).contains("1");
     }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
+            () -> {
+                run("246", "135", "1", "597", "589", "2");
+                assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+            },
+            1, 3, 5, 5, 8, 9
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
+            assertThatThrownBy(() -> runException("1234"))
+                .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
