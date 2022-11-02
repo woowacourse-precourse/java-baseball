baa2249ec7ba2974acf0bc20f1835846fd196a8d
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-03 02:34:33 +0900

    feat(Computer): 컴퓨터는 입력에 따른 게임 결과를 반환한다

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
--- src/test/java/baseball/Computer.java
+++ src/test/java/baseball/Computer.java
@@ -1,15 +1,14 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
 public class Computer {
-    Integer first;
-    Integer second;
-    Integer third;
+    private List<Integer> answer; //answer 3개로 구성되어있는건 검증 못할까>?
     
     public void putAnswer(List<Integer> numbers) {
-        if(!(numbers.size() == 3)) {
+        if(numbers.size() != 3) {
             throw new IllegalArgumentException("입력값의 크기는 3개여야 합니다");
         }
         HashSet<Integer> resultDuplicateRemoved = new HashSet<>(numbers);
@@ -20,13 +19,38 @@ public class Computer {
         if (resultRangeValidated != 3) {
             throw new IllegalArgumentException("1이상 9이하의 숫자만 들어갈 수 있습니다");
         }
-        first = numbers.get(0);
-        second = numbers.get(1);
-        third = numbers.get(2);
+        answer = new ArrayList<>();
+        answer.add(numbers.get(0));
+        answer.add(numbers.get(1));
+        answer.add(numbers.get(2));
     }
 
 
-    public List<Integer> returnResult(List<Integer> integers) {
-        return null;
+    public List<Integer> returnResult(List<Integer> guessNumbers) { // 반환타입 List가 맞을까, []가 맞을까
+        List<Integer> result = new ArrayList<>();
+        Integer strikeCnt = 0;
+        Integer ballCnt = 0;
+
+        if (guessNumbers.size() != 3) {
+            throw new IllegalArgumentException("세 개의 숫자로 추리해야 합니다");
+        }
+        // 숫자들을 한개씩 for문 돈다
+        // 첫 번째 숫자를 비교
+        // 두 번째 숫자를 비교
+        // 세 번째 숫자를 비교
+        for (int i = 0; i < 3; i++) {
+            //입력자리수와 answer의 자리수가 같다면 -> strike
+            if (guessNumbers.get(i) == answer.get(i)) {
+                strikeCnt += 1;
+                continue;
+            }
+            // ball
+            if (answer.contains(guessNumbers.get(i))) {
+                ballCnt += 1;
+            }
+        }
+        result.add(strikeCnt);
+        result.add(ballCnt);
+        return result;
     }
 }
