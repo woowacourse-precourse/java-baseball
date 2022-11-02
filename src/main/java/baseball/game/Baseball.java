4d919dce055ea4a4efff8b73e2add44dea426583
정핸모 <gusah009@naver.com>
2022-11-02 19:43:20 +0900

    feat: 야구 클래스 생성자 구현

--- src/main/java/baseball/game/Baseball.java
+++ src/main/java/baseball/game/Baseball.java
@@ -1,6 +1,34 @@
 package baseball.game;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
+import java.util.List;
+
 public class Baseball implements Game {
+
+  private static final int GOAL_DIGIT = 3;
+  private static final int START_POSSIBLE_NUMBER = 1;
+  private static final int END_POSSIBLE_NUMBER = 9;
+
+  private final int[] computerNumbers = new int[GOAL_DIGIT];
+  private final int[] playerNumbers = new int[GOAL_DIGIT];
+
+  private int strike;
+  private int ball;
+
+  public Baseball() {
+    this.strike = 0;
+    this.ball = 0;
+    setComputerNumbers();
+  }
+
+  private void setComputerNumbers() {
+    List<Integer> uniqueNumbers = pickUniqueNumbersInRange(START_POSSIBLE_NUMBER,
+        END_POSSIBLE_NUMBER, GOAL_DIGIT);
+    for (int i = 0; i < GOAL_DIGIT; i++) {
+      computerNumbers[i] = uniqueNumbers.get(i);
+    }
+  }
 
   @Override
   public void play() {
