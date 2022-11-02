03dca377b3990c9af76bf03bc359661732f53742
ym2629 <dydals2629@naver.com>
2022-11-02 09:23:16 +0900

    feat: get random number for a ball

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -1,5 +1,13 @@
 package baseball;
 
-public class Ball {
+import java.util.*;
 
+public class Ball {
+	static int MAX_NUM = 987;
+	static int MIN_NUM = 123;
+	int ballNumber;
+
+	int randomNumberBall() {
+		return (int) (Math.random()*(MAX_NUM-MIN_NUM))+MIN_NUM;
+	}
 }
