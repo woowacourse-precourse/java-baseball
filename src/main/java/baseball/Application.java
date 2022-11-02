995ce47073b2d335d10f57ecaf897eaf4ff7308b
mydreamis-18 <hitoryok@naver.com>
2022-11-02 17:38:36 +0900

    [feat] startGame() 함수와 getPlayerNumbers() 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,38 @@
 package baseball;
 
+import java.util.List;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Application {
-	
-    public static void main(String[] args) {
-    	
-    	System.out.println("숫자 야구 게임을 시작합니다.");
-    }
+
+	public static void main(String[] args) {
+
+		startGame();
+
+		ArrayList<String> playerNumbers = getPlayerNumbers();
+		
+		System.out.println(playerNumbers);
+	}
+
+	public static ArrayList<String> getPlayerNumbers() {
+
+		Scanner scin = new Scanner(System.in);
+		
+		String numbers = scin.next();
+		
+		List<String> numberList = Arrays.asList(numbers.split(""));
+		
+		ArrayList<String> playerNumbers = new ArrayList<String>(numberList);
+		
+		scin.close();
+		
+		return playerNumbers;
+	}
+
+	public static void startGame() {
+
+		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
 }
