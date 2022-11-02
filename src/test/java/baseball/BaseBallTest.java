be4000bdb8a7c1bc6ae61e0cfd9f074d608afe5d
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 23:34:53 +0900

    Test: 라운드 테스트

---
+++ src/test/java/baseball/BaseBallTest.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+public class BaseBallTest {
+
+    @Test
+    void round_확인() {
+        BaseBall baseBall = new BaseBall();
+        baseBall.roundOfGame();
+    }
+}
