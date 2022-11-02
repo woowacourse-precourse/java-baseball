e4402aa8fc5f6c08f080aefbe07309d7616169ad
greeng00se <thegreengoose7@gmail.com>
2022-11-02 17:13:17 +0900

    feat(BaseBalls): Baseball 리스트를 관리하는 일급 컬렉션인 Baseballs 클래스 추가

---
+++ src/main/java/baseball/domain/Baseballs.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Baseballs {
+
+    List<Baseball> baseballs;
+
+    public Baseballs(List<Baseball> baseballs) {
+        this.baseballs = baseballs;
+    }
+}
