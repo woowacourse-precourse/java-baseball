f961a0b5da2e09ca066353ab8c0322f82e72fccc
kokodak <kokodakadokok@gmail.com>
2022-11-02 23:16:23 +0900

    feat(gameController): 게임 시작 기능 일부 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,13 +1,22 @@
 package baseball.controller;
 
+import baseball.model.Computer;
+import baseball.model.Judgement;
 import baseball.model.User;
 import baseball.view.InputView;
 
 public class GameController {
     User user = new User();
+    Computer computer = new Computer();
+    Judgement judgement = new Judgement();
 
     public void gameStart() {
-        String userInput = InputView.getUserInput();
-        user.setUserInputNumber(userInput);
+        computer.generateRandomNumber();
+
+        while (true) {
+            String userInput = InputView.getUserInput();
+            user.setUserInputNumber(userInput);
+
+        }
     }
 }
