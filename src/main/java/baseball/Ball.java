5c47dfd5f7ae14fcba73172fc5777968478602fa
jkroh1995 <85445649+jkroh1995@users.noreply.github.com>
2022-11-03 02:14:34 +0900

    feat(Ball) : 사용자의 입력값에서 예외를 확인하는 기능(valid() series) 구현

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -4,16 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Ball {
 
-    private List <Integer> ball;
+    private List<Integer> ball;
 
-    public Ball(){
+    public Ball() {
 
     }
 
-    public void makeComputerBalls(){
+    public void makeComputerBalls() {
         List<Integer> comBall = new ArrayList<>();
         while (comBall.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -21,26 +22,58 @@ public class Ball {
                 comBall.add(randomNumber);
             }
         }
-        this.ball=comBall;
+        this.ball = comBall;
     }
 
-    public void makeHumanBalls(){
+    public void makeHumanBalls() {
         List<Integer> humanBall = new ArrayList<>();
         System.out.print("숫자를 입력해주세요 : ");
-        while (humanBall.size() < 3) {
-            String [] humanInput = Console.readLine().split("");
-            if(validate()){
-                arrayToList(humanBall, humanInput);
-            }
+        String[] humanInput = Console.readLine().split("");
+        if (validate(humanInput)) {
+            arrayToList(humanBall, humanInput);
         }
         this.ball = humanBall;
     }
 
     private void arrayToList(List<Integer> humanBall, String[] humanInput) {
-        for(int i = 0; i< humanInput.length; i++){
+        for (int i = 0; i < humanInput.length; i++) {
             humanBall.add(Integer.parseInt(humanInput[i]));
         }
     }
 
+    private boolean validate(String[] arr) {
+        return true;
+    }
 
+    private boolean validOnlyNumber(String[] arr) {
+        String pattern = "^[0-9]*$";
+        StringBuilder validateIndex = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            validateIndex.append(arr[i]);
+        }
+        return !Pattern.matches(pattern, validateIndex.toString());
+    }
+
+    private boolean validDuplicate(String[] arr) {
+        int validateIndex = 0;
+        if (arr[0].equals(arr[1]) || arr[1].equals(arr[2]) || arr[0].equals(arr[2])) {
+            validateIndex = 1;
+        }
+        return validateIndex == 1;
+    }
+
+    private boolean validNonZeroValue(String[] arr) {
+        int validateIndex = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals("0")) {
+                validateIndex = 1;
+                break;
+            }
+        }
+        return validateIndex == 1;
+    }
+
+    private boolean validLength(String[] arr) {
+        return arr.length != 3;
+    }
 }
