d59a3f661e5c91e0a2bbba8bfe3147e09653d4b9
정핸모 <gusah009@naver.com>
2022-11-03 00:33:52 +0900

    feat: 메인 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.game.Baseball;
+import baseball.service.GamePlay;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+  public static void main(String[] args) {
+    // TODO: 프로그램 구현
+    GamePlay.start(Baseball.class);
+  }
 }
