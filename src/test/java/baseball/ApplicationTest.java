4745b395fecd190ed9109bf13224e527c5423848
dmdrk1414 <seungchan141414@gmail.com>
2022-11-03 00:44:37 +0900

    :white_check_mark:[Test] 유저숫자_스트라이트_볼_낫싱_체크_case_9 추가
    user : 798, 486, 589, 497, 465, 564, 464, 564, 654, 899, 789
    computer : 123
    결과 : 스트라이크 =>  0 , 볼 => 0

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -247,6 +247,31 @@ class ApplicationTest extends NsTest {
         boolean result = resultList.stream().allMatch(list -> list.containsAll(List.of(0, 3)));
         assertThat(result).isTrue();
     }
+
+    @Test
+        // Computer.isRightThreeNumOfComputer
+        // 스트라이크 0 , 볼 0
+    void 유저숫자_스트라이트_볼_낫싱_체크_case_9() {
+        int[] testInt = new int[]{798, 486, 589, 497, 465, 564, 464, 564, 654, 899, 789};
+        Computer computer = new Computer();
+        computer.numThreeRanOfComputerList = new ArrayList<>(List.of(
+            1, 2, 3
+        ));
+        ArrayList<ArrayList<Integer>> resultList = new ArrayList<>();
+        int resultStrike = 0;
+        int resultBall = 0;
+        for (int num : testInt) {
+            computer.setCntBall(0);
+            computer.setCntStrike(0);
+            computer.isRightThreeNumOfComputer(num);
+            resultStrike = computer.getCntStrike();
+            resultBall = computer.getCntBall();
+            ArrayList<Integer> list = new ArrayList<>(List.of(resultStrike, resultBall));
+            resultList.add(list);
+        }
+        boolean result = resultList.stream().allMatch(list -> list.containsAll(List.of(0, 0)));
+        assertThat(result).isTrue();
+    }
     // computer Test 종료
 
     @Test
