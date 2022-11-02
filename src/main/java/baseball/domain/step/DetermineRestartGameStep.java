64819ae36df904b98c48e41b531459581aff3e10
shinD <huipulci1@naver.com>
2022-11-03 05:43:10 +0900

    feat: 게임 재실행 여부 단계 실행 시 사용자로부터 입력 받아오는 기능 구현

--- src/main/java/baseball/domain/step/DetermineRestartGameStep.java
+++ src/main/java/baseball/domain/step/DetermineRestartGameStep.java
@@ -1,16 +1,22 @@
 package baseball.domain.step;
 
 import baseball.application.context.BaseBallGameContext;
+import baseball.application.io.Reader;
 import baseball.application.io.Writer;
 
 public class DetermineRestartGameStep implements Step {
 
     private static final String RESTART_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+
     @Override
     public void execute(BaseBallGameContext context) {
         Writer writer = context.writer();
         writer.println(RESTART_GAME_MESSAGE);
+
+        Reader reader = context.reader();
+        String input = reader.readLine();
+
     }
 
     @Override
