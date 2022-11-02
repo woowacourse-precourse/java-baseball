547a06409a67b983d83d517f0092bf3258dc04f8
ym2629 <dydals2629@naver.com>
2022-11-02 09:30:59 +0900

    feat: init number of ball
    assign a random number

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -5,8 +5,12 @@ import java.util.*;
 public class Ball {
 	static int MAX_NUM = 987;
 	static int MIN_NUM = 123;
-	int ballNumber;
-
+	int number;
+	
+	void Ball() {
+		number = randomNumberBall();
+	}
+	
 	int randomNumberBall() {
 		return (int) (Math.random()*(MAX_NUM-MIN_NUM))+MIN_NUM;
 	}
