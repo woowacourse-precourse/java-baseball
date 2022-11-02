6a3d329a53d3761396f08daa77d405e8da35a429
소대훈 <theogns0681@naver.com>
2022-11-02 18:14:13 +0900

    [feat] Book클래스 구현

---
+++ src/main/java/baseball/ball/Ball.java
@@ -0,0 +1,28 @@
+package baseball.ball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ball {
+    private int score;
+
+    public int getScore() {
+        return score;
+    }
+    Ball(){
+        this.score = generateScore();
+    }
+
+    public int generateScore(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer.get(0) * 100 + computer.get(1) * 10 + computer.get(2);
+    }
+}
