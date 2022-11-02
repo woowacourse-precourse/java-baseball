1ad378e084a135d48b4cc0eb9514170114e4265f
whdcks420 <whdcks420@gmail.com>
2022-11-02 22:14:54 +0900

    refector: 불필요한 테스트 케이스 삭제

--- src/test/java/baseball/model/BaseballTest.java
+++ src/test/java/baseball/model/BaseballTest.java
@@ -1,28 +1,21 @@
 package baseball.model;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class BaseballTest {
+
+    private final String BASEBALL_NUMBER = Baseball.generateNumber();
 
     @Test
     void 서로_다른_세자리_숫자_생성_성공() {
-        String baseballNumber = Baseball.generateNumber();
-        Set<Character> list = baseballNumber.chars().mapToObj(i -> (char) i).collect(Collectors.toSet());
+        Set<Character> list = BASEBALL_NUMBER.chars().mapToObj(i -> (char) i).collect(Collectors.toSet());
 
-        Assertions.assertEquals(3, list.size());
-        Assertions.assertEquals(3, baseballNumber.length());
-    }
-
-    @Test
-    void 서로_다른_세자리_숫자_생성_실패() {
-        String baseballNumber = Baseball.generateNumber();
-        Set<Character> list = baseballNumber.chars().mapToObj(i -> (char) i).collect(Collectors.toSet());
-
-        Assertions.assertNotEquals(4, list.size());
-        Assertions.assertNotEquals(4, baseballNumber.length());
+        assertEquals(3, list.size());
+        assertEquals(3, BASEBALL_NUMBER.length());
     }
 }
