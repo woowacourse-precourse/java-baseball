2333af4252b7c2ab978807680879b0e8314dbb0f
ym2629 <dydals2629@naver.com>
2022-11-02 09:01:16 +0900

    feat: output result of everything right

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -8,4 +8,9 @@ public class Output {
 	void wrongEverything() {
 		System.out.println("낫싱");
 	}
+	
+	void rightEverything() {
+		System.out.println("3스트라이크");
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+	}
 }
