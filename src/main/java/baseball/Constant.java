0915e04fd96b735dce327bff3c442e1ae1f3c9a4
SH <gram753@naver.com>
2022-11-02 20:28:15 +0900

    feat: 상수 목록을 관리하는 Constant 클래스 생성

---
+++ src/main/java/baseball/Constant.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class Constant {
+
+	public final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+	public final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+	public final String STRIKE_CALL = "스트라이크";
+	public final String BALL_CALL = "볼";
+	public final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	public final String END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	public final String RESTART_GAME = "1";
+	public final String END_GAME = "2";
+
+}
