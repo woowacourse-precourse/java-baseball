6c28675065f14c8ebe3c355f24d72c681edf3543
unknown <angelbaek>
2022-11-02 16:00:36 +0900

    사용자가 입력하는 숫자 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	/* 
+    	 * 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
+    	 * 3항 연산자를 쓰지 않는다.
+    	 * indent(인덴트, 들여쓰기) depth를 3이 넘지 않도록 구현한다. 2까지만 허용한다.
+			예를 들어 while문 안에 if문이 있으면 들여쓰기는 2이다.
+			힌트: indent(인덴트, 들여쓰기) depth를 줄이는 좋은 방법은 함수(또는 메서드)를 분리하면 된다.
+    	 */
+    	// 서로 다른 3자리수    	
+    	System.out.println("숫자 야구 게임을 시작합니다.");
+    	System.out.println("숫자를 입력해주세요 : ");
+    	final String StringUserAnotherNumbers = camp.nextstep.edu.missionutils.Console.readLine(); // 라이브러리
+    	final int intUserAnotherNumbers = Integer.parseInt(StringUserAnotherNumbers);
+    	System.out.println(intUserAnotherNumbers);
+    	    	
     }
 }
