1cf77f29250d4152a8c4259b2d550998719bf09d
whdcks420 <whdcks420@gmail.com>
2022-11-02 22:24:02 +0900

    test(validator): 숫자가 아닌 문자 입력시 예외 test

--- src/test/java/baseball/validator/NumberValidatorTest.java
+++ src/test/java/baseball/validator/NumberValidatorTest.java
@@ -1,4 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball.validator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class NumberValidatorTest {
-  
+
+    @Test
+    void 숫자가_아닌_문자_입력_예외() {
+        String test = "bellCold";
+        assertThrows(IllegalArgumentException.class, () -> NumberValidator.validate(test));
+    }
 }
