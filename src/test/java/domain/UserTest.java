3d6c3e98f09498e8700509c7e37c232414e7aab5
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 21:56:28 +0900

    Test: 사용자 숫자 외 입력 예외 처리테스트

---
+++ src/test/java/domain/UserTest.java
@@ -0,0 +1,19 @@
+package domain;
+
+import constant.Messages;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class UserTest {
+    @Test
+    void 숫자외_문자_입력시_예외_처리() {
+        User user = new User();
+
+        IllegalArgumentException exception =
+                assertThrows(IllegalArgumentException.class, () -> user.input());
+
+        assertEquals(exception.getMessage(), Messages.wrongInput);
+    }
+}
