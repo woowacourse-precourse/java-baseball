b675f1debcc7557d470aa0138922e6ce2203340f
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-02 22:13:22 +0900

    Feat : 컴퓨터 랜덤 숫자 선택 기능 추가 1에서 9까지의 서로다른 임의의 수 3개를 선택하여 저장 missionutils 의 Randoms 및 Console API 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        // commit test
+        Application app = new Application();
+        List<Integer> computer = new ArrayList<>(app.SelectRandomNumbers());
+
+    }
+
+    public List<Integer> SelectRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)){
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
     }
 }
