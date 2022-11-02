3a107e8435e9f53c931e88d0c745cd6a46840f23
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 18:16:09 +0900

    Feat:add String comment constant

---
+++ src/main/java/baseball/constant.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class constant {
+    static final String gameStart = "숫자 야구 게임을 시작합니다.";
+    static final String inputNumber = "숫자를 입력해주세요 : ";
+    static final String ball = "볼";
+    static final String strike = "스트라이크";
+    static final String correctNumber = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    static final String restartOrClose = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
