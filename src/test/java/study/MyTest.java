a128184f3e31ab0c038dacc874f5fe36394eece0
이한솔 <hansol8701@naver.com>
2022-11-03 02:36:58 +0900

    feat: 게임 시작

--- src/test/java/study/MyTest.java
+++ src/test/java/study/MyTest.java
@@ -1,9 +1,20 @@
 package study;
 
+import baseball.Application;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static baseball.Application.startGame;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class MyTest {
 
-
+    @Test
+    @DisplayName("게임 시작")
+    void 게임_시작() {
+        startGame();
+        assertThat(true).isTrue();
+    }
 }
