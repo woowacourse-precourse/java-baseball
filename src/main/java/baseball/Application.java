eab601f21c88446cdd0a4775bf7f801781f26236
junhei Lee <jun2ee22@naver.com>
2022-11-02 22:01:25 +0900

    feat: 게임 시작 시 컴퓨터가 준비하는 기능
    시작 문구 출력, 정답 숫자 추출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
+    private static final int answerLength = 3;
+
+    private static List<Integer> startGame() {
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        List<Integer> correctAnswer = new ArrayList<>();
+        while (correctAnswer.size() < answerLength) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!correctAnswer.contains(randomNumber)) {
+                correctAnswer.add(randomNumber);
+            }
+        }
+        return  correctAnswer;
+    }
+
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
     }
 }
