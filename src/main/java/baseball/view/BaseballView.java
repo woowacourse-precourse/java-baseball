83e65911b9d8cfac63183450b3c9638a7a07399f
whdcks420 <whdcks420@gmail.com>
2022-11-03 00:29:35 +0900

    feat(view): 현재 스코어 출력 기능 구현

--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import baseball.model.Score;
+
 public class BaseballView {
 
     public void showStart() {
@@ -9,4 +11,24 @@ public class BaseballView {
     public void showInput() {
         System.out.print("숫자를 입력해주세요 : ");
     }
+
+    public String showScore(Score currentScore) {
+        int strike = currentScore.getStrike();
+        int ball = currentScore.getBall();
+
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        }
+        if (strike == 0 && ball != 0) {
+            System.out.println(ball + "볼");
+        }
+        if (strike != 0 && ball != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+        if (strike != 0 && ball == 0) {
+            System.out.println(strike + "스트라이크");
+        }
+        return strike + "스트라이크";
+    }
+
 }
