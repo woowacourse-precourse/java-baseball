c247c744b8f203d95fb7de4f9d85de6da0dcfff6
shinD <huipulci1@naver.com>
2022-11-02 22:47:39 +0900

    test: 게임 시작 단계 실행 시 시작 메세지 출력 테스트 추가
    GameStartStep이 실행 시 시작 메세지를 출력하는지 테스트.

--- src/test/java/baseball/domain/step/GameStartStepTest.java
+++ src/test/java/baseball/domain/step/GameStartStepTest.java
@@ -1,9 +1,11 @@
 package baseball.domain.step;
 
+import baseball.application.context.BaseBallGameContext;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
 @DisplayName("GameStartStep 은 ")
 class GameStartStepTest {
@@ -32,5 +34,20 @@ class GameStartStepTest {
 
         // then
         assertThat(next).isExactlyInstanceOf(GameInitStep.class);
+    }
+
+    @Test
+    @DisplayName("실행 시 게임 실행 문구를 출력한다.")
+    public void printStartMessageWhenExecuted() throws Exception {
+        // given
+        final String startMessage = "숫자 야구 게임을 시작합니다.";
+        BaseBallGameContext context = mock(BaseBallGameContext.class);
+        GameStartStep gameStartStep = new GameStartStep();
+
+        // when
+        gameStartStep.execute(context);
+
+        // then
+        verify(context, times(1)).println(startMessage);
     }
 }
