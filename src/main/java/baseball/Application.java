70d19c4eff5b16810ac08ed69d2387d0ecf959c1
jmj005 <jmj00005@naver.com>
2022-11-02 20:18:25 +0900

    feat: 컴퓨터 랜덤 3자리의 수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    private static void createComputerNumber(List<Integer> computerNumber) {
+        int randomNumber = Randoms.pickNumberInRange(1, 9);
+        if (!computerNumber.contains(randomNumber)) {
+            computerNumber.add(randomNumber);
+        }
+    }
+
+    private static List<Integer> getComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            createComputerNumber(computerNumber);
+        }
+        return computerNumber;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
