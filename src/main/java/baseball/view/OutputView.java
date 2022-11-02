f8949ca7819293fba867e7426d8746ed7de37515
greeng00se <thegreengoose7@gmail.com>
2022-11-03 00:41:22 +0900

    refactor(OutputView): StartView -> OutputView로 클래스명 변경
    - 싱글턴 인스턴스로 생성하기 위해 enum으로 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,9 +1,11 @@
 package baseball.view;
 
-public class StartView {
+public enum OutputView {
+    INSTANCE;
+
     private final static String START_BASEBALL_GAME = "숫자 야구 게임을 시작합니다.";
 
-    public static void startBaseballGame() {
+    public void startBaseballGame() {
         System.out.println(START_BASEBALL_GAME);
     }
 }
--- src/test/java/baseball/view/OutputViewTest.java
+++ src/test/java/baseball/view/OutputViewTest.java
@@ -8,8 +8,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class StartViewTest {
+class OutputViewTest {
     private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final OutputView sut = OutputView.INSTANCE;
 
     @BeforeEach
     public void setup() {
@@ -24,8 +25,7 @@ class StartViewTest {
 
     @Test
     void 게임을_시작하면_숫자_야구_게임을_시작합니다_라고_화면에_출력한다() {
-        StartView.startBaseballGame();
-
+        sut.startBaseballGame();
         assertThat(output.toString()).isEqualTo("숫자 야구 게임을 시작합니다.\n");
     }
 }
