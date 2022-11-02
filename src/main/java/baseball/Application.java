003aec037a95c90f41b3e2436fe9d1af3bf4fc20
eunbii0213 <eunbii0213@naver.com>
2022-11-03 06:42:54 +0900

    feat: 플레이어가 서로 다른 3개의 수 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,11 +4,20 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Computer computer = makeComputerEntity();
+        Player player = makePlayerEntity();
     }
+
     private static Computer makeComputerEntity() {
         Computer computer = new Computer();
         computer.makeRandomNumber();
 
         return computer;
+    }
+
+    private static Player makePlayerEntity() {
+        Player player = new Player();
+        player.makeUserNumber();
+
+        return player;
     }
 }
---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,64 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+	private static final int UNIT_HUNDRED = 100;
+	private static final int UNIT_TEN = 10;
+	private static final int INIT_LIST_CAPACITY = 3;
+	private static final int INIT_INDEX = 0;
+	private static final String USER_INPUT_GUIDE_MESSAGE = "숫자를 입력해주세요 : ";
+	private static final String ERROR = "[ERROR]";
+	public static List<Integer> userNumberList;
+
+	public Player() {
+		userNumberList = new ArrayList<>(INIT_LIST_CAPACITY);
+	}
+
+	protected void makeUserNumber() {
+		initUserNumber();
+		checkUserInput();
+	}
+
+	private void checkUserInput() {
+		int userInput = Integer.parseInt(inputNumber());
+
+		addUserNumber(userInput);
+
+		for (int targetIndex = INIT_INDEX; targetIndex < userNumberList.size() - 1; targetIndex++) {
+			int target = userNumberList.get(targetIndex);
+
+			checkSameNumberExist(targetIndex, target);
+		}
+	}
+
+	private String inputNumber() {
+		System.out.print(USER_INPUT_GUIDE_MESSAGE);
+		String str = Console.readLine();
+
+		return str;
+	}
+
+	private void checkSameNumberExist(int targetIndex, int target) {
+		for (int compareIndex = targetIndex + 1; compareIndex < userNumberList.size(); compareIndex++) {
+			int compareNumber = userNumberList.get(compareIndex);
+
+			if (target == compareNumber) {
+				throw new IllegalArgumentException(ERROR + "중복되는 숫자는 입력할 수 없습니다.");
+			}
+		}
+	}
+
+	private void addUserNumber(int userNumber) {
+		userNumberList.add(userNumber / UNIT_HUNDRED);
+		userNumberList.add((userNumber % UNIT_HUNDRED) / UNIT_TEN);
+		userNumberList.add((userNumber % UNIT_HUNDRED) % UNIT_TEN);
+	}
+
+	private void initUserNumber() {
+		userNumberList.clear();
+	}
+}
