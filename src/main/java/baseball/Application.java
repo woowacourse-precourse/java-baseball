5634c9b67118137bcb09ebf7938b6bae28afcc40
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-02 22:56:28 +0900

    feat() : main 메서드 작성
     - main 메서드에 전체적인 과정 처리하는 코드 추가
     - 3스트라이크 처리하는 부분에서 코드 수정 필요

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,6 +8,26 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computerNumber = getThreeRandomNumber();
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            List<Integer> userNumber = disposeOfInputNumber();
+            int numberOfStrikes = getNumberOfStrikes(computerNumber, userNumber);
+            int numberOfBalls = getNumberOfBalls(computerNumber, userNumber);
+            String gameResult = disposeOfGameResult(numberOfStrikes, numberOfBalls);
+            System.out.println(gameResult);
+            if (gameResult.equals("3스트라이크")) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료.");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int restartOrNot = Integer.parseInt(Console.readLine());
+                if (restartOrNot == 1) {
+                    computerNumber = getThreeRandomNumber();
+                    continue;
+                }
+                break;
+            }
+        }
     }
 
     public static List<Integer> getThreeRandomNumber() {
