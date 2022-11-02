cd248032c082ceb4604ae03e253a5e1ec610126e
sangil <gsl0505@naver.com>
2022-11-02 22:12:34 +0900

    style: 자바 코드 컨벤션에 맞게 LF 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,9 +3,9 @@ package baseball;
 import baseball.controller.BaseballGame;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        BaseballGame game = new BaseballGame();
-        game.run();
-    }
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+		BaseballGame game = new BaseballGame();
+		game.run();
+	}
 }
