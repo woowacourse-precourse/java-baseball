e853ff9157293bc666aa11c7e75c4973ff03bc60
jhon3242 <rldotnr2@gmail.com>
2022-11-02 22:25:57 +0900

    feat(출력 처리): 게임 종료 후 재시작 여부 메시지 처리
    OutputView
    - 게임 종료시 게임 재시작 여부 메시지를 출력하는 메서드 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,6 +3,7 @@ package baseball.view;
 public class OutputView {
 	private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 	private static final String WIN_MESSAGE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 	public static void gameStartView(){
 		System.out.println(START_MESSAGE);
@@ -22,5 +23,9 @@ public class OutputView {
 
 	public static void displayWin(){
 		System.out.println(WIN_MESSAGE);
+	}
+
+	public static void displayRestartMessage(){
+		System.out.println(RESTART_MESSAGE);
 	}
 }
