ae665cc8b5e662abb807cab639624a0d153fcb1c
whdcks420 <whdcks420@gmail.com>
2022-11-02 20:03:08 +0900

    feat(view): 게임 시작 문구 출력 기능구현

---
+++ src/main/java/baseball/view/BaseBallView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseBallView {
+
+    public void showBaseBallStartView() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+}
