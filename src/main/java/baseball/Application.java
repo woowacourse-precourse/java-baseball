dd7cbde1350de90a6df3348b434778bec2b4bd51
HyunJoong Kim <80106238+hjk0761@users.noreply.github.com>
2022-11-02 23:57:26 +0900

    feat: implement main methodPlaying math baseball game. If end, restart with input 1, exit with input 2, other makes IllegalArgumentException.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -80,6 +80,16 @@ public class Application {
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        checkGameState("1");
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true){
+            List<Integer> answer = generateNumber();
+            playMathBaseball(answer);
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String state = Console.readLine();
+            checkGameState(state);
+            if (state.equals("2")){
+                break;
+            }
+        }
     }
 }
