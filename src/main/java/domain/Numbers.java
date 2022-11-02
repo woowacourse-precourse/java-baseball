7238a478d8e8b84d4d97a093d4044cb3afa91a3f
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 21:34:38 +0900

    Refactor: Computer와 User 숫자 생성을 같이 사용하기 위해 숫자 유효성 확인 함수 생성자에서 제외

--- src/main/java/domain/Numbers.java
+++ src/main/java/domain/Numbers.java
@@ -8,16 +8,14 @@ public class Numbers {
     private List<Number> numbers;
 
     public Numbers(List<Number> numbers) {
-        isValidSize(numbers);
-        isDuplicate(numbers);
         this.numbers = numbers;
     }
 
-    private void isDuplicate(List<Number> numbers) {
+    public void isDuplicate(List<Number> numbers) {
         if(numbers.stream().distinct().count() != 3) throw new IllegalArgumentException(Messages.duplicateNumber);
     }
 
-    private void isValidSize(List<Number> numbers) {
+    public void isValidSize(List<Number> numbers) {
         if(numbers.size() != 3) throw new IllegalArgumentException(Messages.wrongInputSize);
     }
 }
