31503d488f686c4cc7bc1956166c26f4deeb8359
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-02 20:49:54 +0900

    feat: 테스트 실패를 위한 객체 ComputerNumberGenerator 생성

---
+++ src/main/java/baseball/ComputerNumberGenerator.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerNumberGenerator {
+    private List<Integer> numbers;
+
+    public ComputerNumberGenerator(){
+        numbers = new ArrayList<>();
+    }
+
+    public int createRandomNumber(){
+        return 100;
+    }
+
+    public boolean isAlreadyInNumbers(int number){
+        return false;
+    }
+
+    public void insertNumber(int number){
+
+    }
+
+    public int[] returnNumbersToArray(){
+        return new int[]{};
+    }
+
+}
