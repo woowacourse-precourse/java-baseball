94673a465013d38ba7bb58601ea74b5357ea1c2d
yg0585 <yg0585@naver.com>
2022-11-02 19:40:24 +0900

    feat: 유저에게 입력받는 기능

---
+++ src/main/java/baseball/util/UserInput.java
@@ -0,0 +1,10 @@
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInput {
+    public String getInput() {
+        String input = Console.readLine();
+        return input;
+    }
+}
