984f461dd4535a5a39700d5973596f6781c52cd6
WhoIsWoony <WhoIsWoony@gmail.com>
2022-11-03 00:37:39 +0900

    feat: Add Game class

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,33 +1,27 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
+import baseball.util.InputOutput;
+import java.util.List;
 
-public class Game extends IO{
-    final static int PLAYING = 1;
-    final static int END = 2;
-
-    int state;
+public class Game extends InputOutput {
     public void start(){
-        while(state != END){
-            match();
-            askContinue();
-        }
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        do match();
+        while(askContinue() == GAME_PLAYING);
+        System.out.println("게임 종료");
     }
-    public void askContinue(){
-        this.state = readInt("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        if(this.state==END)
-            System.out.println("게임 종료");
-        else if(this.state != PLAYING)
-            throw new IllegalArgumentException("잘못된 선택입니다.");
+    public int askContinue(){
+        return readState("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
     public void match(){
         Player computer = new Player();
-        int resultCode;
+        boolean isThreeStrike;
         do {
-            String input = readStr("숫자를 입력해주세요 : ");
-            Player user = new Player(input);
-            resultCode = computer.printCompareTo(user);
-        } while (resultCode != Player.HAS_ALL_STRIKES);
+            List<Integer> balls = readBall("숫자를 입력해주세요 : ");
+            Player user = new Player(balls);
+            List<Integer> strikeAndBall = computer.getStrikes(user);
+            isThreeStrike = writeStrikes(strikeAndBall);
+        } while (!isThreeStrike);
     }
 }
