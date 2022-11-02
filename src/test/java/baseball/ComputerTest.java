a74bf5e3903eabfb8ba440c52b8ba470d7e91dc2
Reznov <reznov03048@gmail.com>
2022-11-02 21:56:17 +0900

    test: 생성한 랜덤 숫자가 세자리 인지 확인하는 Test 구현

---
+++ src/test/java/baseball/ComputerTest.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class ComputerTest {
+
+    Computer testComputer = new Computer();
+
+    @Test
+    public void 생성한_랜덤숫자가_세자리인지(){
+        // given
+        List<Integer> randomDigits = testComputer.createRandomDigits();
+        // when
+
+        // then
+        Assertions.assertThat(randomDigits.size()).isEqualTo(3);
+    }
+}
