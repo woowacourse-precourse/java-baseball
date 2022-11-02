6f848d32edf8471c0fd94bf6266db30fe309a035
akskflwn <akskflwn@gmail.com>
2022-11-03 01:04:37 +0900

    feat : 컴퓨터와 사용자 숫자에 대한 객체 생성 기능 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,10 +1,38 @@
 package baseball.controller;
 
+import baseball.domain.Ball;
+import baseball.domain.Balls;
+import baseball.domain.ComputerBallGenerator;
 import baseball.view.InputView;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GameController {
 
+    ComputerBallGenerator generator = new ComputerBallGenerator();
+
     public void run() {
-        InputView.inputPlayerNumber();
+        Balls answer = getComputerBalls();
+        Balls playerBalls = getPlayerBalls();
+
+    }
+
+    private Balls getComputerBalls() {
+        List<Integer> numbers = generator.createComputerBalls();
+        return getBalls(numbers);
+    }
+
+    private Balls getPlayerBalls() {
+        List<Integer> numbers = InputView.inputPlayerNumber();
+        return getBalls(numbers);
+    }
+
+    private static Balls getBalls(List<Integer> numbers) {
+        List<Ball> ballList = new ArrayList<>();
+
+        for (int i = 0; i < numbers.size(); i++) {
+            ballList.add(Ball.of(numbers.get(i), i + 1));
+        }
+        return Balls.of(ballList);
     }
 }
