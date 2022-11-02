eb08226d1ef3796d74e92846b5a2312a7760cb1b
ym2629 <dydals2629@naver.com>
2022-11-02 09:39:38 +0900

    feat: check error for the length of input

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -1,5 +1,13 @@
 package baseball;
 
 public class Input {
-
+	static int MAX_LENGHT = 3;
+	
+	boolean checkLengthError(String userAnswer) {
+		if(userAnswer.length() != MAX_LENGHT) {
+			return false;
+		}else {
+			return true;
+		}
+	}
 }
