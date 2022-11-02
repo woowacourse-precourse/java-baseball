318faf39504f822c1e1fb8595ad963fc22466c18
HyebinShin <hyeviin.dev@gmail.com>
2022-11-03 08:00:07 +0900

    Feat: 게임 진행 전체적인 흐름

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,14 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        boolean retry = false;
+
+        do {
+
+        } while (retry);
+
+        System.out.println("애플리케이션 종료");
     }
 }
