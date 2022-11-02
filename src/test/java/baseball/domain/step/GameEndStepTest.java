1a0af4c0a08e16172d6423805207ed0c4b7800cb
shinD <huipulci1@naver.com>
2022-11-03 04:53:23 +0900

    test: 게임 종료 단계 테스트코드 작성
    GameEndStep 의 실행 가능 여부 테스트.
    GameEndStep 가 다음 단계로 [게임 재실행 여부 판단 단계]를 갖는지 테스트.
    GameEndStep 실행 시 게임 종료 메세지를 출력하는지 테스트.

---
+++ src/test/java/baseball/domain/step/GameEndStepTest.java
@@ -0,0 +1,59 @@
+package baseball.domain.step;
+
+import baseball.application.context.BaseBallGameContext;
+import baseball.application.io.Writer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@DisplayName("GameEndStep 은 ")
+class GameEndStepTest {
+
+    @Test
+    @DisplayName("실행 가능하다")
+    void isExecutable() {
+        // given
+        GameEndStep gameEndStep = new GameEndStep();
+
+        // when
+        boolean executable = gameEndStep.executable();
+
+        // then
+        assertThat(executable).isTrue();
+    }
+
+    @Test
+    @DisplayName("실행 시 정답을 맞추었다는 문구를 출력한다")
+    void printRightAnswerMessageWhenExecuted() {
+        // given
+        final String rightAnswerMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+        GameEndStep gameEndStep = new GameEndStep();
+
+        BaseBallGameContext context = mock(BaseBallGameContext.class);
+        Writer writer = mock(Writer.class);
+
+        when(context.writer()).thenReturn(writer);
+
+        // when
+        gameEndStep.execute(context);
+
+        // then
+        verify(writer, times(1)).println(rightAnswerMessage);
+    }
+
+    @Test
+    @DisplayName("다음 단계로 [게임 재실행 여부 판단 단계]를 갖는다")
+    void nextIsGameInitStep() {
+        // given
+        GameEndStep gameEndStep = new GameEndStep();
+
+        // when
+        Step next = gameEndStep.next();
+
+        // then
+        assertThat(next).isExactlyInstanceOf(DetermineRestartGameStep.class);
+    }
+}
