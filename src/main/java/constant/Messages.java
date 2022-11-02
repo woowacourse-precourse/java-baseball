b4719273bd5ffcae16272ed3c2402435bf8ea431
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 19:16:29 +0900

    Refactoring:private 삭제

--- src/main/java/constant/Messages.java
+++ src/main/java/constant/Messages.java
@@ -1,13 +1,13 @@
 package constant;
 
 public class Messages {
-    private static final String gameStart = "숫자 야구 게임을 시작합니다.";
-    private static final String inputNumber = "숫자를 입력해주세요 : ";
-    private static final String ball = "볼";
-    private static final String strike = "스트라이크";
-    private static final String correctNumber = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
-    private static final String restartOrClose = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String rangeOut = "1부터 9까지의 숫자만 가능합니다.";
-    private static final String inputIsNull = "입력이 안된 경우 `NULL 값은 입력이 될 수 없습니다.";
-    private static final String duplicateNumber = "중복 숫자가 입력된 경우 `각 다른 숫자가 입력되야 합니다.";
+    static final String gameStart = "숫자 야구 게임을 시작합니다.";
+    static final String inputNumber = "숫자를 입력해주세요 : ";
+    static final String ball = "볼";
+    static final String strike = "스트라이크";
+    static final String correctNumber = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    static final String restartOrClose = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    static final String rangeOut = "1부터 9까지의 숫자만 가능합니다.";
+    static final String inputIsNull = "입력이 안된 경우 `NULL 값은 입력이 될 수 없습니다.";
+    static final String duplicateNumber = "중복 숫자가 입력된 경우 `각 다른 숫자가 입력되야 합니다.";
 }
