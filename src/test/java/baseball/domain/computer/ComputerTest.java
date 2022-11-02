220d59364ca57573f8253194f1e1733bb9e10ef8
shinD <huipulci1@naver.com>
2022-11-03 02:10:34 +0900

    test: Computer 테스트코드 작성
    BaseBallGameNumber를 통해 생성되는지 테스트.

---
+++ src/test/java/baseball/domain/computer/ComputerTest.java
@@ -0,0 +1,25 @@
+package baseball.domain.computer;
+
+import baseball.domain.number.BaseBallGameNumbers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Computer 는 ")
+class ComputerTest {
+
+    @Test
+    @DisplayName("BaseBallGameNumber를 가지고 생성된다.")
+    public void playerConstructedWithBaseBallGameNumbers() {
+        // given
+        String input = "123";
+        BaseBallGameNumbers baseBallGameNumbers = new BaseBallGameNumbers(input);
+
+        // when
+        Computer computer = new Computer(baseBallGameNumbers);
+
+        // then
+        assertThat(computer.baseBallGameNumbers()).isEqualTo(baseBallGameNumbers);
+    }
+}
