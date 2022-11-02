4e0fc6d73046088bfaa34bb54790d5a942d97bb0
정핸모 <gusah009@naver.com>
2022-11-03 00:02:12 +0900

    feat: 사용자에게 재시작을 할 지, 종료를 할 지 묻는 로직 구현

--- src/main/java/baseball/service/GamePlay.java
+++ src/main/java/baseball/service/GamePlay.java
@@ -1,19 +1,48 @@
 package baseball.service;
 
 import baseball.game.Game;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 
 public class GamePlay {
 
-  public static void start(Class<Game> gameClazz) {
+  public static void start(Class<? extends Game> gameClazz) {
     Game game = getGameInstance(gameClazz);
-    game.play();
+    do {
+      game.play();
+    } while (getPlayerWant() == GameStatus.RESTART);
   }
 
-  private static Game getGameInstance(Class<Game> gameClazz) {
+  private static Game getGameInstance(Class<? extends Game> gameClazz) {
     try {
       return gameClazz.getDeclaredConstructor().newInstance();
     } catch (Exception e) {
       throw new IllegalArgumentException("잘못된 게임 클래스입니다.");
+    }
+  }
+
+  private static GameStatus getPlayerWant() {
+    String input = Console.readLine();
+    return Arrays.stream(GameStatus.values())
+        .filter(status -> status.getCode().equals(input))
+        .findFirst()
+        .orElseThrow(IllegalArgumentException::new);
+  }
+
+  /**
+   * @code 변수는 사용자가 입력한 값을 의미합니다. 따라서 GameStatus 내에서 각 code 값은 unique 해야 합니다.
+   */
+  private enum GameStatus {
+    RESTART("1"), END("2");
+
+    final String code;
+
+    GameStatus(String s) {
+      code = s;
+    }
+
+    public String getCode() {
+      return code;
     }
   }
 }
