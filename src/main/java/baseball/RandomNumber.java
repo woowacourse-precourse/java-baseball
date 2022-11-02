b9852e1ceec2e4cd889d319c293da2a041c4a305
sjk <80821744+ShinjaeKwon@users.noreply.github.com>
2022-11-02 16:09:08 +0900

    feat : 랜덤 숫자 할당
    1에서 9사이의 숫자들로 3자리 숫자의 랜덤값을 할당한다.
    동일한 숫자가 있는경우, 3자리 모두 다른 숫자가 할당될까지 반복해 세자리 모두 다른 랜덤한 숫자들로 구성해 리턴한다.

---
+++ src/main/java/baseball/RandomNumber.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumber {
+
+	public static final int RANGE_START = 1;
+	public static final int RANGE_END = 9;
+
+	public static int makeRandomNumber() {
+
+		int hundreds = Randoms.pickNumberInRange(RANGE_START, RANGE_END);
+		int tens = Randoms.pickNumberInRange(RANGE_START, RANGE_END);
+		int units = Randoms.pickNumberInRange(RANGE_START, RANGE_END);
+
+		while (isSameNumber(hundreds, tens, units)) {
+			tens = Randoms.pickNumberInRange(RANGE_START, RANGE_END);
+			units = Randoms.pickNumberInRange(RANGE_START, RANGE_END);
+		}
+
+		return (hundreds * 100) + (tens * 10) + units;
+	}
+
+	private static boolean isSameNumber(int hundreds, int tens, int units) {
+		return hundreds == tens || hundreds == units || tens == units;
+	}
+
+}
