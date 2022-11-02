6889777ccfe47750f32d54c095537efc455979a0
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 23:08:45 +0900

    Refactor: nothing message 추가

--- src/main/java/constant/Messages.java
+++ src/main/java/constant/Messages.java
@@ -5,6 +5,7 @@ public class Messages {
     public static final String inputNumber = "숫자를 입력해주세요 : ";
     public static final String ball = "볼";
     public static final String strike = "스트라이크";
+    public static final String nothing = "낫싱";
     public static final String successMessage = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
     public static final String restartOrClose = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String wrongInput = "1부터 9까지의 숫자만 가능합니다.";
