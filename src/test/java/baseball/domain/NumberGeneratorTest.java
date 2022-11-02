1515c591a83c7154076bd8fc587de5ee8267ed26
alstjr706 <alstjr706@naver.com>
2022-11-03 06:04:01 +0900

    feat : (숫자 야구) create skeleton

---
+++ src/test/java/baseball/domain/NumberGeneratorTest.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberGeneratorTest {
+
+    @Test
+    void createRandomNumbers() {
+    }
+}
