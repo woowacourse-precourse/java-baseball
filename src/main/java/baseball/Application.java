935c598edeb53b280408e87b296046ea7ab7c895
jmj005 <jmj00005@naver.com>
2022-11-03 01:56:28 +0900

    feat: 사용자의 입력 값 검증

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
 
@@ -12,8 +14,32 @@ public class Application {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    private static String getUserNumber() {
-        String userNumber = Console.readLine();
+    private static boolean isValidScope(String userNumber) {
+        return userNumber.matches("[1-9]+");
+    }
+
+    private static boolean isThreeLength(String userNumber) {
+        return userNumber.length() == 3;
+    }
+
+    private static boolean isDuplicate(String userNumber) {
+        Set<Character> notDuplication = new HashSet<>();
+        for (int index = 0; index < userNumber.length(); index ++) {
+            notDuplication.add(userNumber.charAt(index));
+        }
+        return notDuplication.size() == userNumber.length();
+    }
+
+    private static List<Integer> getUserNumber() throws IllegalArgumentException {
+        String inputUserNumber = Console.readLine();
+        List<Integer> userNumber = new ArrayList<>();
+        if (!isValidScope(inputUserNumber) || !isThreeLength(inputUserNumber)
+                || !isDuplicate(inputUserNumber)) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+        for (char element : inputUserNumber.toCharArray()) {
+            userNumber.add(element - '0');
+        }
         return userNumber;
     }
 
@@ -33,6 +59,6 @@ public class Application {
     }
 
     public static void main(String[] args) {
-
+        
     }
 }
