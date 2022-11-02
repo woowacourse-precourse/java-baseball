0d6015443ddd245e2f5fbbac77ba6c57d5b7d7f2
Reznov <reznov03048@gmail.com>
2022-11-03 00:09:13 +0900

    test: 입력값의 길이가 게임에 사용할 숫자의 길이와 같은지 확인하는 Test 구현

--- src/test/java/baseball/user/InputTest.java
+++ src/test/java/baseball/user/InputTest.java
@@ -1,5 +1,6 @@
 package baseball.user;
 
+import baseball.Computer;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -10,6 +11,7 @@ import java.util.List;
 public class InputTest {
 
     Input input = new Input();
+    Computer computer = new Computer();
 
     @Test
     public void 문자열로_받은_입력값이_리스트에_제대로_들어갔는지_확인(){
@@ -21,5 +23,16 @@ public class InputTest {
         inputList = input.inputDigit(inputString);
         // then
         Assertions.assertThat(inputList).isEqualTo(testList);
+    }
+
+    @Test
+    public void 입력값의_길이가_게임에_사용할_숫자의_길이와_같은지_확인(){
+        // given
+        List<Integer> inputList = List.of(1, 2, 3);
+        int randomNumLength = computer.getRANDOM_NUMBER_LENGTH();
+        // when
+
+        // then
+        Assertions.assertThat(inputList.size()).isEqualTo(randomNumLength);
     }
 }
