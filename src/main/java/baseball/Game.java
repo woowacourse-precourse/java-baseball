b2e10695c6fa07f6a1c27d20290fbf9d63ea1f80
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-02 16:55:34 +0900

    [fix] : 정답값을 전역으로 설정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,21 +4,21 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Game {
-    public static void startGame(){
-        String answerNumber="";
+    private String answerNumber="";
+    public void startGame(){
         for(int i=0;i<3;i++){
             answerNumber+=Integer.toString(pickNumberInRange(1,9));
         }
         do{
             System.out.println("숫자를 입력해주세요: ");
             String getNumber=readLine();
-            rightNumber(getNumber);
+            rightNumber();
         }while(true);
     }
-    public static void rightNumber(String number){
-        if(number.length()!=3) throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+    public void rightNumber(){
+        if(answerNumber.length()!=3) throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
         for(int i=0;i<3;i++){
-            if(number.charAt(i)<1||number.charAt(i)>9) throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+            if(answerNumber.charAt(i)<1||answerNumber.charAt(i)>9) throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
         }
     }
 }
