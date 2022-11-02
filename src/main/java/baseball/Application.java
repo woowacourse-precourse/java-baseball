cee79bcc0a8ef5149b7e58764590a7c7c7e1b666
Jeangyu <gjwlsrb1700@naver.com>
2022-11-02 16:23:56 +0900

    Add: 정답이 되는 3자리 숫자생성 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(makeAnswer());
+    }
+
+    public static String makeAnswer(){
+        String result = "";
+        List<Integer> nums = new ArrayList<Integer>();
+
+        while (true){
+            if (nums.size() == 3) break;
+            int rand_num = Randoms.pickNumberInRange(1, 9);
+            if (!nums.contains(rand_num)) nums.add(rand_num);
+        }
+        for (Integer num: nums) result += Integer.toString(num);
+
+        return result;
     }
 }
