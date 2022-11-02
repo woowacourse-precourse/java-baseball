64421f08ab9f49224d38ca58bd63758bb169ee33
이요환 <91039622+bumstead-bumstead@users.noreply.github.com>
2022-11-02 22:13:19 +0900

    test() : 볼 개수 세기 기능 테스트 작성
     - getNumberOfBalls() 기능 테스트 작성

--- src/test/java/baseball/functionTest.java
+++ src/test/java/baseball/functionTest.java
@@ -6,8 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static baseball.Application.getNumberOfStrikes;
-import static baseball.Application.isValidBaseballNumber;
+import static baseball.Application.*;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,6 +38,38 @@ public class functionTest {
         b.add(2);b.add(3);b.add(1);
 
         assertThat(getNumberOfStrikes(a, b)).isEqualTo(0);
+    }
+
+    @Test
+    void 볼_개수_세기_테스트() {
+        List<Integer> a = new ArrayList<>();
+        List<Integer> b = new ArrayList<>();
+
+        a.add(1);a.add(2);a.add(3);
+        b.add(1);b.add(2);b.add(3);
+        assertThat(getNumberOfBalls(a, b)).isEqualTo(0);
+
+        a.clear();b.clear();
+        a.add(1);a.add(2);a.add(3);
+        b.add(1);b.add(4);b.add(5);
+        assertThat(getNumberOfBalls(a, b)).isEqualTo(0);
+
+        a.clear();b.clear();
+        a.add(1);a.add(2);a.add(3);
+        b.add(2);b.add(5);b.add(6);
+        assertThat(getNumberOfBalls(a, b)).isEqualTo(1);
+
+        a.clear();b.clear();
+        a.add(1);a.add(2);a.add(3);
+        b.add(2);b.add(3);b.add(1);
+
+        assertThat(getNumberOfBalls(a, b)).isEqualTo(3);
+
+        a.clear();b.clear();
+        a.add(1);a.add(2);a.add(4);
+        b.add(2);b.add(1);b.add(4);
+
+        assertThat(getNumberOfBalls(a, b)).isEqualTo(2);
     }
 
     @Test
