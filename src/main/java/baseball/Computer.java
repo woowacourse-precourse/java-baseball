74bf0187504ec2f8d015220e396ad607f43e331d
Reznov <reznov03048@gmail.com>
2022-11-02 21:16:16 +0900

    feat: Computer 클래스 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import java.util.List;
+
+/**
+ * 상대방 역할을 할 컴퓨터 클래스 입니다.
+ */
+public class Computer {
+
+    private List<Integer> completedDigits;
+
+    public Computer(){
+
+    }
+
+
+}
