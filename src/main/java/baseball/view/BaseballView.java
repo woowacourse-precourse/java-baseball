261229d5354a605a290fb45f291d0955d1c4bc19
whdcks420 <whdcks420@gmail.com>
2022-11-02 21:13:04 +0900

    feat(view): 사용자 숫자 입력 출력 기능 구현

--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -1,10 +1,12 @@
 package baseball.view;
 
-import camp.nextstep.edu.missionutils.Console;
+public class BaseballView {
 
-public class BaseBallView {
-
-    public void showBaseBallStartView() {
+    public void showStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void showInput() {
+        System.out.print("숫자를 입력해주세요 : ");
     }
 }
