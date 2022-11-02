e32cc6e6d55330650e9b9e7e89b3672798243b7b
antaeho <an981022@naver.com>
2022-11-03 02:08:41 +0900

    :sparkles: Feat: 4. 숫자야구 메서드 완성
    점수 초기화 -> 점수 계산 -> 결과 출력
    아직 성공시 반복 묻기 기능은 미구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,10 +4,20 @@ package baseball;
 
 public class Application {
 
+    static int MAX_COUNT = 3;
+    static int STRIKE_CASE = 2;
+    static int BALL_CASE = 1;
+
+
+    static int strike = 0;
+    static int ball = 0;
+
+    static int[] computerNumber;
+    static int[] userNumber;
+
     public static void main(String[] args) {
 
-        int[] computerNumber;
-        int[] userNumber;
+        boolean keepGoing = true;
 
         Number answerNumber = new Number();
         Number userPrediction = new Number();
@@ -16,9 +26,60 @@ public class Application {
         computerNumber = answerNumber.getDigits();
 
 
-        userPrediction.inputPrediction();
-        userNumber = userPrediction.getDigits();
+        while(keepGoing) {
+            userPrediction.inputPrediction();
+            userNumber = userPrediction.getDigits();
 
+            initializeScore();
+            calcScore();
+            printResult();
+
+        }
+
+    }
+
+    private static void printResult() {
+        if(strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        }
+        StringBuilder sb = new StringBuilder();
+        if(ball > 0) {
+            sb.append(ball).append("볼 ");
+        }
+        if(strike > 0) {
+            sb.append(strike).append("스트라이크");
+        }
+        System.out.println(sb);
+    }
+
+    private static void calcScore() {
+        for(int i = 0; i < MAX_COUNT; i++) {
+            int checkCase = checkBallOrStike(userNumber[i], i);
+            if(checkCase == BALL_CASE) {
+                ball++;
+                continue;
+            }
+            if(checkCase == STRIKE_CASE) {
+                strike++;
+            }
+        }
+    }
+
+    private static int checkBallOrStike(int num, int index) {
+        for(int j = 0; j < MAX_COUNT; j++) {
+            if(num == computerNumber[j] && j == index) {
+                return STRIKE_CASE;
+            }
+            if(num == computerNumber[j]) {
+                return BALL_CASE;
+            }
+        }
+        return 0;
+    }
+
+    private static void initializeScore() {
+        strike = 0;
+        ball = 0;
     }
 
 }
