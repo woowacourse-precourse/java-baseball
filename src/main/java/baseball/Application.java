6046ebe33b9b680b7a1302975acf07d798786ec0
imjy2 <imjy2@192.168.0.39>
2022-11-02 17:16:45 +0900

    feat: 게임시작 시 기능 구현
    게임시작을 알리는 텍스트 출력
    숫자를 랜덤하게 설정하는 select_Random_Number method 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+	private static List<Integer> select_Random_Number(){
+		
+	}
+    private static void start_new_Baseball_Game() {
+    	System.out.println("숫자 야구 게임을 시작합니다.");
+    	List<Integer> computer_num = select_Random_Number();
+    }
+	public static void main(String[] args) {
+        start_new_Baseball_Game();
     }
 }
