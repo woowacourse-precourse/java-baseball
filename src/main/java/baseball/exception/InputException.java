fc1aebdc6e1e553df2290410ae77ef7890ec4188
sangil <gsl0505@naver.com>
2022-11-02 21:13:17 +0900

    feat(Exception): 입력받은 사용자의 수가 중복됬을 경우 예외처리 기능 구현

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -1,10 +1,23 @@
 package baseball.exception;
 
+import java.util.Arrays;
+
 public class InputException {
 	private static final int NUMBER_COUNT = 3;
+	private static final String ONE_LETTER_DIVISION_REGEX = "";
 
 	private void checkNumberCount(String input) {
 		if (input.length() != NUMBER_COUNT) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void checkDuplicatedNumber(String input) {
+		int distinctNumberCount = (int)Arrays.stream(input.split(ONE_LETTER_DIVISION_REGEX))
+			.map(Integer::parseInt)
+			.distinct()
+			.count();
+		if (distinctNumberCount != NUMBER_COUNT) {
 			throw new IllegalArgumentException();
 		}
 	}
