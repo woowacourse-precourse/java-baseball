9a9de2c836ea45b9712e9a555978b448be076dab
slbin-park <smpts00@naver.com>
2022-11-02 22:00:10 +0900

    docs : 구현할 기능 목록 추가 - 입력받은 숫자를 한글자씩 나누는 함수

---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
+public class User {
+    public ArrayList<Integer> userNumbers;
+
+    public void getUserNumbers() {
+        Console.readLine();
+    }
+}
