e7ca7c8ad95daeff01a6e8efe6754d8c785a0058
Reznov <reznov03048@gmail.com>
2022-11-02 22:11:51 +0900

    test: 생성한 랜덤숫자 3개가 모두 다른 숫자인지 확인하는 Test 구현

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -3,7 +3,9 @@ package baseball;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ComputerTest {
 
@@ -17,5 +19,18 @@ public class ComputerTest {
 
         // then
         Assertions.assertThat(randomDigits.size()).isEqualTo(3);
+    }
+    
+    @Test
+    public void 생성한_랜덤숫자의_세개_모두_다른_숫자인지(){
+        // given
+        List<Integer> randomDigits = testComputer.createRandomDigits();
+        Set<Integer> randomDigitsSet = new HashSet<>();
+        // when
+        for(int digit : randomDigits){
+            randomDigitsSet.add(digit);
+        }
+        // then
+        Assertions.assertThat(randomDigitsSet.size()).isEqualTo(3);
     }
 }
