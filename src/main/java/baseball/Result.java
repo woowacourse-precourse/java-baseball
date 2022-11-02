ef7643bffa224af0c50e0b582a93d1bec5c31d9c
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 23:18:02 +0900

    Feat: 볼 또는 스트라이크만 나오는 경우의 문구 추가

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -15,6 +15,8 @@ public class Result {
     public String getResult() {
         if(this.strikeCount == 3) return Messages.successMessage;
         if(this.strikeCount == 0 && this.ballCount == 0) return Messages.nothing;
+        if(this.strikeCount == 0) return this.ballCount + Messages.ball;
+        if(this.ballCount == 0) return this.strikeCount + Messages.strike;
 
         return this.ballCount + Messages.ball + " " + this.strikeCount + Messages.strike;
     }
