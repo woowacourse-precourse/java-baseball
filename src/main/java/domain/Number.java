28836af07ef9b1c9a7f8c84952c4e7b588878b96
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 19:49:08 +0900

    Feat: 값이 일치할때 같은 객체로 판단하도록 equals/hashcode 추가

--- src/main/java/domain/Number.java
+++ src/main/java/domain/Number.java
@@ -3,6 +3,7 @@ package domain;
 import constant.Messages;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Number {
     private int number;
@@ -14,5 +15,18 @@ public class Number {
 
     private void isValidNumber(int number) {
         if(number < 1 || 9 < number) throw new IllegalArgumentException(Messages.rangeOut);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Number number1 = (Number) o;
+        return number == number1.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
