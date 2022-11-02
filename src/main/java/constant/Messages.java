0fa59cf9bad03ba8b5c45142998c2e57fcdf3842
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 19:37:19 +0900

    Refactoring:public 추가 및 숫자 길이 예외처리 문구 추가

--- src/main/java/constant/Messages.java
+++ src/main/java/constant/Messages.java
@@ -1,13 +1,14 @@
 package constant;
 
 public class Messages {
-    static final String gameStart = "숫자 야구 게임을 시작합니다.";
-    static final String inputNumber = "숫자를 입력해주세요 : ";
-    static final String ball = "볼";
-    static final String strike = "스트라이크";
-    static final String correctNumber = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
-    static final String restartOrClose = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    static final String rangeOut = "1부터 9까지의 숫자만 가능합니다.";
-    static final String inputIsNull = "입력이 안된 경우 `NULL 값은 입력이 될 수 없습니다.";
-    static final String duplicateNumber = "중복 숫자가 입력된 경우 `각 다른 숫자가 입력되야 합니다.";
+    public static final String gameStart = "숫자 야구 게임을 시작합니다.";
+    public static final String inputNumber = "숫자를 입력해주세요 : ";
+    public static final String ball = "볼";
+    public static final String strike = "스트라이크";
+    public static final String correctNumber = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    public static final String restartOrClose = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String rangeOut = "1부터 9까지의 숫자만 가능합니다.";
+    public static final String inputIsNull = "입력이 안된 경우 `NULL 값은 입력이 될 수 없습니다.";
+    public static final String duplicateNumber = "중복 숫자가 입력된 경우 `각 다른 숫자가 입력되야 합니다.";
+    public static final String wrongInputSize = "입력은 3자리 숫자만 가능합니다.";
 }
