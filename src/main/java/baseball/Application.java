fb51a499015b18386e814937e4c3508edfc397b7
HyebinShin <hyeviin.dev@gmail.com>
2022-11-03 08:06:11 +0900

    Feat: 컴퓨터 숫자 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,9 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -8,9 +12,29 @@ public class Application {
         boolean retry = false;
 
         do {
-
+            List<Integer> computer = getComputer();
+            System.out.println("computer: "+Arrays.toString(computer.toArray()));
         } while (retry);
 
         System.out.println("애플리케이션 종료");
+    }
+
+    //컴퓨터 숫자 입력
+    public static List<Integer> getComputer() {
+        List<Integer> computer = new ArrayList<>();
+
+        while(computer.size() < 3) {
+            addComputer(computer);
+        }
+
+        return computer;
+    }
+
+    private static void addComputer(List<Integer> computer) {
+        int random = Randoms.pickNumberInRange(1, 9);
+
+        if(!computer.contains(random)) {
+            computer.add(random);
+        }
     }
 }
