d7830686809dc9428e3e9d54ac350f1ee10fd632
ym2629 <dydals2629@naver.com>
2022-11-02 10:04:21 +0900

    feat: check error for overlap of each digit

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -3,11 +3,34 @@ package baseball;
 public class Input {
 	static int MAX_LENGHT = 3;
 	
-	boolean checkLengthError(String userAnswer) {
-		if(userAnswer.length() != MAX_LENGHT) {
+	boolean checkLengthError(int userAnswer) {
+		int length = (int)(Math.log10(userAnswer)+1);
+		if(length != MAX_LENGHT) {
 			return false;
 		}else {
 			return true;
+		}
+	}
+	
+	boolean checkOverlapError(int userAnswer) {
+		if(checkLengthError(userAnswer)) {
+			int firth = userAnswer/100;
+			int second = (userAnswer%100)/10;
+			int thirth = userAnswer%10;
+			int disFirstToSecond = firth-second;
+			int disThirthToSecond = thirth-second;
+			
+			if(disFirstToSecond == disThirthToSecond) {
+				return false;
+			}else if(disFirstToSecond == 0) {
+				return false;
+			}else if(disThirthToSecond == 0) {
+				return false;
+			}else {
+				return true;
+			}
+		}else {
+			return false;
 		}
 	}
 }
