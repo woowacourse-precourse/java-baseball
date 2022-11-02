3ab2d613c1d65c328b04bf2b1a426b4334f607a8
dev_kong <ahdjd5@gmail.com>
2022-11-02 23:36:04 +0900

    feat(GameStart): add check number method

--- src/main/java/baseball/utils/Validator.java
+++ src/main/java/baseball/utils/Validator.java
@@ -1,12 +1,22 @@
 package baseball.utils;
 
+import java.util.regex.Pattern;
+
 public class Validator {
     public static void checkUserAnswerInput(String userAnswer) {
         checkStringLength(userAnswer, 3);
+        checkNumber(userAnswer);
     }
 
     private static void checkStringLength(String string, int length) {
         if(string.length() != length) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkNumber(String string) {
+        boolean isNumber = Pattern.matches("^[0-9]*$", string);
+        if(!isNumber){
             throw new IllegalArgumentException();
         }
     }
