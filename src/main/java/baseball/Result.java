ac7e049f22888783deb7bb3c2267068a4ad9aee5
ywj9811 <ywj9811@naver.com>
2022-11-03 03:37:02 +0900

    [feat] Result 클래스 사용자 입력값 받는 부분 추가

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -9,25 +9,20 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.*;
 
 public class Result {
-    public int ReOrClose(int GamerInput, List<Integer> RandomNumber) {
-        int strike = 3;
+    public int ReOrClose(List<Integer> RandomNumber) {
+        int strike = 1;
         int ball;
         int want;
-        List<Integer> inputNumber = new ArrayList<>();
 
-        for (int i = 0; i < 3; i++) {
-            inputNumber.add(GamerInput % 10);
-            System.out.println(GamerInput % 10);
-            GamerInput /= 10;
-        }
-        Collections.reverse(inputNumber);
+        int inputNum;
 
         while (strike != 3) {
-
+            System.out.print("숫자를 입력해 주세요 : ");
+            inputNum = Integer.parseInt(Console.readLine());
         }
         System.out.println("맞췄습니다.");
         System.out.println("다시 하시려면 1번 종료하시려면 2번을 눌러주세요");
-        want = Integer.parseInt(Console.readLine());
-        return 2;
+        want = Integer.parseInt(readLine());
+        return want;
     }
 }
