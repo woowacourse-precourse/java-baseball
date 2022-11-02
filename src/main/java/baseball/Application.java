c8c819308299cb0b2a71b4edcd865c3b16231772
김경훈 <54941130+ouvir@users.noreply.github.com>
2022-11-02 17:13:04 +0900

    추가 (Application.java) 전반적인 프레임 구현:
    설계도를 보며, 기능들을 함수로 쪼개며, 전체적인 프레임을 만듬.
    아직 기능 구현은 하지 않음.
    startGame(): 게임 시작
    settingNumber(): 컴퓨터의 무작위 3자리 수 세팅
    askAndAnswerLoop(): 묻고 답해주며, 루프 순환
    getNumber(): 사용자에게 숫자를 입력받음
    compareOfNumber(): 입력받은 숫자와 정답을 비교
    *세분화 가능성 O
    askRetry(): 다시하기에 대한 입력 받기
    
    더 세분화 될 여지 있음.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,46 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        startGame();
+    }
+    private static void startGame(){
+        // 출력: 숫자 야구 게임을 시작합니다.
+        String correctNumber = settingNumber();
+        askAndAnswerLoop(correctNumber);
+    }
+    private static String settingNumber(){
+        String randomNumber = "123";
+        // 컴퓨터의 숫자 3개 만들기.
+        return randomNumber;
+    }
+
+    private static void askAndAnswerLoop(String correctNumber){
+        String askNumber;
+        boolean loop = true;
+        while (loop) {
+            askNumber = getNumber();
+            loop = compareOfNumber(correctNumber, askNumber);
+        }
+        // 종료문구 출력
+        if (askRetry()){
+            startGame();
+        }
+    }
+
+    private static String getNumber(){
+        String askNumber = "";
+        //사용자에게 숫자 3개 입력받고 예외 검사
+        return askNumber;
+    }
+
+    private static boolean compareOfNumber(String correctNumber, String askNumber){
+        // 비교하며, 몇 볼, 몇 스트라이크 or 낫싱 인지 출력
+        // 만약 3스트라이크면 false 반환
+        return true;
+    }
+
+    private static boolean askRetry(){
+        // 입력받기
+        return false;
     }
 }
