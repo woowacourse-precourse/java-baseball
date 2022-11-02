3677205dedea977c788d751f9adaa8207747bf38
shinD <huipulci1@naver.com>
2022-11-03 02:39:33 +0900

    test: CompareResult 테스트코드 작성
    strike 개수 증가 테스트.
    ball 개수 증가 테스트.
    0 strike 0 ball 인 경우 "낫싱" 반환 테스트
    3 strike인 경우 정답 반환 테스트

---
+++ src/test/java/baseball/domain/result/CompareResultTest.java
@@ -0,0 +1,62 @@
+package baseball.domain.result;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("CompareResult 는")
+class CompareResultTest {
+
+    @Test
+    @DisplayName("strike 개수를 증가시킬 수 있다.")
+    public void addStrike() {
+        // given
+        CompareResult compareResult = new CompareResult();
+
+        // when
+        compareResult.addStrike();
+
+        // then
+        assertThat(compareResult.toString()).isEqualTo("1스트라이크");
+    }
+
+    @Test
+    @DisplayName("ball 개수를 증가시킬 수 있다.")
+    public void addBall() {
+        // given
+        CompareResult compareResult = new CompareResult();
+
+        // when
+        compareResult.addBall();
+
+        // then
+        assertThat(compareResult.toString()).isEqualTo("1볼");
+    }
+
+    @Test
+    @DisplayName("ball 과 strike가 없는 경우 `낫싱`을 반환한다.")
+    public void return_낫싱_whenNoBallAndStrike() {
+        // given
+        CompareResult compareResult = new CompareResult();
+
+        // when, then
+        assertThat(compareResult.toString()).isEqualTo("낫싱");
+    }
+
+    @Test
+    @DisplayName("3 strike인 경우 정답이다.")
+    public void isAnswerWhen3Strike() {
+        // given
+        CompareResult compareResult = new CompareResult();
+
+        // when
+        compareResult.addStrike();
+        compareResult.addStrike();
+        compareResult.addStrike();
+
+        // when, then
+        assertThat(compareResult.isAnswer()).isTrue();
+        assertThat(compareResult.toString()).isEqualTo("3스트라이크");
+    }
+}
