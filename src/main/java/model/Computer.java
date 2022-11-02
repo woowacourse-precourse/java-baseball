09d84d079a3df101df567f0e6f1224f239ca64ef
dmdrk1414 <seungchan141414@gmail.com>
2022-11-02 18:28:10 +0900

    [Add] 3개의 랜덤숫자를 list에 저장하는 메서드 추가함

--- src/main/java/model/Computer.java
+++ src/main/java/model/Computer.java
@@ -14,7 +14,14 @@ public class Computer {
         this.numThreeRanOfComputerList =new ArrayList<>();
     }
 
-
+    public void creatRanNumOfComputerList(){
+        while (numThreeRanOfComputerList.size() < 3) {
+            int randomNumber = getOneRanNumAnInt();
+            if (!numThreeRanOfComputerList.contains(randomNumber)) {
+                numThreeRanOfComputerList.add(randomNumber);
+            }
+        }
+    }
 
     public int getOneRanNumAnInt() {
         return Randoms.pickNumberInRange(NUM_START_RANGE_RAM, NUM_END_RANGE_RAM);
