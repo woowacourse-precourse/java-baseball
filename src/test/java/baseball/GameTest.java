99de373435d05c709980e49fef0dbabb550bc96c
EUNJEONGMUN <munej26@gmail.com>
2022-11-03 05:21:56 +0900

    test: 컴퓨터_3자리_숫자_정하기 테스트 코드 추가

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,4 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 class GameTest {
-  
+    @Test
+    void 컴퓨터_3자리_숫자_정하기() {
+        List<Integer> result = Game.pickThreeNumber();
+        Assertions.assertThat(result.size()).isEqualTo(3);
+    }
 }
