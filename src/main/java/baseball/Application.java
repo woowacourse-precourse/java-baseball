88ef8e662981ece98ae3523e0ce28cb02ddce7c2
shinD <huipulci1@naver.com>
2022-11-03 06:22:18 +0900

    feat: main 메서드에서 BaseBallApplication 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.application.BaseBallApplication;
+import baseball.application.config.BaseBallGameConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseBallGameConfig config = new BaseBallGameConfig();
+
+        BaseBallApplication baseBallApplication = new BaseBallApplication(config);
+        baseBallApplication.play();
     }
 }
