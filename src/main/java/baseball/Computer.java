f8d361dbb797c651c948af5d203852128dbd1004
Reznov <reznov03048@gmail.com>
2022-11-02 21:50:10 +0900

    feat: 1~9 사이의 랜덤한 숫자 3개를 만드는 메소드 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,10 +11,32 @@ import java.util.List;
 public class Computer {
 
     private List<Integer> completedDigits;
+    private final int RANDOM_NUMBER_MIN = 1;
+    private final int RANDOM_NUMBER_MAX = 9;
+    private final int RANDOM_NUMBER_LENGTH = 3;
 
     public Computer(){
-
+        this.completedDigits = createRandomDigits();
     }
 
+    /**
+     * 1~9 사이의 랜덤한 숫자 3개를 만드는 메소드
+     * @return 랜덤한 숫자 3개가 들어있는 List
+     */
+    public List<Integer> createRandomDigits(){
+        List<Integer> randomDigits = new ArrayList<>();
+        boolean[] isDuplicated = new boolean[RANDOM_NUMBER_MAX + 1];
+
+        while (randomDigits.size() < RANDOM_NUMBER_LENGTH){
+            int randomDigit = Randoms.pickNumberInRange(RANDOM_NUMBER_MIN, RANDOM_NUMBER_MAX);
+
+            if(!isDuplicated[randomDigit]){
+                isDuplicated[randomDigit] = true;
+                randomDigits.add(randomDigit);
+            }
+        }
+
+        return randomDigits;
+    }
 
 }
