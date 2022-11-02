57462bbf3eb3b1bf46635e7200f2f2e2aad9c941
ym2629 <dydals2629@naver.com>
2022-11-03 08:11:08 +0900

    test: checkball method logic

---
+++ src/test/java/study/BaseballGameMethodTest.java
@@ -0,0 +1,30 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class BaseballGameMethodTest {
+
+    @Test
+    void checkBall_사용자답변_볼의개수_로직() {
+        String result = "";
+        String userAnswer = "123";
+        String computer = "321";
+        char[] userNumber = userAnswer.toCharArray();
+        char[] computerNumber = computer.toCharArray();
+
+        for(int i = 0; i < 3; i++) {
+            for(int j = 0; j < 3; j++) {
+                if(i == j) {
+                    continue;
+                }else if(userNumber[i] == computerNumber[j]) {
+                    result += "O";
+                }
+            }
+        }
+
+        assertThat(result).isEqualTo("OO");
+    }
+
+}
