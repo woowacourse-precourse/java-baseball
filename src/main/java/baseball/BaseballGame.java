a34012f09a49e6bc570edcadd8950186ba17cf04
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-02 20:11:15 +0900

    feat: 게임 시작시 컴퓨터는 1~9 사이의 중복되지 않는 숫자 3개를 생성한다.
    BaseballGame Class에 createCumputerNumbers method 생성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,12 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
 
     public final static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
+    public final static int NUMBER_COUNT_TO_CREATED = 3;
+
+    private List<Integer> computerNumbers = new ArrayList<>();
+
     public void start() {
         System.out.println(GAME_START_MESSAGE);
+        createComputerNumbers();
     }
 
+    private void createComputerNumbers() {
+        while (computerNumbers.size() < NUMBER_COUNT_TO_CREATED) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+    }
 
+    public List<Integer> getComputerNumbers() {
+        return computerNumbers;
+    }
 }
