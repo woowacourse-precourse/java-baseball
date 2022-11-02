cbf9f1048653e8bafe97824d91cba5467aeb05c6
jeujeon <1132jjw@naver.com>
2022-11-03 01:18:26 +0900

    feat(computer): 서로 다른 3자리 난수 생성 기능 추가
    computer class를 작성하여 computerNumbers를 담는 일급 컬렉션을 작성하였다.
    그 후 computer를 생성하면 computerNumbers를 자동으로 생성하도록 하였다.

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    List<Integer> computerNumbers;
+
+    public Computer() {
+        this.computerNumbers = randomNumberGenerator();
+    }
+
+    private List<Integer> randomNumberGenerator() {
+        computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return computerNumbers;
+    }
+}
