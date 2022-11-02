86611758dbefd40da257b31b48306455b0e296aa
shinD <huipulci1@naver.com>
2022-11-03 04:10:39 +0900

    feat: BaseBallGameContext 기능 구현
    게임 초기화 시 임의의 3개의 값을 가진 컴퓨터 생성.
    현재 사용자의 입력값을 바꿀 수 있는 기능 제공.
    Comparator 반환 기능 제공.

--- src/main/java/baseball/application/context/BaseBallGameContext.java
+++ src/main/java/baseball/application/context/BaseBallGameContext.java
@@ -3,21 +3,32 @@ package baseball.application.context;
 import baseball.application.config.BaseBallGameConfig;
 import baseball.application.io.Reader;
 import baseball.application.io.Writer;
+import baseball.application.random.RandomNumberGenerator;
+import baseball.application.random.Range;
+import baseball.domain.comparator.Comparator;
 import baseball.domain.computer.Computer;
 import baseball.domain.number.BaseBallGameNumbers;
 import baseball.domain.player.Player;
 
+import java.util.List;
+
 public class BaseBallGameContext {
+
+    private static final Range RANGE = new Range(1, 9);
+    private static final int COUNT = 3;
 
     private final Writer writer;
     private final Reader reader;
+    private final RandomNumberGenerator randomNumberGenerator;
 
     private Player player;
     private Computer computer;
+    private final Comparator comparator = new Comparator();
 
     public BaseBallGameContext(BaseBallGameConfig config) {
         this.writer = config.writer();
         this.reader = config.reader();
+        this.randomNumberGenerator = config.randomNumberGenerator();
     }
 
     public Writer writer() {
@@ -28,11 +39,27 @@ public class BaseBallGameContext {
         return reader;
     }
 
-    public void init() {
+    public Player player() {
+        return player;
+    }
 
+    public Computer computer() {
+        return computer;
+    }
+
+    public Comparator comparator() {
+        return comparator;
+    }
+
+    public void init() {
+        this.player = new Player();
+
+        List<Integer> numbers = randomNumberGenerator.generateUniqueNumberInRange(RANGE, COUNT);
+        BaseBallGameNumbers result = new BaseBallGameNumbers(numbers);
+        this.computer = new Computer(result);
     }
 
     public void changePlayerNumbers(BaseBallGameNumbers baseBallGameNumbers) {
-
+        player.changeNumbers(baseBallGameNumbers);
     }
 }
