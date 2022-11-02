da28b7050722c341ca43e7d4f1f5ea64f1639722
sangil <gsl0505@naver.com>
2022-11-02 18:45:18 +0900

    feat(InputView): 서로 다른 3자리 user의 숫자 입력하는 함수 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,13 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+	private static final String INPUT_USER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+	private static final String INPUT_RESTART_OR_EXIT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+	public static String userNumber() {
+		System.out.println(INPUT_USER_NUMBER_MESSAGE);
+		return Console.readLine();
+	}
+}
