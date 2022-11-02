5ea3c5553fd157639a3910e5b9cc94a0980357a8
EUNJEONGMUN <munej26@gmail.com>
2022-11-03 05:19:52 +0900

    feat: 상대방(컴퓨터)의 3자리 숫자 정하기 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    private List<Integer> computer;
+
+    public Game() {
+        this.computer = pickThreeNumber();
+    }
+
+    public static List<Integer> pickThreeNumber() {
+        List<Integer> threeNumber = new ArrayList<>();
+        while (threeNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!threeNumber.contains(randomNumber)) {
+                threeNumber.add(randomNumber);
+            }
+        }
+        return threeNumber;
+    }
 }
