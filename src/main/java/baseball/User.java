c925be0272f382dd5a1c036b3cf5d16fe6c69b8b
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-02 22:57:12 +0900

    refactor : toIntUserInput()를 static 메서드로 변경

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class User {
 
-    public List<Integer> toIntUserInput(String userInput) {
+    public static List<Integer> toIntUserInput(String userInput) {
         List<Integer> userNum = new ArrayList<>();
         int[] userInputArr =  new int[] {Integer.valueOf(userInput)};
         for (int i = 0; i < userInput.length(); i++) {
