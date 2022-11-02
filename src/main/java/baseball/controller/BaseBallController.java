83571ed3534845d8fc2e7719a6c2534cb4e69120
backFox <jorealbusiness@gmail.com>
2022-11-03 03:18:41 +0900

    feat: 지금까지 만든 입력, 검증 기능이 애플리케이션 실행 시 정상 작동하도록 구현 코드에 연결

---
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -0,0 +1,22 @@
+package baseball.controller;
+
+import baseball.system.conversion.Converter;
+import baseball.system.conversion.StringToIntegerListConverter;
+import baseball.system.validation.NumberValidator;
+import baseball.system.validation.Validator;
+import baseball.view.InputView;
+
+import java.util.List;
+
+public class BaseBallController {
+    public void startGame() {
+        InputView inputView = new InputView();
+        String input = inputView.getUserInput();
+
+        Converter<String, List<Integer>> converter = new StringToIntegerListConverter();
+        List<Integer> inputList = converter.convert(input);
+
+        Validator<List<Integer>> validator = new NumberValidator();
+        validator.validate(inputList);
+    }
+}
