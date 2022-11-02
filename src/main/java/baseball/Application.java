78a6890b03004503cebb4bfd669f3291b6223ef1
yeonsu <jack0810@kookmin.ac.kr>
2022-11-02 21:20:45 +0900

    feat(): GenerateNum (src/main/java/baseball/Application)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
-
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
 public class Application {
+    public static List<Integer> GenerateNum(){ // 숫자 생성
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
+    }
     public static void main(String[] args) {
+        List<Integer> answer = GenerateNum();
+        System.out.println(answer);
         // TODO: 프로그램 구현
     }
 }
