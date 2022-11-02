389be0bd5350149889fa909cec845b7b7546fa33
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-02 18:06:33 +0900

    test: 입력시 오류 발생경우 케이스 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -9,6 +10,25 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 입력시오류경우_확인(){
+
+        Assertions.assertThatThrownBy(() ->{
+            Application.isInputAlright("122");
+            }).isInstanceOf(IllegalArgumentException.class);
+
+
+        Assertions.assertThatThrownBy(() ->{
+            Application.isInputAlright("1234");
+        }).isInstanceOf(IllegalArgumentException.class);
+
+        Assertions.assertThatThrownBy(() ->{
+            Application.isInputAlright("12");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
