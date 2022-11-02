7d77cf81b15e4f6dea1a90482c5bacb84b81e397
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-11-02 21:32:00 +0900

    feat : Prompt 창 띄우기 - 사용자로부터 입력받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Application {
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
+public class Application {
+    public static final int USER_LENGTH_NOT_3 = 1;
+    public static final int ALPHA_IN_USER = 2;
+
+    public static void showPrompt(List<Integer> answer) {
+        String user = null;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        Console console = null;
+        while (true) {
+            System.out.println("숫자를 입력해주세요 : ");
+            user = Console.readLine();
+            System.out.println(user);
+        }
+    }
     public static List<Integer> getAnswer() {
         List<Integer> answer = new ArrayList<>();
         while (answer.size() < 3) {
@@ -21,5 +36,6 @@ public class Application {
         // TODO: 프로그램 구현
         List<Integer> answer = getAnswer();
         System.out.println(answer);
+        showPrompt(answer);
     }
 }
