0a016556843520858b6098730c0ece2357080c69
ywj9811 <ywj9811@naver.com>
2022-11-03 02:44:54 +0900

    [feat]Result 클래스 사용자 입력값 자리별로 리스트로 변환

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -1,20 +1,33 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 
 public class Result {
     public int ReOrClose(int GamerInput, List<Integer> RandomNumber) {
-        int strike = 0;
+        int strike = 3;
         int ball;
         int want;
+        List<Integer> inputNumber = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            inputNumber.add(GamerInput % 10);
+            System.out.println(GamerInput % 10);
+            GamerInput /= 10;
+        }
+        Collections.reverse(inputNumber);
+
         while (strike != 3) {
 
         }
         System.out.println("맞췄습니다.");
         System.out.println("다시 하시려면 1번 종료하시려면 2번을 눌러주세요");
-        want = Integer.parseInt(readLine());
-        return want;
+        want = Integer.parseInt(Console.readLine());
+        return 2;
     }
 }
