5527a532643616732e187bf3862be69ed0036c1a
shinD <huipulci1@naver.com>
2022-11-02 22:54:26 +0900

    test: 게임 초기화 단계 테스트코드 작성
    GameInitStep 의 실행 가능 여부 테스트.
    GameInitStep 가 다음 단계로 [숫자 입력 단계]를 갖는지 테스트.
    GameInitStep 실행 시 게임 정보를 초기화하는지 테스트.

---
+++ src/test/java/baseball/domain/step/GameInitStepTest.java
@@ -0,0 +1,52 @@
+package baseball.domain.step;
+
+import baseball.application.context.BaseBallGameContext;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
+
+@DisplayName("GameInitStep 은 ")
+class GameInitStepTest {
+
+    @Test
+    @DisplayName("실행 가능하다")
+    public void isExecutable() {
+        // given
+        GameInitStep gameInitStep = new GameInitStep();
+
+        // when
+        boolean executable = gameInitStep.executable();
+
+        // then
+        assertThat(executable).isTrue();
+    }
+
+    @Test
+    @DisplayName("다음 단계로 [숫자 입력 단계]를 갖는다")
+    public void nextIsInputNumberStep() {
+        // given
+        GameInitStep gameInitStep = new GameInitStep();
+
+        // when
+        Step next = gameInitStep.next();
+
+        // then
+        assertThat(next).isExactlyInstanceOf(InputNumberStep.class);
+    }
+
+    @Test
+    @DisplayName("실행 시 게임의 상태를 초기화한다")
+    public void initContextWhenExecuted() throws Exception {
+        // given
+        GameInitStep gameInitStep = new GameInitStep();
+        BaseBallGameContext context = mock(BaseBallGameContext.class);
+
+        // when
+        gameInitStep.execute(context);
+
+        // then
+        verify(context, times(1)).init();
+    }
+}
