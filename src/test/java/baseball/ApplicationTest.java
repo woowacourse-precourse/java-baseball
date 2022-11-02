efa009e1c6e27bad29824acecd1f90699269193c
dmdrk1414 <seungchan141414@gmail.com>
2022-11-02 23:57:35 +0900

    :white_check_mark:[Test] 유저숫자_스트라이트_볼_낫싱_체크_case_3 추가
    유저 132 321 213
    컴퓨터 123
    결과   스트라이크 : 1, 볼 2

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -93,6 +93,31 @@ class ApplicationTest extends NsTest {
         assertThat(result).isTrue();
     }
 
+    @Test
+        // Computer.isRightThreeNumOfComputer
+        // 스트라이크 1 , 볼 2
+    void 유저숫자_스트라이트_볼_낫싱_체크_case_3() {
+        int[] testInt = new int[]{132, 321, 213};
+        Computer computer = new Computer();
+        computer.numThreeRanOfComputerList = new ArrayList<>(List.of(
+            1, 2, 3
+        ));
+        ArrayList<ArrayList<Integer>> resultList = new ArrayList<>();
+        int resultStrike = 0;
+        int resultBall = 0;
+        for (int num : testInt) {
+            computer.setCntBall(0);
+            computer.setCntStrike(0);
+            computer.isRightThreeNumOfComputer(num);
+            resultStrike = computer.getCntStrike();
+            resultBall = computer.getCntBall();
+            ArrayList<Integer> list = new ArrayList<>(List.of(resultStrike, resultBall));
+            resultList.add(list);
+        }
+
+        boolean result = resultList.stream().allMatch(list -> list.containsAll(List.of(1, 2)));
+        assertThat(result).isTrue();
+    }
     // computer Test 종료
 
     @Test
