bc5cc44f6997eacdaf32711499c54443eaa775c7
rio <75190035+HyoJongPark@users.noreply.github.com>
2022-11-02 16:44:20 +0900

    refactor: 매직 넘버, 리터럴을 상수로 변경

--- src/main/java/baseball/utils/UserInputValidator.java
+++ src/main/java/baseball/utils/UserInputValidator.java
@@ -6,6 +6,12 @@ import java.util.Set;
 
 public class UserInputValidator {
 
+    private static final int INPUT_SIZE = 3;
+    private static final String ERROR_MESSAGE_NOT_VALID_INPUT_SIZE = "입력 숫자는 총 3개여야 합니다.";
+    private static final String ERROR_MESSAGE_DUPLICATE_NUMBER = "입력 숫자 중 중복된 숫자는 존재할 수 없습니다.";
+    private static final String ERROR_MESSAGE_NOT_DIGIT = "입력은 숫자만 허용합니다.";
+    private static final String ERROR_MESSAGE_NOT_VALID_NUMBER_RANGE = "입력은 1 ~ 9 사이의 수만 허용합니다.";
+
     public static void isValidBalls(List<Integer> balls) {
         isValidInputSize(balls.size());
         hasDuplicateNumber(balls);
@@ -17,27 +23,27 @@ public class UserInputValidator {
     }
 
     private static void isValidInputSize(int inputSize) {
-        if (inputSize != 3) {
-            throw new IllegalArgumentException("입력 숫자는 총 3개여야 합니다.");
+        if (inputSize != INPUT_SIZE) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_NOT_VALID_INPUT_SIZE);
         }
     }
 
     private static void hasDuplicateNumber(List<Integer> balls) {
         Set<Integer> duplicateChecker = new HashSet<>(balls);
-        if (duplicateChecker.size() != 3) {
-            throw new IllegalArgumentException("입력 숫자 중 중복된 숫자는 존재할 수 없습니다.");
+        if (duplicateChecker.size() != INPUT_SIZE) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_DUPLICATE_NUMBER);
         }
     }
 
     private static void isDigit(char ballNo) {
         if (!Character.isDigit(ballNo)) {
-            throw new IllegalArgumentException("입력은 숫자만 허용합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE_NOT_DIGIT);
         }
     }
 
     private static void isNotZero(char ballNo) {
         if (ballNo == 48) {
-            throw new IllegalArgumentException("입력은 1 ~ 9 사이의 수만 허용합니다.");
+            throw new IllegalArgumentException(ERROR_MESSAGE_NOT_VALID_NUMBER_RANGE);
         }
     }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,8 +3,11 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+
+    public static final String INPUT_MESSAGE_ENTER_USER_INPUT = "숫자를 입력해주세요 : ";
+
     public static String receiveUserInput() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_MESSAGE_ENTER_USER_INPUT);
         return Console.readLine();
     }
 }
