542b27d4aa9f9fe6792434fc111ddfadf88e2d7f
jmj005 <jmj00005@naver.com>
2022-11-03 01:58:03 +0900

    feat: 사용자의 입력 값이 올바르지 않을 때 예외 발생 후 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -59,6 +59,11 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        
+        List<Integer> userNumber = new ArrayList<>();
+        try {
+            userNumber = getUserNumber();
+        } catch (IllegalArgumentException exception) {
+            exception.printStackTrace();
+        }
     }
 }
