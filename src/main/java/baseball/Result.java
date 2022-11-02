32f067613b6a875f71f766f7982da1eee62f096e
ywj9811 <ywj9811@naver.com>
2022-11-03 03:53:36 +0900

    [feat] Result 클래스 메소드 private로 변경 및 글자 수정

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -28,20 +28,20 @@ public class Result {
             if (strike == 0 && ball == 0) {
                 System.out.println("낫싱");
             } else if (strike != 0 && ball == 0) {
-                System.out.println(strike + " 스트라이크");
+                System.out.println(strike + "스트라이크");
             } else if (strike == 0 && ball != 0) {
-                System.out.println(ball + " 볼");
+                System.out.println(ball + "볼");
             } else {
-                System.out.println(ball + " 볼 " + strike + " 스트라이크");
+                System.out.println(ball + "볼 " + strike + "스트라이크");
             }
         }
-        System.out.println("맞췄습니다.");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("다시 하시려면 1번 종료하시려면 2번을 눌러주세요");
         want = Integer.parseInt(readLine());
         return want;
     }
 
-    List<Integer> returnList(int inputNum) {
+    private List<Integer> returnList(int inputNum) {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             list.add(inputNum % 10);
