f64ddceb31732c5c6d20fe9d36af2a7eb395c4e8
jhon3242 <rldotnr2@gmail.com>
2022-11-02 22:11:59 +0900

    feat(출력 처리): 입력 전 문구 출력
    InputView
    - 정답 시도 입력 시 메시지 출력하고 입력을 받는 메서드 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,9 +3,10 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
+	private static final String TRY_MESSAGE = "숫자를 입력해주세요 : ";
 
 	public static String tryAnswerInput(){
-		System.out.println();
+		System.out.println(TRY_MESSAGE);
 		return Console.readLine();
 	}
 }
