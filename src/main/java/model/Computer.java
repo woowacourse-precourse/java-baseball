cde1951e0850c402f4a07c82c2954e4eb08e8b6a
dmdrk1414 <seungchan141414@gmail.com>
2022-11-02 18:56:05 +0900

    :sparkles:[Feat] 컴퓨터의 숫자와 비교를 원하는 숫자를 확인하는 함수 선언 구현X

--- src/main/java/model/Computer.java
+++ src/main/java/model/Computer.java
@@ -27,6 +27,17 @@ public class Computer {
         return Randoms.pickNumberInRange(NUM_START_RANGE_RAM, NUM_END_RANGE_RAM);
     }
 
+    // 컴퓨터야 이 3개의 숫자가 너의 랜덤리스트와 맞니?
+    // 컴퓨터의 숫자와 매개변수의 숫자를 비교합니다.
+    public Boolean isRightThreeNumOfComputer(int numOfCompare) {
+        // TODO: 1. numOfCompare를 3개의 숫자로 분리한다.
+        // TODO: 2. 각숫자가 컴퓨터 랜덤 리스트에 포함하는가 안하는가 확인
+        // TODO: 2.1 if 포함한다면?
+        // TODO: 2.1.1 스트라이크 볼 체크
+        // TODO: 2.2 else 포함안한다면? => 낫싱
+        return true;
+    }
+
     public List<Integer> getNumThreeRanOfComputerList() {
         return numThreeRanOfComputerList;
     }
