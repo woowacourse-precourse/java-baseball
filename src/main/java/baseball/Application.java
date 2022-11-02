6455933eee2d805d7cfeb5643ea29f49305a4f7e
Kim woo jong <skekdnwhd@gmail.com>
2022-11-02 19:59:14 +0900

    add ConsoleLog 시작 알림메시지 출력기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,9 @@
 package baseball;
 
+import baseball.console.ConsoleLog;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ConsoleLog.println("숫자 야구 게임을 시작합니다.");
     }
 }
---
+++ src/main/java/baseball/console/ConsoleLog.java
@@ -0,0 +1,12 @@
+package baseball.console;
+
+public class ConsoleLog {
+    public ConsoleLog() {
+    }
+    public static void print(String message) {
+        System.out.print(message);
+    };
+    public static void println(String message) {
+        System.out.println(message);
+    };
+}
