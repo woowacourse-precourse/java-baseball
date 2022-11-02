2d6653bf28c63aa7c5346b894ba3fc4a51ea4f24
01192mg <01192mg@gmail.com>
2022-11-03 03:00:03 +0900

    feat: 게임 결과 계산기 기능 구현
    같은 수가 같은 자리에 있으면 '스트라이크' 개수를 올려주고, 다른 자리에 있으면 '볼' 개수를 올려준다.

--- src/main/java/baseball/ResultCalculator.java
+++ src/main/java/baseball/ResultCalculator.java
@@ -4,6 +4,23 @@ import java.util.List;
 
 public class ResultCalculator {
     public static List<Integer> calculateBallAndStrike(List<Integer> computer, List<Integer> player) {
-        return List.of(2, 1);
+        int ballCount = 0;
+        int strikeCount = 0;
+
+        for (int digit = 0; digit < computer.size(); digit++) {
+            int computerNumber = computer.get(digit);
+            int playerNumber = player.get(digit);
+
+            if (computerNumber == playerNumber) {
+                strikeCount++;
+                continue;
+            }
+
+            if (computer.contains(playerNumber)) {
+                ballCount++;
+            }
+        }
+
+        return List.of(ballCount, strikeCount);
     }
 }
