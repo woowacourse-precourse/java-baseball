d469f08ea52129d6f139fe0406c9fb809ec486a2
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 23:10:40 +0900

    Feat: 결과 문구 리턴하는 메세지

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -1,9 +1,21 @@
 package baseball;
 
+import constant.Messages;
 import domain.Numbers;
 
 public class Result {
     private int strikeCount;
     private int ballCount;
 
+    public Result(Numbers computerNumber, Numbers userNumber) {
+        strikeCount = computerNumber.countStrike(userNumber);
+        ballCount = computerNumber.countBall(userNumber);
+    }
+
+    public String getResult() {
+        if(this.strikeCount == 3) return Messages.successMessage;
+        if(this.strikeCount == 0 && this.ballCount == 0) return Messages.nothing;
+
+        return this.ballCount + Messages.ball + " " + this.strikeCount + Messages.strike;
+    }
 }
