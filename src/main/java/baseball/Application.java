d6bbbb1bea9793b1926a8a6ec5127cabc29f7390
yeonsu <jack0810@kookmin.ac.kr>
2022-11-02 21:40:36 +0900

    feat(): InputNum (src/main/java/baseball/Application.java)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,8 +3,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.*;
 public class Application {
-    public static List<Integer> GenerateNum(){ // 숫자 생성
-        List<Integer> answer = new ArrayList<>();
+    public static ArrayList<Integer> GenerateNum(){ // 숫자 생성
+        ArrayList<Integer> answer = new ArrayList<>();
         while (answer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!answer.contains(randomNumber)) {
@@ -13,9 +13,19 @@ public class Application {
         }
         return answer;
     }
+
+    public static ArrayList<Integer> InputNum(){
+        char[] playerNumString = Console.readLine().toCharArray();
+        ArrayList<Integer> playerNum = new ArrayList<Integer>();
+        for(char num : playerNumString){
+            playerNum.add(num - '0');
+        }
+        return playerNum;
+    }
     public static void main(String[] args) {
-        List<Integer> answer = GenerateNum();
-        System.out.println(answer);
+//        List<Integer> answer = GenerateNum();
+        System.out.println(InputNum());
+
         // TODO: 프로그램 구현
     }
 }
