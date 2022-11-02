a4e150c20ce8dbace01e8545f23ad403f0870aba
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 23:21:44 +0900

    Test: 볼/스트라이크 동시 발생 테스트

--- src/test/java/baseball/ResultTest.java
+++ src/test/java/baseball/ResultTest.java
@@ -44,6 +44,22 @@ public class ResultTest{
         assertEquals(result.getResult(), "3볼");
     }
 
+    @Test
+    void 볼_스트라이크_문구_출력_확인() {
+        List<Number> list = new ArrayList<>();
+        for (int i = 1; i <= 3; i++) {
+            list.add(new Number(i));
+        }
+        Numbers cNum = new Numbers(list);
 
+        list = new ArrayList<>();
+        for (int i = 3; i >= 1; i--) {
+            list.add(new Number(i));
+        }
 
+        Numbers uNum = new Numbers(list);
+        Result result = new Result(cNum, uNum);
+
+        assertEquals(result.getResult(), "2볼 1스트라이크");
+    }
 }
