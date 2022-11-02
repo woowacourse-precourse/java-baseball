06cb3580a32440fd8f7529c70f10569e91e1a464
Jeangyu <gjwlsrb1700@naver.com>
2022-11-02 17:01:43 +0900

    Add: 문자열 입력 & 입력 체크 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,6 +9,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println(makeAnswer());
+        System.out.println(input());
     }
 
     public static String makeAnswer(){
@@ -21,6 +22,22 @@ public class Application {
             if (!nums.contains(rand_num)) nums.add(rand_num);
         }
         for (Integer num: nums) result += Integer.toString(num);
+
+        return result;
+    }
+
+    public static Boolean CheckSameInput(String str){
+        if (str.charAt(0) == str.charAt(1) || str.charAt(1) == str.charAt(2) || str.charAt(0) == str.charAt(2))
+            return true;
+        return false;
+    }
+    public static String input() throws IllegalArgumentException{
+        String result = camp.nextstep.edu.missionutils.Console.readLine();
+
+        if (!result.matches("[1-9][1-9][1-9]") ||
+             result.length() != 3 ||
+             CheckSameInput(result))
+            throw new IllegalArgumentException();
 
         return result;
     }
