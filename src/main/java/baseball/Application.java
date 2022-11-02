3dc82d810cfbaf207926275ec80e476d473fb815
junhei Lee <jun2ee22@naver.com>
2022-11-03 02:32:34 +0900

    refactor: 게임 시작 시 컴퓨터가 게임 준비하는 기능
    생성자 호출 시 시작 문구 출력, 정답추출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,44 +8,32 @@ import java.util.*;
 
 public class Application {
 
-    private static final int answerLength = 3;
-
-    private static List<Integer> startGame() {
-
-        System.out.println("숫자 야구 게임을 시작합니다.");
-
-        List<Integer> correctAnswer = new ArrayList<>();
-        while (correctAnswer.size() < answerLength) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!correctAnswer.contains(randomNumber)) {
-                correctAnswer.add(randomNumber);
-            }
-        }
-        return  correctAnswer;
-    }
-
-    private static List<Integer> getUserAnswer() {
-        System.out.print("숫자를 입력해주세요 : ");
-        String[] inputArray = Console.readLine().split("");
-
-        List<Integer> userAnswer = new ArrayList<>();
-        try {
-            for (String input : inputArray) {
-                userAnswer.add(Integer.parseInt(input));
-            }
-        }catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
-        }
-
-        Set<Integer> userAnswerSet = new HashSet<>(userAnswer);
-        if (userAnswer.size() != answerLength) {
-            throw new IllegalArgumentException();
-        } else if (userAnswer.size() != userAnswerSet.size()) {
-            throw new IllegalArgumentException();
-        }
-
-        return userAnswer;
-    }
+//
+//    private static List<Integer> startGame() {
+//
+//
+//    private static List<Integer> getUserAnswer() {
+//        System.out.print("숫자를 입력해주세요 : ");
+//        String[] inputArray = Console.readLine().split("");
+//
+//        List<Integer> userAnswer = new ArrayList<>();
+//        try {
+//            for (String input : inputArray) {
+//                userAnswer.add(Integer.parseInt(input));
+//            }
+//        }catch (NumberFormatException e) {
+//            throw new IllegalArgumentException();
+//        }
+//
+//        Set<Integer> userAnswerSet = new HashSet<>(userAnswer);
+//        if (userAnswer.size() != Start.answerLength) {
+//            throw new IllegalArgumentException();
+//        } else if (userAnswer.size() != userAnswerSet.size()) {
+//            throw new IllegalArgumentException();
+//        }
+//
+//        return userAnswer;
+//    }
 
 
 
---
+++ src/main/java/baseball/Start.java
@@ -0,0 +1,38 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Start {
+    protected static final int answerLength = 3;
+    private List<Integer> correctAnswer;
+
+    public Start() {
+        printStart();
+        this.correctAnswer = setCorrectAnswer();
+    }
+
+    public List<Integer> getCorrectAnswer() {
+        return this.correctAnswer;
+    }
+
+    private List<Integer> setCorrectAnswer() {
+        List<Integer> randomNumbers = new ArrayList<>();
+
+        while (randomNumbers.size() < answerLength) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+
+        return  randomNumbers;
+    }
+    private void printStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+
+}
