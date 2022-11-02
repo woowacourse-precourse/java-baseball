0561c75563b02bcca4ebd186b1eef1b27992dbba
Kimhyeyun <cqqwer@gmail.com>
2022-11-02 23:37:21 +0900

    Test: 3스트라이크 후 종료 여부 테스트

--- src/test/java/baseball/BaseBallTest.java
+++ src/test/java/baseball/BaseBallTest.java
@@ -9,4 +9,10 @@ public class BaseBallTest {
         BaseBall baseBall = new BaseBall();
         baseBall.roundOfGame();
     }
+
+    @Test
+    void 게임_종료_여부_확인() {
+        BaseBall baseBall = new BaseBall();
+        baseBall.playGame();
+    }
 }
