0f972f6461a8f88b68373cea37fc340b8ea3f317
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-02 16:47:49 +0900

    [feat] : 게임 시작 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,17 @@
 package baseball;
 
-public class Game {
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
+public class Game {
+    public static void startGame() {
+        String answerNumber = "";
+        for (int i = 0; i < 3; i++) {
+            answerNumber += Integer.toString(pickNumberInRange(1, 9));
+        }
+        do {
+            System.out.println("숫자를 입력해주세요: ");
+            String getNumber = readLine();
+        } while (true);
+    }
 }
