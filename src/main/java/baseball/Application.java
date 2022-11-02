86a611e9eedffaf49b10c3b28ccedcb22e2487ef
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-03 01:45:48 +0900

    Feat : 사용자로부터 숫자를 입력받아 리턴하는 기능 추가 missionutils.Console의 readLine()을 사용하여 입력받음 사용자가 잘못된 값을 입력할 시 IllegalArgumentException 발생 후 애플리케이션 종료 서로다른 임의의 수 3개를 리스트에 담아 리턴

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,10 +10,9 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Application app = new Application();
-        List<Integer> computer = new ArrayList<>(app.SelectRandomNumbers());
-
     }
 
+    // TODO : 컴퓨터가 숫자를 랜덤으로 선택하여 리턴
     public List<Integer> SelectRandomNumbers() {
         List<Integer> numbers = new ArrayList<>();
         while (numbers.size() < 3) {
@@ -21,6 +21,21 @@ public class Application {
                 numbers.add(randomNumber);
             }
         }
+        return numbers;
+    }
+
+    // TODO : 사용자가 숫자를 선택한 숫자를 리턴
+    public List<Integer> selectNumbers(){
+        String inputNumber = Console.readLine();
+
+        // TODO : 사용자가 잘못된 값을 입력할 시 IllegalArgumentException 발생
+        //isInputMatch(inputNumber);
+
+        // TODO : 사용자가 선택한 숫자를 List에 담아 리턴
+        List<Integer> numbers = new ArrayList<>();
+        for (char number : inputNumber.toCharArray())
+            numbers.add(Character.getNumericValue(number));
+
         return numbers;
     }
 }
