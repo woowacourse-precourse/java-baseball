0620526791108111d83ca53c64512ca5c9e51a95
shinD <huipulci1@naver.com>
2022-11-03 06:19:35 +0900

    fix: 랜던 값 생성 기능 pickNumberInRange를 사용하도록 변경
    ApplicationTest 에서 설정한 값으로 Random number가 생성되기 위해서는
    pickNumberInRange를 사용하여야 함.

--- src/main/java/baseball/infrastructure/random/NextStepRandomNumberGenerator.java
+++ src/main/java/baseball/infrastructure/random/NextStepRandomNumberGenerator.java
@@ -4,16 +4,23 @@ import baseball.application.random.RandomNumberGenerator;
 import baseball.application.random.Range;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NextStepRandomNumberGenerator implements RandomNumberGenerator {
 
     @Override
     public List<Integer> generateUniqueNumberInRange(Range range, int count) {
-        return Randoms.pickUniqueNumbersInRange(
-                range.startInclusive(),
-                range.endInclusive(),
-                count
-        );
+        Set<Integer> numbers = new LinkedHashSet<>();
+
+        while (numbers.size() < count) {
+            // Application Test 통과를 위해 pickNumberInRange 사용
+            int number = Randoms.pickNumberInRange(range.startInclusive(), range.endInclusive());
+            numbers.add(number);
+        }
+
+        return new ArrayList<>(numbers);
     }
 }
