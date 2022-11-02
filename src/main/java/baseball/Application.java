547cddb0e300cdf8c0ce910b565b5a6f328a42bd
minsu <bill7666@naver.com>
2022-11-02 20:40:31 +0900

    랜덤숫자를 뽑아서 list에 저장

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static List<Integer> PickRandomNumber(){
+        List<Integer> Number = new ArrayList<>();
+        while (Number.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!Number.contains(randomNumber)) {
+                Number.add(randomNumber);
+            }
+        }
+        return Number;
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+            List<Integer> RandomNumber = PickRandomNumber();
+
+
     }
 }
