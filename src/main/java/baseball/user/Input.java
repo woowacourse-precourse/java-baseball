7ad46dc48c5f9f0cae5b68dc957a604983e7296b
Reznov <reznov03048@gmail.com>
2022-11-02 23:33:42 +0900

    feat: 문자열로 받은 입력값을 숫자 리스트로 반환하는 메소드 구현

--- src/main/java/baseball/user/Input.java
+++ src/main/java/baseball/user/Input.java
@@ -1,5 +1,8 @@
 package baseball.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 사용자의 입력을 받는 기능
  * 입력받은 숫자의 유효성을 파악하는 기능
@@ -8,5 +11,21 @@ package baseball.user;
 public class Input {
 
     public Input(){}
+
+    /**
+     * 문자열로 받은 입력값을 숫자 리스트로 반환하는 메소드
+     * @param inputString 문자열로 입력받은 값
+     * @return 입력받은 숫자를 넣은 리스트
+     */
+    private List<Integer> inputDigit(String inputString){
+        List<Integer> inputDigit = new ArrayList<>();
+
+        for(int inputStringIdx=0; inputStringIdx<inputString.length(); inputStringIdx++){
+            inputDigit.add(inputString.charAt(inputStringIdx) - '0');
+        }
+
+        return inputDigit;
+    }
+
 
 }
