e762ea2f1b9fe473e80c848365dda6d18cb2b276
jhon3242 <rldotnr2@gmail.com>
2022-11-02 23:58:14 +0900

    feat(게임 진행 처리): 게임 시작시 정답 숫자 초기화 구현
    Application 내 메서드 추가
    - 정답을 랜덤 숫자로 초기화하는 메서드 구현

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -4,10 +4,25 @@ import baseball.model.Validator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
+import java.util.stream.IntStream;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class BaseballController {
 	private final int CORRECT_LENGTH = 3;
+	private String answer = "";
 	public void start(){
 		OutputView.gameStartView();
+		setRandomAnswer();
 		Validator tryValidator = new Validator(InputView.tryAnswerInput(), CORRECT_LENGTH);
+	}
+
+	private void setRandomAnswer(){
+		while (answer.length() < CORRECT_LENGTH){
+			int pickedNum = pickNumberInRange(0, 9);
+			if (!answer.contains(String.valueOf(pickedNum))){
+				answer += String.valueOf(pickedNum);
+			}
+		}
 	}
 }
