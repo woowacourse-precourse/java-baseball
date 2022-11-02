b5cca96e63863eb5fc94735ac310a294885c0097
sugamypapa <sk000801@naver.com>
2022-11-02 22:04:28 +0900

    feat: 랜덤 값 저장하는 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static List<Integer> getRandomNumber() {
+        List<Integer> randomNumber = new ArrayList<>();
+        for(int i = 0; i < 3; i++) {
+            randomNumber.add(Randoms.pickNumberInRange(1, 9));
+        }
+        return randomNumber;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
     }
 }
