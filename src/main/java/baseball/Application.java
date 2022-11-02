3f580824dc8577c7d7f3e6e6ecd294176dcc3003
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-11-02 21:15:31 +0900

    feat : answer 숫자 3개 정하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public static List<Integer> getAnswer() {
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> answer = getAnswer();
+        System.out.println(answer);
     }
 }
