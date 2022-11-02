f117a41082ae9cf229afe066a9f4afb2d86f25f3
dooboocookie <bkh3030@gmail.com>
2022-11-02 22:48:56 +0900

    feat: Numbers클래스 추가, 생성자 추가 및 검증 로직 추가

---
+++ src/main/java/baseball/Numbers.java
@@ -0,0 +1,57 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Numbers {
+
+    private final String ERROR_MESSAGE_THREE_DIGITS = "3자리 숫자를 입력해주세요.";
+    private final String ERROR_MESSAGE_DUPLICATE_NUMBER = "각각 다른 숫자를 입력해주세요.";
+
+    private List<Number> numbers = new ArrayList<>();
+
+    public Numbers(int input) {
+        validateThreeDigits(input);
+        validateDuplicateNumber(input);
+        createNumberList(input);
+    }
+
+    private void validateThreeDigits(int input) {
+        if ((input < 111) || (input > 999)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_THREE_DIGITS);
+        }
+    }
+
+    private void validateDuplicateNumber(int input) {
+        if (checkDuplicateNumber(input)) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_DUPLICATE_NUMBER);
+        }
+    }
+
+    private boolean checkDuplicateNumber(int input){
+        int firstNumberInt = input/100;
+        int secondNumberInt = input%100/10;
+        int thirdNumberInt = input%10;
+
+        return (firstNumberInt == secondNumberInt)
+                ||(secondNumberInt == thirdNumberInt)
+                ||(thirdNumberInt == firstNumberInt);
+    }
+
+    private void createNumberList(int input) {
+        for (int exponent = 2; exponent >= 0; exponent--) {
+            int decimalNumber = (int) Math.pow(10, exponent);
+            int number = input / decimalNumber;
+
+            addNumber(number);
+
+            input = input % decimalNumber;
+        }
+    }
+
+    private void addNumber(int numberInt) {
+        Number number = new Number(numberInt);
+        numbers.add(number);
+    }
+
+}
