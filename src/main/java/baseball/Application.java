9ff3bc34efc26330ef1a9496b4fd4e82e042a755
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-03 16:06:42 +0900

    feat: 게임시작후 컴퓨터가 랜덤한 수 3가지를 정하여 List에 저장하는 Method구현 중복되지 않는 수를 3개 넣어줌

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,19 @@
 package baseball;
 
+import java.util.LinkedList;
 import java.util.List;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Application {
 	private static List<Integer> select_Random_Number(){
-		
+		List<Integer> com_Random_Num = new LinkedList<>();
+		while(com_Random_Num.size()<3){
+			int now_num = pickNumberInRange(1,9);
+			if(!com_Random_Num.contains(now_num)){
+				com_Random_Num.add(now_num);
+			}
+		}
+		return com_Random_Num;
 	}
     private static void start_new_Baseball_Game() {
     	System.out.println("숫자 야구 게임을 시작합니다.");
