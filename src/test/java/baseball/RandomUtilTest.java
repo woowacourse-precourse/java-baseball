d76bfcb5254082464dc3c3d6a96582bc5cd20694
ksh-beginner <ohk9134@naver.com>
2022-11-03 21:04:51 +0900

    test(RandomUtilTest) : 랜덤 수의 각 자릿수가 서로 다른 수인지 테스트
    랜덤 수의 각 자릿수를 List<String>에 하나씩 넣고,
    중복을 제거하는 Set<String>으로 변환 시 size가 그대로 3이라면 중복이 없는 것이므로
    테스트 통과

--- src/test/java/baseball/RandomUtilTest.java
+++ src/test/java/baseball/RandomUtilTest.java
@@ -3,6 +3,13 @@ package baseball;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -21,5 +28,13 @@ public class RandomUtilTest {
         int randomNumber = RandomUtil.createRandomNumber();
 
         assertThat(randomNumber).isBetween(123, 987);
+    }
+
+    @Test
+    void 랜덤_수의_각_자릿수는_서로_다른_수() {
+        List<String> numberEachDigitList = Arrays.asList(String.valueOf(RandomUtil.createRandomNumber()).split(""));
+        Set<String> numberEachDigitSet = new HashSet<>(numberEachDigitList);
+
+        assertThat(numberEachDigitSet.size()).isEqualTo(3);
     }
 }
