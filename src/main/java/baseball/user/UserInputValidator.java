a367e48410ba23a8d9e7aa78d17f0a29b0af7715
K-Diger <kitten.diger@gmail.com>
2022-11-03 20:24:52 +0900

    refactor : 기능 목록 7번, 사용자 입력 값 검증하는 메서드 수정 - 숫자 외의 값이 입력된 경우 IllegalArgumentException - 0이 들어간 값을 입력 했을 경우 IllegalArgumentException

--- src/main/java/baseball/user/UserInputValidator.java
+++ src/main/java/baseball/user/UserInputValidator.java
@@ -8,16 +8,18 @@ public class UserInputValidator {
 
     public void validate(List<String> inputValue) {
 
-        if (inputValue.size() != 3) {
+        if (inputValue.size() != 3 || inputValue.contains("0")) {
             throw new IllegalArgumentException();
         }
 
         String temp = inputValue.get(0);
 
-        for (int i = 1; i < 3; i++) {
-            if (temp.equals(inputValue.get(i))) {
+        for (int i = 0; i < 2; i++) {
+            if (temp.charAt(0) < 47 || temp.charAt(0) > 58) {
                 throw new IllegalArgumentException();
-            } else if (!temp.equals(inputValue.get(i))) {
+            } else if (temp.equals(inputValue.get(i + 1))) {
+                throw new IllegalArgumentException();
+            } else if (!temp.equals(inputValue.get(i + 1))) {
                 temp = inputValue.get(i);
             }
         }
