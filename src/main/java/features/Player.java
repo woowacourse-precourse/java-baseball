758dfae88893054e8f12d130119384252b599eb2
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-04 01:22:57 +0900

    refactor :: change List to Map

--- src/main/java/features/Player.java
+++ src/main/java/features/Player.java
@@ -1,20 +1,31 @@
 package features;
 
+import static java.lang.String.valueOf;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Player {
 
     // 플레이어 기능 : 상대방(컴퓨터)의 수를 생성
-    public static List<Integer> createComputerNumber(){
+    public static Map<Integer, Character> createComputerNumber() {
 
-        List<Integer> computer = new ArrayList<>();
+        Map<Integer, Character> computerRepository = new HashMap<>();
 
-        while (computer.size()<=3){
-            computer.add(Randoms.pickNumberInRange(1, 9));
-        }
+        int playerNumber = Randoms.pickNumberInRange(1, 9);
+        String numberToString = valueOf(playerNumber);
 
-        return computer;
+        char indexZero = numberToString.charAt(0);
+        char indexOne = numberToString.charAt(1);
+        char indexTwo = numberToString.charAt(2);
+
+        computerRepository.put(0, indexZero);
+        computerRepository.put(1, indexOne);
+        computerRepository.put(12, indexTwo);
+
+        return computerRepository;
     }
 }
