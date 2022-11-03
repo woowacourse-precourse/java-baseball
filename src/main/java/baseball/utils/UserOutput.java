aa05f4efee7e6d50fe45965f271d3dfe811c042b
Jeangyu <gjwlsrb1700@naver.com>
2022-11-03 17:19:38 +0900

    [UPDATE] 게임 종료 되었을 때, 정답임을 알리도록 업그레이드

--- src/main/java/baseball/utils/UserOutput.java
+++ src/main/java/baseball/utils/UserOutput.java
@@ -5,13 +5,19 @@ public class UserOutput {
     public static void printResult(int ball, int strike){
         String result = "";
 
-        if (ball != 0)
-            result+= ball +"볼";
-        if (strike != 0)
-            result+= " " + strike +"스트라이크";
-        if (result.length() == 0)
+        if (ball != 0) {
+            result += ball + "볼";
+        }
+        if (strike != 0) {
+            result += " " + strike + "스트라이크";
+        }
+        if (result.length() == 0) {
             result = "낫싱";
+        }
 
         System.out.println(result.strip());
+        if (strike == 3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
     }
 }
