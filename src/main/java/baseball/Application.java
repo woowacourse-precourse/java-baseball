4584dab60a5e6d85402dc4a4b5e7f6a365ac34a7
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-04 00:04:52 +0900

    Feat: 사용자 입력 예외발생 기능 구현
    사용자가 3자리 숫자를 입력하지 않으면 IllegalStateException을 발생시키고 프로그램을 종료하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,24 @@
 package baseball;
 
+import javax.imageio.IIOException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         // TODO: 프로그램 구현
         int randomNumber = makeRandomNumber();
+        try {
+            int userInput = userInput();
+            System.out.println(userInput);
+        }catch (IllegalStateException e) {
+            System.out.println("IllegalStateException 발생");
+            System.out.println("프로그램 종료");
+        }
     }
 
 
@@ -30,5 +42,16 @@ public class Application {
         tmp = tmp.substring(0,3);
 
         return Integer.parseInt(tmp);
+    }
+
+
+    public static int userInput() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력해주세요: ");
+        int n = sc.nextInt();
+
+        if (!(n >= 100 && n <1000)) throw new IllegalStateException();
+
+        return n;
     }
 }
