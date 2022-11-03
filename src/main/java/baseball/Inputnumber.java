2c4745a4dcdecac86480cd71dd103efd92150ba3
eugene1204 <s1061001@naver.com>
2022-11-03 18:17:14 +0900

    fix: randomNumber 메소드 수정

--- src/main/java/baseball/Inputnumber.java
+++ src/main/java/baseball/Inputnumber.java
@@ -4,13 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Inputnumber {
+    String[] args;
 
-    public List<Integer> stringTointList(String[] args){
+    Inputnumber(){
+
+    }
+    Inputnumber(String[] args){
+        this.args = args;
+    }
+
+    public List<Integer> stringTointList(){
         List<Integer> argList = new ArrayList<>();
         for(String arg : args){
             argList.add(Integer.valueOf(arg));
         }
         return argList;
     }
+
+
 
 }
