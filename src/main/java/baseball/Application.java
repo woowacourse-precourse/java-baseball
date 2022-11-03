591443f841c47d57317d21c93faf68be708aa370
choyeram <choyeram8@gmail.com>
2022-11-03 21:12:44 +0900

    feat: 게임 시작 시 컴퓨터가 난수를 추출하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,15 +3,18 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Computer computer = new Computer();
 
         while (true) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
             String input = Console.readLine();
             if (isValidNumber(input)) {
-
+                computer.startGame();
             } else {
                 throw new IllegalArgumentException("서로 다른 3자리의 수를 입력해야 합니다.");
             }
@@ -34,7 +37,7 @@ public class Application {
     }
 
     public static boolean isDifferentNumber(String input) {
-        ArrayList<Character> numberList = new ArrayList<>();
+        List<Character> numberList = new ArrayList<>();
         for (char number : input.toCharArray()) {
             if (numberList.contains(number)) {
                 return false;
