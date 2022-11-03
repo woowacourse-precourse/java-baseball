c45abeca450385fb589be5c5e8ef22295a1612f6
jeujeon <1132jjw@naver.com>
2022-11-03 16:51:03 +0900

    feat(OutputView): 입력 받기 위한 힌트 문구 출력

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -1,9 +1,14 @@
 package baseball;
 
 public class OutputView {
-    public static final String GAME_START_STR = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_START_STR = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_HINT_STR = "숫자를 입력해주세요 : ";
 
     public void printGameStart() {
         System.out.println(GAME_START_STR);
+    }
+
+    public void printInputHint() {
+        System.out.print(INPUT_HINT_STR);
     }
 }
