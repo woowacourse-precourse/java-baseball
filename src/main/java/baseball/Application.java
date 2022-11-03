6d2383ac71880a2dc21023f7f926f079556cc53e
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-03 16:44:36 +0900

    숫자야구 게임을 실행하는 main 클래스

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,9 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-
+        BaseballGame baseballGame = new BaseballGame();
+        while (baseballGame.endGame()) {
+            baseballGame.start();
+        }
     }
 }
