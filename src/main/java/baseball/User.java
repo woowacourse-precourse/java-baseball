2a32dd4d01f9c8855f584a8e48c39dfc7dac2601
Jimin <thswlals0219@gmail.com>
2022-11-04 00:33:59 +0900

    feat: 문자열을 List로 반환하는 함수 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,10 +1,35 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class User {
+    private List<Integer> bat;
+    private final InputValueValidation inputValueValidation = new InputValueValidation();
+
+    public List<Integer> getBat() {
+        return this.bat;
+    }
+
     public void hitTheBall() {
         System.out.print("숫자를 입력해주세요 : ");
-        Console.readLine();
+
+        String inputValue = Console.readLine().strip();
+
+        if (!inputValueValidation.isValid(inputValue)) {
+            throw new IllegalArgumentException();
+        }
+
+        bat = toListInteger(inputValue);
     }
+
+    private List<Integer> toListInteger(String number) {
+        return Arrays.stream(number.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+
 }
