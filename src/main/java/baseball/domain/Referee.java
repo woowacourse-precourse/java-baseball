a11de7d45eea521e6372906158c9e78e13eee99e
jinny-l <jinny.l.dev@gmail.com>
2022-11-03 14:41:55 +0900

    feat(domain): Referee 클래스에 최종 결과(볼, 스트라이크, 낫싱)를 출력하는 메소드 생성

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -4,6 +4,35 @@ import java.util.List;
 
 public class Referee {
 
+	private static final String STRIKE = "스트라이크";
+	private static final String BALL = "볼";
+	private static final String NOTHING = "낫싱";
+
+	public static void printResult(List<Integer> computerNumbers, List<Integer> playerNumbers) {
+		String result = getResult(computerNumbers, playerNumbers);
+		System.out.println(result);
+	}
+
+	private static String getResult(List<Integer> computerNumbers, List<Integer> playerNumbers) {
+		int sameCount = getSameCount(computerNumbers, playerNumbers);
+		int strikeCount = getStrikeCount(computerNumbers, playerNumbers);
+		int ballCount = getBallCount(computerNumbers, playerNumbers);
+
+		if (sameCount == 0) {
+			return NOTHING;
+		}
+
+		if (strikeCount == 0) {
+			return ballCount + BALL;
+		}
+
+		if (ballCount == 0) {
+			return strikeCount + STRIKE;
+		}
+
+		return ballCount + BALL + " " + strikeCount + STRIKE;
+	}
+
 	private static int getBallCount(List<Integer> computerNumbers, List<Integer> playerNumbers) {
 		int sameCount = getSameCount(computerNumbers, playerNumbers);
 		int strikeCount = getStrikeCount(computerNumbers, playerNumbers);
