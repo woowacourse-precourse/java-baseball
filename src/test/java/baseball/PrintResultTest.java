1309d9be11aefc7568c58b4ca78f584bc66ed128
jiwon1217 <koagjw@gmail.com>
2022-11-04 01:36:52 +0900

    test(PrintResultTest.java): 3스트라이크 결과 출력

--- src/test/java/baseball/PrintResultTest.java
+++ src/test/java/baseball/PrintResultTest.java
@@ -12,4 +12,12 @@ public class PrintResultTest {
         Game game = new Game();
         assertThat(game.printResult(strikeCount, ballCount)).isEqualTo(Game.NOTHING);
     }
+
+    @ParameterizedTest
+    @CsvSource("3, 0")
+    void _3_STRIKE_결과_출력(int strikeCount, int ballCount) {
+        Game game = new Game();
+        assertThat(game.printResult(strikeCount, ballCount)).isEqualTo(
+                strikeCount + Game.STRIKE + Game.NEW_LINE + Game.GAME_OVER);
+    }
 }
