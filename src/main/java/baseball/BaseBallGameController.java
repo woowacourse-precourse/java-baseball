abe6df541583316e6f397317c42360e0726f2ce4
cpot5620 <=cpot5620@naver.com>
2022-11-03 23:23:35 +0900

    feat(BaseBallGameControoler): create class to control the overall game

--- src/main/java/baseball/BaseBallGameController.java
+++ src/main/java/baseball/BaseBallGameController.java
@@ -1,9 +1,41 @@
 package baseball;
 
-public class BaseBallGameController {
+import camp.nextstep.edu.missionutils.Console;
 
-    public String receiveUserNumber(){
-        String userInput = System.console().readLine();
+import java.util.List;
+
+public class BaseBallGameController {
+    private BaseBallGameModel baseBallGameModel;
+    private UserNumber userNumber;
+
+    public BaseBallGameController() {
+        baseBallGameModel = new BaseBallGameModel();
+        userNumber = new UserNumber();
+    }
+
+    public void startGame() {
+        baseBallGameModel.createRandomNumber();
+
+        while (!baseBallGameModel.isFinish()) {
+            System.out.print("숫자를 입력해주세요 : ");
+
+            String userInput = receiveUserNumber();
+            userNumber.setUserNumbers(userInput);
+
+            printResult();
+        }
+    }
+
+    private void printResult() {
+        List<Integer> userNumbers = userNumber.getUserNumbers();
+        int strike = baseBallGameModel.strikeCount(userNumbers);
+        int ball = baseBallGameModel.ballCount(userNumbers);
+
+        BaseBallGameView.printResult(ball, strike);
+    }
+
+    private String receiveUserNumber() {
+        String userInput = Console.readLine();
 
         return userInput;
     }
