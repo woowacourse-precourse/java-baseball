cc3b10544d8117a92eb35f8870efb290cde0e274
YSEO <1park4170@gmail.com>
2022-11-03 15:01:47 +0900

    refactor<making Random> : delete "static" in method declaration
    in retrospect, there is no reason to keep two methods static. It is waste of memory
    
    Before:
    method declaration
    {
    public static int[] makeRandomNums()
    public static boolean checkIfDuplicatedNumber(int random, int[] randomNumbers)
    }
    
    After:
    method declaration
    {
    public int[] makeRandomNums()
    public boolean checkIfDuplicatedNumber(int random, int[] randomNumbers)
    }

--- src/main/java/baseball/CreateRandom.java
+++ src/main/java/baseball/CreateRandom.java
@@ -8,7 +8,7 @@ import java.util.stream.IntStream;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 class CreateRandom {
-    public static int[] makeRandomNums() {
+    public int[] makeRandomNums() {
         int random;
         int[] randomNumbers = new int[3];
         int i = 0;
@@ -25,7 +25,7 @@ class CreateRandom {
         return randomNumbers;
     }
 
-    private static boolean checkIfDuplicatedNumber(int random, int[] randomNumbers) {
+    public boolean checkIfDuplicatedNumber(int random, int[] randomNumbers) {
         for(int i = 0 ; i < 3; i++ ) {
             if (randomNumbers[i] == random) {
                 return true;
