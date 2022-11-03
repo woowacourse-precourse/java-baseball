2196ce8f8d1e170f7f55ac18c08a8ce29dce7870
jongseo98 <jspark98@unist.ac.kr>
2022-11-03 22:51:27 +0900

    메인 프로그램 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Output output = new Output();
+        Terminate terminate = new Terminate();
+        boolean exit = false;
+
+        while (!exit) {
+            List<Integer> computer = RandomNumber.generate();
+            String result = "";
+            while (!result.equals("3스트라이크")) {
+                List<Integer> player = Input.playerNumber();
+                Validate.check(player);
+                result = output.result(computer, player);
+                System.out.println(result);
+            }
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            exit = terminate.exit();
+        }
+
     }
+
 }
