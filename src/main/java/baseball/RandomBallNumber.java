2315cdb82ad29790657ab7107b0d66921eb4616b
CMHyuk <alsgur9710@naver.com>
2022-11-03 13:04:45 +0900

    feat: 랜덤 숫자 생성

---
+++ src/main/java/baseball/RandomBallNumber.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomBallNumber {
+
+    private static final int BALL_NUMBER_LENGTH = 3;
+
+    String randomNum = "";
+    public String createRandomNum() {
+        while(randomNum.length() < BALL_NUMBER_LENGTH) {
+            String randomNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
+            if(!randomNum.contains(randomNumber)) {
+                randomNum = randomNum.concat(randomNumber);
+            }
+        }
+        System.out.println(randomNum);
+        return randomNum;
+    }
+}
