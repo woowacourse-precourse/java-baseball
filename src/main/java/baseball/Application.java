637985af6418f24fe1cad26401df3e585a1a01e8
eora21 <eora21@naver.com>
2022-11-03 15:43:15 +0900

    feat: 🎸 추가 사용자의 입력을 비교하여 목표값을 맞췄는지 계속 확인하는 메서드
    사용자의 입력을 받고, 해당 입력으로 결과를 받고, 그 결과가 기준에 충족했는지 비교하며 반복하는 메서드를 작성하였다.
    strike, ball과 같은 텍스트는 게임의 룰과 관련이 되어 있기에, 해당 단어를 쓰지 않도록 노력했으며 정답을 맞춘 횟수
    == 랜덤한 숫자를 유니크하게 선택한 횟수이기에 매직넘버를 사용하지 않도록 주의를 기울였다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,9 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
@@ -14,13 +16,29 @@ public class Application {
         private static final int START_NUMBER = 1;
         private static final int END_NUMBER = 9;
         private static final int PICK_COUNT = 3;
+        private static final String CORRECT_ANSWER = "strike";
+        private static final String SIMILAR_ANSWER = "ball";
+
         public void play() {
             System.out.println("숫자 야구 게임을 시작합니다.");
             List<Integer> targetNumbers = getTargetNumbers();
+            repeatUntilUserHitsTargetNumber();
         }
 
         private List<Integer> getTargetNumbers() {
             return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, PICK_COUNT);
+        }
+
+        private void repeatUntilUserHitsTargetNumber() {
+            Map<String, Integer> gameResult = new HashMap<>();
+
+            do {
+                int[] usersInput = getUsersInput();
+                getGameResult(usersInput, gameResult);
+                printGameResult(gameResult);
+            } while (gameResult.get(CORRECT_ANSWER) != PICK_COUNT);
+
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
     }
 }
