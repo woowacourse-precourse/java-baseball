55425bf753315ce80d2e4d914e2ac8c3c90a4d85
raycho <raycho521@yonsei.ac.kr>
2022-11-03 14:40:04 +0900

    test (test/baseball) : add strike tests
    2022.11.03 by Ray
    스트라이크 판독 메소드를 작성하기 위한 테스트 생성함

---
+++ src/main/java/baseball/ScoreDetect.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import java.util.List;
+
+public class ScoreDetect {
+
+    /**
+     * get strike count by number
+     * @param answer
+     * @param numbers
+     * @return
+     */
+    public int getStrikeCount (
+            List<Integer> answer,
+            List<Integer> numbers
+    ) {
+        if (numbers.size() != 3)
+            return -1;
+        return 0;
+    }
+
+    public int getBallCount (
+            List<Integer> answer,
+            List<Integer> numbers
+    ) {
+        if (numbers.size() != 3)
+            return -1;
+        return 0;
+    }
+}
