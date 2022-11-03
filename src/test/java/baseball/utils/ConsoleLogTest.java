bc7a3d4012673960691c4458b2e199e1680c734c
Kim woo jong <skekdnwhd@gmail.com>
2022-11-03 18:51:44 +0900

    update ConsoleLogTest 기능 분리에 따른 테스트케이스 수정 및 업데이트

--- src/test/java/baseball/utils/ConsoleLogTest.java
+++ src/test/java/baseball/utils/ConsoleLogTest.java
@@ -4,24 +4,49 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConsoleLogTest extends NsTest {
+    ConsoleLog console = ConsoleLog.getInstance();
     @Test
     @DisplayName("사용자 입력테스트")
-    void 사용자_입력테스트() {
+    void userInputTest() {
         // given
-        run("123");
+        String number = "123";
 
         // when
-        List<Integer> userInput = ConsoleLog.input("message");
+        run(number);
+        String inputNum = console.input();
 
         // then
-        assertThat(output()).contains("message");
-        assertEquals(List.of(1,2,3),userInput);
+        assertEquals("123",inputNum);
+    }
+
+    @Test
+    @DisplayName("console창 테스트")
+    void consoleOutput() {
+        // given
+        String message = "message";
+
+        // when
+        console.print(message);
+
+        // then
+        assertThat(output()).isEqualTo("message");
+    }
+
+    @Test
+    @DisplayName("console창 테스트2")
+    void consoleOutput2() {
+        // given
+        String message = "message";
+
+        // when
+        console.println(message);
+
+        // then
+        assertThat(output()).isEqualTo("message");
     }
 
     @Override
