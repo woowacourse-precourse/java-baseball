016b62558ace10a6f661bf0ea038eefe823b6bf2
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-03 16:26:25 +0900

    style : 코딩 컨벤션 규칙에 맞게 수정

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -1,8 +1,5 @@
 package baseball;
 
-import static baseball.BaseballGame.*;
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
 public class Message {
     public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_NUM = "숫자를 입력해주세요 : ";
@@ -10,5 +7,4 @@ public class Message {
     public static final String ASK_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String END_GAME = "숫자 야구 게임을 종료합니다.";
     public static final String INPUT_ONE_OR_TWO = "잘못 입력하셨습니다. 1 또는 2를 입력해주세요.";
-
 }
