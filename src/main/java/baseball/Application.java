adb576a72b9c4891cc5427c13ca01adc83ab46f8
kdkdhoho <hkim4410@naver.com>
2022-11-03 16:34:33 +0900

    feat: 컴퓨터가 임의의 수를 생성하도록 하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -10,5 +13,18 @@ public class Application {
 
     public static void printStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static List<Integer> generateAnswer() {
+        List<Integer> answer = new ArrayList<>();
+
+        while(answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+
+        return answer;
     }
 }
