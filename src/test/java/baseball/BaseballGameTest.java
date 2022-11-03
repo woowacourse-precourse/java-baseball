911d0a97756463c4bfd11c007f4175d15b4b7df6
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-03 20:05:24 +0900

    test: 문자열 길이가 3인지 확인하는 메서드 테스트

--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -17,4 +17,76 @@ class BaseballGameTest {
         // then
         Assertions.assertThat(list.size()).isEqualTo(3);
     }
+
+    @Test
+    void 문자열_길이가_3인_경우_1() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+
+        // when
+        boolean check = baseballGame.isStringLengthThree("123");
+
+        // then
+        Assertions.assertThat(check).isEqualTo(true);
+    }
+
+    @Test
+    void 문자열_길이가_3인_경우_2() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+
+        // when
+        boolean check = baseballGame.isStringLengthThree("1b3");
+
+        // then
+        Assertions.assertThat(check).isEqualTo(true);
+    }
+
+    @Test
+    void 문자열_길이가_3인_경우_3() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+
+        // when
+        boolean check = baseballGame.isStringLengthThree("abc");
+
+        // then
+        Assertions.assertThat(check).isEqualTo(true);
+    }
+
+    @Test
+    void 문자열_길이가_3이_아닌_경우_1() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+
+        // when
+        boolean check = baseballGame.isStringLengthThree("");
+
+        // then
+        Assertions.assertThat(check).isEqualTo(false);
+    }
+
+    @Test
+    void 문자열_길이가_3이_아닌_경우_2() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+
+        // when
+        boolean check = baseballGame.isStringLengthThree("1234");
+
+        // then
+        Assertions.assertThat(check).isEqualTo(false);
+    }
+
+    @Test
+    void 문자열_길이가_3이_아닌_경우_3() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+
+        // when
+        boolean check = baseballGame.isStringLengthThree("a23dq");
+
+        // then
+        Assertions.assertThat(check).isEqualTo(false);
+    }
 }
