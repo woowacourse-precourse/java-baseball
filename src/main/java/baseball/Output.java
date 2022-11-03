ea91e3a2ae8d76f3bc8f415fd80de7d37b1afbe6
sieun <sieun960513@naver.com>
2022-11-04 02:36:22 +0900

    출력 문자열 시작메시지 [추가]

---
+++ src/main/java/baseball/Output.java
@@ -0,0 +1,15 @@
+package baseball;
+
+public class Output {
+    public static void startMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void scoreMessage(int[] score) {
+
+    }
+
+    public static void endMessage() {
+
+    }
+}
