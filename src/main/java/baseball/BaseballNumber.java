8e73ae2ad7ad6c4055ea75a954c404830cd27e49
Joonsoo Kim <kjsoo1901@gmail.com>
2022-11-03 16:51:15 +0900

    feat: Create BaseballNumber.java
    BaseballNumber.java 생성

---
+++ src/main/java/baseball/BaseballNumber.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
+public class BaseballNumber {
+    ArrayList<Integer> numbers;
+
+
+}
