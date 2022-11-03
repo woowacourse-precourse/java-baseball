053a0f6afd82c0be28077790b7ecf33f3682b0a8
code-tree <dltjdqls1200@naver.com>
2022-11-03 13:01:38 +0900

    feat: 1~9 사이의 랜덤한 숫자 3개를 생성

---
+++ src/main/java/baseball/service/NumberGenerator.java
@@ -0,0 +1,34 @@
+package baseball.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberGenerator {
+    static final int START_NUM = 1;
+    static final int END_NUM = 9;
+    static final int PICK_MAX_COUNT = 3;
+
+    public List<Integer> createRandomNumbers() {
+        List<Integer> randomNumbers = new ArrayList<>(PICK_MAX_COUNT);
+        int pickCount = 0;
+
+        while (pickCount < PICK_MAX_COUNT) {
+            randomNumbers.add(pickUniqueNumber(randomNumbers));
+            pickCount++;
+        }
+
+        return randomNumbers;
+    }
+
+    private int pickUniqueNumber(List<Integer> randomNumbers) {
+        int pickNumber = Randoms.pickNumberInRange(START_NUM, END_NUM);
+
+        while (randomNumbers.contains(pickNumber)) {
+            pickNumber = Randoms.pickNumberInRange(START_NUM, END_NUM);
+        }
+
+        return pickNumber;
+    }
+}
