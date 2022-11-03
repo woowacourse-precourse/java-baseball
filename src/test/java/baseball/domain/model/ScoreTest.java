6b24ca4505dedf525fd713dc9677c180e0f850ba
sangwon <swchoi1997@naver.com>
2022-11-03 21:15:32 +0900

    test: 나올 수 있는 모든 결과값 경우의 수 테스트
    user와 computer 를 비교하여 나타낼 수 있는 모든 결과값 테스트

--- src/test/java/baseball/domain/model/ScoreTest.java
+++ src/test/java/baseball/domain/model/ScoreTest.java
@@ -3,6 +3,7 @@ package baseball.domain.model;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import java.util.Map;
 
 import org.assertj.core.api.Assertions;
@@ -20,5 +21,122 @@ class ScoreTest {
 			BallCount.STRIKE, 0
 		);
 		assertThat(new Score()).isEqualTo(new Score(map));
+	}
+
+	@Test
+	void 비교결과_도출_3스트라이크(){
+		List<String> computerBalls = List.of("3", "5", "7");
+		List<String> userBalls = List.of("3", "5", "7");
+		Map<BallCount, Integer> result = Map.of(
+			BallCount.OUT, 0,
+			BallCount.BALL, 0,
+			BallCount.STRIKE, 3
+		);
+
+		assertThat(new Score(new Computer(computerBalls), new User(userBalls))).isEqualTo(new Score(result));
+	}
+
+	@Test
+	void 비교결과_도출_2스트라이크(){
+		List<String> computerBalls = List.of("4", "5", "7");
+		List<String> userBalls = List.of("3", "5", "7");
+		Map<BallCount, Integer> result = Map.of(
+			BallCount.OUT, 1,
+			BallCount.BALL, 0,
+			BallCount.STRIKE, 2
+		);
+
+		assertThat(new Score(new Computer(computerBalls), new User(userBalls))).isEqualTo(new Score(result));
+	}
+
+	@Test
+	void 비교결과_도출_1스트라이크(){
+		List<String> computerBalls = List.of("4", "8", "7");
+		List<String> userBalls = List.of("3", "5", "7");
+		Map<BallCount, Integer> result = Map.of(
+			BallCount.OUT, 2,
+			BallCount.BALL, 0,
+			BallCount.STRIKE, 1
+		);
+
+		assertThat(new Score(new Computer(computerBalls), new User(userBalls))).isEqualTo(new Score(result));
+	}
+
+	@Test
+	void 비교결과_도출_2볼_1스트라이크(){
+		List<String> computerBalls = List.of("3", "5", "7");
+		List<String> userBalls = List.of("5", "3", "7");
+		Map<BallCount, Integer> result = Map.of(
+			BallCount.OUT, 0,
+			BallCount.BALL, 2,
+			BallCount.STRIKE, 1
+		);
+
+		assertThat(new Score(new Computer(computerBalls), new User(userBalls))).isEqualTo(new Score(result));
+	}
+
+	@Test
+	void 비교결과_도출_1볼_1스트라이크(){
+		List<String> computerBalls = List.of("3", "5", "7");
+		List<String> userBalls = List.of("3", "7", "6");
+		Map<BallCount, Integer> result = Map.of(
+			BallCount.OUT, 1,
+			BallCount.BALL, 1,
+			BallCount.STRIKE, 1
+		);
+
+		assertThat(new Score(new Computer(computerBalls), new User(userBalls))).isEqualTo(new Score(result));
+	}
+
+	@Test
+	void 비교결과_도출_1볼(){
+		List<String> computerBalls = List.of("3", "5", "7");
+		List<String> userBalls = List.of("4", "7", "1");
+		Map<BallCount, Integer> result = Map.of(
+			BallCount.OUT, 2,
+			BallCount.BALL, 1,
+			BallCount.STRIKE, 0
+		);
+
+		assertThat(new Score(new Computer(computerBalls), new User(userBalls))).isEqualTo(new Score(result));
+	}
+
+	@Test
+	void 비교결과_도출_2볼(){
+		List<String> computerBalls = List.of("3", "5", "7");
+		List<String> userBalls = List.of("5", "3", "9");
+		Map<BallCount, Integer> result = Map.of(
+			BallCount.OUT, 1,
+			BallCount.BALL, 2,
+			BallCount.STRIKE, 0
+		);
+
+		assertThat(new Score(new Computer(computerBalls), new User(userBalls))).isEqualTo(new Score(result));
+	}
+
+	@Test
+	void 비교결과_도출_3볼(){
+		List<String> computerBalls = List.of("3", "5", "7");
+		List<String> userBalls = List.of("5", "7", "3");
+		Map<BallCount, Integer> result = Map.of(
+			BallCount.OUT, 0,
+			BallCount.BALL, 3,
+			BallCount.STRIKE, 0
+		);
+
+		assertThat(new Score(new Computer(computerBalls), new User(userBalls))).isEqualTo(new Score(result));
+	}
+
+	@Test
+	void 비교결과_도출_낫싱(){
+		List<String> computerBalls = List.of("3", "5", "7");
+		List<String> userBalls = List.of("2", "1", "4");
+		Map<BallCount, Integer> result = Map.of(
+			BallCount.OUT, 3,
+			BallCount.BALL, 0,
+			BallCount.STRIKE, 0
+		);
+
+		assertThat(new Score(new Computer(computerBalls), new User(userBalls))).isEqualTo(new Score(result));
 	}
 }
