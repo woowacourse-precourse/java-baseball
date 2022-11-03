711aa91f93cd0092abd3cbbc6f3e0ca531d9e44b
Minjune <minjune8506@gmail.com>
2022-11-03 15:57:08 +0900

    feat: 게임 재시작 메시지 출력

--- src/main/java/baseball/Printer.java
+++ src/main/java/baseball/Printer.java
@@ -13,4 +13,8 @@ public class Printer {
     public static void gameEndMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public static void gameRestartMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
