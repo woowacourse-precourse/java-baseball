60eb7ca9b27ff589ca08506c22ef9469cd423358
swandevson <shai4956@gmail.com>
2022-11-04 01:36:32 +0900

    Feat: Computer 생성자 및 숫자 생성 구현
    - 생성자 호출 시 랜덤으로 숫자 생성
    - 중복된 숫자를 제외시키면서 숫자 생성

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,9 +1,34 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
+
 
 public class Computer {
     BaseballNumber computerNumber;
 
 
-    public Computer() {}
+    public Computer() {
+        computerNumber = new BaseballNumber();
+
+        this.generateNumber();
+    }
+
+
+    public void generateNumber() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        String number = computer.toString().replaceAll("[^0-9]", "");
+
+        computerNumber.set(number);
+    }
 }
