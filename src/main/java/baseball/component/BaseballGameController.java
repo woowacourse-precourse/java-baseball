2603d22391d0b00d40e8ba14f252cca1170affd3
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-03 17:08:01 +0900

    feat(BaseballGameController): player number 입력받기

--- src/main/java/baseball/component/BaseballGameController.java
+++ src/main/java/baseball/component/BaseballGameController.java
@@ -1,5 +1,6 @@
 package baseball.component;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -7,6 +8,20 @@ import java.util.List;
 
 public class BaseballGameController {
 
+
+
+    private List<Integer> readPlayerNumbers(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String playerInputString = Console.readLine();
+
+        List<Integer> playerNumbers = new ArrayList<>();
+        for(int index = 0; index < playerInputString.length(); index++){
+            int number = Character.getNumericValue(playerInputString.charAt(index));
+            playerNumbers.add(number);
+        }
+
+        return playerNumbers;
+    }
 
     private List<Integer> generateThreeRandomNumbers(){
         List<Integer> randomNumbers = new ArrayList<>();
