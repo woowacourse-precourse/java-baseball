7d46592414e8cf218536dfb9362980ce374acd17
kokodak <kokodakadokok@gmail.com>
2022-11-03 16:15:23 +0900

    style(inputException): 자바 코드 컨벤션 적용

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -5,13 +5,19 @@ public class InputException {
     private static final int USER_INPUT_SIZE = 3;
 
     public static String isValidInput(String userInput) {
-        if (!isLengthThree(userInput)) throw new IllegalArgumentException();
-        if (!isThreeDifferentDigits(userInput) || !isNumberBetweenOneAndNine(userInput)) throw new IllegalArgumentException();
+        if (!isLengthThree(userInput)) {
+            throw new IllegalArgumentException();
+        }
+        if (!isThreeDifferentDigits(userInput) || !isNumberBetweenOneAndNine(userInput)) {
+            throw new IllegalArgumentException();
+        }
         return userInput;
     }
 
     public static String isOneOrTwo(String userInput) {
-        if (!userInput.equals("1") && !userInput.equals("2")) throw new IllegalArgumentException();
+        if (!userInput.equals("1") && !userInput.equals("2")) {
+            throw new IllegalArgumentException();
+        }
         return userInput;
     }
 
@@ -23,13 +29,17 @@ public class InputException {
         char firstDigit = userInput.charAt(0);
         char secondDigit = userInput.charAt(1);
         char thirdDigit = userInput.charAt(2);
-        if (firstDigit == secondDigit || firstDigit == thirdDigit || secondDigit == thirdDigit) return false;
+        if (firstDigit == secondDigit || firstDigit == thirdDigit || secondDigit == thirdDigit) {
+            return false;
+        }
         return true;
     }
 
     private static boolean isNumberBetweenOneAndNine(String userInput) {
         for (int i = 0; i < USER_INPUT_SIZE; i++) {
-            if (userInput.charAt(i) < '1' || userInput.charAt(i) > '9') return false;
+            if (userInput.charAt(i) < '1' || userInput.charAt(i) > '9') {
+                return false;
+            }
         }
         return true;
     }
