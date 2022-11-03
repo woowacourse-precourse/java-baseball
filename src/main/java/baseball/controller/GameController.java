899aebe42af8ef34a27ed2d4f7cda5cefba0322e
JeonghakLee <wjdgkr0527@naver.com>
2022-11-03 15:40:25 +0900

    feat: 컴퓨터의 랜덤 수 생성 기능 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,8 +1,29 @@
 package baseball.controller;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
+
+  private static final int RANDOM_NUMBER_SIZE = 3;
+  private static final int BEGIN = 1;
+  private static final int END = 9;
   private final List<Integer> computerNumbers;
+
+  public GameController() {
+    this.computerNumbers = getRandomNumbers();
+  }
+
+  private List<Integer> getRandomNumbers() {
+    List<Integer> randomNumbers = new ArrayList<>();
+    while (randomNumbers.size() < RANDOM_NUMBER_SIZE) {
+      int randomNumber = Randoms.pickNumberInRange(BEGIN, END);
+      if (!randomNumbers.contains(randomNumber)) {
+        randomNumbers.add(randomNumber);
+      }
+    }
+    return randomNumbers;
+  }
 
 }
