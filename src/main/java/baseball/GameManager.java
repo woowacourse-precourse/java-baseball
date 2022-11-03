b65fd053a050b35ea14385399e50e20e05ed330b
cpot5620 <=cpot5620@naver.com>
2022-11-03 23:58:57 +0900

    feat(GameManager): create class running the baseball game

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,2 +1,33 @@
-package baseball;public class GameManager {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+
+public class GameManager {
+    private boolean isContinue;
+
+    public GameManager() {
+        this.isContinue = true;
+    }
+
+    public void startBaseBallGame() {
+        BaseBallGameController baseBallGameController = new BaseBallGameController();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (isContinue) {
+            baseBallGameController.startGame();
+
+            askContinue();
+        }
+    }
+
+    private void askContinue() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String userInput = Console.readLine();
+
+        if (!userInput.equals("1"))
+            isContinue = false;
+    }
 }
