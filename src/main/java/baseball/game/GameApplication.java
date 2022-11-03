355ef005a303b6749c230592eb7de66444b01072
im-shung <seoyoung1113@gmail.com>
2022-11-03 23:55:48 +0900

    feat: :sparkles: 사용자 number 입력 받기

---
+++ src/main/java/baseball/game/GameApplication.java
@@ -0,0 +1,12 @@
+package baseball.game;
+
+import baseball.input.view.InputView;
+
+public class GameApplication {
+
+    public static void startGame() {
+        String userNumber = InputView.inputNumber();
+
+    }
+
+}
