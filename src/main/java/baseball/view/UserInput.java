998fe8c3ff7cc0e31216ef57b7989baa7d3ef68d
김현재 <41482946+galug@users.noreply.github.com>
2022-11-04 00:03:40 +0900

    feat(userinput): add 사용자에게 입력을 받는 기능

---
+++ src/main/java/baseball/view/UserInput.java
@@ -0,0 +1,14 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserInput {
+
+    public String input() {
+        return Console.readLine();
+    }
+}
