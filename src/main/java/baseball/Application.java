86014d8a7490cd6e51a24bb2529d12ae4524058e
jkde7721 <jkde7721@naver.com>
2022-11-03 13:34:27 +0900

    feat: 게임 종료 후, 플레이어 입력에 따른 게임 재시작 or 프로그램 종료 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,11 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private static final int RESTART_INPUT = 1;
+    private static final int TERMINATE_INPUT = 2;
 
     public static void main(String[] args) {
         try {
@@ -17,7 +22,11 @@ public class Application {
     }
 
     private static boolean isRestartingGame() {
-        //TODO: 게임 재시작 여부 입력 구현
-        return false;
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int playerInput = Integer.parseInt(Console.readLine());
+        if (playerInput != RESTART_INPUT && playerInput != TERMINATE_INPUT) {
+            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
+        }
+        return playerInput == RESTART_INPUT;
     }
 }
