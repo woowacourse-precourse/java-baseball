15708233be85e489dead54700fce6e3c7b6855aa
c4fiber <qudcjf153@gmail.com>
2022-11-03 21:18:47 +0900

    feat: 게임 시작 문구 출력 기능
    Game 객체를 생성하면 게임시작 문구가 자동으로 출력되게함.
    객체생성 후 바로 gameStart를 호출하면 필요없는 코드 한줄이 추가된다고 판단함.

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class Game {
+
+    public Game() {
+        gameStart();
+    }
+
+    private void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+}
