0c40fecde7e4821f34d9334973380abd4b7f4970
jinny-l <jinny.l.dev@gmail.com>
2022-11-03 14:46:51 +0900

    refactor(domain): Player 클래스의 플레이어의 입력 값을 받는 getInput() 메소드의 반환 타입 수정

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,15 +1,11 @@
 package baseball.domain;
 
-import baseball.util.StringToArrayList;
 import camp.nextstep.edu.missionutils.Console;
-
-import java.util.List;
 
 public class Player {
 
-	public List<Integer> getInput() {
-		String playerInput = Console.readLine();
-		return StringToArrayList.convert(playerInput);
+	public String getInput() {
+		return Console.readLine();
 	}
 
 }
