b2fb18b0af50345bbf4238bc9706f8feb234d2fc
Devtist <yt930310@snu.ac.kr>
2022-11-04 03:23:04 +0900

    feat: 출력할 메세지 정리 및 출력하는 기능 구현

---
+++ src/main/java/User/Outputs.java
@@ -0,0 +1,20 @@
+package User;
+
+public enum Outputs {
+	GAME_START_MESSAGE("숫자 야구 게임을 시작합니다."),
+	ASK_INPUT_MESSAGE("숫자를 입력해주세요 : "),
+	NUMBER_OF_BALL_MESSAGE("%d볼"),
+	NUMBER_OF_STRIKE_MESSAGE("%d스트라이크"),
+	NOTHING_MESSAGE("낫싱"),
+	GAME_END_MESSAGE("%d개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+	RESTART_OR_NOT_MESSAGE("게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.");
+	private final String message;
+
+	Outputs(String message) {
+		this.message = message;
+	}
+
+	public String print() {
+		return this.message;
+	}
+}
