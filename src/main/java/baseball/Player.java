abcf7b371665cb3f447466d9a3aec82d231c9eac
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-03 17:33:11 +0900

    recator : 빈 값을 입력 받을 시 예외처리 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -8,6 +8,9 @@ import java.util.List;
 public class Player {
 
     public static List<Integer> toIntegerUserInput(String userInput) {
+        if (userInput.isEmpty()) {
+            throw new IllegalArgumentException("값을 입력하지 않았습니다. 3자리 숫자를 입력해주세요.");
+        }
         List<Integer> userInputNumber = new ArrayList<>(NUM_LIMIT_LENGTH);
         String[] userInputArr = userInput.split("");
         for (String str : userInputArr) {
