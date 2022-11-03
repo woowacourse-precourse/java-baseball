4cc19a7abf579d2f588584cd25b8da3392012836
SEONMl <sm000117@naver.com>
2022-11-04 00:27:27 +0900

    fix: 불필요한 메서드 삭제

--- src/main/java/baseball/service/NumberCondition.java
+++ src/main/java/baseball/service/NumberCondition.java
@@ -1,7 +1,6 @@
 package baseball.service;
 
 
-import baseball.mapper.StringMapper;
 
 public class NumberCondition {
     private final String answer;
@@ -10,34 +9,11 @@ public class NumberCondition {
         this.answer = answer;
     }
 
-    public int countContainmentRightness(String number) {
-        int cnt = 0;
-        for(int i=0 ; i<number.length() ; i++) {
-            String oneOfNum = StringMapper.ctos(number.charAt(i));
-            if (doesContain(oneOfNum)) {
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-
-    public int countDigitRightness(String number) {
-        int cnt = 0;
-        for(int i=0 ; i<number.length() ; i++) {
-            String oneOfNum = StringMapper.ctos(number.charAt(i));
-            if (isDigitCorrect(oneOfNum, i)) {
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-
-
-    private boolean doesContain(String oneOfNum) {
+    boolean doesContain(String oneOfNum) {
         return answer.contains(oneOfNum);
     }
 
-    private boolean isDigitCorrect(String oneOfNum, int digit) {
+    protected boolean isDigitCorrect(String oneOfNum, int digit) {
         boolean result = false;
         if (answer.indexOf(oneOfNum) == digit) {
             result = true;
