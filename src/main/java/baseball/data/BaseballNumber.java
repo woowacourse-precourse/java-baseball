18f424cbb1f0c414d4428ce6c48bfc0937168f57
devmizz <garamminchan@gmail.com>
2022-11-03 15:55:26 +0900

    feat: implement BaseballNumber class
    부모 클래스 메서드 구현

--- src/main/java/baseball/data/BaseballNumber.java
+++ src/main/java/baseball/data/BaseballNumber.java
@@ -1,50 +1,37 @@
 package baseball.data;
 
+import baseball.GameConstant;
 import baseball.WarningMessage;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class BaseballNumber {
 
-    private static final int DIGIT = 3;
+    private List<Integer> numbers;
 
-    private static BaseballNumber baseballNumber;
-    private static List<Integer> baseballNumberList;
-
-    private BaseballNumber() {
-        baseballNumberList = new ArrayList<>();
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
-    public static BaseballNumber getInstance() {
-        if(baseballNumber == null) {
-            baseballNumber = new BaseballNumber();
-        }
-
-        return baseballNumber;
-    }
-
-    public BaseballNumber updateNumbers(List<Integer> updateNumbers) {
-        baseballNumberList = updateNumbers;
+    public void updateNumbers(List<Integer> updateNumbers) {
+        numbers = updateNumbers;
         isBetweenOneAndNine();
         isExistDuplicateNumber();
-
-        return this.baseballNumber;
     }
 
-    private void isBetweenOneAndNine() {
-        for(Integer i : baseballNumberList) {
-            if(i < 1 || 9 < i) {
+    void isBetweenOneAndNine() {
+        for(Integer number : numbers) {
+            if(number < 1 || 9 < number) {
                 throw new IllegalArgumentException(WarningMessage.IllegalValueWarningMessage.getMessage());
             }
         }
     }
 
-    private void isExistDuplicateNumber() {
-        Set<Integer> set = new HashSet<>(baseballNumberList);
-        if(set.size() < DIGIT) {
+    void isExistDuplicateNumber() {
+        Set<Integer> set = new HashSet<>(numbers);
+        if(set.size() < GameConstant.DIGIT.getValue()) {
             throw new IllegalArgumentException(WarningMessage.DuplicateValueWarningMessage.getMessage());
         }
     }
