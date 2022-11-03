ad00e7dd7078b8db9d30e082e90ed111fb4304c3
kimminsu <xjfcnfw3@naver.com>
2022-11-03 13:45:26 +0900

    feat: 유저와 컴퓨터 간의 숫자야구 규칙 적용 기능 구현

---
+++ src/main/java/baseball/rule/Rule.java
@@ -0,0 +1,39 @@
+package baseball.rule;
+
+import java.util.List;
+
+public class Rule {
+    private int ball;
+    private int strike;
+
+    public Rule() {
+        ball = 0;
+        strike = 0;
+    }
+
+    private void init() {
+        ball = 0;
+        strike = 0;
+    }
+
+    public void check(List<Integer> user, List<Integer> computer) {
+        for (int userIdx = 0; userIdx < 3; userIdx++) {
+            checkStrike(user.get(userIdx), computer.get(userIdx));
+            checkBall(user.get(userIdx), computer, userIdx);
+        }
+    }
+
+    private void checkStrike(Integer userDigit, Integer computerDigit) {
+        if (userDigit.equals(computerDigit)) {
+            strike++;
+        }
+    }
+
+    private void checkBall(Integer userDigit, List<Integer> computer, int userIdx) {
+        for (int computerIdx = 0; computerIdx < 3; computerIdx++) {
+            if ((userDigit == computer.get(computerIdx)) && (userIdx != computerIdx)) {
+                ball++;
+            }
+        }
+    }
+}
