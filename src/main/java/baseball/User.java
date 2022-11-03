9ab9d23d8b92befdd5a41dd10199df97a53205e0
kevstevie <jianna442@gmail.com>
2022-11-03 12:54:44 +0900

    feat: 입력받은 String을 integer list로 변환, throw illegalArgumentException 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -2,16 +2,24 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
 
 
-    public List userInput() {
-        try {
-            String str = Console.readLine();
-        } catch (IllegalArgumentException e) {
+    public List userInput() throws IllegalArgumentException{
+        String str = Console.readLine();
+        if (userInputValidate) {
+            throw new IllegalArgumentException() ;
         }
         return StringToNumberList(str);
+    }
+    private static List StringToNumberList(String str) {
+        List<Integer> result = new ArrayList();
+        for (int i = 0; i < str.length(); i++) {
+            result.add(str.charAt(i) - '0');
+        }
+        return result;
     }
 }
