b287379f6fa5937689939ecf0c8376a07d78cb0a
bomin1996 <bomin199628@gmail.com>
2022-11-03 19:29:55 +0900

    1.서로 다른 랜덤 3개의 숫자 생성 기능 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
+
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }//컴퓨터 랜덤 숫자 생성
         // TODO: 프로그램 구현
     }
 }
