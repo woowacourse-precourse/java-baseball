f5848422ab16e8d0a2a7ae10f5732e5fa2e8b646
Sehbeom <j3beom@gmail.com>
2022-11-03 22:47:44 +0900

    test(StringTest): 테스트 관련 연습
    테스트 관련 정리 내용에 대한 연습 수행
    
    StringTest.java 연습 코드 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,9 +1,22 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class StringTest {
 
     @Test
@@ -45,6 +58,94 @@ public class StringTest {
         assertThatThrownBy(() -> input.charAt(5))
                 .isInstanceOf(StringIndexOutOfBoundsException.class)
                 .hasMessageContaining("String index out of range: 5");
+    }
+
+    @Test
+    @DisplayName("이름 표시 연습 테스트")
+    void nameExpressionTest() {
+
+    }
+
+    @RepeatedTest(5)
+    @DisplayName("반복 테스트 연습")
+    void repeatedTest(RepetitionInfo repetitionInfo) {
+        System.out.println(repetitionInfo.getCurrentRepetition() + " / " + repetitionInfo.getTotalRepetitions());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("매개변수 테스트 연습")
+    void parameterizedTest(int input) {
+        System.out.println(input);
+    }
+
+    @Test
+    @DisplayName("assertThat 기본 테스트 연습")
+    void assertThatBasicTest() {
+        String string = "Woowa Tech Course";
+
+        assertThat(string)
+            .isNotNull()
+            .isNotEmpty()
+            .startsWith("Woowa")
+            .endsWith("Course")
+            .contains("Tech")
+            .isEqualTo("Woowa Tech Course")
+            .isNotEqualTo("abcd");
+    }
+
+    public class ExampleClass {
+        public String string;
+        public ExampleClass(String string){
+            this.string = string;
+        }
+    }
+
+    @Test
+    @DisplayName("동일성, 동등성 비교 연습")
+    void instanceCompareTest() {
+        ExampleClass ec1 = new ExampleClass("ex");
+        ExampleClass ec2 = new ExampleClass("ex");
+
+        // assertThat(ec1).isEqualTo(ec2); //fail. 동일성 비교
+        assertThat(ec1).usingRecursiveComparison().isEqualTo(ec2); //success. 동등성 비교
+    }
+
+    @Test
+    @DisplayName("Map 관련 테스트 연습")
+    void aboutMapTest() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("a", 1);
+        map.put("b", 2);
+
+        assertThat(map)
+            .containsKey("a")
+            .containsValue(1)
+            .containsEntry("a", 1)
+            .contains(entry("a", 1), entry("b", 2));
+    }
+
+    @Test
+    @DisplayName("예외처리 관련 테스트 연습")
+    void exampleTest() {
+        assertThatThrownBy(() -> {
+            List<String> list = new ArrayList<>(List.of("a", "b"));
+            list.get(2);
+        }).isInstanceOf(IndexOutOfBoundsException.class);
+
+        assertThatExceptionOfType(IndexOutOfBoundsException.class)
+            .isThrownBy(() -> {
+                List<String> list = new ArrayList<>(List.of("a", "b"));
+                list.get(2);
+            });
+
+        Throwable thrown = catchThrowable(() -> {
+            List<String> list = new ArrayList<>(List.of("a", "b"));
+            list.get(2);
+        });
+
+        assertThat(thrown)
+            .isInstanceOf(IndexOutOfBoundsException.class);
     }
 
 }
