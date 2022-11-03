83161ae0bf636f35b7d404ffa2b5e0dede6292b6
sangeun-b <baek0007@algonquinlive.com>
2022-11-04 04:56:40 +0900

    feat: 컴퓨터의 랜덤 숫자 생성 기능 추가

--- src/main/java/baseball/number/ComputerNumber.java
+++ src/main/java/baseball/number/ComputerNumber.java
@@ -1,4 +1,23 @@
 package baseball.number;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class ComputerNumber {
+    private List<Integer> computerNumber;
+
+    public ComputerNumber(){
+        computerNumber = new ArrayList<>();
+    }
+
+    public List<Integer> generateNumber() {
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+        return computerNumber;
+    }
 }
--- src/main/java/baseball/play/Play.java
+++ src/main/java/baseball/play/Play.java
@@ -1,5 +1,11 @@
 package baseball.play;
 
+import baseball.number.ComputerNumber;
+
+import java.lang.ref.PhantomReference;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Play {
     public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
@@ -9,8 +15,17 @@ public class Play {
     public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String OPTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    private List<Integer> computerNumberList;
+    private ComputerNumber computerNumber;
+
+    public Play(){
+        computerNumber = new ComputerNumber();
+        computerNumberList = new ArrayList<>();
+    }
     public void playBaseball(){
         System.out.println(START_MESSAGE);
+        computerNumber.generateNumber();
+
 
 
     }
