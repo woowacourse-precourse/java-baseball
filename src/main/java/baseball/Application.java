b8bb380948d031ce7910ff1f2a452867b99005b5
wonseokjee <wsji9404@gmail.com>
2022-11-03 16:39:46 +0900

    Feat: "추가 컴퓨터의 임의의 값 세자리 생성"
    랜덤으로 컴퓨터가 임의의 값 선택. 사용자가 알 수 없게 하면 좋을 듯. 메서드의 범위를 조절???

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        randomPick();
+    }
+
+    /*1.랜덤으로 컴퓨터가 임의의 값 선택.*/
+    public static void randomPick() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
 }
