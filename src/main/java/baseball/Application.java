73b78538a42ab7bc71a5a986618ff37290d41ef7
LeeJaeHong <0915jhlee@daum.net>
2022-11-03 15:59:03 +0900

    2. 사용자 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,4 +1,5 @@
 package baseball;
+
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -6,18 +7,23 @@ import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     static List<Integer> computerRandomNumber = new ArrayList<>();
-    public static void main(String[] args) {
-        while(true){
-            if (computerRandomNumber.size()==0){
-                computerRandomNumber=makeRandomNumber();
-            }
 
+    public static void main(String[] args) {
+        while (true) {
+            if (computerRandomNumber.size() == 0) {
+                System.out.println("숫자 야구 게임을 시작합니다.");
+                computerRandomNumber = makeRandomNumber();
+            }
+            System.out.print("숫자를 입력해주세요 : ");
+            readLine();
         }
     }
-    
-    static List<Integer> makeRandomNumber(){
+
+    static List<Integer> makeRandomNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
