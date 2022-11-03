d8a03838c98c0e1d75080243810e52ec7eca5646
Nugur1i <duckhee092@naver.com>
2022-11-03 18:10:07 +0900

    feat<Baseball> : Print the comments (ex 숫자를 입력해주세요.) & Enter number of user input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
@@ -16,6 +17,22 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        int escapeNumber = 1;
+        while (escapeNumber != 1){
+
+            System.out.println("숫자를 입력해주세요 :");
+            Scanner scanner = new Scanner(System.in);
+            int userInputNumber = scanner.nextInt();
+
+
+
+        }
+
+
+
+
 
 
 
