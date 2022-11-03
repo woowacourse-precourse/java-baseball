f597ca611f4b556be989b2d1519a58efaafbf6d1
jiwon1217 <koagjw@gmail.com>
2022-11-03 23:29:50 +0900

    test(InputNumberValidateTest.java): 숫자를 3개만 입력

---
+++ src/test/java/baseball/InputNumberValidateTest.java
@@ -0,0 +1,16 @@
+package baseball;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputNumberValidateTest {
+    @ParameterizedTest
+    @ValueSource(strings = {" ", "1", "12", "1234"})
+    void INVALID_INPUT_LENGTH_숫자를_3개만_입력(String input) {
+        Game game = new Game();
+        assertThatThrownBy(() -> game.validate(input))
+                .hasMessage(Game.INVALID_INPUT_LENGTH);
+    }
+}
