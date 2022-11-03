2c7be431ab12dbf5b2b37d755b840441cbe0e2a3
meunrud <y3a0522@gmail.com>
2022-11-04 01:53:13 +0900

    ✨게임 시작 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import java.io.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+
+    public void gameStartMessage (String userPickNumber){
+        Console console = System.console();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println("숫자를 입력 해 주세요.");
+    }
 }
+
