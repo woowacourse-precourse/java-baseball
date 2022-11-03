f8060763c2f1be0a6551c4ceb94588d1d82a7f84
yoonjoonseok <yjsygm@naver.com>
2022-11-03 19:37:09 +0900

    1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 생성 camp.nextstep.edu.missionutils.Randoms의 pickUniqueNumbersInRange를 사용)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
-public class Application {
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.*;
+
+public class Application{
     public static void main(String[] args) {
+    	int answer = makeRandomNumber();
+    	
+    	return;
         // TODO: 프로그램 구현
+    }
+    
+    public static int makeRandomNumber() {
+    	int result = 0;
+    	int cnt = 1;
+    	
+    	List<Integer> uniqueNumberList = new ArrayList<>();
+    	uniqueNumberList = Randoms.pickUniqueNumbersInRange(1,9,9);
+    	
+    	for(int i=0;i<uniqueNumberList.size();i++) {
+    		result += uniqueNumberList.get(i)*cnt;
+    		cnt*=10;
+    	}
+    	return result;
     }
 }
