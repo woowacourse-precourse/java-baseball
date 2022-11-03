60108d6e52d549b29c7e1f88d166cbf345076b5b
ksh-beginner <ohk9134@naver.com>
2022-11-03 20:48:17 +0900

    test(RandomUtilTest) : 랜덤 수 생성 잘 되는지 테스트
    RandomUtil.createRandomNumber()로 생성한 랜덤 수가 null이 아니면 테스트 통과

--- src/test/java/baseball/RandomUtilTest.java
+++ src/test/java/baseball/RandomUtilTest.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class RandomUtilTest {
 
+    @Test
+    void 랜덤_수_생성() {
+        Integer randomNumber = RandomUtil.createRandomNumber();
+        if (randomNumber == null) {
+            fail();
+        }
+    }
 }
