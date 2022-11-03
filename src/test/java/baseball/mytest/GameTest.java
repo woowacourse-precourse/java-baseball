b5b2e68e8aa8afab1b8111a2f0afe586e99001c3
Limjongho <whdgh9873@icloud.com>
2022-11-03 21:17:45 +0900

    refactor: 무작위 숫자 범위 수정 및 무관한 테스트 삭제

--- src/test/java/baseball/mytest/GameTest.java
+++ src/test/java/baseball/mytest/GameTest.java
@@ -12,24 +12,6 @@ import java.util.List;
 public class GameTest {
 
     @Test
-    void 게임_1회_진행_테스트() {
-        List<Integer> gameNumbers = Randoms.pickUniqueNumbersInRange(0, 9, 3);
-        GameNumber gameNumber = new GameNumber(gameNumbers);
-
-        List<Integer> userNums = Arrays.asList(1, 2, 3);
-        int balls= gameNumber.getBalls(userNums);
-        int strikes = gameNumber.getStrikes(userNums);
-
-        System.out.println("strikes = " + strikes);
-        System.out.println("balls = " + balls);
-
-
-        List<Integer> gameNums = gameNumber.getGameNums();
-        gameNums.forEach(System.out::println);
-        Assertions.assertThat(gameNums.size()).isEqualTo(3);
-    }
-
-    @Test
     void 낫싱_테스트1() {
         List<Integer> gameNumbers = Arrays.asList(1, 2, 3);
         GameNumber gameNumber = new GameNumber(gameNumbers);
--- src/test/java/baseball/mytest/RandomGameNumberTest.java
+++ src/test/java/baseball/mytest/RandomGameNumberTest.java
@@ -11,19 +11,19 @@ public class RandomGameNumberTest {
 
     @Test
     void 랜덤_숫자_테스트(){
-        List<Integer> randomNums = Randoms.pickUniqueNumbersInRange(0, 9, 3);
+        List<Integer> randomNums = Randoms.pickUniqueNumbersInRange(1, 9, 3);
 
     }
 
     @Test
     void 랜덤_숫자_객체_생성_테스트() {
-        List<Integer> randomNums = Randoms.pickUniqueNumbersInRange(0, 9, 3);
+        List<Integer> randomNums = Randoms.pickUniqueNumbersInRange(1, 9, 3);
         GameNumber gameNumber = new GameNumber(randomNums);
     }
 
     @Test
     void 게임_숫자_리스트_변경_불가_테스트() {
-        List<Integer> gameNums = Randoms.pickUniqueNumbersInRange(0,9,3);
+        List<Integer> gameNums = Randoms.pickUniqueNumbersInRange(1,9,3);
         GameNumber gameNumber = new GameNumber(gameNums);
 
 //        gameNumber.getGameNums().remove(0);
