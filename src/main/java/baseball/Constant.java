ffe7610416688cd2d3eebfa42bd425921b383bdb
Devtist <yt930310@snu.ac.kr>
2022-11-03 19:47:46 +0900

    feat: 프로그램에 쓰이는 상수들을 정리

---
+++ src/main/java/baseball/Constant.java
@@ -0,0 +1,8 @@
+package baseball;
+
+public abstract class Constant {
+	public static final int NUMBER_AMOUNT = 3;
+
+	public static final int RESTART_GAME = 1;
+	public static final int END_GAME = 2;
+}
