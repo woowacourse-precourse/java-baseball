acbf9f5a5f2a90be5ea85d1797bf98a396b79608
saturnone1 <saturnone1@naver.com>
2022-11-03 10:11:32 +0900

    feature($Application): check input baseball number
    Couple of fixed:
    - setAnswer : set strike, ball num
    - showResult : output ball, strike info
    - resetStrikeBall : reset strike,ball to zero for every input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
-import java.util.Collections;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -7,9 +8,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public List<Integer> numList = new ArrayList<>();
+    public int ball = 0;
+    public int strike = 0;
     public boolean answer = false;
-    public String inputNum = "";
+    public List<Integer> inputNum = new ArrayList<>();
+    public List<Integer> comNum = new ArrayList<>();
 
     public Application(){
         createNum();
@@ -17,27 +20,54 @@ public class Application {
     }
 
     public void createNum() {
-        while (numList.size() < 3) {
+        while (comNum.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!numList.contains(randomNumber)) {
-                numList.add(randomNumber);
+            if (!comNum.contains(randomNumber)) {
+                comNum.add(randomNumber);
             }
         }
     }
-    public void startGame(){
+    public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (!answer){
             getInputNum();
+            setAnswer();
+            showResult();
+            resetStrikeBall();
         }
     }
 
     public void getInputNum() {
         System.out.print("숫자를 입력해주세요 : ");
-        inputNum = Console.readLine();
+        String[] numChar = Console.readLine().split("");
+        inputNum = Arrays.stream(numChar).map(Integer::parseInt).collect(Collectors.toList());
     }
 
-    public List<Integer> getNumList() {
-        return numList;
+    public void setAnswer() {
+        for (int number: inputNum){
+            if (inputNum.indexOf(number) == comNum.indexOf(number)){
+                strike += 1;
+            } else if (comNum.contains(number)) {
+                ball += 1;
+            }
+        }
+    }
+
+    public void showResult() {
+        if (ball != 0 && strike != 0) {
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+        } else if (ball != 0) {
+            System.out.printf("%d볼\n", ball);
+        } else if (strike != 0) {
+            System.out.printf("%d스트라이크\n", ball);
+        } else {
+            System.out.print("낫싱\n");
+        }
+    }
+
+    public void resetStrikeBall() {
+        strike = 0;
+        ball = 0;
     }
 
     public static void main(String[] args) {
