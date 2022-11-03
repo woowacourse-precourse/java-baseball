cbc65e205ee15bba6d9810219c4f158cac5bbe1c
bomin1996 <bomin199628@gmail.com>
2022-11-03 20:45:52 +0900

    <feat> : 이용자의 숫자 입력 기능 변환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,23 @@ import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> ComputerNumber = CreatComputer();
+        System.out.println("computer = " + ComputerNumber);
+        List<Integer> UserNumber = playerNumber();
+        System.out.println("UserNumber = " + UserNumber);
+        // TODO: 프로그램 구현
+    }
+    public static List<Integer> playerNumber(){
+        System.out.println("숫자를 입력해주세요");
+        String UserNumber = Console.readLine();
+        List<Integer> playerNum = new ArrayList<>();
+        for(String number: UserNumber.split("")){
+            playerNum.add(Integer.parseInt(number));
+        }
+        return playerNum;
+    }//유저 넘버를 입력받고 List<Integer>타입으로 변환
+    public static List<Integer> CreatComputer(){
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -14,11 +30,6 @@ public class Application {
                 computer.add(randomNumber);
             }
         }//컴퓨터 랜덤 숫자 생성
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요 : ");
-        String userNumber = Console.readLine();
-        System.out.println("userNumber = " + userNumber);
-
-        // TODO: 프로그램 구현
+        return computer;
     }
 }
