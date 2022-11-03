784c514781a5e3c2bea66bb9b031455c6e8e961b
Laeng <Laeng@users.noreply.github.com>
2022-11-03 21:01:20 +0900

    feat(GameUtil): 더미값을 반환하는 메서드 추가

---
+++ src/main/java/baseball/utils/GameUtil.java
@@ -0,0 +1,18 @@
+package baseball.utils;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+public class GameUtil {
+    public static List<Integer> splitNumberToOneDigits(int number) {
+        return List.of(1,2,3);
+    }
+    
+    public static List<Integer> compareNumber(int computer, int player) {
+        return List.of(0, 0);
+    }
+
+    public static int generateNumber() {
+        return 123;
+    }
+}
