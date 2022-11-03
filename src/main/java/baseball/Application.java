6fc7337a84be3cd6c75972619178c9f0ec926425
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-11-03 16:12:19 +0900

    feat($main): 사용자의 입력값이 틀릴 경우 에러 처리 기능 구현
    checkIllegalInputForBaseballGame 구현
    -> 에러 케이스
    1. 3자리가 아닐 경우
    2. 값에 1-9 이외의 값이 포함되어있을 때
    3. 3자리 수 중 중복이 있을 때

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -44,4 +44,29 @@ public class Application {
 
         return numList;
     }
+    public static void checkIllegalInputForBaseballGame(int input){
+
+        // 1. 3자리가 아닐 경우
+        String inputStr = String.valueOf(input);
+        int lengthOfInput = inputStr.length();
+        if (lengthOfInput != 3)
+            throw new IllegalArgumentException("3자리의 숫자를 입력하십시오.");
+
+        List<Character> appearedNumList = new ArrayList<>();
+        for (int inputStringIndex = 0; inputStringIndex < lengthOfInput; inputStringIndex++){
+
+            // 2. 값에 1-9 이외의 값이 포함되어있을 때
+            char tmpInputStr = inputStr.charAt(inputStringIndex);
+            int ASCII_tmpInputStr = tmpInputStr;
+            if (ASCII_tmpInputStr < 49 && ASCII_tmpInputStr > 58)
+                // 1: 49, 9: 57
+                throw new IllegalArgumentException("1~9 사이의 숫자만 가능합니다.");
+
+            // 3. 3자리 수 중 중복이 있을 때
+            if (appearedNumList.contains(tmpInputStr))
+                throw new IllegalArgumentException("3자리의 숫자 중 중복되는 값이 존재해서는 안됩니다.");
+            else
+                appearedNumList.add(tmpInputStr);
+        }
+    }
 }
