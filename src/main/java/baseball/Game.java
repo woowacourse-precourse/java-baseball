6555288f7ea18dc0c9c755fa62e2f89b708205ed
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-03 15:59:14 +0900

    feat : 스트라이크 체크 기능 구현

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,15 @@
+package baseball;
+
+import java.util.Objects;
+
+public class Game {
+
+
+	public boolean isStrike(int computerNumber, int computerIdx, int userNumber, int userIdx) {
+		return Objects.equals(computerNumber, userNumber) && Objects.equals(computerIdx, userIdx);
+	}
+
+
+
+
+}
