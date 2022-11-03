672562dd7ce82ec880abfd1946f7b067ba57b492
egoing <dudqk9696@naver.com>
2022-11-04 00:15:49 +0900

    refactor : UserNumber domain 추가

---
+++ src/main/java/baseball/domain/UserNumber.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserNumber {
+    List<Integer> userNumber = new ArrayList<>();
+
+    public List<Integer> getUserNumber() { return userNumber; }
+    public void setUsernumber(List<Integer> comnumber) {
+        this.userNumber = comnumber;
+    }
+}
