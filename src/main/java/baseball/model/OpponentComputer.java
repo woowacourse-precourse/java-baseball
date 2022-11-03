5416e1edc86205c77c5e87c9d9016d5d5ada78a8
ksh-beginner <ohk9134@naver.com>
2022-11-03 17:00:00 +0900

    feat(OpponentComputer) : 상대방 생성 시 스트라이크, 볼 카운트 맵 초기화 구현

--- src/main/java/baseball/model/OpponentComputer.java
+++ src/main/java/baseball/model/OpponentComputer.java
@@ -1,11 +1,17 @@
 package baseball.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class OpponentComputer {
 
     private int number;
+    private final Map<String, Integer> strikeAndBallCountMap = new HashMap<>();
 
     public OpponentComputer(int randomNumber) {
         this.number = randomNumber;
+        strikeAndBallCountMap.put("Strike", 0);
+        strikeAndBallCountMap.put("Ball", 0);
     }
 
     public int getNumber() {
