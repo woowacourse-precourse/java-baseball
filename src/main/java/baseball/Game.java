17fd910fb08d973e705563389564b210759d8941
jiwon1217 <koagjw@gmail.com>
2022-11-03 22:48:24 +0900

    feat(Game.java): 사용자의 숫자 입력

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,5 +22,9 @@ public class Game {
         if (!numbers.contains(number)) {
             numbers.add(number);
         }
+    }
+
+    public void input() {
+        String inputNumbers = Console.readLine();
     }
 }
