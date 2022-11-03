cd48b9dc70f29c3f20c9d75b87d5aa596ea67f0c
sangwon <swchoi1997@naver.com>
2022-11-03 22:04:33 +0900

    feat: value에 해당하는 getter 추가

--- src/main/java/baseball/domain/model/BallCount.java
+++ src/main/java/baseball/domain/model/BallCount.java
@@ -2,15 +2,19 @@ package baseball.domain.model;
 
 public enum BallCount {
 
-	OUT(0,"낫싱"),
-	BALL(1, "볼"),
-	STRIKE(2, "스트라이크");
+    OUT(0, "아웃"),
+    BALL(1, "볼"),
+    STRIKE(2, "스트라이크");
 
-	private final int key;
-	private final String value;
+    private final int code;
+    private final String value;
 
-	BallCount(final int key, final String value) {
-		this.key = key;
-		this.value = value;
-	}
+    BallCount(final int code, final String value) {
+        this.code = code;
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
