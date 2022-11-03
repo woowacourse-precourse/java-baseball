b9e2fb3f9c8a2c42d909ae2a47c6405b1cb644fe
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-03 22:24:16 +0900

    clean: 오타 수정

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -227,7 +227,7 @@ class ApplicationTest extends NsTest {
                 run("246", "2");
                 assertThat(output()).contains("3스트라이크", "3개의 숫자를 모두 맞히셨습니다! 게임 종료"
                 ,"게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", "게임을 종료합니다.");
-            }, 2, 4, 6, 2);
+            }, 2, 4, 6);
         }
 
         @Test
