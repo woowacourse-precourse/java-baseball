b10216a581a486381e1a8032eb283bb1fae0c121
ywj9811 <ywj9811@naver.com>
2022-11-04 02:32:46 +0900

    feat: ResultService 클래스 수정
    ResultService 클래스에서 입력값 받는 부분을 InputValue 클래스를 통해 대체함

--- src/main/java/baseball/service/ResultService.java
+++ src/main/java/baseball/service/ResultService.java
@@ -1,5 +1,6 @@
 package baseball.service;
 
+import baseball.domain.InputValue;
 import baseball.exception.InputException;
 
 import java.util.ArrayList;
@@ -12,20 +13,14 @@ public class ResultService {
     public int ReOrClose(List<Integer> RandomNumber) {
         RuleService rules = new RuleService();
         InputException validation = new InputException();
+        InputValue inputValue = new InputValue();
 
         int strike = 0;
         int ball;
         int want;
 
-        String inputNum;
-
         while (strike != 3) {
-            System.out.print("숫자를 입력해 주세요 : ");
-            inputNum = readLine();
-            validation.isNumber(inputNum);
-
-            List<Integer> inputNumber = returnList(inputNum);
-            System.out.println("input : " + inputNumber);
+            List<Integer> inputNumber = inputValue.inputNum();
 
             validation.InputValid(RandomNumber, inputNumber);
 
@@ -46,14 +41,5 @@ public class ResultService {
         System.out.println("다시 하시려면 1번 종료하시려면 2번을 눌러주세요");
         want = Integer.parseInt(readLine());
         return want;
-    }
-
-    private List<Integer> returnList(String inputNum) {
-        List<Integer> list = new ArrayList<>();
-        for (int idx = 0; idx < inputNum.length(); idx++) {
-            list.add(Integer.parseInt(
-                    String.valueOf(inputNum.charAt(idx))));
-        }
-        return list;
     }
 }
