64c1bf07187d035a06cb7c63cf02d405a35e9d37
ksh-beginner <ohk9134@naver.com>
2022-11-03 14:56:50 +0900

    test(OpponentComputerTest) : 상대방 생성과 동시에 랜덤 수 저장하는 테스트 추가
    OpponentComputer의 생성자로 랜덤 수를 넘겨서 생성 후
    생성한 OpponentComputer가 null이 아니라면 테스트 통과

--- src/test/java/baseball/model/OpponentComputerTest.java
+++ src/test/java/baseball/model/OpponentComputerTest.java
@@ -1,5 +1,18 @@
 package baseball.model;
 
+import baseball.RandomUtil;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class OpponentComputerTest {
+
+    @Test
+    void 상대방_생성과_동시에_랜덤_수_저장() {
+        OpponentComputer opponentComputer = new OpponentComputer(RandomUtil.createRandomNumber());
+        if (opponentComputer == null) {
+            fail();
+        }
+    }
 
 }
