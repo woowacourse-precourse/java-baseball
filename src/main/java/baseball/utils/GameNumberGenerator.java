74a93cda8a5778a32fbea26ba0a7459472567faa
JHS <wnghdtjr129@naver.com>
2022-11-03 10:37:30 +0900

    refactor: GameNumber 자료구조 ArrayList로 변경

--- src/main/java/baseball/utils/GameNumberGenerator.java
+++ src/main/java/baseball/utils/GameNumberGenerator.java
@@ -2,17 +2,18 @@ package baseball.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 import static baseball.resources.GameConfig.*;
 
 public class GameNumberGenerator {
-    public static Set<Integer> generate(){
+    public static ArrayList<Integer> generate(){
         Set<Integer> gameNumber = new HashSet<>();
         while (gameNumber.size() < SIZE) {
             gameNumber.add(Randoms.pickNumberInRange(START_NUMBER, END_NUMBER));
         }
-        return gameNumber;
+        return new ArrayList<>(gameNumber);
     }
 }
--- src/test/java/baseball/utils/GameNumberGeneratorTest.java
+++ src/test/java/baseball/utils/GameNumberGeneratorTest.java
@@ -3,7 +3,7 @@ package baseball.utils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Set;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -12,7 +12,7 @@ class GameNumberGeneratorTest {
     @Test
     @DisplayName("서로다른 3자리 자연수 생성")
     void createGameNumber(){
-        Set<Integer> gameNumber = GameNumberGenerator.generate();
+        List<Integer> gameNumber = GameNumberGenerator.generate();
         assertThat(gameNumber.size()).isEqualTo(3);
         assertThat(gameNumber).doesNotHaveDuplicates();
     }
