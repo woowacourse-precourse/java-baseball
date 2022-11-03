26c6bb59804f778c81f8f8fe7e5746ee14996c31
euna <christinejung10@daum.net>
2022-11-03 22:41:32 +0900

    fix(Application): set the entire flow of the program

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        BullsAndCows game = new BullsAndCows();
+        do {
+            game.playGame();
+        } while ((doRepeat()) == 1);
     }
+
+    private static int doRepeat() {
+        int input = 0;
+        // "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+        return input;
+    }
+
 }
