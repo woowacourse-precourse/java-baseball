0b014ff07f274d3d1d84a83d0c8fb09fe433acb8
dooboocookie <bkh3030@gmail.com>
2022-11-03 13:06:23 +0900

    test: InputTest.java 추가
    입력 값에 대한 검증 로직 테스트 및 값 테스트

--- src/test/java/baseball/InputTest.java
+++ src/test/java/baseball/InputTest.java
@@ -1,2 +1,47 @@
-package baseball;public class InputTest {
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputTest {
+
+    @Test
+    void inputValueTest() throws Exception {
+
+        int input = 123;
+        int[] inputs = {1,2,3};
+
+        Numbers numbers = new Numbers(123);
+
+        for (int index = 0; index < 3; index++) {
+            assertThat(numbers.findNumber(index))
+                    .isEqualTo(new Number(inputs[index]));
+        }
+    }
+
+
+    @Test
+    void inputExceptionTest() throws Exception {
+
+        int input1 = 123;
+        // 예외 발생 케이스
+        int input2 = 1234;
+        int input3 = 12;
+        int input4 = 100;
+
+        assertThat(new Numbers(input1))
+                .isInstanceOf(Numbers.class);
+        // 예외 발생 테스트
+        assertThatThrownBy(() -> new Numbers(input2))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Numbers(input3))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> new Numbers(input4))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
 }
