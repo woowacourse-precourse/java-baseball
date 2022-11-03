b54d30b27349a13d7bafc3f2d8af10506c7a2781
ywj9811 <ywj9811@naver.com>
2022-11-04 02:34:07 +0900

    feat: GameStartController 수정
    GameStartController 클래스에서 랜덤값 생성하는 부분 RandomValue 클래스를 이용하여 대체

--- src/main/java/baseball/controller/GameStartController.java
+++ src/main/java/baseball/controller/GameStartController.java
@@ -1,32 +1,21 @@
 package baseball.controller;
 
+import baseball.domain.RandomValue;
 import baseball.service.ResultService;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class GameStartController {
     public void GameStart() {
         ResultService result = new ResultService();
-
+        RandomValue randomValue = new RandomValue();
         int Restart = 1;
+
         while (Restart == 1) {
-            List<Integer> RandomNumber = GetRandomNum();
+            List<Integer> RandomNumber = randomValue.randomValue();
             System.out.println("숫자 야구 게임을 시작하겠습니다.");
             Restart = result.ReOrClose(RandomNumber);
         }
-        System.out.println("종료 되었습니다.");
-    }
 
-    private List<Integer> GetRandomNum() {
-        List<Integer> GetNum = new ArrayList<>();
-        while (GetNum.size() < 3) {
-            int rdNum = Randoms.pickNumberInRange(1, 9);
-            if (!GetNum.contains(rdNum)) {
-                GetNum.add(rdNum);
-            }
-        }
-        return GetNum;
+        System.out.println("종료 되었습니다.");
     }
 }
