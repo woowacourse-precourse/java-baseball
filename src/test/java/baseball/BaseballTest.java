33cc6838038af099fe52b0c656c7425405a42999
recicoco068 <gkapf838@gmail.com>
2022-11-03 19:06:24 +0900

    test : 볼 개수 구하는 기능 테스트

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -8,13 +8,22 @@ import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class BaseballTest {
-
     @Test
     void 랜덤_숫자_확인() {
         List<Integer> number = Baseball.getRandomNumber();
         Set<Integer> check = new HashSet<>(number);
         assertThat(number.size()).isEqualTo(3);
         assertThat(check.size()).isEqualTo(3);
-        System.out.println(number);
+    }
+
+    @Test
+    void 볼_개수_확인() {
+        List<Integer> computer1 = List.of(1, 2, 3);
+        List<Integer> user1 = List.of(3, 8, 2);
+        assertThat(Baseball.getBallCount(computer1, user1)).isEqualTo(2);
+
+        List<Integer> computer2 = List.of(1, 2, 3);
+        List<Integer> user2 = List.of(9, 8, 7);
+        assertThat(Baseball.getBallCount(computer2, user2)).isEqualTo(0);
     }
 }
