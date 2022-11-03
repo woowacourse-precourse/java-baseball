bcfc747c25c6028197da92e7df108e9baaab4022
seokjin8678 <rnrn8678@naver.com>
2022-11-03 22:40:01 +0900

    feat: 컴퓨터와 사용자의 숫자를 비교하여 strike, ball 갯수를 계산하는 기능 추가

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,5 +1,8 @@
 package baseball.game;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Game {
 	private User user;
 	private Computer computer;
@@ -17,5 +20,22 @@ public class Game {
 	public void start() {
 		computer.generateComputerNumbers();
 		user.receiveUserInput();
+
+		List<Integer> computerNumbers = computer.getNumbers();
+		List<Integer> userNumbers = user.getNumbers();
+
+		int strike = 0;
+		int ball = 0;
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				if (Objects.equals(computerNumbers.get(i), userNumbers.get(j))) {
+					ball++;
+				}
+			}
+			if (Objects.equals(computerNumbers.get(i), userNumbers.get(i))) {
+				strike++;
+				ball--;
+			}
+		}
 	}
 }
