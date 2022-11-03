644868478d4015f84a423e55156112793d31cb74
EUNJEONGMUN <munej26@gmail.com>
2022-11-03 13:10:40 +0900

    feat: 사용자에게 입력받은 값 검증하기 - 모두 숫자인지 검증

--- src/main/java/baseball/UserInputNumbers.java
+++ src/main/java/baseball/UserInputNumbers.java
@@ -2,6 +2,7 @@ package baseball;
 
 public class UserInputNumbers {
     static final int NUMBER_LENGTH = 3;
+    static final int START_IDX = 0;
 
     private int first;
     private int second;
@@ -18,6 +19,18 @@ public class UserInputNumbers {
     public static boolean validUserInputNumber(String numbers) {
         if (numbers.length()!=NUMBER_LENGTH) {
             throw new IllegalArgumentException();
+        }
+        if (!isDigit(numbers)) {
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
+    public static boolean isDigit(String num) {
+        for (int idx=START_IDX; idx<num.length(); idx++) {
+            if (!Character.isDigit(num.charAt(idx))) {
+                return false;
+            }
         }
         return true;
     }
