a5d0e7e856eafd411b7ac0a175a1d0bb5e9fd1da
UngHeung <t01047377477@gmail.com>
2022-11-04 01:09:52 +0900

    feat : (baseball) 컴퓨터 숫자 랜덤 선택 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+	}
+
+	private static List<Integer> batterNumberList() {
+
+		List<Integer> computerSelectList = new ArrayList<>();
+
+		while(computerSelectList.size() < 3) {
+
+			int computerSelect = Randoms.pickNumberInRange(1, 9);
+
+			if(!computerSelectList.contains(computerSelect)) {
+				computerSelectList.add(computerSelect);
+			}
+
+		}
+
+		return computerSelectList;
+	}
 }
