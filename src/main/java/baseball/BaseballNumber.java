d286e11b73e09ab303b44f01ea9aec9776abe6e6
Joonsoo Kim <kjsoo1901@gmail.com>
2022-11-03 16:59:12 +0900

    feat: Add setNumbersbyRandom in BaseballNumber

--- src/main/java/baseball/BaseballNumber.java
+++ src/main/java/baseball/BaseballNumber.java
@@ -6,7 +6,16 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 
 public class BaseballNumber {
-    ArrayList<Integer> numbers;
+    ArrayList<Integer> numbers = new ArrayList<>();
 
+    public void setNumbersByRandom(){
+        numbers.clear();
+        while(numbers.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!numbers.contains(randomNumber)){
+                numbers.add(randomNumber);
+            }
+        }
+    }
 
 }
