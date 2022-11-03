481b252814d8494c28456ad80463d5332617cfd7
JeonghakLee <wjdgkr0527@naver.com>
2022-11-03 17:04:54 +0900

    feat: 컴퓨터 숫자와 사용자 숫자를 비교해 볼, 스트라이크를 구하는 기능 구현

---
+++ src/main/java/baseball/service/HintService.java
@@ -0,0 +1,44 @@
+package baseball.service;
+
+import java.util.List;
+
+public class HintService {
+
+  private final int strikeCount;
+  private final int ballCount;
+
+  public HintService(List<Integer> computerNumbers, List<Integer> playerNumbers) {
+    this.strikeCount = getStrikeCount(computerNumbers, playerNumbers);
+    this.ballCount = getBallCount(computerNumbers, playerNumbers);
+  }
+
+  int getStrikeCount(List<Integer> computerNumbers, List<Integer> playerNumbers) {
+    int strikeCount = 0;
+    for (int i = 0; i < computerNumbers.size(); i++) {
+      if (computerNumbers.get(i).equals(playerNumbers.get(i))) {
+        strikeCount++;
+      }
+    }
+    return strikeCount;
+  }
+  private int getBallCount(List<Integer> computerNumbers, List<Integer> playerNumbers) {
+    int ballCount = 0;
+    for (int i = 0; i < computerNumbers.size(); i++) {
+      if (hasBall(computerNumbers, playerNumbers, i)) {
+        ballCount += 1;
+      }
+    }
+    return ballCount;
+  }
+
+  private boolean hasBall(List<Integer> computerNumbers, List<Integer> playerNumbers,
+      int i) {
+    for (int j = 0; j < playerNumbers.size(); j++) {
+      if (i != j && computerNumbers.get(i).equals(playerNumbers.get(j))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+}
