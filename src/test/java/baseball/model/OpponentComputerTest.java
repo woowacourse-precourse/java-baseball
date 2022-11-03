1911461bcef7e955423f7a189dd37bf9acfa524e
ksh-beginner <ohk9134@naver.com>
2022-11-03 19:45:26 +0900

    test(OpponentComputerTest) : 상대방 수 리스트와 플레이어 수 리스트 비교해서 스트라이크, 볼 개수 판정 - 1볼 1스트라이크

--- src/test/java/baseball/model/OpponentComputerTest.java
+++ src/test/java/baseball/model/OpponentComputerTest.java
@@ -112,4 +112,15 @@ public class OpponentComputerTest {
         assertThat(opponentComputer.getStrikeAndBallCountMap().get("Strike")).isEqualTo(2);
         assertThat(opponentComputer.getStrikeAndBallCountMap().get("Ball")).isEqualTo(0);
     }
+
+    @Test
+    void 상대방_수_리스트와_플레이어_수_리스트를_비교해서_스트라이크_볼_개수_판정_1볼_1스트라이크() {
+        OpponentComputer opponentComputer = new OpponentComputer(123);
+        List<String> opponentComputerNumberEachDigits = List.of("1", "2", "3");
+        List<String> playerNumberEachDigits = List.of("1", "3", "6");
+        opponentComputer.judgeStrikeOrBall(opponentComputerNumberEachDigits, playerNumberEachDigits);
+
+        assertThat(opponentComputer.getStrikeAndBallCountMap().get("Strike")).isEqualTo(1);
+        assertThat(opponentComputer.getStrikeAndBallCountMap().get("Ball")).isEqualTo(1);
+    }
 }
