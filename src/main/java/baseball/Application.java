fec5fa9fe8cd4784cce7a95f6379d4d4bd87a23d
eunji53 <duqtj00@naver.com>
2022-11-03 13:34:53 +0900

    <feat>:랜덤 숫자 생성
    1~9자연수,중복X,세자리수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,17 @@
 package baseball;
-
+import java.util.*;
+import camp.nextstep.edu.missionutils.*;
 public class Application {
+    public static List<Integer> getRandomNum() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
