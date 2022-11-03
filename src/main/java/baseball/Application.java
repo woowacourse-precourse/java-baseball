fc653bb825a7c043491d597c221a371b34a2a10a
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-03 21:55:28 +0900

    feat: 숫자 범위 판정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,8 @@ import java.util.Set;
 
 public class Application {
     private static final int CNT_NUMBER = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -16,12 +18,22 @@ public class Application {
 
     private static void isValidInput(String input) {
         isDuplicated(input);
+        input.chars().forEach(c -> isDigitRange((char) c));
     }
 
     private static void isDuplicated(String str) {
         Set<Character> stringCharSet = new HashSet<>();
         str.chars().forEach(c -> stringCharSet.add((char) c));
         if (stringCharSet.size() != CNT_NUMBER) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void isDigitRange(Character charSingleDigit) {
+        System.out.printf(String.valueOf(charSingleDigit));
+        if (charSingleDigit < '0' + MIN_NUMBER) {
+            throw new IllegalArgumentException();
+        } else if (charSingleDigit > '0' + MAX_NUMBER) {
             throw new IllegalArgumentException();
         }
     }
