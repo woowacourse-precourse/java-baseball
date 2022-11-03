2781c058f316b402576f7126e6946f99d0dc14f4
eyeofsol <kingjihong@naver.com>
2022-11-03 18:38:51 +0900

    feat(Application): 게임 진행하는 기능 구현
    while문을 이용해 새로운 Game 객체를 사용해 정답을 맞출 때 까지 진행하는 기능을 구현했습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,28 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input;
+        int flag = 1;
+
+        while (flag == 1) {
+            doGame();
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            // flag = continueOrEnd();
+        }
+    }
+
+    public static void doGame() {
+        String input = "";
+        Game game = new Game();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (!game.isEnd(input)) {
+            input = Console.readLine();
+            if (InputCheck.isInputError(input)) {
+                throw new IllegalArgumentException("입력오류");
+            }
+            game.play(input);
+        }
     }
 }
