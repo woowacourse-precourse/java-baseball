c81b0ecf94550eebc292a390058dedca937a9257
tjwjdgus12 <01048733438a@gmail.com>
2022-11-03 17:12:10 +0900

    feat: inputGuess 기능 추가
    유저로부터 추측을 입력받고, 올바르지 않은 입력의 경우 예외 throw

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,11 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-
+        play();
     }
 
     public static String generateAnswer() {
@@ -25,5 +26,21 @@ public class Application {
         return numbers.charAt(0) != numbers.charAt(1)
                 && numbers.charAt(1) != numbers.charAt(2)
                 && numbers.charAt(2) != numbers.charAt(0);
+    }
+
+    public static String inputGuess() {
+        String guess = Console.readLine();
+        if (!isValid(guess)) {
+            throw new IllegalArgumentException();
+        }
+        return guess;
+    }
+
+    public static void play() {
+        String answer = generateAnswer();
+        String guess;
+        do {
+            guess = inputGuess();
+        } while (!guess.equals(answer));
     }
 }
