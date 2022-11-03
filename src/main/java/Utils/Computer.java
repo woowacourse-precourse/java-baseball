bc7724242b5e194dc7a7266dd312f4c97749dce4
glass <glass7312@gmail.com>
2022-11-03 20:01:52 +0900

    getThreeNumber():Computer클래스의 threeNumber의 값을 하나의 스트링문자열로 반환하는 기능

--- src/main/java/Utils/Computer.java
+++ src/main/java/Utils/Computer.java
@@ -4,4 +4,14 @@ import java.util.List;
 
 public class Computer {
     public static List<Integer> threeNumber_List;
+    public static void setThreeNumber_List(){
+        Computer.threeNumber_List=game.anotherThree_RandomNumber();
+    }
+    public static String getThreeNumber(){
+        String threeNum="";
+        for (Integer item : Computer.threeNumber_List){
+            threeNum.concat(String.valueOf(item));
+        }
+        return threeNum;
+    }
 }
