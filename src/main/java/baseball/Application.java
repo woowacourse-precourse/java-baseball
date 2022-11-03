37364c5b276077cc77bd61cb20f152a9a78b4fa0
kyo705 <mission0705@naver.com>
2022-11-04 02:28:18 +0900

    [REFACTOR] 랜덤 값 생성 기능
    랜덤 값 생성 기능을 세분화 및 모듈화하여 RandomNumber 클래스로 만듬

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,34 +1,8 @@
 package baseball;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-    }
-    
-    public static List<Integer> createRamdomNumbers(int startNumber, int endNumber, int size) {
-    	
-    	Set<Integer> randomNumberSet = new HashSet<>(size);
-    	
-    	while(true) {
-    		int randomNumber = Randoms.pickNumberInRange(startNumber, endNumber);
-    		randomNumberSet.add(randomNumber);
-    		
-    		if(randomNumberSet.size()==size) {
-    			break;
-    		}
-    	}
-    	
-    	List<Integer> randomNumberList = new ArrayList<>(size);
-    	randomNumberList.addAll(randomNumberSet);
-    	
-    	return randomNumberList;
     }
     
 }
---
+++ src/main/java/baseball/RandomNumber.java
@@ -0,0 +1,66 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumber {
+
+	public List<Integer> createRamdomNumbers(int startNumber, int endNumber, int size) {
+    	validateRange(startNumber, endNumber);
+    	validateCount(startNumber, endNumber, size);
+    	
+    	return createRandomNumberList(startNumber, endNumber, size);
+    }
+    
+    
+    private Set<Integer> createRandomNumberSet(int start, int end, int size){
+    	Set<Integer> randomNumberSet = new HashSet<>(size);
+    	
+    	while(true) {
+    		int randomNumber = Randoms.pickNumberInRange(start, end);
+    		randomNumberSet.add(randomNumber);
+    		
+    		if(randomNumberSet.size()==size) {
+    			break;
+    		}
+    	}
+    	return randomNumberSet;
+    }
+    
+    
+    private List<Integer> createRandomNumberList(int start, int end, int size){
+    	Set<Integer> randomNumberSet = createRandomNumberSet(start, end, size);
+    	
+    	List<Integer> randomNumberList = new ArrayList<>(size);
+    	randomNumberList.addAll(randomNumberSet);
+    	
+    	return randomNumberList;
+    }
+    
+    
+    private void validateRange(final int start, final int end) {
+        if (start > end) {
+            throw new IllegalArgumentException("startInclusive cannot be greater than endInclusive.");
+        }
+        if (end == Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("endInclusive cannot be greater than Integer.MAX_VALUE.");
+        }
+        if (end - start >= Integer.MAX_VALUE) {
+            throw new IllegalArgumentException("the input range is too large.");
+        }
+    }
+    
+    
+    private void validateCount(final int start, final int end, final int count) {
+        if (count < 0) {
+            throw new IllegalArgumentException("count cannot be less than zero.");
+        }
+        if (end - start + 1 < count) {
+            throw new IllegalArgumentException("count cannot be greater than the input range.");
+        }
+    }
+}
