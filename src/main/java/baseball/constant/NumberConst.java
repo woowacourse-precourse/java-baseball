6ee926b7d345e2269451a824b3e56d92beea1447
jiwon <ljwon77@daum.net>
2022-11-04 00:42:40 +0900

    feat: add constant class about number, message

---
+++ src/main/java/baseball/constant/NumberConst.java
@@ -0,0 +1,13 @@
+package baseball.constant;
+
+/**
+ * 숫자 관련 상수 관리 클래스
+ */
+public class NumberConst {
+
+    private final Integer RESTART_NUM = 1;
+
+    private final Integer EXIT_NUM = 2;
+
+    private final Integer INPUT_LENGTH = 3;
+}
