27138edb13f43d158664b6d8e756b5712d00cc85
kimminsu <xjfcnfw3@naver.com>
2022-11-03 11:51:15 +0900

    feat: user 숫자 입력 구현

---
+++ src/main/java/baseball/user/User.java
@@ -0,0 +1,26 @@
+package baseball.user;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class User {
+    private List<Integer> gameNumber;
+
+    public User() {
+        gameNumber = new ArrayList<>();
+    }
+
+    public List<Integer> getGameNumber() {
+        return gameNumber;
+    }
+
+    public void inputNumber() {
+        gameNumber.clear();
+        String userInput = Console.readLine();
+        Stream<String> userStream = Stream.of(userInput.split(""));
+        gameNumber = userStream.mapToInt(Integer::valueOf).boxed().collect(Collectors.toList());
+    }
+}
