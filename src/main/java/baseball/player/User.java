cfea7ad82449bfce0ec68d2818a0c2eccbf85635
myungsikyoon <yms8795@gmail.com>
2022-11-03 16:29:35 +0900

    feat(User) : generateNumberList 구현.

--- src/main/java/baseball/player/User.java
+++ src/main/java/baseball/player/User.java
@@ -6,8 +6,26 @@ public class User {
 
     private List<Integer> userNumberList;
 
-    User(String inputNumber){
+    User(String inputNumber) {
         this.userNumberList = generateNumberList(inputNumber);
+    }
+
+    private List<Integer> generateNumberList(String inputNumber) {
+
+        boolean exceptionCheck = false;
+
+        if (isNotNumber(inputNumber)) {
+            throw new IllegalArgumentException();
+        }
+
+        List<Integer> numberList = changeStringToInteger(inputNumber);
+
+        if (isNotThreeLength(numberList) || isDuplication(numberList)) {
+            throw new IllegalArgumentException();
+        }
+
+        return numberList;
+
     }
 
 
