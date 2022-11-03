aa67468e317f524c2d3a20a6016d034bc5d22bf1
callusk0 <callusk00@gmail.com>
2022-11-04 01:53:12 +0900

    feat: Game 비교 함수 구현
    비교해서 strike, ball을 증가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -12,18 +13,47 @@ public class Game {
 
     public void init() {
         computer_answer = new ArrayList<>();
+
         while (computer_answer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer_answer.contains(randomNumber)) {
                 computer_answer.add(randomNumber);
             }
         }
+
         currentBall = 0;
         currentStrike = 0;
     }
 
     public void compare_player_input_and_answer(int playerNum) {
+        currentBall = 0;
+        currentStrike = 0;
+        List<Integer> player_list = new ArrayList<>();
+        int_to_list(playerNum, player_list);
+        compare_list_and_answer(player_list);
+    }
 
+    private void compare_list_and_answer(List<Integer> player_list) {
+        for(int i =0;i<3;i++)
+        {
+            if(player_list.get(i) == computer_answer.get(i))
+                currentStrike++;
+            
+            else if(computer_answer.contains(player_list.get(i)))
+                currentBall++;
+            
+        }
+    }
+
+    private void int_to_list(int playerNum, List<Integer> player_list) {
+        int num = playerNum;
+
+        while(num > 0) {
+            player_list.add(num %10);
+            num /= 10;
+        }
+
+        Collections.reverse(player_list);
     }
 
     public int get_strike() {
