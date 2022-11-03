c13fcf278051daad0f92a516ed3494d4ac565a5e
whdcks420 <whdcks420@gmail.com>
2022-11-03 23:16:11 +0900

    test: valiadate 기능 test

--- src/test/java/baseball/validator/NumberValidatorTest.java
+++ src/test/java/baseball/validator/NumberValidatorTest.java
@@ -2,25 +2,51 @@ package baseball.validator;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class NumberValidatorTest {
 
     @Test
     void 숫자가_아닌_문자_입력_예외() {
-        String userNumber = "bellCold";
-        assertThrows(IllegalArgumentException.class, () -> NumberValidator.validate(userNumber));
+        assertThatThrownBy(() -> NumberValidator.validate("bellCold"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 중복된_숫자_입력시_예외() {
-        String userNumber = "777";
-        assertThrows(IllegalArgumentException.class, () -> NumberValidator.validate(userNumber));
+        assertThatThrownBy(() -> NumberValidator.validate("772"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 세자리가_아닌_숫자_입력시_예외() {
-        String userNumber = "12";
-        assertThrows(IllegalArgumentException.class, () -> NumberValidator.validate(userNumber));
+        assertThatThrownBy(() -> NumberValidator.validate("12"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 문자_입력시_에러_메시지() {
+        Throwable exception = assertThrows(
+                IllegalArgumentException.class, () -> NumberValidator.validate("bellCold")
+        );
+        assertEquals("Only numbers can be entered.", exception.getMessage());
+    }
+
+    @Test
+    void 중복된_숫자_입력_에러_메시지() {
+        Throwable exception = assertThrows(
+                IllegalArgumentException.class, () -> NumberValidator.validate("112")
+        );
+        assertEquals("Duplicate numbers cannot be entered.", exception.getMessage());
+    }
+
+    @Test
+    void 세자리수가_아닌_숫자_입력_에러_메시지() {
+        Throwable exception = assertThrows(
+                IllegalArgumentException.class, () -> NumberValidator.validate("12")
+        );
+        assertEquals("It's not a three-digit number.", exception.getMessage());
     }
 }
