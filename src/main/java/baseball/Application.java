f47d4dc35c54c3331d0c1723d00d36cd9beff30d
jjongwa <troas96@naver.com>
2022-11-04 01:28:02 +0900

    feat: 컴퓨터의 3자리 랜덤 값 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -22,7 +22,12 @@ public class Application {
 
     static List<Integer> pickComputerNum(){
         List<Integer> computer = new ArrayList<>();
-
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
         return computer;
     }
 
