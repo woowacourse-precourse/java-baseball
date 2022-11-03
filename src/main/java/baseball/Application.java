1b6f0bc59f8905043a9e6cc3dff5bae1abafbcc2
Konahyeon <koo@inha.edu>
2022-11-03 15:31:20 +0900

    feat: player 세 자리 수 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -15,6 +16,20 @@ public class Application {
             }
         }
         return computer;
+    }
+
+    public static List<Integer> playerNumber() {
+        List<Integer> player = new ArrayList<>();
+        String input = Console.readLine();
+
+        while (player.size() < 3) {
+            int idx = 0;
+            char inputNumber = input.charAt(idx);
+            int inputNumberToInt = inputNumber - '0';
+            player.add(inputNumberToInt);
+            idx++;
+        }
+        return player;
     }
 
     public static void main(String[] args) {
