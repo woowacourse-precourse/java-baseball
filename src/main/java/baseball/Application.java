79e23f35444b9fef9a64b60865cb2c13c0d16d24
12novel30 <79260472+12novel30@users.noreply.github.com>
2022-11-03 15:19:51 +0900

    feat($main): 컴퓨터가 임의로 선택한 3자리 수 list로 저장
    createComputerLength3IntegerAnswerToList 구현
    - 이후 작업에서의 값 비교를 위해 List로 저장
    - 3자리수, 1~9 사이의 숫자만 가능하도록 제한
    - 중복 제외

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static List<Integer> createComputerLength3IntegerAnswerToList(){
+        List<Integer> computerAnswer = new ArrayList<>();
+        while (computerAnswer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerAnswer.contains(randomNumber)) {
+                computerAnswer.add(randomNumber);
+            }
+        }
+
+        return computerAnswer;
     }
 }
