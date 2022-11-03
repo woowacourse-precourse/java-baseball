4816bd0d25ec04bb4257bb1f3f59671c0da587f1
HwangSeokJin <ghkd3703@gmail.com>
2022-11-04 01:59:20 +0900

    fix: 게임 종료 문구 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -34,6 +34,7 @@ public class BaseballGame {
                 throw exception;
             }
         }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     public boolean restart() {
