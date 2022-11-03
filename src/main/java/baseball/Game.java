9b0deded1f85520561c36f04c8ec958c7052c29e
choiejae <ejae8319@gmail.com>
2022-11-04 02:20:13 +0900

    test: Game 생성 테스트

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,2 +1,11 @@
-package baseball;public class Game {
+package baseball;
+
+public class Game {
+  public static int toBeContinued = 1;
+  public Game() {
+    this(toBeContinued);
+  }
+  public Game(int toBeContinued) {
+    this.toBeContinued = toBeContinued;
+  }
 }
--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -2,11 +2,22 @@ package baseball;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.assertj.core.api.Assertions;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
-  @Test
-  public void nothing(){
+  Game game;
 
+  @BeforeEach
+  public void setUp(){
+    game = new Game(1);
+  }
+
+  @Test
+  public void isGameStartWhenFlagIs1(){
+    game = new Game(1);
+    Assertions.assertThat(game.toBeContinued).isEqualTo(1);
   }
 }
