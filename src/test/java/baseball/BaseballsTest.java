8944e7e568fa484b363f0152e161911d957888da
situodys <situodys@gmail.com>
2022-11-04 01:47:30 +0900

    test(Baseballs): 입력의 길이가 3이 아닌 경우 테스트

--- src/test/java/baseball/BaseballsTest.java
+++ src/test/java/baseball/BaseballsTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class BaseballsTest {
 
@@ -12,6 +14,19 @@ public class BaseballsTest {
     public void test1() throws Exception{
         //given
         String userInput = "";
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            Baseballs.of(userInput);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "12", "1234", "123456789", "0123456789"," 2"})
+    @DisplayName("입력의 길이가 3이 아닌 경우 IllegalArgumentException 발생")
+    public void test2(String input) throws Exception {
+        //given
+        String userInput = input;
         //when
         //then
         assertThatThrownBy(() -> {
