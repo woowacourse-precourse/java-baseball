813527ea4aa73c7b0a786650f40fcb7f3c7892af
akskflwn <akskflwn@gmail.com>
2022-11-04 01:07:37 +0900

    refactor : OutputView에 printResult 메서드 변수를  상수로 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,6 +7,8 @@ public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String NOTHING = "낫싱";
+    public static final String BALL = "볼 ";
+    public static final String STRIKE = "스트라이크 ";
 
     public static void printStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -18,10 +20,10 @@ public class OutputView {
             sb.append(NOTHING);
         }
         if (playResult.getBall() != 0) {
-            sb.append(playResult.getBall() + "볼 ");
+            sb.append(playResult.getBall() + BALL);
         }
         if (playResult.getStrike() != 0) {
-            sb.append(playResult.getStrike() + "스트라이크 ");
+            sb.append(playResult.getStrike() + STRIKE);
         }
         System.out.println(sb);
     }
