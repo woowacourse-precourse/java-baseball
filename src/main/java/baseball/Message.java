c7dc19b16b1f3c46666da3181d5025a714948058
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-03 16:23:03 +0900

    style : START_GAME, INPUT_NUM 변수를 매직넘버로 수정

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -4,8 +4,8 @@ import static baseball.BaseballGame.*;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Message {
-    public static String START_GAME = "숫자 야구 게임을 시작합니다.";
-    public static String INPUT_NUM = "숫자를 입력해주세요 : ";
+    public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUM = "숫자를 입력해주세요 : ";
     public static final String USER_WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String ASK_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String END_GAME = "숫자 야구 게임을 종료합니다.";
