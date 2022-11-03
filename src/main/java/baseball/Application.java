8479287b7a454d9df1304a5783a040fea68e7ce6
KSH <tkdgns543@gmail.com>
2022-11-04 02:09:31 +0900

    feat(Applications): 잘못된 재시도 여부 입력에 대해 예외 발생 메서드 구현
    입력으로는 정확히 1과 2만 허용
    나머지 경우는 모두 예외 발생 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,4 +5,10 @@ public class Application {
         final int NUMBER_LENGTH = 3;
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    private void checkRetryInputException(String retry) throws IllegalArgumentException{
+        if (!(retry.equals("1") || retry.equals("2"))) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
