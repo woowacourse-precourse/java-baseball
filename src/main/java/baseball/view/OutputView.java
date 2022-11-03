64ba1e990245b7b5c97fa26eec7d2649d6e21398
TaeHwan <kimta2hwan@gmail.com>
2022-11-03 19:57:59 +0900

    feat(outputview): 게임 종료 후 재시작/종료 여부를 묻는 문구를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,6 +5,7 @@ public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String ANSWER_INPUT_GUIDE = "숫자를 입력해주세요 : ";
     private static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_OR_STOP_GUIDE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -16,5 +17,9 @@ public class OutputView {
 
     public static void printGameEndMessage() {
         System.out.println(GAME_END);
+    }
+
+    public static void printRestartOrStopGuideMessage() {
+        System.out.println(RESTART_OR_STOP_GUIDE);
     }
 }
