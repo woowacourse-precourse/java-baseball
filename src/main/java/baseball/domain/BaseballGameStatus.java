1611dc72e82f036bbda3710f4d1cfd8949878ce2
greeng00se <thegreengoose7@gmail.com>
2022-11-03 22:38:08 +0900

    feat(BaseballGameStatus): enum을 반환하는 메서드 추가
    - isPlay -> isGameContinues 메서드명 변경

--- src/main/java/baseball/domain/BaseballGameStatus.java
+++ src/main/java/baseball/domain/BaseballGameStatus.java
@@ -1,20 +1,25 @@
 package baseball.domain;
 
+import java.util.Arrays;
+
 public enum BaseballGameStatus {
 
     PLAY(1), STOP(2);
-    
+
     private Integer statusCode;
 
     BaseballGameStatus(Integer statusCode) {
         this.statusCode = statusCode;
     }
 
-    public boolean isPlay() {
-        return this == PLAY;
+    public static BaseballGameStatus getEnum(Integer statusCode) {
+        return Arrays.stream(values())
+                .filter(status -> status.statusCode.equals(statusCode))
+                .findAny()
+                .orElseThrow(IllegalArgumentException::new);
     }
 
-    public boolean isStop() {
-        return this == STOP;
+    public boolean isGameContinues() {
+        return this == PLAY;
     }
 }
