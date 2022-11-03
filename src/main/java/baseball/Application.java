99ffc0c17dd4789e484bc47008b03fd8cbcce328
sunwng <sunwgna@gmail.com>
2022-11-03 13:15:31 +0900

    feat: #1 initialize a game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,38 @@
 package baseball;
 
+import java.util.*;
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
+    static int answer;
+
+    static int listToInt(List<Integer> listIn) {
+        StringBuilder output = new StringBuilder();
+        for (Integer integer : listIn) {
+            output.append(integer);
+        }
+        return Integer.parseInt(output.toString());
+    }
+
+    static List<Integer> generateNumberSet() {
+        List<Integer> generatedSet = new ArrayList<>();
+        while (generatedSet.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!generatedSet.contains(randomNumber)) {
+                generatedSet.add(randomNumber);
+            }
+        }
+        return generatedSet;
+    }
+
+    static int generateAnswer() {
+        List<Integer> generatedSet = generateNumberSet();
+        return listToInt(generatedSet);
+    }
+    static void initGame() {
+        answer = generateAnswer();
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
