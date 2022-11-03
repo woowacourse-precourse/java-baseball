2a88bf059c32808ae146de6ce012265bcbc1726a
Minjune <minjune8506@gmail.com>
2022-11-03 15:12:16 +0900

    feat: 서로 다른 숫자 생성 기능
    - 1부터 9까지의 서로 다른 3개의 숫자 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,34 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Computer {
+
+    public static final int START_NUMBER = 1;
+    public static final int END_NUMBER = 9;
+    public static final int RANDOM_NUMBERS_SIZE = 3;
+
+    private final List<Integer> randomNumbers;
+
+    public Computer() {
+        randomNumbers = new ArrayList<>();
+    }
+
+    public void generateRandomNumbers() {
+        randomNumbers.clear();
+        while (randomNumbers.size() < RANDOM_NUMBERS_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getRandomNumbers() {
+        return Collections.unmodifiableList(randomNumbers);
+    }
+}
