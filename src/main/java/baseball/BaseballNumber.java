b21ce2136769790392c5c42a2dd4b4b6d1560525
swandevson <shai4956@gmail.com>
2022-11-04 00:54:28 +0900

    Feat: BaseballNumber 클래스 생성
    - 야구게임 숫자를 관리하는 클래스
    - set 메소드를 사용해 입력 받은 값을 저장

---
+++ src/main/java/baseball/BaseballNumber.java
@@ -0,0 +1,15 @@
+package baseball;
+
+
+
+public class BaseballNumber {
+    private String baseballNumber;
+
+
+    public BaseballNumber() {}
+
+
+    public void set(String input) {
+        this.baseballNumber = input;
+    }
+}
