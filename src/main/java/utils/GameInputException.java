f8eadb33f78b05b013b0b1504d815960b9b1a56a
KSH <tkdgns543@gmail.com>
2022-11-03 23:18:26 +0900

    feat(GameInputException): 유저 입력에 중복되는 숫자가 있으면 예외 발생 메서드 구현
    문자열의 모든 문자를 Set에 넣었을때 크기와 문자열의 길이를 비교
    일치하지 않을 시 예외 발생

--- src/main/java/utils/GameInputException.java
+++ src/main/java/utils/GameInputException.java
@@ -1,5 +1,9 @@
 package utils;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class GameInputException {
     int numberLength;
 
@@ -26,6 +30,18 @@ public class GameInputException {
 
     private void checkContainZero(String userInput) throws IllegalArgumentException {
         if (userInput.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void checkUniqueNumbers(String userInput) throws IllegalArgumentException {
+        Set<Character> sizeChecker = new HashSet<>();
+
+        for (int stringIndex = 0; stringIndex < numberLength; stringIndex++) {
+            sizeChecker.add(userInput.charAt(stringIndex));
+        }
+
+        if (sizeChecker.size() != numberLength) {
             throw new IllegalArgumentException();
         }
     }
