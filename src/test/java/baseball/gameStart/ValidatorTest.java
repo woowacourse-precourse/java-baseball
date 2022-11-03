e58a2efe790fea04201df4d02ba9dfdfd7d3ba7e
dev_kong <ahdjd5@gmail.com>
2022-11-03 15:40:47 +0900

    test(GameEnd): add validate user finish input test code

--- src/test/java/baseball/gameStart/ValidatorTest.java
+++ src/test/java/baseball/gameStart/ValidatorTest.java
@@ -14,4 +14,11 @@ public class ValidatorTest {
         Assertions.assertThatThrownBy(() -> Validator.checkUserAnswerInput(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1234", "3", "0", "4", "asd", "12a", "_12", "112", "122"})
+    void user_finish_input_number_test(String input) {
+        Assertions.assertThatThrownBy(() -> Validator.checkUserFinishInput(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
