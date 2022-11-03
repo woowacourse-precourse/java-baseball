44b270b2ca9261bd50c7824bfef03f2d5c2e9660
eora21 <eora21@naver.com>
2022-11-03 14:58:56 +0900

    feat: 🎸 추가 플레이어가 맞춰야 할 임의의 수 3개 선택하는 메서드
    Randoms의 pickUniqueNumbersInRange를 사용하여 1부터 9까지의 값 중 3개를 뽑도록 함,
    pickUniqueNumbersInRange 내부 로직 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        NumbersBaseballGame numbersBaseballGame = new NumbersBaseballGame();
+        numbersBaseballGame.play();
+    }
+
+    static class NumbersBaseballGame {
+        private static final int START_NUMBER = 1;
+        private static final int END_NUMBER = 9;
+        private static final int PICK_COUNT = 3;
+        public void play() {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            List<Integer> targetNumbers = getTargetNumbers();
+        }
+
+        private List<Integer> getTargetNumbers() {
+            return Randoms.pickUniqueNumbersInRange(START_NUMBER, END_NUMBER, PICK_COUNT);
+        }
     }
 }
