562f91bcfff36d62456150d686afb59def2b6dc7
JDK <ehdrjf2062@gmail.com>
2022-11-03 15:06:15 +0900

    Feat: Comment 클래스 for 문자열데이터

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,3 +5,10 @@ public class Application {
         // TODO: 프로그램 구현
     }
 }
+
+class Comment {
+    final static String STARTGAME = "숫자 야구 게임을 시작합니다.";
+    final static String ENDGAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    final static String REGAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    final static String INPUTNUMBER = "숫자를 입력해주세요 : ";
+}
