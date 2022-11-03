0cd36288a7f0524b7eca986d132d9102c4f820b0
K-jun98 <alap_u@naver.com>
2022-11-04 01:01:07 +0900

    feat - 플레이어의 숫자는 3개로 한정한다.

--- src/main/java/baseball/domain/Balls.java
+++ src/main/java/baseball/domain/Balls.java
@@ -5,16 +5,25 @@ import java.util.stream.Collectors;
 
 public class Balls {
 
+    public static final int BALLS_FORMAL_SIZE = 3;
+    public static final String BALLS_SIZE_EXCEPTION_MESSAGE = "입력은 3자리만 가능합니다.";
     private final List<Ball> balls;
 
     private Balls(List<Ball> balls) {
-        this.balls = balls;
+        this.balls = sizeValidate(balls);
     }
 
     public static Balls createBalls(List<Integer> ballNumbers) {
         return new Balls(ballNumbers.stream()
                 .map(Ball::new)
                 .collect(Collectors.toUnmodifiableList()));
+    }
+
+    private List<Ball> sizeValidate(List<Ball> balls) {
+        if (balls.size() == BALLS_FORMAL_SIZE) {
+            return balls;
+        }
+        throw new IllegalArgumentException(BALLS_SIZE_EXCEPTION_MESSAGE);
     }
 
     public int getSize() {
