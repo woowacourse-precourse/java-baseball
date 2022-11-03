5ef56ef6c5bc4974b47346ae9db8aec5ef830e14
Jueun <jueun000101@gmail.com>
2022-11-03 17:50:02 +0900

    feat: check input, throw exception
    길이가 3인 문자열이 아닐 때, 0이상의 숫자가 아닐 때, 중복된 숫자일 때 예외를 던짐

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,6 +11,19 @@ public class Application {
         System.out.println("숫자를 입력해주세요 : ");
         return readLine();
     }
+    static void checkInput(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        boolean[] used = new boolean[10];
+        used[0] = true;
+        for (char ch : input.toCharArray()) {
+            if (!Character.isDigit(ch) || used[ch - '0']) {
+                throw new IllegalArgumentException();
+            }
+            used[ch - '0'] = true;
+        }
+    }
 
     public static void main(String[] args) {
         printGameStart();
