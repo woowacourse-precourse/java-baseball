04b1df0871b20060f5f381b647555d0c5b340b14
eugene1204 <s1061001@naver.com>
2022-11-03 17:57:47 +0900

    feat: randomNumber 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static List<Integer> randomNumber(){
+        List<Integer> randomNumbers = new ArrayList<>();
+        while(randomNumbers.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!randomNumbers.contains(randomNumber)){
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
+
     }
 }
