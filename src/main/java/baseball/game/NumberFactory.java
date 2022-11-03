1146affebfd933486aedf79181455fca152775f6
wjh2012 <wjh2012@naver.com>
2022-11-03 18:48:00 +0900

    feat(NumberFactory): Add 랜덤넘버 생성 클래스 3개의 랜덤숫자를 생성하여 List<Integer>을 반환하는 static 메서드

---
+++ src/main/java/baseball/game/NumberFactory.java
@@ -0,0 +1,24 @@
+package baseball.game;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberFactory {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int NUMBERS_SIZE = 3;
+
+    public static List<Integer> createRandomNumbers(){
+        List<Integer> numbers = new ArrayList<>();
+
+        while (numbers.size() < NUMBERS_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+
+        return numbers;
+    }
+}
