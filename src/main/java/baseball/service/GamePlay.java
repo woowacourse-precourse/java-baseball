81df6fc3bcb6c246d59876f28faffb432043f3ea
정핸모 <gusah009@naver.com>
2022-11-03 18:33:09 +0900

    feat: 게임 플레이 클래스 확장성을 위해 인자를 받을 args 변수 추가

--- src/main/java/baseball/service/GamePlay.java
+++ src/main/java/baseball/service/GamePlay.java
@@ -2,23 +2,35 @@ package baseball.service;
 
 import baseball.game.Game;
 import camp.nextstep.edu.missionutils.Console;
+
 import java.util.Arrays;
 
 public class GamePlay {
 
-    public static void start(Class<? extends Game> gameClazz) {
+    /**
+     * @param gameClazz 플레이 할 게임의 클래스를 넣어주시면 됩니다.
+     * @param args      플레이 할 게임 클래스 생성자의 인자들을 넣어주시면 됩니다.
+     */
+    public static void start(Class<? extends Game> gameClazz, Object... args) {
         do {
-            Game game = getGameInstance(gameClazz);
+            Game game = getGameInstance(gameClazz, args);
             game.play();
         } while (getPlayerWant() == GameStatus.RESTART);
     }
 
-    private static Game getGameInstance(Class<? extends Game> gameClazz) {
+    private static Game getGameInstance(Class<? extends Game> gameClazz, Object... args) {
         try {
-            return gameClazz.getDeclaredConstructor().newInstance();
+            return gameClazz.getDeclaredConstructor(getArgsClass(args))
+                    .newInstance(args);
         } catch (Exception e) {
             throw new IllegalArgumentException("잘못된 게임 클래스입니다.");
         }
+    }
+
+    private static Class<?>[] getArgsClass(Object[] args) {
+        return Arrays.stream(args)
+                .map(Object::getClass)
+                .toArray(Class<?>[]::new);
     }
 
     private static GameStatus getPlayerWant() {
