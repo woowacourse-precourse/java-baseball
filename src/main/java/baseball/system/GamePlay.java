84454cd8a2934f5841421a46a1e88bec5a660ca8
myungsikyoon <yms8795@gmail.com>
2022-11-03 18:19:18 +0900

    feat(GamePlay) : 인스턴스 변수와 생성자 선언.

--- src/main/java/baseball/system/GamePlay.java
+++ src/main/java/baseball/system/GamePlay.java
@@ -1,4 +1,21 @@
 package baseball.system;
 
+import baseball.player.*;
+
+import java.util.List;
+
 public class GamePlay {
+
+    private Computer computer;
+    private User user;
+
+    GamePlay(Computer computer , User user){
+        this.computer = computer;
+        this.user = user;
+    }
+
+  
+
+
+
 }
