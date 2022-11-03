02bc3b38de7eddc4a36f22233f54976b8000d95b
ksh-beginner <ohk9134@naver.com>
2022-11-03 22:52:13 +0900

    test(OpponentComputerTest) : 두 리스트를 비교해서 스트라이크, 볼 개수 판정 - 3볼

--- src/test/java/baseball/model/OpponentComputerTest.java
+++ src/test/java/baseball/model/OpponentComputerTest.java
@@ -92,6 +92,17 @@ public class OpponentComputerTest {
     }
 
     @Test
+    void 상대방_수_리스트와_플레이어_수_리스트를_비교해서_스트라이크_볼_개수_판정_3볼() {
+        OpponentComputer opponentComputer = new OpponentComputer(123);
+        List<String> opponentComputerNumberEachDigits = List.of("1", "2", "3");
+        List<String> playerNumberEachDigits = List.of("2", "3", "1");
+        opponentComputer.judgeStrikeOrBall(opponentComputerNumberEachDigits, playerNumberEachDigits);
+
+        assertThat(opponentComputer.getStrikeAndBallCountMap().get("Strike")).isEqualTo(0);
+        assertThat(opponentComputer.getStrikeAndBallCountMap().get("Ball")).isEqualTo(3);
+    }
+
+    @Test
     void 상대방_수_리스트와_플레이어_수_리스트를_비교해서_스트라이크_볼_개수_판정_1스트라이크() {
         OpponentComputer opponentComputer = new OpponentComputer(123);
         List<String> opponentComputerNumberEachDigits = List.of("1", "2", "3");
