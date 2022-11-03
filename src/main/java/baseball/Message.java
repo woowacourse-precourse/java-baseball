ddfa6d12392c1b059efa5f236b45b879508981ed
splitube <voicesplit@daum.net>
2022-11-03 21:45:52 +0900

    feat(Message): 결과 메시지 반환 기능 구현
    입력받은 결과에 따라 4가지 메시지를 반환

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.List;
+
 public class Message {
     public String start() {
         return "숫자 야구 게임을 시작합니다.";
@@ -15,5 +17,20 @@ public class Message {
 
     public String requestNewOrStop() {
         return "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    }
+
+    String result(List<Integer> result) {
+        int ball = result.get(0);
+        int strike = result.get(1);
+        if (ball == 0 && strike == 0) {
+            return "낫싱";
+        }
+        if (ball == 0 && strike > 0) {
+            return strike + "스트라이크";
+        }
+        if (ball > 0 && strike == 0) {
+            return ball + "볼";
+        }
+        return ball + "볼 " + strike + "스트라이크";
     }
 }
