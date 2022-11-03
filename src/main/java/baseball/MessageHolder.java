bcbe6c277539c891d3f6ef51546a088e4380db64
callusk0 <callusk00@gmail.com>
2022-11-04 00:15:49 +0900

    feat: MessageHolder 숫자 입력 과정
    예외처리, 입력 함수 작성

--- src/main/java/baseball/MessageHolder.java
+++ src/main/java/baseball/MessageHolder.java
@@ -12,9 +12,34 @@ public class MessageHolder {
     }
 
     public int print_get_game_input() {
-        return 0;
+        System.out.print("숫자를 입력해주세요 : ");
+        int getInput = get_player_input();
+        return getInput;
     }
-    
+
+    private int get_player_input() {
+        String playerString = Console.readLine();
+        int playerNum = string_to_int(playerString);
+        return playerNum;
+    }
+
+    private int string_to_int(String playerString) {
+        String checkIndex = "^[0-9]{3}$";
+        int number = -1;
+        if (!playerString.matches(checkIndex)) {
+            throw new IllegalArgumentException("세자리 숫자를 입력해주십시오.");
+        }
+        try{
+            number = Integer.parseInt(playerString);
+            System.out.println(number); // output = 25
+        }
+        catch (NumberFormatException ex){
+            throw new IllegalArgumentException("세자리 숫자를 입력해주십시오.");
+        }
+
+        return number;
+    }
+
     public void print_ball_and_strke(int currentBall, int currentStrike) {
     }
 
