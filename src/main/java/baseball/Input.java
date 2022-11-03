593bf869f08d1b7ffd1943d53b03cc5027396c6b
HangSub999 <gkdtjq999@gmail.com>
2022-11-03 19:28:40 +0900

    feat: 게임 재시작 숫자 입력받는 기능

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -9,4 +9,8 @@ public class Input {
         return Console.readLine();
     }
 
+    public static String inputRestartNumber(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
+    }
 }
