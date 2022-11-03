e6d425133fac91db53b3eacfc4174dbf24825a78
khsrla9806 <kimhunsope@naver.com>
2022-11-03 23:59:40 +0900

    refactor(Game): 게임 시작 문구 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,6 +3,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Game {
     public static void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         Computer computer = new Computer();
         Player player = new Player();
 
