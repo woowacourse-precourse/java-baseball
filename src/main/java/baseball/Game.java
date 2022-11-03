85835275e71d9d918c021c398ccd3524519c1115
Soomin Lim <augustth511@gmail.com>
2022-11-03 19:50:08 +0900

    feat: 사용자 숫자 입력 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,13 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Game {
 
     public void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
+
+            List<Integer> user = new ArrayList<>();
+            for (String s : Arrays.asList(input.split(""))) {
+                user.add(Integer.parseInt(s));
+            }
+
+        }
 
     }
 
