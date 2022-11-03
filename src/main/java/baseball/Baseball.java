86b520d25a2e98c088d1716c316213a52ae2a7a4
KSH <tkdgns543@gmail.com>
2022-11-03 23:37:49 +0900

    feat(Baseball): 단일 게임에 사용되는 최대 숫자 길이를 매개변수 받는 생성자 구현 기본 생성자 오버라이딩으로 다른 생성자의 매개변수에 3을 넣어 호출

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -4,8 +4,15 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Baseball {
     String userNumbers;
+    int numberLength;
 
-    Baseball() {}
+    Baseball() {
+        this(3);
+    }
+
+    Baseball(int numberLength) {
+        this.numberLength = numberLength;
+    }
 
     private void setUserNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
