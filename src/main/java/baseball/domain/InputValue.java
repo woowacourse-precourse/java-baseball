2fc49fca3f0b1602b27e0eb4889dab743a810275
ywj9811 <ywj9811@naver.com>
2022-11-04 02:31:11 +0900

    feat: InputValue 클래스 생성
    domain 패키지 내부에 입력값을 다루는 클래스 생성 및 작성

---
+++ src/main/java/baseball/domain/InputValue.java
@@ -0,0 +1,35 @@
+package baseball.domain;
+
+import baseball.exception.InputException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputValue {
+    InputException validation = new InputException();
+    private String input;
+
+    public List<Integer> inputNum() {
+
+        System.out.print("숫자를 입력해 주세요 : ");
+        input = readLine();
+
+        validation.isNumber(input);
+
+        List<Integer> inputNumber = returnList(input);
+
+        return inputNumber;
+    }
+
+    private List<Integer> returnList(String inputNum) {
+        List<Integer> list = new ArrayList<>();
+        for (int idx = 0; idx < inputNum.length(); idx++) {
+            list.add(Integer.parseInt(
+                    String.valueOf(inputNum.charAt(idx))));
+        }
+
+        return list;
+    }
+}
