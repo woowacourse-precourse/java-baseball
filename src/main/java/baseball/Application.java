9500d586d6d6baa730e4194e65d1b9a5e61b3280
eunji53 <duqtj00@naver.com>
2022-11-03 13:39:51 +0900

    <feat>:숫자 입력
    1~9자연수 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 import java.util.*;
-import camp.nextstep.edu.missionutils.*;
+import camp.nextstep.edu.missionutils.Randoms;
+import static camp.nextstep.edu.missionutils.Console.*;
+
 public class Application {
     public static List<Integer> getRandomNum() {
         List<Integer> computer = new ArrayList<>();
@@ -11,6 +13,11 @@ public class Application {
             }
         }
         return computer;
+    }
+    public static String numberInput(){
+        System.out.println("숫자를 입력해주세요 : ");
+        String num = readLine();
+        return num;
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
