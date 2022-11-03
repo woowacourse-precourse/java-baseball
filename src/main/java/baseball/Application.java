f60ecdc47fd4a6b6fee8f93a6076974420730bb9
sumin-Heo <102477741+sumin-Heo@users.noreply.github.com>
2022-11-03 15:01:43 +0900

    feat : 숫자 입력받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+    public void playGame(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        String guess = "";
+
+        System.out.print("숫자를 입력해주세요 : ");
+        guess = String.valueOf(new Scanner(System.in));
+
+        if(guess.length() == 3 ) System.out.println(guess);
+        else throw new IllegalArgumentException ("입력이 잘못되었습니다");
     }
 }
