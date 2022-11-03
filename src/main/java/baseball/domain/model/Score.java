0b39bf2ae1f9a5c9dd80fedee2cd8a42259d7f3c
sangwon <swchoi1997@naver.com>
2022-11-03 22:12:01 +0900

    feat: score 로 부터 result를 만드는 기능 추가
    score의 Map<Enum, Integer>를 Map<String, Integer> 로 변경하여 리턴

--- src/main/java/baseball/domain/model/Score.java
+++ src/main/java/baseball/domain/model/Score.java
@@ -7,48 +7,58 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Score {
-	private final Map<BallCount, Integer> score;
+    private final Map<BallCount, Integer> score;
 
-	public Score() {
-		this.score = intiScoreMap();
-	}
+    public Score() {
+        this.score = intiScoreMap();
+    }
 
-	public Score(final Map<BallCount, Integer> score) {
-		this.score = score;
-	}
+    public Score(final Map<BallCount, Integer> score) {
+        this.score = score;
+    }
 
-	public Score(final Computer computer, final User user) {
-		this.score = calculateBallCountScore(computer, user);
-	}
+    public Score(final Computer computer, final User user) {
+        this.score = calculateBallCountScore(computer, user);
+    }
 
-	private Map<BallCount, Integer> intiScoreMap() {
-		return Arrays.stream(BallCount.values())
-			.collect(Collectors.toMap(value -> value, count -> 0, (a, b) -> b, () -> new EnumMap<>(BallCount.class)));
-	}
+    private Map<BallCount, Integer> intiScoreMap() {
+        return Arrays.stream(BallCount.values())
+                .collect(Collectors.toMap(value -> value, count -> 0, (a, b) -> b,
+                        () -> new EnumMap<>(BallCount.class)));
+    }
 
-	private Map<BallCount, Integer> calculateBallCountScore(final Computer computer, final User user) {
-		return user.compareWithComputerBalls(computer, intiScoreMap());
-	}
+    private Map<BallCount, Integer> calculateBallCountScore(final Computer computer, final User user) {
+        return user.compareWithComputerBalls(computer, intiScoreMap());
+    }
 
-	@Override
-	public boolean equals(Object o) {
-		if (this == o)
-			return true;
-		if (o == null || getClass() != o.getClass())
-			return false;
-		Score score1 = (Score)o;
-		return Objects.equals(score, score1.score);
-	}
+    public Map<String, Integer> getResult() {
+        return score.keySet().stream()
+                .collect(Collectors.toMap(BallCount::getValue, score::get, (a, b) -> b));
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(score);
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Score score1 = (Score) o;
+        return Objects.equals(score, score1.score);
+    }
 
-	@Override
-	public String toString() {
-		return "Score{" +
-			"score=" + score +
-			'}';
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(score);
+    }
+
+    @Override
+    public String toString() {
+        return "Score{" +
+                "score=" + score +
+                '}';
+    }
+
+
 }
