c678d0857abe7c3641b2220d6bbe968f332669f1
seokjin8678 <rnrn8678@naver.com>
2022-11-03 21:03:10 +0900

    test: 테스트 코드 수정
    - BeforeEach로 매 테스트 시 게임 초기화
    - 게임시작시 시작메세지 출력 테스트 수정

--- src/test/java/baseball/game/GameTest.java
+++ src/test/java/baseball/game/GameTest.java
@@ -2,6 +2,7 @@ package baseball.game;
 
 import baseball.Application;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -9,20 +10,23 @@ import static org.assertj.core.api.Assertions.*;
 
 class GameTest extends NsTest {
 
+	Game game;
+
+	@BeforeEach
+	void setUp() {
+		game = Game.create();
+	}
+
 	@Test
 	void 게임시작시_시작메세지가_출력돼야한다() {
-		assertSimpleTest(
-				() -> {
-					runMain();
-					assertThat(output().equals("숫자 야구 게임을 시작합니다."))
-							.isTrue();
-				}
+		assertSimpleTest(() ->
+				assertThat(output().contains("숫자 야구 게임을 시작합니다."))
+						.isTrue()
 		);
 	}
 
 	@Test
 	void 사용자의_입력이_3자리가_아니면_예외가_발생한다() {
-		Game game = Game.create();
 		assertThatThrownBy(() -> game.validateUserInput("12"))
 				.isInstanceOf(IllegalArgumentException.class)
 				.hasMessage("1~9까지 3자리 숫자를 입력해주세요.");
@@ -30,7 +34,6 @@ class GameTest extends NsTest {
 
 	@Test
 	void 사용자의_입력이_정수가_아니면_예외가_발생한다() {
-		Game game = Game.create();
 		assertThatThrownBy(() -> game.validateUserInput("12삼"))
 				.isInstanceOf(IllegalArgumentException.class)
 				.hasMessage("1~9까지 정수만 입력해주세요.");
