91dfe0e7095579dc02d312fbc76c9ff63c5d2746
KimDongho <hkim4410@naver.com>
2022-11-03 21:32:42 +0900

    docs(docs/README): 게임을 새로 시작하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,8 +12,7 @@ public class Application {
     private final String STRIKE = "스트라이크";
     private final String BALL = "볼";
     private final String NOTHING = "낫싱";
-    private final String THREE_STRIKE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private final String EXCEPTION_LOG = "예외 상황이 발생했습니다. 프로그램을 종료합니다.";
+    private final String THREE_STRIKE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -99,18 +98,6 @@ public class Application {
         int count = 0;
         char[] inputCharArray = input.toCharArray();
         char[] answerCharArray = answer.toCharArray();
-
-        for (int i = 0; i < inputCharArray.length; i++) {
-            for (int j = 0; j < answerCharArray.length; j++) {
-                if(i == j) {
-                    continue;
-                }
-
-                if(inputCharArray[i] == answerCharArray[j]) {
-                    count++;
-                }
-            }
-        }
 
         return count;
     }
