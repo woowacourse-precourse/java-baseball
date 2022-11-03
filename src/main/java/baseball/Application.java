d40e86a6c7f21a879ee1f317ccdaba69011d2ead
SH <gram753@naver.com>
2022-11-03 23:21:50 +0900

    feat: 예외처리 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.operator.Operator;
+
 public class Application {
     public static void main(String[] args) {
 
+    Operator operator;
+
+        do {
+            operator = new Operator();
+            operator.run();
+        }while (operator.requestRestart());
     }
 }
