1504108f05ba54a0e05999889e310d6bcb3bca12
sinsu <sinsu19012@gmail.com>
2022-11-03 20:59:43 +0900

    ✨feat : 사용자에게 서로 다른 3개의 수 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,19 +3,56 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         List<Integer> computer = RandomNum(3);
+        System.out.println(computer);
+
+        String usernum = userInput();
+        confirmUserInput(usernum);
+
+        while(true){
+
+
+        }
+
 
     }
 
-    public static List<Integer> RandomNum(int count){
+    public static void confirmUserInput(String userInput) {
         List<Integer> list = new ArrayList<>();
-        while(list.size() <count){
-            int randomNumber = Randoms.pickNumberInRange(1,9);
-            if(!list.contains(randomNumber)){
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException("3자리 숫자가 아닙니다.");
+        }
+        for (char usernumchar : userInput.toCharArray()) {
+            int usernum = Character.getNumericValue(usernumchar);
+            if (!(usernumchar >= '0' && usernumchar <= '9')) {
+                throw new IllegalArgumentException("숫자가 아닌 문자 입니다.");
+            }
+            if (list.contains(usernum)) {
+                throw new IllegalArgumentException("서로 다른 수가 아닙니다.");
+            }
+            list.add(usernum);
+        }
+    }
+
+
+    public static String userInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String usernum = Console.readLine();
+        return usernum;
+    }
+
+
+    public static List<Integer> RandomNum(int count) {
+        List<Integer> list = new ArrayList<>();
+        while (list.size() < count) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!list.contains(randomNumber)) {
                 list.add(randomNumber);
             }
         }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,7 +1,9 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -10,31 +12,60 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
     void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
+        assertRandomNumberInRangeTest(() -> {
+            run("246", "135", "1", "597", "589", "2");
+            assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+        }, 1, 3, 5, 5, 8, 9);
     }
 
     @Test
     void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1234")).isInstanceOf(
+            IllegalArgumentException.class));
     }
 
     @Test
     @DisplayName("랜덤함수 개수가 맞게 생성됬는지 테스트")
-    void 랜덤함수_테스트(){
-        int num =3;
-        assertThat(Application.RandomNum(num).size()).isEqualTo(num);
+    void 랜덤함수_테스트() {
+        int num = 3;
+        List<Integer> randomnum = Application.RandomNum(num);
+        assertThat(randomnum.size()).isEqualTo(num);
+
     }
+
+    @Nested
+    class 사용자_입력_검증_테스트 {
+
+        @Test
+        void 사용자_입력_검증_길이가다른_예외() {
+            assertSimpleTest(
+                () -> assertThatThrownBy(() -> Application.confirmUserInput("1233")).isInstanceOf(
+                    IllegalArgumentException.class));
+        }
+
+        @Test
+        void 사용자_입력_검증_서로다른수_예외() {
+            assertSimpleTest(
+                () -> assertThatThrownBy(() -> Application.confirmUserInput("111")).isInstanceOf(
+                    IllegalArgumentException.class));
+        }
+
+        @Test
+        void 사용자_입력_검증_숫자가아닌수_예외() {
+            assertSimpleTest(
+                () -> assertThatThrownBy(() -> Application.confirmUserInput("ㅁㅁㅁ")).isInstanceOf(
+                    IllegalArgumentException.class));
+            assertSimpleTest(
+                () -> assertThatThrownBy(() -> Application.confirmUserInput("aaa")).isInstanceOf(
+                    IllegalArgumentException.class));
+        }
+
+
+    }
+
 
     @Override
     public void runMain() {
