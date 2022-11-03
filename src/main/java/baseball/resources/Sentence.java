a7214dd4cebd05884493bb38dd469b7328ca3da9
JHS <wnghdtjr129@naver.com>
2022-11-03 23:06:25 +0900

    refactor: 출력문 enum에서 클래스 상수값으로 변경 출력로직 분리

--- src/main/java/baseball/resources/Sentence.java
+++ src/main/java/baseball/resources/Sentence.java
@@ -2,41 +2,13 @@ package baseball.resources;
 
 import static baseball.resources.GameConfig.*;
 
-public enum Sentence {
-    START("숫자 야구 게임을 시작합니다."),
-    ASK_INPUT("숫자를 입력해주세요 : "),
-    ASK_RESTART(SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 " + RESTART + ", 종료하려면 " + FINISH + "를 입력하세요."),
-
-    BALL_STRIKE("%d볼 %d스트라이크\n"),
-    STRIKE("%d스트라이크\n"),
-    BALL("%d볼\n"),
-    NOTHING("낫싱"),
-
-    EXCEPTION("서로 다른 " + SIZE + "개의 자연수를 입력하세요");
-
-    private final String message;
-
-    Sentence(String message) {
-        this.message = message;
-    }
-
-    public static void print(Sentence sentence) {
-        System.out.print(sentence.message);
-    }
-
-    public static void println(Sentence sentence) {
-        System.out.println(sentence.message);
-    }
-
-    public static void printf(Sentence sentence, int count) {
-        System.out.printf(sentence.message, count);
-    }
-
-    public static void printf(Sentence sentence, int ball, int strike) {
-        System.out.printf(sentence.message, ball, strike);
-    }
-
-    public static void throwException(Sentence sentence) {
-        throw new IllegalArgumentException(sentence.message);
-    }
+public abstract class Sentence {
+    public final static String START = "숫자 야구 게임을 시작합니다.";
+    public final static String ASK_INPUT = "숫자를 입력해주세요 : ";
+    public final static String ASK_RESTART = SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 " + RESTART + ", 종료하려면 " + FINISH + "를 입력하세요.";
+    public final static String BALL_STRIKE = "%d볼 %d스트라이크\n";
+    public final static String STRIKE = "%d스트라이크\n";
+    public final static String BALL = "%d볼\n";
+    public final static String NOTHING = "낫싱";
+    public final static String EXCEPTION = "서로 다른 " + SIZE + "개의 자연수를 입력하세요";
 }
