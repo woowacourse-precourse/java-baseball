b8678ca7a22b70596353a3feab959d3620991bd8
JUHA <khabh@naver.com>
2022-11-03 15:17:33 +0900

    feat(StrikeNumbersGenerator): 스트라이크에 해당되는 숫자 3개 생성

---
+++ src/main/java/domain/StrikeNumbersGenerator.java
@@ -0,0 +1,29 @@
+package domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+
+public class StrikeNumbersGenerator {
+    private final static int MIN_STRIKE_NUMBER = 1;
+    private final static int MAX_STRIKE_NUMBER = 9;
+    private final static int NUMBER_AMOUNT = 3;
+
+    private final ArrayList<Integer> strikeNumbers = new ArrayList<>();
+
+    public ArrayList<Integer> generateStrikeNumbers() {
+        for (int countOfNumbers = 0; countOfNumbers < NUMBER_AMOUNT; countOfNumbers++) {
+            strikeNumbers.add(pickUniqueNumber());
+        }
+
+        return strikeNumbers;
+    }
+
+    private int pickUniqueNumber() {
+        int uniqueNumber;
+        do {
+            uniqueNumber = Randoms.pickNumberInRange(MIN_STRIKE_NUMBER, MAX_STRIKE_NUMBER);
+        } while (strikeNumbers.contains(uniqueNumber));
+
+        return uniqueNumber;
+    }
+}
