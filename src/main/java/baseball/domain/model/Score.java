bc0ecce6797706a22731c4e21e0f532cd3dca143
sangwon <swchoi1997@naver.com>
2022-11-03 16:49:12 +0900

    feat: 결과 정보를 저장할 Score 객체 추가
    아웃, 볼, 스트라이크의 정보를 EnumMap으로 저장 및 관리

--- src/main/java/baseball/domain/model/Score.java
+++ src/main/java/baseball/domain/model/Score.java
@@ -3,6 +3,9 @@ package baseball.domain.model;
 import java.util.Map;
 
 public class Score {
-	private Map<BallCount, Integer> score;
+	private final Map<BallCount, Integer> score;
 
+	public Score(Map<BallCount, Integer> score) {
+		this.score = score;
+	}
 }
