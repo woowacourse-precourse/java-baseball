167a365b755b2005705e20cd59493cc1695d09cb
rhqudco <rhqudco1204@naver.com>
2022-11-03 18:04:53 +0900

    FEAT : Application main Method Completion

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,28 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> correctAnswer = Start.startGame();
+        while (true) {
+            UserInput userInput = new UserInput();
+            List<Integer> slicedInput = userInput.input();
 
+            NumberComparison comparison = new NumberComparison();
+            int ballCount = comparison.checkBall(correctAnswer, slicedInput);
+            int strikeCount = comparison.checkStrike(correctAnswer, slicedInput);
+
+            GameBranch gameBranch = new GameBranch();
+            int gameStatus = gameBranch.status(ballCount, strikeCount);
+
+            if (gameStatus == -2) {
+                correctAnswer = Start.reStart();
+            } else if (gameStatus == -3) {
+                break;
+            }
+        }
     }
 }
