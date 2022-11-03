8cc1692c1bd2d7005148fb6739d38ec033c5d861
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-03 11:15:40 +0900

    refactor(Player): 매직 넘버 제외 및 문맥 분리를 위한 공백라인
    주요 변경사항:
    - validateZeroException 메서드의 숫자 0을 상수로 변경
    - 클래스 변수와 인스턴스 변수 사이에 공백 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -4,30 +4,39 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Pitcher {
-	private static final int PITCHING_NUMBER_SIZE = 3;
-	private final List<Integer> pitchingNumbers;
+public class Player {
+	private static final int PLAYING_NUMBER_SIZE = 3;
+	private static final int NUMBER_OF_EXCEPTION = 0;
 
-	public Pitcher(List<Integer> pitchingNumbers) {
-		validateSize(pitchingNumbers);
-		validateDuplicate(pitchingNumbers);
-		this.pitchingNumbers = pitchingNumbers;
+	private final List<Integer> playingNumbers;
+
+	public Player(List<Integer> playingNumbers) {
+		validateSize(playingNumbers);
+		validateDuplicate(playingNumbers);
+		validateZeroException(playingNumbers);
+		this.playingNumbers = playingNumbers;
 	}
 
-	private void validateSize(List<Integer> pitchingNumbers) {
-		if(pitchingNumbers.size() != PITCHING_NUMBER_SIZE) {
-			throw new IllegalArgumentException();
+	private void validateSize(List<Integer> playingNumbers) {
+		if (playingNumbers.size() != PLAYING_NUMBER_SIZE) {
+			throw new IllegalArgumentException("3자리 숫자만 입력해주세요.");
 		}
 	}
 
-	private void validateDuplicate(List<Integer> pitchingNumbers) {
-		Set<Integer> nonDuplicateNumbers = new HashSet<>(pitchingNumbers);
-		if(nonDuplicateNumbers.size() != PITCHING_NUMBER_SIZE) {
-			throw new IllegalArgumentException();
+	private void validateDuplicate(List<Integer> playingNumbers) {
+		Set<Integer> nonDuplicateNumbers = new HashSet<>(playingNumbers);
+		if (nonDuplicateNumbers.size() != PLAYING_NUMBER_SIZE) {
+			throw new IllegalArgumentException("서로 다른 숫자들만 입력해주세요.");
 		}
 	}
 
-	public List<Integer> getPitchingNumbers() {
-		return pitchingNumbers;
+	private void validateZeroException(List<Integer> playingNumbers) {
+		if (playingNumbers.contains(NUMBER_OF_EXCEPTION)) {
+			throw new IllegalArgumentException("0은 포함될 수 없습니다.");
+		}
+	}
+
+	public List<Integer> getPlayingNumbers() {
+		return playingNumbers;
 	}
 }
