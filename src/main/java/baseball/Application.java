096291e501ea846038556abb9078820e12cb39ab
bomin1996 <bomin199628@gmail.com>
2022-11-03 19:42:31 +0900

    <feat> : 이용자의 숫자 입력 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
@@ -13,6 +14,10 @@ public class Application {
                 computer.add(randomNumber);
             }
         }//컴퓨터 랜덤 숫자 생성
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+        String userNumber = Console.readLine();
+        System.out.println("userNumber = " + userNumber);
         // TODO: 프로그램 구현
     }
 }
