ad3a393cfbe11bdcb05a5990c29866d49e3ff5e2
Kim woo jong <skekdnwhd@gmail.com>
2022-11-04 03:15:45 +0900

    Fix : Exception 처리 안된 부분 처리, 의미없는 ; 삭제
    NoSuchElement를 IllegalArgument로 처리함

--- src/main/java/baseball/utils/ConsoleLog.java
+++ src/main/java/baseball/utils/ConsoleLog.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Objects;
 
+import static baseball.enums.ErrorMessage.IS_BLANK;
+
 public class ConsoleLog {
     private static ConsoleLog instance;
 
@@ -19,13 +21,17 @@ public class ConsoleLog {
 
     public void print(String message) {
         System.out.print(message);
-    };
+    }
 
     public void println(String message) {
         System.out.println(message);
-    };
+    }
 
     public String input() {
-        return Console.readLine();
+        try {
+            return Console.readLine();
+        } catch (Exception e) {
+            throw new IllegalArgumentException(IS_BLANK.message());
+        }
     }
 }
