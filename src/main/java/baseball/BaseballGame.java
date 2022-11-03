c2f7a80d52091c0a704a9518213ac3e9388e6ef5
jini_11 <jieun21124@naver.com>
2022-11-03 22:13:49 +0900

    feat: 사용자 입력을 받는 부분 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,18 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static constant.BaseballConstant.INPUT_MESSAGE;
 import static constant.BaseballConstant.START_MESSAGE;
 
 public class BaseballGame {
     private static final List<Integer> computerNumbers = new ArrayList<>();
+    private static String userNumber;
 
     public static void start() {
         makeRandomNumber();
         printMessage(START_MESSAGE);
+        getUserNumber();
     }
 
     private static void makeRandomNumber() {
@@ -26,5 +30,10 @@ public class BaseballGame {
 
     public static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    public static void getUserNumber() {
+        printMessage(INPUT_MESSAGE);
+        userNumber = Console.readLine();
     }
 }
--- src/main/java/constant/BaseballConstant.java
+++ src/main/java/constant/BaseballConstant.java
@@ -2,4 +2,5 @@ package constant;
 
 public class BaseballConstant {
     public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 }
