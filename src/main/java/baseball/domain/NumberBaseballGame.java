98cea9d6eb4af28fe60fdcd09bb3b82374b19d0b
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-04 08:29:20 +0900

    feat: 입력값 검증 함수 구현

--- src/main/java/baseball/domain/NumberBaseballGame.java
+++ src/main/java/baseball/domain/NumberBaseballGame.java
@@ -1,10 +1,12 @@
 package baseball.domain;
 
+import baseball.util.GameExceptionMessage;
 import baseball.util.ShowGameMessage;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class NumberBaseballGame {
     private final ShowGameMessage showGameMessage = new ShowGameMessage();
@@ -15,8 +17,28 @@ public class NumberBaseballGame {
         showGameMessage.gameStartMessage();
     }
 
+    private List<NumberBall> getUserNumberBall() {
+        String userInput = getUserInputString();
+        validate(userInput);
+    }
     private String getUserInputString() {
         return Console.readLine();
+    }
+
+    private void validate(String userInput) {
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException(GameExceptionMessage.USER_NUMBER_LENGTH);
+        }
+        for (int i = 0; i < userInput.length(); i++) {
+            if (userInput.charAt(i) >= '1' && userInput.charAt(i) <= '9') {
+                continue;
+            }
+            throw new IllegalArgumentException(GameExceptionMessage.USER_NUMBER_RANGE);
+        }
+        int len = (int) Stream.of(userInput.split("")).distinct().count();
+        if (len != 3) {
+            throw new IllegalArgumentException(GameExceptionMessage.USER_NUMBER_LENGTH);
+        }
     }
 
     private List<NumberBall> initializeSystemNumberBall() {
