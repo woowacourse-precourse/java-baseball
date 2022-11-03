45e7dabea7264f19f3b236eeec706ddff9bbd892
jaehyeon <jhjh0022@naver.com>
2022-11-03 22:02:26 +0900

    feat: 사용자에게 숫자 입력 받기 구현

--- src/main/java/baseball/InputNumber.java
+++ src/main/java/baseball/InputNumber.java
@@ -1,2 +1,13 @@
-package baseball;public class InputNumber {
+package baseball;
+
+import java.util.Scanner;
+
+public class InputNumber {
+    public  int getInputNumber(){
+        Scanner number = new Scanner(System.in);
+        System.out.print("숫자를 입력해주세요 : ");
+        int receivedNumber = number.nextInt();
+
+        return receivedNumber;
+    }
 }
