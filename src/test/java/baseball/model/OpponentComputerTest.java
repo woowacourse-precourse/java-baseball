fb9f53a0169ff820b4ab8f42e4a8ecba341d77bc
ksh-beginner <ohk9134@naver.com>
2022-11-03 15:26:17 +0900

    test(OpponentComputerTest) : 상대방 생성과 동시에 랜덤 수 저장 테스트 랜덤 수 검증하도록 리팩토링
    opponentComputer.getNumber()로 상대방 객체의 저장된 랜덤 수를 받아서
    그 수가 null이 아닐 때 테스트 통과하도록 리팩토링

--- src/test/java/baseball/model/OpponentComputerTest.java
+++ src/test/java/baseball/model/OpponentComputerTest.java
@@ -1,18 +1,19 @@
 package baseball.model;
 
 import baseball.RandomUtil;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class OpponentComputerTest {
 
     @Test
     void 상대방_생성과_동시에_랜덤_수_저장() {
         OpponentComputer opponentComputer = new OpponentComputer(RandomUtil.createRandomNumber());
-        if (opponentComputer == null) {
-            fail();
-        }
+        int opponentComputerNumber = opponentComputer.getNumber();
+        assertThat(opponentComputerNumber).isNotNull();
     }
 
 }
