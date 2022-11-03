6e0941d794697b653fafcef994d1e4b15370a60b
sangwon <swchoi1997@naver.com>
2022-11-03 17:34:10 +0900

    feat: Score 객체의 Map을 Enum값으로 초기화
    Map자료형 score를 Enum.value : 0 형태로 초기화

--- src/main/java/baseball/domain/model/Score.java
+++ src/main/java/baseball/domain/model/Score.java
@@ -1,11 +1,40 @@
 package baseball.domain.model;
 
+import java.util.Arrays;
+import java.util.EnumMap;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 
 public class Score {
 	private final Map<BallCount, Integer> score;
 
+	public Score() {
+		this.score = initEnumMap();
+	}
+
 	public Score(Map<BallCount, Integer> score) {
 		this.score = score;
+	}
+
+	private Map<BallCount, Integer> initEnumMap() {
+		return Arrays.stream(BallCount.values())
+			.collect(Collectors.toMap(value -> value, count -> 0, (a, b) -> b, () -> new EnumMap<>(BallCount.class)));
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		Score score1 = (Score)o;
+		return Objects.equals(score, score1.score);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(score);
 	}
 }
