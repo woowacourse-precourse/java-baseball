9dc0e5e729c200cf98552feed24dce0d15e26d5a
dooboocookie <bkh3030@gmail.com>
2022-11-03 15:07:09 +0900

    feat: Round.java, 라운드에서 컴퓨터와 입력한 숫자를 비교하여 힌트를 제공하는 기능 구현

--- src/main/java/baseball/Round.java
+++ src/main/java/baseball/Round.java
@@ -2,24 +2,71 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Round {
 
     private Computer computer;
     private Numbers numbers;
 
+    private int countBall;
+    private int countStrike;
+
     public Round(Computer computer) {
         this.computer = computer;
-        startNewRound();
     }
 
-    private void startNewRound() {
+    public void startNewRound() {
         Print.printRoundStart();
         String inputString = Console.readLine();
+        resetScore();
         inputNewNumbers(inputString);
     }
 
     private void inputNewNumbers(String inputString) {
         int input = Integer.parseInt(inputString);
         numbers = new Numbers(input);
+    }
+
+    private void resetScore() {
+        this.countBall = 0;
+        this.countStrike = 0;
+    }
+
+    public void playRound() {
+        List<Hint> hints = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            hints.add(getHint(i));
+        }
+    }
+
+    private Hint getHint(int index) {
+        if (isStrike(index)) {
+            return Hint.STRIKE;
+        }
+        if (isBall(index)) {
+            return Hint.BALL;
+        }
+        return Hint.NOTHING;
+    }
+
+    private boolean isBall(int index) {
+        // 이전 인덱스 : 0 -> 2
+        int prevIndex = (index + 2) % 3;
+        // 이후 인덱스 : 2 -> 0;
+        int nextIndex = (index + 1) % 3;
+
+        boolean isPrevBall = computer.findComputerNumber(prevIndex)
+                .equals(numbers.findNumber(index));
+        boolean isNextBall = computer.findComputerNumber(nextIndex)
+                .equals(numbers.findNumber(index));
+
+        return  isPrevBall || isNextBall;
+    }
+
+    private boolean isStrike(int index) {
+        return computer.findComputerNumber(index)
+                .equals(numbers.findNumber(index));
     }
 }
