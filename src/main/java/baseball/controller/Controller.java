01f2a4784f99448aacc6f032324bbe6a6bd5fdff
소대훈 <theogns0681@naver.com>
2022-11-03 09:57:23 +0900

    [refactor] 로직 수정

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -6,34 +6,40 @@ import baseball.view.View;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Controller {
-    Model model= new Model();
+    Model model = new Model();
     View view = new View();
     Ball ball = new Ball();
-    public void start(){
+
+    public void start() {
         view.startMention();
         while (true) {
             view.printInput();
             String input = Console.readLine();
+
             if (!model.findException(input)) throw new IllegalArgumentException();
+
             int inp = Integer.parseInt(input);
             int Strike = model.findStrike(ball.getScore(), inp);
             int Ball = model.findBall(ball.getScore(), inp);
-            if (Strike == 3){
+
+            if (Strike == 3) {
                 view.threeStrike();
                 String in = Console.readLine();
+
                 if (model.InputException(in)) throw new IllegalArgumentException();
-                if ("1".equals(in)){
+
+                if ("1".equals(in)) {
                     ball = new Ball();
-                }else if ("2".equals(in)){
+                } else if ("2".equals(in)) {
                     break;
                 }
             } else if (Ball != 0 && Strike != 0) {
-                view.strikeAndBall(Strike,Ball);
-            } else if (Ball != 0){
+                view.strikeAndBall(Strike, Ball);
+            } else if (Ball != 0) {
                 view.ball(Ball);
-            }else if (Strike !=0){
+            } else if (Strike != 0) {
                 view.strike(Strike);
-            }else if (Ball == 0 && Strike == 0){
+            } else if (Ball == 0 && Strike == 0) {
                 view.incorrect();
             }
         }
