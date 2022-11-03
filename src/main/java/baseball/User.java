cd6c9d95cd0e9e3218b1c4e02dfee7c87d13a437
miffyKing <whqudghk28@gmail.com>
2022-11-04 01:35:03 +0900

    feat: Make User Input Validation - is it Number?

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -5,21 +5,32 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class User {
 
   int value;
+  static String inputNumInString;
+
   User () {
-  }
 
+  }
   public static String inputValue() {
-
-    return (validateInput("123"));
+    inputNumInString = readLine();
+    return (validateInput());
   }
 
-  static String validateInput(String str) {
-    if (str == "123") {
-      System.out.println(str);
-      return str;
+  static String validateInput() {
+    checkInputIsNum();
+
+//    if (str == "123") {
+//      System.out.println(str);
+//      return str;
+//    }
+//    System.out.println("EEE");
+    return inputNumInString;
+  }
+
+  static void checkInputIsNum() {
+    try {
+      int number = Integer.parseInt(inputNumInString);
+    } catch (NumberFormatException ex) {
+      ex.printStackTrace();
     }
-    System.out.println("EEE");
-    return "EEE";
   }
-
 }
