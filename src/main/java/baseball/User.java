c03f84e1be79e06dce8d89eae4a378fbdfaa24d7
donghae <donghae1999@gmail.com>
2022-11-03 17:47:39 +0900

    feat: add verifyUserNumber

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -5,18 +5,52 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 
 public class User {
+    public static ArrayList<Integer> userNumber = new ArrayList<>();
     public static ArrayList<Integer> setUserNumber() {
-        ArrayList<Integer> userNumber = new ArrayList<>();
+        String answer;
 
-        while(userNumber.size()<3){
-            String answer;
-            inputMessage();
-            answer = Console.readLine();
-        }
+        inputMessage();
+        answer = Console.readLine();
+
+        verifyUserNumber(answer);
 
         return userNumber;
     }
     private static void inputMessage(){
-        System.out.println("숫자를 입력해 주세요: ");
+        System.out.print("숫자를 입력해 주세요: ");
+    }
+
+    private static void verifyUserNumber(String answer){
+        try {
+            checkSize(answer);
+            checkDigit(answer);
+            addUserNumber(answer);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.toString());
+        }
+
+    }
+    private static void addUserNumber(String answer)throws IllegalArgumentException{
+        for(int i=0; i<answer.length(); i++){
+            int answerChar = answer.charAt(i)-'0';
+            if(userNumber.contains(answerChar)){
+                throw new IllegalArgumentException("User Number dup error");
+            }
+            userNumber.add(answerChar);
+        }
+    }
+    private static void checkSize(String answer) throws IllegalArgumentException{
+        if(answer.length()!=3){
+            throw new IllegalArgumentException("Size error");
+        }
+    }
+
+    private static void checkDigit(String answer) throws IllegalArgumentException{
+        for(int i=0; i<answer.length(); i++){
+            char answerNum = answer.charAt(i);
+            if(answerNum<48 || answerNum>57){
+                throw new IllegalArgumentException("Digit error");
+            }
+        }
     }
 }
