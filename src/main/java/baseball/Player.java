aad111a6425430ccb45d1a6b388549737bd8f83c
khsrla9806 <kimhunsope@naver.com>
2022-11-03 23:24:18 +0900

    style(Player): 사용하지 않는 주석 제거

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -11,7 +11,6 @@ public class Player {
     public void EnterNumberOfPlayer() {
         System.out.print("숫자를 입력해주세요 : ");
         String[] enteredNumber = Console.readLine().split("");
-        // 입력된 숫자가 올바른지 테스트하는 부분 (예외 처리, 중복된 숫자가 입력되었는지)
         playersNumber = Arrays.asList(enteredNumber);
     }
 }
