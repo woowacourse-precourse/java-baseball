9db0bc81c8d45d23ed9318b47eec189a3b5f0744
bsh1603 <bsh1603@github.com>
2022-11-03 13:59:56 +0900

    feat: user 숫자 입력받아 배열 저장 기능 추가

---
+++ src/main/java/domain/UserNumber.java
@@ -0,0 +1,13 @@
+package domain;
+
+public class UserNumber {
+    int[] user_numbers;
+
+    public int[] getUser_numbers() {
+        return user_numbers;
+    }
+
+    public void setUser_numbers(int[] user_numbers) {
+        this.user_numbers = user_numbers;
+    }
+}
