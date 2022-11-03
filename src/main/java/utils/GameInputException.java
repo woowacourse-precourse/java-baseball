68f302c88744be090b8b81fb5e3b693f6b0440f5
KSH <tkdgns543@gmail.com>
2022-11-03 23:05:17 +0900

    feat(GameInputException): 입력이 주어진 길이(기본 3)아닐 경우 예외 발생 메서드 구현

--- src/main/java/utils/GameInputException.java
+++ src/main/java/utils/GameInputException.java
@@ -8,4 +8,11 @@ public class GameInputException {
     GameInputException(int numberLength) {
         this.numberLength = numberLength;
     }
+
+    private void checkNumberLength(String userInput) throws IllegalArgumentException{
+        if (userInput.length() == numberLength) {
+            return;
+        }
+        throw new IllegalArgumentException();
+    }
 }
