260f3408645a4e899cd9c0cfedcc8791a3f9eea4
Gupeng-1021 <87390396+Gupeng-1021@users.noreply.github.com>
2022-11-04 00:20:58 +0900

    feat: Random값 추출하는 RandomNum

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
     //잘못된 입력을 받을 경우
     public static void IllegalInput(int input) {
         //입력이 3자리의 숫자가 아님
@@ -9,13 +15,20 @@ public class Application {
         }
     }
 
+    //랜덤값을 추출
+    public static List<Integer> RandomNum(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     public static void main(String[] args) {
         System.out.print("숫자 야구 게임을 시작합니다.");
+
     }
 }
-
-
-//구현 목록
-//잘못된 입력을 처리하는 함수
-//입력과 값이 맞는 경우인지를 확인하는 함수
-//게임 시작 문구를 출력하는 함수 -> 다 맞으면 함수 다시 출력
