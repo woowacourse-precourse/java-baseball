4b20cf22e8cf474ca714132ed97babec251ddc29
Lee Sang Bin <rhkd2612@naver.com>
2022-11-03 18:12:18 +0900

    refactor: check Strike add variable

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -57,7 +57,7 @@ public class Application {
     private static boolean isNewGame(List<Integer> computer, HashMap<Integer, Integer> computerIndexMap) {
         String userInput;
         boolean loopFlag;
-        
+
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
@@ -103,7 +103,9 @@ public class Application {
         int strikeCount = 0;
 
         for (int idx = 0; idx < userInput.length(); idx++) {
-            if(userInput.charAt(idx) - '0' == computer.get(idx)){
+            int userNumber = userInput.charAt(idx) - '0';
+
+            if(userNumber == computer.get(idx)){
                 strikeCount++;
             }
         }
