fb6c0b8ec3d5903d232222b0e0ce0333e4a9cd9d
JHS <wnghdtjr129@naver.com>
2022-11-03 16:12:32 +0900

    test: GameNumber 변경으로 인한 테스트코드 변경

--- src/test/java/baseball/utils/GameNumberGeneratorTest.java
+++ src/test/java/baseball/utils/GameNumberGeneratorTest.java
@@ -1,9 +1,13 @@
 package baseball.utils;
 
+import baseball.model.GameNumber;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -11,8 +15,17 @@ class GameNumberGeneratorTest {
     @Test
     @DisplayName("서로다른 3자리 자연수 생성")
     void createGameNumber(){
-        List<Integer> gameNumber = GameNumberGenerator.generate();
-        assertThat(gameNumber.size()).isEqualTo(3);
-        assertThat(gameNumber).doesNotHaveDuplicates();
+        GameNumber gameNumber = GameNumberGenerator.generate();
+        List<Integer> gameNumberList = getGameNumber(gameNumber);
+
+        assertThat(gameNumberList.size()).isEqualTo(3);
+        assertThat(gameNumberList).doesNotHaveDuplicates();
+    }
+
+    private List<Integer> getGameNumber(GameNumber gameNumber) {
+        return IntStream.range(1, 9)
+                .filter(gameNumber::contains)
+                .boxed()
+                .collect(Collectors.toList());
     }
 }
