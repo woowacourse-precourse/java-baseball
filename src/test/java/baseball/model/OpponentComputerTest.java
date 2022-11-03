b59109bd1f81702dac6ffd3b1a369dede174dd36
ksh-beginner <ohk9134@naver.com>
2022-11-03 15:43:39 +0900

    test(OpponentComputerTest) : 상대방 생성과 동시에 랜덤 수 저장 테스트 공백 라인으로 문맥 분리 리팩토링
    변수 선언부와 검증부를 공백라인으로 문맥 분리하는 리팩토링

--- src/test/java/baseball/model/OpponentComputerTest.java
+++ src/test/java/baseball/model/OpponentComputerTest.java
@@ -13,6 +13,7 @@ public class OpponentComputerTest {
     void 상대방_생성과_동시에_랜덤_수_저장() {
         OpponentComputer opponentComputer = new OpponentComputer(RandomUtil.createRandomNumber());
         int opponentComputerNumber = opponentComputer.getNumber();
+        
         assertThat(opponentComputerNumber).isNotNull();
     }
 
