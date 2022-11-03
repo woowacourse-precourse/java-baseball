d70f37db4c982e2d0f0cfc10439a7c49b091bb79
Eungyu-code <Agyu625@gmail.com>
2022-11-03 19:56:26 +0900

    feat: startGame() 함수 추가
    "숫자 야구 게임을 시작합니다." 문구를 출력하는 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,8 +9,15 @@ public class Application {
 
     public static void playGame() {
 
+        startGame();
+        
         while(true) {
 
         }
+    }
+
+    public static void startGame() {
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
