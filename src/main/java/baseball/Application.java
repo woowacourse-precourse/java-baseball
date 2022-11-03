eae9e4e4e9ca21a5502b9beadcdb432a6e5b4ffb
ddogga <hayeonlee60327@gmail.com>
2022-11-03 17:02:21 +0900

    임의의 서로 다른 수 3개를 구하는 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,36 @@
 package baseball;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        List<Integer> computer_num = getRandomNumber();
+
+
+
+    }
+
+
+    public static List<Integer> getRandomNumber() {
+
+        List<Integer> num = new ArrayList<>();
+
+        while (num.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if (!num.contains(randomNumber)) {
+                num.add(randomNumber);
+            }
+        }
+
+        return num;
+
+
     }
 }
