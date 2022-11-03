f9ba9d7084be86c08f45f42749d83e15249354a3
HangSub999 <gkdtjq999@gmail.com>
2022-11-03 19:26:16 +0900

    feat: 숫자를 입력받는 기능

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -1,4 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Input {
+
+    public static String inputUserNumber() {
+        System.out.println("숫자를 입력해 주세요 : ");
+        return Console.readLine();
+    }
+
 }
