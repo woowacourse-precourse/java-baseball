d3611a2d83c79dfd488375117ab0bf99b9f7e876
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-03 21:45:38 +0900

    type:feat 3개 숫자에 대해 볼/스트라이크/아무것도 아님을 판정하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 public class Application {
     private static final int midGame = 1;
     private static final int endGame = 2;
+    private static int ball = 0;
+    private static int strike = 0;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -40,6 +42,21 @@ public class Application {
         if (gameState == endGame) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+    }
+
+    public static void findBallAndStrike (String userInput, String answer) {
+        for (int digit = 0; digit < 3; digit++) {
+            char presentNumber = userInput.charAt(digit);
+            int ballStrikeOrNot = answer.indexOf(presentNumber);
+            if (ballStrikeOrNot == digit) {
+                strike++;
+                continue;
+            }
+            if (ballStrikeOrNot == -1) {
+                continue;
+            }
+            ball+;
         }
     }
 }
