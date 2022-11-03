3378a5ede680b3d2a2f482d94fe73a4526344d21
ksh-beginner <ohk9134@naver.com>
2022-11-03 23:14:01 +0900

    feat(OutputView) : 입력한 수에 대해 게임 결과 메시지를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,4 +5,24 @@ public class OutputView {
     public static void printGameStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public static void printGameResultMessage(int strikeCount, int ballCount) {
+        if (strikeCount == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        }
+        if (strikeCount != 0 && ballCount != 0) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+        if (strikeCount != 0 && strikeCount != 3 && ballCount == 0) {
+            System.out.println(strikeCount + "스트라이크");
+        }
+        if (ballCount != 0 && strikeCount == 0) {
+            System.out.println(ballCount + "볼");
+        }
+    }
 }
