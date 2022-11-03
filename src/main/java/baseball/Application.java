7f2a70d6cb16c0a30b79858fb57da58a54c20d10
Jueun <jueun000101@gmail.com>
2022-11-03 17:48:43 +0900

    feat: input number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,15 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     static void printGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    static String inputNumber() {
+        System.out.println("숫자를 입력해주세요 : ");
+        return readLine();
     }
 
     public static void main(String[] args) {
