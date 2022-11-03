7e3656bd45242c5645006f44255af40efab6189e
squirMM <jsm6616@naver.com>
2022-11-03 16:16:59 +0900

    :rocket: refect(Computer): refectoring fun name

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -8,6 +8,6 @@ public class BaseballGame {
     private static final int STOP_GAME = 2;
 
     public static void baseballGame() {
-        List<Integer> balls = new Controller().makeBalls();
+        List<Integer> balls = new Controller().getBalls();
     }
 }
--- src/main/java/baseball/computer/Controller.java
+++ src/main/java/baseball/computer/Controller.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Controller {
     private final Computer computer = new Computer();
 
-    public List<Integer> makeBalls() {
+    public List<Integer> getBalls() {
         computer.makeBalls();
         return computer.getBaseballs();
     }
