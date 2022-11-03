84605ec08be976ec881c48858ab2cd10fd77f711
myungsikyoon <yms8795@gmail.com>
2022-11-03 16:05:04 +0900

    feat(Computer) : 인스턴스 변수와 생성자 선언.

--- src/main/java/baseball/player/Computer.java
+++ src/main/java/baseball/player/Computer.java
@@ -1,4 +1,20 @@
 package baseball.player;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Computer {
+
+    private List<Integer> computerNumberList;
+
+    public Computer() {
+        this.computerNumberList = generateRandomThreeNumbers();
+    }
+
+
+
+
 }
