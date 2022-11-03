beb4c55fe1e4778ae3e5e9dc91a882d9b548d15c
swandevson <shai4956@gmail.com>
2022-11-04 00:55:33 +0900

    Feat: BaseballNumber 중복된 번호 체크
    - 입력받은 숫자에 중복된 번호가 없는지 확인
    - 존재할 경우 true

--- src/main/java/baseball/BaseballNumber.java
+++ src/main/java/baseball/BaseballNumber.java
@@ -12,4 +12,15 @@ public class BaseballNumber {
     public void set(String input) {
         this.baseballNumber = input;
     }
+
+
+    public boolean containDuplicatedNumber() {
+        for (int index = 0; index < baseballNumber.length(); index++) {
+            if(baseballNumber.indexOf(baseballNumber.charAt(index)) != index) {     // 중복된 문자가 있는 경우 가장 앞의 문자를 반환하는 것을 이용
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
