ebd55de654e6c94e42f159228c8278acbf3493b5
neppiness <0414kjh@naver.com>
2022-11-04 00:28:18 +0900

    feat: generate answer number (src/main/java/baseball)
    Generate answer number:
    - Created public static generateAnswer() method
    - Return to variable answer

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
-package baseball;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+// package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static ArrayList<Integer> generateAnswer() {
+        ArrayList<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber))
+                answer.add(randomNumber);
+        }
+        return answer;
+    }
+    public static void main(String[] args) throws Exception {
+        ArrayList<Integer> answer = generateAnswer();
     }
 }
