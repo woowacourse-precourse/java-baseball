b1c386bb15b8c3b73357847a0cc461772bacc6ab
Kim woo jong <skekdnwhd@gmail.com>
2022-11-04 01:26:34 +0900

    Refactor : 메세지 enum으로 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,13 +6,16 @@ import baseball.utils.MakeRandom;
 
 import java.util.List;
 
+import static baseball.enums.Message.ENTER_NUMBER;
+import static baseball.enums.Message.GAME_START;
+
 public class Application {
     public static void main(String[] args) {
         ConsoleLog console = ConsoleLog.getInstance();
         MakeRandom makeRandom = MakeRandom.getInstance();
 
-        console.println("숫자 야구 게임을 시작합니다.");
+        console.println(GAME_START.message());
         List<Integer> computerNumbers = makeRandom.numbers();
-        List<Integer> userNumbers = UserInput.number("숫자를 입력해주세요 : ");
+        List<Integer> userNumbers = UserInput.number(ENTER_NUMBER.message());
     }
 }
