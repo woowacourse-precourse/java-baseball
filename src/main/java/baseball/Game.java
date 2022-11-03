181b76d697446baf45fd7f4076bfd71c1709ce3e
c4fiber <qudcjf153@gmail.com>
2022-11-03 21:55:56 +0900

    fix: 게임시작 문구출력 기능의 함수명을 직관적으로 변경
    gameStart 에서 game 부분이 중복된다고 판단함

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,10 +3,14 @@ package baseball;
 public class Game {
 
     public Game() {
-        gameStart();
+        title();
     }
 
-    private void gameStart() {
+    private void title() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void play() {
+        // TODO 게임 플레이
     }
 }
