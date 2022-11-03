2d355608d78da923f38de92d60646ec97520b388
WonJeongYeon <wonjungyoun@naver.com>
2022-11-03 10:28:05 +0900

    20221103 게임을 진행할 숫자를 Randoms.pickNumberInRange를 사용하여 추출하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> gameNumberList = pickGameNumber();
+        System.out.println(gameNumberList);
+    }
+
+    public static List<Integer> pickGameNumber() {
+        List<Integer> gameNumberList = new ArrayList<>();
+        while (gameNumberList.size() != 3) {
+            gameNumberList = checkNumber(gameNumberList);
+        }
+        return gameNumberList;
+    }
+
+    public static List<Integer> checkNumber(List<Integer> gameNumberList) {
+        int num = Randoms.pickNumberInRange(0, 9);
+        System.out.println(num);
+        if (!gameNumberList.contains(num)) {
+            gameNumberList.add(num);
+        }
+        System.out.println(gameNumberList);
+        return gameNumberList;
     }
 }
