eae8850ff64f9ef4bcdbcb3c28ec393def4b40ba
haisley77 <qhfg3145@naver.com>
2022-11-03 12:15:51 +0900

    feat(pickRandomNumber()): pick the random number for the answer

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,13 +5,25 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Application {
-    
+
+    private static List<Integer> computer = new ArrayList<>();
     private static void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    private static void pickRandomNumber() {
+        while (computer.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if (!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         start();
+        pickRandomNumber();
     }
 }
