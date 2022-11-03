a84635b46ed7d2b689bcb277b3f42c6ad0273eb2
JunHee <ljh19980823@gmail.com>
2022-11-03 21:57:32 +0900

    feat : 볼과 스트라이크의 개수를 가지고 맞는 결과를 출력하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -70,6 +70,20 @@ public class Application {
         return strike;
     }
 
+    public static void printResult(int ball, int strike) {
+        if (strike == 3) {
+            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+    }
+
 
     public static void main(String[] args) {
         List<Integer> computerNumber = makeRandomNumber();
@@ -84,6 +98,7 @@ public class Application {
             }
             int ball = countBall(computerNumber, userInputNumber);
             int strike = countStrike(computerNumber, userInputNumber);
+            printResult(ball, strike);
         }
     }
 }
