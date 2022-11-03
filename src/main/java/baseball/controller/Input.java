7e047fbd02892f7b908e5e38b95fe0f2f9b89240
iamminho <minho100227@kakao.com>
2022-11-03 11:36:12 +0900

    refactor(input): 입력 숫자의 반환 형태 List로 변경

--- src/main/java/baseball/controller/Input.java
+++ src/main/java/baseball/controller/Input.java
@@ -1,16 +1,21 @@
 package baseball.controller;
 
+import baseball.view.InputView;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Input {
-	public static String getGameNumber() {
+	public static List<Integer> getGameNumber() {
 		while (true) {
 			try {
+				InputView.printInputNumber();
 				String inputGameNumber = getInputGameNumber();
 				Exception.validOnlyOneToNine(inputGameNumber);
 				Exception.validNumberLength(inputGameNumber);
 				Exception.validOverlapNumber(inputGameNumber);
-				return inputGameNumber;
+				return stringToList(inputGameNumber);
 			} catch (IllegalArgumentException error) {
 				System.out.println(error);
 			}
@@ -19,5 +24,12 @@ public class Input {
 
 	private static String getInputGameNumber() {
 		return Console.readLine();
+	}
+
+	private static List<Integer> stringToList(String inputGameNumber) {
+		String[] gameNumberArray = inputGameNumber.split("");
+		return Arrays.stream(gameNumberArray)
+			.map(gameNumber -> Integer.parseInt(gameNumber))
+			.collect(Collectors.toList());
 	}
 }
