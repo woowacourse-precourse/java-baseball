ae418010a1ae0e6243a792d8ec9ae14fd7e2d053
myungsikyoon <yms8795@gmail.com>
2022-11-03 20:52:43 +0900

    feat(NumberComparison) : startComparison 구현.

--- src/main/java/baseball/system/NumberComparison.java
+++ src/main/java/baseball/system/NumberComparison.java
@@ -6,5 +6,27 @@ public class NumberComparison {
 
     List<Integer> computerNumbers;
     List<Integer> userNumbers;
-    
+
+    public boolean startComparison(List<Integer> userNumbers) {
+
+        setUserNumbers(userNumbers);
+
+        int strikeCount = getStrikeCount();
+        int ballCount = getBallCount();
+
+        if (isAnswer(strikeCount)) {
+            printAnswerText();
+            return true;
+        }
+
+        if(isNothing(strikeCount,ballCount)){
+            printNothingText();
+            return false;
+        }
+
+        printStrikeBallCount(strikeCount,ballCount);
+        return false;
+
+    }
+
 }
