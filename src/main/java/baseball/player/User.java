271f0fa1203f9a76327bcedabc23e4ce2f073707
myungsikyoon <yms8795@gmail.com>
2022-11-03 16:21:20 +0900

    feat(User) : 인스턴스 변수와 생성자 선언.

--- src/main/java/baseball/player/User.java
+++ src/main/java/baseball/player/User.java
@@ -1,4 +1,14 @@
 package baseball.player;
 
+import java.util.List;
+
 public class User {
+
+    private List<Integer> userNumberList;
+
+    User(String inputNumber){
+        this.userNumberList = generateNumberList(inputNumber);
+    }
+
+
 }
