4d5246abf5b44e66132a8a250bafe99893dadd1e
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-03 19:55:19 +0900

    type:feat 사용자의 입력을 받는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        String userInput = getUserInput (scanner);
+    }
+
+    public static String getUserInput (Scanner scanner) {
+        String userInput = scanner.nextLine();
+        return userInput;
     }
 }
