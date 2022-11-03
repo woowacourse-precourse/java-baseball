8e1e21cc28d544e1d3475178e7f1073792c17909
splitube <voicesplit@daum.net>
2022-11-03 21:33:33 +0900

    feat(NumberMaker): 3개의 숫자 리스트 반환 기능 구현
    원하는 범위 안의 서로다른 3개의 숫자를 가지고 있는 리스트 구현.
    문제 요구대로 camp 패키지에 있는 Randoms 클래스 사용하여 구현.

--- src/main/java/baseball/NumberMaker.java
+++ src/main/java/baseball/NumberMaker.java
@@ -11,4 +11,12 @@ public class NumberMaker {
     public int makeNumberInRange(int min, int max) {
         return Randoms.pickNumberInRange(min, max);
     }
+
+    public List<Integer> makeThreeDifferentNumberListInRange(int min, int max) {
+        Set<Integer> numberList = new HashSet<>();
+        while (numberList.size() < 3) {
+            numberList.add(makeNumberInRange(min, max));
+        }
+        return new ArrayList<>(numberList);
+    }
 }
