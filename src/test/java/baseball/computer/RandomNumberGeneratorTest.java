4c60b5dd8faa8f510b2abf848fe6b36fa20dfd85
kimminsu <xjfcnfw3@naver.com>
2022-11-03 12:43:19 +0900

    test: 컴퓨터 난수의 각 자릿수가 0이 되지 않는지 테스트

--- src/test/java/baseball/computer/RandomNumberGeneratorTest.java
+++ src/test/java/baseball/computer/RandomNumberGeneratorTest.java
@@ -15,6 +15,14 @@ public class RandomNumberGeneratorTest {
         }
     }
 
+    @Test
+    void 난수_0_확인() {
+        for (int testNumbers = 0; testNumbers < 100; testNumbers++) {
+            List<Integer> gameNumbers = RandomNumberGenerator.generateRandomNumbers();
+            assertThat(gameNumbers.contains(0)).isEqualTo(false);
+        }
+    }
+
     private static boolean isDigitAOtherNumber() {
         List<Integer> gameNumbers = RandomNumberGenerator.generateRandomNumbers();
         HashSet<Integer> numberSet = new HashSet<>();
