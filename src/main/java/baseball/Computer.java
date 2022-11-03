6e38967de13a763cef66cbdac64204850c0424cc
khsrla9806 <kimhunsope@naver.com>
2022-11-03 20:49:52 +0900

    feat(Computer): 입력값과 비교해 결과를 내는 compareWithNumberOfPlayer 함수 구현 Computer 생성자에서 pickNumber를 문자열로 바꾸는 부분 코드 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,7 +1,10 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer {
@@ -10,9 +13,39 @@ public class Computer {
     public Computer() {
         while (computersNumber.size() < 3) {
             int pickNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computersNumber.contains(pickNumber)) {
+            if (!computersNumber.contains(String.valueOf(pickNumber))) {
                 computersNumber.add(computersNumber.size(), String.valueOf(pickNumber));
             }
         }
+    }
+
+    public void compareWithNumberOfPlayer(List<String> playersNumber) {
+        Map<String, Integer> strikeBallMap = new HashMap<>();
+        for (String numberOfPlayer : playersNumber) {
+            int indexOfComputersNumber = computersNumber.indexOf(numberOfPlayer);
+            int indexOfPlayersNumber = playersNumber.indexOf(numberOfPlayer);
+            if (indexOfComputersNumber == -1) {
+                continue;
+            }
+            if (indexOfComputersNumber == indexOfPlayersNumber) {
+                strikeBallMap.put("스트라이크", strikeBallMap.getOrDefault("스트라이크", 0) + 1);
+            } else {
+                strikeBallMap.put("볼", strikeBallMap.getOrDefault("볼", 0) + 1);
+            }
+        }
+
+        if (strikeBallMap.getOrDefault("볼", 0) != 0) {
+            System.out.print(strikeBallMap.get("볼") + "볼 ");
+        }
+
+        if (strikeBallMap.getOrDefault("스트라이크", 0) != 0) {
+            System.out.print(strikeBallMap.get("스트라이크") + "스트라이크");
+        }
+
+        if (!strikeBallMap.containsKey("볼") && !strikeBallMap.containsKey("스트라이크")) {
+            System.out.print("낫싱");
+        }
+
+        System.out.println();
     }
 }
