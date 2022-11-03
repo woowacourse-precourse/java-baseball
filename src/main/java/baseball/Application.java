c5090bef1bd79de024cbe551136d9e91d841da01
penrose15 <seongju99@naver.com>
2022-11-03 17:33:15 +0900

    feat: add hint about entered numbers at loop in Application.java
    Resolves: #6

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,24 +10,29 @@ public class Application {
         // TODO: 프로그램 구현
         StartGame startGame = new StartGame();
         GenerateRandomNumber generateRandomNumber = new GenerateRandomNumber();
+        HintsAboutNumbers hint = new HintsAboutNumbers();
 
         Scanner sc = new Scanner(System.in);
-        int randomNumber = generateRandomNumber.generateRandomNumber();
         int clientNumber;
-
-        System.out.println(randomNumber);
 
         startGame.printStartGameMessage();
         int quitGame;
+
+        boolean isMatched;
         do{
+            int randomNumber = generateRandomNumber.generateRandomNumber();
+            System.out.println(randomNumber);
             do {
                 startGame.printInputThreeNumber();
                 clientNumber = sc.nextInt(); //사용자 숫자 입력
 
+                isMatched = hint.hintAboutNumbers(clientNumber, randomNumber);
                 //숫자 입력시 힌트 출력 메서드 추가
 
-            } while (clientNumber != randomNumber); //입력한 숫자와 컴퓨터의 숫자가 같을때까지 반복
+            } while (!isMatched); //입력한 숫자와 컴퓨터의 숫자가 같을때까지 반복
 
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
             quitGame = sc.nextInt();
             while(!(quitGame == 1 || quitGame ==2)) { //1 또는 2 입력할 때까지 반복
