4b6ea23e177f2c34089c58d064c09114e5917b69
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-03 16:42:36 +0900

    feat : 테스트 기능 구현 - LinkedHashMap에서 key, value 추출 기능 확인

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -167,6 +167,34 @@ class ApplicationTest extends NsTest {
 	}
 
 
+	@DisplayName("LinkedHashMap에서 key 값을 꺼내는 기능을 확인한다")
+	@Test
+	void canExtractKey() {
+		LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>(){
+			{
+				put(1, 0);
+				put(2, 1);
+				put(3, 2);
+			}
+		};
+		assertThat(game.extractNumber(map,0)).isEqualTo(1);
+	}
+
+	@DisplayName("LinkedHashMap에서 value 값을 꺼내는 기능을 확인한다")
+	@Test
+	void canExtractValue() {
+		LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>(){
+			{
+				put(1, 0);
+				put(2, 1);
+				put(3, 2);
+			}
+		};
+		assertThat(game.extractIndex(map,0)).isEqualTo(0);
+	}
+
+
+
 	@Test
 	void 게임종료_후_재시작() {
 		assertRandomNumberInRangeTest(
