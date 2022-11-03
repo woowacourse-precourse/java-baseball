1c72fb1a7154aaff918de1f46e9e03f5e7f0a47a
ksh-beginner <ohk9134@naver.com>
2022-11-03 15:01:50 +0900

    feat(OpponentComputer) : 상대방 객체 생성과 동시에 랜덤 수 저장하는 기능 구현
    생성자 파라미터로 랜덤 수를 받아서 OpponentComputer가 가진
    number 필드에 주입하는 형식으로 랜덤 수 저장

--- src/main/java/baseball/model/OpponentComputer.java
+++ src/main/java/baseball/model/OpponentComputer.java
@@ -2,4 +2,9 @@ package baseball.model;
 
 public class OpponentComputer {
 
+    private int number;
+
+    public OpponentComputer(int randomNumber) {
+        this.number = randomNumber;
+    }
 }
