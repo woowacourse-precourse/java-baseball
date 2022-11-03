163de31f2b3f81da39b72d0b03df5cdec2ab0eb4
bsh1603 <bsh1603@github.com>
2022-11-03 11:31:12 +0900

    feat: random 숫자 3개 생성 기능 추가

---
+++ src/main/java/domain/RandomNumber.java
@@ -0,0 +1,27 @@
+package domain;
+
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumber {
+
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int NUM_SIZE = 3;
+
+    private static ArrayList<Integer> random_numbers;
+
+    public RandomNumber() {
+
+    }
+    public ArrayList<Integer> setRandomNumber(){
+        while(random_numbers.size() < NUM_SIZE){
+            random_numbers.add(getRandomNumber());
+        }
+        return random_numbers;
+    }
+
+    public int getRandomNumber(){
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+    }
+}
