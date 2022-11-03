c23b014a7f7a8bfc00ebfbf54f6cbb7f34558245
ghchoi <ghchoi0427@gmail.com>
2022-11-03 12:20:51 +0900

    feat: 숫자 입력 유효성 검사

---
+++ src/main/java/baseball/validation/InputValidation.java
@@ -0,0 +1,26 @@
+package baseball.validation;
+
+public class InputValidation {
+
+    public void validateNumber(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        if (!isNumeric(input)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isNumeric(String input) {
+        if (input == null) {
+            return false;
+        }
+        try {
+            Double.parseDouble(input);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+}
