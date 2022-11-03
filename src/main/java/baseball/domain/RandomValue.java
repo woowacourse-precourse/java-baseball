df220a5631782e227eae7242925eeebe24b7560f
ywj9811 <ywj9811@naver.com>
2022-11-04 02:31:41 +0900

    feat: RandomValue 클래스 생성
    domain패키지 내부에 랜덤값을 생성하는 클래스 생성 및 작성

---
+++ src/main/java/baseball/domain/RandomValue.java
@@ -0,0 +1,25 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomValue {
+    private final static int MIN_NUM = 1;
+    private final static int MAX_NUM = 9;
+    private final static int MAX_SIZE = 3;
+
+    public List<Integer> randomValue() {
+        List<Integer> RandomNum = new ArrayList<>();
+
+        while (RandomNum.size() < MAX_SIZE) {
+            int rdNum = Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
+            if (!RandomNum.contains(rdNum)){
+                RandomNum.add(rdNum);
+            }
+        }
+
+        return RandomNum;
+    }
+}
