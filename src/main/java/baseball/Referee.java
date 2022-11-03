599cc83691d23b030ce4b988ebe8eb49b6d7e2b9
splitube <voicesplit@daum.net>
2022-11-03 21:52:45 +0900

    feat(Referee): 게임의 정답을 심판이 저장하는 기능 구현

---
+++ src/main/java/baseball/Referee.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import java.util.List;
+
+class Referee {
+    List<Integer> answer;
+
+    public void setAnswer(List<Integer> answer) {
+        this.answer = answer;
+    }
+}
