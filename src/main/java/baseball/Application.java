f16efdee323eeff7a7a82c844775b6e43fe4707a
jisoolee7101@gmail.com <phllo154@naver.com>
2022-11-03 15:02:02 +0900

    임의의 중복되지 않는 세 자리수 생성 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(CreateThreeNumsOfComputer());
+    }
+
+    public static List<Integer> CreateThreeNumsOfComputer() {
+        List<Integer> numsOfComputer = new ArrayList<>();
+
+        while(numsOfComputer.size() < 3) {
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if(numsOfComputer.contains(randomNum) == false) {
+                numsOfComputer.add(randomNum);
+            }
+        }
+        return numsOfComputer;
     }
 }
