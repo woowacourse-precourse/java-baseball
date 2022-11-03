575adfbeb5a3c8a2e54cc9121a6c0834454e35f1
alstjr706 <alstjr706@naver.com>
2022-11-04 08:19:02 +0900

    feat : (숫자 야구) 컴퓨터수와 플레이어수 중에서 같은수가 있는지 구현

--- src/main/java/baseball/domain/GameJudgment.java
+++ src/main/java/baseball/domain/GameJudgment.java
@@ -1,4 +1,18 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class GameJudgment {
+
+    public int compare(List<Integer> computer, List<Integer> player) {
+        int correctCount = 0;
+        for (int playerIndex = 0; playerIndex < player.size(); playerIndex++) {
+            Integer playerNumber = player.get(playerIndex);
+
+            if (computer.contains(playerNumber)) {
+                correctCount++;
+            }
+        }
+        return correctCount;
+    }
 }
