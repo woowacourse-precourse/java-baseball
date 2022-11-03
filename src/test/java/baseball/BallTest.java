f186e19370561f47768937c7dac5253952c8095a
sangwon <swchoi1997@naver.com>
2022-11-03 11:11:42 +0900

    test: 컴퓨터 야구공 번호 생성 테스트
    컴퓨터의 공 번호가 3 자리인지 확인

--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -8,11 +8,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BallTest {
-
+	Ball ball = Ball.getInstance();
 	List<String> range = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9");
+
 	@Test
 	void 랜덤_공번호_생성() {
-		Ball ball = Ball.getInstance();
 		assertTrue(range.contains(ball.createRandomBall()));
+	}
+
+	@Test
+	void 컴퓨터_공번호_3자리_확인() {
+		Assertions.assertThat(ball.makeComputerBalls().size()).isEqualTo(3);
 	}
 }
