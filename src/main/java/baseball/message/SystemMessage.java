a71943395b85c1afe39dccb90a6df083598cbabb
minju412 <ln8847@ajou.ac.kr>
2022-11-04 01:22:24 +0900

    feat: define system message as constant
    Added new event to $browser:
    - define the system message to proceed with the game as a constant

---
+++ src/main/java/baseball/message/SystemMessage.java
@@ -0,0 +1,11 @@
+package baseball.message;
+
+public final class SystemMessage {
+    public static final String START_NUMBER_BASEBALL_GAME = "숫자 야구 게임을 시작합니다.";
+    public static final String ENTER_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+    public static final String THREE_NUMBERS_RIGHT_GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String START_NEW_GAME_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
