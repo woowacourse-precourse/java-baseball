8e94be17a925e3a4593497232f1d4378262ddd46
kevstevie <jianna442@gmail.com>
2022-11-03 15:46:06 +0900

    feat: RESTART 메시지 추가, 분기문 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,8 +8,9 @@ public class Application {
             Computer opponent = new Computer();
             do {
                 opponent.checkAnswer(User.userInput());
-            } while (opponent.isPlaying);
+            } while (!opponent.isPlaying);
             System.out.println(Message.CORRECT_ANSWER);
+            System.out.println(Message.RESTART);
         } while (User.restart());
     }
 }
