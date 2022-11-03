ff7a7ba717f595b21da9002970bb670aa7849969
kevstevie <jianna442@gmail.com>
2022-11-03 17:20:44 +0900

    feat: 변수명 명확하게 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,7 @@ public class Application {
             Computer opponent = new Computer();
             do {
                 opponent.checkAnswer(User.userInput());
-            } while (!opponent.isPlaying);
+            } while (!opponent.isCorrect);
             System.out.println(Message.CORRECT_ANSWER);
             System.out.println(Message.RESTART);
         } while (User.restart());
