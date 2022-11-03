264fa857b699183ca43e9db7951e0a3ac0712579
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-03 19:30:32 +0900

    Feat: 1부터 9까지의 서로 다른 세자리수를 생성하는 메서드 구현
    두 가지 메서드로 분리했습니다.
    makeRandomNumber() 메서드로 1부터 9까지의 난수 10개를 List에 넣어주었습니다. 이때 리스트의 길이는 길어도 무관하지만 10개면 충분하다고 생각했습니다.
    removeRepetitionNumber() 의 매개변수에 위 리스트를 넣어주고 중복을 제거하여 세자리 숫자를 서로 다른 수로 만들어 줍니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int randomNumber = makeRandomNumber();
+        System.out.println(randomNumber);
+    }
+
+
+    public static int makeRandomNumber() {
+        List<Integer> numList = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            int num = (int) ((Math.random() * 9) + 1);
+            numList.add(num);
+        }
+        return removeRepetitionNumber(numList);
+    }
+
+    public static int removeRepetitionNumber(List<Integer> numList) {
+        String tmp = "";
+
+        for (int i = 0; i < numList.size()-1; i++) {
+            if (numList.get(i) != numList.get(i+1)) tmp += numList.get(i);
+        }
+
+        tmp = tmp.substring(0,3);
+        return Integer.parseInt(tmp);
     }
 }
