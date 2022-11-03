aa483dc382b50fe9150d3b135fff4d3c0e873118
jkde7721 <jkde7721@naver.com>
2022-11-03 14:40:39 +0900

    feat: 숫자 야구 게임 시작 구현
    setup → play → gameOver 3단계로 진행

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -3,6 +3,20 @@ package baseball;
 public abstract class NumberBaseballGame {
 
     public static void start() {
-        //TODO: 게임 구현
+        setup();
+        play();
+        gameOver();
+    }
+
+    private static void setup() {
+        //TODO: 게임 준비 구현
+    }
+
+    private static void play() {
+        //TODO: 게임 진행 구현
+    }
+
+    private static void gameOver() {
+        //TODO: 게임 종료 구현
     }
 }
