b6e4fe7173998b296e0ddfe29c136edcbedb6caa
hojunking96 <junsong96@naver.com>
2022-11-03 16:39:28 +0900

    feat : 3자리 수 사용자 입력, 저장 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,15 +2,31 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String userInput;
+        List<Integer> guess = new ArrayList<>();
+        try {
+            userInput = br.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        for (int i = 0; i < userInput.length(); i++) {
+            guess.add(userInput.charAt(i) - '0');
+        }
+
         // TODO: 프로그램 구현
-        List<Integer> computer;
-        computer = makeRandomNum();
+        List<Integer> answer;
+        answer = makeRandomNum();
     }
+
     public static List<Integer> makeRandomNum() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
