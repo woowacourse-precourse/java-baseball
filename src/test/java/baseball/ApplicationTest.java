bae947dd801605409574f382d896932a40b00322
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-03 20:05:26 +0900

    Feat: 서로다른 세 자리 랜덤한 숫자 테스트케이스 작성
    랜덤한 수의 테스트 케이스 작성 방법을 잘 모르겠습니다.
    여기에 기능 구현을 모두 마치고 리팩토링 단계에서 더 고민해볼 예정입니다.

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,12 +3,33 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+    @Test
+    void listNumber1() {
+        List<Integer> list = List.of(1,2,3,4,5,6,7,8,9);
+        assertThat(Application.removeRepetitionNumber(list)).isEqualTo(123);
+    }
+
+    @Test
+    void listNumber2() {
+        List<Integer> list = List.of(1,2,2,2,2,2,2,2,2,3);
+        assertThat(Application.removeRepetitionNumber(list)).isEqualTo(123);
+    }
+
+    @Test
+    void listNumber3() {
+        List<Integer> list = List.of(1,1,2,2,3,4,5);
+        assertThat(Application.removeRepetitionNumber(list)).isEqualTo(123);
+
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
