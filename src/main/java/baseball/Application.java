0739063c9a489e356be472072c5fd448686f84d1
lee <whddlsk123@naver.com>
2022-11-04 02:50:51 +0900

    feat(checkStrike) : 입력받은 숫자의 스트라이크 갯수 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,19 +6,34 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    static List<String> computer;
+    static List<Character> computerNumber;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
 
-    public void initComputer() {
-        computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            String randomNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
+    public static void initComputerNumber() {
+        computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            Character randomNumber = Character.forDigit(Randoms.pickNumberInRange(1, 9), 10);
 
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
             }
         }
+    }
+
+    public static int checkStrike(String userInput) {
+        int strikeCount = 0;
+
+        for (int place = 0; place < userInput.length(); place++) {
+            Character computerNum = computerNumber.get(place);
+            Character userNum = userInput.charAt(place);
+
+            if(computerNum.equals(userNum)) {
+                strikeCount++;
+            }
+        }
+
+        return strikeCount;
     }
 }
--- src/test/java/baseball/MyTest.java
+++ src/test/java/baseball/MyTest.java
@@ -1,21 +1,55 @@
 package baseball;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class MyTest {
+//    @BeforeEach
+//    void initComputerNumber() {
+//        Application application = new Application();
+//        application.initComputerNumber();
+//    }
+
     @Test
-    void initComputerTest() {
+    void computerNumberSizeTest() {
         //given(준비)
-        Application application = new Application();
-
+        Application.initComputerNumber();
+        List<Character> computerNumber = Application.computerNumber;
         //when(실행)
-        application.initComputer();
-
-
+        
         //then(검증)
-        for(String number : Application.computer) {
-            System.out.println(number);
+        Assertions.assertThat(computerNumber).size().isEqualTo(3);
+    }
+
+    @Test
+    void computerNumberRangeTest() {
+        //given(준비)
+        Application.initComputerNumber();
+        List<Character> computerNumber = Application.computerNumber;
+        //when(실행)
+        
+        //then(검증)
+        for (int i = 0; i < 3; i++) {
+            Assertions.assertThat(computerNumber.get(i)).isBetween('1','9');
         }
     }
+
+    @Test
+    void checkStrike() {
+        //given(준비)
+        Application.computerNumber = List.of('1','2','3');
+
+        //when(실행)
+        int strikeCount = Application.checkStrike("456");
+
+        //then(검증)
+        Assertions.assertThat(strikeCount).isEqualTo(0);
+    }
+
+    
+
 
 }
