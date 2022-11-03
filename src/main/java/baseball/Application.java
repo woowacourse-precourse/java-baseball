c6f6a2a99c0f34f392e9ee9042c3c8f77fbfcbd9
eunji53 <duqtj00@naver.com>
2022-11-03 13:44:59 +0900

    <feat>:정답과 입력값 비교 후 결과 출력
    자리와 숫자 모두 맞으면 스트라이크+1
    자리는 틀리고 숫자만 맞으면 볼+1

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -19,6 +19,28 @@ public class Application {
         String num = readLine();
         return num;
     }
+    public static int Check(String num, List<Integer> answer){
+        int strike=0;
+        int ball=0;
+        for (int i=0;i<answer.size();i++) {
+            if (answer.get(i) == (int)num.charAt(i)){
+                strike += 1;
+            } else if (answer.contains((int)num.charAt(i))){
+                ball += 1;
+            }
+        }
+        if (strike==0 && ball==0){
+            System.out.println("낫싱");
+        } else if (strike==0){
+            System.out.println(ball+"볼");
+        } else if (ball==0) {
+            System.out.println(strike+"스트라이크");
+        } else {
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
+
+        return strike;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
