8fd9fba08682298adf0b10e4b884c3fb2f88b0de
jaehee329 <jaehee329@yonsei.ac.kr>
2022-11-03 16:02:26 +0900

    feat(InputValidator): 응답 문자열의 중복을 확인하는 로직 추가

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -4,17 +4,30 @@ public class InputValidator {
 
 	public void validateAnsString(String ansString) throws IllegalArgumentException {
 		checkStringLength(ansString);
-		checkisNumber(ansString);
+		checkIsNumber(ansString);
+		checkIsDuplicated(ansString);
 	}
 
-	public void checkStringLength(String string) {
+	private void checkStringLength(String string) {
 		if (string.length() != 3) {
 			throw new IllegalArgumentException();
 		}
 	}
 
-	public void checkisNumber(String string) {
+	private void checkIsNumber(String string) {
 		if (!string.matches("[+-]?\\d*(\\.\\d+)?")) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void checkIsDuplicated(String string) {
+		boolean[] used = new boolean[10];
+		for (char c : string.toCharArray()) {
+			int idx = c-'0';
+			if (!used[idx]) {
+				used[idx] = true;
+				continue;
+			}
 			throw new IllegalArgumentException();
 		}
 	}
