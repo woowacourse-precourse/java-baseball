a91703156bf93d934174b428b6450dab4a9f629e
49ehyeon42 <49ehyeon42@gmail.com>
2022-11-03 21:12:33 +0900

    test: 볼 개수와 스트라이크 개수를 통해 결과 문자열을 반환 메서드 테스트

--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -124,5 +125,89 @@ class BaseballGameTest {
 
         // then
         Assertions.assertThat(check).isEqualTo(false);
+    }
+
+    @Test
+    void 볼_개수와_스트라이크_개수를_통한_결과_문자열_3스트라이크() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+        List<Integer> computer = List.of(1, 2, 3);
+        List<Integer> user = List.of(1, 2, 3);
+
+        // when
+        String resultString = baseballGame.getAnswerString(computer, user);
+
+        // when
+        Assertions.assertThat(resultString).isEqualTo("3스트라이크");
+    }
+
+    @Test
+    void 볼_개수와_스트라이크_개수를_통한_결과_문자열_2볼_1스트라이크() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+        List<Integer> computer = List.of(3, 5, 8);
+        List<Integer> user = List.of(3, 8, 5);
+
+        // when
+        String resultString = baseballGame.getAnswerString(computer, user);
+
+        // when
+        Assertions.assertThat(resultString).isEqualTo("2볼 1스트라이크");
+    }
+
+    @Test
+    void 볼_개수와_스트라이크_개수를_통한_결과_문자열_3볼() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+        List<Integer> computer = List.of(4, 2, 6);
+        List<Integer> user = List.of(6, 4, 2);
+
+        // when
+        String resultString = baseballGame.getAnswerString(computer, user);
+
+        // when
+        Assertions.assertThat(resultString).isEqualTo("3볼");
+    }
+
+    @Test
+    void 볼_개수와_스트라이크_개수를_통한_결과_문자열_2볼() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+        List<Integer> computer = List.of(4, 2, 6);
+        List<Integer> user = List.of(6, 4, 1);
+
+        // when
+        String resultString = baseballGame.getAnswerString(computer, user);
+
+        // when
+        Assertions.assertThat(resultString).isEqualTo("2볼");
+    }
+
+    @Test
+    void 볼_개수와_스트라이크_개수를_통한_결과_문자열_1볼() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+        List<Integer> computer = List.of(4, 2, 6);
+        List<Integer> user = List.of(6, 1, 3);
+
+        // when
+        String resultString = baseballGame.getAnswerString(computer, user);
+
+        // when
+        Assertions.assertThat(resultString).isEqualTo("1볼");
+    }
+
+    @Test
+    void 볼_개수와_스트라이크_개수를_통한_결과_문자열_낫싱() {
+        // given
+        BaseballGame baseballGame = new BaseballGame();
+        List<Integer> computer = List.of(4, 2, 6);
+        List<Integer> user = List.of(9, 8, 7);
+
+        // when
+        String resultString = baseballGame.getAnswerString(computer, user);
+
+        // when
+        Assertions.assertThat(resultString).isEqualTo("낫싱");
     }
 }
