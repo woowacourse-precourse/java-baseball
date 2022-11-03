ba0cd42ed1b56aaa57686a399295ee3546c57b41
JJ <jeongjae1996@naver.com>
2022-11-03 20:37:54 +0900

    feat: 정답을 맞춘 경우, 정해진 문구 출력
    아래의 문구가 출력됩니다.
    3개의 숫자를 모두 맞히셨습니다! 게임 종료
    게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -92,9 +92,9 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computer_num = createRandomNumber();
 
         while (true) {
-            List<Integer> computer_num = createRandomNumber();
             System.out.println(computer_num);
             System.out.print("숫자를 입력해주세요 : ");
             String guess_number = Console.readLine();
@@ -102,7 +102,12 @@ public class Application {
             int strike = countStrike(computer_num, user_num);
             int ball = countBall(computer_num, user_num, strike);
             System.out.println(setHint(ball, strike));
-            // break; // 기능 완전 구현 전까지 반복문을 탈출하기 위함.
+
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            }
+            break; // 기능 완전 구현 전까지 반복문을 탈출하기 위함.
         }
     }
 }
