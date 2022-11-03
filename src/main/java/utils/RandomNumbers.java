9dd23e280524d21caa17a2c2bc1a3398bb09baad
KSH <tkdgns543@gmail.com>
2022-11-03 22:07:28 +0900

    feat(RandomNumbers): 컴퓨터가 무작위 숫자 리스트를 채우고 반환하는 메서드 구현
    이 클래스의 최종 결과물
    외부에서 접근가능한 유일한 메서드

--- src/main/java/utils/RandomNumbers.java
+++ src/main/java/utils/RandomNumbers.java
@@ -16,6 +16,12 @@ public class RandomNumbers {
     }
     private List<Integer> computerNumbers = new ArrayList<>(numberLength);
 
+    public List<Integer> getComputerNumbers() {
+        fillListRandomNumber();
+
+        return computerNumbers;
+    }
+
     private void fillListRandomNumber() {
         while (computerNumbers.size() < numberLength) {
             addSoloNumber();
