f3977f205cb0b529b947c6d104ce230b1a366f7d
jeujeon <1132jjw@naver.com>
2022-11-03 16:32:38 +0900

    feat(OutputView): 게임 시작을 위한 문구를 출력한다
    출력을 위한 OutputView 클래스에 게임 시작 문구 출력 메서드를 생성했다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,11 +1,13 @@
 package baseball;
 
 public class BaseballGame {
+    private final OutputView outputView = new OutputView();
     public BaseballGame() {
     }
 
     public void gameStart() {
         while (true) {
+            outputView.printGameStart();
             Computer computer = new Computer();
             game();
         }
---
+++ src/main/java/baseball/OutputView.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class OutputView {
+    public static final String GAME_START_STR = "숫자 야구 게임을 시작합니다.";
+
+    public void printGameStart() {
+        System.out.println(GAME_START_STR);
+    }
+}
