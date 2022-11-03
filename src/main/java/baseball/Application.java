526abb8637d05ddfe01e84f03d0baa75bc1d9698
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-04 03:16:44 +0900

    feat: 컴퓨터의 랜덤한 세 자리수 생성
    서로 다른 세 자리 수 List에 추가하고 stream을 사용해 String 타입으로 변환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Application {
+    private static final int COMPUTER_THREE_DIGIT = 3;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> computer = new ArrayList<>();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (computer.size() < COMPUTER_THREE_DIGIT) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        String computerNumber = computer.stream().map(String::valueOf).collect(Collectors.joining());
     }
 }
