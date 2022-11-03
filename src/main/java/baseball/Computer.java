82d9efaba49197d79fcc51fa91a8f198618ecc87
khsrla9806 <kimhunsope@naver.com>
2022-11-03 17:50:46 +0900

    refactor(Computer): computersDigit -> computersNumber로 변수명 변경

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,13 +5,13 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer {
-    List<String> computersDigit = new ArrayList<>();
+    List<String> computersNumber = new ArrayList<>();
 
     public Computer() {
-        while (computersDigit.size() < 3) {
+        while (computersNumber.size() < 3) {
             int pickNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computersDigit.contains(pickNumber)) {
-                computersDigit.add(computersDigit.size(), String.valueOf(pickNumber));
+            if (!computersNumber.contains(pickNumber)) {
+                computersNumber.add(computersNumber.size(), String.valueOf(pickNumber));
             }
         }
     }
