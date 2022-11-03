65e3b1e1a0e4cd3ac639eef00ba3244e9f0dbdd7
p <gustjr2332@naver.com>
2022-11-03 20:52:56 +0900

    feat(NumberList): 랜덤으로 생성한 숫자와 입력받은 숫자의 저장소

---
+++ src/main/java/baseball/Number/NumberList.java
@@ -0,0 +1,25 @@
+package baseball.Number;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberList {
+    private List<Integer> RandomNumberList;
+    private List<Integer> InputNumberList;
+
+    public List<Integer> getRandomNumberList() {
+        return RandomNumberList;
+    }
+
+    public void setRandomNumberList(List<Integer> randomNumberList) {
+        RandomNumberList = randomNumberList;
+    }
+
+    public List<Integer> getInputNumberList() {
+        return InputNumberList;
+    }
+
+    public void setInputNumberList(List<Integer> inputNumberList) {
+        InputNumberList = inputNumberList;
+    }
+}
