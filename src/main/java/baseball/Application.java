70f9c6613cfaa76c87d9e7f009ca14f04bce4535
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-11-03 14:31:01 +0900

    feature : get user input
    The goal of this game is that
    A user guess what is the number of the computer
    That user offers a number in advance

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,13 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 import java.util.ArrayList;
 
 public class Application {
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     public static void main(String[] args) {
         gameStart();
 
@@ -13,6 +15,10 @@ public class Application {
 
     static void gameStart(){
         int[] computer = initializeComputerNumber();
+        boolean playBaseball = true;
+        while(playBaseball){
+            getUserInput();
+        }
     }
 
     static int[] initializeComputerNumber(){
@@ -25,5 +31,11 @@ public class Application {
         }
         int[] computerNumberArray = computerNumberList.stream().mapToInt(i -> i).toArray();
         return computerNumberArray;
+    }
+
+    static String getUserInput(){
+        System.out.print(INPUT_MESSAGE);
+        String userInput = Console.readLine();
+        return userInput;
     }
 }
