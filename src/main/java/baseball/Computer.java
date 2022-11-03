f37c2c5b54810e5126b25326f99e8f612cba7469
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-03 13:36:02 +0900

    feat : 자리수 만족 확인 기능 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,13 +2,44 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import static baseball.Configure.*;
+
+
 public class Computer {
 
-	public void random(){
-		createRandomNumber();
+
+	public List<Integer> getComputerNumber() {
+		List<Integer> numberList = new ArrayList<>();
+		createMultipleRandomNumber(numberList);
+		return numberList;
 	}
 
-	private static int createRandomNumber() {
-		return Randoms.pickNumberInRange(1,9);
+	private void createMultipleRandomNumber(List<Integer> randomNumber) {
+		do {
+			int createdNumber = createOneRandomNumber();
+
+			if (!hasDuplication(randomNumber, createdNumber)) {
+				randomNumber.add(createdNumber);
+			}
+
+		} while (!isSatisfyDigitRule(randomNumber));
 	}
+
+	private static boolean isSatisfyDigitRule(List<Integer> randomNumber) {
+		return Objects.equals(randomNumber.size(), NUMBER_DIGIT.getValue());
+	}
+
+	public boolean hasDuplication(List<Integer> randomNumber, int createdNumber) {
+		return randomNumber.contains(createdNumber);
+	}
+
+	public int createOneRandomNumber() {
+		return Randoms.pickNumberInRange(1, 9);
+	}
+
+
 }
