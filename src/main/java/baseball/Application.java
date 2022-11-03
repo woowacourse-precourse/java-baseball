8119e874145549aa4818e940d3c3fd64692d4b9c
jissssoo1717 <jisoolee7101@gmail.com>
2022-11-03 17:34:21 +0900

    test: Console.readLine()을 통한 사용자 입력 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Computer computer = new Computer();
-        System.out.println(computer.GenerateRandomThreeNumber());
+
+        InputTest();
+    }
+
+    public static void InputTest() {
+        String input = Console.readLine();
+        System.out.println(input);
     }
 
 
