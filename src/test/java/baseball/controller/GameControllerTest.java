ad3ea69ec48bd188cc71d9d1c34e0dd6f2302b5d
JeonghakLee <wjdgkr0527@naver.com>
2022-11-03 15:47:11 +0900

    test: 사용자 숫자 입력 성공 테스트

--- src/test/java/baseball/controller/GameControllerTest.java
+++ src/test/java/baseball/controller/GameControllerTest.java
@@ -1,4 +1,20 @@
+package baseball.controller;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class GameControllerTest {
-  
+  @Test
+  void 사용자_숫자_입력테스트_성공() {
+    InputStream userInput = new ByteArrayInputStream("123".getBytes());
+    System.setIn(userInput);
+    GameController gc = new GameController();
+    List<Integer> playerNumbers = gc.getPlayerNumbers();
+    Assertions.assertThat(playerNumbers).isEqualTo(List.of(1, 2, 3));
+  }
 }
