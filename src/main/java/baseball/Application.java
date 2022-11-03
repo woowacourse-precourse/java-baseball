cad33f6b6b458bd5eda57fc14dd56160256ab419
OhJintaek <sunshot@hanyang.ac.kr>
2022-11-03 20:35:59 +0900

    type:feat 볼, 스트라이크 수에 따른 문장을 출력하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,32 @@
 package baseball;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Scanner scanner = new Scanner(System.in);
-        String userInput = getUserInput (scanner);
     }
 
     public static String getUserInput (Scanner scanner) {
         String userInput = scanner.nextLine();
         return userInput;
+    }
+
+    public static void announceBallAndStrike (int ball, int strike) {
+        ArrayList<String> ballAndStrike = new ArrayList<>();
+        if (ball > 0) {
+            ballAndStrike.add(String.format("%d볼", ball));
+        }
+        if (strike > 0) {
+            ballAndStrike.add(String.format("%d스트라이크", strike));
+        }
+        if (ballAndStrike.size() > 0) {
+            System.out.println(String.join(",", ballAndStrike));
+            return;
+        }
+        System.out.println("낫싱");
+        return;
     }
 }
