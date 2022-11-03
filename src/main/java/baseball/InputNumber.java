f75eb4a5e92566ebc805d3255991865712158d4f
csj <koreaxman@naver.com>
2022-11-03 20:20:53 +0900

    fix: create IllegalArgumentException statement

--- src/main/java/baseball/InputNumber.java
+++ src/main/java/baseball/InputNumber.java
@@ -9,17 +9,17 @@ public class InputNumber {
         this.number = number;
     }
 
-    public void checkStringNumberLength(String number) {
+    public static void checkStringNumberLength(String number) {
         if (!(number.length() == 3)) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력한 숫자길이가 3이 아닙니다.");
         }
     }
 
-    public void checkStringNumberIsNumber(String number) {
+    public static void checkStringNumberIsNumber(String number) {
         boolean isCheckNumber = number.chars().allMatch(Character::isDigit);
 
         if (!isCheckNumber) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력한 내용이 숫자가 아닙니다.");
         }
     }
 }
