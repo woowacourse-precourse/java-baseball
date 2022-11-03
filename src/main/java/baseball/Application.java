d49521957f350d763271102a3f000181d5b56bd5
HubCreator <chocomilk8604@gmail.com>
2022-11-03 16:13:01 +0900

    refactor : 애플리케이션 코드 indent 줄이기 및 ConstVariable 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,30 +10,26 @@ import static baseball.ConstVariable.*;
 
 public class Application {
     public static void main(String[] args) {
-        Balls computer;
         String userKeyInput = "1";
 
         while (userKeyInput.equals("1")) {
-            computer = makeRandomComputerBalls();
-            try {
-                doGame(computer);
-                userKeyInput = Console.readLine();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-                return;
-            }
+            doGame(makeRandomComputerBalls());
+            userKeyInput = Console.readLine();
         }
     }
 
     private static Balls makeRandomComputerBalls() {
         List<Integer> comBalls = new ArrayList<>();
-        while (comBalls.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(MIN, MAX);
-            if (!comBalls.contains(randomNumber)) {
-                comBalls.add(randomNumber);
-            }
+        while (comBalls.size() < VALID_CNT) {
+            addNotDuplicatedRandomNumber(comBalls, Randoms.pickNumberInRange(MIN, MAX));
         }
         return new Balls(comBalls);
+    }
+
+    private static void addNotDuplicatedRandomNumber(List<Integer> comBalls, int randomNumber) {
+        if (!comBalls.contains(randomNumber)) {
+            comBalls.add(randomNumber);
+        }
     }
 
     private static void doGame(Balls computer) {
@@ -48,8 +44,7 @@ public class Application {
             System.out.println(result);
         }
 
-        System.out.print("3개의 숫자를 모두 맞히셨습니다! ");
-        System.out.println("게임 종료");
+        System.out.print(VALID_CNT + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
