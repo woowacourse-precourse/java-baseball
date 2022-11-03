238916a3bc2af3a319088df2907f8d612dca2a52
jiwon1217 <koagjw@gmail.com>
2022-11-04 00:18:02 +0900

    feat(Game.java): 입력한 수는 중복되지 않아야 함

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -10,6 +10,7 @@ public class Game {
     public static final String INPUT_GUIDE = "숫자를 입력해주세요 : ";
     public static final String INVALID_INPUT_LENGTH = "숫자를 3개 입력해야 합니다.";
     public static final String INVALID_INPUT_TYPE = "숫자만 입력해야 합니다.";
+    public static final String DUPLICATE_INPUT_NUMBER = "중복된 수를 입력할 수 없습니다.";
 
     public List<Integer> generateRandomNumber() {
         List<Integer> numbers = new ArrayList<>();
@@ -40,5 +41,13 @@ public class Game {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException(INVALID_INPUT_TYPE);
         }
+        if (hasDuplicateNumber(inputNumbers)) {
+            throw new IllegalArgumentException(DUPLICATE_INPUT_NUMBER);
+        }
+    }
+
+    public boolean hasDuplicateNumber(String inputNumbers) {
+        return inputNumbers.charAt(0) == inputNumbers.charAt(1) || inputNumbers.charAt(1) == inputNumbers.charAt(2)
+                || inputNumbers.charAt(0) == inputNumbers.charAt(2);
     }
 }
