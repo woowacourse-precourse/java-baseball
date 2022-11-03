0e3747cae53ce733cc1b3a8e6d2f5e90d5318640
splitube <voicesplit@daum.net>
2022-11-03 22:01:50 +0900

    feat(Referee): 숫자를 받으면 결과를 알려주는 기능 구현

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -3,9 +3,22 @@ package baseball;
 import java.util.List;
 
 class Referee {
+    final int NOTHING = 0;
+    final int BALL = 1;
+    final int STRIKE = 2;
     List<Integer> answer;
 
     public void setAnswer(List<Integer> answer) {
         this.answer = answer;
+    }
+
+    int judgeNumber(int num, int index) {
+        if (answer.get(index) == num) {
+            return STRIKE;
+        }
+        if (answer.contains(num)) {
+            return BALL;
+        }
+        return NOTHING;
     }
 }
