f4b2879a56720a01f6af846b03efd42fb3ed9faf
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-03 16:35:28 +0900

    feat : 3자리의 난수를 생성하는 createRandomNumbers()메서드 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,19 +2,21 @@ package baseball;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 public class Computer {
-    static final int NUM_LIMIT_LENGTH = 3;
+    public static final int NUM_LIMIT_LENGTH = 3;
 
-    void createRandomNumbers() {
+    public static List<Integer> createRandomNumbers() {
         Set<Integer> randomNumber = new LinkedHashSet<>(NUM_LIMIT_LENGTH);
-        while (randomNumber.size()!=NUM_LIMIT_LENGTH) {
+        while (randomNumber.size() != NUM_LIMIT_LENGTH) {
             int number = pickNumberInRange(1, 9);
             randomNumber.add(number);
         }
+        List<Integer> computerNum = new ArrayList<>(randomNumber);
+        return computerNum;
     }
-
-
 }
