04b942055dceda9c3278970102430db3bc7aa6ca
myungsikyoon <yms8795@gmail.com>
2022-11-03 22:10:13 +0900

    feat(MenuSelection) : startMenuSelection 구현.

--- src/main/java/baseball/system/MenuSelection.java
+++ src/main/java/baseball/system/MenuSelection.java
@@ -1,4 +1,22 @@
 package baseball.system;
 
 public class MenuSelection {
+
+    private final boolean PLAY_GAME = true;
+    private final boolean QUIT = false;
+
+
+    public boolean startMenuSelection() {
+
+        printMenuSelection();
+
+        String selectionNumber = inputSelectionNumber();
+
+        if(selectionNumber.equals("1")){
+            return PLAY_GAME;
+        }
+
+        return QUIT;
+
+    }
 }
