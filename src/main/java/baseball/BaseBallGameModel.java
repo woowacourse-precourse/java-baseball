8173306abde1cb50d9a2f3c759ec8cfa78b4f349
cpot5620 <=cpot5620@naver.com>
2022-11-03 16:15:58 +0900

    refactor(BaseBallGameController): change class name Controller to Model

--- src/main/java/baseball/BaseBallGameModel.java
+++ src/main/java/baseball/BaseBallGameModel.java
@@ -5,11 +5,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BaseBallGameController {
+public class BaseBallGameModel {
     private int ball, strike;
     private List<Integer> computer;
 
-    public BaseBallGameController() {
+    public BaseBallGameModel() {
         this.ball = 0;
         this.strike = 0;
     }
--- src/test/java/baseball/BaseBallGameModelTest.java
+++ src/test/java/baseball/BaseBallGameModelTest.java
@@ -9,19 +9,19 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-class BaseBallGameControllerTest {
+class BaseBallGameModelTest {
     @Test
     void 랜덤한_번호_생성() {
-        BaseBallGameController baseBallGameController = new BaseBallGameController();
+        BaseBallGameModel baseBallGameModel = new BaseBallGameModel();
 
         try {
-            Method method = baseBallGameController.getClass().getDeclaredMethod("createRandomNumber");
+            Method method = baseBallGameModel.getClass().getDeclaredMethod("createRandomNumber");
             method.setAccessible(true);
-            Field field = baseBallGameController.getClass().getDeclaredField("computer");
+            Field field = baseBallGameModel.getClass().getDeclaredField("computer");
             field.setAccessible(true);
 
-            method.invoke(baseBallGameController);
-            List<Integer> randomNumbers = (ArrayList) field.get(baseBallGameController);
+            method.invoke(baseBallGameModel);
+            List<Integer> randomNumbers = (ArrayList) field.get(baseBallGameModel);
 
             for (int i = 0; i < randomNumbers.size(); i++) {
                 int number = randomNumbers.get(i);
