e7cf207d3dfe4fc036de084d5ce56ba1a48a7581
김경훈 <54941130+ouvir@users.noreply.github.com>
2022-11-03 16:27:07 +0900

    변경 (static) 객체지향적 코드 작성  :
    static 을 너무 자주 쓰게 되면, 메모리의 static 영역에 모두 할당되어 굉장히 비효율적인 코드가 될 수 있어, 이를 줄이고자 객체를 생성하여 사용하도록 바꾸어 봄.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,19 +4,21 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         // 출력: 숫자 야구 게임을 시작합니다.
-        startGame();
+        Application newGame = new Application();
+        newGame.startGame();
     }
-    private static void startGame(){
+    public void startGame() {
         String correctNumber = settingNumber();
         askAndAnswerLoop(correctNumber);
     }
-    private static String settingNumber(){
+
+    private String settingNumber() {
         String randomNumber = "123";
         // 컴퓨터의 숫자 3개 만들기.
         return randomNumber;
     }
 
-    private static void askAndAnswerLoop(String correctNumber){
+    private void askAndAnswerLoop(String correctNumber) {
         String askNumber;
         boolean loop = true;
         while (loop) {
@@ -24,24 +26,24 @@ public class Application {
             loop = compareOfNumber(correctNumber, askNumber);
         }
         // 종료문구 출력
-        if (askRetry()){
-            startGame();
+        if (askRetry()) {
+            this.startGame();
         }
     }
 
-    private static String getNumber(){
+    private String getNumber() {
         String askNumber = "";
         //사용자에게 숫자 3개 입력받고 예외 검사
         return askNumber;
     }
 
-    private static boolean compareOfNumber(String correctNumber, String askNumber){
+    private boolean compareOfNumber(String correctNumber, String askNumber) {
         // 비교하며, 몇 볼, 몇 스트라이크 or 낫싱 인지 출력
         // 만약 3스트라이크면 false 반환
         return true;
     }
 
-    private static boolean askRetry(){
+    private boolean askRetry() {
         // 입력받기
         return false;
     }
