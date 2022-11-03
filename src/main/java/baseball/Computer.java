de34b7af85159f5865da727daa7d2f7e12a0cf47
khsrla9806 <kimhunsope@naver.com>
2022-11-03 17:35:42 +0900

    refactor(Computer): computersNumber 리스트의 인자 값을 문자열로 변경 Console.readLine() 함수를 보니 문자열을 입력받기 때문에 일치시키기 위해 변경함

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,13 +5,13 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer {
-    List<Integer> computersDigit = new ArrayList<>();
+    List<String> computersDigit = new ArrayList<>();
 
     public Computer() {
         while (computersDigit.size() < 3) {
             int pickNumber = Randoms.pickNumberInRange(1, 9);
             if (!computersDigit.contains(pickNumber)) {
-                computersDigit.add(computersDigit.size(), pickNumber);
+                computersDigit.add(computersDigit.size(), String.valueOf(pickNumber));
             }
         }
     }
