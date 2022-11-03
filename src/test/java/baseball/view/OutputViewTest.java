943deb50a536902c1b5fa979cbedbfc7b0fe7670
ksh-beginner <ohk9134@naver.com>
2022-11-03 23:06:49 +0900

    test(OutputViewTest) : 스트라이크가 3개면 3스트라이크 + 게임 종료 문구 + 재시작 문구 출력 테스트

--- src/test/java/baseball/view/OutputViewTest.java
+++ src/test/java/baseball/view/OutputViewTest.java
@@ -110,4 +110,14 @@ public class OutputViewTest {
 
         assertThat(output.toString()).isEqualTo(gameResultMessage);
     }
+
+    @Test
+    void 입력한_수에_대해_스트라이크가_3개면_3스트라이크_게임_종료_재시작_문구_출력() {
+        int strikeCount = 3;
+        int ballCount = 0;
+        String gameResultMessage = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+        OutputView.printGameResultMessage(strikeCount, ballCount);
+
+        assertThat(output.toString()).isEqualTo(gameResultMessage);
+    }
 }
