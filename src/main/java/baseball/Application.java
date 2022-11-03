3093fa245a6f34cb554952248493e3f29fccdff9
LeeJaeHong <0915jhlee@daum.net>
2022-11-03 16:39:05 +0900

    2.1 입력 에러 발견 후 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.awt.*;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -18,11 +20,17 @@ public class Application {
                 System.out.println("숫자 야구 게임을 시작합니다.");
                 computerRandomNumber = makeRandomNumber();
             }
+
             System.out.print("숫자를 입력해주세요 : ");
-            readLine();
+            String userInput=readLine();
+            if(!checkUserInput(userInput)){
+                break;
+            }
+
         }
     }
 
+    //컴퓨터 랜덤 숫자 생성
     static List<Integer> makeRandomNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
@@ -33,4 +41,27 @@ public class Application {
         }
         return computer;
     }
+    //유저 입력 체크
+    static boolean checkUserInput(String userInput){
+        Set<Integer> numbersSet = new HashSet<>();
+        for (int strIndex = 0; strIndex < userInput.length(); strIndex++) {
+            int number=userInput.charAt(strIndex)-'0';
+            numbersSet.add(number);
+        }
+        if(numbersSet.size()==3){
+            return true;
+        }
+        try{
+            if (userInput.equals("1")||userInput.equals("2")){
+                return true;
+            }
+            else{
+                throw new IllegalArgumentException();
+            }
+
+        }catch (IllegalArgumentException e){
+            return false;
+        }
+    }
+
 }
