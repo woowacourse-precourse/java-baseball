b6378af6a30e59af61138ee2d88659114367b06f
junhei Lee <jun2ee22@naver.com>
2022-11-03 14:47:25 +0900

    test: 규격에 맞지 않는 답안 입력 시 IllgalArgumentException 발생 확인
    규격에 맞지 않는 답안: 숫자가 아닌 문자 입력, 3개 이상 입력, 중복해서 입력, 0을 포함해 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,9 @@
 package baseball;
 
-import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
-        Game game = new Game();
-        game.start();
+
     }
 }
---
+++ src/test/java/baseball/IllegalExceptionTest.java
@@ -0,0 +1,36 @@
+package baseball;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class IllegalExceptionTest {
+    IllegalException illegalException = new IllegalException();
+
+    @Test
+    void 입력_문자() {
+        String[] arrays = {"1","2","d"};
+        Assertions.assertThrows(IllegalArgumentException.class,()->illegalException.check(arrays));
+    }
+
+    @Test
+    void 입력_3초과() {
+        String[] arrays = {"1","2","3","4"};
+        Assertions.assertThrows(IllegalArgumentException.class,()->illegalException.check(arrays));
+
+    }
+
+    @Test
+    void 입력_중복포함() {
+        String[] arrays = {"3","4","3"};
+        Assertions.assertThrows(IllegalArgumentException.class,()->illegalException.check(arrays));
+    }
+
+    @Test
+    void 입력_0포함() {
+        String[] arrays = {"2","4","0"};
+        Assertions.assertThrows(IllegalArgumentException.class,()->illegalException.check(arrays));
+    }
+}
