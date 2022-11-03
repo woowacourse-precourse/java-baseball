2dec648998fcbbb1ce71fd6bf3665eb58018c140
bfly_notebook <kkyxw@soongsil.ac.kr>
2022-11-04 01:05:04 +0900

    style: revise the indent and empty line and rename a few function

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,11 +1,11 @@
-package baseball.validate;
+package baseball;
 
 public class Validation {
 
 	public static void validateAnswer(String input) {
 		validateIsNumber(input);
 		validateLength(input);
-		validateNotHaveZero(input);
+		validateNotContainZero(input);
 		validateNotHaveDuplicateNumber(input);
 	}
 
@@ -21,14 +21,13 @@ public class Validation {
 	private static void validateIsNumber(String input) {
 		try {
 			Integer.parseInt(input);
-		}
-		catch (IllegalArgumentException e) {
+		} catch (IllegalArgumentException e) {
 			throw new IllegalArgumentException("문자가 입력되었습니다. 숫자를 입력해 주세요.");
 		}
 
 	}
 
-	private static void validateNotHaveZero(String input) {
+	private static void validateNotContainZero(String input) {
 		if (input.contains("0")) {
 			throw new IllegalArgumentException("입력값에 0이 포함되어 있습니다. 1부터 9 사이의 숫자를 입력해 주세요.");
 		}
@@ -37,8 +36,9 @@ public class Validation {
 	private static void validateNotHaveDuplicateNumber(String input) {
 		for (int i = 0; i < input.length(); i++) {
 			char c = input.charAt(i);
-			if (i != input.lastIndexOf(Character.toString(c)))
-				throw new IllegalArgumentException("입력하신 값에 중복된 숫자가 있습니다. 1부터 9 사이의 숫자 중 서로 다른 3가지 숫자를 입력해 주세요.");
+			if (i != input.lastIndexOf(Character.toString(c))) {
+				throw new IllegalArgumentException("입력하신 값에 중복된 숫자가 있습니다. 서로 다른 3가지 숫자를 입력해 주세요.");
+			}
 		}
 	}
 
