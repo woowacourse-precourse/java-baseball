3577cc83d291f5a520d633713a70aacdbdb1a3ba
splitube <voicesplit@daum.net>
2022-11-03 21:39:40 +0900

    test(NumberMaker): 3개의 숫자 리스트 생성 기능 테스트 구현
    1번의 테스트는 의미가 없다 생각하여 100개의 테스트 케이스를 검사하는 테스트로 구현

--- src/test/java/baseball/NumberMakerTest.java
+++ src/test/java/baseball/NumberMakerTest.java
@@ -2,6 +2,8 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 class NumberMakerTest {
@@ -14,6 +16,16 @@ class NumberMakerTest {
         for (int i = 0; i < 100; i++) {
             int randomNumber = numberMaker.makeNumberInRange(start, end);
             assertThat(randomNumber).isBetween(start, end);
+        }
+    }
+
+    @Test
+    void makeThreeDifferentNumberListInRange() {
+        int start = 1;
+        int end = 9;
+        for (int i = 0; i < 100; i++) {
+            List<Integer> numberList = numberMaker.makeThreeDifferentNumberListInRange(start, end);
+            assertThat(numberList).allSatisfy(number -> assertThat(number).isBetween(start, end));
         }
     }
 }
