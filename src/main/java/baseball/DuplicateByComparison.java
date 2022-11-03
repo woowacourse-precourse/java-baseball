c339376f9de80ede076cf8a67aa64013559ce42a
jaehyeon <jhjh0022@naver.com>
2022-11-04 00:35:47 +0900

    두개를 비교하여 중복인 숫자 찾기 구현

---
+++ src/main/java/baseball/DuplicateByComparison.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.AnswerNumber.makeAnswerNumber;
+import static baseball.BeforeVerificationSetting.makeNumberSplitList;
+
+public class DuplicateByComparison {
+
+    public static List<Integer> findOverlapNumber(int i){
+        List<Integer> overlapNumber = new ArrayList<>();
+        makeAnswerNumber();
+        makeNumberSplitList(i);
+        for(int number : makeAnswerNumber()){
+            if(makeNumberSplitList(i).equals(number)){
+                overlapNumber.add(number);
+            }
+        }
+        return overlapNumber;
+    }
+}
