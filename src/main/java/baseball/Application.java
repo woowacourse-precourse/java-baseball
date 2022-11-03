3894adf2896af614467b61ffd71492c1282b030a
haisley77 <qhfg3145@naver.com>
2022-11-03 12:06:05 +0900

    feat(start()): print the start msg & start a game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
+    
+    private static void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        start();
     }
 }
