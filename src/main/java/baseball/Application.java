a0c2de7464cb8576f392389185394d8f31a35307
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-11-03 20:12:27 +0900

    feature : check whether the user input is valid
    Since there is requirements of the Input,
    the application needs to check whether the user input is valid or not
    added the check feature

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,6 +9,14 @@ import java.util.ArrayList;
 public class Application {
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_LENGTH_ERROR_MESSAGE = "입력한 값이 3자리가 아닙니다.";
+    private static final String INPUT_REPLAY_ERROR_MESSAGE = "재시작이나 종료를 위해서는 1이나 2를 입력해야 합니다.";
+    private static final String INPUT_NOT_NUMERIC_ERROR_MESSAGE = "숫자가 아닌 문자가 입력되었습니다.";
+    private static final String INPUT_DUPLICATED_ERROR_MESSAGE = "중복된 숫자가 입력되었습니다.";
+    private static final int INPUT_BASEBALL_LENGTH = 3;
+    private static final int INPUT_REPLAY_LENGTH = 3;
+    private static char REPLAY = '1';
+    private static char END = '2';
     public static void main(String[] args) {
         gameStart();
 
@@ -20,6 +28,12 @@ public class Application {
         boolean playBaseball = true;
         while(playBaseball){
             String userInput = getUserInput();
+            try {
+                isValidInput(userInput ,InputType.BASEBALL_NUMBER);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                System.out.println(illegalArgumentException);
+                break;
+            }
         }
     }
 
@@ -40,4 +54,58 @@ public class Application {
         String userInput = Console.readLine();
         return userInput;
     }
+
+    static void isValidInput(String userInput, InputType inputType) {
+        if(inputType == InputType.BASEBALL_NUMBER) {
+            if(userInput.length() != INPUT_BASEBALL_LENGTH) {
+                throw new IllegalArgumentException(INPUT_LENGTH_ERROR_MESSAGE);
+            }
+            if(!isNumeric(userInput)) {
+                throw new IllegalArgumentException(INPUT_NOT_NUMERIC_ERROR_MESSAGE);
+            }
+            if(hasDuplicatedNumber(userInput)) {
+                throw new IllegalArgumentException(INPUT_DUPLICATED_ERROR_MESSAGE);
+            }
+        } else if(inputType == InputType.WHETHER_REPLAY) {
+            if(userInput.length() != INPUT_REPLAY_LENGTH) {
+                throw new IllegalArgumentException(INPUT_REPLAY_ERROR_MESSAGE);
+            }
+            if(!(userInput.charAt(0) == REPLAY || userInput.charAt(0) == END)) {
+                throw new IllegalArgumentException(INPUT_REPLAY_ERROR_MESSAGE);
+            }
+        }
+    }
+
+    static boolean isNumeric(String userInput) {
+        boolean result = true;
+        for(int i = 0; i < 3; ++i) {
+            if(!(userInput.charAt(i) >= '1' && userInput.charAt(i) <= '9')) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    static boolean hasDuplicatedNumber(String userInput) {
+        boolean isDuplicated = false;
+        char firstLetter = userInput.charAt(0);
+        char secondLetter = userInput.charAt(1);
+        char thirdLetter = userInput.charAt(2);
+        if(firstLetter == secondLetter) {
+            isDuplicated = true;
+        } else if(firstLetter == thirdLetter) {
+            isDuplicated = true;
+        } else if(secondLetter == thirdLetter) {
+            isDuplicated = true;
+        }
+
+        return isDuplicated;
+    }
+}
+
+enum InputType {
+    BASEBALL_NUMBER,
+    WHETHER_REPLAY
 }
