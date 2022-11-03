329904032b409e0869e0b81cee3df0fb9444d7e9
SH <gram753@naver.com>
2022-11-03 20:54:05 +0900

    refactor: method 접근제어자 변경

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -8,13 +8,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 ;
 
 public class Computer {
-	List<Integer> answer = new ArrayList<>();
+	public List<Integer> answer = new ArrayList<>();
 
 	public Computer() {
 		setAnswer(answer);
 	}
 
-	private List<Integer> setAnswer(List<Integer> answer) {
+	public List<Integer> setAnswer(List<Integer> answer) {
 		while (answer.size() < 3) {
 			int randomNumber = Randoms.pickNumberInRange(1, 9);
 			if (!answer.contains(randomNumber)) {
