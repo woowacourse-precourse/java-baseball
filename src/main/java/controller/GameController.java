15650dcbeef953e5b04e4fd98fd7e006d565e020
dmdrk1414 <seungchan141414@gmail.com>
2022-11-03 20:53:03 +0900

    :sparkles:[Feat] 유저 입력값이 정답인지 아닌지 확인하는 메서드 선언

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -16,6 +16,11 @@ public class GameController {
     public void callInputNumOfUser() {
         this.user.inputNumOfUser();
     }
-    
+
+    public boolean isRightNumOfUser2Computer() {
+        int numInputOfUser = user.getNumSelecUser();
+        // TODO : 1. 컴퓨터에게 값을준다.
+        return true;
+    }
 
 }
