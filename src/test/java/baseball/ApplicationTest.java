0cb3e5b14640bdc09f27adf469889dcb35987216
ma-an-jong <alswhd1113@gmail.com>
2022-11-03 23:02:13 +0900

    test(ApplicationTest.java): Mockito를 이용한 테스트 학습
    과제 기능 테스트를 위한 예제 테스트 코드의 작동 과정을 학습
    메소드 내부를 뜯어봐서 알게된 내용으로 Mockito의 예제 코드를 추가
    
    BREAKING CHANGE: Mockito 예제 코드 추가
    
    Before:
    
        ApplicationTest.java
        {
            게임종료_후_재시작
            예외_테스트
        }
    
    After:
    
        ApplicationTest.java
        {
            when에_전달한_메소드를_호출하면_false를리턴
            연속적으로_스터빙도_가능하다
            runMain을통한_입출력을_이용하여_채점을하는_시스템
            게임종료_후_재시작
            예외_테스트
        }
    
        runMain을 이용하여 테스트를 진행하고 when-thenReturn에 대하여 학습

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -2,13 +2,49 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void when에_전달한_메소드를_호출하면_false를리턴()
+    {
+        List<String> listMock = Mockito.mock(List.class);
+        when(listMock.add(anyString())).thenReturn(false);
+
+        boolean added = listMock.add("1234");
+        assertThat(added).isFalse();
+    }
+    @Test
+    void 연속적으로_스터빙도_가능하다()
+    {
+        List<String> mockedList2 = Mockito.mock(List.class);
+        when(mockedList2.get(0)).thenReturn("Hello", "Dexter");
+
+        assertThat(mockedList2.get(0)).isEqualTo("Hello");
+        assertThat(mockedList2.get(0)).isEqualTo("Dexter");
+        assertThat(mockedList2.get(0)).isEqualTo("Dexter");
+    }
+
+    void runMain을통한_입출력을_이용하여_채점을하는_시스템(){
+//        System.out.println("낫싱");
+//        System.out.println("3스트라이크");
+//        System.out.println("1볼 1스트라이크");
+//        System.out.println("3스트라이크");
+//        System.out.println("게임 종료");
+
+        throw new IllegalArgumentException();
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -32,4 +68,9 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
+//    @Override
+//    public void runMain() {
+//        runMain을통한_입출력을_이용하여_채점을하는_시스템();
+//    }
 }
