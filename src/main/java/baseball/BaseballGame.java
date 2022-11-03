6f21b3e4b64f8c71294ded08a8ea81c9d9df0017
jeong-yeji <jeongyeji.mail@gmail.com>
2022-11-03 22:41:37 +0900

    style: change constant variable

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -10,7 +10,7 @@ public class BaseballGame {
     private static final String NOTHING = "낫싱";
     private static final String BALL = "볼";
     private static final String STRIKE = "스트라이크";
-    private static final String GAME_START = "숫자 야구 게임을 시작합니다";
+    private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
     private static final String GET_NUMBER = "숫자를 입력해주세요 : ";
     private static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String START_AGAIN = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
