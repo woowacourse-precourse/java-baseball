fdc596d2426f88bbf77c835b98a8e4cbdff4c293
ghchoi <ghchoi0427@gmail.com>
2022-11-03 12:21:23 +0900

    feat: 볼, 스트라이크 값 저장하는 엔티티

---
+++ src/main/java/baseball/entity/Result.java
@@ -0,0 +1,22 @@
+package baseball.entity;
+
+public class Result {
+    int ball;
+    int strike;
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+}
