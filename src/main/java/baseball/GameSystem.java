30ab01361e0e339a0686570658215c64c7f25ce1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-03 14:57:43 +0900

    feat: test fail을 위한 최소한의 클래스 GameSystem 생성

---
+++ src/main/java/baseball/GameSystem.java
@@ -0,0 +1,22 @@
+package baseball;
+
+public class GameSystem {
+    int[] computer;
+    int[] player;
+
+    String askOpinion(){
+        return "";
+    }
+
+    boolean isMoreGame(String numbers){
+        return true;
+    }
+
+    boolean isEndOfGame(String numbers){
+        return true;
+    }
+
+    boolean isNotRightAnswer(String answer){
+        return true;
+    }
+}
