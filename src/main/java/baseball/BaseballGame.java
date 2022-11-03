8e9734c9605571f5485de5fcac45b1267d2ea2db
jeong-yeji <jeongyeji.mail@gmail.com>
2022-11-03 22:03:59 +0900

    feat: get number from user

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,16 +8,20 @@ import java.util.List;
 public class BaseballGame {
 
     private static final String GAME_START = "숫자 야구 게임을 시작합니다";
+    private static final String GET_NUMBER = "숫자를 입력해주세요 : ";
 
     private List<Integer> computer;
+    private List<Integer> user;
 
     public BaseballGame() {
     }
 
     public void run() {
         System.out.println(GAME_START);
-
         makeRandomNumber();
+
+        System.out.print(GET_NUMBER);
+        getUserNumber();
     }
 
     private void makeRandomNumber() {
@@ -28,4 +33,27 @@ public class BaseballGame {
             }
         }
     }
+
+    private void getUserNumber() {
+        String input = Console.readLine();
+        user = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            if (i == 3) {
+                throw new IllegalArgumentException();
+            }
+
+            char c = input.charAt(i);
+            int number = Character.getNumericValue(c);
+            if (!isValidateNumber(number)) {
+                throw new IllegalArgumentException();
+            }
+            user.add(number);
+        }
+    }
+
+    private boolean isValidateNumber(int number) {
+        return number != 0 & !user.contains(number);
+    }
+
+
 }
