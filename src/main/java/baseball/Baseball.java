7f9e2c8020e755b1875e4cf1b42b70dd27f01306
KSH <tkdgns543@gmail.com>
2022-11-04 02:14:20 +0900

    refactor(Baseball): 클래스 변수들의 접근제어자를 default -> private로 변경
    메서드를 통해서만 접근 할 수 있도록 함

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -8,9 +8,9 @@ import java.util.List;
 import java.util.ArrayList;
 
 public class Baseball {
-    String userNumbers;
-    int numberLength;
-    List<Integer> computerNumber;
+    private String userNumbers;
+    private int numberLength;
+    private List<Integer> computerNumber;
 
     Baseball() {
         this(3);
