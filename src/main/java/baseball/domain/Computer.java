42c2e7a272fac74f83fbbe606426368caf8f80be
nuyh <hjaehyun25@gmail.com>
2022-11-04 02:17:29 +0900

    feat(Computer): Computer 엔티티 추가(필드 변수, 도메인 로직 생성)

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,24 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Computer {
+    private String answer;
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    /**
+     * 도메인 로직
+     */
+    public void setAnswerByRandom() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 10, 3);
+        this.answer = randomNumbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+    }
+}
