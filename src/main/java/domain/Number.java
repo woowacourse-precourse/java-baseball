2a717d400711ad8c5a3ec916d4c65022a8b99e4f
1eejisoo <jisu991012@gmail.com>
2022-11-03 21:56:47 +0900

    Feat: 입력 값 유효성 검증 기능 추가 - 숫자는 세 자리 수여야 한다.

--- src/main/java/domain/Number.java
+++ src/main/java/domain/Number.java
@@ -1,5 +1,18 @@
 package domain;
 
-public class Number {
+import java.util.List;
 
+public class Number {
+    public final List<Integer> number;
+
+    public Number(List<Integer> number) {
+        this.number = number;
+        validateThreeDigitNumber();
+    }
+
+    public void validateThreeDigitNumber() {
+        if (number.size() != 3) {
+            throw new IllegalArgumentException("숫자는 세 자리 자연수여야 합니다.");
+        }
+    }
 }
