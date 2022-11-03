0a79afe9d80de091e5f110f1f4d239442e60fcf6
slbin-park <smpts00@naver.com>
2022-11-03 15:10:50 +0900

    feat(Game) : 힌트를 출력하는 기능 추가
    - 스트라이크 , 볼의 개수를 출력하는 함수 추가
    - 출력값을 만드는 함수 추가

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -6,6 +6,9 @@ public class Game {
     private static String START_GAME = "숫자 야구 게임을 시작합니다.";
     private static String STRIKE = "스트라이크";
     private static String BALL = "볼";
+    private static String NOTHING = "낫싱";
+    private static int IS_ANSWER = 3;
+    private static int IS_NONE = 0;
     private int strike;
     private int ball;
     User user;
@@ -29,8 +32,9 @@ public class Game {
         ball = 0;
         for (int i = 0; i < computer.size(); i++) {
             strike += isStrike(computer.get(i), user.get(i));
-            ball += isBall(computer,user.get(i),i);
+            ball += isBall(computer, user.get(i), i);
         }
+        System.out.println(createOutput(strike,ball));
     }
 
     public int isStrike(int computerNumber, int userNumber) {
@@ -41,10 +45,24 @@ public class Game {
     }
 
     public int isBall(ArrayList<Integer> computer, int userNumber, int index) {
-        if (computer.get(index) != userNumber && computer.contains(userNumber)){
+        if (computer.get(index) != userNumber && computer.contains(userNumber)) {
             return 1;
         }
         return 0;
+    }
+
+    public String createOutput(int strike, int ball) {
+        if (strike == IS_ANSWER) {
+            return strike + STRIKE;
+        } else if (strike != IS_NONE && ball != IS_NONE) {
+            return ball + BALL + " " + strike + STRIKE;
+        } else if (strike > IS_NONE) {
+            return strike + STRIKE;
+        } else if (ball > IS_NONE) {
+            return ball + BALL;
+        } else {
+            return NOTHING;
+        }
     }
 
 }
