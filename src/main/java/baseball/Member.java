fbafc28cfa92353bdc94d3684045b980f7a7af27
Leehyougwoo <dlguddn3024@gmail.com>
2022-11-03 21:52:07 +0900

    feature: 입력 멤버 기능 구현

---
+++ src/main/java/baseball/Member.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class Member {
+    private final int number;
+
+    public Member(int number){
+        this.number = number;
+    }
+
+    public int getNumber(){
+        return number;
+    }
+    
+}
