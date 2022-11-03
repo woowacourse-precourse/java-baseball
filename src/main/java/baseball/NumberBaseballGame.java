06b151b19f1eecbbb8f0171428e2a828eba971d7
jkde7721 <jkde7721@naver.com>
2022-11-03 22:25:26 +0900

    feat: 게임 실행 단계별 메소드 선언
    플레이어 숫자 입력 → ball, strike 수 세기 → 맞춘 결과에 대한 힌트 출력
    → 게임 종료 조건 확인 → 게임 종료 or 다시 첫번째 단계로

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -13,7 +13,33 @@ public abstract class NumberBaseballGame {
     }
 
     private static void play() {
-        //TODO: 게임 진행 구현
+        do {
+            inputPlayerNumberList();
+            countBall();
+            countStrike();
+            printHint();
+        } while(!isGameOver());
+    }
+
+    private static void inputPlayerNumberList() {
+        //TODO: 플레이어로부터 숫자 입력받는 기능 구현
+    }
+
+    private static void countBall() {
+        //TODO: ball 수 세기 구현
+    }
+
+    private static void countStrike() {
+        //TODO: strike 수 세기 구현
+    }
+
+    private static void printHint() {
+        //TODO: 플레이어가 입력한 숫자 기반 힌트 출력 구현
+    }
+
+    private static boolean isGameOver() {
+        //TODO: 게임 종료 여부 반환하는 기능 구현 (3스트라이크일 때 종료)
+        return false;
     }
 
     private static void gameOver() {
