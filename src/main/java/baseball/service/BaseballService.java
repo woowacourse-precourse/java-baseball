656b42a1102e5d9c692ec00119931d75966ef513
whdcks420 <whdcks420@gmail.com>
2022-11-03 23:14:02 +0900

    feat: BaseballService 숫자비교 로직 기능 추가

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,2 +1,14 @@
-package baseball.service;public class BaseballService {
+package baseball.service;
+
+import baseball.model.Score;
+import baseball.util.Calculator;
+import baseball.validator.NumberValidator;
+
+public class BaseballService {
+
+    public Score compareNumber(String baseballNumber, String userNumber) {
+        NumberValidator.validate(userNumber);
+        return Calculator.score(baseballNumber, userNumber);
+    }
+
 }
