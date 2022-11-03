4c910e31a9ddaf33bfd33aa0b2cf64311f8ba4b3
sungjun kim <dd8873@naver.com>
2022-11-03 19:58:59 +0900

    feat: 컴퓨터 랜덤숫자 생성기능(중복x)

---
+++ src/main/java/baseball/ComputerRandomNumbersFactory.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+public class ComputerRandomNumbersFactory {
+
+    private static final int NUMBER_MAX_LENGTH = 3;
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int MIN_RANDOM_NUMBER = 1;
+
+    public List<Integer> createComputerRandomNumber() {
+        Set<Integer> computerRandomNumbers = new LinkedHashSet<>();
+        while (computerRandomNumbers.size() < NUMBER_MAX_LENGTH) {
+            computerRandomNumbers.add(pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER));
+        }
+        return new ArrayList<>(computerRandomNumbers);
+    }
+
+}
