18410960e5ef61ade4abc9c3bb259e110e03640b
EUNJEONGMUN <munej26@gmail.com>
2022-11-03 12:42:17 +0900

    test: 사용자에게 입력받은 값 검증하기 - 길이 충족여부 테스트

---
+++ src/test/java/baseball/UserInputNumbersTest.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserInputNumbersTest {
+    @Test
+    void 입력값_테스트_정상입력() {
+        String userInputValue = "123";
+        boolean result = UserInputNumbers.validUserInputNumber(userInputValue);
+        assertTrue(result);
+    }
+
+    @Test
+    void 입력값_테스트_비정상입력_길이초과() {
+        String userInputValue = "1234";
+        assertThatThrownBy(() -> UserInputNumbers.validUserInputNumber(userInputValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력값_테스트_비정상입력_길이미달() {
+        String userInputValue = "1";
+        assertThatThrownBy(() -> UserInputNumbers.validUserInputNumber(userInputValue))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
