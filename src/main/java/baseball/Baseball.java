3ad662cb23eaafa037f6c5c750672937ffe439af
recicoco068 <gkapf838@gmail.com>
2022-11-03 19:05:55 +0900

    feat : 볼 개수를 구하는 기능 구현

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -7,4 +7,8 @@ public class Baseball {
     public static List<Integer> getRandomNumber() {
         return Randoms.pickUniqueNumbersInRange(1, 9, 3);
     }
+
+    public static int getBallCount(List<Integer> computer, List<Integer> user) {
+        return (int) computer.stream().filter(user::contains).count();
+    }
 }
