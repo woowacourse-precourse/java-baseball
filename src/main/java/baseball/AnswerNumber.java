460cd567f4e542bb206fd185b54f88b70aa305ad
jaehyeon <jhjh0022@naver.com>
2022-11-03 21:58:59 +0900

    rename: 숫자 게임 정답 만들기

--- src/main/java/baseball/AnswerNumber.java
+++ src/main/java/baseball/AnswerNumber.java
@@ -1,20 +1,19 @@
 package baseball;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class AnswerNumber {
-    public static void main(String[] args)
-    {
-        inputPrint();
-        getInputNumber();
-    }
-    public static void inputPrint(){
-        System.out.println("숫자를 입력해주세요 : ");
-    }
-    public static int getInputNumber(){
-        Scanner number = new Scanner(System.in);
-        int receivedNumber = number.nextInt();
-
-        return receivedNumber;
+    public static List<Integer> makeAnswerNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
