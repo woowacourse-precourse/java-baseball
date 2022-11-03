7a3283e00664b9e4bffe9a86b380e26b9bfed28b
ghchoi <ghchoi0427@gmail.com>
2022-11-03 21:08:15 +0900

    feat: 게임 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.util.GameLogic;
+import baseball.validation.InputValidation;
+import baseball.view.GameView;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IOException {
+        GameController gameController = new GameController(new GameView(new InputValidation()), new GameLogic());
+        gameController.startGame();
     }
 }
