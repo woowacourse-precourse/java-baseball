7c52a52683e1411ff8fed6cf2a6cca6daea2ba97
jini_11 <jieun21124@naver.com>
2022-11-03 21:52:53 +0900

    feat: 게임 시작 문구를 출력하는 부분 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,11 +5,18 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+import static constant.BaseballConstant.START_MESSAGE;
+
 public class BaseballGame {
     private static final List<Integer> computerNumbers = new ArrayList<>();
 
     public static void start() {
         makeRandomNumber();
+        printMessage(START_MESSAGE);
+    }
+
+    public static void printMessage(String message) {
+        System.out.println(message);
     }
 
     private static void makeRandomNumber() {
---
+++ src/main/java/constant/BaseballConstant.java
@@ -0,0 +1,5 @@
+package constant;
+
+public class BaseballConstant {
+    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+}
