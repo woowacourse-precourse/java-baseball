a32f8fa48550b4a33f130adfb513154642ddb00d
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-03 15:46:52 +0900

    test(BaseballGameRule): baseball game rule testcase 추가
    - 낫싱 케이스 테스트
    - 2볼 1스트라이크 케이스
    - 3스트라이크 케이스

--- src/test/java/baseball/component/BaseballGameRuleTest.java
+++ src/test/java/baseball/component/BaseballGameRuleTest.java
@@ -1,4 +1,53 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball.component;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class BaseballGameRuleTest {
-  
+
+    @Test
+    @DisplayName("1. 0ball 0strike")
+    void nothingTest(){
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> playerNumbers = List.of(4, 5, 6);
+
+        BaseballGameRule baseballGameRule = new BaseballGameRule();
+        baseballGameRule.setComputerNumbers(computerNumbers);
+
+        List<Integer> result = baseballGameRule.verifyPlayerScore(playerNumbers);
+
+        assertThat(result).isEqualTo(List.of(0, 0));
+    }
+
+    @Test
+    @DisplayName("2. 2ball 1strike")
+    void strikeAndBallTest(){
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> playerNumbers = List.of(1, 3, 2);
+
+        BaseballGameRule baseballGameRule = new BaseballGameRule();
+        baseballGameRule.setComputerNumbers(computerNumbers);
+
+        List<Integer> result = baseballGameRule.verifyPlayerScore(playerNumbers);
+
+        assertThat(result).isEqualTo(List.of(2, 1));
+    }
+
+    @Test
+    @DisplayName("2. 0ball 3strike")
+    void threeStrikeTest(){
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> playerNumbers = List.of(1, 2, 3);
+
+        BaseballGameRule baseballGameRule = new BaseballGameRule();
+        baseballGameRule.setComputerNumbers(computerNumbers);
+
+        List<Integer> result = baseballGameRule.verifyPlayerScore(playerNumbers);
+
+        assertThat(result).isEqualTo(List.of(0, 3));
+    }
 }
