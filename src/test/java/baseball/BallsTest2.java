87eca3d2187f68500e1772fa76f42bbfba51c092
HubCreator <chocomilk8604@gmail.com>
2022-11-03 13:47:45 +0900

    feat : 사용자 다중 input에 대해 n볼

--- src/test/java/baseball/BallsTest2.java
+++ src/test/java/baseball/BallsTest2.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -18,7 +19,32 @@ public class BallsTest2 {
     @Test
     void nothing() {
         Result result = computer.play(Arrays.asList(4, 5, 6));
-        assertThat(result.getStrike()).isEqualTo(0);
         assertThat(result.getBall()).isEqualTo(0);
+        assertThat(result.getStrike()).isEqualTo(0);
     }
+
+    @Nested
+    class NBallTest {
+        @Test
+        void one_ball() {
+            Result result = computer.play(Arrays.asList(3, 5, 6));
+            assertThat(result.getBall()).isEqualTo(1);
+            assertThat(result.getStrike()).isEqualTo(0);
+        }
+
+        @Test
+        void two_ball() {
+            Result result = computer.play(Arrays.asList(3, 1, 6));
+            assertThat(result.getBall()).isEqualTo(2);
+            assertThat(result.getStrike()).isEqualTo(0);
+        }
+
+        @Test
+        void three_ball() {
+            Result result = computer.play(Arrays.asList(3, 1, 2));
+            assertThat(result.getBall()).isEqualTo(3);
+            assertThat(result.getStrike()).isEqualTo(0);
+        }
+    }
+
 }
