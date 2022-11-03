88c2322176c79f7e1a084466402c84d1a88638bd
ki-met-hoon <101192772+ki-met-hoon@users.noreply.github.com>
2022-11-03 16:31:58 +0900

    컴퓨터에 1부터 9까지 서로 다른 임의의 수 3개를 넣는 함수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        List<Integer> computer = new ArrayList<Integer>();
+        CreateRandomNum(computer);
+
+    }
+
+    public static List<Integer> CreateRandomNum(List<Integer> computer){
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
