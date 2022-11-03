756791d3ef234307023e876ea7720a8ca1eae3b1
Laeng <Laeng@users.noreply.github.com>
2022-11-03 22:15:22 +0900

    feat(GameUtil): 랜덤 숫자 생성 메서드 구현

--- src/main/java/baseball/utils/GameUtil.java
+++ src/main/java/baseball/utils/GameUtil.java
@@ -1,18 +1,52 @@
 package baseball.utils;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
 public class GameUtil {
     public static List<Integer> splitNumberToOneDigits(int number) {
-        return List.of(1,2,3);
+        List<Integer> oneDigitNumbers = new ArrayList<>();
+
+        while (number > 0) {
+            oneDigitNumbers.add(number % 10);
+            number /= 10;
+        }
+
+        Collections.reverse(oneDigitNumbers);
+
+        return oneDigitNumbers;
     }
-    
+
     public static List<Integer> compareNumber(int computer, int player) {
         return List.of(0, 0);
     }
 
-    public static int generateNumber() {
-        return 123;
+    public static int pickNumber(int numberDigit) {
+        int number = pickOneDigitNumber();
+
+        for (int length = 1; length < numberDigit; length++) {
+             number = appendRandomOneDigit(number);
+        }
+
+        return number;
+    }
+
+    private static int pickOneDigitNumber() {
+        return Randoms.pickNumberInRange(1,9);
+    }
+
+    private static int appendRandomOneDigit(int number) {
+        List<Integer> oneDigitNumbers = splitNumberToOneDigits(number);
+        int randomOneDigitNumber = pickOneDigitNumber();
+
+        if (oneDigitNumbers.contains(randomOneDigitNumber)) {
+            return appendRandomOneDigit(number);
+        }
+
+        return number + (randomOneDigitNumber * (int) Math.pow(10, oneDigitNumbers.size()));
     }
 }
