221a1313f8d93403afa62dafb3423a5029e62799
1eejisoo <jisu991012@gmail.com>
2022-11-03 21:58:19 +0900

    Feat: 입력 값 유효성 검증 기능 추가 - 각 자리 수는 서로 다른 숫자여야 한다.

--- src/main/java/domain/Number.java
+++ src/main/java/domain/Number.java
@@ -1,5 +1,6 @@
 package domain;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Number {
@@ -9,6 +10,7 @@ public class Number {
         this.number = number;
         validateThreeDigitNumber();
         validateNaturalNumber();
+        validateDifferentNumber();
     }
 
     public void validateThreeDigitNumber() {
@@ -21,6 +23,18 @@ public class Number {
         for (Integer digit : number) {
             if (digit <= 0) {
                 throw new IllegalArgumentException("각 자리 수는 0이 아닌 자연수여야 합니다.");
+            }
+        }
+    }
+
+    public void validateDifferentNumber() {
+        List<Integer> compareDigits = new ArrayList<>();
+        compareDigits.addAll(number);
+
+        for (int digit = 0; digit < this.number.size(); digit++) {
+            compareDigits.remove(number.get(digit));
+            if (compareDigits.contains(digit)) {
+                throw new IllegalArgumentException("각 자리 수는 서로 다른 숫자여야 합니다.");
             }
         }
     }
