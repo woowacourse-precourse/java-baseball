b6c68136e4ba6345faecf2865dbeed096203f9d7
ChaCha3088 <cha3088@gmail.com>
2022-11-03 23:26:00 +0900

    feat(/src/main/java/baseball/Application.java): create CheckPlayerNumberValidation method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static Boolean CheckPlayerNumberValidation(ArrayList<Integer> playerNumberArray) {
+        
     }
 }
