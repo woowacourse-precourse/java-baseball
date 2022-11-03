22ebeae348543968c55e6ba7765a1920166a9650
wltks98 <wltks98@hanmail.net>
2022-11-03 10:27:27 +0900

    test : 세자리 수 랜덤 생성 기능 테스트 코드 구현

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import baseball.domain.Computer;
 import camp.nextstep.edu.missionutils.test.NsTest;
+
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -8,28 +10,47 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.HashSet;
+
 class ApplicationTest extends NsTest {
-    @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
-    }
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
+	@Test
+	void 세자리_수를_랜덤으로_생성() {
+		Computer computer = new Computer();
+		computer.createAnswer();
 
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
+		assertThat(new HashSet<>(computer.getAnswer()))
+			.size()
+			.isEqualTo(3);
+
+		for (int i = 0; i < 3; i++) {
+			assertThat(computer.getAnswer()
+				.get(i))
+				.isBetween(1, 9);
+		}
+	}
+
+	@Test
+	void 게임종료_후_재시작() {
+		assertRandomNumberInRangeTest(
+			() -> {
+				run("246", "135", "1", "597", "589", "2");
+				assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+			},
+			1, 3, 5, 5, 8, 9
+		);
+	}
+
+	@Test
+	void 예외_테스트() {
+		assertSimpleTest(() ->
+			assertThatThrownBy(() -> runException("1234"))
+				.isInstanceOf(IllegalArgumentException.class)
+		);
+	}
+
+	@Override
+	public void runMain() {
+		Application.main(new String[] {});
+	}
 }
