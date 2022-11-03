dedbaa32d0e8d95eb6ea33ec6800206f3d50f758
Jeangyu <gjwlsrb1700@naver.com>
2022-11-03 16:18:50 +0900

    [UPDATE] Random 함수의 매직 넘버를 Constant값으로 생성하여 반영

--- src/main/java/baseball/Random/NumberGenerator.java
+++ src/main/java/baseball/Random/NumberGenerator.java
@@ -8,12 +8,15 @@ import java.util.Set;
 
 public class NumberGenerator {
 
+    public static final int NUMBER_SIZE = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
     public static String generateAnswerNumber(){
         String result = "";
         Set<Integer> nums = new HashSet<>();
 
-        while (nums.size() < 3){
-            int rand_num = Randoms.pickNumberInRange(1, 9);
+        while (nums.size() < NUMBER_SIZE){
+            int rand_num = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             nums.add(rand_num);
         }
         for (Integer num: nums) result += Integer.toString(num);
