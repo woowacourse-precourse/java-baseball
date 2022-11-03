4ae49abe3e4d4cb28ad025402417dd65bed94fc2
Limjongho <whdgh9873@icloud.com>
2022-11-03 14:50:57 +0900

    feat: 문자열 숫자 리스트/배열로 변환하는 기능 추가

---
+++ src/main/java/baseball/UserNumber.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class UserNumber {
+    private final List<Integer> userNums;
+
+    public UserNumber(String input) {
+        this.userNums = convertInputStringToUserNums(input);
+    }
+
+    private List<Integer> convertInputStringToUserNums(String input) {
+        List<Integer> userNums = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            userNums.add(Integer.valueOf(String.valueOf(input.charAt(0))));
+        }
+        return userNums;
+    }
+
+    public List<Integer> getUserNums() {
+        return userNums;
+    }
+}
