e1cef7412dfef4c7a0618a2df102ec372a96b258
SH <gram753@naver.com>
2022-11-03 21:20:02 +0900

    feat: 예외처리 구현

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -4,27 +4,20 @@ import static baseball.Constant.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
 	public List<Integer> numbers = new ArrayList<>();
-	public String inputNumbers;
 
-	public User() {
-		playGame();
-	}
-
-	public void playGame() {
-		this.inputNumbers = inputNumbers();
-		this.numbers = convertStringToIntList(inputNumbers);
-	}
-
-	public String inputNumbers() {
-		System.out.printf(INPUT_MESSAGE);
-		return Console.readLine();
+	public void play() {
+		System.out.println(INPUT_MESSAGE);
+		String inputNumbers = Console.readLine();
+		numbers = convertStringToIntList(inputNumbers);
 	}
 
 	public List<Integer> convertStringToIntList(String string) {
@@ -35,4 +28,33 @@ public class User {
 			.collect(Collectors.toList());
 	}
 
+	private static void inputIsValidate(String input) {
+		if (!validateDuplication(input) || !validateLength(input) || !validateType(input)) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private static boolean validateDuplication(String input) {
+		Set<Character> checkDuplication = new HashSet<>();
+		for (char number : input.toCharArray()) {
+			checkDuplication.add(number);
+		}
+		return checkDuplication.size() == NUM_LENGTH;
+	}
+
+	private static boolean validateLength(String input) {
+		if (input.length() == NUM_LENGTH) {
+			return true;
+		}
+		return false;
+	}
+
+	private static boolean validateType(String input) {
+		for (char number : input.toCharArray()){
+			if (number < 1 || number > 9){
+				return false;
+			}
+		}
+		return true;
+	}
 }
