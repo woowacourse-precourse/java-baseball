b46781a707bfcb18e5c70c60229ad3cd93bf4f1d
slbin-park <smpts00@naver.com>
2022-11-03 14:57:41 +0900

    feat(Game): 스트라이크 개수를 구하는 함수 추가

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,18 +1,41 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+
 public class Game {
     private static String START_GAME = "숫자 야구 게임을 시작합니다.";
+    private static String STRIKE = "스트라이크";
+    private static String BALL = "볼";
+    private int strike;
+    private int ball;
     User user;
     Computer computer;
-    public Game(){  
+
+    public Game() {
         this.user = new User();
         this.computer = new Computer();
     }
+
     public void Start() {
         while (true) {
             computer.createRandomNumbers();
             System.out.println(START_GAME);
             user.inputUserNumbers();
         }
+    }
+
+    public void outputHint(ArrayList<Integer> computer, ArrayList<Integer> user) {
+        strike = 0;
+        ball = 0;
+        for (int i = 0; i < computer.size(); i++) {
+            strike += isStrike(computer.get(i),user.get(i));
+        }
+    }
+
+    public int isStrike(int computerNumber, int userNumber) {
+        if (computerNumber == userNumber) {
+            return 1;
+        }
+        return 0;
     }
 }
