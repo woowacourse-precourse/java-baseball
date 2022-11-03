35377442aea43770b9aceb3450c393d743208902
egoing <dudqk9696@naver.com>
2022-11-03 09:33:27 +0900

    feat : 컴퓨터 세자리 수 삽입 기능 구현

---
+++ src/main/java/baseball/controller/InputController.java
@@ -0,0 +1,20 @@
+package baseball.controller;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputController {
+
+    public List<Integer> insertComNumber() {
+        List<Integer> insertNum = new ArrayList<>();
+        while(insertNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!insertNum.contains(randomNumber)) {
+                insertNum.add(randomNumber);
+            }
+        }
+        return insertNum;
+    }
+}
---
+++ src/main/java/baseball/domain/ComNumber.java
@@ -0,0 +1,16 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComNumber {
+    List<Integer> comNumber = new ArrayList<>();
+
+    public List<Integer> getComnumber() {
+        return comNumber;
+    }
+
+    public void setComnumber(List<Integer> comnumber) {
+        this.comNumber = comnumber;
+    }
+}
