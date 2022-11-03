82901605f79700d7476eb723117330409117404a
Reznov <reznov03048@gmail.com>
2022-11-03 23:38:09 +0900

    feat: 사용자의 입력을 받아 List로 반환하는 메소드 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,8 +1,36 @@
 package baseball;
 
+import baseball.user.Input;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 /**
  * 게임의 전반적인 기능을 하는 클래스
  */
 public class Game {
 
+    Input input = new Input();
+
+    public Game() {
+    }
+
+    /**
+     * 사용자의 입력을 받아 List로 변환해 넘겨주는 메소드
+     * @return 숫자를 차례로 넣은 List
+     */
+    public List<Integer> getUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+
+        String userInput = Console.readLine();
+
+        if (!input.isAllDigits(userInput)
+                || !input.isAllDifferentDigit(userInput)
+                || !input.isDigitsInRange(userInput)
+                || !input.isInputLengthEqualsGameLength(userInput)) {
+            throw new IllegalArgumentException();
+        }
+
+        return input.inputDigit(userInput);
+    }
 }
