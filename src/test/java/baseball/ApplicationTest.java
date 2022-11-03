9a571e4eb27770f60489489afa2f10abe00d0312
Devtist <yt930310@snu.ac.kr>
2022-11-04 03:23:53 +0900

    test: 메세지를 출력하는 기능 테스트 코드 작성

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,13 +3,24 @@ package baseball;
 import static camp.nextstep.edu.missionutils.test.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import User.Outputs;
 import camp.nextstep.edu.missionutils.test.NsTest;
 
 class ApplicationTest extends NsTest {
+	@Test
+	void print_메소드로_출력할_문자열_리턴() {
+		List<String> result = new ArrayList<>();
+		for (Outputs output : Outputs.values()) {
+			result.add(output.print());
+		}
+		assertThat(result).containsExactly("숫자 야구 게임을 시작합니다.", "숫자를 입력해주세요 : ", "%d볼", "%d스트라이크", "낫싱",
+			"%d개의 숫자를 모두 맞히셨습니다! 게임 종료", "게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.");
+	}
 
 	@Test
 	void 상수_확인() {
