0fe6aeb00e20f9c9cb5c9ca6b2c1d16fdd7dd994
ywj9811 <ywj9811@naver.com>
2022-11-04 00:06:23 +0900

    feat : Validation 클래스 내용 추가
    숫자가 아닌 다른 내용을 입력받는 경우 예외를 발생시키기 위한 코드 추가

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -5,6 +5,11 @@ import java.util.regex.Pattern;
 
 public class Validation {
     private static final Pattern IsOnlyNumber = Pattern.compile("^[0-9]*?");
+    public void isNumber(String input) {
+        if (!IsOnlyNumber.matcher(input).matches()) {
+            throw new IllegalArgumentException("숫자 외의 값이 입력되었습니다.");
+        }
+    }
     public void InputValid(List<Integer> RandomList, List<Integer> inputList) {
         /**
          * 3자리 숫자가 입력되어야 함 -> 3자리가 아니면 예외 처리
@@ -13,8 +18,7 @@ public class Validation {
          * 만약 3이 아닌 다른 크기로 변경되었을 경우에도 코드 수정을 최소화 할 수 있도록
          */
 
-        if (inputList.size() != RandomList.size() ||
-                IsOnlyNumber.matcher(inputList.toString()).matches()) {
+        if (inputList.size() != RandomList.size()) {
             throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
         }
 
