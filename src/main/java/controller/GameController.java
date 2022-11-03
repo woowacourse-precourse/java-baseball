44022df63a99fe7a0edf926ec0dc8c3e62ca7655
dmdrk1414 <seungchan141414@gmail.com>
2022-11-03 21:06:56 +0900

    :sparkles:[Feat] 유저에게 값을 얻어와서 컴퓨터에게 확인하는 메서드 추가

--- src/main/java/controller/GameController.java
+++ src/main/java/controller/GameController.java
@@ -20,7 +20,13 @@ public class GameController {
     public boolean isRightNumOfUser2Computer() {
         int numInputOfUser = user.getNumSelecUser();
         // TODO : 1. 컴퓨터에게 값을준다.
-        return true;
+        if (computer.isRealRightNumOfUser(numInputOfUser)) {
+            // 스트라이크와 볼이 있다면
+            return true;
+        } else {
+            // 낫싱만 있다면
+            return true;
+        }
     }
 
 }
