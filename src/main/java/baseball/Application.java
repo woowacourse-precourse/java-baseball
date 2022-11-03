5b461d14c8adc7f26314ae17004611c1bba08eb0
hwi215 <buc06137@naver.com>
2022-11-03 20:28:10 +0900

    feat: [기능1] 서로 다른 임의의 수 3개 선택하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+
+import java.util.Random;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int[] randomNum = new int[3]; // 서로다른 3개 숫자
+        Random rand = new Random();
+
+        for(int count = 0; count < 3; count++){ // 랜덤 숫자 3개 뽑기
+            randomNum[count] = rand.nextInt(9) + 1; // 1 ~9
+            for(int curCount = 0; curCount < count; curCount++){
+                if(randomNum[count] == randomNum[curCount]){ // 중복이면
+                    count--; // 중복 아닌 수로 다시 찾기
+                }
+            }
+            System.out.println("num: " + randomNum[count]);
+        }
+
     }
 }
