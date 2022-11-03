fa98ebc6c09351270c76761b8357110afb837ae1
임원정 <LWJ@MacBookPro.local>
2022-11-03 19:26:54 +0900

    feat : 게임 시작 문구를 출력하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+
+class Computer {
+    
+}
+
+class Player {
+
+}
+
+class Umpire {
+    static void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Umpire.gameStart();
+
+
     }
 }
