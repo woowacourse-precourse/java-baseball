30fa9432fd84bd6fbe464f3f0c057814644462a7
imsongj <imsongj@gmail.com>
2022-11-03 21:27:58 +0900

    feat(Turn) : 사용자 입력을 정수 리스트로 변환 기능 추가

--- src/main/java/baseball/Turn.java
+++ src/main/java/baseball/Turn.java
@@ -1,14 +1,26 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Turn {
+    private static final int NUMBER_OF_INPUT_DIGITS= 3;
     private static final String ASK_FOR_NUMBER = "숫자를 입력해주세요 : ";
     private List<Integer> playerNumberList;
 
     public Turn() {
+        this.playerNumberList = new ArrayList<>();
+    }
+
+    public void transformPlayerInputToList() {
+        String playerInput = getPlayerInput();
+        for (int index = 0; index < playerInput.length(); index++) {
+            playerNumberList.add(playerInput.charAt(index) - '0');
+        }
     }
 
     public String getPlayerInput() {
--- src/test/java/baseball/TurnTest.java
+++ src/test/java/baseball/TurnTest.java
@@ -3,18 +3,40 @@ package baseball;
 import static org.assertj.core.api.Assertions.*;
 
 import java.io.ByteArrayInputStream;
+
 import java.util.List;
+
+import java.lang.reflect.Field;
+
 import org.junit.jupiter.api.Test;
 
 public class TurnTest {
+    @Test
+    void transformPlayerInputToList_test_transforming_input_string_to_list() {
+        Turn turn = new Turn();
+        final byte[] buf = String.join("\n", "234").getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+        turn.transformPlayerInputToList();
+        try {
+            Field field = turn.getClass().getField("playerNumberList");
+            List<Integer> list = (List<Integer>) field.get(turn);
+
+            List<Integer> result = List.of(2, 3, 4);
+
+            assertThat(list).isEqualTo(result);
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
     void getPlayerInput_test_getting_input() {
         Turn turn = new Turn();
         final byte[] buf = String.join("\n", "234").getBytes();
         System.setIn(new ByteArrayInputStream(buf));
 
-        String result = "234";
-
-        assertThat(turn.getPlayerInput()).isEqualTo(result);
+        assertThat(turn.getPlayerInput()).isEqualTo("234");
     }
 }
