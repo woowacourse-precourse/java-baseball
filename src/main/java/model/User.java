c996ac70ddd071d4e1e3a978b5e06b1c000d3c07
dmdrk1414 <seungchan141414@gmail.com>
2022-11-03 11:33:30 +0900

    :sparkles:[Feat] 숫자를 입력 받는 함수 선언

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,5 +1,17 @@
 package model;
 
 public class User {
+
+    public int numSelecUser;
+
+    public User() {
+    }
+
+    public void choiceNum() {
+        // TODO : 숫자의 검증
+        // TODO : 숫자인지?
+        // TODO : 길이가 3개인지
+        // TODO : 문자열인지 or 정수인지
+    }
     
 }
