3c3942820bedb41ed4b7211b92c290299d41797f
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-03 23:11:57 +0900

    feat: 컴퓨터 랜덤 숫자 기능 구현

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Game {
+
+    public  List<Integer> getComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<Integer>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+        return computerNumber;
+    }
+
+
+}
