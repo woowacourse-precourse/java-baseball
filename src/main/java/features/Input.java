df51c72739255175ffc50af9d37a5c818709db01
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-03 13:31:41 +0900

    feat(features) :: Input

---
+++ src/main/java/features/Input.java
@@ -0,0 +1,26 @@
+package features;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Input {
+    private static List<Integer> inputNumberRepository = new ArrayList(3);
+    private static final int EXCEPTION = -1;
+
+    // 입력 기능
+    public static List<Integer> input() {
+
+        Scanner inputNumbers = new Scanner(System.in);
+        int i = inputNumbers.nextInt();
+
+        for (Integer integer : inputNumberRepository) {
+            inputNumberRepository.add(i);
+
+            if (inputNumberRepository.size()>3) { inputNumberRepository.add(EXCEPTION); }
+
+        }
+        return inputNumberRepository;
+    }
+}
