66bd6c5dd2fbe3a1c657e7dff8e2e6a6a8e23479
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-03 21:57:20 +0900

    feat : 결과 출력 메시지 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,6 +3,7 @@ package baseball;
 import java.util.LinkedHashMap;
 import java.util.Objects;
 
+
 public class Game {
 	Computer computer = new Computer();
 	User user = new User();
@@ -11,24 +12,108 @@ public class Game {
 	LinkedHashMap<Integer, Integer> fromUsers;
 
 
+	int computerNumber;
+	int computerNumIdx;
+	int userNumber;
+	int userNumIdx;
+
+	int strike;
+	int ball;
+
+
 	void game() {
-		fetchNumbers();
+		fetchComputerNumbers();
+
+		do {
+			strike = 0;
+			ball = 0;
+
+			fetchUserNumbers();
+			calculateGuess();
+			System.out.println(createAnswerMessage());
+
+
+		} while (!userWin());
+
 
 	}
 
-	public void fetchNumbers() {
+
+	public String createAnswerMessage() {
+		StringBuilder sb = new StringBuilder();
+
+		if (isNothing()) {
+			return String.valueOf(sb.append("낫싱"));
+		}
+
+		sb.append(ball).append("볼 ");
+		sb.append(strike).append("스트라이크");
+
+		if (strike == 0) {
+			return sb.toString().replaceAll("\\s[0-9]스트라이크", "");
+		}
+
+		if (ball == 0) {
+			return sb.toString().replaceAll("[0-9]볼\\s", "");
+		}
+
+		return sb.toString();
+
+
+	}
+
+	public void calculateGuess() {
+		for (int comIdx = 0; comIdx < fromComputers.size(); comIdx++) {
+
+			for (int userIdx = 0; userIdx < fromUsers.size(); userIdx++) {
+				checkEachNumAndIdx(comIdx, userIdx);
+				countStrike();
+				countBall();
+
+			}
+
+		}
+	}
+
+	public void countStrike() {
+		if (isStrike(computerNumber, computerNumIdx, userNumber, userNumIdx)) {
+			strike++;
+		}
+	}
+
+	public void countBall() {
+		if (isBall(computerNumber, computerNumIdx, userNumber, userNumIdx)) {
+			ball++;
+		}
+	}
+
+
+	public void fetchComputerNumbers() {
 		fromComputers = computer.getNumbers();
+	}
+
+	public void fetchUserNumbers() {
 		fromUsers = user.getNumbers();
 	}
 
 
-    public int extractNumber(LinkedHashMap<Integer, Integer> numbersMap, int idx) {
-        return (int) numbersMap.keySet().toArray()[idx];
-    }
+	public void checkEachNumAndIdx(int comIdx, int userIdx) {
 
-    public int extractIndex(LinkedHashMap<Integer, Integer> numbersMap, int idx) {
-        return (int) numbersMap.values().toArray()[idx];
-    }
+		computerNumber = extractNumber(fromComputers, comIdx);
+		computerNumIdx = extractIndex(fromComputers, comIdx);
+		userNumber = extractNumber(fromUsers, userIdx);
+		userNumIdx = extractIndex(fromUsers, userIdx);
+
+	}
+
+
+	public int extractNumber(LinkedHashMap<Integer, Integer> numbersMap, int idx) {
+		return (int) numbersMap.keySet().toArray()[idx];
+	}
+
+	public int extractIndex(LinkedHashMap<Integer, Integer> numbersMap, int idx) {
+		return (int) numbersMap.values().toArray()[idx];
+	}
 
 
 	public boolean isStrike(int computerNumber, int computerIdx, int userNumber, int userIdx) {
@@ -39,9 +124,11 @@ public class Game {
 		return Objects.equals(computerNumber, userNumber) && !Objects.equals(computerIdx, userIdx);
 	}
 
-	public boolean isNothing(int computerNumber, int computerIdx, int userNumber, int userIdx) {
-		return !Objects.equals(computerNumber, userNumber) && !Objects.equals(computerIdx, userIdx);
+	public boolean isNothing() {
+		return Objects.equals(strike, 0) && Objects.equals(ball, 0);
 	}
 
-
+	public boolean userWin() {
+		return strike == 3;
+	}
 }
