cc3f84c345bcb71dfb960ea04f2d0413a991d3ab
Eungyu-code <Agyu625@gmail.com>
2022-11-03 19:53:57 +0900

    feat: playGame() 함수 추가
    숫자 야구 게임이 진행되는 함수
    - 게임 종료를 선택하지 않으면 게임을 재시작하기 위해 while(true) 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,14 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        playGame();
+    }
+
+    public static void playGame() {
+
+        while(true) {
+
+        }
     }
 }
