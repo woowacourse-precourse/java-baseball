511da43efb90d7cf618401a01c8b73c9baae8301
SH <gram753@naver.com>
2022-11-03 12:24:47 +0900

    feat: 상수 수정 및 추가

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -2,14 +2,17 @@ package baseball;
 
 public class Constant {
 
-	public final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-	public final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-	public final String STRIKE_CALL = "스트라이크";
-	public final String BALL_CALL = "볼";
-	public final String NOTHING_MESSAGE = "낫싱";
-	public final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-	public final String END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-	public final String RESTART_GAME = "1";
-	public final String END_GAME = "2";
+	public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+	public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+	public static final String STRIKE = "스트라이크";
+	public static final String BALL = "볼";
+	public static final String NOTHING = "낫싱";
+	public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다!";
+	public static final String END_MESSAGE = "게임 종료";
+	public static final String REQUEST_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	public static final String RESTART_GAME = "1";
+	public static final String END_GAME = "2";
+
+	public static int NUM_LENGTH = 3;
 
 }
