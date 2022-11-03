691c60e5e7205cabd8d36f9465a2eb03c8401309
donghae <donghae1999@gmail.com>
2022-11-03 16:33:16 +0900

    feat: add AnswerNumber verify

--- src/main/java/baseball/GameService.java
+++ src/main/java/baseball/GameService.java
@@ -2,9 +2,12 @@ package baseball;
 
 import baseball.RandomNumber;
 
+import java.util.ArrayList;
+
 public class GameService {
     public void setGame(){
+        ArrayList<Integer> answerNumber = new ArrayList<>();
         RandomNumber randomNumber = new RandomNumber();
-        randomNumber.setRandomNumber();
+        answerNumber = randomNumber.setRandomNumber();
     }
 }
--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -2,8 +2,27 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumber {
-    public static void setRandomNumber(){
+import java.util.ArrayList;
 
+public class RandomNumber {
+    public static ArrayList<Integer> setRandomNumber(){
+        ArrayList<Integer> answerNumber = new ArrayList<>();
+        answerNumber = allocateRandomNumber(answerNumber);
+        return answerNumber;
+    }
+    private static ArrayList<Integer> allocateRandomNumber(ArrayList<Integer> answerNumber){
+        for(; answerNumber.size()<3;){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(verifyAnswerNumber(answerNumber,randomNumber))
+                continue;
+            answerNumber.add(randomNumber);
+        }
+        return answerNumber;
+    }
+    private static boolean verifyAnswerNumber(ArrayList<Integer> answerNumber, int randomNumber){
+        if(answerNumber.contains(randomNumber)){
+            return true;
+        }
+        return false;
     }
 }
