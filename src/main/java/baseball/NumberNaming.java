d8a9c454880efa0f1092040535ff44e49ff0d8e9
Jeongthingamajig <netpetet@naver.com>
2022-11-03 20:22:25 +0900

    Feat : 클래스 추가
    각 숫자에 대한 뜻을 가지고 있는 변수들을 모아놓은 클래스

---
+++ src/main/java/baseball/NumberNaming.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class NumberNaming {
+    public final static int MIN_RANGE_NUM = 1;
+    public final static int MAX_RANGE_NUM = 9;
+    public final static int MAX_NUMBER_SIZE = 3;
+
+    public final static int RE_START_GAME = 3;
+    public final static int GAME_END = 3;
+
+}
