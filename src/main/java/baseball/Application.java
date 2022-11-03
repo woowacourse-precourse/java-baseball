91707385282219bf4bdb671bf3c6579e0b565198
임원정 <LWJ@MacBookPro.local>
2022-11-03 20:17:56 +0900

    feat : 정답이 들어오기 전까지 반복해서 숫자를 입력받는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 class Computer {
     static void getThreeRandomNumber() {
         List<Integer> numberList = new ArrayList<>();
@@ -23,16 +25,36 @@ class Player {
 }
 
 class Umpire {
+    static List<Integer> playerNumberList = new ArrayList<>();
+
     static void gameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    static void getNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        changeStringToList(readLine());
+    }
+
+    private static void changeStringToList(String playerNumberString) {
+        Integer playerNumber = Integer.parseInt(playerNumberString);
+
+        while (playerNumber > 0) {
+            playerNumberList.add(0, playerNumber % 10);
+            playerNumber /= 10;
+        }
     }
 }
 
 public class Application {
     public static void main(String[] args) {
+        boolean getAnswer = false;
         Umpire.gameStart();
         Computer.getThreeRandomNumber();
-
+        while (!getAnswer) {
+            Umpire.getNumber();
+            System.out.println(Umpire.playerNumberList);
+        }
 
     }
 }
