a502405a322cfa99db55ffe42c9f7fcb96282280
sangwon <swchoi1997@naver.com>
2022-11-03 15:38:12 +0900

    feat: 사용자 야구공 번호 입력 오류 - 1~9 사이의 숫자 아님
    사용자가 야구공 번호 입력 시 1~9 사이의 숫자인지 확인하는 기능, 이후 동일한 숫자가 들어왔는지 체크하기위해 List형태로 리턴

--- src/main/java/baseball/InputValidator.java
+++ src/main/java/baseball/InputValidator.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class InputValidator {
@@ -10,10 +12,25 @@ public class InputValidator {
 
 	public static void checkUserBallInput(String userBallInput) {
 		checkBallNumberLength(userBallInput);
+		List<String> splitUserBalls = checkBallNumberIsValidNumber(userBallInput);
 	}
 
 	private static void checkBallNumberLength(String userBallInput) {
 		if (userBallInput.length() != 3) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private static List<String> checkBallNumberIsValidNumber(String userBallInput) {
+		String[] splitUserBall = userBallInput.split("");
+		for (String ball : splitUserBall) {
+			checkStandardNumber(ball);
+		}
+		return new ArrayList<>(Arrays.asList(splitUserBall));
+	}
+
+	private static void checkStandardNumber(String ball) {
+		if (!STANDARD_NUMBER.contains(ball)) {
 			throw new IllegalArgumentException();
 		}
 	}
