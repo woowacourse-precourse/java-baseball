ed867f42b7e988ffcf42f8c16e6b8afec2943782
K-Diger <kitten.diger@gmail.com>
2022-11-03 20:22:02 +0900

    feat : 기능 목록 6번, Referee 클래스 메서드 추가 - judgement() 메서드 추가로, strike(), ball() 메서드를 호출하여 최종 스트라이크/볼 갯수를 파악한다. - say() 메서드 추가로, judgement() 에서 알아낸 최종 스트라이크/볼 갯수를 바탕으로 어떤 문구를 출력할지 판단한다.

--- src/main/java/baseball/referee/Referee.java
+++ src/main/java/baseball/referee/Referee.java
@@ -1,5 +1,6 @@
 package baseball.referee;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -28,5 +29,46 @@ public class Referee {
             }
         }
         return ballCount;
+    }
+
+    public Map<String, Integer> judgement(List<String> computerGeneratedValue, List<String> userInputValue) {
+
+        int strikeCount = 0;
+        int ballCount = 0;
+
+        if (computerGeneratedValue.equals(userInputValue)) {
+            Map<String, Integer> strikeBallCount = new HashMap<>();
+            strikeBallCount.put("strikeCount", 3);
+            strikeBallCount.put("ballCount", 3);
+
+            return strikeBallCount;
+        }
+
+        Map<String, Integer> strikeBallCount = new HashMap<>();
+
+        for (int computerValueIndex = 0; computerValueIndex < 3; computerValueIndex++) {
+            strikeCount += strike(computerValueIndex, computerGeneratedValue, userInputValue);
+            ballCount += ball(computerValueIndex, computerGeneratedValue, userInputValue);
+        }
+
+        strikeBallCount.put("strikeCount", strikeCount);
+        strikeBallCount.put("ballCount", ballCount);
+
+        return strikeBallCount;
+    }
+
+    public void say(Map<String, Integer> judgement) {
+        if (judgement.get("strikeCount") == 3) {
+            System.out.println(judgement.get("strikeCount") + "스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else if (judgement.get("strikeCount") == 0 && judgement.get("ballCount") == 0) {
+            System.out.println("낫싱");
+        } else if (judgement.get("strikeCount") != 0 && judgement.get("ballCount") == 0) {
+            System.out.println(judgement.get("strikeCount") + "스트라이크");
+        } else if (judgement.get("strikeCount") == 0 && judgement.get("ballCount") != 0) {
+            System.out.println(judgement.get("ballCount") + "볼");
+        } else if (judgement.get("strikeCount") != 0 && judgement.get("ballCount") != 0) {
+            System.out.println(judgement.get("ballCount") + "볼 " + judgement.get("strikeCount") + "스트라이크");
+        }
     }
 }
