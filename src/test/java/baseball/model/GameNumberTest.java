7feca18ccb165fbf2817111ec24977cb9389461e
code-tree <dltjdqls1200@naver.com>
2022-11-03 18:31:33 +0900

    test: 숫자 문자열을 리스트로 반환하는 메서드 생성

--- src/test/java/baseball/model/GameNumberTest.java
+++ src/test/java/baseball/model/GameNumberTest.java
@@ -4,6 +4,10 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("게임 숫자 생성 테스트")
@@ -17,14 +21,19 @@ class GameNumberTest {
         @DisplayName("숫자가 3개면 정상적으로 생성된다.")
         @Test
         void validGameNumberSize() {
-            assertDoesNotThrow(() -> new GameNumber("123"));
+            List<Integer> numbers = createNumbers("123");
+
+            assertDoesNotThrow(() -> new GameNumber(numbers));
         }
+
 
         @Order(2)
         @DisplayName("숫자가 3개가 아니면 예외가 발생한다.")
         @ParameterizedTest(name = "{index}. 예외 발생 입력값 = {0}")
         @ValueSource(strings = {"1", "12", "1234"})
-        void invalidGameNumberSize(String numbers) {
+        void invalidGameNumberSize(String input) {
+            List<Integer> numbers = createNumbers(input);
+
             assertThrows(IllegalArgumentException.class, () ->
                     new GameNumber(numbers));
         }
@@ -39,13 +48,17 @@ class GameNumberTest {
         @DisplayName("숫자가 1~9 범위면 정상적으로 생성된다.")
         @Test
         void validGameNumberRange() {
-            assertDoesNotThrow(() -> new GameNumber("123"));
+            List<Integer> numbers = createNumbers("123");
+
+            assertDoesNotThrow(() -> new GameNumber(numbers));
         }
 
         @DisplayName("숫자가 1~9 범위를 벗어나면 예외가 발생한다.")
         @Test
         void invalidGameNumberRange() {
-            assertThrows(IllegalArgumentException.class, () -> new GameNumber("012"));
+            List<Integer> numbers = createNumbers("012");
+
+            assertThrows(IllegalArgumentException.class, () -> new GameNumber(numbers));
         }
     }
 
@@ -57,14 +70,24 @@ class GameNumberTest {
         @DisplayName("숫자가 중복되지 않으면 정상적으로 생성된다.")
         @Test
         void gameNumberNonDuplicate() {
-            assertDoesNotThrow(() -> new GameNumber("123"));
+            List<Integer> numbers = createNumbers("123");
+
+            assertDoesNotThrow(() -> new GameNumber(numbers));
         }
 
         @Order(2)
         @DisplayName("숫자중 중복되는 숫자가 있으면 예외가 발생한다.")
         @Test
         void gameNumberDuplicate() {
-            assertThrows(IllegalArgumentException.class, () -> new GameNumber("122"));
+            List<Integer> numbers = createNumbers("122");
+
+            assertThrows(IllegalArgumentException.class, () -> new GameNumber(numbers));
         }
+    }
+
+    private List<Integer> createNumbers(String numbers) {
+        return Arrays.stream(numbers.split(""))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
     }
 }
