dd89e80da7cf248b2942b7292d51c7d0191020e9
KSH <tkdgns543@gmail.com>
2022-11-04 02:23:16 +0900

    feat(Application): main 메서드에 숫자 야구 게임과 결과 출력을 포함하는 루프 구현 본 게임을 진행하는 메서드 호출 루프 밖에 게임 재시작 여부를 저장하는 문자열 변수를 둠 한 게임 종료시 결과문과 재시작 여부 입력 안내문 출력 게임 재시작 여부를 저장하는 문자열 변수가 1이면 재시작, 2면 종료 마지막에 사용자 입력 예외 확인 메서드 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         final int NUMBER_LENGTH = 3;
         System.out.println("숫자 야구 게임을 시작합니다.");
+        String loopChecker = "1";
+
+        while (loopChecker.equals("1")) {
+            Baseball baseball = new Baseball(NUMBER_LENGTH);
+            baseball.play();
+
+            System.out.println(NUMBER_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+            loopChecker = Console.readLine();
+            checkRetryInputException(loopChecker);
+        }
     }
 
     private static void checkRetryInputException(String retry) throws IllegalArgumentException{
