ed3e1bff17acdab6c06b5a91f65f162283e731f9
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-03 10:27:52 +0900

    refactor : README에 따른 ComputerNumberGeneratorTest 수정

--- src/test/java/baseball/ComputerNumberGeneratorTest.java
+++ src/test/java/baseball/ComputerNumberGeneratorTest.java
@@ -2,8 +2,6 @@ package baseball;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.*;
 public class ComputerNumberGeneratorTest {
 
@@ -15,25 +13,47 @@ public class ComputerNumberGeneratorTest {
     }
 
     @Test
-    void 랜덤_넘버_1부터9_리턴(){
-        assertThat(100).isLessThanOrEqualTo(9)
+    void 랜덤_넘버_1부터9를_만든다(){
+        assertThat(computerNumberGenerator.createRandomNumber()).isLessThanOrEqualTo(9)
                 .isGreaterThanOrEqualTo(1);
     }
 
     @Test
-    void 이미_저장_돼있으면_true(){
-        assertThat(false).isTrue();
-    }
-
-    @Test
-    void 클래스_변수에_값이_저장(){
-        computerNumberGenerator.insertNumber(100);
+    void 값을_넣어주면_들어간다(){
+        computerNumberGenerator.putNumber(100);
         assertThat(computerNumberGenerator.returnNumbersToArray()).contains(100);
     }
 
     @Test
-    void 랜덤값_3개가_저장(){
+    void 이미_저장_돼있으면_true(){
+        computerNumberGenerator.putNumber(100);
+        assertThat(computerNumberGenerator.isAlreadyInNumbers(100)).isTrue();
+    }
+
+    @Test
+    void 값이_존재하지_않으면_false(){
+        assertThat(computerNumberGenerator.isAlreadyInNumbers(100)).isFalse();
+    }
+
+    @Test
+    void 랜덤값_1부터9가_이미_있지_않으면_저장시킨다(){
+        computerNumberGenerator.insertNumber();
+        assertThat(computerNumberGenerator.returnNumbersToArray().length == 1);
+        assertThat(computerNumberGenerator.returnNumbersToArray()[0]).isGreaterThanOrEqualTo(1)
+                .isLessThanOrEqualTo(9);
+    }
+
+    @Test
+    void 세자리_랜덤_생성시_총_3자리만_나온다(){
         computerNumberGenerator.makeThreeDigitNumbers();
         assertThat(computerNumberGenerator.returnNumbersToArray().length).isEqualTo(3);
+    }
+
+    @Test
+    void 세자리_랜덤_생성시_모두_다른값이_나온다(){
+        computerNumberGenerator.makeThreeDigitNumbers();
+        for(int numberEach : computerNumberGenerator.returnNumbersToArray()){
+            assertThat(numberEach).isLessThanOrEqualTo(9).isGreaterThanOrEqualTo(1);
+        }
     }
 }
