b225f3476994b7d8d19837087858d690b5932c17
i960107 <i960107@naver.com>
2022-11-03 15:45:25 +0900

    test(BaseballNumber): add unit test code

--- src/test/java/baseball/BaseballNumberTest.java
+++ src/test/java/baseball/BaseballNumberTest.java
@@ -1,5 +1,168 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 public class BaseballNumberTest {
+
+    @Test
+    void 중복된_숫자가_있다면_예외_발생() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int count = 1; count <= BaseballNumber.NUMBER_COUNT; count++) {
+            numbers.add(BaseballNumber.MIN_NUMBER);
+        }
+
+        assertThatThrownBy(() -> new BaseballNumber(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(BaseballNumber.DUPLICATE_NUMBER_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    void 숫자_개수가_규칙보다_적으면_예외_발생() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int count = 1; count <= 2; count++) {
+            numbers.add(count);
+        }
+        assertThatThrownBy(() -> new BaseballNumber(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(BaseballNumber.NOT_PROPER_SIZE_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    void 숫자_개수가_규칙보다_많으면_예외_발생() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int count = 1; count <= 4; count++) {
+            numbers.add(count);
+        }
+        assertThatThrownBy(() -> new BaseballNumber(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(BaseballNumber.NOT_PROPER_SIZE_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    void 유효한_숫자가_범위를_벗어나면_에러발생2() {
+        List<Integer> numbers = List.of(10, 1, 2);
+        assertThatThrownBy(() -> new BaseballNumber(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(BaseballNumber.OUT_OF_RANGE_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    void 유효한_숫자가_범위를_벗어나면_에러발생3() {
+        List<Integer> numbers = List.of(0, 1, 2);
+        assertThatThrownBy(() -> new BaseballNumber(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(BaseballNumber.OUT_OF_RANGE_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    void 유효한_숫자가_범위를_벗어나면_에러발생4() {
+        List<Integer> numbers = List.of(-1, 1, 2);
+        assertThatThrownBy(() -> new BaseballNumber(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(BaseballNumber.OUT_OF_RANGE_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    void 유효한_숫자가_범위를_벗어나면_에러발생5() {
+        List<Integer> numbers = List.of(1000000, -1000000, 2);
+        assertThatThrownBy(() -> new BaseballNumber(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(BaseballNumber.OUT_OF_RANGE_EXCEPTION_MESSAGE);
+    }
+
+    @Test
+    void 컴퓨터가_선택한_숫자는_항상_올바르다() {
+        BaseballNumber computerNumbers = BaseballNumber.getComputerPickedNumbers();
+        List<Integer> retrievedNumbers = new ArrayList<>();
+        for (int index = 0; index < BaseballNumber.NUMBER_COUNT; index++) {
+            retrievedNumbers.add(computerNumbers.get(index));
+        }
+        assertDoesNotThrow(() -> new BaseballNumber(retrievedNumbers));
+    }
+
+    @Test
+    void 특정_인덱스의_숫자를_가져온다() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int index = 0; index < BaseballNumber.NUMBER_COUNT; index++) {
+            numbers.add(index + 1);
+        }
+
+        BaseballNumber baseballNumber = new BaseballNumber(numbers);
+
+        for (int index = 0; index < BaseballNumber.NUMBER_COUNT; index++) {
+            int expected = numbers.get(index);
+            int result = baseballNumber.get(index);
+            assertThat(result).isEqualTo(expected);
+        }
+    }
+
+    @Test
+    void 특정_인덱스의_숫자가_맞다면_참_반환() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int index = 0; index < BaseballNumber.NUMBER_COUNT; index++) {
+            numbers.add(index + 1);
+        }
+
+        BaseballNumber baseballNumber = new BaseballNumber(numbers);
+
+        boolean expected = true;
+
+        for (int index = 0; index < BaseballNumber.NUMBER_COUNT; index++) {
+            boolean result = baseballNumber.is_number_in_index_equal_to(index, numbers.get(index));
+            assertThat(result).isEqualTo(expected);
+        }
+
+    }
+
+    @Test
+    void 특정_인덱스의_숫자가_틀리다면_거짓_반환() {
+        List<Integer> numbers = new ArrayList<>();
+        for (int index = 0; index < BaseballNumber.NUMBER_COUNT; index++) {
+            numbers.add(index + 1);
+        }
+
+        BaseballNumber baseballNumber = new BaseballNumber(numbers);
+
+        boolean expected = false;
+        boolean result = baseballNumber.is_number_in_index_equal_to
+                (BaseballNumber.NUMBER_COUNT - 1, -1);
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void 숫자를_포함하고_있다면_참_반환() {
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int index = 0; index < BaseballNumber.NUMBER_COUNT; index++) {
+            numbers.add(index + 1);
+        }
+
+        BaseballNumber baseballNumber = new BaseballNumber(numbers);
+
+        boolean expected = true;
+        boolean result = baseballNumber.contains(baseballNumber.get(BaseballNumber.NUMBER_COUNT - 1));
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void 숫자를_포함하지_않는다면_거짓_반환() {
+
+        List<Integer> numbers = new ArrayList<>();
+        for (int index = 0; index < BaseballNumber.NUMBER_COUNT; index++) {
+            numbers.add(index + 1);
+        }
+
+        BaseballNumber baseballNumber = new BaseballNumber(numbers);
+
+        boolean expected = false;
+        boolean result = baseballNumber.contains(-1);
+        assertThat(result).isEqualTo(expected);
+    }
 
 }
