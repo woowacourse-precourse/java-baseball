be77b079c17bb9b429995fde78f9d3f5f8c9a02e
sunwng <sunwgna@gmail.com>
2022-11-03 14:45:00 +0900

    feat: feature #5

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,30 @@ import camp.nextstep.edu.missionutils.*;
 
 public class Application {
     static String answer;
+    static int[] hint; // index 0: 볼, index 1: 스트라이크
+
+    static void findHint(String strIn) {
+        for (int i = 0; i < strIn.length(); i++) {
+            if (answer.charAt(i) == strIn.charAt(i)) {
+                hint[1]++;
+            } else if (answer.indexOf(strIn.charAt(i)) != -1) {
+                hint[0]++;
+            }
+        }
+    }
+
+    static void generateHint(String strIn) {
+        findHint(strIn);
+        StringBuilder hintOut = new StringBuilder();
+        if (hint[0] > 0) {
+            hintOut.append(String.format("%d볼 ", hint[0]));
+        } else if (hint[1] > 0) {
+            hintOut.append(String.format("%d스트라이크", hint[1]));
+        } else {
+            hintOut.append("낫싱");
+        }
+        System.out.println(hintOut.toString());
+    }
 
     static String checkUserInput(String strIn) {
         UserInputException.lengthCheck(strIn);
@@ -38,9 +62,12 @@ public class Application {
     }
     static void initGame() {
         answer = generateAnswer();
+        hint = new int[2];
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String tes = "123";
+        System.out.println(tes.indexOf('2'));
     }
 }
