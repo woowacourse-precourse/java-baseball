dc80115a96a78207b5f26266060d36a1f440f059
jiwon1217 <koagjw@gmail.com>
2022-11-04 01:27:22 +0900

    feat(Game.java): 입력한 수에 대한 게임 결과를 출력

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -11,6 +11,12 @@ public class Game {
     public static final String INVALID_INPUT_LENGTH = "숫자를 3개 입력해야 합니다.";
     public static final String INVALID_INPUT_TYPE = "숫자만 입력해야 합니다.";
     public static final String DUPLICATE_INPUT_NUMBER = "중복된 수를 입력할 수 없습니다.";
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String NOTHING = "낫싱";
+    public static final String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String NEW_LINE = System.lineSeparator();
+    public static final String BLANK = " ";
 
     public List<Integer> generateRandomNumber() {
         List<Integer> numbers = new ArrayList<>();
@@ -49,5 +55,15 @@ public class Game {
     public boolean hasDuplicateNumber(String inputNumbers) {
         return inputNumbers.charAt(0) == inputNumbers.charAt(1) || inputNumbers.charAt(1) == inputNumbers.charAt(2)
                 || inputNumbers.charAt(0) == inputNumbers.charAt(2);
+    }
+
+    public String printResult(int strikeCount, int ballCount) {
+        if (strikeCount == 0 && ballCount == 0) {
+            return NOTHING;
+        }
+        if (strikeCount == 3) {
+            return strikeCount + STRIKE + NEW_LINE + GAME_OVER;
+        }
+        return ballCount + BALL + BLANK + strikeCount + STRIKE;
     }
 }
