5d82463f2c031be197743a9a749401d558752dc4
win9 <krrr8@naver.com>
2022-11-04 01:36:43 +0900

    Feat: 콘솔입력부분 구현
    요구사항에 따라 camp.nextstep.edu.missionutils.Console를 이용하여
    사용자가 scanner를 이용하여 콘솔에 입력하도록 개발

---
+++ src/main/java/baseball/model/User.java
@@ -0,0 +1,11 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Scanner;
+
+public class User {
+    public void inputNumber(){
+        Console.readLine();
+    }
+}
