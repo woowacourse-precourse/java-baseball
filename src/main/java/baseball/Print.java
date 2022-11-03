91a063b6b16541e3d17e1534f807644c45f63e15
dooboocookie <bkh3030@gmail.com>
2022-11-03 14:17:56 +0900

    feat: Print.java 추가, 라운드 시작 시 숫자 입력 메세지 추가

---
+++ src/main/java/baseball/Print.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class Print {
+
+    private static final String ROUND_START_MESSAGE = "숫자를 입력해주세요 : ";
+
+    private static void printRoundStart() {
+        System.out.print(ROUND_START_MESSAGE);
+    }
+
+}
