f9f4f1ca632d38cded0ec34c282c817800805879
junhei Lee <jun2ee22@naver.com>
2022-11-03 18:20:56 +0900

    feat: 재시작여부 확인하는 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,25 +1,42 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class Game {
     protected static final int answerLength = 3;
-    public static List<Integer> correctAnswer = new ArrayList<>();
+    private static final int newGame = 1;
+    private static final int endGame = 2;
 
+    public static List<Integer> correctAnswer = new ArrayList<>();
+    private int newOrEnd = 0;
+
+    AnswerSheet answerSheet = new AnswerSheet();
+    Hint hint = new Hint();
 
     public void start() {
         guideToStart();
-        setCorrectAnswer();
+        setComputerAnswer();
+
+        while (true) {
+            answerSheet.getUserAnswer();
+            if (answerSheet.isCorrectAnswer(hint.getHint())) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+        }
+        askContinue();
     }
 
     private void guideToStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    private void setCorrectAnswer() {
+    private void setComputerAnswer() {
         correctAnswer.clear();
 
         while (correctAnswer.size() < answerLength) {
@@ -30,10 +47,16 @@ public class Game {
         }
     }
 
+    private void askContinue() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        newOrEnd = Integer.parseInt(Console.readLine());
 
-
-
-
-
-
+        if (newOrEnd == newGame) {
+            this.start();
+        } else if (newOrEnd == endGame) {
+            return;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
 }
