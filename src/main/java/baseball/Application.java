faba3ce0791c0822cb3b01e1f86b692982b68d02
Jueun <jueun000101@gmail.com>
2022-11-03 17:52:09 +0900

    feat: pick random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,11 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -23,6 +29,18 @@ public class Application {
             }
             used[ch - '0'] = true;
         }
+    }
+
+    static String pickRandomNumber() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        List<String> stringNumbers = numbers.stream().map(String::valueOf).collect(Collectors.toList());
+        return String.join("", stringNumbers);
     }
 
     public static void main(String[] args) {
