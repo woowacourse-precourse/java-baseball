35c557aa6179b0a1b9cb8fade366752468f9ad06
EUNJEONGMUN <munej26@gmail.com>
2022-11-04 02:16:34 +0900

    test: 게임종료 후 재시작 것헙 예시 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,6 +21,16 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 게임종료_후_재시작_깃헙예시() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                        run("123", "145", "671", "216", "713", "1","123", "452", "2");
+                        assertThat(output()).contains("1볼 1스트라이크", "1볼", "2볼", "1스트라이크", "3스트라이크", "게임 종료", "1볼");
+                },
+                7, 1, 3, 4, 5, 2
+        );
+    }
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
