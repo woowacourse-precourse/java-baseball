9611fc45c34f476c07742ddd914ca88e91baaaf1
whdcks420 <whdcks420@gmail.com>
2022-11-03 22:13:32 +0900

    feat(view): 게임 재시작, 종료 출력 기능구현

---
+++ src/main/java/baseball/util/BaseballNumber.java
@@ -0,0 +1,25 @@
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Baseball {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int BALL_COUNT = 3;
+
+    public static String generateNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() != BALL_COUNT) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer.stream().map(String::valueOf).collect(Collectors.joining());
+    }
+
+}
--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -31,4 +31,8 @@ public class BaseballView {
         return strike + "스트라이크";
     }
 
+    public void showContinueOrStop() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
