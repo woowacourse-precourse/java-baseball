501df7cd6ac1c3d75c0da63765d82f1e3926e368
situodys <situodys@gmail.com>
2022-11-04 01:56:01 +0900

    feat(Baseballs): 1~9 이외의 문자가 포함된 경우 검증 구현

--- src/main/java/baseball/Baseballs.java
+++ src/main/java/baseball/Baseballs.java
@@ -2,6 +2,8 @@ package baseball;
 
 public class Baseballs {
     private static final int BASEBALL_COUNT = 3;
+    private static char MIN_VALID_RANGE = '1';
+    private static char MAX_VALID_RANGE = '9';
 
     public static Baseballs of(String userInput) {
         return new Baseballs(userInput);
@@ -17,6 +19,11 @@ public class Baseballs {
         }
         if (userInput.length() != BASEBALL_COUNT) {
             throw new IllegalArgumentException();
+        }
+        for (Character characterInUserInput : userInput.toCharArray()) {
+            if (characterInUserInput < MIN_VALID_RANGE || characterInUserInput > MAX_VALID_RANGE) {
+                throw new IllegalArgumentException();
+            }
         }
     }
 }
