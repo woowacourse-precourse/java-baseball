38baef4b229fdeccca8529c104ee4b7417444460
SH <gram753@naver.com>
2022-11-03 20:55:00 +0900

    feat: 상수 풀 update

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -7,9 +7,10 @@ public class Constant {
 	public static final String STRIKE = "스트라이크";
 	public static final String BALL = "볼";
 	public static final String NOTHING = "낫싱";
-	public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다!";
+	public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! ";
 	public static final String END_MESSAGE = "게임 종료";
-	public static final String REQUEST_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	public static final String RESTART_REQUEST = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	public static final String ERROR_MESSAGE = "잘못된 입력입니다.";
 	public static final String RESTART_GAME = "1";
 	public static final String END_GAME = "2";
 
