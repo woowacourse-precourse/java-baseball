aa80728ca60ec0e8c2ea62627a6241c4ecaf1c05
dhkim <akdlzmf1123@gmail.com>
2022-11-03 23:01:28 +0900

    test(BallsTest): Balls 클래스 BallsTest로 이름 변경

--- src/test/java/baseball/model/BallsTest.java
+++ src/test/java/baseball/model/BallsTest.java
@@ -1,11 +1,11 @@
-package baseball;
+package baseball.model;
 
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class Balls {
+class BallsTest {
     @Test
     void 같은_숫자_같은_위치_스트라이크_확인하기() {
         Balls computer = new Balls(135);
