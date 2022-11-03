b97aa556b7dd53096c2d35640cafb7a399e61c6f
김현재 <41482946+galug@users.noreply.github.com>
2022-11-03 13:32:22 +0900

    feat(baseballgame): add 사용자의 선택에 따라 게임을 반복하는 기능

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 public class BaseBallGame {
     private static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+    private boolean repeatFlag;
 
     public BaseBallGame() {
     }
@@ -11,5 +12,11 @@ public class BaseBallGame {
     public void startGame() {
         System.out.println(START_GAME);
         repeatGame();
+    }
+
+    public void repeatGame() {
+        while (repeatFlag == true) {
+            startNewGame();
+        }
     }
 }
