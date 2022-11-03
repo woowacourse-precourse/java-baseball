00b8b50e70b10b7dfb73eba729e62c81f26eb88f
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-11-03 17:07:46 +0900

    test: 컴퓨터 숫자 생성 테스트 수정

--- src/test/java/baseball/domain/ComputerNumberTest.java
+++ src/test/java/baseball/domain/ComputerNumberTest.java
@@ -8,8 +8,8 @@ import org.junit.jupiter.api.Test;
 
 class ComputerNumberTest {
 	@Test
-	void Random_길이_테스트() {
-		List<Integer> result = ComputerNumber.createRandomNumber();
+	void 숫자_길이_테스트() {
+		List<Integer> result = new ComputerNumber().computerNumber;
 		assertEquals(3, result.size());
 	}
 }
