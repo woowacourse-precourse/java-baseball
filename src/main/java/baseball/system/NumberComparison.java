19c3af58800623bf62fdabcf8db78bc02787f91e
myungsikyoon <yms8795@gmail.com>
2022-11-03 21:42:18 +0900

    feat(NumberComparison) : isNothing 구현.

--- src/main/java/baseball/system/NumberComparison.java
+++ src/main/java/baseball/system/NumberComparison.java
@@ -32,6 +32,10 @@ public class NumberComparison {
 
     }
 
+    private boolean isNothing(int strikeCount, int ballCount) {
+        return strikeCount == 0 && ballCount == 0;
+    }
+
     private void printAnswerText() {
         System.out.println("3스트라이크");
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
