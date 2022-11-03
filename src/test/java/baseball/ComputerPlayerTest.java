6ac0d3cd8f1df7648876ac59f09d451d2c7928b8
baexxbin <soo3886@gmail.com>
2022-11-03 19:44:26 +0900

    Feat(ComputerPlayerTest): 상대방 플레이어 클래스 테스트 생성
    상대방 플레이어의 숫자갯수 및 랜덤생성여부 테스트

---
+++ src/test/java/baseball/ComputerPlayerTest.java
@@ -0,0 +1,41 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class ComputerPlayerTest {
+
+	@DisplayName("상대플레이어의 숫자갯수 테스트")
+	@Test
+	void isRightDigitNumber() {
+		// given
+		ComputerPlayer computerPlayer = new ComputerPlayer();
+
+		// when
+		List<Integer> computerNumbers = computerPlayer.getNumberList();
+
+		// then
+		System.out.println("computerNumbers = " + computerNumbers);
+		assertThat(computerNumbers.size()).isEqualTo(GameRules.DIGIT_NUMBER.getCode());
+	}
+
+	@DisplayName("상대플레이어의 숫자랜덤생성여부 테스트")
+	@Test
+	void isRandomNumber() {
+		// given
+		ComputerPlayer computerPlayer = new ComputerPlayer();
+		ComputerPlayer otherPlayer = new ComputerPlayer();
+
+		// when
+		List<Integer> computerNumbers = computerPlayer.getNumberList();
+		List<Integer> otherNumbers = otherPlayer.getNumberList();
+
+		// then
+		assertThat(computerNumbers).isNotEqualTo(otherNumbers);
+	}
+
+}
