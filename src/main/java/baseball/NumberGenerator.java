bd62ef958d19fd7143fe1878df9892d47c662a8f
chlwodyd9829 <chlwodyd9829@naver.com>
2022-11-03 11:05:05 +0900

    feat: 1-9까지 서로 다른 수로 이루어진 수 생성

---
+++ src/main/java/baseball/NumberGenerator.java
@@ -0,0 +1,37 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberGenerator {
+    private static final int MIN_RANGE = 1;
+    private static final int MAX_RANGE = 9;
+    private static final int SIZE = 3;
+    private List<Integer> computer = new ArrayList<>();
+
+    public List<Integer> getComputer() {
+        return computer;
+    }
+
+    public void setComputer(List<Integer> computer) {
+        this.computer = computer;
+    }
+
+    public void init() {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (check(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+    private boolean check(int randomNumber){
+        if (!computer.contains(randomNumber)) {
+            return true;
+        }
+        return false;
+    }
+}
