aaf71befc16cec62b956943418fb2f6ea14d0356
eyeofsol <kingjihong@naver.com>
2022-11-03 17:07:43 +0900

    test(Game): 스트라이크, 볼 갯수 구하는 기능 테스트

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -2,6 +2,8 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,4 +17,58 @@ class GameTest {
         assertThat(game1.computer).isNotEqualTo(game2.computer);
     }
 
+    @Test
+    void getStrike_스트라이크_갯수_1() {
+        Game game = new Game();
+        game.computer = List.of(1, 2, 3);
+        List<Integer> player = List.of(1, 2, 3);
+
+        assertThat(game.getStrike(game.computer, player)).isEqualTo(3);
+    }
+
+    @Test
+    void getStrike_스트라이크_갯수_2() {
+        Game game = new Game();
+        game.computer = List.of(1, 2, 3);
+        List<Integer> player = List.of(1, 4, 3);
+
+        assertThat(game.getStrike(game.computer, player)).isEqualTo(2);
+    }
+
+    @Test
+    void getStrike_스트라이크_갯수_3() {
+        Game game = new Game();
+        game.computer = List.of(1, 2, 3);
+        List<Integer> player = List.of(3 , 2, 1);
+
+        assertThat(game.getStrike(game.computer, player)).isEqualTo(1);
+    }
+
+    @Test
+    void getBall_볼_갯수_1() {
+        Game game = new Game();
+        game.computer = List.of(1, 2, 3);
+        List<Integer> player = List.of(1, 2, 3);
+
+        assertThat(game.getBall(game.computer, player)).isEqualTo(0);
+    }
+
+    @Test
+    void getBall_볼_갯수_2() {
+        Game game = new Game();
+        game.computer = List.of(1, 2, 3);
+        List<Integer> player = List.of(3 , 2, 1);
+
+        assertThat(game.getBall(game.computer, player)).isEqualTo(2);
+    }
+
+    @Test
+    void getBall_볼_갯수_3() {
+        Game game = new Game();
+        game.computer = List.of(1, 2, 3);
+        List<Integer> player = List.of(3 , 1, 2);
+
+        assertThat(game.getBall(game.computer, player)).isEqualTo(3);
+    }
 }
+
