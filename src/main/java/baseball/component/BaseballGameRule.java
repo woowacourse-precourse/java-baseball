4e5949c3941d4e0783bc896edd1804a4c10a2bfb
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-03 14:30:00 +0900

    feat(BaseballGameRule): 스트라이크 수 확인

--- src/main/java/baseball/component/BaseballGameRule.java
+++ src/main/java/baseball/component/BaseballGameRule.java
@@ -15,5 +15,16 @@ public class BaseballGameRule {
         this.computerNumbers = computerNumbers;
     }
 
+    private int verifyStrikes(List<Integer> playerNumbers) {
+
+        int strikeCount = 0;
+        for(int index = 0; index<computerNumbers.size(); index++){
+            if(playerNumbers.get(index).equals(computerNumbers.get(index))){
+                strikeCount += 1;
+            }
+        }
+
+        return strikeCount;
+    }
 
 }
