e9eb94f47d5c6a4e8dbe23d7563b6e6d96ff5400
cpot5620 <=cpot5620@naver.com>
2022-11-03 16:45:47 +0900

    feat(BaseBallGameView): create method printing game result

---
+++ src/main/java/baseball/BaseBallGameView.java
@@ -0,0 +1,27 @@
+package baseball;
+
+public class BaseBallGameView {
+    private static String NOTHING = "낫싱";
+    private static String BALL = "볼";
+    private static String STRIKE = "스트라이크";
+    private static String FINISH = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static void printResult(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            System.out.println(NOTHING);
+            return;
+        }
+
+        if (strike == 3) {
+            System.out.println(strike + STRIKE);
+            System.out.println(FINISH);
+            return;
+        }
+
+        if (ball != 0)
+            System.out.print(ball + BALL + " ");
+
+        if (strike != 0)
+            System.out.println(strike + STRIKE);
+    }
+}
---
+++ src/test/java/baseball/BaseBallGameViewTest.java
@@ -0,0 +1,59 @@
+package baseball;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+class BaseBallGameViewTest {
+    final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    final PrintStream standardOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    void 결과_출력_1() {
+        String result = "1볼 1스트라이크";
+        BaseBallGameView.printResult(1, 1);
+        Assertions.assertEquals(result, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void 결과_출력_2() {
+        String result = "낫싱";
+        BaseBallGameView.printResult(0, 0);
+        Assertions.assertEquals(result, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void 결과_출력_3() {
+        String result = "1볼 1스트라이크";
+        BaseBallGameView.printResult(1, 1);
+        Assertions.assertEquals(result, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void 결과_출력_4() {
+        String result = "3볼";
+        BaseBallGameView.printResult(3, 0);
+        Assertions.assertEquals(result, outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    void 결과_출력_5() {
+        String result = "3스트라이크" + System.lineSeparator() + "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+        BaseBallGameView.printResult(0, 3);
+        Assertions.assertEquals(result, outputStreamCaptor.toString().trim());
+    }
+}
