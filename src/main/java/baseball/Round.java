c75f63c2f0c16c9c1ee6ed223e4ccd4268280348
dooboocookie <bkh3030@gmail.com>
2022-11-03 14:27:03 +0900

    feat: Round.jav 추가, 라운드 시작 시 메세지 출력과 Numbers 입력 받는 기능 추가

--- src/main/java/baseball/Round.java
+++ src/main/java/baseball/Round.java
@@ -1,2 +1,25 @@
-package baseball;public class Round {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Round {
+
+    private Computer computer;
+    private Numbers numbers;
+
+    public Round(Computer computer) {
+        this.computer = computer;
+        startNewRound();
+    }
+
+    private void startNewRound() {
+        Print.printRoundStart();
+        String inputString = Console.readLine();
+        inputNewNumbers(inputString);
+    }
+
+    private void inputNewNumbers(String inputString) {
+        int input = Integer.parseInt(inputString);
+        numbers = new Numbers(input);
+    }
 }
