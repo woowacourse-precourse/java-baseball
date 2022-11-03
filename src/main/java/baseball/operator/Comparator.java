326b464e3b772a265cb4e51354ad6d75e7cfd1a9
SH <gram753@naver.com>
2022-11-03 20:57:02 +0900

    refactor: operator 클래스 분리

---
+++ src/main/java/baseball/operator/Comparator.java
@@ -0,0 +1,73 @@
+package baseball.operator;
+
+import static baseball.Constant.*;
+
+import baseball.Computer;
+import baseball.User;
+
+public class Comparator {
+	Computer computer;
+	User user;
+
+	public Comparator(Computer computer, User user) {
+		this.computer = computer;
+		this.user = user;
+	}
+
+	static int strikeCount = 0;
+	static int ballCount = 0;
+
+	public Comparator() {
+		this.computer = new Computer();
+		this.user = new User();
+	}
+
+	public void printHintMessage() {
+		if (ballCount == 0 && strikeCount == 0) {
+			System.out.println(NOTHING);
+		}
+		if (ballCount != 0 && strikeCount == 0) {
+			System.out.println(ballCount + BALL);
+		}
+		if (strikeCount != 0 && ballCount == 0) {
+			System.out.println(strikeCount + STRIKE);
+		}
+		if (strikeCount != 0 && strikeCount != NUM_LENGTH && ballCount != 0) {
+			System.out.println(ballCount + BALL + " " + strikeCount + STRIKE);
+		}
+		if (strikeCount == NUM_LENGTH) {
+			System.out.printf(WIN_MESSAGE);
+			System.out.println(END_MESSAGE);
+		}
+	}
+
+	public void setCounts() {
+		for (int i = 0; i < NUM_LENGTH; i++) {
+			if (!isStrike(i)) {
+				isBall(i);
+			}
+		}
+	}
+
+	public void resetCounts() {
+		this.strikeCount = 0;
+		this.ballCount = 0;
+	}
+
+	public boolean isStrike(int index) {
+		if (computer.answer.get(index).equals(user.numbers.get(index))) {
+			strikeCount++;
+			return true;
+		}
+		return false;
+	}
+
+	public boolean isBall(int index) {
+		if (computer.answer.contains(user.numbers.get(index))) {
+			ballCount++;
+			return true;
+		}
+		return false;
+	}
+
+}
---
+++ src/main/java/baseball/operator/Operator.java
@@ -0,0 +1,56 @@
+package baseball.operator;
+
+import static baseball.Constant.*;
+
+import baseball.Computer;
+import baseball.User;
+import camp.nextstep.edu.missionutils.Console;
+
+public class Operator {
+
+	User user = new User();
+	Computer computer = new Computer();
+	Comparator comparator = new Comparator(computer, user);
+	boolean checkGameOver = false;
+
+	public void run() {
+		System.out.println(START_MESSAGE);
+		do {
+			gameSystem();
+		} while (!checkGameOver);
+
+		requestRestart();
+	}
+
+	public void gameSystem() {
+		user.play();
+		comparator.setCounts();
+		comparator.printHintMessage();
+		checkGameIsOver();
+		comparator.resetCounts();
+	}
+
+	public void checkGameIsOver() {
+		if (Comparator.strikeCount == NUM_LENGTH) {
+			this.checkGameOver = true;
+		}
+	}
+
+	public boolean requestRestart() {
+		String request;
+		System.out.println(RESTART_REQUEST);
+		request = Console.readLine();
+		isValidate(request);
+		if (request.equals(RESTART_GAME)) {
+			return true;
+		}
+		return false;
+	}
+
+	private void isValidate(String request){
+		if(!request.equals(RESTART_GAME) && request.equals(END_GAME)){
+			throw new IllegalArgumentException();
+		}
+	}
+
+}
