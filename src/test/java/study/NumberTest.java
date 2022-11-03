aa9751b5e0e10f4e03e470203793fd41f39f3351
bsh1603 <bsh1603@github.com>
2022-11-03 13:29:20 +0900

    test: 랜덤숫자 3자리에서 중복된 숫자 검사 테스트

--- src/test/java/study/NumberTest.java
+++ src/test/java/study/NumberTest.java
@@ -4,7 +4,8 @@ import domain.RandomNumber;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,5 +36,20 @@ public class NumberTest {
 
         //then
         assertEquals(NUM_SIZE, computer.getNumbers().length);
+    }
+
+    @Test
+    public void 중복숫자검사() throws Exception{
+        //given
+        RandomNumber computer = new RandomNumber();
+        computer.setRandomNumber();
+        int[] nums = computer.getNumbers();
+        Set<Integer> compare = new HashSet<>();
+        //when
+        for (int i = 0; i < nums.length; i++) {
+            compare.add(nums[i]);
+        }
+        //then
+        assertEquals(nums.length, compare.size());
     }
 }
