f49aaf67aa6cf77bace33933e51984b37f10c0da
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-03 18:13:16 +0900

    feat(Baseball): Baseball 클래스 생성
    Baseball의 메소드 목록:
    - 1~9 사이 서로 다른 세 자연수로 이루어진 문자열 생성 메서드
    - 숫자로 이루어진 문자열을 자리수별 정수 리스트로 변환하는 메서드

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,2 +1,39 @@
-package baseball;public class Baseball {
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Randoms.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Baseball {
+	private static final int PLAYING_NUMBER_SIZE = 3;
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 9;
+
+	static String make3DigitNumber() {
+		StringBuilder stringBuilder = new StringBuilder();
+		List<Integer> prev = new ArrayList<>();
+
+		while (stringBuilder.length() != PLAYING_NUMBER_SIZE) {
+			int newNumber = pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+
+			if (!prev.contains(newNumber)) {
+				stringBuilder.append(newNumber);
+				prev.add(newNumber);
+			}
+		}
+		return stringBuilder.toString();
+	}
+
+	static List<Integer> splitDigits(String threeDigitNumber) {
+		if (threeDigitNumber.equals("")) {
+			return Collections.emptyList();
+		}
+		List<String> splitString = new ArrayList<>(Arrays.asList(threeDigitNumber.split("")));
+		return splitString.stream().map(Integer::parseInt).collect(Collectors.toList());
+	}
+
 }
