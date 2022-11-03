a3d9b3f7219a472c6c753de197f6a2dcd06d0701
swonny <wonny921@gmail.com>
2022-11-03 19:40:42 +0900

    feat: 선택한 수가 기존에 선택한 수와 중복되는 수인지 판별

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+}
+
+class Computer {
+    static List<Integer> numbers;
+
+    private static boolean hasNumber(Integer number) {
+        if (numbers.contains(number)) {
+            return true;
+        }
+        return false;
     }
 }
