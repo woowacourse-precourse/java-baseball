b8df525754b249b0fd336a008fadf1f2780aa569
JunHee <ljh19980823@gmail.com>
2022-11-03 22:09:34 +0900

    feat : 게임 시작 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -88,6 +88,7 @@ public class Application {
     }
 
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         List<Integer> computerNumber = makeRandomNumber();
         boolean flag = false;
         while (!flag) {
