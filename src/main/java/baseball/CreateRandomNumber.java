5d8b5ea26612c1c7496ef47e32b16652cf41790f
Sinryuji <siler2474@gmail.com>
2022-11-03 16:33:31 +0900

    feat: CreateRandomNumber
    각 자릿수가 서로 다른 랜덤한 3자리 숫자를 생성하여 반환함.

---
+++ src/main/java/baseball/CreateRandomNumber.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import java.util.List;
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class CreateRandomNumber {
+    public List<Integer> create() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+}
