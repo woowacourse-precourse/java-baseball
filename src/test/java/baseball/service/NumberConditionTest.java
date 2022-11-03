65b443a0fe5ceb78dff50da26190491f689c7b51
SEONMl <sm000117@naver.com>
2022-11-04 00:28:03 +0900

    test: 코드 수정에 따라서 테스트 코드 수정

--- src/test/java/baseball/service/NumberConditionTest.java
+++ src/test/java/baseball/service/NumberConditionTest.java
@@ -9,24 +9,28 @@ class NumberConditionTest {
     private NumberCondition numberCondition = new NumberCondition("123");
 
     @Test
-    @DisplayName("몇 개의 숫자가 포함되는지")
-    void count_how_many_contain_number() {
-        int correct1 = numberCondition.countContainmentRightness("192");
-        int correct2 = numberCondition.countContainmentRightness("375");
-        int correct3 = numberCondition.countContainmentRightness("231");
-
-        assertThat(correct1).isEqualTo(2);
-        assertThat(correct2).isEqualTo(1);
-        assertThat(correct3).isEqualTo(3);
+    @DisplayName("자릿수가 일치하는지")
+    void testDigitCorrect_true() {
+        boolean mock = numberCondition.isDigitCorrect("1", 0);
+        assertThat(mock).isTrue();
     }
-
     @Test
-    @DisplayName("자릿수가 몇 개가 일치하는지")
-    void count_how_many_correct_digit() {
-        int correct1 = numberCondition.countDigitRightness("168");
-        int correct2 = numberCondition.countDigitRightness("231");
-
-        assertThat(correct1).isEqualTo(1);
-        assertThat(correct2).isEqualTo(0);
+    @DisplayName("자릿수가 일치하는지")
+    void testDigitCorrect_false() {
+        boolean mock = numberCondition.isDigitCorrect("9", 0);
+        assertThat(mock).isFalse();
     }
+    @Test
+    @DisplayName("입력값을 포함하는지")
+    void testContainsNumber_true() {
+        boolean mock = numberCondition.doesContain("3");
+        assertThat(mock).isTrue();
+    }
+    @Test
+    @DisplayName("입력값을 포함하는지")
+    void testContainsNumber_false() {
+        boolean mock = numberCondition.doesContain("9");
+        assertThat(mock).isFalse();
+    }
+
 }
