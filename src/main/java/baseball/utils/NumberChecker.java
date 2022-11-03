06deca2ba7c7d819aebb0c66b08372bfc19ca77c
Jeangyu <gjwlsrb1700@naver.com>
2022-11-03 16:54:01 +0900

    [ADD] 게임을 종료할지 안할지 확인 시 올바른 값이 들어왔는지 확인하는 함수 구현

--- src/main/java/baseball/utils/NumberChecker.java
+++ src/main/java/baseball/utils/NumberChecker.java
@@ -2,21 +2,32 @@ package baseball.utils;
 
 public class NumberChecker {
 
-    public void checkRange(String num){
+    public static void checkUserInput(String userInput){
+        checkRange(userInput);
+        isNumber(userInput);
+        hasSameNumber(userInput);
+    }
+    private static void checkRange(String num){
         if (num.length() != 3){
             throw new IllegalArgumentException("문자의 길이가 다릅니다.");
         }
     }
 
-    public void isNumber(String num){
+    private static void isNumber(String num){
         if (!num.matches("[1-9][1-9][1-9]")){
             throw new IllegalArgumentException("숫자를 입력해주세요.");
         }
     }
 
-    public void hasSameNumber(String num){
+    private static void hasSameNumber(String num){
         if (num.charAt(0) == num.charAt(1) || num.charAt(1) == num.charAt(2) || num.charAt(0) == num.charAt(2)){
             throw new IllegalArgumentException("반복된 숫자를 입력했습니다.");
+        }
+    }
+
+    public static void isOneOrTwo(String num){
+        if (!(num.equals("1") || num.equals("2"))){
+            throw new IllegalArgumentException("입력 값으로 1, 2만 허용됩니다.");
         }
     }
 }
