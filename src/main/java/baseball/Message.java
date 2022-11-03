7dd02f256a517d925529058a40f20875fbb7b609
Jeongthingamajig <netpetet@naver.com>
2022-11-04 00:12:12 +0900

    Feat : 클래스 추가
    유저의 행동에 따라 출력하는 메세지를 모아놓은 클래스

---
+++ src/main/java/baseball/Message.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class Message {
+    private final static String STRIKE_MESSAGE = "스트라이크";
+    private final static String BALL_MESSAGE = "볼";
+    private final static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private final static String SET_USER_INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private final static String GAME_CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    private final static String RESTART_QUESTION_TO_USER_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
