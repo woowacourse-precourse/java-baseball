8b60d6d1525b7932496c891bcd24b3ba3d9d133c
wugawuga <dnr3655@naver.com>
2022-11-04 00:36:07 +0900

    feat: 정답 리스트 생성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() != 3) {
+            int random = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(random)) {
+                computer.add(random);
+            }
+        }
     }
 }
