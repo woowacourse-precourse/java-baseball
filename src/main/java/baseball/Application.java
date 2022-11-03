38b1706210e86debabd8cb59120696edc5c4d301
곽준영 <kwak@gwagjun-yeong-ui-MacBookPro.local>
2022-11-04 02:47:25 +0900

    Random값 추출과 사용자 입력값 받기를 제시된 라이브러리를 통해 받도록 수정.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.Scanner;
 import java.util.Random;
 
 public class Application {
+
+    static void print_result(int ball, int strike) {
+        if (ball != 0) System.out.print(ball + "볼 ");
+        if (strike != 0) System.out.println(strike + "스트라이크");
+    }
+
     static int count_strike(int user_number, int computer_number) {
         int ball = 0;
         int strike = 0;
@@ -12,12 +20,9 @@ public class Application {
         for (int i = 0; i < 3; i++) {
             if (user_str.charAt(i) == computer_str.charAt(i)) strike += 1;
             else if (user_str.indexOf(computer_str.charAt(i)) != -1) ball += 1;
-
         }
 
-        if (ball != 0) System.out.print(ball + "볼 ");
-
-        if (strike != 0) System.out.println(strike + "스트라이크");
+        print_result(ball, strike);
 
         return strike;
     }
@@ -25,17 +30,20 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Scanner sc = new Scanner(System.in);
-        Random rd = new Random();
-        int computer_number = (int) ((Math.random() * (899)) + 100);
+//        int computer_number = (int) ((Math.random() * (899)) + 100);
+        int computer_number = Randoms.pickNumberInRange(100, 1000);
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         int strike = 0;
         int input_number;
-        int[] count_result = {-1, -1};
         while (strike != 3) {
             System.out.print("숫자를 입력해주세요: ");
-            input_number = sc.nextInt();
-            strike = count_strike(input_number, computer_number);
+            try {
+                input_number = Integer.parseInt(Console.readLine());
+                strike = count_strike(input_number, computer_number);
+            } catch (IllegalArgumentException e) {
+                break;
+            }
         }
 
     }
