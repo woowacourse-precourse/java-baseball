1a3353b4ac1ee5f7d75f3c1835897e9940ed7f77
greeng00se <thegreengoose7@gmail.com>
2022-11-04 00:16:47 +0900

    test(OutputView): 계층 구조의 테스트 코드로 수정

--- src/test/java/baseball/view/OutputViewTest.java
+++ src/test/java/baseball/view/OutputViewTest.java
@@ -6,8 +6,15 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("OutputView 클래스")
+@SuppressWarnings({"InnerClassMayBeStatic", "NonAsciiCharacters"})
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class OutputViewTest {
     private final ByteArrayOutputStream output = new ByteArrayOutputStream();
     private final OutputView sut = OutputView.INSTANCE;
@@ -23,9 +30,19 @@ class OutputViewTest {
         output.reset();
     }
 
-    @Test
-    void 게임을_시작하면_숫자_야구_게임을_시작합니다_라고_화면에_출력한다() {
-        sut.startBaseballGame();
-        assertThat(output.toString()).isEqualTo("숫자 야구 게임을 시작합니다.\n");
+    @Nested
+    class startBaseballGame_메서드는 {
+
+        @Nested
+        class 숫자_야구_게임을_시작합니다_라는_문자를 {
+            private final String consolePrint = "숫자 야구 게임을 시작합니다.\n";
+
+            @Test
+            void 화면에_출력한다() {
+                sut.startBaseballGame();
+                assertThat(output.toString()).isEqualTo(consolePrint);
+            }
+        }
     }
+
 }
