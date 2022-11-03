13d9d909078f0395eaa3d11a43c581bea8090314
Jinuk <wlsdnr214@gmail.com>
2022-11-03 17:26:21 +0900

    feat: 서로 다른 3자리의 수를 입력 받기 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.*;
 
 
@@ -17,5 +18,31 @@ public class Application {
             }
         }
 
+        List<Integer> numberList = inputNum();
+        System.out.println(numberList);
+
     }
+
+    //입력 메서드
+    public static List<Integer> inputNum() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String number = Console.readLine();
+
+        boolean isNumeric = number.chars().allMatch(Character::isDigit);
+
+        List<Integer> numberList = new ArrayList<>();
+        for(int i = 0; i<3; i++){
+            char charNum = number.charAt(i);
+            int num = Character.getNumericValue(charNum);
+            if (!numberList.contains(num)) {
+                numberList.add(num);
+            }
+        }
+        if (number.length() == 3 && isNumeric == true && numberList.size() == 3) { //서로 다른 수 추가
+            return numberList;
+        } else {
+            throw new IllegalArgumentException("잘못된 값입니다.");
+        }
+    }
+
 }
