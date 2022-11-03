b908ecdc2549bf8fc85071448bb82ee68b324a78
myungsikyoon <yms8795@gmail.com>
2022-11-03 15:15:05 +0900

    feat(GameSystem) : runGame() 구현.

--- src/main/java/baseball/system/GameSystem.java
+++ src/main/java/baseball/system/GameSystem.java
@@ -1,4 +1,30 @@
 package baseball.system;
 
 public class GameSystem {
+
+    public void runGame() {
+
+        printRunGameText();
+
+        for (; ; ) {
+
+            startGame();
+
+            if (!selectMenu()) {
+                break;
+            }
+            
+        }
+
+
+    }
+
+    private void startGame() {
+
+    }
+
+    private void printRunGameText() {
+    }
+
+
 }
