bedd0d0c9098c1e8ad8827c52481f5fa7b39b074
TaeHwan <kimta2hwan@gmail.com>
2022-11-03 19:12:02 +0900

    feat(outputview): 입력 안내 문구를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,8 +3,13 @@ package baseball.view;
 public class OutputView {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String ANSWER_INPUT_GUIDE = "숫자를 입력해주세요 : ";
 
     public static void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
+    }
+
+    public static void printAnswerInputGuide() {
+        System.out.println(ANSWER_INPUT_GUIDE);
     }
 }
