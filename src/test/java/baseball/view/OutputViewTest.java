9ff0c9edefebd6f0c2d87ff51b793ef028dfb4aa
ksh-beginner <ohk9134@naver.com>
2022-11-03 21:41:38 +0900

    test(OutputViewTest) : System.out.println()을 테스트하기 위한 초기 설정 추가

--- src/test/java/baseball/view/OutputViewTest.java
+++ src/test/java/baseball/view/OutputViewTest.java
@@ -1,5 +1,26 @@
 package baseball.view;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class OutputViewTest {
-    
+
+    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(output));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(System.out);
+        output.reset();
+    }
 }
