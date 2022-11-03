93b1538e5884710d701e9bd655756fb88954c390
chaos0103 <lyt1228@naver.com>
2022-11-03 10:36:49 +0900

    feat: 서로 다른 수로 이루어진 3자리의 수를 생성하는 기능 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,36 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    private final static int MIN_RANGE = 1;
+    private final static int MAX_RANGE = 9;
+    private final static int MAX_SIZE = 3;
+
+    List<Integer> computerNumber = new ArrayList<>();
+
+    public List<Integer> createComputerNumber() {
+        while (isMaxSize()) {
+            addComputerNumber(Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE));
+        }
+        return computerNumber;
+    }
+
+    private boolean isMaxSize() {
+        return computerNumber.size() < MAX_SIZE;
+    }
+
+    private void addComputerNumber(int number) {
+        if (isNotContains(number)) {
+            computerNumber.add(number);
+        }
+    }
+
+    private boolean isNotContains(int number) {
+        return !computerNumber.contains(number);
+    }
+}
