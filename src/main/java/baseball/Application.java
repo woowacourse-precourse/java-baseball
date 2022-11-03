b34ed0cb5af307cbd8afcfb1dd35ec80c3617aa7
sugamypapa <sk000801@naver.com>
2022-11-03 15:04:07 +0900

    refactor: 볼/스트라이크 결과 함수 분리
    함수가 하나의 기능만 갖도록 더 쪼갬

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,36 +18,33 @@ public class Application {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
-
     public static List<Integer> getRandomNumber() {
         List<Integer> randomNumber = new ArrayList<>();
-        for(int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             randomNumber.add(Randoms.pickNumberInRange(1, 9));
         }
         return randomNumber;
     }
-
     public static boolean isEnterNumberExcept(String userEnterNumber) {
         boolean isExcepted = false;
         List<Integer> OneToNine = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-        if(userEnterNumber.length() != 3) {
+        if (userEnterNumber.length() != 3) {
             isExcepted = true;
-        } else if(userEnterNumber.charAt(0) == userEnterNumber.charAt(1) || userEnterNumber.charAt(1) == userEnterNumber.charAt(2)
+        } else if (userEnterNumber.charAt(0) == userEnterNumber.charAt(1) || userEnterNumber.charAt(1) == userEnterNumber.charAt(2)
             || userEnterNumber.charAt(0) == userEnterNumber.charAt(2)) {
             isExcepted = true;
         }
-        for(int i = 0; i < 3; i++) {
-            if(!OneToNine.contains(Character.getNumericValue(userEnterNumber.charAt(i)))) {
+        for (int i = 0; i < 3; i++) {
+            if (!OneToNine.contains(Character.getNumericValue(userEnterNumber.charAt(i)))) {
                 isExcepted = true;
             }
         }
         return isExcepted;
     }
-
     public static List<Integer> getEnterNumber(String userEnterNumber) {
         List<Integer> enterNumber = new ArrayList<>();
-        if(!isEnterNumberExcept(userEnterNumber)) {
-            for(int i = 0; i < 3; i++) {
+        if (!isEnterNumberExcept(userEnterNumber)) {
+            for (int i = 0; i < 3; i++) {
                 enterNumber.add(Character.getNumericValue(userEnterNumber.charAt(i)));
             }
         } else {
@@ -71,41 +68,45 @@ public class Application {
     public static void announceBallAndStrike(List<Integer> randomNumber) {
         if (strikeNumber == 3) {
             finishAnnounce();
-            if (Console.readLine().equals("1")) {
-                game();
-            } else if (Console.readLine().equals("2")) {
-                System.exit(0);
-            } else {
-                throw new IllegalArgumentException("숫자를 1, 2 중에 하나를 입력해주세요!");
-            }
+        if (Console.readLine().equals("1")) {
+            game();
+        } else if (Console.readLine().equals("2")) {
+            System.exit(0);
+        } else {
+            throw new IllegalArgumentException("숫자를 1, 2 중에 하나를 입력해주세요!");
+        }
         } else {
             if (ballNumber == 0 && strikeNumber == 0) {
-                System.out.println("낫싱");
+            System.out.println("낫싱");
             } else if (ballNumber == 0){
                 System.out.println(strikeNumber+"스트라이크");
             } else if (strikeNumber == 0) {
                 System.out.println(ballNumber+"볼 ");
-            } else {
+        } else {
                 System.out.println(ballNumber+"볼 "+strikeNumber+"스트라이크");
-            }
+        }
+    public static void announceBallAndStrike(List<Integer> randomNumber, List<Integer> ballAndStrikeNumber) {
+        if (strikeNumber == 3) {
+            finishAnnounce();
+            caseOfAllStrike(Console.readLine());
+        } else {
+            caseOfAllBallAndStrike(ballAndStrikeNumber);
             repeatGameUntilSuccess(randomNumber);
         }
     }
-
     public static void repeatGameUntilSuccess(List<Integer> randomNumber) {
         System.out.print("숫자를 입력해주세요 : ");
         String userEnterNumber = Console.readLine();
         List<Integer> enterNumber = getEnterNumber(userEnterNumber);
         countBallAndStrike(randomNumber, enterNumber);
     }
-
     public static void game() {
         List<Integer> randomNumber = getRandomNumber();
         System.out.print("숫자를 입력해주세요 : ");
         String userEnterNumber = Console.readLine();
         List<Integer> enterNumber = getEnterNumber(userEnterNumber);
-        countBallAndStrike(randomNumber, enterNumber);
-        announceBallAndStrike(randomNumber);
+        List<Integer> ballAndStrikeNumber = countBallAndStrike(randomNumber, enterNumber);
+        announceBallAndStrike(randomNumber, ballAndStrikeNumber);
     }
 
     public static void main(String[] args) {
