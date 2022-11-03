da14f0f665059712788ca3ca54fd222f3248e7a2
HubCreator <chocomilk8604@gmail.com>
2022-11-03 14:03:57 +0900

    refactor : ValidationUtil의 MIN, MAX를 static 변수로 추출

--- src/main/java/baseball/ValidationUtil.java
+++ src/main/java/baseball/ValidationUtil.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 public class ValidationUtil {
 
+    public static final int MIN = 1;
+    public static final int MAX = 9;
     public static final int VALID_CNT = 3;
 
     public static boolean isValidRange(List<Integer> list) {
-        return list.stream().filter(m -> m >= 1 && m <= 9).count() == list.size();
+        return list.stream().filter(m -> m >= MIN && m <= MAX).count() == list.size();
     }
 
     public static boolean isValidCount(List<Integer> list) {
