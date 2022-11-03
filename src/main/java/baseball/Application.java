54f68fd904b7684d96440b7a0412042ae05151b7
Lee Sang Bin <rhkd2612@naver.com>
2022-11-03 15:38:06 +0900

    docs: baseball game methods

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,15 @@
 package baseball;
 
+/*
+* [X] 랜덤 번호 생성
+* [X] 잘못된 입력 예외 처리(어플리케이션 종료)
+* [X] 입력된 숫자에 대한 스트라이크/볼/낫싱 판별
+* [X] 숫자 반복 입력 기능
+* [X] 게임 종료 시 재시작 기능
+* [X] 기능별 테스트 작성
+* [X] 테스트 확인
+*/
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
