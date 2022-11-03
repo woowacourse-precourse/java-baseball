e24db162bb297e72744bcb6637f4666a1e5cbdda
ksh-beginner <ohk9134@naver.com>
2022-11-03 16:48:32 +0900

    test(OpponentComputerTest) : 상대방 생성과 동시에 초기화되는 맵의 Key는 Strike나 Ball인지 테스트
    OpponentComputer.getStrikeAndBallCountMap()으로 맵을 받아서
    "Strike"와 "Ball"이 Key에 포함되는지 테스트

--- src/test/java/baseball/model/OpponentComputerTest.java
+++ src/test/java/baseball/model/OpponentComputerTest.java
@@ -4,6 +4,8 @@ import baseball.RandomUtil;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -13,8 +15,16 @@ public class OpponentComputerTest {
     void 상대방_생성과_동시에_랜덤_수_저장() {
         OpponentComputer opponentComputer = new OpponentComputer(RandomUtil.createRandomNumber());
         int opponentComputerNumber = opponentComputer.getNumber();
-        
+
         assertThat(opponentComputerNumber).isNotNull();
     }
 
+    @Test
+    void 상대방_생성과_동시에_초기화되는_스트라이크_볼_카운트_맵의_Key는_Strike_Or_Ball() {
+        OpponentComputer opponentComputer = new OpponentComputer(123);
+        Map<String, Integer> strikeAndBallCountMap = opponentComputer.getStrikeAndBallCountMap();
+
+        assertThat(strikeAndBallCountMap.containsKey("Strike")).isTrue();
+        assertThat(strikeAndBallCountMap.containsKey("Ball")).isTrue();
+    }
 }
