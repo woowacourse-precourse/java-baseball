a128a8108959353cafb8569b56773e8eaa7f1ab5
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-04 08:15:48 +0900

    feat: 예외 메세지 class 구현

---
+++ src/main/java/baseball/util/GameExceptionMessage.java
@@ -0,0 +1,6 @@
+package baseball.util;
+
+public class GameExceptionMessage {
+    public static final String USER_NUMBER_LENGTH = "서로 다른 3개의 숫자를 입력해주세요.";
+    public static final String USER_NUMBER_RANGE = "1~9 범위의 숫자를 입력해주세요";
+}
