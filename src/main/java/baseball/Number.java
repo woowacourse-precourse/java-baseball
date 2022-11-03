c8675fd17f6ecfa9034f5fc418c4eca0abf478dc
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-03 23:55:23 +0900

    feat: 사용자 숫자 입력 예외처리 기능 구현

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -9,6 +9,10 @@ public class Number {
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 9;
     private static final int CNT_NUMBER = 3;
+    private static final String ANSWER_LENGTH_ERR_MESSAGE = "숫자의 자리수가 올바르지 않습니다.";
+    private static final String ANSWER_DUPLICATION_ERR_MESSAGE = "중복된 수가 포함되어 있습니다.";
+    private static final String ANSWER_RANGE_ERR_MESSAGE = "범위를 벗어난 수가 포함되어 있습니다.";
+    private static final String ANSWER_DATATYPE_ERR_MESSAGE = "숫자가 아닌 문자가 포함되어 있습니다.";
 
     public Number() {
     }
@@ -25,5 +29,63 @@ public class Number {
         }
 
         return computerNumbers;
+    }
+
+    public boolean isValidAnswer(String answer) {
+        checkLength(answer);
+        checkDuplication(answer);
+        checkDataType(answer);
+        checkRange(answer);
+
+        return true;
+    }
+
+    private void checkLength(String answer) {
+        if (answer.length() != 3) {
+            throw new IllegalArgumentException(ANSWER_LENGTH_ERR_MESSAGE);
+        }
+    }
+
+    private void checkDuplication(String answer) {
+        String uniqueAnswer = "";
+
+        for (int idx = 0; idx < answer.length(); idx++) {
+            if (answer.indexOf(answer.charAt(idx)) == idx) {
+                uniqueAnswer += answer.charAt(idx);
+            }
+        }
+
+        if (uniqueAnswer.length() != 3) {
+            throw new IllegalArgumentException(ANSWER_DUPLICATION_ERR_MESSAGE);
+        }
+    }
+
+    private void checkDataType(String answer) {
+        List<Integer> asciiAnswer = new ArrayList<>();
+        char character;
+        int ascii;
+
+        for (int idx = 0; idx < answer.length(); idx++) {
+            character = answer.charAt(idx);
+            ascii = character;
+            asciiAnswer.add(ascii);
+        }
+
+        boolean answerDataType = asciiAnswer.stream().allMatch(num -> num >= 48 && num <= 57);
+
+        if (!answerDataType) {
+            throw new IllegalArgumentException(ANSWER_DATATYPE_ERR_MESSAGE);
+        }
+    }
+
+    private void checkRange(String answer) {
+        List<String> stringAnswers = List.of(answer.split(""));
+
+        boolean answerRange = stringAnswers.stream().map(Integer::valueOf)
+                .allMatch(num -> num >= MIN_RANGE && num <= MAX_RANGE);
+
+        if (!answerRange) {
+            throw new IllegalArgumentException(ANSWER_RANGE_ERR_MESSAGE);
+        }
     }
 }
--- src/test/java/baseball/NumberTest.java
+++ src/test/java/baseball/NumberTest.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import java.util.List;
 import java.util.stream.Collectors;
 import org.assertj.core.api.Assertions;
@@ -11,6 +13,10 @@ class NumberTest {
     private static final int MIN_RANGE = 1;
     private static final int MAX_RANGE = 9;
     private static final int CNT_NUMBER = 3;
+    private static final String ANSWER_LENGTH_ERR_MESSAGE = "숫자의 자리수가 올바르지 않습니다.";
+    private static final String ANSWER_DUPLICATION_ERR_MESSAGE = "중복된 수가 포함되어 있습니다.";
+    private static final String ANSWER_RANGE_ERR_MESSAGE = "범위를 벗어난 수가 포함되어 있습니다.";
+    private static final String ANSWER_DATATYPE_ERR_MESSAGE = "숫자가 아닌 문자가 포함되어 있습니다.";
 
     Number number;
 
@@ -38,5 +44,48 @@ class NumberTest {
         List<Integer> computerNumbers = number.generateRandomNumber();
         int uniqueComputerNumbers = computerNumbers.stream().distinct().collect(Collectors.toList()).size();
         Assertions.assertThat(uniqueComputerNumbers).isEqualTo(3);
+    }
+
+    @Test
+    void 사용자_입력_유효성_검사() {
+        String answer = "123";
+        boolean available = number.isValidAnswer(answer);
+        Assertions.assertThat(available).isTrue();
+    }
+
+    @Test
+    void 사용자_입력_유효성_검사_자리수_초과_케이스() {
+        String answer = "1234";
+
+        assertThatThrownBy(() -> number.isValidAnswer(answer))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ANSWER_LENGTH_ERR_MESSAGE);
+    }
+
+    @Test
+    void 사용자_입력_유효성_검사_중복_케이스() {
+        String answer = "122";
+
+        assertThatThrownBy(() -> number.isValidAnswer(answer))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ANSWER_DUPLICATION_ERR_MESSAGE);
+    }
+
+    @Test
+    void 사용자_입력_유효성_검사_범위_위반_케이스() {
+        String answer = "120";
+
+        assertThatThrownBy(() -> number.isValidAnswer(answer))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ANSWER_RANGE_ERR_MESSAGE);
+    }
+
+    @Test
+    void 사용자_입력_유효성_검사_자료형_위반_케이스() {
+        String answer = "1S9";
+
+        assertThatThrownBy(() -> number.isValidAnswer(answer))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(ANSWER_DATATYPE_ERR_MESSAGE);
     }
 }
