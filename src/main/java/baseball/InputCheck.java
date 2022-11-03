314cdeb6c0889f693cea391d46b7ae29ab90b4b4
eyeofsol <kingjihong@naver.com>
2022-11-03 21:44:28 +0900

    feat(InputCheck): 모두 다른 숫자인지 판별하는 기능 추가
    중복되는 수가 있는지 검사하는 기능입니다.

--- src/main/java/baseball/InputCheck.java
+++ src/main/java/baseball/InputCheck.java
@@ -11,6 +11,9 @@ public class InputCheck {
         if (hasNonDigit(input)) {
             return true;
         }
+        if (hasSameNumber(input)) {
+            return true;
+        }
         return false;
     }
 
@@ -24,5 +27,26 @@ public class InputCheck {
             }
         }
         return false;
+    }
+
+    public static boolean hasSameNumber(String input) {
+        char c1 = input.charAt(0);
+        char c2 = input.charAt(1);
+        char c3 = input.charAt(2);
+
+        if ((c1 == c2) || (c1 == c3)) {
+            return true;
+        }
+        if (c2 == c3) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean isEndInputError(String input) {
+        if (input.equals("1") || input.equals("2")) {
+            return false;
+        }
+        return true;
     }
 }
