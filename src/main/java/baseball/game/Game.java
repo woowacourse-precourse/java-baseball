4cba471b3ab88ccd1493111f85c2c8d7af3cf5f6
seokjin8678 <rnrn8678@naver.com>
2022-11-03 20:08:11 +0900

    feat: 중복되지 않는 임의의 난수 3자리 생성 기능 구현

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,6 +1,13 @@
 package baseball.game;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+	private List<Integer> computerNumbers;
+
 	private Game() {
 		System.out.println("숫자 야구 게임을 시작합니다.");
 	}
@@ -10,6 +17,21 @@ public class Game {
 	}
 
 	public void start() {
+		computerNumbers = generateComputerNumbers();
+	}
 
+	public List<Integer> getComputerNumbers() {
+		return List.copyOf(computerNumbers);
+	}
+
+	private List<Integer> generateComputerNumbers() {
+		List<Integer> numbers = new ArrayList<>();
+		while (numbers.size() < 3) {
+			int number = Randoms.pickNumberInRange(1, 9);
+			if (!numbers.contains(number)) {
+				numbers.add(number);
+			}
+		}
+		return numbers;
 	}
 }
