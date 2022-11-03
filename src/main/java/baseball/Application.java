bab3ee152e212b8410782cdc9c969495d8275e4a
Jeangyu <gjwlsrb1700@naver.com>
2022-11-03 17:26:38 +0900

    [DELETE] 사용하지 않는 dependency 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,6 @@
 package baseball;
 
 import baseball.gameController.GameController;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
