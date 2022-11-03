84f6848ec858eec276666615e73ea9a5f5bf4329
SEONMl <sm000117@naver.com>
2022-11-03 22:15:36 +0900

    feat: 출력 화면 view 구현

---
+++ src/main/java/baseball/view/Display.java
@@ -0,0 +1,33 @@
+package baseball.view;
+
+import static baseball.entity.NumberBaseball.GAME_ANSWER_MAX_VALUE;
+
+public class Display {
+    public static void printInitMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printInputNumberMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public static void printEndingMessage() {
+        System.out.println(GAME_ANSWER_MAX_VALUE+"개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static void printReGameMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    private static void printBallMessage(int n) {
+        System.out.print(n+"볼");
+    }
+
+    private static void printStrikeMessage(int n) {
+        System.out.print(n+"스트라이크");
+    }
+
+    private static void printNothingMessage() {
+        System.out.println("낫싱");
+    }
+}
