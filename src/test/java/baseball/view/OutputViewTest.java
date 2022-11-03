13dc353c824dc901b4f5d517db91fcdb5d184421
ksh-beginner <ohk9134@naver.com>
2022-11-03 22:49:14 +0900

    test(OutputViewTest) : 입력한 수에 대한 볼, 스트라이크 개수 출력 테스트 - 1볼

--- src/test/java/baseball/view/OutputViewTest.java
+++ src/test/java/baseball/view/OutputViewTest.java
@@ -30,4 +30,14 @@ public class OutputViewTest {
 
         assertThat(output.toString()).isEqualTo(gameStartMessage);
     }
+
+    @Test
+    void 입력한_수에_대한_볼_스트라이크_개수_출력_1볼() {
+        int strikeCount = 0;
+        int ballCount = 1;
+        String gameResultMessage = "1볼\n";
+        OutputView.printGameResultMessage(strikeCount, ballCount);
+
+        assertThat(output.toString()).isEqualTo(gameResultMessage);
+    }
 }
