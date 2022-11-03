decb775a46874a34aafdefd4b1dc5ea8bf6bccd8
youngkyunIm <yee0230@naver.com>
2022-11-03 22:49:35 +0900

    feat : add check input continuous

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -8,11 +8,18 @@ public class Input {
 
     public Input(String input) {
         CheckLength(input);
+        CheckContinuous(input);
         this.input = input;
     }
 
-
     private void CheckLength(String input) {
         if (input.length() != INPUT_LENGTH) throw new IllegalArgumentException();
+    }
+
+    private void CheckContinuous(String input) {
+        for (int i = 0; i < 3; i += 1) {
+            int tmp = input.indexOf(input.charAt(i));
+            if (tmp != i) throw new IllegalArgumentException();
+        }
     }
 }
