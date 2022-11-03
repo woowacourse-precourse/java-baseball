8ff2235bf6cea4089fd644f1042f3743d7d283c7
dmdrk1414 <seungchan141414@gmail.com>
2022-11-03 19:18:26 +0900

    :white_check_mark:[Test] user가 입력한 각각의 숫자가 같은가 확인 테스트 case_1
    입력 : "123", "234", "345", "456", "678", "789"
    값 : true

--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -2,6 +2,8 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.List;
 import model.User;
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +17,17 @@ public class UserTest {
         assertThat(result).isEqualTo(123);
     }
 
+    @Test
+    void 입력_각각의_숫자가_같은가_case_1() {
+        User user = new User();
+        String[] arr = new String[]{"123", "234", "345", "456", "678", "789"};
+        List<Boolean> resultList = new ArrayList<>();
+        for (String str : arr) {
+            resultList.add(user.isValidNumOfIndividually(str));
+        }
 
+        assertThat(resultList).allMatch((boo) -> {
+            return boo.equals(true);
+        });
+    }
 }
