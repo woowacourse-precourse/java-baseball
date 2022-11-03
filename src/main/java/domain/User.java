96e11a368bde8e8bd96f8193321e24f3be5e11c9
Jeonsoyeon <itso22@naver.com>
2022-11-03 19:32:16 +0900

    feat(User): 유저의 숫자 뽑아오기

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -1,4 +1,17 @@
 package domain;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class User {
+    public static List<Integer> getUserNumber(int inputNumber){
+        List<Integer> userNumbers = new ArrayList<>();
+        int[] userNumberArr = Stream.of(String.valueOf(inputNumber).split("")).mapToInt(Integer::parseInt).toArray();
+        for(int i=0; i<userNumberArr.length; i++){
+            userNumbers.add(userNumberArr[i]);
+        }
+        return userNumbers;
+    }
+
 }
