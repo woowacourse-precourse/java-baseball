64d12baf5224427109ddfebf7b14ecf8e05cd613
unknown <angelbaek>
2022-11-03 12:45:09 +0900

    save

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,14 +9,6 @@ import java.util.regex.Pattern;
 
 public class Application {
 	public static void main(String[] args) {
-		// TODO: 프로그램 구현
-		/*
-		 * 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다. 3항 연산자를
-		 * 쓰지 않는다. indent(인덴트, 들여쓰기) depth를 3이 넘지 않도록 구현한다. 2까지만 허용한다. 예를 들어 while문 안에
-		 * if문이 있으면 들여쓰기는 2이다. 힌트: indent(인덴트, 들여쓰기) depth를 줄이는 좋은 방법은 함수(또는 메서드)를 분리하면
-		 * 된다.
-		 */
-		// 서로 다른 3자리수
 		gameStart(getComputerNumbers());
 	}
 
@@ -25,7 +17,7 @@ public class Application {
 		int intUserNumbers = 0;
 		try {
 			System.out.print("숫자를 입력해주세요 : ");
-			String StringUserAnotherNumbers = camp.nextstep.edu.missionutils.Console.readLine(); // 라이브러리
+			String StringUserAnotherNumbers = camp.nextstep.edu.missionutils.Console.readLine();
 			Pattern userNumberPattern = Pattern.compile("^[1-9]*$");
 			Matcher matchUserNumber = userNumberPattern.matcher(StringUserAnotherNumbers);
 			boolean isUserNumberChecked = matchUserNumber.find();
@@ -33,7 +25,7 @@ public class Application {
 				throw new IllegalArgumentException();
 
 			if (StringUserAnotherNumbers.length() != 3) {
-				throw new IllegalArgumentException(); // 3개가 아닐때
+				throw new IllegalArgumentException();
 			}
 			char[] charFromUserNumbers = new char[StringUserAnotherNumbers.length()];
 			for (int userNums = 0; userNums < StringUserAnotherNumbers.length(); userNums++) {
@@ -43,7 +35,7 @@ public class Application {
 				if (charNumbers != StringUserAnotherNumbers.indexOf(StringUserAnotherNumbers.charAt(charNumbers)))
 					throw new IllegalArgumentException();
 			}
-			intUserNumbers = Integer.parseInt(StringUserAnotherNumbers); // 숫자만 입력하게끔
+			intUserNumbers = Integer.parseInt(StringUserAnotherNumbers);
 			for (int number = 0; number < StringUserAnotherNumbers.length(); number++) {
 				char[] userNumberChar = StringUserAnotherNumbers.toCharArray();
 				userNumbersList.add((int) userNumberChar[number] - 48);
@@ -51,7 +43,7 @@ public class Application {
 		} catch (IllegalArgumentException e) {
 			System.err.println("서로 다른 숫자(1~9) 3개만 입력하세요"); // 숫자가 아님
 			System.exit(0);
-		}		
+		}
 		return userNumbersList;
 	}
 
@@ -62,7 +54,7 @@ public class Application {
 			if (!computerNumbers.contains(randomComputerNumber)) {
 				computerNumbers.add(randomComputerNumber);
 			}
-		}		
+		}
 		return computerNumbers;
 	}
 
