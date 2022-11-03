421f6351c1004e8e2f44c02ca93d2ccd259914ae
KSH <tkdgns543@gmail.com>
2022-11-03 22:27:56 +0900

    feat(Baseball): 유저의 입력을 요구하고 받는 메서드 구현
    끝에 줄바꿈 하지 않는 "숫자를 입력해주세요 : " 출력
    입력을 받아 저장 후 줄바꿈

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Baseball {
     String userNumbers;
 
     Baseball() {}
+
+    private void setUserNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        userNumbers = Console.readLine();
+        System.out.println();
+    }
 }
