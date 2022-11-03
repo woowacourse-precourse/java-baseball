ff673354afdc612812ae0ce6bd13cd9d7d17efe9
1eejisoo <jisu991012@gmail.com>
2022-11-03 21:59:56 +0900

    Feat: 컴퓨터의 랜덤 값 생성 기능 추가

--- src/main/java/domain/Computer.java
+++ src/main/java/domain/Computer.java
@@ -1,5 +1,20 @@
 package domain;
 
-public class Computer {
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    public static final List<Integer> computer = new ArrayList<>();
+
+    public static List<Integer> makeRandomNumber() {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
