9168979ff8c7ef4f1e80ec7f396082aa941756c9
HangSub999 <gkdtjq999@gmail.com>
2022-11-03 17:24:43 +0900

    refactor: 상수 추가 및 타입 변경

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -7,10 +7,14 @@ import java.util.List;
 
 public class Computer {
 
+    private static final int COMPUTER_LIST_MAX_THREE = 3;
+    private static final int RANDOM_NUMBER_MINIMUM_ONE = 1;
+    private static final int RANDOM_NUMBER_MAXIMUM_NINE = 9;
+
     public static List<Long> makeRandomNumber() {
         List<Long> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            long randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (computer.size() < COMPUTER_LIST_MAX_THREE) {
+            long randomNumber = Randoms.pickNumberInRange(RANDOM_NUMBER_MINIMUM_ONE, RANDOM_NUMBER_MAXIMUM_NINE);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
