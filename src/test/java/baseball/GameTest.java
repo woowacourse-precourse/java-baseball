6777c0f4f7c18cfda94ee573a516f5833984db69
kanghyki <kanghyki@gmail.com>
2022-11-04 01:52:10 +0900

    Fix: GameTest에서 사용하지 않는 import 삭제

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -11,7 +11,6 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 
 public class GameTest {
