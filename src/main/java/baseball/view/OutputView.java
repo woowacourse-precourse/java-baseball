115f5ac4c5de97ce14e197cab74b964f49d03ce8
kokodak <kokodakadokok@gmail.com>
2022-11-03 12:33:38 +0900

    feat(outputView): 게임 재시작/종료 문구 출력 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,6 +3,7 @@ package baseball.view;
 public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -10,5 +11,9 @@ public class OutputView {
 
     public static void printGameEndMessage() {
         System.out.println(GAME_END_MESSAGE);
+    }
+
+    public static void printGameRestartOrEnd() {
+        System.out.println(GAME_RESTART_OR_END_MESSAGE);
     }
 }
