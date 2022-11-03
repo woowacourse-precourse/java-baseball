36c6aa3c4c3f8163ff6bb4affa71822a6680def7
neppiness <0414kjh@naver.com>
2022-11-04 01:45:07 +0900

    feat: print starting comment (src/main/java/baseball)
    Print-숫자 야구 게임을 시작합니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,10 +13,12 @@ public class Application {
         }
         return answer;
     }
+
     public static void main(String[] args) throws Exception {
         int executionCode = 1;
-        ArrayList<Integer> answer = new ArrayList<>();
+        ArrayList<Integer> answer;
         while (executionCode == 1) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
             answer = generateAnswer();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             executionCode = Integer.parseInt(Console.readLine());
