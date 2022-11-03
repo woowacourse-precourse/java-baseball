b585b0a51e50e82c1db0fcd97fc9a61f4d6d8272
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-03 18:04:15 +0900

    test(BaseballTest): Baseball 테스트 코드 작성
    BaseballTest 목록:
    - 숫자로 이루어진 문자열을 자리수별로 나눠 숫자 리스트화 하는 함수 테스트
    - 1~9 사이의자연수로 이루어진 서로 다른 세 수를 문자열로 리턴하는 함수 테스트

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -1,4 +1,58 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 class BaseballTest {
-  
+	@Nested
+	class SplitDigits {
+		@Test
+		void case1() {
+			String threeDigitNumber = "369";
+			List<Integer> result = List.of(3, 6, 9);
+			assertThat(Baseball.splitDigits(threeDigitNumber)).isEqualTo(result);
+		}
+
+		@Test
+		void case2() {
+			String threeDigitNumber = "4235";
+			List<Integer> result = List.of(4, 2, 3, 5);
+			assertThat(Baseball.splitDigits(threeDigitNumber)).isEqualTo(result);
+		}
+
+		@Test
+		void case3() {
+			String threeDigitNumber = "";
+			List<Integer> result = Collections.emptyList();
+			assertThat(Baseball.splitDigits(threeDigitNumber)).isEqualTo(result);
+		}
+	}
+
+	@Nested
+	class make3DigitNumberTest {
+		private static final int PLAYING_NUMBER_SIZE = 3;
+
+		@Test
+		void case1() {
+			for (int i = 0; i < 100000; i++) {
+				String threeDigitNumber = Baseball.make3DigitNumber();
+				List<Integer> threeNumbers = Baseball.splitDigits(threeDigitNumber);
+				Set<Integer> nonDuplicateNumbers = new HashSet<>(threeNumbers);
+
+				assertThat(threeNumbers.size()).isEqualTo(PLAYING_NUMBER_SIZE);
+				assertThat(nonDuplicateNumbers.size()).isEqualTo(PLAYING_NUMBER_SIZE);
+				for (int number : threeNumbers) {
+					assertThat(number).isBetween(1, 9);
+				}
+			}
+		}
+	}
+
 }
