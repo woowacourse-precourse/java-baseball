18b8b389760a93bcc50d5987d055ebd4e4f7d529
dev_kong <ahdjd5@gmail.com>
2022-11-03 13:32:14 +0900

    feat(GameStart): add printResult method to print game result

--- src/main/java/baseball/view/SystemMessage.java
+++ src/main/java/baseball/view/SystemMessage.java
@@ -1,7 +1,32 @@
 package baseball.view;
 
+import java.util.Map;
+
 public class SystemMessage {
     public static void printGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printGameResult(Map<String,Integer> result) {
+        String resultMessage = "";
+        Integer ball = result.get("ball");
+        Integer strike = result.get("strike");
+        if (ball == 0) {
+            resultMessage = strike + "스트라이크";
+        }
+
+        if (strike == 0) {
+            resultMessage = ball + "볼";
+        }
+
+        if(ball != 0 && strike != 0) {
+            resultMessage = ball + "볼 " + strike + "스트라이크";
+        }
+
+        if (ball == 0 && strike == 0) {
+            resultMessage = "낫싱";
+        }
+
+        System.out.println(resultMessage);
     }
 }
