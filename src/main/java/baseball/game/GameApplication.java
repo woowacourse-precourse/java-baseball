5aba8f6127e7e4c269dbaaefce29fbe2a95c63dd
im-shung <seoyoung1113@gmail.com>
2022-11-04 00:17:47 +0900

    feat: :sparkles: computer number 생성

--- src/main/java/baseball/game/GameApplication.java
+++ src/main/java/baseball/game/GameApplication.java
@@ -1,5 +1,6 @@
 package baseball.game;
 
+import baseball.game.utils.GameUtil;
 import baseball.input.utils.NumberUtil;
 import baseball.input.validation.UserNumberValidation;
 import baseball.input.view.InputView;
@@ -8,10 +9,11 @@ import java.util.List;
 public class GameApplication {
 
     public static void startGame() {
-        playGame();
+        List<Integer> computerNumber = GameUtil.getComputerNumber();
+        playGame(computerNumber);
     }
 
-    public static void playGame() {
+    public static void playGame(List<Integer> computerNumber) {
         String userNumber = InputView.inputNumber();
         if (!UserNumberValidation.isValidNumberLength(userNumber)) {
             throw new IllegalArgumentException();
