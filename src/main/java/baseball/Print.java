12ce939f43b89692c6ca4069f7215c5ff276d92c
dooboocookie <bkh3030@gmail.com>
2022-11-03 14:19:57 +0900

    fix: 라운드 시작 메세지 출력 메소드 접근지정자 수정

--- src/main/java/baseball/Print.java
+++ src/main/java/baseball/Print.java
@@ -4,7 +4,7 @@ public class Print {
 
     private static final String ROUND_START_MESSAGE = "숫자를 입력해주세요 : ";
 
-    private static void printRoundStart() {
+    public static void printRoundStart() {
         System.out.print(ROUND_START_MESSAGE);
     }
 
