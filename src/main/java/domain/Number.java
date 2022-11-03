90da0b0d0b12711da9139d0b17ad1b8e4b06e0eb
1eejisoo <jisu991012@gmail.com>
2022-11-03 21:57:35 +0900

    Feat: 입력 값 유효성 검증 기능 추가 - 각 자리 수는 0이 아닌 자연수여야 한다.

--- src/main/java/domain/Number.java
+++ src/main/java/domain/Number.java
@@ -8,11 +8,20 @@ public class Number {
     public Number(List<Integer> number) {
         this.number = number;
         validateThreeDigitNumber();
+        validateNaturalNumber();
     }
 
     public void validateThreeDigitNumber() {
         if (number.size() != 3) {
             throw new IllegalArgumentException("숫자는 세 자리 자연수여야 합니다.");
+        }
+    }
+
+    public void validateNaturalNumber() {
+        for (Integer digit : number) {
+            if (digit <= 0) {
+                throw new IllegalArgumentException("각 자리 수는 0이 아닌 자연수여야 합니다.");
+            }
         }
     }
 }
