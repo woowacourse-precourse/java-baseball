7a3cdbc4c895164901c928f64b628619691fb014
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-03 14:40:23 +0900

    feat : 문구와 함께 사용자가 값을 입력하는 기능 추가 '숫자를 입력해주세요 :' 문구와 함께 사용자가 값을 입력한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,20 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        List<Integer> computer = makeRandomNumber();
+        List<Integer> computerNumber = makeRandomNumber();
+
+        String userNumber = inputUserNumber();
     }
 
     public static List<Integer> makeRandomNumber(){
@@ -24,4 +28,12 @@ public class Application {
 
         return computer;
     }
+
+    public static String inputUserNumber(){
+        System.out.println("숫자를 입력해주세요 :");
+        String userNumber = Console.readLine();
+
+        return userNumber;
+    }
+
 }
