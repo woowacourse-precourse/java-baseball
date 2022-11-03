b6bd75e5e7cbc9bee813ee0954f09a2b6dfb36ff
MIN-04 <kimji_0424@naver.com>
2022-11-03 17:47:05 +0900

    feat: change class Application
    add message
    기능 목록 - 2. 게임 시작 문구 출력 구현 ("숫자 야구 게임을 시작합니다.")

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+
         List<Integer> computer = createComputerNumber();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     private static List<Integer> createComputerNumber() {
