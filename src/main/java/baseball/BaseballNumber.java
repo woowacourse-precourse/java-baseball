11407c6d3fec51ea0895c7aed73b00b1040609fe
i960107 <i960107@naver.com>
2022-11-03 15:47:27 +0900

    refactor(BaseballNumber): delete duplicate code

--- src/main/java/baseball/BaseballNumber.java
+++ src/main/java/baseball/BaseballNumber.java
@@ -66,21 +66,7 @@ public class BaseballNumber {
         }
         return computerNumbers;
     }
-    public static BaseballNumber getComputerPickedNumbers() {
-        boolean hasException = true;
 
-        BaseballNumber computerNumbers = null;
-        while (hasException) {
-            try {
-                computerNumbers = new BaseballNumber
-                        (Randoms.pickUniqueNumbersInRange(MIN_NUMBER, MAX_NUMBER, NUMBER_COUNT));
-                hasException = false;
-            } catch (IllegalArgumentException e) {
-
-            }
-        }
-        return computerNumbers;
-    }
 
     public boolean contains(int number) {
         return numbersSet.contains(number);
