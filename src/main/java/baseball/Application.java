8825549f9685af86764d692e6158e7481dfc3f67
LeeJaeHong <0915jhlee@daum.net>
2022-11-03 14:52:21 +0900

    1. 컴퓨터 랜덤 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,30 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-public class Application {
-    public static void main(String[] args) {
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Application {
+    static List<Integer> computerRandomNumber = new ArrayList<>();
+    public static void main(String[] args) {
         while(true){
+            if (computerRandomNumber.size()==0){
+                computerRandomNumber=makeRandomNumber();
+            }
 
         }
+    }
+    
+    static List<Integer> makeRandomNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
