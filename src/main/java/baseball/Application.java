1d338d950a5e3fbf761914dba6807ff6ebf4738b
seonghun120614 <seonghun120614@github.com>
2022-11-03 18:32:25 +0900

    [Add] Application draft designed

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,45 @@
 package baseball;
 
+import java.io.IOException;
+import java.util.LinkedHashSet;
+import baseball.calculate.BaseballCalculator;
+
+import static baseball.inputoutput.Inputing.*;
+import static baseball.inputoutput.Outputing.*;
+
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    private static BaseballCalculator bc = new BaseballCalculator();
+
+    public static void main(String[] args) throws IOException {
+
+        BaseballGame bg = new BaseballGame();
+
+        LinkedHashSet<Character> inputNum;
+        printGameStart();
+
+        while (bg.getStatus()) {
+            inputNum = input3LenNumber();
+
+            bg.setStrike(bc.strikeJudge(bg.getCorrectNum(), inputNum));
+            bg.setBall(bc.ballJudge(bg.getCorrectNum(), inputNum));
+
+            writeCase(bg.getStrike(), bg.getBall());
+            printLine();
+
+            if (bg.getStrike() == 3) {
+                printGameEnd();
+
+                if ("2".equals(inputEnd())){
+                    printEnd();
+                    bg.endGame();
+                } else {
+                    bg.makeRandomNum();
+                }
+            }
+        }
+
+        exitOutput();
     }
 }
