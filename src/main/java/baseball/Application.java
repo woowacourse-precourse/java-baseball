5cf27f17ebba9da26362c297218aae8360d0703a
ghchoi <ghchoi0427@gmail.com>
2022-11-03 21:41:09 +0900

    refactor: 불필요한 예외 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,10 +5,8 @@ import baseball.util.GameLogic;
 import baseball.validation.InputValidation;
 import baseball.view.GameView;
 
-import java.io.IOException;
-
 public class Application {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         GameController gameController = new GameController(new GameView(new InputValidation()), new GameLogic());
         gameController.startGame();
     }
