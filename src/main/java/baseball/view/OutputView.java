6322bf65e7a39e235ce07337f1fabc4ddd032cfe
TaeHwan <kimta2hwan@gmail.com>
2022-11-03 19:55:32 +0900

    feat(outputview): 정답일 때 정답 문구와 게임 종료 문구를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String ANSWER_INPUT_GUIDE = "숫자를 입력해주세요 : ";
+    private static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -11,5 +12,9 @@ public class OutputView {
 
     public static void printAnswerInputGuide() {
         System.out.println(ANSWER_INPUT_GUIDE);
+    }
+
+    public static void printGameEndMessage() {
+        System.out.println(GAME_END);
     }
 }
