3a655f00b2be24ddd1e1fb20974983106795dcb8
ymkim97 <rladudaud97@naver.com>
2022-11-04 00:30:00 +0900

    fix: modify random number generate test code

--- src/test/java/controller/BaseballGameTest.java
+++ src/test/java/controller/BaseballGameTest.java
@@ -1,5 +1,7 @@
 package controller;
 
+import model.Computer;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
@@ -9,8 +11,11 @@ public class BaseballGameTest {
     @Test
     @DisplayName("컴퓨터의 랜덤 생성 결과")
     void startGame() {
+        while (Computer.getLengthOfNumber() < 3) {
+            Computer.generateRandomNumber();
+        }
         Integer result = 3;
-        assertThat(result).isEqualTo(3);
+        assertThat(Computer.getLengthOfNumber()).isEqualTo(3);
     }
 
 }
