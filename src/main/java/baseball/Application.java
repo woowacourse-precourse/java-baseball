bcaa749308af71134597e84768a99d875e0ab40b
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-03 11:12:17 +0900

    feat: 서로 다른 1 ~ 9 사이의 수를 가지는 3자리 랜덤 값 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        boolean gameLife = true;
+        String number;
+
+        while (gameLife) {
+            number = setNumber();
+        }
+    }
+
+    private static String setNumber() {
+
+        int first = Randoms.pickNumberInRange(1, 9);
+        int second = Randoms.pickNumberInRange(1, 9);
+        int third = Randoms.pickNumberInRange(1, 9);
+
+        while (second == first) {
+            second = Randoms.pickNumberInRange(1, 9);
+        }
+
+        while (third == first || third == second) {
+            third = Randoms.pickNumberInRange(1, 9);
+        }
+
+        Integer number = (first * 100) + (second * 10) + third;
+        return number.toString();
     }
 }
