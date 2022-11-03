45d992f10d3618211ab97cb539b94e68f248df8c
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2022-11-04 05:26:13 +0900

    feat : 사용자 입력 구현
    Console.readLine()으로 입력을 받고
    setUserNumber 메서드에서 String 타입으로 받은 입력을
    Integer타입으로 변환 시킨 후 userNumbers리스트에 맨 앞자리부터 넣음(ex. 입력 123 -> 리스트에 1,2,3순서로 넣음)

--- src/main/java/baseball/GameServer.java
+++ src/main/java/baseball/GameServer.java
@@ -1,18 +1,36 @@
 package baseball;
+
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameServer {
-    private List<Integer> computer = new ArrayList<>();
+    private List<Integer> computerNumbers = new ArrayList<>();
+    private List<Integer> userNumbers = new ArrayList<>();
 
-    public void createRandomNumber(){
-        while(computer.size()<3){
-            int randomNumber= Randoms.pickNumberInRange(1,9);
-            if(!computer.contains(randomNumber)){
-                computer.add(randomNumber);
+    public void createRandomNumber() {
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
             }
+        }
+    }
+
+    public String getUserNumber() {
+        String userNumber = Console.readLine();
+        return userNumber;
+    }
+
+    public void setUserNumber(String userNum) {
+        int userNumber = Integer.parseInt(userNum);
+        int divideNum = 100;
+        for (int i = 0; i < 3; i++) {
+            userNumbers.add(userNumber / divideNum);
+            userNumber /= divideNum;
+            divideNum /= 10;
         }
     }
 }
