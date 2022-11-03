750485f6d396018a5619b92621712221cc60dbdd
kyo705 <mission0705@naver.com>
2022-11-04 00:55:59 +0900

    [ADD] 랜덤값 생성 기능 추가
    camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()을 활용해 서로 다른
    3자리 랜덤 값 생성하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,32 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+    
+    private static List<Integer> createRamdomNumbers() {
+    	Set<Integer> randomNumberSet = new HashSet<>(3);
+    	
+    	while(true) {
+    		int randomNumber = Randoms.pickNumberInRange(0, 9);
+    		randomNumberSet.add(randomNumber);
+    		
+    		if(randomNumberSet.size()==3) {
+    			break;
+    		}
+    	}
+    	
+    	List<Integer> randomNumberList = new ArrayList<>(3);
+    	randomNumberList.addAll(randomNumberSet);
+    	
+    	return randomNumberList;
     }
 }
