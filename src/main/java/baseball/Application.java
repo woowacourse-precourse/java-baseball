4bffd9a0de1738eb8872da01afa733bcda3405d6
leesuengyeol <48343629+syhojeo@users.noreply.github.com>
2022-11-03 15:04:05 +0900

    feat: 숫자 입력 메시지 출력 및 숫자를 입력 받는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
@@ -18,6 +19,16 @@ public class Application {
     private static boolean playGame(String number) {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        String inputNumber = inputNumberFromPlayer();
+    }
+
+    public static String inputNumberFromPlayer() {
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputNumber = Console.readLine();
+
+        return inputNumber;
     }
 
     public static String setNumber() {
