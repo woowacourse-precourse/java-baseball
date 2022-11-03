b2e1d291f65ab1c36ca055b8e486199216c11e41
code-tree <dltjdqls1200@naver.com>
2022-11-03 18:10:32 +0900

    test: 사용자 입력 숫자 범위 테스트

--- src/test/java/baseball/model/GameNumberTest.java
+++ src/test/java/baseball/model/GameNumberTest.java
@@ -23,10 +23,27 @@ class GameNumberTest {
         @DisplayName("게임 숫자가 3개가 아니면 예외가 발생한다.")
         @ParameterizedTest(name = "{index}. 예외 발생 입력값 = {0}")
         @ValueSource(strings = {"1", "12", "1234"})
-        void InvalidGameNumberSize(String numbers) {
+        void invalidGameNumberSize(String numbers) {
             assertThrows(IllegalArgumentException.class, () ->
                     new GameNumber(numbers));
         }
     }
 
+    @Nested
+    @DisplayName("게임 숫자 범위 테스트")
+    @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
+    class GameNumberRangeTest {
+
+        @DisplayName("1~9 범위 내의 숫자 입력시 정상적으로 생성된다.")
+        @Test
+        void validGameNumberRange() {
+            assertDoesNotThrow(() -> new GameNumber("123"));
+        }
+
+        @DisplayName("1~9 범위를 벗어난 숫자 입력시 예외가 발생한다.")
+        @Test
+        void invalidGameNumberRange() {
+            assertThrows(IllegalArgumentException.class, () -> new GameNumber("012"));
+        }
+    }
 }
