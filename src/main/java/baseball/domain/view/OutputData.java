6cf3f721efa88aa2780e330b9f9bf0d8ea21b8fa
sangwon <swchoi1997@naver.com>
2022-11-04 08:57:08 +0900

    feat: 결과를 출력하는 OutputData 생성
    DTO로부터 결과를 받아 출력해주는 클래스

---
+++ src/main/java/baseball/domain/view/OutputData.java
@@ -0,0 +1,55 @@
+package baseball.domain.view;
+
+import baseball.domain.dto.ResultDto;
+import java.util.Map;
+
+public class OutputData {
+
+    public static final String THREE_STRIKE_WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    private OutputData() {
+    }
+
+    public static void gameResultPrint(ResultDto resultDto) {
+        Map<String, Integer> result = resultDto.getResult();
+        for (String ballCount : result.keySet()) {
+            checkBallCount(ballCount, result.get(ballCount));
+        }
+    }
+
+    private static void checkBallCount(String ballCount, int count) {
+        if (ballCount.equals("아웃")) {
+            printOutCount(count);
+            return;
+        }
+        if (ballCount.equals("볼")) {
+            printBallCount(ballCount, count);
+        }
+        if (ballCount.equals("스트라이크")) {
+            printStrikeCount(ballCount, count);
+        }
+    }
+
+    private static void printOutCount(int count) {
+        if (count == 3) {
+            System.out.print("낫싱");
+        }
+    }
+
+    private static void printBallCount(String ballCount, int count) {
+        if (count != 0) {
+            System.out.print(count + ballCount + " ");
+        }
+    }
+
+    private static void printStrikeCount(String ballCount, int count) {
+        if (count == 3) {
+            System.out.println(count + ballCount);
+            System.out.println(THREE_STRIKE_WIN_MESSAGE);
+            return;
+        }
+        if (count != 0) {
+            System.out.print(count + ballCount);
+        }
+    }
+}
