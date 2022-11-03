71750fb659174174c7ad9e7c3e49d00f607c87a8
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2022-11-04 06:07:57 +0900

    feat : 게임 끝났을 때 사용자 입력 및 예외처리
    1,2가 아닌 수를 입력한다면 프로그램 종료

--- src/main/java/baseball/GameServer.java
+++ src/main/java/baseball/GameServer.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class GameServer {
     private List<Integer> computerNumbers = new ArrayList<>();
     private List<Integer> userNumbers = new ArrayList<>();
+    private boolean restart = false;
 
     public void createRandomNumber() {
         while (computerNumbers.size() < 3) {
@@ -39,18 +40,33 @@ public class GameServer {
         if (userNumber.length() != 3) {
             throw new IllegalArgumentException();//사용할 때 try-catch문으로 throw처리
         }
-        if(userNumber.matches("^[1-9]*$")){
+        if (userNumber.matches("^[1-9]*$")) {
             throw new IllegalArgumentException();
         }
-        for(int i=0;i<2;i++){
-            String str = userNumber.substring(i+1,2);
-            if(str.contains(userNumber.substring(i,i+1))){
+        for (int i = 0; i < 2; i++) {
+            String str = userNumber.substring(i + 1, 2);
+            if (str.contains(userNumber.substring(i, i + 1))) {
                 throw new IllegalArgumentException();
             }
+        }
+    }
+
+    public void finishGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userNumber = Console.readLine();
+        if (userNumber.equals("1")) {
+            restart = true;
+        }
+        if (userNumber.equals("2")) {
+            restart = false;
+        }
+        if (!(userNumber.equals("1") || userNumber.equals("2"))) {
+            restart = false;
         }
     }
 
     public void printGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
 }
