46b551a8851904eb4cc2e2086241bdf060305d3e
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-11-03 13:58:51 +0900

    feature : initialize 3 digits of number of computer
    Before playing number baseball game,
    A computer needs three digits of number
    which was made randomly
    This feature generates randomly generated number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         gameStart();
 
     }
 
-    static public void gameStart(){
+    static void gameStart(){
+        int[] computer = initializeComputerNumber();
+    }
 
+    static int[] initializeComputerNumber(){
+        List<Integer> computerNumberList = new ArrayList<>();
+        while(computerNumberList.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!computerNumberList.contains(randomNumber)){
+                computerNumberList.add(randomNumber);
+            }
+        }
+        int[] computerNumberArray = computerNumberList.stream().mapToInt(i -> i).toArray();
+        return computerNumberArray;
     }
 }
