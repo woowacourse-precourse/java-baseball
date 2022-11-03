766719c0d2c3d8f0bcbf2df0780d6d9972dda3e1
JUHA <khabh@naver.com>
2022-11-03 22:16:38 +0900

    feat(Input): 사용자 입력의 길이가 3인지 확인
    Input 클래스 내 여러 메서드에서 playerGuess 문자열이 사용될 것 같아 이를 인스턴스 변수로 변환하고 예외  메서드
    추가

--- src/main/java/baseball/domain/Input.java
+++ src/main/java/baseball/domain/Input.java
@@ -4,9 +4,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
     private final static String  REQUEST_PLAYER_GUESS = "숫자를 입력해주세요 : ";
+    private final static int COMMON_GUESS_LENGTH = 3;
+    private String playerGuess;
 
-    public static void getPlayerGuess() {
+    public void getPlayerGuess() {
         System.out.print(REQUEST_PLAYER_GUESS);
-        String playerGuess = Console.readLine();
+        playerGuess = Console.readLine();
+    }
+
+    private void validateGuess() {
+        if (playerGuess.length() != COMMON_GUESS_LENGTH) {
+            throw new IllegalArgumentException("올바른 형식으로 입력해주세요.");
+        }
     }
 }
