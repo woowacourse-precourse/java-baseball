1bb577ab54b6447a8756cde8fa80be195677a462
Soomin Lim <augustth511@gmail.com>
2022-11-03 19:34:24 +0900

    feat: 랜덤 숫자 생성 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,8 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
 
     public void startGame() {
 
+    }
+
+    private static List<Integer> getRandomNumberList() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
     }
 }
