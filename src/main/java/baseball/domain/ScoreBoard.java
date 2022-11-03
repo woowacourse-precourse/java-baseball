3ece362083f3c5a13b61f9a1180681d5a666a012
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-03 21:18:44 +0900

    feat: 시스템 숫자들을 저장할 ScoreBoard class 구현

---
+++ src/main/java/baseball/domain/ScoreBoard.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class ScoreBoard {
+    List<NumberBall> systemNumberBalls;
+
+    public ScoreBoard(List<NumberBall> systemNumberBalls) {
+        this.systemNumberBalls = systemNumberBalls;
+    }
+
+    public void getScore() {
+
+    }
+}
