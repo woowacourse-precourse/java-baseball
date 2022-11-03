26fd253c5de5a80620448e73bec90e5c43dbff8a
kevstevie <jianna442@gmail.com>
2022-11-03 14:25:16 +0900

    feat: validateUserInput 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -4,14 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
+
 
 public class User {
-
-
+    final static int NUMBER_DIGIT = 3;
     public List userInput() throws IllegalArgumentException{
+        System.out.print(Message.INPUT_NUMBER);
         String str = Console.readLine();
-        if (userInputValidate) {
-            throw new IllegalArgumentException() ;
+        if (validateUserInput(str)) {
+            throw new IllegalArgumentException();
         }
         return StringToNumberList(str);
     }
@@ -21,5 +23,20 @@ public class User {
             result.add(str.charAt(i) - '0');
         }
         return result;
+    }
+
+    private boolean validateUserInput(String str) {
+        StringBuilder regex = new StringBuilder();
+        regex.append("[1-9]{").append(NUMBER_DIGIT).append("}");
+
+        if (!Pattern.matches(regex.toString(), str)) {
+            return true;
+        }
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (str.contains(str.substring(i, i + 1))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
