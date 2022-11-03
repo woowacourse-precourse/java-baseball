b675696f4f96ca2095dc41e8e1362292e5001eac
neppiness <0414kjh@naver.com>
2022-11-04 01:12:02 +0900

    feat: restart or end game (src/main/java/baseball)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,7 @@
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-// package baseball;
+//package baseball;
 
 public class Application {
     public static ArrayList<Integer> generateAnswer() {
@@ -14,6 +14,12 @@ public class Application {
         return answer;
     }
     public static void main(String[] args) throws Exception {
-        ArrayList<Integer> answer = generateAnswer();
+        int executionCode = 1;
+        ArrayList<Integer> answer = new ArrayList<>();
+        while (executionCode == 1) {
+            answer = generateAnswer();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            executionCode = Integer.parseInt(Console.readLine());
+        }
     }
 }
