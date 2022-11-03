2f5ea5b526f61209abc1994bca6888e30d0c15dd
JUHA <khabh@naver.com>
2022-11-04 06:53:43 +0900

    feat(Input): 사용자의 입력이 1부터 9 사이 숫자가 맞는지 확인

--- src/main/java/baseball/domain/Input.java
+++ src/main/java/baseball/domain/Input.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
     private final static String  REQUEST_PLAYER_GUESS = "숫자를 입력해주세요 : ";
     private final static int COMMON_GUESS_LENGTH = 3;
+    private final static char MIN_GUESS_NUMBER = '1';
+    private final static char MAX_GUESS_NUMBER = '9';
     private String playerGuess;
 
     public void getPlayerGuess() {
@@ -16,5 +18,12 @@ public class Input {
         if (playerGuess.length() != COMMON_GUESS_LENGTH) {
             throw new IllegalArgumentException("올바른 형식으로 입력해주세요.");
         }
+
+        for (char guessNumber : playerGuess.toCharArray()) {
+            if (guessNumber < MIN_GUESS_NUMBER || guessNumber > MAX_GUESS_NUMBER) {
+                throw new IllegalArgumentException("올바른 형식으로 입력해주세요");
+            }
+        }
+
     }
 }
