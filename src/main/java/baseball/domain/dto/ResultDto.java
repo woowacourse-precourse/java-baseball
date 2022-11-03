9a340406b8a9dda81b81aa5ab7826c91ca66f060
sangwon <swchoi1997@naver.com>
2022-11-03 22:33:31 +0900

    feat: 테스트를 위한 생성자와 getter 추가

--- src/main/java/baseball/domain/dto/ResultDto.java
+++ src/main/java/baseball/domain/dto/ResultDto.java
@@ -6,11 +6,19 @@ import java.util.Map;
 public class ResultDto {
     private final Map<String, Integer> result;
 
+    public ResultDto(Map<String, Integer> result) {
+        this.result = result;
+    }
+
     public ResultDto(Score score) {
         this.result = getResult(score);
     }
 
     private Map<String, Integer> getResult(Score score) {
         return score.getResult();
+    }
+
+    public Map<String, Integer> getResult() {
+        return result;
     }
 }
