bf4d99f37ed13876f96185f7237531fd2c0b57cd
EUNJEONGMUN <munej26@gmail.com>
2022-11-03 21:09:29 +0900

    feat: 숫자 입력 멘트 출력 - PrintComment 클래스에서 출력

--- src/main/java/baseball/PrintComment.java
+++ src/main/java/baseball/PrintComment.java
@@ -2,8 +2,13 @@ package baseball;
 
 public class PrintComment {
     private static final String START = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_COMMENT = "숫자를 입력해주세요 : ";
 
     public static void printStartComment() {
         System.out.println(START);
+    }
+
+    public static void printInputComment() {
+        System.out.println(INPUT_COMMENT);
     }
 }
