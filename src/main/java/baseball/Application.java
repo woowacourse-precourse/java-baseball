369ccd6e0e40950023e94b7887b4a89f70332153
KSH <tkdgns543@gmail.com>
2022-11-04 02:04:22 +0900

    feat(Application): 숫자의 길이를 정하는 상수 추가
    기능 요구 사항을 넘어 유연한 변경 가능하도록 함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
+        final int NUMBER_LENGTH = 3;
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
