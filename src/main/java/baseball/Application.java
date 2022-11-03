bcaa9d4c905fd3b332dc874cc99226a252b35054
K-Diger <kitten.diger@gmail.com>
2022-11-03 20:27:28 +0900

    feat : 클라이언트 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,35 @@
 package baseball;
 
+import baseball.starter.BaseBallGameStarter;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Application {
+
+
+    static BaseBallGameStarter baseBallGameStarter = new BaseBallGameStarter();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        List<List<String>> operatedGame = baseBallGameStarter.operate();
+        List<String> userInputValue = operatedGame.get(0);
+        List<String> computerGeneratedValue = operatedGame.get(1);
+        int option = 0;
+
+        while (true) {
+            if (baseBallGameStarter.judgementOperate(userInputValue, computerGeneratedValue)) {
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+                option = Integer.parseInt(Console.readLine());
+            }
+            if (option == 1) {
+                operatedGame = baseBallGameStarter.operate();
+                userInputValue = operatedGame.get(0);
+                computerGeneratedValue = operatedGame.get(1);
+            } else if (option == 2) {
+                break;
+            }
+        }
     }
 }
