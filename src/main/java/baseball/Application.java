2bfb9fbb08e3b113dab0ba72e207d165820aaf3b
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-03 12:17:25 +0900

    유저의 답안을 기반으로 정답을 비교 및 다시 호출하는 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,35 +5,38 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.sql.Array;
 import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Application {
+    static int strike = 0, ball = 0;
 
     public static void main(String[] args) {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         List<Integer> answer = new ArrayList<>(3);
         List<Integer> user_answer = new ArrayList<>(3);
-        getRandomNum(answer);
-        getUserAnswer(user_answer);
+        getRandomNum(answer, user_answer);
+        System.out.println(answer);
+//        getUserAnswer(user_answer);
+//        compare_Answer(answer, user_answer);
 
 
     }
 
 
-    public static void getRandomNum(List<Integer> answer){
+    public static void getRandomNum(List<Integer> answer, List<Integer> userAnswer){
         while (answer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!answer.contains(randomNumber)) {
                 answer.add(randomNumber);
             }
         }
+        System.out.println(answer);
+        getUserAnswer(answer, userAnswer);
     }
 
-    public static void getUserAnswer(List<Integer> userAnswer){
+    public static void getUserAnswer(List<Integer>answer, List<Integer> userAnswer){
+        userAnswer.clear();
         System.out.print("숫자를 입력해주세요 : ");
         try {
             int user_answer = Integer.parseInt(Console.readLine());
@@ -45,16 +48,49 @@ public class Application {
                 user_answer = user_answer/10;
             }
             Collections.reverse(userAnswer);
+            compare_Answer(answer, userAnswer);
         }
         catch (IllegalArgumentException e){
+            System.out.println("에러");
             System.exit(0);
         }
-
-
-
     }
 
+    public static void compare_Answer(List<Integer> answer, List<Integer>userAnswer){
+        for(int i=0; i<3; i++){
+            for(int j=0; j<3; j++){
+                compare_eachAnswer(i,j,answer,userAnswer);
+            }
+        }
+        if(strike == 3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.exit(0);
+        } else if(strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        }
+        else if (ball == 0){
+            System.out.println(strike+"스트라이크");
+        }
+        else if (strike == 0){
+            System.out.println(ball+"볼");
+        }
+        else {
+            System.out.println(ball+"볼 "+ strike+"스트라이크 ");
+        }
+        strike =0; ball =0;
+        getUserAnswer(answer, userAnswer);
+    }
 
+    public static void compare_eachAnswer(int i, int j, List<Integer> answer, List<Integer>userAnswer){
+        //System.out.println("compare i,j = " + i +" + "+ j +"정답 = "+answer.get(i)+ ", 유저정답 = "+ userAnswer.get(j));
+        if(i == j) {
+            if (Objects.equals(answer.get(i), userAnswer.get(j)))
+                strike++;
+        } else if (Objects.equals(answer.get(i), userAnswer.get(j)))
+            ball++;
+        //System.out.println(" 스트라이크 = "+  strike  +", 볼 ="+  ball);
+
+    }
 
 }
 
