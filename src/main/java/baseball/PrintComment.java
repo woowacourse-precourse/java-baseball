b45a2e51bc78ace4c5751c9bc4d01be1d58265ad
EUNJEONGMUN <munej26@gmail.com>
2022-11-03 20:53:18 +0900

    feat: 시작 문구 출력 - PrintComment 클래스에서 출력

--- src/main/java/baseball/PrintComment.java
+++ src/main/java/baseball/PrintComment.java
@@ -1,5 +1,9 @@
 package baseball;
 
 public class PrintComment {
+    private static final String START = "숫자 야구 게임을 시작합니다.";
 
+    public static void printStartComment() {
+        System.out.println(START);
+    }
 }
