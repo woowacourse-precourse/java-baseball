b4e74894b25ae7c2764316457c79ef0883f89601
Jeangyu <gjwlsrb1700@naver.com>
2022-11-03 16:54:51 +0900

    [ADD] 게임을 진행하기 위한 입력 함수와 게임을 종료 여부를 묻는 입력 함수를 구현

--- src/main/java/baseball/utils/UserInput.java
+++ src/main/java/baseball/utils/UserInput.java
@@ -1,2 +1,24 @@
-package baseball.utils;public class UserInput {
+package baseball.utils;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class UserInput {
+
+    public static String input(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String userInput = readLine();
+
+        NumberChecker.checkUserInput(userInput);
+
+        return userInput;
+    }
+
+    public static String endInput(){
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userInput = readLine();
+
+        NumberChecker.isOneOrTwo(userInput);
+
+        return userInput;
+    }
 }
