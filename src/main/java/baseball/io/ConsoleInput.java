b3aba05f6835fc656c09a0d8e01f32c5a2e3e195
UJ15 <dbwogml15@naver.com>
2022-11-03 20:16:38 +0900

    refactor : change user input use to Console util class

--- src/main/java/baseball/io/ConsoleInput.java
+++ src/main/java/baseball/io/ConsoleInput.java
@@ -1,16 +1,15 @@
 package baseball.io;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Console;
+
 
 public class ConsoleInput implements Input {
     private static final String INPUT_NUMBER_PROMPT = "숫자를 입력해 주세요 : ";
-
-    private final Scanner scanner = new Scanner(System.in);
 
     @Override
     public String inputNumber() {
         System.out.print(INPUT_NUMBER_PROMPT);
 
-        return scanner.nextLine();
+        return Console.readLine();
     }
 }
