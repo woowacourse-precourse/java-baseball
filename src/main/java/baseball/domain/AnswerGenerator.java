f97b0e670f145c49beb72f7716f55b6542dd55db
akskflwn <akskflwn@gmail.com>
2022-11-04 02:08:27 +0900

    Fix : ComputerBallGenerater 클래스 AnswerGenerator로 수정

--- src/main/java/baseball/domain/AnswerGenerator.java
+++ src/main/java/baseball/domain/AnswerGenerator.java
@@ -4,9 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ComputerBallGenerator {
+public class AnswerGenerator {
 
-    public static List<Integer> createComputerBalls() {
+    public static List<Integer> createAnswer() {
         List<Integer> computerBalls = new ArrayList<>();
         while (computerBalls.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
