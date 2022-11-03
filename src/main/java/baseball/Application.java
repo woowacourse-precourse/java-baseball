c620e820e28e35fef402c45e3e857de26d33000c
seoyoung <sy9723@korea.ac.kr>
2022-11-03 19:40:08 +0900

    feat: create number of computer

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        
+        int SIZE = 3;
+        List<Integer> computer = CreateNumbers(SIZE);
+
+    }
+
+    private static List<Integer> CreateNumbers(int SizeOfNum) {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+               computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
