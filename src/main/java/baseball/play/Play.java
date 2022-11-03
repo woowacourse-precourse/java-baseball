86dd358bf120e28c4a9da3a1f27686089673078d
sangeun-b <baek0007@algonquinlive.com>
2022-11-04 04:27:21 +0900

    feat: 게임 시작 메시지 출력

--- src/main/java/baseball/play/Play.java
+++ src/main/java/baseball/play/Play.java
@@ -1,11 +1,17 @@
 package baseball.play;
 
 public class Play {
-    public static final String GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String STRIKE_MESSAGE = "스트라이크";
     public static final String BALL_MESSAGE = "볼";
     public static final String NOTHING_MESSAGE = "낫싱";
     public static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String OPTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void playBaseball(){
+        System.out.println(START_MESSAGE);
+
+
+    }
 }
