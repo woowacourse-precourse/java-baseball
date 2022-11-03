2ea297e01fbd5c380cb783b0023f1b43c7b3ed7f
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-11-03 16:13:28 +0900

    1. 숫자 야구 게임 시작 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // TODO : 1. 숫자 야구 게임 시작 문구 출력
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // TODO : 2. 컴퓨터가 가지고 있는 정답 문자열을 랜덤으로 지정
+        // TODO : (각 자리마다 숫자가 다르게)
+
+
+
+
     }
 }
