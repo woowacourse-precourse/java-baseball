05f06266e15c6addd0b7c0592e36e4f1bc1d23c7
JunHee <ljh19980823@gmail.com>
2022-11-03 17:17:10 +0900

    feat : user의 입력을 받고, 올바른 입력이 아닐 경우 Exception

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,26 +2,53 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.text.NumberFormat;
+import java.text.ParsePosition;
 import java.util.ArrayList;
 import java.util.List;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
 
-    public static List<Integer> makeRandomNumber(){
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+    public static boolean isNumeric(String userInput) {
+        ParsePosition pos = new ParsePosition(0);
+        NumberFormat.getInstance().parse(userInput, pos);
+        return userInput.length() == pos.getIndex();
+    }
+
+    public static List<Integer> makeRandomNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
             }
         }
 
-        return computer;
+        return computerNumber;
     }
+
+    public static List<Integer> getUserNumber() {
+        List<Integer> userInputNumber = new ArrayList<>();
+        String userInput = readLine();
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        if(!isNumeric(userInput)){
+            throw new IllegalArgumentException();
+        }
+        return userInputNumber;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        List<Integer> computer = makeRandomNumber();
+        List<Integer> computerNumber = makeRandomNumber();
+        try {
+            List<Integer> userInputNumber = getUserNumber();
+        } catch (IllegalArgumentException exception) {
+            System.out.println("fuck you!");
+            return;
+        }
 
-        
     }
 }
