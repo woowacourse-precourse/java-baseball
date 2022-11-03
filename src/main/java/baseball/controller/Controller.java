2fc3f7d56091c73d8b37ef8ff5d1ac979efaf67a
소대훈 <theogns0681@naver.com>
2022-11-03 09:34:03 +0900

    [feat] 컨트롤러 구현

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -1,27 +1,45 @@
 package baseball.controller;
 
 import baseball.ball.Ball;
+import baseball.model.Model;
+import baseball.view.View;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Controller {
-
+    Model model= new Model();
+    View view = new View();
     Ball ball = new Ball();
-
-    public boolean findException(String input){
-        boolean check = false;
-        if (input.length()==3){
-            check = !checkInput(input);
-        }
-        return check;
-    }
-
-    private static boolean checkInput(String input) {
-        boolean check = false;
-        for (int i = 0 ; i < 3 ; i++){
-            if (input.charAt(i) < '1' || input.charAt(i) > '9') {
-                check = true;
+    public void start(){
+        view.startMention();
+        while (true) {
+            view.printInput();
+            String input = Console.readLine();
+            try {
+                if (!model.findException(input)) throw new IllegalArgumentException();
+            } catch (IllegalArgumentException e) {
+                e.printStackTrace();
                 break;
             }
+            int inp = Integer.parseInt(input);
+            int Strike = model.findStrike(ball.getScore(), inp);
+            int Ball = model.findBall(ball.getScore(), inp);
+            if (Strike == 3){
+                view.threeStrike();
+                int regame = Integer.parseInt(Console.readLine());
+                if (regame==1){
+                    ball = new Ball();
+                }else if (regame==2){
+                    break;
+                }
+            } else if (Ball != 0 && Strike != 0) {
+                view.strikeAndBall(Strike,Ball);
+            } else if (Ball != 0){
+                view.ball(Ball);
+            }else if (Strike !=0){
+                view.strike(Strike);
+            }else if (Ball == 0 && Strike == 0){
+                view.incorrect();
+            }
         }
-        return check;
     }
 }
