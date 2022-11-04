48c2e06663945cee8d44348002afd1312a14cf8b
kim0914 <kosa0914@naver.com>
2022-11-04 23:41:26 +0900

    feat(Message): 게임 결과를 출력하는 함수 추가

--- src/main/java/constant/Message.java
+++ src/main/java/constant/Message.java
@@ -1,20 +1,40 @@
 package constant;
 
+import static constant.Const.*;
+
 public class Message {
 
-    public static void showNumberRequestMessage(){
-        System.out.print(Const.NUMBER_INPUT_REQUEST_MESSAGE);
+    public static void showNumberRequestMessage() {
+        System.out.print(NUMBER_INPUT_REQUEST_MESSAGE);
     }
 
-    public static void showGameStartMessage(){
-        System.out.println(Const.GAME_START_MESSAGE);
+    public static void showGameStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
     }
 
-    public static void showGameEndMessage(){
-        System.out.println(Const.GAME_END_MESSAGE);
+    public static void showGameEndMessage() {
+        System.out.println(GAME_END_MESSAGE);
     }
 
-    public static void showRestartRequestMessage(){
-        System.out.println(Const.RESTART_REQUEST_MESSAGE);
+    public static void showRetryRequestMessage() {
+        System.out.println(RESTART_REQUEST_MESSAGE);
+    }
+
+    public static void showGameResultMessage(int strikeCount, int ballCount) {
+        if (strikeCount > 0 && ballCount > 0) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+
+        if (strikeCount > 0 && ballCount == 0) {
+            System.out.println(strikeCount + "스트라이크");
+        }
+
+        if (strikeCount == 0 && ballCount > 0) {
+            System.out.println(ballCount + "볼");
+        }
+
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        }
     }
 }
