5799febcfcd53f4e5eb3da4c6c226561e242f21f
박원종 <wonjong1030@naver.com>
2022-11-04 20:13:24 +0900

    feat: [기능2] 컴퓨터 숫자 랜덤생성 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,9 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
     public void printStartMessage(){
         System.out.println(START_MESSAGE);
+    }
+    public List<Integer> createComputerNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
