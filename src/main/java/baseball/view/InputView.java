7abeb3d3018a686fa33eb841a461bc7e70b9517c
ksh-beginner <ohk9134@naver.com>
2022-11-04 10:02:55 +0900

    feat(InputView) : 사용자가 입력하는 수 입력 받는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,10 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
-    
+
+    public static String inputNumber() {
+        return Console.readLine();
+    }
 }
