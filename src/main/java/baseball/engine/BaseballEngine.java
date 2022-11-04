ba2041aad406f87a50fde9f60af1e99ceb1df7b5
lcw3176 <lcw3176@naver.com>
2022-11-05 00:49:38 +0900

    feat: 숫자 야구 정답 제작 함수

---
+++ src/main/java/baseball/engine/BaseballEngine.java
@@ -0,0 +1,22 @@
+package baseball.engine;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballEngine {
+
+    public List<Integer> generateAnswer() {
+        List<Integer> answer = new ArrayList<>();
+
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+
+        return answer;
+    }
+}
