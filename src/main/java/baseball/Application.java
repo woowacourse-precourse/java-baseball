74e8b5c3deadbba79f6c7aaeaaf1621fc63d61a8
JuneYub <ajsjdlwj0123@gmail.com>
2022-11-04 22:55:26 +0900

    [feat] 1~9 사이의 3자리 수 생성 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+
 public class Application {
+
+    public static class AnswerNumber {
+        private final int maxNumber = 3;
+        private final int rangeStartValue = 1;
+        private final int rangeEndValue = 9;
+        int[] numArr = new int[maxNumber];
+
+        public int[] getNumberArr() {
+            for (int i = 0; i<maxNumber; i++) {
+                numArr[i] = Randoms.pickNumberInRange(rangeStartValue, rangeEndValue);
+            }
+            return numArr;
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 랜덤으로 숫자 세개 생성
+        AnswerNumber answerNumber = new AnswerNumber();
+
+        int[] answer = new int[3];
+        answer = answerNumber.getNumberArr();
+
+
     }
 }
