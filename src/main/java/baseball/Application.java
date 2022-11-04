582d25dad7f3eb96fe596134cf302ff8b569545b
hyeongcheolkim <tori1753@naver.com>
2022-11-04 22:16:25 +0900

    feat: 볼,스트라이크 계산기
    볼, 스트라이크 계산기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static baseball.Application.StringConst.BALL;
+import static baseball.Application.StringConst.STRIKE;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    }
+
+    public static Map<String, Integer> score(Integer computerNumber, Integer userNumber) {
+        Map<String, Integer> ret = new HashMap<>();
+        ret.put(STRIKE , 0);
+        ret.put(BALL , 0);
+        String computerNumberStringType = String.valueOf(computerNumber);
+        char[] computerPlaceValues = computerNumberStringType.toCharArray();
+        char[] userPlaceValues = String.valueOf(userNumber).toCharArray();
+
+        for (int i = 0; i < 3; ++i)
+            if (computerNumberStringType.contains(String.valueOf(userPlaceValues[i])))
+                ret.put(BALL, ret.get(BALL) + 1);
+        for (int i = 0; i < 3; ++i)
+            if (userPlaceValues[i] == computerPlaceValues[i]) {
+                ret.put(STRIKE, ret.get(STRIKE) + 1);
+                ret.put(BALL, ret.get(BALL) - 1);
+            }
+
+        return ret;
+    }
+
+    interface StringConst {
+        String BALL = "ball";
+        String STRIKE = "strike";
     }
 }
