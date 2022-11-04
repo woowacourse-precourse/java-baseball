365747cc12d0d10ce183414c31e3024cab6c6bdb
JUHA <khabh@naver.com>
2022-11-04 14:14:22 +0900

    test(InputGuessTest): 사용자 입력을 ArrayList로 변환하는 기능 테스트

--- src/test/java/baseball/InputTest.java
+++ src/test/java/baseball/InputTest.java
@@ -4,8 +4,11 @@ import baseball.domain.Input;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.util.ArrayList;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputTest {
@@ -47,10 +50,25 @@ class InputTest {
             );
         }
 
+        @Test
+        void case5() {
+            final byte[] buf = String.join("\n", "123").getBytes();
+            ArrayList<Integer> expectedResult = new ArrayList<>();
+            expectedResult.add(1);
+            expectedResult.add(2);
+            expectedResult.add(3);
+
+            Input input = new Input();
+            System.setIn(new ByteArrayInputStream(buf));
+            ArrayList<Integer> result = input.readPlayerGuess();
+
+            assertThat(result).isEqualTo(expectedResult);
+        }
+
         @Override
         public void runMain() {
             Input input = new Input();
-            input.getPlayerGuess();
+            input.readPlayerGuess();
         }
     }
 
