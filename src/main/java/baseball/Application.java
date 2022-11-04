db55713b87f6b0f7c724c48d43cab730319580c3
Minjune <minjune8506@gmail.com>
2022-11-04 15:45:39 +0900

    feat(BaseBallGame): 사용자 입력 받는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,6 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         BaseBallGame baseBallGame = new BaseBallGame(new Computer());
+        baseBallGame.start();
     }
 }
--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class BaseBallGame {
 
     private final Computer computer;
@@ -8,7 +10,22 @@ public class BaseBallGame {
         this.computer = computer;
     }
 
+    public void start() {
+        Printer.gameStartMessage();
+        init();
+        play();
+    }
+
+    private void play() {
+        String userGuess = inputUserGuess();
+    }
+
     private void init() {
         computer.generateUniqueRandomNumbers();
+    }
+
+    private String inputUserGuess() {
+        Printer.gameInputMessage();
+        return Console.readLine();
     }
 }
