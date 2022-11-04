9ec174a46fdd774848c89bb30714933e3849e0ea
eastsage <dpwk8@naver.com>
2022-11-04 21:29:31 +0900

    refactor: 정답을 생성하는 함수의 위치를 이동
    정답을 생성하는 함수의 위치가 잘못되었다고 판단되어 generateAnswer 함수를 BaseBallGame -> Rule 클래스로 이동

--- src/main/java/baseball/Rule.java
+++ src/main/java/baseball/Rule.java
@@ -1,10 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Rule {
+    private List<Integer> answer;
+
     public Rule() {
+        this.answer = generateAnswer();
     }
 
     public void print(String message) {
-        System.out.println(Message.END);
+        System.out.println(message);
+    }
+
+    public List<Integer> generateAnswer() {
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
     }
 }
