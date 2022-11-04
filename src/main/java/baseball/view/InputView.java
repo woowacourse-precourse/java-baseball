dfb54799e3868c3f0513131cd9bbccc81cd039ed
ksh-beginner <ohk9134@naver.com>
2022-11-04 10:35:11 +0900

    feat(InputView) : 사용자가 입력한 수가 숫자가 아니면 예외 발생시키는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -11,6 +11,9 @@ public class InputView {
     }
 
     private static void validateInputNumber(String inputNumber) {
+        if (isInputNumberNotDigit(inputNumber)) {
+            throw new IllegalArgumentException("[ERROR] : 입력하는 수는 숫자여야합니다.");
+        }
         if (isInputNumberLengthNotSame3(inputNumber)) {
             throw new IllegalArgumentException("[ERROR] : 입력하는 수는 3자리여야합니다.");
         }
@@ -18,5 +21,9 @@ public class InputView {
 
     private static boolean isInputNumberLengthNotSame3(String inputNumber) {
         return inputNumber.length() != 3;
+    }
+
+    private static boolean isInputNumberNotDigit(String inputNumber) {
+        return !inputNumber.matches("^[0-9]*$");
     }
 }
