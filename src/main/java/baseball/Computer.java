bbde1cfb06d465f654d3c6f20d09c3e4ebb96161
Donguk <95donguk.dev@gmail.com>
2022-11-05 04:23:12 +0900

    refactor(Computer): change variable name
    change START_NUMBER, END_NUMBER to BEGIN_NUMBER_RANGE, ENG_NUMBER_RANGE

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -6,14 +6,14 @@ import java.util.List;
 
 public class Computer {
     private static final int MAX_NUMBER_OF_DIGITS = 3;
-    private static final int START_NUMBER = 1;
-    private static final int END_NUMBER = 9;
+    private static final int BEGIN_NUMBER_RANGE = 1;
+    private static final int END_NUMBER_RANGE = 9;
 
     private final List<Integer> number = new ArrayList<>();
 
     public void generateRandomNumber() {
         while (number.size() < MAX_NUMBER_OF_DIGITS) {
-            int randomNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
+            int randomNumber = Randoms.pickNumberInRange(BEGIN_NUMBER_RANGE, END_NUMBER_RANGE);
             if (!number.contains(randomNumber)) {
                 number.add(randomNumber);
             }
