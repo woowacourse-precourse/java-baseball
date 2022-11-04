d3fa9521600b79493c9a51bc24b8dbf7b33b625b
eugene1204 <s1061001@naver.com>
2022-11-04 20:24:45 +0900

    feat: Comparenumber 클래스 생성자

--- src/main/java/baseball/Comparenumber.java
+++ src/main/java/baseball/Comparenumber.java
@@ -1,4 +1,15 @@
 package baseball;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Comparenumber {
+    List<Integer> randomList;
+    List<Integer> inputList;
+    boolean iscontinue;
+    Comparenumber(){}
+    Comparenumber(List<Integer> randomList, List<Integer> inputList){
+        this.randomList=randomList;
+        this.inputList=inputList;
+        iscontinue=true;
+    }
 }
