03399ab84757f32452318e35bf0cdc4ca7458311
choiejae <ejae8319@gmail.com>
2022-11-04 16:36:32 +0900

    feat (Game): 게임을 생성하는 기능 구현 1. 게임 시작, 종료를 구분하는 toBeContinued 변수 생성 2. toBeContinued 값을 입력받는 기능(generateNumber) 3. 입력 받은 값에 대한 유효성 체크 기능(validityChecker)

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,11 +1,41 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
-  public static int toBeContinued = 1;
+  public static String toBeContinued = "1";
   public Game() {
     this(toBeContinued);
   }
-  public Game(int toBeContinued) {
+  public Game(String toBeContinued) {
     this.toBeContinued = toBeContinued;
+  }
+
+  public String generateNumber() {
+    String inputNumber = Console.readLine();
+    if (!validityChecker(inputNumber)) {
+      throw new IllegalArgumentException("1, 또는 2를 입력해야 합니다.");
+    }
+    return inputNumber;
+  }
+
+//  forTest
+  public void generateNumberTest(String input) {
+    if (!validityChecker(input)) {
+      throw new IllegalArgumentException();
+    }
+    System.out.print(input);
+  }
+
+  public boolean validityChecker(String input) {
+    return input == "1" || input == "2";
+  }
+
+  public void setRestart() {
+//    3개의 숫자를 모두 맞히셨습니다! 게임 종료
+//    게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.
+    this.toBeContinued = generateNumber();
   }
 }
