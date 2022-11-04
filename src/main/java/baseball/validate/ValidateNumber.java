e32ebe9db78b5e30a76491b9b38085796b4a1cdc
bellljun3395 <195850@jnu.ac.kr>
2022-11-05 02:41:56 +0900

    feat(ValidateNumber): 구현 입력 검증 기능 / 서로 다른 3자리 수
    - 각 자리가 중복되지 않는 수를 가지는지 검증

--- src/main/java/baseball/validate/ValidateNumber.java
+++ src/main/java/baseball/validate/ValidateNumber.java
@@ -1,7 +1,6 @@
 package baseball.validate;
 
-import java.util.List;
-import java.util.function.BiPredicate;
+import java.util.LinkedList;
 import java.util.function.Predicate;
 
 public class ValidateNumber {
@@ -12,6 +11,22 @@ public class ValidateNumber {
         if (!p.test(length)) {
             throw new IllegalStateException("숫자의 자릿수를 확인해주세요");
         }
+    }
+
+    public static void validateOverlap(int number) {
+        String[] splitNumber = String.valueOf(number).split("");
+        LinkedList<String> linkedList = new LinkedList<>();
+        for (String n : splitNumber) {
+            linkedList.add(n);
+        }
+
+        while (linkedList.size() != 0) {
+            String pop = linkedList.pop();
+            if (linkedList.contains(pop)) {
+                throw new IllegalStateException("각 자리가 중복되지 않는 수를 입력해주세요");
+            }
+        }
+
     }
 
 }
