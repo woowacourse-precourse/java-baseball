d67fbcdbb84d2080a0a9e6c499af603b256de189
Donguk <95donguk.dev@gmail.com>
2022-11-05 01:56:42 +0900

    test: add test to check digits of random number
    add test to check random number is all different numbers

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashSet;
 import org.junit.jupiter.api.Test;
 
 class ComputerTest {
@@ -22,5 +23,14 @@ class ComputerTest {
         computer.generateRandomNumber();
 
         assertThat(computer.getNumber()).hasSize(3);
+    }
+
+    @Test
+    void 세자리의_난수가_모두_서로_다른_숫자_인지_확인() {
+        Computer computer = new Computer();
+
+        computer.generateRandomNumber();
+
+        assertThat(new HashSet<>(computer.getNumber()).size()).isEqualTo(computer.getNumber().size());
     }
 }
