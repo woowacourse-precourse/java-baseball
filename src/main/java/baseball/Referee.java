d9676cdc378581ab7e0cd94364f16732aedc738d
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-04 14:39:38 +0900

    feat(Referee): Referee 객체 생성
    Referee 주요 기능:
    - getNumberOfBall 메서드 (서로 같은 숫자의 개수 리턴)
    - getNumberOfStrike 메서드 (같은 자리에 같은 숫자의 개수 리턴)
    - judgeResult 메서드 (심판 결과 리턴)
    - getResultMessage (심판 결과 메시지를 외부에 리턴)

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,2 +1,63 @@
-package baseball;public class Referee {
+package baseball;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Referee {
+	private final List<Integer> pitcherPlayingNumbers;
+	private final List<Integer> batterPlayingNumbers;
+
+	public Referee(Pitcher pitcher, Batter batter) {
+		this.pitcherPlayingNumbers = pitcher.getPlayingNumbers();
+		this.batterPlayingNumbers = batter.getPlayingNumbers();
+	}
+
+	public Referee(List<Integer> pitcherPlayingNumbers, List<Integer> batterPlayingNumbers) {
+		this.pitcherPlayingNumbers = pitcherPlayingNumbers;
+		this.batterPlayingNumbers = batterPlayingNumbers;
+	}
+
+	public String getResultMessage() {
+		if (pitcherPlayingNumbers.equals(batterPlayingNumbers)) {
+			return "3스트라이크";
+		}
+		// 볼 개수 메서드
+		int numberOfBalls = getNumberOfBalls();
+		// 스트라이크 개수 메서드
+		int numberOfStrikes = getNumberOfStrikes();
+		//리턴 볼이 0일때, 스트라이크가 0일때 낫싱일때
+		return judgeResult(numberOfBalls, numberOfStrikes);
+	}
+
+	private int getNumberOfStrikes() {
+		int numberOfStrikes = 0;
+		for (int i = 0; i < 3; i++) {
+			if (pitcherPlayingNumbers.get(i).equals(batterPlayingNumbers.get(i))) {
+				numberOfStrikes++;
+			}
+		}
+		return numberOfStrikes;
+	}
+
+	private int getNumberOfBalls() {
+		Set<Integer> ballNumbers = pitcherPlayingNumbers
+			.stream()
+			.filter(batterPlayingNumbers::contains)
+			.collect(Collectors.toSet());
+		return ballNumbers.size();
+	}
+
+	private String judgeResult(int numberOfBalls, int numberOfStrikes) {
+		if (numberOfBalls - numberOfStrikes == 0 && numberOfStrikes == 0) {
+			return "낫싱";
+		}
+		if (numberOfBalls - numberOfStrikes == 0) {
+			return String.format("%d스트라이크", numberOfStrikes);
+		}
+		if (numberOfStrikes == 0) {
+			return String.format("%d볼", numberOfBalls);
+		}
+		return String.format("%d볼 %d스트라이크", (numberOfBalls - numberOfStrikes), numberOfStrikes);
+	}
 }
