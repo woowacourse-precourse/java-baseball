09334de3f0fb377dc5435b555b10ec9b45908bb7
EUNJEONGMUN <munej26@gmail.com>
2022-11-04 12:57:24 +0900

    test: 메서드 명 변경

--- src/test/java/baseball/UserInputNumbersTest.java
+++ src/test/java/baseball/UserInputNumbersTest.java
@@ -8,36 +8,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class UserInputNumbersTest {
     @Test
     void 입력값_테스트_정상입력() {
-        String userInputValue = "123";
-        boolean result = UserInputNumbers.validUserInputNumber(userInputValue);
+        String userInput = "123";
+        boolean result = UserInputNumbers.validUserInputNumber(userInput);
         assertTrue(result);
     }
 
     @Test
     void 입력값_테스트_비정상입력_길이초과() {
-        String userInputValue = "1234";
-        assertThatThrownBy(() -> UserInputNumbers.validUserInputNumber(userInputValue))
+        String userInput = "1234";
+        assertThatThrownBy(() -> UserInputNumbers.validUserInputNumber(userInput))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 입력값_테스트_비정상입력_길이미달() {
-        String userInputValue = "1";
-        assertThatThrownBy(() -> UserInputNumbers.validUserInputNumber(userInputValue))
+        String userInput = "1";
+        assertThatThrownBy(() -> UserInputNumbers.validUserInputNumber(userInput))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 입력값_테스트_비정상입력_문자포함() {
-        String userInputValue = "12c";
-        assertThatThrownBy(() -> UserInputNumbers.validUserInputNumber(userInputValue))
+        String userInput = "12c";
+        assertThatThrownBy(() -> UserInputNumbers.validUserInputNumber(userInput))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 입력값_테스트_비정상입력_같은숫자포함() {
-        String userInputValue = "224";
-        assertThatThrownBy(() -> UserInputNumbers.validUserInputNumber(userInputValue))
+        String userInput = "224";
+        assertThatThrownBy(() -> UserInputNumbers.validUserInputNumber(userInput))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
