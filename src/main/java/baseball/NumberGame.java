267ca3a3f8be3e68713b6ebb8398837a62480abe
thdwoqor <celebarte01@gmail.com>
2022-11-04 22:00:42 +0900

    docs: `난수 생성 기능` 작성

--- src/main/java/baseball/NumberGame.java
+++ src/main/java/baseball/NumberGame.java
@@ -15,4 +15,15 @@ abstract public class NumberGame {
     abstract public boolean play();
 
     abstract public boolean end();
+
+    public void setRandomNumber() {
+        List<String> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            String randomNumber = Integer.toString(Randoms.pickNumberInRange(1, 9));
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        this.computer=String.join("", computer);
+    }
 }
