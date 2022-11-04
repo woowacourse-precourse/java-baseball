f0bf8835b1f713352e874e3608bbce2e1c03cc80
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-04 22:11:22 +0900

    feat(User): 입력 받은 사용자의 3개의 숫자를 저장, 예외처리 기능 구현

--- src/main/java/baseball/players/User.java
+++ src/main/java/baseball/players/User.java
@@ -1,47 +1,39 @@
 package baseball.players;
 
-import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class User {
 
-    List<Integer> userNumbers = new ArrayList<>();
+    private String userNumbers;
 
-    private void checkError(String num) throws IllegalArgumentException {
+    public User(String userNumbers) {
+        userNumbers = userNumbers.replaceAll(" ", "");
+        checkError(userNumbers);
+        this.userNumbers = userNumbers;
+    }
+
+    public String getUserNumbers() {
+        return userNumbers;
+    }
+
+    private void checkError(String userNumbers) throws IllegalArgumentException {
         final String REGEX = "[0-9]+";
-        int length = num.length();
+        int length = userNumbers.length();
 
         if (!(length == 3)) {
             throw new IllegalArgumentException("숫자를 3개 입력해주세요.");
-        } else if (num.matches(REGEX)) {
+        } else if (!userNumbers.matches(REGEX)) {
             throw new IllegalArgumentException("문자는 입력할 수 없습니다.");
         }
 
         Set<Character> duplicateCheck = new HashSet<>();
 
-        for (char x : num.toCharArray()) {
+        for (char x : userNumbers.toCharArray()) {
             duplicateCheck.add(x);
         }
-        if(!(duplicateCheck.size() == 3))
+        if (!(duplicateCheck.size() == 3))
             throw new IllegalArgumentException("중복된 숫자가 있습니다.");
-
-    }
-
-    public List<Integer> createUserNumbers() {
-        System.out.print("숫자를 입력해주세요 : ");
-        String num = Console.readLine();
-        num = num.replaceAll(" ", "");
-
-        checkError(num);
-
-        for (int i = 0; i < num.length(); i++) {
-            userNumbers.add(Character.getNumericValue(num.charAt(i)));
-        }
-
-        return userNumbers;
     }
 }
