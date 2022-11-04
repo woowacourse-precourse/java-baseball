64df7ccde3c53dbdaea32c186f9f7bc94d30b162
JunH <tjdtls690@gmail.com>
2022-11-04 17:32:46 +0900

    :sparkles: feat(Controller : BaseBall) : 3스트라이크 되기 전까지 반복해서 사용자 숫자 입력 받기 기능 구현

--- src/main/java/baseball/controller/BaseBall.java
+++ src/main/java/baseball/controller/BaseBall.java
@@ -1,5 +1,7 @@
 package baseball.controller;
 
+import baseball.domain.BaseBallGame;
+import baseball.domain.Referee;
 import baseball.dto.UserNumbersDTO;
 import baseball.view.InputView;
 import baseball.view.OutputView;
@@ -7,7 +9,26 @@ import baseball.view.OutputView;
 public class BaseBall {
     public void start() {
         OutputView.printApplicationStartMessage();
-        
-        final UserNumbersDTO userNumbersDTO = InputView.inputUserBalls();
+        playBaseBallGame();
+    }
+    
+    private void playBaseBallGame() {
+        repeatBaseBallGame(new BaseBallGame());
+    }
+    
+    private void repeatBaseBallGame(final BaseBallGame baseBallGame) {
+        boolean isEnd = false;
+        while(!isEnd) {
+            final Referee referee = playResult(baseBallGame);
+            isEnd = referee.isEnd();
+        }
+    }
+    
+    private Referee playResult(final BaseBallGame baseBallGame) {
+        return baseBallGame.play(inputUserBalls().getUserNumbers());
+    }
+    
+    private UserNumbersDTO inputUserBalls() {
+        return InputView.inputUserBalls();
     }
 }
