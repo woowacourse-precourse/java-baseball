cc45875b7d22e5be4eb1884a0f833bffa1296666
kimminsu <xjfcnfw3@naver.com>
2022-11-04 09:56:49 +0900

    feat: [예외 처리추가] 각 자릿수는 1 ~ 9여야 한다.

--- src/main/java/baseball/user/validation/UserValidation.java
+++ src/main/java/baseball/user/validation/UserValidation.java
@@ -1,5 +1,7 @@
 package baseball.user.validation;
 
+import java.util.stream.Stream;
+
 public class UserValidation {
 
     private static final int NUMBER_LENGTH = 3;
@@ -8,6 +10,7 @@ public class UserValidation {
     public static void validate(String userInput) {
         validateOnlyNumber(userInput);
         validateNumberLength(userInput);
+        validateNumberRange(userInput);
     }
 
     public static void validateOnlyNumber(String userInput) {
@@ -19,6 +22,15 @@ public class UserValidation {
     public static void validateNumberLength(String userInput) {
         if (userInput.length() != NUMBER_LENGTH) {
             throw new IllegalArgumentException(ExceptionMessage.NUMBER_LENGTH);
+        }
+    }
+
+    public static void validateNumberRange(String userInput) {
+        boolean correctRange = Stream.of(userInput.split(""))
+            .mapToInt(Integer::parseInt)
+            .allMatch((number) -> (number <= 9) && (number >= 1));
+        if (!correctRange) {
+            throw new IllegalArgumentException(ExceptionMessage.NUMBER_RANGE);
         }
     }
 }
