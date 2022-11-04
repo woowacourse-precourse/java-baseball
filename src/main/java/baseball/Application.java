fef260b6e95e290b455e4f7ffab626e3583fde2e
kkh <kkho9654@naver.com>
2022-11-04 20:41:05 +0900

    Feat: 사용자 입력 예외처리
    - 3자리 숫자인지확인
    - 각 숫자가 1부터 9사이인지확인
    - 중복입력된 숫자 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
@@ -31,6 +32,23 @@ public class Application {
         String userInputString = Console.readLine();
         System.out.println(userInputString);
 
+        userInputValidation(userInputString);
         return userInputString;
+    }
+
+    private static void userInputValidation(String userInputString) {
+        String pattern = "^[1-9]{3}$";
+        if (!Pattern.matches(pattern, userInputString)) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < userInputString.length(); i++) {
+            String indexDigitNumber = userInputString.charAt(i) + "";
+            String checkOverlapString = userInputString.replace(indexDigitNumber, "");
+
+            if (checkOverlapString.length() < 2) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
