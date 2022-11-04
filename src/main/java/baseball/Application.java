ed0a046fe3fb287ffc6ba4b0310151518a32ec26
woo-chang <clllickme@naver.com>
2022-11-04 20:47:31 +0900

    chore: 게임 실행 반복을 위한 안내 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,15 @@
 package baseball;
 
 import baseball.domain.BaseBall;
+import baseball.domain.Game;
 import baseball.util.GameManager;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GameManager gameManager = new GameManager(new BaseBall());
+        Game game = new BaseBall();
+        GameManager gameManager = new GameManager(game);
         gameManager.execute();
     }
 }
--- src/main/java/baseball/util/GameManager.java
+++ src/main/java/baseball/util/GameManager.java
@@ -14,6 +14,7 @@ public class GameManager {
     public void execute() {
         do {
             game.play();
+            GamePrinter.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         } while (Integer.parseInt(Console.readLine()) == 1);
     }
 
