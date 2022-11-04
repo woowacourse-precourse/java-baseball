859676f7043acc2ed0f3c8201200f1d4d6b4387c
YuHayeon <youha0930@gmail.com>
2022-11-04 13:27:15 +0900

    feat(Hint): 게임 결과에 대한 힌트 출력값을 만듦
    스트라이크, 볼의 개수로 게임 힌트값을 만듦

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public class Hint {
 
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final Integer BASEBALL_NUMBERS_LENGTH = 3;
 
     private Integer strike;
@@ -28,5 +32,26 @@ public class Hint {
                 this.ball += 1;
             }
         }
+    }
+
+    private String makeHintMessage() {
+        String result = "";
+        if (this.strike == 3 && this.ball == 0) {
+            result = this.strike + STRIKE_MESSAGE + "\n" + WIN_MESSAGE;
+        } else if (this.strike == 0 && this.ball == 0) {
+            result = NOTHING_MESSAGE;
+        } else if (this.strike == 0 && this.ball > 0) {
+            result += this.ball + BALL_MESSAGE;
+        } else if (this.strike > 0 && this.ball == 0) {
+            result = this.strike + STRIKE_MESSAGE;
+        } else if (this.strike > 0 && this.ball > 0) {
+            result = this.ball + BALL_MESSAGE + " " + this.strike + STRIKE_MESSAGE;
+        }
+        return result;
+    }
+
+    public String getBaseballResult(List<Integer> player, List<Integer> computer) {
+        countBaseballResult(player, computer);
+        return makeHintMessage();
     }
 }
