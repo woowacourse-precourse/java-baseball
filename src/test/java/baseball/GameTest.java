0aaf5e9e23fd930820d6e391e975c74dc513cdf1
Laeng <Laeng@users.noreply.github.com>
2022-11-04 21:33:46 +0900

    test(GameTest): 테스트 추가

---
+++ src/test/java/baseball/GameTest.java
@@ -0,0 +1,35 @@
+package baseball;
+
+import baseball.utils.GameUtil;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayName("Game 테스트")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class GameTest extends NsTest {
+    private final int computerNumber = 123;
+
+    @Test
+    void 게임_플레이() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("987", "198", "918", "132", "123");
+                    assertThat(output()).contains("낫싱", "1스트라이크", "1볼", "2볼 1스트라이크", "3스트라이크");
+                },
+                1, 3, 5, 5, 8, 9
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Game game = new Game(this.computerNumber);
+        game.run(GameUtil.splitNumberToOneDigitNumbers(this.computerNumber).size());
+    }
+}
