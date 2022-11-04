15cde2047efb0cabf5163f34297f5f173a8a173b
skydreamer21 <skydreamer210@gmail.com>
2022-11-04 18:51:30 +0900

    feat: 유효한 입력인지 검사하는 isValidNumberForGame 메서드 구현
    4가지 하위 검사로 진행
    - isOnlyNumber (true)
    - hasCorrectSize (true)
    - isAllUnique (true)
    - hasZero (false)
    (괄호 안의 값은 해당 검사가 통과되기 위한 기준)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,12 +3,53 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+    }
+
+    public static boolean isValidNumbersForGame (String input) {
+        if (!isOnlyNumbers(input)) {
+            return false;
+        }
+
+        if (!hasCorrectSize(input)) {
+            return false;
+        }
+
+        if (!isAllUnique(input)) {
+            return false;
+        }
+
+        if (hasZero(input)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isOnlyNumbers (String input) {
+        return Pattern.matches("^[0-9]*$", input);
+    }
+
+    public static boolean hasCorrectSize (String input) {
+        return input.length() == 3;
+    }
+
+    public static boolean isAllUnique (String input) {
+        Set<Character> uniqueNumbers = new HashSet<>();
+        for (Character number : input.toCharArray()) {
+            uniqueNumbers.add(number);
+        }
+        return uniqueNumbers.size() == 3;
+    }
+
+    public static boolean hasZero (String input) {
+        return Pattern.matches("0", input);
     }
 
     public static List<Integer> makeAnswer() {
