cbacdaa54b40dd4dbe2460aa01631d6b09ae4c2c
MinHyeok Moon <odo27@MinHyeoks-MacBook-Air.local>
2022-11-04 19:20:26 +0900

    feat(Result): add constructor

---
+++ src/main/java/baseball/Result.java
@@ -0,0 +1,19 @@
+package baseball;
+
+public class Result {
+    private int ball;
+    private int strike;
+
+    Result() {
+        ball = 0;
+        strike = 0;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+}
