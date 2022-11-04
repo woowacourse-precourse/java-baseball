5b374d1aac542460254046f9ba5dfa20a42d54e4
recicoco068 <gkapf838@gmail.com>
2022-11-04 22:52:06 +0900

    fix(Baseball.java) : 컴퓨터의 랜덤 숫자를 만드는 기능 코드 수정 및 함수명 수정

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,12 +1,19 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.IntStream;
 
 public class Baseball {
-    public static List<Integer> getRandomNumber() {
-        return Randoms.pickUniqueNumbersInRange(1, 9, 3);
+    public static List<Integer> getComputerNumber() {
+        Set<Integer> numbers = new HashSet<>();
+        while (numbers.size() < 3) {
+            numbers.add(Randoms.pickNumberInRange(1, 9));
+        }
+        return new ArrayList<>(numbers);
     }
 
     public static int getBallCount(List<Integer> computer, List<Integer> user) {
@@ -14,8 +21,6 @@ public class Baseball {
     }
 
     public static int getStrikeCount(List<Integer> computer, List<Integer> user) {
-        return (int) IntStream.range(0, 3)
-                .filter(i -> computer.get(i).equals(user.get(i)))
-                .count();
+        return (int) IntStream.range(0, 3).filter(i -> computer.get(i).equals(user.get(i))).count();
     }
 }
