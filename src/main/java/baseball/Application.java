fdb4a49a34d3a22c3adbabf8ca223f12fecb6256
hwi215 <buc06137@naver.com>
2022-11-05 02:20:55 +0900

    feat: [기능3] 숫자 맞추는 게임 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -20,6 +20,7 @@ public class Application {
             }
             System.out.println("num: " + randomNum[count]);
         }
+        //System.out.println("randomNum: " + randomNum);
 
 
         while(Game(randomNum)){
@@ -34,10 +35,33 @@ public class Application {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
-        int userNum = sc.nextInt();
+        String userNum = sc.nextLine();
 
         System.out.println("userNum: " + userNum);
 
+        String[] userNumArr = userNum.split("");
+
+        int ball = 0;
+        int strike = 0;
+        int nothing = 0;
+        for(int i = 0; i < randomNum.length; i++){
+            System.out.println("num: " + userNumArr[i]);
+            String userTarget = String.valueOf(randomNum[i]); // int -> string 형변환
+            if(userNum.contains(userTarget)){
+                for(int j = 0; j < userNumArr.length; j++){
+                    if(userTarget.equals(userNumArr[j])){
+                        if(i == j){
+                            ball++;
+                        }else{
+                            strike++;
+                        }
+                    }
+                }
+                System.out.println(userTarget + "포함되어있음");
+            }else{
+                nothing++;
+            }
+        }
 
 
         return result;
