d0666ef26ddabaa316219ef701cf4c01cfa6de47
thdwoqor <celebarte01@gmail.com>
2022-11-04 22:16:45 +0900

    feat: `hint` 기능 구현
    볼의 경우 중복 숫자를 모두 더한값에서 스트라이크 값을 빼서 구한다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -24,4 +24,20 @@ public class BaseballGame extends NumberGame {
     public boolean end() {
         return false;
     }
+
+    public Point hint(String playerInput) {
+        int strike;
+        int ball;
+        int temp;
+
+        List<String> numberList = Arrays.asList(playerInput.split(""));
+        List<String> randomNumberList = Arrays.asList(computer.split(""));
+
+        temp = (int) randomNumberList.stream().filter(e -> numberList.stream().anyMatch(Predicate.isEqual(e))).count();
+        strike = (int) IntStream.range(0, 3).filter(e -> numberList.get(e).equals(randomNumberList.get(e))).count();
+        ball = temp - strike;
+        Point point = new Point();
+        point.setBall(ball).setStrike(strike);
+        return point;
+    }
 }
