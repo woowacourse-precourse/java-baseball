5a6e266cdb9e0c331457b51609da77f1b80d6c37
d0ri123 <lsh81550@gmail.com>
2022-11-05 01:42:12 +0900

    Feat: 숫자를 입력하는 기능

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,2 +1,10 @@
-package baseball;public class User {
+package baseball;
+
+public class User {
+    int response;
+    int result;
+
+    public void setResponse(int response) { //숫자를 입력하는 기능
+        this.response = response;
+    }
 }
