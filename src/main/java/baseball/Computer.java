ef913797511850b747742ede50960f65d36da893
이경범 <tkdwk567@naver.com>
2022-11-04 20:14:27 +0900

    feat : 난수를 가지는 컴퓨터 생성
    난수 값 3개를 가지는 컴퓨터 클래스 입니다.

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    private final NumberGenerator numberGenerator;
+    private final List<Integer> numbers;
+
+    public Computer(NumberGenerator generateNumber) {
+        this.numberGenerator = generateNumber;
+        this.numbers = new ArrayList<>();
+    }
+
+    public void generateNumbers() {
+        numberGenerator.generate(numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+}
