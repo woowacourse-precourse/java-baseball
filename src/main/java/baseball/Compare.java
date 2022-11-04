0b4032096001773202e324f38554b9f9cb8122ad
wonho-seo <sjdlrnen0507@naver.com>
2022-11-05 03:12:47 +0900

    feat(baseball.Compare.java): implement baseBall-compare-02
    implement baseBall-compare-02 의 기능을 구현
    
    숫자가 담겨있는 두 List를 비교한다
    숫자의 위치와 값이 둘다 일치하는지 비교한다.

---
+++ src/main/java/baseball/Compare.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Compare {
+    public Map<String, Integer> getResultToMap(List<Integer> randomNumberList, List<Integer> inputNumberList){
+        return new HashMap<>();
+    }
+
+    public int getNumberOfStrike(List<Integer> randomNumberList, List<Integer> inputNumberList){
+        int numberOfStrike = 0;
+        for (int digit = 0; digit < randomNumberList.size(); digit++){
+            if (randomNumberList.get(digit).equals(inputNumberList.get(digit))){
+                numberOfStrike += 1;
+            }
+        }
+        return numberOfStrike;
+    }
+
+    public int getNumberOfBall(List<Integer> randomNumberList, List<Integer> inputNumberList){
+        return 0;
+    }
+}
