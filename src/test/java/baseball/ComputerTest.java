8ca1e72658f23e27e55c253881a99ad5e8d92e82
Park shinyeong <syngh503@gmail.com>
2022-11-04 17:22:55 +0900

    Test(ComputerTest.java): 힌트 생성 기능 테스트 생성 힌트 생성 기능이 잘 작동하는지 테스트합니다. - case1() 낫싱인 경우 - case2() 1볼인 경우 - case3() 2볼인 경우 - case4() 3볼인 경우 - case5() 2볼 1스트라이크인 경우 - case6 2스트라이크인 경우 - case7 3스트라이크인 경우
    Relate to: #7, #11
    Closes: #11

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -44,4 +44,71 @@ public class ComputerTest {
         }
     }
 
+    @Nested
+    class TestGetHint{
+        @Test
+        @DisplayName("낫싱인 경우")
+        void case1() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> playerNumber = List.of(4, 5, 6);
+
+            void result = Computer.getHint(baseballNumber, playerNumber);
+            assertThat(result).isEqualTo("낫싱");
+        }
+
+        @Test
+        @DisplayName("1볼인 경우")
+        void case2() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> playerNumber = List.of(3, 6, 7);
+            String result = Computer.getHint(baseballNumber, playerNumber);
+            assertThat(result).isEqualTo("1볼");
+        }
+
+        @Test
+        @DisplayName("2볼인 경우")
+        void case3() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> playerNumber = List.of(3, 1, 7);
+            String result = Computer.getHint(baseballNumber, playerNumber);
+            assertThat(result).isEqualTo("2볼");
+        }
+
+        @Test
+        @DisplayName("3볼인 경우")
+        void case4() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> playerNumber = List.of(3, 1, 2);
+            String result = Computer.getHint(baseballNumber, playerNumber);
+            assertThat(result).isEqualTo("3볼");
+        }
+
+        @Test
+        @DisplayName("2볼 1스트라이크인 경우")
+        void case5() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> playerNumber = List.of(1, 3, 2);
+            String result = Computer.getHint(baseballNumber, playerNumber);
+            assertThat(result).isEqualTo("2볼 1스트라이크");
+        }
+
+        @Test
+        @DisplayName("2스트라이크인 경우")
+        void case6() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> playerNumber = List.of(1, 9, 3);
+            String result = Computer.getHint(baseballNumber, playerNumber);
+            assertThat(result).isEqualTo("2스트라이크");
+        }
+
+        @Test
+        @DisplayName("3스트라이크인 경우")
+        void case7() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> playerNumber = List.of(1, 2, 3);
+            String result = Computer.getHint(baseballNumber, playerNumber);
+            assertThat(result).isEqualTo("3스트라이크");
+        }
+    }
+
 }
