87bcd200048915676fe694035630d656c2bfc64e
rohsik2 <rohsik2@gmail.com>
2022-11-04 17:19:46 +0900

    feat : main method내에 게임 추가 진행여부 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -9,8 +10,21 @@ import java.util.List;
 import java.util.Set;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IllegalArgumentException {
+        while (true) {
+            playGame();
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String more = Console.readLine();
+
+            if (more.equals("1")) {
+                continue;
+            } else if (more.equals("2")) {
+                break;
+            } else {
+                throw new IllegalArgumentException("종료를 위해서는 2를 입력해야 합니다.");
+            }
+        }
     }
 
     public static Integer[] generateAnswer() {
@@ -44,22 +58,56 @@ public class Application {
         return result;
     }
 
-    public static int countStrike(Integer[] realAnswer, Integer[] userAnswer){
+    public static int countStrike(Integer[] realAnswer, Integer[] userAnswer) {
         int cnt = 0;
-        for(int i= 0; i<3; i++){
-            if(realAnswer[i].equals(userAnswer[i]))
+        for (int i = 0; i < 3; i++) {
+            if (realAnswer[i].equals(userAnswer[i])) {
                 cnt++;
+            }
         }
         return cnt;
     }
 
-    public static int countBall(Integer[] realAnswer, Integer[] userAnswer, int strike){
+    public static int countBall(Integer[] realAnswer, Integer[] userAnswer, int strike) {
         int cnt = 0;
         Set answerSet = new HashSet(Arrays.asList(realAnswer));
-        for(int i = 0; i<3; i++){
-            if(answerSet.contains(userAnswer[i]))
+        for (int i = 0; i < 3; i++) {
+            if (answerSet.contains(userAnswer[i])) {
                 cnt++;
+            }
         }
         return cnt - strike;
+    }
+
+    public static void printBallCount(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (ball == 0) {
+            System.out.printf("%d스트라이크\n", strike);
+        } else if (strike == 0) {
+            System.out.printf("%d볼\n", ball);
+        } else {
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+        }
+    }
+
+    public static void playGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int strike = 0;
+        int ball;
+        Integer[] answer = generateAnswer();
+        while (strike != 3) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String strUserAns = Console.readLine();
+
+            Integer[] arrUserAns = userInputToIntegerArray(strUserAns);
+
+            strike = countStrike(answer, arrUserAns);
+            ball = countBall(answer, arrUserAns, strike);
+
+            printBallCount(strike, ball);
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
