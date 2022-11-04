fd1a19f8da93148f80ac9f4825451575d8806e38
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-05 02:15:27 +0900

    test : 자동으로 컴퓨터 숫자를 생성하는 메서드 테스트 - 컴퓨터는 3자리의 숫자를 생성하는가? - 컴퓨터가 생성한 숫자들은 서로 다른가?

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,10 +1,13 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import jdk.jfr.Description;
+import org.assertj.core.util.Sets;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -49,9 +52,36 @@ class ApplicationTest extends NsTest {
         testBallNumberSet.add(new BallNumber(4));
 
         // when, then
-        assertThatThrownBy(() -> new BallNumberSet(testBallNumberSet))
+        assertThatThrownBy(() -> new PlayerNumber(testBallNumberSet))
                 .isInstanceOf(IllegalArgumentException.class);
     }
+
+    @Test
+    void 컴퓨터는_3자리의_숫자를_생성한다() {
+        // given
+        int randomNumberSetSize = 3;
+
+        // when
+        List<BallNumber> testBallNumberList = GameNumber.generateRandomNumberSet();
+
+        // then
+        assertThat(randomNumberSetSize).isEqualTo(testBallNumberList.size());
+    }
+
+    @Test
+    @Description("List를 Set으로 변경했을 때 크기가 변화지 않다면, 중복된 숫자가 없다.")
+    void 컴퓨터는_서로다른_숫자의_집합을_생성한다() {
+        // given
+        int randomNumberSetSize = 3;
+        List<BallNumber> testBallNumberList = GameNumber.generateRandomNumberSet();
+
+        // when
+        Set<BallNumber> testBallNumberSet = Sets.newHashSet(testBallNumberList);
+
+        // then
+        assertThat(randomNumberSetSize).isEqualTo(testBallNumberSet.size());
+    }
+
 
     @Override
     public void runMain() {
