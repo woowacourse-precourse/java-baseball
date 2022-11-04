fa9d086cb2625630bd7b7757a8ad4df3c2926c35
cuttlefish55 <funade7@gmail.com>
2022-11-04 15:59:47 +0900

    feat: Add computer number generator

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,44 @@
 package baseball;
 
+import java.util.List;
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean isRestart = true;
+        while (isRestart) {
+            isRestart = StartNumberBaseballGame();
+        }
     }
+
+    public static boolean StartNumberBaseballGame(){
+        List<Integer> computer = ChooseComputerNumbers(); // 컴퓨터의 숫자 입력받기
+        return false;
+    }
+
+    public static List<Integer> ChooseComputerNumbers(){
+        List<Integer> computer = new ArrayList<>(0);
+        while(computer.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+
 }
+//        List<Integer> computer = new ArrayList<>();
+//        while (computer.size() < 3) {
+//            int randomNumber = Randoms.pickNumberInRange(1, 9);
+//            if (!computer.contains(randomNumber)) {
+//                computer.add(randomNumber);
+//            }
+//        }
+//        String input = Console.readLine();
+//        System.out.println(input);
+//        System.out.println(computer);
