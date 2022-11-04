b604a2fa77151515c73399e76053a617a9e9124e
ddubidubap <park62000@gmail.com>
2022-11-05 01:23:07 +0900

    feat: 선택지를 올바른 형식으로 작성했는지 확인

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -3,4 +3,7 @@ package baseball;
 public class Constant {
     public static String WRITE_ANSWER = "숫자를 입력해주세요 : ";
     public static String GAME_FINISH = "게임 종료";
+    public static String OPTION = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static String NEW_GAME = "1";
+    public static String QUIT = "2";
 }
