8b71536f2c336346013801bf0a6201b97823da0e
KIMSEI1124 <workju1124@gmail.com>
2022-11-04 22:09:38 +0900

    test: 컴퓨터의 랜덤숫자 범위 테스트 추가

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -3,15 +3,27 @@ package baseball;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
-import java.util.logging.Logger;
+import java.util.Set;
 
 class ComputerTest {
     @Test
     void 컴퓨터의_랜덤숫자_리스트의_크기는_3() {
         Computer computer = new Computer();
         List<Integer> randomNumbers = computer.getRandomNumbers();
-        System.out.println(randomNumbers.toString());
+        System.out.println("randomNumbers = " + randomNumbers.toString());
         Assertions.assertThat(randomNumbers).hasSize(3);
+    }
+
+    @Test
+    void 컴퓨터의_랜덤숫자_범위는_1부터9() {
+        Set<Integer> range = new HashSet<>();
+        for (int count = 1; count <= 10000; count++) {
+            Computer computer = new Computer();
+            List<Integer> randomNumbers = computer.getRandomNumbers();
+            range.addAll(randomNumbers);
+        }
+        Assertions.assertThat(range).containsOnly(1, 2, 3, 4, 5, 6, 7, 8, 9);
     }
 }
