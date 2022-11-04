591beda64843d19c46e71364452a0ff54e23cd7e
JH-TT <leejongho9803@gmail.com>
2022-11-05 03:21:00 +0900

    feat(application): chooseComputerNumber function
    Added new function to Application:
    - choose computer's number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
+    private static String chooseComputerNumber() {
+        String computer = "";
+        while(computer.length() != 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (computer.contains(randomNumber + "0")) {
+                continue;
+            }
+            computer += randomNumber + "0";
+        }
+        return computer;
+    }
+
+    private static Integer playGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        String computer = chooseComputerNumber();
+        return 1;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        do {
+            playGame();
+        }while(playGame() == 1);
+        System.out.println("게임 종료");
     }
 }
