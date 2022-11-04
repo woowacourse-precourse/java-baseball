7d85218f8da0f0600ad998f09fdb04fff842ca65
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-04 15:22:28 +0900

    feat: 플레이어의 서로다른 3자리수 입력받는 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,16 +1,19 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Game {
 
-    public  List<Integer> getComputerNumber() {
+public class Game {
+    static final int INPUT_LENGTH = 3;
+
+    public List<Integer> getComputerNumber() {
         List<Integer> computerNumber = new ArrayList<Integer>();
-        while (computerNumber.size() < 3) {
+        while (computerNumber.size() < INPUT_LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber);
@@ -18,6 +21,24 @@ public class Game {
         }
         return computerNumber;
     }
+
+    public List<Integer> getPlayerNumber() throws IllegalArgumentException {
+        List<Integer> playerNumber = new ArrayList<Integer>();
+        String input = Console.readLine();
+        if (input.length() != INPUT_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < input.length(); i++) {
+            playerNumber.add(Integer.valueOf(String.valueOf(input.charAt(i))));
+        }
+        if (playerNumber.size() != playerNumber.stream().distinct().count()) {
+            throw new IllegalArgumentException();
+        }
+        return playerNumber;
+    }
+
+
+
 
 
 }
