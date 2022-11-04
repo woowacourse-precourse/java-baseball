831f37868b2482f3c560cb3a7dc38d4e063cb620
JuneYub <ajsjdlwj0123@gmail.com>
2022-11-05 02:28:25 +0900

    [feat] 사용자가 숫자를 입력할 수 있는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
+import camp.nextstep.edu.missionutils.Console;
 
 
 public class Application {
@@ -20,13 +20,50 @@ public class Application {
         }
     }
 
+    public static boolean isInput(String value) {
+
+        if(value.length() != 3){ // 3자리 글자인지 확인
+            throw new IllegalArgumentException();
+        }
+
+        for(int i=0; i<3; i++){ // 숫자만 넣었는지 확인
+            if(!Character.isDigit(value.charAt(i))) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        if(!dupliccatCheck(value)){ // 중복 값 확인
+            throw new IllegalArgumentException();
+        }
+        return false;
+    }
+
+    public static boolean dupliccatCheck(String str) {
+        int a = (int)str.charAt(0);
+        int b = (int)str.charAt(1);
+        int c = (int)str.charAt(2);
+
+        if(a != b && a != c && b != c) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
     public static void main(String[] args) {
         // 랜덤으로 숫자 세개 생성
         AnswerNumber answerNumber = new AnswerNumber();
-
         int[] answer = new int[3];
         answer = answerNumber.getNumberArr();
 
+        // 사용자 입력 받기
+        String inputValue = Console.readLine();
+        try {
+            isInput(inputValue);
+        } catch (IllegalArgumentException e) {
+            return;
+        }
 
     }
 }
