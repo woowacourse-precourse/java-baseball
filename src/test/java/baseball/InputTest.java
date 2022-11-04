e625669bf5331ba8fefdb140c1b7c55496e20c2f
yback1223 <yoyob1223@gmail.com>
2022-11-04 10:22:53 +0900

    test: add first test to check whether Input class works

--- src/test/java/baseball/InputTest.java
+++ src/test/java/baseball/InputTest.java
@@ -6,6 +6,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 class InputTest {
 
@@ -22,5 +23,12 @@ class InputTest {
   @AfterEach
   public void restoreStreams() {
     System.setOut(originalOut);
+  }
+
+  @Test
+  public void Input클래스의_정상적인_작동_테스트() {
+    String sysIn = "123";
+    inputClass.printInputString(sysIn);
+    assertEquals("[1, 2, 3]", outContent.toString().trim());
   }
 }
