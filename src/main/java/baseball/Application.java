cffc2bcda231661e19fe75dbb584f5bd13f77bd5
syr9707 <yrshin9771@gmail.com>
2022-11-04 18:44:49 +0900

    feat(baseball): 컴퓨터의 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,4 +4,19 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    // 컴퓨터의 숫자 생성
+    public static List<Integer> computer() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
 }
