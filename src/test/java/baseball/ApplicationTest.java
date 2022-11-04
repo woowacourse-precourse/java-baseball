191601f8350df497d252aefcf3087bc045577f92
hyeongcheolkim <tori1753@naver.com>
2022-11-04 22:18:18 +0900

    test: 볼, 스트라이크 계산기 테스트
    볼, 스트라이크 계산기 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,6 +3,8 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,6 +28,24 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 볼스트라이크스코어1스트라이크1볼(){
+        Integer computerNumber = 583;
+        Integer userNumber = 532;
+        Map<String, Integer> score = Application.score(computerNumber, userNumber);
+        assertThat(score.get(Application.StringConst.STRIKE)).isEqualTo(1);
+        assertThat(score.get(Application.StringConst.BALL)).isEqualTo(1);
+    }
+
+    @Test
+    void 볼스트라이크스코어0스트라이크3볼(){
+        Integer computerNumber = 123;
+        Integer userNumber = 231;
+        Map<String, Integer> score = Application.score(computerNumber, userNumber);
+        assertThat(score.get(Application.StringConst.STRIKE)).isEqualTo(0);
+        assertThat(score.get(Application.StringConst.BALL)).isEqualTo(3);
     }
 
     @Override
