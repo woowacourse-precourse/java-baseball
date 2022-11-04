89e479567b9f87b105368b10461f2594b491f6f5
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-11-04 21:31:14 +0900

    feat : length exception

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,8 @@ public class Application {
     public static final int USER_LENGTH_NOT_3 = 1;
     public static final int ALPHA_IN_USER = 2;
 
+
+
     public static void showPrompt(List<Integer> answer) {
         String user = null;
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -19,7 +21,6 @@ public class Application {
         while (true) {
             System.out.println("숫자를 입력해주세요 : ");
             user = Console.readLine();
-            System.out.println(user);
         }
     }
     public static List<Integer> getAnswer() {
---
+++ src/main/java/baseball/ExceptionBaseball.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class ExceptionBaseball {
+    public final int USER_LENGTH_NOT_3 = 1;
+    public final int ALPHA_IN_USER = 2;
+
+    public int check_length(String userNum) {
+        if (userNum.length() == 3)
+            return 0;
+        else
+            return USER_LENGTH_NOT_3;
+    }
+}
