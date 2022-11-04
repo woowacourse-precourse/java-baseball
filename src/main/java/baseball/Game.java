93dd7ed01a181bb65c81eec574377051eefdf93b
hingjae <ljh23270@naver.com>
2022-11-05 02:24:52 +0900

    컴퓨터와 유저의 값 비교 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -6,13 +6,10 @@ import java.util.List;
 import java.util.Map;
 
 public class Game {
-    private List<Integer> computerNumber = new ArrayList<>();
-    private List<Integer> userNumber = new ArrayList<>();
-
     Map<String, Integer> ballAndStrike = new HashMap<>();
 
     // 두 수를 비교함.
-    public Map compareNumbers(){
+    public Map compareNumbers(List computerNumber, List userNumber){
         int ballCount = 0, strikeCount = 0;
         for(int i = 0; i < userNumber.size(); i++) {
             if(userNumber.get(i) == computerNumber.get(i)) {
