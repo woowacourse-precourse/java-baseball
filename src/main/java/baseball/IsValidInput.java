7dbe95c0a2bb7406e2b22fa85aec9fa18957a765
은지 <midzy0228@gmail.com>
2022-11-04 15:24:59 +0900

    feat: 올바른 입력 값인지 확인

--- src/main/java/baseball/IsValidInput.java
+++ src/main/java/baseball/IsValidInput.java
@@ -12,6 +12,16 @@ public class IsValidInput {
         return ((playing) && (input.length() == 3)) || ((! playing) && (input.length()==1));
     }
 
+    boolean IsValidValue(String input, boolean playing) {
+        if (! playing) {
+            return input.equals("1") || input.equals("2");
+        }
+        HashSet<String> validNumber = ValidNumber();
+        HashSet<String> inputSet = StringToSet(input);
+        inputSet.retainAll(validNumber);
+        return inputSet.size()==3;
+    }
+
     HashSet<String> ValidNumber() {
         HashSet<String> validNumber = new HashSet<>();
         for(int i=1; i<=9; i++) {
@@ -19,7 +29,7 @@ public class IsValidInput {
         }
         return validNumber;
     }
-    
+
     HashSet<String> StringToSet(String input) {
         HashSet<String> inputSet = new HashSet<>();
         for(int i=0; i<3; i++) {
