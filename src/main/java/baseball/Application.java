6fefa662f2ecde72947cfaea182861dfd8c88a5a
sjh910805@gmail.com <sjh910805@gmail.com>
2022-11-04 18:55:37 +0900

    feat: 입력에 따른 예외처리 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,12 +5,16 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException{
         Operator op = new Operator();
-        op.playGame();
+        while (op.getRestart()) {
+            op.playGame();
+        }
     }
 }
 
@@ -18,6 +22,7 @@ class Computer {
     private List<Integer> number;
 
     public void setNumber() {
+        showInputStatement();
         List<Integer> numberList = new ArrayList<>();
         while (numberList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -31,17 +36,29 @@ class Computer {
     public List<Integer> getNumber() {
         return number;
     }
+    public void showInputStatement() {
+        System.out.println("서로 다른 숫자 3자리 수를 입력하세요");
+    }
 }
 
 class Player {
     private List<Integer> number;
 
-    public void inputNumber() {
+    public void inputNumber() throws IllegalArgumentException{
         List<Integer> numberList = new ArrayList<>();
-        int num = Integer.valueOf(Console.readLine());
+        int num;
+        try {
+            num = Integer.parseInt(Console.readLine());
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 잘못 입력했습니다. \n 프로그램을 종료합니다.");
+        }
         while (num > 0) {
             numberList.add(num % 10);
             num /= 10;
+        }
+        if(!Operator.isCorrectNumber(numberList)) {
+            throw new IllegalArgumentException("숫자를 잘못 입력했습니다. \n 프로그램을 종료합니다.");
         }
         Collections.reverse(numberList);
         number = numberList;
@@ -58,6 +75,7 @@ class Operator {
     private int ballCount;
     private int strikeCount;
     private boolean correctAnswer;
+    private boolean restart;
 
     public Operator() {
         computer = new Computer();
@@ -65,9 +83,10 @@ class Operator {
         ballCount = 0;
         strikeCount = 0;
         correctAnswer = false;
+        restart = true;
     }
 
-    public void playGame() {
+    public void playGame() throws IllegalArgumentException{
         showStartStatement();
         computer.setNumber();
         while (!correctAnswer) {
@@ -75,6 +94,12 @@ class Operator {
             compareNumber();
             showResult();
         }
+        showRestartStatement();
+        inputWhetherToRestart();
+    }
+
+    public boolean getRestart() {
+        return restart;
     }
 
     public void compareNumber() {
@@ -98,6 +123,27 @@ class Operator {
             correctAnswer = true;
         }
     }
+    public static boolean isCorrectNumber(List<Integer> numberList) {
+        if(numberList.size() != 3) return false;
+        HashMap<Integer, Integer> mapNumber = new HashMap<>();
+        for(Integer num: numberList) {
+            if(mapNumber.containsKey(num)) return false;
+            else mapNumber.put(num, 1);
+        }
+        return true;
+    }
+
+    public void inputWhetherToRestart() throws IllegalArgumentException{
+        String inputString = Console.readLine();
+        if (inputString.equals("1")) {
+            restart = true;
+        } else if (inputString.equals("2")) {
+            restart = false;
+        }
+        else {
+            throw new IllegalArgumentException("숫자를 잘못 입력했습니다. \n 프로그램을 종료합니다.");
+        }
+    }
 
     public void showResult() {
         if (ballCount == 0 && strikeCount == 0) {
@@ -112,5 +158,9 @@ class Operator {
 
     public void showStartStatement() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void showRestartStatement() {
+        System.out.println("재시작 하시겠습니까?('1'입력시 재시작/'2'입력시 게임종료): ");
     }
 }
