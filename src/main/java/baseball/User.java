e0f115622ef292ed82f9e627b41570630e33783d
hingjae <ljh23270@naver.com>
2022-11-05 02:57:08 +0900

    세자리 수 입력

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,2 +1,26 @@
-package baseball;public class User {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    List<Integer> inputNumberList = new ArrayList<>();
+    private int inputNumber;
+
+    public void input() {
+        String inputString;
+        inputString = Console.readLine();
+        setInputNumber(Integer.parseInt(inputString));
+    }
+
+    public int getInputNumber() {
+        return inputNumber;
+    }
+
+    public void setInputNumber(int inputNumber) {
+        this.inputNumber = inputNumber;
+    }
 }
