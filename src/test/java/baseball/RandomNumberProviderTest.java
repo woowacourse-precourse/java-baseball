189228f50f04d782dd152e1eb7f09abd9d91d5ca
southoftheriver <anwjsrlrhwkd@naver.com>
2022-11-04 17:55:32 +0900

    test: add cases of generating list of random int
    test cases
    - not overlapping int
    - size of list
    - range of int

--- src/test/java/baseball/RandomNumberProviderTest.java
+++ src/test/java/baseball/RandomNumberProviderTest.java
@@ -1,4 +1,34 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class RandomNumberProviderTest {
-  
+
+    List<Integer> randomInts = RandomNumberProvider.generateRandomIntList();
+    final int MAX_SIZE = 3;
+    final int MAX_INT = 10;
+
+    @Test
+    @DisplayName("list size success test")
+    void listSizeSuccessTest() {
+        assertThat(randomInts.size()).isEqualTo(MAX_SIZE);
+    }
+
+    @Test
+    @DisplayName("list int overlapping success test")
+    void listOverlappingSuccessTest() {
+        assertThat(randomInts.stream()
+                .distinct().count()).isEqualTo(MAX_SIZE);
+    }
+
+    @Test
+    @DisplayName("list int range success test")
+    void intRangeSuccessTest() {
+        assertThat(randomInts.stream()
+                .filter(i -> i < MAX_INT).count()).isEqualTo(MAX_SIZE);
+    }
 }
