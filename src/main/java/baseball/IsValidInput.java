1b1bfc0ce05c72d3f0e0e62786ea7eeeec28488a
은지 <midzy0228@gmail.com>
2022-11-04 15:18:40 +0900

    feat: 사용자의 입력을 HashSet으로 변환

--- src/main/java/baseball/IsValidInput.java
+++ src/main/java/baseball/IsValidInput.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.HashSet;
+
 public class IsValidInput {
     String input;
     IsValidInput(String input) {
@@ -8,5 +10,13 @@ public class IsValidInput {
 
     boolean IsValidLength(String input, boolean playing) {
         return ((playing) && (input.length() == 3)) || ((! playing) && (input.length()==1));
+    }
+
+    HashSet<String> StringToSet(String input) {
+        HashSet<String> inputSet = new HashSet<>();
+        for(int i=0; i<3; i++) {
+            inputSet.add(String.valueOf(input.charAt(i)));
+        }
+        return inputSet;
     }
 }
