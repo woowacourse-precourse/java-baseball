1e44302973bda1bb7f0ac8d7f8673c8dde1fb4a2
정현수 <wjdgustn15@naver.com>
2022-11-04 17:32:10 +0900

    feat : implements Printer

--- src/main/java/baseball/Printer.java
+++ src/main/java/baseball/Printer.java
@@ -1,9 +1,15 @@
 package baseball;
+import baseball.score.Score;
 
 public class Printer {
-    public static void print(Score score) {
+    public static void println(Score score) {
+        Printer.println(score.toString());
+    }
+    public static void println(String str) {
+        System.out.println(str);
+    }
+    public static void print(String str) {
+        System.out.print(str);
     }
 
-    public static void print(String str) {
-    }
 }
