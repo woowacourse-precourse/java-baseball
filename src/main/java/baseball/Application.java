28cf7859c858643c8bcc298b7f44c17e4082236f
youngkyunIm <yee0230@naver.com>
2022-11-04 22:23:23 +0900

    style(main) : remove duplicate print

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,7 +15,6 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         while (COIN.equals("1")) {
-            System.out.print("숫자를 입력해주세요 : ");
             List<Integer> computer = getComputerValue();
 
             game(computer);
