68b6c644e09c46e6907d72fc3eef4106cf6009d7
alstjr706 <alstjr706@naver.com>
2022-11-04 09:53:48 +0900

    feat : (숫자 야구) 볼, 스트라이크, 낫싱 판별 로직 구현

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -1,4 +1,41 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class Referee {
+    public String resultJudgment(List<Integer> computer, List<Integer> player) {
+        NumberComparison numberComparison = new NumberComparison();
+        int correctCount = numberComparison.compare(computer, player);
+
+        int strike = 0;
+        for (int i = 0; i < player.size(); i++) {
+            Integer integer = player.get(i);
+            if (computer.get(i) == integer) {
+                strike++;
+            }
+        }
+
+        if (correctCount == 0) {
+            return "낫싱";
+        }
+        int ball = correctCount - strike;
+
+        return getResult(strike, ball);
+    }
+
+    private String getResult(int strike, int ball) {
+        if (ball >= 1 && strike == 0) {
+            return ball + "볼";
+        }
+
+        if (strike >= 1 && ball == 0) {
+            return strike + "스트라이크";
+        }
+
+        if (strike >= 1 && ball >= 1) {
+            return ball + "볼 " + strike + "스트라이크";
+        }
+
+        return null;
+    }
 }
