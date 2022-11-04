00919114a1e914823ef282bde8f56ea6cc6c5c42
YuHayeon <youha0930@gmail.com>
2022-11-05 01:34:01 +0900

    feat(BaseballGame): 처음 게임 시작시 시작 문구를 출력

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class BaseballGame {
+
+    private static void printStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void play() {
+        printStartMessage();
+
+    }
+}
