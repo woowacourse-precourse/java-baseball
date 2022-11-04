be419baa7abf40f8f7982357ee025761abd1ba3d
asas6978 <asas6978@naver.com>
2022-11-04 12:38:58 +0900

    feat: 상대방의 숫자를 랜덤으로 지정하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<String> computerNumber = getComputerNumber();
+    }
+
+    private static ArrayList<String> getComputerNumber() {
+        ArrayList<String> computerNumber = new ArrayList<>();
+
+        while (computerNumber.size() < 3) {
+            String number = Integer.toString(Randoms.pickNumberInRange(1, 9));
+
+            if (!computerNumber.contains(number)) {
+                computerNumber.add(number);
+            }
+        }
+
+        return computerNumber;
     }
 }
