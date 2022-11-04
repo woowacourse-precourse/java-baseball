df31f00b9c22d43c753b9eff5c9cb71ad8f8375d
rabbit0216 <rabbit1999k@gmail.com>
2022-11-04 20:56:55 +0900

    feat: 사용자 입력 1~9 사이인지 체크하는 함수 추가

--- src/main/java/baseball/CheckException.java
+++ src/main/java/baseball/CheckException.java
@@ -4,6 +4,19 @@ public class CheckException {
     static final char ONE = 81;
     static final char NINE = 89;
 
+    public static boolean checkPlayerNumber(int playerNumber){
+        String playerStringNumber = String.valueOf(playerNumber);
+        char[] playerNumberArray = playerStringNumber.toCharArray();
+        boolean isNumber;
+        for(char number : playerNumberArray){
+            isNumber = checkNumber(number);
+            if(!isNumber){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static boolean checkNumber(char number){
         boolean isNumber = true;
         if(number >= ONE && number <= NINE){
