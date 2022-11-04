7835a84b71c1e52be6fd6369fbf664bc821233f5
kjb95 <topmedia44@gmail.com>
2022-11-05 01:22:28 +0900

    feat: 숫자 야구 게임의 비정상적인 입력값을 처리하는 기능 추가
    - 숫자가 아닌 경우
    - 숫자가 3자리가 아닌경우
    - 숫자에 0이 있는 경우
    - 중복되는 숫자가 있는 경우

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,10 +2,14 @@ package baseball.view;
 
 import baseball.utils.Utils;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class InputView {
 
+    private static final int NUM_SIZE = 3;
     private static final String RE_GAME = "1";
+    private static final String THREE_SIZE_NUM_REGEX = "^[1-9]{3}$";
 
     public static String requestNum() {
         OutputView.printRequestNum();
@@ -16,7 +20,16 @@ public class InputView {
 
     static String validateRequestNum(String input) {
         input = Utils.deleteAllSpace(input);
+        if (!Pattern.matches(THREE_SIZE_NUM_REGEX, input) || hasDuplicatedNum(input)) {
+            throw new IllegalArgumentException();
+        }
         return null;
+    }
+
+    private static boolean hasDuplicatedNum(String input) {
+        return Arrays.stream(input.split(""))
+            .distinct()
+            .count() != NUM_SIZE;
     }
 
     public static boolean requestIsReGame() {
