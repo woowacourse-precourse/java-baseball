8b61a48389d71d3e59414840d5bc641ba062f353
YuHayeon <youha0930@gmail.com>
2022-11-05 01:34:44 +0900

    feat(BaseballGame): 게임에 필요한 객체를 생성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,12 +2,32 @@ package baseball;
 
 public class BaseballGame {
 
+    private static final Integer RESTART = 1;
+    private static final Integer EXIT = 2;
+
+    private static Player player;
+
+    private static Computer computer;
+
+    private static Hint hint;
+
+    private static boolean progress;
+
     private static void printStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public void play() {
+        this.progress = true;
         printStartMessage();
+        while (progress) {
+            setGame();
+        }
+    }
 
+    private void setGame() {
+        this.computer = new Computer();
+        this.player = new Player();
+        this.hint = new Hint();
     }
 }
