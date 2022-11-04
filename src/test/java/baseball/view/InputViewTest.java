a373b98cdd485a5757647afa53d06d0fb5f2cacc
ksh-beginner <ohk9134@naver.com>
2022-11-04 11:01:38 +0900

    test(InputViewTest) : 사용자가 입력한 수에 0이 포함될 경우 예외 처리 테스트

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -38,4 +38,14 @@ public class InputViewTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] : 입력하는 수는 숫자여야합니다.");
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"102", "012", "910"})
+    void 사용자가_입력한_수_입력_받기_0이_포함될_경우_예외_처리(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatThrownBy(InputView::inputNumber)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] : 입력하는 수는 0이 포함될 수 없습니다. 1~9까지의 수를 입력해주세요.");
+    }
 }
