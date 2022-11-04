edbbb365c21d1864f256fcbf52b938509cf72fa5
kmw2378 <rlarla677@gmail.com>
2022-11-05 00:00:40 +0900

    refactor (Application)
    컴퓨터로부터 숫자 입력 받는 것을 별도의 메소드로 추출
    - createRandomNumber 메소드명 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,11 +8,15 @@ import java.util.List;
 /** 기능 목록
  * 1. 컴퓨터가 1 ~ 9 까지 서로 다른 임의의 수 3개를 선택
  *   1-1. 요구사항에 나와있는 라이브러리를 사용하여 구현한다.
- *
- */
+**/
+
 public class Application {
     public static void main(String[] args) {
 
+        List<Integer> result = creatRandomNumber();
+    }
+
+    private static List<Integer> creatRandomNumber() {
         List<Integer> computer = new ArrayList<>();
 
         while (computer.size() < 3) {
@@ -23,5 +27,7 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+
+        return computer;
     }
 }
