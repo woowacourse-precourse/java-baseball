7ceefa3d045bf9b54229ae53a5adc4f59c2ee00d
yback1223 <yoyob1223@gmail.com>
2022-11-04 18:21:28 +0900

    refactor: fix beforEach

--- src/test/java/baseball/EndOrReTest.java
+++ src/test/java/baseball/EndOrReTest.java
@@ -5,27 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class EndOrReTest {
 
   private static ByteArrayOutputStream outContent;
-  private static PrintStream originalOut;
   EndOrRe endOrRe;
 
   @BeforeEach
   public void setEndOrTest() {
     outContent = new ByteArrayOutputStream();
-    originalOut = System.out;
     System.setOut(new PrintStream(outContent));
     endOrRe = new EndOrRe();
-  }
-
-  @AfterEach
-  public void restoreStreams() {
-    System.setOut(originalOut);
   }
 
   @Test
