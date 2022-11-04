681a09e5935bf8996d39eacd50813430becaa9c0
rt3310 <btac3310@gmail.com>
2022-11-05 00:03:30 +0900

    feat: 게임 시작 문구 출력 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,4 +1,9 @@
 package baseball;
 
 public class BaseballGame {
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public void playGame() {
+        System.out.println(GAME_START_MESSAGE);
+    }
 }
