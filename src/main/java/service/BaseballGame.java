75dc1202255bf2064939007ca39199d6a72448cb
MyunghyunNero <mh1kim0910@gmail.com>
2022-11-05 03:27:14 +0900

    feat : [숫자 야구 게임] 유저 숫자 입력 메서드 구현

--- src/main/java/service/BaseballGame.java
+++ src/main/java/service/BaseballGame.java
@@ -1,8 +1,11 @@
 package service;
 
+import camp.nextstep.edu.missionutils.Console;
 import data.ComputerData;
+import data.UserData;
 import repository.ComputerRepository;
 import repository.UserRepository;
+import util.Utils;
 
 public class BaseballGame {
 
@@ -16,7 +19,24 @@ public class BaseballGame {
             ComputerData computerData = new ComputerData();
             computerRepository = new ComputerRepository(computerData);
             computerRepository.setRandomNum();
+            getuserinput();
         } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    //유저 입력 받기
+    private void getuserinput() throws IllegalArgumentException{
+        UserData userData = new UserData();
+        userRepository = new UserRepository(userData);
+        strike=0;
+        ball=0;
+        System.out.println(Utils.INPUTNUMBER);
+        try{
+            String userinput= Console.readLine();
+            userRepository.inputanswer(userinput);
+            return;
+        }catch (IllegalArgumentException e){
             throw new IllegalArgumentException();
         }
     }
