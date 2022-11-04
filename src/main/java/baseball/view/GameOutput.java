4c8b1fe63a146e6dc35ed4efe2061ed63aeec7a8
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-11-04 12:49:02 +0900

    feat(view/GameOutput): User에게 보여질 view output 관련 문구생성

--- src/main/java/baseball/view/GameOutput.java
+++ src/main/java/baseball/view/GameOutput.java
@@ -1,4 +1,16 @@
 package baseball.view;
 
 public class GameOutput {
+    public static void Strike(){
+        System.out.println("스트라이크");
+    }
+    public static void Ball(){
+        System.out.println("볼");
+    }
+    public static void Nothing(){
+        System.out.println("낫싱");
+    }
+    public static void finishGame(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
