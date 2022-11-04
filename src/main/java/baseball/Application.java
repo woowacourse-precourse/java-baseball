21be9515b83c88b7c343ca199d9dc7b75886437b
tjwjdgus12 <01048733438a@gmail.com>
2022-11-04 15:37:52 +0900

    feat: 게임 종료 시 재시작 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        play();
+        do {
+            play();
+        }
+        while (wantNewGame());
     }
 
     public static String generateAnswer() {
@@ -65,5 +68,13 @@ public class Application {
             result.show();
         }
         while (!guess.equals(answer));
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static boolean wantNewGame(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        return input.equals("1");
     }
 }
