034364b98cdee4b6489efd29a26c0710c7db9371
UJ15 <dbwogml15@naver.com>
2022-11-04 17:29:15 +0900

    refactor : transferred parse method to BaseBallGameEngine

--- src/main/java/baseball/engine/BaseBallGame.java
+++ src/main/java/baseball/engine/BaseBallGame.java
@@ -2,11 +2,8 @@ package baseball.engine;
 
 import baseball.io.ConsoleInput;
 import baseball.io.ConsoleOutput;
-import baseball.model.Numbers;
-import java.util.stream.Collectors;
 
 public class BaseBallGame implements Runnable {
-    private static final int NUMBERS_SIZE = 3;
 
     private final ConsoleOutput output;
     private final ConsoleInput input;
@@ -19,29 +16,5 @@ public class BaseBallGame implements Runnable {
     @Override
     public void run() {
         output.printStartText();
-    }
-
-    private Numbers parseToNumbers(String stringNumber) {
-        validateStringNumber(stringNumber);
-
-        return new Numbers(stringNumber.chars()
-                .map(Character::getNumericValue)
-                .boxed()
-                .collect(Collectors.toList())
-        );
-    }
-
-    private void validateStringNumber(String stringNumber) {
-        if (stringNumber.length() != NUMBERS_SIZE) {
-            throw new IllegalArgumentException();
-        }
-
-        long count = stringNumber.chars()
-                .filter(Character::isDigit)
-                .count();
-
-        if (count != NUMBERS_SIZE) {
-            throw new IllegalArgumentException();
-        }
     }
 }
