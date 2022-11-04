f4061a148873207ecb392d128247c60a04a81fda
kimdahee0815 <diana815@naver.com>
2022-11-04 14:23:59 +0900

    feat(while 반복문) : 게임을 종료했을 때 다시 시작할지 완전히 종료할지 사용자 입력을 받는 if,else-if,else 구문 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,22 @@ public class Application {
     public static void main(String[] args) {
     	randomComNum(computer);  //randomComNum 함수로 computer로부터 랜덤 숫자 정하기
     	String numstr="";      //numstr string에 사용자 입력 받기
+    	
+    	System.out.println("숫자 야구 게임을 시작합니다.");
+		while(true) {
+			if(numstr.equals("1")) {  //게임을 새로 시작할 것인지 종료할 것인지 1,2 선택
+				numstr="";            //numstr = ""로 만들어 else구문으로 다시 진입하게 만드는 장치
+				continue;
+			}
+			else if(numstr.equals("2")) {
+				System.out.println("게임종료");
+				break;
+			}
+			else {                     //numstr이 1,2가 아니라면 무조건 else구문으로 들어와서 check
+				System.out.print("숫자를 입력해주세요 : "); 
+				numstr = Console.readLine();
+			}
+		}	
     }
     
     public static void randomComNum(List<Integer> computer) { //randomnumber 서로 다른 3자리수를 컴퓨터가 생성.
