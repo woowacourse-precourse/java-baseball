6a41a8e9dcc3fe92c6aa29f4180dfb1a32c1b07c
charlesuu <tiger0192004@gmail.com>
2022-11-04 22:58:48 +0900

    feat: 랜덤 수 생성하기 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,52 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int opponentNum = generateOpponentNumber();
+
+
+        return;
     }
+
+    private static int generateOpponentNumber() {
+        List<Integer> numList = new ArrayList<>();
+
+        while (numList.size() != 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numList.contains(randomNumber)) {
+                numList.add(randomNumber);
+            }
+        }
+
+        int opponentNumber = numListToThreeDigits(numList);
+
+        return opponentNumber;
+    }
+
+
+    private static List<Integer> threeDigitsToNumList(int number) {
+        List<Integer> eachDigits = new ArrayList<>();
+        for (int index = 0; index < 3; index++) {
+            eachDigits.add(number % 10);
+            number /= 10;
+        }
+        return eachDigits;
+    }
+
+    private static int numListToThreeDigits(List<Integer> eachDigits) {
+        int threeDigits = 0;
+        for (int index = 0; index < 2; index++) {
+            threeDigits += eachDigits.get(index);
+            threeDigits *= 10;
+        }
+        threeDigits += eachDigits.get(2);
+
+        return threeDigits;
+    }
+
 }
