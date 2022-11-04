db51dddd5aacd1b6232fb425dd2af4ef26c854b3
situodys <situodys@gmail.com>
2022-11-04 16:43:08 +0900

    refactor(Player): 플레이어 입력과 플레이어 정답 생성 분리

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,14 +1,12 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Player {
     private Baseballs input;
 
     public Player() {
     }
 
-    public void typeAnswerByConsole() {
-        this.input = Baseballs.of(Console.readLine());
+    public void initMyAnswerBy(String playerInput) {
+        this.input = Baseballs.of(playerInput);
     }
 }
---
+++ src/main/java/baseball/Utils.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Utils {
+
+    public static String getPlayerInputForGenerateAnswerByConsole() {
+        System.out.println("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+}
