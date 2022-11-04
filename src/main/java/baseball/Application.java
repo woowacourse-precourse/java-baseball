07004cbf3dbd4d829c6125b2c7e244061a084c85
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-11-04 17:50:47 +0900

    feat(application): 입력 값 검증

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -24,11 +24,28 @@ public class Application {
     }
 
     public static List<Integer> createInputNumberList(String input) {
+        validateAnswer(input);
+
         List<Integer> inputList = new ArrayList<>();
         for (String number : input.split("")){
             inputList.add(Integer.parseInt(number));
         }
         return inputList;
     }
-
+    public static void validateAnswer(String answer) {
+        String regexNumber = "^[1-9]*$";
+        if(!Pattern.matches(regexNumber, answer)){
+            throw new IllegalArgumentException("1~9까지의 숫자만 입력할 수 있습니다.");
+        }
+        if(answer.length() != 3){
+            throw new IllegalArgumentException("3자리 숫자만 입력하여 주십시오.");
+        }
+        if(!isDuplication(answer)){
+            throw new IllegalArgumentException("각 자리 수는 중복이 될 수 없습니다.");
+        }
+    }
+    public static boolean isDuplication(String input) {
+        List<String> duplicationCheckList = Arrays.asList(input.split(""));
+        return duplicationCheckList.size() == duplicationCheckList.stream().distinct().count();
+    }
 }
