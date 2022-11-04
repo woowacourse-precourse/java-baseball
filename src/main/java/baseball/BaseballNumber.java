96e49d703d79224334f2a2dd9250a960922fe839
swandevson <shai4956@gmail.com>
2022-11-04 18:41:20 +0900

    Refactor: BaseballNumber의 유효성 검사 메소드 분리
    - 숫자보다는 숫자 검사 행위를 하는 것에 더 가까움
    - Validiator에 유효성 검사 메소드 분리

--- src/main/java/baseball/BaseballNumber.java
+++ src/main/java/baseball/BaseballNumber.java
@@ -10,40 +10,10 @@ public class BaseballNumber {
 
 
     public void set(String input) {
-        if (!isValid(input)) {
+        if (!Validator.isValid(input)) {
             throw new IllegalArgumentException();
         }
 
         number = input;
     }
-
-
-    static public boolean containDuplicatedNumber(String input) {
-        for (int index = 0; index < input.length(); index++) {
-            if (input.indexOf(input.charAt(index)) != index) {     // 중복된 문자가 있는 경우 가장 앞의 문자를 반환하는 것을 이용
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    static public boolean isValid(String input) {
-        final String REGEX = "[1-9]+";
-
-        if (input.length() != 3) {
-            return false;
-        }
-
-        if (!input.matches(REGEX)) {
-            return false;
-        }
-
-        if(containDuplicatedNumber(input)) {
-            return false;
-        }
-
-        return true;
-    }
-
 }
