f5f0039a3bc26a59e884b2336836c1ec6fe8bfbd
yoojin Jang <yoojinjangjang@gmail.com>
2022-11-05 00:56:30 +0900

    [feat] 게임 시작 문구 출력하기

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class Game {
+    private final String openingPhrase = "숫자 야구 게임을 시작합니다.";
+
+    public Game() {
+    }
+
+
+    public String opening() {
+        return openingPhrase;
+    }
+}
---
+++ src/test/java/baseball/GameTest.java
@@ -0,0 +1,16 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void 게임_시작_문구_출력() {
+        String openingPhrase = "숫자 야구 게임을 시작합니다.";
+        Game game = new Game();
+        assertThat(openingPhrase).isEqualTo(game.opening());
+    }
+
+}
