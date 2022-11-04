2070b99b3f1b21e48f29de96f755a6abce7f84a2
안광민 <creativeduck0518@gmail.com>
2022-11-04 22:18:10 +0900

    feat(NumberComparator): 0. 숫자 검증 클래스 생성

---
+++ src/main/java/baseball/NumberComparator.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class NumberComparator {
+    private final int numberSize;
+    private final String NOTHING = "낫싱";
+    private final String STRIKE = "스트라이크";
+    private final String BALL = "볼";
+    private final String SPACE = " ";
+
+    public NumberComparator(int numberSize) {
+        this.numberSize = numberSize;
+    }
+}
