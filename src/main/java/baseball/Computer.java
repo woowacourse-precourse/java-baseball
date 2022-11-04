ffc193626596dfacfae0c82153d391fc15f6f76d
buddy <jang_taehwan@naver.com>
2022-11-05 00:46:04 +0900

    feat(Computer): 입력값 검증

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,7 +1,9 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Computer {
 
@@ -17,5 +19,22 @@ public class Computer {
 
     private void init() {
         answer = Randoms.pickUniqueNumbersInRange(MIN_VALUE, MAX_VALUE, ANSWER_LENGTH);
+    }
+
+    private void validateAnswer(List<Integer> answer) {
+        if (answer.size() != 3) {
+            throw new IllegalArgumentException("3자리 숫자만 입력 가능합니다.");
+        }
+
+        Set<Integer> answerSet = new HashSet<>(answer);
+        if (answerSet.size() != 3) {
+            throw new IllegalArgumentException("중복되지 않은 숫자만 입력 가능합니다.");
+        }
+
+        for (Integer ans : answer) {
+            if (ans < MIN_VALUE || ans > MAX_VALUE) {
+                throw new IllegalArgumentException("각 자리수는 1부터 9까지의 숫자만 입력 가능합니다.");
+            }
+        }
     }
 }
