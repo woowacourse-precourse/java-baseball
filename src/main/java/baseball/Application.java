378119c4d7e4bb6ce1a5da9ad7eef9b5ab7ec19a
Geun Woo(Kay) Park <64457922+parklaus1078@users.noreply.github.com>
2022-11-04 16:10:26 +0900

    [ADD] 숫자 야구 - 상수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,23 @@
 package baseball;
 
 public class Application {
+    private static final String EXCEPTION_TEXT = "INPUT MUST CONTAIN 3 DIFFERENT DIGITS ONLY";
+
+    private static final String STRIKE = "strike";
+    private static final String BALL = "ball";
+
+    private static final String MESSAGE_STRIKE = "스트라이크";
+    private static final String MESSAGE_BALL = "볼";
+    private static final String MESSAGE_NOTHING = "낫싱";
+    private static final String MESSAGE_START = "숫자 야구 게임을 시작합니다.";
+    private static final String MESSAGE_THREE_STRIKES = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String MESSAGE_RESTART_OR_QUIT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String MESSAGE_INPUT = "숫자를 입력해주세요 : ";
+
+    private static final String FLAG_RESTART = "1";
+    private static final String FLAG_QUIT = "2";
+
+    private static final int DIGIT_RESTRICTION = 3;
     public static void main(String[] args) {
 
     }
