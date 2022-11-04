7806327e58af79cf3aa8246124e2fc8462fe7a16
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-04 15:39:36 +0900

    feat(Exception) : 세자리 수인지 판별하는 기능 구현

--- src/main/java/baseball/exception/NumberExceptionUtils.java
+++ src/main/java/baseball/exception/NumberExceptionUtils.java
@@ -7,4 +7,10 @@ public class NumberExceptionUtils {
             throw new IllegalArgumentException("input can't have duplicate number.");
         }
     }
+
+    public static void isThreeDigits(String num) {
+        if (num.length() != 3) {
+            throw new IllegalArgumentException("input must be three digits.");
+        }
+    }
 }
