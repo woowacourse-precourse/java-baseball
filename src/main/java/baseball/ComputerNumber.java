dd576f8ab7d98a588e6fafcf41b6e0c076dd9aa6
Woogie95 <choisw950928@gmail.com>
2022-11-04 19:33:27 +0900

    feat: 컴퓨터 랜덤 숫자 구현

---
+++ src/main/java/baseball/ComputerNumber.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class ComputerNumber {
+
+    private static final int MIN_RANDOM_NUMBER = 1;
+    private static final int MAX_RANDOM_NUMBER = 9;
+    private static final int DEFAULT_SIZE_NUMBER = 3;
+
+    public Set<Integer> generateRandomNumber() {
+        Set<Integer> computerNumbers = new LinkedHashSet<>();
+        while (computerNumbers.size() != DEFAULT_SIZE_NUMBER) {
+            computerNumbers.add(Randoms.pickNumberInRange(MIN_RANDOM_NUMBER, MAX_RANDOM_NUMBER));
+        }
+        return computerNumbers;
+    }
+
+}
