4a397155d8f493a48fc0b2e6b46a8597e26bbdb4
splitube <voicesplit@daum.net>
2022-11-05 01:42:35 +0900

    feat(GameMachine): 게임의 정답을 만드는 기능 구현

--- src/main/java/baseball/NumberBaseBallGameMachine.java
+++ src/main/java/baseball/NumberBaseBallGameMachine.java
@@ -3,11 +3,18 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class NumberBaseBallGameMachine {
+    Referee referee = new Referee();
+    NumberMaker numberMaker = new NumberMaker();
+
     public void display(String message) {
         System.out.print(message);
     }
 
     public String getInputLine() {
         return Console.readLine();
+    }
+
+    public void makeNewAnswer() {
+        referee.setAnswer(numberMaker.makeThreeDifferentNumberListInRange(1, 9));
     }
 }
