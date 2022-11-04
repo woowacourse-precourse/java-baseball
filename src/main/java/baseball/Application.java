349c326b0a09e6c0e8c2effc20ead8c9a39e115d
kkh <kkho9654@naver.com>
2022-11-04 21:01:19 +0900

    Refactor: if문에 space추가 (자동정렬)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -65,13 +65,13 @@ public class Application {
                 ball += 1;
             }
         }
-        if(ball!=0){
+        if (ball != 0) {
             System.out.print(ball + "볼 ");
         }
-        if(strike!=0){
+        if (strike != 0) {
             System.out.print(strike + "스트라이크");
         }
-        if(ball==0&&strike==0){
+        if (ball == 0 && strike == 0) {
             System.out.print("낫싱");
         }
         System.out.println();
