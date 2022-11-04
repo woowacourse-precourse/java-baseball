ddd50ec7e230a8aa8bbfb417516cbe7d091800fa
choiejae <ejae8319@gmail.com>
2022-11-04 23:49:22 +0900

    refactor: 중복되는 함수 단일처리(assertStrike)

--- src/test/java/baseball/GameResultJudgementTest.java
+++ src/test/java/baseball/GameResultJudgementTest.java
@@ -11,33 +11,39 @@ import org.junit.jupiter.api.Test;
 class GameResultJudgementTest {
 
   GameResultJudgement judge;
+
   @BeforeEach
   void GameResultJudgmentCreate() {
     judge = new GameResultJudgement(3);
   }
 
+  void assertStrike(String input, String answer, int expected) {
+    assertEquals(expected, judge.judgeStrike(input, answer));
+  }
+
+
   @Test
   @Order(1)
   @DisplayName("스트라이크 3 확인")
   void JudgeStrikeCount3() {
-    assertEquals(3, judge.judgeStrike("123", "123"));
-    assertEquals(3, judge.judgeStrike("456", "456"));
-    assertEquals(3, judge.judgeStrike("789", "789"));
+    assertStrike("123", "123", 3);
+    assertStrike("456", "456", 3);
+    assertStrike("789", "789", 3);
   }
   @Test
   @Order(2)
   @DisplayName("스트라이크 2 확인")
   void JudgeStrikeCount2() {
-    assertEquals(2, judge.judgeStrike("123", "143"));
-    assertEquals(2, judge.judgeStrike("456", "356"));
-    assertEquals(2, judge.judgeStrike("789", "782"));
+    assertStrike("123", "124", 2);
+    assertStrike("456", "457", 2);
+    assertStrike("789", "289", 2);
   }
   @Test
   @Order(3)
   @DisplayName("스트라이크 1 확인")
   void JudgeStrikeCount1() {
-    assertEquals(1, judge.judgeStrike("125", "143"));
-    assertEquals(1, judge.judgeStrike("451", "356"));
-    assertEquals(1, judge.judgeStrike("719", "782"));
+    assertStrike("123", "524", 1);
+    assertStrike("456", "957", 1);
+    assertStrike("789", "762", 1);
   }
 }
