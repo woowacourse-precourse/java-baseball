54db67bb66f4dab194e7e8e3e55e80144ce00242
wkdtmdgnsp <wkdtmdgnsp@gmail.com>
2022-11-04 21:07:55 +0900

    점수 판단 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,8 +8,9 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        comNumber();
-        userNumber();
+        List comList = comNumber();
+        List userList = userNumber();
+        score(comList, userList);
     }
 
     // 컴퓨터의 서로 다른 숫자 3개 저장
@@ -42,5 +43,22 @@ public class Application {
         }
 
         return userList;
+    }
+    
+    // 점수 판단 기능
+    public static void score(List comList, List userList) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i=0; i<comList.size(); i++) {
+            if(comList.get(i) == userList.get(i)) {
+                strike++;
+                userList.set(i, 0);
+            } else if (userList.contains(comList.get(i))) {
+                ball++;
+            }
+        }
+
+        System.out.println(strike +" 스트라이크 " +ball +" 볼");
     }
 }
