d749593d351019fb5e9e80c73b75f080f33d9ab0
yumyeonghan <mgtmh991013@gmail.com>
2022-11-05 00:49:54 +0900

    style: 커밋 컨벤션

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ * feat: 새로운 기능 추가
+ * fix: 버그 수정
+ * docs: 문서 수정
+ * style: 코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
+ * refactor: 코드 리펙토링
+ * test: 테스트 코드, 리펙토링 테스트 코드 추가
+ * chore: 빌드 업무 수정, 패키지 매니저 수정
+ */
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -21,6 +31,7 @@ public class Application {
         int strikeCount = 0;
 
         strike(computer, player, strikeIndex, strikeCount);
+
 
     }
 
