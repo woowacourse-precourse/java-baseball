96320ebcbf2980e124ef3f8466c8decf53f286b8
syr9707 <yrshin9771@gmail.com>
2022-11-04 18:45:23 +0900

    feat(baseball): 플레이어의 숫자 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -19,4 +19,36 @@ public class Application {
 
         return computer;
     }
+
+    // 플레이어의 숫자 입력
+    public static List<Integer> playerNumber() {
+        List<Integer> playerNumber = new ArrayList<>();
+        int[] playerNumArr = new int[3];
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String numberStr = Console.readLine();
+
+        // 3개의 숫자가 아닐 때
+        if(numberStr.length() != 3) {
+            throw new IllegalArgumentException("Not Three Number");
+        }
+
+        for(int i = 0; i < 3; i++) {
+            playerNumArr[i] = Integer.parseInt(numberStr.substring(i, i + 1));
+
+            // 1에서 9까지가 아닐 때
+            if(!(playerNumArr[i] >= 1 && playerNumArr[i] <= 9)) {
+                throw new IllegalArgumentException("Wrong Number");
+            }
+
+            // 서로 다른 임의의 수가 아닐 때
+            if(i != 0 && (playerNumArr[i] == playerNumArr[i - 1])) {
+                throw new IllegalArgumentException("Same Number");
+            }
+
+            playerNumber.add(playerNumArr[i]);
+        }
+
+        return playerNumber;
+    }
 }
