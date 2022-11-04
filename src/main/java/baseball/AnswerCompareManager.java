9065a381dc50e6eb9797f3e9500c07dd6e46eb0f
jagajang <kosb7544@gmail.com>
2022-11-04 20:36:27 +0900

    add: add answer-input comparator

--- src/main/java/baseball/AnswerCompareManager.java
+++ src/main/java/baseball/AnswerCompareManager.java
@@ -1,4 +1,26 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AnswerCompareManager {
+    public static boolean compareAnswer(int answer, int input) {
+        int ballNum = 0, strikeNum = 0;
+        String answerStr = String.valueOf(answer);
+        String inputStr = String.valueOf(input);
+
+        for(int idx = 0; idx < 3; idx++) {
+            char digit = inputStr.charAt(idx);
+            String digitStr = inputStr.substring(idx, idx+1);
+
+            if(answerStr.charAt(idx) == digit)
+                strikeNum++;
+            else if(answerStr.contains(String.valueOf(input)))
+                ballNum++;
+        }
+
+        IOManager.getHint(ballNum, strikeNum);
+
+        return strikeNum == 3;
+    }
 }
--- src/main/java/baseball/BaseballGameManager.java
+++ src/main/java/baseball/BaseballGameManager.java
@@ -1,2 +1,20 @@
-package baseball;public class BaseballGameManager {
+package baseball;
+
+public class BaseballGameManager {
+    private int answer;
+
+    public void playGame() {
+        this.answer = RandomAnswerGenerator.getRandomAnswer();
+
+        while(true) {
+            int inputNum = IOManager.inputNumber();
+
+            // compare & print hint
+
+            if(inputNum == answer) {
+                IOManager.correctAnswer();
+                break;
+            }
+        }
+    }
 }
