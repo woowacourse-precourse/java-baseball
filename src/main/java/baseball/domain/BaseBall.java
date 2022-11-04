3fffba83c6ac4272951fab4083bace7b10df0b0c
woo-chang <clllickme@naver.com>
2022-11-04 17:43:16 +0900

    feat: 플레이어 입력을 게임 숫자 리스트로 저장하는 기능 구현

--- src/main/java/baseball/domain/BaseBall.java
+++ src/main/java/baseball/domain/BaseBall.java
@@ -1,20 +1,39 @@
 package baseball.domain;
 
-import baseball.util.GameValidation;
+import baseball.util.GameValidator;
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BaseBall implements Game {
 
     private final List<Integer> computerNumbers;
+    private final List<Integer> playerNumbers = new ArrayList<>(GameValidator.GAME_NUMBER_LENGTH);
 
     public BaseBall() {
-        computerNumbers = Randoms.pickUniqueNumbersInRange((int) GameValidation.START_RANGE,
-            (int) GameValidation.END_RANGE, GameValidation.GAME_NUMBER_LENGTH);
+        computerNumbers = Randoms.pickUniqueNumbersInRange((int) GameValidator.START_RANGE,
+            (int) GameValidator.END_RANGE, GameValidator.GAME_NUMBER_LENGTH);
     }
 
     @Override
     public void play() {
+        playerInput();
+    }
 
+    private void playerInput() {
+        String playerInput = Console.readLine();
+        GameValidator.validate(playerInput);
+        storePlayerNumbers(playerInput);
+    }
+
+    private void storePlayerNumbers(String playerInput) {
+        playerNumbers.clear();
+        playerNumbers.addAll(
+            playerInput.chars()
+                .boxed()
+                .collect(Collectors.toList())
+        );
     }
 }
