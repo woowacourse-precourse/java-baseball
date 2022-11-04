a65762baec6772d1d118f1f81c664ce803a3436c
sangwon <swchoi1997@naver.com>
2022-11-04 09:20:14 +0900

    feat: GameController 생성 - 입력 1회 실행
    사용자의 입력 1회 실행 가능한 GameController 생성, 사용자 입력이 있는 DTO를 받아 적절한 서비스를 실행시기고 결과를 ResultDTO에 담아 리턴해줌

---
+++ src/main/java/baseball/domain/controller/GameController.java
@@ -0,0 +1,24 @@
+package baseball.domain.controller;
+
+import baseball.domain.dto.ResultDto;
+import baseball.domain.dto.UserInputDto;
+import baseball.domain.model.Computer;
+import baseball.domain.model.Score;
+import baseball.domain.model.User;
+import java.util.List;
+
+public class GameController {
+
+    private final Computer computer;
+
+    public GameController(List<String> computerBalls) {
+        this.computer = new Computer(computerBalls);
+    }
+
+    public ResultDto doGame(UserInputDto userInputDto) {
+        User user = new User(userInputDto.getUserInput());
+        Score score = new Score(this.computer, user);
+
+        return new ResultDto(score);
+    }
+}
