dc124c777b2863460b8691a7af4dd34cd6256b76
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-04 15:48:37 +0900

    Create Input_Output.java
    type : Feat : "입출력 함수 추가"
    
    List로 입출력 받은 함수 추가하였습니다. Input_Output.java을 만들어 코딩했습니다.

---
+++ src/main/java/baseball/Input_Output.java
@@ -0,0 +1,34 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Input_Output {
+    List<Integer> computer = new ArrayList<>();
+    List<Integer> Scan = new ArrayList<>();
+
+    public void Computer_RandomNumber() {//컴퓨터 랜덤 숫자 생성
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber))//List에 숫자가 없으면 추가
+                computer.add(randomNumber);
+        }
+    }
+
+    public void User_enter() {//유저 숫자 입력
+        String Number = Console.readLine();
+        int a = Integer.parseInt(Number);
+
+        for (int i = 1; i < Number.length() + 1; i++) {//랜덤숫자가 저장된 리스트형태로 변환
+            Scan.add(a % 10);
+            a /= 10;
+        }
+
+        Collections.reverse(Scan);
+    }
+}
