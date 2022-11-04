414b9418c50e66bf19d25dcc4257da03ead83528
정현수 <wjdgustn15@naver.com>
2022-11-04 17:31:28 +0900

    feat : implements RandomGenerator

--- src/main/java/baseball/RandomGenerator.java
+++ src/main/java/baseball/RandomGenerator.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class RandomGenerator {
     public static int getRandomNumber(int length){
-     return -1;
+        Set<Integer> computer = new HashSet<>();
+        while (computer.size() < length) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        int number = 0;
+        for (Integer value : computer) {
+            number*=10;
+            number+=value;
+        }
+        return number;
     }
 }
