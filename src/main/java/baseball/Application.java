8193fdb2f2d0c5254c96c125e4ed5b9d0af2c635
unknown <angelbaek>
2022-11-04 11:19:21 +0900

    변수이름 재설정과 로직 순서 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -14,47 +13,15 @@ public class Application {
 	public static void main(String[] args) {
 		gameStart(getComputerNumbers());
 	}
-
-	public static List<Integer> getUserNumbers() {
-		System.out.print("숫자를 입력해주세요 : ");
-		String UserAnotherNumbers = Console.readLine();
-		List<Integer> userNumbersList = exceptionProcessing(UserAnotherNumbers);
-		return userNumbersList;
-	}
-
-	public static List<Integer> exceptionProcessing(String userNumbers) {
-		List<Integer> userNumbersList = new ArrayList<>();
-		int intUserNumbers = 0;
-		try {
-			Pattern userNumberPattern = Pattern.compile("^[1-9]*$");
-			Matcher matchUserNumber = userNumberPattern.matcher(userNumbers);
-			boolean isUserNumberChecked = matchUserNumber.find();
-			if (!isUserNumberChecked)
-				throw new IllegalArgumentException();
-
-			if (userNumbers.length() != 3) {
-				throw new IllegalArgumentException();
-			}
-			char[] charUserNumbers = new char[userNumbers.length()];
-			for (int userNums = 0; userNums < userNumbers.length(); userNums++) {
-				charUserNumbers[userNums] = userNumbers.charAt(userNums);
-			}
-			for (int charNumbers = 0; charNumbers < charUserNumbers.length; charNumbers++) {
-				if (charNumbers != userNumbers.indexOf(userNumbers.charAt(charNumbers)))
-					throw new IllegalArgumentException();
-			}
-			intUserNumbers = Integer.parseInt(userNumbers);
-			for (int number = 0; number < userNumbers.length(); number++) {
-				char[] userNumberChar = userNumbers.toCharArray();
-				userNumbersList.add((int) userNumberChar[number] - 48);
-			}
-		} catch (IllegalArgumentException illegal) {
-			System.err.println(illegal);
-			System.exit(0);
+	public static void gameStart(List<Integer> comNumber) {
+		System.out.println("숫자 야구 게임을 시작합니다.");
+		boolean isGameContinued = true;
+		Map<String, Integer> userScore = new HashMap<>();
+		while (isGameContinued) {
+			userScore = gameScorePut(comNumber);
+			scoreProcess(userScore);
 		}
-		return userNumbersList;
 	}
-
 	public static List<Integer> getComputerNumbers() {
 		List<Integer> computerNumbers = new ArrayList<>();
 		while (computerNumbers.size() < 3) {
@@ -66,7 +33,44 @@ public class Application {
 		System.out.println(computerNumbers);
 		return computerNumbers;
 	}
-
+	public static List<Integer> getUserNumbers() {
+		System.out.print("숫자를 입력해주세요 : ");
+		String userAnotherNumbers = Console.readLine();
+		List<Integer> userNumbersList = userNumberException(userAnotherNumbers);
+		return userNumbersList;
+	}
+	public static List<Integer> userNumberException(String userNumbers) {
+		List<Integer> userNumbersList = new ArrayList<>();
+		int intUserNumbers = 0;		
+		try {
+			Pattern userNumberPattern = Pattern.compile("^[1-9]*$");
+			Matcher matchUserNumber = userNumberPattern.matcher(userNumbers);
+			boolean isUserNumberMatch = matchUserNumber.find();
+			if (!isUserNumberMatch) {
+				throw new IllegalArgumentException();
+			}
+			if (userNumbers.length() != 3) {
+				throw new IllegalArgumentException();
+			}
+			char[] charUserNumbers = new char[userNumbers.length()];
+			for (int userNums = 0; userNums < userNumbers.length(); userNums++) {
+				charUserNumbers[userNums] = userNumbers.charAt(userNums);
+			}
+			for (int charNumbers = 0; charNumbers < charUserNumbers.length; charNumbers++) {
+				if (charNumbers != userNumbers.indexOf(userNumbers.charAt(charNumbers))) 
+					throw new IllegalArgumentException();					
+			}
+			intUserNumbers = Integer.parseInt(userNumbers);
+			for (int number = 0; number < userNumbers.length(); number++) {
+				char[] userNumberChar = userNumbers.toCharArray();
+				userNumbersList.add((int) userNumberChar[number] - 48);
+			}
+		} catch (IllegalArgumentException illegal) {
+			System.err.println(illegal);
+			System.exit(0);
+		}	
+		return userNumbersList;
+	}	
 	public static Map<String, Integer> gameScorePut(List<Integer> comNumber) {
 		Map<String, Integer> userScore = new HashMap<>();
 		List<Integer> userNumber = getUserNumbers();
@@ -79,7 +83,6 @@ public class Application {
 		}
 		return userScore;
 	}
-
 	public static void scoreProcess(Map<String, Integer> userScore) {
 		Integer strike = userScore.get("스트라이크");
 		Integer ball = userScore.get("볼");
@@ -89,35 +92,27 @@ public class Application {
 			System.out.println(ball + "볼 " + strike + "스트라이크");
 		} else if (strike != null && ball == null) {
 			System.out.println(strike + "스트라이크");
-			if(strike==3)gameContinued(strike);			
+			if(strike==3) gameContinued(strike);			
 		} else {
 			System.out.println(ball + "볼");
 		}
 	}
-
-	public static void gameContinued(Integer strike) {
-		try {
-			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-			String continued = camp.nextstep.edu.missionutils.Console.readLine();
+	public static void gameContinued(Integer strike) {		
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		String continued = Console.readLine();	
+		continuedExceptionProcess(continued);
+	}	
+	public static void continuedExceptionProcess(String continued) {
+		try {			
 			if (continued.equals("1"))
 				gameStart(getComputerNumbers());
 			else if (continued.equals("2"))
 				System.exit(0);		
 			else throw new IllegalArgumentException();
 		} catch (IllegalArgumentException illegal) {
-			// TODO: handle exception
 			System.err.println(illegal);
 			System.exit(0);
-		}		
-	}
-	public static void gameStart(List<Integer> comNumber) {
-		System.out.println("숫자 야구 게임을 시작합니다.");
-		boolean isGameContinued = true;
-		Map<String, Integer> userScore = new HashMap<>();
-		while (isGameContinued) {
-			userScore = gameScorePut(comNumber);
-			scoreProcess(userScore);
-		}
-	}
+		}	
+	}		
 }
