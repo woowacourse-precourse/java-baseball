42a8382f728b8d26a92f32b13ca083ae079e99b2
JunDonghyuk <ehdgur4814@naver.com>
2022-11-05 02:57:50 +0900

    feat(printEndMessage): update
    Update new function
    - 숫자 야구 게임 종료 메시지 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,8 +7,14 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        List<Integer> randomNumber = getRandomNumber();
+
         printStartMessage();
+        boolean gameFlag = true;
+        while (gameFlag) {
+            List<Integer> randomNumber = getRandomNumber();
+            
+            printEndMessage();
+        }
 
 
     }
@@ -26,5 +32,10 @@ public class Application {
 
     public static void printStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printEndMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
