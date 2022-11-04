237f48c7ce4716cabf268b1b29ca2ca5ed4d2164
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-04 11:35:00 +0900

    feat(Pitcher): Pitcher 객체 생성
    Pitcher 객체 주요 기능:
    - 1~9 사이 서로 다른 세 자연수로 이루어진 랜덤 문자열 생성 클래스 메서드
    - PlayingNumber에 세 자연수 저장
    - getPlayingNumber로 세자리  자연수 반환
    - getPlayingNumbers로 1~9 사이 서로 다른 세 자연수로 이루어진 리스트 반환

--- src/main/java/baseball/Pitcher.java
+++ src/main/java/baseball/Pitcher.java
@@ -1,2 +1,43 @@
-package baseball;public class Pitcher {
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Randoms.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pitcher {
+	private static final int PLAYING_NUMBER_SIZE = 3;
+	private static final int MIN_NUMBER = 1;
+	private static final int MAX_NUMBER = 9;
+
+	private final PlayingNumber playingNumber;
+
+	public Pitcher() {
+		this.playingNumber = new PlayingNumber(make3DigitNumber());
+	}
+
+	public int getPlayingNumber() {
+		return playingNumber.getPlayingNumber();
+	}
+
+	public List<Integer> getPlayingNumbers() {
+		return playingNumber.getPlayingNumbers();
+	}
+
+	private static String make3DigitNumber() {
+		StringBuilder stringBuilder = new StringBuilder();
+		List<Integer> prev = new ArrayList<>();
+
+		while (stringBuilder.length() != PLAYING_NUMBER_SIZE) {
+			int newNumber = pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+
+			if (!prev.contains(newNumber)) {
+				stringBuilder.append(newNumber);
+				prev.add(newNumber);
+			}
+		}
+
+		return stringBuilder.toString();
+	}
+
 }
