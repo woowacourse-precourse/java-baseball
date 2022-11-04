60833d230afc3129a8f2b9baedf1bff881a6e0e0
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-04 22:25:37 +0900

    feat($Computer): count strikes and balls
    - Compare each digit of user input and computer generated number
    - Maintain strikeCnt, ballCnt variables

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -9,6 +9,9 @@ import java.util.List;
 public class Computer {
 
     private static final int LEN_NUM = 3;
+    private int strikeCnt = 0;
+    private int ballCnt = 0;
+    private boolean isNothing = false;
 
     private List<Integer> numbers;
 
@@ -26,7 +29,22 @@ public class Computer {
         }
     }
 
+    // 같은 수가 같은 자리에 있으면 "스트라이크"
+    // 같은 수가 다른 자리에 있으면 "볼"
+    public void count(List<Integer> userNumber) {
+        for (int i = 0; i < userNumber.size(); i++) {
+            Integer digit = userNumber.get(i);
+            Integer target = numbers.get(i);
 
+            if (digit == target) {
+                this.strikeCnt += 1;
+                continue;
+            }
 
+            if (numbers.contains(digit)) {
+                this.ballCnt += 1;
+            }
+        }
+    }
 
 }
