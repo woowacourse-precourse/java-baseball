2e46081377e5290fb1453c15a13b404391d9be74
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-04 16:04:32 +0900

    feat(src/main): start game
    startGame(): 게임  시작할 때 "숫자 야구 게임을 시작합니다" 프린트.
    
    해결: 1. 게임 시작- 게임 시작 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,8 +15,14 @@ public class Application {
         return randomNums;
     }
 
+    private static void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public static void main(String[] args) {
-        List<Integer> answer=generateRandom3Numbers();
+        List<Integer> answer = generateRandom3Numbers();
         System.out.println(answer);
+        startGame();
+
     }
 }
