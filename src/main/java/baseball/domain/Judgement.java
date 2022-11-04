7af05ed952c55d3e34dfb3075d91e19cc7d75865
anneu <b.imnuj@gmail.com>
2022-11-04 15:34:27 +0900

    feat: 플레이어 수와 컴퓨터 수를 비교해 동일한 위치에 있는 개수를 구하는 기능 구현

--- src/main/java/baseball/domain/Judgement.java
+++ src/main/java/baseball/domain/Judgement.java
@@ -17,4 +17,19 @@ public class Judgement {
 
         return count;
     }
+
+    /**
+     * 플레이어 수와 컴퓨터 수를 비교해 동일한 위치에 있는 개수를 반환한다.
+     */
+    public int countSamePosition(List<Integer> computerNumbers, List<Integer> playerNumbers) {
+        int count = 0;
+
+        for (int position = 0; position < computerNumbers.size(); position++) {
+            if (computerNumbers.get(position).equals(playerNumbers.get(position))) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
