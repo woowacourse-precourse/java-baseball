96bad862eee67e67535c557ccf46ab25b0ccd534
Brian <bwook9908@gmail.com>
2022-11-04 11:53:35 +0900

    feat: 시작 문구 출력
    - 숫자 야구 게임을 시작합니다 문구를 출력하는 함수 구현
    - 기능목록을 보면서 개선할 점들을 개선함
    - 해당 기능은 기능목록에서 A.1 에 해당함
    
    게임 시작 문구 출력 함수 구현, 기능목록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+
+import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        printStartJavaBaseball();
+
     }
+
+    public static void printStartJavaBaseball(){
+        System.out.println("숫자 야구 게임을 시작합니다");
+    }
+
 }
