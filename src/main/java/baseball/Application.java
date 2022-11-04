235556d9dbc19f6fa53d156e7d3d0f6805480aa0
Hi-Imjaeyoung <imjy2757@naver.com>
2022-11-04 14:47:40 +0900

    refactor(Application): Computer 숫자저장 구조를 List에서 String으로 변경.
    Method의 parameter도 String 형으로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,30 @@
 package baseball;
 
-import java.util.LinkedList;
-import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-	private static List<Integer> select_Random_Number(){
-		List<Integer> com_Random_Num = new LinkedList<>();
-		while(com_Random_Num.size()<3){
-			int now_num = pickNumberInRange(1,9);
+	public static String select_Random_Number(){
+		String com_Random_Num = "";
+		while(com_Random_Num.length()<3){
+			String now_num = String.valueOf(pickNumberInRange(1,9));
 			if(!com_Random_Num.contains(now_num)){
-				com_Random_Num.add(now_num);
+				com_Random_Num+=now_num;
 			}
 		}
 		return com_Random_Num;
 	}
-    private static void start_new_Baseball_Game() {
+	public static String compare_two_case(String com, String user){
+		return "tmp";
+	}
+    public static void start_new_Baseball_Game() {
     	System.out.println("숫자 야구 게임을 시작합니다.");
-    	List<Integer> computer_num = select_Random_Number();
+    	String computer_num = select_Random_Number();
+		System.out.print("숫자를 입력해주세요 : ");
+		String input_num = Console.readLine();
+		//input에 따른 예외 처리 필요.
+		System.out.println(compare_two_case(computer_num,input_num));
     }
 	public static void main(String[] args) {
         start_new_Baseball_Game();
