9384fd8b9e60a5dd8bbae3cf7bbf847c556bec2e
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-04 15:44:34 +0900

    refactor(/Hint) :: 볼 비교하는 경우의 수 고려해보기

--- src/main/java/features/Hint.java
+++ src/main/java/features/Hint.java
@@ -1,9 +1,9 @@
 package features;
 
+import static extract.Key.getKey;
 import static features.Input.input;
 import static features.Player.createComputerNumber;
 
-import java.util.List;
 import java.util.Map;
 
 public class Hint {
@@ -26,26 +26,39 @@ public class Hint {
     // 힌트 기능 동작
     public void runHint(Map<Integer, Character> playerNumber) {
 
-        for (int i = 0; i < playerNumber.size(); i++) {
+        Character userValue0 = userNumber.get(0);
+        Character userValue1 = userNumber.get(1);
+        Character userValue2 = userNumber.get(2);
 
-            boolean userIndex = userNumber.containsValue(userNumber.get(i));
-            boolean computeIndex = computerNumber.containsValue(computerNumber.get(i));
+        Character computerValue0 = computerNumber.get(0);
+        Character computerValue1 = computerNumber.get(1);
+        Character computerValue2 = computerNumber.get(2);
 
-            // 스트라이크
-            if (userNumber.get(i) == computerNumber.get(i)) {
+        Integer userKey0 = getKey(userNumber, userValue0);
+        Integer userKey1 = getKey(userNumber, userValue1);
+        Integer userKey2 = getKey(userNumber, userValue2);
+
+        Integer computerKey0 = getKey(computerNumber, computerValue0);
+        Integer computerKey1 = getKey(computerNumber, computerValue1);
+        Integer computerKe2 = getKey(computerNumber, computerValue2);
+
+        for (key = 0; key < playerNumber.size(); key++){
+            // 스트라이크 (같은 수 == 같은 수 && 같은 자리 == 같은 자리)
+            if (userValue == computerValue && userKey == computerKey) {
                 strike++;
             }
 
             // 볼 (같은 수 == 같은 수 && 같은 자리 != 같은 자리)
-            for (int index = 0; index < 3; index++) {
-                if (userNumber.get(i) == computerNumber.get(i) || userIndex != computeIndex) {
-                    ball++;
-                }
+            if (userValue == computerValue && userKey != computerKey) {
+                ball++;
             }
 
-            // 낫싱을 세어야 하나?
-
+            // 낫싱 (같은 수 != 같은 수 && 같은 자리 != 같은 자리)
+            if (userValue != computerValue && userKey != computerKey) {
+                nothing++;
+            }
         }
+
     }
 
     public int getStrike() {
