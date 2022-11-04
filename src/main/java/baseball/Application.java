626d16abffe835d8f7e5104c275203362f7d2e4f
asas6978 <asas6978@naver.com>
2022-11-04 12:41:53 +0900

    feat: 사용자로부터 숫자 입력 받는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,9 +4,16 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         List<String> computerNumber = getComputerNumber();
+
+        List<String> userInput = new ArrayList<>(Arrays.asList(readLine().split("")));
+        if (!isValidLength(userInput) || !isExistOnlyNumber(userInput)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private static ArrayList<String> getComputerNumber() {
@@ -21,5 +28,18 @@ public class Application {
         }
 
         return computerNumber;
+    }
+
+    private static boolean isValidLength(List<String> userInput) {
+        return userInput.size() == 3;
+    }
+
+    private static boolean isExistOnlyNumber(List<String> userInput) {
+        boolean isOnlyNumber = false;
+
+        for (String element : userInput) {
+            isOnlyNumber = isOnlyNumber || Character.isDigit(element.charAt(0));
+        }
+        return isOnlyNumber;
     }
 }
