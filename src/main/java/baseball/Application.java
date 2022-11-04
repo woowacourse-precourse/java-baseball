62993bf72b9afb2d280930063f8d1fdaa69db908
ashlovesliitea <skyrider1811@gmail.com>
2022-11-04 17:43:32 +0900

    feat: 컴퓨터가 랜덤으로 수 세 개를 생성하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
+import camp.nextstep.edu.missionutils.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다");
+
+        while(true){
+            List<Integer> computerNum=generateComputerNum();
+
+
+        }
     }
+
+    public static List<Integer> generateComputerNum(){
+        List<Integer> computerNum=new ArrayList<>();
+        while(computerNum.size()<3){
+            int random=Randoms.pickNumberInRange(1,9);
+            computerNum.add(random);
+        }
+        return computerNum;
+    }
+
+
+
 }
