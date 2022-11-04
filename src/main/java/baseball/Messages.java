3dcd4c927aea6bfb5eee08e3acf8c2c6bffddc21
TaemHam <tk4003950@gmail.com>
2022-11-04 13:35:59 +0900

    Refactor: Messages 다른 형태의 출력 구분을 위한 printer 추가

--- src/main/java/baseball/Messages.java
+++ src/main/java/baseball/Messages.java
@@ -1,19 +1,34 @@
 package baseball;
 
+import java.util.StringJoiner;
+import java.util.function.Consumer;
+
+class Printer{
+    public static final Consumer<String> PRINTLN = System.out::println;
+    public static final Consumer<String> PRINT = System.out::print;
+}
+
 public enum Messages {
 
-    INIT("숫자 야구 게임을 시작합니다."),
-    INPUT("숫자를 입력해주세요 : "),
-    END("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    ASK(Printer.PRINTLN,
+            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+    END(Printer.PRINTLN,
+            "3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+    INIT(Printer.PRINTLN,
+            "숫자 야구 게임을 시작합니다."),
+    INPUT(Printer.PRINT,
+            "숫자를 입력해주세요 : ");
 
+    private final Consumer<String> printer;
     private final String message;
 
-    Messages(String message) {
+    Messages(Consumer<String> printer, String message) {
+        this.printer = printer;
         this.message = message;
     }
 
     public void printMessage() {
-        System.out.println(this.message);
+        this.printer.accept(this.message);
     }
 
     public static void printScore(int balls, int strikes) {
