17bd72f03c6659367a99af9a9bf238e731764a23
TaemHam <tk4003950@gmail.com>
2022-11-04 10:50:53 +0900

    Fix: BaseballGame 리스트 원소 자료형 변경

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,11 +1,12 @@
 package baseball;
 
 import abstracts.Game;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGame extends Game {
 
-    private List[] answerNumbers;
+    private List<Integer> answerNumbers;
 
     BaseballGame() {
         this.gameName = "Baseball";
@@ -13,17 +14,18 @@ public class BaseballGame extends Game {
         this.initialize();
     }
 
-    private List[] getAnswerNumbers() {
+    private List<Integer> getAnswerNumbers() {
         return this.answerNumbers;
     }
 
-    private void setAnswerNumbers(List[] answerNumbers) {
+    private void setAnswerNumbers(List<Integer> answerNumbers) {
         this.answerNumbers = answerNumbers;
     }
 
     @Override
     protected void initialize() {
     }
+
 
     @Override
     protected void terminate() {
