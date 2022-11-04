6f444e5dd8ff2bf010f7539aafcc2706a628707f
southoftheriver <anwjsrlrhwkd@naver.com>
2022-11-04 17:52:54 +0900

    feat: generate list of random int
    rules
    - int is not overlapping
    - size of list is 3
    - range of int is 1 to 9

---
+++ src/main/java/baseball/RandomNumberProvider.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class RandomNumberProvider {
+
+    private static final int START = 1;
+    private static final int END = 9;
+    private static final int MAX_SIZE_OF_LIST = 3;
+
+
+    private static Integer generateRandomInt() {
+        return Randoms.pickNumberInRange(START, END);
+    }
+
+    public static List<Integer> generateRandomIntList() {
+        Set<Integer> RandomInts = new HashSet<>();
+
+        while (RandomInts.size() < MAX_SIZE_OF_LIST) {
+            RandomInts.add(generateRandomInt());
+        }
+
+        return RandomInts.stream().collect(Collectors.toList());
+    }
+}
