acd985689172b0d3275bc3af2f97ec8649e3373c
woo-chang <clllickme@naver.com>
2022-11-04 16:55:03 +0900

    feat: 숫자 야구 게임 생성 시 컴퓨터가 가지는 1~9 범위의 서로 다른 3개의 수를 선택

--- src/main/java/baseball/domain/BaseBall.java
+++ src/main/java/baseball/domain/BaseBall.java
@@ -1,6 +1,17 @@
 package baseball.domain;
 
+import baseball.util.GameValidation;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class BaseBall implements Game {
+
+    private final List<Integer> computerNumbers;
+
+    public BaseBall() {
+        computerNumbers = Randoms.pickUniqueNumbersInRange((int) GameValidation.START_RANGE,
+            (int) GameValidation.END_RANGE, GameValidation.GAME_NUMBER_LENGTH);
+    }
 
     @Override
     public void play() {
