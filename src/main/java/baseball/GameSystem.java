ab6544d0fc950761613485dfbd3c74210d81cd57
swandevson <shai4956@gmail.com>
2022-11-04 22:21:54 +0900

    Feat: 게임 1회 실행 구현
    - 생성값과 입력값에 대한 예외 처리
    - checkWin 메소드를 통한 승리 확인
    - calculateScore 메소드를 통해 볼과 스트라이크 개수를 계산

--- src/main/java/baseball/GameSystem.java
+++ src/main/java/baseball/GameSystem.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 
 
 public class GameSystem {
@@ -18,4 +20,51 @@ public class GameSystem {
         this.scoreBoard = new ScoreBoard();
     }
 
+    public void calculateScore() {
+        String playerNumber = player.getNumber();
+        String computerNumber = computer.getNumber();
+
+        for(char playerNumberChar : playerNumber.toCharArray()) {
+            if (playerNumber.indexOf(playerNumberChar) == computerNumber.indexOf(playerNumberChar)) {
+                scoreBoard.upStrikeCount();
+            } else if(computerNumber.indexOf(playerNumberChar) != -1) {
+                scoreBoard.upBallCount();
+            }
+        }
+    }
+
+
+    public boolean checkWinGame() {
+        if (scoreBoard.getStrikeCount() == WIN_STRIKE_COUNT) {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public void run() {
+        this.initGame();
+
+        try {
+            computer.generateNumber();
+        } catch (IllegalArgumentException Exception) {
+            throw Exception;
+        }
+
+        do {
+            String inputNumber = readLine();
+
+            try {
+                player.setNumber(inputNumber);
+            } catch (IllegalArgumentException Exception) {
+                throw Exception;
+            }
+
+            this.scoreBoard.initScore();
+            this.calculateScore();
+            GameMessagePrinter.printScore(this.scoreBoard);
+        } while (!this.checkWinGame());
+
+    }
 }
