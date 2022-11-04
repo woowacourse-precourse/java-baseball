7bad195724375b8a6e8f32e6b8368733be048db5
Choi-JJunho <junho5336@gmail.com>
2022-11-04 20:13:27 +0900

    feat(Validation): add validateNumberLength method

--- src/main/java/baseball/ValidationHandler.java
+++ src/main/java/baseball/ValidationHandler.java
@@ -2,10 +2,18 @@ package baseball;
 
 public class ValidationHandler {
 
+    private final int NUMBER_LENGTH = 3;
     private final String inputString;
 
     ValidationHandler(String inputString) {
         this.inputString = inputString;
     }
 
+    public boolean validateNumberLength() {
+        if (inputString.length() == NUMBER_LENGTH) {
+            return true;
+        }
+
+        return false;
+    }
 }
