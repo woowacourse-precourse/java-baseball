5df74060bebf35f66de6b29262bb886ab668f4a4
Kim woo jong <skekdnwhd@gmail.com>
2022-11-05 04:17:42 +0900

    Add, Feat : Result 추가, game score를 가지고 결과를 분석 및 콘솔에 결과를 띄우는 기능 추가

---
+++ src/main/java/baseball/service/Result.java
@@ -0,0 +1,45 @@
+package baseball.service;
+
+import baseball.utils.ConsoleLog;
+
+import java.util.Map;
+
+public class Result {
+    private static final boolean CORRECT_ANSWER = true;
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
+    private static final String WHITE_SPACE = " ";
+    private Result() {
+    }
+
+    private static class LazyHolder {
+        private static final Result INSTANCE = new Result();
+    }
+
+    public static Result getInstance() {
+        return Result.LazyHolder.INSTANCE;
+    }
+
+    public boolean analysis(Map<String, Integer> score){
+        notify(score);
+        if (score.get("strike") == 3)
+            return CORRECT_ANSWER;
+        return !CORRECT_ANSWER;
+    }
+
+    private void notify(Map<String, Integer> score) {
+        int strike = score.get("strike");
+        int ball = score.get("ball");
+
+        String notification = "";
+        if (ball > 0) {
+            notification += ball+BALL+WHITE_SPACE;
+        } else if (strike > 0) {
+            notification += strike+STRIKE;
+        } else if (ball == 0 && strike == 0) {
+            notification = NOTHING;
+        }
+        ConsoleLog.getInstance().println(notification);
+    }
+}
