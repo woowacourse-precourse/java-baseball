63a017635f46c93556e2835c6180719c3b7f9445
ashlovesliitea <skyrider1811@gmail.com>
2022-11-04 18:34:18 +0900

    feat: 유저가 입력한 문자열을 리스트로 분리하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,30 +1,61 @@
 package baseball;
+
 import camp.nextstep.edu.missionutils.*;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다");
 
-        while(true){
-            List<Integer> computerNum=generateComputerNum();
-
+        while (true) {
+            List<Integer> computerNumList = generateComputerNum();
+            String userNum = inputUserNum();
+            List<Integer> userNumList = splitUserNumToList(userNum);
 
         }
     }
 
-    public static List<Integer> generateComputerNum(){
-        List<Integer> computerNum=new ArrayList<>();
-        while(computerNum.size()<3){
-            int random=Randoms.pickNumberInRange(1,9);
+    public static List<Integer> splitUserNumToList(String userNum) {
+        List<Integer> userNumList = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            userNumList.add(userNum.charAt(i) - '0');
+        }
+
+        return userNumList;
+    }
+
+    public static String inputUserNum() {
+        System.out.println("숫자를 입력해주세요:");
+        String userNumStr = readLine();
+
+        try {
+            Integer.parseInt(userNumStr);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+
+        if (userNumStr.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        return userNumStr;
+    }
+
+
+    public static List<Integer> generateComputerNum() {
+        List<Integer> computerNum = new ArrayList<>();
+        while (computerNum.size() < 3) {
+            int random = Randoms.pickNumberInRange(1, 9);
             computerNum.add(random);
         }
         return computerNum;
     }
-
 
 
 }
