732b448ca4202b9b67da4805e7c46081ede3069e
Laeng <Laeng@users.noreply.github.com>
2022-11-04 09:15:31 +0900

    test(Scoreboard): ScoreBoard 테스트 추가

---
+++ src/test/java/baseball/ScoreboardTest.java
@@ -0,0 +1,53 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ScoreboardTest {
+    @Test
+    void 리스트형으로_반환() {
+        Scoreboard scoreboard = new Scoreboard(2, 1);
+        List<Integer> list = scoreboard.toList();
+
+        assertThat(list).containsExactly(2, 1);
+    }
+
+    @Test
+    void 볼_점수_가져오기() {
+        Scoreboard scoreboard = new Scoreboard(2, 0);
+        int ballPoint = scoreboard.getBallPoint();
+
+        assertThat(ballPoint).isEqualTo(2);
+    }
+
+    @Test
+    void 스트라이크_점수_가져오기() {
+        Scoreboard scoreboard = new Scoreboard(0, 3);
+        int strikePoint = scoreboard.getBallPoint();
+
+        assertThat(strikePoint).isEqualTo(3);
+    }
+
+    @Test
+    void 볼_점수_더하기() {
+        Scoreboard scoreboard = new Scoreboard();
+        scoreboard.addBallPoint(2);
+
+        int ballPoint = scoreboard.getBallPoint();
+
+        assertThat(ballPoint).isEqualTo(2);
+    }
+
+    @Test
+    void 스트라이크_점수_더하기() {
+        Scoreboard scoreboard = new Scoreboard();
+        scoreboard.addStrikePoint(2);
+
+        int strikePoint = scoreboard.getStrikePoint();
+
+        assertThat(strikePoint).isEqualTo(2);
+    }
+}
