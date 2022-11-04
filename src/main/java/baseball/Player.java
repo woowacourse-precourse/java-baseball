9c47afc40392ebe04d2ab78ff2a706baa83a0567
KIMSEI1124 <workju1124@gmail.com>
2022-11-04 23:11:30 +0900

    feat: 문자를 정수형 리스트에 넣을때 예외 탐지 기능 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -24,7 +24,8 @@ public class Player {
 
     /**
      * 플레이어의 입력받은 수 저장
-     * @param inputNumbers  플레이어의 입력한 숫자들
+     *
+     * @param inputNumbers 플레이어의 입력한 숫자들
      */
     private void setPlayerNumber(String inputNumbers) {
         this.playerNumbers = toList(inputNumbers);
@@ -32,14 +33,29 @@ public class Player {
 
     /**
      * 문자열을 리스트로 변환한다.
-     * @param inputNumbers  플레이어의 입력한 숫자들
+     *
+     * @param inputNumbers 플레이어의 입력한 숫자들
      * @return 플레이어의 입력한 숫자들 리스트로 변환한 것을 반환
      */
     private List<Integer> toList(String inputNumbers) {
         List<Integer> list = new ArrayList<>(Collections.emptyList());
         for (String number : inputNumbers.split("")) {
-            list.add(Integer.parseInt(number));
+            inputNumberByList(list, number);
         }
         return list;
+    }
+
+    /**
+     * 문자를 리스트에 넣는다.<br>
+     * 만약 리스트에 넣지 못한다면 IllegalArgumentException를 던진다.
+     * @param list 리스트
+     * @param number    문자로 되어있는 숫자
+     */
+    private void inputNumberByList(List<Integer> list, String number) {
+        try {
+            list.add(Integer.parseInt(number));
+        } catch (IllegalArgumentException exception) {
+            exception.printStackTrace();
+        }
     }
 }
