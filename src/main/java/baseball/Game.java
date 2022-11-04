042462c988bc492509751f988bda95e1a55ee48b
정현수 <wjdgustn15@naver.com>
2022-11-04 17:32:24 +0900

    feat : implements Game

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,6 +1,54 @@
 package baseball;
 
+import baseball.score.Score;
+
 public class Game {
-    public void play(){
+    private boolean isPlay;
+    private final ScoreCalculator scoreCalculator;
+    private final Reader reader;
+    private final int numberLength;
+    public Game(ScoreCalculator scoreCalculator, Reader reader, int numberLength){
+        isPlay=true;
+        this.scoreCalculator = scoreCalculator;
+        this.reader = reader;
+        this.numberLength = numberLength;
     }
+    public void play(){
+        startPrint();
+        while(isPlay){
+            int actual = RandomGenerator.getRandomNumber(numberLength);
+            guessing(actual);
+            endIfWinMessage();
+            endIfWin();
+        }
+    }
+
+    private void endIfWin() {
+        int end = reader.readInt(1,2);
+        if(end==2)
+            isPlay=false;
+    }
+
+    private void endIfWinMessage() {
+        Printer.println(numberLength+"개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    private void guessing(int actual) {
+        int minValue = (int)Math.pow(10,numberLength-1);
+        int maxValue = (int)Math.pow(10,numberLength)-1;
+        while(true){
+            Printer.print("숫자를 입력해주세요 : ");
+            int expect=reader.readInt(minValue,maxValue);
+            Score score=scoreCalculator.calculateScore(expect, actual);
+            Printer.println(score);
+            if(score.isSame(numberLength,0))
+                return;
+        }
+    }
+
+    private void startPrint(){
+     Printer.println("숫자 야구 게임을 시작합니다.");
+    }
+
 }
