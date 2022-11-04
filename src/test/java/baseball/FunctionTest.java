d32785f1f29ecf5adbbcb573f9d6d9118d6012c0
jthugg <jthug@naver.com>
2022-11-05 05:05:03 +0900

    Feat: 기능 테스트 클래스 생성

---
+++ src/test/java/baseball/FunctionTest.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FunctionTest {
+}
