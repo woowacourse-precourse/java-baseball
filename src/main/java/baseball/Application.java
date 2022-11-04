3926da444da096cdd24a6e469155787124eec9ff
JDK <ehdrjf2062@gmail.com>
2022-11-04 10:22:47 +0900

    Destroy: 힌트 Enum 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -19,7 +19,9 @@ public class Application {
             ArrayList<Integer> inputList = stringToList(inputStr);
 
             // 로직
-            List<Hint> hints = ComputerList.compareTo(inputList);
+//            List<Hint> hints = ComputerList.compareTo(inputList);
+
+            // 출력
         }
     }
 
@@ -57,23 +59,6 @@ class ComputerList {
         list = Randoms.pickUniqueNumbersInRange(1, 9, 3);
     }
 
-    public static List<Hint> compareTo(List<Integer> input) {
-        List<Hint> hintArr = new ArrayList<>();
-        for (int i = 0; i < list.size(); i++) {
-            Hint hint = checkInputValue(i, input.get(i));
-            hintArr.add(hint);
-        }
-        return hintArr;
-    }
-
-    private static Hint checkInputValue(Integer index, Integer value) {
-        if (list.contains(value) && (list.indexOf(value) == index)) {
-            return Hint.STRIKE;
-        } else if (list.contains(value)) {
-            return Hint.BALL;
-        }
-        return Hint.MISS;
-    }
 }
 
 class Comment {
@@ -81,10 +66,4 @@ class Comment {
     final static String ENDGAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     final static String REGAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     final static String INPUTNUMBER = "숫자를 입력해주세요 : ";
-}
-
-enum Hint {
-    BALL,
-    STRIKE,
-    MISS
 }
