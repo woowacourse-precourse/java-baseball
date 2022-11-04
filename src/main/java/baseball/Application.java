2462a085c482c1f8803bca41e74d59e5e0f1831d
moonchaewon <codnjs8989@gmail.com>
2022-11-05 03:04:46 +0900

    feat(random): implement the function to generate random number by size

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,13 +2,30 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    private static final int NUMBER_SIZE = 3;
+
+    private static List<Integer> generateRandomNumberBySize() {
+        List<Integer> randomNumber = new ArrayList<>();
+        while (randomNumber.size() < Application.NUMBER_SIZE) {
+            int tempRandomNumber = generateRandomNumber();
+            if (!randomNumber.contains(tempRandomNumber)) {
+                randomNumber.add(tempRandomNumber);
+            }
+        }
+        return randomNumber;
+    }
 
     private static int generateRandomNumber() {
         return Randoms.pickNumberInRange(1, 9);
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println(generateRandomNumber());
+        List<Integer> randomNumber = generateRandomNumberBySize();
+        System.out.println(randomNumber);
     }
 }
