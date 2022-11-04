ee0d4b80d203dfaa92c13df0f93bfcacf5a0af64
KangHyeon3315 <son2786w@gmail.com>
2022-11-04 11:31:48 +0900

    feat: 랜덤한 3개의 숫자를 생성하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    /**
+     * 게임을 시작 할 때 생성하는 랜덤 넘버 3개 (이때 각 숫자는 중복이 없다)
+     *
+     * @return 랜덤으로 생성된 숫자를 가지는 List
+     */
+    public static List<Integer> generateRandomNumber() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while(randomNumbers.size() < 3) {
+            int randomValue = Randoms.pickNumberInRange(1, 9);
+
+            if(randomNumbers.contains(randomValue)) continue;
+
+            randomNumbers.add(randomValue);
+        }
+
+        return randomNumbers;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
