9b1b96fba57629d7f5c95537dfa56f093c065b23
안광민 <creativeduck0518@gmail.com>
2022-11-04 15:41:54 +0900

    feat(User): 0. 사용자 숫자 관리 클래스 생성

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private final List<Integer> numbers;
+
+    public User() {
+        numbers = new ArrayList<>();
+    }
+}
