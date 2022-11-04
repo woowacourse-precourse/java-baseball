7b55045f94e2499808aabff7d79bb4c729b2c496
bjk1649 <qhwns1649@naver.com>
2022-11-04 20:04:45 +0900

    feat: 입력으로 들어온 숫자가 주어진 조건에 맞는지 검사하는 메소드 추가

--- src/main/java/baseball/InputController.java
+++ src/main/java/baseball/InputController.java
@@ -1,11 +1,30 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
+import static baseball.NumberController.isStringNumberInRange;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class InputController {
     private InputController() {
     }
 
+    private static boolean isUniqueChars(String str) {
+        List<Character> strings = new ArrayList<>();
+        for (char c : str.toCharArray()) {
+            if (strings.contains(c)) {
+                return false;
+            }
+            strings.add(c);
+        }
+        return true;
+    }
+
     public static void checkThreeNum(String input) {
+        if (!isStringNumberInRange(input, 1, 9)
+                || input.length() != 3
+                || !isUniqueChars(input)) {
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
     }
 }
