37f836febbc0a1e8813037f3dda0f0cc65b15223
TaeHwan <kimta2hwan@gmail.com>
2022-11-04 23:45:41 +0900

    feat(user): 사용자가 추측한 답을 저장하는 기능 구현

---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,10 @@
+package baseball.domain;
+
+public class User {
+
+    private String guessedAnswer;
+
+    public void setGuessedAnswer(String guessedAnswer) {
+        this.guessedAnswer = guessedAnswer;
+    }
+}
