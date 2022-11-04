5538d6195cc4ceacd1156a3418f5753316ab4eda
Limjongho <whdgh9873@icloud.com>
2022-11-04 19:51:59 +0900

    feat: 상수 클래스 정의 및 상수 멤버 추가

---
+++ src/main/java/baseball/Constant.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class Constant {
+
+    public static final int MAX_RANGE = 9;
+    public static final int MIN_RANGE = 1;
+
+    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String ASK_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String RE_GAME_ASK_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+
+}
