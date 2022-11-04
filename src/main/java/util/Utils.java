7b220f89c7cfd0388d69b6e0207af7cd2c7316e4
MyunghyunNero <mh1kim0910@gmail.com>
2022-11-05 02:28:34 +0900

    feat : [숫자 야구 게임] 자주 사용할 문자열 정리(utils)

---
+++ src/main/java/util/Utils.java
@@ -0,0 +1,13 @@
+package util;
+
+public class Utils {
+    public static int STARTNUM=1;
+    public static int ENDNUM=9;
+    public static int Numlen=3;
+    public static String RESTARTQUESTION="게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static String ALLSTRIKE="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static String INPUTNUMBER="숫자를 입력해주세요 : ";
+    public static String NOBALLANDNOSTRIKE="낫싱";
+    public static String BALL="볼 ";
+    public static String STRIKE="스트라이크";
+}
