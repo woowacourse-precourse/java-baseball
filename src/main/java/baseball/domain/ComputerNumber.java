73117dba1414fdb314895ef5f04930a727daf493
yeonhee <ydol2@naver.com>
2022-11-04 16:25:12 +0900

    feat : 컴퓨터 수 랜덤 3가지를 생성하는 함수 구현 createComputerNumber

---
+++ src/main/java/baseball/domain/ComputerNumber.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerNumber {
+
+    /**
+     * 컴퓨터 수 생성
+     */
+    public List<Integer> createComputerNumber() {
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return computerNumbers;
+    }
+}
