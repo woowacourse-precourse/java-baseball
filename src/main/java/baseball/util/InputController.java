73408cea8dd4f002bde69219919ec45b030ce6fb
hoonn9 <hun9935@gmail.com>
2022-11-05 00:25:28 +0900

    feat(InputController): N자리 숫자, 1자리 숫자 입력 기능 추가

---
+++ src/main/java/baseball/util/InputController.java
@@ -0,0 +1,43 @@
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputController {
+
+    private String getLine() {
+        return Console.readLine();
+    }
+
+    private Integer getNumberLine(Integer length) {
+        String line = getLine();
+
+        if (line.length() > length) {
+            throw new IllegalArgumentException();
+        }
+
+        return parseInt(line);
+    }
+
+    public List<Integer> getEachNumber(Integer length) {
+        return convertToEachNumberList(getNumberLine(length));
+    }
+
+    public Integer getOneNumber() {
+        return getNumberLine(1);
+    }
+
+    private List<Integer> convertToEachNumberList(Integer number) {
+        return Arrays.stream(number.toString().split("")).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private Integer parseInt(String input) throws IllegalArgumentException {
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
