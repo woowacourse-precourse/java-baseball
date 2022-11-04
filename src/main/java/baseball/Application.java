5644846cf9d464d7a4b454d806b07b01c3042e91
moonchaewon <codnjs8989@gmail.com>
2022-11-05 03:15:00 +0900

    feat(input): implement the function to input number by player

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -23,9 +24,20 @@ public class Application {
     private static int generateRandomNumber() {
         return Randoms.pickNumberInRange(1, 9);
     }
+
+    private static List<Integer> inputPlayerNumber() {
+        List<Integer> playerNumber = new ArrayList<>();
+        String playerInputs = Console.readLine();
+        for (String playerInput : playerInputs.split(""))
+            playerNumber.add(Integer.parseInt(playerInput));
+        return playerNumber;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         List<Integer> randomNumber = generateRandomNumberBySize();
         System.out.println(randomNumber);
+        List<Integer> playerNumber = inputPlayerNumber();
+        System.out.println(playerNumber);
     }
 }
