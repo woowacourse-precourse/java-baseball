383c9eb93a7e123ab42e0d1f4d22357953c0a73d
jmj005 <jmj00005@naver.com>
2022-11-04 14:20:15 +0900

    test(ApplicationTest): 두 자릿수 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -51,6 +51,14 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 글자수_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
     void 중복_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("111"))
