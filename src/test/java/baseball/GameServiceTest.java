3fa4e7774a27882cdb283ebe6071f900cf1f8c6c
CMHyuk <alsgur9710@naver.com>
2022-11-04 13:44:01 +0900

    test: 스트라이크, 볼 출력 테스트

--- src/test/java/baseball/GameServiceTest.java
+++ src/test/java/baseball/GameServiceTest.java
@@ -36,4 +36,115 @@ class GameServiceTest {
         assertThat(checkStrike).isSameAs(2);
         assertThat(checkBall).isSameAs(1);
     }
+
+    @Test
+    void 낫싱_테스트() {
+        int strike = 0;
+        int ball = 0;
+        Result result = new Result(strike, ball);
+        gameService.printResult(result);
+        String expectation = "낫싱\n";
+        assertThat(output.toString()).isEqualTo(expectation);
+    }
+
+    @Test
+    void 원스트라이크_테스트() {
+        int strike = 1;
+        int ball = 0;
+        Result result = new Result(strike, ball);
+        gameService.printResult(result);
+        String expectation = "1스트라이크\n";
+        assertThat(output.toString()).isEqualTo(expectation);
+    }
+
+    @Test
+    void 투스트라이크_테스트() {
+        int strike = 2;
+        int ball = 0;
+        Result result = new Result(strike, ball);
+        gameService.printResult(result);
+        String expectation = "2스트라이크\n";
+        assertThat(output.toString()).isEqualTo(expectation);
+    }
+
+    @Test
+    void 쓰리스트라이크_테스트() {
+        int strike = 3;
+        int ball = 0;
+        Result result = new Result(strike, ball);
+        gameService.printResult(result);
+        String expectation = "3스트라이크\n" +
+                "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+        assertThat(output.toString()).isEqualTo(expectation);
+    }
+
+    @Test
+    void 원볼_테스트() {
+        int strike = 0;
+        int ball = 1;
+        Result result = new Result(strike, ball);
+        gameService.printResult(result);
+        String expectation = "1볼\n";
+        assertThat(output.toString()).isEqualTo(expectation);
+    }
+
+    @Test
+    void 투볼_테스트() {
+        int strike = 0;
+        int ball = 2;
+        Result result = new Result(strike, ball);
+        gameService.printResult(result);
+        String expectation = "2볼\n";
+        assertThat(output.toString()).isEqualTo(expectation);
+    }
+
+    @Test
+    void 쓰리볼_테스트() {
+        int strike = 0;
+        int ball = 3;
+        Result result = new Result(strike, ball);
+        gameService.printResult(result);
+        String expectation = "3볼\n";
+        assertThat(output.toString()).isEqualTo(expectation);
+    }
+
+    @Test
+    void 원볼_원스트라이크_테스트() {
+        int strike = 1;
+        int ball = 1;
+        Result result = new Result(strike, ball);
+        gameService.printResult(result);
+        String expectation = "1볼 1스트라이크\n";
+        assertThat(output.toString()).isEqualTo(expectation);
+    }
+
+    @Test
+    void 원볼_투스트라이크_테스트() {
+        int strike = 1;
+        int ball = 1;
+        Result result = new Result(strike, ball);
+        gameService.printResult(result);
+        String expectation = "1볼 1스트라이크\n";
+        assertThat(output.toString()).isEqualTo(expectation);
+    }
+
+    @Test
+    void 투볼_원스트라이크_테스트() {
+        int strike = 1;
+        int ball = 2;
+        Result result = new Result(strike, ball);
+        gameService.printResult(result);
+        String expectation = "2볼 1스트라이크\n";
+        assertThat(output.toString()).isEqualTo(expectation);
+    }
+
+    @Test
+    void 투볼_투스트라이크_테스트() {
+        int strike = 2;
+        int ball = 2;
+        Result result = new Result(strike, ball);
+        gameService.printResult(result);
+        String expectation = "2볼 2스트라이크\n";
+        assertThat(output.toString()).isEqualTo(expectation);
+    }
 }
