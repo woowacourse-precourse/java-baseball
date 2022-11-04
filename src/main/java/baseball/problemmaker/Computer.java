039eb67737d8bcdd17ceac403c4185648fe59051
aksmf1442 <aksmf1442@naver.com>
2022-11-05 02:58:23 +0900

    feat: 입력받은 값이 몇 스트라이크 몇 볼인지 반환해주는 기능 구현

--- src/main/java/baseball/problemmaker/Computer.java
+++ src/main/java/baseball/problemmaker/Computer.java
@@ -2,6 +2,7 @@ package baseball.problemmaker;
 
 import baseball.Numbers;
 import baseball.dto.BallCount;
+import java.util.Objects;
 
 public class Computer implements ProblemMaker{
 
@@ -13,6 +14,29 @@ public class Computer implements ProblemMaker{
 
     @Override
     public BallCount findBallCount(Numbers numbers) {
-        return new BallCount(0, 0);
+        int strike = 0;
+        int ball = 0;
+        for (int numberIndex = 0; numberIndex < numbers.getNumbers().size(); numberIndex++) {
+            Integer number = numbers.getNumbers().get(numberIndex);
+            if (isStrike(number, numberIndex)) {
+                strike++;
+            }
+
+            if (isBall(number, numberIndex)) {
+                ball++;
+            }
+        }
+        return new BallCount(strike, ball);
+    }
+
+    private boolean isBall(Integer number, int numberIndex) {
+        if (!numbers.getNumbers().contains(number)) {
+            return false;
+        }
+        return !Objects.equals(numbers.getNumbers().get(numberIndex), number);
+    }
+
+    private boolean isStrike(Integer number, int numberIndex) {
+        return Objects.equals(numbers.getNumbers().get(numberIndex), number);
     }
 }
