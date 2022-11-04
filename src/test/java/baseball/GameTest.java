1ce5c9042d1f486e6e0f106988461e8528fd7118
choiejae <ejae8319@gmail.com>
2022-11-04 15:51:25 +0900

    test: Game validityChecker 5. 랜덤 값 확인

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -3,6 +3,7 @@ package baseball;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.assertj.core.api.Assertions;
@@ -11,8 +12,12 @@ import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GameTest {
   Game game;
 
@@ -32,18 +37,21 @@ public class GameTest {
     System.setOut(originalOut);
   }
   @Test
+  @Order(1)
   @DisplayName("Game 생성 테스트")
   public void isGameStartWhenFlagIs1(){
     Assertions.assertThat(game.toBeContinued).isEqualTo(1);
   }
 
   @Test
+  @Order(2)
   @DisplayName("유효성 검사 테스트")
   public void generateNumberValidate() {
     game.generateNumberTest("1");
   }
 
   @Test
+  @Order(3)
   @DisplayName("유효성 검사 1. Null 확인")
   public void generateNumberValidateIsStringNotNull() {
     Assertions.assertThatThrownBy(() -> {
@@ -52,12 +60,15 @@ public class GameTest {
   }
 
   @Test
+  @Order(4)
   @DisplayName("유효성 검사 2. 유효한 값 1 확인")
   public void generateNumberValidateIsStringOne() {
     game.generateNumberTest("1");
     assertEquals("1", outContent.toString());
   }
+
   @Test
+  @Order(5)
   @DisplayName("유효성 검사 3. 유효한 값 2 확인")
   public void generateNumberValidateIsStringTwo() {
     game.generateNumberTest("2");
@@ -65,10 +76,26 @@ public class GameTest {
   }
 
   @Test
+  @Order(6)
   @DisplayName("유효성 검사 4. 유효하지 않은 값 확인")
   public void generateNumberValidateIsStringUnavailable() {
     Assertions.assertThatThrownBy(() -> {
       game.generateNumberTest("3");
     }).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @Order(7)
+  @DisplayName("유효성 검사 5. 랜덤 값 확인")
+  public void generateNumberValidateRandomTest() {
+    String number = Integer.toString(Randoms.pickNumberInRange(0, 9));
+    if (number == "1" || number == "2") {
+      game.generateNumberTest(number);
+      assertEquals(number, outContent.toString());
+    } else {
+      Assertions.assertThatThrownBy(() -> {
+        game.generateNumberTest(number);
+      }).isInstanceOf(IllegalArgumentException.class);
+    }
   }
 }
