3052936b81da4b722196bff4bb25778a21b9cc74
squirMM <jsm6616@naver.com>
2022-11-05 01:22:00 +0900

    :rocket: feat(BaseballGame): user exception add, make game logic, add extends message

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,28 +1,71 @@
 package baseball;
 
 import baseball.computer.Controller;
-import baseball.Message;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+import static baseball.Message.*;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
-public class BaseballGame {
-    private static final int RE_GAME = 1;
-    private static final int STOP_GAME = 2;
-    private final Message message = new Message();
+public class BaseballGame extends Message {
+    private static final String RE_GAME = "1";
+    private static final String STOP_GAME = "2";
 
     public void play() {
-        message.printStartMessage();
+        printStartMessage();
+        String statusInput;
         do {
             runGame();
-        } while (true);
+            printRestartMessage();
+            statusInput = getStatusInput();
+        } while (Objects.equals(statusInput, RE_GAME));
     }
 
     public void runGame() {
         List<Integer> balls = new Controller().getBalls();
         do {
-            message.printInputMessage();
-            //값 받아오기
+            printInputMessage();
+            String input;
+            try {
+                input = getInput();
+            } catch (IllegalArgumentException e) {
+                return;
+            }
+            int[] userBalls = getUserBalls(input);
             //값 비교
         } while (true);
+        // 모두 맞출때까지
     }
+
+    public String getInput() throws IllegalArgumentException {
+        String input = readLine();
+        if (checkInputException(input)) {
+            throw new IllegalArgumentException("형식에 맞지 않음");
+        }
+        return input;
+    }
+
+    public String getStatusInput(){
+        return readLine();
+    }
+
+    public int[] getUserBalls(String input) {
+        return Stream.of(input).mapToInt(Integer::parseInt).toArray();
+    }
+
+    public Boolean checkInputException(String input) {
+        // 길이가 3이 아닐 경우
+        if (input.length() != 3) {
+            return true;
+        }
+        // 모두 숫자가 아닐 경우
+        boolean isNumeric = input.matches("[+-]?\\d*(\\.\\d+)?");
+        if (!isNumeric) {
+            return true;
+        }
+        return false;
+    }
+
 }
--- src/main/java/baseball/User.java
+++
@@ -1,5 +0,0 @@
-package baseball;
-
-public class User {
-    // 인풋 값 처리 및 에러 반환
-}
