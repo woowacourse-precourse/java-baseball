bd9ede9cdcd363332d0228f9d81e8e663a622cfe
linda2927 <linda2927@naver.com>
2022-11-04 14:32:39 +0900

    fix: add javadoc for each method in InputValidation class

--- src/main/java/baseball/InputValidation.java
+++ src/main/java/baseball/InputValidation.java
@@ -2,6 +2,12 @@ package baseball;
 
 public class InputValidation {
 
+    /**
+     * Validates user's input
+     * @param input 사용자가 입력한 값
+     * @return boolean
+     * @throws IllegalArgumentException input not valid
+     */
     static boolean isValidInput(String input) throws IllegalArgumentException {
         if (input.length() != 3) {
             throw new IllegalArgumentException("입력한 숫자는 3 자리여야 합니다.");
@@ -18,6 +24,11 @@ public class InputValidation {
         return true;
     }
 
+    /**
+     * Check if input string contains only numbers
+     * @param input 사용자가 입력한 값
+     * @throws IllegalArgumentException input contains non-number string
+     */
     private static void isIntegerStr(String input) throws IllegalArgumentException {
         char[] inputCharArr = input.toCharArray();
         for (char c : inputCharArr) {
@@ -26,7 +37,11 @@ public class InputValidation {
         }
     }
 
-
+    /**
+     * Check if input string has any duplicate number digit
+     * @param input 사용자가 입력한 값
+     * @throws IllegalArgumentException input contains duplicates
+     */
     private static void hasDuplicates(String input) throws IllegalArgumentException {
         for (int i = 0; i < input.length() - 1; i++) {
             if (input.substring(i+1).indexOf(input.charAt(i)) != -1) {
