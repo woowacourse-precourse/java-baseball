716e5a3fc9c2d476d3316fb1b5d08d8b611559c0
YSEO <1park4170@gmail.com>
2022-11-04 09:45:21 +0900

    refactor<get input from user> : make "getInputNumForGame" return int array, not int ,and modified "checkIfInputDuplicated" to check int array, not int
    in relation with "ScoreJudge", it is better to return int[] because comparing get easier
    
    Before:
    "getInputNumForGame" return int, this caused missing of "hundred" in int array when converting int to int array
    example) 032 -> [3,2]
    
    After:
    getInputNumForGame" return int array

--- src/main/java/baseball/NumsInputFromUser.java
+++ src/main/java/baseball/NumsInputFromUser.java
@@ -3,22 +3,27 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class NumsInputFromUser {
-    public int getInputNumForGame() throws IllegalArgumentException{
+    public int[] getInputNumForGame() throws IllegalArgumentException{
         int input = Integer.parseInt(Console.readLine());
-        checkIfInputDuplicated(input);
-
-        return input;
-    }
-
-    public void checkIfInputDuplicated(int input) throws IllegalArgumentException {
-        IllegalArgumentException exception = new IllegalArgumentException();
-
-        // 범위 확인
-        if (input < 100 || input > 999) throw exception;
-
         int hundred = input / 100;
         int ten     = (input % 100) / 10;
         int one     = ((input % 100) % 10);
+
+        checkIfInputDuplicated(hundred, ten, one);
+
+        int[] InputArr = new int[3];
+        InputArr[0]    = hundred;
+        InputArr[1]    = ten;
+        InputArr[2]    = one;
+
+        return InputArr;
+    }
+
+    public void checkIfInputDuplicated(int hundred, int ten , int one) throws IllegalArgumentException {
+        IllegalArgumentException exception = new IllegalArgumentException();
+
+        if (hundred > 9) throw exception;
+        if (hundred < 0 || ten < 0 || one < 0) throw exception;
 
         if (hundred == ten || hundred == one || ten == one) {
             throw exception;
