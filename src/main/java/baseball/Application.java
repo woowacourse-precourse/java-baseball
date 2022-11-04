87049f3a81f99764b5440fc474ad85bcb112237f
chaeyeong222 <iky1505@naver.com>
2022-11-04 14:40:37 +0900

    feat: 랜덤숫자 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int randomNumber = GetRandomNumber();
+        System.out.println("숫자를 입력해주세요 : ");
+
     }
+
+    private static int GetRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        int randomNumber = computer.get(0)*100 + computer.get(1)*10 + computer.get(2);
+        return randomNumber;
+    }
+
 }
