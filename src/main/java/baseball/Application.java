408d92168f94c05ddce3a6b7397107b310f19462
ParkSangCheon <pswlove38@naver.com>
2022-11-05 05:53:47 +0900

    숫자 스트라이크/볼 체크 checkNum함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,18 +15,18 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
-        System.out.println(computer);
+        System.out.println("맞출 수 : " + computer);
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         while(true){
-            List<Integer> numList = new ArrayList<>();
             System.out.print("숫자를 입력해주세요 : ");
             int num = sc.nextInt();
-            numList = returnList(num);
-            System.out.println(numList);
+            List<Integer> numList = returnList(num);
+            checkNum(computer, numList);
         }
     }
 
+    // 받은 숫자를 각 자리수로 짤라서 리스트에 넣음
     public static List<Integer> returnList(int num){
         List<Integer> each = new ArrayList<>();
         while(num != 0) {
@@ -36,8 +36,35 @@ public class Application {
         Collections.reverse(each);
         return each;
     }
-    // 받은 숫자를 각 자리수로 짤라서 리스트에 넣음
-    public static void checkNum(int num){
 
+    // 숫자 스트라이크/볼 체크
+    public static void checkNum(List<Integer> computer, List<Integer> my){
+        Scanner sc = new Scanner(System.in);
+        int s = 0;
+        int b = 0;
+        if (computer.get(0) == my.get(0) && computer.get(1) == my.get(1) && computer.get(2) == my.get(2)){
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            //int result = sc.nextInt();
+        }
+
+        for (int i = 0; i < computer.size(); i++){
+            if (computer.contains(my.get(i)) && computer.get(i) == my.get(i)){
+                s++;
+            } else if (computer.contains(my.get(i))) {
+                b++;
+            }
+        }
+
+        if (s == 0 && b == 0)
+            System.out.println("낫싱");
+        else if (s == 0) {
+            System.out.println(b + "볼");
+        } else if (b == 0) {
+            System.out.println(s + "스트라이크");
+        }else {
+            System.out.println(b + "볼 " + s + "스트라이크");
+        }
     }
 }
