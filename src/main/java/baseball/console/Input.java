8f4645523067dfc33673333c02f17f098dd9dc60
민병욱 <woook27@naver.com>
2022-11-04 11:20:34 +0900

    refactor (Input): get 함수 return type 변경

--- src/main/java/baseball/console/Input.java
+++ src/main/java/baseball/console/Input.java
@@ -3,13 +3,12 @@ package baseball.console;
 import java.util.Scanner;
 
 public class Input {
-
     private static final String inputMessage = "숫자를 입력해주세요 : ";
 
-    public static int get() {
+    public static String get() {
         System.out.print(inputMessage);
         Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
+        String number = scanner.nextLine();
         return number;
     }
 }
