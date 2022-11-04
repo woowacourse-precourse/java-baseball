dab9b96a54fdc76533aa06e89d483374e8c1bfef
jeongwle <dldl4568@naver.com>
2022-11-04 16:05:36 +0900

    refactor(computertest) : 코드 변경에 따라 수정
    컴퓨터 클래스 코드 변경에 따라 세개의 다른 수를 가져오는 방법 변경

--- src/test/java/computer/ComputerTest.java
+++ src/test/java/computer/ComputerTest.java
@@ -4,14 +4,23 @@ import static org.assertj.core.api.Assertions.*;
 
 import baseball.Computer;
 import java.util.List;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 public class ComputerTest {
 
-    private final List<Integer> computer = Computer.getComputer();
+    private static List<Integer> computerNumber;
+
+    @BeforeAll
+    public static void beforeAll() {
+        Computer computer = new Computer();
+        computerNumber = computer.getComputerNumber();
+    }
+
+
     @Test
     void 각_숫자는_1에서_9입니다() {
-        for (Integer number : computer) {
+        for (Integer number : computerNumber) {
             boolean result = number >= 1 && number <= 9;
             assertThat(result).isEqualTo(true);
         }
@@ -19,9 +28,9 @@ public class ComputerTest {
 
     @Test
     void 각_숫자는_서로_다른_숫자입니다() {
-        Integer number1 = computer.get(0);
-        Integer number2 = computer.get(1);
-        Integer number3 = computer.get(2);
+        Integer number1 = computerNumber.get(0);
+        Integer number2 = computerNumber.get(1);
+        Integer number3 = computerNumber.get(2);
 
         int count = 0;
         if (!number1.equals(number2)) {
@@ -39,7 +48,7 @@ public class ComputerTest {
 
     @Test
     void 컴퓨터가_들고있는_숫자의_개수는_3개입니다() {
-        boolean result = computer.size() == 3;
+        boolean result = computerNumber.size() == 3;
 
         assertThat(result).isEqualTo(true);
     }
