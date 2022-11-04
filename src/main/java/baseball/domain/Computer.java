88dbf7fe1b6e72653afefa32b7685d7f804c2a7a
TaeHwan <kimta2hwan@gmail.com>
2022-11-05 05:34:08 +0900

    feat(user): 게임마다 3자리의 수의 정답을 랜덤하게 생성하는 기능 구현

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,32 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Computer {
+
+    private static final int ANSWER_LENGTH = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBEr = 9;
+    private int[] answer;
+
+    public Computer() {
+        answer = new int[ANSWER_LENGTH];
+    }
+
+    public void generateAnswer() {
+        Set<Integer> answerSet = new HashSet<>();
+
+        while (answerSet.size() != ANSWER_LENGTH) {
+            answerSet.add(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBEr));
+        }
+
+        int index = 0;
+
+        for (int number : answerSet) {
+            answer[index] = number;
+            index++;
+        }
+    }
+}
