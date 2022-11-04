0c94999f2c0ebafa3454c12c409301611d15468d
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-11-04 14:32:50 +0900

    feat(domain/User): User에게서 수 입력받기

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -1,4 +1,19 @@
 package baseball.domain;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import baseball.view.UserInput;
 
 public class User {
+    public static ArrayList<Integer> main(String[] args) {
+        UserInput.getUserNumber();
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> UserNumber = new ArrayList();
+
+        for (int i=0; i<3; i++) {
+            Integer num = sc.nextInt();
+            UserNumber.add(num);
+        }
+        return UserNumber;
+    }
 }
