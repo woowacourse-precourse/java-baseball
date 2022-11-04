f20f71493ced9c76a30bfa4f570c87cb50422e34
sen <seeunchoi99@gmail.com>
2022-11-04 19:01:16 +0900

    ✨ 사용자 입력에 알맞은 결과 값 반환하는 기능 구현
    잘못된 값, 스트라이크, 볼을 판별하는 로직 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,15 +1,23 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer {
     private final List<Integer> answer;
+    private List<Integer> inputList;
+    private HashMap<String, Integer> counts;
 
     public Computer() {
         this.answer = getAnswer();
+        this.counts = new HashMap<>();
     }
 
     private List<Integer> getAnswer() {
@@ -24,6 +32,45 @@ public class Computer {
         Integer randomNumber = Randoms.pickNumberInRange(1, 9);
         if (!answer.contains(randomNumber)) {
             answer.add(randomNumber);
+        }
+    }
+
+    private void getResult(String input) {
+        this.inputList = stringToIntegerList(input);
+        this.counts = initializeCounts();
+
+        for (int index = 0; index < 3; index++) {
+            getStrikeOrBallCount(index);
+        }
+    }
+
+    private void getStrikeOrBallCount(int index) {
+        if (answer.get(index).equals(inputList.get(index))) {
+            counts.put("strike", counts.get("strike") + 1);
+        } else if (answer.contains(inputList.get(index))) {
+            counts.put("ball", counts.get("ball") + 1);
+        }
+    }
+
+    private HashMap<String, Integer> initializeCounts() {
+
+        counts.put("strike", 0);
+        counts.put("ball", 0);
+        return counts;
+    }
+
+    private static List<Integer> stringToIntegerList(String string) {
+        validateInput(string);
+
+        return Stream.of(string.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static void validateInput(String input) {
+        final Pattern pattern = Pattern.compile("^[1-9]*$");
+        if (!pattern.matcher(input).matches() || input.length() != 3) {
+            throw new IllegalArgumentException("잘못된 입력값입니다. 애플리케이션을 종료합니다.");
         }
     }
 }
