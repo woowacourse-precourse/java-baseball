39b383d1543e530e9fcc5d835ba8f509dd077a5c
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-04 15:46:28 +0900

    feat(src/main): 랜덤 숫자 형성
    1~9의 서로 다른 수 세 개를 만든다. -> generateRandom3Numbers()
    main에서 정답 값으로 사용할 것이다.
    
    해결: 게임 시작-숫자를 만든다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
+    private static List<Integer> generateRandom3Numbers() {
+        List<Integer> randomNums = new ArrayList<>();
+        for (int repetition = 0; repetition < 3; repetition++) {
+            randomNums.add(Randoms.pickNumberInRange(1, 9));
+        }
+        return randomNums;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> answer=generateRandom3Numbers();
+        System.out.println(answer);
     }
 }
