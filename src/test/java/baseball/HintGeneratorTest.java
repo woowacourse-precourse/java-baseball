fc2b3a9ad1c618d2bf3515c73a15e50675c0e966
JUHA <khabh@naver.com>
2022-11-04 17:41:46 +0900

    test(HintGeneratorTest): 힌트 생성 기능 테스트

---
+++ src/test/java/baseball/HintGeneratorTest.java
@@ -0,0 +1,46 @@
+package baseball;
+
+import baseball.domain.HintGenerator;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import org.junit.jupiter.api.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HintGeneratorTest {
+    @Test
+    void case1() {
+        ArrayList<Integer> strikeNumbers = new ArrayList<>();
+        ArrayList<Integer> playerGuess = new ArrayList<>();
+        HashMap<Integer, Integer> result = new HashMap<>();
+        int[] strikeNumbersArray = new int[] {1, 2, 3};
+        int[] playerGuessArray = new int[] {2, 1, 3};
+
+        Arrays.stream(strikeNumbersArray).forEach(strikeNumbers::add);
+        Arrays.stream(playerGuessArray).forEach(playerGuess::add);
+
+        result.put(0, 1);
+        result.put(1, 2);
+
+        assertThat(new HintGenerator(strikeNumbers).generateHint(playerGuess)).isEqualTo(result);
+    }
+
+    @Test
+    void case2() {
+        ArrayList<Integer> strikeNumbers = new ArrayList<>();
+        ArrayList<Integer> playerGuess = new ArrayList<>();
+        HashMap<Integer, Integer> result = new HashMap<>();
+        int[] strikeNumbersArray = new int[] {1, 2, 3};
+        int[] playerGuessArray = new int[] {4, 5, 6};
+
+        Arrays.stream(strikeNumbersArray).forEach(strikeNumbers::add);
+        Arrays.stream(playerGuessArray).forEach(playerGuess::add);
+
+        result.put(0, 0);
+        result.put(1, 0);
+
+        assertThat(new HintGenerator(strikeNumbers).generateHint(playerGuess)).isEqualTo(result);
+    }
+}
