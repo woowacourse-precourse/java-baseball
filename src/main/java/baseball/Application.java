9296e36080491b1aa92fdc0a509404dda1874a30
csj <koreaxman@naver.com>
2022-11-04 16:48:28 +0900

    fix: change the start to game class

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        Player player = new Player();
-        player.inputNumbers();
+        Game game = new Game();
+        game.start();
     }
 }
