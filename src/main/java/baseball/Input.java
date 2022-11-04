a12c8af7512bf55eead610579f3b25b5fd2df103
yback1223 <yoyob1223@gmail.com>
2022-11-04 10:19:40 +0900

    feat: read what user typed, validate input, method for String to List

---
+++ src/main/java/baseball/Input.java
@@ -0,0 +1,47 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Input {
+
+  private static final int STRINGLEN = 3;
+
+  public List<Character> readAndMakeInputList() {
+    String inputString = Console.readLine();
+    validStringLength(inputString);
+    return stringToList(inputString);
+  }
+
+  private List<Character> stringToList(String inputString) {
+    List<Character> list = new ArrayList<>();
+    for (int i = 0; i < inputString.length(); i++) {
+      list.add(inputString.charAt(i));
+    }
+    validOnlyOneNumInList(list);
+    validNumElementAndNoZero(list);
+    return list;
+  }
+
+  private void validStringLength(String inputString) {
+    if (inputString.length() != STRINGLEN) {
+      throw new IllegalArgumentException(inputString + "은 3자리가 아닙니다.");
+    }
+  }
+
+  private void validOnlyOneNumInList(List<Character> list) {
+    if (list.get(0) == list.get(1) || list.get(1) == list.get(2) || list.get(0) == list.get(2)) {
+      throw new IllegalArgumentException("중복이 있습니다.");
+    }
+  }
+
+  private void validNumElementAndNoZero(List<Character> list) {
+    for (Character character : list) {
+      if (character < '1' || '9' < character) {
+        throw new IllegalArgumentException("1에서 9까지만 가능합니다.");
+      }
+    }
+  }
+
+}
