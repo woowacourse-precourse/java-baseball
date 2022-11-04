93be03d8245f933f92fc93c99b1986c99e7401c9
JunDonghyuk <ehdgur4814@naver.com>
2022-11-05 02:46:08 +0900

    feat(getRandomNumber): update
    Update new function
    - 3자리 랜덤 숫자 생성 후 리스트 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static List<Integer> getRandomNumber() {
+        List<Integer> baseballNumber = new ArrayList<>();
+        while (baseballNumber.size() <= 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!baseballNumber.contains(randomNumber)) {
+                baseballNumber.add(randomNumber);
+            }
+        }
+        return baseballNumber;
+    }
+
 }
