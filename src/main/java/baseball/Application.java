8ac231eaffa3d74f7c77201ebdc4a3bbf0fdea6d
d0ri123 <lsh81550@gmail.com>
2022-11-04 15:49:10 +0900

    feat: 상대방(컴퓨터)이 난수를 생성하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public static int random_num(){
+        List<Integer> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        while(list.size()<3){
+            int random = Randoms.pickNumberInRange(1,9);
+            if(!list.contains(random)){
+                list.add(random);
+                sb.append(random);
+            }
+        }
+        int answer = Integer.parseInt(String.valueOf(sb));
+        System.out.println(answer);
+        return answer;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
