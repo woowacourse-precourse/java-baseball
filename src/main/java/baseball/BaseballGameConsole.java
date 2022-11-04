ace02e222509eb64498f23a9e229c463062121de
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-05 00:09:05 +0900

    feat(playGame): build command flow

--- src/main/java/baseball/BaseballGameConsole.java
+++ src/main/java/baseball/BaseballGameConsole.java
@@ -2,4 +2,10 @@ package baseball;
 
 public class BaseballGameConsole {
 
+    public void playGame() { // Player
+        // Starting guide
+        do {
+            // Play single baseball game
+        } while (true); // Ask player to play a new game
+    }
 }
