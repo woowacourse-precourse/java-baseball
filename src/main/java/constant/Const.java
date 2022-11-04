a0039e500a6909784f0a793d4278de8540d528a7
kim0914 <kosa0914@naver.com>
2022-11-04 17:45:15 +0900

    feat(constant): 정적 변수 및 출력문 상수화

---
+++ src/main/java/constant/Const.java
@@ -0,0 +1,14 @@
+package constant;
+
+public class Const {
+
+    public static final String GAME_START_MESSAGE = "숫자 야구를 시작합니다.";
+    public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String NUMBER_INPUT_REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String RESTART_REQUEST_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final int NUMBER_LENGTH = 3;
+    public static final int NUMBER_MIN_VALUE =  1;
+    public static final int NUMBER_MAX_VALUE =  9;
+    public static final int GAME_END_STRIKE_COUNT = 3;
+}
---
+++ src/main/java/constant/Message.java
@@ -0,0 +1,20 @@
+package constant;
+
+public class Message {
+
+    public static void showNumberRequestMessage(){
+        System.out.print(Const.NUMBER_INPUT_REQUEST_MESSAGE);
+    }
+
+    public static void showGameStartMessage(){
+        System.out.println(Const.GAME_START_MESSAGE);
+    }
+
+    public static void showGameEndMessage(){
+        System.out.println(Const.GAME_END_MESSAGE);
+    }
+
+    public static void showRestartRequestMessage(){
+        System.out.println(Const.RESTART_REQUEST_MESSAGE);
+    }
+}
