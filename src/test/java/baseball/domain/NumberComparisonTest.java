3f35b52449b1244ad27f239b44960131044a86f3
alstjr706 <alstjr706@naver.com>
2022-11-05 01:25:18 +0900

    test : (숫자 야구) 테스트 코드 리펙토링

--- src/test/java/baseball/domain/NumberComparisonTest.java
+++ src/test/java/baseball/domain/NumberComparisonTest.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -9,10 +10,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NumberComparisonTest {
 
+    private NumberComparison numberComparison;
+
+    @BeforeEach
+    void setUp() {
+        numberComparison = new NumberComparison();
+    }
+
     @Test
     void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_모두_맞을_경우() {
         int result = 3;
-        NumberComparison numberComparison = new NumberComparison();
         int compare = numberComparison.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
         assertThat(compare).isEqualTo(result);
     }
@@ -20,7 +27,6 @@ class NumberComparisonTest {
     @Test
     void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_2개_맞을_경우() {
         int result = 2;
-        NumberComparison numberComparison = new NumberComparison();
         int compare = numberComparison.compare(Arrays.asList(1, 2, 5), Arrays.asList(1, 2, 3));
         assertThat(compare).isEqualTo(result);
     }
@@ -28,7 +34,6 @@ class NumberComparisonTest {
     @Test
     void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_1개_맞을_경우() {
         int result = 1;
-        NumberComparison numberComparison = new NumberComparison();
         int compare = numberComparison.compare(Arrays.asList(1, 9, 5), Arrays.asList(1, 2, 3));
         assertThat(compare).isEqualTo(result);
     }
@@ -36,7 +41,6 @@ class NumberComparisonTest {
     @Test
     void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_없을_경우() {
         int result = 0;
-        NumberComparison numberComparison = new NumberComparison();
         int compare = numberComparison.compare(Arrays.asList(4, 9, 5), Arrays.asList(1, 2, 3));
         assertThat(compare).isEqualTo(result);
     }
