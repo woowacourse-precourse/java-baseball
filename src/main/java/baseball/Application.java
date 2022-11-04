f1ac17f42bf025dfd850b3413d529507aeab4bab
JDK <ehdrjf2062@gmail.com>
2022-11-04 14:41:57 +0900

    Style: 메서드 정렬

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -39,14 +39,32 @@ public class Application {
         answer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
     }
 
-    private static boolean checkRestart() {
-        System.out.println(Comment.REGAME);
-        String restartRely = Console.readLine();
-        validateInput(restartRely, 1);
-        if (restartRely.equals("1")) {
-            return true;
+    private static void validateInput(String input, int digit) {
+        String tmpInput = input.trim();
+
+        String numberOnlyRegex = "^[0-9]+$";
+        boolean matches = tmpInput.matches(numberOnlyRegex);
+        if (!matches) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
         }
-        return false;
+
+        if (tmpInput.length() != digit) {
+            throw new IllegalArgumentException(digit + "자릿수를 입력해주세요.");
+        }
+    }
+
+    private static void calculateHint(List<Integer> answer, String inputStr) {
+        initializeHint();
+        for (int i = 0; i < inputStr.length(); i++) {
+            Integer target = Integer.parseInt(inputStr.charAt(i) + "");
+            if (answer.contains(target) && answer.get(i).equals(target)) {
+                Integer beforeCnt = hintMap.get(Hint.STRIKE);
+                hintMap.put(Hint.STRIKE, ++beforeCnt);
+            } else if (answer.contains(target)) {
+                Integer beforeCnt = hintMap.get(Hint.BALL);
+                hintMap.put(Hint.BALL, ++beforeCnt);
+            }
+        }
     }
 
     private static void printHint() {
@@ -69,38 +87,20 @@ public class Application {
         System.out.println(hintStr);
     }
 
-    private static void calculateHint(List<Integer> answer, String inputStr) {
-        initializeHint();
-        for (int i = 0; i < inputStr.length(); i++) {
-            Integer target = Integer.parseInt(inputStr.charAt(i) + "");
-            if (answer.contains(target) && answer.get(i).equals(target)) {
-                Integer beforeCnt = hintMap.get(Hint.STRIKE);
-                hintMap.put(Hint.STRIKE, ++beforeCnt);
-            } else if (answer.contains(target)) {
-                Integer beforeCnt = hintMap.get(Hint.BALL);
-                hintMap.put(Hint.BALL, ++beforeCnt);
-            }
+    private static boolean checkRestart() {
+        System.out.println(Comment.REGAME);
+        String restartRely = Console.readLine();
+        validateInput(restartRely, 1);
+        if (restartRely.equals("1")) {
+            return true;
         }
+        return false;
     }
 
     private static void initializeHint() {
         hintMap = new HashMap();
         hintMap.put(Hint.STRIKE, 0);
         hintMap.put(Hint.BALL, 0);
-    }
-
-    private static void validateInput(String input, int digit) {
-        String tmpInput = input.trim();
-
-        String numberOnlyRegex = "^[0-9]+$";
-        boolean matches = tmpInput.matches(numberOnlyRegex);
-        if (!matches) {
-            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
-        }
-
-        if (tmpInput.length() != digit) {
-            throw new IllegalArgumentException(digit + "자릿수를 입력해주세요.");
-        }
     }
 }
 
