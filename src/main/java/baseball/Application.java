a4019f17967dce127c5f394e98920ca4ba14a9a5
kkh <kkho9654@naver.com>
2022-11-04 20:35:18 +0900

    Feat: 3가지 랜덤한 숫자 뽑는 기능
    StringBuilder를 이용해  3개의 integer 리스트를 하나의 문자열로 반환한다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    }
+
+    private static String getRandomThreeDigitNumberString() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        StringBuilder threeDigitNumberStringBuilder = new StringBuilder();
+
+        for (Integer digitNumber : computer) {
+            threeDigitNumberStringBuilder.append(digitNumber);
+        }
+
+        return String.valueOf(threeDigitNumberStringBuilder);
     }
 }
