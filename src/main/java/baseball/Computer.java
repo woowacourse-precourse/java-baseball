5dbe8a8fedb823c747e84948e0e5cb4b293907eb
miffyKing <whqudghk28@gmail.com>
2022-11-04 20:42:53 +0900

    feat: Make Computer Class methods that prints calculate result from Referee Class

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -3,8 +3,10 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Computer {
+
   private static String RandomValue;
 
   private static void printGameStart() {
@@ -15,9 +17,12 @@ public class Computer {
     System.out.print("숫자를 입력해주세요 : ");
   }
 
+  private static void printCalculateResult(String calculatedResult) {
+    System.out.println(calculatedResult);
+  }
   private static String generateRandom() {
     StringBuilder sb = new StringBuilder();
-    ArrayList <Integer> generateRandNum = new ArrayList<>();
+    ArrayList<Integer> generateRandNum = new ArrayList<>();
 
     while (generateRandNum.size() < 3) {
       int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -26,17 +31,24 @@ public class Computer {
         sb.append(Integer.toString(randomNumber));
       }
     }
+
     RandomValue = sb.toString();
     return RandomValue;
   }
 
-  public static void testComputerPrint(int mode) {
-    if (mode == 1) printGameStart();
-    if (mode == 2) printInputNumber();
-    //if (mode == 3) generateRandom();
+  public static String testComputerPrint(int mode, String input) {
+    if (mode == 1) {
+      printGameStart();
+    }
+    if (mode == 2) {
+      printInputNumber();
+    }
+    if (mode == 3) {
+      return generateRandom();
+    }
+    if (mode == 4) {
+      printCalculateResult(input);
+    }
+    return "";
   }
-  public static String testRandomGen() {
-    return generateRandom();
-  }
-
 }
