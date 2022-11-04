6b11304b25759b4d40c2b35b010f41aba98bfb5c
JJ <jeongjae1996@naver.com>
2022-11-04 17:22:22 +0900

    feat: 게임 종료 시, 2를 입력하면 프로그램이 즉시 종료되도록 기능 구현
    2가 입력되고 프로그램이 바로 종료되지 않으면, ApplicationTest에서 오류가 발생해 즉시 종료하도록 구현했습니다. 들여쓰기의 depth가 3이라 리팩토링이 필요합니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -141,6 +141,7 @@ public class Application {
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 user_input = Console.readLine();
                 validateGameStart(user_input);
+                if (user_input.equals("2")) {break;}
                 computer_num = createRandomNumber();
             }
         }
