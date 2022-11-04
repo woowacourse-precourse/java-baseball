ddb2e203710935dbe08ba945f653fb9d69ad18cf
SeolYoungKim <nasur4da@gmail.com>
2022-11-04 16:06:14 +0900

    comment: 기존에 작성되어 있던 TODO 프로그램 구현 주석 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,8 +6,6 @@ import baseball.print.MessagePrinter;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
         BaseballGame baseballGame = new BaseballGame(
                 new NumberScanner(),
                 new BallAndStrikeCounter(),
