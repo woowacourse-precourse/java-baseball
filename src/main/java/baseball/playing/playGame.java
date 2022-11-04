4fdb6c731d99d328e26a113f211dcd722e61c533
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-04 23:14:46 +0900

    feat(playGame): checkStrikeOrBall 구현과 세부 구현 사항 Strike, Ball 구현

--- src/main/java/baseball/playing/playGame.java
+++ src/main/java/baseball/playing/playGame.java
@@ -4,28 +4,86 @@ import baseball.players.Computer;
 import baseball.players.User;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.*;
+
+
 public class playGame {
 
-    private String computerNumbers;
-    private String userNumbers;
+    private String computerNumber;
+    private String userNumber;
 
     public playGame() {
         startGame();
         Computer computer = new Computer();
-        computerNumbers = computer.getComputerNumbers();
+        computerNumber = computer.getComputerNumbers();
     }
 
     private void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    private List<Integer> stringToList(String number) {
+        List<Integer> numbers = new ArrayList<>();
+        for (char num : number.toCharArray()) {
+            numbers.add(Character.getNumericValue(num));
+        }
+
+        return numbers;
+    }
+
+    private String printStrikeOrBall(String computerNumber, String userNumber) {
+
+        String printResult = "";
+        List<Integer> computerNumberList = stringToList(computerNumber);
+        List<Integer> userNumberList = stringToList(userNumber);
+
+        return printResult;
+    }
+
+    private Map<String, Integer> checkStrikeOrBall(List<Integer> computerNumberList, List<Integer> userNumberList) {
+
+        Map<String, Integer> strikeBall = new HashMap<>();
+
+        strikeBall.put("Strike", Strike(computerNumberList, userNumberList));
+        strikeBall.put("Ball", Ball(computerNumberList, userNumberList));
+
+        return strikeBall;
+    }
+
+    private int Strike(List<Integer> computerNumberList, List<Integer> userNumberList) {
+
+        int numberSize = 3;
+        int strike = 0;
+
+        for (int i = 0; i < numberSize; i++) {
+            if (computerNumberList.get(i) == userNumberList.get(i))
+                strike++;
+        }
+
+        return strike;
+    }
+
+    private int Ball(List<Integer> computerNumberList, List<Integer> userNumberList) {
+
+        int numberSize = 3;
+        int ball = 0;
+
+        for (int i = 0; i < numberSize; i++) {
+            if (!(computerNumberList.get(i) == userNumberList.get(i))
+                    && computerNumberList.contains(userNumberList.get(i)))
+                ball++;
+        }
+
+        return ball;
+    }
+
     private String inputUserNumbers() {
 
-        String inputNumbers = Console.readLine();
-        User user = new User(inputNumbers);
+        String inputNumber = Console.readLine();
+        User user = new User(inputNumber);
 
-        userNumbers = user.getUserNumbers();
+        userNumber = user.getUserNumbers();
 
-        return userNumbers;
+        return userNumber;
     }
 }
