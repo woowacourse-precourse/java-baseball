168bbec96f45c1522a6e15c8b17f7bf0e03f5ac1
sungjun kim <dd8873@naver.com>
2022-11-04 17:34:51 +0900

    feat : 값 비교 - 유저숫자와 컴퓨터숫자를 비교

--- src/main/java/baseball/ApplicationManager.java
+++ src/main/java/baseball/ApplicationManager.java
@@ -9,6 +9,14 @@ public class ApplicationManager {
         List<Integer> computerNumbers = ComputerRandomNumbersFactory.createComputerRandomNumber();
         UserNumber userNumber = new UserNumber(Input.inputUserNumber());
 
+        while (true) {
+            int total = Comparison.compare(userNumber, computerNumbers);
+            int strike = Comparison.findStrike(userNumber, computerNumbers);
+            int ball = total - strike;
+
+
+        }
+
     }
 
 }
