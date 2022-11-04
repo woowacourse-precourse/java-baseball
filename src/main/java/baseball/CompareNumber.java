c35e6a9b83bac800a91feca1d680c80fe2ce166b
Sinryuji <siler2474@gmail.com>
2022-11-04 22:28:34 +0900

    refactor: CompareNumber
    change for statement to foreach statement

--- src/main/java/baseball/CompareNumber.java
+++ src/main/java/baseball/CompareNumber.java
@@ -6,23 +6,13 @@ public class CompareNumber {
     public static Ball compare(List<Integer> computer, List<Integer> input) {
         Ball ball = new Ball();
 
-        for (int i = 0; i < input.size(); i++) {
-            if (isBall(computer, i, input.get(i))) {
-                ball.increaseBall();
-            }
-            else if (isStrike(computer, i, input.get(i))) {
+        for (int num : input) {
+            if (computer.get(input.indexOf(num)) == num)
                 ball.increaseStrike();
-            }
+            else if (computer.contains(num))
+                ball.increaseBall();
         }
 
         return ball;
-    }
-
-    private static boolean isBall(List<Integer> computer, int i, int num) {
-        return computer.contains(num) && computer.get(i) != num;
-    }
-
-    private static boolean isStrike(List<Integer> computer, int i, int num) {
-        return computer.contains(num) && computer.get(i) == num;
     }
 }
