935ccf1ca76ad7581b81a386c77fff9faf91f947
KIMSEI1124 <workju1124@gmail.com>
2022-11-04 20:54:29 +0900

    feat: 서로 다른 임의의 수 3개를 선택 기능 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,68 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Computer {
+    List<Integer> randomNumbers = new ArrayList<>(Collections.emptyList());
+
+    /**
+     * 리스트에 숫자를 추가한다.
+     *
+     * @param number 숫자
+     */
+    private void addRandomNumberByList(int number) {
+        if (canInputNumber(number)) {
+            randomNumbers.add(number);
+        }
+    }
+
+    /**
+     * 랜덤 숫자를 구한다.
+     *
+     * @return 랜덤 숫자
+     */
+    private int getRandomNumber() {
+        Random random = new Random();
+        return random.nextInt(9);
+    }
+
+    /**
+     * 리스트에 해당 숫자가 있는지 확인한다.
+     *
+     * @param number 숫자
+     * @return 리스트에 해당 숫자가 있으면 ture
+     */
+    private boolean hasNumberByList(int number) {
+        return randomNumbers.contains(number);
+    }
+
+    /**
+     * 랜덤 숫자들을 구한다.
+     */
+    public void getRandomNumbers() {
+        while ((randomNumbers.size() != 3)) {
+            addRandomNumberByList(getRandomNumber());
+        }
+    }
+
+    /**
+     * 숫자를 추가할수 있는가
+     * @param number    숫자
+     * @return  숫자를 추가할수 있으면 ture
+     */
+    private boolean canInputNumber(int number) {
+        return !isZeroNumber(number) && !hasNumberByList(number);
+    }
+
+    /**
+     * 숫자가 0인지 판별
+     * @param number 숫자
+     * @return 숫자가 0이면 ture
+     */
+    private boolean isZeroNumber(int number) {
+        return number == 0;
+    }
+}
