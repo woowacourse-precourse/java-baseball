0985eb6340371364731910c9ac08653cd07b063d
anneu <b.imnuj@gmail.com>
2022-11-04 18:45:35 +0900

    feat: 게임 재시작 여부를 묻는 기능 구현

--- src/main/java/baseball/domain/GameController.java
+++ src/main/java/baseball/domain/GameController.java
@@ -27,4 +27,9 @@ public class GameController {
 
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public boolean askRestart() {
+        System.out.println("현게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return readLine().equals("1");
+    }
 }
