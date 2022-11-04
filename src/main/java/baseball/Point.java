a23c2b1215df1fa63e8b56d6fa5715645e30f69d
thdwoqor <celebarte01@gmail.com>
2022-11-04 21:25:11 +0900

    refactor: Getter와 Setter 메소드 추가

--- src/main/java/baseball/Point.java
+++ src/main/java/baseball/Point.java
@@ -1,11 +1,24 @@
 package baseball;
 
 class Point {
-    int strike;
-    int ball;
+    private int strike;
+    private int ball;
 
-    public Point(int strike, int ball) {
-        this.strike = strike;
+    public int getBall() {
+        return ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public Point setBall(int ball) {
         this.ball = ball;
+        return this;
+    }
+
+    public Point setStrike(int strike) {
+        this.strike = strike;
+        return this;
     }
 }
