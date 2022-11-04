b7c66603dfd8a496c7c837511437bbdbb2b5899c
code-tree <dltjdqls1200@naver.com>
2022-11-04 17:11:17 +0900

    feat: 게임이 끝나고 사용자 입력 값에 따라 처리
    1을 누르면 새로운 게임
    2를 누르면 게임 종료
    그 외에 경우 예외 발생

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -6,20 +6,24 @@ import baseball.view.GameScreen;
 import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballGameController {
+    private static final int NEW_GAME = 1;
+    private static final int GAME_EXIT = 2;
 
     private final BaseballGameService baseballGameService = new BaseballGameService();
 
     public void run() {
         GameScreen.printGameStart();
-        startGame();
+        newGame();
     }
 
-    public void startGame() {
+    public void newGame() {
         baseballGameService.newGame();
+
         boolean isGameClear = false;
         while (!isGameClear) {
             isGameClear = playGame();
         }
+
         GameScreen.printGameEnd();
         askRetryGame();
     }
@@ -35,6 +39,13 @@ public class BaseballGameController {
 
     private void askRetryGame() {
         GameScreen.printAskNewGame();
-        String playerInputKey = Console.readLine();
+        int playerPressKey = Integer.parseInt(Console.readLine());
+
+        if (playerPressKey != NEW_GAME && playerPressKey != GAME_EXIT) {
+            throw new IllegalArgumentException();
+        }
+        if (playerPressKey == NEW_GAME) {
+            newGame();
+        }
     }
 }
