5668a9ddbb7aff62d2fa32797b336c5cd80712dc
SeongukBaek <bsu6235@gmail.com>
2022-11-04 16:55:36 +0900

    docs: write flow of Application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,17 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // TODO: 1. Status가 EXIT가 아닐 동안 반복되는 반복문 안에서 게임 진행
+
+        // TODO: 2. Computer 생성-> 게임용 숫자도 생성
+
+        // TODO: 3. User 생성, User의 숫자 입력 -> 유효성 검사를 자체적으로 시행, 유효하지 않다면 예외 발생 후 종료
+
+        // TODO: 4. Computer와 User의 숫자를 가지고 게임 결과 생성
+
+        // TODO: 5. 게임 결과에 따라 사용자 입력을 받거나, 3으로 이동
+
+        // TODO: 6. 사용자 입력을 받아 게임 추가 진행 여부 확인, 추가 진행 시 2번으로 이동, 종료 시 Status를 EXIT로 변경
     }
 }
