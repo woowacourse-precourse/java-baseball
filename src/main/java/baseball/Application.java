507ff7abf0f14b9832d7061a78edfbcb66f3b696
zcae1020 <zcae1020@naver.com>
2022-11-04 12:45:02 +0900

    feat(class): add GameManger class that administrate game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 
 public class Application {
+    public static boolean exitSign;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
---
+++ src/main/java/baseball/GameManager.java
@@ -0,0 +1,57 @@
+package baseball;
+
+public class GameManager {
+    private String[] guessNumber;
+
+    public void startGame() {
+
+    }
+
+    private void init() {
+
+    }
+
+    private boolean isValid(String input) {
+
+    }
+
+    private boolean isOneToNine(String input) {
+
+    }
+
+    private boolean isThreeChar(String input) {
+
+    }
+
+    private boolean hasDuplicatedDigit(String input) {
+
+    }
+
+    private void makeNewGuessNumber() {
+
+    }
+
+    private String getResultOfUserGuess(String userGuess) {
+
+    }
+
+    private int getStrike(String userGuess) {
+
+    }
+
+    private int getBall(String userGuess) {
+
+    }
+
+    private boolean isNothing(int strike, int ball) {
+
+    }
+
+    private void gameover() {
+
+    }
+
+    private void exitGame(Exception e) {
+
+    }
+}
