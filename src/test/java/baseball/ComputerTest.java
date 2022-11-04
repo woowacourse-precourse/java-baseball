cf0450ad26e1379f98efb3a08928cab244197137
yback1223 <yoyob1223@gmail.com>
2022-11-04 10:32:05 +0900

    test: add test which contains 100 for loops

---
+++ src/test/java/baseball/ComputerTest.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ComputerTest {
+
+  Computer computer;
+
+  @BeforeEach
+  public void setComputerClass() {
+    computer = new Computer();
+  }
+
+  @Test
+  void 진짜_난수인지() {
+    for (int i = 0; i < 100; i++) {
+      List<Character> characters = computer.makeRandomNum();
+      assertThat(characters.get(0)).isNotEqualTo(characters.get(1));
+      assertThat(characters.get(1)).isNotEqualTo(characters.get(2));
+      assertThat(characters.get(0)).isNotEqualTo(characters.get(2));
+    }
+  }
+}
