e7f127fe0108a6f36d102adc287e642ccda4c1bb
iamminho <minho100227@kakao.com>
2022-11-04 16:49:06 +0900

    test(exception): 숫자 이외의 다른 문자들 예외처리 테스트 케이스 추가

--- src/test/java/baseball/controller/ExceptionTest.java
+++ src/test/java/baseball/controller/ExceptionTest.java
@@ -2,22 +2,63 @@ package baseball.controller;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ExceptionTest {
-	@Test
-	@DisplayName("제한된 숫자 범위를 넘어가는 경우")
-	public void validateOnlyOneToNineTest() {
-		String playerNumber = "092";
-		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			Exception.validateOnlyOneToNine(playerNumber);
-		});
+	@Nested
+	@DisplayName("numberBoundary")
+	class numberBoundary {
+		@Test
+		@DisplayName("제한된 숫자 범위를 넘어가는 경우")
+		public void validateNumberBoundaryTest1() {
+			String playerNumber = "092";
+			Assertions.assertThrows(IllegalArgumentException.class, () -> {
+				Exception.validateNumberBoundary(playerNumber);
+			});
+		}
+
+		@Test
+		@DisplayName("알파벳이 들어가는 경우")
+		public void validateNumberBoundaryTest2() {
+			String playerNumber = "98a";
+			Assertions.assertThrows(IllegalArgumentException.class, () -> {
+				Exception.validateNumberBoundary(playerNumber);
+			});
+		}
+
+		@Test
+		@DisplayName("공백이 들어가는 경우")
+		public void validateNumberBoundaryTest3() {
+			String playerNumber = " 897";
+			Assertions.assertThrows(IllegalArgumentException.class, () -> {
+				Exception.validateNumberBoundary(playerNumber);
+			});
+		}
+
+		@Test
+		@DisplayName("특수기호가 들어가는 경우")
+		public void validateNumberBoundaryTest4() {
+			String playerNumber = "8@5";
+			Assertions.assertThrows(IllegalArgumentException.class, () -> {
+				Exception.validateNumberBoundary(playerNumber);
+			});
+		}
 	}
 
 	@Test
 	@DisplayName("제한된 숫자 개수를 넘어가는 경우")
-	public void validateNumberLengthTest() {
+	public void validateNumberLengthTest1() {
 		String playerNumber = "9358";
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Exception.validateNumberLength(playerNumber);
+		});
+	}
+
+	@Test
+	@DisplayName("제한된 숫자 개수보다 적은 경우")
+	public void validateNumberLengthTest2() {
+		String playerNumber = "93";
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
 			Exception.validateNumberLength(playerNumber);
 		});
