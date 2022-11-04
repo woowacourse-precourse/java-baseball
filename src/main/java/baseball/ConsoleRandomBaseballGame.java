1af8afe2451dd028817476574830e90f95956926
2jun0 <soo28819@gmail.com>
2022-11-05 00:08:18 +0900

    Feat(ConsoleBaseballGame): 콘솔 기반 랜덤 야구 게임 클래스 구현

---
+++ src/main/java/baseball/ConsoleRandomBaseballGame.java
@@ -0,0 +1,45 @@
+package baseball;
+
+import baseball.GameConsole;
+import baseball.baseball.Digits;
+import baseball.baseball.RandomBaseballGame;
+import baseball.baseball.Result;
+
+public class ConsoleRandomBaseballGame {
+    private final RandomBaseballGame randomBaseballGame;
+    private final GameConsole gameConsole;
+
+    public ConsoleRandomBaseballGame(RandomBaseballGame randomBaseballGame, GameConsole gameConsole) {
+        this.randomBaseballGame = randomBaseballGame;
+        this.gameConsole = gameConsole;
+    }
+
+    public void play() {
+        gameConsole.printInit();
+
+        while (true) {
+            playOneGame();
+            boolean shallReplay = gameConsole.inputShallReplay();
+
+            if (!shallReplay) break;
+        }
+    }
+
+    private void playOneGame() {
+        randomBaseballGame.newGame();
+
+        while (true) {
+            if (playOneTurn()) break;
+        }
+    }
+
+    /**
+     * @return 게임 종료 여부
+     */
+    private boolean playOneTurn() {
+        Digits digits = gameConsole.readDigits();
+        Result result = randomBaseballGame.match(digits);
+
+        return gameConsole.printGameResult(result);
+    }
+}
