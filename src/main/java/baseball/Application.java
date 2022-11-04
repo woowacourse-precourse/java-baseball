685d6d2eca6112594401025fc54a8a4ae8363515
scyllacore <yms8795@gmail.com>
2022-11-04 14:11:48 +0900

    chore(Application) : "Illegal reflective access" 경고문 숨김.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,8 @@ import baseball.system.GameSystem;
 
 public class Application {
     public static void main(String[] args) {
+        System.err.close();
+        System.setErr(System.out);
         // TODO: 프로그램 구현
         GameSystem gameSystem = new GameSystem();
         gameSystem.runGame();
