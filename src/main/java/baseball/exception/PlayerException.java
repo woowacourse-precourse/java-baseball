8561903d5e3c596830a76732d6d2d887744813f9
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-04 22:42:50 +0900

    refactor: PlayerException의 예외 내용 획일화, 구분했을 시 정보를 조금씩만 알려준다.

--- src/main/java/baseball/exception/PlayerException.java
+++ src/main/java/baseball/exception/PlayerException.java
@@ -8,48 +8,33 @@ public class PlayerException {
     private static final int FIRST = 0;
     private static final int SECOND = 1;
     private static final int THIRD = 2;
-    private static final String NOT_NUMBER_EXCEPTION = "입력은 숫자로만 해야 합니다.";
-    private static final String NOT_THREE_DIGIT_EXCEPTION = "입력은 세자리 숫자여야 합니다. ";
-    private static final String NO_ZERO_EXCEPTION = "입력한 값에 0이 없어야 합니다.";
-    private static final String SAME_NUMBER_EXCEPTION = "입력한 값이 서로 다른 숫자여야 합니다. ";
+    private static final String WRONG_NUMBER_EXCEPTION = "입력은 1부터 9중 서로 다른 3개의 숫자여야 합니다.";
 
     public void checkException(String input){
-        if(isNotNumber(input)) noNumberException();
-        if(isNotThreeDigit(input)) notThreeDigitException();
-        if(containsZero(input)) containsZeroException();
-        if(containsSameNumber(input)) containsSameNumberException();
+        if(isNotNumber(input) || isNotThreeDigit(input)
+                || containsZero(input) || containsSameNumber(input)){
+            wrongNumberException();
+        }
     }
 
     public boolean isNotNumber(String input){
         return !Pattern.matches("^[1-9]*$", input);
     }
 
-    public void noNumberException(){
-        throw new IllegalArgumentException(NOT_NUMBER_EXCEPTION);
+    public void wrongNumberException(){
+        throw new IllegalArgumentException(WRONG_NUMBER_EXCEPTION);
     }
 
     public boolean isNotThreeDigit(String input){
         return input.length() != LENGTH_LIMIT;
     }
-
-    public void notThreeDigitException(){
-        throw new IllegalArgumentException(NOT_THREE_DIGIT_EXCEPTION);
-    }
     public boolean containsZero(String input){
         return input.contains("0");
-    }
-
-    public void containsZeroException(){
-        throw new IllegalArgumentException(NO_ZERO_EXCEPTION);
     }
 
     public boolean containsSameNumber(String input){
         return input.charAt(FIRST) == input.charAt(SECOND)
                 || input.charAt(SECOND) == input.charAt(THIRD)
                 || input.charAt(THIRD) == input.charAt(FIRST);
-    }
-
-    public void containsSameNumberException(){
-        throw new IllegalArgumentException(SAME_NUMBER_EXCEPTION);
     }
 }
