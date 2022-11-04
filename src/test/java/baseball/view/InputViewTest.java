693e8f8f8b7b1cf150e4c813c6fad7559fd4e1a4
ksh-beginner <ohk9134@naver.com>
2022-11-04 10:17:35 +0900

    test(InputViwTest) : 사용자가 입력한 수 3자리가 아니면 예외 처리 테스트

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -18,4 +18,14 @@ public class InputViewTest {
         String playerInputNumber = InputView.inputNumber();
         assertThat(playerInputNumber).isEqualTo(input);
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"23", "1", "2345", "12345235"})
+    void 사용자가_입력한_수_입력_받기_3자리가_아니면_예외_처리(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThatThrownBy(InputView::inputNumber)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] : 입력하는 수는 3자리여야합니다.");
+    }
 }
