be1cd373167154bb9c0810920489f859384972ae
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-04 11:40:28 +0900

    feat : 유저 intention 입력시 오류 처리 기능 구현

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -23,6 +23,9 @@ public class User {
 	public int getIntention() {
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : ");
 		String answer = getInput();
+
+		handleIntentionInputError(answer);
+
 		return Integer.parseInt(answer);
 	}
 
