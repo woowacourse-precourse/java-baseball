f2e7e4d5ad5b150961cbf2b22efa48e08b24ca78
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-04 12:16:25 +0900

    feat(Batter): Batter 객체 생성
    Batter 주요 기능:
    - 유저가 입력한 값을 PlayingNumber에 저장

--- src/main/java/baseball/Batter.java
+++ src/main/java/baseball/Batter.java
@@ -1,2 +1,23 @@
-package baseball;public class Batter {
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
+import java.util.List;
+
+public class Batter {
+	private final PlayingNumber playingNumber;
+
+	public Batter() {
+		Broadcast.printInputMessage();
+		this.playingNumber = new PlayingNumber(readLine());
+	}
+
+	public int getPlayingNumber() {
+		return playingNumber.getPlayingNumber();
+	}
+
+	public List<Integer> getPlayingNumbers() {
+		return playingNumber.getPlayingNumbers();
+	}
+
 }
