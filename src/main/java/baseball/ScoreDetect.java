b244be85a7eb92aa1cabb1a1ba191331f4783eaa
raycho <raycho521@yonsei.ac.kr>
2022-11-04 19:08:45 +0900

    feat (baseball) : strike score detect
    2022.11.04 by Ray
    스트라이크 테스트(StrikeTest.class)를 바탕으로 스트라이크 점수 계산 코드 작성

--- src/main/java/baseball/ScoreDetect.java
+++ src/main/java/baseball/ScoreDetect.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ScoreDetect {
 
@@ -14,9 +16,22 @@ public class ScoreDetect {
             List<Integer> answer,
             List<Integer> numbers
     ) {
-        if (numbers.size() != 3)
+        if (answer.size() != 3
+                || numbers.size() != 3
+                || !checkDuplicate(answer)
+                || !checkDuplicate(numbers)
+                || !checkAllowedRange(answer)
+                || !checkAllowedRange(numbers))
             return -1;
-        return 0;
+
+        int count = 0;
+
+        for (int i=0; i<3; i++){
+            if (answer.get(i) == numbers.get(i))
+                count++;
+        }
+
+        return count;
     }
 
     public int getBallCount (
@@ -27,4 +42,28 @@ public class ScoreDetect {
             return -1;
         return 0;
     }
+
+
+    /**
+     * 중복검사
+     */
+    private boolean checkDuplicate(List<Integer> numList) {
+        Set<Integer> numSet = new HashSet<>(numList);
+        if (numSet.size() == numList.size())
+            return true;
+        else
+            return false;
+    }
+
+    /**
+     * 범위(1~9)를 넘어가는 숫자 검사
+     */
+    private boolean checkAllowedRange(List<Integer> numList) {
+        for(int i=0; i<numList.size(); i++){
+            if(numList.get(i) > 9 ||numList.get(i) < 1)
+                return false;
+        }
+        return true;
+    }
+
 }
