6a992160d570714eaa983ff0f3f36ffad0955081
hwi215 <buc06137@naver.com>
2022-11-05 02:21:19 +0900

    feat: [기능4] 출력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -64,6 +64,20 @@ public class Application {
         }
 
 
+        if(ball == 3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+        else if(nothing == 3) {
+            System.out.println("낫싱");
+        }else if(ball == 0 && strike != 0){
+            System.out.println(strike + "스트라이크");
+        }else if(ball != 0 && strike == 0){
+            System.out.println(ball + "볼");
+        }else{
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+
         return result;
     }
 
