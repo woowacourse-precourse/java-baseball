9b7cd3a65d0bf6f1105fa0cb59212fb73d9f0770
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-11-04 21:33:09 +0900

    fix(GameOutput): 볼, 스트라이크, 낫싱을 if문으로 나누어 조건에 맞게 출력

--- src/main/java/baseball/view/GameOutput.java
+++ src/main/java/baseball/view/GameOutput.java
@@ -1,15 +1,21 @@
 package baseball.view;
 
 public class GameOutput {
-    public static void Strike(){
-        System.out.println("스트라이크");
+    public static void scoreMessage(int BallNum, int StrikeNum){
+        if (BallNum>0 && StrikeNum>0){
+            System.out.println(BallNum+"볼"+StrikeNum+"스트라이크");
+        }
+        if (BallNum==0 && StrikeNum!=0){
+            System.out.println(StrikeNum+"스트라이크");
+        }
+        if (BallNum!=0 && StrikeNum==0){
+            System.out.println(BallNum+"볼");
+        }
+        if (BallNum==0 && StrikeNum==0){
+            System.out.println("낫싱");
+        }
     }
-    public static void Ball(){
-        System.out.println("볼");
-    }
-    public static void Nothing(){
-        System.out.println("낫싱");
-    }
+
     public static void finishGame(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
