8ec22be2583f080c45f2bdcb0610d8886aafbbe0
GyuJeGal <lkjdee0329@gmail.com>
2022-11-04 18:55:52 +0900

    각 자리 수가 서로 다른 세자리 수를 리스트로 반환하는 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,39 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.*;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import org.assertj.core.api.Assertions;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        
+        int nextProgress = 1;  //숫자 야구 게임을 재시작 또는 종료를 판단하는 변수
+        while (nextProgress == 1) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+
+            List<Integer> computer = getRandomInteger();
+
+
+        }
+        System.out.println("게임 종료");
     }
+    
+    private static List<Integer> getRandomInteger() {
+        List<Integer> result = new ArrayList<>();
+        while (result.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            //서로 다른 3자리수를 저장
+            if (!result.contains(randomNumber)) {
+                result.add(randomNumber);
+            }
+        }
+        return result;
+    }
+
 }
