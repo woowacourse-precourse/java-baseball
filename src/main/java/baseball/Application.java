7c4b5d6107ffdb08478f8da6789ebade6f69c8df
JaeUpSu <hyeongs2323@gmail.com>
2022-11-04 17:53:11 +0900

    getTargetNumbers() | 세 개의 랜덤 목표 숫자 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    private static final int SIZE = 3;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    private static List<Integer> getTargetNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
 }
