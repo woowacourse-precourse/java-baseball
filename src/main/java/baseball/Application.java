927f4adc81ff417149523d29485b1a8ab72a3a75
YunByungil <dbsquddlfz@naver.com>
2022-11-04 13:42:48 +0900

    feat(HintMessage): 힌트 메세지 클래스 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
-
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        boolean result = false;
+        boolean win = true;
+        ComputerNumber computerNumber = new ComputerNumber();
+        computerNumber.makeRandomNumber();
+        List<Integer> com = computerNumber.getComputerNumber();
+        for (Integer integer : com) {
+            System.out.println("integer = " + integer);
+        }
+
+
+        do {
+            UserNumber userNumber = new UserNumber();
+            userNumber.makeUserNumber();
+            List<Integer> user = userNumber.getUserNumber();
+            if (com.equals(user)) {
+                System.out.println("승리");
+                win = false;
+            }
+        }while (win);
     }
 }
---
+++ src/main/java/baseball/HintMessage.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class HintMessage {
+    final static String STRIKE = "스트라이크";
+    final static String BALL = "볼";
+    final static String NOTHING = "낫싱";
+}
