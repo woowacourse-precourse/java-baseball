f8d7f3f883fa5b1be00719ed1ce7f75b693e34d8
SonMinGyu <littleking13@naver.com>
2022-11-04 21:49:12 +0900

    [FEAT] 잘못된 입력값 처리 기능 추가
    - 잘못된 입력값 입력시 IllegalArgumentException throw
    - 처리한 잘못된 입력값은 아래와 같음
        1. 문자가 포함된 입력값
        2. 3자리가 넘어간 입력값
        3. 같은 숫자를 가진 입력값

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,9 +18,13 @@ public class Application {
             }
         }
 
-
         String userNumberString = Console.readLine();
         String[] userNumberStringList = userNumberString.split("");
+
+        if (isWrongInput(userNumberStringList)) {
+            throw new IllegalArgumentException("잘못된 입력값을 입력하셨습니다.");
+        }
+
         List<Integer> user = listToArrayList(userNumberStringList);
 
 
@@ -35,4 +39,49 @@ public class Application {
 
         return userNumberIntegerList;
     }
+
+    public static boolean isWrongInput (String[] userNumberStringList) {
+        if (
+                checkIntegerOrCharacter(userNumberStringList)
+                        || checkInputLength(userNumberStringList)
+                        || checkSameNumbers(userNumberStringList)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean checkIntegerOrCharacter(String[] userNumberStringList) {
+        for (int i = 0; i < userNumberStringList.length; i++) {
+            if (!Character.isDigit(userNumberStringList[i].charAt(0))) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean checkInputLength(String[] userNumberStringList) {
+        if (userNumberStringList.length > 3 || userNumberStringList.length < 1) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean checkSameNumbers(String[] userNumberStringList) {
+        List<String> haveNumberList = new ArrayList<>();
+
+        for (int i = 0; i < userNumberStringList.length; i++) {
+            if (haveNumberList.contains(userNumberStringList[i])) {
+                return true;
+            }
+            else {
+                haveNumberList.add(userNumberStringList[i]);
+            }
+        }
+
+        return false;
+    }
+
 }
