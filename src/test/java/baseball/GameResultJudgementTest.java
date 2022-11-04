66a66c0d1bd826b8c52c52ef25c632eed2a2b6c3
choiejae <ejae8319@gmail.com>
2022-11-04 17:57:44 +0900

    test: strike 3 테스트 생성

--- src/test/java/baseball/GameResultJudgementTest.java
+++ src/test/java/baseball/GameResultJudgementTest.java
@@ -2,7 +2,10 @@ package baseball;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 class GameResultJudgementTest {
@@ -10,10 +13,15 @@ class GameResultJudgementTest {
   GameResultJudgement judge;
   @BeforeEach
   void GameResultJudgmentCreate() {
-    judge = new GameResultJudgement();
-  }
-  @Test
-  void GameResultJudgmentCreateWithMaxLen() {
     judge = new GameResultJudgement(3);
+  }
+
+  @Test
+  @Order(1)
+  @DisplayName("스트라이크 3 확인")
+  void JudgeStrikeCount3() {
+    assertEquals(3, judge.judgeStrike("123", "123"));
+    assertEquals(3, judge.judgeStrike("456", "456"));
+    assertEquals(3, judge.judgeStrike("789", "789"));
   }
 }
