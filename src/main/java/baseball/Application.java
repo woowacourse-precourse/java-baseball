743d689afb8bfe7eaf8c16eb79749808761b4aeb
HubCreator <chocomilk8604@gmail.com>
2022-11-04 10:52:01 +0900

    refactor : Game 클래스로 doGame()로직 이동

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,35 +1,11 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.List;
-
-import static baseball.ConstVariable.*;
 
 public class Application {
     public static void main(String[] args) {
-        String userKeyInput = "1";
-
-        while (userKeyInput.equals("1")) {
-            doGame(RandomUtil.makeRandomComputerBalls());
-            userKeyInput = Console.readLine();
-        }
-    }
-
-
-    private static void doGame(Balls computer) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        Result result = new Result();
-
-        while (!result.isGameOver()) {
-            System.out.print("숫자를 입력해주세요 : ");
-            List<Integer> userNums = ValidationUtil.mapStringToList(Console.readLine());
-            result = computer.play(userNums);
-
-            System.out.println(result);
-        }
-
-        System.out.print(VALID_CNT + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        Game game;
+        do {
+            game = new Game(RandomUtil.makeRandomComputerBalls());
+        } while (game.isNotOver());
     }
 }
---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,40 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+import static baseball.ConstVariable.VALID_CNT;
+
+public class Game {
+    private boolean isNotOver = true;
+
+    public Game(Balls computer) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        Result result = new Result();
+
+        while (!result.isGameOver()) {
+            System.out.print("숫자를 입력해주세요 : ");
+            List<Integer> userNums = ValidationUtil.mapStringToList(Console.readLine());
+            result = computer.play(userNums);
+
+            System.out.println(result);
+        }
+
+        System.out.print(VALID_CNT + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String userKeyInput = Console.readLine();
+        if (userKeyInput.equals("2")) {
+            setIsNotOver(false);
+        }
+    }
+
+    public boolean isNotOver() {
+        return isNotOver;
+    }
+
+    private void setIsNotOver(boolean bool) {
+        isNotOver = bool;
+    }
+}
