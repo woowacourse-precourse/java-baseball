213c0f554564030012399bbb829a45ba05d32737
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-04 12:10:34 +0900

    feat(Broadcast): Broadcast 객체 생성
    Broadcast 주요 기능:
    - printStartMessage 메서드
    - printInputMessage 메서드
    - printResultMessage 메서드 : referee의 게임 결과 메시지를 출력
    - printEndMessage 메서드
    - printRestart 메서드

---
+++ src/main/java/baseball/Broadcast.java
@@ -0,0 +1,28 @@
+package baseball;
+
+public class Broadcast {
+	private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+	private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+	private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+	static void printStartMessage() {
+		System.out.println(START_MESSAGE);
+	}
+
+	static void printInputMessage() {
+		System.out.print(INPUT_MESSAGE);
+	}
+
+	static void printResultMessage(String resultMessage) {
+		System.out.println(resultMessage);
+	}
+
+	static void printEndMessage() {
+		System.out.println(END_MESSAGE);
+	}
+
+	static void printRestartMessage() {
+		System.out.println(RESTART_MESSAGE);
+	}
+}
