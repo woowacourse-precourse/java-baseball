f18bf40aa46f0c0ab12b7044682dd30c582f37a6
youngkyunIm <yee0230@naver.com>
2022-11-04 22:34:28 +0900

    fix(main) : Coin grobal->local

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,12 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
-    public static String COIN = "1";
-
     public static void main(String[] args) {
-
+        String COIN = "1";
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         while (COIN.equals("1")) {
@@ -19,7 +14,7 @@ public class Application {
 
             game(computer.getComputer());
 
-            inputCoin();
+            COIN = inputCoin();
         }
     }
 
@@ -41,10 +36,10 @@ public class Application {
         }
     }
 
-    private static void inputCoin() {
+    private static String inputCoin() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         Coin coin = new Coin(readLine());
-        COIN = coin.getCoin();
+        return coin.getCoin();
     }
 
     private static int countStrike(List<Integer> computer, List<Integer> user) {
