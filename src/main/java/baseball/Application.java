fdde62d2a36587ebbe6a383810202cae34a6ef74
mydreamis-18 <hitoryok@naver.com>
2022-11-04 18:13:12 +0900

    [feat] getPlayerNumbers() 함수 수정 - Console 라이브러리 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,46 +1,48 @@
 package baseball;
 
 import java.util.List;
-import java.util.Arrays;
-import java.util.Scanner;
 import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+
+	static final int LENGTH = 3;
 
 	public static void main(String[] args) {
 
 		startGame();
 
-		List<String> playerNumbers = getPlayerNumbers();
 		List<Integer> computerNumbers = getComputerNumbers();
+		List<Integer> playerNumbers = getPlayerNumbers();
 		
-		System.out.println(playerNumbers);
 		System.out.println(computerNumbers);
+		System.out.println(playerNumbers);
 	}
 
 	public static List<Integer> getComputerNumbers() {
-		
+
 		List<Integer> computer = new ArrayList<>();
-		while (computer.size() < 3) {
-			
-		    int randomNumber = Randoms.pickNumberInRange(1, 9);
-		    if (!computer.contains(randomNumber)) {
-		        computer.add(randomNumber);
-		    }
+		while (computer.size() < LENGTH) {
+
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+			if (!computer.contains(randomNumber)) {
+				computer.add(randomNumber);
+			}
 		}
 		return computer;
 	}
-	
-	public static List<String> getPlayerNumbers() {
 
-		Scanner scin = new Scanner(System.in);
-		
-		String numbers = scin.next();
-		List<String> numberList = Arrays.asList(numbers.split(""));
-		
-		scin.close();
-		return numberList;
+	public static List<Integer> getPlayerNumbers() {
+
+		List<Integer> playerNumbers = new ArrayList<Integer>(LENGTH);
+
+		for (int count = 0; count < LENGTH; count++) {
+
+			Integer number = Integer.parseInt(Console.readLine());
+			playerNumbers.add(number);
+		}
+		return playerNumbers;
 	}
 
 	public static void startGame() {
