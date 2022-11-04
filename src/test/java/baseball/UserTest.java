ef891a37af689b72c2603b4e4eec120562009dd6
dmdrk1414 <seungchan141414@gmail.com>
2022-11-04 22:46:04 +0900

    :white_check_mark:[Test] user의 입력이 제대로 들어갔는가 확인하는 테스트

--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -1,6 +1,35 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import model.User;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
+
+    private static User user;
+
+    @BeforeAll
+    static void initAll() {
+        user = new User();
+    }
+
+    @Test
+        // 입력을 제대로 받았는지 확인
+    void 문자_입력_테스트() throws Exception {
+        //given
+        User user = new User();
+        String input = "123";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        // when
+        int result = user.getInputAllRightNumOfUser();
+        // then
+        assertThat(result).isEqualTo(123);
+    }
 
 //    @Test
 //    void 문자열을_숫자로_변환_case_1() {
