091e53f8d76cffa67147bb09ec7ba4703f7530a2
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-04 16:56:45 +0900

    test(Input) : 입력받은 숫자 예외 처리 테스트
    - 중복 숫자 예외 테스트
    - 자리수 예외 테스트
    - 음수 예외 테스트

---
+++ src/test/java/baseball/exception/NumberExceptionTest.java
@@ -0,0 +1,45 @@
+package baseball.exception;
+
+import baseball.Application;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static baseball.exception.ExceptionMessage.*;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class NumberExceptionTest extends NsTest {
+
+    @Test
+    void 중복_숫자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("333"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(DUPLICATE_DIGIT_EXCEPTION.message())
+        );
+    }
+
+    @Test
+    void 글자수_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12345"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(INVALID_DIGIT_EXCEPTION.message())
+        );
+    }
+
+
+    @Test
+    void 음수_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-123"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(NEGATIVE_DIGIT_EXCEPTION.message())
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
