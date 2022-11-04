8e1d324da074086d04cd8a4fbc8c0787dff43592
Hyes-y <gptjs7970@naver.com>
2022-11-05 02:54:19 +0900

    feat: [2] 입력값 유효성 검사 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,19 @@ import java.util.Arrays;
 import java.util.HashSet;
 
 public class Application {
+    // TODO: [2] 입력값 유효성 검사 기능
+    public static boolean isValid(String sth) {
+        if (!sth.matches("[1-9]{3}")) {
+            return false;
+        }
+        String[] charArr = sth.split("");
+        HashSet<String> charSet = new HashSet<String>(Arrays.asList(charArr));
+        if (charSet.size() != sth.length()) {
+            return false;
+        }
+        return true;
+    }
+
     // TODO: [1] 사용자 입력 및 게임 재시작/종료 기능 구현
     public static boolean game() {
         String answer = "123";
@@ -13,9 +26,9 @@ public class Application {
         while (true) {
             System.out.println("숫자를 입력해주세요 : ");
             String inputString = Console.readLine();
-//            if (!isValid(inputString)) {
-//                throw new IllegalArgumentException("1-9 사이의 서로 다른 숫자 3개를 공백없이 입력해주셔야 합니다.");
-//            }
+            if (!isValid(inputString)) {
+                throw new IllegalArgumentException("1-9 사이의 서로 다른 숫자 3개를 공백없이 입력해주셔야 합니다.");
+            }
 
             if (answer.equals(inputString)) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
