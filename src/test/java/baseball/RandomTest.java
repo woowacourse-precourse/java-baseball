31539b89ede5571d1e169caf9690bb9abe826637
Jimin <thswlals0219@gmail.com>
2022-11-04 22:40:37 +0900

    test: 랜덤 수 생성 테스트 코드 수정

--- src/test/java/baseball/RandomTest.java
+++ src/test/java/baseball/RandomTest.java
@@ -2,27 +2,31 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
+import baseball.util.InputValueValidation;
+import baseball.util.RandomUtil;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class RandomTest extends NsTest {
-    @Test
-    void ThrowTheBallTest() {
-        // given
-        int ballNumberLength = 3;
-        Computer computer = new Computer();
+public class RandomTest {
 
-        // when
-        computer.throwTheBall();
-
-        // then
-
-        assertThat(computer.getBallNumber().stream().distinct().count())
-                .isEqualTo(ballNumberLength);
+    @Nested
+    class PickRandomNumberTest {
+        @Test
+        void case1() {
+            InputValueValidation inputValueValidation = new InputValueValidation();
+            String number = String.valueOf(RandomUtil.pickRandomNumber());
+            assertThat(inputValueValidation.isNumber(number)).isEqualTo(true);
+        }
     }
 
-    @Override
-    protected void runMain() {
-
+    @Nested
+    class PickNumbersTest {
+        @Test
+        void case1() {
+            int ballNumberLength = 3;
+            assertThat(RandomUtil.pickNumbers().stream().distinct().count())
+                    .isEqualTo(ballNumberLength);
+        }
     }
+
 }
