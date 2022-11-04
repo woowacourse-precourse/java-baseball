c9f5d641abfe9a9ad0f42c210e919626fd97f358
taeyun-work <wmfajr1357@gmail.com>
2022-11-04 16:20:37 +0900

    refactor: BaseBallGame 클래스 추가 및 수정
    기존에는 모든 기능을 구현 후 리팩토링 할 생각이었지만,
    처음부터 기능 구현하면서 동시에 코드를 분리하며 진행하려함.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,45 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Application {
+    public static class BaseBallGame{
+        private static BaseBallGame gameProgram = new BaseBallGame();
+        private BaseBallGame() {}
+
+        public static BaseBallGame getGame() { return gameProgram; }
+
+        public void gameStart () {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            inputNumber();
+        }
+        public int inputNumber(){
+            System.out.print("숫자를 입력해주세요 : ");
+            String inputNumber = Console.readLine();
+            String inputNumberRegex = "^[1-9]{3}$";
+            HashSet duplRemoveList = new HashSet(Arrays.asList(inputNumber.split("")));
+            ArrayList<String> list = new ArrayList<>(duplRemoveList);
+            String inputValidNumber = String.join("", list);
+
+            System.out.println(inputValidNumber);
+
+            if(!inputValidNumber.matches(inputNumberRegex)){
+                System.out.println("Bad");
+                // Error 처리 하기!
+            }
+            return Integer.parseInt(inputNumber);
+        }
+
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-    }
+        BaseBallGame baseballGame = BaseBallGame.getGame();
 
-    public static int inputNumber(){
-        String inputNumber = Console.readLine();
-        return Integer.parseInt(inputNumber);
+        while(true){
+            baseballGame.gameStart();
+        }
+
     }
 }
