52ddb952902050c640c96b377e46706ded5c8cd2
윤서영 <yoonseoyoung@yunseoyeong-ui-MacBookPro.local>
2022-11-04 21:15:04 +0900

    FEAT : 랜덤으로 서로 다른 임의의 수 생성하기

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -3,10 +3,13 @@ package baseball;
 import java.util.List;
 
 public class Controller {
-    List<Integer> answerNum;
-    List<Integer> inputNum;
+    final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    Service service = new Service();
 
-    public void game() {
-
+    public void startGame() {
+        System.out.println("START_MESSAGE = " + START_MESSAGE);
+        List<Integer> answerNum = service.setAnswerNum();
     }
+
 }
