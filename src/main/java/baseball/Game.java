2292e19e6ddf34207e4e54370c910ce640c3ea0d
sen <seeunchoi99@gmail.com>
2022-11-04 16:40:30 +0900

    ✨ Game.start 구현
    게임 시작 시 computer 초기화

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class Game {
+    private Computer computer;
+
+    private void start() {
+        computer = new Computer();
+        play();
+    }
+
+    private void play() {
+    }
+}
