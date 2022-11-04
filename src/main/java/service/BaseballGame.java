26b72c4abce02de112a5528a266be70d2164441b
MyunghyunNero <mh1kim0910@gmail.com>
2022-11-05 03:29:35 +0900

    feat : [숫자 야구 게임] 볼이랑 스트라이크 개수 구하는 메서드 구현

--- src/main/java/service/BaseballGame.java
+++ src/main/java/service/BaseballGame.java
@@ -7,6 +7,8 @@ import repository.ComputerRepository;
 import repository.UserRepository;
 import util.Utils;
 
+import java.util.List;
+
 public class BaseballGame {
 
     private ComputerRepository computerRepository;
@@ -35,9 +37,23 @@ public class BaseballGame {
         try{
             String userinput= Console.readLine();
             userRepository.inputanswer(userinput);
+            countBallAndStrike();
             return;
         }catch (IllegalArgumentException e){
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void countBallAndStrike() {
+        List<Integer> user = userRepository.getUserData();
+        int index = 1;
+        for (int i = 0; i < Utils.Numlen; i++) {
+            if (computerRepository.getDataIndex(user.get(i)) == index) {
+                strike++;
+            } else if (computerRepository.getDataIndex(user.get(i)) != 0) {
+                ball++;
+            }
+            index++;
         }
     }
 }
