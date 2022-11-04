5696a51e2e28a583083a44928450bbf9ed1ecde4
ycopp9 <aciees95@gmail.com>
2022-11-05 00:22:31 +0900

    test(BaseballGame): write the test code for BaseballGame.printResult()
    Write the test code for BaseBallGame.printResult() that prints result of
    the baseballgame for given format.

--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -1,8 +1,14 @@
 package baseball;
 
+import com.sun.jdi.ByteType;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -23,15 +29,54 @@ public class BaseballGameTest {
     void check_user_number_test() {
         assertThat(game.checkUserNumber("123")).isEqualTo(List.of(1, 2, 3));
         assertThat(game.checkUserNumber("359")).isEqualTo(List.of(3, 5, 9));
-        assertThatThrownBy(() -> game.checkUserNumber("3456"))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> game.checkUserNumber("61"))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> game.checkUserNumber("ab2"))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> game.checkUserNumber("224"))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> game.checkUserNumber("204"))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> game.checkUserNumber("3456")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> game.checkUserNumber("61")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> game.checkUserNumber("ab2")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> game.checkUserNumber("224")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> game.checkUserNumber("204")).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Nested
+    class BaseballGamePrintTest {
+        OutputStream captor;
+        PrintStream standardOut = System.out;
+        Computer computer;
+
+        @BeforeEach
+        public void beforeEach() {
+            captor = new ByteArrayOutputStream();
+            System.setOut(new PrintStream(captor));
+            computer = new Computer();
+        }
+
+        @AfterEach
+        public void afterEach() {
+            System.setOut(standardOut);
+        }
+
+        @Test
+        void print_result_strike_and_ball_test() {
+            game.printResult(computer.new GameResult(1, 2));
+            assertThat(captor.toString().trim()).isEqualTo("2볼 1스트라이크");
+        }
+
+        @Test
+        void print_result_only_strike_test() {
+            game.printResult(computer.new GameResult(3, 0));
+            assertThat(captor.toString().trim()).isEqualTo("3스트라이크");
+        }
+
+        @Test
+        void print_result_only_ball_test() {
+            game.printResult(computer.new GameResult(0, 2));
+            assertThat(captor.toString().trim()).isEqualTo("2볼");
+        }
+
+        @Test
+        void print_result_nothing_test() {
+            game.printResult(computer.new GameResult(0, 0));
+            assertThat(captor.toString().trim()).isEqualTo("낫싱");
+        }
+
     }
 }
