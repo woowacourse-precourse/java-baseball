2ea95de233b8925b607e0978a617f6d14969047a
JH-web115 <78305338+JH-web115@users.noreply.github.com>
2022-11-04 17:11:33 +0900

    feat: add Game start
    feat: add Game start
    
    add "startGame" function to start game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,6 +9,18 @@ public class Application {
         // TODO: 게임 시작 호출
     }
 
+    public static void startGame() {
+        String answer; // TODO: 3자리 숫자 랜덤 생성 호출
+        boolean isAnswer = false;
+
+        while (!isAnswer) {
+            // TODO: 입력 호출
+            // TODO: 정답 비교 호출
+        }
+
+        // TODO: 게임 종료 호출
+    }
+
     public static void main(String[] args) {
         startApp();
     }
