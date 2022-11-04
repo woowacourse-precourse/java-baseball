c973b26db0a98db795177b6ac2053341dcfc55f7
cuttlefish55 <funade7@gmail.com>
2022-11-04 18:17:19 +0900

    feat: Add result printer

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -23,6 +23,20 @@ public class Application {
             player = GeneratePlayerNumbers();
             strike = CheckStrike(computer, player);
             ball = CheckBall(computer, player) - strike;
+            PrintResult(strike, ball);
+        }
+    }
+
+    public static void PrintResult(int strike, int ball){
+        if(strike == 0 && ball == 0){
+            System.out.println("낫싱");
+            return;
+        }
+        if(ball > 0){
+            System.out.print(ball + "볼 ");
+        }
+        if(strike > 0){
+            System.out.print(strike + "스트라이크");
         }
     }
     public static int CheckStrike(List<Integer> computer, List<Integer> player){
