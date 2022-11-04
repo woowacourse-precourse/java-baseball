d8151712651047fc4de7c05b387c764f798e150e
situodys <situodys@gmail.com>
2022-11-04 12:37:37 +0900

    test(Baseballs): 볼인 경우 개수 테스트

--- src/test/java/baseball/baseballs/LogicTest.java
+++ src/test/java/baseball/baseballs/LogicTest.java
@@ -15,6 +15,7 @@ public class LogicTest {
 
     private static Stream<Arguments> provideBaseballInputsAndStrikeCounts() {
         return Stream.of(
+                //answerInput, userInput, strikeCounts
                 Arguments.of(String.valueOf(123), String.valueOf(123), 3),
                 Arguments.of(String.valueOf(123), String.valueOf(124), 2),
                 Arguments.of(String.valueOf(123), String.valueOf(189), 1),
@@ -23,6 +24,21 @@ public class LogicTest {
                 Arguments.of(String.valueOf(123), String.valueOf(321), 1),
                 Arguments.of(String.valueOf(123), String.valueOf(567), 0),
                 Arguments.of(String.valueOf(123), String.valueOf(312), 0)
+        );
+    }
+
+    private static Stream<Arguments> provideBaseballInputsAndBallCounts() {
+        return Stream.of(
+                //answerInput, userInput, ballCounts
+                Arguments.of(String.valueOf(123), String.valueOf(123), 0),
+                Arguments.of(String.valueOf(456), String.valueOf(456), 0),
+                Arguments.of(String.valueOf(123), String.valueOf(189), 1),
+                Arguments.of(String.valueOf(123), String.valueOf(412), 2),
+                Arguments.of(String.valueOf(123), String.valueOf(312), 3),
+                Arguments.of(String.valueOf(159), String.valueOf(561), 0),
+                Arguments.of(String.valueOf(159), String.valueOf(923), 1),
+                Arguments.of(String.valueOf(159), String.valueOf(921), 2),
+                Arguments.of(String.valueOf(237), String.valueOf(372), 3)
         );
     }
 
@@ -38,5 +54,19 @@ public class LogicTest {
         //then
         assertThat(result).isInstanceOf(Result.class);
         assertThat(result.countStrike()).isEqualTo(strikeCount);
+    }
+
+    @ParameterizedTest
+    @DisplayName("다른 위치에서 값이 일치하는 경우 볼")
+    @MethodSource("provideBaseballInputsAndBallCounts")
+    public void test2(String answerInput, String userInput, int ballCount) throws Exception{
+        //given
+        Baseballs answerBaseballs = Baseballs.of(answerInput);
+        Baseballs userBaseballs = Baseballs.of(userInput);
+        //when
+        Result result = answerBaseballs.calculateResult(userBaseballs);
+        //then
+        assertThat(result).isInstanceOf(Result.class);
+        assertThat(result.countBall()).isEqualTo(ballCount);
     }
 }
