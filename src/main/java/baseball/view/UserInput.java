194501201109cc4a7bcef931e072b37ccb993586
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-11-04 12:48:33 +0900

    feat(view/UserInput): User에게 보여질 view 문구 생성

--- src/main/java/baseball/view/UserInput.java
+++ src/main/java/baseball/view/UserInput.java
@@ -1,4 +1,10 @@
 package baseball.view;
 
 public class UserInput {
+    public static void getUserNumber() {
+        System.out.println("1에서 9까지의 수 3개를 입력하세요");
+    }
+    public static void getRetryUserNumber(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
