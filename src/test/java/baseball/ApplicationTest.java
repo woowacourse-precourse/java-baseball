8417818b8c0959429b0b716ccc4b0a693c6f5e24
yg0585 <yg0585@naver.com>
2022-11-04 19:24:18 +0900

    style: 사용하지 않는 import 제거

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,9 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import java.util.ArrayList;
 import java.util.List;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
