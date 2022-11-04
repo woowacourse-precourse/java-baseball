ddadc4142d016dc8dd352a0381b499ffee53784a
ddubidubap <park62000@gmail.com>
2022-11-05 00:04:36 +0900

    feat: 답이 올바른 형식으로 작성됐는지 확인
    숫자로만 이루어져 있지 않거나 글자 수가 3개가 아니라면
    IllegalArgumentException 처리하고 프로그램 종료

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -2,4 +2,5 @@ package baseball;
 
 public class Constant {
     public static String WRITE_ANSWER = "숫자를 입력해주세요 : ";
+    public static String GAME_FINISH = "게임 종료";
 }
--- src/main/java/baseball/Guess.java
+++ src/main/java/baseball/Guess.java
@@ -2,11 +2,25 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static baseball.Constant.GAME_FINISH;
 import static baseball.Constant.WRITE_ANSWER;
 
 public class Guess {
     public static String takeGuess() {
         System.out.print(WRITE_ANSWER);
         return Console.readLine();
+    }
+
+    public static void checkGuess(String input) throws IllegalArgumentException {
+        // 입력값이 숫자로만 이루어져 있는지 확인
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(GAME_FINISH);
+        }
+
+        // 입력값이 세 글자인지 확인
+        if (input.length() != 3)
+            throw new IllegalArgumentException(GAME_FINISH);
     }
 }
