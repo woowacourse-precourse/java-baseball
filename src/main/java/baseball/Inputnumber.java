36c1896d0bc91f772b5c4afe022d8a48cad44522
eugene1204 <s1061001@naver.com>
2022-11-04 20:21:01 +0900

    refactor: Inputnumber클래스 생성자 수정

--- src/main/java/baseball/Inputnumber.java
+++ src/main/java/baseball/Inputnumber.java
@@ -1,53 +1,49 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Inputnumber {
-    String arg;
+    String input;
 
     Inputnumber(){
-
-    }
-    Inputnumber(String[] args){
-        for(String arg : args){
-           this.arg +=arg;
-        }
+        String input = Console.readLine();
+        this.input =input;
     }
 
     public List<Integer> stringTointList(){
         List<Integer> argList = new ArrayList<>();
-        String[] argsTemp = arg.split("");
+        String[] argsTemp = input.split("");
         for(String args: argsTemp) argList.add(Integer.valueOf(args));
         return argList;
     }
 
     public boolean is1to10(){
-        for(int i =0; i<arg.length();i++){
-            int checkNumber = Integer.valueOf(arg.substring(i));
+        for(int i =0; i<input.length();i++){
+            int checkNumber = Integer.valueOf(input.substring(i));
             if(checkNumber<1 || checkNumber>9) return false;
         }
         return true;
     }
 
     public boolean isdifferentNumber(){ //123
-        String sub = arg.substring(0);
-        String compare = arg.substring(1);
+        String sub = input.substring(0);
+        String compare = input.substring(1);
         if(sub.equals(compare)) return false;
 
-        compare = arg.substring(2);
+        compare = input.substring(2);
         if(sub.equals(compare)) return false;
 
-        sub = arg.substring(1);
-        compare = arg.substring(2);
+        sub = input.substring(1);
+        compare = input.substring(2);
         if(sub.equals(compare)) return false;
         return true;
     }
 
     public boolean isNumber(){
-        return arg.chars().allMatch(Character::isDigit);
+        return input.chars().allMatch(Character::isDigit);
     }
     public void inputCheck(){
         if(!is1to10()) new IllegalArgumentException("1부터 9사이의 값이 아닙니다.");
