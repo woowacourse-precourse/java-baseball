f7ad0e8623bdcb2849b23076cece6a90ac33521e
Devtist <yt930310@snu.ac.kr>
2022-11-04 23:28:10 +0900

    feat: 유효한 숫자들의 리스트 상수 선언

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -1,8 +1,11 @@
 package baseball;
 
+import java.util.List;
+
 public abstract class Constant {
-	public static final int NUMBER_AMOUNT = 3;
+	public static final int LENGTH_OF_NUMBER = 3;
 
 	public static final int RESTART_GAME = 1;
 	public static final int END_GAME = 2;
+	public static final List<Integer> VALID_NUMBERS = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
 }
