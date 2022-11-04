ae3bc8f8e75664b4d267eb67186f6896c583a9d9
situodys <situodys@gmail.com>
2022-11-04 17:12:25 +0900

    feat(Utils): 게임 재시작 여부 입력 메서드 구현

--- src/main/java/baseball/Utils.java
+++ src/main/java/baseball/Utils.java
@@ -5,7 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class Utils {
 
     public static String getPlayerInputForGenerateAnswerByConsole() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+
+    public static String getPlayerInputForRestartGameByConsole() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
 }
