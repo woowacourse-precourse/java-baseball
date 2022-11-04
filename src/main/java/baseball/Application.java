f0bfe59f0b33dcb75f683698a1b95604754e033c
MIN-04 <kimji_0424@naver.com>
2022-11-04 14:59:45 +0900

    feat: change class Application
    add method validateInputNumber()
    기능 목록 - 3. 숫자야구 게임 구현
    - 3-2. 사용자 입력 구현 (-> "숫자를 입력해주세요 : ")
    - 3-2-1. 사용자가 잘못된 값을 입력했는지 구분(IllegalArgumentException)
            - 숫자를 하나만 입력
            - 숫자 외의 문자 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -17,6 +18,10 @@ public class Application {
             int strike = 0;
             int ball = 0;
             boolean correctFlag = false;
+
+            System.out.print("숫자를 입력해주세요 : ");
+            String inputNumber = Console.readLine();
+            validateInputNumber(inputNumber);
         }
     }
 
@@ -30,4 +35,11 @@ public class Application {
         }
         return computer;
     }
+
+    private static void validateInputNumber(String inputNumber) {
+        if (inputNumber.length() != 3 || !inputNumber.matches("^[1-9]+$")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
