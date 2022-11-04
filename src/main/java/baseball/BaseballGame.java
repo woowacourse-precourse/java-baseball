b8a8637a4670581d729f0c76af2b1ffee6e17ce0
2jun0 <soo28819@gmail.com>
2022-11-04 22:17:46 +0900

    Feat(BaseballGame): 스트라이크, 볼 개수를 구하는 메서드 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,9 +1,28 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+import static baseball.Digits.DIGIT_COUNT;
+
 public class BaseballGame {
-    static final int DIGIT_COUNT = 3;
 
     public BaseballGame() {
-        
+
+    }
+
+    private int getStrikeCount(Digits digits1, Digits digits2) {
+        return (int) IntStream.range(0, DIGIT_COUNT)
+                .filter(idx -> digits1.get(idx) == digits2.get(idx))
+                .count();
+    }
+
+    private int getBallCount(Digits digits1, Digits digits2) {
+        Set<Integer> distinctDigits = new HashSet<>(digits1.getList());
+        distinctDigits.addAll(digits2.getList());
+
+        int distinctDigitsCount = DIGIT_COUNT * 2 - distinctDigits.size();
+        return distinctDigitsCount - getStrikeCount(digits1, digits2);
     }
 }
