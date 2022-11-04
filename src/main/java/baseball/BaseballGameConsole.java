092046345bb91753c358c0b5ed4064e73b14c055
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-05 00:27:34 +0900

    feat(BaseballGameConsole): guide player of game

--- src/main/java/baseball/BaseballGameConsole.java
+++ src/main/java/baseball/BaseballGameConsole.java
@@ -3,9 +3,14 @@ package baseball;
 public class BaseballGameConsole {
 
     public void playGame(Player player) {
-        // Starting guide
+        startingGuide(player.getName());
         do {
             // Play single baseball game
         } while (true); // Ask player to play a new game
+    }
+
+    private void startingGuide(String playerName) {
+        System.out.println(playerName + "반갑습니다.");
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
