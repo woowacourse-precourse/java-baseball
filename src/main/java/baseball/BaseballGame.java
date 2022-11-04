15b49803f8fd68f18f4082dc908c5d6c21d18a7f
jeongwle <dldl4568@naver.com>
2022-11-04 16:34:45 +0900

    feat : 숫자 야구 게임 시작하는 기능
    게임을 시작하는 코드 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,2 +1,32 @@
-package baseball;public class BaseballGame {
+package baseball;
+
+import java.util.List;
+
+public class BaseballGame {
+
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String ERROR_MESSAGE = "잘못된 입력입니다. 게임을 종료합니다.";
+    private boolean onGame;
+    public BaseballGame() {
+        onGame = true;
+    }
+
+    public void startGame() {
+        System.out.println(GAME_START_MESSAGE);
+
+        Computer computer = new Computer();
+        User user = new User();
+
+        while (onGame) {
+            try {
+                user.receiveThreeNumber();
+            } catch (IllegalArgumentException e) {
+                System.out.println(ERROR_MESSAGE);
+                onGame = false;
+                continue;
+            }
+
+            List<Integer> userNumber = user.getThreeNumber();
+        }
+    }
 }
