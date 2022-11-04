7931f07399d1003d879d365a551b8e047c8ff64d
recicoco068 <gkapf838@gmail.com>
2022-11-04 21:21:38 +0900

    test : 게임 종료 및 재시작을 묻는 기능 테스트

--- src/test/java/baseball/UserInterfaceTest.java
+++ src/test/java/baseball/UserInterfaceTest.java
@@ -3,16 +3,69 @@ package baseball;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.InputStreamAssert;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class UserInterfaceTest {
-    @Test
-    void 결과_출력_테스트() {
-        assertThat(UserInterface.resultOutput(3, 0)).isEqualTo(true);
-        assertThat(UserInterface.resultOutput(1, 1)).isEqualTo(false);
-        assertThat(UserInterface.resultOutput(1, 0)).isEqualTo(false);
-        assertThat(UserInterface.resultOutput(0, 1)).isEqualTo(false);
-        assertThat(UserInterface.resultOutput(0, 0)).isEqualTo(false);
+    public static InputStream generateUserInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
+
+    @Nested
+    class 결과_출력_테스트 {
+        @Test
+        void 스트라이크3() {
+            assertThat(UserInterface.resultOutput(3, 0)).isEqualTo(true);
+        }
+
+        @Test
+        void 스트라이크1볼1() {
+            assertThat(UserInterface.resultOutput(1, 1)).isEqualTo(false);
+        }
+
+        @Test
+        void 스트라이크1() {
+            assertThat(UserInterface.resultOutput(1, 0)).isEqualTo(false);
+        }
+
+        @Test
+        void 볼1() {
+            assertThat(UserInterface.resultOutput(0, 1)).isEqualTo(false);
+        }
+
+        @Test
+        void 낫싱() {
+            assertThat(UserInterface.resultOutput(0, 0)).isEqualTo(false);
+        }
+    }
+
+    @Nested
+    class 게임_종료_재시작 {
+        @Test
+        void 게임_재시작() {
+            System.setIn(generateUserInput("1"));
+            boolean result = UserInterface.exitOrRestart();
+            assertThat(result).isFalse();
+        }
+
+        @Test
+        void 게임_종료() {
+            System.setIn(generateUserInput("2"));
+            boolean result = UserInterface.exitOrRestart();
+            assertThat(result).isTrue();
+        }
+
+        @Test
+        void 잘못된_입력_에러() {
+            System.setIn(generateUserInput("aa"));
+            assertThatThrownBy(UserInterface::exitOrRestart).isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
