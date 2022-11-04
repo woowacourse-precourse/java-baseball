45041a20a17a9aa74377b90863557c66c6304c2e
bsh1603 <bsh1603@github.com>
2022-11-04 14:40:03 +0900

    feat: 게임을 다시 하는 기능

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -1,9 +1,13 @@
 package Controller;
 
 import Service.GameService;
+import view.RequestUser;
+import view.SystemMessage;
 
 public class GameController {
     private final int NUM_SIZE = 3;
+    private final int RETRY = 1;
+    private final int EXIT = 2;
 
     GameService gameService = new GameService();
 
@@ -20,5 +24,16 @@ public class GameController {
 
     public void playGame(){
         gameService.play_game();
+    }
+
+    public void endGame(){
+        SystemMessage.end_game_message();
+    }
+
+    public void askContinue(){
+        RequestUser.request_retry();
+        if(Userinput == RETRY){
+            run();
+        }
     }
 }
