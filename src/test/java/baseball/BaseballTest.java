43314fda027f75d319c870bc38a5544e8ba1dc45
chanminmaxscaler <jcm04@naver.com>
2022-11-04 22:57:35 +0900

    test: Computer 클래스의 setComputerRandomNumber, getComputerRandomNumber 메소드를 테스트한다
    setComputerRandomNumber 메소드는 멤버변수 computerNumber에 세 자리 난수로 이뤄진 Ball 객체를 저장한다.
    getComputerRandomNumber 메소드는 멤버변수 computerNumber를 반환한다.
    다음을 테스트한다.
    1. setComputerRandomNumber 메소드를 호출하지 않았을 경우, getComputerRandomNumber 메소드가 null을 반환하는지 확인한다.
    2. setComputerRandomNumber 메소드를 한 번만 호출한 경우, 두 getComputerRandomNumber 메소드가 동일한 computerNumber를 반환하는지 확인한다.
    3. setComputerRandomNumber 메소드를 두 번 호출한 경우, getComputerRandomNumber 메소드가 서로 다른 computerNumber를 반환하는지 확인한다.

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import baseball.domain.Ball;
+import baseball.model.Computer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -10,10 +11,11 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BaseballTest {
     @Nested
-    class Ball_class_test {
+    @DisplayName("Ball 클래스")
+    class Ball_test {
         @Nested
         @DisplayName("validateForm 메소드는")
-        class validateFormTest {
+        class validateForm_test {
             @Nested
             @DisplayName("세 자리 숫자가 입력되지 않았을 때")
             class Context_with_non_three_digit_number {
@@ -21,14 +23,14 @@ class BaseballTest {
                 @DisplayName("IllegalArgumentException을 발생시킨다.")
                 void it_returns_IllegalArgumentException() {
                     String twoDigitNumber = "12";
-                    assertThatThrownBy(()-> new Ball(twoDigitNumber)).isInstanceOf(IllegalArgumentException.class);
+                    assertThatThrownBy(() -> new Ball(twoDigitNumber)).isInstanceOf(IllegalArgumentException.class);
                 }
             }
         }
 
         @Nested
         @DisplayName("validateDuplication 메소드는")
-        class validateDuplicationTest {
+        class validateDuplication_test {
             @Nested
             @DisplayName("중복을 갖는 숫자가 입력되었을 때")
             class Context_with_duplicated_number {
@@ -36,7 +38,7 @@ class BaseballTest {
                 @DisplayName("IllegalArgumentException을 발생시킨다.")
                 void it_returns_IllegalArgumentException() {
                     String duplicatedNumber = "333";
-                    assertThatThrownBy(()-> new Ball(duplicatedNumber)).isInstanceOf(IllegalArgumentException.class);
+                    assertThatThrownBy(() -> new Ball(duplicatedNumber)).isInstanceOf(IllegalArgumentException.class);
                 }
             }
         }
@@ -47,6 +49,54 @@ class BaseballTest {
             String nonDuplicateThreeDigitNumber = "123";
             Ball ball = new Ball(nonDuplicateThreeDigitNumber);
             assertThat(ball.toString()).isEqualTo(nonDuplicateThreeDigitNumber);
+        }
+    }
+
+    @Nested
+    @DisplayName("Computer 클래스")
+    class Computer_test {
+
+        @Nested
+        @DisplayName("getComputerRandomNumber 메소드는")
+        class getComputerRandomNumber_test {
+            @Nested
+            @DisplayName("setComputerRandomNumber를 호출하지 않았을 때")
+            class Context_without_set_method_call {
+                @Test
+                @DisplayName("null을 반환한다.")
+                void it_returns_null() {
+                    Computer computer = new Computer();
+                    assertThat(computer.getComputerRandomNumber()).isNull();
+                }
+            }
+
+            @Nested
+            @DisplayName("setComputerRandomNumber를 한 번 호출하였을 때")
+            class Context_with_set_method_call {
+                @Test
+                @DisplayName("동일한 값을 반환한다.")
+                void it_returns_equal_value() {
+                    Computer computer = new Computer();
+                    computer.setComputerRandomNumber();
+                    Ball computerNumber = computer.getComputerRandomNumber();
+                    assertThat(computer.getComputerRandomNumber()).isEqualTo(computerNumber);
+                }
+            }
+
+            @Nested
+            @DisplayName("setComputerRandomNumber를 두 번 호출하였을 때")
+            class Context_with_two_set_method_calls {
+                @Test
+                @DisplayName("서로 다른 값을 반환한다.")
+                void it_returns_equal_value() {
+                    Computer computer = new Computer();
+                    computer.setComputerRandomNumber();
+                    Ball computerNumber = computer.getComputerRandomNumber();
+
+                    computer.setComputerRandomNumber();
+                    assertThat(computer.getComputerRandomNumber()).isNotEqualTo(computerNumber);
+                }
+            }
         }
     }
 }
