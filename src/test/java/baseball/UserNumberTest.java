7f2096d0be5cacd53e5b61e291ed3d40d7951e7c
Eunkeee <eunkeeeee@gmail.com>
2022-11-04 16:09:00 +0900

    refactor: 테스트 코드 공통 부분 추출

--- src/test/java/baseball/UserNumberTest.java
+++ src/test/java/baseball/UserNumberTest.java
@@ -1,19 +1,26 @@
 package baseball;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserNumberTest {
 
+
+    private UserNumber user;
+
+    @BeforeEach
+    void createUser() {
+        user = new UserNumber();
+    }
+
     @Test
     void canSetUserNumberByTest() {
-        UserNumber user = new UserNumber();
         user.setUserNumber("123");
         System.out.println(user.userNumber);
     }
     @Test
     void isUserNumberLengthEqualsThree() {
-        UserNumber user = new UserNumber();
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             user.setUserNumber("1234");
         });
