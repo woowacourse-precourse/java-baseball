f645278040bef5acaaee937a576a57ffc271ce7e
SHEOMM <esh0821@snu.ac.kr>
2022-11-04 14:45:08 +0900

    feat(HintView): implement hintView that takes hint elements
    유저에게 Hint 모델을 프린트할 HintView class 구현

---
+++ src/main/java/baseball/UI/HintView.java
@@ -0,0 +1,14 @@
+package baseball.UI;
+
+import baseball.model.Hint;
+
+public class HintView {
+    private Hint hint;
+
+    public Hint getHint() {
+        return hint;
+    }
+    public void printHint(){
+        System.out.println(hint);
+    }
+}
