bd74d748edd3c93a1d54edd1cfdf98dee4c0cf0e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-04 23:37:15 +0900

    feat: GameStatusService와 테스트코드 생성

---
+++ src/main/java/baseball/service/GameStatusService.java
@@ -0,0 +1,20 @@
+package baseball.service;
+
+public class GameStatusService {
+
+    private boolean gameStatus;
+
+    public GameStatusService(){}
+
+    public boolean isEndOfGame(String opinion){
+        return false;
+    }
+
+    public void stopGame(){
+
+    }
+
+    public boolean getGameStatus(){
+        return false;
+    }
+}
---
+++ src/test/java/baseball/GameStatusServiceTest.java
@@ -0,0 +1,31 @@
+package baseball;
+import baseball.service.GameStatusService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+public class GameStatusServiceTest {
+
+    GameStatusService gameStatusService;
+
+    @BeforeEach
+    void initializeClass(){
+        gameStatusService = new GameStatusService();
+    }
+
+    @Test
+    void 입력문자열이_2이면_참을_반환(){
+        assertThat(gameStatusService.isEndOfGame("2")).isTrue();
+    }
+
+    @Test
+    void 스탑게임_하면_게임상태_거짓(){
+        gameStatusService.stopGame();
+        assertThat(gameStatusService.getGameStatus()).isFalse();
+    }
+
+    @Test
+    void 스탑게임_안하면_게임상태_참(){
+        assertThat(gameStatusService.getGameStatus()).isTrue();
+    }
+}
