f054021b65bcde3dfe55e344469ec46d3f42a4d1
이경범 <tkdwk567@naver.com>
2022-11-04 19:46:59 +0900

    test : 난수 생성 기능 테스트
    1. 1 ~ 9의 난수 3개가 리스트에 저장이 잘 되는지?
    2. 난수의 범위 가 유효한지?

---
+++ src/test/java/baseball/NumbersGeneratorTest.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NumbersGeneratorTest {
+
+    @Test
+    @DisplayName("1부터 9까지의 난수 숫자 3자리를 생성하는 테스트")
+    void GenerateNumberTest() {
+        // given
+        NumberGenerator generateNumber = new NumberGenerator();
+        List<Integer> randomNumbers = new ArrayList<>();
+
+        // when
+        generateNumber.generate(randomNumbers);
+
+        // then
+        assertThat(randomNumbers.size()).isEqualTo(3);
+        assertThat(randomNumbers.stream()
+                .allMatch(number -> number >= 1 && number <= 9))
+                .isTrue();
+    }
+}
