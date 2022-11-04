b814d7321de9841a16829b4ad5da06c5d4656ad5
Mingeun Park <mindal99546@gmail.com>
2022-11-04 15:32:03 +0900

    feat(잘못된 입력): 지정된 exception을 발생시킨다

--- src/main/java/baseball/GameContext.java
+++ src/main/java/baseball/GameContext.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -8,7 +9,9 @@ import java.util.stream.Collectors;
 /** 게임 흐름을 제어하는 메서드를 제공합니다. */
 public class GameContext {
     private final static int ANSWER_LENGTH = 3;
+    private final static int SELECTOR_LENGTH = 1;
     private String answer;
+    private String userInput;
     private GameState state;
 
     public String getAnswer() {
@@ -39,5 +42,37 @@ public class GameContext {
 
         if (!numbers.contains(randomNumber))
             numbers.add(randomNumber);
+    }
+
+    public void handleUserInput() throws IllegalArgumentException {
+        userInput = Console.readLine();
+
+        if (!isValidityInput(userInput))
+            throw new IllegalArgumentException();
+    }
+
+    private boolean isValidityInput(String userInput) {
+        if (state == GameState.RUNNING && isNDigitNumberInRange(userInput, ANSWER_LENGTH))
+            return true;
+        else if (state == GameState.THREE_STRIKE && isNDigitNumberInRange(userInput, SELECTOR_LENGTH))
+            return true;
+        else
+            return false;
+    }
+
+    private boolean isNDigitNumberInRange(String number, int n) {
+        boolean retVal = true;
+
+        if (number.length() != n)
+            return false;
+
+        for (char c : number.toCharArray())
+            retVal = isNumberInRange(c) && retVal;
+
+        return retVal;
+    }
+
+    private boolean isNumberInRange(char num) {
+        return (num <= '9'&& num >= '1');
     }
 }
--- src/test/java/baseball/MyTest.java
+++ src/test/java/baseball/MyTest.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MyTest {
 
@@ -16,5 +21,25 @@ public class MyTest {
         assertThat(answer).isGreaterThanOrEqualTo(111);
         assertThat(answer).isLessThanOrEqualTo(999);
         assertThat(gameCtx.getState()).isEqualTo(GameState.RUNNING);
+    }
+
+    @Test
+    void 잘못된_값_입력에_대해_예외_발생() {
+        // given
+        GameContext gameCtx = new GameContext();
+        gameCtx.initializeContext();
+        // when
+        String[] invalidInputs = {"12345", "101", "a8c", "1@1asd"};
+        inputNumbersIntoStdin(invalidInputs);
+        // then
+        for (String input : invalidInputs)
+            assertThatThrownBy(() -> gameCtx.handleUserInput())
+                    .isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    private void inputNumbersIntoStdin(String[] numbers) {
+        final byte[] buf = String.join("\n", numbers).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
     }
 }
