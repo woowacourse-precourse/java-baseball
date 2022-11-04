2482e0a4eb8d542ec3678e2596c5dded1dfe2306
bsh1603 <bsh1603@github.com>
2022-11-04 12:16:07 +0900

    feat: game play(n볼 n스트라이크) 기능 추가

--- src/main/java/Service/GameService.java
+++ src/main/java/Service/GameService.java
@@ -5,6 +5,7 @@ import domain.Game;
 import domain.RandomNumber;
 import domain.UserNumber;
 import domain.UserValidate;
+import view.SystemMessage;
 
 public class GameService {
     private static final int NUM_SIZE = 3;
@@ -13,12 +14,20 @@ public class GameService {
     UserNumber userNumber = new UserNumber();
     UserValidate userValidate = new UserValidate();
     Game game;
+    SystemMessage systemMessage;
 
     public void set_game(){
         game = new Game(randomNumber.getNumbers());
     }
 
-    public void start_game(){
-
+    public void play_game(){
+        int strike = 0;
+        while(strike != 3){
+            game.initialize();
+            userNumber.setUser_numbers(userNumber.getUser_numbers());
+            // 계산
+            systemMessage.game_score_message(game.getBall_count(), game.getStrike_count());
+            strike = game.getStrike_count();
+        }
     }
 }
