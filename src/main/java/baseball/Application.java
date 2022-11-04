bdd441f297ec15e371911167fcca7a5341f68530
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-04 17:32:13 +0900

    feat(src/main): 유저 인풋 분리, loop 생성
    separate3Number():유저가 입력한 수를 자릿수로 분리하여 list에 넣는다.
    gameOn: 게임이 진행중인 한 loop를 돌며 유저의 인풋을 받아 반응한다.
    startGame()은 true를 리턴한다.
    
    해결: 2.게임 진행-플레이어가 입력한 수를 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -15,14 +16,33 @@ public class Application {
         return randomNums;
     }
 
-    private static void startGame() {
+    private static boolean startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean gameStarted = true;
+        return gameStarted;
+    }
+
+    private static List<Integer> seperate3Numbers(int input){
+        List<Integer> separated = new ArrayList<>();
+        for (int i = 2; i >= 0; i--) {
+            int digit = input / (int) Math.pow(10, i);
+            input = input % (int) Math.pow(10, i);
+            separated.add(digit);
+        }
+        return separated;
     }
 
     public static void main(String[] args) {
         List<Integer> answer = generateRandom3Numbers();
         System.out.println(answer);
-        startGame();
+        boolean gameOn = startGame();
+        Scanner sc = new Scanner(System.in);
+        while (gameOn) {
+            int userInput = sc.nextInt();
+            List<Integer> separatedInput = seperate3Numbers(userInput);
 
+            System.out.println(separatedInput);
+            gameOn = false;
+        }
     }
 }
