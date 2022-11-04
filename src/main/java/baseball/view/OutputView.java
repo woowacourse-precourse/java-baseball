924755cd7b8c91c56700c6fb634c7a808800a84f
ksh-beginner <ohk9134@naver.com>
2022-11-04 20:35:42 +0900

    refactor(OutputView) : 하드코딩했던 것 상수로 리팩토링

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,27 +2,38 @@ package baseball.view;
 
 public class OutputView {
 
+
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final int THREE_STRIKE_COUNT = 3;
+    private static final String THREE_STRIKE_MESSAGE = "3스트라이크";
+    private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final int ZERO_VALUE = 0;
+    private static final String NOTHING_RESULT_MESSAGE = "낫싱";
+    private static final String BALL_MESSAGE = "볼 ";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+
     public static void printGameStartMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(GAME_START_MESSAGE);
     }
 
     public static void printGameResultMessage(int strikeCount, int ballCount) {
-        if (strikeCount == 3) {
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        if (strikeCount == THREE_STRIKE_COUNT) {
+            System.out.println(THREE_STRIKE_MESSAGE);
+            System.out.println(GAME_END_MESSAGE);
+            System.out.println(GAME_RESTART_OR_END_MESSAGE);
         }
-        if (strikeCount == 0 && ballCount == 0) {
-            System.out.println("낫싱");
+        if (strikeCount == ZERO_VALUE && ballCount == ZERO_VALUE) {
+            System.out.println(NOTHING_RESULT_MESSAGE);
         }
-        if (strikeCount != 0 && ballCount != 0) {
-            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        if (strikeCount != ZERO_VALUE && ballCount != ZERO_VALUE) {
+            System.out.println(ballCount + BALL_MESSAGE + strikeCount + STRIKE_MESSAGE);
         }
-        if (strikeCount != 0 && strikeCount != 3 && ballCount == 0) {
-            System.out.println(strikeCount + "스트라이크");
+        if (strikeCount != ZERO_VALUE && strikeCount != THREE_STRIKE_COUNT && ballCount == ZERO_VALUE) {
+            System.out.println(strikeCount + STRIKE_MESSAGE);
         }
-        if (ballCount != 0 && strikeCount == 0) {
-            System.out.println(ballCount + "볼");
+        if (ballCount != ZERO_VALUE && strikeCount == ZERO_VALUE) {
+            System.out.println(ballCount + BALL_MESSAGE);
         }
     }
 }
