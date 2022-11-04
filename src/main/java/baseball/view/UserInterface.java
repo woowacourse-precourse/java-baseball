09f4418860988568ba3d6c130019d3a7ceeedb81
davejang <davejang98@naver.com>
2022-11-04 13:10:26 +0900

    feat: user interface input number
    유저의 숫자 입력 인터페이스 기능 구현

---
+++ src/main/java/baseball/view/UserInterface.java
@@ -0,0 +1,20 @@
+package baseball.view;
+
+import baseball.model.MyAnswer;
+
+import java.util.Scanner;
+
+public class UserInterface {
+    public UserInterface() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public void inputNumber() {
+        int number;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("숫자를 입력해주세요 : ");
+        number = scanner.nextInt();
+        MyAnswer myAnswer = new MyAnswer(number);
+        System.out.println(myAnswer.getAnswer());
+    }
+}
