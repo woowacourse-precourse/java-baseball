06f296a664b8d93c52c9ba367fbd93db29b2bb3e
swandevson <shai4956@gmail.com>
2022-11-04 22:23:08 +0900

    Feat: 종료값이 들어오기 전 까지 게임 실행
    - 정답을 맞춘 후, 종료값(1)이 들어오면 게임을 완전히 종료
    - 재시작값(2)가 들어오면 게임을 재시작
    - 나머지 값은 허가하지 않음

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,35 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+
+
 public class Application {
+    static final String RESTART_GAME = "1";
+    static final String END_GAME = "2";
+
+
+    public static boolean isExit(String command) {
+        if (command.equals(RESTART_GAME)) {
+            return false;
+        }
+        if (command.equals(END_GAME)) {
+            return true;
+        }
+
+        throw new IllegalArgumentException();
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameSystem game = new GameSystem();
+        String command;
+
+        do {
+            GameMessagePrinter.printStart();
+            game.run();
+            GameMessagePrinter.printGameWin();
+
+            command = readLine();
+        } while(!isExit(command));
     }
 }
