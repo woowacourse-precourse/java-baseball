4f6c77f92b8d6117c648a374cfdb6b0d2c2d8d70
areyouhun <areyouhun@gmail.com>
2022-11-05 02:15:24 +0900

    style(dealer): 메소드명 getNewThreeDifferentNumbers에서 createThreeDifferentNumbers로 변경

--- src/main/java/baseball/Dealer.java
+++ src/main/java/baseball/Dealer.java
@@ -1,20 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
 public class Dealer {
     private boolean isGameGoing = true;
     private List<Integer> threeDifferentNumbers;
 
-    public List<Integer> getNewThreeDifferentNumbers() {
+    public List<Integer> createThreeDifferentNumbers() {
         threeDifferentNumbers = new ArrayList<>();
         Set<Integer> pickedNumbers = new HashSet<>();
+
         while (pickedNumbers.size() < 3) {
             Integer number = Randoms.pickNumberInRange(1, 9);
             pickedNumbers.add(number);
@@ -22,17 +22,5 @@ public class Dealer {
         threeDifferentNumbers.addAll(pickedNumbers);
         Collections.shuffle(threeDifferentNumbers); // Unless the numbers will be arranged in ascending order.
         return threeDifferentNumbers;
-    }
-
-    public static void main(String[] args) {
-        Dealer dealer = new Dealer();
-        System.out.println(dealer.getNewThreeDifferentNumbers());
-        System.out.println(dealer.getNewThreeDifferentNumbers());
-        System.out.println(dealer.getNewThreeDifferentNumbers());
-        System.out.println(dealer.getNewThreeDifferentNumbers());
-        System.out.println(dealer.getNewThreeDifferentNumbers());
-        System.out.println(dealer.getNewThreeDifferentNumbers());
-        System.out.println(dealer.getNewThreeDifferentNumbers());
-        System.out.println(dealer.getNewThreeDifferentNumbers());
     }
 }
