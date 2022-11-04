ba1d40e07e5560b21c30f8b915a5025d3d64ffe2
hi-june <wnsgur735@gmail.com>
2022-11-04 15:59:40 +0900

    feat(Application): implement main program

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,14 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        while (true) {
+            NumberBaseball game = new NumberBaseball();
+
+            game.startGame();
+
+            if (!game.wantRestart()) {
+                break;
+            }
+        }
     }
 }
