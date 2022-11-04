a46c163e3ffd0f782386fe1bfc5fc299e87df79c
skydreamer21 <skydreamer210@gmail.com>
2022-11-04 17:54:38 +0900

    feat: 서로 다른 랜덤 세자리 수를 반환하는 함수 makeAnswer 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,45 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+    }
+
+    public static List<Integer> makeAnswer() {
+        int numOfPossibleNumbers = 9;
+        int[] possibleNumbers = new int[numOfPossibleNumbers];
+        for (int i = 0; i < 9; i++) {
+            possibleNumbers[i] = i+1;
+        }
+
+        int chosenIndex;
+        List<Integer> answer = new ArrayList<>();
+        for (int times = 1; times <= 3; times++) {
+            chosenIndex = Randoms.pickNumberInRange(0, numOfPossibleNumbers - 1);
+            answer.add(possibleNumbers[chosenIndex]);
+            swap(possibleNumbers, 0, chosenIndex);
+            possibleNumbers = subArray(possibleNumbers, 1, numOfPossibleNumbers - 1);
+            numOfPossibleNumbers--;
+        }
+
+        return answer;
+    }
+
+    public static int[] subArray (int[] numbers, int start, int end) {
+        return IntStream.rangeClosed(start, end)
+                .map(i -> numbers[i])
+                .toArray();
+    }
+
+    public static void swap (int[] numbers, int index1, int index2) {
+        int temp = numbers[index1];
+        numbers[index1] = numbers[index2];
+        numbers[index2] = temp;
     }
 }
