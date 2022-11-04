7c9972c57b09cb03267117873e1ea3ab71e0b6eb
endlessmomo <soohyuk96@naver.com>
2022-11-04 10:49:13 +0900

    feat(BaseballGame) : 사용자로부터 입력 받는 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class BaseballGame {
     public void playGame() {
         do {
@@ -22,5 +24,21 @@ public class BaseballGame {
         String doReGame = getUserInputRegame();
 
         return doReGame.equals("1");
+    }
+
+    public String getUserInput() {
+        return readLine();
+    }
+
+    public String getUserInputNumber() {
+        System.out.print("숫자를 입력해 주세요 : ");
+
+        return getUserInput();
+    }
+
+    public String getUserInputRegame() {
+        System.out.println("재시작을 하려면 1을, 게임을 종료하려면 2를 눌러주세요. : ");
+
+        return getUserInput();
     }
 }
