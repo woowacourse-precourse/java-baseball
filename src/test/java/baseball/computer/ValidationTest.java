c43eddd8f06a41e86b59413bc6006bc4d3d1a89f
situodys <situodys@gmail.com>
2022-11-04 14:07:47 +0900

    test(Computer): 정상적인 정답 생성 확인 테스트

---
+++ src/test/java/baseball/computer/ValidationTest.java
@@ -0,0 +1,22 @@
+package baseball.computer;
+
+import static org.assertj.core.api.Assertions.*;
+
+import baseball.Computer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+public class ValidationTest {
+
+    @RepeatedTest(10)
+    @DisplayName("Computer 에서 정답 생성 시 Randoms.pickNumberInRange 에 의해 정상적인 Baseballs 가 생성되었는지 확인")
+    public void test1() throws Exception{
+        //given
+        Computer computer = new Computer();
+        //when
+        computer.generateAnswer();
+        //then
+        assertThat(computer).isInstanceOf(Computer.class);
+    }
+}
