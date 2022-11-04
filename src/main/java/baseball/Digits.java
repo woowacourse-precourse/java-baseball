03e0a8aa95130fd647797d086c4b97ef9a1ff953
2jun0 <soo28819@gmail.com>
2022-11-04 22:01:43 +0900

    Feat(Digits): 숫자 배열의 제약사항 구현

--- src/main/java/baseball/Digits.java
+++ src/main/java/baseball/Digits.java
@@ -2,14 +2,33 @@ package baseball;
 
 import java.util.List;
 
+import static baseball.BaseballGame.DIGIT_COUNT;
+
 public class Digits {
     private final List<Integer> digitList;
 
     public Digits(List<Integer> digitList) {
+        validateDigitListSize(digitList);
+        assertDigitListRange(digitList);
+
         this.digitList = digitList;
     }
 
     public List<Integer> get() {
         return digitList;
+    }
+
+    private void validateDigitRange(int digit) {
+        if (1<=digit && digit<=9)
+            throw new IllegalArgumentException("digit 범위는 1~9의 값이여야 합니다.");
+    }
+
+    private void validateDigitListSize(List<Integer> digitList) {
+        if (digitList.size() == DIGIT_COUNT)
+            throw new IllegalArgumentException("digitList는 항상 " + DIGIT_COUNT + "이여야 합니다.");
+    }
+
+    private void assertDigitListRange(List<Integer> digitList) {
+        digitList.forEach(this::validateDigitRange);
     }
 }
