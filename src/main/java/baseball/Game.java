cca1f8346284b90f3b2e3806eb255ec719064df1
Eunkeee <eunkeeeee@gmail.com>
2022-11-04 21:30:17 +0900

    feat: 게임 결과 출력 (스트라이크, 볼, 낫싱)

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,8 +1,12 @@
 package baseball;
 
-public class Game {
+import java.util.Arrays;
 
+public class Game {
     static final int NUMBER_LENGTH = 3;
+    String STRING_BALL = "볼";
+    String STRING_STRIKE = "스트라이크";
+    String STRING_NOTHING = "낫싱";
     public Game() {
     }
     public void play() {
@@ -12,11 +16,13 @@ public class Game {
         // get random number from computer
         computer.getRandomNumber();
         int randomNumber = computer.randomNumber;
+        System.out.println(randomNumber);
 
         while(true) {
             // get user number from user
             user.setUserNumber();
             int userNumber = user.getUserNumber();
+            System.out.println(userNumber);
 
             int strike = 0;
             int ball = 0;
@@ -24,39 +30,37 @@ public class Game {
             String[] randomNumberArr = String.valueOf(randomNumber).split("");
             String[] userNumberArr = String.valueOf(userNumber).split("");
 
-            // calculate strike
             for(int i=0; i<NUMBER_LENGTH; i++) {
+                // calculate strike
                 if(randomNumberArr[i].equals(userNumberArr[i])) {
                     strike++;
                 }
-            }
-            System.out.println(strike + " 스트라이크");
-        }
-    }
-
-    // method for testing
-    String strikeAndBall(String randomNumber, String userNumber) {
-        int strike = 0;
-        int ball = 0;
-
-        String[] randomNumberArr = String.valueOf(randomNumber).split("");
-        String[] userNumberArr = String.valueOf(userNumber).split("");
-
-        for(int i=0; i<NUMBER_LENGTH; i++) {
-            // calculate strike
-            if(randomNumberArr[i].equals(userNumberArr[i])) {
-                strike++;
-            }
-            // calculate ball
-            for(int k=0; k<NUMBER_LENGTH; k++){
-                if(randomNumberArr[i].equals(userNumberArr[k]) && i != k) {
-                    ball++;
+                // calculate ball
+                for(int k=0; k<NUMBER_LENGTH; k++){
+                    if(randomNumberArr[i].equals(userNumberArr[k]) && i != k) {
+                        ball++;
+                    }
                 }
             }
+
+            // paint result
+            String result = "";
+            if (ball == 0 && strike == 0) {
+                result = STRING_NOTHING;
+            }else if (ball == 0 && strike > 0) {
+                result = strike + STRING_STRIKE;
+            }else if (ball > 0 && strike == 0) {
+                result = ball + STRING_BALL;
+            }else if (ball > 0 && strike > 0){
+                result = ball + STRING_BALL + " " + strike + STRING_STRIKE;
+            }
+            System.out.println(result);
+
+            if(strike == 3) {
+                break;
+            }
+
         }
-
-
-        return ball + "볼 " + strike + "스트라이크";
     }
 }
 
--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -6,10 +6,6 @@ import org.junit.jupiter.api.Test;
 
 class GameTest {
 
-    @Test
-    void returnStrikeAndBall() {
-        Game game = new Game();
-        assertEquals("1볼 1스트라이크", game.strikeAndBall("589", "597"));
-    }
+
 
 }
