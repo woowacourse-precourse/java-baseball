5cc9906605d3bcb4ce3658996720e9d01f7bce4c
iamminho <minho100227@kakao.com>
2022-11-04 15:09:53 +0900

    style(exception): 어노테이션 DisplayName 추가 및 변수명 가독성 좋게 변경

--- src/test/java/baseball/controller/ExceptionTest.java
+++ src/test/java/baseball/controller/ExceptionTest.java
@@ -1,30 +1,34 @@
 package baseball.controller;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class ExceptionTest {
 	@Test
-	public void validOnlyOneToNineTest() {
-		String inputGameNumber = "0358";
+	@DisplayName("제한된 숫자 범위를 넘어가는 경우")
+	public void validateOnlyOneToNineTest() {
+		String playerNumber = "092";
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			Exception.validOnlyOneToNine(inputGameNumber);
+			Exception.validateOnlyOneToNine(playerNumber);
 		});
 	}
 
 	@Test
-	public void validNumberLengthTest() {
-		String inputGameNumber = "0358";
+	@DisplayName("제한된 숫자 개수를 넘어가는 경우")
+	public void validateNumberLengthTest() {
+		String playerNumber = "9358";
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			Exception.validNumberLength(inputGameNumber);
+			Exception.validateNumberLength(playerNumber);
 		});
 	}
 
 	@Test
-	public void validOverlapNumber() {
-		String inputGameNumber = "889";
+	@DisplayName("증복된 숫자가 포함되는 경우")
+	public void validateOverlapNumber() {
+		String playerNumber = "889";
 		Assertions.assertThrows(IllegalArgumentException.class, () -> {
-			Exception.validOverlapNumber(inputGameNumber);
+			Exception.validateOverlapNumber(playerNumber);
 		});
 	}
 }
