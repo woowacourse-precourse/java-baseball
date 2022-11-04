57d9f6c8fb2a57f3d08d12b60b2b65e9d15b78a7
YunDaeHyeok <bmong4mong0318@gmail.com>
2022-11-04 16:15:37 +0900

    test: add BaseBallNumber size validation

---
+++ src/test/java/baseball/BaseBallNumberTest.java
@@ -0,0 +1,37 @@
+package baseball;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BaseBallNumberTest {
+
+  @Test
+  @DisplayName("validate number size_empty")
+  void validateSizeEmpty() {
+    Assertions.assertThatThrownBy(()->{
+      BaseBallNumber baseBallNumber = new BaseBallNumber(Collections.emptyList());
+    }).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName("validate number size_less")
+  void validateSizeLess() {
+    Assertions.assertThatThrownBy(()->{
+      BaseBallNumber baseBallNumber = new BaseBallNumber(List.of('1', '2'));
+    }).isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  @DisplayName("validate number size_more")
+  void validateSizeMore() {
+    Assertions.assertThatThrownBy(()->{
+      BaseBallNumber baseBallNumber = new BaseBallNumber(List.of('1', '2', '3', '4'));
+    }).isInstanceOf(IllegalArgumentException.class);
+  }
+}
