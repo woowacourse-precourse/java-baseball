543667a6ddcfdb5b1104338de016876a962d3782
K-jun98 <alap_u@naver.com>
2022-11-04 23:17:03 +0900

    test - Ball Equals 테스트

--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Assertions;
@@ -20,6 +21,20 @@ class BallTest {
     @DisplayName("Ball 생성 예외 테스트")
     void rangeValidateTest() {
         Assertions.assertThrows(IllegalArgumentException.class, () -> new Ball(10));
+    }
+
+    @Test
+    @DisplayName("Ball Equals 테스트 ")
+    void BallEquals() {
+        Ball ball = new Ball(1);
+        assertThat(ball).isEqualTo(new Ball(1));
+    }
+
+    @Test
+    @DisplayName("Ball Equals 테스트")
+    void BallNotEquals() {
+        Ball ball = new Ball(1);
+        assertThat(ball).isNotEqualTo(new Ball(2));
     }
 
 }
