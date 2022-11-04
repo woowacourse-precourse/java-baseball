1c083dc6ca38e8cc17147cb678093c87e4a244d2
jmj005 <jmj00005@naver.com>
2022-11-05 02:01:00 +0900

    refactor(Application): 메인 클래스 refactoring

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,108 +1,66 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.*;
 
-class ConsolePrint {
-    public static final String BALL_MESSAGE = "볼 ";
-    public static final String STRIKE_MESSAGE = "스트라이크";
-    public static final String NOTHING_MESSAGE = "낫싱";
 
-    public static void printGameStart() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+enum MessageType {
+    BALL_MESSAGE("볼 "),
+    STRIKE_MESSAGE("스트라이크"),
+    NOTHING_MESSAGE("낫싱");
+
+    private final String message;
+
+    MessageType(String message) {
+        this.message = message;
     }
 
-    public static void printInputNumber() {
-        System.out.print("숫자를 입력해주세요 : ");
-    }
-
-    public static void printGameOver() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    }
-
-    public static void printGameSelect() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    public String getMessage() {
+        return message;
     }
 }
 
 public class Application {
 
-    private static boolean isGameOver(int strikeCount) {
-        return strikeCount == 3;
+    static User user = new User();
+    static Computer computer = new Computer();
+    static GameScores gameScores = new GameScores();
+
+    public static boolean isGameOver() {
+        return gameScores.getStrikeCount() == 3;
     }
 
-    private static int findStrike(int userNumber, int computerNumber) {
-        if (userNumber == computerNumber) {
-            return 1;
-        }
-        return 0;
-    }
-
-    private static int getStrikeCount(List<Integer> userNumber, List<Integer> computerNumber) {
-        int strikeCount = 0;
-        for (int index = 0; index < userNumber.size(); index++) {
-            strikeCount += findStrike(userNumber.get(index), computerNumber.get(index));
-        }
-        return strikeCount;
-    }
-
-    private static int getBallCount(List<Integer> userNumber, List<Integer> computerNumber) {
-        int ballCount = 0;
-        for (Integer computerElement : computerNumber) {
-            ballCount += Collections.frequency(userNumber, computerElement);
-        }
-        return ballCount;
-    }
-
-    private static boolean isNothing(List<Integer> userNumber, List<Integer> computerNumber) {
-        return Collections.disjoint(userNumber, computerNumber);
-    }
-
-    private static String getGameResult(List<Integer> userNumber, List<Integer> computerNumber) {
-        String gameResult = "";
-        int ballCount = getBallCount(userNumber, computerNumber);
-        int strikeCount = getStrikeCount(userNumber, computerNumber);
-
-        if (ballCount > 0 && strikeCount != ballCount) {
-            gameResult += (ballCount - strikeCount) + ConsolePrint.BALL_MESSAGE;
-        }
-        if (strikeCount > 0) {
-            gameResult += strikeCount + ConsolePrint.STRIKE_MESSAGE;
-        }
-        if (isNothing(userNumber, computerNumber)) {
-            gameResult += ConsolePrint.NOTHING_MESSAGE;
-        }
-        return gameResult + "\n";
-    }
-
-    private static int getGameSelect() {
+    public static int getGameSelect() {
         String gameSelect = Console.readLine();
         if (!gameSelect.equals("1") && !gameSelect.equals("2")) {
-            ConsolePrint.printGameSelect();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             getGameSelect();
         }
         return Integer.parseInt(gameSelect);
     }
 
-
-    private static void startGameLogic() {
-        int strikeCount = 0;
-        List<Integer> computerNumber = Computer.getRandomNumber();
+    public static void startGameLogic() {
+        computer.setComputerNumber();
         do {
-            ConsolePrint.printInputNumber();
-            List<Integer> userNumber = User.getNumber();
-            System.out.print(getGameResult(userNumber, computerNumber));
-            strikeCount = getStrikeCount(userNumber, computerNumber);
-        } while(!isGameOver(strikeCount));
+            gameScores.gameScoresZero();
+            System.out.print("숫자를 입력해주세요 : ");
+            user.setUserNumber();
+            if (user.getUserNumber().isEmpty()) {
+                break;
+            }
+            gameScores.printGameResult(user.getUserNumber(), computer.getComputerNumber());
+        } while (!isGameOver());
     }
 
     public static void main(String[] args) {
-        int gameSelect = 0;
-        ConsolePrint.printGameStart();
+        int gameSelect;
+        System.out.println("숫자 야구 게임을 시작합니다.");
         do {
             startGameLogic();
-            ConsolePrint.printGameOver();
-            ConsolePrint.printGameSelect();
+            if (user.getUserNumber().isEmpty()) {
+                break;
+            }
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             gameSelect = getGameSelect();
         } while (gameSelect != 2);
     }
