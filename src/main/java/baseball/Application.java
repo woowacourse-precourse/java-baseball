66ce737a78458960201e09c042291ae903406043
minsu <bill7666@naver.com>
2022-11-04 19:18:20 +0900

    볼,스트라이크,총계산,자릿수확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
 public class Application {
@@ -20,6 +21,19 @@ public class Application {
         }
         return Number;
     }
+
+    public static boolean GoAndStop(String num,int[] UserChoiceNum){
+        boolean GoAndStop = true;
+        if (num.length() == 3) {
+            UserChoiceNum = InputNumber(num);
+            //System.out.println(Arrays.toString(UserChoiceNum));
+            GoAndStop = true;
+        } else if(num.length() !=3){
+            //System.out.println("세자리수만 입력가능합니다");
+            GoAndStop = false;
+        }
+        return GoAndStop;
+    }
     public static int[] InputNumber(String num){
         int ch[] = new int[3];
         int choice = Integer.parseInt(num);
@@ -29,17 +43,57 @@ public class Application {
         }
         return ch;
     }
+
+    public static int Ball(int com,int user,int i,int j){
+        int ball = 0;
+        if((com == user) && i!=j){
+            ball = ball+1;
+        }
+        return ball;
+    }
+
+    public static int Strike(int com,int user,int i,int j){
+        int strike = 0;
+        if((com == user) && i==j){
+            strike = strike +1;
+        }
+        return strike;
+    }
+
+    public static HashMap<String,Integer> BallStrike(List<Integer> RandomNumber, int[] UserChoiceNum){
+        HashMap<String,Integer> map = new HashMap<>();
+        int Ball = 0;
+        int Strike = 0;
+        for(int i = 0;i<3;i++){
+            for(int j = 0;j<UserChoiceNum.length;j++){
+                Ball += Ball(RandomNumber.get(i),UserChoiceNum[j],i,j);
+                Strike += Strike(RandomNumber.get(i),UserChoiceNum[j],i,j);
+            }
+        }
+        if(Strike == 3){
+            map.put("홈런",1);
+        } else if (Strike != 3) {
+            map.put("볼",Ball);
+            map.put("스트라이크",Strike);
+        }
+        return map;
+    }
+
+
+
     public static void main(String[] args) {
         List<Integer> RandomNumber = PickRandomNumber();
+        int UserChoiceNum[] = new int[3];
         System.out.println(RandomNumber);
         System.out.println("숫자야구게임을 시작합니다.");
         System.out.println("숫자를 입력해주세요");
         String num = Console.readLine();
-        if (num.length() == 3) {
-            int UserChoiceNum[] = InputNumber(num);
-        } else {
-            System.out.println("세자리수만 입력가능합니다");
-        }
+        boolean GoAndStop = GoAndStop(num,UserChoiceNum);
+        System.out.println(GoAndStop);
+        HashMap<String,Integer> result = BallStrike(RandomNumber,UserChoiceNum);
+        System.out.println(result.get("볼")+ "볼" + result.get("스트라이크") + "스트라이크");
+
+
 
 
 
--- src/test/java/baseball/FunctionTest.java
+++ src/test/java/baseball/FunctionTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.logging.Logger;
 
-import static baseball.Application.InputNumber;
-import static baseball.Application.PickRandomNumber;
+import static baseball.Application.*;
 
 public class FunctionTest {
 @Nested
@@ -36,5 +36,69 @@ class 랜덤으로숫자를뽑기{
             int UserChoiceNum[] = InputNumber(num);
 
         }
+    }
+
+    @Nested
+    class 입력자릿수확인{
+        @Test
+        void CheckInputNum(){
+            String num = "456";
+            String num1 = "45678";
+            int UserChoiceNum[] = new int[3];
+            boolean result = GoAndStop(num,UserChoiceNum);
+            boolean result1 = GoAndStop(num1,UserChoiceNum);
+            assert (result == true);
+            assert (result1 == false);
+        }
+    }
+
+    @Nested
+    class 볼{
+        @Test
+        void BallTest(){
+            int user = 1;
+            int com = 1;
+            int ball = 0;
+            int i = 1;
+            int j = 0;
+            ball = Ball(com,user,i,j);
+            assert (ball == 1);
+        }
+    }
+
+    @Nested
+    class 스트라이크{
+        @Test
+        void StrikeTest(){
+            int user = 1;
+            int com = 1;
+            int strike = 0;
+            int i = 0;
+            int j = 0;
+            strike = Strike(com,user,i,j);
+            assert (strike == 1);
+        }
+    }
+
+    @Nested
+    class 총연산{
+
+        @Test
+        void ResultTest1(){
+            List<Integer> RandomNumber = List.of(0,2,1);
+            int[] UserChoiceNum = new int[]{1,2,3};
+            HashMap<String,Integer> map = new HashMap<>();
+            map = BallStrike(RandomNumber,UserChoiceNum);
+            assert (map.get("볼") == 1 && map.get("스트라이크") == 1);
+        }
+        @Test
+        void ResultTest2(){
+            List<Integer> RandomNumber = List.of(1,2,3);
+            int[] UserChoiceNum = new int[]{1,2,3};
+            HashMap<String,Integer> map = new HashMap<>();
+            map = BallStrike(RandomNumber,UserChoiceNum);
+            assert (map.get("홈런") == 1);
+        }
+
     }
 }
