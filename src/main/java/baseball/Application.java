f5e68bee847fa6ca4c1665440dabe5a83664e8c5
jsk5341 <102288039+jsk5341@users.noreply.github.com>
2022-11-05 04:44:56 +0900

    feat(pick): implement picking random numbers for computer function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(computersNumbers());
+    }
+
+    public static ArrayList<Integer> computersNumbers() {
+
+        ArrayList<Integer> computerRandomNumbers = new ArrayList<Integer>();
+        while (computerRandomNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerRandomNumbers.contains(randomNumber)) {
+                computerRandomNumbers.add(randomNumber);
+            }
+        }
+
+        return computerRandomNumbers;
     }
 }
+
