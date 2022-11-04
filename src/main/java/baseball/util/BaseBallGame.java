852a60952f07e6bc2772f22b38ec12c476893cd0
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-04 21:22:20 +0900

    feat: 사용자 입력 값과 컴퓨터 랜덤 값 비교

--- src/main/java/baseball/util/BaseBallGame.java
+++ src/main/java/baseball/util/BaseBallGame.java
@@ -7,9 +7,6 @@ import java.util.List;
 public class BaseBallGame {
     private final List<Integer> computerRandomThreeValue;
 
-
-
-
     public BaseBallGame(List<Integer> computerRandomThreeValue) {
         this.computerRandomThreeValue = List.copyOf(computerRandomThreeValue);
     }
@@ -20,7 +17,27 @@ public class BaseBallGame {
         do {
 
             userData = Console.readLine();
+            List<Integer> ballStrikeCount = computerRandomCompareToUserInput(computerRandomThreeValue, userData);
+
         } while (false);
     }
 
+
+    private static List<Integer> computerRandomCompareToUserInput (List<Integer> computerRandom, String userInput) {
+        int ballCount = 0,
+                strikeCount = 0;
+
+        for (int i = 0; i < computerRandom.size(); i++) {
+            if (userInput.charAt(i) - '0' == computerRandom.get(i)) {
+                strikeCount++;
+                continue;
+            }
+
+            if (computerRandom.contains(userInput.charAt(i) - '0')) {
+                ballCount++;
+            }
+        }
+
+        return List.of(ballCount, strikeCount);
+    }
 }
