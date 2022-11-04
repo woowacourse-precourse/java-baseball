daea38417f024e81f2f4ab2ca24e17432ba05015
kimdahee0815 <diana815@naver.com>
2022-11-04 14:15:42 +0900

    feat : 나올 수 있는 결과(볼,스트라이크)를 각각 몇번 나올 수 있는지 매칭한 hashmap 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,9 +2,11 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
 
 public class Application {
+	static HashMap<String,Integer> possibleBallResult = new HashMap<>();
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+   
     }
 }
