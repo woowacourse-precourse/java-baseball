5d2e503ebf4718c24f51f593cc181d5aad23cbff
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-05 00:47:11 +0900

    feat(playSingleGame): build command flow

--- src/main/java/baseball/BaseballGameConsole.java
+++ src/main/java/baseball/BaseballGameConsole.java
@@ -5,12 +5,28 @@ public class BaseballGameConsole {
     public void playGame(Player player) {
         startingGuide(player.getName());
         do {
-            // Play single baseball game
+            playSingleGame(player);
         } while (true); // Ask player to play a new game
     }
 
     private void startingGuide(String playerName) {
         System.out.println(playerName + "반갑습니다.");
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    private void playSingleGame(Player player) {
+        boolean matchedAllNumber = false;
+        // Computer selects random number
+
+        do {
+            System.out.println("숫자를 입력해주세요. :");
+            // Player predicts number
+            // Validator validates players prediction
+
+            // Computer returns result of prediction
+            // Update matchedAllNumber
+
+            // Show result message
+        } while (!matchedAllNumber);
     }
 }
