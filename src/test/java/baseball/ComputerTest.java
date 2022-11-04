3fb1924031c65d7902925b213c82b454f2d29817
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-04 21:30:09 +0900

    feat: ComputerTest 테스트 생성

---
+++ src/test/java/baseball/ComputerTest.java
@@ -0,0 +1,38 @@
+package baseball;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+public class ComputerTest {
+
+    Computer computer;
+
+    @BeforeEach
+    void initializeComputer(){
+        computer = new Computer();
+    }
+
+    @Test
+    void 랜덤값은_1부터_9사이(){
+        assertThat(computer.createRandomNumber()).isLessThanOrEqualTo(9)
+                .isGreaterThat.orEqualTo(1);
+    }
+
+    @Test
+    void 값을_입력하면_리스트에_저장(){
+        computer.putNumber(1);
+        assertThat(computer.getNumbers()).contains(1);
+    }
+
+    @Test
+    void 값이_이미_있으면_참(){
+        computer.putNumber(1);
+        assertThat(computer.isAlreadyInNumbers(1)).isTrue();
+    }
+
+    @Test
+    void 값이_없으면_거짓(){
+        computer.putNumber(1);
+        assertThat(computer.isAlreadyInNumbers(2)).isFalse();
+    }
+}
