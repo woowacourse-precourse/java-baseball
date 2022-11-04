a255d4c76982cd3772d24f03c568a7bd00ffaf28
eugene1204 <s1061001@naver.com>
2022-11-04 20:38:32 +0900

    feat: gameRestart 메소드 생성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,5 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,19 @@ public class Game {
             comparenumber.compareprint();
             if (!comparenumber.isbaseballcontinue()) return false;
         }
+    }
+    public boolean gameRestart(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        if(Integer.valueOf(input)==1){
+            return true;
+        }else if(Integer.valueOf(input)==2) return false;
+        else {
+            new IllegalArgumentException("해당하는 숫자가 아닙니다. 프로그램을 종료합니다.");
+            return false;
+        }
+
     }
 
 
