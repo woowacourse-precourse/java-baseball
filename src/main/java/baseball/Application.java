ffdfe2a5ba4b5d35c225138eb05934d284e27e32
JunDonghyuk <ehdgur4814@naver.com>
2022-11-05 03:15:15 +0900

    feat(printGameResult): update
    Update new function
    - 사용자 입력에 따른 숫자 야구 게임 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,7 +12,7 @@ public class Application {
         boolean gameFlag = true;
         while (gameFlag) {
             List<Integer> randomNumber = getRandomNumber();
-            
+
             printEndMessage();
         }
 
@@ -38,4 +38,24 @@ public class Application {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+
+    public static void printGameResult(int ballCount, int strikeCount) {
+        StringBuilder resultMessage = new StringBuilder();
+        if (ballCount != 0) {
+            resultMessage.append(ballCount + "볼");
+        }
+
+        if (strikeCount != 0) {
+            if (resultMessage.length() != 0) {
+                resultMessage.append(" ");
+            }
+            resultMessage.append(strikeCount + "스트라이크");
+        }
+
+        if (resultMessage.length() == 0) {
+            resultMessage.append("낫싱");
+        }
+    }
+
+    
 }
