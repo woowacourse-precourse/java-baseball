2b5bcfdaf973de82da97e32497c7adb138eb498f
bjk1649 <qhwns1649@naver.com>
2022-11-05 00:37:15 +0900

    feat: 숫자 입력 안내메시지를 출력하는 메소드 추가

--- src/main/java/baseball/InfoPrinter.java
+++ src/main/java/baseball/InfoPrinter.java
@@ -9,6 +9,10 @@ public class InfoPrinter {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public static void printInputInfo() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
     public static void printStrikesAndBalls(List<Integer> strikesAndBalls) {
         int strikes = strikesAndBalls.get(0);
         int balls = strikesAndBalls.get(1);
