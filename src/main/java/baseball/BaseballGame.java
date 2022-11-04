b04c379e8a67b280234a553a2fdf88a8a942497f
thdwoqor <celebarte01@gmail.com>
2022-11-04 22:14:09 +0900

    feat: '야구 게임' 클래스 구현

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.IntStream;
+
+import static baseball.Validate.*;
+
+public class BaseballGame extends NumberGame {
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public boolean play() {
+        return false;
+    }
+
+    @Override
+    public boolean end() {
+        return false;
+    }
+}
