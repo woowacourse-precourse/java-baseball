46e14fbacbd48bdf23a9da3a6cb1a912f2cbede4
endlessmomo <soohyuk96@naver.com>
2022-11-04 11:27:04 +0900

    feat(BaseballGame) : 입력 받은 문자 검증 및 예외처리

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,11 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseballGame {
@@ -25,20 +31,60 @@ public class BaseballGame {
 
         return doReGame.equals("1");
     }
-
-    public String getUserInput() {
+    public String getUserInput(){
         return readLine();
     }
-
-    public String getUserInputNumber() {
+    public String getUserInputNumber(){
         System.out.print("숫자를 입력해 주세요 : ");
 
-        return getUserInput();
+        String s = validateUserInputNum(getUserInput());
+        System.out.println(s);
+        return s;
+    }
+
+    private String validateUserInputNum(String userInput) {
+       if(userInput.length() != 3){
+           throw new IllegalArgumentException("세자리 숫자를 입력하세요.");
+       }
+
+       if(!isDigits(userInput)){
+           throw new IllegalArgumentException("숫자를 제외한 다른 문자는 입력이 불가능합니다.");
+       }
+
+       if(!checkDuplicate(userInput)){
+            throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다.");
+       }
+
+       return userInput;
+    }
+
+    public boolean isDigits(String userInput){
+        for(char digit : userInput.toCharArray()){
+            if(!Character.isDigit(digit)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean checkDuplicate(String userInput) {
+        Set <String> checkDuplicateSet = new HashSet <>(List.of(userInput.split("")));
+
+        return checkDuplicateSet.size() == 3;
     }
 
     public String getUserInputRegame() {
         System.out.println("재시작을 하려면 1을, 게임을 종료하려면 2를 눌러주세요. : ");
 
-        return getUserInput();
+        return validateUserInputRegame(getUserInput());
+    }
+
+    private String validateUserInputRegame(String userInput) {
+        if(!(userInput.equals("1") || !userInput.equals("2"))) {
+            throw new IllegalArgumentException("1, 2만 입력 가능합니다. 다시 입력해 주세요.");
+        }
+
+        return userInput;
     }
 }
