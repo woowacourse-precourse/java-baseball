f21e2cf64edbfb02d001effbc3ec80609e28c528
csj <koreaxman@naver.com>
2022-11-04 14:54:55 +0900

    feat: compare two strings and increase count if matched

--- src/main/java/baseball/Strike.java
+++ src/main/java/baseball/Strike.java
@@ -1,2 +1,17 @@
-package baseball;public class Strike {
+package baseball;
+
+public class Strike {
+    Computer computer = new Computer();
+
+    public int compareCharacterLocationMatch() {
+        int count = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if(InputNumber.number.charAt(i)
+                    == computer.randomNumberListToString().charAt(i)){
+                count++;
+            }
+        }
+        return count;
+    }
 }
