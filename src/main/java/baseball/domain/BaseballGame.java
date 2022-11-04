8c36d979917524733aab86f18a6de50e87d0b5c6
hum02 <ehdgma02@gmail.com>
2022-11-04 16:46:25 +0900

    feat:플레이어 예상숫자가 컴퓨터 숫자와 다르면 다시 입력받기

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -1,10 +1,24 @@
 package baseball.domain;
 
+import baseball.util.Input;
+
 public class BaseballGame {
     private final Computer computer = new Computer();
+    private boolean isFinish = false;
 
-    public BaseballGame() {
+    public BaseballGame() {}
 
+    public void play() {
+        do {
+            String playerGuess = Input.inputPlayerNumber();
+            Hint hint = computer.createHint(playerGuess);
+            checkCorrect(hint);
+        } while (!isFinish);
     }
 
+    private void checkCorrect(Hint hint) {
+        if (hint.isFullStrikes()) {
+            this.isFinish = true;
+        }
+    }
 }
