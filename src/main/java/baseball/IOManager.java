c26899b343accbde6f9ab6d6378463cc2e15bb67
jagajang <kosb7544@gmail.com>
2022-11-04 17:19:47 +0900

    dev: create message list and program start message

--- src/main/java/baseball/IOManager.java
+++ src/main/java/baseball/IOManager.java
@@ -1,4 +1,12 @@
 package baseball;
 
+import java.util.Map;
+
 public class IOManager {
+    private static final Map<String, String> messages = Map.of(
+            "PROGRAM_START", "숫자 야구 게임을 시작합니다."
+    );
+    public static void printProgramStartMessage() {
+        System.out.println(messages.get("PROGRAM_START"));
+    }
 }
