0106d69444cfcb892348a79e40f19bf02fedf1de
Kim woo jong <skekdnwhd@gmail.com>
2022-11-05 00:08:58 +0900

    Add, Feat : NumericalComparison 컴퓨터숫자와 사용자입력 숫자를 비교해서 stike와 ball을 던져주는 기능

---
+++ src/main/java/baseball/service/NumericalComparison.java
@@ -0,0 +1,43 @@
+package baseball.service;
+
+import java.util.List;
+import java.util.Map;
+
+public class NumericalComparison {
+    private int ball;
+    private int strike;
+
+    private static final int INITIALIZATION = 0;
+
+    private NumericalComparison() {
+    }
+
+    private static class LazyHolder {
+        private static final NumericalComparison INSTANCE = new NumericalComparison();
+    }
+
+    public static NumericalComparison getInstance() {
+        return NumericalComparison.LazyHolder.INSTANCE;
+    }
+
+    public Map<String, Integer> compareList(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        resetCounts();
+
+        for (int i = 0; i < 3; i++) {
+            if (computerNumbers.get(i).equals(userNumbers.get(i)))
+                strike++;
+            else if (userNumbers.contains(computerNumbers.get(i)))
+                ball++;
+        }
+
+        return Map.of(
+                "strike", strike,
+                "ball", ball
+        );
+    }
+
+    private void resetCounts() {
+        strike = INITIALIZATION;
+        ball = INITIALIZATION;
+    }
+}
