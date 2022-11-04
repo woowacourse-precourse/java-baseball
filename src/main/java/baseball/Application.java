3f4dced262e5b0a2bb0bfe6787793175b3b92168
chopinballadeno4 <qo98333@naver.com>
2022-11-05 00:05:21 +0900

    feat: Compose GameController, make instance and run

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,13 @@
 package baseball;
 
+import baseball.controller.GameController;
 import baseball.view.AppView;
 
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Application {
     public static void main(String[] args) {
-
+        GameController controller = new GameController();
+        controller.run();
     }
 }
--- src/main/java/baseball/view/AppView.java
+++ src/main/java/baseball/view/AppView.java
@@ -19,5 +19,4 @@ public class AppView {
         String line = readLine();
         return line;
     }
-
 }
