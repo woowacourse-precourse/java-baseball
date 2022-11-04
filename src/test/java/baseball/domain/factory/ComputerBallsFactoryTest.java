2f73a3dcfc5d22aa2642015e919eee5796b31567
JunH <tjdtls690@gmail.com>
2022-11-04 20:52:55 +0900

    :art: refactor(Domain : ComputerBallsFactoryTest) : 가독성을 위해 코드 작성을 단계별 작성으로 리팩토링

--- src/test/java/baseball/domain/factory/ComputerBallsFactoryTest.java
+++ src/test/java/baseball/domain/factory/ComputerBallsFactoryTest.java
@@ -3,6 +3,7 @@ package baseball.domain.factory;
 import baseball.domain.Ball;
 import baseball.domain.BallStatus;
 import baseball.domain.Balls;
+import baseball.domain.strategy.BallsCreateStrategy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,8 +16,10 @@ class ComputerBallsFactoryTest {
     @Test
     @DisplayName("Computer Balls 생성")
     void create_computer_balls() {
-        Balls balls = ComputerBallsFactory.createComputerBalls(() -> List.of(new Ball(0, 4), new Ball(1, 5), new Ball(2, 2)));
-        
-        assertThat(balls.compareBalls(new Balls("415"))).isEqualTo(List.of(BallStatus.STRIKE, BallStatus.NOTHING, BallStatus.BALL));
+        final BallsCreateStrategy ballsCreateStrategy = () -> List.of(new Ball(0, 4), new Ball(1, 5), new Ball(2, 2));
+        Balls balls = ComputerBallsFactory.createComputerBalls(ballsCreateStrategy);
+    
+        final List<BallStatus> expectedPitchResults = List.of(BallStatus.STRIKE, BallStatus.NOTHING, BallStatus.BALL);
+        assertThat(balls.compareBalls(new Balls("415"))).isEqualTo(expectedPitchResults);
     }
 }
