e93c73a2bcdd09ff24c2b344e1f07702addd5a84
woosung1223 <dntjd991223@naver.com>
2022-11-04 21:00:43 +0900

    test(IntComparator): ParameterizedTest를 통해 여러 케이스를 한번에 수행하도록 수정

--- src/test/java/baseball/IntComparatorTest.java
+++ src/test/java/baseball/IntComparatorTest.java
@@ -2,19 +2,16 @@ package baseball;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class IntComparatorTest {
-    @DisplayName("다른 수라면 false를 반환한다")
-    @Test
-    void returnFalse() {
-        assertFalse(IntComparator.isEqual(1, 2));
-    }
-
-    @DisplayName("같은 수라면 true를 반환한다")
-    @Test
-    void returnTrue() {
-        assertTrue(IntComparator.isEqual(3, 3));
+    @CsvSource(value = {"1, 1, true", "1, 0, false", "1, 2, false"})
+    @ParameterizedTest
+    void returnTrue(int num1, int num2, boolean expect) {
+        assertThat(IntComparator.isEqual(3, 3));
     }
 }
