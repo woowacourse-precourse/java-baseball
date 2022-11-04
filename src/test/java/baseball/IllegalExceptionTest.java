e882e2b6b5f16d150e10fd1f24d5a2b244a00462
junhei Lee <jun2ee22@naver.com>
2022-11-05 00:53:50 +0900

    test: 길이 확인 메소드 변경으로 test 변경

--- src/test/java/baseball/IllegalExceptionTest.java
+++ src/test/java/baseball/IllegalExceptionTest.java
@@ -12,14 +12,14 @@ public class IllegalExceptionTest {
     void 입력_문자() {
         String[] arrays = {"1","2","d"};
         Assertions.assertThrows(IllegalArgumentException.class,
-                ()->illegalArgument.check(arrays));
+                ()->illegalArgument.check(arrays, Game.answerLength));
     }
 
     @Test
     void 입력_3초과() {
         String[] arrays = {"1","2","3","4"};
         Assertions.assertThrows(IllegalArgumentException.class,
-                ()->illegalArgument.check(arrays));
+                ()->illegalArgument.check(arrays, Game.answerLength));
 
     }
 
@@ -27,13 +27,13 @@ public class IllegalExceptionTest {
     void 입력_중복포함() {
         String[] arrays = {"3","4","3"};
         Assertions.assertThrows(IllegalArgumentException.class,
-                ()->illegalArgument.check(arrays));
+                ()->illegalArgument.check(arrays, Game.answerLength));
     }
 
     @Test
     void 입력_0포함() {
         String[] arrays = {"2","4","0"};
         Assertions.assertThrows(IllegalArgumentException.class,
-                ()->illegalArgument.check(arrays));
+                ()->illegalArgument.check(arrays, Game.answerLength));
     }
 }
