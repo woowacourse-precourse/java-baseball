d1c3e0bc5acf60182ae8eb52b0f76e7cfc95dc07
junhei Lee <jun2ee22@naver.com>
2022-11-05 02:16:15 +0900

    test: 재시작 여부 확인 시 테스트

--- src/test/java/baseball/IllegalExceptionTest.java
+++ src/test/java/baseball/IllegalExceptionTest.java
@@ -9,14 +9,14 @@ public class IllegalExceptionTest {
     IllegalArgument illegalArgument = new IllegalArgument();
 
     @Test
-    void 입력_문자() {
+    void 정답입력_문자() {
         String[] arrays = {"1", "2", "d"};
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> illegalArgument.check(arrays, Game.answerLength, Game.numberForAnswer));
     }
 
     @Test
-    void 입력_3초과() {
+    void 정답입력_3초과() {
         String[] arrays = {"1", "2", "3", "4"};
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> illegalArgument.check(arrays, Game.answerLength, Game.numberForAnswer));
@@ -24,16 +24,23 @@ public class IllegalExceptionTest {
     }
 
     @Test
-    void 입력_중복포함() {
+    void 정답입력_중복포함() {
         String[] arrays = {"3", "4", "3"};
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> illegalArgument.check(arrays, Game.answerLength, Game.numberForAnswer));
     }
 
     @Test
-    void 입력_0포함() {
+    void 정답입력_0포함() {
         String[] arrays = {"2", "4", "0"};
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> illegalArgument.check(arrays, Game.answerLength, Game.numberForAnswer));
+    }
+
+    @Test
+    void 재시작여부입력_1과2_외의_숫자() {
+        String[] arrays = {"7"};
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> illegalArgument.check(arrays, Game.intentionLength, Game.numberForIntention));
     }
 }
