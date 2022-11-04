9666a1702371c9763f3b7157e9d84c85f29d0b1e
kkh <kkho9654@naver.com>
2022-11-04 20:47:12 +0900

    Feat: 사용자 입력과 정답을 비교
    비교한 후 스트라이크개수와 볼개수를 출력한다
    스트라이크와 볼이 모두 0이면 낫싱출력
    게임종료여부를 알려주기 위해 스트라이크개수 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -51,4 +51,30 @@ public class Application {
             }
         }
     }
+
+
+    private static int compareUserInputToAnswer(String userInput, String answer) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (userInput.charAt(i) == answer.charAt(i)) {
+                strike += 1;
+            } else if (answer.contains(userInput.charAt(i) + "")) {
+                ball += 1;
+            }
+        }
+        if(ball!=0){
+            System.out.print(ball + "볼 ");
+        }
+        if(strike!=0){
+            System.out.print(strike + "스트라이크");
+        }
+        if(ball==0&&strike==0){
+            System.out.print("낫싱");
+        }
+        System.out.println();
+
+        return strike;
+    }
 }
