dc3c755734ca7ae75f62ed39ec7daa2fbbef7cec
ksh-beginner <ohk9134@naver.com>
2022-11-04 10:18:48 +0900

    feat(InputView) : 사용자가 입력한 수가 3자리 아닐 때 예외 발생시키는 기능 구

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,6 +5,18 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
 
     public static String inputNumber() {
-        return Console.readLine();
+        String inputNumber = Console.readLine();
+        validateInputNumber(inputNumber);
+        return inputNumber;
+    }
+
+    private static void validateInputNumber(String inputNumber) {
+        if (isInputNumberLengthNotSame3(inputNumber)) {
+            throw new IllegalArgumentException("[ERROR] : 입력하는 수는 3자리여야합니다.");
+        }
+    }
+
+    private static boolean isInputNumberLengthNotSame3(String inputNumber) {
+        return inputNumber.length() != 3;
     }
 }
