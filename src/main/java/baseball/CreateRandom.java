3d7cfd41599af9f5034205b7a42c9b68b73162fe
YSEO <1park4170@gmail.com>
2022-11-04 16:35:36 +0900

    refactor<making Random> : change access modifier, add one more method for single responsibility principle
    - change access modifiers of methods that are not referenced outside class into "private"
    - seperate method "fillArray", when filling array with random numbers
    
    Before:
    method declaration
    {
     public boolean checkIfDuplicatedNumber
    }
    
    After:
    method declaration
    {
     private boolean checkIfDuplicatedNumber
    }
    add
    {
    fillArray method
    }

--- src/main/java/baseball/CreateRandom.java
+++ src/main/java/baseball/CreateRandom.java
@@ -1,18 +1,19 @@
 package baseball;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.IntStream;
-
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 class CreateRandom {
     public int[] makeRandomNums() {
-        int random;
         int[] randomNumbers = new int[3];
         int i = 0;
 
+        fillArray(i, randomNumbers);
+
+        return randomNumbers;
+    }
+
+    private void fillArray(int i, int[] randomNumbers) {
+        int random;
         while (i < 3) {
             random = pickNumberInRange(0, 9);
             boolean check = checkIfDuplicatedNumber(random,randomNumbers);
@@ -21,11 +22,9 @@ class CreateRandom {
             randomNumbers[i] = random;
             i++;
         }
-
-        return randomNumbers;
     }
 
-    public boolean checkIfDuplicatedNumber(int random, int[] randomNumbers) {
+    private boolean checkIfDuplicatedNumber(int random, int[] randomNumbers) {
         for(int i = 0 ; i < 3; i++ ) {
             if (randomNumbers[i] == random) {
                 return true;
