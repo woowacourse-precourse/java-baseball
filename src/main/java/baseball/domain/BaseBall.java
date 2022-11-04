e544a19ceb4abed58dded49ce3efba7f9c1d95fa
woo-chang <clllickme@naver.com>
2022-11-04 17:59:53 +0900

    feat: 컴퓨터와 플레이어 숫자 비교 후 게임 상태를 저장하는 기능 구현

--- src/main/java/baseball/domain/BaseBall.java
+++ src/main/java/baseball/domain/BaseBall.java
@@ -5,21 +5,27 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class BaseBall implements Game {
 
     private final List<Integer> computerNumbers;
     private final List<Integer> playerNumbers = new ArrayList<>(GameValidator.GAME_NUMBER_LENGTH);
+    private int strike;
+    private int ball;
 
     public BaseBall() {
         computerNumbers = Randoms.pickUniqueNumbersInRange((int) GameValidator.START_RANGE,
             (int) GameValidator.END_RANGE, GameValidator.GAME_NUMBER_LENGTH);
+        resetGameStatus();
     }
 
     @Override
     public void play() {
         playerInput();
+        compareNumbers();
     }
 
     private void playerInput() {
@@ -36,4 +42,35 @@ public class BaseBall implements Game {
                 .collect(Collectors.toList())
         );
     }
+
+    private void compareNumbers() {
+        resetGameStatus();
+        IntStream.range(0, playerNumbers.size())
+            .forEach(this::storeGameStatus);
+    }
+
+    private void resetGameStatus() {
+        strike = 0;
+        ball = 0;
+    }
+
+    private void storeGameStatus(int index) {
+        if (isStrike(index)) {
+            strike += 1;
+        }
+
+        if (!isStrike(index) && isBall(index)) {
+            ball += 1;
+        }
+    }
+
+    private boolean isStrike(int index) {
+        return Objects.equals(computerNumbers.get(index), playerNumbers.get(index));
+    }
+
+    private boolean isBall(int index) {
+        return computerNumbers.contains(playerNumbers.get(index));
+    }
+
+
 }
