3a3c61c4868a9f32a83e98ae3a053a45845b4257
Minjune <minjune8506@gmail.com>
2022-11-04 20:56:14 +0900

    feat: 볼 개수 세는 메서드 추가

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -7,9 +7,28 @@ public class Referee {
 
     public static final int CONTINUATION = 0;
     public static final int END = 1;
+    private static final int LIST_SIZE = 3;
 
     public static void judge(String guess, List<Integer> answer) {
         List<Integer> convertedGuess = stringToIntegerList(guess);
+        int ballCount = getBallCount(convertedGuess, answer);
+    }
+
+    private static int getBallCount(List<Integer> guess, List<Integer> answer) {
+        int ballCount = 0;
+
+        for (int position = 0; position < LIST_SIZE; position++) {
+            if (isBall(guess, answer, position)) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+
+    private static boolean isBall(List<Integer> guess, List<Integer> answer, int position) {
+        int guessNumber = guess.get(position);
+        int answerNumber = answer.get(position);
+        return answer.contains(guessNumber) && answerNumber != guessNumber;
     }
 
     private static List<Integer> stringToIntegerList(String guess) {
