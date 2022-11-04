bbd355f1e74a10d94e8caa171060a10289549fe0
i960107 <i960107@naver.com>
2022-11-04 15:04:35 +0900

    feat(BaseballScore): add feature of converting score to message

--- src/main/java/baseball/BaseballScore.java
+++ src/main/java/baseball/BaseballScore.java
@@ -1,4 +1,58 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballScore {
+    private final int ALL_STRIKE = BaseballNumber.NUMBER_COUNT;
+    private final int NOTHING = 0;
+
+    private final String STRIKE_STRING = "스트라이크";
+    private final String BALL_STRING = "볼";
+    private final String NOTHING_MESSAGE = "낫싱";
+    private final String ALL_STRIKE_MESSAGE = Integer.toString(ALL_STRIKE) + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    private int strikes;
+    private int ball;
+
+
+    BaseballScore(int strikes, int ball) {
+        this.strikes = strikes;
+        this.ball = ball;
+    }
+
+    @Override
+    public String toString() {
+        if (is_nothing()) {
+            return NOTHING_MESSAGE;
+        }
+
+        List<String> stringList = new ArrayList<>();
+
+        if (ball != NOTHING) {
+            stringList.add(Integer.toString(ball) + BALL_STRING);
+        }
+
+        if (strikes != NOTHING) {
+            if (stringList.size() > 0) {
+                stringList.add(" ");
+            }
+            stringList.add(Integer.toString(strikes) + STRIKE_STRING);
+        }
+
+        if (is_all_strike()) {
+            stringList.add(System.lineSeparator() + ALL_STRIKE_MESSAGE);
+        }
+
+        return String.join("", stringList);
+    }
+
+    public boolean is_nothing() {
+        return this.strikes == NOTHING && this.ball == NOTHING;
+    }
+
+    public boolean is_all_strike() {
+        return strikes == ALL_STRIKE;
+    }
+
 }
--- src/test/java/baseball/BaseballScoreTest.java
+++ src/test/java/baseball/BaseballScoreTest.java
@@ -1,4 +1,53 @@
 package baseball;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 public class BaseballScoreTest {
+    @Test
+    void 야구_게임_점수를_출력한다_올_스트라이크() {
+        BaseballScore score = new BaseballScore(BaseballNumber.NUMBER_COUNT, 0);
+        String result = score.toString();
+        String expected = Integer.toString(BaseballNumber.NUMBER_COUNT)
+                + "스트라이크"
+                + System.lineSeparator()
+                + Integer.toString(BaseballNumber.NUMBER_COUNT)
+                + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+//        String expected = "3스트라이크\n"
+//                + "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void 야구_게임_점수를_출력한다_낫싱() {
+        BaseballScore score = new BaseballScore(0, 0);
+        String result = score.toString();
+        String expected = "낫싱";
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void 야구_게임_점수를_출력한다_1볼2스트라이크() {
+        BaseballScore score = new BaseballScore(2, 1);
+        String result = score.toString();
+        String expected = "1볼 2스트라이크";
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void 야구_게임_점수를_출력한다_2스트라이크() {
+        BaseballScore score = new BaseballScore(2, 0);
+        String result = score.toString();
+        String expected = "2스트라이크";
+        assertEquals(result, expected);
+    }
+
+    @Test
+    void 야구_게임_점수를_출력한다_2볼() {
+        BaseballScore score = new BaseballScore(0, 2);
+        String result = score.toString();
+        String expected = "2볼";
+        assertEquals(result, expected);
+    }
 }
