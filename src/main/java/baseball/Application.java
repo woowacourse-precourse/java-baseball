c7a9609899866d4b4699c1b4156c763c0d02ddb2
jeongjihun <wlgns2234@naver.com>
2022-11-05 08:03:56 +0900

    feat: 게임 재시작할지 확인하는 기능구현
    사용자 입력 숫자 값에따라 게임을 재시작 혹은 종료하도록 기능구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,18 @@
 package baseball;
 
+class Game {
+    public final int RESTART = 1;
+    public final int END = 2;
+
+    public void restartGame(int userInputNum) {
+        if (userInputNum == RESTART)
+            System.out.println("게임시작함수");
+
+        if (userInputNum == END)
+            System.out.println("게임종료함수");
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
