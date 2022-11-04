82ed6b601b9f6c1284fb228abf6e45e45c278084
TaeHwan <kimta2hwan@gmail.com>
2022-11-04 23:33:06 +0900

    refactor(inputview): 추측한 답을 입력받는 메서드명을 더 명확하게 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public static String getAnswer() {
+    public static String getGuessedAnswer() {
         return Console.readLine();
     }
 
