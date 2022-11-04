2cf9958223fc93e4ef7a2bc6b6f214976f1ea3f4
jingyu9804 <swc05161@gmail.com>
2022-11-05 02:00:58 +0900

    refactor : 입력 유효성 출력 메세지 추가

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -9,4 +9,8 @@ public class Constant {
     public static final String strikeMessage="스트라이크";
     public static final String ballMessage="볼";
     public static final String nothingMessage="낫싱";
+
+    public static final String invalidNumberMessage="1-9 사이의 유효한 숫자만 입력하세요.";
+    public static final String invalidLengthMessage="3자리 숫자만 입력하세요.";
+    public static final String duplicatedNumberMessage="중복된 숫자를 입력하셨습니다.";
 }
