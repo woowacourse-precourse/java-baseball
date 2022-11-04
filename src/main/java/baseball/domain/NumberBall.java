5ee99dee8b975f352c40da368d5400002b2d429a
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-04 22:30:37 +0900

    feat<Numberball>: equals hashcode 추가

--- src/main/java/baseball/domain/NumberBall.java
+++ src/main/java/baseball/domain/NumberBall.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import java.util.Objects;
+
 public class NumberBall {
     private int number;
 
@@ -16,5 +18,26 @@ public class NumberBall {
 
     private boolean rangeNumber(int number) {
         return number >= 1 && number <= 9;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        NumberBall that = (NumberBall) o;
+        return getNumber() == that.getNumber();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNumber());
     }
 }
