72e80d786ae146dd07a7762b2ad07447126f8e38
JaeHong-Ahn <63873742+JaeHong-Ahn@users.noreply.github.com>
2022-11-04 17:10:56 +0900

    <기능추가> cntBall 메서드 완료
    Ball의 개수를 count하는 기능 완료
    
    임의로 생성된 난수와 사용자가 입력한 숫자를 각각 비교하여 위치에 상관없이 숫자가 포함되어 있을 때마다 cnt에 1씩 추가함.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,47 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
+    public String randomNum = "";
+
+    public void randomNum(){
+        while(randomNum.length() < 3) {
+            for (int i = 0; i < 3; i++) {
+                int num = Randoms.pickNumberInRange(1, 9);
+                String numToString = Integer.toString(num);
+                if (randomNum.contains(numToString) == false) {
+                    randomNum += numToString;
+                }
+            }
+        }
+    }
+
+    public int cntBall(String getNum){
+        int cnt = 0;
+        char[] getNumList = getNum.toCharArray();
+        char[] randomNumList = randomNum.toCharArray();
+
+        for(int i = 0; i< 3; i++){
+            for(int j = 0 ; j < 3 ; j ++){
+                if(randomNumList[i] == getNumList[j]){
+                    cnt ++;
+                }
+            }
+        }
+        return cnt;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Application application = new Application();
+        application.randomNum(); // 난수생성
+        String userNum = Console.readLine(); // 점수받기
+
+        System.out.println(application.cntBall(userNum));
+
     }
 }
