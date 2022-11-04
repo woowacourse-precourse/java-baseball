465c715693750b1189e6c49296f854f1339abfb0
rt3310 <btac3310@gmail.com>
2022-11-05 00:12:27 +0900

    feat: 숫자와 순서를 가진 공 객체 구현

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -1,4 +1,11 @@
 package baseball;
 
 public class Ball {
+    private int number;
+    private int order;
+
+    public Ball(int number, int order) {
+        this.number = number;
+        this.order = order;
+    }
 }
