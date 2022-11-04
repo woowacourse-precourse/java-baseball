8eb5ad32b17d5bfe0841df04d7898157b73d7e95
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-04 09:51:02 +0900

    <feat>PlayingBaseball : 컴퓨터의 수와 사용자의 수를 입력 받는 기능

---
+++ src/main/java/baseball/PlayingBaseball.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class PlayingBaseball {
+    private final String numberOfComputer = new String();
+    private final String numberOfPlayer = new String();
+    private int strike = 0;
+    private int ball = 0;
+    private List indexOfStrike = new ArrayList<String>();
+
+    PlayingBaseball (String numberComputer, String numberPlayer) {
+        this.numberOfComputer = numberComputer;
+        this.numberOfPlayer = numberPlayer;
+    }
+
+
+
+
+}
