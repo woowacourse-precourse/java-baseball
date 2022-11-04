77cff5589fe1bd99bb1b3d5202bcbd5846615cf6
ghkdgus29 <ghkdgus29@naver.com>
2022-11-04 11:18:04 +0900

    feat: NumberHandler 클래스에 난수 생성 기능 추가

---
+++ src/test/java/baseball/NumberHandlerTest.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberHandlerTest {
+
+    @Test
+    void 랜덤난수생성() {
+        NumberHandler numberHandler = new NumberHandler();
+        ArrayList<Integer> computerNumber = numberHandler.getComputerNumber();
+
+        for (Integer number : computerNumber) {
+            System.out.print(number);
+        }
+        System.out.println();
+    }
+}
