33eadf7792a3c5257993a58f6bcd629ec6c00539
ysng_ysng <yeasung67@gmail.com>
2022-11-04 12:27:48 +0900

    feat(PlayerComputer): Implement features : isAbleToAddDigit & generateInputNumber
    PlayerComputerTest 클래스에 isAbleToAddDigit와 generateInputNumber 메서드를 구현했습니다.

--- src/main/java/baseball/playerComputer/PlayerComputer.java
+++ src/main/java/baseball/playerComputer/PlayerComputer.java
@@ -4,14 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.HashMap;
 
+// todo: 싱글톤 객체로 만드는게 가능하지 않나?
 public class PlayerComputer {
-	private static HashMap<Integer, Integer> digits;
-
-	private static int getRandomDigit() {
-		return Randoms.pickNumberInRange(1, 9);
-	}
-
 	public int getRandomInputNumber() {
+		HashMap<Integer, Integer> digits;
+		digits = generateInputNumber();
 
 
 	}
@@ -19,8 +16,24 @@ public class PlayerComputer {
 	private HashMap<Integer, Integer> generateInputNumber() {
 		HashMap<Integer, Integer> digits = new HashMap<>();
 		while (!isqualifiedNumber(digits)) {
-			int
+			int randomDigit = getRandomDigit();
+			if (isAbleToAddDigit(digits)) {
+				int order = digits.size() + 1;
+				// randomDigit는 유일한 값이므로 key, order는 value로 삽입된 순서(1부터 시작)
+				digits.put(randomDigit, order);
+				continue;
+			}
+			break;
 		}
+		return digits;
+	}
+
+	private static int getRandomDigit() {
+		return Randoms.pickNumberInRange(1, 9);
+	}
+
+	private boolean isAbleToAddDigit(HashMap<Integer, Integer> digits) {
+		return digits.size() <= 3;
 	}
 
 	private boolean isqualifiedNumber(HashMap<Integer, Integer> digits) {
--- src/test/java/baseball/platerComputer/PlayerComputerTest.java
+++ src/test/java/baseball/platerComputer/PlayerComputerTest.java
@@ -10,14 +10,14 @@ import java.util.TreeSet;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerComputerTest {
-	// 0. Randoms.PickNumberInRange테스트
-	//	@Test void PickNumberInRangeTest() {
+	// 0. getRandomDigitTest
+	//	@Test void getRandomDigitTest() {
 	//		PlayerComputer tester = new PlayerComputer();
-	//		int input = tester.getRandomNumber();
+	//		int input = tester.getRandomdigit();
 	//		assertThat(input).isGreaterThan(0).isLessThan(10);
 	//	}
 
-	//
+	// 중복된 수가 아닌 세자리 수가 나오는지 확인합니다.
 	@Test void getRandomInputNumberTest() {
 		PlayerComputer tester = new PlayerComputer();
 		int input = tester.getRandomInputNumber();
