dac93119548186bd1ae1d93e240df03bd26bdace
June <68291395+june-777@users.noreply.github.com>
2022-11-05 01:25:15 +0900

    feat: 야구공 입력 3개미만 예외처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+		readBalls();
+	}
+
+	public static void readBalls() {
+		String balls = Console.readLine();
+		checkLessThanThreeExecption(balls);
+	}
+
+	public static void checkLessThanThreeExecption(String balls) {
+		if (balls.length() < 3) {
+			throw new IllegalArgumentException("잘못된 입력입니다. 공의 개수가 3개 보다 적습니다.");
+		}
+	}
 }
