f878e903c1ebb908d4e1d70d85fe1b0db445cffd
jthugg <jthug@naver.com>
2022-11-04 18:32:22 +0900

    Feat: 게임 재시작 여부 확인 중 유저 인풋 값 받아오기
    :warning: 미션 제한 사항 중 사용자의 입력은
    camp.nextstep.edu.missionutils
    에서 제공하는 Randoms 및 Console API를 사용하여 구현해야 한다.
    
    Console 클래스의 readline() 메서드를 사용 해 테스트 구동 중
    사용자의 입력을 다시 한번 받을 때 아래와 같이 경고 발생
    
    WARNING: An illegal reflective access operation has occurred
    WARNING: Illegal reflective access by
                    camp.nextstep.edu.missionutils.Console
                    (file:/Users/jt/.gradle/caches/modules-2/files-2.1/
                    com.github.woowacourse-projects/mission-utils/1.0.0/
                    dad5230ec970560465a42a1cade24166e6a424f4/
                    mission-utils-1.0.0.jar) to field
                    java.util.Scanner.sourceClosed
    WARNING: Please consider reporting this to the maintainers of
                    camp.nextstep.edu.missionutils.Console
    WARNING: Use --illegal-access=warn to enable warnings of
                    further illegal reflective access operations
    WARNING: All illegal access operations will be denied
                    in a future release
    
    경고 메세지가 출력 돼도 프로그램은 구동 됨
    자바8 버전 이후 이러한 경고메세지가 출력되는것을 확인
    프로그램 실행 시 --illegal-access에 대한 옵션을
    부여해주면 메세지가 발생하지 않지만 근본적인 해결은 아님

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    final static int INITE_GAME_START_VALUE = 0;
+    final static int INITE_GAME_START_VALUE = 1;
     final static int FLAG_RETRY_INT = 1;
     final static int MIN_RANDOM_VALUE = 1;
     final static int MAX_RANDOM_VALUE = 9;
     final static int MAX_NUMBERS_COUNT = 3;
 
     final static String MESSAGE_START = "숫자 야구 게임을 시작합니다.";
+    final static String MESSAGE_REQUEST_INPUT = "숫자를 입력해주세요 : ";
+    final static String MESSAGE_GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    final static String MESSAGE_RETRY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void main(String[] args) {
         System.out.println(MESSAGE_START);
@@ -35,12 +39,15 @@ public class Application {
         final List<Integer> COMPUTERS_NUMBERS = List.copyOf(setComputersNumbers());
 
         /* 게임 시퀀스 루프 */
-        int strike = INITE_STRIKE_COUNT;
-        while(strike < MAX_STRIKE_COUNT) {
-
-        }
+//        int strike = INITE_STRIKE_COUNT;
+//        while(strike < MAX_STRIKE_COUNT) {
+//
+//        }
 
         /* 게임 재시작 여부 확인 파트 */
+        System.out.println(MESSAGE_GAME_OVER);
+        System.out.println(MESSAGE_RETRY);
+        String playAgain = checkRePlay();
 
         return /* gameStart 변수에 입력할 값 1 or 2 */;
     }
@@ -57,5 +64,10 @@ public class Application {
         }
 
         return computersNumbers;
+    }
+
+    private static String checkRePlay() {
+        System.out.println(MESSAGE_REQUEST_INPUT);
+        return Console.readLine();
     }
 }
