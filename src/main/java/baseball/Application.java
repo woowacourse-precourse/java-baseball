b32973fbe5e5aa93a09d4bcc00b0c6713f94daaf
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-11-04 20:42:36 +0900

    docs: 기능목록에 입력 문자열 set으로 변환 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
 
@@ -20,6 +21,14 @@ public class Application {
     public static boolean isValidLength(int n) {
         return n == 3;
     }
+
+    public static Set<Integer> isInputIsNonDuplicate(String s) {
+
+        return null;
+
+
+    }
+
 
     public static int GenerateNonDuplicateRandomNumber(List<Integer> randNum) {
         int k;
