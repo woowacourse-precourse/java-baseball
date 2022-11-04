9f993cacf2eb8a2cbf19838dc35740ef9f565ea8
BuildTools <unconfigured@null.spigotmc.org>
2022-11-04 18:25:19 +0900

    feat: 추가 사용자에게 입력값을 받는기능

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class View {
+    static Scanner scanner = new Scanner(System.in);
     static final void printStartGame(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    static final String getUserInput(){
+        return scanner.next();
     }
 }
