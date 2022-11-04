22ec68b50bfe5480c5ea2f778de8ec662e43e162
kjb95 <topmedia44@gmail.com>
2022-11-04 20:17:32 +0900

    feat: 스트라이크 수 세기 기능 추가

--- src/main/java/baseball/model/GameResult.java
+++ src/main/java/baseball/model/GameResult.java
@@ -1,18 +1,31 @@
 package baseball.model;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class GameResult {
 
-    private final int ballCount;
+    private static final int NUM_SIZE = 3;
+
     private final int strikeCount;
+    private final int ballCount;
 
     public GameResult(List<Integer> answerNums, List<Integer> playerNums) {
+        strikeCount = countStrike(answerNums, playerNums);
         ballCount = 0;
-        strikeCount = 0;
     }
 
-    public boolean isSameStrikeCount(int strikeCount) {
-        return this.strikeCount == strikeCount;
+    private int countStrike(List<Integer> answerNums, List<Integer> playerNums) {
+        return (int) IntStream.range(0, NUM_SIZE)
+            .filter(i -> answerNums.get(i) == playerNums.get(i))
+            .count();
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
     }
 }
--- src/test/java/baseball/model/GameResultTest.java
+++ src/test/java/baseball/model/GameResultTest.java
@@ -9,15 +9,12 @@ class GameResultTest {
 
     @Test
     void 스트라이크_수_세기() {
-        GameResult threeStrike = new GameResult(List.of(1, 2, 3), List.of(2, 3, 1));
-        GameResult twoStrike = new GameResult(List.of(1, 2, 3), List.of(2, 3, 4));
-        GameResult zeroStrike = new GameResult(List.of(1, 2, 3), List.of(6, 4, 3));
+        GameResult threeStrike = new GameResult(List.of(1, 2, 3), List.of(1, 2, 3));
+        GameResult oneStrike = new GameResult(List.of(1, 2, 3), List.of(1, 3, 2));
+        GameResult zeroStrike = new GameResult(List.of(1, 2, 3), List.of(2, 3, 1));
 
-        Assertions.assertThat(threeStrike.isSameStrikeCount(3))
-            .isTrue();
-        Assertions.assertThat(twoStrike.isSameStrikeCount(2))
-            .isTrue();
-        Assertions.assertThat(zeroStrike.isSameStrikeCount(0))
-            .isTrue();
+        Assertions.assertThat(threeStrike.getStrikeCount()).isEqualTo(3);
+        Assertions.assertThat(oneStrike.getStrikeCount()).isEqualTo(1);
+        Assertions.assertThat(zeroStrike.getStrikeCount()).isEqualTo(0);
     }
 }
