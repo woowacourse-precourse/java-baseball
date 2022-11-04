3bd7e119f6c19e5ef3b35e45cd617afce9852dcc
Kim woo jong <skekdnwhd@gmail.com>
2022-11-05 06:58:10 +0900

    Fix : 테스트시 에러나는 현상을 확인. 랜덤 넘버 리스트 만드는 메서드 수정

--- src/main/java/baseball/utils/MakeRandom.java
+++ src/main/java/baseball/utils/MakeRandom.java
@@ -2,6 +2,7 @@ package baseball.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MakeRandom {
@@ -15,13 +16,12 @@ public class MakeRandom {
     }
 
     public List<Integer> numbers() {
-//        List<Integer> numberList = new ArrayList<>();
-//        while (numberList.size() < 3) {
-//            int randomNumber = Randoms.pickNumberInRange(1, 9);
-//            if (!numberList.contains(randomNumber))
-//                numberList.add(randomNumber);
-//        }
-//        return numberList;
-        return Randoms.pickUniqueNumbersInRange(1,9,3);
+        List<Integer> numberList = new ArrayList<>();
+        while (numberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numberList.contains(randomNumber))
+                numberList.add(randomNumber);
+        }
+        return numberList;
     }
 }
