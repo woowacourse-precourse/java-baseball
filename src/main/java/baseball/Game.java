df2b61fe348c7f9faf3a23dfbbb66d67fd58a1da
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-04 21:13:52 +0900

    Update Game.java
    type : refactor : "함수 줄이기 및 전역변수 사용"
    
    처음 사용된 코드에는 스크라이크 와 볼 함수에 같은 반복문이 들어가는 경우가 많이 발생하였다. 그걸 줄이기 위해서 함수 하나에 반복문을 사용하고 거기에 스크라이크와 볼을 찾는 함수를 호출하였다. 또한 return으로 지역 변수에 값을 저장할려고 하니 return에 호출이 많아져서 가독성이 떨어져 보였다. 그러므로 전역변수로 카운트를 해 가독성을 더 좋게 하였다.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,57 +4,51 @@ import java.util.List;
 import java.util.Objects;
 
 public class Game {
-Input_Output IO = new Input_Output();
-
-public void Print()
-{
-    int Strike_count=0;
-    int Ball_count=0;
-
-    IO.Computer_RandomNumber();
-    System.out.println(IO.computer);
-    IO.User_enter();
-
-
-    Strike_count = Strike_Check(IO.computer,IO.Scan,Strike_count);
-    Ball_count = Ball_Check(IO.computer,IO.Scan,Ball_count);
-
-    System.out.println(Strike_count + " 스트라이크 "+ Ball_count + "볼");
-
-
-}
-
-public static int Strike_Check(List<Integer> computer,List<Integer> User_Number,int Strike_count)
-{
-    for(int i=0; i<computer.size();i++)
-      Strike_count = Number_Check(computer.get(i), User_Number.get(i),Strike_count);
-
-   return Strike_count;
-}
-
-public static int Ball_Repeat(int computer, List<Integer> User, int repeat_Number,int Ball_count)//볼을 확인하기 위한 반복
-{
-    for(int j =0; j<User.size();j++)
-        if(repeat_Number != j)//스트라이크 제외하기
-           Ball_count = Number_Check(computer,User.get(j),Ball_count);
-
-    return Ball_count;
-}
-
-    public static int Ball_Check(List<Integer> computer,List<Integer> User,int Ball_count)
+    Input_Output IO = new Input_Output();
+    static int Strike_count=0;
+    static int Ball_count=0;
+    public void Print()
     {
-        for(int i =0;i<computer.size();i++)
-            Ball_count =Ball_Repeat(computer.get(i),User,i,Ball_count);
 
-        return Ball_count;
+
+        IO.Computer_RandomNumber();
+        System.out.println(IO.computer);
+        IO.User_enter();
+        BaseNumber_Check(IO.computer,IO.Scan);
+        System.out.println(Strike_count + " 스트라이크 "+ Ball_count + "볼");
+
     }
 
-    public static int Number_Check(int computer,int User,int count)//같은 숫자가 맞는지 확인
+    public static void BaseNumber_Check(List<Integer> computer,List<Integer> User)
+    {
+        for(int i=0; i<computer.size();i++)
+        {
+            if(Number_Check(computer.get(i),User.get(i)))
+                Strike_count++;
+            else
+                Ball_Repeat(computer.get(i),User);
+
+
+        }
+    }
+
+    public static boolean Ball_Repeat(int computer, List<Integer> User)//볼을 확인하기 위한 반복
+    {
+        for(int j =0; j<User.size();j++)
+        {
+            if(Number_Check(computer,User.get(j)))
+                Ball_count++;
+        }
+
+        return false;
+    }
+
+    public static boolean Number_Check(int computer,int User)//같은 숫자가 맞는지 확인
     {
         if (Objects.equals(computer, User))
-            count++;
+            return true;
 
-        return count;
+        return false;
     }
 
 
