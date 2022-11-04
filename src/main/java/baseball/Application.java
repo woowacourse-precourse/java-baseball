c876949e64eaced2094e578bfe8e754752f87e4b
choyeram <choyeram8@gmail.com>
2022-11-04 10:23:58 +0900

    fix: 미사용 상수 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    private static final int RESTART = 1;
     private static final int FINISH = 2;
 
     public static void main(String[] args) {
