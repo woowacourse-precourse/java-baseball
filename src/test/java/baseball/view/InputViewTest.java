4aacc0e29b05bce2555d3be7dd6c6f87f7b75c24
ksh-beginner <ohk9134@naver.com>
2022-11-04 09:54:19 +0900

    test(InputViewTest) : 사용자가 입력한 수 입력 받기 테스트

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -1,5 +1,21 @@
 package baseball.view;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class InputViewTest {
-    
+
+    @ParameterizedTest
+    @ValueSource(strings = {"234"})
+    void 사용자가_입력한_수_입력_받기(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        String playerInputNumber = InputView.inputNumber();
+        assertThat(playerInputNumber).isEqualTo(input);
+    }
 }
