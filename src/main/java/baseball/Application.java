115c9d5c080515fd30f2cf7fbe7db2a3c40d808e
jthugg <jthug@naver.com>
2022-11-04 09:30:06 +0900

    Feat: 상대방의 숫자 리스트를 생성한다
    상대방(컴퓨터)의 숫자 리스트를 생성한다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    final static int INITE_GAME_START_VALUE = 0;
+    final static int FLAG_RETRY_INT = 1;
+    final static int MIN_RANDOM_VALUE = 1;
+    final static int MAX_RANDOM_VALUE = 9;
+    final static int MAX_NUMBERS_COUNT = 3;
+
     public static void main(String[] args) {
         int gameStart = INITE_GAME_START_VALUE;
 
@@ -17,7 +28,7 @@ public class Application {
 
     private static int playBall() {
         /* 컴퓨터 숫자 정하기 파트 */
-        final List<Integer> COMPUTERS_NUMBERS;
+        final List<Integer> COMPUTERS_NUMBERS = List.copyOf(setComputersNumbers());
 
         /* 게임 시퀀스 루프 */
         int strike = INITE_STRIKE_COUNT;
@@ -28,5 +39,19 @@ public class Application {
         /* 게임 재시작 여부 확인 파트 */
 
         return /* gameStart 변수에 입력할 값 1 or 2 */;
+    }
+
+    private static List<Integer> setComputersNumbers() {
+        List<Integer> computersNumbers = new ArrayList<>(MAX_NUMBERS_COUNT);
+
+        while(computersNumbers.size() < MAX_NUMBERS_COUNT) {
+            final int RANDOM_NUMBER = Randoms.pickNumberInRange(MIN_RANDOM_VALUE, MAX_RANDOM_VALUE);
+
+            if(!computersNumbers.contains(RANDOM_NUMBER)) {
+                computersNumbers.add(RANDOM_NUMBER);
+            }
+        }
+
+        return computersNumbers;
     }
 }
