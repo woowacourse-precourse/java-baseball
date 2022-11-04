0cd87f65050756800504a0378e79d4b8bdbc6928
nn98 <jkllhgb@gmail.com>
2022-11-05 06:50:00 +0900

    Feat : 사용할 메소드 선언

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,27 @@
 package baseball;
-
+import java.util.*;
 public class Application {
+    static int target,input,count;
+    static int game(int target,int input){
+        int strikeCount=0;
+        return strikeCount;
+    }
+    static List<Integer>splitDigits(int value){
+        List<Integer>returnDate=Collections.emptyList();
+        return returnDate;
+    }
+    static int checkResult(List<Integer> target,List<Integer> input){
+        int result=0;
+        return result;
+    }
+    static int isStrike(int target,int input){
+        int result=0;
+        return result;
+    }
+    static int isBall(int target,List<Integer> input){
+        int result=0;
+        return result;
+    }    
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
