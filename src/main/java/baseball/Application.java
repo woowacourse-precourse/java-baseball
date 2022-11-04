9bb3eea5bb61f57df4355faafec878250247d8dd
LEE-haeuk <rkwksmswhdk@naver.com>
2022-11-04 23:59:27 +0900

    feat: add the computer's random numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+        Application application = new Application();
+        List<Integer> computer = new ArrayList<>();
+
         System.out.println("숫자 야구 게임을 시작합니다.");
+        computer = application.createRandomNumbersInList();
+    }
+
+    public List<Integer> createRandomNumbersInList() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
     }
 }
