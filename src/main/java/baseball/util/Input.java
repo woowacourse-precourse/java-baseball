7cc609069e6595a3ede56f62678adc6d5af78fbd
hum02 <ehdgma02@gmail.com>
2022-11-04 16:40:29 +0900

    feat:게임 재시작/종료값 입력받기

---
+++ src/main/java/baseball/util/Input.java
@@ -0,0 +1,9 @@
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    public static String inputRestartFlag() {
+        return Console.readLine();
+    }
+}
