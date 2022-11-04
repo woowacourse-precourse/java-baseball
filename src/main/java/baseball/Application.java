018e02ea305b0d93242142988c9c7603a9eb8958
cksdud8028 <cksdud8028@naver.com>
2022-11-04 16:59:39 +0900

    Writting FunctionList and Practice API

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
+        String readLine = Console.readLine();
+
+        int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+
+
+        System.out.println("readLine = " + readLine);
+        System.out.println("randomNumber = " + randomNumber);
         // TODO: 프로그램 구현
     }
 }
