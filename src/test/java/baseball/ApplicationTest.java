c6306538b1441f3b8fd308329cc3d6255b23b5dc
함궈 <heoja0123@naver.com>
2022-11-04 15:47:02 +0900

    test : 사용자 입력값을 검증하는 테스트 코드 작성

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -32,4 +34,55 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
+
+    @Nested
+    @DisplayName("사용자의 입력 테스트")
+    class PlayerInputTest{
+
+        @Test
+        @DisplayName("입력값에 문자가 포함된 경우 예외처리")
+        void PlayerInputTest1(){
+            Player player = new Player();
+
+            assertThatThrownBy(() -> player.input("a23"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("입력값은 숫자만 포함되어야 합니다.");
+
+        }
+
+        @Test
+        @DisplayName("입력값에 0이 포함된 경우 예외처리")
+        void PlayerInputTest2(){
+            Player player = new Player();
+
+            assertThatThrownBy(() -> player.input("012"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("입력값에 0이 포함될 수 없습니다.");
+
+        }
+
+
+        @Test
+        @DisplayName("입력값이 세자리가 아닌 경우 예외처리")
+        void PlayerInputTest3(){
+            Player player = new Player();
+
+            assertThatThrownBy(() -> player.input("0123"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("입력값은 세자리여야 합니다.");
+        }
+
+
+        @Test
+        @DisplayName("입력값에 수의 중복이 존재하는 경우 예외처리")
+        void PlayerInputTest4(){
+            Player player = new Player();
+
+            assertThatThrownBy(() -> player.input("113"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("입력값에 수의 중복이 존재합니다.");
+        }
+    }
+
 }
