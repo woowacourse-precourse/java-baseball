86c1560c1d39df794196563326da10586c60a107
nn98 <jkllhgb@gmail.com>
2022-11-05 08:02:22 +0900

    Feat : 출력값 상수 String으로 분리 및 초기화

---
+++ src/main/java/constant/Texts.java
@@ -0,0 +1,18 @@
+package constant;
+
+public class Texts {
+
+    public static final String GAME_BEGIN = "숫자 야구 게임을 시작합니다.";
+    public static final String GAME_END = "숫자 야구 게임을 종료합니다.";
+    public static final String GAME_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String USER_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String USER_INPUT_ILLEGAL = "잘못된 값을 입력했습니다! 게임을 종료합니다.";
+    public static final String USER_INPUT_ILLEGAL_LENGTH = "입력된 수가 3자리를 초과합니다. ";
+    public static final String USER_INPUT_ILLEGAL_DUPLICATED = "입력된 수에 중복값이 존재합니다. ";
+
+    public static final String RUN_CHECK_STRIKE = "스트라이크";
+    public static final String RUN_CHECK_BALL = "볼";
+    public static final String RUN_CHECK_NOTTING = "낫싱";
+
+}
