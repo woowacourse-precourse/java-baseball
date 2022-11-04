eb532df7b99d56567d3de0ca4fa6700752bf3c65
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-05 00:12:48 +0900

    feat: 컴퓨터 숫자 랜덤 추출 기능
    랜덤 추출 후 String으로 변환 후 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,40 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        try {
+            String computerNumber = setComputerNumber();
+
+        } catch (IllegalArgumentException exception) {
+
+        }
     }
+
+    private static String setComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+
+        return toStringTarget(computerNumber);
+    }
+
+    private static String toStringTarget(List<Integer> target) {
+        String result="";
+        for (Integer num : target) {
+            result += num.toString();
+        }
+
+        return result;
+    }
+
+
 }
