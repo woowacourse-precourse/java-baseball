7909f87014b6edceaeb0211a494e30ad6947e761
tjrqhd121@naver.com <tjrqhd121@naver.com>
2022-11-04 17:44:15 +0900

    feat(application): 정답 리스트 생성
    createBaseballNumberList: 숫자야구를 위한 3자리 랜덤난수 생성 메서드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
+
+    public static List<Integer> createBaseballNumberList() {
+        List<Integer> numberList = new ArrayList<>();
+        while (numberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numberList.contains(randomNumber)) {
+                numberList.add(randomNumber);
+            }
+        }
+        return numberList;
+    }
+
 }
