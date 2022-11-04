ed02847832a97f31ea3f0002c47e702e3158945d
heesang <dlagmltkd12@naver.com>
2022-11-05 06:55:16 +0900

    feat: 1~9까지의 서로 다른 랜덤 숫자 3개를 만드는 기능

--- src/test/java/baseball/RandomNumberCreatorTest.java
+++ src/test/java/baseball/RandomNumberCreatorTest.java
@@ -2,6 +2,8 @@ package baseball;
 
 import org.junit.jupiter.api.RepeatedTest;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RandomNumberCreatorTest {
@@ -10,5 +12,13 @@ public class RandomNumberCreatorTest {
     void _1_9_까지_랜덤숫자_한개_생성() {
         int result = RandomNumberCreator.createRandomNumber();
         assertThat(result).isBetween(1, 9);
+    }
+
+    @RepeatedTest(50)
+    void _1_9_까지_서로다른_랜덤숫자_3개_생성() {
+        List<Integer> result = RandomNumberCreator.createRandomNumbers();
+        for (int randomNumber : result) {
+            assertThat(randomNumber).isBetween(1, 9);
+        }
     }
 }
