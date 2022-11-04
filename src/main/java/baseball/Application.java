68053e85d0aa16acd53e9f101751d88a3f4c19fe
taeyun-work <wmfajr1357@gmail.com>
2022-11-04 18:46:23 +0900

    chore: Application main class 로직 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,16 +5,11 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BaseBallGame baseballGame = BaseBallGame.getGame();
-
-        while(true){
-            try{
-                baseballGame.gameStart();
-                baseballGame.gamePlaying();
-            } catch(IllegalArgumentException e){
-                System.out.println(e);
-                break;
-            }
+        try{
+            baseballGame.gameStart();
+            baseballGame.gamePlaying();
+        } catch(IllegalArgumentException e){
+            System.out.println(e);
         }
-
     }
 }
