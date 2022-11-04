99584e277f3da1925b91f98f89663bd6c879f4de
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-04 22:29:28 +0900

    feat: PlayerException 입력은 세자리여야 합니다 예외처리하는 기능 구현

--- src/main/java/baseball/exception/PlayerException.java
+++ src/main/java/baseball/exception/PlayerException.java
@@ -6,6 +6,7 @@ public class PlayerException {
 
     private static final int LENGTH_LIMIT = 3;
     private static final String NOT_NUMBER_EXCEPTION = "입력은 숫자로만 해야 합니다.";
+    private static final String NOT_THREE_DIGIT_EXCEPTION = "입력은 세자리 숫자여야 합니다. ";
 
     public void checkException(String input){
 
@@ -24,6 +25,7 @@ public class PlayerException {
     }
 
     public void notThreeDigitException(){
+        throw new IllegalArgumentException(NOT_THREE_DIGIT_EXCEPTION);
     }
     public boolean containsZero(String input){
         return false;
