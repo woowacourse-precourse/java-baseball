bb4010dc13e5f539f01b1fcda137d88c3e1d4b61
minhee020 <minhee020@naver.com>
2022-11-04 20:51:55 +0900

    feat : 볼, 스트라이크 개수 출력 함수 구현

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -1,7 +1,7 @@
 package baseball;
 
 public class Hint {
-    private int strike;
+    public int strike;
     private int ball;
 
     public void getHint(int[] userNumber, int[] computerNumber){
@@ -9,6 +9,32 @@ public class Hint {
             countStrike(userNumber[i], computerNumber[i]);
             countBall(userNumber[i], computerNumber, i);
         }
+
+        printHint();
     }
 
+    private void printHint(){
+        if (this.ball > 0) {
+            System.out.print(this.ball + "볼 ");
+        }
+        if (this.strike > 0) {
+            System.out.print(this.strike + "스트라이크 ");
+        }
+        if (this.ball == 0 && this.strike == 0) {
+            System.out.print("낫싱");
+        }
+        System.out.println();
+    }
+    private void countStrike(int user, int computer){
+        if (user == computer){
+            this.strike++;
+        }
+    }
+    private void countBall(int user, int[] computerNumber, int presentIndex){
+        for (int i = 0; i < 3; i++){
+            if (presentIndex != i && computerNumber[i] == user){
+                this.ball++;
+            }
+        }
+    }
 }
