3ac7cf7848d7d1f0182cd35d90459c12bbc99eaa
Jihoon9809 <dkdb521@naver.com>
2022-11-04 16:57:57 +0900

    유저 입력 메소드 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,9 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
 import java.util.*;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static List<Integer> setComputerNumbers(List<Integer> computerNumbers){
@@ -17,12 +18,25 @@ public class Application {
         return computerNumbers;
     }
 
-    public static void main(String[] args) {
-        List<Integer> computerNumbers = new ArrayList<>();
+public static void setUserNumbers(){
+        String userNumber;
+        userNumber = readLine();
+
+        System.out.println(userNumber);
+}
+
+    public static void playBaseball(){
+        /*List<Integer> computerNumbers = new ArrayList<>();
 
         computerNumbers = setComputerNumbers(computerNumbers);
 
-        for(int numbers : computerNumbers) System.out.println(numbers); //난수 출력 확인.
+        for(int numbers : computerNumbers) System.out.println(numbers); //난수 출력 확인.*/
+
+        setUserNumbers();
+    }
+
+    public static void main(String[] args) {
+        playBaseball();
     }
 }
 
