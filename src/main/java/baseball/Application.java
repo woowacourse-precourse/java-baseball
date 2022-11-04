696ad6f4fc7d9de7f5a93bf6abc62d6d50798d6e
yejin <kwonyj1022@naver.com>
2022-11-05 01:37:57 +0900

    게임 시작을 위한 세팅 골격

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ComputerNumber {
+    List<Integer> computerNumber;
+
+    public ComputerNumber() {
+
+    }
+}
+
+class PlayerNumber {
+    List<Integer> playerNumber;
+
+    public PlayerNumber() {
+    }
+}
+
+class GameInit{
+    public GameInit() {
+        ComputerNumber computerNumber = (ComputerNumber) new ComputerNumber().computerNumber;
+        PlayerNumber playerNumber = (PlayerNumber) new PlayerNumber().playerNumber;
+    }
+
+}
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        GameInit startGame = new GameInit();
     }
 }
