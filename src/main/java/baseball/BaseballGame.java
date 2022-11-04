d1fb45396eaa492de64801ca00c48f0d5bcb447a
jeongwle <dldl4568@naver.com>
2022-11-04 21:35:42 +0900

    feat : 게임 종료 또는 재시작 하는 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,9 +5,8 @@ import java.util.List;
 public class BaseballGame {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private boolean onGame;
+    private static final int RESTART_NUMBER = 1;
     public BaseballGame() {
-        onGame = true;
     }
 
     public void startGame() {
@@ -16,12 +15,19 @@ public class BaseballGame {
         Computer computer = new Computer();
         User user = new User();
 
+        boolean onGame = true;
         while (onGame) {
             user.receiveThreeNumber();
 
             List<Integer> userNumbers = user.getInputNumber();
             onGame = !computer.isCorrectAnswer(userNumbers);
             user.resetUserNumber();
+        }
+
+        user.receiveOneNumber();
+        List<Integer> restartOrExit = user.getInputNumber();
+        if (restartOrExit.contains(RESTART_NUMBER)) {
+            startGame();
         }
     }
 }
