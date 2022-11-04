8265a95520f9ea4ae528c35bfd6ce6a27c0a0751
ywj9811 <ywj9811@naver.com>
2022-11-04 14:33:05 +0900

    style: InputException 클래스 가독성 고려

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -5,11 +5,13 @@ import java.util.regex.Pattern;
 
 public class InputException {
     private static final Pattern IsOnlyNumber = Pattern.compile("^[1-9]*?");
+
     public void isNumber(String input) {
         if (input.isEmpty() || !IsOnlyNumber.matcher(input).matches()) {
             throw new IllegalArgumentException("숫자 외의 값이 입력되었습니다.");
         }
     }
+
     public void InputValid(List<Integer> RandomList, List<Integer> inputList) {
 
         if (inputList.size() != RandomList.size()) {
@@ -22,5 +24,6 @@ public class InputException {
                     throw new IllegalArgumentException("입력값이 잘못 되었습니다.");
             }
         }
+
     }
 }
