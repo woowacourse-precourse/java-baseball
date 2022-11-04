cf34da0f87b90cd86e516587422e226044aed18d
jeongwle <dldl4568@naver.com>
2022-11-04 18:51:44 +0900

    chore : try catch문 삭제
    예외를 대처하려 했지만 테스트코드를 보니 불필요하여 삭제

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,7 +5,6 @@ import java.util.List;
 public class BaseballGame {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final String ERROR_MESSAGE = "잘못된 입력입니다. 게임을 종료합니다.";
     private boolean onGame;
     public BaseballGame() {
         onGame = true;
@@ -18,15 +17,11 @@ public class BaseballGame {
         User user = new User();
 
         while (onGame) {
-            try {
-                user.receiveThreeNumber();
-            } catch (IllegalArgumentException e) {
-                System.out.println(ERROR_MESSAGE);
-                onGame = false;
-                continue;
-            }
+            user.receiveThreeNumber();
 
-            List<Integer> userNumber = user.getThreeNumber();
+            List<Integer> userNumbers = user.getThreeNumber();
+            onGame = !computer.isCorrectAnswer(userNumbers);
+            user.resetUserNumber();
         }
     }
 }
