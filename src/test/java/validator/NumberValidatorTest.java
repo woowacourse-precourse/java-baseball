f9efbb7a4d3287d7afecf7f83fa3a68697b9ebbb
kim0914 <kosa0914@naver.com>
2022-11-04 21:38:08 +0900

    test(NumberValidator): 입력 숫자에서 각 자리 수 중복 예외 발생 테스트 추가

--- src/test/java/validator/NumberValidatorTest.java
+++ src/test/java/validator/NumberValidatorTest.java
@@ -12,9 +12,18 @@ class NumberValidatorTest {
     void 숫자_크기가_다른_경우_예외_발생(){
         String checkNumber = "1234";
 
-        assertThatThrownBy(() -> NumberValidator.validate(checkNumber))
+        assertThatThrownBy(() -> NumberValidator.checkSize(checkNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력 숫자의 크기(길이)를 확인하세요.");
+    }
+
+    @Test
+    void 중복된_숫자가_있는_경우_예외_발생(){
+        String checkNumber = "112";
+
+        assertThatThrownBy(() -> NumberValidator.checkUnique(checkNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("각 자리수는 중복을 허락하지 않습니다.");
     }
 
 }
