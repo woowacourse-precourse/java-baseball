03b23ee1e88fac2d37f307b125c5126845fa67ef
ChoiYeonho0903 <903yh@naver.com>
2022-11-05 02:25:26 +0900

    feat: 사용자 숫자 Console을 통해서 입력 기능 구현

---
+++ src/main/java/baseball/view/BaseballConsole.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseballConsole {
+
+    public static String userInputNumber() {
+        return Console.readLine();
+    }
+}
