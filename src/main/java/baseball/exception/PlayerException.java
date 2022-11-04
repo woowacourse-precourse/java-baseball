e7b686af345317b6a6ec415bb0a35525a0758314
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-04 22:27:14 +0900

    feat: PlayerException에서 '입력은 숫자로만 해야 합니다' 예외처리하는 기능 구현

--- src/main/java/baseball/exception/PlayerException.java
+++ src/main/java/baseball/exception/PlayerException.java
@@ -4,6 +4,8 @@ import java.util.regex.Pattern;
 
 public class PlayerException {
 
+    private static final String NOT_NUMBER_EXCEPTION = "입력은 숫자로만 해야 합니다.";
+
     public boolean isNotNumber(String input){
         return !Pattern.matches("^[1-9]*$", input);
     }
@@ -13,7 +15,7 @@ public class PlayerException {
     }
 
     public void noNumberException(){
-
+        throw new IllegalArgumentException(NOT_NUMBER_EXCEPTION);
     }
 
     public boolean isNotThreeDigit(String input){
