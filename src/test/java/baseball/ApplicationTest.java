d68a92c04e3b3b06951229f9213433fede28604e
Devtist <yt930310@snu.ac.kr>
2022-11-04 20:02:23 +0900

    test: 사용자의 입력을 받는 기능 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,15 +3,25 @@ package baseball;
 import static camp.nextstep.edu.missionutils.test.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
+import java.io.ByteArrayInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import User.Input;
 import User.Outputs;
 import camp.nextstep.edu.missionutils.test.NsTest;
 
 class ApplicationTest extends NsTest {
+
+	@Test
+	void getUserInput_메소드로_유저_입력_받기() {
+		String input = "768";
+		System.setIn(new ByteArrayInputStream(input.getBytes()));
+		assertThat(Input.getUserInput()).isEqualTo("768");
+	}
+
 	@Test
 	void print_메소드로_출력할_문자열_리턴() {
 		List<String> result = new ArrayList<>();
@@ -30,21 +40,16 @@ class ApplicationTest extends NsTest {
 
 	@Test
 	void 게임종료_후_재시작() {
-		assertRandomNumberInRangeTest(
-			() -> {
-				run("246", "135", "1", "597", "589", "2");
-				assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-			},
-			1, 3, 5, 5, 8, 9
-		);
+		assertRandomNumberInRangeTest(() -> {
+			run("246", "135", "1", "597", "589", "2");
+			assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+		}, 1, 3, 5, 5, 8, 9);
 	}
 
 	@Test
 	void 예외_테스트() {
-		assertSimpleTest(() ->
-			assertThatThrownBy(() -> runException("1234"))
-				.isInstanceOf(IllegalArgumentException.class)
-		);
+		assertSimpleTest(
+			() -> assertThatThrownBy(() -> runException("1234")).isInstanceOf(IllegalArgumentException.class));
 	}
 
 	@Override
