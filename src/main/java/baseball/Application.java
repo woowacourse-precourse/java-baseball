481eeea44da785278f96207b92fcb40aa8e5ce76
Limjongho <whdgh9873@icloud.com>
2022-11-04 19:28:18 +0900

    feat: 중복되지 않는 랜덤 숫자(상대방 숫자) 추출 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,41 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static List<Integer> pickUniqueNumbersInRange(
+            int startInclusive,
+            int endInclusive,
+            int range
+    ) {
+        List<Integer> uniqueNumbers = new ArrayList<>();
+
+        while (uniqueNumbers.size() < range) {
+            int randomNumber = Randoms.pickNumberInRange(startInclusive, endInclusive);
+            saveUniqueNumber(uniqueNumbers, randomNumber);
+        }
+
+        return uniqueNumbers;
+    }
+
+    public static void saveUniqueNumber(List<Integer> uniqueNumbers, int randomNumber) {
+        if (isExistNumber(uniqueNumbers, randomNumber)) {
+            return;
+        }
+        uniqueNumbers.add(randomNumber);
+    }
+
+    public static boolean isExistNumber(List<Integer> uniqueNumbers, int randomNumber) {
+        if (uniqueNumbers.contains(randomNumber)) {
+            return true;
+        }
+        return false;
     }
 }
