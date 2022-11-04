e59589b94fb7d4e49d3912fca8fab105ac155c26
bellljun3395 <195850@jnu.ac.kr>
2022-11-05 03:20:20 +0900

    feat(ViewConsole): 구현 출력 기능 / 종료
    - 게임 종료 문구를 출력할 수 있어야 한다.

--- src/main/java/baseball/view/ViewConsole.java
+++ src/main/java/baseball/view/ViewConsole.java
@@ -9,6 +9,7 @@ public class ViewConsole implements Viewable {
 
     private final String START_LINE = "숫자 야구 게임을 시작합니다.";
     private final String INPUT_LINE = "숫자를 입력해주세요 :";
+    private final String END_LINE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private final String CONTINUE_LINE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
 
@@ -52,7 +53,7 @@ public class ViewConsole implements Viewable {
 
     @Override
     public void printEnd() {
-
+        System.out.println(END_LINE);
     }
 
     @Override
