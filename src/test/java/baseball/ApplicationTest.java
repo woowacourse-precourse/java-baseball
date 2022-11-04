d3f5cb0b5f44fbe3b98a66e2cfd1518613148243
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-05 00:14:05 +0900

    Style: 누락된 @Test 애너테이션 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -115,18 +115,22 @@ class ApplicationTest extends NsTest {
         assertThat(Application.checkStrikeCount(123, 321)).isEqualTo(1);
     }
 
+    @Test
     void checkBall1(){
         assertThat(Application.checkStrikeCount(123, 143)).isEqualTo(0);
     }
 
+    @Test
     void checkBall2(){
         assertThat(Application.checkStrikeCount(987, 765)).isEqualTo(1);
     }
 
+    @Test
     void checkBall3(){
         assertThat(Application.checkStrikeCount(915, 125)).isEqualTo(1);
     }
 
+    @Test
     void checkBall4(){
         assertThat(Application.checkStrikeCount(915, 159)).isEqualTo(2);
     }
