62753ac768d8f2a98db71aab2d117cc7a7544d0f
CMHyuk <alsgur9710@naver.com>
2022-11-04 20:51:46 +0900

    feat: 숫자 야구 게임 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import static baseball.Config.gameService;
+import static baseball.PlayGameController.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        run(gameService());
     }
 }
