b40e370fde7dd5032769d3f6bbe7e26d3cfa0986
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-04 18:47:44 +0900

    feat(Baseball): Baseball 객체 생성
    Baseball 주요 기능:
    - 전체 게임 진행 기능
    - 한 판(Inning) 진행 기능
    - 게임 종료 여부 판단

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,5 +1,48 @@
 package baseball;
 
-public class Baseball {
+import camp.nextstep.edu.missionutils.Console;
 
+public class Baseball {
+	private static boolean isKeepInning = true;
+	private static boolean isKeepGaming = true;
+
+	public void startGame() {
+		while (isKeepGaming) {
+			playInning();
+			Broadcast.printRestartMessage();
+			checkGameEnd();
+		}
+	}
+
+	private static void checkGameEnd() {
+		String endCode = Console.readLine();
+		if (endCode.equals("1")) {
+			isKeepInning = true;
+			return;
+		}
+		if (endCode.equals("2")) {
+			isKeepGaming = false;
+			return;
+		}
+		throw new IllegalArgumentException("1이나 2만 입력해주세요");
+	}
+
+	private void playInning() {
+		Broadcast.printStartMessage();
+		Pitcher pitcher = new Pitcher();
+		while (isKeepInning) {
+			Batter batter = new Batter();
+			Referee referee = new Referee(pitcher, batter);
+			String resultMessage = referee.getResultMessage();
+			Broadcast.printResultMessage(resultMessage);
+			checkInningEnd(resultMessage);
+		}
+	}
+
+	private void checkInningEnd(String resultMessage) {
+		if (resultMessage.equals("3스트라이크")) {
+			isKeepInning = false;
+			Broadcast.printEndMessage();
+		}
+	}
 }
