4e5c5788f1e93154e2a3c51fd14a6a320bf163d2
yummygyudon <bang2brew@gmail.com>
2022-11-04 12:19:08 +0900

    TEST : 입력값 유효성 검사 및 예외 처리 테스트
    1. 입력값_Validation_Test()
    isCorrectInput() 메서드에 잘못된 Argument를 입력했을 때, IllegalArgumentException이 발생되는지 확인하는 Test
    
    2. void 입력값_Return_Test()
     유효성 검사를 정상적으로 마친 후, split 작업까지 처리되어 List<String> 타입의 숫자 리스트가 반환되는지 Proccessing 클래스 내부에 위치한 askInputNumber 메서드를 확인하는 Test
     해당 메서드는 readLine메서드를 활요하고 있기 때문에 테스트 시점에만 해당 메서드에 고정 값을 지정 후 테스트를 진행했고 기댓값과 동일하게 반환되어 정상적으로 처리되었습니다.

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,14 +1,51 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static baseball.Application.*;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    private Validations validations;
+    private Processing processing ;
+
+
+    @BeforeEach
+    void getInstanceOfMethodClass(){
+        this.validations = new Validations();
+        this.processing = new Processing();
+    }
+
+    @Test
+    void 입력값_Validation_Test(){
+        // given
+        String inputNum = "4234";
+
+        // then
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> validations.isCorrectInput(inputNum))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void 입력값_Return_Test(){
+        // given
+        String inputNum = "423";
+        List<String> answer = List.of("4","2","3");
+
+        // then
+        // readLine 메서드 특성상 테스트 시점에만 해당 메서드에 고정 값 지정 후 테스트 진행
+        assertThat(answer).isEqualTo(processing.askInputNumber());
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -30,6 +67,6 @@ class ApplicationTest extends NsTest {
 
     @Override
     public void runMain() {
-        Application.main(new String[]{});
+        main(new String[]{});
     }
 }
