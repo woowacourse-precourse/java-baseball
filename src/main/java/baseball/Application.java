86db57a9c9588985c81754f6f996a7e63faa3566
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-04 17:32:22 +0900

    feat: 게임 재시작 코드 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,23 @@
 package baseball;
 
 import baseball.util.BaseBallGame;
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 
 public class Application {
+    private static final int RESTART_CODE = 1;
+    private static final int END_CODE = 2;
+
     public static void main(String[] args) {
-        BaseBallGame baseBallGame = new BaseBallGame(Randoms.pickUniqueNumbersInRange(0, 9, 3));
+        Integer restartOrEnd;
+
+        do {
+            BaseBallGame baseBallGame = new BaseBallGame(Randoms.pickUniqueNumbersInRange(0, 9, 3));
+
+
+            restartOrEnd = Integer.valueOf(Console.readLine());
+        } while (restartOrEnd == RESTART_CODE);
 
     }
 }
