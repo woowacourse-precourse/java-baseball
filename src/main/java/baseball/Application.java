cba28333100a220d53d4c6d468de7452879ee46b
kmw2378 <rlarla677@gmail.com>
2022-11-04 23:48:47 +0900

    feat (Application)
    컴퓨터가 1 ~ 9까지 서로 다른 임의의 수 3개를 선택하는 기능을 추가
    - 요구사항에 나와있는 라이브러리를 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/** 기능 목록
+ * 1. 컴퓨터가 1 ~ 9 까지 서로 다른 임의의 수 3개를 선택
+ *   1-1. 요구사항에 나와있는 라이브러리를 사용하여 구현한다.
+ *
+ */
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
 }
