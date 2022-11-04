195cc5508e548b12d56519d1bf86f79dd85de6b3
jingyu9804 <swc05161@gmail.com>
2022-11-04 19:12:48 +0900

    feat : nothingMessage 추가

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -8,4 +8,5 @@ public class Constant {
     public static final String inputNumberMessage="숫자를 입력해주세요.";
     public static final String strikeMessage="스트라이크";
     public static final String ballMessage="볼";
+    public static final String nothingMessage="낫싱";
 }
