8c39f1a7f752ba95c38471f90b7ff6498c8220a9
egoing <dudqk9696@naver.com>
2022-11-04 10:20:15 +0900

    feat : ball 숫자 반환

--- src/main/java/baseball/controller/OutputController.java
+++ src/main/java/baseball/controller/OutputController.java
@@ -4,6 +4,7 @@ import baseball.domain.ComNumber;
 import baseball.domain.UserNumber;
 
 public class OutputController {
+
     public int getStrike(ComNumber comNumber, UserNumber userNumber) {
         int strike = 0;
         for(int i = 0; i< InputController.NUMBER_SIZE; i++) {
@@ -12,5 +13,15 @@ public class OutputController {
             if(comnum == usernum) {strike++;}
         }
         return strike;
+    }
+
+    public int getBall(ComNumber comNumber, UserNumber userNumber) {
+        int ball = 0;
+        for(int i = 0; i< InputController.NUMBER_SIZE; i++) {
+            Integer comnum = comNumber.getComnumber().get(i);
+            int usernum = userNumber.getUserNumber().get(i);
+            if(comnum != usernum && userNumber.getUserNumber().contains(comnum)) {ball++;}
+        }
+        return ball;
     }
 }
