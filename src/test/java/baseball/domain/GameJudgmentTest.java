5187347801786d88cd7084417f65d804be34342c
alstjr706 <alstjr706@naver.com>
2022-11-05 01:01:42 +0900

    test : (숫자 야구) 컴퓨터와 플레이어 사이에 서로 포함된 숫자 개수 구하기 단위 테스트 작성

--- src/test/java/baseball/domain/GameJudgmentTest.java
+++
@@ -1,5 +0,0 @@
-package baseball.domain;
-
-class GameJudgmentTest {
-
-}
---
+++ src/test/java/baseball/domain/NumberComparisonTest.java
@@ -0,0 +1,46 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberComparisonTest {
+
+    @Test
+    void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_모두_맞을_경우() {
+        int result = 3;
+        NumberComparison numberComparison = new NumberComparison();
+        int compare = numberComparison.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
+        assertThat(compare).isEqualTo(result);
+    }
+
+    @Test
+    void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_2개_맞을_경우() {
+        int result = 2;
+        NumberComparison numberComparison = new NumberComparison();
+        int compare = numberComparison.compare(Arrays.asList(1, 2, 5), Arrays.asList(1, 2, 3));
+        assertThat(compare).isEqualTo(result);
+    }
+
+    @Test
+    void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_1개_맞을_경우() {
+        int result = 1;
+        NumberComparison numberComparison = new NumberComparison();
+        int compare = numberComparison.compare(Arrays.asList(1, 9, 5), Arrays.asList(1, 2, 3));
+        assertThat(compare).isEqualTo(result);
+    }
+
+    @Test
+    void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_없을_경우() {
+        int result = 0;
+        NumberComparison numberComparison = new NumberComparison();
+        int compare = numberComparison.compare(Arrays.asList(4, 9, 5), Arrays.asList(1, 2, 3));
+        assertThat(compare).isEqualTo(result);
+    }
+
+
+
+}
