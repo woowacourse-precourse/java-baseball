c2d495e5dcb04b614177119bc92c1bfe44f1a8f6
TaeHwan <kimta2hwan@gmail.com>
2022-11-04 18:13:03 +0900

    feat(outputview): 오답일 때 볼만을 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,6 +5,7 @@ public class OutputView {
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String ANSWER_INPUT_GUIDE_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String ONLY_BALL_MESSAGE = "%d볼\n";
     private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART_OR_STOP_GUIDE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
@@ -18,6 +19,10 @@ public class OutputView {
 
     public static void printNothingMessage() {
         System.out.println(NOTHING_MESSAGE);
+    }
+
+    public static void printOnlyBallMessage() {
+        System.out.printf(ONLY_BALL_MESSAGE);
     }
 
     public static void printGameEndMessage() {
