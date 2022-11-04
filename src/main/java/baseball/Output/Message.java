ad0ddd35642f73177048172ae090025443217304
ma-an-jong <alswhd1113@gmail.com>
2022-11-04 22:30:20 +0900

    feat(HintGenerator,Message) : 힌트 생성 기능 추가
    List로 전달받은 랜덤 정수와 사용자 입력을 비교하여 strike와 ball의 개수를 세어
    사용자에게 힌트를 제공하는 기능 추가
    힌트를 위한 메세지는 Message 클래스와 협력하여 구현
    
    BREAKING CHANGE: 힌트 생성 기능 추가
    
    Before:
    
        HintGenerator
        {
    
        }
    
        Message
        {
    
        }
    
    After:
    
        HintGenerator
        {
            generateHint
        }
    
        Message
        {
            getNothingMessage
            getBallMessage
            getStrikeMessage
            getBallAndStrikeMessage
        }
    
        힌트 생성 기능을 위한 HintGenerator 추가, 메세지는 Message 클래스가 생성하도록 구현

--- src/main/java/baseball/Output/Message.java
+++ src/main/java/baseball/Output/Message.java
@@ -7,4 +7,29 @@ public class Message {
     public static final String INPUT_CORRECT_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료.\n" +
             "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
     public static final String NUMBER_INPUT_REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
+
+    private static final String NOTHING_MESSAGE = "낫싱\n";
+    private static final String BALL_MESSAGE_FORMAT = "%d ball\n";
+    private static final String STRIKE_MESSAGE_FORMAT = "%d strike\n";
+    private static final String BALL_AND_STRIKE_MESSAGE_FORMAT = "%d ball %d strike\n";
+
+    public static String getNothingMessage()
+    {
+        return NOTHING_MESSAGE;
+    }
+    public static String getBallMessage(int ballCount)
+    {
+        return String.format(Message.BALL_MESSAGE_FORMAT,ballCount);
+    }
+
+    public static String getStrikeMessage(int strikeCount)
+    {
+        return String.format(Message.STRIKE_MESSAGE_FORMAT,strikeCount);
+    }
+
+    public static String getBallAndStrikeMessage(int ballCount,int strikeCount)
+    {
+        return String.format(Message.BALL_AND_STRIKE_MESSAGE_FORMAT,ballCount,strikeCount);
+    }
+
 }
