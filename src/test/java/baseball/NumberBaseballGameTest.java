19c0a5e35f37becd69375fe4e2e72c0762c18877
rt3310 <btac3310@gmail.com>
2022-11-05 02:47:02 +0900

    test: 투수가 랜덤한 서로 다른 숫자의 공을 3개 던지는 기능 테스트 코드 구현

--- src/test/java/baseball/NumberBaseballGameTest.java
+++ src/test/java/baseball/NumberBaseballGameTest.java
@@ -7,6 +7,10 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -64,5 +68,62 @@ public class NumberBaseballGameTest {
         int order = field.getInt(ball);
 
         assertThat(order).isEqualTo(result);
+    }
+
+    @Test
+    void 랜덤한_1과_9사이의_숫자를_하나_생성한다() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Pitcher pitcher = new Pitcher();
+
+        Method method = pitcher.getClass().getDeclaredMethod("getNotDuplicatedRandomNumber");
+        method.setAccessible(true);
+        int number = (int) method.invoke(pitcher);
+
+        assertThat(number).isGreaterThan(0).isLessThan(10);
+    }
+
+    @Test
+    void 투수가_랜덤한_1과_9사이의_숫자와_순서를_가진_공을_하나_생성한다() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Pitcher pitcher = new Pitcher();
+
+        Method method = pitcher.getClass().getDeclaredMethod("throwRandomBall", int.class);
+        method.setAccessible(true);
+        Object ball = method.invoke(pitcher, 3);
+
+        assertThat(ball).isInstanceOf(Ball.class);
+    }
+
+    @Test
+    void 투수가_생성한_공에_1과_9사이의_숫자가_있는지_확인() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Pitcher pitcher = new Pitcher();
+
+        Method method = pitcher.getClass().getDeclaredMethod("throwRandomBall", int.class);
+        method.setAccessible(true);
+        Ball ball = (Ball) method.invoke(pitcher, 3);
+
+        assertThat(ball.getNumber()).isInstanceOf(Integer.class).isGreaterThan(0).isLessThan(10);
+    }
+
+    @Test
+    void 서로_다른_1과_9사이의_랜덤_숫자를_가진_공_3개를_생성한다() {
+        Pitcher pitcher = new Pitcher();
+
+        List<Ball> balls = pitcher.throwRandomBalls(3);
+
+        assertThat(balls.size()).isEqualTo(3);
+    }
+
+    @Test
+    void 투수가_생성한_공_3개의_숫자가_모두_다른지_확인() {
+        Pitcher pitcher = new Pitcher();
+        List<Integer> checkList = new ArrayList<>(List.of(1, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+
+        List<Ball> balls = pitcher.throwRandomBalls(3);
+
+        balls.stream()
+                .peek(ball -> System.out.println(ball.getNumber()))
+                .forEach(ball -> {
+                    assertThat(checkList.get(ball.getNumber())).isEqualTo(0);
+                    checkList.set(ball.getNumber(), 1);
+                });
     }
 }
