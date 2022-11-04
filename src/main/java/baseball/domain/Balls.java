1066e4b4ebd5dd4ec66543107d14d12c68e8316b
EcoFriendlyAppleSu <yeunsu3737@gmail.com>
2022-11-04 14:25:06 +0900

    feat : balls test

--- src/main/java/baseball/domain/Balls.java
+++ src/main/java/baseball/domain/Balls.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,32 +10,40 @@ public class Balls {
     private List<Ball> balls;
 
     public Balls(List<Integer> numbers) {
-        Set<Integer> numberSet = setBallNumber();
+        balls = new ArrayList<>();
+        Set<Integer> numberSet = setBallNumber(numbers);
         if (isOkaySize(numbers)) {
             throw new IllegalArgumentException();
         }
 
-        if (isSameSize(numbers, numberSet)) {
+        if (isNotSameSize(numbers, numberSet)) {
             throw new IllegalArgumentException();
         }
 
+        for (Integer number : numbers) {
+            balls.add(Ball.of(number, numbers.indexOf(number)));
+        }
     }
 
     public static Balls from(List<Integer> numbers) {
         return new Balls(numbers);
     }
 
+    public List<Ball> currentBalls() {
+        return balls;
+    }
+
     private boolean isOkaySize(List<Integer> numbers) {
         return numbers.size() != 3;
     }
 
-    private boolean isSameSize(List<Integer> numbers, Set<Integer> numberSet) {
+    private boolean isNotSameSize(List<Integer> numbers, Set<Integer> numberSet) {
         return numbers.size() != numberSet.size();
     }
 
-    private Set<Integer> setBallNumber() {
+    private Set<Integer> setBallNumber(List<Integer> numbers) {
         Set<Integer> testNumbers = new HashSet<>();
-        for (Integer number : testNumbers) {
+        for (Integer number : numbers) {
             testNumbers.add(number);
         }
         return testNumbers;
--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -1,24 +1,36 @@
 package baseball.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class BallsTest {
 
     @Test
     public void ballSizeExceptionTest() throws Exception {
-        List<Integer> integers = List.of(1234);
+        List<Integer> integers = List.of(1, 2, 3, 4);
         assertThatThrownBy(() -> Balls.from(integers))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     public void ballSameNumberExceptionTest() throws Exception {
-        List<Integer> integers = List.of(778);
+        List<Integer> integers = List.of(7, 7, 8);
         assertThatThrownBy(() -> Balls.from(integers))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void ballsSettingTest() throws Exception {
+        List<Integer> integers = List.of(1, 2, 3);
+        Ball ball01 = Ball.of(1, 0);
+        Ball ball02 = Ball.of(2, 1);
+        Ball ball03 = Ball.of(3, 2);
+        assertThat(Balls.from(integers).currentBalls()).contains(ball01, ball02, ball03);
     }
 
 }
