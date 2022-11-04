917d2b955b17be6ea8ab9f4b8250c14f1491dd10
bnbnac <bnbnac@naver.com>
2022-11-04 20:55:11 +0900

    feat: 게임 진행과정 반복 로직 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,17 @@
 package baseball;
 
 public class Application {
+    public static Computer computer;
+    public static Player player;
+
     public static void main(String[] args) {
         Game game = new Game();
+
         game.start();
+
+        while (game.again) {
+            game.play();
+        }
+
     }
 }
