6729d2e2b7d9e7ad3a79c5aa592726d77cee0f98
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-05 01:29:57 +0900

    Test: 메서드 기능별로 테스트캐이스 묶기

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -17,6 +18,7 @@ class ApplicationTest extends NsTest {
         String numList = "123456789";
         assertThat(Application.removeRepetitionNumber(numList)).isEqualTo(123);
     }
+
 
     @Test
     void listNumber2() {
@@ -36,104 +38,117 @@ class ApplicationTest extends NsTest {
         assertThat(Application.removeRepetitionNumber(numList)).isEqualTo(486);
     }
 
-    @Test
-    void userInput1() {
-        int num = 12;
-        assertThatThrownBy(() -> Application.exceptionHandler(num))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("n은 세자리 숫자입니다.");
-    }
-
-    @Test
-    void userInput2() {
-        int num = 1234;
-        assertThatThrownBy(() -> Application.exceptionHandler(num))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("n은 세자리 숫자입니다.");
-    }
-
-    @Test
-    void userInput3() {
-        int num = 1;
-        assertThatThrownBy(() -> Application.exceptionHandler(num))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("n은 세자리 숫자입니다.");
-    }
-
-    @Test
-    void userInput4() {
-        int num = 123;
-        assertThat(Application.exceptionHandler(num)).isEqualTo(123);
-    }
-
-    @Test
-    void userInput5() {
-        int num = (int) (Math.random()*1000)+1;
-        System.out.println("num = " + num);
-
-        if (!(num >= 100 && num < 1000)) {
+    @Nested
+    class userInputTest {
+        @Test
+        void userInput1() {
+            int num = 12;
             assertThatThrownBy(() -> Application.exceptionHandler(num))
                     .isInstanceOf(IllegalArgumentException.class)
                     .hasMessageContaining("n은 세자리 숫자입니다.");
         }
-    }
 
-
-    @Test
-    void restartGame1() {
-        int num = 2;
-        assertThat(Application.breakExceptionHandler(num)).isEqualTo(num);
-    }
-
-    @Test
-    void restartGame12() {
-        int num = 3;
-        if (!(num == 1 || num ==2)) {
-            assertThatThrownBy(() -> Application.breakExceptionHandler(num))
+        @Test
+        void userInput2() {
+            int num = 1234;
+            assertThatThrownBy(() -> Application.exceptionHandler(num))
                     .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("1 또는 2를 입력해야 합니다.");
+                    .hasMessageContaining("n은 세자리 숫자입니다.");
+        }
+
+        @Test
+        void userInput3() {
+            int num = 1;
+            assertThatThrownBy(() -> Application.exceptionHandler(num))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("n은 세자리 숫자입니다.");
+        }
+
+        @Test
+        void userInput4() {
+            int num = 123;
+            assertThat(Application.exceptionHandler(num)).isEqualTo(123);
+        }
+
+        @Test
+        void userInput5() {
+            int num = (int) (Math.random() * 1000) + 1;
+            System.out.println("num = " + num);
+
+            if (!(num >= 100 && num < 1000)) {
+                assertThatThrownBy(() -> Application.exceptionHandler(num))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("n은 세자리 숫자입니다.");
+            }
         }
     }
 
-    @Test
-    void checkStrike1(){
+
+    @Nested
+    class restartTest {
+        @Test
+        void restartGame1() {
+            int num = 2;
+            assertThat(Application.breakExceptionHandler(num)).isEqualTo(num);
+        }
+
+        @Test
+        void restartGame12() {
+            int num = 3;
+            if (!(num == 1 || num == 2)) {
+                assertThatThrownBy(() -> Application.breakExceptionHandler(num))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("1 또는 2를 입력해야 합니다.");
+            }
+        }
+    }
+
+    @Nested
+    class checkStrikeTest{
+        @Test
+        void checkStrike1 () {
         assertThat(Application.checkStrikeCount(123, 143)).isEqualTo(2);
-    }
+        }
 
-    @Test
-    void checkStrike2(){
+        @Test
+        void checkStrike2 () {
         assertThat(Application.checkStrikeCount(123, 456)).isEqualTo(0);
-    }
+        }
 
-    @Test
-    void checkStrike3(){
+        @Test
+        void checkStrike3 () {
         assertThat(Application.checkStrikeCount(123, 173)).isEqualTo(2);
-    }
+        }
 
-    @Test
-    void checkStrike4(){
+        @Test
+        void checkStrike4 () {
         assertThat(Application.checkStrikeCount(123, 321)).isEqualTo(1);
+        }
     }
 
-    @Test
-    void checkBall1(){
-        assertThat(Application.checkBallCount(123, 143)).isEqualTo(0);
+    @Nested
+    class checkBallTest {
+        @Test
+        void checkBall1() {
+            assertThat(Application.checkBallCount(123, 143)).isEqualTo(0);
+        }
+
+        @Test
+        void checkBall2() {
+            assertThat(Application.checkBallCount(987, 765)).isEqualTo(1);
+        }
+
+        @Test
+        void checkBall3() {
+            assertThat(Application.checkBallCount(915, 125)).isEqualTo(1);
+        }
+
+        @Test
+        void checkBall4() {
+            assertThat(Application.checkBallCount(915, 159)).isEqualTo(3);
+        }
     }
 
-    @Test
-    void checkBall2(){
-        assertThat(Application.checkBallCount(987, 765)).isEqualTo(1);
-    }
-
-    @Test
-    void checkBall3(){
-        assertThat(Application.checkBallCount(915, 125)).isEqualTo(1);
-    }
-
-    @Test
-    void checkBall4(){
-        assertThat(Application.checkBallCount(915, 159)).isEqualTo(3);
-    }
 
 
     @Test
