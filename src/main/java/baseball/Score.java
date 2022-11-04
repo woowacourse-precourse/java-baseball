6a104fe661e32088fed16171315fbd7dcac839f6
woosung1223 <dntjd991223@naver.com>
2022-11-04 17:27:05 +0900

    feat(Score): Score 클래스 및 생성자 추가

---
+++ src/main/java/baseball/Score.java
@@ -0,0 +1,13 @@
+package baseball;
+
+
+
+public class Score {
+    private final int ball;
+    private final int strike;
+
+    Score(int ball, int strike) {
+        this.ball = ball;
+        this.strike = strike;
+    }
+}
