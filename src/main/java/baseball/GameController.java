61df60964fcd26b70114f1066101dda4e331907b
ma-an-jong <alswhd1113@gmail.com>
2022-11-04 17:13:15 +0900

    feat(GameController) : 숫자 야구애 쓸 정수 생성 기능 추가
    Randoms를 3번 사용하여 3자리 정수를 생성하는 기능 추가
    
    BREAKING CHANGE:
    
    Before:
    
        GameController
        {
            isValidUserInput(String userInput)
        }
    
    After:
    
        GameController
        {
            generateRandomNumber()
            isValidUserInput(String userInput)
        }
    
        생성된 3자리 정수는 strike,ball 체크를 위해 List로 보관

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,15 +1,33 @@
 package baseball;
 
 import baseball.Input.InputManageable;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class GameController {
     private static final Integer INPUT_SIZE = 3;
+    private static final Integer START_INCLUSIVE = 1;
+    private static final Integer END_INCLUSIVE = 9;
     private static final String VALID_INPUT_PATTERN = "^[1-9]*$";
+    private List<Integer> randomNumber;
     public GameController()
     {
+        generateRandomNumber();
+    }
 
+    public void generateRandomNumber()
+    {
+        randomNumber = List.of(
+                Randoms.pickNumberInRange(START_INCLUSIVE,END_INCLUSIVE),
+                Randoms.pickNumberInRange(START_INCLUSIVE,END_INCLUSIVE),
+                Randoms.pickNumberInRange(START_INCLUSIVE,END_INCLUSIVE));
+    }
+
+    public Integer getRandomNumber()
+    {
+        return randomNumber.get(0)*100 + randomNumber.get(1)*10 + randomNumber.get(2);
     }
 
     public boolean isValidUserInput(String userInput)
