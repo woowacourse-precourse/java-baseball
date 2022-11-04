6eb7a9f8ec3ebd364dc75a946bb4136aefaa9bf6
yumyeonghan <mgtmh991013@gmail.com>
2022-11-04 18:51:58 +0900

    feat: "컴퓨터가 임의의 수 3개 선택 기능 구현"
    random3NumberOutput(), 컴퓨터가 1에서 9까지 서로 다른 임의의 수 3개를 선택하는 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,9 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        Computer computer = new Computer();
+        computer.random3NumberOutput();
+
     }
 }
