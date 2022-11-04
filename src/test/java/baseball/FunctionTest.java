ec4bec1da50b82eb2a8efa53643d52c6241df551
minsu <bill7666@naver.com>
2022-11-04 16:14:59 +0900

    랜덤숫자뽑기기능 테스트구현

--- src/test/java/baseball/FunctionTest.java
+++ src/test/java/baseball/FunctionTest.java
@@ -1,26 +1,26 @@
 package baseball;
 
-
 import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Application {
-    public static List<Integer> PickRandomNumber(){
+public class FunctionTest {
+@Nested
+class Application{
+    @Test
+    void CreateRandomNumber() {
         List<Integer> Number =
                 new ArrayList<>();
         while (Number.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!Number.contains(randomNumber)) {
                 Number.add(randomNumber);
+                }
             }
         }
-        return Number;
-    }
-    public static void main(String[] args) {
-            List<Integer> RandomNumber = PickRandomNumber();
-
-
     }
 }
