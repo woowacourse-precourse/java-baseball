79c1f3e2bf5132bfb1263092d62bf8557f4a5822
kim0914 <kosa0914@naver.com>
2022-11-04 23:39:06 +0900

    feat(user): 사용자가 입력한 숫자 검증 로직 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -2,17 +2,28 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import constant.Message;
+import validator.NumberValidator;
 
 public class User {
     private static String inputNumber;
 
-    public void setNumber(){
+    public void setNumber() {
         inputNumber = getNumberFromInput();
     }
 
-    public String getNumberFromInput(){
+    public String getInputNumber() {
+        return inputNumber;
+    }
+
+    public String getNumberFromInput() {
         Message.showNumberRequestMessage();
         String inputNumber = Console.readLine();
+
+        validateNumber(inputNumber);
         return inputNumber;
+    }
+
+    private void validateNumber(String inputNumber) {
+        NumberValidator.validate(inputNumber);
     }
 }
