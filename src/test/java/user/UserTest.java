bf9e1275370bb9482634d2ae1b3577888d56d402
jeongwle <dldl4568@naver.com>
2022-11-04 18:01:03 +0900

    style : static import문 수정
    static import에서 와일드 카드 삭제

--- src/test/java/user/UserTest.java
+++ src/test/java/user/UserTest.java
@@ -1,6 +1,6 @@
 package user;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import baseball.User;
 import java.io.ByteArrayInputStream;
