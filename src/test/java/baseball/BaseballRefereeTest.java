34ccd8115efd7fdbef389955f95f738748cbd74f
youngkyunIm <yee0230@naver.com>
2022-11-04 23:51:08 +0900

    test : move BaseballRefereeTest class
    기존 테스트 코드를 BaseballRefereeTest로 이동

---
+++ src/test/java/baseball/BaseballRefereeTest.java
@@ -0,0 +1,90 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+class BaseballRefereeTest extends NsTest {
+    @Test
+    @DisplayName("3 스트라이크")
+    void ThreeStrike() {
+        //given
+        //when
+        List<Integer> computer = new ArrayList<>();
+        computer.add(1);
+        computer.add(2);
+        computer.add(3);
+        List<Integer> input = new ArrayList<>();
+        input.add(1);
+        input.add(2);
+        input.add(3);
+        BaseballReferee baseballReferee = new BaseballReferee(computer, input);
+        //then
+        assertThat(baseballReferee.isThreeStrike()).isEqualTo(true);
+    }
+
+
+    @Test
+    @DisplayName("낫싱_테스트")
+    void 낫싱_테스트() {
+        //given
+        //when
+        List<Integer> computer = new ArrayList<>();
+        computer.add(1);
+        computer.add(2);
+        computer.add(3);
+        List<Integer> input = new ArrayList<>();
+        input.add(4);
+        input.add(5);
+        input.add(6);
+        BaseballReferee baseballReferee = new BaseballReferee(computer, input);
+        assertThat(output()).isEqualTo("낫싱");
+        //then
+    }
+
+    @Test
+    @DisplayName("2 볼 테스트")
+    void countBallTest() {
+        //given
+        //when
+        List<Integer> computer = new ArrayList<>();
+        computer.add(1);
+        computer.add(2);
+        computer.add(3);
+        List<Integer> input = new ArrayList<>();
+        input.add(2);
+        input.add(1);
+        input.add(6);
+        BaseballReferee baseballReferee = new BaseballReferee(computer, input);
+        //then
+        assertThat(output()).isEqualTo("2볼");
+    }
+
+    @Test
+    @DisplayName("2 스트라이크 테스트")
+    void countStrikeTest() {
+        //given
+        //when
+        List<Integer> computer = new ArrayList<>();
+        computer.add(1);
+        computer.add(2);
+        computer.add(3);
+        List<Integer> input = new ArrayList<>();
+        input.add(1);
+        input.add(2);
+        input.add(6);
+        BaseballReferee baseballReferee = new BaseballReferee(computer, input);
+        //then
+        assertThat(output()).isEqualTo("2스트라이크");
+    }
+
+    @Override
+    protected void runMain() {
+    }
+}
