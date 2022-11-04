a7e98ff72e324a4f4cfbd82bbc7a8212eae4d056
kim0914 <kosa0914@naver.com>
2022-11-04 21:50:12 +0900

    test(NumberValidator): 입력 숫자 범위가 조건에 맞지 않는 경우 테스트 추가

--- src/test/java/validator/NumberValidatorTest.java
+++ src/test/java/validator/NumberValidatorTest.java
@@ -1,15 +1,14 @@
 package validator;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 class NumberValidatorTest {
 
     @Test
-    void 숫자_크기가_다른_경우_예외_발생(){
+    void 숫자_크기가_다른_경우_예외_발생() {
         String checkNumber = "1234";
 
         assertThatThrownBy(() -> NumberValidator.checkSize(checkNumber))
@@ -18,12 +17,48 @@ class NumberValidatorTest {
     }
 
     @Test
-    void 중복된_숫자가_있는_경우_예외_발생(){
+    void 중복된_숫자가_있는_경우_예외_발생() {
         String checkNumber = "112";
 
         assertThatThrownBy(() -> NumberValidator.checkUnique(checkNumber))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("각 자리수는 중복을 허락하지 않습니다.");
+    }
+
+    @Test
+    void 숫자_범위가_조건에_맞지않는_경우_예외_발생1() {
+        String checkNumber = "102";
+
+        assertThatThrownBy(() -> NumberValidator.checkNumberCondition(checkNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1~9 까지의 수 조건에 충족하지 않습니다.");
+    }
+
+    @Test
+    void 숫자_범위가_조건에_맞지않는_경우_예외_발생2() {
+        String checkNumber = "1 0";
+
+        assertThatThrownBy(() -> NumberValidator.checkNumberCondition(checkNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1~9 까지의 수 조건에 충족하지 않습니다.");
+    }
+
+    @Test
+    void 숫자_범위가_조건에_맞지않는_경우_예외_발생3() {
+        String checkNumber = "abc";
+
+        assertThatThrownBy(() -> NumberValidator.checkNumberCondition(checkNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1~9 까지의 수 조건에 충족하지 않습니다.");
+    }
+
+    @Test
+    void 숫자_범위가_조건에_맞지않는_경우_예외_발생4() {
+        String checkNumber = "ac#";
+
+        assertThatThrownBy(() -> NumberValidator.checkNumberCondition(checkNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1~9 까지의 수 조건에 충족하지 않습니다.");
     }
 
 }
