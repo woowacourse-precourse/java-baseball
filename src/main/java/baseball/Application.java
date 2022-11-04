c585f4942f32e34c60b85c3f64d88bb8a998218d
minju412 <ln8847@ajou.ac.kr>
2022-11-04 15:03:48 +0900

    feat: add inputPlayerNumber method
    - add inputPlayerNumber method
      to receive player number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,13 +2,14 @@ package baseball;
 
 import baseball.game.Computer;
 import baseball.game.Player;
+import java.util.List;
 
 public class Application {
+
     public static void main(String[] args) {
         Computer computer = new Computer();
         Player player = new Player();
-        do {
-            computer.startGame();
-        } while ();
+        computer.startGame();
+        List<Integer> integers = player.inputPlayerNumber();
     }
 }
--- src/main/java/baseball/game/Player.java
+++ src/main/java/baseball/game/Player.java
@@ -1,14 +1,22 @@
 package baseball.game;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
 
-    private String playerNumber;
-
-    public String getPlayerNumber() {
-        return playerNumber;
+    public List<Integer> inputPlayerNumber() {
+        String playerNumber = Console.readLine();
+        return stringToList(playerNumber);
     }
 
-    public void setPlayerNumber(String playerNumber) {
-        this.playerNumber = playerNumber;
+    private List<Integer> stringToList(String playerInput) {
+        List<Integer> playerNumber = new ArrayList<>();
+        for (int i = 0; i < playerInput.length(); i++) {
+            char current = playerInput.charAt(i);
+            playerNumber.add(Character.getNumericValue(current));
+        }
+        return playerNumber;
     }
 }
