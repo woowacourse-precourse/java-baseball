b3e50968507488ab0df1e67de47c6e67dd6a9f61
jihyun <jihyun@jihyunui-MacBookAir.local>
2022-11-04 23:20:49 +0900

    feat: 게임 시작, 재시작/종료 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,8 +4,27 @@ import java.util.List;
 
 public class BaseballGame {
 
+    private static final String RESTART_GAME = "1";
+    private static final String END_GAME = "2";
+    private static final String ENDANSWER_ERR_MESSAGE = "1 혹은 2가 아닙니다.";
+
     Number number = new Number();
     GameResult gameResult = new GameResult();
+    InputAndOutput inputAndOutput = new InputAndOutput();
+
+    public void startGame() {
+        InputAndOutput.printGameStartPhrase();
+        List<Integer> computerNumbers = number.generateRandomNumber();
+        String resultPhrase;
+
+        do {
+            resultPhrase = playGame(computerNumbers);
+            gameResult.printGameResult(resultPhrase);
+        } while (!gameResult.isUserWin(resultPhrase));
+        String endAnswer = InputAndOutput.getRestartOrEnd();
+        inputAndOutput.isValidEndAnswer(endAnswer);
+        replayOrEndGame(endAnswer);
+    }
 
     public String playGame(List<Integer> computerNumbers) {
         String answer = InputAndOutput.getUserAnswer();
@@ -17,5 +36,18 @@ public class BaseballGame {
         }
 
         return gameResult.getGameResult(computerNumbers, userNumbers);
+    }
+
+    public void replayOrEndGame(String endAnswer) {
+        if (endAnswer.equals(RESTART_GAME)) {
+            startGame();
+            return;
+        }
+
+        if (endAnswer.equals(END_GAME)) {
+            return;
+        }
+
+        throw new IllegalArgumentException(ENDANSWER_ERR_MESSAGE);
     }
 }
