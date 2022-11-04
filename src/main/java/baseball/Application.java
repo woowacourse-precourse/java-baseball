b8f2928ceea78c296b32e374b03ae6582706b48b
gumgu <bienlee@naver.com>
2022-11-04 23:18:15 +0900

    Feat: 3개의 랜덤한 숫자 생성
    - 1~9까지 서로 다른 3개의 랜덤 숫자 생성 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        String randomNum = makeRandomNum();
+        System.out.println("숫자를 입력해 주세요. :");
+    }
+
+    public static String makeRandomNum() {
+        String randomNum = "";
+        while(randomNum.length()<3) {
+            int ranNum = Randoms.pickNumberInRange(1, 9);
+            if(!randomNum.contains(ranNum+"")) {
+                randomNum = randomNum + ranNum;
+            }
+        }
+        return randomNum;
     }
 }
