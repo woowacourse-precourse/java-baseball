94ee97a7d8ad62fa1b7c3404a90497cbed939207
JunH <tjdtls690@gmail.com>
2022-11-04 19:22:41 +0900

    :white_check_mark: test(Dto : SelectedNumberForRestartDTOTest) : 재시작 선택 여부 번호 입력 시, 특수 문자 입력 시 예외 처리 기능 테스트 완료

--- src/test/java/baseball/dto/SelectedNumberForRestartDTOTest.java
+++ src/test/java/baseball/dto/SelectedNumberForRestartDTOTest.java
@@ -1,6 +1,7 @@
 package baseball.dto;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -8,6 +9,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class SelectedNumberForRestartDTOTest {
+    private static final String NUMBER_FORM_EXCEPTION_MESSAGE = "1 또는 2만 입력할 수 있습니다.";
+    
     @DisplayName("예외 처리 : null 또는 \"\" 입력 시")
     @ParameterizedTest(name = "{displayName} => {0}")
     @NullAndEmptySource
@@ -23,6 +26,14 @@ class SelectedNumberForRestartDTOTest {
     void alphabet_exception(String input) {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new SelectedNumberForRestartDTO(input))
-                .withMessage("1 또는 2만 입력할 수 있습니다.");
+                .withMessage(NUMBER_FORM_EXCEPTION_MESSAGE);
+    }
+    
+    @Test
+    @DisplayName("예외 처리 : 특수 문자 입력 시")
+    void special_characters_exception() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new SelectedNumberForRestartDTO("$"))
+                .withMessage(NUMBER_FORM_EXCEPTION_MESSAGE);
     }
 }
