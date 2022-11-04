b1bae7bace03a477946286634ea18efee63016b6
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-11-04 21:41:10 +0900

    feat(GameServie): 게임의 최종 점수를 return하고, 스트라이크가 세 개라면 다시 시작할 것인지 return하는 함수 생성

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,16 +1,18 @@
 package baseball.service;
 
 import baseball.domain.User.*;
+import baseball.view.GameOutput;
+import baseball.view.UserInput;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GameService {
-    //판단하는 함수
+    //판단해서 점수를 산출하는 함수
     int Strikescore;
     int Ballscore;
 
     public void initScore() {
-
         Strikescore = 0;
         Ballscore = 0;
     }
@@ -26,12 +28,27 @@ public class GameService {
 
     public void getBallCount(ArrayList<Integer> User, ArrayList<Integer> Computer) {
         //자리는 안 맞지만 숫자는 맞으면 Ball
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (i != j && User.get(i) == Computer.get(j)) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if ((i != j) && (User.get(i) == Computer.get(j))) {
                     Ballscore += 1;
                 }
             }
         }
+    }
+
+    public void finalScore(){
+        this.Strikescore = Strikescore;
+        this.Ballscore = Ballscore;
+        GameOutput.scoreMessage(Ballscore,Strikescore);
+        if (Strikescore==3) isRestart();
+    }
+
+    public boolean isRestart(){
+        this.Strikescore=Strikescore;
+        UserInput.getRetryUserNumber();
+        Scanner sc = new Scanner(System.in);
+        Integer replay_input = sc.nextInt();
+        return replay_input==1;
     }
 }
