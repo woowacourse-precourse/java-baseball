d72e309a744369a7e964dbaf39947fb62adea9bc
JUHA <khabh@naver.com>
2022-11-04 10:30:41 +0900

    test(InputChoiceTest): 사용자의 종료/재시작 입력에 대한 예외 처리 테스트

--- src/test/java/baseball/InputTest.java
+++ src/test/java/baseball/InputTest.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.api.Test;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class InputTest extends NsTest {
+class InputTest {
     @Nested
-    class InputGuessTest {
+    class InputGuessTest extends NsTest{
         @Test
         void case1() {
             String playerInput = "abc";
@@ -46,11 +46,58 @@ class InputTest extends NsTest {
                             .isInstanceOf(IllegalArgumentException.class)
             );
         }
+
+        @Override
+        public void runMain() {
+            Input input = new Input();
+            input.getPlayerGuess();
+        }
     }
 
-    @Override
-    public void runMain() {
-        Input input = new Input();
-        input.getPlayerGuess();
+    @Nested
+    class InputChoiceTest extends NsTest{
+        @Test
+        void case1() {
+            String playerInput = "3";
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(playerInput))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        void case2() {
+            String playerInput = "12";
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(playerInput))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        void case3() {
+            String playerInput = "12";
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(playerInput))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Test
+        void case4() {
+            String playerInput = "a";
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException(playerInput))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Override
+        public void runMain() {
+            Input input = new Input();
+            input.readPlayerChoice();
+        }
     }
+
+
 }
