c02662f8dc9bd356dab63766d66a26c11e630bea
jjdone <ehdlfaos@gmail.com>
2022-11-04 16:06:36 +0900

    feat: Computer 클래스 상세 구현1
    setNumbers, getNumbers 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,6 +1,32 @@
 package baseball;
 
+import java.util.*;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Computer {
 
+    private List<Integer> numbers;
+
     public Computer() {}
+
+    public void setNumbers() {
+        Set<Integer> numbersSet = new HashSet<>();
+        while(numbersSet.size() < 3) {
+            numbersSet.add(Randoms.pickNumberInRange(1,9));
+        }
+        setToArrayList(numbersSet);
+        numberShuffle(this.numbers);
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
+    public void numberShuffle(List<Integer> lst) {
+        this.numbers = Randoms.shuffle(lst);
+    }
+
+    public void setToArrayList(Set<Integer> set) {
+        this.numbers = new ArrayList<>(set);
+    }
 }
--- src/main/java/baseball/GameControl.java
+++ src/main/java/baseball/GameControl.java
@@ -1,11 +1,19 @@
 package baseball;
 
+import java.util.*;
+
 public class GameControl {
+
+    private List<Integer> computer;
+    private ArrayList<Integer> player;
 
     public GameControl() {}
 
     public void startGame() {
         Computer computerNumber = new Computer();
         Player playerNumber = new Player();
+
+        computerNumber.setNumbers();
+        computer = computerNumber.getNumbers();
     }
 }
