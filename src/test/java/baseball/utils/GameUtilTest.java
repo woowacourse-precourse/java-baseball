1828bda16d7f505bab227bec60ae45361b5f2191
Laeng <Laeng@users.noreply.github.com>
2022-11-04 12:05:43 +0900

    test(GameUtil): isValidNumber 메서드에 대한 테스트 추가

--- src/test/java/baseball/utils/GameUtilTest.java
+++ src/test/java/baseball/utils/GameUtilTest.java
@@ -1,6 +1,8 @@
 package baseball.utils;
 
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
@@ -10,8 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 public class GameUtilTest {
     @Test
-    @DisplayName("숫자 램덤 생성")
-    void generate_number() {
+    void 숫자_랜덤_생성() {
         int number = GameUtil.pickNumber(3);
         List<Integer> splitOneDigits = GameUtil.splitNumberToOneDigitNumbers(number);
 
@@ -21,4 +22,22 @@ public class GameUtilTest {
                 .doesNotHaveDuplicates()
                 .hasSize(3);
     }
+
+    @DisplayName("숫자 체크")
+    @Nested
+    class is_valid_Number {
+        @ParameterizedTest(name = "올바르지 않은 숫자 => {0}")
+        @ValueSource(ints = {2, 21, 210, 212, 220, 221, 222})
+        void value_of_false(int number) {
+            assertThat(GameUtil.isValidNumber(number)).isFalse();
+        }
+
+        @ParameterizedTest(name = "올바른 숫자 => {0}")
+        @ValueSource(ints = {123})
+        void value_of_true(int number) {
+            assertThat(GameUtil.isValidNumber(number)).isTrue();
+        }
+
+    }
+
 }
