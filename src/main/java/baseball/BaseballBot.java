68ab9702d6ca80051f3245074b663bc5a4a6985c
hongjk7777 <51441261+hongjk7777@users.noreply.github.com>
2022-11-04 21:35:06 +0900

    feat: 사용자 입력에 따른 힌트(스트라이크, 볼, 낫싱)를 주는 기능 구현
    [구현 방법]
    1. getStrikeNum()이라는 함수에서 사용자 입력과 answer를 한 자리씩 비교해서 스트라이크의 수를 얻는다.
    2. getBallNum()이라는 함수에서 사용자 입력의 숫자들 중 answer에 포함되어 있는 갯수를 먼저 구한다. 이 때 answer에 사용자 입력의 숫자가 속해 있는 경우는 스트라이크이거나 볼일 경우이다. 따라서 앞에서 구한 갯수에서 스트라이크를 빼서 볼의 수를 얻는다.
    3. 앞에 나온 strike와 ball의 개수에 따라 맞는 문자열을 반환한다. 이 때, strike와 ball의 갯수가 모두 0일 경우 낫싱을 반환한다.

--- src/main/java/baseball/BaseballBot.java
+++ src/main/java/baseball/BaseballBot.java
@@ -7,12 +7,14 @@ import java.util.List;
 public class BaseballBot {
     private static final int START_RANGE = 1;
     private static final int END_RANGE = 9;
-
     private static final int NUM_LENGTH = 3;
 
-    private int answer;
+    private static final String STRIKE_STR = "스트라이크 ";
+    private static final String BALL_STR = "볼 ";
+    private static final String NOTHING_STR = "낫싱";
 
-    //TODO: unique한 값인지 확인
+    private String answer;
+
     public void createRandomValue() {
         StringBuilder randomNumber = new StringBuilder();
         List<Integer> randomUniqueDigits = Randoms.pickUniqueNumbersInRange(START_RANGE, END_RANGE, NUM_LENGTH);
@@ -21,7 +23,49 @@ public class BaseballBot {
             randomNumber.append(randomUniqueDigit);
         }
 
-        answer = Integer.parseInt(randomNumber.toString());
-        answer++;
-    }//서로
+        answer = randomNumber.toString();
+    }
+
+    public String checkHint(String userInput) {
+        int strike = getStrikeNum(userInput);
+        int ball = getBallNum(userInput, strike);
+
+        return createHintStr(strike, ball);
+    }
+
+    private int getStrikeNum(String userInput) {
+        int strike = 0;
+
+        for (int i = 0; i < NUM_LENGTH; i++) {
+            if (answer.charAt(i) == userInput.charAt(i)) {
+                strike++;
+            }
+        }
+
+        return strike;
+    }
+
+    private int getBallNum(String userInput, int strike) {
+        int sameNumberCount = 0;
+
+        for (int i = 0; i < NUM_LENGTH; i++) {
+            char curChar = userInput.charAt(i);
+
+            if (answer.contains(String.valueOf(curChar))) {
+                sameNumberCount++;
+            }
+        }
+
+        //같은 숫자가 나오는 경우는 스트라이크이거나 볼일 경우 두 가지이다
+        //따라서 (볼의 개수) = (같은 숫자의 개수) - (스트라이크 개수)
+        return sameNumberCount - strike;
+    }
+
+    private String createHintStr(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            return NOTHING_STR;
+        }
+
+        return strike + STRIKE_STR + ball + BALL_STR;
+    }
 }
