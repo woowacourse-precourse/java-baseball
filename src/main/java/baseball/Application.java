0d318bda3de1fcfb27b797c7207e242d8fa29905
ParkSangCheon <pswlove38@naver.com>
2022-11-05 05:28:12 +0900

    받은 숫자를 각 자리수로 짤라서 리스트로 반환하는 returnList함수 구현 checkNum 함수 기능 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,35 +6,38 @@ public class Application {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random random = new Random();
-        int com1, com2, com3;
-        while(true) {
-            //1~9사이의 난수생성
-            com1 = random.nextInt(100) % 9 + 1;
-            com2 = random.nextInt(100) % 9 + 1;
-            com3 = random.nextInt(100) % 9 + 1;
-            if(!(com1==com2 || com2==com3 || com3==com1)) {
-                //중복되지 않는 난수 생성에 성공하면 루프탈출
-                break;
+        List<Integer> computer = new ArrayList<>();
+
+        // 맞출 컴퓨터의 수 생성
+        while (computer.size() < 3) {
+            int randomNumber = (random.nextInt(9)+1);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
             }
         }
-        //난수확인
-        System.out.println(com1+" "+com2+" "+com3);
+        System.out.println(computer);
+
         System.out.println("숫자 야구 게임을 시작합니다.");
-//        while(true){
-//            System.out.print("숫자를 입력해주세요 : ");
-//            int num = sc.nextInt();
-//            checkNum(num);
-//        }
-        checkNum(123);
+        while(true){
+            List<Integer> numList = new ArrayList<>();
+            System.out.print("숫자를 입력해주세요 : ");
+            int num = sc.nextInt();
+            numList = returnList(num);
+            System.out.println(numList);
+        }
     }
 
-    public static void checkNum(int num){
-        ArrayList<Integer> each = new ArrayList<>();
+    public static List<Integer> returnList(int num){
+        List<Integer> each = new ArrayList<>();
         while(num != 0) {
             each.add(num % 10);
             num = num / 10;
         }
         Collections.reverse(each);
-        System.out.println(each);
+        return each;
+    }
+    // 받은 숫자를 각 자리수로 짤라서 리스트에 넣음
+    public static void checkNum(int num){
+
     }
 }
