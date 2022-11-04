7b3727974a0d5b69940a920a89b08fd23806bdfb
YunDaeHyeok <bmong4mong0318@gmail.com>
2022-11-04 16:30:00 +0900

    test: add Nested class for better DisplayName

--- src/test/java/baseball/BaseBallNumberTest.java
+++ src/test/java/baseball/BaseBallNumberTest.java
@@ -1,55 +1,66 @@
 package baseball;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class BaseBallNumberTest {
 
-  @Test
-  @DisplayName("validate number size_empty")
-  void validateSizeEmpty() {
-    Assertions.assertThatThrownBy(()->{
-      BaseBallNumber baseBallNumber = new BaseBallNumber(Collections.emptyList());
-    }).isInstanceOf(IllegalArgumentException.class);
+  @Nested
+  @DisplayName("BaseBallNumber Class")
+  class BaseBallNumberKoTest {
+
+    @Nested
+    @DisplayName("validateSize method is")
+    class DescribeValidateSize {
+
+      @Test
+      @DisplayName("empty")
+      void validateSizeEmpty() {
+        Assertions.assertThatThrownBy(() -> {
+          BaseBallNumber baseBallNumber = new BaseBallNumber(Collections.emptyList());
+        }).isInstanceOf(IllegalArgumentException.class);
+      }
+
+      @Test
+      @DisplayName("less")
+      void validateSizeLess() {
+        Assertions.assertThatThrownBy(() -> {
+          BaseBallNumber baseBallNumber = new BaseBallNumber(List.of('1', '2'));
+        }).isInstanceOf(IllegalArgumentException.class);
+      }
+
+      @Test
+      @DisplayName("more")
+      void validateSizeMore() {
+        Assertions.assertThatThrownBy(() -> {
+          BaseBallNumber baseBallNumber = new BaseBallNumber(List.of('1', '2', '3', '4'));
+        }).isInstanceOf(IllegalArgumentException.class);
+      }
+    }
+
+    @Nested
+    @DisplayName("validateDuplicate method is")
+    class DescribeValidateDuplicate {
+
+      @Test
+      @DisplayName("two")
+      void validateDuplicateTwo() {
+        Assertions.assertThatThrownBy(() -> {
+          BaseBallNumber baseBallNumber = new BaseBallNumber(List.of('1', '1', '2'));
+        }).isInstanceOf(IllegalArgumentException.class);
+      }
+
+      @Test
+      @DisplayName("three")
+      void validateDuplicateThree() {
+        Assertions.assertThatThrownBy(() -> {
+          BaseBallNumber baseBallNumber = new BaseBallNumber(List.of('1', '1', '1'));
+        }).isInstanceOf(IllegalArgumentException.class);
+      }
+    }
   }
-
-  @Test
-  @DisplayName("validate number size_less")
-  void validateSizeLess() {
-    Assertions.assertThatThrownBy(()->{
-      BaseBallNumber baseBallNumber = new BaseBallNumber(List.of('1', '2'));
-    }).isInstanceOf(IllegalArgumentException.class);
-  }
-
-  @Test
-  @DisplayName("validate number size_more")
-  void validateSizeMore() {
-    Assertions.assertThatThrownBy(()->{
-      BaseBallNumber baseBallNumber = new BaseBallNumber(List.of('1', '2', '3', '4'));
-    }).isInstanceOf(IllegalArgumentException.class);
-  }
-
-
-  @Test
-  @DisplayName("validate number duplicate_two")
-  void validateDuplicateTwo() {
-    Assertions.assertThatThrownBy(()->{
-      BaseBallNumber baseBallNumber = new BaseBallNumber(List.of('1', '1', '2'));
-    }).isInstanceOf(IllegalArgumentException.class);
-  }
-
-  @Test
-  @DisplayName("validate number duplicate_three")
-  void validateDuplicateThree() {
-    Assertions.assertThatThrownBy(()->{
-      BaseBallNumber baseBallNumber = new BaseBallNumber(List.of('1', '1', '1'));
-    }).isInstanceOf(IllegalArgumentException.class);
-  }
-
 }
