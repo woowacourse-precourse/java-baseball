74fa2ca590eee1e0d91c931b72e9bfb5959dbb10
Hayoon <hy97@sju.ac.kr>
2022-11-04 19:28:25 +0900

    feat: 게임 재시작 여부 확인 메서드(replayGame) 생성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -13,7 +13,6 @@ import java.util.stream.Stream;
 public class Game {
 
     List<Integer> computer;
-    HashMap<String, Integer> strikeAndBallCount;
 
     Game() {
         computer = new ArrayList<>();
@@ -29,24 +28,29 @@ public class Game {
         return Console.readLine();
     }
 
-    public void checkUserAnswerWhichIsCorrect(String userAnswer) {
-        strikeAndBallCount = new HashMap<>();
+    public boolean checkUserAnswerWhichIsCorrect(String userAnswer) {
+        HashMap<String, Integer> strikeAndBallCount = new HashMap<>();
 
         strikeAndBallCount.put("Strike", 0);
         strikeAndBallCount.put("Ball", 0);
 
-        checkAnswerState(strikeAndBallCount, stringToListInteger(userAnswer));
+        HashMap<String, Integer> strikeAndBallCountResult = checkAnswerState(strikeAndBallCount, stringToListInteger(userAnswer));
+
+        return printResultCompareWithAnswer(strikeAndBallCountResult);
     }
 
-    private void checkAnswerState(HashMap<String, Integer> strikeAndBallCount, List<Integer> userAnswer) {
+    private HashMap<String, Integer> checkAnswerState(HashMap<String, Integer> strikeAndBallCount, List<Integer> userAnswer) {
         for (int i = 0; i < 3; i++) {
             if (computer.contains(userAnswer.get(i))
                     && (userAnswer.get(i).equals(computer.get(i)))) {
+                System.out.println("스트라이크 " + userAnswer.get(i) + " " + computer.get(i));
                 strikeAndBallCount.put("Strike", strikeAndBallCount.get("Strike") + 1);
             } else if (computer.contains(userAnswer.get(i))
-                    && (!userAnswer.get(i).equals(computer.get(i))))
-            strikeAndBallCount.put("Ball", strikeAndBallCount.get("Ball") + 1);
+                    && (!userAnswer.get(i).equals(computer.get(i)))) {
+                strikeAndBallCount.put("Ball", strikeAndBallCount.get("Ball") + 1);
+            }
         }
+        return strikeAndBallCount;
     }
 
     public void incorrectUserAnswerRule(String userAnswer) {
@@ -66,25 +70,33 @@ public class Game {
                 .collect(Collectors.toList());
     }
 
-    public boolean printResultCompareWithAnswer(HashMap<String, Integer> strikeAndBallCount) {
+    private boolean printResultCompareWithAnswer(HashMap<String, Integer> strikeAndBallCount) {
         Integer strike = strikeAndBallCount.get("Strike");
         Integer ball = strikeAndBallCount.get("Ball");
 
         if (strike == 0 && ball != 0) {
-            System.out.println(strikeAndBallCount.get("Strike") + "스트라이크");
-        } else if (strike != 0 && ball == 0) {
             System.out.println(strikeAndBallCount.get("Ball") + "볼");
+        } else if (strike != 3 && strike != 0 && ball == 0) {
+            System.out.println(strikeAndBallCount.get("Strike") + "스트라이크");
         } else if (strike != 0 && ball != 0) {
             System.out.println(strikeAndBallCount.get("Ball") + "볼 "
                     + strikeAndBallCount.get("Strike") + "스트라이크");
         } else if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
         } else if (strike == 3) {
-            System.out.print("3스트라이크" +"\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+//            System.out.println(strikeAndBallCount);
+            System.out.print("3스트라이크" + "\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+
+
             return true;
         }
 
         return false;
+    }
+
+    private String replayGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
     }
 }
 
