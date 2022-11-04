84a0f5017f2c72e531534df2baaf20230c0d5b22
Jin409 <sualng123@gmail.com>
2022-11-04 17:28:38 +0900

    test(NumberValidatorTest): static 메서드 수정에 따른 테스트 코드 수정 및 hasValidType 테스트 코드 추가

--- src/test/java/baseball/utils/NumberValidatorTest.java
+++ src/test/java/baseball/utils/NumberValidatorTest.java
@@ -11,28 +11,34 @@ public class NumberValidatorTest {
     @Test
     void 유효성_검사_테스트() {
         List<Integer> validNum = new ArrayList<>(List.of(1, 2, 3));
-        NumberValidator numberValidator = new NumberValidator();
-        boolean validNumResult = numberValidator.hasValidInput(validNum);
+        boolean validNumResult = NumberValidator.hasValidInput(validNum);
         assertThat(validNumResult).isEqualTo(true);
 
         String rangeErrorMessage = "각 자리 수는 최소 1 이상, 최대 9 이하의 정수여야 합니다.";
         String lengthErrorMessage = "숫자는 세 자리 수여야 합니다.";
         String duplicateErrorMessage = "숫자는 중복되어서는 안됩니다.";
+        String typeErrorMessage = "숫자만 입력 가능합니다.";
 
         List<Integer> invalidRangeNum = new ArrayList<>(List.of(369, 1, 2));
-        assertThatThrownBy(() -> numberValidator.hasValidInput(invalidRangeNum))
+        assertThatThrownBy(() -> NumberValidator.hasValidInput(invalidRangeNum))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(rangeErrorMessage);
 
         List<Integer> invalidLengthNum = new ArrayList<>(List.of(1,2));
-        assertThatThrownBy(() -> numberValidator.hasValidInput(invalidLengthNum))
+        assertThatThrownBy(() -> NumberValidator.hasValidInput(invalidLengthNum))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(lengthErrorMessage);
 
         List<Integer> duplicateNum = new ArrayList<>(List.of(1,2,2));
-        assertThatThrownBy(() -> numberValidator.hasValidInput(duplicateNum))
+        assertThatThrownBy(() -> NumberValidator.hasValidInput(duplicateNum))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(duplicateErrorMessage);
+
+        String typeErrorStr = "안123";
+        assertThatThrownBy(() -> NumberValidator.hasValidType(typeErrorStr))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(typeErrorMessage);
+
     }
 
 }
