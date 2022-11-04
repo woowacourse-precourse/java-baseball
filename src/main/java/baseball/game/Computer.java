def46452d3d896da2d84fa9f0ae8341198a408af
minju412 <ln8847@ajou.ac.kr>
2022-11-04 13:45:23 +0900

    feat: set computer number

--- src/main/java/baseball/game/Computer.java
+++ src/main/java/baseball/game/Computer.java
@@ -7,6 +7,19 @@ import java.util.List;
 public class Computer {
 
     public static final int COMPUTER_NUMBER_SIZE = 3;
+    private List<Integer> computerNumber;
+
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
+
+    public void setComputerNumber(List<Integer> computerNumber) {
+        this.computerNumber = computerNumber;
+    }
+
+    public void initial() {
+        setComputerNumber(createComputerNumber());
+    }
 
     public List<Integer> createComputerNumber() {
         List<Integer> computerNumber = new ArrayList<>();
