203ed581c6d4ad317e13db6fe754ccda4ca2b659
splitube <voicesplit@daum.net>
2022-11-05 01:35:35 +0900

    feat(GameMachine): 한줄을 입력 받는 기능 구현
    문제에서 지정한대로 Console 클래스 사용하여 구현

--- src/main/java/baseball/NumberBaseBallGameMachine.java
+++ src/main/java/baseball/NumberBaseBallGameMachine.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class NumberBaseBallGameMachine {
     public void display(String message) {
         System.out.print(message);
+    }
+
+    public String getInputLine() {
+        return Console.readLine();
     }
 }
