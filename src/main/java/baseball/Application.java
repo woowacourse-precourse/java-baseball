3feeab01ad661e2203894a16d555852bcc8c5666
nn98 <jkllhgb@gmail.com>
2022-11-05 08:07:40 +0900

    Feat : 메소드 삭제, 클래스 분리 및 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,28 +1,17 @@
 package baseball;
-import java.util.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import constant.Texts;
 public class Application {
-    static int target,input,count;
-    static int game(int target,int input){
-        int strikeCount=0;
-        return strikeCount;
-    }
-    static List<Integer>splitDigits(int value){
-        List<Integer>returnDate=Collections.emptyList();
-        return returnDate;
-    }
-    static int checkResult(List<Integer> target,List<Integer> input){
-        int result=0;
-        return result;
-    }
-    static int isStrike(int target,int input){
-        int result=0;
-        return result;
-    }
-    static int isBall(int target,List<Integer> input){
-        int result=0;
-        return result;
-    }    
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game=new Game();
+        game.run();
     }
+
 }
