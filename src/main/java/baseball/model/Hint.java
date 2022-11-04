99cd6fb98088fe83eef0cf7dab04a68d0777370a
SHEOMM <esh0821@snu.ac.kr>
2022-11-04 14:42:08 +0900

    refactor(Hint): refactor the hint's attribute to final
    한번 생성되면 수정될 필요 없는 hint의 attribute final로 리팩토링

--- src/main/java/baseball/model/Hint.java
+++ src/main/java/baseball/model/Hint.java
@@ -1,11 +1,16 @@
 package baseball.model;
 
 public class Hint {
-    private int strike;
-    private int ball;
+    private final int strike;
+    private final int ball;
     public Hint(int ball, int strike){
-        this.ball = ball;
-        this.strike = strike;
+        if(ball <= 3 && strike <=3){
+            this.ball = ball;
+            this.strike = strike;
+        }else{
+            throw new IllegalStateException("ball과 strike는 0이상 3 미만이어야 합니다.");
+        }
+
     }
     @Override
     public String toString() {
