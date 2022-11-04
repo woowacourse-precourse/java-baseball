5869896f9aa0d408c0fb64ec941cac0e895e15ff
Dongmin Kim <kimdongmin68@naver.com>
2022-11-04 15:20:50 +0900

    feat(inputException): 유저의 입력 값에 중복 숫자가 들어가 있는 경우 예외 처리

--- src/main/java/baseball/InputException.java
+++ src/main/java/baseball/InputException.java
@@ -1,11 +1,14 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class InputException {
     private static final String INVALID_INPUT_NUMBER_ERROR_MESSAGE = "ERROR!! 정상적인 값이 아닙니다. 게임을 종료합니다.";
     private static int NUMBER_LENGTH = 3;
 
     public static void checkInputNumberValidation(String number) {
-        if (isThreeLength(number) && isInteger(number) && hasNotZero(number)) {
+        if (isThreeLength(number) && isInteger(number) && hasNotZero(number) && hasNotDuplicate(number)) {
             return;
         }
         throw new IllegalArgumentException(INVALID_INPUT_NUMBER_ERROR_MESSAGE);
@@ -34,5 +37,16 @@ public class InputException {
             }
         }
         return true;
+    }
+
+    public static boolean hasNotDuplicate(String number) {
+        Set<Character> numberSet = new HashSet<>();
+        for (int digit = 0; digit < NUMBER_LENGTH; digit++) {
+            numberSet.add(number.charAt(digit));
+        }
+        if (NUMBER_LENGTH == numberSet.size()) {
+            return true;
+        }
+        return false;
     }
 }
