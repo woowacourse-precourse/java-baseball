f952a51761e0b37a1762e6ef10221577b12e148e
yg0585 <yg0585@naver.com>
2022-11-04 17:58:54 +0900

    feat: BallCount toString 재구현

--- src/main/java/baseball/BallCount.java
+++ src/main/java/baseball/BallCount.java
@@ -29,4 +29,18 @@ public class BallCount {
     public int getBall() {
         return ball;
     }
+
+    @Override
+    public String toString() {
+        if (ball == 0 && strike == 0)
+            return "낫싱";
+
+        if (ball == 0)
+            return strike + "스트라이크";
+
+        if (strike == 0)
+            return ball + "볼";
+
+        return ball + "볼 " + strike + "스트라이크";
+    }
 }
