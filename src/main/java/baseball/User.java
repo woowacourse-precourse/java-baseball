3283e499e4891c64b3d7ade38937def4e4b3be8f
jmj005 <jmj00005@naver.com>
2022-11-05 02:03:18 +0900

    refactor(User): 사용자 클래스 refactoring

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -8,45 +8,48 @@ import java.util.Set;
 
 public class User {
 
-    private static boolean isValidScope(String userNumber) {
-        return userNumber.matches("[1-9]+");
-    }
+    private List<Integer> userNumber;
+    private String inputUserNumber;
 
-    private static boolean isThreeLength(String userNumber) {
-        return userNumber.length() == 3;
-    }
-
-    private static boolean isDuplication(String userNumber) {
-        Set<Character> notDuplication = new HashSet<>();
-        for (int index = 0; index < userNumber.length(); index++) {
-            notDuplication.add(userNumber.charAt(index));
+    public void setUserNumber() {
+        this.userNumber = new ArrayList<>();
+        this.inputUserNumber = Console.readLine();
+        try {
+            validateNumber();
+        } catch (IllegalArgumentException exception) {
+            System.out.println(exception.getMessage());
+            return ;
         }
-        return notDuplication.size() == userNumber.length();
+        for (char element : this.inputUserNumber.toCharArray()) {
+            this.userNumber.add(element - '0');
+        }
     }
 
-    private static void isValidNumber(String inputUserNumber) {
-        if (!isValidScope(inputUserNumber)) {
-            throw new IllegalArgumentException("1 ~ 9 시이의 수만 입력할 수 있습니다.");
+    public void validateNumber() {
+        if (!isValidScope()) {
+            throw new IllegalArgumentException("1~9 사이의 수만 입력할 수 있습니다.");
         }
-        if (!isThreeLength(inputUserNumber)) {
+        if (!hasSameNumber()) {
+            throw new IllegalArgumentException("중복되지 않는 수만 입력할 수 있습니다.");
+        }
+        if (this.inputUserNumber.length() != 3) {
             throw new IllegalArgumentException("세 자리의 수만 입력할 수 있습니다.");
         }
-        if (!isDuplication(inputUserNumber)) {
-            throw new IllegalArgumentException("중복되지 않은 수만 입력할 수 있습니다.");
-        }
     }
 
-    public static List<Integer> getNumber() {
-        String inputUserNumber = Console.readLine();
-        List<Integer> userNumber = new ArrayList<>();
-        try {
-            isValidNumber(inputUserNumber);
-        } catch (IllegalArgumentException exception) {
-            throw new IllegalArgumentException(exception);
+    public boolean isValidScope() {
+        return this.inputUserNumber.matches("^[1-9]{3}$");
+    }
+
+    public boolean hasSameNumber() {
+        Set<Character> uniqueNumbers = new HashSet<>();
+        for (char element : this.inputUserNumber.toCharArray()) {
+            uniqueNumbers.add(element);
         }
-        for (char element : inputUserNumber.toCharArray()) {
-            userNumber.add(element - '0');
-        }
-        return userNumber;
+        return uniqueNumbers.size() == 3;
+    }
+
+    public List<Integer> getUserNumber() {
+        return this.userNumber;
     }
 }
