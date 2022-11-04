471b0d15b30eaa73fff772b4589a692a258c5278
안광민 <creativeduck0518@gmail.com>
2022-11-04 16:24:42 +0900

    feat(ErrorChecker): 0. 에러 처리 클래스 생성

---
+++ src/main/java/baseball/ErrorChecker.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import java.util.List;
+
+public class ErrorChecker {
+    private final int numberSize;
+    private final int startInclusive;
+    private final int endInclusive;
+    private final List<Integer> continueGameInputList;
+    private static final String DUPLICATE_NUMBER = "중복된 숫자가 포함되어있습니다.";
+    private static final String ILLEGAL_INPUT = "잘못된 값이 입력되었습니다.";
+    private static final String ILLEGAL_SIZE = "입력값이 정해진 자릿수가 아닙니다.";
+
+    public ErrorChecker(int numberSize, int startInclusive, int endInclusive, List<Integer> continueGameInputList) {
+        this.numberSize = numberSize;
+        this.startInclusive = startInclusive;
+        this.endInclusive = endInclusive;
+        this.continueGameInputList = continueGameInputList;
+    }
+}
