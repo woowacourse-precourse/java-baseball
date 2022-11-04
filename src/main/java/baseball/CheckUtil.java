526d4fa564428406c8729462b98b76cb1e857455
jingyu9804 <swc05161@gmail.com>
2022-11-05 02:01:39 +0900

    feat : 중복성 체크 method 추가

--- src/main/java/baseball/CheckUtil.java
+++ src/main/java/baseball/CheckUtil.java
@@ -1,19 +1,29 @@
 package baseball;
 
+import static baseball.Constant.*;
 public class CheckUtil {
 
     public void checkIsValidDigit(String input){
         for(char c:input.toCharArray()){
             int c_int = Character.getNumericValue(c);
             if(c_int<1 || c_int>9){
-                throw new IllegalArgumentException("1-9 사이의 유효한 숫자만 입력하세요.");
+                throw new IllegalArgumentException(invalidNumberMessage);
             }
         }
     }
 
     public void checkIsValidLength(int length){
         if(length != 3){
-            throw new IllegalArgumentException("3자리 숫자만 입력하세요.");
+            throw new IllegalArgumentException(invalidLengthMessage);
+        }
+    }
+
+    public void checkIsDuplicated(String input){
+        char firstChar = input.charAt(0);
+        char secondChar = input.charAt(1);
+        char thirdChar = input.charAt(2);
+        if(firstChar == secondChar || secondChar == thirdChar || firstChar == thirdChar){
+            throw new IllegalArgumentException(duplicatedNumberMessage);
         }
     }
 }
