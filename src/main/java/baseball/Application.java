b0c71617e8739481af423f0131b84ab411ba758f
kimdahee0815 <diana815@naver.com>
2022-11-04 14:28:26 +0900

    feat(check 함수) : strikeCount, ballCount를 선언해준 후에 볼과 스트라이크 개수를 체크해주는 check 함수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,6 +15,9 @@ public class Application {
     	
     	System.out.println("숫자 야구 게임을 시작합니다.");
 		while(true) {
+			int strikeCount=0;
+			int ballCount=0;
+			
 			if(numstr.equals("1")) {  //게임을 새로 시작할 것인지 종료할 것인지 1,2 선택
 				numstr="";            //numstr = ""로 만들어 else구문으로 다시 진입하게 만드는 장치
 				continue;
@@ -26,6 +29,7 @@ public class Application {
 			else {                     //numstr이 1,2가 아니라면 무조건 else구문으로 들어와서 check
 				System.out.print("숫자를 입력해주세요 : "); 
 				numstr = Console.readLine();
+				check(numstr,strikeCount,ballCount);
 			}
 		}	
     }
@@ -35,6 +39,23 @@ public class Application {
 			int randomNumber = Randoms.pickNumberInRange(1,9);
 			if(!computer.contains(randomNumber)) {
 				computer.add(randomNumber);
+			}
+		}
+	}
+    
+    public static void check(String numstr,int strikeCount,int ballCount) {
+		possibleBallResult.put("볼",0);
+		possibleBallResult.put("스트라이크",0);
+		
+		for(int i =0;i<num.length;i++) {
+			if(Integer.parseInt(num[i])==computer.get(i)){  //스트라이크 개수 세기(computer 리스트 글자 하나와 사용자 입력 숫자 하나씩 같은지, 같은 위치인지까지 체크 - 스트라이크)
+				strikeCount = possibleBallResult.getOrDefault("스트라이크",0 );
+				strikeCount++;
+				possibleBallResult.put("스트라이크", strikeCount);
+			}
+			else if(computer.contains(Integer.parseInt(num[i]))){ //볼 개수 세기(computer 리스트에 있는 모든 원소 중 사용자 입력 숫자가 하나라도 있는지 체크 - 볼)
+				ballCount++;
+				possibleBallResult.put("볼", ballCount);
 			}
 		}
 	}
