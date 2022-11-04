6a210cbb154907508101973d41e1bf22415afd1b
Jueun <jueun000101@gmail.com>
2022-11-04 21:57:07 +0900

    refactor(OutputView): add print method
    서비스 로직 중에 출력 -> OutputView를 통해서만 뷰(콘솔)에 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,7 +3,19 @@ package baseball.view;
 import baseball.util.Message;
 
 public class OutputView {
+    public static void printGameStart() {
+        System.out.println(Message.GAME_START);
+    }
+
     public static void printInputRequest() {
         System.out.print(Message.INPUT_REQUEST);
+    }
+
+    public static void printEndOrNot() {
+        System.out.println(Message.END_OR_NOT);
+    }
+
+    public static void printResult(String result) {
+        System.out.println(result);
     }
 }
