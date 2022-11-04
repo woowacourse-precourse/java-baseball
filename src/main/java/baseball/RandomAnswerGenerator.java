e320af3bf396c827b3c6996ed908d2445649d7d8
jagajang <kosb7544@gmail.com>
2022-11-04 16:09:53 +0900

    dev: add numeric generating method to create 3 digit answer

--- src/main/java/baseball/RandomAnswerGenerator.java
+++ src/main/java/baseball/RandomAnswerGenerator.java
@@ -2,12 +2,24 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RandomAnswerGenerator {
     public static int getRandomAnswer() {
         int answer = 0;
+        Set<Integer> usedNumeric = new HashSet<>();
 
         // generate random answer
+        for(int i = 0; i < 3; i++) {
+            int numeric = getRandomNumeric(usedNumeric);
+            answer = 10*answer + numeric;
+        }
 
+        return answer;
+    }
+
+    private static int getRandomNumeric(Set<Integer> usedNumeric) {
         return 0;
     }
 }
