b657fba02f2cd3c7b12365c331a87e29bbf39df7
KimTaeKang57 <zizoen123@gmail.com>
2022-11-04 13:16:24 +0900

    [리팩토링] 컴퓨터 숫자를 입력하는 메소드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -20,13 +20,7 @@ public class Application {
     }
 
     private static void baseballGame() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
+        List<Integer> computer = getComputerNumber();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         for (int i = 0; i < 3; i++) {
@@ -71,5 +65,31 @@ public class Application {
                 break;
             }
         }
+    }
+
+    // 컴퓨터 숫자 입력받는 메소드
+    private static List<Integer> getComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (isSize(computer)) {
+            getRandomNumber(computer, Randoms.pickNumberInRange(1, 9));
+        }
+        return computer;
+    }
+
+    // 컴퓨터 리스트의 사이즈가 3 미만인지 확인하는 메소드
+    private static boolean isSize(List<Integer> computer) {
+        return computer.size() < 3;
+    }
+
+    // 컴퓨터의 숫자를 랜덤으로 부여하는 메소드
+    private static void getRandomNumber(List<Integer> computer, int randomNumber) {
+        if (isContains(computer, randomNumber)) {
+            computer.add(randomNumber);
+        }
+    }
+
+    // 중복된 숫자가 있는지 확인하는 메소드
+    private static boolean isContains(List<Integer> computer, int randomNumber) {
+        return !computer.contains(randomNumber);
     }
 }
