c97adafa63aeec368db6425b7c22cce70412a19f
kch4731 <kch4731@naver.com>
2022-11-04 19:38:20 +0900

    Feat : 컴퓨터 숫자 설정하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,28 @@
 package baseball;
 
-import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    private static List<Integer> getNumberOfComputer(List<Integer> computer) {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
+
+    public static void main(String[] args) {
+
+        // 1. 상대방(컴퓨터)의 숫자를 설정한다.
+        List<Integer> computer = new ArrayList<>();
+        computer = getNumberOfComputer(computer);
+        System.out.println(computer); // 테스트 출력
+    }
+
+
 }
