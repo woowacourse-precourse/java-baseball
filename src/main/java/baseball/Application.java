d0318ab8df65136ffb61211e0a1f09c8f5264e24
LEE-haeuk <rkwksmswhdk@naver.com>
2022-11-05 00:04:58 +0900

    feat: add the phrase making the player enter numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,7 @@ public class Application {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         computer = application.createRandomNumbersInList();
+        System.out.print("숫자를 입력해주세요 : ");
     }
 
     public List<Integer> createRandomNumbersInList() {
