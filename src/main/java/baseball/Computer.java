51aab3956fc9b40177e4a17affea5c0d08f42832
miffyKing <whqudghk28@gmail.com>
2022-11-04 20:03:13 +0900

    feat: Make Computer Class methods that generate valid random value

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+
 public class Computer {
-  private static String RandonValue;
+  private static String RandomValue;
 
   private static void printGameStart() {
     System.out.println("숫자 야구 게임을 시작합니다.");
@@ -11,10 +15,28 @@ public class Computer {
     System.out.print("숫자를 입력해주세요 : ");
   }
 
+  private static String generateRandom() {
+    StringBuilder sb = new StringBuilder();
+    ArrayList <Integer> generateRandNum = new ArrayList<>();
+
+    while (generateRandNum.size() < 3) {
+      int randomNumber = Randoms.pickNumberInRange(1, 9);
+      if (!generateRandNum.contains(randomNumber)) {
+        generateRandNum.add(randomNumber);
+        sb.append(Integer.toString(randomNumber));
+      }
+    }
+    RandomValue = sb.toString();
+    return RandomValue;
+  }
+
   public static void testComputerPrint(int mode) {
     if (mode == 1) printGameStart();
     if (mode == 2) printInputNumber();
+    //if (mode == 3) generateRandom();
   }
-
+  public static String testRandomGen() {
+    return generateRandom();
+  }
 
 }
