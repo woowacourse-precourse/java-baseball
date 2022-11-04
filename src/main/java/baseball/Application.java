01a1f941692d51c5f7361025c54497795c9b203e
HamSeungChan <head0618@naver.com>
2022-11-04 18:41:37 +0900

    feat : 상대방(컴퓨터) 3개의 다른 수 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+    private static List<Integer> setComputerNumberGroup(){
+        List<Integer> computerNumberGroup = new ArrayList<>();
+        while(computerNumberGroup.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computerNumberGroup.contains(randomNumber)){
+                computerNumberGroup.add(randomNumber);
+            }
+        }
+        return computerNumberGroup;
     }
 }
