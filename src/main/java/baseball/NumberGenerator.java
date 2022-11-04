88719ec48108bbbce1c7ee539c57a3ded2b1a5ee
iamjooon2 <iamjooon2@gmail.com>
2022-11-05 00:12:20 +0900

    feat: 랜덤한 세 숫자를 생성하는 기능 구현

---
+++ src/main/java/baseball/NumberGenerator.java
@@ -0,0 +1,19 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberGenerator {
+    public List<Integer> createNumbers() {
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (computerNumbers.contains(number)) {
+                continue;
+            }
+            computerNumbers.add(number);
+        }
+        return computerNumbers;
+    }
+}
