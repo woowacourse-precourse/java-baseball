f4aee6d76fa6a3ca3a39aca7b0f5e9ae97d25861
aud19 <aud19980518@gmail.com>
2022-11-04 19:07:49 +0900

    domain의 computer class 작성

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,17 @@
+package baseball.domain;
+
+public class Computer {
+    private String computerAnswer;
+
+    public Computer(String computerAnswer) {
+        this.computerAnswer = computerAnswer;
+    }
+
+    public String getComputerAnswer() {
+        return computerAnswer;
+    }
+
+    public void setComputerAnswer(String computerAnswer) {
+        this.computerAnswer = computerAnswer;
+    }
+}
