17469d3d8d080941522d52a929a2c59e482df353
chener <49267211+chanosong@users.noreply.github.com>
2022-11-04 14:59:23 +0900

    feat: Add Game, BallCount Class and list up function
    + Class Game
    + Class BallCount

---
+++ src/main/java/baseball/BallCount.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class BallCount {
+    // TODO: <Class> Ball Count (strikes, balls)
+    private int strike;
+    private int ball;
+
+    public BallCount(){
+        // TODO: <Func> Class Initializer
+    }
+
+    // TODO: GETTER, SETTER
+}
---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,29 @@
+package baseball;
+
+public class Game {
+    // TODO: <Local Value> Game Class
+    private int answer;
+    private BallCount ballcount;
+
+    public Game(){
+        // TODO: <Func> Initializer
+    }
+
+    private void setAnswer(){
+        // TODO: <Func> Set random number size 3 Integer for answer
+    }
+
+    private void checkAnswer(){
+        // TODO: <Func> Check input value and compare with answer
+    }
+
+    private void countUp(){
+        // TODO: <Func> Change status
+    }
+    private void printCount(){
+        // TODO: <Func> Print ball count
+    }
+    private void isOver(){
+        // TODO: <Func> Check game is over
+    }
+}
