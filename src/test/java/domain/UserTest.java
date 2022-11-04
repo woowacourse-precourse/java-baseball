85050914f7d6030f75c70a15abb456202fda86eb
Kimhyeyun <cqqwer@gmail.com>
2022-11-04 19:25:09 +0900

    Test: test내에서 입력 처리

--- src/test/java/domain/UserTest.java
+++ src/test/java/domain/UserTest.java
@@ -1,15 +1,26 @@
 package domain;
 
+import baseball.Application;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import constant.Messages;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.time.Year;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class UserTest {
+public class UserTest{
     @Test
     void 숫자외_문자_입력시_예외_처리() {
         User user = new User();
+        String input = "abc";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
         IllegalArgumentException exception =
                 assertThrows(IllegalArgumentException.class, () -> user.input());
 
@@ -19,6 +30,11 @@ public class UserTest {
     @Test
     void 입력_길이가_3이_아닌_경우_예외_처리() {
         User user = new User();
+
+        String input = "1234";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
         IllegalArgumentException exception =
                 assertThrows(IllegalArgumentException.class, () -> user.input());
         assertEquals(exception.getMessage(), Messages.wrongInputSize);
@@ -27,8 +43,14 @@ public class UserTest {
     @Test
     void 입력에_중복이_있는_경우_예외_처리() {
         User user = new User();
+
+        String input = "112";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
         IllegalArgumentException exception =
                 assertThrows(IllegalArgumentException.class, () -> user.input());
         assertEquals(exception.getMessage(), Messages.duplicateNumber);
     }
+
 }
