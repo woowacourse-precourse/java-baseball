eb837e71c0791607e53222c1748d7ae8e778c77f
youngkyunIm <yee0230@naver.com>
2022-11-04 14:47:13 +0900

    feat : add count ball

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,7 +11,23 @@ public class Application {
     public static void main(String[] args) {
 
         List<Integer> computer = getComputerValue();
+        System.out.println(computer);
+        List<Integer> user = new Input(readLine()).getInputList();
 
+        int ball = 0;
+        for (int i = 0; i < computer.size(); i += 1) {
+            ball += countBall(computer, user, i);
+        }
+        if (ball != 0) System.out.print(ball + "볼 ");
+    }
+
+    private static int countBall(List<Integer> computer, List<Integer> user, int i) {
+        int ball = 0;
+        for (int j = 0; j < computer.size(); j += 1) {
+            if (i == j) continue;
+            if (computer.get(i) == user.get(j)) ball += 1;
+        }
+        return ball;
     }
 
     private static List<Integer> getComputerValue() {
