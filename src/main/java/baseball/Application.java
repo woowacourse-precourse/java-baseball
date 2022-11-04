cb65fc5db279754864bdc24dbac9be2485aeb7f7
sjh910805@gmail.com <sjh910805@gmail.com>
2022-11-04 17:53:30 +0900

    feat: 결과 출력, 시작 문구 출력 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -27,6 +27,7 @@ class Computer {
         }
         number = numberList;
     }
+
     public List<Integer> getNumber() {
         return number;
     }
@@ -38,23 +39,26 @@ class Player {
     public void inputNumber() {
         List<Integer> numberList = new ArrayList<>();
         int num = Integer.valueOf(Console.readLine());
-        while(num>0) {
+        while (num > 0) {
             numberList.add(num % 10);
             num /= 10;
         }
         Collections.reverse(numberList);
         number = numberList;
     }
+
     public List<Integer> getNumber() {
         return number;
     }
 }
+
 class Operator {
     Computer computer;
     Player player;
     private int ballCount;
     private int strikeCount;
     private boolean correctAnswer;
+
     public Operator() {
         computer = new Computer();
         player = new Player();
@@ -62,33 +66,51 @@ class Operator {
         strikeCount = 0;
         correctAnswer = false;
     }
+
     public void playGame() {
+        showStartStatement();
         computer.setNumber();
-        while(!correctAnswer) {
+        while (!correctAnswer) {
             player.inputNumber();
             compareNumber();
             showResult();
         }
     }
+
     public void compareNumber() {
         int ball = 0;
         int strike = 0;
         List<Integer> computerNumber = computer.getNumber();
         List<Integer> playerNumber = player.getNumber();
-        for(int i = 0; i<computerNumber.size(); i++) {
+        for (int i = 0; i < computerNumber.size(); i++) {
             int index = playerNumber.indexOf(computerNumber.get(i));
-            if(index != -1) {
-                if(index == i) strike++;
-                else ball++;
+            if (index != -1) {
+                if (index == i) {
+                    strike++;
+                } else {
+                    ball++;
+                }
             }
         }
         ballCount = ball;
         strikeCount = strike;
-        if(strikeCount==3)
+        if (strikeCount == 3) {
             correctAnswer = true;
+        }
     }
+
     public void showResult() {
-
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.println("낫싱");
+        } else if (strikeCount == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
     }
 
+    public void showStartStatement() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
 }
