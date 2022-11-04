cc725c698d94c5e253f731a7df1e8c057f3eb767
wlsh44 <k87913j@gmail.com>
2022-11-04 11:18:07 +0900

    fix: 시작 메세지 상수 접근 제어자 수정

--- src/main/java/baseball/printer/StartMessagePrinter.java
+++ src/main/java/baseball/printer/StartMessagePrinter.java
@@ -2,7 +2,7 @@ package baseball.printer;
 
 public class StartMessagePrinter implements MessagePrinter {
 
-    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
     @Override
     public void printMessage() {
