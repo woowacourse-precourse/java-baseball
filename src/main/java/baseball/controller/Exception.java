13bb7f6d70e252eac692647614ed0cc11098968d
iamminho <minho100227@kakao.com>
2022-11-04 12:00:26 +0900

    refactor(exception): 예외 검사후 예외 발생 기능 해당 클레스로 이동

--- src/main/java/baseball/controller/Exception.java
+++ src/main/java/baseball/controller/Exception.java
@@ -6,41 +6,59 @@ import java.util.Set;
 
 public class Exception {
 	private static final int NUMBER_LENGTH = 3;
-	private static final int RESTART_NUMBER = 1;
-	private static final int END_NUMBER = 2;
 	private static final String ALLOW_ONLY_ONE_TO_NINE = "[ERROR] 1부터 9까지의 숫자만 입력해 주시기 바랍니다.";
 	private static final String ALLOW_ONLY_STANDARD_NUMBER_LENGTH = "[ERROR] 3자리의 숫자만 입력해 주시기 바랍니다.";
 	private static final String ALLOW_ONLY_OVERLAP_NUMBER = "[ERROR] 중복되지 않는 숫자만 입력해주시기 바랍니다.";
 
-	public static void validOnlyOneToNine(String inputGameNumber) {
+	public static void validatePlayerNumber(String inputPlayerNumber) {
+		try {
+			validateOnlyOneToNine(inputPlayerNumber);
+			validateNumberLength(inputPlayerNumber);
+			validateOverlapNumber(inputPlayerNumber);
+		} catch (IllegalArgumentException error) {
+			System.out.println(error);
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public static void validateOnlyOneToNine(String inputPlayerNumber) {
 		String regExp = "^[1-9]+$";
-		if (!inputGameNumber.matches(regExp)) {
+		if (!inputPlayerNumber.matches(regExp)) {
 			throw new IllegalArgumentException(ALLOW_ONLY_ONE_TO_NINE);
 		}
 	}
 
-	public static void validNumberLength(String inputGameNumber) {
-		if (inputGameNumber.length() != NUMBER_LENGTH) {
+	public static void validateNumberLength(String inputPlayerNumber) {
+		if (inputPlayerNumber.length() != NUMBER_LENGTH) {
 			throw new IllegalArgumentException(ALLOW_ONLY_STANDARD_NUMBER_LENGTH);
 		}
 	}
 
-	public static void validOverlapNumber(String inputGameNumber) {
-		String[] inputGameNumbersArray = inputGameNumber.split("");
-		Set<String> inputGameNumberSet = new HashSet<>();
-		inputGameNumbersToSet(inputGameNumberSet, inputGameNumbersArray);
+	public static void validateOverlapNumber(String inputPlayerNumber) {
+		String[] inputPlayerNumbers = inputPlayerNumber.split("");
+		Set<String> inputPlayerNumberSet = new HashSet<>();
+		inputPlayerNumbersToSet(inputPlayerNumberSet, inputPlayerNumbers);
 
-		if (inputGameNumberSet.size() != inputGameNumbersArray.length) {
+		if (inputPlayerNumberSet.size() != inputPlayerNumbers.length) {
 			throw new IllegalArgumentException(ALLOW_ONLY_OVERLAP_NUMBER);
 		}
 	}
 
-	private static void inputGameNumbersToSet(Set<String> inputGameNumberSet, String[] inputGameNumbersList) {
-		Arrays.stream(inputGameNumbersList)
-			.forEach(number -> inputGameNumberSet.add(number));
+	private static void inputPlayerNumbersToSet(Set<String> inputPlayerNumberSet, String[] inputPlayerNumber) {
+		Arrays.stream(inputPlayerNumber)
+			.forEach(number -> inputPlayerNumberSet.add(number));
 	}
 
-	public static void validRestartOrExitNumber(String inputRestartNumber, int restartNumber, int exitNumber) {
+	public static void validateRestartNumber(String inputRestartNumber, int restartNumber, int exitNumber) {
+		try {
+			Exception.validateRestartOrExitNumber(inputRestartNumber, restartNumber, exitNumber);
+		} catch (IllegalArgumentException error) {
+			System.out.println(error);
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public static void validateRestartOrExitNumber(String inputRestartNumber, int restartNumber, int exitNumber) {
 		String stringRestartNumber = Integer.toString(restartNumber);
 		String stringExitNumber = Integer.toString(exitNumber);
 		if (!inputRestartNumber.equals(stringRestartNumber) && !inputRestartNumber.equals(stringExitNumber)) {
