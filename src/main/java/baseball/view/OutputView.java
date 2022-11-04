9c92f88e68281cd2ece57782379e943c830270f0
TaeHwan <kimta2hwan@gmail.com>
2022-11-04 17:59:59 +0900

    feat(outputview): 오답일 때 낫싱을 출력하는 기능 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,6 +4,7 @@ public class OutputView {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String ANSWER_INPUT_GUIDE = "숫자를 입력해주세요 : ";
+    private static final String NOTHING = "낫싱";
     private static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART_OR_STOP_GUIDE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
@@ -13,6 +14,10 @@ public class OutputView {
 
     public static void printAnswerInputGuideMessage() {
         System.out.print(ANSWER_INPUT_GUIDE);
+    }
+
+    public static void printNothingMessage() {
+        System.out.println(NOTHING);
     }
 
     public static void printGameEndMessage() {
