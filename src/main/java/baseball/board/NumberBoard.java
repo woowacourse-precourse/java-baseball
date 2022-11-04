3fbfe9c739fc5279f1c12b19e89481770490fb10
hoonn9 <hun9935@gmail.com>
2022-11-04 22:58:43 +0900

    refactor(NumberBoard): 숫자 비교 메소드가 해시맵을 반환하도록 수정

--- src/main/java/baseball/board/NumberBoard.java
+++ src/main/java/baseball/board/NumberBoard.java
@@ -3,14 +3,15 @@ package baseball.board;
 import baseball.compare.ComparisonResult;
 import baseball.compare.ComparisonResultType;
 import baseball.compare.Referee;
+import java.util.HashMap;
 import java.util.List;
 
 public class NumberBoard {
 
     private final ComparisonResult comparisonResult = new ComparisonResult();
-    ;
 
-    public boolean compare(List<Integer> userNumbers, List<Integer> opponentNumbers) {
+    public HashMap<ComparisonResultType, Integer> compareNumbers(List<Integer> userNumbers,
+            List<Integer> opponentNumbers) {
         for (int i = 0; i < userNumbers.size(); i++) {
             Integer userNumber = userNumbers.get(i);
             Integer opponentNumber = opponentNumbers.get(i);
@@ -18,11 +19,10 @@ public class NumberBoard {
             compareNumber(userNumber, opponentNumber);
         }
 
-        return comparisonResult.getFrequency().get(ComparisonResultType.STRIKE).equals(opponentNumbers.size());
+        return comparisonResult.getFrequency();
     }
 
     private void compareNumber(Integer userNumber, Integer opponentNumber) {
         this.comparisonResult.add(Referee.judge(userNumber, opponentNumber));
     }
-
 }
