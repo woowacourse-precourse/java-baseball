d8b32b39f18db24dbeb4912ecfeccdafcc39e4a9
ywj9811 <ywj9811@naver.com>
2022-11-04 14:33:39 +0900

    style: RuleService 클래스 가독성 고려

--- src/main/java/baseball/service/RuleService.java
+++ src/main/java/baseball/service/RuleService.java
@@ -5,20 +5,24 @@ import java.util.List;
 public class RuleService {
     public int Strike(List<Integer> RandomNumber, List<Integer> InputNumber) {
         int cnt = 0;
+
         for (int i = 0; i < RandomNumber.size(); i++) {
             if (RandomNumber.get(i) == InputNumber.get(i)) {
                 cnt++;
             }
         }
+
         return cnt;
     }
 
     public int Ball(List<Integer> RandomNumber, List<Integer> InputNumber) {
         int cnt = 0;
+
         for (int in = 0; in < InputNumber.size(); in++) {
             if (RandomNumber.contains(InputNumber.get(in)))
                 cnt++;
         }
+
         return cnt - Strike(RandomNumber, InputNumber);
     }
 
