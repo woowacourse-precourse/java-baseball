1a0aa6f371c6d4fe2b2199e62e71be3e76fdf311
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-04 21:41:38 +0900

    test(Computer): Computer의 리팩토링에 따른 테스트 코드 재작성.

--- src/test/java/baseball/players/ComputerTest.java
+++ src/test/java/baseball/players/ComputerTest.java
@@ -2,8 +2,6 @@ package baseball.players;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.*;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ComputerTest {
@@ -11,20 +9,21 @@ class ComputerTest {
     @Test
     void 컴퓨터의_숫자_생성을_확인하는_기능() {
         Computer computer = new Computer();
-        List<Integer> computerNumbers = computer.getComputerNumbers();
-        assertThat(computerNumbers.size()).isEqualTo(3);
+        String computerNumbers = computer.getComputerNumbers();
+        assertThat(computerNumbers.length()).isEqualTo(3);
     }
 
     @Test
     void 컴퓨터의_중복된_숫자_생성하는지_확인하는_기능() {
         Computer computer = new Computer();
-        List<Integer> computerNumbers = computer.getComputerNumbers();
+        String computerNumbers = computer.getComputerNumbers();
+        String tmp = "";
 
-        Map<Integer, Integer> dupCheck = new HashMap<>();
-
-        for (int numbers : computerNumbers)
-            dupCheck.put(numbers, 0);
-
-        assertThat(dupCheck.size()).isEqualTo(3);
+        for (char x : computerNumbers.toCharArray()) {
+            if(!tmp.contains(String.valueOf(x)))
+                tmp += String.valueOf(x);
+        }
+        assertThat(tmp.length()).isEqualTo(3);
     }
+
 }
