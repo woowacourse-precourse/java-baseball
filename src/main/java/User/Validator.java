7a46e6d268ac626d6df2973dc76a17f01311602a
Devtist <yt930310@snu.ac.kr>
2022-11-04 23:58:25 +0900

    feat: 유효하지 않은 길이의 입력에 대한 예외 처리 기능 구현

--- src/main/java/User/Validator.java
+++ src/main/java/User/Validator.java
@@ -8,6 +8,7 @@ public class Validator {
 	public static void validateNumberInput(String numberInput) {
 		existDuplicateNumber(numberInput);
 		existInvalidLetter(numberInput);
+		haveInvalidLength(numberInput);
 	}
 
 	private static void existDuplicateNumber(String numberInput) throws IllegalArgumentException {
@@ -19,6 +20,12 @@ public class Validator {
 	private static void existInvalidLetter(String numberInput) throws IllegalArgumentException {
 		if (Arrays.stream(numberInput.split(""))
 			.anyMatch(letter -> !VALID_NUMBERS.contains(Integer.parseInt(letter)))) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private static void haveInvalidLength(String numberInput) throws IllegalArgumentException {
+		if (numberInput.length() != LENGTH_OF_NUMBER) {
 			throw new IllegalArgumentException();
 		}
 	}
