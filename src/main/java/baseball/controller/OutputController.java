fc26df4e78b95744a02da3772941d61764c78a52
egoing <dudqk9696@naver.com>
2022-11-04 10:19:02 +0900

    feat : strike 숫자 반환

---
+++ src/main/java/baseball/controller/OutputController.java
@@ -0,0 +1,16 @@
+package baseball.controller;
+
+import baseball.domain.ComNumber;
+import baseball.domain.UserNumber;
+
+public class OutputController {
+    public int getStrike(ComNumber comNumber, UserNumber userNumber) {
+        int strike = 0;
+        for(int i = 0; i< InputController.NUMBER_SIZE; i++) {
+            int comnum = comNumber.getComnumber().get(i);
+            int usernum = userNumber.getUserNumber().get(i);
+            if(comnum == usernum) {strike++;}
+        }
+        return strike;
+    }
+}
