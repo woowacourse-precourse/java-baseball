11cad68445830f957f9b7bb56b4cc5c4b77bdf7b
chopinballadeno4 <qo98333@naver.com>
2022-11-04 17:42:28 +0900

    feat: Compose AppView input and output

--- src/main/java/baseball/view/AppView.java
+++ src/main/java/baseball/view/AppView.java
@@ -3,5 +3,24 @@ package baseball.view;
 import java.util.Scanner;
 
 public class AppView {
-    Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void output (String Message) {
+        System.out.println(Message);
+    }
+
+    public static void outputLine(String Message) {
+        System.out.println(Message);
+    }
+
+    public static String inputLine() {
+        String line = AppView.scanner.nextLine().trim();
+
+        while(line.equals("")) {
+            line = AppView.scanner.nextLine().trim();
+        }
+
+        return line;
+    }
+
 }
