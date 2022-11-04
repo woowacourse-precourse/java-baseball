13fac79be43ec921a0518d237dc7ba8d4821b24d
Donguk <95donguk.dev@gmail.com>
2022-11-04 21:22:25 +0900

    feat: add game start text output fuction
    add a text output fuction that says "숫자 야구 게임을 시작합니다." at start of game

---
+++ src/main/java/baseball/GameTextPrinter.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class GameTextPrinter {
+    private static final String GAME_START_TEXT = "숫자 야구 게임을 시작합니다.";
+
+    public static void printGameStartText() {
+        System.out.println(GAME_START_TEXT);
+    }
+}
