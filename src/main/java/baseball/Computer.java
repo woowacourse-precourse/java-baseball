893aca36410921d6d1abd09280a85b7b1f800b16
bnbnac <bnbnac@naver.com>
2022-11-04 13:38:11 +0900

    feat: 컴퓨터 객체가 임의의 수 3개를 선택하여 저장하는 기능 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,16 +1,33 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
-    private String correctAnswer;
+    private List<Integer> computerAnswer;
 
     Computer() {
-        this.correctAnswer = getRandomAnswer();
+        this.computerAnswer = setComputerAnswer();
     }
 
-    public String getRandomAnswer() {
-        //TODO
-        return "";
+    public List<Integer> setComputerAnswer() {
+        List<Integer> computerAnswer = new ArrayList<>();
+        while (computerAnswer.size() < 3) {
+            addNewDigit(computerAnswer);
+        }
+        return computerAnswer;
     }
+
+    public List<Integer> addNewDigit(List<Integer> computerAnswer) {
+        Integer newDigit = Randoms.pickNumberInRange(1, 9);
+        if (!computerAnswer.contains(newDigit)) {
+            computerAnswer.add(newDigit);
+        }
+        return computerAnswer;
+    }
+    
 
     public String getHint (String playerAnswer) {
         //TODO
