ce415ae7cd2831f2f4b336eda2d166c92efc05ab
GwansuShin <happysun7080@naver.com>
2022-11-04 15:29:48 +0900

    refactor(UserNumber): 검증 로직 삭제
    유저 입력에 대한 검증은 View 레이어에서 수행되도록 기존 검증 로직 삭제

--- src/main/java/baseball/model/number/UserNumber.java
+++ src/main/java/baseball/model/number/UserNumber.java
@@ -1,35 +1,9 @@
 package baseball.model.number;
 
-import baseball.model.Validation;
-
-import static baseball.model.Constant.*;
 import static baseball.model.Utilization.convertStringToIntegerList;
-import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public abstract class UserNumber {
-    public static NumberDto generate() {
-        String userInput = readLine();
-        if (!UserNumberValidation.validate(userInput)) {
-            throw new IllegalArgumentException();
-        }
+    public static NumberDto generate(String userInput) {
         return new NumberDto(convertStringToIntegerList(userInput));
-    }
-
-    abstract static class UserNumberValidation extends Validation {
-        public static boolean validate(String userInput) {
-            return validateNotNull(userInput)
-                    && isInteger(userInput)
-                    && validateStringLength(userInput, NUMBER_LENGTH)
-                    && validateEachUserNumberInRange(userInput)
-                    && validateNumberNotRepeated(convertStringToIntegerList(userInput), NUMBER_LENGTH);
-        }
-
-        private static boolean validateEachUserNumberInRange(String userInput) {
-            for (int number : convertStringToIntegerList(userInput)) {
-                if (!validateIntegerRange(number, MIN_NUMBER_VALUE, MAX_NUMBER_VALUE))
-                    return false;
-            }
-            return true;
-        }
     }
 }
