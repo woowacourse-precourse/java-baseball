019866b8219b88d4c3ddd6b5554dd4be7c09615f
ykmxxi <ykmxxi97@gmail.com>
2022-11-04 16:30:54 +0900

    test: 4.힌트를 생성하는 기능 테스트

---
+++ src/test/java/baseball/hint/HintTest.java
@@ -0,0 +1,70 @@
+package baseball.hint;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import baseball.gamenumber.ComputerNumber;
+import baseball.gamenumber.PlayerNumber;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HintTest {
+    Hint hint;
+    ComputerNumber computerNumber;
+    PlayerNumber playerNumber;
+
+    @BeforeEach
+    void beforeEach() {
+        hint = new Hint();
+        computerNumber = new ComputerNumber();
+        playerNumber = new PlayerNumber();
+    }
+
+    @Test
+    void 볼_확인() {
+        // given
+        List<Integer> computerNumberList = new ArrayList<>(List.of(1, 2, 3));
+        playerNumber = new PlayerNumber("312");
+        List<Integer> playerNumberList = playerNumber.getNumberList();
+
+        // when
+       hint.calculateBall(computerNumberList, playerNumberList);
+       int ball = hint.getBall();
+
+       // then
+        assertThat(ball).isEqualTo(3);
+    }
+
+    @Test
+    void 스트라이크_확인() {
+        // given
+        List<Integer> computerNumberList = new ArrayList<>(List.of(1, 2, 3));
+        playerNumber = new PlayerNumber("931");
+        List<Integer> playerNumberList = playerNumber.getNumberList();
+
+        // when
+        hint.calculateStrike(computerNumberList, playerNumberList);
+        int strike = hint.getStrike();
+
+        // then
+        assertThat(strike).isEqualTo(0);
+    }
+
+    @Test
+    void 낫싱_확인() {
+        // given
+        List<Integer> computerNumberList = new ArrayList<>(List.of(1, 2, 3));
+        playerNumber = new PlayerNumber("456");
+        List<Integer> playerNumberList = playerNumber.getNumberList();
+
+        // when
+        hint.isNothing(computerNumberList, playerNumberList);
+        String nothing = hint.getNothing();
+
+        // then
+        assertThat(nothing).isEqualTo("낫싱");
+    }
+
+
+}
