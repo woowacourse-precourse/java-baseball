b692ec27d5ac99f64749a39ba97bb80b06838f23
TaeHwan <kimta2hwan@gmail.com>
2022-11-04 18:02:09 +0900

    refactor(outputview): 출력 메시지들의 이름을 더 명확하게 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,28 +3,28 @@ package baseball.view;
 public class OutputView {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final String ANSWER_INPUT_GUIDE = "숫자를 입력해주세요 : ";
-    private static final String NOTHING = "낫싱";
-    private static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private static final String RESTART_OR_STOP_GUIDE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String ANSWER_INPUT_GUIDE_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_OR_STOP_GUIDE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
     }
 
     public static void printAnswerInputGuideMessage() {
-        System.out.print(ANSWER_INPUT_GUIDE);
+        System.out.print(ANSWER_INPUT_GUIDE_MESSAGE);
     }
 
     public static void printNothingMessage() {
-        System.out.println(NOTHING);
+        System.out.println(NOTHING_MESSAGE);
     }
 
     public static void printGameEndMessage() {
-        System.out.println(GAME_END);
+        System.out.println(GAME_END_MESSAGE);
     }
 
     public static void printRestartOrStopGuideMessage() {
-        System.out.println(RESTART_OR_STOP_GUIDE);
+        System.out.println(RESTART_OR_STOP_GUIDE_MESSAGE);
     }
 }
