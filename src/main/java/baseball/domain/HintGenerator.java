70a8a8c30c535c87051096fecb67859215281fb6
JUHA <khabh@naver.com>
2022-11-04 17:39:25 +0900

    feat(HintGenerator): 스크라이크와 볼의 개수 구해서 힌트 생성

--- src/main/java/baseball/domain/HintGenerator.java
+++ src/main/java/baseball/domain/HintGenerator.java
@@ -1,26 +1,40 @@
 package baseball.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class HintGenerator {
+    private final static int STRIKE = 0;
+    private final static int BALL = 1;
     private final ArrayList<Integer> strikeNumbers;
 
     public HintGenerator(ArrayList<Integer> strikeNumbers) {
         this.strikeNumbers = strikeNumbers;
     }
 
+    public HashMap<Integer, Integer> generateHint(ArrayList<Integer> playerGuess) {
+        HashMap<Integer, Integer> hint = new HashMap<>();
+        int NumberOfStrike = countStrike(playerGuess);
+        int NumberOfBall = countStrikeOrBall(playerGuess) - NumberOfStrike;
+
+        hint.put(STRIKE, NumberOfStrike);
+        hint.put(BALL, NumberOfBall);
+
+        return hint;
+    }
+
     private int countStrike(ArrayList<Integer> playerGuess) {
-        int numberOfStrikes = 0;
+        int numberOfStrike = 0;
         int currentIndex = 0;
 
         for (int strikeNumber : strikeNumbers) {
             if (strikeNumber == playerGuess.get(currentIndex)) {
-                numberOfStrikes++;
+                numberOfStrike++;
             }
             currentIndex++;
         }
 
-        return numberOfStrikes;
+        return numberOfStrike;
     }
 
     private int countStrikeOrBall(ArrayList<Integer> playerGuess) {
