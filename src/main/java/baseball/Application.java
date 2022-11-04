0c02e0e7ef94cb7842f968ab9504af63cad4898d
recicoco068 <gkapf838@gmail.com>
2022-11-04 23:03:02 +0900

    feat(Application.java) : 숫자 야구 게임 실행하는 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean exit = false;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (!exit) {
+            boolean collect = false;
+            baseballGame(collect);
+            exit = UserInterface.exitOrRestart();
+        }
+    }
+
+    private static void baseballGame(boolean collect) {
+        List<Integer> computer = Baseball.getComputerNumber();
+        while (!collect) {
+            List<Integer> user = UserInterface.getUsersAnswer();
+            int strikeCount = Baseball.getStrikeCount(computer, user);
+            int ballCount = Baseball.getBallCount(computer, user) - strikeCount;
+            collect = UserInterface.resultOutput(strikeCount, ballCount);
+        }
     }
 }
