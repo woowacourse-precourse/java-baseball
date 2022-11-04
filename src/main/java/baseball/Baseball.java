d560f1a23173a35a8a6f82ca83bc17024ae059bb
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-04 11:41:25 +0900

    refactor(Baseball, BaseballTest): make3DigitNumber 메서드 및 테스트 코드 삭제
    주요 변경 사항:
    - make3DigitNumber 메서드가 Pitcher 객체로 옮겨짐으로 인한 메서드 및 테스트 코드 삭제

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,28 +1,5 @@
 package baseball;
 
-import static camp.nextstep.edu.missionutils.Randoms.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Baseball {
-	private static final int PLAYING_NUMBER_SIZE = 3;
-	private static final int MIN_NUMBER = 1;
-	private static final int MAX_NUMBER = 9;
-
-	static String make3DigitNumber() {
-		StringBuilder stringBuilder = new StringBuilder();
-		List<Integer> prev = new ArrayList<>();
-
-		while (stringBuilder.length() != PLAYING_NUMBER_SIZE) {
-			int newNumber = pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
-
-			if (!prev.contains(newNumber)) {
-				stringBuilder.append(newNumber);
-				prev.add(newNumber);
-			}
-		}
-		return stringBuilder.toString();
-	}
 
 }
--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -1,34 +1,5 @@
 package baseball;
 
-import static org.assertj.core.api.Assertions.*;
-
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
-
 class BaseballTest {
-
-	@Nested
-	class make3DigitNumberTest {
-		private static final int PLAYING_NUMBER_SIZE = 3;
-
-		@Test
-		void case1() {
-			for (int i = 0; i < 100000; i++) {
-				String threeDigitNumber = Baseball.make3DigitNumber();
-				List<Integer> threeNumbers = Baseball.splitDigits(threeDigitNumber);
-				Set<Integer> nonDuplicateNumbers = new HashSet<>(threeNumbers);
-
-				assertThat(threeNumbers.size()).isEqualTo(PLAYING_NUMBER_SIZE);
-				assertThat(nonDuplicateNumbers.size()).isEqualTo(PLAYING_NUMBER_SIZE);
-				for (int number : threeNumbers) {
-					assertThat(number).isBetween(1, 9);
-				}
-			}
-		}
-	}
 
 }
