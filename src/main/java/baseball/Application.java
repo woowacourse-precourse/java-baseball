13a82ad9f3c53190e014167cd6ffba585dac2336
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-04 16:51:19 +0900

    feat(Input) : 사용자로부터 숫자 입력받는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+
+import baseball.exception.NumberExceptionUtils;
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String playerNum = getPlayerInputNum();
+    }
+
+    public static String getPlayerInputNum() {
+        String input = Console.readLine();
+        checkValidInput(input);
+        return input;
+    }
+
+    private static void checkValidInput(String input) {
+        NumberExceptionUtils.isPositiveDigits(input);
+        NumberExceptionUtils.isThreeDigits(input);
+        NumberExceptionUtils.isNotDuplicateNumber(input);
     }
 }
