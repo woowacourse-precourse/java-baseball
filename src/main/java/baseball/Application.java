91e5a9bcb6954123634ff5e55d8f2cc383377cd5
yumyeonghan <mgtmh991013@gmail.com>
2022-11-05 00:45:20 +0900

    feat: 스트라이크 요구사항 기능
    strike(), 컴퓨터와 플레이어의 서로 다른 3개의 수를 비교하여 같은 자리에 같은 숫자가 있는경우 스트라이크 횟수를 +1 시키고, 자리의 인덱스 값을 strikeIndex 컬렉션에 넣어줌

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,5 +15,21 @@ public class Application {
         Player player = new Player(new ArrayList<>());
         player.player3NumberInput();
 
+        List ballIndex = new ArrayList<Integer>();
+        int ballCount = 0;
+        List strikeIndex = new ArrayList<Integer>();
+        int strikeCount = 0;
+
+        strike(computer, player, strikeIndex, strikeCount);
+
+    }
+
+    private static void strike(Computer computer, Player player, List strikeIndex, int strikeCount) {
+        for(int numericComparisonIndex = 0; numericComparisonIndex < 3; numericComparisonIndex++) {
+            if(computer.computerNumber.get(numericComparisonIndex) == player.playerNumber.get(numericComparisonIndex)) {
+                strikeCount++;
+                strikeIndex.add(numericComparisonIndex);
+            }
+        }
     }
 }
