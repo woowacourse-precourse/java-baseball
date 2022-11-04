dd726d541e8b2692acf7deee85022bedf4bce4a5
youngkyunIm <yee0230@naver.com>
2022-11-04 18:05:35 +0900

    feat : loop until insert coin

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,23 +8,40 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
+    public static String COIN = "1";
+    public static List<Integer> computer;
+
     public static void main(String[] args) {
-        List<Integer> computer = getComputerValue();
-        List<Integer> user = new Input(readLine()).getInputList();
+        computer = getComputerValue();
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
-        int ball = countBall(computer, user);
-        int strike = countStrike(computer, user);
+        while (COIN.equals("1")) {
+            System.out.print("숫자를 입력해주세요 : ");
+            List<Integer> user = new Input(readLine()).getInputList();
 
-        game(ball, strike);
+            int ball = countBall(computer, user);
+            int strike = countStrike(computer, user);
+            game(ball, strike);
+        }
     }
 
     private static void game(int ball, int strike) {
         if (ball != 0) System.out.print(ball + "볼 ");
-        if (strike != 0) System.out.println(strike + "스트라이크");
+        if (strike != 0) System.out.print(strike + "스트라이크");
+        System.out.println();
         if (ball == 0 && strike == 0) System.out.println("낫싱");
         if (strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            inputCoin();
         }
+    }
+
+    private static void inputCoin() {
+        Coin coin;
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        coin = new Coin(readLine());
+        COIN = coin.getCoin();
+        if (COIN.equals("1")) computer = getComputerValue();
     }
 
     private static int countStrike(List<Integer> computer, List<Integer> user) {
