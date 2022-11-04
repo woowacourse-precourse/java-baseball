54c6f4532d042aa326390626dbdc8a9f20d7267b
ksh-beginner <ohk9134@naver.com>
2022-11-04 16:22:38 +0900

    test(InputViewTest) : 검증부 분리 리팩토링

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -16,6 +16,7 @@ public class InputViewTest {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         String playerInputNumber = InputView.inputNumber();
+
         assertThat(playerInputNumber).isEqualTo(input);
     }
 
@@ -24,6 +25,7 @@ public class InputViewTest {
     void 사용자가_입력한_수_입력_받기_3자리가_아니면_예외_처리(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
+
         assertThatThrownBy(InputView::inputNumber)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] : 입력하는 수는 3자리여야합니다.");
@@ -34,6 +36,7 @@ public class InputViewTest {
     void 사용자가_입력한_수_입력_받기_숫자가_아니면_예외_처리(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
+
         assertThatThrownBy(InputView::inputNumber)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] : 입력하는 수는 숫자여야합니다.");
@@ -44,6 +47,7 @@ public class InputViewTest {
     void 사용자가_입력한_수_입력_받기_0이_포함될_경우_예외_처리(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
+
         assertThatThrownBy(InputView::inputNumber)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] : 입력하는 수는 0이 포함될 수 없습니다. 1~9까지의 수를 입력해주세요.");
@@ -54,6 +58,7 @@ public class InputViewTest {
     void 사용자가_입력한_수_입력_받기_서로_다른_수가_아니면_예외_처리(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
+
         assertThatThrownBy(InputView::inputNumber)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] : 입력하는 수는 서로 다른 수여야합니다.");
@@ -65,6 +70,7 @@ public class InputViewTest {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         String playerInputNumber = InputView.inputRestartOrEndNumber();
+
         assertThat(playerInputNumber).isEqualTo(input);
     }
 
@@ -73,6 +79,7 @@ public class InputViewTest {
     void 게임_끝난_후에_재시작_종료_수가_1이나_2가_아니면_예외_처리(String input) {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
+        
         assertThatThrownBy(InputView::inputRestartOrEndNumber)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("[ERROR] : 재시작 또는 종료 Number는 1, 2입니다. 그 외는 입력할 수 없습니다.");
