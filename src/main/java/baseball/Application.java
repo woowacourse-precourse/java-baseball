07b3d37abcb06da4a271c6dc4bcd3fdfd06192ae
Donghyun <flaehan@gmail.com>
2022-11-04 13:18:26 +0900

    type : feat
    body : 컴퓨터가 3개의 랜덤 수를 생성하는 메서드를 생성, 중복되는 코드들이 있는 듯 하여서 코드를 완성하면 오류가 날 수 있을 것 같은 걱정이 된다.
    
    footer :

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,39 @@
 package baseball;
 
+import java.util.ArrayList;
+import static camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange;
+
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
 
+    }
+
+
+    // 컴퓨터가 3개의 랜덤 수 생성
+    public static ArrayList<Integer> computerRandomNum(int min, int max, int count){
+        ArrayList <Integer> comNum = new ArrayList<>();
+        pickUniqueNumbersInRange(1, 9, 3);
+        return comNum;
+    }
+
+    // 사용자가 3개의 수 입력
+    public static userInputNum(){
+
+    }
+
+    // 사용자 입력 정답 검사
+    public static checkAnswer{
+
+    }
+
+    // 정답 검사 결과 출력
+    public static printAnswer{
+
+    }
+
+    // 종료 조건 검사
+    public static checkFinish{
 
     }
 }
