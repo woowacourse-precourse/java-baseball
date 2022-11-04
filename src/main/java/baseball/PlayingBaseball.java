a21098bb346a1c4294ae6e2da4bf3fc091bbae29
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-04 10:06:17 +0900

    <feat>PlayingBaseball : 컴퓨터의 수와 사용자의 수를 비교하여 스크라이크 개수 저장

--- src/main/java/baseball/PlayingBaseball.java
+++ src/main/java/baseball/PlayingBaseball.java
@@ -6,13 +6,24 @@ import java.util.List;
 class PlayingBaseball {
     private final String numberOfComputer;
     private final String numberOfPlayer;
+    private final int LENGTH = 3;
     private int strike = 0;
     private int ball = 0;
-    private List indexOfStrike = new ArrayList<String>();
+    private List indexOfStrike = new ArrayList<Integer>();
 
     PlayingBaseball (String numberComputer, String numberPlayer) {
         this.numberOfComputer = numberComputer;
         this.numberOfPlayer = numberPlayer;
+    }
+
+    private int countStrikes () {
+        for (int i = 0; i < LENGTH; i++) {
+            if (numberOfComputer.charAt(i) == numberOfComputer.charAt(i)) {
+                indexOfStrike.add(i);
+                strike += 1;
+            }
+        }
+        return strike;
     }
 
 
