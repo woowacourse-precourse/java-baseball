c3233c63f97faad901e18c24360333d722313ad9
miffyKing <whqudghk28@gmail.com>
2022-11-04 21:32:40 +0900

    feat: make strike, ball, nothing count method in Referee

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -2,4 +2,45 @@ package baseball;
 
 public class Referee {
 
+  public static String Answer;
+
+  public static String calculateAnswer(String userInput, String randNum) {
+    int ball;
+    int strike;
+
+    ball = calculateBall(userInput, randNum);
+    strike = calculateStrike(userInput, randNum);
+    if (calculateStrike(userInput, randNum) == 0 && calculateBall(userInput, randNum) == 0) {
+      return "낫싱\n";
+    }
+    if (ball != 0 && strike != 0) {
+      return (ball + "볼 " + strike + "스트라이크\n");
+    } else if (ball != 0 && strike == 0) {
+      return (ball + "볼\n");
+    } else if (ball == 0 && strike != 0) {
+      return (strike + "스트라이크\n");
+    }
+    return "";
+  }
+
+  private static int calculateBall(String userInput, String randNum) {
+    int strikeNum = calculateStrike(userInput, randNum);
+    int ballNum = 0;
+    for (int i = 0; i < 3; i++) {
+      if (randNum.indexOf(userInput.toString()) != 0) {
+        ballNum++;
+      }
+    }
+    return (ballNum - strikeNum);
+  }
+
+  private static int calculateStrike(String userInput, String randNum) {
+    int strikeCnt = 0;
+    for (int i = 0; i < 3; i++) {
+      if (userInput.charAt(i) == randNum.charAt(i)) {
+        strikeCnt++;
+      }
+    }
+    return strikeCnt;
+  }
 }
