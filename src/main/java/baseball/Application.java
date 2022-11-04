44ff50acc4953d48a9db7f0d549187dfa8d8ce0a
ghltjd369 <ghltjd369@gmail.com>
2022-11-04 21:10:53 +0900

    feat : 컴퓨터의 수 생성하는 기능 추가 - makeComputerRandomNumber : 길이가 3이 될 때까지 pickRandomNumber 수행 - pickRandomNumber : 1~9 사이의 랜덤한 수를 골라 리스트에 있는지 확인 후 없으면 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,39 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            List<Integer> computerNumber = makeComputerRandomNumber();
+
+        }
+
+
+    }
+
+    public static List<Integer> makeComputerRandomNumber() {
+        List<Integer> numArr = new ArrayList<Integer>();
+
+        while (numArr.size() < 3) {
+            pickRandomNumber(numArr);
+        }
+
+        return numArr;
+    }
+
+    public static List<Integer> pickRandomNumber(List<Integer> numArr) {
+        int number = Randoms.pickNumberInRange(1, 9);
+
+        if (!numArr.contains(number)) {
+            numArr.add(number);
+        }
+
+        return numArr;
     }
 }
