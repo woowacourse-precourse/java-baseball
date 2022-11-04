1f461258e49b5aef4c3ad113a44e0d4f0f1ca78d
jingyu9804 <swc05161@gmail.com>
2022-11-05 01:28:03 +0900

    refactor : gameEndingMessage 내용 수정

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class Constant {
     public static final String gameStartMessage="숫자 야구 게임을 시작합니다.";
-    public static final String gameEndingMessage="개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String gameEndingMessage="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String gameRestartMessage="게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static final String inputNumberMessage="숫자를 입력해주세요.";
