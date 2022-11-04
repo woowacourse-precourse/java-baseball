6a50a4b3a859ade36d8da3f8b8533d8924230880
csj <koreaxman@naver.com>
2022-11-04 14:07:21 +0900

    feat: split input number to string list

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -2,13 +2,24 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Player {
-    private InputNumber inputNumber;
+    static InputNumber inputNumber;
 
     public String inputNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         inputNumber = new InputNumber(Console.readLine());
 
         return inputNumber.number;
+    }
+
+    public List<String> splitInputNumberToList() {
+        String[] strArr = inputNumber.number.split("");
+        List<String> inputNumberList = new ArrayList<>(Arrays.asList(strArr));
+
+        return inputNumberList;
     }
 }
