10317700b39fd0644a965f0bc29e7d94978d9116
situodys <situodys@gmail.com>
2022-11-04 16:59:07 +0900

    feat(Player): 게임 재시작에 대한 입력값 검증 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,6 +1,9 @@
 package baseball;
 
 public class Player {
+    private static final String WANT_RESTART="1";
+    private static final String WANT_EXIT="2";
+
     private Baseballs input;
 
     public Player() {
@@ -8,5 +11,17 @@ public class Player {
 
     public void initMyAnswerBy(String playerInput) {
         this.input = Baseballs.of(playerInput);
+    }
+
+    public boolean wantRestart(String playerInput) {
+        validate(playerInput);
+        return true;
+    }
+
+    private void validate(String playerInput) {
+        if (playerInput.equals(WANT_EXIT) || playerInput.equals(WANT_RESTART)) {
+            return;
+        }
+        throw new IllegalArgumentException();
     }
 }
