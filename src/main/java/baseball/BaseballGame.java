d7b2a3add6c79ed4865c546f1c354f9e5139ba78
BuildTools <unconfigured@null.spigotmc.org>
2022-11-04 18:09:35 +0900

    feat: 추가 야구게임클래스 및 게임시작함수
    게임별로 관리할 수 있게 야구게임클래스 생성 및 기본구조 함수생성

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,21 @@
+package baseball;
+
+public class BaseballGame {
+
+    public void play(){
+
+        View.printStartGame();
+        do{
+            playGame();
+        }while(endGame());
+
+    }
+
+    public void playGame(){
+
+    }
+
+    public boolean endGame(){
+
+    }
+}
