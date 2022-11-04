d2fee27e35f9c584254452c72b92ec437488e170
Laeng <Laeng@users.noreply.github.com>
2022-11-04 21:53:24 +0900

    feat(Applcation): main 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,36 @@
 package baseball;
 
+import baseball.utils.ConsoleUtil;
+import baseball.utils.GameUtil;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class Application {
+    private static final int PICK_NUMBER_LENGTH = 3;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Queue<Integer> computeNumbers = new LinkedList<>(List.of(135, 589));
+        boolean isExecute = true;
+
+        ConsoleUtil.printIntro();
+        ConsoleUtil.printHowToPlay();
+
+        while (isExecute) {
+            computeNumbers.add(GameUtil.pickNumber(PICK_NUMBER_LENGTH));
+            isExecute = executeGame(computeNumbers.poll());
+        }
+
+        ConsoleUtil.printQuit();
+    }
+
+    private static boolean executeGame(int computerNumber) {
+        Game game = new Game(computerNumber);
+        game.run(PICK_NUMBER_LENGTH);
+
+        ConsoleUtil.printFinish(PICK_NUMBER_LENGTH);
+
+        String newGameFlag = ConsoleUtil.printGameRestartQuestion();
+        return GameUtil.parseInt(newGameFlag) == 1;
     }
 }
