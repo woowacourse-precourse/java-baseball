d61f5a971a85f1ee377d3491504f17a96f939c00
marinesnow34 <m4rinesnow34@gmail.com>
2022-11-04 10:50:45 +0900

    feat: ball 판정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,9 +4,12 @@ public class Game {
     private final int CNT_NUMBER = 3;
     private final int STRIKE = 1;
     private final int NOT_STRIKE = 0;
+    private final int BALL = 1;
+    private final int NOT_BALL = 0;
     private String computer;
     private String player;
     private int strike;
+    private int ball;
 
     public Game() {
     }
@@ -22,6 +25,7 @@ public class Game {
         player = playerNumber.getNumber();
 
         strike = countStrike();
+        ball = countBall();
     }
 
     private int countStrike() {
@@ -37,5 +41,20 @@ public class Game {
             return STRIKE;
         }
         return NOT_STRIKE;
+    }
+
+    private int countBall() {
+        int count = 0;
+        for (int i = 0; i < CNT_NUMBER; i++) {
+            count += isBall(player.charAt(i));
+        }
+        return count - strike;
+    }
+
+    private int isBall(Character playerChar) {
+        if (computer.indexOf(playerChar) == -1) {
+            return NOT_BALL;
+        }
+        return BALL;
     }
 }
