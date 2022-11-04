d9e7b8c5c4445abd98b3a2ca99ffdea4b5d6c14b
Cheon-DM <76736548+Cheon-DM@users.noreply.github.com>
2022-11-04 17:03:43 +0900

    feat: add print count msg function

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,6 +7,11 @@ public class Game {
     static final int SIZE = 3;
     static final int START_INCLUSIVE = 1;
     static final int END_INCLUSIVE = 9;
+    static final String BALL_MSG = "볼";
+    static final String STRIKE_MSG = "스트라이크";
+    static final String NOTHING_MSG = "낫싱";
+    static final String VICTORY_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    static final String RESTART_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public int getRandomNumber() {
         int number = 0;
@@ -37,15 +42,45 @@ public class Game {
     }
 
     public void countBallAndStrike(String randomNumber, String input){
-        // 자리 & 숫자 모두 일치
-        if (randomNumber.equals(input)) return;
-
-        // 볼, 스트라이크 개수 세기
         int ball = 0;
         int strike = 0;
+
+        // 자리 & 숫자 모두 일치
+        if (randomNumber.equals(input)) {
+            strike = 3;
+            printCountMsg(ball, strike);
+            return;
+        }
+
+        // 볼, 스트라이크 개수 세기
         for (int i =0; i< SIZE; i++){
             if (randomNumber.charAt(i) == input.charAt(i)) strike++;
             else if (randomNumber.contains(String.valueOf(input.charAt(i)))) ball++;
+        }
+        printCountMsg(ball, strike);
+    }
+
+    public void printCountMsg(int ball, int strike){
+        // 3개의 숫자를 모두 맞힐 경우
+        if(strike == SIZE) {
+            System.out.println(VICTORY_MSG);
+            System.out.println(RESTART_MSG);
+        }
+        // 하나도 없는 경우
+        else if (ball == 0 && strike == 0){
+            System.out.println(NOTHING_MSG);
+        }
+        // 볼, 스트라이크 둘 다 있는 경우
+        else if (ball != 0 && strike != 0){
+            System.out.println(ball + BALL_MSG + " " + strike + STRIKE_MSG);
+        }
+        // 볼만 있는 경우
+        else if (ball == 0){
+            System.out.println(ball + BALL_MSG);
+        }
+        // 스트라이크만 있는 경우
+        else if (strike == 0) {
+            System.out.println(strike + STRIKE_MSG);
         }
     }
 }
