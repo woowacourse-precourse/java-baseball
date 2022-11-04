79e6a14d7e0e3fdf029ad124d7b185b0b5a0f3be
ysng_ysng <yeasung67@gmail.com>
2022-11-05 00:05:30 +0900

    feat(PlayerUser): Implement new method: isRightNumberSize(String number)
    PlayerUser 클래스에 메서드 isRightNumberSize(String number)를 작성했습니다

--- src/main/java/baseball/playerUser/PlayerUser.java
+++ src/main/java/baseball/playerUser/PlayerUser.java
@@ -16,4 +16,22 @@ public class PlayerUser {
 		return Console.readLine();
 	}
 
+	private boolean isInputNumberException(String number) {
+		return !isRightNumberSize(number) || !isPositiveNumber(number) || !isNoDuplicatedNumber(
+			number);
+	}
+
+	private boolean isNoDuplicatedNumber(String number) {
+	}
+
+	private boolean isPositiveNumber(String number) {
+		for (char elem : number.toCharArray()) {
+
+		}
+	}
+
+	private boolean isRightNumberSize(String number) {
+		return number.length() == 3;
+	}
+
 }
