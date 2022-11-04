61c4285bb9d5b70da2702e344a7b60433a870d22
UJ15 <dbwogml15@naver.com>
2022-11-04 15:27:15 +0900

    test : add random number generate test

---
+++ src/test/java/baseball/engine/NextStepNumberGeneratorTest.java
@@ -0,0 +1,30 @@
+package baseball.engine;
+
+import baseball.model.Numbers;
+import java.util.HashSet;
+import java.util.Set;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class NextStepNumberGeneratorTest {
+    private final static int RANDOM_NUMBER_SIZE = 3;
+    private NumberGenerator generator;
+
+    @BeforeEach
+    void setup() {
+        generator = new NextStepNumberGenerator();
+    }
+
+    @Test
+    @DisplayName("랜덤 숫자 생성 기능 테스트")
+    void randomNumberGenerate() {
+        Numbers numbers = generator.generate(RANDOM_NUMBER_SIZE);
+        Set<Integer> distinctNumbers = new HashSet<>();
+
+        numbers.iterateForEach((i, number) -> distinctNumbers.add(number));
+
+        Assertions.assertThat(distinctNumbers.size()).isEqualTo(RANDOM_NUMBER_SIZE);
+    }
+}
