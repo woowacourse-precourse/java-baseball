7920269b8102dbd8a9c48ca0c74712fdb26cd66e
csj <koreaxman@naver.com>
2022-11-04 16:11:12 +0900

    feat: check computer number contain input number

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,7 +4,20 @@ public class Game {
     public final static String START_NOTIFICATION = "숫자 야구 게임을 시작합니다.";
     public final static String END_NOTIFICATION = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    Computer computer = new Computer();
+
     public void start() {
         System.out.println(START_NOTIFICATION);
+    }
+
+    public boolean isContainAnyNumber() {
+        Player player = new Player();
+        for (int i = 0; i < 3; i++) {
+            if (computer.randomNumberListToString()
+                    .contains(player.splitInputNumberToList().get(i))) {
+                return true;
+            }
+        }
+        return false;
     }
 }
