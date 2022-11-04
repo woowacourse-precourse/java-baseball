8f0d616fcb436b52c519cfbb9c194252387b7342
yejin130 <jinis2023@gmail.com>
2022-11-05 00:06:23 +0900

    feat (generateNumber) : generateNumber 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,16 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-
+        System.out.println(generateNumber());
     }
-
+    public static List<Integer> generateNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
