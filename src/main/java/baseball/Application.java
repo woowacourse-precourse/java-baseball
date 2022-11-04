91414e8329742a40db20cfdcc3812f1c9afdf57a
jthugg <jthug@naver.com>
2022-11-04 09:33:31 +0900

    Feat: 프로그램 시작 메세지 출력
    프로그램 시작 메세지 출력
    "숫자 야구 게임을 시작합니다."

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,7 +12,11 @@ public class Application {
     final static int MAX_RANDOM_VALUE = 9;
     final static int MAX_NUMBERS_COUNT = 3;
 
+    final static String MESSAGE_START = "숫자 야구 게임을 시작합니다.";
+
     public static void main(String[] args) {
+        System.out.println(MESSAGE_START);
+
         int gameStart = INITE_GAME_START_VALUE;
 
         /* 프로그램 메인 구조, 게임 중단 플래그가 세워질 때 까지 반복, 예외 발생 시 프로그램 중단 */
