4c05f5184ec3c4ae21d2dec665d18695a21e8683
ChoiYeonho0903 <903yh@naver.com>
2022-11-05 02:23:58 +0900

    feat: 사용자 숫자 입력 기능 구현

---
+++ src/main/java/baseball/model/User.java
@@ -0,0 +1,42 @@
+package baseball.model;
+
+import baseball.view.BaseballConsole;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class User {
+
+    private List<Integer> numbers;
+
+    public void inputNumber() {
+        String input = BaseballConsole.userInputNumber();
+        validateNumbers(input);
+        validateLength(input);
+        validateDifferentNumbers(input);
+    }
+
+    private void validateNumbers(String input) {
+        try {
+            int number = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자 외의 문자는 입력할 수 없습니다.");
+        }
+    }
+    private void validateLength(String input) {
+        if(input.length() != 3) {
+            throw new IllegalArgumentException("3자리 숫자를 입력해야 합니다.");
+        }
+    }
+
+    private void validateDifferentNumbers(String input) {
+        Set<Character> set = new HashSet<>();
+        char[] arr = input.toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if(set.contains(arr[i])) {
+                throw new IllegalArgumentException("중복되는 숫자를 입력할 수 없습니다.");
+            }
+            set.add(arr[i]);
+        }
+    }
+}
