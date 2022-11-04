83bf11433f963ceaaa636cf7bf182ff1c2fe1393
woo-chang <clllickme@naver.com>
2022-11-04 18:14:27 +0900

    feat: 게임 상태를 바탕으로 플레이어에게 결과를 출력하는 기능 구현

--- src/main/java/baseball/domain/BaseBall.java
+++ src/main/java/baseball/domain/BaseBall.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import baseball.util.GamePrinter;
 import baseball.util.GameValidator;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
@@ -26,6 +27,7 @@ public class BaseBall implements Game {
     public void play() {
         playerInput();
         compareNumbers();
+        showResult();
     }
 
     private void playerInput() {
@@ -72,5 +74,25 @@ public class BaseBall implements Game {
         return computerNumbers.contains(playerNumbers.get(index));
     }
 
+    private void showResult() {
+        StringBuilder result = new StringBuilder();
+        if (ball == 0 && strike == 0) {
+            result.append("낫싱");
+        }
+
+        if (ball != 0) {
+            result.append(ball).append("볼").append(" ");
+        }
+
+        if (strike != 0) {
+            result.append(strike).append("스트라이크").append("\n");
+        }
+
+        if (strike == GameValidator.GAME_NUMBER_LENGTH) {
+            result.append(GameValidator.GAME_NUMBER_LENGTH).append("개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+
+        GamePrinter.println(result.toString());
+    }
 
 }
