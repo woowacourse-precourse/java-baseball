8a28f1b426662564b69b8072de510e66d6d21c32
woosung1223 <dntjd991223@naver.com>
2022-11-04 18:32:48 +0900

    test(Score): getScore에 대한 테스트 코드 생성

---
+++ src/test/java/baseball/ScoreTest.java
@@ -0,0 +1,39 @@
+package baseball;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ScoreTest {
+    private Score score;
+    @BeforeEach
+    public void setScore() {
+        score = new Score(1, 2);
+    }
+    @DisplayName("길이는 2여야 한다")
+    @Test
+    void getScoreTest() {
+        assertThat(score.getScore().size()).isEqualTo(2);
+    }
+
+    @DisplayName("ArrayList여야 한다")
+    @Test
+    void typeTest() {
+        assertThat(score.getScore()).isInstanceOf(ArrayList.class);
+    }
+
+    @DisplayName("각 원소는 Integer여야 한다")
+    @Test
+    void elementTest() {
+        assertThat(score.getScore())
+                .allSatisfy(element -> {
+                    assertThat(element).isInstanceOf(Integer.class);
+                });
+    }
+}
