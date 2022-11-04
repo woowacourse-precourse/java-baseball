9316230d182c0a8f8c6af9abac5a520e3938be2b
jagajang <kosb7544@gmail.com>
2022-11-04 16:05:26 +0900

    dev: create random number generator class

---
+++ src/main/java/baseball/RandomAnswerGenerator.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomAnswerGenerator {
+    public static int getRandomAnswer() {
+        int answer = 0;
+
+        // generate random answer
+
+        return 0;
+    }
+}
