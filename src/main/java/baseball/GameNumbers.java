80fd7fe93549ee9db792aa2b908eb95c896e6ad9
Laeng <Laeng@users.noreply.github.com>
2022-11-04 11:01:03 +0900

    fix(GameNumbers): 숫자 비교 메서드에서 메서드 분리

--- src/main/java/baseball/GameNumbers.java
+++ src/main/java/baseball/GameNumbers.java
@@ -6,33 +6,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameNumbers {
-    private final int computer;
-    private final int player;
     private final List<Integer> oneDigitComputerNumbers;
     private final List<Integer> oneDigitPlayerNumbers;
 
     public GameNumbers(int computer, int player) {
-        this.computer = computer;
-        this.player = player;
         this.oneDigitComputerNumbers = GameUtil.splitNumberToOneDigits(computer);
         this.oneDigitPlayerNumbers = GameUtil.splitNumberToOneDigits(player);
     }
 
     public Scoreboard compare() {
-        if (this.oneDigitComputerNumbers.equals(this.oneDigitPlayerNumbers)) {
+        if (this.isThreeStrikes()) {
             return new Scoreboard(0, 3); // 3스트라이크
         }
 
         Scoreboard scoreboard = new Scoreboard();
-        List<Integer> retainOneDigitNumbers = new ArrayList<>(this.oneDigitComputerNumbers);
-
-        retainOneDigitNumbers.retainAll(this.oneDigitPlayerNumbers);
+        List<Integer> retainOneDigitNumbers = this.retainOneDigitNumbers();
 
         for (int oneDigitNumber : retainOneDigitNumbers) {
             scoreboard.addAllPoint(this.compareOneDigitNumber(oneDigitNumber));
         }
 
         return scoreboard;
+    }
+
+    private boolean isThreeStrikes() {
+        return this.oneDigitComputerNumbers.equals(this.oneDigitPlayerNumbers);
+    }
+
+    private List<Integer> retainOneDigitNumbers() {
+        List<Integer> retainOneDigitNumbers = new ArrayList<>(this.oneDigitComputerNumbers);
+
+        retainOneDigitNumbers.retainAll(this.oneDigitPlayerNumbers);
+
+        return retainOneDigitNumbers;
     }
 
     private Scoreboard compareOneDigitNumber(int oneDigit) {
