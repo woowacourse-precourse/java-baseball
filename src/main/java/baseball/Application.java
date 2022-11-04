196625d653aa0ec908efead069cae73d6e58c38d
jwp345 <jwp135@naver.com>
2022-11-04 13:26:24 +0900

    feat: 임의의 수 3개를 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String answer = makeBall();
+    }
+
+    static String makeBall() {
+        List<Integer> cands = new ArrayList<>();
+        while(cands.size() < 3) {
+            int num = Randoms.pickNumberInRange(1, 9);
+            if(!cands.contains(num)) {
+                cands.add(num);
+            }
+        }
+        String balls = "";
+        for(int ball : cands) {
+            balls += ball;
+        }
+        return balls;
     }
 }
