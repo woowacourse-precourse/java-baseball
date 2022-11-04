9c1725e30c583eeb2f27cdae94fa8bc3ce6790b4
dohyun <ldh19738@gmail.com>
2022-11-04 22:20:46 +0900

    feat: Add messages related to game

---
+++ src/main/java/baseball/GameMessage.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class GameMessage {
+    public static final String start = "숫자 야구 게임을 시작합니다.";
+    public static final String input = "숫자를 입력해주세요 : ";
+    public static final String end = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String restart = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String strike = "스트라이크";
+    public static final String ball = "볼";
+    public static final String nothing = "낫싱";
+}
