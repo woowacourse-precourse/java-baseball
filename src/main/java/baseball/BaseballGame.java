856f7042aa073b51b95bdb704201f7227d0f7dd1
minhee020 <minhee020@naver.com>
2022-11-04 20:21:49 +0900

    feat : 사용자 숫자 입력 받기

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,25 +1,43 @@
 package baseball;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class BaseballGame {
-    BaseballGame() {
+    int[] computerNumber;
+    int[] userNumber;
+    BaseballGame(){
     }
 
-    public int[] makeRandomNumber() {
-        int[] result = new int[3];
-        boolean[] alreadyUsed = new boolean[9];
+    public void makeRandomNumber() {
+        computerNumber = new int[3];
+        boolean[] alreadyUsed = new boolean[10];
         Random random = new Random();
         for (int i = 0; i < 3; i++) {
             int number = random.nextInt(9) + 1;
 
             if (!alreadyUsed[number]) {
                 alreadyUsed[number] = true;
-                result[i] = number;
+                computerNumber[i] = number;
             } else {
                 i--;
             }
         }
-        return result;
+    }
+
+    public void startGame(){
+        String userInput;
+        Scanner scanner = new Scanner(System.in);
+        userInput = scanner.nextLine();
+        if (invalidUserNumber(userInput)){
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+    }
+    public boolean restartGame(){
+        return true;
+    }
+
+    public boolean invalidUserNumber(String userInput){
+
     }
 }
