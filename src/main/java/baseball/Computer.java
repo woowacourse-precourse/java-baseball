3a73613bacb7ffc5c20471133e526ee8e1671299
lzlkolo <lzlko1209@gmail.com>
2022-11-04 16:28:53 +0900

    feat: 1~9사이의 서로 다른 임의의 수로 이루어진 3자릿수 생성 기능 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,8 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
     public static List<Integer> createRandomNumber() {
+        List<Integer> randomNumber = new ArrayList<>(3);
+
+        while (randomNumber.size() < 3) {
+            int temp = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumber.contains(temp)) {
+                randomNumber.add(temp);
+            }
+        }
+
+        return randomNumber;
     }
 }
