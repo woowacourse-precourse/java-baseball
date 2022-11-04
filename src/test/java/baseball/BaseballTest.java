ee128781511692be83f4c2c5c24e5d4f045a3e29
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-04 10:26:19 +0900

    test(BaseballTest) : splitDigits 테스트 케이스 삭제

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -2,7 +2,6 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.*;
 
-import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -11,36 +10,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class BaseballTest {
-	@Nested
-	class SplitDigits {
-		@Test
-		void case1() {
-			String threeDigitNumber = "369";
-			List<Integer> result = List.of(3, 6, 9);
-			assertThat(Baseball.splitDigits(threeDigitNumber)).isEqualTo(result);
-		}
-
-		@Test
-		void case2() {
-			String threeDigitNumber = "4235";
-			List<Integer> result = List.of(4, 2, 3, 5);
-			assertThat(Baseball.splitDigits(threeDigitNumber)).isEqualTo(result);
-		}
-
-		@Test
-		void case3() {
-			String threeDigitNumber = "";
-			List<Integer> result = Collections.emptyList();
-			assertThat(Baseball.splitDigits(threeDigitNumber)).isEqualTo(result);
-		}
-
-		@Test
-		void case4() {
-			String threeDigitNumber = "abc";
-			List<Integer> result = Collections.emptyList();
-			assertThat(Baseball.splitDigits(threeDigitNumber)).isEqualTo(result);
-		}
-	}
 
 	@Nested
 	class make3DigitNumberTest {
