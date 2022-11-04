3335a50722c58bf05d427950e085591289bf0ac6
aksmf1442 <aksmf1442@naver.com>
2022-11-04 21:33:20 +0900

    feat: 숫자 3개를 검사했을 때 잘못된 값일 경우 IllegalArgumentException 발생시키는 기능 구현

--- src/main/java/baseball/Numbers.java
+++ src/main/java/baseball/Numbers.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Numbers {
 
@@ -35,11 +37,38 @@ public class Numbers {
         return new Numbers(numbers);
     }
 
+    public void validate() {
+        validateRange();
+        validateSize();
+    }
+
+    private void validateSize() {
+        Set<Integer> numberSet = new HashSet<>(numbers);
+
+        if (numbers.size() != NUMBERS_SIZE) {
+            throw new IllegalArgumentException("숫자를 " + NUMBERS_SIZE + "개 입력해주세요.");
+        }
+        if (numberSet.size() != NUMBERS_SIZE) {
+            throw new IllegalArgumentException("서로 다른 숫자를 입력해주세요.");
+        }
+    }
+
+    private void validateRange() {
+        String expression = "^[1-9]+$";
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (Integer number : numbers) {
+            stringBuilder.append(number);
+        }
+
+        String numbersString = stringBuilder.toString();
+        if (!numbersString.matches(expression)) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 1~9 범위의 숫자만 입력해주세요.");
+        }
+    }
+
     public List<Integer> getNumbers() {
         return numbers;
     }
 
-    public void validate() {
-
-    }
 }
