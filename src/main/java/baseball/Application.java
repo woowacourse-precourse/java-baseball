872f576c1a5007a5a1ca9220f24942d1ec258436
moonchaewon <codnjs8989@gmail.com>
2022-11-05 03:01:59 +0900

    feat(random): implement the function to generate random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
+    private static int generateRandomNumber() {
+        return Randoms.pickNumberInRange(1, 9);
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(generateRandomNumber());
     }
 }
