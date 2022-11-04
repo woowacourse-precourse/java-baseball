43b88518e93001906e6094fad64302386dc53961
yback1223 <yoyob1223@gmail.com>
2022-11-04 18:20:46 +0900

    refactor: fix beforEach

--- src/test/java/baseball/InputTest.java
+++ src/test/java/baseball/InputTest.java
@@ -5,25 +5,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InputTest {
 
-  private static final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-  private static final PrintStream originalOut = System.out;
+  private static ByteArrayOutputStream outContent;
   Input inputClass;
 
   @BeforeEach
   public void setUpStreams() {
+    outContent = new ByteArrayOutputStream();
     System.setOut(new PrintStream(outContent));
     inputClass = new Input();
-  }
-
-  @AfterEach
-  public void restoreStreams() {
-    System.setOut(originalOut);
   }
 
   @Test
@@ -31,6 +25,13 @@ class InputTest {
     String sysIn = "123";
     inputClass.printInputString(sysIn);
     assertEquals("[1, 2, 3]", outContent.toString().trim());
+  }
+
+  @Test
+  public void Input클래스의_정상적인_작동_테스트2() {
+    String sysIn = "456";
+    inputClass.printInputString(sysIn);
+    assertEquals("[4, 5, 6]", outContent.toString().trim());
   }
 
   @Test
