b313a5959b8f96075d0aa5698cc84052eec2d599
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-04 16:27:26 +0900

    style : 중괄호 수정 isComputerNumber()메소드 사용 부분에서 중괄호 오류 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -30,7 +30,6 @@ public class Application {
                     System.out.print("3개의 숫자를 모두 맞히셨습니다!");
                     break;
                 }
-                }
             }
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String gameCoin = Console.readLine();
