7133a72782390e593329c3dd878e6a389e55240d
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-11-04 11:37:26 +0900

    feat(RandomNumber): 난수 생성하는 함수 생성

--- src/main/java/baseball/domain/RandomNumber.java
+++ src/main/java/baseball/domain/RandomNumber.java
@@ -1,4 +1,15 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+
+import static java.lang.Math.*;
+
 public class RandomNumber {
+    public static void main(){
+        //1부터 9까지의 난수 생성
+        ArrayList<Integer> RandomNumList= new ArrayList();
+        for (int i=0; i < 5; i++){
+            int ran_num = (int) floor((random() * 10));
+        }
+    }
 }
