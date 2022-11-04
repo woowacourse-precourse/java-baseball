dd8addde87b79f5e41e59427aa2fce784bffdc70
bjk1649 <qhwns1649@naver.com>
2022-11-04 22:41:35 +0900

    feat: 시작, 종료 안내메시지를 출력하는 메소드 추가

--- src/main/java/baseball/InfoPrinter.java
+++ src/main/java/baseball/InfoPrinter.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class InfoPrinter {
     private InfoPrinter() {
     }
+    public static void printStartInfo() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
 
     public static void printStrikesAndBalls(List<Integer> strikesAndBalls) {
         int strikes = strikesAndBalls.get(0);
@@ -21,5 +24,10 @@ public class InfoPrinter {
             System.out.println("낫싱");
         }
         System.out.println(result.trim());
+    }
+
+    public static void printExitInfo() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
