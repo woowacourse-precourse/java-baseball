1d20c3f502a86f3d4464cefeb1b55a3cfcc0b9d9
minju412 <ln8847@ajou.ac.kr>
2022-11-04 23:58:30 +0900

    fix: add exception handling
    - when player input a non-three digit number
    - when player input a number containing zeroes
    - when player input a number other than 1 and 2
      at the end of the game

--- src/main/java/baseball/game/Player.java
+++ src/main/java/baseball/game/Player.java
@@ -1,5 +1,9 @@
 package baseball.game;
 
+import static baseball.Application.CONTINUE_GAME;
+import static baseball.Application.STOP_GAME;
+import static baseball.game.Computer.COMPUTER_NUMBER_SIZE;
+
 import baseball.message.SystemMessage;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
@@ -10,12 +14,27 @@ public class Player {
     public List<Integer> inputPlayerNumber() {
         System.out.print(SystemMessage.ENTER_NUMBER);
         String playerNumber = Console.readLine();
+
+        if (playerNumber.length() != COMPUTER_NUMBER_SIZE) {
+            throw new IllegalArgumentException();
+        }
+        if (playerNumber.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+
         return stringToList(playerNumber);
     }
 
     public String inputContinueOrStop() {
         System.out.println(SystemMessage.START_NEW_GAME_OR_END);
-        return Console.readLine();
+        String continueOrStop = Console.readLine();
+
+        if (!continueOrStop.equals(Integer.toString(CONTINUE_GAME))
+                && !continueOrStop.equals(Integer.toString(STOP_GAME))) {
+            throw new IllegalArgumentException();
+        }
+
+        return continueOrStop;
     }
 
     private List<Integer> stringToList(String playerInput) {
