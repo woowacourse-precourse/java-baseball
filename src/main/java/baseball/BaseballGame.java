c1fc46754f0497b1ac86c7686306b105f8240a0c
ycopp9 <aciees95@gmail.com>
2022-11-04 17:55:12 +0900

    feat(BaseballGame): implement application1
    Add the function pickTargetNumber() that returns three different numbers
    from 1 to 9.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -7,6 +7,13 @@ import java.util.List;
 
 public class BaseballGame {
     public List<Integer> pickTargetNumber() {
-        return null;
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
