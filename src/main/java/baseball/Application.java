f7a7096ed41eb33eeb08d66546c4bc1e3a37e758
minju412 <ln8847@ajou.ac.kr>
2022-11-04 14:23:46 +0900

    feat: create player

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,12 @@
 package baseball;
 
 import baseball.game.Computer;
+import baseball.game.Player;
 
 public class Application {
     public static void main(String[] args) {
         Computer computer = new Computer();
+        Player player = new Player();
         do {
             computer.startGame();
         } while ();
---
+++ src/main/java/baseball/game/Player.java
@@ -0,0 +1,14 @@
+package baseball.game;
+
+public class Player {
+
+    private String playerNumber;
+
+    public String getPlayerNumber() {
+        return playerNumber;
+    }
+
+    public void setPlayerNumber(String playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+}
