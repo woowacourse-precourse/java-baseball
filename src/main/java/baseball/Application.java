4737d4c5bcbb7f6e90ef5ed35f4948722cd22a93
asas6978 <asas6978@naver.com>
2022-11-04 13:41:22 +0900

    feat: 게임 종료 후 새로운 게임 시작/종료 여부를 받는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 import static baseball.Game.play;
+import static baseball.Validation.isExistOnlyNumber;
+import static baseball.Validation.isValidLength;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     public static void main(String[] args) {
@@ -14,7 +17,27 @@ public class Application {
             List<String> computerNumber = getComputerNumber();
 
             play(computerNumber);
+
+            isContinue = getRestartFromUser();
         }
+    }
+
+    private static boolean getRestartFromUser() {
+        List<String> userInput = new ArrayList<>(Arrays.asList(readLine().split("")));
+        if (!isValidLength(userInput, 1) || !isExistOnlyNumber(userInput)) {
+            throw new IllegalArgumentException();
+        }
+
+        int number = Integer.parseInt(userInput.get(0));
+        if (number == 1) {
+            return true;
+        }
+
+        if (number == 2) {
+            return false;
+        }
+
+        throw new IllegalArgumentException();
     }
 
     private static ArrayList<String> getComputerNumber() {
