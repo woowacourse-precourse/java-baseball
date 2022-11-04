7a7e56ba9c0fbf84bd208cea41436357a2c87f22
01192mg <01192mg@gmail.com>
2022-11-04 21:03:53 +0900

    refactor: 상수 추출

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,7 +1,6 @@
 package baseball;
 
-import static baseball.utils.Constants.CONTINUE;
-import static baseball.utils.Constants.RANDOM_NUMBER_LENGTH;
+import static baseball.utils.Constants.*;
 
 import java.util.List;
 
@@ -9,7 +8,7 @@ public class BaseballGame implements Game {
 
     @Override
     public void intro() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(INTRO);
     }
 
     @Override
@@ -21,19 +20,19 @@ public class BaseballGame implements Game {
 
         System.out.println(computer);
         while (strike != RANDOM_NUMBER_LENGTH) {
-            System.out.print("숫자를 입력해주세요 : ");
+            System.out.print(INPUT_PLAYER_NUMBER);
             player = BaseballGameService.getPlayerNumber();
             List<Integer> ballAndStrike = ResultCalculator.calculateBallAndStrike(computer, player);
             ball = ballAndStrike.get(0);
             strike = ballAndStrike.get(1);
-            System.out.println(ball + "볼 " + strike + "스트라이크:");
+            System.out.println(ball + BALL + SPACE + strike + STRIKE);
         }
     }
 
     @Override
     public void outro() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(OUTRO);
+        System.out.println(CONTINUE_OR_END);
         if (BaseballGameService.getContinueOrBreak() == CONTINUE) {
             replay();
         }
