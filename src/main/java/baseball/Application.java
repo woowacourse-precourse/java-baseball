61245e748f3a03d8869255e9fe79cdf8123ff1bf
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-04 15:17:58 +0900

    feat : 게임이 끝난 경우 재시작과 종료를 선택하는 기능 추가 validate()메소드를 validateUserNumber()와 validateGameCoin()으로 분리 게임이 끝나면 '게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.'문구 추가 추가된 요구사항의 indent의 depth가 3을 넘어갔다. 모든 기능 구현 후 class를 나누어 depth를 줄이려고한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -13,20 +12,31 @@ public class Application {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        List<Integer> computerNumber = makeRandomNumber();
+        while (true) {
+            List<Integer> computerNumber = makeRandomNumber();
+            System.out.println(computerNumber);
+            while (true) {
+                String userNumber = inputUserNumber();
 
-        while(true) {
-            String userNumber = inputUserNumber();
+                //제한사항 체크
+                try {
+                    validateUserNumber(userNumber);
+                } catch (IllegalArgumentException e) {
+                    throw new IllegalArgumentException(e);
+                }
 
-            //제한사항 체크
+                // 맞춘다면 반복문 빠져나오기
+                if (computerNumber.equals(toList(userNumber))) break;
+            }
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String gameCoin = Console.readLine();
             try {
-                validate(userNumber);
+                validateGameCoin(gameCoin);
             } catch (IllegalArgumentException e) {
                 throw new IllegalArgumentException(e);
             }
 
-            // 맞춘다면 반복문 빠져나오기
-            if (computerNumber.equals(toList(userNumber))) break;
+            if (gameCoin.equals("2")) break;
         }
     }
 
@@ -60,7 +70,11 @@ public class Application {
         return listUserNumber;
     }
 
-    public static void validate(String userNumber){
+    public static void validateUserNumber(String userNumber){
+        if(userNumber.length() != 3){
+            throw new IllegalArgumentException("3자리 숫자를 입력해주세요!");
+        }
+
         for(char number : userNumber.toCharArray()){
             // 중복된 숫자가 있으면 예외
             if( userNumber.chars().filter(c -> c == number).count() > 1 ){
@@ -71,6 +85,13 @@ public class Application {
             if(Character.isDigit(number)==false){
                 throw new IllegalArgumentException("숫자 이외의 값을 입력하셨습니다!");
             }
+        }
+    }
+
+    public static void validateGameCoin(String gameCoin) {
+        // 1,2가 아니면 exception
+        if(Pattern.matches("[1-2]",gameCoin) == false){
+            throw new IllegalArgumentException("1,2 이외의 값을 입력하셨습니다!");
         }
     }
 
