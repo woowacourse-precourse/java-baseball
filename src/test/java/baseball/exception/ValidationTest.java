71172152e242ebf5e74ff314bb6442ac78353e00
Jzakka <mouse4786@gmail.com>
2022-11-05 00:55:28 +0900

    test: 검증 기능 테스트 추가

---
+++ src/test/java/baseball/exception/ValidationTest.java
@@ -0,0 +1,32 @@
+package baseball.exception;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class ValidationTest {
+    Validator validator = new Validator();
+
+    @Test
+    void 입력_유효성_검사_숫자가_아닌_경우() {
+        String input = "a75";
+        Assertions.assertThatThrownBy(()->
+                validator.validateInput(input))
+                        .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력_유효성_검사_범위를_벗어난_경우() {
+        String input = "010";
+        Assertions.assertThatThrownBy(()->
+                validator.validateInput(input))
+                        .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력_유효성_검사_길이가_3이_아닌_경우() {
+        String input = "46678";
+        Assertions.assertThatThrownBy(()->
+                validator.validateInput(input)).
+                        isInstanceOf(IllegalArgumentException.class);
+    }
+}
