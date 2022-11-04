46e3d6836fdbcc1b11291fc55ee10dc24cb2a372
연주 <50358956+Yeon-jooKim@users.noreply.github.com>
2022-11-04 16:20:06 +0900

    feat: 이미 만들어진 숫자가 아닌 난수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class Application {
+
+
+    private static int GenerateNonDuplicateRandomNumber(List<Integer> randNum) {
+        int k;
+        do {
+            k = Randoms.pickNumberInRange(1, 9);
+        }
+        while (randNum.contains(k));
+        return k;
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
---
+++ src/test/java/baseball/FeatureListTest.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+public class FeatureListTest {
+
+    @Test
+    void 서로_다른_3개의_난수_검증(){
+
+    }
+}
