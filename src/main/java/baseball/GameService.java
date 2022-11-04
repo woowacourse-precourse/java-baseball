f1a941bae85fb4f503ab40928dfe0064841b8ca6
CMHyuk <alsgur9710@naver.com>
2022-11-04 13:43:11 +0900

    feat: 게임 결과 출력

--- src/main/java/baseball/GameService.java
+++ src/main/java/baseball/GameService.java
@@ -1,6 +1,9 @@
 package baseball;
 
 public class GameService {
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
 
     private final InputBallNumber inputBallNumber;
     private final RandomBallNumber randomBallNumber;
@@ -34,5 +37,47 @@ public class GameService {
 
     private boolean isEqualNumber(String randomNumber, String inputNumber, int ballNumber) {
         return inputNumber.charAt(ballNumber) == randomNumber.charAt(ballNumber);
+    }
+
+    public void printResult(Result result) {
+        int strike = result.getStrike();
+        int ball = result.getBall();
+
+        isThreeStrike(strike);
+        isStrikeAndBall(strike, ball);
+        isOnlyBall(strike, ball);
+        isOnlyStrike(strike, ball);
+        isNothing(strike, ball);
+    }
+
+    private void isOnlyStrike(int strike, int ball) {
+        if (strike < 3 && strike > 0 && ball == 0) {
+            System.out.println(strike + STRIKE);
+        }
+    }
+
+    private void isNothing(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            System.out.println(NOTHING);
+        }
+    }
+
+    private void isOnlyBall(int strike, int ball) {
+        if (strike == 0 && ball != 0) {
+            System.out.println(ball + BALL);
+        }
+    }
+
+    private void isStrikeAndBall(int strike, int ball) {
+        if (strike != 0 && ball != 0) {
+            System.out.println(ball + BALL + " " + strike + STRIKE);
+        }
+    }
+
+    private void isThreeStrike(int strike) {
+        if (strike == 3) {
+            System.out.println(strike + STRIKE);
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
     }
 }
