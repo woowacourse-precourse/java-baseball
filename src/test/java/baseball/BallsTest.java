1e266e5a545a6187b4647c5ada532ac31c588e43
jaeyeon kim <jakind@naver.com>
2022-11-04 16:24:06 +0900

    test(BallTest): 세자리 숫자를 만들어주는 기능 테스트코드 추가

--- src/test/java/baseball/BallsTest.java
+++ src/test/java/baseball/BallsTest.java
@@ -7,10 +7,25 @@ public class BallsTest {
 
     @Test
     void 이미_있는_숫자와_겹치는지_확인하는_기능() {
+        // given
         Balls balls = new Balls();
         balls.addBall(new Ball(1));
 
+        // then
         assertThat(balls.checkDuplicate(new Ball(1))).isTrue();
         assertThat(balls.checkDuplicate(new Ball(2))).isFalse();
+    }
+
+    @Test
+    void 세자리_숫자를_만들어주는_기능() {
+        // given
+        Balls balls = new Balls();
+
+        // when
+        balls.generate();
+
+        //then
+        assertThat(balls.size()).isEqualsTo(3);
+        assertThat(balls.getBallList().stream().allMatch(number) -> 1 <= number && number <= 9).isTrue();
     }
 }
