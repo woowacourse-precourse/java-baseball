d360e6b074359d701f51fbed561d9e2666dc6333
iamminho <minho100227@kakao.com>
2022-11-04 16:37:06 +0900

    test(calculator): calculator 로직 변경으로 인해 테스트 케이스 로직 변경

--- src/test/java/baseball/controller/CalculatorTest.java
+++ src/test/java/baseball/controller/CalculatorTest.java
@@ -2,6 +2,7 @@ package baseball.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import baseball.Player;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
@@ -11,45 +12,61 @@ class CalculatorTest {
 
 	@Test
 	@DisplayName("볼인 경우")
-	public void getBallStrikeCountTest1() {
-		List<Integer> randomNumberList = Arrays.asList(1, 4, 7);
-		List<Integer> gameNumberList = Arrays.asList(4, 3, 9);
-		List<Integer> ballStrikeCount = Calculator.getBallStrikeCount(randomNumberList, gameNumberList);
-		List<Integer> result = Arrays.asList(1, 0);
+	public void checkPlayerNumbersTest1() {
+		List<Integer> randomNumbers = Arrays.asList(1, 4, 7);
+		List<Integer> playerNumbers = Arrays.asList(4, 3, 9);
+		Player player = new Player(playerNumbers);
+		Calculator.checkPlayerNumbers(player, randomNumbers);
 
-		assertThat(ballStrikeCount).isEqualTo(result);
+		int ballCount = 1;
+		int strikeCount = 0;
+
+		assertThat(player.getBall()).isEqualTo(ballCount);
+		assertThat(player.getStrike()).isEqualTo(strikeCount);
 	}
 
 	@Test
 	@DisplayName("볼, 스트라이크인 경우")
-	public void getBallStrikeCountTest2() {
-		List<Integer> randomNumberList = Arrays.asList(3, 4, 9);
-		List<Integer> gameNumberList = Arrays.asList(4, 3, 9);
-		List<Integer> ballStrikeCount = Calculator.getBallStrikeCount(randomNumberList, gameNumberList);
-		List<Integer> result = Arrays.asList(2, 1);
+	public void checkPlayerNumbersTest2() {
+		List<Integer> randomNumbers = Arrays.asList(3, 4, 9);
+		List<Integer> playerNumbers = Arrays.asList(4, 3, 9);
+		Player player = new Player(playerNumbers);
+		Calculator.checkPlayerNumbers(player, randomNumbers);
 
-		assertThat(ballStrikeCount).isEqualTo(result);
+		int ballCount = 2;
+		int strikeCount = 1;
+
+		assertThat(player.getBall()).isEqualTo(ballCount);
+		assertThat(player.getStrike()).isEqualTo(strikeCount);
 	}
 
 	@Test
 	@DisplayName("낫싱인 경우")
-	public void getBallStrikeCountTest3() {
-		List<Integer> randomNumberList = Arrays.asList(2, 5, 8);
-		List<Integer> gameNumberList = Arrays.asList(4, 3, 9);
-		List<Integer> ballStrikeCount = Calculator.getBallStrikeCount(randomNumberList, gameNumberList);
-		List<Integer> result = Arrays.asList(0, 0);
+	public void checkPlayerNumbersTest3() {
+		List<Integer> randomNumbers = Arrays.asList(2, 5, 8);
+		List<Integer> playerNumbers = Arrays.asList(4, 3, 9);
+		Player player = new Player(playerNumbers);
+		Calculator.checkPlayerNumbers(player, randomNumbers);
 
-		assertThat(ballStrikeCount).isEqualTo(result);
+		int ballCount = 0;
+		int strikeCount = 0;
+
+		assertThat(player.getBall()).isEqualTo(ballCount);
+		assertThat(player.getStrike()).isEqualTo(strikeCount);
 	}
 
 	@Test
 	@DisplayName("3스트라이크인 경우")
 	public void getBallStrikeCountTest4() {
-		List<Integer> randomNumberList = Arrays.asList(4, 3, 9);
-		List<Integer> gameNumberList = Arrays.asList(4, 3, 9);
-		List<Integer> ballStrikeCount = Calculator.getBallStrikeCount(randomNumberList, gameNumberList);
-		List<Integer> result = Arrays.asList(0, 3);
+		List<Integer> randomNumbers = Arrays.asList(4, 3, 9);
+		List<Integer> playerNumbers = Arrays.asList(4, 3, 9);
+		Player player = new Player(playerNumbers);
+		Calculator.checkPlayerNumbers(player, randomNumbers);
 
-		assertThat(ballStrikeCount).isEqualTo(result);
+		int ballCount = 0;
+		int strikeCount = 3;
+
+		assertThat(player.getBall()).isEqualTo(ballCount);
+		assertThat(player.getStrike()).isEqualTo(strikeCount);
 	}
 }
