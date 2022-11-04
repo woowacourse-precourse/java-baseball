3b34e05277dd36625d647a34f25a54405c42de40
lecocococo <chemwang13@naver.com>
2022-11-05 03:08:35 +0900

    feat(controller): 컨트롤러의 구조 생성

---
+++ src/main/java/baseball/controller/Controller.java
@@ -0,0 +1,27 @@
+package baseball.controller;
+
+public class Controller {
+
+    public void run() {
+        prepareGame();
+        startGame();
+        endGame();
+        retry();
+    }
+
+    public void prepareGame() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void endGame() {
+
+    }
+
+    public void retry() {
+
+    }
+}
