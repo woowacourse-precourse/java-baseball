e5cbde69c408f46f070476a44839063ae7751ac2
hossang <zkzk104@naver.com>
2022-11-04 19:35:03 +0900

    feat: 게임 진행 상황을 출력하기

---
+++ src/main/java/baseball/BaseballView.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class BaseballView {
+
+    public void printGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+}
