0501774b0cba428f3267a51056510059bf3f8acb
kimdahee0815 <diana815@naver.com>
2022-11-04 14:36:25 +0900

    feat : 스트라이크 개수가 3일 때, 게임을 재시작할지 종료할지 콘솔 출력, numstr으로 다시 사용자 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -33,6 +33,11 @@ public class Application {
 				numstr = Console.readLine();
 				check(numstr,strikeCount,ballCount);
 			}
+			if(strikeCount==3) {       //3개의 숫자를 모두 맞추고 난 후(strikeCount 3개) 게임 다시 시작, 종료를 고르는 구문
+				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+				System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+				numstr = Console.readLine();
+			}	
 		}	
     }
     
