3a1178b47f47673ea0d8be3f1935e4be559fbb71
Kimhyeyun <cqqwer@gmail.com>
2022-11-04 19:31:26 +0900

    Test: 에러로 라운드 테스트 삭제 → compareTest와 userTest의 중복으로 지워도 상관없다고 판단

--- src/test/java/baseball/BaseBallTest.java
+++ src/test/java/baseball/BaseBallTest.java
@@ -1,14 +1,13 @@
 package baseball;
 
+import domain.Computer;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 public class BaseBallTest {
 
-    @Test
-    void round_확인() {
-        BaseBall baseBall = new BaseBall();
-        baseBall.roundOfGame();
-    }
 
     @Test
     void 게임_종료_여부_확인() {
