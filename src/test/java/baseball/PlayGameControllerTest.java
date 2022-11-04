22794d911b3c7d5b28b7a6656e3034dce5e1af58
CMHyuk <alsgur9710@naver.com>
2022-11-04 20:51:29 +0900

    test: 숫자 야구 게임 컨트롤러 테스트

---
+++ src/test/java/baseball/PlayGameControllerTest.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static baseball.Config.gameService;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlayGameControllerTest extends NsTest {
+
+    @Test
+    void 게임_컨트롤러_실행() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("456", "123", "1", "416", "456", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "2스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 2, 3, 4, 5, 6
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        PlayGameController.run(gameService());
+    }
+}
