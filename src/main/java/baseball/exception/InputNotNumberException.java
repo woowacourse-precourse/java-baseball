f14727d24871aefd323aa84448c04904b20ca8de
gnoesnooj <306yyy@naver.com>
2022-11-04 18:19:20 +0800

    feat : 숫자가 아닌 경우에 대한 예외 상황 구현

---
+++ src/main/java/baseball/exception/InputNotNumberException.java
@@ -0,0 +1,11 @@
+
+package baseball.exception;
+
+public class InputNotNumberException extends IllegalArgumentException{
+
+    private static final String message = "입력 값이 숫자가 아닙니다.";
+
+    public InputNotNumberException(){
+        super(message);
+    }
+}
