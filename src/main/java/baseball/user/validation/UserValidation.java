10e23b1d8a595d2030ba1d94a7eaf011932e7ef6
kimminsu <xjfcnfw3@naver.com>
2022-11-04 09:00:18 +0900

    feat: [예외 처리추가] 입력된 숫자는 3자리수여야 한다.

--- src/main/java/baseball/user/validation/UserValidation.java
+++ src/main/java/baseball/user/validation/UserValidation.java
@@ -7,11 +7,18 @@ public class UserValidation {
 
     public static void validate(String userInput) {
         validateOnlyNumber(userInput);
+        validateNumberLength(userInput);
     }
 
     public static void validateOnlyNumber(String userInput) {
         if (!userInput.matches(NUMBER_REGEXP)) {
             throw new IllegalArgumentException(ExceptionMessage.ONLY_NUMBER);
+        }
+    }
+
+    public static void validateNumberLength(String userInput) {
+        if (userInput.length() != NUMBER_LENGTH) {
+            throw new IllegalArgumentException(ExceptionMessage.NUMBER_LENGTH);
         }
     }
 }
