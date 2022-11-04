5a5a97b4a2e1a17853523861a5773d2a551cd15c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-04 22:33:14 +0900

    feat: PlayerException 입력에 같은 숫자가 있을 때 참을 반환하는 기능 구현

--- src/main/java/baseball/exception/PlayerException.java
+++ src/main/java/baseball/exception/PlayerException.java
@@ -5,6 +5,9 @@ import java.util.regex.Pattern;
 public class PlayerException {
 
     private static final int LENGTH_LIMIT = 3;
+    private static final int FIRST = 0;
+    private static final int SECOND = 1;
+    private static final int THIRD = 2;
     private static final String NOT_NUMBER_EXCEPTION = "입력은 숫자로만 해야 합니다.";
     private static final String NOT_THREE_DIGIT_EXCEPTION = "입력은 세자리 숫자여야 합니다. ";
     private static final String NO_ZERO_EXCEPTION = "입력은 1부터 9까지의 숫자 3개여야 합니다.";
@@ -37,7 +40,9 @@ public class PlayerException {
     }
 
     public boolean containsSameNumber(String input){
-        return false;
+        return input.charAt(FIRST) == input.charAt(SECOND)
+                || input.charAt(SECOND) == input.charAt(THIRD)
+                || input.charAt(THIRD) == input.charAt(FIRST);
     }
 
     public void containsSameNumberException(){
