e4644fe31f57a0f90fce9e761116c305fb528420
JunH <tjdtls690@gmail.com>
2022-11-04 22:27:35 +0900

    :art: refactor(ALL) : import 문 최적화

--- src/main/java/baseball/domain/factory/ComputerBallsFactory.java
+++ src/main/java/baseball/domain/factory/ComputerBallsFactory.java
@@ -2,7 +2,6 @@ package baseball.domain.factory;
 
 import baseball.domain.Balls;
 import baseball.domain.strategy.BallsCreateStrategy;
-import baseball.domain.strategy.RandomBallsCreateStrategy;
 
 public class ComputerBallsFactory {
     private static final String UTILITY_CLASS_CREATE_EXCEPTION_MESSAGE = "생성할 수 없는 유틸 클래스입니다.";
