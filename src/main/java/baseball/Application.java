01444e36e7dbe7bd56dd261dd54f06ca99d4ca58
asas6978 <asas6978@naver.com>
2022-11-04 13:21:43 +0900

    refactor: 게임 실행 파트를 별도의 클래스로 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,26 +4,17 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
 
-import static baseball.CompareNumber.compare;
-import static baseball.Score.BALL;
-import static baseball.Score.STRIKE;
-import static camp.nextstep.edu.missionutils.Console.readLine;
+import static baseball.Game.play;
 
 public class Application {
     public static void main(String[] args) {
-        List<String> computerNumber = getComputerNumber();
+        boolean isContinue = true;
 
-        List<String> userInput = new ArrayList<>(Arrays.asList(readLine().split("")));
-        if (!isValidLength(userInput) || !isExistOnlyNumber(userInput)) {
-            throw new IllegalArgumentException();
+        while(isContinue) {
+            List<String> computerNumber = getComputerNumber();
+
+            play(computerNumber);
         }
-
-        Map<Score, Integer> score = new HashMap<>(3);
-        initializeScore(score);
-
-        compare(computerNumber, userInput, score);
-
-        printResult(score);
     }
 
     private static ArrayList<String> getComputerNumber() {
@@ -38,46 +29,5 @@ public class Application {
         }
 
         return computerNumber;
-    }
-
-    private static boolean isValidLength(List<String> userInput) {
-        return userInput.size() == 3;
-    }
-
-    private static boolean isExistOnlyNumber(List<String> userInput) {
-        boolean isOnlyNumber = false;
-
-        for (String element : userInput) {
-            isOnlyNumber = isOnlyNumber || Character.isDigit(element.charAt(0));
-        }
-        return isOnlyNumber;
-    }
-
-    private static void initializeScore(Map<Score, Integer> score) {
-        for (Score name : Score.values()) {
-            score.put(name, 0);
-        }
-    }
-
-    private static void printResult(Map<Score, Integer> score) {
-        int numberOfSTRIKE = score.get(STRIKE);
-        int numberOfBALL = score.get(BALL);
-
-        if (numberOfSTRIKE == 0 && numberOfBALL == 0) {
-            System.out.println("낫싱");
-            return;
-        }
-
-        if (numberOfBALL == 0) {
-            System.out.println(numberOfSTRIKE + "스트라이크");
-            return;
-        }
-
-        if (numberOfSTRIKE == 0) {
-            System.out.println(numberOfBALL + "볼");
-            return;
-        }
-
-        System.out.println(numberOfSTRIKE + "스트라이크 " + numberOfBALL + "볼");
     }
 }
---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,69 @@
+package baseball;
+
+import java.util.*;
+
+import static baseball.CompareNumber.compare;
+import static baseball.Score.BALL;
+import static baseball.Score.STRIKE;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Game {
+    public static void play(List<String> computerNumber) {
+        Map<Score, Integer> score = new HashMap<>(3);
+        initializeScore(score);
+
+        while (score.get(STRIKE) != 3) {
+            List<String> userInput = new ArrayList<>(Arrays.asList(readLine().split("")));
+            if (!isValidLength(userInput) || !isExistOnlyNumber(userInput)) {
+                throw new IllegalArgumentException();
+            }
+
+            compare(computerNumber, userInput, score);
+
+            printResult(score);
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    private static void initializeScore(Map<Score, Integer> score) {
+        for (Score name : Score.values()) {
+            score.put(name, 0);
+        }
+    }
+
+    private static void printResult(Map<Score, Integer> score) {
+        int numberOfSTRIKE = score.get(STRIKE);
+        int numberOfBALL = score.get(BALL);
+
+        if (numberOfSTRIKE == 0 && numberOfBALL == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+
+        if (numberOfBALL == 0) {
+            System.out.println(numberOfSTRIKE + "스트라이크");
+            return;
+        }
+
+        if (numberOfSTRIKE == 0) {
+            System.out.println(numberOfBALL + "볼");
+            return;
+        }
+
+        System.out.println(numberOfSTRIKE + "스트라이크 " + numberOfBALL + "볼");
+    }
+
+    private static boolean isValidLength(List<String> userInput) {
+        return userInput.size() == 3;
+    }
+
+    private static boolean isExistOnlyNumber(List<String> userInput) {
+        boolean isOnlyNumber = false;
+
+        for (String element : userInput) {
+            isOnlyNumber = isOnlyNumber || Character.isDigit(element.charAt(0));
+        }
+        return isOnlyNumber;
+    }
+}
