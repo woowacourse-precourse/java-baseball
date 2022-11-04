c7c55dcb47045943febf965cd8795b65bb3f03bb
Devtist <yt930310@snu.ac.kr>
2022-11-04 23:50:00 +0900

    feat: 유효하지 않은 글자를 입력하는 예외 처리 기능 구현

--- src/main/java/User/Validator.java
+++ src/main/java/User/Validator.java
@@ -1,14 +1,24 @@
 package User;
 
+import static baseball.Constant.*;
+
 import java.util.Arrays;
 
 public class Validator {
 	public static void validateNumberInput(String numberInput) {
 		existDuplicateNumber(numberInput);
+		existInvalidLetter(numberInput);
 	}
 
 	private static void existDuplicateNumber(String numberInput) throws IllegalArgumentException {
 		if (Arrays.stream(numberInput.split("")).distinct().count() != numberInput.length()) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private static void existInvalidLetter(String numberInput) throws IllegalArgumentException {
+		if (Arrays.stream(numberInput.split(""))
+			.anyMatch(letter -> !VALID_NUMBERS.contains(Integer.parseInt(letter)))) {
 			throw new IllegalArgumentException();
 		}
 	}
