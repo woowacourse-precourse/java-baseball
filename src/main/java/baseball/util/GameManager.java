62ad037259722eb8907e8317856fdf97afb8e9e8
woo-chang <clllickme@naver.com>
2022-11-04 15:00:41 +0900

    feat: 플레이어 입력에 의해 게임이 재실행되거나 종료되는 기능 구현
    - 플레이어 입력 검증 추가 예정
    - 게임을 멤버로 가지도록 생성자 구현 예정
    - 게임 재실행 상태를 가지는 상수 추가 예정

---
+++ src/main/java/baseball/util/GameManager.java
@@ -0,0 +1,13 @@
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameManager {
+
+    public void execute() {
+        do {
+
+        } while (Integer.parseInt(Console.readLine()) == 1);
+    }
+
+}
