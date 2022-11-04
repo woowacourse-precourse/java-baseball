2809439a6c32d5e95a40de0808ba238a503b3591
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-05 02:10:49 +0900

    feat : 컴퓨터가 생성한 숫자 객체, BallNumber 객체 비교를 위한 equals 메서드 추가 - 자동으로 랜덤의 3자리의 야구 숫자 생성하는 메서드

--- src/main/java/baseball/BallNumber.java
+++ src/main/java/baseball/BallNumber.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.Objects;
+
 public class BallNumber {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
@@ -15,5 +17,18 @@ public class BallNumber {
         if (number < MIN_NUMBER || number > MAX_NUMBER) {
             throw new IllegalArgumentException(NUMBER_OUT_OF_RANGE);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BallNumber that = (BallNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
---
+++ src/main/java/baseball/GameNumber.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameNumber {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int GAME_NUMBER_SET_SIZE = 3;
+    private final List<BallNumber> gameNumberSet;
+
+    public GameNumber(List<BallNumber> gameNumberSet) {
+        this.gameNumberSet = gameNumberSet;
+    }
+
+    public static List<BallNumber> generateRandomNumberSet() {
+        List<BallNumber> numberSet = new ArrayList<>();
+        while (numberSet.size() < GAME_NUMBER_SET_SIZE) {
+            BallNumber randomNumber = new BallNumber(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
+            if (!numberSet.contains(randomNumber)) {
+                numberSet.add(randomNumber);
+            }
+        }
+        return numberSet;
+    }
+}
