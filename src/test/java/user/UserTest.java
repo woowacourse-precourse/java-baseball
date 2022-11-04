ca9af6792e2ec2013b4babbe141e3867b212f39b
jeongwle <dldl4568@naver.com>
2022-11-04 15:35:19 +0900

    test(user) : 사용자입력이 유효하지 않을 경우 테스트
    사용자 입력이 유효하지 않은 경우 예외를 발생시키는 지 테스트
     - 숫자가 아닌 경우 테스트
     - 세자리가 아닌 경우 테스트
     - 서로다른 숫자가 아닌 경우 테스트

--- src/test/java/user/UserTest.java
+++ src/test/java/user/UserTest.java
@@ -1,2 +1,43 @@
-package user;public class UserTest {
+package user;
+
+import static org.assertj.core.api.Assertions.*;
+
+import baseball.User;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    private final User user = new User();
+
+    @Test
+    void 입력된_값은_세자리이어야_합니다() {
+        String testInput = "1234";
+        InputStream in = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(user::receiveThreeNumber)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력된_값은_숫자이어야_합니다() {
+        String testInput = "1a3";
+        InputStream in = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(user::receiveThreeNumber)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 입력된_값은_서로_다른_숫자이어야_합니다() {
+        String testInput = "122";
+        InputStream in = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(user::receiveThreeNumber)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
