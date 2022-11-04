b5388adccd678085b268bb79a47b0253ead87522
kim0914 <kosa0914@naver.com>
2022-11-04 21:36:09 +0900

    feat(NumberValidator): 입력 숫자에서 각 자리 수 중복 예외 발생 기능 추가

--- src/main/java/validator/NumberValidator.java
+++ src/main/java/validator/NumberValidator.java
@@ -9,11 +9,24 @@ public class NumberValidator {
 
     public static void validate(String checkNumber){
         checkSize(checkNumber);
+        checkUnique(checkNumber);
     }
 
-    private static void checkSize(String checkNumber) throws IllegalArgumentException {
+    public static void checkSize(String checkNumber) throws IllegalArgumentException {
         if (checkNumber.length() != NUMBER_LENGTH) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("입력 숫자의 크기(길이)를 확인하세요.");
+        }
+    }
+
+    public static void checkUnique(String checkNumber) throws IllegalArgumentException {
+        List<Character> characterList = new ArrayList<>();
+
+        for (int i = 0; i < checkNumber.length(); i++) {
+            char currentChar = checkNumber.charAt(i);
+            if (characterList.contains(currentChar)) {
+                throw new IllegalArgumentException("각 자리수는 중복을 허락하지 않습니다.");
+            }
+            characterList.add(currentChar);
         }
     }
 }
