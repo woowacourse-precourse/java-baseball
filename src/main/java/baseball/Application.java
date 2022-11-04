c719a2cf75823519ca43548d7148f13a1d389c3b
Mingeun Park <mindal99546@gmail.com>
2022-11-04 22:18:20 +0900

    feat(전체 애플리케이션 완성): main 함수 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,30 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameContext gameContext = new GameContext();
+        gameContext.initializeContext();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while(!gameContext.gameShouldStop()) {
+
+            switch (gameContext.getState()) {
+                case RUNNING:
+                    System.out.print("숫자를 입력해주세요 : ");
+                    gameContext.handleUserInput();
+                    break;
+                case REPLAY:
+                    gameContext.initializeContext();
+                    break;
+                case THREE_STRIKE:
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    gameContext.handleUserInput();
+                    break;
+            }
+
+        }
+
+        System.out.println("게임 종료.");
+
     }
 }
