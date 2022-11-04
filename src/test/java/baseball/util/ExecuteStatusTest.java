acdfc25af7495d795171c6874b7cde00a6d23da2
woo-chang <clllickme@naver.com>
2022-11-04 21:53:13 +0900

    test: 게임 실행/종료 관리 상수 생성 테스트

---
+++ src/test/java/baseball/util/ExecuteStatusTest.java
@@ -0,0 +1,35 @@
+package baseball.util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ExecuteStatusTest {
+
+    @DisplayName("존재하는 실행상태는 알맞은 실행상태를 생성한다.")
+    @Test
+    void generateExecuteStatus() {
+        //given
+        String status = ExecuteStatus.RESTART.getStatus();
+
+        //when
+        ExecuteStatus result = ExecuteStatus.from(status);
+
+        //then
+        assertThat(result).isEqualTo(ExecuteStatus.RESTART);
+    }
+
+    @DisplayName("존재하지 않는 실행상태는 IllegalArgumentException 반환한다.")
+    @Test
+    void generateExecuteStatusFail() {
+        //given
+        String status = "@!";
+
+        //then
+        assertThatThrownBy(() -> ExecuteStatus.from(status))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
