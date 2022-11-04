13248ff8b2fd7947ad14e40d6f3bcc65e730b63c
glass <glass7312@gmail.com>
2022-11-04 14:14:19 +0900

    객체 생성:싱글턴 패턴적용 Computer,User 클래스 getter,setter 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,9 +7,5 @@ import domain.User;
 public class Application {
     public static void main(String[] args) {
         game.start();
-        Computer computer=Computer.getInstance();
-        User user= User.getInstance();
-        computer.setNumber();
-
     }
 }
