fd0a73264ed7309e44bf0cbefbda7518493f3c9a
woo-chang <clllickme@naver.com>
2022-11-04 15:40:13 +0900

    feat: 플레이어의 게임 숫자 각 자리가 정해진 범위에 속하는지 검증하는 기능 구현

--- src/main/java/baseball/util/GameValidation.java
+++ src/main/java/baseball/util/GameValidation.java
@@ -3,6 +3,8 @@ package baseball.util;
 public class GameValidation {
 
     private final static int GAME_NUMBER_LENGTH = 3;
+    private final static char START_RANGE = '1';
+    private final static char END_RANGE = '9';
 
     private GameValidation() {
     }
@@ -14,6 +16,15 @@ public class GameValidation {
     public static boolean validateDigit(String input) {
         return input.chars()
             .allMatch(Character::isDigit);
+    }
+
+    public static boolean validateRange(String input) {
+        return input.chars()
+            .allMatch(GameValidation::inRange);
+    }
+
+    private static boolean inRange(int codePoint) {
+        return START_RANGE <= codePoint && codePoint <= END_RANGE;
     }
 
 }
