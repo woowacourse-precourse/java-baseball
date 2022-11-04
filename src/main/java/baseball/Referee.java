56971d8a897e937df4e46ca8488f86f7c62aa9e2
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-04 15:22:15 +0900

    refactor(Referee): getNumberofStrikes 메서드 리팩토링 외 2건 수정
    주요 변경 사항:
    - getNumberOfStrikes 메서드 에서 addNumberOfStrikes 메서드 분리
    - getNumberOfBalss 메서드에서 스트라이크 갯수를 미리 제거한 후 리턴
    - 매직 넘버 삭제 (private static final int PLAYING_NUMBER_SIZE 추가)

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -5,6 +5,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Referee {
+	private static final int PLAYING_NUMBER_SIZE = 3;
 	private final List<Integer> pitcherPlayingNumbers;
 	private final List<Integer> batterPlayingNumbers;
 
@@ -22,42 +23,51 @@ public class Referee {
 		if (pitcherPlayingNumbers.equals(batterPlayingNumbers)) {
 			return "3스트라이크";
 		}
-		// 볼 개수 메서드
-		int numberOfBalls = getNumberOfBalls();
 		// 스트라이크 개수 메서드
 		int numberOfStrikes = getNumberOfStrikes();
+		// 볼 개수 메서드
+		int numberOfBalls = getNumberOfBalls(numberOfStrikes);
 		//리턴 볼이 0일때, 스트라이크가 0일때 낫싱일때
 		return judgeResult(numberOfBalls, numberOfStrikes);
 	}
 
 	private int getNumberOfStrikes() {
 		int numberOfStrikes = 0;
-		for (int i = 0; i < 3; i++) {
-			if (pitcherPlayingNumbers.get(i).equals(batterPlayingNumbers.get(i))) {
-				numberOfStrikes++;
-			}
+		for (int i = 0; i < PLAYING_NUMBER_SIZE; i++) {
+			numberOfStrikes += addNumberOfStrikes(i);
 		}
 		return numberOfStrikes;
 	}
 
-	private int getNumberOfBalls() {
+	private int addNumberOfStrikes(int i) {
+		if (pitcherPlayingNumbers.get(i).equals(batterPlayingNumbers.get(i))) {
+			return 1;
+		}
+		return 0;
+	}
+
+	private int getNumberOfBalls(int numberOfStrikes) {
 		Set<Integer> ballNumbers = pitcherPlayingNumbers
 			.stream()
 			.filter(batterPlayingNumbers::contains)
 			.collect(Collectors.toSet());
-		return ballNumbers.size();
+
+		if (ballNumbers.size() < numberOfStrikes) {
+			return 0;
+		}
+		return ballNumbers.size() - numberOfStrikes;
 	}
 
 	private String judgeResult(int numberOfBalls, int numberOfStrikes) {
-		if (numberOfBalls - numberOfStrikes == 0 && numberOfStrikes == 0) {
+		if (numberOfBalls == 0 && numberOfStrikes == 0) {
 			return "낫싱";
 		}
-		if (numberOfBalls - numberOfStrikes == 0) {
+		if (numberOfBalls == 0) {
 			return String.format("%d스트라이크", numberOfStrikes);
 		}
 		if (numberOfStrikes == 0) {
 			return String.format("%d볼", numberOfBalls);
 		}
-		return String.format("%d볼 %d스트라이크", (numberOfBalls - numberOfStrikes), numberOfStrikes);
+		return String.format("%d볼 %d스트라이크", numberOfBalls, numberOfStrikes);
 	}
 }
