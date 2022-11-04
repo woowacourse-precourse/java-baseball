782228977a271da1410ef37d824d9e605f141d25
wkdghdwns199 <35947667+wkdghdwns199@users.noreply.github.com>
2022-11-04 14:09:21 +0900

    7. 1을 입력 받으면 2. 에서 6. 까지의 과정을 다시 반복, 2을 입력 받으면 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,86 +10,92 @@ public class Application {
         // TODO: 프로그램 구현
         
         //프로그램 종료 의사를 확인하는 변수
-        int endTheProcess =0; 
+        int endTheProcess =1;
         
         // TODO : 1. 숫자 야구 게임 시작 문구 출력
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        // TODO : 2. 컴퓨터가 가지고 있는 정답 문자열을 랜덤으로 지정
-        // TODO : (각 자리마다 숫자가 다르게)
-        String computer="";
-        while(computer.length() < 3){
-            int randomNumber = Randoms.pickNumberInRange(0,9);
-            if (!computer.contains(Integer.toString(randomNumber))){
-                computer += randomNumber;
+        // TODO  : 7. 1을 입력 받으면 2. 에서 6. 까지의 과정을 다시 반복, 2을 입력 받으면 종료
+        while (endTheProcess==1){
+
+            // TODO : 2. 컴퓨터가 가지고 있는 정답 문자열을 랜덤으로 지정
+            // TODO : (각 자리마다 숫자가 다르게)
+            String computer="";
+            while(computer.length() < 3){
+                int randomNumber = Randoms.pickNumberInRange(0,9);
+                if (!computer.contains(Integer.toString(randomNumber))){
+                    computer += randomNumber;
+                }
             }
+
+            // TODO : 5. 답을 맞추지 못하면 3. 다시 실행, 아니면 반복 멈춤
+
+            // 사용자가 맞출 때 까지의 무한 반복
+            while (true){
+
+                // TODO : 3. 사용자가 도전할 숫자를 입력
+                System.out.print("숫자를 입력해주세요 : ");
+
+                //사용자가 입력하는 숫자에 대한 문자열
+                String guessingNumberString = Console.readLine();
+
+                if (guessingNumberString.length() != 3)
+                    throw new IllegalArgumentException();
+
+
+
+
+                // TODO : 4. 컴퓨터의 정답과 사용자가 입력한 숫자를 비교 후 결과 출력
+                int strike=0;
+                int ball=0;
+
+                char[] guessingNumberArray = guessingNumberString.toCharArray();
+
+                int indexOfFindingNumber = 0;
+                for (Character findingNumber : guessingNumberArray){
+                    //strike, ball 인지 판단하는 함수
+                    String checkResult = checkIfStrikeOrBall(computer, findingNumber.toString(), indexOfFindingNumber);
+                    indexOfFindingNumber++;
+                    if (checkResult.equals("strike")){
+                        strike++;
+                    }
+                    else if (checkResult.equals("ball")){
+                        ball++;
+                    }
+                }
+
+
+                if (strike == 0 && ball == 0){
+                    System.out.println("낫싱");
+                }
+                // TODO : 5. 답을 맞추지 못하면  3. 다시 실행, 아니면 반복 멈춤
+                else if (strike == 3){
+                    System.out.println(strike + "스트라이크");
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    break;
+                }
+                else{
+                    if (ball != 0){
+                        System.out.print(ball + "볼 ");
+                    }
+                    if (strike != 0){
+                        System.out.print(strike + "스트라이크");
+                    }
+                    System.out.println();
+                }
+            }
+
+            //사용자가 입력을 잘못해서 반복이 멈춘 거라면 반복 멈춤
+            if (endTheProcess==2) break;
+
+
+            //TODO : 6. 다른 게임을 진행할 지 , 아니면 프로그램을 멈출 지 1 나 2 입력 받기
+            if (Console.readLine().equals("1")) endTheProcess=1;
+            else endTheProcess=2;
+
         }
 
-        // TODO : 5. 답을 맞추지 못하면 3. 다시 실행, 아니면 반복 멈춤
-
-        // 사용자가 맞출 때 까지의 무한 반복
-        while (true){
-
-            // TODO : 3. 사용자가 도전할 숫자를 입력
-            System.out.print("숫자를 입력해주세요 : ");
-
-            //사용자가 입력하는 숫자에 대한 문자열
-            String guessingNumberString;
-
-            try{
-                guessingNumberString = Console.readLine();
-            }
-            catch(IllegalArgumentException e){
-                endTheProcess=1;
-                break;
-            }
-
-
-            // TODO : 4. 컴퓨터의 정답과 사용자가 입력한 숫자를 비교 후 결과 출력
-            int strike=0;
-            int ball=0;
-
-            char[] guessingNumberArray = guessingNumberString.toCharArray();
-
-            int indexOfFindingNumber = 0;
-            for (Character findingNumber : guessingNumberArray){
-                //strike, ball 인지 판단하는 함수
-                String checkResult = checkIfStrikeOrBall(computer, findingNumber.toString(), indexOfFindingNumber);
-                indexOfFindingNumber++;
-                if (checkResult.equals("strike")){
-                    strike++;
-                }
-                else if (checkResult.equals("ball")){
-                    ball++;
-                }
-            }
-
-
-            if (strike == 0 && ball == 0){
-                System.out.println("낫싱");
-            }
-            // TODO : 5. 답을 맞추지 못하면  3. 다시 실행, 아니면 반복 멈춤
-            else if (strike == 3){
-                System.out.println(strike + "스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                break;
-            }
-            else{
-                if (ball != 0){
-                    System.out.print(ball + "볼 ");
-                }
-                if (strike != 0){
-                    System.out.print(strike + "스트라이크");
-                }
-                System.out.println();
-            }
-        }
-
-
-        //TODO :
-        if (Console.readLine().equals(1)) endTheProcess=0;
-        else endTheProcess=1;
 
     }
 
