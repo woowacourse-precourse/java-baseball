e0b1bacf00c3b6d196371de589c8cea13b92357b
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-05 01:20:35 +0900

    test(selectRandomNumber): validate random number computer selected

---
+++ src/test/java/baseball/ComputerTest.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ComputerTest {
+
+    @Test
+    void selectRandomNumber() {
+        // given
+        Computer computer = new Computer();
+
+        // when
+        String randomNumber = computer.selectRandomNumber();
+        Set<String> set = Arrays.stream(randomNumber.split("")).collect(Collectors.toSet());
+
+        // then
+        assertThat(set.size()).isEqualTo(3);
+        assertThat(randomNumber.matches("[1-9]+")).isTrue();
+    }
+}
