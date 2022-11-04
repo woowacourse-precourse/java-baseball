43dba68df0b5f549b00be60a7273102984b41638
hyeongcheolkim <tori1753@naver.com>
2022-11-04 21:49:40 +0900

    feat: ConsoleReader
    사용자가 콘솔에 작성한 내용을 Integer로 파싱하는 클래스

---
+++ src/main/java/baseball/ConsoleReader.java
@@ -0,0 +1,9 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class ConsoleReader {
+    public static Integer readInteger(){
+        return Integer.parseInt(Console.readLine());
+    }
+}
