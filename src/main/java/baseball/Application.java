08d0389de91b752d6f7aa39d64243193446bcfdf
limzzum <chfhddl1234@naver.com>
2022-11-04 18:59:31 +0900

    feat : 랜덤숫자 3개 선택 함수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
+    static List<Integer> targetNum = new ArrayList<>();
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        createRandomTargetNum();
+    }
+
+    public static void createRandomTargetNum() {
+        while (targetNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!targetNum.contains(randomNumber)) {
+                targetNum.add(randomNumber);
+            }
+        }
     }
 }
