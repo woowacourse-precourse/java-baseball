9d5d163cbbb7ad96b03979dce12ed3babc3b5a39
kjb95 <topmedia44@gmail.com>
2022-11-04 19:43:45 +0900

    test: 정상적인 숫자를 입력받고 파싱하는 기능에 대한 테스트 코드 작성

---
+++ src/main/java/baseball/model/PlayerNumber.java
@@ -0,0 +1,16 @@
+package baseball.model;
+
+import java.util.List;
+
+public class PlayerNumber {
+
+    private final List<Integer> nums;
+
+    public PlayerNumber(String input) {
+        nums = null;
+    }
+
+    public List<Integer> get() {
+        return nums;
+    }
+}
