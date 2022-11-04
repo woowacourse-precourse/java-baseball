28c0bf3c70891ad111a708f5ca69d006a2175802
hyeongcheolkim <tori1753@naver.com>
2022-11-04 22:30:16 +0900

    feat: 스코어 결과 출력
    스코어 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,17 +3,29 @@ package baseball;
 import java.util.HashMap;
 import java.util.Map;
 
-import static baseball.Application.StringConst.BALL;
-import static baseball.Application.StringConst.STRIKE;
+import static baseball.Application.StringConst.*;
 
 public class Application {
     public static void main(String[] args) {
     }
 
+    public static void printScore(Map<String, Integer> score) {
+        Integer ballCount = score.get(BALL);
+        Integer strikeCount = score.get(STRIKE);
+
+        if (!ballCount.equals(0))
+            System.out.print(ballCount + BALL);
+        if (!strikeCount.equals(0))
+            System.out.print(" " + strikeCount + STRIKE);
+        if (ballCount.equals(0) && strikeCount.equals(0))
+            System.out.print(NOTHING);
+        System.out.print("\n");
+    }
+
     public static Map<String, Integer> score(Integer computerNumber, Integer userNumber) {
         Map<String, Integer> ret = new HashMap<>();
-        ret.put(STRIKE , 0);
-        ret.put(BALL , 0);
+        ret.put(STRIKE, 0);
+        ret.put(BALL, 0);
         String computerNumberStringType = String.valueOf(computerNumber);
         char[] computerPlaceValues = computerNumberStringType.toCharArray();
         char[] userPlaceValues = String.valueOf(userNumber).toCharArray();
@@ -31,7 +43,8 @@ public class Application {
     }
 
     interface StringConst {
-        String BALL = "ball";
-        String STRIKE = "strike";
+        String BALL = "볼";
+        String STRIKE = "스트라이크";
+        String NOTHING = "낫싱";
     }
 }
