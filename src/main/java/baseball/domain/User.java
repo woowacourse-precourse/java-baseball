7bbeba74523d0aaa89f9b8d2166e19fbdda89868
slbin-park <smpts00@naver.com>
2022-11-04 23:38:16 +0900

    refactor(User) : 출력관련 함수들 User에서 InputUtil로 이동
    - checkIsDigit 함수 이동
    - checkDuplicationNumber 함수 이동
    - checkValidNumber 함수 이동
    - checkLength 함수 이동
    - charToInt 함수 이동

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -5,12 +5,9 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 
 public class User {
-    private static final String DUPLICATION_EXCEPTION = "중복된 숫자가 존재합니다.";
-    private static final String VALID_NUMBER_EXCEPTION = "가능하지 않은 숫자 입니다.";
-    private static final String NUMBER_LENGTH_EXCEPTION = "글자의 길이가 초과되었습니다.";
+
     private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
-    private static final int MIN_NUM = 1;
-    private static final int MAX_NUM = 9;
+
     public ArrayList<Integer> userNumbers;
 
     public void inputUserNumbers() {
@@ -34,26 +31,4 @@ public class User {
     }
 
 
-
-    public void checkDuplicationNumber(ArrayList<Integer> numbers, int number) {
-        if (numbers.contains(number)) {
-            throw new IllegalArgumentException(DUPLICATION_EXCEPTION);
-        }
-    }
-
-    public void checkValidNumber(int number) {
-        if (number < MIN_NUM || number > MAX_NUM) {
-            throw new IllegalArgumentException(VALID_NUMBER_EXCEPTION);
-        }
-    }
-
-    public void checkLength(String number) {
-        if (number.length() > 3) {
-            throw new IllegalArgumentException(NUMBER_LENGTH_EXCEPTION);
-        }
-    }
-
-    public int charToInt(char number) {
-        return Integer.parseInt(String.valueOf(number));
-    }
 }
--- src/main/java/baseball/utils/InputUtil.java
+++ src/main/java/baseball/utils/InputUtil.java
@@ -1,13 +1,43 @@
 package baseball.utils;
 
+import java.util.ArrayList;
+
 public class InputUtil {
     private static final String ISDIGIT_EXCEPTION = "숫자가 아닙니다.";
+    private static final String DUPLICATION_EXCEPTION = "중복된 숫자가 존재합니다.";
+    private static final String VALID_NUMBER_EXCEPTION = "가능하지 않은 숫자 입니다.";
+    private static final String NUMBER_LENGTH_EXCEPTION = "글자의 길이가 초과되었습니다.";
+    private static final int MIN_NUM = 1;
+    private static final int MAX_NUM = 9;
 
-    public InputUtil(){
+    public InputUtil() {
     }
+
     public void checkIsDigit(char number) {
         if (!Character.isDigit(number)) {
             throw new IllegalArgumentException(ISDIGIT_EXCEPTION);
         }
+    }
+
+    public void checkDuplicationNumber(ArrayList<Integer> numbers, int number) {
+        if (numbers.contains(number)) {
+            throw new IllegalArgumentException(DUPLICATION_EXCEPTION);
+        }
+    }
+
+    public void checkValidNumber(int number) {
+        if (number < MIN_NUM || number > MAX_NUM) {
+            throw new IllegalArgumentException(VALID_NUMBER_EXCEPTION);
+        }
+    }
+
+    public void checkLength(String number) {
+        if (number.length() > 3) {
+            throw new IllegalArgumentException(NUMBER_LENGTH_EXCEPTION);
+        }
+    }
+
+    public int charToInt(char number) {
+        return Integer.parseInt(String.valueOf(number));
     }
 }
