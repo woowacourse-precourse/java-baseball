d3e9a87f180020e09c1ba50a0ac2e4116ec7e981
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-04 11:29:50 +0900

    refactor: BaseballGameLauncher - 메시지들 상수화

--- src/main/java/baseball/BaseballGameLauncher.java
+++ src/main/java/baseball/BaseballGameLauncher.java
@@ -5,10 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class BaseballGameLauncher {
 
     public final static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public final static String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public final static String GAME_CLOSE_MESSAGE = "게임을 완전히 종료합니다.";
+    public final static String INVALID_VALUE_MESSAGE = "올바른 값을 입력해주세요";
 
     public final static String RESTART_GAME = "1";
     public final static String CLOSE_GAME = "2";
-
 
     public void run() {
         System.out.println(GAME_START_MESSAGE);
@@ -16,11 +18,11 @@ public class BaseballGameLauncher {
         do {
             baseballGame.start();
         } while (isRestartGame());
-        System.out.println("게임을 완전히 종료합니다.");
+        System.out.println(GAME_CLOSE_MESSAGE);
     }
 
     private boolean isRestartGame() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(GAME_RESTART_MESSAGE);
 
         String input = Console.readLine();
 
@@ -32,7 +34,7 @@ public class BaseballGameLauncher {
             return false;
         }
 
-        System.out.println("올바른 값을 입력해주세요");
+        System.out.println(INVALID_VALUE_MESSAGE);
         return isRestartGame();
     }
 }
