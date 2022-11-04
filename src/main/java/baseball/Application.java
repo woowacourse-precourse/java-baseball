0f74b790cc63deaa85b593971380454e29c417aa
newh08 <newh08@gmail.com>
2022-11-05 00:22:38 +0900

    feat: implement main function
    Implement main function
    detail of function {
    - main: make random number list through Computer class.
     make user instance with random number list.
     call user.main() function.
     repeat it through do-while sentence.
    }

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        User user;
+        do {
+            List<Integer> computerNumberList = new Computer().computerNumberList;
+            user = new User(computerNumberList);
+            user.main();
+        } while (user.keepGame);
     }
 }
+
