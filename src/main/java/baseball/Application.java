3319d6be453c77426ecb5191a155a5da5e841c69
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-04 17:25:12 +0900

    feat<Application>: 게임 시작 기능 구현
    - 게임 객체 생성 및 플레이
    - 검증할 때 발생하는 예외를 잡아서 출력한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.domain.NumberBaseballGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        NumberBaseballGame game = new NumberBaseballGame();
+        try {
+            game.play();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
