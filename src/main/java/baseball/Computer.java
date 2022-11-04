c674d9dfa1ae2a20e792b38916b4b7d7a4a08b23
miffyKing <whqudghk28@gmail.com>
2022-11-04 16:54:21 +0900

    feat: Make Printing methods in Computer Class

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,5 +1,16 @@
 package baseball;
 
 public class Computer {
+  private static String RandonValue;
+
+  private static void printGameStart() {
+    System.out.println("숫자 야구 게임을 시작합니다.");
+  }
+
+  private static void printInputNumber() {
+    System.out.println("숫자를 입력해주세요 : ");
+  }
+
+
 
 }
