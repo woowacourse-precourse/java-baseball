d7ef922b04317399d6335dd5cba3341295c69d59
bsh1603 <bsh1603@github.com>
2022-11-04 12:05:06 +0900

    feat: GameService Service create

---
+++ src/main/java/Service/GameService.java
@@ -0,0 +1,24 @@
+package Service;
+
+import camp.nextstep.edu.missionutils.Console;
+import domain.Game;
+import domain.RandomNumber;
+import domain.UserNumber;
+import domain.UserValidate;
+
+public class GameService {
+    private static final int NUM_SIZE = 3;
+
+    RandomNumber randomNumber = new RandomNumber();
+    UserNumber userNumber = new UserNumber();
+    UserValidate userValidate = new UserValidate();
+    Game game;
+
+    public void set_game(){
+        game = new Game(randomNumber.getRandomNumber());
+    }
+
+    public void start_game(){
+
+    }
+}
