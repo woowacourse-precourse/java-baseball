62c55e4db54b5fef86893c29ca9eaebed8201095
ksh-beginner <ohk9134@naver.com>
2022-11-04 13:50:30 +0900

    feat(NumberBaseballGameController) : 게임 스타트 전 상대방 생성 기능 구현

--- src/main/java/baseball/controller/NumberBaseballGameController.java
+++ src/main/java/baseball/controller/NumberBaseballGameController.java
@@ -1,5 +1,17 @@
 package baseball.controller;
 
+import baseball.RandomUtil;
+import baseball.model.OpponentComputer;
+
 public class NumberBaseballGameController {
 
+    private static OpponentComputer opponentComputer;
+
+    public void generateOpponentComputer() {
+        opponentComputer = new OpponentComputer(RandomUtil.createRandomNumber());
+    }
+
+    public OpponentComputer getOpponentComputer() {
+        return opponentComputer;
+    }
 }
