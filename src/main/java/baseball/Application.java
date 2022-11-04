0b2efe4b34f2cc09653c54e78ced653c053f052c
tenta <eunoia3802@gmail.com>
2022-11-04 22:15:06 +0900

    move: 숫자 랜덤 생성 메서드 computer 클래스로 이동

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,24 +1,7 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Application {
     public static void main(String[] args) {
-        List<Integer> answer = createRandomNumber();
-        System.out.println(answer);
-    }
 
-    public static List<Integer> createRandomNumber() {
-        List<Integer> computerNumbers = new ArrayList<>();
-        while (computerNumbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computerNumbers.contains(randomNumber)) {
-                computerNumbers.add(randomNumber);
-            }
-        }
-        return computerNumbers;
     }
 }
