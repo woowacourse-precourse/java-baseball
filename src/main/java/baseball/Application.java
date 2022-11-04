c37b14234be40c363c1d834b81a277914aa96ce8
bsh1603 <bsh1603@github.com>
2022-11-04 15:09:58 +0900

    feat: 메인 Application Controller 연결

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import Controller.GameController;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController controller = new GameController();
+        controller.run();
     }
 }
