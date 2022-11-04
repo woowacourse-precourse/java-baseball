0dc6b97f2a21a4a7fdbd83b061bea310beb3c0da
이정우 <acg6138@naver.com>
2022-11-04 13:38:09 +0900

    feat(ComputerNumGenerator): 컴퓨터 숫자 생성 기능 구현

---
+++ src/main/java/baseball/ComputerNumGenerator.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerNumGenerator {
+    private final List<Integer> NUMS;
+
+    public ComputerNumGenerator() {
+        NUMS = generate();
+    }
+
+    public List<Integer> generate() {
+        List<Integer> nums = new ArrayList<>();
+        while (nums.size() != 3) {
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if (!nums.contains(randomNum)) {
+                nums.add(randomNum);
+            }
+        }
+        return nums;
+    }
+}
