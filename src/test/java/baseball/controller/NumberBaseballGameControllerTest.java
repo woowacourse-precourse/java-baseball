2a30838d498d50e7363330fcb5cabbe2ce3e6b3f
ksh-beginner <ohk9134@naver.com>
2022-11-04 13:50:04 +0900

    test(NumberBaseballGameControllerTest) : 게임 스타트 전 상대방 생성 테스트

--- src/test/java/baseball/controller/NumberBaseballGameControllerTest.java
+++ src/test/java/baseball/controller/NumberBaseballGameControllerTest.java
@@ -1,5 +1,17 @@
 package baseball.controller;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
 public class NumberBaseballGameControllerTest {
 
+    @Test
+    void 게임_스타트_전_상대방_생성() {
+        NumberBaseballGameController numberBaseballGameController = new NumberBaseballGameController();
+        numberBaseballGameController.generateOpponentComputer();
+        if (numberBaseballGameController.getOpponentComputer() == null) {
+            fail();
+        }
+    }
 }
