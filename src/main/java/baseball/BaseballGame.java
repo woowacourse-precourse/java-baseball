42bb2eeb20ef1f83240401a7aa2d223a05b3998c
stk346 <stk346@gamil.com>
2022-11-05 00:44:13 +0900

    feat: 예외케이스 구현완료.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -128,11 +128,39 @@ public class BaseballGame {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-        String userInput = Console.readLine();
-        String isRestart = userInput;
+        String isRestart = raiseErrorWhenInputIsNot1Or2();
         return isRestart;
     }
 
+    private String raiseErrorWhenInputIsNot1Or2() throws IllegalArgumentException {
+        String userInput = Console.readLine();
 
+        try {
+            if (!(userInput.equals("1") || userInput.equals("2"))) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println("1과 2만 입력할 수 있습니다.");
+            return raiseErrorWhenInputIsNot1Or2();
+        }
+        return userInput;
+    }
 
+    private void raiseErrorWhenInputIsNotDigitOrZero(char separatedInput) throws IllegalArgumentException {
+        if (!Character.isDigit(separatedInput)) {
+            System.out.println("숫자만 입력해주세요.");
+            throw new IllegalArgumentException();
+        }
+        if (separatedInput == '0') {
+            System.out.println("1과 9사이의 숫자만 입력해주세요.");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void raiseErrorWhenInputLengthOver3(String userInput) throws IllegalArgumentException {
+        if (userInput.length() > 3) {
+            System.out.println("입력은 세자리 숫자만 허용됩니다.");
+            throw new IllegalArgumentException();
+        }
+    }
 }
