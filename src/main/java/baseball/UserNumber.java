7a12729b0e38f4c5259480aacfb2a1af15199741
Eunkeee <eunkeeeee@gmail.com>
2022-11-04 15:55:50 +0900

    feat: 세 자리 자연수가 아닌 입력값 예외 처리

--- src/main/java/baseball/UserNumber.java
+++ src/main/java/baseball/UserNumber.java
@@ -1,33 +1,54 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserNumber {
-
+    String REGEX = "[0-9]{3,3}";
     int userNumber;
 
     public UserNumber() {}
-    public UserNumber(int userNumber) {
-        // validation
-        this.userNumber = userNumber;
-    }
 
     public void setUserNumber() {
-        int userNumberStr = Integer.parseInt(Console.readLine());
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+
         // validation
-        if(false) {
+        Boolean checkLength = input.length() == 3;
+        System.out.println("CheckLength :" + checkLength);
+
+        // 세 자리 자연수인지 확인
+        Boolean checkType = Pattern.matches(REGEX, input);
+        System.out.println("CheckType :" + checkType);
+
+        Boolean validation = checkLength && checkType;
+        if(!validation) {
             throw new IllegalArgumentException();
         }
-        this.userNumber = userNumber;
+
+        this.userNumber = Integer.parseInt(input);
 
     }
 
     // override method for test
     public void setUserNumber(String input) {
+
         // validation
-        if(false) {
+
+        // 세 자리인지 확인
+        Boolean checkLength = input.length() == 3;
+        System.out.println("CHECK1 :" + checkLength);
+
+        // 세 자리 자연수인지 확인
+        Boolean checkType = Pattern.matches(REGEX, input);
+        System.out.println("CHECK2 :" + checkType);
+
+        Boolean validation = checkLength && checkType;
+        if(!validation) {
             throw new IllegalArgumentException();
         }
+
         this.userNumber = Integer.parseInt(input);
     }
 
--- src/test/java/baseball/UserNumberTest.java
+++ src/test/java/baseball/UserNumberTest.java
@@ -1,15 +1,29 @@
 package baseball;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class UserNumberTest {
 
     @Test
-    void isUserNumberSetByTest() {
-        UserNumber user = new UserNumber(123);
+    void canSetUserNumberByTest() {
+        UserNumber user = new UserNumber();
+        user.setUserNumber("123");
         System.out.println(user.userNumber);
+    }
+    @Test
+    void isUserNumberLengthEqualsThree() {
+        UserNumber user = new UserNumber();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            user.setUserNumber("1234");
+        });
+    }
+    @Test
+    void isUserNumberNotString() {
+        UserNumber user = new UserNumber();
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            user.setUserNumber("aaa");
+        });
     }
 
 }
