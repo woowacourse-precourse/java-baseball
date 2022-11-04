4800b6815a857eb7bfa505c352a7912d858924c7
JDK <ehdrjf2062@gmail.com>
2022-11-04 10:51:14 +0900

    Feat: 컴퓨터의 정답과 사용자의 입력 하나씩 검사

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Application {
     public static void main(String[] args) {
@@ -19,7 +21,7 @@ public class Application {
             ArrayList<Integer> inputList = stringToList(inputStr);
 
             // 로직
-//            List<Hint> hints = ComputerList.compareTo(inputList);
+            Map<Hint, Integer> hintCountMap = ComputerList.compare(inputList);
 
             // 출력
         }
@@ -51,14 +53,51 @@ public class Application {
 
 class ComputerList {
     private static List<Integer> list;
+    private static Map<Hint, Integer> hintCountMap;
 
     private ComputerList() {
     }
 
     public static void initialize() {
         list = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        generateHintMap();
     }
 
+    public static Map<Hint, Integer> compare(ArrayList<Integer> inputList) {
+        validateList(inputList);
+        for (int index = 0; index < inputList.size(); index++) {
+            eachValueMatching(index, list.get(index));
+        }
+        return hintCountMap;
+    }
+
+    private static void eachValueMatching(int index, Integer value) {
+        if (list.contains(value) && list.get(index) == value) {
+            Integer before = hintCountMap.get(Hint.STRIKE);
+            hintCountMap.put(Hint.STRIKE, before + 1);
+        } else if (list.contains(value)) {
+            Integer before = hintCountMap.get(Hint.BALL);
+            hintCountMap.put(Hint.BALL, before + 1);
+        } else {
+            Integer before = hintCountMap.get(Hint.MISS);
+            hintCountMap.put(Hint.MISS, before + 1);
+        }
+    }
+
+
+    private static Map<Hint, Integer> generateHintMap() {
+        Map<Hint, Integer> hintCountMap = new HashMap<>();
+        hintCountMap.put(Hint.STRIKE, 0);
+        hintCountMap.put(Hint.BALL, 0);
+        hintCountMap.put(Hint.MISS, 0);
+        return hintCountMap;
+    }
+
+    private static void validateList(ArrayList<Integer> inputList) {
+        if (list == null || inputList == null) {
+            throw new IllegalArgumentException("리스트 값이 유효하지 않습니다.");
+        }
+    }
 }
 
 class Comment {
@@ -66,4 +105,8 @@ class Comment {
     final static String ENDGAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     final static String REGAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     final static String INPUTNUMBER = "숫자를 입력해주세요 : ";
+}
+
+enum Hint {
+    STRIKE, BALL, MISS
 }
