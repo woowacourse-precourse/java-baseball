f3c117c3fb06b82d1f38cc6ef4a0363ab3802789
Gaby <gaby@Gabyui-MacBookPro.local>
2022-11-04 14:29:17 +0900

    fix: use Console.readLine instead of sc.nextLine

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,7 +16,7 @@ public class Application {
     private static String result = null;
     private static Integer strike = 0;
     private static Integer ball = 0;
-    private static Scanner sc = new Scanner(System.in);
+    
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -24,7 +24,7 @@ public class Application {
             // Start game
             start_game();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String input = sc.nextLine();
+            String input = Console.readLine();
             if (input.equals("1"))
                 continue;
             else if (input.equals("2"))
@@ -55,7 +55,7 @@ public class Application {
     private static void read() {
         duplicate = new HashSet<>();
         user = new ArrayList<>();
-        String input = sc.nextLine();
+        String input = Console.readLine();
         if (input.length() != 3)
             invalid();
         for (int i = 0; i < 3; ++i) {
