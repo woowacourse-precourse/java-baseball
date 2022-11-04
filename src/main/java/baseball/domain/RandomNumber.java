5d320a97dcbc6ef80a576fd10e2ad2af71dab7cb
Youngseo-Jeon0313 <20wjsdudtj@gmail.com>
2022-11-04 11:37:26 +0900

    feat(RandomNumber):  받아온 리스트 속 원소들이 모두 다른지 확인하는 함수 isSet 생성
    List를 Set으로 변환한 set 하나 만들고, List속 원소의 수와 Set 속 원소의 수가 같은지 확인

--- src/main/java/baseball/domain/RandomNumber.java
+++ src/main/java/baseball/domain/RandomNumber.java
@@ -1,4 +1,22 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.Math.*;
+
 public class RandomNumber {
+    public static boolean isSet(ArrayList<Integer> numList){
+        Set<Integer> numSet = new HashSet<>(numList);
+        return (numSet.size() == numList.size());
+    }
+
+    public static void main(){
+        //1부터 9까지의 난수 생성
+        ArrayList<Integer> RandomNumList= new ArrayList();
+        for (int i=0; i < 3; i++){
+            int ran_num = (int) floor((random() * 10));
+        }
+    }
 }
