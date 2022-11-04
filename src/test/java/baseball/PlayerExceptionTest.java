4637ffeb829af0c5e832fc84101c597f57c74e47
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-04 22:20:52 +0900

    fix: PlayerExceptionTest의 예외처리 람다 추가

--- src/test/java/baseball/PlayerExceptionTest.java
+++ src/test/java/baseball/PlayerExceptionTest.java
@@ -21,9 +21,9 @@ public class PlayerExceptionTest {
 
     @Test
     void 숫자가_아니면_예외처리(){
-        assertThatThrownBy(playerException.noNumberException())
+        assertThatThrownBy(()->playerException.checkException("abcd"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("입력한 값은 숫자여야 합니다. ");
+                .hasMessageContaining("입력한 값은 세 자리여야 합니다. ");
     }
 
     @Test
@@ -33,7 +33,7 @@ public class PlayerExceptionTest {
 
     @Test
     void 세자리가_아니면_예외처리(){
-        assertThatThrownBy(playerException.notThreeDigitException())
+        assertThatThrownBy(()->playerException.checkException("0123"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력한 값은 세 자리여야 합니다. ");
     }
@@ -45,7 +45,7 @@ public class PlayerExceptionTest {
 
     @Test
     void 숫자0이_있으면_예외처리(){
-        assertThatThrownBy(playerException.containsZeroException())
+        assertThatThrownBy(()->playerException.checkException("012"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력한 값에 0이 없어야 합니다. ");
     }
@@ -57,7 +57,7 @@ public class PlayerExceptionTest {
 
     @Test
     void 같은_숫자가_있으면_예외처리(){
-        assertThatThrownBy(playerException.containsSameNumberException())
+        assertThatThrownBy(()->playerException.checkException("112"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력한 값이 서로 다른 숫자여야 합니다. ");
     }
