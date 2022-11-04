d83e12b00f1e6e5e0a0989243b71de19bcb8ee0e
jh868 <2weeks9947@naver.com>
2022-11-04 20:15:05 +0900

    feat: 겹치지 않는 랜덤 숫자 3개 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        for(int i = 0; i < 1000; i++){
+            System.out.println(RandomBallNum());
+        }
+    }
+
+    public static List<Integer> RandomBallNum() {
+        List<Integer> BallNum = new ArrayList<>(3);
+        while (BallNum.size() < 3) {
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if (!BallNum.contains(randomNum)) {
+                BallNum.add(randomNum);
+            }
+        }
+
+        return BallNum;
     }
 }
