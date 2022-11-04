dcc88cc04aad46a62a2cc47b1a4d2a9cd213f020
csj <koreaxman@naver.com>
2022-11-04 16:56:21 +0900

    feat: print end game notification

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,7 +2,8 @@ package baseball;
 
 public class Game {
     public final static String START_NOTIFICATION = "숫자 야구 게임을 시작합니다.";
-    public final static String END_NOTIFICATION = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public final static String END_NOTIFICATION = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public final static String RESTART_NOTIFICATION = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public final static String BALL = "볼";
     public final static String STRIKE = "스트라이크";
     public final static String NOTHING = "낫싱";
@@ -14,6 +15,7 @@ public class Game {
     public void start() {
         System.out.println(START_NOTIFICATION);
         repeatUntilAnswerCorrect();
+        System.out.println(END_NOTIFICATION);
     }
 
     public boolean isContainAnyNumber() {
