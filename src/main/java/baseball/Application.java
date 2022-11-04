9660853e2b83f33e986066b64b689cf0fb65bf45
sjh910805@gmail.com <sjh910805@gmail.com>
2022-11-04 16:45:37 +0900

    feat: 컴퓨터 클래스 정의, 랜덤 숫자 생성 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // playGame();
+
+    }
+}
+
+class Computer {
+    private int number;
+
+    public void setNumber() {
+        List<Integer> numberList = new ArrayList<>();
+        while (numberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numberList.contains(randomNumber)) {
+                numberList.add(randomNumber);
+            }
+        }
+        int numCanditate = numberList.indexOf(0) * 100 + numberList.indexOf(0) * 10 + numberList.indexOf(0);
+        this.number = numCanditate;
     }
 }
