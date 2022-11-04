c8b860d288dc38b194ca1cad1b39c8dcbfc8ea39
linda2927 <linda2927@naver.com>
2022-11-04 16:29:49 +0900

    feat: add methods and test codes for comparing user input number and game instance's random number

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Game {
     private static final int NUM_START_RANGE = 1;
@@ -29,6 +31,11 @@ public class Game {
                 numbers.add(randomNum);
         }
         return numbers;
+    }
+
+    // for testing purposes
+    void setRandomNum(List<Integer> randomNum) {
+        this.randomNum = randomNum;
     }
 
     /**
@@ -75,6 +82,53 @@ public class Game {
         }
         else
             endGame();
+    }
+
+    /**
+     * Compare user input with game instance's randomNum and calculate result
+     * @param userInput 사용자가 입력한 값
+     * @return resultList 볼과 스트라이크의 개수가 차례대로 담긴 리스트
+     */
+    List<Integer> result(List<Integer> userInput) {
+        List<Integer> resultList = new ArrayList<>(Arrays.asList(0, 0));
+        int ballCount = 0;
+        for (int i = 0; i < this.randomNum.size(); i++) {
+            if (Objects.equals(this.randomNum.get(i), userInput.get(i)))
+                resultList.set(1, resultList.get(1) + 1);
+            else {
+                ballCount += checkBall(userInput.get(i), this.randomNum);
+            }
+        }
+        resultList.set(0, ballCount);
+        return resultList;
+    }
+
+    /**
+     * Check whether certain digit exists in game instance's randomNum
+     * @param digit 사용자가 입력한 값 중 하나
+     * @param answer game 인스턴스의 randomNum
+     * @return 포함하고 있으면 1, 아니면 0
+     */
+    int checkBall(Integer digit, List<Integer> answer) {
+        if(answer.contains(digit))
+            return 1;
+        return 0;
+    }
+
+    void printResult(List<Integer> resultList) {
+        int ball = resultList.get(0);
+        int strike = resultList.get(1);
+        if (ball == 0 && strike == 0)
+            System.out.println("낫싱");
+        else if (ball == 0 || strike == 0) {
+            if (resultList.indexOf(Integer.max(ball, strike)) == 0)
+                System.out.println(ball + "볼");
+            else
+                System.out.println(strike + "스트라이크");
+        }
+        else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
     }
 
     public void start() {}
--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -24,10 +24,6 @@ public class GameTest {
         output.reset();
     }
 
-    /**
-     * 사용자가 입력한 값이 List 형태로 출력되는지 확인
-     * @see InputValidationTest 사용자 입력값 유효성 검사 테스트
-     */
     @Test
     void get_valid_user_input() {
         InputStream in = generateUserInput("123");
@@ -36,10 +32,6 @@ public class GameTest {
         assertThat(Game.getUserInput()).isEqualTo(testInput);
     }
 
-    /**
-     * 사용자가 유효하지 않은 값을 입력했을 때 게임이 종료되는지 확인
-     * @see InputValidationTest 사용자 입력값 유효성 검사 테스트
-     */
     @Test
     void end_game_invalid_input() {
         String compareResult = "게임 종료";
@@ -54,4 +46,39 @@ public class GameTest {
                 .contains(compareResult);
     }
 
+    @Test
+    void judge_result_case1() {
+        List<Integer> userInput = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> randomNum = new ArrayList<>(Arrays.asList(4, 5, 6));
+        List<Integer> result = new ArrayList<>(Arrays.asList(0, 0));
+
+        Game game = new Game();
+        game.setRandomNum(randomNum);
+
+        assertThat(game.result(userInput)).isEqualTo(result);
+    }
+
+    @Test
+    void judge_result_case2() {
+        List<Integer> userInput = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> randomNum = new ArrayList<>(Arrays.asList(1, 3, 6));
+        List<Integer> result = new ArrayList<>(Arrays.asList(1, 1));
+
+        Game game = new Game();
+        game.setRandomNum(randomNum);
+
+        assertThat(game.result(userInput)).isEqualTo(result);
+    }
+
+    @Test
+    void judge_result_case3() {
+        List<Integer> userInput = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> randomNum = new ArrayList<>(Arrays.asList(1, 2, 3));
+        List<Integer> result = new ArrayList<>(Arrays.asList(0, 3));
+
+        Game game = new Game();
+        game.setRandomNum(randomNum);
+
+        assertThat(game.result(userInput)).isEqualTo(result);
+    }
 }
