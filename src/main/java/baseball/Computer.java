7c1f6b44c7c3bcda0841cdc47500d87825394a0f
csj <koreaxman@naver.com>
2022-11-04 14:11:12 +0900

    feat: convert random number list to string

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -4,11 +4,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Computer {
-    static List<Integer> randomNumberList = new ArrayList<>();
+
+    static List<Integer> randomNumberList;
 
     public void pickThreeNumbersRandomly() {
+        randomNumberList = new ArrayList<>();
         while (randomNumberList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
 
@@ -16,5 +19,16 @@ public class Computer {
                 randomNumberList.add(randomNumber);
             }
         }
+        System.out.println(randomNumberList);
+    }
+
+    public String randomNumberListToString() {
+        String randomNumber = "";
+
+        for (Integer item : randomNumberList) {
+            randomNumber += item;
+        }
+
+        return randomNumber;
     }
 }
