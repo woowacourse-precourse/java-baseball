ac470c6d9a5200b3d857691730338a2331cc3fc3
byeongsoon <byeongsoon94@gmail.com>
2022-11-04 21:28:10 +0900

    feat: 사용자 입력값 검증 및 Integer 리스트로 변환 구현

--- src/main/java/baseball/service/BaseballGameService.java
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -5,16 +5,59 @@ import baseball.domain.User;
 import baseball.printer.GameGuidePrinter;
 import baseball.util.RandomNumber;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 
 public class BaseballGameService {
 
     private User user;
     private Computer computer;
+    private int size;
 
 
     public void initGame(int start, int end, int size) {
         computer = new Computer(RandomNumber.getRandomNumbers(start, end, size));
+        this.size = size;
+
         GameGuidePrinter.startGameMessage();
+    }
+
+    private List<Integer> getUserNumbers() {
+        GameGuidePrinter.requestNumberMessage();
+
+        String input = Console.readLine();
+        checkUserInput(input);
+
+        String[] numbers = input.split("");
+        return Arrays.stream(numbers)
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+    }
+
+    private void checkUserInput(String input) throws IllegalArgumentException {
+        checkInputSize(input);
+
+        String[] numbers = input.split("");
+
+        for (String number: numbers) {
+            if (!checkNumberRange(number)) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void checkInputSize(String input) throws IllegalArgumentException {
+        if (input.length() != size) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean checkNumberRange(String number) throws IllegalArgumentException {
+        return '0' <= number.charAt(0) && number.charAt(0) <= '9';
     }
 
 }
