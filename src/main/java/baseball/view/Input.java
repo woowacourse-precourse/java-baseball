afba3b39456865edf969276225d4ba7f9103e796
ykmxxi <ykmxxi97@gmail.com>
2022-11-04 15:05:40 +0900

    feat: 3.사용자의 입력을 처리하는 생성자 구현

---
+++ src/main/java/baseball/view/Input.java
@@ -0,0 +1,17 @@
+package baseball.view;
+
+public class Input {
+    String input;
+
+    public Input() {
+    }
+
+    public Input(String input) {
+        this.input = input;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+}
---
+++ src/main/java/baseball/view/PlayerNumberInput.java
@@ -0,0 +1,16 @@
+package baseball.view;
+
+
+public class PlayerNumberInput extends Input {
+    public PlayerNumberInput() {
+    }
+
+    public PlayerNumberInput(String input) {
+        super(input);
+    }
+
+    @Override
+    public String getInput() {
+        return super.getInput();
+    }
+}
