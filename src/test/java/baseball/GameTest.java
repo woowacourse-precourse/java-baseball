4b19786f4d11534a0ca0c6b3e2d31e8e4884bf8a
박원종 <wonjong1030@naver.com>
2022-11-04 20:14:53 +0900

    test: [기능2] 컴퓨터가 생성한 숫자 조건체크 테스트 추가

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
 class GameTest {
-  
+    @Test
+    void 컴퓨터가_생성한_숫자_조건체크(){
+        Game game = new Game();
+        List<Integer> result = game.createComputerNumber();
+
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.get(0) != result.get(1)).isEqualTo(true);
+        assertThat(result.get(0) != result.get(2)).isEqualTo(true);
+        assertThat(result.get(1) != result.get(2)).isEqualTo(true);
+    }
+
 }
