d9f2e6e1154126c897ac4ccb9d91293f6a34b73d
01192mg <01192mg@gmail.com>
2022-11-04 19:43:55 +0900

    refactor: GameMachine 메소드 분리

--- src/main/java/baseball/GameMachine.java
+++ src/main/java/baseball/GameMachine.java
@@ -9,24 +9,18 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class GameMachine {
-    public static IntInputValidator intInputValidator;
+    private static int strike;
+    private static int ball;
+    private static List<Integer> computer;
+    private static List<Integer> player;
+    private static IntInputValidator intInputValidator;
 
     public static void startBaseballGame() {
-        int strike = 0;
-        int ball;
-        int playerNumberInput;
-        List<Integer> player;
-        List<Integer> computer = RandomThreeDigitNumberGenerator.generate();
         System.out.print("숫자 야구 게임을 시작합니다.");
-
+        initializeGameStatus();
         while (strike != RANDOM_NUMBER_LENGTH) {
             System.out.print("숫자를 입력해주세요 : ");
-            playerNumberInput = StringToIntConvertor.convert(Console.readLine());
-            intInputValidator = new PlayerNumberValidator();
-            intInputValidator.validate(playerNumberInput);
-            player = Arrays.stream(String.valueOf(playerNumberInput).split("")).map(Integer::parseInt)
-                    .collect(Collectors.toList());
-
+            player = getPlayerNumber();
             List<Integer> ballAndStrike = ResultCalculator.calculateBallAndStrike(computer, player);
             ball = ballAndStrike.get(0);
             strike = ballAndStrike.get(1);
@@ -34,11 +28,27 @@ public class GameMachine {
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        if (getContinueOrBreak() == CONTINUE) {
+            GameMachine.startBaseballGame();
+        }
+    }
+
+    private static void initializeGameStatus() {
+        computer = RandomThreeDigitNumberGenerator.generate();
+    }
+
+    private static List<Integer> getPlayerNumber() {
+        int playerNumberInput = StringToIntConvertor.convert(Console.readLine());
+        intInputValidator = new PlayerNumberValidator();
+        intInputValidator.validate(playerNumberInput);
+        return Arrays.stream(String.valueOf(playerNumberInput).split("")).map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    private static int getContinueOrBreak() {
         int continueOrBreak = StringToIntConvertor.convert(Console.readLine());
         intInputValidator = new ContinueOrBreakValidator();
         intInputValidator.validate(continueOrBreak);
-        if (continueOrBreak == CONTINUE) {
-            GameMachine.startBaseballGame();
-        }
+        return continueOrBreak;
     }
 }
