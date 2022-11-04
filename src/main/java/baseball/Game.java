f01cf87fcfc3a5f9ba35cf261c4f10c48f681893
박원종 <wonjong1030@naver.com>
2022-11-04 19:56:16 +0900

    feat: [기능1] 게임 시작 문구 출력 기능 추가

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class Game {
+    static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public void printStartMessage(){
+        System.out.println(START_MESSAGE);
+    }
+}
