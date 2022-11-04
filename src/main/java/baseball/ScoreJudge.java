3e252d2399f4f2d3007059effecd73b58c63f064
YSEO <1park4170@gmail.com>
2022-11-04 16:52:25 +0900

    refactor<compute score> : add three methods, change local variable to class variable, change access modifier
    - change access modifiers of methods that are not referenced outside class into "private"
    - separate method "Initializer", "computeScore", "strikeOrBall"
    - declare variables strike, ball, nothing as class variable to maintain state
    
    Before
    method declaration
    {
     public int searchNumInArray
    }
    After:
    method declaration
    {
    private int searchNumInArray
    }
    add
    {
    Initializer, computeScore, StrikeOrBall method
    }
    variables
    {
    change variables to class variable which was local variable
    like, private int strike, ball, nothing;
    }

--- src/main/java/baseball/ScoreJudge.java
+++ src/main/java/baseball/ScoreJudge.java
@@ -1,42 +1,47 @@
 package baseball;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
 public class ScoreJudge {
-    private CreateRandom random;
     private NumsInputFromUser input;
+    private int strike, ball, nothing;
 
     public ScoreJudge() {
-        this.random = new CreateRandom();
         this.input  = new NumsInputFromUser();
     }
 
-    public String Judge() {
-         int[] randomNums = random.makeRandomNums();
+    public void Initializer() {
+        strike   = 0;
+        ball     = 0;
+        nothing  = 0;
+    }
+
+    public String Judge(int[] randomNums) {
          int[] inputNums = input.getInputNumForGame();
 
-         int strike  = 0;
-         int ball    = 0;
-         int nothing = 0;
-
-         for (int i = 0; i < inputNums.length; i++) {
-             int index = searchNumInArray(randomNums, inputNums[i]);
-             if (index > -1 && i == index) {
-                strike += 1;
-             } else if (index > -1 && i != index) {
-                 ball += 1;
-             }
-         }
+         computeScore(inputNums, randomNums);
 
          if (strike == 0 && ball == 0) nothing += 1;
 
-         String result = resultString(strike, ball, nothing);
+         String result = resultString();
 
          return result;
     }
 
-    public int searchNumInArray(int [] arr, int findNum) {
+    private void computeScore(int[] inputNums, int[] randomNums) {
+        for (int i = 0; i < inputNums.length; i++) {
+            int index = searchNumInArray(randomNums, inputNums[i]);
+            strikeOrBall(i,index);
+        }
+    }
+
+    private void strikeOrBall(int i , int index) {
+        if (index > -1 && i == index) {
+            strike += 1;
+        } else if (index > -1 && i != index) {
+            ball += 1;
+        }
+    }
+
+    private int searchNumInArray(int [] arr, int findNum) {
         for (int j = 0 ; j < 3; j++) {
             if (arr[j] == findNum) {
                 return j;
@@ -45,15 +50,20 @@ public class ScoreJudge {
         return -1;
     }
 
-    public String resultString(int strike, int ball, int nothing) {
-        if (nothing == 1) return "낫싱";
+    private String resultString() {
+        if (this.nothing == 1) return "낫싱";
 
         String result = "";
 
         if (ball > 0) result += Integer.toString(ball) + "볼";
-        if (strike >0) result += Integer.toString(strike) + "스트라이크";
+        if (strike > 0) {
+            if (result.length() != 0) {
+                result += " " + Integer.toString(strike) + "스트라이크";
+            } else {
+                result += Integer.toString(strike) + "스트라이크";
+            }
+        }
 
         return result;
     }
-
 }
