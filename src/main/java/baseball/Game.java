224f714454ee40f4dd05d03f6d6d0039f58866d3
situodys <situodys@gmail.com>
2022-11-05 17:55:47 +0900

    feat(Game): 플레이어 입력에 따른 야구게임 진행 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -10,12 +10,18 @@ public class Game {
         this.computer = new Computer();
     }
 
+    public void start() {
+        do {
+            play();
+        } while (player.wantRestart(Utils.getPlayerInputForRestartGameByConsole()));
+    }
+
     private void play() {
         computer.generateAnswer();
-        do{
+        do {
             player.initMyAnswerBy(Utils.getPlayerInputForGenerateAnswerByConsole());
             result = computer.resultBy(player);
             Utils.announce(result);
-        }while(!result.isEnd());
+        } while (!result.isEnd());
     }
 }
