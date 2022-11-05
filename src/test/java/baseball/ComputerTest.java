8b0e555d2c0c9646275878ffdd7871297f79471e
kim0914 <kosa0914@naver.com>
2022-11-05 19:31:37 +0900

    test(Computer): 게임 시작 시 정답 생성 테스트 추가

---
+++ src/test/java/baseball/ComputerTest.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+class ComputerTest {
+
+    @Test
+    void 컴퓨터가_만드는_정답은_3자리_숫자이다(){
+        // given
+        Computer computer = new Computer();
+        int expectedLength = 3;
+
+        // when
+        computer.generateAnswer();
+
+        // then
+        assertThat(computer.getAnswer().length()).isEqualTo(expectedLength);
+    }
+}
