8120e8cbe7b75aa4886c22153255034d9ce5cffd
whibaek <jibaek0513@gmail.com>
2022-11-05 22:34:24 +0900

    feat(Computer Class): add method
    add countResultString method
    - return String that includes information about strike and ball

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -40,4 +40,15 @@ public class Computer {
             ball += checkBall(userNumber.get(i), i);
         }
     }
+
+    public String countResultString(){
+        if(strike == 0 && ball == 0) {
+            return "낫싱";
+        } else if(ball == 0) {
+            return strike + "스트라이크";
+        } else if(strike == 0) {
+            return ball + "볼";
+        }
+        return ball + "볼 " + strike + "스트라이크";
+    }
 }
