5935bc311c054a1cef120d9e2f384e0694944f33
woosung1223 <dntjd991223@naver.com>
2022-11-05 20:19:27 +0900

    feat(OutputMsgContainer): 클래스 및 상수 선언

---
+++ src/main/java/baseball/OutputMsgContainer.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public final class OutputMsgContainer {
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String SPACE = " ";
+}
