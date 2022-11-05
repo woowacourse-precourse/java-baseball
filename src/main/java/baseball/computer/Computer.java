8e9993e4198161e02be2deaf9b32179f678c35a2
math0917 <math0917@naver.com>
2022-11-05 16:39:07 +0900

    feat: 작성 computer 랜덤 정수 리스트반환 메서드
    random value를 생성하는 일을 담당하는 객체인 Random을 추가하여
    computer는 겹치지 않는 길이 3 리스트를 반환하는 책임에 집중하도록 한다

--- src/main/java/baseball/computer/Computer.java
+++ src/main/java/baseball/computer/Computer.java
@@ -1,9 +1,18 @@
 package baseball.computer;
 
+import baseball.random.Random;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
     public static List<Integer> createThreeLengthIntegerList() {
-        return null;
+        List<Integer> threeLengthNonDuplicateIntegerList = new ArrayList<>();
+        while (threeLengthNonDuplicateIntegerList.size() < 3) {
+            int randomValue = Random.randomIntegerCreatorFromArgs1ToArgs2(1, 9);
+            if (!(threeLengthNonDuplicateIntegerList.contains(randomValue))) {
+                threeLengthNonDuplicateIntegerList.add(randomValue);
+            }
+        }
+        return threeLengthNonDuplicateIntegerList;
     }
 }
