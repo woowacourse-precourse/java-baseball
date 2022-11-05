8fe2b69e69716e692564202f5b8a0851462e9ca6
bellljun3395 <195850@jnu.ac.kr>
2022-11-05 18:35:09 +0900

    comment(Viewable): 추가 Viewable 메서드 주석

--- src/main/java/baseball/view/Viewable.java
+++ src/main/java/baseball/view/Viewable.java
@@ -4,13 +4,29 @@ import java.util.List;
 
 public interface Viewable {
 
+    /**
+     * 힌트를 받아 힌트를 출력하는 메서드이다.
+     * @param hints Gameplayer와 Computer의 수를 비교한 결과 나오는 hints이다.
+     */
     public void printHint(List<Integer> hints);
 
+    /**
+     * 게임 시작 멘트를 출력하는 메서드이다.
+     */
     public void printStart();
 
+    /**
+     * 게임 플레이중 입력을 유도하는 멘트를 출력하는 메서드이다.
+     */
     public void printInput();
 
+    /**
+     * 게임이 종료되었을 때 출력하는 메서드이다.
+     */
     public void printEnd();
 
+    /**
+     * 게임을 계속 진행할 것인지 물어보는 멘트를 출력하는 메서드이다.
+     */
     public void printIsContinue();
 }
