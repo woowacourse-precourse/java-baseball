da005bc3519e50bdc3f2ca831c7c336d8866107f
KimTaeKang57 <zizoen123@gmail.com>
2022-11-05 14:08:06 +0900

    [테스트] 여러가지 실행 경우의수 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,6 +21,29 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 게임종료() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 게임종료_2번_재시작() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("123", "456", "1", "789", "1", "157", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "3스트라이크", "3스트라이크", "게임 종료");
+                },
+                4,5,6,7,8,9,1,5,7
+        );
+    }
+
+
+    @Test
     void 예외_테스트() {
         // 길이가 3 이 아닐 때
         assertSimpleTest(() ->
