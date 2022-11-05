c57131d154e04b3d9e825ada35f1e3eb08ef57c1
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-05 10:41:59 +0900

    style : User -> Player 변수, 메서드 이름 변경

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,7 +3,7 @@ package baseball;
 import static baseball.Computer.NUM_LIMIT_LENGTH;
 import static baseball.Computer.createRandomNumbers;
 import static baseball.Message.*;
-import static baseball.Player.toIntegerUserInput;
+import static baseball.Player.toIntegerPlayerInput;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -29,41 +29,41 @@ public class BaseballGame {
     public void start() {
         System.out.println(START_GAME);
         System.out.print(INPUT_NUM);
-        String userInput = readLine();
-        userNumber = toIntegerUserInput(userInput);
+        String PlayerInput = readLine();
+        userNumber = toIntegerPlayerInput(PlayerInput);
         checkInputData(userNumber);
         strike = checkStrikeCount(userNumber);
         ball = checkBallCount(userNumber);
         gameResult();
     }
 
-    private void checkInputData(List<Integer> userNumber) {
-        if (userNumber.size() != 3) {
+    private void checkInputData(List<Integer> PlayerNumber) {
+        if (PlayerNumber.size() != 3) {
             throw new IllegalArgumentException("3자리 숫자를 입력해야 합니다. 다시 입력해주세요.");
         }
-        if (userNumber.contains(0)) {
+        if (PlayerNumber.contains(0)) {
             throw new IllegalArgumentException("1부터 9까지의 숫자만을 입력할 수 있습니다. 다시 입력해주세요");
         }
-        Set<Integer> duplicate = new HashSet<>(userNumber);
-        if (duplicate.size() != userNumber.size()) {
+        Set<Integer> duplicate = new HashSet<>(PlayerNumber);
+        if (duplicate.size() != PlayerNumber.size()) {
             throw new IllegalArgumentException("중복된 숫자가 있어서는 안됩니다. 서로 다른 숫자로 다시 입력해주세요.");
         }
     }
 
-    private int checkStrikeCount(List<Integer> userNumber) {
+    private int checkStrikeCount(List<Integer> PlayerNumber) {
         int strikeCount = 0;
         for (int i = 0; i < NUM_LIMIT_LENGTH; i++) {
-            if (computerNumber.indexOf(userNumber.get(i)) == i) {
+            if (computerNumber.indexOf(PlayerNumber.get(i)) == i) {
                 strikeCount++;
             }
         }
         return strikeCount;
     }
 
-    private int checkBallCount(List<Integer> userNumber) {
+    private int checkBallCount(List<Integer> PlayerNumber) {
         int ballCount = 0;
         for (int i = 0; i < NUM_LIMIT_LENGTH; i++) {
-            if (computerNumber.contains(userNumber.get(i)) && computerNumber.indexOf(userNumber.get(i)) != i) {
+            if (computerNumber.contains(PlayerNumber.get(i)) && computerNumber.indexOf(PlayerNumber.get(i)) != i) {
                 ballCount++;
             }
         }
@@ -87,7 +87,7 @@ public class BaseballGame {
 
     public boolean endGame() {
         if (strike == 3) {
-            System.out.println(USER_WIN);
+            System.out.println(PLAYER_WIN);
             System.out.println(ASK_MESSAGE);
 
             String ask = readLine();
