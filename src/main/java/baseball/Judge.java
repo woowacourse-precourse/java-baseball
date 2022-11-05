6387dd243422843c21edf53a776c2fdb477c3292
김태용 <amico741@naver.com>
2022-11-05 17:17:27 +0900

    feat: 게임 진행자 생성자 추가

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -1,5 +1,12 @@
 package baseball;
 
 public class Judge {
-  public Judge(){};
+  private Computer computer;
+  private Player player;
+
+  public Judge(boolean progress){
+    this.player = new Player(progress);
+    this.computer = new Computer();
+    System.out.println("숫자 야구 게임을 시작합니다.");
+  }
 }
