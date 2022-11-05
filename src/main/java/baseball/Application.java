46004f2ca9628393faec09afef02ea6c4f0bee30
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-11-05 21:57:34 +0900

    feat : 컴퓨터의 수 할당
    난수 세 자리를 컴퓨터에게 할당한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         sendMassage(GameMassage.START_MASSAGE);
+        List<Integer> computer = randomThreeDigit();
+
     }
 
     private static void sendMassage(GameMassage massage){
         System.out.print(massage.getMassage());
+    }
+
+    private static List<Integer> randomThreeDigit() {
+        List<Integer> digits = new ArrayList<>();
+        while (digits.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!digits.contains(randomNumber)) {
+                digits.add(randomNumber);
+            }
+        }
+        return digits;
     }
 }
