7dc08cf8bd04898b6e747e811f211c6d1c3d9460
Donghyun <flaehan@gmail.com>
2022-11-05 22:27:33 +0900

    type : feat 사용자 입력 정답 검사
    body : 스트라이크 카운트 검사
    
    footer :

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,34 +3,41 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Application {
 
+
+    static int gameButton = 0;
+
+
+    public static void main(String[] args)  {
+
+
+
+    }
+
+}
+
+class ComUserNumber {
     static final int MINIMUM_NUMBER = 1;
     static final int MAX_NUMBER = 9;
     static int comNumber1, comNumber2, comNumber3;
     static int userNumber1, userNumber2, userNumber3;
-    static String userThreeNumber;
-    static int strikeCount = 0;
-    static int ballCount = 0;
-    static int gameButton = 0;
-
-
-    public static void main(String[] args) {
-        System.out.println(userThreeInputNumber());
-    }
-
+    public static String userThreeNumber;
     public static ArrayList<Integer> computerThreeRandomNumber(){
         ArrayList<Integer> comList = new ArrayList<>();
         while(true) {
             comNumber1 = Randoms.pickNumberInRange(MINIMUM_NUMBER, MAX_NUMBER);
             comNumber2 = Randoms.pickNumberInRange(MINIMUM_NUMBER, MAX_NUMBER);
             comNumber3 = Randoms.pickNumberInRange(MINIMUM_NUMBER, MAX_NUMBER);
+
             if((comNumber1 != comNumber2) && (comNumber1 != comNumber3) && (comNumber2 != comNumber3)){
                 comList.add(comNumber1);
                 comList.add(comNumber2);
                 comList.add(comNumber3);
+
                 break;
             }
         }
@@ -58,6 +65,25 @@ public class Application {
 
         return userList;
     }
+}
+
+class CheckCount {
+    static int strikeCount = 0;
+    static int ballCount = 0;
+    public static int checkStrikeCount(){
+        if(ComUserNumber.comNumber1 == ComUserNumber.userNumber1){
+            strikeCount++;
+        }
+        if(ComUserNumber.comNumber2 == ComUserNumber.userNumber2){
+            strikeCount++;
+        }
+        if(ComUserNumber.comNumber3 == ComUserNumber.userNumber3){
+            strikeCount++;
+        }
+
+        return strikeCount;
+    }
 
 }
+
 
