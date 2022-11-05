ad7cfafde256fd45dcfcd22305b42bed21fb8ac7
minhee020 <minhee020@naver.com>
2022-11-05 13:36:58 +0900

    refactor : public method의 파라미터 1word인 것 변경

--- src/main/java/util/RamdomNumber.java
+++ src/main/java/util/RamdomNumber.java
@@ -6,13 +6,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RamdomNumber {
     private int[] randomNumber;
 
-    public RamdomNumber(int size){
-        randomNumber = new int[size];
+    public RamdomNumber(int numberLength){
+        randomNumber = new int[numberLength];
     }
 
-    public int[] makeRandomNumber(int size) {
+    public int[] makeRandomNumber(int numberLength) {
         boolean[] alreadyUsed = new boolean[Const.MAX_NUMBER + 1];
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < numberLength; i++) {
             int number = Randoms.pickNumberInRange(Const.MIN_NUMBER, Const.MAX_NUMBER);
 
             if (!alreadyUsed[number]) {
