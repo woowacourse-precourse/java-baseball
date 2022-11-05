194caa27c4ef12279f549c500239115d4398ba1f
qdeu22 <qdeu22@gmail.com>
2022-11-05 13:22:34 +0900

    feat(NumberCalculator): 스트라이크, 볼 계산 구현

--- src/main/java/baseball/utils/NumberCalculator.java
+++ src/main/java/baseball/utils/NumberCalculator.java
@@ -1,4 +1,44 @@
 package baseball.utils;
 
+import java.util.List;
+
 public class NumberCalculator {
+    private static List<Integer> computerNumber;
+    private static List<Integer> userNumber;
+    private static int strike;
+    private static int ball;
+
+    public NumberCalculator(List<Integer> computerNumber, List<Integer> userNumber) {
+        this.computerNumber = computerNumber;
+        this.userNumber = userNumber;
+        strike = 0;
+        ball = 0;
+    }
+
+    public void resultCalculator() {
+        for (int i = 0; i < computerNumber.size(); i++) {
+            isNumber(i, userNumber.get(i));
+        }
+    }
+
+    public void isNumber(int index, int userNumber) {
+        if (computerNumber.contains(userNumber)) {
+            isSamePlace(index, userNumber);
+        }
+    }
+
+    public void isSamePlace(int index, int userNumber) {
+        if (computerNumber.get(index) == userNumber) {
+            strike++;
+        } else {
+            ball++;
+        }
+    }
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
 }
