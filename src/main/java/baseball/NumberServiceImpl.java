a7fdc2c3c0d5d2dd19634e82bf88f763ea1a17d7
Seung-Been Kim <mainlib990@gmail.com mainlib990s-MacBookPro.local>
2022-11-05 16:11:28 +0900

    feat: 사용자가 올바른 값을 입력하도록 제어하는 기능을 구현하라
    처음에는 유효성 검사를 메소드를 이용하여 확인하였으나 가독성이 좋지 않아 정규표현식을 이용하여 구현하였다.

--- src/main/java/baseball/NumberServiceImpl.java
+++ src/main/java/baseball/NumberServiceImpl.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
@@ -12,6 +13,7 @@ public class NumberServiceImpl implements NumberService {
     private static final int END_INCLUSIVE = 9;
 
     private List<Integer> computerNumbers;
+    private List<Integer> userNumbers;
 
     @Override
     public void pickNumbersByComputer() {
@@ -24,7 +26,27 @@ public class NumberServiceImpl implements NumberService {
 
     @Override
     public void enterNumbersByUser() {
+        final String userInput = Console.readLine();
 
+        /*
+         * 정규표현식을 이용한 유효성 검사로 아래의 정규표현식의 의미는 다음과 같음
+         *
+         * ([${start}-${end}]) -> 사용자가 입력한 값의 첫번째 값이 올바른지 검사, 이것은 백레퍼런스의 1번 임
+         * (?!\\1) -> 부정 룩어헤드(Negative Lookahead)로 백레퍼런스의 1번이 아닌 경우를 의미함, 이것은 zero-width 연산임을 기억해야 함
+         * ([${start}-${end}])(?!\\1)([${start}-${end}]) -> 사용자가 입력한 값의 첫번째 값이 정상 범위이며,
+         *                                                  두번째 값은 첫번째 값과 동일하지 않으면서 정상 범위인 경우를 뜻 함
+         */
+        final String regexUserNumbers = "^([${start}-${end}])(?!\\1)([${start}-${end}])(?!\\1|\\2)[${start}-${end}]$"
+                .replaceAll("\\$\\{start}", Integer.toString(START_INCLUSIVE))
+                .replaceAll("\\$\\{end}", Integer.toString(END_INCLUSIVE));
+
+        if (userInput.matches(regexUserNumbers)) {
+            userNumbers = userInput.codePoints()
+                    .mapToObj(Character::getNumericValue)
+                    .collect(Collectors.toList());
+        } else {
+            throw new IllegalArgumentException("Your input string: " + userInput);
+        }
     }
 
     @Override
