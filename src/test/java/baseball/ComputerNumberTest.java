3255428700b93a7114a70fff938860a45d73e8c9
DongwookKim0823 <dwkim0823@naver.com>
2022-11-06 05:33:24 +0900

    test: 무작위로 선택한 숫자가 이미 선택한 숫자인지 검증하는 기능 테스트

--- src/test/java/baseball/ComputerNumberTest.java
+++ src/test/java/baseball/ComputerNumberTest.java
@@ -3,6 +3,7 @@ package baseball;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
@@ -23,5 +24,25 @@ class ComputerNumberTest {
         //then
         Assertions.assertThat(method.invoke(computerNumber, list1)).isEqualTo(true);
         Assertions.assertThat(method.invoke(computerNumber, list2)).isEqualTo(false);
+    }
+
+    @Test
+    void 무작위로_선택한_숫자가_이미_선택했던_숫자인지의_검증_테스트() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, NoSuchFieldException {
+        //given
+        ComputerNumber computerNumber = new ComputerNumber();
+        Method method = computerNumber.getClass().getDeclaredMethod("checkListAlreadyContainNumber", Character.class);
+        method.setAccessible(true);
+
+        Field fld = computerNumber.getClass().getDeclaredField("numberList");
+        fld.setAccessible(true);
+        fld.set(computerNumber, Arrays.asList('1', '2'));
+
+        //when
+        Character tempNumber1 = '1';
+        Character tempNumber2 = '3';
+
+        //then
+        Assertions.assertThat(method.invoke(computerNumber, tempNumber1)).isEqualTo(true);
+        Assertions.assertThat(method.invoke(computerNumber, tempNumber2)).isEqualTo(false);
     }
 }
