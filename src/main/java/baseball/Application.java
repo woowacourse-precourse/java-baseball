6acc2994bb5d81638330d8eeab7c835508ef8d07
simba <shim9597@gmail.com>
2022-11-05 17:25:38 +0900

    feat: newRandomNumber 기능 구현
    gameStart 기능이 호출 하는 randomNumber 기능 구현 완료.
    gameStart 기능이 호출 될 때 함께 호출 되어 하나의 게임 마다 새로운 random number 를 부여함.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,42 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public List<Integer> randomNum;
+
+    public void gameStart() {
+        System.out.println("숫자를 입력해 주세요 : ");
+        String input = readLine();
+
+        validateInGame(input);
+        newRandomNumber();
+    }
+
+    public void validateInGame(String input) {
+
+    }
+
+    public void newRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        randomNum = computer;
+    }
+
 }
