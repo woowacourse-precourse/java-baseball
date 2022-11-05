525128d3c8540466effe19455f7458eb7761c38b
jaehyeon <jhjh0022@naver.com>
2022-11-06 01:32:13 +0900

    feat: 입력값 검증 메서드 테스트

---
+++ src/test/java/baseball/Input/InputNumberVerificationTest.java
@@ -0,0 +1,83 @@
+package baseball.Input;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputNumberVerificationTest {
+
+    @Nested
+    class verifyNumberTest {
+
+
+        @Test
+        @DisplayName("올바른 숫자 형식")
+        void case1(){
+            int number = 123;
+            boolean result = InputNumberVerification.verifyNumber(number);
+            assertEquals(true, result);
+        }
+
+        @Test
+        @DisplayName("숫자 갯수가 3개가 아닌 4개인 경우")
+        void case2(){
+            int number = 1234;
+
+            assertThatThrownBy(()-> InputNumberVerification.verifyNumber(number))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("3개의 숫자만 입력해주세요");
+        }
+
+        @Test
+        @DisplayName("숫자 갯수가 3개가 아닌 2개인 경우")
+        void case3(){
+            int number = 12;
+
+            assertThatThrownBy(()-> InputNumberVerification.verifyNumber(number))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("3개의 숫자만 입력해주세요");
+        }
+
+        @Test
+        @DisplayName("숫자 갯수가 3개가 아닌 1개인 경우")
+        void case4(){
+            int number = 1;
+
+            assertThatThrownBy(()-> InputNumberVerification.verifyNumber(number))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("3개의 숫자만 입력해주세요");
+        }
+
+        @Test
+        @DisplayName("중복된 숫자가 있는 경우")
+        void case5(){
+            int number = 112;
+
+            assertThatThrownBy(()-> InputNumberVerification.verifyNumber(number))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("각자 다른 숫자를 입력해주세요");
+        }
+
+        @Test
+        @DisplayName("1~9 까지 숫자가 아닌 경우")
+        void case6(){
+            int number = 160;
+
+            assertThatThrownBy(()-> InputNumberVerification.verifyNumber(number))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("1~9 사이의 숫자만 입력해주세요");
+        }
+
+
+
+
+
+
+    }
+
+}
