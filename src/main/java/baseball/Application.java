6896269ef0fae72820dca76acb6f282fd6d12524
mydreamis-18 <hitoryok@naver.com>
2022-11-05 23:29:49 +0900

    [feat] endGame() 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -20,6 +20,18 @@ public class Application {
 		System.out.println(playerNumbers);
 	}
 
+	public static void endGame() {
+		
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		
+		int result = Integer.parseInt(Console.readLine());
+		if (result == 1) {
+			
+			startGame();
+		}
+	}
+	
 	public static List<Integer> getComputerNumbers() {
 
 		List<Integer> computer = new ArrayList<>();
