8c9614357d00e5d5f159669660dd6dc192403654
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-05 15:53:32 +0900

    [feat]: 컴퓨터 랜덤 난수 만들기

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -6,5 +6,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-
+    private List<Integer> computerNumber;
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
+    public void setComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+        this.computerNumber=computerNumber;
+    }
 }
