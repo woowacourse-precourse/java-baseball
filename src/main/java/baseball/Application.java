bfc49bd3fa9a318ac2c53425eb14d1d43f752496
박승민 <tmddudals369@naver.com>
2022-11-05 22:16:54 +0900

    feat: 힌트 출력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -22,4 +22,41 @@ public class Application {
     public static void notifyGameStart() {
         System.out.println(GAME_START_SIGN);
     }
+
+    public static String getHint(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        int ballCnt = getBallCnt(computerNumbers, userNumbers);
+        int strikeCnt = getStrikeCnt(computerNumbers, userNumbers);
+        ballCnt -= strikeCnt;
+
+        if (ballCnt == 0 && strikeCnt == 0) {
+            return "낫싱";
+        } else if (strikeCnt == 0) {
+            return String.format("%d볼", ballCnt);
+        } else if (ballCnt == 0) {
+            return String.format("%d스트라이크", strikeCnt);
+        } else {
+            return String.format("%d볼 %d스트라이크", ballCnt, strikeCnt);
+        }
+    }
+
+    private static int getStrikeCnt(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        int strikeCnt = 0;
+        for (int i = 0; i < userNumbers.size(); i++) {
+            if (computerNumbers.get(i).equals(userNumbers.get(i))) {
+                strikeCnt++;
+            }
+        }
+
+        return strikeCnt;
+    }
+
+    private static int getBallCnt(List<Integer> computerNumbers, List<Integer> userNumbers) {
+        int ballCnt = 0;
+        for (Integer userNumber : userNumbers) {
+            if (computerNumbers.contains(userNumber)) {
+                ballCnt++;
+            }
+        }
+        return ballCnt;
+    }
 }
