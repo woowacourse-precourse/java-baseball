87a143915ad40fe6e00ba1f89ef2d7f15c8bca45
Jeonsoyeon <itso22@naver.com>
2022-11-06 00:13:56 +0900

    test(ApplicationTest): 예외처리 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -34,89 +34,117 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 유저의숫자_뽑아오기_테스트(){
+    void 유저의숫자_뽑아오기_테스트() {
         String input = "456";
-        List<Integer> expect = Arrays.asList(4,5,6);
+        List<Integer> expect = Arrays.asList(4, 5, 6);
         List<Integer> result = User.getUserNumber(input);
         assertThat(result).isEqualTo(expect);
     }
 
     @Test
-    void 스트라이크_판단_테스트(){
-        List<Integer> computer = Arrays.asList(3,4,5);
-        List<Integer> user = Arrays.asList(1,3,5);
-        String result = getResultStrike(computer,user)+STRIKE;
+    void 스트라이크_판단_테스트() {
+        List<Integer> computer = Arrays.asList(3, 4, 5);
+        List<Integer> user = Arrays.asList(1, 3, 5);
+        String result = getResultStrike(computer, user) + STRIKE;
 
-        assertThat(result).isEqualTo(1+STRIKE);
-    }
-    @Test
-    void 볼_판단_테스트(){
-        List<Integer> computer = Arrays.asList(3,1,4);
-        List<Integer> user = Arrays.asList(1,3,4);
-        String result = getResultBall(computer,user)+BALL;
-
-        assertThat(result).isEqualTo(2+BALL);
+        assertThat(result).isEqualTo(1 + STRIKE);
     }
 
     @Test
-    void threeStrikeTest(){
-        List<Integer> computer = Arrays.asList(3,4,5);
-        List<Integer> user = Arrays.asList(3,4,5);
-        String result = getJudgement(computer,user);
+    void 볼_판단_테스트() {
+        List<Integer> computer = Arrays.asList(3, 1, 4);
+        List<Integer> user = Arrays.asList(1, 3, 4);
+        String result = getResultBall(computer, user) + BALL;
 
-        assertThat(result).isEqualTo(3+STRIKE);
+        assertThat(result).isEqualTo(2 + BALL);
     }
-    @Test
-    void threeBallTest(){
-        List<Integer> computer = Arrays.asList(3,4,5);
-        List<Integer> user = Arrays.asList(5,3,4);
-        String result = getJudgement(computer,user);
 
-        assertThat(result).isEqualTo(3+BALL);
-    }
     @Test
-    void nothingTest(){
-        List<Integer> computer = Arrays.asList(3,4,5);
-        List<Integer> user = Arrays.asList(7,8,9);
-        String result = getJudgement(computer,user);
+    void threeStrikeTest() {
+        List<Integer> computer = Arrays.asList(3, 4, 5);
+        List<Integer> user = Arrays.asList(3, 4, 5);
+        String result = getJudgement(computer, user);
+
+        assertThat(result).isEqualTo(3 + STRIKE);
+    }
+
+    @Test
+    void threeBallTest() {
+        List<Integer> computer = Arrays.asList(3, 4, 5);
+        List<Integer> user = Arrays.asList(5, 3, 4);
+        String result = getJudgement(computer, user);
+
+        assertThat(result).isEqualTo(3 + BALL);
+    }
+
+    @Test
+    void nothingTest() {
+        List<Integer> computer = Arrays.asList(3, 4, 5);
+        List<Integer> user = Arrays.asList(7, 8, 9);
+        String result = getJudgement(computer, user);
 
         assertThat(result).isEqualTo(NOTHING);
     }
 
     @Test
-    void oneStrikeOneBallTest(){
-        List<Integer> computer = Arrays.asList(3,4,5);
-        List<Integer> user = Arrays.asList(3,7,4);
-        String result = getJudgement(computer,user);
+    void oneStrikeOneBallTest() {
+        List<Integer> computer = Arrays.asList(3, 4, 5);
+        List<Integer> user = Arrays.asList(3, 7, 4);
+        String result = getJudgement(computer, user);
 
-        assertThat(result).isEqualTo(1+BALL+" "+1+STRIKE);
+        assertThat(result).isEqualTo(1 + BALL + " " + 1 + STRIKE);
     }
+
     @Test
-    void oneStrikeTwoBallTest(){
-        List<Integer> computer = Arrays.asList(3,4,5);
-        List<Integer> user = Arrays.asList(3,5,4);
-        String result = getJudgement(computer,user);
+    void oneStrikeTwoBallTest() {
+        List<Integer> computer = Arrays.asList(3, 4, 5);
+        List<Integer> user = Arrays.asList(3, 5, 4);
+        String result = getJudgement(computer, user);
 
-        assertThat(result).isEqualTo(2+BALL+" "+1+STRIKE);
+        assertThat(result).isEqualTo(2 + BALL + " " + 1 + STRIKE);
     }
+
     @Test
-    void twoBallTest(){
-        List<Integer> computer = Arrays.asList(3,4,5);
-        List<Integer> user = Arrays.asList(7,3,4);
-        String result = getJudgement(computer,user);
+    void twoBallTest() {
+        List<Integer> computer = Arrays.asList(3, 4, 5);
+        List<Integer> user = Arrays.asList(7, 3, 4);
+        String result = getJudgement(computer, user);
 
-        assertThat(result).isEqualTo(2+BALL);
+        assertThat(result).isEqualTo(2 + BALL);
     }
+
     @Test
-    void oneStrikeTest(){
-        List<Integer> computer = Arrays.asList(3,4,5);
-        List<Integer> user = Arrays.asList(7,1,5);
-        String result = getJudgement(computer,user);
+    void oneStrikeTest() {
+        List<Integer> computer = Arrays.asList(3, 4, 5);
+        List<Integer> user = Arrays.asList(7, 1, 5);
+        String result = getJudgement(computer, user);
 
-        assertThat(result).isEqualTo(1+STRIKE);
+        assertThat(result).isEqualTo(1 + STRIKE);
     }
 
+    @Test
+    void 일부터_구까지_숫자말고_다른것입력_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12c"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
+    @Test
+    void 입력숫자_자릿수범위_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("23"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 반복되는_숫자_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("112"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
 
     @Override
