be41437ef949c1e16192ad547eeac62f2a7539af
leeyongjun <leeyongjun0310@naver.com>
2022-11-05 18:33:59 +0900

    fix(Generate): randomNumber 접근제어자 static 추가

--- src/main/java/baseball/Generate.java
+++ src/main/java/baseball/Generate.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Generate {
-    public List<Integer> randomNumber(){
+    public static List<Integer> randomNumber(){
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < Constant.NUMBER_LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
