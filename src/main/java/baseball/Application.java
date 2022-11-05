3f18e1b1c4bff319bf2dff0a62eb55f5f5125fac
Soseung Soo <sss9073@naver.com>
2022-11-05 15:18:44 +0900

    feat(Printer): add Printer that print console message

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,16 @@
 package baseball;
 
+import baseball.Controller.Computer;
+import baseball.Controller.Executor;
 import camp.nextstep.edu.missionutils.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        Executor.startGame();
     }
 }
--- src/main/java/baseball/Controller/Executor.java
+++ src/main/java/baseball/Controller/Executor.java
@@ -1,2 +1,18 @@
-package baseball.Controller;public class Executor {
+package baseball.Controller;
+
+import baseball.model.Baseball;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Executor {
+    private static Scanner scanner = new Scanner(System.in);
+    private static Computer computer;
+
+    public static void startGame(){
+        Baseball randomBaseball = new Computer().createRandomBaseball();
+    }
+
+
 }
