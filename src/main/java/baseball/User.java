ceabb9b4250b78213d48e5a31b4da97148375fdb
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-05 15:54:32 +0900

    [feat]: 유저 숫자 받기

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -7,5 +7,14 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class User {
-
+    private List<Integer> userNumber;
+    public List<Integer> makeUserNumber() {
+        System.out.println("숫자를 입력해주세요: ");
+        String inputString = readLine();
+        List<Integer> inputNumber=new ArrayList<>();
+        for(int i=0;i<inputString.length();i++){
+            inputNumber.add(inputString.charAt(i)-'0');
+        }
+        return inputNumber;
+    }
 }
