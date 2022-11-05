8a2c010a93c5da3a671b7c99bfce13a9f347981b
syahn <496300@naver.com>
2022-11-05 19:02:05 +0900

    FEAT : 게임을 종료 or 재시작하는 기능 추가
    사용자 입력을 받아 1이면 다시 게임 재시작, 2면 종료하도록 함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,49 +18,59 @@ public class Application {
         return computerNum;
     }
 
-    public static List<Integer> getUserInput() throws IllegalArgumentException{
+    public static List<Integer> getUserInput() throws IllegalArgumentException {
         List<Integer> userNum = new ArrayList<>();
         String userInput = Console.readLine();
 
-        if(userInput.length() != 3)
+        if (userInput.length() != 3)
             throw new IllegalArgumentException("게임 종료");
 
-        for (int i = 0; i < userInput.length(); i++){
-            if((int)userInput.charAt(i) == 0)
+        for (int i = 0; i < userInput.length(); i++) {
+            if ((int) userInput.charAt(i) == 0)
                 throw new IllegalArgumentException("게임 종료");
             userNum.add((int) userInput.charAt(i));
         }
         return userNum;
     }
 
-    public void compareNums(List<Integer> computerNum, List<Integer> userNum){
-        if(computerNum.equals(userNum)) {
+    public static void compareNums(List<Integer> computerNum, List<Integer> userNum) {
+        if (computerNum.equals(userNum)) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다!");
-        }else{
+            chooseGameCondition();
+        } else {
             List<Integer> copyUserNum = new ArrayList<>(userNum);
             copyUserNum.retainAll(computerNum);
             int count = 0;
 
-            if(copyUserNum.isEmpty())
+            if (copyUserNum.isEmpty())
                 System.out.println("낫싱");
-            else{
-                for(int i = 0; i<copyUserNum.size(); i++){
+            else {
+                for (int i = 0; i < copyUserNum.size(); i++) {
                     int indexComputer = computerNum.indexOf(copyUserNum.get(i));
                     int indexUser = userNum.indexOf(copyUserNum.get(i));
-                    if(indexComputer == indexUser)
+                    if (indexComputer == indexUser)
                         count++;
                 }
                 if (count == 0)
                     System.out.println(count + "볼");
-                else if(copyUserNum.size() - count == 0)
+                else if (copyUserNum.size() - count == 0)
                     System.out.println(count + "스트라이크");
                 else
-                    System.out.println((copyUserNum.size()-count) + "볼" +count + "스트라이크");
+                    System.out.println((copyUserNum.size() - count) + "볼" + count + "스트라이크");
             }
         }
     }
 
-    public void startGame() {
+    public static void chooseGameCondition() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userInput = Console.readLine();
+
+        if (userInput.equals("1")) {
+        } else {
+        }
+    }
+
+    public static void startGame() {
         List<Integer> computerNum = new ArrayList<>();
         System.out.println("숫자 야구 게임을 시작합니다.");
         computerNum = createComputerNum();
@@ -68,9 +78,9 @@ public class Application {
 
         while (continueGame) {
             List<Integer> userNum = new ArrayList<>();
-            try{
+            try {
                 userNum = getUserInput();
-            }catch(IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.toString());
             }
             compareNums(computerNum, userNum);
@@ -78,5 +88,6 @@ public class Application {
     }
 
     public static void main(String[] args) {
+        startGame();
     }
 }
