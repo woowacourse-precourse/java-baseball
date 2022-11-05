52e9fa7d07b4b7ad32dc12be2bd8a7b94906cfc0
jaehyeon <jhjh0022@naver.com>
2022-11-06 03:58:32 +0900

    feat: 테스트 코드 구현

---
+++ src/test/java/baseball/tool/BeforeVerificationSettingTest.java
@@ -0,0 +1,40 @@
+package baseball.tool;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BeforeVerificationSettingTest {
+
+    @Nested
+    class makeNumberSplitListTest{
+
+        @Test
+        void case1(){
+            int testNumber = 123;
+            List<Integer> answerNumber = List.of(1,2,3);
+            List<Integer> testNumberList = BeforeVerificationSetting.makeNumberSplitList(testNumber);
+            assertEquals(answerNumber, testNumberList);
+        }
+
+        @Test
+        void case2(){
+            int testNumber = 456;
+            List<Integer> answerNumber = List.of(4,5,6);
+            List<Integer> testNumberList = BeforeVerificationSetting.makeNumberSplitList(testNumber);
+            assertEquals(answerNumber, testNumberList);
+        }
+
+        @Test
+        void case3(){
+            int testNumber = 789;
+            List<Integer> answerNumber = List.of(7,8,9);
+            List<Integer> testNumberList = BeforeVerificationSetting.makeNumberSplitList(testNumber);
+            assertEquals(answerNumber, testNumberList);
+        }
+
+    }
+}
---
+++ src/test/java/baseball/tool/DuplicateByComparisonTest.java
@@ -0,0 +1,53 @@
+package baseball.tool;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DuplicateByComparisonTest {
+
+    @Nested
+    class findOverlapNumber{
+
+        @Test
+        void case1(){
+            int testNumber = 123;
+            List<Integer> testNumber2 = List.of(1,2,4);
+            List<Integer> answerNumber = List.of(1,2);
+            List<Integer> testResult = DuplicateByComparison.findOverlapNumber(testNumber2, testNumber);
+            assertEquals(answerNumber, testResult);
+        }
+
+        @Test
+        void case2(){
+            int testNumber = 753;
+            List<Integer> testNumber2 = List.of(5,4,2);
+            List<Integer> answerNumber = List.of(5);
+            List<Integer> testResult = DuplicateByComparison.findOverlapNumber(testNumber2, testNumber);
+            assertEquals(answerNumber, testResult);
+        }
+
+        @Test
+        void case3(){
+            int testNumber = 975;
+            List<Integer> testNumber2 = List.of(1,2,3);
+            List<Integer> answerNumber = List.of();
+            List<Integer> testResult = DuplicateByComparison.findOverlapNumber(testNumber2, testNumber);
+            assertEquals(answerNumber, testResult);
+        }
+
+        @Test
+        void case4(){
+            int testNumber = 321;
+            List<Integer> testNumber2 = List.of(1,2,3);
+            List<Integer> answerNumber = List.of(1,2,3);
+            List<Integer> testResult = DuplicateByComparison.findOverlapNumber(testNumber2, testNumber);
+            assertEquals(answerNumber, testResult);
+        }
+    }
+
+}
