de0c30476978e5a914565b92fc84b681c7a5f42f
qdeu22 <qdeu22@gmail.com>
2022-11-05 12:06:56 +0900

    feat(InputView): 사용자 입력 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,12 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    private static final String PLEASE_INPUT = "숫자를 입력해주세요 : ";
+
+    public static String inputNumber(){
+        System.out.print(PLEASE_INPUT);
+        return Console.readLine();
+    }
 }
