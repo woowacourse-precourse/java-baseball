93b7e4ab4af8295fb923f363a8feac249ee4dc2a
leeyongjun <leeyongjun0310@naver.com>
2022-11-05 19:29:35 +0900

    style(Change): 코드 사이 공백 추가

--- src/main/java/baseball/Change.java
+++ src/main/java/baseball/Change.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Change {
-    public static List<Integer> numberToList(String number){
+    public static List<Integer> numberToList(String number) {
         List<Integer> newList = new ArrayList<>();
-        for (int i = 0; i<number.length(); i++){
+        for (int i = 0; i < number.length(); i++) {
             newList.add(number.charAt(i) - '0');
         }
         return newList;
