914d789a73d28904b1f1cf2fee102ca66d75f8c8
Invidam <hansupark0208@gmail.com>
2022-11-05 17:51:10 +0900

    feat(User): 게임 제어를 위한 수 (1, 2)를 입력 기능 추가
    - User 게임 제어 위한 숫자 입력받는 코드 추가
    - GameControlNum
      - 클래스 생성
      - 유효성 검증 로직 추가

---
+++ src/main/java/baseball/domain/GameControlNum.java
@@ -0,0 +1,24 @@
+package baseball.domain;
+
+public class GameControlNum {
+
+    public static final int RESTART_VALUE = 1;
+    public static final int STOP_VALUE = 2;
+
+    private int value;
+
+    public GameControlNum(int num) {
+        value = num;
+        validateValue();
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    private void validateValue() {
+        if(value != RESTART_VALUE && value != STOP_VALUE) {
+            throw new IllegalArgumentException("유효하지 않은 입력입니다.");
+        }
+    }
+}
--- src/main/java/baseball/user/User.java
+++ src/main/java/baseball/user/User.java
@@ -14,4 +14,9 @@ public class User {
         return new ThreeDigitNum((InputControl.inputInt()));
     }
 
+    public ThreeDigitNum inputGu2essedNum() {
+        OutputControl.println("숫자를 입력해주세요 : ");
+        return new ThreeDigitNum((InputControl.inputInt()));
+    }
+
 }
