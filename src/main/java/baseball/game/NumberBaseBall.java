1a469a67090c4811cb9c556ad4c92ad2e7fb8466
bellljun3395 <195850@jnu.ac.kr>
2022-11-05 17:15:57 +0900

    feat(NumberBaseBall): 구현 NumberBaseBall start 메서드
    - 게임의 값을 초기화 한다.
    - 게임 시작 멘트를 출력한다.
    - 컴퓨터의 랜덤한 값을 생성한다.

--- src/main/java/baseball/game/NumberBaseBall.java
+++ src/main/java/baseball/game/NumberBaseBall.java
@@ -10,7 +10,7 @@ import baseball.view.Viewable;
 
 import java.util.List;
 
-public class NumberBaseBall {
+public class NumberBaseBall implements PlayAble {
     private boolean stopGame = false;
     private boolean endGame = false;
 
@@ -25,5 +25,27 @@ public class NumberBaseBall {
 
     public boolean isEndGame() {
         return endGame;
+    }
+
+    @Override
+    public void start() {
+        initializeSetting();
+        view.printStart();
+        computer.setNumber(Computer.makeRandomNumber());
+    }
+
+    private void initializeSetting() {
+        this.stopGame = false;
+        this.endGame = false;
+    }
+    
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void end() {
+
     }
 }
---
+++ src/main/java/baseball/game/PlayAble.java
@@ -0,0 +1,11 @@
+package baseball.game;
+
+public interface PlayAble {
+
+    public void start();
+
+    public void play();
+
+    public void end();
+
+}
