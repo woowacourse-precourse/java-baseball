9fb8ceb0e8adf98019c887c723e5ed69b288cb1f
Kim-Jong-Gyu <whdrb2643@gmail.com>
2022-11-05 16:21:05 +0900

    feat(add): 입력값을 어레이로 반환 하는 기능
    입력값을 어레이로 반환 하는 기능:
    -createInputNumArray(int inputNum) : Stream을 이용한 입력 값인 int를 int 어레이로 반환

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -1,14 +1,22 @@
 package baseball.game;
 
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class BaseballGame {
 
     private int receiveInput(){
-        int inputValue;
+        int inputNum;
         Scanner sc = new Scanner(System.in);
-        inputValue = sc.nextInt();
-        return inputValue;
+        inputNum = sc.nextInt();
+        return inputNum;
     }
+
+    private int[] createInputNumArray(int inputNum){
+        int[] inputNumArray = Stream.of(String.valueOf(inputNum).split("")).mapToInt(Integer::parseInt).toArray();
+        return inputNumArray;
+    }
+
 
 }
