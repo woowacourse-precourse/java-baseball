aae2670a9123e55fecd5fb2e1dd487451438683b
Devtist <yt930310@snu.ac.kr>
2022-11-05 16:28:02 +0900

    feat: 숫자 입력 안내를 출력하는 기능 구현

--- src/main/java/Controller/Printer.java
+++ src/main/java/Controller/Printer.java
@@ -6,4 +6,8 @@ public class Printer {
 	public static void printGameStartMessage() {
 		System.out.println(GAME_START_MESSAGE.get());
 	}
+
+	public static void printAskInputMessage() {
+		System.out.print(ASK_INPUT_MESSAGE.get());
+	}
 }
