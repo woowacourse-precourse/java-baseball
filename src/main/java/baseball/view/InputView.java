3d50681d99f7a0ff742dec623ea6bb5f6e65d2e7
qdeu22 <qdeu22@gmail.com>
2022-11-05 13:45:21 +0900

    refactor(InputView): 재시작/종료 여부 입력값 유효성 검사 및 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,11 +5,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String PLEASE_INPUT = "숫자를 입력해주세요 : ";
+    private static final String CHOICE_QUIT_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
 
     public static String inputNumber() {
         System.out.print(PLEASE_INPUT);
         String inputNumber = Console.readLine();
         ValidNumber.validUserNumber(inputNumber);
         return inputNumber;
+    }
+
+    public static boolean restartChoiceNumber() {
+        System.out.println(CHOICE_QUIT_RESTART);
+        String restartChoiceNumber = Console.readLine();
+        return ValidNumber.validRestartNumber(restartChoiceNumber);
     }
 }
