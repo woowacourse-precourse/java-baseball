902de8c0bad0ec245c8e7da67fc4ca8b9c0fa143
es <mascoeskim@gmail.com>
2022-11-05 17:11:57 +0900

    feat(numberReferee): 생성된정답과 입력값을 비교해 형식대로 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,10 +10,15 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다!!!");
-        System.out.println(generateAnswer(args));
-        System.out.println(inputUserNumber(args));
 
+        ArrayList<Integer> generateAnswer = generateAnswer(args);
+        ArrayList<Integer> inputUserNumber = inputUserNumber(args);
+        String AA = numberReferee(generateAnswer,inputUserNumber);
+
+        System.out.println(AA);
     }
+
+
 
     public static ArrayList<Integer> generateAnswer(String[] args) {
         ArrayList<Integer> answerNumber = new ArrayList<>();
@@ -35,7 +40,7 @@ public class Application {
         System.out.println(inputNumber.charAt(2));
         try {
             if (inputNumber.charAt(0) == inputNumber.charAt(1) || inputNumber.charAt(0) == inputNumber.charAt(2)
-            || inputNumber.charAt(1) == inputNumber.charAt(2)){
+            || inputNumber.charAt(1) == inputNumber.charAt(2) || inputNumber.length() !=3){
             throw new IllegalArgumentException();
             }
             if (!Character.isDigit(inputNumber.charAt(0)) || !Character.isDigit(inputNumber.charAt(1))
@@ -54,4 +59,61 @@ public class Application {
         return inputNumberArray;
     }
 
+    public static String numberReferee(ArrayList<Integer> generateAnswer, ArrayList<Integer> inputUserNumber) {
+        System.out.println(generateAnswer);
+        System.out.println(inputUserNumber);
+        String answer = "";
+        int strike = 0;
+        int ball = 0;
+
+        for (int i=0; i<3; i++){
+            if(Objects.equals(generateAnswer.get(0), inputUserNumber.get(i))) {
+                if(i == 0){
+                    System.out.println("스트라이크!!");
+                    strike++;
+                }else{
+                    System.out.println("뽈!");
+                    ball++;
+                }
+            }
+        }
+
+        for (int i=0; i<3; i++){
+            if(Objects.equals(generateAnswer.get(1), inputUserNumber.get(i))) {
+                if(i == 1){
+                    System.out.println("스트라이크!!");
+                    strike++;
+                }else{
+                    System.out.println("뽈!");
+                    ball++;
+                }
+            }
+        }
+
+        for (int i=0; i<3; i++){
+            if(Objects.equals(generateAnswer.get(2), inputUserNumber.get(i))) {
+                if(i == 2){
+                    System.out.println("스트라이크!!");
+                    strike++;
+                }else{
+                    System.out.println("뽈!");
+                    ball++;
+                }
+            }
+        }
+
+        if (strike == 0 && ball ==0){
+            answer += "낫싱";
+        }
+
+        else if (strike == 0){
+            answer += (ball+"볼");
+        }
+
+        else if (ball ==0){
+            answer += (strike+"스트라이크");
+        }
+        else{answer += (ball+"볼")+" "+(strike+"스트라이크");}
+        return answer;
+    }
 }
