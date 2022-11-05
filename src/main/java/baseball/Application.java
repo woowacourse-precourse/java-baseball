a5b732bcaa13fc0dac86b50ba9a476cd3edd2be6
minje <alswp0910@naver.com>
2022-11-05 16:18:17 +0900

    feat: 사용자에게 숫자 입력받는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,6 +10,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    //기능1.
     public static List<Integer> computer(){
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
@@ -19,5 +22,31 @@ public class Application {
         }
 
         return computer();
+    }
+
+    //기능2.
+    public static List<Integer> user(){
+        List<Integer> user = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
+        String userNum = Console.readLine();
+        char[] charArr; //입력된 값을 3개의 수로 분할할 배열
+
+        //예외상황 3가지
+        if (userNum.length() != 3){
+            throw new IllegalArgumentException("3개의 숫자를 입력하지 않았습니다.");
+        } else if (!userNum.matches("-?\\d+")) {
+            throw new IllegalArgumentException("숫자를 입력하지 않았습니다.");
+        }else {
+            charArr = userNum.toCharArray();
+            if (charArr[0] == charArr[1] || charArr[0] == charArr[2] || charArr[1] == charArr[2]){
+                throw new IllegalArgumentException("겹치는 숫자가 있습니다.");
+            }else {
+                user.add(charArr[0] - '0');
+                user.add(charArr[1] - '0');
+                user.add(charArr[2] - '0');
+            }
+        }
+
+        return user;
     }
 }
