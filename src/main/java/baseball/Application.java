8c2596c546f7212c448e184e9fa35a897b866da9
Eungyu-code <Agyu625@gmail.com>
2022-11-05 15:54:34 +0900

    update: playGame 함수의 이름 startGame으로 변경
    - 수정 전
    void playGame()
    
    - 수정 후
    void startGame()
    
    실제로는 게임을 시작하는 함수이기 때문에 함수의 기능에 대해 명확하게 하기 위해 이름 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,10 +10,10 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        playGame();
+        startGame();
     }
 
-    public static void playGame() {
+    public static void startGame() {
 
         showGameStart();
 
