5378b7fab445e333224e56b6f061f2e2c24c483b
hoonn9 <hun9935@gmail.com>
2022-11-05 16:06:30 +0900

    feat(BaseballGame): 게임 진행 기능 추가

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -1,20 +1,73 @@
 package baseball.game;
 
+import baseball.board.NumberBoard;
 import baseball.compare.ComparisonResultType;
 import baseball.util.InputController;
+import baseball.util.Logger;
+import baseball.util.RandomGenerator;
 import java.util.HashMap;
+import java.util.List;
 
 public class BaseballGame {
 
     private final InputController inputController;
     private final Integer boardSize;
+    private final List<Integer> opponentNumbers;
 
     public BaseballGame(InputController inputController, Integer size) {
         this.inputController = inputController;
         this.boardSize = size;
+        this.opponentNumbers = new RandomGenerator().genNumberList(size);
+    }
+
+    public void play() {
+        start();
+
+        while (true) {
+            boolean isSuccess = tryGuess();
+
+            if (isSuccess) {
+                break;
+            }
+        }
+
+        end();
+    }
+
+    private void start() {
+        Logger.log(BaseballConstants.START_MESSAGE);
+    }
+
+    private void end() {
+        Logger.log(BaseballConstants.END_MESSAGE);
+    }
+
+    private boolean tryGuess() {
+        Logger.log(BaseballConstants.INPUT_MESSAGE);
+
+        List<Integer> userNumbers = inputController.getEachNumber(boardSize);
+        validateNumberInput(userNumbers);
+
+        NumberBoard numberBoard = new NumberBoard();
+
+        if (checkSuccess(numberBoard.compareNumbers(userNumbers, opponentNumbers))) {
+            Logger.log(boardSize + BaseballConstants.SUCCESS_MESSAGE);
+            return true;
+        }
+
+        return false;
     }
 
     private boolean checkSuccess(HashMap<ComparisonResultType, Integer> resultFrequencyCounter) {
-        return resultFrequencyCounter.get(ComparisonResultType.STRIKE).equals(boardSize);
+        return resultFrequencyCounter.containsKey(ComparisonResultType.STRIKE) && resultFrequencyCounter.get(
+                ComparisonResultType.STRIKE).equals(boardSize);
+    }
+
+    private void validateNumberInput(List<Integer> inputNumbers) throws IllegalArgumentException {
+        for (Integer number : inputNumbers) {
+            if (number < 1 || number > 9) {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
