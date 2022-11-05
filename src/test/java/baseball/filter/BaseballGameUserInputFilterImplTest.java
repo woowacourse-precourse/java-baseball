674b265ee3b1142e7b0b8dfd27b862c3d2d1f0d8
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-05 18:49:52 +0900

    test(BaseballGameUserInputFilterImplTest): 유저 입력을 변환하여 리스트 반환 기능 테스트 추가

--- src/test/java/baseball/filter/BaseballGameUserInputFilterImplTest.java
+++ src/test/java/baseball/filter/BaseballGameUserInputFilterImplTest.java
@@ -1,5 +1,7 @@
 package baseball.filter;
 
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -34,5 +36,22 @@ class BaseballGameUserInputFilterImplTest implements RestrictIntegerConstants {
 
 		checkResult = filter.checkUserInput("123", LIMIT_LIST_SIZE, MIN_NUMBER, MAX_NUMBER);
 		Assertions.assertThat(checkResult).isTrue();
+	}
+
+	@Test
+	void bindingResultTest() {
+		List<Integer> temp;
+		List<Integer> bindingResult;
+		UserInputFilter<Integer> filter = new BaseballGameUserInputFilterImpl();
+
+		bindingResult = filter.bindingResult(null, 3);
+		Assertions.assertThat(bindingResult).isEmpty();
+
+		bindingResult = filter.bindingResult("12", 3);
+		Assertions.assertThat(bindingResult).isEmpty();
+
+		temp = List.of(1, 2, 3);
+		bindingResult = filter.bindingResult("123", 3);
+		Assertions.assertThat(bindingResult).isEqualTo(temp);
 	}
 }
