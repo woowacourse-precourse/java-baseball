fb0e87bb499e6439748d26bc4e59ae11f54a267b
anneu <b.imnuj@gmail.com>
2022-11-05 17:33:41 +0900

    feat: 입력 값이 1~9에 해당하는지 확인하는 기능 구현

--- src/main/java/baseball/validation/InputValidation.java
+++ src/main/java/baseball/validation/InputValidation.java
@@ -4,4 +4,16 @@ public class InputValidation {
     public boolean isNotThreeLetters(String input) {
         return input.length() != 3;
     }
+
+    public boolean isNot1To9(String input) {
+        char[] chars = input.toCharArray();
+
+        for (char letter : chars) {
+            if (!('1' <= letter && letter <= '9')) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
