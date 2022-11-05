1510960d47ccca16c9c66a4feb63c47bc15a4f75
chanminmaxscaler <jcm04@naver.com>
2022-11-05 11:22:18 +0900

    feat: Messenger 클래스의 printInputMessage 메소드를 구현한다
    printInputMessage 메소드는 "숫자를 입력하세요. : " 메시지를 출력한다.

--- src/main/java/baseball/view/Messenger.java
+++ src/main/java/baseball/view/Messenger.java
@@ -5,4 +5,8 @@ public class Messenger {
     public void printStartMessage() {
         System.out.print("게임을 시작합니다.\n");
     }
+
+    public void printInputMessage() {
+        System.out.print("숫자를 입력하세요. : ");
+    }
 }
