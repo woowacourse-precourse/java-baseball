147b281d9b63c0b4f1f54295506f2ee953e6ae12
bini59 <myuni0234@gmail.com>
2022-11-06 00:44:34 +0900

    test(compareNums) : test compareNums()
    test compareNums()
    - check compareNums with data
    - user : 1 2 3, computer 1 2 3 -> 0 3
    - user : 1 2 4, computer 1 2 3 -> 0 2
    - user : 3 1 2, computer 1 2 3 -> 3 0
    - user : 3 2 1, computer 1 2 3 -> 1 2
    - user : 4 5 6, computer 1 2 3 -> 0 0

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -61,6 +61,23 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("generateCompareData")
+    void 컴퓨터_유저_숫자_비교_테스트(final List<Integer> user, final List<Integer> computer, final List<Integer> res){
+        List<Integer> test_res = Application.compareNums(user, computer);
+        assertThat(res.equals(test_res));
+    }
+
+    static Stream<Arguments> generateCompareData(){
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3), Arrays.asList(0, 3)),
+                Arguments.of(Arrays.asList(1, 2, 4), Arrays.asList(1, 2, 3), Arrays.asList(0, 2)),
+                Arguments.of(Arrays.asList(3, 1, 2), Arrays.asList(1, 2, 3), Arrays.asList(3, 0)),
+                Arguments.of(Arrays.asList(3, 2, 1), Arrays.asList(1, 2, 3), Arrays.asList(1, 2)),
+                Arguments.of(Arrays.asList(4, 5, 6), Arrays.asList(1, 2, 3), Arrays.asList(0, 0))
+        );
+    }
+
 
 
     @Override
