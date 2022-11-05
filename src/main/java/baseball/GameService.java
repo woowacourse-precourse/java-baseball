1c2dc4ea596e059bacb06945d0698ffa5e993864
heesang <dlagmltkd12@naver.com>
2022-11-05 17:52:59 +0900

    feat: 컴퓨터가 1~9까지 서로 다른 랜덤 볼 3개를 선택하는 기능

--- src/main/java/baseball/GameService.java
+++ src/main/java/baseball/GameService.java
@@ -7,6 +7,10 @@ public class GameService {
     private final Player player;
     private final Player computer;
 
+    public GameService(List<Integer> playerNumbers) {
+        this(playerNumbers, RandomNumberCreator.createRandomNumbers());
+    }
+
     public GameService(List<Integer> playerNumbers, List<Integer> computerNumbers) {
         this.player = new Player(playerNumbers);
         this.computer = new Player(computerNumbers);
