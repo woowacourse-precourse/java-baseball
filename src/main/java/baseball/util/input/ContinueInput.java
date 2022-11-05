667c012c9113e5afbdc3186b8b31ea14afd58ed2
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-06 00:01:01 +0900

    refactor<ContinueInput>: 검증 함수 분리
    - 기존 검증 코드를 함수로 분리

--- src/main/java/baseball/util/input/ContinueInput.java
+++ src/main/java/baseball/util/input/ContinueInput.java
@@ -1,5 +1,7 @@
 package baseball.util.input;
 
+import baseball.util.GameExceptionMessage;
+
 public class ContinueInput implements UserInput {
     private final String continueInput;
     private final int CONTINUE_GAME = '1';
@@ -11,18 +13,23 @@ public class ContinueInput implements UserInput {
 
     @Override
     public void validate(String input) {
-        if (input.length() > 1) {
-            throw new IllegalArgumentException("길이 1초과");
-        } else if (!(input.charAt(0) == CONTINUE_GAME || input.charAt(0) == EXIT_GAME)) {
-            throw new IllegalArgumentException("1혹은 2만 입력");
+        if (input.length() > 1 || !continueOrExit(input)) {
+            throw new IllegalArgumentException(GameExceptionMessage.CONTINUE_OR_EXIT);
         }
     }
 
+    private boolean continueOrExit(String input) {
+        return continueGame(input) || exitGame(input);
+    }
+
+    private boolean exitGame(String input) {
+        return input.charAt(0) == EXIT_GAME;
+    }
+
+    private boolean continueGame(String input) {
+        return input.charAt(0) == CONTINUE_GAME;
+    }
     public boolean continueGame() {
-        if (continueInput.charAt(0) == '1') {
-            return true;
-        }
-        return false;
+        return continueGame(continueInput);
     }
-
 }
