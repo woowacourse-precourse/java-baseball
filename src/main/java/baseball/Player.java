0e0239ff61ff157e22f4b22da8b9589139a0ce62
MinHyeok Moon <odo27@MinHyeoks-MacBook-Air.local>
2022-11-05 12:48:26 +0900

    feat(Player): add isOneLetter

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -54,4 +54,8 @@ public class Player {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
+
+    public boolean isOneLetter(String restart) {
+        return restart.length() == 1;
+    }
 }
