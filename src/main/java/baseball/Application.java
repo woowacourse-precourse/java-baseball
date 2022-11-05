9cdade958c526263916d2696fe509d28b4b68012
lunchRamen <bloom6561@naver.com>
2022-11-06 05:01:09 +0900

    feat(Computer): 숫자야구 프로젝트 & 컴퓨터 객체를 통한 랜덤한 3개의 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import java.util.*;
+import camp.nextstep.edu.missionutils.*;
+
+
+class Computer{
+    private List<Integer> numbers = new ArrayList<>();
+
+	public List<Integer> getNumbers(){return this.numbers;}
+	public void generateNumbers(){
+		while (this.getNumbers().size()<3) {
+			int randomNumber = Randoms.pickNumberInRange(1,9);
+			if (!this.getNumbers().contains(randomNumber)){
+				this.getNumbers().add(randomNumber);
+			}
+		}
+	}
+}
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        Computer computer = new Computer();
+		computer.generateNumbers();
+
     }
 }
