234d0f27af0f8170d741550ddcaf7bf9433a188c
bellljun3395 <195850@jnu.ac.kr>
2022-11-05 16:15:59 +0900

    feat(Computer): 구현 Computer judge 메서드
    - 게임 플레이어의 숫자와 컴퓨터의 숫자를 비교하여 힌트를 제공할 수 있어야 한다.

--- src/main/java/baseball/player/Computer.java
+++ src/main/java/baseball/player/Computer.java
@@ -5,10 +5,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Computer extends Player {
+public class Computer extends Player implements JudgeAble {
     private static final int NUMBER_SIZE = 3;
     private static final int RANDOM_NUMBER_START = 1;
     private static final int RANDOM_NUMBER_END = 9;
+    private static final int HINT_INITIAL_VALUE = 0;
+    private static final int BALL_INDEX = 0;
+    private static final int STRIKE_INDEX = 1;
+
     public static int makeRandomNumber() {
         List<Integer> randomNumberList = new ArrayList<>();
         while (randomNumberList.size() < NUMBER_SIZE) {
@@ -28,4 +32,41 @@ public class Computer extends Player {
         }
         return randomNumber;
     }
+
+    @Override
+    public List<Integer> judge(int number) {
+        String baseNumber = String.valueOf(super.getNumber());
+        String compareNumber = String.valueOf(number);
+        List<String> containNumbers = filterContainNumbers(baseNumber, compareNumber);
+        List<Integer> hints = judgeCount(baseNumber, compareNumber, containNumbers);
+        return hints;
+    }
+
+    private List<String> filterContainNumbers(String baseNumber, String compareNumber) {
+        ArrayList<String> containNumbers = new ArrayList<>();
+        for (String number : compareNumber.split("")) {
+            if (baseNumber
+                    .contains(number)) {
+                containNumbers.add(number);
+            }
+        }
+        return containNumbers;
+    }
+
+    private List<Integer> judgeCount(String baseNumber, String compareNumber, List<String> containNumbers) {
+        ArrayList<Integer> hints = new ArrayList<>(List.of(HINT_INITIAL_VALUE, HINT_INITIAL_VALUE));
+        for (String containNumber : containNumbers) {
+            if (baseNumber
+                    .indexOf(containNumber)
+                    ==
+                    compareNumber
+                            .indexOf(containNumber)) {
+                hints.set(STRIKE_INDEX, hints.get(STRIKE_INDEX) + 1);
+            } else {
+                hints.set(BALL_INDEX, hints.get(BALL_INDEX) + 1);
+            }
+        }
+        return hints;
+    }
 }
+
