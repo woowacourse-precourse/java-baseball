42e62d2b5b10b330b3b35d627b97e668fc27c320
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-05 16:25:17 +0900

    style: sort code

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,38 +5,36 @@ import camp.nextstep.edu.missionutils.Console;
 
 class GameReadyMachine {
     private int answerValue;
-    public boolean checkDigitValue(int value){
+
+    public boolean checkDigitValue(int value) {
         int hundredDigits = value / 100;
-        value = value%100;
+        value = value % 100;
         int tensDigits = value / 10;
-        value = value%10;
+        value = value % 10;
         int oneDigits = value;
 
-        if(hundredDigits==tensDigits)
-        {
+        if (hundredDigits == tensDigits) {
             return false;
-        }
-        else if(tensDigits==oneDigits)
-        {
+        } else if (tensDigits == oneDigits) {
             return false;
-        }
-        else if(hundredDigits==oneDigits)
-        {
+        } else if (hundredDigits == oneDigits) {
             return false;
-        }
-        else
-        {
+        } else {
             return true;
         }
     }
-    public boolean setAnswerValue (){
-        answerValue = Randoms.pickNumberInRange(100,999);
-        if(answerValue>=100||answerValue<=999)
+
+    public boolean setAnswerValue() {
+        answerValue = Randoms.pickNumberInRange(100, 999);
+        if (answerValue >= 100 || answerValue <= 999)
             return true;
         else
             return false;
     }
+
+
 }
+
 public class Application {
     public static void main(String[] args) {
 
