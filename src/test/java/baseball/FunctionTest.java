d810347451c40b207fdb8da9791dcb2444146027
jthugg <jthug@naver.com>
2022-11-06 08:04:50 +0900

    Test: 컴퓨터의 숫자 리스트 생성 테스트

--- src/test/java/baseball/FunctionTest.java
+++ src/test/java/baseball/FunctionTest.java
@@ -1,5 +1,27 @@
 package baseball;
 
-public class FunctionTest {
+import static org.assertj.core.api.Assertions.assertThat;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.regex.Pattern;
+import org.junit.jupiter.api.Test;
+
+public class FunctionTest extends NsTest {
+
+	@Test
+	void TestSetComputersList() {
+		Computer computer = new Computer();
+		computer.setComputersList();
+
+		for(int i = 0; i < 3; i++) {
+			assertThat(computer.computersList.get(i)).isBetween(1, 9);
+		}
+
+		assertThat(computer.computersList.size()).isEqualTo(3);
+	}
+
+	@Override
+	public void runMain() {
+		Application.main(new String[]{});
+	}
 }
