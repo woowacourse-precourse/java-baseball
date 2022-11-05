4b1da886b3c926024c5650d02ef261620dbf20e7
wlsh44 <k87913j@gmail.com>
2022-11-05 20:47:28 +0900

    test: 3자리 숫자 변환 테스트 코드 추가

---
+++ src/test/java/baseball/console/ThreeNumberConverterTest.java
@@ -0,0 +1,162 @@
+package baseball.console;
+
+import baseball.converter.ThreeNumberConverter;
+import baseball.exception.DuplicateNumberException;
+import baseball.exception.GameEachInputNumberOutOfRangeException;
+import baseball.exception.GameInputNumberOutOfRangeException;
+import baseball.exception.InputNumberFormatException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ThreeNumberConverterTest {
+
+    ThreeNumberConverter converter;
+
+    @BeforeEach
+    void init() {
+        converter = new ThreeNumberConverter();
+    }
+
+    @Test
+    @DisplayName("정상 출력 케이스1")
+    void successCase1() {
+        //given
+        String inputString = "528";
+        List<Integer> expected = List.of(5, 2, 8);
+
+        //when
+        List<Integer> res = converter.getThreeNumbers(inputString);
+
+        //then
+        assertThat(res).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("정상 출력 케이스2")
+    void successCase2() {
+        //given
+        String inputString = "498";
+        List<Integer> expected = List.of(4, 9, 8);
+
+        //when
+        List<Integer> res = converter.getThreeNumbers(inputString);
+
+        //then
+        assertThat(res).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("올바른 숫자가 아닌 경우 - 문자")
+    void inputNumberFormatException_case1() {
+        //given
+        String inputString = "hello";
+
+        //when then
+        assertThatThrownBy(() -> converter.getThreeNumbers(inputString))
+                .isInstanceOf(InputNumberFormatException.class)
+                .hasMessage("잘못된 숫자 입력입니다. 입력 문자: " + inputString);
+    }
+
+
+    @Test
+    @DisplayName("올바른 숫자가 아닌 경우 - 음수")
+    void inputNumberFormatException_case2() {
+        //given
+        String inputString = "-123";
+
+        //when then
+        assertThatThrownBy(() -> converter.getThreeNumbers(inputString))
+                .isInstanceOf(InputNumberFormatException.class)
+                .hasMessage("잘못된 숫자 입력입니다. 입력 문자: " + inputString);
+    }
+
+    @Test
+    @DisplayName("1~9 사이의 숫자가 아닌 경우 - 012")
+    void gameEachInputNumberOutOfRangeException_case1() {
+        //given
+        String inputString = "012";
+
+        //when then
+        assertThatThrownBy(() -> converter.getThreeNumbers(inputString))
+                .isInstanceOf(GameEachInputNumberOutOfRangeException.class)
+                .hasMessage("1~9 사이의 숫자를 입력해주세요. 입력 문자: " + inputString);
+    }
+
+    @Test
+    @DisplayName("1~9 사이의 숫자가 아닌 경우 - 709")
+    void gameEachInputNumberOutOfRangeException_case2() {
+        //given
+        String inputString = "709";
+
+        //when then
+        assertThatThrownBy(() -> converter.getThreeNumbers(inputString))
+                .isInstanceOf(GameEachInputNumberOutOfRangeException.class)
+                .hasMessage("1~9 사이의 숫자를 입력해주세요. 입력 문자: " + inputString);
+    }
+
+    @Test
+    @DisplayName("3자리 숫자가 아닌 경우 - 1234")
+    void gameInputNumberOutOfRangeException_case1() {
+        //given
+        String inputString = "1234";
+
+        //when then
+        assertThatThrownBy(() -> converter.getThreeNumbers(inputString))
+                .isInstanceOf(GameInputNumberOutOfRangeException.class)
+                .hasMessage("123~789 사이의 숫자를 입력해주세요. 입력 문자: " + inputString);
+    }
+
+    @Test
+    @DisplayName("3자리 숫자가 아닌 경우 - 1244")
+    void gameInputNumberOutOfRangeException_case2() {
+        //given
+        String inputString = "1244";
+
+        //when then
+        assertThatThrownBy(() -> converter.getThreeNumbers(inputString))
+                .isInstanceOf(GameInputNumberOutOfRangeException.class)
+                .hasMessage("123~789 사이의 숫자를 입력해주세요. 입력 문자: " + inputString);
+    }
+
+    @Test
+    @DisplayName("3자리 숫자가 아닌 경우 - 12")
+    void gameInputNumberOutOfRangeException_case3() {
+        //given
+        String inputString = "12";
+
+        //when then
+        assertThatThrownBy(() -> converter.getThreeNumbers(inputString))
+                .isInstanceOf(GameInputNumberOutOfRangeException.class)
+                .hasMessage("123~789 사이의 숫자를 입력해주세요. 입력 문자: " + inputString);
+    }
+
+    @Test
+    @DisplayName("중복된 숫자가 있는 경우 - 144")
+    void duplicateNumberException_case1() {
+        //given
+        String inputString = "144";
+
+        //when then
+        assertThatThrownBy(() -> converter.getThreeNumbers(inputString))
+                .isInstanceOf(DuplicateNumberException.class)
+                .hasMessage("서로 다른 숫자를 입력해주세요. 입력 문자: " + inputString);
+    }
+
+    @Test
+    @DisplayName("중복된 숫자가 있는 경우 - 221")
+    void duplicateNumberException_case2() {
+        //given
+        String inputString = "221";
+
+        //when then
+        assertThatThrownBy(() -> converter.getThreeNumbers(inputString))
+                .isInstanceOf(DuplicateNumberException.class)
+                .hasMessage("서로 다른 숫자를 입력해주세요. 입력 문자: " + inputString);
+    }
+}
