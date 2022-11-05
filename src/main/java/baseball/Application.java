80833512f38467373eece8c1e4c585241c92d34d
jissssoo1717 <jisoolee7101@gmail.com>
2022-11-05 20:59:13 +0900

    test: main에서 Player의 숫자와 컴퓨터의 숫자 각각 출력 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,15 +2,24 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         PlayerInput player = new PlayerInput();
-        String input = Console.readLine();
+        Computer computer = new Computer();
+        List<Integer> numOfComputer = new ArrayList<Integer>();
 
+        String input = Console.readLine();
         player.InputTest(input);
 
+        numOfComputer = computer.GenerateRandomThreeNumber();
+
+        System.out.println("Player's number: " + input);
+        System.out.println("Computer's number: " + numOfComputer);
 
     }
 
