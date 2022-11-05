657238954861d545c12cbcc9dfe6277a6a86ec3a
sysliar <sysliar@gmail.com>
2022-11-06 02:47:48 +0900

    feat(Player): set random numbers
    Constraint
        - number between 1 and 9
        - three digits
        - not duplicated

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,4 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
 public class Player {
+    private static final int NUMBER_LENGTH = 3;
+    private static final int NUMBER_RANGE_START = 1;
+    private static final int NUMBER_RANGE_END = 9;
+
+    private ArrayList<Integer> number = new ArrayList<Integer>(NUMBER_LENGTH);
+
+    public void setRandomNumber() {
+        while (number.size() < NUMBER_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(NUMBER_RANGE_START, NUMBER_RANGE_END);
+            if (!number.contains(randomNumber)) {
+                number.add(randomNumber);
+            }
+        }
+    }
 }
