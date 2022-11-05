81e0f601d0c34a3b8c9a28a9b979463be747e2bf
saint6839 <saint6839@gmail.com>
2022-11-05 11:55:32 +0900

    feat(service) : 컴퓨터와 관련된 기능의 비즈니스 로직을 담당하는 ComputerService 생성

---
+++ src/main/java/baseball/service/ComputerService.java
@@ -0,0 +1,11 @@
+package baseball.service;
+
+import baseball.Ball;
+
+import java.util.List;
+
+public class ComputerService {
+    public List<Ball> generateRandomBalls() {
+        return null;
+    }
+}
