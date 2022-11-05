c1712adf426aeeafe780546f15fcbecca0e1ad74
SeokHwan An <asok369258@naver.com>
2022-11-06 00:34:18 +0900

    test : 생성된 문제가 1과 9사이의 숫자로 구성되어있는지 검증하는 테스트

---
+++ src/test/java/baseball/player/ComputerTest.java
@@ -0,0 +1,26 @@
+package baseball.player;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class ComputerTest {
+
+    private Computer computer = new Computer();
+
+    @Test
+    void 생성된_문제가_1과_9사이의_숫자로_구성되어있는지_검증하는_테스트() {
+        computer.createProblem();
+        List<Integer> problem = computer.getProblem();
+        boolean check = true;
+
+        for (int i = 0; i < problem.size(); i++) {
+            if (!(1 <= problem.get(i) && problem.get(i) <= 9)) {
+                check = false;
+            }
+        }
+
+        assertThat(check).isEqualTo(true);
+    }
+}
