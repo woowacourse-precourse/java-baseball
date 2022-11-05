2705bc41982fb7ab2a9f05ff766227bbba3df950
qdeu22 <qdeu22@gmail.com>
2022-11-05 12:59:14 +0900

    feat(ComputerRandomNumber): 컴퓨터 랜덤 기능 구현

--- src/main/java/baseball/model/ComputerRandomNumber.java
+++ src/main/java/baseball/model/ComputerRandomNumber.java
@@ -1,4 +1,29 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComputerRandomNumber {
+
+    private static List<Integer> computerNumber;
+
+    public ComputerRandomNumber() {
+        this.computerNumber = computerRandomNumber();
+    }
+
+    private List<Integer> computerRandomNumber() {
+        List<Integer> computerRandomNumber = new ArrayList<>();
+        while (computerRandomNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerRandomNumber.contains(randomNumber)) {
+                computerRandomNumber.add(randomNumber);
+            }
+        }
+        return computerRandomNumber;
+    }
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
 }
