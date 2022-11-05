f1c9cd956111b6ec43fd0e6addbddb22030a1da4
kkm5291 <kkm23125291@gmail.com>
2022-11-05 15:24:43 +0900

    Test : 쓰지 않는 라이브러리 삭제

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,7 +3,6 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.Executable;
 
 import java.util.ArrayList;
 import java.util.List;
