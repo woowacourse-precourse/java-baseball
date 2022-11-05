0a4480665baea4428201d63c9ca21837304d6e29
hoonn9 <hun9935@gmail.com>
2022-11-05 19:19:38 +0900

    test(NumberBoard): 테스트 코드 업데이트

--- src/test/java/baseball/board/NumberBoardTest.java
+++ src/test/java/baseball/board/NumberBoardTest.java
@@ -35,7 +35,6 @@ public class NumberBoardTest {
 
         HashMap<ComparisonResultType, Integer> result = new HashMap<>();
         result.put(ComparisonResultType.STRIKE, 2);
-        result.put(ComparisonResultType.BALL, 1);
 
         assertThat(numberBoard.compareNumbers(userNumbers, opponentNumbers)).isEqualTo(result);
     }
@@ -46,8 +45,57 @@ public class NumberBoardTest {
         List<Integer> opponentNumbers = List.of(4, 5, 6);
 
         HashMap<ComparisonResultType, Integer> result = new HashMap<>();
+
+        assertThat(numberBoard.compareNumbers(userNumbers, opponentNumbers)).isEqualTo(result);
+    }
+
+    @Test
+    void compareNumbers_메소드는_두_리스트를_비교한_빈도수_해시맵을_반환4() {
+        List<Integer> userNumbers = List.of(1, 2, 3);
+        List<Integer> opponentNumbers = List.of(2, 1, 3);
+
+        HashMap<ComparisonResultType, Integer> result = new HashMap<>();
+        result.put(ComparisonResultType.STRIKE, 1);
+        result.put(ComparisonResultType.BALL, 2);
+
+        assertThat(numberBoard.compareNumbers(userNumbers, opponentNumbers)).isEqualTo(result);
+    }
+
+    @Test
+    void compareNumbers_메소드는_두_리스트를_비교한_빈도수_해시맵을_반환5() {
+        List<Integer> userNumbers = List.of(1, 2, 3);
+        List<Integer> opponentNumbers = List.of(2, 3, 1);
+
+        HashMap<ComparisonResultType, Integer> result = new HashMap<>();
         result.put(ComparisonResultType.BALL, 3);
 
         assertThat(numberBoard.compareNumbers(userNumbers, opponentNumbers)).isEqualTo(result);
+    }
+
+    @Test
+    void getResultMessage_메소드는_각_비교_결과_타입별_개수를_공백을_사이로_문자열을_반환() {
+        List<Integer> userNumbers = List.of(1, 2, 3);
+        List<Integer> opponentNumbers = List.of(1, 2, 3);
+        numberBoard.compareNumbers(userNumbers, opponentNumbers);
+
+        assertThat(numberBoard.getResultMessage()).isEqualTo("3스트라이크");
+    }
+
+    @Test
+    void getResultMessage_메소드는_각_비교_결과_타입별_개수를_공백을_사이로_문자열을_반환2() {
+        List<Integer> userNumbers = List.of(1, 2, 3);
+        List<Integer> opponentNumbers = List.of(2, 1, 3);
+        numberBoard.compareNumbers(userNumbers, opponentNumbers);
+
+        assertThat(numberBoard.getResultMessage()).isEqualTo("2볼 1스트라이크");
+    }
+
+    @Test
+    void getResultMessage_메소드는_각_비교_결과_타입별_개수를_공백을_사이로_문자열을_반환3() {
+        List<Integer> userNumbers = List.of(1, 2, 3);
+        List<Integer> opponentNumbers = List.of(4, 5, 6);
+        numberBoard.compareNumbers(userNumbers, opponentNumbers);
+
+        assertThat(numberBoard.getResultMessage()).isEqualTo(NumberBoardConstant.NOT_MATCH_AT_ALL_MESSAGE);
     }
 }
