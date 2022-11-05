21d1247a3ea5653531b2cb0255d9ae9ff3124eb7
nn98 <jkllhgb@gmail.com>
2022-11-05 09:14:56 +0900

    Feat: 정수 판별 메시지 추가, 입력값 Illegal시 게임 종료 안내 추가

--- src/main/java/constant/Texts.java
+++ src/main/java/constant/Texts.java
@@ -7,8 +7,9 @@ public class Texts {
     public static final String GAME_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static final String USER_INPUT_NUMBER = "숫자를 입력해주세요 : ";
-    public static final String USER_INPUT_ILLEGAL = "잘못된 값을 입력했습니다! 게임을 종료합니다.";
-    public static final String USER_INPUT_ILLEGAL_LENGTH = "입력된 수가 3자리를 초과합니다. ";
+    public static final String USER_INPUT_ILLEGAL = "잘못된 값을 입력했습니다! "+GAME_END;
+    public static final String USER_INPUT_ILLEGAL_MISMATCHING = "입력된 값이 숫자가 아닙니다. ";
+    public static final String USER_INPUT_ILLEGAL_DIGIT = "입력된 수가 3자리를 초과합니다. ";
     public static final String USER_INPUT_ILLEGAL_DUPLICATED = "입력된 수에 중복값이 존재합니다. ";
 
     public static final String RUN_CHECK_STRIKE = "스트라이크";
