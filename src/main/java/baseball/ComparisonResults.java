8e79f7b7e3d38b46d66e2514457326a8db78e82a
mirasoy <nkskjjbb@gmail.com>
2022-11-05 23:35:21 +0900

    [Feat](ComparisonResults) 컴퓨터와 입력값을 비교한 결과값을 관리하는 class생성

---
+++ src/main/java/baseball/ComparisonResults.java
@@ -0,0 +1,16 @@
+package baseball;
+
+
+/**
+* ComparisonResults
+* 컴퓨터와 입력값을 비교한 결과값
+* 
+* @author mirasoy
+* @version 0.1, 작업 내용
+*/
+public class ComparisonResults {
+	int strike; 	//Strike의 갯수
+	int ball;		//ball의 갯수
+
+	
+}
