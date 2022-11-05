2fb3afee4bedc660aa9abd5560241ec36ac47211
dmdrk1414 <seungchan141414@gmail.com>
2022-11-05 19:22:22 +0900

    :fire:[Remove] 필요없는 테스트의 객체 생성 제거

--- src/test/java/baseball/GameViewTest.java
+++ src/test/java/baseball/GameViewTest.java
@@ -2,18 +2,10 @@ package baseball;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import view.GameView;
 
 class GameViewTest {
-
-    private static GameView gameView;
-
-    @BeforeAll // (테스트 콜한때 한번 출력)
-    static void initAll() {
-        gameView = new GameView();
-    }
 
     @Test
     void 게임_다시_시작_인풋_테스트_1() throws Exception {
