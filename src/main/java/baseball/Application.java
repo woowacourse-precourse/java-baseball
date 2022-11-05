c45a8f525857dfa1a933fb826e88bbc74760a249
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-06 00:25:08 +0900

    feat: Baseball class 생성 및 상수 선언

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,3 +5,18 @@ public class Application {
         // TODO: 프로그램 구현
     }
 }
+
+class Baseball {
+    private static final int START_NUM_INCLUSIVE = 1;
+    private static final int END_NUM_INCLUSIVE = 9;
+    private static final int NUMBER_PITCH = 3;
+    private static final int NEW_GAME = 1;
+    private static final int END_GAME = 2;
+    private static final String BALL = "볼 ";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
