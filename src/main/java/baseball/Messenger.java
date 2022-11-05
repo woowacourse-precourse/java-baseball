1589bcf192c94c471e26c9eafac46228a3a02d1f
BoyCho <iijokv@naver.com>
2022-11-06 00:37:16 +0900

    feat : 입력 결과에 따른 생성 메시지 출력 기능 추가

--- src/main/java/baseball/Messenger.java
+++ src/main/java/baseball/Messenger.java
@@ -3,6 +3,11 @@ package baseball;
 public class Messenger {
     private String message = "";
 
+    public void printResultMessage (AnswerChecker answerChecker) {
+        makeResultMessage(answerChecker.getStrikeCnt(), answerChecker.getBallCnt());
+        System.out.println(message);
+    }
+
     public void makeResultMessage (int strikeCnt, int ballCnt) {
         if (ballCnt > 0) {
             message += (ballCnt - '0') + "볼 ";
