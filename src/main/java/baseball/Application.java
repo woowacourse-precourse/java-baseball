373f021c9fb0fa81882dd4c8c449d4cc4fe1f468
tdmdgh <tdmdgh@gmail.com>
2022-11-05 15:50:04 +0800

    feat: add createTargetNums method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,20 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class Application {
+    private static List<Integer> createTargetNums(){
+        Random r = new Random();
+        List<Integer> targetNums = new ArrayList<>();
+        int digit = 3;
+        while(digit > 0 ){
+            targetNums.add(r.nextInt(9)+1);
+        }
+
+        return targetNums;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
