c567dee19e29fd129782f569b916bbf19ed2cc44
kimJaeHyeon <kmr2644@gmail.com>
2022-11-05 16:21:02 +0900

    feat: 게임종료 문자열과 다음 선택을 입력받는 메서드 구현

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -8,4 +8,11 @@ public class View {
         System.out.print("숫자를 입력해주세요 :");
         return Console.readLine();
     }
+
+    public String inputExitOrRestart() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+        return Console.readLine();
+    }
+
 }
