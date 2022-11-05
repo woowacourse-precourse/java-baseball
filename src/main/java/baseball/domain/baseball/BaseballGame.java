ec99c4997cbe45899d42d033004dcb4ac5e2b225
JaehongDev <println@kakao.com>
2022-11-06 00:03:00 +0900

    refactor (BaseballGame) : 게임의 책임을 컨텍스트로 변경

--- src/main/java/baseball/domain/baseball/BaseballGame.java
+++ src/main/java/baseball/domain/baseball/BaseballGame.java
@@ -1,53 +1,15 @@
 package baseball.domain.baseball;
 
-import baseball.domain.baseball.status.BaseballGameStatus;
-import baseball.domain.baseball.status.InitStatus;
-import baseball.domain.player.Computer;
-import baseball.domain.player.Participant;
-import baseball.domain.player.Player;
-import baseball.domain.referee.Referee;
-import baseball.domain.score.Score;
-import camp.nextstep.edu.missionutils.Console;
-
 public class BaseballGame {
-    private final Referee referee;
-    private BaseballGameStatus gameStatus;
-    private Player computer;
-    private Player participant;
+    private final BaseballContext baseballContext;
 
-    public BaseballGame() {
-        this.referee = new Referee();
-        this.gameStatus = new InitStatus();
+    public BaseballGame(BaseballContext baseballContext) {
+        this.baseballContext = baseballContext;
     }
 
     public void run() {
-        while (gameStatus.runAble()) {
-            this.gameStatus = gameStatus.next(this);
+        while (baseballContext.runAble()) {
+            baseballContext.next();
         }
-    }
-
-
-    public String input() {
-        return Console.readLine();
-    }
-
-    public void print(final Object message) {
-        System.out.print(message);
-    }
-
-    public void println(final Object message) {
-        System.out.println(message);
-    }
-
-    public void initializeComputerNumber() {
-        this.computer = new Computer();
-    }
-
-    public void initializeParticipantNumber(String input) {
-        this.participant = new Participant(input);
-    }
-
-    public Score ruled() {
-        return referee.ruled(computer.numbers(), participant.numbers());
     }
 }
