b3990999c10fb9a6c79254658878aa24fa0249cd
ineiw <75738349+ineiw@users.noreply.github.com>
2022-11-05 15:36:22 +0900

    feat: 컴퓨터의 힌트 계산 기능 추가
    사용자의 입력을 받아서 힌트를 위한 계산을하고 힌트를 출력하는 함수를 작성

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,11 +2,12 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Computer {
     List<Integer> computer = new ArrayList<>();
-
+    
     public Computer() {
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -18,5 +19,21 @@ public class Computer {
 
     public List<Integer> getRandomNumber() {
         return computer;
+    }
+
+    public List<Integer> calcHint(String hint) {
+        int firstElementOfHint = 0;
+        int secondElementOfHint = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (computer.toString().charAt(i) == hint.charAt(i)) {
+                firstElementOfHint += 1;
+            }
+            if (computer.toString().contains(Character.toString(hint.charAt(i)))) {
+                secondElementOfHint += 1;
+            }
+        }
+
+        return Arrays.asList(firstElementOfHint, secondElementOfHint);
     }
 }
