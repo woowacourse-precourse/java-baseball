958914756067efba5c0f5bf9eec6aa062a481b73
kim0914 <kosa0914@naver.com>
2022-11-05 19:30:50 +0900

    test(User): 재시작 응답 값에 따른 테스트 추가

--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -38,7 +38,7 @@ class UserTest {
         System.setIn(in);
 
         // then
-        assertThatThrownBy(() -> user.enterNumber())
+        assertThatThrownBy(user::enterNumber)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력 숫자의 크기(길이)를 확인하세요.");
     }
@@ -54,7 +54,7 @@ class UserTest {
         System.setIn(in);
 
         // then
-        assertThatThrownBy(() -> user.enterNumber())
+        assertThatThrownBy(user::enterNumber)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력 숫자의 크기(길이)를 확인하세요.");
     }
@@ -70,7 +70,7 @@ class UserTest {
         System.setIn(in);
 
         // then
-        assertThatThrownBy(() -> user.enterNumber())
+        assertThatThrownBy(user::enterNumber)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("1~9 까지의 수 조건에 충족하지 않습니다.");
     }
@@ -86,7 +86,7 @@ class UserTest {
         System.setIn(in);
 
         // then
-        assertThatThrownBy(() -> user.enterNumber())
+        assertThatThrownBy(user::enterNumber)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("1~9 까지의 수 조건에 충족하지 않습니다.");
     }
@@ -102,8 +102,100 @@ class UserTest {
         System.setIn(in);
 
         // then
-        assertThatThrownBy(() -> user.enterNumber())
+        assertThatThrownBy(user::enterNumber)
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("1~9 까지의 수 조건에 충족하지 않습니다.");
+    }
+
+    @Test
+    void 재시작_응답이_1인경우_참_반환() {
+        // given
+        String retryIntention = "1";
+
+        // when
+        User user = new User();
+        InputStream in = new ByteArrayInputStream(retryIntention.getBytes(StandardCharsets.UTF_8));
+        System.setIn(in);
+
+        // then
+        assertThat(user.hasRetryIntention()).isEqualTo(true);
+    }
+
+    @Test
+    void 재시작_응답이_2인경우_거짓_반환() {
+        // given
+        String retryIntention = "2";
+
+        // when
+        User user = new User();
+        InputStream in = new ByteArrayInputStream(retryIntention.getBytes(StandardCharsets.UTF_8));
+        System.setIn(in);
+
+        // then
+        assertThat(user.hasRetryIntention()).isEqualTo(false);
+    }
+
+    @Test
+    void 재시작_응답이_조건에_맞지않는_경우_예외_발생1() {
+        // given
+        String retryIntention = "a";
+
+        // when
+        User user = new User();
+        InputStream in = new ByteArrayInputStream(retryIntention.getBytes(StandardCharsets.UTF_8));
+        System.setIn(in);
+
+        // then
+        assertThatThrownBy(user::hasRetryIntention)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("게임 재시작 여부는 1또는 2 숫자만 가능합니다.");
+    }
+
+    @Test
+    void 재시작_응답이_조건에_맞지않는_경우_예외_발생2() {
+        // given
+        String retryIntention = "0";
+
+        // when
+        User user = new User();
+        InputStream in = new ByteArrayInputStream(retryIntention.getBytes(StandardCharsets.UTF_8));
+        System.setIn(in);
+
+        // then
+        assertThatThrownBy(user::hasRetryIntention)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("게임 재시작 여부는 1또는 2 숫자만 가능합니다.");
+    }
+
+    @Test
+    void 재시작_응답이_조건에_맞지않는_경우_예외_발생3() {
+        // given
+        String retryIntention = "asd";
+
+        // when
+        User user = new User();
+        InputStream in = new ByteArrayInputStream(retryIntention.getBytes(StandardCharsets.UTF_8));
+        System.setIn(in);
+
+        // then
+        assertThatThrownBy(user::hasRetryIntention)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("게임 재시작 여부는 1또는 2 숫자만 가능합니다.");
+    }
+
+    @Test
+    void 재시작_응답이_조건에_맞지않는_경우_예외_발생4() {
+        // given
+        String retryIntention = "123";
+
+        // when
+        User user = new User();
+        InputStream in = new ByteArrayInputStream(retryIntention.getBytes(StandardCharsets.UTF_8));
+        System.setIn(in);
+
+        // then
+        assertThatThrownBy(user::hasRetryIntention)
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("게임 재시작 여부는 1또는 2 숫자만 가능합니다.");
     }
 }
