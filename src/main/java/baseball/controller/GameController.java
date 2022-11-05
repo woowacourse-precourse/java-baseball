f90f283bf9a96d5b0fb78088276a5f5a048bb34b
chopinballadeno4 <qo98333@naver.com>
2022-11-05 11:30:47 +0900

    feat: Generate random number , Apply counterpartmodel

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -4,6 +4,11 @@ import baseball.model.CounterpartModel;
 import baseball.view.AppView;
 
 public class GameController {
+    private CounterpartModel counterpart;
+
+    public GameController() {
+        this.counterpart = new CounterpartModel();
+    }
 
     public void run() {
         AppView.outputLine("숫자 야구 게임을 시작합니다.");
