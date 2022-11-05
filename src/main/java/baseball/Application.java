aefeebf1dc6b2189f5b2f152f038b31e2a76c80e
DaWoon Lee <qkgk61@gmail.com>
2022-11-06 03:44:45 +0900

    Feat : 전체 게임을 실행하는 Application 클래스 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private final static int CONTINUE = 1;
+    private final static int EXIT = 2;
+    private final static int DIGIT = 3;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        View view = View.getInstance();
+        Judge judge = Judge.getInstance();
+        DigitNumber digitNumber = DigitNumber.getInstance(DIGIT);
+        int command = CONTINUE;
+
+        while (command == CONTINUE) {
+            Game game = new Game(view, judge, digitNumber);
+
+            try {
+                game.play();
+                command = Integer.parseInt(Console.readLine());
+                Validator.isRestartCommand(command);
+            } catch (IllegalArgumentException e) {
+                command = EXIT;
+            }
+        }
     }
+
 }
