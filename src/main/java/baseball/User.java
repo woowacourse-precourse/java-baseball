9d51415a99913ac485a254114cc6f168f3b4375d
Ridealist <cjsn92@gmail.com>
2022-11-05 17:27:20 +0900

    refactor(User): userInput 필드 및 setUserInput 메소드 정적 멤버 해제

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -4,11 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class User {
 
-    private static String userInput;
+    private String userInput;
 
-    public static void setUserInput(String userInput) {
+    public void setUserInput() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        User.userInput = input;
+        userInput = input;
     }
 }
--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -1,13 +1,22 @@
 package baseball;
 
 import java.io.*;
+import java.lang.reflect.Field;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
+    private static User user;
+
+    @BeforeAll
+    static void initAll() {
+        user = new User();
+    }
+
     @Test
     void setUserInput_WhenRun_PrintStartStatement() {
         String input = "123";
@@ -15,7 +24,26 @@ class UserTest {
         System.setIn(in);
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
-        User.setUserInput(input);
+        user.setUserInput();
         assertThat(out.toString()).isEqualTo("숫자를 입력해주세요 : ");
+    }
+
+    @Test
+    void setUserInput_WhenRun_setUserInput_Success() {
+        String input = "123";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        user.setUserInput();
+        try {
+            Field field = user.getClass().getDeclaredField("userInput");
+            field.setAccessible(true);
+
+            String value = String.valueOf(field.get(user));
+            assertThat(value).isEqualTo("123");
+        } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
     }
 }
