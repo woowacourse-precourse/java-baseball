a662baddeb0a6152f3a2c9a4285a18169d165bd0
ffolabear <ffolabear@gmail.com>
2022-11-05 17:13:05 +0900

    feat(Computer): 숫자 생성하는 메서드 리턴값 변경

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -2,6 +2,7 @@ package baseball.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
@@ -10,17 +11,32 @@ public class Computer {
     final int COMPUTER_NUMBER_END = 9;
     final int COMPUTER_NUMBER_SIZE = 3;
 
-    public String generateComputerAnswer() {
+    public List<String> generateComputerAnswer() {
         List<Integer> generatedNumbers = Randoms.pickUniqueNumbersInRange(COMPUTER_NUMBER_START, COMPUTER_NUMBER_END, COMPUTER_NUMBER_SIZE);
-        return parsing(generatedNumbers);
+        return toStringList(generatedNumbers);
     }
 
-    private String parsing(List<Integer> generatedNumbers) {
-        StringBuilder parsedNumbers = new StringBuilder();
-        for (Integer generatedNumber : generatedNumbers) {
-            parsedNumbers.append(String.valueOf(generatedNumber));
+//    public String generateComputerAnswer() {
+//        List<Integer> generatedNumbers = Randoms.pickUniqueNumbersInRange(COMPUTER_NUMBER_START, COMPUTER_NUMBER_END, COMPUTER_NUMBER_SIZE);
+//        return parsing(generatedNumbers);
+//    }
+
+//    private String parsing(List<Integer> generatedNumbers) {
+//        StringBuilder parsedNumbers = new StringBuilder();
+//        for (Integer generatedNumber : generatedNumbers) {
+//            parsedNumbers.append(String.valueOf(generatedNumber));
+//        }
+//        return parsedNumbers.toString();
+//    }
+
+    private List<String> toStringList(List<Integer> generatedNumbers) {
+        List<String> parsedList = new ArrayList<>();
+        for (Integer i : generatedNumbers) {
+            parsedList.add(String.valueOf(i));
         }
-        return parsedNumbers.toString();
+        return parsedList;
     }
+
+
 
 }
