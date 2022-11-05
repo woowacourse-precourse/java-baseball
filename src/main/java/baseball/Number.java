dbf83efb139064ef58a6bc8328034a48e4f8b4ca
김연진 <love_homebody@naver.com>
2022-11-05 21:24:31 +0900

    feat: 사용자 입력값 예외 처리 기능 구현

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -41,6 +41,39 @@ public class Number {
             userInputNumber.add(n);
             number /= 10;
         }
+        if (!isValidInput(userInput)) {
+            throw new IllegalArgumentException();
+        }
         Collections.reverse(userInputNumber);
+    }
+
+    public boolean isValidInput(String userInput) {
+        if (!isValidLength(userInput) || !isValidString(userInput) || !isNotDuplicate()) {
+            return false;
+        }
+        return true;
+    }
+    public boolean isValidLength(String userInput) {
+        if (userInput.length() != 3) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isValidString(String userInput) {
+        for (int i = 0; i < userInput.length(); i++) {
+            if (userInput.charAt(i) > 57 || userInput.charAt(i) < 49) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isNotDuplicate() {
+        Set<Integer> userInputSet = new HashSet<>(userInputNumber);
+        if (userInputSet.size() != 3) {
+            return false;
+        }
+        return true;
     }
 }
--- src/test/java/baseball/NumberTest.java
+++ src/test/java/baseball/NumberTest.java
@@ -1,12 +1,13 @@
 package baseball;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class NumberTest {
 
@@ -30,5 +31,20 @@ public class NumberTest {
         result.add(2);
         result.add(3);
         assertThat(number.userInputNumber).isEqualTo(result);
+    }
+
+    @Test
+    void 올바르지_않은_길이() {
+        assertThrows(IllegalArgumentException.class, () -> number.userInput("1234"));
+    }
+
+    @Test
+    void 숫자_이외_문자_입력() {
+        assertThrows(IllegalArgumentException.class, () -> number.userInput("12A"));
+    }
+
+    @Test
+    void 중복된_숫자_입력() {
+        assertThrows(IllegalArgumentException.class, () -> number.userInput("122"));
     }
 }
