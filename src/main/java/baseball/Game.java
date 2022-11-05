078728cfd5f7aa9adb311d56e672243379e1b96a
leeyongjun <leeyongjun0310@naver.com>
2022-11-05 20:54:53 +0900

    feat(Game): 게임을 새로 시작할지 여부를 입력받는 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -8,6 +8,9 @@ import camp.nextstep.edu.missionutils.Console;
 public class Game {
     private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_SENTENCE = "숫자를 입력해주세요 : ";
+    private static final String WIN_SENTENCE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_SENTENCE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     private static final String BALL_TEXT = "볼 ";
     private static final String STRIKE_TEXT = "스트라이크";
     private static final String NOTHING_TEXT = "낫싱";
@@ -51,5 +54,9 @@ public class Game {
             gameResult.append(this.score.getStrike()).append(STRIKE_TEXT);
         }
         return gameResult.toString();
+    }
+
+    public boolean restart() {
+        return Console.readLine().equals("1");
     }
 }
