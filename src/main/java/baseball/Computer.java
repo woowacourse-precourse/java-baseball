76853a27d3d609d7ade5b56afec37727dac3a6cf
h0ngwon <w___on@naver.com>
2022-11-05 22:09:09 +0900

    feat(Computer) : 난수 생성 기능

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,4 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
+  private List<Integer> computerNumbers;
+  public Computer() {
+  }
+
+  private void generateNumbers() {
+    computerNumbers = new ArrayList<>();
+    while(computerNumbers.size() < 3) {
+      int randomNumber = Randoms.pickNumberInRange(1, 9);
+      if (!computerNumbers.contains(randomNumber)) {
+        computerNumbers.add(randomNumber);
+      }
+    }
+  }
 }
