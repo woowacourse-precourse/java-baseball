dd00e72eed26835ff562210468c5992053705ccc
rt3310 <btac3310@gmail.com>
2022-11-06 02:34:10 +0900

    feat: 심판이 스트라이크와 볼 카운트를 측정하는 기능 구현

--- src/main/java/baseball/Ball.java
+++ src/main/java/baseball/Ball.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.List;
+
 public class Ball {
     private int number;
     private int order;
@@ -9,7 +11,32 @@ public class Ball {
         this.order = order;
     }
 
+    public BaseballJudge getResult(List<Ball> balls) {
+        if (isStrike(balls)) {
+            return BaseballJudge.STRIKE;
+        }
+
+        if (isBall(balls)) {
+            return BaseballJudge.BALL;
+        }
+
+        return BaseballJudge.NOTHING;
+    }
+
+    private boolean isStrike(List<Ball> balls) {
+        return number == balls.get(order).getNumber();
+    }
+
+    private boolean isBall(List<Ball> balls) {
+        return balls.stream().anyMatch(ball ->
+                ball.getNumber() == this.number && ball.getOrder() != this.order);
+    }
+
     public int getNumber() {
         return number;
+    }
+
+    public int getOrder() {
+        return order;
     }
 }
--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,4 +1,37 @@
 package baseball;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Referee {
+    private int strikeCount;
+    private int ballCount;
+
+    public Referee() {
+        this.strikeCount = 0;
+        this.ballCount = 0;
+    }
+
+    public void judgeGameResult(List<Ball> hitterBalls, List<Ball> pitcherBalls) {
+        hitterBalls.stream()
+                .forEach(hitterBall -> count(hitterBall.getResult(pitcherBalls)));
+    }
+
+    private void count(BaseballJudge baseballJudge) {
+        if (baseballJudge.equals(BaseballJudge.STRIKE)) {
+            strikeCount += 1;
+        }
+
+        if (baseballJudge.equals(BaseballJudge.BALL)) {
+            ballCount += 1;
+        }
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
 }
