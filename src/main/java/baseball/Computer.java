a811bdca8015ac19f8c9a9cf2f86e88c5f6d5ab2
jthugg <jthug@naver.com>
2022-11-06 07:42:10 +0900

    Feat: 컴퓨터의 숫자 리스트 생성

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,5 +1,21 @@
 package baseball;
 
-public class Computer {
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
+public class Computer {
+	List<Integer> computersList = new ArrayList<>();
+
+	Computer() {}
+
+	public void setComputersList() {
+		while(computersList.size() < 3) {
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+			if(!computersList.contains(randomNumber)) {
+				computersList.add(randomNumber);
+			}
+		}
+	}
 }
