5ff86e9e68d9b43e5f81fe0e62e576a6a04cd669
lee2963 <sodel2963@naver.com>
2022-11-05 16:44:58 +0900

    FEAT 유저 숫자 입력 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,16 +8,30 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> targetNum = makeRandomTargetNum();
+
+
+
+
     }
     public static List<Integer> makeRandomTargetNum() {
         List<Integer> computer = new ArrayList<>();
+
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
+
         return computer;
+    }
+
+    public static int inputUserNum() {
+        System.out.print("숫자를 입력하세요: ");
+        String userNum = Console.readLine();
+
+        return Integer.parseInt(userNum);
     }
 }
