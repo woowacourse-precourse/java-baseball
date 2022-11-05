c37c90fc69a3e5d5cb6124a216ff468a8534886a
kim0914 <kosa0914@naver.com>
2022-11-05 14:02:56 +0900

    feat(BaseballGame): 게임 시작기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,9 +1,33 @@
 package baseball;
 
-public class BaseballGame {
+import static constant.Const.GAME_END_STRIKE_COUNT;
 
-    public void run(){
-        Computer computer = new Computer();
-        computer.setAnswerNumber();
+public class BaseballGame {
+    private Computer computer;
+    private User user;
+    private Referee referee;
+
+    public BaseballGame() {
+        this.computer = new Computer();
+        this.user = new User();
+        this.referee = new Referee();
+    }
+
+    public void run() {
+        init();
+        start();
+    }
+
+    private void init() {
+        computer.setAnswer();
+        referee.initCount();
+    }
+
+    private void start() {
+        while (referee.getStrikeCount() != GAME_END_STRIKE_COUNT) {
+            user.enterNumber();
+            referee.judge(computer.getAnswer(), user.getInputNumber());
+            referee.showResult();
+        }
     }
 }
