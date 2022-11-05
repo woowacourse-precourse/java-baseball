7d1ef692028b281cd5c29aa4e861e714a1325fca
wjh2012 <wjh2012@naver.com>
2022-11-05 17:02:14 +0900

    test(NumberFactory): 랜덤숫자를 생성하는 기능 테스트

---
+++ src/test/java/baseball/NumberFactoryTest.java
@@ -0,0 +1,15 @@
+package baseball;
+
+import baseball.game.NumberFactory;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NumberFactoryTest {
+
+    @Test
+    void createRandomNumbers_메서드_사용시_숫자3개를_받음() {
+        List<Integer> numbers = NumberFactory.createRandomNumbers();
+        Assertions.assertThat(numbers.size()).isEqualTo(3);
+    }
+}
