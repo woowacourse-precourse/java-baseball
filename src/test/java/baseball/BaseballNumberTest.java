8ca813d4d71ae83666968abb1d54ebee4c6d38bf
woosung1223 <dntjd991223@naver.com>
2022-11-05 17:11:38 +0900

    test(BaseballNumber): contains 메소드에 대한 테스트 생성

--- src/test/java/baseball/BaseballNumberTest.java
+++ src/test/java/baseball/BaseballNumberTest.java
@@ -10,13 +10,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class BaseballNumberTest {
     @Nested
+    @DisplayName("세자리 수는 0, 1, 2 인덱스에 대해 올바른 자리수 값을 반환한다")
     class GetNumberAt {
-        @DisplayName("세자리 수는 0, 1, 2 인덱스에 대해 올바른 자리수 값을 반환한다")
         @ParameterizedTest
         @CsvSource(value = {"123, 0, 1", "123, 1, 2", "123, 2, 3"})
         void threeDigitsTest(int number, int index, int expected) {
             BaseballNumber baseballNumber = new BaseballNumber(number);
             assertThat(baseballNumber.getNumberAt(index)).isEqualTo(expected);
+        }
+    }
+
+    @Nested
+    @DisplayName("BaseballNumber가 해당 number를 가지고 있다면 true를 반환한다")
+    class Contains {
+        @ParameterizedTest
+        @CsvSource(value = {"123, 1, true", "324, 4, true", "589, 2, false"})
+        void containsTest(int number, int target, boolean expected) {
+            BaseballNumber baseballNumber = new BaseballNumber(number);
+            assertThat(baseballNumber.contains(target)).isEqualTo(expected);
         }
     }
 }
