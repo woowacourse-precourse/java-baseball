4dd71a5a24fd62334cb9f0631ec497c8487aeb7c
wlsh44 <k87913j@gmail.com>
2022-11-06 00:37:48 +0900

    feat: 게임 재시작 또는 종료 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import baseball.console.GameConsole;
+import baseball.converter.RestartOrExitCodeConverter;
 import baseball.converter.ThreeNumberConverter;
 import baseball.dto.BallStrikeDto;
 import baseball.opponent.ComputerOpponent;
@@ -9,10 +10,13 @@ import baseball.printer.GameMessagePrinter;
 
 import java.util.List;
 
+import static baseball.converter.RestartOrExitCodeConverter.EXIT_CODE;
+
 public class BaseballGame {
 
     private ComputerOpponent opponent;
     private ThreeNumberConverter threeNumberConverter;
+    private RestartOrExitCodeConverter codeConverter;
     private GameMessagePrinter messagePrinter;
     private GameConsole console;
 
@@ -20,25 +24,32 @@ public class BaseballGame {
         this.messagePrinter = new GameMessagePrinter();
         this.opponent = new ComputerOpponent(RandomNumberGenerator.generate());
         this.threeNumberConverter = new ThreeNumberConverter();
+        this.codeConverter = new RestartOrExitCodeConverter();
         this.console = new GameConsole();
         messagePrinter.printStartMessage();
     }
 
     public void play() throws IllegalArgumentException {
-        try {
-            while (true) {
-                messagePrinter.printNumberInputMessage();
-                List<Integer> threeNumbers = threeNumberConverter.getThreeNumbers(console.input());
-                BallStrikeDto answer = opponent.answer(threeNumbers);
-                messagePrinter.printResultMessage(answer);
-                if (answer.getStrikeCount() == 3) {
-                    messagePrinter.printFinishMessage();
+        while (true) {
+            try {
+                while (true) {
+                    messagePrinter.printNumberInputMessage();
+                    List<Integer> threeNumbers = threeNumberConverter.getThreeNumbers(console.input());
+                    BallStrikeDto answer = opponent.answer(threeNumbers);
+                    messagePrinter.printResultMessage(answer);
+                    if (answer.getStrikeCount() == 3) {
+                        messagePrinter.printFinishMessage();
+                        break;
+                    }
+                }
+                messagePrinter.printRestartOrExitMessage();
+                int statusCode = codeConverter.getRestartOrExitCode(console.input());
+                if (statusCode == EXIT_CODE) {
                     break;
                 }
+            } catch (Exception e) {
+                throw new IllegalArgumentException(e.getMessage());
             }
-            messagePrinter.printRestartOrExitMessage();
-        } catch (Exception e) {
-            throw new IllegalArgumentException(e.getMessage());
         }
     }
 }
