209d1075206ac46b6d14a3e982d59cf5fd020751
Yongwoo Choi <cyw7515@naver.com>
2022-11-05 22:07:08 +0900

    fix (Game): 게임 시작시 정답 난수 생성 로직 에러 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -8,20 +8,18 @@ import java.util.List;
 public class Game {
     List<Integer> answer;
 
-    public Game(){
-        List<Integer> pool = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+    public Game() {
         answer = new ArrayList<>();
 
-        for (int i = 0; i < 3; i++) {
-            int pick = Randoms.pickNumberInList(pool);
-
-            answer.add(pick);
-
-            pool.remove(pick - 1);
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
         }
     }
 
-    public List<Integer> getAnswer(){
+    public List<Integer> getAnswer() {
         return answer;
     }
 }
