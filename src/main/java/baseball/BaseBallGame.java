c83d20e872479f51b0ba54e51f8f9475474fcdc2
sujin0607 <goweg0607@gmail.com>
2022-11-06 01:21:20 +0900

    feat: findNumber인 strike와 ball을 구하는 함수 추가

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class BaseBallGame {
     private List<Integer> computer = new ArrayList<>();
     private List<Integer> user = new ArrayList<>();
+    private List<Integer> strikeAndBall = new ArrayList<>();
 
     public List<Integer> randomNumbers() {
         while (computer.size() < 3) {
@@ -17,6 +18,7 @@ public class BaseBallGame {
                 computer.add(randomNum);
             }
         }
+        System.out.println("computer숫자"+computer);
         return computer;
     }
 
@@ -37,6 +39,21 @@ public class BaseBallGame {
         }
         return user;
     }
-    
+
+    public List<Integer> findNumber(List<Integer> computer, List<Integer> user){
+        int strike = 0;
+        int ball = 0;
+
+        for(int i=0; i<computer.size(); i++){
+            if(computer.get(i) == user.get(i)){
+                strike ++;
+            }else if(computer.contains(user.get(i))){
+                ball ++;
+            }
+        }
+        strikeAndBall.add(strike);
+        strikeAndBall.add(ball);
+        return strikeAndBall;
+    }
 
 }
