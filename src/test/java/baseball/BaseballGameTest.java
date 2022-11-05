5c57c34ffdc01a7c698b807dbdc6719013d692f1
Yeongseon Park <yseon406@gmail.com>
2022-11-06 04:22:48 +0900

    test: checkStrike() Test
    checkStrike() 테스트 코드 작성

--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -8,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BaseballGameTest {
     BaseballGame baseballGame = new BaseballGame();
+
     @Test
     void computerNumber() {
         assertThat(baseballGame.createComputerNumber()).isNotEmpty();
@@ -16,10 +18,37 @@ public class BaseballGameTest {
 
     @Test
     void isValidNumberTest() {
-        List<String> testNumber = List.of("012","102","120","1234","111","123","abc","972");
-        List<Boolean> result = List.of(false,false,false,false,false,true,false,true);
-        for(int index = 0; index<testNumber.size(); index++) {
+        List<String> testNumber = List.of("012", "102", "120", "1234", "111", "123", "abc", "972");
+        List<Boolean> result = List.of(false, false, false, false, false, true, false, true);
+        for (int index = 0; index < testNumber.size(); index++) {
             assertThat(baseballGame.isValidNumber(testNumber.get(index))).isEqualTo(result.get(index));
+        }
+    }
+
+    @Nested
+    class checkStrikeTest {
+        @Test
+        void case1() {
+            List<Integer> computerNumber = List.of(1, 2, 3);
+            List<Integer> userNumber = List.of(1, 2, 3);
+            int result = 3;
+            assertThat(baseballGame.checkStrike(computerNumber, userNumber)).isEqualTo(result);
+        }
+
+        @Test
+        void case2() {
+            List<Integer> computerNumber = List.of(1, 2, 3);
+            List<Integer> userNumber = List.of(1, 2, 4);
+            int result = 2;
+            assertThat(baseballGame.checkStrike(computerNumber, userNumber)).isEqualTo(result);
+        }
+
+        @Test
+        void case3() {
+            List<Integer> computerNumber = List.of(1, 2, 3);
+            List<Integer> userNumber = List.of(2, 3, 5);
+            int result = 0;
+            assertThat(baseballGame.checkStrike(computerNumber, userNumber)).isEqualTo(result);
         }
     }
 }
