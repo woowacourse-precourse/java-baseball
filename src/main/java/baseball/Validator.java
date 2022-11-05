fc8d537891b8df2d7862f7fc1e0718355203362c
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-05 11:45:27 +0900

    feat(validateDigits): build command flow

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -3,14 +3,18 @@ package baseball;
 public class Validator {
 
     public void validatePrediction(String predictedNumber) {
-        if (!validateSize(predictedNumber))
+        if (!validateSize(predictedNumber) && !validateDigits(predictedNumber))
             throw new IllegalArgumentException("잘못된 입력값입니다.");
-        // Validate size
-        // Validate digits
         // If false throw IllegalArgumentException
     }
 
     private boolean validateSize(String predictedNumber) {
         return predictedNumber.length() == 3;
+    }
+
+    private boolean validateDigits(String predictedNumber) {
+        // Validate if predictedNumber is decimal number
+        // Validate if digits of predictedNumber are all different
+        return true;
     }
 }
