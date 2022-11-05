f4e3d4e2b3aa20a59781ede427f2216d223a22ce
alstjr706 <alstjr706@naver.com>
2022-11-05 16:26:28 +0900

    test : (숫자 야구) @Nested로 테스트 코드 전체 분류

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -1,6 +1,7 @@
 package baseball.domain;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -16,46 +17,55 @@ class RefereeTest {
         referee = new Referee();
     }
 
-    @Test
-    void 스트라이크3_맞는지_확인() {
-        String result = "3스트라이크";
-        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(1, 2, 3));
-        assertThat(judgment).isEqualTo(result);
+    @Nested
+    class StrikeCase {
+        @Test
+        void 스트라이크3_맞는지_확인() {
+            String result = "3스트라이크";
+            String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(1, 2, 3));
+            assertThat(judgment).isEqualTo(result);
+        }
+
+        @Test
+        void 스트라이크1_맞는지_확인() {
+            String result = "1스트라이크";
+            String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(1, 5, 7));
+            assertThat(judgment).isEqualTo(result);
+        }
     }
 
-    @Test
-    void 스트라이크1_맞는지_확인() {
-        String result = "1스트라이크";
-        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(1, 5, 7));
-        assertThat(judgment).isEqualTo(result);
+    @Nested
+    class BallCase {
+        @Test
+        void 볼3_맞는지_확인() {
+            String result = "3볼";
+            String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(3, 1, 2));
+            assertThat(judgment).isEqualTo(result);
+        }
+
+        @Test
+        void 볼1_맞는지_확인() {
+            String result = "1볼";
+            String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(7, 6, 1));
+            assertThat(judgment).isEqualTo(result);
+        }
     }
 
-    @Test
-    void 볼3_맞는지_확인() {
-        String result = "3볼";
-        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(3, 1, 2));
-        assertThat(judgment).isEqualTo(result);
-    }
+    @Nested
+    class Ball_Strike_Case {
+        @Test
+        void 볼1_스트라이크1_맞는지_확인() {
+            String result = "1볼 1스트라이크";
+            String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(5, 1, 3));
+            assertThat(judgment).isEqualTo(result);
+        }
 
-    @Test
-    void 볼1_맞는지_확인() {
-        String result = "1볼";
-        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(7, 6, 1));
-        assertThat(judgment).isEqualTo(result);
-    }
-
-    @Test
-    void 볼1_스트라이크1_맞는지_확인() {
-        String result = "1볼 1스트라이크";
-        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(5, 1, 3));
-        assertThat(judgment).isEqualTo(result);
-    }
-
-    @Test
-    void 볼2_스트라이크1_맞는지_확인() {
-        String result = "2볼 1스트라이크";
-        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(1, 3, 2));
-        assertThat(judgment).isEqualTo(result);
+        @Test
+        void 볼2_스트라이크1_맞는지_확인() {
+            String result = "2볼 1스트라이크";
+            String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(1, 3, 2));
+            assertThat(judgment).isEqualTo(result);
+        }
     }
 
     @Test
