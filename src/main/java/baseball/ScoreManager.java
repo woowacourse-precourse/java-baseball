6fe2940aa546e7aa32b48671368bb24242b9d5ab
woosung1223 <dntjd991223@naver.com>
2022-11-05 17:19:28 +0900

    feat(ScoreManager): getUserScore 메소드 추가

--- src/main/java/baseball/ScoreManager.java
+++ src/main/java/baseball/ScoreManager.java
@@ -1,9 +1,15 @@
 package baseball;
 
+import java.util.List;
+
 public class ScoreManager {
     private Score userScore;
 
     public void makeUserScore(BaseballNumber baseballNum1, BaseballNumber baseballNum2) {
         userScore = ScoreCalculator.calculateUserFinalScore(baseballNum1, baseballNum2);
+    }
+
+    public List<Integer> getUserScore() {
+        return userScore.getScore();
     }
 }
