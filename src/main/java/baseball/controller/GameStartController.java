a6f9a7296e10b3bb73b73a3bff4f8559f351464b
ywj9811 <ywj9811@naver.com>
2022-11-06 00:53:08 +0900

    refactor: GameStartController 클래스 변수명 변경

--- src/main/java/baseball/controller/GameStartController.java
+++ src/main/java/baseball/controller/GameStartController.java
@@ -10,12 +10,12 @@ public class GameStartController {
         ResultService resultService = new ResultService();
         RandomValue randomValue = new RandomValue();
 
-        int Restart = 1;
+        int restart = 1;
 
-        while (Restart == 1) {
-            List<Integer> RandomNumber = randomValue.randomValue();
+        while (restart == 1) {
+            List<Integer> randomNumber = randomValue.getRandom();
             System.out.println("숫자 야구 게임을 시작하겠습니다.");
-            Restart = resultService.ReOrClose(RandomNumber);
+            restart = resultService.ReOrClose(randomNumber);
         }
 
         System.out.println("종료 되었습니다.");
