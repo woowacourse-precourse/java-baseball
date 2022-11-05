b6c72dd4c35459e56e449ae48c9fb062e4fa6521
chanminmaxscaler <jcm04@naver.com>
2022-11-05 11:31:37 +0900

    feat: Messenger 클래스의 printRestartOrEndMessage 메소드를 구현한다
    printRestartOrEndMessage 메소드는 "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n" 메시지를 출력한다.

--- src/main/java/baseball/view/Messenger.java
+++ src/main/java/baseball/view/Messenger.java
@@ -13,4 +13,8 @@ public class Messenger {
     public void printAnswerMessage() {
         System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
     }
+
+    public void printRestartOrEndMessage() {
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+    }
 }
