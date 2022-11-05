9f3516bacbaa6e26602bcefd1a4febbc846f80a6
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-05 17:59:07 +0900

    test(BallMaker): 사용자에게 제대로 된 입력을 받았을 경우 올바른 결과값 반환 테스트

--- src/test/java/baseball/testcase/BallMakerTest.java
+++ src/test/java/baseball/testcase/BallMakerTest.java
@@ -4,9 +4,11 @@ import baseball.game.BallMaker;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.*;
 
+import java.io.ByteArrayInputStream;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,6 +35,33 @@ public class BallMakerTest extends NsTest {
         //then
         assertThat(isUniqueBalls && ball.size() == 3)
                 .isTrue();
+    }
+
+    @Test
+    @DisplayName("BallMaker가 UserBall을 제대로 확인 하는지 확인")
+    void BALLMAKER가_USERBALL를_제대로_생성하는지_확인() {
+        command("123","321","589","485");
+        List<Integer> firstBall = ballMaker.getUserBall();
+        List<Integer> secondBall = ballMaker.getUserBall();
+        List<Integer> thirdBall = ballMaker.getUserBall();
+        List<Integer> fourthBall = ballMaker.getUserBall();
+
+        String first = firstBall.stream().map(n -> String.valueOf(n)).collect(Collectors.joining());
+        String second = secondBall.stream().map(n -> String.valueOf(n)).collect(Collectors.joining());
+        String third = thirdBall.stream().map(n -> String.valueOf(n)).collect(Collectors.joining());
+        String fourth = fourthBall.stream().map(n -> String.valueOf(n)).collect(Collectors.joining());
+
+        boolean firstCheck = first.equals("123");
+        boolean secondCheck = second.equals("321");
+        boolean thirdCheck = third.equals("589");
+        boolean fourthCheck = fourth.equals("485");
+
+        assertThat(firstCheck && secondCheck && thirdCheck && fourthCheck).isTrue();
+    }
+
+    private void command(final String... args){
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
     }
 
     @Override
