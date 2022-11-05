c2f129a7adfed8c09b90da9d7e49133eaa630bf4
cmong0516 <cmong0516@gmail.com>
2022-11-05 15:39:42 +0900

    feat(BaseBallGame Class): countBall 메서드 작성
    유저의 입력값과 컴퓨터의 랜덤값을 비교하여 ball 수를 반환하는 메서드

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.List;
+
 public class BaseBallGame {
 
     public void startGame() {
@@ -8,5 +10,18 @@ public class BaseBallGame {
 
     public void userInput() {
         System.out.print("숫자를 입력해주세요  :  ");
+    }
+
+    public int countBall(List<Integer> userInputArr, List<Integer> computerNumbers) {
+
+        int ball = 0;
+
+        for (int i = 0; i < computerNumbers.size(); i++) {
+            if (!userInputArr.get(i).equals(computerNumbers.get(i)) && computerNumbers.contains(userInputArr.get(i))) {
+                ball++;
+            }
+        }
+
+        return ball;
     }
 }
