547234a2abfa1972b0d2e11963f523d04bf4721a
saehoon0501 <76736828+saehoon0501@users.noreply.github.com>
2022-11-05 22:17:37 +0900

    feat: 컴퓨터 정답 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,10 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        int answer = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(111,999);
+        
     }
+
 }
