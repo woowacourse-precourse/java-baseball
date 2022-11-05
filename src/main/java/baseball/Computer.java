c6fe3874b9a14ad35a603c182a64eb2ea4113bd8
park <pjh971212@gmail.com>
2022-11-05 17:35:07 +0900

    feat: 컴퓨터 임의의 수 세자리 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int COMPUTER_SIZE = 3;
+    List<Integer> computerNumber = new ArrayList<>();
+
+    public void generate() {
+        while (computerNumber.size() < COMPUTER_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+
+    }
+}
---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,8 @@
+package baseball;
+
+public class Game {
+    Computer computer = new Computer();
+    public void start() {
+        computer.generate();
+    }
+}
