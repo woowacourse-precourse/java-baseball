5be4c49c82a40d596ddce90b7a1b2f6c8a5dc7a6
saint6839 <saint6839@gmail.com>
2022-11-05 20:41:15 +0900

    feat(controller) : 랜덤 숫자리스트를 생성하는 기능을 호출 하도록 구현

--- src/main/java/baseball/controller/ComputerController.java
+++ src/main/java/baseball/controller/ComputerController.java
@@ -1,12 +1,19 @@
 package baseball.controller;
 
 import baseball.Ball;
+import baseball.service.ComputerService;
 
 import java.util.List;
 
 public class ComputerController {
 
+    private final ComputerService computerService;
+
+    public ComputerController(ComputerService computerService) {
+        this.computerService = computerService;
+    }
+
     public List<Ball> generateRandomBalls() {
-        return null;
+        return computerService.generateRandomBalls();
     }
 }
