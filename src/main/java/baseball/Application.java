8e3e873b40d517185870793bb3999c2b6ab310bc
simba <shim9597@gmail.com>
2022-11-05 18:12:24 +0900

    refactor: gameStart 기능, 그 하위 기능 이동
    InGame.class 추가에 따른 gameStart 기능과 그 하위 기능 이동.
    Application.class 에서 InGame.class 로.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,35 +8,17 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Console.*;
 
 public class Application {
+    public static List<Integer> randomNum;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        InGame inGame = new InGame();
+        inGame.gameStart();
     }
 
-    public List<Integer> randomNum;
 
-    public void gameStart() {
-        System.out.println("숫자를 입력해 주세요 : ");
-        String input = readLine();
-
-        validateInGame(input);
-        newRandomNumber();
-    }
-
-    public void validateInGame(String input) {
-
-    }
-
-    public void newRandomNumber() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        randomNum = computer;
-    }
 
 }
