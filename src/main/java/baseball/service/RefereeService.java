0b3dfc44d136c1b07f812883827d398def696d90
saint6839 <saint6839@gmail.com>
2022-11-06 01:39:09 +0900

    feat(service) : 사용자 입력 결과에 대한 문구를 출력 해주는 기능 추가

--- src/main/java/baseball/service/RefereeService.java
+++ src/main/java/baseball/service/RefereeService.java
@@ -22,4 +22,21 @@ public class RefereeService {
         }
         return result;
     }
+
+    public void printResult(Result result) {
+        if (result.hasNothing()) {
+            System.out.println("낫싱");
+        }
+        if (result.hasBall()) {
+            System.out.print(result.getBallCount() + "볼 ");
+            if (result.hasStrike()) {
+                System.out.println(result.getStrikeCount() + "스트라이크");
+            }
+            System.out.println();
+        }
+        if (result.isGameOver()) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다.");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+    }
 }
