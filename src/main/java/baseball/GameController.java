23df884f3eac3f8ae3e93d9bfb211072c67026b3
ma-an-jong <alswhd1113@gmail.com>
2022-11-05 13:34:12 +0900

    fix(GameController) : 랜덤 정수 생성 과정 버그 수정
    GameController에서 랜덤 정수 생성하는 과정에서 중복된
    숫자가 발생하는것을 발견하여 generateRandomNumber 수정
    validation 함수도 같이 수정
    Hint를 생성하여 전달하는 메소드와 정답 체크 메소드 추가
    
    BREAKING CHANGE: 랜덤 정수 생성 메소드 수정
    
    Before:
    
        GameController
        {
            generateRandomNumber
            isValidUserInput
        }
    
    After:
    
        GameController
        {
            generateRandomNumber(로직 수정)
            isValidUserInput(로직 수정)
            hasInvalidCharacter(구현 추가)
            isDuplicatedInput(구현 추가)
            getHint(구현 추가)
            isAnswer(구현 추가)
        }
    
        정수 생성 메소드에서 버그를 발견하여 수정 및 Hint 생성과 정답 여부를 판별하는 메소드 추가

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,9 +1,13 @@
 package baseball;
 
 import baseball.Input.InputManageable;
+import baseball.Output.OutputManageable;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class GameController {
@@ -12,33 +16,82 @@ public class GameController {
     private static final Integer END_INCLUSIVE = 9;
     private static final String VALID_INPUT_PATTERN = "^[1-9]*$";
     private List<Integer> randomNumber;
+
     public GameController()
     {
-        generateRandomNumber();
+        randomNumber = new ArrayList<>();
     }
 
     public void generateRandomNumber()
     {
-        randomNumber = List.of(
-                Randoms.pickNumberInRange(START_INCLUSIVE,END_INCLUSIVE),
-                Randoms.pickNumberInRange(START_INCLUSIVE,END_INCLUSIVE),
-                Randoms.pickNumberInRange(START_INCLUSIVE,END_INCLUSIVE));
+        if(!randomNumber.isEmpty())
+        {
+            randomNumber.removeAll(randomNumber);
+        }
+
+        while(randomNumber.size() != 3)
+        {
+            int randInt = Randoms.pickNumberInRange(START_INCLUSIVE,END_INCLUSIVE);
+            if(!randomNumber.contains(randInt)) randomNumber.add(randInt);
+        }
+
+        System.out.println("getRandomNumber() = " + getRandomNumber());
     }
 
     public Integer getRandomNumber()
     {
+        if(randomNumber.isEmpty()) generateRandomNumber();
         return randomNumber.get(0)*100 + randomNumber.get(1)*10 + randomNumber.get(2);
     }
 
+    private boolean hasDuplication(String userInput)
+    {
+        Set<Character> inputCharacterSet = new HashSet<>();
+
+        for(int index = 0; index < userInput.length();index++)
+        {
+            char userInputCharacterAtIndex = userInput.charAt(index);
+
+            if(inputCharacterSet.contains(userInputCharacterAtIndex))
+            {
+                return true;
+            }
+
+            inputCharacterSet.add(userInputCharacterAtIndex);
+        }
+
+        return false;
+    }
+
+    private boolean hasInvalidCharacter(String userInput)
+    {
+        return !Pattern.matches(VALID_INPUT_PATTERN,userInput);
+    }
     public boolean isValidUserInput(String userInput)
     {
-        if (userInput.length() != INPUT_SIZE || !Pattern.matches(VALID_INPUT_PATTERN,userInput))
+        if (userInput.length() != INPUT_SIZE || hasInvalidCharacter(userInput) || hasDuplication(userInput))
         {
             return false;
         }
 
         return true;
     }
+
+    public String getHint(String userInput)
+    {
+        if(!isValidUserInput(userInput))
+            throw new IllegalArgumentException("잘못된 숫자를 입력하였습니다. 프로그램을 종료합니다.");
+
+        int userInputNumber = Integer.parseInt(userInput);
+        return HintGenerator.generateHint(userInputNumber,randomNumber);
+    }
+
+    public boolean isAnswer(String userInput)
+    {
+        return getRandomNumber() == Integer.parseInt(userInput);
+    }
+
+
 
 
 }
