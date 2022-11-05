26c3ac263ac93c46404c302c3c6fa77cd5e9eba1
unknown <jeon98@soongsil.ac.kr>
2022-11-05 19:53:26 +0900

    Fetch: 제대로 된 입력 형식인지 확인하는 메서드 추가 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,40 +7,59 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        if(!check_input(args)){
+           // IllegalArgumentException
+        }
         // TODO: 프로그램 구현
         List<String> computer = new ArrayList<>();
-        while(computer.size() <3){
+        while(computer.size()<3){
             int randomNumber = Randoms.pickNumberInRange(1,9);
-            if(!computer.contains(randomNumber)){
-                computer.add(String.valueOf(randomNumber));
+            String str = String.valueOf(randomNumber);
+            if(!computer.contains(str)){
+                computer.add(str);
             }
         }
         int strike = 0;
         int ball = 0;
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println("args 개수 = " + args.length);
         System.out.println("computer = " + computer);
-        System.out.println(args[0]);
-        System.out.println(args[0].charAt(1));
         strike = strike(args,computer);
         ball = ball(args,computer);
         print_score(strike,ball);
         System.out.println("게임을 종료합니다");
     }
 
+    // 제대로 된 입력형식인지 확인하는 코드
+    private static boolean check_input(String[] args) {
+        // 입력 길이가 3인지 확인
+        if(args.length != 3){
+            return false;
+        }
+        //서로 다른 숫자를 입력받는지 확인
+        if(args[0] == args[1] | args[0] == args[2] | args[1] == args[2]){
+            return false;
+        }
+        return true;
+    }
+
     public static int strike(String[] args, List<String> computer){
         int strike = 0;
         for(int i=0; i<3 ; i++){
             Character arg = args[0].charAt(i);
-            Character ans = computer.get(i).charAt(i);
-            System.out.println("arg = " + arg);
-            System.out.println("ans = " + ans);
+            String str = computer.get(i);
+            Character ans = str.charAt(0);
+//            System.out.println("arg = " + arg);
+//            System.out.println("ans = " + ans);
             strike = isitstrike(arg,ans,strike);
         }
+
+    //    System.out.println("strike = " + strike);
         return strike;
     }
 
-    public static int isitstrike(Character arg, Character ans, int strike){
+    public static int isitstrike(Character arg, Character ans, int val){
+        int strike = val;
+        //System.out.println("isitstrike 실행");
         if(arg == ans){
             strike += 1;
         }
@@ -49,6 +68,7 @@ public class Application {
 
     public static int ball(String[] args,List<String> computer){
         int ball = 0;
+        //System.out.println("ball 실행");
         for(int idx=0 ; idx<3 ; idx++){
             Character arg = args[0].charAt(idx);
             if(computer.contains(arg)){
@@ -59,7 +79,10 @@ public class Application {
     }
 
     public static int isitball(Character arg,List<String> computer, int idx, int ball){
-        if(computer.get(0).charAt(idx) != arg){
+        //System.out.println("isitball 실행");
+        String str = computer.get(idx);
+        Character chr = str.charAt(0);
+        if(chr != arg){
          ball += 1;
         }
         return ball;
