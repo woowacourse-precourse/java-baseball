23b062bed03b66c165df7a62e2bf3219689fd757
bellljun3395 <195850@jnu.ac.kr>
2022-11-05 15:17:16 +0900

    feat(GamePlayer): 구현 GamePlayer

---
+++ src/main/java/baseball/player/GamePlayer.java
@@ -0,0 +1,4 @@
+package baseball.player;
+
+public class GamePlayer extends Player {
+}
---
+++ src/main/java/baseball/player/Player.java
@@ -0,0 +1,14 @@
+package baseball.player;
+
+public class Player {
+
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
