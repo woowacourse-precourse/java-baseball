09a5c3facabbde118d24da26758aab6b3dbcee64
choiejae <ejae8319@gmail.com>
2022-11-05 18:47:42 +0900

    test: InputSuccess 테스트 생성

--- src/test/java/baseball/UserBallsInputTest.java
+++ src/test/java/baseball/UserBallsInputTest.java
@@ -2,12 +2,33 @@ package baseball;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserBallsInputTest {
-
-  @Test
-  void IsCreateUserBalls() {
-    UserBallsInput input = new UserBallsInput(3);
+  private UserBallsInput input;
+  private static ByteArrayOutputStream outContent;
+  private static PrintStream originalOut;
+  @BeforeEach
+  void setUp() {
+    outContent = new ByteArrayOutputStream();
+    originalOut = System.out;
+    System.setOut(new PrintStream(outContent));
+    input = new UserBallsInput(3);
   }
+
+  @AfterEach
+  public void restoreStreams() {
+    System.setOut(originalOut);
+  }
+  
+  @Test
+  void InputSuccess() {
+    input.inputNumberTest("123");
+    assertEquals("123", outContent.toString());
+  }
+
 }
