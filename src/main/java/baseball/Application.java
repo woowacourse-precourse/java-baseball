aa8e9945a04633f9609bdd504b5160870d60fc45
unknown <ymecca12@gmail.com>
2022-11-05 14:15:01 +0900

    feat : 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,11 @@
 package baseball;
 
+import baseball.view.UserInput;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        String input = UserInput.getInput();
 //
     }
 }
--- src/main/java/baseball/view/UserInput.java
+++ src/main/java/baseball/view/UserInput.java
@@ -1,4 +1,14 @@
 package baseball.view;
 
+import baseball.domain.Validator;
+
+import java.io.*;
+
 public class UserInput {
+    private int getInput() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(br.readLine());
+        Validator.validate(input);
+        return input;g
+    }
 }
