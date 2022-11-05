84ee1419ad6aa4df1eb476973a4c36143913fcf7
heesang <dlagmltkd12@naver.com>
2022-11-06 01:03:43 +0900

    feat: 숫자 입력 멘트 출력 기능

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,7 +2,14 @@ package baseball.view;
 
 public class OutputView {
 
+    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
     public static void printGameStart() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(GAME_START_MESSAGE);
+    }
+
+    public static void printInputNumber() {
+        System.out.print(INPUT_NUMBER_MESSAGE);
     }
 }
