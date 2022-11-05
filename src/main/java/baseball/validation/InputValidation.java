4883f165c04c6f9d9c70f6fc59f1bf2b4cad5246
anneu <b.imnuj@gmail.com>
2022-11-05 17:44:53 +0900

    feat: 입력 값이 서로 다른 값인지 확인하는 기능 구현

--- src/main/java/baseball/validation/InputValidation.java
+++ src/main/java/baseball/validation/InputValidation.java
@@ -1,5 +1,8 @@
 package baseball.validation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class InputValidation {
     public boolean isNotThreeLetters(String input) {
         return input.length() != 3;
@@ -10,6 +13,20 @@ public class InputValidation {
 
         for (char letter : chars) {
             if (!('1' <= letter && letter <= '9')) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean hasDuplicatedValues(String input) {
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String letter : input.split("")) {
+            map.put(letter, map.getOrDefault(letter, 0) + 1);
+
+            if (map.get(letter) >= 2) {
                 return true;
             }
         }
