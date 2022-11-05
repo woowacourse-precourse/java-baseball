fec43761042707a9e030f0ebc99f4980871a27a3
simba <shim9597@gmail.com>
2022-11-05 22:56:18 +0900

    refactor: newRandomNumber 기능 이동
    utils 패키지에 RandomNumber 클래스 생성, newRandomNumber() 해당 클래스로 이동

---
+++ src/main/java/baseball/utils/RandomNumber.java
@@ -0,0 +1,20 @@
+package baseball.utils;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumber {
+
+    public List<Integer> newRandomNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+        return computerNumber;
+    }
+}
