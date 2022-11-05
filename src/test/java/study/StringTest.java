207239cff13334df823a65211d9fe253ba043d5d
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2022-11-05 20:42:44 +0900

    test(feature) : add tests for making Answer functions

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,7 +1,8 @@
 package study;
 
 import org.junit.jupiter.api.Test;
-
+import java.util.*;
+import static baseball.Computer.*;
 import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
@@ -45,6 +46,31 @@ public class StringTest {
         assertThatThrownBy(() -> input.charAt(5))
                 .isInstanceOf(StringIndexOutOfBoundsException.class)
                 .hasMessageContaining("String index out of range: 5");
+    }
+
+    @Test
+    void addRandomNumber_리스트에_1부터_9까지의_랜덤정수_추가() {
+        boolean result = false;
+        List<Integer> list = new ArrayList<>();
+        addRandomNumber(list);
+        Set<Integer> set = new HashSet<>(list);
+        if(list.size() == set.size()){
+            result = true;
+        }
+        for (int i = 0; i< list.size(); i++){
+            int num = list.get(i);
+            if(num<=9 && num>=1){
+                result = true;
+            }
+        }
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    void makeAnswer_컴퓨터_정답_만들기(){
+        List<Integer> answer = makeAnswer();
+        int result = answer.size();
+        assertThat(result).isEqualTo(3);
     }
 
 }
