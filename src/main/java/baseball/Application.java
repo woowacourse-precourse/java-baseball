f43a2e973aed93def0269b27d11af8432d549724
jipyo0106 <68218081+jipyo0106@users.noreply.github.com>
2022-11-05 14:33:22 +0900

    fix: 컴퓨터 랜덤 값 오류
    - 기존 0~9였던 값을 문서대로 1~9 변경
    - pickNumberInRange 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,18 +4,22 @@ import baseball.util.BaseBallGame;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Application {
     private static final int RESTART_CODE = 1;
     private static final int END_CODE = 2;
+
 
     public static void main(String[] args) {
         int statusCode = 0;
 
         do {
             System.out.println("숫자 야구 게임을 시작합니다.");
-            BaseBallGame baseBallGame = new BaseBallGame(Randoms.pickUniqueNumbersInRange(0, 9, 3));
 
+            BaseBallGame baseBallGame = new BaseBallGame(pickRandom());
             baseBallGame.gameStart();
 
             statusCode = convertUserDataToStatusCode(Console.readLine());
@@ -28,11 +32,25 @@ public class Application {
 
     }
 
+
     private static int convertUserDataToStatusCode(String restartOrEndOrError) throws IllegalArgumentException {
         if (!restartOrEndOrError.matches("[12]")) {
             throw new IllegalArgumentException();
         }
 
         return Integer.valueOf(restartOrEndOrError);
+    }
+
+
+    private static List<Integer> pickRandom() {
+        List<Integer> computerRandom = new ArrayList<>();
+        while (computerRandom.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerRandom.contains(randomNumber)) {
+                computerRandom.add(randomNumber);
+            }
+        }
+
+        return List.copyOf(computerRandom);
     }
 }
