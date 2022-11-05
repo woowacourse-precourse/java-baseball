90f8a6d589de3d62283e1c290b5c5723306993b7
tdmdgh <tdmdgh@gmail.com>
2022-11-05 17:26:25 +0800

    test: add duplicat test in createTargetNums_test

--- src/test/java/baseball/ApplicationTest2.java
+++ src/test/java/baseball/ApplicationTest2.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -20,6 +22,8 @@ class ApplicationTest2 extends NsTest {
         assertEquals(3, result.size());
         for(int element : result)
             assertThat(element).isBetween(1, 9);
+        Set<Integer> numSet = new HashSet<>(result);
+        assertEquals(3,numSet.size());
     }
 
 
