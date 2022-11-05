9732ded35859bdbefb2417dc94fae70a259d7ecb
Yunsik-Choi <choiys0311@gmail.com>
2022-11-06 02:11:33 +0900

    ⛑️ test : InputTest Nested 적용 InputValidate 한번에 검증 할 수 있도록 리팩토링

--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -9,7 +9,7 @@ public class Input {
     public List<Integer> inputNumber() {
         String text = Console.readLine();
         validateInput(text);
-        
+
         return text.chars().boxed()
                 .collect(Collectors.toList());
     }
@@ -20,19 +20,19 @@ public class Input {
         validateOverlap(text);
     }
 
-    public static void validateNumber(String text) {
+    private static void validateNumber(String text) {
         if (!text.chars().allMatch(Character::isDigit)) {
             throw new IllegalArgumentException("숫자가 아닌 값이 있습니다.");
         }
     }
 
-    public static void validateLength(String text) {
+    private static void validateLength(String text) {
         if (text.length() != 3) {
             throw new IllegalArgumentException("3자리가 아닙니다.");
         }
     }
 
-    public static void validateOverlap(String text) {
+    private static void validateOverlap(String text) {
         long count = text.chars()
                 .distinct()
                 .boxed()
--- src/test/java/baseball/view/InputTest.java
+++ src/test/java/baseball/view/InputTest.java
@@ -4,35 +4,48 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class InputTest {
 
-    @DisplayName("숫자가 아닐 경우 IllegalArgumentException 예외를 throw한다.")
-    @Test
-    void validate_number() {
-        Assertions.assertAll(
-                () -> assertThatThrownBy(() -> Input.validateNumber("   "))
-                        .isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> Input.validateNumber("abc"))
-                        .isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> Input.validateNumber("!@#"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
+    @Nested
+    @DisplayName("서로 다른 3자리의 수를 입력받는다.")
+    class InputValidate {
+        @DisplayName("숫자가 아닐 경우 IllegalArgumentException 예외를 throw한다.")
+        @Test
+        void validate_number() {
+            Assertions.assertAll(
+                    () -> assertThatThrownBy(() -> Input.validateInput("   "))
+                            .isInstanceOf(IllegalArgumentException.class),
+                    () -> assertThatThrownBy(() -> Input.validateInput("abc"))
+                            .isInstanceOf(IllegalArgumentException.class),
+                    () -> assertThatThrownBy(() -> Input.validateInput("!@#"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
 
-    @DisplayName("3자리의 수가 아닐 경우 IllegalArgumentException 예외를 throw한다.")
-    @Test
-    void validate_length() {
-        Assertions.assertAll(
-                () -> assertThatThrownBy(() -> Input.validateLength("12")),
-                () -> assertThatThrownBy(() -> Input.validateLength("1234"))
-        );
-    }
+        @DisplayName("3자리의 수가 아닐 경우 IllegalArgumentException 예외를 throw한다.")
+        @Test
+        void validate_length() {
+            Assertions.assertAll(
+                    () -> assertThatThrownBy(() -> Input.validateInput("12")),
+                    () -> assertThatThrownBy(() -> Input.validateInput("1234"))
+            );
+        }
 
-    @DisplayName("서로 다른 수가 아닐 경우 IllegalArgumentException 예외를 throw 한다.")
-    @Test
-    void validate_overlap() {
-        assertThatThrownBy(() -> Input.validateOverlap("121")).isInstanceOf(IllegalArgumentException.class);
+        @DisplayName("서로 다른 수가 아닐 경우 IllegalArgumentException 예외를 throw 한다.")
+        @Test
+        void validate_overlap() {
+            assertThatThrownBy(() -> Input.validateInput("121")).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest(name = "정상 입력")
+        @ValueSource(strings = {"123", "456", "789", "623", "985", "714"})
+        void input(String text) {
+            Assertions.assertDoesNotThrow(() -> Input.validateInput(text));
+        }
     }
 }
