4f59555279a4b6f4c4612aa930e66b6c6a235993
hanjaegyeong <hjg000223@naver.com>
2022-11-05 23:50:33 +0900

    feat: 게임 초기화 및 playGame 관련 코드 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,4 +1,36 @@
 package baseball;
 
+import static constants.GameConstant.*;
 public class BaseballGame {
+    private final Player player;
+    private final Computer computer;
+
+    public BaseballGame() {
+        player = new Player();
+        computer = new Computer();
+    }
+
+    public void initializeGame() {
+        computer.makeComputerNumber();
+        playGame();
+        checkRestart();
+    }
+
+    private void playGame() {
+        while(isNotThreeStrike) {
+            //game 유지 코드 작성
+        }
+        printClearMessage();
+    }
+
+    private void printClearMessage() {
+        System.out.println(CLEAR_GAME_MESSAGE);
+    }
+
+    private void checkRestart() {
+        int restartOrEndNumber = player.getRestartOrEndNumber();
+        if (restartOrEndNumber == RESTART_NUMBER) {
+            initializeGame();
+        }
+    }
 }
