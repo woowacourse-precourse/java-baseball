837d240f21c2a00ace72325da28b83c336835273
Kid-Chang <pch3310@gmail.com>
2022-11-06 04:51:49 +0900

    feat(Application): game 작동 및 재시작을 물어보는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.controller.Game;
+import baseball.view.Input;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        do {
+            Game game = new Game();
+            game.playGame();
+        }
+        while (Input.continueGame());
     }
 }
