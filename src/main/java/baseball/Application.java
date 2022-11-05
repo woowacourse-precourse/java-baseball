f2d7e36bf109227b69cc5364117ce25c66832dcf
lvalentine6 <ysr1227@naver.com>
2022-11-06 00:33:05 +0900

    feat : 사용자가 숫자를 입력하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,4 +12,10 @@ public class Application {
                 Randoms.pickNumberInRange(1, 9), Randoms.pickNumberInRange(1, 9));
 
     }
+
+    static String userInput() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println("숫자를 입력해 주세요 : ");
+    }
 }
