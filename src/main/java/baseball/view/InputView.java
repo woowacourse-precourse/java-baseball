5114ae2cbd8342b9beb0e76430f66941c2576c9e
heesang <dlagmltkd12@naver.com>
2022-11-06 02:30:21 +0900

    feat: 숫자 입력 기능

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,25 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class InputView {
+
+    public static final String CRITERION_SPLIT = "";
+
+    public static List<Integer> inputNumbers() {
+        String numberString = Console.readLine();
+        String[] splitNumberString = numberString.split(CRITERION_SPLIT);
+        return mapToInteger(splitNumberString);
+    }
+
+    private static List<Integer> mapToInteger(String[] split) {
+        return Arrays.stream(split)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+}
