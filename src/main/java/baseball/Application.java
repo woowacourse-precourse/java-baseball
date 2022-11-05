53bd662292123e8461b4335027ca583e6f1136ae
noyes5 <noyes5@naver.com>
2022-11-06 02:30:48 +0900

    feat: 게임실행 및 결과값 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,11 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Baseball baseball = new Baseball();
-
-        baseball.play();
+        Computer computer = new Computer();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        do {
+            ArrayList<String> computerBall = computer.createNumber();
+            baseball.play(computerBall);
+        } while (baseball.restart());
     }
 }
--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -11,17 +11,20 @@ public class Baseball {
     int strikeCount;
     User user = new User();
 
-    public void play() {
+    public void play(ArrayList<String> computerBall) {
         strikeCount = 0;
         ballCount = 0;
-        Computer computer = new Computer();
-        ArrayList<String> computerBall = computer.createNumber();
 
         System.out.print("숫자를 입력해주세요: ");
         String inputNumbers = Console.readLine();
 
         ArrayList<String> userBall = user.input(inputNumbers);
         compare(computerBall,userBall);
+        resultReturn();
+
+        if (strikeCount != 3) {
+            play(computerBall);
+        }
     }
 
     public void compare(ArrayList<String> computerBall,ArrayList<String> userBall) {
@@ -33,6 +36,38 @@ public class Baseball {
             if (computerBall.contains(userBall.get(i))) {
                 ballCount++;
             }
+        }
+    }
+    public void resultReturn() {
+        if (strikeCount == MAX_SIZE) {
+            System.out.println(MAX_SIZE + "스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+        if (strikeCount != 0 && ballCount == 0) {
+            System.out.println(strikeCount + "스트라이크");
+        }
+        if (strikeCount != 0 && ballCount != 0) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+        if (strikeCount == 0 && ballCount != 0) {
+            System.out.println(ballCount + "볼");
+        }
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        }
+    }
+
+    public boolean restart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input2 = Console.readLine();
+
+        if (input2.equals("1")) {
+            return true;
+        }
+        if (input2.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException();
         }
     }
 }
