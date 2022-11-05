f2982ffe7440c98dacb57c8ec246d2711934b585
Cyma-s <ssol0319@naver.com>
2022-11-06 02:13:29 +0900

    fix(InputView): change class name 'InputView' to 'BaseballGameView'

--- src/main/java/baseball/view/BaseballGameView.java
+++ src/main/java/baseball/view/BaseballGameView.java
@@ -2,7 +2,7 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class InputView {
+public class BaseballGameView {
     private static final String START_BASEBALL_GAME = "숫자 야구 게임을 시작합니다.";
     private static final String GAME_USER_INPUT = "숫자를 입력해주세요 : ";
     private static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
