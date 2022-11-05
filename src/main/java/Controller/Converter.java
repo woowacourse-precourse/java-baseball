1c39d16ec37db7c81e5694a8e138e779b55452c3
Devtist <yt930310@snu.ac.kr>
2022-11-05 18:52:51 +0900

    fix: convertStringToIntegerList 메소드 디버그

--- src/main/java/Controller/Converter.java
+++ src/main/java/Controller/Converter.java
@@ -6,7 +6,7 @@ import java.util.stream.Stream;
 
 public class Converter {
 	public static List<Integer> convertStringToIntegerList(String number) {
-		return Stream.of(number)
+		return Stream.of(number.split(""))
 			.map(Integer::parseInt)
 			.collect(Collectors.toList());
 	}
