b48164269a913c720d42dce1f12913fa66f8b1bc
math0917 <math0917@naver.com>
2022-11-05 16:19:55 +0900

    feat: 호출 컴퓨터 숫자 리스트 생성 함수
    GameController가 Computer에게 숫자야구 랜덤 숫자 3자리 생성을 요청

---
+++ src/main/java/baseball/computer/Computer.java
@@ -0,0 +1,9 @@
+package baseball.computer;
+
+import java.util.List;
+
+public class Computer {
+    public static List<Integer> createThreeLengthIntegerList() {
+        return null;
+    }
+}
--- src/main/java/baseball/game/GameController.java
+++ src/main/java/baseball/game/GameController.java
@@ -1,8 +1,12 @@
 package baseball.game;
 
+import baseball.computer.Computer;
+import java.util.List;
+
 public class GameController {
 
     public static void gameStart() {
+        List<Integer> computerRandomNonDuplicateThreeLengthIntegerList = Computer.createThreeLengthIntegerList();
 
     }
 }
