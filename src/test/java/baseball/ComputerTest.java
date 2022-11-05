16862f74692f4928c79dc4d6883f0075f9964833
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-05 22:26:33 +0900

    test(Computer): strike 메서드 테스트 코드 작성
    정답과 입력된 guess 를 비교해서 스트라이크 개수를 반환하는 strike 메서드에 대한 테스트 코드 작성

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -2,7 +2,9 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,7 +19,21 @@ public class ComputerTest {
             Numbers answer = (Numbers) field.get(computer);
             boolean result = true;
             assertThat(result).isEqualTo(answer.validation());
-        } catch (NoSuchFieldException | IllegalAccessException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void strike_확인() {
+        try {
+            Constructor<Computer> constructor = Computer.class.getDeclaredConstructor(Numbers.class);
+            constructor.setAccessible(true);
+            Computer test = constructor.newInstance(new Numbers(List.of(1, 2, 3)));
+            Numbers guess = new Numbers(List.of(1, 2, 4));
+            int result = 2;
+            assertThat(result).isEqualTo(test.strike(guess));
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
