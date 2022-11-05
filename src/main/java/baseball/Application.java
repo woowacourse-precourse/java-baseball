1e86df61ec80f892c02f1b8351f66b2445350679
yu-heejin <jkgt0521@naver.com>
2022-11-05 21:30:38 +0900

    feat : 숫자 입력받는 기능 추가 기능 목록 중 첫번째 항목인 숫자 입력 받기 기능을 추가했다. 테스트 코드를 살펴보니 문자열로 받는 것 같아서 정수형이 아닌 문자열로 받았다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner input = new Scanner(System.in);
+        boolean isTrue = true;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            String number = input.next();
+            System.out.println(number);
+            isTrue = false;
+        } while (isTrue);
     }
 }
