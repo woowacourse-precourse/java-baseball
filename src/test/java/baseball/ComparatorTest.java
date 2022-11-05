62b886612146a6b44139625e58cddbd67ba6b86d
KIMSEI1124 <workju1124@gmail.com>
2022-11-05 11:48:54 +0900

    test: 비교 결과 테스트 생성

---
+++ src/test/java/baseball/ComparatorTest.java
@@ -0,0 +1,127 @@
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class ComparatorTest {
+    @Test
+    void 낫싱() {
+        List<Integer> computerNumbers = List.of(1, 2, 3);
+        List<Integer> playerNumbers = List.of(4, 5, 6);
+        Comparator comparator = new Comparator(computerNumbers, playerNumbers);
+        int[] result = comparator.getResult();
+        int ball = result[0];
+        int strike = result[1];
+        Assertions.assertThat(ball).isZero();
+        Assertions.assertThat(strike).isZero();
+    }
+
+    @Nested
+    class 스트라이크 {
+        @Test
+        void 스트라이크1() {
+            List<Integer> computerNumbers = List.of(1, 2, 3);
+            List<Integer> playerNumbers = List.of(1, 4, 5);
+            Comparator comparator = new Comparator(computerNumbers, playerNumbers);
+            int[] result = comparator.getResult();
+            int ball = result[0];
+            int strike = result[1];
+            Assertions.assertThat(ball).isZero();
+            Assertions.assertThat(strike).isEqualTo(1);
+        }
+
+        @Test
+        void 스트라이크2() {
+            List<Integer> computerNumbers = List.of(1, 2, 3);
+            List<Integer> playerNumbers = List.of(1, 2, 4);
+            Comparator comparator = new Comparator(computerNumbers, playerNumbers);
+            int[] result = comparator.getResult();
+            int ball = result[0];
+            int strike = result[1];
+            Assertions.assertThat(ball).isZero();
+            Assertions.assertThat(strike).isEqualTo(2);
+        }
+
+        @Test
+        void 스트라이크3() {
+            List<Integer> computerNumbers = List.of(1, 2, 3);
+            List<Integer> playerNumbers = List.of(1, 2, 3);
+            Comparator comparator = new Comparator(computerNumbers, playerNumbers);
+            int[] result = comparator.getResult();
+            int ball = result[0];
+            int strike = result[1];
+            Assertions.assertThat(ball).isZero();
+            Assertions.assertThat(strike).isEqualTo(3);
+        }
+    }
+
+    @Nested
+    class 볼 {
+        @Test
+        void 볼1() {
+            List<Integer> computerNumbers = List.of(1, 2, 3);
+            List<Integer> playerNumbers = List.of(4, 1, 5);
+            Comparator comparator = new Comparator(computerNumbers, playerNumbers);
+            int[] result = comparator.getResult();
+            int ball = result[0];
+            int strike = result[1];
+            Assertions.assertThat(ball).isEqualTo(1);
+            Assertions.assertThat(strike).isZero();
+        }
+
+        @Test
+        void 볼2() {
+            List<Integer> computerNumbers = List.of(1, 2, 3);
+            List<Integer> playerNumbers = List.of(4, 1, 2);
+            Comparator comparator = new Comparator(computerNumbers, playerNumbers);
+            int[] result = comparator.getResult();
+            int ball = result[0];
+            int strike = result[1];
+            Assertions.assertThat(ball).isEqualTo(2);
+            Assertions.assertThat(strike).isZero();
+        }
+
+        @Test
+        void 볼3() {
+            List<Integer> computerNumbers = List.of(1, 2, 3);
+            List<Integer> playerNumbers = List.of(3, 1, 2);
+            Comparator comparator = new Comparator(computerNumbers, playerNumbers);
+            int[] result = comparator.getResult();
+            int ball = result[0];
+            int strike = result[1];
+            Assertions.assertThat(ball).isEqualTo(3);
+            Assertions.assertThat(strike).isZero();
+        }
+    }
+
+    @Nested
+    class 볼_스트라이크 {
+        @Test
+        void 볼1_스트라이크1() {
+            List<Integer> computerNumbers = List.of(1, 2, 3);
+            List<Integer> playerNumbers = List.of(1, 3, 4);
+            Comparator comparator = new Comparator(computerNumbers, playerNumbers);
+            int[] result = comparator.getResult();
+            int ball = result[0];
+            int strike = result[1];
+            Assertions.assertThat(ball).isEqualTo(1);
+            Assertions.assertThat(strike).isEqualTo(1);
+        }
+
+        @Test
+        void 볼2_스트라이크1() {
+            List<Integer> computerNumbers = List.of(1, 2, 3);
+            List<Integer> playerNumbers = List.of(1, 3, 2);
+            Comparator comparator = new Comparator(computerNumbers, playerNumbers);
+            int[] result = comparator.getResult();
+            int ball = result[0];
+            int strike = result[1];
+            Assertions.assertThat(ball).isEqualTo(2);
+            Assertions.assertThat(strike).isEqualTo(1);
+        }
+
+    }
+}
