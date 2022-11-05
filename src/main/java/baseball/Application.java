71f659080d501b845dd854530c8f6f7461df1228
맹순영 <symaeng98@naver.com>
2022-11-05 13:29:46 +0900

    feat: 랜덤 정수 3개 생성
    세 자리수 정수가 아닌 List에 3개의 정수를 저장 후 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
 
     public static void main(String[] args) {
         start();
+        List<Integer> answer = generateRandomNumber();
     }
 
     public static void start() {
         System.out.println(START_SENTENCE);
+    }
+
+    public static List<Integer> generateRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
