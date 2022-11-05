5e0961dc1d9782cfd7b5814f29f941ce49c1c669
kimJaeHyeon <kmr2644@gmail.com>
2022-11-05 23:03:34 +0900

    feat: printBaseballResult() 테스트 작성

--- src/test/java/baseball/ViewTest.java
+++ src/test/java/baseball/ViewTest.java
@@ -1,14 +1,14 @@
 package baseball;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.*;
 
 
 public class ViewTest {
@@ -19,6 +19,7 @@ public class ViewTest {
     void init() {
         view = mock(View.class);
     }
+
     @Test
     @DisplayName("view inputBaseballNumber() 호출 시 문자열이 반환되어야함")
     void view_input_test() {
@@ -26,10 +27,42 @@ public class ViewTest {
         //when
         String actual = view.inputBaseballNumber();
         //then
-        Assertions.assertEquals(actual,"457");
+        assertEquals(actual, "457");
 
         then(view).should(times(1))
                   .inputBaseballNumber();
+
+    }
+    @Test
+    @DisplayName("view inputExitOrRestart() 호출 시 문자열이 반환되어야함")
+    void inputExitOrRestart() {
+        given(view.inputExitOrRestart()).willReturn("2");
+        //when
+        String actual = view.inputExitOrRestart();
+        //then
+        assertEquals(actual, "2");
+
+        then(view).should(times(1))
+                  .inputExitOrRestart();
+
+    }
+
+    @Test
+    @DisplayName("BaseballDto getResult() 를 통해 1볼 2스트라이크 가 출력되어야해")
+    public void printBaseBallResult() {
+        //given
+        ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStreamCaptor));
+
+        BaseBallDto baseBallDto = new BaseBallDto(2, 1);
+        View testView = new View();
+        //when
+        testView.printBaseBallResult(baseBallDto);
+
+        String actual = outputStreamCaptor.toString()
+                                          .trim();
+        //then
+        assertEquals(actual,"1볼 2스트라이크");
 
     }
 }
