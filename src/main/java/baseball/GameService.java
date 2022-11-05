9f44ff5e77e7a78770afd0d23485d84058a43b5c
Dongmin Kim <kimdongmin68@naver.com>
2022-11-05 19:54:08 +0900

    style(gameService): NOTHING_MESSAGE 출력 시 개행 문자 삭제

--- src/main/java/baseball/GameService.java
+++ src/main/java/baseball/GameService.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class GameService {
     private static final int NUMBER_LENGTH = 3;
-    private static final String NOTHING_MESSAGE = "낫싱\n";
+    private static final String NOTHING_MESSAGE = "낫싱";
     private static final String ALL_STRIKE_MESSAGE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String BALL_STRIKE_MESSAGE = "%d볼 %d스트라이크\n";
     private static final String STRIKE_MESSAGE = "%d스트라이크\n";
