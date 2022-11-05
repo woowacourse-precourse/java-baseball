1ce72bcd69bf7deda885fb3216ebbc76275e0d64
es <mascoeskim@gmail.com>
2022-11-05 12:55:37 +0900

    feat(generateAnswer): 정답난수생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,21 @@
 package baseball;
 
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다");
-        Scanner in=new Scanner(System.in);
-        ArrayList<integer> answer = new ArrayList<integer>();
+        System.out.println("숫자 야구 게임을 시작합니다!!!");
+        System.out.println(answer(args));
+
+    }
+
+    public static ArrayList<Integer> answer(String[] args) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
         for (int i = 0; i < 3; i++) {
-            int
+            answer.add((int)(Math.random() * 9 + 1));
         }
+        return answer;
     }
 }
+
