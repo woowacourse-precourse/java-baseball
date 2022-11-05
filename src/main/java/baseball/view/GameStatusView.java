d37eb52ccb3d3991fd4046663bfb953249c5b1b8
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-05 18:17:16 +0900

    feat: GameStatusView 게임 종료후 입력에 대한 요청문 출력 기능 구현

--- src/main/java/baseball/view/GameStatusView.java
+++ src/main/java/baseball/view/GameStatusView.java
@@ -1,12 +1,18 @@
 package baseball.view;
 
 public class GameStatusView {
+
+    private static final String END_GAME_COMMAND = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String ASK_OPINION_COMMAND = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     String opinion;
 
-    public GameStatusView(){}
+    public GameStatusView(){
+        opinion = "";
+    }
 
     public void askOpinion(){
-
+        System.out.println(END_GAME_COMMAND);
+        System.out.println(ASK_OPINION_COMMAND);
     }
 
     public void readOpinion(){
