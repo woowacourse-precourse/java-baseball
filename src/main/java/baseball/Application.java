66526d53c9b13cc206fc37284224ede3d45b2675
lvalentine6 <ysr1227@naver.com>
2022-11-06 00:43:31 +0900

    refactor : 사용자가 잘못된 값을 입력할 경우 예외처리 하는 기능 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,30 +5,31 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
 
 public class Application {
     public static void main(String[] args) {
         ComputerNumbers computerNumbers = new ComputerNumbers(Randoms.pickNumberInRange(1, 9),
                 Randoms.pickNumberInRange(1, 9), Randoms.pickNumberInRange(1, 9));
-
+        System.out.println(userInput());
     }
 
     static String userInput() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println("숫자를 입력해 주세요 : ");
+        System.out.print("숫자를 입력해 주세요 : ");
         boolean flag;
         String inputString;
 
         try {
             inputString = br.readLine();
             flag = isValid(inputString);
-
         } catch (IOException ioException) {
             throw new IllegalArgumentException(ioException);
         }
 
-        if(flag) {
+        if (flag) {
             return inputString;
         } else {
             throw new IllegalArgumentException();
@@ -37,9 +38,9 @@ public class Application {
     }
 
     static boolean isValid(String inputString) {
-        if(inputString.length() == 3) {
-            return true;
-        }
-        return false;
+        ParsePosition parsePosition = new ParsePosition(0);
+        NumberFormat.getInstance().parse(inputString, parsePosition);
+
+        return (inputString.length() == 3) && (inputString.length() == parsePosition.getIndex());
     }
 }
