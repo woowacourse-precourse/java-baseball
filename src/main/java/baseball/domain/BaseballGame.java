ba6cf9b21ff75eea8dbc6eaf052e976a29e6d646
hum02 <ehdgma02@gmail.com>
2022-11-05 15:43:44 +0900

    fix($출력): 게임시작 메시지 호출 위치 수정
    BaseballGame의 play()가 시작할 때 한번만 호출되도록 수정

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -3,20 +3,27 @@ package baseball.domain;
 import baseball.util.Input;
 import baseball.util.View;
 
+import java.util.List;
+
 public class BaseballGame {
-    private final Computer computer = new Computer();
+    private final Computer computer;
     private boolean isFinish = false;
 
-    public BaseballGame() {}
+    public BaseballGame() {
+        this.computer = new Computer();
+    }
+
+    public BaseballGame(List<Integer> computerNumbers) {
+        this.computer = new Computer(computerNumbers);
+    }
 
     public void play() {
+        View.printGameStart();
         do {
-            View.printGameStart();
             View.printRequestPlayerGuess();
             Hint hint = computer.createHint(Input.inputPlayerNumber());
             checkCorrect(hint);
         } while (!isFinish);
-
         View.printGameFinish();
     }
 
