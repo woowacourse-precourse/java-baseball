13221cca2a649938b8d3189ac1b8b06a5b3ca7c1
ostar11 <cjw5877@gmail.com>
2022-11-06 00:07:48 +0900

    feat(Application) : generate computer number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        List<Integer> computer = new ArrayList<>();
+        getComputerNumber(computer);
+
+
+    }
+
+    static void getComputerNumber(List<Integer> computer) {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
 }
