5d39ce91c792a78b00494bb7870e334012baa478
2021110806 <2021110806@dgu.ac.kr>
2022-11-05 20:49:50 +0900

    feat($Application):add checkBall method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,17 @@
 package baseball;
 
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
+    public int checkBall (List<Integer> answer, List<Integer> userInput) {
+        int howManyBall = 0;
+        for (int i=0; i<3; i++) {
+            if (answer.contains(userInput.get(i))) {
+                howManyBall++;
+            }
+        }
+        return howManyBall;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
