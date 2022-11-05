0ee2dc60f321f1c6926d07c23f0ba054109e84a3
dohyun <ldh19738@gmail.com>
2022-11-05 19:28:19 +0900

    feat(Computer): Add a fuction to set number

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private static final int DIGIT_NUMBER = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    public List<Integer> computerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+
+        do {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        } while (computerNumber.size() != DIGIT_NUMBER);
+
+        return computerNumber;
+    }
+}
