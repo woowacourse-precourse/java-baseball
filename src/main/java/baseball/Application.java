4d67dc988555fc39fa53a249e8b675421c94fd08
Ridealist <cjsn92@gmail.com>
2022-11-06 00:47:21 +0900

    feat(Application): 게임 시작 및 재시작/종료 선택 후 해당 로직 처리 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
 public class Application {
+    static final int EXIT_GAME = 2;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean is_quit = false;
+        while (!is_quit) {
+            Game.startGame();
+            // if문 2번 쓰면 error 반환
+            // TODO 입력값 요청 및 저장, int 반환이 한번에 있는 구조 Refactoring할 필요성
+            if (Game.ResetGameOrQuit() == EXIT_GAME) {
+                is_quit = true;
+            }
+        }
     }
 }
