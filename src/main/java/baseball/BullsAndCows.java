4cea3ec554f489f7106d97604e9547809faf19b5
Choi-JJunho <junho5336@gmail.com>
2022-11-05 21:19:41 +0900

    refactor(BullsAndCows): replace validation logic
    replace Validation logic in BullsAndCows
    - remove ValidationHandler
    - add validateIsNumber() method
    - add validateLength() method
    - add const NUMBER_LENGTH, REGEX_NOT_NUMBER

--- src/main/java/baseball/BullsAndCows.java
+++ src/main/java/baseball/BullsAndCows.java
@@ -12,19 +12,24 @@ import java.util.stream.IntStream;
 import static baseball.ResultMessage.*;
 
 public class BullsAndCows {
+
+    private static final int NUMBER_LENGTH = 3;
+    private static final String REGEX_NOT_NUMBER = "^\\d";
+    private static final int STRIKE_COUNT_FOR_END = 3;
+
     private final List<Integer> answerNumberList = new ArrayList<>();
-
     private List<Integer> userInput;
-
     private int strikeCount = 0;
     private int ballCount = 0;
-    private static final int STRIKE_COUNT_FOR_END = 3;
 
     BullsAndCows() {
         createRandomAnswer();
     }
 
     public String getResultOfGuessNumber(String userInput) {
+        validateIsNumber(userInput);
+        validateLength(userInput);
+
         this.userInput = Arrays.stream(userInput.split(""))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
@@ -95,5 +100,17 @@ public class BullsAndCows {
 
     public boolean isEnd() {
         return strikeCount == STRIKE_COUNT_FOR_END;
+    }
+
+    private void validateLength(String input) {
+        if (input.length() != NUMBER_LENGTH) {
+            throw new IllegalArgumentException("number length must be 3");
+        }
+    }
+
+    private void validateIsNumber(String input) {
+        if (input.matches(REGEX_NOT_NUMBER)) {
+            throw new IllegalArgumentException("input value must be number");
+        }
     }
 }
