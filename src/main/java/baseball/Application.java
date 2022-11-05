e00498e80971922febb24e0f5d6574196400eac0
jinny-codes <77732679+jinny-codes@users.noreply.github.com>
2022-11-05 16:01:59 +0900

    feat(Application): add a random number generator a method that generates three distinct random integers that the user has to guess - input: none - output: List<Integer>

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.*;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+    static List<Integer> generateRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        int randomNumber;
+        while (computer.size() < 3) {
+            randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
