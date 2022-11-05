7a39555529de88864efca2f135910f4f9ba1f625
meat7238 <meat7238@kakao.com>
2022-11-06 07:47:14 +0900

    Add Game.java (사용자의 입력값을 저장하는 기능) Update docs.README.md
    사용자의 입력값을 저장하는 기능 : 주어진 api의 readLine()활용
    예외처리
    1.입력된 숫자의 갯수가 3개가 아닐 때
    2.입력값이 0이 포함되어 있을 때
    3.중복된 값이 있을 때

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,32 +2,64 @@ package baseball;
 
 import java.util.LinkedList;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Game {
     private static final int RANDOM_START_RANGE = 1;
     private static final int RANDOM_END_RANGE = 9;
     static final int MAX_NUMBER_COUNT = 3;
-    static LinkedList<Integer> answer;
 
     Game() {
-        answer = new LinkedList<>();
+        LinkedList<Integer> answer = new LinkedList<>();
         answer = setComputerNumber();
+        LinkedList<Integer> UserAnswer = getUserAnswer();
     }
 
     public LinkedList<Integer> setComputerNumber() {
         int RandomNumber;
         LinkedList<Integer> result = new LinkedList<>();
         while (result.size() < MAX_NUMBER_COUNT) {
-            RandomNumber = this.getRandomNumber();
-            if (!result.contains(RandomNumber)) {
+            RandomNumber = getRandomNumber();
+            if (!checkListContains(result, RandomNumber))
                 result.add(RandomNumber);
-            }
         }
         return result;
     }
 
+    static boolean checkListContains(LinkedList<Integer> list, int num) {
+        for (int value : list) {
+            if (value == num) return true;
+        }
+        return false;
+    }
+
     private int getRandomNumber() {
         return pickNumberInRange(Game.RANDOM_START_RANGE, Game.RANDOM_END_RANGE);
+    }
+
+    public static LinkedList<Integer> getUserAnswer() {
+        LinkedList<Integer> userAnswer = new LinkedList<>();
+        System.out.println("숫자를 입력해주세요 : ");
+        for (String str : readLine().split(""))
+            userAnswer.add(Integer.valueOf(str));
+        Game.checkInputValue(userAnswer);
+        return userAnswer;
+    }
+
+    private static void checkInputValue(LinkedList<Integer> userAnswer) {
+        if (userAnswer.size() != Game.MAX_NUMBER_COUNT) {
+            throw new IllegalArgumentException();
+        }
+        if (userAnswer.contains(0)) {
+            throw new IllegalArgumentException();
+        }
+        LinkedList<Integer> result = new LinkedList<>();
+        for (int num : userAnswer) {
+            if (!checkListContains(result, num))
+                result.add(num);
+            else
+                throw new IllegalArgumentException();
+        }
     }
 }
