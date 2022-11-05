c4a58802e7ec5d3f9895a07f7e821de5046e5947
K-Diger <kitten.diger@gmail.com>
2022-11-05 20:46:33 +0900

    refactor : BaseBallGameStater 클래스명 변경 - BaseBallGameStarter -> BaseballGameStarter

--- src/main/java/baseball/starter/BaseballGameStarter.java
+++ src/main/java/baseball/starter/BaseballGameStarter.java
@@ -9,32 +9,31 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BaseBallGameStarter {
-
-    static UserInputValidator userInputValidator = new UserInputValidator();
+public class BaseballGameStarter {
 
     public List<List<String>> operate() {
+        UserInputValidator userInputValidator = new UserInputValidator();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
+
         List<String> userInputValue = new User(userInputValidator)
                 .stringConvertToStringList(
                         Console.readLine());
         List<String> computerGeneratedValue = new Computer().generateNumber();
-
         return new ArrayList<>() {{
             add(userInputValue);
             add(computerGeneratedValue);
         }};
     }
 
-    public boolean judgementOperate(List<String> userInputValue, List<String> computerGeneratedValue) {
+    public boolean operateRefereeJudgement(List<String> userInputValue, List<String> computerGeneratedValue) {
         Referee referee = new Referee();
+        UserInputValidator userInputValidator = new UserInputValidator();
 
         while (referee.judgement(computerGeneratedValue, userInputValue).get("strikeCount") != 3) {
-
             if (referee.judgement(computerGeneratedValue, userInputValue).get("strikeCount") != 3) {
-                referee.say(referee.judgement(computerGeneratedValue, userInputValue));
+                referee.sayJudgement(referee.judgement(computerGeneratedValue, userInputValue));
                 System.out.print("숫자를 입력해주세요 : ");
                 userInputValue = new User(userInputValidator)
                         .stringConvertToStringList(
