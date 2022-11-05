b000c8e91e22e9aff4b4df5f3a51ef876bf50bcf
swonny <wonny921@gmail.com>
2022-11-06 00:09:13 +0900

    feat(main): 게임 반복 여부 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    final static String REPEAT_OPTION_GUIDE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    final static String QUIT_GAME_CONDITION = "2";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        while (true) {
+            Game game = new Game();
+            game.play();
+            System.out.print(REPEAT_OPTION_GUIDE_MESSAGE);
+            if (Console.readLine() == QUIT_GAME_CONDITION) {
+                break;
+            }
+        }
     }
 }
