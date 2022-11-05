f440c1156c32f0af508aad0f1e551f656c0f532c
Choi-JJunho <junho5336@gmail.com>
2022-11-05 22:09:36 +0900

    refactor(Application): simplify start logic

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,28 +1,9 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Application {
+
     public static void main(String[] args) {
-
         BullsAndCows bullsAndCows = new BullsAndCows();
-
-        while (true) {
-            String input = Console.readLine();
-            if(bullsAndCows.isEnd()) {
-
-                if(input.equals("1")) {
-                    bullsAndCows.restart();
-                }
-
-                if(input.equals("2")) {
-                    System.out.println("게임 종료");
-                    break;
-                }
-
-            }
-
-            System.out.println(bullsAndCows.getResultOfGuessNumber(input));
-        }
+        bullsAndCows.playGame();
     }
 }
