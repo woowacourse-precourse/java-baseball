35699449b9a74ae1753615c628b20bb5bd2baf23
MinHyeok Moon <odo27@MinHyeoks-MacBook-Air.local>
2022-11-05 13:10:34 +0900

    feat(Game): add finishMessage

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,4 +4,8 @@ public class Game {
     public void startMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void finishMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
