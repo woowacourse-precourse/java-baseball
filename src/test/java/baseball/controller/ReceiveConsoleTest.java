105cff47338507939ed4d8c8e7d22752bd703c9f
gyuturn <rbtjrrbals@gmail.com>
2022-11-06 03:17:51 +0900

    test($controller): 함수변경 receiveNum->receiveNumIncludeAllException
    요구사항에맞게 사용자가 유요하지 않은 입력시 모든 예외를 IllegalAccessException처리하기 위함
    
    C-4
    ReceiveConsoleTest의 모든 테스트케이스에 적용

--- src/test/java/baseball/controller/ReceiveConsoleTest.java
+++ src/test/java/baseball/controller/ReceiveConsoleTest.java
@@ -3,42 +3,52 @@ package baseball.controller;
 
 import baseball.dto.NumListDto;
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.List;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 class ReceiveConsoleTest {
+    @BeforeEach
+    void 콘솔값clear() {
+        System.setOut(null);
+    }
 
     @Test
-    void receiveNum_성공테스트() {
+    void receiveNum_성공테스트() throws IllegalAccessException {
         //given
         System.setIn(new ByteArrayInputStream(("123"+'\n').getBytes()));
         //when
-        NumListDto numListDto = ReceiveConsole.receiveNum();
+        NumListDto numListDto = ReceiveConsole.receiveNumIncludeAllException();
         //then
         assertThat(numListDto.getNumList()).isEqualTo(Arrays.asList(1,2,3));
     }
 
+
     @Test
-    void receiveNum_한글이있는경우() {
+    void receiveNum_숫자외다른것이있는케이스() {
         //given
-        System.setIn(new ByteArrayInputStream(("한1글"+'\n').getBytes()));
+        System.setIn(new ByteArrayInputStream(("h1!"+'\n').getBytes()));
         //when
         //then
-        assertThatThrownBy(() -> ReceiveConsole.receiveNum())
+        assertThatThrownBy(() -> ReceiveConsole.receiveNumIncludeAllException())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void receiveNum_영어가있는경우() {
+    void receiveNum_한글이있는경우테스트() {
         //given
-        System.setIn(new ByteArrayInputStream(("h1i"+'\n').getBytes()));
+        System.setIn(new ByteArrayInputStream(("한1글"+'\n').getBytes()));
         //when
         //then
-        assertThatThrownBy(() -> ReceiveConsole.receiveNum())
+        assertThatThrownBy(() -> ReceiveConsole.receiveNumIncludeAllException())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
