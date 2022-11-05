7190d56f4c636e1f825fe3a55076011bee77a3fa
woo-chang <clllickme@naver.com>
2022-11-05 21:39:01 +0900

    refactor: GameManger에 종속적인 ExecuteStatus를 Nested Enum으로 수정

--- src/main/java/baseball/util/GameManager.java
+++ src/main/java/baseball/util/GameManager.java
@@ -2,6 +2,7 @@ package baseball.util;
 
 import baseball.domain.Game;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 
 public class GameManager {
 
@@ -20,5 +21,26 @@ public class GameManager {
     private ExecuteStatus restart() {
         GamePrinter.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return ExecuteStatus.from(Console.readLine());
+    }
+
+    enum ExecuteStatus {
+        RESTART("1"), END("2");
+
+        private final String status;
+
+        ExecuteStatus(String status) {
+            this.status = status;
+        }
+
+        public String getStatus() {
+            return status;
+        }
+
+        public static ExecuteStatus from(String status) {
+            return Arrays.stream(ExecuteStatus.values())
+                .filter(e -> e.getStatus().equals(status))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 상태 입력입니다. 입력: " + status));
+        }
     }
 }
