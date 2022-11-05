de259f7d3f77c9c2e4381108bacd137dd6372e06
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-06 00:27:24 +0900

    feat: 컴퓨터의 값을 생성하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,9 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -7,6 +11,7 @@ public class Application {
 }
 
 class Baseball {
+    private static List<Integer> computerNumber;
     private static final int START_NUM_INCLUSIVE = 1;
     private static final int END_NUM_INCLUSIVE = 9;
     private static final int NUMBER_PITCH = 3;
@@ -19,4 +24,13 @@ class Baseball {
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String INPUT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    private static void buildComputerNumber() {
+        while (computerNumber.size() < NUMBER_PITCH) {
+            int randomNumber = Randoms.pickNumberInRange(START_NUM_INCLUSIVE, END_NUM_INCLUSIVE);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+    }
 }
