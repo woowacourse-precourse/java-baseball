eb44eee8e2bc8ce3cce504ecb0cc6a4a512c4b6c
splitube <voicesplit@daum.net>
2022-11-05 17:32:33 +0900

    test(NumberMaker): 단위 테스트 추가
    랜덤 숫자 생성 1000번 테스트, 숫자 리스트 생성 1000번 테스트 단위 테스트 추가.
    given, when, then 형식, @DisplayName 추가

--- src/test/java/baseball/NumberMakerTest.java
+++ src/test/java/baseball/NumberMakerTest.java
@@ -4,28 +4,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class NumberMakerTest {
-    NumberMaker numberMaker = new NumberMaker();
-
+    @DisplayName("랜덤 숫자 생성 1000번 테스트")
     @Test
-    void makeNumberInRange() {
-        int start = 1;
-        int end = 9;
-        for (int i = 0; i < 100; i++) {
-            int randomNumber = numberMaker.makeNumberInRange(start, end);
-            assertThat(randomNumber).isBetween(start, end);
+    void makeNumberInRangeRepeat() {
+        for (int i = 0; i < 1000; i++) {
+            makeNumberInRange();
         }
     }
 
+    @DisplayName("숫자 리스트 생성 1000번 테스트")
     @Test
-    void makeThreeDifferentNumberListInRange() {
+    void makeThreeDifferentNumberListInRangeRepeat() {
+        for (int i = 0; i < 1000; i++) {
+            makeThreeDifferentNumberListInRange();
+        }
+    }
+
+    @DisplayName("원하는 범위의 랜덤 숫자 생성 테스트")
+    @Test
+    void makeNumberInRange() {
+        //given
+        NumberMaker numberMaker = new NumberMaker();
         int start = 1;
         int end = 9;
-        for (int i = 0; i < 100; i++) {
-            List<Integer> numberList = numberMaker.makeThreeDifferentNumberListInRange(start, end);
-            assertThat(numberList).allSatisfy(number -> assertThat(number).isBetween(start, end));
-        }
+        //when
+        int randomNumber = numberMaker.makeNumberInRange(start, end);
+        //then
+        assertThat(randomNumber).isBetween(start, end);
+    }
+
+    @DisplayName("원하는 범위의 서로 다른 3개의 숫자 리스트 생성 테스트")
+    @Test
+    void makeThreeDifferentNumberListInRange() {
+        //given
+        NumberMaker numberMaker = new NumberMaker();
+        int start = 1;
+        int end = 9;
+        //when
+        List<Integer> numberList = numberMaker.makeThreeDifferentNumberListInRange(start, end);
+        //then
+        assertThat(numberList.size()).isEqualTo(3);
+        assertThat(numberList).allSatisfy(number -> assertThat(number).isBetween(start, end));
     }
 }
