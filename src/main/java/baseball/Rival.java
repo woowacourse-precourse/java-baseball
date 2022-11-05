02856adbb4c2efa25fd9996c034d9fe2aad2ee69
hectick <chyeon1223@cau.ac.kr>
2022-11-06 08:53:33 +0900

    feat : 서로 다른 3자리의 수를 생성하는 기능 추가

---
+++ src/main/java/baseball/Rival.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rival {
+    private List<Integer> number;
+
+    Rival(){
+        createNumber();
+    }
+
+    private void createNumber(){
+        number = new ArrayList<>();
+        while(number.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!number.contains(randomNumber)){
+                number.add(randomNumber);
+            }
+        }
+    }
+
+}
