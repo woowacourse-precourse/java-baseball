e4f3085a3286a6d661faf6565027dbeb378240d0
jyeost <pig9619@naver.com>
2022-11-05 16:33:12 +0900

    feat : 사용자의 입력값이 제대로 되었다면 볼리스트로 반환해주는 기능추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,7 +1,10 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
@@ -9,9 +12,48 @@ public class User {
     public static List<Integer> inputBalls() {
         System.out.println("숫자야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
-        String userInput =Console.readLine();
-        System.out.print(userInput);
 
-        return new ArrayList<Integer>() ;
+        String userInput =Console.readLine();
+        List<Integer> userBallsList = checkUserInput(userInput);
+
+        return userBallsList;
+    }
+
+
+    public static List<Integer>checkUserInput(String userInput){
+
+        if(userInput== null){
+            throw new IllegalArgumentException();
+        }
+
+        if(userInput.length()!=3){
+            throw new IllegalArgumentException();
+        }
+
+        int userInputNum = 0;
+
+        try{
+            userInputNum = Integer.parseInt(userInput);
+
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException();
+        }
+
+        return makeUserBalls(userInputNum);
+    }
+
+
+    private static List<Integer> makeUserBalls(int userInputNum) {
+
+        List<Integer> userBallsList = Arrays.asList(userInputNum/100, userInputNum%100/10, userInputNum%10);
+        Stream<Integer> userBallStream = userBallsList.stream();
+
+        int count = (int) userBallStream.distinct().count();
+        if(count!=3){
+            throw new IllegalArgumentException();
+        }
+
+        return userBallsList;
+
     }
 }
