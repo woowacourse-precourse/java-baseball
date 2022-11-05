70d9997b8041dd0f041ed7912f63f207a56e7b01
YE-EUN <musiclove529@naver.com>
2022-11-05 12:00:18 +0900

    feat : ProblemMaker 클래스 생성
    - 문제를 제출하는 Player 구체 클래스 생성

---
+++ src/main/java/baseball/ProblemMaker.java
@@ -0,0 +1,18 @@
+package baseball;
+
+public class ProblemMaker extends Player{
+
+    @Override
+    public void setNumbers(int sizeOfNumbers) {
+
+    }
+
+    public boolean hasNumber(int number){
+        return false;
+    }
+
+    public boolean isSameAtGivenLocation(int number, int index){
+
+        return false;
+    }
+}
