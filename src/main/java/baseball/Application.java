e7624a8cc7ef9aaa682acf8a7cb6acbd737ca8da
psy-choi <84831081+psy-choi@users.noreply.github.com>
2022-11-05 23:24:48 +0900

    feat : baseball_random() 함수 구현
    readme.md 2번

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,33 @@
 package baseball;
 
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
+
 public class Application {
+
+    // 2번 랜덤으로 computer에서 제공하는 정답을 얻어내는 메소드
+    public static String baseball_random() {
+
+        String baseball = "";
+
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        for (int i : computer){
+            baseball += Integer.toString(i);
+        }
+
+        return baseball;
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,12 +3,27 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+
+import static baseball.Application.*;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void test_baseball_random() {
+        String input = baseball_random();
+
+        assertThatThrownBy(() -> input.charAt(3))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
+
+
+
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
