12b3fc30203db1541438329c8bef5d36e7f41b3c
NotableDeveloper <castlereal@naver.com>
2022-11-05 01:03:05 -0700

    <feat> : 입력 중복확인 기능 추가 사용자가 입력한 내용에 중복된 숫자가 있는 지를 검사하느 기능 추가. 사용자 입력이 1부터 9사이의 숫자가 아닌 경우에 대한 예외 메시지 및 테스트 메서드 이름 변경.

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -10,8 +10,9 @@ public class User {
         String inputValue;
         System.out.print("숫자를 입력해주세요 : ");
         inputValue = stdInput.nextLine();
-        validateInput(inputValue);
-        this.number = 0;
+
+        if(validateInput(inputValue))
+            this.number = Integer.parseInt(inputValue);
     }
 
     private boolean validateInput(String inputValue) throws IllegalArgumentException {
@@ -20,17 +21,34 @@ public class User {
         if(inputValue.length() != 3)
             throw new IllegalArgumentException("입력하신 숫자가 세 자리가 아닙니다.");
 
-        // To do : 서로 다른 숫자를 입력하였는 지를 확인
+        // 서로 다른 숫자를 입력하였는 지를 확인
+        if(containsValue(inputValue))
+            throw new IllegalArgumentException("입력하신 내용 중에 중복되는 숫자가 있습니다.");
 
         // 1부터 9사이의 숫자인 지를 확인
         for(char c : inputValue.toCharArray()) {
             if(!Character.isDigit(c) && !(c == '0')) {
-                throw new IllegalArgumentException("입력하신 내용 중에 숫자가 아닌 값이 있습니다.");
+                throw new IllegalArgumentException("입력하신 내용 중에 숫자가 아니거나, 0인 값이 있습니다.");
             }
         }
 
         return true;
     }
 
+    private boolean containsValue(String inputValue) {
+        char[] temp = new char[inputValue.length() + 1];
+
+        for(int i = 0; i < inputValue.length(); i++)
+            temp[i] = inputValue.charAt(i);
+
+        temp[inputValue.length()] = '0';
+
+        for(int i = 0; i < inputValue.length(); i++){
+            if(temp[i] == temp[i + 1])
+                return true;
+        }
+
+        return false;
+    }
 
 }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -35,20 +35,34 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 입력이_숫자가_아닌경우() {
+    void 입력이_1부터_9사이의_숫자가_아닌경우() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("q12"))
                         .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage("입력하신 내용 중에 숫자가 아닌 값이 있습니다.")
+                        .hasMessage("입력하신 내용 중에 숫자가 아니거나, 0인 값이 있습니다.")
         );
 
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("qwe"))
                         .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessage("입력하신 내용 중에 숫자가 아닌 값이 있습니다.")
+                        .hasMessage("입력하신 내용 중에 숫자가 아니거나, 0인 값이 있습니다.")
         );
     }
 
+    @Test
+    void 같은_숫자를_입력한경우() {
+        assertSimpleTest(() ->
+            assertThatThrownBy(() -> runException("122"))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessage("입력하신 내용 중에 중복되는 숫자가 있습니다.")
+        );
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("333"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage("입력하신 내용 중에 중복되는 숫자가 있습니다.")
+        );
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
