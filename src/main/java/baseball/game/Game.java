473d4c0bfd237ea53355f25c447f8c15eba17473
ysng_ysng <yeasung67@gmail.com>
2022-11-06 02:04:55 +0900

    feat(Game): implement play method in Game class
    Game 클래스의 play메서드를 구현했습니다.

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -3,13 +3,11 @@ package baseball.game;
 import baseball.playerComputer.PlayerComputer;
 import baseball.playerUser.PlayerUser;
 
+import java.util.List;
+
 public class Game {
 	private static final Game instance = new Game();
 
-
-	private enum JudgeIndex {
-		STRIKE, BALL
-	}
 
 	private Game() {
 	}
@@ -18,9 +16,17 @@ public class Game {
 		return instance;
 	}
 
-	public void play(PlayerComputer computer, PlayerUser user) {
+	public int play(PlayerComputer computer, PlayerUser user) {
+		do {
+			String answer = computer.getRandomNumber();
+			String userInput = user.getInputNumber();
 
+			List<Integer> judgement = compareUserInputToAnswer(userInput, answer);
+
+			printMessage(judgement);
+			int numberOfStrike = judgement.get(STRIKE);
+		} while (!isCorrectAnswer(numberOfStrike));
+
+		return askReplay();
 	}
-
-
 }
