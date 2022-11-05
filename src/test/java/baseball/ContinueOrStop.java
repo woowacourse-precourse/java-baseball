11e0a535013eaee8a78c8bdcb60240de6d264d4f
saewoo1 <wooozooo1123@gmail.com>
2022-11-05 19:41:26 +0900

    refactor(ContinueOrStop): 올바른 버튼에 대한 추가조건 설정 한 자리만 들어왔을 때 숫자만 들어왔을 때

--- src/test/java/baseball/ContinueOrStop.java
+++ src/test/java/baseball/ContinueOrStop.java
@@ -2,11 +2,12 @@ package baseball;
 
 public class ContinueOrStop {
   public static boolean choice(String num) {
-    if (num.contains("1"))
-      return true;
-    else if (num.contains("2"))
-      return false;
-    else
-      throw new IllegalArgumentException();
+    if (Exception.isValidButton(num)) {
+      if (num.contains("1"))
+        return true;
+      else if (num.contains("2"))
+        return false;
+    }
+    throw new IllegalArgumentException();
   }
 }
