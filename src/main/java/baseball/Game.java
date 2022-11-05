ca027f8b526b908f435583bdb5c54fbfc2cefaad
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-05 23:49:19 +0900

    feat(Game): hint 메서드 구현
    strike, ball 개수가 주어져있을 때 힌트 결과를 문자열로 반환하는 메서드 hint 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -13,4 +13,18 @@ public class Game {
         Computer computer = Computer.generateAnswer();
         return new Game(computer, user);
     }
+
+    String hint(int strike, int ball) {
+        StringBuilder hint = new StringBuilder();
+        if (ball > 0) {
+            hint.append(ball).append("볼 ");
+        }
+        if (strike > 0) {
+            hint.append(strike).append("스트라이크 ");
+        }
+        if (hint.length() == 0) {
+            hint.append("낫싱");
+        }
+        return hint.toString().trim();
+    }
 }
