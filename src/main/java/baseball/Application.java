e8ae1192ae3c33ef8f1e644b80de19c2ded98f82
baekeunsun <eunsun5460@gmail.com>
2022-11-05 16:13:44 +0900

    feat(ComputerGenerate, AddComputerValue): 상대방(컴퓨터) 숫자 생성 기능
    - ComputerGenerate: 사이즈가 3인 computer리스트 생성
    - AddComputerValue: 1~9의 랜덤 값이 중복되지 않았을 경우, computer리스트에 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static final int NUM_SIZE = 3;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> computer = ComputerGenerate();
+    }
+
+    private static List<Integer> ComputerGenerate() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < NUM_SIZE) {
+            AddComputerValue(computer);
+        }
+        return computer;
+    }
+
+    private static List<Integer> AddComputerValue(List<Integer> computer) {
+        int randomNumber = Randoms.pickNumberInRange(1,9);
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber);
+        }
+        return computer;
     }
 }
