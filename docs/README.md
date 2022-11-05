5ef8e59c79e5798dfa473786b4bbbec24b0e635f
coldbucks <met.hsw@gmail.com>
2022-11-05 16:32:51 +0900

    feat: 기능구현 2번째 추가

--- docs/README.md
+++ docs/README.md
@@ -8,10 +8,15 @@
  - Project Encoding: UTF-8
  - Defalut encoding for properties files: UTF-8
  - (체크) Transparent native-to-ascil convention
- 이후 인텔리제이 재시작
- 1차 실패
-  
+ - 이후 인텔리제이 재시작
+ - 1차 실패
+ - <<구글 검색>>
   - Help - Edit Custum VM options
   - -Dfile.encoding=UTF-8  <== 추가입력
- 인텔리제이 재시작
- 성공
+  - 인텔리제이 재시작
+  - 성공
+
+
+2. String 매개변수를 List(Integer)로 전환하기
+
+3. 컴퓨터 숫자와 내 숫자를 비교하는 check() 메소드 만들기
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,18 +5,42 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
+
+
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Application T = new Application();
 
+        //컴퓨터 랜덤 3개의 숫자
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
 
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println("숫자를 입력해주세요 : ");
         String userNum = Console.readLine();
+        System.out.println("숫자를 입력해주세요 : "+ userNum);
 
+        // 내숫자 List<Integer> 배열로 만들기
+        String[] s = userNum.split("");
+        List<Integer> myList = new ArrayList<>();
+        for(int i = 0 ; i<userNum.length() ; i++) {
+            myList.add(Integer.parseInt(s[i]));
+        }
+        System.out.println("computer= " + computer);
+        System.out.println("내 숫자= "+ myList);
 
+        }
+    private static void changeUserNum() {
     }
+
+
 }
+
