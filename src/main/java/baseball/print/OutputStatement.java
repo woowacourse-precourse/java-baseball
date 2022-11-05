2a24e79041c3d3eb58369f5e0eb54a469beb5956
jaehyeon <jhjh0022@naver.com>
2022-11-05 14:38:31 +0900

    fix: 코드 수정 및 추가

--- src/main/java/baseball/print/OutputStatement.java
+++ src/main/java/baseball/print/OutputStatement.java
@@ -10,25 +10,21 @@ public class OutputStatement {
 
     }
 
-    public static void informBallCountResult(){
-        System.out.println("볼");
-
+    public static void notSing(){
+        System.out.println("낫싱");
     }
 
-    public static void informStrikeCountResult(){
-        System.out.println("스트라이크");
-    }
-
-    public static void hint(int ball, int strike){
-        if(ball ==0){
+    public static void hint(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            notSing();
+        } else if (ball == 0) {
             System.out.println(strike + "스트라이크");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else {
+            System.out.println(ball + "볼" + " " + strike + "스트라이크");
         }
-        if(strike ==0){
-            System.out.println(ball +"볼");
-        }
-        System.out.println(ball + '볼' + " " + strike + "스트라이크");
     }
-
     public static void informResultAnswer(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
