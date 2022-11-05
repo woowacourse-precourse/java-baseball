6d135c1d306a0389d2596ec405d35cec1b66104f
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-05 21:21:38 +0900

    feat : input 클래스 생성 및 기초 입력 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+
+class input{
+    public String input_string;
+    public String input_number()
+    {
+        String input_string = Console.readLine();
+        return input_string;
+    }
+    public void make_integer_arraylist()
+    {
+        this.input_string = input_number();
+        System.out.println(input_string);
+    }
+}
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        input input = new input();
+        input.make_integer_arraylist();
     }
 }
