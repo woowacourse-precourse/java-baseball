6634c6585d8a49ca4ead38d5190618f3c11709b8
gyuturn <rbtjrrbals@gmail.com>
2022-11-06 01:42:01 +0900

    test(model): generateRandomNum 테스트
    컴퓨터가 올바른 numList를 반환하는지 테스트위함
    
    B-1-1
    10000번의 테스트를 통해 정확도 100퍼센트 테스트

--- src/test/java/baseball/model/ComputerTest.java
+++ src/test/java/baseball/model/ComputerTest.java
@@ -1,12 +1,26 @@
 package baseball.model;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.*;
+import baseball.controller.Valid;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class ComputerTest {
 
     @Test
-    void 랜덤한() {
+    void generateRandomNum_numList정확도테스트_10000번테스트() {
+        //given
+        Computer[] computer = new Computer[10000];
+        //when
+        int notValidTestCase=0;
+        for (int i = 0; i < 10000; i++) {
+            computer[i] = new Computer();
+            List<Integer> generateRandomNumList = computer[i].generateRandomNum();
+            if(!Valid.isValidNumList(generateRandomNumList)){
+                notValidTestCase++;
+            }
+        }
+        //then
+        assertThat(notValidTestCase).isEqualTo(0);
     }
 }
