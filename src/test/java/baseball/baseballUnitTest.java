50fbcb620c9cb469f1d87165a33b0cbb6496a26c
Heosejin98 <sejin3319@naver.com>
2022-11-05 23:52:08 +0900

    Test : 테스트케이스 주석처리
    캡슐화로 인해 사용할 수 없는 케이스들 주석
    처리

--- src/test/java/baseball/baseballUnitTest.java
+++ src/test/java/baseball/baseballUnitTest.java
@@ -21,7 +21,7 @@ public class baseballUnitTest {
         @Test
         @DisplayName("유저 입력값 숫자 2개")
         void 적은입력() {
-            assertThatThrownBy(() -> new Exception().Check("12"))
+            assertThatThrownBy(() -> new Exception().check("12"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자를 3개 입력하세요");
         }
@@ -29,7 +29,7 @@ public class baseballUnitTest {
         @Test
         @DisplayName("유저 입력값 숫자 4개")
         void 많은입력() {
-            assertThatThrownBy(() -> new Exception().Check("1234"))
+            assertThatThrownBy(() -> new Exception().check("1234"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자를 3개 입력하세요");
         }
@@ -37,7 +37,7 @@ public class baseballUnitTest {
         @Test
         @DisplayName("유저 입력값 형식 오류")
         void 문자입력() {
-            assertThatThrownBy(() -> new Exception().Check("12a"))
+            assertThatThrownBy(() -> new Exception().check("12a"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자만 입력하세요");
         }
@@ -45,7 +45,7 @@ public class baseballUnitTest {
         @Test
         @DisplayName("중복 숫자 입력")
         void 중복숫자() {
-            assertThatThrownBy(() -> new Exception().Check("121"))
+            assertThatThrownBy(() -> new Exception().check("121"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("중복된 숫자가 있습니다");
         }
@@ -53,37 +53,38 @@ public class baseballUnitTest {
         @Test
         @DisplayName("음수 입력")
         void 음수입력() {
-            assertThatThrownBy(() -> new Exception().Check("-12"))
+            assertThatThrownBy(() -> new Exception().check("-12"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("양수만 입력하세요");
         }
     }
 
+    /*
     @Nested
     @DisplayName("게임 기능 검증")
     class baseballGameTestCase {
 
-        BaseballGame baseballGame = new BaseballGame();
+        BaseballUmpire baseballUmpire = new BaseballUmpire();
 
         @Test
         @DisplayName("스트라이크 볼 판독기")
         void 스트라이크() {
-            baseballGame.ballReader(1, 1, true);
-            assertThat(baseballGame.getStrike()).isEqualTo(1);
+            baseballUmpire.ballReader(1, 1, true);
+            assertThat(baseballUmpire.getStrike()).isEqualTo(1);
         }
 
         @Test
         @DisplayName("ball 볼 판독기")
         void 볼() {
-            baseballGame.ballReader(1, 1, false);
-            assertThat(baseballGame.getBall()).isEqualTo(1);
+            baseballUmpire.ballReader(1, 1, false);
+            assertThat(baseballUmpire.getBall()).isEqualTo(1);
         }
 
         @Test
         @DisplayName("볼 판독기 (노스트라이크 노볼)")
         void 노볼_노스트라이크() {
-            baseballGame.ballReader(1, 3, false);
-            assertThat(baseballGame.getBall()).isEqualTo(0);
+            baseballUmpire.ballReader(1, 3, false);
+            assertThat(baseballUmpire.getBall()).isEqualTo(0);
         }
 
         @Test
@@ -91,7 +92,7 @@ public class baseballUnitTest {
         void 공던지기case1() {
             ArrayList<Integer> computer = new ArrayList<>(Arrays.asList(1, 2, 3));
 
-            assertThat(baseballGame.throwBall("123", computer)).isEqualTo(false);
+            assertThat(baseballUmpire.throwBall("123", computer)).isEqualTo(false);
         }
 
         @Test
@@ -99,11 +100,10 @@ public class baseballUnitTest {
         void 공던지기case2() {
             ArrayList<Integer> computer = new ArrayList<>(Arrays.asList(1, 5, 3));
 
-            baseballGame.throwBall("134", computer);
-            assertThat(baseballGame.getStrike()).isEqualTo(1);
-            assertThat(baseballGame.getBall()).isEqualTo(1);
+            baseballUmpire.throwBall("134", computer);
+            assertThat(baseballUmpire.getStrike()).isEqualTo(1);
+            assertThat(baseballUmpire.getBall()).isEqualTo(1);
         }
-
 
         @Test
         @DisplayName("print hint test (3스크라이크)")
@@ -113,9 +113,11 @@ public class baseballUnitTest {
             ArrayList<Integer> computer = new ArrayList<>(Arrays.asList(1, 5, 4));
             final String message = 3 + "스트라이크\r\n";
 
-            baseballGame.throwBall("154", computer);
+            baseballUmpire.throwBall("154", computer);
             assertThat(message).isEqualTo(out.toString());
         }
 
     }
+*/
+
 }
