05073174d833fae73f0a57fda2dab2b512a6704d
HiiWee <wpdlzhf159@gmail.com>
2022-11-06 00:38:53 +0900

    Feat: 숫자 야구 게임을 위한 임의의 서로다른 3가지 숫자 생성 기능

--- src/main/java/baseball/domain/ComputerNumber.java
+++ src/main/java/baseball/domain/ComputerNumber.java
@@ -1,6 +1,12 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class ComputerNumber {
+    private static final int FIRST = 0;
+    private static final int SECOND = 1;
+    private static final int THIRD = 2;
+
     private int firstNumber;
     private int secondNumber;
     private int thirdNumber;
@@ -10,6 +16,12 @@ public class ComputerNumber {
         this.firstNumber = firstNumber;
         this.secondNumber = secondNumber;
         this.thirdNumber = thirdNumber;
+    }
+
+    public ComputerNumber(List<Integer> numberList) {
+        this.firstNumber = numberList.get(FIRST);
+        this.secondNumber = numberList.get(SECOND);
+        this.thirdNumber = numberList.get(THIRD);
     }
 
     public int getFirstNumber() {
--- src/main/java/baseball/repository/NumberBaseBallRepository.java
+++ src/main/java/baseball/repository/NumberBaseBallRepository.java
@@ -1,10 +1,40 @@
 package baseball.repository;
 
 import baseball.domain.ComputerNumber;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class NumberBaseBallRepository {
+    private static final int LIMIT_NUMBER = 10;
+    private static final int START_NUMBER = 1;
+    private static final int PICK_NUMBER_COUNT = 3;
+
+
     private static final NumberBaseBallRepository instance = new NumberBaseBallRepository();
     private static final List<ComputerNumber> numberStore = new ArrayList<>();
+
+    public void generateNumber() {
+        clearNumberStore();
+        List<Integer> positiveIntegerList = createPositiveIntegerList();
+        List<Integer> pickNumberList = new ArrayList<>();
+        for (int currentIndex = 0; currentIndex < PICK_NUMBER_COUNT; currentIndex++) {
+            pickNumberList.add(Randoms.pickNumberInList(positiveIntegerList));
+            positiveIntegerList.remove(pickNumberList.get(currentIndex));
+        }
+        numberStore.add(new ComputerNumber(pickNumberList));
+    }
+
+
+    public List<Integer> createPositiveIntegerList() {
+        return IntStream.range(START_NUMBER, LIMIT_NUMBER)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+
+    public void clearNumberStore() {
+        numberStore.clear();
+    }
 }
