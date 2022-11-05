4b0d531f811c967169b4cac715fb132b44e53e64
Jin409 <sualng123@gmail.com>
2022-11-05 18:03:26 +0900

    feat(GameView): GameView 생성 및 구현
    - 게임 시작 문구 출력
    - 숫자 입력 문구 출력
    - 힌트 출력
    - 정답여부 출력
    - 게임 시작 여부 입력받는 문구 출력

---
+++ src/main/java/baseball/view/GameView.java
@@ -0,0 +1,50 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameView {
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String ASK_USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String ASK_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String ONLY_STRIKE_MESSAGE = "%d스트라이크\n";
+    private static final String ONLY_BALL_MESSAGE = "%d볼\n";
+    private static final String BALL_AND_STRIKE_MESSAGE = "%d볼 %d스트라이크\n";
+    private static final String NOTHING_MESSAGE = "낫싱";
+
+    public static void printStartMessage() {
+        System.out.print(START_MESSAGE);
+    }
+
+    public static String askUserInput() {
+        System.out.print(ASK_USER_INPUT_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static void printHint(int strikeCount, int ballCount) {
+        if(strikeCount == 0 && ballCount == 0){
+            System.out.println(NOTHING_MESSAGE);
+            return;
+        }
+        if (strikeCount >= 0 && ballCount == 0) {
+            System.out.printf(ONLY_STRIKE_MESSAGE, strikeCount);
+            return;
+        }
+        if (strikeCount == 0 && ballCount >= 0) {
+            System.out.printf(ONLY_BALL_MESSAGE, ballCount);
+            return;
+        }
+        System.out.printf(BALL_AND_STRIKE_MESSAGE, ballCount, strikeCount);
+    }
+
+    public static String askRestart() {
+        System.out.println(ASK_RESTART_MESSAGE);
+        return Console.readLine();
+    }
+
+    public static void printResult(boolean correct){
+        if(correct){
+            System.out.println(FINISH_MESSAGE);
+        }
+    }
+}
