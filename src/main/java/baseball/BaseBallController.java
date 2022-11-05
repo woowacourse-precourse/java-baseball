6d5ad251063dc1e30b6e45a0cad04aca4ddc6339
Danden1 <goh1211@ajou.ac.kr>
2022-11-06 00:57:13 +0900

    feat : controller playGame, starGame method 추가

--- src/main/java/baseball/BaseBallController.java
+++ src/main/java/baseball/BaseBallController.java
@@ -1,6 +1,30 @@
 package baseball;
 
+import java.util.Map;
+
 public class BaseBallController {
 
+    private BaseBallModel baseBallModel = null;
+
+    public BaseBallController(int numbersLength){
+        baseBallModel = new BaseBallModel(numbersLength);
+    }
+
+    public void startGame(){
+        baseBallModel.generateNumbers();
+    }
+
+    public Map<String, Integer> playGame(String inputNumbers) throws IllegalArgumentException{
+        try{
+            baseBallModel.checkNumbers(inputNumbers);
+        }
+        catch(IllegalArgumentException ex){
+            throw ex;
+        }
+
+        Map<String, Integer> result = baseBallModel.gameResult(inputNumbers);
+
+        return result;
+    }
 
 }
