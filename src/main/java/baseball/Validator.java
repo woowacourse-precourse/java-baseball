5f1c6f6ccbfe9deb559451b46b5f2d7c14902cb8
yun <yshyeon102@gmail.com>
2022-11-06 07:50:27 +0900

    feat(validator): 예외 상황에 따른 예외 메세지 정의

---
+++ src/main/java/baseball/Validator.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class Validator {
+    public static final String LENGTH_EXCEPTION = "3자리 숫자만 입력할 수 있습니다.";
+    public static final String HAVING_ZERO_EXCEPTION = "0은 포함될 수 없습니다.";
+    public static final String HAVING_ANOTHER_CHARACTER_EXCEPTION = "숫자만 포함될 수 있습니다.";
+    public static final String HAVING_SAME_NUMBER_EXCEPTION = "서로 다른 숫자만 입력할 수 있습니다";
+    public static final String RESTART_RESPONSE_EXCEPTION = "1, 2 이외의 문자가 입력되었습니다.";
+}
