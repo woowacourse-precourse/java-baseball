77f9180ef0408c5d2d683ca899bf09d722f96d6d
nakalic <nakalic28@gmail.com>
2022-11-05 20:10:30 +0900

    feat(InputValue): 각 자리의 수가 범위안에 있는지 확인하는 기능 구현

--- src/main/java/baseball/InputValue.java
+++ src/main/java/baseball/InputValue.java
@@ -9,6 +9,8 @@ public class InputValue {
 
     static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     static final int NUMBER_LENGTH = 3;
+    static final int FIRST_NUM = 1;
+    static final int LAST_NUM = 9;
 
     public List<Integer> getUserNumber() {
         List<Integer> userNumber = new ArrayList<>();
@@ -25,6 +27,22 @@ public class InputValue {
 
     public boolean checkLength(List<Integer> num) {
         return num.size() == NUMBER_LENGTH;
+    }
+
+    public boolean checkRange(List<Integer> num) {
+        int cnt = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (FIRST_NUM <= num.get(i) && num.get(i) <= LAST_NUM) {
+                cnt += 1;
+            }
+        }
+
+        if (cnt == NUMBER_LENGTH) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
 }
