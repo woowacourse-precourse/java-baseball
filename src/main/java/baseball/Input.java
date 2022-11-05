998b634310461979f537f22740ca2c318975e6c2
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-05 12:52:22 +0900

    feat: add number input validating method

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -2,13 +2,16 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Input {
 
     public static List<Integer> number() {
         String input = Console.readLine();
+        validateNumberInput(input);
         String[] splitInput = input.split("");
         return Arrays.stream(splitInput)
                 .map(Integer::parseInt)
@@ -18,5 +21,22 @@ public class Input {
     public static int oneOrTwo() {
         String input = Console.readLine();
         return Integer.parseInt(input);
+    }
+
+    public static void validateNumberInput(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        Set<Integer> numberSet = new HashSet<>();
+        String[] splitInput = input.split("");
+        for (String number : splitInput) {
+            if (number.charAt(0) < '1' || number.charAt(0) > '9') {
+                throw new IllegalArgumentException();
+            }
+            numberSet.add(Integer.parseInt(number));
+        }
+        if (numberSet.size() != 3) {
+            throw new IllegalArgumentException();
+        }
     }
 }
