8c33b5946cfbdc1b534cacc23f77c4be0a7050a9
jjdone <ehdlfaos@gmail.com>
2022-11-05 15:11:24 +0900

    fix: Player 클래스 버그 수정
    새로운 숫자 입력시 숫자가 갱신이 안되는 버그 수정

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -5,12 +5,13 @@ import camp.nextstep.edu.missionutils.Console;
 public class Player {
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
-    private List<Integer> numbers = new ArrayList<>();
+    private List<Integer> numbers;
     public Player() {}
 
     public void setNumbers() {
         inputMessage();
         String input = Console.readLine();
+        numbers = new ArrayList<>();
         stringToList(input);
     }
 
