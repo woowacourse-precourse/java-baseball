471558dba43848186706db845f910940507b3946
Invidam <hansupark0208@gmail.com>
2022-11-05 15:35:20 +0900

    test(Opponent): 힌트 결과 테스트코드 추가

--- src/test/java/baseball/OpponentTest.java
+++ src/test/java/baseball/OpponentTest.java
@@ -16,4 +16,81 @@ class OpponentTest {
         }
     }
 
+    @Nested
+    @DisplayName("힌트 유효성 검증")
+    class HintTest {
+        @Test
+        void 스트라이크_3번() {
+            List<Integer> answer = List.of(1,2,3);
+            List<Integer> input = List.of(1,2,3);
+
+            Hint hint = new Hint(answer,input);
+            assertThat(hint.toString()).isEqualTo("3스트라이크");
+        }
+        @Test
+        void 스트라이크_2번() {
+            List<Integer> answer = List.of(1,2,4);
+            List<Integer> input = List.of(1,2,5);
+
+            Hint hint = new Hint(answer,input);
+            assertThat(hint.toString()).isEqualTo("2스트라이크");
+        }
+        @Test
+        void 스트라이크_1번() {
+            List<Integer> answer = List.of(1,2,7);
+            List<Integer> input = List.of(1,4,5);
+
+            Hint hint = new Hint(answer,input);
+            assertThat(hint.toString()).isEqualTo("1스트라이크");
+        }
+        @Test
+        void 볼_3번() {
+            List<Integer> answer = List.of(1,2,3);
+            List<Integer> input = List.of(2,3,1);
+
+            Hint hint = new Hint(answer,input);
+            assertThat(hint.toString()).isEqualTo("3볼");
+        }
+        @Test
+        void 볼_2번() {
+            List<Integer> answer = List.of(1,2,3);
+            List<Integer> input = List.of(2,6,1);
+
+            Hint hint = new Hint(answer,input);
+            assertThat(hint.toString()).isEqualTo("2볼");
+        }
+        @Test
+        void 볼_1번() {
+            List<Integer> answer = List.of(1,2,3);
+            List<Integer> input = List.of(2,5,6);
+
+            Hint hint = new Hint(answer,input);
+            assertThat(hint.toString()).isEqualTo("1볼");
+        }
+        @Test
+        void 스트라이크_2번_볼_1번() {
+            List<Integer> answer = List.of(1,2,3);
+            List<Integer> input = List.of(1,2,1);
+
+            Hint hint = new Hint(answer,input);
+            assertThat(hint.toString()).isEqualTo("1볼 2스트라이크");
+        }
+        @Test
+        void 스트라이크_1번_볼_2번() {
+            List<Integer> answer = List.of(1,2,3);
+            List<Integer> input = List.of(1,3,2);
+
+            Hint hint = new Hint(answer,input);
+            assertThat(hint.toString()).isEqualTo("2볼 1스트라이크");
+        }
+        @Test
+        void 낫싱() {
+            List<Integer> answer = List.of(1,2,3);
+            List<Integer> input = List.of(4,5,6);
+
+            Hint hint = new Hint(answer,input);
+            assertThat(hint.toString()).isEqualTo("낫싱");
+        }
+    }
+
 }
