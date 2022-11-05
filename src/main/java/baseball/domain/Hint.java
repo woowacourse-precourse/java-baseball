1f82dce55d057784808f8f9c0bd061490172dd19
lee <higgs95@naver.com>
2022-11-05 18:51:52 +0900

    feat(Game): 힌트 도메인으로 따로 분리 볼 스트라이크 갯수 판단 로직 포함

---
+++ src/main/java/baseball/domain/Hint.java
@@ -0,0 +1,36 @@
+package baseball.domain;
+
+import static baseball.type.NumberType.NUMBER_SIZE;
+
+import java.util.List;
+
+public class Hint {
+    private int strike;
+    private int ball;
+
+    public Hint(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public void countHint(List<Integer> userNumbers, List<Integer> computerNumbers) {
+        for (int index = 0; index < NUMBER_SIZE; index++) {
+            if (userNumbers.get(index) == computerNumbers.get(index)) {
+                strike++;
+                continue;
+            }
+
+            if (computerNumbers.contains(userNumbers.get(index))) {
+                ball++;
+            }
+        }
+    }
+}
