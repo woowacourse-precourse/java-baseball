43e1ffb144bcf67dd0d018866ded5534abc4c607
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-05 12:23:22 +0900

    test(GameController): Game객체와 DecisionMaker의 값에 따른 GameController 작동 검사

--- src/test/java/testcase/GameControllerTest.java
+++ src/test/java/testcase/GameControllerTest.java
@@ -1,2 +1,62 @@
-package testcase;public class GameControllerTest {
+package testcase;
+
+import controller.GameController;
+import decision.DecisionMaker;
+import game.Game;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+public class GameControllerTest {
+    @Mock
+    DecisionMaker decisionMaker;
+
+    @Mock
+    Game game;
+
+    @InjectMocks
+    GameController gameController;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @DisplayName("야구 게임을 3번 반복한다")
+    @Test
+    void 야구게임_3번_반복() {
+        when(decisionMaker.getDecision())
+                .thenReturn("Play")
+                .thenReturn("Play")
+                .thenReturn("Stop");
+        gameController.playRecurringGame();
+
+        assertThat(gameController.getTimesOfPlayGame()).isEqualTo(3);
+    }
+
+    @DisplayName("Game 객체에서 에러 발생 가정")
+    @Test
+    void Game_객체에서_발생한_에러는_GameController의_에러로_전파된다() {
+        when(game.play())
+                .thenThrow(new IllegalArgumentException("잘못된 인자 값 입력입니다."));
+
+        assertThatThrownBy(() -> gameController.playRecurringGame())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("DecisionMaker 객체에서 에러 발생 가정")
+    @Test
+    void DecisionMaker_객체에서_발생한_에러는_GameController의_에러로_전파된다() {
+        when(decisionMaker.getDecision())
+                .thenThrow(new IllegalArgumentException("잘못된 인자 값 입력입니다."));
+
+        assertThatThrownBy(() -> gameController.playRecurringGame())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
