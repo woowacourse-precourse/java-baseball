c1e10f059b8e2b8e048ea5c1f3efc0722a1d362e
Yeongseon Park <yseon406@gmail.com>
2022-11-06 05:29:43 +0900

    feat: Application() 게임 프로세스 구현
    Application() 클래스에 게임 프로세스 초기 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame();
+
+        baseballGame.startGame();
+        List<Integer> computerNumber = baseballGame.createComputerNumber();
+
+        while (true) {
+            List<Integer> userNumber = baseballGame.getUserNumber();
+            int strike = baseballGame.checkStrike(computerNumber, userNumber);
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+            int ball = baseballGame.checkBall(computerNumber, userNumber);
+            String result = baseballGame.getResult(strike, ball);
+            System.out.println(result);
+        }
     }
 }
