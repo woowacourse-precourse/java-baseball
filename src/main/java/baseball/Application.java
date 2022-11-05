f8d3f64046d94457df482bb27b32cd28d3cf46ae
khj <bbb6254177@gmail.com>
2022-11-05 20:14:31 +0900

    feat: add function to process exception

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -46,6 +45,17 @@ public class Application {
         return false;
     }
 
+    private static boolean process_exception(List<Integer> input_num) {
+        try {
+            if (is_execption(input_num)) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
@@ -55,5 +65,8 @@ public class Application {
         //2. 사용자로부터 세 자리수를 입력받는다.
         System.out.print("숫자를 입력해주세요 : ");
         List<Integer> input_num = rand_num_input();
+
+        //3. 사용자로부터 입력 받은 수가 잘못된 값인 경우 프로그램을 종료한다.
+        if (process_exception(input_num)) {}
     }
 }
