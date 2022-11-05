cf81a9dec1414c0212773c05ad16a8bf9bbbdee7
Cyma-s <ssol0319@naver.com>
2022-11-06 00:14:54 +0900

    feat(Computer): add method that insert random numbers in List<Integer>
    isUniqueNumber(): return false when random number list contains a given random number.
    addRandomNumber(): add number in list when number is unique.

--- src/main/java/baseball/model/player/Computer.java
+++ src/main/java/baseball/model/player/Computer.java
@@ -2,6 +2,7 @@ package baseball.model.player;
 
 import baseball.model.number.BaseballNumbers;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
 
 public class Computer {
     private static final int START_RANDOM_NUMBER_RANGE = 1;
@@ -13,5 +14,18 @@ public class Computer {
     private Integer createRandomNumber() {
         int randomNumber = Randoms.pickNumberInRange(START_RANDOM_NUMBER_RANGE, END_RANDOM_NUMBER_RANGE);
         return randomNumber;
+    }
+
+    private void addRandomNumber(List<Integer> randomNumbers, Integer uncheckedNumber) {
+        if(isUniqueNumber(randomNumbers, uncheckedNumber)) {
+            randomNumbers.add(uncheckedNumber);
+        }
+    }
+
+    private boolean isUniqueNumber(List<Integer> randomNumbers, Integer uncheckedNumber) {
+        if(randomNumbers.contains(uncheckedNumber)) {
+            return false;
+        }
+        return true;
     }
 }
