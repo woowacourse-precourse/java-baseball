aa055585d8a1a714ad1442a7040157e4c9aeb838
davejang <davejang98@naver.com>
2022-11-06 05:03:54 +0900

    feat: game process
    숫자야구 진행 메서드

--- src/main/java/baseball/controller/GameSystem.java
+++ src/main/java/baseball/controller/GameSystem.java
@@ -4,25 +4,48 @@ import baseball.model.GameAnswer;
 import baseball.model.MyAnswer;
 import baseball.view.UserInterface;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameSystem {
     public UserInterface userInterface;
     public GameAnswer gameAnswer;
     public MyAnswer myAnswer;
+    private int strikeCount;
+    private int ballCount;
 
     public GameSystem() {
         gameAnswer = new GameAnswer();
-        System.out.println(gameAnswer.getNumber());
-
+        myAnswer = new MyAnswer();
         userInterface = new UserInterface();
 
-        setUserAnswer();
-        System.out.println(myAnswer.getAnswer());
+        gameProcess();
+    }
+    public void gameProcess() {
+        boolean correctAnswer;
+        while(true) {
+            initCount();
+            setUserAnswer();
+            correctAnswer = checkStrikeBall();
+            if(correctAnswer) break;
+        }
     }
 
     public void setUserAnswer() {
         int number;
         number = userInterface.inputNumber();
-        myAnswer = new MyAnswer(number);
+        myAnswer.setAnswer(number);
+    }
+
+    public boolean checkStrikeBall() {
+        for(int i = 0; i < 3; i++) {
+
+        }
+    }
+
+    public void initCount() {
+        strikeCount = 0;
+        ballCount = 0;
     }
 
 }
