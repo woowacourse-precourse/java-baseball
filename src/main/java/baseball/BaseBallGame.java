4a135c1ebaa86b90cde0e2b75f17cb8a430c26e3
cmong0516 <cmong0516@gmail.com>
2022-11-05 15:42:51 +0900

    feat(BaseBallGame Class): makeHint 메서드 작성
    유저의 입력값과 컴퓨터의 랜덤값을 비교하여 ball 과 strike 값을 출력하는 메서드

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -38,4 +38,20 @@ public class BaseBallGame {
         return strike;
     }
 
+    public void makeHint(int ballCount, int strikeCount) {
+
+        if (ballCount > 0 && strikeCount > 0) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+        if (ballCount > 0 && strikeCount == 0) {
+            System.out.println(ballCount + "볼");
+        }
+        if (strikeCount > 0 && ballCount == 0) {
+            System.out.println(strikeCount + "스트라이크");
+        }
+        if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("낫싱");
+        }
+    }
+
 }
