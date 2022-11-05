903c27c11e956ff666b345aef5725e56201962bd
limecats <limecats0331@gmail.com>
2022-11-06 02:53:51 +0900

    refactor(Judge): change JudgeTest class
    change test to nested test

--- src/test/java/baseball/JudgeTest.java
+++ src/test/java/baseball/JudgeTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
@@ -10,94 +11,102 @@ import static org.assertj.core.api.Assertions.*;
 
 class JudgeTest {
 
-    @Test
-    @DisplayName("볼이 없을 때")
-    void countZeroBallTest() throws Exception {
-        //Given
-        List<Integer> answer = List.of(1, 2, 3);
-        List<Integer> guess = List.of(4, 5, 6);
+    @Nested
+    @DisplayName("볼의 갯수를 찾는 메소드")
+    class countBall {
+        @Test
+        @DisplayName("볼이 없을 때")
+        void countZeroBallTest() throws Exception {
+            //Given
+            List<Integer> answer = List.of(1, 2, 3);
+            List<Integer> guess = List.of(4, 5, 6);
 
-        Judge judge = new Judge(answer);
+            Judge judge = new Judge(answer);
 
-        Method countBall = Judge.class.getDeclaredMethod("countBall", List.class);
-        countBall.setAccessible(true);
+            Method countBall = Judge.class.getDeclaredMethod("countBall", List.class);
+            countBall.setAccessible(true);
 
-        //When
-        Integer cnt = (Integer) countBall.invoke(judge, guess);
+            //When
+            Integer cnt = (Integer) countBall.invoke(judge, guess);
 
-        //Then
-        assertThat(cnt).isEqualTo(0);
+            //Then
+            assertThat(cnt).isEqualTo(0);
+        }
+
+        @Test
+        @DisplayName("볼의 2개 일 때")
+        void count2BallTest() throws Exception {
+            List<Integer> answer = List.of(1, 2, 3);
+            List<Integer> guess = List.of(3, 2, 5);
+
+            Judge judge = new Judge(answer);
+
+            Method countBall = Judge.class.getDeclaredMethod("countBall", List.class);
+            countBall.setAccessible(true);
+
+            //When
+            Integer cnt = (Integer) countBall.invoke(judge, guess);
+
+            //Then
+            assertThat(cnt).isEqualTo(2);
+        }
     }
 
-    @Test
-    @DisplayName("볼의 2개 일 때")
-    void count2BallTest() throws Exception {
-        List<Integer> answer = List.of(1, 2, 3);
-        List<Integer> guess = List.of(3, 2, 5);
+    @Nested
+    @DisplayName("스크라이크 갯수를 찾는 메소드")
+    class countStrike {
+        @Test
+        @DisplayName("스트라이크가 없을 때")
+        void countNoStrike() throws Exception {
+            List<Integer> answer = List.of(1, 2, 3);
+            List<Integer> guess = List.of(3, 1, 2);
 
-        Judge judge = new Judge(answer);
+            Judge judge = new Judge(answer);
 
-        Method countBall = Judge.class.getDeclaredMethod("countBall", List.class);
-        countBall.setAccessible(true);
+            Method countStrike = Judge.class.getDeclaredMethod("countStrike", List.class);
+            countStrike.setAccessible(true);
 
-        //When
-        Integer cnt = (Integer) countBall.invoke(judge, guess);
+            //When
+            Integer cnt = (Integer) countStrike.invoke(judge, guess);
 
-        //Then
-        assertThat(cnt).isEqualTo(2);
-    }
+            //Then
+            assertThat(cnt).isEqualTo(0);
+        }
 
-    @Test
-    @DisplayName("스트라이크가 없을 때")
-    void countNoStrike() throws Exception {
-        List<Integer> answer = List.of(1, 2, 3);
-        List<Integer> guess = List.of(3, 1, 2);
+        @Test
+        @DisplayName("스트라이크가 2개일 때")
+        void count2Strike() throws Exception {
+            List<Integer> answer = List.of(1, 2, 3);
+            List<Integer> guess = List.of(6, 2, 3);
 
-        Judge judge = new Judge(answer);
+            Judge judge = new Judge(answer);
 
-        Method countStrike = Judge.class.getDeclaredMethod("countStrike", List.class);
-        countStrike.setAccessible(true);
+            Method countStrike = Judge.class.getDeclaredMethod("countStrike", List.class);
+            countStrike.setAccessible(true);
 
-        //When
-        Integer cnt = (Integer) countStrike.invoke(judge, guess);
+            //When
+            Integer cnt = (Integer) countStrike.invoke(judge, guess);
 
-        //Then
-        assertThat(cnt).isEqualTo(0);
-    }
+            //Then
+            assertThat(cnt).isEqualTo(2);
+        }
 
-    @Test
-    @DisplayName("스트라이크가 2개일 때")
-    void count2Strike() throws Exception {
-        List<Integer> answer = List.of(1, 2, 3);
-        List<Integer> guess = List.of(6, 2, 3);
+        @Test
+        @DisplayName("스트라이크가 3개일 때")
+        void countAllStrike() throws Exception {
+            List<Integer> answer = List.of(1, 2, 3);
+            List<Integer> guess = List.of(1, 2, 3);
 
-        Judge judge = new Judge(answer);
+            Judge judge = new Judge(answer);
 
-        Method countStrike = Judge.class.getDeclaredMethod("countStrike", List.class);
-        countStrike.setAccessible(true);
+            Method countStrike = Judge.class.getDeclaredMethod("countStrike", List.class);
+            countStrike.setAccessible(true);
 
-        //When
-        Integer cnt = (Integer) countStrike.invoke(judge, guess);
+            //When
+            Integer cnt = (Integer) countStrike.invoke(judge, guess);
 
-        //Then
-        assertThat(cnt).isEqualTo(2);
-    }
-
-    @Test
-    @DisplayName("스트라이크가 3개일 때")
-    void countAllStrike() throws Exception {
-        List<Integer> answer = List.of(1, 2, 3);
-        List<Integer> guess = List.of(1, 2, 3);
-
-        Judge judge = new Judge(answer);
-
-        Method countStrike = Judge.class.getDeclaredMethod("countStrike", List.class);
-        countStrike.setAccessible(true);
-
-        //When
-        Integer cnt = (Integer) countStrike.invoke(judge, guess);
-
-        //Then
-        assertThat(cnt).isEqualTo(3);
+            //Then
+            assertThat(cnt).isEqualTo(3);
+        }
     }
 }
