e079876cd2e01871d1f1d4ceabed21e28be1a662
watterricecake <fruturum@nate.com>
2022-11-05 23:44:15 +0900

    feat($Application): add game start
    Added new field to $Option:
    - add scanner to input method
    - add start method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.game.GameController;
+
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner scanner = new Scanner(System.in);
+        GameController.start(scanner);
     }
 }
