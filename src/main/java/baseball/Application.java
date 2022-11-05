58d8cdcf9529cdd860fecccebdf63a89fdcb2f2e
Lee-Jiseung <ljss98@naver.com>
2022-11-05 20:17:54 +0900

    feat: 힌트 출력하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -50,4 +50,29 @@ public class Application {
         }
         return player;
     }
+
+    private static boolean printHint(List<Integer> computer, List<Integer> player) {
+        int strike = 0, ball = 0;
+        for (int i = 0; i < 3; i++) {
+            if (computer.get(i).equals(player.get(i))) {
+                strike++;
+            } else if (computer.contains(player.get(i))) {
+                ball++;
+            }
+        }
+
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        } else if (ball == 0 && strike == 3) {
+            System.out.println("3스트라이크");
+            return true;
+        } else if (ball == 0) {
+            System.out.printf("%d스트라이크%n", strike);
+        } else if (strike == 0) {
+            System.out.printf("%d볼%n", ball);
+        } else {
+            System.out.printf("%d볼 %d스트라이크%n", ball, strike);
+        }
+        return false;
+    }
 }
