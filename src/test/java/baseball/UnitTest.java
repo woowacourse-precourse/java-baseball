3cdb83b8bb92a49f47a48ea50abf23ed7ac9249a
DongWooE <ehddn977@naver.com>
2022-11-06 00:40:58 +0900

    [feat] printStartGame 추가

---
+++ src/test/java/baseball/UnitTest.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+class UnitTest {
+
+    @Test
+    public void printStartGame_정상_테스트(){
+        //given
+        Application application = new Application();
+
+        //when
+        application.printStartGame();
+
+        //then
+    }
+}
