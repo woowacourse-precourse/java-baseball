c519656813daa3d213e4070ca4257b650d0b4060
HiiWee <wpdlzhf159@gmail.com>
2022-11-06 00:08:15 +0900

    Feat: ComputerNumber 도메인 추가

---
+++ src/main/java/baseball/domain/ComputerNumber.java
@@ -0,0 +1,26 @@
+package baseball.domain;
+
+public class ComputerNumber {
+    private int firstNumber;
+    private int secondNumber;
+    private int thirdNumber;
+
+
+    public ComputerNumber(int firstNumber, int secondNumber, int thirdNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+        this.thirdNumber = thirdNumber;
+    }
+
+    public int getFirstNumber() {
+        return firstNumber;
+    }
+
+    public int getSecondNumber() {
+        return secondNumber;
+    }
+
+    public int getThirdNumber() {
+        return thirdNumber;
+    }
+}
