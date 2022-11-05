4828dc7d6e787e30b17b4f6099ef7997cafc338f
yu-heejin <jkgt0521@naver.com>
2022-11-06 01:39:49 +0900

    feat : 해당 숫자 포함 여부 확인 및 게임 계속 여부 입력 기능 구현 내가 입력한 숫자 자리수를 하나하나 분리하여 랜덤 숫자안에 포함되었는지 확인하는 기능을 구현했다. 또한, 숫자를 모두 맞춘 경우 게임을 계속할지 말지에 대해 입력받는 기능도 구현했다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,28 +13,34 @@ public class Application {
         int ballCount = 0;
         String randomNumber = String.valueOf(pickNumberInRange(100, 999));
 
-        System.out.println(randomNumber);
         System.out.println("숫자 야구 게임을 시작합니다.");
-        // 숫자 입력 및 랜덤 수 생성
-        System.out.print("숫자를 입력해주세요 : ");
-        String number = readLine();
+        while (goGame == 1) {
+            System.out.println(randomNumber);
+            // feat: 숫자 입력 및 랜덤 수 생성
+            System.out.print("숫자를 입력해주세요 : ");
+            String number = readLine();
 
-        for (int i = 0; i < MAX_NUMBER_SIZE; i++) {
-            char dividedNumber = number.charAt(i);
-            // dividedNumber의 자리수는 따로 구할 필요 없이 i를 사용한다.
-            if (randomNumber.indexOf(dividedNumber) == i) {
-                strikeCount++;
-            } else if (randomNumber.indexOf(dividedNumber) != -1) {
-                ballCount++;
+            for (int i = 0; i < MAX_NUMBER_SIZE; i++) {
+                char dividedNumber = number.charAt(i);
+                // dividedNumber의 자리수는 따로 구할 필요 없이 i를 사용한다.
+                if (randomNumber.indexOf(dividedNumber) == i) {
+                    strikeCount++;
+                } else if (randomNumber.indexOf(dividedNumber) != -1) {
+                    ballCount++;
+                }
             }
-        }
 
-        if (strikeCount == 3) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            goGame = 2;
-        }
+            if (strikeCount == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                goGame = Integer.parseInt(readLine());
+                randomNumber = String.valueOf(pickNumberInRange(100, 999));
+            } else {
+                System.out.println(strikeCount + "스트라이크 " + ballCount + "볼");
+            }
 
-        System.out.println(strikeCount + "스트라이크 " + ballCount + "볼");
+            strikeCount = 0;
+            ballCount = 0;
+        }
     }
 }
