181ded017a5bcae47ba9b47f6a24b9edca287742
prontand <keaseon@gmail.com>
2022-11-05 18:42:06 +0900

    Add : function 1 By prontand

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,18 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //1. 게임 시작 문구 <출력>
+        //2. 컴퓨터의 임의의 수 3자리 선택
+        //3. 숫자 3자리 입력 받기
+        //4. 입력한 수에 대한 결과 표시 출력
+        //5. 3스트라이크면 게임 종료 1(리셋)또는 2 입력
+        //6. 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println("숫자를 입력해주세요 : ");
+
+
+
+
     }
 }
