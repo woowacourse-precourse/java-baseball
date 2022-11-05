2aa73b132156bfc2742879093974362b4b0df882
Invidam <hansupark0208@gmail.com>
2022-11-05 18:22:28 +0900

    refactor(User): 정수 입력 메서드명 변경

--- src/main/java/baseball/io/InputControl.java
+++ src/main/java/baseball/io/InputControl.java
@@ -6,7 +6,7 @@ public class InputControl {
     private static String readLine() {
         return Console.readLine();
     }
-    public static int inputInt() {
+    public static int readInt() {
         return strToInt(readLine());
     }
 
