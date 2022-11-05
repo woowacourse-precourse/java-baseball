9a4afd144c228a926bc27976dffbe845ac1fdc7e
wonjunYou <youwjune@gmail.com>
2022-11-05 16:43:24 +0900

    feat : 받은 입력값을 검증하고 처리하는 InputFilter 구현

---
+++ src/main/java/baseball/input/InputFilter.java
@@ -0,0 +1,46 @@
+package baseball.input;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class InputFilter {
+
+    public static final int INPUT_LENGTH = 3;
+    private static final String SPLIT_DELIMETER = "";
+    private static final InputReader reader = new InputReader();
+
+    public static String readLine() {
+        String input = reader.read();
+
+        return Optional.ofNullable(input)
+                .orElseThrow(() -> new IllegalArgumentException("null값이 입력되었습니다."));
+    }
+
+    public static String[] splitToLetters(String input) {
+        return input.split(SPLIT_DELIMETER);
+    }
+
+    public static List<Integer> convertToNumbers(String[] letters) {
+        List<Integer> numbers = Arrays.stream(letters)
+                .filter(InputFilter::isNumberNotZero)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        checkLength(numbers);
+
+        return numbers;
+    }
+
+    private static boolean isNumberNotZero(String letter) {
+        return letter.matches("^[1-9]+$");
+    }
+
+    private static void checkLength(List<Integer> numbers) {
+        if (numbers.size() != INPUT_LENGTH) {
+            throw new IllegalArgumentException("잘못된 길이의 입력값입니다.");
+        }
+    }
+
+}
