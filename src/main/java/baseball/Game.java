6a175d735cbaeeb1d6f14648c47a5ffd3629670d
Hayoon <hy97@sju.ac.kr>
2022-11-05 20:44:38 +0900

    refactor: 메서드(stringToListInteger) 이중변환 없이 String -> List<Integer> 자바 Stream 단일변환 리팩토링

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,10 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Game {
@@ -17,6 +14,7 @@ public class Game {
         computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
+
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
@@ -42,7 +40,6 @@ public class Game {
         for (int i = 0; i < 3; i++) {
             if (computer.contains(userAnswer.get(i))
                     && (userAnswer.get(i).equals(computer.get(i)))) {
-                System.out.println("스트라이크 " + userAnswer.get(i) + " " + computer.get(i));
                 strikeAndBallCount.put("Strike", strikeAndBallCount.get("Strike") + 1);
             } else if (computer.contains(userAnswer.get(i))
                     && (!userAnswer.get(i).equals(computer.get(i)))) {
@@ -57,6 +54,7 @@ public class Game {
         incorrectUserAnswerDigit(userAnswer);
         incorrectUserAnswerDuplicate(userAnswer);
     }
+
     private void incorrectUserAnswerDigit(String userAnswer) {
         List<Integer> answerList = stringToListInteger(userAnswer);
         if (answerList.size() != 3) {
@@ -79,15 +77,24 @@ public class Game {
         }
     }
 
-    private List<Integer> stringToListInteger(String userAnswer) {
-        return listToListInteger(List.of(userAnswer.split("")));
-    }
-
-    private List<Integer> listToListInteger(List<String> answerList) {
-        return answerList.stream()
+    public List<Integer> stringToListInteger(String userAnswer) {
+        List<Integer> x = Arrays.stream(userAnswer.split(""))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+        System.out.println(x);
+        return x;
     }
+
+//    private List<Integer> stringToListInteger(String userAnswer) {
+//        System.out.println(listToListInteger(List.of(userAnswer.split(""))));
+//        return listToListInteger(List.of(userAnswer.split("")));
+//    }
+//
+//    private List<Integer> listToListInteger(List<String> answerList) {
+//        return answerList.stream()
+//                .map(Integer::parseInt)
+//                .collect(Collectors.toList());
+//    }
 
     private boolean printResultCompareWithAnswer(HashMap<String, Integer> strikeAndBallCount) {
         Integer strike = strikeAndBallCount.get("Strike");
@@ -103,7 +110,6 @@ public class Game {
         } else if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
         } else if (strike == 3) {
-//            System.out.println(strikeAndBallCount);
             System.out.println("3스트라이크" + "\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return true;
         }
@@ -116,15 +122,3 @@ public class Game {
         return Console.readLine();
     }
 }
-
-/** 숫자를 입력해주세요 : 123
-        * 1볼 1스트라이크
-        * 숫자를 입력해주세요 : 145
-        * 1볼
-        * 숫자를 입력해주세요 : 671
-        * 2볼
-        * 숫자를 입력해주세요 : 216
-        * 1스트라이크
-        * 숫자를 입력해주세요 : 713
-        * 3스트라이크
- */
