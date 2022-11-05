5f7247447081c9d8b36e32866659d0aeab1c44a6
jjdone <ehdlfaos@gmail.com>
2022-11-05 13:01:40 +0900

    GameControl 클래스 상세구현1
    startGame: 게임 진행 중인 부분, startMessage, scoreCalculation: strike와 ball 개수 구하기, printScoreMessage: ball, strike, nothing 출력, resetScore 구현

--- src/main/java/baseball/GameControl.java
+++ src/main/java/baseball/GameControl.java
@@ -3,20 +3,70 @@ package baseball;
 import java.util.*;
 
 public class GameControl {
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final int NUMBER_SIZE = 3;
+    private static final String Ball_MESSAGE = "볼";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final boolean PLAYING = true;
+    private static final boolean GAMEOVER = false;
 
     private List<Integer> computer;
     private List<Integer> player;
 
+    private int strike = 0;
+    private int ball = 0;
+    private int strikePlusBall = 0;
+    private boolean isGameOver = PLAYING;
+
     public GameControl() {}
 
     public void startGame() {
+        startMessage();
         Computer computerNumber = new Computer();
         Player playerNumber = new Player();
 
         computerNumber.setNumbers();
-        this.computer = computerNumber.getNumbers();
+        computer = computerNumber.getNumbers();
 
-        playerNumber.setNumbers();
-        this.player = playerNumber.getNumbers();
+        while(isGameOver) {
+            playerNumber.setNumbers();
+            player = playerNumber.getNumbers();
+            scoreCalculation(player);
+        }
+    }
+
+    public void startMessage() {
+        System.out.println(START_MESSAGE);
+    }
+
+    public void scoreCalculation(List<Integer> player) {
+        for(int i=0; i<NUMBER_SIZE; i++){
+            if(player.get(i) == computer.get(i)){
+                strike++;
+            }
+            if(computer.contains(player.get(i))){
+                strikePlusBall++;
+            }
+        ball = strikePlusBall - strike;
+        }
+    }
+
+    public void printScoreMessage() {
+        if(ball != 0){
+            System.out.print(ball + Ball_MESSAGE + " ");
+        }
+        if(strike != 0){
+            System.out.println(strike + STRIKE_MESSAGE);
+        }
+        if(ball == 0 && strike == 0){
+            System.out.println(NOTHING);
+        }
+    }
+
+    public void resetScore() {
+        strike = 0;
+        ball = 0;
+        strikePlusBall = 0;
     }
 }
