6a018e876784464910b19a0f72128a1a2e03fbaa
ineiw <75738349+ineiw@users.noreply.github.com>
2022-11-05 13:14:43 +0900

    test: 리스트 원소 중복 테스트 추가
    리스트의 원소가 중복되는지 테스트

--- src/test/java/CustumTest.java
+++ src/test/java/CustumTest.java
@@ -1,6 +1,7 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
 import baseball.Computer;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class CustumTest {
@@ -8,6 +9,12 @@ public class CustumTest {
     void 랜덤으로_값_가져오기() {
         Computer computer = new Computer();
 
-        assertThat(computer.getRandomNumber().size()).isEqualTo(3);
+        List<Integer> computerRandomNumber = computer.getRandomNumber();
+
+        assertThat(computerRandomNumber.size()).isEqualTo(3);
+
+        assertThat(computerRandomNumber).containsOnlyOnce(computerRandomNumber.get(0));
+        assertThat(computerRandomNumber).containsOnlyOnce(computerRandomNumber.get(1));
+        assertThat(computerRandomNumber).containsOnlyOnce(computerRandomNumber.get(2));
     }
 }
