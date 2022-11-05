ee56fc10b0ecc85b369f9ed505d2c349397ba4f1
saint6839 <saint6839@gmail.com>
2022-11-06 00:57:45 +0900

    feat(model) : 낫싱, 볼, 스트라이크, 게임종료 여부를 판별하는 메서드 구현
    -hasNothing() : 볼 0, 스트라이크 0 인지
    -hasBall() : 볼이 0이 아닌지
    -hasStrike() : 스트라이크가 0이 아닌지
    -isGameOver : 스트라이크가 3인지

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -19,4 +19,20 @@ public class Result {
     public void increaseStrikeCount() {
         strikeCount++;
     }
+
+    public boolean hasNothing() {
+        return ballCount == 0 && strikeCount == 0;
+    }
+
+    public boolean hasBall() {
+        return ballCount != 0;
+    }
+
+    public boolean hasStrike() {
+        return strikeCount != 0;
+    }
+
+    public boolean isGameOver() {
+        return strikeCount == 3;
+    }
 }
