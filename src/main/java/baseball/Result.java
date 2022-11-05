bce977a30e7ef66b0daf282f9f7afb11524e5631
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-05 14:23:35 +0900

    feat(calculateType): calculate type of result

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import static baseball.ResultType.*;
+
 public class Result {
 
     private ResultType resultType;
@@ -13,5 +15,13 @@ public class Result {
 
     public void increaseBall() {
         ball++;
+    }
+
+    public void calculateType() {
+        if (strike == 3) resultType = EXACT_MATCH;
+        else if (strike > 0 && ball > 0) resultType = BALL_AND_STRIKE;
+        else if (strike > 0) resultType = ONLY_STRIKE;
+        else if (ball > 0) resultType = ONLY_BALL;
+        else resultType = NOTHING;
     }
 }
