ab8681c152b83b8ab3855c72ef8e07a66961e0ef
baekeunsun <eunsun5460@gmail.com>
2022-11-05 19:17:19 +0900

    feat(Application): 사용자 숫자 생성 기능
    - UserGenerate: - "숫자를 입력해주세요 : " 출력, 사용자의 입력을 받아 user리스트 생성
    - StringToList: String을 List<Integer>로 변환
    - CheckInput: input값 예외처리
    - isSize3: 입력값이 세자리 수인지 확인
    - isNotDuplicate: 중복이 없는지 확인
    - isNumber: 입력값에 숫자 외 문자가 없는지 확인
    - isNotContain0: 입력값에 0이 없는지 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,18 @@
 package baseball;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Stream;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+import jdk.jshell.execution.Util;
 
 public class Application {
     private static final int NUM_SIZE = 3;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         List<Integer> computer = ComputerGenerate();
+        List<Integer> user = UserGenerate();
     }
 
     private static List<Integer> ComputerGenerate() {
@@ -26,5 +29,62 @@ public class Application {
             computer.add(randomNumber);
         }
         return computer;
+    }
+
+    private static List<Integer> UserGenerate() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        if (CheckInput(input)) {
+            List<Integer> user = StringToList(input);
+        }
+        throw new IllegalArgumentException("잘못된 입력");
+    }
+
+    private static List<Integer> StringToList(String input) {
+        List<Integer> user = new ArrayList<>();
+        for(int i=0; i<input.length(); i++)
+            user.add(input.charAt(i) - '0');
+        return user;
+    }
+
+    private static boolean CheckInput(String input) {
+        if (isSize3(input) && isNotDuplicate(input) && isNumber(input) && isNotContain0(input)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isSize3(String input) {
+        if (input.length() == 3){
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isNotDuplicate(String input) {
+        String checkinput = "";
+        for (char charinput : input.toCharArray()) {
+            if (checkinput.contains(String.valueOf(charinput))) {
+                return false;
+            }
+            checkinput += String.valueOf(charinput);
+        }
+        return true;
+    }
+
+    private static boolean isNumber(String input) {
+        for (char charinput : input.toCharArray()) {
+            if (Character.isDigit(charinput) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean isNotContain0(String input) {
+        if (input.contains("0")) {
+            return false;
+        }
+        return true;
     }
 }
