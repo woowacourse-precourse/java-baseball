3232f4fd87222e0c2e4054e48fbfd532c47caec9
YE-EUN <musiclove529@naver.com>
2022-11-05 11:04:21 +0900

    feat : Result 클래스 생성
    - 사용자가 입력한 수에 따른 결과를 관리하는 클래스 생성

---
+++ src/main/java/baseball/Result.java
@@ -0,0 +1,27 @@
+package baseball;
+
+public class Result {
+    private int ballCount;
+    private int strikeCount;
+
+    public void initialize(){
+
+    }
+    public void increaseBallCount(){
+
+    }
+    public void increaseStrikeCount(){
+
+    }
+    public void printResultMessage(){
+
+    }
+    public String createResultMessage(){
+        String resultMessage = "";
+
+        return resultMessage;
+    }
+    public boolean isWrongAnswer(){
+        return false;
+    }
+}
