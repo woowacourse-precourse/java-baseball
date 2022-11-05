806c61a1a9b16c58422f1265834fdf81750a5ea5
tdmdgh <tdmdgh@gmail.com>
2022-11-05 17:21:13 +0800

    feat: add input exception case - range of input
    정규 표현식을 이용해 Input이 숫자가 아닌 모든 경우와 Input의 범위가 1~9사이가 아닌 경우를 동시에 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,8 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    private static boolean isString_in_1to9(String str){
+        return str.matches("[1-9.]+");
+    }
     public static boolean isValidInput(String input){
-
+        //정규 표현식을 이용해 Input이 숫자가 아닌 모든 경우와 Input의 범위가 1~9사이가 아닌 경우를 동시에 처리
+        if(!isString_in_1to9(input)) return false;
         return true;
     }
     public static List<Integer> createTargetNums(){
