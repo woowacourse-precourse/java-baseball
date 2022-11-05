8d1f162fe59a2c1d8cbbd55a9b126ed0598ce696
정승구 <jeongseung-gu@jeongseung-guui-MacBookAir.local>
2022-11-05 22:21:19 +0900

    feat : 체크 클래스및 내부 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,17 +4,70 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 
+class error_check
+{
+    public boolean check_input_lenght(String target)
+    {
+        if (target.length() != 3)
+        {
+            return false;
+        }
+        return true;
+    }
+    public boolean check_isnum(String target)
+    {
+        for(int i = 0; i < target.length(); i++)
+        {
+            if ((!Character.isDigit(target.charAt(i))))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean check_duplicate(String target)
+    {
+        int[] check_array = new int[10];
+        int now_value = 0;
+        for (int i = 0; i < target.length(); i++)
+        {
+            now_value = Character.getNumericValue(target.charAt(i));
+            check_array[now_value] += 1;
+            if (check_array[now_value] > 1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean launch_input_check(String target)
+    {
+        if (!check_input_lenght(target)) {
+            return false;
+        }
+        if (!check_isnum(target)){
+            return false;
+        }
+        if (!check_duplicate(target)){
+            return false;
+        }
+        return true;
+    }
+}
 class input{
-    public String input_string;
-    public String input_number()
+    private String input_string;
+    private String input_number()
     {
         String input_string = Console.readLine();
         return input_string;
     }
     public void make_integer_arraylist()
     {
+        error_check check = new error_check();
+
         this.input_string = input_number();
         System.out.println(input_string);
+        System.out.println(check.launch_input_check(input_string));
     }
 }
 public class Application {
