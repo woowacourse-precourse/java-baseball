309b0a52e4f048810f2b9096b290c6ea4c66d2fc
kimJaeHyeon <kmr2644@gmail.com>
2022-11-05 23:39:23 +0900

    feat: ComputerBaseball 생성시 랜덤한 정수 생성 메서드 구현

---
+++ src/main/java/baseball/ComputerBaseball.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerBaseball {
+    private List<Integer> baseballNumberList = new ArrayList<>();
+
+    public ComputerBaseball() {
+        initNumbers();
+    }
+
+    private void initNumbers() {
+        while (baseballNumberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!baseballNumberList.contains(randomNumber)) {
+                baseballNumberList.add(randomNumber);
+            }
+        }
+    }
+    public List<Integer> getBaseballNumberList() {
+        return baseballNumberList;
+    }
+
+}
