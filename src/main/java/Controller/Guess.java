0dd65ad9b4bbc5c4ccd4a3f0fe8e8bd854965705
Devtist <yt930310@snu.ac.kr>
2022-11-05 18:53:53 +0900

    feat: 사용자가 숫자를 입력하면 정답과 비교하여 결과를 출력하는 과정을 하나의 메소드로 구현

---
+++ src/main/java/Controller/Guess.java
@@ -0,0 +1,27 @@
+package Controller;
+
+import static Controller.Converter.*;
+import static Controller.Printer.*;
+import static User.Input.*;
+import static User.Validator.*;
+import static baseball.Constant.*;
+import static computer.BallsAndStrikesCountCalculator.*;
+
+import java.util.List;
+
+public class Guess {
+	public static boolean guess(List<Integer> answer) {
+		printAskingInputMessage();
+
+		String number = getUserInput();
+		validateNumberInput(number);
+		List<Integer> digits = convertStringToIntegerList(number);
+		System.out.println();
+
+		int ballsCount = calculateBallsCount(digits, answer);
+		int strikesCount = calculateStrikesCount(digits, answer);
+		printResult(ballsCount, strikesCount);
+
+		return strikesCount == LENGTH_OF_NUMBER;
+	}
+}
