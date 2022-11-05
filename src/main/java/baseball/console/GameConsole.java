ca50cc1d69db125f18b069ef24e6f4ba1e16d7bc
wlsh44 <k87913j@gmail.com>
2022-11-06 00:43:57 +0900

    refactor: console 내부로 converter 이동

--- src/main/java/baseball/console/GameConsole.java
+++ src/main/java/baseball/console/GameConsole.java
@@ -1,8 +1,30 @@
 package baseball.console;
 
+import baseball.converter.RestartOrExitCodeConverter;
+import baseball.converter.ThreeNumberConverter;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
 public class GameConsole {
+
+    private final ThreeNumberConverter threeNumberConverter;
+    private final RestartOrExitCodeConverter codeConverter;
+
+    public GameConsole() {
+        this.threeNumberConverter = new ThreeNumberConverter();
+        this.codeConverter = new RestartOrExitCodeConverter();
+    }
+
+    public List<Integer> inputThreeNumbers() {
+        String inputString = input();
+        return threeNumberConverter.getThreeNumbers(inputString);
+    }
+
+    public Integer inputCode() {
+        String inputString = input();
+        return codeConverter.getRestartOrExitCode(inputString);
+    }
 
     public String input() {
         return Console.readLine();
