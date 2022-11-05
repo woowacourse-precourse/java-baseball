eac497c27981e14e19bdd95e18dcd137883ab38e
Choi-JJunho <junho5336@gmail.com>
2022-11-05 17:59:46 +0900

    feat(Application): add test logic

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        BullsAndCows bullsAndCows = new BullsAndCows();
+
+        while (true) {
+            String input = Console.readLine();
+            if(bullsAndCows.isEnd()) {
+
+                if(input.equals("1")) {
+                    bullsAndCows.restart();
+                }
+
+                if(input.equals("2")) {
+                    System.out.println("게임 종료");
+                    break;
+                }
+
+            }
+
+            System.out.println(bullsAndCows.getResultOfGuessNumber(input));
+        }
     }
 }
