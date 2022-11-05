730234f7f746d6892b292f711b1baa1160969e5c
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-05 23:32:25 +0900

    test(Computer) : 중복되지 않는 세자리 난수 생성 테스트

--- src/test/java/baseball/GameUtilsTest.java
+++ src/test/java/baseball/GameUtilsTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import baseball.exception.NumberExceptionUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +19,14 @@ public class GameUtilsTest {
         System.setIn(generateUserInput("135"));
         List<Integer> input = GameUtils.getPlayerInputNum();
         assertThat(input).isEqualTo(Arrays.asList(1,3,5));
+    }
+
+    @DisplayName("중복되지 않는 세자리 정답을 랜덤으로 생성할 수 있다.")
+    @Test
+    void 난수_생성() {
+        List<Integer> answer = GameUtils.getComputerAnswerNum();
+        assertThat(answer.size()).isEqualTo(3);
+        assertThat(answer.stream().distinct().count()).isEqualTo(3);
     }
 
     public static InputStream generateUserInput(String input) {
