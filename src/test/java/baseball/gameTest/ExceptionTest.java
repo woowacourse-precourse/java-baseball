8eb2184b549a6feb7a608ed37c2f2caa373a5ec0
watterricecake <fruturum@nate.com>
2022-11-06 00:01:11 +0900

    test($gameTest): test catchSizeException(moreNumberInputExceptionTest/lessNumberInputExceptionTest)
    Added new test to $gameTest:
    - test input more exception
    - test input less exception

---
+++ src/test/java/baseball/gameTest/ExceptionTest.java
@@ -0,0 +1,29 @@
+package baseball.gameTest;
+
+import baseball.Application;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ExceptionTest extends NsTest {
+    @Test
+    void moreNumberInputExceptionTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Test
+    void lessNumberInputExceptionTest() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
