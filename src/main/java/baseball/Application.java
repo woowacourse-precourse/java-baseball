be749844d39a0dd0170dfbf5ee8ffbfd958b0574
=ryucb <=chouck12@naver.com>
2022-11-05 18:27:13 +0900

    random 함수구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static List<Integer>  computer = new ArrayList<>();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
+
+    public static void createRandomNumber(){
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+
+
 }
+
+
