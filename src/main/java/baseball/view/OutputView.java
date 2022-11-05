844832a443fbde58a8fbe66b368041ec9a591d87
hong-sile <gurwns9325@naver.com>
2022-11-05 19:03:37 +0900

    feat(OutputView): 게임의 결과(볼, 스트라이크, 낫싱)를 템플릿에 맞게 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,8 +2,12 @@ package baseball.view;
 
 public interface OutputView {
 	void printGameStartMessage();
+
 	void printInputMessage();
+
 	void printRestartMessage();
+
 	void print3StrikeMessage();
+
 	void printBSOResult(int[] resultBSO);
 }
