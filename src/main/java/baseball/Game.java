f70ab37fdfa7530f06fe6e77c38fc87adc28ed8d
juhee park <jouhi3322@gmail.com>
2022-11-06 01:35:01 +0900

    feat: 시작문구 출력

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class Game {
+    private static final String START_MSG  = "숫자 야구 게임을 시작합니다.";
+
+    public void start() {
+        System.out.println(START_MSG);
+    }
+
+
+}
