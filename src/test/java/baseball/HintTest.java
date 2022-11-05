29c58e5898268afe135b9aae75c2cac7c28d76eb
YuHayeon <youha0930@gmail.com>
2022-11-05 12:47:39 +0900

    test(Hint): 힌트 메시지 추가 테스트
    모든 경우의 수로 힌트 메시지 테스트

--- src/test/java/baseball/HintTest.java
+++ src/test/java/baseball/HintTest.java
@@ -16,9 +16,21 @@ class HintTest {
         List<Integer> computer = Arrays.asList(3, 2, 1);
         assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("2볼 1스트라이크"));
 
+        player = Arrays.asList(6, 2, 7);
+        computer = Arrays.asList(7, 2, 5);
+        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("1볼 1스트라이크"));
+
+        player = Arrays.asList(7, 2, 5);
+        computer = Arrays.asList(7, 2, 5);
+        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"));
+
         player = Arrays.asList(6, 3, 7);
         computer = Arrays.asList(6, 3, 1);
         assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("2스트라이크"));
+
+        player = Arrays.asList(6, 9, 1);
+        computer = Arrays.asList(6, 3, 4);
+        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("1스트라이크"));
 
         player = Arrays.asList(9, 3, 7);
         computer = Arrays.asList(6, 2, 1);
@@ -28,8 +40,12 @@ class HintTest {
         computer = Arrays.asList(9, 3, 4);
         assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("3볼"));
 
-        player = Arrays.asList(7, 2, 5);
-        computer = Arrays.asList(7, 2, 5);
-        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"));
+        player = Arrays.asList(1, 8, 3);
+        computer = Arrays.asList(8, 7, 1);
+        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("2볼"));
+
+        player = Arrays.asList(6, 9, 2);
+        computer = Arrays.asList(9, 3, 4);
+        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("1볼"));
     }
 }
