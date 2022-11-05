61c93c2d2b712a6e88bb610fb9fb32c24f6d720e
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-06 05:08:10 +0900

    refactor : 예외 검사 기능 수정( 특수 기호, 문자열, 중복, 범위 벗어난 수 입력 추가)

--- src/main/java/extract/Validation.java
+++ src/main/java/extract/Validation.java
@@ -4,35 +4,45 @@ import static java.lang.String.valueOf;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Validation {
     private static boolean exception = false;
 
     //  예외 검사 기능
-    public static boolean validationCheck(int threeDigitsNumber) {
-        String numberToString = valueOf(threeDigitsNumber);
+    public static boolean validationCheck(String userLine) {
 
-        for (int i = 0; i < numberToString.length(); i++) {
-            char checkDuplicateZero = numberToString.charAt(0);
-            char checkDuplicateOne = numberToString.charAt(1);
-            char checkDuplicateTwo = numberToString.charAt(2);
 
-            // 중복 검사
-            if (checkDuplicateZero == checkDuplicateOne
-                    || checkDuplicateOne == checkDuplicateTwo
-                    || checkDuplicateZero == checkDuplicateTwo) {
-                exception = true;
-            }
-        }
+        String onlyNumberCheck = "1234567890";
 
-        // 특수 기호
-        String onlyNumberCheck = "^[[0-9]$@$!%*#?&]*$";
-        Pattern compile = Pattern.compile(onlyNumberCheck);
-        boolean matches = onlyNumberCheck.matches(numberToString);
+        String asterRiskPattern = "^[[0-9]₩~!@#$%^&*()-+=/.,|]*$";
+        String stringPattern = "^[[0-9]abcdefghijklmnopqrstuvwxyz]*$";
+
+        // 특수 기호, 문자열 검사
+        Pattern.compile(asterRiskPattern);
+        Pattern.compile(stringPattern);
+
+        boolean asterRiskCheck = asterRiskPattern.matches(userLine);
+        boolean stringCheck = stringPattern.matches(userLine);
+
+        int inputToInteger = Integer.parseInt(userLine);
 
         // 세자리 범위
-        if (threeDigitsNumber < 100 || threeDigitsNumber > 999 || !matches) {
+        if ( inputToInteger < 100 || inputToInteger > 999
+                || !asterRiskCheck || !stringCheck ) {
+            exception = true;
+        }
+
+        char checkDuplicateZero = userLine.charAt(0);
+        char checkDuplicateOne = userLine.charAt(1);
+        char checkDuplicateTwo = userLine.charAt(2);
+
+        // 중복 검사
+        if (checkDuplicateZero == checkDuplicateOne
+                || checkDuplicateZero == checkDuplicateTwo
+                || checkDuplicateOne == checkDuplicateTwo) {
             exception = true;
         }
 
