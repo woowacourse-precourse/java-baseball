bd7be5c82a66c3562d0ee9d41d0f9497f4a1481b
JaehongDev <println@kakao.com>
2022-11-05 17:11:08 +0900

    test (InitStatus) : 준비 상태 테스트 코드 추가

---
+++ src/test/java/baseball/domain/baseball/status/InitStatusTest.java
@@ -0,0 +1,27 @@
+package baseball.domain.baseball.status;
+
+import baseball.domain.baseball.BaseballGame;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class InitStatusTest {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void 준비상태에서_next함수를_실행시키면_게임시작이_출력됩니다() {
+        new InitStatus().next(new BaseballGame());
+        Assertions.assertThat(outputStreamCaptor.toString().trim()).isEqualTo(InitStatus.GAME_START_MESSAGE);
+    }
+}
