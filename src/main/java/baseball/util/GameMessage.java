8924f2f2178027fbd2bdbee88d22daf2c86c5655
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-05 14:55:48 +0900

    feat: 게임 결과 메세지 추가

--- src/main/java/baseball/util/GameMessage.java
+++ src/main/java/baseball/util/GameMessage.java
@@ -3,4 +3,9 @@ package baseball.util;
 public class GameMessage {
     public static final String START = "숫자 야구 게임을 시작합니다.";
     public static final String USER_INPUT = "숫자를 입력해주세요 : ";
+    public static final String ALL_STRIKE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String CONTINUE_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String NOTHING = "낫싱";
+    public static final String BALL= "볼";
+    public static final String STRIKE= "스트라이크";
 }
