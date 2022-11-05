dff0f6182c46853610302c0671862f671e56ee8e
Devtist <yt930310@snu.ac.kr>
2022-11-05 17:12:40 +0900

    feat: 결과를 사용자에게 안내하는 기능 구현

--- src/main/java/Controller/Printer.java
+++ src/main/java/Controller/Printer.java
@@ -3,6 +3,8 @@ package Controller;
 import static User.Outputs.*;
 
 public class Printer {
+	private static final String SPACE = " ";
+
 	public static void printGameStartMessage() {
 		System.out.println(GAME_START_MESSAGE.get());
 	}
@@ -11,9 +13,27 @@ public class Printer {
 		System.out.print(ASK_INPUT_MESSAGE.get());
 	}
 
+	public static void printResult(int ballsCount, int strikesCount) {
+		if (ballsCount == 0 && strikesCount == 0) {
+			printNothingMessage();
+			return;
+		}
+		if (ballsCount != 0) {
+			printBallsCountMessage(ballsCount);
+			if (strikesCount != 0) {
+				System.out.print(SPACE);
+			}
+		}
+		if (strikesCount != 0) {
+			printStrikesCountMessage(strikesCount);
+		}
+		System.out.println();
+	}
+
 	public static void printStrikesCountMessage(int strikesCount) {
 		System.out.printf(STRIKES_COUNT_MESSAGE.get(), strikesCount);
 	}
+
 	public static void printBallsCountMessage(int ballsCount) {
 		System.out.printf(BALLS_COUNT_MESSAGE.get(), ballsCount);
 	}
