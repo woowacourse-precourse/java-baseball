7172d441bfdc0bc96426ddccd15ab96c3c916709
ashlovesliitea <skyrider1811@gmail.com>
2022-11-05 20:27:14 +0900

    feat: 전체 결과 판정 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,11 +14,52 @@ public class Application {
 
         while (true) {
             List<Integer> computerNumList = generateComputerNum();
-            String userNum = inputUserNum();
-            List<Integer> userNumList = splitUserNumToList(userNum);
+            Boolean win=false;
+
+            while(win==false) {
+                String userNum = inputUserNum();
+                List<Integer> userNumList = splitUserNumToList(userNum);
+                String judgeStr = judge(computerNumList,userNumList);
+
+                if(judgeStr.equals("3스트라이크")){
+                    break;
+                }
+            }
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
         }
     }
+
+    public static String judge(List<Integer> computerNumList, List<Integer> userNumList) {
+        //수만 같으면 ball,자릿수 까지 같으면 strike
+        int ball=judgeStrike(computerNumList,userNumList);
+        int strike=judgeBall(computerNumList,userNumList);
+
+        if(ball!=0 && strike!=0){
+            return ball+"볼 "+strike+" 스트라이크";
+        }
+        if(ball!=0){
+            return ball+"볼";
+
+        }
+        if(strike!=0){
+            return strike+"스트라이크";
+        }
+
+        return "낫싱";
+    }
+
+    private static int judgeBall(List<Integer> computerNumList, List<Integer> userNumList) {
+        int ball=0;
+        return ball;
+    }
+
+    private static int judgeStrike(List<Integer> computerNumList, List<Integer> userNumList) {
+        int strike=0;
+        return strike;
+    }
+
 
     public static List<Integer> splitUserNumToList(String userNum) {
         List<Integer> userNumList = new ArrayList<>();
