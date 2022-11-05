978f9a5172496c29329bbb966e3caa33777849a1
keon0711 <fnc6488@gmail.com>
2022-11-06 01:05:01 +0900

    feat: main function for baseball game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,7 +16,12 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (restart) {
+            List<Integer> computerNumber = generateComputerNum();
+            readAndCompare(computerNumber);
+            checkRestart();
+        }
     }
 
     private static List<Integer> generateComputerNum() {
