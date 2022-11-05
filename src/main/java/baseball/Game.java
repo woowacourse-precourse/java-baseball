803359a9704ea4bc1c4f03082e32e908d5f15146
myeongju00 <j51677767@gmail.com>
2022-11-06 02:54:39 +0900

    feat : 재시작 or 종료 판별 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,17 +1,29 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Pattern;
 
 public class Game {
     private static final int DIGITS = 3;
+    private static final String RESTART = "1";
     private static final String BALL_STR = "볼 ";
     private static final String STRIKE_STR = "스트라이크 ";
     private static final String NOTHING = "낫싱";
     private static final String THREE_STRIKE_SENTENCE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART_OR_EXIT_SENTENCE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    private boolean exitFlag = false;
+
+    private void setRestartOrExit() {
+        String input = inputRestartOrExit();
+        if(input.equals(RESTART)) {
+        //TODO(computer 숫자 재생성)
+            return;
+        }
+        exitFlag = true;
+    }
 
     private String inputRestartOrExit() {
         System.out.println(RESTART_OR_EXIT_SENTENCE);
@@ -27,13 +39,14 @@ public class Game {
         return pattern.matcher(input).matches();
     }
 
-    private void printHint(List<Character> answer, List<Character> input) {
+    private void printHint(List<Integer> answer, List<Integer> input) {
         int strike = countStrike(answer, input);
         int ball = countBall(answer, input);
 
         System.out.println(makeHint(strike, ball));
         if(strike == DIGITS) {
             System.out.println(THREE_STRIKE_SENTENCE);
+            setRestartOrExit();
         }
     }
 
@@ -51,20 +64,20 @@ public class Game {
         return NOTHING;
     }
 
-    private int countStrike(List<Character> answer, List<Character> input) {
+    private int countStrike(List<Integer> answer, List<Integer> input) {
         int count = 0;
         for (int idx = 0; idx < answer.size(); idx++) {
-            if(answer.get(idx) == input.get(idx)) {
+            if(answer.get(idx).equals(input.get(idx))) {
                 count++;
             }
         }
         return count;
     }
 
-    private int countBall(List<Character> answer, List<Character> input) {
+    private int countBall(List<Integer> answer, List<Integer> input) {
         int count = 0;
         for(int idx = 0; idx < answer.size(); idx++) {
-            if(answer.get(idx) != input.get(idx) && input.contains(answer.get(idx))) {
+            if(!answer.get(idx).equals(input.get(idx)) && input.contains(answer.get(idx))) {
                 count++;
             }
         }
