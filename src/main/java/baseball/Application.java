3c55025c52188d229ff7768d91662f5241e5c5ed
Heesom <heesom.hs@gmail.com>
2022-11-05 16:28:36 +0900

    Feat(Application) : setComputer 메소드 추가
    컴퓨터가 서로 다른 임의의 수 3개를 선택해서 리턴하는 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public List<Integer> setComputer() {
+        List<Integer> computer = new ArrayList<>();
+
+        while(computer.size() < 3) {
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if(!computer.contains(randomNum)) {
+                computer.add(randomNum);
+            }
+        }
+
+        return computer;
+    }
+
 }
