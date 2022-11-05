34bfd43eefe2ddd6cb971372ad63a5edcd975ba3
qdeu22 <qdeu22@gmail.com>
2022-11-05 12:13:13 +0900

    feat(BaseballGameController): start함수 생성

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -1,4 +1,13 @@
 package baseball.controller;
 
 public class BaseballGameController {
+    private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+
+    public static void start() {
+        System.out.println(GAME_START);
+        startGame();
+    }
+
+    private static void startGame() {
+    }
 }
