b3a18cdc985e1b11c5337b409faf2fd746a85a27
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-05 23:15:37 +0900

    feat(User): guess 메서드 구현
    콘솔 입력을 받아서 필드 guess 에 저장하는 메서드 guess 구현

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private Numbers guess;
+
+    void guess() {
+        String input = Console.readLine();
+        List<Integer> inputList = new ArrayList<>();
+        for (char c : input.toCharArray()) {
+            inputList.add(c - '0');
+        }
+        guess = new Numbers(inputList);
+        if (!guess.validation()) {
+            throw new IllegalArgumentException("잘못된 입력입니다");
+        }
+    }
+}
