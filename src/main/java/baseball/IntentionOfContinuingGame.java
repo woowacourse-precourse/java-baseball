15135e2b2becb9669f8bb266c512bf2570adfba3
hodomaroo <enghemdwkd@gmail.com>
2022-11-05 17:20:36 +0900

    [Feat] IntentionOfContinuingGame enum 클래스를 Docs 명세에 맞추어 구현, JavaDoc 주석을 추가하였습니다. 추후 기능 메서드 테스트 예정입니다.

--- src/main/java/baseball/IntentionOfContinuingGame.java
+++ src/main/java/baseball/IntentionOfContinuingGame.java
@@ -1,2 +1,38 @@
-package baseball;public enum IntentionOfContinuingGame {
+package baseball;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+/**
+ * 게임 종료 후 선택지 목록 정의한 enum
+ */
+public enum IntentionOfContinuingGame {
+    CONTINUE(1,true),STOP(2,false);
+    private final int intention;
+    private final boolean wantContinue;
+    IntentionOfContinuingGame(int intention, boolean wantContinue){
+        this.intention = intention;
+        this.wantContinue = wantContinue;
+    }
+
+    /**
+     * @param userIntention
+     * @return 1을 입력한 경우 CONTINUE, 2를 입력한 경우 STOP enum 객체를 반환
+     * @throws IllegalArgumentException 유효하지 않은 값이 입력되는 경우 반환
+     */
+    public static IntentionOfContinuingGame getMatchIntention(int userIntention){
+        try {
+            return Arrays.stream(IntentionOfContinuingGame.values())
+                .filter((intention) -> (intention.intention == userIntention))
+                .findAny().get();
+        }catch (Exception e) {
+            throw new IllegalArgumentException("잘못된 선택지를 입력하였습니다.");
+        }
+    }
+
+    public boolean wantContinue(){
+        return this.wantContinue;
+    }
 }
