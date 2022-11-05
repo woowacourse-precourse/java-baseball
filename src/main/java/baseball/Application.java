cd97e4f9b727041c8f627dafaa9e2224a6ede571
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-05 19:02:36 +0900

    feat(Application): 어플리케이션 실행 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.service.BaseballGameServiceImpl;
+import baseball.service.GameService;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+		GameService baseballGameService = new BaseballGameServiceImpl();
+
+		baseballGameService.startGame();
     }
 }
