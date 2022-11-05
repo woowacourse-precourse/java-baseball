cabe405bf5f30b94b58bebe35e33989dbd201a03
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-05 21:49:24 +0900

    feat(Computer): strike 메서드 구현
    사용자의 추측 guess 와 정답을 비교해서 스트라이크 개수를 출력하는 메서드 strike 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.Numbers.LENGTH;
+
 public class Computer {
     private final Numbers answer;
 
@@ -14,12 +16,22 @@ public class Computer {
 
     public static Computer generateAnswer() {
         List<Integer> answer = new ArrayList<>();
-        while (answer.size() < Numbers.LENGTH) {
+        while (answer.size() < LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!answer.contains(randomNumber)) {
                 answer.add(randomNumber);
             }
         }
         return new Computer(new Numbers(answer));
+    }
+
+    public int strike(Numbers guess) {
+        int countStrike = 0;
+        for (int i = 0; i < LENGTH; i++) {
+            if (answer.get(i) == guess.get(i)) {
+                countStrike++;
+            }
+        }
+        return countStrike;
     }
 }
