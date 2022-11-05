89262f72f39b691eaefd394b77364b549006dfec
jaehyuenjung <86586206+jaehyuenjung@users.noreply.github.com>
2022-11-05 22:09:17 +0900

    feat($location): 입력
    플레어어로부터 문자를 입력 받고 반환하는 기능
    - 플레이어 클래스 생성
    - 플레이어로부터 입력을 받고 반환하는 getCommand 메소드 생성

---
+++ src/main/java/baseball/entity/Player.java
@@ -0,0 +1,10 @@
+package baseball.entity;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+    public String getCommand(){
+        String command = Console.readLine();
+        return command;
+    }
+}
