c6a89da436c3c4da03f11137083c2c791e726240
HyeokJoon Kong <orijoon98@gmail.com>
2022-11-05 17:18:58 +0900

    feat: add returning ball and strike counts method

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,6 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Game {
@@ -20,5 +21,19 @@ public class Game {
                 this.answer.add(randomNumber);
             }
         }
+    }
+
+    // 입력 받은 숫자에 대해 정답과 비교해서 첫번째 인덱스에 볼 개수, 두번째 인덱스에 스트라이크 개수를 담은 리스트를 반환
+    public List<Integer> getBallAndStrikeList(List<Integer> answer, List<Integer> number) {
+        int ball = 0;
+        int strike = 0;
+        for (int i = 0; i < 3; i++) {
+            if (answer.get(i).equals(number.get(i))) {
+                strike++;
+            } else if (answer.contains(number.get(i))) {
+                ball++;
+            }
+        }
+        return new ArrayList<>(Arrays.asList(ball, strike));
     }
 }
