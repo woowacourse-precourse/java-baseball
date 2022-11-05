d66b7dbf6808262dd4cb44a8605d4c6c685f4323
parksey <parkseyeon99@naver.com>
2022-11-06 03:20:53 +0900

    feat: 사용자 입력 및 전반적인 구조 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,44 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        PrintResult.printLine(PrintResult.GAME_START);
+        PrintResult.printLineln(PrintResult.GAME_START);
+
+        try {
+            startGame();
+        } catch (IllegalArgumentException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    /**
+     * 게임 시작 함수
+     */
+    public static void startGame() {
+        while (true) {
+            startEachGame();
+
+            String gameStatus = Console.readLine();
+            if ("2".equals(gameStatus)) {
+                break;
+            }
+        }
+    }
+
+    /**
+     * 각 게임 1판 시작 함수
+     */
+    public static void startEachGame() {
+        Baseball baseball = new Baseball();
+
+        while (true) {
+            PrintResult.printLine(PrintResult.INPUT_INFO);
+            String ret = Console.readLine();
+
+            // 예외처리 : 1~9 이외의 값인지 확인
+        }
     }
 }
--- src/main/java/baseball/PrintResult.java
+++ src/main/java/baseball/PrintResult.java
@@ -4,8 +4,14 @@ public class PrintResult {
     public static final String GAME_START = "숫자 야구 게임을 시작합니다.";
     public static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
             "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String INPUT_INFO = "숫자를 입력해주세요 : ";
 
-    public static void printLine(String line) {
+    public static void printLineln(String line) {
         System.out.println(line);
     }
+
+    public static void printLine(String line) {
+        System.out.print(line);
+    }
+
 }
