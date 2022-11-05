731fca77d23984d00ff6a16e465a1b4f6719f8ec
K-Diger <kitten.diger@gmail.com>
2022-11-05 20:47:37 +0900

    refactor : 클라이언트 코드 static 변수 제거 - 기존 static 으로 선언된 BaseballGameStater()를 - main 메서드에서 호출하는 것으로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,30 +1,28 @@
 package baseball;
 
-import baseball.starter.BaseBallGameStarter;
+import baseball.starter.BaseballGameStarter;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
 
 public class Application {
 
-    static BaseBallGameStarter baseBallGameStarter = new BaseBallGameStarter();
-
     public static void main(String[] args) {
-
-        List<List<String>> operatedGame = baseBallGameStarter.operate();
+        BaseballGameStarter baseballGameStarter = new BaseballGameStarter();
+        List<List<String>> operatedGame = baseballGameStarter.operate();
         List<String> userInputValue = operatedGame.get(0);
         List<String> computerGeneratedValue = operatedGame.get(1);
         int option = 0;
 
         while (true) {
-            if (baseBallGameStarter.judgementOperate(userInputValue, computerGeneratedValue)) {
+            if (baseballGameStarter.operateRefereeJudgement(userInputValue, computerGeneratedValue)) {
                 System.out.println("3스트라이크");
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 option = Integer.parseInt(Console.readLine());
             }
             if (option == 1) {
-                operatedGame = baseBallGameStarter.operate();
+                operatedGame = baseballGameStarter.operate();
                 userInputValue = operatedGame.get(0);
                 computerGeneratedValue = operatedGame.get(1);
             } else if (option == 2) {
