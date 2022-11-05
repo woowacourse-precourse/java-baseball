d2c126d246a8db953c9790880a4699c789cd45f0
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-05 15:05:08 +0900

    feat(BallReader): 입력 받은 공들로 게임의 결과를 반환하는 공 판별기 Ball Reader 생성

---
+++ src/main/java/baseball/game/BallReader.java
@@ -0,0 +1,19 @@
+package baseball.game;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BallReader {
+    private static String BALL = "BALL";
+    private static String STRIKE = "STRIKE";
+
+    public boolean isFinished(List<Integer> userBall, List<Integer> computerBall) {
+        return true;
+    }
+
+    public Map<String, Integer> getResult(List<Integer> userBall, List<Integer> computerBall) {
+        Map<String, Integer> result = new HashMap<>();
+        return result;
+    }
+}
