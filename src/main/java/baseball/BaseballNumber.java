a96abcc3dd607d03ac5f93dc6b6d5dc16fcb808f
woosung1223 <dntjd991223@naver.com>
2022-11-05 15:53:36 +0900

    feat(SystemConstant): GAME_DIGIT 상수가 SystemConstant에 위치하도록 변

--- src/main/java/baseball/BaseballNumber.java
+++ src/main/java/baseball/BaseballNumber.java
@@ -7,8 +7,7 @@ import baseball.utils.Util;
 
 public class BaseballNumber {
     private List<Integer> listNumber;
-    private final int GAME_DIGIT = 3;
-    // 나중에 GAME_DIGIT, setRandomValue는 상황 체크 후에 BaseballManager에게 책임 위임
+    // setRandomValue는 상황 체크 후에 BaseballManager에게 책임 위임
     BaseballNumber(int number) {
         this.listNumber = Util.splitDigitsIntoList(number);
     }
@@ -19,7 +18,7 @@ public class BaseballNumber {
 
     public void setRandomValue() {
         this.listNumber = Util.splitDigitsIntoList(
-                RandomNumGenerator.makeRandomNumWithNDigits(GAME_DIGIT)
+                RandomNumGenerator.makeRandomNumWithNDigits(SystemConstant.GAME_DIGIT)
         );
     }
 
---
+++ src/main/java/baseball/SystemConstant.java
@@ -0,0 +1,5 @@
+package baseball;
+
+public final class SystemConstant {
+    public static final int GAME_DIGIT = 3;
+}
