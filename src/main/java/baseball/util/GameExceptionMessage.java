cbbbad4ad90688a0f7efe96f24c08c9af6043898
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-06 00:00:39 +0900

    feat<GameExceptionMessage>: continue or exit 입력값 검증 예외 메세지 추가

--- src/main/java/baseball/util/GameExceptionMessage.java
+++ src/main/java/baseball/util/GameExceptionMessage.java
@@ -3,4 +3,5 @@ package baseball.util;
 public class GameExceptionMessage {
     public static final String USER_NUMBER_LENGTH = "서로 다른 3개의 숫자를 입력해주세요.";
     public static final String USER_NUMBER_RANGE = "1~9 범위의 숫자를 입력해주세요";
+    public static final String CONTINUE_OR_EXIT = "1과 2 둘 중 하나만 입력해주세요.";
 }
