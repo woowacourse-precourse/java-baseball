914227674d7b42e1eefd1b9aabdcad1f02c69b1d
woosung1223 <dntjd991223@naver.com>
2022-11-05 15:25:16 +0900

    feat(RandomNumGenerator): makeRandomNumWithNDigits 메소드 생성

--- src/main/java/baseball/RandomNumGenerator.java
+++ src/main/java/baseball/RandomNumGenerator.java
@@ -3,10 +3,19 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public final class RandomNumGenerator {
-    private final static int START_INCLUSIVE = 1;
-    private final static int END_INCLUSIVE = 9;
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+    private RandomNumGenerator() {
+    }
 
-    public static int makeRandomDigitWithRange() {
+    private static int makeRandomDigit() {
         return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+    }
+    public static int makeRandomNumWithNDigits(int digits) {
+        int result = 0;
+        while (--digits >= 0) {
+            result += makeRandomDigit() * Math.pow(10, digits);
+        }
+        return result;
     }
 }
