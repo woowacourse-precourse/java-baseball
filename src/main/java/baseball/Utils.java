9be1da590436baf1f313d12920053c370689627e
situodys <situodys@gmail.com>
2022-11-05 17:37:10 +0900

    feat(Utils): 결과 문구 출력 구현

--- src/main/java/baseball/Utils.java
+++ src/main/java/baseball/Utils.java
@@ -13,4 +13,8 @@ public class Utils {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
+
+    public static void announce(Result result) {
+        System.out.println(result);
+    }
 }
