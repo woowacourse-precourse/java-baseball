e75d2bbfeaa9463be7e121adbb4bbf4622139030
dydtmd6801 <dydtmd6801@naver.com>
2022-11-05 16:40:42 +0900

    test: 각 자리수가 다른지 확인 테스트

--- src/test/java/study/PrepareGameTest.java
+++ src/test/java/study/PrepareGameTest.java
@@ -16,4 +16,14 @@ public class PrepareGameTest {
         List<Integer> computerNumber = prepareGame.setComputerNumber();
         assertThat(computerNumber.size()).isEqualTo(3);
     }
+
+    @Test
+    void computer_각자리_숫자_다른지_테스트(){
+        List<Integer> computerNumber = prepareGame.setComputerNumber();
+        int state = 0;
+        if(computerNumber.get(0) == computerNumber.get(1) || computerNumber.get(0) == computerNumber.get(2) || computerNumber.get(1) == computerNumber.get(2)){
+            state = 1;
+        }
+        assertThat(state).isEqualTo(0);
+    }
 }
