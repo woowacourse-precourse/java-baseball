1f924a8ed809760eec5df08e3ae8f519260ca86b
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-05 23:46:14 +0900

    feat(Output) : 게임 시작을 출력하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,24 +1,18 @@
 package baseball;
 
 
-import baseball.exception.NumberExceptionUtils;
-import camp.nextstep.edu.missionutils.Console;
+
+
+
+import static baseball.GameUtils.*;
 
 
 public class Application {
+
     public static void main(String[] args) {
-        String playerNum = getPlayerInputNum();
-    }
-
-    public static String getPlayerInputNum() {
-        String input = Console.readLine();
-        checkValidInput(input);
-        return input;
-    }
-
-    private static void checkValidInput(String input) {
-        NumberExceptionUtils.isPositiveDigits(input);
-        NumberExceptionUtils.isThreeDigits(input);
-        NumberExceptionUtils.isNotDuplicateNumber(input);
+        initBaseBallGame();
+        while (true) {
+            playBaseBallGame();
+        }
     }
 }
--- src/main/java/baseball/GameUtils.java
+++ src/main/java/baseball/GameUtils.java
@@ -17,6 +17,19 @@ import static baseball.common.Constant.DIGIT_LENGTH_LIMIT;
 
 public class GameUtils {
 
+    private static List<Integer> answerNum;
+
+    private static List<Integer> playerNum;
+
+    public static void initBaseBallGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        answerNum = getComputerAnswerNum();
+    }
+
+    public static void playBaseBallGame() {
+        playerNum = getPlayerInputNum();
+    }
+
     public static List<Integer> getPlayerInputNum() {
         System.out.println("숫자를 입력해주세요 : ");
         String input = Console.readLine();
