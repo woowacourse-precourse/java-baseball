f5ba842bc19af277cb3710ba6d9496ce7ed4f324
lvalentine6 <ysr1227@naver.com>
2022-11-06 00:33:24 +0900

    feat : 사용자가 잘못된 값을 입력할 경우 예외처리 하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,5 +17,29 @@ public class Application {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.println("숫자를 입력해 주세요 : ");
+        boolean flag;
+        String inputString;
+
+        try {
+            inputString = br.readLine();
+            flag = isValid(inputString);
+
+        } catch (IOException ioException) {
+            throw new IllegalArgumentException(ioException);
+        }
+
+        if(flag) {
+            return inputString;
+        } else {
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    static boolean isValid(String inputString) {
+        if(inputString.length() == 3) {
+            return true;
+        }
+        return false;
     }
 }
