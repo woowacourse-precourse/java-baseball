a2fe99d2c77a74b4e7d18e9e1e9b56c31f87f522
LEE-haeuk <rkwksmswhdk@naver.com>
2022-11-06 00:19:29 +0900

    feat: add the game result

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,12 +14,16 @@ public class Application {
         Application application = new Application();
         List<Integer> computer = new ArrayList<>();
         List<Integer> player = new ArrayList<>();
+        List<Integer> result = new ArrayList<>();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         computer = application.createRandomNumbersInList();
-        System.out.print("숫자를 입력해주세요 : ");
-        player = application.getPlayerNumberInList();
-
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            player = application.getPlayerNumberInList();
+            result = application.computeResult(computer, player);
+            application.printResult(result);
+        } while (result.get(1) < 3);
     }
 
     public List<Integer> createRandomNumbersInList() {
@@ -35,10 +39,39 @@ public class Application {
 
     public List<Integer> getPlayerNumberInList() {
         String playerNumber = Console.readLine();
-        List<Integer> playerNumberList =
-                Arrays.stream(playerNumber.split(""))
-                        .map(Integer::parseInt)
-                        .collect(Collectors.toList());
-        return playerNumberList;
+        return Arrays.stream(playerNumber.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> computeResult(
+            List<Integer> computerNumberList,
+            List<Integer> playerNumberList) {
+        List<Integer> resultList = new ArrayList<>(Arrays.asList(0, 0));
+        for (int i = 0; i < computerNumberList.size(); i++) {
+            int index = playerNumberList.indexOf(computerNumberList.get(i));
+            if (index < 0) {
+                continue;
+            } else if (index != i) {
+                resultList.set(0, resultList.get(0) + 1);
+            } else {
+                resultList.set(1, resultList.get(1) + 1);
+            }
+        }
+        return resultList;
+    }
+
+    public void printResult(List<Integer> resultList) {
+        String gameResult = "";
+        if (resultList.get(0) > 0) {
+            gameResult = gameResult + resultList.get(0).toString() + "볼 ";
+        }
+        if (resultList.get(1) > 0) {
+            gameResult = gameResult + resultList.get(1).toString() + "스트라이크";
+        }
+        if (resultList.get(0) == 0 & resultList.get(1) == 0) {
+            gameResult = gameResult + "낫싱";
+        }
+        System.out.println(gameResult);
     }
 }
