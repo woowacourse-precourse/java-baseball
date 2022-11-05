edbc19e05191c8be3572078f7f5f61d166f002f8
saint6839 <saint6839@gmail.com>
2022-11-06 01:49:38 +0900

    feat(model) : 상태에 대해 유효하지 않은 입력값이 들어왔을때 검증하는 메서드 구현
    - 상수들의 의미가 분명해지도록 변수명과 예외 메시지 내용 수정

--- src/main/java/baseball/utils/Validator.java
+++ src/main/java/baseball/utils/Validator.java
@@ -1,18 +1,19 @@
 package baseball.utils;
 
 import baseball.Ball;
+import baseball.GameStatus;
 
 import java.util.List;
 
 public final class Validator {
     private Validator() {}
-    public static final String INVALID_INPUT_VALUE_MESSAGE = "입력값은 서로 다른 1부터 9사이의 숫자로 이루어져야 합니다.";
-
+    public static final String INVALID_BALL_INPUT_VALUE_MESSAGE = "사용자의 공 입력값은 서로 다른 1부터 9사이의 숫자로 이루어져야 합니다.";
+    public static final String INVALID_GAME_STATUS_INPUT_VALUE_MESSAGE = "게임 상태는 재시작:1, 종료:2 중에서 입력해야 합니다";
     public static void validateParseStringToInt(String playerInput) {
         try {
             Integer.parseInt(playerInput);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(INVALID_INPUT_VALUE_MESSAGE);
+            throw new IllegalArgumentException(INVALID_BALL_INPUT_VALUE_MESSAGE);
         }
     }
 
@@ -20,13 +21,13 @@ public final class Validator {
         if (playerInput.length() == 3) {
             return;
         }
-        throw new IllegalArgumentException(INVALID_INPUT_VALUE_MESSAGE);
+        throw new IllegalArgumentException(INVALID_BALL_INPUT_VALUE_MESSAGE);
     }
 
 
     public static void validateDuplication(List<Ball> playerBalls, Ball ball) {
         if (playerBalls.contains(ball)) {
-            throw new IllegalArgumentException(INVALID_INPUT_VALUE_MESSAGE);
+            throw new IllegalArgumentException(INVALID_BALL_INPUT_VALUE_MESSAGE);
         }
     }
 
@@ -34,6 +35,13 @@ public final class Validator {
         if (ball.isValidRange()) {
             return;
         }
-        throw new IllegalArgumentException(INVALID_INPUT_VALUE_MESSAGE);
+        throw new IllegalArgumentException(INVALID_BALL_INPUT_VALUE_MESSAGE);
+    }
+
+    public static void validateGameStatus(GameStatus gameStatus) {
+        if (gameStatus.isValid()) {
+            return;
+        }
+        throw new IllegalArgumentException(INVALID_GAME_STATUS_INPUT_VALUE_MESSAGE);
     }
 }
