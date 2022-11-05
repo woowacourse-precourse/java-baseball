0af7079ae19eac9460a55f6b8ed33933e5f58032
nakalic <nakalic28@gmail.com>
2022-11-05 20:08:25 +0900

    feat(InputValue): 사용자에게 입력 받는 기능 구현

---
+++ src/main/java/baseball/InputValue.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputValue {
+
+    static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
+
+    public List<Integer> getUserNumber() {
+        List<Integer> userNumber = new ArrayList<>();
+
+        System.out.print(INPUT_MESSAGE);
+        String inputValue = Console.readLine();
+
+        for (int i = 0; i < inputValue.length(); i++) {
+            userNumber.add(Integer.parseInt(inputValue.substring(i, i + 1)));
+        }
+
+        return userNumber;
+    }
+
+}
