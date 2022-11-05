cb03bf22c633e93aa51bb9eecd7f478634de5546
d0ri123 <lsh81550@gmail.com>
2022-11-05 16:06:53 +0900

    Fix: 정답인지 체크하는 기능 구현

--- src/main/java/baseball/Inspector.java
+++ src/main/java/baseball/Inspector.java
@@ -3,9 +3,9 @@ package baseball;
 import java.util.ArrayList;
 
 public class Inspector {
-    int ball=0;
-    int strike=0;
-    String result="";
+    int ball;
+    int strike;
+    String result = "";
 
     public ArrayList<Integer> insert_list(int ans, ArrayList<Integer> list){ //문자를 ArrayList에 넣는 메소드
         String str = String.valueOf(ans);
@@ -19,6 +19,7 @@ public class Inspector {
     public String check(int correct_answer, int answer){ //사용자의 답을 체크하는 기능
         ArrayList<Integer> computer_list = new ArrayList<>();
         ArrayList<Integer> user_list = new ArrayList<>();
+        ball = 0; strike = 0; result = "";
 
         if(correct_answer == answer) {
             result = "3스트라이크";
@@ -28,25 +29,29 @@ public class Inspector {
         insert_list(correct_answer, computer_list);
         insert_list(answer, user_list);
 
-        for(int a:computer_list) {
-            if(user_list.contains(a)){
-                ball++;
-            }else{
-                result = "낫싱";
-                return result;
-            }
+        System.out.println(computer_list.toString());
+        System.out.println(user_list.toString());
 
+        for(int a:computer_list) {
+            System.out.println("비교할 숫자 :" + a);
+            if(user_list.contains(a)) {
+                ball++;
+            }
             if(user_list.indexOf(a) == computer_list.indexOf(a)) {
                 ball--;
                 strike++;
             }
         }
 
-        if(ball>0)
-            result += (ball + "볼 ");
-        if(strike>0)
-            result += (strike + "스트라이크 ");
-        return result;
+        if(ball>0 && strike==0)
+            result = (ball + "볼 ");
+        if(ball==0 && strike>0)
+            result = (strike + "스트라이크 ");
+        if(ball>0 && strike>0)
+            result = (ball + "볼 " + strike + "스트라이크");
+        if(ball==0 && strike==0)
+            result = "낫싱";
 
+        return result;
     }
 }
