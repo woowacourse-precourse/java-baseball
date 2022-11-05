17b9cc2789619a1c8145cdcf2d497bb44563ad79
bnbnac <bnbnac@naver.com>
2022-11-06 00:47:49 +0900

    test: 플레이어 객체가 입력한 숫자에 대한 테스트

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -1,4 +1,42 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
+
 class PlayerTest {
-  
+    @Test
+    public void playerAnswerSizeShouldBeThree() {
+        Player player = new Player();
+        List<String> playerAnswer = List.of("1234".split(""));
+        assertThatThrownBy(() -> player.validate(playerAnswer))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void playerAnswerDigitsShouldBeNumber() {
+        Player player = new Player();
+        List<String> playerAnswer = List.of("abc".split(""));
+        assertThatThrownBy(() -> player.validate(playerAnswer))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void playerAnswerDigitsShouldBeNonZero() {
+        Player player = new Player();
+        List<String> playerAnswer = List.of("012".split(""));
+        assertThatThrownBy(() -> player.validate(playerAnswer))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void playerAnswerDigitsShouldBeDistinct() {
+        Player player = new Player();
+        List<String> playerAnswer = List.of("112".split(""));
+        assertThatThrownBy(() -> player.validate(playerAnswer))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
