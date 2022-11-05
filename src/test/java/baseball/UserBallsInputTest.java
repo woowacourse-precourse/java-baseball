8c38170864906b686bd16a5e4cbfd67999824afe
choiejae <ejae8319@gmail.com>
2022-11-05 19:15:37 +0900

    test: InputNumber validityChecker 1. 유효한 길이 확인

--- src/test/java/baseball/UserBallsInputTest.java
+++ src/test/java/baseball/UserBallsInputTest.java
@@ -11,34 +11,44 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 class UserBallsInputTest {
-  private UserBallsInput input;
-  private static ByteArrayOutputStream outContent;
-  private static PrintStream originalOut;
-  @BeforeEach
-  void setUp() {
-    outContent = new ByteArrayOutputStream();
-    originalOut = System.out;
-    System.setOut(new PrintStream(outContent));
-    input = new UserBallsInput(3);
-  }
 
-  @AfterEach
-  public void restoreStreams() {
-    System.setOut(originalOut);
-  }
+    private UserBallsInput input;
+    private static ByteArrayOutputStream outContent;
+    private static PrintStream originalOut;
 
-  @Test
-  @Order(1)
-  @DisplayName("Input 생성 테스트")
-  void inputSuccess() {
-    input.inputNumberTest("123");
-    assertEquals("123", outContent.toString());
-  }
+    @BeforeEach
+    void setUp() {
+        outContent = new ByteArrayOutputStream();
+        originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+        input = new UserBallsInput(3);
+    }
 
-  @Test
-  @Order(2)
-  @DisplayName("유효성 검사 테스트")
-  void inputnumberValidate() {
-    input.validityChecker();
-  }
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    @Order(1)
+    @DisplayName("Input 생성 테스트")
+    void inputSuccess() {
+        input.inputNumberTest("123");
+        assertEquals("123", outContent.toString());
+    }
+
+    @Test
+    @Order(2)
+    @DisplayName("유효성 검사 테스트")
+    void    inputnumberValidate() {
+        input.validityChecker();
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("유효성 검사 1. 유효한 길이 확인")
+    void    isInputNumberIsValidLength() {
+        input.inputNumberTest("123");
+        assertEquals(input.getBallsLength(), outContent.toString().length());
+    }
 }
