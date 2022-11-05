a8580eda3dc018c88d7547c6805a9fafa59c9f21
dohyun <ldh19738@gmail.com>
2022-11-05 20:20:02 +0900

    feat(BaseballGame): Add strike counting function

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -3,6 +3,18 @@ package baseball;
 import java.util.List;
 
 public class BaseballGame {
+    private  int ballCount;
+    private int strikeCount;
+    private List<Integer> userNumber;
+    private List<Integer> computerNumber;
+
+    public BaseballGame(List<Integer> user, List<Integer> computer) {
+        this.ballCount = 0;
+        this.strikeCount = 0;
+        this.userNumber = user;
+        this.computerNumber = computer;
+    }
+
     public void start() {
         System.out.println(GameMessage.start);
     }
@@ -14,5 +26,18 @@ public class BaseballGame {
         if(!computerNumber.contains(userNumber)) {
             System.out.println(GameMessage.nothing);
         }
+    }
+
+    public boolean isStrike() {
+        boolean isStrike = false;
+
+        for (int strikeCountIndex = 0; strikeCountIndex < computerNumber.size(); strikeCountIndex++) {
+            if (computerNumber.get(strikeCountIndex).equals(userNumber.get(strikeCountIndex))) {
+                strikeCount++;
+                isStrike = true;
+            }
+        }
+
+        return isStrike;
     }
 }
