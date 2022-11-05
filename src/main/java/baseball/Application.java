36966d8863c2635b5f96fd217b5d14f46cd8e6f6
ineiw <75738349+ineiw@users.noreply.github.com>
2022-11-05 17:22:50 +0900

    feat: main 함수에 게임 구현코드 추가
    conputer 의 calchint 과정 추가
    게임을 다시시작 혹은 종료 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,40 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         Computer computer = new Computer();
 
+        List<Integer> endListFormat = Arrays.asList(0, 3);
+
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
+            String gameControlValue = "";
             System.out.print("숫자를 입력해주세요 : ");
+
             String playerInput = Console.readLine();
+
             String computerAnswer = computer.getRandomNumber().toString();
+
+            System.out.println(computerAnswer);
+
+            List<Integer> calcHintResult = computer.calcHint(playerInput, computerAnswer);
+            String stringResult = computer.printHint(calcHintResult);
+            System.out.println(stringResult);
+
+            if (calcHintResult.equals(endListFormat)) {
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                gameControlValue = Console.readLine();
+            }
+
+            if (gameControlValue.equals("1")) {
+                computer = new Computer();
+            } else if (gameControlValue.equals("2")) {
+                break;
+            }
         }
     }
 }
