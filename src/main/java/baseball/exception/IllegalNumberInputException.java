c1239dfe580084fb6d8b8a985b94abd204b28053
wlsh44 <k87913j@gmail.com>
2022-11-05 19:14:58 +0900

    fix: 입력 예외 클래스 생성자 추가

--- src/main/java/baseball/exception/IllegalNumberInputException.java
+++ src/main/java/baseball/exception/IllegalNumberInputException.java
@@ -1,4 +1,8 @@
 package baseball.exception;
 
 public class IllegalNumberInputException extends RuntimeException {
+
+    public IllegalNumberInputException(String str) {
+        super(str);
+    }
 }
