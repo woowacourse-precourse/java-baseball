16659897490119fc7892830a74c9fa197e7df41a
BoyCho <iijokv@naver.com>
2022-11-06 03:25:41 +0900

    fix : 메시지 생성시 잘못된 문자열 생성 수정
    잘못된 변환 방식 수정

--- src/main/java/baseball/Messenger.java
+++ src/main/java/baseball/Messenger.java
@@ -10,13 +10,17 @@ public class Messenger {
 
     public void makeResultMessage (int strikeCnt, int ballCnt) {
         if (ballCnt > 0) {
-            message += (ballCnt - '0') + "볼 ";
+            message += ballCnt + "볼 ";
         }
         if (strikeCnt > 0) {
-            message += (strikeCnt - '0') + "스트라이크";
+            message += strikeCnt + "스트라이크";
         }
         if (message.equals("")) {
             message = "낫싱";
         }
+    }
+
+    public boolean selectRepalyOrEnd (String userOpinion) {
+        return userOpinion.equals("1");
     }
 }
