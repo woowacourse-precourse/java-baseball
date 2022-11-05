9d1390ee779cd9df79c9bf02b3da7a7dce74c1c1
BoyCho <iijokv@naver.com>
2022-11-05 20:01:29 +0900

    feat : 볼/스트라이크 확인 기능 추가

---
+++ src/main/java/baseball/AnswerChecker.java
@@ -0,0 +1,46 @@
+package baseball;
+
+import fixed.FixedList;
+
+public class AnswerChecker {
+    private final int[] answer;
+    private int strikeCnt;
+    private int ballCnt;
+
+    public AnswerChecker(int[] answer) {
+        this.answer = answer;
+        this.strikeCnt = 0;
+        this.ballCnt = 0;
+    }
+
+    public void checkStrikeOrBall(int[] userInput) {
+        for (int i = 0; i < FixedList.LENGTH; i++){
+            checkStrike(answer[i], userInput[i]);
+        }
+    }
+
+    public void checkStrike(int answerNum, int inputNum) {
+        if (answerNum == inputNum) {
+            strikeCnt++;
+            return;
+        }
+        checkBall(inputNum);
+    }
+
+    public void checkBall (int inputNum) {
+        for (int i = 0; i < FixedList.LENGTH; i++) {
+            if (answer[i] == inputNum) {
+                ballCnt++;
+                return;
+            }
+        }
+    }
+
+    public int getBallCnt() {
+        return ballCnt;
+    }
+
+    public int getStrikeCnt() {
+        return strikeCnt;
+    }
+}
