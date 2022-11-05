a1c561a03378d764d94c46374acfe3aaab65b40d
JeonghakLee <wjdgkr0527@naver.com>
2022-11-05 15:06:07 +0900

    feat: 사용자의 재시작, 종료 입력 기능 구현

--- src/main/java/baseball/service/InputService.java
+++ src/main/java/baseball/service/InputService.java
@@ -1,17 +1,35 @@
 package baseball.service;
 
+import static baseball.controller.GameController.BEGIN;
+import static baseball.controller.GameController.END;
+import static baseball.controller.GameController.RANDOM_NUMBER_SIZE;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class InputService {
+
   public List<Integer> getPlayerNumbers() {
+    System.out.print("숫자를 입력해주세요 : ");
     String playerInput = readLine();
+    return convertStringToIntegerList(playerInput);
+  }
+
+  private static List<Integer> convertStringToIntegerList(String playerInput) {
     return Stream.of(playerInput.split(""))
         .mapToInt(Integer::parseInt)
         .boxed()
         .collect(Collectors.toList());
   }
+
+  public boolean DoesPlayerWantToRestart() {
+    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    String playerInput = readLine();
+    return playerInput.equals("1");
+  }
+
 }
