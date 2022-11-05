e6a020cfdfd349a9d9d21f7ee5fbeec7b88f4390
saint6839 <saint6839@gmail.com>
2022-11-06 00:03:53 +0900

    feat(service) : 사용자가 입력한 값을 Ball List로 담아 반환하는 기능 구현

--- src/main/java/baseball/service/PlayerService.java
+++ src/main/java/baseball/service/PlayerService.java
@@ -1,11 +1,34 @@
 package baseball.service;
 
 import baseball.Ball;
+import baseball.utils.MessagePrinter;
+import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static baseball.utils.Validator.*;
 
 public class PlayerService {
     public List<Ball> generatePlayerBalls() {
-        return null;
+        MessagePrinter.showInputNumber();
+        String playerInput = Console.readLine();
+
+        validateInputLength(playerInput);
+        validateParseStringToInt(playerInput);
+
+        return convertInputToBallList(playerInput);
+    }
+
+    private List<Ball> convertInputToBallList(String playerInput) {
+        String[] inputSplits = playerInput.split("");
+
+        List<Ball> playerBalls = new ArrayList<>();
+        for (String inputSplit : inputSplits) {
+            Ball ball = new Ball(Integer.parseInt(inputSplit));
+            validateDuplication(playerBalls, ball);
+            playerBalls.add(ball);
+        }
+        return playerBalls;
     }
 }
