ef49ffeac05e120d47b27aa86be70927645d76e9
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-05 09:55:15 +0900

    refactor(PlayingNumber): 문자열을 상수로 치환 및 스트링 포맷 사용

--- src/main/java/baseball/PlayingNumber.java
+++ src/main/java/baseball/PlayingNumber.java
@@ -12,7 +12,10 @@ public class PlayingNumber {
 	private static final int PLAYING_NUMBER_SIZE = 3;
 	private static final int MIN_NUMBER = 1;
 	private static final int MAX_NUMBER = 9;
-	private static final String REGEX = "^[" + MIN_NUMBER + "-" + MAX_NUMBER + "]*$";
+	private static final String REGEX = String.format("^[%d-%d]*$", MIN_NUMBER, MAX_NUMBER);
+	private static final String SIZE_MESSAGE = "%d자리 숫자만 입력해주세요.";
+	private static final String DUPLICATE_MESSAGE = "서로 다른 숫자들만 입력해주세요.";
+	private static final String REGEX_MESSAGE = "%d ~ %d 사이의 자연수만 입력해주세요.";
 
 	private final int playingNumber;
 
@@ -34,7 +37,7 @@ public class PlayingNumber {
 
 	private void validateSize(String playingNumber) {
 		if (playingNumber.length() != PLAYING_NUMBER_SIZE) {
-			throw new IllegalArgumentException("3자리 숫자만 입력해주세요.");
+			throw new IllegalArgumentException(String.format(SIZE_MESSAGE, PLAYING_NUMBER_SIZE));
 		}
 	}
 
@@ -42,15 +45,14 @@ public class PlayingNumber {
 		List<String> splitNumbers = new ArrayList<>(Arrays.asList(playingNumber.split("")));
 		Set<String> nonDuplicateNumbers = new HashSet<>(splitNumbers);
 		if (nonDuplicateNumbers.size() != PLAYING_NUMBER_SIZE) {
-			throw new IllegalArgumentException("서로 다른 숫자들만 입력해주세요.");
+			throw new IllegalArgumentException(DUPLICATE_MESSAGE);
 		}
 	}
 
 	private void validateREGEX(String playingNumber) {
 		Pattern numberPattern = Pattern.compile(REGEX);
 		if (!numberPattern.matcher(playingNumber).matches()) {
-			throw new IllegalArgumentException("1 ~ 9 사이의 자연수만 입력해주세요.");
+			throw new IllegalArgumentException(String.format(REGEX_MESSAGE, MIN_NUMBER, MAX_NUMBER));
 		}
 	}
-
 }
