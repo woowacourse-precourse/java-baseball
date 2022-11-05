0eb034a211ca9df63f1c6392c9ca30de604f474d
김재현 <kjhkk1020@naver.com>
2022-11-05 18:08:26 +0900

    feat: 1-9 사이의 서로 다른 무작위 수 3개 리스트 반환 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    /** 컴퓨터가 생성하는 1-9 사이의 서로 다른 임의의 숫자로 이루어진 3개의 수로 이루어진 리스트를 생성 */
+    public static List<Integer> getThreeRandomNumbersList() {
+        List<Integer> computerNumberList = new ArrayList<>();
+
+        while (computerNumberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumberList.contains(randomNumber)) {
+                computerNumberList.add(randomNumber);
+            }
+        }
+
+        return computerNumberList;
     }
 }
