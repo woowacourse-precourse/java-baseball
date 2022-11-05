31a9435f8841d25e82fce98df6d3f647f62b4048
YE-EUN <musiclove529@naver.com>
2022-11-05 12:11:14 +0900

    feat : Game 클래스 생성
    - 게임을 관리하는 클래스 생성

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,36 @@
+package baseball;
+
+public class Game {
+    Player problemMaker = new ProblemMaker();
+    Player problemSolver = new ProblemSolver();
+    Result result = new Result();
+    int status = Settings.START_STATUS;
+
+    public void start(){
+
+    }
+
+    public void printMessage(){
+
+    }
+
+    public boolean isStartStatus(){
+        return false;
+    }
+
+    public void requestAnswer(){
+
+    }
+
+    public void checkAnswer(){
+
+    }
+
+    public void countResult(){
+
+    }
+
+    public void setStatusByProblemSolver(){
+
+    }
+}
