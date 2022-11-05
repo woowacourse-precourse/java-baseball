4cc1c7ec4ed8ac91afd4b9fd9843cdffd151d69a
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-05 16:10:42 +0900

    feat(method): add setAnswerValue
    function list 1-1
    add setAnswerValue method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,21 @@
 package baseball;
 
-class GameReadyMachine {
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
+class GameReadyMachine {
+    private int answerValue;
+
+    public boolean setAnswerValue (){
+        answerValue = Randoms.pickNumberInRange(100,999);
+        if(answerValue>=100||answerValue<=999)
+            return true;
+        else
+            return false;
+    }
 }
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
