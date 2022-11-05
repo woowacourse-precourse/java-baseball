5e103892e99813a82b407bdefa69adc6d780a1f7
seongha <11pi885@gmail.com>
2022-11-05 23:32:10 +0900

    feat: 랜덤한 3자리수 생성하는 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static int createRandomNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        int answerNumber = 0; int cnt =100;
+        for (Integer number : computer) {
+            answerNumber += number * cnt;
+            cnt /= 10;
+        }
+        return answerNumber;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
