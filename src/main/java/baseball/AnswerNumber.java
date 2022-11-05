af4af128d6d0fdba478d0d2f0a28b0fc3dc8e0d5
BoyCho <iijokv@naver.com>
2022-11-05 18:29:06 +0900

    feat : 중복 없는 정답수 생성 기능 추가

--- src/main/java/baseball/AnswerNumber.java
+++ src/main/java/baseball/AnswerNumber.java
@@ -6,6 +6,23 @@ public class AnswerNumber {
 
     private static final int LENGTH = 3;
 
+    // 3자리 숫자 생성하기
+    public int[] getAnswerNum () {
+        int[] answer = new int[LENGTH];
+        int cnt = 0;
+
+        while (cnt < LENGTH) {
+            int randomNum = Randoms.pickNumberInRange(1,9);
+            int checkedNum = checkDuplication(answer, randomNum);
+
+            if (isNotDuplicated(checkedNum)) {
+                answer[cnt] = randomNum;
+                cnt++;
+            }
+        }
+        return answer;
+    }
+
     // 중복 확인하기
     int checkDuplication(int[] answer, int randomNum) {
         for (int i = 0; i < LENGTH; i++) {
@@ -15,6 +32,10 @@ public class AnswerNumber {
             }
         }
         return randomNum;
+    }
+
+    boolean isNotDuplicated (int n) {
+        return n != 0;
     }
 
     public AnswerNumber() {}
