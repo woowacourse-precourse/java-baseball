700838e2971a57ac171ba8c689a54b8fcb5e7e2a
nn98 <jkllhgb@gmail.com>
2022-11-05 10:27:26 +0900

    Feat: 사용자 숫자 채점 기능 구현, 3스트라이크시 게임 승리/그 외 재시도 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,13 +3,17 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import constant.Texts;
 
+import java.util.List;
+
 public class Game {
     Computer computer;
     User user;
+
     public Game() {
         computer = new Computer();
         user = new User();
     }
+
     public void run() {
         System.out.println(Texts.GAME_BEGIN);
         do {
@@ -18,8 +22,20 @@ public class Game {
         } while (Console.readLine().equals("1"));
         System.out.println(Texts.GAME_END);
     }
+
     public void play() {
         computer.makeComputerNumber();
-        user.makeUserNumber();
+        List<Integer> result;
+        List<Integer> computerNumbers;
+        computerNumbers = computer.getNumber();
+        do {
+            user.makeUserNumber();
+            Checker checker = new Checker();
+            List<Integer> userNumbers = user.getNumber();
+            result = checker.checkByDigit(userNumbers, computerNumbers);
+            String score=checker.checkResult(result);
+            System.out.println(score);
+        } while (result.get(0) < 3);
+        System.out.println(Texts.GAME_PLAY_WIN);
     }
 }
