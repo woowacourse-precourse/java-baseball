59d517b94481420b6cbd722323785f7573d86f6f
wlsh44 <k87913j@gmail.com>
2022-11-05 22:55:18 +0900

    style: final static 순서 수정

--- src/main/java/baseball/exception/DuplicateNumberException.java
+++ src/main/java/baseball/exception/DuplicateNumberException.java
@@ -2,7 +2,7 @@ package baseball.exception;
 
 public class DuplicateNumberException extends IllegalNumberInputException {
 
-    private final static String DUPLICATE_NUMBER_ERROR = "서로 다른 숫자를 입력해주세요. 입력 문자: %s";
+    private static final String DUPLICATE_NUMBER_ERROR = "서로 다른 숫자를 입력해주세요. 입력 문자: %s";
 
     public DuplicateNumberException(String str) {
         super(String.format(DUPLICATE_NUMBER_ERROR, str));
