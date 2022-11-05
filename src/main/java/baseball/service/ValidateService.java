49ad96843b8f6b5d34d3213cf02c2f87e0fc4f34
Longstick0 <789456jang@naver.com>
2022-11-06 02:45:45 +0900

    fix: 예외클래스 변경

--- src/main/java/baseball/service/ValidateService.java
+++ src/main/java/baseball/service/ValidateService.java
@@ -1,15 +1,13 @@
 package baseball.service;
 
-import java.util.stream.IntStream;
-
 public class ValidateService {
 
     public void validateInputValue(String inputBaseBallNumbers) {
         if (isOverSize(inputBaseBallNumbers)) {
-            throw new IllegalStateException("3자리 수의 정수만 가능합니다.");
+            throw new IllegalArgumentException("3자리 수의 정수만 가능합니다.");
         }
         if (isNotNumber(inputBaseBallNumbers)) {
-            throw new IllegalStateException("숫자만 입력 가능합니다.");
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
         }
     }
 
