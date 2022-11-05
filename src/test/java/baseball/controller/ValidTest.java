4db3dc73cdcd698aa41fd89f2d994993db3b3517
gyuturn <rbtjrrbals@gmail.com>
2022-11-06 00:58:15 +0900

    test($controller): numList내 숫자중 0이 있는경우 테스트케이스 추가
    첫번째, 두번째 index에도 0이 있는경우를 테스트위함
    
    F-1-1
    1,0,2의 list를 생성하여 false를 리턴하는지 테스트
    0,1,2의 list를 생성하여 false를 리턴하는지 테스트

--- src/test/java/baseball/controller/ValidTest.java
+++ src/test/java/baseball/controller/ValidTest.java
@@ -11,6 +11,22 @@ class ValidTest {
     void 숫자가0이있는경우() {
         //given
         List<Integer> includeZeroNumList = Arrays.asList(1, 2, 0);
+        List<Integer> includeZeroNumList2 = Arrays.asList(1, 0,2);
+        List<Integer> includeZeroNumList3 = Arrays.asList(0, 1,2);
+        //when
+        boolean notValid = Valid.isValidNumList(includeZeroNumList);
+        boolean notValid2 = Valid.isValidNumList(includeZeroNumList2);
+        boolean notValid3 = Valid.isValidNumList(includeZeroNumList3);
+        //then
+        assertThat(notValid).isEqualTo(false);
+        assertThat(notValid2).isEqualTo(false);
+        assertThat(notValid3).isEqualTo(false);
+    }
+
+    @Test
+    void 중복된숫자가있는경우(){
+        //given
+        List<Integer> includeZeroNumList = Arrays.asList(1, 2, 2);
         //when
         boolean notValid = Valid.isValidNumList(includeZeroNumList);
         //then
