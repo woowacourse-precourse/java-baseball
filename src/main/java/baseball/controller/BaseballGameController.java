fa8c585b520f388cbe4e1f115c4dae0e99666cea
qdeu22 <qdeu22@gmail.com>
2022-11-05 13:29:21 +0900

    refactor(BaseballGameController): 상수 추가 및 statrGame함수 내 코드 수정

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -2,10 +2,12 @@ package baseball.controller;
 
 import baseball.model.ComputerRandomNumber;
 import baseball.model.UserBaseballNumber;
+import baseball.utils.NumberCalculator;
 import baseball.view.InputView;
 
 public class BaseballGameController {
     private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    private static final int THREE_COUNT = 3;
 
     public static void start() {
         System.out.println(GAME_START);
@@ -13,9 +15,22 @@ public class BaseballGameController {
     }
 
     private static void startGame() {
+
+        int strike = 0;
+        int ball = 0;
+
         ComputerRandomNumber computerRandomNumber = new ComputerRandomNumber();
-        for (int i = 0; i < 3; i++) {
-          UserBaseballNumber userBaseballNumber = new UserBaseballNumber(InputView.inputNumber());
-        }
+
+        do {
+
+            UserBaseballNumber userBaseballNumber = new UserBaseballNumber(InputView.inputNumber());
+
+            NumberCalculator numberCalculator = new NumberCalculator(computerRandomNumber.getComputerNumber(), userBaseballNumber.getUserNumber());
+            numberCalculator.resultCalculator();
+
+            strike = numberCalculator.getStrike();
+            ball = numberCalculator.getBall();
+
+        } while (strike != THREE_COUNT);
     }
 }
