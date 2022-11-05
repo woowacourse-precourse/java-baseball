8c62d7509ebfcdde298d71edca916ead67649093
Jimin <thswlals0219@gmail.com>
2022-11-05 23:31:45 +0900

    feat: 사용자 수와 컴퓨터의 수를 비교하여 결과를 반환하는 기능 추가

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -2,7 +2,9 @@ package baseball.model;
 
 import baseball.BaseballScore;
 import baseball.util.RandomUtil;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Computer {
 
@@ -10,6 +12,25 @@ public class Computer {
 
     public void throwTheBall() {
         ballNumber = RandomUtil.pickNumbers();
+    }
+
+    public Map<BaseballScore, Integer> compare(List<Integer> batNumber) {
+        Map<BaseballScore, Integer> result = new HashMap<>() {
+            {
+                put(BaseballScore.STRIKE, 0);
+                put(BaseballScore.BALL, 0);
+                put(BaseballScore.NOTHING, 0);
+            }
+        };
+        computeScoreCount(batNumber, result);
+        return result;
+    }
+
+    public void computeScoreCount(List<Integer> batNumber, Map<BaseballScore, Integer> result) {
+        for (int number : batNumber) {
+            BaseballScore score = computeNumberScore(number, batNumber);
+            result.put(score, result.get(score) + 1);
+        }
     }
 
     public BaseballScore computeNumberScore(int number, List<Integer> batNumber) {
