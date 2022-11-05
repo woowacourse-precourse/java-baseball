1aac7d01828f3177a3b49f315fc7ae088c5901cc
1eejisoo <jisu991012@gmail.com>
2022-11-05 17:45:55 +0900

    Feat: 입력 값과 랜덤 값을 비교하여 결과를 출력하는 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -8,6 +8,11 @@ import java.util.List;
 
 public class BaseballGame {
     private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
+    private static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private int restartNumber;
 
     public List<Integer> input() {   // 입력
         System.out.print(INPUT_NUMBER);
@@ -23,6 +28,29 @@ public class BaseballGame {
             number.add(Integer.parseInt(String.valueOf(inputNumber.charAt(digit))));
         }
         return number;
+    }
+
+    public void makeResult(Number number, List<Integer> computerNumber) {   // 결과 생성
+        int strike = 0;
+        int ball = 0;
+
+        strike = judgeStrike(number, computerNumber, strike);
+        ball = judgeBall(number, computerNumber, ball);
+
+        if (ball > 0) {
+            System.out.print(ball + BALL + " ");
+        }
+        if (strike > 0) {
+            System.out.print(strike + STRIKE);
+        }
+        if (strike == 3) {
+            System.out.print("\n" + GAME_END);
+            restartNumber++;
+        }
+        if (strike == 0 && ball == 0) {
+            System.out.print(NOTHING);
+        }
+        System.out.print("\n");
     }
 
     public int judgeStrike(Number number, List<Integer> computerNumber,  int strike) {   // 스트라이크 판정
