678d86e819b3b1aa136c368a62a7be099b8ab391
syahn <496300@naver.com>
2022-11-05 13:47:57 +0900

    FEAT : 게임을 시작하기 위한 기능 구현
    - 숫자 게임을 위한 랜덤 숫자를 저장하는 LIST 생성
    - 시작 메시지를 알림
    - createComputerNum 함수 호출하여 랜덤 숫자 3개를 LIST에 저장

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class Application {
 
-    public static List<Integer> createComputerNum(){
+    public static List<Integer> createComputerNum() {
         List<Integer> computerNum = new ArrayList<>();
         while (computerNum.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -15,6 +15,12 @@ public class Application {
                 computerNum.add(randomNumber);
         }
         return computerNum;
+    }
+
+    public void startGame() {
+        List<Integer> computerNum = new ArrayList<>();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        computerNum = createComputerNum();
     }
 
     public static void main(String[] args) {
