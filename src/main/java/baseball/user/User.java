606e7e244f26abc7f5215d04e48071944f257056
K-Diger <kitten.diger@gmail.com>
2022-11-05 21:04:33 +0900

    refactor : User 클래스 UserInputValidator 의존성 주입 방식 변경 - 기존 생성자 주입 방식에서 - UserInputValidator 싱글턴 객체 가져오는 것으로 변경

--- src/main/java/baseball/user/User.java
+++ src/main/java/baseball/user/User.java
@@ -4,15 +4,9 @@ import java.util.List;
 
 public class User {
 
-    private final UserInputValidator userInputValidator;
-
-    public User(UserInputValidator userInputValidator) {
-        this.userInputValidator = userInputValidator;
-    }
-
     public List<String> stringConvertToStringList(String inputValue) {
         List<String> inputValueList = List.of(inputValue.split(""));
-        userInputValidator.validate(inputValueList);
+        UserInputValidator.getInstance().validate(inputValueList);
         return inputValueList;
     }
 }
