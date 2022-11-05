8dffd15f575498660ebe6554c453aa32273576fd
Yongwoo Choi <cyw7515@naver.com>
2022-11-05 15:37:07 +0900

    feat (PrintService): 게임 시작 및 사용자의 입력을 요청하는 문구 추가

--- src/main/java/baseball/ExecuteService.java
+++ src/main/java/baseball/ExecuteService.java
@@ -5,12 +5,18 @@ import camp.nextstep.edu.missionutils.Console;
 public class ExecuteService {
     private Game game;
     private boolean complete;
+    private static final PrintService printService = new PrintService();
+
+    public ExecuteService() {
+        printService.gameStart();
+    }
 
     public void execute() {
         this.game = new Game();
         complete = false;
 
         while (!complete) {
+            printService.requestInput();
             String userInput = getUserInput();
         }
     }
---
+++ src/main/java/baseball/PrintService.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import java.util.List;
+
+public class PrintService {
+    public void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void requestInput() {
+        System.out.print("숫자를 입력해 주세요: ");
+    }
+}
