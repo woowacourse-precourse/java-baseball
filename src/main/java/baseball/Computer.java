13ca5db1c90d2514d798d481b3ca96cc6e21d841
dsaf2007 <dsaf2007@naver.com>
2022-11-06 03:27:54 +0900

    style : formatting source

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -3,33 +3,30 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+
 /*
  * 임의의 세자리 수를 생성하는 클래스.
  * ""를 통해 정답 체크.
  */
 public class Computer {
-    
-    private String answer = "";
-    private int INPUT_LENGTH = 3;
-    private List<Integer> list = new ArrayList<Integer>();
-    public void createNumber()
-    {
-        while(list.size() <INPUT_LENGTH)
-        {
-            int temp = Randoms.pickNumberInRange(1,9);
-            if(!list.contains(temp))
-            {
-                list.add(temp);
-            }
-        }
-        for(Integer list_ : list)
-        {
-            answer += Integer.toString(list_);
-        }
-    }
 
-    public String getAnswer()
-    {
-        return this.answer;
+  private String answer = "";
+  private int INPUT_LENGTH = 3;
+  private List<Integer> list = new ArrayList<Integer>();
+
+  public void createNumber() {
+    while (list.size() < INPUT_LENGTH) {
+      int temp = Randoms.pickNumberInRange(1, 9);
+      if (!list.contains(temp)) {
+        list.add(temp);
+      }
     }
+    for (Integer list_ : list) {
+      answer += Integer.toString(list_);
+    }
+  }
+
+  public String getAnswer() {
+    return this.answer;
+  }
 }
