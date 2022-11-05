42e9b7f204c29533f4b74a52a273ea68a0cef2fa
Timothy <timothy@jungnanum.com>
2022-11-05 20:22:56 +0900

    feat: 출력용 문자열 상수 작성
    1. 상수 정의
    > - 출력용 문자열 상수들 작성

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -8,6 +8,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import baseball.controller.BaseBallController;
 import baseball.vo.enumtype.GameNumberInclusive;
 import baseball.vo.enumtype.GameRule;
+import baseball.vo.enumtype.UserInterfaceMessage;
+import baseball.vo.enumtype.ValidationMessage;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.HashSet;
 import java.util.List;
@@ -69,6 +71,27 @@ class ApplicationTest extends NsTest {
         Set<Integer> baseballGameNumberSet = new HashSet<>(baseballGameNumberList);
         // then
         assertThat(baseballGameNumberList.size()).isEqualTo(baseballGameNumberSet.size());
+    }
+
+    @Test
+    void UserInterfaceMessage_메시지_확인_테스트() {
+        // give
+        String gameStartMessage =
+                "야구게임에 오신 것을 환영합니다.\n야구게임은 숫자 3개를 입력받습니다.\n만약 종료를 원하시면 2를 입력하세요 ";
+        // when
+        String gameStartMessageEnum = UserInterfaceMessage.GAME_INFO.getValue();
+        // then
+        assertThat(gameStartMessage).isEqualTo(gameStartMessageEnum);
+    }
+
+    @Test
+    void ValidationMessage_메시지_확인_테스트() {
+        // give
+        String properTypeMessage = "바르게 입력되었습니다.";
+        // when
+        String properTypeMessageEnum = ValidationMessage.PROPER_TYPE.getValue();
+        // then
+        assertThat(properTypeMessage).isEqualTo(properTypeMessageEnum);
     }
 
 
