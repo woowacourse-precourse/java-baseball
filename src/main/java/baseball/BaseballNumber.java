768f2478c7e1e0057073ade68cbab9670754b4aa
woosung1223 <dntjd991223@naver.com>
2022-11-05 15:33:41 +0900

    feat(BaseballNumber): setRandomValue 메소드 생성

--- src/main/java/baseball/BaseballNumber.java
+++ src/main/java/baseball/BaseballNumber.java
@@ -2,16 +2,24 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import baseball.utils.Util;
 
 public class BaseballNumber {
-    private final List<Integer> listNumber;
-
+    private List<Integer> listNumber;
+    private final int GAME_DIGIT = 3;
+    // 나중에 GAME_DIGIT, setRandomValue는 상황 체크 후에 BaseballManager에게 책임 위임
     BaseballNumber(int number) {
         this.listNumber = Util.splitDigitsIntoList(number);
     }
 
-    int getNumberAt(int index) {
+    public int getNumberAt(int index) {
         return listNumber.get(index);
+    }
+
+    public void setRandomValue() {
+        this.listNumber = Util.splitDigitsIntoList(
+                RandomNumGenerator.makeRandomNumWithNDigits(GAME_DIGIT)
+        );
     }
 }
