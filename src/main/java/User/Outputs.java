c0626159ee8b1a4e5bb20a9a90c6d3ba005a4ebf
Devtist <yt930310@snu.ac.kr>
2022-11-05 17:21:36 +0900

    style: 상수명 변경

--- src/main/java/User/Outputs.java
+++ src/main/java/User/Outputs.java
@@ -2,11 +2,11 @@ package User;
 
 public enum Outputs {
 	GAME_START_MESSAGE("숫자 야구 게임을 시작합니다."),
-	ASK_INPUT_MESSAGE("숫자를 입력해주세요 : "),
+	ASKING_INPUT_MESSAGE("숫자를 입력해주세요 : "),
 	BALLS_COUNT_MESSAGE("%d볼"),
 	STRIKES_COUNT_MESSAGE("%d스트라이크"),
 	NOTHING_MESSAGE("낫싱"),
-	GAME_END_MESSAGE("%d개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+	GETTING_RIGHT_ANSWER_MESSAGE("%d개의 숫자를 모두 맞히셨습니다! 게임 종료"),
 	RESTART_OR_NOT_MESSAGE("게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.");
 	private final String message;
 
