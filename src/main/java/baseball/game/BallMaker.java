04f602aa164280d4db0d0e7112025dda2d815252
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-05 16:41:14 +0900

    feat(BallMaker): Randoms객체에서 받은 값을 바탕으로 유효한 Ball 생성하는 기능 추가

--- src/main/java/baseball/game/BallMaker.java
+++ src/main/java/baseball/game/BallMaker.java
@@ -1,12 +1,40 @@
 package baseball.game;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BallMaker {
+    private int ballSize;
+    private boolean allowDuplicate;
+
+    private int minimum;
+    private int maximum;
+
+
+    public BallMaker() {
+        this.ballSize = 3;
+        this.allowDuplicate = false;
+        this.maximum=9;
+        this.minimum=1;
+    }
+
     public List<Integer> getRandomBall() {
-        List<Integer> randomBall = new ArrayList<>();
-        return randomBall;
+        if (!allowDuplicate) {
+            return getUniqueRandomBall();
+        }
+        return new ArrayList<>();
+    }
+
+    private List<Integer> getUniqueRandomBall() {
+        Set<Integer> result = new HashSet<>();
+        while (result.size() != ballSize) {
+            result.add(Randoms.pickNumberInRange(minimum,maximum));
+        }
+        return new ArrayList<>(result);
     }
 
     public List<Integer> getUserBall() {
