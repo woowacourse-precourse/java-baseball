6ef3c361c1c00f0b2f6c267f7952290fbf4418a8
Heesom <heesom.hs@gmail.com>
2022-11-05 16:36:59 +0900

    Test(MyTest) : checkUserHasException 메소드 테스트 추가
     적절한 예외를 발생시키는지 확인하는 테스트 추가

--- src/test/java/baseball/MyTest.java
+++ src/test/java/baseball/MyTest.java
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.Mockito.spy;
 
 public class MyTest extends NsTest {
@@ -18,10 +20,17 @@ public class MyTest extends NsTest {
         assertThat(spyList.size()).as("out of range").isEqualTo(3);
     }
 
+    @Test
+    void 사용자가_입력한_값이_예외인가() {
+        List<Integer> userList = Arrays.asList(2, 2, 3);
+
+        assertThatThrownBy( () -> spy(Application.class).checkUserHasException(userList) )
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
     }
-
 
 }
