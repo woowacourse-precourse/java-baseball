a3a0f34b9882dfa1a39c3377248a18b029d2afa0
Kid-Chang <pch3310@gmail.com>
2022-11-06 02:12:47 +0900

    refactor(tools/Validator): 숫자야구의 값이 규칙에 맞게 입력되었는지 확인하는 함수 구현
    1. 3자리 숫자인지 확인
    2. 각 자리수마다 1~9자리 숫자를 입력했는지 확인

---
+++ src/main/java/baseball/tools/Validator.java
@@ -0,0 +1,33 @@
+package baseball.tools;
+
+public class Validator {
+    private final String baseballNumber;
+
+    public Validator(String baseballNumber) {
+        this.baseballNumber = baseballNumber;
+    }
+
+    public boolean checkBaseballNumber() {
+        // 3자리인지
+        if (!checkBaseballNumberLength())
+            return false;
+        // 1~9 사이의 숫자를 입력했는지
+        if (!checkBaseballNumberRange())
+            return false;
+        return false;
+    }
+
+    private boolean checkBaseballNumberLength() {
+        return baseballNumber.length() == 3;
+    }
+
+    private boolean checkBaseballNumberRange() {
+        for (int i = 0; i < 3; i++) {
+            int number = baseballNumber.charAt(i);
+            if (number < 1 || number > 9)
+                return false;
+        }
+        return true;
+    }
+
+}
