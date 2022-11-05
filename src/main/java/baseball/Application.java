5782780bb2020d971344eae1e0efebac4fdcd956
kim0914 <kosa0914@naver.com>
2022-11-05 14:12:34 +0900

    feat(Application): 야구게임 시작 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import constant.Message;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame();
+        Message.showGameStartMessage();
+        baseballGame.run();
     }
 }
