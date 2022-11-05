7cb81ee3b4b845a1994f48a0cccb936455823843
wlsh44 <k87913j@gmail.com>
2022-11-05 17:38:50 +0900

    feat: BaseballGame 클래스에 기능 연결

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,8 +1,30 @@
 package baseball;
 
+import baseball.inputter.NumberInputter;
+import baseball.opponent.ComputerOpponent;
+import baseball.printer.MessagePrinter;
+import baseball.printer.NumberInputMessagePrinter;
+import baseball.printer.StartMessagePrinter;
+
 public class BaseballGame {
 
-    public void play() {
+    private ComputerOpponent opponent;
+    private MessagePrinter inputMessagePrinter;
+    private NumberInputter numberInputter;
 
+    public BaseballGame() {
+        MessagePrinter startMessagePrinter = new StartMessagePrinter();
+        this.inputMessagePrinter = new NumberInputMessagePrinter();
+        this.opponent = new ComputerOpponent();
+        startMessagePrinter.printMessage();
+    }
+
+    public void play() throws IllegalArgumentException {
+        try {
+            inputMessagePrinter.printMessage();
+            Integer inputNumber = numberInputter.input();
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 }
