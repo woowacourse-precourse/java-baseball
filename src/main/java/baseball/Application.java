fae073bc54217eb2ddb4b684f335f52c7651a6a5
khj <bbb6254177@gmail.com>
2022-11-05 19:14:13 +0900

    feat: add function to get input from user

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -18,9 +19,25 @@ public class Application {
         return computer;
     }
 
+    private static List<Integer> rand_num_input() {
+        List<Integer> input_tolist = new ArrayList<>();
+        String input = Console.readLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            input_tolist.add(Character.getNumericValue(input.charAt(i)));
+        }
+
+        return input_tolist;
+    }
+
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
 
         //1. 상대방이 랜덤한 세 자리수를 생성한다.
         List<Integer> rand_num = rand_num_gen();
+
+        //2. 사용자로부터 세 자리수를 입력받는다.
+        System.out.print("숫자를 입력해주세요 : ");
+        List<Integer> input_num = rand_num_input();
     }
 }
