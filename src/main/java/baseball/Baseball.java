471c143b1992c149030e007ffe4afaeaf2e7c8c0
hossang <zkzk104@naver.com>
2022-11-05 14:54:01 +0900

    feat: 서로 다른 3개의 숫자 예외 확인하기

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -3,17 +3,13 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Baseball {
     private List<Integer> computer;
     private List<Integer> userNumber;
-
-    public void setUserNumber(String number) {
-        for (int index = 0; index < number.length(); index++) {
-            userNumber.add(Integer.valueOf(number.charAt(index)));
-        }
-    }
 
     public Baseball() {
         computer = new ArrayList<>();
@@ -23,5 +19,34 @@ public class Baseball {
                 computer.add(randomNumber);
             }
         }
+    }
+
+    public List<Integer> setUserNumber(String number) {
+        if (isException(number)) {
+            throw new IllegalArgumentException();
+        }
+        userNumber = new ArrayList<>();
+        for (int index = 0; index < number.length(); index++) {
+            userNumber.add(Integer.valueOf(number.charAt(index) - '0'));
+        }
+        return userNumber;
+    }
+
+    private boolean isException(String number) {
+        if (number.length() != 3) {
+            return true;
+        }
+        Set<Integer> set = new HashSet<>();
+        for (int index = 0; index < number.length(); index++) {
+            int charNumber = number.charAt(index) - '0';
+            if (charNumber < 0 || charNumber > 9) {
+                return true;
+            }
+            if (set.contains(charNumber)) {
+                return true;
+            }
+            set.add(charNumber);
+        }
+        return false;
     }
 }
