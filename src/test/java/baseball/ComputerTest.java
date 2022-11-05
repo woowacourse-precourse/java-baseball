4809244ec3e48861d09f5a6e9f3a541431ead513
csj <koreaxman@naver.com>
2022-11-05 15:49:42 +0900

    test: fix for to repeatedtest

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,10 +11,8 @@ import static org.junit.jupiter.api.Assertions.*;
 class ComputerTest {
 
     @DisplayName("난수를 1000번 돌려 1과 9사이 숫자만 나오는지")
-    @Test
+    @RepeatedTest(1000)
     void createRandomNumber() {
-        for (int i = 0; i < 1000; i++) {
             assertThat(Randoms.pickNumberInRange(1, 9)).isBetween(1, 9);
-        }
     }
 }
