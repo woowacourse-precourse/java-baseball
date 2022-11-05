866c25c3274e7051db4900a6c6882b34e50c976c
plasticHong <zakkslash18@naver.com>
2022-11-05 18:34:51 +0900

    feat(Application.java): 기능 추가
    정답 만들기 makeRandomNumStr() 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,35 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String correctAnswer = makeRandomNumStr();
+
     }
+
+    private static String makeRandomNumStr() {
+        Set<String> correctAnswer = new HashSet<>();
+
+        while (correctAnswer.size() < 3) {
+            String str = Randoms.pickNumberInRange(1, 9) + "";
+            correctAnswer.add(str);
+        }
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (String s : correctAnswer
+        ) {
+            stringBuilder.append(s);
+        }
+
+        String randomNumStr = String.valueOf(stringBuilder);
+
+        return randomNumStr;
+    }
+
 }
