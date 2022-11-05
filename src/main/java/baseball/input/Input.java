c18ccd4d941040bf5a2b61231006cc93d84a9d39
math0917 <math0917@naver.com>
2022-11-05 17:17:39 +0900

    feat: 호출 input 메써드
    사용자는 valid한 input인지 확인하는데 집중하고 input은 input을 받는 책임만하도록 하여 재사용성
    을 증가시킨다

---
+++ src/main/java/baseball/input/Input.java
@@ -0,0 +1,9 @@
+package baseball.input;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Input {
+    public static String inputString() {
+        return Console.readLine();
+    }
+}
--- src/main/java/baseball/user/User.java
+++ src/main/java/baseball/user/User.java
@@ -1,9 +1,11 @@
 package baseball.user;
 
+import baseball.input.Input;
 import java.util.List;
 
 public class User {
     public static List<Integer> createUserNonDuplicateIntegerList() {
+        String userInput = Input.inputString();
         return null;
     }
 }
