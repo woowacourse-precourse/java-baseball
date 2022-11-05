202ac1b1c71f98ef7489f803f2cb54d33710702f
leeyongjun <leeyongjun0310@naver.com>
2022-11-05 20:22:37 +0900

    feat(Game): 사용자가 입력한 숫자에서 볼과 스트라이크를 반영하는 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,16 +3,29 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
+    private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_SENTENCE = "숫자를 입력해주세요 : ";
     private List<Integer> userNumber = new ArrayList<>();
     private List<Integer> computerNumber = new ArrayList<>();
+    private Score score;
 
-    public Game(String userNumber) {
-        this.userNumber = Change.numberToList(userNumber);
+    public Game() {
         this.computerNumber = Generate.randomNumber();
     }
 
-    public void start(){
+    public void play() {
+        for (int i = 0; i < Constant.NUMBER_LENGTH; i++) {
+            if (this.userNumber.get(i).equals(this.computerNumber.get(i))) {
+                this.score.setStrike(score.getStrike() + 1);
+                continue;
+            }
 
+            if (this.computerNumber.contains(this.userNumber.get(i))) {
+                this.score.setBall(score.getBall() + 1);
+            }
+        }
     }
 }
