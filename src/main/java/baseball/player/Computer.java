8f6d3b7769c6c848576c8ee96c300494c25068c2
SeokHwan An <asok369258@naver.com>
2022-11-05 22:39:16 +0900

    feat : 컴퓨터에서 중복된 수가 있는지 검증하는 기능 추가

--- src/main/java/baseball/player/Computer.java
+++ src/main/java/baseball/player/Computer.java
@@ -4,14 +4,20 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Computer {
+
     private List<Integer> randomNum;
 
     /**
      * 랜덤한 수를 생성하는 기능
      */
     public int createRandomNum() {
-        return Randoms.pickNumberInRange(1,9);
+        return Randoms.pickNumberInRange(1, 9);
     }
 
-
+    /**
+     * 중복된 수가 있는지 검증하는 기능
+     */
+    public boolean checkOverlap(int num) {
+        return randomNum.contains(num);
+    }
 }
