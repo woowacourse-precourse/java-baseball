c21bb29005a7d18b9ea2b4fce29c733bd3c08f42
phobyjun <phobyjun@gmail.com>
2022-11-06 05:12:39 +0900

    test(BaseballGameResult): getter test

--- src/test/java/baseball/BaseballGameResultTest.java
+++ src/test/java/baseball/BaseballGameResultTest.java
@@ -31,4 +31,18 @@ class BaseballGameResultTest {
 
         assertThat(result1).isEqualTo(2);
     }
+
+    @Test
+    void getterTest() {
+        List<Integer> list1 = List.of(1, 4, 3);
+        List<Integer> list2 = List.of(3, 4, 1);
+
+        BaseballGameResult gameResult = new BaseballGameResult(list1, list2);
+
+        int numOfStrikes = gameResult.numOfStrikes();
+        int numOfBalls = gameResult.numOfBalls();
+
+        assertThat(numOfStrikes).isEqualTo(1);
+        assertThat(numOfBalls).isEqualTo(2);
+    }
 }
