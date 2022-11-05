dd5cfc691c1523238633ba73c3050bec5043bbde
opuopuo <pureuml.dev@gmail.com>
2022-11-05 15:41:52 +0900

    feat(computer): 랜덤 값 생성 및 저장 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,33 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Computer {
+
+    private Integer[] answer;
+    private static final int MAX_LENGTH = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+
+    public Computer() {
+        answer = new Integer[MAX_LENGTH];
+    }
+
+    public void generateValue() {
+        Set<Integer> value = new HashSet<>();
+        while (value.size() < MAX_LENGTH) {
+            value.add(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
+        }
+        setToArray(value);
+    }
+
+    private void setToArray(Set<Integer> value) {
+        int index = 0;
+        for (Integer number : value) {
+            answer[index] = number;
+            index++;
+        }
+    }
+}
