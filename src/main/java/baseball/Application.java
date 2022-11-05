88e8c1999382dbbc4f9102883f4e280ec6b638e9
LEE-haeuk <rkwksmswhdk@naver.com>
2022-11-05 12:30:12 +0900

    feat: add the getting player number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
+
     public static void main(String[] args) {
         Application application = new Application();
         List<Integer> computer = new ArrayList<>();
+        List<Integer> player = new ArrayList<>();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         computer = application.createRandomNumbersInList();
         System.out.print("숫자를 입력해주세요 : ");
+        player = application.getPlayerNumberInList();
+
     }
 
     public List<Integer> createRandomNumbersInList() {
@@ -24,5 +31,14 @@ public class Application {
             }
         }
         return randomNumbers;
+    }
+
+    public List<Integer> getPlayerNumberInList() {
+        String playerNumber = Console.readLine();
+        List<Integer> playerNumberList =
+                Arrays.stream(playerNumber.split(""))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+        return playerNumberList;
     }
 }
