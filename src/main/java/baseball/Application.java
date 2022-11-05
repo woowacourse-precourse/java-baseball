44f2893662b162e9f5eb41b8ccb5a1b6a1f4c9dc
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-05 16:22:39 +0900

    feat(method): add checkDigitValue
    function list 1-2
    add checkDigitValue method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,30 @@ import camp.nextstep.edu.missionutils.Console;
 
 class GameReadyMachine {
     private int answerValue;
+    public boolean checkDigitValue(int value){
+        int hundredDigits = value / 100;
+        value = value%100;
+        int tensDigits = value / 10;
+        value = value%10;
+        int oneDigits = value;
 
+        if(hundredDigits==tensDigits)
+        {
+            return false;
+        }
+        else if(tensDigits==oneDigits)
+        {
+            return false;
+        }
+        else if(hundredDigits==oneDigits)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
     public boolean setAnswerValue (){
         answerValue = Randoms.pickNumberInRange(100,999);
         if(answerValue>=100||answerValue<=999)
