c2ddf157f2751c676c5ffcf3a7145ebbb5a97199
myeongju00 <j51677767@gmail.com>
2022-11-06 02:13:37 +0900

    feat : 게임 재시작 or 게임종료를 물어보는 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class Game {
     private static final int DIGITS = 3;
@@ -8,6 +10,22 @@ public class Game {
     private static final String STRIKE_STR = "스트라이크 ";
     private static final String NOTHING = "낫싱";
     private static final String THREE_STRIKE_SENTENCE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_OR_EXIT_SENTENCE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+
+    private String inputRestartOrExit() {
+        System.out.println(RESTART_OR_EXIT_SENTENCE);
+        String input = Console.readLine();
+        if(!isRightAnswer(input)) {
+            throw new IllegalArgumentException();
+        }
+        return input;
+    }
+
+    private boolean isRightAnswer(String input) {
+        Pattern pattern = Pattern.compile("[1-2]");
+        return pattern.matcher(input).matches();
+    }
 
     private void printHint(List<Character> answer, List<Character> input) {
         int strike = countStrike(answer, input);
