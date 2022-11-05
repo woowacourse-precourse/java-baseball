8435530339b7d8a99327183637c1ed899d789ef7
Invidam <hansupark0208@gmail.com>
2022-11-05 23:03:29 +0900

    refactor(Opponent): Hint 리팩터링
    - 출력 메시지 상수 처리.
    - 반복문과 조건문 메서드 추출.

--- src/main/java/baseball/domain/Hint.java
+++ src/main/java/baseball/domain/Hint.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class Hint {
 
+    public static final String STRIKE_MESSAGE = "스트라이크";
+    public static final String BALL_MESSAGE = "볼";
+    public static final String SPACE_MESSAGE = " ";
     private int strikeCount;
     private int ballCount;
 
@@ -14,41 +17,116 @@ public class Hint {
         strikeCount = 0;
         ballCount = 0;
 
-        for (int i = 0; i < answerList.size(); ++i) {
-            if (answerList.get(i).equals(inputList.get((i)))) {
-                ++strikeCount;
-            }
-        }
-        for (int i = 0; i < answerList.size(); ++i) {
-            for (int j = 0; j < inputList.size(); ++j) {
-                if (i != j && answerList.get(i).equals(inputList.get((j)))) {
-                    ++ballCount;
-                }
-            }
-        }
+        updateStrikeCount(answerList, inputList);
+        updateBallCount(answerList, inputList);
     }
 
     @Override
     public String toString() {
-        if(strikeCount == 0 && ballCount == 0) {
+        if (isAllCountEmpty()) {
             return "낫싱";
         }
-        String strikeMsg = String.format("%d스트라이크", strikeCount);
-        String ballMsg = String.format("%d볼", ballCount);
+        String strikeMsg = String.format("%d%s", strikeCount, STRIKE_MESSAGE);
+        String ballMsg = String.format("%d%s", ballCount, BALL_MESSAGE);
 
-        String hintMsg;
-        StringBuffer    stringBuffer = new StringBuffer();
-        if (!(ballCount == 0)) {
-            stringBuffer.append(ballMsg);
+        return makeHintMsg(strikeMsg, ballMsg);
+    }
+
+    private void updateBallCount(List<Integer> answerList, List<Integer> inputList) {
+        for (int answerIdx = 0; answerIdx < answerList.size(); ++answerIdx) {
+            for (int inputIdx = 0; inputIdx < inputList.size(); ++inputIdx) {
+                updateBallCountEach(answerList, inputList, answerIdx, inputIdx);
+            }
         }
-        if (!(strikeCount == 0 || ballCount == 0)) {
-            stringBuffer.append(" ");
+    }
+
+    private void updateBallCountEach(List<Integer> answerList, List<Integer> inputList, int answerIdx, int inputIdx) {
+        if (isEqualBirth(answerIdx, inputIdx)) {
+            return;
         }
-        if(!(strikeCount == 0)) {
+        if (!isEqualValue(answerList.get(answerIdx), inputList.get(inputIdx))) {
+            return;
+        }
+        addBallCount(answerList.get(answerIdx), inputList.get(inputIdx));
+    }
+
+    private void addBallCount(Integer answerDigit, Integer inputDigit) {
+        ++ballCount;
+    }
+
+    private void updateStrikeCount(List<Integer> answerList, List<Integer> inputList) {
+        for (int answerIdx = 0; answerIdx < answerList.size(); ++answerIdx) {
+            updateStrikeCountEach(answerList, inputList, answerIdx);
+        }
+    }
+
+    private void updateStrikeCountEach(List<Integer> answerList, List<Integer> inputList, int answerIdx) {
+        if (!isEqualBirth(answerList, inputList, answerIdx)) {
+            return;
+        }
+        addStrikeCount();
+    }
+
+    private void addStrikeCount() {
+        ++strikeCount;
+    }
+
+    private boolean isEqualBirth(List<Integer> answerList, List<Integer> inputList, int answerIdx) {
+        return isEqualValue(answerList.get(answerIdx), inputList.get(answerIdx));
+    }
+
+    private boolean isEqualBirth(int answerIdx, int inputIdx) {
+        return answerIdx == inputIdx;
+    }
+
+
+    private boolean isEqualValue(Integer answerDigit, Integer inputDigit) {
+        return answerDigit.equals(inputDigit);
+    }
+
+
+    private String makeHintMsg(String strikeMsg, String ballMsg) {
+        StringBuffer stringBuffer = new StringBuffer();
+
+        addBallMsg(ballMsg, stringBuffer);
+        addSpaceMsg(stringBuffer);
+        addStrikeMsg(strikeMsg, stringBuffer);
+
+        return stringBuffer.toString();
+    }
+
+    private void addStrikeMsg(String strikeMsg, StringBuffer stringBuffer) {
+        if (!isStrikeCountEmpty()) {
             stringBuffer.append(strikeMsg);
         }
-        hintMsg = stringBuffer.toString();
-        return hintMsg;
+    }
+
+    private void addSpaceMsg(StringBuffer stringBuffer) {
+        if (isAllCountExist()) {
+            stringBuffer.append(SPACE_MESSAGE);
+        }
+    }
+
+    private void addBallMsg(String ballMsg, StringBuffer stringBuffer) {
+        if (!isBallCountEmpty()) {
+            stringBuffer.append(ballMsg);
+        }
+    }
+
+    private boolean isAllCountExist() {
+        return !(isStrikeCountEmpty() || isBallCountEmpty());
+    }
+
+    private boolean isAllCountEmpty() {
+        return isStrikeCountEmpty() && isBallCountEmpty();
+    }
+
+    private boolean isBallCountEmpty() {
+        return ballCount == 0;
+    }
+
+    private boolean isStrikeCountEmpty() {
+        return strikeCount == 0;
     }
 
     public boolean isAnswer() {
