8082575129d12f61e5f38d3be21c29856b9a569f
Yeongseon Park <yseon406@gmail.com>
2022-11-06 01:19:18 +0900

    feat : createComputerNumber() 메서드 추가
    상대방(컴퓨터) 숫자를 랜덤으로 생성하는 메서드

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public List<Integer> createComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+
+        }
+        return computerNumber;
     }
 }
