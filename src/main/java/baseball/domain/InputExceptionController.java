75c8b14d052d3186bc5b484a91bb94f2e682821e
anneu <b.imnuj@gmail.com>
2022-11-05 22:54:31 +0900

    feat: 플레이어 수 입력에 대한 예외를 확인하는 기능 구현

---
+++ src/main/java/baseball/domain/InputExceptionController.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+import baseball.validation.InputValidation;
+
+public class InputExceptionController {
+    private final InputValidation inputValidation = new InputValidation();
+
+    public void checkPlayerNumbersInputException(String input) {
+        if (inputValidation.isNotThreeLetters(input)
+                || inputValidation.isNot1To9(input)
+                || inputValidation.hasDuplicatedValues(input)) {
+            throw new IllegalArgumentException("1부터 9까지 서로 다른 3자리 수를 입력해야 합니다.");
+        }
+    }
+}
