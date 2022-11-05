bc8fc9e07774a7d5fe47ddb3da89d7883237dc43
pjw03659 <pjw03659@naver.com>
2022-11-05 16:46:03 +0900

    random 3digit creating function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static int[] makenumber(){
+        int[] randomNumber = new int[3];
+        for(int i=0;i<randomNumber.length;i++){
+            randomNumber[i]=(int) (Math.random()*10);
+            for(int j=0;j<i;j++){
+                if(randomNumber[i]==randomNumber[j]){
+                    i--;
+                }
+            }
+        }
+        return randomNumber;
     }
-}
+    public static void main(String[] args) {
+        int answer[]= new int[3];
+
+
+        }
+    }
+
