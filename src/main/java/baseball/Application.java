1afb7be10baea35fb8b21ce3f05993e99b5fd33a
chesthyeon <niw927@naver.com>
2022-11-05 20:18:25 +0900

    input 기능 수정 -> Console.readLined으로 입력받고 입력 받은 값을 올바른 값인지 확인하는 메소드(inputCheck) 추가.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,19 +1,28 @@
 package baseball;
-import camp.nextstep.edu.missionutils.Randoms;
+import  camp.nextstep.edu.missionutils.Randoms;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 import java.io.*;
 import java.util.*;
 
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
         System.out.println("숫자 야구 게임을 시작합니다.");
-        start();
+        while (true){
+            start();
+        }
     }
 
-    public static void start(){
+    public static void start() throws IllegalArgumentException{
         List<Integer> computerNumber = computerNumber();
-        List<Integer> userNumber = input();
-        List<Integer> strikeBallList = checkStrikeBall(computerNumber,userNumber);
+        List<Integer> strikeBallList;
+        String input;
+        int strike, ball;
+        while (true){
+            System.out.print("숫자를 입력해주세요 : ");
+            input = readLine();
+            inputCheck(input);
 
+        }
 
     }
 
@@ -28,21 +37,14 @@ public class Application {
         return computer;
     }
 
-    public static List<Integer> input() {
-        System.out.print("숫자를 입력해주세요 : ");
-        Scanner sc = new Scanner(System.in);
-        String inputNumber = sc.nextLine();
-        List<Integer> inputNumberList = new ArrayList<>();
-        boolean isNumeric = inputNumber.matches("[0-9]{3}"); //3글자 숫자로 이루어져있는지 확인하는 정규식
-
-        if (!isNumeric) {
-            throw new IllegalArgumentException("입력이 잘못 됐습니다.");
+    public static void inputCheck(String input) throws IllegalArgumentException {
+        Set<Character> overlapcheck = new HashSet<>();
+        for (int i = 0; i < input.length(); i++){
+            overlapcheck.add(input.charAt(i));
         }
-
-        for (int i = 0; i < inputNumber.length(); i++){
-            inputNumberList.add(Character.getNumericValue(inputNumber.charAt(i)));
-        }
-        return inputNumberList;
+        if (input.matches("[0-9]") && overlapcheck.size() == 3) return;
+        if (input.matches("[1-2]{1}"))  return;
+        new IllegalArgumentException("잘못된 입력입니다.");
     }
 
     public static List<Integer> checkStrikeBall(List<Integer> userNumber, List<Integer> computerNumber){
@@ -57,4 +59,23 @@ public class Application {
         }
         return Arrays.asList(strikeCount,ballCount - strikeCount);
     }
+
+    public static boolean endGame(int strike, int ball){ // 스트라이크와 볼의 수를 출력하고 스트라이크 == 3이면 true를 리턴한다.
+        if (strike == 0 && ball == 0) System.out.println("낫싱");
+        else if (strike != 0 && ball != 0){
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
+        else if (ball == 0) {
+            System.out.println(strike+"스트라이크");
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return true;
+            }
+        }
+        else {
+            System.out.println(ball+"볼");
+        }
+        return false;
+    }
+
 }
