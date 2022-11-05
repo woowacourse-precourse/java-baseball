0a42eaccdedf6c9d02c6aaeb2d4200f365bea325
BoyCho <iijokv@naver.com>
2022-11-05 20:53:25 +0900

    refactor : 클래스의 명확성을 위해 클래스명 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,17 +1,16 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import fixed.FixedList;
 
-public class AnswerNumber {
-
-    private static final int LENGTH = 3;
+public class Computer {
 
     // 3자리 숫자 생성하기
     public int[] getAnswerNum () {
-        int[] answer = new int[LENGTH];
+        int[] answer = new int[FixedList.LENGTH];
         int cnt = 0;
 
-        while (cnt < LENGTH) {
+        while (cnt < FixedList.LENGTH) {
             int randomNum = Randoms.pickNumberInRange(1,9);
             int checkedNum = checkDuplication(answer, randomNum);
 
@@ -25,7 +24,7 @@ public class AnswerNumber {
 
     // 중복 확인하기
     int checkDuplication(int[] answer, int randomNum) {
-        for (int i = 0; i < LENGTH; i++) {
+        for (int i = 0; i < FixedList.LENGTH; i++) {
             if (randomNum == answer[i]) {
                 randomNum = 0;
                 break;
@@ -38,5 +37,5 @@ public class AnswerNumber {
         return n != 0;
     }
 
-    public AnswerNumber() {}
+    public Computer() {}
 }
