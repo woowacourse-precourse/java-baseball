e3ae6560e892636780d7d441fad3d8cdd7603053
yeada <yeadalm@gmail.com>
2022-11-05 16:17:43 +0900

    feat 랜덤으로 숫자 3자리 숫자 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
+// 연습용 파일
+
 package baseball;
+import java.util.*;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> computer = makeNumber();
+    }
+
+    public static List<Integer> makeNumber(){
+        List<Integer> computer = new ArrayList<>();
+
+        while(computer.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+
+    public static List<Integer> user(){
+
     }
 }
