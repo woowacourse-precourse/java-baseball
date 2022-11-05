e944e20fd2d4cab7467947cb333a8f87b049dfa6
tseop <77163842+tseop@users.noreply.github.com>
2022-11-05 14:46:19 +0900

    Feat: 사용자 기능 클래스 생성 함수 생성 (#100)
    - 사용자 기능 클래스 생성 및 기능 함수 생성
    - main class에 사용자 입력 값 유효성 검사 함수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    private List<Integer> createUserList(String input) {
+        return null;
+    }
+
+
 }
---
+++ src/main/java/baseball/user/UserFunction.java
@@ -0,0 +1,35 @@
+package baseball.user;
+
+import java.util.List;
+
+public class UserFunction {
+
+    public boolean inputLengthCheck(String input) {
+        return false;
+    }
+
+
+    public boolean inputCheckInt(String input) {
+        return false;
+    }
+
+
+    public int[] inputToIntArr(String input) {
+        return new int[0];
+    }
+
+
+    public boolean intArrValidation(int[] intArr) {
+        return false;
+    }
+
+
+    public List<Integer> intArrToCollectionList(int[] intArr) {
+        return null;
+    }
+
+
+    public boolean collectionListValidation(List<Integer> list) {
+        return false;
+    }
+}
