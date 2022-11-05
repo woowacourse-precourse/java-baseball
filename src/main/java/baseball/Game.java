89607bc8305b9b49d4446f3d030344555145215a
kawkmin <86940335+kawkmin@users.noreply.github.com>
2022-11-05 17:12:13 +0900

    [feat]: 스트라이크 볼을 계산하고 출력하는 메서드 생성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -34,9 +34,30 @@ public class Game {
     }
 
     public void calGame() {
+        for (int i = 0; i < 3; i++) {
+            int index = computerNumber.indexOf(i);
+            if (index == userNumber.indexOf(i)) strike++;
+            else if (userNumber.contains(index)) ball++;
+        }
 
+        if (ball > 0) {
+            System.out.print(ball + "볼");
+            if (strike > 0) {
+                System.out.println(" " + strike + "스트라이크");
+            }
+        } else if (strike > 0) {
+            System.out.println(strike + "스트라이크");
+        } else{
+            System.out.println("낫싱");
+        }
+
+        if(strike==3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            gameContinue();
+        }
     }
-    public boolean gameContinue(){
+
+    public boolean gameContinue() {
         return true;
     }
 }
