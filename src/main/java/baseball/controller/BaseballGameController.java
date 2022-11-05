eb3a0c7b14185adf0947a322155daae80b7e5462
byeongsoon <byeongsoon94@gmail.com>
2022-11-05 12:26:41 +0900

    feat: 컨트롤러 구현 및 서비스 레이어 연결, 실행

---
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -0,0 +1,54 @@
+package baseball.controller;
+
+import baseball.printer.GameGuidePrinter;
+import baseball.service.BaseballGameService;
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseballGameController {
+
+    private BaseballGameService baseballGameService = new BaseballGameService();
+
+    private final int START = 1;
+    private final int END = 9;
+    private final int SIZE = 3;
+
+    private void init() {
+        baseballGameService.initGame(START, END, SIZE);
+    }
+
+    public void run() {
+        init();
+        start();
+        end();
+        retry();
+    }
+
+    private void start() {
+        baseballGameService.playGame();
+    }
+
+    private void end() {
+        GameGuidePrinter.endGameMessage();
+    }
+
+    private void retry() throws IllegalArgumentException{
+        GameGuidePrinter.retryGameMessage();
+        if (getInputNumber() == 1) {
+            run();
+        }
+    }
+
+    private int getInputNumber() {
+        String input = Console.readLine();
+
+        if (input.length() != 1) {
+            throw new IllegalArgumentException();
+        }
+        if (input.charAt(0) != '1' && input.charAt(0) != '2') {
+            throw new IllegalArgumentException();
+        }
+
+        return Integer.parseInt(input);
+    }
+
+}
