6d09ee23527ff31c7a7a535352fa905d1c8800f3
맹순영 <symaeng98@naver.com>
2022-11-05 13:30:42 +0900

    fix: 게임 시작 문구 출력 수정
    불필요한 함수 호출이라고 판단함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,12 +9,8 @@ public class Application {
     private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
 
     public static void main(String[] args) {
-        start();
-        List<Integer> answer = generateRandomNumber();
-    }
-
-    public static void start() {
         System.out.println(START_SENTENCE);
+        List<Integer> answer = generateRandomNumber();
     }
 
     public static List<Integer> generateRandomNumber() {
