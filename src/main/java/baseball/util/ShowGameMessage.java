4bceb4551b3ce310643d40df9ae50b0fc55151ed
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-05 15:21:43 +0900

    feat<ShowGameMessage>: 점수 결과 타입에 따라 메세지를 출력

--- src/main/java/baseball/util/ShowGameMessage.java
+++ src/main/java/baseball/util/ShowGameMessage.java
@@ -1,7 +1,14 @@
 package baseball.util;
 
+import baseball.dto.ScoreResultType;
+import baseball.dto.ScoreResult;
+
 public class ShowGameMessage {
-    private boolean isFirstTurn = true;
+    private boolean isFirstTurn;
+
+    public ShowGameMessage() {
+        isFirstTurn = true;
+    }
 
     public void gameStartMessage() {
         if (isFirstTurn) {
@@ -12,5 +19,44 @@ public class ShowGameMessage {
 
     public void inputUserNumberMessage() {
         System.out.print(GameMessage.USER_INPUT);
+    }
+
+    public void gameResultMessage(ScoreResult scoreResult) {
+        if (scoreResult.getType() == ScoreResultType.ALL_STRIKE) {
+            allStrikeMessage();
+            continueGameMessage();
+        } else if (scoreResult.getType() == ScoreResultType.STRIKE) {
+            strikeMessage(scoreResult.getStrike());
+        } else if (scoreResult.getType() == ScoreResultType.BALL) {
+            ballMessage(scoreResult.getBall());
+        } else if (scoreResult.getType() == ScoreResultType.STRIKE_BALL) {
+            strikeAndBallMessage(scoreResult.getStrike(), scoreResult.getBall());
+        } else {
+            nothingMessage();
+        }
+    }
+
+    private void allStrikeMessage() {
+        System.out.println(GameMessage.ALL_STRIKE);
+    }
+
+    private void nothingMessage() {
+        System.out.println(GameMessage.NOTHING);
+    }
+
+    private void ballMessage(int ball) {
+        System.out.printf(GameMessage.BALL, ball);
+    }
+
+    private void strikeMessage(int strike) {
+        System.out.printf(GameMessage.STRIKE, strike);
+    }
+
+    private void strikeAndBallMessage(int strike, int ball) {
+        System.out.printf(GameMessage.STRIKE_BALL, strike, ball);
+    }
+
+    private void continueGameMessage() {
+        System.out.println(GameMessage.CONTINUE_GAME);
     }
 }
