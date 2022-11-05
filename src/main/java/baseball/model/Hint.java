e550f29d192c8741f599509dbd0abec3779c3f06
SHEOMM <esh0821@snu.ac.kr>
2022-11-06 00:36:27 +0900

    fix(Hint): remove unnecessary new line
    toString 함수에서 불필요한 줄나눔 기호를 제거

--- src/main/java/baseball/model/Hint.java
+++ src/main/java/baseball/model/Hint.java
@@ -15,12 +15,16 @@ public class Hint {
     @Override
     public String toString() {
         if(ball == 0 && strike == 0){
-            return "낫싱\n";
+            return "낫싱";
         }else if(ball == 0){
-            return strike + "스트라이크\n";
+            return strike + "스트라이크";
         }else if(strike == 0){
-            return ball + "볼\n";
+            return ball + "볼";
         }
-        return ball + "볼 " + strike + "스트라이크\n";
+        return ball + "볼 " + strike + "스트라이크";
+    }
+
+    public int getStrike() {
+        return strike;
     }
 }
