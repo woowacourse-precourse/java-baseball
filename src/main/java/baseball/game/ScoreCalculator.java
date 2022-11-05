a01c45de3283d992e733eef0ab619d03df61a1cf
wjh2012 <wjh2012@naver.com>
2022-11-05 17:05:25 +0900

    feat(calculator): add ScoreCalculator 숫자를 비교하는 기능을 static 클래스로 분리 return ScoreBoard

---
+++ src/main/java/baseball/game/ScoreCalculator.java
@@ -0,0 +1,23 @@
+package baseball.game;
+
+import java.util.List;
+
+public class ScoreCalculator {
+
+    public static ScoreBoard calculate(List<Integer> targetNumbers, List<Integer> requestNumbers) {
+        ScoreBoard scoreBoard = new ScoreBoard();
+
+        for (int number : requestNumbers) {
+            if (targetNumbers.indexOf(number) == requestNumbers.indexOf(number)) {
+                scoreBoard.addStrike();
+                continue;
+            }
+
+            if (targetNumbers.contains(number)) {
+                scoreBoard.addBall();
+            }
+        }
+
+        return scoreBoard;
+    }
+}
