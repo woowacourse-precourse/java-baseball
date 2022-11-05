52b3f20204081686f1f1e6c37dce367b9b2b9e73
MoonJeWoong <munjin0201@naver.com>
2022-11-06 01:56:05 +0900

    feat(core): printGameResult 메서드 구현
    매개변수로 스트라이크와 볼 개수가 주어지면 그 결과를 출력하는 메서드.
    스트라이크 혹은 볼 중 0이 입력으로 들어온 것은 출력하지 않는다.

--- src/main/java/baseball/Core.java
+++ src/main/java/baseball/Core.java
@@ -94,4 +94,26 @@ public class Core {
         }
         return integerList;
     }
+
+    public void printGameResult(int strikes, int balls){
+        if(strikes==3){
+            System.out.println("3스트라이크");
+            return;
+        }
+        if(strikes==0 && balls==0){
+            System.out.println("낫싱");
+            return;
+        }
+        if(strikes>0 && balls>0){
+            System.out.printf("%d볼 %d스트라이크\n", balls, strikes);
+            return;
+        }
+        if(strikes>0){
+            System.out.printf("%d스트라이크\n",strikes);
+            return;
+        }
+        if(balls>0){
+            System.out.printf("%d볼\n",balls);
+        }
+    }
 }
