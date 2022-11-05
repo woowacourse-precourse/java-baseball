0b1701d1931c01c2842de33321b05ff42faf9cb1
Jzakka <mouse4786@gmail.com>
2022-11-05 21:57:23 +0900

    feat: 게임 실행 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import baseball.domain.Computer;
+import baseball.domain.User;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    private static User user = new User();
+    private static final Character RESTART = '1';
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game game = new GameContext().game();
+        Character retry = RESTART;
+
+        while (retry == RESTART) {
+            game.start(new Computer(), user);
+            retry = user.inputRetry();
+        }
     }
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,21 +7,32 @@ import baseball.logic.GameLogic;
 import baseball.screen.OutputResolver;
 
 public class Game {
-    Computer computer = new Computer();
-    User user = new User();
-    GameLogic gameLogic = new GameLogic();
-    Result result = new Result(-1, -1);
+    GameLogic gameLogic;
+    OutputResolver outputResolver;
 
-    OutputResolver outputResolver = new OutputResolver();
-    public void start() {
+    public Game(GameLogic gameLogic, OutputResolver outputResolver) {
+        this.gameLogic = gameLogic;
+        this.outputResolver = outputResolver;
+    }
+
+    public void start(Computer computer, User user) {
         gameLogic.setUp(computer.getNumbers());
 
+        outputResolver.print("숫자 야구 게임을 시작합니다.\n");
+
+        play(user);
+
+        outputResolver.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+    }
+
+    private void play(User user) {
+        Result result = new Result(-1, -1);
+
         while (result.getStrike() != 3) {
-            user.input();
+            outputResolver.print("숫자를 입력해주세요 : ");
+            user.inputNumbers();
             gameLogic.process(user.getNumbers(), result);
             outputResolver.print(result);
         }
     }
-
-
 }
