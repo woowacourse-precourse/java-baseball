2a660a7c66ab703febf3abc7a55981ac962b597b
math0917 <math0917@naver.com>
2022-11-05 17:55:44 +0900

    feat: 반환 valid userInput

--- src/main/java/baseball/user/User.java
+++ src/main/java/baseball/user/User.java
@@ -5,12 +5,15 @@ import baseball.input.Input;
 import baseball.output.Print;
 import baseball.validation.Validation;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class User {
     public static List<Integer> createUserNonDuplicateIntegerList() {
         Print.printInline(GameProcedureConstantString.USER_SIDE_BASEBALL_CREATE_MESSAGE.getStringMessage());
         String userInput = Input.inputString();
         Validation.userBaseballInputValidation(userInput);
-        return null;
+        return userInput.chars()
+                .mapToObj(charDigit -> charDigit - '0')
+                .collect(Collectors.toList());
     }
 }
