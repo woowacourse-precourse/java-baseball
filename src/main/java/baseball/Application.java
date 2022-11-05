9f8d014a709319b13c9fe5cd63f3e1e4b40ca2e0
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-05 22:39:57 +0900

    feat(method): add gameReadyComplete
    function list 1-4
    add gameReadyComplete method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -40,6 +40,11 @@ class GameReadyMachine {
             return false;
     }
 
+    public void gameReadyComplete()
+    {
+        this.setAnswerValue();
+        System.out.printf("숫자 야구 게임을 시작합니다.\n");
+    }
 
 }
 
