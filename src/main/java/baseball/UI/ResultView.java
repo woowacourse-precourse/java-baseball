0319540b8887e8c14fdd9267f5086bfa44979d11
SHEOMM <esh0821@snu.ac.kr>
2022-11-06 00:43:29 +0900

    fix(ResultView): change result's basic value to Result.RESTART
    ResultView 함수의 result가 기본 value를 Restart로 가지도록 변경

--- src/main/java/baseball/UI/ResultView.java
+++ src/main/java/baseball/UI/ResultView.java
@@ -5,23 +5,22 @@ import baseball.model.State;
 import camp.nextstep.edu.missionutils.Console;
 
 public class ResultView {
-    private Result result;
+    private Result result = Result.RESTART;
+
     public void printResult(State state){
         switch (state){
             case FINISH:
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 getResultNumber();
                 break;
-            case ERROR:
-                throw new IllegalArgumentException("제대로 된 input을 입력해주세요");
             case PROCEEDING:
                 System.out.println("게임이 아직 종료되지 않았습니다.");
         }
     }
 
-    private void getResultNumber(){
+    public void getResultNumber(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        int intResult = -1;
+        int intResult;
         try{
             intResult = Integer.parseInt(Console.readLine());
         }catch (NumberFormatException e){
