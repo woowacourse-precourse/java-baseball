61fc9bd2d72af74db781997e34bc2600d0d005cc
은지 <midzy0228@gmail.com>
2022-11-05 17:03:06 +0900

    feat: 스트라이크와 볼의 갯수 카운트

--- src/main/java/baseball/Compare.java
+++ src/main/java/baseball/Compare.java
@@ -1,6 +1,6 @@
 package baseball;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static baseball.Constant.*;
@@ -12,6 +12,21 @@ public class Compare {
     Compare(List<Integer> computer, List<Integer> user) {
         this.computerNumbers = computer;
         this.userNumbers = user;
+    }
+
+    public List<Integer> CountScore() {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i=0; i<PLAYING_NUMBERS_LENGTH; i++) {
+            if (isStrike(computerNumbers.get(i), userNumbers.get(i))) {
+                strike += 1;
+            }
+            if (isBall(computerNumbers, userNumbers.get(i))) {
+                ball += 1;
+            }
+        }
+        return Arrays.asList(strike, ball);
     }
 
     public boolean isStrike(Integer computerNumber, Integer userNumber) {
