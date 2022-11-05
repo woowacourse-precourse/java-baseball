0688865560620e5d7759f8de207a6a0267f321c8
kang jihyeong <her4386@gmail.com>
2022-11-06 01:46:18 +0900

    Feat / 맞춘 개수 Enum 작성

---
+++ src/main/java/baseball/ui/CorrectEnum.java
@@ -0,0 +1,14 @@
+package baseball.ui;
+
+public enum CorrectEnum {
+
+    STRIKE("스트라이크"),
+    BALL("볼"),
+    NOTHING("낫싱");
+
+    private final String correct;
+
+    CorrectEnum(String correct) {
+        this.correct = correct;
+    }
+}
