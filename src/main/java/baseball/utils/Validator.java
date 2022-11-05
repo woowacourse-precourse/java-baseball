f94708538d77e3f1cbbc5ceb6d410cfb90d7f781
saint6839 <saint6839@gmail.com>
2022-11-05 23:03:56 +0900

    feat(utils) : 사용자 입력값 검증을 위한 Validator 클래스 생성

---
+++ src/main/java/baseball/utils/Validator.java
@@ -0,0 +1,22 @@
+package baseball.utils;
+
+import baseball.Ball;
+
+import java.util.List;
+
+public final class Validator {
+    private Validator() {}
+
+    public static void validateParseStringToInt(String playerInput) {
+
+    }
+
+    public static void validateInputLength(String playerInput) {
+
+    }
+
+
+    public static void validateDuplication(List<Ball> playerBalls, Ball ball) {
+
+    }
+}
