d07534771075230dc719d7c0f54c831013183926
0chil <0@chll.it>
2022-11-06 02:09:12 +0900

    test(ThreeDigitsTest): 정상 값

--- src/test/java/baseball/ThreeDigitsTest.java
+++ src/test/java/baseball/ThreeDigitsTest.java
@@ -10,12 +10,20 @@ import org.junit.jupiter.api.Test;
 public class ThreeDigitsTest {
 
     @Test
-    @DisplayName("숫자는 세 자리다")
-    void digits_are_length_of_three() {
+    @DisplayName("세 자리가 아니면 예외를 던진다")
+    void exception_on_digits_length_is_three() {
         List<Integer> twoDigits = List.of(1, 2);
         List<Integer> fourDigits = List.of(1, 2, 3, 4);
 
         assertThatIllegalArgumentException().isThrownBy(() -> new ThreeDigits(twoDigits));
         assertThatIllegalArgumentException().isThrownBy(() -> new ThreeDigits(fourDigits));
+    }
+
+    @Test
+    @DisplayName("숫자는 세 자리다")
+    void digits_length_is_three() {
+        List<Integer> threeDigits = List.of(1, 2, 3);
+
+        assertThatNoException().isThrownBy(() -> new ThreeDigits(threeDigits));
     }
 }
