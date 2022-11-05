07a98d711d0a870212350039694fb7a0f76b27be
JeonghakLee <wjdgkr0527@naver.com>
2022-11-05 15:11:15 +0900

    feat: 게임 종료 or 재시작 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.service.InputService;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+  private static final InputService inputService = new InputService();
+
+  public static void main(String[] args) {
+    // TODO: 프로그램 구현
+    do {
+      GameController gameController = new GameController();
+      gameController.play();
+    } while (inputService.DoesPlayerWantToRestart());
+  }
 }
