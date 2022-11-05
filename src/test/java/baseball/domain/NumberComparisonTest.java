b7e2365278bee579d9f63817bf20693c5adb69db
alstjr706 <alstjr706@naver.com>
2022-11-05 13:41:05 +0900

    test : (숫자 야구) 테스트 코드 asList에서 List.of() 코드 수정

--- src/test/java/baseball/domain/NumberComparisonTest.java
+++ src/test/java/baseball/domain/NumberComparisonTest.java
@@ -3,7 +3,7 @@ package baseball.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,28 +19,28 @@ class NumberComparisonTest {
     @Test
     void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_모두_맞을_경우() {
         int result = 3;
-        int compare = numberComparison.compare(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
+        int compare = numberComparison.compare(List.of(1, 2, 3), List.of(1, 2, 3));
         assertThat(compare).isEqualTo(result);
     }
 
     @Test
     void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_2개_맞을_경우() {
         int result = 2;
-        int compare = numberComparison.compare(Arrays.asList(1, 2, 5), Arrays.asList(1, 2, 3));
+        int compare = numberComparison.compare(List.of(1, 2, 3), List.of(1, 2, 5));
         assertThat(compare).isEqualTo(result);
     }
 
     @Test
     void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_1개_맞을_경우() {
         int result = 1;
-        int compare = numberComparison.compare(Arrays.asList(1, 9, 5), Arrays.asList(1, 2, 3));
+        int compare = numberComparison.compare(List.of(1, 2, 3), List.of(1, 9, 5));
         assertThat(compare).isEqualTo(result);
     }
 
     @Test
     void 컴퓨터와_플레이어_사이에_서로_포함된_숫자가_없을_경우() {
         int result = 0;
-        int compare = numberComparison.compare(Arrays.asList(4, 9, 5), Arrays.asList(1, 2, 3));
+        int compare = numberComparison.compare(List.of(1, 2, 3), List.of(4, 9, 5));
         assertThat(compare).isEqualTo(result);
     }
 }
--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -3,7 +3,7 @@ package baseball.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,63 +19,49 @@ class RefereeTest {
     @Test
     void 스트라이크3_맞는지_확인() {
         String result = "3스트라이크";
-        String judgment = referee.resultJudgment(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3));
-        assertThat(judgment).isEqualTo(result);
-    }
-
-    @Test
-    void 스트라이크2_맞는지_확인() {
-        String result = "2스트라이크";
-        String judgment = referee.resultJudgment(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 7));
+        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(1, 2, 3));
         assertThat(judgment).isEqualTo(result);
     }
 
     @Test
     void 스트라이크1_맞는지_확인() {
         String result = "1스트라이크";
-        String judgment = referee.resultJudgment(Arrays.asList(1, 2, 3), Arrays.asList(1, 5, 7));
+        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(1, 5, 7));
         assertThat(judgment).isEqualTo(result);
     }
 
     @Test
     void 볼3_맞는지_확인() {
         String result = "3볼";
-        String judgment = referee.resultJudgment(Arrays.asList(1, 2, 3), Arrays.asList(3, 1, 2));
-        assertThat(judgment).isEqualTo(result);
-    }
-
-    @Test
-    void 볼2_맞는지_확인() {
-        String result = "2볼";
-        String judgment = referee.resultJudgment(Arrays.asList(1, 2, 3), Arrays.asList(7, 3, 2));
+        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(3, 1, 2));
         assertThat(judgment).isEqualTo(result);
     }
 
     @Test
     void 볼1_맞는지_확인() {
         String result = "1볼";
-        String judgment = referee.resultJudgment(Arrays.asList(1, 2, 3), Arrays.asList(7, 6, 1));
+        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(7, 6, 1));
         assertThat(judgment).isEqualTo(result);
     }
 
     @Test
     void 볼1_스트라이크1_맞는지_확인() {
         String result = "1볼 1스트라이크";
-        String judgment = referee.resultJudgment(Arrays.asList(1, 2, 3), Arrays.asList(5, 1, 3));
+        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(5, 1, 3));
         assertThat(judgment).isEqualTo(result);
     }
 
     @Test
     void 볼2_스트라이크1_맞는지_확인() {
         String result = "2볼 1스트라이크";
-        String judgment = referee.resultJudgment(Arrays.asList(1, 2, 3), Arrays.asList(1, 3, 2));
+        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(1, 3, 2));
         assertThat(judgment).isEqualTo(result);
     }
 
     @Test
     void 낫싱_맞는지_확인() {
         String result = "낫싱";
-        String judgment = referee.resultJudgment(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6));
+        String judgment = referee.resultJudgment(List.of(1, 2, 3), List.of(4, 5, 6));
         assertThat(judgment).isEqualTo(result);
     }
 }
