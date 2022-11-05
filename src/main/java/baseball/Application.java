78a940a3cf840b7c2a649701632610f1e4907656
ineiw <75738349+ineiw@users.noreply.github.com>
2022-11-05 16:35:55 +0900

    feat: 메인함수 작성
    메인함수 프로그램을 작성시작
    Computer 클래스를 생성하고
    게임 시작 출력 메세지와 while 문등 기본 구조 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Computer computer = new Computer();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String playerInput = Console.readLine();
+            String computerAnswer = computer.getRandomNumber().toString();
+        }
     }
 }
