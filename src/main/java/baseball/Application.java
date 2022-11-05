d6963b75aca5745a801d6abf2ef5425c47689459
devjordy14 <devjordy14@gmail.com>
2022-11-05 21:36:58 +0900

    feat(receiveInputOfPlayer): add new features and remove try-catch
    Added new features to receiveInputOfPlayer:
    - checkLengthOfString function to check the length of String is 3 or not
    - changeStringToIntegerList function to replace the String value with List of Integer
    makes easier to manage and check the duplication
    - checkDuplicationFromIntegerList function to check the duplication of numbers from user input
    - removed try-catch from receiveInputOfPlayer because I found it was useless
    So I moved try-catch(IllegalArgumentException) to checkLengthOfString,
    checkDuplicationFromIntegerList() and changeStringToIntegerList()
    which are more appropriate function to error-handling

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,16 +4,17 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        List<Integer> opponent = new ArrayList<>();
-        opponent = generateOpponent();
+        List<Integer> opponent = generateOpponent();
+        List<Integer> player = receiveInputOfPlayer();
 
-        String player = receiveInputOfPlayer();
     }
 
     public static List<Integer> generateOpponent(){
@@ -27,13 +28,33 @@ public class Application {
         return computer;
     }
 
-    public static String receiveInputOfPlayer(){
+    public static List<Integer> receiveInputOfPlayer(){
         System.out.print("숫자를 입력해주세요 : ");
+        String inputString = Console.readLine();
+        List<Integer> integerList = changeStringToIntegerList(inputString);
+        checkDuplicationFromIntegerList(integerList);
+        return integerList;
+    }
+
+    public static Boolean checkDuplicationFromIntegerList(List<Integer> checkList){
+        List<Integer> newList = new ArrayList<>();
+        for (Integer number : checkList) {
+            if (!newList.contains(number)) {
+                newList.add(number);
+            }
+        }
+        if(newList != checkList){
+            throw new IllegalArgumentException("중복된 숫자가 입력되었습니다.\n프로그램을 종료합니다.");
+        }
+        return true;
+    }
+
+    public static List<Integer> changeStringToIntegerList(String numberString){
         try {
-            String randomNumber = Console.readLine();
-            return randomNumber;
-        }catch(IllegalStateException exception){
-            throw new IllegalStateException(exception);
+            List<Integer> integerList = Arrays.stream(numberString.split("")).map(Integer::parseInt).collect(Collectors.toList());
+            return integerList;
+        }catch(IllegalArgumentException e){
+            throw new IllegalArgumentException("숫자가 아닌 값이 입력되었습니다.\n프로그램을 종료합니다.");
         }
     }
 }
