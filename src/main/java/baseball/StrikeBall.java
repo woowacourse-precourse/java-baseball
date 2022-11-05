3f78fcdadf7e31b2b10a173859cc2a7ff244d260
GILHUN <gilhun@GILHUNs-MacBook-Air.local>
2022-11-05 18:14:22 +0900

    StrikeBall.class generate  Generate StrikeBall.class for print result

---
+++ src/main/java/baseball/StrikeBall.java
@@ -0,0 +1,46 @@
+package baseball;
+
+public class StrikeBall {
+    private int strike;
+    private int ball;
+
+    StrikeBall(){
+        strike = 0;
+        ball = 0;
+    }
+
+    StrikeBall(int strike, int ball){
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+
+    public String toString(){
+        String output = "";
+        if(strike!=0 && ball!=0)
+            output = ball+"볼 "+strike+"스트라이크";
+        else if(strike!=0)
+            output = strike+"스트라이크";
+        else if(ball!=0)
+            output = ball+"볼";
+        else
+            output = "낫싱";
+
+        return output;
+    }
+}
