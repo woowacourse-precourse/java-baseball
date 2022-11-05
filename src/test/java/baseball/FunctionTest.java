5a6188e6b575604a76e2b7a9c19a90a13ec78fb2
ssssudan <kankgm0324@gmail.com>
2022-11-05 21:11:59 +0900

    <test>: 컴퓨가 랜덤 번호의 각 자리가 서로 다른 숫자인지 검사하는 테스트

--- src/test/java/baseball/FunctionTest.java
+++ src/test/java/baseball/FunctionTest.java
@@ -19,4 +19,15 @@ public class FunctionTest {
             assertThat(comNumber.get(i)).isGreaterThan(0).isLessThan(10);
         }
     }
+
+    @DisplayName("컴퓨가 랜덤 번호의 각 자리가 서로 다른 숫자인지 검사")
+    @Test
+    void RandomNumberTest() {
+        ComputerRandomNumber computerRandomNumber = new ComputerRandomNumber();
+        List<Integer> comNumber = computerRandomNumber.makeComputerNumber();
+
+        assertThat(comNumber.get(0)).isNotEqualTo(comNumber.get(1));
+        assertThat(comNumber.get(0)).isNotEqualTo(comNumber.get(2));
+        assertThat(comNumber.get(1)).isNotEqualTo(comNumber.get(2));
+    }
 }
