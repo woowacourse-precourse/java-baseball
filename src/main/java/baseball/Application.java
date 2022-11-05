c4d21ba4578adbd520e8f44c63e9cbfa95134cac
moonchaewon <codnjs8989@gmail.com>
2022-11-05 14:15:49 +0900

    refactor(progress): separate restart function from start function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -90,6 +90,10 @@ public class Application {
             List<Integer> playerNumber = inputPlayerNumber();
             isFinish = getNumberHint(randomNumber, playerNumber);
         }
+        restartGame();
+    }
+
+    private static void restartGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String gameProgress = Console.readLine();
         if (gameProgress.equals(Application.GAME_RESTART))    startGame();
