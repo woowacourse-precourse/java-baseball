6f7304e184445d0abb38d8f3cf0197cfbdb66944
5witchkr <hob4419@gmail.com>
2022-11-06 07:14:08 +0900

    test(baseball/NumberMatchProcessorTest): 야구게임의 숫자비교기능 테스트 코드

---
+++ src/main/java/baseball/NumberMatchProcessorImpl.java
@@ -0,0 +1,13 @@
+package baseball;
+
+
+import java.util.List;
+
+public class NumberMatchProcessorImpl implements NumberMatchProcessor{
+
+    @Override
+    public List<Integer> compare(int gameNum, int userNum) {
+
+        return null;
+    }
+}
---
+++ src/test/java/baseball/NumberMatchProcessorTest.java
@@ -0,0 +1,84 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+public class NumberMatchProcessorTest {
+
+    private NumberMatchProcessor numberMatchProcessor;
+
+    @TestFactory
+    @DisplayName("야구숫자게임의 숫자비교기능을 테스트한다.")
+    Stream<DynamicTest> numberMatchTest() {
+        final int gameNumber = 123;
+        numberMatchProcessor = new NumberMatchProcessorImpl();
+
+        return Stream.of(
+                DynamicTest.dynamicTest("하나도 일치하지 않는 경우", () -> {
+
+                    List<Integer> result = numberMatchProcessor.compare(gameNumber, 456);
+
+                    assertThat(result).contains(0);
+                    assertThat(result).containsExactly(0, 0);
+                    assertThat(result.size()).isEqualTo(2);
+
+                }),
+                DynamicTest.dynamicTest("1스트라이크인 경우", () -> {
+
+                    List<Integer> result = numberMatchProcessor.compare(gameNumber, 333);
+
+                    assertThat(result).contains(0, 1);
+                    assertThat(result).containsExactly(1, 0);
+                    assertThat(result.size()).isEqualTo(2);
+
+                }),
+                DynamicTest.dynamicTest("1볼인 경우", () -> {
+
+                    List<Integer> result = numberMatchProcessor.compare(gameNumber, 442);
+
+                    assertThat(result).contains(0, 1);
+                    assertThat(result).containsExactly(0, 1);
+                    assertThat(result.size()).isEqualTo(2);
+
+                }),
+                DynamicTest.dynamicTest("1볼 1스트라이크인 경우", () -> {
+
+                    List<Integer> result = numberMatchProcessor.compare(gameNumber, 152);
+
+                    assertThat(result).contains(1, 1);
+                    assertThat(result).containsExactly(1, 1);
+                    assertThat(result.size()).isEqualTo(2);
+
+                }),
+                DynamicTest.dynamicTest("3개의 숫자를 모두 맞힐 경우", () -> {
+
+                    List<Integer> result = numberMatchProcessor.compare(gameNumber, 123);
+
+                    assertThat(result).contains(0, 3);
+                    assertThat(result).containsExactly(3, 0);
+                    assertThat(result.size()).isEqualTo(2);
+
+                }),
+                DynamicTest.dynamicTest("입력값이 3자리숫자가 아닌경우 예외를 리턴한다. - case 4자리 숫자", () -> {
+                    assertThatThrownBy(() -> numberMatchProcessor.compare(gameNumber, 1376))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("3자리 숫자만 입력 가능합니다.");
+
+                }),
+                DynamicTest.dynamicTest("입력값이 3자리숫자가 아닌경우 예외를 리턴한다 - case 2자리 숫자.", () -> {
+                    assertThatThrownBy(() -> numberMatchProcessor.compare(gameNumber, 97))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("3자리 숫자만 입력 가능합니다.");
+                })
+        );
+    }
+}
