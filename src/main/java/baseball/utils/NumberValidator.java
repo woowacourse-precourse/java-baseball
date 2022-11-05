0a762f34d281ddcd6607de2bf2a5aac3a9c24366
Jin409 <sualng123@gmail.com>
2022-11-05 18:05:24 +0900

    refactor(NumberValidator): 멤버 상수에서 전역 상수를 사용하도록 수정

--- src/main/java/baseball/utils/NumberValidator.java
+++ src/main/java/baseball/utils/NumberValidator.java
@@ -1,15 +1,11 @@
 package baseball.utils;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class NumberValidator {
-    private static final int VALID_LENGTH = 3;
-    private static final int VALID_MIN_RANGE = 1;
-    private static final int VALID_MAX_RANGE = 9;
     private static final String LENGTH_ERROR_MESSAGE = "숫자는 세 자리 수여야 합니다.";
     private static final String RANGE_ERROR_MESSAGE = "각 자리 수는 최소 1 이상, 최대 9 이하의 정수여야 합니다.";
     private static final String DUPLICATE_ERROR_MESSAGE = "숫자는 중복되어서는 안됩니다.";
@@ -22,24 +18,23 @@ public class NumberValidator {
         return true;
     }
 
-    public static boolean hasValidType(String userInputStr){
+    public static void hasValidType(String userInputStr){
         for(int i=0;i<userInputStr.length();i++){
             char c = userInputStr.charAt(i);
             if(!Character.isDigit(c)){
                 throw new IllegalArgumentException(TYPE_ERROR_MESSAGE);
             }
         }
-        return true;
     }
 
     private static void hasValidLength(List<Integer> userInput) {
-        if (!(userInput.size() == VALID_LENGTH)) {
+        if (!(userInput.size() == Constants.VALID_LENGTH)) {
             throw new IllegalArgumentException(LENGTH_ERROR_MESSAGE);
         }
     }
 
     private static void hasValidRange(List<Integer> userInput) {
-        if (!(Collections.max(userInput) <= VALID_MAX_RANGE && Collections.min(userInput) >= VALID_MIN_RANGE)) {
+        if (!(Collections.max(userInput) <= Constants.VALID_MAX_RANGE && Collections.min(userInput) >= Constants.VALID_MIN_RANGE)) {
             throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
         }
     }
