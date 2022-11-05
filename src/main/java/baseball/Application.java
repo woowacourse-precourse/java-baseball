588449e102074a18747c51d39902dea25b40a84a
seoyoung <sy9723@korea.ac.kr>
2022-11-06 02:26:25 +0900

    feat: check each digit of the input and handle exception

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -35,7 +35,14 @@ public class Application {
         }
 
         int intInput = Integer.parseInt(strInput);
-        
+        int[] arrInput = Stream.of(String.valueOf(intInput).split("")).mapToInt(Integer::parseInt).toArray();
+        int[] check = new int[10];
+        for (int i = 0; i < strInput.length(); i++) {
+            if (check[arrInput[i]] == 1) { // 2. 각 자리수의 값이 중복이 있는 경우
+                throw new IllegalArgumentException();
+            }
+            check[arrInput[i]] = 1;
+        }
 
         return intInput;
     }
