00a98d00da05737781ea0a62343070de5e823c6f
matt <matt1235@naver.com>
2022-11-05 18:59:59 +0900

    feat: 3자리수 생성 기능 구현 랜덤하게 3자리의 임의의 다른 수를 생성하는 기능을 구현

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class BaseballGame {
+
+    private final RandomNumberGenerator randomNumberGenerator;
+
+    public BaseballGame(RandomNumberGenerator randomNumberGenerator) {
+        this.randomNumberGenerator = randomNumberGenerator;
+    }
+}
