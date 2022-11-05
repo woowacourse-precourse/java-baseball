0bcc18f9c0aaa1d07bdb9cabe235a86ea19d6604
Jeonsoyeon <itso22@naver.com>
2022-11-05 16:44:29 +0900

    fix(Application): 오타수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,6 @@ import domain.Game;
 public class Application {
     public static void main(String[] args) {
         Game game = new Game();
-        Game.startGame();
+        game.startGame();
     }
 }
