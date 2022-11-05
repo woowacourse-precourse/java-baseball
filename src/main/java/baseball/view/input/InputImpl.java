4679d8db18b11509a2db7b4db44ed60f8a81f7c3
5uhwann <5uhwann27@gmail.com>
2022-11-05 15:49:57 +0900

    feat(검증): 서로 다른 3자리 숫자 입력 검증 구현
    3자리 숫자 입력 검증
    각 자릿수가 1~9의 서로다른 숫자인지 검증

--- src/main/java/baseball/view/input/InputImpl.java
+++ src/main/java/baseball/view/input/InputImpl.java
@@ -2,17 +2,44 @@ package baseball.view.input;
 
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class InputImpl implements Input {
 
+    private final String REGEX = "^[1-9]{3}$";
+
     @Override
     public String baseballInput() {
-        return Console.readLine();
+        String baseballInput = Console.readLine();
+        if (isAllowedBaseballInput(baseballInput) && isEachNumberDifferent(baseballInput)) {
+            return baseballInput;
+        }
+        return null;
     }
 
     @Override
     public String endInput() {
         return Console.readLine();
+    }
+
+    private boolean isAllowedBaseballInput(String baseballInput) throws IllegalArgumentException {
+        if (baseballInput.matches(REGEX)) {
+            return true;
+        }
+        throw new IllegalArgumentException("서로다른 3자리 숫자만 입력 가능합니다.");
+    }
+
+    private boolean isEachNumberDifferent(String baseballInput) throws IllegalArgumentException{
+        String[] baseballNumber = baseballInput.split("");
+        List<String> distinctNumber = Arrays.stream(baseballNumber)
+                .distinct()
+                .collect(Collectors.toList());
+        if (distinctNumber.size() == 3) {
+            return true;
+        }
+        throw new IllegalArgumentException("서로다른 3자리 숫자만 입력 가능합니다.");
     }
 
 }
