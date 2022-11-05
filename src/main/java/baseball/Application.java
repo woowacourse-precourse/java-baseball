ca972e727a3484bb9cf1c622ed033ce80cc0a163
DongWooE <ehddn977@naver.com>
2022-11-06 01:38:59 +0900

    [feat] makeRandomNumber 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -7,5 +9,28 @@ public class Application {
 
     public void printStartGame(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public int makeRandomNumber(){
+        int n = 100, value = 0;
+        boolean [] check = new boolean[10];
+        while(n >= 1){
+            int i = getRandomNumber(check);
+            value += i*n;
+            n/=10;
+        }
+        return value;
+    }
+
+    private int getRandomNumber(boolean[] check) {
+        int i;
+        do{
+            i = Randoms.pickNumberInRange(1, 9);
+            if(!check[i]){
+                check[i] = true;
+                break;
+            }
+        }while(true);
+        return i;
     }
 }
--- src/test/java/baseball/UnitTest.java
+++ src/test/java/baseball/UnitTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class UnitTest {
@@ -13,5 +14,27 @@ class UnitTest {
         application.printStartGame();
 
         //then
+    }
+
+    @Test
+    public void makeRandomNumber_정상_테스트(){
+        //given
+        Application application = new Application();
+
+        //when
+        int result = application.makeRandomNumber();
+        boolean state = checkIfEachDifferent(result);
+
+        //then
+        Assertions.assertThat(result).isGreaterThanOrEqualTo(100);
+        Assertions.assertThat(result).isLessThanOrEqualTo(999);
+        Assertions.assertThat(state).isEqualTo(true);
+    }
+
+    private boolean checkIfEachDifferent(int result){
+        int i1 = result/100;
+        int i2 = (result % 100) / 10;
+        int i3 = (result%10);
+        return (i1 != i2) && (i2 != i3) && (i1 != i3);
     }
 }
