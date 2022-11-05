f1ef7089406e3cdc84e04d2e1e4656daad8c11b7
Moon-Tree <ellaya1012@gmail.com>
2022-11-05 17:51:38 +0900

    Feat : 1부터 9까지 서로 다른 3자리의 수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,13 @@
 package baseball;
 
+import java.util.Random;
+
 public class Application {
+
+    public static int randomNumber() {
+        Random random = new Random();
+        return random.nextInt(9) + 1;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
