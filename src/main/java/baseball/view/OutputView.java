d48cd9ba662a876b340537e9f65acad082fedcf2
heesang <dlagmltkd12@naver.com>
2022-11-06 01:58:58 +0900

    feat: 게임 재시작 여부 멘트 출력 기능

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,12 +6,14 @@ public class OutputView {
 
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
     public static final String STRIKE_MESSAGE = "%d스트라이크\n";
     public static final String NOTHING_MESSAGE = "낫싱";
     public static final String BALL_MESSAGE = "%d볼\n";
     public static final String BALL_STRIKE_MESSAGE = "%d볼 %d스트라이크\n";
-    public static final int CRITERION_ZERO = 0;
+
     public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void printGameStart() {
         System.out.println(GAME_START_MESSAGE);
@@ -54,6 +56,10 @@ public class OutputView {
 
     public static void printGameEnd() {
         System.out.println(GAME_END_MESSAGE);
+    }
+
+    public static void printGameRestart() {
+        System.out.println(GAME_RESTART_MESSAGE);
     }
 
 }
