548bdd36ae9085b5625d371109cee53afdb46c84
MINJU <alswn9938@gmail.com>
2022-11-05 18:53:33 +0900

    feat($Application): add 사용자 입력 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -10,16 +11,16 @@ public class Application {
 
     }
 
-    public List<Integer> getComputerNumberList(){
+    public List<Integer> getComputerNumberList() {
         ArrayList<Integer> computerNumberList = new ArrayList<>();
-        while(computerNumberList.size() < 3){
+        while (computerNumberList.size() < 3) {
             int randomNumber = getValidRandomNumber(computerNumberList);
             computerNumberList.add(randomNumber);
         }
         return computerNumberList;
     }
 
-    private int getValidRandomNumber(List<Integer> lst){
+    private int getValidRandomNumber(List<Integer> lst) {
         int pickNumber;
         do {
             pickNumber = Randoms.pickNumberInRange(1, 9);
@@ -27,10 +28,38 @@ public class Application {
         return pickNumber;
     }
 
-    public boolean isValidNumber(int number, List<Integer> lst){
-        if(number < 1 || number > 9){
+    public boolean isValidNumber(int number, List<Integer> lst) {
+        if (number < 1 || number > 9) {
             return false;
         }
         return !lst.contains(number);
+    }
+
+    public List<Integer> getUserNumberList() {
+        List<Integer> lst = new ArrayList<>();
+        String input = Console.readLine();
+        if (!isValidSize(input)) {
+            throw new IllegalArgumentException();
+        }
+        for (int index = 0; index < 3; index++) {
+            int indexInt = charToInt(input.charAt(index));
+            if (!isValidNumber(indexInt, lst)) {
+                throw new IllegalArgumentException();
+            }
+            lst.add(indexInt);
+        }
+        return lst;
+    }
+
+
+    private int charToInt(char c) {
+        if (!Character.isDigit(c)) {
+            throw new IllegalArgumentException();
+        }
+        return c - '0';
+    }
+
+    private boolean isValidSize(String input) {
+        return input.length() == 3;
     }
 }
