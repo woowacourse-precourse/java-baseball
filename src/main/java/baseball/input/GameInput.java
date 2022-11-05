94e8e15c0cd97862a2bbf8d6365e8a28fbd99fcc
Kayoung <rkdkdudjd@gmail.com>
2022-11-05 18:37:05 +0900

    feat: GameInput 생성자 인자 검증
    * isValidRange: 3자리수인지 검증
    * hasDistinctDigits: 다른 3자리수를 가지는지 검증

--- src/main/java/baseball/input/GameInput.java
+++ src/main/java/baseball/input/GameInput.java
@@ -17,8 +17,27 @@ public class GameInput {
     }
 
     private void validateInput(int num) {
+        isValidRange(num);
+        hasDistinctDigits(num);
     }
 
+    private void isValidRange(int num) {
+        if (100 <= num && num < 1000) {
+            return;
+        }
+        throw new IllegalArgumentException("3자리 수를 입력해주세요.");
+    }
+
+    private void hasDistinctDigits(int num) {
+        List<Integer> seperatedDigits = separateToDigits(num);
+        Integer firstDigit = seperatedDigits.get(0);
+        Integer secondDigit = seperatedDigits.get(1);
+        Integer thirdDigit = seperatedDigits.get(2);
+
+        if (firstDigit.equals(secondDigit) || secondDigit.equals(thirdDigit) || firstDigit.equals(thirdDigit)) {
+            throw new IllegalArgumentException("서로 다른 3자리 수를 입력해주세요.");
+        }
+    }
 
     private List<Integer> separateToDigits(int num) {
         Integer firstDigit = num / 100;
--- src/test/java/baseball/input/GameInputTest.java
+++ src/test/java/baseball/input/GameInputTest.java
@@ -1,6 +1,7 @@
 package baseball.input;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
@@ -14,5 +15,33 @@ class GameInputTest {
         assertThat(input.first).isEqualTo(1);
         assertThat(input.second).isEqualTo(2);
         assertThat(input.third).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("3자리수가 아닐 때 예외를 던진다")
+    public void notThreeDigitThrowsException() {
+        assertThatThrownBy(()-> new GameInput(1))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("3자리 수를 입력해주세요.");
+    }
+
+    @Test
+    @DisplayName("3자리수 중 같은 숫자가 있을 때 예외를 던진다")
+    public void duplicateDigitThrowsException() {
+        assertThatThrownBy(()-> new GameInput(111))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("서로 다른 3자리 수를 입력해주세요.");
+
+        assertThatThrownBy(()-> new GameInput(772))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("서로 다른 3자리 수를 입력해주세요.");
+
+        assertThatThrownBy(()-> new GameInput(121))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("서로 다른 3자리 수를 입력해주세요.");
+
+        assertThatThrownBy(()-> new GameInput(633))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("서로 다른 3자리 수를 입력해주세요.");
     }
 }
