e5c20b6c90ae6b01063bb5a19237b2cff8801b29
1eejisoo <jisu991012@gmail.com>
2022-11-05 16:25:26 +0900

    Feat: 사용자로 부터 값을 입력 받는 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,9 +1,19 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGame {
+    private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public List<Integer> input() {   // 입력
+        System.out.print(INPUT_NUMBER);
+        String inputNumber = Console.readLine();
+
+        return stringToIntegerList(inputNumber);
+    }
+
     public List<Integer> stringToIntegerList(String inputNumber) {  // 입력 값을 List<Integer>로
         List<Integer> number = new ArrayList<>();
 
