e856d644182faaaab237777c42be2ecf583e457a
jyeost <pig9619@naver.com>
2022-11-05 18:05:01 +0900

    test : 사용자의 입력값에 대한 테스트 코드 작성

--- src/test/java/baseball/MyTest.java
+++ src/test/java/baseball/MyTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class MyTest {
 
@@ -27,8 +28,40 @@ public class MyTest {
         }
     }
 
+    @Test
+    void 사용자의_입력값이_제대로_userBallList로_반환되는지_알아본다(){
+        String userInput = "134";
+        List<Integer> result = List.of(1,3,4);
+        assertThat(User.checkUserInput(userInput)).isEqualTo(result);
+    }
 
+    @Test
+    void 사용자가_잘못된_입력값을_입력했을때_exception이_제대로_발생하는지_알아본다(){
+        /*List<String> errorInputList = List.of("1", "10", "223", "", "3455" , "하하하");
+        for(String errorInput :errorInputList){
+            assertThatThrownBy(()-> User.checkUserInput(errorInput))
+                        .isInstanceOf(IllegalArgumentException.class);
+        }*/
+        assertThatThrownBy(()-> User.checkUserInput("1"))
+                .isInstanceOf(IllegalArgumentException.class);
 
+        assertThatThrownBy(()-> User.checkUserInput("10"))
+                .isInstanceOf(IllegalArgumentException.class);
 
+        assertThatThrownBy(()-> User.checkUserInput("223"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(()-> User.checkUserInput(""))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(()-> User.checkUserInput("3455"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(()-> User.checkUserInput("하하하"))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(()-> User.checkUserInput("-256"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
