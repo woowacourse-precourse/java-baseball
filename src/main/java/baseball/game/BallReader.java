d9742180c312e19dfc746180a533abb2415df0ac
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-05 21:29:53 +0900

    feat(BallReader): getResult함수 구현

--- src/main/java/baseball/game/BallReader.java
+++ src/main/java/baseball/game/BallReader.java
@@ -1,5 +1,6 @@
 package baseball.game;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,7 +14,41 @@ public class BallReader {
     }
 
     public Map<String, Integer> getResult(List<Integer> userBall, List<Integer> computerBall) {
+        validateBalls(userBall, computerBall);
+        List<String> ballData = evaluateBall(userBall, computerBall);
+        return makeResult(ballData);
+    }
+
+    private List<String> evaluateBall(List<Integer> userBall, List<Integer> computerBall) {
+        List<String> ballData = new ArrayList<>();
+        for (int number : userBall) {
+            int userIndex = userBall.indexOf(number);
+            int computerIndex = computerBall.indexOf(number);
+            if (computerIndex == -1) {
+                continue;
+            }
+            if (userIndex == computerIndex) {
+                ballData.add(STRIKE);
+            }
+            if (userIndex != computerIndex) {
+                ballData.add(BALL);
+            }
+        }
+        return ballData;
+    }
+
+    private Map<String, Integer> makeResult(List<String> ballData) {
         Map<String, Integer> result = new HashMap<>();
+        for (String data : ballData) {
+            result.computeIfPresent(data, (key, value) -> value + 1);
+            result.computeIfAbsent(data, key -> 1);
+        }
         return result;
+    }
+
+    private void validateBalls(List<Integer> userBall, List<Integer> computerBall) {
+        if(userBall.size()!=computerBall.size()){
+            throw new IllegalArgumentException("서로 호환 되지 않는 공입니다");
+        }
     }
 }
