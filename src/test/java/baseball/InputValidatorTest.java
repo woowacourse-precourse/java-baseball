2d266c465dacc72139883d1d50734530532b9137
Donguk <95donguk.dev@gmail.com>
2022-11-05 19:39:08 +0900

    test: add validateNumber method test case
    add exception test case when input value is non-three digits

--- src/test/java/baseball/InputValidatorTest.java
+++ src/test/java/baseball/InputValidatorTest.java
@@ -1,4 +1,21 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
 class InputValidatorTest {
-  
+
+    @Test
+    void 입력값이_문자일_때_예외_테스트() {
+        assertThatThrownBy(() -> InputValidator.validateNumber("가나다"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자가 아닌 값을 입력하였습니다.");
+    }
+
+    @Test
+    void 입력값이_세_자리가_아닌_수일_때_예외_테스트() {
+        assertThatThrownBy(() -> InputValidator.validateNumber("1234"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
