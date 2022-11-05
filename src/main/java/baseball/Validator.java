c9bb46ab8381fcdfb8a0b1aced62bf891c5f50d3
김한준 (Hanjun Kim) <dhepd8849@gmail.com>
2022-11-05 19:10:23 +0900

    feat: 입력된 숫자가 세 자리 숫자가 아닌 경우 예외처리

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,10 +1,20 @@
 package baseball;
 
 public class Validator {
+
+	public static final int PROPER_LENGTH = 3;
+
 	public static void validate(String source) {
 		if (containsNonNumericValue(source)) {
 			throw new IllegalArgumentException("숫자가 아닌 값을 입력할 수 없습니다.");
 		}
+		if (isImproperLength(source)) {
+			throw new IllegalArgumentException("반드시 세자리 숫자를 입력해야 합니다.");
+		}
+	}
+
+	private static boolean isImproperLength(String source) {
+		return source.length() != PROPER_LENGTH;
 	}
 
 	private static boolean containsNonNumericValue(String source) {
--- src/test/java/baseball/ValidatorTest.java
+++ src/test/java/baseball/ValidatorTest.java
@@ -9,4 +9,9 @@ public class ValidatorTest {
 	void 숫자가_아닌_값이_포함된_경우_예외처리() {
 		assertThatIllegalArgumentException().isThrownBy(() -> Validator.validate("1f3"));
 	}
+
+	@Test
+	void 입력된_숫자가_세자리가_아닐_경우_예외처리() {
+		assertThatIllegalArgumentException().isThrownBy(() -> Validator.validate("1234"));
+	}
 }
