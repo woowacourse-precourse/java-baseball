67f7a580f807e7710cf90998b8a4895f137cf608
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-05 19:36:05 +0900

    feat: 사용자 입력 값, 컴퓨터의 결과를 비교해 스트라이크 횟수를 세는 메소드 구현
    substring 이용하여 스트라이크를 판단하고 strikeCount를 return

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,17 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 public class Application {
     private static final int RESULT_SIZE = 3;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
@@ -23,7 +19,9 @@ public class Application {
 
         List<Integer> computerNumber = generateComputerNumber();
         String computerNumberResult = computerNumberListToString(computerNumber);
-
+        int strikeResult = countStrike(inputNumber,computerNumberResult);
+        System.out.println("컴퓨터 랜덤 숫자 : " + computerNumberResult);
+        System.out.println(countStrike(inputNumber,computerNumberResult) + "스트라이크");
     }
     /*
      * 컴퓨터의 랜덤한 세 자리 수를 List에 저장함
@@ -42,11 +40,24 @@ public class Application {
         return computerNumberList;
     }
     /*
-     * 입력받은 Integer
+     * Integer List를 String 으로 변환함
      */
     static String computerNumberListToString(List<Integer> computerNumberList) {
         return computerNumberList.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining());
+    }
+    static int countStrike (String userInput, String computerInput) {
+        int strikeCount = 0;
+        if (userInput.substring(0,1).equals(computerInput.substring(0,1))) {
+            strikeCount ++;
+        }
+        if (userInput.substring(1,2).equals(computerInput.substring(1,2))) {
+            strikeCount ++;
+        }
+        if (userInput.substring(2,3).equals(computerInput.substring(2,3))) {
+            strikeCount ++;
+        }
+        return  strikeCount;
     }
 }
