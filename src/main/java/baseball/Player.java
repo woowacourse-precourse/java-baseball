89534177a3eeae9ceb3465894e5ec2341c6cf8f5
jjdone <ehdlfaos@gmail.com>
2022-11-05 12:55:35 +0900

    feat: Player 클래스 상세구현2
    stringToList, inputMessage 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -3,24 +3,30 @@ package baseball;
 import java.util.*;
 import camp.nextstep.edu.missionutils.Console;
 public class Player {
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
     private List<Integer> numbers = new ArrayList<>();
     public Player() {}
 
     public void setNumbers() {
+        inputMessage();
         String input = Console.readLine();
         stringToList(input);
     }
 
     public List<Integer> getNumbers() {
-        return this.numbers;
+        return numbers;
     }
 
     public void stringToList(String string) {
         char[] characterArray = string.toCharArray();
 
         for(char c : characterArray) {
-            this.numbers.add((int)c - '0');
+            numbers.add((int)c - '0');
         }
+    }
+
+    public void inputMessage() {
+        System.out.print(INPUT_MESSAGE);
     }
 }
