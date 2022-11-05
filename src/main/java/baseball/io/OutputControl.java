2674ec8a767741ed2bfd378ccc160349b50e3e3f
Invidam <hansupark0208@gmail.com>
2022-11-05 18:54:11 +0900

    fix(User): 메시지 출력을 위한 객체 추가

---
+++ src/main/java/baseball/io/OutputControl.java
@@ -0,0 +1,10 @@
+package baseball.io;
+
+public class OutputControl {
+    public static void println(String msg) {
+        System.out.println(msg);
+    }
+    public static void print(String msg) {
+        System.out.print(msg);
+    }
+}
