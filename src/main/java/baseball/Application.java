b9fa8f5228b181e8a4589aa090ed7aa071d9bb73
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-05 22:55:15 +0900

    feat: 게임 진행 메세지 출력 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,7 @@ public class Application {
 
     public static void main(String[] args) {
         try {
+            System.out.println("숫자 야구 게임을 시작합니다.");
             while(true){
 
                 boolean[] isBall = new boolean[10];
@@ -23,6 +24,7 @@ public class Application {
 
 
                 while(true){
+                    System.out.print("숫자를 입력해주세요 : ");
                     String playerNumber = Console.readLine();
 
                     if(!checkPlayerNumber(playerNumber)){
@@ -47,6 +49,8 @@ public class Application {
 
                     if (strikeCount == 3) break;
                 }
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
                 String playerSelect = Console.readLine();
                 int playerSelectNumber = Integer.parseInt(playerSelect);
