98ba81b231809ca43866f1dda0251d4ab421ef6f
nylonmusk <106235834+nylonmusk@users.noreply.github.com>
2022-11-06 02:54:37 +0900

    test: 공백이 포함된 숫자 입력시 테스트 코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -52,6 +52,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 예외_테스트_공백이_포함된_숫자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1 23"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
