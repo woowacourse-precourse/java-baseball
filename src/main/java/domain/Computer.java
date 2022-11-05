d29f32125a5eb47b09be7cae67505a208345990e
1eejisoo <jisu991012@gmail.com>
2022-11-05 16:14:31 +0900

    Refactor: 접근지정자 변경 및 static 키워드 삭제

--- src/main/java/domain/Computer.java
+++ src/main/java/domain/Computer.java
@@ -6,9 +6,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    public static final List<Integer> computer = new ArrayList<>();
+    private final List<Integer> computer = new ArrayList<>();
 
-    public static List<Integer> makeRandomNumber() {
+    public List<Integer> makeRandomNumber() {
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
