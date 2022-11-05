798dc98d1ed97bf461b25f5b6699a0fdf68822f7
haisley77 <qhfg3145@naver.com>
2022-11-05 16:24:01 +0900

    fix: handle exceptions for invalid inputs & throw exceptions

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,19 +1,21 @@
 package baseball;
 
-
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
 import java.util.List;
 import java.util.ArrayList;
-
+import java.util.Set;
 
 public class Application {
 
     private static List<Integer> computer = new ArrayList<>();
-    private static Integer inputNumber;
+    private static String inputNumber;
     private static List<Integer> inputNumberList = new ArrayList<>();
     private static int ball=0;
     private static int strike=0;
+    private static int exceptionIsThrown=0;
     private static void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
@@ -28,20 +30,27 @@ public class Application {
         }
     }
 
-    private static void input() throws IllegalArgumentException {
+    private static void input() {
         System.out.print("숫자를 입력해주세요 : ");
-        inputNumber = Integer.valueOf(Console.readLine());
+        inputNumber = String.valueOf(Console.readLine());
+        inputNumberList.clear();
+
+        for (int i=0;i<inputNumber.length();i++){
+            inputNumberList.add(Integer.valueOf(inputNumber.charAt(i)-'0'));
+        }
+
+        Set<Integer> set = new HashSet<>(inputNumberList);
+        if (inputNumberList.size()!=3 || inputNumberList.contains(0) || inputNumberList.size()!=set.size()) {
+            exceptionIsThrown = 1;
+            throw new IllegalArgumentException();
+        }
+
+
 
         countBall();
     }
 
     private static void countBall() {
-        String inputNumberString = Integer.toString(inputNumber);
-        inputNumberList.clear();
-
-       for (int i=0;i<inputNumberString.length();i++){
-           inputNumberList.add(Integer.valueOf(inputNumberString.charAt(i)-'0'));
-       }
 
        ball = 0;
        for (Integer element:inputNumberList){
@@ -50,8 +59,7 @@ public class Application {
            }
        }
 
-
-       if (ball!=0) System.out.print(ball+"볼");
+       if (ball!=0) System.out.print(ball+"볼 ");
 
        countStrike();
     }
@@ -65,31 +73,46 @@ public class Application {
             }
         }
 
-        if (strike!=0) System.out.println(" "+strike+"스트라이크");
+        if (strike!=0) System.out.println(strike+"스트라이크");
         if (ball==0 && strike==0) System.out.println("낫싱");
     }
 
 
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
         start();
         pickRandomNumber();
-        do {
 
+        do {
             input();
 
-            if (strike==3) {
+            if (strike==3 && exceptionIsThrown == 0) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                int userInput = Integer.valueOf(Console.readLine());
-                if (userInput==1) {
-                    pickRandomNumber();
-                    strike = 0;
-                }
+                checkRestart();
             }
+            if (strike!=3 && exceptionIsThrown == 1) {
+                System.out.println("게임 종료");
+                checkRestart();
+            }
+
         } while (strike!=3);
 
 
+    }
+
+    private static void checkRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int userinput = Integer.valueOf(Console.readLine());
+        if (userinput==1) {
+            pickRandomNumber();
+            strike = 0;
+            exceptionIsThrown = 0;
+        }
+
+        if (userinput<=0 || userinput>=3){
+            exceptionIsThrown = 1;
+            throw new IllegalArgumentException();
+        }
     }
 }
