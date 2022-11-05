f695d41eb055c667169aaab6f1afa32e8dfab7e9
hyun-seung <ardimento22@naver.com>
2022-11-05 14:47:49 +0900

    [TEST] 1부터 9까지의 서로 다른 수로 이루어진 3자리의 수인지 확인하고, 잘못된 값일 경우 에러를 발생시키고 애플리케이션을 종료시킨다.
    fix : 기존 코드 변경으로 인한 테스트 코드 변경

--- src/test/java/baseball/ValidationNumberTest.java
+++ src/test/java/baseball/ValidationNumberTest.java
@@ -6,13 +6,11 @@ import org.junit.jupiter.api.Test;
 
 public class ValidationNumberTest {
 
-    private static final int SIZE = 3;
-
     @Test
     void 숫자인지_확인_예외_발생() {
         String input = "14r";
 
-        ValidationNumber validationNumber = new ValidationNumber(SIZE);
+        ValidationNumber validationNumber = new ValidationNumber();
         assertThatThrownBy(() -> validationNumber.checkTotalAndConvertIntegerList(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -21,7 +19,7 @@ public class ValidationNumberTest {
     void 숫자_0_가지고_있는지_확인_예외_발생() {
         String input = "409";
 
-        ValidationNumber validationNumber = new ValidationNumber(SIZE);
+        ValidationNumber validationNumber = new ValidationNumber();
         assertThatThrownBy(() -> validationNumber.checkTotalAndConvertIntegerList(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -30,7 +28,7 @@ public class ValidationNumberTest {
     void 중복되는_값_있는지_확인_예외_발생() {
         String input = "112";
 
-        ValidationNumber validationNumber = new ValidationNumber(SIZE);
+        ValidationNumber validationNumber = new ValidationNumber();
         assertThatThrownBy(() -> validationNumber.checkTotalAndConvertIntegerList(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -39,7 +37,7 @@ public class ValidationNumberTest {
     void 주어진_크기와_동일한지_확인_예외_발생() {
         String input = "28";
 
-        ValidationNumber validationNumber = new ValidationNumber(SIZE);
+        ValidationNumber validationNumber = new ValidationNumber();
         assertThatThrownBy(() -> validationNumber.checkTotalAndConvertIntegerList(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
