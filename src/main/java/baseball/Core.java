280286c173f7b42f6ee91e181676879b76c4d919
MoonJeWoong <munjin0201@naver.com>
2022-11-05 11:27:11 +0900

    feat(core): countStrike 메서드 구현
    두 개의 정수 리스트가 주어지면 strike 개수가 몇 개인지 count하여 반환하는
    메서드.

--- src/main/java/baseball/Core.java
+++ src/main/java/baseball/Core.java
@@ -6,4 +6,14 @@ public class Core {
     public boolean isDigitStrike(List<Integer> computerNumber, List<Integer> userNumber, int index){
         return computerNumber.get(index).equals(userNumber.get(index));
     }
+
+    public int countStrike(List<Integer> computerNumber, List<Integer> userNumber){
+        int count=0;
+        for(int index=0; index<userNumber.size(); index++){
+            if(isDigitStrike(computerNumber,userNumber,index)){
+                count++;
+            }
+        }
+        return count;
+    }
 }
