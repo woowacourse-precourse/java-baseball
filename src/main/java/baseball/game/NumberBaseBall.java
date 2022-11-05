6b100f62607a6dd6eccc0b14ddab0a50e03fa11e
bellljun3395 <195850@jnu.ac.kr>
2022-11-05 17:20:30 +0900

    feat(NumberBaseBall): 구현 NumberBaseBall play 메서드
    - 입력을 위한 멘트를 출력
    - 입력을 받아 그 값을 검증한다.
    - 입력 받은 값을 게임 플레이어의 값으로 설정
    - 컴퓨터의 값과 비교하여 힌트를 얻어온다.
    - 힌트를 보여준다.
    - 3스트라이크면 stopGame을 true로 하여 우선 게임을 멈춘다.

--- src/main/java/baseball/game/NumberBaseBall.java
+++ src/main/java/baseball/game/NumberBaseBall.java
@@ -11,6 +11,10 @@ import baseball.view.Viewable;
 import java.util.List;
 
 public class NumberBaseBall implements PlayAble {
+    private final int INPUT_NUMBER_LENGTH = 3;
+    private final int STRIKE_INDEX = 1;
+    private final int STRIKE_OUT = 3;
+
     private boolean stopGame = false;
     private boolean endGame = false;
 
@@ -38,10 +42,23 @@ public class NumberBaseBall implements PlayAble {
         this.stopGame = false;
         this.endGame = false;
     }
-    
+
     @Override
     public void play() {
+        view.printInput();
+        int inputNumber = input.acceptInt();
+        validateInputNumber(inputNumber);
+        gamePlayer.setNumber(inputNumber);
+        List<Integer> hints = computer.judge(gamePlayer.getNumber());
+        view.printHint(hints);
+        if (hints.get(STRIKE_INDEX) == STRIKE_OUT) {
+            this.stopGame = true;
+        }
+    }
 
+    private void validateInputNumber(int inputNumber) {
+        ValidateNumber.validateLength(inputNumber, (Integer length) -> length == INPUT_NUMBER_LENGTH);
+        ValidateNumber.validateOverlap(inputNumber);
     }
 
     @Override
