d968413f50d1219b0af4ee2acfd96851e88da586
jaehyeon <jhjh0022@naver.com>
2022-11-05 13:30:05 +0900

    feat: 입력기능과 입력 검증 한번에 하는 기능 구현

--- src/main/java/baseball/Input/InputAndVerification.java
+++ src/main/java/baseball/Input/InputAndVerification.java
@@ -1,15 +1,24 @@
 package baseball.Input;
 
 public class InputAndVerification {
+    InputNumber inputNumber = new InputNumber();
+    InputNumberVerification inputNumberVerification = new InputNumberVerification();
 
     public int receiveAndVerifyNumber(){
-        InputNumber inputNumber = new InputNumber();
-        InputNumberVerification inputNumberVerification = new InputNumberVerification();
         int receivedNumber = inputNumber.getInputNumber();
 
         if(!inputNumberVerification.verifyNumber(receivedNumber)){
-            //프로그램 종료 시키는 메서드
+            throw new IllegalArgumentException("형식에 맞게 입력하세요");
         }
         return receivedNumber;
+    }
+
+    public int receiveAfterGameEndOneOrTwo(){
+        int receivedButtonNumber = inputNumber.getRestartAndEndNumber();
+
+        if(inputNumberVerification.verifyOneOrTwoButton(receivedButtonNumber)){
+            throw new IllegalArgumentException("1, 2 둘 중 하나만 골르세요");
+        }
+        return receivedButtonNumber;
     }
 }
