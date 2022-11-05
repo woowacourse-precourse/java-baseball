004bf0683ab014a9798e5193006c64fa8790210e
Heejun-0219 <heejun0219@gmail.com>
2022-11-05 17:36:25 +0900

    feat(Computer_Number) : 1. Computer automatically sets the numbers.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.io.*;
 public class Application {
+    public static List<Integer> Computer_Number() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> computer = Computer_Number();
     }
 }
