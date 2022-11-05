4cd780e2c532839a957e74c341130068595f2218
chesthyeon <niw927@naver.com>
2022-11-05 18:26:11 +0900

    컴퓨터 랜덤 숫자 기능 -> camp.nextstep.edu.missionutils에서 제공하는 Randoms 및 Console API를 사용하여 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.io.*;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        start();
+    }
+
+    public static void start(){
+
+    }
+
+    public static List<Integer> coputerNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
