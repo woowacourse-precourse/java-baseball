1545f7c2f5289c4d595bd584586271c11d8a53ce
young2866 <62212610+young2866@users.noreply.github.com>
2022-11-06 01:51:36 +0900

    feat(User): 게임 플레이어로부터 숫자를 입력 받는 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,8 +7,10 @@ public class Game {
 
 	private final static String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 	private Opponent opponent = new Opponent();
+	private User user = new User();
 	public void start() {
 		System.out.println(START_MESSAGE);
 		opponent.generateAnswerNumber();
+		user.inputNumber();
 	}
 }
--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,5 +1,26 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+	private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+	private static final int DIGIT_LENGTH = 3;
+
+	private static List<Integer> injectNumber = new ArrayList<>(DIGIT_LENGTH);
+
+	public void inputNumber() {
+		System.out.print(INPUT_MESSAGE);
+		String inputNumber = readLine();
+
+		for (int index = 0; index < DIGIT_LENGTH; index++) {
+			int digit = inputNumber.charAt(index) - '0';
+
+			injectNumber.set(index, digit);
+		}
+	}
 
 }
