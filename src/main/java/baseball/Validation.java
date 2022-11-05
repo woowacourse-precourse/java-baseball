de4d5af9c0ada310a171fd39d022bcf69f57072b
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-05 14:22:56 +0900

    refactor : validateFameCoin()메소드에서 static 키워드 삭제

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,2 +1,30 @@
-package baseball;public class Validation {
+package baseball;
+
+import java.util.regex.Pattern;
+
+public class Validation {
+    public void validateUserNumber(String userNumber){
+        if(userNumber.length() != 3){
+            throw new IllegalArgumentException("3자리 숫자를 입력해주세요!");
+        }
+
+        for(char number : userNumber.toCharArray()){
+            // 중복된 숫자가 있으면 예외
+            if( userNumber.chars().filter(c -> c == number).count() > 1 ){
+                throw new IllegalArgumentException("중복된 숫자를 입력하셨습니다!");
+            }
+
+            // 숫자가 아니면 예외
+            if(Character.isDigit(number)==false){
+                throw new IllegalArgumentException("숫자 이외의 값을 입력하셨습니다!");
+            }
+        }
+    }
+
+    public void validateGameCoin(String gameCoin) {
+        // 1,2가 아니면 exception
+        if(Pattern.matches("[1-2]",gameCoin) == false){
+            throw new IllegalArgumentException("1,2 이외의 값을 입력하셨습니다!");
+        }
+    }
 }
