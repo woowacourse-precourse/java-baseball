afb617c740c3e4b4ad10c5f722ce298ac345f54d
UJ15 <dbwogml15@naver.com>
2022-11-05 21:01:41 +0900

    feat : implement toString BallStatus

--- src/main/java/baseball/model/BallStatus.java
+++ src/main/java/baseball/model/BallStatus.java
@@ -17,4 +17,21 @@ public class BallStatus {
     public int getStrike() {
         return strike;
     }
+
+    @Override
+    public String toString() {
+        if (ball == 0 && strike == 0) {
+            return "낫싱";
+        }
+
+        if (ball == 0) {
+            return strike + "스트라이크";
+        }
+
+        if (strike == 0) {
+            return ball + "볼";
+        }
+
+        return ball + "볼 " + strike + "스트라이크";
+    }
 }
