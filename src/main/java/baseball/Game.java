15fe85f2b2f48952c2c89508469989e4ceccc349
JerryK026 <kskyung0624@gmail.com>
2022-11-05 22:43:36 +0900

    Feat(Game) : initAnswer 메서드 구현
    중복 값을 가지지 않고 answer를 세팅하는 initAnswer를 구현한다.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 public class Game {
+    private List<Integer> answer = new ArrayList<>();
+
+    public void initAnswer() {
+        answer.clear();
+
+        for (int i = 0; i < 3; i++) {
+            answer.add(Randoms.pickNumberInRange(1, 9));
+        }
+
+        if (isDuplicated(answer)) {
+            initAnswer();
+        }
+    }
+
+    public List<Integer> getAnswer() {
+        return answer;
+    }
+
+    private boolean isDuplicated(List<Integer> answer) {
+        return answer.size() != new HashSet<>(answer).size();
+    }
 }
