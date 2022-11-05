5b2c24df8ee8e8f7719ed08836632217a36ae2bb
qdeu22 <qdeu22@gmail.com>
2022-11-05 13:15:46 +0900

    feat(UserBaseballNumber): 사용자 입력값 처리 구현

--- src/main/java/baseball/model/UserBaseballNumber.java
+++ src/main/java/baseball/model/UserBaseballNumber.java
@@ -1,4 +1,24 @@
 package baseball.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserBaseballNumber {
+    private static List<Integer> userNumber;
+
+    public UserBaseballNumber(String baseballNumber) {
+        this.userNumber = userBaseballNumber(baseballNumber);
+    }
+
+    public List<Integer> userBaseballNumber(String baseballNumber) {
+        List<Integer> userNumberList = new ArrayList<>();
+        for (int i = 0; i < baseballNumber.length(); i++) {
+            userNumberList.add(baseballNumber.charAt(i)-'0');
+        }
+        return userNumberList;
+    }
+
+    public List<Integer> getUserNumber() {
+        return userNumber;
+    }
 }
