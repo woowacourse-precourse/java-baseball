1a9a3b3ffff37f550e30866aff5af46ed00d2922
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-05 19:15:52 +0900

    feat: 랜덤 세 자리수 생성 기능 메소드 분리
    List에 저장된 세 자리수를 String으로 return 하는 메소드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,21 +11,42 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Application {
-    private static final int COMPUTER_THREE_DIGIT = 3;
+    private static final int RESULT_SIZE = 3;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        List<Integer> computer = new ArrayList<>();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
+        String inputNumber = Console.readLine();
 
-        while (computer.size() < COMPUTER_THREE_DIGIT) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+        List<Integer> computerNumber = generateComputerNumber();
+        String computerNumberResult = computerNumberListToString(computerNumber);
+
+    }
+    /*
+     * 컴퓨터의 랜덤한 세 자리 수를 List에 저장함
+     */
+    static List<Integer> generateComputerNumber() {
+        List<Integer> computerNumberList = new ArrayList<>();
+        for (int i = 0; i < RESULT_SIZE; i++) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+            if (computerNumberList.contains(randomNumber)) {
+                i--;
+            }
+            if (!computerNumberList.contains(randomNumber)) {
+                computerNumberList.add(randomNumber);
             }
         }
-
-        String computerNumber = computer.stream().map(String::valueOf).collect(Collectors.joining());
+        return computerNumberList;
+    }
+    /*
+     * 입력받은 Integer
+     */
+    static String computerNumberListToString(List<Integer> computerNumberList) {
+        return computerNumberList.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
     }
 }
