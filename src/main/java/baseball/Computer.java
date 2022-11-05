91578de75319ffe7728288e3a95ccb7dda35dd5e
juhee park <jouhi3322@gmail.com>
2022-11-06 03:50:50 +0900

    fix: 랜덤 생성 함수 변경

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,14 +2,28 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer extends Player {
 
     public Computer() {
-        setNums(Randoms.pickUniqueNumbersInRange(1, 9, 3));
+        setRandNums();
+    }
+
+    public void setRandNums() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        setNums(computer);
     }
 
     @Override
     public String toString() {
-        return "Computer"+super.toString();
+        return "Computer" + super.toString();
     }
 }
