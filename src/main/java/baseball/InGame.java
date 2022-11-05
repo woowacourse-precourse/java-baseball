3d69ef0034335448625c85db32df1c4539971095
simba <shim9597@gmail.com>
2022-11-05 18:59:22 +0900

    feat: validateInGame 기능 구현 완료
    input 값의 길이가 3 이상 혹은 0 일때, input 값에 0 이 포함 되어있을 때, input 값에 중복된 숫자가 있을 때 IllegalArgumentException 발생시키고 애플리케이션 종료.

--- src/main/java/baseball/InGame.java
+++ src/main/java/baseball/InGame.java
@@ -3,6 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -18,14 +19,29 @@ public class InGame {
     }
 
     public void validateInGame(String input) {
-        isNumeric(input);
+        isInteger(input);
+        if (input.length() > 3 || input.length() == 0) {
+            throw new IllegalArgumentException();
+        }
+        if (input.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+        duplicateNumber(input);
     }
 
-    private static void isNumeric(String input) {
+    private static void duplicateNumber(String input) {
+        int intInput = Integer.parseInt(input);
+        List<Integer> numList = List.of(intInput);
+        if(numList.size() != numList.stream().distinct().count()){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void isInteger(String input) {
         try {
-            int intInput = Integer.parseInt(input);
+            Integer.parseInt(input);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(e);
+            throw new IllegalArgumentException(e); // IllegalArgumentException 이 아닌 NumberFormatException 을 던지는 이슈있음
         }
     }
 
