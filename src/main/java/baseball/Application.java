20d75a352fcce33f2dd741f81de8db8786aa5dc5
jeein2222 <jiinbag057@gmail.com>
2022-11-05 20:08:38 +0900

    컴퓨터가 1~9까지 서로 다른 임의의 수 3개 선택

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
-
+import java.util.*;
 public class Application {
+    public static String getResult(int[] cNum, int pNum){
+        return "";
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println("Let's");
+        Scanner sc=new Scanner(System.in);
+        while(true){
+            int[] comNum=new int[3];
+            for(int i=0;i<comNum.length;i++){
+                comNum[i]=(int)(Math.random()*9)+1;
+            }
+
+
+        }
     }
 }
