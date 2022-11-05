8f9482ea74635fd14b4a4971f2ae076026e83094
syahn <496300@naver.com>
2022-11-06 01:15:26 +0900

    REMOVE(Application) : main을 제외한 모든 메소드를 삭제
    게임을 진행하는 전체적인 과정을 하나의 메소드로 관리하기 위해 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -43,61 +43,6 @@ class Game{
 
 public class Application {
 
-    public static void compareNums(List<Integer> computerNum, List<Integer> userNum) {
-        if (computerNum.equals(userNum)) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다!");
-            chooseGameCondition();
-        } else {
-            List<Integer> copyUserNum = new ArrayList<>(userNum);
-            copyUserNum.retainAll(computerNum);
-            int count = 0;
-
-            if (copyUserNum.isEmpty())
-                System.out.println("낫싱");
-            else {
-                for (int i = 0; i < copyUserNum.size(); i++) {
-                    int indexComputer = computerNum.indexOf(copyUserNum.get(i));
-                    int indexUser = userNum.indexOf(copyUserNum.get(i));
-                    if (indexComputer == indexUser)
-                        count++;
-                }
-                if (count == 0)
-                    System.out.println(count + "볼");
-                else if (copyUserNum.size() - count == 0)
-                    System.out.println(count + "스트라이크");
-                else
-                    System.out.println((copyUserNum.size() - count) + "볼" + count + "스트라이크");
-            }
-        }
-    }
-
-    public static void chooseGameCondition() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String userInput = Console.readLine();
-
-        if (userInput.equals("1")) {
-        } else {
-        }
-    }
-
-    public static void startGame() {
-        List<Integer> computerNum = new ArrayList<>();
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        computerNum = createComputerNum();
-        boolean continueGame = true;
-
-        while (continueGame) {
-            List<Integer> userNum = new ArrayList<>();
-            try {
-                userNum = getUserInput();
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.toString());
-            }
-            compareNums(computerNum, userNum);
-        }
-    }
-
     public static void main(String[] args) {
-        startGame();
     }
 }
