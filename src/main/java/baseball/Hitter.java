7e1b20b8c1a9127a3f05e372e033e8f47147a7c6
rt3310 <btac3310@gmail.com>
2022-11-06 01:45:24 +0900

    feat: 타자가 입력된 숫자의 개수만큼 공을 던지는 기능(3개)

--- src/main/java/baseball/Hitter.java
+++ src/main/java/baseball/Hitter.java
@@ -1,4 +1,15 @@
 package baseball;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Hitter {
+
+    public List<Ball> hitBalls(String number) {
+        return IntStream.range(0, number.length())
+                .mapToObj(index ->
+                        new Ball(Character.getNumericValue(number.charAt(index)), index))
+                .collect(Collectors.toList());
+    }
 }
