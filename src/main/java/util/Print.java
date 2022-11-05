1c6bfe4b3aa4f929262ceedfc3410ebcba07b807
sudo-dustle <asdf748596123@gmail.com>
2022-11-05 13:27:17 +0900

    fix: Print 클래스 printScore 에러 수정

--- src/main/java/util/Print.java
+++ src/main/java/util/Print.java
@@ -15,23 +15,19 @@ public class Print {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
-    public static String printScore(GameResult gameResult) {
-        StringBuilder sb = new StringBuilder();
+    public static void printScore(GameResult gameResult) {
 
         if (gameResult.isBallScoreValid()) {
-            sb.append(gameResult.getBall() + "볼 ");
+            System.out.print(gameResult.getBall() + "볼 ");
         }
 
         if (gameResult.isStrikeScoreValid()) {
-            sb.append(gameResult.getStrike() + "스트라이크 ");
+            System.out.print(gameResult.getStrike() + "스트라이크 ");
         }
 
         if (gameResult.isNoScore()) {
-            sb.append("낫싱");
+            System.out.print("낫싱");
         }
-
-        sb.append("\n");
-
-        return sb.toString();
+        System.out.println();
     }
 }
