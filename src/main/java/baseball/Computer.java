9bfd95d281df1a8aa0269742bdeb5d9316d1b62b
whibaek <jibaek0513@gmail.com>
2022-11-05 22:10:21 +0900

    feat(Computer Class): add method
    add makeAnswerNumber method
    - make answerNumber in Computer Class

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,10 +1,19 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    private List<Integer> answerNumber;
+    private List<Integer> answerNumber = new ArrayList<>();
     private Integer strike;
     private Integer ball;
 
+    public void makeAnswerNumber(){
+        int randomNumber = Randoms.pickNumberInRange(1, 9);
+        if (!answerNumber.contains(randomNumber)) {
+            answerNumber.add(randomNumber);
+        }
+    }
 }
