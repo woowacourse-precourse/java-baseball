1780a1e185ea56e060a5407260c61efcc956b2f9
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-06 05:10:05 +0900

    feat : 숫자 야구 게임 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import configuration.AppConfig;
+
 public class Application {
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        AppConfig appConfig = new AppConfig();
+        appConfig.playGame();
+
+
     }
 }
