43df14a7ae589469c2d3278812db72189d0ecab4
yj2980 <gum0945@naver.com>
2022-11-06 00:01:27 +0900

    feat : 숫자 비교하는 조건문 추가

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,7 +1,7 @@
 package baseball.service;
 
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -9,7 +9,9 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class GameService {
 	static final int min = 1;
 	static final int max = 9;
-
+	private static int strike = 1;
+	private static int ball = 2;
+	private static int root = 0;
 	public static String createRandomNumber() {
 		String random = "";
 
@@ -30,33 +32,40 @@ public class GameService {
 	public static boolean checkSameNumber(String number, int addedNumber) {
 		return number.contains(String.valueOf(addedNumber));
 	}
-	public static List<String> convertStringToList(String word) {
-		List<String> list = new ArrayList<>();
-
-		for (int i = 0; i < word.length(); i++)
-			list.add(String.valueOf(word.charAt(i)));
-		return list;
-	}
-	public static List<Integer> compareNumber(List<String> computer, List<String> user) {
-		List<Integer> result = new ArrayList<>();
-		int ball = 0;
-		int strike = 0;
-
-		for (int i = 0; i < computer.size(); i++) {
-			if (checkSameNumber(String.join("", computer), Integer.parseInt(user.get(i)))) {
-				if (checkSameLocation(computer.get(i), user.get(i))) {
+	public static List<Integer> compareNumber(String computer, String user) {
+		List<Integer> result = Arrays.asList(0, 0, 0);
+		/*
+		for (int i = 0; i < computer.length(); i++) {
+			if (checkSameNumber(computer, Character.getNumericValue(user.charAt(i)))) {
+				if (checkSameLocation(Character.toString(computer.charAt(i)), Character.toString(user.charAt(i)))) {
 					strike++;
 					continue;
 				}
 				ball++;
 			}
 		}
-		result.add(strike);
-		result.add(ball);
+		 */
+		for (int i = 0; i < computer.length(); i++) {
+			result.set(root, i);
+			isStrikeOrBall(computer, Character.toString(user.charAt(i)), result);
+		}
+		//result.add(strike);
+		//result.add(ball);
 
-		return result;
+		return result.subList(strike,result.size());
 	}
 	public static boolean checkSameLocation(String computer, String user) {
 		return computer.equals(user);
+	}
+	public static List<Integer> isStrikeOrBall(String computer, String user, List<Integer> result) {
+		if (checkSameNumber(computer, Integer.parseInt(user))) {
+			if (checkSameLocation(Character.toString(computer.charAt(result.get(root))), user)) {
+				result.set(strike, result.get(strike) + 1);
+			}
+			if (!checkSameLocation(Character.toString(computer.charAt(result.get(root))), user)) {
+				result.set(ball, result.get(ball) + 1);
+			}
+		}
+		return result;
 	}
 }
