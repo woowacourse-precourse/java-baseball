8aca3028f38d842126d8734de63006d68eed790a
woosung1223 <dntjd991223@naver.com>
2022-11-06 02:14:54 +0900

    refactor(OutputMsgGenerator): NOTHING 조건 추가

--- src/main/java/baseball/OutputMsgContainer.java
+++ src/main/java/baseball/OutputMsgContainer.java
@@ -4,4 +4,5 @@ public final class OutputMsgContainer {
     public static final String BALL = "볼";
     public static final String STRIKE = "스트라이크";
     public static final String SPACE = " ";
+    public static final String NOTHING = "낫싱";
 }
--- src/main/java/baseball/OutputMsgGenerator.java
+++ src/main/java/baseball/OutputMsgGenerator.java
@@ -5,6 +5,9 @@ import java.util.List;
 public final class OutputMsgGenerator {
     private OutputMsgGenerator() {}
     public static String generate(List<Integer> code) {
+        if (code.get(0) == 0 && code.get(1) == 0) {
+            return OutputMsgContainer.NOTHING;
+        }
         StringBuilder result = new StringBuilder();
         if (code.get(0) != 0) {
             result.append(code.get(0));
