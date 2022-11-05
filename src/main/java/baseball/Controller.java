c68e79fb2f8168b85cb40e5155804dc33e15ccdc
areyouhun <areyouhun@gmail.com>
2022-11-05 13:50:25 +0900

    refactor(controller): 게임을 진행하는 하나의 메서드를 여러 메서드로 분리

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -4,9 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Controller {
-    public static void start() {
-        boolean isGameGoing = true;
+    private static boolean isGameGoing = true;
 
+    public static void gameStart() {
         Dealer dealer = new Dealer();
         Player player = new Player();
         List<Integer> dealerNumbers = dealer.createThreeDifferentNumbers();
@@ -17,18 +17,35 @@ public class Controller {
             Calculator calculator = new Calculator(dealerNumbers, playerNumbers);
             List<Integer> scores = calculator.getScores();
             ResultViewer.printOut(scores);
-
-            // checkThreeStrikes
-            if (scores.get(1) == 3) {
-                isGameGoing = false;
-            }
+            checkThreeStrikes(scores.get(1));
         }
+        gameShouldContinue();
+    }
 
-        // shouldContinue
-        System.out.println("계속 고?");
-        String yesOrNo = Console.readLine();
-        if (yesOrNo.equals("1")) {
-            start();
+    public static void checkThreeStrikes(Integer strikeCount) {
+        if (strikeCount == 3) {
+            isGameGoing = false;
         }
+    }
+
+    public static void gameShouldContinue() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String playerResponse = Console.readLine();
+        checkResponseCode(playerResponse);
+    }
+
+    public static void checkResponseCode(String responseCode) {
+        if (responseCode.equals("1")) {
+            isGameGoing = true;
+            gameStart();
+        }
+        if (!responseCode.equals("1") && !responseCode.equals("2")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void main(String[] args) {
+        gameStart();
     }
 }
