6776e7aa069e39fae79fc3c1329d0a4d260ffdd1
박승민 <tmddudals369@naver.com>
2022-11-05 18:03:59 +0900

    feat: 게임 시작 안내 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
 public class Application {
+
+    private static final String GAME_START_SIGN = "숫자 야구 게임을 시작합니다.";
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        notifyGameStart();
+    }
+
+    public static void notifyGameStart() {
+        System.out.println(GAME_START_SIGN);
     }
 }
