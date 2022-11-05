5bf030642ab09e24c1742b25ef3a3d57749f0139
minaver <psmjoshua@gmail.com>
2022-11-05 11:57:28 +0900

    feat(Ballball): Baseball 객체 생성 및 메소드 초안 생성

---
+++ src/main/java/baseball/Baseball.java
@@ -0,0 +1,63 @@
+package baseball;
+
+public class Baseball {
+    private Boolean Gaming;
+    private int targetNumber;
+    private int guessNumber;
+    private BallCount ballCount;
+
+    // 생성자
+    public Baseball(){ }
+
+
+    // 메소드
+    // 게임 시작 method
+    public void start(){
+
+    }
+
+    // 게임 재시작 method
+    public void restart(){
+
+    }
+
+    // 게임 종료 method
+    public void end(){
+
+    }
+
+    // GuessNumber Setter method
+    public void setGuessNumber(int guessNumber){
+
+    }
+
+    // 볼 카운트 Getter method
+    public void getBallCount(){
+
+    }
+
+    // 스트라이크 카운트 Getter method
+    public void getStrikeCount(){
+
+    }
+
+    // 결과 ENUM 출력 method
+    public void getGuessResult(){
+
+    }
+
+    // 1에서 9까지 서로 다른 임의의 수 3개를 선택 method
+    private void resetTarget(){
+
+    }
+
+    // 볼 카운트 계산 method
+    private void ruleBallCount(){
+
+    }
+
+    // 스트라이크 카운트 계산 method
+    private void ruleStrikeCount(){
+
+    }
+}
