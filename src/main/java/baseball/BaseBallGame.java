9c200c192107b65daa584f99c0f3d04b5add51a0
cmong0516 <cmong0516@gmail.com>
2022-11-05 15:37:25 +0900

    feat(BaseBallGame Class): userInput 메서드 작성
    유저에게 숫자 입력을 하라는 지시를 내리는 메서드

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -5,4 +5,8 @@ public class BaseBallGame {
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void userInput() {
+        System.out.print("숫자를 입력해주세요  :  ");
+    }
 }
