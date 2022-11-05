cd58df8ad4aa31b6e33846615930484735d554c4
lzlkolo <lzlko1209@gmail.com>
2022-11-05 10:52:13 +0900

    refactor: 변수 scope 변경(local->global)

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -3,9 +3,10 @@ package baseball;
 import java.util.List;
 
 public class GameController {
+    Computer computer = new Computer();
+    List<Integer> goalNumber = Computer.createRandomNumber();
 
     public static void start() {
         Message.start();
-        List<Integer> goalNumber = Computer.createRandomNumber();
     }
 }
