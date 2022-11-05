9e862bedfcf589a354a54f0b4d9168c43b066101
1eejisoo <jisu991012@gmail.com>
2022-11-05 16:24:15 +0900

    Feat: 입력 값을 List<Integer> 형태로 변환하는 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,15 @@
 package baseball;
 
-public class BaseballGame {
+import java.util.ArrayList;
+import java.util.List;
 
+public class BaseballGame {
+    public List<Integer> stringToIntegerList(String inputNumber) {  // 입력 값을 List<Integer>로
+        List<Integer> number = new ArrayList<>();
+
+        for (int digit = 0; digit < inputNumber.length(); digit++) {
+            number.add(Integer.parseInt(String.valueOf(inputNumber.charAt(digit))));
+        }
+        return number;
+    }
 }
