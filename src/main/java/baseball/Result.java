4e8a0364b138c0ed46eacb1680c06a650bec6dac
situodys <situodys@gmail.com>
2022-11-05 17:09:59 +0900

    feat(Result): 게임 결과에 맞는 문구 생성 구현

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -4,9 +4,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Result {
-    private static final String STRIKE = "strike";
-    private static final String BALL = "ball";
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
     private static final int THREE_STRIKE = 3;
+    private static final int ZERO = 0;
+    private static final String NOTHING = "낫싱";
+    private static final String GAME_END = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String WHITE_SPACE = " ";
 
     private Map<String, Integer> result;
 
@@ -33,6 +37,28 @@ public class Result {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public String toString() {
+        if (countStrike() == ZERO && countBall() == ZERO) {
+            return NOTHING;
+        }
+        if (isEnd()) {
+            return GAME_END;
+        }
+        return buildAnswerAnnouncement();
+    }
+
+    private String buildAnswerAnnouncement() {
+        StringBuilder announcement = new StringBuilder();
+        if (countBall() == ZERO) {
+            return announcement.append(countStrike() + STRIKE).toString();
+        }
+        if (countStrike() == ZERO) {
+            return announcement.append(countBall() + BALL).toString();
+        }
+        return announcement.append(countBall() + BALL + WHITE_SPACE + countStrike() + STRIKE).toString();
     }
 
 }
