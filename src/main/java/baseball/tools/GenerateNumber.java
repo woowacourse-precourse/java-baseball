219bfdad5f05b086f486f1c83f7e6b896c2636e0
Kid-Chang <pch3310@gmail.com>
2022-11-06 00:39:27 +0900

    refactor(tools/GenerateNumber): pickNumberInRange함수를 사용하도록 변경
     pickUniqueNumbersInRange -> pickNumberInRange으로 변경하면서, 이에 따라 구현 코드 수정

--- src/main/java/baseball/tools/GenerateNumber.java
+++ src/main/java/baseball/tools/GenerateNumber.java
@@ -1,10 +1,18 @@
 package baseball.tools;
 
-import java.util.List;
+import java.util.HashSet;
 
 public class GenerateNumber {
 
-    public List<Integer> pickThreeUniqueRandomNumber(){
-        return  camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange(1, 9, 3);
+    public HashSet<Integer> pickThreeUniqueRandomNumber() {
+        HashSet<Integer> uniqueNumbers = new HashSet<>();
+
+        while (uniqueNumbers.size() < 3) {
+            int newNum = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(1, 9);
+            if (uniqueNumbers.contains(newNum))
+                continue;
+            uniqueNumbers.add(newNum);
+        }
+        return uniqueNumbers;
     }
 }
