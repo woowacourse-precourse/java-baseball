acbcfa69ce0bbbd273b4b1db7fa7fef9f588e955
mirasoy <nkskjjbb@gmail.com>
2022-11-05 22:18:15 +0900

    [Test](FunctionTest) : 사용자 정답값 입력 기능 테스트 코드

--- src/test/java/baseball/FunctionTest.java
+++ src/test/java/baseball/FunctionTest.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.List;
 
 import org.junit.jupiter.api.Nested;
@@ -20,6 +27,55 @@ public class FunctionTest {
 			for (Integer number : computer) {
 				assertThat(computer).containsOnlyOnce(number);
 			}
+		}
+	}
+
+	@Nested
+	class UserInputIntegerTest {
+		@Test
+		void 사용자_입력값_유효성확인_길이넘침() {
+			String input = "1234";
+			SetSystemInput(input);
+			assertThatThrownBy(() -> BaseballUtil.getUserInteger()).isInstanceOf(IllegalArgumentException.class);
+
+		}
+
+		@Test
+		void 사용자_입력값_유효성확인_길이부족() {
+			String input = "12";
+			SetSystemInput(input);
+			assertThatThrownBy(() -> BaseballUtil.getUserInteger()).isInstanceOf(IllegalArgumentException.class);
+		}
+
+		@Test
+		void 사용자_입력값_유효성확인_숫자외문자입력() {
+			String input = "12a";
+			SetSystemInput(input);
+			assertThatThrownBy(() -> BaseballUtil.getUserInteger()).isInstanceOf(IllegalArgumentException.class);
+
+		}
+
+		@Test
+		void 사용자_입력값_유효성확인_중복숫자입력() {
+			String input = "122";
+			SetSystemInput(input);
+			assertThatThrownBy(() -> BaseballUtil.getUserInteger()).isInstanceOf(IllegalArgumentException.class);
+
+		}
+		@Test
+		void 사용자_입력값_Integer_List_반환() {
+			String input = "123";
+			SetSystemInput(input);
+			List<Integer> inputList=  BaseballUtil.getUserInteger();
+			
+			assertThat(inputList).contains(1,2,3);
+		}
+
+		private void SetSystemInput(String input) {
+			OutputStream out = new ByteArrayOutputStream();
+			System.setOut(new PrintStream(out));
+			InputStream in = new ByteArrayInputStream(input.getBytes());
+			System.setIn(in);
 		}
 	}
 }
