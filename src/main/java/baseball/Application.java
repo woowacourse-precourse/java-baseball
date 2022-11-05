98f52b3e4d5152ebbaa65f6f9dbe2b100b1de023
JaehongDev <println@kakao.com>
2022-11-06 00:07:58 +0900

    refactor (Application) : 변수 이름 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,13 @@
 package baseball;
 
 
+import baseball.domain.baseball.BaseballContextImpl;
 import baseball.domain.baseball.BaseballGame;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        var game = new BaseballGame();
-        game.run();
+        var baseballGame = new BaseballGame(new BaseballContextImpl());
+        baseballGame.run();
     }
 }
