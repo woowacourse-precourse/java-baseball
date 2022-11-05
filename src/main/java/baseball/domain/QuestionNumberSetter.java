7fa709f79d8952941eabe40e3ca582d2537e4a35
Sehbeom <j3beom@gmail.com>
2022-11-05 14:25:16 +0900

    feat(QuestionNumberSetter): GREEN 숫자 비교 기능 구현
    사용자 입력 숫자와 랜덤 숫자 비교 기능 구현
    
    QuestionNumberSetter.java 의
    comparingWithRandomNumbers() 함수 추가

--- src/main/java/baseball/domain/QuestionNumberSetter.java
+++ src/main/java/baseball/domain/QuestionNumberSetter.java
@@ -1,8 +1,11 @@
 package baseball.domain;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import baseball.constants.ComparingResults;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class QuestionNumberSetter {
@@ -25,5 +28,41 @@ public class QuestionNumberSetter {
 		} while (randomNumbers.contains(uniqueNumber));
 
 		return uniqueNumber;
+	}
+
+	public Map<ComparingResults, Integer> compareWithRandomNumbers(List<Integer> inputNumbers) {
+		Map<ComparingResults, Integer> comparingResult = new HashMap<>();
+
+		for (int i = 0; i < inputNumbers.size(); i++) {
+			int inputNumberPosition = i;
+			int inputNumber = inputNumbers.get(i);
+
+			if (randomNumbers.contains(inputNumber)) {
+				int randomNumberPosition = randomNumbers.indexOf(inputNumber);
+				int count = 1;
+
+				if (randomNumberPosition == inputNumberPosition) {
+					if (comparingResult.containsKey(ComparingResults.STRIKE)) {
+						count = comparingResult.get(ComparingResults.STRIKE) + 1;
+					}
+
+					comparingResult.put(ComparingResults.STRIKE, count);
+
+					continue;
+				}
+
+				if (comparingResult.containsKey(ComparingResults.BALL)) {
+					count = comparingResult.get(ComparingResults.BALL) + 1;
+				}
+
+				comparingResult.put(ComparingResults.BALL, count);
+			}
+		}
+
+		if (comparingResult.isEmpty() == true) {
+			comparingResult.put(ComparingResults.NOTHING, 1);
+		}
+
+		return comparingResult;
 	}
 }
