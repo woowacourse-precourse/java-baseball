4311ebe0990f25f86d748a7200b96b2c6ae16e41
Choi-JJunho <junho5336@gmail.com>
2022-11-05 16:16:49 +0900

    fix(Validation): remove local variable

--- src/main/java/baseball/ValidationHandler.java
+++ src/main/java/baseball/ValidationHandler.java
@@ -4,21 +4,15 @@ public class ValidationHandler {
 
     private static final int NUMBER_LENGTH = 3;
     private static final String REGEX_NOT_NUMBER = "^\\d";
-    private final String inputString;
 
-    ValidationHandler(String inputString) {
-        this.inputString = inputString;
+    ValidationHandler() {
     }
 
-    public boolean validateNumberLength() {
-        if (inputString.length() == NUMBER_LENGTH) {
-            return true;
-        }
-
-        return false;
+    public boolean validateLength(String inputValue) {
+        return inputValue.length() == NUMBER_LENGTH;
     }
 
-    public boolean isNumber() {
-        return inputString.matches(REGEX_NOT_NUMBER);
+    public boolean isNumber(String inputValue) {
+        return inputValue.matches(REGEX_NOT_NUMBER);
     }
 }
