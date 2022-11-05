8194bc81477220cbb0ef1e93e444669993821c5d
Jin409 <sualng123@gmail.com>
2022-11-05 17:00:19 +0900

    feat(Judge, Constants): 정답 여부를 판단하는 Judge 클래스 생성 및 기능 구현
    - Judge 클래스 생성
    - Ball 횟수를 세는 기능
    - Strike 횟수를 세는 기능
    - 답인지 아닌지 확인하는 기능

--- src/main/java/baseball/utils/Constants.java
+++ src/main/java/baseball/utils/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final int VALID_LENGTH = 3;
     public static final int VALID_MIN_RANGE = 1;
     public static final int VALID_MAX_RANGE = 9;
+    public static final int CORRECT_STRIKE_COUNT = 3;
 }
