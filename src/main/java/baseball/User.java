1fb2f02ea0009b4f631c2c9f7151bc5137b39630
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-05 14:03:20 +0900

    refactor : user객체를 따로 class 분리 userNumber를 private로 선언하여 캡슐화 userNumber를 접근하기 위해서는 get,set메소드 이용 setUserNumber()메소드 에서는 요구사항과 같이 user에게 직접 숫자를 입력받는다 setUserNumber()메소드에서 입력받은 숫자를 toList()메소드를 이용해 List로 만든 후 userNumber에 저장 Vaildate Class를 만든 후 setUserNumber에 유효성 검사 추가 예정

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,2 +1,31 @@
-package baseball;public class User {
+package baseball;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+    private List<Integer> userNumber;
+
+    public void setUserNumber(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String userNumber = Console.readLine();
+
+        this.userNumber = toList(userNumber);
+    }
+
+    public List<Integer> toList(String userNumber){
+        List<Integer> userNumberList = new ArrayList<>();
+
+        for(int userNumberIndex = 0; userNumberIndex<userNumber.length();userNumberIndex++){
+            // validate를 통해 3글자 숫자임이 확정이므로 0의 아스키 코드 값이 48임을 이용해 문자를 숫자로 바꾸어준다.
+            userNumberList.add(userNumber.charAt(userNumberIndex) - 48);
+        }
+
+        return userNumberList;
+    }
+
+    public List<Integer> getUserNumber(){
+        return this.userNumber;
+    }
 }
