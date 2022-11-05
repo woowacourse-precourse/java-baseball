1d9323ae8803e21fff610d4b5344c4a3a15d9972
JaehongDev <println@kakao.com>
2022-11-05 11:51:24 +0900

    feature (Score) : 스코어도메인 요구사항 기능 구현 완료

---
+++ src/main/java/baseball/domain/score/Score.java
@@ -0,0 +1,37 @@
+package baseball.domain.score;
+
+public class Score {
+    private final Integer ball;
+    private final Integer strike;
+
+    private Score(final Integer ball, final Integer strike) {
+        this.ball = ball;
+        this.strike = strike;
+    }
+
+    public static Score generateZero() {
+        return new Score(0, 0);
+    }
+
+    public String result() {
+        if (this.strike == 0 && this.ball == 0) {
+            return "낫싱";
+        }
+        StringBuilder result = new StringBuilder();
+        if (this.ball != 0) {
+            result.append(this.ball).append("볼 ");
+        }
+        if (this.strike != 0) {
+            result.append(this.strike).append("스트라이크");
+        }
+        return result.toString().trim();
+    }
+
+    public Score strike() {
+        return new Score(this.ball, this.strike + 1);
+    }
+
+    public Score ball() {
+        return new Score(this.ball + 1, this.strike);
+    }
+}
