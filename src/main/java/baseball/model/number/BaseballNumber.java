de82f9641ee12564088627eaba77788d6c38a34a
Cyma-s <ssol0319@naver.com>
2022-11-05 23:24:59 +0900

    feat(BaseballNumber): add override function equals()
    equals() checks given BaseballNumber has same ballNumber.

--- src/main/java/baseball/model/number/BaseballNumber.java
+++ src/main/java/baseball/model/number/BaseballNumber.java
@@ -20,4 +20,17 @@ public class BaseballNumber {
         return ballInteger;
     }
 
+    @Override
+    public boolean equals(Object object) {
+        if(!(object instanceof BaseballNumber)) {
+            return false;
+        }
+
+        BaseballNumber otherBaseballNumber = (BaseballNumber) object;
+        Integer otherBaseballValue = otherBaseballNumber.ballNumber;
+        if(otherBaseballValue.equals(this.ballNumber)) {
+            return true;
+        }
+        return false;
+    }
 }
