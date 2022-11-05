43ca36442b01a4526ff6451edf18bf1fc91dabe9
tdmdgh <tdmdgh@gmail.com>
2022-11-05 18:54:46 +0800

    test: add Input 유효성 테스트

--- src/test/java/baseball/ApplicationTest2.java
+++ src/test/java/baseball/ApplicationTest2.java
@@ -12,7 +12,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest2 extends NsTest {
     @DisplayName("올바른 랜덤 값 생성")
@@ -24,6 +24,18 @@ class ApplicationTest2 extends NsTest {
             assertThat(element).isBetween(1, 9);
         Set<Integer> numSet = new HashSet<>(result);
         assertEquals(3,numSet.size());
+    }
+    @DisplayName("Input 유효성 테스트")
+    @Test
+    void isValidInput_test(){
+        assertFalse(Application.isValidInput(""));
+        assertFalse(Application.isValidInput("1234"));
+        assertFalse(Application.isValidInput("abcd"));
+        assertFalse(Application.isValidInput("111"));
+        assertFalse(Application.isValidInput("012"));
+        assertFalse(Application.isValidInput("A12"));
+
+        assertTrue(Application.isValidInput("123"));
     }
 
 
