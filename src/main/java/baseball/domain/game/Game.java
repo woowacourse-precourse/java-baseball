1c2b4bbdb4b1da61002ada8115ddf8d6a0277feb
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-11-05 20:16:52 +0900

    feat :: 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.

--- src/main/java/baseball/domain/game/Game.java
+++ src/main/java/baseball/domain/game/Game.java
@@ -16,5 +16,6 @@ public class Game {
             }
         }
         OutputView.printEnd();
+        OutputView.printRestart();
     }
 }
--- src/main/java/baseball/domain/view/OutputView.java
+++ src/main/java/baseball/domain/view/OutputView.java
@@ -2,6 +2,8 @@ package baseball.domain.view;
 
 public class OutputView {
     public static final String startGame = "숫자 야구 게임을 시작합니다.";
+    public static final String endGame = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String restartGame = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static void printStart() {
         System.out.println(startGame);
@@ -23,6 +25,10 @@ public class OutputView {
     }
 
     public static void printEnd() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(endGame);
+    }
+
+    public static void printRestart() {
+        System.out.println(restartGame);
     }
 }
