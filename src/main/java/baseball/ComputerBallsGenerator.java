e1d651b370c072a9715eabbc3c89b66b055b9749
choiejae <ejae8319@gmail.com>
2022-11-05 13:04:27 +0900

    feat: ComputerBallsGenerator 생성 테스트 구현

--- src/main/java/baseball/ComputerBallsGenerator.java
+++ src/main/java/baseball/ComputerBallsGenerator.java
@@ -1,5 +1,12 @@
 package baseball;
 
 public class ComputerBallsGenerator {
+  private int maxLen;
 
+  public ComputerBallsGenerator() {
+    this(3);
+  }
+  public ComputerBallsGenerator(int maxLen) {
+    this.maxLen = maxLen;
+  }
 }
