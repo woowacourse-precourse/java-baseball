177ace2039286fea1d46fa544eae8770dae12721
이현진 <ehj1107@naver.com>
2022-11-05 15:29:11 +0900

    [build] 랜덤 숫자 받아오는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static List<Integer> randomNumber;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        StartGame();
+    }
+
+    public static void StartGame(){
+        GetRandomNumber();
+    }
+
+    public static void GetRandomNumber(){
+        randomNumber = new ArrayList<>();
+        while (randomNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!Application.randomNumber.contains(randomNumber)) {
+                Application.randomNumber.add(randomNumber);
+            }
+        }
     }
 }
