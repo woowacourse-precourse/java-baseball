4d68e5738f7414fb80e8b2de1a417b2077b1b83d
d0ri123 <lsh81550@gmail.com>
2022-11-05 19:26:48 +0900

    Feat: 입력받은 숫자가 조건에 맞는지 확인하는 기능 구현

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -14,4 +14,24 @@ public class Baseball {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         user_answer = Integer.parseInt(Console.readLine());
     }
+
+    public boolean possibleNum(String response){
+        boolean result = false;
+        for(int i=0; i<response.length()-1; i++){
+            if((response.charAt(i)!=response.charAt(i+1)) || (response.charAt(i)!=response.charAt(2-i)))
+                result = true; //숫자 3개가 모두 다른 숫자인지 확인
+        }
+
+        for(int i=0; i<response.length(); i++){
+            if(response.charAt(i)>=49 || response.charAt(i)<=57)
+                result = true; //숫자인지 확인
+        }
+        return result;
+    }
+
+    public void generateEx(int response){
+        String str_response = String.valueOf(response);
+        if(!(str_response.length()==3 && possibleNum(str_response)))
+           throw new IllegalArgumentException();
+    }
 }
