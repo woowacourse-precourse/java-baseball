673add028ac7b4f2bcc4a414f64be3c16e5b7bd3
왕준영 <wangjunyoung@wangjun-yeong-ui-MacBookPro.local>
2022-11-05 15:52:41 +0900

    refactor: 사용자에게 입력 받을때 메세지 출력 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class User {
     public static String readLine(){
+        System.out.print("숫자를 입력해주세요 : ");
         String str = Console.readLine();
         return str;
     }
