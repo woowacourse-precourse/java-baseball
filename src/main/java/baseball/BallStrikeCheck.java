f7f3aa308f1479c70bbde0d2ca9c2239124ce770
hyowonhan <wain1719@gmail.com>
2022-11-05 21:08:04 +0900

    fix: ball, strike 개수 초기화 기능 추가

--- src/main/java/baseball/BallStrikeCheck.java
+++ src/main/java/baseball/BallStrikeCheck.java
@@ -3,15 +3,10 @@ package baseball;
 import java.util.List;
 
 public class BallStrikeCheck {
-    Number num = new Number();
-
-    List computerNumber =  num.randomNumber();
-    String userNumber = num.userNumber();
-
+    Number number = new Number();
     int ball = 0;
     int strike = 0;
-
-    private void ballStrikeCount() {
+    private void ballStrikeCount(List computerNumber, String userNumber) {
         for(int i = 0; i<userNumber.length(); i++) {
             if(computerNumber.contains(userNumber.charAt(i)+"") == true && i != computerNumber.indexOf(userNumber.charAt(i)+"")) {
                 ball += 1;
@@ -21,15 +16,26 @@ public class BallStrikeCheck {
         }
     }
 
-    public void ballStrikePrint() {
-        ballStrikeCount();
+    public int ballStrikePrintSet(List computerNumber, String userNumber) {
+        ballStrikeCount(computerNumber, userNumber);
         if(ball == 0 && strike == 0) {
             System.out.println("낫싱");
+            ball = 0;
+            strike = 0;
+            return 0;
         } else if (strike == 3) {
             System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            ball = 0;
+            strike = 0;
+            return 2;
         } else {
-            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+            System.out.printf("%d볼 %d스트라이크\n",ball,strike);
+            ball = 0;
+            strike = 0;
+            return 1;
         }
     }
+
+
 }
