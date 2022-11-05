1fe990dbcaaad0e67054e1e9906cd0b0d6efa0a2
comet7001 <comet7001@gmail.com>
2022-11-06 06:28:15 +0900

    feat(application): implement the beginning of the game
    Added new codes to start the game
    -enum class(Instructions for user)
    -main method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,51 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        List<Integer> digitsToCompare= new ArrayList<>();
+        System.out.println(Referee.START_GAME.ordered());
+        putRandomNumbers(digitsToCompare);
+        playGame();
+    }
+
+    public static void playGame(){
+
+    }
+
+    public static void putRandomNumbers(List<Integer> digitsToCompare) {
+        while (digitsToCompare.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            getDistinctNumbers(randomNumber, digitsToCompare);
+        }
+    }
+
+    public static void getDistinctNumbers(int randomNumber, List<Integer> digitsToCompare){
+        if (!digitsToCompare.contains(randomNumber)) {
+            digitsToCompare.add(randomNumber);
+        }
     }
 }
+enum Referee {
+    START_GAME("숫자 야구 게임을 시작합니다"),
+    ASK_NUMBERS("숫자를 입력해주세요 : "),
+    BALL("볼 "),
+    STRIKE("스트라이크 "),
+    NOTHING("낫싱"),
+    FINISH_GAME("세 개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+    ASK_AGAIN_OR_NOT("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
+    private final String saying;
+
+    Referee(String saying){
+        this.saying = saying;
+    }
+    public String ordered(){
+        return saying;
+    }
+}
