a877e90eaba8a208506804884666377827105ad7
wjh2012 <wjh2012@naver.com>
2022-11-05 17:06:05 +0900

    test(calculator): add ScoreCalculator 테스트 볼, 스트라이크 계산 테스트

---
+++ src/test/java/baseball/ScoreCalculatorTest.java
@@ -0,0 +1,32 @@
+package baseball;
+
+import baseball.game.ScoreBoard;
+import baseball.game.ScoreCalculator;
+import java.util.Arrays;
+import java.util.List;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ScoreCalculatorTest {
+
+    @Test
+    void calculator_strike_테스트() {
+        List<Integer> targetNumbers = Arrays.asList(1, 2, 3);
+        List<Integer> attackNumbers = Arrays.asList(1, 2, 3);
+
+        ScoreBoard scoreBoard = ScoreCalculator.calculate(targetNumbers, attackNumbers);
+
+        Assertions.assertThat(scoreBoard.getBallAndStrike()).isEqualTo(Arrays.asList(0, 3));
+    }
+
+    @Test
+    void calculator_ball_테스트() {
+        List<Integer> targetNumbers = Arrays.asList(1, 2, 3);
+        List<Integer> attackNumbers = Arrays.asList(3, 1, 2);
+
+        ScoreBoard scoreBoard = ScoreCalculator.calculate(targetNumbers, attackNumbers);
+
+        Assertions.assertThat(scoreBoard.getBallAndStrike()).isEqualTo(Arrays.asList(3, 0));
+    }
+}
+
