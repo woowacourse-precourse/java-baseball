4b47938003f41d508b596cab2ee0accac9aada63
penrose15 <seongju99@naver.com>
2022-11-05 16:22:43 +0900

    refactor : refactor Application.java
    Resolves: #12

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,32 +1,44 @@
 package baseball;
 
-import baseball.utils.GenerateRandomNumber;
-import baseball.utils.StartGame;
+import baseball.number.client.ClientNumbers;
+import baseball.number.client.HintsAboutNumbers;
+import baseball.number.computer.ComputerNumbers;
+import baseball.utils.validation.Validation;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
+
+    public static void main(String[] args){
         // TODO: 프로그램 구현
-        StartGame startGame = new StartGame();
-        GenerateRandomNumber generateRandomNumber = new GenerateRandomNumber();
+        ComputerNumbers computerNumbers = new ComputerNumbers();
         HintsAboutNumbers hint = new HintsAboutNumbers();
+        ClientNumbers clientNumbers = new ClientNumbers(new Validation());
 
-        Scanner sc = new Scanner(System.in);
-        int clientNumber;
-
-        startGame.printStartGameMessage();
         int quitGame;
-
+        List<Integer> client;
         boolean isMatched;
-        do{
-            int randomNumber = generateRandomNumber.generateRandomNumber();
-            System.out.println(randomNumber);
-            do {
-                startGame.printInputThreeNumber();
-                clientNumber = sc.nextInt(); //사용자 숫자 입력
 
-                isMatched = hint.hintAboutNumbers(clientNumber, randomNumber);
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        do{
+            List<Integer> computerNumber = computerNumbers.generateRandomNumbers();
+            for (Integer integer : computerNumber) {
+                System.out.print(integer); //for test
+            }
+            do {
+                System.out.print("숫자를 입력해주세요 : ");
+
+                try {
+                    client = clientNumbers.clientNumberToList();
+                } catch (IOException e) {
+                    throw new IllegalArgumentException("문자 변환 에러");
+                }
+
+                isMatched = hint.hintAboutNumbers(client, computerNumber);
                 //숫자 입력시 힌트 출력 메서드 추가
 
             } while (!isMatched); //입력한 숫자와 컴퓨터의 숫자가 같을때까지 반복
@@ -34,14 +46,25 @@ public class Application {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-            quitGame = sc.nextInt();
+            quitGame = quitGame();
             while(!(quitGame == 1 || quitGame ==2)) { //1 또는 2 입력할 때까지 반복
                 System.out.println("1 또는 2 만 입력해주십시오");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-                quitGame = sc.nextInt();
+                quitGame = quitGame();
             }
         } while(quitGame != 2); //1입력시 게임 재시작, 2 입력시 게임 종료
 
         System.out.println("게임을 종료합니다.");
+    }
+
+    static int quitGame() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int quit = 0;
+        try {
+            quit = Integer.parseInt(br.readLine());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return quit;
     }
 }
