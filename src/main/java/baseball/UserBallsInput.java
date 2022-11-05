80238aba0f6f189a0764e82e253435fce35c8ec9
choiejae <ejae8319@gmail.com>
2022-11-05 19:01:24 +0900

    feat: InputNumber validityChecker 테스트 구현

--- src/main/java/baseball/UserBallsInput.java
+++ src/main/java/baseball/UserBallsInput.java
@@ -5,23 +5,30 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserBallsInput {
-  private int maxLen;
 
-  public UserBallsInput() {
-    this(3);
-  }
-  public UserBallsInput(int maxLen) {
-    this.maxLen = maxLen;
-  }
-  public String inputNumber() {
-    String buffer = Console.readLine();
-    System.out.println(buffer);
-    return buffer;
-  }
-  public void inputNumberTest(String input) {
+    private int maxLen;
+
+    public UserBallsInput() {
+        this(3);
+    }
+
+    public UserBallsInput(int maxLen) {
+        this.maxLen = maxLen;
+    }
+
+    public String inputNumber() {
+        String buffer = Console.readLine();
+        System.out.println(buffer);
+        return buffer;
+    }
+
+    public void inputNumberTest(String input) {
 //    if (!validityChecker(input)) {
 //      throw new IllegalArgumentException();
 //    }
-    System.out.print(input);
-  }
+        System.out.print(input);
+    }
+
+    public void validityChecker() {
+    }
 }
