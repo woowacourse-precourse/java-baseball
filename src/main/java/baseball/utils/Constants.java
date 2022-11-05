1afaf58c9d6a6fab345cf9b36bfd42694ca162e5
Jin409 <sualng123@gmail.com>
2022-11-05 16:52:47 +0900

    feat(Constants): 전역에서 사용되는 상수 클래스에 선언
    - 전역에서 주로 사용되는 최소 범위, 최대 범위, 유효한 숫자의 길이를 상수 클래스에 모아 한번에 선언

---
+++ src/main/java/baseball/utils/Constants.java
@@ -0,0 +1,7 @@
+package baseball.utils;
+
+public class Constants {
+    public static final int VALID_LENGTH = 3;
+    public static final int VALID_MIN_RANGE = 1;
+    public static final int VALID_MAX_RANGE = 9;
+}
