69f64e1c9c73be4e2c2065176e9e78e9e49f34f1
saint6839 <saint6839@gmail.com>
2022-11-05 11:55:12 +0900

    feat(controller) : 컴퓨터와 관련된 기능 호출을 담당하는 ComputerController 생성

---
+++ src/main/java/baseball/controller/ComputerController.java
@@ -0,0 +1,12 @@
+package baseball.controller;
+
+import baseball.Ball;
+
+import java.util.List;
+
+public class ComputerController {
+
+    public List<Ball> generateRandomBalls() {
+        return null;
+    }
+}
