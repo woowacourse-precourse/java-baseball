ed345f3ae990422b091dc11e467ba7542c21300a
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-11-05 22:06:31 +0900

    feat : 사용자 입력
    세 자리수를 입력 받고, 입력이 올바른지 확인한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -7,13 +8,42 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        sendMassage(GameMassage.START_MASSAGE);
-        List<Integer> computer = randomThreeDigit();
+
+        try {
+            sendMassage(GameMassage.START_MASSAGE);
+
+            List<Integer> computer = randomThreeDigit();
+            sendMassage(GameMassage.INPUT_NUMBER);
+            List<Integer> user = getUserInput();
+
+        } catch (IllegalArgumentException e) {
+            System.out.println("IllegalArgumentException 발생");
+        }
 
     }
 
     private static void sendMassage(GameMassage massage){
         System.out.print(massage.getMassage());
+    }
+
+    private static String removeDuplicated(String str){
+        String removeDuplicatedCharRegex = "(([a-z])\\2{1,})";
+        return str.replaceAll(removeDuplicatedCharRegex, "");
+    }
+
+    private static List<Integer> getUserInput() {
+        List<Integer> values = new ArrayList<>();
+        String input = Console.readLine();
+        input = removeDuplicated(input);
+        String regex = "[1-9]{3,3}";
+
+        if (input.matches(regex)) {
+            for (int i = 0; i < input.length(); i++) {
+                values.add(input.charAt(i) - '0');
+            }
+            return values;
+        }
+        throw new IllegalArgumentException();
     }
 
     private static List<Integer> randomThreeDigit() {
