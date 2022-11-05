cbdddecb6e37d1d70c31b07073b4e81f7f0c4e8b
bini59 <myuni0234@gmail.com>
2022-11-05 23:23:34 +0900

    feat : input ints from user
    Added input and parse function
    - user input data, parse to integer list
    - string split by '' and trans to int

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +18,31 @@ public class Application {
         return nums;
     }
 
+    public static List<Integer> parseString(String s){
+        List<Integer> user = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            user.add(c-'0');
+        }
+        return user;
+    }
+
+    public static void game(){
+        List<Integer> computer = generateRandNum();
+        while(true){
+            String input = Console.readLine();
+            List<Integer> user = parseString(input);
+        }
+    }
+
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        while(true){
+            game();
+        }
+
+
     }
 }
