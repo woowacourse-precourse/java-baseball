504d83fb95d308fb2e7bd935faefc81ad2b7dd8d
Eungyu-code <Agyu625@gmail.com>
2022-11-05 15:00:47 +0900

    feat: replayGame 함수 추가
    게임의 재시작/종료 여부를 선택할 수 있음을 알리고, 사용자가 입력한 값을 저장하는 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -197,4 +197,14 @@ public class Application {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
     }
+
+    public static boolean replayGame() {
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        Scanner scanner = new Scanner(System.in);
+
+        String replayNumberString = scanner.nextLine();
+
+        return true;
+    }
 }
