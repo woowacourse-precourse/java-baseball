f1781f8e0fd23436b68a641c6ecedaae9f527760
김태용 <amico741@naver.com>
2022-11-05 18:20:09 +0900

    feat: 컴퓨터와 유저간 숫자 비교 메서드 추가
    strike개수와 ball개수를 이용하여 결과 반환하는 메서드

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -1,12 +1,29 @@
 package baseball;
 
+import java.util.ArrayList;
+
 public class Judge {
   private Computer computer;
   private Player player;
-
+  private static final int MAX_SIZE = 3;
   public Judge(){
     this.player = new Player();
     this.computer = new Computer();
     System.out.println("숫자 야구 게임을 시작합니다.");
+  }
+
+  public int compareBalls(){
+    ArrayList<Integer> playerNumbers = this.player.decideNumbers();
+    ArrayList<Integer> computerNumbers = this.computer.getNumbers();
+    int strike = 0;
+    int ball = 0;
+    for(int i = 0; i < MAX_SIZE; i++){
+      if(playerNumbers.get(i).equals(computerNumbers.get(i))) {
+        strike ++;
+      } else if (computerNumbers.contains(playerNumbers.get(i))) {
+        ball++;
+      }
+    }
+    return strike;
   }
 }
