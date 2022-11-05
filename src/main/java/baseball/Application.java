1479958200dbcb44b8aa42e9139a39859187bd86
minaver <psmjoshua@gmail.com>
2022-11-05 15:39:39 +0900

    feat(Application): Baseball 객체를 사용하여 서비스 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,27 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Baseball baseball = new Baseball();
+        baseball.start();
+        while (baseball.isGaming()){
+            try{
+                baseball.setGuessNumber();
+            } catch (IllegalArgumentException ie){
+                return;
+            }
+
+            System.out.println(baseball);
+            GuessResult guessResult = baseball.getGuessResult();
+            if (guessResult.equals(GuessResult.BALL_STRIKE)){
+                continue;
+            } else if(guessResult.equals(GuessResult.NOT_SWING)){
+                continue;
+            } else if(guessResult.equals(GuessResult.END)){
+                baseball.chooseContinue();
+                System.out.println(baseball.isGaming());
+            }
+        }
+
+        return;
     }
 }
