1107e7f502b8f6d79e6981cbbe93ad3ec1ba04db
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-05 22:25:49 +0900

    test(BallReader): 두 공이 다를 경우 거짓을 반환하는지 확인

--- src/test/java/baseball/testcase/BallReaderTest.java
+++ src/test/java/baseball/testcase/BallReaderTest.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -33,8 +35,40 @@ public class BallReaderTest {
     @Test
     void BALLREADER_예외_테스트() {
         List<Integer> firstBall = Randoms.pickUniqueNumbersInRange(1, 9, 3);
-        List<Integer> errorBall = List.of(1,2,3,4);
+        List<Integer> errorBall = List.of(1, 2, 3, 4);
 
-        assertThatThrownBy(()->ballReader.getResult(firstBall,errorBall)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> ballReader.getResult(firstBall, errorBall)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("BallReader가 3스트라이크를 일 때 게임이 끝났다 판단하는지 판독")
+    @RepeatedTest(5)
+    void BALLREADER_판독_결과가_참인_경우_테스트() {
+        List<Integer> firstBall = Randoms.pickUniqueNumbersInRange(1,9,3);
+        List<Integer> secondBall = new ArrayList<>(firstBall);
+
+        Map<String, Integer> result = ballReader.getResult(firstBall, secondBall);
+        System.out.println("result = " + result);
+        System.out.println();
+
+        assertThat(ballReader.isFinished(firstBall,secondBall)).isTrue();
+    }
+
+    @DisplayName("BallReader가 3스트라이크를 아닐 때 게임이 끝나지 않았다 판단하는지 판독")
+    @RepeatedTest(5)
+    void BALLREADER_판독_결과가_거짓인_경우_테스트() {
+        List<Integer> firstBall = Randoms.pickUniqueNumbersInRange(1,9,3);
+        List<Integer> secondBall = Randoms.pickUniqueNumbersInRange(1,9,3);
+
+        while(ballReader.isFinished(firstBall,secondBall)){
+            secondBall = Randoms.pickUniqueNumbersInRange(1,9,3);
+        }
+
+        Map<String, Integer> result = ballReader.getResult(firstBall, secondBall);
+        System.out.println("firstBall = " + firstBall);
+        System.out.println("secondBall = " + secondBall);
+        System.out.println("result = " + result);
+        System.out.println();
+
+        assertThat(ballReader.isFinished(firstBall,secondBall)).isFalse();
     }
 }
