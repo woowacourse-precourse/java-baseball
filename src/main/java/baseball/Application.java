80a189a4d624e9e0e568b22f0742436d651e98dd
wath1457 <wath1457@gmail.com>
2022-11-05 20:48:24 +0900

    feat: add printMessage func
    Print message according to userNumber

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,23 @@
 package baseball;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        while (true) {
+            int userNumber = -1;
+            printMessage(userNumber);
+        }
+
+    }
+
+    public static void printMessage(int userNumber) {
+        if (userNumber == 1) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+        } else {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
     }
 }
