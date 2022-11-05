01be717862bb29314e196b420bd165a1dc2516a0
saint6839 <saint6839@gmail.com>
2022-11-05 23:52:53 +0900

    feat(serviceTest) : 중복값 있을 때 예외가 던져지는지 테스트

--- src/test/java/baseball/service/PlayerServiceTest.java
+++ src/test/java/baseball/service/PlayerServiceTest.java
@@ -27,4 +27,25 @@ class PlayerServiceTest {
         Assertions.assertThat(playerBalls.size())
                   .isEqualTo(playerInput.length());
     }
+
+    @Test
+    void convertInputToBallList_중복값이_있을_때_예외가_던져지는지_테스트() {
+        //given
+        String playerInput = "111";
+        String[] inputSplits = playerInput.split("");
+
+        List<Ball> playerBalls = new ArrayList<>();
+        //when
+        //then
+        Assertions.assertThatThrownBy(() -> {
+                      for (String inputSplit : inputSplits) {
+                          Ball ball = new Ball(Integer.parseInt(inputSplit));
+                          validateDuplication(playerBalls, ball);
+                          playerBalls.add(ball);
+                      }
+                  })
+                  .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
