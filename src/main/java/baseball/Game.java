12537c7fb0835f9f805d3441a56f429377dddc97
meat7238 <meat7238@kakao.com>
2022-11-06 08:33:19 +0900

    Add Game.java (스트라이크볼 계산 기능) Update docs.README.md
    입력값과 정답을 입력받아 각 자리를 비교해 일치한다면 스트라이크값 증가, 그 외 정답에 포함된 갯수만큼 볼 증가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -8,21 +8,38 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class Game {
     private static final int RANDOM_START_RANGE = 1;
     private static final int RANDOM_END_RANGE = 9;
-    static final int MAX_NUMBER_COUNT = 3;
+    static final int MAX_NUMBER = 3;
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String OUT = "낫싱";
+    static int ballCount,strikeCount;
 
     Game() {
         LinkedList<Integer> answer = new LinkedList<>();
         answer = setComputerNumber();
-        LinkedList<Integer> UserAnswer = getUserAnswer();
+        LinkedList<Integer> userAnswer = getUserAnswer();
+        strikeBallCount(answer,userAnswer);
+    }
+
+    private void strikeBallCount(LinkedList<Integer> answer, LinkedList<Integer> userAnswer) {
+        for(int num : userAnswer){
+            if(answer.get(userAnswer.indexOf(num))==num) {
+                strikeCount++;
+                continue;
+            }
+            if(checkListContains(answer,num)){
+                ballCount++;
+            }
+        }
     }
 
     public LinkedList<Integer> setComputerNumber() {
-        int RandomNumber;
+        int randomNumber;
         LinkedList<Integer> result = new LinkedList<>();
-        while (result.size() < MAX_NUMBER_COUNT) {
-            RandomNumber = getRandomNumber();
-            if (!checkListContains(result, RandomNumber))
-                result.add(RandomNumber);
+        while (result.size() < MAX_NUMBER) {
+            randomNumber = getRandomNumber();
+            if (!checkListContains(result, randomNumber))
+                result.add(randomNumber);
         }
         return result;
     }
@@ -48,7 +65,7 @@ public class Game {
     }
 
     private static void checkInputValue(LinkedList<Integer> userAnswer) {
-        if (userAnswer.size() != Game.MAX_NUMBER_COUNT) {
+        if (userAnswer.size() != Game.MAX_NUMBER) {
             throw new IllegalArgumentException();
         }
         if (userAnswer.contains(0)) {
@@ -62,4 +79,6 @@ public class Game {
                 throw new IllegalArgumentException();
         }
     }
+
+
 }
