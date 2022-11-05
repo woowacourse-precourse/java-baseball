7ac1937c3f902456163c575d6e1754c582a60a4e
whibaek <jibaek0513@gmail.com>
2022-11-06 02:05:07 +0900

    feat(Game Class): add method
    add checkExceptionFromUserInputData method
    - To check invalid data
    - if data is invalided, generate exception
    - e.g.> 5412, 333, asdf ...

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,9 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -16,19 +18,49 @@ public class Game {
         return listNumber;
     }
 
-    public void gameStart(){
+    public void checkExceptionFromUserInputData(String userNumber){
+
+        Set<Character> setNumber = new HashSet<>();
+
+        if(userNumber.length() != 3){
+            throw new IllegalArgumentException();
+        }
+
+        for(int i=0; i<3; i++){
+            if(userNumber.charAt(i) < '1' && userNumber.charAt(i) > '9') {
+                throw new IllegalArgumentException();
+            }
+            setNumber.add(userNumber.charAt(i));
+        }
+
+        if(setNumber.size() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+
+    }
+
+    public int gameStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
         Computer computer = new Computer();
         computer.makeAnswerNumber();
 
         while(computer.getStrike() != 3){
             System.out.print("숫자를 입력해주세요 : ");
-            List<Integer>userNumber = makeUserNumberToList(readLine());
 
-            computer.checkNumberCount(userNumber);
-            System.out.println(computer.countResultString());
+            String userNumberString = readLine();
+            checkExceptionFromUserInputData(userNumberString);
+
+            try {
+                List<Integer>userNumber = makeUserNumberToList(Integer.parseInt(userNumberString));
+                computer.checkNumberCount(userNumber);
+                System.out.println(computer.countResultString());
+            } catch (IllegalArgumentException e) {
+                return -1;
+            }
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        return 1;
     }
 
 }
