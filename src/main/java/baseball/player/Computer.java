d8598b29814bb00b02705481a941989816ba8f9f
SeokHwan An <asok369258@naver.com>
2022-11-05 22:55:31 +0900

    feat : 컴퓨터에서 문제를 생성하는 기능 추가

--- src/main/java/baseball/player/Computer.java
+++ src/main/java/baseball/player/Computer.java
@@ -3,11 +3,26 @@ package baseball.player;
 import static baseball.util.Constant.*;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
     private List<Integer> randomNum;
+
+    /**
+     * 문제를 생성하는 기능
+     */
+    public void createProblem() {
+        randomNum = new ArrayList<>();
+        while(checkNumLength()){
+            int num = createRandomNum();
+            if(checkOverlap(num)){
+                continue;
+            }
+            randomNum.add(num);
+        }
+    }
 
     /**
      * 랜덤한 수를 생성하는 기능
