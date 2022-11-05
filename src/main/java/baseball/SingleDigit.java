4ec21db6c137cffeae6cdf1bdf42c86890d60f0b
hodomaroo <enghemdwkd@gmail.com>
2022-11-05 20:43:00 +0900

    [Feat] : SingleDigit 클래스 구현
    SingleDigit 클래스의 객체 생성 간의 범위 체크
    메서드(validateNumberInRange)와 객체 간의 일치 여부를 반환하는equals
    기능을 구현하였고, SingleDigitTest를 통해 각 기능의 정상 동작 여부를
    확인하였습니다.

--- src/main/java/baseball/SingleDigit.java
+++ src/main/java/baseball/SingleDigit.java
@@ -1,27 +1,31 @@
 package baseball;
 
 /**
- * 한자리 수를 담는 일급컬랙션 클래스
+ * 한자리 수(singleDigit)를 담는 일급컬랙션 클래스
  *
  * @author 호두
  * @version 1.0
  */
 
 public class SingleDigit {
-    /**
-     * @param otherNumber 현재 Number 객체와 비교할 다른 Number 객체
-     * @return 두 객체의 값이 같다면 true, 그렇지 않다면 false 반환
-     */
+    private int singleDigit;
+
+    public SingleDigit(int singleDigit) throws IllegalArgumentException{
+        if(!validateDigitInRange(singleDigit)){
+            throw new IllegalArgumentException("각 자리의 숫자는 [1-9] 사이의 값을 가져야 합니다.");
+        }
+        this.singleDigit = singleDigit;
+    }
+
     public boolean equals(SingleDigit otherNumber){
-        return false;
+        return this.singleDigit == otherNumber.singleDigit;
     }
 
     /**
-     * 입력으로 주어진 수가 알맞은 입력값인지 확인함
-     * @param number 검증할 int 형 숫자값
-     * @return 알맞은 입력값인 경우 true, 그렇지 않다면 false 반환
+     * @param singleDigit
+     * @return [1,9] 사이의 값인지 여부 반환
      */
-    private boolean validateNumber(int number){
-        return false;
+    private boolean validateDigitInRange(int singleDigit){
+        return (1 <= singleDigit && singleDigit <= 9);
     }
 }
