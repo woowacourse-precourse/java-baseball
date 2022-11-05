794ad8b093b8548cfe4778ba5b393b7e4cd71f87
whibaek <jibaek0513@gmail.com>
2022-11-06 00:58:49 +0900

    feat(Game Class): add method
    add gameStart method
    - progresses whole game
      - user inputs value
      - check strike and ball
      - if hit the answer, quit the method

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,6 +3,8 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Game {
 
     public List<Integer> makeUserNumberToList(String userNumberString){
@@ -13,6 +15,21 @@ public class Game {
             userNumber /= 10;
         }
         return listNumber;
+    }
+
+    public void gameStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        Computer computer = new Computer();
+        computer.makeAnswerNumber();
+
+        while(computer.getStrike() != 3){
+            System.out.print("숫자를 입력해주세요 : ");
+            List<Integer>userNumber = makeUserNumberToList(readLine());
+
+            computer.checkNumberCount(userNumber);
+            System.out.println(computer.countResultString());
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
 }
