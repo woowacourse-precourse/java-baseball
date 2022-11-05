87a92294456fb092e5c04686c028764297dbb629
jissssoo1717 <jisoolee7101@gmail.com>
2022-11-05 20:52:08 +0900

    refactor: PlayerInput으로 클래스 분리 후 main에서 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,81 +6,12 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        InputTest();
-    }
-
-    public static void InputTest() {
+        PlayerInput player = new PlayerInput();
         String input = Console.readLine();
-        System.out.println(input);
 
-        ExceptionIfFalse(CheckInputLength(input));
-        // 길이 체크
-        ExceptionIfFalse(CheckIsInputNumber(input));
-        // 숫자인지 체크
-        ExceptionIfFalse(CheckIsNumberDupli(input));
-        // 중복 체크
-    }
-
-    public static boolean CheckInputLength(String input) {
-        boolean isLengthCorrect = true;
-
-        if(input.length() != 3) {
-            isLengthCorrect = false;
-        }
-
-        return isLengthCorrect;
-    }
-
-    public static boolean CheckIsNumberDupli(String input) {
-        boolean isNotDuplication = true;
-
-        for(int i = 0; i < input.length()-1; i++) {
-            if(!ReturnFalseIfDupli(input, i)){
-                isNotDuplication = false;
-            }
-        }
-
-        return isNotDuplication;
-    }
-
-    public static boolean ReturnFalseIfDupli(String input, int idxNumber) {
-        for(int i = idxNumber + 1; i < input.length(); i++) {
-            if(input.charAt(idxNumber) == input.charAt(i)) {
-                return false;
-            }
-        }
-
-        return true;
-    }
+        player.InputTest(input);
 
 
-    public static boolean CheckIsInputNumber(String input) {
-        boolean isNumberCorrect = true;
-
-        for(int i = 0; i < input.length(); i++) {
-            if(!ReturnIsNumberDigit(input.charAt(i))) {
-                isNumberCorrect = false;
-                break;
-            }
-        }
-
-        return isNumberCorrect;
-    }
-
-    public static boolean ReturnIsNumberDigit(char digit) {
-        if(digit >= '1' && digit <= '9') {
-            return true;
-        }
-
-        return false;
-    }
-
-    public static void ExceptionIfFalse(boolean isResultFalse)
-            throws IllegalArgumentException {
-
-        if(!isResultFalse) {
-            throw new IllegalArgumentException();
-        }
     }
 
 
---
+++ src/main/java/baseball/PlayerInput.java
@@ -0,0 +1,76 @@
+package baseball;
+
+
+public class PlayerInput {
+    public void InputTest(String input) {
+
+        ExceptionIfFalse(CheckInputLength(input));
+        // 길이 체크
+        ExceptionIfFalse(CheckIsInputNumber(input));
+        // 숫자인지 체크
+        ExceptionIfFalse(CheckIsNumberDupli(input));
+        // 중복 체크
+    }
+
+    public boolean CheckInputLength(String input) {
+        boolean isLengthCorrect = true;
+
+        if(input.length() != 3) {
+            isLengthCorrect = false;
+        }
+
+        return isLengthCorrect;
+    }
+
+    public boolean CheckIsNumberDupli(String input) {
+        boolean isNotDuplication = true;
+
+        for(int i = 0; i < input.length()-1; i++) {
+            if(!ReturnFalseIfDupli(input, i)){
+                isNotDuplication = false;
+            }
+        }
+
+        return isNotDuplication;
+    }
+
+    public boolean ReturnFalseIfDupli(String input, int idxNumber) {
+        for(int i = idxNumber + 1; i < input.length(); i++) {
+            if(input.charAt(idxNumber) == input.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public boolean CheckIsInputNumber(String input) {
+        boolean isNumberCorrect = true;
+
+        for(int i = 0; i < input.length(); i++) {
+            if(!ReturnIsNumberDigit(input.charAt(i))) {
+                isNumberCorrect = false;
+                break;
+            }
+        }
+
+        return isNumberCorrect;
+    }
+
+    public boolean ReturnIsNumberDigit(char digit) {
+        if(digit >= '1' && digit <= '9') {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void ExceptionIfFalse(boolean isResultFalse)
+            throws IllegalArgumentException {
+
+        if(!isResultFalse) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
