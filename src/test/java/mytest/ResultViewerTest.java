125e469ef93f1f238e7c520bb9caedcc40aca1b9
areyouhun <areyouhun@gmail.com>
2022-11-05 16:04:41 +0900

    style(test): 코드 순서 변경

--- src/test/java/mytest/ResultViewerTest.java
+++ src/test/java/mytest/ResultViewerTest.java
@@ -11,59 +11,59 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class ResultViewerTest {
-    @DisplayName("볼과 스트라이크의 개수가 예상된 프린트문과 똑같이 출력되는지 확인")
+    @DisplayName("볼과 스트라이크 개수가 예상한 프린트문과 똑같이 출력되는지 확인")
     @Test
     void case1() {
-        OutputStream expectedPrintout = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(expectedPrintout));
-        System.out.println("3스트라이크");
-
         List<Integer> scores = List.of(0, 3);
         OutputStream actualPrintout = new ByteArrayOutputStream();
         System.setOut(new PrintStream(actualPrintout));
         ResultViewer.printOut(scores);
+
+        OutputStream expectedPrintout = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(expectedPrintout));
+        System.out.println("3스트라이크");
 
         assertThat(actualPrintout.toString()).isEqualTo(expectedPrintout.toString());
     }
 
     @Test
     void case2() {
-        OutputStream expectedPrintout = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(expectedPrintout));
-        System.out.println("2볼");
-
         List<Integer> scores = List.of(2, 0);
         OutputStream actualPrintout = new ByteArrayOutputStream();
         System.setOut(new PrintStream(actualPrintout));
         ResultViewer.printOut(scores);
+
+        OutputStream expectedPrintout = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(expectedPrintout));
+        System.out.println("2볼");
 
         assertThat(actualPrintout.toString()).isEqualTo(expectedPrintout.toString());
     }
 
     @Test
     void case3() {
-        OutputStream expectedPrintout = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(expectedPrintout));
-        System.out.println("낫싱");
-
         List<Integer> scores = List.of(0, 0);
         OutputStream actualPrintout = new ByteArrayOutputStream();
         System.setOut(new PrintStream(actualPrintout));
         ResultViewer.printOut(scores);
+
+        OutputStream expectedPrintout = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(expectedPrintout));
+        System.out.println("낫싱");
 
         assertThat(actualPrintout.toString()).isEqualTo(expectedPrintout.toString());
     }
 
     @Test
     void case4() {
-        OutputStream expectedPrintout = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(expectedPrintout));
-        System.out.println("1볼 1스트라이크");
-
         List<Integer> scores = List.of(1, 1);
         OutputStream actualPrintout = new ByteArrayOutputStream();
         System.setOut(new PrintStream(actualPrintout));
         ResultViewer.printOut(scores);
+
+        OutputStream expectedPrintout = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(expectedPrintout));
+        System.out.println("1볼 1스트라이크");
 
         assertThat(actualPrintout.toString()).isEqualTo(expectedPrintout.toString());
     }
