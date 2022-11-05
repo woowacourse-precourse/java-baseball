12d848403d6dd910c09478bfe1992891963e7d07
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-05 15:04:18 +0900

    feat(BallMaker): 공을 만드는 기능을 가진 BallMaker 생성

---
+++ src/main/java/baseball/game/BallMaker.java
@@ -0,0 +1,16 @@
+package baseball.game;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BallMaker {
+    public List<Integer> getRandomBall() {
+        List<Integer> randomBall = new ArrayList<>();
+        return randomBall;
+    }
+
+    public List<Integer> getUserBall() {
+        List<Integer> userBall = new ArrayList<>();
+        return userBall;
+    }
+}
