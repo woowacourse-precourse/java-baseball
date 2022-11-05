f4e458cd310791145a9abc3232c0defe8d2dc290
kimjeonghan <[rlawjdgks318@naver.com]>
2022-11-05 19:06:58 +0900

    test(ApplicationTest): 어플리케이션 실행 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -28,6 +28,28 @@ class ApplicationTest extends NsTest {
         );
     }
 
+	@Test
+	void exceptionTest() {
+		assertRandomNumberInRangeTest(
+			() -> {
+				assertThatThrownBy(() -> runException("246", "135", "1", "597", "589", "3"))
+					.isInstanceOf(IllegalArgumentException.class);
+			},
+			1, 3, 5, 5, 8, 9
+		);
+	}
+
+	@Test
+	void normalGameStartTest() {
+		assertRandomNumberInRangeTest(
+			() -> {
+				run("987", "654", "321", "312", "132", "213", "123", "2");
+				assertThat(output()).contains("낫싱", "낫싱", "2볼 1스트라이크", "3볼", "2볼 1스트라이크", "3스트라이크", "게임 종료");
+			},
+			1, 2, 3
+		);
+	}
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
