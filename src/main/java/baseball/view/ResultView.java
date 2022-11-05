62953dc952ca3d3d4ec50fd496ae054e7f604853
qdeu22 <qdeu22@gmail.com>
2022-11-05 13:36:27 +0900

    feat(ResultView): 결과 화면 기능 구현

--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -1,4 +1,38 @@
 package baseball.view;
 
 public class ResultView {
+    public static final String GAME_QUIT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String STRING_FORMAT = "%d%s";
+    public static final String NOTHING = "낫싱";
+    public static final String BALL = "볼 ";
+    public static final String STRIKE = "스트라이크";
+
+    public static void resultPrint(int strike, int ball) {
+        ballInfo(ball);
+        strikeInfo(strike);
+        nothingInfo(strike, ball);
+        System.out.println();
+    }
+
+    private static void strikeInfo(int strike) {
+        if (strike != 0) {
+            System.out.printf(STRING_FORMAT, strike, STRIKE);
+        }
+    }
+
+    private static void ballInfo(int ball) {
+        if (ball != 0) {
+            System.out.printf(STRING_FORMAT, ball, BALL);
+        }
+    }
+
+    private static void nothingInfo(int strike, int ball) {
+        if (ball == 0 && strike == 0) {
+            System.out.print(NOTHING);
+        }
+    }
+
+    public static void endGameMessage() {
+        System.out.println(GAME_QUIT);
+    }
 }
