e4ffa7fd70590bf99cdb31a6be0650d31bd35bfa
jonghun <jonghuncu@gmail.com>
2022-11-05 17:52:52 +0900

    Feat : 정답 숫자 3개 정하기 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        answerNumber();
+    }
+
+    public static void answerNumber() {
+        List<Integer> arrLst = new ArrayList<>();
+        List<Integer> answerLst = new ArrayList<>();
+
+        for(int i=1; i<10; i++) {
+            arrLst.add(i);
+        }
+        Collections.shuffle(arrLst);
+        for(int i=0; i<3; i++) {
+            answerLst.add(i, arrLst.get(i));
+        }
     }
 }
