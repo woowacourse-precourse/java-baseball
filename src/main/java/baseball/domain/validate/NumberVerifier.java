c51130932ba8aa550980c9eb3e1dce5a3ee6a927
wonyongChoi05 <rltgjqmduftlagl@gmail.com>
2022-11-05 19:32:34 +0900

    feat :: 사용자가 입력한 숫자가 얼마나 일치하는지 판단한다.

---
+++ src/main/java/baseball/domain/validate/NumberVerifier.java
@@ -0,0 +1,18 @@
+package baseball.domain.validate;
+
+import baseball.domain.view.OutputView;
+
+public class NumberVerifier {
+    public static void numberVerifier(String randomNumbers, String inputNumbers) {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            if (randomNumbers.charAt(i) == inputNumbers.charAt(i)) {
+                strike++;
+            } else if (randomNumbers.contains(String.valueOf(inputNumbers.charAt(i)))) {
+                ball++;
+            }
+        }
+        OutputView.printResult(strike, ball);
+    }
+}
