a034806a76894f97ed59d76d4a2cf59cebf1cc7a
Hayoon <hy97@sju.ac.kr>
2022-11-05 21:08:59 +0900

    test : 정리한 기능 목록이 정상 동작함을 테스트 코드로 확인하기 위한 GameTest.java 테스트 파일 생성

---
+++ src/test/java/study/GameTest.java
@@ -0,0 +1,27 @@
+package study;
+
+import baseball.Game;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameTest {
+
+    Game game = new Game();
+
+    /**
+     * JUnit 5와 AssertJ를 이용하여 본인이 정리한 기능 목록이 정상 동작함을 테스트 코드로 확인한다.
+     */
+
+    @Test
+    void 사용자가_입력받은_수를_String에서_ListInteger로_데이터_타입_변환() {
+
+        String input = "321";
+        List<Integer> integers = new ArrayList<>();
+
+        assertThat(game.stringToListInteger(input).getClass().getName()).isEqualTo(integers.getClass().getName());
+    }
+}
