980b51cb5601ee3819a696f39f50faa555e61399
newh08 <newh08@gmail.com>
2022-11-05 19:03:08 +0900

    refactor: delete function unrelated with Model in MVC, update function related with Model in MVC, change some variables to class variable
    Couple of functions were deleted {
    - all print sentence is deleted
    - checkStrike, checkBall will be moved to Game Class
    - printResult will be moved to OutputView Class
    - main : The role of main function will be moved to GameController Class
    - constructor: Delete constructor because computerNumberList is not use in User class.
    }
    Couple of functions and variables were updated {
    - userNumber, userNumberList variables change to class variable.
    - inputNumber: Change name to inputGameNumber. Change return type to void since userNumber change to class variable.
    - errorControl: Change name to makeError since it is more suitable name.
    - checkNumber: Delete parameter since userNumber change to class variable.
    - checkType: Change parameter name to align with where it uses.
    - keepOrNot: Change name to inputEndNumber since it is more suitable name.
    }

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -3,78 +3,52 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 
 public class User {
-    List<Integer> computerNumberList;
     boolean keepGame = true;
-    boolean gameOngoing = true;
-
+    List<Integer> userNumberList;
+    int userNumber;
     User() {
-
     }
 
-    User(List<Integer> computerNumberList) {
-        this.computerNumberList = computerNumberList;
-    }
+    void inputGameNumber() {
+        userNumber = -1;
 
-    void main() {
-        while (gameOngoing) {
-            int userNumber = inputNumber();
-            checkNumber(userNumber);
-            List<Integer> userNumberList = numberToList(userNumber);
-            int strike = checkStrike(computerNumberList, userNumberList);
-            int ball = checkBall(computerNumberList, userNumberList, strike);
-            printResult(strike, ball);
-            if (strike == 3) {
-                gameOngoing = false;
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                keepOrNot();
-            }
-        }
-    }
-
-    int inputNumber() {
-        int userNumber = -1;
-
-        System.out.print("숫자를 입력해주세요 : ");
         String userInput = Console.readLine();
         if (checkType(userInput)) {
             userNumber = Integer.parseInt(userInput);
         }
-        return userNumber;
+        checkNumber();
     }
 
-    void errorControl() {
-        System.out.println("잘못된 값을 입력했습니다.");
-        System.out.println("프로그램을 종료합니다.");
+    private void makeError() {
         throw new IllegalArgumentException();
     }
 
-    void checkNumber(int userNumber) {
+    private void checkNumber() {
         if (100 <= userNumber & userNumber <= 999) {
             if (numberToList(userNumber).contains(0)) {
-                errorControl();
+                makeError();
                 return;
             }
             return;
         }
-        errorControl();
+        makeError();
     }
 
-    boolean checkType(String userNumber) {
+    private boolean checkType(String userInput) {
         try {
-            Integer.parseInt(userNumber);
+            Integer.parseInt(userInput);
             return true;
         } catch (NumberFormatException e) {
-            errorControl();
+            makeError();
         }
         return false;
     }
 
     List<Integer> numberToList(int userNumber) {
-        List<Integer> userNumberList = new ArrayList<>(3);
+        userNumberList = new ArrayList<>(3);
         for (int divideNumber = 100; divideNumber >= 1; divideNumber /= 10) {
             userNumberList.add(userNumber / divideNumber);
             userNumber -= (userNumber / divideNumber) * divideNumber;
@@ -82,53 +56,16 @@ public class User {
         return userNumberList;
     }
 
-    int checkStrike(List<Integer> computerNumberList, List<Integer> userNumberList) {
-        int strike = 0;
-        for (int index = 0; index < 3; index++) {
-            if (Objects.equals(computerNumberList.get(index), userNumberList.get(index))) {
-                strike++;
-            }
-        }
-        return strike;
-    }
-
-    int checkBall(List<Integer> computerNumberList, List<Integer> userNumberList, int strike) {
-        int ball = 0;
-        for (int index = 0; index < 3; index++) {
-            if (computerNumberList.contains(userNumberList.get(index))) {
-                ball++;
-            }
-        }
-        return ball - strike;
-    }
-
-    void printResult(int strike, int ball) {
-        if (Objects.equals(strike, 0) & Objects.equals(ball, 0)) {
-            System.out.println("낫싱");
-            return;
-        } else if (Objects.equals(strike, 0)) {
-            System.out.println(ball + "볼");
-            return;
-        } else if (Objects.equals(ball, 0)) {
-            System.out.println((strike + "스트라이크"));
-            return;
-        }
-        System.out.println(ball + "볼 " + strike + "스트라이크");
-    }
-
-    void keepOrNot() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    void inputEndNumber() {
         String userInput = Console.readLine();
-        if (checkType(userInput)) {
-            if (userInput.equals("1")) {
-                keepGame = true;
-                return;
-            } else if (userInput.equals("2")) {
-                System.out.println("프로그램을 종료합니다.");
-                keepGame = false;
-                return;
-            }
+        checkType(userInput);
+        if (userInput.equals("1")) {
+            keepGame = true;
+            return;
+        } else if (userInput.equals("2")) {
+            keepGame = false;
+            return;
         }
-        errorControl();
+        makeError();
     }
 }
