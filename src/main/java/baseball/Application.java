ed59d3f444723f900d907dfa22526188b938742d
Heejun-0219 <heejun0219@gmail.com>
2022-11-05 18:03:28 +0900

    feat(Check_contain_Number, Check_Strike_or_Ball) : 4. Make sure that the number in the first position among the numbers entered by the user is included in the number set by the computer. 5. If included, take the index from the number the computer has and increase the ball if the index is the same, if the strike is different. 6. Repeat 3 times with the number entered by the user.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.io.*;
 public class Application {
     public static List<Integer> Computer_Number() {
         List<Integer> computer = new ArrayList<>();
@@ -19,17 +18,42 @@ public class Application {
         }
         return computer;
     }
-
     public static void isValidLength(String s) {
         if (s.length() != 3) {
             throw new IllegalArgumentException("세자리 수보다 크거나 작음");
+        }
+    }
+    public static void Check_Strike_or_Ball(List count, int user_index, int computer_index){
+        int ball = 0;
+        int strike = 0;
+        if (computer_index == user_index)
+            strike++;
+        else
+            ball++;
+        count.set(0, (int)count.get(0) + ball);
+        count.set(1, (int)count.get(1) + strike);
+    }
+    public static void Check_contain_Number(String user, List computer, List count) {
+        for (int i = 0; i < 3; i++) {
+            int current_user = user.charAt(i) - '0';
+
+            if (computer.contains(current_user)) {
+                int computer_index = computer.indexOf(current_user);
+                Check_Strike_or_Ball(count, i, computer_index);
+            }
         }
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         List<Integer> computer = Computer_Number();
 
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        System.out.print("숫자를 입력해주세요 : ");
         String user = Console.readLine();
         isValidLength(user);
+
+        List<Integer> count = new ArrayList<Integer>(Arrays.asList(0, 0));
+        Check_contain_Number(user, computer, count);
     }
 }
