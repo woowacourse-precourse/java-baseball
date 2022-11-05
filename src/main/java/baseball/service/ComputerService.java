0205315455ce6fa6b6bfc8e4961914118cee0c93
saint6839 <saint6839@gmail.com>
2022-11-05 20:40:30 +0900

    feat(service) : 컴퓨터가 랜덤한 중복되지 않는 세 개의 숫자를 리스트에 담아 반환하도록 구현

--- src/main/java/baseball/service/ComputerService.java
+++ src/main/java/baseball/service/ComputerService.java
@@ -1,11 +1,23 @@
 package baseball.service;
 
 import baseball.Ball;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerService {
     public List<Ball> generateRandomBalls() {
-        return null;
+        List<Ball> computerBalls = new ArrayList<>();
+
+        while (computerBalls.size() < 3) {
+            Ball computerBall = new Ball(Randoms.pickNumberInRange(1, 9));
+            if (computerBalls.contains(computerBall)) {
+                continue;
+            }
+            computerBalls.add(computerBall);
+        }
+
+        return computerBalls;
     }
 }
