31090fc5853d509a708790ad96629c4ffab02f91
lee <higgs95@naver.com>
2022-11-05 18:53:20 +0900

    feat(Type): 게임 로직 및 숫자 관련 상수 분리

---
+++ src/main/java/baseball/type/GameType.java
@@ -0,0 +1,6 @@
+package baseball.type;
+
+public class GameType {
+    public static final boolean NO_ANSWER = false;
+    public static final boolean YES_ANSWER = true;
+}
--- src/main/java/baseball/type/NumberType.java
+++ src/main/java/baseball/type/NumberType.java
@@ -4,4 +4,11 @@ public class NumberType {
     public static final int MINIMUM_NUMBER = 1;
     public static final int MAXIMUM_NUMBER = 9;
     public static final int NUMBER_SIZE = 3;
+
+    public static final int STRIKE_ZERO = 0;
+    public static final int BALL_ZERO = 0;
+
+    public static final int RETRY = 1;
+    public static final int EXIT = 2;
+
 }
