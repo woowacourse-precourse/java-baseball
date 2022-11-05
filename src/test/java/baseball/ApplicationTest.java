3001835a52db29412c2dfb1328122f15190281c9
bini59 <myuni0234@gmail.com>
2022-11-05 20:55:35 +0900

    test(generateNum) : test generateRandNum()
    test generateRandNum()
    - check List<Integer> size is 3

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,6 +3,8 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,6 +28,13 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 랜덤_생성_테스트(){
+        List<Integer> computers = Application.generateRandNum();
+        assertThat(computers.size()).isEqualTo(3);
+
     }
 
     @Override
