ff416f733566ecb09b8b6c10ab882777848c04bf
cocomozila <joseyun94@naver.com>
2022-11-05 23:18:33 +0900

    feat: 유저의 입력값이 숫자가 아닐경우 예외처리

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -1,6 +1,7 @@
 package baseball.domain;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 public class User {
@@ -12,6 +13,7 @@ public class User {
     // 일급컬렉션 유저넘버
     public User(String inputUserNumber) {
         List<Integer> userNumberList = stringToUserNumberList(inputUserNumber);
+        validateSize(userNumberList);
         this.userNumberList = userNumberList;
     }
 
@@ -19,11 +21,20 @@ public class User {
     private List<Integer> stringToUserNumberList(String inputUserNumber) {
         List<Integer> userNumberList = new ArrayList<>();
         for (char c : inputUserNumber.toCharArray()) {
+            validateCharacter(c);
             userNumberList.add(Integer.parseInt(String.valueOf(c)));
         }
         return userNumberList;
     }
 
+    // 유저의 입력값이 숫자가 아닐경우 예외처리
+    private void validateCharacter(char digit) {
+        if (!Character.isDigit(digit)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // 유저의 입력값이 3자리가 아닐경우 예외처리
     private void validateSize(List<Integer> userNumber) {
         if (userNumber.size() != USER_NUMBER_SIZE) {
             throw new IllegalArgumentException();
