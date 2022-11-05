876d5aead14ae1e6ef6b75f137f983759b05f25b
Voyager003 <85725033+Voyager003@users.noreply.github.com>
2022-11-05 12:09:04 +0900

    feat: 구현 기능목록 2번,3번 2번: '숫자를 입력해주세요'. 출력, 3번: 제공 라이브러리를 이용하여 입력값받기

--- src/main/java/baseball/logic/Inputnumber.java
+++ src/main/java/baseball/logic/Inputnumber.java
@@ -1,10 +1,13 @@
 package baseball.logic;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.Scanner;
 
 public class Inputnumber {
 
     public String inputNum(){
-        System.out.println("숫자를 입력해주세요");
+        System.out.println("숫자를 입력해주세요 : ");
+        return Console.readLine();
     }
 }
