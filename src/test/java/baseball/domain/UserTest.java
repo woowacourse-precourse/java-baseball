d58d7726639944a3622059f6f1beb7f2dcdd1432
Jzakka <mouse4786@gmail.com>
2022-11-05 20:37:38 +0900

    test: 게임 재시작 입력 테스트 추가

--- src/test/java/baseball/domain/UserTest.java
+++ src/test/java/baseball/domain/UserTest.java
@@ -1,28 +1,42 @@
 package baseball.domain;
 
-import org.assertj.core.api.Assertions;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
     User user = new User();
 
     @Test
-    void input() {
+    void 유저_입력_숫자_검증() {
         command("123");
-        user.input();
-        Assertions.assertThat(user.getNumbers()).isEqualTo("123");
+        user.inputNumbers();
+        assertThat(user.getNumbers()).isEqualTo("123");
 
         command("456");
-        user.input();
-        Assertions.assertThat(user.getNumbers()).isEqualTo("456");
+        user.inputNumbers();
+        assertThat(user.getNumbers()).isEqualTo("456");
 
         command("789");
-        user.input();
-        Assertions.assertThat(user.getNumbers()).isEqualTo("789");
+        user.inputNumbers();
+        assertThat(user.getNumbers()).isEqualTo("789");
+    }
+
+    @Test
+    void 게임_재시작_입력_검증_올바름() {
+        command("1");
+        assertThat(user.inputRetry()).isEqualTo('1');
+
+        command("2");
+        assertThat(user.inputRetry()).isEqualTo('2');
+    }
+
+    @Test
+    void 게임_재시작_입력_검증_예외발생() {
+        command("3");
+        assertThatThrownBy(() -> user.inputRetry()).isInstanceOf(IllegalArgumentException.class);
     }
 
     private void command(final String... args) {
