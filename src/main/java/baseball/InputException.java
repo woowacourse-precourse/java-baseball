87e365e03df1d63745ab389df044b550f44d57f0
leeyongjun <leeyongjun0310@naver.com>
2022-11-05 19:42:32 +0900

    feat(InputException): 입력된 숫자가 모두 숫자인지 확인하는 기능 구현

--- src/main/java/baseball/InputException.java
+++ src/main/java/baseball/InputException.java
@@ -1,7 +1,16 @@
 package baseball;
 
 public class InputException {
-    public static boolean checkLength(String number){
+    public static boolean checkLength(String number) {
         return number.length() == Constant.NUMBER_LENGTH;
+    }
+
+    public static boolean isAllNumber(String number) {
+        for (int i = 0; i < number.length(); i++) {
+            if (!Character.isDigit(number.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
     }
 }
