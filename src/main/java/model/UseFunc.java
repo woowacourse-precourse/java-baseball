9dc76ec4b45c8e1545088df1fcdd54aeda6ff2a8
dmdrk1414 <seungchan141414@gmail.com>
2022-11-05 13:15:04 +0900

    :sparkles:[Feat] UseFunc 클래스에 각각의 자릿수가 같은지 확인하는 메서드 추가

--- src/main/java/model/UseFunc.java
+++ src/main/java/model/UseFunc.java
@@ -1,6 +1,36 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UseFunc {
 
-   
+    // 서로 다른 3자리의 수인지 확인
+    // 서로 다른 3자리의 수인지 확인하는 방법은?
+    // 1번을 for문 2번?
+    public static boolean isNumOfIndividually(String str) {
+        List<Integer> splitStrList = getListStrArr2Split(str);
+        int cntSameNum = 0;
+        for (int value : splitStrList) {
+            cntSameNum = 0;
+            for (int index = 0; index < splitStrList.size(); index++) {
+                if (value == splitStrList.get(index)) {
+                    cntSameNum = cntSameNum + 1;
+                }
+            }
+            if (cntSameNum > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List getListStrArr2Split(String str) {
+        String[] strArr = str.split("");
+        List<Integer> list = new ArrayList<>();
+        for (String value : strArr) {
+            list.add(Integer.valueOf(value));
+        }
+        return list;
+    }
 }
