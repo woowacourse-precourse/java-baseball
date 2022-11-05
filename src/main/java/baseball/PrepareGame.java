bfe9983da7daa0cf925b5baf0760b8350ab2a163
dydtmd6801 <dydtmd6801@naver.com>
2022-11-05 16:39:31 +0900

    feat: 각 자리수를 다르게 하는 기능

--- src/main/java/baseball/PrepareGame.java
+++ src/main/java/baseball/PrepareGame.java
@@ -9,7 +9,14 @@ public class PrepareGame {
     public List<Integer> setComputerNumber(){
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            computer = isEachDigit(computer);
+        }
+        return computer;
+    }
+
+    public List<Integer> isEachDigit(List<Integer> computer) {
+        int randomNumber = Randoms.pickNumberInRange(1,9);
+        if(!computer.contains(randomNumber)){
             computer.add(randomNumber);
         }
         return computer;
