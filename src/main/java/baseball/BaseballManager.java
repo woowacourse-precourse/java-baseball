a6ed2277f6240d379a1a8be026cbdf670714a6d1
woosung1223 <dntjd991223@naver.com>
2022-11-05 17:23:37 +0900

    feat(BaseballManager): addUserBaseballNumInfo 메소드 추가

---
+++ src/main/java/baseball/BaseballManager.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class BaseballManager {
+    BaseballNumber userNumber;
+    BaseballNumber computerNumber;
+    ScoreManager scoreManager;
+
+
+    void addUserBaseballNumInfo(BaseballNumber baseballNum1) {
+        userNumber = baseballNum1;
+    }
+
+}
