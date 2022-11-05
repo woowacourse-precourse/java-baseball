e7e69201bc1e9e47dc1644818eed0bf4a5dba1c7
JUHA <khabh@naver.com>
2022-11-05 23:24:33 +0900

    refactor(Game): static 메서드 삭제
    게임 재시작 시 Game 인스턴스를 다시 생성하지 않도록 run에서 컴퓨터의 숫자 설정 메서드 호출 추가
    
    BREAKING CHANGE: - player 인스턴스 변수로 수정
    - playTheNextRound 메서드 분리
    - 사용자가 새로운 게임을 원하는지 확인하는 메서드 추가 (main에서 게임 재시작 필요성 체크를 위해 사용)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,12 @@ import baseball.domain.Game;
 
 public class Application {
     public static void main(String[] args) {
-        Game.play();
+        boolean playerWantNewGame = true;
+        Game game = new Game();
+
+        while (playerWantNewGame) {
+            game.run();
+            playerWantNewGame = game.dosePlayerWantNewGame();
+        }
     }
 }
--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -5,38 +5,32 @@ import java.util.ArrayList;
 public class Game {
     private final static int THREE_STRIKES = 3;
     private final static int RESTART = 1;
-    private final static Player player = new Player();
 
+    private final Player player;
     private final Computer computer;
 
-    Game() {
+    public Game() {
+        this.player = new Player();
         this.computer = new Computer();
     }
 
-    private void run() {
+    public void run() {
+        computer.setStrikeNumbers();
         boolean isGameCleared = false;
+
         while(!isGameCleared) {
-            ArrayList<Integer> playerGuess = player.guessNumbers();
-            int strikeCount = computer.provideHint(playerGuess);
-            isGameCleared = (strikeCount == THREE_STRIKES);
+            int roundResult = playTheNextRound();
+            isGameCleared = (roundResult == THREE_STRIKES);
         }
         Output.printGameClear();
     }
 
-    private boolean askPlayerForRestart() {
+    private int playTheNextRound() {
+        ArrayList<Integer> playerGuess = player.guessNumbers();
+        return computer.provideHint(playerGuess);
+    }
+
+    public boolean dosePlayerWantNewGame() {
         return player.chooseRestartOrExit() == RESTART;
-    }
-
-    private static boolean playNewGame() {
-        Game game = new Game();
-        game.run();
-        return game.askPlayerForRestart();
-    }
-
-    public static void play() {
-        boolean needNewGame = true;
-        while (needNewGame) {
-            needNewGame = playNewGame();
-        }
     }
 }
