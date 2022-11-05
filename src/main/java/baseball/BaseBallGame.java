2ea18fc572f083ef1603d3937a0a89b9928cdb7f
sujin0607 <goweg0607@gmail.com>
2022-11-06 03:55:27 +0900

    feat: 재시작 여부를 판단하고 실행시켜주는 reStart함수 추가

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -59,6 +59,18 @@ public class BaseBallGame {
         return strikeAndBall;
     }
 
+    public void reStart(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userInput = Console.readLine();
+        if(userInput.equals("1")){
+            run();
+        }else if(userInput.equals("2")){
+            return;
+        }else{
+            throw new IllegalStateException();
+        }
+    }
+
     public void run() {
         List<Integer> computerNumber = randomNumbers();
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -71,6 +83,7 @@ public class BaseBallGame {
 
             if (strike == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                reStart();
                 break;
             } else if (strike != 0 && ball != 0) {
                 System.out.printf("%d볼 %d스트라이크\n", strike, ball);
