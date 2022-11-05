c30eabf04ee2d24aa52483744a6323b394314642
shpark30 <seohyeonpark30.gmail.com>
2022-11-05 21:50:24 +0900

    feat(Hitter): in charge of game answer
    To generate and be in charge of answer(3 numbers)
    make Hitter class.

---
+++ src/main/java/baseball/participants/Hitter.java
@@ -0,0 +1,21 @@
+package baseball.participants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+public class Hitter {
+    public static StrikeZone swingZone;
+
+    public Hitter() {
+    }
+
+    public void swing() {
+        List<Integer> swingList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            swingList.add(pickNumberInRange(1,9));
+        }
+        swingZone = new StrikeZone(swingList);
+    }
+}
