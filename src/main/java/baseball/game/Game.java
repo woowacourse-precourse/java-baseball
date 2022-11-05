78bd8eccfa616ff72d7d365c10e8904ca5648022
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-05 15:15:28 +0900

    feat(Game): 기본 생성자와 BallMaker, BallReader를 주입해주는 생성자 제작

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,8 +1,26 @@
 package baseball.game;
 
+import java.util.List;
+import java.util.Map;
+
 public class Game {
-    public String play(){
-        return "";
+
+    private int numberOfBall;
+    private int numberOfAttempts = 0;
+
+    BallMaker ballMaker;
+    BallReader ballReader;
+
+    public Game() {
+        this.numberOfBall = 3;
+        this.ballMaker = new BallMaker();
+        this.ballReader = new BallReader();
+    }
+
+    public Game(BallMaker ballMaker, BallReader ballReader) {
+        this.numberOfBall = 3;
+        this.ballMaker = ballMaker;
+        this.ballReader = ballReader;
     }
 
 }
