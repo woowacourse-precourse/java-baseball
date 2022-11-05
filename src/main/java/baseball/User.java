d9e3c04b4cfda50ae978892e0b135c498e81ea31
tenta <eunoia3802@gmail.com>
2022-11-05 14:36:50 +0900

    fix: 숫자 입력값 정수에서 문자열로 변경

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -3,21 +3,26 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 public class User {
+    private String number;
 
-    public int inputNumber() {
+    public void inputNumber() {
         Computer computer = new Computer();
         computer.startGame();
-        int number = Integer.parseInt(Console.readLine());
-        return number;
+        number = Console.readLine();
     }
 
     public List<Integer> createUserNumber() {
+       inputNumber();
+       String[] splitNumber = number.split("");
        List<Integer> userNumbers = new ArrayList<>();
-       while (userNumbers.size() < 3) {
-           userNumbers.add(inputNumber());
+       for (String number : splitNumber) {
+           int numberToInt = Integer.parseInt(number);
+           userNumbers.add(numberToInt);
        }
        return userNumbers;
     }
