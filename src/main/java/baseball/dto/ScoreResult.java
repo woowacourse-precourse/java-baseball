73289ead33cd2b19df06138f37e77b67e00a3471
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-05 15:06:45 +0900

    feat<ScoreResult>: 점수 결과에 타입을 반환하는 함수 구현
    - 타입에는 전부 스트라이크, 볼, 스트라이크, 스트라이크와 볼, 낫싱이 있다.
    - 게임에서 처리나 출력할 때 편하게 하기 위해 타입 반환

--- src/main/java/baseball/dto/ScoreResult.java
+++ src/main/java/baseball/dto/ScoreResult.java
@@ -17,11 +17,36 @@ public class ScoreResult {
         return ball;
     }
 
-    public boolean isAllStrike() {
+    public ScoreResultType getType() {
+        if (allStrike()) {
+            return ScoreResultType.ALL_STRIKE;
+        } else if (onlyStrike()) {
+            return ScoreResultType.STRIKE;
+        } else if (onlyBall()) {
+            return ScoreResultType.BALL;
+        } else if (strikeAndBall()) {
+            return ScoreResultType.STRIKE_BALL;
+        }
+        return ScoreResultType.NOTHING;
+    }
+
+    private boolean onlyBall() {
+        return ball > 0 && strike == 0;
+    }
+
+    private boolean onlyStrike() {
+        return strike > 0 && ball == 0;
+    }
+
+    private boolean strikeAndBall() {
+        return strike > 0 && ball > 0;
+    }
+
+    private boolean allStrike() {
         return strike == 3;
     }
 
-    public boolean isNothing() {
+    private boolean nothing() {
         return strike == 0 && ball == 0;
     }
 
