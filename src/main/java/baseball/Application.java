8dd46c5727f497385e852cbee5e60dc55b72e219
Limjongho <whdgh9873@icloud.com>
2022-11-06 01:11:03 +0900

    feat(Application): 숫자 야구 main 메서드 구현 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.Constant.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int startOrEnd = START_GAME;
+        while (startOrEnd == START_GAME) {
+            List<Integer> uniqueRandomNumbers = pickUniqueNumbersInRange(END_INCLUSIVE, START_INCLUSIVE, RANGE);
+            GameNumber gameNumber = new GameNumber(uniqueRandomNumbers);
+
+            System.out.println(GAME_START_MESSAGE);
+            System.out.print(ASK_NUMBER_MESSAGE);
+
+            playGame(gameNumber);
+
+            String reGameAnswer = Console.readLine();
+            Exceptions.isValidReGameAnswer(reGameAnswer);
+            startOrEnd = Integer.parseInt(reGameAnswer);
+        }
     }
 
     public static List<Integer> pickUniqueNumbersInRange(
@@ -24,6 +41,46 @@ public class Application {
 
         return uniqueNumbers;
     }
+
+    private static void playGame(GameNumber gameNumber) {
+        while (true) {
+            String userNumbersInString = Console.readLine();
+            UserNumber userNumber = new UserNumber(userNumbersInString);
+
+            int strikes = gameNumber.getStrikes(userNumber);
+            int balls = gameNumber.getBalls(userNumber);
+
+            if (strikes + balls == NOTHING_POINT) {
+                System.out.println(NOTHING_MESSAGE);
+                continue;
+            }
+            if (strikes == WIN_POINT) {
+                showWinResult(strikes);
+                break;
+            }
+
+            StringBuilder roundResult = new StringBuilder();
+            showRoundResult(strikes, balls, roundResult);
+        }
+    }
+
+    private static void showWinResult(int strikes) {
+        System.out.println(strikes + STRIKE_MESSAGE);
+        System.out.println(WIN_GAME_MESSAGE);
+        System.out.println(RE_GAME_ASK_MESSAGE);
+    }
+
+    private static void showRoundResult(int strikes, int balls, StringBuilder roundResult) {
+        if (balls != 0) {
+            roundResult.append(balls).append(BALL_MESSAGE);
+        }
+        if (strikes != 0) {
+            roundResult.append(strikes).append(STRIKE_MESSAGE);
+        }
+
+        System.out.println(roundResult);
+    }
+
 
     public static void saveUniqueNumber(List<Integer> uniqueNumbers, int randomNumber) {
         if (isExistNumber(uniqueNumbers, randomNumber)) {
