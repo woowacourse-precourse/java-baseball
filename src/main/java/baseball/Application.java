f62e96aca6a07196b6d0b2e8366ccf36481577c7
UJ15 <dbwogml15@naver.com>
2022-11-05 20:45:59 +0900

    fix : add generator in gameEngin constructor

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,10 @@
 package baseball;
 
 import baseball.engine.BaseBallGame;
+import baseball.engine.BaseBallGameEngine;
+import baseball.engine.DefaultBaseBallGameEngine;
+import baseball.engine.NextStepNumberGenerator;
+import baseball.engine.NumberGenerator;
 import baseball.io.ConsoleInput;
 import baseball.io.ConsoleOutput;
 
@@ -8,7 +12,9 @@ public class Application {
     public static void main(String[] args) {
         ConsoleOutput output = new ConsoleOutput();
         ConsoleInput input = new ConsoleInput();
+        NumberGenerator generator = new NextStepNumberGenerator();
+        BaseBallGameEngine engine = new DefaultBaseBallGameEngine(generator);
 
-        new BaseBallGame(output, input).run();
+        new BaseBallGame(output, input, engine).run();
     }
 }
