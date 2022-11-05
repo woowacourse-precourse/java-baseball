a87b4955cd49cd9ec406ab3a190afa175f9169f9
pjw03659 <pjw03659@naver.com>
2022-11-05 17:20:08 +0900

    added input number funtion

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,11 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class Application {
-    public static List<Integer> makenumber(){
+    public static List<Integer> makeNumber(){
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -14,11 +15,22 @@ public class Application {
         }
         return computer;
     }
+    public static List<Integer> inputNumber(){
+        List<Integer> inputNumber = new ArrayList<>();
+        String input = new String();
+        input= Console.readLine();
+        for(int i=0; i<input.length();i++){
+            inputNumber.add(input.charAt(i)-'0');
+        }
+        return inputNumber;
+    }
     public static void main(String[] args) {
         List<Integer> computer = new ArrayList<>();
-        computer = makenumber();
-        
-
+        List<Integer> myNum = new ArrayList<>();
+        computer = makeNumber();
+        myNum = inputNumber();
+        System.out.println(computer);
+        System.out.println(myNum);
 
         }
     }
