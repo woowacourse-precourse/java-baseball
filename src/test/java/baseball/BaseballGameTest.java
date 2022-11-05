13f90f3ea2ddbaf5feef87e35871144eb69427ea
Yeongseon Park <yseon406@gmail.com>
2022-11-06 03:30:27 +0900

    test: isValidNumber() Test
    isValidNumber() 테스트 코드 작성

--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -2,6 +2,8 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BaseballGameTest {
@@ -10,5 +12,14 @@ public class BaseballGameTest {
     void computerNumber() {
         assertThat(baseballGame.createComputerNumber()).isNotEmpty();
         assertThat(baseballGame.createComputerNumber()).doesNotContain(0);
+    }
+
+    @Test
+    void isValidNumberTest() {
+        List<String> testNumber = List.of("012","102","120","1234","111","123","abc","972");
+        List<Boolean> result = List.of(false,false,false,false,false,true,false,true);
+        for(int index = 0; index<testNumber.size(); index++) {
+            assertThat(baseballGame.isValidNumber(testNumber.get(index))).isEqualTo(result.get(index));
+        }
     }
 }
