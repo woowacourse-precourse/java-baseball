d43d9ad1921f0006f0730813d5153f3cd0a044a7
gnoesnooj <306yyy@naver.com>
2022-11-05 23:17:00 +0800

    feat : referee 볼카운트 기능 구현 및 필드 초기화 기능 구현

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -4,24 +4,62 @@ package baseball.domain;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Referee {
-    // 볼 스트라이크 알려주기
-    private boolean[] usedNumber = new boolean[9];
 
-    private List<Integer> numbers = new ArrayList<>();
+    private static final int DIGITS = 3;
+
+    private final boolean[] usedNumber = new boolean[9];
+
+    private final List<Integer> numbers = new ArrayList<>();
+
+    private final BallCount ballCount;
+
+    public Referee() {
+        this.ballCount = new BallCount();
+    }
 
     public List<Integer> makeRandomValues() {
-        for (int index = 0; index < 3; index++) {
+        initNumbers();
+        for (int index = 0; index < DIGITS; index++) {
             int picked = Randoms.pickNumberInRange(1, 9);
             if (checkDuplicate(picked)) {
                 numbers.add(picked);
+                System.out.print(picked);
             } else {
                 index--;
             }
         }
         return numbers;
+    }
+
+    public String checkCounts(List<Integer> inputs) {
+        ballCount.init();
+
+        for (int input : inputs) {
+            if (numbers.contains(input)) {
+                ballCount.increaseBall();
+            }
+        }
+
+        for (int i = 0; i < DIGITS; i++) {
+            if (numbers.get(i).equals(inputs.get(i))) {
+                ballCount.decreaseBall();
+                ballCount.increaseStrike();
+            }
+        }
+        return ballCount.toString();
+    }
+
+    public boolean isAllStrike() {
+        return ballCount.isAllStrike();
+    }
+
+    private void initNumbers() {
+        numbers.clear();
+        initUsedNumbers();
     }
 
     private boolean checkDuplicate(int number) {
@@ -31,5 +69,9 @@ public class Referee {
         } else {
             return false;
         }
+    }
+
+    private void initUsedNumbers() {
+        Arrays.fill(usedNumber, false);
     }
 }
