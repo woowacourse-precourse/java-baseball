ca8c16756c5661fb7ae8d11c971651f289bdbc63
saewoo1 <wooozooo1123@gmail.com>
2022-11-05 17:54:58 +0900

    feat(Exception): 중복된 숫자가 있을 경우 예외처리

--- src/test/java/baseball/Exception.java
+++ src/test/java/baseball/Exception.java
@@ -1,9 +1,23 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Exception {
   // 길이가 3이 아니면 - 추후 전역상수로 바꾸는게..
   public static boolean isRightLength(String userNumber) {
     if (userNumber.length() != 3)
+      throw new IllegalArgumentException();
+    return true;
+  }
+
+  //중복된 숫자가 있으면
+  public static boolean isDuplicated(String userNumber) {
+    Set<Character> set = new HashSet<>();
+
+    for (int i = 0; i < userNumber.length(); i++)
+      set.add(userNumber.charAt(i));
+    if (userNumber.length() != set.size())
       throw new IllegalArgumentException();
     return true;
   }
