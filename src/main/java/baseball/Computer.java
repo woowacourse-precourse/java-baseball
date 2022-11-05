13aa76859341904b3de47daeedf6896df23fd2b4
BuildTools <unconfigured@null.spigotmc.org>
2022-11-05 20:32:05 +0900

    feat: 추가 Compuer클래스 및 makeRandomNumber, addRandomNumber 기능
    랜덤숫자를 만드는 기능 추가

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,34 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private static final int BASEBALL_GAME_DIGIT = 3;
+    private List<Character> baseballNumber = new ArrayList<>();
+
+    public Computer() {
+        makeRandomNumber();
+    }
+
+    public List<Character> getBaseballNumber() {
+        return baseballNumber;
+    }
+
+    public void makeRandomNumber(){
+        while (baseballNumber.size() < BASEBALL_GAME_DIGIT) {
+            addRandomNumber();
+        }
+    }
+
+    private void addRandomNumber(){
+        char randomNumber = Character.forDigit((Randoms.pickNumberInRange(1, 9)), 10);
+        if (!baseballNumber.contains(randomNumber)) {
+            baseballNumber.add(randomNumber);
+        }
+    }
+
+
+}
