a861a3bc8154e2f47e1e5f1e6504603655851bca
Cyma-s <ssol0319@naver.com>
2022-11-05 23:32:11 +0900

    feat(BaseballNumbers): add validation function about duplicate numbers
    validateDuplicateNumbers() checks ballNumbers whether it has duplicate numbers.

--- src/main/java/baseball/model/number/BaseballNumbers.java
+++ src/main/java/baseball/model/number/BaseballNumbers.java
@@ -3,18 +3,20 @@ package baseball.model.number;
 import java.util.List;
 
 public class BaseballNumbers {
-    private static final String INPUT_SIZE_ERROR = "입력하는 수는 3자리여야 합니다";
+    private static final String INPUT_SIZE_EXCEPTION = "입력하는 수는 3자리여야 합니다";
+    private static final String DUPLICATE_NUMBER_EXCEPTION = "중복되지 않는 수를 입력해야 합니다";
 
     List<BaseballNumber> ballNumbers;
 
     BaseballNumbers(String numberString) {
         validateSize(numberString);
         createBaseballNumber(numberString);
+        validateDuplicateNumbers();
     }
 
     private void validateSize(String uncheckNumbers) {
         if (uncheckNumbers.length() != 3) {
-            throw new IllegalArgumentException(INPUT_SIZE_ERROR);
+            throw new IllegalArgumentException(INPUT_SIZE_EXCEPTION);
         }
     }
 
@@ -23,6 +25,19 @@ public class BaseballNumbers {
             char uncheckNumber = uncheckNumbers.charAt(index);
             BaseballNumber baseballNumber = new BaseballNumber(uncheckNumber);
             ballNumbers.add(baseballNumber);
+        }
+    }
+
+    private void validateDuplicateNumbers() {
+        boolean isDuplicatedValue = false;
+        for(int index = 0; index < ballNumbers.size(); index++) {
+            BaseballNumber baseballNumber = ballNumbers.get(index);
+            BaseballNumber nextBaseballNumber = ballNumbers.get(index+1);
+            isDuplicatedValue = baseballNumber.equals(nextBaseballNumber);
+        }
+
+        if(isDuplicatedValue) {
+            throw new IllegalArgumentException(DUPLICATE_NUMBER_EXCEPTION);
         }
     }
 }
