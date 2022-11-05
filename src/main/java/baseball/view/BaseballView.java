e28acc3cec76b834d00f0a01b7d1bbf369abb5ad
hossang <zkzk104@naver.com>
2022-11-05 21:09:48 +0900

    feat: 게임을 다시 시작하거나 완전히 종료할지 출력하기

--- src/main/java/baseball/view/BaseballView.java
+++ src/main/java/baseball/view/BaseballView.java
@@ -27,4 +27,9 @@ public class BaseballView {
         }
         System.out.println(sb);
     }
+
+    public void resumeOrQuitGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
