49d9024875dc9284ab0a82e7b70b865de91de0b6
김태용 <amico741@naver.com>
2022-11-05 14:48:41 +0900

    feat: 서로 다른 3자리 수 결정하는 메서드 생성
    컴퓨터 클래스에서 가지는 서로 다른 3자리 랜덤 수 결정하기

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,9 +1,27 @@
 package baseball;
 
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 
 public class Computer {
-  private List<Integer> numbers;
+  private final ArrayList<Integer> numbers;
+  private static final int MAX_SIZE = 3;
+  public Computer() {
+    this.numbers = makeRandomNumbers();
+  }
+  public ArrayList<Integer> makeRandomNumbers(){
+    ArrayList<Integer> numbers = new ArrayList<>();
 
-  public Computer() {};
+    while(numbers.size() < MAX_SIZE){
+      int randomNumber = Randoms.pickNumberInRange(1, 9);
+      if(!numbers.contains(randomNumber)){
+        numbers.add(randomNumber);
+      }
+    }
+    return numbers;
+  }
+
+  public ArrayList<Integer> getNumbers(){
+    return this.numbers;
+  }
 }
