7651d99beb5d9ea9393f29fc66d695bdde15fd43
Kayoung <rkdkdudjd@gmail.com>
2022-11-05 19:30:24 +0900

    feat: Game.start() 메서드 정의

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,19 +1,66 @@
 package baseball.game;
 
+import baseball.input.GameInput;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Game {
     public List<Integer> digits;
+    private GameInput playerInput;
+
+    private final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    private final int BALL_IDX = 0;
+    private final int STRIKE_IDX = 1;
 
     public Game() {
         digits = new ArrayList<>();
         while (digits.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!digits.contains(randomNumber)) {
-                digits.add(randomNumber);
-            }
+            addUniqueRandomDigit();
         }
+    }
+
+    public void addUniqueRandomDigit() {
+        int randomNumber = Randoms.pickNumberInRange(1, 9);
+        if (!digits.contains(randomNumber)) {
+            digits.add(randomNumber);
+        }
+    }
+
+    public void start() {
+        System.out.println(START_MESSAGE);
+        List<Integer> result;
+        do {
+            playerInput = readInput();
+            result = calculateResult();
+            printResult(result);
+        } while (isCorrect(result));
+        System.out.println(END_MESSAGE);
+    }
+
+    private GameInput readInput() {
+        // TODO
+        return null;
+    }
+
+    private List<Integer> calculateResult() {
+        // TODO
+        return Collections.emptyList();
+    }
+
+    private void printResult(List<Integer> result) {
+        // TODO
+    }
+
+    private boolean isCorrect(List<Integer> result) {
+        // TODO
+        return false;
+    }
+
+    private void end() {
+
     }
 }
