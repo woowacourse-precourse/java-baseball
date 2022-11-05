a0396eae8145787230ed38cd259da2b3106f02a3
Yeongseon Park <yseon406@gmail.com>
2022-11-06 05:18:41 +0900

    refactor: 사용자 입력시 문구 추가
    getUserNumber()에 사용자 입력시 출력할 문구 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -22,6 +22,7 @@ public class BaseballGame {
     }
 
     public List<Integer> getUserNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
         String userInput = Console.readLine();
         List<Integer> userNumber = new ArrayList<>();
         for (char c : userInput.toCharArray()) {
