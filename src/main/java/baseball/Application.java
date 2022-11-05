c24008a07afb2eec3b1995ba7e0159d6d47490a0
Hongjun <ggaye159@naver.com>
2022-11-05 16:15:47 +0900

    컴퓨터숫자 받기와 게임 재시작 여부 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
         // TODO: 프로그램 구현
+        int play=1;
+        List <Integer> computernum=new ArrayList<Integer>();
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        while(play==1) {
+            makecomputernum(computernum);
+            baseballgame(computernum);
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            play=br.read();
+        }
+    }
+    public static void makecomputernum(List<Integer> computer){
+        int randomnum;
+        while(computer.size()<3){
+            randomnum= Randoms.pickNumberInRange(1,9);
+            if(!computer.contains(randomnum))
+                computer.add(randomnum);
+        }
     }
 }
