1cbf2a20be539d172a7e01b458ca5b29dcce5f37
situodys <situodys@gmail.com>
2022-11-05 17:01:19 +0900

    fix(Computer): 랜덤한 정답 숫자 생성 로직 변경

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -9,7 +9,6 @@ public class Computer {
     private static final int BASEBALL_COUNT = 3;
     private static final int MIN_RANGE_VALUE = 1;
     private static final int MAX_RANGE_VALUE = 9;
-    private static final int TO_INDEX = 1;
 
     private Baseballs answer;
 
@@ -25,25 +24,15 @@ public class Computer {
     }
 
     private String generateRandomAnswerInput() {
-        StringBuilder generatedRandomAnswerInput = new StringBuilder();
-        List<Integer> candidateValues = generateProperRangedCandidateValues();
-        int loopCnt = 0;
+        String generatedRandomAnswerInput = "";
 
-        while (loopCnt < BASEBALL_COUNT) {
-            int selectedValueIndex = Randoms.pickNumberInRange(MIN_RANGE_VALUE - TO_INDEX,
-                    candidateValues.size() - TO_INDEX);
-            generatedRandomAnswerInput.append(candidateValues.get(selectedValueIndex));
-            candidateValues.remove(selectedValueIndex);
-
-            loopCnt++;
+        while (generatedRandomAnswerInput.length() < BASEBALL_COUNT) {
+            int selectedValue = Randoms.pickNumberInRange(MIN_RANGE_VALUE,MAX_RANGE_VALUE);
+            if (!generatedRandomAnswerInput.contains(String.valueOf(selectedValue))) {
+                generatedRandomAnswerInput += selectedValue;
+            }
         }
 
-        return generatedRandomAnswerInput.toString();
-    }
-
-    private List<Integer> generateProperRangedCandidateValues() {
-        return IntStream.rangeClosed(MIN_RANGE_VALUE, MAX_RANGE_VALUE)
-                .boxed()
-                .collect(Collectors.toList());
+        return generatedRandomAnswerInput;
     }
 }
