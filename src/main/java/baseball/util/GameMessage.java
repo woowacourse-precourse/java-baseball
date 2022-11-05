2a9d8be41154a3936b7defb27581114fb4d21eaa
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-05 15:19:24 +0900

    feat<GameMessage>: printf로 출력하기 위해 메세지에 %d 입력
    - 결과를 표시하는 메세지를 printf로 출력하기 위해 변경

--- src/main/java/baseball/util/GameMessage.java
+++ src/main/java/baseball/util/GameMessage.java
@@ -6,6 +6,7 @@ public class GameMessage {
     public static final String ALL_STRIKE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String CONTINUE_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String NOTHING = "낫싱";
-    public static final String BALL= "볼";
-    public static final String STRIKE= "스트라이크";
+    public static final String BALL = "%d볼\n";
+    public static final String STRIKE = "%d스트라이크\n";
+    public static final String STRIKE_BALL = "%d볼 %d스트라이크\n";
 }
