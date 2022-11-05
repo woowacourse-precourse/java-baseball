b8224d2e851c97c2e15ec77c999f740543ad6adc
Kid-Chang <pch3310@gmail.com>
2022-11-05 19:51:54 +0900

    feat(tools/GenerateNumber): 랜덤 숫자 3개를 배열로 출력하는 함수

---
+++ src/main/java/baseball/tools/GenerateNumber.java
@@ -0,0 +1,10 @@
+package baseball.tools;
+
+import java.util.List;
+
+public class GenerateNumber {
+
+    public List<Integer> pickThreeUniqueRandomNumber(){
+        return  camp.nextstep.edu.missionutils.Randoms.pickUniqueNumbersInRange(1, 9, 3);
+    }
+}
