618e4b7c2b7707cf0504fdef96341d83fde1b44b
win9 <krrr8@naver.com>
2022-11-06 00:51:38 +0900

    Fix(Util): 3자리가 넘는 숫자도 에러를 잡도록 수정
    에러 문구를 추가하고 조건문을 바꾸어 3자리가 아닌 숫자는 모두 오류를 표시하도록 수정

--- src/main/java/baseball/util/Util.java
+++ src/main/java/baseball/util/Util.java
@@ -9,19 +9,19 @@ public class Util {
     }
 
     public static void isCorrectInput(String numbers){
-        if (numbers.length() < 3){
-            throw new IllegalArgumentException();
+        if (numbers.length() != 3){
+            throw new IllegalArgumentException("3자리의 숫자를 입력하세요.");
         }
 
         if (!numbers.matches("-?\\d+(\\.\\d+)?")) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("숫자만 입력하세요.");
         }
 
     }
 
     public static void isCorrectInputRestart(int number){
         if (number != 1 || number != 2) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1또는 2의 숫자를 입력하세요.");
         }
     }
 }
