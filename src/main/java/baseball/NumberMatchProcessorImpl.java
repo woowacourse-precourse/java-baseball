ae06b7f7f67d2611a258e18eac1ca12bd5ef731d
5witchkr <hob4419@gmail.com>
2022-11-06 08:03:14 +0900

    feat(NumberMatchProcessorImpl): 두 숫자를 비교하는 기능 * 스트라이크 갯수와 볼 갯수를 숫자배열로 리턴해준다. * 3자리 숫자가 아니면 예외를 던져준다.

--- src/main/java/baseball/NumberMatchProcessorImpl.java
+++ src/main/java/baseball/NumberMatchProcessorImpl.java
@@ -1,13 +1,39 @@
 package baseball;
 
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class NumberMatchProcessorImpl implements NumberMatchProcessor{
 
     @Override
     public List<Integer> compare(int gameNum, int userNum) {
+        if (userNum > 999 || userNum < 99) throw new IllegalArgumentException("3자리 숫자만 입력 가능합니다.");
 
-        return null;
+
+        List<Integer> result = new ArrayList<>(List.of(0, 0));
+
+
+        List<String> gameNumList = Stream.of(String.valueOf(gameNum).split(""))
+                .collect(Collectors.toList());
+        List<String> userNumList = Stream.of(String.valueOf(userNum).split(""))
+                .collect(Collectors.toList());
+
+
+        int strike = 0;
+        int ball = 0;
+        for (int i=0; i<3; i++) {
+            //스트라이크
+            if (gameNumList.get(i).equals(userNumList.get(i)))strike++;
+            //볼
+            else if (gameNumList.contains(userNumList.get(i))) ball++;
+        }
+
+        result.set(0, strike);
+        result.set(1, ball);
+
+        return result;
     }
 }
