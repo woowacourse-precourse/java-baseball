c56406ff8eb669a59bac8dc39cfc69ebdea50a6e
khj <bbb6254177@gmail.com>
2022-11-05 18:42:15 +0900

    feat: add random number generator

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static List<Integer> rand_num_gen() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        //1. 상대방이 랜덤한 세 자리수를 생성한다.
+        List<Integer> rand_num = rand_num_gen();
     }
 }
