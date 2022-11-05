e7aa63c4013fd1b9d81fee1ea486f59f4c17bc22
KIMSEI1124 <workju1124@gmail.com>
2022-11-05 12:48:23 +0900

    fix: 볼과 스트라이크 결과를 못구하는 에러 수정

--- src/main/java/baseball/Comparator.java
+++ src/main/java/baseball/Comparator.java
@@ -3,13 +3,10 @@ package baseball;
 import java.util.List;
 
 public class Comparator {
-    private final List<Integer> computerNumbers;
-    private final List<Integer> playerNumbers;
-    private final int[] result = {0, 0};
+    private int[] result = {0, 0};
 
     public Comparator(List<Integer> computerNumbers, List<Integer> playerNumbers) {
-        this.computerNumbers = computerNumbers;
-        this.playerNumbers = playerNumbers;
+        this.result = inputResult(computerNumbers, playerNumbers);
     }
 
     /**
@@ -18,24 +15,56 @@ public class Comparator {
      * @return 숫자 야구의 결과를 반환
      */
     public int[] getResult() {
+        return result;
+    }
+
+    /**
+     * 컴퓨터의 숫자들에 플레이어의 숫자가 있는가를 확인
+     *
+     * @param playerNumber 플레이어의 숫자
+     * @return 플레이어의 숫자가 컴퓨터의 숫자들에 있으면 true
+     */
+    private boolean hasPlayerNumberByComputerNumbers(List<Integer> computerNumbers, int playerNumber) {
+        return computerNumbers.contains(playerNumber);
+    }
+
+    /**
+     * 숫자 야구의 결과를 입력한다.
+     */
+    public int[] inputResult(List<Integer> computerNumbers, List<Integer> playerNumbers) {
         for (int i = 0; i < 3; i++) {
             int computerNumber = computerNumbers.get(i);
             int playerNumber = playerNumbers.get(i);
-            inputResult(computerNumber, playerNumber);
+            if (hasPlayerNumberByComputerNumbers(computerNumbers, playerNumber)) {
+                isStrike(computerNumber, playerNumber);
+                isBall(computerNumber, playerNumber);
+            }
         }
         return result;
     }
 
     /**
-     * 컴퓨터와 플레이어의 숫자를 비교하여 결과를 넣는다.
+     * 컴퓨터와 플레이어의 숫자를 비교하여 볼인지 확인하고
+     * 볼이면 1점을 더한다.
      *
      * @param computerNumber 컴퓨터의 숫자
      * @param playerNumber   플레이어의 숫자
      */
-    private void inputResult(int computerNumber, int playerNumber) {
-        if (computerNumber == playerNumber) {
+    private void isBall(int computerNumber, int playerNumber) {
+        if (computerNumber != playerNumber) {
             result[0] += 1;
-        } else {
+        }
+    }
+
+    /**
+     * 컴퓨터와 플레이어의 숫자를 비교하여 스트라이크인지 확인하고
+     * 스트라이크면 1점을 더한다.
+     *
+     * @param computerNumber 컴퓨터의 숫자
+     * @param playerNumber   플레이어의 숫자
+     */
+    private void isStrike(int computerNumber, int playerNumber) {
+        if (computerNumber == playerNumber) {
             result[1] += 1;
         }
     }
