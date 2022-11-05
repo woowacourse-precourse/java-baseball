b39f57ac46dfd17219b9430ead60798ab8359791
Haaae <101785628+Haaae@users.noreply.github.com>
2022-11-06 04:46:07 +0900

    feat: 예외발생 가능성이 높은 메서드의 테스트코드 작성
    - countSameLetterInSameIndexWithoutDuplicate
    - countSameLetterInDifferentIndexWithoutDuplicate
    - isValidBaseballNumber
    - isValidAnswer

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -8,6 +8,13 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.time.Duration;
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
+import static org.junit.jupiter.api.Assertions.assertTimeout;
+
+
 class ApplicationTest extends NsTest {
     @Test
     void 게임종료_후_재시작() {
@@ -26,6 +33,81 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 예외_테스트_를_테스트() {
+            assertThatThrownBy(() -> runException("1234"))
+                    .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 스트라이크_숫자_반환() {
+        String computer = "123";
+        String user = "123";
+        int result = Application.countSameLetterInSameIndexWithoutDuplicate(computer, user);
+
+        assertThat(result).isEqualTo(3);
+    }
+
+    @Test
+    void 볼_숫자_반환() {
+        String computer = "12345";
+        String user = "54321";
+        int result = Application.countSameLetterInDifferentIndexWithoutDuplicate(computer, user);
+
+        assertThat(result).isEqualTo(4);
+    }
+
+
+    @Test
+    void Timeout_Test() {
+        assertTimeoutPreemptively(Duration.ofSeconds(1L),
+                () -> assertThatThrownBy(() -> Application.isValidBaseballNumber("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자맞추기_입력_길이_예외_테스트() {
+            assertThatThrownBy(() -> Application.isValidBaseballNumber("1234"))
+                    .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자맞추기_입력_문자_예외_테스트() {
+        assertThatThrownBy(() -> Application.isValidBaseballNumber("12T"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자맞추기_입력_중복_예외_테스트() {
+        assertThatThrownBy(() -> Application.isValidBaseballNumber("122"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 게임_재시작_길이_예외_테스트() {
+        String answer = "123";
+        boolean result = Application.isValidAnswer(answer);
+
+        assertThat(result).isEqualTo(false);
+    }
+
+    @Test
+    void 게임_재시작_문자_예외_테스트() {
+        String answer = "a";
+        boolean result = Application.isValidAnswer(answer);
+
+        assertThat(result).isEqualTo(false);
+    }
+
+    @Test
+    void 게임_재시작_길이_예외없음_테스트() {
+        String answer = "1";
+        boolean result = Application.isValidAnswer(answer);
+
+        assertThat(result).isEqualTo(true);
     }
 
     @Override
