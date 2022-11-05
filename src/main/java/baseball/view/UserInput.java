85c66baffebb7e960b4ec140e03a0c49afe82293
김현재 <41482946+galug@users.noreply.github.com>
2022-11-06 02:45:50 +0900

    feat(userinput): add 재시작 여부를 묻는 입력에 대한 검증 기능

--- src/main/java/baseball/view/UserInput.java
+++ src/main/java/baseball/view/UserInput.java
@@ -2,13 +2,16 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class UserInput {
-
     public String input() {
         return Console.readLine();
+    }
+
+    public void validateContinueInput(String inputContinueGame) {
+        String pattern = "[1-2]";
+        if (!inputContinueGame.matches(pattern)) {
+            throw new IllegalArgumentException();
+        }
     }
 }
