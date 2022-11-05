64a7bcf88a9087c9cc17e80ad3a1683a050c7afe
nn98 <jkllhgb@gmail.com>
2022-11-05 09:17:50 +0900

    Feat: 입력값 정오 판별 기능 구현, Checker 클래스 참조 형식으로 변경, getNumber deepCopy 방식으로 구현

--- src/main/java/baseball/Checker.java
+++ src/main/java/baseball/Checker.java
@@ -1,19 +1,57 @@
 package baseball;
 
+import constant.Texts;
+
 import java.util.List;
 
 public class Checker {
-    static int checkResult(List<Integer> target, List<Integer> input) {
+    public void checkLegalNumber(String number) throws IllegalArgumentException {
+        if (!this.isIntegerNumeric(number)) {
+            throw new IllegalArgumentException(Texts.USER_INPUT_ILLEGAL_MISMATCHING+Texts.USER_INPUT_ILLEGAL);
+        }
+        if (!this.isNumberDigitLegal(number)) {
+            throw new IllegalArgumentException(Texts.USER_INPUT_ILLEGAL_DIGIT+Texts.USER_INPUT_ILLEGAL);
+        }
+        if (this.isNumberDuplicated(number)) {
+            throw new IllegalArgumentException(Texts.USER_INPUT_ILLEGAL_DUPLICATED+Texts.USER_INPUT_ILLEGAL);
+        }
+    }
+    public boolean isIntegerNumeric(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    public boolean isNumberDigitLegal(String number) {
+        if (number.length() > 3) {
+            return false;
+        }
+        return true;
+    }
+    public boolean isNumberDuplicated(String number) {
+        String[] digits = number.split("");
+        for(int i=0;i<digits.length-1;i++) {
+            for(int j=i+1;j<digits.length;j++){
+                if(digits[i].equals(digits[j])){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public int checkResult(List<Integer> target, List<Integer> input) {
         int result = 0;
         return result;
     }
 
-    static int isStrike(int target, int input) {
+    public int isStrike(int target, int input) {
         int result = 0;
         return result;
     }
 
-    static int isBall(int target, List<Integer> input) {
+    public int isBall(int target, List<Integer> input) {
         int result = 0;
         return result;
     }
--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -7,21 +7,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    List<Integer> number;
+    private static List<Integer> number;
     public User() {
         number=new ArrayList<>();
     }
     public void makeUserNumber() {
         System.out.print(Texts.USER_INPUT_NUMBER);
-        String input = Console.readLine();
-        if (input.length() > 3) {
-            throw new IllegalArgumentException(Texts.USER_INPUT_ILLEGAL_LENGTH+Texts.USER_INPUT_ILLEGAL);
+        String number = Console.readLine();
+
+        Checker checker = new Checker();
+        checker.checkLegalNumber(number);
+
+        for (String s : number.split("")) {
+            this.number.add(Integer.parseInt(s));
         }
-        try {
-            Integer.parseInt(input);
-        } catch (Exception e) {
-            throw new IllegalArgumentException(Texts.USER_INPUT_ILLEGAL);
-        }
-        for (String s : input.split("")) this.number.add(Integer.parseInt(s));
+
     }
+    public List<Integer> getNumber() {
+        List<Integer> copyNumber = new ArrayList<>();
+        for(int i : number) copyNumber.add(i);
+        return copyNumber;
+    }
+
 }
