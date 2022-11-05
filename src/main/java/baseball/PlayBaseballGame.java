7852246579192787f0fc793d848d99f7cf8e6505
seongbeom <84623098+hi12123123@users.noreply.github.com>
2022-11-05 23:05:10 +0900

    feat : 입력 받은 숫자에 대하여 정해진 요구사항에 맞는지 검사하는 기능 추가

--- src/main/java/baseball/PlayBaseballGame.java
+++ src/main/java/baseball/PlayBaseballGame.java
@@ -1,13 +1,18 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class PlayBaseballGame {
     private Integer strike;
     private Integer ball;
     private List<Integer> RandomNum;
     private List<Integer> UserNum;
+
+    String userInput = Console.readLine();
+    UserNum = getUserNum(userInput);
 
     public void RandomNum(){
         while(RandomNum.size() < 3){
@@ -18,8 +23,28 @@ public class PlayBaseballGame {
         }
     }
 
-    public void CorrectNum(){
+    public void checkUserInput(String userInput){
+        if (userInput.length() != 3){
+            new throw IllegalArgumentException();
+        }
 
+        for (int i = 0; i < userInput.length(); i++){
+            if(userInput.charAt(i) == 0){
+                new throw IllegalArgumentException();
+            }
+        }
+
+        // 각 자리 숫자 비교해서 달라야함
+
+    }
+
+    public static List<Integer> getUserNum(String userInput){
+        List<Integer> userNum = new ArrayList<>();
+        for (int i = 0; i < userInput.length(); i++){
+            userNum.add(Character.getNumericValue(userInput.charAt(i)));
+        }
+
+        return userNum;
     }
 
 }
