449ca111394f21beba75b911102daefd98412ec0
wjh2012 <wjh2012@naver.com>
2022-11-05 17:04:02 +0900

    fix(test): 단순 타이핑 수정

--- src/test/java/missionuutils/ConsoleAPITest.java
+++ src/test/java/missionuutils/ConsoleAPITest.java
@@ -9,15 +9,16 @@ import org.junit.jupiter.api.Test;
 public class ConsoleAPITest {
 
     @Test
-    void readLine_메서드_사용시_문자열을_입력받음(){
+    void readLine_메서드_사용시_문자열을_입력받음() {
         String input = "hello world!";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
         Assertions.assertThat(Console.readLine()).isEqualTo("hello world!");
     }
+
     @Test
-    void readLine_메서드_사용시_문자열을_입력받음_실패(){
+    void readLine_메서드_사용시_문자열을_입력받음_실패() {
         String input = "hi world!";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
