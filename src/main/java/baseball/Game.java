53f27614576d50de04cfbd28ee47a17618522d3b
myeongju00 <j51677767@gmail.com>
2022-11-06 02:06:56 +0900

    feat : 힌트 출력 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,14 +3,24 @@ package baseball;
 import java.util.List;
 
 public class Game {
+    private static final int DIGITS = 3;
     private static final String BALL_STR = "볼 ";
     private static final String STRIKE_STR = "스트라이크 ";
     private static final String NOTHING = "낫싱";
+    private static final String THREE_STRIKE_SENTENCE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
-
-    private String makeHint (List<Character> answer, List<Character> input) {
+    private void printHint(List<Character> answer, List<Character> input) {
         int strike = countStrike(answer, input);
         int ball = countBall(answer, input);
+
+        System.out.println(makeHint(strike, ball));
+        if(strike == DIGITS) {
+            System.out.println(THREE_STRIKE_SENTENCE);
+        }
+    }
+
+    private String makeHint (int strike, int ball) {
+//        StringBuilder sb = new StringBuilder();
         if(strike != 0) {
             if(ball != 0) {
                 return ball + BALL_STR + strike + STRIKE_STR;
