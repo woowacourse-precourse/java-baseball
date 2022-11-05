70e2bc2cfcc60b99d9f2216868ad0c504b78af87
Invidam <hansupark0208@gmail.com>
2022-11-05 23:01:42 +0900

    refactor(Opponent): Computer Opponent 리팩터링
    - shuffle 로직 메서드 추출.
    - public 메서드 위치 위로 변경

--- src/main/java/baseball/opponent/ComputerOpponent.java
+++ src/main/java/baseball/opponent/ComputerOpponent.java
@@ -11,14 +11,13 @@ public class ComputerOpponent implements Opponent {
 
     private ThreeDigitNum answer;
 
-    public ComputerOpponent() {
-        answer = createAnswer();
-    }
-
-
     @Override
     public int getType() {
         return Opponent.COMPUTER_OPPONENT;
+    }
+
+    public ComputerOpponent() {
+        answer = createAnswer();
     }
 
     @Override
@@ -26,41 +25,53 @@ public class ComputerOpponent implements Opponent {
         return createRandomThreeDigitNum();
     }
 
+    @Override
+    public Hint makeHint(ThreeDigitNum inputValue) {
+        return new Hint(this.answer, inputValue);
+    }
+
     private ThreeDigitNum createRandomThreeDigitNum() {
         List<Integer> threeDigitList = new ArrayList<>();
         while (threeDigitList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
+
             if (!threeDigitList.contains(randomNumber)) {
                 threeDigitList.add(randomNumber);
             }
         }
         return new ThreeDigitNum(threeDigitList);
+
         /*
-        셔플 방식으로 짠 코드입니다.
-        랜덤 생성이 작위적인 이유로, 오류가 나 주석처리하였습니다.
-        List<Integer> integerList = new ArrayList<>();
-        for (int num = ThreeDigitNum.START_RANGE; num <= ThreeDigitNum.END_RANGE; ++num) {
-            integerList.add(num);
-        }
+        //셔플 방식으로 짠 코드입니다.
+        //랜덤 생성이 작위적인 이유로, 오류가 나 주석처리하였습니다.
+        List<Integer> integerList = getListInRangeNums(ThreeDigitNum.START_RANGE,
+                ThreeDigitNum.END_RANGE);
 
-        for (int currIdx = 0; currIdx < integerList.size()-1; currIdx++) {
-
-            int changeIdx = Randoms.pickNumberInRange(currIdx,  integerList.size()-1);
-            int temp = integerList.get(changeIdx);
-            integerList.set(changeIdx, integerList.get(currIdx));
-            integerList.set(currIdx, temp);
-
-        }
-
-        List<Integer> suffled = integerList.subList(0, ThreeDigitNum.SIZE);
-
-        return new ThreeDigitNum(suffled);
+        return new ThreeDigitNum(shuffle(integerList).subList(0, ThreeDigitNum.SIZE));
         */
     }
 
-    @Override
-    public Hint makeHint(ThreeDigitNum inputValue) {
-        return new Hint(this.answer, inputValue);
+    private List<Integer> shuffle(List<Integer> integerList) {
+        for (int currIdx = 0; currIdx < integerList.size() - 1; currIdx++) {
+            swap(integerList, Randoms.pickNumberInRange(currIdx, integerList.size() - 1), currIdx);
+        }
+
+        return integerList;
     }
+
+    private static void swap(List<Integer> integerList, int changeIdx, int currIdx) {
+        int temp = integerList.get(changeIdx);
+        integerList.set(changeIdx, integerList.get(currIdx));
+        integerList.set(currIdx, temp);
+    }
+
+    private static List<Integer> getListInRangeNums(int startNum, int endNum) {
+        List<Integer> integerList = new ArrayList<>();
+        for (int num = startNum; num <= endNum; ++num) {
+            integerList.add(num);
+        }
+        return integerList;
+    }
+
 
 }
