20508dffccd9656379b5e77f52d898e96f5ef497
K-Diger <kitten.diger@gmail.com>
2022-11-05 21:06:33 +0900

    refactor : BaseballGameStater 클래스 operate() 의존성 사용 방법 변경 - User 클래스에서 의존성 주입 방식을 변경한 내용을 반영한다.

--- src/main/java/baseball/starter/BaseballGameStarter.java
+++ src/main/java/baseball/starter/BaseballGameStarter.java
@@ -15,9 +15,8 @@ public class BaseballGameStarter {
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
 
-        List<String> userInputValue = new User(UserInputValidator.getInstance())
-                .stringConvertToStringList(
-                        Console.readLine());
+        List<String> userInputValue = new User().stringConvertToStringList(
+                Console.readLine());
         List<String> computerGeneratedValue = new Computer().generateNumber();
         return new ArrayList<>() {{
             add(userInputValue);
@@ -27,14 +26,12 @@ public class BaseballGameStarter {
 
     public boolean operateRefereeJudgement(List<String> userInputValue, List<String> computerGeneratedValue) {
         Referee referee = new Referee();
-        UserInputValidator userInputValidator = UserInputValidator.getInstance();
 
         while (referee.judgement(computerGeneratedValue, userInputValue).get("strikeCount") != 3) {
             if (referee.judgement(computerGeneratedValue, userInputValue).get("strikeCount") != 3) {
                 referee.sayJudgement(referee.judgement(computerGeneratedValue, userInputValue));
                 System.out.print("숫자를 입력해주세요 : ");
-                userInputValue = new User(userInputValidator)
-                        .stringConvertToStringList(
+                userInputValue = new User().stringConvertToStringList(
                                 Console.readLine());
             }
         }
