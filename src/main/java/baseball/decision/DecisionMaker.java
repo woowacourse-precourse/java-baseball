f5aaa9dd032eea01c61ca6adede4abe2eb24faf6
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-05 22:52:11 +0900

    feat(DecisionMaker): 유저로 부터 입력을 받아 게임 진행에 대한 의사 결정을 생성하여 반환한다

--- src/main/java/baseball/decision/DecisionMaker.java
+++ src/main/java/baseball/decision/DecisionMaker.java
@@ -1,7 +1,28 @@
 package baseball.decision;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class DecisionMaker {
+
+    final String InformationMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     public String getDecision() {
-        return "";
+        return acceptOpinion();
+    }
+
+    private String acceptOpinion() {
+        printInformation();
+        String opinion = Console.readLine();
+        if (opinion.equals("1")) {
+            return "Play";
+        }
+        if (opinion.equals("2")) {
+            return "Stop";
+        }
+        throw new IllegalArgumentException("유효하지 않은 입력입니다.");
+    }
+
+    private void printInformation() {
+        System.out.println(InformationMessage);
     }
 }
