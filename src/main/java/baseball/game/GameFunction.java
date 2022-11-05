32c6a1a62238cb4d57f1b12d942dd0170950c92b
tseop <77163842+tseop@users.noreply.github.com>
2022-11-05 14:49:50 +0900

    Feat: 게임 기능 클래스 생성 함수 생성 (#300)
    - 게임 기능 클래스 생성 및 기능 함수 생성

---
+++ src/main/java/baseball/game/GameFunction.java
@@ -0,0 +1,30 @@
+package baseball.game;
+
+import java.util.List;
+
+public class GameFunction {
+
+    public int userNumListAndComNumListCheckBallCount(List<Integer> userNumList, List<Integer> computerNumList) {
+        return 0;
+    }
+
+    public int userNumListAndComNumListCheckStrikeCount(List<Integer> userNumList, List<Integer> computerNumList) {
+        return 0;
+    }
+
+    public boolean again(String input) {
+        return false;
+    }
+
+    public void printScoreMsg(int strikeScore, int ballScore) {
+
+    }
+
+    public void printGameStartMsg() {
+
+    }
+
+    public void printInputRequestMsg() {
+
+    }
+}
