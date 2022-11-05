88e1b4ad249d53e22f7f471e639dfaf5623b3aa0
shpark30 <seohyeonpark30.gmail.com>
2022-11-05 21:54:37 +0900

    feat(Referee): progress baseball game
    to manage the process of baseball game
    make Referee class.
    
    1. start game
    2. start pitch
    3. judge a pitch and a swing result
    4. end game

--- src/main/java/baseball/participants/Referee.java
+++ src/main/java/baseball/participants/Referee.java
@@ -1,2 +1,26 @@
-package baseball.participants;public class Referee {
+package baseball.participants;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Referee {
+    public static boolean isGameInProgress;
+
+    public void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        this.isGameInProgress = true;
+    }
+
+    public String startPitch() {
+        System.out.println("3자리 숫자를 입력해주세요 : ");
+        String pitch = readLine();
+        return pitch;
+    }
+
+    public void judge(Pitcher  pitcher, Hitter hitter) {
+    }
+
+    public void endGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        this.isGameInProgress = false;
+    }
 }
