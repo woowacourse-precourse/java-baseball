c0ab4b138c023bb814a27129fb0b0f50dd58a4cb
soyunim <togksk123@naver.com>
2022-11-06 03:10:44 +0900

    feat: re-input or correct input number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -126,4 +126,14 @@ public class Application {
         }
         return result;
     }
+
+    public static void keepOrCorrect(List<Integer> computerNum, List<Integer> userNum){
+        if(!computerNum.equals(userNum)){
+            userNum.clear();
+        }
+        else{
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+    }
+
 }
