20e92b5798b8ed37e4317581c77fec8b5f00c93e
minaver <psmjoshua@gmail.com>
2022-11-05 11:47:27 +0900

    feat(BallCount): BallCount 객체 생성

---
+++ src/main/java/baseball/BallCount.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class BallCount {
+    private int ballCount;
+    private int strikeCount;
+
+    public BallCount(int ballCount, int strikeCount) {
+        this.ballCount = ballCount;
+        this.strikeCount = strikeCount;
+    }
+}
