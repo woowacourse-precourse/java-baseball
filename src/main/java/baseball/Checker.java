d6f3782784d7c9c24ede51a8ff6d66786e4f4869
nn98 <jkllhgb@gmail.com>
2022-11-05 10:22:09 +0900

    Style & Feat: InteliJ Code Formatting & 사용자 숫자 채점 기능 구현

--- src/main/java/baseball/Checker.java
+++ src/main/java/baseball/Checker.java
@@ -8,15 +8,16 @@ import java.util.List;
 public class Checker {
     public void checkLegalNumber(String number) throws IllegalArgumentException {
         if (!this.isIntegerNumeric(number)) {
-            throw new IllegalArgumentException(Texts.USER_INPUT_ILLEGAL_MISMATCHING+Texts.USER_INPUT_ILLEGAL);
+            throw new IllegalArgumentException(Texts.USER_INPUT_ILLEGAL_MISMATCHING + Texts.USER_INPUT_ILLEGAL);
         }
         if (!this.isNumberDigitLegal(number)) {
-            throw new IllegalArgumentException(Texts.USER_INPUT_ILLEGAL_DIGIT+Texts.USER_INPUT_ILLEGAL);
+            throw new IllegalArgumentException(Texts.USER_INPUT_ILLEGAL_DIGIT + Texts.USER_INPUT_ILLEGAL);
         }
         if (this.isNumberDuplicated(number)) {
-            throw new IllegalArgumentException(Texts.USER_INPUT_ILLEGAL_DUPLICATED+Texts.USER_INPUT_ILLEGAL);
+            throw new IllegalArgumentException(Texts.USER_INPUT_ILLEGAL_DUPLICATED + Texts.USER_INPUT_ILLEGAL);
         }
     }
+
     public boolean isIntegerNumeric(String number) {
         try {
             Integer.parseInt(number);
@@ -25,38 +26,74 @@ public class Checker {
             return false;
         }
     }
+
     public boolean isNumberDigitLegal(String number) {
         if (number.length() == 3) {
             return true;
         }
         return false;
     }
+
     public boolean isNumberDuplicated(String number) {
         String[] digits = number.split("");
-        for(int i=0;i<digits.length-1;i++) {
-            for(int j=i+1;j<digits.length;j++){
-                if(digits[i].equals(digits[j])){
+        for (int i = 0; i < digits.length - 1; i++) {
+            for (int j = i + 1; j < digits.length; j++) {
+                if (digits[i].equals(digits[j])) {
                     return true;
                 }
             }
         }
         return false;
     }
-    public List<Integer> checkResult(List<Integer> userNumber, List<Integer> computerNumber) {
+
+    public List<Integer> checkByDigit(List<Integer> userNumbers, List<Integer> computerNumbers) {
         List<Integer> result = new ArrayList<>();
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            int userNumber = userNumbers.get(i);
+            int computerNumber = computerNumbers.get(i);
+            if (isStrike(userNumber, computerNumber)) {
+                strike += 1;
+            } else if (isBall(userNumber, computerNumbers)) {
+                ball += 1;
+            }
+        }
+        result.add(strike);
+        result.add(ball);
         return result;
     }
+
+    public boolean isStrike(int userNumber, int computerNumber) {
+        return userNumber == computerNumber;
+    }
+
+    public boolean isBall(int userNumber, List<Integer> computerNumbers) {
+        return computerNumbers.contains(userNumber);
+    }
+
+    public String checkResult(List<Integer> result) {
+        StringBuffer scoreBuffer = new StringBuffer();
+        if (isNothing(result)) {
+            scoreBuffer.append(Texts.CHECKER_RESULT_NOTTING);
+        } else {
+            int strike = result.get(0);
+            int ball = result.get(1);
+            if (ball > 0) {
+                scoreBuffer.append(ball);
+                scoreBuffer.append(Texts.CHECKER_RESULT_BALL);
+                scoreBuffer.append(" ");
+            }
+            if (strike > 0) {
+                scoreBuffer.append(strike);
+                scoreBuffer.append(Texts.CHECKER_RESULT_STRIKE);
+            }
+        }
+        return scoreBuffer.toString();
+    }
+
     public boolean isNothing(List<Integer> result) {
-        return false;
+        return result.get(0) == 0 && result.get(1) == 0;
     }
 
-    public int isStrike(int target, int input) {
-        int result = 0;
-        return result;
-    }
-
-    public int isBall(int target, List<Integer> input) {
-        int result = 0;
-        return result;
-    }
 }
