45be8087c78e02577bbd3341b5c70c82bbcfbdba
Donghyun <flaehan@gmail.com>
2022-11-05 21:29:41 +0900

    type : feat
    body : 컴퓨터가 3개의 수 랜덤으로 생성, 세 자리 연수, 1부터 9까지의 정수로 구성, 각 숫자는 중복 불가
    
    footer :

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,80 +1,41 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
-
-import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
 
-    private static final int MINIMUM_NUMBER = 1;
-    private static final int MAX_NUMBER = 9;
+    static final int MINIMUM_NUMBER = 1;
+    static final int MAX_NUMBER = 9;
+    static int comNumber1, comNumber2, comNumber3;
+    static int userNumber1, userNumber2, userNumber3;
+    static String userThreeNumber;
+    static int strikeCount = 0;
+    static int ballCount = 0;
+    static int gameButton = 0;
 
-    public static void main(String[] args)  {
-        System.out.println(computerNumber());
-        System.out.println(userNumber());
+
+    public static void main(String[] args) {
     }
 
-
-    // 컴퓨터가 3개의 랜덤 수 생성  -> computerNumber() 로 호출
-
-    public static ArrayList<Integer> computerNumber() {
-        ArrayList<Integer> computerList = new ArrayList<>();
-        while (computerList.size() != 3) {
-            int randomNum = Randoms.pickNumberInRange(MINIMUM_NUMBER, MAX_NUMBER);
-            if (!computerList.contains(randomNum)) {
-                computerList.add(randomNum);
+    public static ArrayList<Integer> computerThreeRandomNumber(){
+        ArrayList<Integer> comList = new ArrayList<>();
+        while(true) {
+            comNumber1 = Randoms.pickNumberInRange(MINIMUM_NUMBER, MAX_NUMBER);
+            comNumber2 = Randoms.pickNumberInRange(MINIMUM_NUMBER, MAX_NUMBER);
+            comNumber3 = Randoms.pickNumberInRange(MINIMUM_NUMBER, MAX_NUMBER);
+            if((comNumber1 != comNumber2) && (comNumber1 != comNumber3) && (comNumber2 != comNumber3)){
+                comList.add(comNumber1);
+                comList.add(comNumber2);
+                comList.add(comNumber3);
+                break;
             }
         }
-        return computerList;
+        return comList;
     }
-
-//     사용자가 3개의 수 입력
-
-    public static int[] userNumber() {
-//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        String userInputNumber = br.readLine();
-        Scanner sc = new Scanner(System.in);
-        String userInputNumber = sc.nextLine();
-        int[] userList = new int[computerNumber().size()];
-
-        if (userInputNumber.length() == 3) {
-            String[] usersList = userInputNumber.split("");
-            for (int i = 0; i < userInputNumber.length(); i++) {
-                userList[i] = Integer.parseInt(usersList[i]);
-            }
-        } else {
-            throw new IllegalArgumentException("put three numbers");
-        }
-
-        return userList;
-    }
-//
-//
-//
-//        return userNum;
-//    }
-//
-//    // 사용자 입력 정답 검사
-//    public static checkAnswer{
-//
-//    }
-//
-//    // 정답 검사 결과 출력
-//    public static printAnswer{
-//
-//    }
-//
-//    // 종료 조건 검사
-//    public static checkFinish{
-//
-//    }
-//}
-
 
 }
+
+//  인덴트 문제 존재
