591ec8344d4145309badf1ca93738677986453a9
JUHA <khabh@naver.com>
2022-11-05 13:17:59 +0900

    feat(Game): 사용자가 종료를 원할 때까지 새로운 게임 실행

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 public class Game {
     private final static int THREE_STRIKES = 3;
+    private final static int RESTART = 1;
     private final static Player player = new Player();
 
     private final Computer computer;
@@ -20,5 +21,22 @@ public class Game {
             isGameCleared = (strikeCount == THREE_STRIKES);
         }
         Output.printGameClear();
+    }
+
+    private boolean askPlayerForRestart() {
+        return player.chooseRestartOrExit() == RESTART;
+    }
+
+    private static boolean playNewGame() {
+        Game game = new Game();
+        game.run();
+        return game.askPlayerForRestart();
+    }
+
+    public static void play() {
+        boolean needNewGame = true;
+        while (needNewGame) {
+            needNewGame = playNewGame();
+        }
     }
 }
