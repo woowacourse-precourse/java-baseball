634e1343deda6c332dbbe5d952ec55c0589cd2df
saint6839 <saint6839@gmail.com>
2022-11-05 23:50:22 +0900

    feat(serviceTest) : 중복값 없을 때 입력값 리스트로 잘 변환 되는지 테스트

---
+++ src/test/java/baseball/service/PlayerServiceTest.java
@@ -0,0 +1,30 @@
+package baseball.service;
+
+import baseball.Ball;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.utils.Validator.validateDuplication;
+
+class PlayerServiceTest {
+    @Test
+    void convertInputToBallList_중복값이_없을_때_문자열_입력값이_공_리스트로_잘_변환되는지_테스트() {
+        //given
+        String playerInput = "123";
+        String[] inputSplits = playerInput.split("");
+
+        List<Ball> playerBalls = new ArrayList<>();
+        //when
+        for (String inputSplit : inputSplits) {
+            Ball ball = new Ball(Integer.parseInt(inputSplit));
+            validateDuplication(playerBalls, ball);
+            playerBalls.add(ball);
+        }
+        //then
+        Assertions.assertThat(playerBalls.size())
+                  .isEqualTo(playerInput.length());
+    }
+}
