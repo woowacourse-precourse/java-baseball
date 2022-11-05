d8d1fb8b7ad0df7a6cf5d771f873ddb3c82a079a
wlsh44 <k87913j@gmail.com>
2022-11-06 00:37:48 +0900

    feat: 게임 재시작 또는 종료 기능 추가

---
+++ src/main/java/baseball/exception/InvalidStatusCodeException.java
@@ -0,0 +1,10 @@
+package baseball.exception;
+
+public class InvalidStatusCodeException extends IllegalNumberInputException {
+
+    private static final String INVALID_STATUS_NUMBER = "0 또는 1을 입력해주세요. 입력 문자: %s";
+
+    public InvalidStatusCodeException(String str) {
+        super(String.format(INVALID_STATUS_NUMBER, str));
+    }
+}
