d2487b538f56c612d7df24d5f7a1335a3642f9c3
juhee park <jouhi3322@gmail.com>
2022-11-06 04:00:17 +0900

    feat: 게임종료시에 1,2를 입력하여 게임을 재실행 및 종료한다.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,10 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
     private static final String START_MSG = "숫자 야구 게임을 시작합니다.";
-    private static final Computer computer = new Computer();
-    private static final User user = new User();
-    private static Status status = Status.Playing;
+    public static final String GAME_FINISH_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String CHECKING_REPLAYING_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final int ballSize = 3;
+
+    private Computer computer;
+    private User user;
+    private Status status;
+
+    public Game() {
+        computer = new Computer();
+        user = new User();
+        status = Status.Playing;
+    }
 
     public void start() {
         System.out.println(START_MSG);
@@ -16,14 +28,32 @@ public class Game {
             countResult();
         }
 
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(GAME_FINISH_MSG);
+        if (checkingReplaying()) {
+            restart();
+        }
+    }
+
+    private void restart() {
+        status = Status.Playing;
+        computer.setRandNums();
+        start();
+    }
+
+    private boolean checkingReplaying() {
+        System.out.println(CHECKING_REPLAYING_MSG);
+        int userStatus = Integer.parseInt(Console.readLine());
+
+        if (userStatus == 1) return true;
+        else if (userStatus == 2) return false;
+        else throw new IllegalArgumentException();
     }
 
     private void countResult() {
         int ballCnt = 0;
         int strikeCnt = 0;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < ballSize; i++) {
             int cpNum = computer.getNums().get(i);
             int userNum = user.getNums().get(i);
 
@@ -32,7 +62,7 @@ public class Game {
         }
 
         System.out.println(showResult(ballCnt, strikeCnt));
-        if(strikeCnt==3)status=Status.End;
+        if (strikeCnt == ballSize) status = Status.End;
     }
 
     private String showResult(int ballCnt, int strikeCnt) {
