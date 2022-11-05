30dc23b155ec072c660cca6ef247031ec4cdee33
saint6839 <saint6839@gmail.com>
2022-11-05 23:10:30 +0900

    feat(utils) : 문자 -> 숫자 변환에 대한 예외 검증 메서드 구현

--- src/main/java/baseball/utils/Validator.java
+++ src/main/java/baseball/utils/Validator.java
@@ -6,9 +6,14 @@ import java.util.List;
 
 public final class Validator {
     private Validator() {}
+    public static final String INVALID_INPUT_VALUE_MESSAGE = "입력값은 서로 다른 1부터 9사이의 숫자로 이루어져야 합니다.";
 
     public static void validateParseStringToInt(String playerInput) {
-
+        try {
+            Integer.parseInt(playerInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT_VALUE_MESSAGE);
+        }
     }
 
     public static void validateInputLength(String playerInput) {
