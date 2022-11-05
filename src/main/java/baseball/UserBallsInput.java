70f1d5abbbe767a04d277548ab678393fe6a8190
choiejae <ejae8319@gmail.com>
2022-11-05 18:09:58 +0900

    test: IsCreateUserBalls 테스트 구현

--- src/main/java/baseball/UserBallsInput.java
+++ src/main/java/baseball/UserBallsInput.java
@@ -1,2 +1,12 @@
-package baseball;public class UserBallsInput {
+package baseball;
+
+public class UserBallsInput {
+  private int maxLen;
+
+  public UserBallsInput() {
+    this(3);
+  }
+  public UserBallsInput(int maxLen) {
+    this.maxLen = maxLen;
+  }
 }
