6cd38c9d3c74d46714ce6cb445f4d260522ed0ba
whibaek <jibaek0513@gmail.com>
2022-11-05 22:36:54 +0900

    feat(Aplication Class): add method
    add makeUserNumberToList method
    - change userNumber from Integer to List<Integer> type

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,19 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public List<Integer> makeUserNumberToList(Integer userNumber){
+        List<Integer> listNumber = new ArrayList<>();
+        while(userNumber < 0){
+            listNumber.add(userNumber%10);
+            userNumber /= 10;
+        }
+        return listNumber;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
