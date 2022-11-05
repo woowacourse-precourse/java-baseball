fe1212cd8696442e65f0d5f7f5d6072559b01b7b
yoojin Jang <yoojinjangjang@gmail.com>
2022-11-05 17:53:50 +0900

    [feat] 컴퓨터의 3가지 랜덤 숫자 생성 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,8 @@ public class Application {
         Game game = new Game();
         System.out.println(game.opening());
 
-        ComputerNums computerNums = new ComputerNums();
+        do {
+            game.doGame(); // 게임을 수행한다. (컴퓨터의 랜덤 발생, 사용자 입력 받기, 비교하기, 정답인 경우 return )
+        } while (true);
     }
 }
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,10 +4,15 @@ public class Game {
     private final String openingPhrase = "숫자 야구 게임을 시작합니다.";
 
     public Game() {
+        ComputerNums computerNums = new ComputerNums();
     }
 
 
     public String opening() {
         return openingPhrase;
+    }
+
+    public void doGame() {
+
     }
 }
