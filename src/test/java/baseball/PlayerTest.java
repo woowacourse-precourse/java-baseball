5eaff6dbbf404b3323c61b33ea601cd9fc03d881
5nam <ohnam90@gmail.com>
2022-11-05 22:45:02 +0900

    test: 플레이어가 입력한 서로 다른 숫자 3개 반환 기능에 대한 예외처리 테스트 코드 추가 1. 플레이어가 입력한 값 숫자인지 테스트 2. 플레이어가 입력한 숫자 길이 테스트 3. 플레이어가 입력한 값 서로 다른 숫자인지 테스트

---
+++ src/test/java/baseball/PlayerTest.java
@@ -0,0 +1,42 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+public class PlayerTest {
+
+    // 플레이어 기능 구현을 위한 테스트
+    @Test
+    public void 플레이어가_입력한_값_숫자인지_테스트() {
+        // given
+        String userInput = "1b2";
+        // when, then
+        assertThatThrownBy(() -> {
+            player.checkNumber(userInput);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("숫자만 입력 가능합니다.");
+    }
+
+    @Test
+    public void 플레이어가_입력한_숫자_길이_테스트() {
+        // given
+        String userInput = "1234";
+        // when, then
+        assertThatThrownBy(() -> {
+            player.checkLength(userInput);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("3자리 숫자만 입력 가능합니다.");
+    }
+
+    @Test
+    public void 플레이어가_입력한_값_서로_다른_숫자인지_테스트() {
+        // given
+        String userInput = "155";
+        // when, then
+        assertThatThrownBy(() -> {
+            player.checkDuplication(userInput);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("서로 다른 3자리 숫자만 입력이 가능합니다.");
+    }
+}
