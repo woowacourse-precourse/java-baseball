443fcf3da963c702825fc896dce284ba32afaba1
Chohye <cindykim2010@gmail.com>
2022-11-05 21:43:54 +0900

    S1. 컴퓨터 숫자 생성 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 컴퓨터 숫자 생성
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int[] pcNum = genPcNum();
+        // System.out.println(pcNum[0]+" "+pcNum[1]+" "+pcNum[2]);
+
     }
+    private static int[] genPcNum() {
+        int[] pcNum = new int[3];
+        pcNum[0] = pickNumberInRange(1, 9);
+        do {
+            pcNum[1] = pickNumberInRange(1, 9);
+        } while (pcNum[1] == pcNum[0]);
+        do {
+            pcNum[2] = pickNumberInRange(1, 9);
+        } while (pcNum[0] == pcNum[2] || pcNum[1] == pcNum[2]);
+        return pcNum;
+    }
+
 }
