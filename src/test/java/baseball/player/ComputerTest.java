7ca192abc646b4b1380f41216f7a3e742b638ada
SeokHwan An <asok369258@naver.com>
2022-11-06 00:45:48 +0900

    test : 생성된 문제가 서로 다른 숫자로 구성되어있는지 검증하는 테스트

--- src/test/java/baseball/player/ComputerTest.java
+++ src/test/java/baseball/player/ComputerTest.java
@@ -2,7 +2,9 @@ package baseball.player;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 class ComputerTest {
@@ -30,5 +32,22 @@ class ComputerTest {
         List<Integer> problem = computer.getProblem();
 
         assertThat(problem.size()).isEqualTo(3);
+    }
+
+    @Test
+    void 생성된_문제가_서로_다른_숫자로_구성되어있는지_검증하는_테스트() {
+        computer.createProblem();
+        List<Integer> problem = computer.getProblem();
+        List<Integer> numList = new ArrayList<>(List.of(0, 0, 0, 0, 0, 0, 0, 0, 0, 0));
+
+        for (Integer problemNum : problem) {
+            numList.set(problemNum, numList.get(problemNum) + 1);
+        }
+
+        List<Integer> usedNum = numList.stream()
+            .filter(n -> n > 1)
+            .collect(Collectors.toList());
+
+        assertThat(usedNum.size()).isEqualTo(0);
     }
 }
