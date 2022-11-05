d87d776615138bc094d3f06e05f4accb2c872555
syahn <496300@naver.com>
2022-11-05 13:34:25 +0900

    FEAT : 숫자 야구 게임을 위한 3자리의 수 생성 기능 추가
    1부터 9까지 서로 다른 임의의 수 3개를 랜덤으로 추출함.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public static List<Integer> createComputerNum(){
+        List<Integer> computerNum = new ArrayList<>();
+        while (computerNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNum.contains(randomNumber))
+                computerNum.add(randomNumber);
+        }
+        return computerNum;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
     }
 }
