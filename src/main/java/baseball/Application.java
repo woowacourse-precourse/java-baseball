0d8ec1eca81b408aff436c526eb720373fb91a0a
yenawee <yaena0319@naver.com>
2022-11-05 22:14:44 +0900

    [FEAT] 맞춰야 하는 3자리 숫자 만들기(random)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    }
+}
+
+class Computer {
+    List<Integer> pickRandomNumber(){
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3){
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(number)){
+                answer.add(number);
+            }
+        }
+        return answer;
     }
 }
