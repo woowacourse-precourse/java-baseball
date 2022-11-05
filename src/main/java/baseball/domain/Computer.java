0cec33090405832d05553c69118039e929ad569a
JUHA <khabh@naver.com>
2022-11-05 13:08:19 +0900

    feat(Computer): 컴퓨터 class 생성
    포함된 기능:
    - 스트라이크 숫자 3자리 생성
    - 힌트 제공

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,24 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Computer {
+    private final static int STRIKE = 0;
+    private final static int BALL = 1;
+
+    private final HintGenerator hintGenerator;
+
+    Computer() {
+        ArrayList<Integer> strikeNumbers = new StrikeNumbersGenerator().generateStrikeNumbers();
+        this.hintGenerator = new HintGenerator(strikeNumbers);
+    }
+
+    public int provideHint(ArrayList<Integer> playerGuess) {
+        HashMap<Integer, Integer> hint = hintGenerator.generateHint(playerGuess);
+        int strikeCount = hint.get(STRIKE);
+        Output.printResult(hint.get(BALL), strikeCount);
+
+        return strikeCount;
+    }
+}
