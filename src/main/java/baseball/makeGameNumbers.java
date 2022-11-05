820632593badcd494a39dc6058a4a856ca4f7395
ch200203 <ch200205@naver.com>
2022-11-05 10:05:45 +0900

    fix : 클래스명 변경

--- src/main/java/baseball/makeGameNumbers.java
+++ src/main/java/baseball/makeGameNumbers.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import baseball.domain.BaseBallNumber;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,7 +9,7 @@ public class makeGameNumbers {
     private List<Number> numberList;
 
     public makeGameNumbers(List<Integer> numberList) {
-        this.numberList = numberList.stream().map(Number::new).collect(Collectors.toList());
+        this.numberList = numberList.stream().map(BaseBallNumber::new).collect(Collectors.toList());
     }
 
 }
