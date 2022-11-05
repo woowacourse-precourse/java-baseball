89f032b5b8f6121ce9dac667d3fe3e65fce0bb5b
choiejae <ejae8319@gmail.com>
2022-11-05 16:43:53 +0900

    test: IsGenerateThreeBalls 테스트 생성

--- src/test/java/baseball/ComputerBallsGeneratorTest.java
+++ src/test/java/baseball/ComputerBallsGeneratorTest.java
@@ -2,13 +2,24 @@ package baseball;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 class ComputerBallsGeneratorTest {
-
-  @Test
-  void isClassCreate() {
-    ComputerBallsGenerator generator = new ComputerBallsGenerator(3);
+  ComputerBallsGenerator generator;
+//  @BeforeEach
+  @BeforeEach
+  void setUp() {
+    generator = new ComputerBallsGenerator(3);
   }
 
+  @Test
+  @Order(1)
+  @DisplayName("")
+  void isGenerateThreeBalls() {
+    String str = generator.ballGenerator();
+    assertEquals(3, str.length());
+  }
 }
