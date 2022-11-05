25f107a6afb0630838c2dba198b373190fa864de
MinHyeok Moon <odo27@MinHyeoks-MacBook-Air.local>
2022-11-05 12:45:19 +0900

    feat(Player): add getRestart

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -49,4 +49,9 @@ public class Player {
         }
         return isAllDifferent(number);
     }
+
+    public String getRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Console.readLine();
+    }
 }
