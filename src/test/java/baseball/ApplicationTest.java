bdc991e6071847e124dff50c188a32b46ab13816
cmong0516 <cmong0516@gmail.com>
2022-11-05 22:02:36 +0900

    test(ApplicationTest): Add Test Case
    validUserInput 메서드에 올바르지 않은 값에 대한 메세지 검증 및 올바른 값 검증

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,16 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import java.util.Objects;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -32,4 +36,105 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+
+    @Test
+    void computerMakeRandomNumber() {
+        //given
+        Computer computer = new Computer();
+        int cnt = 0;
+
+        //when
+        List<Integer> computerNumbers = computer.makeComputerNumbers();
+
+        for (int i = 1; i < computerNumbers.size(); i++) {
+            if (Objects.equals(computerNumbers.get(i - 1), computerNumbers.get(i))) {
+                cnt++;
+            }
+        }
+
+        //then
+        Assertions.assertThat(cnt).isEqualTo(0);
+    }
+
+    @Test
+    void validUserInputOverLap() {
+        //given
+        User user = new User();
+        String userInput = "111";
+
+        //when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            user.validUserInput(userInput);
+        });
+
+        String message = exception.getMessage();
+
+        //then
+        assertThat(message).isEqualTo("이미 입력한 숫자는 입력할수 없습니다.");
+    }
+
+    @Test
+    void validUserInputZero() {
+        //given
+        User user = new User();
+        String userInput = "012";
+
+        //when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            user.validUserInput(userInput);
+        });
+
+        String message = exception.getMessage();
+
+        //then
+        assertThat(message).isEqualTo("0은 입력할수 없습니다.");
+    }
+
+    @Test
+    void validUserInputSpace() {
+        //given
+        User user = new User();
+        String userInput = "  1";
+
+        //when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            user.validUserInput(userInput);
+        });
+
+        String message = exception.getMessage();
+
+        //then
+        assertThat(message).isEqualTo("공백은 입력할수 없습니다.");
+    }
+
+    @Test
+    void validUserInputLengthOver() {
+        //given
+        User user = new User();
+        String userInput = "1234";
+
+        //when
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            user.validUserInput(userInput);
+        });
+
+        String message = exception.getMessage();
+
+        //then
+        assertThat(message).isEqualTo("숫자는 세자리 수만 입력 가능합니다.");
+    }
+
+    @Test
+    void validUserInputOk() {
+        //given
+        User user = new User();
+        String userInput = "123";
+
+        //when
+        String validUserInput = user.validUserInput(userInput);
+
+        //then
+        assertThat(validUserInput).isEqualTo(userInput);
+    }
+
 }
