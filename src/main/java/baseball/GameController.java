e1e90f705344632fc2d4236c9b64c7bc77080cb6
lzlkolo <lzlko1209@gmail.com>
2022-11-05 11:08:20 +0900

    feat: 사용자에게 숫자 입력받는 기능 구현

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,5 +1,9 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.nio.channels.IllegalChannelGroupException;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameController {
@@ -8,5 +12,17 @@ public class GameController {
 
     public static void start() {
         Message.start();
+    }
+
+    public void playing() {
+        List<Integer> userNumber = getUserNumber();
+    }
+
+    public List<Integer> getUserNumber() throws IllegalArgumentException {
+        List<Integer> userNumber = new ArrayList<>();
+        Message.getUserNumber();
+        String inputNumber = Console.readLine();
+
+        return userNumber;
     }
 }
