b859112f0495b35dc01a2a27a96fe06c46794f0d
Jimin <thswlals0219@gmail.com>
2022-11-05 23:57:01 +0900

    feat: 스트라이크 스코어 결과 값을 구하는 기능 추가
    getStrikeCountResult()

--- src/main/java/baseball/view/GameMessage.java
+++ src/main/java/baseball/view/GameMessage.java
@@ -1,7 +1,23 @@
 package baseball.view;
 
+import baseball.BaseballScore;
+import java.util.Map;
+
 public class GameMessage {
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printGameResult(Map<BaseballScore, Integer> resultScore) {
+        String resultString = "";
+        resultString += getStrikeCountResult(resultScore.get(BaseballScore.STRIKE));
+
+    }
+
+    public String getStrikeCountResult(int strikeCount) {
+        if(strikeCount > 0) {
+            return strikeCount + BaseballScore.STRIKE.getScoreName();
+        }
+        return "";
     }
 }
