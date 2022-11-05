01a8549e26e9b5288dfa768a5fa5d46585604a81
왕준영 <wangjunyoung@wangjun-yeong-ui-MacBookPro.local>
2022-11-05 14:03:34 +0900

    feat: 힌트 생성 기능 추가

--- src/main/java/baseball/Opponent.java
+++ src/main/java/baseball/Opponent.java
@@ -2,6 +2,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -17,10 +18,43 @@ public class Opponent {
             }
         }
     }
+    public boolean isStrike(int key,int value){
+        if(numbers.get(key)==value)
+            return true;
+        return false;
+    }
+    public boolean isBall(int key,int value){
+        for(int i=0; i<3; i++){
+            if(key==i) continue;
+            if(numbers.get(i)==value)return true;
+        }
+        return false;
+    }
+    public int getValue(char value){
+        return Character.getNumericValue(value);
+    }
+    public String makeHint(String input){
+        String hint="";
+        int strike=0;
+        int ball=0;
+        int value;
+        for(int i=0; i<3; i++){
+            value = getValue(input.charAt(i));
+            if(isStrike(i,value)){
+              strike++;
+              continue;
+            }
+            if(isBall(i,value)) ball++;
+        }
+        hint += Integer.toString(strike)+Integer.toString(ball);
+        return hint;
+    }
 
     public List<Integer> getNumbers() {
         return numbers;
     }
-
-
+    public void setNumbers(List<Integer> numberArray){
+       numbers.clear();
+       numbers.addAll(new ArrayList<Integer>(numberArray));
+    }
 }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -4,6 +4,10 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 import baseball.Opponent;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,10 +32,75 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
     @Test
-    void OpponentHaveThreeNumber(){
+    void opponentHaveThreeNumber(){
         Opponent cpu = new Opponent();
         assertThat(cpu.getNumbers().size()).isEqualTo(3);
+    }
+
+    @Test
+    void ballTestTrue(){
+        Opponent cpu = new Opponent();
+        List<Integer> cpu_numbers = List.of(1,2,3);
+        cpu.setNumbers(cpu_numbers);
+
+        assertThat(cpu.isBall(0,3)).isEqualTo(true);
+        assertThat(cpu.isBall(1,3)).isEqualTo(true);
+    }
+
+    @Test
+    void ballTestFalse(){
+        Opponent cpu = new Opponent();
+        List<Integer> cpu_numbers = List.of(1,2,3);
+        cpu.setNumbers(cpu_numbers);
+
+        //strike
+        assertThat(cpu.isBall(0,1)).isEqualTo(false);
+        assertThat(cpu.isBall(1,2)).isEqualTo(false);
+        assertThat(cpu.isBall(2,3)).isEqualTo(false);
+
+        //no-ball
+        assertThat(cpu.isBall(0,5)).isEqualTo(false);
+        assertThat(cpu.isBall(1,9)).isEqualTo(false);
+    }
+
+    @Test
+    void strikeTestTrue(){
+        Opponent cpu = new Opponent();
+        List<Integer> cpu_numbers = List.of(1,2,3);
+        cpu.setNumbers(cpu_numbers);
+
+        assertThat(cpu.isStrike(0,1)).isEqualTo(true);
+        assertThat(cpu.isStrike(1,2)).isEqualTo(true);
+        assertThat(cpu.isStrike(2,3)).isEqualTo(true);
+    }
+
+    @Test
+    void strikeTestFalse(){
+        Opponent cpu = new Opponent();
+        List<Integer> cpu_numbers = List.of(1,2,3);
+        cpu.setNumbers(cpu_numbers);
+
+        //ball
+        assertThat(cpu.isStrike(0,2)).isEqualTo(false);
+        assertThat(cpu.isStrike(1,3)).isEqualTo(false);
+        assertThat(cpu.isStrike(2,1)).isEqualTo(false);
+        //no-strike;
+        assertThat(cpu.isStrike(1,9)).isEqualTo(false);
+        assertThat(cpu.isStrike(2,8)).isEqualTo(false);
+    }
+
+    @Test
+    void makeHintTest(){
+        Opponent cpu = new Opponent();
+        List<Integer> cpu_numbers = List.of(1,2,3);
+        cpu.setNumbers(cpu_numbers);
+
+        assertThat(cpu.makeHint("123")).isEqualTo("30");
+        assertThat(cpu.makeHint("231")).isEqualTo("03");
+        assertThat(cpu.makeHint("124")).isEqualTo("20");
+        assertThat(cpu.makeHint("132")).isEqualTo("12");
     }
 
     @Override
