f7c76f32da65e29053d33bb5fde55305d33e80da
sujin0607 <goweg0607@gmail.com>
2022-11-06 03:44:32 +0900

    feat: BaseBallGame 실행함수인 run 추가

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -3,26 +3,27 @@ package baseball;
 import camp.nextstep.edu.missionutils.*;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class BaseBallGame {
-    private List<Integer> computer = new ArrayList<>();
-    private List<Integer> user = new ArrayList<>();
-    private List<Integer> strikeAndBall = new ArrayList<>();
+    private List<Integer> computer;
+    private List<Integer> user;
 
     public List<Integer> randomNumbers() {
+        computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNum = Randoms.pickNumberInRange(1, 9);
             if (computer.contains(randomNum) == false) {
                 computer.add(randomNum);
             }
         }
-        System.out.println("computer숫자"+computer);
+        System.out.println("computer숫자" + computer);
         return computer;
     }
 
-    public List<Integer> inputNumbers() throws IllegalStateException {
+    public List<Integer> inputNumbers(){
+         user = new ArrayList<>();
+        System.out.printf("숫자를 입력해주세요 : ");
         String inputNum = Console.readLine();
         String[] nums = inputNum.split("");
 
@@ -40,20 +41,47 @@ public class BaseBallGame {
         return user;
     }
 
-    public List<Integer> findNumber(List<Integer> computer, List<Integer> user){
+    public List<Integer> findNumber(List<Integer> computer, List<Integer> user) {
         int strike = 0;
         int ball = 0;
 
-        for(int i=0; i<computer.size(); i++){
-            if(computer.get(i) == user.get(i)){
-                strike ++;
-            }else if(computer.contains(user.get(i))){
-                ball ++;
+        for (int i = 0; i < computer.size(); i++) {
+            if (computer.get(i) == user.get(i)) {
+                strike++;
+            } else if (computer.contains(user.get(i))) {
+                ball++;
             }
         }
+
+        List<Integer> strikeAndBall = new ArrayList<>();
         strikeAndBall.add(strike);
         strikeAndBall.add(ball);
         return strikeAndBall;
+    }
+
+    public void run() {
+        List<Integer> computerNumber = randomNumbers();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (true) {
+            List<Integer> userNumber = inputNumbers();
+            List<Integer> result = findNumber(computerNumber, userNumber);
+            int strike = result.get(0);
+            int ball = result.get(1);
+
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            } else if (strike != 0 && ball != 0) {
+                System.out.printf("%d볼 %d스트라이크\n", strike, ball);
+            } else if (strike != 0) {
+                System.out.printf("%d스트라이크\n", strike);
+            } else if (ball != 0) {
+                System.out.printf("%d볼\n", ball);
+            } else {
+                System.out.println("낫싱");
+            }
+        }
     }
 
 }
