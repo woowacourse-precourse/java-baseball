608a9ffea9d06d1a5b4bfce218d79fafdce50c98
qdeu22 <qdeu22@gmail.com>
2022-11-05 13:11:09 +0900

    refactor(inputNumber): 유효성 검사 기능 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,12 +1,15 @@
 package baseball.view;
 
+import baseball.utils.ValidNumber;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String PLEASE_INPUT = "숫자를 입력해주세요 : ";
 
-    public static String inputNumber(){
+    public static String inputNumber() {
         System.out.print(PLEASE_INPUT);
-        return Console.readLine();
+        String inputNumber = Console.readLine();
+        ValidNumber.validUserNumber(inputNumber);
+        return inputNumber;
     }
 }
