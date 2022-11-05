8b0234eb3cd51e950d4d99077adbdf5aa26894dd
Dongmin Kim <kimdongmin68@naver.com>
2022-11-05 15:40:12 +0900

    fix(user, computer): 숫자 값을 담는 List 객체 생성 위치 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -10,13 +10,15 @@ public class Computer {
     private static final int MAX_NUMBER = 9;
     private static final int ANSWER_LENGTH = 3;
 
-    private List<Integer> computer = new ArrayList<>();
+    private List<Integer> computer;
 
     public void generateAnswer() {
+        computer = new ArrayList<>();
         while (computer.size() < ANSWER_LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             if(!computer.contains(randomNumber)){
                 computer.add(randomNumber);
+                System.out.print(randomNumber + " ");
             }
         }
     }
--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -6,13 +6,14 @@ import java.util.List;
 public class User {
     private final static int NUMBER_LENGTH = 3;
 
-    private List<Integer> userNumber = new ArrayList<>();
+    private List<Integer> userNumber;
 
     public List<Integer> getUserNumber() {
         return userNumber;
     }
 
     public void setUserNumber(String userInput) {
+        userNumber = new ArrayList<>();
         for (int digit = 0; digit < NUMBER_LENGTH; digit++) {
             userNumber.add(Character.getNumericValue(userInput.charAt(digit)));
         }
