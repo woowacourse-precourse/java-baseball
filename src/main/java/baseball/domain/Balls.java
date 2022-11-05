3d062b0a771b0fe80132789cec65cbd177244f93
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-11-05 11:39:30 +0900

    feat(3단계) : Balls 유효성 검사

---
+++ src/main/java/baseball/domain/Balls.java
@@ -0,0 +1,36 @@
+package baseball.domain;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Balls {
+    private List<Ball> balls;
+
+    public Balls(List<Integer> ballNumbers) {
+        validateSize(ballNumbers);
+        validateUnique(ballNumbers);
+        balls = ballNumbers.stream()
+                .map(Ball::new)
+                .collect(Collectors.toList());
+    }
+
+    private void validateUnique(List<Integer> ballNumbers) {
+        if (ballNumbers.size() != ballNumbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("서로 다른 값을 입력해주세요.");
+        }
+
+    }
+
+    private void validateSize(List<Integer> ballNumbers) {
+        if (ballNumbers.size() != 3) {
+            throw new IllegalArgumentException("3개의 숫자를 입력해주세요.");
+        }
+    }
+    public Judgement play(Ball ball) {
+        balls.stream()
+
+    }
+    public Judgement play(Balls balls) {
+    }
+
+}
--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -1,4 +1,15 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class BallsTest {
+    @Test
+    public void ballsTest() {
+        Balls computerBalls = new Balls(List.of(1,2,3));
+        assertThat(computerBalls.play(new Ball(4))).isEqualTo(Judgement.NOTHING);
+    }
 }
