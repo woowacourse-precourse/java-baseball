625878f00962be6279401d38856209bf1eaab05a
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-05 17:02:55 +0900

    feat(showResultMessage): show player the result of prediction

--- src/main/java/baseball/BaseballGameConsole.java
+++ src/main/java/baseball/BaseballGameConsole.java
@@ -31,7 +31,20 @@ public class BaseballGameConsole {
             Result result = computer.resultOfPrediction(randomNumber, predictedNumber);
             matchedAllNumber = result.getResultType() == EXACT_MATCH;
 
-            // Show result message
+            showResultMessage(result.getResultType(), result.numberOfBall(), result.numberOfStrike());
         } while (!matchedAllNumber);
+    }
+
+    private void showResultMessage(ResultType resultType, int numberOfBall, int numberOfStrike) {
+        if (resultType == EXACT_MATCH)
+            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        else if (resultType == BALL_AND_STRIKE)
+            System.out.println(numberOfBall + "볼 " + numberOfStrike + "스트라이크");
+        else if (resultType == ONLY_STRIKE)
+            System.out.println(numberOfStrike + "스트라이크");
+        else if (resultType == ONLY_BALL)
+            System.out.println(numberOfBall + "볼");
+        else if (resultType == NOTHING)
+            System.out.println("낫싱");
     }
 }
