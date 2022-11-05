ce1c69c59a1e4a6aaf62c9de46cc1ee85c5bebfd
shinyehwan <jghisjl007@naver.com>
2022-11-05 18:14:36 +0900

    feat : InputView 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,15 @@
 package baseball.view;
 
+import java.util.Scanner;
+
 public class InputView {
+	private static final String startMessage = "숫자를 입력해주세요 : ";
+	private static final String retryMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+	public static String printStartMessage() {
+		return startMessage;
+	}
+	public static String printRetryMessage() {
+		return retryMessage;
+	}
 }
