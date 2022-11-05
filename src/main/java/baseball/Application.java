29547da638b1b66e99b6e49064d7587c93a9c238
yu-heejin <jkgt0521@naver.com>
2022-11-06 01:52:58 +0900

    fix : 낫싱 출력 오류 수정하기 출력 결과에서 스트라이크와 볼만 생각하고 낫싱을 추가하지 못했다. 오류를 해결하기 위해 낫싱을 추가했다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -20,6 +20,8 @@ public class Application {
             System.out.print("숫자를 입력해주세요 : ");
             String number = readLine();
 
+            if (number.length() < MAX_NUMBER_SIZE) continue;
+
             for (int i = 0; i < MAX_NUMBER_SIZE; i++) {
                 char dividedNumber = number.charAt(i);
                 // dividedNumber의 자리수는 따로 구할 필요 없이 i를 사용한다.
@@ -35,6 +37,8 @@ public class Application {
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 goGame = Integer.parseInt(readLine());
                 randomNumber = String.valueOf(pickNumberInRange(100, 999));
+            } else if (ballCount == 0 && strikeCount == 0) {
+                System.out.println("낫싱");
             } else {
                 System.out.println(strikeCount + "스트라이크 " + ballCount + "볼");
             }
