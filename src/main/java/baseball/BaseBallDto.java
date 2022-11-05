afd0e79a1534a3956062d3e6cb01ca28f5300809
kimJaeHyeon <kmr2644@gmail.com>
2022-11-05 22:57:21 +0900

    feat: baseballDto 구현

--- src/main/java/baseball/BaseBallDto.java
+++ src/main/java/baseball/BaseBallDto.java
@@ -1,2 +1,20 @@
-package baseball;public class BaseBallDto {
+package baseball;
+
+public class BaseBallDto {
+
+    private int strike;
+    private int ball;
+
+    public BaseBallDto(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public String getResult() {
+        if (strike==0&&ball==0){
+            return "낫싱";
+        }
+        return String.format("%s볼 %s스트라이크\n", ball, strike);
+    }
+
 }
