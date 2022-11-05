5aaa98d9b45344f4e69f9de4ffe2fb65d9f6d8fa
cmong0516 <cmong0516@gmail.com>
2022-11-05 15:58:40 +0900

    feat(Application Class): BaseBall 게임 실행
    BaseBall 객체를 만들어 게임을 실행하며 게임이 종료된후 user 가 새로운 게임 진행값인 1 을 입력할 경우 게임이 계속
    진행된다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,13 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseBallGame baseBallGame = new BaseBallGame();
+
+        baseBallGame.startGame();
+
+        do {
+            baseBallGame.playGame();
+        } while (baseBallGame.restartOrStop() == 1);
     }
 }
+
