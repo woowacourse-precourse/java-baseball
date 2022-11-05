4db4851409e768d9f24017f1b48bfe1eeece2250
jthugg <jthug@naver.com>
2022-11-06 08:15:13 +0900

    Feat: 게임 진행 상태 초기화 기능 추가

--- src/main/java/baseball/Status.java
+++ src/main/java/baseball/Status.java
@@ -1,5 +1,16 @@
 package baseball;
 
 public class Status {
+	String status;
+	String STATUS_INIT = "INIT";
+	String STATUS_PLAYING = "PLAYING";
+	String STATUS_TERMINATING = "TERMINATING";
 
+	Status() {
+		initStatus();
+	}
+
+	public void initStatus() {
+		status = STATUS_INIT;
+	}
 }
