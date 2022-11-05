4abc78917c0de193e201613ccfd28d9223f22d83
matt <matt1235@naver.com>
2022-11-06 02:15:32 +0900

    feat: 게임 재시작/종료 선택 기능 구현 사용자가 정답을 맞췄을 때 게임을 계속 진행할 지 종료할 지 선택할 수 있다. 1입력 시 재시작, 2입력 시 종료. 1과 2중 하나의 수만 유효한 입력이다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,10 +1,35 @@
 package baseball;
 
+import java.util.List;
+
 public class BaseballGame {
 
     private final RandomNumberGenerator randomNumberGenerator;
+    private final UserInput userInput;
+    private final BaseballGameHint baseballGameHint;
+    private final ResultChecker resultChecker;
 
-    public BaseballGame(RandomNumberGenerator randomNumberGenerator) {
-        this.randomNumberGenerator = randomNumberGenerator;
+    public BaseballGame() {
+        this.randomNumberGenerator = new RandomNumberGenerator();
+        this.userInput = new UserInput();
+        this.baseballGameHint = new BaseballGameHint();
+        this.resultChecker = new ResultChecker();
+    }
+
+    public void run(){
+        do{
+            play();
+        }while(userInput.isContinue());
+    }
+
+    public void play(){
+        String hint = "";
+        List<Integer> computer = randomNumberGenerator.createNumber();
+
+        do{
+            List<Integer> player = userInput.inputNumber();
+            hint = baseballGameHint.generateHint(computer, player);
+            System.out.println(hint);
+        }while(!resultChecker.checkRightAnswer(hint));
     }
 }
