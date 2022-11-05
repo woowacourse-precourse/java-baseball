ba63301d92bae569b5b131c62f7a6c2f9f9ec683
Ridealist <cjsn92@gmail.com>
2022-11-05 21:26:53 +0900

    feat(User): 사용자 입력 유효성 판단, 올바른 사용자 입력값 List 반환 기능 구현

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -2,13 +2,49 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
     private String userInput;
+    private List<Integer> validatedInput;
 
     public void setUserInput() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
         userInput = input;
+    }
+
+    private void validateUserInput() {
+        Validator validator = new Validator(userInput);
+        validator.isLengthThree();
+        validator.isAllNumber();
+        validator.notContainsZero();
+        validator.isAllDifferent();
+    }
+
+    private void setValidatedInput() {
+        try {
+            validateUserInput();
+            List<Integer> arrayList = new ArrayList<>();
+            for (int i = 0; i < userInput.length(); i++) {
+                arrayList.add(userInput.charAt(i) - '0');
+            }
+            validatedInput = arrayList;
+        }
+        catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+    }
+
+    public List<Integer> getValidatedInput() {
+        try {
+            setValidatedInput();
+            return validatedInput;
+        }
+        catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
     }
 }
--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -2,6 +2,8 @@ package baseball;
 
 import java.io.*;
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -28,6 +30,12 @@ class UserTest {
         assertThat(out.toString()).isEqualTo("숫자를 입력해주세요 : ");
     }
 
+    /*
+    WARNING: An illegal reflective access operation has occurred
+    WARNING: Please consider reporting this to the maintainers of camp.nextstep.edu.missionutils.Console
+    WARNING: Use --illegal-access=warn to enable warnings of further illegal reflective access operations
+    private 필드 입력값 확인 test는 추후 삭제하기
+     */
     @Test
     void setUserInput_WhenRun_setUserInput_Success() {
         String input = "123";
@@ -45,5 +53,25 @@ class UserTest {
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    void getValidatedInput_WhenRun_getUserInputList_Success() {
+        String input = "123";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        user.setUserInput();
+        List<Integer> inputArray = user.getValidatedInput();
+        assertThat(inputArray).isEqualTo(Arrays.asList(1, 2, 3));
+    }
+
+    @Test
+    void getValidatedInput_WhenRun_getUserInputList_Fail() {
+        String input = "103";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        user.setUserInput();
+        assertThatThrownBy(() -> user.getValidatedInput())
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
