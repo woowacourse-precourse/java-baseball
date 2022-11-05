e422a4294c02f2d81cdd9241bdf33d2a6f8ac7ac
soyunim <togksk123@naver.com>
2022-11-06 02:57:43 +0900

    feat: create user's input number and exceptions

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -26,5 +27,52 @@ public class Application {
             }
         }
         return computerNum;
+    }
+
+    public static List<Integer> getInputNum(){
+        List<String> numbers = new ArrayList<>(List.of("1","2","3","4","5","6","7","8","9"));
+
+        System.out.print("숫자를 입력해주세요: ");
+        String userInput = Console.readLine();
+        System.out.println(userInput);
+
+        String[] inputNum = userInput.split("");
+        List<Integer> userNum = new ArrayList<>();
+
+        exception(numbers, userInput, inputNum, userNum);
+
+        return userNum;
+    }
+
+    public static void exception(List<String> numbers, String userInput, String[] inputNum, List<Integer> userNum){
+        if(userInput.contains("0")){
+            throw new IllegalArgumentException("1부터 9사이의 숫자만 입력해주세요.");
+        }
+        for(int inputIdx=0; inputIdx < inputNum.length; inputIdx++){
+            if(!numbers.contains(inputNum[inputIdx])){
+                throw new IllegalArgumentException("숫자만 입력해주세요.");
+            }
+            else{
+                userNum.add(Integer.valueOf(inputNum[inputIdx]));
+            }
+        }
+        if (userNum.size()!=numLen){
+            throw new IllegalArgumentException("3자리의 숫자만 입력해주세요.");
+        }
+        int cnt = 0;
+        int startIdx = 0;
+        int endIdx = 1;
+        while(cnt<numLen){
+            if(userNum.get(startIdx).equals(userNum.get(endIdx))){
+                throw new IllegalArgumentException("중복된 값이 입력되었습니다.");
+            }
+            else if(endIdx!=numLen-1){
+                endIdx++;
+            }
+            else if(endIdx==numLen-1){
+                startIdx++;
+            }
+            cnt++;
+        }
     }
 }
