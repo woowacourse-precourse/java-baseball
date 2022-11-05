4a05d787b6c5c5c0b4512a3df625908908e57f44
Minjune <minjune8506@gmail.com>
2022-11-05 14:23:58 +0900

    refactor(Printer): 힌트 생성 기능, 상수 추가
    - 기존 문자열들을 상수로 선언
    - hintMessage를 만들어주는 메서드 추가

--- src/main/java/baseball/Printer.java
+++ src/main/java/baseball/Printer.java
@@ -2,38 +2,50 @@ package baseball;
 
 public class Printer {
 
+    public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
+    public static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    public static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+    public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public static final String NOTHING = "낫싱";
+    public static final String STRIKE = "%d스트라이크";
+    public static final String BALL = "%d볼";
+    public static final String BALL_AND_STRIKE = "%d볼 %d스트라이크";
+
     private Printer() {
     }
 
     public static void printGameStart() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print(START_MESSAGE);
     }
 
     public static void printInputNumber() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_NUMBER_MESSAGE);
     }
 
     public static void printGameEnd() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.print(END_MESSAGE);
     }
 
     public static void printGameRestart() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.print(RESTART_MESSAGE);
     }
 
     public static void printHintMessage(int ballCount, int strikeCount) {
+        String hintMessage = buildHintMessage(ballCount, strikeCount);
+        System.out.println(hintMessage);
+    }
+
+    private static String buildHintMessage(int ballCount, int strikeCount) {
         if (ballCount == 0 && strikeCount == 0) {
-            System.out.println("낫싱");
-            return;
+            return NOTHING;
         }
-        if (ballCount > 0 && strikeCount > 0) {
-            System.out.printf("%d볼 %d스트라이크\n", ballCount, strikeCount);
-            return;
+        if (ballCount == 0) {
+            return String.format(STRIKE, strikeCount);
         }
-        if (ballCount > 0) {
-            System.out.printf("%d볼\n", ballCount);
-            return;
+        if (strikeCount == 0) {
+            return String.format(BALL, ballCount);
         }
-        System.out.printf("%d스트라이크\n", strikeCount);
+        return String.format(BALL_AND_STRIKE, ballCount, strikeCount);
     }
 }
