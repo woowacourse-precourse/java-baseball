75f5ad2888c141f4cc89aec66d74db8b0d1d1bf3
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-05 14:01:51 +0900

    feat:사용자 입력시 숫자가 아닌경우가 올 경우 예외 출력 기능 추가

--- src/main/java/baseball/Domain.java
+++ src/main/java/baseball/Domain.java
@@ -9,7 +9,6 @@ public class Domain {
 
     String userNumber;
     List<Integer> computerNumber;
-    List<Integer> userNumberList;
 
 
 
@@ -50,21 +49,48 @@ public class Domain {
 
 
     public static void isInputAlright(String userNumber) { // test 입력시오류경우_확인
+
+        //입력된 것이 숫자가 아닌 경우 ex) 알파벳이 올 수도 있음
+        if(!isNumber(userNumber)){
+            throw new IllegalArgumentException("숫자가 아님");
+        }
+
+
         //입력시 길이가 숫자길이가 3이 아닌경우
         if (userNumber.length() != 3) {
             throw new IllegalArgumentException("입력 오류");
         }
+
+
+
         //중복된 숫자가 있는지?
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                if (i == j) {
-                    continue;
-                }
-                char nowIndexUserNumber = userNumber.charAt(i);
-                char otherIndexUserNumber = userNumber.charAt(j);
-                inputOverlapCheck(nowIndexUserNumber, otherIndexUserNumber);
+                overlapCheckProcess(userNumber, i, j);
             }
         }
+    }
+
+    private static void overlapCheckProcess(String userNumber, int i, int j) {
+        if (i == j) {
+            return;
+        }
+        char nowIndexUserNumber = userNumber.charAt(i);
+        char otherIndexUserNumber = userNumber.charAt(j); // 여기 depth 3
+        inputOverlapCheck(nowIndexUserNumber, otherIndexUserNumber);
+    }
+
+    private static boolean isNumber(String userNumber) {
+        for(int index=0; index<userNumber.length(); index++){
+            char c = userNumber.charAt(index);
+            if(c>='1' && c<='9'){
+                continue;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
     }
 
     public static List<Integer> inputProcess(String userNumber) { // test 입력시오류경우_확인
