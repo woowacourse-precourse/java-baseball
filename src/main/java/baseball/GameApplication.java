771f45e2cf789f2699985af4c82cf0eb456ac02a
hodomaroo <enghemdwkd@gmail.com>
2022-11-05 18:49:53 +0900

    [Feat] : GameApplication 인터페이스 추가
    Docs 명세에 따라 전체 Game 주기를 관리하는 GameApplication 인터페이스를
    작성하고, JavaDocs로 간단한 설명 주석을 작성하였습니다.

---
+++ src/main/java/baseball/GameApplication.java
@@ -0,0 +1,11 @@
+package baseball;
+/**
+ * Game 전체 주기를 관리하는 인터페이스.
+ * 게임의 재실행 및 종료와 같은 기능을 담당
+ */
+public interface GameApplication {
+    /**
+     * GameApplication 실행 메서드
+     */
+    void execute();
+}
