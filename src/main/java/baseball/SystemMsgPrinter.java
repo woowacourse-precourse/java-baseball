c51703d9bd66f3085f0d1d6d312c1ed235dcd573
woosung1223 <dntjd991223@naver.com>
2022-11-05 19:29:48 +0900

    feat(SystemMsgPrinter): 시스템 메세지를 출력하는 메소드 추가

---
+++ src/main/java/baseball/SystemMsgPrinter.java
@@ -0,0 +1,17 @@
+package baseball;
+
+public final class SystemMsgPrinter {
+    private SystemMsgPrinter() {}
+
+    public static void printGameStartMessage() {
+        System.out.println(SystemConstant.GAME_START_MESSAGE);
+    }
+
+    public static void printGameEndMessage() {
+        System.out.println(SystemConstant.GAME_END_MESSAGE);
+    }
+
+    public static void printGameRestartMessage() {
+        System.out.println(SystemConstant.GAME_RESTART_MESSAGE);
+    }
+}
