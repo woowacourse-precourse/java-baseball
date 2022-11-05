2b839f6cb580ef57ca01eb7e8f08ba84e0cc5e6b
CHO Min HO <chominho96@naver.com>
2022-11-05 21:47:29 +0900

    feat(NumberComparator): add number list comparator
    Added computer's number and user's number comparator
    
     - Score class which has number of strikes and number of balls is defined
     - number comparator returns score class in each case

---
+++ src/main/java/baseball/numbercomparator/Score.java
@@ -0,0 +1,24 @@
+package baseball.numbercomparator;
+
+/**
+ * 스트라이크의 개수와 볼의 개수를 저장하는 클래스입니다.
+ */
+public class Score {
+    private final int strike; // 스트라이크의 개수
+    private final int ball; // 볼의 개수
+
+    public Score(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+
+    public int getBall() {
+        return ball;
+    }
+
+}
