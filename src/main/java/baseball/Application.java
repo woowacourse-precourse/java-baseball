5f60179d15ea0ad05c309af5969ffa35c7c62e10
Sungjaeyeong <wodud2587@gmail.com>
2022-11-05 20:39:34 +0900

    feat($compile): number 2 in the feature list
    Game start statement output.
    Add two functions:
    - printInitMessage
    - getUserAnswer

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -8,6 +9,17 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        printInitMessage();
+        getUserAnswer();
+    }
+
+    private static void printInitMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    private static void getUserAnswer() {
+        System.out.print("숫자를 입력해주세요 : ");
+        Console.readLine();
     }
 
     public static List<Integer> pickThreeNumbers() {
