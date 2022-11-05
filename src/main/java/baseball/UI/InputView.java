dd83a4935a9300378b8ab90b866c3b441434c5b1
SHEOMM <esh0821@snu.ac.kr>
2022-11-05 15:58:35 +0900

    feat(inputView): check error when takes input
    inputView에서 input을 입력받을 때 에러 체크
    - 주어진 input의 크기가 3인지, null이 아닌지
    - 주어진 input이 숫자인지
    - 중복된 숫자가 없는지

--- src/main/java/baseball/UI/InputView.java
+++ src/main/java/baseball/UI/InputView.java
@@ -1,6 +1,8 @@
 package baseball.UI;
 import baseball.model.Input;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.Set;
 
 public class InputView {
     private Input input;
@@ -8,9 +10,43 @@ public class InputView {
     public void getBaseballInput(){
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
+        checkInputValidation(input);
         this.input = new Input(input);
     }
 
+    private void checkInputValidation(String input){
+        checkInputSize(input);
+        checkNumericValue(input);
+        checkDuplicateValue(input);
+    }
+
+    private void checkInputSize(String input){
+        if(input == null || input.length() != 3){
+            throw new IllegalArgumentException("1~9 사이 자연수 3개를 입력해주세요.");
+        }
+    }
+
+    private void checkNumericValue(String input){
+
+        int notNumber = input.chars()
+                .filter(ch -> !Character.isDigit(ch))
+                .toArray()
+                .length;
+        if(notNumber != 0){
+            throw new IllegalArgumentException("숫자가 아닌 값은 입력될 수 없습니다.");
+        }
+    }
+
+    private void checkDuplicateValue(String input){
+        Set<Character> inputSet = new HashSet<>();
+        char[] inputChars = input.toCharArray();
+        for(char inputChar : inputChars){
+            inputSet.add(inputChar);
+        }
+        if(inputSet.size() != input.length()){
+            throw new IllegalArgumentException("중복된 값은 입력될 수 없습니다.");
+        }
+    }
     public Input getInput() {
         return input;
     }
