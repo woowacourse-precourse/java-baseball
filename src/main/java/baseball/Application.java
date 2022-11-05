7b5ddf9ecdc4530a5b545bdfbaba4a9bcb64036e
bini59 <myuni0234@gmail.com>
2022-11-05 20:54:25 +0900

    feat : generate Random number
    Add generate random number function
    - generate 3 different number
    - return List<Integer> size 3

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static List<Integer> generateRandNum(){
+        List<Integer> nums = new ArrayList<>();
+        while(nums.size() < 3){
+            int randNum = Randoms.pickNumberInRange(1, 9);
+            if(!nums.contains(randNum)){
+                nums.add(randNum);
+            }
+        }
+        return nums;
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
