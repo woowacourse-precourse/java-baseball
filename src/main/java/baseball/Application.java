4463d588a92c7f97faef68ff9fa3402832391f83
MINJU <alswn9938@gmail.com>
2022-11-05 11:31:58 +0900

    feat($Application): add 컴퓨터 랜덤 숫자 리스트 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,36 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    public List<Integer> getComputerNumberList(){
+        ArrayList<Integer> computerNumberList = new ArrayList<>();
+        while(computerNumberList.size() < 3){
+            int randomNumber = getValidRandomNumber(computerNumberList);
+            computerNumberList.add(randomNumber);
+        }
+        return computerNumberList;
+    }
+
+    private int getValidRandomNumber(List<Integer> lst){
+        int pickNumber;
+        do {
+            pickNumber = Randoms.pickNumberInRange(1, 9);
+        } while (!isValidNumber(pickNumber, lst));
+        return pickNumber;
+    }
+
+    public boolean isValidNumber(int number, List<Integer> lst){
+        if(number < 1 || number > 9){
+            return false;
+        }
+        return !lst.contains(number);
     }
 }
