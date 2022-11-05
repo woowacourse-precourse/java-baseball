be0c074e6411348b96272a406ccb9b8ff5f8715b
gyuturn <rbtjrrbals@gmail.com>
2022-11-05 23:37:44 +0900

    feat($model): 컴퓨터가 1~9까지의 서로다른 임의의 수 3개 생성
    B-1
    return 값은 List<Integer>

---
+++ src/main/java/baseball/model/Computer.java
@@ -0,0 +1,20 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Integer> randomNum;
+
+    public List<Integer> generateRandomNum(){
+        this.randomNum = new ArrayList<>();
+        while (randomNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNum.contains(randomNumber)) {
+                randomNum.add(randomNumber);
+            }
+        }
+        return this.randomNum;
+    }
+}
