664c3a63125e0b47ca4826101a1aa2a5b61eb269
syahn <496300@naver.com>
2022-11-05 16:10:42 +0900

    FEAT : 사용자 입력을 받는 기능 구현
    - 사용자 입력을 받아 arraylist에 저장하여 반환하도록 함
    - continueGame이 true일 때, 사용자 입력을 반복해서 받도록 함
    
    등)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -17,10 +18,24 @@ public class Application {
         return computerNum;
     }
 
+    public static List<Integer> getUserInput() {
+        List<Integer> userNum = new ArrayList<>();
+        String userInput = Console.readLine();
+        for (int i = 0; i < userInput.length(); i++) {
+            userNum.add((int) userInput.charAt(i));
+        }
+        return userNum;
+    }
+
     public void startGame() {
         List<Integer> computerNum = new ArrayList<>();
         System.out.println("숫자 야구 게임을 시작합니다.");
         computerNum = createComputerNum();
+        boolean continueGame = true;
+
+        while (continueGame) {
+            List<Integer> userNum = getUserInput();
+        }
     }
 
     public static void main(String[] args) {
