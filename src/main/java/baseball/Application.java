70ff6c928ca047b87764ba48b31413e563e0cba9
bunsung92 <bunsung92@naver.com>
2022-11-05 15:04:12 +0900

    feat: Application 야구게임 시작 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import domain.Computer;
+import domain.Referee;
+import domain.User;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        User user = new User();
+        Referee referee = new Referee();
+        Computer computer = new Computer();
+
+        playBaseballGame(user, referee, computer);
     }
+
+    private static void playBaseballGame(User user, Referee referee, Computer computer) {
+        
+    }
+
 }
