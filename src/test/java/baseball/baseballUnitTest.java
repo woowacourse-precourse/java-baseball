eede700648d153e338772facc37ed32576beaf25
Heosejin98 <sejin3319@naver.com>
2022-11-05 22:40:15 +0900

    Test : 테스트 케이스 수정
    메서드 형식이 바뀌어 테스트 케이스 수정

--- src/test/java/baseball/baseballUnitTest.java
+++ src/test/java/baseball/baseballUnitTest.java
@@ -8,7 +8,6 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -22,7 +21,7 @@ public class baseballUnitTest {
         @Test
         @DisplayName("유저 입력값 숫자 2개")
         void 적은입력() {
-            assertThatThrownBy(() -> new InputException("12"))
+            assertThatThrownBy(() -> new Exception().Check("12"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자를 3개 입력하세요");
         }
@@ -30,7 +29,7 @@ public class baseballUnitTest {
         @Test
         @DisplayName("유저 입력값 숫자 4개")
         void 많은입력() {
-            assertThatThrownBy(() -> new InputException("1234"))
+            assertThatThrownBy(() -> new Exception().Check("1234"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자를 3개 입력하세요");
         }
@@ -38,7 +37,7 @@ public class baseballUnitTest {
         @Test
         @DisplayName("유저 입력값 형식 오류")
         void 문자입력() {
-            assertThatThrownBy(() -> new InputException("12a"))
+            assertThatThrownBy(() -> new Exception().Check("12a"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자만 입력하세요");
         }
@@ -46,7 +45,7 @@ public class baseballUnitTest {
         @Test
         @DisplayName("중복 숫자 입력")
         void 중복숫자() {
-            assertThatThrownBy(() -> new InputException("121"))
+            assertThatThrownBy(() -> new Exception().Check("121"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("중복된 숫자가 있습니다");
         }
@@ -54,7 +53,7 @@ public class baseballUnitTest {
         @Test
         @DisplayName("음수 입력")
         void 음수입력() {
-            assertThatThrownBy(() -> new InputException("-12"))
+            assertThatThrownBy(() -> new Exception().Check("-12"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("양수만 입력하세요");
         }
@@ -90,20 +89,17 @@ public class baseballUnitTest {
         @Test
         @DisplayName("공던지기 3스트라이크")
         void 공던지기case1() {
-            ArrayList<Integer> user = new ArrayList<>(Arrays.asList(1, 2, 3));
             ArrayList<Integer> computer = new ArrayList<>(Arrays.asList(1, 2, 3));
 
-            baseballGame.throwball(user, computer);
-            assertThat(baseballGame.getStrike()).isEqualTo(3);
+            assertThat(baseballGame.throwBall("123", computer)).isEqualTo(false);
         }
 
         @Test
         @DisplayName("공던지기 1스트라이크 1볼")
         void 공던지기case2() {
-            ArrayList<Integer> user = new ArrayList<>(Arrays.asList(1, 3, 4));
             ArrayList<Integer> computer = new ArrayList<>(Arrays.asList(1, 5, 3));
 
-            baseballGame.throwball(user, computer);
+            baseballGame.throwBall("134", computer);
             assertThat(baseballGame.getStrike()).isEqualTo(1);
             assertThat(baseballGame.getBall()).isEqualTo(1);
         }
@@ -114,12 +110,10 @@ public class baseballUnitTest {
         void 결과case1() {
             OutputStream out = new ByteArrayOutputStream();
             System.setOut(new PrintStream(out));
-            ArrayList<Integer> user = new ArrayList<>(Arrays.asList(1, 5, 4));
             ArrayList<Integer> computer = new ArrayList<>(Arrays.asList(1, 5, 4));
             final String message = 3 + "스트라이크\r\n";
 
-            baseballGame.throwball(user, computer);
-            baseballGame.printHint();
+            baseballGame.throwBall("154", computer);
             assertThat(message).isEqualTo(out.toString());
         }
 
