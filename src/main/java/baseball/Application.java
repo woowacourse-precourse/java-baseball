1748b2200c92d4015529916ea0a1c410f50bc3d2
miffyKing <whqudghk28@gmail.com>
2022-11-05 14:31:14 +0900

    feat: Initiates first game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,8 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("!!!!");
+        Game game = new Game();
+        game.startGame(0);
     }
 }
