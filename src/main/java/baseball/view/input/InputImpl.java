da888b5860db210f5a254b01be9270c76f74e088
5uhwann <5uhwann27@gmail.com>
2022-11-05 16:08:33 +0900

    feat(검증): 재시작/종료 입력 검증 구현
    재시작/종료 입력은 1 또는 2만 가능

--- src/main/java/baseball/view/input/InputImpl.java
+++ src/main/java/baseball/view/input/InputImpl.java
@@ -9,6 +9,8 @@ import java.util.stream.Collectors;
 public class InputImpl implements Input {
 
     private final String REGEX = "^[1-9]{3}$";
+    private final int NEW_GAGE = 1;
+    private final int QUIT = 2;
 
     @Override
     public String baseballInput() {
@@ -21,7 +23,19 @@ public class InputImpl implements Input {
 
     @Override
     public String endInput() {
-        return Console.readLine();
+        String endInput = Console.readLine();
+        if (isAllowedEndInput(endInput)) {
+            return endInput;
+        }
+        return null;
+    }
+
+    private boolean isAllowedEndInput(String endInput) {
+        int endInputNumber = Integer.parseInt(endInput);
+        if (endInputNumber == NEW_GAGE || endInputNumber == QUIT) {
+            return true;
+        }
+        throw new IllegalArgumentException("새로운 게임을 시작혀려면1, 종료하려면 2를 입력하세요.");
     }
 
     private boolean isAllowedBaseballInput(String baseballInput) throws IllegalArgumentException {
