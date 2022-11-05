34fcb21e22676a9d9a1620fef9c1babcf30b3608
miffyKing <whqudghk28@gmail.com>
2022-11-05 15:52:21 +0900

    style: Follow google java coding convention style

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,11 +2,10 @@ package baseball;
 
 public class Application {
 
-    private static final int STARTOFGAME = 0;
+  private static final int STARTOFGAME = 0;
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        Game game = new Game();
-        game.startGame(STARTOFGAME);
-    }
+  public static void main(String[] args) {
+    Game game = new Game();
+    game.startGame(STARTOFGAME);
+  }
 }
