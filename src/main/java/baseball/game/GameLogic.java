40c16497cff5f667f09cec3c796fcd0d918ede3f
math0917 <math0917@naver.com>
2022-11-05 17:08:33 +0900

    feat: 호출 사용자 list 생성 메써드
    사용자에게 valid한 list를 반환하는 책임을 부여한다

--- src/main/java/baseball/game/GameLogic.java
+++ src/main/java/baseball/game/GameLogic.java
@@ -2,10 +2,12 @@ package baseball.game;
 
 import baseball.constant.GameProcedureConstantString;
 import baseball.output.Print;
+import baseball.user.User;
 import java.util.List;
 
 public class GameLogic {
 
     public static void doGameLogicUntilGameFinish(List<Integer> computerRandomNonDuplicateThreeLengthIntegerList) {
+        User.createUserNonDuplicateIntegerList();
     }
 }
---
+++ src/main/java/baseball/user/User.java
@@ -0,0 +1,9 @@
+package baseball.user;
+
+import java.util.List;
+
+public class User {
+    public static List<Integer> createUserNonDuplicateIntegerList() {
+        return null;
+    }
+}
