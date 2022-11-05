daae9a0baac74dd948b1d39055e3835ebb481c2b
LEE-haeuk <rkwksmswhdk@naver.com>
2022-11-06 00:26:33 +0900

    feat: add the ending phrase when game finish

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -24,6 +24,8 @@ public class Application {
             result = application.computeResult(computer, player);
             application.printResult(result);
         } while (result.get(1) < 3);
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
     public List<Integer> createRandomNumbersInList() {
