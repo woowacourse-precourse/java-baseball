b646ab110117f6348d0f2ea1dd797a206adc2a43
1eejisoo <jisu991012@gmail.com>
2022-11-05 17:41:42 +0900

    Feat: 스트라이크 값을 체크하는 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,12 +1,14 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import domain.Number;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGame {
     private static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
+
     public List<Integer> input() {   // 입력
         System.out.print(INPUT_NUMBER);
         String inputNumber = Console.readLine();
@@ -21,5 +23,14 @@ public class BaseballGame {
             number.add(Integer.parseInt(String.valueOf(inputNumber.charAt(digit))));
         }
         return number;
+    }
+
+    public int judgeStrike(Number number, List<Integer> computerNumber,  int strike) {   // 스트라이크 판정
+        for (int digit = 0; digit < 3; digit++) {
+            if (computerNumber.get(digit).equals(number.getDigit(digit))) {
+                strike++;
+            }
+        }
+        return strike;
     }
 }
