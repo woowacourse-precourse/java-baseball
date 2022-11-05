8f2eb7ceeeb1b00eeb6d9465964db5552fa99e96
맹순영 <symaeng98@naver.com>
2022-11-05 13:16:24 +0900

    feat: 게임 시작 안내 문구 출력 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
 public class Application {
+    private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        start();
+    }
+
+    public static void start() {
+        System.out.println(START_SENTENCE);
     }
 }
