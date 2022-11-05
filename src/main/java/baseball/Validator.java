51edbfde27e12468ad79b93feb330006ab419e9c
JaeWon Choi <87649850+codesver@users.noreply.github.com>
2022-11-05 11:32:53 +0900

    feat(validateSize): validate size of predicted number

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -3,8 +3,14 @@ package baseball;
 public class Validator {
 
     public void validatePrediction(String predictedNumber) {
+        if (!validateSize(predictedNumber))
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
         // Validate size
         // Validate digits
         // If false throw IllegalArgumentException
+    }
+
+    private boolean validateSize(String predictedNumber) {
+        return predictedNumber.length() == 3;
     }
 }
