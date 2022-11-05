990b4d1c4ac56f3403785da289afd1686031a51d
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-05 10:42:21 +0900

    style : USER_WIN -> PLAYER_WIN 으로 상수명 변경

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -3,7 +3,7 @@ package baseball;
 public class Message {
     public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_NUM = "숫자를 입력해주세요 : ";
-    public static final String Player_WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String PLAYER_WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String ASK_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String END_GAME = "숫자 야구 게임을 종료합니다.";
     public static final String INPUT_ONE_OR_TWO = "잘못 입력하셨습니다. 1 또는 2를 입력해주세요.";
