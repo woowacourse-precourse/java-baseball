ac2bf57dadd0fd68bdce803bf1acab12c269a54e
sangwon <swchoi1997@naver.com>
2022-11-05 14:54:53 +0900

    test: HashSet 유사 정렬 확인
    컴퓨터의 랜덤공 번호가 무조건 오름차순으로 나오는 문제 발생, HashSet에 값을 넣을 때 넣은 키값, HashSet 테이블크기, 연산을 진행하여 테이블에 담는 문제로인해 유사 정렬처럼 생각이됨

--- src/test/java/baseball/utils/BallTest.java
+++ src/test/java/baseball/utils/BallTest.java
@@ -1,32 +1,41 @@
 package baseball.utils;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
-import baseball.utils.Ball;
-
 class BallTest {
-	Ball ball = Ball.getInstance();
-	List<String> range = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9");
+    Ball ball = Ball.getInstance();
+    List<String> range = List.of("1", "2", "3", "4", "5", "6", "7", "8", "9");
 
-	@Test
-	void 랜덤_공번호_생성() {
-		assertTrue(range.contains(ball.createRandomBall()));
-	}
+    @Test
+    void 랜덤_공번호_생성() {
+        assertTrue(range.contains(ball.createRandomBall()));
+    }
 
-	@Test
-	void 컴퓨터_공번호_3자리_확인() {
-		assertThat(ball.makeComputerBalls().size()).isEqualTo(3);
-	}
+    @Test
+    void 컴퓨터_공번호_3자리_확인() {
+        assertThat(ball.makeComputerBalls().size()).isEqualTo(3);
+    }
 
-	@Test
-	void 컴퓨터_공번호_중복숫자_판별(){
-		List<String> balls = ball.makeComputerBalls();
-		assertThat(new HashSet<>(balls).size()).isEqualTo(balls.size());
-	}
+    @Test
+    void 컴퓨터_공번호_중복숫자_판별() {
+        List<String> balls = ball.makeComputerBalls();
+        assertThat(new HashSet<>(balls).size()).isEqualTo(balls.size());
+    }
+
+    @Test
+    void HashSet_유사정렬_확인() {
+        Set<String> tmp = new HashSet<>();
+        tmp.add("2");
+        tmp.add("8");
+        tmp.add("1");
+        tmp.add("4");
+        assertThat(new ArrayList<>(tmp)).isEqualTo(List.of("1", "2", "4", "8"));
+    }
 }
