d5d3da302be34a479f6e3476318d3f2460ba9947
swonny <wonny921@gmail.com>
2022-11-05 23:56:46 +0900

    refactor(Computer): Application에 속하지 않는 클래스 파일 분리

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,2 +1,34 @@
-package baseball;public class Computer {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
+public class Computer {
+    final static int NUMBER_LENGTH = 3;
+    private ArrayList<Integer> randomNumbers = new ArrayList<>(NUMBER_LENGTH);
+
+    Computer() {
+        while (randomNumbers.size() < NUMBER_LENGTH) {
+            Integer newRandomNumber = pickRandomNumber();
+            if (!has(newRandomNumber)) {
+                randomNumbers.add(newRandomNumber);
+            }
+        }
+    }
+
+    public ArrayList<Integer> getRandomNumbers() {
+        return this.randomNumbers;
+    }
+
+    private boolean has(Integer number) {
+        if (this.randomNumbers.contains(number)) {
+            return true;
+        }
+        return false;
+    }
+
+    private static Integer pickRandomNumber() {
+        return Randoms.pickNumberInRange(1, 9);
+    }
 }
