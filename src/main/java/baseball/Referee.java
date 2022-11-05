44457f42ee4c61c2cac815ea72e411eedc989700
kim0914 <kosa0914@naver.com>
2022-11-05 13:55:15 +0900

    feat(Referee): 스트라이크, 볼 판정 기능 추가

---
+++ src/main/java/baseball/Referee.java
@@ -0,0 +1,54 @@
+package baseball;
+
+import constant.Message;
+
+public class Referee {
+    private int strikeCount;
+    private int ballCount;
+
+    public static final int INDEX_NOT_FOUND = -1;
+
+    public Referee() {
+        initCount();
+    }
+
+    public void initCount(){
+        strikeCount = 0;
+        ballCount = 0;
+    }
+
+    public void judge(String answer, String userNumber) {
+        setStrikeCount(answer, userNumber);
+        setBallCount(answer, userNumber);
+    }
+
+    private void setStrikeCount(String answer, String userNumber) {
+        int count = 0;
+
+        for (int index = 0; index < answer.length(); index++) {
+            if (isStrike(answer, userNumber, index)) {
+                count++;
+            }
+        }
+        strikeCount = count;
+    }
+
+    private void setBallCount(String answer, String userNumber) {
+        int count = 0;
+
+        for (int index = 0; index < userNumber.length(); index++) {
+            if (isBall(answer.indexOf(userNumber.charAt(index)), index)) {
+                count++;
+            }
+        }
+        ballCount = count;
+    }
+
+    private boolean isStrike(String answer, String userNumber, int i) {
+        return answer.charAt(i) == userNumber.charAt(i);
+    }
+
+    private Boolean isBall(int findIndexWithUserChar, int userNumberCharIndex) {
+        return (findIndexWithUserChar != INDEX_NOT_FOUND && findIndexWithUserChar != userNumberCharIndex);
+    }
+}
