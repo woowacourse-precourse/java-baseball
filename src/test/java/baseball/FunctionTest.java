5595b045bb5fb3cebb19d06c6ba6b67a48d0df8a
jthugg <jthug@naver.com>
2022-11-05 18:46:25 +0900

    Style: 스타일가이드 준수

--- src/test/java/baseball/FunctionTest.java
+++ src/test/java/baseball/FunctionTest.java
@@ -4,20 +4,21 @@ import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class FunctionTest {
+
 	@Test						/* 컴퓨터 숫자 생성 테스트 */
 	void randomGeneratingTest() {
 		List<Integer> generatedNumbers = Application.setComputersNumbers();
 
 		assertThat(generatedNumbers.size() == 3);
-		for(int number : generatedNumbers) {
+		for (int number : generatedNumbers) {
 			assertThat(number > 0 && number < 10);
 		}
 	}
+
 	@Test						/* 유저 인풋/유효성 검사 테스트 */
 	void userInputTest() {
 		assertThatThrownBy(() -> Application.validateInputPattern("as"))
