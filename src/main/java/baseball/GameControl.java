a240fa33cd113058684b9280aff47169980be488
jjdone <ehdlfaos@gmail.com>
2022-11-05 15:17:02 +0900

    feat: GameControl 클래스 상세구현2
    예외 처리를 제외한 기능 구현 완료
    몇몇 메소드 수정
    isRestart: 게임 재시작 or 종료
    isThreeStrike: 3스트라이크인지 확인 후 isRestart로 이동 그리고 score 리셋

--- src/main/java/baseball/GameControl.java
+++ src/main/java/baseball/GameControl.java
@@ -1,15 +1,19 @@
 package baseball;
 
 import java.util.*;
+import camp.nextstep.edu.missionutils.Console;
 
 public class GameControl {
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final int NUMBER_SIZE = 3;
+    private static final String STOP_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String Ball_MESSAGE = "볼";
     private static final String STRIKE_MESSAGE = "스트라이크";
     private static final String NOTHING = "낫싱";
+    private static final String GAME_OVER_CODE = "2";
     private static final boolean PLAYING = true;
     private static final boolean GAMEOVER = false;
+    private static final int NUMBER_COUNT = 3;
 
     private List<Integer> computer;
     private List<Integer> player;
@@ -28,11 +32,13 @@ public class GameControl {
 
         computerNumber.setNumbers();
         computer = computerNumber.getNumbers();
-
+        
         while(isGameOver) {
             playerNumber.setNumbers();
             player = playerNumber.getNumbers();
             scoreCalculation(player);
+            printScore();
+            isThreeStrike(computerNumber);
         }
     }
 
@@ -41,32 +47,56 @@ public class GameControl {
     }
 
     public void scoreCalculation(List<Integer> player) {
-        for(int i=0; i<NUMBER_SIZE; i++){
-            if(player.get(i) == computer.get(i)){
-                strike++;
-            }
-            if(computer.contains(player.get(i))){
-                strikePlusBall++;
-            }
-        ball = strikePlusBall - strike;
+        for(int i = 0; i< NUMBER_COUNT; i++) {
+          if(player.get(i) == computer.get(i)) {
+              strike++;
+          }
+          if(computer.contains(player.get(i))) {
+              strikePlusBall++;
+          }
+            ball = strikePlusBall - strike;
         }
     }
 
-    public void printScoreMessage() {
-        if(ball != 0){
+    public void printScore() {
+        if(ball != 0) {
             System.out.print(ball + Ball_MESSAGE + " ");
         }
-        if(strike != 0){
-            System.out.println(strike + STRIKE_MESSAGE);
+        if(strike != 0) {
+            System.out.print(strike + STRIKE_MESSAGE);
         }
-        if(ball == 0 && strike == 0){
-            System.out.println(NOTHING);
+        if(ball == 0 && strike == 0) {
+            System.out.print(NOTHING);
         }
+        System.out.println();
     }
 
     public void resetScore() {
         strike = 0;
         ball = 0;
         strikePlusBall = 0;
+    }
+
+    public void gameStop() {
+        System.out.println(STOP_MESSAGE);
+        System.out.println(RESTART_MESSAGE);
+    }
+
+    public void isRestart(Computer computerNumber) {
+        String restartCode = Console.readLine();
+        if(restartCode.equals(GAME_OVER_CODE)) {
+            isGameOver = GAMEOVER;
+        } else {
+            computerNumber.setNumbers();
+            computer = computerNumber.getNumbers();
+        }
+    }
+
+    public void isThreeStrike(Computer computerNumber) {
+        if(strike == NUMBER_COUNT) {
+            gameStop();
+            isRestart(computerNumber);
+            resetScore();
+        }
     }
 }
