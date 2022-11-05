5824beda5a7ea9e39f1dff9182f98870e1e11cfe
맹순영 <symaeng98@naver.com>
2022-11-05 13:41:29 +0900

    feat: 숫자 입력 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -7,10 +8,12 @@ import java.util.List;
 
 public class Application {
     private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_SENTENCE = "숫자를 입력해주세요 : ";
 
     public static void main(String[] args) {
         System.out.println(START_SENTENCE);
         List<Integer> answer = generateRandomNumber();
+        String inputNum = input();
     }
 
     public static List<Integer> generateRandomNumber() {
@@ -22,5 +25,10 @@ public class Application {
             }
         }
         return computer;
+    }
+
+    public static String input() {
+        System.out.print(INPUT_SENTENCE);
+        return Console.readLine();
     }
 }
