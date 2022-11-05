b74f2460ea66b2bbf204575a4ad0cbf33d950856
tdmdgh <tdmdgh@gmail.com>
2022-11-05 17:57:32 +0800

    feat: add input exception case - duplicate integer of input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Application {
@@ -17,10 +18,11 @@ public class Application {
         }
         return newList;
     }
-//    private static boolean isNotDuplicate(String str){
-//
-//        return
-//    }
+    private static boolean isNotDuplicate(String str){
+        List<Integer> numList = stringToIntegerList(str);
+        Set<Integer> numSet = new HashSet<>(numList);
+        return numSet.size() == numList.size();
+    }
     private static boolean isValidSize(String str){
         return str.length()==3;
     }
@@ -28,10 +30,10 @@ public class Application {
         return str.matches("[1-9.]+");
     }
     public static boolean isValidInput(String input){
-        //정규 표현식을 이용해 Input이 숫자가 아닌 모든 경우와 Input의 범위가 1~9사이가 아닌 경우를 동시에 처리
         if(!isString_in_1to9(input)) return false;
+        //정규 표현식을 이용해 Input이 숫자가 아닌 모든 경우와 Input의 범위가 1~9사이가 아닌 경우를 동시에 처리
         if(!isValidSize(input)) return false;
-//        if(!isNotDuplicate(input)) return false;
+        if(!isNotDuplicate(input)) return false;
         return true;
     }
     public static List<Integer> createTargetNums(){
