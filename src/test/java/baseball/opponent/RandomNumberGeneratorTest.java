be82c88edffdd3a71388aef9a4f8111cf96d69ab
wlsh44 <k87913j@gmail.com>
2022-11-05 16:47:40 +0900

    test: 랜덤 번호 생성 테스트 코드 추가

---
+++ src/test/java/baseball/opponent/RandomNumberGeneratorTest.java
@@ -0,0 +1,31 @@
+package baseball.opponent;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomNumberGeneratorTest {
+
+    @Test
+    @DisplayName("generate 테스트")
+    void test() {
+        //given
+        int expectedMinNum = 1;
+        int expectedMaxNum = 9;
+        int expectedSize = 3;
+
+        //when
+        List<Integer> res = RandomNumberGenerator.generate();
+
+        //then
+        assertThat(res.size()).isEqualTo(expectedSize);
+        for (Integer num : res) {
+            assertThat(num).isGreaterThanOrEqualTo(expectedMinNum);
+            assertThat(num).isLessThanOrEqualTo(expectedMaxNum);
+        }
+    }
+
+}
