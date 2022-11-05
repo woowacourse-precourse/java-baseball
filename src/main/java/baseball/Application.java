88e2b0ca68d0d141589f40718294d048be005205
syahn <496300@naver.com>
2022-11-06 01:24:54 +0900

    FEAT(Game) : 게임 시작 메시지를 출력하는 메소드 추가
    게임 시작 시 게임 시작을 알리는 메시지를 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -23,6 +23,10 @@ class Game{
         return computerNum;
     }
 
+    void start(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     void setUserInput() {
         userInput = Console.readLine();
         checkUserInput();
