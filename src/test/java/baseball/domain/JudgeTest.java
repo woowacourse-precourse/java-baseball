91cae48a9d963fa615c5b2bfc3b2f12d48f211b6
Jin409 <sualng123@gmail.com>
2022-11-05 17:06:44 +0900

    test(JudgeTest): 정답 여부를 판단하는 Judge 클래스 테스트코드 추가
    - Ball 횟수를 세는 기능 테스트
    - Strike 횟수를 세는 기능 테스트
    - 답인지 아닌지 확인하는 기능 테스트

---
+++ src/test/java/baseball/domain/JudgeTest.java
@@ -0,0 +1,54 @@
+package baseball.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class JudgeTest {
+    @Test
+    void 알맞는_볼_횟수_반환() {
+        //given
+        List<Integer> tempNums = List.of(1, 2, 3);
+        Answer testAnswer = new Answer(tempNums);
+        Judge testJudge = new Judge(testAnswer);
+
+        //when
+        List<Integer> testUserInput = List.of(1, 3, 5); // 1스트라이크 1볼
+        int ballCount = testJudge.getBallCount(testUserInput);
+
+        //then
+        assertThat(ballCount).isEqualTo(1);
+    }
+
+    @Test
+    void 알맞는_스트라이크_횟수_반환() {
+        //given
+        List<Integer> tempNums = List.of(1, 2, 3);
+        Answer testAnswer = new Answer(tempNums);
+        Judge testJudge = new Judge(testAnswer);
+
+        //when
+        List<Integer> testUserInput = List.of(1, 3, 5); // 1스트라이크 1볼
+        int strikeCount = testJudge.getStrikeCount(testUserInput);
+
+        //then
+        assertThat(strikeCount).isEqualTo(1);
+    }
+
+    @Test
+    void 알맞는_정답_여부_반환() {
+        //given
+        List<Integer> tempNums = List.of(1, 2, 3);
+        Answer testAnswer = new Answer(tempNums);
+        Judge testJudge = new Judge(testAnswer);
+
+        //when
+        List<Integer> testUserInput = List.of(1, 3, 5); // 1스트라이크 1볼
+        int strikeCount = testJudge.getStrikeCount(testUserInput);
+        boolean correct = testJudge.isCorrect(strikeCount);
+
+        //then
+        assertThat(correct).isEqualTo(false);
+    }
+}
