b4a31b9c25b1fe694e7c273e8656056e222565fd
cmong0516 <cmong0516@gmail.com>
2022-11-05 15:19:48 +0900

    feat(UserClass): validUserInput 메서드 작성.
    입력한 유저 input 값이 올바르지 않은 경우 exception 을 발생시키는 메서드.

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,4 +1,30 @@
 package baseball;
 
 public class User {
+
+
+    public String validUserInput(String userInput) {
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException("숫자는 세자리 수만 입력 가능합니다.");
+        }
+
+        for (int i = 0; i < userInput.length(); i++) {
+            if (userInput.charAt(i) == ' ') {
+                throw new IllegalArgumentException("공백은 입력할수 없습니다.");
+            }
+
+            if (userInput.charAt(i) == '0') {
+                throw new IllegalArgumentException("0은 입력할수 없습니다.");
+            }
+        }
+
+        for (int i = 1; i < userInput.length(); i++) {
+            if (userInput.charAt(i - 1) == userInput.charAt(i)) {
+                throw new IllegalArgumentException("이미 입력한 숫자는 입력할수 없습니다.");
+            }
+        }
+
+        return userInput;
+    }
+
 }
