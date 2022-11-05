b910ed9a19c6ca55db309a24a596bcf9bcf9e756
JerryK026 <kskyung0624@gmail.com>
2022-11-05 22:40:09 +0900

    Test(Game) : initAnswer 중복 검사 테스트
    initAnswer 했을 때 answer이 중복 값을 가지고 있는지 검사한다. 랜덤 값이기 때문에 검증을 위해 100회 검사한다.

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,4 +17,17 @@ public class GameTest {
         assertThat(answer).hasSize(3);
     }
 
+    @Test
+    void initAnswer_중복숫자가_없어야_함() {
+        for (int i = 0; i < 100; i++) {
+            Game game = new Game();
+            game.initAnswer();
+            List<Integer> answer = game.getAnswer();
+            assertThat(isUnique(answer)).isTrue();
+        }
+    }
+
+    private boolean isUnique(List<Integer> answer) {
+        return answer.size() == new HashSet<>(answer).size();
+    }
 }
