472e9e1e658263ae2359e94f185c02a1f5abd198
watterricecake <fruturum@nate.com>
2022-11-05 22:44:59 +0900

    feat($game): create field about user, computer, score (userNumber/computerNumber/ball/strike)
    Added new field to $game:
    - list of userNumbers
    - list of computerNumbers
    - integer of ball
    - integer of strike

---
+++ src/main/java/baseball/game/Game.java
@@ -0,0 +1,16 @@
+package baseball.game;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    public static List<Integer> userNumbers;
+    public static List<Integer> computerNumbers;
+
+    public static Integer ball;
+    public static Integer strike;
+
+
+}
