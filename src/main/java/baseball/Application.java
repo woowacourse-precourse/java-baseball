7e1df97b46df92681a2af88ff52631781430ea0b
박승민 <tmddudals369@naver.com>
2022-11-05 20:39:12 +0900

    feat: 상대방(컴퓨터)의 수 생성 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class Application {
 
     private static final String GAME_START_SIGN = "숫자 야구 게임을 시작합니다.";
+    private static final int START_VALUE = 1;
+    private static final int END_VALUE = 9;
+    private static final int NUMBER_COUNT = 3;
+
     public static void main(String[] args) {
         notifyGameStart();
+        List<Integer> computerNumbers = createComputerNumbers();
+    }
+
+    public static List<Integer> createComputerNumbers() {
+        return Randoms.pickUniqueNumbersInRange(START_VALUE, END_VALUE, NUMBER_COUNT);
     }
 
     public static void notifyGameStart() {
