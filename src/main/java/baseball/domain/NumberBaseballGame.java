3e1730d14c83b771d0e53539b56e04ff6e368c84
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-05 15:50:24 +0900

    feat<NumberBaseballGame>: play 함수 구현
    - 점수 결과로 메세지 출력
    - 3스트라이크라면 게임을 계속 이어갈지 input을 받는다.
    - input에 대해 검증
    - 1을 입력받으면 init 함수 호출
    - 2를 입력받으면 isPlaying false로 while문에서 빠져나가 게임 종료

--- src/main/java/baseball/domain/NumberBaseballGame.java
+++ src/main/java/baseball/domain/NumberBaseballGame.java
@@ -1,6 +1,7 @@
 package baseball.domain;
 
 import baseball.dto.ScoreResult;
+import baseball.dto.ScoreResultType;
 import baseball.util.GameExceptionMessage;
 import baseball.util.ShowGameMessage;
 import camp.nextstep.edu.missionutils.Console;
@@ -22,7 +23,15 @@ public class NumberBaseballGame {
         init();
         while (scoreBoard.isPlaying()) {
             showGameMessage.inputUserNumberMessage();
-            ScoreResult result = scoreBoard.getScore(getUserNumberBall());
+            ScoreResult result = scoreBoard.getScoreResult(getUserNumberBall());
+            showGameMessage.gameResultMessage(result);
+            if (result.getType() == ScoreResultType.ALL_STRIKE) {
+                if (continueGame(getUserInputString())) {
+                    init();
+                } else {
+                    scoreBoard.setIsPlaying();
+                }
+            }
         }
     }
 
@@ -37,6 +46,22 @@ public class NumberBaseballGame {
 
     private String getUserInputString() {
         return Console.readLine();
+    }
+
+    private boolean continueGame(String userInput) {
+        validateContinueUserInput(userInput);
+        if (userInput.charAt(0) == '1') {
+            return true;
+        }
+        return false;
+    }
+
+    private void validateContinueUserInput(String userInput) {
+        if (userInput.length() > 1) {
+            throw new IllegalArgumentException("길이 1초과");
+        } else if (!(userInput.charAt(0) == '1' || userInput.charAt(0) == '2')) {
+            throw new IllegalArgumentException("1혹은 2만 입력");
+        }
     }
 
     private void validate(String userInput) {
