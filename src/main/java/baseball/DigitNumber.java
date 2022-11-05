3c6587a00a093ab5a406d0b94a6a63bc38ee5ba3
DaWoon Lee <qkgk61@gmail.com>
2022-11-06 00:52:52 +0900

    Feat : DigitNumber 클래스를 싱글톤으로 변환
    게임을 세 자리의 수로 진행하지만 실제로는 플레이어가 지정할 수도 있기 때문에 특정 자리수를 외부에서 받아야 한다. 따라서 인자를 받는 싱글톤이어야 하므로 enum과 enum 빌더를 이용하여 싱글톤으로 정의하였다.

--- src/main/java/baseball/DigitNumber.java
+++ src/main/java/baseball/DigitNumber.java
@@ -4,12 +4,42 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashMap;
 import java.util.Map;
 
-public class DigitNumber {
+public enum DigitNumber {
 
-    public Map<Integer, Integer> generate3DigitNumberWithoutDuplication() {
+    INSTANCE;
+
+    private int digit;
+
+    DigitNumber() {
+
+    }
+
+    private void build(DigitNumberBuilder builder) {
+        this.digit = builder.digit;
+    }
+
+    public static DigitNumber getInstance() {
+        return INSTANCE;
+    }
+
+    private static class DigitNumberBuilder {
+
+        private final int digit;
+
+        private DigitNumberBuilder(int digit) {
+            this.digit = digit;
+        }
+
+        public void build() {
+            DigitNumber.INSTANCE.build(this);
+        }
+
+    }
+
+    public Map<Integer, Integer> generateDigitNumberWithoutDuplication() {
         Map<Integer, Integer> computerNumber = new HashMap<>();
 
-        for (int index = 0; index < 3; ) {
+        for (int index = 0; index < digit; ) {
             int picked = Randoms.pickNumberInRange(1, 9);
 
             if (computerNumber.containsKey(picked)) {
