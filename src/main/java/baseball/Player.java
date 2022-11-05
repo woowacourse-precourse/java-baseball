6eecf3c439098f61293f3f6e03d682010cb72907
김태용 <amico741@naver.com>
2022-11-05 16:59:49 +0900

    feat: 사용자 숫자 입력 메서드 생성
    서로 다른 숫자인지, 입력 값이 모두 숫자인지 검사하고 반환하는 메서드

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,5 +1,40 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.regex.Pattern;
+
 public class Player {
-  public Player(){};
+  private final ArrayList<Integer> numbers;
+  private static final int MAX_SIZE = 3;
+  private boolean progress;
+  public Player(boolean progress){
+    this.numbers = new ArrayList<>();
+    this.progress = progress;
+  };
+
+  public ArrayList<Integer> decideNumbers(){
+    String input = Console.readLine();
+    String pattern = "^[0-9]*$";
+    boolean result = Pattern.matches(pattern, input);
+
+    if(!result) {
+      throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+    }
+    addNumber(input);
+    if(this.numbers.size() != MAX_SIZE){
+      throw new IllegalArgumentException("서로 다른 3개의 숫자를 입력해야 합니다.");
+    }
+    return this.numbers;
+  }
+
+  public void addNumber(String input){
+    for(int i = 0; i < input.length(); i++){
+      int number = Integer.parseInt(String.valueOf(input.charAt(i)));
+      if(!this.numbers.contains(number)){
+        this.numbers.add(number);
+      }
+    }
+  }
 }
