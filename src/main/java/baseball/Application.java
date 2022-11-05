04376d7feb92933e7efeb2e7f1b2aece65fc3933
miffyKing <whqudghk28@gmail.com>
2022-11-05 15:41:14 +0900

    fix: Make variable constants

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,12 @@
 package baseball;
 
 public class Application {
+
+    private static final int STARTOFGAME = 0;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("!!!!");
         Game game = new Game();
-        game.startGame(0);
+        game.startGame(STARTOFGAME);
     }
 }
