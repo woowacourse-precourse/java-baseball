0234e19d00ea24b19a5dd91ebab33632683bbf97
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-05 13:41:08 +0900

    feat(application) : 게임 시작 메서드 호출 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,13 +2,15 @@ package baseball;
 
 public class Application {
 
-    static void printStartMessage(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
+	static void printStartMessage() {
+		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        printStartMessage();
+		printStartMessage();
 
-    }
+		Game game = new Game();
+		game.playGame();
+	}
 }
