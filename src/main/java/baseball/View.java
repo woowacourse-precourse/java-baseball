72c03b8eb781869321f61c8610a452779528d4d6
BuildTools <unconfigured@null.spigotmc.org>
2022-11-05 19:59:13 +0900

    refactor: 수정 getUserInput
    getUserInput의 읽는방식을 nextLine -> readLine 으로 변경

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,9 +1,13 @@
 package baseball;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class View {
-    static Scanner scanner = new Scanner(System.in);
+    static InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+    static BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
     static final void printStartGame(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
@@ -12,7 +16,11 @@ public class View {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    static final String getUserInput(){
-        return scanner.nextLine();
+    static final String getUserInput() {
+        try {
+            return bufferedReader.readLine();
+        } catch (IOException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
