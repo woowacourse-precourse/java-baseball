2356feb7acab48110289b2da60bc981c44c9050c
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-05 17:40:33 +0900

    feat: 리드미에 따른 GameController 메서드 생성

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,65 @@
+package baseball.controller;
+
+import baseball.model.Computer;
+import baseball.model.Player;
+import baseball.service.GameService;
+import baseball.service.GameStatusService;
+import baseball.view.GameStatusView;
+import baseball.view.ScoreResultView;
+
+public class GameController {
+    private Player player;
+    private Computer computer;
+    private GameService gameService;
+    private GameStatusService gameStatusService;
+    private GameStatusView gameStatusView;
+    private ScoreResultView scoreResultView;
+
+    private void initializeGame(){
+
+    }
+
+    private void generateComputer(){
+
+    }
+
+    private void generateGameStatus(){
+
+    }
+
+    private void startGame(){
+
+    }
+
+    private void clearScore(){
+
+    }
+
+    private void getPlayerNumbers(){
+
+    }
+
+    private void matchGame(){
+
+    }
+
+    private void showPlayersScore(){
+
+    }
+
+    private boolean isThreeStrike(){
+        return true;
+    }
+
+    private void whatToDoNext(){
+
+    }
+
+    private String getPlayersOpinion(){
+        return "";
+    }
+
+    private void setGameStatus(String opinion){
+
+    }
+}
