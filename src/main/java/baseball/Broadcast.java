0c5e67a8a76dd6e999734af553d58a02fa78276b
junpakPark <112045553+junpakPark@users.noreply.github.com>
2022-11-05 09:38:14 +0900

    refactor(Broadcast): printEndMessage 외 1개 메서드에 파라미터 추가
    주요 변경사항:
    - printEndMessage에 파라미터 추가하여 추후에 숫자야구의 자리수가 달라져도 대응가능하게 변경함
    - printRestartMessage에 파라미터 추가하여 추후에 게임 재시작/종료 인풋값이 달라져도 대응가능하게 변경함

--- src/main/java/baseball/Broadcast.java
+++ src/main/java/baseball/Broadcast.java
@@ -3,8 +3,8 @@ package baseball;
 public class Broadcast {
 	private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 	private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-	private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-	private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	private static final String END_MESSAGE = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.";
 
 	static void printStartMessage() {
 		System.out.println(START_MESSAGE);
@@ -18,11 +18,11 @@ public class Broadcast {
 		System.out.println(resultMessage);
 	}
 
-	static void printEndMessage() {
-		System.out.println(END_MESSAGE);
+	static void printEndMessage(int PLAYING_NUMBER_SIZE) {
+		System.out.printf((END_MESSAGE) + "%n", PLAYING_NUMBER_SIZE);
 	}
 
-	static void printRestartMessage() {
-		System.out.println(RESTART_MESSAGE);
+	static void printRestartMessage(int START_INPUT, int END_INPUT) {
+		System.out.printf((RESTART_MESSAGE) + "%n", START_INPUT, END_INPUT);
 	}
 }
