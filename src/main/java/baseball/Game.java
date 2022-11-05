f0d7a1372b1889bb96bbb702a6c9102bc7a2b284
juhee park <jouhi3322@gmail.com>
2022-11-06 02:57:41 +0900

    feat: 유저의 점수를 계산하고 출력한다.
    같은위치에 있으면 스트라이크
    숫자를 포함하고 있으면 볼을 출력한다.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -9,9 +9,36 @@ public class Game {
         System.out.println(START_MSG);
         //테스트용 출력
         System.out.println(computer);
+
         user.scanUserNums();
         //테스트용 출력
         System.out.println(user);
+        countResult();
+
+    }
+
+    private void countResult() {
+        int ballCnt = 0;
+        int strikeCnt =0;
+
+        for(int i=0;i<3;i++){
+            int cpNum = computer.getNums().get(i);
+            int userNum = user.getNums().get(i);
+
+            if(cpNum==userNum) strikeCnt++;
+            else if(user.getNums().contains(cpNum))ballCnt++;
+        }
+
+        System.out.println(showResult(ballCnt,strikeCnt));
+    }
+
+    private String showResult(int ballCnt, int strikeCnt) {
+        StringBuilder result = new StringBuilder();
+        if(ballCnt==0 && strikeCnt==0) return result.append("낫싱").toString();
+
+        if(ballCnt>0) result.append(ballCnt).append("볼 ");
+        if(strikeCnt>0) result.append(strikeCnt).append("스트라이크 ");
+        return result.toString().trim();
     }
 
 
