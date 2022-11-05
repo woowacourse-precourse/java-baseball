cf44e39778b63bf96da9cb2b4c775e0df88c5bd5
SHEOMM <esh0821@snu.ac.kr>
2022-11-06 01:00:17 +0900

    feat(HintView): add setter
    Hint attribute의 Setter 추가

--- src/main/java/baseball/UI/HintView.java
+++ src/main/java/baseball/UI/HintView.java
@@ -5,10 +5,12 @@ import baseball.model.Hint;
 public class HintView {
     private Hint hint;
 
-    public Hint getHint() {
-        return hint;
+    public void setHint(Hint hint) {
+        this.hint = hint;
     }
+
     public void printHint(){
         System.out.println(hint);
     }
+
 }
