6ead5df40b2aa2e65bce1788bf09b6c9fe369b0d
d0ri123 <lsh81550@gmail.com>
2022-11-05 18:20:17 +0900

    Feat: 3자리 숫자를 입력하는 기능 구현

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,10 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class User {
     int response;
-    int result;
+    boolean result;
+    boolean oneMore=true;
 
-    public void setResponse(int response) { //숫자를 입력하는 기능
-        this.response = response;
+    public void request(){
+        System.out.println("숫자를 입력해주세요 : ");
+        response = Integer.parseInt(Console.readLine());
+    }
+
+    public void pass(String answer){
+        if(answer.equals("3스트라이크")){
+            result = true;
+        }else {
+            System.out.println("숫자를 입력해주세요 : ");
+            response = Integer.parseInt(Console.readLine());
+            result = false;
+        }
     }
 }
