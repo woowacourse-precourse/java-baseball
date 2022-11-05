b5e2901832d0c6a85d1bb2cccbb28c17b7095ccd
nn98 <jkllhgb@gmail.com>
2022-11-05 10:28:33 +0900

    Feat: 게임 승리 메시지 추가, 메소드 구조 변경에 따른 상수명 변경

--- src/main/java/constant/Texts.java
+++ src/main/java/constant/Texts.java
@@ -5,6 +5,7 @@ public class Texts {
     public static final String GAME_BEGIN = "숫자 야구 게임을 시작합니다.";
     public static final String GAME_END = "숫자 야구 게임을 종료합니다.";
     public static final String GAME_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String GAME_PLAY_WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static final String USER_INPUT_NUMBER = "숫자를 입력해주세요 : ";
     public static final String USER_INPUT_ILLEGAL = "잘못된 값을 입력했습니다! "+GAME_END;
@@ -12,8 +13,8 @@ public class Texts {
     public static final String USER_INPUT_ILLEGAL_DIGIT = "입력된 수가 3자리가 아닙니다. ";
     public static final String USER_INPUT_ILLEGAL_DUPLICATED = "입력된 수에 중복값이 존재합니다. ";
 
-    public static final String RUN_CHECK_STRIKE = "스트라이크";
-    public static final String RUN_CHECK_BALL = "볼";
-    public static final String RUN_CHECK_NOTTING = "낫싱";
+    public static final String CHECKER_RESULT_BALL = "볼";
+    public static final String CHECKER_RESULT_STRIKE = "스트라이크";
+    public static final String CHECKER_RESULT_NOTTING = "낫싱";
 
 }
