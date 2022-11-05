02ca5c4e9c96c85414b3079d93c58728ef53c537
wlsh44 <k87913j@gmail.com>
2022-11-05 22:20:44 +0900

    feat: 숫자 맞추기 기능 추가

---
+++ src/main/java/baseball/dto/BallStrikeDto.java
@@ -0,0 +1,20 @@
+package baseball.dto;
+
+public class BallStrikeDto {
+
+    private final int ballCount;
+    private final int strikeCount;
+
+    public BallStrikeDto(int ballCount, int strikeCount) {
+        this.ballCount = ballCount;
+        this.strikeCount = strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+}
