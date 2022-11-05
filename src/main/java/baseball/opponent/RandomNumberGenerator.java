d54edfc4a19d1772142ddddca57713845fc0bfb8
wlsh44 <k87913j@gmail.com>
2022-11-05 16:43:50 +0900

    feat: 랜덤 숫자 생성 클래스 추가

---
+++ src/main/java/baseball/opponent/RandomNumberGenerator.java
@@ -0,0 +1,16 @@
+package baseball.opponent;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class RandomNumberGenerator {
+
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+    private static final int COUNT = 3;
+
+    public static List<Integer> generate() {
+        return Randoms.pickUniqueNumbersInRange(START_INCLUSIVE, END_INCLUSIVE, COUNT);
+    }
+}
