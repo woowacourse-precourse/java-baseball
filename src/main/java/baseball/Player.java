fa8588685fa406e7f297d126e5b4a496934ebc03
SuhHyeonjun <dmsgktn0208@gmail.com>
2022-11-05 10:41:46 +0900

    style : User -> Player 변수, 메서드 이름 변경

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -7,15 +7,15 @@ import java.util.List;
 
 public class Player {
 
-    public static List<Integer> toIntegerUserInput(String userInput) {
-        if (userInput.isEmpty()) {
+    public static List<Integer> toIntegerPlayerInput(String PlayerInput) {
+        if (PlayerInput.isEmpty()) {
             throw new IllegalArgumentException("값을 입력하지 않았습니다. 3자리 숫자를 입력해주세요.");
         }
-        List<Integer> userInputNumber = new ArrayList<>(NUM_LIMIT_LENGTH);
-        String[] userInputArr = userInput.split("");
-        for (String str : userInputArr) {
-            userInputNumber.add(Integer.parseInt(str));
+        List<Integer> PlayerInputNumber = new ArrayList<>(NUM_LIMIT_LENGTH);
+        String[] PlayerInputArr = PlayerInput.split("");
+        for (String str : PlayerInputArr) {
+            PlayerInputNumber.add(Integer.parseInt(str));
         }
-        return userInputNumber;
+        return PlayerInputNumber;
     }
 }
