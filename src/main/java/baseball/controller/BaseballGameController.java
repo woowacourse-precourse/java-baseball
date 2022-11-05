093d0c8fed365d17610766a557e894574810f6d5
qdeu22 <qdeu22@gmail.com>
2022-11-05 13:16:54 +0900

    refactor(BaseballGameController): startGame 함수 내 코드 수정

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -1,5 +1,9 @@
 package baseball.controller;
 
+import baseball.model.ComputerRandomNumber;
+import baseball.model.UserBaseballNumber;
+import baseball.view.InputView;
+
 public class BaseballGameController {
     private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
 
@@ -9,5 +13,9 @@ public class BaseballGameController {
     }
 
     private static void startGame() {
+        ComputerRandomNumber computerRandomNumber = new ComputerRandomNumber();
+        for (int i = 0; i < 3; i++) {
+          UserBaseballNumber userBaseballNumber = new UserBaseballNumber(InputView.inputNumber());
+        }
     }
 }
