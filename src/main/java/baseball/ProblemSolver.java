003d624ab805223a503ffef4e618321c7603dde7
YE-EUN <musiclove529@naver.com>
2022-11-05 12:02:49 +0900

    feat : ProblemSolver 클래스 생성
    - 문제를 푸는 Player 구체 클래스 생성

---
+++ src/main/java/baseball/ProblemSolver.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class ProblemSolver extends Player{
+    @Override
+    public void setNumbers(int sizeOfNumbers) {
+
+    }
+
+    public int chooseToRestart(){
+        int answer = 0;
+        return answer;
+    }
+}
