532cd8c05c531b65c0d1f3f8b11350aa39ac814d
KIMSEI1124 <workju1124@gmail.com>
2022-11-05 11:59:22 +0900

    test: 제공하는 라이브러리 테스트 생성

---
+++ src/test/java/baseball/LibraryTest.java
@@ -0,0 +1,15 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LibraryTest {
+    @Test
+    void randoms() {
+        int randomNumber = Randoms.pickNumberInRange(1, 9);
+        System.out.println("randomNumber = " + randomNumber);
+        Assertions.assertThat(randomNumber).isGreaterThan(1).isLessThan(9);
+    }
+}
