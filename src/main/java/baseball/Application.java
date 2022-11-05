c00dbd794b0a589927e457bd6d273cdbb83bf299
pjw03659 <pjw03659@naver.com>
2022-11-05 17:06:42 +0900

    random 3digit creating function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,20 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+
 public class Application {
-    public static int[] makenumber(){
-        int[] randomNumber = new int[3];
-        for(int i=0;i<randomNumber.length;i++){
-            randomNumber[i]=(int) (Math.random()*10);
-            for(int j=0;j<i;j++){
-                if(randomNumber[i]==randomNumber[j]){
-                    i--;
-                }
+    public static List<Integer> makenumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
             }
         }
-        return randomNumber;
+        return computer;
     }
     public static void main(String[] args) {
-        int answer[]= new int[3];
+        List<Integer> computer = new ArrayList<>();
+        computer = makenumber();
+        
 
 
         }
