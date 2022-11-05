8b3dfe62d46df48b16e2222c0f5ce9001b306364
dohyun <ldh19738@gmail.com>
2022-11-05 18:58:32 +0900

    feat(BaseballGame): Add fuction to find nothing

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import java.util.List;
+
 public class BaseballGame {
     public void start() {
         System.out.println(GameMessage.start);
+    }
+
+    public void isNothing(List<Integer> computer, List<Integer> user) {
+        if (!computer.contains(user)) {
+            System.out.println(GameMessage.nothing);
+        }
     }
 }
