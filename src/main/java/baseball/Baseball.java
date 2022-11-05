e78e8d545ac44da4d959a37cf314a78ad26a36ee
d0ri123 <lsh81550@gmail.com>
2022-11-05 18:29:26 +0900

    Feat: 게임 시작 문구 출력 기능 구현

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,2 +1,9 @@
-package baseball;public class Baseball {
+package baseball;
+
+public class Baseball {
+    int user_answer;
+
+    public void start(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
 }
