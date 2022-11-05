0671589127a0bc64d824a186197c68f8baf32990
yeonhee <ydol2@naver.com>
2022-11-05 18:38:52 +0900

    feat : user가 3개의 숫자를 입력했는지 확인하는 검증 함수 구현

--- src/main/java/baseball/UserNumberValidator.java
+++ src/main/java/baseball/UserNumberValidator.java
@@ -11,4 +11,12 @@ public class UserNumberValidator {
         Integer userNumberLen = Math.toIntExact(userNumberList.stream().distinct().count());
         return userNumberLen==3;
     }
+
+    /**
+     * 유저가 3개의 숫자를 입력했는지 확인하는 함수
+     */
+    public boolean isThreeNumber(List<Integer> userNumberList){
+        Integer userNumberLen = userNumberList.size();
+        return userNumberLen==3;
+    }
 }
