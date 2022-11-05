1b24315d08bce264225b92393b3ba6cf0626e49c
김재현 <kjhkk1020@naver.com>
2022-11-06 00:16:25 +0900

    feat: String 형의 user input을 한 숫자씩 List<Integer>에 저장해 반환하는 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
@@ -22,5 +23,23 @@ public class Application {
         }
 
         return computerNumberList;
+    }
+
+    /** String 형의 유저 입력을 List<Integer>형으로 변환 */
+    public static List<Integer> getStringInputToList(String userInput) {
+        String[] splitUserInput = userInput.split("");
+        List<String> userInputList = new ArrayList<>(List.of(splitUserInput));
+        List<Integer> userInputIntegerList = new ArrayList<>();
+
+        for (String oneInputNumber : userInputList) {
+            userInputIntegerList.add(Integer.valueOf(oneInputNumber));
+        }
+
+        return userInputIntegerList;
+    }
+
+    /** 유저로부터 입력받은 값이 컴퓨터가 생성한 값과 일치하는지 판별 */
+    public static boolean isUserComputerNumberCorrespond(String userNumber, List<Integer> computerNumber) {
+
     }
 }
