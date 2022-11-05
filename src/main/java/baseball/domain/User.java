bf7f34b5e9f3057380d0a468971aa239f779736a
Jzakka <mouse4786@gmail.com>
2022-11-05 16:55:17 +0900

    feat: User 클래스 추가

---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+import java.util.Scanner;
+
+public class User {
+    String numbers;
+
+    public void setNumbers(String numbers) {
+        this.numbers = numbers;
+    }
+
+    public String getNumbers() {
+        return numbers;
+    }
+}
