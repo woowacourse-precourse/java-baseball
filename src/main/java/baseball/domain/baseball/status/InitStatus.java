61efbecb4a20c4d7dcd112b6a005b35526523cb1
JaehongDev <println@kakao.com>
2022-11-06 00:03:42 +0900

    refactor (InitStatus) : 게임이 아닌 컨텍스트를 참조하도록 변경

--- src/main/java/baseball/domain/baseball/status/InitStatus.java
+++ src/main/java/baseball/domain/baseball/status/InitStatus.java
@@ -1,13 +1,13 @@
 package baseball.domain.baseball.status;
 
-import baseball.domain.baseball.BaseballGame;
+import baseball.domain.baseball.BaseballContext;
 
 public class InitStatus implements BaseballGameStatus {
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
     @Override
-    public BaseballGameStatus next(BaseballGame baseballGame) {
-        baseballGame.println(GAME_START_MESSAGE);
+    public BaseballGameStatus next(BaseballContext baseballContext) {
+        baseballContext.println(GAME_START_MESSAGE);
         return new GenerateAnswerStatus();
     }
 
