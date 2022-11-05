d1868166fc9b9275287367e88731427f20ddc865
Brian <bwook9908@gmail.com>
2022-11-05 22:48:43 +0900

    fix: 문제 조건에 맞게 숫자 입력 함수 조정
    - 원래 함수 입력을 scanner로 받고, 진행했었음
    - 그런데 문제를 읽다가 readLine() 함수를 사용하라고 해서 변경함
    - README.md에도 관련 사항 수정함
    
    Scanner 삭제, readLine() 함수로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,66 +1,64 @@
 package baseball;
 
 
-import java.util.Scanner;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Application {
     public static void main(String[] args) {
         printStartJavaBaseball();
-        char[] input= InputCheckValidAndReturn();
+        char[] input = InputCheckValidAndReturn();
     }
 
-    public static void printStartJavaBaseball(){
+    public static void printStartJavaBaseball() {
         System.out.println("숫자 야구 게임을 시작합니다");
     }
 
-    public static char[] InputCheckValidAndReturn(){
-        String InputString = getInputAsString();
+    public static char[] InputCheckValidAndReturn() {
+        String InputString = descriptionAndInput();
         char[] InputCharArray = revertStringToCharArray(InputString);
-        int [] InputAsciiArray= revertCharArrayToAsciiArray(InputCharArray);
+        int[] InputAsciiArray = revertCharArrayToAsciiArray(InputCharArray);
 
-        if(!isLength3(InputAsciiArray)){
+        if (!isLength3(InputAsciiArray)) {
             throw new IllegalArgumentException();
-        }else if(!isAsciiArrayValid(InputAsciiArray)){
+        } else if (!isAsciiArrayValid(InputAsciiArray)) {
             throw new IllegalArgumentException();
         }
         return InputCharArray;
     }
 
-    public static String getInputAsString(){
+    public static String descriptionAndInput() {
         System.out.print("숫자를 입력해주세요:");
-        Scanner input = new Scanner(System.in);
 
-        return input.nextLine();
+        return readLine();
     }
 
-    public static char[] revertStringToCharArray(String input){
+    public static char[] revertStringToCharArray(String input) {
         return input.toCharArray();
     }
 
-    public static int[] revertCharArrayToAsciiArray(char[] charArray){
+    public static int[] revertCharArrayToAsciiArray(char[] charArray) {
         int[] asciiArray = new int[charArray.length];
+
         for (int i = 0; i < charArray.length; i++) {
             asciiArray[i] = charArray[i];
         }
-
         return asciiArray;
     }
 
-    public static boolean isLength3(int[] array){
+    public static boolean isLength3(int[] array) {
         return (array.length == 3);
     }
 
-    public static boolean isAsciiArrayValid(int [] asciiArray){
-        int count =0;
-        for (int asciiValue: asciiArray) {
+    public static boolean isAsciiArrayValid(int[] asciiArray) {
+        int count = 0;
+        for (int asciiValue : asciiArray) {
             if (asciiValue >= 49 && asciiValue <= 59) {
                 count++;
             }
         }
         return (count == 3);
     }
-
-
 
 
 
