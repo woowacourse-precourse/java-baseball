324d7ec3798d541fccd166a11972c37128dbaf0b
ineiw <75738349+ineiw@users.noreply.github.com>
2022-11-05 16:01:47 +0900

    refactor: calcHint 의 hint element index 이름 변경
    firstElementOfHint 를 ballOfHint 로
    secondElementOfHint 를 strikeOfHint 로 변경하여 ball 과 strike 의 힌트의 위치를 잘보여주게 바꿈

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -22,18 +22,18 @@ public class Computer {
     }
 
     public List<Integer> calcHint(String playerInput, String computerInput) {
-        int firstElementOfHint = 0;
-        int secondElementOfHint = 0;
+        int ballOfHint = 0;
+        int strikeOfHint = 0;
 
         for (int i = 0; i < 3; i++) {
             if (computerInput.charAt(i) == playerInput.charAt(i)) {
-                firstElementOfHint += 1;
+                strikeOfHint += 1;
             }
             if (computerInput.contains(Character.toString(playerInput.charAt(i)))) {
-                secondElementOfHint += 1;
+                ballOfHint += 1;
             }
         }
 
-        return Arrays.asList(firstElementOfHint, secondElementOfHint);
+        return Arrays.asList(ballOfHint, strikeOfHint);
     }
 }
