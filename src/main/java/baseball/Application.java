97de2a0882ff5041ad511abcbc6ec05c132d7228
panorama713 <yaiba713@gmail.com>
2022-11-05 18:21:15 +0900

    refactor: exit

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,21 +8,20 @@ import java.util.List;
 import java.util.Objects;
 
 public class Application {
+    private static String gameStart = "숫자 야구 게임을 시작합니다.";
+    private static String gameExit = "프로그램을 종료합니다.";
+
     public static void main(String[] args) {
         //프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(gameStart);
         gameStart();
     }
+
     //게임 시작
-    public static void gameStart(){
+    public static void gameStart() {
         int start_yn = 1; //exit 변수
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
+        //랜덤으로 3자리 숫자를 정하는 기능
+        List<Integer> computer = randomNum();
         // 3스트라이크가 나올때 까지 반복
         while (start_yn == 1) {
             // 3자리 숫자 입력 기능 실행
@@ -35,22 +34,39 @@ public class Application {
             //숫자 검증 기능(출력기능)
             print_verification(strike, ball, nothing);
             //3스트라이크일 경우 while문 종료 선택 문구
-            if(strike == 3){
+            if (strike == 3) {
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 start_yn = exit();
             }
-            if(strike == 3 && start_yn == 1){
+            if (strike == 3 && start_yn == 1) {
                 gameStart();
                 break;
-            }else if(strike == 3 && start_yn == 2){
+            } else if (strike == 3 && start_yn == 2) {
+                gameExit();
                 return;
             }
         }
     }
-    public static void gameExit(){
-        System.out.println("프로그램을 종료합니다.");
+
+    //랜덤으로 3자리 숫자를 정하는 기능
+    public static List<Integer> randomNum() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
+
+    //게임종료 선택시 프로그램 종료 출력 기능
+    public static void gameExit() {
+        System.out.println(gameExit);
+    }
+
     //숫자를 입력받는 기능
-    private static int int_input() throws IllegalArgumentException {
+    static int int_input() throws IllegalArgumentException {
         // Console 사용하여 String 형태 int 변경
         String input_num_string = Console.readLine();
         int input_num_int = Integer.parseInt(input_num_string);
@@ -86,7 +102,7 @@ public class Application {
         return strike;
     }
 
-    //ball의 개수 구하는 기능
+    //ball 개수 구하는 기능
     public static int ball(List<Integer> input_num_list, List<Integer> computer) {
         //1개 이상의 숫자가 같고 자리가 다른 경우 = (맞은개수)볼
         int ball = 0;
@@ -143,7 +159,6 @@ public class Application {
     }
 
     static int exit() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Integer.parseInt(Console.readLine());
     }
 }
