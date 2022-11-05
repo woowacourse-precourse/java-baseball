39f5ee6b345ad741bc04f29a4aa71c9ed15672a6
Kid-Chang <pch3310@gmail.com>
2022-11-06 00:19:37 +0900

    refactor(view/input): 함수이름에서 불필욯한 input 텍스트 제거
    이미 클래스에서 input 기능이라는 것을 이해할 수 있음.
    inputThreeNumber, inputContinueGame을
    threeNumber, continueGame으로 수정

--- src/main/java/baseball/view/input.java
+++ src/main/java/baseball/view/input.java
@@ -4,12 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class input {
 
-    public String inputThreeNumber() {
+    public String threeNumber() {
         System.out.println("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
-    public boolean inputContinueGame() {
+    public boolean continueGame() {
         int replay;
         while (true) {
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
