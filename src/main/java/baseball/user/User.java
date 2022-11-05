c4646afad03d6d5f6fadd58bd734be68cfbd1108
Invidam <hansupark0208@gmail.com>
2022-11-05 17:58:03 +0900

    fix(User): 게임 제어를 위한 수 (1, 2)를 입력 메서드 수정
    - 메서드 명 수정
    - 참고 객체 변경

--- src/main/java/baseball/user/User.java
+++ src/main/java/baseball/user/User.java
@@ -1,5 +1,6 @@
 package baseball.user;
 
+import baseball.domain.GameControlNum;
 import baseball.domain.ThreeDigitNum;
 import baseball.io.InputControl;
 import baseball.io.OutputControl;
@@ -14,9 +15,9 @@ public class User {
         return new ThreeDigitNum((InputControl.inputInt()));
     }
 
-    public ThreeDigitNum inputGu2essedNum() {
-        OutputControl.println("숫자를 입력해주세요 : ");
-        return new ThreeDigitNum((InputControl.inputInt()));
+    public GameControlNum inputGameControlNum() {
+        OutputControl.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return new GameControlNum((InputControl.inputInt()));
     }
 
 }
