7ff6d74c698595b389563ad421406be4ece1993e
rabbit0216 <rabbit1999k@gmail.com>
2022-11-06 21:59:59 +0900

    feat: run 함수에 컴퓨터,플레이어 숫자 뽑기 추가

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,26 @@
+package baseball;
+import baseball.*;
+
+public class Game {
+    public static void run() {
+        /*
+        게임 초기화
+            컴퓨터 번호 뽑기
+            사용자 번호 입력받기
+         */
+        GameSet.setNumber();
+
+        /*
+         컴퓨터와 사용자 번호 비교
+         */
+        //CompareNumber.
+
+        /*
+        게임 재시작 여부
+         */
+
+    }
+
+
+
+}
