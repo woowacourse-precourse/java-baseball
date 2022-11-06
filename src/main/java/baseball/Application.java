5ef3a4256c59c7542ea060c40fa572a923059b1f
KimRiun <kimriun27@gmail.com>
2022-11-07 05:02:53 +0900

    feat: 초기 및 종료 출력, 게임 종료 입력받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,44 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final int QUIT = 2;
+
+        while (true) {
+            try {
+                printInitial();
+
+//                Game game = new Game();
+//                game.run();
+
+                printFinish();
+
+                if(input() == QUIT)
+                    break;
+            }
+            catch (IllegalArgumentException e) {
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+        }
+    }
+
+    static void printInitial() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    static void printFinish() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    static int input() {
+        int in = Integer.parseInt(Console.readLine());
+        if (!(in == 1 || in == 2)) {
+            System.out.println("잘못된 입력값 입니다. 1 또는 2를 입력해주세요.");
+            throw new IllegalArgumentException();
+        }
+        return in;
     }
 }
