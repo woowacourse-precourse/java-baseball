8cf58b71130fb690f59e9c35a8ba5dd6eb5a932d
wonju-dev <wonjulee.dev@gmail.com>
2022-11-06 22:55:16 +0900

    feat: 사용자 입력 반환 함수 구현
    GameUtil 클래스의 getUserInput 메소드는 Condition 인터페이스의 메소드를 오버라이딩 한 것이다.
    해당 메소드는 Console API를 통해 사용자 입력을 받은 뒤, 유효성을 검증한 뒤, 유효하면
    Integer 타입으로 변환한뒤 반환한다.
    
    3가지 항목에 대해 유효성 검증을 진행한다.
    1. 길이가 3 이하인가
    2. 숫자로만 이루어져 있는가
    3. 중복된 숫자가 사용되었는가

--- src/main/java/baseball/game/util/GameUtil.java
+++ src/main/java/baseball/game/util/GameUtil.java
@@ -1,6 +1,12 @@
 package baseball.game.util;
 
 import baseball.console.Interactive;
+import baseball.validation.Validator;
+import baseball.validation.condition.Condition;
+import baseball.game.condition.input.AllDigitsUniqueCond;
+import baseball.game.condition.input.AllDigitsNumberCond;
+import baseball.game.condition.input.SmallerThanMaxSizeCond;
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -10,6 +16,7 @@ import java.util.stream.Collectors;
 public class GameUtil implements Interactive {
 
     private static final GameUtil instance = new GameUtil();
+    private final static Validator validator = Validator.getInstance();
     private static final Integer RANDOM_RANGE_MIN = 1;
     private static final Integer RANDOM_RANGE_MAX = 9;
     public static final Integer STRIKE_OUT_COUNT = 3;
@@ -36,6 +43,18 @@ public class GameUtil implements Interactive {
 
     @Override
     public Integer getUserInput() {
-        return 0;
+        Integer guess = Integer.parseInt(Console.readLine());
+
+        List<Condition> conditions = List.of(
+                AllDigitsUniqueCond.getInstance(),
+                AllDigitsNumberCond.getInstance(),
+                SmallerThanMaxSizeCond.getInstance()
+        );
+
+        if (validator.isAnyConditionsFalse(guess, conditions)) {
+            throw new IllegalArgumentException();
+        }
+
+        return guess;
     }
 }
