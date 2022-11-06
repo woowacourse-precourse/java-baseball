29c24ea817b9b6eb637a5b69fe3b492122b72e94
은지 <midzy0228@gmail.com>
2022-11-06 16:21:40 +0900

    refactor: 상수 분리

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -8,7 +8,7 @@ public class Constant {
     public static final String RE_START_MESSAGE = "1";
     public static final String QUIT_GAME_MESSAGE = "2";
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    public static final String GAME_END_MESSAGE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String CHOOSE_TO_START_OR_QUIT_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String PLEASE_ENTER_NUMBERS = "숫자를 입력해주세요 : ";
     public static final String STRIKE = "스트라이크";
