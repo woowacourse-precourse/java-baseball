143b29b4976e6e6b270e965754e220ca1fe99aad
kwYoohae <gocksdl7979@gmail.com>
2022-11-06 23:45:21 +0900

    fix(PrintAndRead): 구문하나가 빠진것 추가

--- src/main/java/baseball/view/PrintAndRead.java
+++ src/main/java/baseball/view/PrintAndRead.java
@@ -47,9 +47,11 @@ public class PrintAndRead {
     }
 
     public static int printRestartGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String answer = Console.readLine();
 
         return Integer.parseInt(answer);
     }
+
 }
