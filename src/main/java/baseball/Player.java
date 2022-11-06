6ec55fafb111243d09cb8ca3f89bfe3027d15b4d
hyejungg <kimhyejung12@naver.com>
2022-11-07 02:20:31 +0900

    feat: Player class 구현

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,47 @@
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    private final List<Integer> numbers;
+
+    public Player() {
+        this.numbers = new ArrayList<>();
+    }
+
+    public List<Integer> getNumbers() {
+        return numbers;
+    }
+
+    public void input() {
+        System.out.printf("숫자를 입력해주세요 : ");
+        setUserNumbers();
+    }
+
+    public int gameOverInput() {
+        System.out.println("축하합니다! 경기를 다시 시작하겠습니까? 다시 시작 : 1, 종료 : 2");
+        String input = readLine();
+        return Integer.parseInt(input);
+    }
+
+    public void setUserNumbers() {
+        this.numbers.clear();
+        String input = readLine();
+        for (int i = 0; i < input.length(); i++) {
+            int number = Character.getNumericValue(input.charAt(i));
+            this.numbers.add(number);
+        }
+    }
+
+    public void setComputerNumbers() {
+        while (this.numbers.size() < 3) {
+            int randomNumber = pickNumberInRange(1, 9);
+            this.numbers.add(randomNumber);
+        }
+    }
+}
