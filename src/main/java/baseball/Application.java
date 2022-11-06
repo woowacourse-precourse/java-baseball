7d1ffc5b4478f191990869d336d16bea5aca3e19
impati <impati0716@icloud.com>
2022-11-06 15:58:54 +0900

    feat : 입력에 대한 결과 출력 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -34,7 +34,12 @@ public class Application {
         }
 
     }
-
+    public static String printCurrentSituation(Integer ball , Integer strike){
+        if(ball == 0 && strike == 0) return "낫싱";
+        else if(ball == 0) return strike + "스트라이크";
+        else if(strike == 0) return ball + "볼";
+        else return ball + "볼" + " " + strike + "스트라이크";
+    }
     public static Integer calculationStrike(List<Integer> computer,List<Integer> user){
         Integer strike = 0;
         for(int i = 0;i<computer.size();i++){
