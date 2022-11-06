d3a63b7f368bc6cef06f233a552e3c89b63f0400
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-06 16:01:58 +0900

    test(Game): 스트라이크와 볼 개수 반환 테스트

--- src/test/java/baseball/GameUtilsTest.java
+++ src/test/java/baseball/GameUtilsTest.java
@@ -1,6 +1,5 @@
 package baseball;
 
-import baseball.exception.NumberExceptionUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -27,6 +26,34 @@ public class GameUtilsTest {
         List<Integer> answer = GameUtils.getComputerAnswerNum();
         assertThat(answer.size()).isEqualTo(3);
         assertThat(answer.stream().distinct().count()).isEqualTo(3);
+    }
+
+    @DisplayName("두 숫자에 대한 스트라이크 개수를 반환한다.")
+    @Test
+    void 스트라이크_개수_반환() {
+        List<Integer> answerNum1 = List.of(1, 2, 3);
+        List<Integer> playerNum1 = List.of(4, 2, 3);
+        int strike1 = GameUtils.checkStrikeAndUpdateScore(answerNum1, playerNum1);
+        assertThat(strike1).isEqualTo(2);
+
+        List<Integer> answerNum2 = List.of(1, 2, 3);
+        List<Integer> playerNum2 = List.of(3, 4, 1);
+        int strike2 = GameUtils.checkStrikeAndUpdateScore(answerNum2, playerNum2);
+        assertThat(strike2).isEqualTo(0);
+    }
+
+    @DisplayName("두 숫자에 대한 볼 개수를 반환한다.")
+    @Test
+    void 볼_개수_반환() {
+        List<Integer> answerNum1 = List.of(1, 2, 3);
+        List<Integer> playerNum1 = List.of(4, 1, 5);
+        int ball1 = GameUtils.checkBallAndUpdateScore(answerNum1, playerNum1);
+        assertThat(ball1).isEqualTo(1);
+
+        List<Integer> answerNum2 = List.of(1, 2, 3);
+        List<Integer> playerNum2 = List.of(3, 2, 1);
+        int ball2 = GameUtils.checkBallAndUpdateScore(answerNum2, playerNum2);
+        assertThat(ball2).isEqualTo(2);
     }
 
     public static InputStream generateUserInput(String input) {
