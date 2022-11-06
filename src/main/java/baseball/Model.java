4504348736e1889a5a2e9e109bdd0cc7f32490ee
tjddnr7760 <tjddnr7760@naver.com>
2022-11-06 16:46:33 +0900

    feat : Model클래스 변수, get, set 함수 설정

--- src/main/java/baseball/Model.java
+++ src/main/java/baseball/Model.java
@@ -3,4 +3,22 @@ package baseball;
 public class Model {
     Model() {}
 
+    private int humannumber;
+    private int computernumber;
+
+    public int getHumannumber() {
+        return humannumber;
+    }
+
+    public void setHumannumber(int humannumber) {
+        this.humannumber = humannumber;
+    }
+
+    public int getComputernumber() {
+        return computernumber;
+    }
+
+    public void setComputernumber(int computernumber) {
+        this.computernumber = computernumber;
+    }
 }
