58f0624c68e249d663b81308de10845c405a68b9
jootang2 <joohwan.song15b@gmail.com>
2022-11-06 19:05:35 +0900

    :sparkles: [기능1] : 컴퓨터가 서로 다른 임의의 수 3개 선택

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        baseball();
+
     }
+
+    public static void baseball() {
+        String answer = selectRandomNumber();
+    }
+
+    public static String selectRandomNumber() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            while (sb.toString().contains(Integer.toString(randomNumber))) {
+                randomNumber = Randoms.pickNumberInRange(1, 9);
+            }
+            sb.append(randomNumber);
+        }
+
+        return sb.toString();
+    }
+
 }
