0fea459c9e4a55762dd04be52a530bd663fb4a31
swonny <wonny921@gmail.com>
2022-11-06 11:07:48 +0900

    feat(ExceptionFor2): 기능요구사항2번 예외 처리

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -1,9 +1,6 @@
 package baseball;
 
 public class Exception {
-    final static Integer EXCEPTION4_NUMBER_LENGTH = 1;
-    final static String ANSWER_REPEAT_GAME = "1";
-    final static String ANSWER_QUIT_GAME = "2";
 
     public static void hasCharacters(String input) {
         String regex = "^[0-9]*$";
@@ -14,7 +11,35 @@ public class Exception {
 
 }
 
+class ExceptionFor2 extends Exception {
+    final static int NUMBER_LENGTH = 3;
+    public static void checkGamePlayerNumberInput(String gamePlayerNumberInput) {
+        checkNumberLength(gamePlayerNumberInput);
+        hasSameNumber(gamePlayerNumberInput);
+        hasCharacters(gamePlayerNumberInput);
+    }
+
+    private static void checkNumberLength(String stringGamePlayerInput) {
+        int stringLength =  stringGamePlayerInput.length();
+        if (stringLength > NUMBER_LENGTH || stringLength < 1) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void hasSameNumber(String gamePlayerInput) {
+        for (int stringIndex = 0; stringIndex < NUMBER_LENGTH; stringIndex++) {
+            if (gamePlayerInput.lastIndexOf(gamePlayerInput.charAt(stringIndex)) != stringIndex) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+}
+
 class ExceptionFor4 extends Exception {
+    final static Integer EXCEPTION4_NUMBER_LENGTH = 1;
+    final static String ANSWER_REPEAT_GAME = "1";
+    final static String ANSWER_QUIT_GAME = "2";
 
     public static void checkException(String input) {
         hasCharacters(input);
