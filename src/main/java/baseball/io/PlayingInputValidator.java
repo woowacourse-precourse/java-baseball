815ca54926b173ac80387b6e05c6a27c8b301ea4
yubin <dbqls2821@gmail.com>
2022-11-06 21:18:23 +0900

    feat(PlayingInputValidator): Input의 각 digit이 1에서 9까지의 숫자가 아닌 경우 예외를 던지는 함수 구현

--- src/main/java/baseball/io/PlayingInputValidator.java
+++ src/main/java/baseball/io/PlayingInputValidator.java
@@ -8,9 +8,21 @@ public class PlayingInputValidator implements InputValidator{
   @Override
   public void validate(String input) {
     checkLength(input);
+    checkEachDigit(input);
   }
 
   private void checkLength(String input) {
     if(input.length() != 3) throw new IllegalArgumentException();
   }
+
+  private void checkEachDigit(String input) {
+    for (int i = 0; i < input.length(); i++) {
+      isDigitBetween1to9(input.charAt(i));
+    }
+  }
+
+  private void isDigitBetween1to9(char c) {
+    if(!Character.isDigit(c)) throw new IllegalArgumentException();
+  }
+
 }
