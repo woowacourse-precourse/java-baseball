1be277742c490139ac8cab3d555206c8e11eb8ea
h0ngwon <w___on@naver.com>
2022-11-06 18:16:03 +0900

    feat(Game) : 문자열을 정수형 리스트로 바꾸는 기능

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Game {
   private int strike;
   private int ball;
@@ -19,4 +24,24 @@ public class Game {
     ball = 0;
   }
 
+  public void start() {
+    List<Integer> userNumbers = new ArrayList<>();
+    String userInput;
+    System.out.println("숫자 야구 게임을 시작합니다.");
+    while(strike < 3) {
+      System.out.print("숫자를 입력해 주세요 : ");
+      userInput = readLine();
+    }
+  }
+
+  private List<Integer> stringToIntegerList(String userInput) {
+    String[] userInputSplit = userInput.split("");
+    ArrayList<Integer> result = new ArrayList<>();
+
+    for (String u : userInputSplit) {
+      result.add(Integer.parseInt(u));
+    }
+
+    return result;
+  }
 }
