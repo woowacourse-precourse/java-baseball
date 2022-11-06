d8ad84d18a9a56a61b4a694bd8ad12705ec7ba7d
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-07 00:40:52 +0900

    feat: 사용자에게 숫자 입력을 하라는 출력 기능 추가

--- src/main/java/baseball/view/UserInputView.java
+++ src/main/java/baseball/view/UserInputView.java
@@ -7,4 +7,9 @@ public class UserInputView {
 	public static String receiveInput() {
 		return Console.readLine();
 	}
+
+	public static void printPickUserBalls() {
+		System.out.print("숫자를 입력해주세요 : ");
+	}
+
 }
