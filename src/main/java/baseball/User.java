0eb61f60175c579b6b3e33f89807b974356076d7
kjg1514 <kjg1514@naver.com>
2022-11-06 18:12:34 +0900

    feat : 문자열을 잘라서 List<Integer>로 만들기

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -2,13 +2,25 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class User {
 
-    public String userInputNumber() {
+    public List<Integer> userInputNumber() {
         String userInput = Console.readLine();
-        return userInput;
+        List<Integer> user_Input_List = string_User_NumtoInt_List(userInput);
+        return user_Input_List;
+    }
+
+    public List<Integer> string_User_NumtoInt_List(String userInput) {
+        String[] string_user_num = userInput.split("");
+        List<Integer> int_user_list = new ArrayList<>();
+        for (int i=0; i<userInput.length(); i++) {
+            int_user_list.add(Integer.valueOf(string_user_num[i]));
+        }
+        return int_user_list;
     }
 
 }
