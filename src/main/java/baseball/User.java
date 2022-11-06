09b1dee91361534a5a444dd85b9c7327ca2ce1fe
kjg1514 <kjg1514@naver.com>
2022-11-06 17:20:48 +0900

    feat : 유저 input 받기

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+
+
+public class User {
+
+    public String userInputNumber() {
+        String userInput = Console.readLine();
+        return userInput;
+    }
+
+}
