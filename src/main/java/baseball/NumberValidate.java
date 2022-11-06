ae2e00936b506e4676d0e3447551c60509ffae0c
5witchkr <hob4419@gmail.com>
2022-11-06 20:54:37 +0900

    refactor(NumberValidate): inputNumber 입력값변경, 입력값 size 추가 * 검증대상 inputNumber를 int 에서 List<Integer>로 변경 * 재사용성 향상을 위한 size 입력값 추가

--- src/main/java/baseball/NumberValidate.java
+++ src/main/java/baseball/NumberValidate.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -9,17 +10,18 @@ public class NumberValidate {
 
     /**
      * 1-9 까지의 숫자인지 검증한다.
-     * 3개의 다른 숫자만 입력했는지 검증한다.
-     * 3자리의 숫자인지 검증한다.
-     * @param inputNumber
+     * 서로 다른 숫자만 입력했는지 검증한다.
+     * size 자리의 숫자인지 검증한다.
+     * @param inputNumber - 유저의 입력 숫자 리스트
+     * @param size - 현재게임의 숫자 사이즈
      */
-    void validation(int inputNumber){
-        if (inputNumber > 999 || inputNumber < 99) throw new IllegalArgumentException("3자리 숫자만 입력 가능합니다.");
+    void validation(List<Integer> inputNumber, Integer size){
+        if (inputNumber.size() != size) throw new IllegalArgumentException(size+"자리 숫자만 입력 가능합니다.");
 
-        Set<String> inputNumberSet = Stream.of(String.valueOf(inputNumber).split("")).collect(Collectors.toSet());
+        Set<Integer> inputNumberSet = new HashSet<>(inputNumber);
 
-        if (inputNumberSet.contains("0")) throw new IllegalArgumentException("1부터 9까지의 숫자만 입력 가능합니다.");
+        if (inputNumberSet.contains(0)) throw new IllegalArgumentException("1부터 9까지의 숫자만 입력 가능합니다.");
 
-        if (inputNumberSet.size() != 3) throw new IllegalArgumentException("3개의 숫자는 서로 다른 숫자만 입력 가능합니다.");
+        if (inputNumberSet.size() != size) throw new IllegalArgumentException(size+"개의 숫자는 서로 다른 숫자만 입력 가능합니다.");
     }
 }
