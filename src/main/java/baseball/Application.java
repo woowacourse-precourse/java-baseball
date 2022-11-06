82e5c3c5fff2b40f91b8e0b2b7db7de5d250068e
korjun1993 <corojoon93@naver.com>
2022-11-06 16:28:52 +0900

    feat (Application) : 게임 결과를 계산하는 기능 추가
    게임 결과를 계산하기 위해 Map을 활용했다. Map의 value는 리스트다. 리스트에는 key에 해당하는 숫자가 몇번째에 등장했는지를 담는다. 예를 들어, 컴퓨터의 1번째 수가 7이고 사용자도 7이라면 맵에는 다음과 같이 저장된다. 맵[7]->[1,1]. 맵의 크기가 2이고 두 개의 숫자(순서)가 같다면 스트라이크로 판단한다. 반면에 숫자가 다르다면 볼로 판단한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,6 @@
 package baseball;
 
-import java.util.List;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
@@ -10,9 +10,40 @@ public class Application {
         if (!user.isPossiblePlay()) {
             throw new IllegalArgumentException();
         }
+        GameResult gameResult = play(computer, user);
+        gameResult.printResult();
     }
 
     public static Player createPlayer(List<Integer> numbers) {
         return new Player(numbers);
+    }
+
+    public static GameResult play(Player computer, Player user) {
+        GameResult gameResult = new GameResult();
+
+        HashMap<Integer, List<Integer>> ordersMap = new HashMap<>();
+        List<Integer> comNumbers = computer.getNumbers();
+        List<Integer> usrNumbers = user.getNumbers();
+
+        // 숫자를 key로 지정하고 등장하는 순서를 맵에 저장한다.
+        // 예: hashMap[3] -> [2,2]
+        // 숫자 3은 computer의 2번째 숫자이고 user의 2번째 숫자이다. -> 스트라이크
+        for (int index = 0; index < NumberUtil.NUMBER_SIZE; index++) {
+            ordersMap.computeIfAbsent(comNumbers.get(index), k -> new ArrayList<>()).add(index);
+            ordersMap.computeIfAbsent(usrNumbers.get(index), k -> new ArrayList<>()).add(index);
+        }
+
+        for (List<Integer> orders : ordersMap.values()) {
+            if (orders.size() < 2) {
+                continue;
+            }
+            if (orders.get(0) == orders.get(1)) {
+                gameResult.addStrike();
+            } else {
+                gameResult.addBall();
+            }
+        }
+
+        return gameResult;
     }
 }
