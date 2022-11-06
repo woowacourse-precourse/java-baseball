c7857bc3b0955440572818611566cd9079d59e41
Namyunsuk <yunsuk0328@naver.com>
2022-11-06 14:17:01 +0900

    feat(Computer): 무작위  3자리 수를 생성 및 저장하는 기능 구현

--- src/main/java/baseball/mainframe/Computer.java
+++ src/main/java/baseball/mainframe/Computer.java
@@ -1,4 +1,33 @@
 package baseball.mainframe;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
+
+    private static String randomNumber;
+
+    public static void setRandomNumber(String number) {
+        randomNumber = randomNumber;
+    }
+
+    public static String getRandomNumber() {
+        return randomNumber;
+    }
+
+    public static String makeListToString(List<Integer> list) {
+        return String.join(list.toString());
+    }
+
+    public static void generateRandomNumber() {
+        List<Integer> randomNumberList = new ArrayList<>();
+        while (randomNumberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumberList.contains(randomNumber)) {
+                randomNumberList.add(randomNumber);
+            }
+        }
+        setRandomNumber(makeListToString(randomNumberList));
+    }
 }
