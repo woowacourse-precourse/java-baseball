a6fb2693d4a022182b35ed5231c2edf67cd3fd5e
choiejae <ejae8319@gmail.com>
2022-11-06 14:43:21 +0900

    test: InputNumber validityChecker 3. 유효한 구성요소, 4. 유효하지 않은 구성요소 테스트 생성

--- src/test/java/baseball/UserBallsInputTest.java
+++ src/test/java/baseball/UserBallsInputTest.java
@@ -20,12 +20,10 @@ class UserBallsInputTest {
     void setUp() {
         input = new UserBallsInput(3);
     }
-
 //    String 상태로 내가 넣어준 테스트용 input을 바이트 코드로 바꾸어 준다.
     public static InputStream generateUserInput(String input) {
         return new ByteArrayInputStream(input.getBytes());
     }
-
     @Order(1)
     @DisplayName("Input 생성 테스트")
     @ParameterizedTest(name ="{displayName}) {index} = {0} ")
@@ -33,15 +31,13 @@ class UserBallsInputTest {
         void inputSuccess(String inputString) {
             assertEquals(inputString, input.inputNumberTest(inputString));
         }
-
     @Order(2)
     @DisplayName("유효성 검사 테스트")
     @ParameterizedTest(name ="{displayName}) {index} = {0} ")
-    @ValueSource(strings = {"123", "321"})
+    @ValueSource(strings = {"123", "321", "789"})
     void    inputnumberValidate(String inputString) {
         input.validityChecker(inputString);
     }
-
     @Order(3)
     @DisplayName("유효성 검사 1. 유효한 길이")
     @ParameterizedTest(name ="{displayName}) {index} = {0} ")
@@ -65,5 +61,29 @@ class UserBallsInputTest {
         assertThatThrownBy(() -> input.validLength(scanner.toString()))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessage("input의 길이가 유효하지 않습니다");
+    }
+    @Order(5)
+    @DisplayName("유효성 검사 3. 유효한 구성요소")
+    @ParameterizedTest(name ="{displayName}) {index} = {0} ")
+    @ValueSource(strings = {"123", "321", "789"})
+    void    isInputNumberIsValidElement(String inputString) {
+        InputStream in = generateUserInput(inputString);
+        System.setIn(in);
+        Scanner scanner = new Scanner(System.in);
+        assertThatThrownBy(() -> input.validElement(scanner.toString()))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("input의 구성요소가 숫자로만 이루어지지 않습니다");
+    }
+    @Order(6)
+    @DisplayName("유효성 검사 3. 유효한 구성요소")
+    @ParameterizedTest(name ="{displayName}) {index} = {0} ")
+    @ValueSource(strings = {"ㅁ23", "a21", "7vㅇ"})
+    void    isInputNumberIsValidElement(String inputString) {
+        InputStream in = generateUserInput(inputString);
+        System.setIn(in);
+        Scanner scanner = new Scanner(System.in);
+        assertThatThrownBy(() -> input.validElement(scanner.toString()))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("input의 구성요소가 숫자로만 이루어지지 않습니다");
     }
 }
