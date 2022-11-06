d4fabda42edeb57beaef675d0c81b42e0059a6f3
peterysh <52229539+peterysh@users.noreply.github.com>
2022-11-06 17:48:44 +0900

    feat: construct BaseBallService logic

--- src/main/java/baseball/baseball/BaseBallService.java
+++ src/main/java/baseball/baseball/BaseBallService.java
@@ -5,14 +5,30 @@ import baseball.dto.BaseBallGameOutput;
 
 public class BaseBallService {
 
-    public BaseBallGameOutput playBall(BaseBallGameInput gameInfo){
-        BaseBallGameOutput baseBallGameOutput = new BaseBallGameOutput();
+    boolean[] isExist = new boolean[10];
 
-        /**
-         * 숫자야구 시행
-         */
+    public BaseBallGameOutput playBall(BaseBallGameInput baseBallGameInput){
+        String randomNumber = baseBallGameInput.getRandomNumber();
+        setUpList(randomNumber);
 
+        String guessNumber = baseBallGameInput.getGuessNumber();
+        int ballCount = 0;
+        int strikeCount = 0;
+        for(int i=0;i<3;i++){
+            if(guessNumber.charAt(i) == randomNumber.charAt(i)){
+                strikeCount++;
+            }else if(isExist[guessNumber.charAt(i) - '0']){
+                ballCount++;
+            }
+        }
 
-        return baseBallGameOutput;
+        return new BaseBallGameOutput(ballCount,strikeCount);
+    }
+
+    private void setUpList(String randomNumber) {
+        for(int i=0;i<3;i++){
+            int idx = randomNumber.charAt(i) - '0';
+            isExist[idx] = true;
+        }
     }
 }
