2af54c19a05ae8f5e950595d15285a9bdf2b4b96
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-06 20:50:53 +0900

    컴퓨터의 랜덤 숫자 초기화

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,8 +5,17 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.Util.Valid.validNumberList;
+
 public class Computer {
     private List<Integer> randomNumber;
+    private static final boolean INVALID = false;
+
+    public void init() {
+        while (validNumberList(randomNumber) == INVALID) {
+            makeRandomList();
+        }
+    }
 
     public void makeRandomList() {
         randomNumber = new ArrayList<>();
