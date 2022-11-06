454d8210ec50c64b07d0b82de0d5a27ee840570b
lunchRamen <bloom6561@naver.com>
2022-11-07 04:20:08 +0900

    feat(User): 입력받은 숫자 검증받아 유저객체 생성 완료.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,9 @@
 package baseball;
 
 import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import camp.nextstep.edu.missionutils.*;
 
 
@@ -9,22 +12,54 @@ class Computer{
 
 	public List<Integer> getNumbers(){return this.numbers;}
 	public void generateNumbers(){
-		while (this.getNumbers().size()<3) {
-			int randomNumber = Randoms.pickNumberInRange(1,9);
-			if (!this.getNumbers().contains(randomNumber)){
-				this.getNumbers().add(randomNumber);
-			}
-		}
+		numbers = Randoms.pickUniqueNumbersInRange(1,9,3);
 	}
 }
+
+class User{
+	private List<Integer> numbers = new ArrayList<>();
+
+	public List<Integer> getNumbers(){return this.numbers;}
+
+	public void checkNumbersLength(String stringNumbers) throws IllegalArgumentException{
+		if (stringNumbers.length()!=3) {
+			throw new IllegalArgumentException("숫자를 총 3개 입력해주세요.");
+		}
+	}
+
+	public void checkNumbersInteger(String stringNumbers) throws IllegalArgumentException{
+
+		for (int i=0;i<stringNumbers.length();i++){
+			int digit = stringNumbers.charAt(i)-48;
+
+			if (digit<1 || digit>9) throw new IllegalArgumentException("1부터 9까지의 숫자만 입력해주세요.");
+			else this.getNumbers().add(digit);
+		}
+	}
+	public void inputNumbers() throws IllegalArgumentException {
+		String stringNumbers = Console.readLine();
+
+		checkNumbersLength(stringNumbers);	//길이가 3인지 확인
+		checkNumbersInteger(stringNumbers);	//입력받은게 모두 숫자인지 확인.
+
+	}
+}
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
+//		int checkReplayOrEnd = -1;
+//		while (checkReplayOrEnd != 2){}
         Computer computer = new Computer();
 		computer.generateNumbers();
+		System.out.println("computer.getNumbers() = " + computer.getNumbers());
+
+		User user = new User();
+		user.inputNumbers();
+		System.out.println("user.getNumbers() = " + user.getNumbers());
 
     }
 }
