f66deac059f58c7e15e1b085287c3752eb0ecee2
jjongwa <troas96@naver.com>
2022-11-07 00:19:51 +0900

    test: 테스트 코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import org.mockito.internal.matchers.Null;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static camp.nextstep.edu.missionutils.test.Assertions.*;
+//import static org.assertj.core.api.Assertions.assertThat;
+//import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -19,6 +24,44 @@ class ApplicationTest extends NsTest {
                 1, 3, 5, 5, 8, 9
         );
     }
+
+    @Test
+    void 이상한테스트1(){
+        assertRandomNumberInListTest(
+                () -> {
+                    run(String.valueOf(List.of(1,2,3)));
+                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5, 5, 8, 9
+        );
+    }
+
+    @Test
+    void 이상한테스트(){
+        assertRandomUniqueNumbersInRangeTest(
+                () -> {
+                    run("246", "135", "1", "597", "589", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                List.of(1, 3, 5, 5, 8, 9)
+        );
+    }
+
+    @RepeatedTest(100)
+    void 입력값_받아오기_테스트() {
+        var number = Randoms.pickUniqueNumbersInRange(1,9, 3);
+        StringBuilder num = new StringBuilder();
+        for(int i=0; i < 3; i++){
+            num.append(number.get(i));
+        }
+        System.out.println(String.valueOf(num));
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(String.valueOf(num)))
+                        .isInstanceOf(Null.class)
+        );
+    }
+
 
     @Test
     void 예외_테스트() {
@@ -51,6 +94,24 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 예외_테스트_중복숫자() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("131"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_0포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("120"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+
 
     @Override
     public void runMain() {
