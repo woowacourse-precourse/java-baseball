fb68efc1d07550e799f5709b91c2e307288600c0
abbd2 <ajkety00@gmail.com>
2022-11-07 02:32:30 +0900

    feat(ShowMessageClass): 게임진행과 관련된 메세지를 출력하는 메소드 생성

--- src/main/java/baseball/ShowMessage.java
+++ src/main/java/baseball/ShowMessage.java
@@ -14,4 +14,46 @@ public class ShowMessage {
     private static final String GAME_SET_MESSAGE = GAME_SET_COUNT + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String NEW_GAME_CHECK_MESSAGE = "게임을 새로 시작하려면" + NEW_GAME + ", " + "종료하려면 " + QUIT_GAME
             + "를 입력하세요.";
+
+    // 게임 시작
+    public static void showGameStart() {
+        System.out.print(GAME_START_MESSAGE);
+    }
+
+    // 숫자 입력
+    public static void showInputNumber() {
+        System.out.println();
+        System.out.print(INPUT_NUMBER_MESSAGE);
+    }
+
+    // 결과
+    public void showResult() {
+
+        BaseballCountVO baseballCountVO = new BaseballCountVO();
+
+        if (baseballCountVO.getBallCount() >= 1) {
+            System.out.print(baseballCountVO.getBallCount() + BALL_WORD);
+        }
+        if (baseballCountVO.getStrikeConut() >= 1) {
+            System.out.print(baseballCountVO.getStrikeConut() + STRIKE_WORD);
+        }
+        if (baseballCountVO.getBallCount() < 1 && baseballCountVO.getStrikeConut() < 1) {
+            System.out.println(NOT_MATCH_WORD);
+        }
+    }
+
+    // 게임 종료
+    public static void showGameSet() {
+        System.out.println(GAME_SET_MESSAGE);
+    }
+
+    // 게임 재시작 여부
+    public static void showNewGameCheck() {
+        System.out.println(NEW_GAME_CHECK_MESSAGE);
+    }
+
+    // 숫자의 개수
+    public int getGameSetCount() {
+        return GAME_SET_COUNT;
+    }
 }
