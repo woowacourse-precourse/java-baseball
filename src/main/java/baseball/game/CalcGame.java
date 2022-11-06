0d692001430e57ce86f543c34b6681528b55bbe7
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-07 00:43:16 +0900

    feat: add calc result
    - 예외 검사 메소드 추가
    - 게임 결과 연산 메소드 추가
    - Strike Number 설정자 추가

--- src/main/java/baseball/game/CalcGame.java
+++ src/main/java/baseball/game/CalcGame.java
@@ -3,17 +3,79 @@ package baseball.game;
 import baseball.object.GameResult;
 import baseball.object.StrikeNumber;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class CalcGame {
     private static StrikeNumber number;
+    private static final Integer LENGTH = 3;
 
     public static void setStrikeNumber(StrikeNumber strikeNumber) {
+        number = strikeNumber;
     }
 
     public static void setStrikeNumber(Integer strikeNumber) {
 
+        List<Integer> numberList = new ArrayList<>();
+
+        while (strikeNumber != 0) {
+            numberList.add(strikeNumber % 10);
+            strikeNumber /= 10;
+        }
+
+        Collections.reverse(numberList);
+        number = new StrikeNumber(numberList);
     }
 
-    public static GameResult calcGameResult(Integer userNumber) {
-        return null;
+    public static GameResult calcGameResult(Integer inputNumber) throws IllegalAccessException {
+
+        List<Integer> userNumber = new ArrayList<>();
+
+        while (inputNumber > 0) {
+            Integer restNumber = inputNumber % 10;
+
+            checkNumberRange(restNumber);
+            checkNumberDuplicate(userNumber, restNumber);
+
+            userNumber.add(restNumber);
+            inputNumber /= 10;
+        }
+
+        checkNumberLength(userNumber);
+        Collections.reverse(userNumber);
+
+        Integer ball = 0;
+        Integer strike = 0;
+
+        for (int i = 0; i < LENGTH; i++) {
+            Integer idx = number.getNumber().indexOf(userNumber.get(i));
+
+            if (idx.equals(i)) {
+                strike += 1;
+            } else if (idx != -1) {
+                ball += 1;
+            }
+        }
+
+        return new GameResult(ball, strike);
+    }
+
+    private static void checkNumberRange(Integer number) throws IllegalAccessException {
+        if (number < 1 || number > 9) {
+            throw new IllegalArgumentException("Number length is 1 to 9");
+        }
+    }
+
+    private static void checkNumberLength(List<Integer> list) throws IllegalAccessException {
+        if (list.size() != LENGTH) {
+            throw new IllegalArgumentException("Number length out of range");
+        }
+    }
+
+    private static void checkNumberDuplicate(List<Integer> numberList, Integer number) {
+        if (numberList.contains(number)) {
+            throw new IllegalArgumentException("Numbers must consist of different letters");
+        }
     }
 }
