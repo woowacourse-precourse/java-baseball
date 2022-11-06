393b5ffaa630884db3e51c3186b3bccae35d11f5
Rovna99 <80767387+Rovna99@users.noreply.github.com>
2022-11-06 18:23:48 +0900

    test: 기능 변경으로 인한 테스트 코드 수정:

--- src/test/java/baseball/domain/ComputerNumberTest.java
+++ src/test/java/baseball/domain/ComputerNumberTest.java
@@ -9,13 +9,13 @@ import org.junit.jupiter.api.Test;
 class ComputerNumberTest {
 	@Test
 	void 숫자_길이_테스트() {
-		List<Integer> result = new ComputerNumber().computerNumber;
+		List<Integer> result = new ComputerNumber().getComputerNumber();
 		assertEquals(3, result.size());
 	}
 
 	@Test
 	void 숫자_중복_테스트() {
-		int result = (int)new ComputerNumber().computerNumber.stream().distinct().count();
+		int result = (int)new ComputerNumber().getComputerNumber().stream().distinct().count();
 		assertEquals(3, result);
 	}
 }
