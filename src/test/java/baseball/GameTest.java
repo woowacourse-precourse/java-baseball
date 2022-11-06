82ba2e73572cd6c82ce441f6907274413eff82b9
choiejae <ejae8319@gmail.com>
2022-11-06 22:19:24 +0900

    test: Game 생성 테스트 setUp과 중복으로 판단되어 삭제

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,14 +1,11 @@
 package baseball;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.assertj.core.api.Assertions;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -36,22 +33,16 @@ public class GameTest {
   public void restoreStreams() {
     System.setOut(originalOut);
   }
-  @Test
-  @Order(1)
-  @DisplayName("Game 생성 테스트")
-  public void isGameStartWhenFlagIs1(){
-    Assertions.assertThat(game.toBeContinued).isEqualTo("1");
-  }
 
   @Test
-  @Order(2)
+  @Order(1)
   @DisplayName("유효성 검사 테스트")
   public void generateNumberValidate() {
     game.generateNumberTest("1");
   }
 
   @Test
-  @Order(3)
+  @Order(2)
   @DisplayName("유효성 검사 1. Null 확인")
   public void generateNumberValidateIsStringNotNull() {
     Assertions.assertThatThrownBy(() -> {
@@ -60,7 +51,7 @@ public class GameTest {
   }
 
   @Test
-  @Order(4)
+  @Order(3)
   @DisplayName("유효성 검사 2. 유효한 값 1 확인")
   public void generateNumberValidateIsStringOne() {
     game.generateNumberTest("1");
@@ -68,7 +59,7 @@ public class GameTest {
   }
 
   @Test
-  @Order(5)
+  @Order(4)
   @DisplayName("유효성 검사 3. 유효한 값 2 확인")
   public void generateNumberValidateIsStringTwo() {
     game.generateNumberTest("2");
@@ -76,7 +67,7 @@ public class GameTest {
   }
 
   @Test
-  @Order(6)
+  @Order(5)
   @DisplayName("유효성 검사 4. 유효하지 않은 값 확인")
   public void generateNumberValidateIsStringUnavailable() {
     Assertions.assertThatThrownBy(() -> {
@@ -85,11 +76,11 @@ public class GameTest {
   }
 
   @Test
-  @Order(7)
+  @Order(6)
   @DisplayName("유효성 검사 5. 랜덤 값 확인")
   public void generateNumberValidateRandomTest() {
     String number = Integer.toString(Randoms.pickNumberInRange(0, 9));
-    if (number == "1" || number == "2") {
+    if (number.equals("1") || number.equals("2")) {
       game.generateNumberTest(number);
       assertEquals(number, outContent.toString());
     } else {
