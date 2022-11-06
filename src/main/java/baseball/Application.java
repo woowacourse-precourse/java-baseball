eb2bf2a2385c9e5eb0441cf1248915f514c618ce
CMHyuk <alsgur9710@naver.com>
2022-11-06 15:34:08 +0900

    refactor: 게임 실행 코드 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,10 @@
 package baseball;
 
-import static baseball.Config.gameService;
-import static baseball.PlayGameController.*;
+import static baseball.Config.playGameController;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        run(gameService());
+        playGameController().run();
     }
 }
