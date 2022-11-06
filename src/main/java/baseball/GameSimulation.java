7909cbb64a222795ba616ae6f8a6b73691db2c80
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-06 21:14:59 +0900

    유저의 숫자를 만든다

--- src/main/java/baseball/GameSimulation.java
+++ src/main/java/baseball/GameSimulation.java
@@ -1,4 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GameSimulation {
+    private Computer computer;
+    private Integer strike;
+    private Integer ball;
+
+    public void playGame() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String userInput = Console.readLine();
+        List<Integer> userNumber = makeList(userInput);
+    }
+
+    public List<Integer> makeList(String userInput) {
+        String[] bitInput = userInput.split("");
+        List<Integer> userNumbers = Arrays
+                .stream(bitInput)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        return userNumbers;
+    }
 }
