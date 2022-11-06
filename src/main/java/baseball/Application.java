e768bf6c78cad7aadc6fde7cfde2cf7664b04129
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-06 11:50:45 +0900

    feat(application): 게임 시작하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,12 @@
 package baseball;
 
 import baseball.domain.Game;
+import baseball.view.View;
 
 public class Application {
     public static void main(String[] args) {
         Game baseballGame = new Game();
+        View.startView();
         baseballGame.start();
     }
 }
