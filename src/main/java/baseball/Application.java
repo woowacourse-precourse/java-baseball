93255197551215440d0188b000de40f719d87184
RadSparkle <wwwartg1@naver.com>
2022-11-07 02:25:06 +0900

    FEAT : 입력받은 숫자가 3자릿수인지 확인하는 메소드 생성 및 세자릿수를 의미하는 상수 선언

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    private static final int threeDigits = 999;
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+    public static boolean checkNumber (int test) {
+
+        if(test > threeDigits) {
+            return false;
+        }
+        return true;
     }
 }
