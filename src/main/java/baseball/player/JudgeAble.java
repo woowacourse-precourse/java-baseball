b08dbb3d0259b7f3d4888309034a898b9c006f31
bellljun3395 <195850@jnu.ac.kr>
2022-11-06 16:37:50 +0900

    refactor(JudgeAble): 변경 JudgeAble
    - Player 멤버변수 변경에 따른 변경

--- src/main/java/baseball/player/JudgeAble.java
+++ src/main/java/baseball/player/JudgeAble.java
@@ -6,10 +6,10 @@ public interface JudgeAble {
 
     /**
      * 상대방의 숫자를 받아 자신의 숫자와 비교하여 힌트를 반환하는 메서드이다.
-     * @param number 상대방의 숫자이다.
+     * @param playerNumbers 상대방의 숫자이다.
      * @return 비교한 결과를 List 형식으로 반환하고 첫 번째가 Ball 그리고 두 번째가 Strike다.
      */
-    public List<Integer> judge(int number);
+    public List<Integer> judge(PlayerNumbers playerNumbers);
 
 
 }
