7c5b9f497d4d1d169f9bffc815314b5f20eb3f41
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-06 22:52:42 +0900

    feat: 상수 클래스 분리
    숫자 야구 게임 메세지 및 상수를 Constants 클래스에 저장

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class Constants {
+    public static final int RESULT_SIZE = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final int ZERO = 0;
+    public static final int MIN_ACCESS_INPUT_NUMBER = 122;
+    public static final int MAX_ACCESS_INPUT_NUMBER = 987;
+    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+}
