8817059218df3f12aaca574574b02f3cd12c7ddc
khsrla9806 <kimhunsope@naver.com>
2022-11-06 18:58:14 +0900

    test(InputExceptionTest): 예외 테스트 함수 이름 변경 후 DisplayName 어노테이션 적용

--- src/test/java/baseball/exception/InputExceptionTest.java
+++ src/test/java/baseball/exception/InputExceptionTest.java
@@ -1,5 +1,6 @@
 package baseball.exception;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import java.util.Arrays;
@@ -7,9 +8,10 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputExceptionTest {
+    @DisplayName("플레이어 입력 숫자 예외 테스트")
     @ParameterizedTest
     @ValueSource(strings = {"113", "12", "가13", "ABC", "12354", ""})
-    void 입력_에외_테스트(String inputData) {
+    void checkExceptionNumberTest(String inputData) {
         List<String> playersNumber = Arrays.asList(inputData.split(""));
 
         assertThatThrownBy(() -> InputException.checkExceptionNumber(playersNumber))
