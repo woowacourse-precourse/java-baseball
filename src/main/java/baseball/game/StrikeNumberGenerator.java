379d1b8e2a941be2d174ec2398305126732be37e
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-06 22:41:24 +0900

    test: add random number test
    - 랜덤 숫자 생성 테스트 코드 추가
    - 결정 숫자를 저장할 StrikeNumber 추가
    - 랜덤 숫자 메소드 헤더 추가

---
+++ src/main/java/baseball/game/StrikeNumberGenerator.java
@@ -0,0 +1,10 @@
+package baseball.game;
+
+import baseball.object.StrikeNumber;
+
+public class StrikeNumberGenerator {
+
+    public static StrikeNumber getStrikeNumber() {
+        return null;
+    }
+}
