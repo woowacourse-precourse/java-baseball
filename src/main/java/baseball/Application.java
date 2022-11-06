193aea73f1caa907393ef4bedfb38d0334abc854
homecoder98 <homecoder98@gmail.com>
2022-11-06 18:18:33 +0900

    feat : 게임이 끝나고 종료할지 다시 할지 정하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        chooseResetOrExit();
+    }
+
+    static void startGame() {
+    }
+
+    static void chooseResetOrExit() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String s = Console.readLine();
+
+        if (s.equals("1")) {
+            startGame();
+        } else if (s.equals("2")) {
+            return;
+        }
     }
 }
