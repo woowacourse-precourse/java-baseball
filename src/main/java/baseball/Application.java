fdf22c1994643f1b9d8b3aa3ab481985a4fa83c5
jihun-24k <gi020477@gmail.com>
2022-11-06 16:41:47 +0900

    Feat: 서로 다른 임의의 3개 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) {
+        final List<Integer> computer = makeRandomNumbers();
+
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         outer:
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
-
             break outer;
         }
+    }
+
+    public static List<Integer> makeRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
