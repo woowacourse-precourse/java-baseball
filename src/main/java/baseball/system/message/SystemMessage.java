22e1312f40b6c9a751270ad547d33d15cfda83b6
wonju-dev <wonjulee.dev@gmail.com>
2022-11-07 01:49:27 +0900

    feat: SystemMessage 구현
    SystemMessage는 게임의 결과와 관련된 사용자 출력을 관리하는 열거형이다.

---
+++ src/main/java/baseball/system/message/SystemMessage.java
@@ -0,0 +1,20 @@
+package baseball.system.message;
+
+import baseball.message.Message;
+
+public enum SystemMessage implements Message {
+    START_PROGRAM("숫자 야구 게임을 시작합니다."),
+    STOP_PROGRAM("게임 종료");
+
+    private String message;
+
+    SystemMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return this.message;
+    }
+}
+
