a31b5cf9e159288b222632a159217a91e4c00007
이지혁 <olzlgur@naver.com>
2022-11-06 22:21:44 +0900

    feat: 스트라이크, 볼 값 반환 메서드 구현

--- src/main/java/baseball/result/Result.java
+++ src/main/java/baseball/result/Result.java
@@ -5,24 +5,8 @@ public class Result {
     private int ball;
 
     public Result(int strike, int ball) {
-        this.strike = 0;
-        this.ball = 0;
-    }
-
-    private void countStrike(String randomNumber, String userNumber) {
-        for (int index = 0; index < randomNumber.length(); index++) {
-            if (randomNumber.charAt(index) == userNumber.charAt(index)) {
-                strike++;
-            }
-        }
-    }
-
-    public void countBall(String randomNumber, String userNumber){
-
-    }
-
-    public void countBall() {
-
+        this.strike = strike;
+        this.ball = ball;
     }
 
     public int getStrike() {
@@ -33,10 +17,4 @@ public class Result {
         return ball;
     }
 
-    public boolean istNothing() {
-        if (strike == 0 && ball == 0) {
-            return true;
-        }
-        return false;
-    }
 }
