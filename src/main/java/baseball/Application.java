843ee2a3a5d8a1a48e16fe5b31252d36fe462b1e
shin5774 <babo5774@naver.com>
2022-11-06 20:09:00 +0900

    feat(main,set_computer): [프로그램 시작,문제 생성] 기능
    프로그램 시작 및 문제생성 기능구현 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,22 @@
 package baseball;
 
+import java.util.List;
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+    static List<Integer> set_computer(){
+        List<Integer> computer=new ArrayList<>();
+        while(computer.size()<3){
+            int randomNumber= Randoms.pickNumberInRange(1,9);
+            if(!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computer = set_computer();
     }
 }
