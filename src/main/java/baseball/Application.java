85f5d9329fba07b4369add58015515bee649ad47
qpdh <74190329+qpdh@users.noreply.github.com>
2022-11-06 23:20:04 +0900

    feat($Application) generateNumber
    Application에 generateNumber 메소드 추가:
    - 중복되지 않는 size가 3인 배열 반환하는 메소드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,40 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static void baseballGame() {
+
+    }
+
+    static List<Integer> generateNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+
+    static void startGame() {
+
+    }
+
+    static boolean checkStrike() {
+        return false;
+    }
+
+    static boolean checkBall() {
+        return false;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println(generateNumber());
     }
 }
