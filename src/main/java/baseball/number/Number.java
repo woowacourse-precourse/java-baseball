2e25f62a5ccbd9902b3bf071ce635fca863652a3
이지혁 <olzlgur@naver.com>
2022-11-06 23:28:16 +0900

    feat: 입력 값 길이 유효성 검사 메서드 구현

--- src/main/java/baseball/number/Number.java
+++ src/main/java/baseball/number/Number.java
@@ -6,4 +6,13 @@ public class Number {
     public Number(String number) {
         this.number = number;
     }
+
+    private static boolean validateLength(String number) {
+        if (number.length() != 3) {
+            return false;
+        }
+        return true;
+    }
+
+
 }
