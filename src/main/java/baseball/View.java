413f74aa0d1ef0fc69d52450665e22fb4bfc8927
BuildTools <unconfigured@null.spigotmc.org>
2022-11-07 01:49:19 +0900

    refactor: 수정 출력함수 default -> public
    다른 패키지에서도 접근가능하게 변경

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -3,39 +3,39 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class View {
-    static final void printStartGame(){
+    public static final void printStartGame(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    static final void printChoiceEndOrNewGame(){
+    public static final void printChoiceEndOrNewGame(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    static final void printPleaseInputNumber(){
+    public static final void printPleaseInputNumber(){
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    static final void printStrike(int num){
+    public static final void printStrike(int num){
         System.out.println(num + "스트라이크");
     }
 
-    static final void printBall(int num){
+    public static final void printBall(int num){
         System.out.println(num + "볼");
     }
 
-    static final void printBallAndStrike(int ball, int strike){
+    public static final void printBallAndStrike(int ball, int strike){
         System.out.println(ball + "볼 " + strike + "스트라이크");
     }
 
-    static final void printNothing(){
+    public static final void printNothing(){
         System.out.println("낫싱");
     }
 
-    static final void printCollect(){
+    public static final void printCollect(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    static final String getUserInput() {
+    public static final String getUserInput() {
         return Console.readLine();
     }
 }
