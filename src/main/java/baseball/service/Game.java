bf6c11fe0e3ef11571fdfb71770ecedcfa67a083
footdev <ekclstkfka5@gmail.com>
2022-11-06 20:00:19 +0900

    feat: 전체적인 게임 흐름 구현
    게임의 전체적인 흐름을 구성하는 로직 작성

--- src/main/java/baseball/service/Game.java
+++ src/main/java/baseball/service/Game.java
@@ -1,4 +1,17 @@
 package baseball.service;
 
 public class Game {
+    Rule rule;
+
+    public Game() {
+        this.rule = new Rule();
+    }
+
+    public void run() {
+        do {
+            rule.getNumber();
+            rule.isValid();
+            rule.decideStrikeOrBall();
+        } while (rule.reStart);
+    }
 }
