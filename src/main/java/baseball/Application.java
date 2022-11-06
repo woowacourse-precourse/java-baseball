91bbc4f69fa940601498f2fe9a7b38901e00f079
elizabethgim <yujingim@kookmin.ac.kr>
2022-11-06 23:10:33 +0900

    feat: 숫자 야구 게임하기, playBall

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,10 +5,9 @@ import java.util.Scanner;
 
 public class Application {
     private static String makeRandomNumbers(){
-        double randomNumber = Math.random();
         StringBuilder result = new StringBuilder();
         for(int i=0;i<3;i++){
-            int iValue = (int)(randomNumber * 10);
+            int iValue = (int)(Math.random() * 10);
             result.append(iValue);
         }
         return result.toString();
@@ -20,17 +19,50 @@ public class Application {
         }
         return result;
     }
+    public static String playBall(ArrayList<String> answer, String input){
+        int strike = 0, ballHit = 0;
+        System.out.println(answer);
+        System.out.println(input);
+        for(int i = 0;i<answer.size();i++){
+            if(input.indexOf(answer.get(i)) == i){
+                strike += 1;
+                continue;
+            }
+            
+            if(input.contains(answer.get(i))){
+                ballHit += 1;
+            }
+        }
+
+        String result = "";
+        if(strike == 0 && ballHit == 0){
+            result += "낫싱";
+        }
+        else if(strike == 0){
+            result += ballHit + "볼";
+        } else if(ballHit == 0){
+            result += strike + "스트라이크";
+        } else{
+            result += ballHit + "볼 " + strike + "스트라이크";
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         Scanner inputObject = new Scanner(System.in);
+
         System.out.println("숫자 야구 게임을 시작합니다.");
         String answer = makeRandomNumbers();
         System.out.println(answer);
 
-        System.out.print("숫자를 입력해주세요 : ");
-        String number = inputObject.nextLine();
-        ArrayList<String> numberArray = splitInputToArray(number);
-        System.out.println(numberArray);
-
+        String result = "";
+        while(!result.equals("3스트라이크")){
+            System.out.print("숫자를 입력해주세요 : ");
+            String number = inputObject.nextLine();
+            result = playBall(splitInputToArray(answer), number);
+            System.out.println(playBall(splitInputToArray(answer), number));
+        }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
