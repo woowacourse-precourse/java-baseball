b84ca5230676e6c9586b673af5f847533307676f
JoSihun <kepler452k@naver.com>
2022-11-06 19:23:23 +0900

    feat(Application.java): 1. 컴퓨터 랜덤숫자 생성 기능 추가 getComputerNumber() 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    /** 컴퓨터 랜덤숫자 생성 기능 */
+    public static List<Integer> getComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
