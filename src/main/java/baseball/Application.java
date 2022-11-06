f6bef8969d29f420bb2080231a9f13b7e53c1341
bearjun05 <bearjun05@naver.com>
2022-11-06 14:58:43 +0900

    feat : add createRandomNum

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,44 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        boolean flag = true;
+        while (flag) {
+            List<Integer> randomNum = createRandomNum();
+            System.out.println(randomNum);
+            guessNum();
+            checkNum();
+            closeOrRestart();
+        }
     }
+
+    public static List<Integer> createRandomNum() {
+        List<Integer> computerNum = new ArrayList<>();
+        while (computerNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNum.contains(randomNumber)) {
+                computerNum.add(randomNumber);
+            }
+        }
+        return computerNum;
+    }
+
+    private static void closeOrRestart() {
+
+    }
+
+    private static void checkNum() {
+
+    }
+
+    private static void guessNum() {
+
+    }
+
+
 }
