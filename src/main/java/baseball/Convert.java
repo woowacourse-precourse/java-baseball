e7f1ade6f9b8da33c128888c916ba0aa59d65644
은지 <midzy0228@gmail.com>
2022-11-06 16:46:33 +0900

    fix: 스트라이크와 볼 동시 처리 오류 수정

--- src/main/java/baseball/Convert.java
+++ src/main/java/baseball/Convert.java
@@ -7,13 +7,14 @@ import static baseball.Constant.NUMBERS_LENGTH;
 
 public class Convert {
 
-    public List<Integer> IntegerList (String input) {
+    public List<Integer> IntegerList(String input) {
         List<String> stringList = StringToList(input);
         return StringListToIntegerList(stringList);
     }
+
     private List<String> StringToList(String input) {
         List<String> stringList = new ArrayList<>();
-        for (int i=0; i<NUMBERS_LENGTH; i++) {
+        for (int i = 0; i < NUMBERS_LENGTH; i++) {
             stringList.add(String.valueOf(input.charAt(i)));
         }
         return stringList;
@@ -21,7 +22,7 @@ public class Convert {
 
     private List<Integer> StringListToIntegerList(List<String> stringList) {
         List<Integer> integerList = new ArrayList<>();
-        for (int i=0; i<NUMBERS_LENGTH; i++) {
+        for (int i = 0; i < NUMBERS_LENGTH; i++) {
             integerList.add(Integer.parseInt(stringList.get(i)));
         }
         return integerList;
