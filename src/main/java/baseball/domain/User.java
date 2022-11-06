89cd4cbf518082146946bc03216d268aa0dcb6fe
sosow0212 <sosow0212@naver.com>
2022-11-06 14:10:58 +0900

    feat : 유저의 게임 번호를 반환해주는 기능 구현

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -10,6 +10,10 @@ public class User {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public List<Integer> getUserGameNumbers() {
+        return userGameNumbers;
+    }
+
     public void setUserGameNumbers(List<Integer> userGameNumbers) {
         this.userGameNumbers = userGameNumbers;
     }
