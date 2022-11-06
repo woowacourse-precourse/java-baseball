07747d0c716ba3861bb937870c8af306e25b9db1
shpark30 <seohyeonpark30.gmail.com>
2022-11-06 15:50:06 +0900

    fix(Referee): change the way to manage play again or not
    Before : return boolean where to play again or not
    After : create instance variable to store status of terminate

--- src/main/java/baseball/participants/Referee.java
+++ src/main/java/baseball/participants/Referee.java
@@ -3,13 +3,15 @@ package baseball.participants;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Referee {
-    public static boolean isGameInProgress;
+    public boolean isGameInProgress;
+    public boolean toTerminateGame;
     private final String PLAY_AGAIN = "1";
     private final String TERMINATE = "2";
 
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        this.isGameInProgress = true;
+        isGameInProgress = true;
+        toTerminateGame = false;
     }
 
     public void startPitch() {
@@ -19,7 +21,12 @@ public class Referee {
     public void judge(Pitcher  pitcher, Hitter hitter) {
     }
 
-    public String playAgainOrTerminate() {
+    public void endGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        isGameInProgress = false;
+    }
+
+    public void playAgainOrNot() {
         String againOrTerminate = readLine();
         while (!validatePlayAgain(againOrTerminate)) {
             System.out.println(PLAY_AGAIN + " 혹은 " + TERMINATE + "만 입력해주세요.");
@@ -32,12 +39,7 @@ public class Referee {
         if (againOrTerminate.equals(TERMINATE)) {
             System.out.println("게임을 완전히 종료합니다.");
         }
-        return againOrTerminate;
-    }
-
-    public void endGame() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        this.isGameInProgress = false;
+        toTerminateGame = againOrTerminate.equals(TERMINATE);
     }
 
     private boolean validatePlayAgain(String againOrTerminate) {
