417abbeb0caad5cc53c2d8b2f752abf3c554c9ef
bearjun05 <bearjun05@naver.com>
2022-11-06 22:07:08 +0900

    feat : add closeOrRestart

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,13 +10,6 @@ public class Application {
 
     public static void main(String[] args) {
 
-        boolean flag = true;
-        while (flag) {
-            String computerNum = createRandomNum().toString();
-            String userNum = guessNum();
-            checkNum(computerNum, userNum);
-            closeOrRestart();
-        }
     }
 
     public static List<Integer> createRandomNum() {
@@ -30,36 +23,47 @@ public class Application {
         return computerNum;
     }
 
-    private static void closeOrRestart() {
-
-    }
-
-    public static String checkNum(String computerNum, String userNum) {
-        char[] computerNumCharArr = computerNum.toCharArray();
+    public static String checkNum(List<Integer> computerNum, String userNum) {
+        String computerNumStr = "";
+        for (Integer x : computerNum) {
+            computerNumStr += x.toString();
+        }
+        char[] computerNumCharArr = computerNumStr.toCharArray();
         char[] userNumCharArr = userNum.toCharArray();
         int ballCnt = 0;
         int strikeCnt = 0;
         for (int i = 0; i < 3; i++) {
-            if (computerNum.contains(userNumCharArr[i] + "")) {
+            if (computerNumStr.contains(userNumCharArr[i] + "")) {
                 ballCnt++;
             }
             if (computerNumCharArr[i] == userNumCharArr[i]) {
                 strikeCnt++;
             }
         }
-
         if (ballCnt == 0 && strikeCnt == 0) {
             return "낫싱";
-        } else if (ballCnt == 0){
+        } else if (ballCnt-strikeCnt == 0) {
             return strikeCnt + "스트라이크";
         } else if (strikeCnt == 0) {
-            return  ballCnt + "볼";
+            return ballCnt + "볼";
         }
         return ballCnt - strikeCnt + "볼 " + strikeCnt + "스트라이크";
     }
 
     public static String guessNum() {
         return readLine();
+    }
+
+    public static boolean closeOrRestart() {
+        System.out.println("정답입니다.");
+        System.out.println("다시 시작하시려면 1번을 종료하시려면 2번을 눌러주세요.");
+        String oneOrTwo = readLine();
+        if (oneOrTwo.equals("1")){
+            return true;
+        } else {
+            System.out.println("게임이 종료되었습니다");
+            return false;
+        }
     }
 
 
