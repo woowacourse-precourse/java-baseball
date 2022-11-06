ecb429bcbf954f269ed6304251615836c91d44da
kimdoha <kkhhjj3393@gmail.com>
2022-11-07 02:10:28 +0900

    feat(Result): strike, ball count 매개변수 추가

--- src/main/java/baseball/score/Result.java
+++ src/main/java/baseball/score/Result.java
@@ -1,35 +1,41 @@
 package baseball.score;
 
 public class Result {
+  private final Integer strikeCount;
+  private final Integer ballCount;
+
+  private Result(Integer strikeCount, Integer ballCount){
+    this.strikeCount = strikeCount;
+    this.ballCount = ballCount;
+  }
+
+  public boolean isNothing(){
+    return this.strikeCount == 0 && this.ballCount == 0;
+  }
+
+  public boolean isBall(){
+    return this.ballCount > 0;
+  }
+
+  public boolean isStrike(){
+    return this.strikeCount > 0;
+  }
 
   public String getResults(Integer strikeCount, Integer ballCount) {
-    if (isNothing(strikeCount, ballCount)) {
+    if (isNothing()) {
       return "낫싱";
     }
 
     StringBuilder stringBuilder = new StringBuilder();
 
-    if (isBall(ballCount)) {
+    if (isBall()) {
       stringBuilder.append(ballCount).append("볼 ");
     }
 
-    if (isStrike(strikeCount)) {
+    if (isStrike()) {
       stringBuilder.append(strikeCount).append("스트라이크");
     }
 
     return stringBuilder.toString().stripTrailing();
   }
-
-  public boolean isNothing(Integer strikeCount, Integer ballCount){
-    return strikeCount == 0 && ballCount == 0;
-  }
-
-  public boolean isBall(Integer ballCount){
-    return ballCount > 0;
-  }
-
-  public boolean isStrike(Integer strikeCount){
-    return strikeCount > 0;
-  }
-
 }
