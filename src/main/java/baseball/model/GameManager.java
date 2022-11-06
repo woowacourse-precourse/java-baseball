94138fc11adcfe466423e2cf45c0592e14a13c3c
ChanghyunKim <variety82p@gamil.com>
2022-11-06 21:40:27 +0900

    feat : 게임 진행 계산 메서드 구현

--- src/main/java/baseball/model/GameManager.java
+++ src/main/java/baseball/model/GameManager.java
@@ -7,11 +7,39 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static baseball.constant.ConstantValue.BASEBALL_NUMBER_SIZE;
+import static baseball.constant.ConstantValue.*;
 
 public class GameManager {
     public GameManager(){
     }
+
+    public int [] playGame(List<Integer> computer, List<Integer> player){
+        int[] score = new int[3];
+        for(int i = 0; i < BASEBALL_NUMBER_SIZE; i++){
+            int computerNumber = computer.get(0);
+            int computerIndex = i;
+
+            int result = calculateScore(computerNumber, computerIndex, player);
+            score[result]++;
+        }
+        return score;
+    }
+
+    public int calculateScore(int computerNumber, int computerIndex, List<Integer> player) {
+        for(int i = 0; i < BASEBALL_NUMBER_SIZE; i++){
+            int playerNumber = player.get(0);
+            int playerIndex = i;
+
+            if(computerNumber == playerNumber && computerIndex == playerIndex){
+                return STRIKE;
+            }
+            if(computerNumber == playerNumber){
+                return BALL;
+            }
+        }
+        return NOTHING;
+    }
+
 
     public List<Integer> toList(String number){
         return Arrays.stream(number.split(""))
