8c4a4fc8b428c4339a973c99f106dcafdc19bb86
KYR <freekyr7529@gmail.com>
2022-11-06 20:10:02 +0900

    refactor : User 클래스의 userNumber 제한자 제거

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,10 +1,9 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class User {
-    private List<Integer> usersNumber;
+    List<Integer> usersNumber;
 
     public User(List<Integer> numberList) {
         this.usersNumber = numberList;
