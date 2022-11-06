38442a7297bf07ac358122071e2a9c6c432f8870
KYR <freekyr7529@gmail.com>
2022-11-06 20:07:18 +0900

    feature : 사용자와 컴퓨터의 입력을 비교해서 스트라이크와 볼 여부를 저장하는 compareNumber 메소드 생성

--- src/main/java/baseball/NumberBaseball.java
+++ src/main/java/baseball/NumberBaseball.java
@@ -4,27 +4,32 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class NumberBaseball {
-    private User user;
-    private Computer computer;
+    User user;
+    Computer computer;
+    ValidInput validInput;
+    GameScore gameScore;
 
-    private List<Integer> userInputList;
-    private ValidInput validInput;
+    public NumberBaseball() {
+    }
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다");
         System.out.print("숫자를 입력해주세요 : ");
         try {
-            this.userInputList = inputNumber();
-            user = new User(userInputList);
+            user = new User(inputNumber());
+            computer = new Computer();
         } catch (IllegalArgumentException exception) {
             exception.printStackTrace();
         }
         computer.generateRandomNumber();
 
+        compareNumber();
 
+        gameScore.printResult();
     }
 
     private List<Integer> inputNumber() throws IllegalArgumentException {
@@ -39,5 +44,42 @@ public class NumberBaseball {
         List<String> list = new ArrayList<>(Arrays.asList(strArr));
 
         return list.stream().map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private void compareNumber() {
+        List<List<Integer>> compareResult = new ArrayList<>();
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (makeCompareList(i, j).size() > 0) {
+                    compareResult.add(makeCompareList(i, j));
+                };
+            }
+        }
+
+        for (List<Integer> result: compareResult) {
+            if (Objects.equals(result.get(0), result.get(1))) {
+                strike += 1;
+            }
+
+            if (!(Objects.equals(result.get(0), result.get(1)))) {
+                ball += 1;
+            }
+        }
+
+        gameScore = new GameScore(strike, ball);
+    }
+
+    private List<Integer> makeCompareList(int i, int j) {
+        List<Integer> result = new ArrayList<>();
+
+        if (Objects.equals(user.usersNumber.get(i), computer.computerNumber.get(j))) {
+            result.add(i);
+            result.add(j);
+        }
+
+        return result;
     }
 }
