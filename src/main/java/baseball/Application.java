46eda2e51cefd869c03f547c79e7270b4ed5ba11
Soseung Soo <sss9073@naver.com>
2022-11-07 03:28:07 +0900

    refactor: delete static method and variable using new Executor();

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,7 @@ import baseball.Controller.Executor;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Executor.startGame();
+        Executor executor = new Executor();
+        executor.startGame();
     }
 }
