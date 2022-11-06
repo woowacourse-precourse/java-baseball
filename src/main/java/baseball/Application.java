b4ddf62d99dd64e1ba057468748ad31a8f51f4b3
오승윤 <>
2022-11-06 19:00:17 +0900

    feat (main) : main() 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.game.Game;
+import baseball.utils.appConfig.AppConfig;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = AppConfig.createGame();
+        game.play();
     }
 }
