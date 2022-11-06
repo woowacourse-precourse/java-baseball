7c425b5ffbc3aa0b6c67b7bc356d9223a10f4d4c
seongha <11pi885@gmail.com>
2022-11-06 21:10:06 +0900

    test: checkBall 함수 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,6 +3,10 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,10 +32,26 @@ class ApplicationTest extends NsTest {
         );
     }
     @Test
-    void exceptionCase2(){
+    void 입력_예외테스트2(){
         assertSimpleTest(()->
                 assertThatThrownBy(()-> runException("abc"))
                         .isInstanceOf(IllegalArgumentException.class));
+    }
+    @Test
+    void checkBall_함수_테스트1(){
+        int index = 0;
+        int value = 2;
+        List<Integer> computer = new ArrayList<>(Arrays.asList(1,2,3));
+        int ballCount = baseball.Application.checkBall(index,value,computer);
+        assertThat(ballCount).isEqualTo(1);
+    }
+    @Test
+    void checkBall_함수_테스트2(){
+        int index = 0;
+        int value = 4;
+        List<Integer> computer = new ArrayList<>(Arrays.asList(1,2,3));
+        int ballCount = baseball.Application.checkBall(index,value,computer);
+        assertThat(ballCount).isEqualTo(0);
     }
     @Override
     public void runMain() {
