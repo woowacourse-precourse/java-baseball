4a9a5fa5a52556ebaf3530b7db84e7001ad3f3ae
goldcollar95 <yjs7298@naver.com>
2022-11-07 02:17:05 +0900

    feat(User): startAndEnd(재시작/종료구분)

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -20,6 +20,19 @@ public class User {
         return inputValue;
     }
 
+    public static int startAndEnd(String userInput){
+        if (userInput.equals("1")) {
+                return 1;
+
+        }
+
+        else if(userInput.equals("2")){
+                return 2;
+        }
+
+        throw new IllegalArgumentException("예외처리");
+    }
+
 }
 
 
