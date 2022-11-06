0d53740cfaabacb9242e2a7fc03b142d8b622d46
chordpli <chordplaylist@gmail.com>
2022-11-06 18:02:53 +0900

    Comment(BasballGame): run메소드 설계

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,15 @@
+package baseball;
+
+public class BaseballGame {
+    
+    // 애플리케이션 실행
+    public void run(){
+        // 컴퓨터의 숫자 초기화
+
+        // 사용자의 입력값 저장
+
+        // 입력 값과 결과 비교
+
+    }
+
+}
