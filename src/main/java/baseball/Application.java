191ae5c31fbd63af64ca17523555850f16b45f8b
namtndus <rnrmf0721@gmail.com>
2022-11-07 02:17:26 +0900

    feat(printToRestartingGameMessage): 함수의 기능 구현
    '"게임을 새로 시작하려면 1, 종류하려면 2를 입력하세요." 출력'를 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -100,6 +100,7 @@ public class Application {
 
     public static void printToRestartingGameMessage(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종류하려면 2를 입력하세요.");
     }
 
     public static void main(String[] args) {
