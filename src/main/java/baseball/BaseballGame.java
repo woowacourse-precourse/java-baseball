32174fc1a31d31f610cc033b1f3551f693c497c4
hectick <chyeon1223@cau.ac.kr>
2022-11-06 11:07:09 +0900

    feat : 게임 종료 문구를 출력하는 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -79,5 +79,10 @@ public class BaseballGame {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    private void printGameEndStatement(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
 
 }
