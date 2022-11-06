65b5ade3bb9fd505dae7bc56434df2abc34eacd5
Cho SangHyun <98shcho@naver.com>
2022-11-06 21:33:11 +0900

    refactor(게임 시작 기능): simplify feature to print
    Remove 'message' member variable for convenience
    
    Before:
    1. set 'message' by method(setMessage)
    2. print message
    
    After:
    1. print message directly(not set message)

--- src/main/java/baseball/GameMessageDisplay.java
+++ src/main/java/baseball/GameMessageDisplay.java
@@ -1,15 +1,7 @@
 package baseball;
 
 public class GameMessageDisplay {
-    protected String message;
-    public void setMessage(String newMessage) {
-        this.message = newMessage;
-    }
-    public void printMessage() {
-        System.out.println(this.message);
-    }
     public void printGameStartMessage() {
-        setMessage("숫자 야구 게임을 시작합니다.");
-        printMessage();
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
