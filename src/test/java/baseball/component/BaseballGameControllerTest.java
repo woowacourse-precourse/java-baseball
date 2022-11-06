2c3e7700853be261617e3c6415dbffff032432d0
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-06 14:23:59 +0900

    test(BaseballGameController): 사용하지 않는 import 제거

--- src/test/java/baseball/component/BaseballGameControllerTest.java
+++ src/test/java/baseball/component/BaseballGameControllerTest.java
@@ -8,9 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 
 class BaseballGameControllerTest extends NsTest {
 
