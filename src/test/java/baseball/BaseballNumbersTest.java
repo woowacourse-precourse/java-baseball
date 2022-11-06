a3d422806215bb823ffe5c41c4aed86f83f8af89
juhwang <juhong4930@naver.com>
2022-11-06 18:10:13 +0900

    feat(BaseballNumbers): find out that input string is "records" command

--- src/test/java/baseball/BaseballNumbersTest.java
+++ src/test/java/baseball/BaseballNumbersTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class BaseballNumbersTest {
     BaseballNumbers baseballNumbers = new BaseballNumbers();
@@ -28,6 +29,32 @@ class BaseballNumbersTest {
         @Test
         void case3_2자리_숫자_입력() {
             String input = "12";
+            boolean result = false;
+            assertThat(baseballNumbers.isNumericReadline(input)).isEqualTo(result);
+        }
+
+    }
+
+    @Nested
+    class 입력값이_RECORDS_명령어인지_확인 {
+
+        @Test
+        void case1_records_소문자_명령어_입력() {
+            String input = "records";
+            boolean result = true;
+            assertThat(baseballNumbers.isCommandReadline(input)).isEqualTo(result);
+        }
+
+        @Test
+        void case2_records_대문자_명령어_입력() {
+            String input = "RECORDS";
+            boolean result = true;
+            assertThat(baseballNumbers.isCommandReadline(input)).isEqualTo(result);
+        }
+
+        @Test
+        void case3_명령어_외_다른_문자_입력() {
+            String input = "record";
             boolean result = false;
             assertThat(baseballNumbers.isNumericReadline(input)).isEqualTo(result);
         }
