0c7f54e32c3c86e64a302335fd6a048977a2084c
leesky <leehaneul0623@gmail.com>
2022-11-06 23:08:41 +0900

    feat(BaseballGame) : 멤버 변수 생성
    BaseballGame 로직 구현을 위해 필요한 멤버 변수 생성 :
     - GREETING MESSAGE
     - CLOSE MESSAGE
     - NOTHING MEESAGE
     - INPUT MESSAGE
     - RESTART MESSAGE
     - CLOSE NUMBER
     - RESTART NUMBER
     - NUMBER LIMIT
     - isFirst (첫 시작은 재시작 여부가 무조건 참이다.)

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,6 +1,17 @@
 package baseball;
 
 public class BaseballGame extends Game {
+    private final int NUMBER_LIMIT = 3;
+    private final int RESTART_NUMBER = 1;
+    private final int CLOSE_NUMBER = 2;
+    private final String GREETING_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private final String CLOSE_MESSAGE = NUMBER_LIMIT + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private final String NOTHING_MESSAGE = "낫싱";
+    private final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    private boolean isFirst = true;
+
     @Override
     protected void greet() {
 
