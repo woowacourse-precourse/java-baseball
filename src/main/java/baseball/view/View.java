caa2efe23381c931ab0a468a86c3d3b2d4bc7f0b
ChanghyunKim <variety82p@gamil.com>
2022-11-06 21:26:23 +0900

    refactor : initView에서 입력값을 받지 않고 게임시작 메시지만 출력

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -7,11 +7,9 @@ import static baseball.constant.ConstantValue.*;
 public class View {
     public View(){}
 
-    public String initView(){
+    public void initView(){
         System.out.println(GAME_START_MESSAGE);
-        System.out.println(NUBMER_INPUT_MESSAGE);
-        String inputNumber = Console.readLine();
-        return inputNumber;
     }
+
 
 }
