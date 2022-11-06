3553eac00081d748d5d40066730d8033c629e0ee
choiejae <ejae8319@gmail.com>
2022-11-06 21:58:07 +0900

    feat: Converter의 결과를 출력하는 Printer 기능 생성

--- src/main/java/baseball/GameResultPrinter.java
+++ src/main/java/baseball/GameResultPrinter.java
@@ -3,28 +3,12 @@ package baseball;
 import java.util.List;
 
 public class GameResultPrinter {
-
-    public String convertGameResult(List<Integer> gameResult) {
-        String converter = "";
-        if (isNothing(gameResult)) {
-            return "낫싱";
-        }
-        if (isOnlyBall(gameResult)) {
-            return gameResult.get(1) + " 볼";
-        }
-        if (isOnlyStrike(gameResult)) {
-            return gameResult.get(0) + " 스트라이크";
-        }
-        return gameResult.get(1) + " 볼 " + gameResult.get(0) + " 스트라이크";
+    private GameResultConverter converter;
+    public GameResultPrinter() {
+        converter = new GameResultConverter();
     }
-
-    private static boolean isOnlyBall(List<Integer> gameResult) {
-        return gameResult.get(1) != 0 && gameResult.get(0) == 0;
-    }
-    private static boolean isOnlyStrike(List<Integer> gameResult) {
-        return gameResult.get(0) != 0 && gameResult.get(1) == 0;
-    }
-    private static boolean isNothing(List<Integer> gameResult) {
-        return gameResult.get(2) != 0;
+    public void printer(List<Integer> result) {
+        String buffer = converter.convertGameResult(result);
+        System.out.println(buffer);
     }
 }
