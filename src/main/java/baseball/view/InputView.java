9e16c145031dcf922dae68c7e603fa59b0ba0250
jihong <clara5091@naver.com>
2022-11-06 21:00:48 +0900

    feat: 숫자 입력 기능 추가

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+    public int inputNumbers(){
+        System.out.print("숫자를 입력해주세요 : ");
+        return Integer.parseInt(Console.readLine());
+    }
+}
