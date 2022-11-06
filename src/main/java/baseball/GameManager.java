e9412eb72b33cfd28a8cd1cfc7ebbec53ab0e448
woosung1223 <dntjd991223@naver.com>
2022-11-06 14:19:06 +0900

    refactor(GameManager): 인스턴스 변수에서 초기화하도록 변경

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -7,20 +7,13 @@ import java.util.List;
 import static baseball.SystemMsgPrinter.*;
 
 public class GameManager {
-    private InputHandler inputStream;
-    private OutputHandler outputStream;
-    private BaseballManager baseballManager;
+    private final InputHandler inputStream = new InputHandler();
+    private final OutputHandler outputStream = new OutputHandler();
+    private final BaseballManager baseballManager = new BaseballManager();
 
     public void run() {
-        init();
         startFirstGame();
         startRepeatedGame();
-    }
-
-    private void init() {
-        inputStream = new InputHandler();
-        outputStream = new OutputHandler();
-        baseballManager = new BaseballManager();
     }
 
     private boolean isGameOver(List<Integer> userScore) {
