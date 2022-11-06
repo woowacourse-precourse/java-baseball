167c6e6872b6205f9eb2e7cabf1823275ddd400b
choiejae <ejae8319@gmail.com>
2022-11-06 20:42:08 +0900

    test: GameResultPrinter 볼 + 스트라이크 확인

--- src/test/java/baseball/GameResultPrinterTest.java
+++ src/test/java/baseball/GameResultPrinterTest.java
@@ -38,4 +38,10 @@ class GameResultPrinterTest {
         assertEquals(printer.convertGameResult(new ArrayList<>(Arrays.asList(0, 2, 0))), "2 볼");
         assertEquals(printer.convertGameResult(new ArrayList<>(Arrays.asList(0, 3, 0))), "3 볼");
     }
+    @Order(4)
+    @Test
+    void BallStrikeResultPrinter() {
+        assertEquals(printer.convertGameResult(new ArrayList<>(Arrays.asList(1, 1, 0))), "1 볼 1 스트라이크");
+        assertEquals(printer.convertGameResult(new ArrayList<>(Arrays.asList(2, 1, 0))), "2 볼 1 스트라이크");
+    }
 }
