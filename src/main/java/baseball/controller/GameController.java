8a981e6baef6e8d5da0bc66dd48c3f417fd1b89d
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-06 12:33:19 +0900

    refactor: GameController 기능단위 테스트 후 설계를 위해 컨트롤러 롤백

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -7,78 +7,46 @@ import baseball.view.PlayerView;
 import baseball.view.ScoreResultView;
 
 public class GameController {
-    private static final int THREE_STRIKE = 3;
-    GameService gameService;
-    GameStatusService gameStatusService;
-    PlayerView playerView;
-    ScoreResultView scoreResultView;
-    GameStatusView gameStatusView;
 
     public GameController(){
-        gameService = new GameService();
-        playerView = new PlayerView();
-        scoreResultView = new ScoreResultView();
-        gameStatusView = new GameStatusView();
-        gameStatusService = new GameStatusService();
     }
 
     public void runGame(){
-        generateGameStatus();
-        while(gameStatusService.getGameStatus()){
-            generateComputer();
-            startGame();
-            whatToDoNext();
-        }
     }
 
     private void generateComputer(){
-        gameService.setComputer();
     }
 
     private void generateGameStatus(){
-        gameStatusService = new GameStatusService();
     }
 
     private void startGame(){
-        while(!isThreeStrike()){
-            clearScore();
-            getPlayerNumbers();
-            matchGame();
-            showPlayersScore();
-        }
     }
 
     private void clearScore(){
-        gameService.initializeScore();
     }
 
     private void getPlayerNumbers(){
-        gameService.setPlayer(playerView.getPlayerOpinion());
     }
 
     private void matchGame(){
-        gameService.checkComputerWithPlayerNumber();
     }
 
     private void showPlayersScore(){
-        scoreResultView.setBallCount(gameService.getBallCount());
-        scoreResultView.setStrikeCount(gameService.getStrikeCount());
-        scoreResultView.printScore();
     }
 
     private boolean isThreeStrike(){
-        return gameService.getStrikeCount() == THREE_STRIKE;
+        return false;
     }
 
     private void whatToDoNext(){
-        setGameStatus(getPlayersOpinion());
     }
 
     private String getPlayersOpinion(){
-        return gameStatusView.getOpinion();
+        return "";
     }
 
     private void setGameStatus(String opinion){
-        gameStatusService.setGameStatus(opinion);
+
     }
 }
