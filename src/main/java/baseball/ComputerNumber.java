c13403fc9cd66b5fd6ab9e88b5a25961f44c5dea
Jaehong Lee <woohong24@naver.com>
2022-11-06 23:02:07 +0900

    feat: add class 'ComputerNumber'

---
+++ src/main/java/baseball/ComputerNumber.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class ComputerNumber {
+
+    int number1;
+    int number2;
+    int number3;
+
+    public ComputerNumber(int number1, int number2, int number3) {
+        this.number1 = number1;
+        this.number2 = number2;
+        this.number3 = number3;
+    }
+}
