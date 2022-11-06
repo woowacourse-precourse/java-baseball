e7ffbc291c64ef43b7d8d4662ab47f35c988aeab
Jae Ung Jung <roosa0210@gmail.com>
2022-11-07 04:46:55 +0900

    feat(Answer): 중복되지 않는 랜덤한 3자리 수 선택
    랜덤되지 않는 3개의 수를 List로 반환하는 매서드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,11 +3,12 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
+import org.mockito.stubbing.Answer;
 
 public class Application {
-    public static void main(String[] args) {
 
-        //  랜덤한 숫자 3개 선택
+    //  중복되지 않는 랜덤한 3자리 수 선택
+    public static List<Integer> Answer() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -15,6 +16,15 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+        return computer;
+    }
+    public static void main(String[] args) {
+        List<Integer> answer = new ArrayList<>();
+        game :while(true) {
+            answer = Answer();
+
+        }
+
 
     }
 }
