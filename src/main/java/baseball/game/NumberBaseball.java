0585a96e479c06a328b7d0ecbf5f7106770d0438
eora21 <eora21@naver.com>
2022-11-06 19:39:09 +0900

    feat: 🎸 추가 유저가 완벽한 답을 맞추기 전까지 게임을 반복하는 메서드
    유저가 완벽한 답을 이끌어내기 전까지 계속해서 값을 요구하는 메서드를 작성, 값의 입력은 staff에게, 값의 판단은
    referee에게 맡긴다.

--- src/main/java/baseball/game/NumberBaseball.java
+++ src/main/java/baseball/game/NumberBaseball.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class NumberBaseball {
     private final Staff staff = new Staff();
+
+    private final Referee referee = new Referee();
+
     public void play() {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
@@ -11,5 +14,14 @@ public class NumberBaseball {
             List<Integer> targetNumbers = staff.getTargetNumbers();
             repeatUntilUserGetsPerfectAnswer(targetNumbers);
         } while (isUserWantMoreGame());
+    }
+
+    private void repeatUntilUserGetsPerfectAnswer(List<Integer> targetNumbers) {
+
+        do {
+            List<Integer> userNumbers = staff.getUserNumbers();
+        } while (referee.isPerfectAnswer(targetNumbers, userNumbers));
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
