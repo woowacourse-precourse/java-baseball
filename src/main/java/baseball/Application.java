1af92189bae16995a0c9471ad800bd131b1bd16a
cksdud8028 <cksdud8028@naver.com>
2022-11-06 18:44:16 +0900

    FEAT add askToUserGameAgain

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,9 +18,20 @@ public class Application {
         }
     }
 
+    private static boolean askToUserGameAgain(int strike) {
+        if (strike == 3) {
+            printGameOver();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String userAnswer = Console.readLine();
+            
+        }
+    }
+
     private static void printGameOver() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+
 
     private static int getResult() {
         List<String> receivingNumbers = receivingValue();
