da14b5b13494e25d3a7d00f283c587cae5634ae8
abbd2 <ajkety00@gmail.com>
2022-11-07 05:58:05 +0900

    feat(Application): 게임의 진행 상황을 체크하는 메소드 구현, 반복되는 코드 메소드화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,7 @@ public class Application {
 
     static GameController gameController = new GameController();
     static RandomNumber randomNumber = new RandomNumber();
+    static NumBerVO numberVO = new NumBerVO();
     public static void main(String[] args) {
 
 
@@ -13,20 +14,42 @@ public class Application {
         ShowMessage.showGameStart(); // 게임 시작 메세지 출력
         List<Integer> randomNumberList = randomNumber.createRandomNumber(); // 난수 생성
         System.out.println("난수:" + randomNumberList + ":");
-        ShowMessage.showInputNumber(); // 숫자 입력 메세지 출력
-        List<Integer> userNumberList = gameController.inputUserNumber(); // 사용자 입력
-        System.out.println("입력값:" + userNumberList + ":");
-        gameResult(userNumberList, randomNumberList);
+        numberVO.setRandomNumber(randomNumberList);
+
+        keepGame();
 
     }
 
-    public static boolean gameResult(List<Integer> userNumberList, List<Integer> randomNumberList){
+    public static int gameResult(){
+
+        List<Integer> userNumberList = numberVO.getUserNumber();
+        List<Integer> randomNumberList = numberVO.getRandomNumber();
 
         int ballCount = gameController.checkIntersection(userNumberList, randomNumberList);
         System.out.println("교집합:" + ballCount + ":");
-        gameController.checkGameResult(userNumberList, randomNumberList, ballCount); // 결과 계산
+        return gameController.checkNotMatch(userNumberList, randomNumberList, ballCount); // 결과 계산;
+    }
+
+    public static boolean gameCheck(){
+
+        int gameFlag = gameResult();
+
+        if(gameFlag == 0){
+            System.out.println(gameFlag+"반복");
+        } else if (gameFlag == 1) {
+            System.out.println(gameFlag+"새로 시작");
+        } else if (gameFlag == 2) {
+            System.out.println(gameFlag+"종료");
+        }
 
         return false;
     }
 
+    public static void keepGame(){
+        ShowMessage.showInputNumber(); // 숫자 입력 메세지 출력
+        List<Integer> userNumberList = gameController.inputUserNumber(); // 사용자 입력
+        System.out.println("입력값:" + userNumberList + ":");
+        numberVO.setUserNumber(userNumberList);
+        gameCheck(); // 게임 결과 체크
+    }
 }
--- src/main/java/baseball/ShowMessage.java
+++ src/main/java/baseball/ShowMessage.java
@@ -11,7 +11,7 @@ public class ShowMessage {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-    private static final String GAME_SET_MESSAGE = ANSWER_COUNT + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String END_GAME_MESSAGE = ANSWER_COUNT + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String NEW_GAME_CHECK_MESSAGE = "게임을 새로 시작하려면" + NEW_GAME + ", " + "종료하려면 " + QUIT_GAME
             + "를 입력하세요.";
 
@@ -39,8 +39,8 @@ public class ShowMessage {
     }
 
     // 게임 종료
-    public static void showGameSet() {
-        System.out.println(GAME_SET_MESSAGE);
+    public static void showEndGame() {
+        System.out.println(END_GAME_MESSAGE);
     }
 
     // 게임 재시작 여부
