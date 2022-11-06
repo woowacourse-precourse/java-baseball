23f3d7d22d6b22224261d21fc99dc2bc212494bf
ddogga <hayeonlee60327@gmail.com>
2022-11-06 22:17:12 +0900

    getRandomNumber메서드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,26 +11,26 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        List<Integer> computer_num = getRandomNumber();
+        List<Integer> computer_num = Randoms.pickUniqueNumbersInRange(1,9,3);
 
-
-
-    }
-
-
-    public static List<Integer> getRandomNumber() {
-
-        List<Integer> num = new ArrayList<>();
-
-        while (num.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1,9);
-            if (!num.contains(randomNumber)) {
-                num.add(randomNumber);
-            }
-        }
-
-        return num;
-
+        System.out.println("computer_num = " + computer_num);
 
     }
+
+
+    //해당 메서드가 이미 Randoms 클래스 안에 구현되어져 있었음.
+//    public static List<Integer> getRandomNumber() {
+//
+//        List<Integer> num = new ArrayList<>();
+//
+//        while (num.size() < 3) {
+//            int randomNumber = Randoms.pickNumberInRange(1,9);
+//            if (!num.contains(randomNumber)) {
+//                num.add(randomNumber);
+//            }
+//        }
+//
+//        return num;
+//
+//    }
 }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,8 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -13,10 +14,14 @@ class ApplicationTest extends NsTest {
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("246", "135", "1", "597", "589", "2");
+                    //입력값 입력.
+                    //run => 인자값들을 입력값으로 바꾸고 Application을 실행시킴.
+                    //Application의 출력값이 각각 아래와 같다면 테스트가 통과하는 형식...?
+                    run("246", "135", "1", "597", "589", "2"); //1:게임 재시작 2:게임 종료
                     assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
                 },
-                1, 3, 5, 5, 8, 9
+
+                1, 3, 5, 5, 8, 9  //정답 숫자가 135 -> 589 로 바뀜..
         );
     }
 
@@ -27,6 +32,13 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 중복되지_않은_3개의_숫자() {
+
+
+    }
+
 
     @Override
     public void runMain() {
