d09c1898557104e127a92a75298483994bdd1e58
davejang <davejang98@naver.com>
2022-11-06 13:07:30 +0900

    feat: user number input validation
    숫자 야구 유저 입력값 검증

--- src/main/java/baseball/controller/InputValidator.java
+++ src/main/java/baseball/controller/InputValidator.java
@@ -1,13 +1,24 @@
 package baseball.controller;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class InputValidator {
+    HashSet duplicateList = new HashSet<>();
     public InputValidator() {
     }
 
     public boolean checkValidateNumber(String number) {
         if(number.length() != 3) return false;
+        String[] numberList = number.split("");
+        Pattern pattern = Pattern.compile("[1-9]");
+        for(int i = 0; i < 3; i++) {
+            if(pattern.matcher(numberList[i]).matches() == false) return false;
+            if(duplicateList.contains(numberList[i]) == true) return false;
+            duplicateList.add(numberList[i]);
+        }
         return true;
     }
 
