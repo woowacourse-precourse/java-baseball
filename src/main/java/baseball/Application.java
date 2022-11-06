b0d62056aa9040e8b8aac618e9a128876e206d93
lvalentine6 <ysr1227@naver.com>
2022-11-06 19:30:33 +0900

    refactor : 예외 처리 기능 수정, 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,40 +7,83 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.text.NumberFormat;
 import java.text.ParsePosition;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         ComputerNumbers computerNumbers = new ComputerNumbers(Randoms.pickNumberInRange(1, 9),
                 Randoms.pickNumberInRange(1, 9), Randoms.pickNumberInRange(1, 9));
+
+        System.out.println(
+                computerNumbers.getFirst() + " " + computerNumbers.getSecond() + " " + computerNumbers.getThird());
         System.out.println(userInput());
     }
 
     static String userInput() {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해 주세요 : ");
-        boolean flag;
         String inputString;
 
         try {
             inputString = br.readLine();
-            flag = isValid(inputString);
         } catch (IOException ioException) {
             throw new IllegalArgumentException(ioException);
         }
 
-        if (flag) {
-            return inputString;
-        } else {
-            throw new IllegalArgumentException();
-        }
+        isValidInput(inputString);
+        isValidSize(inputString);
+        isContainsZero(inputString);
+        isOverLap(inputString);
 
+        return inputString;
     }
 
-    static boolean isValid(String inputString) {
+    static void isValidInput(String inputString) {
         ParsePosition parsePosition = new ParsePosition(0);
         NumberFormat.getInstance().parse(inputString, parsePosition);
 
-        return (inputString.length() == 3) && (inputString.length() == parsePosition.getIndex());
+        if (inputString.length() != parsePosition.getIndex()) {
+            throw new IllegalArgumentException("숫자만 입력해 주세요");
+        }
+    }
+
+    static void isValidSize(String inputString) {
+        if (inputString.length() == 0) {
+            throw new IllegalArgumentException("입력된 숫자가 없습니다.");
+        }
+        if (inputString.length() != 3) {
+            throw new IllegalArgumentException("3자리 숫자를 입력해 주세요");
+        }
+    }
+
+    static void isContainsZero(String inputString) {
+        if (inputString.contains("0")) {
+            throw new IllegalArgumentException("1부터 9 사이 숫자만 입력해 주세요");
+        }
+    }
+
+    static void isOverLap(String inputString) {
+        Set<Integer> set = new HashSet<>();
+        String[] arr = inputString.split("");
+
+        for (int i = 0; i < arr.length; i++) {
+            set.add(Integer.parseInt(arr[i]));
+        }
+
+        if (set.size() != 3) {
+            throw new IllegalArgumentException("서로 다른 숫자 3개를 입력해 주세요");
+        }
+    }
+
+    static boolean continueGame(String inputString) {
+        if(inputString.equals("1")) {
+            return true;
+        } else if (inputString.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException("1이나 2를 입력해 주세요");
+        }
     }
 }
