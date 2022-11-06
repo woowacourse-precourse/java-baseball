133465a5f46daa3a2a06553f923f3ff3a8e0670e
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-06 20:05:24 +0900

    feat: 스트라이크와 볼의 갯수를 출력하는 기능 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -67,9 +67,24 @@ public class Application {
                 strikeCnt++;
         }
         int ballCnt = input.length()-input.replaceAll("["+answer+"]","").length()-strikeCnt;
+        strikeMessage(strikeCnt, ballCnt);
         if (strikeCnt == 3)
             return true;
         return false;
+    }
+    private static void strikeMessage (int strike, int ball) {
+        if (strike == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n"+"게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        } else if (strike + ball == 0) {
+            System.out.println("낫싱");
+        } else if (ball == 0) {
+            System.out.println(strike+"스트라이크");
+        } else if (strike == 0) {
+            System.out.println(ball+"볼");
+        } else if (strike > 0 && ball > 0) {
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
     }
 
 }
