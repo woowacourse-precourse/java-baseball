476ec476f6d183aa35c69c89255c3e852853229c
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-06 17:33:40 +0900

    feat: add user input view
    - 입력 상황 시 출력할 함수 추가
    - 사용자에게 숫자를 입력 받는 함수 추가

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -1,8 +1,18 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class GameView {
 
-    public void printStartView() {
+    public static void printStartView() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printInputView() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public static String getInputNumber() {
+        return Console.readLine();
     }
 }
