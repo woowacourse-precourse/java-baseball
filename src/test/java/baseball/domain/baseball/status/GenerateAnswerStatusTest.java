876e30c9468a59cd255092ce6c5ecb461a167f60
JaehongDev <println@kakao.com>
2022-11-06 21:31:30 +0900

    refactor (GenerateAnswerStatus) : 가짜 context를 주입받도록 변경함

--- src/test/java/baseball/domain/baseball/status/GenerateAnswerStatusTest.java
+++ src/test/java/baseball/domain/baseball/status/GenerateAnswerStatusTest.java
@@ -2,7 +2,7 @@ package baseball.domain.baseball.status;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import baseball.domain.baseball.BaseballContextImpl;
+import baseball.domain.baseball.BaseballContextFake;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -12,7 +12,9 @@ class GenerateAnswerStatusTest {
 
     @Test
     void 정답생성_상태_뒤에_올_단계는_사용자_입력_상태입니다() {
-        var status = new GenerateAnswerStatus().next(new BaseballContextImpl());
+        var context = new BaseballContextFake();
+        var status = new GenerateAnswerStatus().next(context);
+
         assertThat(status).isInstanceOf(UserInputStatus.class);
     }
 
