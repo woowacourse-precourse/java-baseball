455082cfdf0898ef4733be30e198c203cb9feb7d
jihong <clara5091@naver.com>
2022-11-07 07:54:24 +0900

    test: 기능에 대한 테스트 추가

---
+++ src/test/java/baseball/FunctionTest.java
@@ -0,0 +1,60 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class FunctionTest {
+    @Test
+    void 초과입력_예외_테스트() {
+        Numbers numbers = new Numbers();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(1);
+
+        assertThatThrownBy(numbers::validateNumbers)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 범위초과_입력_예외() {
+        Numbers numbers = new Numbers();
+
+        assertThatThrownBy(() -> numbers.add(0))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자가 1~9 사이의 수가 아닙니다.");
+    }
+
+    @Test
+    void 문자가_입력될_때_예외() {
+        BaseballGame baseballGame = new BaseballGame();
+
+        assertThatThrownBy(() -> baseballGame.validateStringToInteger("12a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자만 입력되어야 합니다.");
+    }
+
+    @Test
+    void 볼_확인_테스트() {
+        Numbers numbers = new Numbers();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        int result = numbers.check(2, 2);
+
+        assertThat(result).isEqualTo(1);
+    }
+
+    @Test
+    void 스트라이크_확인_테스트() {
+        Numbers numbers = new Numbers();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        int result = numbers.check(2, 1);
+
+        assertThat(result).isEqualTo(2);
+    }
+}
