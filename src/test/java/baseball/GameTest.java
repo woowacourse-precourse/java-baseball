8af0fe306547b26bcdb3b83b79c460f1df216937
장기환 <jang-gihwan@jang-gihwan-ui-MacBookPro.local>
2022-11-06 21:11:34 +0900

    refacotr : 게임 테스트 수정

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-import baseball.model.Game;
 import baseball.utility.Utility;
 import camp.nextstep.edu.missionutils.test.NsTest;
 
