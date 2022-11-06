8a7ae928995a5f926bf6f8172eec89ce48e377c7
ksh-beginner <ohk9134@naver.com>
2022-11-06 23:05:16 +0900

    test(NumberBaseballGameControllerTest) : 게임 재시작 시 컨트롤러 gameStatus play인지 테스트

--- src/test/java/baseball/controller/NumberBaseballGameControllerTest.java
+++ src/test/java/baseball/controller/NumberBaseballGameControllerTest.java
@@ -1,6 +1,29 @@
 package baseball.controller;
 
 
+import baseball.view.InputView;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class NumberBaseballGameControllerTest {
-    
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1"})
+    void 게임_재시작_시_gameStatus_play(String input) {
+        NumberBaseballGameController numberBaseballGameController = new NumberBaseballGameController();
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        numberBaseballGameController.gameRestartOrEnd();
+        String gameStatus = numberBaseballGameController.getGameStatus();
+
+        assertThat(gameStatus).isEqualTo("play");
+    }
 }
