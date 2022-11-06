0b3b756d3d01753a7b46eba4b20ece9abfcd1994
윤서영 <yoonseoyoung@yunseoyeong-ui-MacBookPro.local>
2022-11-06 17:21:23 +0900

    FEAT : 입력받은 수의 형식 확인

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Controller {
@@ -12,6 +14,7 @@ public class Controller {
         System.out.println("START_MESSAGE = " + START_MESSAGE);
         List<Integer> answerNum = service.setAnswerNum();
         String inputString = getInputString();
+        List<Integer> inputNum = changeInputFormat(inputString);
 
     }
 
@@ -19,5 +22,23 @@ public class Controller {
         System.out.print(INPUT_MESSAGE);
         String inputString = Console.readLine();
         return inputString;
+    }
+
+    private List<Integer> changeInputFormat(String inputString) {
+        List<Integer> inputNum = new ArrayList<>();
+
+        if(inputString.length() != 3)
+            throw new IllegalArgumentException();
+
+        inputString.chars().forEach(c -> {
+            Integer temp = Character.getNumericValue(c);
+            if(temp < 1 || temp > 9)
+                throw new IllegalArgumentException();
+            if(inputNum.contains(temp)) {
+                throw new IllegalArgumentException();
+            }
+            inputNum.add(temp);
+        });
+        return inputNum;
     }
 }
