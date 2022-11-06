09ac9ea02a0e90923deb48afde02d1b8e3c46479
KimSangHyeok <tkdgur0906@naver.com>
2022-11-07 00:10:52 +0900

    Feat: 유저 숫자를 컴퓨터와 비교해 게임 결과 구하는 기능 추가

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -8,9 +8,13 @@ import java.util.List;
 public class NumberBaseballGame {
     private static List<Integer> computerNumber;
     private static List<Integer> userNumber;
+    private static int strike;
+    private static int ball;
 
-    public NumberBaseballGame(){
+    public NumberBaseballGame() {
         computerNumber = new ArrayList<>();
+        strike = 0;
+        ball = 0;
     }
 
     public static void getRandomThreeDigitNumber() {
@@ -22,13 +26,25 @@ public class NumberBaseballGame {
         }
     }
 
-    public static List<Integer> getUserNumberList(String userNumber){
+    public static List<Integer> getUserNumberList(String userNumber) {
         String[] userNumberArray = userNumber.split("");
         List<Integer> userNumberList = new ArrayList<>();
         for (String number : userNumberArray) {
             userNumberList.add(Integer.parseInt(number));
         }
         return userNumberList;
+    }
+
+    public static void compareUserAndComputer() {
+        for (int i = 0; i<userNumber.size();i++){
+            if(computerNumber.indexOf(userNumber.get(i)) == i){
+                strike++;
+                continue;
+            }
+            if(computerNumber.contains(userNumber.get(i))){
+                ball++;
+            }
+        }
     }
 
 }
