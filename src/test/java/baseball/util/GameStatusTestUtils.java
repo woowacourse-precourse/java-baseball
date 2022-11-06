d2daed116b33cc7aa9dba0abd99b6702ed164931
apptie <appti@naver.com>
2022-11-06 13:23:37 +0900

    test(GameCommandTest): GameCommand 테스트 추가

---
+++ src/test/java/baseball/util/GameStatusTestUtils.java
@@ -0,0 +1,16 @@
+package baseball.util;
+
+import java.util.Arrays;
+
+public final class GameStatusTestUtils {
+
+    private GameStatusTestUtils() {
+    }
+
+    public static GameStatus findGameStatusByName(String name) {
+        return Arrays
+            .stream(GameStatus.values())
+            .filter(gameStatus -> gameStatus.name().equals(name))
+            .findAny().orElseThrow();
+    }
+}
