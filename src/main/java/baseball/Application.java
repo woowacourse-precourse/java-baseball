dd646adac8f2fe7f2faf69e076e8bd78a9c792a9
pushedrumex <pushedrumex@naver.com>
2022-11-06 16:00:58 +0900

    feat(Game.init): init game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,12 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game.init();
+    }
+}
+class Game {
+    static final String INITGAME = "숫자 야구 게임을 시작합니다.";
+    static void init() {
+        System.out.println(INITGAME);
     }
 }
