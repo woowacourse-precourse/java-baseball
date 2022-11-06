3bf601152ff20d32b8a73e17fe097de0e54254f1
lee <higgs95@naver.com>
2022-11-07 00:21:54 +0900

    refactor(UserNumber): String 변환 기능 utils 분리

--- src/main/java/baseball/service/UserNumberService.java
+++ src/main/java/baseball/service/UserNumberService.java
@@ -1,25 +1,22 @@
 package baseball.service;
 
-import static baseball.type.NumberType.NUMBER_SIZE;
 import static camp.nextstep.edu.missionutils.Console.readLine;
+import static utils.Converter.convertStringToIntegerList;
 
 import baseball.domain.Validation;
-import java.util.ArrayList;
 import java.util.List;
 
 public class UserNumberService {
 
     public static List<Integer> generate() {
-        List<Integer> userNumbers = new ArrayList<>();
         String inputNumber = readLine();
 
         Validation.validate(inputNumber);
 
-        for (int index = 0; index < NUMBER_SIZE.getValue(); index++) {
-            int numberToInt = inputNumber.charAt(index) - '0';
-            userNumbers.add(numberToInt);
-        }
+        List<Integer> userNumbers = convertStringToIntegerList(inputNumber);
 
         return userNumbers;
     }
+
+
 }
