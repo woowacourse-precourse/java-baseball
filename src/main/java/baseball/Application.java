189a4b4f03b7aecd390eb289259cd44e17a55dcf
dener8 <8dener@gmail.com>
2022-11-07 01:21:27 +0900

    feat: main에 randomNumber와 playerNumber을 불러옴

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,22 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        NumberMaker numberMaker = new NumberMaker();
+        String randomNumber = numberMaker.makeRandomNumber();
+        String playerNumber = numberMaker.makePlayerNumber();
+
+        int ball;
+        int strike;
+        // 완전히 동일한 경우
+        if (randomNumber.equals(numberMaker)) {
+
+        } else {
+
+        }
+        // while문 들어가야 함
+        BallStrikeChecker ballStrikeChecker = new BallStrikeChecker();
+        ball = ballStrikeChecker.countBall(randomNumber, playerNumber);
+        strike = ballStrikeChecker.countStrike(randomNumber, playerNumber);
+
     }
 }
