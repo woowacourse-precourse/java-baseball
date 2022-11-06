d23fe8a8e3c0f29021ffb4d082cb15672991dfcd
CAEL0 <kjongmin26@gmail.com>
2022-11-06 17:18:40 +0900

    feat(main): 메인 메서드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,8 @@ import java.util.regex.Pattern;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        playGame();
     }
 
     public static void playGame() {
