4e1a2cfef69a41ddb30e3ead5996d9127a21fcfe
sodlfmag <sodlfmag@khu.ac.kr>
2022-11-06 13:42:30 +0900

    feat: add isAnswervalid() to validate random answer number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,6 +14,19 @@ public class Application {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
+    public static boolean isAnswerNumberValid(int answerNum){
+        int hundred = answerNum / 100;
+        int ten = (answerNum % 100) / 10;
+        int one = answerNum % 10;
+
+        if(hundred==0 || ten ==0 || one==0)
+            return false;
+
+        if(hundred==ten || hundred==one || ten==one)
+            return false;
+
+        return true;
+    }
 }
 
 
