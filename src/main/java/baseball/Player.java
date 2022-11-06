c6f94763d26dacfef967462032873ba8cdbfd98d
JerryK026 <kskyung0624@gmail.com>
2022-11-06 19:46:25 +0900

    Feat(Player) : aksNumber 구현
    유저에게 입력을 요구하는 askNumber와 이를 파싱 및 검증하는 parseUserNumbersToList를 구현한다

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,4 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Player {
+    public List<Integer> askNumbers() {
+        String userInput = Console.readLine();
+        return parseUserNumbersToList(userInput);
+    }
+
+    public List<Integer> parseUserNumbersToList(String userAnswer) {
+        List<Integer> parsedAnswer = new ArrayList<>();
+
+        if (userAnswer.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        try {
+            Arrays.stream(userAnswer.split(""))
+                    .forEach((num) -> parsedAnswer.add(Integer.parseInt(num)));
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+
+        return parsedAnswer;
+    }
 }
