4a6b6f496453172932e42a11b3aaa8d59799cf34
YooSuYeon <usuyn000@gmail.com>
2022-11-07 04:47:47 +0900

    feat: 출력을 위한 문자열 상수로 정의

---
+++ src/main/java/constants/Text.java
@@ -0,0 +1,22 @@
+package constants;
+
+public enum Text {
+    NOTIFY_START("숫자 야구 게임을 시작합니다."),
+    INPUT_REQUEST("숫자를 입력해주세요 : "),
+    STRIKE("스트라이크"),
+    BALL("볼"),
+    NOTING("낫싱"),
+    NOTIFY_END("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+    RESTART_OR_END("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+    private final String value;
+
+    Text(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+}
