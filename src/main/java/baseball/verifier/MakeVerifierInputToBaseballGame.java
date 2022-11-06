3ea9b1763527504290f8a335ce9a389688f8825e
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 21:39:12 +0900

    refactor: 수정 매직넘버
    가독성 향상을 위해 수정

--- src/main/java/baseball/verifier/MakeVerifierInputToBaseballGame.java
+++ src/main/java/baseball/verifier/MakeVerifierInputToBaseballGame.java
@@ -5,11 +5,14 @@ import baseball.verifiable.CheckInputLength;
 import baseball.verifiable.CheckInputRange;
 
 public class MakeVerifierInputToBaseballGame implements MakeVerifier {
+    private final static int NUMBER_LENGTH_MAX = 3;
+    private final static int LOWER_NUMBER = 1;
+    private final static int UPPER_NUMBER = 9;
     private final Verifier verifier = new Verifier();
 
     public MakeVerifierInputToBaseballGame() {
-        verifier.addRule(new CheckInputLength(3));
-        verifier.addRule(new CheckInputRange(1, 9));
+        verifier.addRule(new CheckInputLength(NUMBER_LENGTH_MAX));
+        verifier.addRule(new CheckInputRange(LOWER_NUMBER, UPPER_NUMBER));
         verifier.addRule(new CheckDuplicatedInput());
     }
 
