be9b43545ac02a3b04834724ebe9e703bd16fed6
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-06 16:36:11 +0900

    Feat : "스트리아크, 볼 기능 구현"
    사용자 숫자 야구 진행 중에 숫자가 4개 이상 늘어나면 오류가 발생하도록 만들었습니다.

--- src/main/java/baseball/Input_Output.java
+++ src/main/java/baseball/Input_Output.java
@@ -31,4 +31,12 @@ public class Input_Output {
 
         Collections.reverse(Scan);
     }
+
+    public void File_Check(List<Integer> User_Number)//사용자 입력 중 예외 사항 체크
+    {
+
+        if(User_Number.size()>3)
+            throw new IllegalArgumentException();
+
+    }
 }
