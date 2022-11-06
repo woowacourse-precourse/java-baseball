e378272fbd5b57ff71d38525a89e12ba13e725a2
flydog98 <guinnessoverflow@gmail.com>
2022-11-07 00:40:04 +0900

    feat: write game end announcing logic

--- src/main/java/baseball/GameClient.java
+++ src/main/java/baseball/GameClient.java
@@ -18,7 +18,8 @@ public class GameClient {
     }
 
     private void announceGameEnd() {
-
+        this.baseballUtil.printlnLine("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        this.baseballUtil.printlnLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
     }
 
     private int getUserInput() {
