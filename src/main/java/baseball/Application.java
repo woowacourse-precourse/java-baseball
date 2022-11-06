dddfcf78f7eb00a1579567643d7b81b1c27a2e73
BaeDongHyeon <sasd730@gmail.com>
2022-11-07 00:50:42 +0900

    feat: 사용자의 입력값 저장

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        
+
+        while (true) {
+            List<Integer> user = getUserNumber();
+            
+        }
+    }
+
+    private static List<Integer> getUserNumber() {
+        List<String> userNumber = Arrays.asList(Console.readLine().split(""));
+        return userNumber.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
