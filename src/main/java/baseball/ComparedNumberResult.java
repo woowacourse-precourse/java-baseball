a30e259cd98c616a06d8ad59b185691b9cac6ecc
JuHyeokParkk <hyeok5596@naver.com>
2022-11-06 18:22:38 +0900

    feat: 숫자 비교 결과를 저장하고 출력하는 클래스 구현

---
+++ src/main/java/baseball/ComparedNumberResult.java
@@ -0,0 +1,50 @@
+package baseball;
+
+public class ComparedNumberResult {
+
+    private int ballCount;
+    private int strikeCount;
+
+    public ComparedNumberResult() {
+        this.ballCount = 0;
+        this.strikeCount = 0;
+    }
+
+    public int getStrikeCount() {
+        return strikeCount;
+    }
+
+    public void plusStrikeCount() {
+        strikeCount++;
+    }
+
+    public void plusBallCount() {
+        ballCount++;
+    }
+
+    @Override
+    public String toString() {
+
+        if(ballCount + strikeCount == 0) {
+            return Message.getNothingMessage();
+        }
+
+        StringBuilder message = new StringBuilder();
+
+        if(ballCount > 0) {
+            message.append(ballCount + Message.getBallMessage());
+        }
+
+        if(ballCount > 0 && strikeCount > 0) {
+            message.append(" ");
+        }
+
+        if(strikeCount > 0) {
+            message.append(strikeCount + Message.getStrikeMessage());
+        }
+
+        return message.toString();
+
+
+    }
+}
