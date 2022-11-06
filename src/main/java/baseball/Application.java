c50c065191d4348413bcc5c3bca1e83e0950630d
kksks3192 <kksks3192@gmail.com>
2022-11-06 17:57:55 +0900

    feat(baseball): 정답과 일치 정도(스트라이크, 볼, 낫싱)를 판별하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,6 +16,7 @@ public class Application {
 
         System.out.print("숫자를 입력해주세요 : ");
         List<Integer> guess = inputGuess();
+        String result = checkGuess(answer, guess);
 
         return;
     }
@@ -41,6 +42,36 @@ public class Application {
         }
         return guess;
     }
+
+    public static String checkGuess(List<Integer> answer, List<Integer> guess) throws IOException {
+        String check_result = "";
+        int ball = 0;
+        int strike = 0;
+
+        if(answer.equals(guess)){
+            check_result = "3스트라이크";
+            return check_result;
+        }else{
+            int[] cntArray = new int[2];
+            cntArray = new int[2];
+            ball = cntArray[0];
+            strike = cntArray[1];
+        }
+
+        //출력문 구현
+        if(ball==0&&strike==0){
+            check_result = "낫싱";
+        }else if(ball==0){
+            check_result = strike+"스트라이크";
+        }else if(strike==0){
+            check_result = ball+"볼";
+        }else{
+            check_result = ball+"볼 "+strike+"스트라이크";
+        }
+        return check_result;
+    }
+
+    public static
 
 
 
