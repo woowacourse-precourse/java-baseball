b042f8e0500e9ceba16ab6ef276d71ba9e10b4ad
TaeMin <xoals3094@naver.com>
2022-11-06 19:02:18 +0900

    feat : isValidData 메소드 구현
    유저가 입력한 정수값이 유효한지 판별하는 메소드 입니다
    disassembleDigitNumber 메소드를 이용해 각 자리의 정수를 획득하고 리스트의 크기를 확인합니다
     - 3이 아니면 유효값이 아님
    각 자리의 정수 중 중복되는 값이 있는지 확인합니다
     - duplicatedList에 값이 존재한다면 해당 숫자는 유효하지 않은 숫자입니다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,35 @@
 package baseball;
+
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
 
-    public static List<Integer> toPlaceValue(List<Integer> disassembledNumber){
+    public static boolean isValidData(int userInput) {
+        List<Integer> disassembledNumber = disassembleDigitNumber(userInput);
+        if (disassembledNumber.size() != 3) {
+            return false;
+        }
+
+        List<Integer> duplicatedList = new ArrayList<>();
+        for (int number : disassembledNumber) {
+            if (duplicatedList.contains(number)) {
+                return false;
+            }
+            duplicatedList.add(number);
+        }
+
+        return true;
+    }
+
+    public static List<Integer> toPlaceValue(List<Integer> disassembledNumber) {
         List<Integer> placeValue = new ArrayList<Integer>();
         int digit = 1;
-        for(int number: disassembledNumber){
+        for (int number : disassembledNumber) {
             placeValue.add(number * digit);
             digit *= 10;
         }
@@ -17,16 +37,16 @@ public class Application {
         return placeValue;
     }
 
-    public static List<Integer> disassembleDigitNumber(int number){
+    public static List<Integer> disassembleDigitNumber(int number) {
         List<Integer> disassembledNumber = new ArrayList<Integer>();
-        for(; number > 0; number /= 10){
+        for (; number > 0; number /= 10) {
             disassembledNumber.add(number % 10);
         }
 
         return disassembledNumber;
     }
 
-    public static int getRandomThreeDigitNumber(){
+    public static int getRandomThreeDigitNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -36,7 +56,7 @@ public class Application {
         }
 
         int randomValue = 0;
-        for(int i = 0, digit = 1; digit <= 100; i++, digit *= 10){
+        for (int i = 0, digit = 1; digit <= 100; i++, digit *= 10) {
             randomValue += computer.get(i) * digit;
         }
         return randomValue;
