f491ee5a694b932591259eab8c030d76211fe868
yoon <beatus96@naver.com>
2022-11-06 15:04:03 +0900

    test : 추가 컴퓨터수 생성하기 기능 테스트
    1) 컴퓨터 수 범위 테스트: 생성된 컴퓨터 수가 1-9 사이의 값인지 확인하기
    2) 컴퓨터 수 서로 다른 수 테스트 : 생성된 컴퓨터 수가 서로 다른 3 자리 수인지 확인하기

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,6 +3,11 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static baseball.Application.createComputerNumber;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,5 +36,24 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    @Test
+    void 컴퓨터수_범위_테스트() {
+        //when
+        List<Integer> computerNum = Application.createComputerNumber();
+        //then
+        assertThat(computerNum.stream().allMatch(v->1<=v && v<=9)).isTrue();
+    }
+    @Test
+    void 컴퓨터수_서로다른수_테스트() {
+        //given
+        Set<Integer> numbers = new HashSet<>();
+        //when
+        List<Integer> computerNum = Application.createComputerNumber();
+        computerNum.stream().
+                forEach(v->numbers.add(v));
+        //then
+        assertThat(numbers.size()).isEqualTo(3);
     }
 }
