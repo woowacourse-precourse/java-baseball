825aae272f0ed2cd50e7de3b12aba7b84ef769aa
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-06 16:38:41 +0900

    test : 중복룰 검증 기능 메서드 테스트 구현

--- src/test/java/baseball/controller/computer/ComputerErrorTest.java
+++ src/test/java/baseball/controller/computer/ComputerErrorTest.java
@@ -20,4 +20,14 @@ public class ComputerErrorTest {
 		assertThat(ComputerError.hasDuplication(list2, 7)).isTrue();
 		assertThat(ComputerError.hasDuplication(list3, 6)).isFalse();
 	}
+
+	@DisplayName("난수 생성 후 중복 룰을 따르는지 확인한다")
+	@Test
+	void canVerifyDuplicationRule() {
+		List<Integer> list1 = List.of(3, 3);
+		List<Integer> list2 = List.of(1, 7);
+		assertThat(ComputerError.followDuplicationRule(list1, 3)).isFalse();
+		assertThat(ComputerError.followDuplicationRule(list2, 5)).isTrue();
+	}
+
 }
