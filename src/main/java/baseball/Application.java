304d68274842dd16680e704dd6e5f3dd938480d3
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-06 19:55:50 +0900

    feat: 랜덤한 숫자 3개를 받아 정답을 만드는 기능 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.lang.IllegalArgumentException;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         startMessage();
+        String answer = createAnswer();
     }
     private static void startMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    private static String createAnswer () {
+        List<String> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(Integer.toString(randomNumber));
+            }
+        }
+        return String.join("",computer);
+    }
+
 }
