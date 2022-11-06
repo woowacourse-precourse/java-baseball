9418347779a83e2b80045cb916d6d8bf5e6584ea
Be-student <presentsong@naver.com>
2022-11-07 01:12:08 +0900

    feat(Strike): Strike의 범위를 enum으로 관리

---
+++ src/main/java/baseball/domain/Strike.java
@@ -0,0 +1,25 @@
+package baseball.domain;
+
+import java.util.Arrays;
+
+public enum Strike {
+
+    ZERO(0),
+
+    ONE(1),
+
+    TWO(2),
+
+    THREE(3);
+    private final int value;
+
+    Strike(int value) {
+        this.value = value;
+    }
+
+    public static Strike from(int value) {
+        return Arrays.stream(Strike.values())
+                .filter(it -> it.value == value)
+                .findFirst().orElseThrow(IllegalArgumentException::new);
+    }
+}
