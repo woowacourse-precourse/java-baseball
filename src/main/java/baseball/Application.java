3e7a58e058149272d204e97f0967aa0229199df6
UngHeung <t01047377477@gmail.com>
2022-11-07 02:52:42 +0900

    refactor : (baseball) 예외처리 코드 수정 및 코드 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,9 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
@@ -12,15 +14,21 @@ public class Application {
 	}
 
 	private static void playGame() {
-
 		String message = "숫자 야구 게임을 시작합니다.";
 		System.out.println(message);
 
 		List<Integer> batterSelectList = batterNumberList();
 
 		while(true) {
+			List<Integer> pitcherInputList = pitcherNumberList();
+			
+			if(pitcherInputList == null) {
+				throwException();
+				return;
+			}
+			
 
-			message = roundResult(batterSelectList, pitcherNumberList());
+			message = roundResult(batterSelectList, pitcherInputList);
 			System.out.println(message);
 
 			if(message.equals("3스트라이크")) {
@@ -28,15 +36,14 @@ public class Application {
 				System.out.println(message);
 				break;
 			}
-
 		}
 
 		selectGameStart();
 	}
 
 	private static void selectGameStart()  {
-
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+		String message = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+		System.out.println(message);
 		String select = Console.readLine();
 
 		if(select.equals("1")) {
@@ -50,7 +57,6 @@ public class Application {
 	}
 
 	private static List<Integer> batterNumberList() {
-
 		List<Integer> computerSelectList = new ArrayList<>();
 
 		while(computerSelectList.size() < 3) {
@@ -62,49 +68,60 @@ public class Application {
 			}
 
 		}
-
 		return computerSelectList;
 	}
 
 	private static List<Integer> pitcherNumberList() {
-
 		List<Integer> userSelectList = new ArrayList<>();
-
-		System.out.print("숫자를 입력해주세요 : ");
-		int userSelect = Integer.parseInt(Console.readLine());
-
-		userSelectList.add(userSelect / 100);
-		userSelectList.add(userSelect % 100 / 10);
-		userSelectList.add(userSelect % 10);
-
-		for(int i = 0; i < userSelectList.size(); i++) {
-			for(int j = i + 1; j < userSelectList.size(); j++) {
-				checkValidate(userSelectList.get(i), userSelectList.get(j));
-			}
+		
+		String message = "숫자를 입력해주세요 : ";
+		System.out.print(message);
+		String userInput = Console.readLine();
+		
+		if(!checkValidate(userInput)) {
+			return null;
 		}
+
+		int userInputInt = Integer.parseInt(userInput);
+		userSelectList.add(userInputInt / 100);
+		userSelectList.add(userInputInt % 100 / 10);
+		userSelectList.add(userInputInt % 10);
 
 		return userSelectList;
 	}
 
-	private static void checkValidate(int firstNumber, int secondNumber) {
-		if(firstNumber == secondNumber) {
-			try {
-				throw new IllegalArgumentException();
-			} catch(IllegalArgumentException e) {
-				e.printStackTrace();
-				return;
-			}
+	private static boolean checkValidate(String userInput) {
+		if(userInput.length() != 3) {
+			return false;
+		}
+		
+		Set<Character> userInputSet = new HashSet<>();
+		
+		for(int i = 0; i < userInput.length(); i++) {
+			userInputSet.add(userInput.charAt(i));
+		}
+		
+		if(userInputSet.size() != 3) {
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private static void throwException() {
+		try {
+			throw new IllegalArgumentException();
+		} catch(IllegalArgumentException e) {
+			e.printStackTrace();
 		}
 	}
 
 	private static String roundResult(List<Integer> batterNumberList ,List<Integer> pitcherNumberList) {
-
 		String result = "";
 
 		int countStrike = 0, countBall = 0;
 
 		for(int i = 0; i < 3; i++) {
-
 			if(batterNumberList.get(i) != pitcherNumberList.get(i)) {
 
 				if(batterNumberList.contains(pitcherNumberList.get(i))) {
@@ -116,11 +133,9 @@ public class Application {
 			if(batterNumberList.get(i) == pitcherNumberList.get(i)) {
 				countStrike++;
 			}
-
 		}
 
 		if(countBall > 0) {
-
 			String space = "";
 
 			if(countStrike > 0) {
@@ -140,5 +155,4 @@ public class Application {
 
 		return result;
 	}
-
 }
