9f463ea9be280f0cf772683b23f4ee22674f54b0
Ilchul Kim <huj0159@gmail.com>
2022-11-07 05:08:12 +0900

    feat: start baseball game

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -3,6 +3,37 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameController {
+    public void start() {
+
+        BaseBallGame baseBallGame = new BaseBallGame();
+
+        baseBallGame.useGeneratedAnswer();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+
+            System.out.print("숫자를 입력해주세요 : ");
+            String number = Console.readLine();
+
+            BaseballGameResultDto gameResult = baseBallGame.submitAnswer(number);
+
+            System.out.println(printResult(gameResult));
+
+            if (gameResult.getIsCorrect()) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int userContinue = Integer.parseInt(Console.readLine());
+                if (userContinue == 1) {
+                    baseBallGame = new BaseBallGame();
+                    baseBallGame.useGeneratedAnswer();
+                    continue;
+                }
+                if (userContinue == 2) {
+                    break;
+                }
+            }
+        }
+    }
 
 
     public String printResult(BaseballGameResultDto baseballResultDto) {
