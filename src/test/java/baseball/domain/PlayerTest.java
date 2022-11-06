2772e0b5001fc9deecf9d77fefbd96a9cc705e71
Jueun <jueun000101@gmail.com>
2022-11-06 12:17:03 +0900

    test(PlayerTest): test string's each char to integer list method

--- src/test/java/baseball/domain/PlayerTest.java
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -1,22 +1,30 @@
 package baseball.domain;
 
 import baseball.util.Number;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PlayerTest {
-    private Player player;
+    Player player = new Player();
 
-    @BeforeEach
-    protected final void init() {
-        player = new Player();
+    @Test
+    void convertInput_메서드_사용해_문자열을_정수리스트로_변환() {
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(5);
+        list.add(9);
+        assertThat(player.convertInput("159")).isEqualTo(list);
     }
 
     @Nested
     class InputValidationTest {
+
         @Test
         void checkInput_메서드_사용시_입력된_문자열_길이가_길거나_짧을_때_예외_발생() {
             assertThatThrownBy(() -> player.checkInput("1234"))
