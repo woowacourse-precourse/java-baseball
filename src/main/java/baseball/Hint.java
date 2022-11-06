b76dda1f7978b277a7101d9464a2a4f4fb9d59e7
xxeol2 <jhj01177@naver.com>
2022-11-06 23:09:05 +0900

    test: 힌트 출력 테스트 구현

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -1,5 +1,9 @@
 package baseball;
 
+import static baseball.constant.Hint.BALL;
+import static baseball.constant.Hint.NOTHING;
+import static baseball.constant.Hint.STRIKE;
+
 public class Hint {
 
     private int strike = 0;
@@ -11,6 +15,28 @@ public class Hint {
 
     public int getBall() {
         return ball;
+    }
+
+    public boolean isThreeStrike() {
+        return strike == 3;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (ball > 0) {
+            stringBuilder.append(ball).append(BALL);
+        }
+        if (strike > 0) {
+            if (stringBuilder.length() > 0) {
+                stringBuilder.append(" ");
+            }
+            stringBuilder.append(strike).append(STRIKE);
+        }
+        if (stringBuilder.length() == 0) {
+            stringBuilder.append(NOTHING);
+        }
+        return stringBuilder.toString();
     }
 
     public static HintBuilder builder() {
