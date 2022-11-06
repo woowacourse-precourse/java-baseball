9c5d1abcc79b19d5fde2ff8d68a5a905732ebc94
wugawuga <dnr3655@naver.com>
2022-11-07 01:16:48 +0900

    test: 사용자 정답 리스트 각 자리의 수가 1 ~ 9가 아닐 때 예외

--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -1,12 +1,15 @@
 package baseball;
 
+import static baseball.Constants.RANGE_OF_BALL_EXCEPTION;
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class BaseballGameTest {
     public static BaseballGame game;
@@ -32,5 +35,19 @@ class BaseballGameTest {
         String actual = (String) makeResultMethod.invoke(game, ball, strike);
 
         assertThat(actual).isEqualTo(result);
+    }
+
+    @ParameterizedTest
+    @DisplayName("정답 각 자리의 수가 1 ~ 9가 아닐 때 예외")
+    @ValueSource(ints = {0, -1, 10})
+    void validateRange(int ball) throws Exception {
+        Method validateMethod = BaseballGame.class.getDeclaredMethod("validateRangeOfBall", int.class);
+        validateMethod.setAccessible(true);
+
+        try {
+            validateMethod.invoke(game, ball);
+        } catch (InvocationTargetException e) {
+            assertThat(e.getCause().getMessage()).isEqualTo(RANGE_OF_BALL_EXCEPTION);
+        }
     }
 }
