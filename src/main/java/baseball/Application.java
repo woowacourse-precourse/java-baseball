6aa131c54e5efc2dcb510b7e70d772a8a1ae2982
tlsqj <rnjstlsqja@inha.edu>
2022-11-06 21:12:54 +0900

    feat(Application): 상대방이 생각하고 있는 랜덤한 숫자 생성 함수 구현
    Application에 새로운 함수 추가:
    -상대방이 생각하고 있는 랜덤한 숫자 생성 함수 구현
    -main 함수에 새로운 랜덤한 숫자 생성할 조건 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,36 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+    public static String NewGameNumber(){
+        String targetNumber = "";
+        while(targetNumber.length() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if (!targetNumber.contains(Integer.toString(randomNumber))){
+                targetNumber += Integer.toString(randomNumber);
+            }
+        }
+
+        return targetNumber;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        boolean newNumberFlag = true;
+        String gameNumber = null;
+
+        System.out.println("숫자 야구 게임을 시작합니다.\n");
+
+        while(true){
+            if (newNumberFlag == true) {
+                gameNumber = NewGameNumber();
+            }
+
+            newNumberFlag = false;
+        }
     }
 }
