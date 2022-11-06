1a31687428bad7fb660eeb7938c4ea331c42758b
jihong <clara5091@naver.com>
2022-11-06 21:57:20 +0900

    feat: 숫자 입력 시 올바른 수인지 확인하는 기능
    중복되지 않는 수 3개인 지 확인

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseballGame {
     private Numbers randomNumbers;
+    private Numbers predictNumbers;
 
     private void pickRandomNumbers() {
         randomNumbers = new Numbers();
@@ -12,8 +13,16 @@ public class BaseballGame {
         }
     }
 
+    private void getPredictNumbers(int number) {
+        predictNumbers = new Numbers();
+        while (number > 0) {
+            predictNumbers.add(number % 10);
+            number /= 10;
+        }
+        predictNumbers.validateNumbers();
+    }
+
     public void run() {
         pickRandomNumbers();
-
     }
 }
--- src/main/java/baseball/Numbers.java
+++ src/main/java/baseball/Numbers.java
@@ -10,6 +10,15 @@ public class Numbers {
         numbers = new ArrayList<>();
     }
 
+    public void validateNumbers() {
+        if (numbers.size() != 3) {
+            throw new IllegalArgumentException("입력된 수가 지정 갯수 초과입니다.");
+        }
+        if (numbers.stream().distinct().toArray().length != 3) {
+            throw new IllegalArgumentException("중복된 수가 존재합니다.");
+        }
+    }
+
     public void add(int number) {
         if (!numbers.contains(number)) {
             numbers.add(number);
