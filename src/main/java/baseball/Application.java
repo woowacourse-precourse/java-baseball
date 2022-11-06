1642d55edef8ce671ea569b6fd8ebbe1e3f27ab5
Donguk <95donguk.dev@gmail.com>
2022-11-07 01:30:18 +0900

    feat: add number comparison function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,16 @@
 package baseball;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
 public class Application {
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTING = "낫싱";
+
+    private Map<String, Integer> result = new HashMap<>();
 
     public static void main(String[] args) {
         Application application = new Application();
@@ -15,5 +25,25 @@ public class Application {
         computer.generateRandomNumber();
         GameTextPrinter.printUserInputText();
         user.inputNumber();
+        compareTwoNumbers(computer.getNumber(), user.getNumber());
+    }
+
+    private void compareTwoNumbers(List<Integer> computerNumber, List<Integer> userNumber) {
+        int sameDigitCount = Math.toIntExact(userNumber.stream().filter(computerNumber::contains).count());
+        int strikeCount = getStrikeCount(computerNumber, userNumber);
+        int ballCount = sameDigitCount - strikeCount;
+
+        result.put(BALL, ballCount);
+        result.put(STRIKE, strikeCount);
+    }
+
+    private int getStrikeCount(List<Integer> computerNumber, List<Integer> userNumber) {
+        int strikeCount = 0;
+        for (int index = 0; index < computerNumber.size(); index++) {
+            if (Objects.equals(computerNumber.get(index), userNumber.get(index))) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
     }
 }
