bd1f9aeceb2ce45c56baba14609c248800a7e2d7
hectick <chyeon1223@cau.ac.kr>
2022-11-06 12:09:35 +0900

    feat : 컴퓨터가 특정 숫자를 포함하고 있는지 판단하는 기능 추가

--- src/main/java/baseball/Rival.java
+++ src/main/java/baseball/Rival.java
@@ -6,11 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Rival {
+    private final int LENGTH_OF_NUMBER = 3;
     private List<Integer> number;
 
     public void createNumber(){
         number = new ArrayList<>();
-        while(number.size() < 3){
+        while(number.size() < LENGTH_OF_NUMBER){
             int randomNumber = Randoms.pickNumberInRange(1,9);
             if(!number.contains(randomNumber)){
                 number.add(randomNumber);
@@ -20,6 +21,15 @@ public class Rival {
 
     public List<Integer> getNumber(){
         return number;
+    }
+
+    public boolean contains(int n){
+        for(int i = 0; i < LENGTH_OF_NUMBER; i++){
+            if(number.get(i) == n){
+                return true;
+            }
+        }
+        return false;
     }
 
 }
