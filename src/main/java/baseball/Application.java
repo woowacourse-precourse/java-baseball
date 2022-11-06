6e21792d3c2c368f8db7c7bf84d982a1c1c34c17
NoseSSG <dmlcks1998@naver.com>
2022-11-06 23:25:43 +0900

    랜덤 3자리 숫자를 생성하는 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,27 @@
 package baseball;
 
+import java.util.*;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    static List<Integer> make_rand_num(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        //test
+
     }
+
 }
