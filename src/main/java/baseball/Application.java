a5589e23735eecacfdf258272eed8aaf8242d69f
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-07 00:09:37 +0900

    feat(application): 의존성 주입 및 viewer 실행 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import custom.controller.BaseBallController;
+import custom.service.BaseBallService;
+import custom.table.Table;
+import custom.viewer.Viewer;
+
 public class Application {
+
+    private static Viewer viewer;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        dependencyInjection();
+        viewer.run();
+    }
+
+    private static void dependencyInjection() {
+        Table table = Table.init("000");
+        BaseBallService baseBallService = new BaseBallService(table);
+        BaseBallController baseBallController = new BaseBallController(baseBallService);
+        viewer = new Viewer(baseBallController);
     }
 }
