ee69929d8b1e2700abc39323098a1c448e180280
namtndus <rnrmf0721@gmail.com>
2022-11-06 20:26:58 +0900

    feat(randomNumberCreate): 함수의 기능 코드 작성
    "세자리수의 난수 생성" 코드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     public static void baseballGameStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public static List<Integer> randomNumberCreate(){
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return computerNumbers;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         baseballGameStart();
+        randomNumberCreate();
     }
 }
