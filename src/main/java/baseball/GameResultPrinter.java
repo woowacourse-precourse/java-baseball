70fc84a3b8866e150c9c4f74d1074a3e2e53e863
choiejae <ejae8319@gmail.com>
2022-11-06 20:12:28 +0900

    feat: GameResultPrinter 낫싱 테스트 구현

--- src/main/java/baseball/GameResultPrinter.java
+++ src/main/java/baseball/GameResultPrinter.java
@@ -1,5 +1,15 @@
 package baseball;
 
+import java.util.List;
+
 public class GameResultPrinter {
 
+    public String convertGameResult(List<Integer> gameResult) {
+        String converter = "";
+        if (gameResult.get(2) == 0) {
+            return "낫싱";
+        }
+
+        return "";
+    }
 }
