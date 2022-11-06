afeb280e497b35748fe939d164320420c6acdfff
olrlobt <317tmdgjs@naver.com>
2022-11-06 21:51:21 +0900

    feat: 3. 입력받은 값 저장 & 잘못된 값 예외처리

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -16,7 +16,34 @@ public class Player {
     }
 
     public void setPlayerNumber(String inputNumber) {
+        checkInputStyle(inputNumber);
+        setNumber(inputNumber);
+    }
 
+    public void checkInputStyle(String inputNumber) {
+        String REGEX = "^[1-9]*";
+
+        if (inputNumber.length() != 3 || !inputNumber.matches(REGEX)) {
+            inputError();
+        }
+    }
+
+    public void setNumber(String inputNumber) {
+        for (int i = 0; i < inputNumber.length(); i++) {
+            int unit = inputNumber.charAt(i) - '0';
+            checkOverlab(unit);
+            playerNumber.add(unit);
+        }
+    }
+
+    public void checkOverlab(int unit) {
+        if (playerNumber.contains(unit)) {
+            inputError();
+        }
+    }
+
+    public void inputError() {
+        throw new IllegalArgumentException();
     }
 }
 
