b974cd89315acbe3bcdb1105104b20a56498a4e9
LHS-11 <ghktjq1119@naver.com>
2022-11-06 17:21:43 +0900

    [Refactor] : OutputView 클래스에서 종료 메시지 상수로 선언

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,9 +5,10 @@ import baseball.model.Player;
 
 public class OutputView {
 
+    private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public void printGameEnd(){
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(END_MESSAGE);
     }
     public void printHint(String hint){
         System.out.println(hint);
