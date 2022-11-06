c76909435c143ba0bd1461a79bac3b2c874fab5f
eora21 <eora21@naver.com>
2022-11-06 23:52:34 +0900

    feat: 🎸 추가 입력값 3자리를 비교하는 메서드 작성
    입력값 3자리를 비교하여 같다면 스트라이크, 목표값에 포함된다면 볼을 판단하는 메서드를 작성

--- src/main/java/baseball/game/Referee.java
+++ src/main/java/baseball/game/Referee.java
@@ -15,7 +15,7 @@ public class Referee {
     public boolean isPerfectAnswer(List<Integer> targetNumbers, List<Integer> userNumbers) {
         validateNumbersLength(targetNumbers, userNumbers);
 
-        Map<Accuracy, Integer> judgeAccuracy = getJudgeAccuracy();
+        Map<Accuracy, Integer> judgeAccuracy = getJudgeAccuracy(targetNumbers, userNumbers);
         shout(judgeAccuracy);
 
         return judgeAccuracy.get(Accuracy.CORRECT_ANSWER) == PICK_COUNT;
@@ -25,5 +25,25 @@ public class Referee {
         if (targetNumbers.size() != PICK_COUNT || userNumbers.size() != PICK_COUNT) {
             throw new IllegalArgumentException("배열의 길이가 " + PICK_COUNT + "와 같지 않습니다");
         }
+    }
+
+    private Map<Accuracy, Integer> getJudgeAccuracy(List<Integer> targetNumbers, List<Integer> userNumbers) {
+        Map<Accuracy, Integer> judgeAccuracy = new HashMap<>();
+
+        for (int idx = 0; idx < targetNumbers.size(); idx++) {
+            int targetNumber = targetNumbers.get(idx);
+            int userNumber = userNumbers.get(idx);
+
+            if (targetNumber == userNumber) {
+                judge(judgeAccuracy, Accuracy.CORRECT_ANSWER);
+                continue;
+            }
+
+            if (targetNumbers.contains(userNumber)) {
+                judge(judgeAccuracy, Accuracy.SIMILAR_ANSWER);
+            }
+        }
+
+        return judgeAccuracy;
     }
 }
