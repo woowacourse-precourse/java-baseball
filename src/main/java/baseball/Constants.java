2c803e2bfbf0285ae12e5c2465cfafa28d27b403
이정우 <acg6138@naver.com>
2022-11-06 13:24:18 +0900

    refactor(Constants): 최대 스트라이크 수 추가

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final int INPUT_LENGTH = 3;
     public static final String RESTART = "1";
     public static final String END = "2";
+    public static final int MAX_STRIKE = 3;
 }
