084a6b62f2f1721e6a1d35c096655303ded3cb4c
rabbit0216 <rabbit1999k@gmail.com>
2022-11-07 02:41:43 +0900

    refactor: 게임 재시작 여부 묻는 함수 수정

--- src/main/java/baseball/Restart.java
+++ src/main/java/baseball/Restart.java
@@ -3,14 +3,15 @@ package baseball;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Restart {
+
     public static void restartGame(){
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        //System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String menu = readLine();
 
-        if(menu == "1"){
+        if(menu.equals("1")){
             Game.run();
         }
-        else if(menu == "2"){
+        else if(menu.equals("2")){
             System.out.println("게임 종료");
             return ;
         }
