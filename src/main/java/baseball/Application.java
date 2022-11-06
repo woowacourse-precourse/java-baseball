abb76519d6db24eeccd7523a44aaabc5780b0b40
jun <59019322+mrgentle1@users.noreply.github.com>
2022-11-06 17:25:01 +0900

    feat(입력): Input user number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,19 +1,59 @@
 package baseball;
 
+import java.lang.reflect.Array;
 import java.util.*;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     // TODO: 코드 구현
-    public static void main(String[] args) {
-        List<Integer> computer = new ArrayList<>();
+    public static ArrayList<Integer> generateNum(){
+        ArrayList<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
+        return computer;
+    }
+
+    public static ArrayList<Integer> userInput(){
+        String user_num = Console.readLine();
+        if(checkVal(user_num)) throw new IllegalArgumentException("Invalid input");
+
+        char[] playerNumArray = user_num.toCharArray();
+        ArrayList<Integer> playerNum = new ArrayList<>();
+
+        for(char num : playerNumArray){
+            playerNum.add(num - '0');
+        }
+        
+        return playerNum;
+    }
+
+    public static boolean checkVal(String num){
+        return num.length() != 3;
+    }
+
+
+
+    public static void run(){
+        ArrayList<Integer> computer = generateNum();
+        ArrayList<Integer> player = userInput();
+
+        System.out.println(computer);
+        System.out.println(player);
+
+//        System.out.println(getResult(computer, player));
+    }
+
+
+    public static void main(String[] args) {
+
+        run();
 
     }
+
+
 }
