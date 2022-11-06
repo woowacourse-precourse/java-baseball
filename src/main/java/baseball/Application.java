0cb028da2d4edafb6053b395cbd91daf660e19fd
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-06 21:54:23 +0900

    feat(Application) : finish DatabBase class, implement userInput method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,18 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.*;
 
 public class Application {
+    static int inputSize = 3;
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         DataBase data = new DataBase();
         RunGame game = new RunGame();
         data.setAnswer(game.answer);
+        data.setUserInput(game.userInput());
     }
-    static int inputSize = 3;
     static class RunGame {
         List<Integer> answer = new ArrayList<>();
         RunGame() {
@@ -21,8 +23,19 @@ public class Application {
                 }
             }
         }
+
+        public List<Integer> userInput() {
+            List<Integer> intUserInput = new ArrayList<>();
+            String[] stringUserInput = {};
+            stringUserInput = Console.readLine().split("");
+            for (String number : stringUserInput) {
+                intUserInput.add(Integer.parseInt(number));
+            }
+            return intUserInput;
+        }
     }
 }
+
 class DataBase {
     private List<Integer> answer = new ArrayList<>();
     private List<Integer> userInput = new ArrayList<>();
@@ -31,13 +44,25 @@ class DataBase {
     public void setAnswer(List<Integer> answer) {
         this.answer = answer;
     }
+    public List<Integer> getAnswer() {
+        return this.answer;
+    }
     public void setUserInput(List<Integer> userInput) {
         this.userInput = userInput;
+    }
+    public List<Integer> getUserInput() {
+        return this.userInput;
     }
     public void setStrike(int strike) {
         this.strike = strike;
     }
+    public int getStrike() {
+        return this.strike;
+    }
     public void setBall(int ball) {
         this.ball = ball;
+    }
+    public int getBall(){
+        return this.ball;
     }
 }
