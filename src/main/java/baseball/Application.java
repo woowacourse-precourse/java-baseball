a15ee4c4b3e72ac7210cebc137299e867bc3e22f
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-07 00:35:44 +0900

    feat(main): 게임 시작 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -19,6 +20,6 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
