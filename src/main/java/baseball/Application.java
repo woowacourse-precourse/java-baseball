e7a5adb1c1f067a61f8b296a5380b88a69cad3ca
htogether7 <htogether7@gmail.com>
2022-11-06 23:41:49 +0900

    사용자의 입력갑과 컴퓨터가 생성한 숫자를 비교하여 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,10 +14,14 @@ public class Application {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
         List<String> answer = makeAnswer();
-//        System.out.println(answer);
+        System.out.println(answer);
 //        System.out.println(answer.get(0).equals("1"));
+//        System.out.println(answer.get(1).equals("1"));
+//        System.out.println(answer.get(2).equals("1"));
         while (true) {
             List<String> playerInput = checkAvailable(Input());
+            List<Integer> compareResult = compareNumbers(answer, playerInput);
+            showResult(compareResult);
         }
 
     }
@@ -54,5 +58,41 @@ public class Application {
         }
 
         return numList;
+    }
+
+    public static List<Integer> compareNumbers(List<String> answer, List<String> playerInput) {
+        List<Integer> countList = new ArrayList<>(Arrays.asList(0,0,0));
+//        System.out.println(countList);
+        for (int index = 0; index < 3; index++) {
+            if (!answer.contains(playerInput.get(index))) {
+                countList.set(2, countList.get(2)+1);
+            } else if (answer.contains(playerInput.get(index))) {
+                if (playerInput.get(index).equals(answer.get(index))) {
+                    countList.set(0, countList.get(0)+1);
+                } else {
+                    countList.set(1, countList.get(1)+1);
+                }
+            }
+        }
+        return countList;
+    }
+
+    public static void showResult(List<Integer> countList) {
+        if (countList.get(2).equals(3)) {
+            System.out.println("낫싱");
+        } else {
+            if (countList.get(0).equals(3)) {
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            } else {
+                if (countList.get(0).equals(0)) {
+                    System.out.println(String.format("%d볼", countList.get(1)));
+                } else if (countList.get(1).equals(0)) {
+                    System.out.println(String.format("%d스트라이크", countList.get(0)));
+                } else {
+                    System.out.println(String.format("%d볼 %d스트라이크",countList.get(1), countList.get(0)));
+                }
+            }
+        }
     }
 }
