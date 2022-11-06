91222001bbafbe56144caac19d24097c5aac3647
woosung1223 <dntjd991223@naver.com>
2022-11-06 12:39:20 +0900

    refactor(RandomNumGenerator): 랜덤한 수를 생성하는 메소드명 변경, Util을 활용하도록 수정

--- src/main/java/baseball/RandomNumGenerator.java
+++ src/main/java/baseball/RandomNumGenerator.java
@@ -1,33 +1,31 @@
 package baseball;
 
+import baseball.utils.Util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+
 
 public final class RandomNumGenerator {
     private static final int START_INCLUSIVE = 1;
     private static final int END_INCLUSIVE = 9;
+
     private RandomNumGenerator() {
     }
 
     private static int makeRandomDigit() {
-        int test = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
-        return test;
+        return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
     }
-    public static int makeRandomNumWithGameDigits() {
-        HashSet<Integer> forDeleteDuplication = new HashSet<>();
-        while(forDeleteDuplication.size() < SystemConstant.GAME_DIGIT) {
+
+    public static int makeDistinctRandomNumWithSize(int sizeLimit) {
+        List<Integer> resultList = new ArrayList<>();
+        int size;
+        do {
             int randomDigit = makeRandomDigit();
-            forDeleteDuplication.add(randomDigit);
-        }
-        List<Integer> resultList = new ArrayList<>(forDeleteDuplication);
-        return resultList
-                .stream()
-                .reduce((x, y) -> x * 10 + y)
-                .get();
+            resultList.add(randomDigit);
+            size = Util.getDistinctCountInIntegerList(resultList);
+        } while (size < sizeLimit);
+        return Util.mergeDistinctIntegerInList(resultList);
     }
 }
