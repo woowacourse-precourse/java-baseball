e6c698cca27498635eacffa968a04fbf0a10f949
aud19 <aud19980518@gmail.com>
2022-11-06 18:58:22 +0900

    domain의 user, again 클래스

---
+++ src/main/java/baseball/domain/Again.java
@@ -0,0 +1,17 @@
+package baseball.domain;
+
+public class Again {
+    private int doTry;
+
+    public Again(int doTry) {
+        this.doTry = doTry;
+    }
+
+    public int getDoTry() {
+        return doTry;
+    }
+
+    public void setDoTry(int doTry) {
+        this.doTry = doTry;
+    }
+}
--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -1,17 +1,35 @@
 package baseball.domain;
 
-public class User {
-    private String userAnswer;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-    public User(String userAnswer) {
-        this.userAnswer = userAnswer;
+public class User {
+    final static int MAX_SIZE = 3;
+    private List<Integer> userAnswer;
+
+    public User() {
+        this.userAnswer = new ArrayList<>();
     }
 
-    public String getUserAnswer() {
+    public User(List<Integer> userAnswer) {
+        this.userAnswer.addAll(userAnswer);
+    }
+
+    public List<Integer> getUserAnswer() {
         return userAnswer;
     }
 
-    public void setUserAnswer(String userAnswer) {
-        this.userAnswer = userAnswer;
+    public void setUserAnswer(List<Integer> userAnswer) {
+        this.userAnswer.addAll(userAnswer);
+    }
+
+    public User(int answer) {
+        this.userAnswer = new ArrayList<>();
+        while(answer > 0) {
+            userAnswer.add(answer % 10);
+            answer /= 10;
+        }
+        Collections.reverse(userAnswer);
     }
 }
