a9b90df91bdffa2f16320f5b642210cf472f4957
chosunghyun18 <krismon@naver.com>
2022-11-06 17:15:58 +0900

    feat(player): add the player input
    - 기능 목록 1.1, 1.2 구현
    -1.1 사용자로 부터 숫자를 입력 받는 기능
    -1.2 선수의 3자리수를 리스트로 반환하는 기능

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Player {
+    private final String playerNumbers;
+    private final List<Integer> playerNumbersList;
+    public Player() {
+        this.playerNumbers = inputNumbers();
+        this.playerNumbersList =setPlayerNumberList(this.playerNumbers);
+    }
+    public String inputNumbers() {
+        String userInput = Console.readLine();
+        System.out.println(userInput);
+        return userInput;
+    }
+    private List<Integer> setPlayerNumberList(String inputNumber)
+    {   List<Integer> inputNumberList = Arrays.stream(inputNumber.split("",inputNumber.length())).map(Integer::parseInt).collect(Collectors.toList());
+        return inputNumberList;
+    }
+    public List<Integer> getPlayerNumbersList(){
+        return playerNumbersList;
+    }
+
+}
