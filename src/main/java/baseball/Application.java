5cdd1edcde81a91f6d9cdf9d389a410ddd5cb66e
gwj0421 <gwj0421@gmail.com>
2022-11-06 22:58:58 +0900

    feat: 요구사항 구현을 위한 함수의 기본구조 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,42 @@
 package baseball;
 
 public class Application {
+
+    private void start_game() {
+        // 기능 요구사항 1번
+    }
+    private void make_RandomNum() {
+        // 기능 요구사항 2번
+    }
+
+    private void input_PlayerNum() {
+        // 기능 요구사항 3번
+    }
+
+    private void get_BallAndStrikeCount() {
+        // 기능 요구사항 4번
+    }
+
+    private void progress_GameRule() {
+        // 기능 요구사항 5번
+    }
+
+    private void close_game() {
+        // 기능 요구사항 6번
+    }
+
+    private void play() {
+        start_game();
+        make_RandomNum();
+        input_PlayerNum();
+        get_BallAndStrikeCount();
+        progress_GameRule();
+        close_game();
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application application = new Application();
+        application.play();
+
     }
 }
