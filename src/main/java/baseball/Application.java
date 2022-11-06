a11c80f0d065c39570080eecd803f14b99e3863d
dydtmd6801 <dydtmd6801@naver.com>
2022-11-06 10:12:13 +0900

    refactor(PlayGame): PlayGame 생성자 추가
    PlayGame 생성자 추가로 기능 구현 다시 시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,7 +14,7 @@ public class Application {
         List<Integer> computerNumber = prepareGame.setComputerNumber();
         String stringUserNumber = prepareGame.setUserNumber();
         List<Integer> userNumber = prepareGame.userNumberToList(stringUserNumber);
-        PlayGame playGame = new PlayGame();
-        playGame.compareNumberForStrike(computerNumber, userNumber);
+        PlayGame playGame = new PlayGame(computerNumber, userNumber);
+        playGame.compareNumber();
     }
 }
--- src/main/java/baseball/PlayGame.java
+++ src/main/java/baseball/PlayGame.java
@@ -3,16 +3,16 @@ package baseball;
 import java.util.List;
 
 public class PlayGame {
-    public int compareNumberForStrike(List<Integer> computerNumber, List<Integer> userNumber){
-        int strike = 0;
+    private int strike;
+    private int ball;
+    private List<Integer> computerNumber;
+    private List<Integer> userNumber;
 
-        for(int digit = 0; digit < computerNumber.size(); digit++){
-            if(computerNumber.get(digit) == userNumber.get(digit)){
-                computerNumber.remove(digit);
-                userNumber.remove(digit);
-                strike++;
-            }
-        }
-        return strike;
+    PlayGame(List<Integer> setComputerNumber, List<Integer> setUserNumber){
+        this.computerNumber = setComputerNumber;
+        this.userNumber = setUserNumber;
+    }
+
+    public void compareNumber(){
     }
 }
