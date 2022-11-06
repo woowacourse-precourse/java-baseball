d56bcfcf074bf4d542fe3ace1f2a4d93d345733f
hectick <chyeon1223@cau.ac.kr>
2022-11-06 09:56:30 +0900

    feat : 세자리 숫자를 입력받는 기능 추가

--- src/main/java/baseball/baseballGame.java
+++ src/main/java/baseball/baseballGame.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
+    private final int LENGTH_OF_NUMBER = 3;
     private Rival computer;
+    private List<Integer> player;
 
     BaseballGame(){
         computer = new Rival();
@@ -9,6 +16,18 @@ public class BaseballGame {
 
     public void play(){
         computer.createNumber();
+        getPlayerNumber();
+    }
+
+    private void getPlayerNumber(){
+        player = new ArrayList<>();
+        String input = Console.readLine();
+        int number = Integer.parseInt(input);
+        for(int i = LENGTH_OF_NUMBER-1; i >= 0; i--) {
+            int digit = number % 10;
+            number = number / 10;
+            player.add(0, digit);
+        }
     }
 
 }
