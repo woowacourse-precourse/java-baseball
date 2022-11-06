7d22fe448cd8e403b520160226ac8ad91fdfb089
tjddnr7760 <tjddnr7760@naver.com>
2022-11-06 16:37:22 +0900

    feat : View클래스 생성자, final변수, 필요한 함수 설정

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,6 +1,33 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class View {
+    private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_SENTNECE = "숫자를 입력해주세요 : ";
+    private static final String CORRECT_SENTENCE = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    private static final String RESTART_SENTENCE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String EXIT_SENTENCE = "게임 종료";
+
     View() {}
 
+    void gameStartText() {
+
+    }
+
+    int numberInputText() {
+
+    }
+
+    void gameResultOutputText(String text) {
+
+    }
+
+    int victoryOutputText() {
+
+    }
+
+    void gameEnd() {
+
+    }
 }
