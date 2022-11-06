cfbe7970ac7f5d168edfce5887771d6b25562269
bellljun3395 <195850@jnu.ac.kr>
2022-11-06 14:59:41 +0900

    refactor(IntInputNumber): 구현 IntInputNumber
    - input 값을 int로 변환
    - input 값이 int인지 검증

--- src/main/java/baseball/input/InputConsole.java
+++ src/main/java/baseball/input/InputConsole.java
@@ -3,14 +3,10 @@ package baseball.input;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputConsole implements InputAble {
-    private final String ONLY_NUMBER = "숫자만 입력할 수 있습니다";
     @Override
     public int acceptInt() {
-        try {
-            String readLine = Console.readLine();
-            return Integer.parseInt(readLine);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(ONLY_NUMBER);
-        }
+        String readLine = Console.readLine();
+        IntInputNumber intInputNumber = new IntInputNumber(readLine);
+        return intInputNumber.getIntInputNumber();
     }
 }
