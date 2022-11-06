8b8cc26287fe25d1b83cb43eb49d8492e5cfba4d
nuyh <hjaehyun25@gmail.com>
2022-11-06 17:52:01 +0900

    feat(Baseball): Baseball 엔티티 추가(필드 변수, 도메인 로직 생성)

---
+++ src/main/java/baseball/domain/Baseball.java
@@ -0,0 +1,53 @@
+package baseball.domain;
+
+import java.util.stream.IntStream;
+
+public class Baseball {
+    //result[0]: 볼,    result[1]: 스트라이크
+    private int[] result;
+    private final Computer computer;
+
+    public Baseball(Computer computer) {
+        this.computer = computer;
+    }
+
+    public String getAnswer() {
+        return computer.getAnswer();
+    }
+
+    public int[] getResult() {
+        return result;
+    }
+
+    /**
+     * 도메인 로직
+     */
+    public void setBaseballGame() {
+        computer.setAnswerByRandom();
+    }
+
+    public void comparingResult(String baseballNumber) {
+        int contains = countContains(baseballNumber);
+        int strikes = countStrike(baseballNumber);
+
+        result[0] = contains - strikes;
+        result[1] = strikes;
+    }
+
+    private int countContains(String baseballNumber) {
+        String answer = getAnswer();
+
+        return (int) baseballNumber.chars()
+                .filter(o -> answer.contains(String.valueOf(o)))
+                .count();
+    }
+
+    private int countStrike(String baseballNumber) {
+        char[] answer = getAnswer().toCharArray();
+        char[] input = baseballNumber.toCharArray();
+
+        return (int) IntStream.range(0, 3)
+                .filter(o -> answer[o] == input[o])
+                .count();
+    }
+}
