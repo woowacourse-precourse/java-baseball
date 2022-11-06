4954d6a57be4b6d34194312d24e864bc4b030a73
yangjiyoung <0112jyoung@naver.com>
2022-11-06 11:02:54 +0900

    refactor: Add static to use

--- src/main/java/baseball/utils/RandomUtils.java
+++ src/main/java/baseball/utils/RandomUtils.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomUtils {
-    public List<Integer> makeRandomList(){
+    public static List<Integer> makeRandomList(){
         List<Integer> computer =new ArrayList<>();
 
         while(computer.size()<3){
