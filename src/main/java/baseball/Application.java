e611cae1553c38a6c0c30f2cf862530e8ef7966f
박준용 <jo101044@naver.com>
2022-11-06 13:13:02 +0900

    스트라이크, 볼 계산 메소드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,30 +1,63 @@
 package baseball;
 
-import java.util.Random;
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Scanner sc = new Scanner(System.in);
+
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println("숫자를 입력해주세요 : ");
-        int answerNum = createAnswer();
+        String answerNum = createAnswer();
         System.out.println(answerNum);
+
+        int endPoint = 0;
+        while(endPoint != 2){
+            System.out.println("숫자를 입력해주세요 : ");
+            String inputNum = sc.next();
+            int strike = countStrike(answerNum, inputNum);
+            int ball = countBall(answerNum, inputNum, strike);
+
+        }
+
+
         sc.close();
     }
 
-    private static int createAnswer() {
-        int min = 1;
-        int max = 9;
 
-        StringBuilder answer = new StringBuilder();
-        Random random = new Random();
+
+    private static int countBall(String answerNum, String inputNum, int strike) {
+        int count = 0;
 
         for (int i = 0; i < 3; i++) {
-            answer.append((random.nextInt(max)+min));
+            if(answerNum.contains(String.valueOf(inputNum.charAt(i)))){
+                count++;
+            }
         }
 
-        return Integer.parseInt(answer.toString());
+        return count - strike;
+    }
+
+    private static int countStrike(String answerNum, String inputNum) {
+        int count = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if(answerNum.charAt(i) == inputNum.charAt(i)){
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private static String createAnswer() {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < 3; i++) {
+            answer.append(Randoms.pickNumberInRange(1,9));
+        }
+
+        return answer.toString();
     }
 }
