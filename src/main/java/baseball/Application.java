fc5237a8d76bcf57e1dd2c7b6921a612c0389764
5witchkr <hob4419@gmail.com>
2022-11-07 02:12:51 +0900

    feat:(Application): 프로그램 실행기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Integer NumberSize = 3;
+
+        GameController controller = new GameController();
+        controller.programStart(NumberSize);
     }
+
 }
