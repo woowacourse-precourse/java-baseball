c302c4df28511e293da13e38f6d88adbd325b5a4
JUHA <khabh@naver.com>
2022-11-06 12:23:45 +0900

    refactor(Computer): 삼진 아웃 여부 확인하는 멤버 변수 추가
    플레이어에게 힌트를 제공할 때 삼진 아웃이 발생했는지 미리 확인하여 Game 클래스에서 게임이 종료됐는지 확인하는 불필요한 과정을 삭제
    
    BREAKING CHANGE: - isStruckOut 변수 추가
    - 힌트 제공 메서드 인자를 Player로 변경
    - 힌트 제공 시 strikeCount가 3인지 확인

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,24 +1,35 @@
 package baseball.domain;
 
+import baseball.view.Output;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Computer {
     private final static int STRIKE = 0;
     private final static int BALL = 1;
+    private final static int THREE_STRIKES = 3;
 
     private HintGenerator hintGenerator;
+    private boolean isStruckOut;
 
-    public void setStrikeNumbers() {
+    public void setForNewGame() {
+        isStruckOut = false;
         ArrayList<Integer> strikeNumbers = new StrikeNumbersGenerator().generateStrikeNumbers();
-        this.hintGenerator = new HintGenerator(strikeNumbers);
+        hintGenerator = new HintGenerator(strikeNumbers);
     }
 
-    public int provideHint(ArrayList<Integer> playerGuess) {
+    public void  provideHintTo(Player player) {
+        ArrayList<Integer> playerGuess = player.guessNumbers();
         HashMap<Integer, Integer> hint = hintGenerator.generateHint(playerGuess);
         int strikeCount = hint.get(STRIKE);
-        Output.printResult(hint.get(BALL), strikeCount);
 
-        return strikeCount;
+        if (strikeCount == THREE_STRIKES) {
+            isStruckOut = true;
+        }
+        Output.printResult(hint.get(BALL), strikeCount);
+    }
+
+    public boolean isStruckOut() {
+        return isStruckOut;
     }
 }
