14c9ea2a368b4a2fe2070f52b72d6edd2009f2fa
unknown <dragontaek98@naver.com>
2022-11-07 05:03:15 +0900

    fix: rename class name to Answer

--- src/main/java/baseball/Answer.java
+++ src/main/java/baseball/Answer.java
@@ -5,10 +5,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GenerateAnswer {
+public class Answer {
     private List<Integer> answer;
 
-    protected GenerateAnswer(){
+    protected Answer(){
         this.answer = generateRandomAnswer();
     }
 
