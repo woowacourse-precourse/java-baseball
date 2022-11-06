70f79e89d41f59511fc52cc3db571e896557126b
JaeUpSu <hyeongs2323@gmail.com>
2022-11-06 21:22:27 +0900

    Record | Record 클래스를 만들어 정리

---
+++ src/main/java/baseball/Record.java
@@ -0,0 +1,50 @@
+package baseball;
+
+import java.util.List;
+
+public class Record {
+
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+
+    private static final int SIZE = 3;
+    private static final int ZERO = 0;
+
+    int ballCount;
+    int strikeCount;
+
+    public void setEachCounts(Game computer, Game user) {
+
+        List<Integer> computerNumbers = computer.numbers;
+        List<Integer> userNumbers = user.numbers;
+
+        for (int i = 0; i < SIZE; i++) {
+            int computerNumber = computerNumbers.get(i);
+            int userNumber = userNumbers.get(i);
+
+            if (userNumber == computerNumber) {
+                strikeCount++;
+                continue;
+            }
+            if (computerNumbers.contains(userNumber)) {
+                ballCount++;
+            }
+        }
+    }
+    public void displayRecord() {
+        String record = "";
+
+        if (ballCount > ZERO && strikeCount > ZERO) {
+            record = ballCount + "볼 " + strikeCount + "스트라이크";
+        } else if (ballCount > ZERO) {
+            record = ballCount + "볼";
+        } else if (strikeCount > ZERO) {
+            record = strikeCount + "스트라이크";
+        } else if (ballCount == ZERO && strikeCount == ZERO) {
+            record = "낫싱";
+        }
+
+        System.out.println(record);
+    }
+}
