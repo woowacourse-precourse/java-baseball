0e761b0b8b64b6d6a5b88d6592a12ef889ee95ce
Dan-HeeYoung9182 <ldan9182@gmail.com>
2022-11-06 18:34:45 +0900

    게임 플레이어 3개의 숫자 확인 -> 중복숫자 포함할 경우 false 리턴하는 hasDuplicates 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
@@ -25,5 +27,13 @@ public class Application {
         int sum = 1;
         for (int elem : list) sum *= elem;
         return sum == 0;
+    }
+
+    public static boolean hasDuplicates(List<Integer> list) {
+        Set<Integer> set = new HashSet<>();
+        for (int elem : list) {
+            if (!set.add(elem)) return false;
+        }
+        return true;
     }
 }
