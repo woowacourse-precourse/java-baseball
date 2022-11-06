a481dd8832caa29dba222ab8e670ef765bd61624
Park shinyeong <syngh503@gmail.com>
2022-11-06 16:43:35 +0900

    Feat(Game): 게임 관련 클래스 생성
    게임을 시작하고, 종료하며 게임 상태를 저장하는 Game 클래스를 생성하였습니다.
    
    Relate to: #5, #8

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,21 @@
+package baseball;
+
+public class Game {
+    private Integer gameStatus;
+
+    public Integer getGameStatus() {
+        return gameStatus;
+    }
+
+    public void setGameStatus(Integer gameStatus) {
+        this.gameStatus = gameStatus;
+    }
+
+    public void isGameRestart(Integer playerInput) {
+
+    }
+
+    public boolean isValidInput(String playerInput) {
+        return true;
+    }
+}
