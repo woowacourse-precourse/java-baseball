7b8db38c613c4f498a4da2da758e71e9caac92c4
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-06 19:18:42 +0900

    feat(test): 스트라이크 수 반환 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,10 +1,20 @@
 package baseball;
 
+import baseball.domain.Computer;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static baseball.Setting.LENGTH;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -28,8 +38,42 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 유저가_입력한_수의_스트라이크_수를_반환하는_기능() throws Exception {
+        //given
+        Computer computer = new Computer();
+        Field field = Computer.class.getDeclaredField("computerNumber");
+        field.setAccessible(true);
+
+        //when
+        field.set(computer, List.of(1, 2, 3));
+
+        //then
+        assertThat(computer.countStrike(List.of(1, 2, 3))).isEqualTo(3);
+        assertThat(computer.countStrike(List.of(1, 2, 4))).isEqualTo(2);
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
+
+/*    private static boolean isNaturalNumber(String input) {
+        char[] userNumber = input.toCharArray();
+        for (char c : userNumber) {
+            int num = Character.getNumericValue(c);
+            if(!(0 < num && num < 10)) return false;
+        }
+        return true;
+    }
+
+    private static boolean isContainOverlapNumber(String input) {
+        Set<Character> numbers = new HashSet<>();
+
+        for (char c : input.toCharArray()) {
+            if(!numbers.add(c)) return false;
+        }
+
+        return true;
+    }*/
 }
