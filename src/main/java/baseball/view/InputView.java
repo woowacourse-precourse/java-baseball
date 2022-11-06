28b31b70b0923743def19e64ab130dd0a8bb7db0
Sieun Park <psieun0606@gmail.com>
2022-11-06 14:38:39 +0900

    feat: 게임 재시작/종료 여부 입력 문구를 출력하는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,6 +1,8 @@
 package baseball.view;
 
 public class InputView {
+    private int RESTART_NUM = 1;
+    private int EXIT_NUM = 2;
     private String GAMESTART = "숫자 야구 게임을 시작합니다.";
     private String INPUT_USERNUM = "숫자를 입력해주세요 : ";
 
@@ -10,6 +12,11 @@ public class InputView {
 
     public void InputUserNum() {
         System.out.println(INPUT_USERNUM);
+    }
+
+    public void RestartOrExit() {
+        String restartOrExit = String.format("게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.", RESTART_NUM, EXIT_NUM);
+        System.out.println(restartOrExit);
     }
 
 }
