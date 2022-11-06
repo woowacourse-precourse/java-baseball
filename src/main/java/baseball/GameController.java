785ef29616cab4df8ab6b12fc47635843d87c7ff
abbd2 <ajkety00@gmail.com>
2022-11-07 03:17:25 +0900

    feat(GameControlelr): 사용자가 입력한 숫자와 컴퓨터가 생성한 난수의 교집합을 반환하는 메소드 생성

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -8,9 +8,9 @@ import java.util.stream.Stream;
 
 public class GameController {
 
-    public void inputUserNumber() {
+    NumberVO numberVO = new NumberVO();
 
-        NumberVO numberVO = new NumberVO();
+    public void inputUserNumber() {
 
         String userNumber = Console.readLine(); // 입력 받기
         List<Integer> userNumberList = userNumberConvertToList(userNumber); // 자릿수로 나눈 리스트로 변환
@@ -23,5 +23,20 @@ public class GameController {
         List<Integer> userNumberList = Stream.of(userNumber.split("")).mapToInt(Integer::parseInt)
                 .boxed().collect(Collectors.toList());
         return userNumberList;
+    }
+
+    // userNumber와 randomNumber의 교집합 검사
+    public List<Integer> checkContains(){
+
+        List<Integer> userNumber = numberVO.getUserNumber();
+        List<Integer> randomNumber = numberVO.getRandomNumber();
+
+        return randomNumber.stream().filter(userNumber::contains).collect(Collectors.toList());
+    }
+
+    public void compareGameCount(){
+
+
+
     }
 }
