5dee22fb407805f9bcb523a68af373ac9d690e91
Namyunsuk <yunsuk0328@naver.com>
2022-11-06 19:40:54 +0900

    feat: 사용자 입력 값 예외처리 기능 구현

--- src/main/java/baseball/exception/UserInputException.java
+++ src/main/java/baseball/exception/UserInputException.java
@@ -9,7 +9,7 @@ public class UserInputException {
 
     private static final int VALID_LENGTH = 3;
 
-    public void checkUserInputException(String userInput) {
+    public static void checkUserInputException(String userInput) {
         if (isValidLength(userInput) && isNumber(userInput) && isNotContainZero(userInput)
                 && isNotDuplicate(userInput)) {
             return;
@@ -17,14 +17,14 @@ public class UserInputException {
         throw new IllegalArgumentException();
     }
 
-    public boolean isValidLength(String userInput) {
+    public static boolean isValidLength(String userInput) {
         if (userInput.length() == VALID_LENGTH) {
             return true;
         }
         return false;
     }
 
-    public boolean isNumber(String userInput) {
+    public static boolean isNumber(String userInput) {
         for (int userInputIndex = 0; userInputIndex < userInput.length(); userInputIndex++) {
             if (!Character.isDigit(userInput.charAt(userInputIndex))) {
                 return false;
@@ -33,14 +33,14 @@ public class UserInputException {
         return true;
     }
 
-    public boolean isNotContainZero(String userInput) {
+    public static boolean isNotContainZero(String userInput) {
         if (userInput.contains("0")) {
             return false;
         }
         return true;
     }
 
-    public boolean isNotDuplicate(String userInput) {
+    public static boolean isNotDuplicate(String userInput) {
         List<String> splitUserInput = Arrays.asList(userInput.split(""));
         Set<String> userInputSet = new HashSet<>(splitUserInput);
         if (splitUserInput.size() == userInputSet.size()) {
@@ -49,7 +49,7 @@ public class UserInputException {
         return false;
     }
 
-    public void checkUserRestartInputException(String userInput) {
+    public static void checkUserRestartInputException(String userInput) {
         if (userInput.equals("1") || userInput.equals("2")) {
             return;
         }
--- src/main/java/baseball/view/GameInput.java
+++ src/main/java/baseball/view/GameInput.java
@@ -1,6 +1,7 @@
 package baseball.view;
 
 import baseball.mainframe.User;
+import baseball.exception.UserInputException;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameInput {
@@ -8,12 +9,14 @@ public class GameInput {
     public static void getUserInput() {
         GameOutput.printUserInputMessage();
         String userInput = Console.readLine();
+        UserInputException.checkUserInputException(userInput);
         User.setUserInputValue(userInput);
     }
 
     public static void getUserRestartInput() {
         GameOutput.printRestartGame();
         String userRestartInput = Console.readLine();
+        UserInputException.checkUserRestartInputException(userRestartInput);
         User.setUserRestartInputValue(userRestartInput);
     }
 }
