6b439ddd9f8099f44f1a9eca74d94f586bc34d02
Songyi Kim <ksl2950@naver.com>
2022-11-06 22:43:01 +0900

    feat: 사용자가 잘못된 값을 입력했을 때, Exception을 던진 후 종료되도록 하는 기능 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -7,37 +7,50 @@ import java.util.stream.Collectors;
 
 public class Player {
 
-    private final int RESTART_NUMBER = 1;
-    private final int EXIT_NUMBER = 2;
-
     public List<Integer> enterNumber(){
         System.out.print("숫자를 입력해주세요 : ");
         String playerInput = Console.readLine();
+
+        if(!validateIsThreeNumber(playerInput)|| !validateIsUniqueNumber(playerInput)){
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
 
         return convertToIntegerList(playerInput);
     }
 
     public boolean wantToRestart(){
+        int RESTART_NUMBER = 1;
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int playerInput = enterRestartNumber();
-        if (playerInput == RESTART_NUMBER)
-            return true;
-        if (playerInput == EXIT_NUMBER)
-            return false;
-        throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
+        return playerInput == RESTART_NUMBER;
     }
 
 
     public int enterRestartNumber(){
         String playerInput = Console.readLine();
-
+        if (!validateIsRestartNumber(playerInput)){
+            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
+        }
         return Integer.parseInt(playerInput);
+    }
+
+    private boolean validateIsThreeNumber(String playerInput) {
+        return playerInput.matches("^[0-9]{3}$");
+    }
+
+    private boolean validateIsUniqueNumber(String playerInput) {
+        int COMPARE_LENGTH = 3;
+        long inputLength = Arrays.stream(playerInput.split("")).distinct().count();
+        return COMPARE_LENGTH == inputLength;
+    }
+
+    private boolean validateIsRestartNumber(String playerInput) {
+        return playerInput.matches("^[1-2]$");
     }
 
 
     public static List<Integer> convertToIntegerList(String playerInput) {
         return Arrays.stream(playerInput.split("")).map(Integer::parseInt).collect(Collectors.toList());
     }
-
 
 }
