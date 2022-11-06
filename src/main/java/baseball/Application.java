67e41654a0015f865262fe1c62f45057fc622104
dong young <dong5854@gmail.com>
2022-11-07 00:40:02 +0900

    refactor: gameplay 함수명을 checkResult로 수정 후 기능 축소

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -53,12 +53,10 @@ public class Application {
         return answer;
     }
 
-    public static boolean gamePlay(List<Integer> answer) {
+    // 사용자가 숫자를 맞추면 true 맞추지 못하면 false 반환
+    public static boolean checkResult(List<Integer> answer, List<Integer> userInput) {
         int ball = 0;
         int strike = 0;
-
-        System.out.print("숫자를 입력해주세요: ");
-        List<Integer> userInput = userInput();
 
         for (int i = 0; i < userInput.size(); i++) {
             if (Objects.equals(userInput.get(i), answer.get(i))) {
@@ -68,20 +66,20 @@ public class Application {
             }
         }
 
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+
         if (strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return true;
-        } else if (strike == 0 && ball == 0) {
-            System.out.println("낫싱");
-            return false;
-        } else if (strike == 0) {
-            System.out.println(ball + "볼");
-            return false;
-        } else if (ball == 0) {
-            System.out.println(strike + "스트라이크");
-            return false;
         } else {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
             return false;
         }
     }
