7d24535b2c5864192e43834619a83f8dfc7ac0f7
pushedrumex <pushedrumex@naver.com>
2022-11-06 16:08:15 +0900

    feat(Game.makeThreeNums): make three Nums

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         Game.init();
@@ -9,5 +14,16 @@ class Game {
     static final String INITGAME = "숫자 야구 게임을 시작합니다.";
     static void init() {
         System.out.println(INITGAME);
+        List answer = makeThreeNums();
+    }
+    static List makeThreeNums() {
+        List<Integer> threeNums = new ArrayList<>();
+        while (threeNums.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!threeNums.contains(randomNumber)) {
+                threeNums.add(randomNumber);
+            }
+        }
+        return threeNums;
     }
 }
