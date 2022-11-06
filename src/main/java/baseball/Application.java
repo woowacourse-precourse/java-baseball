12bb31e319578e1f8a2041f90689785fd3b53221
박준용 <jo101044@naver.com>
2022-11-06 12:23:08 +0900

    정답숫자 생성 메소드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println("숫자를 입력해주세요 : ");
+        int answerNum = createAnswer();
+        System.out.println(answerNum);
+        sc.close();
+    }
+
+    private static int createAnswer() {
+        int min = 1;
+        int max = 9;
+
+        StringBuilder answer = new StringBuilder();
+        Random random = new Random();
+
+        for (int i = 0; i < 3; i++) {
+            answer.append((random.nextInt(max)+min));
+        }
+
+        return Integer.parseInt(answer.toString());
     }
 }
