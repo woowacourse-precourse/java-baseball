9aef333ff7b20cf875b0ed04e4f9d7dad17ec8a0
hdsung98 <hdsung98@gmail.com>
2022-11-07 03:14:47 +0900

    feat: guess_validity_check declare
    범위, 0포함, 숫자 중복 등의 경우에 대한 IllegalArgumentException throw 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -20,7 +20,27 @@ public class Application {
             }
         }
     }
-    static boolean guess_validity_check(String guess){
+    static boolean guess_validity_check(String guess) throws IllegalArgumentException{
+        Integer guess_int_format = Integer.parseInt(guess);
+        if (guess_int_format < 123 || guess_int_format > 987) {
+            throw new IllegalArgumentException("범위를 준수하지 않았습니다.");
+        }
+
+        ArrayList<Integer> guess_parsed = new ArrayList<>();
+        for (int divisor=100; divisor!= 0; guess_int_format%=divisor, divisor /= 10) {
+            guess_parsed.add(guess_int_format / divisor);
+        }
+
+        for (Integer toCheck : guess_parsed) {
+            if (toCheck == 0) {
+                throw new IllegalArgumentException("1부터 9까지의 수로만 이루어져야 합니다.");
+            }
+        }
+
+        Set<Integer> unique_element_only = new HashSet<>(guess_parsed);
+        if (unique_element_only.size() != guess_parsed.size()) {
+            throw new IllegalArgumentException("숫자의 중복이 있습니다.");
+        }
         return true;
     }
     static void judge_guess(String guess, ArrayList<Integer> answer) {
