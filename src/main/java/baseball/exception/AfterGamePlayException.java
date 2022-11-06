75abcd6fdf6239455c90aafc164fa79b08b4adb8
sosow0212 <sosow0212@naver.com>
2022-11-06 14:16:29 +0900

    feat : 게임 종료 후 다음 게임을 위해 입력 받은 값의 예외를 종합적으로 처리해주는 기능

--- src/main/java/baseball/exception/AfterGamePlayException.java
+++ src/main/java/baseball/exception/AfterGamePlayException.java
@@ -4,6 +4,11 @@ public class AfterGamePlayException {
     private static final String PERMIT_NUMBER_REGEX = "[1-2]+";
     private static final int PERMIT_NUMBER_LENGTH = 1;
 
+    public void askAfterResultException(String numberTypeOfString) {
+        isRightLength(numberTypeOfString);
+        isRightNumber(numberTypeOfString);
+    }
+
     public void isRightLength(String numberTypeOfString) {
         if (numberTypeOfString.length() != PERMIT_NUMBER_LENGTH) {
             throw new IllegalArgumentException("하나의 숫자만 입력하셔야 합니다.");
