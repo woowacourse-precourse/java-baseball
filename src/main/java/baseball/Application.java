e8db3abb2f7f7d28c1a8919bf2f33ea46fba72b6
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-07 01:14:25 +0900

    feat: Add generateAnswer function to create random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    /**
+     * Generate the correct answer for a number baseball game.
+     * @return answer
+     */
+    public static List<Integer> generateAnswer() {
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
     }
 }
