a1481a7e9a84e0a8e1b3d37db940bd528f9e1f29
Lee Min Woo <lmw7414@naver.com>
2022-11-07 03:53:04 +0900

    test : 예외 상황에 대해 테스트 확인
    - 입력된 값이 3자리 수 가 아닌 경우
    - 3자리 수 중에서 중복된 값이 있을 경우
    - 사용자가 입력한 수 중 0이 있을 경우
    
    위와 같은 상황에서 앞서 만들어 놓은 예외 클래스가 던져지는지에 대한 테스트를 확인해보기 위해 코드를 작성하였다. 테스트는 정상적으로 작동한다.

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -1,33 +1,90 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
+import baseball.inputException.ContainingZeroException;
+import baseball.inputException.DuplicatedNumberException;
+import baseball.inputException.IllegalDigitsException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerTest {
 
+    @DisplayName("정상적인 Input 값을 받았을 때 사이즈가 3인 리스트를 받는다.")
     @Test
     void confirmInputValue() {
-        //TODO : 예외 케이스를 던졌을 때 IllegalException을 반환할 것
         List<Integer> userInput = getUserInputValue();
-
         assertThat(userInput.size()).isEqualTo(3);
 
     }
 
-    List<Integer> getUserInputValue () {
+    @DisplayName("입력된 값이 3자리 수가 아닐 때 에러 확인")
+    @Test
+    void givenNotThreeDigits_whenConfirmInputValue_thenReturnIllegalDigitsException() {
+        assertThatThrownBy(() -> givenWrongSituationGetUserInputValue("1234"))
+                .isInstanceOf(IllegalDigitsException.class);
+    }
+
+    @DisplayName("3자리 수 중에서 중복된 값이 있을 때 에러 확인")
+    @Test
+    void givenDuplicatedNumber_whenConfirmInputValue_thenReturnDuplicatedNumberException() {
+        assertThatThrownBy(() -> givenWrongSituationGetUserInputValue("122"))
+                .isInstanceOf(DuplicatedNumberException.class);
+    }
+
+    @DisplayName("사용자가 입력한 수 중에서 0이 있을 때 에러 확인")
+    @Test
+    void givenContainingZeroNumber_whenConfirmInputValue_thenReturnContainingZeroException() {
+        assertThatThrownBy(() -> givenWrongSituationGetUserInputValue("102"))
+                .isInstanceOf(ContainingZeroException.class);
+    }
+
+    List<Integer> getUserInputValue() {
 
         List<Integer> userInput = new ArrayList<>();
         String input = "123";
 
-        for(int i = 0; i < input.length(); i++) {
-            userInput.add(input.charAt(i) -'0');
+        for (int i = 0; i < input.length(); i++) {
+            userInput.add(input.charAt(i) - '0');
         }
 
+        if (userInput.size() != 3)
+            throw new IllegalDigitsException();
+        if (!confirmDuplicationNumber(userInput))
+            throw new DuplicatedNumberException();
+        if (isContainingZero(userInput))
+            throw new ContainingZeroException();
+
         return userInput;
+    }
+
+    List<Integer> givenWrongSituationGetUserInputValue(String input) {
+
+        List<Integer> userInput = new ArrayList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            userInput.add(input.charAt(i) - '0');
+        }
+
+        if (userInput.size() != 3)
+            throw new IllegalDigitsException();
+        if (!confirmDuplicationNumber(userInput))
+            throw new DuplicatedNumberException();
+        if (isContainingZero(userInput))
+            throw new ContainingZeroException();
+
+        return userInput;
+    }
+
+    boolean confirmDuplicationNumber(List<Integer> number) {
+        return (number.get(0) != number.get(1)) && (number.get(1) != number.get(2)) && (number.get(0) != number.get(2));
+    }
+
+    boolean isContainingZero(List<Integer> number) {
+        return (number.contains(0));
     }
 }
