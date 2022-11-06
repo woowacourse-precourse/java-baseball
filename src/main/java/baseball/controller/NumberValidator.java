7e7cd3c20bb9286e9dc234756386eae0ca99393a
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-06 21:37:45 +0900

    feat(NumberValidator): 입력 데이터가 1~9 사이의 숫자인지 확인 기능
    입력 데이터가 숫자인지 확인한 후 아니라면 오류 발생 후 애플리케이션을 종료한다.
    docs: 숫자인지 확인 완료 체크

--- src/main/java/baseball/controller/NumberValidator.java
+++ src/main/java/baseball/controller/NumberValidator.java
@@ -3,14 +3,21 @@ package baseball.controller;
 public class NumberValidator {
     private static final int BASEBALL_GAME_NUMBER_LENGTH = 3;
     private static final String WRONG_LENGTH_INPUT_MESSAGE = "세 자리까지만 입력 가능합니다.";
+    private static final String WRONG_RANGE_INPUT_MESSAGE = "1 ~ 9 사이의 숫자만 입력 가능합니다.";
 
     public void toValidateData(String inputNumber) {
         if (!isTreeLetters(inputNumber)) {
             throw new IllegalArgumentException(WRONG_LENGTH_INPUT_MESSAGE);
+        } else if (!isNumericSting(inputNumber)) {
+            throw new IllegalArgumentException(WRONG_RANGE_INPUT_MESSAGE);
         }
     }
 
     public boolean isTreeLetters(String inputNumber) {
         return inputNumber.length() == BASEBALL_GAME_NUMBER_LENGTH;
+    }
+
+    public boolean isNumericSting(String inputNumber) {
+        return inputNumber.chars().allMatch(Character::isDigit);
     }
 }
