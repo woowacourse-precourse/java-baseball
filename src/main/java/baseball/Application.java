51b2f2b854e8476ce46088fd54e1ef41c508f379
CAEL0 <kjongmin26@gmail.com>
2022-11-06 15:26:35 +0900

    feat(Application): 백의 자리 숫자와 다른 십의 자리 숫자를 생성하는 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static int createTensDifferentFrom(int hundreds) {
+        while (true) {
+            int tens = Randoms.pickNumberInRange(1, 9);
+            if (hundreds != tens)
+                return tens;
+        }
     }
 }
