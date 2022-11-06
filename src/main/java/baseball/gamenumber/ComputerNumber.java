f7e17ebb6f29b4d2bf72d052a2371aec898f1368
ykmxxi <ykmxxi97@gmail.com>
2022-11-06 17:02:00 +0900

    fix: 2.상대방(컴퓨터)의 숫자를 설정하는 생성자 수정

--- src/main/java/baseball/gamenumber/ComputerNumber.java
+++ src/main/java/baseball/gamenumber/ComputerNumber.java
@@ -1,16 +1,8 @@
 package baseball.gamenumber;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-
 public class ComputerNumber extends GameNumber {
-    private static final int START_INCLUSIVE = 1;
-    private static final int END_INCLUSIVE = 9;
-
-    private final List<Integer> computerNumberList = new ArrayList<>(NUMBER_DIGITS);
-
     public ComputerNumber() {
     }
 
@@ -18,27 +10,8 @@ public class ComputerNumber extends GameNumber {
         super(numberList);
     }
 
-    public int getRandomNumber() {
-        return pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
-    }
-
     @Override
     public List<Integer> getNumberList() {
-        int index = 0;
-        while (index < NUMBER_DIGITS) {
-            int randomNumber = getRandomNumber();
-            if (!isDuplicate(computerNumberList, randomNumber)) {
-                computerNumberList.add(randomNumber);
-                index++;
-            }
-        }
-        return computerNumberList;
-    }
-
-    public boolean isDuplicate(List<Integer> computerNumberList, int randomNumber) {
-        if (computerNumberList.contains(randomNumber)) {
-            return true;
-        }
-        return false;
+        return super.getNumberList();
     }
 }
