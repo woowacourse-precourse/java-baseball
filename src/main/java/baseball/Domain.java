f91a71e376a79c9a988d2b0b0834029fe6929d1b
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-06 16:46:05 +0900

    refactor: stream을 사용하여 중복 검증 코드 리팩토링

--- src/main/java/baseball/Domain.java
+++ src/main/java/baseball/Domain.java
@@ -3,7 +3,10 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 public class Domain {
 
@@ -11,9 +14,8 @@ public class Domain {
     List<Integer> computerNumber;
 
 
-
     public List<Integer> getUserNumber() {
-         return convertStringToIntegerList(getInputUserNumber());
+        return convertStringToIntegerList(getInputUserNumber());
     }
 
     public void setUserNumber(String userNumber) {
@@ -51,42 +53,40 @@ public class Domain {
     public void isInputAlright(String userNumber) { // test 입력시오류경우_확인
 
         //입력된 것이 숫자가 아닌 경우 ex) 알파벳이 올 수도 있음
-        if(!isNumber(userNumber)){
+        if (!isNumber(userNumber)) {
             throw new IllegalArgumentException("숫자가 아님");
         }
-
 
         //입력시 길이가 숫자길이가 3이 아닌경우
         if (userNumber.length() != 3) {
             throw new IllegalArgumentException("입력 오류");
         }
 
-
-
         //중복된 숫자가 있는지?
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                overlapCheckProcess(userNumber, i, j);
-            }
+        Stream<String> overlapCheck = Arrays.stream(userNumber.split(""));
+
+        long count_exceptDuplicate = overlapCheck.distinct().count();
+
+        if (isDuplicate(userNumber, count_exceptDuplicate)) {
+            throw new IllegalArgumentException("입력 숫자 중복");
         }
+        ;
+
+
     }
 
-    private void overlapCheckProcess(String userNumber, int i, int j) {
-        if (i == j) {
-            return;
-        }
-        char nowIndexUserNumber = userNumber.charAt(i);
-        char otherIndexUserNumber = userNumber.charAt(j); // 여기 depth 3
-        inputOverlapCheck(nowIndexUserNumber, otherIndexUserNumber);
+    private boolean isDuplicate(String userNumber, long count_exceptDuplicate) {
+        return count_exceptDuplicate != userNumber.length();
     }
+
 
     private boolean isNumber(String userNumber) {
-        for(int index=0; index<userNumber.length(); index++){
+
+        for (int index = 0; index < userNumber.length(); index++) {
             char c = userNumber.charAt(index);
-            if(c>='1' && c<='9'){
+            if (c >= '1' && c <= '9') {
                 continue;
-            }
-            else{
+            } else {
                 return false;
             }
         }
@@ -106,10 +106,4 @@ public class Domain {
         return userNumberList;
     }
 
-    public void inputOverlapCheck(char nowIndexUserNumber,
-        char otherIndexUserNumber) {  // test 입력시오류경우_확인
-        if (nowIndexUserNumber == otherIndexUserNumber) {
-            throw new IllegalArgumentException("입력 숫자중 중복");
-        }
-    }
 }
