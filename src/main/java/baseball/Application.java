bced5a2dd3f1b7c65e8c688ac00ac0cdf5233971
Wooseobee <dntjq0807@naver.com>
2022-11-07 01:53:59 +0900

    feat: 게임구현 - 종료 조건 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -50,6 +50,7 @@ public class Application {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 int end = Integer.parseInt(Console.readLine());
+                return checkFinish(end);
             }
         }
     }
@@ -90,6 +91,14 @@ public class Application {
             }
         } else {
             System.out.println(answer[1] + "스트라이크");
+        }
+    }
+
+    public static boolean checkFinish(int finish) {
+        if (finish == 2) {
+            return true;
+        } else {
+            return false;
         }
     }
 }
