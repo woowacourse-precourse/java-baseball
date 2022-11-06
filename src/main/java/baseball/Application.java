184d368cc9d709f36a5a5a6d031e62bc84f8eaa7
kyliecamila <kyliecamila@naver.com>
2022-11-06 16:04:49 +0900

    [feat]play함수 작성-난수생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,9 +6,13 @@ public class Application {
         isNewGame();
     }
     public static void play(){
-
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        int min = 100;
+        int max = 999;
+        String random = String.valueOf((int) ((Math.random() * (max - min)) + min));
+        guess(random);
     }
-    public static void guess(){
+    public static void guess(String com_num){
 
     }
     public static void isNewGame(){
