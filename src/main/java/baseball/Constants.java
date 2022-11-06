5cb7be40d645de05001656ebeeaf610b2df47fa6
이정우 <acg6138@naver.com>
2022-11-06 13:17:10 +0900

    feat(Constants): 원시값, 문자열을 포장하기 위한 상수 클래스 구현

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class Constants {
+    public static final int INPUT_LENGTH = 3;
+    public static final String RESTART = "1";
+    public static final String END = "2";
+}
