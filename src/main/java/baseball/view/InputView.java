26f4873258baf01d812af2cb50a4115f03849fed
LHS-11 <ghktjq1119@naver.com>
2022-11-06 17:19:17 +0900

    [Refactor] : InputView 클래스에서 시작 메시지, 재시작 메시지, 입력 메시지 상수로 선언

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -6,18 +6,22 @@ public class InputView {
 
     private static Scanner scanner = new Scanner(System.in);
 
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
     public void printGameStart(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(START_MESSAGE);
     }
 
     public String restart(){
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(RESTART_MESSAGE);
         String input = scanner.next();
         return input;
     }
 
     public String playerInput(){
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_MESSAGE);
         String input=scanner.next();
         return input;
     }
