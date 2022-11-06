72b8a26e8044caff612a50a91444ea3f978cac22
Seung-Been Kim <mainlib990@gmail.com mainlib990s-MacBookPro.local>
2022-11-06 15:07:09 +0900

    feat: 게임 플레이를 작성하라

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,6 +2,9 @@ package baseball;
 
 public class BaseballGame extends AbstractBaseballGame {
     private static final String EVENT_MESSAGE_BEFORE_PLAY = "숫자 야구 게임을 시작합니다.";
+    private static final String EVENT_MESSAGE_PLAYING = "숫자를 입력해주세요 : ";
+
+    private final NumberService numberService = new NumberServiceImpl();
 
     @Override
     protected void startEventBeforePlay() {
@@ -10,7 +13,15 @@ public class BaseballGame extends AbstractBaseballGame {
 
     @Override
     protected void play() {
+        Score score;
 
+        numberService.pickNumbersByComputer();
+        do {
+            System.out.print(EVENT_MESSAGE_PLAYING);
+            numberService.enterNumbersByUser();
+            score = numberService.evaluateNumbers();
+            System.out.println(score);
+        } while (!score.allMatch());
     }
 
     @Override
