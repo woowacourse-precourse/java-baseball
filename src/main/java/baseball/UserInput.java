16d109a083601d7304c32f7259a3d9c5f36f32e8
은지 <midzy0228@gmail.com>
2022-11-06 20:40:55 +0900

    refactor: 불필요한 변수 제거(playing)

--- src/main/java/baseball/UserInput.java
+++ src/main/java/baseball/UserInput.java
@@ -6,21 +6,18 @@ import static baseball.Constant.*;
 
 public class UserInput {
     private String input;
-    private boolean playing;
 
-    UserInput(String input, boolean playing) {
+    UserInput(String input) {
         this.input = input;
-        this.playing = playing;
     }
 
-    public boolean IsValid() {
-        if (!playing && IsValidRestartOrEndGame()) {
-            return true;
+    public void IsValid() {
+        if (!IsValidRestartOrEndGame()) {
+            throw new IllegalArgumentException("1 또는 2를 입력하셔야 합니다.");
         }
-        if (playing && IsValidGuessNumber() && IsValidLength()) {
-            return true;
+        if (!(IsValidGuessNumber() && IsValidLength())) {
+            throw new IllegalArgumentException("3자리의 서로 다른 수를 입력하셔야 합니다.");
         }
-        return false;
     }
 
     private boolean IsValidLength() {
@@ -40,7 +37,7 @@ public class UserInput {
 
     private HashSet<String> SetRange1To9() {
         HashSet<String> setRange1To9 = new HashSet<>();
-        for(int i=MIN_RANGE_NUMBER; i<=MAX_RANGE_NUMBER; i++) {
+        for (int i = MIN_RANGE_NUMBER; i <= MAX_RANGE_NUMBER; i++) {
             setRange1To9.add(String.valueOf(i));
         }
         return setRange1To9;
@@ -48,7 +45,7 @@ public class UserInput {
 
     private HashSet<String> StringToSet() {
         HashSet<String> inputSet = new HashSet<>();
-        for(int i=0; i<NUMBERS_LENGTH; i++) {
+        for (int i = 0; i < NUMBERS_LENGTH; i++) {
             inputSet.add(String.valueOf(input.charAt(i)));
         }
         return inputSet;
