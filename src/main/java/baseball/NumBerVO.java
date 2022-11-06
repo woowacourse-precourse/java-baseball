a78550628764bddb1dcb4f7131b6c236eaecdeac
abbd2 <ajkety00@gmail.com>
2022-11-07 05:56:19 +0900

    feat(NumberVO): NumberVO 클래스 생성, getter&setter 구현

---
+++ src/main/java/baseball/NumBerVO.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import java.util.List;
+
+public class NumBerVO {
+
+    List<Integer> userNumber;
+    List<Integer> randomNumber;
+
+    public List<Integer> getUserNumber() {
+        return userNumber;
+    }
+
+    public void setUserNumber(List<Integer> userNumber) {
+        this.userNumber = userNumber;
+    }
+
+    public List<Integer> getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void setRandomNumber(List<Integer> randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+}
