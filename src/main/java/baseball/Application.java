0d4df7034f263280a64233ce29e5fd55506eba12
sodlfmag <sodlfmag@khu.ac.kr>
2022-11-06 22:30:19 +0900

    feat: add getThreeDigitsValues() to get individual digit numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,95 +1,106 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
 
-    public static void printProgramStart(){
+    public static void printProgramStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static void printGameEnd(){
+    public static void printGameEnd() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public static boolean isAnswerNumberValid(int answerNum){
+    public static boolean isAnswerNumberValid(int answerNum) {
         int hundred = answerNum / 100;
         int ten = (answerNum % 100) / 10;
         int one = answerNum % 10;
 
-        if(hundred==0 || ten ==0 || one==0)
+        if (hundred == 0 || ten == 0 || one == 0)
             return false;
 
-        if(hundred==ten || hundred==one || ten==one)
+        if (hundred == ten || hundred == one || ten == one)
             return false;
 
         return true;
     }
 
-    public static int makeAnswerNumber(){
-        int answerNum = Randoms.pickNumberInRange(123,987);
+    public static int makeAnswerNumber() {
+        int answerNum = Randoms.pickNumberInRange(123, 987);
 
         while (!isAnswerNumberValid(answerNum))
-            answerNum = Randoms.pickNumberInRange(123,987);
+            answerNum = Randoms.pickNumberInRange(123, 987);
 
         return answerNum;
     }
 
-    public static boolean isInteger(String inputValue){
+    public static boolean isInteger(String inputValue) {
 
-        try{
+        try {
             Integer.parseInt(inputValue);
             return true;
-        } catch (IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             return false;
         }
     }
 
-    public static boolean isInputValid(String inputValue){
+    public static boolean isInputValid(String inputValue) {
 
-        if(!isInteger(inputValue))
+        if (!isInteger(inputValue))
             return false;
 
         int inputNumber = Integer.parseInt(inputValue);
 
-        if(inputNumber < 123 || inputNumber > 987)
+        if (inputNumber < 123 || inputNumber > 987)
             return false;
 
-        if(!isAnswerNumberValid(inputNumber))
+        if (!isAnswerNumberValid(inputNumber))
             // 랜덤 정답 숫자 유효성과 같은 제한조건을 검사
             return false;
-        
+
         return true;
     }
 
-    public static int tryAnswer(){
+    public static int tryAnswer() {
         int answerTrialNum;
         System.out.println("숫자를 입력해주세요 : ");
         String answerTrialInput = Console.readLine();
 
-        if(!isInputValid(answerTrialInput))
+        if (!isInputValid(answerTrialInput))
             throw new IllegalArgumentException("정답 입력값 오류");
 
         answerTrialNum = Integer.parseInt(answerTrialInput);
         return answerTrialNum;
     }
 
-    public static int countStrike(int answerNum, int hundred, int ten, int one){
+    public static int countStrike(int answerNum, int hundred, int ten, int one) {
         int strikeCnt = 0;
 
-        if(hundred == answerNum/100)
+        if (hundred == answerNum / 100)
             strikeCnt++;
 
-        if(ten == (answerNum%100)/10)
+        if (ten == (answerNum % 100) / 10)
             strikeCnt++;
 
-        if(one == answerNum%10)
+        if (one == answerNum % 10)
             strikeCnt++;
 
         return strikeCnt;
     }
+
+    public static List<Integer> getThreeDigitsValues(int number){
+        // 세 자리의 정수가 입력되면 각 자릿수를 List에 담아 반환
+        List<Integer> digits = new ArrayList<>();
+        digits.add(number / 100);
+        digits.add((number%100) / 10);
+        digits.add(number%10);
+        return digits;
+    }
 }
-
-
