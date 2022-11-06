75c8415cbdb7c5b0593ec10a11afa671d07ab2a0
sujin0607 <goweg0607@gmail.com>
2022-11-06 16:01:40 +0900

    fix: 실행함수에서 결과를 출력해주는 부분을 메서드로 따로 분리

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -8,6 +8,7 @@ import java.util.List;
 public class BaseBallGame {
     private List<Integer> computer;
     private List<Integer> user;
+    private String answer;
 
     public List<Integer> randomNumbers() {
         computer = new ArrayList<>();
@@ -28,12 +29,12 @@ public class BaseBallGame {
         String[] nums = inputNum.split("");
 
         if (nums.length != 3) {
-            throw new IllegalStateException();
+            throw new IllegalArgumentException();
         }
         for (String num : nums) {
             int numInt = Integer.parseInt(num);
             if (user.contains(numInt)) {
-                throw new IllegalStateException();
+                throw new IllegalArgumentException();
             } else {
                 user.add(numInt);
             }
@@ -59,6 +60,25 @@ public class BaseBallGame {
         return strikeAndBall;
     }
 
+    public String numberToString(List<Integer> strikeAndBall){
+        int strike = strikeAndBall.get(0);
+        int ball = strikeAndBall.get(1);
+
+        if (strike == 3) {
+            System.out.printf("3개의 숫자를 모두 맞히셨습니다! ");
+            return "게임 종료";
+        } else if (strike != 0 && ball != 0) {
+            answer = ball+"볼 "+strike+"스트라이크";
+        } else if (strike != 0) {
+            answer = strike+"스트라이크";
+        } else if (ball != 0) {
+            answer = ball+"볼";
+        } else {
+            answer = "낫싱";
+        }
+        return answer;
+    }
+
     public void reStart(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String userInput = Console.readLine();
@@ -77,24 +97,12 @@ public class BaseBallGame {
 
         while (true) {
             List<Integer> userNumber = inputNumbers();
-            List<Integer> result = findNumber(computerNumber, userNumber);
-            int strike = result.get(0);
-            int ball = result.get(1);
-
-            if (strike == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                reStart();
-                break;
-            } else if (strike != 0 && ball != 0) {
-                System.out.printf("%d볼 %d스트라이크\n", strike, ball);
-            } else if (strike != 0) {
-                System.out.printf("%d스트라이크\n", strike);
-            } else if (ball != 0) {
-                System.out.printf("%d볼\n", ball);
-            } else {
-                System.out.println("낫싱");
-            }
+            List<Integer> strikeAndBall = findNumber(computerNumber, userNumber);
+            String answer = numberToString(strikeAndBall);
+            if(answer.equals("게임 종료")) break;
+            else System.out.println(answer);
         }
+        reStart();
     }
 
 }
