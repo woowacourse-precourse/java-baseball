f0cca98cbbb5dcca225d39531839c936be2ce765
DESKTOP-1ANNTGI\USER <cvc100326@naver.com>
2022-11-06 15:51:03 +0900

    test: 가독성을 위한 테스트 수정
    - 테스트 제목을 좀더 구체적으로 수정
    - 테스트의 값을 상수로 수정

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -8,11 +8,12 @@ import java.io.OutputStream;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameTest {
-    @DisplayName("컴퓨터의 랜덤 숫자얻기")
+    @DisplayName("컴퓨터의 0이 아닌 서로다른 3자리 랜덤 숫자 얻기")
     @Test
     void getComputerNumber() {
         Game game = new Game();
@@ -20,7 +21,7 @@ public class GameTest {
         assertThat(game.getComputerNumber()).doesNotContain(0).hasSize(3).doesNotHaveDuplicates();
     }
 
-    @DisplayName("플레이어의 중복된 숫자이면 예외")
+    @DisplayName("플레이어의 입력에 중복된 숫자가 존재하면 예외")
     @Test
     void isPlayerNumberDuplicate() {
         Game game = new Game();
@@ -40,7 +41,7 @@ public class GameTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("플레이어의 숫자 체크")
+    @DisplayName("플레이어의 숫자 체크하기")
     @Test
     void checkPlayerNumber() {
         Game game = new Game();
@@ -51,7 +52,7 @@ public class GameTest {
         assertThat(result).isEqualTo(new int[]{2, 1});
     }
 
-    @DisplayName("체크결과 볼스트라이크 출력")
+    @DisplayName("체크결과를 올바르게 출력 - 볼 스트라이크 ")
     @Test
     void printResult1() {
         Game game = new Game();
@@ -62,7 +63,7 @@ public class GameTest {
         assertThat(out.toString()).isEqualTo("2볼 1스트라이크\r\n");
     }
 
-    @DisplayName("체크결과 낫싱 출력")
+    @DisplayName("체크결과를 올바르게 출력 - 낫싱")
     @Test
     void printResult2() {
         Game game = new Game();
@@ -73,7 +74,7 @@ public class GameTest {
         assertThat(out.toString()).isEqualTo("낫싱\r\n");
     }
 
-    @DisplayName("체크결과 3스트라이크 게임종료 출력")
+    @DisplayName("체크결과를 올바르게 출력 - 3스트라이크 게임종료")
     @Test
     void printResult3() {
         Game game = new Game();
@@ -81,19 +82,19 @@ public class GameTest {
         System.setOut(new PrintStream(out));
         game.printResult(new int[]{0, 3});
 
-        assertThat(out.toString()).isEqualTo("3스트라이크\r\n" + game.WIN_NOTICE+"\r\n");
+        assertThat(out.toString()).isEqualTo("3스트라이크\r\n" + game.WIN_NOTICE + "\r\n");
     }
 
-    @DisplayName("게임 재시작/프로그램 종료 입력값 확인")
+    @DisplayName("게임 재시작/프로그램 종료를 선택하는 입력값 확인")
     @Test
     void checkRestartOrEnd() {
         Game game = new Game();
         System.setIn(new ByteArrayInputStream("1".getBytes()));
 
-        assertThat(game.getRestartOrEndNumber()).isEqualTo(1);
+        assertThat(game.getRestartOrEndNumber()).isEqualTo(game.RESTART_NUMBER);
     }
 
-    @DisplayName("게임 재시작/프로그램 종료 입력값 예외확인")
+    @DisplayName("게임 재시작/프로그램 종료를 선택하는 입력값이 아닌 걸 입력할 경우 예외")
     @Test
     void checkRestartOrEndException() {
         Game game = new Game();
