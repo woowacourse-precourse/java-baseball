7191f5f6514a62efd08b1c0dc719f0e0882e264d
이승훈 <qwqw8019@naver.com>
2022-11-07 03:17:32 +0900

    feat : 사용자 입력 메소드 및 입력값 검증 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,11 +3,12 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Baseball_Game Game = new Baseball_Game();
+        BaseballGame Game = new BaseballGame();
 
         Game.start();
 
@@ -15,25 +16,20 @@ public class Application {
     }
 }
 
-class Baseball_Game {
+class BaseballGame {
+    int hiddenNumber;
 
-    public Baseball_Game() {
+    public BaseballGame() {
         System.out.println("Baseball_Game.Baseball_Game");
-        int hidden_number;
-        hidden_number = randomThreeDigitNumber();
-        System.out.println("hidden_number = " + hidden_number);
+
+        hiddenNumber = randomThreeDigitNumber();
+        System.out.println("hidden_number = " + hiddenNumber);
 
     }
 
-    /**
-     * 숫자야구게임을 시작한다.
-     */
-    public void start() {
-        System.out.println("Baseball_game.Play_game");
-    }
 
     /**
-     * 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 반환한다.
+     * 1에서 9까지 서로 다른 임의의 수 3개를 선택
      */
     public static int randomThreeDigitNumber() {
 
@@ -65,7 +61,7 @@ class Baseball_Game {
      * @return boolean
      */
     public static boolean isValidRandomNumber(int new_number, List<Integer> list) {
-        if (isZero(new_number)) {
+        if (new_number == 0) {
             return false;
         }
 
@@ -84,15 +80,116 @@ class Baseball_Game {
         return list.contains(new_number);
     }
 
-    /**
-     * 해당 숫자가 0인지 확인한다.
-     *
-     * @param number:int
-     * @return boolean
-     */
-    public static boolean isZero(int number) {
 
-        return number == 0;
+    /**
+     * 숫자야구게임을 시작한다.
+     */
+    public void start() {
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (true) {
+            int userNumber = inputUserNumber();
+
+            checkValidityOfNumber(userNumber);
+
+            computeResult(userNumber, hiddenNumber);
+        }
+    }
+
+    public static int inputUserNumber() {
+        Scanner scanner = new Scanner(System.in);
+        int userNumber;
+        String input;
+
+        System.out.print("숫자를 입력해주세요 : ");
+        try {
+            input = scanner.nextLine();
+            userNumber = Integer.parseInt(input);
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+
+
+        return userNumber;
+    }
+
+    public static void checkValidityOfNumber(int input) {
+        List<Integer> numList;
+        numList = splitNumberByDigit(input);
+
+        if (input < 100 || 999 < input) {
+            throw new IllegalArgumentException();
+        }
+
+        if (numList.contains(0)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (isDuplicatedUserNumber(numList)) {
+            throw new IllegalArgumentException();
+        }
+
+    }
+
+    public static List<Integer> splitNumberByDigit(int number) {
+
+        String strNumber = Integer.toString(number);
+        List<Integer> resultList = new ArrayList<>();
+
+
+        for (int i = 0; i < strNumber.length(); i++) {
+            resultList.add(strNumber.charAt(i) - '0');
+        }
+
+        return resultList;
+    }
+
+    public static boolean isDuplicatedUserNumber(List<Integer> numList) {
+        int numSize = numList.size();
+        for (int i = 0; i < numSize; i++) {
+            int currentNumber = numList.get(i);
+            if (numList.subList(i + 1, numSize).contains(currentNumber)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public static void computeResult(int userNumber, int hiddenNumber) {
+        int strike;
+        int ball;
+
+        strike = computeStrike(userNumber, hiddenNumber);
+        ball = computeBall(userNumber, hiddenNumber);
+    }
+
+    public static int computeStrike(int userNumber, int hiddenNumber) {
+        int strike = 0;
+        List<Integer> userNumberList = splitNumberByDigit(userNumber);
+        List<Integer> hiddenNumberList = splitNumberByDigit(hiddenNumber);
+
+        for (int i = 0; i < 3; i++) {
+            int currentUserNumber = userNumberList.get(i);
+            int currentHiddenNumber = hiddenNumberList.get(i);
+
+            if (currentUserNumber == currentHiddenNumber) {
+                strike++;
+            }
+        }
+        System.out.println("strike = " + strike);
+        return strike;
+    }
+
+    public static int computeBall(int userNumber, int hiddenNumber) {
+        // TODO: ball 메소드 구현하기
+        int ball = 0;
+
+
+
+        return ball;
     }
 
 }
