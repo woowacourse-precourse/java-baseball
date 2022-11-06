4a0e77cd303dda155dc3867e44b94a2dba75f415
MoonJeWoong <munjin0201@naver.com>
2022-11-06 18:10:26 +0900

    feat(application): 숫자야구게임 메인 프로그램 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Core T = new Core();
+        Baseball game = new Baseball();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        //야구게임
+        while(true){
+            game.run();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String userInputOneOrTwo = readLine();
+            T.validateUserInputOneOrTwo(userInputOneOrTwo);
+            if(userInputOneOrTwo.equals("1")){
+                continue;
+            }
+            if(userInputOneOrTwo.equals("2")){
+                System.out.println("프로그램을 종료합니다.");
+                break;
+            }
+        }
     }
 }
---
+++ src/main/java/baseball/Baseball.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Baseball {
+    public void run() {
+        Core T = new Core();
+        List<Integer> computer = T.generateComputerRandomNumber();
+        boolean endFlag=false;
+        while(!endFlag){
+            System.out.print("숫자를 입력해주세요 : ");
+            String userInput = readLine();
+            T.validateUserInputThreeDifferentNumbers(userInput);
+            List<Integer> user = T.stringToIntegerList(userInput);
+
+            int strikes = T.countStrike(computer, user);
+            int balls = T.countBall(computer, user);
+            T.printGameResult(strikes, balls);
+
+            endFlag = T.isEndFlagged(strikes);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+}
