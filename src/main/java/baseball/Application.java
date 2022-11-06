1ece3afe1977bacdf288884681ce94f2130aec37
coldbucks <met.hsw@gmail.com>
2022-11-07 01:09:29 +0900

    REFACTOR:입력값 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,17 +5,20 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
+
 
 public class Application {
 
 
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Application T = new Application();
+        Application application = new Application();
+        application.create();
+        application.plyerNuber();
+    }
 
-        //컴퓨터 랜덤 3개의 숫자
+
+    public List<Integer> create() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -23,24 +26,32 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+        System.out.println("컴퓨터 숫자= " + computer);
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        String userNum = Console.readLine();
-        System.out.println("숫자를 입력해주세요 : "+ userNum);
+        return computer;
 
-        // 내숫자 List<Integer> 배열로 만들기
-        String[] s = userNum.split("");
-        List<Integer> myList = new ArrayList<>();
-        for(int i = 0 ; i<userNum.length() ; i++) {
-            myList.add(Integer.parseInt(s[i]));
-        }
-        System.out.println("computer= " + computer);
-        System.out.println("내 숫자= "+ myList);
-
-        }
-    private static void changeUserNum() {
     }
 
+    // 입력값
+    public List<Integer> plyerNuber() {
+        System.out.println("숫자를 입력하세요");
+        String userNum = Console.readLine();
+        String[] s = userNum.split("");
+        List<Integer> playerNum = new ArrayList<>();
+        for (int i = 0; i < userNum.length(); i++) {
+            playerNum.add(Integer.parseInt(s[i]));
+        }
+        System.out.println("내 숫자= " + playerNum);
+        return playerNum;
 
+    }
 }
+
+
+
+
+
+
+
+
 
