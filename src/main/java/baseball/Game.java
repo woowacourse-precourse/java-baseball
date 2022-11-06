a983db29201e8cbe0f082a076f604377034be59d
sysliar <sysliar@gmail.com>
2022-11-06 21:00:19 +0900

    feat(Game): get score to user input

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Game {
     private static final String START_STRING = "숫자 야구 게임을 시작합니다.";
+    private static final int BALL_INDEX = 0;
+    private static final int STRIKE_INDEX = 1;
 
     private final Player computer;
     private final Player player;
@@ -28,5 +34,31 @@ public class Game {
             compareBoard.put(computerNumbers.get(i), i);
         }
         return compareBoard;
+    }
+
+    private ArrayList<Integer> getScore(Player player, HashMap<Integer, Integer> compareBoard) {
+        ArrayList<Integer> score = new ArrayList<>(List.of(0, 0));
+        ArrayList<Integer> playerNumbers = player.getNumbers();
+        for (int i = 0; i < playerNumbers.size(); i++) {
+            if (isStrike(playerNumbers.get(i), i, compareBoard)) {
+                score.set(STRIKE_INDEX, score.get(STRIKE_INDEX) + 1);
+                continue;
+            }
+            if (isBall(playerNumbers.get(i), compareBoard)) {
+                score.set(BALL_INDEX, score.get(BALL_INDEX) + 1);
+            }
+        }
+        return score;
+    }
+
+    private Boolean isStrike(Integer number, Integer index, HashMap<Integer, Integer> compareBoard) {
+        if (compareBoard.containsKey(number)) {
+            return compareBoard.get(number).equals(index);
+        }
+        return false;
+    }
+
+    private Boolean isBall(Integer number, HashMap<Integer, Integer> compareBoard) {
+        return compareBoard.containsKey(number);
     }
 }
