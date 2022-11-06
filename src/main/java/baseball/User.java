71307d9735c572d5655dd146b19df807654155fc
hyeonseog-won <smreo75@gmail.com>
2022-11-06 15:06:30 +0900

    feat: 사용자 입력 기능 추가

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+
+    public static void inputNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+    }
+}
