6ff04b1be973b9bc4970627e0b6c455e9c4e9bfc
jingyu9804 <swc05161@gmail.com>
2022-11-06 20:02:04 +0900

    refactor : inputNumberMessage 변수 내용 수정

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -5,7 +5,7 @@ public class Constant {
     public static final String gameEndingMessage="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String gameRestartMessage="게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    public static final String inputNumberMessage="숫자를 입력해주세요.";
+    public static final String inputNumberMessage="숫자를 입력해주세요 : ";
     public static final String strikeMessage="스트라이크";
     public static final String ballMessage="볼";
     public static final String nothingMessage="낫싱";
