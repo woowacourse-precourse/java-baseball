fe93b8fb1c1d1306b8515948b4ed5b5a21d333ae
Jae Ung Jung <roosa0210@gmail.com>
2022-11-07 03:58:44 +0900

    랜덤한 숫자 3개 선택

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        //  랜덤한 숫자 3개 선택
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
     }
 }
