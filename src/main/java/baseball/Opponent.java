3cf56de6f2f02ea032be093c3eb30082f5d7e4bd
Yejin Shin <shinyejin0212@gmail.com>
2022-11-06 20:36:36 +0900

    feat($Opponent): add  #(컴퓨터(상대방) 클래스)0. 랜덤 3가지 숫자 생성 메소드

---
+++ src/main/java/baseball/Opponent.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Opponent {
+    protected static List<Integer> answer;
+    public Opponent(){
+        Opponent.answer = createAnswer();
+    }
+
+    public static boolean checkRandomNumber(int num, List<Integer> list) {
+        return !list.contains(num);
+    }
+    public static List<Integer> createAnswer(){
+        List<Integer> list = new ArrayList<>();
+        while(list.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!checkRandomNumber(randomNumber, list)){
+                continue;
+            }
+            list.add(randomNumber);
+        }
+        System.out.println(list);
+        return list;
+    }
+}
