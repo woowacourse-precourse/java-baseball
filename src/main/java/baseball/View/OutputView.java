dd575391f2eaf18827588573b586846f35468262
aossuper7 <aossuper7@naver.com>
2022-11-06 15:36:27 +0900

    refactor(OutputView): public -> private 변경, static 추가

--- src/main/java/baseball/View/OutputView.java
+++ src/main/java/baseball/View/OutputView.java
@@ -6,8 +6,8 @@ public class OutputView {
     private static final String STRIKE = "스트라이크";
     private static final int ZERO = 0;
     private static final String NOTTHING = "낫싱";
-    public static final String THREESTRIKE = "3스트라이크";
-    private final String INPUTNUM = "숫자를 입력해주세요 : ";
+    private static final String THREESTRIKE = "3스트라이크";
+    private static final String INPUTNUM = "숫자를 입력해주세요 : ";
     private static final String STARTMESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String VICTORY = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String STARTOREND = "게임을 새로 시작하려면 1, 종료하면 2를 입력하세요.";
