910ccec926f225345c7fbd7568a3e06a1721eb8a
JunHee <ljh19980823@gmail.com>
2022-11-06 15:33:07 +0900

    feat : 입력 검증 시 경우에 따라 메시지 구분

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -24,16 +24,19 @@ public class Game {
     }
 
     public void checkValidNumber(String userInput) {
-        if (userInput.length() != 3 || !isNumeric(userInput)) {
-            throw new IllegalArgumentException();
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException("3개의 문자만 입력해주세요!");
+        }
+        if (!isNumeric(userInput)) {
+            throw new IllegalArgumentException("숫자만 입력해주세요!");
         }
         List<Integer> userInputNumber = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             Integer tempNum = Integer.parseInt(String.valueOf(userInput.charAt(i)));
             if (tempNum.equals(0)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("0은 입력할 수 없습니다!");
             } else if (userInputNumber.contains(tempNum)) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("중복된 값을 입력하지 마세요!");
             } else {
                 userInputNumber.add(tempNum);
             }
