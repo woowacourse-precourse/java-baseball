7f8bae85a1c09578f32c95f6f5ee92fccad55e61
yujaemin <woawoa0513@gmail.com>
2022-11-07 01:43:20 +0900

    feat: 올바른 숫자 입력했는지 검사하는 함수 구현
    다음 4가지 조건을 만족하는 문자인지 검사한다.
    1. 숫자만 포함한다.
    2. 올바른 길이의 문자다.
    3. 0을 포함하지
    않는다.
    4. 한 숫자를 두 번 이상 포함하지 않는다.

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 public class Player {
     private static final int DESIRED_LENGTH = 3;
 
+    public boolean isValidInput(String input) {
+        return containOnlyDigit(input) && isCorrectLength(input) && !containZero(input) && !hasDuplicatedDigit(input);
+    }
+
     public boolean containOnlyDigit(String input) {
         for (Character character : input.toCharArray()) {
             if (!Character.isDigit(character)) {
