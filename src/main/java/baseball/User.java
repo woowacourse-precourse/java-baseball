61a81c8669d3455b3b031283ca61b2a50e591a71
hyeonseog-won <smreo75@gmail.com>
2022-11-06 15:42:44 +0900

    feat: 사용자 입력 검증 기능 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -4,8 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class User {
 
-    public static void inputNumbers() {
+    public void inputNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
+        verifyInput(input);
+    }
+
+    public void verifyInput (String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("입력한 수가 3자리가 아닙니다.");
+        }
+        if (!input.matches("^[1-9]*$")) {
+            throw new IllegalArgumentException("입력한 수가 1-9가 아닙니다.");
+        }
     }
 }
