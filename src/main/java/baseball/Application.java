dafdb59bd01954e7d7baa86af736194413b64239
brotherstone97 <brotherstone97@gmail.com>
2022-11-06 17:51:32 +0900

    Feat(scanStartNumber): 게임 시작 시 세자리 수를 입력받는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
+
+    static int scanStartNumber() {
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
+
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int userNumber = scanStartNumber();
     }
 }
