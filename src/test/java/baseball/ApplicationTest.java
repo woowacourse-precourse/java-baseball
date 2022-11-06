88ef721157594170005cb96a236a132d19a9281b
woosung1223 <dntjd991223@naver.com>
2022-11-06 13:32:18 +0900

    test(Application): 게임 재시작을 두번할 때를 위한 테스트 생성

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -10,15 +10,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
+    @Nested
+    class GameTest {
+        @Test
+        void 게임종료_후_재시작() {
+            assertRandomNumberInRangeTest(
+                    () -> {
+                        run("246", "135", "1", "597", "589", "2");
+                        assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                    },
+                    1, 3, 5, 5, 8, 9
+            );
+        }
+        @Test
+        void 게임종료_후_재시작_후_재시작() {
+            assertRandomNumberInRangeTest(
+                    () -> {
+                        run("325", "145", "1", "847", "254", "1", "742", "2");
+                        assertThat(output()).contains("1스트라이크", "3스트라이크", "1볼", "3스트라이크", "3스트라이크", "게임 종료");
+                    },
+                    1, 4, 5, 2, 5, 4, 7, 4, 2
+            );
+        }
     }
 
     @Nested
