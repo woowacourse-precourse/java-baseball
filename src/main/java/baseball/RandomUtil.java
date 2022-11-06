cdb25c0972138a09b8ec0018b7b2ad0337951268
jingyu9804 <swc05161@gmail.com>
2022-11-07 00:50:04 +0900

    feat : makeComputerNumber method 추가

--- src/main/java/baseball/RandomUtil.java
+++ src/main/java/baseball/RandomUtil.java
@@ -2,14 +2,30 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.HashSet;
+import java.util.*;
 
 public class RandomUtil {
-    public HashSet<String> giveComputerNumber(){
-        HashSet<String> computerNumberSet = new HashSet<>();
-        while(computerNumberSet.size()<3){
-            computerNumberSet.add(Integer.toString(Randoms.pickNumberInRange(1,9)));
+
+    public List<String> giveComputerNumber(){
+        RandomUtil randomUtil = new RandomUtil();
+        CheckUtil checkUtil = new CheckUtil();
+
+        List<String>comNum = randomUtil.makeComputerNumber();
+        checkUtil.checkIsDuplicated(comNum);
+
+        return comNum;
+    }
+
+    private List<String> makeComputerNumber(){
+        List<String> comNumberList = new ArrayList<>();
+        boolean[] ch=new boolean[10];
+        while (comNumberList.size()<3) {
+            int tmp = Randoms.pickNumberInRange(1,9);
+            if(!ch[tmp]){
+                comNumberList.add(Integer.toString(tmp));
+                ch[tmp]=true;
+            }
         }
-        return computerNumberSet;
+        return comNumberList;
     }
 }
