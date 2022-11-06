c3853e291e75016ebe69dcd4212ec2c4b42faedd
yangjiyoung <0112jyoung@naver.com>
2022-11-06 23:27:59 +0900

    feat: add compare function

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -11,32 +11,51 @@ import java.util.List;
 public class GameService {
     private List<Integer> answerList;
     private List<Integer> userList;
-    public Game game= new Game();
+    public Game game = new Game();
 
-    public GameService(){
+    public GameService() {
         game.initGame();
 
     }
-    public void startGame(){
-            answerList= RandomUtils.makeRandomList();
-            getUserNumber();
-    }
-    public void getUserNumber(){
+
+    public void startGame() {
         SystemMessage.startGameMessage();
-        String userString= Console.readLine();
+        answerList = RandomUtils.makeRandomList();
+        while (game.getStrikeCount() <3) {
+            getUserNumber();
+            compareNumber();
+            //비교하기
+
+        }
+    }
+
+    public void getUserNumber() {
+
+        SystemMessage.requestNumberMessage();
+        String userString = Console.readLine();
         //추가적으로 맞는지 처리하기
 
         //문자열을 list로 바꾸기
-        userList=makeList(userString);
+        userList = makeList(userString);
+        System.out.println("유저: " + userList + ", 랜덤 숫자:" + answerList);
     }
 
-    public List<Integer> makeList(String str){
+    public List<Integer> makeList(String str) {
         List<Integer> returnList = new ArrayList<>();
-        for(String s: str.split("")){
-            int tmpInteger=Integer.parseInt(s);
+        for (String s : str.split("")) {
+            int tmpInteger = Integer.parseInt(s);
             returnList.add(tmpInteger);
         }
         return returnList;
     }
 
+    public void compareNumber() {
+        for (int i = 0; i < userList.size(); i++) {
+            if (userList.get(i).equals(answerList.get(i)))
+                game.addStrikeCount();
+            else if (answerList.contains(userList.get(i)))
+                game.addBallCount();
+        }
+        SystemMessage.printResultMessage(game.getStrikeCount(), game.getBallCount());
+    }
 }
