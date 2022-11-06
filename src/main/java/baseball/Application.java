a617fbf4c8fce2e9e5b9022f1893f55a60b494bf
swonny <wonny921@gmail.com>
2022-11-06 10:37:58 +0900

    feat(ExceptionFor4, main): 게임 종료/반복 여부 입력에 대한 예외 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
     final static String REPEAT_OPTION_GUIDE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     final static String QUIT_GAME_CONDITION = "2";
+    final static ExceptionFor4 exceptionFor4 = new ExceptionFor4();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -12,9 +13,12 @@ public class Application {
             Game game = new Game();
             game.play();
             System.out.print(REPEAT_OPTION_GUIDE_MESSAGE);
-            if (Console.readLine() == QUIT_GAME_CONDITION) {
+            String repeat = Console.readLine();
+            exceptionFor4.checkException(repeat);
+            if (repeat == QUIT_GAME_CONDITION) {
                 break;
             }
         }
     }
+
 }
--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -1,6 +1,9 @@
 package baseball;
 
 public class Exception {
+    final static Integer EXCEPTION4_NUMBER_LENGTH = 1;
+    final static String ANSWER_REPEAT_GAME = "1";
+    final static String ANSWER_QUIT_GAME = "2";
 
     public static void hasCharacters(String input) {
         String regex = "^[0-9]*$";
@@ -9,4 +12,25 @@ public class Exception {
         }
     }
 
+}
+
+class ExceptionFor4 extends Exception {
+
+    public static void checkException(String input) {
+        hasCharacters(input);
+        checkInputLength(input);
+        checkYesOrNo(input);
+    }
+
+    public static void checkInputLength(String input) {
+        if (input.length() != EXCEPTION4_NUMBER_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkYesOrNo(String input) {
+        if (input != ANSWER_REPEAT_GAME && input != ANSWER_QUIT_GAME) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
