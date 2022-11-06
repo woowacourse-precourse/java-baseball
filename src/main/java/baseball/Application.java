88aac92b74695747362b4129b53697e3a17813ea
qkrdmswl <eunji303@naver.com>
2022-11-06 19:33:43 +0900

    fix(numBaseball) : throw exception about problem conditions

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,36 +7,58 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public static void numBaseball(List<Integer> computerNum) {
+    public static void numBaseball(List<Integer> computerNum) throws IllegalArgumentException {
         List<Integer> userNum = new ArrayList<>();
         System.out.print("숫자를 입력하세요 : ");
         char[] nums = Console.readLine().toCharArray();
+
+        if (nums.length > 3 || nums[0] == nums[1] || nums[1] == nums[2] || nums[0] == nums[2]) {
+            throw new IllegalArgumentException("예외 발생");
+        }
 
         for (int num : nums) {
             userNum.add(Character.getNumericValue(num));
         }
 
-        if (comparator(computerNum, userNum)) { return; }
+        if (comparator(computerNum, userNum)) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+            if (Integer.parseInt(Console.readLine()) == 1) {
+                numBaseball(opponentNum());
+            }
+            return;
+        }
         numBaseball(computerNum);
     }
 
     public static boolean comparator(List<Integer> computerNum, List<Integer> userNum) {
         int strike = 0;
         int ball = 0;
-        for (int idx = 0; idx < 3; idx++){
-            if (computerNum.get(idx) == userNum.get(idx))       { strike += 1; }
-            else if (userNum.contains(computerNum.get(idx)))    { ball += 1;   }
+
+        for (int idx = 0; idx < 3; idx++) {
+            if (computerNum.get(idx) == userNum.get(idx)) {
+                strike += 1;
+            }
+            else if (userNum.contains(computerNum.get(idx))) {
+                ball += 1;
+            }
         }
 
-        if (strike == 3)  { System.out.println("3스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료"); return true; }
+        if (strike == 3) {
+            System.out.println("3스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
         else if (ball == 0 && strike == 0) {
             System.out.println("낫싱");
-        } else if (ball != 0 && strike != 0) {
-            System.out.println((char)ball + "볼 "+ (char)strike + "스트라이크");
-        } else if (ball != 0 && strike == 0) {
-            System.out.println((char)ball + "볼");
-        } else if (ball == 0 && strike != 0) {
-            System.out.println((char)strike + "스트리이크");
+        }
+        else if (ball != 0 && strike != 0) {
+            System.out.println(String.valueOf(ball) + "볼 " + String.valueOf(strike) + "스트라이크");
+        }
+        else if (ball != 0 && strike == 0) {
+            System.out.println(String.valueOf(ball) + "볼");
+        }
+        else if (ball == 0 && strike != 0) {
+            System.out.println(String.valueOf(strike) + "스트리이크");
         }
         return false;
     }
@@ -53,14 +75,8 @@ public class Application {
         return computerNum;
     }
 
-
     public static void main(String[] args) {
-        int choice = 1;
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while(choice == 1) {
-            numBaseball(opponentNum());
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            choice = Integer.parseInt(Console.readLine());
-        }
+        numBaseball(opponentNum());
     }
 }
