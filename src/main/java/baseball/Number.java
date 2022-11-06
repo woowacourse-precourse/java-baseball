bbcf2b64cdb790478ab9c06c99eb9c4b22a40bbc
wndnjs9878 <leeez0128@gmail.com>
2022-11-06 18:52:43 +0900

    feat($browser): check whether user number is valid(exception)

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -2,7 +2,10 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -15,6 +18,7 @@ public class Number {
     private static final int END_INCLUSIVE = 9;
 
     private static final String INPUT_USER_NUMBER_ANNOUNCE = "숫자를 입력해주세요 : ";
+    private static final String REGAX = "^["+START_INCLUSIVE+"-"+END_INCLUSIVE+"]*$";
     
     private List<Integer> gameNumber;
     
@@ -40,7 +44,17 @@ public class Number {
         this.gameNumber = computerNumber;
     }
 
+    private boolean isValidString(String str) {
+        if(!isPatternMatch(str) || !isRightLength(str) || !isNotDuplicate(str)) {
+            return false;
+        }
+        return true;
+    }
+
     private void setGameNumer(String input) {
+        if(!isValidString(input)) {
+            throw new IllegalArgumentException();
+        }
         this.gameNumber =  Arrays.stream(input.split(""))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
@@ -50,5 +64,21 @@ public class Number {
         System.out.print(INPUT_USER_NUMBER_ANNOUNCE);
         String input = Console.readLine();
         setGameNumer(input);
+    }
+
+    private boolean isPatternMatch(String input) {
+        return Pattern.compile(REGAX).matcher(input).matches();
+    }
+
+    private boolean isRightLength(String input) {
+        return input.length() == GAME_NUMBER_SIZE;
+    }
+
+    private boolean isNotDuplicate(String input) {
+        Set<Character> checkDuplication = new HashSet<>();
+        for(char s: input.toCharArray()) {
+            checkDuplication.add(s);
+        }
+        return input.length() == checkDuplication.size();
     }
 }
