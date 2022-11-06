db441428141d32c08e061b2d92e6d85b6ffe8d6b
Ridealist <cjsn92@gmail.com>
2022-11-07 01:22:27 +0900

    chore(Judge): 객체지향 생활 체조 원칙 적용 TODO 태그 작성

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -6,6 +6,7 @@ public class Judge {
 
     static final int GAME_COUNT = 3;
 
+    // TODO "indent(인덴트, 들여쓰기) depth를 3이 넘지 않도록 구현한다. 2까지만 허용한다." indent depth 줄여보기
     static int countStrike(List<Integer> userNumbers, List<Integer> computerNumbers) {
         int strikeCnt = 0;
         for (int i = 0; i < GAME_COUNT; i++) {
@@ -16,6 +17,8 @@ public class Judge {
         return strikeCnt;
     }
 
+    // TODO "indent(인덴트, 들여쓰기) depth를 3이 넘지 않도록 구현한다. 2까지만 허용한다." indent depth 줄여보기
+    // TODO Enum 클래스 적용 고민해보기
     static int countBall(List<Integer> userNumbers, List<Integer> computerNumbers) {
         int ballCnt = 0;
         for (int i = 0; i < GAME_COUNT; i++) {
@@ -28,6 +31,7 @@ public class Judge {
         return ballCnt;
     }
 
+    // TODO "else 예약어를 쓰지 않았는가?" 객체지향 생활 체조 원칙 적용해보기
     static void getGameScore(List<Integer> userNumbers, List<Integer> computerNumbers) {
         int strikeCnt = countStrike(userNumbers, computerNumbers);
         int ballCnt = countBall(userNumbers, computerNumbers);
