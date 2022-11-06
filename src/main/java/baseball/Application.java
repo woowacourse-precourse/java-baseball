e27825e8af8c5303267042b54853ae50dfffc167
5nam <ohnam90@gmail.com>
2022-11-06 19:06:16 +0900

    feat: Application 에 게임 시작, 게임 계속하는 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,11 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameManager gameManager = new GameManager();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            gameManager.startGame();
+            gameManager.continueGame();
+        }
     }
 }
