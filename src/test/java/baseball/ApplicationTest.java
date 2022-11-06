0a41f1d83bd27f1fea4b5cb4ce41c5fdac2f9120
LHS-11 <ghktjq1119@naver.com>
2022-11-06 18:02:43 +0900

    [Test] : 9. 예외사항 ( 3스트라이크일 때 1,2가 아닌 다른 숫자 입력했을 때 ) 에러 메시지 테스트 완료

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,11 +3,13 @@ package baseball;
 import baseball.model.Computer;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.constValue.Constants.ExceptionMessage.RESTART_INPUT_ERROR_MESSAGE;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,54 +40,71 @@ class ApplicationTest extends NsTest {
         Application.main(new String[]{});
     }
 
-    @Test
-    void 컴퓨터_숫자_생성기_크기_테스트() {
-        Computer computer = new Computer();
-        computer.generateComputerNumberThree();
-        assertThat(computer.getComputerNumber().size()).isEqualTo(3);
+    @Nested
+    class ComputerFeatOrExceptionTest {
+        @Test
+        void 컴퓨터_숫자_생성기_크기_테스트() {
+            Computer computer = new Computer();
+            computer.generateComputerNumberThree();
+            assertThat(computer.getComputerNumber().size()).isEqualTo(3);
 
-    }
+        }
 
-    @Test
-    void 컴퓨터_숫자_생성기_범위_테스트() {
-        Computer computer = new Computer();
-        computer.generateComputerNumberThree();
-        ArrayList<Integer> computerNumber = computer.getComputerNumber();
-        assertThat(computerNumber).containsAnyOf(1,2,3,4,5,6,7,8,9);
+        @Test
+        void 컴퓨터_숫자_생성기_범위_테스트() {
+            Computer computer = new Computer();
+            computer.generateComputerNumberThree();
+            ArrayList<Integer> computerNumber = computer.getComputerNumber();
+            assertThat(computerNumber).containsAnyOf(1, 2, 3, 4, 5, 6, 7, 8, 9);
 
-        for (Integer num : computerNumber) {
-            Assertions.assertTrue(num>=1 && num<=9);
+            for (Integer num : computerNumber) {
+                Assertions.assertTrue(num >= 1 && num <= 9);
+            }
+        }
+
+        @Test
+        void 컴퓨터_숫자_중복_숫자_테스트() {
+            Computer computer = new Computer();
+            computer.generateComputerNumberThree();
+            ArrayList<Integer> computerNumber = computer.getComputerNumber();
+            assertThat(computerNumber).doesNotHaveDuplicates();
+
+        }
+
+        @Test
+        void 컴퓨터_숫자_플레이어_숫자_스트라이크_카운트_테스트() {
+            Computer computer = new Computer();
+            int strike = computer.countStrike(new ArrayList<>(List.of(1, 2, 3)), new ArrayList<Integer>(List.of(1, 2, 3)));
+            assertThat(3).isEqualTo(strike);
+        }
+
+        @Test
+        void 컴퓨터_숫자_플레이어_숫자_볼_카운트_테스트() {
+            Computer computer = new Computer();
+            int ball = computer.countBall(new ArrayList<>(List.of(6, 7, 1)), new ArrayList<Integer>(List.of(7, 1, 3)));
+            assertThat(2).isEqualTo(ball);
+        }
+
+        @Test
+        void 힌트_출력_테스트() {
+            Computer computer = new Computer();
+            String hint = computer.getHint(new ArrayList<>(List.of(2, 1, 6)), new ArrayList<Integer>(List.of(7, 1, 3)));
+            assertThat("1스트라이크").isEqualTo(hint);
+        }
+
+        @Test
+        void 재시작_종료_입력_조건_예외_테스트(){
+            Computer computer = new Computer();
+            assertThatThrownBy(() -> {
+                computer.checkNotRestartInput("a");
+            }).isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(RESTART_INPUT_ERROR_MESSAGE);
         }
     }
 
-    @Test
-    void 컴퓨터_숫자_중복_숫자_테스트(){
-        Computer computer = new Computer();
-        computer.generateComputerNumberThree();
-        ArrayList<Integer> computerNumber = computer.getComputerNumber();
-        assertThat(computerNumber).doesNotHaveDuplicates();
-
-    }
-
-    @Test
-    void 컴퓨터_숫자_플레이어_숫자_스트라이크_카운트_테스트(){
-        Computer computer = new Computer();
-        int strike = computer.countStrike(new ArrayList<>(List.of(1, 2, 3)), new ArrayList<Integer>(List.of(1, 2, 3)));
-        assertThat(3).isEqualTo(strike);
-    }
-
-    @Test
-    void 컴퓨터_숫자_플레이어_숫자_볼_카운트_테스트(){
-        Computer computer = new Computer();
-        int ball = computer.countBall(new ArrayList<>(List.of(6, 7, 1)), new ArrayList<Integer>(List.of(7, 1, 3)));
-        assertThat(2).isEqualTo(ball);
-    }
-
-    @Test
-    void 힌트_출력_테스트(){
-        Computer computer = new Computer();
-        String hint = computer.getHint(new ArrayList<>(List.of(2,1,6)), new ArrayList<Integer>(List.of(7, 1, 3)));
-        assertThat("2볼").isEqualTo(hint);
-
+    @Nested
+    class PlayerFeatOrExceptionTest{
+//        @Test
+//        void
     }
 }
