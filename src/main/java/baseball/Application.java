7ccf771ad6abf5ebb64b703b0d4adf1bec787503
seonho <ysh10216@gmail.com>
2022-11-06 17:20:51 +0900

    feat(main): create getComputerNumbers method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public static List<Integer> getComputerNumbers(){
+        List<Integer> computer_numbers = new ArrayList<>();
+
+        while(computer_numbers.size() < 3){
+            int random_number = Randoms.pickNumberInRange(1, 9);
+
+            if(!computer_numbers.contains(random_number)){
+                computer_numbers.add(random_number);
+            }
+        }
+
+        return computer_numbers;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
