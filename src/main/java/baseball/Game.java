37c4c2160ba08198d4fd42f53576b30bd5e81fd7
jthugg <jthug@naver.com>
2022-11-06 11:21:43 +0900

    Feat: 게임 실행 시퀀스 구축

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,28 @@
 package baseball;
 
 public class Game {
+	Computer computer;
+	Status status;
+	User user;
 
+	int strike = 0;
+	int ball = 0;
+
+	Game(Computer computer, Status status, User user) {
+		this.computer = computer;
+		this.status = status;
+		this.user = user;
+	}
+
+	public void gameStart() {
+		System.out.println(MESSAGE_START);
+		status.setStatusPlaying();
+
+		while(strike < 3) {
+			compare();
+		}
+
+		System.out.println(MESSAGE_RETRY);
+		retryOrNot();
+	}
 }
