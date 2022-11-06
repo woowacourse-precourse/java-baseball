c963aa10469ea4d144ca78c481f34387e6857e38
SeongukBaek <bsu6235@gmail.com>
2022-11-06 22:48:09 +0900

    refactor: extract methods from proceedGameUntil3S()

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -64,22 +64,7 @@ public class Application {
                 System.out.print(INPUT_MESSAGE);
                 User.setGameNumber();
 
-                Map<Character, Integer> numberIndexMap = computer.getNumberIndexMap();
-                String userGameNumber = user.getGameNumber();
-                int score = 0;
-
-                for (int index = 0; index < userGameNumber.length(); index++) {
-                    char currentUserNumber = userGameNumber.charAt(index);
-                    if (numberIndexMap.containsKey(currentUserNumber)) {
-                        if (numberIndexMap.get(currentUserNumber) == index) {
-                            score += 10;
-                        } else {
-                            score += 1;
-                        }
-                    }
-                }
-
-                result = Result.getResultByCode(score);
+                result = Result.getResultByCode(calculateScore(computer, user));
                 System.out.println(result.getMessage());
             }
         } catch (IllegalArgumentException exception) {
@@ -99,5 +84,39 @@ public class Application {
         } catch (IllegalArgumentException exception) {
             throw new IllegalArgumentException();
         }
+    }
+
+    /*
+     * 컴퓨터와 사용자의 정보를 받아 점수를 계산한다.
+     * 스트라이크 10점, 볼 1점, 낫싱 0점
+     *
+     * @return int
+     * */
+    private static int calculateScore(Computer computer, User user) {
+        Map<Character, Integer> numberIndexMap = computer.getNumberIndexMap();
+        String userGameNumber = user.getGameNumber();
+        int score = 0;
+
+        for (int index = 0; index < userGameNumber.length(); index++) {
+            char currentUserNumber = userGameNumber.charAt(index);
+            if (numberIndexMap.containsKey(currentUserNumber)) {
+                score += computeScoreIndex(numberIndexMap.get(currentUserNumber), index);
+            }
+        }
+
+        return score;
+    }
+
+    /*
+     * 컴퓨터 숫자별 인덱스 정보 Map에서 가져온 numberIndex와 사용자의 현재 숫자를 받아, 점수를 반환
+     * 같다면 10점, 다르다면 1점
+     *
+     * @return int
+     * */
+    private static int computeScoreIndex(int numberIndex, int index) {
+        if (numberIndex == index) {
+            return 10;
+        }
+        return 1;
     }
 }
