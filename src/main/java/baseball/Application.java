42572e086fbb484bed31e8ab5ee78224ae1e1c68
Jongha Park <jongha2788@u.sogang.ac.kr>
2022-11-07 01:03:45 +0900

    feat: 랜덤으로 세개의 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
+    static int[] number = new int[3];
+    public static int getRandomNumber()
+    {
+        return Randoms.pickNumberInRange(1, 9);
+    }
+    public static void gameInitialize()
+    {
+        for (int i = 0 ; i< 3; i++)
+            number[i] = getRandomNumber();
+    }
+
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        //gameInitialize();
+        //System.out.println(number[0]);
+        //System.out.println(number[1]);
+        //System.out.println(number[2]);
+
     }
 }
