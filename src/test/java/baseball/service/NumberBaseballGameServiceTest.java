53a75639eafcc22e3aabf1aaf76441bc0b223a25
ksh-beginner <ohk9134@naver.com>
2022-11-06 09:44:32 +0900

    test(NumberBaseballGameServiceTest) : 사용자 수를 받아서 스트라이크/볼 판정 테스트

--- src/test/java/baseball/service/NumberBaseballGameServiceTest.java
+++ src/test/java/baseball/service/NumberBaseballGameServiceTest.java
@@ -1,6 +1,9 @@
 package baseball.service;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -13,5 +16,17 @@ public class NumberBaseballGameServiceTest {
         if (numberBaseballGameService.getOpponentComputer() == null) {
             fail();
         }
+    }
+
+    @Test
+    void 사용자_수를_받아서_스트라이크_볼을_판정() {
+        NumberBaseballGameService numberBaseballGameService = new NumberBaseballGameService();
+        numberBaseballGameService.generateOpponentComputer();
+        int opponentComputerNumber = numberBaseballGameService.getOpponentComputer().getNumber();
+        Map<String, Integer> strikeOrBallCountMap =
+                numberBaseballGameService.judgeStrikeOrBallCount(String.valueOf(opponentComputerNumber));
+        Integer strikeCount = strikeOrBallCountMap.get("Strike");
+
+        Assertions.assertThat(strikeCount).isEqualTo(3);
     }
 }
