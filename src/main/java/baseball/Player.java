d6eade144cad250737f8e0ef7b4a46498d9ddffb
Park shinyeong <syngh503@gmail.com>
2022-11-06 14:01:09 +0900

    Feat(Player): 게임 플레이어 관련 클래스 생성
    게임 플레이어의 입력을 체크하고 저장하는 클래스를 생성하였습니다.
    BaseballNumber 클래스를 상속받습니다.
    
    Relate to: #6

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import java.util.List;
+
+public class Player extends BaseballNumber{
+    @Override
+    public boolean isValidBaseballNumber (List<Integer> baseballNumber){
+        return true;
+    }
+
+}
