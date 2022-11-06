589c30308ff5be243c16becb3c4fdf8bc8b8a622
jissssoo1717 <jisoolee7101@gmail.com>
2022-11-06 20:09:51 +0900

    feat: 게임 종료 시 사용자 입력받아 게임 재시작하거나 종료하는 메소드 생성

--- src/main/java/baseball/GameSystem.java
+++ src/main/java/baseball/GameSystem.java
@@ -7,6 +7,8 @@ import java.util.List;
 public class GameSystem {
     private static final String STR_GAME_START = "숫자 야구 게임을 시작합니다.";
     private static final String STR_PLAYER_INPUT = "숫자를 입력해주세요 : ";
+    private static final String STR_GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String STR_GAME_AGAIN ="게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     GameScore gameScore = new GameScore();
     PlayerInput player = new PlayerInput();
@@ -36,7 +38,7 @@ public class GameSystem {
         return Console.readLine();
     }
 
-    private void GetNumber() {
+    private void GetGameReturnValue() {
         numStrikes = gameScore.CheckStrikes(numOfComputer, numOfPlayer);
         numBalls = gameScore.CheckBalls(numOfComputer, numOfPlayer);
     }
@@ -45,10 +47,33 @@ public class GameSystem {
         numOfComputer = computer.randomNumber;
         numOfPlayer = player.ReturnPlayerNum(InputNumPlayer());
 
-        GetNumber();
+        GetGameReturnValue();
 
         System.out.println(numOfComputer+"  "+numOfPlayer);
         System.out.println(gameScore.ReturnScore(numStrikes, numBalls));
+
+        if(numStrikes == 3) {
+            System.out.println(STR_GAME_END);
+            GameStartAgain();
+        }
+    }
+
+    public void GameStartAgain() {
+        System.out.println(STR_GAME_AGAIN);
+    }
+
+    public boolean InputGameRestartOrEnd() {
+        String inputReOrEnd = Console.readLine();
+
+        if(inputReOrEnd.equals("1")) {
+            return true;
+        }
+
+        if(inputReOrEnd.equals("2")) {
+            return false;
+        }
+
+        return false; // 이외의 값
     }
 
 }
