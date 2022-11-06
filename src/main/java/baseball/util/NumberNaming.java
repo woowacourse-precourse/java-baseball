76b2b78fcc9ecc56acf6ffe904489d1d098b22ac
Jeongthingamajig <netpetet@naver.com>
2022-11-07 00:47:13 +0900

    Feat : 클래스 추가
    유저의 행동에 따라 출력하는 메세지를 모아놓은 클래스

--- src/main/java/baseball/util/NumberNaming.java
+++ src/main/java/baseball/util/NumberNaming.java
@@ -1,11 +1,13 @@
-package baseball;
-
+package baseball.util;
+/*
+변수 네이밍
+ */
 public class NumberNaming {
     public final static int MIN_RANGE_NUM = 1;
     public final static int MAX_RANGE_NUM = 9;
     public final static int MAX_NUMBER_SIZE = 3;
 
-    public final static int RE_START_GAME = 3;
-    public final static int GAME_END = 3;
+    public final static int RE_START_GAME = 1;
+    public final static int GAME_END = 2;
 
 }
