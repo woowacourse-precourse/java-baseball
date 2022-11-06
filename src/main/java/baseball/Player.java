74fe8c69d19b4ec7376f1bf02aa518e1daeff376
5nam <ohnam90@gmail.com>
2022-11-06 16:12:10 +0900

    feat: String 을 List<Integer> 로 변환하는 함수 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Player {
     private static final Player PLAYER = new Player();
 
@@ -7,5 +12,11 @@ public class Player {
         return PLAYER;
     }
 
+    private List<Integer> stringToList(String userInput) {
+        int[] input = Stream.of(userInput.split("")).mapToInt(Integer::parseInt).toArray();
+        return Arrays.stream(input)
+                .boxed()
+                .collect(Collectors.toList());
+    }
 
 }
