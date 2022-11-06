1208c21e14d827de08c84d0b448a8bbcb7c1994d
Jaehong Lee <woohong24@naver.com>
2022-11-06 23:04:07 +0900

    feat: add function 'computerSelectNumber()'

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    private static ComputerNumber computerNumber;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -13,6 +19,18 @@ public class Application {
     }
 
     private static void computerSelectNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        computerNumber = new ComputerNumber(computer.get(0), computer.get(1), computer.get(2));
+        playerSelectNumber();
+    }
+
+    private static void playerSelectNumber() {
 
     }
 }
