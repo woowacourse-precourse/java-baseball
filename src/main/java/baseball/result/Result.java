2bf9af4d6b93614f84394ae521f1c4c9bf58ac9b
채희찬 <eqholic1125@gmail.com>
2022-11-06 21:19:51 +0900

    feat: Result 클래스 생성 및 생성자 생성

---
+++ src/main/java/baseball/result/Result.java
@@ -0,0 +1,11 @@
+package baseball.result;
+
+public class Result {
+    private Integer strikeCount;
+    private Integer ballCount;
+
+    public Result(Integer strikeCount, Integer ballCount){
+        this.strikeCount = strikeCount;
+        this.ballCount = ballCount;
+    }
+}
