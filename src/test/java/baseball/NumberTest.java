1a0ce08a172ba6d4bccc6b999e9a8cd018d66df4
aossuper7 <aossuper7@naver.com>
2022-11-06 15:28:23 +0900

    Test(NumberTest): 숫자가 아닌 다른게 포함되어 있다면 예외발생 Test

--- src/test/java/baseball/NumberTest.java
+++ src/test/java/baseball/NumberTest.java
@@ -10,6 +10,20 @@ public class NumberTest {
     private Number number = new Number();
 
     @Test
+    void 숫자가_아닌_다른게_포함되면_예외발생() {
+        Assertions.assertThatThrownBy(() -> {
+            number.createUserNumber("a12");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자가_아닌_다른게_포함되면_예외발생2() {
+        Assertions.assertThatThrownBy(() -> {
+                number.createUserNumber("1 2");
+            }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     void 정상적인_입력() {
         Assertions.assertThat(number.createUserNumber("159")).containsExactly(1,5,9);
         Assertions.assertThat(number.createUserNumber("142")).containsExactly(1,4,2);
