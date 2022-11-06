9f97c465cd47897ac51c947122d5cec0a449e46f
yoonjuho <dbswn990@naver.com>
2022-11-07 06:01:37 +0900

    feat($Application) : 프로그램 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Number numFactory = new PlayerNum();
+        Computer computer = new Computer();
+        List<Integer> playerNumber;
+        while (true) {
+            playerNumber = numFactory.createNumber();
+            numFactory.verifyNumber(playerNumber);
+            if (computer.getResult(playerNumber) == "3스트라이크") {
+                if (computer.closeGame()) {
+                    computer = new Computer();
+                    continue;
+                } else {
+                    break;
+                }
+            }
+        }
     }
 }
