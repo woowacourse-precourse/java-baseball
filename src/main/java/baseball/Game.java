327cd4ac1967d0032d7a685713c06a20a7fc3da5
yeada <yeadalm@gmail.com>
2022-11-06 23:31:33 +0900

    feat: 볼 판단 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -12,5 +12,18 @@ public class Game {
         return count;
     }
 
+    public static int ball(List<Integer> user, List<Integer> computer) {
+        int count = 0;
+        List<Integer> tempUser = new ArrayList<>();
+        for (int num = 0; num < user.size(); num++) {
+            tempUser.addAll(user);
+            tempUser.remove(user.get(num));
+
+            if (tempUser.contains(computer.get(num))) count++;
+            System.out.println(tempUser + "<- tempUser " + count + "<-count");
+            tempUser.removeAll(user);
+        }
+        return count;
+    }
 }
 
