909f058ef41c5fea2643d541bb64619a78ea057b
Seiyon Park <seiyonpdev@gmail.com>
2022-11-06 22:48:27 +0900

    feat(Application): 메소드 correctOrHint 구현
    숫자 야구 게임의 정답과 플레이어의 답을 받아 비교한 뒤 힌트를 주고, 정답인지 오답인지를 Boolean으로 반환하는 메소드 correctOrHint를 구현했다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -67,4 +67,25 @@ public class Application {
         }
         return numericOrNot;
     }
+
+    static Boolean correctOrHint(String playerGuess, String answer) {
+        boolean correct = false;
+        List<Integer> strikeBallCnt = cntStrikeBall(playerGuess, answer);
+        int strike = strikeBallCnt.get(0);
+        int ball = strikeBallCnt.get(1);
+
+        if (strike == 3) {
+            System.out.println(strike + "스트라이크");
+            correct = true;
+        } else if (ball + strike == 0) {
+            System.out.println("낫싱");
+        } else if (ball == 0 && strike > 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (strike == 0 && ball > 0) {
+            System.out.println(ball + "볼");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+        return correct;
+    }
 }
