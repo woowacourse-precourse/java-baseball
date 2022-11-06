496587946b90791fe83e989f8f02b5153a859985
NoseSSG <dmlcks1998@naver.com>
2022-11-07 00:10:27 +0900

    FEAT : 최종완성구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -121,7 +121,12 @@ public class Application {
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        boolean again = true;
+        while(again) {
+            List<Integer> answer_num = make_rand_num();
+            doing_baseball(answer_num);
+            again = check_again();
+        }
     }
 
 }
