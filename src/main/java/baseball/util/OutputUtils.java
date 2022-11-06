1262c08b7599e475e7a1fb4de078d824ace33d07
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-06 23:52:42 +0900

    refactor(Output): 시스템 외부로 출력하는 기능을 한곳에서 제공하도록 변경

---
+++ src/main/java/baseball/util/OutputUtils.java
@@ -0,0 +1,26 @@
+package baseball.util;
+
+import baseball.domain.GameResult;
+
+public class OutputUtils {
+
+    public static void printInitView() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printInputView() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    public static void printFinishView() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static void printRestartView() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public static void printGameResultView(GameResult gameResult) {
+        System.out.println(gameResult);
+    }
+}
