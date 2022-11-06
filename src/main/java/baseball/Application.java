4734010f7b751fe3e8ecbd8774f9be51ef343186
kimdonghun <zhfptm12@naver.com>
2022-11-06 22:29:10 +0900

    Feat : 컴퓨터 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> computer = new ArrayList<>();
+        
+
+    }
+
+    private static List<Integer> createComputerNumber(List<Integer> computer) {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
