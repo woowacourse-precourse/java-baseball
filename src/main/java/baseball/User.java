a3f995504ee838a40ced89f13e2d0416cb961c90
hunko <rhwlgns4386@naver.com>
2022-11-06 21:09:23 +0900

    feat : Creating a function that raises a non-numeric input exception

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -2,8 +2,13 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.regex.Pattern;
+
 public class User {
+    private static final  Pattern compile= Pattern.compile("(([^1-9])\\2{0,})");
     public String getConsoleInput() {
-        return Console.readLine();
+        String input = Console.readLine();
+        if(compile.matcher(input).find()) throw new IllegalArgumentException();
+        return input;
     }
 }
--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -6,6 +6,7 @@ import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UserTest {
 
@@ -15,5 +16,12 @@ public class UserTest {
         System.setIn(new ByteArrayInputStream("123".getBytes(StandardCharsets.UTF_8)));
         String input=user.getConsoleInput();
         assertThat("123").isEqualTo(input);
+    }
+
+    @Test
+    public void 사용자입력_실패_케이스1(){
+        User user = new User();
+        System.setIn(new ByteArrayInputStream("13a".getBytes(StandardCharsets.UTF_8)));
+        assertThatThrownBy(()->user.getConsoleInput()).isInstanceOf(IllegalArgumentException.class);
     }
 }
