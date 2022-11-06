43ae67b1aefd3671a30fd1165957c18360f187d2
jiwon <ljwon77@daum.net>
2022-11-07 00:54:59 +0900

    style: rename validation method name
    검증 메서드의 이름을 verify* 대신 validated*로 수정

--- src/main/java/baseball/validation/InputValidation.java
+++ src/main/java/baseball/validation/InputValidation.java
@@ -1,6 +1,5 @@
 package baseball.validation;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,15 +15,15 @@ public class InputValidation {
     /**
      * 사용자의 숫자 입력값에 대한 모든 검증을 진행한다.
      */
-    public List<Integer> verifyInputNum(String input) {
-        verifyInputLength(input);
+    public List<Integer> validateInputNum(String input) {
+        validateInputLength(input);
 
         List<Integer> inputNums = Arrays.stream(input.split(""))
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
-        verifyInputRange(inputNums);
-        verifyInputDuplicated(inputNums);
+        validateInputRange(inputNums);
+        validateInputDuplicated(inputNums);
 
         return inputNums;
     }
@@ -33,7 +32,7 @@ public class InputValidation {
      * 입력값의 길이가 3인지 검증한다.
      * @param input 사용자의 입력값
      */
-    public void verifyInputLength(String input) {
+    public void validateInputLength(String input) {
         if (input.length() != INPUT_LENGTH) {
             throw new IllegalArgumentException(INPUT_LENGTH_EXCEPTION_MSG);
         }
@@ -43,7 +42,7 @@ public class InputValidation {
      * 입력값이 1~9 사이의 숫자인지 검증한다.
      * @param inputNums 사용자의 입력값의 각 자리를 쪼갠 리스트
      */
-    public void verifyInputRange(List<Integer> inputNums) {
+    public void validateInputRange(List<Integer> inputNums) {
         boolean checkRange = inputNums.stream()
                 .allMatch(num -> FIRST_RANGE <= num && num <= LAST_RANGE);
 
@@ -56,7 +55,7 @@ public class InputValidation {
      * 입력값에 중복된 값이 존재하는지 검증한다.
      * @param inputNums 사용자의 입력값의 각 자리를 쪼갠 리스트
      */
-    public void verifyInputDuplicated(List<Integer> inputNums) {
+    public void validateInputDuplicated(List<Integer> inputNums) {
         if (inputNums.stream().distinct().count() != INPUT_LENGTH) {
             throw new IllegalArgumentException(INPUT_DUPLICATE_EXCEPTION_MSG);
         }
@@ -66,7 +65,7 @@ public class InputValidation {
      * 게임 진행을 제어하는 입력값에 대해 검증한다.
      * @param input 사용자의 입력값
      */
-    public void verifyGameControlInput(int input) {
+    public void validateGameControlInput(int input) {
         if (input < RESTART_NUM || input > EXIT_NUM) {
             throw new IllegalArgumentException(GAME_CONTROL_EXCEPTION_MSG);
         }
