f6331130d7cb087cf1771add5202293ee570f833
jingyu9804 <swc05161@gmail.com>
2022-11-06 18:12:37 +0900

    feat : giveComputerNumber 반환형 및 기능 변경

--- src/main/java/baseball/RandomUtil.java
+++ src/main/java/baseball/RandomUtil.java
@@ -6,20 +6,11 @@ import java.util.HashSet;
 
 public class RandomUtil {
 
-    public HashSet<String> makeComputerNumber(){
+    public static HashSet<String> giveComputerNumber(){
         HashSet<String> computerNumberSet = new HashSet<>();
         while(computerNumberSet.size()<3){
             computerNumberSet.add(Integer.toString(Randoms.pickNumberInRange(1,9)));
         }
         return computerNumberSet;
-    }
-
-    public static String giveComputerNumber(){
-        RandomUtil randomUtil = new RandomUtil();
-
-        String computerNumber = "";
-        for(String str:randomUtil.makeComputerNumber()) computerNumber+=str;
-
-        return computerNumber;
     }
 }
