9f3401fafe30011bdd85371292acdbe1a178b7ff
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-06 17:17:46 +0900

    feat: BaseballGameView  - printRestartMessage 메서드 추가
    게임 재시작 여부 메시지 출력

--- src/main/java/baseball/view/BaseballGameView.java
+++ src/main/java/baseball/view/BaseballGameView.java
@@ -27,4 +27,8 @@ public class BaseballGameView {
     public void printEndMessage() {
         System.out.println("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public void printRestartMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
