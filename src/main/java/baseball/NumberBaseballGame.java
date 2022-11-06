bc4f7eb95f17dbba05cf934ef54b96f8641c6a63
KimSangHyeok <tkdgur0906@naver.com>
2022-11-07 00:15:57 +0900

    Feat: 게임 결과를 문자열로 바꿔주는 기능 추가

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -47,4 +47,17 @@ public class NumberBaseballGame {
         }
     }
 
+    public static String getResultToString(){
+        if(ball != 0 && strike != 0){
+            return ball + "볼 " + strike + "스트라이크";
+        }
+        if(ball != 0 && strike == 0){
+            return ball + "볼";
+        }
+        if(ball == 0 && strike != 0){
+            return strike + "스트라이크";
+        }
+        return "낫싱";
+    }
+
 }
