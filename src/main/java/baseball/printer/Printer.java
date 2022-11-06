c7d40ebca9ee9ee9ff03f4444ec49a130a0180d5
채희찬 <eqholic1125@gmail.com>
2022-11-06 21:29:54 +0900

    feat: 결과 출력

--- src/main/java/baseball/printer/Printer.java
+++ src/main/java/baseball/printer/Printer.java
@@ -1,13 +1,29 @@
 package baseball.printer;
 
+import baseball.result.Result;
+
 public class Printer {
 
     public void greet() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void requestUserNumber(){
+    public void requestUserNumber() {
         System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public void printBallCount(Result result) {
+        if (result.isNothing()) {
+            System.out.println("낫싱");
+            return;
+        }
+        if (result.getBallCount() != 0) {
+            System.out.println(result.getBallCount() + "볼 ");
+        }
+        if (result.getStrikeCount() != 0) {
+            System.out.println(result.getStrikeCount() + "스트라이크");
+        }
+        System.out.println();
     }
 
 }
