c92d0899dbf380a3bdd43927e4a52c55ce808a18
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-06 17:29:25 +0900

    refactor(GameController): PlayGame 리팩터링에 따른 코드 수정

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -10,7 +10,6 @@ public class GameController {
             playGame.startGame();
             playGame.playingGame();
             playGame.endGame();
-            playGame.replayGame();
     }
 
 }
