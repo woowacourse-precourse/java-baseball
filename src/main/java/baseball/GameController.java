dd419939f95691ad21fb31676304c169172c6c8c
abbd2 <ajkety00@gmail.com>
2022-11-07 03:39:22 +0900

    refactor(GameController): 생성자를 통한 갱신이 아닌 리턴하여 갱신

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -10,12 +10,12 @@ public class GameController {
 
     NumberVO numberVO = new NumberVO();
 
-    public void inputUserNumber() {
+    public List<Integer> inputUserNumber() {
 
         String userNumber = Console.readLine(); // 입력 받기
         List<Integer> userNumberList = userNumberConvertToList(userNumber); // 자릿수로 나눈 리스트로 변환
-        numberVO.setUserNumber(userNumberList);
-        System.out.println("사용자 값:"+numberVO.getUserNumber()+":");
+        System.out.println(userNumberList);
+        return userNumberList;
     }
 
     // 숫자 하나씩 나누어서 리스트에 보관
@@ -26,17 +26,11 @@ public class GameController {
     }
 
     // userNumber와 randomNumber의 교집합 검사
-    public List<Integer> checkContains(){
-
-        List<Integer> userNumber = numberVO.getUserNumber();
-        List<Integer> randomNumber = numberVO.getRandomNumber();
-
-        return randomNumber.stream().filter(userNumber::contains).collect(Collectors.toList());
+    public List<Integer> checkContains(List<Integer> userNumberList, List<Integer> radomNumberList) {
+        return radomNumberList.stream().filter(userNumberList::contains).collect(Collectors.toList());
     }
 
-    public void compareGameCount(){
-
-
-
+    public void compareGameCount(List<Integer> userNumberList, List<Integer> radomNumberList) {
+        System.out.println("교집합:" + checkContains(userNumberList, radomNumberList) + ":");
     }
 }
