0d3ff33661da65f65c17d7a18eb96b98840e629c
aossuper7 <aossuper7@naver.com>
2022-11-06 15:29:04 +0900

    Test(NumberTest): 입력값 길이가 3이 아니면 예외발생 Test

--- src/test/java/baseball/NumberTest.java
+++ src/test/java/baseball/NumberTest.java
@@ -24,6 +24,20 @@ public class NumberTest {
     }
 
     @Test
+    void 길이가_3_아니면_예외발생() {
+        Assertions.assertThatThrownBy(() -> {
+            number.createUserNumber("1234");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 길이가_3_아니면_예외발생2() {
+        Assertions.assertThatThrownBy(() -> {
+                number.createUserNumber("12");
+            }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
     void 정상적인_입력() {
         Assertions.assertThat(number.createUserNumber("159")).containsExactly(1,5,9);
         Assertions.assertThat(number.createUserNumber("142")).containsExactly(1,4,2);
