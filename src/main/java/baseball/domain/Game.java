5d80f29207a5a35c8a0bf93b8946522fcf376208
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-07 00:16:08 +0900

    style(Game): GameResult에서 Game으로 명칭 변경
    게임 결과 뿐만 아니라 상태도 관리하고 있기 때문에 넓은 개념으로 확장

---
+++ src/main/java/baseball/domain/Game.java
@@ -0,0 +1,75 @@
+package baseball.domain;
+
+
+import static baseball.domain.Command.isExit;
+import static baseball.domain.Command.isRestart;
+import static baseball.domain.Game.GameStatus.END;
+import static baseball.domain.Game.GameStatus.RESTART;
+import static baseball.domain.Hint.*;
+
+
+public final class Game {
+
+    private int strike;
+
+    private int ball;
+
+    public enum GameStatus {
+        PROGRESS,
+        RESTART,
+        END
+    }
+
+    private GameStatus gameStatus;
+
+    public Game() {
+        this.gameStatus = GameStatus.PROGRESS;
+    }
+
+    public GameStatus getStatus() {
+        return gameStatus;
+    }
+
+    public int getStrike() { return strike; }
+
+    public void setGameResult(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public void setStatusByCommand(String command){
+        if (isExit(command)) {
+            this.gameStatus = END;
+        }
+
+        if (isRestart(command)) {
+            this.gameStatus = RESTART;
+        }
+    }
+
+    public void restart() {
+        this.gameStatus = GameStatus.PROGRESS;
+    }
+
+    public void clearResult() {
+        strike = 0;
+        ball = 0;
+    }
+
+    @Override
+    public String toString() {
+        if (ball == 0 && strike == 0) {
+            return NOTHING.name();
+        }
+
+        if (strike == 0) {
+            return ball + BALL.name();
+        }
+
+        if (ball == 0) {
+            return strike + STRIKE.name();
+        }
+
+        return ball + BALL.name() + " " + strike + STRIKE.name();
+    }
+}
