9568777e59503653b57fd2034b9ea5b6070ad3f9
이지혁 <olzlgur@naver.com>
2022-11-06 21:23:21 +0900

    feat: 결과 값 계산 클래스 및 생성자 구현

---
+++ src/main/java/baseball/result/Result.java
@@ -0,0 +1,11 @@
+package baseball.result;
+
+public class Result {
+    private int strike;
+    private int ball;
+
+    public Result(int strike, int ball) {
+        this.strike = 0;
+        this.ball = 0;
+    }
+}
