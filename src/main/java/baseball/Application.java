9f3fb0ef6c0190d1ec75be43406cb52aaada834b
korjun1993 <corojoon93@naver.com>
2022-11-06 15:37:34 +0900

    feat (Application) : 플레이어 생성 기능 추가
    숫자 게임을 진행할 컴퓨터, 유저 플레이어를 생성한다. 유저 플레이어의 숫자 입력이 비정상적이면 IllegalArgumentException 을 발생시키고 애플리케이션을 종료한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        Player computer = createPlayer(NumberUtil.random());
+        Player user = createPlayer(NumberUtil.input());
+        if (!user.isPossiblePlay()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static Player createPlayer(List<Integer> numbers) {
+        return new Player(numbers);
     }
 }
