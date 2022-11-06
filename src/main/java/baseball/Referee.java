223d767c8f8d77bdc53cd3cce7c3bebdba1bd6f8
miffyKing <whqudghk28@gmail.com>
2022-11-06 17:50:22 +0900

    fix: Change of code not getting random value from computer straightly. Using RandomGenerator Class to keep random Value safely.

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,51 +1,50 @@
 package baseball;
 
+import java.util.ArrayList;
+
 public class Referee {
 
-  public static String Answer;
-  private static final int CORRECTINPUTLENGTH = 3;
+    private static final int CORRECTINPUTLENGTH = 3;
+    private static RandomNumber randNum;
 
-  public static String calculateAnswer(String userInput, String randNum) {
-    int ball;
-    int strike;
-
-    ball = calculateBall(userInput, randNum);
-    strike = calculateStrike(userInput, randNum);
-    if (strike == 0 && ball == 0) {
-      return "낫싱";
+    Referee() {
+        randNum = RandomGenerator.generateRandom(new StringBuilder(), new ArrayList<Integer>());
     }
-    if (ball != 0 && strike != 0) {
-      return (ball + "볼 " + strike + "스트라이크");
-    } else if (ball != 0 && strike == 0) {
-      return (ball + "볼");
-    } else if (ball == 0 && strike != 0) {
-      return (strike + "스트라이크");
-    }
-    return "";
-  }
 
-  private static int calculateBall(String userInput, String randNum) {
-    int strikeNum = calculateStrike(userInput, randNum);
-    int ballNum = 0;
-    for (int i = 0; i < CORRECTINPUTLENGTH; i++) {
-      if (randNum.indexOf(userInput.charAt(i)) != -1) {
-        ballNum++;
-      }
-    }
-    return (ballNum - strikeNum);
-  }
+    public static String calculateAnswer(String userInput) {
+        int ball;
+        int strike;
 
-  private static int calculateStrike(String userInput, String randNum) {
-    int strikeCnt = 0;
-    for (int i = 0; i < CORRECTINPUTLENGTH; i++) {
-      if (userInput.charAt(i) == randNum.charAt(i)) {
-        strikeCnt++;
-      }
+        ball = calculateBall(userInput, randNum);
+        strike = calculateStrike(userInput, randNum);
+        if (strike == 0 && ball == 0) {
+            return "낫싱";
+        }
+        if (ball != 0 && strike != 0) {
+            return (ball + "볼 " + strike + "스트라이크");
+        } else if (ball != 0 && strike == 0) {
+            return (ball + "볼");
+        } else if (ball == 0 && strike != 0) {
+            return (strike + "스트라이크");
+        }
+        return "";
     }
-    return strikeCnt;
-  }
 
-  public static void testReferee(String userInput, String randNum) {
-    System.out.println(calculateAnswer(userInput, randNum));
-  }
+    private static int calculateBall(String userInput, RandomNumber randNum) {
+        int strikeNum = calculateStrike(userInput, randNum);
+        int ballNum = 0;
+        ballNum = randNum.countDuplicate(userInput);
+        return (ballNum - strikeNum);
+    }
+
+    private static int calculateStrike(String userInput, RandomNumber randNum) {
+        int strikeCnt = 0;
+        for (int i = 0; i < CORRECTINPUTLENGTH; i++) {
+            if (userInput.charAt(i) == randNum.randomNumInChar(i)) {
+                strikeCnt++;
+            }
+        }
+        return strikeCnt;
+    }
+
 }
