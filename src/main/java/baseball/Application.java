27f5382426cefb54de07328083c1fb927af2f6f3
ddogga <hayeonlee60327@gmail.com>
2022-11-06 23:26:54 +0900

    스트라이크 개수 구하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,13 +12,28 @@ public class Application {
         // TODO: 프로그램 구현
 
         List<Integer> computer_num = Randoms.pickUniqueNumbersInRange(1,9,3);
+        System.out.println("computer_num = " + computer_num);
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.println("숫자를 입력해주세요 : ");
         String answer = Console.readLine();
 
-
+        int strike = checkStrike(computer_num,answer);
+        System.out.println("strike = " + strike);
     }
+
+    public static int checkStrike(List<Integer> computer_num, String answer) {
+        int cnt = 0;
+        for (int i = 0; i < 3; i++){
+            if (computer_num.get(i) == answer.charAt(i) - '0'){
+                cnt += 1;
+            }
+        }
+        
+        return cnt;
+    }
+
+
 
 
 }
