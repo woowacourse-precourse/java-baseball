af5399192ef744b5235ff5d036e3b6f9ae69cfbf
hunko <rhwlgns4386@naver.com>
2022-11-06 21:35:02 +0900

    chore : make  user BaseBallNumber make funtion

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -2,13 +2,23 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class User {
     private static final  Pattern compile= Pattern.compile("(([^1-9])\\2{0,})");
+    private BaseBallNumber number;
     public String getConsoleInput() {
         String input = Console.readLine();
         if(compile.matcher(input).find()) throw new IllegalArgumentException();
         return input;
+    }
+
+    public void makeBaseBallNumber() {
+        String consoleInput = getConsoleInput();
+        List<Integer> integerList = Arrays.stream(consoleInput.split("")).map(s -> Integer.parseInt(s)).collect(Collectors.toList());
+        number=BaseBallNumber.of(integerList);
     }
 }
--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -23,5 +24,19 @@ public class UserTest {
         User user = new User();
         System.setIn(new ByteArrayInputStream("13a".getBytes(StandardCharsets.UTF_8)));
         assertThatThrownBy(()->user.getConsoleInput()).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 사용자입력_실패_케이스2(){
+        User user = new User();
+        System.setIn(new ByteArrayInputStream("133".getBytes(StandardCharsets.UTF_8)));
+        assertThatThrownBy(()->user.makeBaseBallNumber()).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void 사용자입력_실패_케이스3(){
+        User user = new User();
+        System.setIn(new ByteArrayInputStream("1234".getBytes(StandardCharsets.UTF_8)));
+        assertThatThrownBy(()->user.makeBaseBallNumber()).isInstanceOf(IllegalArgumentException.class);
     }
 }
