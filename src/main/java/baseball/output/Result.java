60e08574772d3d850718fa578d09076d1ce548f8
MINJU <alswn9938@gmail.com>
2022-11-07 00:55:14 +0900

    refactor($Result): modify Result 클래스 추가
    [userNumberList와 ComputerNumberList에 따른 결과값 출력] 관련 로직 해당 클래스로 분리

---
+++ src/main/java/baseball/output/Result.java
@@ -0,0 +1,84 @@
+package baseball.output;
+
+import baseball.BallType;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Result {
+    private StringBuilder sb = new StringBuilder();
+    private String result;
+    private boolean isCorrect;
+
+    public Result(List<Integer> userNumberList, List<Integer> computerNumberList){
+        this.result =  getResult(userNumberList, computerNumberList);
+        this.isCorrect = (result.equals("3스트라이크"));
+    }
+
+    public void printResult() {
+        System.out.println(result);
+    }
+
+    public boolean getIsCorrect() {
+        return isCorrect;
+    }
+
+
+    public String getResult(List<Integer> userNumberList, List<Integer> computerNumberList){
+        Map<String, Integer> typeScoreMap = getTypeScoreMap();
+
+        for(int index = 0; index < 3; index++){
+            int nowDigit = userNumberList.get(index);
+            String digitResult = getDigitResult(nowDigit, index, computerNumberList);
+            putResult(typeScoreMap, digitResult);
+        }
+        return getResultStr(typeScoreMap);
+    }
+
+    private Map<String, Integer> getTypeScoreMap(){
+        return new HashMap<>(){{
+            put("Strike", 0);
+            put("Ball", 0);
+            put("Nothing", 0);
+        }};
+    }
+
+    private String getDigitResult(int nowDigit, int index, List<Integer> computerNumberList){
+        int digitIndex = computerNumberList.indexOf(nowDigit);
+        if(digitIndex == -1){
+            return "Nothing";
+        }
+        if(index == digitIndex){
+            return "Strike";
+        }
+        return "Ball";
+    }
+
+    private void putResult(Map<String, Integer> typeScoreMap, String digitResult){
+        int prevScore = typeScoreMap.get(digitResult);
+        typeScoreMap.put(digitResult, prevScore+1);
+    }
+
+    private String getResultStr(Map<String, Integer> typeScoreMap){
+        sb.setLength(0);
+        appendScore(sb, "Missing", typeScoreMap);
+        appendScore(sb, "Ball", typeScoreMap);
+        appendScore(sb, "Strike", typeScoreMap);
+        return sb.toString().strip();
+    }
+
+    private void appendScore(StringBuilder sb, String type, Map<String, Integer> typeScoreMap){
+        int typeScore = typeScoreMap.get(type);
+        if(type.equals("Missing") && typeScore == 3){
+            sb.append(BallType.valueOf(type).getKorean());
+        }
+        else if(typeScore > 0){
+            sb.append(typeScoreMap.get(type))
+                    .append(BallType.valueOf(type).getKorean())
+                    .append(" ");
+        }
+    }
+
+
+}
