677cce218daf9591b9d92cb696c4c6f4e333037d
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-06 11:02:23 +0900

    Update Game.java
    type : refactor : "Print() 함수 추가 및 주석 추가"
    
    스트라이크와 볼이 나온 것에 따라서 출력이 다르게 나오도록 Print() 함수를 추가하였습니다. 그리고 이름만으로 알기 어려운 함수들에게 주석을 달아 주었습니다.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,19 +7,33 @@ public class Game {
     Input_Output IO = new Input_Output();
     static int Strike_count=0;
     static int Ball_count=0;
-    public void Print()
+    public void Game_Progress()
     {
 
 
-        IO.Computer_RandomNumber();
+        IO.Computer_RandomNumber();//Input_Output에서 받은 컴퓨터의 수를 호출
         System.out.println(IO.computer);
-        IO.User_enter();
+        IO.User_enter();//Input_Output에서 받은 사용자의 수를 호출
         BaseNumber_Check(IO.computer,IO.Scan);
-        System.out.println(Strike_count + " 스트라이크 "+ Ball_count + "볼");
+        System.out.println(Print_Current_Game());
 
     }
+    public String Print_Current_Game()
+    {
+        if(Strike_count>0 && Ball_count==0)
+            return (Strike_count+ "스트라이크");
 
-    public static void BaseNumber_Check(List<Integer> computer,List<Integer> User)
+        if(Strike_count==0 && Ball_count>0)
+            return (Ball_count+"볼");
+
+        if(Strike_count > 0 && Ball_count > 0)
+            return (Strike_count + "스트라이크 "+ Ball_count + "볼");
+
+        return ("낫싱");
+    }
+
+
+    public static void BaseNumber_Check(List<Integer> computer,List<Integer> User)//받은 값을 스트라이크와 볼인지 분류
     {
         for(int i=0; i<computer.size();i++)
         {
@@ -27,7 +41,6 @@ public class Game {
                 Strike_count++;
             else
                 Ball_Repeat(computer.get(i),User);
-
 
         }
     }
