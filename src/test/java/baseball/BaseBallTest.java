8eb79529c840858598e22a0fd3988e98c993dc3c
namtndus <rnrmf0721@gmail.com>
2022-11-06 21:22:54 +0900

    test: printBassBallGameMessage함수 수정과 createRandomNumbers함수 생성
    테스트 시작하기전에 실행하는 함수와 테스트가 끝난 후 실행되는 함수의 기능을 printBassBallGameMessage에
    추가
    createRandomNumbers는 randomNumbersCreate함수의 출력값이 난수인지 확인하는 함수

--- src/test/java/baseball/BaseBallTest.java
+++ src/test/java/baseball/BaseBallTest.java
@@ -1,7 +1,6 @@
 package baseball;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,26 +8,29 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static baseball.Application.baseballGameStart;
+import static baseball.Application.randomNumberCreate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BaseBallTest {
     private final ByteArrayOutputStream output = new ByteArrayOutputStream();
 
-    @BeforeEach
-    public void setUpStreams(){
-        System.setOut(new PrintStream(output));
-    }
-
-    @AfterEach
-    public void restoreStreams(){
-        System.setOut(System.out);
-        output.reset();
-    }
     @Test
     @DisplayName("야구게임 시작 메세지 출력하기")
     void printBaseballGameMessage(){
+        System.setOut(new PrintStream(output));
         String displayMessage = "숫자 야구 게임을 시작합니다.\n";
         baseballGameStart();
         assertThat(output.toString()).isEqualTo(displayMessage);
+        System.setOut(System.out);
+        output.reset();
+    }
+
+    @Test
+    @DisplayName("컴퓨터의 수가 랜덤하게 출럭되는지 테스")
+    void craeteRandomNumbers(){
+        String num1 = randomNumberCreate().toString();
+        String num2 = randomNumberCreate().toString();
+        System.out.println( "num1 : "+num1+", num2 : "+num2);
+        assertThat(num1).isNotEqualTo(num2);
     }
 }
