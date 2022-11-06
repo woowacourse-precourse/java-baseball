828d48337cabcfa34d2012d3a8ac431dc46d3cc5
Seung-Been Kim <mainlib990@gmail.com mainlib990s-MacBookPro.local>
2022-11-06 20:16:33 +0900

    test: ApplicationTest 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,6 +21,237 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void computer123_user456and789and123and2() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("456", "789", "123", "2");
+                    assertThat(output()).contains("낫싱", "낫싱", "3스트라이크", "게임 종료");
+                },
+                1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user231and123and2() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("231", "123", "2");
+                    assertThat(output()).contains("3볼", "3스트라이크", "게임 종료");
+                },
+                1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_userA12_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("A12"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^A12$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user12A_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("12A"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^12A$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user한12_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("한12"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^한12$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user12한_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("12한"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^12한$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run(""))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user1_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("1"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^1$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user11_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("11"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^11$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user12_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("12"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^12$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user122_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("122"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^122$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user120_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("120"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^120$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user121_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("121"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^121$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user1234_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("1234"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^1234$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user0_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("0"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^0$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user3_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("3"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^3$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user_1_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("-1"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^-1$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_userA_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("A"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^A$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user한_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("한"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^한$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user123and3_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("123", "3"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^3$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
+    void computer123_user123andA_exception() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("123", "A"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining("Your input string is mismatch: ^A$");
+                }, 1, 2, 3
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
