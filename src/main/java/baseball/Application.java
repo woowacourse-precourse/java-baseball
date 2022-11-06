6a02ef312aa0cfc8a417153145ed036ccf89b251
kjg1514 <kjg1514@naver.com>
2022-11-07 06:06:15 +0900

    feat : 기능구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,10 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        Game game = new Game();
+        while (game.restartOrEnd()) {
+            game.playGame();
+        }
     }
 }
