aa4a9afed0e834042e75ad588817d3e9dbdd2557
JaehongDev <println@kakao.com>
2022-11-06 21:29:06 +0900

    refactor (UserInpuStatusTest) : 불필요한 의존성 주입을 제거함

--- src/test/java/baseball/domain/baseball/status/UserInputStatusTest.java
+++ src/test/java/baseball/domain/baseball/status/UserInputStatusTest.java
@@ -3,7 +3,6 @@ package baseball.domain.baseball.status;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import baseball.domain.baseball.BaseballContextFake;
-import java.util.List;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -15,8 +14,7 @@ class UserInputStatusTest {
     void 사용자입력상태_다음_상태는_비교상태입니다() {
 
         var status = new UserInputStatus();
-        var context = new BaseballContextFake(status, List.of(1, 2, 3), List.of(1, 2, 3));
-
+        var context = new BaseballContextFake(status, "123");
         assertThat(status.next(context)).isInstanceOf(CompareAnswerStatus.class);
 
     }
