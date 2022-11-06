ff643f02661743c0c22aadd8e2f6198395af26bd
SeokHwan An <asok369258@naver.com>
2022-11-07 01:19:51 +0900

    test : 플레이어가 제시한 숫자가 1부터 9사이의 수로 구성되어 있는지 검증하는 기능 테스트

--- src/test/java/baseball/validation/ValidationTest.java
+++ src/test/java/baseball/validation/ValidationTest.java
@@ -22,4 +22,18 @@ class ValidationTest {
         assertThat(validation.checkAnswerLength(answer)).isEqualTo(false);
     }
 
+    @Test
+    void 플레이어가_제시한_숫자가_1부터_9사이의_수로_구성되어있는지_검증하는_기능_true반환_테스트() {
+        String answer = "123";
+
+        assertThat(validation.checkAnswerConsistOfNum(answer)).isEqualTo(true);
+    }
+
+    @Test
+    void 플레이어가_제시한_숫자가_1부터_9사이의_수로_구성되어있는지_검증하는_기능_false반환_테스트() {
+        String answer = "012";
+
+        assertThat(validation.checkAnswerConsistOfNum(answer)).isEqualTo(false);
+    }
+
 }
