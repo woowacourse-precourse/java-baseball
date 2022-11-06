e88d4cb0a9629ea4d1c31e035cc3d6553fd46441
unknown <dragontaek98@naver.com>
2022-11-07 02:31:44 +0900

    feat: random 세자리 수 생성 기능

---
+++ src/main/java/baseball/GenerateAnswer.java
@@ -0,0 +1,33 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateAnswer {
+    private List<Integer> answer;
+
+    protected GenerateAnswer(){
+        this.answer = generateRandomAnswer();
+    }
+
+    protected List<Integer> getAnswer(){
+        return answer;
+    }
+
+    protected List<Integer> generateRandomAnswer(){
+        List<Integer> answer = new ArrayList<>();
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            addIfUnique(answer, randomNumber);
+        }
+        return answer;
+    }
+
+    protected void addIfUnique(List<Integer> answer, int randomNumber){
+        if (!answer.contains(randomNumber)) {
+            answer.add(randomNumber);
+        }
+    }
+}
