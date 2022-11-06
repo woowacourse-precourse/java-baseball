93540f6650981a39175c1e228fd9f333aae25e2c
hyeonseog-won <smreo75@gmail.com>
2022-11-07 01:10:46 +0900

    feat: 사용자 입력 중복 체크 기능 추가

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -2,20 +2,34 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 public class User {
+    private static List<Integer> numbers;
 
     public void inputNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        verifyInput(input);
+        if (verifyInput(input)) {
+            duplicateCheck(input);
+        }
+        throw new IllegalArgumentException("숫자는 1부터 9까지로 이루어져야 합니다.");
     }
 
-    public void verifyInput (String input) {
-        if (input.length() != 3) {
-            throw new IllegalArgumentException("입력한 수가 3자리가 아닙니다.");
+    public boolean verifyInput(String input) {
+        return Pattern.matches("^[1-9]*$", input);
+    }
+
+    public List<Integer> duplicateCheck(String input) {
+        numbers = Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        if (numbers.size() != 3) {
+            throw new IllegalArgumentException("서로 다른 수로 이루어진 3자리의 수를 입력해야 합니다.");
         }
-        if (!input.matches("^[1-9]*$")) {
-            throw new IllegalArgumentException("입력한 수가 1-9가 아닙니다.");
-        }
+        return numbers;
     }
 }
