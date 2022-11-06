de81b1ae1a65ace4a8fd79e228cdb9ed2e58baeb
ghltjd369 <ghltjd369@gmail.com>
2022-11-06 09:41:51 +0900

    feat : 두 개의 수를 비교하여 결과를 출력하는 기능 추가 - compareNumber : 두 개의 수를 비교하여 결과를 int형 배열에 저장하는 기능 - checkStrikeBallOut : 스트라이크인지 볼인지 아웃인지 결정하는 기능 - printResult : int형 배열에 저장된 결과를 주어진 조건에 맞춰 출력하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,7 +14,13 @@ public class Application {
             while (true) {
                 System.out.print("숫자를 입력해주세요 : ");
                 String input = Console.readLine();
-                checkTheNumber(input);
+                List<Integer> inputNumber = checkTheNumber(input);
+                int[] result = compareNumber(inputNumber, computerNumber);
+                boolean gameOver = printResult(result);
+
+                if(gameOver){
+                    break;
+                }
             }
         }
 
@@ -110,5 +116,54 @@ public class Application {
         }
 
         return isZero;
+    }
+
+    public static int[] compareNumber(List<Integer> inputNumber, List<Integer> computerNumber) {
+        int[] result = new int[3];
+        int strike;
+        int ball;
+        int out;
+
+        for (int number : inputNumber) {
+            int number_index = inputNumber.indexOf(number);
+
+            checkStrikeBallOut(number, number_index, computerNumber, result);
+        }
+
+        return result;
+    }
+
+    public static void checkStrikeBallOut(int number, int number_index, List<Integer> computerNumber, int[] result) {
+        if (computerNumber.get(number_index) == number) {
+            result[0]++;
+        } else if (computerNumber.contains(number)) {
+            result[1]++;
+        } else {
+            result[2]++;
+        }
+    }
+
+    public static boolean printResult(int[] result) {
+        boolean gameOver = false;
+        int strike = result[0];
+        int ball = result[1];
+        int out = result[2];
+
+        if (ball != 0) {
+            System.out.print(ball + "볼 ");
+        }
+        if (strike != 0) {
+            System.out.print(strike + "스트라이크");
+        }
+        if (strike == 3) {
+            System.out.print("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            gameOver = true;
+        }
+        if (out == 3) {
+            System.out.println("낫싱");
+        }
+        System.out.println();
+
+        return gameOver;
     }
 }
