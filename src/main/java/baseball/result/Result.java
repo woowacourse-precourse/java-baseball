4b6601a64d369767702f493bce74542214d71c76
이지혁 <olzlgur@naver.com>
2022-11-06 21:26:36 +0900

    feat: 스트라이크, 볼 값 반환 메서드 구현

--- src/main/java/baseball/result/Result.java
+++ src/main/java/baseball/result/Result.java
@@ -8,4 +8,12 @@ public class Result {
         this.strike = 0;
         this.ball = 0;
     }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
 }
