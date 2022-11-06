7687d38c238a1e1f661550a2ab91e9aa84670215
ChoiYeonho0903 <903yh@naver.com>
2022-11-07 05:01:21 +0900

    feat: 숫자야구를 계속 진행 할 것인지 사용자 입력받는 기능 구현

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -7,18 +7,43 @@ import baseball.view.BaseballConsole;
 
 public class BaseballController {
 
-    private final Computer computer;
-    private final User user;
-    private final Umpire umpire;
+//    private final Computer computer;
+//    private final User user;
+//    private final Umpire umpire;
 
-    public BaseballController() {
-        this.computer = new Computer();
-        this.user = new User();
-        this.umpire = new Umpire();
+//    public BaseballController() {
+//        this.computer = new Computer();
+//        this.user = new User();
+//        this.umpire = new Umpire();
+//    }
+
+    public void start() {
+        do {
+            play();
+        } while(checkContinueBaseball());
     }
 
+    private boolean checkContinueBaseball() {
+        BaseballConsole.printCheckContinueBaseball();
+        String input = BaseballConsole.readCheckContinueInput();
+        if(input.equals("1")) {
+            return true;
+        }
+        if(input.equals("2")) {
+            return false;
+        }
+        throw new IllegalArgumentException("1과 2의 숫자만 입력할 수 있습니다.");
+    }
+
+
     public void play() {
-        while(!umpire.is3Strike()) {
+        Computer computer = new Computer();
+        User user;
+        Umpire umpire;
+
+        do {
+            user = new User();
+            umpire = new Umpire();
             BaseballConsole.printUserInputNumber();
             String userInputNumber = BaseballConsole.readUserInputNumber();
             user.addUserNumbers(userInputNumber);
@@ -26,7 +51,8 @@ public class BaseballController {
             umpire.decideCount(computer.getNumbers(), user.getNumbers());
 
             BaseballConsole.printStrikeBallCount(umpire.getStrikeCount(), umpire.getBallCount());
-        }
+        } while(!umpire.is3Strike());
+
         BaseballConsole.printEndBaseBall();
     }
 
