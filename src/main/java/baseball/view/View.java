81baca87abd6d2ab8af21a16c35122ea61041f32
ChanghyunKim <variety82p@gamil.com>
2022-11-06 20:56:13 +0900

    refactor : 게임상태 메시지 ConstantValue 클래스로 이동

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -2,11 +2,9 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static baseball.constant.ConstantValue.*;
+
 public class View {
-    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final String NUBMER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-    private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임종료.\n" +
-            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public View(){}
 
     public String initView(){
