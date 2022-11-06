771669228a744bf8dbed00bb2ba7e6f2ba62069b
맹순영 <symaeng98@naver.com>
2022-11-06 10:01:00 +0900

    feat: 입력 예외 처리 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class Application {
     private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_SENTENCE = "숫자를 입력해주세요 : ";
+    private static final int NUMBER_LENGTH = 3;
 
     public static void main(String[] args) {
         System.out.println(START_SENTENCE);
@@ -17,6 +18,8 @@ public class Application {
         int flag = 1; // flag가 1이면 게임을 계속 진행
         while (flag == 1) {
             String inputNum = input();
+            checkInputNumber(inputNum);
+
             List<Integer> ballAndStrikeList = calculate(answer, inputNum);
 
             int ball = ballAndStrikeList.get(0);
@@ -26,7 +29,10 @@ public class Application {
             if (strike == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
                 String input = Console.readLine();
+                checkInputNewGameNumber(input);
+
                 flag = Integer.parseInt(input);
                 answer = generateRandomNumber();
             }
@@ -79,5 +85,49 @@ public class Application {
             System.out.print(strike + "스트라이크 ");
         }
         System.out.println(); // 줄바꿈
+    }
+
+    public static void checkInputNumber(String inputNum) {
+        if (inputNum.length() != NUMBER_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+
+        isValidNumber(inputNum);
+
+        // 0을 포함한 수를 입력받으면 예외 처리
+        if (inputNum.charAt(0) == '0') {
+            throw new IllegalArgumentException();
+        }
+        if (inputNum.charAt(1) == '0') {
+            throw new IllegalArgumentException();
+        }
+        if (inputNum.charAt(2) == '0') {
+            throw new IllegalArgumentException();
+        }
+
+        // 중복된 숫자가 있는지 확인 후 예외 처리
+        if (inputNum.charAt(0) == inputNum.charAt(1)) {
+            throw new IllegalArgumentException();
+        }
+        if (inputNum.charAt(0) == inputNum.charAt(2)) {
+            throw new IllegalArgumentException();
+        }
+        if (inputNum.charAt(1) == inputNum.charAt(2)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkInputNewGameNumber(String inputNum) {
+        if (!inputNum.equals("1") && !inputNum.equals("2")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void isValidNumber(String inputNum) {
+        try {
+            Integer.parseInt(inputNum);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 }
