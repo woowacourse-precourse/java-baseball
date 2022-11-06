a8de9e27a28d1f5d222c29daf075c4fab2c889c1
ddingmin <ddingminyt@gmail.com>
2022-11-07 03:43:29 +0900

    feat(gameProcess): 스트라이크, 볼을 판단하는 기능 구현

--- src/main/java/baseball/GameProcess.java
+++ src/main/java/baseball/GameProcess.java
@@ -1,26 +1,67 @@
 package baseball;
 
 public class GameProcess {
-    int[] answer;
-    int[] havingNum = new int[10];
+    public static final int NUMBER_LENGTH = 3;
+    int[] answer = new int[10];
+    int[] userNum;
+    Boolean isCorrect;
+    int ball, strike;
     public GameProcess(){
     }
 
     public void gameStart(){
         // 게임 진행
+        isCorrect = false;
         setAnswer();
 
+
+        // 맞을 때 까지 게임 진행
+        while (!isCorrect){
+            // 유저의 입력 받고 userNum = 입력 함수;
+            userNum = new int[]{7, 1, 3};
+            // 스트라이크, 볼 구하기
+            countStrikeBall(userNum);
+            isCorrect = checkCorrect();
+            break;
+        }
+        // 3개의 숫자를 모두 맞히셨습니다! 게임 종료 <- 출력
+
+        // 게임 종료 후 재시작 묻기
         if (isRestart()){
             gameRestart();
         }
         gameEnd();
     }
 
+    private Boolean checkCorrect(){
+        return strike == 3;
+    }
+
+    private void setStrikeOrBall(int check, int idx){
+        if (answer[check] == idx + 1){
+            strike += 1;
+            return;
+        }
+        if (answer[check] > 0){
+            ball += 1;
+            return;
+        }
+    }
+
+    private void countStrikeBall(int[] user){
+        strike = 0;
+        ball = 0;
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            setStrikeOrBall(user[i], i);
+        }
+        // 볼, 스트라이크를 출력하는 기능.
+    }
+
     private void setAnswer(){
-        // 랜덤으로 지정된 3자리 입력받기, answer = 입력받은거;
-        answer =  new int[]{1, 2, 3};
-        for (int i : answer) {
-            havingNum[i] = 1;
+        // 랜덤으로 지정된 3자리 입력받기, temp = 입력받은거;
+        int[] temp = new int[]{7, 1, 3};
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            answer[temp[i]] = i + 1;
         }
     }
 
@@ -33,7 +74,7 @@ public class GameProcess {
     }
 
     private Boolean isRestart(){
-        // 1 또는 2를 입력받아 재시작 여부 확인
+        // 게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. <- 출력
         return false;
     }
 }
