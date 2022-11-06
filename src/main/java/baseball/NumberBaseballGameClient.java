ec2537fc1c390069bb95613128f2aac57386292d
DevJay1024 <devjay1024@gmail.com>
2022-11-07 04:26:37 +0900

    feat: 플레이어의 게임숫자 입력 요청 및 유효성 검사 기능 완성

--- src/main/java/baseball/NumberBaseballGameClient.java
+++ src/main/java/baseball/NumberBaseballGameClient.java
@@ -1,10 +1,19 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 // 출력 메시지 및 입력 관리
 public class NumberBaseballGameClient {
 
     public static final String MSG_GAME_START = "숫자 야구 게임을 시작합니다.";
-    public static final String MSG_ASK_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String MSG_ASK_PLAYER_GAME_NUMBER = "숫자를 입력해주세요 : ";
     public static final String MSG_JUDGED_RESULT_BALLS = "볼";
     public static final String MSG_JUDGED_RESULT_STRIKES = "스트라이크";
     public static final String MSG_JUDGED_RESULT_NOTHING = "낫싱";
@@ -18,9 +27,32 @@ public class NumberBaseballGameClient {
         System.out.println(MSG_GAME_START);
     }
 
-    public static int askInputNumber() {
-        // 게임숫자를 맞출 숫자에 대한 입력 요청 (+입력값에 대한 유효성 검사. 에러는 IllegalArgumentException)
-        return Integer.MAX_VALUE;
+    public static int askPlayerGameNumber() throws IllegalArgumentException {
+        System.out.println(MSG_ASK_PLAYER_GAME_NUMBER);
+        String line;
+        try {
+            line = Console.readLine();
+        } catch (NoSuchElementException e) {
+            throw new IllegalArgumentException();
+        }
+
+        int originalLength = line.length();
+        line = line.strip();
+        if(line.length() != 3 || line.length() != originalLength)
+            throw new IllegalArgumentException();
+
+        int playerGameNumber = Integer.parseInt(line);
+        int tempNumber = playerGameNumber;
+
+        int unit1 = tempNumber % 10;
+        tempNumber /= 10;
+        int unit100 = tempNumber / 10;
+        int unit10 = tempNumber % 10;
+
+        if(unit100 == unit10 || unit10 == unit1 || unit1 == unit100)
+            throw new IllegalArgumentException();
+
+        return playerGameNumber;
     }
 
     public static void showJudgedResult(NumberBaseballGameJudgedResultDto dto) {
