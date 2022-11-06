1815d96345cfc5aa41d44f7ff1b286e652d061a7
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-07 02:55:47 +0900

    feat(GameController): add game setting

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -4,11 +4,19 @@ import baseball.service.GameService;
 
 public class GameController {
     private static final GameService gameService = new GameService();
+    private static final int DIGIT = 3;
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 9;
 
     public GameController() {
     }
 
-    public static void run() throws IllegalArgumentException {
+    public void setGame() {
+        gameService.initGame(DIGIT, START_RANGE, END_RANGE);
+    }
+
+    public void run() throws IllegalArgumentException {
+        setGame();
         gameService.startGame();
     }
 }
