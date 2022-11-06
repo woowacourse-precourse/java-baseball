c1826e4a589c0094511953813381378d32dfa372
eastsage <dpwk8@naver.com>
2022-11-06 18:50:22 +0900

    feat: 사용자 입력 검증 기능 추가
    사용자가 잘못된 입력을 했을 시 오류를 발생시키기 위해 1~9 중복없는 3자리 수가 아니면 IllegalArgumentException 발생 후 종료하도록 isValid, is3NumbersFrom1To9 함수 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -2,8 +2,9 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Scanner;
+import java.util.Set;
 
 public class Player {
     private List<Integer> playerNumber;
@@ -12,12 +13,28 @@ public class Player {
     }
 
     public void setNumber() {
-        playerNumber = new ArrayList<>();
-        for (char c : Console.readLine().toCharArray()) {
-            playerNumber.add(Character.getNumericValue(c));
-        }
+        String input = Console.readLine();
+        playerNumber = new ArrayList<>(isValid(input));
     }
-    
+
+    private Set<Integer> isValid(String number) throws IllegalArgumentException {
+        if (!is3NumbersFrom1To9(number)) {
+            throw new IllegalArgumentException();
+        }
+        LinkedHashSet<Integer> validInput = new LinkedHashSet<>();
+        for (char c : number.toCharArray()) {
+            validInput.add(Character.getNumericValue(c));
+        }
+        if (validInput.size() < 3) {
+            throw new IllegalArgumentException();
+        }
+        return validInput;
+    }
+
+    private boolean is3NumbersFrom1To9(String number) {
+        return number.matches("[1-9]{3}");
+    }
+
     public List<Integer> getNumber() {
         return playerNumber;
     }
