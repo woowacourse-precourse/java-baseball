fe1afa2190893cba6e058f6724d9e6684454c813
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-06 16:59:28 +0900

    feat: User - setSelectNumber 메서드에 예외추가
    입력값을 받을때, 예외가 있는지 체크

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -3,12 +3,14 @@ package baseball.domain;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import baseball.exception.InputUserNumberException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class User {
 
     private static List<Integer> selectNumber = new ArrayList<>();
+    private static InputUserNumberException inputUserNumberException = new InputUserNumberException();
 
     public List<Integer> getSelectNumber() {
         return selectNumber;
@@ -16,6 +18,7 @@ public class User {
 
     public void setSelectNumber() {
         String InputNumber = readLine();
+        inputUserNumberException.findUserNumberException(InputNumber);
         initList();
         for (int index = 0; index < InputNumber.length(); index++) {
             selectNumber.add(InputNumber.charAt(index) - '0');
