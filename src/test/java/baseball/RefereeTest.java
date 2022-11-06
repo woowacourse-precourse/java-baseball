9dca6eb7dbcc1cd4857147e019c6d371f5d20d1c
June <68291395+june-777@users.noreply.github.com>
2022-11-06 21:22:30 +0900

    test: 두 야구공 같은 수 하나 존재하는 경우 테스트

--- src/test/java/baseball/RefereeTest.java
+++ src/test/java/baseball/RefereeTest.java
@@ -17,10 +17,13 @@ public class RefereeTest {
 		Computer computer = new Computer();
 		Referee referee = new Referee();
 		List<Integer> allBall = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
 		computer.pitchThreeUniqueBalls();
 		List<Integer> computerBalls = computer.getComputerBalls();
+
 		List<Integer> playerBalls = new LinkedList<>();
 		playerBalls.addAll(allBall);
+		Collections.shuffle(playerBalls);
 
 		// when
 		playerBalls = makeBallsWithoutComputerBalls(computerBalls, playerBalls);
@@ -30,9 +33,39 @@ public class RefereeTest {
 		Assertions.assertThat(nothing).isEqualTo(true);
 	}
 
-	private static List<Integer> makeBallsWithoutComputerBalls(List<Integer> computerBalls, List<Integer> playerBalls) {
+	@Test
+	@DisplayName("낫싱 실패 테스트")
+	void 일치하는공_하나일_때_낫싱_실패() {
+		// given
+		Computer computer = new Computer();
+		Referee referee = new Referee();
+		List<Integer> allBall = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+		computer.pitchThreeUniqueBalls();
+		List<Integer> computerBalls = computer.getComputerBalls();
+
+		List<Integer> playerBalls = new LinkedList<>();
+		playerBalls.addAll(allBall);
 		Collections.shuffle(playerBalls);
+
+		// when
+		playerBalls = makeOneSameBalls(computerBalls, playerBalls);
+
+		// then
+		boolean nothing = referee.isNothing(computerBalls, playerBalls);
+		System.out.println(computerBalls);
+		System.out.println("한개 같은 공: " + playerBalls);
+		Assertions.assertThat(nothing).isEqualTo(false);
+	}
+
+	private static List<Integer> makeBallsWithoutComputerBalls(List<Integer> computerBalls, List<Integer> playerBalls) {
 		playerBalls.removeAll(computerBalls);
+		return playerBalls.subList(0, 3);
+	}
+
+	private static List<Integer> makeOneSameBalls(List<Integer> computerBalls, List<Integer> playerBalls) {
+		playerBalls.removeAll(computerBalls);
+		playerBalls.add(0, computerBalls.get(0));
 		return playerBalls.subList(0, 3);
 	}
 }
