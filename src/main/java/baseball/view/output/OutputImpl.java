e7986410a929287afa6460e733b569dd27fcfa7a
5uhwann <5uhwann27@gmail.com>
2022-11-07 02:07:34 +0900

    refactor(출력): 각 상황에 맞는 메시지 출력 리팩토링
    OutputImpl에 멤버변수로 존재하던 출력메시지들을 OutMessage 클래스로 관리

--- src/main/java/baseball/view/output/OutputImpl.java
+++ src/main/java/baseball/view/output/OutputImpl.java
@@ -1,21 +1,18 @@
 package baseball.view.output;
 
 
-public class OutputImpl implements Output {
+import baseball.utils.OutputMessage;
 
-    private final String START_MESSAGE = "숫자 야구를 시작합니다.";
-    private final String NUMBER_REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
-    private final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private final String END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+public class OutputImpl implements Output {
 
     @Override
     public void printStartMessage() {
-        System.out.println(START_MESSAGE);
+        System.out.println(OutputMessage.START_MESSAGE);
     }
 
     @Override
     public void printNumberRequest() {
-        System.out.print(NUMBER_REQUEST_MESSAGE);
+        System.out.print(OutputMessage.NUMBER_REQUEST_MESSAGE);
     }
 
     @Override
@@ -25,11 +22,11 @@ public class OutputImpl implements Output {
 
     @Override
     public void printSuccessMessage() {
-        System.out.println(SUCCESS_MESSAGE);
+        System.out.println(OutputMessage.SUCCESS_MESSAGE);
     }
 
     @Override
     public void printEndMessage() {
-        System.out.println(END_MESSAGE);
+        System.out.println(OutputMessage.END_MESSAGE);
     }
 }
