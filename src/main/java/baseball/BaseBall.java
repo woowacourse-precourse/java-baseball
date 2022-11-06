607a2ecdca16001adbc6f6dbb4e365d722662713
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-06 15:52:02 +0900

    feat: BaseBall.play 메소드 구현

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -1,8 +1,17 @@
 package baseball;
 
 public class BaseBall {
-    public void helloMessage{
+    public void helloMessage(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public void play(){
+        GameCycle gameCycle = new GameCycle();
+        AfterGame afterGame = new AfterGame();
+        helloMessage();
+        while(true){
+            gameCycle.play();
+            if(!afterGame.play())break;
+        }
     }
 
 }
