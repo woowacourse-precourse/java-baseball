5a74b06a69d15d1346707e7ed88fa662f25bf411
park <pjh971212@gmail.com>
2022-11-07 03:14:06 +0900

    refactor: 유저 - 컴퓨터 값 자료형 동기화 작업 진행

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
     private String inputNum;
 
@@ -11,5 +14,13 @@ public class Player {
         this.inputNum = inputNum;
     }
 
+    public List<Integer> getPlayerNum() {
+        List<Integer> convertList = new ArrayList<>();
+        for (int i = 0; i < this.inputNum.length(); i++) {
+            int convert_int = Integer.parseInt(String.valueOf(this.inputNum.charAt(i)));
+            convertList.add(convert_int);
+        }
 
+        return convertList;
+    }
 }
