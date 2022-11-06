78b75a085f5535ecdde5377bf9680b791cff176d
yeonhee <ydol2@naver.com>
2022-11-06 16:44:58 +0900

    fix : 문자열 일치를 ==에서 equals로 변경

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -49,7 +49,7 @@ public class BaseballGame {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String restartNumber = Console.readLine();
 
-        if (restartNumber == "1") {
+        if (restartNumber.equals("1")) {
             playBaseballGame();
         }
     }
