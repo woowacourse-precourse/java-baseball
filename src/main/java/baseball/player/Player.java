438e5283ab55bf79fa9d128e8db60744aa847748
SeokHwan An <asok369258@naver.com>
2022-11-06 10:01:51 +0900

    feat : 플레이어가 문제에 대한 응답을 하는 기능 추가

---
+++ src/main/java/baseball/player/Player.java
@@ -0,0 +1,15 @@
+package baseball.player;
+
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+
+    /**
+     * 응답하는 기능
+     */
+    public String answerToProblem() {
+        return Console.readLine();
+    }
+
+}
