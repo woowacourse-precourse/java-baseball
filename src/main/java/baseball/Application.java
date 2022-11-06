2dd2ea1d32cac4876198d6776fb992e6a3e13117
tkdtn4657 <tkdtn101226@gmail.com>
2022-11-07 05:44:20 +0900

    기능목록 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,14 +14,12 @@ class baseball {
     Scanner number = new Scanner(System.in);
 	int temp;
 	int result_num;
-	List<Integer> cmp_List = new ArrayList<>(3);
+	List<Integer> tmp_List = new ArrayList<>(3);
 	List<Integer> correct = new ArrayList<>(3);
 	
 	baseball(){
 		System.out.println("숫자 야구 게임을 시작합니다.");
-		get_user_input_num();
-		getrandomNum();
-		result();
+		game();
 	}
 	
 	void getrandomNum() {
@@ -33,22 +31,25 @@ class baseball {
 		    }
 		}		
 	}
-	
-	/*
-	 * baseball(int i){ System.out.println("숫자 야구 게임을 시작합니다."); }
-	 */
+
 	
 	void repeat_game(){
 		
 	}
 	
-	int get_user_input_num() {
+	void get_user_input_num() {
 		System.out.println("숫자를 입력해주세요 : "); // 세자리수 제한 구현필요
 		user_num = number.nextInt();
-		return user_num;
+		int cipher;
+		for(cipher = 0; cipher < tmp_List.size(); cipher++) {
+			tmp_List.add(cipher);
+		}
+		
 	}
 	
 	void game() {
+		get_user_input_num();
+		
 		System.out.println("숫자를 모두 맞히셨습니다! 게임 종료");
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 		repeat_game();
@@ -60,7 +61,6 @@ class baseball {
 		user_num = 713;
 		
 		if(compare(this.result_num, this.user_num) == 1) {
-		game();
 		}
 		
 	}
