5ad28a5250a2fd8e838b66492697b05107184ea5
sosow0212 <sosow0212@naver.com>
2022-11-06 14:12:10 +0900

    feat : 유저가 입력한 게임 넘버 중 중복된 값이 있는지 확인해주는 기능 구현

---
+++ src/main/java/baseball/exception/InputNumbersException.java
@@ -0,0 +1,19 @@
+package baseball.exception;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class InputNumbersException {
+    private static final String ONLY_NUMBER_REGEX = "[1-9]+";
+    private static final int REQUIRE_NUMBER_SIZE = 3;
+
+    public void isOverlapNumber(String numberTypeOfString) {
+        Set<Integer> numSet = new HashSet<>();
+        for (int i = 0; i < numberTypeOfString.length(); i++) {
+            numSet.add(numberTypeOfString.charAt(i) - '0');
+        }
+        if (numSet.size() != REQUIRE_NUMBER_SIZE) {
+            throw new IllegalArgumentException("중복되지 않은 수를 입력해주세요.");
+        }
+    }
+}
