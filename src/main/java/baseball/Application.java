15c74f0895c1decaaaf39413b47eb1905a56bfd1
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-11-06 11:43:09 +0900

    feat(baseball): 게임을 시작하고 재시작 여부에 따라 진행하는 기능 추가
    사용자가 1을 입력하면 재시작, 2를 입력하면 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,23 @@ import camp.nextstep.edu.missionutils.*;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input;
+        boolean retry;
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        do {
+            gameLoop();
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            input = Console.readLine();
+            if (input.equals("1"))
+                retry = true;
+            else if (input.equals("2"))
+                retry = false;
+            else
+                throw new IllegalArgumentException();
+        } while (retry);
     }
 
     private static void gameLoop() {
