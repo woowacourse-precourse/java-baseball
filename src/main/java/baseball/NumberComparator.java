71175908f70a0fce6b1649a1c5fd99a6f0b3454b
Songyi Kim <ksl2950@naver.com>
2022-11-06 21:42:17 +0900

    feat: 사용자가 서로 다른 3자리 숫자를 입력했을 때, 결과를 반환하는 기능 구현
    - isBall() : 볼 여부 리턴 함수 
    - isStrike() : 스트라이크 여부 리턴 함수
    - compare() : 스트라이크, 볼 결과 계산 함수
    - getCompareResult() : 결과 리턴 함수

---
+++ src/main/java/baseball/NumberComparator.java
@@ -0,0 +1,52 @@
+package baseball;
+
+import java.util.List;
+
+public class NumberComparator {
+
+    private final List<Integer> inputNumberList;
+    private final List<Integer> resultNumberList;
+
+    private int strike;
+    private int ball;
+
+    public NumberComparator(List<Integer> inputNumberList, List<Integer> resultNumberList) {
+        this.inputNumberList = inputNumberList;
+        this.resultNumberList = resultNumberList;
+        strike = 0;
+        ball = 0;
+    }
+
+    public String getCompareResult() {
+        if (strike == 0 && ball == 0)
+            return "낫싱";
+        if (ball == 0)
+            return strike + "스트라이크";
+        if (strike == 0)
+            return ball + "볼";
+        return ball + "볼" + " " + strike + "스트라이크";
+    }
+
+    public void compare(){
+        int COMPARE_LENGTH = 3;
+        for (int i = 0; i< COMPARE_LENGTH; i++){
+            if (isStrike(i)){
+                strike ++;
+                continue;
+            }
+            if (isBall(i)){
+                ball ++;
+            }
+        }
+    }
+
+    public boolean isStrike(int index){
+        return inputNumberList.get(index) == resultNumberList.get(index);
+    }
+
+    public boolean isBall(int index){
+        return resultNumberList.contains(inputNumberList.get(index));
+
+    }
+
+}
