8fd9de5902594e0aeb7ccfa43b67287ca254cd90
jingyu9804 <swc05161@gmail.com>
2022-11-06 22:21:22 +0900

    feat : 문자열을 리스트로 변환하는 메소드 추가

--- src/main/java/baseball/InputUtil.java
+++ src/main/java/baseball/InputUtil.java
@@ -6,22 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputUtil {
-
-
-    public static List<String> inputUserNumber(){
+    public List<String> inputUserNumber(){
         CheckUtil checkUtil = new CheckUtil();
 
         String userNumber = Console.readLine();
-
-        List<String> userNumList = new ArrayList<>();
-        for(char c:userNumber.toCharArray()){
-            userNumList.add(Character.toString(c));
-        }
+        List<String> userNumList = stringToListString(userNumber);
 
         checkUtil.checkIsValidDigit(userNumber);
         checkUtil.checkIsValidLength(userNumber.length());
         checkUtil.checkIsDuplicated(userNumList);
 
+        return userNumList;
+    }
+
+    private List<String> stringToListString(String userNumber){
+        List<String> userNumList = new ArrayList<>();
+        for(char c:userNumber.toCharArray()){
+            userNumList.add(Character.toString(c));
+        }
         return userNumList;
     }
 }
