0290d5d324ea07a039be8c0cf438860b1cb2ea71
Sehbeom <j3beom@gmail.com>
2022-11-06 16:42:54 +0900

    test(GameOperator):RED 게임 운영 기능 테스트 수정
    NsTest 상속 받는 방식으로 테스트 내용 수정
    
    GameOperatorTest.java 수정

--- src/test/java/baseball/domain/GameOperatorTest.java
+++ src/test/java/baseball/domain/GameOperatorTest.java
@@ -1,48 +1,42 @@
 package baseball.domain;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import baseball.Application;
 import baseball.constants.GuideSentences;
 import baseball.testhelper.ComparingResultExample;
+import camp.nextstep.edu.missionutils.test.NsTest;
 
-public class GameOperatorTest {
-	GameOperator gameOperator = new GameOperator();
-
-	@Test
-	@DisplayName("숫자 야구 게임 테스트")
-	void numberBaseballGameTest() {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
-
-		setCommand();
-		setExpected();
-
-		InputStream inputStream = new ByteArrayInputStream(command.getBytes());
-		System.setIn(inputStream);
-
-		gameOperator.numberBaseballGame();
-
-		assertThat(out.toString()).isEqualTo(expected);
-
-		System.setOut(System.out);
-	}
-
-	String command = "";
+public class GameOperatorTest extends NsTest {
+	String[] command;
 	String expected = "";
 	String oneBall = ComparingResultExample.ONE_BALL.getNumbers()
 			.toString().replaceAll("[^1-9]", "");
 	String threeStrike = ComparingResultExample.THREE_STRIKE.getNumbers()
 			.toString().replaceAll("[^1-9]", "");
+
+	@Test
+	@DisplayName("숫자 야구 게임 테스트")
+	void numberBaseballGameTest() {
+		setCommand();
+		setExpected();
+
+		assertRandomNumberInRangeTest(
+			() -> {
+				run(command);
+				assertThat(output()).isEqualTo(expected);
+			},
+			4, 5, 6, 4, 5, 6
+		);
+
+	}
 
 	void setCommand() {
 		List<String> commandList = new ArrayList<>();
@@ -57,7 +51,7 @@ public class GameOperatorTest {
 		commandList.add(threeStrike);
 		commandList.add("2");
 
-		command = String.join("\n", commandList);
+		command = commandList.toArray(new String[0]);
 	}
 
 	void setExpected() {
@@ -67,25 +61,27 @@ public class GameOperatorTest {
 		expectedList.add(GuideSentences.START_GUIDE.getSentence());
 
 		// 커맨드 입력 "1볼, 3스트라이크"에 맞추어 '입력 안내문'과 '비교 결과' 출력
-		expectedList.add(GuideSentences.INPUT_GUIDE.getSentence() + oneBall);
 		String oneBallResult = ComparingResultExample.ONE_BALL.getResultSentence();
-		expectedList.add(oneBallResult);
+		expectedList.add(GuideSentences.INPUT_GUIDE.getSentence() + oneBallResult);
 
-		expectedList.add(GuideSentences.INPUT_GUIDE.getSentence() + threeStrike);
 		String threeStrikeResult = ComparingResultExample.THREE_STRIKE.getResultSentence();
-		expectedList.add(threeStrikeResult);
+		expectedList.add(GuideSentences.INPUT_GUIDE.getSentence() + threeStrikeResult);
+
 
 		// 게임 종료 안내문 및 재시작 여부
 		expectedList.add(GuideSentences.END_GUIDE.getSentence());
 		expectedList.add(GuideSentences.RESTART_GUIDE.getSentence());
 
 		// 재시작 -> 3스트라이크 -> 종료
-		expectedList.add("1");
-		expectedList.add(GuideSentences.INPUT_GUIDE.getSentence() + threeStrike);
+		expectedList.add(GuideSentences.INPUT_GUIDE.getSentence() + threeStrikeResult);
 		expectedList.add(GuideSentences.END_GUIDE.getSentence());
 		expectedList.add(GuideSentences.RESTART_GUIDE.getSentence());
-		expectedList.add("2");
 
 		expected = String.join("\n", expectedList);
+	}
+
+	@Override
+	public void runMain() {
+		Application.main(new String[]{});
 	}
 }
