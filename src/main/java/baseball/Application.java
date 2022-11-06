f4942bc2e4e818d1db92301953c1491564245e02
ttaehee <ttaehee1105@gmail.com>
2022-11-07 01:49:10 +0900

    feat : 중복없이 랜덤숫자 3개 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,45 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        new BaseballController(new NumbersGenerator()).run();
+    }
+}
+
+class BaseballController {
+    private final int COUNT_OF_NUMBERS = 3;
+
+    private NumbersGenerator numbersGenerator;
+
+    BaseballController(NumbersGenerator numbersGenerator) {
+        this.numbersGenerator = numbersGenerator;
+    }
+    public void run() {
+        Numbers answer = numbersGenerator.generate(COUNT_OF_NUMBERS);
+    }
+}
+
+class Numbers {
+    private List<Integer> numbers;
+
+    Numbers(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+}
+
+class NumbersGenerator {
+    Numbers generate(int count) {
+
+        return new Numbers(
+                Stream.generate(() -> Randoms.pickNumberInRange(1, 9))
+                        .distinct()
+                        .limit(count)
+                        .collect(Collectors.toList()));
     }
 }
