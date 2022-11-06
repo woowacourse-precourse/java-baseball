e0e62012ecc7c6d69ab490deb68a204ecfdf3eae
scyllacore <yms8795@gmail.com>
2022-11-06 16:51:59 +0900

    feat(BaseBallConstant) : 하드 코딩 방지를 위해 상수 모음 클래스 구현.

---
+++ src/main/java/baseball/system/BaseballConstant.java
@@ -0,0 +1,18 @@
+package baseball.system;
+
+public class BaseballConstant {
+    //
+    public static final int INPUT_LENGTH = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+
+    //
+    public static final int PLAY_GAME = 1;
+    public static final int QUIT = 2;
+    public static final int SELECTION_NUMBER_LENGTH = 1;
+
+    //
+    public static final int ZERO_COUNT = 0;
+    public static final int MAX_COUNT = 3;
+    public static final int NUMBERS_COUNT = 3;
+}
