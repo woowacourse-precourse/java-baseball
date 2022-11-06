4ce8be1ce25a6aea9068aeea714a877688b5176f
JungWoo <rlawjddn103@naver.com>
2022-11-07 00:13:02 +0900

    feat(ComputerManager): 컴퓨터의 수와 유저의 수를 비교하여 문자열을 반환하는 기능 구현

--- src/main/java/baseball/ComputerManager.java
+++ src/main/java/baseball/ComputerManager.java
@@ -2,6 +2,7 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -10,6 +11,9 @@ import camp.nextstep.edu.missionutils.Randoms;
  */
 public class ComputerManager {
     private List<Integer> computerNumbers;
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NONE = "낫싱";
 
     /**
      * 컴퓨터매니저 클래스가 가진 랜덤 수를 초기화하고,
@@ -17,11 +21,56 @@ public class ComputerManager {
      */
     public void generateRandomNumber() {
         computerNumbers = new ArrayList<>();
-        while (computerNumbers.size() < 3){
-            int randomNumber = Randoms.pickNumberInRange(1,9);
-            if (!computerNumbers.contains(randomNumber)){
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
                 computerNumbers.add(randomNumber);
             }
         }
+    }
+
+    /**
+     * @param userNumber 유저가 입력한 수
+     *                   유저가 입력한 수에 대해 현재 컴퓨터가 가지고 있는 수와 비교하여,
+     *                   적절한 출력값을 반환하는 함수
+     */
+    public String compareWithUserNumber(String userNumber) {
+        int ball = 0;
+        int strike = 0;
+        List<Integer> numbers = userNumberToList(userNumber);
+
+        for (int i = 0; i < numbers.size(); i++) {
+            if (Objects.equals(numbers.get(i), computerNumbers.get(i))) {
+                strike++;
+                continue;
+            }
+
+            if (computerNumbers.contains(numbers.get(i))) {
+                ball++;
+            }
+        }
+
+        return makeString(ball, strike);
+    }
+
+    private List<Integer> userNumberToList(String userNumber) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < userNumber.length(); i++) {
+            numbers.add(Integer.parseInt(String.valueOf(userNumber.charAt(i))));
+        }
+        return numbers;
+    }
+
+    private String makeString(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            return NONE;
+        }
+        if (ball == 0) {
+            return strike + STRIKE;
+        }
+        if (strike == 0) {
+            return ball + BALL;
+        }
+        return ball + BALL + " " + strike + STRIKE;
     }
 }
