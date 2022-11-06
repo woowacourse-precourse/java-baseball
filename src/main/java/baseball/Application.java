0d0a53858d47e095845cf183c37964192a4f7d53
pushedrumex <pushedrumex@naver.com>
2022-11-06 16:24:18 +0900

    feat(Game.end): end game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,8 @@ public class Application {
 }
 class Game {
     static final String INITGAME = "숫자 야구 게임을 시작합니다.";
+    static final String ENDGAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    static final String RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     static void init() {
         System.out.println(INITGAME);
         List answer = makeThreeNums();
@@ -25,5 +27,17 @@ class Game {
             }
         }
         return threeNums;
+    }
+    static void end() {
+        System.out.println(ENDGAME);
+        System.out.println(RESTART);
+        String input = camp.nextstep.edu.missionutils.Console.readLine();
+        if (input.equals("1")) {
+            init();
+        } else if (input.equals("2")) {
+            return;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
