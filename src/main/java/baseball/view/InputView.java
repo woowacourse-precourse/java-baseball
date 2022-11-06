7f818c5381bd4bd24ec9878788dbf257d7413ff3
Sieun Park <psieun0606@gmail.com>
2022-11-06 14:18:21 +0900

    refactor: 접근 제한자 수정, public에서 private로 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,7 +1,7 @@
 package baseball.view;
 
 public class InputView {
-    public String GAMESTART = "숫자 야구 게임을 시작합니다.";
+    private String GAMESTART = "숫자 야구 게임을 시작합니다.";
     private String INPUT_USERNUM = "숫자를 입력해주세요 : ";
 
     public void GameStart() {
