6f0fd13167f1b2c17634178e400c735e7a8b2e7d
rabbit0216 <rabbit1999k@gmail.com>
2022-11-07 02:42:20 +0900

    feat: 사용자가 맞출때까지 게임 진행하는 함수 구현

--- src/main/java/baseball/Restart.java
+++ src/main/java/baseball/Restart.java
@@ -4,6 +4,17 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Restart {
 
+    public static void untilRight(boolean isRight, String computerNumber){
+        if(isRight){
+            restartGame();
+        }
+        else{
+            String playerNumber = GameSet.inputPlayerNumber();
+            isRight = CompareNumber.checkRight(computerNumber, playerNumber);
+            untilRight(isRight, computerNumber);
+        }
+    }
+
     public static void restartGame(){
         //System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String menu = readLine();
