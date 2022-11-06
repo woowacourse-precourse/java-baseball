7f1ee2dd5ab31be5e9dd8899e6b2863190d1b514
flydog98 <guinnessoverflow@gmail.com>
2022-11-06 19:20:55 +0900

    chore: write skeleton code for InGame

--- src/main/java/baseball/InGame.java
+++ src/main/java/baseball/InGame.java
@@ -1,4 +1,36 @@
 package baseball;
 
 public class InGame {
+    private final BaseballUtil baseballUtil;
+    public InGame(BaseballUtil baseballUtil) {
+        this.baseballUtil = baseballUtil;
+    }
+
+    public boolean playGame() {
+
+    }
+
+    private int createRandomInt() {
+
+    }
+
+    private void announceUserInput() {
+
+    }
+
+    private void announceInputResult() {
+
+    }
+
+    private int getUserInput() {
+
+    }
+
+    private boolean userInputValidation() {
+
+    }
+
+    private int compareUserInput() {
+
+    }
 }
