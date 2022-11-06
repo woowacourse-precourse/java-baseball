c1e548440fe86596b0c55f1dbcc06febf652bb8d
CMHyuk <alsgur9710@naver.com>
2022-11-06 15:54:03 +0900

    style: 줄바꿈 제거

--- src/main/java/baseball/config/Config.java
+++ src/main/java/baseball/config/Config.java
@@ -7,7 +7,6 @@ import baseball.service.GameService;
 import baseball.view.PrintGameResult;
 
 public class Config {
-
     public static PlayGameController playGameController() {
         return new PlayGameController(new GameService(new InputBallNumber(), new RandomBallNumber()), new PrintGameResult());
     }
