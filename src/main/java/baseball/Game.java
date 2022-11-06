1b1db8493812049432bca221b8ebacd1ed594f80
parkmuhyeun <pjhg410@gmail.com>
2022-11-06 11:57:36 +0900

    feat: 추가 게임 여부 입력

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,10 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
     private static final int CONTINUE = 1;
     private static final int END = 2;
 
     public Game() {
+    }
+
+    private static void validateSelect(String select) {
+        if (select.length() != 1 || !Character.isDigit(getCharacter(select)) || !isRightSelect(select))
+            throw new IllegalArgumentException("1이나 2만 입력 가능합니다.");
+    }
+
+    private static boolean isRightSelect(String select) {
+        int digit = Integer.parseInt(select);
+        return digit == CONTINUE || digit == END;
+    }
+
+    private static char getCharacter(String select) {
+        return select.charAt(0);
+    }
+
+    private String inputSelect() {
+        return Console.readLine();
     }
 
     private static void playGame(Computer computer, User user) {
