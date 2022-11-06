9302aa4a6d2be243a29d449889071519832bf1c2
flydog98 <guinnessoverflow@gmail.com>
2022-11-07 00:38:01 +0900

    feat: write constructor for GameClient

--- src/main/java/baseball/GameClient.java
+++ src/main/java/baseball/GameClient.java
@@ -3,8 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class GameClient {
+    private final BaseballUtil baseballUtil;
     public GameClient() {
-
+        this.baseballUtil = new BaseballUtil();
     }
 
     public boolean gameStart() {
