b98382e936e3603e504d8f41a25dbf6fd7c1749a
wjh2012 <wjh2012@naver.com>
2022-11-06 21:13:12 +0900

    refactor(Game): 결과로 게임 종료를 결정하는 기능 while-break 종료방식에서 boolean Token 메서드 방식으로 변경

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,7 +1,7 @@
 package baseball.game;
 
-import baseball.game.player.Computer;
-import baseball.game.player.User;
+import baseball.game.utils.InputValidator;
+import baseball.game.utils.ScoreBoard;
 import java.util.List;
 
 public class Game {
@@ -9,19 +9,32 @@ public class Game {
     private Computer computer;
     private User user;
 
-    public void run() {
+    public Game() {
         this.computer = new Computer();
         this.user = new User();
+    }
 
-        while (true) {
+    public void run() {
+        boolean token = true;
+
+        while (token) {
+
             System.out.print("숫자를 입력해주세요 : ");
             List<Integer> presumeNumbers = user.presume();
+            InputValidator.validatePresume(presumeNumbers);
 
             ScoreBoard scoreBoard = computer.scoreCheck(presumeNumbers);
+            scoreBoard.printBallAndStrike();
 
-            if (scoreBoard.checkStrikeOut()){
-                break;
-            }
+            token = checkGameOver(scoreBoard);
         }
+    }
+
+    public static boolean checkGameOver(ScoreBoard scoreBoard) {
+        if (scoreBoard.checkStrikeOut()){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return false;
+        }
+        return true;
     }
 }
