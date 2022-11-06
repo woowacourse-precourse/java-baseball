b11bceed6ffd70c0e0ca1d66d8b1edb1dc61746a
areyouhun <areyouhun@gmail.com>
2022-11-07 00:01:41 +0900

    test(validaotr): 입력값의 유효성을 판별하는 테스트 세부화

--- src/test/java/mytest/ValidatorTest.java
+++ src/test/java/mytest/ValidatorTest.java
@@ -4,37 +4,64 @@ import static org.assertj.core.api.Assertions.*;
 
 import baseball.settings.Validator;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class ValidatorTest {
-    @DisplayName("입력값이 1과 9 사이의 서로 다른 세 자리 수가 아닌 경우 IllegalArgumentException 발생")
-    @Test
-    void case1() {
-        assertThatThrownBy(() -> Validator.check("1234"))
-                .isInstanceOf(IllegalArgumentException.class);
+    @Nested
+    @DisplayName("입력값이 유효하지 않을 때 예외가 발생하는지 확인")
+    class InvalidNumberTest {
+        @DisplayName("IllegalArgumentException 테스트")
+        @ParameterizedTest
+        @ValueSource(strings = {"1234", "1!3", "12", "", "a72", "803", "886", "919", "141", "332", "277"})
+        void illegalArgumentExceptionTest(String input) {
+            assertThatThrownBy(() -> Validator.check(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("1과 9 사이의 세 자리 수 테스트")
+        @ParameterizedTest
+        @ValueSource(strings = {"1234", "1!3", "12", "", "a72", "803"})
+        void threeNumbersTest(String input) {
+            boolean result = Validator.isThreeNumbers(input);
+            assertThat(result).isFalse();
+        }
+
+        @DisplayName("중복 여부 테스트")
+        @ParameterizedTest
+        @ValueSource(strings = {"886", "919", "141", "332", "277"})
+        void uniquenessTest(String input) {
+            boolean result = Validator.isAllDifferent(input);
+            assertThat(result).isFalse();
+        }
     }
 
-    @Test
-    void case2() {
-        assertThatThrownBy(() -> Validator.check("12"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+    @Nested
+    @DisplayName("입력값이 유효할 때 통과하는지 확인")
+    class ValidNumberTest {
+        @DisplayName("No exception 테스트")
+        @ParameterizedTest
+        @ValueSource(strings = {"123", "654", "493", "761", "587", "213", "874", "527"})
+        void noExceptionTest(String input) {
+            Throwable throwable = catchThrowable(() -> Validator.check(input));
+            assertThat(throwable).isNull();
+        }
 
-    @Test
-    void case3() {
-        assertThatThrownBy(() -> Validator.check("803"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+        @DisplayName("1과 9 사이의 세 자리 수 테스트")
+        @ParameterizedTest
+        @ValueSource(strings = {"123", "654", "493", "761", "587", "213", "874", "527"})
+        void threeNumbersTest(String input) {
+            boolean result = Validator.isThreeNumbers(input);
+            assertThat(result).isTrue();
+        }
 
-    @Test
-    void case4() {
-        assertThatThrownBy(() -> Validator.check("6a4"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void case5() {
-        assertThatThrownBy(() -> Validator.check("**2"))
-                .isInstanceOf(IllegalArgumentException.class);
+        @DisplayName("중복 여부 테스트")
+        @ParameterizedTest
+        @ValueSource(strings = {"123", "654", "493", "761", "587", "213", "874", "527"})
+        void uniquenessTest(String input) {
+            boolean result = Validator.isAllDifferent(input);
+            assertThat(result).isTrue();
+        }
     }
 }
