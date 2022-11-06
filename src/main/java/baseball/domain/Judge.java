403e8acee01dfd78fa840dda6ff0594ef35bdc7a
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-06 23:49:27 +0900

    refactor(Judge): 스트라이크와 볼 개수를 구하는 기능을 Juge 클래스로 캡슐화
    의존성을 최소화 하기 위해 static 메소드로만 구성

---
+++ src/main/java/baseball/domain/Judge.java
@@ -0,0 +1,30 @@
+package baseball.domain;
+
+import baseball.common.Constant;
+
+import java.util.List;
+
+public final class Judge {
+
+    private Judge() {}
+
+    public static int getStrikeCount(List<Integer> computer, List<Integer> player) {
+        int strike = 0;
+        for (int i = 0; i < Constant.MAX_STRIKE_SIZE; i++) {
+            if (computer.get(i).equals(player.get(i))) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    public static int getBallCount(List<Integer> computer, List<Integer> player) {
+        int ball = 0;
+        for (int i = 0; i < Constant.MAX_STRIKE_SIZE; i++) {
+            if (!computer.get(i).equals(player.get(i)) && player.contains(computer.get(i))) {
+                ball++;
+            }
+        }
+        return ball;
+    }
+}
