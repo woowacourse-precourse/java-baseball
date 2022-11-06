5eb9999d53ae7352a0f47f063ad0054702e29588
lynne921 <dkfls921@gmail.com>
2022-11-06 15:50:24 +0900

    feat($main): makeRandomNumber method
    Added new method to $main:
    - make random numbers between 1~9 without duplication
    - return list with random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
@@ -10,10 +15,21 @@ public class Application {
             playGame();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             gameStart = Integer.parseInt(readLine());
-            System.out.println(gameStart);
         } while (gameStart == 1);
     }
 
     private static void playGame() {
     }
+
+    private static List<Integer> makeComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
+
