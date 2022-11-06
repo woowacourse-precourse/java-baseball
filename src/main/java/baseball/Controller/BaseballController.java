d8a0d2104feaa8ccf3fd229434f60e9513213499
yumble <hnjn1101@gmail.com>
2022-11-06 15:30:11 +0900

    feat : add print functions
                    Add new Functions for print message
                    - PrintInformationView
    			+ printGameStartMessage
    			+ printRequestNumber
    			+ printCorrectMessage
    			+ printRestartMessage
    			+ printResultMessage

--- src/main/java/baseball/Controller/BaseballController.java
+++ src/main/java/baseball/Controller/BaseballController.java
@@ -3,14 +3,25 @@ package baseball.Controller;
 
 import baseball.Service.impl.NumberServiceImpl;
 import baseball.Service.interfaces.NumberService;
+import baseball.View.PrintInformationView;
+
+import static baseball.View.PrintInformationView.printGameStartMessage;
+import static baseball.View.PrintInformationView.printRequestNumber;
 
 public class BaseballController {
 
     private final NumberService numberService = new NumberServiceImpl();
+    private final PrintInformationView printInformationView = new PrintInformationView();
 
     public void gameStart () throws IllegalArgumentException{
+        printGameStartMessage();
         numberService.createComputerNumber();
+
+        printRequestNumber();
         numberService.createUserNumber();
+
+
+
     }
 
 }
