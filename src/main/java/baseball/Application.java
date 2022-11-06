2df5a26552404f00f87e7da39e242978c5a7de17
seonghoo1217 <seonghoo1217@naver.com>
2022-11-06 16:29:34 +0900

    refactor: modify indention structure
    I understand the limitation of the indentation line unconditionally 8 characters
    
    Correct this and proceed with indentation in 4 spaces
    
    - Indent 8 spaces After changing line if code is longer
    - Indent blocks in 4 spaces
    
    Refactor(commit_id): #6ece346

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,12 +3,12 @@ package baseball;
 import camp.nextstep.edu.missionutils.*;
 
 public class Application {
-    	public static void main(String[] args) {
+	public static void main(String[] args) {
 		String user_input_number = Console.readLine();
 		try{
-	    		Validation.validation("user_input_number_validation",user_input_number);
+			Validation.validation("user_input_number_validation",user_input_number);
 		}catch (IllegalArgumentException e){
-	    		return;
+			return;
 		}
-    	}
+	}
 }
