cf8d4416386a3a22d9e7dd955bc94aef11c6d369
TaeMin <xoals3094@naver.com>
2022-11-06 16:50:48 +0900

    feat : getRandomThreeDigitNumber 메소드 구현
    컴퓨터가 각 자리가 중복되지 않는 1~9의 숫자로 구성된 3자리 정수를 반환합니다
    computer 리스트에 중복되지 않는 1~9 사이의 정수를 3개 추가한 후
    각 숫자별로 1, 10, 100을 곱해 결과값에 더합니다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
-
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+    public static int getRandomThreeDigitNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        int randomValue = 0;
+        for(int i = 0, digit = 1; digit <= 100; i++, digit *= 10){
+            randomValue += computer.get(i) * digit;
+        }
+        return randomValue;
     }
 }
