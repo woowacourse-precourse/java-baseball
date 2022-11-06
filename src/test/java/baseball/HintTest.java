719d695e473fbd759130236a1afa6d5a65470f44
YuHayeon <youha0930@gmail.com>
2022-11-06 21:12:32 +0900

    test(Hint): strike와 ball의 개수로 힌트 메시지 테스트
    strike와 ball의 개수로 힌트 메시지를 만드는 것 정상적으로 작동하는지 테스트

--- src/test/java/baseball/HintTest.java
+++ src/test/java/baseball/HintTest.java
@@ -2,50 +2,59 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class HintTest {
 
     @Test
-    void 힌트_메시지() {
-        Hint hint = new Hint();
+    void strike_ball_count로_힌트_메시지() {
+        Map<String, Integer> result = new HashMap<>();
 
-        List<Integer> player = Arrays.asList(1, 2, 3);
-        List<Integer> computer = Arrays.asList(3, 2, 1);
-        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("2볼 1스트라이크"));
+        result.put("strike", 3);
+        result.put("ball", 0);
+        Hint hint = new Hint(result);
+        assertThat(hint.makeHintMessage()).isEqualTo("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
-        player = Arrays.asList(6, 2, 7);
-        computer = Arrays.asList(7, 2, 5);
-        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("1볼 1스트라이크"));
+        result.put("strike", 2);
+        result.put("ball", 0);
+        hint = new Hint(result);
+        assertThat(hint.makeHintMessage()).isEqualTo("2스트라이크");
 
-        player = Arrays.asList(7, 2, 5);
-        computer = Arrays.asList(7, 2, 5);
-        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료"));
+        result.put("strike", 1);
+        result.put("ball", 0);
+        hint = new Hint(result);
+        assertThat(hint.makeHintMessage()).isEqualTo("1스트라이크");
 
-        player = Arrays.asList(6, 3, 7);
-        computer = Arrays.asList(6, 3, 1);
-        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("2스트라이크"));
+        result.put("strike", 1);
+        result.put("ball", 2);
+        hint = new Hint(result);
+        assertThat(hint.makeHintMessage()).isEqualTo("2볼 1스트라이크");
 
-        player = Arrays.asList(6, 9, 1);
-        computer = Arrays.asList(6, 3, 4);
-        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("1스트라이크"));
+        result.put("strike", 1);
+        result.put("ball", 1);
+        hint = new Hint(result);
+        assertThat(hint.makeHintMessage()).isEqualTo("1볼 1스트라이크");
 
-        player = Arrays.asList(9, 3, 7);
-        computer = Arrays.asList(6, 2, 1);
-        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("낫싱"));
+        result.put("strike", 0);
+        result.put("ball", 3);
+        hint = new Hint(result);
+        assertThat(hint.makeHintMessage()).isEqualTo("3볼");
 
-        player = Arrays.asList(4, 9, 3);
-        computer = Arrays.asList(9, 3, 4);
-        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("3볼"));
+        result.put("strike", 0);
+        result.put("ball", 2);
+        hint = new Hint(result);
+        assertThat(hint.makeHintMessage()).isEqualTo("2볼");
 
-        player = Arrays.asList(1, 8, 3);
-        computer = Arrays.asList(8, 7, 1);
-        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("2볼"));
+        result.put("strike", 0);
+        result.put("ball", 1);
+        hint = new Hint(result);
+        assertThat(hint.makeHintMessage()).isEqualTo("1볼");
 
-        player = Arrays.asList(6, 9, 2);
-        computer = Arrays.asList(9, 3, 4);
-        assertThat(assertThat(hint.getBaseballResult(player, computer)).isEqualTo("1볼"));
+        result.put("strike", 0);
+        result.put("ball", 0);
+        hint = new Hint(result);
+        assertThat(hint.makeHintMessage()).isEqualTo("낫싱");
     }
 }
