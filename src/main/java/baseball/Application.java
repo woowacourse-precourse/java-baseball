65543d413f4dfb90d8512c5eb6f3ae8a1fc2a057
임형섭 <hs.yim@illb.co.kr>
2022-11-07 05:58:29 +0900

    fix - 게임 종료,재탐색 메세지 위치 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -62,8 +62,7 @@ public class Application {
 
         if(strike == 3){
             System.out.println(strike+"스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
         }
         else if (ball > 0 && strike > 0) {
             System.out.println(ball+"볼 "+strike+"스트라이크");
@@ -144,6 +143,10 @@ public class Application {
     }
 
     private static boolean reGameOrEnd(String usersAnswer) {
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
         if(usersAnswer.equals("1")){
             return true;
         }else{
