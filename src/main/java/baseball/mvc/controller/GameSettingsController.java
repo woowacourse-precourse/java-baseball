3bc6a47db973d1e4cc4a3727536928e1e4f39e0b
apptie <appti@naver.com>
2022-11-06 13:47:12 +0900

    feat(GameSettingsController): 게임이 새로 시작할 경우 컴퓨터 정답을 생성하는 Controller 추가

---
+++ src/main/java/baseball/mvc/controller/GameSettingsController.java
@@ -0,0 +1,14 @@
+package baseball.mvc.controller;
+
+import baseball.domain.game.GameComputer;
+import baseball.mvc.structure.Model;
+import baseball.mvc.util.GameModelKeyConst;
+import baseball.util.GameStatus;
+
+public class GameSettingsController {
+
+    public GameStatus process(Model model) {
+        model.addAttribute(GameModelKeyConst.COMPUTER_KEY, new GameComputer());
+        return GameStatus.PLAY;
+    }
+}
---
+++ src/main/java/baseball/mvc/util/GameModelKeyConst.java
@@ -0,0 +1,9 @@
+package baseball.mvc.util;
+
+public final class GameModelKeyConst {
+
+    public static final String COMPUTER_KEY = "computer";
+
+    private GameModelKeyConst() {
+    }
+}
