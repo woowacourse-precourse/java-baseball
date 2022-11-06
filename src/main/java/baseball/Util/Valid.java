19b9e7a7f2bcf1289873c92e4ae1bcff58338fd9
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-06 20:39:56 +0900

    유효성 검사 구현

--- src/main/java/baseball/Util/Valid.java
+++ src/main/java/baseball/Util/Valid.java
@@ -1,4 +1,20 @@
 package baseball.Util;
 
+import java.util.List;
+
 public class Valid {
+    private static final boolean VALID = true;
+    private static final boolean INVALID = false;
+
+    public static boolean validNumberList(List<Integer> numberList) {
+        if (numberList.size() != 3) {
+            return INVALID;
+        }
+        if (numberList.get(0) == numberList.get(1)
+                || numberList.get(1) == numberList.get(2)
+                || numberList.get(0) == numberList.get(2)
+        ) return INVALID;
+
+        return VALID;
+    }
 }
