fb8201b567466c02d46bc6bcec89c101ef705f68
Soomin Lim <augustth511@gmail.com>
2022-11-06 19:02:12 +0900

    refactor: validateUserExitInput() 메서드 반환 타입 void로 변경

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -18,18 +18,13 @@ public class Game {
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        while (true) {
+        while (!gameExit) {
             List<Integer> computer = getRandomNumberList();
             playGame(computer);
-
-            if (gameExit)
-                break;
         }
-
     }
 
     public void playGame(List<Integer> computer) {
-
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
             String input = Console.readLine();
@@ -118,21 +113,21 @@ public class Game {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
         String input = Console.readLine();
-        int choose = validateUserExitInput(input);
+        validateUserExitInput(input);
+
+        int choose = Integer.parseInt(input);
 
         if (choose == EXIT)
             gameExit = true;
     }
 
-    public int validateUserExitInput(String input) {
+    public void validateUserExitInput(String input) {
         if (input.length() != 1 || !Character.isDigit(input.charAt(0)))
             throw new IllegalArgumentException("1 또는 2만 입력할 수 있습니다.");
 
         int choose = Integer.parseInt(input);
         if (choose != RESTART && choose != EXIT)
             throw new IllegalArgumentException("1 또는 2만 입력할 수 있습니다.");
-
-        return choose;
     }
 
 }
