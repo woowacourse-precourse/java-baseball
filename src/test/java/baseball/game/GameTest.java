fc3c7791beeb7640d34ea7b8669d0194de249a14
seokjin8678 <rnrn8678@naver.com>
2022-11-06 22:14:19 +0900

    test: 출력 메시지에 대한 테스트 코드 수정

--- src/test/java/baseball/game/GameTest.java
+++ src/test/java/baseball/game/GameTest.java
@@ -2,26 +2,21 @@ package baseball.game;
 
 import baseball.Application;
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.*;
 
 class GameTest extends NsTest {
 
-    Game game;
-
-    @BeforeEach
-    void setUp() {
-        game = Game.create(new Computer(), new User());
-    }
-
     @Test
     void 게임시작시_시작메세지가_출력돼야한다() {
-        assertSimpleTest(() ->
-                assertThat(output().contains("숫자 야구 게임을 시작합니다."))
-                        .isTrue()
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("123", "2");
+                    assertThat(output()).contains("숫자 야구 게임을 시작합니다.");
+                },
+                1,2,3
         );
     }
 
