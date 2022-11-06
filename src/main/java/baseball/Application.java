b17afffa59ade197d9e142aae3f3845ded3ba03d
seongha <11pi885@gmail.com>
2022-11-06 19:23:12 +0900

    feat: 실행시 프로그램을 시작하는 메인 함수작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -111,5 +111,14 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.\n");
+        boolean isPlay = true;
+        while (isPlay){
+            List<Integer> computer = createRandomNumber();
+            repeatInput(computer);
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+            isPlay = isPlay();
+        }
     }
 }
