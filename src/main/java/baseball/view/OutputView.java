9a317e90b6ea5c7b8f5eac5e2862f6444ad778ae
TaeHwan <kimta2hwan@gmail.com>
2022-11-06 20:51:39 +0900

    refactor(outputview): 겱과 메시지의 변수명 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -10,7 +10,7 @@ public class OutputView {
     private static final String BALL_AND_STRIKE_MESSAGE = "%d볼 %d스트라이크\n";
     private static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART_OR_STOP_GUIDE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static String RESULT_MESSAGE;
+    private static String resultMessage;
 
     public static void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
@@ -41,7 +41,7 @@ public class OutputView {
     }
 
     public static void printResult() {
-        System.out.println(RESULT_MESSAGE);
+        System.out.println(resultMessage);
     }
 
     public static void printRestartOrStopGuideMessage() {
