f5468594f571dddfedb1be0cd42ed81255d15288
kimdoha <kkhhjj3393@gmail.com>
2022-11-07 00:09:03 +0900

    feat(Computer): 컴퓨터 랜덤 숫자 세 자리 생성 기능 구현

---
+++ src/main/java/baseball/player/Computer.java
@@ -0,0 +1,22 @@
+package baseball.player;
+
+import baseball.number.Number;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+
+public class Computer {
+  private static final Integer NUMBER_SIZE = 3;
+  private Set<Integer> computer = new LinkedHashSet<>();
+
+  public Computer() {
+    while (getSetSize() < NUMBER_SIZE) {
+      Number randomNumber = new Number();
+      computer.add(randomNumber.getNumber());
+    }
+  }
+
+  public Integer getSetSize(){
+    return this.computer.size();
+  }
+}
