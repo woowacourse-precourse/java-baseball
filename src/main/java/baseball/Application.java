32ca7354f5083591312b2dff028bda6dc915cccf
yoon <beatus96@naver.com>
2022-11-07 00:08:40 +0900

    fix : 고치기 "플레이어 수 List로 만들기" , "플레이어 수 유효성 체크" 기능 나누기
    수정한 것 1)  "플레이어 수 입력 받기" 기능
    : String으로 입력받아서, List<Integer>로 만들기
    
    수정한 것 2)  "플레이어 수 유효성 체크하기 기능" : 내부적으로 3가지 함수로 세분화
     i. 세 자리 수인지 체크 함수
     ii. 모든 수가 지정된 범위 내에 있는지 체크 함수
     iii. 각 자리 수가 서로 다른 수인지 체크 함수
    
    수정 이유
    1. 컴퓨터와 결과 값 비교할 때, 동일한 List 형식으로 만들기 위해
    2. test(PlayerNumberTest) 결과, "1123"을 넣었을 때, 유효한 수 "123"이라고 판단하였다.
    -> 오류 수정하기 위해서.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,10 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Application {
@@ -32,13 +29,16 @@ public class Application {
 
 
     //2. 플레이어 수 입력받기
-    public static String getPlayerNumber () throws IllegalArgumentException {
+    public static List<Integer> getPlayerNumber () throws IllegalArgumentException {
         String input = Console.readLine().trim();
-        if(isValidNumber(input)){
-            return input;
+        if(!isNumber(input)){
+            return input.chars()
+                    .boxed()
+                    .collect(Collectors.toList());
         }
         throw new IllegalArgumentException();
     }
+
 
     // 2-1. 유효성 체크 : 숫자인지
     public static boolean isNumber(String input) {
@@ -46,22 +46,26 @@ public class Application {
                 .allMatch(Character::isDigit) ;
     }
 
-    // 2-2. 유효성 체크 : 1-9 사이의 /서로 다른 / 세 자리 수인지
-    public static boolean isValidNumber(String input){
-        if(!isNumber(input)) {
-            return false;
-        }
-        if (input.contains("0")) {
-            return false;
-        }
-        Set<Integer> numberSet = new HashSet<>();
-        int playerNumber =  Integer.parseInt(input);
-        while(playerNumber!=0){
-            numberSet.add(playerNumber%10);
-            playerNumber /=10;
-        }
-
-        return numberSet.size() == DIGITS ;
+    public static boolean isCorrectSize(List<Integer> playerNumber){
+        return playerNumber.size() == DIGITS;
     }
+
+    public static boolean isInRange(List<Integer> playerNumber){
+        return playerNumber.stream()
+                .allMatch(v->
+                        RANDOM_MIN<=v && v<= RANDOM_MAX);
+    }
+
+    public static boolean isNotDuplicated(List<Integer> playerNumber){
+        return Set.copyOf(playerNumber).size() == DIGITS;
+    }
+
+    // 2-2. 유효성 체크 : 세 자리인지 / 1-9 사이 / 서로 다른 수인지
+    public static boolean isValidNumber(List<Integer> playerNumber) {
+        return isCorrectSize(playerNumber) &&
+                isInRange(playerNumber) &&
+                isNotDuplicated(playerNumber);
+    }
+
 
 }
