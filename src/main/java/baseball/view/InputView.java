9619812ed8bc46cac76e19135d74b269e53c9a9a
gnoesnooj <306yyy@naver.com>
2022-11-06 18:26:03 +0800

    feat : 입력 값의 중복값 검증 메소드 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,15 +1,17 @@
 
 package baseball.view;
 
+import baseball.exception.InputDuplicateException;
 import baseball.exception.InputNotNumberException;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class InputView {
 
-    public int inputNumber(){
+    public int inputNumber() {
         try {
             String number = Console.readLine();
             return Integer.parseInt(number);
@@ -18,16 +20,29 @@ public class InputView {
         }
     }
 
-    public List<Integer> inputNumbers(){
+    public List<Integer> inputNumbers() { // 중복여부, 자릿수 여부까지
         try {
             String inputItems = Console.readLine();
             List<Integer> numbers = new ArrayList<>();
+
             for (char item : inputItems.toCharArray()) {
                 numbers.add(Integer.parseInt(Character.toString(item)));
             }
+
+            validateInputDuplicate(numbers);
+
             return numbers;
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException();
+        }
+    }
+
+    private void validateInputDuplicate(List<Integer> inputs) {
+        System.out.println(" validate ");
+        for(Integer input : inputs){
+            if(Collections.frequency(inputs,input) > 1){
+                throw new InputDuplicateException();
+            }
         }
     }
 }
