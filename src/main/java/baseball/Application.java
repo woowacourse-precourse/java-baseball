723fa829a4ac00d49c6676d6c3342517deb9bf97
5nam <ohnam90@gmail.com>
2022-11-06 19:12:29 +0900

    fix: WARNING: Illegal reflective access 비활성화하는 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,9 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         GameManager gameManager = new GameManager();
+        // Illegal Reflective Access Warning 방지
+        gameManager.disableWarning();
+
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (gameManager.getContinuation()) {
             gameManager.startGame();
