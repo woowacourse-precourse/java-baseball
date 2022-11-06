73dfb1d76f54ba8980ba1d15dc992ba19f16be93
swa07016 <swa07016@khu.ac.kr>
2022-11-07 00:19:49 +0900

    feat: set computer's input and get user's input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 상대방(컴퓨터)의 입력 숫자
+        String computer = "";
+        while(computer.length() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            char randomNumberToChar = (char)(randomNumber + '0');
+            if(computer.contains(Character.toString(randomNumberToChar)))
+                computer += randomNumberToChar;
+        }
+
+        // 유저의 입력들
+        for (String userInput : args) {
+
+        }
     }
 }
