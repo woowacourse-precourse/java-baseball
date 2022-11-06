35e23b90ba5950b84dac0176f3361b6e29913186
somsom13 <somin1113@naver.com>
2022-11-06 13:58:52 +0900

    test(BaseballGameTest): test baseballGame game end condition
    baseballGame 에서 플레이어의 결과가 3스트라이크 일 때, 게임이 정상 종료 되는지를 확인하기 위한 테스트 작성
    
    변경사항:
    - BaseballGameTest : 힌트 결과에 따른 isPlaying 변수 검증
    1) 힌트가 3 스트라이크 일 때 게임 종료 (isPlaying 변수 false)
    2) 힌트가 3 스트라이크가 아닐 때 게임 지속 (isPlaying 변수 true)

---
+++ src/test/java/baseball/domain/BaseballGame/BaseballGameTest.java
@@ -0,0 +1,57 @@
+package baseball.domain.BaseballGame;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import baseball.domain.Hint.HintString;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class BaseballGameTest {
+    private BaseballGame baseballGame;
+    private Method checkGameEnd;
+    private Field isPlaying;
+
+    @BeforeEach
+    void setUp() {
+        baseballGame = new BaseballGame();
+        try {
+            checkGameEnd = baseballGame.getClass().getDeclaredMethod("checkPlayerWin", String.class);
+            checkGameEnd.setAccessible(true);
+        } catch (NoSuchMethodException exception) {
+            System.out.println("BaseballGameTest -> checkPlayerWin Method reflection error");
+        }
+
+        try {
+            isPlaying = baseballGame.getClass().getDeclaredField("isPlaying");
+            isPlaying.setAccessible(true);
+        } catch (NoSuchFieldException exception) {
+            System.out.print("BaseballGameTest -> isPlaying variable reflection error");
+        }
+    }
+
+    @Test
+    @DisplayName("힌트가_3스트라이크일_때_게임_종료를_테스트한다")
+    void 힌트가_3스트라이크일때_게임_종료() throws InvocationTargetException, IllegalAccessException {
+        String hintString = HintString.STRIKE.print(3);
+        boolean result = false;
+
+        checkGameEnd.invoke(baseballGame, hintString);
+
+        assertThat(isPlaying.getBoolean(baseballGame)).isEqualTo(result);
+    }
+
+    @Test
+    @DisplayName("힌트가_스트라이크_볼일_때_게임_종료를_테스트한다")
+    void 힌트가_스트라이크가_아닐_때_게임_지속() throws InvocationTargetException, IllegalAccessException {
+        String hintString = HintString.BALL.print(1) + HintString.STRIKE.print(2);
+        boolean result = true;
+
+        checkGameEnd.invoke(baseballGame, hintString);
+
+        assertThat(isPlaying.getBoolean(baseballGame)).isEqualTo(result);
+    }
+}
