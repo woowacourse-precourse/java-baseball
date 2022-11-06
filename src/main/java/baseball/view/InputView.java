9359c864ef522d5ce0afed895d6f5ff9592decfc
Sieun Park <psieun0606@gmail.com>
2022-11-06 14:17:53 +0900

    feat: 사용자 숫자를 입력받는 문구를 출력하는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,10 +2,14 @@ package baseball.view;
 
 public class InputView {
     public String GAMESTART = "숫자 야구 게임을 시작합니다.";
-
+    private String INPUT_USERNUM = "숫자를 입력해주세요 : ";
 
     public void GameStart() {
         System.out.println(GAMESTART);
+    }
+
+    public void InputUserNum() {
+        System.out.println(INPUT_USERNUM);
     }
 
 }
