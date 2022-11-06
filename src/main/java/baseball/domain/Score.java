b43085680eab080f6c80b14a729177db466c200a
TaeHwan <kimta2hwan@gmail.com>
2022-11-06 20:27:56 +0900

    feat(score): 스트라이크와 볼의 개수를 판단하는 기능 구현

---
+++ src/main/java/baseball/domain/Score.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+public class Score {
+
+    private static final int INITIAL_VALUE = 0;
+    private int ball;
+    private int strike;
+
+    public Score() {
+        this.ball = INITIAL_VALUE;
+        this.strike = INITIAL_VALUE;
+    }
+
+    private void ballAndStrikeCount(int answerIndex, int guessedAnswerInder) {
+        if (answerIndex == guessedAnswerInder) {
+            strike++;
+        }
+
+        if (answerIndex != guessedAnswerInder) {
+            ball++;
+        }
+    }
+}
