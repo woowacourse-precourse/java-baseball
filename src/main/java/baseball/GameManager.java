513b46dcfd3f4a4710ab791cbec435061c47c464
mirasoy <nkskjjbb@gmail.com>
2022-11-06 20:18:53 +0900

    [Feat](GameManager) 게임 진행 - 게임 시작 문구 출력

---
+++ src/main/java/baseball/GameManager.java
@@ -0,0 +1,26 @@
+package baseball;
+
+
+
+/**
+* GameManager
+* 게임 진행을 위한 클래스
+* 
+* @author mirasoy
+* @version 0.1, 작업 내용
+*/
+
+public class GameManager {
+	
+	
+	/**
+     * 게임 시작 문구 출력
+     */
+	public void open() {
+		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
+	
+	
+
+	
+}
