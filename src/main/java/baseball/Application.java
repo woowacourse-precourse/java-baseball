6836ab054213fa95897c51aa9c2877b86900a6e8
dohyun <ldh19738@gmail.com>
2022-11-06 21:37:28 +0900

    feat(Application): Implement a program

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,13 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballGame baseballGame = new BaseballGame();
+        Play play = new Play();
+
+        baseballGame.start();
+
+        do {
+            play.game();
+        } while (!baseballGame.isRestart());
     }
 }
