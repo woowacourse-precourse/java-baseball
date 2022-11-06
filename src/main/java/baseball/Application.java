63398f3966561a42acd4c8443c35c4696deb0b97
meunrud <y3a0522@gmail.com>
2022-11-07 01:56:13 +0900

    ✨ 리스타트 또는 종료 묻는 메세지 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -102,14 +102,9 @@ public class Application {
         }
     }
 
-
-
-
-
-
-
-
-
+    public static void restartOrEnd(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+    }
 
 
 
