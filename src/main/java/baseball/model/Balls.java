20b92f0fc59b34ca5e22428665ef29dea1895327
saint6839 <saint6839@gmail.com>
2022-11-06 22:06:46 +0900

    feat(model) : 생성자에 중복, 사이즈, 숫자 유효 범위 검증 메서드 구현

--- src/main/java/baseball/model/Balls.java
+++ src/main/java/baseball/model/Balls.java
@@ -6,15 +6,40 @@ import java.util.Set;
 
 public class Balls {
     private static final int BALLS_MAX_SIZE = 3;
+    private static final int MIN_BALL_NUMBER = 1;
+    private static final int MAX_BALL_NUMBER = 9;
+    private static final String DUPLICATED_INPUT_NUMBER_MESSAGE = "입력된 숫자의 값에 중복된 값이 포함되어서는 안됩니다.";
+
+    private static final String INVALID_INPUT_SIZE_MESSAGE = "입력된 숫자의 값이 " + BALLS_MAX_SIZE + "개가 아닙니다.";
+    private static final String INVALID_INPUT_RANGE_MESSAGE = "입력된 숫자의 값은 " + MIN_BALL_NUMBER + "부터 " + MAX_BALL_NUMBER + "사이의 값이어야 합니다.";
     private final List<Integer> balls;
+
     public Balls(List<Integer> balls) {
+        validateDuplicate(balls);
+        validateSize(balls);
+        validateNumberRange(balls);
         this.balls = balls;
     }
 
     private void validateDuplicate(List<Integer> balls) {
         Set<Integer> set = new HashSet<>(balls);
         if (set.size() != balls.size()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(DUPLICATED_INPUT_NUMBER_MESSAGE);
+        }
+    }
+
+    private void validateSize(List<Integer> balls) {
+        if (balls.size() == BALLS_MAX_SIZE) {
+            return;
+        }
+        throw new IllegalArgumentException(INVALID_INPUT_SIZE_MESSAGE);
+    }
+
+    private void validateNumberRange(List<Integer> balls) {
+        for (int ball : balls) {
+            if (ball < 1 || ball > 9) {
+                throw new IllegalArgumentException(INVALID_INPUT_RANGE_MESSAGE);
+            }
         }
     }
 
