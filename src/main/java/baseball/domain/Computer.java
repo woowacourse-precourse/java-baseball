616c1239aad0e329309510939126f24507aa023a
aud19 <aud19980518@gmail.com>
2022-11-06 18:58:54 +0900

    domain의 Computer 클래스

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,17 +1,26 @@
 package baseball.domain;
 
-public class Computer {
-    private String computerAnswer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-    public Computer(String computerAnswer) {
-        this.computerAnswer = computerAnswer;
+public class Computer {
+    private List<Integer> computerAnswer;
+
+    public Computer() {
+        this.computerAnswer = new ArrayList<>();
     }
 
-    public String getComputerAnswer() {
+    public Computer(List<Integer> computerAnswer) {
+        this.computerAnswer = new ArrayList<>();
+        this.computerAnswer.addAll(computerAnswer);
+    }
+
+    public List<Integer> getComputerAnswer() {
         return computerAnswer;
     }
 
-    public void setComputerAnswer(String computerAnswer) {
+    public void setComputerAnswer(List<Integer> computerAnswer) {
         this.computerAnswer = computerAnswer;
     }
 }
