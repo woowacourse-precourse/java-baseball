dfe469657112c0e3aa10f52084540c81547a1062
yeonhee <ydol2@naver.com>
2022-11-06 16:39:20 +0900

    feat : application에 야구게임을 진행하는 함수 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,9 @@
 package baseball;
 
+import static baseball.BaseballGame.playBaseballGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        playBaseballGame();
     }
 }
