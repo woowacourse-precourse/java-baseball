6ef3455e9b885618c346092178a335d7284556a6
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-06 17:04:02 +0900

    feat: BaseballGameController - startGame 메서드 추가

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -1,7 +1,23 @@
 package baseball.controller;
 
 
+import baseball.domain.Computer;
+import baseball.domain.User;
+import java.util.List;
+
 public class BaseballGameController {
 
+    private Computer computer = new Computer();
+    private User user;
+    private final String RESTART = "1";
+    private final int THREE_STRIKE = 3;
 
+
+    public void startGame() {
+        computer.setRandomNumber();
+        List<Integer> computerRandomNumber = computer.getRandomNumber();
+    }
 }
+
+
+
