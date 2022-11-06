3949ff62e403c627193f99bc34b083261a9b3ac4
famo1245 <kandallee38@gmail.com>
2022-11-07 01:03:44 +0900

    solving (have to make illegal exception)

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,105 @@
+package baseball;
+
+import java.util.ArrayList;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class BaseballGame {
+    private final int SIZE = 3;
+    private ArrayList<String> answer;
+    private ArrayList<String> inputs;
+
+    private int ballCount;
+    private int strikeCount;
+
+    public BaseballGame() {
+        answer = new ArrayList<>(SIZE);// arrayList 의 크기 3으로 고정
+        inputs = new ArrayList<>(SIZE);
+        ballCount = 0;
+        strikeCount = 0;
+        initAnswer();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void run() {
+        while (true) {
+            System.out.print("숫자를 입력해 주세요: ");
+            String input = Console.readLine();
+
+            try {
+                checkNumberInput(input);
+                System.out.println(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(input);
+                e.printStackTrace();
+                return;
+            }
+
+            countStrikeOrBall();
+            printResult();
+
+            if (strikeCount == 3) {
+                String continued = Console.readLine();
+                System.out.println(continued);
+                answer.clear();
+                if (checkAnswer(continued))
+                    return;
+
+                initAnswer();
+            }
+            ballCount = 0;
+            strikeCount = 0;
+        }
+    }
+
+    private void initAnswer() {
+        for (int i = 0; i < SIZE; i++)
+            answer.add(Integer.toString(Randoms.pickNumberInRange(1, 9)));
+    }
+
+    private void checkNumberInput(String input) throws IllegalArgumentException {
+        if (input.length() != SIZE)
+            throw new IllegalArgumentException("잘못된 입력");
+        else {
+            inputs.clear();
+            for (int i = 0; i < SIZE; i++)
+                inputs.add(Character.toString(input.charAt(i)));
+        }
+    }
+
+    private boolean checkAnswer(String continued) throws IllegalArgumentException {
+        if (continued.equals("1"))
+            return false;
+        else if (continued.equals("2"))
+            return true;
+        else
+            throw new IllegalArgumentException();
+    }
+
+    private void countStrikeOrBall() {
+        for (String s : inputs) {
+            if (answer.contains(s) && (inputs.indexOf(s) == answer.indexOf(s)))
+                strikeCount++;
+            else if (answer.contains(s) && (inputs.indexOf(s) != answer.indexOf(s)))
+                ballCount++;
+        }
+    }
+
+    private void printResult() {
+        if (strikeCount != 0 && ballCount != 0)
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        else if (strikeCount == 0 && ballCount != 0)
+            System.out.println(ballCount + "볼");
+        else if (strikeCount != 0) {
+            if (strikeCount == 3) {
+                System.out.println("3스트라이크\n" +
+                        "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            } else
+                System.out.println(strikeCount + "스트라이크");
+        } else
+            System.out.println("낫싱");
+    }
+}
