44f22eba17d5f0fff2a3c329a7177c047c2ec1af
flydog98 <guinnessoverflow@gmail.com>
2022-11-06 19:57:28 +0900

    feat: write announceing input results logic

--- src/main/java/baseball/InGame.java
+++ src/main/java/baseball/InGame.java
@@ -44,8 +44,20 @@ public class InGame {
         baseballUtil.printLine("숫자를 입력해주세요 : ");
     }
 
-    private void announceInputResult() {
-
+    private void announceInputResult(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            baseballUtil.printlnLine("낫싱");
+        }
+        else {
+            String announce = "";
+            if (ball > 0) {
+                announce += Integer.toString(ball) + "볼 ";
+            }
+            if (strike > 0) {
+                announce += Integer.toString(strike) + "스트라이크 ";
+            }
+            baseballUtil.printlnLine(announce);
+        }
     }
 
     private int getUserInput() {
