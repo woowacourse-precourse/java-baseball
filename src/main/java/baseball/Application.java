d915f51fe31f4005a2fbd84452a52ede9231d2a9
devjordy14 <devjordy14@gmail.com>
2022-11-06 20:01:26 +0900

    chore: fix the name of variables according to code convention and remove some useless code

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -40,7 +40,6 @@ public class Application {
     }
 
     public static void checkLengthOfString(String checkString){
-        List<Integer> newList = new ArrayList<>();
         if(checkString.length() != 3){
             throw new IllegalArgumentException("3자리의 입력값이 아닙니다.\n프로그램을 종료합니다.");
         }
@@ -68,26 +67,26 @@ public class Application {
     }
 
     public static List<Integer> checkOpponentAndPlayer(List<Integer> opponent,List<Integer> player){
-        List<Integer> BallAndStrike = new ArrayList<>(
+        List<Integer> ballAndStrike = new ArrayList<>(
                 Arrays.asList(0,0)
         );
         for (int opponentNum = 0; opponentNum < opponent.size(); opponentNum++) {
             for (int playerNum = 0; playerNum < player.size(); playerNum++) {
-                checkBallAndStrike(BallAndStrike,opponent,player,opponentNum,playerNum);
+                checkBallAndStrike(ballAndStrike,opponent,player,opponentNum,playerNum);
             }
         }
-        return BallAndStrike;
+        return ballAndStrike;
     }
 
-    public static List<Integer> checkBallAndStrike(List<Integer> BallAndStrike,List<Integer> opponent, List<Integer> player, int opponentNum, int playerNum){
+    public static List<Integer> checkBallAndStrike(List<Integer> ballAndStrike,List<Integer> opponent, List<Integer> player, int opponentNum, int playerNum){
         if(opponent.get(opponentNum)==player.get(playerNum)){
             if(opponentNum!=playerNum){
-                BallAndStrike.set(0,BallAndStrike.get(0)+1);
+                ballAndStrike.set(0,ballAndStrike.get(0)+1);
             }else{
-                BallAndStrike.set(1,BallAndStrike.get(1)+1);
+                ballAndStrike.set(1,ballAndStrike.get(1)+1);
             }
         }
-        return BallAndStrike;
+        return ballAndStrike;
     }
 }
 
