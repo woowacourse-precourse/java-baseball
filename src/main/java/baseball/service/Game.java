37e912677adec00cf849f7b8373b4fc28f8d1f68
footdev <ekclstkfka5@gmail.com>
2022-11-07 00:32:00 +0900

    refactor: Game 클래스 코드 수정
    입력 숫자에 대한 처리 결과를 출력하는 과정 추가
    
    #13

--- src/main/java/baseball/service/Game.java
+++ src/main/java/baseball/service/Game.java
@@ -1,17 +1,16 @@
 package baseball.service;
 
 public class Game {
-    Rule rule;
-
-    public Game() {
-        this.rule = new Rule();
-    }
+    private Rule rule;
 
     public void run() {
+        rule = new Rule(new Player(), new Computer());
         do {
+            rule.generateRandomNumber();
             rule.getNumber();
             rule.isValid();
             rule.decideStrikeOrBall();
+            rule.printResult();
         } while (rule.reStart);
     }
 }
