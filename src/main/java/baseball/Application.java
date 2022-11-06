fa7119f1568b08344478b0c2d37b4ec370f9adbc
wonseokjee <wsji9404@gmail.com>
2022-11-06 16:58:51 +0900

    Feat: "추가 실행메서드"
    main에 시작문구 입력해주고, 모든 메서드 받아서 실행하고 3스트라이크일때 분기점 주고 아닐때 재귀하는 answer메서드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,12 +9,13 @@ import java.util.Scanner;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        randomPick();
-        inputNumber();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computer = randomPick();
+        anser(computer);
     }
 
     /*1.랜덤으로 컴퓨터가 임의의 값 선택.*/
-    public static void randomPick() {
+    public static List<Integer> randomPick() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -22,31 +23,41 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+        System.out.println(computer);
+        return computer;
     }
 
     /*2. 게임 플레이어가 입력한 숫자 받기*/
-    public static void inputNumber() {
+    public static List<Integer> inputNumber() {
         Scanner scan = new Scanner(System.in);
         List<Integer> user = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
         String strNumber = scan.next();
+        /*예외처리: 세자리가 아닐때 같은 숫자가 있을때*/
+        /*if (!(strNumber.length() == 3)) {
+            System.out.println("세자리 숫자를 입력해주세요");
+        } else if () {
+
+        }*/
         for (String chr : strNumber.split("")) {
             user.add(Integer.parseInt(chr));
         }
+        return user;
     }
 
     /*3. 컴퓨터와 입력숫자를 비교해서 스트라이크 판정 int로 반환*/
-    public static void strike(List<Integer> user, List<Integer> computer) {
+    public static int strike(List<Integer> user, List<Integer> computer) {
         int countStrike = 0;
         for (int i = 0; i < 3; i++) {
             if (user.get(i).equals(computer.get(i))) {
                 countStrike++;
             }
         }
-//        return countStrike;
+        return countStrike;
     }
 
     /*4. 컴퓨터와 입력숫자를 비교해서 볼 판정 int로 반환*/
-    public static void nothingAndBall(List<Integer> user, List<Integer> computer) {
+    public static int ball(List<Integer> user, List<Integer> computer) {
         int countBall = 0;
         for (int i = 0; i < 3; i++) {
             if (computer.contains(user.get(i))) {
@@ -55,6 +66,40 @@ public class Application {
             if (user.get(i).equals(computer.get(i))) {
                 countBall--;
             }
+        }
+        return countBall;
+    }
+
+    /*5. 결과 두개 합쳐서 출력하는 메서드*/
+    public static String counting(int countStrike, int countBall) {
+        if (countStrike == 0 && countBall == 0) {
+            return "낫싱";
+        } else if (countStrike == 3) {
+            return "3스트라이크";
+        } else {
+            String result = Integer.toString(countBall) + "볼 " + Integer.toString(countStrike)+"스트라이크";
+            return result;
+        }
+    }
+
+    /*6. 시작문구 입력, 새로시작 및 종료 메서드*/
+    public static void anser(List<Integer> computer) {
+        List<Integer> user = inputNumber();
+        int countStrike = strike(user, computer);
+        int countBall = ball(user, computer);
+        String counting = counting(countStrike, countBall);
+        System.out.println(counting);
+        if (counting.equals("3스트라이크")) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            Scanner scanner = new Scanner(System.in);
+            int game = scanner.nextInt();
+            if (game == 1) {
+                List<Integer> computerNext = randomPick();
+                anser(computerNext);
+            }
+        } else {
+            anser(computer);
         }
     }
 }
