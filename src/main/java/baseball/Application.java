1289c2500aa4ad0cfacb525d7c605c984cfd67f0
SeongchanCho <wordnumperiod@gmail.com>
2022-11-06 18:07:28 +0900

    - 사용자가 입력한 3개의 정수값들과 컴퓨터가 랜덤하게 뽑은 3개의 정수값들이 서로 일치하는지 확인하는 기능 구현 완료.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,84 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+
+    private static List<Integer> getRandomValues(int startValueInRange, int endValueInRange, int numberOfValues) {
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < numberOfValues; i++) {
+            result.add(Randoms.pickNumberInRange(1, 9));
+        }
+        return result;
+    }
+
+    private static List<Integer> getValuesThroughConsole(int startValueInRange, int endValueInRange, int numberOfValues) throws NumberFormatException{
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < numberOfValues; i++) {
+            int input = Integer.parseInt(Console.readLine());
+            if (input < 0 || input > 9) {
+                throw new NumberFormatException();
+            }
+            result.add(input);
+        }
+        return result;
+    }
+
+    // indent 수준이 2 초과되지 않도록 한다.
+    // 그러려면 기능을 쪼개어 함수로 만들어야 한다.
+    // 하나의 사용자 입력 정수값과 3개의 컴퓨터 입력 정수값을 받아, ball 인지 strike 인지 확인하는 함수를 만든다.
+    private static ResultOfPlayingGame getBaseballResult(List<Integer> computerInputs, List<Integer> userInputs){
+        ResultOfPlayingGame result=new ResultOfPlayingGame();
+        for(int i=0;i<userInputs.size();i++){
+            int userInput=userInputs.get(i);
+            ResultOfPlayingGameWithOneIntegerValue subResult=playGameWithOneIntegerValue(computerInputs,userInput,i);
+            if(subResult==ResultOfPlayingGameWithOneIntegerValue.STRIKE){
+                result.strike+=1;
+            }else if(subResult==ResultOfPlayingGameWithOneIntegerValue.BALL){
+                result.ball+=1;
+            }else if(subResult==ResultOfPlayingGameWithOneIntegerValue.NOTHING){
+                result.nothing+=1;
+            }
+        }
+        return result;
+    }
+
+    private static ResultOfPlayingGameWithOneIntegerValue playGameWithOneIntegerValue(List<Integer> computerInputs, int userInput, int userInputIdx){
+        for(int i=0;i<computerInputs.size();i++){
+            int computerInput= computerInputs.get(i);
+            if (computerInput==userInput&&i==userInputIdx){
+                return ResultOfPlayingGameWithOneIntegerValue.STRIKE;
+            } else if (computerInput == userInput) {
+                return ResultOfPlayingGameWithOneIntegerValue.BALL;
+            }
+        }
+        return ResultOfPlayingGameWithOneIntegerValue.NOTHING;
+    }
+
+    enum ResultOfPlayingGameWithOneIntegerValue{
+        BALL, STRIKE, NOTHING
+    }
+
+    static class ResultOfPlayingGame{
+        int strike;
+        int ball;
+        int nothing;
+
+        ResultOfPlayingGame(){
+            strike=0;
+            ball=0;
+            nothing=0;
+        }
+        public boolean pass(){
+            return strike==3;
+        }
     }
 }
