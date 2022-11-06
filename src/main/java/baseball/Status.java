9e9b1873258aa3ef5a7ee5950f3aa59635a588ea
SeongukBaek <bsu6235@gmail.com>
2022-11-06 17:17:36 +0900

    feat: implement enum Status
    Added new method to Status
    - getStatusByGameOption()

--- src/main/java/baseball/Status.java
+++ src/main/java/baseball/Status.java
@@ -1,2 +1,29 @@
-package baseball;public enum Status {
+package baseball;
+
+import java.util.Arrays;
+
+public enum Status {
+    PROCEEDING(0),
+    RESTART(1),
+    EXIT(2),
+    EMPTY(-1);
+
+    private int code;
+
+    Status(int code) {
+        this.code = code;
+    }
+
+    /*
+    * 주어진 gameOption에 따른 Status를 반환
+    * 예외 처리 필요
+    *
+    * @return Status
+    * */
+    public Status getStatusByGameOption(int gameOption) {
+        return Arrays.stream(Status.values())
+                .filter(status -> status.code == gameOption)
+                .findAny()
+                .orElse(EMPTY);
+    }
 }
