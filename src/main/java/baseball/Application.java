22899a923a304e37509a4d2686ac2bd91ca71b89
TaemHam <tk4003950@gmail.com>
2022-11-07 01:18:42 +0900

    Feat: Application 숫자야구 프로그램 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game game = new Game();
+        boolean continueProcess = true;
+        while (continueProcess) {
+            continueProcess = game.operate(Console.readLine());
+        }
     }
 }
