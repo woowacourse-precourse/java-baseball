36c85a031e6d46a91125078c8b7a260ffc063dda
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-07 00:44:55 +0900

    fix<Application>: try catch 삭제
    - IllegalArgumentException을 try catch로 잡지 않도록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,12 +10,6 @@ public class Application {
         // TODO: 프로그램 구현
         NumberBaseballGame game = new NumberBaseballGame(new ShowGameMessage(),
                 new ScoreBoard(NumberBall.systemNumberBalls()));
-        try {
-            game.play();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        game.play();
     }
 }
