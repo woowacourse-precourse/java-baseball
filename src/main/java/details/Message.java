81217f9e85e99227f2744db617a5d65d64b699f5
BoyCho <iijokv@naver.com>
2022-11-06 19:37:50 +0900

    refactor : 특정 변수 혹은 메시지를 상세 변수로 설정  - 사용된 값 혹은 메시지의 의도를 좀 더 명확하게 알 수 있도록 상세한 이름을 담은 변수로 치환

---
+++ src/main/java/details/Message.java
@@ -0,0 +1,22 @@
+package details;
+
+public class Message {
+
+    public static final String BALL = "볼 ";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+
+    public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String LAST_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String REPLAY = "1";
+    public static final String END = "2";
+
+    public static final String ISNOTNUMBER = "숫자 입력이 아닙니다.";
+    public static final String ISTOOLONG = "입력이 너무 깁니다. 숫자 3자리를 입력하세요.";
+    public static final String ISTOOSHORT = "입력이 너무 짧습니다. 숫자 3자리를 입력하세요.";
+    public static final String ISWRONGCOMMAND = "잘못된 입력입니다. 1 : 게임 새로 시작, 2 : 게임 종료";
+
+    Message(){
+    }
+}
