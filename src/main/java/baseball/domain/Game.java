f351227149e7335bc3c5803fb4f31c889de39061
yun8565 <yunyun8565@naver.com>
2022-11-07 01:04:13 +0900

    feat: 게임 시작 기능 구현
    Game 클래스로 게임 진행과 관련된 로직을 처리한다.

---
+++ src/main/java/baseball/domain/Game.java
@@ -0,0 +1,9 @@
+package baseball.domain;
+
+public class Game {
+    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public void start() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+}
