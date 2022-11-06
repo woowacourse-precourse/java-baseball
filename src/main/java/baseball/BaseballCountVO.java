6027fd7d6c9dee1f1d2b4c392e5cae5613660a15
abbd2 <ajkety00@gmail.com>
2022-11-07 02:29:52 +0900

    feat(BaseballCountVO): 볼과 스트라이크의 개수를 관리하는 클래스 생성, getter&setter 생성

---
+++ src/main/java/baseball/BaseballCountVO.java
@@ -0,0 +1,24 @@
+package baseball;
+
+public class BaseballCountVO {
+
+    private int ballCount = 0;
+    private int strikeConut = 0;
+
+    public int getBallCount() {
+        return ballCount;
+    }
+
+    public void setBallCount(int ballCount) {
+        this.ballCount = ballCount;
+    }
+
+    public int getStrikeConut() {
+        return strikeConut;
+    }
+
+    public void setStrikeConut(int strikeConut) {
+        this.strikeConut = strikeConut;
+    }
+
+}
