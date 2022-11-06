0f104137b194160be8c8709e2153be8a834b6ac0
yujaemin <woawoa0513@gmail.com>
2022-11-07 01:30:25 +0900

    feat: 같은 숫자가 2번 이상 나오는지 검사하는 함수 구현
    입력받은 문자가 같은 숫자를 2번 이상 포함하고 있는지 검사하는 함수 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.HashMap;
+
 public class Player {
     private static final int DESIRED_LENGTH = 3;
 
@@ -18,5 +20,18 @@ public class Player {
 
     public boolean containZero(String input) {
         return input.contains("0");
+    }
+
+    public boolean hasDuplicatedDigit(String input) {
+        HashMap<Character, Integer> numberOfCharacter = new HashMap<>();
+        for (Character character : input.toCharArray()) {
+            numberOfCharacter.put(character, numberOfCharacter.getOrDefault(character, 0) + 1);
+        }
+        for (int number : numberOfCharacter.values()) {
+            if (number > 1) {
+                return true;
+            }
+        }
+        return false;
     }
 }
