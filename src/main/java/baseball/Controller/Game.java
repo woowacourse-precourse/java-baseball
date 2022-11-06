bef73f3b91f25370ac3667028d44d49620ea1272
aossuper7 <aossuper7@naver.com>
2022-11-06 14:49:11 +0900

    feat: 게임 초기 출력 기능

---
+++ src/main/java/baseball/Controller/Game.java
@@ -0,0 +1,16 @@
+package baseball.Controller;
+
+import baseball.View.OutputView;
+
+public class Game {
+
+    private OutputView outputView = new OutputView();
+
+
+    public void run() {
+        outputView.startGameMessage();
+
+    }
+
+
+}
---
+++ src/main/java/baseball/View/OutputView.java
@@ -0,0 +1,11 @@
+package baseball.View;
+
+public class OutputView {
+
+    private static final String STARTMESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public void startGameMessage() {
+        System.out.println(STARTMESSAGE);
+    }
+
+}
