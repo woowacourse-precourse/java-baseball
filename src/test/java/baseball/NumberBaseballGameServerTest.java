329c5fee5fae5f95cd3c669f37ca485befe4368a
DevJay1024 <devjay1024@gmail.com>
2022-11-07 06:39:38 +0900

    test: 게임숫자(서로 다른 숫자로 이뤄진 3자리 숫자) 생성 기능의 테스트 케이스 리팩토링 (assertThat반복을 1회로 수정 및 actual과 expected 명시)

--- src/test/java/baseball/NumberBaseballGameServerTest.java
+++ src/test/java/baseball/NumberBaseballGameServerTest.java
@@ -10,19 +10,22 @@ class NumberBaseballGameServerTest {
     @Test
     void 게임숫자는_3자리의_숫자입니다() {
         List<Integer> gameNumberList = new NumberBaseballGameServer().makeNewGameNumber();
-        int gameNumberList_ExpectedSize = 3;
-        assertThat(gameNumberList.size()).isEqualTo(gameNumberList_ExpectedSize);
+        int expectedGameNumberList = 3;
+        int actualGameNumberList = gameNumberList.size();
+        assertThat(actualGameNumberList).isEqualTo(expectedGameNumberList);
     }
 
     @Test
     void 게임숫자는_서로_다른_숫자로_구성되어_있습니다() {
         List<Integer> gameNumberList = new NumberBaseballGameServer().makeNewGameNumber();
-        boolean expected = false;
+        boolean expectedIsDuplicated = false;
+        boolean actualIsDuplicated = false;
 
         while (gameNumberList.size() != 0) {
             int degreeNumber = gameNumberList.remove(0);
-            assertThat(gameNumberList.contains(degreeNumber)).isEqualTo(expected);
+            actualIsDuplicated |= gameNumberList.contains(degreeNumber);
         }
+        assertThat(actualIsDuplicated).isEqualTo(expectedIsDuplicated);
     }
 
 }
