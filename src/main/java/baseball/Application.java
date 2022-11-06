dd83754a041366dbdcc87ba43cd6c6506913f8f7
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-11-06 11:35:29 +0900

    feat(baseball): 게임 결과를 판정하는 함수 추가
    컴퓨터와 사용자의 값을 받아 결과를 판정하고 그에 따른 출력
    발생. boolean을 리턴하며 3스트라이크의 경우 true, 이외의 경우는
    false를 리턴.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -44,4 +44,33 @@ public class Application {
 
         return inputNumber;
     }
+
+    private static boolean judgement(List<Integer> computerNumber, List<Integer> userNumber) {
+        int ball = 0, strike = 0;
+
+        for (int i = 0;i < 3;i++) {
+            if (userNumber.get(i) == computerNumber.get(i))
+                strike++;
+            else if (computerNumber.contains(userNumber.get(i)))
+                ball++;
+        }
+
+        if (ball > 0 || strike > 0) {
+            if (ball > 0)
+                System.out.print(ball + "볼");
+            if (strike > 0 && ball > 0)
+                System.out.print(" ");
+            if (strike > 0) {
+                System.out.print(strike + "스트라이크");
+            }
+            System.out.println("");
+        }
+        else {
+            System.out.println("낫싱");
+        }
+
+        if (strike == 3)
+            return true;
+        return false;
+    }
 }
