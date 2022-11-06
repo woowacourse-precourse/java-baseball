4465481f83e1830c3cadeae9fe96f6cd2e1f0fdf
Ridealist <cjsn92@gmail.com>
2022-11-07 03:06:06 +0900

    refactor(Application): main 로직 isAppRun 메소드로 분리, 로직 긍정 조건문으로 수정, 필드 이름 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,22 @@
 package baseball;
 
 public class Application {
-    static final int EXIT_GAME = 2;
+    private static final String QUIT = "2";
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        boolean is_quit = false;
-        while (!is_quit) {
+        boolean isApplicationRun = true;
+        while (isApplicationRun) {
             Game.startGame();
-            // if문 2번 쓰면 error 반환
-            // TODO 입력값 요청 및 저장, int 반환이 한번에 있는 구조 Refactoring할 필요성
-            if (Game.ResetGameOrQuit() == EXIT_GAME) {
-                is_quit = true;
-            }
+            String input = Game.ResetGameOrQuit();
+            isApplicationRun = isAppRun(input);
         }
+    }
+
+    private static boolean isAppRun(String input) {
+        if (input.equals(QUIT)) {
+            return false;
+        }
+        return true;
     }
 }
