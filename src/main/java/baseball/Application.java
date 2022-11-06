066a33657ffd2e60f740d204c8792fa97a1bbf8d
BaeDongHyeon <sasd730@gmail.com>
2022-11-07 01:57:17 +0900

    feat: 3스트라이크 시 게임종료 후 재시작 여부 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,6 +17,13 @@ public class Application {
             List<Integer> userNumber = getUserNumber();
             Map<String, Integer> ballStrikeCount = getBallStrikeCount(userNumber, computerNumber);
             printGameResult(ballStrikeCount);
+
+            int restartOrEnd = 0;
+            if (ballStrikeCount.get("스트라이크") == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                restartOrEnd = Integer.parseInt(Console.readLine());
+            }
         }
     }
 
