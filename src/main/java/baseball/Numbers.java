8f3d9851591718453b21bf740c81d2cd8ae281c2
jihong <clara5091@naver.com>
2022-11-06 18:32:46 +0900

    feat: Numbers 클래스 추가
    서로 다른 숫자 3개를 가지고 있는 클래스

---
+++ src/main/java/baseball/Numbers.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Numbers {
+    private List<Integer> numbers;
+
+    public Numbers(){
+        numbers = new ArrayList<>();
+    }
+}
