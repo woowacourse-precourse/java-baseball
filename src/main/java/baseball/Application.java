f01da9722b5f93dc5bcaea57ba58304e75f13140
yoon <beatus96@naver.com>
2022-11-07 00:16:57 +0900

    feat : 추가 "플레이어 수와 컴퓨터 수 비교하기"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -40,31 +40,58 @@ public class Application {
     }
 
 
-    // 2-1. 유효성 체크 : 숫자인지
+    // 3-1. 플레이어 수 유효성 체크 : 숫자인지
     public static boolean isNumber(String input) {
         return input.chars()
                 .allMatch(Character::isDigit) ;
     }
-
+    // 3-2. 플레이어 수 유효성 체크 : 세 자리 수인지
     public static boolean isCorrectSize(List<Integer> playerNumber){
         return playerNumber.size() == DIGITS;
     }
-
+    // 3-3. 플레이어 수 유효성 체크 : 범위 안의 수인지
     public static boolean isInRange(List<Integer> playerNumber){
         return playerNumber.stream()
                 .allMatch(v->
                         RANDOM_MIN<=v && v<= RANDOM_MAX);
     }
-
+    // 3-4. 플레이어 수 유효성 체크 : 서로 다른 수인지
     public static boolean isNotDuplicated(List<Integer> playerNumber){
         return Set.copyOf(playerNumber).size() == DIGITS;
     }
 
-    // 2-2. 유효성 체크 : 세 자리인지 / 1-9 사이 / 서로 다른 수인지
+    // 3. 유효성 체크 : 세 자리인지 / 1-9 사이 / 서로 다른 수인지
     public static boolean isValidNumber(List<Integer> playerNumber) {
         return isCorrectSize(playerNumber) &&
                 isInRange(playerNumber) &&
                 isNotDuplicated(playerNumber);
+    }
+
+
+    //4. 플레이어 수와 컴퓨터 수 비교하기
+    public static Map<String, Integer> checkAnswer(List<Integer> computerNumber, List<Integer>  playerNumber){
+        Map<String, Integer> resultMap = new HashMap<>();
+
+        for(int i=0; i<DIGITS ;i++){ //세자리 수 비교
+            if(computerNumber.contains(playerNumber.get(i))) { //포함하는지
+                checkBallOrStrike(computerNumber.get(i), playerNumber.get(i), resultMap);
+                continue;
+            }
+            int oldValue = resultMap.getOrDefault("nothing", 0);
+            resultMap.put("nothing", oldValue+1);
+
+        }
+        return resultMap;
+    }
+    //4-1. 볼인지 스트라이크인지 확인하기
+    public static void checkBallOrStrike (Integer computerNum, Integer playerNum, Map<String, Integer> resultMap){
+        if(computerNum == playerNum){ //일치하는지
+            int oldValue = resultMap.getOrDefault("strike", 0);
+            resultMap.put("strike", oldValue+1);
+        }
+        int oldValue = resultMap.getOrDefault("ball", 0);
+        resultMap.put("ball", oldValue+1);
+
     }
 
 
