1957087d0cf988ea44bab00df636702b5b13d8d6
dsaf2007 <dsaf2007@naver.com>
2022-11-06 21:01:59 +0900

    test : Judge class의 getScore 함수를 검증하는 테스트 코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -10,72 +10,76 @@ import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
 
-  @Test
-  void 게임종료_후_재시작() {
-    assertRandomNumberInRangeTest(
-      () -> {
-        run("246", "135", "1", "597", "589", "2");
-        assertThat(output())
-          .contains(
-            "낫싱",
-            "3스트라이크",
-            "1볼 1스트라이크",
-            "3스트라이크",
-            "게임 종료"
-          );
-      },
-      1,
-      3,
-      5,
-      5,
-      8,
-      9
-    );
-  }
+    @Test
+    void 게임종료_후_재시작() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "1", "597", "589", "2");
+                    assertThat(output())
+                            .contains(
+                                    "낫싱",
+                                    "3스트라이크",
+                                    "1볼 1스트라이크",
+                                    "3스트라이크",
+                                    "게임 종료");
+                },
+                1,
+                3,
+                5,
+                5,
+                8,
+                9);
+    }
 
-  @Test
-  void 예외_테스트() {
-    assertSimpleTest(() ->
-      assertThatThrownBy(() -> runException("1234"))
-        .isInstanceOf(IllegalArgumentException.class)
-    );
-  }
+    @Test
+    void 예외_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1234"))
+                .isInstanceOf(IllegalArgumentException.class));
+    }
 
-  @Override
-  public void runMain() {
-    Application.main(new String[] {});
-  }
+    @Override
+    public void runMain() {
+        Application.main(new String[] {});
+    }
 
-  @Test
-  public void testIsInputDuplicated() {
-    InputValidator testClass = new InputValidator("133");
-    assertThat(testClass.isInputDuplicated()).isEqualTo(false);
-  }
+    @Test
+    public void testIsInputDuplicated() {
+        InputValidator testClass = new InputValidator("133");
+        assertThat(testClass.isInputDuplicated()).isEqualTo(false);
+    }
 
-  @Test
-  public void testIsRightLength() {
-    InputValidator testClass = new InputValidator("12");
-    assertThat(testClass.isRightLength()).isEqualTo(false);
-  }
+    @Test
+    public void testIsRightLength() {
+        InputValidator testClass = new InputValidator("12");
+        assertThat(testClass.isRightLength()).isEqualTo(false);
+    }
 
-  @Test
-  public void testIsRightNumber() {
-    InputValidator testClass = new InputValidator("-121");
-    assertThat(testClass.isRightNumber()).isEqualTo(false);
-  }
+    @Test
+    public void testIsRightNumber() {
+        InputValidator testClass = new InputValidator("-121");
+        assertThat(testClass.isRightNumber()).isEqualTo(false);
+    }
 
-  @Test
-  public void testInputValidator() {
-    InputValidator testClass = new InputValidator("356");
-    assertThat(testClass.checkInput()).isEqualTo(true);
-  }
+    @Test
+    public void testInputValidator() {
+        InputValidator testClass = new InputValidator("356");
+        assertThat(testClass.checkInput()).isEqualTo(true);
+    }
 
-  @Test
-  public void testComputer() {
-    Computer com = new Computer();
-    com.createNumber();
-    InputValidator testClass = new InputValidator(com.getAnswer());
-    System.out.println(com.getAnswer());
-    assertThat(testClass.checkInput()).isEqualTo(true);
-  }
+    @Test
+    public void testComputer() {
+        Computer com = new Computer();
+        com.createNumber();
+        InputValidator testClass = new InputValidator(com.getAnswer());
+        System.out.println(com.getAnswer());
+        assertThat(testClass.checkInput()).isEqualTo(true);
+    }
+
+    @Test
+    public void testGetScore() {
+        Judge testJudge = new Judge();
+        testJudge.setAnswerToJudge("123", "143");
+        testJudge.getScore();
+        assertThat(testJudge.makeResultString()).isEqualTo("2 스트라이크");
+    }
 }
