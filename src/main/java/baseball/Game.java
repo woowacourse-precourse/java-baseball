ba113a44aae718f7da41c0d5a5609ae98ac435b2
saint6839 <saint6839@gmail.com>
2022-11-06 21:16:21 +0900

    refactor(runner) : 전체적인 로직의 depth를 줄이기 위해, 단순 기능 호출만 담당하고 있는 controller 계층 제거에 따른 의존 객체 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,11 +1,11 @@
 package baseball;
 
-import baseball.controller.ComputerController;
-import baseball.controller.PlayerController;
-import baseball.controller.RefereeController;
 import baseball.model.Ball;
 import baseball.model.GameStatus;
 import baseball.model.Result;
+import baseball.service.ComputerService;
+import baseball.service.PlayerService;
+import baseball.service.RefereeService;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
@@ -17,27 +17,27 @@ public class Game {
     private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final int GAME_RESTART_STATUS = 1;
 
-    private final ComputerController computerController;
-    private final PlayerController playerController;
-    private final RefereeController refereeController;
+    private final ComputerService computerService;
+    private final PlayerService playerService;
+    private final RefereeService refereeService;
 
-    public Game(ComputerController computerController, PlayerController playerController, RefereeController refereeController) {
+    public Game(ComputerService computerService, PlayerService playerService, RefereeService refereeService) {
         System.out.println(START_GAME_MESSAGE);
-        this.computerController = computerController;
-        this.playerController = playerController;
-        this.refereeController = refereeController;
+        this.computerService = computerService;
+        this.playerService = playerService;
+        this.refereeService = refereeService;
     }
 
     public void startGame() {
-        List<Ball> computerBalls = computerController.generateRandomBalls();
+        List<Ball> computerBalls = computerService.generateRandomBalls();
 
         boolean isGameOver;
         GameStatus gameStatus = new GameStatus(GAME_RESTART_STATUS);
         do {
-            List<Ball> playerBalls = playerController.generatePlayerBalls();
+            List<Ball> playerBalls = playerService.generatePlayerBalls();
 
-            Result result = refereeController.generateResult(computerBalls, playerBalls);
-            refereeController.printResult(result);
+            Result result = refereeService.generateResult(computerBalls, playerBalls);
+            refereeService.printResult(result);
 
             isGameOver = result.isGameOver();
             inputGameStatusIfGameOver(gameStatus, isGameOver);
