274f1c321b7c6f1f08aad3cedc06545bc0e16441
K-Diger <kitten.diger@gmail.com>
2022-11-06 19:11:53 +0900

    fix : UserInputValidator 테스트 케이스 버그 수정 - validate() 메서드의 잘못 지정된 filter 조건 구문 수정

--- src/main/java/baseball/user/UserInputValidator.java
+++ src/main/java/baseball/user/UserInputValidator.java
@@ -1,7 +1,6 @@
 package baseball.user;
 
 import java.util.List;
-import java.util.stream.Stream;
 
 public class UserInputValidator {
     private static final UserInputValidator userInputValidator = new UserInputValidator();
@@ -16,8 +15,8 @@ public class UserInputValidator {
     public void validate(List<String> inputValue) {
         if (inputValue.stream()
                 .distinct()
-                .filter(element -> element.charAt(0) < 49)
-                .filter(element -> element.charAt(0) > 57)
+                .filter(element -> element.charAt(0) > 48)
+                .filter(element -> element.charAt(0) < 58)
                 .count() != 3) {
             throw new IllegalArgumentException();
         }
