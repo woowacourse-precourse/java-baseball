cd620c4faef2461af28343bbe6a20878c796f639
choiejae <ejae8319@gmail.com>
2022-11-06 16:18:52 +0900

    refactor: 숫자 첫, 끝 값에 대한 상수 처리 & Randoms 메서드 교체 (pickNumberInRange)

--- src/main/java/baseball/ComputerBallsGenerator.java
+++ src/main/java/baseball/ComputerBallsGenerator.java
@@ -3,21 +3,38 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class ComputerBallsGenerator {
-  private int maxLen;
-  private String balls;
+    private final int START_NUMBER = 1;
+    private final int END_NUMBER = 9;
+    private int maxLen;
+    private String balls;
 
-  public ComputerBallsGenerator() {
-    this(3);
-  }
-  public ComputerBallsGenerator(int maxLen) {
-    this.maxLen = maxLen;
-  }
-  public void ballsGenerator(){
-    String balls = Randoms.pickUniqueNumbersInRange(1, 9, this.maxLen).toString().replaceAll("[^0-9]","");
-    this.balls = balls;
-  }
+    public ComputerBallsGenerator() {
+        this(3);
+    }
 
-  public int getBallsLength() {
-    return balls.length();
-  }
+    public ComputerBallsGenerator(int maxLen) {
+        this.maxLen = maxLen;
+    }
+
+    public void ballsGenerator() {
+        this.balls = getRandomNumbers();
+    }
+    public String getRandomNumbers() {
+        String randomNumbers = new String();
+        return setRandomNumbers(randomNumbers);
+    }
+    public String setRandomNumbers(String randomNumbers) {
+        int randomNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
+        if (!randomNumbers.contains(randomNumber+"")) {
+            randomNumbers += randomNumber + "";
+        }
+        if (randomNumbers.length() != maxLen) {
+            setRandomNumbers(randomNumbers);
+        }
+        return randomNumbers;
+    }
+    // test에서 사용하기 위한 Public 메서드
+    public int getBallsLength() {
+        return maxLen;
+    }
 }
