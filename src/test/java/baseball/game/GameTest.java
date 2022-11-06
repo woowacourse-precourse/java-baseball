e2826b875a6cf763d8e8b06e70baf0236ed8bd59
seokjin8678 <rnrn8678@naver.com>
2022-11-06 23:15:13 +0900

    test: 재시작시 잘못된 입력을 했을 때의 테스트 케이스 추가

--- src/test/java/baseball/game/GameTest.java
+++ src/test/java/baseball/game/GameTest.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.*;
 
 class GameTest extends NsTest {
@@ -15,6 +16,18 @@ class GameTest extends NsTest {
                 () -> {
                     run("123", "2");
                     assertThat(output()).contains("숫자 야구 게임을 시작합니다.");
+                },
+                1,2,3
+        );
+    }
+
+    @Test
+    void 재시작시_잘못된_입력을_하면_예외가_발생한다() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> runException("123","3"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessage("1 또는 2만 입력해주세요.");
                 },
                 1,2,3
         );
