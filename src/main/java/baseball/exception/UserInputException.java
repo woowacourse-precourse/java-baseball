208d5275b7c50c52572e69c49a4a6cc2e943fa00
Namyunsuk <yunsuk0328@naver.com>
2022-11-06 19:01:09 +0900

    feat(UserInputException): 입력 값의 길이에 대한 예외처리 기능 구현

--- src/main/java/baseball/exception/UserInputException.java
+++ src/main/java/baseball/exception/UserInputException.java
@@ -1,4 +1,13 @@
 package baseball.exception;
 
 public class UserInputException {
+
+    private final int VALID_LENGTH = 3;
+
+    public boolean isValidLength(String userInput) {
+        if (userInput.length() == VALID_LENGTH) {
+            return true;
+        }
+        return false;
+    }
 }
