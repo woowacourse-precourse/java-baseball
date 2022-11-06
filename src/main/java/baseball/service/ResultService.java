d3da7d4109c79e2d03196939dcdde40d0e5d1587
ywj9811 <ywj9811@naver.com>
2022-11-06 21:38:33 +0900

    feat: ResultService 클래스 예외처리 위한 자료형 변경
    int형에서 String으로 변환

--- src/main/java/baseball/service/ResultService.java
+++ src/main/java/baseball/service/ResultService.java
@@ -15,7 +15,7 @@ public class ResultService {
 
         int strike = 0;
         int ball;
-        int want;
+        String want;
 
         while (strike != 3) {
             List<Integer> inputNumber = inputValue.inputNum();
@@ -39,10 +39,10 @@ public class ResultService {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("다시 하시려면 1번 종료하시려면 2번을 눌러주세요");
 
-        want = Integer.parseInt(readLine().replaceAll("\\s", ""));
+        want = readLine().replaceAll("\\s", "");
 
         validation.restart(want);
 
-        return want;
+        return Integer.parseInt(want);
     }
 }
