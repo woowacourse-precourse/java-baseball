7a541dab818fbca1f60ae8b8388ec6e9da32238b
은지 <midzy0228@gmail.com>
2022-11-07 00:17:51 +0900

    feat: 게임 시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        PrintMessage.gameStart();
+        GameSystem gameSystem = new GameSystem();
+        gameSystem.startGame();
     }
 }
