6f6e926f8698ae61b6059c1a97bbe760aed15b4f
JaeWoong Kim <kimje1109@gmail.com>
2022-11-07 00:50:14 +0900

    feat: Add startGame

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    public void startGame() {
+        System.out.print("숫자 야구 게임을 시작합니다.");
+    }
+
     public static String getUserInput() {
         System.out.print("숫자를 입력해주세요 : ");
 
