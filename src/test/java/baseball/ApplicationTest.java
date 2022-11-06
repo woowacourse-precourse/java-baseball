7a3d4d8ec1b197cd7d47e137395559eba3886f79
jootang2 <joohwan.song15b@gmail.com>
2022-11-06 19:08:31 +0900

    :white_check_mark: [TEST-기능1] : 컴퓨터가 서로 다른 임의의 수 3개 선택

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,14 +1,29 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static baseball.Application.selectRandomNumber;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    @DisplayName("1. 컴퓨터가 서로 다른 임의의 수 3개 선택")
+    void test1() {
+        String randomNumber = selectRandomNumber();
+
+        System.out.println("randomNumber = " + randomNumber);
+
+        assertThat(randomNumber.charAt(0)).isNotEqualTo(randomNumber.charAt(1));
+        assertThat(randomNumber.charAt(0)).isNotEqualTo(randomNumber.charAt(2));
+        assertThat(randomNumber.charAt(1)).isNotEqualTo(randomNumber.charAt(2));
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
