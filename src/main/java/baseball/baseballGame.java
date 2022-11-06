e534bada1fd5c464ebf1c3cf4da5d7ce12965729
hectick <chyeon1223@cau.ac.kr>
2022-11-06 10:17:26 +0900

    feat : 잘못된 값을 입력했을 경우 에러 발생 후 종료하는 기능 추가

--- src/main/java/baseball/baseballGame.java
+++ src/main/java/baseball/baseballGame.java
@@ -16,18 +16,37 @@ public class BaseballGame {
 
     public void play(){
         computer.createNumber();
-        getPlayerNumber();
+        try{
+            getPlayerNumber();
+        }catch(IllegalArgumentException e){
+            System.out.println("잘못된 값을 입력하셨습니다. 게임 종료");
+        }
     }
 
-    private void getPlayerNumber(){
+    private void getPlayerNumber() throws IllegalArgumentException {
         player = new ArrayList<>();
         String input = Console.readLine();
+        if(input.length() != LENGTH_OF_NUMBER){
+            throw new IllegalArgumentException();
+        }
         int number = Integer.parseInt(input);
         for(int i = LENGTH_OF_NUMBER-1; i >= 0; i--) {
             int digit = number % 10;
-            number = number / 10;
+            if(!isUniqueDigit(digit)){
+                throw new IllegalArgumentException();
+            }
             player.add(0, digit);
+            number = number / 10;
         }
+    }
+
+    private boolean isUniqueDigit(int digit){
+        for(int i = 0; i < player.size(); i++){
+            if(digit == player.get(i)){
+                return false;
+            }
+        }
+        return true;
     }
 
 }
