6a0c14de46e944a0ea586694b59072c8027ba509
Jae Ung Jung <roosa0210@gmail.com>
2022-11-07 05:39:04 +0900

    feat(UserInput(), user): 유저에게서 3자리수 입력받음
    유저에게서 3자리 수 입력 받은 후 리스트로 변환
    유저가 잘못된 값 입력할 경우 IllegalArgumentException 발생 후 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,8 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import org.mockito.stubbing.Answer;
 
@@ -18,10 +20,44 @@ public class Application {
         }
         return computer;
     }
+
+    public static String UserInput(){
+        String userInputString = Console.readLine();
+        return userInputString;
+    }
+
     public static void main(String[] args) {
         List<Integer> answer = new ArrayList<>();
+        int user_input = Integer.MAX_VALUE;
+        List<Integer> user = new ArrayList<>();
+
         game :while(true) {
             answer = Answer();
+            try {
+                System.out.print("숫자 입력 : ");
+                user_input = Integer.parseInt(UserInput());
+
+                if ( user_input < 100 || user_input > 999) {
+                    throw new IllegalArgumentException();
+                }
+
+                user.add(user_input / 100);
+                user.add((user_input % 100) / 10);
+                user.add(user_input % 10);
+
+                if( user.contains(0)) {
+                    throw new IllegalArgumentException();
+                }
+
+                if ( user.get(0) == user.get(1) || user.get(1) == user.get(2) || user.get(0) == user.get(2)) {
+                    throw new IllegalArgumentException();
+                }
+
+            } catch (IllegalArgumentException e) {
+                System.out.println("error");
+                break game;
+            }
+
 
         }
 
