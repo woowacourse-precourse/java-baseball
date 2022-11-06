c7e267095532e20a57e14b2a95ac4bbe692c8ad1
kokomong2 <ko9603@naver.com>
2022-11-06 19:39:14 +0900

    feat : 랜덤3자리 숫자 생성 함수(createRandomNum) 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.Random;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static int createRandomNum(){
+
+        int randomNum =0;
+        for (int i = 0; i < 3; i++) {
+            randomNum= randomNum*10+Randoms.pickNumberInRange(1,9);
+        }
+        return randomNum;
+
     }
 }
