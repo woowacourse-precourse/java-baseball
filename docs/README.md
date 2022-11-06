cb68710c578d51f30723f1e7a573d974f9e27f70
Sunghyun-0 <zkdldk95@naver.com>
2022-11-06 23:33:40 +0900

    기능목록 최신화, 설명 추가/기능목록 구현

--- docs/README.md
+++ docs/README.md
@@ -1,7 +1,9 @@
 # 숫자야구게임 기능목록
 
-- 입력값 List화 기능
-- 입력값 예외 확인/처리 기능
-- 랜덤 answer 생성 기능
-- 숫자야구 판단/return 기능
-- 게임 다시시작 기능
+- Integer List화 기능(IntegerToList): Integer의 각 자릿수를 List에 저장하여 반환
+- 입력값 예외 확인/처리 기능(checkUserInput): userInput이 서로 다른 세자리수가 아닐 때, IllegalArgumentException 발생
+- 숫자야구 판단 기능(playball): 입력값과 답을 비교하여 스트라이크, 볼 개수를 map으로 반환
+- 숫자야구 결과 출력 기능(printResult): 위 판단 결과를 출력할 String 반환
+- 숫자야구 한 판 구동 기능(playOneGame): 3스트라이크가 나올 때까지 userInput으로 숫자야구 진행하여 결과 출력, 매번 입력값이 들어올 때마다 유효한 값이 입력되었는 지 확인
+- 랜덤 answer 생성 기능(determineAnswer): 서로 다른 3개의 숫자를 random으로 골라 세자릿수로 합쳐서 반환 
+- 게임 진행 및 다시시작 기능(main): answer를 생성하고 게임 한 판을 진행한다. gameStarter로 게임을 계속 진행할 지 결정할 수 있다.
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -9,46 +10,33 @@ import java.util.Map;
 import java.util.Set;
 
 public class Application {
-    public List<Integer> userInputToList(Integer userInput){
-        List userInputList = new ArrayList<>();
-        while(userInput > 0){
-            userInputList.add(userInput%10);
-            userInput /= 10;
+    public static List<Integer> IntegerToList(Integer Input){
+        List<Integer> userInputList = new ArrayList<>();
+        while(Input > 0){
+            userInputList.add(Input%10);
+            Input /= 10;
         }
         Collections.reverse(userInputList);
         return userInputList;
     }
 
-    public static void checkUserInput(Object userInput){
-        try{
-            Class userInputClass = userInput.getClass();
-            if(userInputClass != Integer.class){
-                throw new IllegalArgumentException();
-            }
-
-            Integer userInputInteger = Integer.valueOf((int)userInput);
-            Set userInputSet = new HashSet(userInputToList(userInputInteger));
-            if(userInputSet.size() != 3){
-                throw new IllegalArgumentException();
-            }
-        }
-        catch(IllegalArgumentException e){
-            System.out.println("IllegalArgumentException occurs");
-        }
-        catch(Exception e){
-            System.out.println("Other Exception occurs");
+    public static void checkUserInput(String userInput){
+        Integer userInputInteger = Integer.valueOf(userInput);
+        Set<Integer> userInputSet = new HashSet<>(IntegerToList(userInputInteger));
+        if(userInputSet.size() != 3) {
+            throw new IllegalArgumentException();
         }
     }
 
-    public static Map<String, Integer> playball(Integer answer, Integer userInput){
-        List answerList = userInputToList(answer);
-        List userInputList = userInputToList(userInput);
-        Map<String, Integer> score = Map.of(
-                "Ball",0,
-                "Strike",0)
+    public static Map<String, Integer> playball(Integer answer, Integer userInputInteger){
+        List<Integer> answerList = IntegerToList(answer);
+        List<Integer> userInputList = IntegerToList(userInputInteger);
+        Map<String, Integer> score = new HashMap<>(Map.of(
+                "Ball", 0,
+                "Strike", 0));
 
         for(int i=0; i<userInputList.size(); i++){
-            Integer ball = userInputList.get(i);
+            Object ball = userInputList.get(i);
 
             if(answerList.contains(ball) && answerList.indexOf(ball) == i){
                 score.put("Strike", score.get("Strike")+1);
@@ -60,23 +48,58 @@ public class Application {
         return score;
     }
 
-    public String printResult(Map<String, Integer> score){
+    public static String printResult(Map<String, Integer> score){
         String answer = "";
 
-        if(!score.get("Ball") == 0){
-            answer += score.get("Ball").toString() + "볼 "
+        if(score.get("Ball") != 0){
+            answer += score.get("Ball").toString() + "볼 ";
         }
-        if(!score.get("Strike") == 0){
-            answer += score.get("Strike").toString() + "스트라이크"
+        if(score.get("Strike") != 0){
+            answer += score.get("Strike").toString() + "스트라이크";
         }
-
         if(answer.length() == 0){
             answer = "낫싱";
         }
         return answer;
     }
 
+    public static void playOneGame(Integer answer){
+        String result = "";
+
+        while(!result.equals("3스트라이크")) {
+            String userInput = camp.nextstep.edu.missionutils.Console.readLine();
+            checkUserInput(userInput);
+
+            Integer userInputInteger = Integer.valueOf(userInput);
+            result = printResult(playball(answer, userInputInteger));
+            System.out.println(result);
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static int determineAnswer(){
+        List<String> answer = new ArrayList<>();
+        while(answer.size()<3) {
+            String randomNumber = Integer.toString(Randoms.pickNumberInRange(1, 9));
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+        return Integer.parseInt(answer.get(0) + answer.get(1) + answer.get(2));
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int gameStarter = 1;
+        while(gameStarter == 1){
+            System.out.println("숫자야구 게임을 시작합니다.");
+            int answer = determineAnswer();
+
+            playOneGame(answer);
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            gameStarter = Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+        }
     }
 }
