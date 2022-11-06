19e0ec0eaeb4d319b54fd8849f5d9059263946b1
Jae Ung Jung <roosa0210@gmail.com>
2022-11-07 06:27:03 +0900

    feat(main): 게임의 종료여부 확인
    ball과 strike의 수를 확인하여 볼 스트라이크 수 출력 및 종료여부 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -51,35 +51,56 @@ public class Application {
         List<Integer> user = new ArrayList<>();
         List<Integer> ballstrike = Arrays.asList(0,0);
         int user_input = Integer.MAX_VALUE;
+        int game_set = Integer.MAX_VALUE;
 
         game :while(true) {
             answer = Answer();
-            try {
-                System.out.print("숫자 입력 : ");
-                user_input = Integer.parseInt(UserInput());
+            System.out.println(answer);
+            input: while(true){
+                try {
+                    System.out.print("숫자 입력 : ");
+                    user_input = Integer.parseInt(UserInput());
 
-                if ( user_input < 100 || user_input > 999) {
-                    throw new IllegalArgumentException();
+                    if ( user_input < 100 || user_input > 999) {
+                        throw new IllegalArgumentException();
+                    }
+
+                    user.add(user_input / 100);
+                    user.add((user_input % 100) / 10);
+                    user.add(user_input % 10);
+
+                    if( user.contains(0)) {
+                        throw new IllegalArgumentException();
+                    }
+
+                    if ( user.get(0) == user.get(1) || user.get(1) == user.get(2) || user.get(0) == user.get(2)) {
+                        throw new IllegalArgumentException();
+                    }
+
+                } catch (IllegalArgumentException e) {
+                    System.out.println("error");
+                    break game;
                 }
+                ballstrike = BallStrike(answer, user);
+                if(ballstrike.get(0) == 0 && ballstrike.get(1) == 0){
+                    System.out.println("낫싱");
+                } else if (ballstrike.get(1) == 3) {
+                    user.clear();
+                    System.out.println("3스트라이크");
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    break game;
 
-                user.add(user_input / 100);
-                user.add((user_input % 100) / 10);
-                user.add(user_input % 10);
-
-                if( user.contains(0)) {
-                    throw new IllegalArgumentException();
+                }else if(ballstrike.get(0) == 0 && ballstrike.get(1) > 0){
+                    System.out.println(ballstrike.get(1) + "스트라이크");
+                }else if (ballstrike.get(0) > 0 && ballstrike.get(1) == 0){
+                    System.out.println(ballstrike.get(0) + "볼");
+                }else {
+                    System.out.println(ballstrike.get(0) + "볼 " + ballstrike.get(1) + "스트라이크");
                 }
-
-                if ( user.get(0) == user.get(1) || user.get(1) == user.get(2) || user.get(0) == user.get(2)) {
-                    throw new IllegalArgumentException();
-                }
-
-            } catch (IllegalArgumentException e) {
-                System.out.println("error");
-                break game;
+                user.clear();
             }
 
-            ballstrike = BallStrike(answer, user);
 
         }
 
