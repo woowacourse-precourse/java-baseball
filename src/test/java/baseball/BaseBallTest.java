6a8d9b20948f8c31eb9ccf3ff76a718c33b8cc47
namtndus <rnrmf0721@gmail.com>
2022-11-06 21:35:26 +0900

    test(printUserNumberMessage): userNumberMessagePrint 의 기능이 잘 수행되는 지 확인하는 테스트 함수 작성

--- src/test/java/baseball/BaseBallTest.java
+++ src/test/java/baseball/BaseBallTest.java
@@ -7,8 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static baseball.Application.baseballGameStart;
-import static baseball.Application.randomNumberCreate;
+import static baseball.Application.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BaseBallTest {
@@ -18,19 +17,33 @@ public class BaseBallTest {
     @DisplayName("야구게임 시작 메세지 출력하기")
     void printBaseballGameMessage(){
         System.setOut(new PrintStream(output));
+
         String displayMessage = "숫자 야구 게임을 시작합니다.\n";
         baseballGameStart();
         assertThat(output.toString()).isEqualTo(displayMessage);
+
         System.setOut(System.out);
         output.reset();
     }
 
     @Test
-    @DisplayName("컴퓨터의 수가 랜덤하게 출럭되는지 테스")
+    @DisplayName("컴퓨터의 수가 랜덤하게 출럭하기")
     void craeteRandomNumbers(){
         String num1 = randomNumberCreate().toString();
         String num2 = randomNumberCreate().toString();
-        System.out.println( "num1 : "+num1+", num2 : "+num2);
         assertThat(num1).isNotEqualTo(num2);
+    }
+
+    @Test
+    @DisplayName("사용자의 숫자 를물어는 함수")
+    void printUserNumberMessage(){
+        System.setOut(new PrintStream(output));
+
+        String printMessage = "숫자를 입력하세요 : ";
+        userNumberMessagePrint();
+        assertThat(output.toString()).isEqualTo(printMessage);
+
+        System.setOut(System.out);
+        output.reset();
     }
 }
