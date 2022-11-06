4df0e925fc3df1b3d53110e7bfbe1df3c663cf02
hdsung98 <hdsung98@gmail.com>
2022-11-07 03:05:57 +0900

    feat: init function declare
    1. 무한루프 및 재시작 함수 순차 호출
    2. 루프 탈출 변수 초기화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+
+
 public class Application {
+    static int three_strike = 0;
+    static int first_game = 1;
+
+    static void main_loop() {
+
+    }
+
+    static void check_to_restart() {
+
+    }
+
+    static void init_and_start_game() {
+        three_strike = 0;
+
+        if (first_game == 1) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            first_game = 0;
+        }
+        main_loop();
+        check_to_restart();
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        init_and_start_game();
     }
 }
