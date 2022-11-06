57cbd51322b5135fb16a74e32a254248b873068a
ho2n <jungho2n@gmail.com>
2022-11-06 14:20:05 +0900

    feat: 숫자 비교

--- src/main/java/game/player/Computer.java
+++ src/main/java/game/player/Computer.java
@@ -1,6 +1,7 @@
 package game.player;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import camp.nextstep.edu.missionutils.Randoms;
@@ -15,6 +16,22 @@ public class Computer {
 			int randomNumber = Randoms.pickNumberInRange(1, 9);
 			if (!computersDifferentThreeNumbers.contains(randomNumber)) {
 				computersDifferentThreeNumbers.add(randomNumber);
+			}
+		}
+	}
+
+	public boolean computeAndPrintResult(String userNumbers) {
+		String[] userNumbersArr = userNumbers.split("");
+		List<String> userNumbersList = Arrays.asList(userNumbersArr);
+		for (int user_index = 0; user_index < 3; ++user_index) {
+			int number = Integer.parseInt(userNumbersList.get(user_index));
+			if (computersDifferentThreeNumbers.contains(number)) {
+				ball += 1;
+			}
+			if (computersDifferentThreeNumbers.contains(number)
+				&& user_index == computersDifferentThreeNumbers.indexOf(number)) {
+				strike += 1;
+				ball -= 1;
 			}
 		}
 	}
