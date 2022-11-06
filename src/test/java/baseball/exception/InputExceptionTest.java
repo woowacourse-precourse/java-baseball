62914ba8e25a5e13e0863a3072dcb97ece80c0fa
khsrla9806 <kimhunsope@naver.com>
2022-11-06 18:17:06 +0900

    test(InputExceptionTest): 플레이어의 입력에서 예외 발생 테스트 case 1: 중복된 숫자를 입력한 경우 case 2: 사이즈가 3이 아닌 숫자를 입력한 경우 case 3: 1부터 9의 범위를 벗어난 숫자나 문자를 입력한 경우

---
+++ src/test/java/baseball/exception/InputExceptionTest.java
@@ -0,0 +1,21 @@
+package baseball.exception;
+
+import baseball.domain.Player;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class InputExceptionTest {
+    @ParameterizedTest
+    @ValueSource(strings = {"113", "12", "가13", "ABC", "12354", ""})
+    void 입력_에외_테스트(String inputData) {
+        List<String> playersNumber = Arrays.asList(inputData.split(""));
+
+        assertThatThrownBy(() -> InputException.checkExceptionNumber(playersNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
