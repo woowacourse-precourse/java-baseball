edb4d7a96c7d2e48cfad016597f9d009f7e309e2
BaeDongHyeon <sasd730@gmail.com>
2022-11-07 01:02:33 +0900

    feat: 컴퓨터의 숫자 저장

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,8 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
+        List<Integer> computerNumber = getComputerNumber();
+
         while (true) {
             List<Integer> user = getUserNumber();
 
