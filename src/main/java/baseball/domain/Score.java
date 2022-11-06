71a43d7eb5cd4cb7dbea9a30960e74cdb172aa8e
TaeHwan <kimta2hwan@gmail.com>
2022-11-06 21:17:46 +0900

    feat(score): 스트라이크와 볼의 개수를 초기화하는 기능 구현

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -3,10 +3,15 @@ package baseball.domain;
 public class Score {
 
     private static final int INITIAL_VALUE = 0;
+    private static final int ANSWER_STRIKE_COUNT = 3;
     private int ball;
     private int strike;
 
     public Score() {
+        initBallAndStrike();
+    }
+
+    private void initBallAndStrike() {
         this.ball = INITIAL_VALUE;
         this.strike = INITIAL_VALUE;
     }
