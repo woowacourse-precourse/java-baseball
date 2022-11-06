efe261a330a3c1134c77f177260fb21c10de4d46
eora21 <eora21@naver.com>
2022-11-06 23:41:11 +0900

    feat: 🎸 추가 Referee 클래스 및 정확한 답을 맞췄는지 확인하는 메서드
    Accuracy Enum을 사용하여 정확한 답을 정해진 갯수만큼 맞췄는지 반환하는 메서드를 작성

--- src/main/java/baseball/game/Referee.java
+++ src/main/java/baseball/game/Referee.java
@@ -1,7 +1,23 @@
 package baseball.game;
 
 
+import baseball.constant.Accuracy;
+import baseball.constant.Rules;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static baseball.constant.Rules.PICK_COUNT;
+
 public class Referee {
 
+    public boolean isPerfectAnswer(List<Integer> targetNumbers, List<Integer> userNumbers) {
+        validateNumbersLength(targetNumbers, userNumbers);
 
+        Map<Accuracy, Integer> judgeAccuracy = getJudgeAccuracy();
+        shout(judgeAccuracy);
+
+        return judgeAccuracy.get(Accuracy.CORRECT_ANSWER) == PICK_COUNT;
+    }
 }
