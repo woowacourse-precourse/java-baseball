2458523e4bdcd4a71d1f8390d93fbf20748cfbcf
leorb159 <ldgsocial159@naver.com>
2022-11-06 11:20:30 +0900

    feat(baseball): 랜덤 숫자3개 뽑기 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import  camp.nextstep.edu.*;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        ArrayList<Integer> computerNumberList = computerGetRandom3Number();
+
+    }
+
+    static ArrayList<Integer> computerGetRandom3Number() {
+
+        ArrayList<Integer> computerNumberList = new ArrayList<>();
+        while(computerNumberList.size() <3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!computerNumberList.contains(randomNumber)){
+                computerNumberList.add(randomNumber);
+            }
+        }
+        return computerNumberList;
     }
 }
