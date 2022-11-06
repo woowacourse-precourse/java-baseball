2da01b73c2b7065869a85b973352e3deb4c73739
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-06 17:53:22 +0900

    feat(Application) : implement DataBase class
    - declare DataBase class' field

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    class DataBase {
+        private List<Integer> answer = new ArrayList<>();
+        private List<Integer> userInput = new ArrayList<>();
+        private int strike = 0;
+        private int ball = 0;
+        private int nothing = 0;
     }
 }
+
