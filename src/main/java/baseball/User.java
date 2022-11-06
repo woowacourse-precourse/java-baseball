b4f7a85287cb56ab5ba022dfcaff82f78b897d61
miffyKing <whqudghk28@gmail.com>
2022-11-06 17:57:12 +0900

    style: Follow google java coding convention.

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -7,7 +7,7 @@ public class User {
     private static final int PLAYINGMODE = 1;
     private static final int PLAYAGAINMODE = 2;
     private static final int CORRECTINPUTLENGTH = 3;
-    public static String input;
+    private static String input;
 
     public static String inputValue(int whatMode) {
         input = Console.readLine();
@@ -15,7 +15,9 @@ public class User {
     }
 
     private static String validateInput(int whatMode) {
-        assert (whatMode != PLAYAGAINMODE) || (input.equals("1") || input.equals("2")) : "input value is not 1 or 2.";
+        if ((whatMode == PLAYAGAINMODE) && !(input.equals("1") == true || input.equals("2") == true)) {
+            throw new IllegalArgumentException("input value is not 1 or 2.");
+        }
         if (whatMode == PLAYINGMODE) {
             checkInputLength();
             checkInputIsNumNonZero();
@@ -32,13 +34,17 @@ public class User {
 
     private static void checkInputIsNumNonZero() {
         for (int i = 0; i < CORRECTINPUTLENGTH; i++) {
-            assert input.charAt(i) >= '1' && input.charAt(i) <= '9' : "input contains which is not number.";
+            if (!(input.charAt(i) >= '1' && input.charAt(i) <= '9')) {
+                throw new IllegalArgumentException("input contains which is not number.");
+            }
         }
     }
 
     private static void checkInputDuplicated() {
-        assert input.charAt(0) != input.charAt(1) && input.charAt(1) != input.charAt(2)
-            && input.charAt(0) != input.charAt(2) : "input contains duplicated input.";
+        if (input.charAt(0) == input.charAt(1) || input.charAt(1) == input.charAt(2) || input.charAt(0) == input.charAt(
+            2)) {
+            throw new IllegalArgumentException("input contains duplicated input.");
+        }
     }
 
     public static String inputValueTestPlayingMode(String testInput) {
