e0bfdc287bf1ec773623b84294b17af470761fc2
skck0226 <skck0226@ajou.ac.kr>
2022-11-06 15:05:34 +0900

    [fix] application.java 실수입력 코드 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,7 @@
 package baseball;
 
-import java.util.Scanner;
-
 public class Application {
     public static void main(String[] args) {
-    }
 
-    static class Console {
-        public static void newGame() {
-            Scanner console_input = new Scanner(System.in);
-            int a = console_input.nextInt();
-            System.out.println(a);
-        }
     }
 }
---
+++ src/test/java/baseball/PlayerInputTest.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+public class PlayerInputTest {
+
+    @Test
+    void 플레이어_입력_테스트() {
+
+    }
+
+}
