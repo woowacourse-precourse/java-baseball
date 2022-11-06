f93c94482c6bceb203e13da0619645994fab2a3f
Hongjun <ggaye159@naver.com>
2022-11-06 16:55:00 +0900

    유저 숫자 받기와 스트라이크/볼 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,4 +1,5 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.io.*;
@@ -16,6 +17,7 @@ public class Application {
             baseballgame(computernum);
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             play=br.read();
+            computernum.clear();
         }
     }
     public static void makecomputernum(List<Integer> computer){
@@ -24,6 +26,40 @@ public class Application {
             randomnum= Randoms.pickNumberInRange(1,9);
             if(!computer.contains(randomnum))
                 computer.add(randomnum);
+        }
+    }
+    public static void baseballgame(List<Integer> computer){
+        int strike=0;
+        int ball=0;
+        String usernum;
+        int count[]=new int[2];//0인덱스는 스트라이크 1인덱스는 볼
+        while(count[0]<3){
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            Arrays.fill(count,0);
+            System.out.print("숫자를 입력해주세요 :");
+            usernum=Console.readLine();
+            System.out.println("");
+            for(int i=0;i<usernum.length();i++) {
+                pitch((usernum.charAt(i)-'0'), computer, i, count);
+            }
+            if(count[0]==3) {
+                System.out.println("3스트라이크\n 3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+            if(count[0]==0&&count[1]==0)
+                System.out.println("낫싱");
+            if(count[1]!=0)
+                System.out.print(count[1]+"볼 ");
+            if(count[0]!=0)
+                System.out.println(count[0]+"스트라이크");
+        }
+    }
+    public static void pitch(int usernum,List<Integer> computernum,int index,int[] count){
+        if(computernum.contains(usernum)){
+            if(index==computernum.indexOf(usernum))
+                count[0]=count[0]+1;
+            else
+                count[1]=count[1]+1;
         }
     }
 }
