3144b8a6c8c18d4da85870854f5c96f73fc3729c
ghltjd369 <ghltjd369@gmail.com>
2022-11-06 11:22:52 +0900

    refactor : 사용자로부터 입력을 받을 때 예외 처리를 진행하는 클래스

---
+++ src/main/java/baseball/InputException.java
@@ -0,0 +1,49 @@
+package baseball;
+
+import java.util.List;
+
+public class InputException {
+    public static int changeStrToInt(String input) {
+        int number;
+
+        try {
+            number = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("잘못된 입력입니다. 프로그램을 종료합니다.");
+        }
+
+        return number;
+    }
+
+    public static boolean isOverlap(List<Integer> numArr) {
+        boolean overlap = true;
+
+        if (numArr.size() == numArr.stream().distinct().count()) {
+            overlap = false;
+        }
+
+        return overlap;
+    }
+
+    public static boolean isNotSizeThree(List<Integer> numArr) {
+        boolean notSizeThree = false;
+
+        if (numArr.size() != 3) {
+            notSizeThree = true;
+        }
+
+        return notSizeThree;
+    }
+
+    public static boolean hasZero(List<Integer> numArr) {
+        boolean isZero = false;
+
+        for (int i : numArr) {
+            if (i == 0) {
+                isZero = true;
+            }
+        }
+
+        return isZero;
+    }
+}
