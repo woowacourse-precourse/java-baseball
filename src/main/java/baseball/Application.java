e5511c585f59a58a3a2296352472ecd16d78018f
JH-TT <leejongho9803@gmail.com>
2022-11-07 03:20:31 +0900

    feat(application): add function to check number between computer and user
    Added new functions:
    - matchNumber
    - countStrikeBall
    
    Check the checkpoint.
    Change function chooseComputerNumber's return type String to HashMap to match user's number easier.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,17 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Application {
 
-    private static String chooseComputerNumber() {
-        String computer = "";
-        while(computer.length() != 3) {
+    private static HashMap<String, Integer> chooseComputerNumber() {
+        HashMap<String, Integer> computer = new HashMap<>();
+        while(computer.size() != 3) {
             String randomNumber = Integer.toString(Randoms.pickNumberInRange(1, 9));
-            if (computer.contains(randomNumber)) {
+            if (computer.containsKey(randomNumber)) {
                 continue;
             }
-            computer += randomNumber;
+            computer.put(randomNumber, computer.size());
         }
         return computer;
     }
@@ -23,9 +24,8 @@ public class Application {
         String[] res = Arrays.stream(number.split("")).distinct().toArray(String[]::new);
         if(number.length() == 3 && res.length == 3) {
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 
     private static boolean isNumber(String number) {
@@ -49,9 +49,32 @@ public class Application {
     }
     private static Integer playGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        String computer = chooseComputerNumber();
+        HashMap computer = chooseComputerNumber();
+        System.out.println(computer);
         String user = inputUserNumber();
+        int[] result = matchNumber(computer, user);
+        System.out.println(Arrays.toString(result));
+
         return 2;
+    }
+
+    private static int[] countStrikeBall(HashMap com, String num, Integer idx, int[] res) {
+        if(com.containsKey(num)) {
+            if(com.get(num) == idx) {
+                res[0] += 1;
+            } else {
+                res[1] += 1;
+            }
+        }
+        return res;
+    }
+
+    private static int[] matchNumber(HashMap com, String user) {
+        int[] res = {0, 0};
+        for(int i=0; i<3; i++) {
+            res = countStrikeBall(com, Character.toString(user.charAt(i)), i, res);
+        }
+        return res;
     }
 
     public static void main(String[] args) {
