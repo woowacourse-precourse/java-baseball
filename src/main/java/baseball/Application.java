48e5266de5781cad32535018d22e5db75f731c78
Jaehong Lee <woohong24@naver.com>
2022-11-06 17:19:17 +0900

    feat: add start game function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        startGame();
+    }
+
+    private static void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        computerSelectNumber();
+    }
+
+    private static void computerSelectNumber() {
+
     }
 }
