60deb6d5ea5c7e9f050ac414cd3e5db27760d1d2
rabbit0216 <rabbit1999k@gmail.com>
2022-11-06 16:30:32 +0900

    refactor: 숫자 예외사항 체크 함수이름 및 변수이름 수정

--- src/main/java/baseball/CheckException.java
+++ src/main/java/baseball/CheckException.java
@@ -4,12 +4,12 @@ public class CheckException {
     static final char ONE = 81;
     static final char NINE = 89;
 
-    public static boolean checkPlayerNumber(int playerNumber){
-        String playerStringNumber = String.valueOf(playerNumber);
+    public static boolean checkNumber(int number){
+        String playerStringNumber = String.valueOf(number);
         char[] playerNumberArray = playerStringNumber.toCharArray();
         boolean isNumber;
-        for(char number : playerNumberArray){
-            isNumber = checkNumber(number);
+        for(char num : playerNumberArray){
+            isNumber = checkNumber(num);
             if(!isNumber){
                 return false;
             }
