0b968a15d06b6761697e90d46321f66a749c2486
bearjun05 <bearjun05@naver.com>
2022-11-06 22:40:59 +0900

    test : a couple of tests for checkNum

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
@@ -10,6 +12,7 @@ import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -36,14 +39,14 @@ class ApplicationTest extends NsTest {
     @Test
     void 랜덤_숫자_테스트1_3자리_수() {
         List<Integer> randomNum = Application.createRandomNum();
-        Assertions.assertThat(randomNum.size()).isSameAs(3);
+        assertThat(randomNum.size()).isSameAs(3);
     }
 
     @Test
     void 랜덤_숫자_테스트2_중복_숫자() {
         List<Integer> randomNumList = Application.createRandomNum();
         Set<Integer> randomNumSet = new HashSet<>(randomNumList);
-        Assertions.assertThat(randomNumList.size()).isSameAs(randomNumSet.size());
+        assertThat(randomNumList.size()).isSameAs(randomNumSet.size());
     }
 
     @Test
@@ -55,11 +58,22 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 볼_스트라이크_테스트() {
-        String checkNumAnswer = Application.checkNum("356", "345");
-        Assertions.assertThat(checkNumAnswer).isEqualTo("1볼 1스트라이크");
+    void 볼_스트라이크_테스트1() {
+        String checkNumAnswer = Application.checkNum(List.of(3, 5, 6), "345");
+        assertThat(checkNumAnswer).isEqualTo("1볼 1스트라이크");
     }
 
+    @Test
+    void 볼_스트라이크_테스트2() {
+        String checkNumAnswer = Application.checkNum(List.of(6, 7, 8), "345");
+        assertThat(checkNumAnswer).isEqualTo("낫싱");
+    }
+
+    @Test
+    void 볼_스트라이크_테스트3() {
+        String checkNumAnswer = Application.checkNum(List.of(5, 3, 4), "345");
+        assertThat(checkNumAnswer).isEqualTo("3볼");
+    }
 
     @Override
     public void runMain() {
