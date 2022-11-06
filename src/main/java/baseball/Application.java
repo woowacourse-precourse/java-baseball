02c9fa2df07c3a8df560250559edb4fcbf301c2f
seonho <ysh10216@gmail.com>
2022-11-06 19:36:58 +0900

    docs(readme): update docs README(create new step)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
@@ -91,5 +92,6 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
 }
