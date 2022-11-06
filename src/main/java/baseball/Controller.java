879bae4bf0906226e52012b6166a2804ca262d63
tjddnr7760 <tjddnr7760@naver.com>
2022-11-06 20:45:10 +0900

    feat : Controller 클래스 랜덤 숫자 생성기 구현

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -2,6 +2,9 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Controller {
     Controller() {}
 
@@ -10,14 +13,28 @@ public class Controller {
     }
 
     String compareNumber(int humannumber, int computernumber) {
-
+        return  "";
     }
 
     String compareEachNumber(int humanfirst, int humansecond, int humanthird, int computerfirst, int computersecond, int computerthird) {
-
+        return  "";
     }
 
-    int randomNumber() {
+    String randomNumber() {
+        String result = "";
+        int randomnumber = 0;
 
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        randomnumber = computer.get(0) *100 + computer.get(1) * 10 + computer.get(2);
+        result = Integer.toString(randomnumber);
+
+        return result;
     }
 }
