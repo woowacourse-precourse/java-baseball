f605ebc7a457cc875e255ab138234afafd11f449
DevJay1024 <devjay1024@gmail.com>
2022-11-07 03:50:11 +0900

    test: 플레이어의 게임숫자 입력 요청 및 유효성 검사 기능의 테스트 케이스 작성

--- src/test/java/baseball/NumberBaseballGameClientTest.java
+++ src/test/java/baseball/NumberBaseballGameClientTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -9,6 +10,8 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class NumberBaseballGameClientTest {
@@ -45,5 +48,50 @@ class NumberBaseballGameClientTest {
     void 게임_시작_메시지는_출력스트림에_정상_저장된다() {
         NumberBaseballGameClient.showGameStartMessage();
         assertThat(output()).isEqualTo("숫자 야구 게임을 시작합니다.");
+    }
+
+    @Nested
+    @DisplayName("게임숫자를_맞출_숫자에_대한_입력_요청_기능에서")
+    class AskInputNumberTest {
+        @Test
+        void 정상_입력은_에러를_던지지_않고_정상작동한다() {
+            input("321");
+            int expected = 321;
+            int actual = NumberBaseballGameClient.askPlayerGameNumber();
+            assertThat(output()).isEqualTo("숫자를 입력해주세요 : ");
+            assertThat(actual).isEqualTo(expected);
+        }
+
+        @Test
+        void 입력이_문자라면_IllegalArgumentException이_발생한다() {
+            input("간다스");
+            assertThatThrownBy(
+                () -> NumberBaseballGameClient.askPlayerGameNumber()
+            ).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 입력이_4글자_이상이면_IllegalArgumentException이_발생한다() {
+            input("4321");
+            assertThatThrownBy(
+                () -> NumberBaseballGameClient.askPlayerGameNumber()
+            ).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 입력이_0글자_이하라면_IllegalArgumentException이_발생한다() {
+            input("");
+            assertThatThrownBy(
+                () -> NumberBaseballGameClient.askPlayerGameNumber()
+            ).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 입력에_중복된_숫자가_있다면_IllegalArgumentException이_발생한다() {
+            input("424");
+            assertThatThrownBy(
+                () -> NumberBaseballGameClient.askPlayerGameNumber()
+            ).isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
