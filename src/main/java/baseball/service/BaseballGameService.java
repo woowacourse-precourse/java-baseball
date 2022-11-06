d5eece201712b420f7b54e08d0bbad2101a0bb41
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-06 17:06:19 +0900

    feat: BaseballGameService  - updateStrikeCount 메서드 추가
    스트라이크 수를 업데이트해주는 메서드

--- src/main/java/baseball/service/BaseballGameService.java
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -1,7 +1,22 @@
 package baseball.service;
 
 
+import java.util.List;
+
 public class BaseballGameService {
+    private static int strikeCount;
+    private static int ballCount;
+
+    private final int NUMBER_LENGTH = 3;
+
+    public void updateStrikeCount(List<Integer> computerNumber, List<Integer> userNumber) {
+        strikeCount = 0;
+        for (int index = 0; index < NUMBER_LENGTH; index++) {
+            if (computerNumber.get(index).equals(userNumber.get(index))) {
+                strikeCount++;
+            }
+        }
+    }
 
 }
 
