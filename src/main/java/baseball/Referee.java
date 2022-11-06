e22deecaa7ec74a17fb40532a7fbd8b0d136e1bf
June <68291395+june-777@users.noreply.github.com>
2022-11-06 20:29:51 +0900

    feat: 두 야구공 같은 수 존재여부 판단 기능 구현 방법 수정
    기존의 로직에서 자바API를 활용하도록 수정

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,20 +1,16 @@
 package baseball;
 
-import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.Set;
 
 public class Referee {
 
 	public boolean isNothing(List<Integer> computerBalls, List<Integer> playerBalls) {
-		int sameBallCount = 0;
-		Set<Integer> sameBalls = new HashSet<Integer>(computerBalls);
-		for (int playerBall : playerBalls) {
-			if (sameBalls.contains(playerBall)) {
-				sameBallCount += 1;
-			}
-		}
-		if (sameBallCount == 0) {
+		List<Integer> sameBalls = new LinkedList<>();
+		sameBalls.addAll(computerBalls);
+		sameBalls.retainAll(playerBalls);
+
+		if (sameBalls.isEmpty()) {
 			return true;
 		}
 		return false;
