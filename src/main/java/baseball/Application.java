5f1b2f6b2bce3ce3a80e68bb58b5c9b7e7221b76
ttaehee <ttaehee1105@gmail.com>
2022-11-07 02:03:06 +0900

    feat : 입력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,13 +3,15 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Application {
     public static void main(String[] args) {
         new BaseballController(new NumbersGenerator(),
-                new Console(new Output())).run();
+                new Console(new Input(), new Output()),
+                new GameStatus()).run();
     }
 }
 
@@ -18,14 +20,37 @@ class BaseballController {
 
     private NumbersGenerator numbersGenerator;
     private Console console;
+    private GameStatus gameStatus;
 
-    BaseballController(NumbersGenerator numbersGenerator, Console console) {
+    BaseballController(NumbersGenerator numbersGenerator, Console console, GameStatus gameStatus) {
         this.numbersGenerator = numbersGenerator;
         this.console = console;
+        this.gameStatus = gameStatus;
     }
     public void run() {
         Numbers answer = numbersGenerator.generate(COUNT_OF_NUMBERS);
         console.printOutput("숫자 야구 게임을 시작합니다.");
+
+        while (gameStatus.isRunning()) {
+            console.printOutput("숫자를 입력해주세요 : ");
+            String inputString = console.getInput();
+        }
+    }
+}
+
+class GameStatus {
+    private Boolean status = true;
+
+    public boolean isRunning() {
+        return status;
+    }
+}
+
+class Input {
+    Scanner scanner = new Scanner(System.in);
+
+    public String input() {
+        return scanner.nextLine();
     }
 }
 
@@ -36,10 +61,16 @@ class Output {
 }
 
 class Console {
+    Input input;
     Output output;
 
-    Console(Output output) {
+    Console(Input input, Output output) {
+        this.input = input;
         this.output = output;
+    }
+
+    public String getInput() {
+        return input.input();
     }
 
     public void printOutput(String message) {
