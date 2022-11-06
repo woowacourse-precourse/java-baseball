d410bba1ae61268d49f49b5d2e062be05b819632
flydog98 <guinnessoverflow@gmail.com>
2022-11-06 19:25:45 +0900

    feat: write creator for InGame object when no argument, it creates its BaseballUtil and random answer number if granted, it set them its own field

--- src/main/java/baseball/InGame.java
+++ src/main/java/baseball/InGame.java
@@ -2,8 +2,20 @@ package baseball;
 
 public class InGame {
     private final BaseballUtil baseballUtil;
+    private final int answer;
+
+    public InGame() {
+        this.baseballUtil = new BaseballUtil();
+        this.answer = this.createRandomInt();
+    }
     public InGame(BaseballUtil baseballUtil) {
         this.baseballUtil = baseballUtil;
+        this.answer = this.createRandomInt();
+    }
+
+    public InGame(BaseballUtil baseballUtil, int number) {
+        this.baseballUtil = baseballUtil;
+        this.answer = number;
     }
 
     public boolean playGame() {
