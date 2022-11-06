700828a89f4ef667841de0d2f28e440585c023cf
ensk26 <tjensk26@gmail.com>
2022-11-07 03:11:56 +0900

    feat(Application): 사용자 번호가 유효한 숫자인지 확인하는 메서드
    새로운 메서드 추가 to Application:
    - 사용자 번호가 지정된 길이 3이 아닐때 IllegalArgumentException 발생
    - 사용자 번호가 '0'을 포함하면 IllegalArgumentException 발생
    - 사용자 번호가 숫자가 아니면 IllegalArgumentException 발생

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,16 @@ public class Application {
         // TODO: 프로그램 구현
     }
 
+    static boolean isValidUserNumber(String userNumber) {
+        if (userNumber.length() != 3 || userNumber.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+        if (!isNumber(userNumber)) {
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
     static boolean isNumber(String number) {
         try {
             Integer.parseInt(number);
