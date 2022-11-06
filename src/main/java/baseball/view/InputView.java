e69f36e73bf75fc8cbc15e5703aaf794ca5b9ce6
Sieun Park <psieun0606@gmail.com>
2022-11-06 14:11:51 +0900

    refactor: 게임 문구를 추가하는 기능 InputView로 이동

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class InputView {
+    public String GAMESTART = "숫자 야구 게임을 시작합니다.";
+
+
+    public void GameStart() {
+        System.out.println(GAMESTART);
+    }
+
+}
