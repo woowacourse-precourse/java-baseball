1ceecd66957b4b5f3ae7b36a1807f7118bb7a794
채희찬 <eqholic1125@gmail.com>
2022-11-06 21:58:43 +0900

    feat: Result 스트라이크 카운트 메서드 구현

--- src/main/java/baseball/result/Result.java
+++ src/main/java/baseball/result/Result.java
@@ -1,11 +1,42 @@
 package baseball.result;
 
 public class Result {
+    private static final Integer NUMBER_LENGTH = 3;
     private Integer strikeCount;
     private Integer ballCount;
 
-    public Result(Integer strikeCount, Integer ballCount){
+    public Result(Integer strikeCount, Integer ballCount) {
         this.strikeCount = strikeCount;
         this.ballCount = ballCount;
+    }
+
+    public void countStrike(String computerNumber, String userNumber) {
+        Integer count = 0;
+        for (int index = 0; index < NUMBER_LENGTH; index++) {
+            if (computerNumber.charAt(index) == userNumber.charAt(index)) {
+                count++;
+            }
+        }
+
+        strikeCount = count;
+    }
+
+
+
+
+    public Integer getStrikeCount() {
+        return strikeCount;
+    }
+
+    public Integer getBallCount() {
+        return ballCount;
+    }
+
+    public boolean isNothing() {
+        if (strikeCount == 0 && ballCount == 0) {
+            return true;
+        }
+
+        return false;
     }
 }
