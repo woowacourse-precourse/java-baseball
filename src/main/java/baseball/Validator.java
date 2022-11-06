a79256e79ce21517ebccd32c400a7abe0b0c8c7a
yun <yshyeon102@gmail.com>
2022-11-06 21:43:48 +0900

    feat(validator): 길이 제한 검증 구현

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -6,4 +6,20 @@ public class Validator {
     public static final String HAVING_ZERO_EXCEPTION = "0은 포함될 수 없습니다.";
     public static final String HAVING_SAME_NUMBER_EXCEPTION = "서로 다른 숫자만 입력할 수 있습니다";
     public static final String RESTART_RESPONSE_EXCEPTION = "1, 2 이외의 문자가 입력되었습니다.";
+
+    public static void validateLength(String numberStringUserInput) {
+        if (numberStringUserInput.length() != Game.NUMBER_LENGTH) {
+            throw new IllegalArgumentException(LENGTH_EXCEPTION);
+        }
+    }
+
+    //TODO 숫자만 포함 
+
+    //TODO 0 포함 X
+
+    //TODO 서로 다른 숫자만 입력
+
+    //TODO 재식작 응답 검증 시 1,2 이외의 문자 X
+
+
 }
