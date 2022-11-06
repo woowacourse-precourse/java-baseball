a6d439a86c1c7de254e073b7b9e6258112d218ca
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-07 00:46:13 +0900

    feat(Application) : implement Output class

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -56,6 +56,26 @@ class RunGame {
     }
 }
 
+class Output {
+
+    static void printResult(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        }
+        if (strike == 0 && ball != 0) {
+            System.out.println(ball + "볼");
+        }
+        if (strike != 0 && ball != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+        if (strike != 0 && ball == 0) {
+            System.out.println(strike + "스트라이크");
+        }
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+    }
+}
 
 class CompareNumbers {
 
