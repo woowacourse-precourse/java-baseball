3df8a7a19b9f40101f78df39e597c9dbbb7f3eeb
kmularise <kmula222@gmail.com>
2022-11-07 07:22:20 +0900

    feat(model): add the user class

---
+++ src/main/java/baseball/model/User.java
@@ -0,0 +1,25 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+	private static final int NUMBER_LENGTH = 3;
+	private List<Integer> userData;
+
+	public User(){
+		initData();
+	}
+
+	public void editNumberString(String number){
+		for (int i = 0; i < NUMBER_LENGTH ; i++){
+			this.userData.add(Character.getNumericValue(number.charAt(i)));
+		}
+	}
+	public void initData(){
+		this.userData = new ArrayList<Integer>();
+	}
+	public List<Integer> getUserData(){
+		return userData;
+	}
+}
