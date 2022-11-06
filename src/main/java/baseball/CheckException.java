b218d96234f720d7bb2ccdcc310c4ddb9cf5ac4d
park <pjh971212@gmail.com>
2022-11-07 01:57:47 +0900

    feat: 숫자가 아닌 문자를 입력 할 경우 예외 처리

--- src/main/java/baseball/CheckException.java
+++ src/main/java/baseball/CheckException.java
@@ -4,7 +4,7 @@ public class CheckException {
     private static final int LIMIT_LENGTH = 3;
 
     public static void validationNumber(String inputNum) {
-        if (checkNumberLength(inputNum)) {
+        if (!(checkNumberLength(inputNum) && checkNumeric(inputNum))) {
             throw new IllegalArgumentException();
         }
     }
@@ -13,4 +13,13 @@ public class CheckException {
         return checkNum.length() == LIMIT_LENGTH;
     }
 
+    public static boolean checkNumeric(String checkNum) {
+        try {
+            Integer.parseInt(checkNum);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 }
+
