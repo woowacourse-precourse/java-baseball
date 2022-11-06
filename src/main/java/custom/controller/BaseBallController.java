487626b89649b76130973e84938814adcb009d96
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-06 23:00:24 +0900

    feat(controller): 숫자 입력 기능 구현

--- src/main/java/custom/controller/BaseBallController.java
+++ src/main/java/custom/controller/BaseBallController.java
@@ -2,6 +2,8 @@ package custom.controller;
 
 import custom.dto.Response;
 import custom.service.BaseBallService;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BaseBallController {
     private final BaseBallService baseBallService;
@@ -12,5 +14,19 @@ public class BaseBallController {
 
     public Response init() {
         return baseBallService.init();
+    }
+
+    public Response matchNumber(String input) {
+        validateNumber(input);
+        return baseBallService.matchNumber(input);
+    }
+
+
+    void validateNumber(String input) throws IllegalArgumentException {
+        Pattern pattern = Pattern.compile("^\\d{3}$");
+        Matcher matcher = pattern.matcher(input);
+        if (!matcher.matches()) {
+            throw new IllegalArgumentException("숫자 형태의 3글자 입력만 가능합니다");
+        }
     }
 }
