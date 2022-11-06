fd6aaab148c7ac6557c8c0134c226d51a03ef8b4
dener8 <8dener@gmail.com>
2022-11-06 17:01:43 +0900

    feat: countBall() 구현
    countSumOfBallAndStrike()의 반환값에서 countStrike()의 반환값을 뺀 값을 반환하는 countBall() 구현
    아직 countStrike는 구현되지 않은 상태

--- src/main/java/baseball/BallStrikeChecker.java
+++ src/main/java/baseball/BallStrikeChecker.java
@@ -12,8 +12,15 @@ public class BallStrikeChecker {  // 이 클래스에서 모든 판단을 끝내
         return sumOfBallAndStrike;
     }
 
-    public int countStrike() {
+    public int countStrike(String randomNumber, String playerNumber) {
 
+    }
+
+    public int countBall(String randomNumber, String playerNumber) {
+        int sumOfBallAndStrike = countSumOfBallAndStrike(randomNumber, playerNumber);
+        int strike = countStrike(randomNumber, playerNumber);
+        int ball = sumOfBallAndStrike - strike;
+        return ball;
     }
 
 }
