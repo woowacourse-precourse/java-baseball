350a1d2af73fcbd49fbf6742ac37feecb0c0ca2c
hyunseo <goatsfish@gmail.com>
2022-11-06 17:49:56 +0900

    feat : 게임 규칙 enum 추가

---
+++ src/main/java/baseball/referee/GameRule.java
@@ -0,0 +1,20 @@
+package baseball.referee;
+
+public enum GameRule {
+    BASEBALL_MAX_SIZE(3),
+    BASEBALL_MIN_NUMBER(1),
+    BASEBALL_MAX_NUMBER(9),
+    BASEBALL_GAME_RESTART(1),
+    BASEBALL_GAME_END(2),
+    BASEBALL_STRIKE_WIN_COUNT(3);
+
+    private final int rule;
+
+    GameRule(int rule) {
+        this.rule = rule;
+    }
+
+    public int getRule() {
+        return rule;
+    }
+}
