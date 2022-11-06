a468d98ef5eb996ebd2a833c0b12a3df3255ffa2
abbd2 <ajkety00@gmail.com>
2022-11-07 02:57:25 +0900

    feat(Application): 난수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,10 +8,12 @@ public class Application {
     public static void main(String[] args) {
 
         GameController gameController = new GameController();
+        RandomNumber randomNumber = new RandomNumber();
         
         ShowMessage.showGameStart(); // 게임 시작 메세지 출력
         ShowMessage.showInputNumber(); // 숫자 입력 메세지 출력
         gameController.inputUserNumber(); // 사용자 입력
+        randomNumber.createRandomNumber(); // 난수 생성
         
     }
 
