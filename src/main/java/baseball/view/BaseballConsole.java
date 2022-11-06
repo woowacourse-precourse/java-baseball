c3c11a3f190e07732bd1d9582513da7d9e416965
ChoiYeonho0903 <903yh@naver.com>
2022-11-07 04:42:47 +0900

    refactor: BaseballConsole class 정적 메서드로 수정

--- src/main/java/baseball/view/BaseballConsole.java
+++ src/main/java/baseball/view/BaseballConsole.java
@@ -4,7 +4,39 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballConsole {
 
-    public static String userInputNumber() {
+    public static String readUserInputNumber() {
         return Console.readLine();
     }
+
+    public static String readCheckContinueInput() {
+        return Console.readLine();
+    }
+
+    public static void printUserInputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public static void printEndBaseBall() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static void printCheckContinueBaseball() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public static void printStrikeBallCount(int strikeCount, int ballCount) {
+        StringBuilder sb = new StringBuilder();
+        if(strikeCount == 0 && ballCount == 0) {
+            sb.append("낫싱");
+        }
+        if(strikeCount != 0) {
+            sb.append(strikeCount).append("스트라이크 ");
+        }
+        if(ballCount != 0) {
+            sb.append(ballCount).append("볼 ");
+        }
+        sb.append("\n");
+        System.out.print(sb.toString());
+    }
+
 }
