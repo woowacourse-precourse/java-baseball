4a95b9565901d3d0e27ee5b445df8dbeca419003
Songyi Kim <ksl2950@naver.com>
2022-11-06 22:32:48 +0900

    refactor: generateRandomNumber() 호출시, 랜덤 숫자 리스트 초기화되도록 수정

--- src/main/java/baseball/NumberGenerator.java
+++ src/main/java/baseball/NumberGenerator.java
@@ -9,13 +9,10 @@ public class NumberGenerator {
     private static final int LENGTH = 3;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
-    private final List<Integer> baseballNumberList;
 
-    public NumberGenerator(){
-        this.baseballNumberList = new ArrayList<>();
-    }
-
+    List<Integer> baseballNumberList;
     public List<Integer> generateRandomNumber() {
+        this.baseballNumberList = new ArrayList<>();
         for (int i = 0; i < LENGTH; i++) {
             baseballNumberList.add(getUniqueNumber());
         }
