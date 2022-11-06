9557a70e4cc5c26a09f6f29a2c379467b53c9302
jihong <clara5091@naver.com>
2022-11-07 01:18:26 +0900

    fix: 맞은게 하나도 없을 때 출력 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -7,6 +7,9 @@ public class OutputView {
     private static final int STRIKE=2;
 
     public void printResult(List<Integer> counts){
+        if(counts.get(BALL)==0&&counts.get(STRIKE)==0){
+            System.out.print("낫싱");
+        }
         if(counts.get(BALL)!=0){
             System.out.print(counts.get(BALL)+"볼 ");
         }
