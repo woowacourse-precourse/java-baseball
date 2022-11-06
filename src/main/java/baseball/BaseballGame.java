0ee1590a9293b8f1fe7a4579a5de06776af527fa
신채원 <cw121@naver.com>
2022-11-07 05:29:03 +0900

    feat(baseballgame): 힌트 출력 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,6 +2,9 @@ package baseball;
 
 public class BaseballGame {
 
+    private final String Nothing_Message = "낫생";
+    private final String BALL_Message = "볼";
+    private final String STRIKE_Message = "스트라이크";
     private int ball;
     private int strike;
 
@@ -27,5 +30,17 @@ public class BaseballGame {
     private void resetBallAndStrike() {
         this.ball = 0;
         this.strike = 0;
+    }
+
+    private void printHint() {
+        if (ball == 0 && strike == 0) {
+            System.out.println(Nothing_Message);
+        }else if(ball > 0 && strike > 0){
+            System.out.println(ball+BALL_Message+" "+strike+STRIKE_Message);
+        }else if(ball > 0){
+            System.out.println(ball+BALL_Message);
+        }else if(strike > 0){
+            System.out.println(strike+STRIKE_Message);
+        }
     }
 }
