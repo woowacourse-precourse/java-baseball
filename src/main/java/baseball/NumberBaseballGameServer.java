f9df213aeaacc075e9c6917baa658c3eabc7dfe8
DevJay1024 <devjay1024@gmail.com>
2022-11-07 06:00:15 +0900

    refactor: 클래스 명세 변경사항을 숫자 야구 게임 관리 클래스에 적용
    - 게임 숫자(1부터 9까지 서로 다른 수로 이루어진 3자리의 숫자) 생성 기능의 반환형: int -> List
    - 게임 숫자가 몇 자리 숫자인지 상수로 저장

--- src/main/java/baseball/NumberBaseballGameServer.java
+++ src/main/java/baseball/NumberBaseballGameServer.java
@@ -1,19 +1,22 @@
 package baseball;
 
+import java.util.List;
+
 // 숫자 야구 게임 관리
 public class NumberBaseballGameServer {
 
     private static final int GAME_NUMBER_RANGE_MAX = 9;
     private static final int GAME_NUMBER_RANGE_MIN = 1;
+    private static final int COUNT_GAME_NUMBER = 3;
     private int gameNumber;
 
     public void run() {
         // 게임의 흐름과 순서 관리
     }
 
-    public int makeNewGameNumber() {
+    public List<Integer> makeNewGameNumber() {
         // 게임 숫자(1부터 9까지 서로 다른 수로 이루어진 3자리의 숫자) 생성
-        return Integer.MAX_VALUE;
+        return List.of();
     }
 
     public NumberBaseballGameJudgedResultDto judgeInputNumber(int inputNumber) {
