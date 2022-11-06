23eb7e10245193f568889352e4dd82fd706d5923
Lee Min Woo <lmw7414@naver.com>
2022-11-07 04:04:37 +0900

    feat : Player 클래스 생성
    플레이어는 입력을 받을 수 있는 기능을 가지고 있다.

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+    String input;
+
+    public void receiveInput() {
+        this.input = Console.readLine();
+    }
+}
