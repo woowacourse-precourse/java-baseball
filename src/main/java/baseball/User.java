b9802fd54cc44dfc68a5797e68c067cd87348227
JunHee <ljh19980823@gmail.com>
2022-11-06 13:56:41 +0900

    feat : UserInput을 IntegerList로 변환하는 기능 User로 분리

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,9 +1,13 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class User {
     private String userInput;
+    private List<Integer> userInputNumber = new ArrayList<>();
     private boolean flag = false;
     private Integer isProgramEnd = 1;
 
@@ -11,6 +15,15 @@ public class User {
         System.out.print("숫자를 입력해 주세요 : ");
         String userInput = readLine();
         return userInput;
+    }
+
+    public void setUserInputStringToNumber(String userInput) {
+        List<Integer> tempUserInputNumber = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Integer tempNum = Integer.parseInt(String.valueOf(userInput.charAt(i)));
+            tempUserInputNumber.add(tempNum);
+        }
+        this.userInputNumber = tempUserInputNumber;
     }
 
     public void setUserInput(String userInput) {
@@ -23,6 +36,10 @@ public class User {
 
     public void setIsProgramEnd(Integer isProgramEnd) {
         this.isProgramEnd = isProgramEnd;
+    }
+
+    public List<Integer> getUserInputNumber() {
+        return userInputNumber;
     }
 
     public String getUserInput() {
