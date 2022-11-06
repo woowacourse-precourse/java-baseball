1cbb972150b60186bef0157239413d4ea89e8f91
sosow0212 <sosow0212@naver.com>
2022-11-06 14:12:46 +0900

    feat : 유저가 입력한 게임 넘버가 오로지 숫자로 이루어져있는지 확인해주는 기능

--- src/main/java/baseball/exception/InputNumbersException.java
+++ src/main/java/baseball/exception/InputNumbersException.java
@@ -7,6 +7,14 @@ public class InputNumbersException {
     private static final String ONLY_NUMBER_REGEX = "[1-9]+";
     private static final int REQUIRE_NUMBER_SIZE = 3;
 
+    public void isContainedOnlyNumber(String numberTypeOfString) {
+        for (int i = 0; i < numberTypeOfString.length(); i++) {
+            if (!numberTypeOfString.matches(ONLY_NUMBER_REGEX)) {
+                throw new IllegalArgumentException("1~9의 숫자만 입력해주세요.");
+            }
+        }
+    }
+
     public void isOverlapNumber(String numberTypeOfString) {
         Set<Integer> numSet = new HashSet<>();
         for (int i = 0; i < numberTypeOfString.length(); i++) {
