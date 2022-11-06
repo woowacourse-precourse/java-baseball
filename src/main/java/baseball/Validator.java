616d25d627818633238e056091821a99897f2199
Cho SangHyun <98shcho@naver.com>
2022-11-07 01:18:59 +0900

    feat(게임 진행 기능): add feature to check input has duplicate number

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -2,9 +2,11 @@ package baseball;
 
 import org.assertj.core.api.ThrowableAssert;
 
+import java.util.HashSet;
+
 public class Validator {
     public static final boolean VALIDATE_SUCCESS = true;
-    public static final int LENGTH_LIMIT=  3;
+    public static final int LENGTH_LIMIT = 3;
 
     public boolean isThreeLetters(String userInput) {
         if (userInput.length() == LENGTH_LIMIT){
@@ -26,6 +28,18 @@ public class Validator {
         return userInput.contains("0");
     }
 
+    public boolean hasDuplicateNumber(String userInput) {
+        HashSet<Character> inputs = new HashSet<>();
+        for (int index = 0; index < LENGTH_LIMIT; index++) {
+            inputs.add(userInput.charAt(index));
+        }
+        // inputs의 길이가 LENGTH_LIMIT 이하 = 중복되는 것이 있다.
+        if (inputs.size() == LENGTH_LIMIT) {
+            return false;
+        }
+        return true;
+    }
+
     public boolean validateInput(String userInput) {
         if (!isThreeLetters(userInput)) {
             throw new IllegalArgumentException("3자리 수가 아닙니다");
@@ -35,6 +49,9 @@ public class Validator {
         }
         if (ContainsZero(userInput)) {
             throw new IllegalArgumentException("0이 포함되어 있습니다");
+        }
+        if (hasDuplicateNumber(userInput)) {
+            throw new IllegalArgumentException("중복되는 숫자가 있습니다");
         }
 
         return VALIDATE_SUCCESS;
