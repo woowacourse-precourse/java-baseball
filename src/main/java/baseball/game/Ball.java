48d99dbbcfd3d5d4ae2813e700a15269b7de1cb5
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-06 23:58:34 +0900

    feat(Ball): Ball의 기본 생성자 추가

--- src/main/java/baseball/game/Ball.java
+++ src/main/java/baseball/game/Ball.java
@@ -6,11 +6,15 @@ import java.util.List;
 public class Ball {
     private List<Integer> ballData;
 
+    public Ball() {
+
+    }
+
     public Ball(List<Integer> ball) {
         ballData = new ArrayList<>(ball);
     }
 
-    public List<Integer> getBallData(){
+    public List<Integer> getBallData() {
         return new ArrayList<>(ballData);
     }
 }
