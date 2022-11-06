4b259455bf3027f9166463c3dbe47c0c86b114c0
Bumsoo <rhqjatn2398@naver.com>
2022-11-06 15:12:27 +0900

    feat: 시작 문구를 출력하고 게임을 시작

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Game {
+    public void start() {
+        int numComputer;
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+    }
+}
