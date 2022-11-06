da1a7c80fb24b0a48e2f02370d084ecdd72b2834
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-06 16:54:55 +0900

    feat: InputUserNumberException - checkInputSizeThreeNumber 메서드 추가
    3자리수가 아니라면 예외발생

--- src/main/java/baseball/exception/InputUserNumberException.java
+++ src/main/java/baseball/exception/InputUserNumberException.java
@@ -6,4 +6,11 @@ public class InputUserNumberException {
     private final int PROPER_LENGTH = 3;
 
 
+    private void checkInputSizeThreeNumber(String userNumber) {
+        if (userNumber.length() != PROPER_LENGTH) {
+            throw new IllegalArgumentException("3자리의 수를 입력해주세요.");
+        }
+    }
+
+
 }
