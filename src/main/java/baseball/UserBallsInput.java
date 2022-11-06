04c2717973d55240aa375d53af9fc7c465dbc5c9
choiejae <ejae8319@gmail.com>
2022-11-06 15:20:25 +0900

    test: InputNumber validityChecker 5. 중복성 확인 유효한/ 6. 유효하지 않은 경우 테스트 구현

--- src/main/java/baseball/UserBallsInput.java
+++ src/main/java/baseball/UserBallsInput.java
@@ -1,6 +1,11 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class UserBallsInput {
 
@@ -32,7 +37,7 @@ public class UserBallsInput {
     public void validityChecker(String input) {
         validLength(input);
         validElement(input);
-
+        validContinuity(input);
     }
 
     public void validLength(String input) {
@@ -44,6 +49,15 @@ public class UserBallsInput {
         final String REGEX = "[1-9]+";
         if (!input.matches(REGEX)) {
             throw new IllegalArgumentException("input의 구성요소가 0을 제외한 숫자로만 이루어지지 않습니다");
+        }
+    }
+    public void validContinuity(String input) {
+        Set<Character> numSet = new HashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+            numSet.add(input.charAt(i));
+        }
+        if (numSet.size() != maxLen) {
+            throw new IllegalArgumentException("input의 구성요소가 중복됩니다");
         }
     }
 }
