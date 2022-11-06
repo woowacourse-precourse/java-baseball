150944bdb3b9e36b1b3a37bc28b6f5d3c173a16e
sosow0212 <sosow0212@naver.com>
2022-11-06 14:16:10 +0900

    feat : 게임 종료 후 다음 게임을 위해 입력 받은 값의 수의 길이가 적절한지 확인해주는 기능

--- src/main/java/baseball/exception/AfterGamePlayException.java
+++ src/main/java/baseball/exception/AfterGamePlayException.java
@@ -4,6 +4,12 @@ public class AfterGamePlayException {
     private static final String PERMIT_NUMBER_REGEX = "[1-2]+";
     private static final int PERMIT_NUMBER_LENGTH = 1;
 
+    public void isRightLength(String numberTypeOfString) {
+        if (numberTypeOfString.length() != PERMIT_NUMBER_LENGTH) {
+            throw new IllegalArgumentException("하나의 숫자만 입력하셔야 합니다.");
+        }
+    }
+
     public void isRightNumber(String numberOfString) {
         if (!numberOfString.matches(PERMIT_NUMBER_REGEX)) {
             throw new IllegalArgumentException("1~2 까지의 숫자만 입력하셔야합니다.");
