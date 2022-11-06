aa9d1382a43c971a78f6c94f8d3b3a7b5eb1b156
0chil <0@chll.it>
2022-11-07 07:15:14 +0900

    test(study): ForLoopTest

--- src/test/java/study/ForLoopTest.java
+++ src/test/java/study/ForLoopTest.java
@@ -11,29 +11,38 @@ import org.junit.jupiter.api.Test;
 
 public class ForLoopTest {
 
-    private static final String NANO_SECONDS_POSTFIX = " ns";
+    private static final int TEN_POWER_SIX = 1000_000;
 
     @Test
     @DisplayName("For Loop 시간이 For-Each Loop 에 걸리는 시간보다 큰가")
     void is_time_taken_to_for_loop_is_bigger_than_forEach_loop() {
-        List<Integer> list = IntStream.range(0, 1000).boxed().collect(Collectors.toList());
+        List<Integer> list = IntStream.range(0, 1000)
+                .boxed()
+                .collect(Collectors.toList());
 
-        long start = System.nanoTime();
-        for (Integer integer : list) {
-            int aa = integer + 1;
-        }
-        long end = System.nanoTime();
-        long forEachTimeTaken = end - start;
+        long forEachTimeTaken = measureTimeOf(() -> {
+            for (Integer integer : list) {
+                int aa = integer + 1;
+            }
+        });
 
-        start = System.nanoTime();
-        for (int i = 0; i < list.size(); i++) {
-            int aa = list.get(i) + 1;
-        }
-        end = System.nanoTime();
-        long forLoopTimeTaken = end - start;
+        long forLoopTimeTaken = measureTimeOf(() -> {
+            for (int i = 0; i < list.size(); i++) {
+                int aa = list.get(i) + 1;
+            }
+        });
 
-        System.out.println("For-Each Loop 시간 = " + forEachTimeTaken + NANO_SECONDS_POSTFIX);
-        System.out.println("For Loop 시간 = " + forLoopTimeTaken + NANO_SECONDS_POSTFIX);
+        System.out.printf("For Loop 시간 = %d ns = %f ms\n", forLoopTimeTaken, (double)forLoopTimeTaken / TEN_POWER_SIX);
+        System.out.printf("For-Each Loop 시간 = %d ns = %f ms\n", forEachTimeTaken, (double)forEachTimeTaken / TEN_POWER_SIX);
+        System.out.printf("차이 = %f ms\n", (double)Math.abs(forEachTimeTaken - forLoopTimeTaken) / TEN_POWER_SIX);
+
         assertThat(forEachTimeTaken).isGreaterThanOrEqualTo(forLoopTimeTaken);
+    }
+
+    private long measureTimeOf(Runnable runnable) {
+        long start = System.nanoTime();
+        runnable.run();
+        long end = System.nanoTime();
+        return end - start;
     }
 }
