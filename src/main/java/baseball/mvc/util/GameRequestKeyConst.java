dd00c6910d36f1e5fa401e86f5b07641434c7b88
apptie <appti@naver.com>
2022-11-06 20:20:09 +0900

    feat(GameSettingsAdapter): GameSettingsController Adapter 추가

--- src/main/java/baseball/mvc/util/GameRequestKeyConst.java
+++ src/main/java/baseball/mvc/util/GameRequestKeyConst.java
@@ -3,6 +3,7 @@ package baseball.mvc.util;
 public final class GameRequestKeyConst {
 
     public static final String PLAYER_INPUT_KEY = "playerInput";
+    public static final String COMPUTER_KEY = "computer";
 
     private GameRequestKeyConst() {
     }
