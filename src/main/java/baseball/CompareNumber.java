dad6384c642434977519c4f900d161a5fed5cdec
rabbit0216 <rabbit1999k@gmail.com>
2022-11-07 02:19:43 +0900

    feat: 각 자리 숫자 대조 함수 추가

---
+++ src/main/java/baseball/CompareNumber.java
@@ -0,0 +1,44 @@
+package baseball;
+
+import java.util.*;
+
+public class CompareNumber {
+
+    public static List<String> findNumber(String computerStringNumber, String playerStringNumber){
+        List<String> answerList = new ArrayList<>();
+        char[] playerNumberArray = playerStringNumber.toCharArray();
+        String answer;
+        int computerNumberIndex;
+        int playerNumberIndex = 0;
+        for(char number : playerNumberArray){
+            computerNumberIndex = computerStringNumber.indexOf(number);
+            //플레이어 숫자와 컴퓨터 숫자 인덱스 비교 후 해당하는 문구 저장
+            answer = compareIndex(computerNumberIndex, playerNumberIndex);
+            answerList.add(answer);
+            playerNumberIndex++;
+        }
+        answerList = removeNothing(answerList);
+        return answerList;
+    }
+
+    public static List<String> removeNothing(List<String> answerList){
+        if(answerList.size() != 1 && answerList.contains("낫싱")){
+            answerList.remove("낫싱");
+        }
+        return answerList;
+    }
+
+    public static String compareIndex(int computerIndex, int playerIndex){
+        String answer;
+        if(computerIndex == -1){
+            answer = "낫싱";
+        }
+        else if(computerIndex == playerIndex){
+            answer = "스트라이크";
+        }
+        else{
+            answer = "볼";
+        }
+        return answer;
+    }
+}
