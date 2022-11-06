fbffe85f6d00d7b94b35b1f643a292c889991983
viyott-lover <jackk0616@gmail.com>
2022-11-07 02:57:04 +0900

    feat: Catcher 클래스 작성, 1~9 사이 중복하지 않은 랜덤 수를 리스트에 저장

---
+++ src/main/java/baseball/Catcher.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Catcher {
+    private List<Integer> catcherNumbers = new ArrayList<>();
+
+    Catcher() {
+        setCatcherNumbers();
+    }
+
+    public void setCatcherNumbers() {
+        while (catcherNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!catcherNumbers.contains(randomNumber)) {
+                catcherNumbers.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getCatcherNumbers() {
+        return catcherNumbers;
+    }
+}
