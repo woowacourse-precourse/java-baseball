1c1d05b175b7c40d2b09b760aaece1ad0664c02b
CMHyuk <alsgur9710@naver.com>
2022-11-06 19:08:17 +0900

    refactor: 클래스 이름 변경(OutPutView)으로 인한 코드 수정

--- src/main/java/baseball/config/Config.java
+++ src/main/java/baseball/config/Config.java
@@ -4,10 +4,10 @@ import baseball.ballnumber.InputBallNumber;
 import baseball.ballnumber.RandomBallNumber;
 import baseball.controller.PlayGameController;
 import baseball.service.GameService;
-import baseball.view.PrintGameResult;
+import baseball.view.OutputView;
 
 public class Config {
     public static PlayGameController playGameController() {
-        return new PlayGameController(new GameService(new InputBallNumber(), new RandomBallNumber()), new PrintGameResult());
+        return new PlayGameController(new GameService(new InputBallNumber(), new RandomBallNumber()), new OutputView());
     }
 }
