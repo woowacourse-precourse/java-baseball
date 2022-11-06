ff10e2babde10623db4d0da68962c23f1f800bb8
korjun1993 <corojoon93@naver.com>
2022-11-06 14:52:19 +0900

    feat(GameResult) : 게임 결과 출력 기능 추가

--- src/main/java/baseball/GameResult.java
+++ src/main/java/baseball/GameResult.java
@@ -4,6 +4,17 @@ public class GameResult {
     int strike;
     int ball;
 
+    public void printResult() {
+        if (isUserWin()) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        } else if (isNothing()) {
+            System.out.println("낫싱");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+    }
+
     public boolean isUserWin() {
         return strike == 3;
     }
