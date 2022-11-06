01e1e2e5cab6b7babf02939b0c4c9cf12f17c325
Dan-HeeYoung9182 <ldan9182@gmail.com>
2022-11-06 18:49:36 +0900

    gameStart 메서드 생성, 시작 메세지 프린트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,11 @@ import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        gameStart();
+    }
+
+    public static void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public static List<Integer> createComputerNumbers() {
