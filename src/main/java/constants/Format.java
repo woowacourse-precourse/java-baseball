8aaad740d33e0ee6ce8b3f25e0237f77c9cf893a
YooSuYeon <usuyn000@gmail.com>
2022-11-07 04:47:25 +0900

    feat: 플레이어 입력 검사 정규표현식 상수로 정의

---
+++ src/main/java/constants/Format.java
@@ -0,0 +1,16 @@
+package constants;
+
+public enum Format {
+    PLAYER_NUMBER("^[1-9]{3}$"),
+    PLAYER_CHOICE("^[1-2]{1}*$");
+
+    private final String regex;
+
+    Format(String regex) {
+        this.regex = regex;
+    }
+
+    public String getRegex() {
+        return this.regex;
+    }
+}
