3274d463c2d05bdb23298ff23f7cea90bca6328f
HiiWee <wpdlzhf159@gmail.com>
2022-11-07 00:17:53 +0900

    Feat: 3글자를 입력하지 않으면 IllegalArgumentException 예외 발생 기능 추가

--- src/main/java/baseball/sevice/NumberBaseballService.java
+++ src/main/java/baseball/sevice/NumberBaseballService.java
@@ -5,10 +5,12 @@ import baseball.repository.NumberBaseballRepository;
 public class NumberBaseballService {
     private static final int ASCII_ONE = 49;
     private static final int ASCII_NINE = 57;
+    private static final int CORRECT_LENGTH = 3;
     private static final int FIRST_NUMBER = 0;
     private static final int SECOND_NUMBER = 1;
     private static final int THIRD_NUMBER = 2;
     public static final String NUMERIC_INPUT_ERROR = "1 ~ 9사이의 숫자만 입력해야 합니다.";
+    public static final String NUMERIC_LENGTH_ERROR = "3글자의 숫자를 입력해야 합니다.";
 
     private NumberBaseballRepository numberBaseballRepository = NumberBaseballRepository.getInstance();
 
@@ -26,9 +28,16 @@ public class NumberBaseballService {
         if (!checkNumber(userAnswer)) {
             throw new IllegalArgumentException(NUMERIC_INPUT_ERROR);
         }
+        if (!checkLength(userAnswer)) {
+            throw new IllegalArgumentException(NUMERIC_LENGTH_ERROR);
+        }
     }
 
-    public boolean checkNumber(String userAnswer) {
+    private boolean checkLength(String userAnswer) {
+        return userAnswer.length() == CORRECT_LENGTH;
+    }
+
+    private boolean checkNumber(String userAnswer) {
         if (userAnswer.charAt(FIRST_NUMBER) < ASCII_ONE
                 || userAnswer.charAt(FIRST_NUMBER) > ASCII_NINE) {
             return false;
