7ddc36cbf2417415f82df7322e1082d612407ea6
yeonhee <ydol2@naver.com>
2022-11-06 17:33:54 +0900

    refactor : 게임 출력 메시지 분리

---
+++ src/main/java/baseball/constant/GameMessage.java
@@ -0,0 +1,10 @@
+package baseball.constant;
+
+public class GameMessage {
+
+    public final static String NOTHING = "낫싱";
+    public final static String BALL = "볼 ";
+    public final static String STRIKE = "스트라이크";
+    public final static String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+}
