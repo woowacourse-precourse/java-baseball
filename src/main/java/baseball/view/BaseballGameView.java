c5934935582f0896035b7abd07358cd95c8321ef
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-06 17:18:15 +0900

    feat: BaseballGameView  - printInputNumber 메서드 추가
    입력 받을 숫자 메시지 출력

--- src/main/java/baseball/view/BaseballGameView.java
+++ src/main/java/baseball/view/BaseballGameView.java
@@ -31,4 +31,8 @@ public class BaseballGameView {
     public void printRestartMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+
+    public void printInputNumber() {
+        System.out.print("\n숫자를 입력해주세요 : ");
+    }
 }
