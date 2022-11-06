ff62fc971ebb9130691652b67508b99c815f2aef
이지혁 <olzlgur@naver.com>
2022-11-06 23:53:51 +0900

    feat: 숫자 판별 메서드 구현

--- src/main/java/baseball/number/Number.java
+++ src/main/java/baseball/number/Number.java
@@ -3,13 +3,20 @@ package baseball.number;
 public class Number {
     private String number;
 
-    public Number(String number) {
-        this.number = number;
+    public Number(String userNumber) {
+        this.number = userNumber;
     }
 
-    private static boolean validateLength(String number) {
-            return number.length() == 3;
+    private static boolean isValidateLength(String userNumber) {
+        return userNumber.length() == 3;
     }
 
-
+    private static boolean isDigit(String userNumber) {
+        for (char number : userNumber.toCharArray()) {
+            if (!Character.isDigit(number)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
