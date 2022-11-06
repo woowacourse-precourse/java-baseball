d4ecda8b88d25fc466f840306e3daeded902c8b5
kjg1514 <kjg1514@naver.com>
2022-11-07 05:01:03 +0900

    fix : List 위치 수정

--- src/main/java/baseball/ComputerNumber.java
+++ src/main/java/baseball/ComputerNumber.java
@@ -6,9 +6,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerNumber {
-    List<Integer> computer = new ArrayList<>();
 
-    public List<Integer> getComputerNumber() {
+
+    public static List<Integer> getComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
