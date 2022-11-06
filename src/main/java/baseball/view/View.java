df56a1bf73eb13903b5d631e6a52b4bd2ed5e147
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-06 11:49:02 +0900

    feat(view): 게임 시작 문구 출력하는 기능

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -7,6 +7,8 @@ public class View {
     private static String END_PHRASES = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static String COMPLETE_PHRASES = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
+    private static String START_PHRASES = "숫자 야구 게임을 시작합니다.";
+
     public static String inputView() {
         System.out.println(INPUT_PHRASES);
         return readLine();
@@ -15,6 +17,10 @@ public class View {
     public static String endView() {
         System.out.println(END_PHRASES);
         return readLine();
+    }
+
+    public static void startView() {
+        System.out.println(START_PHRASES);
     }
 
     public static void resultView(int strike, int ball) {
