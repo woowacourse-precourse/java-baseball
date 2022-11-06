0af60d5c6b5961c49f60f205900ed33f73499b62
JunHee <ljh19980823@gmail.com>
2022-11-06 13:35:52 +0900

    feat : Setter, Getter 생성

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -4,11 +4,36 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class User {
     private String userInput;
-    private boolean flag;
+    private boolean flag = false;
+    private Integer isProgramEnd = 1;
 
     public String getUserNumber() {
         System.out.print("숫자를 입력해 주세요 : ");
         String userInput = readLine();
         return userInput;
+    }
+
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
+    public void setFlag(boolean flag) {
+        this.flag = flag;
+    }
+
+    public void setIsProgramEnd(Integer isProgramEnd) {
+        this.isProgramEnd = isProgramEnd;
+    }
+
+    public String getUserInput() {
+        return userInput;
+    }
+
+    public boolean isFlag() {
+        return flag;
+    }
+
+    public Integer getIsProgramEnd() {
+        return isProgramEnd;
     }
 }
