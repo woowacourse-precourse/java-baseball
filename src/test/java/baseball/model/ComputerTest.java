119b909485fae1afaf47f9ef00951eab99e1cc77
win9 <krrr8@naver.com>
2022-11-06 19:18:20 +0900

    Test(computer): 입력에 대한 힌트 출력 테스트
    사용자에 대한 입력에 대하여 컴퓨터의 숫자를 비교하여
    올바른 힌트를 제공하는지 테스트

--- src/test/java/baseball/model/ComputerTest.java
+++ src/test/java/baseball/model/ComputerTest.java
@@ -1,13 +1,17 @@
 package baseball.model;
 
+import baseball.Application;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
-class ComputerTest {
+class ComputerTest extends NsTest {
     private Computer computer = new Computer();
     @Test
     public void 서로다른_3자리의_숫자(){
@@ -16,5 +20,21 @@ class ComputerTest {
         long distinctNumberCount = Arrays.stream(generatedNumbers).distinct().count();
 
         Assertions.assertEquals(distinctNumberCount, 3);
+    }
+
+    @Test
+    public void 힌트_출력(){
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "571", "175", "587", "2");
+                    assertThat(output()).contains("낫싱", "낫싱", "1볼 1스트라이크", "2볼", "3스트라이크", "게임 종료");
+                },
+                5, 8, 7
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
     }
 }
