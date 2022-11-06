9768f8e4c5c5e1be73eef7f0927abc2d10e33d7c
chordpli <chordplaylist@gmail.com>
2022-11-06 18:11:47 +0900

    feat(BasballGame): initAnswerNumber, checkOverlap 메소드 생성
    initAnswerNumber(): 컴퓨터 숫자 초기화
    checkOverlap(): 초기화시 숫자 중복 검사

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,9 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
-    
+
     // 애플리케이션 실행
-    public void run(){
+    public void run() {
         // 컴퓨터의 숫자 초기화
 
         // 사용자의 입력값 저장
@@ -11,5 +16,28 @@ public class BaseballGame {
         // 입력 값과 결과 비교
 
     }
+
+    // 컴퓨터의 숫자 초기화
+    public List<Integer> initAnswerNumber() {
+        List<Integer> answerNumber = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            answerNumber.add(i, Randoms.pickNumberInRange(1, 9));
+            if (!checkOverlap(answerNumber, i)) {
+                i--;
+            }
+        }
+        return answerNumber;
+    }
+
+    // 초기화시 중복 검사
+    public boolean checkOverlap(List<Integer> answerNumber, int i) {
+        for (int j = 0; j < i; j++) {
+            if (answerNumber.get(i) == answerNumber.get(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
 
 }
