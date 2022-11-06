9e9bd5a740b45279fc00ab800bae3de5bfc09f1e
yujaemin <woawoa0513@gmail.com>
2022-11-07 01:39:24 +0900

    refactor: 메소드명 변경
    기존 isContainOnlyDigit 메소드 containOnlyDigit으로 메소드명 변경

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -3,7 +3,7 @@ package baseball;
 public class Player {
     private static final int DESIRED_LENGTH = 3;
 
-    public boolean isContainOnlyDigit(String input) {
+    public boolean containOnlyDigit(String input) {
         for (Character character : input.toCharArray()) {
             if (!Character.isDigit(character)) {
                 return false;
@@ -15,4 +15,5 @@ public class Player {
     public boolean isCorrectLength(String input) {
         return input.length() == DESIRED_LENGTH;
     }
+
 }
