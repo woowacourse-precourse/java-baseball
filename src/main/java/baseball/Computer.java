1b6e4e730e695551c9850e190bcdfdf1b862d05c
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-06 22:39:08 +0900

    feat(Computer): 3자리 랜덤 숫자를 만들어주는 메소드 구현(기능목록1-2)

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,9 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
     private List<Integer> randomNumber;
 
     public Computer(){}
+
+    public List<Integer> createRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
