9afe850b1c48c73a06e59bfb9fa3eee20123f676
lvalentine6 <ysr1227@naver.com>
2022-11-07 04:32:56 +0900

    refactor : 사용자가 잘못된 값을 입력할 경우 예외처리 하는 기능 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,8 +2,12 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.text.NumberFormat;
+import java.text.ParsePosition;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
@@ -41,6 +45,62 @@ public class Application {
         String[] array = inputString.split("");
         for (int i = 0; i < array.length; i++) {
             userList.add(Integer.parseInt(array[i]));
+        }
+    }
+
+    // 사용자가 빈칸을 입력했을 때 예외 처리하는 메서드
+    static void isNull(String inputString) {
+        if (inputString.equals("")) {
+            throw new IllegalArgumentException("입력된 숫자가 없습니다.");
+        }
+    }
+
+    // 사용자가 3글자가 아닌 숫자를 입력했을 때 예외 처리하는 메서드
+    static void isValidSize(String inputString) {
+        if (inputString.length() != 3) {
+            throw new IllegalArgumentException("3자리 숫자를 입력해 주세요");
+        }
+    }
+
+    // 사용자가 숫자가 아닌 문자열을 입력했을 때 예외 처리하는 메서드
+    static void isValidNumber(String inputString) {
+        ParsePosition parsePosition = new ParsePosition(0);
+        NumberFormat.getInstance().parse(inputString, parsePosition);
+
+        if (inputString.length() != parsePosition.getIndex()) {
+            throw new IllegalArgumentException("숫자만 입력해 주세요");
+        }
+    }
+
+    // 사용자가 0이 포함된 숫자를 입력했을 때 예외 처리하는 메서드
+    static void isContainsZero(String inputString) {
+        if (inputString.contains("0")) {
+            throw new IllegalArgumentException("1부터 9 사이 숫자만 입력해 주세요");
+        }
+    }
+
+    // 사용자가 중복된 숫자를 입력했을 때 예외 처리하는 메서드
+    static void isOverLap(String inputString) {
+        Set<Integer> set = new HashSet<>();
+        String[] array = inputString.split("");
+
+        for (int i = 0; i < array.length; i++) {
+            set.add(Integer.parseInt(array[i]));
+        }
+
+        if (set.size() != 3) {
+            throw new IllegalArgumentException("서로 다른 숫자 3개를 입력해 주세요");
+        }
+    }
+
+    // 게임이 종료되고 사용자가 1이나 2을 입력하지 않았을 때 예외 처리하는 메서드
+    static boolean continueGame(String inputString) {
+        if (inputString.equals("1")) {
+            return true;
+        } else if (inputString.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException("1이나 2를 입력해 주세요");
         }
     }
 
