64c59f54e845d385ebc5c56d0bcdb57bc6aae463
hyejungg <kimhyejung12@naver.com>
2022-11-07 02:21:58 +0900

    feat: 입력 값 validate 길이 메소드 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -26,12 +26,14 @@ public class Player {
     public int gameOverInput() {
         System.out.println("축하합니다! 경기를 다시 시작하겠습니까? 다시 시작 : 1, 종료 : 2");
         String input = readLine();
+        validateInputLength(input.length(), 1);
         return Integer.parseInt(input);
     }
 
     public void setUserNumbers() {
         this.numbers.clear();
         String input = readLine();
+        validateInputLength(input.length(), 3);
         for (int i = 0; i < input.length(); i++) {
             int number = Character.getNumericValue(input.charAt(i));
             this.numbers.add(number);
@@ -42,6 +44,18 @@ public class Player {
         while (this.numbers.size() < 3) {
             int randomNumber = pickNumberInRange(1, 9);
             this.numbers.add(randomNumber);
+        }
+    }
+
+    private void validateInputLength(int length, int maxLength) {
+        if (length != maxLength) {
+            throw new IllegalArgumentException(String.format("입력값은 %s 자여야 합니다.", maxLength));
+        }
+    }
+
+    private void validateGameOverInput(int number) {
+        if (number < 1 || number > 2) {
+            throw new IllegalArgumentException("입력값은 1 또는 2 의 값이어야 합니다.");
         }
     }
 }
