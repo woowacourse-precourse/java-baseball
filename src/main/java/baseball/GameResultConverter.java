ac90b57a89debb44a62db9c3c5e900e36e037e2d
choiejae <ejae8319@gmail.com>
2022-11-06 21:59:17 +0900

    refactor: SRP 규칙에 따른 converter와 printer 각 class 분리

---
+++ src/main/java/baseball/GameResultConverter.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import java.util.List;
+
+public class GameResultConverter {
+    public String convertGameResult(List<Integer> gameResult) {
+        String converter = "";
+        if (isNothing(gameResult)) {
+            return "낫싱";
+        }
+        if (isOnlyBall(gameResult)) {
+            return gameResult.get(1) + " 볼";
+        }
+        if (isOnlyStrike(gameResult)) {
+            return gameResult.get(0) + " 스트라이크";
+        }
+        return gameResult.get(1) + " 볼 " + gameResult.get(0) + " 스트라이크";
+    }
+    private static boolean isNothing(List<Integer> gameResult) {
+        return gameResult.get(2) != 0;
+    }
+    private static boolean isOnlyBall(List<Integer> gameResult) {
+        return gameResult.get(1) != 0 && gameResult.get(0) == 0;
+    }
+    private static boolean isOnlyStrike(List<Integer> gameResult) {
+        return gameResult.get(0) != 0 && gameResult.get(1) == 0;
+    }
+}
--- src/test/java/baseball/GameResultPrinterTest.java
+++ src/test/java/baseball/GameResultPrinterTest.java
@@ -5,43 +5,50 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class GameResultPrinterTest {
-    private GameResultPrinter printer;
+class GameResultConverterTest {
+    private GameResultConverter converter;
 
     @BeforeEach
     void setUp() {
-        printer = new GameResultPrinter();
+        converter = new GameResultConverter();
     }
     @Order(1)
+    @DisplayName("Nothing 확인")
     @Test
     void NothingResultPrinter() {
-        assertEquals("낫싱", printer.convertGameResult(new ArrayList<>(Arrays.asList(0, 0, 1))));
+        assertEquals("낫싱", converter.convertGameResult(new ArrayList<>(Arrays.asList(0, 0, 1))));
     }
 
     @Order(2)
-    @Test
-    void StrikeResultPrinter() {
-        assertEquals("1 스트라이크", printer.convertGameResult(new ArrayList<>(Arrays.asList(1, 0, 0))));
-        assertEquals("2 스트라이크", printer.convertGameResult(new ArrayList<>(Arrays.asList(2, 0, 0))));
-        assertEquals("3 스트라이크", printer.convertGameResult(new ArrayList<>(Arrays.asList(3, 0, 0))));
-    }
-    @Order(3)
+    @DisplayName("Ball 확인")
     @Test
     void BallResultPrinter() {
-        assertEquals("1 볼", printer.convertGameResult(new ArrayList<>(Arrays.asList(0, 1, 0))));
-        assertEquals("2 볼", printer.convertGameResult(new ArrayList<>(Arrays.asList(0, 2, 0))));
-        assertEquals("3 볼", printer.convertGameResult(new ArrayList<>(Arrays.asList(0, 3, 0))));
+        assertEquals("1 볼", converter.convertGameResult(new ArrayList<>(Arrays.asList(0, 1, 0))));
+        assertEquals("2 볼", converter.convertGameResult(new ArrayList<>(Arrays.asList(0, 2, 0))));
+        assertEquals("3 볼", converter.convertGameResult(new ArrayList<>(Arrays.asList(0, 3, 0))));
     }
+
+    @Order(3)
+    @DisplayName("Strike 확인")
+    @Test
+    void StrikeResultPrinter() {
+        assertEquals("1 스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(1, 0, 0))));
+        assertEquals("2 스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(2, 0, 0))));
+        assertEquals("3 스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(3, 0, 0))));
+    }
+
     @Order(4)
+    @DisplayName("Ball + Strike 확인")
     @Test
     void BallStrikeResultPrinter() {
-        assertEquals("1 볼 1 스트라이크", printer.convertGameResult(new ArrayList<>(Arrays.asList(1, 1, 0))));
-        assertEquals("2 볼 1 스트라이크", printer.convertGameResult(new ArrayList<>(Arrays.asList(1, 2, 0))));
+        assertEquals("1 볼 1 스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(1, 1, 0))));
+        assertEquals("2 볼 1 스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(1, 2, 0))));
     }
 }
