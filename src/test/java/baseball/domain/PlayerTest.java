73e652e8f09b3cd8d01751c52f2ba14815ed43bf
khsrla9806 <kimhunsope@naver.com>
2022-11-06 18:59:41 +0900

    test(PlayerTest): 플레이어가 입력한 값이 정상적으로 저장되는 테스트 구현 입력에 대한 예외를 처리하기 전에 입력되는 값에 대한 테스트

---
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -0,0 +1,26 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import java.io.*;
+import java.util.Arrays;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayerTest {
+    @DisplayName("플레이어가 입력하는 값이 저장되는지 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"134", "167", "189", "331", "1234", "159"}) // 예외 처리 전
+    void EnterNumberOfPlayerTest(String inputData) {
+        // Given
+        Player player = new Player();
+        InputStream input = new ByteArrayInputStream(inputData.getBytes());
+        System.setIn(input);
+
+        // When
+        player.EnterNumberOfPlayer();
+
+        // Then
+        assertThat(player.playersNumber).isEqualTo(Arrays.asList(inputData.split("")));
+    }
+}
