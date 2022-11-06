8ef6f8b873a0baf2de9ba0992c774a970aa54805
prontand <keaseon@gmail.com>
2022-11-06 14:13:37 +0900

    Unexpected : in fix Bug : infinite loop (with working messages)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,11 +11,6 @@ public class Application {
         //5. 3스트라이크면 게임 종료 1(리셋)또는 2 입력
         //6. 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션은 종료되어야 한다.
 
-        //라이브러리
-        //  -camp.nextstep.edu.missionutils에서 제공하는 Randoms 및 Console API를 사용하여 구현해야 한다.
-        //    Random 값 추출은 camp.nextstep.edu.missionutils.Randoms의 pickNumberInRange()를 활용한다.
-        //  사용자가 입력하는 값은 camp.nextstep.edu.missionutils.Console의 readLine()을 활용한다.
-
 
     int[] numbers = new int[3];
     int index = 0;
@@ -27,6 +22,7 @@ public class Application {
         }
         if (!hasDuplicate){index++;}
     }
+
 //        for(int i=0; i<3; i++){
 //            int ii =numbers[i];
 //            System.out.print(ii);
@@ -42,10 +38,55 @@ public class Application {
 //        }
 
         int inputNum = Integer.parseInt(Console.readLine());
+//        int num1 = inputNum/100; int num2 = inputNum/10; int num3 = inputNum%10;
+//        int inputNumbers[] = new int[] {num1, num2, num3};
+        System.out.print(inputNum);
+        int inputNumbers[] = new int[] {inputNum/100, inputNum/10, inputNum%10};
+
+        int number1= numbers[0]; int number2 = numbers[1]; int number3= numbers[2];
+
+        while (true)
+        {int strikeCount = 0, ballCount = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if(numbers[i] == inputNumbers[j])
+                    {
+                        if (i == j){
+                            strikeCount++;
+                        }
+                        else
+                        {
+                            ballCount++;
+                        }
+                    }
+                }
+            }
+
+        if(ballCount > 0){
+            System.out.print(ballCount+'볼'+"");
+        }
+        if(ballCount > 0){
+            System.out.print(strikeCount+"스트라이크");
+        }
+        if(3-ballCount-strikeCount ==3){
+            System.out.print("낫싱");
+        }
 
 
+            if (strikeCount == 3)
+            {
+                System.out.print(strikeCount+"스트라이크"+ System.lineSeparator()+"3개의 숫자를 모두 맞히셨습니다! 게임종료"
+                        +System.lineSeparator()+"게임을 새로 시작하려면 1, 종료하려면 2를 하세요");
+
+                int finalNum = Integer.parseInt(Console.readLine());
+
+                if (finalNum == 2);
 
 
-
+            }
+        }
     }
 }
