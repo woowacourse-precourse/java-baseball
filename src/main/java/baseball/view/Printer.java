cca70d0bd1e7d7ef159cb560fea6affa69323d0d
Soseung Soo <sss9073@naver.com>
2022-11-06 19:23:50 +0900

    refactor: askRestart() to remove 'readline function'

--- src/main/java/baseball/view/Printer.java
+++ src/main/java/baseball/view/Printer.java
@@ -1,10 +1,8 @@
 package baseball.view;
 
-import camp.nextstep.edu.missionutils.Console;
-
 public class Printer {
 
-    public static void start(){
+    public static void gameStart(){
         String message = "숫자 야구 게임을 시작합니다.";
         System.out.println(message);
     }
@@ -19,11 +17,9 @@ public class Printer {
         System.out.println(message);
     }
 
-    public static String askRestart(){
+    public static void askRestart(){
         String message = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
         System.out.println(message);
-        String input = Console.readLine();
-        return input;
     }
 
 }
