2bf864ea4546b1420cd8ec816c090096ee969551
황민주 <minjoo@hwangminjooui-MacBookAir.local>
2022-11-06 16:28:02 +0900

    feat(Application): printMessage (src/main/java/baseball/Application.java)
    printMessage 메소드 추가:
    -메세지를 출력하기 위해 추가함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,9 @@
 package baseball;
 
 public class Application {
+    public static void printMessage(String message){
+        System.out.println(message);
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
