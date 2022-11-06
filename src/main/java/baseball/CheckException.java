e8c1b24ca6fc070a2cd9026ba22e5bcc90ae184a
rabbit0216 <rabbit1999k@gmail.com>
2022-11-07 00:49:18 +0900

    refactor: 숫자체크 함수 세분화, 범위체크 및 중복체크 함수로 나눔

--- src/main/java/baseball/CheckException.java
+++ src/main/java/baseball/CheckException.java
@@ -1,4 +1,5 @@
 package baseball;
+import java.util.*;
 
 public class CheckException {
     static final char ONE = 49;
@@ -11,12 +12,34 @@ public class CheckException {
 
     public static boolean checkNumber(String number){
         char[] numberArray = number.toCharArray();
+        boolean normalNumber = true;
+        if(!checkRange(numberArray)){
+            normalNumber = false;
+        }
+        if(!checkRedundant(numberArray)){
+            normalNumber = false;
+        }
+        return normalNumber;
+    }
+
+    public static boolean checkRange(char[] numberArray){
         boolean isNumber;
         for(char num : numberArray){
             isNumber = checkDigit(num);
             if(!isNumber){
                 return false;
             }
+        }
+        return true;
+    }
+
+    public static boolean checkRedundant(char[] numberArray){
+        String number = "";
+        for(char digit : numberArray){
+            if(number.contains(String.valueOf(digit))){
+                return false;
+            }
+            number += digit;
         }
         return true;
     }
@@ -32,10 +55,9 @@ public class CheckException {
         return isNumber;
     }
 
-    public static void check(String number){
-        //if(number == "1") Restart.
-        //if(number == "2")
 
+
+    public static void check(String number){
         if(checkNumberLength(number)){
             if(!checkNumber(number)){
                 throw new IllegalArgumentException();
