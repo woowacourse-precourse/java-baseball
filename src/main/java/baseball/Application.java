24fdce37ad5d67207354d8acfea8274b7f796b3d
Jinhui <jda0526@naver.com>
2022-11-06 21:18:05 +0900

    feat: 컴퓨터가 1에서 9까지 서로 다른 임의의 수 3개를 가지는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static List<Integer> computerRandomNumber;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        gameStart();
+    }
+
+    private static void gameStart() {
+        computerRandomNumber = computerGetRandomNumber();
+        System.out.println(computerRandomNumber);
+    }
+
+    private static List<Integer> computerGetRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
