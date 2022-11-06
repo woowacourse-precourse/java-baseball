b5ca81426069077c1d4d29f914af59037d00ce28
nuyh <hjaehyun25@gmail.com>
2022-11-06 20:51:41 +0900

    refactor(Computer): 랜덤 값 추출 Randoms.pickNumberInRange 사용

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -2,7 +2,9 @@ package baseball.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Computer {
@@ -20,7 +22,17 @@ public class Computer {
      * 도메인 로직
      */
     private String setAnswerByRandom() {
-        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        Set<Integer> included = new HashSet<>();
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
+
+        while (included.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!included.contains(randomNumber)) {
+                included.add(randomNumber);
+                randomNumbers.add(randomNumber);
+            }
+        }
 
         return randomNumbers.stream()
                 .map(String::valueOf)
