556c7ea091c92bdef2570293c956d3509bc69578
gusu9 <suhan.ku9@gmail.com>
2022-11-06 22:08:08 +0900

    Created Computer number choice

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+//        기능1. 컴퓨터 숫자선정
+
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            final int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+                System.out.print(randomNumber);
+            }
+        }
+
+
     }
 }
