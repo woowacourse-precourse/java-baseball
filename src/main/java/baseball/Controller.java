5e48aae218c2064323807e324cd5e6438cf7c858
tjddnr7760 <tjddnr7760@naver.com>
2022-11-06 16:57:48 +0900

    feat : Controller클래스 함수 설정

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,12 +1,23 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
 
 public class Controller {
     Controller() {}
 
     void start() {
+
+    }
+
+    String compareNumber(int humannumber, int computernumber) {
+
+    }
+
+    String compareEachNumber(int humanfirst, int humansecond, int humanthird, int computerfirst, int computersecond, int computerthird) {
+
+    }
+
+    int randomNumber() {
 
     }
 }
