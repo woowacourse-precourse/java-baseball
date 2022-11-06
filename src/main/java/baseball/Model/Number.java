b300e54b1ed18df9e6d67c1053bfa15672806188
aossuper7 <aossuper7@naver.com>
2022-11-06 15:23:56 +0900

    feat: 사용자가 입력한 숫자가 3자리가 맞는지 예외처리 기능

--- src/main/java/baseball/Model/Number.java
+++ src/main/java/baseball/Model/Number.java
@@ -10,10 +10,14 @@ public class Number {
 
     private static final int START_NUM = 1;
     private static final int END_NUM = 9;
+    private static final String INPUT_ERROR = "숫자를 잘못 입력하셨습니다. 게임 종료";
     private static final int MAX_LEN = 3;
     public static final char CHARZERO = '0';
 
+
     public List<Integer> createUserNumber(String number) {
+        checkLength(number);
+
         List<Integer> num = new ArrayList<>();
         for (char ch : number.toCharArray()) {
             num.add(ch - CHARZERO);
@@ -30,5 +34,12 @@ public class Number {
             }
         }
         return num;
+    }
+
+    private void checkLength(String number) {
+        if (number.length() == MAX_LEN) {
+            return;
+        }
+        throw new IllegalArgumentException(INPUT_ERROR);
     }
 }
