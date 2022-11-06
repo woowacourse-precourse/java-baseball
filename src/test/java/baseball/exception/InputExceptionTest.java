fcc1684dfbe5d715a11eb01eaebaace90fb34235
khsrla9806 <kimhunsope@naver.com>
2022-11-06 21:11:54 +0900

    test(InputExceptionTest): 입력 받을 때 예외가 발생되는지 확인하는 코드로 수정

--- src/test/java/baseball/exception/InputExceptionTest.java
+++ src/test/java/baseball/exception/InputExceptionTest.java
@@ -1,20 +1,27 @@
 package baseball.exception;
 
+import baseball.domain.Player;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
-import java.util.Arrays;
-import java.util.List;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputExceptionTest {
     @DisplayName("플레이어 입력 숫자 예외 테스트")
     @ParameterizedTest
-    @ValueSource(strings = {"113", "12", "가13", "ABC", "12354", ""})
+    @ValueSource(strings = {"113", "12", "가13", "ABC", "12354", "1", "가나다라"})
     void checkExceptionNumberTest(String inputData) {
-        List<String> playersNumber = Arrays.asList(inputData.split(""));
+        // Given
+        Player player = new Player();
 
-        assertThatThrownBy(() -> InputException.checkExceptionNumber(playersNumber))
+        // When
+        InputStream input = new ByteArrayInputStream(inputData.getBytes());
+        System.setIn(input);
+
+        // Then
+        assertThatThrownBy(() -> player.EnterNumberOfPlayer())
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }
