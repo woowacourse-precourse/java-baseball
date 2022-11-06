21c62c53945be561e16e81482df89e9964765f3e
shpark30 <seohyeonpark30.gmail.com>
2022-11-06 15:47:07 +0900

    feat: design game program
    with GameGuider, Referee, Hitter, Pitcher
    Program the baseball numbering game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import baseball.participants.GameGuider;
+import baseball.participants.Hitter;
+import baseball.participants.Pitcher;
+import baseball.participants.Referee;
+
 public class Application {
+    private static GameGuider guider = new GameGuider();
+    private static Referee referee = new Referee();
+    private static Pitcher pitcher = new Pitcher();
+    private static Hitter hitter = new Hitter();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameGuider.guideGameProcess();
+        referee.startGame();
+        hitter.swing();
+        while (referee.toTerminateGame) {
+            play1Game();
+            referee.playAgainOrNot();
+        }
+    }
+
+    private static void play1Game() {
+        while (referee.isGameInProgress) {
+            referee.startPitch();
+            pitcher.pitch();
+            referee.judge(pitcher, hitter);
+        }
+        referee.endGame();
     }
 }
