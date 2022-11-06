440db7a221776cf921e83be0b532fa9a44314eba
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-07 00:20:45 +0900

    fix<Application>: NumberBaseballGame 생성 수정
    - ShowGameMessage, ScoreBoard를 생성하여 NumberBaseballGame 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,15 @@
 package baseball;
 
+import baseball.domain.NumberBall;
 import baseball.domain.NumberBaseballGame;
+import baseball.domain.ScoreBoard;
+import baseball.util.ShowGameMessage;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        NumberBaseballGame game = new NumberBaseballGame();
+        NumberBaseballGame game = new NumberBaseballGame(new ShowGameMessage(),
+                new ScoreBoard(NumberBall.systemNumberBalls()));
         try {
             game.play();
         } catch (IllegalArgumentException e) {
