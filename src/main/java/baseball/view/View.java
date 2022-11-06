7f0a0404ed40e73eff9ae8ce54e72e72f1cbfbb3
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-06 10:58:01 +0900

    feat(view): 게임이 끝난 경우 입력 받는 기능

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -4,13 +4,26 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class View {
     private String INPUT_PHRASES = "숫자를 입력해주세요 : ";
+    private String END_PHRASES = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private String COMPLETE_PHRASES = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public String inputView() {
         System.out.println(INPUT_PHRASES);
         return readLine();
     }
 
-    public void endView() {
+    public String endView() {
+        System.out.println(END_PHRASES);
+        return readLine();
+    }
 
+    public void resultView(int strike, int ball) {
+        if(ball != 0) System.out.print(ball + "볼");
+        if(strike != 0) System.out.print(strike + "스트라이크");
+        System.out.println();
+    }
+
+    public void completeView(int length) {
+        System.out.println(length + COMPLETE_PHRASES);
     }
 }
