3299db9527d15acbc7573a7c4f4afa4a24977905
JerryK026 <kskyung0624@gmail.com>
2022-11-06 16:16:13 +0900

    Feat(Referee) : scoring 메서드를 구현한다
    테스트를 만족하는 socring 메서드를 구현한다. Game의 answer와 Player의 answer를 비교해서 채점하고 gameCount를 증가시킨다. 정답을 맞출 경우 gameCount를 3으로 만든다.

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,4 +1,48 @@
 package baseball;
 
+import java.util.List;
+
 public class Referee {
+    private int gameCount = 0;
+
+    public String scoring(List<Integer> answer, List<Integer> userAnswer) {
+        int ballCount = 0;
+        int strikeCount = 0;
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (answer.get(i) == userAnswer.get(j)) {
+                    if (i == j) {
+                        strikeCount++;
+                    }
+                    else if (i != j){
+                        ballCount++;
+                    }
+                }
+            }
+        }
+
+        StringBuilder decision = new StringBuilder();
+
+        if (ballCount != 0) {
+            decision.append(ballCount + "볼 ");
+        }
+
+        if (strikeCount != 0) {
+            decision.append(strikeCount + "스트라이크");
+        }
+
+        if (strikeCount == 0 && ballCount == 0) {
+            decision.append("낫싱");
+        }
+
+        gameCount++;
+
+        if (strikeCount == 3) {
+            decision.append("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            gameCount = 3;
+        }
+
+        return decision.toString().trim();
+    }
 }
