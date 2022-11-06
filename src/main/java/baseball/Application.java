564278ab4e4613e0caab2e839293e6cd4ee0f15b
Plastic_hong <101045399+plastichong@users.noreply.github.com>
2022-11-07 00:41:41 +0900

    feat(Application.java): 종료후 재시작 기능 추가 게임 종료 후, 다시 시작 할 수 있도록 runGame 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,20 +9,82 @@ import java.util.Set;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+
 //        String correctAnswer = makeRandomNumStr();
-        String correctAnswer = "123";
-        String userAnswer = Console.readLine();
+//        String correctAnswer = "589";
 
-        lengthCheck(userAnswer);
-        numberCheck(userAnswer);
-        duplicateCheck(userAnswer);
+        boolean run = true;
+        while (run) {
+            String correctAnswer = makeRandomNumStr();
 
-        int strikeCount = getStrikeCount(correctAnswer, userAnswer);
-        int ballCount = getBallCount(correctAnswer, userAnswer);
-        System.out.println(strikeCount+" strike!");
-        System.out.println(ballCount+" ball!");
+            runGame(correctAnswer);
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String next = Console.readLine();
+
+            if (next.equals("1")) {
+//                run = true;
+                continue;
+            } else if (next.equals("2")) {
+                System.out.println("종료합니다.");
+                run = false;
+            } else {
+                throw new IllegalArgumentException();
+            }
+
+
+
+        }
+    }
+
+    private static void runGame(String correctAnswer) {
+        int ballCount;
+        int strikeCount;
+
+        boolean run = true;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (run) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String userAnswer = Console.readLine();
+            System.out.println(userAnswer);
+
+//            입력값 검증
+            lengthCheck(userAnswer);
+            numberCheck(userAnswer);
+            duplicateCheck(userAnswer);
+
+//            계산
+            strikeCount = getStrikeCount(correctAnswer, userAnswer);
+            ballCount = getBallCount(correctAnswer, userAnswer);
+
+
+//            결과 출력
+            if (ballCount != 0 && strikeCount != 0) {
+                System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+            }
+            if (ballCount != 0 && strikeCount == 0) {
+                System.out.println(ballCount + "볼 ");
+            }
+            if (strikeCount != 0 && ballCount == 0) {
+                System.out.println(strikeCount + "스트라이크 ");
+            }
+            if (strikeCount == 0 && ballCount == 0) {
+                System.out.println("낫싱");
+            }
+
+
+            if (strikeCount == 3) {
+                System.out.print("3개의 숫자를 모두 맞히셨습니다! ");
+                System.out.println("게임 종료");
+                run = false;
+            }
+
+        }
 
     }
+
+
     private static int getBallCount(String correctAnswer, String userAnswer) {
 
         int ballCount = 0;
@@ -61,7 +123,7 @@ public class Application {
         char second = userAnswer.charAt(1);
         char third = userAnswer.charAt(2);
 
-        if(first==second||second==third||first==third){
+        if (first == second || second == third || first == third) {
             throw new IllegalArgumentException();
         }
     }
@@ -71,7 +133,7 @@ public class Application {
             char c = userAnswer.charAt(i);
             int numericValue = Character.getNumericValue(c);
 
-            if(numericValue<1||9<numericValue){
+            if (numericValue < 1 || 9 < numericValue) {
                 throw new IllegalArgumentException();
             }
 
@@ -81,12 +143,11 @@ public class Application {
 
     private static void lengthCheck(String userAnswer) {
 
-        if (userAnswer.length()!=3){
+        if (userAnswer.length() != 3) {
             throw new IllegalArgumentException();
         }
 
     }
-
 
 
     private static String makeRandomNumStr() {
@@ -104,9 +165,7 @@ public class Application {
             stringBuilder.append(s);
         }
 
-        String randomNumStr = String.valueOf(stringBuilder);
-
-        return randomNumStr;
+        return String.valueOf(stringBuilder);
     }
 
 }
