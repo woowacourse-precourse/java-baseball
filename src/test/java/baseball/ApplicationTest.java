d926e62f048666cd31e8edcbf8eb0087220be061
jsyeo97 <jsyeo97@naver.com>
2022-11-07 02:12:05 +0900

    test: 1~9 까지의 서로 다른 임의의 3자리 수 생성 기능 단위 테스트 구현

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,20 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static unit.BaseBallGame.makeRandomNumbers;
+import static baseball.Application.*;
+import static baseball.Balls.*;
+import static baseball.BallsNumbers.*;
+
+import java.util.ArrayList;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -32,4 +40,21 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+    
+    @Test
+	@DisplayName("randomNumbers가 서로 다른 3자리 수인지 테스트")
+	void randomNumbersDulicationTest() {
+		ArrayList<Integer> randomNumbers = makeRandomNumbers(); 
+		assertThat(randomNumbers.size() == randomNumbers.stream().distinct().count()).isTrue();
+		assertThat(randomNumbers.size() == 3).isTrue();
+	}
+
+    @Test
+	@DisplayName("randomNumbers가 1~9 사이의 수인지 테스트")
+	void randomNumbersRangeTest() {
+		ArrayList<Integer> randomNumbers = makeRandomNumbers(); 
+		for(int num : randomNumbers) {
+			assertThat(num < 10 &&  num > 0).isTrue();
+		}
+	}
 }
