0f2929295b6a8716399be4f1549c233333f038d8
aud19 <aud19980518@gmail.com>
2022-11-06 20:06:45 +0900

    controller의 startController 게임시작시 시작문 출력과 컴퓨터답을 정하는 메소드와 연결

---
+++ src/main/java/baseball/controller/StartController.java
@@ -0,0 +1,34 @@
+package baseball.controller;
+
+import baseball.domain.Again;
+import baseball.domain.Computer;
+import baseball.service.AgainValidator;
+import baseball.service.Implement;
+import baseball.view.StartEndGame;
+
+import java.io.IOException;
+
+public class StartController {
+    final StartEndGame startEndGame;
+    final Implement implement;
+    final Computer computer;
+
+    public StartController() {
+        this.startEndGame = new StartEndGame();
+        this.implement = new Implement();
+        this.computer = new Computer(implement.computerNumberDecide());
+    }
+
+    public Computer startController() throws IOException {
+        startEndGame.startGame();
+        return computer;
+    }
+
+    public int askOneMore() throws IOException {
+        startEndGame.endGame();
+        Again again = new Again(startEndGame.inputToQuestion());
+        AgainValidator againValidator = new AgainValidator();
+        againValidator.validate(again);
+        return again.getDoTry();
+    }
+}
