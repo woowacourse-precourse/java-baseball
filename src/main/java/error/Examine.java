fdca235a97fd4041f53ae27438bb516281bd0229
ho2n <jungho2n@gmail.com>
2022-11-06 13:44:09 +0900

    feat: 입력값이 세 자리가 맞는지 확인

--- src/main/java/error/Examine.java
+++ src/main/java/error/Examine.java
@@ -3,9 +3,15 @@ package error;
 public class Examine {
 	public static void hasDifferentNumbers(String str) {
 		try {
-
+			isLengthThree(str);
 		} catch (IllegalArgumentException e) {
 			throw e;
+		}
+	}
+
+	private static void isLengthThree(String str) {
+		if (str.length() != 3) {
+			throw new IllegalArgumentException();
 		}
 	}
 }
