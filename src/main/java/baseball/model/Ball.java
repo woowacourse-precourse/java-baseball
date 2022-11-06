6440b21989fff0b237302ca2ace703570261a8d9
dhkim <akdlzmf1123@gmail.com>
2022-11-06 11:55:21 +0900

    feat(Ball): 숫자 유효성 검사 기능 추가

--- src/main/java/baseball/model/Ball.java
+++ src/main/java/baseball/model/Ball.java
@@ -1,11 +1,20 @@
 package baseball.model;
 
 public class Ball {
+    public final int NUMBER_LOWER_BOUNDS = 1;
+    public final int NUMBER_UPPER_BOUNDS = 9;
     private final int number;
     private final int position;
     public Ball(int number, int position) {
         this.number = number;
         this.position = position;
+        validateNumber();
+    }
+
+    private void validateNumber() {
+        if (number < NUMBER_LOWER_BOUNDS || number > NUMBER_UPPER_BOUNDS) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public boolean isStrike(Ball other) {
