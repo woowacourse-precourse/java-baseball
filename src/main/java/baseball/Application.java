0971628d92318a6a4bf357845bce8d456f0001e4
JaeWoong Kim <kimje1109@gmail.com>
2022-11-07 00:47:45 +0900

    feat: Add getUserInput

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,17 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    public static String getUserInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+
+        return Console.readLine();
+    }
+
     public static List<Integer> getAnswer() {
         List<Integer> answer = new ArrayList<>();
 
