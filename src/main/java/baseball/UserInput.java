194f4632e6b24495005797bd368a778c5d2616d2
신채원 <cw121@naver.com>
2022-11-07 05:07:54 +0900

    feat(userinput): 유저의 입력숫자 저장 기능 구현

--- src/main/java/baseball/UserInput.java
+++ src/main/java/baseball/UserInput.java
@@ -5,10 +5,19 @@ import camp.nextstep.edu.missionutils.Console;
 public class UserInput {
 
     private final String USER_Input_Message = "숫자를 입력해주세요 :";
+    private final int NUMBER_LENGTH = 3;
+    private int[] expectedAnswer;
 
     public String inputNumber() {
         System.out.println(USER_Input_Message);
         String names = Console.readLine();
         return names;
+    }
+
+    public void setNumberOfUser() {
+        String userInput =inputNumber();
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            expectedAnswer[i] = userInput.charAt(i)-'0';
+        }
     }
 }
