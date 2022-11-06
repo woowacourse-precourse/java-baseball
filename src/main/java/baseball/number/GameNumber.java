17242a31160bde573813ad89880388ad952d9500
채희찬 <eqholic1125@gmail.com>
2022-11-06 23:15:29 +0900

    refactor: Number 클래스명 변경 및 기능 구현 목록 수정

--- src/main/java/baseball/number/GameNumber.java
+++ src/main/java/baseball/number/GameNumber.java
@@ -1,19 +1,15 @@
-package baseball;
+package baseball.number;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Number {
+public class GameNumber {
     private static final Integer NUMBER_LENGTH = 3;
     private String gameNumber;
 
-    private Number(String number) {
-        gameNumber = number;
-    }
-
-    private String generateRandomNumber() {
+    public void generateRandomNumber() {
         List<Integer> computerNumber = new ArrayList<>();
         while (computerNumber.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -22,7 +18,7 @@ public class Number {
             }
         }
 
-        return computerNumber.toString();
+        this.gameNumber = computerNumber.toString();
     }
 
 
