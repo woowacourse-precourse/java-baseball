6c9b900403e6f32ed0d11bc6d901504e6d6b8970
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-06 15:29:06 +0900

    feat: outputQuest 메소드 구현 및 테스트

---
+++ src/main/java/baseball/AfterGame.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class AfterGame {
+    public void outputQuest(){
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+    }
+}
---
+++ src/test/java/baseball/AfterGameTest.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class AfterGameTest {
+    private static AfterGame after;
+    @BeforeEach
+    public void beforeEach(){
+        after = new AfterGame();
+    }
+    @Test
+    public void outputQuestTest() throws Exception {
+        //given
+        String input = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        after.outputQuest();
+        //expect
+        assertThat(input).isEqualTo(out.toString().trim());
+    }
+
+}
