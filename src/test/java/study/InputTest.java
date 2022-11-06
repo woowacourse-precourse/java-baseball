be12dd2d0cc6e9d3116967098b95662b6f536adf
yoonjuho <dbswn990@naver.com>
2022-11-07 03:12:58 +0900

    test($InputTest) : 플레이어의 I/O 검증
    플레이어의 I/O 가 제대로 수행되는지 검증

---
+++ src/test/java/study/InputTest.java
@@ -0,0 +1,29 @@
+package study;
+
+import baseball.PlayerNum;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+public class InputTest {
+    private static PlayerNum playerNumber;
+    private static final String msg = "숫자를 입력해주세요 : ";
+
+    @BeforeAll
+    static void initAll() {
+        playerNumber = new PlayerNum();
+    }
+
+    @Test
+    void 사용자가_입력() {
+        String input = "123";
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        playerNumber.createNumber();
+        Assertions.assertThat(msg).isEqualTo(out.toString());
+    }
+}
