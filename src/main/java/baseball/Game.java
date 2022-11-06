b21ae13951f1c593b681a8de13ce23bab2685503
sysliar <sysliar@gmail.com>
2022-11-07 02:16:09 +0900

    feat(Game): game start
    Add game core logic
        - game start
        - game progress
        - game over
        - ask restart
        - restart
        -exit
    Add required constants
        - ask user input sentence
        - game over sentence
        - strike out number

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,11 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
 public class Game {
     private static final String START_STRING = "숫자 야구 게임을 시작합니다.";
+    private static final String NUMBER_INPUT_STRING = "숫자를 입력해주세요 : ";
+    private static final String GAME_OVER_STRING = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String ASK_RESTART_STRING = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String BALL_STRING = "볼";
     private static final String STRIKE_STRING = "스트라이크";
@@ -14,6 +18,7 @@ public class Game {
     private static final String EXIT_CODE = "2";
     private static final int BALL_INDEX = 0;
     private static final int STRIKE_INDEX = 1;
+    private static final int STRIKE_OUT = 3;
 
     private final Player computer;
     private final Player player;
@@ -28,10 +33,27 @@ public class Game {
     }
 
     public void start() {
+        Boolean exit = false;
         computer.setRandomNumber();
-        player.setNumber();
-        HashMap<Integer, Integer> compareBoard = new HashMap<>();
+        HashMap<Integer, Integer> compareBoard;
+        ArrayList<Integer> scoreBoard;
         compareBoard = getCompareBoard(computer);
+
+        while (!exit) {
+            System.out.print(NUMBER_INPUT_STRING);
+            player.inputNumbers();
+            scoreBoard = getScore(player, compareBoard);
+            printScoreBoard(scoreBoard);
+            if (scoreBoard.get(STRIKE_INDEX) == STRIKE_OUT) {
+                exit = true;
+                System.out.println(GAME_OVER_STRING);
+            }
+        }
+        if (askRestart()) {
+            start();
+        }
+    }
+
     private boolean askRestart() {
         System.out.println(ASK_RESTART_STRING);
         String restartCode = Console.readLine();
