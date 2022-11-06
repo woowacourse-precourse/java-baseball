886f57d3f7a97ce0f88b57c3d10f6ee63c7757fa
Park shinyeong <syngh503@gmail.com>
2022-11-07 00:12:26 +0900

    Feat(changePlayerInputToList): (기능3) 플레이어 입력 형변환 함수 구현
    플레이어가 세 자리수를 입력하였을 때, 이를 문자열에서 정수 리스트로 변환하는 changePlayerInputToList를 생성하였습니다.
    문자의 각 자리를 분리하여 리스트로 변환합니다.
    
    Relate to: #6

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,9 +1,22 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Player extends BaseballNumber{
 
+    @Override
     public boolean isValidBaseballNumber (String playerInput){
-        return true;
+        List<Integer> playerNumber = this.changePlayerInputToList(playerInput);
+
+    }
+
+    public List<Integer> changePlayerInputToList(String playerInput) {
+        return Stream.of(playerInput.split("")).map(Integer::parseInt).collect(Collectors.toList());
     }
 
 }
