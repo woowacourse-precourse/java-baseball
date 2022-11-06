7bde49cbc015a4811e870ecf9040b0aba6386cc7
BuildTools <unconfigured@null.spigotmc.org>
2022-11-06 17:19:18 +0900

    feat: 추가 replyBallAndStrike함수
    사용자 입력값에대한 볼과 스트라이크 응답함수

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -8,23 +8,43 @@ import java.util.List;
 public class Computer {
     private static final int BASEBALL_GAME_DIGIT = 3;
     private final List<Character> baseballNumber = new ArrayList<>();
+    private final CountBallAndStrike countBallAndStrike = new CountBallAndStrike();
 
     public List<Character> getBaseballNumber() {
         return baseballNumber;
     }
 
-    public void makeRandomNumber(){
+    public void makeRandomNumber() {
         while (baseballNumber.size() < BASEBALL_GAME_DIGIT) {
             addRandomNumber();
         }
     }
 
-    private void addRandomNumber(){
+    private void addRandomNumber() {
         char randomNumber = Character.forDigit((Randoms.pickNumberInRange(1, 9)), 10);
         if (!baseballNumber.contains(randomNumber)) {
             baseballNumber.add(randomNumber);
         }
     }
 
-
+    public void replyBallAndStrike(String input) {
+        countBallAndStrike.countBallAndStrike(baseballNumber, input);
+        int ball = countBallAndStrike.getBall();
+        int strike = countBallAndStrike.getStrike();
+        if ((ball == 0) && (strike == 0)) {
+            View.printNothing();
+        }
+        if ((ball == 0) && (strike == 3)) {
+            View.printCollect();
+        }
+        if ((ball == 0) && !(strike == 0)) {
+            View.printStrike(strike);
+        }
+        if (!(ball == 0) && (strike == 0)) {
+            View.printBall(ball);
+        }
+        if (!(ball == 0) && !(strike == 0)) {
+            View.printBallAndStrike(ball, strike);
+        }
+    }
 }
