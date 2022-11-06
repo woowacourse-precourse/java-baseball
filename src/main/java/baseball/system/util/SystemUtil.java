047270f5eb9fadf89f22fce6738d72904ee5f4c9
wonju-dev <wonjulee.dev@gmail.com>
2022-11-07 01:50:34 +0900

    feat: SytemUtil 구현
    SystemUtil은 Game과 관련이 없으나, 프로그램 실행과 관련있는 작업들을
    관리하는 클래스이다.
    SystemUtil은 싱글톤이다.
    getUserInput()은 사용자로부터 게임이 종료되었을 때의 다음 행동을 입력 받는다.
    이때, validator와 Condition을 통해 입력값을 검증한다.

---
+++ src/main/java/baseball/system/util/SystemUtil.java
@@ -0,0 +1,39 @@
+package baseball.system.util;
+
+import baseball.console.Interactive;
+import baseball.message.Message;
+import baseball.validation.condition.Condition;
+import baseball.validation.condition.systeminput.IsOperationCodeCond;
+import baseball.validation.Validator;
+import baseball.game.message.GameMessage;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class SystemUtil implements Interactive {
+
+    private static final SystemUtil instance = new SystemUtil();
+    private static final Validator validator = Validator.getInstance();
+
+    private SystemUtil() {
+    }
+
+    public static SystemUtil getInstance() {
+        return instance;
+    }
+
+    @Override
+    public Integer getUserInput() {
+        Integer input = Integer.parseInt(Console.readLine());
+
+        List<Condition> conditions = List.of(
+                IsOperationCodeCond.getInstance()
+        );
+
+        if (validator.isAnyConditionsFalse(input, conditions)) {
+            throw new IllegalArgumentException();
+        }
+
+        return input;
+    }
+}
