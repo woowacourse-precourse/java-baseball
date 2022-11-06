195f3bc14f75fb123fb16be8ad3623ccf2b4504c
ljh <dusk5588@naver.com>
2022-11-06 23:51:44 +0900

    feat: 서로 다른 숫자로 이루어진 3자리 난수 생성 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,24 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Application {
+    public static void makeRandomValue(List<Integer> computer){
+        computer.clear();
+        while (computer.size() < 3) {
+            int randomNumber = pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computer = new ArrayList<>();
+        makeRandomValue(computer);
     }
 }
