844982b1b0ae4cda1d83cd1edeb424d19a2e1105
saint6839 <saint6839@gmail.com>
2022-11-07 00:30:12 +0900

    refactor(model) : 문자열 숫자 변환 예외 검증 메서드를 Validator에 의존하지 않도록, 메서드 내에서 구현

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -5,20 +5,19 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
-import static baseball.utils.Validator.validateParseStringToInt;
-
 public class Player {
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String INVALID_INPUT_TYPE_MESSAGE = "숫자를 입력해주세요.";
 
     public Balls generatePlayerBalls() {
         System.out.print(INPUT_NUMBER_MESSAGE);
         String playerInput = Console.readLine();
 
-        validateParseStringToInt(playerInput);
         return new Balls(convertInputToBallList(playerInput));
     }
 
     private List<Integer> convertInputToBallList(String playerInput) {
+        validateParseStringToInt(playerInput);
         String[] inputSplits = playerInput.split("");
 
         List<Integer> playerBalls = new ArrayList<>();
@@ -26,5 +25,13 @@ public class Player {
             playerBalls.add(Integer.parseInt(inputSplit));
         }
         return playerBalls;
+    }
+
+    private void validateParseStringToInt(String playerInput) {
+        try {
+            Integer.parseInt(playerInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT_TYPE_MESSAGE);
+        }
     }
 }
