bb56c7a4e5c66337149b5a822efb59196525a722
yubin <dbqls2821@gmail.com>
2022-11-06 21:56:13 +0900

    chore(GameController): 재시작 및 종료에 대한 input을 체크하는 함수 수정

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,13 +1,16 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
+import baseball.io.MessagePrinter;
+import baseball.io.InputReader;
 
 public class GameController {
 
   BaseballGame game;
   MessagePrinter printer;
+  InputReader reader;
 
   private void init() {
+    reader = new InputReader();
     printer = new MessagePrinter();
     printer.printMessage(InGameMessages.GAME_START);
     game = new BaseballGame();
@@ -16,19 +19,15 @@ public class GameController {
   public void control() {
     init();
     do {
-      game.start();
-    } while (restartChecker());
+      game.play();
+    } while (checkRestart());
   }
 
-  private boolean restartChecker() {
+  private boolean checkRestart() {
     printer.printMessage(InGameMessages.GAME_RESTART_OR_END);
-    String input = Console.readLine();
-    if (input.equals("1")) {
-      return true;
-    } else if (input.equals("2")) {
-      return false;
-    } else {
-      throw new IllegalArgumentException();
-    }
+    String input = reader.readRestartOrEndInput();
+
+    // input을 입력받을때 validation을 끝냈기 떄문에 1인지 아닌지로만 판단
+    return input.equals("1");
   }
 }
