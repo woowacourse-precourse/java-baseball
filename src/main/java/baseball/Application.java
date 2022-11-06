594a51463334f03d4860b033a049755dcdc5e4d8
ysng_ysng <yeasung67@gmail.com>
2022-11-07 01:25:50 +0900

    refactor(Application): Change do-while condition's magic number
    Application 클래스의 main 메서드 안에 있는 do-while문의 조건 정보에서 magic number를 REPLAY로
    변경했습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,8 @@ import baseball.playerComputer.PlayerComputer;
 import baseball.playerUser.PlayerUser;
 
 public class Application {
+	private static final int REPLAY = 1;
+
 	public static void main(String[] args) {
 		PlayerComputer computer = PlayerComputer.getInstance();
 		PlayerUser user = PlayerUser.getInstance();
@@ -14,6 +16,6 @@ public class Application {
 		System.out.println("숫자 야구 게임을 시작합니다.");
 		do {
 			replayOrEndGame = game.play(computer, user);
-		} while (replayOrEndGame != 2);
+		} while (replayOrEndGame == REPLAY);
 	}
 }
