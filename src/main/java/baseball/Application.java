7656b4952e4f7cd5bd137900a4d0cf036a4659a1
pushedrumex <pushedrumex@naver.com>
2022-11-06 22:02:55 +0900

    refactor: add java convention

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,9 +14,11 @@ public class Application {
         Game.init();
     }
 }
+
 class Game {
     static final String INITGAME = "숫자 야구 게임을 시작합니다.";
     static final String ENDGAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
     static void init() {
         System.out.println(INITGAME);
         Hint hint = new Hint(makeThreeNums());
@@ -27,6 +29,7 @@ class Game {
         } while (!result.equals("3스트라이크"));
         end();
     }
+
     static List<Integer> makeThreeNums() {
         List<Integer> threeNums = new ArrayList<>();
         while (threeNums.size() < 3) {
@@ -37,25 +40,29 @@ class Game {
         }
         return threeNums;
     }
+
     static void end() {
         System.out.println(ENDGAME);
         User.getRestartInput();
     }
 }
+
 class User {
     final static String PROMPT = "숫자를 입력해주세요 : ";
     static final String RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     static List getGameInput() {
         System.out.print(PROMPT);
         String input = Console.readLine();
         List result;
         try {
             result = checkGameInput(input);
-        } catch(Exception e) {
+        } catch (Exception e) {
             throw new IllegalArgumentException();
         }
         return result;
     }
+
     static void getRestartInput() {
         System.out.println(RESTART);
         String input = Console.readLine();
@@ -67,41 +74,48 @@ class User {
             throw new IllegalArgumentException();
         }
     }
+
     static List checkGameInput(String input) throws Exception {
         List result = getGameInputFormat(input);
         checkGameInputLength(result);
         checkGameInputDuplicate(result);
         return result;
     }
+
     static void checkGameInputLength(List<Integer> input) throws Exception {
         if (input.size() != 3) {
             throw new Exception();
         }
     }
+
     static void checkGameInputDuplicate(List<Integer> input) throws Exception {
         HashMap<Integer, Boolean> map = new HashMap<>();
-        for (Integer num: input) {
+        for (Integer num : input) {
             if (map.containsKey(num)) {
                 throw new Exception();
             }
             map.put(num, true);
         }
     }
+
     static List<Integer> getGameInputFormat(String input) {
         String[] stringArr = input.split("");
-        int [] intArr = Arrays.stream(stringArr).mapToInt(Integer::parseInt).toArray();
+        int[] intArr = Arrays.stream(stringArr).mapToInt(Integer::parseInt).toArray();
         List result = Arrays.stream(intArr).boxed().collect(Collectors.toList());
         return result;
     }
 }
+
 class Hint {
     final static String BALL = "볼";
     final static String STRIKE = "스트라이크";
     final static String NOTHING = "낫싱";
     final List<Integer> answer;
+
     Hint(List answer) {
         this.answer = answer;
     }
+
     String get(List<Integer> input) {
         String result = "";
         int ball = countBall(input);
@@ -118,18 +132,20 @@ class Hint {
         }
         return result.trim();
     }
+
     int countStrike(List<Integer> input) {
         int result = 0;
-        for (Integer num: input) {
+        for (Integer num : input) {
             if (answer.contains(num) && answer.indexOf(num) == input.indexOf(num)) {
                 result += 1;
             }
         }
         return result;
     }
+
     int countBall(List<Integer> input) {
         int result = 0;
-        for (Integer num: input) {
+        for (Integer num : input) {
             if (answer.contains(num) && answer.indexOf(num) != input.indexOf(num)) {
                 result += 1;
             }
