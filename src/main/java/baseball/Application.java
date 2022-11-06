ff0162605905d6bd4cc072a2e27b20976308565d
Seiyon Park <seiyonpdev@gmail.com>
2022-11-06 23:05:39 +0900

    feat(Application): 메소드 play 수정
    플레이어에게 게임을 재시작할지 묻는 메소드 replay를 흡수하는 방식으로 수정했다

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -22,7 +22,8 @@ public class Application {
             System.out.println("숫자 야구 게임을 시작합니다.");
             String answer = randomNum();
             guess(answer);
-            continueFlag = replay();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            continueFlag = Console.readLine();
         } while (true);
     }
 
