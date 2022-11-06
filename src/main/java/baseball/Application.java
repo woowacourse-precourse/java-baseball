3a3b407773b48bda099a9ac27e632f9e9beb3356
ho2n <jungho2n@gmail.com>
2022-11-06 13:14:59 +0900

    feat: 숫자 야구 게임 접속

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import game.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        access();
+    }
+
+    private static void access() {
+        BaseballGame baseballGame = new BaseballGame();
     }
 }
