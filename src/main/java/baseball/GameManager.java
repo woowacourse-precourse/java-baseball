e8f0d4f0fa768ee1d3ddd347a79040ed2bf22a29
JungWoo <rlawjddn103@naver.com>
2022-11-07 00:35:16 +0900

    feat(GameManager): 게임 진행 함수 구현 및 재시작 여부 파악 기능 구현

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -8,4 +8,40 @@ public class GameManager {
     private static final String PLEASE_INPUT_NUMBER = "숫자를 입력해주세요 : ";
     private static final String END_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    private final ComputerManager computerManager;
+    private final InputNumber inputNumber;
+
+    public GameManager() {
+        this.computerManager = new ComputerManager();
+        this.inputNumber = new InputNumber();
+    }
+
+    public void startGame() {
+        System.out.println(START_GAME);
+
+        while (true) {
+            if (playBaseBall().equals("2")) {
+                break;
+            }
+        }
+    }
+
+    private String playBaseBall() {
+        computerManager.generateRandomNumber();
+
+        while (true) {
+            System.out.print(PLEASE_INPUT_NUMBER);
+            String number = inputNumber.inputGameNumber();
+            String compareResult = computerManager.compareWithUserNumber(number);
+
+            if (compareResult.equals("3스트라이크")) {
+                System.out.println(END_GAME);
+                System.out.println(RESTART_OR_END);
+                return inputNumber.inputRestartNumber();
+            }
+
+            System.out.println(compareResult);
+        }
+    }
 }
