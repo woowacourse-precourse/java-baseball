a2d5372586c684605a8da2d0ee0c5014a2594658
CMHyuk <alsgur9710@naver.com>
2022-11-06 15:23:59 +0900

    refactor: view(출력문) 분리, 스트라이크와 볼 횟수 체크 로직 수정

--- src/main/java/baseball/GameService.java
+++ src/main/java/baseball/GameService.java
@@ -2,13 +2,12 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.List;
+
+import static baseball.Const.QUIT_GAME;
+import static baseball.Const.RESTART_GAME;
+
 public class GameService {
-    private static final String STRIKE = "스트라이크";
-    private static final String BALL = "볼";
-    private static final String NOTHING = "낫싱";
-    private static final String RESTART_GAME = "1";
-    private static final String QUIT_GAME = "2";
-    private static final int THREE_STRIKE = 3;
 
     private final InputBallNumber inputBallNumber;
     private final RandomBallNumber randomBallNumber;
@@ -19,106 +18,29 @@ public class GameService {
     }
 
     public Result checkStrikeAndBall() {
-        String randomNumber = randomBallNumber.createRandomNumber();
+        List<String> randomNumbers = randomBallNumber.createRandomNumber();
         String inputNumber = inputBallNumber.inputBallNumber();
 
         int strike = 0;
         int ball = 0;
         for (int ballNumber = 0; ballNumber < inputNumber.length(); ballNumber++) {
+            String randomNumber = randomNumbers.get(ballNumber);
             if (isEqualNumber(randomNumber, inputNumber, ballNumber)) {
                 strike++;
                 continue;
             }
-            if (existNumber(randomNumber, inputNumber, ballNumber)) {
+            if (existNumber(randomNumber, inputNumber)) {
                 ball++;
             }
         }
         return new Result(strike, ball);
     }
 
-    private boolean existNumber(String randomNumber, String inputNumber, int ballNumber) {
-        return randomNumber.contains(inputNumber.substring(ballNumber, ballNumber + 1));
+    private boolean existNumber(String randomNumber, String inputNumber) {
+        return inputNumber.contains(randomNumber);
     }
 
     private boolean isEqualNumber(String randomNumber, String inputNumber, int ballNumber) {
-        return inputNumber.charAt(ballNumber) == randomNumber.charAt(ballNumber);
-    }
-
-    public void printResult(Result result) {
-        int strike = result.getStrike();
-        int ball = result.getBall();
-
-        isThreeStrike(strike);
-        isStrikeAndBall(strike, ball);
-        isOnlyBall(strike, ball);
-        isOnlyStrike(strike, ball);
-        isNothing(strike, ball);
-    }
-
-    public void reenterOrEndGame(Result result) {
-        int strike = result.getStrike();
-        if (strike == THREE_STRIKE) {
-            restartOrQuitGame();
-        }
-        if (strike != THREE_STRIKE) {
-            playGame();
-        }
-    }
-
-    public void playGame() {
-        Result strikeAndBall = checkStrikeAndBall();
-        printResult(strikeAndBall);
-        reenterOrEndGame(strikeAndBall);
-    }
-
-    private void isOnlyStrike(int strike, int ball) {
-        if (strike < 3 && strike > 0 && ball == 0) {
-            System.out.println(strike + STRIKE);
-        }
-    }
-
-    private void isNothing(int strike, int ball) {
-        if (strike == 0 && ball == 0) {
-            System.out.println(NOTHING);
-        }
-    }
-
-    private void isOnlyBall(int strike, int ball) {
-        if (strike == 0 && ball != 0) {
-            System.out.println(ball + BALL);
-        }
-    }
-
-    private void isStrikeAndBall(int strike, int ball) {
-        if (strike != 0 && ball != 0) {
-            System.out.println(ball + BALL + " " + strike + STRIKE);
-        }
-    }
-
-    private void isThreeStrike(int strike) {
-        if (strike == 3) {
-            System.out.println(strike + STRIKE);
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        }
-    }
-
-    private void restartOrQuitGame() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String num = Console.readLine();
-        isNotRestartedAndQuitedGame(num);
-        isRestartedGame(num);
-    }
-
-    private void isRestartedGame(String num) {
-        if (num.equals(RESTART_GAME)) {
-            randomBallNumber.clearRandomNumber();
-            playGame();
-        }
-    }
-
-    private void isNotRestartedAndQuitedGame(String num) {
-        if (!num.equals(RESTART_GAME) && !num.equals(QUIT_GAME)) {
-            throw new IllegalArgumentException("1 또는 2를 입력해주세요");
-        }
+        return inputNumber.charAt(ballNumber) == randomNumber.charAt(0);
     }
 }
