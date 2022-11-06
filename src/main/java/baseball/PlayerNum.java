f7a6709b7f743d6a5737f996a559b904c2bde5f9
yoonjuho <dbswn990@naver.com>
2022-11-07 03:45:54 +0900

    feat($PlayerNum) : 입력이 숫자인지 검증
    플레이어의 입력이 숫자로만 구성되어 있지 않은 경우 IllegalArgumentException를 발생시킨다

--- src/main/java/baseball/PlayerNum.java
+++ src/main/java/baseball/PlayerNum.java
@@ -22,4 +22,15 @@ public class PlayerNum implements Number {
         return number;
     }
 
+    public void verifyInput(String input){
+        char arr[] = input.toCharArray();
+        for(int i =0; i < arr.length; i++){
+            if(Character.isDigit(arr[i]) == true){
+                continue;
+            }else{
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
 }
