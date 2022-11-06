0c8acaa7fc05d084e4bf7a310ec37483bb0b5fc3
jaehyeon <jhjh0022@naver.com>
2022-11-06 17:29:43 +0900

    refactor: 힌트 프린터해주는 메서드 분리

--- src/main/java/baseball/print/OutputStatement.java
+++ src/main/java/baseball/print/OutputStatement.java
@@ -6,31 +6,44 @@ public class OutputStatement {
     }
 
     public static void requestNumberInput(){
-        System.out.print(("숫자를 입력해주세요 : "));
-
+        System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public static void notSing(){
+    public static void printNotSing(){
         System.out.println("낫싱");
     }
 
-    public static void hint(int ball, int strike) {
+    public static void printHint(int ball, int strike) {
         if (ball == 0 && strike == 0) {
-            notSing();
+            printNotSing();
         } else if (ball == 0) {
-            System.out.println(strike + "스트라이크");
+            printStrikeCntNumber(strike);
         } else if (strike == 0) {
-            System.out.println(ball + "볼");
+            printBallCntNumber(ball);
         } else {
-            System.out.println(ball + "볼" + " " + strike + "스트라이크");
+            printBallAndStrikeCntNumber(ball, strike);
         }
     }
+
     public static void informResultAnswer(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     public static void askGameRestartOrFinish(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
     }
+
+    private static void printBallCntNumber(int ball){
+        System.out.println(ball+"볼");
+    }
+
+    private static void printStrikeCntNumber(int strike){
+        System.out.println(strike + "스트라이크");
+    }
+
+    private static void printBallAndStrikeCntNumber(int ball, int strike){
+        System.out.println(ball + "볼" + " " + strike + "스트라이크");
+    }
+
+
 }
