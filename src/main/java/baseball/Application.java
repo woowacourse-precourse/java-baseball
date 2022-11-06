910a6973d06c5569074545cdad19efcf8470c587
Jinhui <jda0526@naver.com>
2022-11-07 01:36:42 +0900

    feat: 게임 플레이어가 서로 다른 임의의 수 3개를 입력하면 잘못된 값인지 확인하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,12 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
     static List<Integer> computerRandomNumber;
@@ -16,8 +19,19 @@ public class Application {
     private static void gameStart() {
         computerRandomNumber = computerGetRandomNumber();
         System.out.println(computerRandomNumber);
-    }
 
+        boolean isNumberAvailable = playerInputNumber();
+        System.out.println("playerInputNumber 의 isNumberAvailable : " + isNumberAvailable);
+        // 입력한 값이 잘못된 값이면 종료함
+        if(!isNumberAvailable){
+            return;
+        }
+
+        // 결과값을 출력
+        System.out.println("결과값 출력 예정");
+
+        // 1이면 다시 시작, 2이면 종료료
+    }
     private static List<Integer> computerGetRandomNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
@@ -27,5 +41,72 @@ public class Application {
             }
         }
         return computer;
+    }
+
+    private static boolean playerInputNumber() {
+        String input = Console.readLine();
+        boolean checkNumber = false;
+
+        if(isInteger(input)){
+            checkNumber = isLengthThree(input);
+            System.out.println("isLengthThree 의 checkNumber : " + checkNumber);
+            checkNumber = isNumberDifferent(checkNumber, input);
+            System.out.println("isNumberDifferent 의 checkNumber : " + checkNumber);
+        }
+        return checkNumber;
+    }
+
+    private static boolean isInteger(String input) {
+        try{
+            Integer.valueOf(input);
+            return true;
+        }
+        catch (IllegalArgumentException e){
+            System.out.println("숫자가 아닌 잘못된 값을 입력했습니다. 게임을 종료합니다.");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private static boolean isLengthThree(String input) {
+        boolean checkLength = true;
+        try{
+            if(input.length() != 3){
+                checkLength = false;
+                System.out.println("숫자의 길이가 3이 아닌 잘못된 값을 입력했습니다. 게임을 종료합니다.");
+                throw new IllegalArgumentException();
+            }
+        }
+        catch (IllegalArgumentException e){
+            e.printStackTrace();
+        }
+        return checkLength;
+    }
+
+    private static boolean isNumberDifferent(boolean middleCheck, String input) {
+        Set<Character> set = new LinkedHashSet<>();
+        for (int i = 0; i < input.length(); i++) {
+            set.add(input.charAt(i));
+        }
+
+        if(middleCheck){
+            middleCheck = isRepeat(set);
+        }
+        return middleCheck;
+    }
+
+    private static boolean isRepeat(Set<Character> set) {
+        boolean checkRepeat = true;
+        try{
+            if(set.size() != 3){
+                checkRepeat = false;
+                System.out.println("서로 다른 숫자가 아닌 잘못된 값을 입력했습니다. 게임을 종료합니다.");
+                throw new IllegalArgumentException();
+            }
+        }
+        catch (IllegalArgumentException e){
+            e.printStackTrace();
+        }
+        return checkRepeat;
     }
 }
