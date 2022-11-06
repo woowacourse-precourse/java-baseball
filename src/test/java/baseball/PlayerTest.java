dad94a5c48f63edfb6af533ae2817d7b8e974544
Park shinyeong <syngh503@gmail.com>
2022-11-06 15:33:43 +0900

    Test(PlayerTest): 사용자 입력 예외 처리 함수 테스트 생성
    사용자 입력에 대한 예외처리에 대한 테스트를 생성하였습니다.
    사용자 입력에 대해 예외 처리가 필요한 경우 InputMusmatchException 객체인지 확인하였습니다.
    이 부분은 각 예외처리에 대한 사용자 정의 Exception을 생성하고 수정해주어야 할 것 같습니다.
    
    Closes: #10

---
+++ src/test/java/baseball/PlayerTest.java
@@ -0,0 +1,71 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.InputMismatchException;
+import static org.assertj.core.api.Assertions.*;
+
+public class PlayerTest {
+    @Nested
+    class TestIsValidBaseballNumber {
+        Player player = new Player();
+
+        @Test
+        @DisplayName("사용자의 입력에 대한 예외처리 테스트")
+        void case1() {
+            String playerInput = "315";
+            boolean result = player.isValidBaseballNumber(playerInput);
+            assertThat(result).isTrue();
+        }
+
+        @Test
+        @DisplayName("3개 이상의 수를 입력한 경우 ")
+        void case2() {
+            String playerInput = "31895";
+            assertThatThrownBy(() -> player.isValidBaseballNumber(playerInput))
+                    .isInstanceOf(InputMismatchException.class)
+                    .hasMessageContaining("3자리 수를 입력해주세요");
+        }
+
+
+        @Test
+        @DisplayName("3개 이하의 수를 입력한 경우 ")
+        void case3() {
+            String playerInput = "35";
+            assertThatThrownBy(() -> player.isValidBaseballNumber(playerInput))
+                    .isInstanceOf(InputMismatchException.class)
+                    .hasMessageContaining("3자리 수를 입력해주세요");
+
+        }
+
+        @Test
+        @DisplayName("숫자가 아닌 문자를 입력한 경우")
+        void case4() {
+            String playerInput = "숫자말고문자";
+            assertThatThrownBy(() -> player.isValidBaseballNumber(playerInput))
+                    .isInstanceOf( InputMismatchException.class)
+                    .hasMessageContaining("3자리 수를 입력해주세요");
+        }
+
+        @Test
+        @DisplayName("중복된 숫자를 입력한 경우")
+        void case5() {
+            String playerInput = "114";
+            assertThatThrownBy(() -> player.isValidBaseballNumber(playerInput))
+                    .isInstanceOf(InputMismatchException.class)
+                    .hasMessageContaining("중복된 수를 입력할 수 없습니다");
+        }
+
+        @Test
+        @DisplayName("0이 포함된 경우")
+        void case6() {
+            String playerInput = "104";
+            assertThatThrownBy(() -> player.isValidBaseballNumber(playerInput))
+                    .isInstanceOf(InputMismatchException.class)
+                    .hasMessageContaining("1~9까지의 수를 입력해주세요");
+        }
+    }
+
+}
