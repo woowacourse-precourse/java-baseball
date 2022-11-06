d265d749b604f6fd5b2f7c5f988b500decf6c402
gyuturn <rbtjrrbals@gmail.com>
2022-11-06 22:50:14 +0900

    feat($view): 3개의 숫자 맞춘경우 게임종료문구 출력
    D-1-4-1
    출력문구
    3개의 숫자를 모두 맞히셨습니다! 게임 종료
    게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.

--- src/main/java/baseball/view/PrintConsole.java
+++ src/main/java/baseball/view/PrintConsole.java
@@ -12,6 +12,11 @@ public class PrintConsole {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
+    public static void endGame(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
     public static String printResult(Computer resultComputer) {
         String printBall = printBall(resultComputer);
         String printStrike = printStrike(resultComputer);
