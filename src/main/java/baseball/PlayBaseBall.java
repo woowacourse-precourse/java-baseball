b247757b433702bde1240a1e1cb35f5112f033ed
saewoo1 <wooozooo1123@gmail.com>
2022-11-06 16:22:52 +0900

    refactor(PlayBaseBall): delete strike reassign value in if 게임 재시작 시 strike 초기화 값 제거

--- src/main/java/baseball/PlayBaseBall.java
+++ src/main/java/baseball/PlayBaseBall.java
@@ -4,12 +4,11 @@ public class PlayBaseBall {
   public static void playGame() {
     PrintOut.startMessage();
     String computer = CreateRandomNumber.numberMaker();
-    int strike;
     while (true)
     {
       PrintOut.inputMessage();
       String user = UserScanner.input();
-      strike = Hint.strikeCount(computer, user);
+      int strike = Hint.strikeCount(computer, user);
       if (Exception.isValidNumber(user)) {
         PrintOut.scoreMessage(Hint.ballAndStrike(computer, user));
       }
