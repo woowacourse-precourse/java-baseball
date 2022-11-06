17714ca6ac387608e8e8efe2eaf5de71263add75
wonju-dev <wonjulee.dev@gmail.com>
2022-11-07 01:56:34 +0900

    feat: 게임이 종료되었을 때 사용자 입력 검증 조건 구현
    IsOperationCodeCond는 Condtion 인터페이스 구현체이다.
    IsOperationCodeCond의 isTrue메소는 사용자 입력이 1, 2에 해당하는지 검증한다.

---
+++ src/main/java/baseball/validation/condition/IsOperationCodeCond.java
@@ -0,0 +1,21 @@
+package baseball.validation.condition;
+
+import java.util.List;
+
+public class IsOperationCodeCond implements Condition {
+
+    private static final Condition instance = new IsOperationCodeCond();
+    private static final List<Integer> OPERATION_CODE = List.of(1, 2);
+
+    private IsOperationCodeCond() {
+    }
+
+    @Override
+    public Boolean isTrue(Integer input) {
+        return OPERATION_CODE.contains(input);
+    }
+
+    public static Condition getInstance() {
+        return instance;
+    }
+}
