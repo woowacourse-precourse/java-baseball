be69cc634465eab3d5d05280ec2ab5088e2ba4c0
SeongukBaek <bsu6235@gmail.com>
2022-11-06 21:43:07 +0900

    feat: implement TODO 1
    Use while loop for Game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,19 +8,27 @@ public class Application {
     private static final String OPTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String END_GAME = "숫자 야구 게임을 종료합니다.";
 
+    private static Status status;
+
     public static void main(String[] args) throws IllegalArgumentException {
         // TODO: 프로그램 구현
 
-        // TODO: 1. Status가 EXIT가 아닐 동안 반복되는 반복문 안에서 게임 진행
+        status = Status.PROCEEDING;
+        try {
+            // TODO: 1. Status가 EXIT가 아닐 동안 반복되는 반복문 안에서 게임 진행
+            while (status != Status.EXIT) {
+                // TODO: 2. 게임 진행을 위한 초기화
 
-        // TODO: 2. Computer 생성-> 게임용 숫자도 생성
+                // TODO: 3. User 생성, User의 숫자 입력 -> 유효성 검사를 자체적으로 시행, 유효하지 않다면 예외 발생 후 종료
 
-        // TODO: 3. User 생성, User의 숫자 입력 -> 유효성 검사를 자체적으로 시행, 유효하지 않다면 예외 발생 후 종료
+                // TODO: 4. Computer와 User의 숫자를 가지고 게임 결과 생성
 
-        // TODO: 4. Computer와 User의 숫자를 가지고 게임 결과 생성
+                // TODO: 5. 게임 결과에 따라 사용자 입력을 받거나, 3으로 이동
 
-        // TODO: 5. 게임 결과에 따라 사용자 입력을 받거나, 3으로 이동
-
-        // TODO: 6. 사용자 입력을 받아 게임 추가 진행 여부 확인, 추가 진행 시 2번으로 이동, 종료 시 Status를 EXIT로 변경
+                // TODO: 6. 사용자 입력을 받아 게임 추가 진행 여부 확인, 추가 진행 시 2번으로 이동, 종료 시 Status를 EXIT로 변경
+            }
+        } catch (IllegalArgumentException exception) {
+            throw new IllegalArgumentException();
+        }
     }
 }
