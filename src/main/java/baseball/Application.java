32ead350c289c792df3fc24428dd22fef20d55c2
SimSimEEE <rebe123@naver.com>
2022-11-07 08:17:20 +0900

    feat(gameover.class): add function to validate user input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import baseball.end.GameOver;
 import baseball.hint.Hint;
 import baseball.inputdata.Player;
 import baseball.quiz.QuizMaker;
@@ -12,20 +13,30 @@ public class Application {
     private static final String MESSAGE_TO_START = "숫자 야구 게임을 시작합니다.";
     private static final String MESSAGE_TO_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final int LENGTH_OF_QUIZ = 3;
+    private static final String INPUT_OF_CONTINUE = "1";
 
     public static void main(String[] args) {
-        String quiz = QuizMaker.NewQuiz();
+        String quiz = "";
+
         String playerInput = "";
+        String gameoverInput = "1";
         List<Integer> hintList = Arrays.asList(0);
 
         System.out.println(MESSAGE_TO_START);
-        while(hintList.get(0) != LENGTH_OF_QUIZ) {
+        while(gameoverInput.equals(INPUT_OF_CONTINUE)) {
+            quiz = QuizMaker.NewQuiz();
+
             playerInput = Player.Input();
             Player.ExceptionProcess(playerInput);
 
             hintList = Hint.CompareTo(quiz, playerInput);
             Hint.Print(hintList);
+
+            if(hintList.get(0) == LENGTH_OF_QUIZ){
+                System.out.println(MESSAGE_TO_END);
+                gameoverInput = GameOver.input();
+                GameOver.ExceptionProcess(gameoverInput);
+            }
         }
-        System.out.println(MESSAGE_TO_END);
     }
 }
--- src/main/java/baseball/end/GameOver.java
+++ src/main/java/baseball/end/GameOver.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class GameOver {
 
-    private static final String INPUT_OF_CONTINUE = "0";
-    private static final String INPUT_OF_END = "1";
+    private static final int VALID_LENGTH_OF_USER_INPUT = 1;
+    private static final String VALID_PLAYER_INPUT_PATTERN = "^[1-2]";
     private static final String MESSAGE_TO_CONTINUE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public static String input() {
@@ -16,5 +16,13 @@ public class GameOver {
         inputData = sc.next();
 
         return inputData;
+    }
+
+    public static void ExceptionProcess(String inputData) {
+        if(inputData.length() != VALID_LENGTH_OF_USER_INPUT)
+            throw new IllegalArgumentException();
+
+        if(!inputData.matches(VALID_PLAYER_INPUT_PATTERN))
+            throw new IllegalArgumentException();
     }
 }
