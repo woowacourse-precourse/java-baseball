464313eba6fbc6b7a841ae7650d70082edf59e83
solesy <tkddbs9801@gmail.com>
2022-11-06 20:15:54 +0900

    feat(main): check that the user input value is valid
    Add a private function in Application to check if the string is numeric.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private boolean isNumber(String str){
+        if(str == null || str.isEmpty())
+            return false;
+        long numOfDigit = str.chars()
+            .mapToObj(ch -> (char)ch)
+            .filter(Character::isDigit)
+            .count();
+        if(numOfDigit == str.length())
+            return true;
+        return false;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
