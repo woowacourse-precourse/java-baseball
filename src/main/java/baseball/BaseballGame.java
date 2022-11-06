248f06e5fba4c3eace4b14d7247c145e5b8d30f6
yeonhee <ydol2@naver.com>
2022-11-06 15:53:15 +0900

    feat : 게임 재시작 여부 입력받고 재시작하는 함수 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 import static baseball.ComputerNumber.createComputerNumber;
@@ -11,6 +13,15 @@ public class BaseballGame {
         List<Integer> computerNumbers = createComputerNumber();
         while (true) {
             List<Integer> userNumbers = checkInputUserNumber();
+        }
+    }
+
+    public static void restartGame(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String restartNumber = Console.readLine();
+
+        if (restartNumber == "1") {
+            playBaseballGame();
         }
     }
 
