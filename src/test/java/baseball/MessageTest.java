8a6d20d624872edbc93624276108d0b02010e8dd
JJongmen <pjy90123@gmail.com>
2022-11-06 22:04:52 +0900

    test: 게임 시작 문구를 출력하는 기능 테스트 코드 작성

---
+++ src/test/java/baseball/MessageTest.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MessageTest {
+    final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    final PrintStream standardOut = System.out;
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.setOut(standardOut);
+    }
+
+    @Test
+    public void 게임_시작_문구를_출력하는_기능() {
+        Message.GAME_START.print();
+
+        assertEquals("숫자 야구 게임을 시작합니다.", outputStreamCaptor.toString().trim());
+    }
+}
