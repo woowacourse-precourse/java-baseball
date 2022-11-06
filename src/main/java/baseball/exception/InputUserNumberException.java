cf99912dcd0851ae8f3058ccd1025a9bfd535f48
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-06 16:55:47 +0900

    feat: InputUserNumberException - checkInputNotOverlapNumber 메서드 추가
    중복이 있는지 체크

--- src/main/java/baseball/exception/InputUserNumberException.java
+++ src/main/java/baseball/exception/InputUserNumberException.java
@@ -1,5 +1,8 @@
 package baseball.exception;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 public class InputUserNumberException {
 
@@ -9,6 +12,16 @@ public class InputUserNumberException {
     private void checkInputSizeThreeNumber(String userNumber) {
         if (userNumber.length() != PROPER_LENGTH) {
             throw new IllegalArgumentException("3자리의 수를 입력해주세요.");
+        }
+    }
+
+    private void checkInputNotOverlapNumber(String userNumber) {
+        Set<Integer> userNumberSet = new HashSet<>();
+        for (int index = 0; index < userNumber.length(); index++) {
+            userNumberSet.add(userNumber.charAt(index) - '0');
+        }
+        if (userNumberSet.size() != PROPER_LENGTH) {
+            throw new IllegalArgumentException("숫자에 중복이 있으면 안됩니다.");
         }
     }
 
