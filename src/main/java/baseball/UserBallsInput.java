64033a35cbd8172a6baa021009616620e9c4c34e
choiejae <ejae8319@gmail.com>
2022-11-06 16:44:08 +0900

    refactor: Optimizing import

--- src/main/java/baseball/UserBallsInput.java
+++ src/main/java/baseball/UserBallsInput.java
@@ -1,45 +1,34 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class UserBallsInput {
-
     private int maxLen;
-
     public UserBallsInput() {
         this(3);
     }
-
     public UserBallsInput(int maxLen) {
         this.maxLen = maxLen;
     }
-
     public int getBallsLength() {
         return maxLen;
     }
-
     public String inputNumber() {
         String buffer = Console.readLine();
         return buffer;
     }
-
     public String inputNumberTest(String input) {
         String buffer = input;
         validityChecker(buffer);
         return buffer;
     }
-
     public void validityChecker(String input) {
         validLength(input);
         validElement(input);
         validContinuity(input);
     }
-
     public void validLength(String input) {
         if (input.length() != maxLen) {
             throw new IllegalArgumentException("input의 길이가 유효하지 않습니다");
