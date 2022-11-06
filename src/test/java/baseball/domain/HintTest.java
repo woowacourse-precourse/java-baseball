7ca7e803529e9ded961ed9a8d8a47dbeb841eb51
K-jun98 <alap_u@naver.com>
2022-11-07 01:01:02 +0900

    test -Hint 초기화 테스트

---
+++ src/test/java/baseball/domain/HintTest.java
@@ -0,0 +1,30 @@
+package baseball.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
+
+class HintTest {
+
+    Hint hint = new Hint();
+
+    @BeforeEach
+    void init() {
+        hint.init();
+    }
+
+    @ParameterizedTest
+    @DisplayName("스코어 포인트 초기화 테스트")
+    @EnumSource(names = {"STRIKE", "BALL"})
+    void initTest(Score score) throws Exception {
+        assertThat(hint.getScorePoint(score)).isEqualTo(0);
+    }
+}
+
