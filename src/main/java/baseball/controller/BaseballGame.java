ed51c73ec2fc312416a4ea5917d9cf98aa12cd05
Cyma-s <ssol0319@naver.com>
2022-11-07 01:43:15 +0900

    feat(BaseballGame): add method about restart & end logic

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -6,21 +6,27 @@ import baseball.model.player.Computer;
 import baseball.view.BaseballGameView;
 
 public class BaseballGame {
+    private static final int END_CODE_RESTART = 1;
+    private static final int END_CODE_END = 2;
     private final BaseballGameView gameView;
     private final Computer computer;
+    private boolean isEnd = false;
 
     BaseballGame() {
         gameView = new BaseballGameView();
         computer = new Computer();
     }
 
-    private void init() {
+    private void startBaseballGame() {
         computer.createRandomNumbers();
         gameView.startBaseballGame();
-        while(true) {
+        while(!isEnd) {
             BaseballNumbers userBaseballNumbers = getUserInput();
             Hint userHint = makeHint(userBaseballNumbers);
-            checkUserInputCorrect(userHint);
+            boolean isCorrectAnswer = checkUserInputCorrect(userHint);
+            if(isCorrectAnswer){
+                readyToEndGame();
+            }
         }
     }
 
@@ -37,10 +43,18 @@ public class BaseballGame {
         return userInputHint;
     }
 
-    private void checkUserInputCorrect(Hint userInputHint) {
+    private boolean checkUserInputCorrect(Hint userInputHint) {
         boolean isCorrect = userInputHint.isCorrectNumber();
-        if(isCorrect) {
-            gameView.endBaseballGame();
+        return isCorrect;
+    }
+
+    private void readyToEndGame() {
+        int endCode = gameView.endBaseballGame();
+        if(endCode == END_CODE_RESTART) {
+            computer.createRandomNumbers();
+        }
+        if(endCode == END_CODE_END) {
+            this.isEnd = true;
         }
     }
 }
