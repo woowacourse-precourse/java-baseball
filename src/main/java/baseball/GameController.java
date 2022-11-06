dfeac895646859a7543828213054e5e5e69df1a1
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-06 20:56:01 +0900

    refactor(GameController): 객체 지향 코드를 위한 리팩터링

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -5,12 +5,16 @@ import baseball.playing.PlayGame;
 public class GameController {
 
     public GameController() {
-        PlayGame playGame = new PlayGame();
 
-        playGame.startGame();
-        playGame.playingGame();
-        playGame.endGame();
-        playGame.replayGame();
+        PlayGame playGame = new PlayGame();
+        boolean replay = true;
+
+        while (replay) {
+            playGame.startGame();
+            playGame.playingGame();
+            playGame.endGame();
+            replay = playGame.replayGame();
+        }
     }
 
 }
