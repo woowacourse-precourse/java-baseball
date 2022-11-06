c00c2929684960ded913f06a4583379ec0ea50ce
ljh <dusk5588@naver.com>
2022-11-07 01:48:58 +0900

    fix: 한글 오타 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -60,7 +60,7 @@ public class Application {
 
     public static int askRestartOrGameOver(){
         System.out.println("3스트라이크");
-        System.out.println("3개의 숫자를 모두 맞히셨스니다! 게임 종료");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = readLine();
         checkRestartInput(input);
