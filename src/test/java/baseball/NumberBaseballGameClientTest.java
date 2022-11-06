7089b1fe7087d7408fb6c191e257e31e21c3165d
DevJay1024 <devjay1024@gmail.com>
2022-11-07 02:47:45 +0900

    test: 게임 시작 메시지 출력 기능 테스트 케이스 작성

--- src/test/java/baseball/NumberBaseballGameClientTest.java
+++ src/test/java/baseball/NumberBaseballGameClientTest.java
@@ -1,11 +1,13 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-
+import org.junit.jupiter.api.Test;
 
 class NumberBaseballGameClientTest {
 
@@ -27,5 +29,11 @@ class NumberBaseballGameClientTest {
 
     protected final String output() {
         return captor.toString().trim();
+    }
+
+    @Test
+    void 게임_시작_메시지는_출력스트림에_정상_저장된다() {
+        NumberBaseballGameClient.showGameStartMessage();
+        assertThat(output()).isEqualTo("숫자 야구 게임을 시작합니다.");
     }
 }
