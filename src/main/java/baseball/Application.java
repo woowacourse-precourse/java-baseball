fc0ce2bc1d8604447f9a7b832d2240e5a6353560
CJ <choijun0627@naver.com>
2022-11-07 01:14:55 +0900

    <feat>(Application.java): 기능 요구 사항 1번 구현
    기능 요구 사항 1번  전체 게임 함수인startGame()을 Application.java에구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    public static void startGame() throws IllegalArgumentException {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(true) {
+
+            game();
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+            String userInput=Console.readLine();
+
+            if(userInput.equals("1")) {
+                continue;
+            }
+            else if(userInput.equals("2")) {
+                break;
+            }
+            else {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        startGame();
     }
 }
