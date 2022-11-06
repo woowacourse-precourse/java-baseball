ee19cd633ec39c395e7c2a93a3db9ff6790d20b8
CMHyuk <alsgur9710@naver.com>
2022-11-06 15:29:13 +0900

    feat: 공통으로 사용되는 상수 클래스

---
+++ src/main/java/baseball/Const.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class Const {
+    public static final String RESTART_GAME = "1";
+    public static final String QUIT_GAME = "2";
+    public static final int THREE_STRIKE = 3;
+}
