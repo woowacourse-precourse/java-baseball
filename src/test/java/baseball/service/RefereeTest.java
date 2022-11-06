6c390e94af4c237b7b0fa93a783ed47510e1ded3
saint6839 <saint6839@gmail.com>
2022-11-07 02:46:47 +0900

    feat(modelTest) : 컴퓨터가 생성한 값과 플레이어가 입력한 값을 비교해 결과 값을 반환하는 테스트 작성

---
+++ src/test/java/baseball/service/RefereeTest.java
@@ -0,0 +1,33 @@
+package baseball.service;
+
+import baseball.model.dto.Result;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RefereeTest {
+
+    @Test
+    void generateResult_컴퓨터의_값과_플레이어의_값이_2볼_1스트라이크일_경우_결과가_잘_반환되는지_테스트() {
+        //given
+        Result result = new Result();
+        List<Integer> computerValues = List.of(1, 2, 3);
+        List<Integer> playerValues = List.of(1, 3, 2);
+        //when
+        for (int i = 0; i < computerValues.size(); i++) {
+            if (Objects.equals(computerValues.get(i), playerValues.get(i))) {
+                result.increaseStrikeCount();
+                continue;
+            }
+            if (computerValues.contains(playerValues.get(i))) {
+                result.increaseBallCount();
+            }
+        }
+        //then
+        assertThat(result.getBallCount()).isEqualTo(2);
+        assertThat(result.getStrikeCount()).isEqualTo(1);
+    }
+}
