c40ebc1a7cbfec62d202e9b149ba7a3fa42c0fff
Yoobi <quddlr96@naver.com>
2022-11-06 21:31:43 +0900

    feat: add function to choose restart or application exit after the game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,6 +8,20 @@ import java.util.HashSet;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            String randomNumber = generateRandomNumber();
+            playGame(randomNumber);
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String command = Console.readLine();
+            if (command.equals("1")) {
+                continue;
+            } else if (command.equals("2")) {
+                break;
+            } else {
+                throw new IllegalArgumentException("잘못된 입력 값 입니다.");
+            }
+        }
     }
 
     public static String generateRandomNumber() {
