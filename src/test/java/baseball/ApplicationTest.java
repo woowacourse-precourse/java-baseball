65fff17f8342ecf01f4787b99bad21389b711e69
sujin0607 <goweg0607@gmail.com>
2022-11-06 20:42:46 +0900

    feat: 난수생성에 대한 예외사항 테스트 메서드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,14 +1,23 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
+    private BaseBallGame baseBallGame = new BaseBallGame();
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -28,8 +37,30 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Nested
+    class RandomNumbersTest {
+        @Test
+        void 난수_생성시_중복여부_확인() {
+            List<Integer> randomNums = baseBallGame.randomNumbers();
+            Set<Integer> setRandomNums = new HashSet<>(randomNums);
+
+            assertEquals(randomNums.size(), setRandomNums.size());
+        }
+
+        @Test
+        void 난수_범위가_1_9_인지() {
+            List<Integer> randomNums = baseBallGame.randomNumbers();
+            assertThat(randomNums.stream().allMatch(randomNum -> randomNum >= 1 && randomNum <= 9)).isTrue();
+
+        }
+    }
+
+
+
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
 }
+
