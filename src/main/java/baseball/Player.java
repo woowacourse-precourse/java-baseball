7cc36a9fbe88d99127e58accb7237f0db8fda4ab
sysliar <sysliar@gmail.com>
2022-11-06 17:59:50 +0900

    feat(Player): check expected input
    Constraint
        - number between 1 and 9
        - three digits
        - not duplicated
    
    In other cases, throws IllegalArgumentException

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -3,6 +3,8 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Player {
     private static final int NUMBER_LENGTH = 3;
@@ -18,5 +20,20 @@ public class Player {
                 number.add(randomNumber);
             }
         }
+    }
+    public boolean checkNumbers(ArrayList<Integer> listOfNumbers) {
+        if (listOfNumbers.size() != NUMBER_LENGTH) {
+            throw new IllegalArgumentException("세자리 수가 아닙니다.");
+        }
+        Set<Integer> setOfNumbers = new HashSet<Integer>(listOfNumbers);
+        if (setOfNumbers.size() != NUMBER_LENGTH) {
+            throw new IllegalArgumentException("중복된 수를 입력하셨습니다.");
+        }
+        for (int i = 0; i < NUMBER_LENGTH; i++) {
+            if (listOfNumbers.get(i) < NUMBER_RANGE_START || NUMBER_RANGE_END < listOfNumbers.get(i)) {
+                throw new IllegalArgumentException("범위 밖의 수를 입력하셨습니다.");
+            }
+        }
+        return true;
     }
 }
