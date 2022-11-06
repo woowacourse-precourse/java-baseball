861413bbac66e7e8b75d795fc4d1a6f4317db675
sangwon <swchoi1997@naver.com>
2022-11-06 15:28:04 +0900

    refactor: 볼 출력 시 오류 수정
    1볼, 2볼, 3볼 처럼 볼만 있는경우 뒤에 띄어쓰기 하나가 더 있어 출력과 형식이 맞지않는 현상 수정

--- src/main/java/baseball/domain/view/OutputData.java
+++ src/main/java/baseball/domain/view/OutputData.java
@@ -7,50 +7,66 @@ public class OutputData {
 
     public static final String THREE_STRIKE_WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
+    private static String output = "";
+
     private OutputData() {
     }
+
 
     public static void gameResultPrint(ResultDto resultDto) {
         Map<String, Integer> result = resultDto.getResult();
         for (String ballCount : result.keySet()) {
             checkBallCount(ballCount, result.get(ballCount));
         }
+        printOutput();
+    }
+
+    private static void printOutput() {
+        System.out.print(output);
         System.out.println();
+        output = "";
     }
 
     private static void checkBallCount(String ballCount, int count) {
         if (ballCount.equals("아웃")) {
-            printOutCount(count);
-            return;
+            output += printOutCount(count);
         }
         if (ballCount.equals("볼")) {
-            printBallCount(ballCount, count);
+            output += printBallCount(ballCount, count);
         }
         if (ballCount.equals("스트라이크")) {
-            printStrikeCount(ballCount, count);
+            output += printStrikeCount(ballCount, count);
         }
     }
 
-    private static void printOutCount(int count) {
+    private static String printOutCount(int count) {
         if (count == 3) {
-            System.out.print("낫싱");
+            return String.valueOf("낫싱");
         }
+        return "";
     }
 
-    private static void printBallCount(String ballCount, int count) {
+    private static String printBallCount(String ballCount, int count) {
         if (count != 0) {
-            System.out.print(count + ballCount + " ");
+            return (String.valueOf(count) + ballCount);
         }
+        return "";
     }
 
-    private static void printStrikeCount(String ballCount, int count) {
+    private static String printStrikeCount(String ballCount, int count) {
         if (count == 3) {
-            System.out.println(count + ballCount);
-            System.out.print(THREE_STRIKE_WIN_MESSAGE);
-            return;
+            return String.valueOf(count) + ballCount + "\n" + THREE_STRIKE_WIN_MESSAGE;
         }
         if (count != 0) {
-            System.out.print(count + ballCount);
+            return checkJustStrike(ballCount, count);
         }
+        return "";
+    }
+
+    private static String checkJustStrike(String ballCount, int count) {
+        if (output.equals("")) {
+            return String.valueOf(count) + ballCount;
+        }
+        return " " + String.valueOf(count) + ballCount;
     }
 }
