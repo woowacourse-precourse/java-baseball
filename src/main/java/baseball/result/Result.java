8af4dd96a13e65c90437e7d269fb35823a0b2273
이지혁 <olzlgur@naver.com>
2022-11-06 21:36:12 +0900

    refactor: 스트라이크 카운트 메서드 접근 제한자 수정

--- src/main/java/baseball/result/Result.java
+++ src/main/java/baseball/result/Result.java
@@ -9,11 +9,34 @@ public class Result {
         this.ball = 0;
     }
 
+    private void countStrike(String randomNumber, String userNumber) {
+        for (int index = 0; index < randomNumber.length(); index++) {
+            if (randomNumber.charAt(index) == userNumber.charAt(index)) {
+                strike++;
+            }
+        }
+    }
+
+    public void countBall(String randomNumber, String userNumber){
+
+    }
+
+    public void countBall() {
+
+    }
+
     public int getStrike() {
         return strike;
     }
 
     public int getBall() {
         return ball;
+    }
+
+    public boolean istNothing() {
+        if (strike == 0 && ball == 0) {
+            return true;
+        }
+        return false;
     }
 }
