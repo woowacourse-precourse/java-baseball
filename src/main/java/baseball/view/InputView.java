4e157f6a85b4352165ee77cc6116ec93139ae227
dclxxi <dclxxi@kakao.com>
2022-11-06 18:39:29 +0900

    refactor: INPUT_STATE_NUMBER의 숫자를 enum으로 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,10 +1,12 @@
 package baseball.view;
 
+import baseball.State;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_PLAYER_NUMBER = "숫자를 입력해주세요 : ";
-    private static final String INPUT_STATE_NUMBER = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String INPUT_STATE_NUMBER = "게임을 새로 시작하려면 " + State.SHUTDOWN.getNumber()
+            + ", 종료하려면 " + State.SHUTDOWN.getNumber() + "를 입력하세요.";
 
     public static String inputPlayerNumber() {
         System.out.print(INPUT_PLAYER_NUMBER);
