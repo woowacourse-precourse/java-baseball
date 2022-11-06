adcae028f83a9e2b4e5172c7b7184dad8b03a254
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-06 17:15:27 +0900

    feat: BaseballGameView  - printStrikeCount 메서드 추가
    스트라이크 수를 출력, 0이면 출력하지않음

--- src/main/java/baseball/view/BaseballGameView.java
+++ src/main/java/baseball/view/BaseballGameView.java
@@ -5,4 +5,10 @@ public class BaseballGameView {
     public void printStartGame() {
         System.out.print("숫자 야구 게임을 시작합니다.");
     }
+
+    public void printStrikeCount(int strikeCount) {
+        if (strikeCount != 0) {
+            System.out.printf("%d스트라이크", strikeCount);
+        }
+    }
 }
