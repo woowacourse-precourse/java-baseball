a7162ace6b007c5468c8612b9c0c528c4595857d
jagajang <kosb7544@gmail.com>
2022-11-06 23:01:00 +0900

    fix: divide compare string method

--- src/main/java/baseball/AnswerCompareManager.java
+++ src/main/java/baseball/AnswerCompareManager.java
@@ -1,13 +1,17 @@
 package baseball;
 
 public class AnswerCompareManager {
-    public boolean compareAnswer(int answer, int input) {
-        int ballNum = 0, strikeNum = 0;
-        
-        String answerStr = String.valueOf(answer);
-        String inputStr = String.valueOf(input);
+    private int ballNum = 0, strikeNum = 0;
 
-        // 별도 메서드로 분리
+    public boolean compareAnswer(int answer, int input) {
+        compareStr(String.valueOf(answer), String.valueOf(input));
+
+        IOManager.getHint(ballNum, strikeNum);
+
+        return strikeNum == GameConfig.ANSWER_LEN;
+    }
+
+    private void compareStr(String answerStr, String inputStr) {
         for(int idx = 0; idx < GameConfig.ANSWER_LEN; idx++) {
             char digit = inputStr.charAt(idx);
             String digitStr = inputStr.substring(idx, idx+1);
@@ -17,9 +21,5 @@ public class AnswerCompareManager {
             else if(answerStr.contains(String.valueOf(digitStr)))
                 ballNum++;
         }
-
-        IOManager.getHint(ballNum, strikeNum);
-
-        return strikeNum == GameConfig.ANSWER_LEN;
     }
 }
