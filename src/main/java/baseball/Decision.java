50ac0d31ef0fbdebe86d8f08baa73e0a3a31a6da
은지 <midzy0228@gmail.com>
2022-11-06 16:12:01 +0900

    feat: 판정에 따라 메시지 출력

---
+++ src/main/java/baseball/Decision.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import java.util.HashMap;
+
+import static baseball.Constant.*;
+
+public class Decision {
+    public static void call() {
+        HashMap<String, Integer> result = Score.getTotal();
+        if (result.get(STRIKE)==3) {
+            PrintMessage.onlyStrikeCount();
+            PrintMessage.gameEnd();
+            return;
+        }
+        if (result.get(STRIKE) != 0 && result.get(BALL) != 0) {
+            PrintMessage.ballAndStrikeCount();
+            return;
+        };
+        if (result.get(STRIKE) == 0 && result.get(BALL) != 0) {
+            PrintMessage.onlyBallCount();
+            return;
+        }
+        if (result.get(STRIKE) != 0 && result.get(BALL) == 0) {
+            PrintMessage.onlyStrikeCount();
+            return;
+        }
+        PrintMessage.notThing();
+    }
+
+
+}
