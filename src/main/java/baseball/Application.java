92689a35fe39cf3aca88ee0ffd3c61d8b7df39fe
whrod <whrodtube@gmail.com>
2022-11-06 13:35:29 +0900

    Feat : inputNumberOfUser method create
    유저(사용자)의 Console.readLine()으로 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println(generateNumbers());
+        System.out.println(inputNumberOfUser());
+//        if (inputNumberOfUser() == generateNumbers()) {
+//            System.out.println("right");
+//        } else {
+//        }
+//        System.out.println("wrong");
     }
 
     public static List generateNumbers() {
@@ -20,4 +28,18 @@ public class Application {
         }
         return computer;
     }
+
+    public static List inputNumberOfUser() {
+        String input = Console.readLine();
+        int inputNumber = Integer.parseInt(input);
+            if((int)(Math.log10(inputNumber)+1) == 3) {
+                System.out.println("숫자를 입력해주세요: " + input);
+                return Collections.singletonList(input);
+            } else
+                throw new IllegalArgumentException("잘못된 입력값입니다.");
+    }
 }
+
+//    public static String checkNumbers() {
+//
+//    }
