3b8aa9f89bbd6d3b224f79b7d5436e32be834364
vcho1958 <gusckd57@gmail.com>
2022-11-07 05:01:12 +0900

    docs(core): write suedo code

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,10 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //야구 게임 시작
+        //do while 문 시작
+            //랜덤 수 생성
+            //3 스트라이크 전까지 while 문
+            //숫자를 입력받고 검증한 뒤 결과 반환하는 메소드
     }
 }
