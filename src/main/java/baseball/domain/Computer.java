017c3e328d516895fe3a363e95b93f4c89910c52
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-06 16:42:44 +0900

    feat: Computer - setRandomNumber 메서드 추가
    랜덤값을 생성하는 메서드

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,10 +1,21 @@
 package baseball.domain;
 
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
     private static List<Integer> Number;
 
+    public void setRandomNumber() {
+        Number = new ArrayList<>();
+        while (Number.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!Number.contains(randomNumber)) {
+                Number.add(randomNumber);
+            }
+        }
+    }
 }
