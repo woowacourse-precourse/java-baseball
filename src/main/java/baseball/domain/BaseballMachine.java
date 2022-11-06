f4e545340b2aa7b8da0a8bdf469d70a734593ca2
cocomozila <joseyun94@naver.com>
2022-11-06 15:27:38 +0900

    style: 클래스 BaseballMachine으로 이름변경

--- src/main/java/baseball/domain/BaseballMachine.java
+++ src/main/java/baseball/domain/BaseballMachine.java
@@ -2,22 +2,20 @@ package baseball.domain;
 
 import baseball.util.Input;
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class StrikeCounter {
+public class BaseballMachine {
 
     private Computer computer;
     private Input input;
     private User user;
 
-    public StrikeCounter() {
+    public BaseballMachine() {
         this.computer = new Computer();
         this.input = new Input();
     }
 
     public void progressGame() {
         createUserNumber();
+        user.getUserNumberList();
     }
 
     // 유저 일급컬렉션 생성
