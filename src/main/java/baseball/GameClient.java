3c9683a97590fa9025bec5af1e009ed9160781d6
flydog98 <guinnessoverflow@gmail.com>
2022-11-07 00:36:06 +0900

    chore: write skeleton code for Gameclient

--- src/main/java/baseball/GameClient.java
+++ src/main/java/baseball/GameClient.java
@@ -1,4 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class GameClient {
+    public GameClient() {
+
+    }
+
+    public boolean gameStart() {
+
+        return true;
+    }
+
+    private void announceGameStart() {
+
+    }
+
+    private void announceGameEnd() {
+
+    }
+
+    private int getUserInput() {
+        return 0;
+    }
+
 }
