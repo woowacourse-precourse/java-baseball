90ee4889cf19c6d342f1680d58a1b28efc4737b9
choiejae <ejae8319@gmail.com>
2022-11-06 19:43:14 +0900

    test: GameResultPrinterTest 생성

---
+++ src/test/java/baseball/GameResultPrinterTest.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameResultPrinterTest {
+    GameResultPrinter Printer;
+
+    @Test
+    void isCreateClass(){
+        Printer = new GameResultPrinter();
+    }
+}
