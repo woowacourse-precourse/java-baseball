ecab1a2042c63404cd14c2e4d6254037002212f4
kjg1514 <kjg1514@naver.com>
2022-11-06 16:57:28 +0900

    feat : 컴퓨터 3자리번호 List로 만들기

---
+++ src/main/java/baseball/ComputerNumber.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerNumber {
+    List<Integer> computer = new ArrayList<>();
+
+    public List<Integer> getComputerNumber() {
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+}
+
