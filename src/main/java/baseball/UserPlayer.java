56306612c2b711fc6c76260e4cda4a2b076fd2fd
baexxbin <soo3886@gmail.com>
2022-11-06 20:13:27 +0900

    Feat(UserPlayer): 사용자 플레이어 클래스 생성
    하나의 사용자 이용을 위한 싱글톤 구현

---
+++ src/main/java/baseball/UserPlayer.java
@@ -0,0 +1,32 @@
+package baseball;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserPlayer {
+	private static UserPlayer userPlayer = null;
+	private UserPlayer() {
+	}
+
+	public static UserPlayer getInstance() {
+		if (userPlayer == null) {
+			userPlayer = new UserPlayer();
+		}
+		return userPlayer;
+	}
+
+	private List<Integer> numberList;
+
+	public void setNumberList(String userInput) {
+		String[] userInputArray = userInput.split("");
+		this.numberList = Arrays.stream(userInputArray)
+			.map(Integer::parseInt)
+			.collect(Collectors.toList());
+	}
+
+	public List<Integer> getNumberList() {
+		return numberList;
+	}
+
+}
