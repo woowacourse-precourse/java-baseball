1aedfef9e81d0a7e6fc0a3810ffdeffd2c17d596
ChanghyunKim <variety82p@gamil.com>
2022-11-06 21:32:42 +0900

    feat : 컴퓨터, 플레이어 입력 셋팅 구현

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -1,14 +1,25 @@
 package baseball.controller;
 
+import baseball.model.BallNumber;
+import baseball.model.GameManager;
 import baseball.view.View;
+
+import java.util.List;
 
 public class BaseballGame {
     private static final View view = new View();
+    private static final GameManager gm = new GameManager();
 
     public BaseballGame(){
     }
 
     public void startGame(){
         view.initView();
+        BallNumber computer = new BallNumber(gm.createComputerNumber());
+        List<Integer> computerNumber = computer.getBallNumbers();
+
+        String initNumber;
+        BallNumber player;
+        List<Integer> playerNumber;
     }
 }
