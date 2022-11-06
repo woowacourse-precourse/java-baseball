568fd46ac4ffa79aa8e24d5573561018bde18acc
yubin <dbqls2821@gmail.com>
2022-11-07 01:18:36 +0900

    feat(ScoreCounter): ball, strike의 갯수를 count하는 메소드 구현

--- src/main/java/baseball/ScoreCounter.java
+++ src/main/java/baseball/ScoreCounter.java
@@ -13,6 +13,23 @@ public class ScoreCounter {
   public void count(List<Integer> computerNumber, String input) {
     init();
     List<Integer> userInput = convertStringToIntegerList(input);
+    for (int i = 0; i < input.length(); i++) {
+      if (isThereStrikes(computerNumber.get(i), userInput.get(i))) {
+        strikeCount++;
+        continue;
+      }
+      if (isThereBalls(computerNumber, userInput.get(i))) {
+        ballCount++;
+      }
+    }
+  }
+
+  private boolean isThereStrikes(Integer computerNumber, Integer input) {
+    return computerNumber.equals(input);
+  }
+
+  private boolean isThereBalls(List<Integer> computerNumbers, Integer input) {
+    return computerNumbers.contains(input);
   }
 
   private List<Integer> convertStringToIntegerList(String input) {
