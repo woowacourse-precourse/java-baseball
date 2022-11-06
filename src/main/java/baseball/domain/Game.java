f1bed138004ee519124d6811e9d499d228aebcff
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-06 11:33:45 +0900

    feat(game): 입력 값을 list로 변경 해주는 기능

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,5 +1,10 @@
 package baseball.domain;
 
+import baseball.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
     private int LENGTH = 3;
     private Computer computer;
@@ -13,6 +18,7 @@ public class Game {
         int ball = 0;
 
         while(!isComplete(strike)) {
+            String input = View.inputView();
 
         }
     }
@@ -20,5 +26,16 @@ public class Game {
     private boolean isComplete(int strike) {
         if(LENGTH == strike) return true;
         else return false;
+    }
+
+    private List<Integer> inputToIntegerList(String input) {
+        char[] chars = input.toCharArray();
+        List<Integer> list = new ArrayList<>();
+
+        for (char c : chars) {
+            list.add(Character.getNumericValue(c));
+        }
+
+        return list;
     }
 }
