da19ca8ed36ae5acc3d810664916512439bbe885
juhwang <juhong4930@naver.com>
2022-11-06 18:15:48 +0900

    test(BaseballNumbersTest): differentiate that input string is between integers and a "records" command

--- src/test/java/baseball/BaseballNumbersTest.java
+++ src/test/java/baseball/BaseballNumbersTest.java
@@ -87,4 +87,51 @@ class BaseballNumbersTest {
 
     }
 
+    @Nested
+    class 입력값이_IllegalArgumentException인지_확인 {
+
+        @Test
+        void case1_3자리_숫자_입력() {
+            String input = "123";
+            boolean result = true;
+            assertThat(baseballNumbers.checkReadline(input)).isEqualTo(result);
+        }
+
+        @Test
+        void case2_records_소문자_명령어_입력() {
+            String input = "records";
+            boolean result = false;
+            assertThat(baseballNumbers.checkReadline(input)).isEqualTo(result);
+        }
+
+        @Test
+        void case3_명령어_외_다른_문자_입력() {
+            String input = "record";
+            assertThatThrownBy(() -> baseballNumbers.checkReadline(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void case4_1부터_9까지_범위_외_숫자_입력() {
+            String input = "203";
+            assertThatThrownBy(() -> baseballNumbers.checkReadline(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void case5_4자리_숫자_입력() {
+            String input = "1234";
+            assertThatThrownBy(() -> baseballNumbers.checkReadline(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void case6_2자리_숫자_입력() {
+            String input = "12";
+            assertThatThrownBy(() -> baseballNumbers.checkReadline(input))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+    }
+
 }
