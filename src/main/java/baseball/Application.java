122091939e87b49681e874212ac007f5c369ebee
박준용 <jo101044@naver.com>
2022-11-06 13:27:04 +0900

    정답 숫자 생성 메소드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.Scanner;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Scanner sc = new Scanner(System.in);
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         String answerNum = createAnswer();
@@ -15,17 +17,24 @@ public class Application {
         int endPoint = 0;
         while(endPoint != 2){
             System.out.println("숫자를 입력해주세요 : ");
-            String inputNum = sc.next();
+            String inputNum = Console.readLine();
             int strike = countStrike(answerNum, inputNum);
             int ball = countBall(answerNum, inputNum, strike);
 
+            endPoint = giveScore(strike, ball);
         }
 
-
-        sc.close();
     }
 
+    private static int giveScore(int strike, int ball) {
+        if(strike == 3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return 2;
+        }
 
+        System.out.println(ball+"볼 "+strike+"스트라이크");
+        return 0;
+    }
 
     private static int countBall(String answerNum, String inputNum, int strike) {
         int count = 0;
@@ -52,10 +61,16 @@ public class Application {
     }
 
     private static String createAnswer() {
+        Set<Integer> set = new HashSet<>();
         StringBuilder answer = new StringBuilder();
 
-        for (int i = 0; i < 3; i++) {
-            answer.append(Randoms.pickNumberInRange(1,9));
+        while(set.size() < 3){
+            int n = Randoms.pickNumberInRange(1,9);
+            set.add(n);
+        }
+
+        for (Integer integer : set) {
+            answer.append(integer);
         }
 
         return answer.toString();
