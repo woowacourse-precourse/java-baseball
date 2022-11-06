92d0738f30e255d45352afe95343224d4fe64c31
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-07 00:34:18 +0900

    feat: 뽑은 공의 개수가 게임 규칙과 일치하는지 검증하는 기능 추가

--- src/main/java/baseball/validator/BallValidator.java
+++ src/main/java/baseball/validator/BallValidator.java
@@ -1,5 +1,7 @@
 package baseball.validator;
 
+import static baseball.constant.Constants.*;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,6 +10,12 @@ public class BallValidator {
 	public static void validateDuplicate(List<Integer> balls) throws IllegalArgumentException {
 		List<Integer> deDuplicatedBalls = balls.stream().distinct().collect(Collectors.toList());
 		if (deDuplicatedBalls.size() != balls.size()) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public static void validateNumberOfBalls(List<Integer> balls) {
+		if (balls.size() != NUMBER_OF_BALLS_FOR_BASEBALL_GAME) {
 			throw new IllegalArgumentException();
 		}
 	}
