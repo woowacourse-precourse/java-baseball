02e7a645d715147b56dcd00902235ae93ff51cd4
jin-chang-ho <ckdgh8204@naver.com>
2022-11-07 01:08:46 +0900

    2nd commit : 컴퓨터 숫자 생성 함수 변수명 변경
    Style: 컴퓨터 숫자 생성 함수 변수명 변경
    
    변수 타입을 알 수 있도록 변수명 변경
    
    Resolves: #1

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,16 +7,16 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
     static List<Integer> computer_create_number() {
-        List<Integer> computer_number = new ArrayList<Integer>();
+        List<Integer> computer_number_list = new ArrayList<Integer>();
 
-        while (computer_number.size() != 3) {
+        while (computer_number_list.size() != 3) {
             int number = pickNumberInRange(1, 9);
-            if (!computer_number.contains(number)) {
-                computer_number.add(number);
+            if (!computer_number_list.contains(number)) {
+                computer_number_list.add(number);
             }
         }
 
-        return computer_number;
+        return computer_number_list;
     }
 
     public static void main(String[] args) {
@@ -26,8 +26,8 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         while (next_game) {
-            List<Integer> computer_number = new ArrayList<Integer>();
-            computer_number = computer_create_number();
+            List<Integer> computer_number_list = new ArrayList<Integer>();
+            computer_number_list = computer_create_number();
         }
     }
 }
