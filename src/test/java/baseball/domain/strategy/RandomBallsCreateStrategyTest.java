fc691f55a256e85482ffdb8ea4f959a8504ef261
JunH <tjdtls690@gmail.com>
2022-11-07 02:45:04 +0900

    :art: refactor(ALL) : import 문 정리 및 컨벤션에 맞춰서 코드 정리

--- src/test/java/baseball/domain/strategy/RandomBallsCreateStrategyTest.java
+++ src/test/java/baseball/domain/strategy/RandomBallsCreateStrategyTest.java
@@ -14,7 +14,7 @@ class RandomBallsCreateStrategyTest {
     void create_computer_balls() {
         BallsCreateStrategy ballsCreateStrategy =
                 () -> List.of(new Ball(0, 4), new Ball(1, 5), new Ball(2, 2));
-    
+        
         final List<Ball> expectedComputerBalls = List.of(new Ball(0, 4), new Ball(1, 5), new Ball(2, 2));
         assertThat(ballsCreateStrategy.create()).isEqualTo(expectedComputerBalls);
     }
