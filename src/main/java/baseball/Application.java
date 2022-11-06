4d110d0b4d2017286c187696ea333b22f77c06a3
fakerdeft <whakswp123@naver.com>
2022-11-07 03:21:13 +0900

    feat(getUserNumber): add 유저 숫자 얻는 메소드 추가
    숫자를 입력받아 리스트로 반환하는 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }//public static void main
+
+    public static List<Integer> getUserNumber() {
+        List<Integer> user = new ArrayList<>();
+        String userRandomNumber = "";
+        int i = 0;
+
+        System.out.print("숫자를 입력해주세요: ");
+        userRandomNumber = exceptReadingLine(Console.readLine());
+        while (user.size() < 3) {
+            if (!user.contains(userRandomNumber.substring(i, i + 1))) {
+                user.add(Integer.parseInt(userRandomNumber.substring(i, i + 1)));
+                i++;
+            }
+        }
+        return user;
+    }//public static void userNumber
+
+
 }
