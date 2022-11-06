bf678d372327fd56a3b1ccf7f366139fabb5a554
comet7001 <comet7001@gmail.com>
2022-11-06 19:35:50 +0900

    feat(application): implement finishOrRestart(), checkOneOrTwo()

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Application {
@@ -21,9 +20,7 @@ public class Application {
         isValid(whatClientInputs);
         List<Integer> guessedNumbers = stringToIntegerList(whatClientInputs);
         checkNothing(whatComputerInputs, guessedNumbers);
-        checkBallAndStrike();
-
-
+        finishOrRestart();
     }
 
     public static void putRandomNumbersTo(List<Integer> digitsToCompare) {
@@ -54,19 +51,20 @@ public class Application {
                 || whatClientInputs.charAt(0) == whatClientInputs.charAt(2)) {
             makeException();
         }
-        if (isNumber(whatClientInputs) == false) {
-            makeException();
-        }
+        isNumber(whatClientInputs);
     }
 
-    public static boolean isNumber(String certainNumber) {
+    public static void isNumber(String certainNumber) {
+        boolean numberOrNot = false;
         for (int characterIndex = 0; characterIndex < certainNumber.length(); characterIndex++) {
             //ASCII Code ((int)'0')==48, ((int)'9'==57)
             if ((int) certainNumber.charAt(characterIndex) >= 48 || (int) certainNumber.charAt(characterIndex) <= 57) {
-                return true;
+                numberOrNot = true;
             }
         }
-        return false;
+        if (numberOrNot == false) {
+            makeException();
+        }
     }
 
     public static void makeException() {
@@ -74,46 +72,67 @@ public class Application {
         throw exit_program;
     }
 
-    public static List<Integer> stringToIntegerList(String numbers){
+    public static List<Integer> stringToIntegerList(String numbers) {
         String[] temporaryArray = numbers.split("");
         List<Integer> guessedNumbers = new ArrayList<>();
-        for(String stringNumber = temporaryArray){
-           guessedNumbers.add(Integer.parseInt(stringNumber));
+        for (String stringNumber : temporaryArray) {
+            guessedNumbers.add(Integer.parseInt(stringNumber));
         }
         return guessedNumbers;
     }
 
-    public static void checkNothing(List<Integer> numbersToGetRight, List<Integer> guessedNumbers){
+    public static void checkNothing(List<Integer> numbersToGetRight, List<Integer> guessedNumbers) {
         int ballOrStrike = 0;
-        for(int guessedNumber : guessedNumbers) {
+        for (int guessedNumber : guessedNumbers) {
             if (numbersToGetRight.contains(guessedNumber)) {
                 ballOrStrike++;
             }
         }
-        if(ballOrStrike==0){
+        if (ballOrStrike == 0) {
             System.out.println(Referee.NOTHING.ordered());
             playGame(numbersToGetRight);
         }
-        checkBallAndStrike(numbersToGetRight, guessedNumbers,ballOrStrike);
+        checkBallAndStrike(numbersToGetRight, guessedNumbers, ballOrStrike);
     }
 
-    public static void checkBallAndStrike(List<Integer> numbersToGetRight, List<Integer> guessedNumbers, int ballOrStrike){
+    public static void checkBallAndStrike(List<Integer> numbersToGetRight, List<Integer> guessedNumbers, int ballOrStrike) {
         int strike = 0;
-        for(int commonIndex=0; commonIndex<3; commonIndex++){
-            if(numbersToGetRight.get(commonIndex)==guessedNumbers.get(commonIndex)) {
+        for (int commonIndex = 0; commonIndex < 3; commonIndex++) {
+            if (numbersToGetRight.get(commonIndex) == guessedNumbers.get(commonIndex)) {
                 strike++;
             }
         }
-        if(strike==0) {
-            System.out.println(ballOrStrike+Referee.BALL.ordered());
+        if (strike == 0) {
+            System.out.println(ballOrStrike + Referee.BALL.ordered());
             playGame(numbersToGetRight);
         }
-        if(strike!=0&&strike!=3) {
-            System.out.println((ballOrStrike-strike)+Referee.BALL.ordered()+strike+Referee.STRIKE.ordered());
+        if (strike != 0 && strike != 3) {
+            System.out.println((ballOrStrike - strike) + Referee.BALL.ordered() + strike + Referee.STRIKE.ordered());
             playGame(numbersToGetRight);
         }
     }
 
+    public static void finishOrRestart() {
+        System.out.println(Referee.FINISH_GAME.ordered());
+        System.out.print(Referee.ASK_AGAIN_OR_NOT.ordered());
+        String yesOrNo = Console.readLine();
+        checkOneOrTwo(yesOrNo);
+        if(Integer.parseInt(yesOrNo)==2) {
+            System.out.print("");
+        }
+    }
+
+    public static void checkOneOrTwo(String yesOrNo){
+        isNumber(yesOrNo);
+        if(Integer.parseInt(yesOrNo)==1){
+            List<Integer> whatComputerInputs = new ArrayList<>();
+            putRandomNumbersTo(whatComputerInputs);
+            playGame(whatComputerInputs);
+        }
+        if(Integer.parseInt(yesOrNo)!=2){
+            makeException();
+        }
+    }
 
 }
 
