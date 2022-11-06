760367d90376c14f0e0038a53bf96f1799173fc3
ensk26 <tjensk26@gmail.com>
2022-11-07 02:09:49 +0900

    feat(Application): 숫자인지 확인하는 메서드
    새로운 메서드를 추가 to Application:
    - 숫자인 경우 true를 반환
    - 숫자가 아닌 경우 false를 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,4 +4,13 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    static boolean isNumber(String number) {
+        try {
+            Integer.parseInt(number);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
