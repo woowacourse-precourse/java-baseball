815e144b39d590b5482bf902c220fc17bfc36b98
youngkyunIm <yee0230@naver.com>
2022-11-06 20:40:31 +0900

    test(Computer) : add ApplicationTest
    Application Test 에서 사용되는 렌덤값을 넣어주는 값이 잘 작동하는지 확인

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -1,9 +1,13 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class ComputerTest {
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ComputerTest extends NsTest {
     @Test
     @DisplayName("정상 값")
     void getInput() {
@@ -13,5 +17,23 @@ class ComputerTest {
 
         //then
         System.out.println(opponent.getComputer());
+    }
+
+    @Test
+    void checkForApplicationTest() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    Computer opponent = new Computer();
+                    System.out.println(opponent.getComputer());
+                    assertThat(output()).contains("[1, 3, 5]");
+                },
+                1, 3, 5
+        );
+    }
+
+
+    @Override
+    protected void runMain() {
+
     }
 }
