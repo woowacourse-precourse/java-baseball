89cdd6c026ecb3f9ac845864fad63899d6da10f0
korjun1993 <corojoon93@naver.com>
2022-11-06 16:20:12 +0900

    test (Application) : 게임 결과를 올바르게 계산하는지 확인하는 테스트코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,7 +1,16 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -28,8 +37,26 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @ParameterizedTest
+    @MethodSource("providePlayTestcase")
+    @DisplayName("play 함수가 정상적으로 게임 결과를 계산하는지 확인한다")
+    void playTest(List<Integer> comNumber, List<Integer> userNumber, GameResult result) {
+        Player computer = new Player(comNumber);
+        Player user = new Player(userNumber);
+        GameResult gameResult = Application.play(computer, user);
+        Assertions.assertThat(gameResult).isEqualTo(result);
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
     }
+
+    private static Stream<Arguments> providePlayTestcase() {
+        return Stream.of(
+                Arguments.of(Arrays.asList(1, 2, 3), Arrays.asList(1, 2, 3), new GameResult(3, 0)),
+                Arguments.of(Arrays.asList(3, 2, 1), Arrays.asList(1, 2, 3), new GameResult(1, 2)),
+                Arguments.of(Arrays.asList(1, 2, 3), Arrays.asList(4, 5, 6), new GameResult(0, 0)));
+    }
+
 }
