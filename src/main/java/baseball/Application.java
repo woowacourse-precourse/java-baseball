3cb9ae47007e37e9f675c540682e79a440f0ff73
lvalentine6 <ysr1227@naver.com>
2022-11-07 04:34:18 +0900

    feat : 결과를 출력하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -134,4 +134,22 @@ public class Application {
         return 3;
     }
 
+    // 결과를 출력하는 메서드
+    static boolean output(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        } else if (ball > 0 && strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0 && strike > 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+        if(strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            return true;
+        }
+        return false;
+    }
 }
