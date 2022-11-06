44912715a75818fb0ad638c8936b8deefcbb7f15
yujaemin <woawoa0513@gmail.com>
2022-11-07 01:23:16 +0900

    feat: 숫자 외의 문자 포함 여부 검사 함수 구현
    입력받은 문자가 숫자 외의 문자를 포함하고 있는지 검사하는 isContainOnlyDigit 메소드 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -2,4 +2,13 @@ package baseball;
 
 public class Player {
 
+
+    public boolean isContainOnlyDigit(String input) {
+        for (Character character : input.toCharArray()) {
+            if (!Character.isDigit(character)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
