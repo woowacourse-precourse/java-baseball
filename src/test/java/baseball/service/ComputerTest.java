ff3865a12e37c5ec510a8156b161d7f746c32da8
saint6839 <saint6839@gmail.com>
2022-11-07 02:20:21 +0900

    feat(modelTest) : 컴퓨터가 랜덤으로 세 개의 숫자를 잘 생성하는지 테스트 작성

---
+++ src/test/java/baseball/service/ComputerTest.java
@@ -0,0 +1,24 @@
+package baseball.service;
+
+import baseball.model.dto.Balls;
+import camp.nextstep.edu.missionutils.Randoms;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ComputerTest {
+
+    @Test
+    void generateRandomBalls_컴퓨터가_사이즈가_3인_리스트를_잘_생성하는지_테스트() {
+        //given
+        Balls computerBalls = new Balls();
+
+        //when
+        while (!computerBalls.isSize(3)) {
+            computerBalls.addBall(Randoms.pickNumberInRange(1, 9));
+        }
+        //then
+        assertThat(computerBalls.getValues()
+                                .size()).isEqualTo(3);
+    }
+}
