a877a4aa61d495b2ba9308a720a7d1690bf727b9
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-06 11:28:43 +0900

    feat(view): 모든 변수, 메소드 static으로 변경

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -3,28 +3,28 @@ package baseball.view;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class View {
-    private String INPUT_PHRASES = "숫자를 입력해주세요 : ";
-    private String END_PHRASES = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private String COMPLETE_PHRASES = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static String INPUT_PHRASES = "숫자를 입력해주세요 : ";
+    private static String END_PHRASES = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static String COMPLETE_PHRASES = "개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
-    public String inputView() {
+    public static String inputView() {
         System.out.println(INPUT_PHRASES);
         return readLine();
     }
 
-    public String endView() {
+    public static String endView() {
         System.out.println(END_PHRASES);
         return readLine();
     }
 
-    public void resultView(int strike, int ball) {
+    public static void resultView(int strike, int ball) {
         if(ball != 0) System.out.print(ball + "볼");
         if(strike != 0) System.out.print(strike + "스트라이크");
         if(ball == 0 && strike == 0) System.out.print("낫싱");
         System.out.println();
     }
 
-    public void completeView(int length) {
+    public static void completeView(int length) {
         System.out.println(length + COMPLETE_PHRASES);
     }
 }
