9b99647d9004efb5a3ce3bfcc175c9fb27ccc030
Jihoon9809 <dkdb521@naver.com>
2022-11-06 16:55:51 +0900

    컴퓨터 클래스 private로 랜덤숫자 리스트 값 보호.

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -7,16 +7,18 @@ import java.util.List;
 
 public class Computer {
 
-    static List<Integer> computerNumbers = new ArrayList<>();
+    private List<Integer> computerNumbers = new ArrayList<>();
 
     public void setComputerNumbers() {
-
         while (computerNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computerNumbers.contains(randomNumber)) {
                 computerNumbers.add(randomNumber);
             }
         }
+    }
+    public List<Integer> getComputerNumbers(){
+        return computerNumbers;
     }
 
 }
