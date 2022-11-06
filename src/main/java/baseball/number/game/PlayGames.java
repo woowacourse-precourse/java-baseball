a2ff0f0e8cc13017a07c98209042a44c8332cfdc
penrose15 <seongju99@naver.com>
2022-11-06 12:15:11 +0900

    feat : add function about if quit number is not 1 or 2
    If player do not enter 1 or 2 when program ask re-start or quit game, add a function that prompts you to enter again.
    
    Resolves: #14

--- src/main/java/baseball/number/game/PlayGames.java
+++ src/main/java/baseball/number/game/PlayGames.java
@@ -3,13 +3,9 @@ package baseball.number.game;
 import baseball.number.client.ClientNumbers;
 import baseball.number.client.HintsAboutNumbers;
 import baseball.number.computer.ComputerNumbers;
-import baseball.utils.validation.Validation;
+import camp.nextstep.edu.missionutils.Console;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.List;
-import java.util.Scanner;
 
 public class PlayGames {
 
@@ -17,33 +13,32 @@ public class PlayGames {
     HintsAboutNumbers hint = new HintsAboutNumbers();
     ClientNumbers clientNumbers = new ClientNumbers();
 
-    Scanner sc = new Scanner(System.in);
-    boolean isMatched;
-
     public void playGames() {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        int quitGame = 0;
+        int quitGame;
         do{
             List<Integer> computerNumber = computerNumbers.generateRandomNumbers();
-//            for (Integer integer : computerNumber) {
-//                System.out.print(integer); //for test
-//            }
+            for (Integer integer : computerNumber) {
+                System.out.print(integer); //for test
+            }
 
             matchNumber(computerNumber);
 
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-            quitGame = Integer.parseInt(sc.next());
+            quitGame = Integer.parseInt(Console.readLine());
 
         } while(quitGame != 2); //1입력시 게임 재시작, 2 입력시 게임 종료
+
+        System.out.println("게임을 종료합니다.");
     }
 
     public void matchNumber(List<Integer> computerNumber) {
         boolean isMatched;
         do {
-            String number = sc.next();
+            String number = Console.readLine();
 
             List<Integer> client = clientNumbers.clientNumberToList(number);
 
