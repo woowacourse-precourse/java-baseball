8d217956e35aee35ec826017315786145484f15e
MINJU <alswn9938@gmail.com>
2022-11-07 00:54:12 +0900

    refactor($ComputerInput): modify ComputerInput 클래스
    [검증된 컴퓨터 랜덤 숫자] 함수 추가

--- src/main/java/baseball/input/ComputerInput.java
+++ src/main/java/baseball/input/ComputerInput.java
@@ -1,7 +1,7 @@
 package baseball.input;
 
 import baseball.validatioon.Validation;
-
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,10 +9,18 @@ public class ComputerInput {
     public List<Integer> getNumberList() {
         ArrayList<Integer> computerNumberList = new ArrayList<>();
         while (computerNumberList.size() < 3) {
-            int randomNumber = Validation.getValidRandomNumber(computerNumberList);
+            int randomNumber = getValidRandomNumber(computerNumberList);
             computerNumberList.add(randomNumber);
         }
         return computerNumberList;
+    }
+
+    public static int getValidRandomNumber(List<Integer> lst) {
+        int pickNumber;
+        do {
+            pickNumber = Randoms.pickNumberInRange(1, 9);
+        } while (!Validation.isValidNumber(pickNumber, lst));
+        return pickNumber;
     }
 
 }
