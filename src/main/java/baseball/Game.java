2ce71e5793b80d9750f240642f9be9334199970a
yun <yshyeon102@gmail.com>
2022-11-07 02:07:54 +0900

    feat(game): 게임 단판 메서드 정의
    한번의 실행으로 여러 번의 게임을 진행할 수 있도록 새로운 메서드를 하나 정의

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,6 +1,20 @@
 package baseball;
 
 public class Game {
+    public static final int NUMBER_LENGTH = 3;
+    public static MessagePrinter messagePrinter = new MessagePrinter();
+
+    public boolean runningState = true;
+    public Computer computer = new Computer();
+
     public void run() {
+        messagePrinter.printInit();
+        while (runningState) {
+            start(computer.makeRandomNumberString());
+        }
+    }
+
+    private void start(String computerNumber) {
+
     }
 }
