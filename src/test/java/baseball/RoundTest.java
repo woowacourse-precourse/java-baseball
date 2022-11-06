454e860845da9af54d51fb0e93f0e38e7c47062b
dooboocookie <bkh3030@gmail.com>
2022-11-06 20:07:43 +0900

    test: Round에 대하여 입력, 예외, 3스트라이크 테스트 추가

--- src/test/java/baseball/RoundTest.java
+++ src/test/java/baseball/RoundTest.java
@@ -1,34 +1,107 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.*;
+import java.lang.reflect.Field;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 public class RoundTest {
 
+    private static Computer computer;
+    private static int computerNumbers;
+    private static int computerNumber1;
+    private static int computerNumber2;
+    private static int computerNumber3;
+
+    private static Round round;
+
+    @BeforeAll
+    static void beforeAll() {
+        computer = Computer.getComputer();
+        computer.pickNewRandomNumbers();
+        computerNumbers = 0;
+        computerNumber1 = computer.findComputerNumber(0).getNumber();
+        computerNumbers += computerNumber1 * ((int) Math.pow(10, 2));
+        computerNumber2 = computer.findComputerNumber(1).getNumber();
+        computerNumbers += computerNumber1 * ((int) Math.pow(10, 1));
+        computerNumber3 = computer.findComputerNumber(2).getNumber();
+        computerNumbers += computerNumber1 * ((int) Math.pow(10, 0));
+        System.out.println("computerNumbers = " + computerNumbers);
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        round = Round.getRound();
+    }
+
     @ParameterizedTest
-    @ValueSource(strings = {"1245", "12","122"})
+    @ValueSource(strings = {"123"})
     void roundInputTest(String input) throws Exception {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
 
+        round.startNewRound(computer);
 
-        Computer computer = Computer.getComputer();
-        Round round = Round.getRound();
+        Field field = Round.class.getDeclaredField("numbers");
+        field.setAccessible(true);
+        Object numbers = field.get(round);
 
-        assertThatThrownBy(() -> round.startNewRound(computer))
-                .isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> round.startNewRound(computer))
-                .isInstanceOf(IllegalArgumentException.class);
+        System.out.println("computerNumbers = " + computerNumbers);
+        assertThat(numbers)
+                .isNotNull()
+                .isInstanceOf(Numbers.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1234"})
+    void roundInputExceptionTest(String input) throws Exception {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
         assertThatThrownBy(() -> round.startNewRound(computer))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @ParameterizedTest
+    @MethodSource("strikeValue")
+    void roundStrikeTest(String input) throws Exception {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        round.startNewRound(computer);
+        System.out.println("3Strike input = " + input);
+        Field field = Round.class.getDeclaredField("hints");
+        field.setAccessible(true);
+        Hints hints = (Hints) field.get(round);
+
+        assertThat(hints.findHintCount(Hint.STRIKE))
+                .isEqualTo(3);
+    }
+
+    private static Stream<Arguments> strikeValue() {
+        String input = computerNumbers + "";
+        return Stream.of(
+                Arguments.of(input, true)
+        );
+    }
+
+    @Test
+    void roundBallTest() {
+
+    }
+
+    @Test
+    void roundNothing() {
+
+    }
 
 }
