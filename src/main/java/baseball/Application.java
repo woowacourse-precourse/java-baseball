88ef02be0441a5fd542a89ec4fb55f8b94bd3459
seoyoung <sy9723@korea.ac.kr>
2022-11-06 21:31:58 +0900

    feat: input finish number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -35,10 +35,19 @@ public class Application {
         if (input == computer) {
             System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int finishNumber = inputFinish();
+            
         }
     }
 
+    private static int inputFinish() throws IllegalArgumentException {
+        int intInput = Integer.parseInt(Console.readLine());
+        if (intInput != 1 && intInput != 2) {
+            throw new IllegalArgumentException();
+        }
+        return intInput; 
+    }
     private static int CreateNumbers(int SizeOfNum) {
         List<Integer> computerIntList = new ArrayList<>();
         while (computerIntList.size() < 3) {
