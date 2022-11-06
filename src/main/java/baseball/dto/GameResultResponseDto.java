55675c876c3368db1e31693ec42c5c564fc64edf
heesang <dlagmltkd12@naver.com>
2022-11-06 18:00:27 +0900

    refactor: GameResultResponseDto 내 중복 상수 제거

--- src/main/java/baseball/dto/GameResultResponseDto.java
+++ src/main/java/baseball/dto/GameResultResponseDto.java
@@ -1,8 +1,9 @@
 package baseball.dto;
 
+import baseball.domain.Player;
+
 public class GameResultResponseDto {
 
-    public static final int GAME_END_SCORE = 3;
     public static final int CRITERION_ZERO = 0;
     private final int strike;
     private final int ball;
@@ -29,6 +30,6 @@ public class GameResultResponseDto {
     }
 
     public boolean isThreeStrike() {
-        return strike == GAME_END_SCORE;
+        return strike == Player.GAME_ROUND;
     }
 }
