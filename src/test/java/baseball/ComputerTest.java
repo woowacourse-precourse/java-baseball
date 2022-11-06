e0ce557b6da32792cc33eb40ab89fe8262436a2d
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-06 23:37:13 +0900

    test(Computer): status 메서드 테스트 코드 작성
    Computer 의 정답 answer 와 현재 User 의 guess 를 비교해서 현재 상태를 알려주는 status 메서드에 대한 테스트 코드 작성

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class ComputerTest {
 
-    @RepeatedTest(100)
+    @RepeatedTest(value = 100, name = "생성_확인() #{currentRepetition}")
     void generateAnswer_올바른_생성_확인() {
         Computer computer = Computer.generateAnswer();
         try {
@@ -50,6 +50,21 @@ public class ComputerTest {
             user.guess = new Numbers(List.of(2, 1, 3));
             int result = 2;
             assertThat(result).isEqualTo(test.ball(user));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Test
+    void status_확인() {
+        try {
+            Constructor<Computer> constructor = Computer.class.getDeclaredConstructor(Numbers.class);
+            constructor.setAccessible(true);
+            Computer test = constructor.newInstance(new Numbers(List.of(1, 2, 3)));
+            User user = new User();
+            user.guess = new Numbers(List.of(1, 4, 2));
+            String result = "1볼 1스트라이크";
+            assertThat(result).isEqualTo(test.status(user).toString());
         } catch (Exception e) {
             e.printStackTrace();
         }
