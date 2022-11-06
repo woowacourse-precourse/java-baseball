fab35474995775b6c400a03844f9127b53a375dd
qkrdmswl <eunji303@naver.com>
2022-11-06 16:01:24 +0900

    feat(opponentNum) : create opponent num

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,40 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static void numBaseball(List<Integer> computerNum) {
+
+    }
+
+    public static int comparator(List<Integer> computerNum, List<Integer> userNum) {
+        return 0;
+    }
+
+
+    public static List<Integer> opponentNum() {
+        List<Integer> computerNum = new ArrayList<>();
+        while (computerNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNum.contains(randomNumber)) {
+                computerNum.add(randomNumber);
+            }
+        }
+        return computerNum;
+    }
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        int choice = 1;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(choice == 1) {
+            numBaseball(opponentNum());
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            choice = Integer.parseInt(Console.readLine());
+        }
     }
 }
