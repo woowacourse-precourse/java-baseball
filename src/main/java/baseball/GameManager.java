78e029460d9a495f760d9543e42189b835319c17
mirasoy <nkskjjbb@gmail.com>
2022-11-06 20:27:26 +0900

    [Feat](GameManager) 게임 진행 - 재게임 여부 확인

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -2,6 +2,8 @@ package baseball;
 
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
+
 /**
 * GameManager
 * 게임 진행을 위한 클래스
@@ -50,6 +52,29 @@ public class GameManager {
      */
 	public void endGame() {
 		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+	}
+	
+	/**
+     * 재게임 여부 확인
+     * @return 재게임 여부 (재게임 : true / 그만두기 : false / 그외 : 예외)
+     * @exception 1,2 외의 값 입력시 IllegalArgumentException 발생 
+     */
+	public boolean confirmRestart() {
+		
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		String continueResult = Console.readLine();
+		
+		if(continueResult.equals("1")) {
+			return true;
+		}
+		if(continueResult.equals("2")) {
+			return false;
+		}
+		if(continueResult.equals("1")||continueResult.equals("2")) {
+			throw new IllegalArgumentException();
+		}
+		
+		return false;
 	}
 	
 }
