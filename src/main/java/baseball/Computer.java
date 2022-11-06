ffe856acb03b5c3aa79371b5f2830df37b42929f
miffyKing <whqudghk28@gmail.com>
2022-11-06 17:53:00 +0900

    feat: Delete Random Value Generating method and remove unused instance.

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,64 +5,39 @@ import java.util.ArrayList;
 
 public class Computer {
 
-  public static String RandomValue;
-  private static final int CORRECTINPUTLENGTH = 3;
-  private static final int GAMESTARTMODE = 1;
-  private static final int INPUTNUMBERMODE = 2;
-  private static final int GENERATERANDOMMODE = 3;
-  private static final int CALCULATEDOUTPUTMODE = 4;
+    private static RandomNumber RandomValue;
+    private static final int GAMESTARTMODE = 1;
+    private static final int INPUTNUMBERMODE = 2;
+    private static final int GENERATERANDOMMODE = 3;
+    private static final int CALCULATEDOUTPUTMODE = 4;
 
-  Computer() {
-    RandomValue = generateRandom();
-  }
-
-  public static void printGameStart() {
-    System.out.println("숫자 야구 게임을 시작합니다.");
-  }
-
-  public static void printInputNumber() {
-    System.out.print("숫자를 입력해주세요 : ");
-  }
-
-  public static void printEndingMessage() {
-    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-  }
-
-  public static String printCalculateResult(String calculatedResult) {
-    System.out.println(calculatedResult);
-    return calculatedResult;
-  }
-
-  private static String generateRandom() {
-    StringBuilder sb = new StringBuilder();
-    ArrayList<Integer> generateRandNum = new ArrayList<>();
-
-    while (generateRandNum.size() < CORRECTINPUTLENGTH) {
-      int randomNumber = Randoms.pickNumberInRange(1, 9);
-      if (!generateRandNum.contains(randomNumber)) {
-        generateRandNum.add(randomNumber);
-        sb.append(Integer.toString(randomNumber));
-      }
+    public static void printGameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    RandomValue = sb.toString();
-    return RandomValue;
-  }
+    public static void printInputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
 
-  public static String testComputerPrint(int mode, String input) {
-    if (mode == GAMESTARTMODE) {
-      printGameStart();
+    public static void printEndingMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
-    if (mode == INPUTNUMBERMODE) {
-      printInputNumber();
+
+    public static String printCalculateResult(String calculatedResult) {
+        System.out.println(calculatedResult);
+        return calculatedResult;
     }
-    if (mode == GENERATERANDOMMODE) {
-      return generateRandom();
+
+    public static String testComputerPrint(int mode, String input) {
+        if (mode == GAMESTARTMODE) {
+            printGameStart();
+        } else if (mode == INPUTNUMBERMODE) {
+            printInputNumber();
+        } else if (mode == GENERATERANDOMMODE) {
+        //    return RandomGenerator.generateRandom(new StringBuilder(), new ArrayList<Integer>());
+        } else if (mode == CALCULATEDOUTPUTMODE) {
+            printCalculateResult(input);
+        }
+        return "";
     }
-    if (mode == CALCULATEDOUTPUTMODE) {
-      printCalculateResult(input);
-    }
-    return "";
-  }
 }
