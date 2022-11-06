49d69a32e852d786cf08233c688d3320876874a9
flydog98 <guinnessoverflow@gmail.com>
2022-11-06 19:10:13 +0900

    feat: write readLineInt readLineInt reads Line by Console.readLine() and converts it from string to int and returns that int value it throws IllegalArgumentException when input value can't be converted to String to int

--- src/main/java/baseball/BaseballUtil.java
+++ src/main/java/baseball/BaseballUtil.java
@@ -7,6 +7,17 @@ public class BaseballUtil {
     }
 
     public int readLineInt() {
+        String input = Console.readLine();
+        int return_value;
+
+        try{
+            return_value = Integer.parseInt(input);
+        }
+        catch(Exception e) {
+            throw new IllegalArgumentException();
+        }
+
+        return return_value;
     }
 
     public void printLine(String contents) {
