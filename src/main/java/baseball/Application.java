e66f986ac7f590f71d2d00b20c9493cd6f53f1ff
Yoobi <quddlr96@naver.com>
2022-11-06 21:25:26 +0900

    feat: add method to generate random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static String generateRandomNumber() {
+        String randomNumber = "";
+        while (randomNumber.length() < 3) {
+            String randomDigit = Integer.toString(Randoms.pickNumberInRange(1, 9));
+            if (!randomNumber.contains(randomDigit)) {
+                randomNumber = randomNumber + randomDigit;
+            }
+        }
+        return randomNumber;
     }
 }
