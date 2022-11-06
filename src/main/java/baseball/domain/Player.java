4374348aba0f080be27dc84dfb974c87c377587c
heesang <dlagmltkd12@naver.com>
2022-11-06 17:52:18 +0900

    refactor: 중복 숫자를 체크하는 로직을 Set 자료구조를 이용하여 리팩토링

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,6 +1,8 @@
 package baseball.domain;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -8,11 +10,7 @@ public class Player {
 
     public static final String NUMBER_SIZE_EX_MESSAGE = "입력값은 3개의 숫자여야 합니다.";
     public static final int CRITERION_NUMBER_SIZE = 3;
-
-    public static final String NUMBER_SAME_EX_MESSAGE = "입력값은 같은 수가 하나라도 있으면 안됩니다.";
-    public static final int NUMBER_INDEX_ZERO = 0;
-    public static final int NUMBER_INDEX_ONE = 1;
-    public static final int NUMBER_INDEX_TWO = 2;
+    public static final String NUMBER_DUPLICATE_EX_MESSAGE = "입력값은 같은 수가 하나라도 있으면 안됩니다.";
     public static final int START_POSITION_NUMBER = 1;
 
     private List<Ball> balls;
@@ -27,7 +25,7 @@ public class Player {
 
     private void validateNumbers(List<Integer> numbers) {
         validateSize(numbers);
-        validateSameNumber(numbers);
+        validateDuplicateNumber(numbers);
     }
 
     private void validateSize(List<Integer> numbers) {
@@ -36,20 +34,11 @@ public class Player {
         }
     }
 
-    private void validateSameNumber(List<Integer> numbers) {
-        if (isSameNumber(numbers.get(NUMBER_INDEX_ZERO), numbers.get(NUMBER_INDEX_ONE))) {
-            throw new IllegalArgumentException(NUMBER_SAME_EX_MESSAGE);
+    private void validateDuplicateNumber(List<Integer> numbers) {
+        Set<Integer> checkDuplicateNumbers = new HashSet<>(numbers);
+        if (checkDuplicateNumbers.size() != CRITERION_NUMBER_SIZE) {
+            throw new IllegalArgumentException(NUMBER_DUPLICATE_EX_MESSAGE);
         }
-        if (isSameNumber(numbers.get(NUMBER_INDEX_ZERO), numbers.get(NUMBER_INDEX_TWO))) {
-            throw new IllegalArgumentException(NUMBER_SAME_EX_MESSAGE);
-        }
-        if (isSameNumber(numbers.get(NUMBER_INDEX_ONE), numbers.get(NUMBER_INDEX_TWO))) {
-            throw new IllegalArgumentException(NUMBER_SAME_EX_MESSAGE);
-        }
-    }
-
-    private boolean isSameNumber(Integer number, Integer otherNumber) {
-        return number.equals(otherNumber);
     }
 
     private List<Ball> mapToBall(List<Integer> numbers) {
