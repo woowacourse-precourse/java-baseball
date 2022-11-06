bfc6797d765967f1bc0c697eb885e82f44878847
jin-chang-ho <ckdgh8204@naver.com>
2022-11-07 00:50:17 +0900

    1st commit : 컴퓨터 숫자 생성 구현
    feat: 컴퓨터가 서로 다른 수로 이뤄진 3자리 숫자를 생성함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import java.util.*;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
+    static List<Integer> computer_create_number() {
+        List<Integer> computer_number = new ArrayList<Integer>();
+
+        while (computer_number.size() != 3) {
+            int number = pickNumberInRange(1, 9);
+            if (!computer_number.contains(number)) {
+                computer_number.add(number);
+            }
+        }
+
+        return computer_number;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Boolean next_game = true;
+
+        // 게임 시작 문구 출력
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (next_game) {
+            List<Integer> computer_number = new ArrayList<Integer>();
+            computer_number = computer_create_number();
+        }
     }
 }
