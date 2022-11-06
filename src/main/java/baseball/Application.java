ff2820e36c92866fc27b78aa752f17276f389f62
Gibeom Gwon <gb.gwon@stackframe.dev>
2022-11-06 11:25:12 +0900

    feat(baseball): 랜덤한 3개의 숫자를 가진 리스트 반환 함수 추가
    Random 라이브러리를 사용하여 숫자를 생성하며 중복된 숫자가 발생하지
    않을 때까지 반복

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import java.util.List;
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static List<Integer> generateThreeRandomNumber() {
+        List<Integer> computerNumber = new ArrayList<>(3);
+
+        for (int i = 0; i < 3; i++) {
+            Integer num = Randoms.pickNumberInRange(1, 9);
+
+            if (computerNumber.contains(num)) {
+                i--;
+                continue;
+            }
+            computerNumber.add(num);
+        }
+
+        return computerNumber;
     }
 }
