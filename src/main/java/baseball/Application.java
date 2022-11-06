20d095865b47fe3cffad2c761a5d7ce72502349f
seoeunbae <enyoupark@gmail.com>
2022-11-06 16:58:57 +0900

    feat: 입력값에 띠른 볼, 스트라이크 status을 저장 기능 구현, 기능 목록 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -26,16 +26,40 @@ public class Application {
 
     public static void playGame(List<Integer> computer){
         System.out.println("숫자 야구 게임을 시작합니다.");
+
         while(true){
             System.out.print("숫자를 입력해주세요: ");
             String input = Console.readLine();
 
             String[] inputSplits = input.split("");
             int[] inputNumbers = Arrays.stream(inputSplits).mapToInt(Integer::valueOf).toArray();
+
+            printNumberStatus(inputNumbers, computer);
+
         }
     }
 
-    private static void printNumberStatus(Integer[] inputSplits, List<Integer> computer){
+    private static void printNumberStatus(int[] inputNumbers, List<Integer> computer){
+        GameStatus gameStatus = new GameStatus();
+
+        for(int i = 0 ; i < inputNumbers.length; i++){
+            if( !computer.contains( inputNumbers[i] ) ){ //해당 숫자가 정답에 없다.
+                gameStatus.nothing = true;
+            } else { //포함은 되어 있다.
+                if(i == computer.indexOf(inputNumbers[i])){ //스트라이크인 경우
+                    gameStatus.strike++;
+                } else gameStatus.ball++; //볼인 경우
+            }
+        }
+
+    }
+
+    public static class GameStatus{
+        Boolean nothing = false;
+
+        Integer ball = 0;
+
+        Integer strike = 0;
 
     }
 }
