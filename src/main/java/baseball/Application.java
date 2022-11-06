34076d956473453e6edc18915a8b363d771f534c
solesy <tkddbs9801@gmail.com>
2022-11-06 21:49:33 +0900

    feat(main): check that the user input value is valid
    Add three new private function in Application:
    - check whether the string is 3-digit number or not
    - check whether the string has unique character or not
    - if the user input is invalid, throw IllegalArgumentException

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Application {
     private boolean isNumber(String str){
@@ -14,6 +15,31 @@ public class Application {
             .filter(Character::isDigit)
             .count();
         return numOfDigit == str.length();
+    }
+
+    private boolean is3DigitNumber(String str){
+        return isNumber(str) && str.length() == 3;
+    }
+
+    private boolean hasUniqueCharacter(String str){
+        if(str == null || str.isEmpty())
+            return false;
+        long numOfUniqueCharacter = str.chars()
+                .distinct()
+                .count();
+        return numOfUniqueCharacter == str.length();
+    }
+
+    private void validateUserInput(String userInput, boolean gameover){
+        if(gameover){
+            if(!Objects.equals(userInput,"1") && !Objects.equals(userInput,"2")){
+                throw new IllegalArgumentException("userInput should be 1 or 2.");
+            }
+        } else{
+            if(!is3DigitNumber(userInput) && hasUniqueCharacter(userInput)){
+                throw new IllegalArgumentException("userInput should be 3 digit number.");
+            }
+        }
     }
 
     public static void main(String[] args) {
