2f98735667f4545cad3dd43b5b65cf00b65b1ef0
vcho1958 <gusckd57@gmail.com>
2022-11-07 08:43:00 +0900

    feat(core): CorrectAnswer.java에 1~9 중복체크 후 생성하는 기능 추가

--- src/main/java/baseball/CorrectAnswer.java
+++ src/main/java/baseball/CorrectAnswer.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 public class CorrectAnswer {
     List<Integer> randomNumberList = new ArrayList<>();
+    List<Boolean> isAlreadyExists = new ArrayList<>(9);
     /**
      * 정답 배열과 비교
      *
@@ -21,6 +22,26 @@ public class CorrectAnswer {
         //비교 후 결과를 출력한다.
         //3스트라이크면 true 아니면 false 반환
         return 0;
+    }
+
+    /**
+     * 1~9사이의 수가 이미 존재하는지 체크합니다
+     * @param number 1~9사이의 수
+     * @return 중복여부
+     */
+    private boolean isExists(int number){
+        return isAlreadyExists.get(number);
+    }
+
+    /**
+     * 1~9중 isAlreadyExists에 체크되지 않는 숫자를 반환합니다.
+     * @return 1~9사이의 한자리 수
+     */
+    private int generateRandomNumber(){
+        int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (isExists(randomNumber))
+            randomNumber = Randoms.pickNumberInRange(1, 9);
+        return randomNumber;
     }
 
     /**
