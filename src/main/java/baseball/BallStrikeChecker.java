2e67f5bebacd85ea7d3281de53270a4a0d133d7b
dener8 <8dener@gmail.com>
2022-11-06 16:51:06 +0900

    feat: 게임의 결과를 담는 BallStrikeChecker 클래스를 정의하고, 내부에 볼과 스트라이크의 합산을 반환하는 countSumOfBallAndStrike 메서드 구현
    세부적으로 나누기 전에, 모든 결과를 한 클래스로 생성할 수 있게 계획했다.
    볼과 스트라이크의 합산을 먼저 구한 다음, 스트라이크만 구하는 메서드를 하나 더 구현할 계획이다.
    countSumOfBallAndStrike()의 반환값에서 countStrike()의 반환값을 빼주면 자동적으로 볼의 개수가 된다.

---
+++ src/main/java/baseball/BallStrikeChecker.java
@@ -0,0 +1,19 @@
+package baseball;
+
+public class BallStrikeChecker {  // 이 클래스에서 모든 판단을 끝내는걸로?
+
+    public int countSumOfBallAndStrike(String randomNumber, String playerNumber) {
+        int sumOfBallAndStrike = 0;
+        for (int i = 0; i < 3; i++) {
+            if (randomNumber.contains(playerNumber.substring(i, i+1))) { // playerNumber.charAt(i)은 왜 안되지?
+                sumOfBallAndStrike++;
+            }
+        }
+        return sumOfBallAndStrike;
+    }
+
+    public int countStrike() {
+
+    }
+
+}
