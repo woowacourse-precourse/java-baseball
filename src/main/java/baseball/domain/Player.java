c92b79c013347a338ab6612bde517b49ad6ce677
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-06 23:28:02 +0900

    refactor(Player): 사용자 숫자 입력 기능을 Player 객체로 캡슐화
    - 숫자 입력 기능
    - 숫자 검증 기능
    - 입력받은 숫자 문자열을 정수 배열로 반환하는 기능

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,36 @@
+package baseball.domain;
+
+import baseball.exception.NumberExceptionUtils;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class Player {
+
+    private List<Integer> balls;
+
+    public void setInputBall() {
+        String input = Console.readLine();
+        validateBall(input);
+        balls = mapStringToIntegerList(input);
+    }
+
+    public List<Integer> getBall() {
+        return new ArrayList<>(balls);
+    }
+
+    private void validateBall(String number) {
+        NumberExceptionUtils.isPositiveDigits(number);
+        NumberExceptionUtils.isThreeDigits(number);
+        NumberExceptionUtils.isNotDuplicateNumber(number);
+    }
+
+    private static List<Integer> mapStringToIntegerList(String input) {
+        return Stream.of(input.split(""))
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+    }
+}
