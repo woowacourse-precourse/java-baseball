77c6e1cbc23999d5f647d9ca3b46baa5dc783dd3
penrose15 <seongju99@naver.com>
2022-11-06 22:21:42 +0900

    test : add playerNumber test about player entered
    Resolves: #17

---
+++ src/test/java/baseball/PlayerNumberTest.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import baseball.number.client.PlayerNumber;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PlayerNumberTest {
+
+    PlayerNumber playerNumber = new PlayerNumber();
+
+    @Test
+    void 사용자는_세게의_숫자를_입력해야_한다() {
+        List<Integer> expectNumber = List.of(2,3,4);
+        List<Integer> actualNumber = playerNumber.playerNumberToList("234");
+
+        assertThat(expectNumber).isEqualTo(actualNumber);
+    }
+}
