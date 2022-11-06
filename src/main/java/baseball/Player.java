d6082987d7350859a9514ddca227cc14f0323a14
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2022-11-06 21:25:09 +0900

    feat(Player): add the function to guess Answer

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -2,8 +2,12 @@ package baseball;
 
 import java.util.*;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Player {
     protected static List<Integer> Answer;
+
+    public Player() {}
 
     public static boolean isInteger(String s){
         try  {
@@ -37,6 +41,23 @@ public class Player {
             result = false;
         }
         return result;
+    }
+
+    public static void isValidAnswer(String input){
+        if(input.length() != 3 || !isInteger(input) || !isPositiveNumber(input) || !isDifferentNumbers(changeStringtoList(input))){
+            throw new IllegalArgumentException("게임종료");
+        }
+    }
+    public static void guessAnswer() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        try {
+            isValidAnswer(input);
+            Answer = changeStringtoList(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+
     }
 
 
--- src/test/java/baseball/FunctionTest.java
+++ src/test/java/baseball/FunctionTest.java
@@ -57,4 +57,25 @@ public class FunctionTest {
         assertThat(result).isEqualTo(false);
     }
 
+    @Test
+    void isValidAnswer_메서드_사용시_입력받은_문자열이_조건을_만족하지_못할시_예외_발생() {
+        String input = "1234";
+
+        assertThatThrownBy(() -> isValidAnswer(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("게임종료");
+    }
+
+    @Test
+    void guessAnswer_정답_예측_메소드(){
+        Player player = new Player();
+        String input = "123";
+        try {
+            isValidAnswer(input);
+            player.Answer = changeStringtoList(input);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        assertThat(player.Answer).isEqualTo(List.of(1,2,3));
+    }
 }
