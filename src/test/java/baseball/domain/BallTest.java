ae2360d17392d3a533bc52720645670a54885975
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-11-07 03:38:55 +0900

    feat(2단계) : Balls 예외처리 구현 및 테스트 완료

--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -8,10 +8,9 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class BallTest {
-
+    @DisplayName("예외처리 테스트")
     @ParameterizedTest
     @ValueSource(ints = {0,10})
-    @DisplayName("예외처리 테스트")
     public void rangeTest() {
         assertThatThrownBy(() -> {
             new Ball(0);
--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -1,4 +1,31 @@
 package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
 public class BallsTest {
+    private static Stream<Arguments> generateArgumentsStream() {
+        List<Arguments> listOfArguments = new LinkedList<>();
+        listOfArguments.add(Arguments.of(List.of(1,2)));
+        listOfArguments.add(Arguments.of(List.of(1,2,3,4)));
+        listOfArguments.add(Arguments.of(List.of(1,1,2)));
+        return listOfArguments.stream();
+    }
+    @DisplayName("예외처리 테스트")
+    @ParameterizedTest
+    @MethodSource("generateArgumentsStream")
+    public void exceptionTest(List<Integer> balls) {
+        assertThatThrownBy(() -> {
+            new Balls(balls);
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 
 }
