f3743ffc08a8aee4c48b9b682830add8c54656ff
egoing <dudqk9696@naver.com>
2022-11-06 22:30:44 +0900

    feat : View class 문자열 상수 출력해주는 메소드 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -4,4 +4,16 @@ public class InputView {
     public static final String GAME_START = "숫자 야구 게임을 시작합니다.";
     public static final String INSERT_NUMBER = "숫자를 입력해주세요 : ";
     public static final String RESTART_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static void gameStart() {
+        System.out.println(GAME_START);
+    }
+
+    public static void insertNumber() {
+        System.out.println(INSERT_NUMBER);
+    }
+
+    public static void restartGame() {
+        System.out.println(RESTART_GAME);
+    }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,4 +5,16 @@ public class OutputView {
     public static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String THREESTRIKE = "3스트라이크";
     public static final String NOTHING = "낫싱";
+
+    public static void gameEnd() {
+        System.out.println(GAME_END);
+    }
+
+    public static void threeStrike() {
+        System.out.println(THREESTRIKE);
+    }
+
+    public static void nothing() {
+        System.out.println(NOTHING);
+    }
 }
