1afb5b891fb494e72876060449fb2887f43c7449
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-06 22:43:38 +0900

    refactor(Computer): 메소드의 접근제한자를 public에서 private으로 수정(기능목록1-2)

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -10,7 +10,7 @@ public class Computer {
 
     public Computer(){}
 
-    public List<Integer> createRandomNumber() {
+    private List<Integer> createRandomNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
