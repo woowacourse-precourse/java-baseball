2962ecc664d190b1ad83a3f7be901df87a1776b8
leesky <leehaneul0623@gmail.com>
2022-11-06 22:17:29 +0900

    feat(BaseballGame) : 구현 예정 오퍼레이션 시그니처 생성
    추상 클래스 Game에서 구현을 요구하는 오퍼레이션 시그니처 생성 :
     - greet 시그니처 생성
     - play 시그니처 생성
     - close 시그니처 생성
     - restart 시그니처 생성
     - extends Game 키워드 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,4 +1,23 @@
 package baseball;
 
-public class BaseballGame {
+public class BaseballGame extends Game {
+    @Override
+    protected void greet() {
+
+    }
+
+    @Override
+    protected void play() {
+
+    }
+
+    @Override
+    protected void close() {
+
+    }
+
+    @Override
+    protected boolean restart() {
+        return false;
+    }
 }
