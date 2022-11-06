e794c7ddc2de937d3607441216896530954fbec6
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-07 00:41:02 +0900

    refactor(BallReaderTest): BallReaderTest가 Ball기반으로 동작하게끔 변경

--- src/test/java/baseball/testcase/BallReaderTest.java
+++ src/test/java/baseball/testcase/BallReaderTest.java
@@ -1,12 +1,11 @@
 package baseball.testcase;
 
+import baseball.game.Ball;
 import baseball.game.BallReader;
 import camp.nextstep.edu.missionutils.Randoms;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -19,8 +18,11 @@ public class BallReaderTest {
     @DisplayName("BallReader가 제대로 결과값을 반환하는지 테스트")
     @RepeatedTest(5)
     void BALLREADER_결과값_테스트() {
-        List<Integer> firstBall = Randoms.pickUniqueNumbersInRange(1, 9, 3);
-        List<Integer> secondBall = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        Ball firstBall = new Ball(Randoms.pickUniqueNumbersInRange(1, 9, 3));
+        Ball secondBall = new Ball(Randoms.pickUniqueNumbersInRange(1, 9, 3));
+
+        List<Integer> firstBallData = firstBall.getBallData();
+        List<Integer> secondBallData = secondBall.getBallData();
 
         Map<String, Integer> result = ballReader.getResult(firstBall, secondBall);
         System.out.println("firstBall = " + firstBall);
@@ -34,8 +36,8 @@ public class BallReaderTest {
     @DisplayName("BallReader는 호환되지 않는 공을 입력한다면 예외를 발생시킨다.")
     @Test
     void BALLREADER_예외_테스트() {
-        List<Integer> firstBall = Randoms.pickUniqueNumbersInRange(1, 9, 3);
-        List<Integer> errorBall = List.of(1, 2, 3, 4);
+        Ball firstBall = new Ball(Randoms.pickUniqueNumbersInRange(1, 9, 3));
+        Ball errorBall = new Ball(List.of(1, 2, 3, 4));
 
         assertThatThrownBy(() -> ballReader.getResult(firstBall, errorBall)).isInstanceOf(IllegalArgumentException.class);
     }
@@ -43,8 +45,9 @@ public class BallReaderTest {
     @DisplayName("BallReader가 3스트라이크를 일 때 게임이 끝났다 판단하는지 판독")
     @RepeatedTest(5)
     void BALLREADER_판독_결과가_참인_경우_테스트() {
-        List<Integer> firstBall = Randoms.pickUniqueNumbersInRange(1,9,3);
-        List<Integer> secondBall = new ArrayList<>(firstBall);
+        List<Integer> ballData = Randoms.pickUniqueNumbersInRange(1,9,3);
+        Ball firstBall = new Ball(ballData);
+        Ball secondBall = new Ball(ballData);
 
         Map<String, Integer> result = ballReader.getResult(firstBall, secondBall);
         System.out.println("result = " + result);
@@ -56,11 +59,11 @@ public class BallReaderTest {
     @DisplayName("BallReader가 3스트라이크를 아닐 때 게임이 끝나지 않았다 판단하는지 판독")
     @RepeatedTest(5)
     void BALLREADER_판독_결과가_거짓인_경우_테스트() {
-        List<Integer> firstBall = Randoms.pickUniqueNumbersInRange(1,9,3);
-        List<Integer> secondBall = Randoms.pickUniqueNumbersInRange(1,9,3);
+        Ball firstBall = new Ball(Randoms.pickUniqueNumbersInRange(1,9,3));
+        Ball secondBall = new Ball(Randoms.pickUniqueNumbersInRange(1,9,3));
 
         while(ballReader.isFinished(firstBall,secondBall)){
-            secondBall = Randoms.pickUniqueNumbersInRange(1,9,3);
+            secondBall = new Ball(Randoms.pickUniqueNumbersInRange(1,9,3));
         }
 
         Map<String, Integer> result = ballReader.getResult(firstBall, secondBall);
