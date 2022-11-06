0d0403a8951610f4be68558e0be750cac5d9f682
abbd2 <ajkety00@gmail.com>
2022-11-07 02:56:45 +0900

    feat(RandomNumber): 난수를 생성하는 메소드 생성

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -1,4 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class RandomNumber {
+
+    NumberVO numberVO = new NumberVO();
+    public void createRandomNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        numberVO.setRandomNumber(computer);
+        System.out.println("난수:"+numberVO.getRandomNumber()+":");
+
+    }
+
 }
