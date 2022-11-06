8d7ebf56b551c5ba9bd1459da79e57c6ed4db5c3
saint6839 <saint6839@gmail.com>
2022-11-07 00:31:26 +0900

    feat(model) : 게임 재시작, 종료 여부를 입력 받을때, 입력 값에 대해 유효성을 검증하도록 구현

--- src/main/java/baseball/model/GameStatus.java
+++ src/main/java/baseball/model/GameStatus.java
@@ -1,6 +1,8 @@
 package baseball.model;
 
 public class GameStatus {
+    public static final String INVALID_GAME_STATUS_INPUT_VALUE_MESSAGE = "게임 상태는 재시작:1, 종료:2 중에서 입력해야 합니다";
+    private static final String INVALID_INPUT_TYPE_MESSAGE = "숫자를 입력해주세요.";
     int status;
 
     public GameStatus(int status) {
@@ -11,11 +13,25 @@ public class GameStatus {
         return status == 1;
     }
 
-    public boolean isValid() {
-        return status == 1 || status == 2;
+    public void validateGameStatusValue(int status) {
+        if (status == 1 || status == 2) {
+            return;
+        }
+        throw new IllegalArgumentException(INVALID_GAME_STATUS_INPUT_VALUE_MESSAGE);
     }
 
-    public void changeStatus(int status) {
-        this.status = status;
+    public void changeStatus(String playerInput) {
+        validateGameStatusValue(status);
+        validateParseStringToInt(playerInput);
+
+        this.status = Integer.parseInt(playerInput);
+    }
+
+    private void validateParseStringToInt(String playerInput) {
+        try {
+            Integer.parseInt(playerInput);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(INVALID_INPUT_TYPE_MESSAGE);
+        }
     }
 }
