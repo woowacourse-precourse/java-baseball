b19197856da5bc3a4fcfb862d32f53738e3a3550
JungWoo <rlawjddn103@naver.com>
2022-11-06 21:53:09 +0900

    feat(InputNumber): 사용자에게 재시작을 위한 한자리 수를 입력받고 검증하는 기능 구현

--- src/main/java/baseball/InputNumber.java
+++ src/main/java/baseball/InputNumber.java
@@ -7,7 +7,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputNumber {
     static final int GAME_NUMBER_LENGTH = 3;
+    static final int RESTART_NUMBER_LENGTH = 1;
     static final Pattern NUMBER_PATTERN = Pattern.compile("^[0-9]*$");
+    static final Pattern RESTART_NUMBER_PATTERN = Pattern.compile("^[1-2]*$");
 
     public String inputGameNumber() {
         String inputNumber = Console.readLine();
@@ -17,6 +19,29 @@ public class InputNumber {
         }
 
         return inputNumber;
+    }
+
+    public String inputRestartNumber() {
+        String inputNumber = Console.readLine();
+
+        if (!restartNumberExceptionCheck(inputNumber)) {
+            throw new IllegalArgumentException();
+        }
+
+        return inputNumber;
+    }
+
+    /**
+     * @param inputNumber 사용자가 입력한 변수
+     *                    사용자가 입력한 변수가 1글자여야만 하고,
+     *                    1,2만을 받아들여야만 하여 정규식을 사용하여 예외처리를 해줌.
+     */
+    private boolean restartNumberExceptionCheck(String inputNumber) {
+        if (inputNumber.length() == RESTART_NUMBER_LENGTH) {
+            Matcher matchNum = RESTART_NUMBER_PATTERN.matcher(inputNumber);
+            return matchNum.find();
+        }
+        return false;
     }
 
     /**
