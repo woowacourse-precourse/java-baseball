f566e391fa02aff3ecd4e73e0ec51c38b494423a
xxeol2 <jhj01177@naver.com>
2022-11-06 18:18:58 +0900

    refactor: 숫자 범위 및 길이 상수화

---
+++ src/main/java/constant/Rules.java
@@ -0,0 +1,8 @@
+package constant;
+
+public class Rules {
+
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 9;
+    public static final int NUMBER_LENGTH = 3;
+}
