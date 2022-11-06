4e9a6ede9f3f026dadf33160687defe2ac089895
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-06 16:48:18 +0900

    feat: User 변수 추가
    입력값을 담을 List 추가

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -1,7 +1,13 @@
 package baseball.domain;
 
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private static List<Integer> selectNumber = new ArrayList<>();
 
 
 }
