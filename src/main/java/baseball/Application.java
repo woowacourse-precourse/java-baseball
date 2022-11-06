34261d09ecd7b6d90dc547583cfcb4c605e7dceb
qkrdmswl <eunji303@naver.com>
2022-11-06 19:38:03 +0900

    refactor(variable) : rename opponentNum  to computerNum

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -24,7 +24,7 @@ public class Application {
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
             if (Integer.parseInt(Console.readLine()) == 1) {
-                numBaseball(opponentNum());
+                numBaseball(computerNum());
             }
             return;
         }
@@ -64,7 +64,7 @@ public class Application {
     }
 
 
-    public static List<Integer> opponentNum() {
+    public static List<Integer> computerNum() {
         List<Integer> computerNum = new ArrayList<>();
         while (computerNum.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -77,6 +77,6 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        numBaseball(opponentNum());
+        numBaseball(computerNum());
     }
 }
