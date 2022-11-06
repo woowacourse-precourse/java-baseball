1d05f3dd2f8771950a2ef92efcdb2fbf925d6f05
Be-student <presentsong@naver.com>
2022-11-07 00:57:07 +0900

    feat(OutputView): 콘솔 출력 기능 추가

---
+++ src/main/java/baseball/view/console/ConsoleOutput.java
@@ -0,0 +1,11 @@
+package baseball.view.console;
+
+import baseball.view.OutputView;
+
+public class ConsoleOutput implements OutputView {
+
+    @Override
+    public void print(String content) {
+        System.out.println(content);
+    }
+}
