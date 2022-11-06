5270a4f39eeb3d496c6f4a5542127c3f1fdab16a
YongGwan <32181771@dankook.ac.kr>
2022-11-06 20:42:10 +0900

    random한 3자리 값 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        List<Integer> randomValues = makeRandomValues();
+        System.out.println(randomValues);
     }
+
+    private static List<Integer> makeRandomValues() {
+
+        List<Integer> randomValues = new ArrayList<>();
+        while (randomValues.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomValues.contains(randomNumber)) {
+                randomValues.add(randomNumber);
+            }
+        }
+        return randomValues;
+    }
+
+
 }
