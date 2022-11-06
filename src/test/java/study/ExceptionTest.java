9b669d5279e32298e8e622689052afb0b1a09315
park <pjh971212@gmail.com>
2022-11-07 01:58:06 +0900

    test: 숫자가 아닌 문자를 입력 테스트

--- src/test/java/study/ExceptionTest.java
+++ src/test/java/study/ExceptionTest.java
@@ -14,4 +14,12 @@ public class ExceptionTest {
         assertThat(CheckException.checkNumberLength(input_false)).isEqualTo(false);
     }
 
+    @Test
+    void check_입력값_숫자_예외검증() {
+        String input_true = "123";
+        String input_false = "asdf";
+        assertThat(CheckException.checkNumeric(input_true)).isEqualTo(true);
+        assertThat(CheckException.checkNumeric(input_false)).isEqualTo(false);
+    }
+
 }
