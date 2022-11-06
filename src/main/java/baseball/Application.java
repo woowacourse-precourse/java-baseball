3f535ba1e0ed79bb78d19d7e5bce45b2a45d69e8
jootang2 <joohwan.song15b@gmail.com>
2022-11-06 20:54:16 +0900

    :sparkles: [기능5] : 3개의 숫자를 모두 맞춘 경우 게임 종료 여부 선택

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -29,6 +29,10 @@ public class Application {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 flag = false;
             }
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String number = Console.readLine();
+            continueOrFinish(number);
         }
     }
 
@@ -63,6 +67,14 @@ public class Application {
             return String.format("%d스트라이크", strike);
         } else {
             return String.format("%d볼 %d스트라이크", ball, strike);
+        }
+    }
+
+    public static void continueOrFinish(String number) {
+        if (number.equals("1")) {
+            baseball();
+        } else if (number.equals("2")) {
+            System.out.println("게임을 종료합니다");
         }
     }
 
