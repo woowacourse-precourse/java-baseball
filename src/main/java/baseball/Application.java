0b31f15776847b4ddf4757c0995ef3bbf0e27f21
BaeDongHyeon <sasd730@gmail.com>
2022-11-07 01:57:45 +0900

    feat: 1이면 다시시작, 2이면 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -24,6 +24,14 @@ public class Application {
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 restartOrEnd = Integer.parseInt(Console.readLine());
             }
+            if (restartOrEnd == 1) {
+                computerNumber = getComputerNumber();
+                restartOrEnd = 0;
+                ballStrikeCount.clear();
+            }
+            if (restartOrEnd == 2) {
+                break;
+            }
         }
     }
 
