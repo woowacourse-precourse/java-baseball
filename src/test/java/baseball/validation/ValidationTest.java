86bf8da78990a2f129ebc93fbc979e683c45c64f
SeokHwan An <asok369258@naver.com>
2022-11-07 01:24:34 +0900

    test : 플레이어가 제시한 숫자가 서로다른 수로 구성되어 있는지 검증하는 기능 테스트

--- src/test/java/baseball/validation/ValidationTest.java
+++ src/test/java/baseball/validation/ValidationTest.java
@@ -36,4 +36,18 @@ class ValidationTest {
         assertThat(validation.checkAnswerConsistOfNum(answer)).isEqualTo(false);
     }
 
+    @Test
+    void 플레이어가_제시한_숫자가_서로_다른_숫자로_구성되어있는지_검증하는_기능_true반환_테스트() {
+        String answer = "123";
+
+        assertThat(validation.checkAnswerOverlap(answer)).isEqualTo(true);
+    }
+
+    @Test
+    void 플레이어가_제시한_숫자가_서로_다른_숫자로_구성되어있는지_검증하는_기능_false반환_테스트() {
+        String answer = "121";
+
+        assertThat(validation.checkAnswerOverlap(answer)).isEqualTo(false);
+    }
+
 }
