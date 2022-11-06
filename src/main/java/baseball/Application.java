6b4f823420f300b0d053530091c1dd2cb895cfa9
whrod <whrodtube@gmail.com>
2022-11-06 17:08:39 +0900

    Refactor : inputNumberOfUser method refactor
    예외 사항 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,19 +3,20 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println(generateNumbers());
-        System.out.println(inputNumberOfUser());
-//        if (inputNumberOfUser() == generateNumbers()) {
-//            System.out.println("right");
-//        } else {
-//        }
-//        System.out.println("wrong");
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        if(generateNumbers().get(1) == inputNumberOfUser().get(1)) {
+            System.out.println("right");
+        } else {
+            System.out.println("wrong");
+        }
     }
 
     public static List generateNumbers() {
@@ -26,20 +27,35 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+        System.out.println(computer);
         return computer;
     }
 
-    public static List inputNumberOfUser() {
+    public static ArrayList inputNumberOfUser() {
         String input = Console.readLine();
         int inputNumber = Integer.parseInt(input);
-            if((int)(Math.log10(inputNumber)+1) == 3) {
-                System.out.println("숫자를 입력해주세요: " + input);
-                return Collections.singletonList(input);
-            } else
-                throw new IllegalArgumentException("잘못된 입력값입니다.");
-    }
-}
+        List<String> stringNumberList = List.of(input.split(""));
+        List<Integer> inputNumberList = stringNumberList.stream()
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
+        ArrayList<Integer> inputNumberArrayList = new ArrayList<>(inputNumberList);
+        Set<Integer> inputNumberSet = new HashSet<>(inputNumberList);
+        long count = stringNumberList.stream().filter("0"::equals).count();
 
-//    public static String checkNumbers() {
+        if((int)(Math.log10(inputNumber)+1) == 3 && inputNumberSet.size() == 3 && count == 0) {
+            System.out.println("숫자를 입력해주세요: " + input);
+            System.out.println(inputNumberList);
+            return inputNumberArrayList;
+        } else
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+    }
+
+//    public static void countStrike() {
+//        int i = 0;
+//        int strike = 0;
+//        while(true) {
 //
+//            }
+//        }
 //    }
+}
