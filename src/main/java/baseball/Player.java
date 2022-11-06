7f69e229b64d4356460a2c1bccb4d8e8d5739a41
sysliar <sysliar@gmail.com>
2022-11-06 18:06:05 +0900

    feat(Player): set numbers with player input

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,12 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
+import static java.lang.Integer.parseInt;
+
 public class Player {
+    private static final String NUMBER_SET_STRING = "숫자를 입력해주세요 : ";
     private static final int NUMBER_LENGTH = 3;
     private static final int NUMBER_RANGE_START = 1;
     private static final int NUMBER_RANGE_END = 9;
@@ -21,6 +25,28 @@ public class Player {
             }
         }
     }
+
+    public void setNumber() {
+        System.out.print(NUMBER_SET_STRING);
+        ArrayList<Integer> listOfNumbers = new ArrayList<Integer>(NUMBER_LENGTH);
+        String stringOfNumbers = Console.readLine();
+        number.addAll(stringToList(stringOfNumbers));
+        try {
+            checkNumbers(number);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+    }
+
+    private ArrayList<Integer> stringToList(String string) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (String str : string.split("")) {
+            list.add(parseInt(str));
+        }
+        return list;
+    }
+
     public boolean checkNumbers(ArrayList<Integer> listOfNumbers) {
         if (listOfNumbers.size() != NUMBER_LENGTH) {
             throw new IllegalArgumentException("세자리 수가 아닙니다.");
