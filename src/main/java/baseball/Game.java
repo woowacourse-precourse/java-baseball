7e95d7886d3223ef9af8b8f4378af67fed1915fa
kjg1514 <kjg1514@naver.com>
2022-11-07 04:48:21 +0900

    feat : 재시작 or 게임종료기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,24 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.List;
 
 public class Game {
 
-    private int strike = 0;
-    private int ball = 0;
+    private int strike;
+    private int ball;
 
-    List<Integer> comNum;
-    List<Integer> userNum;
+    private List<Integer> comNum;
+    private List<Integer> userNum;
+
 
 
     public void playGame() {
         comNum = ComputerNumber.getComputerNumber();
+        System.out.println(comNum);
+        System.out.println("숫자를 입력해주세요 : ");
         userNum = User.userInputNumber();
         cnt_StrikeOrBall();
         System.out.println(strike_ball_nothing_Message());
     }
 
     public void cnt_StrikeOrBall() {
+        this.strike = 0;
+        this.ball = 0;
         for (int i = 0; i < userNum.size(); i++) {
             if (comNum.indexOf(userNum.get(i)) == i) {
                 strike++;
@@ -48,6 +55,24 @@ public class Game {
             message = ("낫싱");
             return message;
         }
+    }
+
+    public boolean restartOrEnd() {
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하시려면 1, 종료하려면 2를 입력하세요.");
+
+            String answer = Console.readLine();
+            if (answer.equals("1")) {
+                playGame();
+                return true;
+            }
+            else if (answer.equals("2")) {
+                return false;
+            }
+            throw new IllegalArgumentException("올바른 숫자가 아닙니다. 1 또는 2를 입력해주세요");
+        }
+        return true;
     }
 
 }
