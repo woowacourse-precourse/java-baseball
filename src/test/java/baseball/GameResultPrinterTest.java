09ee27a05ef085187615e38e17cce33888207d95
choiejae <ejae8319@gmail.com>
2022-11-06 20:17:04 +0900

    test: GameResultPrinter 스트라이크 확인

--- src/test/java/baseball/GameResultPrinterTest.java
+++ src/test/java/baseball/GameResultPrinterTest.java
@@ -5,8 +5,12 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class GameResultPrinterTest {
     private GameResultPrinter printer;
 
@@ -14,8 +18,17 @@ class GameResultPrinterTest {
     void setUp() {
         printer = new GameResultPrinter();
     }
+    @Order(1)
     @Test
     void NothingResultPrinter() {
-        assertEquals(printer.convertGameResult(new ArrayList<>(Arrays.asList(0, 0, 0))), "낫싱");
+        assertEquals(printer.convertGameResult(new ArrayList<>(Arrays.asList(0, 0, 1))), "낫싱");
+    }
+
+    @Order(2)
+    @Test
+    void StrikeResultPrinter() {
+        assertEquals(printer.convertGameResult(new ArrayList<>(Arrays.asList(1, 0, 0))), "1 스트라이크");
+        assertEquals(printer.convertGameResult(new ArrayList<>(Arrays.asList(2, 0, 0))), "2 스트라이크");
+        assertEquals(printer.convertGameResult(new ArrayList<>(Arrays.asList(3, 0, 0))), "3 스트라이크");
     }
 }
