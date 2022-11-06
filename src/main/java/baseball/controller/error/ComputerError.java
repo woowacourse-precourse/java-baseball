008eebf0b9ffda38fbcf4d1261dca988bd4ae521
renechoi90@gmail.com <renechoi90@gmail.com>
2022-11-06 16:37:27 +0900

    refactor(computer error) : 중복룰 검증에 대한 메소드 기능 분화

--- src/main/java/baseball/controller/error/ComputerError.java
+++ src/main/java/baseball/controller/error/ComputerError.java
@@ -11,7 +11,12 @@ public class ComputerError {
 		return Objects.equals(randomNumber.size(), Integer.parseInt(NUMBER_LENGTH.getValue()));
 	}
 
+	public static boolean followDuplicationRule(List<Integer> randomNumber, int createdNumber) {
+		return Boolean.parseBoolean(DUPLICATION_ALLOWABLE.getValue()) == hasDuplication(randomNumber, createdNumber);
+	}
+
 	public static boolean hasDuplication(List<Integer> randomNumber, int createdNumber) {
 		return randomNumber.contains(createdNumber);
 	}
+
 }
