e67be5f192ba240188c4a80a4b9b88075ceb1277
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-06 23:59:50 +0900

    feat(Game): add function lists in Game class

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,46 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+
+    public static void run() {
+
+    }
+
+    private static void play() {
+
+    }
+
+    private static Integer inputTotalNumber() {
+        return 1;
+    }
+
+
+    private static boolean inputPlayGame() {
+        return true;
+    }
+
+    private static List<Integer> checkOneDigitNumberCount(Integer number, Integer index, List<Integer> secretNumber, List<Integer> hitCount) {
+        return new ArrayList<>();
+    }
+
+
+    private static List<Integer> checkTotalNumberCount(Integer totalNumber, List<Integer> secretNumber) {
+        return new ArrayList<>();
+    }
+
+    private static List<Integer> initHitCount() {
+        return new ArrayList<>();
+    }
+
+    private static void showHitCount(List<Integer> hitCount) {
+
+    }
+
+    private static boolean checkWin(List<Integer> hitCount) {
+        return true;
+    }
 
 }
