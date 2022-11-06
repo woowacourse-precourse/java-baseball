2abb8be066e7b5c66991e9c36035e86246b15e42
Park shinyeong <syngh503@gmail.com>
2022-11-06 22:05:16 +0900

    Feat(setHint): (기능4) 상대방 수와 플레이어 수 비교 기능 구현
    상대방의 수와 플레이어의 수를 비교하는 setHint() 함수를 작성하였습니다.
    힌트는 해시맵으로 구현하였으며 스트라이크, 볼, 낫싱 이라는 키 값을 가지고 있습니다.
    스트라이크인 경우 스트라이크 값이
    볼인 경우 볼 값이
    낫싱인 경우 낫싱 값이 하나씩 더해집니다.
    
    Relate to: #7

--- src/main/java/baseball/GameHost.java
+++ src/main/java/baseball/GameHost.java
@@ -4,7 +4,15 @@ import java.util.*;
 import java.util.stream.Stream;
 
 public class GameHost extends BaseballNumber {
-    private Map<String, Integer> hint = new HashMap<>();
+    private final String STRIKE = "스트라이크";
+    private final String BALL = "볼";
+    private final String NOTHING = "낫싱";
+    private final Map<String, Integer> hint = new HashMap<>() {{
+        put(STRIKE, 0);
+        put(BALL, 0);
+        put(NOTHING, 0);
+    }};
+
 
     @Override
     public boolean isValidBaseballNumber(List<Integer> baseballNumber) {
@@ -20,14 +28,27 @@ public class GameHost extends BaseballNumber {
     }
 
     public void setHint(List<Integer> playerNumber) {
+        for(int i = 0; i < super.BASEBALLNUMBERLENGTH; i++) {
+            int playerNum = playerNumber.get(i);
+            int baseballNum = this.baseballNumber.get(i);
 
+            if(playerNum == baseballNum) {
+                this.hint.put(STRIKE, this.hint.get(STRIKE) + 1);
+                continue;
+            }
+            if(this.baseballNumber.contains(playerNum)) {
+                this.hint.put(BALL, this.hint.get(BALL) + 1);
+                continue;
+            }
+            this.hint.put(NOTHING, this.hint.get(NOTHING) + 1);
+        }
     }
 
     public void setBaseballNumber() {
         Set<Integer> checkValidNumber = new HashSet<>();
         Random random = new Random();
 
-        while(checkValidNumber.size() != 3) {
+        while (checkValidNumber.size() != 3) {
             int randomNum = random.nextInt(9) + 1;
             checkValidNumber.add(randomNum);
         }
