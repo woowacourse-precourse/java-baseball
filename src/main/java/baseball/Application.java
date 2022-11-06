f863d3f475c4176c2c881da65e278503976131fb
suhwan <popomarin28@gmail.com>
2022-11-06 15:14:40 +0900

    refactor : 낭비한 줄바꿈 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,5 @@
 package baseball;
 
-
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
--- src/main/java/baseball/GameSet.java
+++ src/main/java/baseball/GameSet.java
@@ -1,10 +1,8 @@
 package baseball;
 
-
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
-
 
 public class GameSet {
     private List<Integer> computerAnswer;
