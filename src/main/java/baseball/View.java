8ad78edd8beacccf0dcdcd3eeb0164d95ccd5db4
BuildTools <unconfigured@null.spigotmc.org>
2022-11-07 01:52:10 +0900

    refactor: 수정 출력함수final제거
    static이라 final선언안해도 변경이 일어날리 없다고 판단

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -3,39 +3,39 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class View {
-    public static final void printStartGame(){
+    public static void printStartGame(){
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static final void printChoiceEndOrNewGame(){
+    public static void printChoiceEndOrNewGame(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    public static final void printPleaseInputNumber(){
+    public static void printPleaseInputNumber(){
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public static final void printStrike(int num){
+    public static void printStrike(int num){
         System.out.println(num + "스트라이크");
     }
 
-    public static final void printBall(int num){
+    public static void printBall(int num){
         System.out.println(num + "볼");
     }
 
-    public static final void printBallAndStrike(int ball, int strike){
+    public static void printBallAndStrike(int ball, int strike){
         System.out.println(ball + "볼 " + strike + "스트라이크");
     }
 
-    public static final void printNothing(){
+    public static void printNothing(){
         System.out.println("낫싱");
     }
 
-    public static final void printCollect(){
+    public static void printCollect(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public static final String getUserInput() {
+    public static String getUserInput() {
         return Console.readLine();
     }
 }
