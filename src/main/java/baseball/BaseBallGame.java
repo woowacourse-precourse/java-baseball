0831497f2d8140275a6008c91f5fb79a5eeb15e2
Cho SangHyun <98shcho@naver.com>
2022-11-06 19:30:31 +0900

    feat(게임 시작 기능): add feature to set computerNumbers

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,13 +1,32 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 public class BaseBallGame {
+    public static final int COMPUTER_NUMBERS_SIZE = 3;
+    public static final int COMPUTER_NUMBER_MIN = 1;
+    public static final int COMPUTER_NUMBER_MAX = 9;
+
     GameMessageDisplay gameMessageDisplay;
+    List<Integer> computerNumbers;
 
     public BaseBallGame() {
         this.gameMessageDisplay = new GameMessageDisplay();
+        this.computerNumbers = new ArrayList<>();
     }
 
     public void startGame() {
         this.gameMessageDisplay.printGameStartMessage();
+        setComputerNumbers();
+    }
+
+    public void setComputerNumbers() {
+        while (this.computerNumbers.size() < COMPUTER_NUMBERS_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(COMPUTER_NUMBER_MIN, COMPUTER_NUMBER_MAX);
+            if (!this.computerNumbers.contains(randomNumber)) {
+                this.computerNumbers.add(randomNumber);
+            }
+        }
     }
 }
