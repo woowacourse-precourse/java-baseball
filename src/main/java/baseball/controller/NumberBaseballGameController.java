8ed0a4910258d102e2673d02953e9381b0e202de
ksh-beginner <ohk9134@naver.com>
2022-11-06 22:40:34 +0900

    refactor(NumberBaseballGameController) : 서비스 생성 역할 분리로 인한 리팩토링
    Model에게 데이터를 받아서 로직을 처리하는 부분은 서비스로 넘기고,
    서비스에게 데이터를 받아 뷰로 데이터를 전달하는 부분만 컨트롤러가 담당하도록 리팩토링

--- src/main/java/baseball/controller/NumberBaseballGameController.java
+++ src/main/java/baseball/controller/NumberBaseballGameController.java
@@ -2,8 +2,11 @@ package baseball.controller;
 
 import baseball.RandomUtil;
 import baseball.model.OpponentComputer;
+import baseball.service.NumberBaseballGameService;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+
+import java.util.Map;
 
 public class NumberBaseballGameController {
 
@@ -11,12 +14,25 @@ public class NumberBaseballGameController {
     private static final String STRIKE_AND_BALL_COUNT_MAP_BALL_KEY = "Ball";
     private static final int THREE_STRIKE_COUNT = 3;
     private static final String GAME_RESTART_NUMBER = "1";
+    private static final String GAME_END_NUMBER = "2";
+    private static final String GAME_RESTART_STATUS = "play";
+    private static final String GAME_END_STATUS = "end";
 
+    private static String gameStatus = GAME_RESTART_STATUS;
 
-    private static OpponentComputer opponentComputer;
+    NumberBaseballGameService numberBaseballGameService = new NumberBaseballGameService();
 
-    public void generateOpponentComputer() {
-        opponentComputer = new OpponentComputer(RandomUtil.createRandomNumber());
+    public void run() {
+        while (gameStatus.equals(GAME_RESTART_STATUS)) {
+            gameInit();
+            gameStart();
+            gamePlay();
+            gameRestartOrEnd();
+        }
+    }
+
+    public void gameInit() {
+        numberBaseballGameService.generateOpponentComputer();
     }
 
     public void gameStart() {
@@ -24,25 +40,24 @@ public class NumberBaseballGameController {
     }
 
     public void gamePlay() {
-        while(opponentComputer.getStrikeAndBallCountMap().get(STRIKE_AND_BALL_COUNT_MAP_STRIKE_KEY) != THREE_STRIKE_COUNT) {
+        while (numberBaseballGameService.getStrikeCount() != THREE_STRIKE_COUNT) {
             String playerNumber = InputView.inputNumber();
-            opponentComputer.initStrikeAndBallCountMap();
-            opponentComputer.judgeStrikeOrBallCountOfPlayerNumber(Integer.parseInt(playerNumber));
-            Integer strikeCount = opponentComputer.getStrikeAndBallCountMap().get(STRIKE_AND_BALL_COUNT_MAP_STRIKE_KEY);
-            Integer ballCount = opponentComputer.getStrikeAndBallCountMap().get(STRIKE_AND_BALL_COUNT_MAP_BALL_KEY);
-            OutputView.printGameResultMessage(strikeCount, ballCount);
+            Map<String, Integer> strikeAndBallCountMap =
+                    numberBaseballGameService.judgeStrikeOrBallCount(playerNumber);
+            OutputView.printGameResultMessage(
+                    strikeAndBallCountMap.get(STRIKE_AND_BALL_COUNT_MAP_STRIKE_KEY),
+                    strikeAndBallCountMap.get(STRIKE_AND_BALL_COUNT_MAP_BALL_KEY)
+            );
         }
     }
 
     public void gameRestartOrEnd() {
         String restartOrEndNumber = InputView.inputRestartOrEndNumber();
         if (restartOrEndNumber.equals(GAME_RESTART_NUMBER)) {
-            generateOpponentComputer();
-            gamePlay();
+            gameStatus = GAME_RESTART_STATUS;
         }
-    }
-
-    public OpponentComputer getOpponentComputer() {
-        return opponentComputer;
+        if (restartOrEndNumber.equals(GAME_END_NUMBER)) {
+            gameStatus = GAME_END_STATUS;
+        }
     }
 }
