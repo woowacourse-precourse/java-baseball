a7e4ea372037c449257fccf9dadf2feb510fb770
brotherstone97 <brotherstone97@gmail.com>
2022-11-06 18:55:15 +0900

    Feat(isValidNumber): 사용자에게 입력받은 세자리의 수가 유효한지 검증
    입력에 문자가 섞이지는 않았는지, 서로 다른 세자리의 수를 입력했는지, 각 자릿수는 1이상 9이하인지 체크함.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,17 +6,46 @@ import java.util.Scanner;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-public class Application {
 
+public class Application {
     static int scanStartNumber() {
         String input = Console.readLine();
-        return Integer.parseInt(input);
+        if (isValidNumber(input)) {
+            return Integer.parseInt(input);
+        }
+        return -1;
     }
 
-
+    static boolean isValidNumber(String input) throws IllegalArgumentException {
+        int inputNumber;
+        try {
+            inputNumber = Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        if (99 > inputNumber || 999 < inputNumber) {
+            throw new IllegalArgumentException();
+        }
+        List<Integer> digits = new ArrayList<>();
+        int divisor = 100;
+        for (int i = 0; i < 3; i++) {
+            int digit = (int) (inputNumber / divisor);
+            if (digit == 0 || digits.contains(digit)) {
+                throw new IllegalArgumentException();
+            }
+            digits.add(digit);
+            inputNumber %= divisor;
+            divisor /= 10;
+        }
+        return true;
+    }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int userNumber = scanStartNumber();
+        try {
+            int startNumber = scanStartNumber();
+        } catch (IllegalArgumentException e) {
+            System.err.println(e);
+        }
     }
 }
