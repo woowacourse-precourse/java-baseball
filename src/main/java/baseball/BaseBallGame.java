9bfca2cce7314faf14856be2231393403fcde6e1
kimdeokyoung <dudejrss@naver.com>
2022-11-06 18:36:01 +0900

    feat (src.main.java.baseball.BaseBallGame) : 베이스볼 게임을 위한 클래스 생성 및 dummy 메소드 추가

---
+++ src/main/java/baseball/BaseBallGame.java
@@ -0,0 +1,52 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class BaseBallGame implements AutoCloseable {
+
+    State currentState;
+    List<Integer> AnswerNumber;
+
+    public BaseBallGame(int hashcode){
+
+    }
+
+    @Override
+    public void close() throws Exception {
+
+    }
+
+    public void run() {
+
+    }
+
+    private void generateNumber() {
+    }
+
+    private List<Integer> getUserINput() {
+        return Collections.emptyList();
+    }
+
+    private boolean validateUserInput() {
+        return false;
+    }
+
+    private List<Integer> getResultOfAnswer(List<Integer> challengeNumber) {
+        return Collections.emptyList();
+    }
+
+    private void printResult(List<Integer> result) {
+    }
+
+    private enum State {
+
+    }
+
+    private enum Trigger {
+
+    }
+
+
+}
