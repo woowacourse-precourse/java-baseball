1c41a55b5bf048c11fd03a89c3ad5591e63ac7f0
solesy <tkddbs9801@gmail.com>
2022-11-06 22:37:40 +0900

    feat(main): the computer has to generate the number that the user has to match

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class Application {
@@ -36,6 +41,16 @@ public class Application {
                 throw new IllegalArgumentException("userInput should be 3 digit number.");
             }
         }
+    }
+    private List<Integer> createComputerNumbers(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 
     public static void main(String[] args) {
