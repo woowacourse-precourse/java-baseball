f4a50372a9d2c86333e4753c0fcb75640d9240cc
Soseung Soo <sss9073@naver.com>
2022-11-06 19:21:58 +0900

    feat: add guessNumber & askRestart function to refactor startGame .

--- src/main/java/baseball/Controller/Executor.java
+++ src/main/java/baseball/Controller/Executor.java
@@ -4,30 +4,52 @@ import baseball.model.Baseball;
 import baseball.view.Printer;
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.InputMismatchException;
-import java.util.List;
+import java.sql.SQLOutput;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
+
 public class Executor {
-    private static Scanner scanner = new Scanner(System.in);
+    private static final int RESTART = 1;
+    private static final int EXIT = 2;
     private static Computer computer;
+    private static BaseballResult result;
     private static Checker checker = new Checker();
+    private static boolean flag = true;
     public static void startGame(){
         Baseball computerRandomBaseball = new Computer().createRandomBaseball();
-        Printer.start();
-        guessNumber(computerRandomBaseball);
-
+        Printer.gameStart();
+        while(flag){
+            Printer.inputNumber();
+            result = guessNumber(computerRandomBaseball);
+            if(result.isAllCorrect()){
+                String resultString = result.getResult();
+                System.out.println(resultString);
+                Printer.allCorrect();
+                askRestart();
+            }else{
+                String resultString = result.getResult();
+                System.out.println(resultString);
+            }
+        }
     }
-    public static void guessNumber(Baseball computerRandomBaseBall){
+    public static BaseballResult guessNumber(Baseball computerRandomBaseBall){
         Baseball userBaseball = new User().getInput();
-        Map<String, Integer> check_result = checker.start(computerRandomBaseBall, userBaseball);
-
+        HashMap<String, Integer> check_result = checker.start(computerRandomBaseBall, userBaseball);
+        BaseballResult baseballResult = new BaseballResult(check_result);
+        return baseballResult;
     }
-
-
-
-
-
-
+    public static void askRestart(){
+        Printer.askRestart();
+        String s = Console.readLine();
+        int reStart = Integer.valueOf(s);
+        if(reStart == RESTART){
+            startGame();
+        }else if(reStart != EXIT){
+            askRestart();
+        }else{
+            flag = false;
+        }
+    }
 }
