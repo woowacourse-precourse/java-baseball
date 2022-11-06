0fae059762f3c334972aa2f5e8a8d6a7dcbe29b9
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-07 00:57:36 +0900

    feat: 재시작 여부 입력시에 규칙에 맞지 않은 입력을 했을 경우를 검증하는 기능 추가

--- src/main/java/baseball/validator/GameInputValidator.java
+++ src/main/java/baseball/validator/GameInputValidator.java
@@ -1,7 +1,18 @@
 package baseball.validator;
 
+import static baseball.constant.Constants.*;
+
 public class GameInputValidator {
 
 	public static void validateRestartInput(String input) {
+		int inputParseInt;
+		try {
+			inputParseInt = Integer.parseInt(input);
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException();
+		}
+		if (!(inputParseInt == RESTART_GAME || inputParseInt == STOP_GAME)) {
+			throw new IllegalArgumentException();
+		}
 	}
 }
