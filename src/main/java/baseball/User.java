75036fde231579fc63b111bb030b9ef2bafca8d1
JunHee <ljh19980823@gmail.com>
2022-11-06 13:31:48 +0900

    feat : User가 숫자 3개를 입력하는 기능

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,5 +1,14 @@
 package baseball;
 
-public class User {
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
+public class User {
+    private String userInput;
+    private boolean flag;
+
+    public String getUserNumber() {
+        System.out.print("숫자를 입력해 주세요 : ");
+        String userInput = readLine();
+        return userInput;
+    }
 }
