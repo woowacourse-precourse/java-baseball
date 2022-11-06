48cd07db6adef3ea611b56dfc00abe6e6dcf7d65
UJ15 <dbwogml15@naver.com>
2022-11-06 19:58:44 +0900

    feat : implement run method in BaseballGame

--- src/main/java/baseball/engine/BaseBallGame.java
+++ src/main/java/baseball/engine/BaseBallGame.java
@@ -2,19 +2,44 @@ package baseball.engine;
 
 import baseball.io.ConsoleInput;
 import baseball.io.ConsoleOutput;
+import baseball.model.BallStatus;
+import baseball.model.Numbers;
 
 public class BaseBallGame implements Runnable {
 
     private final ConsoleOutput output;
     private final ConsoleInput input;
+    private final BaseBallGameEngine engine;
 
-    public BaseBallGame(ConsoleOutput output, ConsoleInput input) {
+    public BaseBallGame(ConsoleOutput output, ConsoleInput input, BaseBallGameEngine engine) {
         this.output = output;
         this.input = input;
+        this.engine = engine;
     }
 
     @Override
     public void run() {
         output.printStartText();
+        Numbers answer = engine.generateAnswer();
+
+        while (true) {
+            String userInputNumber = input.inputNumber();
+            Numbers userNumber = engine.parseToNumbers(userInputNumber);
+            BallStatus ballStatus = engine.createBallStatus(answer, userNumber);
+            output.printScore(ballStatus);
+
+            if (engine.isNotCorrect(ballStatus)) {
+                continue;
+            }
+
+            output.printEndText(ballStatus);
+            String choice = input.userInput();
+
+            if (engine.isExitGame(choice)) {
+                break;
+            }
+
+            answer = engine.generateAnswer();
+        }
     }
 }
