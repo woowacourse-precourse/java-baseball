ee2982a9cf10ea86731275f29414b064ac224baa
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-06 22:42:13 +0900

    feat: add generate random number
    - 랜덤 숫자를 얻는 함수 로직 추가

--- src/main/java/baseball/game/StrikeNumberGenerator.java
+++ src/main/java/baseball/game/StrikeNumberGenerator.java
@@ -1,10 +1,24 @@
 package baseball.game;
 
 import baseball.object.StrikeNumber;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StrikeNumberGenerator {
 
     public static StrikeNumber getStrikeNumber() {
-        return null;
+
+        List<Integer> number = new ArrayList<>();
+
+        while (number.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!number.contains(randomNumber)) {
+                number.add(randomNumber);
+            }
+        }
+
+        return new StrikeNumber(number);
     }
 }
