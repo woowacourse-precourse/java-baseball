477aa73ec3f0a922f946920c1e13230aec18b5fd
JunHee <ljh19980823@gmail.com>
2022-11-06 13:50:37 +0900

    feat : userInput이 올바른 입력인지 check하는 기능을 Game class로 분리

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,44 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Game {
+    private boolean isGameEnd = false;
+
+    public boolean isGameEnd() {
+        return isGameEnd;
+    }
+
+    public void setGameEnd(boolean gameEnd) {
+        isGameEnd = gameEnd;
+    }
+
+    public static boolean isNumeric(String userInput) {
+        for (int i = 0; i < 3; i++) {
+            if (!Character.isDigit(userInput.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean checkValidNumber(String userInput){
+        if (userInput.length() != 3 || !isNumeric(userInput)) {
+            throw new IllegalArgumentException();
+        }
+        List<Integer> userInputNumber = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            Integer tempNum = Integer.parseInt(String.valueOf(userInput.charAt(i)));
+            if (tempNum.equals(0)) {
+                throw new IllegalArgumentException();
+            } else if (userInputNumber.contains(tempNum)) {
+                throw new IllegalArgumentException();
+            } else {
+                userInputNumber.add(tempNum);
+            }
+        }
+        return true;
+    }
+
 }
