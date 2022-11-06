da870c4d890ce44dee26a1081d49c71df8f762cf
임형섭 <hs.yim@illb.co.kr>
2022-11-06 21:36:42 +0900

    feat - 정답 시 재시작/종료 탐색 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,12 +12,34 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        // 컴퓨터가 3자리 숫자의 리스트 생성
-        List<Integer> computerNumber = GenerateRandomNumber();
+        // 게임 진행 상태
+        boolean usersAnswer = true;
 
-        // 유저의 문자 입력 받기
-        String usersNumber = usersNumber();
+        while (usersAnswer){
 
+            // 컴퓨터가 3자리 숫자의 리스트 생성
+            List<Integer> computerNumber = GenerateRandomNumber();
+
+            // 유저의 문자 입력 받기
+            String usersNumber = usersNumber();
+
+            // 정답 시 재시작/종료 탐색 기능
+            usersAnswer = reGameOrEnd(usersNumber());
+
+        }
+
+
+
+
+
+    }
+
+    private static boolean reGameOrEnd(String usersAnswer) {
+        if(usersAnswer.equals("1")){
+            return true;
+        }else{
+            return false;
+        }
     }
 
     private static String usersNumber() {
