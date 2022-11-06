255415345c62cc5c1823b2ef078ad23503e2b30f
RO-OR <kyk3531@gmail.com>
2022-11-06 19:29:53 +0900

    feat($baseball) : add new function for baseball game progress
    Add function for baseball game progress in main method,
    
    Add function, baseball game progress

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,14 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int gameContinue = 1;
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while(gameContinue == 1){
+            BaseBallGame.baseBallGame();
+            gameContinue = Integer.parseInt(GameContinueInput.gameContinue());
+        }
+
     }
 }
