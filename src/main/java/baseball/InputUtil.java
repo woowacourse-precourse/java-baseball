5eade703daddb339be122ebb2cfe175ec8dcbcb2
jingyu9804 <swc05161@gmail.com>
2022-11-06 18:10:59 +0900

    refactor : inputUserName method 반환형 변경

--- src/main/java/baseball/InputUtil.java
+++ src/main/java/baseball/InputUtil.java
@@ -2,19 +2,26 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InputUtil {
 
-    private static String userNumber="";
 
-    public static String inputUserNumber(){
+    public static List<String> inputUserNumber(){
         CheckUtil checkUtil = new CheckUtil();
 
-        userNumber = Console.readLine();
+        String userNumber = Console.readLine();
+
+        List<String> userNumList = new ArrayList<>();
+        for(char c:userNumber.toCharArray()){
+            userNumList.add(Character.toString(c));
+        }
 
         checkUtil.checkIsValidDigit(userNumber);
         checkUtil.checkIsValidLength(userNumber.length());
-        checkUtil.checkIsDuplicated(userNumber);
+        checkUtil.checkIsDuplicated(userNumList);
 
-        return userNumber;
+        return userNumList;
     }
 }
