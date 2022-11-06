f438bb9eb635c00041c9d644bf16f0bac048aa72
aud19 <aud19980518@gmail.com>
2022-11-06 20:12:01 +0900

    application 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import baseball.controller.GameController;
+import baseball.controller.StartController;
+import baseball.domain.Computer;
+
+import java.io.IOException;
+
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO: 프로그램 구현
+        Computer computer;
+        GameController gameController;
+        StartController startController;
+        do {
+            computer = new Computer();
+            startController = new StartController();
+            gameController = new GameController();
+            computer = startController.startController();
+            gameController.playGame(computer);
+        } while(startController.askOneMore() == 1);
     }
 }
