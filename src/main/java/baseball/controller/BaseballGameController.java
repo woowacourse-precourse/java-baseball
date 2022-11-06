5a5d9d10bb227f17d35fba29b85e332d3ad36cd6
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-06 17:20:55 +0900

    feat: BaseballGameController - getUserNumber 메서드 추가
    유저 입력값을 반환

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -3,21 +3,31 @@ package baseball.controller;
 
 import baseball.domain.Computer;
 import baseball.domain.User;
+import baseball.service.BaseballGameService;
 import java.util.List;
+import baseball.view.BaseballGameView;
 
 public class BaseballGameController {
 
     private Computer computer = new Computer();
-    private User user;
+    private BaseballGameService baseballGameService = new BaseballGameService();
+    private BaseballGameView baseballGameView = new BaseballGameView();
+
     private final String RESTART = "1";
     private final int THREE_STRIKE = 3;
+
+    private User user;
 
 
     public void startGame() {
         computer.setRandomNumber();
         List<Integer> computerRandomNumber = computer.getRandomNumber();
+
+    }
+
+    private List<Integer> getUserNumber() {
+        user = new User();
+        user.setSelectNumber();
+        return user.getSelectNumber();
     }
 }
-
-
-
