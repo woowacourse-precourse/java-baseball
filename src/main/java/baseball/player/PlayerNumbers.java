8fc0e6ca142b5a5df83f11d57849dcf906c6a71f
bellljun3395 <195850@jnu.ac.kr>
2022-11-06 16:35:42 +0900

    feat(PlayerNumbers): 구현 PlayerNumbers makeHints 메서드
    - 상대방의 PlayerNumbers와 비교를 통해 힌트를 제공하는 메서드

--- src/main/java/baseball/player/PlayerNumbers.java
+++ src/main/java/baseball/player/PlayerNumbers.java
@@ -10,6 +10,10 @@ public class PlayerNumbers {
     private final int DIGIT_LENGTH = 3;
     private final String CHECK_DIGIT_LENGTH = "숫자의 자릿수를 확인해주세요";
     private static final String NO_OVERLAP_NUMBER = "각 자리가 중복되지 않는 수를 입력해주세요";
+    private static final int HINT_INITIAL_VALUE = 0;
+    private static final int BALL_INDEX = 0;
+    private static final int STRIKE_INDEX = 1;
+
 
     private final List<PlayerNumber> playerNumbers;
 
@@ -55,6 +59,38 @@ public class PlayerNumbers {
             }
         }
 
+    }
+
+    public List<Integer> makeHints(PlayerNumbers playerNumbers) {
+        ArrayList<Integer> hints = new ArrayList<>(List.of(HINT_INITIAL_VALUE, HINT_INITIAL_VALUE));
+
+        List<PlayerNumber> containPlayerNumber = filterContainPlayerNumber(playerNumbers);
+        List<PlayerNumber> basePlayerNumber = this.playerNumbers;
+        List<PlayerNumber> comparePlayerNumber = playerNumbers.getPlayerNumbers();
+
+        for (PlayerNumber containNumber : containPlayerNumber) {
+            if (basePlayerNumber.indexOf(containNumber)
+                    == comparePlayerNumber.indexOf(containNumber)) {
+                hints.set(STRIKE_INDEX, hints.get(STRIKE_INDEX) + 1);
+            } else {
+                hints.set(BALL_INDEX, hints.get(BALL_INDEX) + 1);
+            }
+        }
+
+        return hints;
+    }
+
+    private List<PlayerNumber> filterContainPlayerNumber(PlayerNumbers playerNumbers) {
+        ArrayList<PlayerNumber> filteredPlayerNumber = new ArrayList<>();
+        List<PlayerNumber> basePlayerNumber = this.playerNumbers;
+
+        for (PlayerNumber p : playerNumbers.getPlayerNumbers()) {
+            if (basePlayerNumber.contains(p)) {
+                filteredPlayerNumber.add(p);
+            }
+        }
+
+        return filteredPlayerNumber;
     }
 
 }
