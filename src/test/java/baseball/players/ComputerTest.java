4fb5166fcec06ca9a9132fd8726d620f1fda7780
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-06 22:34:06 +0900

    test(Computer): 리팩터링에 의한 테스트 코드 변경

--- src/test/java/baseball/players/ComputerTest.java
+++ src/test/java/baseball/players/ComputerTest.java
@@ -8,15 +8,15 @@ class ComputerTest {
 
     @Test
     void 컴퓨터의_숫자_생성을_확인하는_기능() {
-        Computer computer = new Computer();
-        String computerNumbers = computer.getComputerNumbers();
+        Player computer = new Computer();
+        String computerNumbers = computer.generateNumbers();
         assertThat(computerNumbers.length()).isEqualTo(3);
     }
 
     @Test
     void 컴퓨터의_중복된_숫자_생성하는지_확인하는_기능() {
-        Computer computer = new Computer();
-        String computerNumbers = computer.getComputerNumbers();
+        Player computer = new Computer();
+        String computerNumbers = computer.generateNumbers();
         String tmp = "";
 
         for (char x : computerNumbers.toCharArray()) {
