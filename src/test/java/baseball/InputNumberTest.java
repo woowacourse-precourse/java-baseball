5f74387750e838e76212e0f07688c1d4c7123ced
JungWoo <rlawjddn103@naver.com>
2022-11-06 22:03:37 +0900

    test(InputNumberTest): 사용자에게 한자리 수를 입력받고 검증하는 기능에 대한 예외 체크 및 기능 테스트 추가

--- src/test/java/baseball/InputNumberTest.java
+++ src/test/java/baseball/InputNumberTest.java
@@ -71,4 +71,57 @@ class InputNumberTest {
         assertThat(input).isEqualTo(inputNumber.inputGameNumber());
     }
 
+    @Test
+    void 사용자_입력_재시작_숫자_테스트(){
+        String input = "1";
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        System.out.println(input);
+        assertThat(input).isEqualTo(inputNumber.inputRestartNumber());
+    }
+
+    @Test
+    void 사용자_입력_재시작_숫자_한자리_아닌_수_예외_테스트(){
+        String input = "123";
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> inputNumber.inputRestartNumber())
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 사용자_입력_재시작_숫자_숫자가_아닐_때_예외_테스트(){
+        String input = "가";
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> inputNumber.inputRestartNumber())
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 사용자_입력_재시작_숫자_공백_예외_테스트(){
+        String input = " ";
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> inputNumber.inputRestartNumber())
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
 }
