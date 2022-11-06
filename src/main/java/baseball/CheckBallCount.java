c3c9fbc550a6c6d34a9a918c6791d90e11d9e61f
Minsoo-Lee <minsoo.james1101@gmail.com>
2022-11-06 22:58:17 +0900

    feat : check if is ball

--- src/main/java/baseball/CheckBallCount.java
+++ src/main/java/baseball/CheckBallCount.java
@@ -5,15 +5,23 @@ import java.util.List;
 public class CheckBallCount {
     private static final int LENGTH = 3;
 
-    public static int countStrike(List<Integer> userNum, List<Integer> answer) {
+    public static int countStrike(List<Integer> userNumList, List<Integer> answer) {
         int count = 0;
         for (int index = 0; index < LENGTH; index++) {
-            if (userNum.get(index) == answer.get(index)) {
+            if (userNumList.get(index) == answer.get(index)) {
                 count++;
             }
         }
         return count;
     }
 
-    
+    public static int countBall(List<Integer> userNumList, List<Integer> answer) {
+        int count = 0;
+        for (int index = 0; index < LENGTH; index++) {
+            if (answer.contains(userNumList.get(index)) && answer.get(index) != userNumList.get(index)) {
+                count++;
+            }
+        }
+        return count;
+    }
 }
