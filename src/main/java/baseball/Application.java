ec1d17b3839291b4a3148760236e2fb880d69ff9
jootang2 <joohwan.song15b@gmail.com>
2022-11-06 21:16:00 +0900

    :recycle: [리팩토링-기능5] : 게임 종료 여부 기능을 while문 바깥으로 이동

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -29,11 +29,11 @@ public class Application {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 flag = false;
             }
-
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String number = Console.readLine();
-            continueOrFinish(number);
         }
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String number = Console.readLine();
+        continueOrFinish(number);
     }
 
     public static String selectRandomNumber() {
