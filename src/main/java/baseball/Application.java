027d7506e05977f359761a5209d771f08b297cae
fantasyshrimp <fantasy.shrimp@gmail.com>
2022-11-06 16:23:34 +0900

    feat: add printed sentence

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -36,6 +36,34 @@ class UserInput {
     }
 }
 
+
+class BaseballGameSentence {
+    public static String GAME_START = "숫자 야구 게임을 시작합니다.";
+    public static String REQUEST_NUMBER = "숫자를 입력해주세요 : ";
+    public static String BALL = "볼";
+    public static String STRIKE = "스트라이크";
+    public static String NOTHING = "낫싱";
+    public static String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static String SELECT_REPLAY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
+
+class PrintGuidSentence {
+    public static void loadGame() {
+        System.out.println(BaseballGameSentence.GAME_START);
+    }
+    public static void requestNumber() {
+        System.out.print(BaseballGameSentence.REQUEST_NUMBER);
+    }
+    public static void printResult() {
+
+    }
+    public static void endGame() {
+        System.out.print(BaseballGameSentence.GAME_OVER);
+        System.out.print(BaseballGameSentence.SELECT_REPLAY);
+    }
+}
+
+
 class BaseballNumber {
     private final int[] baseballNumber = new int[3];
 
