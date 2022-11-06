3de939bcf427b2c0fb8b8d3cdccb18e79640eb16
윤서영 <yoonseoyoung@yunseoyeong-ui-MacBookPro.local>
2022-11-06 17:20:11 +0900

    FEAT : 서로 다른 3개의 숫자 입력받기

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Controller {
@@ -10,6 +11,13 @@ public class Controller {
     public void startGame() {
         System.out.println("START_MESSAGE = " + START_MESSAGE);
         List<Integer> answerNum = service.setAnswerNum();
+        String inputString = getInputString();
+
     }
 
+    private String getInputString() throws IllegalArgumentException {
+        System.out.print(INPUT_MESSAGE);
+        String inputString = Console.readLine();
+        return inputString;
+    }
 }
--- src/main/java/baseball/Service.java
+++ src/main/java/baseball/Service.java
@@ -1,5 +1,19 @@
 package baseball;
 
-public class Service {
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Service {
+    public List<Integer> setAnswerNum() {
+        List<Integer> numList = new ArrayList<>();
+        while(numList.size() < 3) {
+            int num = Randoms.pickNumberInRange(1, 9);
+            if(!numList.contains(num)) {
+                numList.add(num);
+            }
+        }
+        return numList;
+    }
 }
