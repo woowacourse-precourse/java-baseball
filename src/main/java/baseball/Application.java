7f9d674a1530d0f0f7776567a127d7729dbdbcba
iamjooon2 <iamjooon2@gmail.com>
2022-11-06 11:31:19 +0900

    feat: 게임 종료 및 재시작을 알리는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        String restartCheckInput = "";
+        while (true) {
+            Game.start();
+            restartCheckInput = Console.readLine();
+            if (restartCheckInput.equals("2")) {
+                System.out.println("게임 종료");
+                break;
+            }
+        }
     }
 }
