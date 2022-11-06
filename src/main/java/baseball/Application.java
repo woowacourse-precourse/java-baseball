2872ebd4a1717b2e40a312bb201b96adea724700
abbd2 <ajkety00@gmail.com>
2022-11-07 03:40:26 +0900

    refactor(Application): 생성자를 통한 갱신이 아닌 반환된 값을 통해 변수에 갱신

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,12 +10,16 @@ public class Application {
 
         GameController gameController = new GameController();
         RandomNumber randomNumber = new RandomNumber();
-        
+        NumberVO numberVO = new NumberVO();
+
         ShowMessage.showGameStart(); // 게임 시작 메세지 출력
         ShowMessage.showInputNumber(); // 숫자 입력 메세지 출력
-        gameController.inputUserNumber(); // 사용자 입력
-        randomNumber.createRandomNumber(); // 난수 생성
-        
+        List<Integer> userNumberList = gameController.inputUserNumber(); // 사용자 입력
+        System.out.println("입력값:" + numberVO.getUserNumber() + ":");
+        List<Integer> randomNumberList = randomNumber.createRandomNumber(); // 난수 생성
+        System.out.println("난수:" + numberVO.getRandomNumber() + ":");
+        gameController.compareGameCount(userNumberList, randomNumberList);
+
     }
 
 }
