419aac16973cc1afe733860cbcc9724082260802
flydog98 <guinnessoverflow@gmail.com>
2022-11-07 00:39:08 +0900

    feat: write game start announcing logic

--- src/main/java/baseball/GameClient.java
+++ src/main/java/baseball/GameClient.java
@@ -14,7 +14,7 @@ public class GameClient {
     }
 
     private void announceGameStart() {
-
+        this.baseballUtil.printlnLine("숫자 야구 게임을 시작합니다.");
     }
 
     private void announceGameEnd() {
