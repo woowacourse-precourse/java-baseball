0edb22e19e7159e70674c905f731c3f44e08305a
민병욱 <woook27@naver.com>
2022-11-07 05:11:40 +0900

    feat (Input): 사용자 입력 유효한지 확인하는 isValid 함수 구현

--- src/main/java/baseball/console/Input.java
+++ src/main/java/baseball/console/Input.java
@@ -1,5 +1,6 @@
 package baseball.console;
 
+import java.util.HashSet;
 import java.util.Scanner;
 
 public class Input {
@@ -9,6 +10,29 @@ public class Input {
         System.out.print(inputMessage);
         Scanner scanner = new Scanner(System.in);
         String number = scanner.nextLine();
+        if (!isValid(number)) {
+            throw new IllegalArgumentException();
+        }
         return number;
+    }
+
+    public static boolean isValid(String number) {
+        if (number.length() != 3) {
+            return false;
+        }
+        HashSet<Character> set = new HashSet<Character>();
+        for (char c : number.toCharArray()) {
+            if(!Character.isDigit(c)) {
+                return false;
+            }
+            if(c == '0') {
+                return false;
+            }
+            if(!set.isEmpty() && set.contains(c)) {
+                return false;
+            }
+            set.add(c);
+        }
+        return true;
     }
 }
