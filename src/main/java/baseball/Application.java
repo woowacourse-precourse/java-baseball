0433a65f21d1ff4f9ee2768b2cf3db203ee59824
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-06 18:41:05 +0900

    feat(Application) : implement RunGame class and move DataBase class
    - implement RunGame class which make three random number and save into DataBase class' answer
    - move DataBase class out of the Application class

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,43 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        DataBase data = new DataBase();
+        RunGame game = new RunGame();
+        data.setAnswer(game.answer);
     }
-
-    class DataBase {
-        private List<Integer> answer = new ArrayList<>();
-        private List<Integer> userInput = new ArrayList<>();
-        private int strike = 0;
-        private int ball = 0;
-        private int nothing = 0;
+    static int inputSize = 3;
+    static class RunGame {
+        List<Integer> answer = new ArrayList<>();
+        RunGame() {
+            while(answer.size()<inputSize) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!answer.contains(randomNumber)) {
+                    answer.add(randomNumber);
+                }
+            }
+        }
     }
 }
-
+class DataBase {
+    private List<Integer> answer = new ArrayList<>();
+    private List<Integer> userInput = new ArrayList<>();
+    private int strike = 0;
+    private int ball = 0;
+    public void setAnswer(List<Integer> answer) {
+        this.answer = answer;
+    }
+    public void setUserInput(List<Integer> userInput) {
+        this.userInput = userInput;
+    }
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+}
