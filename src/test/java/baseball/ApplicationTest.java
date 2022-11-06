fb134207e6a764fc89301afaf33a515405a5b98e
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-06 19:44:23 +0900

    feat(test): 입력값 정답 확인 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,10 +1,13 @@
 package baseball;
 
 import baseball.domain.Computer;
+import baseball.domain.Game;
+import baseball.view.View;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.HashSet;
@@ -67,6 +70,19 @@ class ApplicationTest extends NsTest {
         assertThat(computer.countBall(List.of(1, 2, 3))).isEqualTo(0);
         assertThat(computer.countBall(List.of(3, 1, 2))).isEqualTo(3);
         assertThat(computer.countBall(List.of(4, 5, 6))).isEqualTo(0);
+    }
+
+    @Test
+    void 유저가_입력한_수가_정답인지_확인하는_기능 () throws Exception {
+        //given
+        Game game = new Game();
+        Method method = Game.class.getDeclaredMethod("isComplete", int.class);
+        method.setAccessible(true);
+        //when
+
+        //then
+        assertThat(method.invoke(game, 3)).isEqualTo(true);
+        assertThat(method.invoke(game, 1)).isEqualTo(false);
     }
 
     @Override
