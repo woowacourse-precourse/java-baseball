57fd9c3347079a84d7ea4ad4e03b750ce9ce68c2
cocomozila <joseyun94@naver.com>
2022-11-06 20:32:19 +0900

    feat: 입력한 값을 정답과 비교하여 boolean값 반환하는 기능

--- src/main/java/baseball/domain/BaseballMachine.java
+++ src/main/java/baseball/domain/BaseballMachine.java
@@ -7,15 +7,25 @@ public class BaseballMachine {
     private Computer computer;
     private Input input;
     private User user;
+    private StrikeModel strikeModel;
 
-    public BaseballMachine() {
+    public void prepareGame() {
         this.computer = new Computer();
         this.input = new Input();
     }
 
-    public void progressGame() {
+    public void matchNumber() {
         createUserNumber();
-        user.getUserNumberList();
+        this.strikeModel = new StrikeModel(user.getUserNumberList(), computer.getComputerNumberList());
+    }
+
+    // 입력한 값을 정답과 비교하여 boolean값 반환하는 기능
+    public boolean checkAnswer() {
+        int[] answer = strikeModel.getBallAndStrike();
+        if (answer[1] == 3) {
+            return true;
+        }
+        return false;
     }
 
     // 유저 일급컬렉션 생성
