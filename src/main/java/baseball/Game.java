72bd3478ae654a8a403055ecfc0eba79cdfbbcf7
h0ngwon <w___on@naver.com>
2022-11-06 17:42:44 +0900

    feat(Game) : 게임 초기화 기능

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,22 @@
 package baseball;
 
 public class Game {
+  private int strike;
+  private int ball;
+  private int success = 0;
+
+  public Computer computer;
+
+  public Game(Computer computer) {
+    this.computer = computer;
+    this.strike = 0;
+    this.ball = 0;
+  }
+
+  public void init() {
+    computer.init();
+    strike = 0;
+    ball = 0;
+  }
+
 }
