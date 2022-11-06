6689039f5e0f5810912bc78727b35922d3b0b6f1
hyeonseog-won <smreo75@gmail.com>
2022-11-07 01:22:13 +0900

    fix: 기능 수정에 따른 테스트 코드 수정

--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -9,16 +9,8 @@ public class UserTest {
     @Test
     void 사용자_입력이_3자리가_아닐_경우_예외_발생() {
         User user = new User();
-        assertThatThrownBy(() -> user.verifyInput("12"))
+        assertThatThrownBy(() -> user.duplicateCheck("12"))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("입력한 수가 3자리가 아닙니다.");
-    }
-
-    @Test
-    void 사용자_입력이_1부터_9가_아닐_경우_예외_발생() {
-        User user = new User();
-        assertThatThrownBy(() -> user.verifyInput("120"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("입력한 수가 1-9가 아닙니다.");
+                .hasMessage("서로 다른 수로 이루어진 3자리의 수를 입력해야 합니다.");
     }
 }
