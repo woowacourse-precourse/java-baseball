c8e241c28b56ef4254c4afdcd858e9dab45d6bcc
Yejin Shin <shinyejin0212@gmail.com>
2022-11-06 21:17:56 +0900

    feat($Player): add getAnswer 메소드 추가
    2.2.1. 서로 다른 3자리 수를 입력 받아 저장하는 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,5 +4,7 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         Opponent opponent = new Opponent();
+        Player player = new Player();
+        player.getAnswer();
     }
 }
--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,8 +1,14 @@
 package baseball;
 
-import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Player {
     protected static List<Integer> answer;
+    public Player(){}
+    public static String getAnswer(){
+        System.out.println("숫자를 입력해주세요 : ");
+        String str = Console.readLine();
+        return str;
+    }
 }
