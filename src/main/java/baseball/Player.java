158a2b40d3bde75bc68bde848d584b5229b00bca
이경범 <tkdwk567@naver.com>
2022-11-06 11:06:18 +0900

    feat : 플레이어는 심판에게 번호를 넘겨주는 기능 생성
    - 심판에게 번호를 넘겨준다.

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -7,15 +7,22 @@ public class Player {
     private List<Integer> numbers;
 
     public List<Integer> sayNumbers(List<Integer> numbers) {
-        if (numbers.size() > 3) {
-            throw new IllegalArgumentException("Numbers is size Exception!!");
-        }
-
+        validNumbers(numbers);
         this.numbers = numbers;
         return numbers;
     }
 
+    private void validNumbers(List<Integer> numbers) {
+        if (numbers.size() > 3) {
+            throw new IllegalArgumentException("Numbers is size Exception!!");
+        }
+    }
+
+    public List<Integer> getNumbers() {
+        return this.numbers;
+    }
+
     public void giveNumbersToReferee(Referee referee) {
-        referee.receiveNumbers(numbers);
+        referee.receivePlayerNumbers(this.numbers);
     }
 }
