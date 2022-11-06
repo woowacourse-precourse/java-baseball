9180bfc05e30d6ed62079055027db606fd439d59
임형섭 <hs.yim@illb.co.kr>
2022-11-06 21:31:28 +0900

    feat - 컴퓨터 3자리 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.regex.Matcher;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // 컴퓨터가 3자리 숫자의 리스트 생성
+        List<Integer> computerNumber = GenerateRandomNumber();
+
+    }
+
+    private static List<Integer> GenerateRandomNumber() {
+        return Randoms.pickUniqueNumbersInRange(1, 9, 3);
     }
 }
