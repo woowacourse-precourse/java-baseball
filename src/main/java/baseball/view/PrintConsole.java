033b816cf04e34d8d64c56d731f4804e7ef2ea3c
gyuturn <rbtjrrbals@gmail.com>
2022-11-06 17:41:31 +0900

    feat($view): 컴퓨터의 계산에 따른 결과출력
    D-1
    출력목록
    - 같은수가 같은자리 :스트라이크(D-1-1)
    - 같은수가 다른자리 :볼(D-1-2)
    - 같은수가 없음: 낫싱(D-1-3)
    - 3스트라이크인경우(D-1-4)

--- src/main/java/baseball/view/PrintConsole.java
+++ src/main/java/baseball/view/PrintConsole.java
@@ -1,5 +1,7 @@
 package baseball.view;
 
+import baseball.model.Computer;
+
 public class PrintConsole {
 
     public static void startGame() {
@@ -8,6 +10,54 @@ public class PrintConsole {
 
     public static void receiveNum(){
         System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public static String printResult(Computer resultComputer) {
+        String printBall = printBall(resultComputer);
+        String printStrike = printStrike(resultComputer);
+        String printNothing = printNothing(resultComputer);
+
+        int ball = resultComputer.getBall();
+        int strike = resultComputer.getStrike();
+
+        String result = "";
+        if (ball > 0 && strike > 0) {
+            result += printBall + " " + printStrike;
+        } else {
+            result += printBall;
+            result += printStrike;
+            result += printNothing;
+        }
+        result += '\n';
+        System.out.println(result);
+        return result;
+    }
+
+    private static String printBall(Computer resultComputer) {
+        String ballToString = "";
+        int resultBall = resultComputer.getBall();
+        if (resultBall > 0) {
+            ballToString += resultBall + "볼";
+        }
+        return ballToString;
+    }
+
+    private static String printStrike(Computer resultComputer) {
+        String strikeToString = "";
+        int resultStrike = resultComputer.getStrike();
+        if (resultStrike > 0) {
+            strikeToString += resultStrike + "스트라이크";
+        }
+        return strikeToString;
+    }
+
+    private static String printNothing(Computer resultComputer) {
+        String nothingToString = "";
+        boolean isNothing = resultComputer.getNothing();
+        if (isNothing) {
+            nothingToString += "낫싱";
+        }
+        return nothingToString;
     }
 
 }
