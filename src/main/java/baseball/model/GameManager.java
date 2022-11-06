0855e18c20d464a7ed3156b708f2aa1b19ba8b58
ChanghyunKim <variety82p@gamil.com>
2022-11-06 21:21:50 +0900

    feat : 스트링을 리스트로 변경하기 위한 메서드 구현

--- src/main/java/baseball/model/GameManager.java
+++ src/main/java/baseball/model/GameManager.java
@@ -3,12 +3,20 @@ package baseball.model;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static baseball.constant.ConstantValue.BASEBALL_NUMBER_SIZE;
 
 public class GameManager {
     public GameManager(){
+    }
+
+    public List<Integer> toList(String number){
+        return Arrays.stream(number.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> createComputerNumber(){
