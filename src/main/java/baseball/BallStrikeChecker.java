995f551810859b4f21ef44362d849857abcdab91
dener8 <8dener@gmail.com>
2022-11-06 17:11:48 +0900

    feat: countStrike() 구현
    스트라이크의 수를 세는 countStrike() 구현

--- src/main/java/baseball/BallStrikeChecker.java
+++ src/main/java/baseball/BallStrikeChecker.java
@@ -2,6 +2,7 @@ package baseball;
 
 public class BallStrikeChecker {  // 이 클래스에서 모든 판단을 끝내는걸로?
 
+    // countSumOfBallAndStrike()는 countBall()에서만 쓰이고, 다른 곳에서는 쓰이지 않을 듯
     public int countSumOfBallAndStrike(String randomNumber, String playerNumber) {
         int sumOfBallAndStrike = 0;
         for (int i = 0; i < 3; i++) {
@@ -13,7 +14,13 @@ public class BallStrikeChecker {  // 이 클래스에서 모든 판단을 끝내
     }
 
     public int countStrike(String randomNumber, String playerNumber) {
-
+        int strike = 0;
+        for (int i = 0; i < 3; i++) {
+            if (randomNumber.charAt(i) == playerNumber.charAt(i)) {
+                strike++;
+            }
+        }
+        return strike;
     }
 
     public int countBall(String randomNumber, String playerNumber) {
