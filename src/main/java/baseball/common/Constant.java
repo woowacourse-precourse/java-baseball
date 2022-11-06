884e3f997c0cc88f03ffcf58d746e361631e4328
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-06 23:50:54 +0900

    refactor(Constant): 상수들을 한곳에서 관리

--- src/main/java/baseball/common/Constant.java
+++ src/main/java/baseball/common/Constant.java
@@ -1,5 +1,8 @@
 package baseball.common;
 
 public class Constant {
+    public static final int BALL_SIZE = 3;
     public static final int MAX_STRIKE_SIZE = 3;
+    public static final int MIN_RANDOM_NUM = 1;
+    public static final int MAX_RANDOM_NUM = 9;
 }
