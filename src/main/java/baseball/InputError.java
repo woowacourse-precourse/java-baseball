e55073d174fdd3b43968f58ec44e32f00e5e06ab
ssssudan <kankgm0324@gmail.com>
2022-11-06 21:55:22 +0900

    <feat>: 게임 중 사용자 입력이 올바른지 검사, 잘못된 입력일 때 IllegalArgumentException

--- src/main/java/baseball/InputError.java
+++ src/main/java/baseball/InputError.java
@@ -8,13 +8,44 @@ package baseball;
  */
 public class InputError {
 
-    public String replayOrStopException(String answer) {
+    public static final String ERROR_MESSAGE = "잘못된 입력값입니다. 게임을 종료하겠습니다.";
+
+    public String replayOrStopExceptionCheck(String answer) {
         // 1, 2가 아닌 다른 값인지 검사
 
-        if(answer.equals("1"))
+        if (answer.equals("1"))
             return answer;
-        if(answer.equals("2"))
+        if (answer.equals("2"))
             return answer;
-        throw new IllegalArgumentException("잘못된 입력값입니다. 게임을 종료하겠습니다.");
+        throw new IllegalArgumentException(ERROR_MESSAGE);
+    }
+
+    public String userNumberInputExceptionCheck(String answer) {
+        /**
+         * 서로 다른 3자리
+         * 길이가 3이 아닌 경우 예외 발생
+         * 중복된 수가 들어 있을 때 예외 발생
+         * 각 자리가 숫자가 아닌경우 예외발생
+         */
+
+        if (answer.length() != 3)
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+
+        if (answer.charAt(0) == answer.charAt(1))
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+
+        if (answer.charAt(0) == answer.charAt(2))
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+
+        if (answer.charAt(1) == answer.charAt(2))
+            throw new IllegalArgumentException(ERROR_MESSAGE);
+
+        for (int i = 0; i < answer.length(); i++) {
+            char n = answer.charAt(i);
+            if (n < '1' || n > '9')
+                throw new IllegalArgumentException(ERROR_MESSAGE);
+        }
+
+        return answer;
     }
 }
