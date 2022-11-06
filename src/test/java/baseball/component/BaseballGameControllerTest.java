cdeb3f5e5fe699ff1e7c5b6023a578b3921e3c22
pilyang <30036534+pilyang@users.noreply.github.com>
2022-11-06 14:21:39 +0900

    test(BaseballGameController): 중복되는 사용자 입력 예외 테스트 통합

--- src/test/java/baseball/component/BaseballGameControllerTest.java
+++ src/test/java/baseball/component/BaseballGameControllerTest.java
@@ -3,72 +3,40 @@ package baseball.component;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BaseballGameControllerTest extends NsTest {
 
     @Test
-    @DisplayName("예외_테스트 - player number 2자리 수 입력")
-    void playerNumber2digitTest() {
+    @DisplayName("예외_테스트 - 새 게임 시작 선택시 1,2외 입력")
+    void nextGameInputExceptionTest() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> runException("142", "135", "3"))
+                            .isInstanceOf(IllegalArgumentException.class)
+                            .hasMessageContaining(BaseballGameController.NEXT_GAME_INPUT_ERROR_MSG);
+                },
+                1, 3, 5
+        );
+    }
+
+    @ParameterizedTest(name = "사용자 입력 예외 테스트 [{index}] - 사용자 입력값 : {0}")
+    @ValueSource(strings = {"13", "1234", "abc", "709", "122", "332", "565", "777"})
+    void playerNumberInputValidationTest(final String playerInput) {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("13"))
+                assertThatThrownBy(() -> runException(playerInput))
                         .isInstanceOf(IllegalArgumentException.class)
                         .hasMessageContaining(BaseballGameController.PLAYER_NUMBER_INPUT_ERROR_MSG)
         );
     }
 
-    @Test
-    @DisplayName("예외_테스트 - player number 숫자가 아닌 문자 입력")
-    void playerNumberNotNumberTest() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("abc"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining(BaseballGameController.PLAYER_NUMBER_INPUT_ERROR_MSG)
-        );
-    }
-
-    @Test
-    @DisplayName("예외_테스트 - player number 중복되는 수 입력1 - abb")
-    void duplicatedExceptionTest1() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("122"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining(BaseballGameController.PLAYER_NUMBER_INPUT_ERROR_MSG)
-        );
-    }
-
-    @Test
-    @DisplayName("예외_테스트 - player number 중복되는 수 입력2 - aab")
-    void duplicatedExceptionTest2() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("332"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining(BaseballGameController.PLAYER_NUMBER_INPUT_ERROR_MSG)
-        );
-    }
-
-    @Test
-    @DisplayName("예외_테스트 - player number 중복되는 수 입력3 - aba")
-    void duplicatedExceptionTest3() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("383"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining(BaseballGameController.PLAYER_NUMBER_INPUT_ERROR_MSG)
-        );
-    }
-
-    @Test
-    @DisplayName("예외_테스트 - player number 중복되는 수 입력4 - aaa")
-    void duplicatedExceptionTest4() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("777"))
-                        .isInstanceOf(IllegalArgumentException.class)
-                        .hasMessageContaining(BaseballGameController.PLAYER_NUMBER_INPUT_ERROR_MSG)
-        );
-    }
 
     @Override
     protected void runMain() {
