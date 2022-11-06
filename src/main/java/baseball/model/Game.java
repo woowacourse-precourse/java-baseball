c1fc9cd55f3174b4d65a41e3ca8b897e0a3608f4
kmularise <kmula222@gmail.com>
2022-11-07 07:18:37 +0900

    feat(model): add the game class

---
+++ src/main/java/baseball/model/Game.java
@@ -0,0 +1,33 @@
+package baseball.model;
+
+import baseball.controller.HintMaker;
+import baseball.view.Printer;
+import baseball.view.Session;
+
+public class Game {
+	private static final String RESTART = "1";
+	private final User user;
+	private final Computer computer;
+	private final HintMaker hintmaker;
+
+	public Game(){
+		user = new User();
+		computer = new Computer();
+		hintmaker = new HintMaker();
+	}
+	public void start(){
+		computer.initAnswer();
+		computer.makeAnswer();
+		do{
+			Printer.printInputGuide();
+			user.initData();
+			user.editNumberString(Session.getThreeNumbers());
+		}while(!hintmaker.isAnswer(user.getUserData(), computer.getAnswer()));
+
+		Printer.printEnd();
+		String choice = Session.getRestartCondition();
+		if (RESTART.equals(choice)){
+			start();
+		}
+	}
+}
