a9f66fabc3836a196989cd452c723c29e62ef53d
parkmuhyeun <pjhg410@gmail.com>
2022-11-06 12:02:10 +0900

    feat: 게임 시스템 작동

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -6,7 +6,24 @@ public class Game {
     private static final int CONTINUE = 1;
     private static final int END = 2;
 
+    private int system = CONTINUE;
+
     public Game() {
+    }
+
+    public void run() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (isContinue(system)) {
+            Computer computer = new Computer();
+            User user = new User();
+
+            playGame(computer, user);
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String select = inputSelect();
+            validateSelect(select);
+            system = Integer.parseInt(select);
+        }
     }
 
     private static void validateSelect(String select) {
