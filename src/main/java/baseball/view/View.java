4762a1e741b680f95ec86ac1e136b3f499250472
62hoon99 <98267772+62hoon99@users.noreply.github.com>
2022-11-06 10:38:05 +0900

    feat(view): 유저에게 입력받는 기능

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -1,9 +1,13 @@
 package baseball.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class View {
+    private String INPUT_PHRASES = "숫자를 입력해주세요 : ";
 
-    public void startView() {
-
+    public String inputView() {
+        System.out.println(INPUT_PHRASES);
+        return readLine();
     }
 
     public void endView() {
