e0c7a85f02b31886a26041f6139a7097b3a70d41
parksey <parkseyeon99@naver.com>
2022-11-06 11:40:03 +0900

    feat: 사용자 입력 예외처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -36,9 +36,30 @@ public class Application {
 
         while (true) {
             PrintResult.printLine(PrintResult.INPUT_INFO);
-            String ret = Console.readLine();
+            String userInput = Console.readLine();
 
             // 예외처리 : 1~9 이외의 값인지 확인
+            isGameNumber(userInput);
+        }
+    }
+
+    /**
+     * 사용자 입력이 게임 숫자인지 확인
+     * 1. 사이즈가 게임 숫자 사이즈
+     * 2. 숫자 1~9의 숫자만 와야한다.
+     * @param userInput
+     * @return 게임 숫자이면 True / 아니면 False
+     */
+    public static void isGameNumber(String userInput)   {
+        if (userInput.length() != Baseball.GAME_NUMBER_SIZE) {
+            throw new IllegalArgumentException("사용자 입력 길이가 게임 숫자 길이와 다르다.");
+        }
+
+        for (int i=0; i<userInput.length(); i++) {
+            char userOneChar = userInput.charAt(i);
+            if (userOneChar < '1' || userOneChar > '9') {
+                throw new IllegalArgumentException("숫자 이외의 값이 입력되었습니다.");
+            }
         }
     }
 }
--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Baseball {
+    public static final int GAME_NUMBER_SIZE = 3;
+
     List<Integer> gameNumber = new ArrayList<>();
 
     public Baseball() {
@@ -16,7 +18,7 @@ public class Baseball {
      * 컴퓨터 숫자 : GameNumber 초기 값 설정하는 함수
      */
     public void setRandomNumber() {
-        while (gameNumber.size() < 3) {
+        while (gameNumber.size() < GAME_NUMBER_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(1,9);
             if (!gameNumber.contains(randomNumber)) {
                 gameNumber.add(randomNumber);
