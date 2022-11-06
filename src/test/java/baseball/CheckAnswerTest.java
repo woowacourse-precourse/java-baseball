89f11e37c42775e5a65ea663e15526a0e7146f6b
yoon <beatus96@naver.com>
2022-11-07 00:53:10 +0900

    test(checkanswer) : 추가 "컴퓨터 수와 비교한 결과값 문자열로 만들기 기능 테스트"
    resultMap을 넣었을 때, 공백이나 빠짐 없이 문자열로 출력하는가 테스트하였다.

--- src/test/java/baseball/CheckAnswerTest.java
+++ src/test/java/baseball/CheckAnswerTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -23,5 +24,31 @@ public class CheckAnswerTest {
         Map<String, Integer> resultMap = Map.of("strike",1,"ball",1,"nothing",1);
         assertThat(Application.checkAnswer(computerNumber,playerNumber)).isEqualTo(resultMap);
 
+    }
+
+    @Test
+    void 결과값_문자열_테스트1(){
+        Map<String, Integer> resultMap = Map.of("strike",1,"ball",1,"nothing",1);
+        String hint = "1볼 1스트라이크";
+        String result = Application.giveHint(resultMap);
+        assertThat(result).isEqualTo(hint);
+        System.out.println(result);
+    }
+
+    @Test
+    void 결과값_문자열_테스트2(){
+        Map<String, Integer> resultMap = Map.of("nothing",3);
+        String hint = "낫싱";
+        String result = Application.giveHint(resultMap);
+        assertThat(result).isEqualTo(hint);
+        System.out.println(result);
+    }
+    @Test
+    void 결과값_문자열_테스트3(){
+        Map<String, Integer> resultMap = Map.of("strike",2,"nothing",1);
+        String hint = "2스트라이크";
+        String result = Application.giveHint(resultMap);
+        assertThat(result).isEqualTo(hint);
+        System.out.println(result);
     }
 }
