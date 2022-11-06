ecf7f999c8615b18981be45db17319cecea9b3bc
dong young <dong5854@gmail.com>
2022-11-07 01:18:20 +0900

    test: 게임 결과의 확인하는 checkResult 함수의 콘솔 출력 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -9,8 +9,7 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -56,6 +55,41 @@ class ApplicationTest extends NsTest {
         // 정답을 맞추지 못했을 경우
         assertFalse(Application.checkResult(List.of(5,6,7), List.of(5,6,9)));
         assertFalse(Application.checkResult(List.of(1,5,9), List.of(5,9,1)));
+    }
+
+    @Test
+    void 게임_결과_출력_테스트(){
+        Application.checkResult(List.of(5,6,7), List.of(5,6,9));
+        assertEquals(output(), "2스트라이크");
+        init();
+
+        Application.checkResult(List.of(8,6,1), List.of(8,2,7));
+        assertEquals(output(), "1스트라이크");
+        init();
+
+        Application.checkResult(List.of(1,7,8), List.of(4,6,7));
+        assertEquals(output(), "1볼");
+        init();
+
+        Application.checkResult(List.of(7,1,5), List.of(1,5,7));
+        assertEquals(output(), "3볼");
+        init();
+
+        Application.checkResult(List.of(5,7,2), List.of(1,7,5));
+        assertEquals(output(), "1볼 1스트라이크");
+        init();
+
+        Application.checkResult(List.of(7,1,5), List.of(7,5,1));
+        assertEquals(output(), "2볼 1스트라이크");
+        init();
+
+        Application.checkResult(List.of(4,5,6), List.of(3,2,1));
+        assertEquals(output(), "낫싱");
+        init();
+
+        Application.checkResult(List.of(4,1,7), List.of(4,1,7));
+        assertEquals(output(), "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        init();
     }
 
     @Override
