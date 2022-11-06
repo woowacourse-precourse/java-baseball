435190d2aa3a233a9a3c268fddf1d12e3ead6879
goldcollar95 <yjs7298@naver.com>
2022-11-06 21:28:42 +0900

    예외처리 조건을 발생시켜 자리에 부합하도록 하는 메소드

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,4 +1,16 @@
 package baseball;
 
 public class User {
+
+
+    public static String inputValidtor(String inputValue) throws IllegalArgumentException{
+
+
+
+        if(inputValue.length() != 3){
+            throw new IllegalArgumentException("3자리 양식에 맞게 입력하세요");
+        }
+        return inputValue;
+    }
+
 }
