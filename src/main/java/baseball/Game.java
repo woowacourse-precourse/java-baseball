93d3018b17ffaed6efce3ceab445bc5ea816552e
h0ngwon <w___on@naver.com>
2022-11-06 21:43:26 +0900

    feat(Game) : 사용자 입력 검증 기능

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -27,10 +27,13 @@ public class Game {
   public void start() {
     List<Integer> userNumbers = new ArrayList<>();
     String userInput;
+
     System.out.println("숫자 야구 게임을 시작합니다.");
     while(strike < 3) {
       System.out.print("숫자를 입력해 주세요 : ");
       userInput = readLine();
+      validateUserInput(userInput);
+      userNumbers = stringToIntegerList(userInput);
     }
   }
 
@@ -43,5 +46,13 @@ public class Game {
     }
 
     return result;
+  }
+
+  public void validateUserInput(String userInput) {
+    try {
+      Integer.parseInt(userInput);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException("Input is wrong.");
+    }
   }
 }
