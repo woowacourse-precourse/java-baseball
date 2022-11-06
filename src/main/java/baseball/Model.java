50587ba4b52806e230d75aceee40cf7cd77ad8fc
tjddnr7760 <tjddnr7760@naver.com>
2022-11-06 20:15:03 +0900

    feat : Model 클래스 구현

--- src/main/java/baseball/Model.java
+++ src/main/java/baseball/Model.java
@@ -1,24 +1,74 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
 public class Model {
-    Model() {}
+    ArrayList<String> humanarraylist;
 
-    private int humannumber;
-    private int computernumber;
+    Model() { }
 
-    public int getHumannumber() {
+    private String humannumber;
+    private String computernumber;
+
+    public String getHumannumber() {
         return humannumber;
     }
 
-    public void setHumannumber(int humannumber) {
+    public void setHumannumber(String humannumber) {
+        Model model = new Model();
+
+        model.isContainChar(humannumber);
+        model.isContainZero(humannumber);
+        model.isCorrectRange(humannumber);
+        humanarraylist = model.isDuplicate(humannumber);
+
         this.humannumber = humannumber;
     }
 
-    public int getComputernumber() {
+    public String getComputernumber() {
         return computernumber;
     }
 
-    public void setComputernumber(int computernumber) {
+    public void setComputernumber(String computernumber) {
         this.computernumber = computernumber;
+    }
+
+    public void clearArrayList() {
+        humanarraylist.clear();
+    }
+
+    private void isContainChar(String humannumber) {
+        int number = 0;
+
+        try {
+            number = Integer.parseInt(humannumber);
+        }catch (Exception e) {
+            throw new IllegalArgumentException();
+        }//공백 확인
+    }
+
+    private void isContainZero(String humannumber) {
+        if(humannumber.contains("0")) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isCorrectRange(String humannumber) {
+        if(humannumber.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private ArrayList<String> isDuplicate(String humannumber) {
+        HashSet<String> hashset = new HashSet<>(Arrays.asList(humannumber.split("")));
+        ArrayList<String> arraylsit = new ArrayList<>(Arrays.asList(humannumber.split("")));
+
+        if(hashset.size() != arraylsit.size()) {
+            throw new IllegalArgumentException();
+        }
+
+        return arraylsit;
     }
 }
