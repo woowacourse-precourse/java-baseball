ba2535da031553aee3939da958d5ce4373c65786
HyeonJun0530 <nexus2697@pukyong.ac.kr>
2022-11-06 20:55:27 +0900

    refactor(Computer): 객체 지향 코드를 위한 리팩터링

--- src/main/java/baseball/players/Computer.java
+++ src/main/java/baseball/players/Computer.java
@@ -3,20 +3,18 @@ package baseball.players;
 import camp.nextstep.edu.missionutils.Randoms;
 
 
-public class Computer {
+public class Computer implements Player {
 
     private String computerNumbers = "";
 
-    public Computer() {
+    public String generateNumbers() {
         while (computerNumbers.length() < 3) {
             String number = Integer.toString(Randoms.pickNumberInRange(1, 9));
 
             if (!computerNumbers.contains(number))
                 this.computerNumbers += number;
         }
-    }
 
-    public String getComputerNumbers() {
         return computerNumbers;
     }
 }
