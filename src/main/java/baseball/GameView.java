0c9b9060b1f79e53fa3cf3e51bfcbd1a43b7309c
chaerim <qkrdbsk28@naver.com>
2022-11-07 04:48:26 +0900

    feat: 게임 종료 후 재시작 혹은 게임 종료 선택 기능

--- src/main/java/baseball/GameView.java
+++ src/main/java/baseball/GameView.java
@@ -8,14 +8,14 @@ public class GameView {
     private final String GAME_START_TEXT = "숫자 야구 게임을 시작합니다.";
     private final String INPUT_NUMBER_TEXT = "숫자를 입력해주세요 : ";
     private final String EXIT_GAME_TEXT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private final String RESTART_OR_EXIT_GAME_TEXT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void startGame() {
         System.out.println(GAME_START_TEXT);
         String status;
         do {
             status = this.proceedGame();
-        }
-        while (status.equals(RESTART_NUMBER));
+        } while (status.equals(RESTART_NUMBER));
     }
 
     private String proceedGame() {
@@ -32,11 +32,17 @@ public class GameView {
             } catch (IllegalArgumentException e) {
                 throw new IllegalArgumentException(e.getMessage());
             }
-
             System.out.println(game.startMatch(inputValue));
         }
         System.out.print(EXIT_GAME_TEXT);
 
-        return "";
+        System.out.println(RESTART_OR_EXIT_GAME_TEXT);
+        String status = Console.readLine();
+        try {
+            inputValidation.validateIsOneOrTwo(status);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException(e.getMessage());
+        }
+        return status;
     }
 }
