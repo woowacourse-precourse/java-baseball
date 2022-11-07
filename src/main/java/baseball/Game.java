76edca50b5fb1c34869cb689dfcc7a6913ff237f
JUNG-jihoon <73165763+JUNG-jihoon@users.noreply.github.com>
2022-11-07 12:26:48 +0900

    refactor : "{}줄 바꿈과 Strike와 Ball 초기화 예외사항 출력"
    {}를 가독성 좋게 바꿔줬으며 Strike_count 와 Ball_count를 초기화를 위해서 BaseNumber_Check 함수에 넣어줬습니다. 또한 게임 종료 후 1과2를 제외한 다른 숫자를 입력하면 예외 코드에 1 또는 2를 제외한 다른 숫자를 입력했습니다. 가 나오도록 하였습니다.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -5,77 +5,75 @@ import java.util.Objects;
 
 public class Game {
     Input_Output IO = new Input_Output();
-    private static int Strike_count=0;
-    private static int Ball_count=0;
+    private static int Strike_count = 0;
+    private static int Ball_count = 0;
 
 
-    public Game()
-    {
+    public Game() {
         init();
     }
-    public void init()
-    {
+
+    public void init() {
         IO.Computer_RandomNumber();//Input_Output에서 받은 컴퓨터의 수를 호출
     }
-    public void Game_Progress()
-    {
+
+    public void Game_Progress() {
         IO.User_enter();//Input_Output에서 받은 사용자의 수를 호출
         IO.User_File_Check();
-        BaseNumber_Check(IO.computer,IO.Scan);
+        BaseNumber_Check(IO.computer, IO.User);
         System.out.println(Print_Current_Game());
+        IO.User.clear();
+
+
     }
 
+    public String Print_Current_Game() {
+        if (Strike_count > 0 && Ball_count == 0)
+            return (Strike_count + "스트라이크");
 
+        if (Strike_count == 0 && Ball_count > 0)
+            return (Ball_count + "볼");
 
-    public String Print_Current_Game()
-    {
-        if(Strike_count>0 && Ball_count==0)
-            return (Strike_count+ "스트라이크");
-
-        if(Strike_count==0 && Ball_count>0)
-            return (Ball_count+"볼");
-
-        if(Strike_count > 0 && Ball_count > 0)
-            return (Strike_count + "스트라이크 "+ Ball_count + "볼");
+        if (Strike_count > 0 && Ball_count > 0)
+            return (Ball_count + "볼 "+Strike_count + "스트라이크 ");
 
         return ("낫싱");
     }
 
 
-    public void BaseNumber_Check(List<Integer> computer,List<Integer> User)//받은 값을 스트라이크와 볼인지 분류
+    public void BaseNumber_Check(List<Integer> computer, List<Integer> User)//받은 값을 스트라이크와 볼인지 분류
     {
-        for(int i=0; i<computer.size();i++)
-        {
-            if(Number_Check(computer.get(i),User.get(i)))
+        Strike_count = 0;
+        Ball_count = 0;
+        for (int i = 0; i < computer.size(); i++) {
+            if (Number_Check(computer.get(i), User.get(i)))
                 Strike_count++;
             else
-                Ball_Repeat(computer.get(i),User);
+                Ball_Repeat(computer.get(i), User);
 
         }
     }
 
-    public static boolean Ball_Repeat(int computer, List<Integer> User)//볼을 확인하기 위한 반복
+    public boolean Ball_Repeat(int computer, List<Integer> User)//볼을 확인하기 위한 반복
     {
-        for(int j =0; j<User.size();j++)
-        {
-            if(Number_Check(computer,User.get(j)))
+        for (int j = 0; j < User.size(); j++) {
+            if (Number_Check(computer, User.get(j)))
                 Ball_count++;
         }
 
         return false;
     }
 
-    public static boolean Number_Check(int computer,int User)//같은 숫자가 맞는지 확인
+    public boolean Number_Check(int computer, int User)//같은 숫자가 맞는지 확인
     {
-        if(Objects.equals(computer, User))
+        if (Objects.equals(computer, User))
             return true;
 
         return false;
     }
 
-    public boolean Ending_Progress()
-    {
-        if(Strike_count==3) {
+    public boolean Ending_Progress() {
+        if (Strike_count == 3) {
             String End_Number = IO.Ending_Enter();
             if (End_Number.equals("1")) {
                 init();
@@ -84,11 +82,10 @@ public class Game {
             if (End_Number.equals("2")) {
                 return false;
             }
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1 또는 2를 제외한 다른 숫자를 입력했습니다.");
         }
         return true;
     }
-
 
 
 }
