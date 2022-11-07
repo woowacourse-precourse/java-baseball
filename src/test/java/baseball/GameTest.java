7600361b78032a987761ac6875d04c5e5404114a
Park shinyeong <syngh503@gmail.com>
2022-11-07 22:14:48 +0900

    Test(GameTest): GameTest 수정 및 추가
    사용자 입력 값 체크 메서드가 isValidInput => isGameRestart로 변경되었습니다.
    관련된 메서드 명을 변경해주었습니다.
    사용자가 1, 2 외의 다른 숫자를 입력하는 경우도 추가해주었습니다.
    
    Relate to: #12

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -2,6 +2,9 @@ package baseball;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.InputMismatchException;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class GameTest {
@@ -11,7 +14,8 @@ public class GameTest {
     @DisplayName("사용자가 게임 재시작을 원할 경우")
     void case1() {
         String playerInput = "1";
-        boolean result = game.isValidInput(playerInput);
+        game.isGameRestart(playerInput);
+        boolean result = game.getGameStatus();
         assertThat(result).isTrue();
     }
 
@@ -19,17 +23,28 @@ public class GameTest {
     @DisplayName("사용자가 게임을 종료할 경우")
     void case2() {
         String playerInput = "2";
-        boolean result = game.isValidInput(playerInput);
-        assertThat(result).isTrue();
+        game.isGameRestart(playerInput);
+        boolean result = game.getGameStatus();
+        assertThat(result).isFalse();
     }
 
     @Test
-    @DisplayName("사용자가 1, 2 이외의 값을 입력한 경우")
+    @DisplayName("사용자가 1, 2 이외의 수를 입력한 경우")
     void case3() {
+        String playerInput = "3";
+        assertThatThrownBy(() -> game.isGameRestart(playerInput))
+                .isInstanceOf(InputMismatchException.class)
+                .hasMessageContaining("1과 2중 하나만 입력하세요");
+    }
+
+    @Test
+    @DisplayName("사용자가 1, 2 이외의 문자를 입력한 경우")
+    void case4() {
         String playerInput = "시작";
-        boolean result = game.isValidInput(playerInput);
-        assertThatThrownBy(() -> game.isValidInput(playerInput))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("재시작을 원하면 1, 종료를 원하면 2를 입력하세요");
+        game.isGameRestart(playerInput);
+        boolean result = game.getGameStatus();
+        assertThatThrownBy(() -> game.isGameRestart(playerInput))
+                .isInstanceOf(NumberFormatException.class)
+                .hasMessageContaining("1과 2중 하나만 입력하세요");
     }
 }
