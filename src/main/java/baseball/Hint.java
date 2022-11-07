3610fc1405219f6a43e7872fd9a6073ac4c9d238
linda2927 <linda2927@naver.com>
2022-11-08 00:23:26 +0900

    refac: adjust blank line placement for Hint Class

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -14,14 +14,16 @@ public class Hint {
     static List<Integer> result(List<Integer> userInput, Game game) {
         List<Integer> resultList = new ArrayList<>(Arrays.asList(0, 0));
         int ballCount = 0;
+
         for (int i = 0; i < game.getRandomNum().size(); i++) {
-            if (Objects.equals(game.getRandomNum().get(i), userInput.get(i)))
+            if (Objects.equals(game.getRandomNum().get(i), userInput.get(i))) {
                 resultList.set(1, resultList.get(1) + 1);
-            else {
+            } else {
                 ballCount += checkBall(userInput.get(i), game.getRandomNum());
             }
         }
         resultList.set(0, ballCount);
+
         return resultList;
     }
 
@@ -32,8 +34,9 @@ public class Hint {
      * @return 포함하고 있으면 1, 아니면 0
      */
     static int checkBall(Integer digit, List<Integer> answer) {
-        if(answer.contains(digit))
+        if (answer.contains(digit)) {
             return 1;
+        }
         return 0;
     }
 
@@ -42,19 +45,19 @@ public class Hint {
      * @param resultList result() 함수로 반환받은 볼과 스트라이크의 개수가 들어있는 리스트
      * @see Hint#result(List, Game)
      */
-
     static void printResult(List<Integer> resultList) {
         int ball = resultList.get(0);
         int strike = resultList.get(1);
-        if (ball == 0 && strike == 0)
+
+        if (ball == 0 && strike == 0) {
             System.out.println("낫싱");
-        else if (ball == 0 || strike == 0) {
-            if (resultList.indexOf(Integer.max(ball, strike)) == 0)
+        } else if (ball == 0 || strike == 0) {
+            if (resultList.indexOf(Integer.max(ball, strike)) == 0) {
                 System.out.println(ball + "볼");
-            else
+            } else {
                 System.out.println(strike + "스트라이크");
-        }
-        else {
+            }
+        } else {
             System.out.println(ball + "볼 " + strike + "스트라이크");
         }
     }
