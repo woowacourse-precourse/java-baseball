cc20e752b8d0cbd15eb6fa8e03ebe26a46cac1df
LeeJinHo <hosboy93@naver.com>
2022-11-07 16:42:17 +0900

    refactor($BaseballGame): 사용자 입력 값 유효성 검사 분리
    1. ErrorCheck 클래스 생성
    2. isValidNumber, isValidDiffer, isValidLength 메소드 이전(Baseball 클래스 -> ErrorCheck 클래스)
    
    fix($ErrorCheck): 사용자 입력 값이 모두 다른지 체크 오류
    
    1. second -> 0이 출력
    2. second = (userNumber%10)/10 -> second = (userNumber%100)/10으로 수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -20,43 +20,11 @@ public class BaseballGame {
     public static void gamePlay(){
         System.out.print("숫자를 입력하세요 : ");
         String user = Console.readLine();
-        if(isValidLength(user) && isValidDiffer(user) && isValidNumber(user)){
+        if(ErrorCheck.isValidLength(user) && ErrorCheck.isValidDiffer(user) && ErrorCheck.isValidNumber(user)){
             System.out.println(Integer.parseInt(user));
             System.out.println("올바른 값이 입력되었습니다.");
         }
     }
-
-    public static boolean isValidNumber(String user){
-        try{
-            Integer.parseInt(user);
-            return true;
-        }catch(Exception e){
-            throw new IllegalArgumentException();
-        }
-    }
-
-    public static boolean isValidDiffer(String user){
-        int userNumber = Integer.parseInt(user);
-
-        int first = userNumber/100;
-        int second = (userNumber%10)/10;
-        int third = userNumber%10;
-
-        if(first-second!=0 && second-third!=0 && third-first!=0){
-            return true;
-        }else{
-            throw new IllegalArgumentException();
-        }
-    }
-
-    public static boolean isValidLength(String user) {
-        if(user.length()!=3){
-            System.out.println("3자리 숫자가 아닙니다.");
-            throw new IllegalArgumentException();
-        }
-        return true;
-    }
-
 
     public static int randomNumberBall(){
         List<Integer> computer = new ArrayList<>();
