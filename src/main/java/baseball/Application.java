0b03f11dd94f6cdf7d3c2977096e80c09a803474
YusunPark <solarpy.kor@gmail.com>
2022-11-07 22:44:14 +0900

    feat: Add message for user input
    #2. 사용자 입력을 위한 문구를 출력한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
 public class Application {
+
+    static void startMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    static void getUserNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        startMessage();
+        getUserNumber();
     }
 }
