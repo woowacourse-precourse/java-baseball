4f3d90b11c553ffd51f1de60feeb539db7ae7b89
YongGwan <32181771@dankook.ac.kr>
2022-11-07 14:44:47 +0900

    재시작 값 처리 부분 return 값 변경

--- src/main/java/baseball/CheckException.java
+++ src/main/java/baseball/CheckException.java
@@ -2,16 +2,9 @@ package baseball;
 
 public class CheckException {
 
-    private static void occurException() {
-        throw new IllegalArgumentException();
+    public static boolean restartStatusValue(String restartStatus) {
+
+        return !(restartStatus.equals("1") || restartStatus.equals("2"));
     }
 
-    public static void restartStatusValue(String restartStatus) {
-        try {
-            if(!(restartStatus.equals("1") || restartStatus.equals("2")))
-                occurException();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e +" 재시작 관련 값을 잘못 입력하셨습니다. 게임을 종료합니다.");
-        }
-    }
 }
