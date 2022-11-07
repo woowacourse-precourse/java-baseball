cbc0c538eb1e2bee0afa42bfa490e82de63042d9
Jaesun Park <41979078+ppakjae@users.noreply.github.com>
2022-11-08 00:49:04 +0900

    feat: 컴퓨터 임의의 3자리 수 선택

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        while (true) {
+            List<Integer> computer = initializeGame();
+
+        }
+    }
+
+    private static List<Integer> initializeGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.\n");
+
+        return selectRandomNumbers();
+    }
+    private static List<Integer> selectRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
     }
 }
