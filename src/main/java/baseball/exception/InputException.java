dc0164eb65c3fb07e301730f7bf52457b75f0939
TaeHwan <kimta2hwan@gmail.com>
2022-11-07 11:51:58 +0900

    feat(inputexception): 사용자가 입력한 정수가 3자리의 수가 아닌 경우 예외 처리 기능 구현

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -3,8 +3,13 @@ package baseball.exception;
 public class InputException {
 
     private static final String NOT_INTEGER_INPUT = "정수를 입력해 주세요.";
+    private static final String NOT_THREE_LENGTH_INTEGER_INPUT = "3자리의 정수를 입력해 주세요.";
 
     public static void throwNotIntegerInputError() {
         throw new IllegalArgumentException(NOT_INTEGER_INPUT);
+    }
+
+    public static void throwNotThreeLengthIntegerInputError() {
+        throw new IllegalArgumentException(NOT_THREE_LENGTH_INTEGER_INPUT);
     }
 }
