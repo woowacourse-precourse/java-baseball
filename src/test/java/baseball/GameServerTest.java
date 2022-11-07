8e1b2ac78e9a98ae890b91b10f2420c3f766f772
gwanhwi <50892715+gwanhwi@users.noreply.github.com>
2022-11-08 07:31:49 +0900

    feat(GameServerTest) : 사용자 입력에 대한 테스트 구현
    정답을 맞힐 때 서로다른 3자리수에 대한 테스트와
    게임이 끝난 후 1과 2중 하나의 수인지에 대한 테스트

--- src/test/java/baseball/GameServerTest.java
+++ src/test/java/baseball/GameServerTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GameServerTest {
     @Test
@@ -45,5 +46,43 @@ public class GameServerTest {
         gameserver.checkUsersInputIsAnswer();
         assertThat(gameserver.strike).isEqualTo(0);
         assertThat(gameserver.ball).isEqualTo(0);
+    }
+
+    @Test
+    void 사용자_입력_서로다른3자리수() {
+        GameServer gameserver = new GameServer();
+        assertThrows(IllegalArgumentException.class, () -> {
+            gameserver.catchInputException("111");
+        });
+    }
+
+    @Test
+    void 사용자_입력_3자리수_아닌경우() {
+        GameServer gameserver = new GameServer();
+        assertThrows(IllegalArgumentException.class, () -> {
+            gameserver.catchInputException("1234");
+        });
+    }
+
+    @Test
+    void 사용자_입력_숫자_아닌거포함() {
+        GameServer gameserver = new GameServer();
+        assertThrows(IllegalArgumentException.class, () -> {
+            gameserver.catchInputException("a12");
+        });
+    }
+    @Test
+    void 사용자_입력_게임끝난경우_1() {
+        GameServer gameserver = new GameServer();
+        assertThrows(IllegalArgumentException.class, () -> {
+            gameserver.decideRestart("3");
+        });
+    }
+    @Test
+    void 사용자_입력_게임끝난경우_2() {
+        GameServer gameserver = new GameServer();
+        assertThrows(IllegalArgumentException.class, () -> {
+            gameserver.decideRestart("123");
+        });
     }
 }
