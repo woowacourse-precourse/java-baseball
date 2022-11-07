6f2148744cb25992b7ccc33bc6425b48fac32696
YE-EUN <musiclove529@naver.com>
2022-11-08 04:40:31 +0900

    feat(ProblemSolver) : solveProblem() 구현
    - 사용자로부터 서로 다른 3자리 수 입력 받기
    - 예외 처리 -> IllegalArgumentException
      - 3자리 수가 아닐 경우
      - 각 자리수가 서로 다르지 않을 경우

--- src/main/java/baseball/ProblemSolver.java
+++ src/main/java/baseball/ProblemSolver.java
@@ -1,7 +1,22 @@
 package baseball;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class ProblemSolver extends Player{
-    public void solveProblem(int sizeOfProblem){
+    public void solveProblem(int sizeOfProblem) throws IllegalArgumentException{
+        String answer = Console.readLine();
+
+        if(!answer.matches("^[1-9]{3}$")){
+            throw new IllegalArgumentException();
+        }else if(answer.chars().distinct().count() != Settings.SIZE_OF_NUMBERS){
+            throw new IllegalArgumentException();
+        }
+
+        for(int i = 0;i<answer.length();i++){
+            int number = Character.getNumericValue(answer.charAt(i));
+            addNumber(number);
+        }
 
     }
 
