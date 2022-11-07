8d88e191edb68aa247572fcfb5fe1c930de1e5ea
yenawee <yaena0319@naver.com>
2022-11-07 18:41:32 +0900

    feat. 서로 다른 숫자인지 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -28,19 +28,26 @@ class User {
     public static List<Integer> getInputNumber(){
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        if (input.length() > 3){
+        if (input.length() > INPUT_LENGTH){
             throw new IllegalArgumentException();
         }
-        for (int idx = 0; idx < 3; idx++){
+        for (int idx = 0; idx < INPUT_LENGTH; idx++){
             if (input.charAt(idx) < '1' || input.charAt(idx) > '9'){
                 throw new IllegalArgumentException();
             }
         }
+        List<Integer> inputNum = new ArrayList<>();
+        for (int idx = 0; idx < INPUT_LENGTH; idx++){
+            inputNum.add(input.charAt(idx) -'0');
+        }
+        int num1 = inputNum.get(0);
+        int num2 = inputNum.get(1);
+        int num3 = inputNum.get(2);
 
-
-
-
-
+        if (num1 == num2 || num2 == num3 || num1 == num3){
+            throw new IllegalArgumentException();
+        }
+        return inputNum;
     }
 
 
