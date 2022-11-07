c65d034e96545cd4a31512167f353d061b2a3246
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-07 17:13:18 +0900

    test(ApplicationTest): add test for parsing Input to restart application

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -20,6 +20,13 @@ class ApplicationTest extends NsTest {
         assertThat(output()).isEqualTo("숫자 야구 게임을 시작합니다.");
     }
 
+    @DisplayName("게임 종료 후 재시작 질문 출력 테스트")
+    @Test
+    void printGameRestartMessageTest() {
+        Application.printGameRestartMessage();
+        assertThat(output()).isEqualTo("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
     @DisplayName("게임 종료 후 사용자 입력 테스트")
     @Test
     void parseUserInputForRestartTest() {
