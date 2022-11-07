90a0c6115aafcc4cd4361ca6e23664726eb7ddeb
yoon <beatus96@naver.com>
2022-11-08 04:20:31 +0900

    test(computernumber) : model 클래스의 컴퓨터수 생성하기 기능 테스트
    - 생성된 컴퓨터 수의 범위 올바른지
    - 생성된 컴퓨터 수가 중복되지 않는지

---
+++ src/test/java/baseball/ComputerNumberTest.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ComputerNumberTest {
+    @Test
+    void 컴퓨터수_범위_테스트() {
+        //when
+        List<Integer> computerNum = Model.createComputerNumber();
+        //then
+        assertThat(computerNum.stream().allMatch(v -> 1 <= v && v <= 9)).isTrue();
+    }
+
+    @Test
+    void 컴퓨터수_서로다른수_테스트() {
+        //given
+        Set<Integer> numbers = new HashSet<>();
+        //when
+        List<Integer> computerNum = Model.createComputerNumber();
+        numbers.addAll(computerNum);
+        //then
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
