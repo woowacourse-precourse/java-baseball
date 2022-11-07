0044bee21a04f7b138e0c0815af00edbbc91b8e3
이석환 <2018008886@hanyang.ac.kr>
2022-11-07 20:03:41 +0900

    feat: add 게임 진행 반복

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,9 +1,73 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class GameManager {
+    private final String START_GAME_ALERT = "숫자 야구 게임을 시작합니다.";
+    private final String PUT_INPUT_ALERT = "숫자를 입력해주세요 : ";
+    private final String GAME_END_ALERT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private final String AFTER_GAME_END_ALERT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
     private TargetNumber oppositeNumber;
 
     public void runGame() {
-        // TODO 반복하여 input을 받으며 게임을 진행
-   }
+        do {
+            printStartGameAlert();
+            oppositeNumber = TargetNumber.getRandomInstance();
+
+            repeatGetInputAndCompare();
+        } while(getAndProcessRestartInput());
+    }
+
+    private void repeatGetInputAndCompare() {
+        BallStrikeResult result;
+        do {
+            TargetNumber inputNumber = getAndProcessTargetNumberInput();
+            result = oppositeNumber.compareTo(inputNumber);
+            printBallStrikeResult(result);
+        } while(!result.doesWin());
+        printGameEndAlert();
+    }
+
+    private TargetNumber getAndProcessTargetNumberInput() {
+        printInputNumberAlert();
+        String inputString = Console.readLine();
+        if(!UtilMethods.isNumeric(inputString)) {
+            throw new IllegalArgumentException();
+        }
+
+        return new TargetNumber(Integer.parseInt(inputString));
+    }
+
+    private boolean getAndProcessRestartInput() throws IllegalArgumentException {
+        printAfterGameEndAlert();
+        String input = Console.readLine();
+
+        if(input.equals("1")) {
+            return true;
+        } else if(input.equals("2")) {
+            return false;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private void printBallStrikeResult(BallStrikeResult result) {
+        System.out.println(result.toString());
+    }
+
+    private void printStartGameAlert() {
+        System.out.println(START_GAME_ALERT);
+    }
+
+    private void printAfterGameEndAlert() {
+        System.out.println(AFTER_GAME_END_ALERT);
+    }
+
+    private void printGameEndAlert() {
+        System.out.println(GAME_END_ALERT);
+    }
+
+    private void printInputNumberAlert() {
+        System.out.print(PUT_INPUT_ALERT);
+    }
 }
