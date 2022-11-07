27954094d7971dfea270316942f5f72633a0bbcc
chobobdev <chobobdev@naver.com>
2022-11-08 01:47:40 +0900

    feat : get user input and split them into ArrayList

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,7 +16,19 @@ public class Application {
         }
         return computer;
     }
+
+    public static List<Integer> UserNum(String usernum){
+        List<Integer> usernumArray = new ArrayList<>();
+        for(int i=0; i<usernum.length(); i++)
+            usernumArray.add(usernum.charAt(i) - '0');
+        return usernumArray;
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.print("숫자를 입력해주세요 : ");
+        String number = Console.readLine();
+        System.out.println(UserNum(number));
     }
 }
