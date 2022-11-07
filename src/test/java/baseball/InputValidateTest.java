0c0bd6853f73afcefd482d49bef681a6347802c0
baksohyeon <bori12370@gmail.com>
2022-11-07 21:38:44 +0900

    test: 변수 이름 변경

--- src/test/java/baseball/InputValidateTest.java
+++ src/test/java/baseball/InputValidateTest.java
@@ -8,14 +8,15 @@ public class InputValidateTest {
     @Test
     void isValidInput() {
 
-        int EXAMPLE_INVALID_NUMBER_1 = 12345;
-        int EXAMPLE_INVALID_NUMBER_2 = 333;
-        int EXAMPLE_VALID_NUMBER_1 = 357;
-        int EXAMPLE_VALID_NUMBER_2 = 123;
 
-        assertThat(InputValidator.isValidNumbers(EXAMPLE_INVALID_NUMBER_1) && InputValidator.hasDuplicatedNumber(EXAMPLE_INVALID_NUMBER_1)).isFalse();
-        assertThat(InputValidator.isValidNumbers(EXAMPLE_INVALID_NUMBER_2) && InputValidator.hasDuplicatedNumber(EXAMPLE_INVALID_NUMBER_2)).isFalse();
-        assertThat(InputValidator.isValidNumbers(EXAMPLE_VALID_NUMBER_1) && InputValidator.hasDuplicatedNumber(EXAMPLE_VALID_NUMBER_1)).isTrue();
-        assertThat(InputValidator.isValidNumbers(EXAMPLE_VALID_NUMBER_2) && InputValidator.hasDuplicatedNumber(EXAMPLE_VALID_NUMBER_2)).isTrue();
+        String INVALID_NUMBER_1 = "12345";
+        String INVALID_NUMBER_2 = "333";
+        String VALID_NUMBER_1 = "357";
+        String VALID_NUMBER_2 = "123";
+
+        assertThat(InputValidator.isValidNumbers(INVALID_NUMBER_1) && InputValidator.hasDuplicatedNumber(INVALID_NUMBER_1)).isFalse();
+        assertThat(InputValidator.isValidNumbers(INVALID_NUMBER_2) && InputValidator.hasDuplicatedNumber(INVALID_NUMBER_2)).isFalse();
+        assertThat(InputValidator.isValidNumbers(VALID_NUMBER_1) && InputValidator.hasDuplicatedNumber(VALID_NUMBER_1)).isTrue();
+        assertThat(InputValidator.isValidNumbers(VALID_NUMBER_2) && InputValidator.hasDuplicatedNumber(VALID_NUMBER_2)).isTrue();
     }
 }
