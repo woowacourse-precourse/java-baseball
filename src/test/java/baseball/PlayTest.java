367f3b93fc2308e1938ed399f9327bae56b66dab
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-11-07 15:56:09 +0900

    test(Play): getNubmerFrom throws exception when passed invalid input

--- src/test/java/baseball/PlayTest.java
+++ src/test/java/baseball/PlayTest.java
@@ -2,8 +2,11 @@ package baseball;
 
 import java.util.List;
 import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayTest {
 
@@ -15,5 +18,12 @@ class PlayTest {
                 .hasSize(Play.getCount())
                 .isSubsetOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
                 .doesNotHaveDuplicates();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "", "12", "3456", "788", "9a8", "$76" })
+    void getNumberFrom_throws_IllegalArgumentException(String input) {
+        assertThatThrownBy(() -> Play.getNumberFrom(input))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
