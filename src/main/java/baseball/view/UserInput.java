3beca6581f8a5d2a045f824210ff23e8c0cc688c
이건회 <rg970604@naver.com>
2022-11-07 18:47:42 +0900

    feat (UserInput.java) : 게임 종료 여부를 결정할 문자열 입력

--- src/main/java/baseball/view/UserInput.java
+++ src/main/java/baseball/view/UserInput.java
@@ -3,8 +3,14 @@ package baseball.view;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
+
     public static String InputUserNumbers() {
         System.out.println("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+
+    public static String SelectRestartOrExit() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
 }
