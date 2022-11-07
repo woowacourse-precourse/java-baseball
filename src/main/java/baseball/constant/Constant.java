9fb031d6005744ecb91dd5e8964673f2942100ed
chanminmaxscaler <jcm04@naver.com>
2022-11-07 15:54:58 +0900

    feat: 코드 작성 시 사용할 상수를 모아둔 Constant 클래스를 구현한다
    Constant 클래스는 생성자, 기타 메소드 없이 상수들의 필드만 갖는다. 이 상수들은 여러 클래스에서 사용된다.

---
+++ src/main/java/baseball/constant/Constant.java
@@ -0,0 +1,10 @@
+package baseball.constant;
+
+public class Constant {
+
+    public final static String SPACE_STRING = " ";
+    public final static String NEW_LINE_STRING = "\n";
+    public final static String EMPTY_STRING = "";
+    public final static String INTEGER_FORMAT_SPECIFIER = "%d";
+    public final static int ZERO_NUM = 0;
+}
