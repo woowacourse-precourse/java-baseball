dcd16e70294750df182ec4ed99c30653bb0f1079
yujindonut <hyujin0825@gmail.com>
2022-11-08 00:16:55 +0900

    feat : 상수 객체 구현
    출력문, 변수들을 쉽게 관리하도록 상수객체를 구현했습니다.

---
+++ src/main/java/baseball/View/Constant.java
@@ -0,0 +1,20 @@
+package baseball.View;
+
+public class Constant {
+
+  public static final int NUMBER_LENGTH = 3;
+  public static final int MIN_VALUE = 1;
+  public static final int MAX_VALUE = 9;
+  public static final int RETRY = 1;
+  public static final int GAME_OVER = 2;
+  public static final int GAME_WIN_VALUE = 3;
+
+  public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+  public static final String USER_WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+  public static final String RESTART_MESSAGE = "게임을 시작하려면 1, 종료하려면 2를 입력하세요.";
+  public static final String GAME_END_MESSAGE = "게임이 종료되었습니다.";
+  public static final String STRIKE = "스트라이크 ";
+  public static final String BALL = "볼 ";
+  public static final String NOTHING = "낫싱";
+  public static final String WRONG_INPUT = "잘못된 입력입니다.";
+}
