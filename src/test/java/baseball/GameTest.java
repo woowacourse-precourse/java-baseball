f485f621828bc2c0892b78cbd179cb834df0b3be
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-07 23:30:47 +0900

    test(GameTest): add testes for user input exceptions

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -51,6 +51,31 @@ public class GameTest extends NsTest {
         testNumber.add(3);
         assertThat(game.parseUserInput(testInput)).isEqualTo(testNumber);
     }
+
+    @DisplayName("유저 입력 예외 검사 테스트1 - 입력이 3자리 이상일 경우")
+    @Test
+    void isUserNumberValidExceptionTest1() {
+        String testInput = "1234";
+        List<Integer> testNumber = game.parseUserInput(testInput);
+        assertThatThrownBy(() -> game.isUserNumberValid(testNumber)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("유저 입력 예외 검사 테스트2 - 1~9 사이의 숫자가 아닐 경우")
+    @Test
+    void isUserNumberValidTest2() {
+        String testInput = "012";
+        List<Integer> testNumber = game.parseUserInput(testInput);
+        assertThatThrownBy(() -> game.isUserNumberValid(testNumber)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("유저 입력 예외 검사 테스트3 - 중복된 숫자가 있을 경우")
+    @Test
+    void isUserNumberValidTest3() {
+        String testInput = "332";
+        List<Integer> testNumber = game.parseUserInput(testInput);
+        assertThatThrownBy(() -> game.isUserNumberValid(testNumber)).isInstanceOf(IllegalArgumentException.class);
+    }
+
     }
 
     @Override
