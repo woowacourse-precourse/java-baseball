a2c1a77f84f0280998f85b9f2409e5266f7ea546
Jeangyu <gjwlsrb1700@naver.com>
2022-11-07 17:52:00 +0900

    [REFACTOR] UserOutput Class Constants화 작업 진행

--- src/main/java/baseball/utils/BaseballConstants.java
+++ src/main/java/baseball/utils/BaseballConstants.java
@@ -5,6 +5,11 @@ public class BaseballConstants {
     public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
     public static final String WRITE_NUMBER = "숫자를 입력해주세요 : ";
     public static final String RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+    public static final String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
     public static final String PLAY_AGAIN = "1";
     public static final int BALL_NUMBER = 3;
     public static final int MIN_NUMBER = 1;
--- src/main/java/baseball/utils/UserOutput.java
+++ src/main/java/baseball/utils/UserOutput.java
@@ -1,23 +1,25 @@
 package baseball.utils;
 
+import static baseball.utils.BaseballConstants.*;
+
 public class UserOutput {
 
     public static void printResult(int ball, int strike){
         String result = "";
 
         if (ball != 0) {
-            result += ball + "볼";
+            result += ball + BALL;
         }
         if (strike != 0) {
-            result += " " + strike + "스트라이크";
+            result += " " + strike + STRIKE;
         }
         if (result.length() == 0) {
-            result = "낫싱";
+            result = NOTHING;
         }
 
         System.out.println(result.strip());
-        if (strike == 3){
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        if (strike == BALL_NUMBER){
+            System.out.println(GAME_OVER);
         }
     }
 }
