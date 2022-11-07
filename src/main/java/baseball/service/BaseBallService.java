8374f505b09f8b7ddd873863d5ba22bbf233f452
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-08 01:45:41 +0900

    feat: 게임 초기화 서비스 로직 추가

--- src/main/java/baseball/service/BaseBallService.java
+++ src/main/java/baseball/service/BaseBallService.java
@@ -1,5 +1,15 @@
 package baseball.service;
 
-public class BaseBallService {
+import baseball.model.BaseBall;
+import baseball.model.Computer;
+import baseball.model.User;
 
+public class BaseBallService {
+    private BaseBall baseBall = new BaseBall();
+    private User user = new User();
+    private Computer computer = new Computer();
+
+    public void initGame() {
+        computer.randomGenerate();
+    }
 }
