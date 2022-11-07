51000ccfcff42f5d03581445555d7ff125950b2a
michaelp3 <chan-park@hotmail.com>
2022-11-08 02:03:44 +0900

    feat($location): generateThreeDigitNumber function
    Added function generateThreeDigitNumber
    Generates random three digit number
    Added tests for function generateThreeDigitNumber

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
+    public static int[] generateThreeDigitNumber() {
+        int[] threeDigitNum = new int[3];
+        threeDigitNum[0] = Randoms.pickNumberInRange(1, 9);
+        threeDigitNum[1] = Randoms.pickNumberInRange(0, 9);
+        threeDigitNum[2] = Randoms.pickNumberInRange(0, 9);
+        return threeDigitNum;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다");
+
+        int[] randomNumber = generateThreeDigitNumber();
     }
 }
