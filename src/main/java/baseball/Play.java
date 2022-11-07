b7de70312d5ada8e175c1ff578c45546e40401b3
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-11-07 13:15:24 +0900

    feat(Play): define static initialiser block
    Initialise static final COUNT to number of expected digits.
    Initialise NUMBER to the compiled representation of a regular
    expression that matches a string of COUNT unique nonzero digits.

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -3,9 +3,18 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 class Play {
-    private static final int COUNT = 3;
+    private static final int COUNT;
+    private static final Pattern NUMBER;
+
+    static {
+        COUNT = 3;
+        String regex = String.format("^(?:([1-9])(?!.*\\1)){%d}$", COUNT);
+        NUMBER = Pattern.compile(regex);
+    }
+
     /**
      * Enforce class non-instantiability with private constructor.
      */
