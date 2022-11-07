79a9f734b063d8a9753cafe40508936825aa4d17
the9kim <96895686+the9kim@users.noreply.github.com>
2022-11-08 08:24:49 +0900

    feat: 게임 결과 화면 출력 메서드(printGameResult()) 구현
    아래 경우의 수로 나누어 해당 결과를 출력
    스트라이크 볼 모두 존재 하는 경우
    스트라이크만 존재하는 경우
    볼만 존재하는 경우
    스트라이크와 볼 모두 존재하지 않는 경우 - 낫싱

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -11,4 +11,18 @@ public class OutputView {
     public static void printRequestNumber(){
         String input = Console.readLine();
     }
+
+    public static void printGameResult(int strikeCounter, int ballCounter){
+        if (strikeCounter > 0 && ballCounter > 0){
+            System.out.println(ballCounter+"볼"+" "+strikeCounter+"스트라이크");
+        }
+        if (strikeCounter > 0 & ballCounter == 0){
+            System.out.println(strikeCounter+"스트라이크");
+        }
+        if (strikeCounter == 0 & ballCounter > 0){
+            System.out.println(ballCounter+"볼");
+        }
+
+        System.out.println("낫싱");
+    }
 }
