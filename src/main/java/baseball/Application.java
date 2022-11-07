f8124e087926bc059558cba5120c2673cef8a7bc
Wonkyun2 <kimwon1031@hanmail.net>
2022-11-08 02:47:37 +0900

    feat(exception): 잘못된 입력에 대한 오류처리
    게임을 시작하고 진행중에 입력하는 값에 대하여 숫자인지, 0이 들어가 있는지, 3자리의 숫자인지, 서로 다른 숫자인지 판별하는 메소드를 생성.
    게임이 종료되었을 때 입력하는 값이 1또는 2인지 확인.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,8 +18,56 @@ class computer {
     }
 }
 
+class inputException {
+    void judgeNotNumber(String a) {
+        try {
+            Integer.parseInt(a);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력하세요");
+        }
+    }
+    
+    void judgeIncludeZero(String a) {
+        if (a.contains("0")) {
+            throw new IllegalArgumentException("1부터 9까지의 숫자를 입력하세요");
+        }
+    }
+
+    void judgeRange(String a) throws IllegalArgumentException {
+        int i = Integer.parseInt(a);
+        if (i<100 || i >999) {
+            throw new IllegalArgumentException("세 개의 숫자를 입력하세요");
+        }
+    }
+
+    void judgeDifferentNumber(String a) {
+        int i = Integer.parseInt(a);
+        int firstNumber = i/100;
+        int secondNumber =  (i - 100*firstNumber)/10;
+        int lastNumber = i - 100*firstNumber - 10*secondNumber;
+        if (firstNumber == secondNumber || secondNumber == lastNumber || lastNumber == firstNumber) {
+            throw new IllegalArgumentException("서로 다른 숫자를 입력하세요");   
+        }
+    }
+
+    void judgeException(String a) {
+        this.judgeNotNumber(a);
+        this.judgeIncludeZero(a);
+        this.judgeRange(a);
+        this.judgeDifferentNumber(a);
+    }
+
+    void restartJudgeException(String a) {
+        this.judgeNotNumber(a);
+        int i = Integer.parseInt(a);
+        if(i!=1 || i!=2) {
+            throw new IllegalArgumentException("다시 시작하세요");
+        }
+    }
+}
+
 public class Application {
     public static void main(String[] args) {
-        
+    
     }
 }
