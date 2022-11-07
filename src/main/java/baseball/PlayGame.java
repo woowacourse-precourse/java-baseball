082807a4da7d7b8ea0d90d3ad7111cf2daae0d39
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-08 03:46:30 +0900

    feat: get random number from users

--- src/main/java/baseball/PlayGame.java
+++ src/main/java/baseball/PlayGame.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -17,5 +18,28 @@ public class PlayGame {
         }
 
         return gameNumbers;
+    }
+
+    public static List<Integer> getUserNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+
+        try{
+            int input = Integer.parseInt(Console.readLine());
+
+            if (input < 100 || input > 999) {
+                throw new IllegalArgumentException("게임종료: 숫자의 갯수가 3개가 아닙니다.");
+            }
+
+            List<Integer> userNumbers = new ArrayList<>();
+
+            for(int i = 0; i < 3; i ++){
+                userNumbers.add(0, input % 10);
+                input = input / 10;
+            }
+            return userNumbers;
+
+        } catch (IllegalArgumentException e)  {
+            throw new IllegalArgumentException("게임종료: 입력이 숫자가 아닙니다.");
+        }
     }
 }
