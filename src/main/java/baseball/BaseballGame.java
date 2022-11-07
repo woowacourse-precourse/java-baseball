64bd7afa3a984ac903ab67c086f1448f6a7ee93e
silver <silver36c@gmail.com>
2022-11-08 07:29:41 +0900

    feat: 랜덤 숫자 생성 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -32,7 +32,12 @@ public class BaseballGame {
     class Computer{
         List<Integer> setRandomNumber(){
             List<Integer> computer = new ArrayList<>();
-
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                }
+            }
             return computer;
         }
 
