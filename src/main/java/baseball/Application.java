c44c464044e05850112317b9ae4d13c4dad84fde
박시균\psk98 <psg980331@naver.com>
2022-11-07 11:32:06 +0900

    feat: 컴퓨터의 랜덤수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static boolean createComputerNum(List<Integer> computer){
+        while(computer.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        if(computer.size() == 3)
+            return true;
+        else
+            return false;
     }
 }
