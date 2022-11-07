a742882be22ea41c6ee5194b1e2886ed15ad2530
Jeeseob <jeeseob5761@gmail.com>
2022-11-07 16:57:50 +0900

    Add: 메인함수 구현부 - 기본 틀 및 TODO 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,14 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // TODO: 출력 Comment 추가
+        do {
+            // TODO: 게임 시작
+        } while (isRestart());
+    }
+
+    // TODO: 게임 재시작 여부
+    private static boolean isRestart() {
+        return true;
     }
 }
