cc3e08d8aa36534ea8a2812ca637a3a7bab1c78b
Isu Shin <isu010614@naver.com>
2022-11-07 16:34:11 +0900

    feat: Add function to count strikes
    After comparing the number of the user and opponent, if both the position and the number are the same, it is counted

---
+++ src/main/java/baseball/Count.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import java.util.List;
+
+public class Count {
+    public static int Strike(List<Integer> opponent, List<Integer> user) {
+        int strike = 0;
+        for (int i = 0; i < Number.NUMBERS_SIZE; i++) {
+            int opponentNumber = opponent.get(i);
+            int userNumber = user.get(i);
+            strike += compareNumber(opponentNumber, userNumber);
+        }
+        return strike;
+    }
+
+    public static int compareNumber(int opponent, int user) {
+        if (opponent == user) {
+            return 1;
+        }
+        return 0;
+    }
+}
