6abf1cb710da7317fc9234a99dfa1ebe6e8eef03
cocomozila <joseyun94@naver.com>
2022-11-07 11:10:46 +0900

    feat: 게임 시작 안내 메세지

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -1,4 +1,20 @@
 package baseball.service;
 
+import static baseball.message.SystemMessage.*;
+
+import baseball.domain.BaseballMachine;
+import baseball.util.Input;
+
 public class BaseballService {
+
+    private BaseballMachine baseballMachine;
+
+    public BaseballService() {
+        this.baseballMachine = new BaseballMachine();
+    }
+
+    // 인사
+    public void welcome() {
+        System.out.println(GAME_START_MESSAGE);
+    }
 }
