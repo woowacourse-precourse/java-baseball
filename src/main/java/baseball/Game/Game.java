df31334b8f6b10950139c303bc40aab675f0f53d
Ridealist <cjsn92@gmail.com>
2022-11-08 01:05:55 +0900

    chore: Game 클래스명 변경 시도 후 다시 취소

---
+++ src/main/java/baseball/Game/Game.java
@@ -0,0 +1,43 @@
+package baseball.Game;
+
+import baseball.Players.Computer;
+import baseball.Players.UserChoice;
+import baseball.Players.UserNumber;
+import java.util.List;
+
+public class Game {
+
+    private static final int GAME_OVER_STRIKE_COUNT = 3;
+
+    private boolean roundContinue = true;
+
+    private final List<Integer> computerNumbers;
+
+    private Game() {
+        Computer computer = new Computer();
+        List<Integer> computerNumbers = computer.getNumberList();
+        this.computerNumbers = computerNumbers;
+    }
+
+    public static void startGame() {
+        Game game = new Game();
+        do {
+            game.getRoundScore();
+        } while (game.roundContinue);
+    }
+
+    private void getRoundScore() {
+        UserNumber userNumber = new UserNumber();
+        List<Integer> userNumbers = userNumber.getNumberList();
+        Judge.getRoundScore(userNumbers, computerNumbers);
+        if (Judge.countStrike(userNumbers, computerNumbers) == GAME_OVER_STRIKE_COUNT) {
+            roundContinue = false;
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+    }
+
+    public static String ResetGameOrQuit() {
+        UserChoice userChoice = new UserChoice();
+        return userChoice.getResetOrQuit();
+    }
+}
