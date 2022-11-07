7977b1290491afbb520926ef6625497f02012547
LeeMyeongJin <=>
2022-11-07 20:57:49 +0900

    Feat: Player class 생성

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+    public static String getNumber(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String userNumber = Console.readLine();
+        return userNumber;
+    }
+}
