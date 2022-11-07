f65b22d45aa8523be7b0bb4a648a02f35ff13761
zaysverse <zeze@jbnu.ac.kr>
2022-11-08 00:14:10 +0900

    Feat : Print the result of this round
    입력된 값과 비교한 결과를 출력한다
    ex) 1볼 1스트라이크
        3스트라이크
        낫싱

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -44,6 +44,19 @@ public class Baseball {
             else ball++;
         }
 
+        printResult();
         return strike == 3;
+    }
+
+    private void printResult() {
+        if (ball > 0)
+            System.out.println(ball + "볼 ");
+        if (strike > 0)
+            System.out.println(strike + "스트라이크 ");
+        if (ball == 0 && strike == 0)
+            System.out.println("낫싱");
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
     }
 }
