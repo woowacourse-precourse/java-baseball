c8cb24afcb71ba376eee2408698353f1b92a13c6
nahyeon cho <whskgus12@nate.com>
2022-11-07 16:04:52 +0900

    feat : 플레이어의 입력 값 예외처리 공통사항
    플레이어의 입력 값이 들어오면 확인 해야 할 예외 사항들을 한번에 처리하는 함수입니다. 세 자리 수 인지 먼저 확인 후 중복이 있는지 확인합니다.

--- src/main/java/baseball/PlayerInputValueExceptions.java
+++ src/main/java/baseball/PlayerInputValueExceptions.java
@@ -4,6 +4,11 @@ import java.util.ArrayList;
 
 public class PlayerInputValueExceptions {
 
+    static void allInputValueException(ArrayList<String> userSelectedNumbers) {
+        isThreeDigitsNumber(userSelectedNumbers);
+        haveDuplicateNumbers(userSelectedNumbers);
+    }
+
     static void isThreeDigitsNumber(ArrayList<String> userSelectedNumbers) {
         if (userSelectedNumbers.size() != 3) {
             throw new IllegalArgumentException("3자리만 입력하세요");
@@ -16,7 +21,7 @@ public class PlayerInputValueExceptions {
         }
     }
 
-    static void whenExitingNumberOneOrTwo(String userSelectedNumber) {
+    static void whenExitingIsNumberOneOrTwo(String userSelectedNumber) {
         if (!(userSelectedNumber.equals("1") || userSelectedNumber.equals("2"))) {
             throw new IllegalArgumentException("1 또는 2만 입력하세요");
         }
