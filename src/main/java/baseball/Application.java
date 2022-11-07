941d1eacd8f235057893f5598bda884931fc5839
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-08 00:24:19 +0900

    feat(setGameSetting): 컴퓨터 숫자를 맞힐 경우 재시작 또는 종료에 대한 입력을 받음

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -70,7 +70,7 @@ public class Application {
             if (strike == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
+                endPoint = setGameSetting();
             }
         } else if ((strike == 0) && (ball > 0)) {
             System.out.printf("%d볼%n", ball);
@@ -78,6 +78,19 @@ public class Application {
             System.out.println("낫싱");
         }
         return endPoint;
+    }
+
+    public static boolean setGameSetting() {
+        boolean gameSetting;
+        String gameChoice = Console.readLine();
+        if (gameChoice.equals("1")) {
+            gameSetting = true;
+        } else if (gameChoice.equals("2")) {
+            gameSetting = false;
+        } else {
+            throw new IllegalArgumentException();
+        }
+        return gameSetting;
     }
 
     public static void main(String[] args) {
