29ddf7241e24b50111b515bd9d77b748dc2eeefa
Eungyu-code <Agyu625@gmail.com>
2022-11-07 21:53:03 +0900

    update(replayGame()): 입력하는 방법을 Console.readLine()으로 변경
    요구사항에 맞도록 Console.readLine()으로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -204,9 +204,8 @@ public class Application {
     public static boolean replayGame() {
 
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        Scanner scanner = new Scanner(System.in);
 
-        String replayNumberString = scanner.nextLine();
+        String replayNumberString = Console.readLine();
 
         if (!checkReplayNumber(replayNumberString)) {
 
