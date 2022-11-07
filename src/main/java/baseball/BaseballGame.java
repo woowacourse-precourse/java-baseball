ab92bf3a97731a1a018878dda624085a766b1fdb
Dongyeol <eastheat10@gmail.com>
2022-11-07 14:34:17 +0900

    feat: 시작 설정

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class BaseballGame {
+
+    public static void execute() {
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.start();
+    }
+
+    private void start() {
+
+    }
+
+}
