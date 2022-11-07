3dcfc879320a0fba5563a24bfbe34bc6d8d9fd2c
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-07 17:14:26 +0900

    feat(ApplicationTest): add restarting logic from parsing user input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         printGameStartMessage();
+        do {
             printGameRestartMessage();
+        } while (parseUserInputForRestart());
     }
 
     static void printGameStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
     static void printGameRestartMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    static boolean parseUserInputForRestart() {
+        String userInput = Console.readLine();
+        if (userInput.equals("1")) {
+            return true;
+        } else if (userInput.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
