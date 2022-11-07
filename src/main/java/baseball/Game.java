ade04fe96801ceb618daea032c514557f1cee1d9
donghae <donghae1999@gmail.com>
2022-11-07 15:22:43 +0900

    refactor: update loop index

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,24 +7,24 @@ public class Game {
     int ball;
     ArrayList<Integer> answerNumber;
 
-    public void setAnswerNumber(){
+    public void setAnswerNumber() {
         RandomNumber randomNumber = new RandomNumber();
         answerNumber = randomNumber.setRandomNumber();
     }
 
-    public void calculateStrike(ArrayList<Integer> userNumber){
-        strike=0;
-        for(int i=0; i<3; i++){
-            if(answerNumber.get(i) == userNumber.get(i)){
+    public void calculateStrike(ArrayList<Integer> userNumber) {
+        strike = 0;
+        for(int index = 0; index < 3; index++) {
+            if(answerNumber.get(index) == userNumber.get(index)) {
                 strike++;
             }
         }
     }
 
-    public void calculateBall(ArrayList<Integer> userNumber){
-        ball=0;
-        for(int i=0; i<3; i++){
-            if(answerNumber.contains(userNumber.get(i)) && answerNumber.get(i) != userNumber.get(i)){
+    public void calculateBall(ArrayList<Integer> userNumber) {
+        ball = 0;
+        for(int index = 0; index < 3; index++) {
+            if(answerNumber.contains(userNumber.get(index)) && answerNumber.get(index) != userNumber.get(index)) {
                 ball++;
             }
         }
