e435304cd7bd332d988f25b41a44b1ec70782248
jaehyeon <jhjh0022@naver.com>
2022-11-08 02:12:42 +0900

    style: 줄 간격 맞추기

--- src/main/java/baseball/Input/InputNumberVerification.java
+++ src/main/java/baseball/Input/InputNumberVerification.java
@@ -12,7 +12,7 @@ public class InputNumberVerification {
     }
 
     private void verifyNumberOverlap(int receivedNumber) {
-        if(!(makeNumberSplitList(receivedNumber).size() == makeNumberSplitSet(receivedNumber).size())){
+        if (!(makeNumberSplitList(receivedNumber).size() == makeNumberSplitSet(receivedNumber).size())) {
             throw new IllegalArgumentException("각자 다른 숫자를 입력해주세요");
         }
     }
@@ -27,13 +27,13 @@ public class InputNumberVerification {
     }
 
     private void verifyNumberThreeDigit(int receivedNumber) {
-        if (!(makeNumberSplitList(receivedNumber).size() == 3)){
+        if (!(makeNumberSplitList(receivedNumber).size() == 3)) {
             throw new IllegalArgumentException("3개의 숫자만 입력해주세요");
         }
     }
 
     public void verifyOneOrTwoButton(int receivedButtonNumber) {
-        if(!(receivedButtonNumber == 1 || receivedButtonNumber == 2)){
+        if (!(receivedButtonNumber == 1 || receivedButtonNumber == 2)) {
             throw new IllegalArgumentException("1, 2 둘 중 하나만 고르세요");
         }
     }
