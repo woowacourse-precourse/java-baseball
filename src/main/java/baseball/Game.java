215017cd14a5f378c3fa568682ece67f91a1f8e7
subin Park <psb0320@hs.ac.kr>
2022-11-08 02:33:33 +0900

    feat: 사용자가 게임을 종료할 때까지 게임을 계속 반복하는 기능
    - gameContinue가 true면 사용자 입력 및 게임 진행이 반복

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -5,16 +5,28 @@ import java.util.List;
 public class Game {
     private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
 
+    private boolean gameContinue = true;
+
     private List<Integer> computer;
+    private List<Integer> player;
 
     public void startGame() {
         System.out.println(START_SENTENCE);
 
         setComputer();
+
+        while (gameContinue) {
+            setPlayer();
+        }
     }
 
     private void setComputer() {
         Number number = new Number();
         this.computer = number.getComputerNumber();
+    }
+
+    private void setPlayer() {
+        Number number = new Number();
+        this.player = number.getPlayerNumber();
     }
 }
