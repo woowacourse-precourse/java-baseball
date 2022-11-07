5d5b1551018595bcf878694c7e69a73e95e61184
ssssudan <kankgm0324@gmail.com>
2022-11-07 23:59:29 +0900

    <fix>: 사용자의 입력을 받고 List에 저장할 때 List를 초기화 하지 않는 못하는 것을 수정

--- src/main/java/baseball/UserInputToList.java
+++ src/main/java/baseball/UserInputToList.java
@@ -11,6 +11,8 @@ public class UserInputToList {
     }
 
     public void userNumList(String answer) {
+        userNum.clear();
+
         for (int i = 0; i < answer.length(); i++) {
             int n = answer.charAt(i) - 48;
             userNum.add(i, n);
