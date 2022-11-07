0e5d6b568e94931474100fc1ed6a34ee4a6bc34f
TaeHwan <kimta2hwan@gmail.com>
2022-11-07 11:57:29 +0900

    feat(inputexception): 사용자가 입력한 3자리의 정수에 중복이 있는 경우 예외 처리 기능 구현

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -5,6 +5,7 @@ public class InputException {
     private static final String NOT_INTEGER_INPUT = "정수를 입력해 주세요.";
     private static final String NOT_THREE_LENGTH_INTEGER_INPUT = "3자리의 정수를 입력해 주세요.";
     private static final String INCLUDE_ZERO_INPUT = "0이 포함되지 않아야 합니다.";
+    private static final String DUPLICATE_NUMBER_INPUT = "중복되지 않는 수를 입력해 주세요.";
 
     public static void throwNotIntegerInputError() {
         throw new IllegalArgumentException(NOT_INTEGER_INPUT);
@@ -16,5 +17,9 @@ public class InputException {
 
     public static void throwIncludeZeroInputError() {
         throw new IllegalArgumentException(INCLUDE_ZERO_INPUT);
+    }
+
+    public static void throwDuplicateNumberInputError() {
+        throw new IllegalArgumentException(DUPLICATE_NUMBER_INPUT);
     }
 }
