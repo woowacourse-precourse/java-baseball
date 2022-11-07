e7c365a2f86c596113841037d18ead935cb36507
goldcollar95 <yjs7298@naver.com>
2022-11-08 00:42:40 +0900

    feat(Game): restartGame메서드구현(1혹은 2를 누를때 다시시작하게끔 만들어주는 부분구현)

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -56,7 +56,11 @@ public class Game {
         }
     }
 
-    public void succesGame(){
+    public void succesGame() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public void restartGame(){
+        System.out.println("게임을 새로 시작하면 1, 종료하려면 2를 입력하세요");
     }
 }
