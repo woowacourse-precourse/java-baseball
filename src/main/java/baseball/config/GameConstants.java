72583e5288a108eebdb2ade4fdf056e1e435a901
Be-student <presentsong@naver.com>
2022-11-07 23:28:08 +0900

    refactor: 메시지를 상수로부터 만듦

--- src/main/java/baseball/config/GameConstants.java
+++ src/main/java/baseball/config/GameConstants.java
@@ -8,7 +8,8 @@ public final class GameConstants {
     public static final int MIN_INPUT_INCLUSIVE = 100;
     public static final int MAX_INPUT_INCLUSIVE = 999;
     public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-    public static final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다 !게임 종료\n";
+    public static final String GAME_END_MESSAGE =
+            MIN_INPUT_INCLUSIVE + "개의 숫자를 모두 맞히셨습니다 !게임 종료\n";
 
     public static final String ONE_BALL = "1볼 ";
     public static final String TWO_BALL = "2볼 ";
@@ -20,7 +21,8 @@ public final class GameConstants {
 
     public static final String NOTHING = "낫싱";
     public static final String DIGITS_OUT_OF_INDEX = "Digits의 최대 길이보다 큰 index를 참조했습니다";
-    public static final String INPUT_OUT_OF_RANGE = "100부터 999사이의 입력이 아닙니다";
+    public static final String INPUT_OUT_OF_RANGE =
+            MIN_INPUT_INCLUSIVE + "부터 " + MAX_INPUT_INCLUSIVE + "사이의 입력이 아닙니다";
     public static final String INPUT_DUPLICATE = "입력한 값중에 중복된 값이 있습니다";
     public static final String INPUT_ERROR = "사용자의 입력에 문제가 있습니다";
 
