d0c255d385150258e2f88b724eebbb3fdfbae5ed
Jeangyu <gjwlsrb1700@naver.com>
2022-11-07 17:44:19 +0900

    [ADD] Constants 추가

--- src/main/java/baseball/utils/BaseballConstants.java
+++ src/main/java/baseball/utils/BaseballConstants.java
@@ -1,2 +1,8 @@
-package baseball.utils;public class BaseballConstants {
+package baseball.utils;
+
+public class BaseballConstants {
+
+    public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+    public static final String PLAY_AGAIN = "1";
+    public static final int BALL_NUMBER = 3;
 }
