8c539c987f2f7082986d0d3b5e1135df5f83e974
parkjungyoon <tbr06057@naver.com>
2022-11-08 02:00:28 +0900

    feat: RestartNumber에 대한 예외처리 (1과 2만 입력해야된다)

--- src/main/java/baseball/validator/NumberValidator.java
+++ src/main/java/baseball/validator/NumberValidator.java
@@ -4,10 +4,21 @@ public class NumberValidator {
     private final static int MIN_RANGE_NUM = 1;
     private final static int MAX_RANGE_NUM = 9;
     private final static String RANGE_ERROR_MESSAGE = "1부터 9 사이의 자연수를 입력해주세요.";
+    private final static String RESTART_ERROR_MESSAGE = "1또는 2를 입력해주세요.";
 
     public static void validateRange(int input) {
         if (input < MIN_RANGE_NUM || input > MAX_RANGE_NUM) {
             throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
         }
+    }
+
+    public static void validateRestartNumber(int input) {
+        if (input == 1) {
+            return;
+        }
+        if (input == 2) {
+            return;
+        }
+        throw new IllegalArgumentException(RESTART_ERROR_MESSAGE);
     }
 }
