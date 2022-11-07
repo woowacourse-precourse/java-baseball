8a5d3bb54638838a13aa5035d5c6afbf26833020
gilyeon00 <12venus08@naver.com>
2022-11-08 05:10:07 +0900

    feat: setting Controller frame

---
+++ src/main/java/baseball/Controller.java
@@ -0,0 +1,22 @@
+package baseball;
+
+public class Controller {
+
+    public void run() {
+        setGame();
+        startGame();
+        endGame();
+    }
+
+    public void setGame() {
+
+    }
+
+    public void startGame() {
+
+    }
+
+    public void endGame() {
+
+    }
+}
