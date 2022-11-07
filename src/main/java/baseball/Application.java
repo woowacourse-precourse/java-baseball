eb6efb600bb4358771a447e2c6d748782f1ffd48
wnsgml7267 <dlwns7267@naver.com>
2022-11-08 04:18:06 +0900

    refactor(Application): 출력문 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import baseball.controller.BaseballSystem;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         BaseballSystem system = new BaseballSystem();
         system.runGame();
     }
