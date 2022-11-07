3b3de7e129862682bc2ae98837239384223c84d0
inhyeok <ss112d86@gmail.com>
2022-11-08 02:47:08 +0900

    feat: 신호입력 기능 구현

--- src/main/java/baseball/console/ConsoleInput.java
+++ src/main/java/baseball/console/ConsoleInput.java
@@ -1,6 +1,7 @@
 package baseball.console;
 
 import baseball.dto.GameInputNumber;
+import baseball.dto.InputSignal;
 import camp.nextstep.edu.missionutils.Console;
 
 public class ConsoleInput {
@@ -11,5 +12,13 @@ public class ConsoleInput {
 
     String inputString = Console.readLine();
     return new GameInputNumber(inputString);
+  }
+
+  public InputSignal inputSignal(){
+    final String INPUT_SIGNAL_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    System.out.println(INPUT_SIGNAL_MESSAGE);
+
+    String inputStr = Console.readLine();
+    return new InputSignal(inputStr);
   }
 }
--- src/test/java/baseball/console/ConsoleInputTest.java
+++ src/test/java/baseball/console/ConsoleInputTest.java
@@ -1,12 +1,14 @@
 package baseball.console;
 
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import baseball.dto.GameInputNumber;
+import baseball.dto.InputSignal;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -33,7 +35,7 @@ class ConsoleInputTest {
 
       consoleInput.inputNumber();
 
-      Assertions.assertThat(outputStreamCaptor.toString()).contains("숫자를 입력해주세요");
+      assertThat(outputStreamCaptor.toString()).contains("숫자를 입력해주세요");
     }
 
     @Test
@@ -44,10 +46,50 @@ class ConsoleInputTest {
 
       GameInputNumber result = consoleInput.inputNumber();
 
-      Assertions.assertThat(result.getNumbers().size()).isEqualTo(3);
-      Assertions.assertThat(result.getNumbers()).contains(1, 2, 3);
+      assertThat(result.getNumbers().size()).isEqualTo(3);
+      assertThat(result.getNumbers()).contains(1, 2, 3);
     }
 
+    @AfterEach
+    public void tearDown() {
+      System.setOut(standardOut);
+    }
+  }
+
+  @Nested
+  class InputSignalTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+    private final ConsoleInput consoleInput = new ConsoleInput();
+
+    @BeforeEach
+    public void setUp() {
+      System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    void 신호입력전_안내메시지가_출력되어야한다() {
+      String givenSignal = "1";
+      InputStream inputStream = new ByteArrayInputStream(givenSignal.getBytes());
+      System.setIn(inputStream);
+      String INPUT_SIGNAL_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+      consoleInput.inputSignal();
+
+      assertThat(outputStreamCaptor.toString()).contains(INPUT_SIGNAL_MESSAGE);
+    }
+
+    @Test
+    void 신호입력성공시_InputSignal클래스를_리턴한다(){
+      String givenSignal = "1";
+      InputStream inputStream = new ByteArrayInputStream(givenSignal.getBytes());
+      System.setIn(inputStream);
+
+      InputSignal inputSignal = consoleInput.inputSignal();
+
+      assertThat(inputSignal.getSignal()).isEqualTo(Integer.parseInt(givenSignal));
+    }
     @AfterEach
     public void tearDown() {
       System.setOut(standardOut);
