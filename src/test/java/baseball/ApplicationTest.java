723351416de82a0fc20c2881179ad0098c2da654
Ukj0ng <xb253q@gmail.com>
2022-11-08 02:15:30 +0900

    feat: 스트라이크 개수 test, 볼 개수 test, 입력값 유효성 test

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,14 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -32,4 +34,100 @@ class ApplicationTest extends NsTest {
     public void runMain() {
         Application.main(new String[]{});
     }
+    @Nested
+    class GameTest{
+        Game game = new Game();
+        @Test
+        void 스트라이크_갯수_체크1(){
+            final List<Integer> targetNumber = List.of(4, 8, 6);
+            final List<Integer> playerNumber = List.of(4, 8, 7);
+            final BaseBall baseBall = new BaseBall();
+            final int strike = 2;
+
+            final int result = game.countStrike(targetNumber, playerNumber, baseBall);
+
+            assertThat(strike).isEqualTo(result);
+        }
+        @Test
+        void 스트라이크_갯수_체크2(){
+            final List<Integer> targetNumber = List.of(5, 8, 6);
+            final List<Integer> playerNumber = List.of(4, 8, 7);
+            final BaseBall baseBall = new BaseBall();
+            final int strike = 1;
+
+            final int result = game.countStrike(targetNumber, playerNumber, baseBall);
+
+            assertThat(strike).isEqualTo(result);
+        }
+
+        @Test
+        void 스트라이크_갯수_체크3(){
+            final List<Integer> targetNumber = List.of(4, 8, 7);
+            final List<Integer> playerNumber = List.of(4, 8, 7);
+            final BaseBall baseBall = new BaseBall();
+            final int strike = 3;
+
+            final int result = game.countStrike(targetNumber, playerNumber, baseBall);
+
+            assertThat(strike).isEqualTo(result);
+        }
+
+        @Test
+        void 볼_갯수_체크1(){
+            final List<Integer> targetNumber = List.of(4, 8, 6);
+            final List<Integer> playerNumber = List.of(8, 6, 7);
+            final BaseBall baseBall = new BaseBall();
+            final int ball = 2;
+
+            final int result = game.countBall(targetNumber, playerNumber, baseBall);
+
+            assertThat(ball).isEqualTo(result);
+        }
+
+        @Test
+        void 볼_갯수_체크2(){
+            final List<Integer> targetNumber = List.of(3, 6, 7);
+            final List<Integer> playerNumber = List.of(2, 9, 3);
+            final BaseBall baseBall = new BaseBall();
+            final int ball = 1;
+
+            final int result = game.countBall(targetNumber, playerNumber, baseBall);
+
+            assertThat(ball).isEqualTo(result);
+        }
+
+        @Test
+        void 볼_갯수_체크3(){
+            final List<Integer> targetNumber = List.of(1, 2, 8);
+            final List<Integer> playerNumber = List.of(2, 8, 1);
+            final BaseBall baseBall = new BaseBall();
+            final int ball = 3;
+
+            final int result = game.countBall(targetNumber, playerNumber, baseBall);
+
+            assertThat(ball).isEqualTo(result);
+        }
+
+        @Test
+        void 숫자_유효성_검사1(){
+            final String num = "1234";
+
+            Throwable thrown = catchThrowable(() -> {throw new IllegalArgumentException("유효하지 않은 값");});
+
+            assertThat(thrown).isInstanceOf(Exception.class)
+                    .hasMessageContaining("유효하지 않은 값");
+        }
+
+        @Test
+        void 숫자_유효성_검사2(){
+            final String num = "치이카와";
+
+            Throwable thrown = catchThrowable(() -> {throw new IllegalArgumentException("유효하지 않은 값");});
+
+            assertThat(thrown).isInstanceOf(Exception.class)
+                    .hasMessageContaining("유효하지 않은 값");
+        }
+
+    }
+
 }
