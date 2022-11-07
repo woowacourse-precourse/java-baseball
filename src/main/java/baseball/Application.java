893ac2a24dae713a047f4a8b0b27b0e030723edf
JasmineAir <gofoxfox@naver.com>
2022-11-08 00:03:38 +0900

    2.맞출 때까지 반복 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,5 @@
 package baseball;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -9,7 +8,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 
 public class Application {
-    public static List<Integer> InputNumber () throws IOException {
+    public static List<Integer> InputNumber () {
         List<Integer> user = new ArrayList<>();
         String[] userNumber;
 
@@ -22,9 +21,48 @@ public class Application {
 
         return user;
     }
-    public static void main(String[] args) throws IOException {
+
+    public static boolean Compare(List<Integer> computer, List<Integer> Input) {
+        int index = 0;
+        int strike = 0;
+        int ball = 0;
+        for(int i=0; i<computer.size(); i++){
+            ball += Counting(i, computer, Input).get(0);
+            strike += Counting(i, computer, Input).get(1);
+        }
+
+        if (strike+ball==0) System.out.println("낫싱");
+        else System.out.println(ball+"볼 "+strike+"스트라이크");
+
+        if (strike<3)        return true;
+        else return false;
+    }
+    public static List<Integer> Counting(int index, List<Integer> computer, List<Integer> Input){
+        List<Integer> count = new ArrayList<>();
+        int ball = 0;
+        int strike = 0;
+        for(int i=0; i<3; i++){
+            if(i==index){
+                if(computer.get(i) == Input.get(index)) strike++;
+            }
+            else {
+                if (computer.get(i) == Input.get(index)) ball++;
+            }
+        }
+        count.add(ball);
+        count.add(strike);
+        return count;
+    }
+
+    public static List<Integer> Initialize(List<Integer> Input){
+        Input = new ArrayList<>();
+        return Input;
+    }
+
+    public static void main(String[] args) {
         List<Integer> computer = new ArrayList<>();
         List<Integer> Input = new ArrayList<>();
+
 
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -32,9 +70,9 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
+
         System.out.println("숫자 야구게임을 시작합니다.");
-        Input = InputNumber();
-        System.out.println(Input);
+        while(Compare(computer, InputNumber()));
 
     }
 }
