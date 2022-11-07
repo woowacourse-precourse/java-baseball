3473d8ec326a18b6cf1b12ff98f13211bfa1bb50
jjmin9797 <jjmin9797@naver.com>
2022-11-07 18:13:08 +0900

    feat(GameService): checkInputPlayerNumber 기능 구현 플레이어가 입력한 숫자가 3글자인지 확인한다.
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,2 +1,11 @@
-package baseball.service;public class GameService {
+package baseball.service;
+
+public class GameService {
+
+
+    public void checkInputPlayerNumber(String inputPlayerNumbers){
+        if (inputPlayerNumbers.length() != 3){
+            throw new IllegalArgumentException();
+        }
+    }
 }
