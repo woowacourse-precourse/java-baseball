0269f3b16355114a8d36b2493c50a0997a6aaf29
Kijung Park <kjpark4321@naver.com>
2022-11-08 01:37:50 +0900

    feat : 숫자 하나와 정답수 일치하는지 비교 메서드 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,22 +13,28 @@ public class Application {
 
     private static int checkOneAnswerNumberWithTargetNumber(int answerOneNumber, int targetNumber, int answerIdx, int targetIdx) {
         int result = 0;
+        System.out.println(answerOneNumber + " " + targetNumber);
 
         if (answerOneNumber == targetNumber) {
             result += 1;
         }
 
-        if (answerIdx == targetIdx) {
+        if (answerOneNumber == targetNumber && answerIdx == targetIdx) {
             result += 1;
         }
 
         return result;
     }
 
-    private static int checkEachNumber(int targetNumber, int idx) {
+    private static int checkEachNumber(int targetNumber, int targetIdx) {
+        int result = 0;
+        int processAnswerNumber = answerNumber;
         for (int i = 1; i <= 3; i++) {
-
+            int answerOneNumber = processAnswerNumber/(Math.max(1, (int)Math.pow(10,3-i)));
+            result += checkOneAnswerNumberWithTargetNumber(answerOneNumber, targetNumber, i, targetIdx);
+            processAnswerNumber -= answerOneNumber * (int)Math.pow(10,3-i);
         }
+        return result;
     }
 
     private static List<Integer> gameResult(int inputNumber) {
@@ -48,6 +54,7 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
         answerNumber = (int) ((Math.random() * (MAX - MIN)) + MIN);
         System.out.println(answerNumber);
+        System.out.println(checkEachNumber(4, 1));
     }
 
     public static void main(String[] args) {
