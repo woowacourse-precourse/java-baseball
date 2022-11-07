ad0f5ddf52ecded55957f3500eddabe2aa081835
josolha <josolha@nate.com>
2022-11-08 01:35:31 +0900

    feat : add main method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,25 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
+        boolean gameflag = true;
 
+        while (gameflag) {
+
+            System.out.println("숫자 야구 게임을 시작합니다");
+
+            List<Integer> computerNumber = generateNum();
+
+            String checkNumbers = "";
+
+            while (!checkNumbers.equals("3스트라이크")) {
+                System.out.print("숫자를 입력해주세요 : ");
+                List<Integer> userNumber = inputNum(inputSystem());
+                checkNumbers = checkNumbers(computerNumber, userNumber);
+                System.out.println(checkNumbers);
+            }
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+            gameflag = finish(inputSystem());
+        }
     }
 
     static List<Integer> generateNum() {
