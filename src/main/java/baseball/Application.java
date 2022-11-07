f8058832beaad254875dde54655a0aca736f55b4
yeop0740 <ssucgoo@gmail.com>
2022-11-08 00:09:16 +0900

    feat(Application): validateUserNumbers 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -22,4 +22,14 @@ public class Application {
 
         return result;
     }
+
+    public static void validateUserNumbers(String input) throws IllegalArgumentException{
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("세자리가 아닌 수를 입력하였습니다.");
+        }else if (input.charAt(0) == input.charAt(1) || input.charAt(1) == input.charAt(2) || input.charAt(2) == input.charAt(0)) {
+            throw new IllegalArgumentException("중복된 수를 입력하였습니다.");
+        }else if (!Character.isDigit(input.charAt(0)) || !Character.isDigit(input.charAt(1)) || !Character.isDigit(input.charAt(2))) {
+            throw new IllegalArgumentException("숫자가 아닌 값을 입력하였습니다.");
+        }
+    }
 }
