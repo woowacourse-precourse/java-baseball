40fb1ac8756d4de34b83a445f4a290f12082f3ba
twoosky <lhjae3@naver.com>
2022-11-08 01:28:50 +0900

    feat(baseball): Baseball 클래스에서 스트라이크, 볼 개수 반환 메소드 호출

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -13,5 +13,21 @@ public class Baseball {
         this.computer = RandomNumberGenerator.generate();
     }
 
-    public void run() {}
+    public void run() {
+        Output.startGame();
+        List<Integer> player = Input.playerNumber();
+        int ballCount = getBallCount(player);
+        int strikeCount = getStrikeCount(player);
+        Output.getResult(ballCount, strikeCount);
+    }
+
+    private int getBallCount(List<Integer> player) {
+        Ball ball = new Ball(player, computer);
+        return ball.getCount();
+    }
+
+    private int getStrikeCount(List<Integer> player) {
+        Strike strike = new Strike(player, computer);
+        return strike.getCount();
+    }
 }
