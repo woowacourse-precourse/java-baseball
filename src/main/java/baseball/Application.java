3f50d8e7731e1af7d69e580b805c29b0c976c59a
KangSungBum <peter9706@naver.com>
2022-11-07 22:25:10 +0900

    feat(Application.java) : finish main code by using methods
    Added new code for main method
    - new codes follows the flow of the baseball game
    - runs until user enters the exit code

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,17 +8,27 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다");
         while (true) {
-            System.out.println("숫자 야구 게임을 시작합니다");
-            System.out.print("숫자를 입력해주세요 : ");
-            int inputNumber = Integer.parseInt(Console.readLine());
-
-
-            try {
+            List<Integer> randomNumber = createRandomNumber();
+            while(true){
+                System.out.print("숫자를 입력해주세요 : ");
+                int inputNumber = Integer.parseInt(Console.readLine());
                 checkInputCondition(inputNumber);
-            } catch (IllegalArgumentException e) {
-                System.out.println("잘못된 입력입니다");
+
+                List<Integer> listedInputNumber = convertIntegerIntoList(inputNumber);
+//                System.out.println("randomNumber = " + randomNumber);
+
+                int[] countResult = countBallsAndStrikes(randomNumber, listedInputNumber);
+                String resultMessage = printResult(countResult);
+                System.out.println(resultMessage);
+                if (countResult[1] == 3) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
+                }
             }
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
             int exitCode = Integer.parseInt(Console.readLine());
             if (exitCode == 2) {
@@ -102,8 +112,8 @@ public class Application {
             outputStringBuilder.append(strikeCount);
             outputStringBuilder.append("스트라이크");
         } else if (ballCount > 0 && strikeCount == 0) {
-            outputStringBuilder.append("볼 ");
-            outputStringBuilder.append(strikeCount);
+            outputStringBuilder.append(ballCount);
+            outputStringBuilder.append("볼");
         }
 
         return outputStringBuilder.toString();
