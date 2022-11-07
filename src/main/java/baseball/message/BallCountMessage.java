87180f427feb3db64087346774511bc305f1e05d
이원기 <log19951@gmail.com>
2022-11-08 00:49:02 +0900

    feat: 프로그램 내에서 사용될 문자열 메세지 클래스 생성
    - BallCountMessage : 스트라이크, 볼, 낫싱 문자열 선언 클래스
    - ErrorMessage : 사용자 입력 값 오류에 대한 예외처리에 필요한 에러메세지 문자열 선언 클래스
    - OutPutMessage : 게임 진행에 필요한 멘트를 문자열로 선언한 클래스

---
+++ src/main/java/baseball/message/BallCountMessage.java
@@ -0,0 +1,8 @@
+package baseball.message;
+
+public class BallCountMessage {
+
+    public final static String STRIKE = "스트라이크";
+    public final static String BALL = "볼";
+    public final static String NOTHING = "낫싱";
+}
---
+++ src/main/java/baseball/message/ErrorMessage.java
@@ -0,0 +1,9 @@
+package baseball.message;
+
+public class ErrorMessage {
+
+    public final static String WRONG_NUMBER_OF_DIGITS = "입력하신 숫자가 3자리가 아닙니다.";
+    public final static String INPUT_NOT_NUMBER = "숫자만 입력가능합니다.";
+    public final static String INPUT_SAME_NUMBER = "입력하신 숫자에 동일한 숫자가 존재합니다.";
+    public final static String INPUT_WRONG_REGAME_NUMBER = "잘못된 숫자를 입력하셨습니다. 1 혹은 2만 입력가능합니다.";
+}
