62fdfddda1e3c149c26ee828e19127013ae63d05
Anthologia <pyoungjy@gmail.com>
2022-11-08 01:53:06 +0900

    feat: 문자열 야구숫자를 List<Integer> 형태로 변환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,12 +12,21 @@ public class Application {
         printGameStartMessage();
 
         System.out.print("숫자를 입력해주세요 : ");
-        String userNumber = Console.readLine();
-
+        List<Integer> userNumber = splitStrNumberToList(Console.readLine());
         List<Integer> computerNumber = Randoms.pickUniqueNumbersInRange(1, 9, 3);
     }
 
     private static void printGameStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static List<Integer> splitStrNumberToList(String strNumber) {
+        List<Integer> userNumberList = new ArrayList<>();
+
+        userNumberList.add(Character.getNumericValue(strNumber.charAt(0)));
+        userNumberList.add(Character.getNumericValue(strNumber.charAt(1)));
+        userNumberList.add(Character.getNumericValue(strNumber.charAt(2)));
+
+        return userNumberList;
     }
 }
