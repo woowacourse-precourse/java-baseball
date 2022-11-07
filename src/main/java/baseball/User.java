e5f8b639c39adbc440dae74f3b21c9546740b468
ji-mango <susie000301@naver.com>
2022-11-08 01:03:37 +0900

    feat: 입력받는 수 에러처리(3자리 수인지/서로 다른 수인지/숫자로만 이루어져있는지)

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class User {
@@ -12,9 +15,49 @@ public class User {
 
         System.out.print("숫자를 입력해주세요 : ");
         int userNumber = input();
+        checkThreeDigit(userNumber);
     }
 
     public int input() {
-        return Integer.parseInt(readLine());
+        String inputString =readLine();
+        checkInput(inputString);
+
+        return Integer.parseInt(inputString);
+    }
+
+    private void checkInput(String inputString) {
+        char temp;
+
+        for (int i = 0; i < inputString.length(); i++) {
+            temp = inputString.charAt(i);
+            checkDigit(temp);
+        };
+    }
+
+    private void checkDigit(char word) {
+        if (!Character.isDigit(word)) {
+            throw new IllegalArgumentException("숫자만 입력하시오.");
+        }
+    }
+
+    private void checkThreeDigit(int gameNumber) {
+        List<Integer> gameNumberList = new ArrayList<>();
+        while (gameNumber > 0) {
+            int n = gameNumber % 10;
+            checkDuplication(n, gameNumberList);
+
+            gameNumberList.add(gameNumber % 10);
+            gameNumber = gameNumber / 10;
+        }
+
+        if (gameNumberList.size() != 3) {
+            throw new IllegalArgumentException("세자리로 입력하시오.");
+        }
+    }
+
+    private void checkDuplication(int number, List<Integer> gameNumberList) {
+        if (gameNumberList.contains(number)) {
+            throw new IllegalArgumentException("숫자를 중복없이 입력하시오.");
+        }
     }
 }
