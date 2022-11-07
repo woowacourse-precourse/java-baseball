98df4ad78690831af6c648337bd45b7689a1d82d
BaeDongHyeon <sasd730@gmail.com>
2022-11-07 22:31:25 +0900

    refactor: 게임 시작 문구 위치 변경
    - 생성자에서 start 메소드로 이동

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -8,9 +8,10 @@ import java.util.stream.Collectors;
 
 public class Game {
     private Game() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+
     }
     public static void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         Game game = Game.create();
     }
     public static Game create() {
