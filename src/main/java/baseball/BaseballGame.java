6d413608e88e673bb666ba492a9d2218533e6e42
Luka <meena2003@naver.com>
2022-11-07 23:38:18 +0900

    <feat> (기능2) 사용자 3자리 숫자 입력 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,12 +1,14 @@
 package baseball;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+
 import camp.nextstep.edu.missionutils.Randoms;
+
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseballGame {
-    List<Integer> computerNumbers =
+    List<Integer> computerNumbersList = makesComputerNumbers();
+    List<Integer> playerNumbersList =
 
     public void playBaseballGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -17,18 +19,49 @@ public class BaseballGame {
         }
     }
 
-    private List<Integer> makesComputerNumbers() {
-        List<Integer> computerNumbers = new ArrayList<>();
-        while (computerNumbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computerNumbers.contains(randomNumber)) {
-                computerNumbers.add(randomNumber);
-            }
+    private List<Integer> inputPlayerNumbers() {
+        String playerNumbers = readLine();
+        List<Integer> playerNumbersList = new ArrayList<>();
+        try {
+            StringToList(readLine());
+        } catch (UnvailedNumberException e) {
         }
-        return computerNumbers;
+
     }
 
+    private List<String> StringToList(String playerNumbers) throws UnvailedNumberException {
+        List<String> playerNumbersList = new ArrayList<>(Arrays.asList(playerNumbers.split("")));
+        if (checkScopeOfNumbers(playerNumbers) || checkSameNumbers(playerNumbersList)) {
+            throw new UnvailedNumberException();
+        }
+        return playerNumbersList;
+    }
 
+    private boolean checkScopeOfNumbers(String playerNumbers) {
+        Integer number = Integer.parseInt(playerNumbers);
+        if (number < 123 || 987 < number) {
+            return true;
+        }
+        return false;
+    }
 
+    private boolean checkSameNumbers(List<String> playerNumbersList) {
+        Set<String> playerNumbersSet = new HashSet<>(playerNumbersList);
+        if (playerNumbersSet.size() != 3) {
+            return true;
+        }
+        return false;
+    }
+
+    private List<Integer> makesComputerNumbers() {
+        List<Integer> computerNumbersList = new ArrayList<>();
+        while (computerNumbersList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbersList.contains(randomNumber)) {
+                computerNumbersList.add(randomNumber);
+            }
+        }
+        return computerNumbersList;
+    }
 }
 
