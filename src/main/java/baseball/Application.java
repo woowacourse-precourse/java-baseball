e4e31346ac8ff03ecd7d0316c5d57fe1c3d5031d
HamSeungChan <head0618@naver.com>
2022-11-07 21:22:14 +0900

    feat : 출력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -130,5 +130,34 @@ public class Application {
     HashMap<String,Integer> resultStrikeAndBall = findResultStrikeAndBall(myNumberGroup
             ,computerNumberGroup,sameNumberList);
   }
+
+  private static void printGameStart(){
+    System.out.println("숫자 야구 게임을 시작합니다.");
+  }
+
+  private static void printInputMyNumber(){
+    System.out.print("숫자를 입력해주세요 : ");
+  }
+
+  private static void printBall(Integer ball){
+    System.out.println(ball+"볼");
+  }
+
+  private static void printStrike(Integer strike){
+    System.out.println(strike+"스트라이크");
+  }
+
+  private static void printNothing(){
+    System.out.println("낫싱");
+  }
+
+  private static void printStrikeAndBall(Integer ball, Integer strike){
+    System.out.println(ball+"볼"+" "+strike+"스트라이크");
+  }
+
+  private static void endGame(){
+    System.out.println("\"3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+  }
 }
 
