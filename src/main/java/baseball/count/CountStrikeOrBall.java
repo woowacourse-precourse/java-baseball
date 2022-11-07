9b8ac3e7b0b41886963bfc48640eca3aea6446fe
jaehyeon <jhjh0022@naver.com>
2022-11-07 21:35:34 +0900

    refactor: 줄간격 맞추기, 변수 이름 명확하게 수정

--- src/main/java/baseball/count/CountStrikeOrBall.java
+++ src/main/java/baseball/count/CountStrikeOrBall.java
@@ -1,31 +1,31 @@
-package baseball;
+package baseball.count;
 
 import java.util.List;
 
-import static baseball.tool.BeforeVerificationSetting.makeNumberSplitList;
+import static baseball.tool.NumberSplitTool.makeNumberSplitList;
 import static baseball.tool.DuplicateByComparison.findOverlapNumber;
 
 public class CountStrikeOrBall {
 
-    public static int getStrikeNumber(List<Integer> answerNumber, int receivedNumber){
-        List<Integer> overlapNumber = findOverlapNumber(answerNumber, receivedNumber);
+    public static int getStrikeNumber(List<Integer> answerNumber, int receivedNumber) {
+        List<Integer> overlapNumberList = findOverlapNumber(answerNumber, receivedNumber);
         int strikeNumberCnt = 0;
 
-        for(int overlap : overlapNumber){
+        for (int overlapNumber : overlapNumberList) {
 
-            if(answerNumber.indexOf(overlap) == makeNumberSplitList(receivedNumber).indexOf(overlap)){
+            if (answerNumber.indexOf(overlapNumber) == makeNumberSplitList(receivedNumber).indexOf(overlapNumber)) {
                 strikeNumberCnt += 1;
             }
         }
         return strikeNumberCnt;
     }
 
-    public static int getBallNumber(List<Integer> answerNumber, int receivedNumber){
-        List<Integer> overlapNumber = findOverlapNumber(answerNumber, receivedNumber);
+    public static int getBallNumber(List<Integer> answerNumber, int receivedNumber) {
+        List<Integer> overlapNumberList = findOverlapNumber(answerNumber, receivedNumber);
         int ballNumberCnt = 0;
 
-        for(int overlap : overlapNumber){
-            if(answerNumber.indexOf(overlap) != makeNumberSplitList(receivedNumber).indexOf(overlap)){
+        for (int overlapNumber : overlapNumberList) {
+            if (answerNumber.indexOf(overlapNumber) != makeNumberSplitList(receivedNumber).indexOf(overlapNumber)) {
                 ballNumberCnt += 1;
             }
         }
