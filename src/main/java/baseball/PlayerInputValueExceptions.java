2466ffa94cd6e2383fc9ec9d194db60cd451fefa
nahyeon cho <whskgus12@nate.com>
2022-11-07 15:58:51 +0900

    feat : 세 자리 수에 중복이 있으면 예외 발생
    플레이어한테 입력 받은 세 자리 수가 서로 다른 세 개의 수가 아니라면, IllegalArgumentException 예외를 발생시킵니다.

--- src/main/java/baseball/PlayerInputValueExceptions.java
+++ src/main/java/baseball/PlayerInputValueExceptions.java
@@ -4,9 +4,16 @@ import java.util.ArrayList;
 
 public class PlayerInputValueExceptions {
 
-    static void isThreeDigits(ArrayList<String> userSelectedNumbers) {
+    static void isThreeDigitsNumber(ArrayList<String> userSelectedNumbers) {
         if (userSelectedNumbers.size() != 3) {
             throw new IllegalArgumentException("3자리만 입력하세요");
         }
     }
+
+    static void haveDuplicateNumbers(ArrayList<String> userSelectedNumbers) {
+        if (userSelectedNumbers.size() != userSelectedNumbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("서로 다른 3자리의 수를 입력하세요");
+        }
+    }
 }
+
