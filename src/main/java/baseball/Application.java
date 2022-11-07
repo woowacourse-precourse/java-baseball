00767131c51129147441aeed8df43078898c2395
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-08 00:15:10 +0900

    feat(countScore, printScore): 입력값에 따라 결과값 출력\n\n일치하는 숫자가 있을 경우 볼, 스트라이크 개수로 표시하고 없을 경우 낫싱으로 표시

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,10 +2,8 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Application {
     public static List<Integer> pickComputerNumber () {
@@ -39,14 +37,67 @@ public class Application {
         return splitArray.length == numberSet.size();
     }
 
+    public static Map<String, Integer> countScore(List<Integer> userInput, List<Integer> computerInput) {
+        int searchIndex, score;
+        Map<String, Integer> scoreMap = new HashMap<>() {{
+            put("스트라이크", 0);
+            put("볼", 0);
+        }};
+        for (int index=0; index < computerInput.size(); index++) {
+            searchIndex = userInput.indexOf(computerInput.get(index));
+            if (searchIndex == index) {
+                score = scoreMap.get("스트라이크");
+                scoreMap.put("스트라이크", score+1);
+            }
+            if ((searchIndex != index) && (searchIndex != -1)) {
+                score = scoreMap.get("볼");
+                scoreMap.put("볼", score+1);
+            }
+        }
+        return scoreMap;
+    }
+
+    public static boolean printScore(Map<String, Integer> scoreMap) {
+        int strike, ball;
+        boolean endPoint = false;
+        strike = scoreMap.get("스트라이크");
+        ball = scoreMap.get("볼");
+
+        if ((strike > 0) && (ball > 0)) {
+            System.out.printf("%d볼%d스트라이크%n",ball,strike);
+        } else if ((strike > 0) && (ball == 0)) {
+            System.out.printf("%d스트라이크%n", strike);
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+            }
+        } else if ((strike == 0) && (ball > 0)) {
+            System.out.printf("%d볼%n", ball);
+        } else {
+            System.out.println("낫싱");
+        }
+        return endPoint;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String guessNumber;
+        List<Integer> splitNumberArray = new ArrayList<>();
         List<Integer> computerNumber = pickComputerNumber();
+        Map<String, Integer> scoreMap;
+
+        System.out.println(computerNumber);
+
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
         guessNumber = Console.readLine();
-        isInputFormat(guessNumber);
-
+        if (isInputFormat(guessNumber)) {
+            splitNumberArray = Arrays.stream(guessNumber.split(""))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        }
+        scoreMap = countScore(splitNumberArray, computerNumber);
+        printScore(scoreMap);
     }
 }
