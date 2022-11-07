13fa7ad5d636de3148a716a6e9911340bcb748c7
donghae <donghae1999@gmail.com>
2022-11-07 15:22:25 +0900

    refactor: update Indent

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -2,19 +2,22 @@ package baseball;
 
 public class Controller {
     public static GameService gameService = new GameService();
-    public void run() throws IllegalArgumentException{
-            setGame();
-            startGame();
-            finishGame();
+
+    public void run() throws IllegalArgumentException {
+        setGame();
+        startGame();
+        finishGame();
     }
 
-    public void setGame(){
+    public void setGame() {
         gameService.setGame();
     }
-    public void startGame() throws IllegalArgumentException{
+
+    public void startGame() throws IllegalArgumentException {
         gameService.startGame();
     }
-    public void finishGame(){
+
+    public void finishGame() {
         gameService.finishGame();
     }
 }
--- src/main/java/baseball/View/Message.java
+++ src/main/java/baseball/View/Message.java
@@ -1,31 +1,30 @@
 package baseball.View;
 
 public class Message {
-    public static void printResultMessage(int strike, int ball){
-        if(ball !=0 && strike !=0){
-            System.out.println(ball+"볼 "+strike+"스트라이크 ");
+    public static void printResultMessage(int strike, int ball) {
+        if(ball != 0 && strike != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크 ");
         }
-        if(ball!=0 && strike==0){
-            System.out.println(ball+"볼");
+        if(ball != 0 && strike == 0) {
+            System.out.println(ball + "볼");
         }
-        if(ball==0 && strike!=0) {
+        if(ball == 0 && strike != 0) {
             System.out.println(strike + "스트라이크");
         }
-        if(ball ==0 && strike==0){
+        if(ball == 0 && strike == 0) {
             System.out.println("낫싱");
         }
     }
 
-    public static void printEndMessage(){
+    public static void printEndMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public static void printRestartMessage(){
+    public static void printRestartMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    public static void printRequestUserNumber(){
+    public static void printRequestUserNumber() {
         System.out.print("숫자를 입력해 주세요: ");
     }
-
 }
