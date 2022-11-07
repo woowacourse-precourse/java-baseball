3f4d8d0818da3ea64799166fa537f85608b6658d
고동우 <shp05179@gmail.com>
2022-11-07 15:39:35 +0900

    feature: 랜덤 숫자 생성
    1~9 사이의 숫자를 랜덤으로 생성하여 순서에따라 다음과 같은 형태로 Hashmap에 저장해 리턴
    {숫자: 순서}

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class Computer {
+    private static HashMap<Integer,Integer> RandomNumberHash = new HashMap<>();
+
+    static HashMap<Integer, Integer> generateRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        RandomNumberHash.clear();
+        int i = 1;
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+                RandomNumberHash.put(randomNumber,i);
+                i++;
+            }
+        }
+        return RandomNumberHash;
+    }
+}
