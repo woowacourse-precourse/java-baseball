481ecfc7d439be24bdb16a1fb1b7366efe15780d
baekeunsun <eunsun5460@gmail.com>
2022-11-07 13:21:07 +0900

    feat(Application): 게임 힌트 기능
    - Hint: 컴퓨터값과 사용자값을 비교하여 strike와 ball개수를 count
    - HintResult: strike와 ball개수에 따라 힌트 결과 분류

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,9 @@
 package baseball;
 
 import java.util.*;
-import java.util.stream.Stream;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
-import jdk.jshell.execution.Util;
 
 public class Application {
     private static final int NUM_SIZE = 3;
@@ -13,6 +11,8 @@ public class Application {
         // TODO: 프로그램 구현
         List<Integer> computer = ComputerGenerate();
         List<Integer> user = UserGenerate();
+        System.out.println(computer);
+        System.out.println(Hint(computer,user));
     }
 
     private static List<Integer> ComputerGenerate() {
@@ -86,5 +86,36 @@ public class Application {
             return false;
         }
         return true;
+    }
+
+    private static String Hint(List<Integer> computer, List<Integer> user) {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i<NUM_SIZE; i++) {
+            if (computer.get(i) == user.get(i)) {
+                strike += 1;
+                continue;
+            }
+            if (computer.contains(user.get(i))) {
+                ball += 1;
+            }
+        }
+        return (HintResult(strike,ball));
+    }
+
+    private static String HintResult(int strike, int ball) {
+        if (strike == 0 && ball == 0) {
+            return ("낫싱");
+        }
+        if (strike == 3) {
+            return ("3스트라이크\n"+"3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+        if (strike == 0) {
+            return (ball+"볼");
+        }
+        if (ball == 0) {
+            return (strike+"스트라이크");
+        }
+        return (ball+"볼 "+strike+"스트라이크");
     }
 }
