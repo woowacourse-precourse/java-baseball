96f30b47b4c1edcb94a5d0009b32b15592c015cd
TaemHam <tk4003950@gmail.com>
2022-11-07 19:41:07 +0900

    Refactor: 추상클래스와의 혼동을 방지하기 위해 BaseballGame 이름 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,10 +5,10 @@ import camp.nextstep.edu.missionutils.Console;
 public class Application {
 
     public static void main(String[] args) {
-        Game game = new Game();
+        BaseballGame baseballGame = new BaseballGame();
         boolean continueProcess = true;
         while (continueProcess) {
-            continueProcess = game.operate(Console.readLine());
+            continueProcess = baseballGame.operate(Console.readLine());
         }
     }
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -16,12 +16,16 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
-public class Game extends abstracts.Game {
+public class BaseballGame extends abstracts.Game {
 
+    /**
+     * 미션 구현사항엔 없지만, 숫자 개수를 바꾸는 기능을 추가할 수 있도록
+     * 확장성을 고려해 상수화 하지 않았습니다.
+     */
     private int answerNumberCount;
     private List<Integer> answerNumbers;
 
-    Game() {
+    BaseballGame() {
         this.gameName = "Baseball";
         this.status = Status.PLAYING;
         this.answerNumberCount = DEFAULT_NUMBER_COUNT;
@@ -45,7 +49,6 @@ public class Game extends abstracts.Game {
         Messages.INPUT.printMessage();
     }
     private List<Integer> getRandomNumbersOf(int count) {
-        count = Math.max(1, Math.min(9, count));
         List<Integer> numbers = new ArrayList<>();
         while (numbers.size() < count) {
             addNewRandomNumberTo(numbers);
@@ -66,19 +69,32 @@ public class Game extends abstracts.Game {
         setAnswerNumbers(Collections.emptyList());
     }
 
+    /**
+     * 게임 진행 상태에 따라 다르게 입력을 처리합니다.
+     * 프로세스 종료 여부를 판단하기 위해 논리값을 반환합니다.
+     */
     @Override
     public boolean operate(String input) {
-        System.out.println(input);
         return operationMapper
                 .get(this.status)
                 .test(input);
     }
+
+    /**
+     * 게임 진행 상태에 따라 입력을 달리 처리할 때,
+     * if문 없이 분기 처리를 하기 위해 사용되는 해시맵입니다.
+     */
     private final Map<Status, Predicate<String>> operationMapper = new HashMap<>();
     {
         operationMapper.put(Status.PLAYING, this::playTurn);
         operationMapper.put(Status.DONE, this::askAfterGameOption);
     }
 
+    /**
+     * 게임이 진행중이라면, 입력값을 추측 명령어로 판단해 처리합니다.
+     * 게임이 완료되더라도 프로세스는 진행되어야 하므로,
+     * 반환값은 어느 경우든 true입니다.
+     */
     private boolean playTurn(String input) {
         isGuessCommandValid(input, answerNumberCount);
         HashMap<Result, Integer> resultCount = getGuessResult(input);
@@ -113,6 +129,11 @@ public class Game extends abstracts.Game {
                 .findFirst()
                 .get();
     }
+
+    /**
+     * 특정 숫자의 추측 결과를 알려줄 때,
+     * if문 없이 분기 처리를 하기 위해 사용되는 해시맵입니다.
+     */
     private final Map<Result, BiPredicate<Integer, Integer>> resultMapper = new HashMap<>();
     {
         resultMapper.put(Result.STRIKE, this::isNumberStrike);
@@ -142,6 +163,11 @@ public class Game extends abstracts.Game {
                 .get(endCommand)
                 .get();
     }
+
+    /**
+     * 게임 종료 후, 입력에 따라 다음 동작을 처리할 때,
+     * if문 없이 분기 처리를 하기 위해 사용되는 해시맵입니다.
+     */
     private final Map<EndCommand, Supplier<Boolean>> endCommandMapper = new HashMap<>();
     {
         endCommandMapper.put(EndCommand.RESTART, this::restartProcess);
