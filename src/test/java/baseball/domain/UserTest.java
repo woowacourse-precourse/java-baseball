4917c97a78632b1dc5340bff920333771708a366
slbin-park <smpts00@naver.com>
2022-11-07 19:37:00 +0900

    test(BaseballServiceTest) : BaseballServiceTest 클래스 테스트 코드 추가
    - 스트라이크_체크 , 볼_체크 테스트 코드 추가

--- src/test/java/baseball/domain/UserTest.java
+++ src/test/java/baseball/domain/UserTest.java
@@ -1,12 +1,5 @@
 package baseball.domain;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-import java.io.ByteArrayInputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class UserTest {
     User user;
 
---
+++ src/test/java/baseball/service/BaseballServiceTest.java
@@ -0,0 +1,32 @@
+package baseball.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class BaseballServiceTest {
+    private BaseballService baseballService;
+
+    BaseballServiceTest() {
+        this.baseballService = new BaseballService();
+    }
+
+    @Test
+    void 스트라이크_체크() {
+        int answer = 1;
+        int strike = baseballService.isStrike(1, 1);
+        Assertions.assertEquals(answer, strike);
+    }
+
+    @Test
+    void 볼_체크() {
+        int answer = 1;
+        ArrayList<Integer> numbers = new ArrayList<>(Arrays.asList(1, 2, 3));
+        int index = 2;
+        int strike = baseballService.isBall(numbers, answer, index);
+        Assertions.assertEquals(answer, strike);
+    }
+
+}
