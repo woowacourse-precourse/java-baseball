6f58db78195b40c3242c7d1b55f9b5d6075cb9c5
Songyi Kim <ksl2950@naver.com>
2022-11-07 22:28:43 +0900

    test: NumberGenerator 테스트 메서드 추가
    - 1~9사이의 수가 생성되는지 검증하는 메서드

--- src/test/java/baseball/NumberGeneratorTest.java
+++ src/test/java/baseball/NumberGeneratorTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,6 +26,16 @@ class NumberGeneratorTest {
         // then
         long uniqueNumberLength = integerList.stream().distinct().count();
         assertThat(uniqueNumberLength).isEqualTo(expectedNumberLength);
+    }
+
+    @Test
+    void generateRandomNumber_메서드로_1과9사이의_3자리수_반환() {
+        // given, when
+        List<Integer> integerList = numberGenerator.generateRandomNumber();
+        // then
+        List<Integer> collect = integerList.stream()
+                .filter(integer -> integer < 1 || integer > 9).collect(Collectors.toList());
+        assertThat(collect).isEmpty();
     }
 
 }
