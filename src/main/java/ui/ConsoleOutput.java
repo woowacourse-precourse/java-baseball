13461bd8605de3ef6767ba08679dd849497c3118
choiejae <ejae8319@gmail.com>
2022-11-08 01:33:04 +0900

    feat: Enum을 활용한 메세지 내용 상수화, 정리

--- src/main/java/ui/ConsoleOutput.java
+++ src/main/java/ui/ConsoleOutput.java
@@ -10,19 +10,19 @@ public class ConsoleOutput implements Output{
     }
     @Override
     public void printStartMessage() {
-        printMessage("숫자 야구 게임을 시작합니다.\n");
+        printMessage(Messages.START_GAME.getMessage());
     }
     @Override
     public void printRequestNumberInput() {
-        printMessage("숫자를 입력해주세요 : ");
+        printMessage(Messages.REQUEST_NUMBER_INPUT.getMessage());
     }
     @Override
     public void printEndGame() {
-        printMessage("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+        printMessage(Messages.END_GAME.getMessage());
     }
     @Override
     public void printRequestRestartGameInput() {
-        printMessage("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+        printMessage(Messages.REQUEST_RESTART_GAME_INPUT.getMessage());
     }
     @Override
     public void printGameResult(String buffer) {
---
+++ src/main/java/ui/Messages.java
@@ -0,0 +1,17 @@
+package ui;
+
+public enum Messages {
+
+    START_GAME("숫자 야구 게임을 시작합니다.\n"),
+    REQUEST_NUMBER_INPUT("숫자를 입력해주세요 : "),
+    END_GAME("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"),
+    REQUEST_RESTART_GAME_INPUT("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+
+    private final String message;
+    Messages(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+}
