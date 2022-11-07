b3b8a0d44e0d610137fdd67d11edd9409c0580bd
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-07 15:43:55 +0900

    전 기능 구현 후 main() 구성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        showProblem();
     }
 
     static int readUser() {
