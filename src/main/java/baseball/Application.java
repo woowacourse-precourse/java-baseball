aad232583ea1b638ebcbe1e9ca7e6a4f02ea4ba3
MuHyeonSon <smuhyeon84@gmail.com>
2022-11-08 00:03:10 +0900

    feat(Application) : 컴퓨터가 1에서 9까지 서로다른 임의의 수 3개를 선택하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,11 @@ package baseball;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
+
 
 public class Application {
     public static void main(String[] args) {
@@ -12,8 +16,11 @@ public class Application {
 
         while (true) {
             System.out.println("숫자 야구 게임을 시작합니다.");
-            //numberPresentedByPlayer = recivedUeserInput.nextLine();
-            numberPresentedByPlayer = reciveUeserInput();
+            //numberPresentedByPlayer = receivedUserInput.nextLine();
+            List<String> threeNumber = new ArrayList<>();
+            threeNumber = createThreeNumbers();
+            System.out.println(threeNumber);
+            numberPresentedByPlayer = receiveUserInput(); // 게임플레이어로 부터 숫자를 입력받고 변수에 저장
             System.out.println(numberPresentedByPlayer);
 
             break;
@@ -21,19 +28,38 @@ public class Application {
         }
     }
 
-    // user로부터 3가지 숫자를 입력받는 함수
-    public static String reciveUeserInput(){
+    // user로부터 3개의 숫자를 입력받는 메서드
+    public static String receiveUserInput(){
 
-        Scanner recivedUeserInput = new Scanner(System.in); // Scanner 클래스를 통해 시스템의 입력장치로부터 읽은 Scanner 생성하여 변수에 저장
+        Scanner receivedUserInput = new Scanner(System.in); // Scanner 클래스를 통해 시스템의 입력장치로부터 읽은 Scanner 생성하여 변수에 저장
 
-        //BufferedReader reciveInput = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedReader receiveInput = new BufferedReader(new InputStreamReader(System.in));
 
         //입력 데이터 읽기
         System.out.print("숫자를 입력해주세요 : ");
-        String numbers = recivedUeserInput.nextLine();
+        String numbers = receivedUserInput.nextLine();
         return numbers;
 
     }
 
+    // 컴퓨터가 1에서 9까지 서로 다른 임의의 수 3개를 선택하는 메서드
+    public static List<String> createThreeNumbers(){
+        List<String> threeNumbersOfComputer = new ArrayList<>();
+        Random random = new Random();
+        while (true) {
+            if (threeNumbersOfComputer.size() == 3){
+                break;
+            }
+            else{
+                int selectedNumberByComputer =  random.nextInt(9)+1;
+                String selectedNumberByComputerStringType = String.valueOf(selectedNumberByComputer);
+                // 생성된 난수가 이미 이전에 생성됐던 수가 아니라면 선택하기 (서로 다른 3개의 수이므로)
+                if (!threeNumbersOfComputer.contains(selectedNumberByComputerStringType)){
+                    threeNumbersOfComputer.add(String.valueOf(selectedNumberByComputerStringType));
+                }
+            }
+        }
+        return threeNumbersOfComputer;
+    }
 
 }
