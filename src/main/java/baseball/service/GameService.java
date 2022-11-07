e582ea91c5e093a479d6fb59a908aa83210d8806
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-08 02:19:15 +0900

    refactor : 입력한 숫자의 예외처리 inputOrder 함수 리팩토링

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -8,26 +8,31 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 public class GameService {
     public static final int BALL_NUMBERS = 3;
-    public final static List<Integer> RANDOMBALL = new ArrayList<>();
     public static int strike = 0;
+    public final static List<Integer> RANDOMBALL = new ArrayList<>();
     public static List<Integer> userball = new ArrayList<>();
     public static void startOrder(){
         System.out.println("숫자 야구 게임을 시작합니다.");
         GameService.makeRandomBall();
     }
     public static void inputOrder() {
-
         while(strike != 3){
-            try {
-                System.out.print("숫자를 입력해주세요 : ");
-                String inputball = Console.readLine();
-                checkHandler(inputball);
-                userball = stringToList(inputball);
-            } catch (IllegalArgumentException e){
-                System.out.println("잘못된 입력입니다. 프로그램을 종료합니다.");
-                break;
+            int errorcheck = 0;
+            try {   checkInput();
+            } catch (IllegalArgumentException e){   errorcheck = errorOutput();
             }
+            if(errorcheck == 1) break;
         }
+    }
+    public static void checkInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputball = Console.readLine();
+        checkHandler(inputball);
+        userball = stringToList(inputball);
+    }
+    public static int errorOutput() {
+        System.out.println("잘못된 입력입니다. 프로그램을 종료합니다.");
+        return 1;
     }
     public static List<Integer> makeRandomBall(){
         while(RANDOMBALL.size()<BALL_NUMBERS){
@@ -46,7 +51,6 @@ public class GameService {
         }
         return usernumberdata;
     }
-
     public static void checkHandler(String inputball){
         ExceptionHanlder exceptionHanlder = new ExceptionHanlder();
 
