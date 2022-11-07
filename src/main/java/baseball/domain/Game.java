1fcaa911d05e94811a65e07ee6b3763f36305b85
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-07 23:56:23 +0900

    feat(Game): add domain

---
+++ src/main/java/baseball/domain/Game.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+public class Game {
+    private Computer computer;
+    private User user;
+    private int strike;
+    private int ball;
+    private int digit;
+    private int startRange;
+    private int endRange;
+
+}
