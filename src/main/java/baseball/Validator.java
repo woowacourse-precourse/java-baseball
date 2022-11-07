3d7f0b6364938cf291d9c6252e38e3ba60693d70
taekwon-dev <taekwon_dev@naver.com>
2022-11-08 00:15:29 +0900

    feat: 투수 입력 값 0을 제외한 수로만 구성됐는지 검증

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -7,9 +7,25 @@ public class Validator {
         if (!validateGameNumberLength(pitchValue)) {
             throw new IllegalArgumentException("3자리 수를 입력해주세요.");
         }
+
+        if (!validateIsAllDigitExceptForZero(pitchValue)) {
+            throw new IllegalArgumentException("0을 제외한 숫자만 입력해주세요.");
+        }
     }
 
     private boolean validateGameNumberLength(String value) {
         return value.length() == GAME_LENGTH;
+    }
+
+    private boolean validateIsAllDigitExceptForZero(String value) {
+        for (int idx = 0; idx < value.length(); idx++) {
+            if (!Character.isDigit(value.charAt(idx))) {
+                return false;
+            }
+            if (value.charAt(idx) == '0') {
+                return false;
+            }
+        }
+        return true;
     }
 }
