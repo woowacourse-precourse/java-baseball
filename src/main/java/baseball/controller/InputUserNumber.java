82f9eace4f9f5baa4452e6aeb44027b3b2ad260b
Sieun Park <psieun0606@gmail.com>
2022-11-07 09:40:02 +0900

    refactor: return하는 ArrayList의 자료형 Integer로 변경

--- src/main/java/baseball/controller/InputUserNumber.java
+++ src/main/java/baseball/controller/InputUserNumber.java
@@ -1,15 +1,19 @@
 package baseball.controller;
 
 import java.util.ArrayList;
+
+import baseball.view.InputView;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputUserNumber {
-    public ArrayList<Character> InputUserNumber() {
+    private static InputView inputView = new InputView();
+    public ArrayList<Integer> InputUserNumber() {
+        inputView.InputUserNum();
         String userNum = Console.readLine();
-        ArrayList<Character> userNumList = new ArrayList<>();
+        ArrayList<Integer> userNumList = new ArrayList<>();
 
         for (int i = 0; i < userNum.length(); i++) {
-            userNumList.add(userNum.charAt(i));
+            userNumList.add(userNum.charAt(i) - '0');
         }
 
         return userNumList;
