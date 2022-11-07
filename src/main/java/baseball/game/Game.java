f5fb08dd7f4e2696a2170512ddfbb5215a8363c9
wonju-dev <wonjulee.dev@gmail.com>
2022-11-07 14:38:29 +0900

    feat: 결과값 파싱 기능 구현
    getResultMessage()메소드는 사용자의 입력을 게임 규칙에 따라 분석한 결과를
    기능 요구사항에 맞춰 적절히 파싱한 뒤, 콘솔 출력 메시지를 구성하는 기능을 수행한다.

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -6,14 +6,31 @@ import baseball.game.rule.RuleChecker;
 import baseball.game.rule.BallCountRule;
 import baseball.game.rule.StrikeCountRule;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class Game {
     private static boolean isNothing(Map<Result, Integer> guessResult) {
         return guessResult.get(Result.BALL) == 0 && guessResult.get(Result.STRIKE) == 0;
     }
 
+    private static String getResultMessage(Map<Result, Integer> guessResult) {
+        List<String> results = new ArrayList<>();
+
+        Set<Map.Entry<Result, Integer>> entries = guessResult.entrySet();
+        for (Map.Entry<Result, Integer> entry : entries) {
+            Result key = entry.getKey();
+            Integer value = entry.getValue();
+
+            if (value != 0) {
+                results.add(value + key.getValue());
+            }
+        }
+
+        return String.join(" ", results);
+    }
 
     private static Map<Result, Integer> validateGuess(Integer answer, Integer guess) {
         // should keep this list element order for correct console output
