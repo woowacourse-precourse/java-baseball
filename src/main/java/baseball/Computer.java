61ffedfe01cd2bbd29e1c1c1d2d8fabb4d120e9b
Seungyong Choi <ppyooy336@naver.com>
2022-11-08 00:45:16 +0900

    컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 선택한다.

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Computer {
+    private List<Integer> number;
+
+    public Computer() {
+        number = makeRandomNumber();
+    }
+
+    private List<Integer> makeRandomNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+}
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,10 +2,13 @@ package baseball;
 
 public class Game {
 
+    private Computer computer;
     public Game(){
     }
 
-    public void init(){
+    public void init(Computer computer){
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        this.computer = computer;
     }
 }
