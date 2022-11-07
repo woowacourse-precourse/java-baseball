a0c06f7da3570cc6b32add24c262a0c5aae335dc
Eundms <dms011221@gmail.com>
2022-11-07 14:10:24 +0900

    fix(BaseballGame): initCommand

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -35,9 +35,11 @@ class BaseballGame {
         player = new ArrayList<>(Arrays.asList(0, 0, 0));
     }
 
+    private void initCommand(){System.out.println("숫자 야구 게임을 시작합니다.");}
     private void initGame() {
         initComputer();
         initPlayer();
+        initCommand();
     }
 
 
