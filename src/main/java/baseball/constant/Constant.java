74b0ea0bb5138ceff064085f1f8c4e1b6957130e
leesangjin <tkdwls0628@gmail.com>
2022-11-07 23:28:15 +0900

    feat(Constant): 여러 함수에서 사용되는 상수 정의

---
+++ src/main/java/baseball/constant/Constant.java
@@ -0,0 +1,10 @@
+package baseball.constant;
+
+public class Constant {
+    public static final int SIZE_NUMBER = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+
+    public static final String RESTART_GAME = "1";
+    public static final String END_GAME = "2";
+}
