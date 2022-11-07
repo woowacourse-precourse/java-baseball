81e5ab84b6d35c0a41140c9ee053a58eb4435cd2
LEE-sh1673 <lsh901673@gmail.com>
2022-11-07 14:32:16 +0900

    feat(numberGenerator): 중복되지 않은 3자리 숫자 생성 기능 추가

---
+++ src/main/java/baseball/NumberGenerator.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class NumberGenerator {
+
+	public static List<Integer> createNonDuplicateNumbers() {
+		List<Integer> numbers = new ArrayList<>();
+
+		while (numbers.size() < 3) {
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+			if (!numbers.contains(randomNumber)) {
+				numbers.add(randomNumber);
+			}
+		}
+		return numbers;
+	}
+}
---
+++ src/test/java/baseball/NumberGeneratorTest.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.RepeatedTest;
+
+@DisplayNameGeneration(
+	DisplayNameGenerator.ReplaceUnderscores.class
+)
+public class NumberGeneratorTest {
+
+	@DisplayName("중복되지 않은 3자리 숫자 생성 테스트")
+	@RepeatedTest(50)
+	void createNonDuplicateNumbers_중복되지_않은_3자리_숫자_생성() {
+		List<Integer> numbers
+			= NumberGenerator.createNonDuplicateNumbers();
+		assertDoesNotThrow(() -> new Balls(numbers));
+	}
+}
