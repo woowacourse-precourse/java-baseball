a3101602ab97f7c7de20e7ad13e240fbdbd05608
gilyeon00 <12venus08@naver.com>
2022-11-08 05:34:08 +0900

    feat: 랜덤 숫자가 제공되는 기능 구현

--- src/main/java/baseball/Game/Number.java
+++ src/main/java/baseball/Game/Number.java
@@ -3,10 +3,15 @@ package baseball.Game;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Number {
-    static final int MAX_NUM = 9;
-    static final int MIN_NUM = 1;
 
-    public int getRandomNum() {
-        return Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
+    public int getRandomNum(int len, int MIN_NUM, int MAX_NUM) {
+        int num = 0;
+
+        for (int i = 0; i < len; i++){
+            num *= 10;
+            num += Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
+        }
+        return num;
     }
+
 }
