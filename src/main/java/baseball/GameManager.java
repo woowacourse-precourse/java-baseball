e7c15f335e9def6075e084f3232aa42ea87ac5f8
고동우 <shp05179@gmail.com>
2022-11-07 17:16:37 +0900

    feat: 스트라이크, 볼, 아웃 판별
    해시맵을 이용해 자릿수를 검사하여 스트라이크, 볼, 낫싱 여부를 판별하여 결과변수에 저장한다.

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class GameManager {
     private static HashMap<Integer,Integer> RandomNumberHash = new HashMap<>();
     private static HashMap<Integer,Integer> InputNumberHash = new HashMap<>();
-
+    private static HashMap<Character,Integer> resultHash = new HashMap<>();
     void init() {
         RandomNumberHash = Computer.generateRandomNumber();
     }
@@ -20,6 +20,23 @@ public class GameManager {
             InputNumberHash.put(digit, index);
         }
         return InputNumberHash;
+    }
+
+    static HashMap<Character,Integer> judgeResult() {
+        resultHash.put('S', 0);
+        resultHash.put('B', 0);
+        resultHash.put('O', 0);
+        InputNumberHash.forEach((intNumber, intIndex)->{
+            if (!RandomNumberHash.containsKey(intNumber)){
+                resultHash.put('O', resultHash.get('O')+1);
+            } else if (RandomNumberHash.get(intNumber)==intIndex) {
+                resultHash.put('S', resultHash.get('S')+1);
+            } else if (RandomNumberHash.get(intNumber)!=intIndex) {
+                resultHash.put('B', resultHash.get('B')+1);
+            }
+        });
+        System.out.println(resultHash);
+        return resultHash;
     }
 
 }
