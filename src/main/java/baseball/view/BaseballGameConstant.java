4eaeedd0696120434061c9a1dff4fe4a99b34d71
Qulip <ilkwon.ryu@gmail.com>
2022-11-08 05:09:23 +0900

    feat: 입력 유효성 확인 기능 추가

--- src/main/java/baseball/view/BaseballGameConstant.java
+++ src/main/java/baseball/view/BaseballGameConstant.java
@@ -8,4 +8,6 @@ public class BaseballGameConstant {
     public static String NOTHING_MATCHED = "낫싱";
     public static String ALL_MATCHED_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static String GAME_FINISHED_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static String INPUT_NUMBER_ERROR_MESSAGE = "입력한 숫자가 올바르지 않습니다.";
 }
--- src/main/java/baseball/view/BaseballGameInputView.java
+++ src/main/java/baseball/view/BaseballGameInputView.java
@@ -1,14 +1,19 @@
 package baseball.view;
 
+import static baseball.view.BaseballGameConstant.INPUT_NUMBER_ERROR_MESSAGE;
 import static baseball.view.BaseballGameConstant.INPUT_NUMBER_MESSAGE;
 
 import baseball.domain.BaseballNumber;
+import baseball.exception.InvalidInputException;
 import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballGameInputView {
     public static BaseballNumber getPlayerNumber(){
         System.out.print(INPUT_NUMBER_MESSAGE);
         String playerNumber = Console.readLine();
+        if(!BaseballGameInputValidator.validate(playerNumber)){
+            throw new InvalidInputException(INPUT_NUMBER_ERROR_MESSAGE);
+        }
         return new BaseballNumber(playerNumber);
     }
 }
