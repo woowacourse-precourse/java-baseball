f4bceff1dbeb675f8c482184a75e7142783037f1
yun <yshyeon102@gmail.com>
2022-11-07 15:35:26 +0900

    feat(game): 게임 단판 메서드 구현
    #TODO
    - getUserNumber 메서드 : 입력 값을 요청하는 메세지를 보여주고, 사용자에게 검증 받은 입력 값을 요청.
    - compareTwoNumberStrings 메서드 : 두 수를 비교하여 볼과 스트라이크 값을 갖는 해시 맵을 구함.
    - checkAnswer : 스트라이크 개수로 정답을 판단.
    - checkRestart : 재시작 여부를 묻고 종료인 경우 게임 상태를 false로 변경

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import java.util.HashMap;
+
 public class Game {
     public static final int NUMBER_LENGTH = 3;
+    public static final String STRIKE = "strike";
+    public static final String BALL = "ball";
     public static MessagePrinter messagePrinter = new MessagePrinter();
 
     public boolean runningState = true;
@@ -15,6 +19,17 @@ public class Game {
     }
 
     private void start(String computerNumber) {
-
+        while (true) {
+            String userNumber = getUserNumber();
+            HashMap<String, Integer> resultMap = compareTwoNumberStrings(computerNumber, userNumber);
+            int ballCount = resultMap.getOrDefault(BALL, 0);
+            int strikeCount = resultMap.getOrDefault(STRIKE, 0);
+            messagePrinter.printResult(ballCount, strikeCount);
+            if (checkAnswer(strikeCount)) {
+                break;
+            }
+        }
+        messagePrinter.printFinish();
+        checkRestart();
     }
 }
