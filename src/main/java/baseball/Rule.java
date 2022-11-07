f2bf844ce587c2822b281bcbe31019bd2e57e4a8
LeeMyeongJin <=>
2022-11-07 20:56:20 +0900

    Feat: Rule class 생성

---
+++ src/main/java/baseball/Rule.java
@@ -0,0 +1,26 @@
+package baseball;
+
+public class Rule {
+    public int contain(String computer, String user) {
+        int total = 0;
+        for (String number : computer.split("")) {
+            if (user.contains(number)) { // 몇개 숫자가 같은지
+                total++;
+            }
+        }
+        return total;
+    }
+
+
+    public int strike(String computer, String user) {
+        int strike = 0;
+        for (int i = 0; i < 3; i++) {
+            if (user.charAt(i) == computer.charAt(i)) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+
+}
