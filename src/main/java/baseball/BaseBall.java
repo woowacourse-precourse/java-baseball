71bbbc3b0d20a00d79edf039cb96b114d33f9819
BioBeBe <76714219+JaegeonYu@users.noreply.github.com>
2022-11-07 14:56:50 +0900

    refactor: while 내 true -> boolea 상수로 변환

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -3,6 +3,7 @@ package baseball;
 import baseball.game.GameCycle;
 
 public class BaseBall {
+    private static final boolean CONTINUE = true;
     public void helloMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
@@ -12,7 +13,7 @@ public class BaseBall {
         GameCycle gameCycle = new GameCycle();
         AfterGame afterGame = new AfterGame();
         helloMessage();
-        while (true) {
+        while (CONTINUE) {
             gameCycle.play();
             if (!afterGame.play()) break;
         }
