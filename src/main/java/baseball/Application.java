0f4fb96dd6f06717487bc737a5ebba26336951c4
Kijung Park <kjpark4321@naver.com>
2022-11-08 01:14:13 +0900

    feat : 세자리 정답 난수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,13 +5,16 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Application {
-    
+
+    private static final int MIN = 100;
+    private static final int MAX = 999;
     private static final Scanner stdinScanner = new Scanner(System.in);
+    private static int answerNumber;
 
     private static List<Integer> gameResult(int inputNumber){
         ArrayList<Integer> resultList = new ArrayList<>();
 
-        
+
     }
 
     private static boolean eachGame(){
@@ -23,9 +26,12 @@ public class Application {
 
     private static void baseballGameRun(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+        answerNumber = (int)((Math.random()*(MAX - MIN)) + MIN);
+        System.out.println(answerNumber);
     }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        baseballGameRun();
     }
 }
