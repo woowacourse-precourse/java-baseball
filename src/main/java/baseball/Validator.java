57ca410c56b2855f93fc6925beb486ccb6b393d0
dain-lee <dain9934@gmail.com>
2022-11-07 20:51:00 +0900

    feat(validator): 입력한 수의 구성 문자 검증 메서드 구현

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -3,14 +3,24 @@ package baseball;
 public class Validator {
 
     public static final String LENGTH_EXCEPTION = "3자리 숫자를 입력해야 합니다.";
+    public static final String DIGIT_EXCEPTION = "각 자리는 숫자 1~9로 구성되어야 합니다."
 
     public static void validatePlayerInputNumbersString(String input) {
         validateLength(input);
+        validateDigit(input);
     }
 
     public static void validateLength(String input) {
         if (input.length() != 3) {
-            throw new IllegalArgumentException()
+            throw new IllegalArgumentException(LENGTH_EXCEPTION);
+        }
+    }
+
+    public static void validateDigit(String input) {
+        for (char c : input.toCharArray()) {
+            if (c < '1' || c > '9') {
+                throw new IllegalArgumentException(DIGIT_EXCEPTION);
+            }
         }
     }
 }
