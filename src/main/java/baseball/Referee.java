442c91919ab2fc64c7629dbb8cae183a79e31f78
June <68291395+june-777@users.noreply.github.com>
2022-11-07 21:34:48 +0900

    feat: 심판 스트라이크 개수 세기 기능

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -15,4 +15,17 @@ public class Referee {
 		}
 		return false;
 	}
+
+	public int countStrikeNum(List<Integer> computerBalls, List<Integer> playerBalls) {
+		int strikeNum = 0;
+		for (int ballIdx = 0; ballIdx < computerBalls.size(); ballIdx++) {
+			int computerBall = computerBalls.get(ballIdx);
+			int playerBall = playerBalls.get(ballIdx);
+
+			if (computerBall == playerBall) {
+				strikeNum += 1;
+			}
+		}
+		return strikeNum;
+	}
 }
