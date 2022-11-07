bb041db0ec97da02e809353a313047d6a5304441
jongmyeongbak <withpleasure@gmail.com>
2022-11-07 13:11:46 +0900

    feat: FEATURE #6 - game repetition

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
+    private static boolean keepGaming = true;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        do {
+            playSmallGame();
+            isKeepGaming();
+        } while (keepGaming);
+    }
+
+    private boolean isKeepGaming() {
+        String endGame = Console.readLine();
+        if (endGame == "1") {
+            keepGaming = true;
+        } else if (endGame == "2") {
+            keepGaming = false;
+        } else {
+            throw new IllegalArgumentException("잘못된 값을 입력하여 프로그램을 종료합니다.");
+        }
+        return keepGaming;
     }
 }
