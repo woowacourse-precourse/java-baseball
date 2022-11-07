e9e5c3469ec0e2a7addebd6fb21deea57d55ce3a
이건회 <rg970604@naver.com>
2022-11-08 05:03:21 +0900

    style(PrintResult.java) : 매직리터럴 상수선언

--- src/main/java/baseball/view/PrintResult.java
+++ src/main/java/baseball/view/PrintResult.java
@@ -1,20 +1,25 @@
 package baseball.view;
 
 import static java.lang.System.out;
+
 import baseball.domain.BallCount;
 
 public class PrintResult {
 
-    public static void printStartMent(){
-        out.println("숫자 야구 게임을 시작합니다.");
+    private static final String START_BASEBALL_GAME = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_CLEAR_MENT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static void printStartMent() {
+        out.println(START_BASEBALL_GAME);
     }
+
     public static void printBallCount(BallCount ballCount) {
         out.println(ballCount.ballCountResult());
     }
 
     public static void gameClearMent(boolean isgameEnd) {
         if (isgameEnd) {
-            out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            out.println(GAME_CLEAR_MENT);
         }
     }
 
