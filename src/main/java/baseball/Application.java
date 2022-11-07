8aac0a5dc779c1e40a63442010019b04f7de8c54
hyeonseog-won <smreo75@gmail.com>
2022-11-08 03:53:14 +0900

    feat: 게임 시작 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Computer computer = new Computer();
+        User user = new User();
+        Game game = new Game(computer, user);
+        game.start();
     }
 }
