4b5c40934e6cf7f7d43a2552145e14416543c775
=ryucb <=chouck12@naver.com>
2022-11-08 02:07:17 +0900

    input Function update & all refactoring

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,39 +1,31 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+public class Application{
 
-public class Application {
     static List<Integer> computerNumber = new ArrayList<>();
     static List<Integer> userNumber = new ArrayList<>();
     static Integer strikeCount = 0;
     static Integer ballCount = 0;
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        try{
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            System.out.println("숫자 야구 게임을 시작합니다.");
-            while(true) {
-                createRandomNumber();
-                Integer gameResult = startUserGame(br);
-                if(gameResult == 2){
-                    br.close();
-                    break;
-                }else if(gameResult == 1){
-                    resetValue();
-                }
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(true) {
+            createRandomNumber();
+            Integer gameResult = startUserGame();
+            if(gameResult == 2){
+                break;
+            }else if(gameResult == 1){
+                resetValue();
             }
-        }catch (IOException e){
-            e.printStackTrace();
         }
     }
 
+    // 랜덤함수생성
     public static void createRandomNumber(){
         while (computerNumber.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -43,54 +35,37 @@ public class Application {
         }
     }
 
-    public static void userInputNumbers(BufferedReader br) throws IOException{
+    // 게임스타트
+    public static Integer startUserGame(){
+        if(strikeCount != 3){
+            userInputNumbers();
+            countStrikeAndBall();
+            printStrikeAndBall();
+            return 0;
+        }else{
+            return answerNumber();
+        }
+    }
+
+    // 사용자 입력 및 유효성검사
+    public static void userInputNumbers(){
         userNumber.removeAll(userNumber);
 
         System.out.print("숫자를 입력해주세요 : ");
-        String inputStr = br.readLine();
-        String conversion = "";
+        String inputStr = readLine();
+        HashSet<String> overlapCheck = new HashSet<>();
 
-        for(int i=0; i<inputStr.length(); i++) {
-            if(inputStr.indexOf(inputStr.charAt(i)) == i){
-                conversion += inputStr.charAt(i);
-            }
-        }
+        Arrays.stream(inputStr.split("")).forEach(f -> {
+            overlapCheck.add(f);
+            userNumber.add(Integer.parseInt(f));
+        });
 
-        if(!inputStr.matches("[0-9]{3}") && inputStr.equals(conversion)){
+        if(!inputStr.matches("[0-9]{3}") || overlapCheck.size() != 3){
             throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
         }
-
-        for(String inputSingleStr : conversion.split("")){
-            userNumber.add(Integer.parseInt(inputSingleStr));
-        }
     }
 
-    public static Integer startUserGame(BufferedReader br) throws IOException{
-        if(strikeCount != 3){
-            userInputNumbers(br);
-            countStrikeAndBall();
-            printStrikeAndBall();
-        }else if(strikeCount == 3){
-            return answerNumber(br);
-        }
-        return 0;
-    }
-
-    public static void resetValue(){
-        strikeCount = 0;
-        ballCount = 0;
-    }
-
-    public static Integer answerNumber(BufferedReader br) throws IOException{
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String newGameChoice = br.readLine();
-        if(newGameChoice.equals("1") || newGameChoice.equals("2")){
-            return Integer.parseInt(newGameChoice);
-        }
-        throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
-    }
-
+    // ball & strike count
     public static void countStrikeAndBall(){
         strikeCount = 0;
         ballCount = 0;
@@ -104,6 +79,7 @@ public class Application {
         }
     }
 
+    // ball & strike info print
     public static void printStrikeAndBall(){
         if(ballCount > 0 && strikeCount > 0){
             System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
@@ -111,9 +87,28 @@ public class Application {
             System.out.println(strikeCount + "스트라이크");
         }else if(ballCount > 0){
             System.out.println(ballCount + "볼");
-        }else if(ballCount == 0 && strikeCount == 0){
+        }else{
             System.out.println("낫싱");
         }
     }
-}
 
+    // 정답 이후 로직(재시작 or 종료)
+    public static Integer answerNumber(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String newGameChoice = readLine();
+
+        if(newGameChoice.matches("[1-2]{1}")){
+            return Integer.parseInt(newGameChoice);
+        }else{
+            throw new IllegalArgumentException("잘못된 값을 입력했습니다.");
+        }
+    }
+
+    // 전역변수 reset
+    public static void resetValue(){
+        strikeCount = 0;
+        ballCount = 0;
+        computerNumber.removeAll(computerNumber);
+    }
+}
