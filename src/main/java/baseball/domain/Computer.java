0fbe45fb79920fe451bb04c978565420a0fe2667
jmjmjames <chungjm0711@gmail.com>
2022-11-07 14:11:25 +0900

    feat(Computer): Computer 도메인 package 로 이동 및 Constant 분리

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,15 +1,12 @@
-package baseball;
+package baseball.domain;
 
+import static baseball.global.constants.Constants.*;
 import static camp.nextstep.edu.missionutils.Randoms.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 9;
-    private static final int NUMBER_DIGIT = 3;
 
     private List<Integer> randomNumbers;
 
@@ -26,9 +23,9 @@ public class Computer {
     }
 
     private void generateRandomNumbers() {
-        randomNumbers = new ArrayList<>(NUMBER_DIGIT);
+        randomNumbers = new ArrayList<>(THREE_DIGITS);
 
-        while (randomNumbers.size() < NUMBER_DIGIT) {
+        while (randomNumbers.size() < THREE_DIGITS) {
             int number = pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             if (!randomNumbers.contains(number)) {
                 randomNumbers.add(number);
