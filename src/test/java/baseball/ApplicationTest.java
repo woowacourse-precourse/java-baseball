f47f5f0d96cb28ec8bed2e3397dc718f99aaa97b
josolha <josolha@nate.com>
2022-11-07 21:45:02 +0900

    test : unit tests for generate method

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,23 +1,41 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
+        @Nested
+    class generateTest {
+        @Test
+        void 랜덤_숫자_테스트1_3자리_확인() {
+            List<Integer> random = Application.generate();
+            assertThat(random.size()).isEqualTo(3);
+        }
+
+        @Test
+        void 랜덤_숫자_테스트2_숫자_범위_확인() {
+            List<Integer> random = Application.generate();
+            for (int i = 0; i < random.size(); i++) {
+                assertThat(random.get(i)).isBetween(0, 9);
+            }
+        }
+        @Test
+        void 랜덤_숫자_테스트2_중복_숫자() {
+            List<Integer> random = Application.generate();
+            Set<Integer> randomNumSet = new HashSet<>(random);
+            Assertions.assertThat(random.size()).isSameAs(randomNumSet.size());
+        }
     }
 
     @Test
