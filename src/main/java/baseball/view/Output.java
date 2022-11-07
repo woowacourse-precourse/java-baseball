ed143884dffd4999549c16f11b645ff8e5056e22
bumwoo <gld55602@gmail.com>
2022-11-07 22:11:10 +0900

    refactor: 결과 출력 String에서 StringBuffer로 변환

--- src/main/java/baseball/view/Output.java
+++ src/main/java/baseball/view/Output.java
@@ -6,7 +6,8 @@ public class Output {
     private static final String STARTMESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String ENDMESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String STRIKE = "스트라이크";
-    private static final String BALL = "볼 ";
+    private static final String BALL = "볼";
+    private static final String WHITE_SPACE = " ";
     private static final String NOTHING = "낫싱";
 
     private Number number;
@@ -16,18 +17,18 @@ public class Output {
     }
 
     public void printOutput() {
-        String result = "";
+        StringBuffer result = new StringBuffer();
         if (number.isNothing()) {
-            result = NOTHING;
+            result.append(NOTHING);
         }
         int ball = number.getBall();
         int strike = number.getStrike();
 
         if (ball > 0) {
-            result += ball + BALL;
+            result.append(ball).append(BALL).append(WHITE_SPACE);
         }
         if (strike > 0) {
-            result += strike + STRIKE;
+            result.append(strike).append(STRIKE);
         }
 
         System.out.println(result);
