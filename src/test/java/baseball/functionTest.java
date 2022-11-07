7074d099fa62ade5682cf54b64a9adf196471c30
GraphTree <graphnodesed@gmail.com>
2022-11-07 16:13:03 +0900

    test : 정답과 유저의 인풋을 비교하는 로직 테스트 케이스 작성

--- src/test/java/baseball/functionTest.java
+++ src/test/java/baseball/functionTest.java
@@ -43,4 +43,26 @@ public class functionTest {
 
 
     }
+
+    @Test
+    void 케이스별_인풋과_정답_비교() {
+        List<Integer> answer = List.of(1, 2, 3);
+        List<Integer> input1 = List.of(1, 2, 3); // 3스트라이크
+        List<Integer> input2 = List.of(4, 5, 6); // 낫싱
+        List<Integer> input3 = List.of(2, 1, 3); // 2볼 1스트라이크
+        List<Integer> input4 = List.of(5, 1, 3); // 1볼 1스트라이크
+        List<Integer> input5 = List.of(0, 3, 2);// 2볼
+        List<Integer> input6 = List.of(0, 2, 4);// 1스트라이크
+        List<Integer> input7 = List.of(3, 1, 2);// 3볼
+
+        assertThat(Application.compareAnswerToInput(answer, input1)).isEqualTo("3스트라이크");
+        assertThat(Application.compareAnswerToInput(answer, input2)).isEqualTo("낫싱");
+        assertThat(Application.compareAnswerToInput(answer, input3)).isEqualTo("2볼 1스트라이크");
+        assertThat(Application.compareAnswerToInput(answer, input4)).isEqualTo("1볼 1스트라이크");
+        assertThat(Application.compareAnswerToInput(answer, input5)).isEqualTo("2볼");
+        assertThat(Application.compareAnswerToInput(answer, input6)).isEqualTo("1스트라이크");
+        assertThat(Application.compareAnswerToInput(answer, input7)).isEqualTo("3볼");
+
+
+    }
 }
