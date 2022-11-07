32d2be6753d93d31496b97fa5ec0f0cafb6a813a
KIMSEI1124 <workju1124@gmail.com>
2022-11-08 01:13:48 +0900

    feat: service PlayerService 예외 처리 추가
    - inputNumbersToPlayer : 숫자의 개수가 3인지 확인하고 아니면 예외를 던짐
    - checkDigit : 숫자가 아니고 문자라면 예외를 던짐
    - numberToInt : 중복된 숫자가 입력되면 예외를 던짐

--- src/main/java/baseball/service/PlayerService.java
+++ src/main/java/baseball/service/PlayerService.java
@@ -1,6 +1,7 @@
 package baseball.service;
 
 import baseball.domain.Player;
+import baseball.exception.InputException;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -8,33 +9,59 @@ import java.util.List;
 
 public class PlayerService {
     private static final int LIST_SIZE = 3;
+    private static InputException inputException;
     Player player = new Player();
 
     /**
-     * 플레이어가 입력한 inputNumbers 을 String 배열로 변환시킨뒤<br>
-     * 변환된 numbers 리스트를 player 저장후 반환한다.
+     * 입력받은 문자가 숫자가 아니면 예외를 던진다.
+     *
+     * @param inputString 입력받은 문자
+     * @return 숫자인 입력받은 문자
+     */
+    private static String checkDigit(String inputString) {
+        inputException.isDigitException(inputString);
+        return inputString;
+    }
+
+    /**
+     * 입력받은 문자가 리스트에 중복이 있으면 예외를 던지고 없으면 <br>
+     * 문자를 숫자로 변환하여 반환한다.
+     *
+     * @param numbers     플레이어가 입력한 숫자들
+     * @param inputNumber 입력받은 숫자
+     * @return 중복이 아닌 int 로 변환된 숫자
+     */
+    private static int numberToInt(List<Integer> numbers, String inputNumber) {
+        int addNumber = Integer.parseInt(inputNumber);
+        inputException.isDuplicationNumberException(addNumber, numbers);
+        return addNumber;
+    }
+
+    /**
+     * 플레이어가 입력한 숫자의 개수를 확인한후 예외가 있으면 던진다. <br>
+     * 정상이라면 String 배열로 변환시킨뒤 변환된 numbers 리스트를 player 저장후 반환한다.
      *
      * @param inputNumbers 입력한 숫자들
      * @return 플레이어
      */
     public Player inputNumbersToPlayer(String inputNumbers) {
-        player.setNumbers(getNumbers(inputNumbers.split("")));
+        inputException.isInvalidNumbersSize(inputNumbers);
+        player.setNumbers(getNumbersToList(inputNumbers.split("")));
         return player;
     }
 
     /**
-     * inputNumbers 배열을 예외 처리를 거친뒤 예외가 있으면 예외를 던지고<br>
-     * 아니면 리스트로 변환하여 반환
+     * inputNumbers 배열을 리스트로 변환하여 반환
      *
      * @param inputNumbers 입력한 숫자들의 배열
      * @return 변환한 numbers 리스트
      */
-    private List<Integer> getNumbers(String[] inputNumbers) {
+    private List<Integer> getNumbersToList(String[] inputNumbers) {
         List<Integer> numbers = new ArrayList<>(Collections.emptyList());
-        // TODO: 예외처리 ( 문자의 크기가 3이 아닐시 예외를 던짐 )
-        // TODO: 예외처리 (입력한 문자에 숫자가 아닌 문자가 포함시 예외 처리 )
         for (int i = 0; i < LIST_SIZE; i++) {
-            numbers.add(Integer.parseInt(inputNumbers[i]));
+            String inputNumber = checkDigit(inputNumbers[i]);
+            int addNumber = numberToInt(numbers, inputNumber);
+            numbers.add(addNumber);
         }
         return numbers;
     }
