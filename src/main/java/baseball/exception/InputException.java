d57e88d49a05835624ac55860324e968506a71aa
TaeHwan <kimta2hwan@gmail.com>
2022-11-07 11:59:30 +0900

    feat(inputexception): 사용자가 입력한 재시작/종료를 구분하는 명령어가 1과 2가 아닌 경우 예외 처리 기능 구현

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -6,6 +6,7 @@ public class InputException {
     private static final String NOT_THREE_LENGTH_INTEGER_INPUT = "3자리의 정수를 입력해 주세요.";
     private static final String INCLUDE_ZERO_INPUT = "0이 포함되지 않아야 합니다.";
     private static final String DUPLICATE_NUMBER_INPUT = "중복되지 않는 수를 입력해 주세요.";
+    private static final String NOT_ONE_OR_TWO_INPUT = "1 또는 2를 입력해 주세요.";
 
     public static void throwNotIntegerInputError() {
         throw new IllegalArgumentException(NOT_INTEGER_INPUT);
@@ -21,5 +22,9 @@ public class InputException {
 
     public static void throwDuplicateNumberInputError() {
         throw new IllegalArgumentException(DUPLICATE_NUMBER_INPUT);
+    }
+
+    public static void throwNotOneOrTwoInputError() {
+        throw new IllegalArgumentException(NOT_ONE_OR_TWO_INPUT);
     }
 }
