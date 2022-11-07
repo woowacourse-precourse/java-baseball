554325396aa535a61ed0e45b368dda9380c0da9e
wlsh44 <k87913j@gmail.com>
2022-11-07 21:45:23 +0900

    refactor: user 클래스 추가하고 그에 맞게 gameCore 리팩터링

--- src/main/java/baseball/core/BaseballGameCore.java
+++ src/main/java/baseball/core/BaseballGameCore.java
@@ -8,38 +8,43 @@ import java.util.List;
 
 public class BaseballGameCore {
 
+    private final User user;
     private ComputerOpponent opponent;
     private final GameConsole console;
 
     public BaseballGameCore(GameConsole console) {
+        this.user = new User();
         this.console = console;
     }
 
     public void playBaseballGame() {
-        this.opponent = new ComputerOpponent(RandomNumberGenerator.generate());
-        while (true) {
-            List<Integer> threeNumbers = inputProcess();
-            BallStrikeDto answer = checkProcess(threeNumbers);
-            if (isFinished(answer)) {
-                break;
-            }
-        }
+        initOpponent();
+        BallStrikeDto answer;
+        do {
+            inputProcess();
+            answer = answerProcess();
+        } while (isWrongAnswer(answer));
         finishProcess();
     }
 
-    private List<Integer> inputProcess() {
-        console.printNumberInputMessage();
-        return console.inputThreeNumbers();
+    private void initOpponent() {
+        this.opponent = new ComputerOpponent(RandomNumberGenerator.generate());
     }
 
-    private BallStrikeDto checkProcess(List<Integer> threeNumbers) {
+    private void inputProcess() {
+        console.printNumberInputMessage();
+        user.inputThreeNumbers(console);
+    }
+
+    private BallStrikeDto answerProcess() {
+        List<Integer> threeNumbers = user.sayThreeNumbers();
         BallStrikeDto answer = opponent.answer(threeNumbers);
         console.printResultMessage(answer);
         return answer;
     }
 
-    private boolean isFinished(BallStrikeDto answer) {
-        return answer.getStrikeCount() == 3;
+    private boolean isWrongAnswer(BallStrikeDto answer) {
+        return answer.getStrikeCount() != 3;
     }
 
     private void finishProcess() {
