a5dafb7555cf880891561724fc5b3634f1edf672
kkkwp <chxrryda@gmail.com>
2022-11-08 01:42:50 +0900

    refactor(restart): gameOver에서 restart로 함수명 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,10 +18,10 @@ public class Application {
                 // result = computer vs user
                 System.out.println(result);
             }
-        } while (gameOver());
+        } while (restart());
     }
 
-    private static boolean gameOver() {
+    private static boolean restart() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
         String input = Console.readLine();
