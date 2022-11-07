4daeab8d23383ec889ef16ba2cfead386679499a
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-07 22:45:59 +0900

    Feat : 컴퓨터 랜덤값 발생
    makeComputerNumber() 메서드 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -16,6 +16,23 @@ public class BaseballGame {
         play();
     }
 
-    public void play(){}
+    public void play(){
+
+        List<Integer> computer = makeComputerNumber();
+
+    }
+
+    public static List<Integer> makeComputerNumber() {
+
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 
 }
