cd83a96c5b724809ff8e77e1392796a03f8fd533
Kloong1 <dkdlsmsm98@gmail.com>
2022-11-08 02:24:40 +0900

    feat: 사용자의 입력(3자리 수)을 받는 메소드 추가
    콘솔을 통해 사용자의 입력을 받고, 예외처리 한 뒤 int 로 파싱해서
    반환하는 메소드를 추가함.

--- src/main/java/baseball/controller/NumberBaseballController.java
+++ src/main/java/baseball/controller/NumberBaseballController.java
@@ -1,4 +1,23 @@
 package baseball.controller;
 
+import baseball.validation.UserInputValidator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class NumberBaseballController {
+
+    private final UserInputValidator validator;
+
+    public NumberBaseballController(UserInputValidator validator) {
+        this.validator = validator;
+    }
+
+    public int readUserInput() {
+        String input = Console.readLine();
+
+        validator.validateGuessInput(input);
+
+        return Integer.parseInt(input);
+    }
+
+    //guessAnswer()
 }
