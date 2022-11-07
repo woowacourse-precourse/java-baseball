74bf52946d6f6019f65cffa00eda4b7d95571a6d
Kloong1 <dkdlsmsm98@gmail.com>
2022-11-08 03:58:50 +0900

    feat: 숫자 야구 게임의 전체 흐름을 제어하는 메소드 추가

--- src/main/java/baseball/controller/NumberBaseballController.java
+++ src/main/java/baseball/controller/NumberBaseballController.java
@@ -1,14 +1,45 @@
 package baseball.controller;
 
+import baseball.dto.StrikeBallCountDto;
+import baseball.service.NumberBaseballService;
 import baseball.validation.UserInputValidator;
+import baseball.view.ConsoleView;
 import camp.nextstep.edu.missionutils.Console;
 
 public class NumberBaseballController {
 
     private final UserInputValidator validator;
+    private final NumberBaseballService numberBaseballService;
+    private final ConsoleView consoleView;
 
-    public NumberBaseballController(UserInputValidator validator) {
+    public NumberBaseballController(UserInputValidator validator, NumberBaseballService numberBaseballService, ConsoleView consoleView) {
         this.validator = validator;
+        this.numberBaseballService = numberBaseballService;
+        this.consoleView = consoleView;
+    }
+
+    public void playNumberBaseball() {
+        boolean isPlaying = true;
+
+        while (isPlaying) {
+            consoleView.printInputMessage();
+            int input = readUserInput();
+
+            StrikeBallCountDto strikeBallCountDto = numberBaseballService.compareInputAndAnswer(input);
+            consoleView.printStrikeBallCount(strikeBallCountDto);
+
+            if (strikeBallCountDto.getStrikeCount() == 3) {
+                consoleView.printSuccessMessage();
+                consoleView.printRestartMessage();
+                input = readRestartInput();
+            }
+
+            if (input == 1) {
+                numberBaseballService.initAnswerNumber();
+            } else if (input == 2) {
+                isPlaying = false;
+            }
+        }
     }
 
     private int readUserInput() {
