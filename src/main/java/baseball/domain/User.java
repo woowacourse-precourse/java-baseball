8120e1525a6f7aba7787037bf2a71ef7a6aedb2a
김선동 <mok03127@gmail.com>
2022-11-07 15:18:33 +0900

    feat: 사용자에게 수를 입력 받는 기능

---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,19 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+
+public class User {
+    ArrayList<Integer> userNumbers;
+
+    public ArrayList<Integer> getUserNumbers() {
+        return userNumbers;
+    }
+
+    public void setUserNumbers(String userInput) {
+        ArrayList<Integer> userNumberList = new ArrayList<>();
+        for(int i=0; i<userInput.length(); i++){
+            userNumberList.add(userInput.charAt(i)-'0');
+        }
+        this.userNumbers = userNumberList;
+    }
+}
