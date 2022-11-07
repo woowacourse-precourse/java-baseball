a4fe9ae1ee1c5bfb14d9d790dccc890bda5f8fa2
namtndus <rnrmf0721@gmail.com>
2022-11-07 21:14:08 +0900

    feat(inputAvailableUserAnswerService): 함수의 기능 구현
    '사용자가 응답을 옳바르게 했는지 체크하는 변수, checkedAvailableUserAnswer에 false로 초기화'를 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -161,6 +161,7 @@ public class Application {
 
     public static String inputAvailableUserAnswerService(){
         String userAnswer = "";
+        boolean checkedAvailableUserAnswer = false;
         return "";
     }
 
