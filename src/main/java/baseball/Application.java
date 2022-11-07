6536e3a3210cdd984698911da619c4cd255eeec6
Ran <lan4250@naver.com>
2022-11-08 02:44:49 +0900

    FEAT / 세 자리의 target number를 생성해주는 메서드 makeTargetNum() 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,38 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public static int makeTargetNum() {
+
+        List<Integer> target = new ArrayList<>();
+
+        while (target.size() < 3) {
+            int n = Randoms.pickNumberInRange(1, 9);
+            if (!target.contains(n)) {
+                target.add(n);
+            }
+        }
+
+        System.out.println("target " + target);
+        String stringOfTargetNum = "";
+        for (int n : target) {
+            stringOfTargetNum += n;
+        }
+
+        return Integer.parseInt(stringOfTargetNum);
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int target = makeTargetNum();
+
+
     }
+
+
 }
