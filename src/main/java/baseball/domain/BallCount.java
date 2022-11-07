0586d9e7edfdf881e66d48ca508630a79131b92f
이건회 <rg970604@naver.com>
2022-11-07 18:57:17 +0900

    feat (BallCount.java) : 객체의 카운트 정보를 응답

--- src/main/java/baseball/domain/BallCount.java
+++ src/main/java/baseball/domain/BallCount.java
@@ -1,11 +1,25 @@
 package baseball.domain;
 
 public class BallCount {
+
     private int Strikes;
     private int Balls;
 
     public BallCount(int strikes, int balls) {
         Strikes = strikes;
         Balls = balls;
+    }
+
+    public String ballCountResult() {
+        if (Balls == 0 && Strikes == 0) {
+            return "낫싱";
+        }
+        if (Strikes == 0) {
+            return String.valueOf(Balls) + "볼";
+        }
+        if (Balls == 0) {
+            return String.valueOf(Strikes) + "스트라이크";
+        }
+        return String.valueOf(Balls) + "볼 " + String.valueOf(Strikes) + "스트라이크";
     }
 }
