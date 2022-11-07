b9389649281c4f1b83f35a138f889a2278c69248
3juhwan <13selfesteem91@naver.com>
2022-11-08 02:22:59 +0900

    test: add verify verifying numbers function

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -12,6 +12,12 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ComputerTest {
     private static Computer computer;
 
+    @Test
+    void 숫자_유효성_검사_함수_검사() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+        assertThat(computer.isValidNumber(numbers)).isTrue();
+    }
+
     @BeforeEach
     private void createComputer() {
         computer = new Computer();
