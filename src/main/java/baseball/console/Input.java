5d369ea08ccc4d1a4596990cf64dcea05cc3d826
민병욱 <woook27@naver.com>
2022-11-08 05:47:56 +0900

    refactor (Input): 예외 처리를 클래스를 통해 구현

--- src/main/java/baseball/console/Input.java
+++ src/main/java/baseball/console/Input.java
@@ -1,7 +1,7 @@
 package baseball.console;
 
+import baseball.Exception.InputException;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.HashSet;
 
 public class Input {
     private static final String inputMessage = "숫자를 입력해주세요 : ";
@@ -10,45 +10,18 @@ public class Input {
     public static String get() {
         System.out.print(inputMessage);
         String number = Console.readLine();
-        if (!isValid(number)) {
+        if (!InputException.isValidInput(number)) {
             throw new IllegalArgumentException();
         }
         return number;
-    }
-
-    public static boolean isValid(String number) {
-        if (number.length() != 3) {
-            return false;
-        }
-        HashSet<Character> set = new HashSet<Character>();
-        for (char c : number.toCharArray()) {
-            if(!Character.isDigit(c)) {
-                return false;
-            }
-            if(c == '0') {
-                return false;
-            }
-            if(!set.isEmpty() && set.contains(c)) {
-                return false;
-            }
-            set.add(c);
-        }
-        return true;
     }
 
     public static String restart() {
         System.out.println(restartMessage);
         String number = Console.readLine();
-        if (!isValidRestartNumber(number)) {
+        if (!InputException.isValidRestartInput(number)) {
             throw new IllegalArgumentException();
         }
         return number;
-    }
-
-    private static boolean isValidRestartNumber(String number) {
-        if (number.equals("1") || number.equals("2")) {
-            return true;
-        }
-        return false;
     }
 }
