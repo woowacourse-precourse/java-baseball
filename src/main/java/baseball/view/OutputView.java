4de54a2977e4613afff5a637dd7fa9d644108e73
sooyoungh <sooyoung.h8@gmail.com>
2022-11-07 15:29:13 +0900

    feat(OutputView): 힌트 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,8 @@
 package baseball.view;
 
+import baseball.model.Hint;
+import baseball.model.Score;
+
 public class OutputView {
     private static final String START_GAME= "숫자 야구 게임을 시작합니다.";
     private static final String GAME_CLEAR= "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
@@ -30,5 +33,14 @@ public class OutputView {
 
     public static void printGameClear() {
         System.out.println(GAME_CLEAR);
+    }
+
+    public static void printHint(Score score) {
+        Hint hint = score.getScoreHint();
+
+        if (hint.equals(Hint.NOTHING)) OutputView.printNothing();
+        if (hint.equals(Hint.BALL_AND_STRIKE)) OutputView.printBallAndStrike(score.getBall(), score.getStrike());
+        if (hint.equals(Hint.ONLY_BALL)) OutputView.printBall(score.getBall());
+        if (hint.equals(Hint.ONLY_STRIKE)) OutputView.printStrike(score.getStrike());
     }
 }
