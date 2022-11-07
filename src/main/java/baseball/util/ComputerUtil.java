45ba70adea0275a5c3b3f1cebc4138bf476d23dc
SeongbinPark <binni0515@gmail.com>
2022-11-07 15:59:06 +0900

    feat(ComputerUtil): validate created computerNumbers - each digits must be different

--- src/main/java/baseball/util/ComputerUtil.java
+++ src/main/java/baseball/util/ComputerUtil.java
@@ -5,19 +5,45 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ComputerUtil {
+
     public static List<Integer> getComputerNumbers() {
         List<Integer> computerNumbers = new ArrayList<>(List.of(0, 0, 0));
 
         for (int i = 0; i < 3; i++) {
-            int randomNumber = getRandomNumber();
+            int validRandomNumber = getValidRandomNumber(computerNumbers);
 
-            computerNumbers.add(i, randomNumber);
+            computerNumbers.add(i, validRandomNumber);
         }
 
         return computerNumbers;
     }
 
+    private static int getValidRandomNumber(List<Integer> computerNumbers) {
+        while (true) {
+            int randomNumber = getRandomNumber();
+
+            if (isValidNumber(computerNumbers, randomNumber)) {
+                return randomNumber;
+            }
+        }
+    }
+
     private static int getRandomNumber() {
         return Randoms.pickNumberInRange(1, 9);
+    }
+
+    private static boolean isValidNumber(List<Integer> computerNumbers, int randomNumber) {
+        for (int i = 0; i < 3; i++) {
+            int computerNumber = computerNumbers.get(i);
+            if (computerNumber == 0) {
+                break;
+            }
+
+            if (computerNumber == randomNumber) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
