632d04c39ad68a2357eb9c1ed9cb6d6a56fa70df
JunH <tjdtls690@gmail.com>
2022-11-07 15:23:37 +0900

    :white_check_mark: test(Domain : BallPositionTest) : 올바른 볼 포지션인지 확인하는 테스트 추가

--- src/test/java/baseball/domain/BallPositionTest.java
+++ src/test/java/baseball/domain/BallPositionTest.java
@@ -5,8 +5,17 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 class BallPositionTest {
+    @DisplayName("올바른 볼 위치인지 확인")
+    @ParameterizedTest(name = "{displayName} => {0}")
+    @ValueSource(ints = {0, 1, 2})
+    void correct_ball_position(int ballPosition) {
+        assertThatNoException()
+                .isThrownBy(() -> new BallPosition(ballPosition));
+    }
+    
     @DisplayName("예외 처리 : 0~2 범위를 벗어난 경우")
     @ParameterizedTest(name = "{displayName} => {0}")
     @ValueSource(ints = {-1, 3})
