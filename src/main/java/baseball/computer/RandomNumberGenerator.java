2ee412ea7f1530880f80a8bd959ab5de26510372
kimminsu <xjfcnfw3@naver.com>
2022-11-07 09:35:09 +0900

    feat: 항상 새로운 난수만 생성되는 기능 구현

--- src/main/java/baseball/computer/RandomNumberGenerator.java
+++ src/main/java/baseball/computer/RandomNumberGenerator.java
@@ -2,9 +2,13 @@ package baseball.computer;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RandomNumberGenerator {
+    public static final int MAX_NUMBER_OF_CASE = 504;
+    private static final HashSet<String> numberSet = new HashSet<>();
     public static List<Integer> generateRandomNumbers() {
         List<Integer> randomNumbers = new ArrayList<>();
         while (randomNumbers.size() < 3) {
@@ -14,5 +18,31 @@ public class RandomNumberGenerator {
             }
         }
         return randomNumbers;
+    }
+
+    public static List<Integer> getRandomNumber() {
+        int numberCase = 0;
+        List<Integer> randomNumbers = new ArrayList<>();
+        boolean generationComplete = false;
+        while ((numberCase < MAX_NUMBER_OF_CASE) && !generationComplete) {
+            randomNumbers = generateRandomNumbers();
+            generationComplete = checkDuplicateNumbers(randomNumbers);
+            numberCase++;
+        }
+        return randomNumbers;
+    }
+
+    private static boolean checkDuplicateNumbers(List<Integer> randomNumbers) {
+        boolean noDuplicatedNumbers = false;
+        String generatedNumbers = randomNumbers.stream().map(String::valueOf).collect(Collectors.joining());
+        if (!numberSet.contains(generatedNumbers)) {
+            numberSet.add(generatedNumbers);
+            noDuplicatedNumbers = true;
+        }
+        return noDuplicatedNumbers;
+    }
+
+    public static void main(String[] args) {
+        checkDuplicateNumbers(List.of(1,2,3));
     }
 }
