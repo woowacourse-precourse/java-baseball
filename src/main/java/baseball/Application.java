ac2f6b818c83ab9eea7d93412df20d8919730a74
Yujin830 <rladbwls30@naver.com>
2022-11-08 01:48:26 +0900

    Feat: 랜덤 3자리 자연수 정하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,19 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-
+        List<Integer> computer = new ArrayList<>();
+        while(computer.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
         // TODO: 프로그램 구현
     }
 }
