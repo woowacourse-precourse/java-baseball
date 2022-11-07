63f03e9ef7cc63f434ecbcb9942aa40a4cdba4eb
hingjae <ljh23270@naver.com>
2022-11-07 21:06:07 +0900

    게임 재실행

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,2 +1,44 @@
-package baseball;public class Game {
+package baseball;
+
+public class Game {
+    Computer computer;
+    User user;
+
+    public void runGame() {
+        boolean stop = false;
+        while (!stop) {
+            startGame();
+            playGame();
+            stop = playAgain();
+        }
+    }
+
+    public void startGame() {
+        computer = new Computer();
+        user = new User();
+        computer.startMsg();
+    }
+
+    public void playGame() {
+        boolean win = false;
+        while (true) {
+            computer.inputMsg();
+            user.inputNum();
+            win = computer.gameLogic(computer.getRandomNumList(), user.getUserNumList());
+            if (win) {
+                break;
+            }
+        }
+    }
+
+    public boolean playAgain() {
+        int again;
+        computer.printReplay();
+        again = user.inputPlayAgain();
+        if (again == 1) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
