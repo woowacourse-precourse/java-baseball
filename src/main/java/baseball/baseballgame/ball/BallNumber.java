fb7d54a7c3805a96b0163a7c230bcb8acc514548
Albert <guang7226961@naver.com>
2022-11-08 07:36:57 +0900

    refactor($ball) 에러문자 상수로 수정

--- src/main/java/baseball/baseballgame/ball/BallNumber.java
+++ src/main/java/baseball/baseballgame/ball/BallNumber.java
@@ -5,6 +5,7 @@ import java.util.Objects;
 public class BallNumber {
     public static final int MIN_RANGE = 1;
     public static final int MAX_RANGE = 9;
+    public static final String BALL_NUMBER_ERROR = "볼넘버는 " + MIN_RANGE + "이상" + MAX_RANGE + "이하의 숫자여야 합니다.";
     private final int ballNumber;
 
     public BallNumber(int number) {
@@ -14,7 +15,7 @@ public class BallNumber {
 
     private void validateRange(int number) {
         if (number < MIN_RANGE || number > MAX_RANGE) {
-            throw new IllegalArgumentException("볼넘버는 " + MIN_RANGE + "이상" + MAX_RANGE + "이하의 숫자여야 합니다.");
+            throw new IllegalArgumentException(BALL_NUMBER_ERROR);
         }
     }
 
--- src/test/java/baseball/baseballgame/ball/BallNumberTest.java
+++ src/test/java/baseball/baseballgame/ball/BallNumberTest.java
@@ -1,7 +1,6 @@
 package baseball.baseballgame.ball;
 
-import static baseball.baseballgame.ball.BallNumber.MAX_RANGE;
-import static baseball.baseballgame.ball.BallNumber.MIN_RANGE;
+import static baseball.baseballgame.ball.BallNumber.BALL_NUMBER_ERROR;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -13,9 +12,9 @@ public class BallNumberTest {
     @Test
     void ball_number_range() {
         assertThatThrownBy(() -> BallNumber.createBallNumber(10)).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("볼넘버는 " + MIN_RANGE + "이상" + MAX_RANGE + "이하의 숫자여야 합니다.");
+                .hasMessage(BALL_NUMBER_ERROR);
         assertThatThrownBy(() -> BallNumber.createBallNumber(0)).isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("볼넘버는 " + MIN_RANGE + "이상" + MAX_RANGE + "이하의 숫자여야 합니다.");
+                .hasMessage(BALL_NUMBER_ERROR);
 
         assertThatCode(() -> BallNumber.createBallNumber(9)).doesNotThrowAnyException();
     }
@@ -24,7 +23,6 @@ public class BallNumberTest {
     void equals() {
         BallNumber ballNumber = new BallNumber(3);
         assertThat(new BallNumber(3)).isEqualTo(ballNumber);
-
         assertThat(new BallNumber(4)).isNotEqualTo(ballNumber);
     }
 }
