b2f49815ad8f711937f678fc039775612232d06b
paran <paran2586@naver.com>
2022-11-07 21:37:52 +0900

    다시할지 선택하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -80,4 +80,19 @@ public class Application {
     	
     	return message;
     }
+    
+    // 다시할지 선택하기
+    public static Boolean againGame() {
+    	Boolean again = true;
+    	System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    	System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    	Scanner sc = new Scanner(System.in);
+    	int againNum = sc.nextInt();
+    	
+    	if(againNum == 2) {
+    		again = false;
+    	}
+    	return again;
+    	
+    }
 }
