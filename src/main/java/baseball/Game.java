062454a20ce2c7eb53b69eb8e56297533a0e4192
subin Park <psb0320@hs.ac.kr>
2022-11-08 02:38:49 +0900

    feat: 게임 결과 문자열 변환 기능
    - 볼, 스트라이크 둘 다 0일 시 '낫싱' 반환
    - 볼 1 이상일 시 'n볼' StringBuilder에 추가
    - 스트라이크 1 이상일 시 'n스트라이크' StringBuilder에 추가
    - 스트라이크가 3일 시 게임 종료 문구 StringBuilder에 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -5,6 +5,11 @@ import java.util.Objects;
 
 public class Game {
     private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
+    private static final String WIN_SENTENCE = "\n3개의 숫자를 모두 맞히셨습니다! 게임 종료" +
+                                               "\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String BALL_WORD = "볼 ";
+    private static final String STRIKE_WORD = "스트라이크";
+    private static final String NOTHING_WORD = "낫싱";
 
     private boolean gameContinue = true;
 
@@ -49,5 +54,23 @@ public class Game {
                 ball++;
             }
         }
+    }
+
+    private String getResultString() {
+        if (ball == 0 && strike == 0) {
+            return NOTHING_WORD;
+        }
+
+        StringBuilder result = new StringBuilder();
+        if (ball > 0) {
+            result.append(ball).append(BALL_WORD);
+        }
+        if (strike > 0) {
+            result.append(strike).append(STRIKE_WORD);
+        }
+        if (strike == 3) {
+            result.append(WIN_SENTENCE);
+        }
+        return result.toString();
     }
 }
