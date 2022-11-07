45bfbbea56d07dde42f719f03600127817ee98c0
gudals <kimtuti@naver.com>
2022-11-07 17:14:41 +0900

    feat: add run and shutdown features
    gameBreak(), getBreakConditional(int input) 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static Boolean gameBreak(){
+        int input = Integer.parseInt(Console.readLine());
+        return getBreakConditional(input);
+    }
+
+    private static Boolean getBreakConditional(int input) {
+        if(input == 2){
+            return true;
+        } else if (input == 1) {
+            return false;
+        }else {
+            throw new IllegalArgumentException();
+        }
     }
 }
