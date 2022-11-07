7470c96496a92ab448219cc3a10d795a5a4aaa27
Woojin Lee <dhkep03@gmail.com>
2022-11-07 14:29:50 +0900

    feat(Turn): 사용자가 타겟 숫자를 맞힐 때까지 턴 반복

--- src/main/java/baseball/controller/Turn.java
+++ src/main/java/baseball/controller/Turn.java
@@ -1,4 +1,21 @@
 package baseball.controller;
 
+import baseball.type.ResultTurn;
+
 public class Turn {
+    static ResultTurn resultTurn;
+
+    static void repeatToEnd() {
+        while(checkCanRepeat()) {
+            playOneTurn();
+        }
+    }
+
+    static void playOneTurn() {
+
+    }
+
+    static boolean checkCanRepeat() {
+        return resultTurn != ResultTurn.Out;
+    }
 }
