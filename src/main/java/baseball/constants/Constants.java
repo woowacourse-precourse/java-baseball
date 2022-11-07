96478537b9555389480d55cc24d3f1816f9ddcd9
Mun Kyoung-min <mun9659@naver.com>
2022-11-07 18:32:28 +0900

    [Feat] add constants

---
+++ src/main/java/baseball/constants/Constants.java
@@ -0,0 +1,16 @@
+package baseball.constants;
+
+public class Constants {
+	
+	public static final int NUMBER_MAX_LENGTH = 3;
+	public static final int MIN_RANGE = 1;
+	public static final int MAX_RANGE = 9;
+	
+	public static final String inputNumberMessage = "숫자를 입력해 주세요: ";
+	public static final String startMessage = "숫자 야구 게임을 시작합니다.";
+	public static final String correctMessage = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	
+	public static final String nothingMessage = "낫싱";
+	public static final String strikeMessage = "스트라이크";
+	public static final String ballMessage = "볼";
+}
