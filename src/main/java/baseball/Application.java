b5af2652022990f25cde018ad877613155b89234
jhsseonn <hosu0125@gmail.com>
2022-11-07 14:41:19 +0900

    feat: 스트라이크, 볼, 낫싱 판정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,11 +6,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        int strike=0;
+        int matchCount=0;
 
         //랜덤한 숫자 3개 뽑기
 
@@ -26,6 +28,24 @@ public class Application {
         String[] numberToArray = readNumber.split("");
         List<String> userNumber = new ArrayList<String>(Arrays.asList(numberToArray));
 
+        //스트라이크, 볼, 낫싱 판정
+        for(String number: userNumber) {
+            if(computerNumber.contains(number)) {
+                if(computerNumber.indexOf(number)==userNumber.indexOf(number)) {
+                    strike+=1;
+                }
+                matchCount+=1;
+            }
+        }
+
+        if (matchCount==0) {
+            System.out.println("낫싱");
+        } else {
+            int ball = matchCount - strike;
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
+
+        
 
 
     }
