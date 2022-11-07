385ee6b4e9a1a81f80e0430b8adee2b984cd99f6
yeeun <dpdms9621@naver.com>
2022-11-08 06:02:32 +0900

    test(ApplicationTest): 각종 test 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -28,6 +28,41 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void exception_test1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("012"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void exception_test2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("131"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void exception_test3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void runTest1() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("456", "124", "789", "321", "123", "2");
+                    assertThat(output()).contains("낫싱", "2스트라이크", "낫싱", "2볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 2, 3
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
