a83ca658deba7eca323c5f0a51270e1d9f58d4fe
Qulip <ilkwon.ryu@gmail.com>
2022-11-08 04:51:44 +0900

    test: BaseballGameInputValidatorTest 클래스 생성 및 입력 유효성 확인 테스트 추가

---
+++ src/test/java/baseball/view/BaseballGameInputValidatorTest.java
@@ -0,0 +1,62 @@
+package baseball.view;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("입력 유효성 확인")
+class BaseballGameInputValidatorTest {
+    @DisplayName("길이가 3이 아닌 경우")
+    @Test
+    public void inputLengthError() {
+        // given
+        String input = "12";
+
+        // when
+        final boolean result = BaseballGameInputValidator.validate(input);
+
+        // then
+        assertFalse(result);
+    }
+
+    @DisplayName("같은 값이 있는 경우")
+    @Test
+    public void sameNumberError() {
+        // given
+        String input = "112";
+
+        // when
+        final boolean result = BaseballGameInputValidator.validate(input);
+
+        // then
+        assertFalse(result);
+    }
+
+    @DisplayName("숫자가 아닌 값이 있는 경우")
+    @Test
+    public void notNumberError() {
+        // given
+        String input = "1a2";
+
+        // when
+        final boolean result = BaseballGameInputValidator.validate(input);
+
+        // then
+        assertFalse(result);
+    }
+
+    @DisplayName("올바른 값인 경우")
+    @Test
+    public void success() {
+        // given
+        String input = "123";
+
+        // when
+        final boolean result = BaseballGameInputValidator.validate(input);
+
+        // then
+        assertTrue(result);
+    }
+}
