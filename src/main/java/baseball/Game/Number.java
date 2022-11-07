898fb69841b6cbb8b855560871dce8e1305161aa
gilyeon00 <12venus08@naver.com>
2022-11-08 06:18:53 +0900

    feat: convert 랜덤 숫자 int to int[]

--- src/main/java/baseball/Game/Number.java
+++ src/main/java/baseball/Game/Number.java
@@ -2,18 +2,20 @@ package baseball.Game;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+
 public class Number {
     public Number() {
 
     }
-    public int getRandomNum(final int len, final int MIN_NUM, final int MAX_NUM) {
-        int num = 0;
+    public int[] getRandomNum(final int len, final int MIN_NUM, final int MAX_NUM) {
+        int [] arrNum = new int[2];
 
         for (int i = 0; i < len; i++){
-            num *= 10;
-            num += Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
+            arrNum[i] = Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
         }
-        return num;
+
+        return arrNum;
     }
 
 }
