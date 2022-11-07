549ef846526b27f4c8f6596d8c5bde64b28d7af8
jsc1014 <wotlr5209@gmail.com>
2022-11-07 21:42:33 +0900

    test : 볼,스트라이크 개수 세기 유저의 입력에 따라 볼,스트라이크 개수가 달라진다.

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,6 +3,10 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,6 +40,18 @@ class ApplicationTest extends NsTest {
         assertThatThrownBy(() -> app.exceptionCheck("abc"))
                 .isInstanceOf(IllegalArgumentException.class);
 
+    }
+
+    @Test
+    void 볼_스트라이크_개수세기(){
+
+        final Application app = new Application();
+        List <Integer> answer = Arrays.asList(1,3,5);
+        List <Integer> firstUser = Arrays.asList(1,2,3);
+        List <Integer> secondUser = Arrays.asList(1,3,4);
+
+        assertThat(app.getBallAndStrike(answer,firstUser).equals("1볼 1스트라이크"));
+        assertThat(app.getBallAndStrike(answer,secondUser).equals("2스트라이크"));
     }
 
     @Override
