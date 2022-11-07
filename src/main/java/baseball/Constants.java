b91db83f4f66b87f62653d1278d78597857b3d53
TaemHam <tk4003950@gmail.com>
2022-11-07 16:46:09 +0900

    Fix: Constants-Message 테스트시 출력 부재 문제 해결

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringJoiner;
-import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -47,22 +46,16 @@ public class Constants {
         ASK("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
         END("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
         INIT("숫자 야구 게임을 시작합니다."),
-        INPUT("숫자를 입력해주세요 : ", System.out::print);
+        INPUT("숫자를 입력해주세요 : ");
 
         private final String message;
-        private final Consumer<String> printer;
 
         Messages(String message) {
             this.message = message;
-            this.printer = System.out::println;
-        }
-        Messages(String message, Consumer<String> printer) {
-            this.message = message;
-            this.printer = printer;
         }
 
         public void printMessage() {
-            this.printer.accept(this.message);
+            System.out.println(this.message);
         }
 
         public static void printScore(HashMap<Result, Integer> resultCount) {
