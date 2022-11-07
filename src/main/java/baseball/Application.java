b4006ece330367d775c2382eaec1cc7852ad4fea
sypark <soun997@gmail.com>
2022-11-08 06:19:27 +0900

    feat: project design
    Define functions and write TODO

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,49 @@
 package baseball;
 
+import java.util.Collections;
+import java.util.List;
+
 public class Application {
+
+    /*
+     * 컴퓨터가 세자리의 랜덤 숫자를 선택, 각 자리값을 리스트에 담아 반환
+     */
+    public List<Integer> computersChoice() {
+        // TODO: 컴퓨터가 세자리의 랜덤 숫자를 선택, 각 자리값을 리스트에 담아 반환
+        return Collections.emptyList();
+    }
+
+    /*
+     * 사용자가 세자리의 숫자를 입력, 각 자리값을 리스트에 담아 반환
+     */
+    public List<Integer> usersChoice(){
+        // TODO: 사용자가 세자리의 숫자를 입력, 각 자리값을 리스트에 담아 반환
+        return Collections.emptyList();
+    }
+
+    /*
+     * 사용자 입력 예외를 처리함
+     */
+    public void inputExceptionHandler(String input){
+        // TODO: 사용자 입력 예외를 처리함
+    }
+
+    /*
+     * 컴퓨터가 고른 숫자와 사용자가 입력한 숫자를 비교하여 결과를 반환
+     */
+    public String getGameResult(List<Integer> computer, List<Integer> user){
+        // TODO: 컴퓨터가 고른 숫자와 사용자가 입력한 숫자를 비교하여 결과를 반환
+        return "";
+    }
+
+    /*
+     * 게임 종료 후 숫자 야구 게임 재시작 여부 반환
+     */
+    public boolean restartGame(){
+        // TODO: 게임 종료 후 숫자 야구 게임 재시작 여부 반환
+        return false;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
