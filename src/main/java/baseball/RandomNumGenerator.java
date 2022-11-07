1638dd05aec13d2c93a35c140e0b3fa5a5a4a3a9
gayeonP <pagaye@naver.com>
2022-11-07 20:45:25 +0900

    main() 에서 실행할 class 작성

---
+++ src/main/java/baseball/RandomNumGenerator.java
@@ -0,0 +1,9 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumGenerator {
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 9;
+
+}
