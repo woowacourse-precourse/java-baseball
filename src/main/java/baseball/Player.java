6bbba2c6d77869c37d8ed366bcc1de40e1db618b
LeeHaiLim <hailim1997@gmail.com>
2022-11-08 03:56:48 +0900

    feat(Player) : implement method get number input
    사용자로부터 숫자 입력을 받는 기능 구현
    - import camp.nextstep.edu.missionutils.Console
    - 입력 메시지 NUMBER_INPUT_MESSAGE 상수 선언
    - getNumberInput() 메소드 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,4 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Player {
+    private static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public String getNumberInput() {
+        System.out.print(NUMBER_INPUT_MESSAGE);
+        String input = Console.readLine();
+
+        return input;
+    }
 }
