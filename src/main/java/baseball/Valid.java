17c3621394ce130763b05b0028625a7e80e53835
young2866 <62212610+young2866@users.noreply.github.com>
2022-11-08 04:57:58 +0900

    feat(Valid): 유저의 입력 값이 1~9 사이의 숫자인지 검증하는 기능 구현

--- src/main/java/baseball/Valid.java
+++ src/main/java/baseball/Valid.java
@@ -1,22 +1,33 @@
 package baseball;
 
 public class Valid {
+
 	private static final int DIGIT_LENGTH = 3;
 	private static final String EXCEPTION_MESSAGE = "값을 잘못 입력했습니다. 게임을 종료합니다.";
 
 	public static String validInputNumber(String inputNumber) {
-		if (validAnswerLength(inputNumber)) {
+		if (isWrongLength(inputNumber) && isNaturalNumber(inputNumber)) {
 			return inputNumber;
 		}
 
 		throw new IllegalArgumentException(EXCEPTION_MESSAGE);
 	}
 
-	public static boolean validAnswerLength(String input) {
-		if (input.length() == DIGIT_LENGTH) {
+	private static boolean isWrongLength(String inputNumber) {
+		if (inputNumber.length() == DIGIT_LENGTH) {
 			return true;
 		}
 		return false;
+	}
+
+	private static boolean isNaturalNumber(String inputNumber) {
+		for (int index = 0; index < inputNumber.length(); index++) {
+			int digit = inputNumber.charAt(index) - '0';
+			if (!(digit > 0 && digit < 10)) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
