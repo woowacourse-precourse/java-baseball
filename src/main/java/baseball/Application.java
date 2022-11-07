80829bbb63b3392bc28f520b742571e1dec6f355
dev-green-flamingo <devgflamingo@gmail.com>
2022-11-08 00:53:55 +0900

    feat(input): baseball 입력
    정규식을 이용하여 공백없이 숫자3개가 주어지는 입력만 허용한다.
    baseBall값을 만들면서 contains메소드로 현재
    삽입하려는 값의 존재 여부를 판별하여 중복 체크를 수행한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,6 +10,21 @@ public class Application {
 
     }
 
+    public static List<Integer> convertBaseBallByLine(String line) {
+        if (!line.matches("^\\d{3}$")) {
+            throw new IllegalArgumentException();
+        }
+        List<Integer> list = new ArrayList<>();
+        for (String digit : line.split("")) {
+            int value = Integer.parseInt(digit);
+            if (list.contains(value)) {
+                throw new IllegalArgumentException();
+            }
+            list.add(value);
+        }
+        return list;
+    }
+
     public static List<Integer> createBaseBall() {
         List<Integer> baseBallNumber = new ArrayList<>();
         while (baseBallNumber.size() < 3) {
