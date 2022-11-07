5a78d7d2d1ac18344d35d4c2787c711a7e88e38a
jjjGi <jiw720@naver.com>
2022-11-08 01:31:53 +0900

    result game baseball

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,14 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameStart gameStart = new GameStart();
+        boolean gameNowLooping = true;
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (gameNowLooping) {
+            gameNowLooping = gameStart.gameStart();
+        }
+
     }
 }
