3ae651b21263ab675104a21758db9ac549e91ec2
ensk26 <tjensk26@gmail.com>
2022-11-08 02:16:29 +0900

    style(Application): Java 코드 컨벤션에 맞춰 한 줄로 수정
    main 매서드의 출력하는 코드 수정 to Application
    - 구글 Java 코드 컨벤션을 참고해 한 줄 작성으로 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -35,8 +35,7 @@ public class Application {
             if (!isThreeStrike(gameResult[0])) {
                 continue;
             }
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-                    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
             commandNumber = Console.readLine();
 
