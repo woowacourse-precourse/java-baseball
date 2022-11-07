cdb32e3c3e32e5ee4e33696f7a103ce7d5dd10fb
Woogie95 <choisw950928@gmail.com>
2022-11-07 19:20:25 +0900

    refactor: 범위 검증 기능 추가, 메소드 위치 수정

--- src/main/java/baseball/UserNumber.java
+++ src/main/java/baseball/UserNumber.java
@@ -4,35 +4,19 @@ import java.util.*;
 
 public class UserNumber {
 
-    private static final int NUMBER_MIN_VALUE = 1;
-    private static final int NUMBER_MAX_VALUE = 9;
     private static final int INITIAL_REPEAT_VALUE = 0;
     private static final int DEFAULT_LENGTH_VALUE = 3;
+    private static final String STRING_INITIAL_VALUE = "0";
 
     private final List<Integer> userNumbers;
 
-    public UserNumber(List<Integer> userNumbers) {
-        this.userNumbers = userNumbers;
+    public UserNumber(String inputNumber) {
+        this.userNumbers = generateUserNumber(inputNumber);
     }
 
-    public void generateUserNumber(String inputNumber) {
+    private List<Integer> generateUserNumber(String inputNumber) {
         validate(inputNumber);
-        changeNumberType(divideNumber(inputNumber));
-    }
-
-    private List<String> divideNumber(String inputNumber) {
-        List<String> tempNumbers = new ArrayList<>();
-        for (int i = INITIAL_REPEAT_VALUE; i < inputNumber.length(); i++) {
-            tempNumbers.add(String.valueOf(inputNumber.charAt(i)));
-        }
-        return tempNumbers;
-    }
-
-    private void changeNumberType(List<String> inputNumbers) {
-        for (String inputNumber : inputNumbers) {
-            validateType(inputNumber);
-            userNumbers.add(Integer.valueOf(inputNumber));
-        }
+        return modifyNumberType(divideNumber(inputNumber));
     }
 
     private void validate(String inputNumber) {
@@ -45,7 +29,7 @@ public class UserNumber {
             throw new IllegalArgumentException("입력해야 할 숫자가 3개보다 작습니다.");
         }
         if (number > DEFAULT_LENGTH_VALUE) {
-            throw new IllegalArgumentException("입력해야 할 숫자가 3개보다 큽니다.");
+            throw new IllegalArgumentException("입력해야 할 숫자가 3개보다 많습니다.");
         }
     }
 
@@ -55,9 +39,41 @@ public class UserNumber {
         }
     }
 
-    private void validateType(String number) {
-        if (Integer.parseInt(number) / NUMBER_MIN_VALUE < INITIAL_REPEAT_VALUE
-                || Integer.parseInt(number) / NUMBER_MIN_VALUE > NUMBER_MAX_VALUE) {
+    private List<String> divideNumber(String inputNumber) {
+        Set<String> tempNumbers = new LinkedHashSet<>();
+        for (int i = INITIAL_REPEAT_VALUE; i < inputNumber.length(); i++) {
+            tempNumbers.add(String.valueOf(inputNumber.charAt(i)));
+        }
+        validateOverlap(tempNumbers);
+        validateNumberRange(tempNumbers);
+        return new ArrayList<>(tempNumbers);
+    }
+
+    private void validateOverlap(Set<String> inputNumber) {
+        if (inputNumber.size() != DEFAULT_LENGTH_VALUE) {
+            throw new IllegalArgumentException("중복 숫자가 포함되어있습니다.");
+        }
+    }
+
+    private void validateNumberRange(Set<String> tempNumbers) {
+        for (String number : tempNumbers)
+            if (number.equals(STRING_INITIAL_VALUE)) {
+                throw new IllegalArgumentException("입력한 숫자가 1~9 범위를 벗어났습니다.");
+            }
+    }
+
+    private List<Integer> modifyNumberType(List<String> inputNumbers) {
+        List<Integer> userNumbers = new ArrayList<>();
+        for (String inputNumber : inputNumbers) {
+            userNumbers.add(validateType(inputNumber));
+        }
+        return userNumbers;
+    }
+
+    private int validateType(String number) {
+        try {
+            return Integer.parseInt(number);
+        } catch (IllegalArgumentException exceptionMessage) {
             throw new IllegalArgumentException("숫자가 아닙니다.");
         }
     }
