32eec8c3216eeb8fec06d0ab622f18018d97f243
ParkJunYeong1205 <junpreme1205@gmail.com>
2022-11-07 20:50:49 +0900

    feat: 사용자 입력의 유효성을 검사하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,10 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
+    static final int USER_GUESS_NUMBER_LENGTH = 3;
     static final int RANDOM_NUMBER_LENGTH = 3;
     static final int RANDOM_NUMBER_FIRST_RANGE = 1;
     static final int RANDOM_NUMBER_LAST_RANGE = 9;
-
+    static final String REGEX_PATTERN = "[+-]?\\d*(\\.\\d+)?";
     static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     static final String REQUEST_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
 
@@ -33,6 +34,16 @@ public class Application {
         System.out.print(REQUEST_INPUT_MESSAGE);
         String userGuessNumber = Console.readLine();
         return userGuessNumber;
+    }
+
+    static public void userInputValidator(String userGuessNumber) {
+        if (userGuessNumber.length() != USER_GUESS_NUMBER_LENGTH) {
+            throw new IllegalArgumentException();
+        }
+
+        if (!userGuessNumber.matches(REGEX_PATTERN)) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public static void main(String[] args) {
