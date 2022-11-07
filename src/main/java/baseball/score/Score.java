0b31f144a13a5d251173c1e502c248dd2a6ecf43
kimdoha <kkhhjj3393@gmail.com>
2022-11-07 12:03:55 +0900

    feat(Score): 스코어 계산 클래스 생성

---
+++ src/main/java/baseball/score/Score.java
@@ -0,0 +1,14 @@
+package baseball.score;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class Score {
+  private Integer strikeCount;
+  private Integer ballCount;
+
+  private Score(){
+    this.strikeCount = 0;
+    this.ballCount = 0;
+  }
+}
