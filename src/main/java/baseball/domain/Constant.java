19ab8d20afbbf1da7673ba55a18e4d5e38f5198c
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-07 18:44:09 +0900

    feat : class에서 적용시킬 상수 추가

---
+++ src/main/java/baseball/domain/Constant.java
@@ -0,0 +1,35 @@
+package baseball.domain;
+
+public class Constant {
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final int MAX_NUMBER_SIZE = 3;
+    public static final String COMPUTER_GAME_RESTART = "1";
+    public static final String COMPUTER_GAME_EXIT = "2";
+
+    public static final String COMPUTER_OPTION = "[1-2]+";
+
+    public static final String USER_NUMBER_OPTION = "[1-9]+";
+
+    public static final int USER_MIN_NUMBER = 1;
+
+    public static final int USER_MAX_NUMBER = 9;
+
+    public static final String GAME_END_MESSAGE = "게임 종료";
+
+    public static final String MISSION_COMPLETE_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static final String BALL = "볼 ";
+
+    public static final String STRIKE = "스트라이크 ";
+
+    public static final String NOTHING = "낫싱";
+
+    public static final int STRIKE_DEFAULT = 0;
+
+    public static final int BALL_DEFAULT = 0;
+
+    public static final int THREE_STRIKE_NUMBER = 3;
+
+
+}
