2234e731c6436d0cf4cd28db0e46812ebf0385b0
BaeDongHyeon <sasd730@gmail.com>
2022-11-07 13:57:16 +0900

    feat: 사용자 입력 추가

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,10 +1,16 @@
 package baseball.game;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
     private Game() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
     public static void start() {
         Game game = new Game();
+    }
+
+    private String getInputUserNumber() {
+        return Console.readLine();
     }
 }
