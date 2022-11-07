71a7f92bef8ca49a37468b8125eb6f26278f7bf5
kyo705 <mission0705@naver.com>
2022-11-07 21:07:30 +0900

    [ADD] 비지니스 로직
    기존 기능 구현 클래스들을 활용해 비지니스 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,45 @@
 package baseball;
 
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	RandomNumberFactory randomNumberFactory = new RandomNumberFactory();
+    	InputValidation inputValidation = new InputValidation();
+    	Distinguisher inputDistinguisher = new Distinguisher();
+        
+    	List<Integer> randomNumber = randomNumberFactory.createRamdomNumbers(1, 9, 3);
+    	System.out.println("숫자 야구 게임을 시작합니다.");
+    	
+    	while(true) {
+    		System.out.println("숫자를 입력해주세요 : ");
+        	String input = Console.readLine();
+        	
+        	if(!inputValidation.isValidRandomNumbers(input)) {
+        		throw new IllegalArgumentException("잘못된 입력 값입니다.");
+        	}
+        	
+        	String output = inputDistinguisher.distinguish(randomNumber, input);
+        	System.out.println(output);
+        	
+        	if(!output.equals("3스트라이크")) {
+        		continue;
+        	}
+        	
+    		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    		input = Console.readLine();
+    		
+    		if(!inputValidation.isValidReStart(input)) {
+        		throw new IllegalArgumentException("잘못된 입력 값입니다.");
+        	}
+    		if(input.equals("2")) {
+    			break;
+    		}
+    		randomNumber = randomNumberFactory.createRamdomNumbers(1, 9, 3);
+    	}
     }
     
 }
