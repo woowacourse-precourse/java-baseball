9a8e052c597db974a1219c730edeba6afc37f6bc
kariskan <zkfzpf56@naver.com>
2022-11-07 16:03:49 +0900

    feat: 필요한 상수 정의

--- src/main/java/baseball/status/Constants.java
+++ src/main/java/baseball/status/Constants.java
@@ -4,7 +4,14 @@ public enum Constants {
 
     NUMBER_OF_DIGITS(3),
     MIN_RANGE_OF_DIGITS(1),
-    MAX_RANGE_OF_DIGITS(9);
+    MAX_RANGE_OF_DIGITS(9),
+    FIRST_CHARACTER_OF_DIGITS('0'),
+
+    ZERO_COUNT(0),
+    COUNT(1),
+
+    GAME_CONTINUE(1),
+    GAME_STOP(2);
 
     private final int number;
 
