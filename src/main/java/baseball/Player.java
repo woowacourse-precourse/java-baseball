b13e4ff522372a3de01d19e4a03f503162864be7
dain-lee <dain9934@gmail.com>
2022-11-07 21:34:37 +0900

    feat(player): 입력된 수 검증 기능 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -5,11 +5,13 @@ import camp.nextstep.edu.missionutils.Console;
 public class Player {
     public String getPlayerInputNumbersString() {
         String input = Console.readLine();
+        Validator.validatePlayerInputNumbersString(input);
         return input;
     }
 
     public String getPlayerResponseAfterGame() {
         String response = Console.readLine();
+        Validator.validateResponse(response);
         return response;
     }
 }
