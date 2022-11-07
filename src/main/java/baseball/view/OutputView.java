494c0a66bb569a36dbb42630b367345aa3cee629
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-07 19:15:03 +0900

    feat : printExitMessage 메서드 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -16,14 +16,18 @@ public class OutputView {
         if (ballCount != 0 && strikeCount != 0) {
             System.out.println(ballCount + Constant.BALL + strikeCount + Constant.STRIKE);
         }
+    }
 
         public static void printMissionCompleteMessage() {
             System.out.println(Constant.MISSION_COMPLETE_MESSAGE);
         }
 
-
-    }
-
+        public static void printExitMessage() {
+            System.out.println(Constant.GAME_END_MESSAGE);
+        }
 
 }
+
+
+
 
