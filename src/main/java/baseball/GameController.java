2006f0678c9008b113548fa3c8c82c348982a285
nahyeon cho <whskgus12@nate.com>
2022-11-07 19:58:13 +0900

    feat : 게임 시작 문구는 한번만 출력하기
    전역변수 gameCount를 이용하여 게임 시작 문구를 재게임의 경우엔 출력 하지않습니다.

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -3,17 +3,18 @@ package baseball;
 import java.util.ArrayList;
 
 public class GameController {
+    static int gameCount = 0; // "숫자 야구 게임을 시작합니다"는 한번만 출력
     MakeThreeDigitNumber makeThreeDigitNumber = new MakeThreeDigitNumber();
 
     ArrayList<String> computerSelected = makeThreeDigitNumber.computerSelectedNumber();
     ArrayList<String> userSelected;
 
     public void startGame() {
-
+        if (gameCount == 0) System.out.println("숫자 야구 게임을 시작합니다.");
         do {
             userSelected = makeThreeDigitNumber.userSelectedNumber();
             PlayerInputValueExceptions.allInputValueException(userSelected);
-            
+
         } while (GameHintList.countOfStrike() != 3);
 
     }
