ff6ae29d702fa79269195ede0ff94072a686b7e7
GeonTae Park <rssr96@naver.com>
2022-11-07 19:31:02 +0900

    [feat]: 입력한 수에 대한 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,13 +18,43 @@ public class Application {
                 computer.add(randomNumber);
             }
         }
-        threeStrike = 0;
         while (threeStrike == 0) {
             System.out.print("숫자를 입력해 주세요 : ");
-            String user = Console.readLine();
+            String player = Console.readLine();
+            baseballGame(computer, player);
+        }
+
+    }
+
+    private static void baseballGame(List<Integer> computer, String player) {
+        int ball = 0;
+        int strike = 0;
+
+        if (computer.toString().charAt(1) == player.charAt(0)) strike++;
+        if (computer.toString().charAt(4) == player.charAt(1)) strike++;
+        if (computer.toString().charAt(7) == player.charAt(2)) strike++;
+
+        if (computer.toString().charAt(1) == player.charAt(1) || computer.toString().charAt(1) == player.charAt(2))
+            ball++;
+        if (computer.toString().charAt(4) == player.charAt(0) || computer.toString().charAt(4) == player.charAt(2))
+            ball++;
+        if (computer.toString().charAt(7) == player.charAt(0) || computer.toString().charAt(7) == player.charAt(1))
+            ball++;
+
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫씽");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+
+        if (strike == 3) {
+            threeStrike = 1;
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
     }
 }
+
+
 
 
 
