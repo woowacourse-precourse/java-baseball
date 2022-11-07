a3237b88b98c2d5748980af92c9d495f8676e039
Woong99 <dndry1072@naver.com>
2022-11-07 15:43:33 +0900

    feat : 스트라이크와 볼의 결과를 얻는 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,25 +1,46 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        List<Integer> ls = getRandomThreeNumbers();
-        System.out.println(ls);
     }
 
-    public static List<Integer> getRandomThreeNumbers(){
+    public static List<Integer> getRandomThreeNumbers() {
         List<Integer> numberList = new ArrayList<>();
-        while(numberList.size() < 3){
+        while (numberList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if(!numberList.contains(randomNumber)){
+            if (!numberList.contains(randomNumber)) {
                 numberList.add(randomNumber);
             }
         }
         return numberList;
+    }
+
+    public static boolean getResult(List<Integer> computerNumbersList, List<Integer> userNumbersList) {
+        int strikeCount = 0;
+        int ballCount = 0;
+        for (int i = 0; i < 3; i++) {
+            if (Objects.equals(computerNumbersList.get(i), userNumbersList.get(i))) {
+                strikeCount++;
+            }
+            if (!Objects.equals(computerNumbersList.get(i), userNumbersList.get(i))
+                    && computerNumbersList.contains(userNumbersList.get(i))) {
+                ballCount++;
+            }
+        }
+        if (strikeCount != 0 && ballCount != 0) {
+            System.out.println(strikeCount + "스트라이크 " + ballCount + "볼");
+        } else if (strikeCount != 0) {
+            System.out.println(strikeCount + "스트라이크");
+        } else if (ballCount != 0) {
+            System.out.println(ballCount + "볼");
+        } else {
+            System.out.println("낫싱");
+        }
+        return strikeCount == 3;
     }
 }
