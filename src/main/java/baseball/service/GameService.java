3895daf32415596ee3f25e6a5ea7a3fd20d186f1
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-08 03:15:01 +0900

    feat : 입력한 수의 유효타 체크하는 서비스 패키지의 기능 구현

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -9,6 +9,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class GameService {
     public static final int BALL_NUMBERS = 3;
     public static int strike = 0;
+    public static int ball = 0;
     public final static List<Integer> RANDOMBALL = new ArrayList<>();
     public static List<Integer> userball = new ArrayList<>();
     public static void startOrder(){
@@ -22,6 +23,8 @@ public class GameService {
             } catch (IllegalArgumentException e){   errorcheck = errorOutput();
             }
             if(errorcheck == 1) break;
+
+            findRandomBall();
         }
     }
     public static void checkInput() {
@@ -57,5 +60,41 @@ public class GameService {
         exceptionHanlder.checkInputBallSize(inputball);
         exceptionHanlder.checkSameInput(inputball);
         exceptionHanlder.nonZero(inputball);
+    }
+    public static void findRandomBall() {
+        checkStrike();
+        checkBall();
+        giveHint();
+    }
+    private static void giveHint() {
+        if(strike == 0 && ball == 0) System.out.println("낫싱");
+        if(strike == 0 && ball != 0) System.out.println(ball+"볼");
+        if(strike != 0 && ball == 0) System.out.println(strike+"스트라이크");
+        if(strike != 0 && ball != 0) System.out.println(ball+"볼 "+strike+"스트라이크");
+
+        initData();
+    }
+    private static void initData(){
+        strike = 0;
+        ball = 0;
+    }
+    public static void checkStrike(){
+        int prevstrike = strike;
+
+        for(int i = 0; i < BALL_NUMBERS; i++){
+            if(RANDOMBALL.get(i) == userball.get(i)) strike++;
+        }
+    }
+    public static void checkBall(){
+        int prevball = ball;
+
+        for(int i=0; i< BALL_NUMBERS; i++){
+            countBalls(i);
+        }
+    }
+    private static void countBalls(int index) {
+        for(int i=0; i<BALL_NUMBERS; i++){
+            if(i!=index && RANDOMBALL.get(i) == userball.get(index)) ball++;
+        }
     }
 }
