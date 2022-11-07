77a4873b939fce45a1b184032727de702fa27885
jmjmjames <chungjm0711@gmail.com>
2022-11-07 20:42:42 +0900

    feat(Application): Game Start Msg() 메서드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import static baseball.domain.BaseballGameStarter.*;
+import static baseball.global.constants.Constants.Korean.GAME_START_MESSAGE;
+
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        printGameStartMessage();
+        playBaseballGame();
+    }
+
+    public static void printGameStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
     }
 }
