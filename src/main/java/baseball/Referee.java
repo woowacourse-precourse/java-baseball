c56103bbe62fb9aef98c96a54190990179eaa0d6
June <68291395+june-777@users.noreply.github.com>
2022-11-07 21:37:13 +0900

    feat: 심판 볼(공이 아닌 판정의 볼) 개수 세기 기능

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -28,4 +28,24 @@ public class Referee {
 		}
 		return strikeNum;
 	}
+
+	public int countBallNum(List<Integer> computerBalls, List<Integer> playerBalls) {
+		int ballNum = 0;
+		for (int ballIdx = 0; ballIdx < computerBalls.size(); ballIdx++) {
+			int computerBall = computerBalls.get(ballIdx);
+			int playerBall = playerBalls.get(ballIdx);
+
+			if (!canBall(computerBalls, playerBall)) {
+				continue;
+			}
+			if (computerBall != playerBall) {
+				ballNum += 1;
+			}
+		}
+		return ballNum;
+	}
+
+	private boolean canBall(List<Integer> computerBalls, int playerBall) {
+		return computerBalls.contains(playerBall);
+	}
 }
