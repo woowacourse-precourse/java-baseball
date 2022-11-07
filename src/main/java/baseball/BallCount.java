5664180095d3d4a2bade5d4baf4b744b2362d695
Jeeseob <jeeseob5761@gmail.com>
2022-11-07 23:22:06 +0900

    Refactoring: ball count 출력시 사용하는 toString 함수에 중괄호 추가

--- src/main/java/baseball/BallCount.java
+++ src/main/java/baseball/BallCount.java
@@ -39,9 +39,13 @@ public class BallCount {
 
     @Override
     public String toString() {
-        if (ball == 0 && strike == 0) return "낫싱";
-        else if (ball == 0) return strike + "스트라이크";
-        else if (strike == 0) return ball + "볼";
+        if (ball == 0 && strike == 0) {
+            return "낫싱";
+        } else if (ball == 0) {
+            return strike + "스트라이크";
+        } else if (strike == 0) {
+            return ball + "볼";
+        }
         return ball + "볼 " + strike + "스트라이크";
     }
 }
