5d6d168413dca0e4ba78d527082ebe8a467fd0eb
Konahyeon <koo@inha.edu>
2022-11-07 14:08:39 +0900

    feat: 스트라이크 개수 구하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -20,9 +20,11 @@ public class Application {
 
     public static List<Integer> playerNumber() {
         List<Integer> player = new ArrayList<>();
+        System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
 
         while (player.size() < 3) {
+            //잘못 입력한 경우에 대한 예외처리 기능 구현해야 함
             int idx = 0;
             char inputNumber = input.charAt(idx);
             int inputNumberToInt = inputNumber - '0';
@@ -32,7 +34,29 @@ public class Application {
         return player;
     }
 
+    public static int findStrike(List<Integer> computer, List<Integer> player) {
+        int strikeNumber = 0;
+        for (int idx = 0; idx < 3; idx++) {
+            if (computer.get(idx) == player.get(idx)) {
+                strikeNumber++;
+            }
+        }
+        return strikeNumber;
+    }
+
+    public static void findBall(List<Integer> computer, List<Integer> player) {
+
+    }
+
+    public static void compare(List<Integer> computer, List<Integer> player) {
+        int strikeNumber = findStrike(computer, player);
+
+
+
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        compare(computerNumber(), playerNumber());
     }
 }
