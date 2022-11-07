153ee1d04229325639c790944db9c2e761443f6a
gayeonP <pagaye@naver.com>
2022-11-07 23:56:00 +0900

    모두 맞을 경우를 판별하는 기능 구현

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -3,28 +3,36 @@ package baseball;
 public class GameManager {
     private Computer computer = new Computer();
     private User user = new User();
+    private int strike_num = 0;
+    private int ball_num = 0;
     public void gameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
     private void game() {
+        while (!isCorrect()) {
 
+        }
     }
-    private int isStrike() {
-        int strike_num = 0;
-        for (int i = 0; i < computer.getRandomNumList().size(); i++) {
+    private void isStrike() {
+        for (int i = 0; i < user.getUserInput().size(); i++) {
             if (computer.getRandomNumList().get(i) == user.getUserInput().get(i)) {
                 strike_num++;
             }
         }
-        return strike_num;
     }
-    private int isBall() {
-        int ball_num = 0;
+    private void isBall() {
         for (int i = 0; i < computer.getRandomNumList().size(); i++) {
             if (computer.getRandomNumList().contains(user.getUserInput().get(i))) {
                 ball_num++;
             }
         }
-        return ball_num - isStrike();
+        ball_num -= strike_num;
+    }
+    private boolean isCorrect() {
+        for (int i = 0; i < user.getUserInput().size(); i++) {
+            if (computer.getRandomNumList().get(i) == user.getUserInput().get(i)) {}
+            else return false;
+        }
+        return true;
     }
 }
