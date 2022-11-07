a92dc0b156f0a9260aba2a6a2b9929a57575dbfe
YooSuYeon <usuyn000@gmail.com>
2022-11-08 02:22:08 +0900

    test: 플레이어 입력 숫자 안 중복 검사 테스트

--- src/test/java/inputoutput/ValidationTest.java
+++ src/test/java/inputoutput/ValidationTest.java
@@ -14,15 +14,21 @@ public class ValidationTest {
     }
 
     @Test
-    void 플레이어_입력_숫자_구성_예외_발생() {
-        assertThatThrownBy(() -> validator.checkPlayerNumber("1f5"))
-                .isInstanceOf(IllegalArgumentException.class);
+    void 플레이어_입력_숫자_구성_검사() {
+        boolean validateValue = validator.checkPlayerNumber("1f4");
+        assertThat(validateValue).isEqualTo(false);
     }
 
     @Test
-    void 플레이어_입력_숫자_길이_예외_발생() {
-        assertThatThrownBy(() -> validator.checkPlayerNumber("1234"))
-                .isInstanceOf(IllegalArgumentException.class);
+    void 플레이어_입력_숫자_길이_검사() {
+        boolean validateValue = validator.checkPlayerNumber("1234");
+        assertThat(validateValue).isEqualTo(false);
+    }
+
+    @Test
+    void 플레이어_입력_숫자_중복_검사() {
+        boolean validateValue = validator.checkPlayerNumber("252");
+        assertThat(validateValue).isEqualTo(false);
     }
 
 }
