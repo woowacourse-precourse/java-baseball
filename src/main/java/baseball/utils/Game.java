1e4a7e7d072be77edb5221f783bc7562f88feaa0
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-11-07 13:06:58 +0900

    feature : Hint function

--- src/main/java/baseball/utils/Game.java
+++ src/main/java/baseball/utils/Game.java
@@ -1,11 +1,37 @@
 package baseball.utils;
 
 import static baseball.values.Constant.Console.*;
+import static baseball.values.Constant.Hint.*;
 
 public class Game {
     private static int strikeCnt = 0;
     private static int ballCnt = 0;
     private static boolean noting = false;
+
+
+    private static void printHint() {
+        if (noting == true) {
+            System.out.println(NOTING);
+            return;
+        }
+        System.out.println(getHint(ballCnt, strikeCnt));
+    }
+
+    private static StringBuffer getHint(int ballCnt, int strikeCnt) {
+        StringBuffer gameHint = new StringBuffer();
+        if (ballCnt > 0) {
+            gameHint.append(ballCnt);
+            gameHint.append(BALL);
+        }
+        if (gameHint.length() > 0 && strikeCnt > 0) {
+            gameHint.append(SPACE);
+        }
+        if (strikeCnt > 0) {
+            gameHint.append(strikeCnt);
+            gameHint.append(STRIKE);
+        }
+        return gameHint;
+    }
 
 
     private static void printStartText() {
