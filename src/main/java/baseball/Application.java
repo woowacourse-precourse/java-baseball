cdfc1934e0a9bb95a395cd2e593c49bde21413ca
josolha <josolha@nate.com>
2022-11-07 23:50:20 +0900

    refactor : change checkNumbers method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -34,7 +34,7 @@ public class Application {
         return userNumber;
     }
 
-    static int checkNumbers(List<Integer> computerNumber, List<Integer> userNumber) {
+    static String checkNumbers(List<Integer> computerNumber, List<Integer> userNumber) {
 
         int strike = 0;
         int total = 0;
@@ -50,16 +50,13 @@ public class Application {
         int ball = total - strike;
 
         if (total == 0) {
-            System.out.println("낫싱");
+            return "낫싱";
         } else if (strike == 0) {
-            System.out.println(ball + "볼");
+            return ball + "볼";
         } else if (ball == 0) {
-            System.out.println(strike + "스트라이크");
-        } else {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
+            return strike + "스트라이크";
         }
-
-        return strike;
+        return ball + "볼 " + strike + "스트라이크";
     }
 }
 
