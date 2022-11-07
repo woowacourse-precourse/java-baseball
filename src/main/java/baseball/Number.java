18db054f2246c93f11737ba26d8a2afa50d7541a
subin Park <psb0320@hs.ac.kr>
2022-11-08 02:03:47 +0900

    feat: 사용자 숫자 입력 기능

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ public class Number {
     private static final int LENGTH = 3;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
+    private static final String INPUT_SENTENCE = "숫자를 입력해주세요 : ";
 
     public List<Integer> getComputerNumber() {
         List<Integer> computer = new ArrayList<>();
@@ -19,5 +21,12 @@ public class Number {
             }
         }
         return computer;
+    }
+
+    private String getInputNumber() {
+        System.out.print(INPUT_SENTENCE);
+        String input = Console.readLine();
+
+        return input;
     }
 }
