0262e906373391c5dac7a78b15d9b8aa96eeb6fa
MuHyeonSon <smuhyeon84@gmail.com>
2022-11-08 03:41:28 +0900

    feat(Application) : 사용자로부터 받은 입력에 대한 예외처리 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,18 +13,20 @@ public class Application {
     public static void main(String[] args) {
 
         String numberPresentedByPlayer = ""; // User로부터 입력받을 숫자를 저장할 변수
+        List<String> threeNumber = new ArrayList<>(); // 컴퓨터가 선택한 3개의 수를 저정할 리스트
 
         while (true) {
             System.out.println("숫자 야구 게임을 시작합니다.");
-            //numberPresentedByPlayer = receivedUserInput.nextLine();
-            List<String> threeNumber = new ArrayList<>();
-            threeNumber = createThreeNumbers();
+            threeNumber = createThreeNumbers(); // 컴퓨터가 서로 다른 3개의 수를 선택
             System.out.println(threeNumber);
             numberPresentedByPlayer = receiveUserInput(); // 게임플레이어로 부터 숫자를 입력받고 변수에 저장
             System.out.println(numberPresentedByPlayer);
+            // 입력받은 데이터에 대한 예외처리
+            if (!checkValidForInputThreeNumber(numberPresentedByPlayer)) {
+                throw new IllegalArgumentException("잘못된 입력입니다.");
+            }
 
             break;
-
         }
     }
 
@@ -62,4 +64,28 @@ public class Application {
         return threeNumbersOfComputer;
     }
 
+    // 게임플레이어로부터 입력받은 3자리 숫자에 대한 예외처리 메서드
+    public static Boolean checkValidForInputThreeNumber(String input) {
+        if (!(input.length() == 3)) { // 입력받은 숫자 문자열의 길이가 3이 아니라면
+            System.out.println("잘못된 형식의 입력입니다. 입력하신 데이터는 3개의 숫자가 아닙니다.");
+            System.out.println("'456', '147'과 같은 형식으로 3자리의 숫자를 입력해주세요.");
+            return false;
+        }
+        for (int index = 0; index < input.length(); index++) {
+            if (!Character.isDigit(input.charAt(index))) {
+                System.out.println("입력하신 데이터에 숫자가 아닌 것이 존재합니다.");
+                System.out.printf("%s는 숫자가 아닙니다.\n", input.charAt(index + 1));
+                System.out.println("'456', '147'과 같은 형식으로 3자리의 숫자를 입력해주세요.");
+                return false;
+            } else if (!(1 <= input.charAt(index) && 9 >= input.charAt(index))) {
+                System.out.println("입력하신 숫자는 입력 범위를 초과합니다.");
+                System.out.println("1부터 9까지의 숫자 중 하나의 숫자들로 이루어진 3자리 숫자를 입력해주세요.");
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+//numberPresentedByPlayer = receivedUserInput.nextLine();
 }
