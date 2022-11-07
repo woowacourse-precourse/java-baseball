7d11cf9de7232a8f9a980e78244ec0ce6df0a7fe
BHC-Chicken <tij05078@gmail.com>
2022-11-07 21:49:47 +0900

    feat : 프로그램 시작점 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,14 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameStart gameStart = new GameStart();
+
+        boolean gameLoop = true;
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (gameLoop) {
+            gameLoop = gameStart.gameStart();
+        }
     }
 }
