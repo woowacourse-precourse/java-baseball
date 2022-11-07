13de4ca2b74ab2412515dd442d2842e9a62366aa
Qulip <ilkwon.ryu@gmail.com>
2022-11-08 05:31:41 +0900

    feat: BaseballGameService 볼카운트 확인 기능 추가

--- src/main/java/baseball/service/BaseballGameService.java
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -1,11 +1,32 @@
 package baseball.service;
 
 import baseball.domain.BaseballNumber;
+import baseball.view.BaseballGameInputView;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class BaseballGameService {
     private final BaseballNumber computer;
 
-    public BaseballGameService(){
+    public BaseballGameService() {
         computer = new BaseballNumber();
+    }
+
+    public List<Integer> getCounts() {
+        BaseballNumber player = BaseballGameInputView.getPlayerNumber();
+        int strike = 0;
+        int ball = 0;
+        Set<Integer> computerSet = new HashSet<>(computer.getBaseballNumber());
+
+        for (int idx = 0; idx < 3; idx++) {
+            if (player.getBaseballNumber().get(idx) == computer.getBaseballNumber().get(idx)) {
+                strike++;
+            } else if (computerSet.contains(player.getBaseballNumber().get(idx))) {
+                ball++;
+            }
+        }
+
+        return List.of(ball, strike);
     }
 }
