08f2cb270f57ac8690b0bac09591b004e6d3fef0
baksohyeon <bori12370@gmail.com>
2022-11-07 22:46:39 +0900

    feat: 입력을 String으로 받는 메서드 추가

---
+++ src/main/java/baseball/ExecuteGame.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class ExecuteGame {
+
+    // 입력한 값을 String 형태로 반환
+    private String saveInput () {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+        return input;
+    }
+}
