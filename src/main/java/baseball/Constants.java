79ed9dc5ec300399d1d992bc8ae8c993f903e0ae
LeeHaiLim <hailim1997@gmail.com>
2022-11-08 04:09:08 +0900

    feat(baseball) : implement class Constants
    상수 클래스 구현
    - 입력 숫자 갯수 NUMBER_COUNT 상수 선언
    - 입력 범위 시작 NUMBER_RANGE_START 상수 선언
    - 입력 범위 종료 NUMBER_RANGE_END 상수 선언

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,7 @@
+package baseball;
+public final class Constants {
+    public static final int NUMBER_COUNT = 3;
+    public static final int NUMBER_RANGE_START = 1;
+    public static final int NUMBER_RANGE_END = 9;
+}
+
