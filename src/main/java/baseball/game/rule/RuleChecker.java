603d40d108d80bd2aff77c8050f487ba178f9aeb
wonju-dev <wonjulee.dev@gmail.com>
2022-11-07 16:52:11 +0900

    fix: 결과 출력의 '볼', '스트라이크' 배치 순서 문제 해결
    Map에 포함된 원소들의 순서를 고정시키는 LinkedHashMap 자료구조를 사용

--- src/main/java/baseball/game/rule/RuleChecker.java
+++ src/main/java/baseball/game/rule/RuleChecker.java
@@ -2,7 +2,7 @@ package baseball.game.rule;
 
 import baseball.game.enums.Result;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,7 +12,7 @@ public class RuleChecker {
     }
 
     public static Map<Result, Integer> check(List<Rule> rules, Integer answer, Integer guess) {
-        Map<Result, Integer> guessResult = new HashMap<>();
+        Map<Result, Integer> guessResult = new LinkedHashMap<>();
 
         for (Rule rule : rules) {
             guessResult.put(rule.getResult(), rule.check(answer, guess));
