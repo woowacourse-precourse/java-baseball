92d767677430254c91668e0d9a7c66970a1bb800
nahyeon cho <whskgus12@nate.com>
2022-11-07 16:43:33 +0900

    feat : 스트라이크 개수 파악 후 반환
    사용자가 입력한 숫자와 컴퓨터의 숫자를 비교하여 스트라이크 개수를 파악하는 함수입니다. 스트라이크는 숫자의 위치와 값이 모두 일치해야합니다.

--- src/main/java/baseball/GameHintList.java
+++ src/main/java/baseball/GameHintList.java
@@ -6,6 +6,16 @@ public class GameHintList {
     static ArrayList<String> computerSelectedNumbers;
     static ArrayList<String> userSelectedNumbers;
 
+    public static int countOfStrike() {
+        int count = 0;
+        for (int digits = 0; digits < 3; digits++) {
+            if (computerSelectedNumbers.get(digits).equals(userSelectedNumbers.get(digits))) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public int countOfBall() {
         int count = 0;
         for (int digit = 0; digit < 3; digit++) {
