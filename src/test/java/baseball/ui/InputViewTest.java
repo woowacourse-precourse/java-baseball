6e13ec818980d8ec12fc665c43c976d00c4480d4
chkim <cndghks0213@gmail.com>
2022-11-08 07:57:47 +0900

    test(ui): 테스트 메서드명 변경

--- src/test/java/baseball/ui/InputViewTest.java
+++ src/test/java/baseball/ui/InputViewTest.java
@@ -11,15 +11,16 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class InputViewTest {
     @ValueSource(strings = {"123", "111", "222", "333"})
     @ParameterizedTest
-    void 각_자리가_1이상_9이하인_세_자리_숫자를_입력받는다(String input) {
+    void _1이상_9이하인_세_자리_숫자를_입력받는다(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
         assertThat(InputView.numbers()).isEqualTo(input);
     }
 
-    @ValueSource(strings = {"1", "12", "1234", "abc", "가나다", "012", "1a가", "!@#"})
+    @ValueSource(strings = {"12", "012", "1234", "abc", "가나다", "!@#"})
     @ParameterizedTest
-    void 각_자리가_1이상_9이하인_세_자리_숫자가_아닌_입력을_받으면_예외가_발생한다(String input) {
+    void _1이상_9이하인_세_자리_숫자가_아니면_예외가_발생한다(String input) {
         System.setIn(new ByteArrayInputStream(input.getBytes()));
-        assertThatThrownBy(InputView::numbers).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(InputView::numbers)
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
