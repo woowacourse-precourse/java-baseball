7bb184ce3364b37d76d942700748adc817b2a6c5
ensk26 <tjensk26@gmail.com>
2022-11-08 01:28:21 +0900

    refactor(Application): 야구 게임 입출력 결과를 보여주는 main 메서드
    main 메서드 기능 수정 to Application
    - if 문 추가해 가독성 높임

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -39,7 +39,10 @@ public class Application {
                     "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
             commandNumber = Console.readLine();
-            isValidCommandNumber(commandNumber);
+
+            if (!isValidCommandNumber(commandNumber)) {
+                continue;
+            }
             command = getNumber(commandNumber);
 
             if (isReStartGame(command)) {
