69acba7f30a4cf5e77d6616efb6c6b4ee68805fa
sunghyuki <sunghyuk1609@gmail.com>
2022-11-07 16:29:41 +0900

    feat : 정수 검증 메서드 추가

---
+++ src/main/java/baseball/Validator.java
@@ -0,0 +1,10 @@
+package baseball;
+
+public class Validator {
+
+    public static void validateInteger(char character) {
+        if(!Character.isDigit(character)) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
