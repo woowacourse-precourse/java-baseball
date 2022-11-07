c29108e675e34efc11ec6b54e24b01c2f8c76595
phj1120 <hyeonj1998@naver.com>
2022-11-07 21:06:16 +0900

    feat: 1-2 요구사항 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         List<Integer> computerNumber = Utils.pickComputerNumber();
+
+        String line = Console.readLine();
+        List<Integer> userNumber = new ArrayList<>();
+        for (int i = 0; i < line.length(); i++) {
+            userNumber.add(Character.getNumericValue(line.charAt(i)));
+        }
+
+
     }
 }
