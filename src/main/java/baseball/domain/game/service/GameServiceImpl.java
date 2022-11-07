f8c5489685211e17175ae5bc8d61f0d9310af42b
hyun6ik <fkanwm@naver.com>
2022-11-07 15:44:58 +0900

    feat(12) : 플레이어가 입력을 잘 했는지 검증한다. 기능 개발

--- src/main/java/baseball/domain/game/service/GameServiceImpl.java
+++ src/main/java/baseball/domain/game/service/GameServiceImpl.java
@@ -3,13 +3,20 @@ package baseball.domain.game.service;
 public class GameServiceImpl implements GameService{
 
     private final GameMessenger gameMessenger;
+    private final GameValidator gameValidator;
 
-    public GameServiceImpl(GameMessenger gameMessenger) {
+    public GameServiceImpl(GameMessenger gameMessenger, GameValidator gameValidator) {
         this.gameMessenger = gameMessenger;
+        this.gameValidator = gameValidator;
     }
 
     @Override
     public String requestInputNumber() {
         return gameMessenger.requestInputNumber();
+    }
+
+    @Override
+    public void validateNumber(String number) {
+        gameValidator.validateNumber(number);
     }
 }
---
+++ src/main/java/baseball/global/utils/message/ErrorMessage.java
@@ -0,0 +1,8 @@
+package baseball.global.utils.message;
+
+public interface ErrorMessage {
+
+    String IS_NOT_THREE_DIGITS = "3자리 숫자가 아닙니다.";
+    String IS_NOT_NUMBER_OR_CONTAIN_ZERO = "문자열이 포함되어 있거나 0이 포함되어 있습니다.";
+    String CONTAIN_DUPLICATE_NUMBER = "중복되는 숫자가 있습니다.";
+}
