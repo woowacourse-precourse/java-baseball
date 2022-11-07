b7e53fb544786058e5ea549d29b6b5a4882be551
glaehfdl1654 <glaehfdl1654@naver.com>
2022-11-08 02:25:06 +0900

    feat(Application class): 인자로 받은 값을 확인하고 재시작할지 종료할지 알려주는 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+    private static boolean canRestart(String usersInput) {
+        if (usersInput.equals("1")) {
+            return true;
+        } else if (usersInput.equals("2")) {
+            return false;
+        } else {
+            throw new IllegalArgumentException("잘못된 인자를 입력 받았습니다.");
+        }
     }
 }
