562785bd311edfbf39f55f4ad8ba8953154fa3a5
jeomxon <jeomxon@gmail.com>
2022-11-07 17:17:39 +0900

    feat: 볼, 스트라이크, 낫싱을 출력하는 기능

--- src/main/java/game/Hint.java
+++ src/main/java/game/Hint.java
@@ -24,4 +24,20 @@ public class Hint {
         ball = 0;
         strike = 0;
     }
+
+    public void print() {
+        String hintMessage = "";
+
+        if (ball != 0) {
+            hintMessage += ball + "볼 ";
+        }
+        if (strike != 0) {
+            hintMessage += strike + "스트라이크";
+        }
+        if (ball == 0 && strike == 0) {
+            hintMessage += "낫싱";
+        }
+
+        System.out.println(hintMessage);
+    }
 }
