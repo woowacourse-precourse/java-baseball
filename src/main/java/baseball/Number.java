b338a96a8b685f54c4f606fa190a4a212d97e7b5
hhiyeon <hhyyeon0214@gmail.com>
2022-11-07 20:20:59 +0900

    feat: number-add random number to list

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -6,6 +6,12 @@ class Number {
     private static List<Integer> userNumbers;
     private static List<Integer> computers;
 
+    private void addRandomNumber(int randomNumber, List<Integer> computer) {
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber);
+        }
+    }
+
     public static List<Integer> getComputers() {
         return computers;
     }
