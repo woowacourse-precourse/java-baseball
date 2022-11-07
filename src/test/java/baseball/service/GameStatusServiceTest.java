662c70cd5640c7ef4381656ea275d78c5dcbb25e
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-07 11:35:40 +0900

    refactor: GameStatusService의 테스트코드 public 메서드를 사용하게 수정

--- src/test/java/baseball/service/GameStatusServiceTest.java
+++ src/test/java/baseball/service/GameStatusServiceTest.java
@@ -13,19 +13,31 @@ public class GameStatusServiceTest {
         gameStatusService = new GameStatusService();
     }
 
-    @Test
-    void 입력문자열이_2이면_참을_반환(){
-        assertThat(gameStatusService.isEndOfGame("2")).isTrue();
-    }
+//    @Test
+//    void 입력문자열이_2이면_참을_반환(){
+//        assertThat(gameStatusService.isEndOfGame("2")).isTrue();
+//    }
+//
+//    @Test
+//    void 스탑게임_하면_게임상태_거짓(){
+//        gameStatusService.stopGame();
+//        assertThat(gameStatusService.getGameStatus()).isFalse();
+//    }
+//
+//    @Test
+//    void 스탑게임_안하면_게임상태_참(){
+//        assertThat(gameStatusService.getGameStatus()).isTrue();
+//    }
 
     @Test
-    void 스탑게임_하면_게임상태_거짓(){
-        gameStatusService.stopGame();
+    void 입력이_2이면_게임상태_거짓으로_초기화(){
+        gameStatusService.setGameStatus("2");
         assertThat(gameStatusService.getGameStatus()).isFalse();
     }
 
     @Test
-    void 스탑게임_안하면_게임상태_참(){
+    void 입력이_1이면_게임상태_참으로_유지(){
+        gameStatusService.setGameStatus("1");
         assertThat(gameStatusService.getGameStatus()).isTrue();
     }
 }
