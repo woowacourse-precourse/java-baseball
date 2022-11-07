6547af7b85726c63c0ef74f6bf28d4c030ef3130
plusjob70 <plusjob70@gmail.com>
2022-11-07 17:13:55 +0900

    feat: 컴퓨터와 사용자의 숫자를 서로 비교

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,6 +8,8 @@ import java.util.stream.Collectors;
 
 public class Application {
     private static final int NUMBER_DIGIT = 3;
+    private static final int BALL_INDEX = 0;
+    private static final int STRIKE_INDEX = 1;
 
     public static void main(String[] args) {
         printStartGameSentence();
@@ -34,15 +36,15 @@ public class Application {
         return stringToIntegerList(input);
     }
 
+    private static List<Integer> stringToIntegerList(String number) {
+        return Arrays.stream(number.split("")).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
     private static String inputNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
         validateNumber(input);
         return input;
-    }
-
-    private static List<Integer> stringToIntegerList(String number) {
-        return Arrays.stream(number.split("")).map(Integer::parseInt).collect(Collectors.toList());
     }
 
     private static void validateNumber(String input) {
@@ -60,5 +62,29 @@ public class Application {
             }
             history.add(number);
         }
+    }
+
+    private static List<Integer> getGameResults(List<Integer> prediction, List<Integer> answer) {
+        List<Integer> results = Arrays.asList(0, 0);
+        List<Boolean> strike = Arrays.asList(false, false, false);
+
+        int predictionInt;
+        int answerInt;
+        for (int i = 0; i < NUMBER_DIGIT; i++) {
+            predictionInt = prediction.get(i);
+            answerInt = answer.get(i);
+            if (predictionInt == answerInt) {
+                results.set(STRIKE_INDEX, results.get(STRIKE_INDEX) + 1);
+                strike.set(i, true);
+            }
+        }
+
+        for (int i = 0; i < NUMBER_DIGIT; i++) {
+            predictionInt = prediction.get(i);
+            if (!strike.get(i) && answer.contains(predictionInt)) {
+                results.set(BALL_INDEX, results.get(BALL_INDEX) + 1);
+            }
+        }
+        return results;
     }
 }
