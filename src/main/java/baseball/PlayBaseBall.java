9b8cef757ddd8a7f6d2123ec4626a71618c2e2e5
saewoo1 <wooozooo1123@gmail.com>
2022-11-07 14:36:36 +0900

    refactor(PlayBaseBall): computer rename computerNumber

--- src/main/java/baseball/PlayBaseBall.java
+++ src/main/java/baseball/PlayBaseBall.java
@@ -5,14 +5,14 @@ public class PlayBaseBall {
 
   public static void playGame() { //싱글톤
     PrintOut.startMessage();
-    String computer = CreateRandomNumber.numberMaker(); //computerNumber
+    String computerNumber = CreateRandomNumber.numberMaker(); //computerNumber
     while (true) //게임 테이블 / 내부의 턴 횟수 구분
     {
       PrintOut.inputMessage(); //requsetInput~
       String user = UserScanner.input();
-      int strike = Hint.strikeCount(computer, user);
+      int strike = Hint.strikeCount(computerNumber, user);
       if (InputValidator.isValidNumber(user)) {  // void형식
-        PrintOut.scoreMessage(Hint.ballAndStrike(computer, user)); // ball-strike 의존성
+        PrintOut.scoreMessage(Hint.ballAndStrike(computerNumber, user)); // ball-strike 의존성
       }
       if (strike == Constant.MAX_STRIKE) {
         PrintOut.endMessage();
@@ -20,7 +20,7 @@ public class PlayBaseBall {
         if (!ContinueOrStop.choice(UserScanner.input())) { //choice 이름 /  과정이 눈에 보이게 //
           break;
         }
-        computer = CreateRandomNumber.numberMaker();
+        computerNumber = CreateRandomNumber.numberMaker();
       }
     }
   }
