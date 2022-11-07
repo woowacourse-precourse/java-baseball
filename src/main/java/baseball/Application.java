6d5165e44874e84b4ab5415153809d9a64ec91ef
jihwan <sgh002400@naver.com>
2022-11-08 01:44:12 +0900

    feat(Application): programStart() 틀 구성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        OutputView.displayGameStartMessage();
+        programStart();
+    }
+
+    private static void programStart() {
+
     }
 }
