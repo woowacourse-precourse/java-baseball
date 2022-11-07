701495b572f10a26018449da293359fc4fa1b64c
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-07 20:41:15 +0900

    feat(Game): add computerNumber attribute with get/set methods

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,30 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Game {
 
+    private List<Integer> computerNumber = new ArrayList<>();
+
+    public void start() {}
+
+    private List<Integer> setComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+        return computerNumber;
+    }
+
+    public List<Integer> getComputerNumber() {
+        if (computerNumber.isEmpty()) {
+            computerNumber = setComputerNumber();
+        }
+        return computerNumber;
+    }
 }
