39321b7633bee8d1f24ed0c504980bd6ef205f6c
subin Park <psb0320@hs.ac.kr>
2022-11-08 02:01:09 +0900

    feat: 컴퓨터 숫자 랜덤 생성 기능
    - 중복 없이 1부터 9까지 이루어진 3자리 랜덤 숫자 생성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,9 +1,20 @@
 package baseball;
 
+import java.util.List;
+
 public class Game {
     private static final String START_SENTENCE = "숫자 야구 게임을 시작합니다.";
 
+    private List<Integer> computer;
+
     public void startGame() {
         System.out.println(START_SENTENCE);
+
+        setComputer();
+    }
+
+    private void setComputer() {
+        Number number = new Number();
+        this.computer = number.getComputerNumber();
     }
 }
