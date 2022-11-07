f0680c2652a1e883c1aa13091d6f828cc14ce8c0
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-08 03:09:30 +0900

    feat(OutputView): 아무것도 못맞췄을 때와 일부 맞췄을 때 View 메서드 작성

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,8 +2,26 @@ package baseball.view;
 
 public class OutputView {
     private static final String INIT_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String NOTTING = "낫싱";
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
 
     public static void printInitGameMessage() {
         System.out.println(INIT_MESSAGE);
+    }
+
+    public void printNothing() {
+        System.out.println(NOTTING);
+    }
+
+    public void printResult(int strike, int ball) {
+        String result = "";
+        if (ball > 0) {
+            result += ball + BALL + " ";
+        }
+        if (strike > 0 ) {
+            result += strike + STRIKE;
+        }
+        System.out.println(result);
     }
 }
