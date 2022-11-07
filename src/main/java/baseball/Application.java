db17bd400ad1e62a1328bbdeceefcd703fce0f6c
DoooongDong <rhkswls98@kakao.com>
2022-11-07 21:56:41 +0900

    랜덤 함수 생성 함수 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    //랜덤 값을 출력하는 함수 작성
+    public static ArrayList<Integer> createRandomNumber(){
+        ArrayList<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!randomNumbers.contains(randomNumber)){
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
     }
 }
