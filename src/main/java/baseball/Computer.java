0c6901d763413aa11b24cd2d9ce763d46d33ee06
opuopuo <pureuml.dev@gmail.com>
2022-11-07 21:53:18 +0900

    feat(computer): 볼, 스트라이크 판별 기능 추가

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,14 +5,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Computer {
-
     private Integer[] answer;
+    private int ball;
+    private int strike;
     private static final int MAX_LENGTH = 3;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
+    private static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
 
     public Computer() {
         answer = new Integer[MAX_LENGTH];
+        ball = 0;
+        strike = 0;
     }
 
     public void generateValue() {
@@ -26,12 +33,30 @@ public class Computer {
     private void setToArray(Set<Integer> value) {
         int index = 0;
         for (Integer number : value) {
+            //TODO 삭제!!
+            System.out.print(number);
             answer[index] = number;
             index++;
         }
+        System.out.println();
     }
 
-    public boolean isAnswer(Integer[] value) {
-        return true;
+    private void countBallAndStrike(Integer[] value) {
+        for (int index = 0; index < MAX_LENGTH; index++) {
+            if (answer[index] == value[index]) {
+                strike++;
+            }
+            isBall(answer[index], index, value);
+        }
+    }
+
+    private void isBall(int answer, int answerIndex, Integer[] value) {
+        int valueIndex = 0;
+        for (int number : value) {
+            if (number == answer && valueIndex != answerIndex) {
+                ball++;
+            }
+            valueIndex++;
+        }
     }
 }
