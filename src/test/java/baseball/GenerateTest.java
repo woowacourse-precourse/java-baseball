495e0fcfd93ac21f01efec05ca8548ac301b0e26
leeyongjun <leeyongjun0310@naver.com>
2022-11-07 15:55:03 +0900

    test(GenerateTest): 랜덤 숫자의 범위를 확인하는 테스트 코드 추가

--- src/test/java/baseball/GenerateTest.java
+++ src/test/java/baseball/GenerateTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
@@ -13,5 +14,18 @@ public class GenerateTest {
         int input = 3;
 
         assertThat(Generate.randomNumber().size()).isEqualTo(input);
+    }
+
+    @Test
+    void randomNumber_랜덤_숫자_범위_확인() {
+        for (int i = 0; i < 1000; i++) {
+            List<Integer> random = Generate.randomNumber();
+            List<String> input = new ArrayList<>();
+            for (int number : random) {
+                input.add(String.valueOf(number));
+            }
+
+            assertThat(Integer.valueOf(String.join("", input))).isBetween(111, 999);
+        }
     }
 }
