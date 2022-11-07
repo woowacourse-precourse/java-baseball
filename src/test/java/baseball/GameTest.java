c78d159a06d3c135a18a2b9bcb4b4d73c926eb84
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-07 20:56:04 +0900

    refactor(GameTest): refactor initializing Game instance procedure

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -2,18 +2,26 @@ package baseball;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+
 import java.util.ArrayList;
 import java.util.List;
+
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.*;
 
 public class GameTest {
+    private Game game;
+
+    @BeforeEach
+    void setUp() {
+        this.game = new Game();
+    }
 
     @DisplayName("3자리 수 생성")
     @Test
-    void createRandomNumber() {
-        Game game = new Game();
+    void getComputerNumberTest() {
         List<Integer> testComputerNumber = new ArrayList<>();
         testComputerNumber.add(1);
         testComputerNumber.add(2);
