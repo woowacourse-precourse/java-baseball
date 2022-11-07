08551f4cad29f7ead7910a1c7c7c1c4a559049e5
Woojin Lee <dhkep03@gmail.com>
2022-11-08 01:32:49 +0900

    feat(OutputView): 3스트라이크 결과 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,4 +4,8 @@ public class OutputView {
     public static void informStartGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public static void printResultTurnOfOut() {
+        System.out.println("3스트라이크");
+    }
 }
