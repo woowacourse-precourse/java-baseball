b2626ef7091bcc9ce476c9939c914d99ecd964d7
sanghyuk2 <givemesomefoodplz@gmail.com>
2022-11-07 17:25:19 +0900

    feat: "애플리케이션 시작 기능 구현"
    애플리케이션 시작, 재시도 API 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import baseball.domain.BaseballGame;
+import baseball.domain.Retry;
+
+import static baseball.domain.Retry.RETRY_NUM;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        do {
+            BaseballGame baseballGame = new BaseballGame();
+            baseballGame.start();
+        } while (wantAgain());
+    }
+
+    private static boolean wantAgain() {
+        Retry retry = new Retry(readLine());
+
+        return retry.getRetryNum().equals(RETRY_NUM);
     }
 }
