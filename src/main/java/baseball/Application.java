cea85bc58fce9fe61cbe97f295c51d24f7569c7f
Yujin830 <rladbwls30@naver.com>
2022-11-08 03:14:37 +0900

    Feat: 정답 입력받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,12 +8,24 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         List<Integer> computer = new ArrayList<>();
+        String answer;
+        boolean again = true;
+
         while(computer.size() < 3){
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if(!computer.contains(randomNumber)){
                 computer.add(randomNumber);
             }
         }
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+
+        while (again){
+            answer = camp.nextstep.edu.missionutils.Console.readLine();
+
+        }
+
         // TODO: 프로그램 구현
     }
 }
