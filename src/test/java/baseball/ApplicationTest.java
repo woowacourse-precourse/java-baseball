f3b951c16f514a1ee9559054e89465a60690a038
KSH <tkdgns543@gmail.com>
2022-11-08 06:36:15 +0900

    test(ApplicationTest): 게임 중 유저 입력에 대해 예외 발생 테스트
    정상 작동과 예외 발생 경우를 나눠서 메서드 작성
    각 메서드마다 여러 매개변수 입력
    예외 발생 경우는 1~에서 9가 아닌 문자포함, 숫자 자릿수 확인, 중복 문자 확인 포함

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,17 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import utils.GameInputException;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -31,5 +36,35 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    @Nested
+    class GameInputExceptionTest {
+        GameInputException gameInputException = new GameInputException(3);
+
+        @Nested
+        @DisplayName("게임중 입력에서 예외 발생 케이스 체크")
+        class NegativeGameInputExceptionTest {
+            @ParameterizedTest
+            @ValueSource(strings = {"abc", "-13", "1234", "abcd", "", "+15", "057", "221", "222", "a93", "a03k"})
+            void testNegativeCaseZ(String userInputTest) {
+                assertThatThrownBy(() -> gameInputException.checkUserInput(userInputTest)).isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("게임중 입력에서 예외 미발생 케이스 체크")
+        class PositiveGameInputExceptionTest {
+            @ParameterizedTest
+            @ValueSource(strings = {"123", "234", "921"})
+            void testPositiveCase(String userInputTest) {
+                try {
+                    gameInputException.checkUserInput(userInputTest);
+                } catch (IllegalArgumentException illegalArgumentException) {
+                    assertEquals(0,1);
+                }
+
+            }
+        }
     }
 }
