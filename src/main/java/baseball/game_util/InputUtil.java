e21bc8020cdb6ddb3a14d5fa1c99c0f0da12bf3a
LeeJejune <leejejune0625@gmail.com>
2022-11-07 14:37:32 +0900

    feat: Player가 입력한 값의 범위를 확인하는 메서드 구현

---
+++ src/main/java/baseball/game_util/InputUtil.java
@@ -0,0 +1,5 @@
+import static baseball.common.Constants.MAX_USER_NUMBER_INPUT;
+    private static boolean isMaxLength(String userInput) {
+        return (userInput.length() == MAX_USER_NUMBER_INPUT);
+    }
+
