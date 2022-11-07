d000c368e92e87345c2a51725dd412f8b2017184
gmu1026 <gmu1026@gmail.com>
2022-11-08 00:34:32 +0900

    feat: 랜덤 값 생성

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,11 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class BaseBallGame {
+    private int randomNumber = 0;
     public void startGame() {
         printStartMessage();
+        initRandomNumber();
     }
 
     private void printStartMessage() {
         System.out.println(GameMessage.START.getText());
+    }
+
+    private void initRandomNumber() {
+        randomNumber += Randoms.pickNumberInRange(1, 9) * 100;
+        randomNumber += Randoms.pickNumberInRange(1, 9) * 10;
+        randomNumber += Randoms.pickNumberInRange(1, 9);
     }
 }
