6dd1af1099724885d9554a11d7995d9a865b1b06
Ran <lan4250@naver.com>
2022-11-08 04:36:45 +0900

    FEAT / baseball()메서드로 strike, ball 수를 파악하고 Node로 반환, makeTargetNum()의 반환형을 String으로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,9 +5,22 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
+class Node {
+
+    String num;
+    int strike;
+    int ball;
+
+    public Node(String num, int strike, int ball) {
+        this.num = num;
+        this.strike = strike;
+        this.ball = ball;
+    }
+}
+
 public class Application {
 
-    public static int makeTargetNum() {
+    public static String makeTargetNum() {
 
         List<Integer> target = new ArrayList<>();
 
@@ -24,49 +37,93 @@ public class Application {
             stringOfTargetNum += n;
         }
 
-        return Integer.parseInt(stringOfTargetNum);
+        return stringOfTargetNum;
+
     }
 
-    public static void isValidate(String input){
+    public static void isValidate(String input) {
 
         char[] inputToArr = input.toCharArray();
 
         for (char c : inputToArr) {
-            if(!Character.isDigit(c)){
+            if (!Character.isDigit(c)) {
                 throw new IllegalArgumentException("오직 숫자만 입력 가능합니다.");
             }
         }
 
-        if(input.length() != 3){
+        if (input.length() != 3) {
             throw new IllegalArgumentException("세 자리수만 입력 가능합니다.");
         }
 
-
-        if(Integer.parseInt(input) < 123 || 987 < Integer.parseInt(input)){
+        if (Integer.parseInt(input) < 123 || 987 < Integer.parseInt(input)) {
             throw new IllegalArgumentException("중복 없는 세 자리수만 입력 가능합니다.");
         }
 
-        if(input.contains("0")){
+        if (input.contains("0")) {
             throw new IllegalArgumentException("0을 제외한 세 자리 수를 입력하세요.");
         }
 
-        if(inputToArr[0] == inputToArr[1] || inputToArr[1] == inputToArr[2] || inputToArr[0] == inputToArr[2]){
+        if (inputToArr[0] == inputToArr[1] || inputToArr[1] == inputToArr[2]
+            || inputToArr[0] == inputToArr[2]) {
             throw new IllegalArgumentException("중복이 없는 세 자리 수를 입력하세요.");
         }
 
     }
 
+    public static Node baseball(String target, String num) {
+
+        Node node = new Node(num, 0, 0);
+
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                if (i == j && target.charAt(i) == num.charAt(j)) {
+                    ++node.strike;
+                } else if (target.charAt(i) == num.charAt(j)) {
+                    ++node.ball;
+                }
+            }
+        }
+
+        return node;
+    }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         // 1
-        int target = makeTargetNum();
+        String target = makeTargetNum();
 
         // 2
         System.out.println("숫자 야구 게임을 시작합니다.");
-        String input = Console.readLine();
-        isValidate(input);
+
+        // 3
+        while (true) {
+
+            System.out.println("숫자를 입력해주세요 :  ");
+            String input = Console.readLine();
+            isValidate(input);
+            Node node = baseball(target, input);
+
+            if (node.strike + node.ball == 0) {
+                System.out.println("낫싱");
+
+            } else {
+
+                if (node.ball == 0) {
+                    System.out.printf("%d스트라이크 %n", node.strike);
+
+                } else if (node.strike == 0) {
+                    System.out.printf("%d볼 %n", node.ball, node.strike);
+
+                } else {
+                    System.out.printf("%d볼 %d스트라이크 %n", node.ball, node.strike);
+
+                }
+
+            }
+
+
+        }
 
 
     }
