b830460dc21256b2b7c908ae8a076feabdb12c84
cocomozila <joseyun94@naver.com>
2022-11-07 11:05:18 +0900

    refactor: inputUserNumber() static으로 변경

--- src/main/java/baseball/util/Input.java
+++ src/main/java/baseball/util/Input.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     // 플레이어에게 입력 값을 받는 기능
-    public String inputUserNumber() {
+    public static String inputUserNumber() {
         return Console.readLine();
     }
 }
