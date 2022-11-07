ec0cde9dfa9967f7713bd6b449e50960a8b37002
sudo-dustle <asdf748596123@gmail.com>
2022-11-07 20:31:24 +0900

    refactor: getRandomUniqueNumbers 메소드 안에 List -> Set으로 변경

--- src/main/java/util/RandomUniqueNumber.java
+++ src/main/java/util/RandomUniqueNumber.java
@@ -3,14 +3,16 @@ package util;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class RandomUniqueNumber {
     private static final int MINIMUM_RANDOM_NUMBER_RANGE = 1;
     private static final int MAXIMUM_RANDOM_NUMBER_RANGE = 9;
 
     public static List<Integer> getRandomUniqueNumbers(int size) {
-        List<Integer> computer = new ArrayList<>();
+        Set<Integer> computer = new HashSet<>();
 
         while (computer.size() < size) {
             int randomNumber = Randoms.pickNumberInRange(MINIMUM_RANDOM_NUMBER_RANGE, MAXIMUM_RANDOM_NUMBER_RANGE);
@@ -20,6 +22,6 @@ public class RandomUniqueNumber {
             }
         }
 
-        return computer;
+        return new ArrayList<>(computer);
     }
 }
