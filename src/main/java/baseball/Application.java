b95599ed6fdbf845db4c8b18bae6e791080c3eb8
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-07 22:34:33 +0900

    feat: add player input and check input validation

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     private static void playGame() {
         String answer = createRandomNumber();
+        String playerInput;
         boolean isAnswer = false;
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         while (!isAnswer) {
-            // TODO: 게임 진행
+            playerInput = Console.readLine();
+            isValidInput(playerInput);
+            // TODO: 스트라이크, 볼, 정답 판독
         }
     }
 
@@ -25,6 +32,21 @@ public class Application {
         }
 
         return answer;
+    }
+
+    private static void isValidInput(String playerInput) {
+        List<Character> input = new ArrayList<>();
+
+        if (playerInput.length() != 3) {
+            throw new IllegalArgumentException("잘못된 입력 입니다. 게임을 종료합니다.");
+        }
+
+        for (int i = 0; i < playerInput.length(); i++) {
+            if (input.contains(playerInput.charAt(i))) {
+                throw new IllegalArgumentException("잘못된 입력 입니다. 게임을 종료합니다.");
+            }
+            input.add(playerInput.charAt(i));
+        }
     }
 
     public static void main(String[] args) {
