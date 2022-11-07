d2b8065d0c45682500e5bd95ba18c1627db571f4
whdcks420 <whdcks420@gmail.com>
2022-11-07 23:51:50 +0900

    refact: 랜덤 숫자 생성 테스트

--- src/test/java/baseball/utils/BaseballNumberGeneratorTest.java
+++ src/test/java/baseball/utils/BaseballNumberGeneratorTest.java
@@ -7,11 +7,11 @@ import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class NumberGeneratorNumberTest {
+class BaseballNumberGeneratorTest {
 
     @RepeatedTest(value = 10)
     void 서로_다른_세자리_숫자_생성_성공() {
-        String baseballNumber = NumberGenerator.generateNumber();
+        String baseballNumber = BaseballNumberGenerator.generateNumber();
         Set<Character> list = baseballNumber.chars().mapToObj(i -> (char) i).collect(Collectors.toSet());
 
         assertEquals(3, list.size());
