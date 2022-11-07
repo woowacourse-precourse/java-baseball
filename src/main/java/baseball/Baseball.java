df64013eadd4425d97374455aeb7e35d2d7f93a3
seojinvely1004 <seojin0124@naver.com>
2022-11-07 18:58:44 +0900

    startBaseball()

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,8 +1,24 @@
 package baseball;
 
+import static baseball.RandomUtil.createComputerNumbers;
+import static baseball.UserInputUtil.checkProgress;
+import static baseball.UserInputUtil.getThreeDigitInput;
+
 public class Baseball {
 
 	public void startBaseball() {
+		while(true) {
+			progressBaseball();
+
+			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+			boolean check = checkProgress();
+
+			if (check == false) {
+				break;
+			}
+		}
 	}
 
+	
 }
