e7b2d8c7b00a0f6c456071f6cf460c472f38c786
phj1120 <hyeonj1998@naver.com>
2022-11-07 23:52:32 +0900

    feat: 입력에 대한 예외 처리 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
-
 public class Application {
 
     public static void main(String[] args) {
@@ -11,13 +10,28 @@ public class Application {
         while (status) {
             Game game = new Game();
             game.start();
-
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String line = Console.readLine();
-            if ("2".equals(line)) {
+            validInputReplay(line);
+            if (wantExit(line)) {
                 status = false;
                 System.out.println("게임 종료");
             }
         }
     }
+
+    private static void validInputReplay(String line) {
+        if (!(wantRestart(line) || wantExit(line))) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean wantRestart(String line) {
+        return "1".equals(line);
+    }
+
+    private static boolean wantExit(String line) {
+        return "2".equals(line);
+    }
+
 }
--- src/main/java/baseball/Stage.java
+++ src/main/java/baseball/Stage.java
@@ -2,6 +2,9 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Stage {
     private Number computerNumber;
     private Number userNumber;
@@ -13,9 +16,38 @@ public class Stage {
     public void progress() {
         System.out.print("숫자를 입력해주세요 : ");
         String line = Console.readLine();
+        validNumberInput(line);
         userNumber = new Number(line);
         StageResult stageResult = new StageResult(computerNumber, userNumber);
         stageResult.print();
+    }
+
+    private void validNumberInput(String line) {
+        validLength(line.length());
+        validInteger(line);
+        validDuplicate(line);
+    }
+
+    private void validDuplicate(String line) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < line.length(); i++) {
+            set.add(Character.getNumericValue(line.charAt(i)));
+        }
+        validLength(set.size());
+    }
+
+    private void validInteger(String line) {
+        try {
+            Integer.parseInt(line);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void validLength(int length) {
+        if (length != 3) {
+            throw new IllegalArgumentException();
+        }
     }
 
     public boolean isComplete() {
