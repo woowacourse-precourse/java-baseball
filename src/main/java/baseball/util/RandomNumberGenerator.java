5a7264123c5c29bb4c512012c1572a0ac8ca6d42
wlsh44 <k87913j@gmail.com>
2022-11-08 01:18:18 +0900

    refactor: util 패키지와 상수 클래스 따로 분리

--- src/main/java/baseball/util/RandomNumberGenerator.java
+++ src/main/java/baseball/util/RandomNumberGenerator.java
@@ -1,4 +1,4 @@
-package baseball.core;
+package baseball.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -6,11 +6,12 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static baseball.util.BaseballConst.MAX_BALL_COUNT;
+
 public class RandomNumberGenerator {
 
     private static final int START_INCLUSIVE = 1;
     private static final int END_INCLUSIVE = 9;
-    private static final int MAX_BALL_COUNT = 3;
 
     public static List<Integer> generate() {
         return Stream.generate(() -> Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE))
