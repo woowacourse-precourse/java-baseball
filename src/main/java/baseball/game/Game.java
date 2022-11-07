cd81a6afe8411324737d1c11da85e8d82c327713
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-07 21:49:31 +0900

    style(Game): String은 상수가 될 수 있다

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -7,12 +7,12 @@ public class Game {
     private static final int RESULT_HAVE_BALL_AND_STRIKE = 2;
     private static final int RESULT_HAVE_ONLY_BALL_OR_STRIKE = 1;
 
-    private static final String nothingMessage = "낫싱\n";
-    private static final String ballAndStrikeMessage = "%d볼 %d스트라이크\n";
-    private static final String strikeMessage = "%d스트라이크\n";
-    private static final String ballMessage = "%d볼\n";
-    private static final String startingMessage = "숫자 야구 게임을 시작합니다.\n";
-    private static final String endingMessage = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
+    private static final String NOTHING_MESSAGE = "낫싱\n";
+    private static final String BALL_AND_STRIKE_MESSAGE = "%d볼 %d스트라이크\n";
+    private static final String STRIKE_MESSAGE = "%d스트라이크\n";
+    private static final String BALL_MESSAGE = "%d볼\n";
+    private static final String STARTING_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
+    private static final String ENDING_MESSAGE = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
 
     private int numberOfBall;
     private int numberOfAttempts = 0;
@@ -52,7 +52,7 @@ public class Game {
     }
 
     public void printStartingMessage() {
-        System.out.printf(startingMessage);
+        System.out.printf(STARTING_MESSAGE);
     }
 
     public int getNumberOfAttempts() {
@@ -62,21 +62,21 @@ public class Game {
     private void printResult(Ball userBall, Ball computerBall) {
         EnumMap<ResultOfBall, Integer> result = ballReader.getStrikeAndBall(userBall, computerBall);
         if (result.isEmpty()) {
-            System.out.printf(nothingMessage);
+            System.out.printf(NOTHING_MESSAGE);
         }
         if (result.size() == RESULT_HAVE_BALL_AND_STRIKE) {
-            System.out.printf(ballAndStrikeMessage, result.get(ResultOfBall.STRIKE), result.get(ResultOfBall.BALL));
+            System.out.printf(BALL_AND_STRIKE_MESSAGE, result.get(ResultOfBall.STRIKE), result.get(ResultOfBall.BALL));
         }
         if (result.size() == RESULT_HAVE_ONLY_BALL_OR_STRIKE && result.containsKey(ResultOfBall.STRIKE)) {
-            System.out.printf(strikeMessage, result.get(ResultOfBall.STRIKE));
+            System.out.printf(STRIKE_MESSAGE, result.get(ResultOfBall.STRIKE));
         }
         if (result.size() == RESULT_HAVE_ONLY_BALL_OR_STRIKE && result.containsKey(ResultOfBall.BALL)) {
-            System.out.printf(ballMessage, result.get(ResultOfBall.BALL));
+            System.out.printf(BALL_MESSAGE, result.get(ResultOfBall.BALL));
         }
     }
 
     private void printEndingMessage() {
-        System.out.printf(endingMessage, numberOfBall);
+        System.out.printf(ENDING_MESSAGE, numberOfBall);
     }
 
 }
