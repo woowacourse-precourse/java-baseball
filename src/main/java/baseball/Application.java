9900bcc836cfabd1b3341680d2924263e3350eef
tunkcalb <wjdgusdndi@gmail.com>
2022-11-08 01:07:14 +0900

    shape

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,4 +4,31 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+    public static void start(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    public static int make_number(){
+
+    }
+    public static int input_number(){
+
+    }
+    public static int wrong_number(){
+
+    }
+    public static int check_strike(){
+
+    }
+    public static int check_ball(){
+
+    }
+    public static void check_message(){
+
+    }
+    public static void end_message(){
+
+    }
+    public static int end_game(){
+
+    }
 }
