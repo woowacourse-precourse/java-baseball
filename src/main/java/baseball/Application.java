da02a03858803c3351e90eee91438ac74c25b687
parkjungyoon <tbr06057@naver.com>
2022-11-07 13:18:52 +0900

    feat: player 랜덤 3자리 숫자 입력 받는 메서드 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,42 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.validator.NumberValidator.validateRange;
+import static baseball.validator.NumbersValidator.*;
+
 public class Application {
+    private final static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
     public static void main(String[] args) {
-        List<Integer> computerNumbers = computerRandomNumber();
+        List<Integer> computerNumbers = getComputerRandomNumbers();
+
+        System.out.println(GAME_START_MESSAGE);
+        System.out.print("숫자를 입력해주세요 : ");
+        List<Integer> playerNumbers = getPlayerRandomNumbers();
     }
 
-    static List<Integer> computerRandomNumber() {
+    static List<Integer> getPlayerRandomNumbers() {
+        String input = Console.readLine();
+
+        validateDigit(input);
+        validateNonNumeric(input);
+
+        List<Integer> player = new ArrayList<>();
+        for (char inputChar : input.toCharArray()) {
+            validateRange((int) inputChar);
+            player.add((int) inputChar);
+            System.out.println(player);
+        }
+        validateDuplicateNumber(player);
+        return player;
+    }
+
+    static List<Integer> getComputerRandomNumbers() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
