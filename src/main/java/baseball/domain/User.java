703ee698b522d2089e910f318f17b3dee05382b7
kariskan <zkfzpf56@naver.com>
2022-11-07 15:43:45 +0900

    feat: 사용자의 입력을 받고 List 형태로 변환하는 로직 구현

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -1,7 +1,11 @@
 package baseball.domain;
 
+import baseball.utils.NumberValidator;
+import baseball.status.Constants;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class User {
 
@@ -10,6 +14,21 @@ public class User {
 
     public void initializeUserNumbers() {
         userNumbers = new ArrayList<>();
+    }
+
+    public void processUserInput() {
+        input = Console.readLine();
+        NumberValidator.validate(input);
+        parseUserInput();
+    }
+
+    private void parseUserInput() {
+        IntStream.range(Constants.ZERO_COUNT.getNumber(), Constants.NUMBER_OF_DIGITS.getNumber())
+                .forEach(index -> userNumbers.add(parseCharToDigit(input.charAt(index))));
+    }
+
+    public int parseCharToDigit(char charDigit) {
+        return charDigit - Constants.FIRST_CHARACTER_OF_DIGITS.getNumber();
     }
 
     public List<Integer> getUserNumbers() {
