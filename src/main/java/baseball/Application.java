25d09193d11b81714d81eab1fa6f08c6bf17d208
SonMinGyu <littleking13@naver.com>
2022-11-07 21:16:00 +0900

    [REFACTOR] 게임 종료 후 잘못된 입력이 들어올 때의 코드 리팩토링
    - 3스트라이크를 하고 게임이 종료 되었을 때 1 혹은 2말고 다른 입력이 들어오면 IllegalArgumentException을 throw하고 프로그램이 종료되도록 리팩토링함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -177,10 +177,8 @@ public class Application {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int input = Integer.parseInt(Console.readLine());
 
-        while (!(input == 1 || input == 2)) {
-            System.out.println("잘못된 입력입니다.");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            input = Integer.parseInt(Console.readLine());
+        if (!(input == 1 || input == 2)) {
+            throw new IllegalArgumentException("잘못된 입력값을 입력하셨습니다.");
         }
 
         if (input == 2) {
