f6f0bce4fa7c212102cb89a9578e5306f423aa53
Jeeseob <jeeseob5761@gmail.com>
2022-11-07 15:20:20 +0900

    Add: 게임 구현부 - ball count 중 strike 계산 함수 구현

--- src/main/java/baseball/BallCount.java
+++ src/main/java/baseball/BallCount.java
@@ -1,11 +1,28 @@
 package baseball;
 
+import java.util.List;
+
 /**
  * @Author : Jeeseob
  * @CreateAt : 2022/11/07
  */
 public class BallCount {
+    private int strike;
+    private int ball;
+
+    public BallCount(List<Integer> answer, List<Integer> inputAnswer) {
+
+    }
+
     // TODO: stirke 계산
+    private void countStrike(List<Integer> answer, List<Integer> userAnswer) {
+        this.strike = 0;
+        for (int i = 0; i < answer.size(); i++) {
+            if (answer.get(i).equals(userAnswer.get(i))) {
+                strike++;
+            }
+        }
+    }
     // TODO: ball 계산
     // TODO: ballcount comment 반환 override toString
 }
