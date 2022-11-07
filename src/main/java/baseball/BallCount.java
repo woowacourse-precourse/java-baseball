134cbf56b360e2d9b8cd99e8666e9b7c6a4f3d68
juhwang <juhong4930@naver.com>
2022-11-08 00:12:07 +0900

    feat(BallCount): plus ball after comparing between random numbers and input numbers

---
+++ src/main/java/baseball/BallCount.java
@@ -0,0 +1,41 @@
+package baseball;
+
+import java.util.List;
+
+public class BallCount {
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String NOTHING = "낫싱";
+
+
+    private BaseballNumbers inputBaseballNumbers;
+    private int ball = 0;
+    private int strike = 0;
+
+    public void validateBallCount(BaseballNumbers randomNumbers, BaseballNumbers inputBaseballNumbers) {
+        this.inputBaseballNumbers = inputBaseballNumbers;
+        validateBall(randomNumbers);
+    }
+
+    private void validateBall(BaseballNumbers randomNumbers) {
+        for (int index = BaseballConstants.FIRST_INDEX_OF_BASEBALL_NUMBER; index < BaseballConstants.LAST_INDEX_OF_BASEBALL_NUMBER; index++) {
+            validateBallInRandomNumbers(index, randomNumbers);
+        }
+    }
+
+    private void validateBallInRandomNumbers(int index, BaseballNumbers randomNumbers) {
+        for (int randomNumbersIndex = BaseballConstants.FIRST_INDEX_OF_BASEBALL_NUMBER; randomNumbersIndex < BaseballConstants.LAST_INDEX_OF_BASEBALL_NUMBER; randomNumbersIndex++) {
+            if (index == randomNumbersIndex) {
+                continue;
+            }
+            validateBallPlus(inputBaseballNumbers.get(index), randomNumbers.get(randomNumbersIndex));
+        }
+    }
+
+    private void validateBallPlus(int inputNumber, int randomNUmber) {
+        if (inputNumber == randomNUmber) {
+            this.ball++;
+        }
+    }
+
+}
