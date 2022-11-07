c1c2587b43e33125a697863a3e0f6e2c584591d5
wonju-dev <wonjulee.dev@gmail.com>
2022-11-07 14:32:04 +0900

    feat: Game 클래스 구현
    Game는 숫자야구의 주요한 실행흐름과 관련된 메소드들 관리하는 클래스이다.
    validateGuess는 두 개의 Integer타입 파라미터를 사용한다.
    게임의 평가 규칙에 따라 사용자 입력과 시스템의 랜덤 넘버를 비교하여 결과값을
    Map 타입으로 반환한다.

---
+++ src/main/java/baseball/game/Game.java
@@ -0,0 +1,25 @@
+package baseball.game;
+
+import baseball.game.enums.Result;
+import baseball.game.rule.Rule;
+import baseball.game.rule.RuleChecker;
+import baseball.game.rule.BallCountRule;
+import baseball.game.rule.StrikeCountRule;
+
+import java.util.List;
+import java.util.Map;
+
+public class Game {
+
+    private static Map<Result, Integer> validateGuess(Integer answer, Integer guess) {
+        // should keep this list element order for correct console output
+        List<Rule> scoreRules = List.of(
+                BallCountRule.getInstance(),
+                StrikeCountRule.getInstance()
+        );
+
+        Map<Result, Integer> guessResult = RuleChecker.check(scoreRules, answer, guess);
+
+        return guessResult;
+    }
+}
