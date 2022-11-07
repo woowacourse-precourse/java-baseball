6e26e6f1da5c645f7096640cd25dc838e8462a2f
hhiyeon <hhyyeon0214@gmail.com>
2022-11-07 20:15:20 +0900

    feat: response-print init user number

--- src/main/java/baseball/ResponseMessage.java
+++ src/main/java/baseball/ResponseMessage.java
@@ -4,4 +4,8 @@ class ResponseMessage {
     static void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    static void inputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
 }
