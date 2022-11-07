8fb1bd0d3694fdedbb74e5edb68d06d00b4316f4
이건회 <rg970604@naver.com>
2022-11-08 03:17:06 +0900

    style(UserInput.java) : static import 추가

--- src/main/java/baseball/view/UserInput.java
+++ src/main/java/baseball/view/UserInput.java
@@ -1,16 +1,18 @@
 package baseball.view;
 
+import static java.lang.System.out;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserInput {
 
     public static String InputUserNumbers() {
-        System.out.println("숫자를 입력해주세요 : ");
+        out.println("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
     public static String SelectRestartOrExit() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
 }
