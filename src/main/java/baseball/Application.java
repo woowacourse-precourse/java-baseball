c8961699f54f444037c3fbacbc31e8b32beaffda
ljh <dusk5588@naver.com>
2022-11-07 14:40:56 +0900

    style: 불필요한 공백 라인 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,9 @@
 package baseball;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Scanner;
-
 import static camp.nextstep.edu.missionutils.Console.readLine;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
-
 
 public class Application {
     public static void makeRandomValue(List<Integer> computer){
@@ -62,7 +58,7 @@ public class Application {
         System.out.println("3스트라이크");
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String input = readLine();
+        String input = readLine().trim();
         checkRestartInput(input);
         if(input.equals("1"))
             return 1;
@@ -71,14 +67,12 @@ public class Application {
     }
 
     public static void checkRestartInput(String input){
-        input.trim();
         if(!input.matches("[0-9]+"))
             throw new IllegalArgumentException("숫자로 이루어진 입력이 아닙니다.");
         if(input.length() != 1)
             throw new IllegalArgumentException("길이가 1인 숫자 입력이 아닙니다.");
-        if(!input.equals("1") && !input.equals("2")){
+        if(!input.equals("1") && !input.equals("2"))
             throw new IllegalArgumentException("1 또는 2의 입력이 아닙니다.");
-        }
     }
 
     public static int countBall(List<Integer> computer, List<Integer> user){
