2048540a10095a1ee575f8220dea66996988481f
sonsoowon <yuwoo1018@gmail.com>
2022-11-08 05:50:04 +0900

    feat(Player): 입력의 중복 문자 여부 확인
    - doesNotHaveDuplicateInputElement 메서드 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,8 +1,19 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Player {
     static boolean isInputSizeEqualToAnswerSize(String playerInput){
         return playerInput.length() == Game.ANSWER_SIZE;
     }
 
+    static boolean doesNotHaveDuplicateInputElement(String playerInput){
+        List<String> guessTokenList = Arrays.asList(playerInput.split(""));
+        Set<String> guessTokenSet = new HashSet<>(guessTokenList);
+
+        return guessTokenList.size() == guessTokenSet.size();
+    }
 }
