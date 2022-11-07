74b688f738c635bb1130199197d230ff4d1f0879
Jeangyu <gjwlsrb1700@naver.com>
2022-11-07 18:12:55 +0900

    [TEST] 정답으로 사용될 숫자 검증 추가

--- src/test/java/baseball/NumberCheckerTest.java
+++ src/test/java/baseball/NumberCheckerTest.java
@@ -1,10 +1,6 @@
 package baseball;
 
-
-import baseball.gameController.GameController;
 import baseball.utils.NumberChecker;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
--- src/test/java/baseball/NumberGeneratorTest.java
+++ src/test/java/baseball/NumberGeneratorTest.java
@@ -1,2 +1,23 @@
-package baseball;public class NumberGeneratorTest {
+package baseball;
+
+import static baseball.utils.BaseballConstants.*;
+import static org.assertj.core.api.Assertions.*;
+
+import baseball.random.NumberGenerator;
+import baseball.utils.NumberChecker;
+import org.junit.jupiter.api.Test;
+
+
+public class NumberGeneratorTest {
+
+    @Test
+    void 정답_세자리_수_숫자_검증(){
+        String generated_number = NumberGenerator.generateAnswerNumber();
+
+        assertThat(generated_number.length()).isEqualTo(BALL_NUMBER);
+        assertThat(generated_number.charAt(0) == generated_number.charAt(1) ||
+                generated_number.charAt(1) == generated_number.charAt(2) ||
+                generated_number.charAt(0) == generated_number.charAt(2)).isEqualTo(false);
+        assertThat(generated_number.matches("[1-9][1-9][1-9]")).isEqualTo(true);
+    }
 }
