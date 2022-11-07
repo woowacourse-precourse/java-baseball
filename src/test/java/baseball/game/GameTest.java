80668744cbab4a9b9f3f73ee812be1deb76e62b7
BaeDongHyeon <sasd730@gmail.com>
2022-11-07 21:23:25 +0900

    test: 사용자 입력 값 검증 테스트 코드 작성

--- src/test/java/baseball/game/GameTest.java
+++ src/test/java/baseball/game/GameTest.java
@@ -19,6 +19,27 @@ public class GameTest extends NsTest {
         );
     }
 
+    @Test
+    void 사용자가_잘못된_입력을_할_경우_예외_처리_해야한다() {
+        Game game = Game.create();
+        assertThatThrownBy(() -> game.inputCheck("1234"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("3자리 입력만 가능합니다.");
+
+        assertThatThrownBy(() -> game.inputCheck("000"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1~9까지의 숫자만 입력이 가능합니다.");
+
+        assertThatThrownBy(() -> game.inputCheck("b2a"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("1~9까지의 숫자만 입력이 가능합니다.");
+
+        assertThatThrownBy(() -> game.inputCheck("112"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("서로 다른 수만 입력이 가능합니다.");
+
+    }
+
     @Override
     protected void runMain() {
         Application.main(new String[]{});
