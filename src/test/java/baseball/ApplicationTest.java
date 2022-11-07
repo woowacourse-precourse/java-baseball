97f3de550d6a31573677f7dc6255616949c39bc6
Jaesun Park <41979078+ppakjae@users.noreply.github.com>
2022-11-08 01:06:12 +0900

    test: 입력 결과 검중(힌트) 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -80,6 +80,38 @@ class ApplicationTest extends NsTest {
         assertThat(ball).isEqualTo(1);
     }
 
+    @Test
+    void 입력_결과_검증_테스트() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Application application = new Application();
+
+        List<Integer> computer = new ArrayList<>();
+        List<Integer> player = new ArrayList<>();
+
+        computer.add(1);
+        computer.add(2);
+        computer.add(3);
+
+        player.add(1);
+        player.add(5);
+        player.add(2);
+
+        Method countStrike = application.getClass().getDeclaredMethod("countStrike", List.class, List.class);
+        countStrike.setAccessible(true);
+
+        Method countBall = application.getClass().getDeclaredMethod("countBall", List.class, List.class);
+        countBall.setAccessible(true);
+
+        Method makeHint = application.getClass().getDeclaredMethod("makeHint", int.class, int.class);
+        makeHint.setAccessible(true);
+
+        int strike = (int)countStrike.invoke(application, computer, player);
+        int ball = (int)countBall.invoke(application, computer, player);
+
+        String result = (String) makeHint.invoke(application, strike, ball);
+
+        assertThat(result).isEqualTo("1볼 1스트라이크");
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
