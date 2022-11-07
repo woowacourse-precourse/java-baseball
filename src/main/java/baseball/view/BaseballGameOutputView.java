ca2a579b582d7652e63b4c4d8b479636d00369ad
Qulip <ilkwon.ryu@gmail.com>
2022-11-08 04:27:05 +0900

    feat: BaseballGameOutputView 클래스 생성 및 게임시작 메시지 출력 추가

---
+++ src/main/java/baseball/view/BaseballGameOutputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+import static baseball.view.BaseballGameConstant.START_GAME_MESSAGE;
+
+public class BaseballGameOutputView {
+
+    public static void gameStart() {
+        System.out.println(START_GAME_MESSAGE);
+    }
+}
