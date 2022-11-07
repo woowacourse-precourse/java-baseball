f0695f8dcda40c4083db2d06cfb2e41a8d89ac0a
mgOh <issac_oh@naver.com>
2022-11-08 01:24:05 +0900

    fix: 랜덤 숫자 생성 함수 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,6 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -12,7 +13,13 @@ public class Game {
     Randoms Ran;
 
     public Game() {
-        List<Integer> numberList = Ran.pickUniqueNumbersInRange(1, 9, 3);
+        List<Integer> numberList = new ArrayList<>();
+        while (numberList.size() < 3){
+            int term = Ran.pickNumberInRange(1, 9);
+            if (!numberList.contains(term)){
+                numberList.add(term);
+            }
+        }
         this.num1 = numberList.get(0);
         this.num2 = numberList.get(1);
         this.num3 = numberList.get(2);
