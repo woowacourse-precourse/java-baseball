7b5c2f76fd463ea6069f81dd6611dfd37eccf367
Lee-SungMin <saevers26@gmail.com>
2022-11-08 00:39:40 +0900

    feat: 낫싱인지 확인 하는 기능 - 같은 수가 존재하는지 확인하는 기능 구현 - 같은 수가 없다면, 낫싱을 출력하는 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,6 +1,12 @@
 package baseball;
 
+import java.util.ArrayList;
+
 public class Game {
+
+    protected boolean nothing = true;
+
+    protected int same_cnt = 0;
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -8,6 +14,20 @@ public class Game {
 
     public void printInputNumber() {
         System.out.print("숫자를 입력해주세요: ");
+    }
+
+    public void checkNothing(ArrayList<Integer> computer, ArrayList<Character> user) {
+
+        for (int i = 0; i < 3; i++) {
+            if (computer.contains(user.get(i) - '0')) {
+                this.nothing = false;
+                same_cnt++;
+            }
+        }
+
+        if (this.nothing) {
+            System.out.println("낫싱");
+        }
     }
 
 }
