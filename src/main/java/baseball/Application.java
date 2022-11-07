05dc7bff766fb2f46e9cfd6529e9c3ba6bfde4c2
chesthyeon <niw927@naver.com>
2022-11-08 01:29:13 +0900

    endGameCheck 기능 완료 -> (int) strike 입력 받아 strike == 3 이라면 종료 메시지 출력 후 true를 리턴 아니라면 false를 리턴. restartCheck 메소드 완료. -> 인풋을 입력 받은 후 1,2인지 확인 그리고 1이라면 게임을 true, 2라면 false를 리턴

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,26 +6,27 @@ import java.util.*;
 
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
-
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true){
             gameStart();
-            break;
+            if (restartCheck()) return;
         }
     }
 
-    public static void gameStart(){
+    public static void gameStart() throws IllegalArgumentException{
         List<Integer> computerNumber = makeComputerNumber();
         List<Integer> strikeBallList;
         List<Integer> userNumber;
         String input;
-        while (true){
+
+        do {
             System.out.print("숫자를 입력해주세요 : ");
             input = readLine();
-            inputCheck(input);
+            userNumberInputCheck(input);
             userNumber = makeUserNumber(input);
-            break;
-        }
+            strikeBallList = checkStrikeBall(userNumber, computerNumber);
+            printStrikeBallList(strikeBallList.get(0), strikeBallList.get(1));
+        } while (!endGameCheck(strikeBallList.get(0)));
     }
 
     public static List<Integer> makeComputerNumber(){
@@ -39,14 +40,18 @@ public class Application {
         return computer;
     }
 
-    public static void inputCheck(String input) throws IllegalArgumentException {
-        Set<Character> overlapcheck = new HashSet<>();
+    public static void userNumberInputCheck(String input) throws IllegalArgumentException {
+        Set<Character> overlapCheck = new HashSet<>();
         for (int i = 0; i < input.length(); i++){
-            overlapcheck.add(input.charAt(i));
+            overlapCheck.add(input.charAt(i));
         }
-        if (input.matches("[0-9]") && overlapcheck.size() == 3) return;
+        if (input.matches("[0-9]+") && overlapCheck.size() == 3) return;
+        throw new IllegalArgumentException("잘못된 입력입니다.");
+    }
+
+    public static void startEndInputCheck(String input) throws IllegalArgumentException {
         if (input.matches("[1-2]{1}"))  return;
-        new IllegalArgumentException("잘못된 입력입니다.");
+        throw  new IllegalArgumentException("잘못된 입력입니다.");
     }
 
     public static List<Integer> makeUserNumber(String input){
@@ -59,32 +64,36 @@ public class Application {
     public static List<Integer> checkStrikeBall(List<Integer> userNumber, List<Integer> computerNumber){
         int strikeCount = 0, ballCount = 0;
         for (int i = 0; i < 3; i++){ // 스트라이크와 볼을 계산, 스트라이크 일 경우에도 볼 + 1을 해준다. 따라서 최종 볼은 볼 - 스트라이크다.
-            if (Objects.equals(userNumber.get(i), computerNumber.get(i))){
-                strikeCount++;
-            }
-            if (computerNumber.contains(userNumber.get(i))){
-                ballCount++;
-            }
+            if (Objects.equals(userNumber.get(i), computerNumber.get(i))) strikeCount++;
+            if (computerNumber.contains(userNumber.get(i))) ballCount++;
         }
         return Arrays.asList(strikeCount,ballCount - strikeCount);
     }
 
-    public static boolean endGame(int strike, int ball){ // 스트라이크와 볼의 수를 출력하고 스트라이크 == 3이면 true를 리턴한다.
-        if (strike == 0 && ball == 0) System.out.println("낫싱");
-        else if (strike != 0 && ball != 0){
-            System.out.println(ball+"볼 "+strike+"스트라이크");
+    public static void printStrikeBallList(int strike, int ball){ // 스트라이크와 볼의 수를 출력하고 스트라이크 == 3이면 true를 리턴한다.
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+            return;
         }
-        else if (ball == 0) {
-            System.out.println(strike+"스트라이크");
-            if (strike == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                return true;
-            }
-        }
-        else {
-            System.out.println(ball+"볼");
+
+        StringBuilder printStrikeBall = new StringBuilder();
+        if (ball > 0) printStrikeBall.append(ball).append("볼 ");
+        if (strike > 0) printStrikeBall.append(strike).append("스트라이크 ");
+        System.out.println(printStrikeBall);
+    }
+
+    public static boolean endGameCheck(int strike){
+        if (strike == 3) {
+            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
         }
         return false;
     }
 
+    public static boolean restartCheck() throws IllegalArgumentException{
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = readLine();
+        startEndInputCheck(input);
+        return "1".equals(input);
+    }
 }
