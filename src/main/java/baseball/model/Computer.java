66f998e54be900779fde9551c828c4d92a1893e4
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-11-07 13:50:50 +0900

    ADD : 게임이 시작되면 무작위 3자리 중복없는 수를 가지고있는 컴퓨터 클래스

---
+++ src/main/java/baseball/model/Computer.java
@@ -0,0 +1,19 @@
+package baseball.model;
+
+import static baseball.util.BallCode.*;
+
+import baseball.util.BallCode;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Computer {
+    private final static int NUMBER_SIZE = 3;
+    private final static int MAX_NUMBER = 9;
+    private final static int MIN_NUMBER = 1;
+
+    private List<Integer> numbers = new ArrayList<>();
+
+
+}
