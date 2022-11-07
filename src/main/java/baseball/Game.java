6663d8c0ad37bf4faba40389463d9e6065137d49
skck0226 <skck0226@ajou.ac.kr>
2022-11-08 00:55:03 +0900

    [feat] Strike 판단 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,9 +4,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Game {
-
+    private final int ZERO = 0;
+    private final int NUMBER_LENGTH = 3;
     private final List<Integer> answer_number;
 
     public Game() {
@@ -16,7 +18,7 @@ public class Game {
     private List<Integer> makeRandomNumber() {
         List<Integer> number = new ArrayList<>();
 
-        while (number.size() < 3) {
+        while (number.size() < NUMBER_LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (number.contains(randomNumber)) continue;
             number.add(randomNumber);
@@ -26,10 +28,21 @@ public class Game {
 
     public boolean validateAnswer_number() {
         return answer_number.stream()
-                            .allMatch(num -> num > 0 && num < 10)
-            && answer_number.stream()
-                            .distinct()
-                            .count() == 3;
+                .allMatch(num -> num > 0 && num < 10)
+                && answer_number.stream()
+                .distinct()
+                .count() == 3;
+    }
+
+    public int getCountOfStrike(List<Integer> predict_number) {
+        return (int) IntStream.range(ZERO, NUMBER_LENGTH)
+                .mapToObj(index -> List.of(answer_number.get(index), predict_number.get(index)))
+                .filter(this::checkAnswerAndPredictOnebyOne)
+                .count();
+    }
+
+    private boolean checkAnswerAndPredictOnebyOne(List<Integer> answer_and_predict_num) {
+        return answer_and_predict_num.get(0).equals(answer_and_predict_num.get(1));
     }
 
 }
