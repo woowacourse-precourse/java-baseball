b89b60f62133ef91cad8b0a363cbfe3cdb64e2c3
baekeunsun <eunsun5460@gmail.com>
2022-11-07 18:49:45 +0900

    refactor(view/Inputview): MVC 패턴 적용, 게임 재시작 여부 input 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,21 +5,12 @@ import java.util.*;
 import baseball.model.ComputerGenerate;
 import baseball.model.UserGenerate;
 import baseball.controller.Hint;
-import camp.nextstep.edu.missionutils.Console;
 
-import static baseball.model.Constant.NUM_SIZE;
+import static baseball.view.InputView.restartInput;
 
 public class Application {
     public static void main(String[] args) {
         GameStart();
-    }
-
-    private static void GameRestart() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        int input = Integer.valueOf(Console.readLine());
-        if (input == 1) {
-            GameStart();
-        }
     }
 
     private static void GameStart() {
@@ -33,6 +24,6 @@ public class Application {
             System.out.println(result);
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        GameRestart();
+        restartInput();
     }
 }
