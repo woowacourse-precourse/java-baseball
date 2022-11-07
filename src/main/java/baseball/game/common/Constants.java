043b7edad35f1f1ede647218400870e5873e0f1d
jindaram-stu <jinsb1999@naver.com>
2022-11-08 01:43:45 +0900

    feat(Constants): 숫자야구 게임 내에서 사용되는 상수 추가

---
+++ src/main/java/baseball/game/common/Constants.java
@@ -0,0 +1,13 @@
+package baseball.game.common;
+
+public class Constants {
+
+    public final static int FULL_STRIKE = 3;
+    public final static int NOTHING = 0;
+
+    public final static String RE_GAME = "1";
+    public final static String END_GAME = "2";
+
+    public final static int ESSENTIAL_LENGTH = 3;
+
+}
