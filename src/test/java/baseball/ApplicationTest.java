994beedac65f9bb73454c92690f6694930faff2d
HyebinShin <hyeviin.dev@gmail.com>
2022-11-07 15:54:39 +0900

    Test: 테스트 코드 유효성 검사
    유효성 검사에서,
    컴퓨터 숫자 입력 유효성 검사 : 3자리 일 때 테스트 성공 또는 3자리가 아닐 때 예외테스트
    재시작 종료 유효성 검사 : 1 또는 2를 입력하지 않았을 때 예외 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -4,6 +4,9 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -45,6 +48,29 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("222"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    @DisplayName("컴퓨터 숫자 입력 유효성 검사 : 3자리 일 때 테스트 성공")
+    void computerNumberThreeTest() {
+        List<Integer> computer = Application.getComputer();
+        assertThat(computer.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("컴퓨터 숫자 입력 유효성 검사 : 3자리가 아닐 때 예외 테스트")
+    void computerNumberExceptionTest() {
+        List<Integer> computer = List.of(1, 2, 3, 4);
+        assertThatThrownBy(() -> Application.validateComputer(computer)
+                ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("재시작/종료 유효성 검사 : 1 또는 2를 입력하지 않았을 때 예외 테스트")
+    void retryExceptionTest() {
+        String answer = "3";
+        assertThatThrownBy(() -> Application.validateAnswer(answer))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
