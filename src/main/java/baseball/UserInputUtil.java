840a0709d25613405e7f1da6aae5a84836fe009b
seojinvely1004 <seojin0124@naver.com>
2022-11-07 18:52:28 +0900

    feat:checkProgress()

---
+++ src/main/java/baseball/UserInputUtil.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserInputUtil {
+
+    private UserInputUtil() {
+    }
+
+    public static boolean checkProgress() {
+        String input = Console.readLine();
+
+        int number = toInteger(input);
+
+        if (number == 1) {
+            return true;
+        } else if (number == 2) {
+            return false;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+}
