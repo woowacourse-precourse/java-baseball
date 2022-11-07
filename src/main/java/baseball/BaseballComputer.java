c9f85f1803de9f30e09d234ab8c8921521659ac9
jijongkwon <help3451@naver.com>
2022-11-07 19:53:45 +0900

    sytle : 메시지 클래스의 함수 사용

--- src/main/java/baseball/BaseballComputer.java
+++ src/main/java/baseball/BaseballComputer.java
@@ -9,22 +9,7 @@ import java.util.List;
 public class BaseballComputer {
     private List<Integer> computerNumber = new ArrayList<>(); // 3자리수
     ChangeType changeType = new ChangeType();
-
-    public void startGameMessage(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
-
-    public void endGameMessage(){
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
-    }
-
-    public void inputNumberToComputerMessage(){
-        System.out.print("숫자를 입력해 주세요: ");
-    }
-
-    public void printRestartOrEnd(){
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-    }
+    GameMessage gameMessage = new GameMessage();
 
     public String userInputNumberInGame(){
         return Console.readLine();
@@ -36,14 +21,14 @@ public class BaseballComputer {
     }
 
     public void init(){
-        inputNumberToComputerMessage();
+        gameMessage.inputNumberToComputerMessage();
         String answer = userInputNumberInGame();
 
         if(!checkHitNumber(answer)){
             init();
         }
 
-        printRestartOrEnd();
+        gameMessage.printRestartOrEnd();
         int choice = inputRestartOrEnd();
 
         if(1 > choice || choice > 2){
@@ -54,6 +39,7 @@ public class BaseballComputer {
         if(choice == 1){
             start();
         }
+
         return ;
     }
 
@@ -83,7 +69,7 @@ public class BaseballComputer {
 
         // 게임 승리
         if(strike == 3){
-            endGameMessage();
+            gameMessage.endGameMessage();
             return true;
         }
 
