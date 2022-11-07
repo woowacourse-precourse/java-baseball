925d49a6e27f2a17bb87608a2b1cc6c0d2629ca9
jeomxon <jeomxon@gmail.com>
2022-11-07 21:31:09 +0900

    feat: 게임 시작을 위한 메소드 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,5 +5,6 @@ import game.Game;
 public class Application {
     public static void main(String[] args) {
         Game game = new Game();
+        game.start();
     }
 }
---
+++ src/test/java/game/GameTest.java
@@ -0,0 +1,36 @@
+package game;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    @Test
+    void start() {
+    }
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void play() {
+    }
+
+    @Test
+    void stop() {
+    }
+
+    @Test
+    void waiting() {
+    }
+
+    @Test
+    void checkBallAndStrike() {
+    }
+
+    @Test
+    void isThreeStrike() {
+    }
+}
