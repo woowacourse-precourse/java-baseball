fa1ede943d1741c9c429b9c5e5be6172575e1f11
w0ntae <bitcampcoding@gmail.com>
2022-11-07 18:05:13 +0900

    fix(application):Fix endgame
    Fix endgame to allow restart of the game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 
 public class Application {
-    static boolean restart;
+    static boolean endOfGame;
     
     public static void main(String[] args) {
         baseball();
@@ -90,7 +90,7 @@ public class Application {
         else if(strike == 3){
             System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! (1)다시 시작 (2)나가기");
-            restart = true;
+            endOfGame = true;
         }
         else if(strike == 0){
             System.out.println(ball + "볼");
@@ -106,13 +106,14 @@ public class Application {
         String random = random();
         while(run){
             String input = Console.readLine();
+            System.out.println(input);
             boolean check = input(input);
             if (check == true){
                 break;
             }
             else{
                 compare(random.toString(), input);
-                if(restart == true){
+                if(endOfGame == true){
                     run = endGame();
                 }
             }
@@ -120,11 +121,11 @@ public class Application {
     }
     
     public static  boolean endGame(){
-        if(Console.readLine() == "1"){
+        if(Console.readLine().equals("1")){
             System.out.println("숫자 야구 게임을 시작합니다.");
             return true;
         }
-        if(Console.readLine() == "2"){
+        if(Console.readLine().equals("2")){
             System.out.println("게임 끝");
         }
         return false;
