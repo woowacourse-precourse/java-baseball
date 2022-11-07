db18c8738a43df872bd454a0bd348950a6e3a791
qdeu22 <qdeu22@gmail.com>
2022-11-07 21:57:31 +0900

    refactor(BaseballGameController): 재시작 코드 수정

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -4,6 +4,7 @@ import baseball.model.ComputerRandomNumber;
 import baseball.model.UserBaseballNumber;
 import baseball.utils.NumberCalculator;
 import baseball.view.InputView;
+import baseball.view.ResultView;
 
 public class BaseballGameController {
     private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
@@ -11,7 +12,9 @@ public class BaseballGameController {
 
     public static void start() {
         System.out.println(GAME_START);
-        startGame();
+        do {
+            startGame();
+        } while (restartGame());
     }
 
     private static void startGame() {
@@ -31,6 +34,15 @@ public class BaseballGameController {
             strike = numberCalculator.getStrike();
             ball = numberCalculator.getBall();
 
+            ResultView.resultPrint(strike, ball);
+
         } while (strike != THREE_COUNT);
+
+        ResultView.endGameMessage();
+
+    }
+
+    private static boolean restartGame() {
+        return InputView.restartChoiceNumber();
     }
 }
