108680263afdf3a5eb151e8ae4d30d4762bca30e
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-07 15:28:57 +0900

    feat: Add startGame function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,23 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        startGame();
+    }
+
+    /**
+     * function to start a number baseball game.
+     */
+    public static void startGame() {
+        List<Integer> computer = generateAnswer();
+        boolean find = false;
+
+        while(!find) {
+            List<Integer> user = getInputAsList();
+            find = computer.equals(user);
+        }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     /**
