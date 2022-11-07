c31128117079c8ec0bbd0335b2bd3ab256829a03
jsc1014 <wotlr5209@gmail.com>
2022-11-07 22:40:23 +0900

    feat : 게임 종료 3개의 숫자를 모두 맞출시 게임 종료 출력. 1 입력시 게임 재시작, 2 입력시 게임 종료.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -142,4 +142,13 @@ public class Application {
 
     }
 
+    private static String gameFinish(){
+        String finishCode = "";
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        finishCode = Console.readLine();
+
+        return finishCode;
+    }
 }
