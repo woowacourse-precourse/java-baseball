b4350757bf557f31b10e8080f518a1b1cc4b584b
wonseokjee <wsji9404@gmail.com>
2022-11-07 17:18:49 +0900

    Test: "이동 GameTest -> ApplicationTest"
    테스트를 분리하는 것에 큰 이득이 없어서 두개의 테스트를 합침

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,6 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -31,5 +34,76 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+    Game game = new Game();
+    @Test
+    void randomPick_출력_size_동일한지_확인() {
+        /*랜덤값이 나중에 들어오므로 테스트할 때마다 바뀐다. 그래서 given값과 size가 3으로 동일한지 확인*/
+        /*given*/
+        List<Integer> givenNumber = new ArrayList<>();
+        givenNumber.add(1);
+        givenNumber.add(2);
+        givenNumber.add(3);
+
+        /*when*/
+        List<Integer> testGame = game.randomPick();
+
+        /*then*/
+        assertThat(testGame.size()).isEqualTo(givenNumber.size());
+    }
+
+    @Test
+    void inputNumber() {
+        /*given*/
+        List<Integer> givenNumber = new ArrayList<>();
+        givenNumber.add(2);
+        givenNumber.add(4);
+        givenNumber.add(6);
+
+        /*when*/
+
+        /*then*/
+    }
+
+    @Test
+    void checkException() {
+        /*given*/
+        /*when*/
+        /*then*/
+    }
+
+    @Test
+    void strike() {
+        /*given*/
+        /*when*/
+        /*then*/
+    }
+
+    @Test
+    void ball() {
+        /*given*/
+        /*when*/
+        /*then*/
+    }
+
+    @Test
+    void counting() {
+        /*given*/
+        /*when*/
+        /*then*/
+    }
+
+    @Test
+    void allStrike() {
+        /*given*/
+        /*when*/
+        /*then*/
+    }
+
+    @Test
+    void answer() {
+        /*given*/
+        /*when*/
+        /*then*/
     }
 }
