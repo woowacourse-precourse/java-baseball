58181e71641b8729c23939879b5c53943a335f20
CHAYEONIL <jeay123@naver.com>
2022-11-08 02:42:37 +0900

    Refactor : 필요 없는 구문 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,10 @@
 package baseball;
 
-import baseball.controller.baseballController;
-
-import java.util.List;
+import baseball.controller.BaseballController;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        baseballController.run();
+        BaseballController.run();
     }
 }
