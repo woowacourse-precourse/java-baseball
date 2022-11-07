0e27b4d59a3db7d56986a3e049a92aea28e7f39b
KimJaeSung <behappy0916@naver.com>
2022-11-07 15:10:44 +0900

    feat: 사용자가 입력받은 숫자를 List<Integer>로 바꾸어주는 기능 구현

--- src/main/java/baseball/model/User.java
+++ src/main/java/baseball/model/User.java
@@ -3,6 +3,7 @@ package baseball.model;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class User {
@@ -11,6 +12,10 @@ public class User {
     public User() {
         this.numbers = new ArrayList<>();
     }
-
+    public List<Integer> parseNumbers(String numbers) {
+        return Arrays.stream(numbers.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+    }
 
 }
