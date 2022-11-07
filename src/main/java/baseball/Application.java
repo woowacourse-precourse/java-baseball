c7039125ff2ad78f1c941914186021116bb361f5
GraphTree <graphnodesed@gmail.com>
2022-11-07 15:39:58 +0900

    feat : 플레이어의 답변이 올바른 형식인지 체크

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,13 +14,32 @@ public class Application {
     public static List<Integer> createAnswer() {
         List<Integer> answerNumbers = new ArrayList<>();
 
-        for (int i = 0; i < 3; i++) {
+
+        while(answerNumbers.size() < 3) {
             int number = Randoms.pickNumberInRange(0, 9);
             if (!answerNumbers.contains(number)) {
                 answerNumbers.add(number);
             }
         }
         return answerNumbers;
+    }
+
+
+
+    public static boolean validateInput(List<Integer> inputList) {
+        List<Integer> allowedInputNumber = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        Set<Integer> inputSet = new HashSet<>(inputList);
+
+        for (Integer eachInputNumber : inputList) {
+            if (!allowedInputNumber.contains(eachInputNumber)) {
+                throw new IllegalArgumentException();
+            } else if (inputList.size() != inputSet.size()) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return true;
 
     }
+
 }
