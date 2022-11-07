a5ee8133286a36eab1f6bd868ce64c6f56631b19
hwiyeon0831 <aab555586@gmail.com>
2022-11-08 08:12:40 +0900

    feat() : 사용자 정보를 담는 객체 추가 및 기능 구현
    User 객체 추가 및 기능 구현
    - 사용자 입력 숫자를 담는 리스트 선언
    - 스트라이크, 볼 판단을 위한 변수 선언
    - 사용자 입력 값에 대한 유효성 검사 기능 구현
    - 스트라이크, 볼에 대한 판단 기능 구현

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,49 @@
+package baseball;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class User {
+	private List<String> numberList;
+	private int ball;
+	private int strike;
+
+	public User(String number) {
+		validateNumberLength(number);
+		this.numberList = Arrays.asList(number.trim().split(""));
+		validateNumberList(this.numberList);
+		this.ball = 0;
+		this.strike = 0;
+	}
+
+	public int getBall() {
+		return ball;
+	}
+
+	public int getStrike() {
+		return strike;
+	}
+
+	private void validateNumberLength(String number) {
+		if(number.trim().length() != 3) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	private void validateNumberList(List<String> numberList) {
+		if(numberList.get(0).equals("0")) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public void calculateBallAndStrike(List<String> opponentNumberList) {
+		for(String number : numberList) {
+			if(numberList.indexOf(number) == opponentNumberList.indexOf(number)) {
+				strike++;
+			}
+			if(opponentNumberList.indexOf(number) != -1 && numberList.indexOf(number) != opponentNumberList.indexOf(number)) {
+				ball++;
+			}
+		}
+	}
+}
