3dbc6702b877e362ee17203f82a0ade227efeeba
KNH <nh2004@naver.com>
2022-11-08 01:45:06 +0900

    feat(Application) : 프로그램 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.Input.Input;
+import baseball.game.Game;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IllegalArgumentException {
+        Game game = new Game(new Input());
+        game.start();
     }
 }
