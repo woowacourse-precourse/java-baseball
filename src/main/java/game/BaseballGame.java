9e9ca5f8b0f2e60aab40e76d2330e56247f76874
ho2n <jungho2n@gmail.com>
2022-11-07 15:59:46 +0900

    feat: Print 클래스 정의

--- src/main/java/game/BaseballGame.java
+++ src/main/java/game/BaseballGame.java
@@ -3,24 +3,26 @@ package game;
 import camp.nextstep.edu.missionutils.Console;
 import error.Examine;
 import game.player.Computer;
+import print.Print;
 
 public class BaseballGame {
 	private Computer computer;
 
 	public void ready() throws IllegalArgumentException {
-		System.out.println("숫자 야구 게임을 시작합니다.");
+		Print.printGettingStart();
 
 		do {
 			this.computer = new Computer();
 			roundWithComputer();
-			printAllStrike();
+			Print.printGameOverAndAfterRound();
 		} while (again());
+
 	}
 
 	private void roundWithComputer() throws IllegalArgumentException {
 		String userNumbers = "";
 		do {
-			System.out.println("숫자를 입력해주세요 : ");
+			Print.printInputNumbers();
 			userNumbers = Console.readLine();
 			Examine.hasDifferentNumbers(userNumbers);
 		} while (!computer.computeAndPrintResult(userNumbers));
@@ -29,10 +31,5 @@ public class BaseballGame {
 		String str = Console.readLine();
 		Examine.isOneOrTwo(str);
 		return Examine.isOneOrTwo(str) == 1;
-	}
-
-	private void printAllStrike() {
-		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 	}
 }
---
+++ src/main/java/print/Print.java
@@ -0,0 +1,21 @@
+package print;
+
+public class Print {
+    public static final String GETTING_START = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUMBERS = "숫자를 입력해주세요 : ";
+    public static final String GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String AFTER_ROUND = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static void printGettingStart() {
+        System.out.println(GETTING_START);
+    }
+
+    public static void printInputNumbers() {
+        System.out.println(INPUT_NUMBERS);
+    }
+
+    public static void printGameOverAndAfterRound() {
+        System.out.println(GAME_OVER);
+        System.out.println(AFTER_ROUND);
+    }
+}
