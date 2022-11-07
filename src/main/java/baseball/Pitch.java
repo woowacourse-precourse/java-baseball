8267c643b027a77f2e928ce98ec40514b311e829
vcho1958 <gusckd57@gmail.com>
2022-11-07 09:02:32 +0900

    feat(core): Pitch.java getNumber 메소드 추가 일등급 콜렉션에 요소를 가져오는 메소드 추가

--- src/main/java/baseball/Pitch.java
+++ src/main/java/baseball/Pitch.java
@@ -7,8 +7,16 @@ import java.util.List;
 
 public class Pitch {
 
-    List<Integer> pitchNumbers = new ArrayList<>();
+    private List<Integer> pitchNumbers = new ArrayList<>();
 
+    /**
+     * 입력된 값에서 특정 자리의 값을 반환합니다.
+     * @param index 인덱스
+     * @return 해당 인덱스의 값
+     */
+    public int getNumber(int index){
+        return pitchNumbers.get(index);
+    }
     /**
      * 사용자에게 입력을 받은 수를 저장합니다.
      * @throws IllegalArgumentException 개수가 3개가 아닐 때
