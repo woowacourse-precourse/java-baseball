26918a118bb0a4e38bd34a144d645285be90dd8c
Gyu-Bin-Yoon <gyubinyoon99@gmail.com>
2022-11-07 14:36:46 +0900

    feat : print game finish message when user hit 3 strike

--- src/main/java/game/GameProgress.java
+++ src/main/java/game/GameProgress.java
@@ -24,7 +24,7 @@ public class GameProgress {
             String resultMessage = checkResult(resultList);
             System.out.println(resultMessage);
         }
-
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     private List<Integer> getUserNumber() {
