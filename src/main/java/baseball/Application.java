5494ba3b0f77afda7a26b280cd3a2af30991b13a
jissssoo1717 <jisoolee7101@gmail.com>
2022-11-07 16:39:47 +0900

    feat: 게임 재시작시 사용자 입력에 따라 1이면 값 초기화하고 다시 사용자 세 자리 숫자 입력받도록 while문에 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,70 +7,18 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        String inputStr = Console.readLine();
+        GameSystem game = new GameSystem();
 
-        PlayerInput player = new PlayerInput();
-        Computer computer = new Computer();
+        game.GameStart();
 
-        List<Integer> numOfComputer = computer.GenerateRandomThreeNumber();
-        List<Integer> numOfPlayer = player.ReturnPlayerNum(inputStr);
+        while(game.GameStartAgain()) {
+            game.initValue();
 
-        System.out.println("Computer's number: " + numOfComputer);
-        System.out.println("Player's number: " + numOfPlayer);
-
-        int numStrikes = CheckStrikes(numOfComputer, numOfPlayer);
-        int numBalls = CheckBalls(numOfComputer, numOfPlayer);
-
-        ReturnScore(numStrikes, numBalls);
-    }
-
-    public static int CheckStrikes(List<Integer> computer, List<Integer> player) {
-        int strikes = 0;
-
-        for(int i = 0; i < computer.size(); i++) {
-            if(computer.get(i).equals(player.get(i))) {
-                strikes++;
-            }
+            game.GamePlay();
         }
 
-        return strikes;
     }
 
-    public static int CheckBalls(List<Integer> computer, List<Integer> player) {
-        int balls = 0;
 
-        for(int i = 0; i < computer.size(); i++) {
-
-            int intPlayer = player.get(i);
-
-            if(computer.contains(intPlayer)) {
-                balls++;
-            }
-        }
-
-        return balls;
-    }
-
-    public static boolean CheckNothing(int strike, int ball) {
-        if(strike == 0 && ball == 0) {
-            System.out.println("낫싱");
-
-            return false;
-        }
-
-        return true;
-    }
-
-    public static boolean ReturnScore(int strike, int ball) {
-        if(!CheckNothing(strike, ball)) {
-            return false;
-        }
-
-        ball -= strike;
-        System.out.println("Strike: " + strike);
-        System.out.println("Ball: " + ball);
-
-        return true;
-    }
 
 }
