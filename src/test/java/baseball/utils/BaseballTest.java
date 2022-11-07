98a3a5854774e5093b836f20f162fb6df0afbc58
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-11-07 12:47:28 +0900

    test : create BaseballTest

---
+++ src/test/java/baseball/utils/BaseballTest.java
@@ -0,0 +1,47 @@
+package baseball.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class BaseballTest {
+
+    @Test
+    void getBaseballNumberTest() {
+        String input = "123";
+        Baseball player = Baseball.createPlayer(input);
+        assertThat(player.getBaseballNumber()).isEqualTo(Arrays.asList(1,2,3));
+    }
+
+    @Test
+    void createComputerTest() {
+        Baseball computer = Baseball.createComputer();
+        List<Integer> computerBaseballNumber = computer.getBaseballNumber();
+
+        assertThat(computerBaseballNumber.size()).isEqualTo(3);
+    }
+
+    @Test
+    void createPlayerTest() {
+        String input1 = "123";
+        String input2 = "1234";
+        String input3 = "1233";
+        String input4 = "12";
+
+        assertThat(Baseball.class).isEqualTo(Baseball.createPlayer(input1).getClass());
+
+        assertThatThrownBy(() -> Baseball.createPlayer(input2))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> Baseball.createPlayer(input3))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> Baseball.createPlayer(input4))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
+}
