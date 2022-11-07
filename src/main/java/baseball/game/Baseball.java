1c674148b893b5d55f483605783adfb7b8144865
SeongbinPark <binni0515@gmail.com>
2022-11-07 19:16:32 +0900

    feat(Baseball): get computerNumbers, userNumbers from util

--- src/main/java/baseball/game/Baseball.java
+++ src/main/java/baseball/game/Baseball.java
@@ -1,6 +1,33 @@
 package baseball.game;
 
+import static baseball.util.ComputerUtil.getComputerNumbers;
+import static baseball.util.UserUtil.getUserNumbers;
+
+import java.util.List;
+
 public class Baseball {
+
+    private int strikeCount;
+    private int ballCount;
+
     public void proceedGame() {
+        while (true) {
+            startGame();
+        }
+    }
+
+    private void startGame() {
+        List<Integer> computerNumbers = getComputerNumbers();
+
+        while (true) {
+            strikeCount = 0;
+            ballCount = 0;
+
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            System.out.print("숫자를 입력해주세요 : ");
+
+            List<Integer> userNumbers = getUserNumbers();
+
+        }
     }
 }
