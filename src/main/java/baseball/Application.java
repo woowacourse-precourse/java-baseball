18fc61ab8086bf164d3290b748a4a46de715996d
jihoon-gh <jihoon806@gmail.com>
2022-11-08 01:21:04 +0900

    FEAT - generate random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,40 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Application {
+    static Scanner sc = new Scanner(System.in);
+    static Integer restartCounter;
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        do{
+            List<Integer> computer = randomNumberGenerator();
+
+
+
+
+
+
+            restartCounter  = sc.nextInt();
+        }while(restartCounter==2);
+    }
+
+    public static List<Integer> randomNumberGenerator(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> temp = new ArrayList<>();
+        while (temp.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!temp.contains(randomNumber)) {
+                temp.add(randomNumber);
+            }
+        }
+        System.out.println("숫자를 입력해주세요 : ");
+        String input = sc.nextLine();
+        String[] sc =
+
+        return temp;
     }
 }
---
+++ src/test/java/baseball/BaseBallTest.java
@@ -0,0 +1,42 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BaseBallTest {
+
+    @Test
+    @DisplayName("랜덤 숫자 생성 테스트")
+    public void randomNumbersGeneratorTest(){
+        //given
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        //when
+        int checker = 0;
+        for(Integer a : computer){
+            if(a<1||a>9) checker++;
+        }
+        if(computer.size()>3) checker++;
+
+        //then
+        assertThat(checker).isEqualTo(0);
+
+    }
+
+
+
+
+
+}
