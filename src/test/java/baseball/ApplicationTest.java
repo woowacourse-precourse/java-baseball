22b44dfc9e300acbb32201f40a8f895c43aedb38
Polite-Jehu <werber112@kyonggi.ac.kr>
2022-11-07 23:47:14 +0900

    refactor: remove garbage code

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,8 +1,6 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
