50914b6be6f9f4d1d4f9d31c64ec8f461e876332
이건회 <rg970604@naver.com>
2022-11-08 04:38:39 +0900

    style(JudgementRule.java) : 매직넘버 추가

--- src/main/java/baseball/rule/JudgementRule.java
+++ src/main/java/baseball/rule/JudgementRule.java
@@ -6,10 +6,11 @@ import baseball.domain.Numbers;
 public class JudgementRule {
 
     private static final int REGULAR_NUMBERS_SIZE = 3;
+    private static final int NO_COUNT = 0;
 
     public static BallCount makeBallCounts(Numbers computerNumbers, Numbers userNumbers) {
-        int strikes = 0;
-        int balls = 0;
+        int strikes = NO_COUNT;
+        int balls = NO_COUNT;
         for (int i = 0; i < REGULAR_NUMBERS_SIZE; i++) {
             if (computerNumbers.numbers.get(i).equals(userNumbers.numbers.get(i))) {
                 strikes += 1;
