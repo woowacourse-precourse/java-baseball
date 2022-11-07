2a0a65c94494a920d18f9da7f1d2a10e1e511885
kytmt <tlsrka649@naver.com>
2022-11-08 07:25:29 +0900

    feat(Application 구현): Application 구현
    숫자 야구 게임 Application 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+
+	public static void main(String[] args) {
+		Core core = new Core();
+
+		View.start();
+		core.init();
+
+		while (core.start) {
+			View.waitInput();
+			core.user.inputNum();
+			core.checkStrikeBall();
+			core.user.calResult();
+			View.result(core.user.getResult());
+
+			if (core.user.strike == 3) {
+				View.finish();
+				core.checkRestart();
+			}
+			core.user.init();
+		}
+	}
 }
