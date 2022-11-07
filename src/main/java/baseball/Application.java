fd1d1fc3a0e199d434f8860f10639b52613dc436
xortmd <tmd2166@naver.com>
2022-11-07 23:57:43 +0900

    게임 시작 메소드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -95,6 +95,23 @@ public class Application {
         }
     }
 
+    public static void gameStart() {
+        int end = 0;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(end != 2) {
+            List<Integer> comNum = makeComNum();
+
+            playBaseball(comNum);
+
+            System.out.println(NUM_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            end = Integer.parseInt(Console.readLine());
+            if(end != 1 && end != 2) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
     }
