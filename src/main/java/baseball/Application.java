7047402ac4d711d0a7e0c73645486898abc97ebc
gumgu <bienlee@naver.com>
2022-11-07 23:16:10 +0900

    Style: 불필요한 코드 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-import com.sun.nio.sctp.IllegalReceiveException;
 
 import java.util.*;
 
@@ -19,6 +18,7 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         String randomNum = makeRandomNum();
+        System.out.println("randomNum :" +randomNum);
 
         while(result.get(0)!=3) {
             System.out.println("숫자를 입력해 주세요. :");
