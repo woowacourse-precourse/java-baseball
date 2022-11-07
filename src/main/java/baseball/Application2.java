f4862b720eff25d8cf025539ea8e80d98f9b878c
making-a-scene <4936joo@naver.com>
2022-11-07 13:52:11 +0900

    change implementation by using String data instead of int.

--- src/main/java/baseball/Application2.java
+++ src/main/java/baseball/Application2.java
@@ -3,6 +3,8 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.*;
+
 public class Application {
 
     public static int threeStrikesIdentifier = 0;
@@ -13,59 +15,75 @@ public class Application {
 
     public static void processBaseballGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        String computer = getComputersValue();
+        List<Integer> computer = getComputersValue();
 
         threeStrikesIdentifier = 0;
         while (threeStrikesIdentifier == 0) {
             System.out.print("숫자를 입력해 주세요 : ");
-            String user = validateUserInput(Console.readLine());
-            countBallStrike(computer, user);
+            String response = Console.readLine();
+            countBallStrikeFromCurrentInput(computer, Integer.parseInt(response));
         }
     }
 
-    public static String getComputersValue() {
-        StringBuilder stringBuilder = new StringBuilder();
+    public static List<Integer> getComputersValue() {
+        List<Integer> computer = new ArrayList<>();
 
-        while (stringBuilder.length() < 3) {
-            String randInt = String.valueOf(Randoms.pickNumberInRange(1, 9));
-            if (!stringBuilder.toString().contains(randInt)) {
-                stringBuilder.append(randInt);
+        while (computer.size() < 3) {
+            int randomInt = Randoms.pickNumberInRange(1, 9);
+            if (randomInt != 0 && !computer.contains(randomInt)) {
+                computer.add(randomInt);
             }
         }
-        return stringBuilder.toString();
+        return computer;
     }
 
-    public static String validateUserInput(String num) {
+
+    public static List<Integer> getPlaceValuesFromNum(int num) {
         int count = 0;
-        for (int idx = 0; idx < num.length() ; idx++, count++) {
-            if (num.charAt(idx) == '0') {
+        List<Integer> placeValuesFromNum = new ArrayList<>();
+
+        for (int temp = num; temp > 0; temp /= 10, count++) {
+            if (temp % 10 == 0) {
                 throw new IllegalArgumentException("자릿수에 0이 존재할 수 없습니다.");
             }
+            placeValuesFromNum.add(temp % 10);
         }
-        if (count != 3) {
-            throw new IllegalArgumentException("세 자릿수를 입력해주세요.");
-        }
-        if (num.charAt(0) == num.charAt(1) || num.charAt(1) == num.charAt(2) || num.charAt(0) == num.charAt(2)) {
+
+        Set<Integer> placeValuesSet = new HashSet<>(placeValuesFromNum);
+        // Set은 중복을 허용하지 않기 때문에 List을 Set으로 저장했을 때 이전과 size가 달라졌다면 중복이 존재한다는 뜻입니다.
+        if (placeValuesSet.size() != placeValuesFromNum.size()) {
             throw new IllegalArgumentException("세 자릿수 각각은 서로 달라야 합니다.");
         }
-        return num;
+
+        if (count == 3) {
+            int temp = placeValuesFromNum.get(2);
+            placeValuesFromNum.set(2, placeValuesFromNum.get(0));
+            placeValuesFromNum.set(0, temp);
+            return placeValuesFromNum;
+        } else {
+            throw new IllegalArgumentException("세 자릿수를 입력해주세요.");
+        }
+
     }
 
-    public static void countBallStrike(String computer, String user) {
+    public static void countBallStrikeFromCurrentInput(List<Integer> computersInputList, int user) {
+        List<Integer> usersInputList = getPlaceValuesFromNum(user);
+
         int strike = 0;
         int ball = 0;
 
-        for (int idx = 0; idx < computer.length(); idx++) {
-            String computersValue = String.valueOf(computer.charAt(idx));
-            String usersValue = String.valueOf(user.charAt(idx));
+        for (int idx = 0; idx < computersInputList.size(); idx++) {
+            int computersValue = computersInputList.get(idx);
+            int usersValue = usersInputList.get(idx);
 
-            if (computersValue.equals(usersValue)) {
+            if (computersValue == usersValue) {
                 strike++;
-            } else if (computer.contains(usersValue)) {
+            } else if (computersInputList.contains(usersValue)) {
                 ball++;
             }
         }
-        printResult(strike, ball);
+
+    printResult(strike, ball);
     }
 
     private static void printResult(int strike, int ball) {
@@ -92,11 +110,12 @@ public class Application {
 
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String response = Console.readLine();
-        switch (response) {
-            case "1":
+
+        switch (Integer.parseInt(response)) {
+            case 1:
                 processBaseballGame();
                 break;
-            case "2":
+            case 2:
                 return;
             default:
                 throw new IllegalArgumentException("1 또는 2만 입력 가능합니다.");
--- src/test/java/baseball/BaseballGameUnitTest.java
+++ src/test/java/baseball/BaseballGameUnitTest.java
@@ -1,113 +1,71 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class BaseBallGameUnitTest {
-
-    @Test
-    public void getPlaceValuesTest() {
-
-        // given
-        int [] testCases = {285, 153, 895, 706, 140, 6891, 23};
-
-        // when
-        List<Integer> result0 = Application.getPlaceValuesFromNum(testCases[0]);
-        List<Integer> result1 = Application.getPlaceValuesFromNum(testCases[1]);
-        List<Integer> result2 = Application.getPlaceValuesFromNum(testCases[2]);
-
-
-        // then
-        assertThat(result0.get(0)).isEqualTo(5);
-        assertThat(result0.get(1)).isEqualTo(8);
-        assertThat(result0.get(2)).isEqualTo(2);
-        assertThat(result1.get(0)).isEqualTo(3);
-        assertThat(result1.get(1)).isEqualTo(5);
-        assertThat(result1.get(2)).isEqualTo(1);
-        assertThat(result2.get(0)).isEqualTo(5);
-        assertThat(result2.get(1)).isEqualTo(9);
-        assertThat(result2.get(2)).isEqualTo(8);
-
-
-        assertThrows(IllegalArgumentException.class,
-                () -> {Application.getPlaceValuesFromNum(testCases[3]);});
-        assertThrows(IllegalArgumentException.class,
-                () -> {Application.getPlaceValuesFromNum(testCases[4]);});
-        assertThrows(IllegalArgumentException.class,
-                () -> {Application.getPlaceValuesFromNum(testCases[5]);});
-        assertThrows(IllegalArgumentException.class,
-                () -> {Application.getPlaceValuesFromNum(testCases[6]);});
-
-    }
-
+public class BaseballGameUnitTest {
     @Test
     public void getResultFromCurrentInputTest() {
+        System.out.println("getResultFromCurrentInputTest Result");
         // given
-        int [] testUsers = new int[9];
-        testUsers[0] = 279; // 낫싱
-        testUsers[1] = 486; // 3볼
-        testUsers[2] = 683; // 2볼
-        testUsers[3] = 431; // 1볼
-        testUsers[4] = 684; // 2볼 1스트라이크
-        testUsers[5] = 842; // 1볼 1스트라이크
-        testUsers[6] = 367; // 1스트라이크
-        testUsers[7] = 874; // 2스트라이크
-        testUsers[8] = 864; // 3스트라이크
+        String [] testUsers = new String[9];
+        testUsers[0] = "279"; // 낫싱
+        testUsers[1] = "486"; // 3볼
+        testUsers[2] = "683"; // 2볼
+        testUsers[3] = "431"; // 1볼
+        testUsers[4] = "684"; // 2볼 1스트라이크
+        testUsers[5] = "842"; // 1볼 1스트라이크
+        testUsers[6] = "367"; // 1스트라이크
+        testUsers[7] = "874"; // 2스트라이크
+        testUsers[8] = "864"; // 3스트라이크
 
         // when
-        List<Integer> computer = new ArrayList<>();
-        computer.add(4);
-        computer.add(6);
-        computer.add(8);
-        computer.add(864);
+        String computer = "864";
 
         // then
-        for(int user : testUsers) {
-            Application.ballStrikeCountFromCurrentInput(computer, user);
+        for (int idx = 0; idx < testUsers.length; idx++) {
+            Application.countBallStrike(computer, testUsers[idx]);
         }
+        // countBallStrike() 최하단의 printResult()를 주석 처리한 후 테스트 필요.
 
     }
 
     @Test
     public void getRandomIntTest() {
-        List<Integer> testList = Application.getComputersValue();
-        for(int value: testList) {
-            System.out.println(value);
+        System.out.println("getRandomInt Result");
+        int count = 0;
+        while (count < 5) {
+            System.out.println(Application.getComputersValue());
+            count++;
         }
 
-        System.out.println("========================================");
+    }
 
-        List<Integer> testList2 = Application.getComputersValue();
-        for(int value : testList2) {
-            System.out.println(value);
-        }
+    @Test
+    public void validateUserInputTest() {
+        String[] testStrings = {"123", "504", "15", "5316", "336"};
+        assertThat(Application.validateUserInput(testStrings[0])).isEqualTo("123");
+        assertThrows(IllegalArgumentException.class, () -> Application.validateUserInput(testStrings[1]));
+        assertThrows(IllegalArgumentException.class, () -> Application.validateUserInput(testStrings[2]));
+        assertThrows(IllegalArgumentException.class, () -> Application.validateUserInput(testStrings[3]));
+        assertThrows(IllegalArgumentException.class, () -> Application.validateUserInput(testStrings[4]));
+    }
 
-        System.out.println("========================================");
+    @Test
+    public void charAtTest() {
+        String testString = "15328";
+        assertThat(testString.contains("2")).isEqualTo(true);
+        assertThat(testString.contains("9")).isEqualTo(false);
+        assertThat(testString.charAt(0)).isEqualTo('1');
+        assertThat(testString.charAt(1)).isEqualTo('5');
+        assertThat(testString.charAt(2)).isEqualTo('3');
+        assertThat(testString.charAt(3)).isEqualTo('2');
+        assertThat(testString.charAt(4)).isEqualTo('8');
 
-        List<Integer> testList3 = Application.getComputersValue();
-        for(int value : testList3) {
-            System.out.println(value);
-        }
-
-        System.out.println("========================================");
-
-        List<Integer> testList4 = Application.getComputersValue();
-        for(int value : testList4) {
-            System.out.println(value);
-        }
-
-        System.out.println("========================================");
-
-        List<Integer> testList5 = Application.getComputersValue();
-        for(int value : testList5) {
-            System.out.println(value);
-        }
     }
 
 }
