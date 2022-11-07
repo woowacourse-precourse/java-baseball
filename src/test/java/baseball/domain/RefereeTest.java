02425851e1c1e027a6126dd33afaff947cae4daf
kim0914 <kosa0914@naver.com>
2022-11-07 16:09:37 +0900

    test(Referee): 게임결과 객체 생성에 의한 테스트 추가

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -1,7 +1,5 @@
 package baseball.domain;
 
-import baseball.domain.GameResult;
-import baseball.domain.Referee;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
@@ -90,6 +88,63 @@ class RefereeTest {
 
         // when
         GameResult gameResult = referee.judge(answer, userNumber);
+
+        // then
+        assertThat(gameResult.getStrikeCount()).isEqualTo(expectedStrike);
+        assertThat(gameResult.getBallCount()).isEqualTo(expectedBall);
+    }
+
+    @Test
+    void 판정에_따른_경기결과_객체를_생성한다() {
+        // given
+        Referee referee = new Referee();
+
+        // when
+        GameResult gameResult = referee.judge("123", "123");
+
+        // then
+        assertThat(gameResult).isNotNull();
+    }
+
+    @Test
+    void 판정에_의해_생성된_경기결과는_스트라이크_볼_개수를_가진다1() {
+        // given
+        Referee referee = new Referee();
+        int expectedStrike = 1;
+        int expectedBall = 2;
+
+        // when
+        GameResult gameResult = referee.judge("123", "132");
+
+        // then
+        assertThat(gameResult.getStrikeCount()).isEqualTo(expectedStrike);
+        assertThat(gameResult.getBallCount()).isEqualTo(expectedBall);
+    }
+
+    @Test
+    void 판정에_의해_생성된_경기결과는_스트라이크_볼_개수를_가진다2() {
+        // given
+        Referee referee = new Referee();
+        int expectedStrike = 3;
+        int expectedBall = 0;
+
+        // when
+        GameResult gameResult = referee.judge("123", "123");
+
+        // then
+        assertThat(gameResult.getStrikeCount()).isEqualTo(expectedStrike);
+        assertThat(gameResult.getBallCount()).isEqualTo(expectedBall);
+    }
+
+    @Test
+    void 판정에_의해_생성된_경기결과는_스트라이크_볼_개수를_가진다3() {
+        // given
+        Referee referee = new Referee();
+        int expectedStrike = 0;
+        int expectedBall = 3;
+
+        // when
+        GameResult gameResult = referee.judge("123", "312");
 
         // then
         assertThat(gameResult.getStrikeCount()).isEqualTo(expectedStrike);
