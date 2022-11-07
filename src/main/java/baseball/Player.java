2f4f88ab2120390190815c63df8a260b7ecd250f
delaylee <delaylee219@gmail.com>
2022-11-07 20:43:29 +0900

    feat: 사용자 입력 예외처리 구현
    사용자 입력 예외처리 IllegalArgumentException 발생 처리:
    - 숫자 입력이 아닌 경우 예외처리
    - 숫자 입력이 3자리가 아닌 경우 예외처리
    - 입력이 빈 값인 경우 예외처리

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -14,7 +14,28 @@ public class Player {
         System.out.print(inputMessage);
         String input = Console.readLine();
 
+        inputValidationCheck(input);
         this.input = input;
+
+    public void inputValidationCheck(String input) {
+        //IllegalArgumentException 발생 처리
+        //입력이 없는 경우
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("입력을 해야합니다");
+        }
+        //3자리가 아닌 경우
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3자리만 입력 가능합니다");
+        }
+
+        //숫자 입력이 아닌 경우
+        try {
+            Integer.parseInt(input);
+        }
+        catch (NumberFormatException ex) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다");
+        }
+    }
     }
 
     public String getInput() {
