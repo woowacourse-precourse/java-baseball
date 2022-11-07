fb5d0bbe234b6ebff7d761ce481543c505e601e4
woosung1223 <dntjd991223@naver.com>
2022-11-07 18:27:24 +0900

    test(RandomNumGeneratorTest): 랜덤값으로 3자리 수를 만드는 기능 테스트

---
+++ src/test/java/baseball/utils/RandomNumGeneratorTest.java
@@ -0,0 +1,22 @@
+package baseball.utils;
+
+import org.junit.jupiter.api.Test;
+
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RandomNumGeneratorTest {
+    @Test
+    void 랜덤값으로_3자리수를_구성하는지_체크() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThat(RandomNumGenerator.makeDistinctRandomNumWithSize(3))
+                            .isEqualTo(135);
+                    assertThat(RandomNumGenerator.makeDistinctRandomNumWithSize(3))
+                            .isEqualTo(479);
+                },
+                1, 3, 5, 4, 7, 9
+        );
+    }
+}
