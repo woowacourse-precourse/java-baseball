d84a3ddbe20ab979e27b1efe4ab584efedefcfca
jonghun <jonghuncu@gmail.com>
2022-11-07 18:52:51 +0900

    Feat : inputNumber 와 answerNumber 를 비교하는 기능 및 입력 에외처리 기능 구현 중

--- src/main/java/baseball/printCase.java
+++ src/main/java/baseball/printCase.java
@@ -1,15 +1,15 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class printCase {
-    public static void makePrintCase(List<Integer> answerNumber, List<Integer> inputNumber) {
-//        ArrayList<Integer> strike = new ArrayList<>();
-//        ArrayList<Integer> ball = new ArrayList<>();
+    public static boolean makePrintCase(List<Integer> answerNumber, List<Integer> inputNumber) {
 
         int strikeNum = 0;
         int ballNum = 0;
 
+        try{
         for(int i=0; i<3; i++) {
             if(answerNumber.contains(inputNumber.get(i))) {
                 if(answerNumber.get(i).equals(inputNumber.get(i))) {
@@ -22,10 +22,20 @@ public class printCase {
                  */
             }
         }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.toString());
+        }
 
-        System.out.printf("%d볼 %d스트라이크\n", ballNum, strikeNum);
+        if(strikeNum == 0 && ballNum == 0) {
+            System.out.println("낫싱");
+        } else {
+            System.out.printf("%d볼 %d스트라이크\n", ballNum, strikeNum);
+        }
         if(strikeNum == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            return true;
         }
+        return false;
     }
 }
