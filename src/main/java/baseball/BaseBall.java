af86fcbb59873fc2974b2e5354ffae6a78dbbba3
chunghye98 <57451700+chunghye98@users.noreply.github.com>
2022-11-07 19:28:33 +0900

    fix: 기능1 게임 반복하기 수정

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -1,10 +1,6 @@
 package baseball;
 
-
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import static camp.nextstep.edu.missionutils.Console.*;
 import static camp.nextstep.edu.missionutils.Randoms.*;
@@ -13,67 +9,31 @@ public class BaseBall {
 
     private int strike;
     private int ball;
-    private int nothing;
+    private boolean isRestart;
+    private List<String> randomList;
 
     public BaseBall() {
         this.strike = 0;
         this.ball = 0;
-        this.nothing = 0;
+        this.isRestart = true;
+        this.randomList = new ArrayList<>();
     }
 
     public void playBaseBall() {
+        System.out.println(START_GAME);
+        randomList = makeRandom();
         while (true) {
+            initGame();
             playGame();
             boolean isStrike3 = isStrike3();
             if (isStrike3) {
                 restart();
             }
-        }
-    }
-
-    private void playGame() {
-        String random = makeRandom();
-        String input = makeMyInput();
-        List<String> randomList = getList(random);
-        List<String> inputList = getList(input);
-        checkSameOfRandomAndMy(randomList, inputList);
-    }
-
-    private String makeRandom() {
-        return String.valueOf(pickNumberInRange(111, 999));
-    }
-
-    private String makeMyInput() {
-        String input = readLine();
-        List<String> inputList = getList(input);
-        Set<String> inputSet = new HashSet<>(inputList);
-        if (inputSet.size() != inputList.size()) {
-            throw new IllegalArgumentException("입력값이 서로 다른 3자리의 수가 아닙니다.");
-        }
-        return input;
-    }
-
-    private List<String> getList(String s) {
-        return Arrays.asList(s.split(""));
-    }
-
-    private void checkSameOfRandomAndMy(List<String> randomList, List<String> inputList) {
-        Boolean isSameNumber = false;
-        for (int i = 0; i < randomList.size(); i++) {
-            isSameNumber = inputList.contains(randomList.get(i));
-        }
-        if (isSameNumber == false) {
-            nothing++;
-        }
-        checkSameNumberOfDigits(randomList, inputList);
-    }
-
-    private void checkSameNumberOfDigits(List<String> randomList, List<String> inputList) {
-        for (int i = 0; i < randomList.size(); i++) {
-            if (!randomList.get(i).equals(inputList.get(i))) {
-                ball++;
+            if (this.isRestart == true) {
+                continue;
+            } else {
+                break;
             }
-            strike++;
         }
     }
 
