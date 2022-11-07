609255f500aae1d112634c8139ea942266591296
이경범 <tkdwk567@naver.com>
2022-11-07 16:54:55 +0900

    feat : 마지막 Application 조립
    game.start() 의 boolean 값에 의해 반복한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,12 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        View.start();
+        boolean isFinished;
+        do {
+            Game game = new Game();
+            isFinished = game.start();
+
+        } while (isFinished);
     }
 }
