8c5e50a84a1583b3d5f5c7b8582e0729146f530e
권민정 <22alswjd@naver.com>
2022-11-08 00:31:46 +0900

    feat: 정답 확인 및 힌트 제공
    Computer 클래스에 다음 내용 추가:
    - 게임 시작 시, 랜덤으로 서로 다른 3개의 숫자를 뽑음
    - 플레이어가 정답을 맞출 때까지 숫자를 입력받고 정답을 비교하는 과정을 반복하는 playGame 함수 구현
    - 플레이어의 답이 정답인지 확인하여 적절한 메세지를 출력하고 정답 여부를 true, false로 리턴하는 isRightAnswer 함수 구현
    - 스트라이크와 볼의 갯수를 카운트하여 List에 담아 반환하는 countTheNumberOfStrikeAndBall 함수 구현
    - 플레이어와 컴퓨터 숫자의 i번째 수를 비교하는 비교하여 스트라이크이면 1, 볼이면 2, 둘 다 아니라면 -1을 반환하는 isStrkieOrBall 함수 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,21 +1,83 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.List;
 
 public class Computer {
-    private GamePlayer gamePlayer;
+    private final GamePlayer gamePlayer;
+    private List<Integer> threeNumberByComputer;
+    boolean rightAnswer;
 
     public Computer() {
         this.gamePlayer = new GamePlayer();
+        this.rightAnswer = false;
     }
 
     public void gameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        List<Integer> threeNumbers = askForThreeNumbers();
+        threeNumberByComputer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        playGame();
     }
 
-    List<Integer> askForThreeNumbers() {
+    private void playGame() {
+        do {
+            List<Integer> threeNumbers = askForThreeNumbers();
+            rightAnswer = isRightAnswer(threeNumbers);
+        } while (!rightAnswer);
+    }
+
+    private List<Integer> askForThreeNumbers() {
         System.out.print("숫자를 입력해 주세요 : ");
         return gamePlayer.getThreeNumber();
+    }
+
+    private boolean isRightAnswer(List<Integer> threeNumbers) {
+        List<Integer> strikeAndBall = countTheNumberOfStrikeAndBall(threeNumbers);
+        int strikeCount = strikeAndBall.get(0);
+        int ballCount = strikeAndBall.get(1);
+
+        if (strikeCount == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+
+        if ((strikeCount == 0) && (ballCount == 0)) {
+            System.out.println("낫싱");
+        } else if(strikeCount == 0) {
+            System.out.println(ballCount + "볼");
+        } else if(ballCount == 0) {
+            System.out.println(strikeCount + "스트라이크");
+        } else {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+        return false;
+    }
+
+    private List<Integer> countTheNumberOfStrikeAndBall(List<Integer> threeNumbers) {
+        int strike = 0;
+        int ball = 0;
+
+        for (int i = 0; i < 3; i++) {
+            int currentPlayerNumber = threeNumbers.get(i);
+            int currentComputerNumber = threeNumberByComputer.get(i);
+
+            int resultCode = isStrkieOrBall(currentPlayerNumber, currentComputerNumber);
+            if (resultCode == 1) {
+                strike += 1;
+            } else if (resultCode == 2) {
+                ball += 1;
+            }
+        }
+
+        return List.of(strike, ball);
+    }
+
+    private int isStrkieOrBall(int playerNumber, int computerNumber) {
+        if (playerNumber == computerNumber) {
+            return 1;
+        } else if (threeNumberByComputer.contains(playerNumber)) {
+            return 2;
+        }
+        return -1;
     }
 }
