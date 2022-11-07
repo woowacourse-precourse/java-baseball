38ea1faa719580fc830818481d2a2bebd7fa78ec
miffyKing <whqudghk28@gmail.com>
2022-11-08 00:28:15 +0900

    fix: Remove recursive generating game class.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,10 +2,13 @@ package baseball;
 
 public class Application {
 
-    private static final int FIRST_START_OFGAME = 0;
+    private static final int PLAYAGAINMODE = 2;
 
     public static void main(String[] args) {
         Game game = new Game();
-        game.start(FIRST_START_OFGAME);
+        int playAgainReturn;
+        do {
+            playAgainReturn = game.startAndPlayAgain();
+        } while (playAgainReturn == PLAYAGAINMODE);
     }
 }
