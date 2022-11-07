662b86423314bb126dd3382be26a1b1da544bca2
Your Name <dbsdbxkrzz@naver.com>
2022-11-07 15:40:08 +0900

    chore: 불필요 import 제거

--- src/test/java/baseball/InputTest.java
+++ src/test/java/baseball/InputTest.java
@@ -1,11 +1,6 @@
 package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
