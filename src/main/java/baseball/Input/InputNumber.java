e515fb958e26c1b0607d6f0952b01bfdcc5d69d5
jaehyeon <jhjh0022@naver.com>
2022-11-07 21:42:52 +0900

    style: 줄간격 맞추기

--- src/main/java/baseball/Input/InputNumber.java
+++ src/main/java/baseball/Input/InputNumber.java
@@ -3,14 +3,13 @@ package baseball.Input;
 import camp.nextstep.edu.missionutils.Console;
 
 
-
 public class InputNumber {
-    public int getInputNumber(){
+    public int getInputNumber() {
 
         return Integer.parseInt(Console.readLine());
     }
 
-    public int getRestartAndEndNumber(){
+    public int getRestartAndEndNumber() {
 
         return Integer.parseInt(Console.readLine());
     }
