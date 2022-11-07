aa277ef64fca762f70705040c9c0e372e4950b88
mgOh <issac_oh@naver.com>
2022-11-08 01:43:24 +0900

    feat: 게임 재시작 혹은 프로그램 종료 여부 입력, 처리 기능 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,17 +1,38 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        startGame(new Game());
+        boolean gameRestartWhether = true;
+        while (gameRestartWhether){
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            startGame(new Game());
+            gameRestartWhether = getGameRestartWhether();
+        }
     }
 
 
     static void startGame(Game game){
-
     }
 
+    static boolean getGameRestartWhether(){
+        while (true){
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            final String input = Console.readLine();
 
-
+            if (input.equals("1")){
+                return true;
+            }
+            else if(input.equals("2")){
+                return false;
+            }
+            else{
+                System.out.println("입력값을 다시 확인해주세요.");
+                continue;
+            }
+        }
+    }
 }
