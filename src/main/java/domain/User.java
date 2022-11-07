ca3574304ab3ae3524c9874650c9ce97ecd609b3
Jeonsoyeon <itso22@naver.com>
2022-11-07 23:17:05 +0900

    refactor(User): 필요없는 변수 삭제

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -2,15 +2,12 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class User {
-    public static List<Integer> getUserNumber(String inputNumber){
+    public static List<Integer> getUserNumber(String inputNumber) {
         List<Integer> userNumbers = new ArrayList<>();
-        String[] userNumberArr = inputNumber.split("");
-
-        for(int i=0; i<userNumberArr.length; i++){
-            userNumbers.add(Integer.valueOf(userNumberArr[i]));
+        for (int i = 0; i < 3; i++) {
+            userNumbers.add(Integer.valueOf(inputNumber.split("")[i]));
         }
         return userNumbers;
     }
