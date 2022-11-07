00ebd31aaef15031a02f1d5e8db41f0a431b4f4a
juhee park <jouhi3322@gmail.com>
2022-11-08 00:19:25 +0900

    test : 테스트 추가, 중복코드 메서드화, 리팩토링 된 메서드명 변경

--- src/test/java/baseball/functionTest.java
+++ src/test/java/baseball/functionTest.java
@@ -1,25 +1,53 @@
 package baseball;
 
+import baseball.domain.Computer;
 import baseball.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
+import java.io.*;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 
 public class functionTest {
+
     User user = new User();
     Score score = new Score();
+    Computer computer = new Computer();
+
+    @Test
+    @DisplayName("1.컴퓨터 숫자는 세 자리 이다.")
+    void 컴퓨터_숫자_LengthCheck() {
+        computer.setRandNums();
+        assertEquals(3, computer.getNums().size());
+    }
+
+    @Test
+    @DisplayName("2.컴퓨터 숫자는 모두 다른 세 숫자 이다.")
+    void 컴퓨터_숫자_DistinctCheck() {
+        computer.setRandNums();
+        assertArrayEquals(computer.getNums().toArray(), computer.getNums().stream().distinct().toArray());
+    }
+
+    @Test
+    @DisplayName("3.컴퓨터 숫자는 0~9사이이다.")
+    void 컴퓨터_숫자_RangeCheck() {
+        computer.setRandNums();
+        boolean hasRangeError = false;
+        for (int num : computer.getNums()) {
+            if (num <= 0 || 9 < num) {
+                hasRangeError = true;
+                break;
+            }
+        }
+        assertFalse(hasRangeError);
+    }
 
 
     @Test
-    @DisplayName("유저의 숫자는 1~9사이여야 한다")
+    @DisplayName("1.유저의 숫자는 1~9사이여야 한다")
     void 유저의_숫자_RangeCheck() {
         assertThrows(IllegalArgumentException.class, () -> user.checkingNums("023"));
         assertThrows(IllegalArgumentException.class, () -> user.checkingNums("S23"));
@@ -27,7 +55,7 @@ public class functionTest {
     }
 
     @Test
-    @DisplayName("유저의 숫자는 각각의 다른 숫자여야 한다.")
+    @DisplayName("2.유저의 숫자는 각각의 다른 숫자여야 한다.")
     void 유저의_숫자_DistinctCheck() {
         assertThrows(IllegalArgumentException.class, () -> user.checkingNums("112"));
         assertThrows(IllegalArgumentException.class, () -> user.checkingNums("111"));
@@ -35,7 +63,7 @@ public class functionTest {
     }
 
     @Test
-    @DisplayName("유저의 숫자는 3자리여야 한다.")
+    @DisplayName("3.유저의 숫자는 3자리여야 한다.")
     void 유저의_숫자_LengthCheck() {
         assertThrows(IllegalArgumentException.class, () -> user.checkingNums(""));
         assertThrows(IllegalArgumentException.class, () -> user.checkingNums("1"));
@@ -43,82 +71,103 @@ public class functionTest {
     }
 
     @Test
-    @DisplayName("유저의 인풋값이 정확하게 저장되는지 확인")
-    void 유저의_숫자리스트_확인() {
-        assertEquals(List.of(1, 2, 3), user.inputsToList("123"));
-        assertEquals(List.of(6, 3, 9), user.inputsToList("639"));
+    @DisplayName("1.유저의 결과 계산_Strike")
+    void 유저_결과_계산_Strike() {
+        calculateScore(List.of(2, 5, 8));
+        assertEquals(3, score.getStrikeCnt());
+        assertEquals(0, score.getBallCnt());
     }
 
     @Test
-    @DisplayName("유저와 컴퓨터의 점수를 계산한다")
-    void 유저의_결과를_계산한다() {
-        score.setAnswerNums(List.of(2, 5, 8));
-
-        score.setUserNums(List.of(2, 5, 8));
-        score.countResult();
-        assertEquals(3, score.strikeCnt);
-        assertEquals(0, score.ballCnt);
-
-        score.setUserNums(List.of(5, 2, 3));
-        score.countResult();
-        assertEquals(0, score.strikeCnt);
-        assertEquals(2, score.ballCnt);
-
-        score.setUserNums(List.of(1, 3, 4));
-        score.countResult();
-        assertEquals(0, score.strikeCnt);
-        assertEquals(0, score.ballCnt);
+    @DisplayName("2.유저의 결과 계산_Ball")
+    void 유저_결과_계산_Ball() {
+        calculateScore(List.of(5, 2, 3));
+        assertEquals(0, score.getStrikeCnt());
+        assertEquals(2, score.getBallCnt());
 
     }
 
+    @Test
+    @DisplayName("3.유저의 결과 계산_Nothing")
+    void 유저_결과_계산_Nothing() {
+        calculateScore(List.of(1, 3, 4));
+        assertEquals(0, score.getStrikeCnt());
+        assertEquals(0, score.getBallCnt());
+    }
 
     @Test
     @DisplayName("1.유저와 결과를 출력한다._스트라이크")
-    void 유저의_결과를_출력한다1() {
-        score.setAnswerNums(List.of(2, 5, 8));
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        score.setUserNums(List.of(2, 5, 8));
-        score.countResult();
-        score.showResult();
+    void 유저_결과_출력_Strike() {
+        OutputStream out = getOutputStream();
+        calculateAndShowResult(List.of(2, 5, 8));
         assertEquals("3스트라이크\n", out.toString());
     }
 
     @Test
     @DisplayName("2.유저와 결과를 출력한다._낫싱")
-    void 유저의_결과를_출력한다2() {
-        score.setAnswerNums(List.of(2, 5, 8));
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        score.setUserNums(List.of(3, 6, 7));
-        score.countResult();
-        score.showResult();
+    void 유저_결과_출력_Nothing() {
+        OutputStream out = getOutputStream();
+        calculateAndShowResult(List.of(3, 6, 7));
         assertEquals("낫싱\n", out.toString());
-
-
     }
 
     @Test
     @DisplayName("3.유저와 결과를 출력한다._볼과 스트라이크")
-    void 유저의_결과를_출력한다3() {
-        score.setAnswerNums(List.of(2, 5, 8));
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        score.setUserNums(List.of(2, 8, 3));
-        score.countResult();
-        score.showResult();
+    void 유저_결과_출력_BallAndStrike() {
+        OutputStream out = getOutputStream();
+        calculateAndShowResult(List.of(2, 8, 3));
         assertEquals("1볼 1스트라이크\n", out.toString());
     }
 
     @Test
-    @DisplayName("4.유저와 결과를 출력한다._볼과 스트라이크")
-    void 유저의_결과를_출력한다4() {
-        score.setAnswerNums(List.of(2, 5, 8));
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        score.setUserNums(List.of(8, 2, 5));
-        score.countResult();
-        score.showResult();
+    @DisplayName("4.유저와 결과를 출력한다._볼")
+    void 유저_결과_출력_Ball() {
+        OutputStream out = getOutputStream();
+        calculateAndShowResult(List.of(8, 2, 5));
         assertEquals("3볼\n", out.toString());
     }
+
+    @Test
+    @DisplayName("게임 재실행 여부 확인중 유저 인풋값 에러")
+    void 유저_재실행여부_인풋에러() {
+        setInputStream("3");
+        assertThrows(IllegalArgumentException.class,
+                () -> user.scanWantReplayingGame()
+        );
+        setInputStream("S");
+        assertThrows(IllegalArgumentException.class,
+                () -> user.scanWantReplayingGame()
+        );
+
+        setInputStream("1");
+        assertTrue(user.scanWantReplayingGame());
+
+        setInputStream("2");
+        assertFalse(user.scanWantReplayingGame());
+    }
+
+
+    private void calculateScore(List<Integer> input) {
+        score.setAnswerNums(List.of(2, 5, 8));
+        score.setUserNums(input);
+        score.countResult();
+    }
+
+    private void calculateAndShowResult(List<Integer> input) {
+        score.setAnswerNums(List.of(2, 5, 8));
+        score.setUserNums(input);
+        score.calculateAndShowResult();
+    }
+
+    private OutputStream getOutputStream() {
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        return out;
+    }
+
+    private void setInputStream(String input){
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+    }
+
 }
