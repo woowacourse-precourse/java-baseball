4832aafbd23a4e7e1e957c7a2bd50020f80e2b1b
gusu9 <suhan.ku9@gmail.com>
2022-11-07 15:50:42 +0900

    added player input repeatition and game restart

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,83 +9,106 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
 import java.util.HashSet;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
 //        기능1. 컴퓨터 숫자선정
         int randomNumber = 0;
+        int restart = 1;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        do {
+            int strikeNball = 0;
+            int strike = 0;
+            int ball = 0;
+            int userNumber = 0;
 
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-                System.out.print(randomNumber);
-            }
-        }
-
-//      기능2. 참가자 입력
-        System.out.println("\n숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요: ");
-
-        int userNumber = 0;
-        userNumber = Integer.parseInt(Console.readLine());
-
-        int[] digits = Stream.of(String.valueOf(userNumber).split("")).mapToInt(Integer::parseInt).toArray();
-
-        List<Integer> user = new ArrayList<>();
-
-        for (int order = 0; order <digits.length; order++) {
-            user.add(digits[order]);
-        }
-        Set<Integer> userSet = new HashSet<>(user);
-
-        System.out.println("computer: "+computer);  //컴퓨터 출력물 분석용
-        System.out.println("user: "+user);  // 유저 입력값 분석용
-        System.out.println(userNumber); // 유저 입력값 확인용
-
-
-        if(userSet.size()!=user.size()) {
-            throw new IllegalArgumentException("중복되는 숫자를 입력할 수 없습니다.");
-        }
-        if (user.size()!=3) {
-            throw new IllegalArgumentException("숫자 세개를 입력해주세요.");
-        }
-        if (user.contains(0)) {
-            throw new IllegalArgumentException("1~9 사이의 숫자를 입력하셔야 합니다.");
-        }
-//        if (userNumber/100==0 || (userNumber/10)%10==0 || userNumber%10==0) {
-//            throw new IllegalArgumentException("0을 입력할 수 없습니다.");
-//        }
-
-//      기능3. 결과값 비교
-        int i = 0;
-        int j = 0;
-        int strikeNball = 0;
-        int strike = 0;
-
-        for (i=0; i<user.toArray().length; i++) {
-
-            for (j=0; j<computer.toArray().length; j++) {
-                if (computer.get(i) - user.get(j) == 0) {
-                    strikeNball = strikeNball + 1;
+            List<Integer> computer = new ArrayList<>();
+            while (computer.size() < 3) {
+                randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                    System.out.print(randomNumber);
                 }
             }
-            j = 0;
-        }
 
-        for (i=0; i<user.toArray().length; i++) {
-            if (computer.get(i)==user.get(i)) {
-                strike = strike+1;
-            }
-        }
-        int ball = strikeNball-strike;
+    //      기능2. 참가자 입력
 
-        System.out.println("중복전체: "+strikeNball);
-        System.out.println("스트라이크: "+strike);
-        System.out.println("볼: "+ball);
+            do {
+                strikeNball = 0;
+                strike = 0;
+                ball = 0;
+                userNumber = 0;
 
-        System.out.printf("%d 스트라이크 %d 볼\n", strike, ball);
+                System.out.print("\n숫자를 입력해주세요: ");
+
+                userNumber = Integer.parseInt(Console.readLine());
+
+                int[] digits = Stream.of(String.valueOf(userNumber).split("")).mapToInt(Integer::parseInt).toArray();
+
+                List<Integer> user = new ArrayList<>();
+
+                for (int order = 0; order < digits.length; order++) {
+                    user.add(digits[order]);
+                }
+                Set<Integer> userSet = new HashSet<>(user);
+
+                System.out.println("computer: " + computer);  //컴퓨터 출력물 분석용
+                System.out.println("user: " + user);  // 유저 입력값 분석용
+                System.out.println(userNumber); // 유저 입력값 확인용
+
+
+                if (userSet.size() != user.size()) {
+                    throw new IllegalArgumentException("중복되는 숫자를 입력할 수 없습니다.");
+                }
+                if (user.size() != 3) {
+                    throw new IllegalArgumentException("숫자 세개를 입력해주세요.");
+                }
+                if (user.contains(0)) {
+                    throw new IllegalArgumentException("1~9 사이의 숫자를 입력하셔야 합니다.");
+                }
+    //        if (userNumber/100==0 || (userNumber/10)%10==0 || userNumber%10==0) {
+    //            throw new IllegalArgumentException("0을 입력할 수 없습니다.");
+    //        }
+
+    //      기능3. 결과값 비교
+                int i = 0;
+                int j = 0;
+
+
+                for (i = 0; i < user.toArray().length; i++) {
+
+                    for (j = 0; j < computer.toArray().length; j++) {
+                        if (computer.get(i) - user.get(j) == 0) {
+                            strikeNball = strikeNball + 1;
+                        }
+                    }
+                    j = 0;
+                }
+
+                for (i = 0; i < user.toArray().length; i++) {
+                    if (computer.get(i) == user.get(i)) {
+                        strike = strike + 1;
+                    }
+                }
+                ball = strikeNball - strike;
+
+                System.out.println("중복전체: " + strikeNball);
+                System.out.println("스트라이크: " + strike);
+                System.out.println("볼: " + ball);
+
+                System.out.printf("%d 스트라이크 %d 볼\n", strike, ball);
+
+            } while (strike < 3);
+
+            System.out.println("3개의 숫자를 모두 맞히셨습니다.");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 눌러주세요.");
+            Scanner newgame = new Scanner(System.in);
+            restart = newgame.nextInt();
+
+        } while (restart < 2);
+
+        System.out.println("숫자야구게임을 완전히 종료하겠습니다.");
 
 //        boolean compare = computer.get(0) == user.get(0);
 //        System.out.println(compare);
