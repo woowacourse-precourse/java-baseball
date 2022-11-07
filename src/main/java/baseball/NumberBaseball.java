859508d1be885198a53ec62c91cc754c0c59a022
KYR <freekyr7529@gmail.com>
2022-11-07 22:06:05 +0900

    refactor : NumberBaseball의 책임을 줄임
    NumberBaseball에 존재하던 다수의 메소드들을 다른 클래스로 이동시킨 뒤 호출하여 이용함으로써 NumberBaseball 클래스의 코드량과 책임을 줄여냄.

--- src/main/java/baseball/NumberBaseball.java
+++ src/main/java/baseball/NumberBaseball.java
@@ -1,85 +1,44 @@
 package baseball;
 
+import baseball.dto.Computer;
+import baseball.dto.GameScore;
+import baseball.dto.User;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class NumberBaseball {
-    User user;
-    Computer computer;
-    ValidInput validInput;
-    GameScore gameScore;
-
-    public NumberBaseball() {
-    }
+    static User user;
+    static Computer computer;
+    static ValidInput validInput;
+    static GameScore gameScore;
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다");
-        System.out.print("숫자를 입력해주세요 : ");
-        try {
+        computer = new Computer(new GenerateRandomNumber().generateRandomNumber());
+
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+
             user = new User(inputNumber());
-            computer = new Computer();
-        } catch (IllegalArgumentException exception) {
-            exception.printStackTrace();
-        }
-        computer.generateRandomNumber();
-
-        compareNumber();
-
-        gameScore.printResult();
+            gameScore = new CompareNumber().compareNumber(user, computer);
+            new PrintResult().printResult(gameScore);
+        } while (gameScore.getStrike() != 3);
     }
 
     private List<Integer> inputNumber() throws IllegalArgumentException {
         String userInput = Console.readLine();
-        validInput = new ValidInput(userInput);
+        validInput = new ValidInput();
 
-        validInput.inspectInputLength();
-        validInput.inspectAlphabetOrSpecialCharacter();
-        validInput.inspectDuplicatedNumber();
+        validInput.inspectInputLength(userInput);
+        validInput.inspectAlphabetOrSpecialCharacter(userInput);
+        validInput.inspectDuplicatedNumber(userInput);
 
         String[] strArr = userInput.split("");
         List<String> list = new ArrayList<>(Arrays.asList(strArr));
 
         return list.stream().map(Integer::parseInt).collect(Collectors.toList());
-    }
-
-    private void compareNumber() {
-        List<List<Integer>> compareResult = new ArrayList<>();
-        int strike = 0;
-        int ball = 0;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (makeCompareList(i, j).size() > 0) {
-                    compareResult.add(makeCompareList(i, j));
-                };
-            }
-        }
-
-        for (List<Integer> result: compareResult) {
-            if (Objects.equals(result.get(0), result.get(1))) {
-                strike += 1;
-            }
-
-            if (!(Objects.equals(result.get(0), result.get(1)))) {
-                ball += 1;
-            }
-        }
-
-        gameScore = new GameScore(strike, ball);
-    }
-
-    private List<Integer> makeCompareList(int i, int j) {
-        List<Integer> result = new ArrayList<>();
-
-        if (Objects.equals(user.usersNumber.get(i), computer.computerNumber.get(j))) {
-            result.add(i);
-            result.add(j);
-        }
-
-        return result;
     }
 }
