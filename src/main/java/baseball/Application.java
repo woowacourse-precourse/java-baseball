0372f6d9fd489fc9e6efd6830d7ce395b0b19a3f
ensk26 <tjensk26@gmail.com>
2022-11-07 15:14:11 +0900

    refactor(Application): 사용자 번호를 숫자로 반환해주는 메서드 추가
    새로운 메서드 추가 to Application:
    - 사용자 번호가 유효한 숫자면 숫자를 반환해주는 메서드
    - 사용자 번호가 유효하지 않은 숫자면  IllegalArgumentException 발생
    - isNumber 메서드의 기능이 유사해 삭제
    - isValidUserNumber 수정
    
    BREAKING CHANGE: getNumber에서 유효하지 않은 숫자면 IllegalArgumentException 에러를 발생시키기 때문에 기능이 중복되는 isNumber 메서드 제거와 isValidUserNumber를 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,22 +5,18 @@ public class Application {
         // TODO: 프로그램 구현
     }
 
+    static int getNumber(String userNumber) {
+        try {
+            return Integer.parseInt(userNumber);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
     static boolean isValidUserNumber(String userNumber) {
         if (userNumber.length() != 3 || userNumber.contains("0")) {
             throw new IllegalArgumentException();
         }
-        if (!isNumber(userNumber)) {
-            throw new IllegalArgumentException();
-        }
         return true;
-    }
-
-    static boolean isNumber(String number) {
-        try {
-            Integer.parseInt(number);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
     }
 }
