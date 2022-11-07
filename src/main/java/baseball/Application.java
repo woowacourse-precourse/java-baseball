ad614c238829a8caff3420f2df82eb49ccc472a9
Hong Jin-Seop <jinsub1999@khu.ac.kr>
2022-11-08 00:05:33 +0900

    feat: implement generateRandomNumber method
    generateRandomNumbers 메소드를 구현하였습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static String generateRandomNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+
+        // 중복되지 않는 3개의 숫자를 고른다
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        numbers.forEach(sb::append);
+
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다");
     }
 }
