03733211dae6ae76434bedf51cb9eeff0d456ece
eora21 <eora21@naver.com>
2022-11-07 11:27:42 +0900

    feat: 🎸 추가 숫자의 범위와 횟수를 지정하는 Pattern
    정규표현식으로 숫자의 범위와 횟수를 지정하여 값을 확인하는 상수 작성

--- src/main/java/baseball/constant/Rules.java
+++ src/main/java/baseball/constant/Rules.java
@@ -1,10 +1,13 @@
 package baseball.constant;
 
+import java.util.regex.Pattern;
+
 public class Rules {
     public static final int START_NUMBER = 1;
     public static final int END_NUMBER = 9;
     public static final int PICK_COUNT = 3;
-    public static final int FORBIDDEN_NUMBER = 0;
+    public static final Pattern RANGE_COUNT_PATTERN = Pattern.compile(
+            String.format("[%d-%d]{%d}", START_NUMBER, END_NUMBER, PICK_COUNT));
     public static final int DEFAULT_VALUE = 0;
     public static final String MORE = "1";
     public static final String FINISH = "2";
