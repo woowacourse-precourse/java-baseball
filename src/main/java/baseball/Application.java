cda94c6c0a6189bb6d342dd31a66be44daf6502c
DoooongDong <rhkswls98@kakao.com>
2022-11-07 22:05:18 +0900

    맞춰야 하는 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -18,7 +19,9 @@ public class Application {
         return randomNumbers;
     }
     public static void main(String[] args) {
+        //맞춰야 할 컴퓨터 숫자 목록을 할당
+        List<Integer> computer = createRandomNumber();
         System.out.println("숫자 야구 게임을 시작합니다.");
-
+        
     }
 }
