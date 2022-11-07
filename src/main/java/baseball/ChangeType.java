68188e5be001acf1578a86b3e3dae2e464764dcf
jijongkwon <help3451@naver.com>
2022-11-07 18:48:00 +0900

    feat : 타입변환 string to int 기능 추가

--- src/main/java/baseball/ChangeType.java
+++ src/main/java/baseball/ChangeType.java
@@ -1,11 +1,15 @@
 package baseball;
 
 public class ChangeType {
-    public int StringToInt(){
-
+    public int StringToInt(String word){
+        return Integer.parseInt(word);
     }
 
-    public int[] StringArrToIntArr(){
-
+    public int[] StringArrToIntArr(String[] word){
+        int[] number = new int[word.length];
+        for (int i = 0; i < number.length; i++) {
+            number[i] = Integer.parseInt(word[i]);
+        }
+        return number;
     }
 }
