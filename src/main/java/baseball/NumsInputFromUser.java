4ab5d23298affcc6f4faed049ca5b6980402ccd1
YSEO <1park4170@gmail.com>
2022-11-07 17:34:45 +0900

    refactor<get input from user> : vairable InputArr into instance variable, seperate Exception and add proper message to it, try-catch to catch NumberFormatException
    - Changing InputArr into instance variable(before, local vairable), have same reason with commit "refactor<game play>", please check it
    - For better user experience,it is better to diversify error occasion, so declare multiple IllegalArgumentException constant static variable
    - Special case is when user input something that is not number, it will raise NumberFormatException, so add try catch to handle this occasion
    
    before:
    - InputArr is local variable inside makeRandomNums
    - Only one IlleagalArgumentException, handled locally, even no message
    - no error handling for user input that is not integer
    after
    - InputArr into instance variable
    - four static constant IllegalArgumentException objects, containing specific message about mistake
    - handle input that is not integer by try- catch(NumberFormatException ne)

--- src/main/java/baseball/NumsInputFromUser.java
+++ src/main/java/baseball/NumsInputFromUser.java
@@ -4,17 +4,26 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class NumsInputFromUser {
     private int hundred, ten , one;
+    private int[] InputArr;
+    private final static IllegalArgumentException BIGINPUT = new IllegalArgumentException("999 이하의 각 자리가 중복되지 않는 값을 입력해주세요");
+    private final static IllegalArgumentException SMALLINPUT = new IllegalArgumentException("000 이상의 각 자리가 중복되지 않는 값을 입력해주세요");
+    private final static IllegalArgumentException DUPLICATEDINPUT = new IllegalArgumentException("값이 중복됩니다. 각 자리수가 다른 값을 입력해주세요");
+    private final static IllegalArgumentException NOTNUMBER = new IllegalArgumentException("세자리의 서로 다른 \"숫자\" 를 입력해주세요");
+
 
     public int[] getInputNumForGame() throws IllegalArgumentException{
-        int input = Integer.parseInt(Console.readLine());
+        try {
+            int input = Integer.parseInt(Console.readLine());
+            computeEach(input);
 
-        computeEach(input);
+            checkInputException(hundred, ten, one);
 
-        checkInputException(hundred, ten, one);
+            InputArr = toArr();
 
-        int[] InputArr = toArr();
-
-        return InputArr;
+            return InputArr;
+        } catch (NumberFormatException ne) {
+            throw NOTNUMBER;
+        }
     }
 
     private void computeEach(int input) {
@@ -24,13 +33,11 @@ public class NumsInputFromUser {
     }
 
     private void checkInputException(int hundred, int ten , int one) throws IllegalArgumentException {
-        IllegalArgumentException exception = new IllegalArgumentException();
-
-        if (hundred > 9) throw exception;
-        if (hundred < 0 || ten < 0 || one < 0) throw exception;
+        if (hundred > 9) throw BIGINPUT;
+        if (hundred < 0 || ten < 0 || one < 0) throw SMALLINPUT;
 
         if (hundred == ten || hundred == one || ten == one) {
-            throw exception;
+            throw DUPLICATEDINPUT;
         }
     }
 
@@ -42,5 +49,4 @@ public class NumsInputFromUser {
 
         return InputArr;
     }
-
 }
