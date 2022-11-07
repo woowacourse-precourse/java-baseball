6a2989fc51745ab67998a6157d36dc5790a0256a
0112leesy <0112leesy@naver.com>
2022-11-07 17:18:42 +0900

    feat(User): checkInputForAnswer
    사용자가 정답을 맞추기 위해 입력한 값을 확인함:
    - 3자리 수가 아닌 경우 예외 발생
    - 각 숫자들이 서로 다른 숫자가 아닌 경우 예외 발생

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -10,4 +10,16 @@ public class User {
         this.input = readLine();
     }
 
+    public boolean checkInputForAnswer() {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        if ((input.charAt(0) == input.charAt(1))
+            || (input.charAt(1) == input.charAt(2))
+            || (input.charAt(2) == input.charAt(0))) {
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
 }
