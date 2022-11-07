1ec769f80e3c3b6966b98edd8090c340f98e0937
BaeDongHyeon <sasd730@gmail.com>
2022-11-07 22:11:54 +0900

    feat: 볼, 스트라이크 개수에 맞게 출력
    - 개수를 확인하고 볼, 스트라이크 둘 다 없으면 낫싱 출력

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -65,4 +65,19 @@ public class Game {
 
         return result;
     }
+
+    public void printGameResult(Map<String, Integer> ballStrikeCount) {
+        int ballCount = ballStrikeCount.get("볼");
+        int strikeCount = ballStrikeCount.get("스트라이크");
+        if (ballCount != 0) {
+            System.out.print(ballCount + "볼 ");
+        }
+        if (strikeCount != 0) {
+            System.out.print(strikeCount + "스트라이크");
+        }
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.print("낫싱");
+        }
+        System.out.println();
+    }
 }
