23b73a011c2166d4c78f665e5fc98f7939474079
seheonCho <whtpgjs94@gmail.com>
2022-11-08 00:14:37 +0900

    feat(PitchResult): 컴퓨터의 숫자와 위치는 다르지만, 같은 숫자의 갯수를 반환

--- src/main/java/baseball/domain/PitchResult.java
+++ src/main/java/baseball/domain/PitchResult.java
@@ -11,6 +11,7 @@ public class PitchResult {
 
     public void setResult(String userInput, List<Integer> numbers) {
         strikeCount = setStrikeCount(userInput.toCharArray(), numbers);
+        ballCount = setBallCount(userInput.toCharArray(), numbers);
     }
 
     private int setStrikeCount(char[] userInput, List<Integer> numbers) {
@@ -23,7 +24,29 @@ public class PitchResult {
         return strikeCount;
     }
 
+    private int setBallCount(char[] userInput, List<Integer> numbers) {
+        int sumOfStrikeAndBall = 0;
+        for (int index = 0; index < GameRule.NUMBER_OF_NUMBERS; index++) {
+            sumOfStrikeAndBall += numberOfAnyDigitsSameNumber(userInput[index], numbers);
+        }
+        return sumOfStrikeAndBall - strikeCount;
+    }
+
+    private int numberOfAnyDigitsSameNumber(char digit, List<Integer> numbers) {
+        int count = 0;
+        for (int number : numbers) {
+            if (Character.getNumericValue(digit) == number) {
+                count++;
+            }
+        }
+        return count;
+    }
+
     public int getStrikeCount() {
         return strikeCount;
+    }
+
+    public int getBallCount() {
+        return ballCount;
     }
 }
--- src/test/java/baseball/domain/PitchResultTest.java
+++ src/test/java/baseball/domain/PitchResultTest.java
@@ -3,6 +3,7 @@ package baseball.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -27,5 +28,27 @@ class PitchResultTest {
         pitchResult.setResult(userInput, numbers);
 
         assertThat(pitchResult.getStrikeCount()).isEqualTo(2);
+    }
+
+    @DisplayName("컴퓨터의 숫자와 위치는 다르지만, 같은 숫자의 갯수를 반환")
+    @Test
+    void setResult_getBallCount_success() {
+        String userInput = "123";
+        List<Integer> numbers = List.of(3, 4, 5);
+
+        pitchResult.setResult(userInput, numbers);
+
+        assertThat(pitchResult.getBallCount()).isEqualTo(1);
+    }
+
+    @DisplayName("컴퓨터의 숫자와 위치는 다르지만, 같은 숫자의 갯수를 반환 - 2")
+    @Test
+    void setResult_getBallCount_success_2() {
+        String userInput = "123";
+        List<Integer> numbers = List.of(2, 3, 4);
+
+        pitchResult.setResult(userInput, numbers);
+
+        assertThat(pitchResult.getBallCount()).isEqualTo(2);
     }
 }
