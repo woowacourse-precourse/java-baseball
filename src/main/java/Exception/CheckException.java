bf44f00f297c1826bd0c87c60f9dbf9df81f06e7
YongGwan <32181771@dankook.ac.kr>
2022-11-08 02:49:15 +0900

    refactor -> 입력값이 모두 다른지 검사하는 함수 알고리즘 변경

--- src/main/java/Exception/CheckException.java
+++ src/main/java/Exception/CheckException.java
@@ -1,16 +1,18 @@
 package Exception;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class CheckException {
 
-    public static boolean restartStatusValid(String restartStatus) throws ProgressException {
+    public static void restartStatusValid(String restartStatus) throws ProgressException {
 
         if (!(restartStatus.equals("1")) && !(restartStatus.equals("2"))) {
             throw new ProgressException("재시작 입력값이 잘못 입력되었습니다. 게임을 종료합니다.");
         }
-        return true;
     }
 
-    public static void inputGameValid(String inputValue) throws ProgressException{
+    public static void inputGameValid(String inputValue) throws ProgressException {
 
         if (inputValue.length() != 3) {
             throw new ProgressException("입력값의 길이가 다릅니다. 게임을 종료합니다.");
@@ -23,7 +25,6 @@ public class CheckException {
         if (!isDiffAllDigits(inputValue)) {
             throw new ProgressException("입력값에 중복이 있습니다. 게임을 종료합니다.");
         }
-
     }
 
     private static boolean isNumeric(String inputValue) {
@@ -38,10 +39,11 @@ public class CheckException {
 
     private static boolean isDiffAllDigits(String inputValue) {
 
-        if (inputValue.charAt(0) == inputValue.charAt(1)) return false;
+        Set<Character> inputs = new HashSet<>();
 
-        if (inputValue.charAt(1) == inputValue.charAt(2)) return false;
-
-        return inputValue.charAt(0) != inputValue.charAt(2);
+        for (int index = 0; index < inputValue.length(); index++) {
+            inputs.add(inputValue.charAt(index));
+        }
+        return inputs.size() == inputValue.length();
     }
 }
