a32e3a404676a7d786486b8973168f747706c206
wjh2012 <wjh2012@naver.com>
2022-11-07 20:28:33 +0900

    test(Game): add 예측결과로 게임 종료를 결정하는 기능 테스트

---
+++ src/test/java/baseball/GameTest.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import baseball.game.Game;
+import baseball.game.utils.ScoreBoard;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameTest {
+    @Test
+    void 결과로_게임_종료를_결정하는_기능_종료(){
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.addStrike();
+        scoreBoard.addStrike();
+        scoreBoard.addStrike();
+
+        Assertions.assertThat(Game.checkGameOver(scoreBoard)).isEqualTo(false);
+    }
+}
