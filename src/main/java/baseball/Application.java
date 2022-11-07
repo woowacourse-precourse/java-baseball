fdd5e6ea6509ae8c24c22ab48ed6d3019c1e383a
dev-green-flamingo <devgflamingo@gmail.com>
2022-11-08 00:17:35 +0900

    feat(computer): baseball 생성
    게임을 진행하기 위한 baseball 리스트를 생성한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    public static List<Integer> createBaseBall() {
+        List<Integer> baseBallNumber = new ArrayList<>();
+        while (baseBallNumber.size() < 3) {
+            int digit = Randoms.pickNumberInRange(0, 9);
+            if (baseBallNumber.contains(digit)) {
+                continue;
+            }
+            baseBallNumber.add(digit);
+        }
+        return baseBallNumber;
     }
 }
