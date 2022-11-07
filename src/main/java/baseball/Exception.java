dc306f176011b39dc5e7f4dd0d6ab0a95fbc27c6
jw0293 <k38836919@gmail.com>
2022-11-07 16:11:01 +0900

    feature : List of Exceptions [예외 발생 리스트 목록 작성]

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -1,2 +1,43 @@
-package baseball;public class Exception {
+package baseball;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Exception {
+    private final static int USER_INPUT_NUMBER_LENGTH = 3;
+
+    public boolean isValidLength(String userNumber) {
+        return userNumber.length() == USER_INPUT_NUMBER_LENGTH;
+    }
+
+    public boolean isValidNumber(String userNumber) {
+        try {
+            int userRandomNumber = Integer.parseInt(userNumber);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isValidDuplicate(List<Integer> numbers) {
+        Set<Integer> isUsing = new HashSet<>();
+        for (Integer number : numbers) {
+            addNotContainNumber(isUsing, number);
+        }
+        return isUsing.size() == USER_INPUT_NUMBER_LENGTH;
+    }
+
+    private void addNotContainNumber(Set<Integer> isUsing, Integer number) {
+        if (!isUsing.contains(number)) {
+            isUsing.add(number);
+        }
+    }
+
+    public boolean isValidGameMode(int method) {
+        if (method == 1 || method == 2) {
+            return true;
+        }
+        return false;
+    }
 }
