79956ea913bd1ed2d360644d39385340db871e65
Kayoung <rkdkdudjd@gmail.com>
2022-11-07 19:20:25 +0900

    refactor(BaseballGame): 사용자 입력을 GameScanner 클래스에 위임
    * 사용자 입력에 대한 기능을 담당하는 GameScanner 클래스를 정의했습니다.

---
+++ src/main/java/baseball/game/GameScanner.java
@@ -0,0 +1,17 @@
+package baseball.game;
+
+import baseball.game.input.FinishInput;
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameScanner {
+    public Number readPlayerNumber() {
+        String line = Console.readLine();
+        int input = Integer.parseInt(line);
+        return new Number(input);
+    }
+
+    public FinishInput readFinishInput() {
+        int inputNumber = Integer.parseInt(Console.readLine());
+        return FinishInput.numberOf(inputNumber);
+    }
+}
