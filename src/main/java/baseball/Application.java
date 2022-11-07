4e971ebc60ee1123d9a2694aa1f4fbae83f23442
LeeMyeongJin <=>
2022-11-08 02:36:43 +0900

    Feat: 게임 종료 메세지 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,6 @@ import java.awt.desktop.SystemEventListener;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-//        String computerNumber = "";
         Computer computer = new Computer();
         Player player = new Player();
         Start start = new Start();
@@ -14,24 +13,21 @@ public class Application {
         Judge judge = new Judge();
         Rule rule = new Rule();
 
-        start.start(); // 게임 시작
+        start.startGameMessage(); // 게임 시작
 
-        while (again){ // 3 indent
-            // 컴퓨터 숫자 입력
+        while (again) {
             String result = "";
             String computerNumber = computer.getComputerNumber();
             System.out.println("컴퓨터 숫자는 : " + computerNumber);
-            // 플레이어 숫자 입력
             /// ball 계산은 total 에서 strike 빼면 될듯? 예를들어 2개가 겹친다 치고 스트라이크가 1 이면 나머지 1이 볼이된다.
-            while (!result.equals("3스트라이크")){
-                result = Judge.GameStart(computerNumber, Player.getNumber());
+            while (!result.equals("3스트라이크")) {
+                result = judge.GameStart(computerNumber, player.getNumber());
                 System.out.println(result);
-
             }
             again = restart.reStart();
-            /// 몇개 숫자가 같은지 = total
 
-        } // while close
+        }
+        System.out.println("게임 종료");
 
     }
 
