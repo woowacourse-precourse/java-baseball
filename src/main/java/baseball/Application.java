1a015fe73b33b49c047c443a639ddda91bb95bc2
hantae <hantae96@naver.com>
2022-11-07 13:13:30 +0900

    Feat : 숫자 3개를 랜덤 추출 후 변수에 할당하는 기능추가(#1)
    camp.nextstep.edu.missionutils 라이브러리를 사용하여 랜덤 수 선정
    
    close #1

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,5 +4,25 @@ import java.util.*;
 import camp.nextstep.edu.missionutils.*;
 public class Application {
     public static void main(String[] args) {
+
+        // TODO: 프로그램 구현
+        BaseballGame Game = new BaseballGame();
+        List<Integer> list=Game.makeRandomNumber();
+        System.out.println(list);
     }
 }
+
+class BaseballGame {
+    //숫자 3개를 임의로 추출 하고 컴퓨터에 해당하는 변수에 할당하는 기능
+    List<Integer> makeRandomNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while(computer.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+}
+
