c5bbd436e0e70fcfa491b9af53ade5ffb50491d4
the9kim <96895686+the9kim@users.noreply.github.com>
2022-11-07 21:56:19 +0900

    feat: 세자리 수 입력 값의 적정성 검토 메서드 구현
    세 자리 숫자가 맞는지, 숫자 '0'이 포함되어 있지 않은지, 숫자로 구성된 문자열인지, 서로 다른 숫자가 아닌지 여부 판단

--- src/main/java/baseball/util/ValidationUtil.java
+++ src/main/java/baseball/util/ValidationUtil.java
@@ -1,4 +1,32 @@
 package baseball.util;
 
 public class ValidationUtil {
+
+    public static boolean ValidateInputNumber(String input) {
+        if (input.length() == 3 || input.contains("0")
+                || validateIsDigit(input)) {
+        }
+    }
+
+    private static boolean validateIsDigit(String input) {
+        char[] inputArray = input.toCharArray();
+        for (char inputChar : inputArray) {
+            if (Character.isDigit(inputChar)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean validateDuplicateNumber(String input){
+        String[] strArray = input.split("");
+        if (strArray[0].equals(strArray[1]) || strArray[0].equals(strArray[2])
+                || strArray[1].equals(strArray[2])){
+            return false;
+        }
+        return true;
+    }
+
+
 }
+
