844ed1c51ddd937d39b018b9c25863d5f35e0106
yu-heejin <jkgt0521@naver.com>
2022-11-07 14:13:05 +0900

    feat : 코드 리팩토링 들여쓰기 3 이상인 코드들을 모두 메소드로 분리했다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,12 +6,52 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 public class Application {
     static int goGame = 1;
     final static int MAX_NUMBER_SIZE = 3;
+    static String randomNumber = null;
+
+    public static int[] hasNumber(int[] gameResults, char dividedNumber, int index) {
+        // dividedNumber의 자리수는 따로 구할 필요 없이 i를 사용한다.
+        if (randomNumber.indexOf(dividedNumber) == index) {
+            gameResults[0]++;
+            System.out.println(gameResults[0]);
+        } else if (randomNumber.indexOf(dividedNumber) != -1) {
+            gameResults[1]++;
+        }
+
+        return gameResults;
+    }
+
+    public static int[] countGameResult(String number) {
+        int[] gameResults = {0, 0};
+
+        // 중복된 수가 나올 경우 이와 관련된 처리가 필요하다.
+        for (int i = 0; i < MAX_NUMBER_SIZE; i++) {
+            char dividedNumber = number.charAt(i);
+            gameResults = hasNumber(gameResults, dividedNumber, i);
+        }
+
+        return gameResults;
+    }
+
+    public static void printGameResult(int[] gameResults) {
+        if (gameResults[0] == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            inputAnswer();
+        } else if (gameResults[0] == 0 && gameResults[1] == 0) {
+            System.out.println("낫싱");
+        } else {
+            System.out.println(gameResults[0] + "스트라이크 " + gameResults[1] + "볼");
+        }
+    }
+
+    public static void inputAnswer() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        goGame = Integer.parseInt(readLine());
+        randomNumber = String.valueOf(pickNumberInRange(100, 999));
+    }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int strikeCount = 0;
-        int ballCount = 0;
-        String randomNumber = String.valueOf(pickNumberInRange(100, 999));
+        randomNumber = String.valueOf(pickNumberInRange(100, 999));
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (goGame == 1) {
@@ -20,31 +60,12 @@ public class Application {
             System.out.print("숫자를 입력해주세요 : ");
             String number = readLine();
 
+            // 예외 처리 구현 필요
+            // 들여쓰기 3 이상
             if (number.length() < MAX_NUMBER_SIZE) continue;
 
-            for (int i = 0; i < MAX_NUMBER_SIZE; i++) {
-                char dividedNumber = number.charAt(i);
-                // dividedNumber의 자리수는 따로 구할 필요 없이 i를 사용한다.
-                if (randomNumber.indexOf(dividedNumber) == i) {
-                    strikeCount++;
-                } else if (randomNumber.indexOf(dividedNumber) != -1) {
-                    ballCount++;
-                }
-            }
-
-            if (strikeCount == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                goGame = Integer.parseInt(readLine());
-                randomNumber = String.valueOf(pickNumberInRange(100, 999));
-            } else if (ballCount == 0 && strikeCount == 0) {
-                System.out.println("낫싱");
-            } else {
-                System.out.println(strikeCount + "스트라이크 " + ballCount + "볼");
-            }
-
-            strikeCount = 0;
-            ballCount = 0;
+            int[] gameResults = countGameResult(number);
+            printGameResult(gameResults);
         }
     }
 }
