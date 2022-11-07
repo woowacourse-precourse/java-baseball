edd5ca86fb025eb79c78cff321d2b7e31bce6797
Choi-JY <Choi-JY1107@github.com>
2022-11-07 14:55:23 +0900

    feat: 비교결괏값 출력하는 기능 구현

---
+++ src/main/java/baseball/view/Output.java
@@ -0,0 +1,33 @@
+package baseball.view;
+
+import baseball.model.BallCounts;
+
+public class Output {
+    public static void outputBallCounts(BallCounts ballCounts) {
+        outputBall(ballCounts.getBall());
+        outputStrike(ballCounts.getStrike());
+        outputNothing(ballCounts.getIsNothing());
+    }
+    public static void outputRestart() {
+
+    }
+
+    private static void outputBall(int countBall) {
+        if (countBall != 0) {
+            System.out.print(countBall + "볼 ");
+        }
+    }
+
+    private static void outputStrike(int countStrike) {
+        if (countStrike != 0) {
+            System.out.print(countStrike + "스트라이크");
+        }
+    }
+
+    private static void outputNothing(boolean isNothing) {
+        if (isNothing) {
+            System.out.println("낫싱");
+        }
+    }
+
+}
