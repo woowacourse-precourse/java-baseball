192c4eaf41696e9175d3dc2b6ca71724452e5692
Gaby <gaby@Gabyui-MacBookPro.local>
2022-11-07 11:54:08 +0900

    test: add test for strike count

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,6 +21,17 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 스트라이크() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("135", "267", "239", "168", "169", "2");
+                    assertThat(output()).contains("1스트라이크", "2스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 6, 9
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
