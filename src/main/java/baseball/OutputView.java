94a28420dad16f5b4401fe7352c5cc1e88ca90f5
LEE-sh1673 <lsh901673@gmail.com>
2022-11-07 16:04:09 +0900

    refactor(outputView): 화면 출력 기능 static 메서드로 변경
    - display() 를 static 메서드로 변경

--- src/main/java/baseball/OutputView.java
+++ src/main/java/baseball/OutputView.java
@@ -2,9 +2,9 @@ package baseball;
 
 public class OutputView {
 
-	private final String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private final static String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
-	public void display(final CompareResult result) {
+	public static void display(final CompareResult result) {
 		System.out.println(String.join(" ", result.getResults()));
 
 		if (result.isAllStrikes()) {
