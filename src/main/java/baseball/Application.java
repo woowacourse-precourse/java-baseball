efa6b4718cac38e6694c5669b82fda02f946fad9
sypark <soun997@gmail.com>
2022-11-08 08:03:25 +0900

    feat: implement function 'main'
    Overall logic implementation of baseball-game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,18 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Application {
 
     /*
      * 컴퓨터가 세자리의 랜덤 숫자를 선택, 각 자리값을 리스트에 담아 반환
      */
-    public List<Integer> computersChoice() {
+    public static List<Integer> computersChoice() {
 
         List<Integer> computer = new ArrayList<>();
 
@@ -30,7 +30,7 @@ public class Application {
     /*
      * 사용자가 세자리의 숫자를 입력, 각 자리값을 리스트에 담아 반환
      */
-    public List<Integer> usersChoice(){
+    public static List<Integer> usersChoice(){
 
         String input = Console.readLine();
         inputExceptionHandler(input);   // 입력값 예외처리
@@ -47,14 +47,14 @@ public class Application {
     /*
      * 사용자 입력 예외를 처리함
      */
-    public void inputExceptionHandler(String input){
+    public static void inputExceptionHandler(String input){
         // TODO: 사용자 입력 예외를 처리함
     }
 
     /*
      * 컴퓨터가 고른 숫자와 사용자가 입력한 숫자를 비교하여 결과를 반환
      */
-    public String getGameResult(List<Integer> computer, List<Integer> user){
+    public static String getGameResult(List<Integer> computer, List<Integer> user){
 
         int ball = 0;
         int strike = 0;
@@ -98,7 +98,7 @@ public class Application {
     /*
      * 게임 종료 후 숫자 야구 게임 재시작 여부 반환
      */
-    public boolean restartGame(){
+    public static boolean restartGame(){
 
         String input = Console.readLine();
 
@@ -114,6 +114,33 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        List<Integer> computer = new ArrayList<>();
+        List<String> results = new ArrayList<>();
+        List<Integer> user = new ArrayList<>();
+        boolean restart = false;
+
+        computer = computersChoice();
+
+        while(true){
+            user = usersChoice();
+            results.add(getGameResult(computer, user));
+
+            // 3스트라이크라면 게임 종료
+            if (getGameResult(computer, user).equals("3스트라이크")){
+                restart = restartGame();    // 게임을 재시작할지 종료할지 여부 결정
+            }
+
+            // 게임을 재시작하기로 했다면 컴퓨터는 다시 랜덤 숫자를 선택
+            if (restart){
+                computer = computersChoice();
+                restart = false;    // 초기화
+            }
+            else if (getGameResult(computer, user).equals("3스트라이크")){
+                results.add("게임 종료");
+                System.out.println(results);
+                break;
+            }
+        }
     }
 }
