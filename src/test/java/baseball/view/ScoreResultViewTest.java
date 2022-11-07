55090969ee7aeb6c3582ffd6be6d91394fc9d441
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-07 11:14:44 +0900

    refactor: ScoreResultView 테스트코드 public 메서드로 재구성 및 입출력 테스트로 수정

--- src/test/java/baseball/view/ScoreResultViewTest.java
+++ src/test/java/baseball/view/ScoreResultViewTest.java
@@ -3,6 +3,10 @@ import baseball.view.ScoreResultView;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class ScoreResultViewTest {
@@ -14,52 +18,64 @@ public class ScoreResultViewTest {
         scoreResultView = new ScoreResultView();
     }
 
-    @Test
-    void 스트라이크와_볼이_0일때_참(){
-        scoreResultView.setBallCount(0);
-        scoreResultView.setStrikeCount(0);
-        assertThat(scoreResultView.isNothing()).isTrue();
-    }
-
-    @Test
-    void 스트라이크가_0일때_참(){
-        scoreResultView.setBallCount(3);
-        scoreResultView.setStrikeCount(0);
-        assertThat(scoreResultView.isOnlyBall()).isTrue();
-    }
-
-    @Test
-    void 볼이_0일때_참(){
-        scoreResultView.setBallCount(0);
-        scoreResultView.setStrikeCount(3);
-        assertThat(scoreResultView.isOnlyStrike()).isTrue();
-    }
+//    @Test
+//    void 스트라이크와_볼이_0일때_참(){
+//        scoreResultView.setBallCount(0);
+//        scoreResultView.setStrikeCount(0);
+//        assertThat(scoreResultView.isNothing()).isTrue();
+//    }
+//
+//    @Test
+//    void 스트라이크가_0일때_참(){
+//        scoreResultView.setBallCount(3);
+//        scoreResultView.setStrikeCount(0);
+//        assertThat(scoreResultView.isOnlyBall()).isTrue();
+//    }
+//
+//    @Test
+//    void 볼이_0일때_참(){
+//        scoreResultView.setBallCount(0);
+//        scoreResultView.setStrikeCount(3);
+//        assertThat(scoreResultView.isOnlyStrike()).isTrue();
+//    }
 
     @Test
     void 스트라이크와_볼이_0일때_낫싱_반환(){
         scoreResultView.setBallCount(0);
         scoreResultView.setStrikeCount(0);
-        assertThat(scoreResultView.makeResultString()).isEqualTo("낫싱");
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        scoreResultView.printScore();
+        assertThat(out.toString()).isEqualTo("낫싱");
     }
 
     @Test
     void 스트라이크가_0일때_볼만_반환(){
         scoreResultView.setBallCount(3);
         scoreResultView.setStrikeCount(0);
-        assertThat(scoreResultView.makeResultString()).isEqualTo("3볼 ");
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        scoreResultView.printScore();
+        assertThat(out.toString()).isEqualTo("3볼 ");
     }
 
     @Test
     void 볼이_0일때_스트라이크만_반환(){
         scoreResultView.setBallCount(0);
         scoreResultView.setStrikeCount(2);
-        assertThat(scoreResultView.makeResultString()).isEqualTo("2스트라이크");
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        scoreResultView.printScore();
+        assertThat(out.toString()).isEqualTo("2스트라이크");
     }
 
     @Test
     void 그외에는_둘다_반환(){
         scoreResultView.setBallCount(2);
         scoreResultView.setStrikeCount(1);
-        assertThat(scoreResultView.makeResultString()).isEqualTo("2볼 1스트라이크");
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        scoreResultView.printScore();
+        assertThat(out.toString()).isEqualTo("2볼 1스트라이크");
     }
 }
