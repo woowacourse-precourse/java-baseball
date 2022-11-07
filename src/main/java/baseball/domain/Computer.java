d39310cb6889a1f281f58d96aec2779ec415db37
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-07 18:37:40 +0900

    feat : 전역 변수 추가

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,16 @@
+package baseball.domain;
+
+import baseball.exception.ComputerException;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+
+    public static List<Integer> computerRandomNumberList;
+
+
+
+
+}
