41e2552d1bdd9a207c69c5dac48f438b2672f17e
dclxxi <dclxxi@kakao.com>
2022-11-07 18:58:49 +0900

    test: InputValidator 테스트 코드 추가

--- src/test/java/baseball/InputValidatorTest.java
+++ src/test/java/baseball/InputValidatorTest.java
@@ -4,6 +4,7 @@ import baseball.validator.InputValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputValidatorTest {
@@ -25,6 +26,16 @@ public class InputValidatorTest {
     @Test
     void 입력받은_수의_자리_수를_검사한다() {
         assertThatThrownBy(() -> {
+            inputValidator.validatePlayerNumber("12");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Message.DIGIT_LENGTH_EXCEPTION);
+
+        assertThatThrownBy(() -> {
+            inputValidator.validatePlayerNumber("");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Message.DIGIT_LENGTH_EXCEPTION);
+
+        assertThatThrownBy(() -> {
             inputValidator.validatePlayerNumber("1234");
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Message.DIGIT_LENGTH_EXCEPTION);
@@ -34,6 +45,11 @@ public class InputValidatorTest {
     void 입력받은_수의_중복여부를_검사한다() {
         assertThatThrownBy(() -> {
             inputValidator.validatePlayerNumber("122");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Message.DUPLICATION_EXCEPTION);
+
+        assertThatThrownBy(() -> {
+            inputValidator.validatePlayerNumber("111");
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Message.DUPLICATION_EXCEPTION);
     }
@@ -47,9 +63,39 @@ public class InputValidatorTest {
     }
 
     @Test
+    void 입력받은_수가_모든_테스트를_통과한다() {
+        assertThatCode(() -> {
+            inputValidator.validatePlayerNumber("123");
+        }).doesNotThrowAnyException();
+
+        assertThatCode(() -> {
+            inputValidator.validatePlayerNumber("456");
+        }).doesNotThrowAnyException();
+
+        assertThatCode(() -> {
+            inputValidator.validatePlayerNumber("789");
+        }).doesNotThrowAnyException();
+    }
+
+    @Test
     void 게임상태를_나타내는_수의_타입을_검사한다() {
         assertThatThrownBy(() -> {
             inputValidator.validateStateNumber("일");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Message.TYPE_EXCEPTION);
+
+        assertThatThrownBy(() -> {
+            inputValidator.validateStateNumber("A");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Message.TYPE_EXCEPTION);
+
+        assertThatThrownBy(() -> {
+            inputValidator.validateStateNumber("!");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Message.TYPE_EXCEPTION);
+
+        assertThatThrownBy(() -> {
+            inputValidator.validateStateNumber(" ");
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Message.TYPE_EXCEPTION);
     }
@@ -60,10 +106,20 @@ public class InputValidatorTest {
             inputValidator.validateStateNumber("12");
         }).isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(Message.STATE_LENGTH_EXCEPTION);
+
+        assertThatThrownBy(() -> {
+            inputValidator.validateStateNumber("");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Message.STATE_LENGTH_EXCEPTION);
     }
 
     @Test
     void 게임상태를_나타내는_수의_범위를_검사한다() {
+        assertThatThrownBy(() -> {
+            inputValidator.validateStateNumber("0");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Message.STATE_NUMBER_EXCEPTION);
+
         assertThatThrownBy(() -> {
             inputValidator.validateStateNumber("3");
         }).isInstanceOf(IllegalArgumentException.class)
