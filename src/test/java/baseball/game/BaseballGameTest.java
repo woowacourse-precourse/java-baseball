73003d79e11e17d431d6ed9e09370660df46a84a
chkim <cndghks0213@gmail.com>
2022-11-08 08:29:59 +0900

    test(game): InputNumbersGenerationStrategy 생성을 메서드로 분리

--- src/test/java/baseball/game/BaseballGameTest.java
+++ src/test/java/baseball/game/BaseballGameTest.java
@@ -1,6 +1,7 @@
 package baseball.game;
 
 import baseball.numbers.InputNumbersGenerationStrategy;
+import baseball.numbers.NumbersGenerationStrategy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -26,7 +27,7 @@ class BaseballGameTest {
 
     @Test
     void 게임을_시작한다() {
-        BaseballGame game = new BaseballGame(new InputNumbersGenerationStrategy(1, 2, 3));
+        BaseballGame game = new BaseballGame(numbers(1, 2, 3));
         command("123", "2");
 
         game.start();
@@ -36,7 +37,7 @@ class BaseballGameTest {
 
     @Test
     void 게임_종료_후_재시작한다() {
-        BaseballGame game = new BaseballGame(new InputNumbersGenerationStrategy(List.of(1, 2, 3), List.of(7, 8, 9)));
+        BaseballGame game = new BaseballGame(numbers(List.of(1, 2, 3), List.of(7, 8, 9)));
         command("123", "1", "321", "786", "789", "2");
 
         game.start();
@@ -46,12 +47,13 @@ class BaseballGameTest {
 
     @Test
     void 게임_종료_후_여러_번_재시작한다() {
-        BaseballGame game = new BaseballGame(new InputNumbersGenerationStrategy(List.of(1, 2, 3), List.of(7, 8, 9), List.of(1, 4, 7)));
-        command("123", "1", "321", "786", "789", "1", "123", "427", "147", "2");
+        BaseballGame game = new BaseballGame(numbers(List.of(1, 2, 3), List.of(7, 8, 9), List.of(1, 4, 7)));
+        command("123", "1", "321", "789", "1", "123", "427", "147", "2");
 
         game.start();
 
-        assertThat(output()).contains("3스트라이크", "낫싱", "2스트라이크", "3스트라이크", "1스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+        List<String> expected = List.of("3스트라이크", "낫싱", "3스트라이크", "1스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+        assertThat(output()).contains(expected);
     }
 
     @ValueSource(strings = {"111", "112", "122", "121", "1", "12", "1234", "가나다", "!#@"})
@@ -65,7 +67,7 @@ class BaseballGameTest {
 
     @Test
     void 게임_종료_후_1또는_2가_아닌_값을_입력한다() {
-        BaseballGame game = new BaseballGame(new InputNumbersGenerationStrategy(1, 2, 3));
+        BaseballGame game = new BaseballGame(numbers(1, 2, 3));
         command("123", "3");
 
         assertThatThrownBy(game::start).isInstanceOf(IllegalArgumentException.class);
@@ -78,5 +80,13 @@ class BaseballGameTest {
 
     private String output() {
         return outputStream.toString().trim();
+    }
+
+    private NumbersGenerationStrategy numbers(Integer... inputs) {
+        return new InputNumbersGenerationStrategy(inputs);
+    }
+
+    private NumbersGenerationStrategy numbers(List<Integer>... inputs) {
+        return new InputNumbersGenerationStrategy(inputs);
     }
 }
