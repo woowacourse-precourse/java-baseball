dee9498d0606b3474a6d6b17b7c9a91ef27cd470
hyeonseog-won <smreo75@gmail.com>
2022-11-07 20:49:20 +0900

    refactor: 테스트 코드를 위한 User 클래스 코드 수정

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -3,24 +3,24 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class User {
     private static List<Integer> numbers;
 
-    public void inputNumbers() {
+    public String inputNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        validateInput(input);
-        numbers = getNumbers(input);
-        validateDuplicate(numbers);
+        return input;
     }
 
-    private void validateInput (String input) {
+    public void setNumbers(String input) {
+        validateInput(input);
+        this.numbers = validateDuplicate(input);
+    }
+
+    private void validateInput(String input) {
         if (input.length() != 3) {
             throw new IllegalArgumentException("입력한 수가 3자리가 아닙니다.");
         }
@@ -29,17 +29,14 @@ public class User {
         }
     }
 
-    public List<Integer> getNumbers(String input) {
-        return numbers = Arrays.stream(input.split(""))
+    private List<Integer> validateDuplicate(String input) {
+        List<Integer> numbers = Arrays.stream(input.split(""))
                 .map(Integer::parseInt)
                 .distinct()
                 .collect(Collectors.toList());
-    }
-
-    private void validateDuplicate(List<Integer> numbers) {
-        Set<Integer> duplicateCheck = new HashSet<>(numbers);
-        if (duplicateCheck.size() != 3) {
+        if (numbers.size() != 3) {
             throw new IllegalArgumentException("서로 다른 수로 이루어진 3자리의 수를 입력해야 합니다.");
         }
+        return numbers;
     }
 }
