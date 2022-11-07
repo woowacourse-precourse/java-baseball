ceea089e304df60439e781ca5a1dcc1b9314aefa
Sehoon Kim <shggm2000@gmail.com>
2022-11-08 00:07:22 +0900

    예외처리 코드 재성립

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,10 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         Controller controller = new Controller();
-        controller.run();
+        try{
+            controller.run();
+        } catch (IllegalArgumentException e){
+            System.out.println("입력 값 오류입니다");
+        }
     }
 }
