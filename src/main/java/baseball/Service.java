895199855635add4c5bed0c1cb6cf908a4cee6c8
skfkgla <narahim.lee@gmail.com>
2022-11-08 00:39:11 +0900

    feat: 최소값 이상 최대값 미만을 원하는 크기만큼 수가 중복되지 않도록 난수를 반환하는 메서드 구현
    숫자 야구에서 상대방(컴퓨터)이 가진 숫자를 랜덤하게 생성하기 위해 구현

--- src/main/java/baseball/Service.java
+++ src/main/java/baseball/Service.java
@@ -1,6 +1,9 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Service {
@@ -14,5 +17,15 @@ public class Service {
             list.add(string);
         }
         return list;
+    }
+
+    public List<String> generateRandomNumber(int min, int max, int size) {
+        List<String> randomNumbers = new LinkedList<>();
+
+        while(size > randomNumbers.size()) {
+            String randomNumber = Integer.toString(Randoms.pickNumberInRange(min, max));
+            randomNumbers = AddToListExcludeContainsString(randomNumbers, randomNumber);
+        }
+        return randomNumbers;
     }
 }
