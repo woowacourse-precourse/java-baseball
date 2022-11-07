07b97d5a3b8075dbc3d90e5d185da34f6e30add2
이건회 <rg970604@naver.com>
2022-11-08 06:25:19 +0900

    refactor (EndRule.java) : 검증 메소드 클래스 Validator로 변경

--- src/main/java/baseball/rule/EndRule.java
+++ src/main/java/baseball/rule/EndRule.java
@@ -1,13 +1,11 @@
 package baseball.rule;
 
 import baseball.domain.BallCount;
+import baseball.util.Validator;
 
 public class EndRule {
 
     private static final String THREE_STRIKE = "3스트라이크";
-    private static final String ONLY_INPUT_1_OR_2 = "1과 2만 입력 가능합니다";
-
-    private static final String RESTART_NUMBER = "1";
     private static final String END_NUMBER = "2";
 
     public static boolean isGameEnd(BallCount ballCount) {
@@ -15,14 +13,7 @@ public class EndRule {
     }
 
     public static boolean isProgramEnd(String userInput) {
-        validateWrongNum(userInput);
+        Validator.validateWrongNum(userInput);
         return userInput.equals(END_NUMBER);
     }
-
-    private static void validateWrongNum(String userInput) {
-        if (!userInput.equals(RESTART_NUMBER) && !userInput.equals(END_NUMBER)) {
-            throw new IllegalArgumentException(ONLY_INPUT_1_OR_2);
-        }
-    }
-
 }
