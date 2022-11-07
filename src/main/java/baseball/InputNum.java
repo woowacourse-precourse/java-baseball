2dbc92cbf511bfeb9bac7bf271e58ca8592802b5
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-07 13:42:59 +0900

    refactor(InputNum) : Input 인터페이스를 구현
    InputNum 클래스가 Input 인터페이스를 구현하도록 구조를 변경

--- src/main/java/baseball/InputNum.java
+++ src/main/java/baseball/InputNum.java
@@ -4,18 +4,18 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.regex.Pattern;
 
-public class InputNum {
+public class InputNum implements Input{
     static final String INPUT_TEXT = "숫자를 입력해주세요 : ";
 
-    String input(int size) {
+    public String input(int size) {
         String inputNum;
         System.out.println(INPUT_TEXT);
         inputNum = Console.readLine();
-        validateInuput(inputNum, size);
+        validateInput(inputNum, size);
         return inputNum;
     }
 
-    private void validateInuput(String input, int size) {
+    public void validateInput(String input, int size) {
         Pattern p = Pattern.compile("[0-9]{"+size+"}");
         if (!p.matcher(input).matches()) {
             throw new IllegalArgumentException();
