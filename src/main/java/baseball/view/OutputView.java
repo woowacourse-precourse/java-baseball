4e2c1189243da4270b1af90476df00b746ccea26
the9kim <96895686+the9kim@users.noreply.github.com>
2022-11-07 21:06:00 +0900

    feat: 세자리 숫자 입력요청 문구 출력 및 입력 메서드
    숫자 입력 요청 후 세자리수 입력 받기 기능

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,8 +1,14 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class OutputView {
 
     public static void printGameStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printRequestNumber(){
+        String input = Console.readLine();
     }
 }
