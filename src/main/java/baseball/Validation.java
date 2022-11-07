231417751c2071f0e73106a48677af5f3f4a7c60
seonghoo1217 <seonghoo1217@naver.com>
2022-11-08 01:29:52 +0900

    feat : add validation about user entered retry condition
    If user want re-try the game input condition are limited to 1 and 2
    Cause , I think this input value should be restricted
    
    - If user input value not one and two throw IllegalArgumentException

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -14,9 +14,9 @@ public class Validation {
         }
         else if (validation_case.equals("user_retry_condition_validation")&&validation_param instanceof String){
             String user_input_retry = (String) validation_param;
-            boolean retry_regex = user_input_retry.matches("[^12]");
-            if (retry_regex){
-                throw new IllegalArgumentException();
+            boolean retry_regex = user_input_retry.matches("1|2");
+            if (!retry_regex){
+                return true;
             }
         }
 
