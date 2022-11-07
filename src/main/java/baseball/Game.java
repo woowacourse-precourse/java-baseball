943c111bdd543310e697dd23e6dc26825974d67f
Lee-SungMin <saevers26@gmail.com>
2022-11-08 00:57:58 +0900

    feat: 게임 상태를 저장하는 기능 구현 - 정답인 경우, 게임 상태를 변경하여 표현하는 기능

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Game {
 
+    protected int state = 1;
+
     protected boolean nothing = true;
 
     protected int same_cnt = 0;
@@ -13,6 +15,12 @@ public class Game {
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void end(){
+        this.state = 2;
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
     public void printInputNumber() {
@@ -56,8 +64,7 @@ public class Game {
             System.out.println(this.strike + "스트라이크");
 
             if (this.strike == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                end();
             }
 
         }
