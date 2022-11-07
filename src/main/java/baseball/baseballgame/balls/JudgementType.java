15e8f0f1778e579c319a79de5e6363b01fe8232a
Albert <guang7226961@naver.com>
2022-11-08 08:37:38 +0900

    refactor($balls) JudgementType의 원시값을 포장하였다.

--- src/main/java/baseball/baseballgame/balls/JudgementType.java
+++ src/main/java/baseball/baseballgame/balls/JudgementType.java
@@ -5,11 +5,13 @@ import java.util.EnumMap;
 public enum JudgementType {
     STRIKE, BALL, NOTHING;
 
+    public static final int DEFAULT_VALUE = 0;
+
     public static EnumMap<JudgementType, Integer> creatEnumMap() {
         EnumMap<JudgementType, Integer> ballReport = new EnumMap<>(JudgementType.class);
-        ballReport.put(JudgementType.STRIKE, 0);
-        ballReport.put(JudgementType.BALL, 0);
-        ballReport.put(JudgementType.NOTHING, 0);
+        ballReport.put(JudgementType.STRIKE, DEFAULT_VALUE);
+        ballReport.put(JudgementType.BALL, DEFAULT_VALUE);
+        ballReport.put(JudgementType.NOTHING, DEFAULT_VALUE);
         return ballReport;
     }
 }
