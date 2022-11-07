de5ba968f53013055ffb55d52cd75f655e6943bd
kimdoha <kkhhjj3393@gmail.com>
2022-11-07 23:37:30 +0900

    refactor(Score): 스코어 클래스 리팩터링
    - 접근 제어자 리팩터링

--- src/main/java/baseball/score/Score.java
+++ src/main/java/baseball/score/Score.java
@@ -7,7 +7,7 @@ public class Score {
   private Integer strikeCount;
   private Integer ballCount;
 
-  private Score(){
+  public Score(){
     this.strikeCount = 0;
     this.ballCount = 0;
   }
@@ -17,13 +17,13 @@ public class Score {
     setBallCount(computer, user);
   }
 
-  public void setStrikeCount(List<Integer> computer, List<Integer> user) {
+  private void setStrikeCount(List<Integer> computer, List<Integer> user) {
     this.strikeCount = (int) IntStream.range(0, computer.size())
         .filter(i -> computer.get(i).equals(user.get(i)))
         .count();
   }
 
-  public void setBallCount(List<Integer> computer, List<Integer> user) {
+  private void setBallCount(List<Integer> computer, List<Integer> user) {
     this.ballCount = (int) IntStream.range(0, computer.size())
         .filter(i -> user.contains(computer.get(i)) && i != user.indexOf(computer.get(i)))
         .count();
