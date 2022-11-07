6a42a3331747a45589bf1eb9d1e712d9b9403f8f
saewoo1 <wooozooo1123@gmail.com>
2022-11-07 19:03:30 +0900

    test(CreateRandomNumberTest): duplication test code

--- src/test/java/baseball/CreateRandomNumberTest.java
+++ src/test/java/baseball/CreateRandomNumberTest.java
@@ -9,9 +9,13 @@ class CreateRandomNumberTest {
 
   @Test
   void numberMaker() {
-    //길이 검사
+    // 길이 검사
     String testNumber = CreateRandomNumber.numberMaker();
     assertThat(testNumber.length()).isEqualTo(3);
 
+    // 중복 검사
+    for (int i = 0; i < testNumber.length(); i++) {
+      assertThat(testNumber.charAt(i)).isBetween('1', '9');
+    }
   }
 }
