318d3648e0895afd687747cc6c65e8c7f20bbcec
hodomaroo <enghemdwkd@gmail.com>
2022-11-08 00:16:49 +0900

    [Feat] : IOManager 클래스 - 사용자 입력을 숫자로 반환하는 기능 구현
    사용자로부터 숫자로 변환 가능한 문자열을 입력받아 Integer형으로 반환하는
    getNextInt 메서드를 구현하였습니다,
    
    잘못된 형식의 입력이 주어질 경우 IllegalArgumentException이 반환되도록
    처리하였습니다.

--- src/main/java/baseball/IOManager.java
+++ src/main/java/baseball/IOManager.java
@@ -1,25 +1,26 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 입력 관련 기능을 처리하는 클래스
  */
 public class IOManager {
+
     /**
      * @return 입력으로 주어진 숫자를 반환
      * @throws IllegalArgumentException 숫자가 아닌 입력이 들어오는 경우 예외 발생
      */
-    public Integer getNextInt() throws IllegalArgumentException{
+    public static Integer getNextInt() throws IllegalArgumentException {
         try {
             return Integer.parseInt(Console.readLine());
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new IllegalArgumentException("잘못된 입력 형식입니다.");
         }
     }
 
-    public void printString(String string){
-        System.out.println(string);
-    }
 }
