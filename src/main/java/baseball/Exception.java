c2f411c5b2ba1ddba659f449199ed62261d63fc8
YUSEONJUN <seonjun0906@gmail.com>
2022-11-07 14:54:57 +0900

    fix : 플레이어가 입력하는 숫자가 1 ~ 9 범위가 아닐때 예외 발생시키는 함수 구현

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -9,4 +9,11 @@ public class Exception {
 			throw new IllegalArgumentException("입력한 수는 세자리여야 합니다.");
 		}
 	}
+
+	public void inputRange(List<Integer> playerNum) throws IllegalArgumentException {
+		if (playerNum.contains(0)) {
+			throw new IllegalArgumentException("1에서 9사이의 숫자가 아닙니다.");
+		}
+	}
+
 }
