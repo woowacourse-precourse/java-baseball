6cc8f211e991f90c2eaac9b912cda93a873b5e68
jin8073 <jin8073@gmail.com>
2022-11-07 21:57:30 +0900

    feat(Application): 게임 종료 후 사용자 입력값이 2이면 프로그램 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
 
     public static void main(String[] args) {
-        Play.playStart();
+        int checkPoint = 0;
+        do {
+            Play.playStart();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            checkPoint = Integer.parseInt(Console.readLine());
+        } while (checkPoint != 2);
+
     }
 }
