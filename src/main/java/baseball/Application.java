1d8f9844cbb350a246462569f1758af31ac8ee3a
JaeHwanCho <makefuture@kakao.com>
2022-11-08 00:51:26 +0900

    docs(guide): planning for get start

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 프로그램
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
