c15871dfb57caee287a97b446ecd0b57ccde5f34
chaerim <qkrdbsk28@naver.com>
2022-11-07 12:23:18 +0900

    test: 입력 값에 0이 포함된 경우 에러 발생

--- src/test/java/baseball/ValidationTest.java
+++ src/test/java/baseball/ValidationTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class ValidationTest {
@@ -25,5 +26,16 @@ public class ValidationTest {
         assertThatThrownBy(() -> inputValidation.validateInputValue(inputValue))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("3자리 숫자만 입력 가능합니다.");
+    }
+
+    @Test
+    @DisplayName("입력 값에 0이 포함된 경우 에러 발생")
+    void inputValueZeroContainedTest() {
+        String inputValue = "140";
+
+        InputValidation inputValidation = new InputValidation();
+        assertThatThrownBy(() -> inputValidation.validateInputValue(inputValue))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1부터 9까지의 숫자만 입력 가능합니다.");
     }
 }
