03750ccd03187140d93e032ac597a4121d6baaed
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-07 18:53:00 +0900

    feat : Controller에서 사용할 객체 생성

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,20 @@
+package baseball.controller;
+
+import baseball.domain.Computer;
+import baseball.domain.Constant;
+import baseball.exception.ComputerException;
+import baseball.service.GameService;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+import camp.nextstep.edu.missionutils.Console;
+
+public class GameController {
+
+    private GameService gameService = new GameService();
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+
+
+
+
+}
