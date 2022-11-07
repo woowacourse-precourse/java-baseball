afef81159ba959a5fa5bbb85c7a6b5b410181cc5
cuttlefish55 <funade7@gmail.com>
2022-11-07 19:21:31 +0900

    feat: Add "PlayerInput.java"
    플레이어의 입력을 받는 클래스 생성.

---
+++ src/main/java/baseball/data/PlayerInput.java
@@ -0,0 +1,20 @@
+package baseball.data;
+
+import camp.nextstep.edu.missionutils.Console;
+
+/**
+ * 플레이어의 데이터 입력을 담당하는 클래스
+ */
+public class PlayerInput {
+    public String inputPlayerString(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String playerString = Console.readLine();
+        return playerString;
+    }
+
+    public String inputRestartString() {
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        String restartString = Console.readLine();
+        return restartString;
+    }
+}
