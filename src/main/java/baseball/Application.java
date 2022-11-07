daf56f41f317e1628d4f0e9eb29a629439c557fc
김재현 <kjhkk1020@naver.com>
2022-11-07 20:14:33 +0900

    feat: (유저 입력이 컴퓨터 생성 수와 다르다면) 스트라이크와 볼 수 를 계산하는 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,17 +2,20 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class Application {
     public static void main(String[] args) {
 
     }
 
-    /** 컴퓨터가 생성하는 1-9 사이의 서로 다른 임의의 숫자로 이루어진 3개의 수로 이루어진 리스트를 생성 */
-    public static List<Integer> getThreeRandomNumbersList() {
+    /**
+     * 컴퓨터가 생성하는 1-9 사이의 서로 다른 임의의 숫자로 이루어진 3개의 수로 이루어진 리스트를 생성
+     */
+    private static List<Integer> getThreeRandomNumbersList() {
         List<Integer> computerNumberList = new ArrayList<>();
 
         while (computerNumberList.size() < 3) {
@@ -25,8 +28,10 @@ public class Application {
         return computerNumberList;
     }
 
-    /** String 형의 유저 입력을 List<Integer>형으로 변환 */
-    public static List<Integer> getStringInputToList(String userInput) {
+    /**
+     * String 형의 유저 입력을 List<Integer>형으로 변환
+     */
+    private static List<Integer> getStringInputToList(String userInput) {
         String[] splitUserInput = userInput.split("");
         List<String> userInputList = new ArrayList<>(List.of(splitUserInput));
         List<Integer> userInputIntegerList = new ArrayList<>();
@@ -38,10 +43,46 @@ public class Application {
         return userInputIntegerList;
     }
 
-    /** 유저로부터 입력받은 값이 컴퓨터가 생성한 값과 일치하는지 판별 */
-    public static boolean isUserComputerNumberCorrespond(String userInput, List<Integer> computerNumber) {
+    /**
+     * 유저로부터 입력받은 값이 컴퓨터가 생성한 값과 일치하는지 판별
+     */
+    private static boolean isUserComputerNumberCorrespond(String userInput, List<Integer> computerNumber) {
         List<Integer> userNumbers = getStringInputToList(userInput);
 
         return userNumbers.equals(computerNumber);
+    }
+
+    /**
+     * 스트라이크 수를 계산
+     */
+    private static int getStrikes(List<Integer> userInputNumber, List<Integer> computerNumber) {
+        int strike = 0;
+
+        /* 스트라이크 수 계산 */
+        for (int i = 0; i < userInputNumber.size(); i++) {
+            if (Objects.equals(userInputNumber.get(i), computerNumber.get(i))) {
+                strike += 1;
+            }
+        }
+
+        return strike;
+    }
+
+    /**
+     * 볼 수를 계산
+     */
+    private static int getBalls(List<Integer> userInputNumber, List<Integer> computerNumber) {
+        int strike = getStrikes(userInputNumber, computerNumber);
+        int ball = 0;
+
+        /* 볼 수 계산 */
+        for (Integer oneOfUserNum : userInputNumber) {
+            if (computerNumber.contains(oneOfUserNum)) {
+                ball += 1;
+            }
+        }
+        ball -= strike; // ball 계산하며 strike 중복 계산 제거
+
+        return ball;
     }
 }
