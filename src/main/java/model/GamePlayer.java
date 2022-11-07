c36cf5ba3ada57dcfe47bd6ee96a8c35614d0283
wonwoo6y <wonwoo6y@gmail.com>
2022-11-08 01:04:13 +0900

    comment: GamePlayer 클래스 주석 기입

--- src/main/java/model/GamePlayer.java
+++ src/main/java/model/GamePlayer.java
@@ -12,6 +12,11 @@ public class GamePlayer {
         numbers = new int[SIZE];
     }
 
+    /**
+     * 숫자로 이루어진 문자열을 입력 받아 배열에 담는다. 서로 다른 SIZE 크기의 자릿수가 아니라면 올바르게 작동하지 않는다.
+     *
+     * @param str 숫자로 이루어진 문자열
+     */
     public void saveNumbers(String str) {
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = (int) (str.charAt(i) - '0');
