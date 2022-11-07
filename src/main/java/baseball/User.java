1e4f4dfe01e1b39d71871cc5b6d122d286ecbc70
KYR <freekyr7529@gmail.com>
2022-11-07 18:39:15 +0900

    refactor : User 클래스를 DTO 형식에 맞게 수정

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,11 +1,23 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class User {
     List<Integer> usersNumber;
 
+    public User() {
+        this.usersNumber = new ArrayList<>();
+    }
     public User(List<Integer> numberList) {
         this.usersNumber = numberList;
+    }
+
+    public void updateUsersNumber (List<Integer> numberList) {
+        this.usersNumber = numberList;
+    }
+
+    public List<Integer> getUsersNumber () {
+        return usersNumber;
     }
 }
