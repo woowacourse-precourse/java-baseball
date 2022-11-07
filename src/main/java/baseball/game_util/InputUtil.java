4acabc70ed99b3fb4b3d8e8e2f6ecfb547ef387e
LeeJejune <leejejune0625@gmail.com>
2022-11-07 14:40:46 +0900

    feat: 유저의 입력값을 가져오는 메서드 구현

--- src/main/java/baseball/game_util/InputUtil.java
+++ src/main/java/baseball/game_util/InputUtil.java
@@ -1,4 +1,22 @@
+package baseball.game_util;
+
 import static baseball.common.Constants.MAX_USER_NUMBER_INPUT;
+
+import baseball.common.Constants;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+public class InputUtil {
+
+    public static int[] getUserThreeInputNumber() {
+        String userInput = Console.readLine();
+        checkUserInputValidate(userInput);
+
+        return changeStringToIntArray(userInput);
+    }
+
     private static void checkUserInputValidate(String userInput) {
         if (!isMaxLength(userInput)
                 || !isDigit(userInput)
@@ -6,6 +24,8 @@ import static baseball.common.Constants.MAX_USER_NUMBER_INPUT;
                 || !isInRange(userInput)) {
             throw new IllegalArgumentException();
         }
+
+    }
 
     private static boolean isMaxLength(String userInput) {
         return (userInput.length() == MAX_USER_NUMBER_INPUT);
