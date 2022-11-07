200c34e36d79aa6b450fc5e5bf3151d075085dc9
madlimeus <108660314+madlimeus@users.noreply.github.com>
2022-11-08 01:47:25 +0900

    Update GameDealer.java
    add manage one game function

--- src/main/java/baseball/GameDealer.java
+++ src/main/java/baseball/GameDealer.java
@@ -7,6 +7,26 @@ import java.util.regex.Pattern;
 
 
 public class GameDealer {
+    public static String Str_Problem_Num = "";
+    public static boolean ONE_GAME_LOOP() throws IllegalArgumentException{
+        boolean result = true;
+        boolean Check_Ans = false;
+        while(!Check_Ans){
+            PRINT_PROMPT();
+            String User_Try = Console.readLine();
+            System.out.println(User_Try);
+            if(!CHECK_VALID_INPUT(User_Try)){
+                throw new IllegalArgumentException();
+            }
+            Check_Ans = CHECK_AND_PRINT_INFO(User_Try);
+            if(Check_Ans){
+                PRINT_GAME_CLEAR_MSG();
+                result = CHECK_GAME_RETRY();
+            }
+
+        }
+        return result;
+    }
     public static boolean ONE_GAME(String[] args) throws IllegalArgumentException{
         boolean result = true;
         Str_Problem_Num = GEN_PROBLEM();
