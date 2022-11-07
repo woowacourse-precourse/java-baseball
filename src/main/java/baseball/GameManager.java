eb4b4a4f0afcc82fe83e357ca9c8e0cae3de4b3b
고동우 <shp05179@gmail.com>
2022-11-07 16:28:58 +0900

    feat: 숫자입력
    사용자가 입력한 숫자를 해시맵 형태로 저장한다

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,12 +1,24 @@
 package baseball;
-
+import camp.nextstep.edu.missionutils.Console;
 import java.util.HashMap;
 
 public class GameManager {
     private static HashMap<Integer,Integer> RandomNumberHash = new HashMap<>();
+    private static HashMap<Integer,Integer> InputNumberHash = new HashMap<>();
 
     void init() {
         RandomNumberHash = Computer.generateRandomNumber();
+    }
+
+    static HashMap<Integer, Integer> scanNumber() {
+        String numberString = Console.readLine();
+        int index=0;
+        for (char character: numberString.toCharArray()) {
+            index++;
+            int digit = character-'0';
+            InputNumberHash.put(digit, index);
+        }
+        return InputNumberHash;
     }
 
 }
