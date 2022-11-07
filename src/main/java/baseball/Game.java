269ba2caf7df11519659c3545990fb042de9d408
unknown <dragontaek98@naver.com>
2022-11-07 19:14:46 +0900

    feat: 게임 init 기능

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import java.util.List;
+import java.util.Map;
+
+public class Game {
+    protected Game(){
+        this.GameProcess();
+    }
+
+    protected void GameProcess(){
+        while(true){
+            System.out.println("숫자 야구 게임을 시작합니다.\n");
+            List<Integer> answer = new Answer().getAnswer();
+            new InGame(answer, false);
+            //결과 출력 메서드 실행
+            Integer input = new UserTerminateInput().getUserTerminate();
+            if(input.equals(2)){
+                break;
+            }
+        }
+
+    }
+}
