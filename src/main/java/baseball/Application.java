b971b319b397f87ecb66c424b8ab496422fd6ef3
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-07 20:16:49 +0900

    feat(isInputFormat, compareNumber): 잘못된 값을 입력할 경우 예외처리\n\n정규식을 사용해서 숫자가 아닌 경우와 3자리가 아닌 경우 예외가 발생하도록 구현\nSet을 사용하여 서로 다른 수가 아닌 경우 예외가 발생하도록 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,9 +2,10 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
     public static List<Integer> pickComputerNumber () {
@@ -18,11 +19,32 @@ public class Application {
         return computer;
     }
 
+    public static void isInputFormat() {
+        String pattern = "^[1-9]{3}$";
+        try {
+            String guessNumber = Console.readLine();
+            if (!guessNumber.matches(pattern)) {
+                throw new IllegalArgumentException();
+            } else if (!compareNumber(guessNumber)) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static boolean compareNumber(String number) {
+        String[] splitArray = number.split("");
+        Set<String> numberSet = new HashSet<>(List.of(splitArray));
+        return splitArray.length == numberSet.size();
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         List<Integer> computerNumber = pickComputerNumber();
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
-        String guessNumber = Console.readLine();
+        isInputFormat();
+
     }
 }
