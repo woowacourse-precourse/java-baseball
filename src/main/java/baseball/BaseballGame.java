9270fbbf7abb56233ef9377c18845cd3e05246b3
LeeJinHo <hosboy93@naver.com>
2022-11-07 16:40:59 +0900

    feat($BaseballGame): 사용자 입력 값 추가
    1. 사용자 값(3자리) 입력 메소드 추가
    2. 입력값 길이(3자리) 체크 메소드 추가
    3. 입력값의 각 자리가 모두 다른지 체크 메소드 추가
    4. 입력값이 숫자만 입력되었는지 체크 메소드 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -10,7 +11,52 @@ public class BaseballGame {
         System.out.println("게임을 시작합니다.");
         int computer = randomNumberBall();
         System.out.println(computer);
+        while(true){
+            gamePlay();
+            break;
+        }
     }
+
+    public static void gamePlay(){
+        System.out.print("숫자를 입력하세요 : ");
+        String user = Console.readLine();
+        if(isValidLength(user) && isValidDiffer(user) && isValidNumber(user)){
+            System.out.println(Integer.parseInt(user));
+            System.out.println("올바른 값이 입력되었습니다.");
+        }
+    }
+
+    public static boolean isValidNumber(String user){
+        try{
+            Integer.parseInt(user);
+            return true;
+        }catch(Exception e){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static boolean isValidDiffer(String user){
+        int userNumber = Integer.parseInt(user);
+
+        int first = userNumber/100;
+        int second = (userNumber%10)/10;
+        int third = userNumber%10;
+
+        if(first-second!=0 && second-third!=0 && third-first!=0){
+            return true;
+        }else{
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static boolean isValidLength(String user) {
+        if(user.length()!=3){
+            System.out.println("3자리 숫자가 아닙니다.");
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
 
     public static int randomNumberBall(){
         List<Integer> computer = new ArrayList<>();
