fe975c6a09faa5c5125ba1f83f785d7170a7da1a
baksohyeon <bori12370@gmail.com>
2022-11-07 21:31:13 +0900

    feat: 야구 점수를 계산하는 클래스 추가
    참고사항: 받아오는 인자값을 ArrayList 에서 String으로 바꿈

--- src/main/java/baseball/ExecuteBaseBallGame.java
+++ src/main/java/baseball/ExecuteBaseBallGame.java
@@ -1,7 +1,6 @@
 package baseball;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class ExecuteBaseBallGame {
     String INDEX_VALUE_ARE_CORRECT = "스트라이크";
@@ -9,44 +8,65 @@ public class ExecuteBaseBallGame {
     String INDEX_VALUE_ARE_WRONG = "낫싱";
 
     // 컴퓨터가 제시한 배열
-    ArrayList<Integer> computer;
+//    ArrayList<Integer> computer;
 
+    String computer;
+    Map<String, Integer> result = new HashMap<String, Integer>();
     // 스트라이크, 볼, 낫싱 점수를 map으로 저장하는 함수
-    public Map<String, Integer> calculateScores(int input) {
-        List<Integer> userNumbers = getUserDigits(input);
+    public Map<String, Integer> calculateScores(String input) {
 
-        Map<String, Integer> result = new HashMap<String, Integer>();
-
-
-        for (int i = 0; i < userNumbers.size(); i++) {
-            int userDigit =  userNumbers.get(i);
+        for (int i = 0; i < input.length(); i++) {
+            int userDigit =  input.charAt(i);
             int findIndex = computer.indexOf(userDigit);
 
             if (i == findIndex) {
                 saveScore(result, INDEX_VALUE_ARE_CORRECT);
-            } else if (i != findIndex && findIndex != -1) {
+            } else if (findIndex != -1) {
                 saveScore(result, VALUE_IS_CORRECT);
-            } else if (findIndex == -1) {
+            } else if (findIndex == -1){
                 saveScore(result, INDEX_VALUE_ARE_WRONG);
             }
         }
         return result;
     }
 
-    public static List<Integer> getUserDigits(int input) {
-        int[] digits = Integer.toString(input).chars().map(c -> c-'0').toArray();
-        return Arrays.stream(digits).boxed().collect(Collectors.toList());
-    }
 
     public static void saveScore(Map<String, Integer> map, String key) {
         int score = 1;
-        if (map.isEmpty()) {
-            map.put(key, 0);
+        if (!map.containsKey(key)) {
+            map.put(key, 1);
         } else {
             map.put(key, map.get(key) + score);
         }
     }
 
+    // 게임결과 출력
+    public String getResultAsString(Map<String, Integer> scores) {
+        StringBuilder res = new StringBuilder();
+
+        if (scores.containsKey(INDEX_VALUE_ARE_WRONG) && scores.get(INDEX_VALUE_ARE_WRONG) == 3) {
+            return INDEX_VALUE_ARE_WRONG;
+        }
+
+        if (scores.containsKey(VALUE_IS_CORRECT)) {
+            res.append(scores.get(VALUE_IS_CORRECT)).append(VALUE_IS_CORRECT).append(" ");
+        }
+
+        if (scores.containsKey(INDEX_VALUE_ARE_CORRECT)) {
+            res.append(scores.get(INDEX_VALUE_ARE_CORRECT)).append(INDEX_VALUE_ARE_CORRECT);
+        }
+
+        return res.toString();
+    }
 
 
+    public boolean isRightAnswer() {
+        if(this.result.containsKey(INDEX_VALUE_ARE_CORRECT) && this.result.get(INDEX_VALUE_ARE_CORRECT) == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료!");
+            return true;
+        } else {
+            this.result.clear();
+            return false;
+        }
+    }
 }
