fd34962f866341face42941b29448ffeb88a4471
hhiyeon <hhyyeon0214@gmail.com>
2022-11-07 20:46:12 +0900

    feat: exception-check all number is numeric

---
+++ src/main/java/baseball/Exception.java
@@ -0,0 +1,14 @@
+package baseball;
+
+class Exception {
+    private static final int RESTART_GAME = 1;
+    private static final int STOP_PROGRAM = 2;
+    private static final int NUMBER_SIZE = 3;
+    private static final int ZERO_IS_NOT_RANGE = 0;
+
+    static void isAllNumeric(String userNumbers) {
+        if(!userNumbers.chars().allMatch(Character::isDigit)){
+            throw new IllegalArgumentException(ResponseMessage.stopExceptionProgram());
+        }
+    }
+}
