04d19f77d9bf464dd17c07eab6c0ab28f86a3ad8
sjkim <seoungjoo01@gmail.com>
2022-11-08 01:02:55 +0900

    feat(GameService): GameService 클래스 & 게임 시작 기능 구현

---
+++ src/main/java/baseball/Service/GameService.java
@@ -0,0 +1,42 @@
+package baseball.Service;
+
+import baseball.Domain.*;
+
+public class GameService {
+
+    private Computer computer;
+    private Player player;
+    private Hint hint;
+
+    public void gameStart(Computer com, Player player, Hint result) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        setCom(com);
+        setPlayer(player);
+        setHint(result);
+    }
+
+
+    public Player getPlayer() {
+        return player;
+    }
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Computer getComputer() {
+        return computer;
+    }
+    public void setCom(Computer com) {
+        this.computer = com;
+    }
+
+
+    public Hint getHint() {
+        return hint;
+    }
+    public void setHint(Hint hint) {
+        this.hint = hint;
+    }
+
+
+}
