4a2f72dae44f79fed2bc4a368055052d64681eaf
Jeongthingamajig <netpetet@naver.com>
2022-11-07 23:54:55 +0900

    Refactor : 메서드 변경
    반환 해주는 computerNumber를 불변객체로 만들어 유저가 입력 할 때마다 갱신이 되지 않도록 한다

--- src/main/java/baseball/model/ComputerNumber.java
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -4,22 +4,22 @@ import baseball.util.NumberNaming;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 /*
 컴퓨터 숫자를 구해서 List로 반환
  */
 public class ComputerNumber {
 
-
-    public static List<Integer> getComputerRandomNumber() {
+    public List<Integer> getComputerRandomNumber() {
         List<Integer> computerNumber = new ArrayList<>();
         while (computerNumber.size() < NumberNaming.MAX_NUMBER_SIZE) {
             createRandomNumber(computerNumber, Randoms.pickNumberInRange(NumberNaming.MIN_RANGE_NUM, NumberNaming.MAX_RANGE_NUM));
         }
-        return computerNumber;
+        return computerNumber = Collections.unmodifiableList(computerNumber);
     }
 
-    public static void createRandomNumber(List<Integer> RandomNumberList, int number) {
+    public void createRandomNumber(List<Integer> RandomNumberList, int number) {
         if (!RandomNumberList.contains(number)) {
             RandomNumberList.add(number);
         }
