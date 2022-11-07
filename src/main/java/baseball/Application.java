35a2235ffce64509ee3ce2015a7012c432a20443
hantae <hantae96@naver.com>
2022-11-07 14:56:07 +0900

    Feat : 사용자에게 서로 다른 수 3개를 입력받는 기능 추가
    컴퓨터와 같을 때까지 비교하는 기능은 추후 #5번 기능을 추가하고 수정할 예정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,28 +1,92 @@
 package baseball;
-import java.lang.reflect.Array;
 import java.util.*;
 import camp.nextstep.edu.missionutils.*;
+
+
 public class Application {
     public static void main(String[] args) {
 
         // TODO: 프로그램 구현
         BaseballGame Game = new BaseballGame();
-        List<Integer> list=Game.makeRandomNumber();
-        System.out.println(list);
+        List<Integer> computerNumberList=Game.makeRandomNumber();
+        List<List<Integer>> userNumberList = Game.inputNumber();
+
+        System.out.println(computerNumberList);
+        System.out.println(userNumberList);
+
+
+//        Game.compareList(computerNumberList,userNumberList);
     }
 }
 
 class BaseballGame {
     //숫자 3개를 임의로 추출 하고 컴퓨터에 해당하는 변수에 할당하는 기능
-    List<Integer> makeRandomNumber(){
+    List<Integer> makeRandomNumber() {
         List<Integer> computer = new ArrayList<>();
-        while(computer.size()<3){
-            int randomNumber = Randoms.pickNumberInRange(1,9);
-            if(!computer.contains(randomNumber)){
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
         return computer;
     }
-}
+
+    List<List<Integer>> inputNumber() {
+        Scanner sc = new Scanner(System.in);
+        List<List<Integer>> numberList = new ArrayList<>();
+        //입력값의 길이가 3인지 확인하는 기능(#22)
+        while (numberList.size() < 3) {
+            int number = sc.nextInt();
+            // 자리수를 분리
+            List<Integer> tmpList = splitNumber(number);
+            numberList.add(tmpList);
+        }
+        return numberList;
+    }
+
+    List<Integer> splitNumber(int number) {
+        List<Integer> list = new ArrayList<>();
+        while (number > 0) {
+            int div = number % 10;
+            list.add(div);
+            number = number / 10;
+        }
+        Collections.reverse((list));
+        return list;
+    }
+//
+//    void compareList(List<Integer> computerNumberList, List<List<Integer>> userNumberList) {
+//        int stikeCount = 0;
+//        int ballCount = 0;
+//        for (int userIndex = 0; userIndex < userNumberList.size(); userIndex++) {
+//            stikeCount += countStike(stikeCount, computerNumberList, userNumberList.get(userIndex));
+//            ballCount += countBall(ballCount, computerNumberList, userNumberList.get(userIndex));
+//        }
+//
+//        System.out.println(stikeCount);
+//        System.out.println(ballCount);
+
+    }
+
+//
+//    int countBall(int ballCount, List<Integer> computerNumberList, List<Integer> userNumberList) {
+//        for (int i = 0; i < userNumberList.size(); i++) {
+//            if (computerIndex != i && computerNumberList.get(computerIndex) == userNumberList.get(i)) {
+//                ballCount++;
+//            }
+//        }
+//
+//        return ballCount;
+//    }
+//
+//    int countStike(int stikeCount, List<Integer> computerNumberList, List<Integer> userNumberList) {
+//        for (int i = 0; i < userNumberList.size(); i++) {
+//            if (computerNumberList.get(i) == userNumberList.get(i)) {
+//                stikeCount++;
+//            }
+//        }
+//        return stikeCount;
+//    }
+
 
