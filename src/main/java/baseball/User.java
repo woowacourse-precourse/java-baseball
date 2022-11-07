7374cf1707181432199fe756b323e988509783f9
kytmt <tlsrka649@naver.com>
2022-11-08 07:24:44 +0900

    refactor(자바 코드 컨벤션에 맞추어 수정): 자바 코드 컨벤션에 맞추어 수정
    기존 코드가 자바 코드 컨벤션에 맞지 않는 부분이 있어서 수정함

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -3,6 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
+
 	int ball;
 	int strike;
 	String num;
@@ -13,36 +14,43 @@ public class User {
 		strike = 0;
 		result = "";
 	}
-	public void inputNum(){
+
+	public void inputNum() {
 		num = Console.readLine();
 		checkException();
 	}
+
 	public void calResult() {
-		if(ball != 0 && strike != 0) {
+		if (ball != 0 && strike != 0) {
 			result = ball + "볼" + " " + strike + "스트라이크";
 			return;
 		}
-		if(ball != 0) {
+		if (ball != 0) {
 			result = ball + "볼";
 			return;
 		}
-		if(strike != 0) {
+		if (strike != 0) {
 			result = strike + "스트라이크";
 			return;
 		}
-		if(result.isEmpty())
+		if (result.isEmpty()) {
 			result = "낫싱";
+		}
 	}
+
 	public String getResult() {
 		return result;
 	}
 
 	private void checkException() {
-		if(num.length() > 3)
+		if (num.length() > 3) {
 			throw new IllegalArgumentException();
+		}
 
-		for(int index = 0; index<num.length(); index++)
-			if(num.charAt(index) < '1' || num.charAt(index) > '9')
+		for (int index = 0; index < num.length(); index++) {
+			if (num.charAt(index) < '1' || num.charAt(index) > '9') {
 				throw new IllegalArgumentException();
+			}
+		}
 	}
 }
