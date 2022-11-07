dafb9c65301e391f379ed98641820079339c020c
cuttlefish55 <funade7@gmail.com>
2022-11-07 14:28:17 +0900

    test: Add 2 test cases
    1. 두번의 재시작에도 정상 작동하는지 검사.
    2. 재시작 여부를 입력받을 때 예외 발생 여부.

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,6 +21,27 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void CustomTestCase1(){
+        assertRandomNumberInRangeTest(
+            () -> {
+                run("175", "247", "427", "1", "374", "958", "985", "1", "263", "265", "256", "156", "2");
+                assertThat(output()).contains("1볼", "2볼 1스트라이크", "3스트라이크", "낫싱", "2볼", "2스트라이크", "게임 종료");
+            },
+            4, 2, 7, 9, 8, 5, 1, 5, 6
+        );
+    }
+
+    @Test
+    void CustomTestCase2(){
+        assertRandomNumberInRangeTest(
+            () -> {
+                assertThatThrownBy(() -> runException("315", "153", "513", "0"))
+                    .isInstanceOf(IllegalArgumentException.class);
+            },
+            5, 1, 3
+        );
+    }
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
