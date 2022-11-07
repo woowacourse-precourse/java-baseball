2fc359c2c46295826f4f1515cc1fc968eb08fff9
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-07 11:07:38 +0900

    feat(RandomUtility): 서로 다른 수의 난수로 생성하기 기능 추가
    docs: 서로 다른 수의 난수로 생성 완료 체크

--- src/main/java/baseball/controller/RandomUtility.java
+++ src/main/java/baseball/controller/RandomUtility.java
@@ -11,13 +11,29 @@ public class RandomUtility {
         int[] randomNumbers = new int[BASEBALL_GAME_NUMBER_LENGTH];
 
         for (int i = 0; i < BASEBALL_GAME_NUMBER_LENGTH; i++) {
-            randomNumbers[i] = generateRandomNumber();
+            randomNumbers[i] = generateRandomNumber(randomNumbers);
         }
 
         return randomNumbers;
     }
 
-    private int generateRandomNumber() {
-        return Randoms.pickNumberInRange(BASEBALL_GAME_RANDOM_RANGE_START, BASEBALL_GAME_RANDOM_RANGE_END);
+    private int generateRandomNumber(int[] randomNumbers) {
+        while (true) {
+            int randomNumber = Randoms.pickNumberInRange(BASEBALL_GAME_RANDOM_RANGE_START, BASEBALL_GAME_RANDOM_RANGE_END);
+
+            if (isDifferentNumber(randomNumbers, randomNumber)) {
+                return randomNumber;
+            }
+        }
+    }
+
+    private boolean isDifferentNumber(int[] randomNumbers, int randomNumber) {
+        for (int num : randomNumbers) {
+            if (num == randomNumber) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
