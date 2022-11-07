9ce1694cc89980e53a18504f4fca022b4a5106a1
saewoo1 <wooozooo1123@gmail.com>
2022-11-07 21:13:23 +0900

    test(InputValidatorTest): remove import static

--- src/test/java/baseball/InputValidatorTest.java
+++ src/test/java/baseball/InputValidatorTest.java
@@ -2,40 +2,40 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.assertj.core.api.Assertions;
 // 올바르지 않은 입력값이면 throw를 하고 있는지
 class InputValidatorTest {
   //길이에 대한 검사
   @Test
   void isRightLength() {
-    assertThatThrownBy(() -> InputValidator.isRightLength("1234"))
+    Assertions.assertThatThrownBy(() -> InputValidator.isRightLength("1234"))
             .isInstanceOf(IllegalArgumentException.class);
   }
 
 // 중복 검사
   @Test
   void isDuplicated() {
-    assertThatThrownBy(() -> InputValidator.isDuplicated("122"))
+    Assertions.assertThatThrownBy(() -> InputValidator.isDuplicated("122"))
             .isInstanceOf(IllegalArgumentException.class);
   }
 
   //숫자만 들어있는지
   @Test
   void isNumber() {
-    assertThatThrownBy(() -> InputValidator.isNumber("a24"))
+    Assertions.assertThatThrownBy(() -> InputValidator.isNumber("a24"))
             .isInstanceOf(IllegalArgumentException.class);
   }
 
   @Test
   void isValidNumber() {
 
-    assertThatThrownBy(() -> InputValidator.isValidNumber("a25"))
+    Assertions.assertThatThrownBy(() -> InputValidator.isValidNumber("a25"))
             .isInstanceOf(IllegalArgumentException.class);
   }
 
   @Test
   void isValidButton() {
-    assertThatThrownBy(() -> InputValidator.isValidButton("a"))
+    Assertions.assertThatThrownBy(() -> InputValidator.isValidButton("a"))
             .isInstanceOf(IllegalArgumentException.class);
   }
 }
