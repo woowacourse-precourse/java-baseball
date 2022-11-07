d58e5808ab19222e0066be33f1cebba8d2c80f10
kytmt <tlsrka649@naver.com>
2022-11-08 07:24:52 +0900

    refactor(자바 코드 컨벤션에 맞추어 수정): 자바 코드 컨벤션에 맞추어 수정
    기존 코드가 자바 코드 컨벤션에 맞지 않는 부분이 있어서 수정함

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,17 +1,21 @@
 package baseball;
 
 public class View {
-  public static void start() {
-    System.out.println("숫자 야구 게임을 시작합니다.");
-  }
-  public static void waitInput() {
-    System.out.println("숫자를 입력해주세요 : ");
-  }
-  public static void result(String userResult) {
-    System.out.println(userResult);
-  }
-  public static void finish() {
-    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력해주세요.");
-  }
+
+	public static void start() {
+		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
+
+	public static void waitInput() {
+		System.out.println("숫자를 입력해주세요 : ");
+	}
+
+	public static void result(String userResult) {
+		System.out.println(userResult);
+	}
+
+	public static void finish() {
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력해주세요.");
+	}
 }
