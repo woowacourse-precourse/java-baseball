149494aa5cd07bb95248457ff73178f544c8b983
YuHayeon <youha0930@gmail.com>
2022-11-07 11:57:49 +0900

    test(RandomUtilsTest): 3개의 숫자가 모두 다른 숫자인지 테스트
    Random값 3개의 숫자가 중복없이 서로 다른 숫자인지 테스트

--- src/test/java/baseball/utils/RandomUtilsTest.java
+++ src/test/java/baseball/utils/RandomUtilsTest.java
@@ -2,7 +2,9 @@ package baseball.utils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.Test;
 
 class RandomUtilsTest {
@@ -11,5 +13,12 @@ class RandomUtilsTest {
     void 랜덤값_길이_확인() {
         List<Integer> result = RandomUtils.pickThreeRandomNumbers();
         assertThat(result.size()).isEqualTo(RandomUtils.RANDOM_NUMBERS_SIZE);
+    }
+
+    @Test
+    void 랜덤값_check() {
+        List<Integer> result = RandomUtils.pickThreeRandomNumbers();
+        Set<Integer> duplicateResult = new HashSet<>(result);
+        assertThat(result.size()).isEqualTo(duplicateResult.size());
     }
 }
