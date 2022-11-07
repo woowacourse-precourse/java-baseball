fe3fa0ebf656a800040834909e11ea869007f11e
swonny <wonny921@gmail.com>
2022-11-07 19:23:36 +0900

    refactor(Exception): 숫자 범위 예외 처리 추가, 변수명 변경

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -9,26 +9,33 @@ public class Exception {
         }
     }
 
+    public static void isNumberOutOfRange(String input) {
+        String regex = "^[1-9]*$";
+        if (!input.matches(regex)) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
 
 class PlayerNumbersException extends Exception {
     final static int NUMBER_LENGTH = 3;
 
-    public static void verifyPlayerNumbers(String gamePlayerNumberInput) {
-        verifyNumberLength(gamePlayerNumberInput);
-        verifyUniqueNumber(gamePlayerNumberInput);
-        hasCharacters(gamePlayerNumberInput);
+    public static void verifyPlayerNumbers(String gamePlayerNumbers) {
+        verifyNumberLength(gamePlayerNumbers);
+        verifyUniqueNumber(gamePlayerNumbers);
+        hasCharacters(gamePlayerNumbers);
+        isNumberOutOfRange(gamePlayerNumbers);
     }
 
-    private static void verifyNumberLength(String stringGamePlayerInput) {
-        if (stringGamePlayerInput.length() != NUMBER_LENGTH) {
+    private static void verifyNumberLength(String gamePlayerNumbers) {
+        if (gamePlayerNumbers.length() != NUMBER_LENGTH) {
             throw new IllegalArgumentException();
         }
     }
 
-    private static void verifyUniqueNumber(String gamePlayerInput) {
+    private static void verifyUniqueNumber(String gamePlayerNumbers) {
         for (int stringIndex = 0; stringIndex < NUMBER_LENGTH; stringIndex++) {
-            if (gamePlayerInput.lastIndexOf(gamePlayerInput.charAt(stringIndex)) != stringIndex) {
+            if (gamePlayerNumbers.lastIndexOf(gamePlayerNumbers.charAt(stringIndex)) != stringIndex) {
                 throw new IllegalArgumentException();
             }
         }
@@ -37,25 +44,25 @@ class PlayerNumbersException extends Exception {
 }
 
 class GameRepeatMessageException extends Exception {
-    final static Integer EXCEPTION4_NUMBER_LENGTH = 1;
-    final static String ANSWER_REPEAT_GAME = "1";
+    final static Integer NUMBER_LENGTH = 1;
+    final static String ANSWER_RESTART_GAME = "1";
     final static String ANSWER_QUIT_GAME = "2";
 
-    public static void verifyGameRepeatMessage(String input) {
-        input = input.trim();
-        verifyMessageLength(input);
-        hasCharacters(input);
-        checkYesOrNo(input);
+    public static void verifyGameRepeatMessage(String answerOfRestartGame) {
+        answerOfRestartGame = answerOfRestartGame.trim();
+        verifyMessageLength(answerOfRestartGame);
+        hasCharacters(answerOfRestartGame);
+        checkYesOrNo(answerOfRestartGame);
     }
 
-    public static void verifyMessageLength(String input) {
-        if (input.length() != EXCEPTION4_NUMBER_LENGTH) {
+    public static void verifyMessageLength(String answerOfRestartGame) {
+        if (answerOfRestartGame.length() != NUMBER_LENGTH) {
             throw new IllegalArgumentException("exception occurs at checkInputLength()");
         }
     }
 
     public static void checkYesOrNo(String input) {
-        if (!input.contains(ANSWER_REPEAT_GAME) && !input.contains(ANSWER_QUIT_GAME)) {
+        if (!input.contains(ANSWER_RESTART_GAME) && !input.contains(ANSWER_QUIT_GAME)) {
             throw new IllegalArgumentException("exception occurs at checkYesOrNo()");
         }
     }
