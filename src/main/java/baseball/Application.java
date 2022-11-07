6300b1dcfacfb7d8733a3e121ff70244e830fb69
ouohoon <begreat@kakao.com>
2022-11-07 18:56:37 +0900

    fix: const value of Application class
    Application class의 BALL_INDEX 값을 1로 수정하고 사용하지 않는 상수값 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,9 +11,9 @@ public class Application {
     private static final String CONTINUE_INPUT_TEXT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
             "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final int STRIKE_INDEX = 0;
-    private static final int BALL_INDEX = 0;
+    private static final int BALL_INDEX = 1;
     private static final int CONTINUE = 1;
-    private static final int EXIT = 2;
+
     public static void main(String[] args) {
         Computer computer = new Computer();
         boolean isContinue;
