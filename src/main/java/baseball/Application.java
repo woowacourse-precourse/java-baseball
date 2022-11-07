10b491a57b293e11321d56909e597fdf2d916091
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-07 20:33:39 +0900

    Feat: add func Creating ComputerListValue & Checking repeat

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Map<String, List<Integer>> inputValue = new HashMap<>();
+        int start = 1;
+    }
+    public static List<Integer> inputComputer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randNum = Randoms.pickNumberInRange(1, 9);
+            computer = isContain(computer, randNum);
+        }
+        return computer;
+    }
+    public static List<Integer> isContain(List<Integer> list, int value) {
+        if (!list.contains(value)) {
+            list.add(value);
+        }
+        return list;
     }
 }
