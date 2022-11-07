60eeb6845129c46625ab466dadd035034007eac8
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-07 22:50:29 +0900

    fix(service): 중복 입력 검증 추가

--- src/main/java/custom/controller/BaseBallController.java
+++ src/main/java/custom/controller/BaseBallController.java
@@ -3,6 +3,8 @@ package custom.controller;
 import custom.dto.Response;
 import custom.service.BaseBallService;
 import custom.service.vo.ReTry;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,7 +21,8 @@ public class BaseBallController {
     }
 
     public Response matchNumber(String input) {
-        validateNumber(input);
+        validateNumberFormat(input);
+        validateDuplicateNumber(input);
         return baseBallService.matchNumber(input);
     }
 
@@ -29,11 +32,21 @@ public class BaseBallController {
     }
 
 
-    private void validateNumber(String input) throws IllegalArgumentException {
+    private void validateNumberFormat(String input) throws IllegalArgumentException {
         Pattern pattern = Pattern.compile("^\\d{3}$");
         Matcher matcher = pattern.matcher(input);
         if (!matcher.matches()) {
             throw new IllegalArgumentException("숫자 형태의 3글자 입력만 가능합니다");
+        }
+    }
+
+    private void validateDuplicateNumber(String input)  {
+        List<Character> list = new ArrayList<>();
+        for (int i = 0; i < input.length(); ++i) {
+            if (list.contains(input.charAt(i))) {
+                throw new IllegalArgumentException("중복된 숫자는 입력이 불가능합니다");
+            }
+            list.add(input.charAt(i));
         }
     }
 }
