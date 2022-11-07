927f93b9c79bfd5b58602d8169d162ea944bc1f2
yoon <beatus96@naver.com>
2022-11-07 18:04:18 +0900

    refactor : 클래스 분리
    1) Application : 실행
    2) Controller : 게임 로직
    3) View : 입출력(UI)
    4) Model : 게임 요소
    5) Validator : 유효성 체크
    6) Rule : 게임 룰과 관련된 상수(enum클래스)

---
+++ src/main/java/baseball/Rule.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public enum Rule {
+
+    DISITS(3) , RANDOM_MIN(1), RANDOM_MAX(9), COMMAND_RESTART(1),COMMAND_END(2);
+    private final int value;
+    Rule(int value) {
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
+}
