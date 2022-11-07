6384f1b3e2214a2e3d46737fce2fbef79e54a025
aud19 <aud19980518@gmail.com>
2022-11-07 17:15:56 +0900

    문장 만드는 함수를 따로 뺌 0을 ZERO로 기호상수 추가

--- src/main/java/baseball/view/PlayingGameView.java
+++ src/main/java/baseball/view/PlayingGameView.java
@@ -5,6 +5,7 @@ public class PlayingGameView extends GameView {
     private static final String OUTPUT_BALL = "볼 ";
     private static final String OUTPUT_STRIKE = "스트라이크";
     private static final String OUTPUT_NOTHING = "낫싱";
+    private static final int ZERO = 0;
     public void enterNumberPrint() {
         StringBuilder sb = new StringBuilder();
         sb.append(OUTPUT_ENTER_NUMBER);
@@ -13,16 +14,24 @@ public class PlayingGameView extends GameView {
 
     public void gameResultPrint(int strike, int ball) {
         StringBuilder output = new StringBuilder();
-        if (ball > 0) {
-            output.append(ball + OUTPUT_BALL);
+        if (ball > ZERO) {
+            outputBuilder(output, ball, OUTPUT_BALL);
         }
-        if (strike > 0) {
-            output.append(strike + OUTPUT_STRIKE);
+        if (strike > ZERO) {
+            outputBuilder(output, strike, OUTPUT_STRIKE);
         }
-        if (strike == 0 && ball == 0) {
-            output.append(OUTPUT_NOTHING);
+        if (strike == ZERO && ball == ZERO) {
+            outputBuilder(output, OUTPUT_NOTHING);
         }
         newLine(output);
         print(output.toString());
+    }
+
+    public void outputBuilder(StringBuilder output, int judgmentCount, String judgmentString) {
+        output.append(judgmentCount + judgmentString);
+    }
+
+    public void outputBuilder(StringBuilder output, String judgmentString) {
+        output.append(judgmentString);
     }
 }
