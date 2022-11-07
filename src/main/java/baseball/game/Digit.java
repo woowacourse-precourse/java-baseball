8d77b0834fd8ecfc10faf2513e03f675816cbb28
Kayoung <rkdkdudjd@gmail.com>
2022-11-07 17:34:33 +0900

    refactor: Digit 클래스 정의
    Number의 자릿수 하나를 Digit 객체로 관리하기 위해 클래스를 정의했습니다.

---
+++ src/main/java/baseball/game/Digit.java
@@ -0,0 +1,35 @@
+package baseball.game;
+
+import java.util.Objects;
+
+public class Digit {
+    private final int digitValue;
+
+    public Digit(int digitValue) {
+        validateDigitValue(digitValue);
+        this.digitValue = digitValue;
+    }
+
+    private void validateDigitValue(int number) {
+        if (number < 1 || number > 9) {
+            throw new IllegalArgumentException("1 이상 9 이하의 숫자가 아닙니다.");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Digit digit = (Digit) o;
+        return digitValue == digit.digitValue;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(digitValue);
+    }
+}
