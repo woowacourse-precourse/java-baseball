ea0873f6406d58fdfdc9e197b6a7a4c1257b0a11
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-07 15:15:19 +0900

    writeComputer() : 주어진 Randoms 메소드로 1-9 에서 무작위로 숫자를 생성하여 List에 삽입한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -28,5 +28,17 @@ public class Application {
         return result;
     }
 
+    static List<Integer> writeComputer() {
+        List<Integer> result = new ArrayList<>();
+        while (result.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!result.contains(randomNumber)) {
+                result.add(randomNumber);
+            }
+        }
+
+        return result;
+    }
+
 
 }
