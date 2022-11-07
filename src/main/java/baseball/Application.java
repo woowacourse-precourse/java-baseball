f80a58f707f1c272390d5617a8a8b402293beba0
sonbk <sonb9615@gmail.com>
2022-11-07 23:07:40 +0900

    [22.11.07] 주석 삭제 및 console.readLine 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,32 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
-import java.io.*;
 import java.util.*;
 
 public class Application {
 
-    private static FastReader scan = new FastReader();
-    private static StringBuilder sb = new StringBuilder();
+    private static final StringBuilder sb = new StringBuilder();
     private static Map<Integer, Integer> targetNumber = null;
     private static String status = "start";
     private static List<Integer> inputNumber = null;
     private static String inputStr = null;
 
     public static void main(String[] args) {
-        /*
-         * 1. 타겟 랜덤 3자리 숫자 set
-         * 2. input String validation
-         *   1) status : start / end
-         *   2) start : 숫자3개 말고는 다 exception
-         *   3) end : 1/2 말고는 다 exception
-         * 3. 진행
-         *   1) 볼
-         *   2) 스트라이크
-         *   3) 낫싱
-         *   5) 정답
-         * */
         System.out.println("숫자야구를 시작합니다.");
         while("start".equals(status)){
             gameStartFunc();
@@ -36,8 +22,6 @@ public class Application {
     public static void gameStartFunc() {
         try {
             setTargetNumber();
-            /*System.out.print("targetNumber : ");
-            System.out.println(targetNumber);*/
             while("start".equals(status)){
                 System.out.println("숫자를 입력해주세요 : ");
                 input();
@@ -76,7 +60,7 @@ public class Application {
 
     private static void input() {
         try {
-            inputStr = scan.next();
+            inputStr = Console.readLine();
             inputValidation(inputStr);
             inputNumber = new ArrayList<>();
             for(char c : inputStr.toCharArray()){
@@ -150,52 +134,6 @@ public class Application {
         }
         if(sb.toString().length() == 0){
             sb.append("낫싱");
-        }
-    }
-
-    private static class FastReader {
-        BufferedReader br;
-        StringTokenizer st;
-
-        public FastReader() {
-            br = new BufferedReader(new InputStreamReader(System.in));
-        }
-
-        public FastReader(String s) throws FileNotFoundException {
-            br = new BufferedReader(new FileReader(new File(s)));
-        }
-
-        String next() {
-            while (st == null || !st.hasMoreElements()) {
-                try {
-                    st = new StringTokenizer(br.readLine());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            return st.nextToken();
-        }
-
-        int nextInt() {
-            return Integer.parseInt(next());
-        }
-
-        long nextLong() {
-            return Long.parseLong(next());
-        }
-
-        double nextDouble() {
-            return Double.parseDouble(next());
-        }
-
-        String nextLine() {
-            String str = "";
-            try {
-                str = br.readLine();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return str;
         }
     }
 }
