7302d17906e92d671050f56b32aa81c55aa82bdf
Be-student <presentsong@naver.com>
2022-11-07 13:36:28 +0900

    test(Command): 재시도, 종료에 대한 테스트 추가

---
+++ src/test/java/baseball/domain/CommandKeyTest.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CommandKeyTest {
+    @DisplayName("커맨드 키는 1,2만 작동한다")
+    @Test
+    void 커맨드_범위_테스트() {
+        assertThat(CommandKey.from(1)).isEqualTo(CommandKey.RETRY);
+        assertThat(CommandKey.from(2)).isEqualTo(CommandKey.FINISH);
+    }
+
+    @DisplayName("커맨드 키 범위 바깥 체크")
+    @Test
+    void 커맨드_바깥_테스트() {
+        assertThrows(IllegalArgumentException.class, () -> CommandKey.from(0));
+        assertThrows(IllegalArgumentException.class, () -> CommandKey.from(3));
+    }
+}
