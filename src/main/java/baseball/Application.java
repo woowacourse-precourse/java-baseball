a4b39fbd803865f72f79c2f462cd369838f72322
hayeon <qorgkdus12@naver.com>
2022-11-07 15:11:28 +0900

    feat(start game): add start wording

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // 1. 시작 멘트
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
