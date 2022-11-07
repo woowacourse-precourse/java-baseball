bdc24809c0295cc69c010b45d35a21484dd27541
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-08 01:31:43 +0900

    feat(main): 컴퓨터 숫자를 맞출 때까지 게임을 반복

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -59,7 +59,7 @@ public class Application {
 
     public static boolean printScore(Map<String, Integer> scoreMap) {
         int strike, ball;
-        boolean endPoint = false;
+        boolean allStrike = false;
         strike = scoreMap.get("스트라이크");
         ball = scoreMap.get("볼");
 
@@ -68,16 +68,14 @@ public class Application {
         } else if ((strike > 0) && (ball == 0)) {
             System.out.printf("%d스트라이크%n", strike);
             if (strike == 3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                endPoint = setGameSetting();
+                allStrike = true;
             }
         } else if ((strike == 0) && (ball > 0)) {
             System.out.printf("%d볼%n", ball);
         } else {
             System.out.println("낫싱");
         }
-        return endPoint;
+        return allStrike;
     }
 
     public static boolean setGameSetting() {
@@ -99,18 +97,28 @@ public class Application {
         List<Integer> splitNumberArray = new ArrayList<>();
         List<Integer> computerNumber = pickComputerNumber();
         Map<String, Integer> scoreMap;
-
-        System.out.println(computerNumber);
+        boolean stopPoint, gamePoint;
+        stopPoint = true;
 
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.print("숫자를 입력해주세요 : ");
-        guessNumber = Console.readLine();
-        if (isInputFormat(guessNumber)) {
-            splitNumberArray = Arrays.stream(guessNumber.split(""))
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
+        while (stopPoint) {
+            System.out.println(computerNumber);
+
+            System.out.print("숫자를 입력해주세요 : ");
+            guessNumber = Console.readLine();
+            if (isInputFormat(guessNumber)) {
+                splitNumberArray = Arrays.stream(guessNumber.split(""))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+            }
+            scoreMap = countScore(splitNumberArray, computerNumber);
+            gamePoint = printScore(scoreMap);
+            if (gamePoint) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                stopPoint = setGameSetting();
+                computerNumber = pickComputerNumber();
+            }
         }
-        scoreMap = countScore(splitNumberArray, computerNumber);
-        printScore(scoreMap);
     }
 }
