995ad50e360edb1a4a160e1294c636cbcab12b05
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-07 15:36:03 +0900

    feat(ComputerController): 세자리의 난수와 플레이어 입력 숫자 비교 기능 추가
    docs: 세자리의 난수와 플레이어 입력 숫자 비교 완료 체크

--- src/main/java/baseball/controller/ComputerController.java
+++ src/main/java/baseball/controller/ComputerController.java
@@ -3,16 +3,44 @@ package baseball.controller;
 import baseball.model.GameNumber;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
 
 public class ComputerController {
     GameNumber gameNumber = new GameNumber();
     OutputView outputView = new OutputView();
     InputView inputView = new InputView();
 
+    private static final int BASEBALL_GAME_NUMBER_LENGTH = 3;
+    private int strikeCount;
+    private int ballCount;
+
     public void startGame() {
         outputView.printStartGame();
         gameNumber.setComputerNumbers();
-        inputView.enterGameNumber();
+        String inputNumber = Console.readLine();
+        gameNumber.setInputString(inputNumber);
+        int[] playerNumbers = gameNumber.getPlayerNumbers();
+        System.out.println("playerNumbers : " + Arrays.toString(gameNumber.getPlayerNumbers()));
+        for (int i = 0; i < BASEBALL_GAME_NUMBER_LENGTH; i++) {
+            countStrike(playerNumbers[i], i);
+            countBall(playerNumbers[i], i);
+        }
 
+        System.out.println("strick : " + strikeCount + ", ball : " + ballCount);
+    }
+
+    private void countStrike(int playerNumber, int numberIndex) {
+        if (gameNumber.getComputerNumbers()[numberIndex] == playerNumber) {
+            strikeCount++;
+        }
+    }
+
+    private void countBall(int playerNumber, int numberIndex) {
+        int[] computerNumbers = gameNumber.getComputerNumbers();
+        if ((computerNumbers[numberIndex] != playerNumber) && Arrays.stream(computerNumbers).anyMatch(number -> number == playerNumber)) {
+            ballCount++;
+        }
     }
 }
