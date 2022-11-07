e182446568468ab93927b96fc245d2c3294eb067
Yejin Shin <shinyejin0212@gmail.com>
2022-11-07 22:05:34 +0900

    feat($Referee): add isStrike
    4.2.3 strike인지 확인하는 메소드 추가

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -5,8 +5,19 @@ public class Referee {
     public static final String STRIKE = "스트라이크";
     public static final String NOTHING = "낫싱";
 
-    private Opponent computerNumber;
-    private Player quessNumber;
+    private Opponent opponentNumber;
     private int strike;
     private int ball;
+
+    public void saveAnswer(Opponent opponentNumber) {
+        this.opponentNumber = opponentNumber;
+    }
+    private boolean isStrike(Player quessNumber) {
+        if(quessNumber.equals(opponentNumber)){
+            return true;
+        }
+        return false;
+    }
+
+
 }
