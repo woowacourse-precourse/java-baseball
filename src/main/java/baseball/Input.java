e1d7d9bf7a428b7089cd94f9f913c92060b70aa6
Woogie95 <choisw950928@gmail.com>
2022-11-07 16:11:41 +0900

    refactor: Input 시 출력 메시지 추가, RestartNumber 1 or 2 입력 기능 추가

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -5,6 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 public class Input {
 
     public static String inputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
+
+    public static String inputRestartNumber() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
 
