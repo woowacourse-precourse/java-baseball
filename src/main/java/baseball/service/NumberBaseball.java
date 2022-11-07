3cd92ba6704118897785c830ff3290bd9c8e446d
Kloong1 <dkdlsmsm98@gmail.com>
2022-11-07 17:42:02 +0900

    feat: 3자리의 난수를 만드는 메소드 구현
    camp.nextstep.edu.missionutils 에서 제공하는 Randoms 사용함.
    생성자에서 이 메소드를 항상 호출한다.
    test 를 위해 getter를 임시로 추가함. 추후에 삭제가 필요함.

--- src/main/java/baseball/service/NumberBaseball.java
+++ src/main/java/baseball/service/NumberBaseball.java
@@ -6,4 +6,17 @@ import java.util.List;
 
 public class NumberBaseball {
     private List<Integer> answer;
+
+    public NumberBaseball() {
+        generateAnswerNumber();
+    }
+
+    public void generateAnswerNumber() {
+        answer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+    }
+
+    //getter for test code
+    public List<Integer> getAnswer() {
+        return answer;
+    }
 }
