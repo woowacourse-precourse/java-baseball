8dbbea2f1f91bd616f68d9914a3ce681c2754fa2
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-07 20:38:49 +0900

    chore(): TODO 문장 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import baseball.domain.BaseballGame;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         BaseballGame game = BaseballGame.init();
         game.start();
     }
