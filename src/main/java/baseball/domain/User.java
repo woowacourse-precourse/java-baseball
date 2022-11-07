78fc91d0d262549167279a2bfba3034472e1d693
sosow0212 <sosow0212@naver.com>
2022-11-07 10:36:02 +0900

    refactor : 게임 시작시 출력되는 문구를 user 생성자에서 처리했으나, View로 빼기 위해 제거

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -6,10 +6,6 @@ import java.util.List;
 public class User {
     private List<Integer> userGameNumbers = new ArrayList<>();
 
-    public User() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
-
     public List<Integer> getUserGameNumbers() {
         return userGameNumbers;
     }
