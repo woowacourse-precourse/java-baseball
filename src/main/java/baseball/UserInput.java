901a0caee40056ca4fb7b08b3cdcf8be0bf5ca6e
ssssudan <kankgm0324@gmail.com>
2022-11-08 00:00:34 +0900

    <fix>: userInput을 static 메서드로 변경

--- src/main/java/baseball/UserInput.java
+++ src/main/java/baseball/UserInput.java
@@ -9,14 +9,14 @@ import camp.nextstep.edu.missionutils.Console;
  */
 
 public class UserInput {
-    public String userNumberInput() {
+    public static String userNumberInput() {
         System.out.print("숫자를 입력해주새요 : ");
         String userNum = Console.readLine();
         InputError.userNumberInputExceptionCheck(userNum);
         return userNum;
     }
 
-    public String replayOrStop() {
+    public static String replayOrStop() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String choice = Console.readLine();
         InputError.replayOrStopExceptionCheck(choice);
