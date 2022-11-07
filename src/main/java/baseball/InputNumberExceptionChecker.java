f843f5a6a2d2739bec007085ce80bab429960ba5
dener8 <8dener@gmail.com>
2022-11-08 05:32:56 +0900

    refactor: 메서드 전부 static으로 변경
    main에서 객체를 만들지 않고 불러오기 위해서 변경

--- src/main/java/baseball/InputNumberExceptionChecker.java
+++ src/main/java/baseball/InputNumberExceptionChecker.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class InputNumberExceptionChecker {
     // 3글자가 아닐 때
-    public boolean isValidLength(String number) {
+    public static boolean isValidLength(String number) {
         if (number.length() != 3) {
             // 여기서 안내 메세지(GameMessage)도 출력해주면 좋을 듯?! => 통합으로 받으려면 안되겠다
             return false;
@@ -12,7 +12,7 @@ public class InputNumberExceptionChecker {
     }
 
     // 모두 다른 수가 아닐 때
-    public boolean hasDifferentNumberElement(String number) {
+    public static boolean hasDifferentNumberElement(String number) {
         if (number.charAt(0) == number.charAt(1)
                 || number.charAt(1) == number.charAt(2)
                 || number.charAt(2) == number.charAt((0))) {
@@ -23,7 +23,7 @@ public class InputNumberExceptionChecker {
     }
 
     // 숫자에 0이 들어갔는지 확인하는 메서드
-    public boolean hasZero(String number) {
+    public static boolean hasZero(String number) {
         if (number.charAt(0) == 0
                 || number.charAt(1) == 0
                 || number.charAt(2) == 0) {
@@ -34,7 +34,7 @@ public class InputNumberExceptionChecker {
     }
 
     // 재시작 여부를 물었을 때, 1과 2가 아닌 다른 숫자를 입력했을 때
-    public boolean isValidRestartNumber(int restartNumber) {
+    public static boolean isValidRestartNumber(int restartNumber) {
         if (restartNumber != 1 || restartNumber != 2) {
             return false;
         } else {
@@ -43,7 +43,7 @@ public class InputNumberExceptionChecker {
     }
 
     // String type을 parameter로 받아서 판단
-    public boolean hasException(String number) {
+    public static boolean hasException(String number) {
         if (isValidLength(number) && hasDifferentNumberElement(number) && hasZero(number)) {
             return true;
         } else {
@@ -52,7 +52,7 @@ public class InputNumberExceptionChecker {
     }
 
     // int type을 parameter로 받아서 판단 (오버로딩)
-    public boolean hasException(int restartNumber) {
+    public static boolean hasException(int restartNumber) {
         if (isValidRestartNumber(restartNumber)) {
             return true;
         } else {
