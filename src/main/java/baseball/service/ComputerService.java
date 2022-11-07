9fdcf936a4952d4121193ee4094b217dd72c60ab
jjmin9797 <jjmin9797@naver.com>
2022-11-07 18:50:55 +0900

    feat(ComputerService): setComputerNumber 기능 구현 camp.nextstep.edu.missionutils.randoms의 pickUniqueNumbersInRange 메서드를 통해 1~9까지 중복되지 않는 3개의 숫자를 설정하는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

---
+++ src/main/java/baseball/service/ComputerService.java
@@ -0,0 +1,19 @@
+package baseball.service;
+
+import baseball.repository.ComputerRepository;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerService {
+    private ComputerRepository computerRepository = new ComputerRepository();
+
+    public void setComputerNumber() {
+        List<Integer> computerNumbers = Randoms.pickUniqueNumbersInRange(1,9,3);
+        int firstNumber = computerNumbers.get(0);
+        int secondNumber = computerNumbers.get(1);
+        int thirdNumber = computerNumbers.get(2);
+        computerRepository.setComputer(firstNumber,secondNumber,thirdNumber);
+    }
+}
