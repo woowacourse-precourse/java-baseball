1429ebc47504f096361073f1083055f363c7f39a
Woojin Lee <dhkep03@gmail.com>
2022-11-07 23:54:22 +0900

    feat(TurnScore): 객체 상태 및 생성자 구현

--- src/main/java/baseball/model/TurnScore.java
+++ src/main/java/baseball/model/TurnScore.java
@@ -1,4 +1,17 @@
 package baseball.model;
 
+import baseball.type.ResultTurn;
+
 public class TurnScore {
+    private int balls;
+    private int strikes;
+    private ResultTurn resultTurn;
+
+    TurnScore() {}
+
+    TurnScore(int balls, int strikes, ResultTurn resultTurn) {
+        this.balls = balls;
+        this.strikes = strikes;
+        this.resultTurn = resultTurn;
+    }
 }
