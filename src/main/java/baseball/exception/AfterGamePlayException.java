5e227662f4f649185b07fee200cd0cd932f5bd32
sosow0212 <sosow0212@naver.com>
2022-11-07 20:20:55 +0900

    refactor : 유저의 숫자 입력 예외처리 중, if문의 조건 부분을 메서드를 따로 만들어서 분리

--- src/main/java/baseball/exception/AfterGamePlayException.java
+++ src/main/java/baseball/exception/AfterGamePlayException.java
@@ -10,14 +10,28 @@ public class AfterGamePlayException {
     }
 
     public void isRightLength(String numberTypeOfString) {
-        if (numberTypeOfString.length() != PERMIT_NUMBER_LENGTH) {
+        if (!isUserInputLengthPermitted(numberTypeOfString)) {
             throw new IllegalArgumentException("하나의 숫자만 입력하셔야 합니다.");
         }
     }
 
     public void isRightNumber(String numberOfString) {
-        if (!numberOfString.matches(PERMIT_NUMBER_REGEX)) {
+        if (!isUserInputContainsOnlyPermittedNumbers(numberOfString)) {
             throw new IllegalArgumentException("1~2 까지의 숫자만 입력하셔야합니다.");
         }
+    }
+
+    public boolean isUserInputLengthPermitted(String numberTypeOfString) {
+        if (numberTypeOfString.length() != PERMIT_NUMBER_LENGTH) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isUserInputContainsOnlyPermittedNumbers(String numberTypeOfString) {
+        if (!numberTypeOfString.matches(PERMIT_NUMBER_REGEX)) {
+            return false;
+        }
+        return true;
     }
 }
