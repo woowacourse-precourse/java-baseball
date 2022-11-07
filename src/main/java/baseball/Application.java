1ac7de88213638fd76169070c7ce5553dde7b098
ckn1070 <ckn1070@naver.com>
2022-11-08 01:50:10 +0900

    feat(generateRandomNumber): add method for generating 3 digits of random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static List<Integer> generateRandomNumber() {
+        List<Integer> generatedNumber = new ArrayList<>();
+        while (generatedNumber.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!generatedNumber.contains(randomNumber)){
+                generatedNumber.add(randomNumber);
+            }
+        }
+        return generatedNumber;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
