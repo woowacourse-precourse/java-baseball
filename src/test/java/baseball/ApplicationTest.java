5b5ffa86a3372fb91e11d65823d95fe6c4ab54b4
dmajorcode <dianekang93@gmail.com>
2022-11-07 23:18:15 +0900

    feat: test case added ✅

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -5,8 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -31,5 +30,22 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    @Test
+    void error_test(){
+        String userAns = Integer.toString(3);
+        assertThatThrownBy(()->Checker.validateUserAns(userAns)).isInstanceOf(IllegalArgumentException.class);
+    }
+    
+    @Test
+    void num_test() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("106","630","2");
+                    assertThat(output()).contains("2볼", "3스트라이크", "게임 종료");
+                },
+                6,3,0
+        );
     }
 }
