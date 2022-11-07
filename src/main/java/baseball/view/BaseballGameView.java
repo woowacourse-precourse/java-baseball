3b59efc2ed5d4e3a4d5a4a5b606211bb7cd95250
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-07 18:31:55 +0900

    feat: BaseballGameView - inputNumber 메서드 추가
    입력값을 View에서 처리하도록 생성

--- src/main/java/baseball/view/BaseballGameView.java
+++ src/main/java/baseball/view/BaseballGameView.java
@@ -1,6 +1,9 @@
 package baseball.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class BaseballGameView {
+
 
     public void printStartGame() {
         System.out.print("숫자 야구 게임을 시작합니다.");
@@ -34,5 +37,10 @@ public class BaseballGameView {
 
     public void printInputNumber() {
         System.out.print("\n숫자를 입력해주세요 : ");
+    }
+
+    public String inputNumber() {
+        String inputNumber = readLine();
+        return inputNumber;
     }
 }
