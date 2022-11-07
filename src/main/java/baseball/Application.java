4bc52b9c81bbf75778d17ccb1a2f97e9671b2a84
김산 <kimsan@gimsan-ui-MacBookAir.local>
2022-11-07 23:58:37 +0900

    [2] feat: 사용자 입력 예외처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,6 +10,7 @@ public class Application {
     public static void main(String[] args) {
         String computerNumbers = getRandomNumbers(3);
         String userNumbers = readLine();
+        checkUserNumbersValidation(userNumbers);
     }
 
     private static String getRandomNumbers(int numbers) {
@@ -21,6 +22,17 @@ public class Application {
             }
         }
         return randomNumbers.toString();
+    }
+
+    private static void checkUserNumbersValidation(String userNumbers) {
+        if (userNumbers.length() != 3) {
+            throw new IllegalArgumentException("3자리 숫자를 입력해주세요.");
+        }
+        try {
+            Integer.parseInt(userNumbers);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자를 입력해주세요.");
+        }
     }
 }
 
