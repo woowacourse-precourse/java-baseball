a3b5c9715d84c625b6520299077a3fe3fcec0753
lullulu__ <75472116+earthyoung@users.noreply.github.com>
2022-11-07 17:25:12 +0900

    refactor: add baseball() method to reduce number of while loops

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,17 +13,21 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
         while(true){
             generateAnswer();
-            while(true){
-                System.out.print("숫자를 입력해주세요: ");
-                checkValidInput(Console.readLine());
-                boolean result = checkResult();
-                if(result){
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    break;
-                }
-            }
+            baseball();
             boolean exit = checkExitGame();
             if(exit) break;
+        }
+    }
+
+    public static void baseball(){
+        while(true){
+            System.out.print("숫자를 입력해주세요: ");
+            checkValidInput(Console.readLine());
+            boolean result = checkResult();
+            if(result){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
         }
     }
 
