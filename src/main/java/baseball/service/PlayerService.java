a53583b117be3835a62170f3554f4a283f62af49
jjmin9797 <jjmin9797@naver.com>
2022-11-07 18:25:15 +0900

    feat(PlaterService): getPlayerNumber 기능 구현 플레이어가 입력한 정보를 가져오는 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/baseball/service/PlayerService.java
+++ src/main/java/baseball/service/PlayerService.java
@@ -7,11 +7,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class PlayerService {
     private GameService gameService = new GameService();
-    private PlayerRepository playerRepository;
+    private PlayerRepository playerRepository = new PlayerRepository();
 
-    public void setPlayerNumber() {
 
-    }
 
     public ArrayList<Integer> inputPlayerNumber() {
         ArrayList<Integer> playerNumberList = new ArrayList<>();
@@ -25,11 +23,18 @@ public class PlayerService {
 
 
     public void setPlayerNumber(ArrayList<Integer> playerNumberList){
-        playerRepository = new PlayerRepository();
         int firstNumber = playerNumberList.get(0);
         int secondNumber = playerNumberList.get(1);
         int thirdNumber = playerNumberList.get(2);
         playerRepository.setPlayer(firstNumber,secondNumber,thirdNumber);
+    }
+
+    public ArrayList<Integer> getPlayerNumber() {
+        ArrayList<Integer> playerNumberList = new ArrayList<>();
+        playerNumberList.add(playerRepository.getPlayer().getFirstNumber());
+        playerNumberList.add(playerRepository.getPlayer().getSecondNumber());
+        playerNumberList.add(playerRepository.getPlayer().getThirdNumber());
+        return playerNumberList;
     }
 
 
