5b1e1ceee29fc0db120393e83b22f2e4666e43e2
Choi Seongwon <vlflalektzja@naver.com>
2022-11-08 01:33:15 +0900

    feat: 컴퓨터의 수 임의 생성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,11 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
     public BaseballGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public List<Integer> getRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     public void run() {
-        
+        User user = new User();
+        List<Integer> computer = this.getRandomNumbers();
     }
 }
