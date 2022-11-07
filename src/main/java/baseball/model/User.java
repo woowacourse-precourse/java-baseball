1c568eec012e0feade20e70660ce2b35945146bc
KimJaeSung <behappy0916@naver.com>
2022-11-07 15:09:23 +0900

    refactor: 사용자 생성자 추가

---
+++ src/main/java/baseball/model/User.java
@@ -0,0 +1,16 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class User {
+    private List<Integer> numbers;
+
+    public User() {
+        this.numbers = new ArrayList<>();
+    }
+
+
+}
