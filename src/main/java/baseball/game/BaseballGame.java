8a5d091ab292f1cff26e7ba4d950ca4e5df5a7b2
chkim <cndghks0213@gmail.com>
2022-11-08 07:11:49 +0900

    refactor(game): 변수, 메서드명 변경

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -12,45 +12,40 @@ import java.util.Map;
 import static baseball.ui.OutputView.*;
 
 public class BaseballGame {
-    private static final int GAME_OVER_STRIKE_COUNT = 3;
+    private static final int TOTAL_BASEBALL_COUNT = 3;
 
     private final NumbersGenerationStrategy strategy;
 
     public BaseballGame(NumbersGenerationStrategy strategy) {
+        printGameStartMessage();
         this.strategy = strategy;
     }
 
     public void start() {
-        printGameStartMessage();
         Numbers answer = new Numbers(strategy);
-        Map<BallTypes, Integer> result = new EnumMap<>(BallTypes.class);
-
-        game(answer, result);
-        decideRestartOrEnd();
+        game(answer);
+        restartOrEnd();
     }
 
-    private void game(Numbers answer, Map<BallTypes, Integer> result) {
+    private void game(Numbers answer) {
+        Map<BallTypes, Integer> result = new EnumMap<>(BallTypes.class);
+
         while (inProgress(result)) {
             Numbers numbers = new Numbers(InputView.numbers());
             result = answer.compare(numbers);
             printCompareResult(new Result(result));
         }
+        printGameOverMessage();
     }
 
     private boolean inProgress(Map<BallTypes, Integer> result) {
         int strike = result.getOrDefault(BallTypes.STRIKE, 0);
-        return strike != GAME_OVER_STRIKE_COUNT;
+        return strike != TOTAL_BASEBALL_COUNT;
     }
 
-    private void decideRestartOrEnd() {
-        if (isGameOver()) {
-            printGameOverMessage();
-            return;
+    private void restartOrEnd() {
+        if (InputView.decision() == Decision.RE_GAME) {
+            start();
         }
-        start();
-    }
-
-    private boolean isGameOver() {
-        return InputView.decision() == Decision.END;
     }
 }
