45201764f47b2c360b58a32c2c3452865e3ea650
kariskan <zkfzpf56@naver.com>
2022-11-07 15:56:01 +0900

    feat: BaseballDto를 이용해 출력 결과를 반환하는 로직 구현

--- src/main/java/baseball/utils/ConsoleUtil.java
+++ src/main/java/baseball/utils/ConsoleUtil.java
@@ -1,5 +1,9 @@
 package baseball.utils;
 
+import baseball.domain.BaseballDto;
+import baseball.status.Comments;
+import baseball.status.Constants;
+
 public class ConsoleUtil {
 
     public static void consoleOutput(String output) {
@@ -8,5 +12,37 @@ public class ConsoleUtil {
 
     public static void consoleOutputLine(String output) {
         System.out.println(output);
+    }
+
+    public static String ballAndStrikeOutput(BaseballDto baseballDto) {
+        StringBuilder output = new StringBuilder();
+
+        if (baseballDto.isBallExist()) {
+            output.append(baseballDto.getBall()).append(Comments.BALL.getComment());
+        }
+        if (baseballDto.isBallExist() && baseballDto.isStrikeExist()) {
+            output.append(Comments.SPACE.getComment());
+        }
+        if (baseballDto.isStrikeExist()) {
+            output.append(baseballDto.getStrike()).append(Comments.STRIKE.getComment());
+        }
+
+        if (checkIsNothing(baseballDto)) {
+            output.append(Comments.NOTHING.getComment());
+        }
+        if (gameEnd(baseballDto)) {
+            output.append(Comments.NEXT_LINE.getComment()).append(Comments.GAME_END.getComment());
+        }
+
+        return output.toString();
+    }
+
+    private static boolean gameEnd(BaseballDto baseballDto) {
+        return baseballDto.getStrike() == Constants.NUMBER_OF_DIGITS.getNumber();
+    }
+
+    private static boolean checkIsNothing(BaseballDto baseballDto) {
+        return baseballDto.getBall() == Constants.ZERO_COUNT.getNumber()
+                && baseballDto.getStrike() == Constants.ZERO_COUNT.getNumber();
     }
 }
