e03a4a35ae5686c0a70c222fc9502da97c957c08
devsyk <93830779+devsyk@users.noreply.github.com>
2022-11-07 10:43:30 +0900

    feat: 1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 랜덤 생성하는 기능 추가

---
+++ src/main/java/baseball/RandomNum.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RandomNum {
+    // 조건에 맞는 랜덤값 생성
+    public static List<Integer> generate() {
+        List<Integer> numList = new ArrayList<>();
+        boolean[] hasNum = new boolean[9]; // [0]: 1
+        Arrays.fill(hasNum, Boolean.FALSE); // false 초기값
+
+        // 3자리 자연수
+        while (numList.size() < 3) {
+            // 1부터 9로만 구성
+            int newNum = Randoms.pickNumberInRange(1,9);
+
+            // 중복되지 않는 수
+            if(!hasNum[newNum - 1]) {
+                numList.add(newNum);
+                hasNum[newNum - 1] = true;
+            }
+        }
+        
+        return numList;
+    }
+}
