9af0e2681596733f16cb16473bb7c9b8a6ad87b0
twoosky <lhjae3@naver.com>
2022-11-08 01:33:30 +0900

    feat(input): 입력값의 길이가 3이 아닌 경우 예외 처리 구현

--- src/main/java/utils/Validator.java
+++ src/main/java/utils/Validator.java
@@ -8,4 +8,10 @@ public class Validator {
     public static void validate(String inputNumber) {
         input = inputNumber;
     }
+
+    private static void validateSize() {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3개의 수만 입력 가능합니다.");
+        }
+    }
 }
