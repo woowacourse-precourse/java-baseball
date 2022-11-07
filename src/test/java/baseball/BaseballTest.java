22b537b89bf9cc91958c58eedcf96771b71a3aed
leesky <leehaneul0623@gmail.com>
2022-11-07 13:33:31 +0900

    test(BaseballGame) : getBall 테스트 코드 작성
    볼의 갯수를 알기 위해서 getBall 테스트 코드 작성:
     - 입력한 숫자가 정답에 포함된 갯수 반환 테스트 작성
     - 아무 숫자도 포함되지 않은 경우 0 반환 테스트 작성
     - 정확한 볼의 갯수 찾기 테스트 작성
    
    주요 변경 사항 : getBall 메서드는 주어진 숫자가 타겟에 얼마나 포함
        포함되어있는지 알려주는 메서드이다. 즉, 정확한 볼의 갯수를 파악하기
        위해서는 getStrike를빼줘야 한다.

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -2,8 +2,13 @@ package baseball;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.assertj.core.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class BaseballTest {
     private final static int NUMBER_LIMIT = 3;
@@ -12,6 +17,17 @@ public class BaseballTest {
     @BeforeAll
     static void initialization() {
         sut = new BaseballGame();
+    }
+
+    static Stream<Arguments> generateBallTestData() {
+        return Stream.of(
+                Arguments.of(123, 321, 2),
+                Arguments.of(568, 658, 2),
+                Arguments.of(869, 698, 3),
+                Arguments.of(623, 236, 3),
+                Arguments.of(562, 236, 2),
+                Arguments.of(568, 265, 1)
+        );
     }
 
     @Test
@@ -30,5 +46,22 @@ public class BaseballTest {
         assertThat(sut.getStrike(123, 456)).isEqualTo(0);
     }
 
+    @Test
+    void getBall_입력한_숫자가_정답에_포함된_갯수_반환() {
+        assertThat(sut.getBall(123, 321)).isEqualTo(3);
+        assertThat(sut.getBall(134, 431)).isEqualTo(3);
+    }
+
+    @Test
+    void getBall_아무_숫자가_포함되지_않을_경우_0_반환() {
+        assertThat(sut.getBall(123, 456)).isEqualTo(0);
+    }
+
+    @ParameterizedTest(name = "[{index}] 입력값이 {0}이고, 정답이 {1}이면 정확한 볼의 갯수는 {2}")
+    @MethodSource("generateBallTestData")
+    void getBall_정확한_볼의_갯수_찾기(int source, int target, int expected) {
+        int actual = sut.getBall(source, target) - sut.getStrike(source, target);
+        assertThat(actual).isEqualTo(expected);
+    }
 
 }
