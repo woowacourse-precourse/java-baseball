e9b3c64e52375568f70f8ad4e3da5880b297e282
meunrud <y3a0522@gmail.com>
2022-11-08 03:10:31 +0900

    :art: 정답 숫자 유형 리스트로 변환

--- src/main/java/baseball/AnswerNumber.java
+++ src/main/java/baseball/AnswerNumber.java
@@ -6,11 +6,11 @@ import java.util.List;
 import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class AnswerNumber {
-    private String answerNumber;
+    private List<String> answerNumber;
     private List<String> saveAnswerNumber = new ArrayList<>();
 
     public AnswerNumber(){
-        this. answerNumber= "";
+        answerNumber = new ArrayList<>();
     }
 
     public List<String> saveAnswerNumber(){
@@ -19,15 +19,13 @@ public class AnswerNumber {
 
     public void RandomNumber(){
             String [] number = new String[3];
-            StringBuffer numbers = new StringBuffer();
             for(int i=0; i<number.length; i++){
                 String num = String.valueOf(pickNumberInRange(1,9));
                 if(!num.equals(number[i])) {
                     number[i] = num;
-                    numbers.append(num);
+                    answerNumber.add(num);
                 }
             }
-            answerNumber =  numbers.toString();
-            saveAnswerNumber.add(answerNumber);
+            saveAnswerNumber.add(answerNumber.toString());
     }
 }
