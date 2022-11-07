881fea5533c959ef262555beaab5622522a976b0
bumwoo <gld55602@gmail.com>
2022-11-07 17:56:29 +0900

    feat: 랜덤 숫자 생성 기능 구현
    1~9 사이의 중복되지 않는 랜덤 숫자 3개를 생성

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,40 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Computer {
+    private List<Integer> randNumbers;
+
+    public Computer() {
+        randNumbers = new ArrayList<>();
+        generateRandom();
+    }
+
+    public void generateRandom() {
+        while (randNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randNumbers.contains(randomNumber)) {
+                randNumbers.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getRandNumbers() {
+        return randNumbers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Computer computer = (Computer) o;
+        return Objects.equals(randNumbers, computer.randNumbers);
+    }
+}
