e6ee2ff40aeeac2ea87fe75e7a1d87081f7b8473
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-07 16:21:46 +0900

    <fix>PlayingBaseball : 볼과 스트라이크 출력 순서 변경
    README.md 파일에 볼 - 스트라이크 순서대로 나와있기에 변경

--- src/main/java/baseball/PlayingBaseball.java
+++ src/main/java/baseball/PlayingBaseball.java
@@ -48,7 +48,7 @@ class PlayingBaseball {
             return;
         }
 
-        System.out.println(strike+"스트라이크 "+ball+"볼");
+        System.out.println(ball+"볼 "+strike+"스트라이크");
     }
 
     boolean has3Strikes () {
