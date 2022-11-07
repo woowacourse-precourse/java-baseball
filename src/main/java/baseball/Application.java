92a52bfa9a188ed9ac2785c6032ae87a01030645
OhRetry <OhRetry@users.noreply.github.com>
2022-11-07 15:58:27 +0900

    feat: add function for feature 'random generation'
    기능 요구 사항 'random generation'을 구현한 함수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static List<Integer> getRandomNum() {
+        List<Integer> result = new ArrayList<>();
+        boolean[] duplicate = new boolean[10];
+        int randNum;
+
+        for (int i = 0; i < 3; i++) {
+            randNum = Randoms.pickNumberInRange(1, 9);
+            if (duplicate[randNum]) {
+                i--;
+                continue;
+            } else {
+                duplicate[randNum] = true;
+                result.add(randNum);
+            }
+        }
+        return result;
+    }
+
 }
