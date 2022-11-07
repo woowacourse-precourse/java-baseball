524eb3ae1c675bdce08682e526a1068a6a15bda4
lee <higgs95@naver.com>
2022-11-07 21:57:33 +0900

    test(UserNumber): 유저 숫자 생성 기능 테스트 정상 입력 경우 예외 경우

--- src/test/java/baseball/UserNumberServiceTest.java
+++ src/test/java/baseball/UserNumberServiceTest.java
@@ -2,47 +2,70 @@ package baseball;
 
 import static baseball.type.NumberType.NUMBER_SIZE;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import baseball.service.UserNumberService;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.PrintStream;
 import java.util.List;
-import java.util.stream.Collectors;
 import org.junit.jupiter.api.Test;
 
 public class UserNumberServiceTest {
 
     @Test
-    void 유저_숫자_갯수_검증() {
+    void 정상_입력() {
         String input = "123";
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
         List<Integer> userNumbers = UserNumberService.generate();
-        int userNumbersSize = userNumbers.size();
 
+        int userNumbersSize = userNumbers.size();
         assertThat(userNumbersSize).isEqualTo(NUMBER_SIZE.getValue());
     }
 
     @Test
-    void 유저_숫자_중복_여부_검증() {
-        String input = "456";
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+    void 중복_숫자_입력() {
+        String input = "226";
+
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
-        List<Integer> userNumbers = UserNumberService.generate();
+        assertThatThrownBy(UserNumberService::generate)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
-        assertThat(userNumbers)
-                .isEqualTo(userNumbers
-                        .stream()
-                        .distinct()
-                        .collect(Collectors.toList()));
+    @Test
+    void 문자열_길이_3초과_입력() {
+        String input = "2345";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(UserNumberService::generate)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 문자열_길이_3미만_입력() {
+        String input = "25";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(UserNumberService::generate)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자아닌_문자_입력() {
+        String input = "2a5";
+
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(UserNumberService::generate)
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
