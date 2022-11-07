9f5ca3715c9dacefcd07974e0db9b91ca430cfdc
hyeonseog-won <smreo75@gmail.com>
2022-11-08 03:02:22 +0900

    feat: 사용자 입력 및 사용자, 컴퓨터 숫자 생성 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Game {
-    public static void start() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+    private Computer computer;
+    private User user;
+
+    public Game(Computer computer, User user) {
+        this.computer = computer;
+        this.user = user;
     }
+
+    public void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        generateNumbers();
+    }
+
+    public String inputUserNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        return input;
+    }
+
+    private void generateNumbers() {
+        computer.generateRandomNumbers();
+        user.setNumbers(inputUserNumbers());
+    }
+
 }
