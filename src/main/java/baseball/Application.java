bbbe3411ef446c47c52db155775e1c083e6eaba2
ensk26 <tjensk26@gmail.com>
2022-11-07 16:50:08 +0900

    feat(Application): 사용자 번호를 나눠 숫자로 변환해주는 메서드
    새로운 메서드 추가 to Application:
    - 사용자 번호를 나눠 숫자이면 숫자로 변환 후 배열에 저장해서 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,18 @@
 package baseball;
 
+import java.util.stream.Stream;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    static int[] getNumbersToUserNumber(String userNumber) {
+        try {
+            return Stream.of(userNumber.split("")).mapToInt(Integer::parseInt).toArray();
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
     }
 
     static int getNumber(String number) {
