2bfaf193e1b43241ce318215e3f8b49eec698adb
juhwang <juhong4930@naver.com>
2022-11-08 00:58:52 +0900

    test(BallCountTest): print all input result who type "records"

--- src/test/java/baseball/BallCountTest.java
+++ src/test/java/baseball/BallCountTest.java
@@ -9,6 +9,7 @@ public class BallCountTest {
     BallCount ballCount = new BallCount();
     BaseballNumbers randomNumbers = new BaseballNumbers();
     BaseballNumbers inputNumbers = new BaseballNumbers();
+    BaseballNumbers inputNumbers2 = new BaseballNumbers();
 
     @Test
     void case1_랜덤값과_인풋값_비교_후_볼_개수_확인() {
@@ -16,8 +17,7 @@ public class BallCountTest {
         inputNumbers.createBaseballNumbers("234");
         String input = "2볼";
         ballCount.validateBallCount(randomNumbers, inputNumbers);
-        assertThat(ballCount.toString())
-                .isEqualTo(input);
+        assertThat(ballCount.toString()).isEqualTo(input);
     }
 
     @Test
@@ -26,8 +26,7 @@ public class BallCountTest {
         inputNumbers.createBaseballNumbers("145");
         String input = "1스트라이크";
         ballCount.validateBallCount(randomNumbers, inputNumbers);
-        assertThat(ballCount.toString())
-                .isEqualTo(input);
+        assertThat(ballCount.toString()).isEqualTo(input);
     }
 
     @Test
@@ -36,8 +35,16 @@ public class BallCountTest {
         inputNumbers.createBaseballNumbers("134");
         String input = "1볼 1스트라이크";
         ballCount.validateBallCount(randomNumbers, inputNumbers);
-        assertThat(ballCount.toString())
-                .isEqualTo(input);
+        assertThat(ballCount.toString()).isEqualTo(input);
     }
 
+    @Test
+    void case4_모든_인풋값_출력_확인() {
+        GameBoard gameBoard = new GameBoard();
+        inputNumbers.createBaseballNumbers("134");
+        inputNumbers2.createBaseballNumbers("345");
+
+        String input = "현재 입력한 결과입니다.\r\n" + "134 : " + gameBoard.showBallCount(inputNumbers) + "\r\n345 : " + gameBoard.showBallCount(inputNumbers2) + "\r\n";
+        assertThat(gameBoard.showRecords()).isEqualTo(input);
+    }
 }
