8a073e90d10078f73cce6236d4eb5c2d8a7d5a8d
JeongHunHui <deca3248@gmail.com>
2022-11-08 02:45:20 +0900

    feat: GameNumberList를 가져오는 getter 메소드 작성

--- src/main/java/baseball/BaseBallGameNumber.java
+++ src/main/java/baseball/BaseBallGameNumber.java
@@ -8,13 +8,13 @@ public class BaseBallGameNumber {
   private List<Integer> gameNumberList = new ArrayList<>();
 
   public void stringToGameNumber(String inputString) {
-    if(inputString.length() != 3) throw new IllegalArgumentException("세자리가 아닙니다!");
+    if (inputString.length() != 3) throw new IllegalArgumentException("세자리가 아닙니다!");
     int gameNumber = stringToInt(inputString);
     setBaseBallGameNum(gameNumber);
     checkNumbersValid();
   }
 
-  public void setGameNumberByList(List<Integer> numberList){
+  public void setGameNumberByList(List<Integer> numberList) {
     gameNumberList.add(0, numberList.get(0));
     gameNumberList.add(1, numberList.get(1));
     gameNumberList.add(2, numberList.get(2));
@@ -41,8 +41,8 @@ public class BaseBallGameNumber {
 
   private void checkDifferentNumbers() {
     if (gameNumberList.get(0) == gameNumberList.get(1)
-            || gameNumberList.get(1) == gameNumberList.get(2)
-            || gameNumberList.get(0) == gameNumberList.get(2))
+        || gameNumberList.get(1) == gameNumberList.get(2)
+        || gameNumberList.get(0) == gameNumberList.get(2))
       throw new IllegalArgumentException("중복되는 숫자가 있습니다.");
   }
 
@@ -58,4 +58,10 @@ public class BaseBallGameNumber {
     }
     System.out.println(outputString);
   }
+
+  public List<Integer> getGameNumberList() {
+    return this.gameNumberList;
+  }
+
+  public BaseBallGameNumber() {}
 }
