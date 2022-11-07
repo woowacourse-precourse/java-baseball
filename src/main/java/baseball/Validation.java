d530c36cd173859bcd3f9f19e59e7b09bdc2802b
seonghoo1217 <seonghoo1217@naver.com>
2022-11-07 21:06:30 +0900

    refactor : modify Validation class in validation method return boolean
    * Why boolean?
    - In the past, if the user's input value does not match the exception processing condition immediately error throw
    - But I think so , it's not intuitive
    
    Refactor(commit_id) : #3c7690b

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,16 +1,25 @@
 package baseball;
 
 public class Validation {
-    public static <T>void validation(String validation_case,T validation_param){
+    public static <T>boolean validation(String validation_case,T validation_param){
         if (validation_case.equals("user_input_number_validation")&&validation_param instanceof String){
             String user_input_number = (String) validation_param;
 
-            boolean user_input_number_regex_match = user_input_number.matches("^[^0-9]*$");
-            boolean user_input_number_length = user_input_number.length() != 3;
+            boolean user_input_number_regex_match = user_input_number.matches("^[0-9]*$");
+            boolean user_input_number_length = user_input_number.length() == 3;
 
-            if (user_input_number_length||user_input_number_regex_match) {
+            if (user_input_number_length&&user_input_number_regex_match) {
+                return true;
+            }
+        }
+        else if (validation_case.equals("user_retry_condition_validation")&&validation_param instanceof String){
+            String user_input_retry = (String) validation_param;
+            boolean retry_regex = user_input_retry.matches("[^12]");
+            if (retry_regex){
                 throw new IllegalArgumentException();
             }
         }
+
+        return false;
     }
 }
