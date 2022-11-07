5c3235f7e71ac4ccb06bc0c306ac4e77c52f0100
nswon <swnam0729@gmail.com>
2022-11-07 23:00:49 +0900

    style: 매직넘버를 피하기 위한 상수로 선언

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -6,6 +6,9 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Computer {
+    private static final int RANDOM_NUMBERS_LENGTH = 3;
+    private static final int START_RANDOM_NUMBER = 1;
+    private static final int END_RANDOM_NUMBER = 9;
     private final Set<Integer> randomNumbers;
 
     public Computer() {
@@ -18,8 +21,8 @@ public class Computer {
 
     private Set<Integer> generateNumbers() {
         Set<Integer> numbers = new HashSet<>();
-        while (numbers.size() != 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (numbers.size() != RANDOM_NUMBERS_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(START_RANDOM_NUMBER, END_RANDOM_NUMBER);
             numbers.add(randomNumber);
         }
         return numbers;
