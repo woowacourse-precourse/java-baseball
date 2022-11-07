b4d956392fa8be38ce35c0008cea2e2a947fc97a
yback1223 <yoyob1223@gmail.com>
2022-11-08 01:00:10 +0900

    refactor: make listSize final variable

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,9 +5,10 @@ import baseball.io.IOTopClass;
 
 public class Application {
 
+    private static final int LIST_SIZE = 3;
     public static void main(String[] args) {
-        IOTopClass ioTopClass = new IOTopClass(3);
-        Game game = new Game();
+        IOTopClass ioTopClass = new IOTopClass(LIST_SIZE);
+        Game game = new Game(LIST_SIZE);
         game.runGame(ioTopClass);
     }
 }
