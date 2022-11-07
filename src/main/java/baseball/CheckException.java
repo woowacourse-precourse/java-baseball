9cd0b5769a39302086a1f67b94ab1468dd6d2bae
YongGwan <32181771@dankook.ac.kr>
2022-11-07 15:17:00 +0900

    input값에 관한 에러 처리 함수 구현

--- src/main/java/baseball/CheckException.java
+++ src/main/java/baseball/CheckException.java
@@ -7,4 +7,35 @@ public class CheckException {
         return !(restartStatus.equals("1") || restartStatus.equals("2"));
     }
 
+    public static boolean inputGameValue(String inputValue) {
+
+        if (inputValue.length() != 3) return false;
+
+        if (!isNumeric(inputValue)) return false;
+
+        if(!isDiffAllDigits(inputValue)) return false;
+
+        return true;
+    }
+
+    private static boolean isNumeric(String inputValue) {
+
+        try {
+            Integer.parseInt(inputValue);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    private static boolean isDiffAllDigits(String inputValue) {
+
+        if(inputValue.charAt(0) == inputValue.charAt(1)) return false;
+
+        if(inputValue.charAt(1) == inputValue.charAt(2)) return false;
+
+        if (inputValue.charAt(0) == inputValue.charAt(2)) return false;
+
+        return true;
+    }
 }
