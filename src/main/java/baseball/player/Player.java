0b70f1afe1c2c00d5dbea130bbd4ef98d2f5d555
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-08 05:56:47 +0900

    feat(player/Player.java): 플레이어에게 숫자 입력받는 기능 구현

---
+++ src/main/java/baseball/player/Player.java
@@ -0,0 +1,21 @@
+package baseball.player;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Player {
+    private static List<Integer> inputNum;
+    private static final String INPUT_STRING = "숫자를 입력해주세요 : ";
+
+    public void inputNumber(){
+        System.out.print(INPUT_STRING);
+        String input = Console.readLine();
+        checkValidateInput(input);
+        setInputNum(input);
+    }
+
+}
