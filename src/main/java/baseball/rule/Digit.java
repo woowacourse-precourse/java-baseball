89887414931e4d303506376c0bcee42eb022f3f7
gnoesnooj <306yyy@naver.com>
2022-11-07 13:43:47 +0800

    refactor : 중복검사 메소드 구현

--- src/main/java/baseball/rule/Digit.java
+++ src/main/java/baseball/rule/Digit.java
@@ -1,6 +1,12 @@
 package baseball.rule;
 
+import java.util.List;
+
 public class Digit {
 
     public static final int DIGITS = 3;
+
+    public static boolean isMatchDigit(List<Integer> inputs) {
+        return inputs.size() == DIGITS;
+    }
 }
