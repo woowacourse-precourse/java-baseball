2957e21a018ad03bac7b225ee1c698c6fd121d1e
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-08 00:36:39 +0900

    feat : 서로 다른 임의의 숫자 3개 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,17 +6,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
 
-        //1. 랜덤한 숫자 저장
-        List<Integer> computer = new ArrayList<>();
+    //[컴퓨터] 임의의 3자리 숫자 저장
+    public static List<Integer> makeNumber(List<Integer> computer) {
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
+        return computer;
+    }
 
+    public static void main(String[] args) {
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computer = new ArrayList<>();
+
+        makeNumber(computer);
+        //System.out.println(computer);
     }
 }
