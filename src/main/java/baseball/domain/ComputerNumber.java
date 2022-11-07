a04ec5894f148248592c79c182c66499377348c5
the9kim <96895686+the9kim@users.noreply.github.com>
2022-11-07 23:20:17 +0900

    feat: 세자리 난수를 생성하는 컴퓨터 넘버 클래스 구현
    Randoms 유틸을 활용하여 세자리 난수 생성 후 필드값에 저장

--- src/main/java/baseball/domain/ComputerNumber.java
+++ src/main/java/baseball/domain/ComputerNumber.java
@@ -1,5 +1,21 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class ComputerNumber {
+    public Integer hundredNumber;
+    public Integer tenNumber;
+    public Integer number;
+    public List<String> computerNumberList;
+
+    public ComputerNumber(){
+        hundredNumber = Randoms.pickNumberInRange(1, 9);
+        tenNumber = Randoms.pickNumberInRange(1, 9);
+        number = Randoms.pickNumberInRange(1, 9)
+    }
+
 
 }
