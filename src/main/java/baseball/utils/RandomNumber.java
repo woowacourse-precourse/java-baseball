0b4c64d89bca015031415e00205625a345c6fdf4
simba <shim9597@gmail.com>
2022-11-07 19:02:03 +0900

    feat: 생성된 랜덤 숫자 반환 기능
    생성된 랜덤 숫자를 List<Integer> 타입에서 int[] 로 변환 후 반환 하는 기능 구현 완료

--- src/main/java/baseball/utils/RandomNumber.java
+++ src/main/java/baseball/utils/RandomNumber.java
@@ -7,7 +7,16 @@ import java.util.List;
 
 public class RandomNumber {
 
-    public List<Integer> newRandomNumber() {
+    public int[] getRandomNumber() {
+        int[] numbers = new int[3];
+
+        for (int i =0; i < 3; i ++) {
+            numbers[i] = newRandomNumber().get(i);
+        }
+        return numbers;
+    }
+
+    private List<Integer> newRandomNumber() {
         List<Integer> computerNumber = new ArrayList<>();
         while (computerNumber.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
