1dc883424b6cd58969307702ef38313125a75741
CHAYEONIL <jeay123@naver.com>
2022-11-08 02:43:20 +0900

    Refactor : 필요 없는 구문 삭제

--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -2,14 +2,16 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class input {
+public class Input {
+    private static final String REQUIRE_BASEBALL_NUMBER = "숫자를 입력해주세요 : ";
     public static String startMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
+
     public static String askNextGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
         return Console.readLine();
     }
+
 }
