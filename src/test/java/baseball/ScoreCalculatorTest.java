9a56d6a410aa59f703bc082155f299e32e651d43
woosung1223 <dntjd991223@naver.com>
2022-11-07 17:43:54 +0900

    test(ScoreCalculatorTest): calculateUserFinalScore 테스트 추가

--- src/test/java/baseball/ScoreCalculatorTest.java
+++ src/test/java/baseball/ScoreCalculatorTest.java
@@ -4,9 +4,16 @@ import baseball.domain.BaseballNumber;
 import baseball.domain.ScoreCalculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
 
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,6 +40,30 @@ class ScoreCalculatorTest {
             BaseballNumber baseballNum2 = new BaseballNumber(number2);
             assertThat(ScoreCalculator.calculateStrikeCount(baseballNum1, baseballNum2))
                     .isEqualTo(expected);
+        }
+    }
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    @Nested
+    @DisplayName("Ball과 Strike가 둘 다 있는 경우를 테스트한다")
+    class BallAndStrike {
+        @ParameterizedTest
+        @MethodSource("parameterProvider")
+        void calculateBallAndStrikeTest(int number1, int number2, List<Integer> expected) {
+            System.out.println(expected);
+            BaseballNumber baseballNum1 = new BaseballNumber(number1);
+            BaseballNumber baseballNum2 = new BaseballNumber(number2);
+            assertThat(ScoreCalculator
+                    .calculateUserFinalScore(baseballNum1, baseballNum2)
+                    .getScore())
+                    .isEqualTo(expected);
+        }
+
+        private Stream<Arguments> parameterProvider() {
+            return Stream.of(
+                    Arguments.of(123, 136, Arrays.asList(1, 1)),
+                    Arguments.of(123, 132, Arrays.asList(2, 1))
+            );
         }
     }
 }
