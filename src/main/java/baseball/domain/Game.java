92ff8978c2c4b2321005b9efee5d805d61f14159
yun8565 <yunyun8565@naver.com>
2022-11-07 10:57:58 +0900

    feat: 결과 출력 기능 추가
    computer, player 객체를 final로 선언해서 게임이 진행되는 동안 변경할 수 없게 수정했다.

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -2,12 +2,18 @@ package baseball.domain;
 
 public class Game {
     public static final int MAX_STRIKE = 3;
+    public static final String BALL = "볼 ";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
 
     public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맟히셨습니다! ";
+    public static final String GAME_FINISH_MESSAGE = "게임 종료";
+    public static final String RESTART_OR_QUIT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    private Computer computer;
-    private Player player;
+    private final Computer computer;
+    private final Player player;
 
     public Game() {
         computer = new Computer();
@@ -19,7 +25,9 @@ public class Game {
         do {
             computer.generateNumber();
             guessAndCheck();
-
+            System.out.println(SUCCESS_MESSAGE);
+            System.out.println(GAME_FINISH_MESSAGE);
+            System.out.println(RESTART_OR_QUIT_MESSAGE);
         } while(restart);
     }
 
@@ -29,7 +37,24 @@ public class Game {
             System.out.print(INPUT_MESSAGE);
             String guessNumber = player.getInput();
             result = computer.compareNumber(guessNumber);
+            showResult(result);
         } while(isNotFinished(result));
+    }
+
+    public void showResult(Result result) {
+        int ballCount = result.getBallCount();
+        int strikeCount = result.getStrikeCount();
+
+        if(ballCount != 0){
+            System.out.print(ballCount+BALL);
+        }
+        if(strikeCount != 0){
+            System.out.print(strikeCount+STRIKE);
+        }
+        if(ballCount == 0 && strikeCount == 0) {
+            System.out.print(NOTHING);
+        }
+        System.out.println();
     }
 
     public boolean isNotFinished(Result result) {
