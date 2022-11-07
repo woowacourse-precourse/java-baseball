4771827d42457d5dece81211b8140bdbd67b3186
SeongbinPark <binni0515@gmail.com>
2022-11-07 16:37:01 +0900

    feat(UserUtil): get userNumbers from user input - using camp.nextstep.edu.missionutils.Console

--- src/main/java/baseball/util/UserUtil.java
+++ src/main/java/baseball/util/UserUtil.java
@@ -1,4 +1,32 @@
 package baseball.util;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserUtil {
+
+    public static List<Integer> getUserNumbers() {
+        List<Integer> userNumbers = new ArrayList<>();
+
+        String input = getInput();
+
+        String[] digits = input.split("");
+
+        for (String digit : digits) {
+            int eachNumber = numberOf(digit);
+
+            userNumbers.add(eachNumber);
+        }
+
+        return userNumbers;
+    }
+
+    private static String getInput() {
+        return Console.readLine();
+    }
+
+    private static int numberOf(String digit) {
+        return Integer.parseInt(digit);
+    }
 }
