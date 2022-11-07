de1607b1f312c5063fddc47d3a5ee43e0441bee9
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-07 17:12:09 +0900

    feat(Application): add printGameRestartMessage

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,9 +4,13 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         printGameStartMessage();
+            printGameRestartMessage();
     }
 
     static void printGameStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    static void printGameRestartMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
