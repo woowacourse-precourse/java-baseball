4fbaedd87bed33d393af2301dfa8ee2efc87e125
Luka <meena2003@naver.com>
2022-11-07 22:48:49 +0900

    <feat> (기능1) 컴퓨터 랜덤 3자리 숫자 반환 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,9 +1,34 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class BaseballGame {
-    
+    List<Integer> computerNumbers =
+
+    public void playBaseballGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            String playerNumbers = readLine();  // 어레이리스트로 변환하기
+
+        }
+    }
+
+    private List<Integer> makesComputerNumbers() {
+        List<Integer> computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumbers.contains(randomNumber)) {
+                computerNumbers.add(randomNumber);
+            }
+        }
+        return computerNumbers;
+    }
+
+
 
 }
 
