c9ffb02c47d4856d9205ba7ac5034cef222f35f4
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-07 19:52:27 +0900

    feat(BaseballController): 게임 시작 함수와 재시작 감지 메서드, 게임 중에 실행할 로직 빈 메서드 작성

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,4 +1,18 @@
 package baseball.controller;
 
 public class BaseballController {
+
+    public void gameStart() {
+        do {
+            inProgress();
+        } while (restart());
+    }
+
+    public boolean restart() {
+        return true;
+    }
+
+    public void inProgress() {
+
+    }
 }
