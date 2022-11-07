cba646cda999d374e0afe39e57ad12ed4453c33a
ckn1070 <ckn1070@naver.com>
2022-11-08 01:55:10 +0900

    feat(Strike_Test): add a test code for checkStrike method

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -8,7 +8,18 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class ApplicationTest extends NsTest {
+    @Test
+    void Strike_Test() {
+        List<Integer> computerInput = List.of(1, 2, 3);
+        List<Integer> userInput = List.of(3, 2, 9);
+        List<Integer> result = List.of(1);
+        assertThat(Application.checkStrike(computerInput, userInput)).isEqualTo(result);
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
