51a2e9e3acafba01d0ab6bb30a393521d177697e
twknds <twknds@naver.com>
2022-11-07 18:45:17 +0900

    feat: baseBall 메서드 추가 및 @Override 어노테이션 누락 수정

--- src/main/java/baseball/Service.java
+++ src/main/java/baseball/Service.java
@@ -2,5 +2,5 @@ package baseball;
 
 public interface Service {
     void readyGame();
-
+    int[] baseBall(char[] var1, char[] var2);
 }
--- src/main/java/baseball/ServiceImpl.java
+++ src/main/java/baseball/ServiceImpl.java
@@ -1,19 +1,31 @@
+
 package baseball;
+
 
 public class ServiceImpl implements Service {
 
     public ServiceImpl() {
     }
-
+    @Override
     public void readyGame() {
         int strike = 0;
-        char[] number = // 숫자생성
+        char[] number = // 숫자생성;
 
         while(strike != 3) {
             System.out.print("숫자를 입력해주세요 : ");
             char[] input = //입력
-            int[] count = // 게임 시행
-            strike = count[0]; // 스트라이크값 기입
+            int[] count = baseBall(number,input);
+            strike = count[0];
         }
+    }
+    @Override
+    public int[] baseBall(char[] number, char[] input) {
+        int[] count = new int[]{0, 0};
+
+        for(int i = 0; i < 3; ++i) {
+            // 스트라이크 볼 처리
+        }
+
+        return count;
     }
 }
