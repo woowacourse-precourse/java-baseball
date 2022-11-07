00425b9acac7a2df663a5ad1d1cadf064c99fac0
KangSungBum <peter9706@naver.com>
2022-11-07 15:57:58 +0900

    feat(Application.java) : add checkInputCondition method
    Added new method to Application.java
    - check if inputNumber follows the guide instruction
    - throws IllegalArgumentException when not followed

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,6 +11,12 @@ public class Application {
         while (true) {
             int inputNumber = Integer.parseInt(Console.readLine());
 
+            try {
+                checkInputCondition(inputNumber);
+            } catch (IllegalArgumentException e) {
+                System.out.println("잘못된 입력입니다");
+            }
+
             int exitCode = Integer.parseInt(Console.readLine());
             if (exitCode == 2) {
                 break;
@@ -29,4 +35,21 @@ public class Application {
 
         return createdNumber;
     }
+
+    private static void checkInputCondition(int inputNumber) {
+        List<Integer> checkList = new ArrayList<>(3);
+        String strNumber = String.valueOf(inputNumber);
+
+        for (int i = 0; i < strNumber.length(); i++) {
+            int target = Integer.parseInt(String.valueOf(strNumber.charAt(i)));
+            if (!checkList.contains(target) && target > 0 && target <= 9) {
+                checkList.add(target);
+            }
+        }
+
+        if (strNumber.length() > 3 ||checkList.size() < 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
 }
