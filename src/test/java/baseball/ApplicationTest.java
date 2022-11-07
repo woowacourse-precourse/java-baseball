b8ab5812732499cde6edeacaaa0afb31471a7d16
TaemHam <tk4003950@gmail.com>
2022-11-07 16:50:19 +0900

    Fix: DisplayName 삭제

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -6,19 +6,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("숫자야구 게임은")
 class ApplicationTest extends NsTest {
 
     @Nested
-    @DisplayName("게임 종료 후")
     class 종료_테스트 {
 
         @Test
-        @DisplayName("1을 입력해 다시 시작하거나 2를 입력해 종료할 수 있어야 한다.")
         void 게임종료_후_재시작() {
             assertRandomNumberInRangeTest(
                     () -> {
@@ -31,11 +27,9 @@ class ApplicationTest extends NsTest {
     }
 
     @Nested
-    @DisplayName("잘못된 값을 입력할 경우")
     class 오류_테스트 {
 
         @Test
-        @DisplayName("IllegalArgumentException 에러를 호출한다.")
         void 예외_테스트() {
             assertSimpleTest(() ->
                     assertThatThrownBy(() -> runException("1234"))
