bc9037aafb12cd3f78fe8745b72519be0bcb9b5e
kyo705 <mission0705@naver.com>
2022-11-07 18:19:46 +0900

    [ADD] 입력값 유효성 검증 로직
    입력값 String과 int 두 가지를 허용하는 메소드 작성
    0~9 서로 다른 3자리 수면 true
    그렇지 않을 경우 false 리턴

---
+++ src/main/java/baseball/InputValidation.java
@@ -0,0 +1,48 @@
+package baseball;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class InputValidation {
+	int start = 0;
+	int end = 9;
+	int size = 3;
+	
+	public InputValidation() {}
+	
+	public InputValidation(int start, int end, int size) {
+		this.start = start;
+		this.end = end;
+		this.size = size;
+	}
+
+	
+	public boolean isValidRandomNumbers(int inputData) {
+		String data = Integer.toString(inputData);
+		
+		return isValidRandomNumbers(data);
+	}
+	
+	
+	public boolean isValidRandomNumbers(String inputData) {
+		if(inputData.length()!=size) {
+			return false;
+		}
+		Set<Character> set = new HashSet<>();
+		for(char randomNumber : inputData.toCharArray()) {
+			
+			char startChar = (char) ('0' + start);
+			char endChar = (char) ('0' + end);
+			
+			if((randomNumber < startChar) || (randomNumber > endChar)) {
+				return false;
+			}
+			if(set.contains(randomNumber)) {
+				return false;
+			}
+			set.add(randomNumber);
+		}
+		return true;
+	}
+	
+}
