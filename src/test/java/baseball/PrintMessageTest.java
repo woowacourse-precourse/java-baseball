869bb9de765772802296d36b19df47204b1c30df
popomarin28@gmail.com <popomarin28@gmail.com>
2022-11-08 00:56:01 +0900

    test[PrintmessageTest] : each of ball_strike option

--- src/test/java/baseball/PrintMessageTest.java
+++ src/test/java/baseball/PrintMessageTest.java
@@ -1,10 +1,12 @@
 package baseball;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -29,10 +31,37 @@ class PrintMessageTest {
     }
 
     @Test
-    void ballStrikeMessage() {
-
+    void ballStrikeMessage_nothing() {
+        List<Integer> ballStrike = new ArrayList<>(Arrays.asList(0, 0));
+        ByteArrayOutputStream outPut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outPut));
+        PrintMessage.ballStrikeMessage(ballStrike.get(0), ballStrike.get(1));
+        assertThat(outPut.toString()).isEqualTo("낫싱\n");
     }
-
+    @Test
+    void ballStrikeMessage_only_strike() {
+        List<Integer> ballStrike = new ArrayList<>(Arrays.asList(0, 2));
+        ByteArrayOutputStream outPut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outPut));
+        PrintMessage.ballStrikeMessage(ballStrike.get(0), ballStrike.get(1));
+        assertThat(outPut.toString()).isEqualTo("2스트라이크\n");
+    }
+    @Test
+    void ballStrikeMessage_only_ball() {
+        List<Integer> ballStrike = new ArrayList<>(Arrays.asList(3, 0));
+        ByteArrayOutputStream outPut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outPut));
+        PrintMessage.ballStrikeMessage(ballStrike.get(0), ballStrike.get(1));
+        assertThat(outPut.toString()).isEqualTo("3볼\n");
+    }
+    @Test
+    void ballStrikeMessage_mixed() {
+        List<Integer> ballStrike = new ArrayList<>(Arrays.asList(2, 1));
+        ByteArrayOutputStream outPut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outPut));
+        PrintMessage.ballStrikeMessage(ballStrike.get(0), ballStrike.get(1));
+        assertThat(outPut.toString()).isEqualTo("2볼 1스트라이크\n");
+    }
     @Test
     void answerMessage() {
         ByteArrayOutputStream outPut = new ByteArrayOutputStream();
