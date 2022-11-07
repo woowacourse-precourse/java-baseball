23be2821318193459a25a84f4163c119b47c2f21
LeeMyeongJin <=>
2022-11-07 21:02:22 +0900

    Feat: Restart class 생성

--- src/main/java/baseball/Restart.java
+++ src/main/java/baseball/Restart.java
@@ -3,10 +3,10 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Restart {
-    public boolean reStart(){
+    public boolean reStart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int restartNumber = Integer.parseInt(Console.readLine());
-        if(restartNumber == 1){
+        if (restartNumber == 1) {
             return true;
         }
         return false;
