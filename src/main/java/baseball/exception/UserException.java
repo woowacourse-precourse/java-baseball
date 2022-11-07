ac05dbb614bf83ad2035555f922c4eb41319cfb7
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-07 19:09:14 +0900

    feat : checkUserInputNumberBetweenOneAndNine 메서드 추가

--- src/main/java/baseball/exception/UserException.java
+++ src/main/java/baseball/exception/UserException.java
@@ -15,5 +15,15 @@ public class UserException {
         return false;
     }
 
+    public boolean checkUserInputNumberBetweenOneAndNine(String userInputNumber) throws IllegalArgumentException {
+        for (int i = 0; i < Constant.MAX_NUMBER_SIZE; i++) {
+            if (userInputNumber.charAt(i) - '0' < Constant.USER_MIN_NUMBER
+                    || userInputNumber.charAt(i) - '0' > Constant.USER_MAX_NUMBER) {
+                throw new IllegalArgumentException("1부터 9까지의 숫자만 입력해주세요.");
+            }
+        }
+        return true;
+    }
+
 
 }
