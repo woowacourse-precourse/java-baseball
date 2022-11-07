32fc61d6e8fc9e8bc25d0c17d54da9f21ed979c5
chanminmaxscaler <jcm04@naver.com>
2022-11-07 14:44:11 +0900

    refactor: Action 클래스의 하드코딩을 리팩토링한다
    isRestart 메소드에서 "1"을 사용하지 않고, 상수 RESTART를 사용한다.
    oneNumberRegularExpression을 ONE_NUMBER_REGULAR_EXPRESSION으로 변수명을 변경해 상수인 것을 나타낸다.

--- src/main/java/baseball/domain/Action.java
+++ src/main/java/baseball/domain/Action.java
@@ -2,7 +2,8 @@ package baseball.domain;
 
 public class Action {
     private final String numberAction;
-    private final String oneNumberRegularExpression = "[12]";
+    private final String ONE_NUMBER_REGULAR_EXPRESSION = "[12]";
+    private final String RESTART = "1";
 
     public Action(String numberAction) {
         validateForm(numberAction);
@@ -10,12 +11,12 @@ public class Action {
     }
 
     private void validateForm(String numberAction) {
-        if (!numberAction.matches(oneNumberRegularExpression)) {
+        if (!numberAction.matches(ONE_NUMBER_REGULAR_EXPRESSION)) {
             throw new IllegalArgumentException("숫자 1 혹은 2만 입력할 수 있습니다.");
         }
     }
 
     public boolean isRestart() {
-        return numberAction.equals("1");
+        return numberAction.equals(RESTART);
     }
 }
