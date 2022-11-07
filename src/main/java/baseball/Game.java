4a9515f94d63f4c55c6fa6388991a015311f8a93
mgOh <issac_oh@naver.com>
2022-11-08 00:34:19 +0900

    feat: 랜덤 3자리 숫자 생성 기능 생성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,2 +1,32 @@
-package baseball;public class Game {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+
+public class Game {
+    private final static Logger LOG = Logger.getGlobal();
+    int num1, num2, num3;
+    Randoms Ran;
+
+    public Game() {
+        List<Integer> numberList = Ran.pickUniqueNumbersInRange(1, 9, 3);
+        this.num1 = numberList.get(0);
+        this.num2 = numberList.get(1);
+        this.num3 = numberList.get(2);
+    }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public int getNum3() {
+        return num3;
+    }
 }
