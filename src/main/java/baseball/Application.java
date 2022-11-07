551b395a365f9d141f88c428cfada3b52f8280bc
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-07 20:16:19 +0900

    feat: add playGame

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
 public class Application {
+    private static void playGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        // TODO: 게임 진행
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        playGame();
     }
 }
