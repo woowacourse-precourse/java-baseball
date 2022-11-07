d04ee3ea64f29937833572f107d20e326cbc115b
plusjob70 <plusjob70@gmail.com>
2022-11-07 17:10:51 +0900

    feat: 사용자로부터 3자리 숫자 입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Application {
+    private static final int NUMBER_DIGIT = 3;
+
     public static void main(String[] args) {
         printStartGameSentence();
     }
@@ -23,5 +27,38 @@ public class Application {
             }
         }
         return numbers;
+    }
+
+    private static List<Integer> getUserPick() {
+        String input = inputNumber();
+        return stringToIntegerList(input);
+    }
+
+    private static String inputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        validateNumber(input);
+        return input;
+    }
+
+    private static List<Integer> stringToIntegerList(String number) {
+        return Arrays.stream(number.split("")).map(Integer::parseInt).collect(Collectors.toList());
+    }
+
+    private static void validateNumber(String input) {
+        if (!input.matches("[1-9]{3}")) {
+            throw new IllegalArgumentException();
+        }
+
+        Set<Character> history = new HashSet<>();
+        char number;
+        for (int i = 0; i < NUMBER_DIGIT; i++) {
+            number = input.charAt(i);
+
+            if (history.contains(number)) {
+                throw new IllegalArgumentException();
+            }
+            history.add(number);
+        }
     }
 }
