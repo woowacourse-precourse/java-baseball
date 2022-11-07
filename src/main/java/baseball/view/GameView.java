c8d3ef6bd6b10d14dfe9f66336771eaf0af1da70
Jin409 <sualng123@gmail.com>
2022-11-08 02:15:34 +0900

    refactor(GameView): printHint 메서드가 CountStatus 에 따라서 문구를 반환하도록 수정
    - 기존에는 직접 ballCount 와 strikeCount 를 따져보고 문구를 반환했는데, 이제는 status 만 보고 반환하면
    됨. GameView 가 직접 어떤 상태인지 판단하지 않아도 됨.

--- src/main/java/baseball/view/GameView.java
+++ src/main/java/baseball/view/GameView.java
@@ -1,9 +1,16 @@
 package baseball.view;
 
+import static baseball.domain.CountStatus.NOTHING;
+import static baseball.domain.CountStatus.ONLY_BALL;
+import static baseball.domain.CountStatus.ONLY_STRIKE;
+
+import baseball.domain.CountStatus;
+import baseball.domain.Score;
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
 
 public class GameView {
-    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다. ";
     private static final String ASK_USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String FINISH_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String ASK_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
@@ -21,20 +28,23 @@ public class GameView {
         return Console.readLine();
     }
 
-    public static void printHint(int strikeCount, int ballCount) {
-        if (strikeCount == 0 && ballCount == 0) {
+    public static void printHint(Score score) {
+        CountStatus countStatus = score.getCountStatus();
+        List<Integer> strikeAndBallCount = score.getStrikeAndBallCount(countStatus);
+
+        if (countStatus == NOTHING) {
             System.out.println(NOTHING_MESSAGE);
             return;
         }
-        if (strikeCount >= 1 && ballCount == 0) {
-            System.out.printf(ONLY_STRIKE_MESSAGE, strikeCount);
+        if (countStatus == ONLY_STRIKE) {
+            System.out.printf(ONLY_STRIKE_MESSAGE, strikeAndBallCount.get(0));
             return;
         }
-        if (strikeCount == 0 && ballCount >= 1) {
-            System.out.printf(ONLY_BALL_MESSAGE, ballCount);
+        if (countStatus == ONLY_BALL) {
+            System.out.printf(ONLY_BALL_MESSAGE, strikeAndBallCount.get(0));
             return;
         }
-        System.out.printf(BALL_AND_STRIKE_MESSAGE, ballCount, strikeCount);
+        System.out.printf(BALL_AND_STRIKE_MESSAGE, strikeAndBallCount.get(0), strikeAndBallCount.get(1));
     }
 
     public static String askRestart() {
@@ -42,9 +52,7 @@ public class GameView {
         return Console.readLine();
     }
 
-    public static void printResult(boolean correct) {
-        if (correct) {
-            System.out.println(FINISH_MESSAGE);
-        }
+    public static void printResult() {
+        System.out.println(FINISH_MESSAGE);
     }
 }
