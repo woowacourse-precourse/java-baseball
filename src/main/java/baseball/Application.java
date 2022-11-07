32cb0cd790b5d44875b8d56ffaaac8bd60746ae4
khyojun <38220795+khyojun@users.noreply.github.com>
2022-11-07 13:44:17 +0900

    refactor: 불필요한 import문 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,6 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Application {
