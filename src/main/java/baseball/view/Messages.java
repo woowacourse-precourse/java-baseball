886a0851ef2658e7da70df2e3c256112ac103656
chanminmaxscaler <jcm04@naver.com>
2022-11-07 13:26:53 +0900

    feat: Messages enum 클래스를 구현한다
    Messages enum 클래스는 Messenger 클래스에서 사용되는 문구들을 상수로 갖는다. START, INPUT, ANSWER, RESTART_OR_END, BALL, STRIKE, NOTHING이 있으며, 각각 상수값으로 String message를 갖는다. message를 받기 위한 getMessage 메소드도 함께 정의한다.
    
    getMessage 메소드는 String message를 반환한다.

---
+++ src/main/java/baseball/view/Messages.java
@@ -0,0 +1,19 @@
+package baseball.view;
+
+public enum Messages {
+    START("게임을 시작합니다.\n"),
+    INPUT("숫자를 입력하세요. : "),
+    ANSWER("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"),
+    RESTART_OR_END("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"),
+    BALL("볼"),
+    STRIKE("스트라이크"),
+    NOTHING("낫싱");
+
+    private final String message;
+    Messages(String message) {
+        this.message = message;
+    }
+    public String getMessage() {
+        return message;
+    }
+}
