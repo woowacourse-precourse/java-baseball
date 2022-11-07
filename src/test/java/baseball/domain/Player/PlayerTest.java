d850c0b7c042cad609fa6be20377f30481552b4b
somsom13 <somin1113@naver.com>
2022-11-07 14:51:35 +0900

    test(PlayerTest): test player restart input result
    플레이어가 입력하는 재시작 숫자가 재시작, 종료 여부를 정확히 판단하는지를 확인하기 위한 테스트를 진행한다.
    
    변경사항:
    - PlayerTest : 사용자의 공 숫자 입력 테스트를 Nested test 로 분리, 사용자의 재시작 입력 숫자의 결과를 테스트하는 테스트 코드 작성

--- src/test/java/baseball/domain/Player/PlayerTest.java
+++ src/test/java/baseball/domain/Player/PlayerTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
@@ -16,54 +17,92 @@ class PlayerTest {
         player = new Player();
     }
 
-    @Test
-    @DisplayName("사용자 입력 난수에 이상이 없는 케이스를 테스트한다")
-    void 사용자_입력_숫자_리스트_생성_성공() {
-        String inputString = "123";
-        List<Integer> result = List.of(1, 2, 3);
+    @Nested
+    @DisplayName("사용자의 공 숫자 입력을 테스트한다")
+    class testPlayerBallNumberInput {
+        @Test
+        @DisplayName("사용자 입력 난수에 이상이 없는 케이스를 테스트한다")
+        void 사용자_입력_숫자_리스트_생성_성공() {
+            String inputString = "123";
+            List<Integer> result = List.of(1, 2, 3);
 
-        player.readBallNumberInput(inputString);
+            player.readBallNumberInput(inputString);
 
-        assertThat(player.getBallNumbers()).isEqualTo(result);
+            assertThat(player.getBallNumbers()).isEqualTo(result);
+        }
+
+        @Test
+        @DisplayName("사용자 입력 난수에 숫자 외의 문자가 포함되는 케이스를 테스트한다")
+        void 사용자_입력_숫자_문자_포함_예외() {
+            String inputString = "12/3";
+
+            assertThatThrownBy(() -> player.readBallNumberInput(inputString))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(NOT_NUMBER_INCLUDE.toString());
+        }
+
+        @Test
+        @DisplayName("사용자 입력 난수가 3개가 아닌 경우를 테스트한다")
+        void 사용자_입력_숫자_개수_예외() {
+            String inputString = "1234";
+
+            assertThatThrownBy(() -> player.readBallNumberInput(inputString))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(NOT_3_NUMBERS.toString());
+        }
+
+        @Test
+        @DisplayName("사용자 입력 난수가 범위를 벗어난 경우를 테스트한다")
+        void 사용자_입력_숫자_범위_예외() {
+            String inputString = "104";
+
+            assertThatThrownBy(() -> player.readBallNumberInput(inputString))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(NOT_IN_RANGE.toString());
+        }
+
+        @Test
+        @DisplayName("사용자 입력 난수에 중복이 있는 경우를 테스트한다")
+        void 사용자_입력_숫자_중복_예외() {
+            String inputString = "414";
+
+            assertThatThrownBy(() -> player.readBallNumberInput(inputString))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(DUPLICATE_NUMBER.toString());
+        }
     }
 
-    @Test
-    @DisplayName("사용자 입력 난수에 숫자 외의 문자가 포함되는 케이스를 테스트한다")
-    void 사용자_입력_숫자_문자_포함_예외() {
-        String inputString = "12/3";
+    @Nested
+    @DisplayName("사용자의 재시작 입력을 테스트한다")
+    class testPlayerRestartInput {
+        @Test
+        @DisplayName("사용자가 재시작을 입력하는 경우를 테스트한다")
+        void 사용자_재시작_입력() {
+            String inputString = "1";
+            Boolean result = true;
 
-        assertThatThrownBy(() -> player.readBallNumberInput(inputString))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(NOT_NUMBER_INCLUDE.toString());
+            assertThat(player.readRestartInput(inputString)).isEqualTo(result);
+        }
+
+        @Test
+        @DisplayName("사용자가 종료를 입력하는 경우를 테스트한다")
+        void 사용자_종료_입력() {
+            String inputString = "2";
+            Boolean result = false;
+
+            assertThat(player.readRestartInput(inputString)).isEqualTo(result);
+        }
+
+        @Test
+        @DisplayName("사용자가 시스템에 정해지지 않은 값을 입력하는 경우를 테스트한다")
+        void 사용자_잘못된_값_입력() {
+            String inputString = "3";
+
+            assertThatThrownBy(() -> player.readRestartInput(inputString))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining(NOT_RESTART_NUMBER.toString());
+        }
     }
 
-    @Test
-    @DisplayName("사용자 입력 난수가 3개가 아닌 경우를 테스트한다")
-    void 사용자_입력_숫자_개수_예외() {
-        String inputString = "1234";
 
-        assertThatThrownBy(() -> player.readBallNumberInput(inputString))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(NOT_3_NUMBERS.toString());
-    }
-
-    @Test
-    @DisplayName("사용자 입력 난수가 범위를 벗어난 경우를 테스트한다")
-    void 사용자_입력_숫자_범위_예외() {
-        String inputString = "104";
-
-        assertThatThrownBy(() -> player.readBallNumberInput(inputString))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(NOT_IN_RANGE.toString());
-    }
-
-    @Test
-    @DisplayName("사용자 입력 난수에 중복이 있는 경우를 테스트한다")
-    void 사용자_입력_숫자_중복_예외() {
-        String inputString = "414";
-
-        assertThatThrownBy(() -> player.readBallNumberInput(inputString))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining(DUPLICATE_NUMBER.toString());
-    }
 }
