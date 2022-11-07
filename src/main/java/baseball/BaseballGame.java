8819b4338f7303465aa019d49030b9f7a3300c7f
kimminsu <xjfcnfw3@naver.com>
2022-11-07 15:24:17 +0900

    feat: 숫자야구게임 메인 기능 구현

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,35 @@
+package baseball;
+
+import baseball.computer.Computer;
+import baseball.rule.Rule;
+import baseball.user.User;
+
+public class BaseballGame {
+    private final Rule rule;
+    private final User user;
+    private final Computer computer;
+
+    public BaseballGame() {
+        rule = new Rule();
+        user = new User();
+        computer = new Computer();
+    }
+
+    public void start() {
+        boolean status = true;
+
+        while (status) {
+            computer.initComputer();
+            playGame();
+            status = user.inputOption();
+        }
+    }
+
+    private void playGame() {
+        boolean completeGame = false;
+        while (!completeGame) {
+            user.inputNumber();
+            completeGame = rule.getResult(user.getGameNumber(), computer.getGameNumber());
+        }
+    }
+}
