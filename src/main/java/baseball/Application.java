4ccfef253ef12ec256b224738fcdca8c5789168c
YooraKim <62877255+YooraKim@users.noreply.github.com>
2022-11-08 04:41:46 +0900

    feat: add codes to start the game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,14 @@
 package baseball;
 
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     static void startGame() {
-        boolean getAnswer = false;
+        boolean answer = false;
         List<Integer> gameNumbers = PlayGame.getRandomNumbers();
-        while (!getAnswer) {
+        while (!answer) {
             List<Integer> userNumbers = PlayGame.getUserNumbers();
-            List<Integer> counts = PlayGame.getCounts();
+            List<Integer> counts = PlayGame.getCounts(gameNumbers, userNumbers);
             int ball = counts.get(0);
             int strike = counts.get(1);
 
@@ -18,7 +16,7 @@ public class Application {
                 System.out.println("낫싱");
             } else if (strike == 3) {
                 System.out.println("3스트라이크");
-                getAnswer = true;
+                answer = true;
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             } else if (strike == 0) {
                 System.out.println(ball + "볼");
@@ -31,5 +29,11 @@ public class Application {
     }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        boolean gameStop = false;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (!gameStop) {
+            startGame();
+            gameStop = makeGameStop();
+        }
     }
 }
