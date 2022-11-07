2c66b719ec56b5c4b159189c0a14c4c244aa82cf
hodomaroo <enghemdwkd@gmail.com>
2022-11-08 00:26:03 +0900

    [Feat] : NumberBaseballGame - 1번의 게임을 진행하는 기능 구현
    랜덤 숫자 추출부터 게임의 종료까지 한 주기의 게임 수행을 담당하는
    run메서드를 구현하였습니다.

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 한 주기의 게임 운영을 담당하는 클래스
  */
@@ -8,7 +13,23 @@ public class NumberBaseballGame {
     /**
      * 게임을 실행하는 메서드
      */
-    void run(){
+    void run() {
+        MultiDigitNumber randomMultiDigitNumber = new MultiDigitNumber(
+            RandomManager.getDistinctIntegersInRange(1, 9, 3));
+        while (true) {
+            IOManager.print("숫자를 입력해주세요 : ");
 
+            MultiDigitNumber userMultiDigitNumber = new MultiDigitNumber(
+                IOManager.splitIntegerIntoIntegerArray(IOManager.getNextInt()));
+
+            IOManager.println(
+                randomMultiDigitNumber.getCompareResult(userMultiDigitNumber).toString());
+
+            if (userMultiDigitNumber.equals(randomMultiDigitNumber)) {
+                break;
+            }
+        }
+
+        IOManager.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
