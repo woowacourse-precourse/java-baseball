3c80daf921a85c91723f1dc10029ed5030c73478
jiwon <ljwon77@daum.net>
2022-11-07 21:06:52 +0900

    feat: print init message and get computer generated number
    게임 시작 시 시작 메시지 출력
    컴퓨터가 사용할 3글자의 숫자 생성 후 리스트로 반환

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,38 @@
+package baseball;
+
+import baseball.util.ComputerUtil;
+import baseball.util.MessageUtil;
+import baseball.util.UserUtil;
+import baseball.validation.InputValidation;
+
+import java.util.List;
+
+import static baseball.constant.NumberConst.*;
+
+/**
+ * 게임을 제어하는 메인 클래스
+ */
+public class BaseballGame {
+    private final UserUtil userUtil = new UserUtil();
+
+    private final MessageUtil messageUtil = new MessageUtil();
+
+    private final ComputerUtil computerUtil = new ComputerUtil();
+
+    private final InputValidation validator = new InputValidation();
+
+    private List<Integer> computerNums;
+
+    public BaseballGame() {
+        messageUtil.printInitMsg();
+    }
+
+    public void startGame() {
+        int gameControlNum = RESTART_NUM;
+        computerNums = computerUtil.generateNumber();
+        
+
+    }
+
+
+}
