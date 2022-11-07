1eef2264962180db28fdb3e19b3267804bb6898d
Sehbeom <j3beom@gmail.com>
2022-11-08 02:08:31 +0900

    style(Application): TODO 주석 삭제
    TODO: 프로그램 구현 주석 삭제
    
    Application.java 주석 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import baseball.domain.GameOperator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         GameOperator gameOperator = new GameOperator();
         gameOperator.numberBaseballGame();
     }
