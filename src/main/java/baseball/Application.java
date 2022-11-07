e3e487027d7f1e19eb5478981b61299c2f7f5bce
Heesom <heesom.hs@gmail.com>
2022-11-07 19:25:17 +0900

    Feat(Application) : 객체화 시킨 함수를 main 메소드에 불러와서 완성시킴

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,40 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        Application startBaseball = new Application();
+        startBaseball.printStart();
 
+        Computer computer = new Computer();
+        List<Integer> computerInput;
+        computerInput = computer.setComputer();
+
+        boolean resetComputer = false;
+        int userNumToEndGame = 0;
+        while(true) {
+            if(resetComputer) {
+                computerInput = computer.setComputer();
+                resetComputer = false;
+            }
+
+            UserAnswer userAnswer = new UserAnswer();
+            userAnswer.printEnterNum();
+            List<Integer> userInput = userAnswer.setUserAnswer();
+
+            BallOrStrike ballOrStrike = new BallOrStrike();
+            boolean isEndOfGame = ballOrStrike.countStrikeBall(userInput, computerInput);
+            if(isEndOfGame) {
+                userNumToEndGame = userAnswer.setUserEndGame();
+            }
+            if(userNumToEndGame == 1) {
+                resetComputer = true;
+                userNumToEndGame = 0;
+            } else if (userNumToEndGame == 2) {
+                break;
+            }
+        }
     }
 
     public void printStart() {
