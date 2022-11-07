db7ce6fcd3c68faacf6607fad3ff12c53ceb650b
dev-green-flamingo <devgflamingo@gmail.com>
2022-11-08 00:25:33 +0900

    refactor(computer): 코드규칙 변경
    intelij의 기본 설정에서 google style로 변경

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,32 +1,32 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
-import java.util.List;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class ApplicationTest extends NsTest {
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
+            () -> {
+                run("246", "135", "1", "597", "589", "2");
+                assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+            },
+            1, 3, 5, 5, 8, 9
         );
     }
 
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
+            assertThatThrownBy(() -> runException("1234"))
+                .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
@@ -39,8 +39,8 @@ class ApplicationTest extends NsTest {
     void createBaseBall_RandomNumber_NotDuplicatedAndSize3List() {
         List<Integer> baseball = Application.createBaseBall();
         assertThat(baseball.size())
-                .isEqualTo(3);
+            .isEqualTo(3);
         assertThat(baseball)
-                .doesNotHaveDuplicates();
+            .doesNotHaveDuplicates();
     }
 }
