c75e54cad810a77296a5f263ca1095c61792064b
devMuscle <hongsbien@naver.com>
2022-11-08 03:24:10 +0900

    feat: 스트라이크, 볼 카운트 기능

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -1,2 +1,36 @@
-package baseball;public class Judge {
+package baseball;
+
+import java.util.List;
+
+public class Judge {
+
+    public Integer countStrike(List<Integer> randomNumberList, List<Integer> userNumberList) {
+        Integer strikeCnt=0;
+
+        for(int i=0; i<randomNumberList.size(); i++) {
+            if(randomNumberList.get(i) == userNumberList.get(i)) {
+                strikeCnt++;
+            }
+        }
+
+        return strikeCnt;
+    }
+
+    public Integer countBall(List<Integer> randomNumberList, List<Integer> userNumberList) {
+        Integer ballCnt=0;
+
+        for(int i=0; i<randomNumberList.size(); i++) {
+            for(int j=0; j<userNumberList.size(); j++) {
+                if(i==j) {
+                    continue;
+                }
+
+                if(randomNumberList.get(i)==userNumberList.get(j)) {
+                    ballCnt++;
+                }
+            }
+        }
+
+        return ballCnt;
+    }
 }
