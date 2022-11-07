f331682f22c8c815d9092740f198c5b3eaa42d44
seonghoo1217 <seonghoo1217@naver.com>
2022-11-08 01:45:08 +0900

    feat : add baseBall game continuous by user input value condition
    BaseBall Game should be continue if user want to.
    So, I add while loop, which was designed as feature , ends when user press 2 and then continues to press to 1
    
     - Additionally try/catch was delete and modify to a new validation method structure

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,35 +3,60 @@ package baseball;
 import camp.nextstep.edu.missionutils.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Application {
+
+	private static int COIN;
 	private static List<Integer> random_number_list =new ArrayList<>();
+
 	public static void main(String[] args) {
+		System.out.println("숫자 야구 게임을 시작합니다.");
 		random_number_list=RandomNumber.createRandomNumber();
-		try{
+		COIN=1;
+		while (COIN==1){
+			System.out.print("숫자를 입력해주세요 : ");
 			String user_input_number = Console.readLine();
-			Validation.validation("user_input_number_validation",user_input_number);
-
-			CompareNumber.compareUserNumberAndRandomNumber(user_input_number,random_number_list);
-
-			List<Integer> score_ist = RoundScore.getScoreList();
-			System.out.println(random_number_list);
-			scoreConsoleOutput(score_ist);
-		}catch (IllegalArgumentException e){
-			return;
+			System.out.println(Validation.validation("user_input_number_validation",user_input_number));
+			if(!Validation.validation("user_input_number_validation",user_input_number)){
+				throw new IllegalArgumentException();
+			}
+			CompareNumber.compareUserNumberAndRandomNumber(user_input_number, random_number_list);
+			HashMap<String, Integer> score_map = RoundScore.getScoreMap();
+			scoreConsoleOutput(score_map);
 		}
 	}
 
 
-	private static void scoreConsoleOutput(List<Integer> score_list){
-		int strike_score = score_list.get(0);
-		int ball_score = score_list.get(1);
+
+	private static void scoreConsoleOutput(HashMap<String,Integer>score_map){
+		int strike_score = score_map.get("STRIKE");
+		int ball_score = score_map.get("BALL");
 		if (strike_score==0&&ball_score==0) {
 			System.out.println("낫싱");
+		}
+		else if(strike_score==3){
+			scoreConsoleOutputAtAllStrike(strike_score);
 		}
 		else {
 			System.out.println(ball_score+"볼 "+strike_score+"스트라이크");
 		}
+	}
+
+	private static void scoreConsoleOutputAtAllStrike(int strike_score){
+		System.out.println(strike_score+"스트라이크");
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		reTryCondition();
+	}
+	private static void reTryCondition(){
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		String user_retry_condition = Console.readLine();
+		if(Validation.validation("user_retry_condition_validation",
+											user_retry_condition)) {
+			throw new IllegalArgumentException();
+		}
+		random_number_list=RandomNumber.createRandomNumber();
+		COIN = Integer.parseInt(user_retry_condition);
 	}
 }
