5ad15d7d10f04d555fa5907a356c3727e1f44528
LeeHaiLim <hailim1997@gmail.com>
2022-11-08 04:19:14 +0900

    feat(Computer) : implement method set answer
    정답을 생성하는 기능 구현
    - import camp.nextstep.edu.missionutils.Randoms
    - import java.util.HashSet
    - 정답 answer 인스턴스 변수 선언
    - setAnswer() 메소드 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,4 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+
 public class Computer {
+    private String answer;
+
+    public void setAnswer() {
+        StringBuilder answerBuilder = new StringBuilder();
+        HashSet<Integer> numSet = new HashSet<>();
+
+        while (numSet.size() < Constants.NUMBER_COUNT) {
+            int randomNumber = Randoms.pickNumberInRange(Constants.NUMBER_RANGE_START, Constants.NUMBER_RANGE_END);
+            if (numSet.add(randomNumber)) {
+                answerBuilder.append(randomNumber);
+            }
+        }
+
+        this.answer = answerBuilder.toString();
+    }
 }
