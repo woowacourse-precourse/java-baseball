032d1a74b9c7a8edecd054a8e7dcc2da466c3e57
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2022-11-07 17:35:48 +0900

    feat :: ApplicationTest 경우 예외사항 추가 및 name 변경

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import net.bytebuddy.pool.TypePool;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -10,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
-    void 게임종료_후_재시작() {
+    void end_and_restart_correctly() {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("246", "135", "1", "597", "589", "2");
@@ -21,7 +22,18 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void end_and_restart_but_hasException() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> runException("246", "135", "1", "597", "589", "3"))
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                5, 7, 9, 2, 5, 3
+        );
+    }
+
+    @Test
+    void exception_test_overSize() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
@@ -29,7 +41,7 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트_2() {
+    void exception_test_subSize() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("12"))
                         .isInstanceOf(IllegalArgumentException.class)
