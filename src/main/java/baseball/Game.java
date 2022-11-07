8b80c136503d9694fec299d7cbbb5d827113a3d9
Park shinyeong <syngh503@gmail.com>
2022-11-08 02:13:26 +0900

    Refactor(Game): endGame 메서드 삭제
    endGame 메서드가 사용되지 않아 삭제하였습니다.
    게임이 재시작될 때마다 새로운 객체가 할당되기 때문에 기존의 객체를 참조하는 변수가 없어 가비지 컬렉터가 자동으로 정리합니다.
    이렇게 보았을 때 endGame 메서드가 필요하지 않을 것 같아 삭제하였습니다.
    
    Relate to: #8

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -60,12 +60,6 @@ public class Game {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public void endGame() {
-        this.gameStatus = false;
-        this.gamehost = null;
-        this.player = null;
-    }
-
     public boolean isGameEnd(String hint) {
         return hint.equals("3스트라이크");
     }
