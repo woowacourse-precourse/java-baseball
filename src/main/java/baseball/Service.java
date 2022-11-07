2fdb3072d2226e048ccc39092ff39f0715264550
twknds <twknds@naver.com>
2022-11-07 18:28:43 +0900

    feat: Service 인터페이스 추가

---
+++ src/main/java/baseball/Service.java
@@ -0,0 +1,17 @@
+package baseball;
+
+public interface Service {
+    boolean checkBall();
+
+    boolean checkStrike();
+
+    int[] baseBall();
+
+    char[] changetoChar();
+
+    void printBaseBallCount();
+
+    int randNumGenerate();
+
+    void readyGame();
+}
