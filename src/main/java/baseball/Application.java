bc229c32aa5d34851207c38b18b8cb165dd0e8b3
solesy <tkddbs9801@gmail.com>
2022-11-07 11:54:57 +0900

    feat(main): add functions for playing game
    게임을 하기위한 함수와 게임을 직접 하는 함수를 추가한다.
    - 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Application {
     private boolean isNumberWithoutZero(String str){
@@ -42,6 +44,18 @@ public class Application {
             }
         }
     }
+    private List<Integer> createComputerNumbers(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
