b1e671d36c4b6a397f4f13f009a730c0aafa5ddc
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-07 19:07:26 +0900

    feat : checkComputerGameOptionNumberBetweenOneAndTwo 메서드 추가

--- src/main/java/baseball/exception/ComputerException.java
+++ src/main/java/baseball/exception/ComputerException.java
@@ -11,6 +11,15 @@ public class ComputerException {
         }
         return false;
     }
+    public boolean checkComputerGameOptionNumberBetweenOneAndTwo(String ComputerGameOptionNumber)
+            throws IllegalArgumentException {
+        if (!ComputerGameOptionNumber.equals(Constant.COMPUTER_GAME_RESTART) && !ComputerGameOptionNumber.equals(
+                Constant.COMPUTER_GAME_EXIT)) {
+            throw new IllegalArgumentException("1, 2 외의 숫자를 입력하셨습니다.");
+        }
+        return true;
+    }
+
 
 
 }
