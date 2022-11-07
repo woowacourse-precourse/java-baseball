41bdc64a87cf3074fa8a422ae793e1f1f8c13091
sonsoowon <yuwoo1018@gmail.com>
2022-11-08 06:15:01 +0900

    test: 볼/스트라이크 개수를 리스트로 반환

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -117,6 +117,32 @@ class ApplicationTest extends NsTest {
 
         assertThat(expectedOutput).isEqualTo(realOutput);
     }
+
+    @Test
+    void getBallStrikeCnt_볼_스트라이크_개수를_리스트로_반환(){
+        Game game = new Game(List.of(1, 2, 3));
+        List<List<Integer>> playerInput = List.of(
+                List.of(1, 2, 3),
+                List.of(1, 5, 3),
+                List.of(3, 1 ,2),
+                List.of(3, 2, 1),
+                List.of(9, 8, 7)
+        );
+        List<List<Integer>> expectedOutput = List.of(
+                List.of(0, 3),
+                List.of(0, 2),
+                List.of(3, 0),
+                List.of(2, 1),
+                List.of(0, 0)
+        );
+
+        List<List<Integer>> realOutput = playerInput.stream()
+                .map(input -> game.getBallStrikeCnt(input))
+                .collect(Collectors.toList());
+
+        assertThat(expectedOutput).isEqualTo(realOutput);
+
+    }
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
