2867e93f5e43f6ca47a2493544d8f654c92eb23c
keon0711 <fnc6488@gmail.com>
2022-11-08 03:14:42 +0900

    style: remove unnecessary blank space

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,7 +16,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-        do  {
+        do {
             List<Integer> computerNumber = generateComputerNum();
             readAndCompare(computerNumber);
         } while (checkRestart());
