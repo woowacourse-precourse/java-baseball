2bc595a2ca8f1b8948973a3fa32a1b1c1233766c
jonghun <jonghuncu@gmail.com>
2022-11-07 18:50:47 +0900

    Feat : 정답 check 기능 구현 중

--- src/main/java/baseball/checkAnswer.java
+++ src/main/java/baseball/checkAnswer.java
@@ -1,10 +1,28 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class checkAnswer {
-    public static boolean checkAnswer(List<Integer> InputNumber) {
+    public static List<Integer> checkAnswer(List<Integer> inputNumber, List<Integer> answerNumber, int i) {
+        List<Integer> numLst = new ArrayList<>();
 
-        return true;
+        int strikeNum = 0;
+        int ballNum = 0;
+
+        if(answerNumber.contains(inputNumber.get(i))) {
+            if(answerNumber.get(i).equals(inputNumber.get(i))) {
+                strikeNum += 1;
+            } else {
+                ballNum += 1;
+            }
+            /**
+             * indent 3초과 리팩터링 하기
+             */
+        }
+        numLst.add(strikeNum);
+        numLst.add(ballNum);
+
+        return numLst;
     }
 }
