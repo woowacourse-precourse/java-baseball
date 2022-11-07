744c1fa4a9074272a9d8bd7a7b69361e9463a16b
sonsoowon <yuwoo1018@gmail.com>
2022-11-08 06:34:46 +0900

    feat(Application): 게임 시작
    - 볼/스트라이크 리스트에 접근할 때의 인덱스를 상수로 선언
    - startGame 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
+    public static int BALL_INDEX = 0;
+    public static int STRIKE_INDEX = 1;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    static void startGame() {
+        Game game = new Game();
+
+        List<Integer> playerGuess;
+        int ball;
+        int strike;
+
+        do {
+            playerGuess = Player.guessAnswerOfGame();
+
+            List<Integer> ballStrikeCnt = game.getBallStrikeCnt(playerGuess);
+            ball = ballStrikeCnt.get(BALL_INDEX);
+            strike = ballStrikeCnt.get(STRIKE_INDEX);
+
+            System.out.println(Game.getResultOfPlayerGuess(ball, strike));
+        } while (!Game.isAllStrike(strike));
+
+
     }
 }
