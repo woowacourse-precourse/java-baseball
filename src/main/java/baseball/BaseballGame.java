c0f83ef8b8afda449b9191d1371c8cb1363ae72d
Fizioo0102 <a27313797@gmail.com>
2022-11-07 23:48:48 +0900

    feat: ballCheck() 함수 생성
    - user List 와 computer List 를 비교하여 다른 자리에 같은 숫자가 있다면 cnt 변수를 증가시켜 볼의 갯수를 정수형으로 반환해주는 함수 작성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -15,4 +15,14 @@ public class BaseballGame {
         return cnt;
     }
 
+    static int ballCheck(List<Integer> cut, List<Integer> computer) {
+        int cnt = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if ((cut.get(i) != computer.get(i)) && computer.contains(cut.get(i))) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
 }
