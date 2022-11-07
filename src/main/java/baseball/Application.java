40f5143981edabb6910c643ca7638b3e7d78d2ea
jhsseonn <hosu0125@gmail.com>
2022-11-08 02:25:05 +0900

    chore: application 파일에 게임 시작 함수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,11 @@
 package baseball;
 
+import baseball.Controller.BaseballController;
+
 public class Application {
+    public static BaseballController baseballController;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        baseballController.startBaseballGame();
     }
 }
