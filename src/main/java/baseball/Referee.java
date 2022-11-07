c1286be76a3663860d356b433737edad428af9cf
taekwon-dev <taekwon_dev@naver.com>
2022-11-07 14:47:59 +0900

    feat: 투수가 입력한 숫자와 포수가 생성한 숫자 비교 후 아래 힌트 중 하나 반환
    - 낫싱
    - 1스트라이크
    - 2스트라이크
    - 3스트라이크
    - 1볼
    - 2볼
    - 3볼
    - 1볼 1스트라이크
    - 2볼 1스트라이크

---
+++ src/main/java/baseball/Referee.java
@@ -0,0 +1,51 @@
+package baseball;
+
+import java.util.List;
+
+import static baseball.Hint.NOTHING;
+import static baseball.Hint.S3;
+
+public class Referee {
+    public String getHint(List<Integer> catcherBalls, List<Integer> pitcherBalls) {
+        if (catcherBalls.equals(pitcherBalls)) {
+            return S3.getLabel();
+        }
+        List<Integer> ballAndStrikeCounts = countBallAndStrike(catcherBalls, pitcherBalls);
+        Hint hint = generateHint(ballAndStrikeCounts);
+        return hint.getLabel();
+    }
+
+    private List<Integer> countBallAndStrike(List<Integer> catcherBalls, List<Integer> pitcherBalls) {
+        int ballCount = 0;
+        int strikeCount = 0;
+        for (int pitcherBallIdx = 0; pitcherBallIdx < pitcherBalls.size(); pitcherBallIdx++) {
+            int pitcherBall = pitcherBalls.get(pitcherBallIdx);
+            if (catcherBalls.contains(pitcherBall) && !compare(catcherBalls.indexOf(pitcherBall), pitcherBallIdx)) {
+                ballCount++;
+            } else if (catcherBalls.contains(pitcherBall) && compare(catcherBalls.indexOf(pitcherBall), pitcherBallIdx)) {
+                strikeCount++;
+            }
+        }
+        return List.of(ballCount, strikeCount);
+    }
+
+    private boolean compare(int catcherBallIdx, int pitcherBallIdx) {
+        return catcherBallIdx == pitcherBallIdx;
+    }
+
+    private Hint generateHint(List<Integer> ballAndStrikeCounts) {
+        int ballCount = ballAndStrikeCounts.get(0);
+        int strikeCount = ballAndStrikeCounts.get(1);
+        if (ballCount == 0 && strikeCount == 0) {
+            return NOTHING;
+        }
+        StringBuilder hintBuffer = new StringBuilder();
+        if (ballCount != 0) {
+            hintBuffer.append("B").append(ballCount);
+        }
+        if (strikeCount != 0) {
+            hintBuffer.append("S").append(strikeCount);
+        }
+        return Hint.valueOf(hintBuffer.toString());
+    }
+}
