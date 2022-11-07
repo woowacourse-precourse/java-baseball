9490f8cfd66304ab1a50d7c3e1335de0197129b2
ffolabear <ffolabear@gmail.com>
2022-11-08 00:49:24 +0900

    refactor(Input): 코드 리팩토링

--- src/main/java/baseball/util/Input.java
+++ src/main/java/baseball/util/Input.java
@@ -3,7 +3,6 @@ package baseball.util;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -14,22 +13,14 @@ public class Input {
     }
 
     public static List<String> inputNumber() {
-        String userAnswerInput = Console.readLine();
-        List<String> inputNumberList = new ArrayList<>();
-        for (int i = 0; i < userAnswerInput.length(); i++) {
-            inputNumberList.add(String.valueOf(userAnswerInput.charAt(i)));
-        }
+        List<String> inputNumberList = List.of(Console.readLine().split(""));
         isValidNumber(inputNumberList);
         return inputNumberList;
     }
 
     private static void isValidNumber(List<String> inputNumberList) {
-        if (inputNumberList.size() == 3) {
-            Set<String> listSet = new HashSet<>(inputNumberList);
-            if (listSet.size() != 3) {
-                throw new IllegalArgumentException();
-            }
-        } else {
+        Set<String> listSet = new HashSet<>(inputNumberList);
+        if (listSet.size() != 3) {
             throw new IllegalArgumentException();
         }
     }
@@ -41,11 +32,9 @@ public class Input {
     }
 
     private static void isValidReplayNumber(String inputNumber) {
-
         if (!inputNumber.matches("[12]")) {
             throw new IllegalArgumentException();
         }
-
     }
 
 }
