3b5dabec037f384280040bcc077b22fb6a28b240
Jeongthingamajig <netpetet@naver.com>
2022-11-07 22:02:45 +0900

    Refactor : 메서드 변경
    static을 이용해 메서드 안에서 리스트를 선언하여 nullPointerException 안터지게 변경, 직접 만든 생성자 삭제

---
+++ src/main/java/baseball/model/ComputerNumber.java
@@ -0,0 +1,33 @@
+package baseball.model;
+
+import baseball.util.NumberNaming;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+/*
+컴퓨터 숫자를 구해서 List로 반환
+ */
+public class ComputerNumber {
+
+
+    public static List<Integer> getComputerRandomNumber(){
+        List<Integer> computerNumber = new ArrayList<>();
+        while(computerNumber.size() < NumberNaming.MAX_NUMBER_SIZE){
+            createRandomNumber(computerNumber, Randoms.pickNumberInRange(NumberNaming.MIN_RANGE_NUM, NumberNaming.MAX_RANGE_NUM));
+            }
+        return computerNumber;
+        }
+
+    public static void createRandomNumber(List<Integer> RandomNumberList, int number){
+        if(!RandomNumberList.contains(number)){
+            RandomNumberList.add(number);
+        }
+    }
+
+    public ComputerNumber(List<Integer> computerRandomNumber) {
+        getComputerRandomNumber();
+        }
+    }
+
+
