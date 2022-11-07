1039b57259c64b03ef9fa3d652bcbb46b959b010
anso33 <hyeon131812@gmail.com>
2022-11-07 10:10:49 +0900

    [FEAT](Game): 해당하는 자릿수의 숫자만 뽑아주는 함수 findDigitNumber 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -17,17 +17,29 @@ public class Game {
 
     }
 
-    private boolean isStrike(int where) {
+    private int findDigitNumber(int number, int where) {
         int digit = (int) Math.pow(10, where);
         int digit_under = (int) Math.pow(10, where - 1);
 
-        int random_digit_number = (random_number % digit) / digit_under;
-        int input_digit_number = (random_number % digit) / digit_under;
+        int digit_number = (number % digit) / digit_under;
+
+        return digit_number;
+    }
+
+    private boolean isStrike(int where) {
+        int random_digit_number = findDigitNumber(random_number, where);
+        int input_digit_number = findDigitNumber(input_number, where);
         if (random_digit_number == input_digit_number)
             return true;
         else
             return false;
     }
+//
+//    private boolean isBall(int where) {
+//        for (int i = 0; i < 3; i++) {
+//
+//        }
+//    }
 }
 
 ///245   200 % 1000 -> 200
