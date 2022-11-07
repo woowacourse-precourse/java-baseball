3d695c892bf7168ae21b057e3dc51cb0e3d2c191
KIMSEI1124 <workju1124@gmail.com>
2022-11-07 17:13:29 +0900

    refactor: Game 객체 주석 처리
    빌드 오류 확인을 위한 주석 처리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Game game = new Game();
-        game.initGame();
+//        Game game = new Game();
+//        game.initGame();
     }
 }
