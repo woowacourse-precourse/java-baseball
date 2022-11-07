e260fe9e4c2de67f9ab8bb60e3f03441f17607ed
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-07 19:15:40 +0900

    feat : Application을 실행하기 위한 controller 객체 생성 및 run함수 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import static baseball.view.InputView.printFirstNumberBaseballGameMessage;
+
+import baseball.controller.GameController;
 public class Application {
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        printFirstNumberBaseballGameMessage();
+        GameController GameController = new GameController();
+        GameController.run();
     }
+
+
+
+
+
+
+
+
 }
