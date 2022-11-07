65d8af4746da9d0e742b5f5807a09b2d559d969d
inhyeok <ss112d86@gmail.com>
2022-11-08 02:40:25 +0900

    refactor: 게임종료 메시지 수정
    게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. 메시지를 ConsoleInput으로 변경

--- src/main/java/baseball/console/ConsoleOutput.java
+++ src/main/java/baseball/console/ConsoleOutput.java
@@ -8,7 +8,7 @@ public class ConsoleOutput {
   }
 
   public void printEndMessage() {
-    final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     System.out.println(END_MESSAGE);
   }
 }
