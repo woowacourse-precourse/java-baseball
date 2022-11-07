f1be6b469a2a5dca3945bece8f0ce9250ea33bca
Junho Choi <ajchoi0928@gmail.com>
2022-11-08 00:40:41 +0900

    feat($main): 사용자 입력에 대해 예외를 검증
    void processComparing(String answer) 메소드 내부에서 사용자 입력을 받고
    - 문자열 길이 검증
    - 1~9까지의 숫자로 구성되었는지 검증
    - 중복이 없는지 검증
    
    사용자가 잘못된 값을 입력한 경우 `IllegalArgumentException` 발생

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
@@ -25,7 +27,7 @@ public class Application {
             }
             GameFormatter.gameStartMessage();
             String answer = initComputerAnswer();
-            GameFormatter.gameInputMessage();
+            processComparing(answer);
             GameFormatter.gameEndMessage();
             status = setGameStatus(Console.readLine());
         } while (true);
@@ -53,6 +55,36 @@ public class Application {
         return status;
     }
 
+    /**
+     * 상대방(컴퓨터)가 1부터 9까지 셔플한 결과 중 3개를 뽑아 문자열로 반환
+     *
+     * @return answer
+     */
+    String initComputerAnswer() {
+        StringBuffer answer = new StringBuffer();
+        // 1~9까지 숫자 중 shuffle 한 다음, 앞의 3개의 수를 반환
+        List<Integer> list = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        for (Integer uniqueNum : list) {
+            answer.append(uniqueNum);
+        }
+        return answer.toString();
+    }
+
+    /**
+     * 정답과 사용자 입력값을 비교하고 결과를 반환하는 함수
+     *
+     * @param answer
+     */
+    void processComparing(String answer) {
+        do {
+            GameFormatter.gameInputMessage();
+            String player = Console.readLine();
+            validateLength(player);
+            validateNumericValue(player);
+            validateUniqueNumber(player);
+        } while (true);
+    }
+
     boolean isStopStatus(GameStatus status) {
         return status.equals(GameStatus.STOP);
     }
@@ -61,18 +93,37 @@ public class Application {
         return status.equals(GameStatus.EXCEPTION);
     }
 
-    /**
-     * 상대방(컴퓨터)가 1부터 9까지 셔플한 결과 중 3개를 뽑아 문자열로 반환
-     * @return answer
-     */
-    String initComputerAnswer(){
-        StringBuffer answer = new StringBuffer();
-        // 1~9까지 숫자 중 shuffle 한 다음, 앞의 3개의 수를 반환
-        List<Integer> list = Randoms.pickUniqueNumbersInRange(1, 9, 3);
-        for(Integer uniqueNum : list){
-            answer.append(uniqueNum);
+    void validateLength(String input) {
+        if (input.length() == 0 || input.length() > 3) {
+            throw new IllegalArgumentException();
         }
-        return answer.toString();
     }
 
+    /**
+     * [1~9] 에 해당하는 숫자 데이터가 아닐 떄
+     *
+     * @param input
+     */
+    void validateNumericValue(String input) {
+        for (char c : input.toCharArray()) {
+            if (!Character.isDigit(c) || c == '0') {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    /**
+     * Set의 특성을 이용해 중복 문자 발생시 예외 발생
+     *
+     * @param input
+     */
+    void validateUniqueNumber(String input) {
+        Set<Character> set = new HashSet<>();
+        for (char c : input.toCharArray()) {
+            set.add(c);
+        }
+        if (set.size() != input.length()) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
