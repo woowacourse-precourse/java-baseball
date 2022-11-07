1a19322e52c5d6fe6265223abae06088aba78492
Qulip <ilkwon.ryu@gmail.com>
2022-11-08 05:21:55 +0900

    feat: BaseballGameService 클래스 추가

---
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -0,0 +1,11 @@
+package baseball.service;
+
+import baseball.domain.BaseballNumber;
+
+public class BaseballGameService {
+    private final BaseballNumber computer;
+
+    public BaseballGameService(){
+        computer = new BaseballNumber();
+    }
+}
