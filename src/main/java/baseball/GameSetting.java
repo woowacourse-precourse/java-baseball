4d8ba611f1237912ef039d50a5c99a5e932e5c23
chaeyeong222 <iky1505@naver.com>
2022-11-07 15:00:29 +0900

    refactor : 게임 시작 시 랜덤값 생성 기능 외부클래스로 다시 구현

--- src/main/java/baseball/GameSetting.java
+++ src/main/java/baseball/GameSetting.java
@@ -6,9 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameSetting {
-
-
-    private static int GetRandomNumber() {
+    
+    public static int getRandomNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
