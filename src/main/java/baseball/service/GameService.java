0b998b9fdeafce8902bd10824d1932f9fdd9034e
simba <shim9597@gmail.com>
2022-11-07 18:18:11 +0900

    feat: getUserNumber 기능 구현
    input 값을 받아서 검증하고 조작한 후 반환하는 기능 구현 완료

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -2,6 +2,11 @@ package baseball.service;
 
 import baseball.utils.PrintMessage;
 import baseball.utils.RandomNumber;
+import domain.Game;
+import domain.User;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -9,11 +14,33 @@ public class GameService {
 
     PrintMessage print = new PrintMessage();
     RandomNumber computer = new RandomNumber();
+    Game game = new Game();
+    User user = new User();
     InGame inGame = new InGame();
     String input;
-    public void gameStart() {
-        input = readLine();
-        print.inputRequest();
-        inGame.validate(input);
+
+    List<Integer> computerNumber;
+
+    public void playGame() {
+        computerNumber = computer.newRandomNumber();
+        int strike = 0;
+        while (strike != 3) {
+            print.inputRequest();
+            play();
+            print.status(game.getBall(), game.getStrike());
+            strike = game.getStrike();
+        }
     }
+
+    private void play() {
+        game.initStrike();
+        game.initBall();
+        int[] userNumbers = getUserNumbers();
+        user.setUserNumber(userNumbers);
+    }
+    private int[] getUserNumbers() throws IllegalArgumentException{
+        input = readLine();
+        return inGame.userInputToInt(input);
+    }
+
 }
