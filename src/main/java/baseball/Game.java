2d082d83c8327344d3336ccbbeec9f3f7ba580f6
meunrud <y3a0522@gmail.com>
2022-11-08 03:17:39 +0900

    :sparkles: 볼 & 스트라이크 계산 구현 및 결과 출력

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -18,4 +18,43 @@ public class Game {
     }
 
 
+    public void getScore(){
+        String input = inputNumberList.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        String answer = answerNumberList.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+
+        for (int i = 0; i<4; i++){
+            int index = input.indexOf(answer.charAt(i));
+            if(index == i){
+                strike++;
+            }
+            if(index != -1){
+                ball++;
+            }
+        }
+    }
+
+    public void getHintMessage(){
+        if(ball > 0){
+            System.out.println(ball + "볼");
+        }
+        if(strike > 0){
+            System.out.println(strike + "스트라이크");
+        }
+        if(ball + strike == 0){
+            System.out.println("낫싱");
+        }
+        System.out.println();
+    }
+
+    public boolean getWin(){
+        return strike == 3;
+    }
+
+
+
+
 }
