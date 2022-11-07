5fb806182e28935451656d52259df6d381e1e207
OhRetry <OhRetry@users.noreply.github.com>
2022-11-07 16:02:37 +0900

    feat: add function for feature 'parse user input'
    기능 요구 사항 'parse user input'를 구현한 함수 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -28,4 +28,34 @@ public class Application {
         return result;
     }
 
+    public static List<Integer> parseUserInput(String ipt) throws IllegalArgumentException {
+        List<Integer> result = new ArrayList<>();
+        boolean[] duplicate = new boolean[10];
+
+        //3자릿수 길이제한
+        if (ipt.length() != 3) {
+            throw new IllegalArgumentException("입력의 길이가 맞지 않음");
+        }
+
+        /*
+         * Integer.parseInt 함수를 쓰면 오히려 복잡해지는 부분이 생김(음수 등)
+         * 그냥 아스키 코드로 직접 처리했다.
+         */
+        char c;
+        for (int i = 0; i < ipt.length(); i++) {
+            c = ipt.charAt(i);
+            // 1~9까지 숫자가 아닌 경우
+            if (c < 49 || c > 57) {
+                throw new IllegalArgumentException("유효 숫자가 아님");
+            }
+            //중복되는 경우
+            if (duplicate[c - 48]) {
+                throw new IllegalArgumentException("중복되는 숫자 존재");
+            }
+            duplicate[c - 48] = true;
+            result.add(c - 48);
+        }
+        return result;
+    }
+
 }
