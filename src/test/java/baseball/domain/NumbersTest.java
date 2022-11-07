f1a213b1c93c9b7aa932307ea40fe9a147acb121
이건회 <rg970604@naver.com>
2022-11-08 06:31:30 +0900

    test(NumbersTest.java) : 예외 관련 테스트코드 ValidatorTest로 이동

--- src/test/java/baseball/domain/NumbersTest.java
+++ src/test/java/baseball/domain/NumbersTest.java
@@ -18,26 +18,4 @@ public class NumbersTest {
         assertThat(numbers.numbers.size()).isEqualTo(REGULAR_NUMBERS_SIZE);
     }
 
-    @Test
-    @DisplayName("리스트에 중복 숫자 발견 시 IllegalArgumentException 예외 발생")
-    void 중복된_숫자_예외() {
-        List<Integer> numbers = List.of(1, 2, 2);
-        assertThatThrownBy(
-            () -> {
-                new Numbers(numbers);
-            }
-        ).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("리스트에 숫자 4개 이상 입력 시 IllegalArgumentException 예외 발생")
-    void 숫자_4개이상_예외() {
-        List<Integer> numbers = List.of(1, 2, 3, 4);
-        assertThatThrownBy(
-            () -> {
-                new Numbers(numbers);
-            }
-        ).isInstanceOf(IllegalArgumentException.class);
-    }
-
 }
