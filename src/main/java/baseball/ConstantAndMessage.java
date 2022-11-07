a0b373906fafb9cf6dc252185ff67434c76544fb
MuSeung <peter0757@nvaer.com>
2022-11-07 17:50:33 +0900

    feat: 숫자야구게임 기능 구현
    NumberBaseBallGame
        - 생성자 인자로 computer의 숫자 리스틀 받도록 함.
        - 유저 입력을 받아 compareUserAndComputer(User)로 볼과 스트라이크를 변경해주는 메서드 getUserNumber() 구현
        - ball, strike 모두 0일때 낫싱을 출력하는 메서드 printNothing() 구현
        - ball과 strike가 1이상 일 때 출력해주는 printBall()과 printStrike() 구현
        - 3스트라이크 상황이 되기전까지 입력을 계속 받으며 문구를 출력해주는 play() 메서드 구현.
    ConstantAndMessage
        -게임 출력 메세지를 추가

--- src/main/java/baseball/ConstantAndMessage.java
+++ src/main/java/baseball/ConstantAndMessage.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConstantAndMessage {
+
     public static final int NUMBER_LENGTH = 3;
     public static final int START_NUMBER = 1;
     public static final int END_NUMBER = 9;
     public static final List<Integer> ALLOWED_INPUT_NUMBERS = allowedNumbers();
-    private static List<Integer> allowedNumbers(){
+    private static List<Integer> allowedNumbers() {
         List<Integer> numbers = new ArrayList<>();
-        for(int i=START_NUMBER;i<=END_NUMBER;i++){
+        for (int i = START_NUMBER; i <= END_NUMBER; i++) {
             numbers.add(i);
         }
         return numbers;
     }
+    public static final String STRIKE_MESSAGE = "스트라이크";
+    public static final String BALL_MESSAGE = "볼";
+    public static final String NOTHING_MESSAGE = "낫싱";
+    public static final String GETTING_USER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String GAME_END_MESSAGE = NUMBER_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
 }
---
+++ src/main/java/baseball/NumberBaseBallGame.java
@@ -0,0 +1,58 @@
+package baseball;
+
+import static baseball.ConstantAndMessage.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
+public class NumberBaseBallGame {
+
+    private int strike;
+    private int ball;
+    private final List<Integer> computer;
+
+    public void play() {
+        while (strike < NUMBER_LENGTH) {
+            System.out.print(GETTING_USER_NUMBER_MESSAGE);
+            getUserNumber();
+            printNothing();
+            printBall();
+            printStrike();
+            System.out.println();
+        }
+        System.out.println(GAME_END_MESSAGE);
+    }
+    protected void getUserNumber(){
+        User user = new User(Console.readLine());
+        user.errorChecking();
+        compareUserAndComputer(user);
+    }
+    protected void compareUserAndComputer(User user) {
+        strike = user.strike(computer);
+        ball = user.ball(computer);
+    }
+    protected void printNothing() {
+        if (strike == 0 && ball == 0) {
+            System.out.print(NOTHING_MESSAGE);
+        }
+    }
+    protected void printBall() {
+        if (ball > 0) {
+            System.out.print(ball + BALL_MESSAGE);
+            if(strike > 0){
+                System.out.print(" ");
+            }
+        }
+    }
+    protected void printStrike() {
+        if (strike > 0) {
+            System.out.print(strike + STRIKE_MESSAGE);
+        }
+    }
+
+    public NumberBaseBallGame(List<Integer> computer) {
+        this.computer = computer;
+        strike = 0;
+        ball = 0;
+    }
+}
