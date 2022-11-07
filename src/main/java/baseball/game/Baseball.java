6ff90fc9f838bd512fa2e2694f6462d4b6f4a9ff
hyun0404woo <hyun0404woo@gmail.com>
2022-11-07 16:06:54 +0900

    feat: 유저의 입력과 컴퓨터의 숫자를 비교하는 메서드 생성
    - Ball Count 반환
    - Strike Count 반환
    - Guess Fail 여부 반환

--- src/main/java/baseball/game/Baseball.java
+++ src/main/java/baseball/game/Baseball.java
@@ -2,6 +2,7 @@ package baseball.game;
 
 import java.util.List;
 
+import static baseball.game.BaseballGame.BASEBALL_LENGTH_LIMIT;
 import static baseball.util.RandomGenerator.createRandomIntList;
 
 public class Baseball {
@@ -10,5 +11,24 @@ public class Baseball {
 
     public Baseball(int start, int end, int limit) {
         this.numbers = createRandomIntList(start, end, limit);
+    }
+
+    public int getStrikeCount(List<Integer> input) {
+        return (int) input.stream()
+                .filter(n -> input.indexOf(n) == numbers.indexOf(n))
+                .count();
+    }
+
+    public int getBallCount(List<Integer> input) {
+        return (int) input.stream()
+                .filter(numbers::contains)
+                .filter(n -> input.indexOf(n) != numbers.indexOf(n))
+                .count();
+    }
+
+    public boolean isGuessFailed(List<Integer> input) {
+        return input.stream()
+                .filter(n -> input.indexOf(n) == numbers.indexOf(n))
+                .count() != BASEBALL_LENGTH_LIMIT;
     }
 }
