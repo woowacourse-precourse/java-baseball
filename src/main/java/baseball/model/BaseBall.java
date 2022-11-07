cba3adc871b1a669c735d5e28b083a17651ec5d9
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-08 00:15:11 +0900

    feat(BaseBall): strike, ball setter 추가 및 초기화 메서드 작성

--- src/main/java/baseball/model/BaseBall.java
+++ src/main/java/baseball/model/BaseBall.java
@@ -8,4 +8,17 @@ public class BaseBall {
         this.strike = 0;
         this.ball = 0;
     }
+
+    public void setStrike(int strikeCount) {
+        this.strike = strikeCount;
+    }
+
+    public void setBall(int ballCount) {
+        this.ball = ballCount;
+    }
+
+    public void resetCount() {
+        this.strike = 0;
+        this.ball = 0;
+    }
 }
