78b49fcae215d0d7c2b590cc2b5953b05db0ab2d
c1ce <chalicenuclear.gmail.com>
2022-11-07 13:52:56 +0900

    feat : 랜덤숫자를 리스트로 생성

---
+++ src/main/java/baseball/NumberManagement.java
@@ -0,0 +1,21 @@
+package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberManagement {
+
+    int number;
+
+    public static List<Integer> randomNumberMake(int loopFrequency) {
+        List<Integer> randomList = new ArrayList<>();
+        while(randomList.size()<loopFrequency) {
+            int checkNumber = Randoms.pickNumberInRange(1,9);
+            if (!randomList.contains(checkNumber)) {
+                randomList.add(checkNumber);
+            }
+        }
+        return randomList;
+    }
+}
