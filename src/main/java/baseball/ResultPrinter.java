494d2b57a6bb75477ad36b4119f7b492dce6f70e
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-07 13:33:40 +0900

    feat(ResultPrinter) : printResult 메서드 추가
    입력된 숫자의 결과로 볼, 스트라이크, 낫싱을 출력함

--- src/main/java/baseball/ResultPrinter.java
+++ src/main/java/baseball/ResultPrinter.java
@@ -1,5 +1,28 @@
 package baseball;
 
+import java.util.List;
+
 public class ResultPrinter {
 
+    public void printResult(String input, String createdNumber) {
+        final int STRIKE_INDEX = 0;
+        final int BALL_INDEX = 1;
+        final String BALL_STRING = "볼";
+        final String STRIKE_STRING = "스트라이크";
+        final String NOTHING_STRING = "낫싱";
+
+        ResultValidator resultValidator = new ResultValidator();
+        List<Integer> result = resultValidator.validate(input, createdNumber);
+        int strike = result.get(STRIKE_INDEX);
+        int ball = result.get(BALL_INDEX);
+        if (ball != 0) {
+            System.out.println(ball + BALL_STRING);
+        }
+        if (strike != 0) {
+            System.out.println(strike + STRIKE_STRING);
+        }
+        if (strike == 0 && ball == 0) {
+            System.out.println(NOTHING_STRING);
+        }
+    }
 }
--- src/main/java/baseball/ResultValidator.java
+++ src/main/java/baseball/ResultValidator.java
@@ -3,6 +3,7 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class ResultValidator {
     public List<Integer> validate(String input, String createdNumber) {
         List<Integer> result = new ArrayList<>();
