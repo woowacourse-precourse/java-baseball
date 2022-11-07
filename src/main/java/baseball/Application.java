45d58175caa8e01298c350a6094052f6efd1e9ca
canbeyour <wuzucy@gmail.com>
2022-11-07 18:46:57 +0900

    feat: add ability to init game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
+    public static List<Integer> generateGameAnswer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+    public static List<Integer> initGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        return generateGameAnswer();
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
