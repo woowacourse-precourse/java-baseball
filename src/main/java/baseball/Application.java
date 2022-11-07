21052cd15eca4070a888929ab7f0338d802e3e95
dener8 <8dener@gmail.com>
2022-11-07 20:01:01 +0900

    feat: main에 대략적인 기능 구현
    수정해야 하는 부분:
    스트라이크와 볼의 개수를 모두 출력하기 때문에, 한 가지만 나올 때를 대비하여 수정해야 함
    우선적으로 main에 전부 구현했으므로, 기능을 작게 쪼개야 함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,43 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
-        NumberMaker numberMaker = new NumberMaker();
-        String randomNumber = numberMaker.makeRandomNumber();
-        String playerNumber = numberMaker.makePlayerNumber();
 
         int ball;
         int strike;
-        // 완전히 동일한 경우
-        if (randomNumber.equals(numberMaker)) {
+        int restartOrNot;
+        Scanner sc = new Scanner(System.in);
+        // 우선 main에서 짜고 분리하는 방식으로 해야겠다 ..
+        NumberMaker numberMaker = new NumberMaker();
+        String randomNumber;
+        String playerNumber;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        randomNumber = numberMaker.makeRandomNumber();
+        while (true) {
+            System.out.println("숫자를 입력해주세요 :");
+            playerNumber = numberMaker.makePlayerNumber();
+            // 완전히 동일한 경우
+            if (randomNumber.equals(numberMaker)) {
+                System.out.println("3스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                restartOrNot = sc.nextInt();
+                if (restartOrNot == 1) {
+                    randomNumber = numberMaker.makeRandomNumber();
+                    continue;
+                } else {
+                    break;
+                }
 
-        } else {
-
+            } else {
+                BallStrikeChecker ballStrikeChecker = new BallStrikeChecker();
+                ball = ballStrikeChecker.countBall(randomNumber, playerNumber);
+                strike = ballStrikeChecker.countStrike(randomNumber, playerNumber);
+                System.out.println(strike + "스트라이크 " + ball + "볼");
+            }
         }
-        // while문 들어가야 함
-        BallStrikeChecker ballStrikeChecker = new BallStrikeChecker();
-        ball = ballStrikeChecker.countBall(randomNumber, playerNumber);
-        strike = ballStrikeChecker.countStrike(randomNumber, playerNumber);
 
     }
 }
