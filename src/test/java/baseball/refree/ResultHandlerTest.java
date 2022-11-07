907c5cf015cccd28aae86fac27ce93ff18bcc0eb
math0917 <math0917@naver.com>
2022-11-07 20:30:16 +0900

    test: 추가 ValidationTest 모든 상황
    ++ 원볼 투스트라이크는 불가능하다.

--- src/test/java/baseball/refree/ResultHandlerTest.java
+++ src/test/java/baseball/refree/ResultHandlerTest.java
@@ -9,26 +9,47 @@ import org.junit.jupiter.api.Test;
 
 class ResultHandlerTest {
     @Test
-    void 볼스트라이크맞는지확인1() {
+    void 낫싱() {
         BallStrikeConstant ballStrikeResult = ResultHandler.createBallStrikeResult(List.of(1, 2, 3), List.of(4, 5, 6));
         assertThat(ballStrikeResult).isEqualTo(BallStrikeConstant.NOTHING);
     }
 
     @Test
-    void 볼스트라이크맞는지확인2() {
+    void 원볼() {
         BallStrikeConstant ballStrikeResult = ResultHandler.createBallStrikeResult(List.of(1, 2, 3), List.of(2, 5, 6));
         assertThat(ballStrikeResult).isEqualTo(BallStrikeConstant.ONE_BALL);
     }
 
     @Test
-    void 볼스트라이크맞는지확인3() {
-        BallStrikeConstant ballStrikeResult = ResultHandler.createBallStrikeResult(List.of(1, 2, 3), List.of(1, 3, 6));
+    void 원볼원스트라이크() {
+        BallStrikeConstant ballStrikeResult = ResultHandler.createBallStrikeResult(List.of(1, 2, 3), List.of(2, 5, 3));
         assertThat(ballStrikeResult).isEqualTo(BallStrikeConstant.ONE_BALL_ONE_STRIKE);
     }
 
+
     @Test
-    void 볼스트라이크맞는지확인4() {
+    void 투볼() {
+        BallStrikeConstant ballStrikeResult = ResultHandler.createBallStrikeResult(List.of(1, 2, 3), List.of(2, 5, 1));
+        assertThat(ballStrikeResult).isEqualTo(BallStrikeConstant.TWO_BALL);
+    }
+
+    @Test
+    void 투볼원스트라이크() {
+        BallStrikeConstant ballStrikeResult = ResultHandler.createBallStrikeResult(List.of(1, 2, 3), List.of(1, 3, 2));
+        assertThat(ballStrikeResult).isEqualTo(BallStrikeConstant.TWO_BALL_ONE_STRIKE);
+    }
+
+    @Test
+    void 쓰리볼() {
+        BallStrikeConstant ballStrikeResult = ResultHandler.createBallStrikeResult(List.of(1, 2, 3), List.of(2, 3, 1));
+        assertThat(ballStrikeResult).isEqualTo(BallStrikeConstant.THREE_BALL);
+    }
+
+    @Test
+    void 쓰리스트라이크() {
         BallStrikeConstant ballStrikeResult = ResultHandler.createBallStrikeResult(List.of(1, 2, 3), List.of(1, 2, 3));
         assertThat(ballStrikeResult).isEqualTo(BallStrikeConstant.THREE_STRIKE);
     }
+
+
 }
