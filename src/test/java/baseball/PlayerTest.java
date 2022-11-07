0a79bf3130732ea47095d113ac4823a01825d1d7
이경범 <tkdwk567@naver.com>
2022-11-07 20:01:47 +0900

    test :  Player class test
    1. 데이터가 숫자가 아닌 경우와 3자리가 아닌 경우 IllegalArgumentException 예외 발생
    2. 플레이어는 숫자 3개를 입력받아 데이터에 대한 유효성 검사와 리스트로 정제하여 저장한다.
    3. 플레이어는 숫자 리스트를 심판에게 준다. 플레이어와 심판이 가지고 있는 숫자 리스트는 같아야 한다.

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -6,41 +6,53 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerTest {
 
     @Test
-    @DisplayName("플레이어는 숫자 3개를 입력받아 리스트에 저장한다.")
-    void sayNumbers() {
+    @DisplayName("데이터가 숫자가 아닌 경우와 3자리가 아닌 경우 IllegalArgumentException 예외 발생")
+    void validation() {
         // given
         Player player = new Player();
+        String pushNumber = "a";
 
-        // when
-        List<Integer> numbers = player.sayNumbers(List.of(1, 2, 3));
-
-        // then
-        assertThat(numbers.size()).isEqualTo(3);
-        assertAll(() -> assertThat(numbers.get(0)).isEqualTo(1),
-                () -> assertThat(numbers.get(1)).isEqualTo(2),
-                () -> assertThat(numbers.get(2)).isEqualTo(3));
+        // when && then
+        assertThatThrownBy(() -> player.sayNumbers(pushNumber))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Number is Not Valid Exception!!");
     }
 
     @Test
-    @DisplayName("입력받은 숫자 3개를 심판에게 준다.")
-    void giveNumbersToReferee() {
+    @DisplayName("플레이어는 숫자 3개를 입력받아 데이터에 대한 유효성 검사와 리스트로 정제하여 저장한다.")
+    void sayNumbers() {
         // given
         Player player = new Player();
-        List<Integer> numbers = player.sayNumbers(List.of(1, 2, 3));
+        String pushNumber = "123";
 
-        Referee referee = new Referee();
+        // when
+        player.sayNumbers(pushNumber);
+
+        // then
+        assertThat(player.getNumbers()).isEqualTo(List.of(1, 2, 3));
+
+    }
+
+    @Test
+    @DisplayName("플레이어는 숫자 리스트를 심판에게 준다. 플레이어와 심판이 가지고 있는 숫자 리스트는 같아야 한다.")
+    void giveNumbersToReferee() {
+        // given
+        Referee referee = new Referee(new Rule());
+
+        Player player =  new Player();
+        player.sayNumbers("123");
 
         // when
         player.giveNumbersToReferee(referee);
 
         // then
-        List<Integer> refereeNumbers = referee.getNumbers();
-        assertThat(refereeNumbers.size()).isEqualTo(3);
-        assertThat(refereeNumbers).isEqualTo(List.of(1,2,3));
+        List<Integer> refereePlayerNumbers = referee.getPlayerNumbers();
+        List<Integer> playerNumbers = player.getNumbers();
+        assertThat(refereePlayerNumbers).isEqualTo(playerNumbers);
     }
 }
