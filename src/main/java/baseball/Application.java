4abd261340459a5e84fb9dc72c6623c4b2d103f8
kkkwp <chxrryda@gmail.com>
2022-11-08 01:31:05 +0900

    feat(inputNumber): 사용자에게 입력을 받는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -10,6 +11,21 @@ public class Application {
         // TODO: 프로그램 구현
 
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    private static List<Integer> inputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+
+        List<Integer> user = new ArrayList<>();
+        String input = Console.readLine();
+        for (String number : input.split("")) {
+            user.add(Integer.parseInt(number));
+        }
+        if (user.size() != 3)
+            throw new IllegalArgumentException("오류가 발생했습니다.");
+        if (user.stream().noneMatch(x -> x % 10 > 0))
+            throw new IllegalArgumentException("오류가 발생했습니다.");
+        return user;
     }
 
     private static List<Integer> makeAnswer() {
