df613c69fb7ba62d36f8874a12030569e7c7b328
mirasoy <nkskjjbb@gmail.com>
2022-11-07 20:44:56 +0900

    [Feat](Application) : 게임 진행 총괄 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    	boolean continueGame = true;
+    	GameManager gameManager = new GameManager();
+    	gameManager.open();
+    	
+    	while(continueGame) {
+    		gameManager.setGame();
+    		gameManager.startGame();
+    		gameManager.endGame();
+    		continueGame = gameManager.confirmRestart();
+    	}
+    	gameManager.terminateGame();
+    	
+    	
     }
+
+	
 }
