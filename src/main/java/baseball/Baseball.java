fd1925720f000353c9151db7c47ded8f20c8aa59
rosesua318 <rosesua@duksung.ac.kr>
2022-11-07 23:54:45 +0900

    feat: 사용자에게 숫자 입력 받고 입력값 검사하기 기능 추가

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -52,13 +52,44 @@ public class Baseball {
         return result;
     }
 
+    // 사용자가 입력한 숫자에 대해 검사하기
+    private String inputNumber() {
+        String input = readLine();
+
+        if (input.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        char check = input.charAt(0);
+        if (check == '0') {
+            throw new IllegalArgumentException();
+        }
+
+        char checkTwice = input.charAt(0);
+        for (int i = 1; i < input.length(); i++) {
+            if (checkTwice == input.charAt(i)) { // 2번째 자릿수와 3번째 자릿수가 같을 경우
+                throw new IllegalArgumentException();
+            }
+            if (input.charAt(i) == '0') {
+                throw new IllegalArgumentException();
+            }
+            if (check == input.charAt(i)) {
+                throw new IllegalArgumentException();
+            }
+            checkTwice = input.charAt(i);
+        }
+
+        return input;
+    }
+
+
 
     public void play() {
         System.out.println(Message.START);
         String result = "";
         do {
             System.out.print(Message.INPUT);
-
+            userNumber = inputNumber();
         } while (!result.equals(Message.CLEAR));
     }
 }
