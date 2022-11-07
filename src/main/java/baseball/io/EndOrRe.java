ab590b4dff8cc64bfb31a1dec4460fd126074de8
yback1223 <yoyob1223@gmail.com>
2022-11-07 22:38:26 +0900

    refactor: finish EndOrRe class

--- src/main/java/baseball/io/EndOrRe.java
+++ src/main/java/baseball/io/EndOrRe.java
@@ -13,17 +13,23 @@ public class EndOrRe {
         return stringToInt(inputString);
     }
 
-    // method for test
-    public static void printInputString(String inputString) {
+    /**
+     * 테스트를 위한 메소드
+     * 입력받은 문자열을 활용하여 클래스의 메소드들을 테스트하기 위한 메소드
+     *
+     * @param inputString 입력 받은 문자열이라고 가정한 변수
+     * @return 1이나 2의 정수값이 반환된다.
+     */
+    public Integer printInputString(String inputString) {
         validOneOrTwo(inputString);
-        System.out.println(stringToInt(inputString));
+        return stringToInt(inputString);
     }
 
-    private static Integer stringToInt(String inputString) {
+    private Integer stringToInt(String inputString) {
         return Integer.valueOf(inputString);
     }
 
-    private static void validOneOrTwo(String inputString) {
+    private void validOneOrTwo(String inputString) {
         if (!inputString.equals(ONE) && !inputString.equals(TWO)) {
             throw new IllegalArgumentException();
         }
