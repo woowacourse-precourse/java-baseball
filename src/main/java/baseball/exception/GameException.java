1b1d179f45f816b0455314b07c35f3f7d15455ce
broden-horizon <rlghd1698@naver.com>
2022-11-07 18:06:32 +0900

    refactor. Exception은 문제에 주어진 IllegalArgumentException를 상속받도록 수정

--- src/main/java/baseball/exception/GameException.java
+++ src/main/java/baseball/exception/GameException.java
@@ -1,9 +1,13 @@
 package baseball.exception;
 
-public class GameException extends RuntimeException{
+public class GameException extends IllegalArgumentException{
     private ExceptionCode exceptionCode;
 
     public GameException(ExceptionCode exceptionCode) {
         this.exceptionCode = exceptionCode;
+    }
+
+    public ExceptionCode getExceptionCode() {
+        return exceptionCode;
     }
 }
