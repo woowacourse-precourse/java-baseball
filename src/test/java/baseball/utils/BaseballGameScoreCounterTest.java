f37c42ecfc8633cea14f93abd562835b5e7f8dc3
whdcks420 <whdcks420@gmail.com>
2022-11-07 23:51:07 +0900

    test: 스코어 계산기 테스트

---
+++ src/test/java/baseball/utils/BaseballGameScoreCounterTest.java
@@ -0,0 +1,18 @@
+package baseball.utils;
+
+import baseball.model.Score;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseballGameScoreCounterTest {
+
+    @Test
+    void 숫자_계산기_테스트() {
+        Score score = BaseballGameScoreCounter.checkScore("423", "456");
+        assertAll(
+                () -> assertEquals(1, score.getStrike()),
+                () -> assertEquals(0, score.getBall())
+        );
+    }
+}
