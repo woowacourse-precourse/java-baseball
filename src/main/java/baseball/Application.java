218b4be848d4f0792b303ec961476e800aef04ce
cgk95 <kjkj5868@gmail.com>
2022-11-08 06:37:44 +0900

    feat : 요구 기능 모두 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,39 +13,33 @@ class BaseballGame {
     private static boolean inputCondition;
     int ball;
     int strike;
+
     public BaseballGame() {
         computer = new ArrayList<>();
         player = new ArrayList<>();
-        inputCondition=true;
-        ball=0;
-        strike=0;
-        newGame();
+        inputCondition = true;
+        ball = 0;
+        strike = 0;
     }
+
     private void newGame() {
         selectNumbersByComputer(computer);
+
         while (inputCondition) {
             System.out.print(getPleaseInput());
-            String input=Console.readLine();
-            selectNumberByPlayer(player,input);
+            String input = Console.readLine();
+            selectNumberByPlayer(player, input);
 
             getBallCounts(getComputerNumberList(), getPlayerNumberList());
-            inputCondition=checkGameCondition(ball,strike);
+            inputCondition = checkGameCondition(ball, strike);
+            ball = 0;
+            strike = 0;
+            player.clear();
         }
     }
 
-    private boolean checkGameCondition(int ball, int strike) {
-        if (strike==3) {
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
-            return false;
-        } else if (ball > 0) {
-            System.out.printf("%d볼 %d스트라이크", ball - strike, strike);
-        } else {
-            System.out.println("낫싱");
-        }
-        this.ball=0;
-        this.strike=0;
-        return true;
+    public void getNewGame() {
+        newGame();
     }
 
     private void selectNumbersByComputer(List<Integer> computer) {
@@ -60,16 +54,20 @@ class BaseballGame {
             integerList.add(randomNumber);
         }
     }
+
     public List<Integer> getComputerNumberList() {
         return this.computer;
     }
+
     public List<Integer> getPlayerNumberList() {
         return this.player;
     }
+
     public static String getPleaseInput() {
         return pleaseInput;
     }
-    private List<Integer> selectNumberByPlayer(List<Integer> player,String input) throws IllegalArgumentException {
+
+    private List<Integer> selectNumberByPlayer(List<Integer> player, String input) throws IllegalArgumentException {
         try {
             isValidPlayerInput(input);
         } catch (IllegalArgumentException e) {
@@ -77,58 +75,86 @@ class BaseballGame {
         }
         return player;
     }
-    private boolean isValidPlayerInput(String input){
+
+    private boolean isValidPlayerInput(String input) {
         try {
             Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("입력이 변환가능한 정수가 아님");
         }
-        if (input.length()!=3) {
+        if (input.length() != 3) {
             throw new IllegalArgumentException("입력이 규정길이를 초과");
-        } else{
+        } else {
             checkEqualityOfPlayerInput(input);
         }
         return true;
     }
+
     private void checkEqualityOfPlayerInput(String input) {
         for (int i = 0; i < input.length(); i++) {
-            addNumberIfDifferent(player,Integer.parseInt(String.valueOf(input.charAt(i))));
+            addNumberIfDifferent(player, Integer.parseInt(String.valueOf(input.charAt(i))));
         }
         if (player.size() < 3) {
             throw new IllegalArgumentException("서로 다른 임의의 수가 아님");
         }
     }
+
     private void getBallCounts(List<Integer> computerNumberList, List<Integer> playerNumberList) {
         for (Integer n : computerNumberList) {
             increaseBallCount(playerNumberList, n);
         }
-        for(int i=0;i<computerNumberList.size();i++){
+        for (int i = 0; i < computerNumberList.size(); i++) {
             increaseStrikeCount(computerNumberList, playerNumberList, i);
         }
     }
 
-    private void increaseStrikeCount(List<Integer> computerNumberList, List<Integer> playerNumberList, int i) {
+    private void increaseStrikeCount(List<Integer> computerNumberList, List<Integer> playerNumberList, Integer i) {
         if (computerNumberList.get(i).equals(playerNumberList.get(i))) {
-            strike+=1;
+            strike++;
         }
     }
 
     private void increaseBallCount(List<Integer> playerNumberList, Integer n) {
         if (playerNumberList.contains(n)) {
-            ball+=1;
+            ball++;
         }
+    }
+
+    private boolean checkGameCondition(int ball, int strike) {
+        if (strike == 3) {
+            System.out.println("3스트라이크");
+            System.out.print("3개의 숫자를 모두 맞히셨습니다! ");
+            System.out.println("게임 종료");
+            return false;
+        } else if (ball > 0) {
+            System.out.printf("%d볼 %d스트라이크", ball - strike, strike);
+        } else {
+            System.out.println("낫싱");
+        }
+        return true;
     }
 }
 
 public class Application {
     public static void main(String[] args) throws IllegalArgumentException {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while (true) {
+        boolean restartFlag = true;
+        while (restartFlag) {
             BaseballGame game = new BaseballGame();
+            game.getNewGame();
+
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String exitCode=Console.readLine();
+            String exitCode = Console.readLine();
+            isExitCodeValid(exitCode);
+            if (exitCode.equals("2")) {
+                restartFlag = false;
+            }
         }
     }
 
-
+    static void isExitCodeValid(String exitCode) {
+        if (!(exitCode.equals("1") || exitCode.equals("2"))) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
