c7060b73306e62b68daa7a379fdc5dfae55b46aa
yujaemin <woawoa0513@gmail.com>
2022-11-07 22:38:59 +0900

    feat: 스트라이크 여부 검사 함수 구현
    같은 자리에 같은 숫자를 입력했다면 스트라이크 처리

--- src/main/java/baseball/NonPlayerCharacter.java
+++ src/main/java/baseball/NonPlayerCharacter.java
@@ -1,6 +1,14 @@
 package baseball;
 
 public class NonPlayerCharacter {
+    private static final int NOTHING = -1;
+    private static final int BALL = 0;
+    private static final int STRIKE = 1;
+    private static final int DIGITS_OF_RANDOM_NUMBER = 3;
+
+    RandomNumber randomNumber = RandomNumber.getInstance();
+
+    static int[] numberOfBallAndStrike;
 
     public void printGameStartNotice() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -12,6 +20,12 @@ public class NonPlayerCharacter {
 
     public void printGameEndNotice() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-                            "게임 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                "게임 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+
+    public boolean isStrike(char input, int index) {
+        return randomNumber.toString().charAt(index) == input;
+    }
+
+
 }
