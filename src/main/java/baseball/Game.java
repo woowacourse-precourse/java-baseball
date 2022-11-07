9b2940047eb7747fc01fc8ea1fa354c34cd0abea
nayooniverse <kangna99@naver.com>
2022-11-08 08:54:58 +0900

    feat: 게임 기능 init, play, replay 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -10,4 +10,25 @@ public class Game {
         computer = new Computer();
         player = new Player();
     }
+
+    public void initGame() {
+        computer.randNumber();
+    }
+
+    public void playGame() throws IllegalArgumentException {
+        while(computer.strike != 3) {
+            System.out.print(GAME_GUESS);
+            computer.judge(player.inputGuess());
+        }
+        System.out.println(GAME_SUCCESS);
+    }
+
+    public void replayGame() throws IllegalArgumentException {
+        System.out.println(GAME_RETRY);
+        boolean replay = player.inputRetry();
+        if(replay) {
+            Controller controller = new Controller();
+            controller.run();
+        }
+    }
 }
