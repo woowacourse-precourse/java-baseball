41c8bd08a1f16b71463ac210a170407e320b506a
bnbnac <bnbnac@naver.com>
2022-11-08 00:54:17 +0900

    test: 컴퓨터 객체 힌트 반환 테스트 작성

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import java.awt.*;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -9,17 +10,17 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// class @Nest
+@SuppressWarnings("unchecked")
 class ComputerTest {
-
     List<String> getPrivateAccessToComputerAnswer(Computer computer) {
-        List<String> computerAnswer = new ArrayList<String>();
+        List<String> computerAnswer = new ArrayList<>();
         try {
             Field privateField = computer.getClass().getDeclaredField("computerAnswer");
             privateField.setAccessible(true);
+
             return (List<String>) privateField.get(computer);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
+        } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
         return computerAnswer;
@@ -54,15 +55,33 @@ class ComputerTest {
         Computer computer = new Computer();
         List<String> computerAnswer = getPrivateAccessToComputerAnswer(computer);
         Boolean distinct = true;
-        HashMap<String, String> digitsArray = new HashMap<>();
+        HashMap<String, String> digitsMap = new HashMap<>();
 
         for (String digit : computerAnswer) {
-            if (digitsArray.get(digit) != null) {
+            if (digitsMap.get(digit) != null) {
                 distinct = false;
                 break;
             }
-            digitsArray.put(digit, "checked");
+            digitsMap.put(digit, "checked");
         }
         assertEquals(true, distinct);
+    }
+
+    @Test
+    void giveHintTest() {
+        Computer computer = new Computer();
+        List<String> computerAnswer = getPrivateAccessToComputerAnswer(computer);
+
+        List<String> playerAnswer = new ArrayList<>();
+        List<String> hint = new ArrayList<>();
+
+        playerAnswer.add(computerAnswer.get(2));
+        playerAnswer.add(computerAnswer.get(1));
+        playerAnswer.add(computerAnswer.get(0));
+        hint.add("2");
+        hint.add("1");
+
+        List<String> expected = computer.giveHint(playerAnswer);
+        assertEquals(expected, hint);
     }
 }
