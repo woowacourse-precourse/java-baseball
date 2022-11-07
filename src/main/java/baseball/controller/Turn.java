fad20178c07916114895ad67838cb50c18d7302f
Woojin Lee <dhkep03@gmail.com>
2022-11-07 14:40:12 +0900

    feat(Turn): 사용자의 입력 숫자와 타겟을 비교

--- src/main/java/baseball/controller/Turn.java
+++ src/main/java/baseball/controller/Turn.java
@@ -1,6 +1,8 @@
 package baseball.controller;
 
 import baseball.type.ResultTurn;
+import baseball.view.InputView;
+import java.util.List;
 
 public class Turn {
     static ResultTurn resultTurn;
@@ -12,10 +14,17 @@ public class Turn {
     }
 
     static void playOneTurn() {
-
+        String userNumberOfString = InputView.userNumber();
+        List<Integer> userNumberOfList = convertNumberStringToList(userNumberOfString);
+        compareWithTarget(userNumberOfList);
     }
 
     static boolean checkCanRepeat() {
         return resultTurn != ResultTurn.Out;
+    }
+
+    static void compareWithTarget(List<Integer> numberOfList) {
+        countBalls(numberOfList);
+        countStrikes(numberOfList);
     }
 }
