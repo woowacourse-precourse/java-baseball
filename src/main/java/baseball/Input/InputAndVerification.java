8698733bdaf9ce9c1374b6fac3b26bafda8d0c44
jaehyeon <jhjh0022@naver.com>
2022-11-07 21:40:41 +0900

    style: 줄간격 맞추기

--- src/main/java/baseball/Input/InputAndVerification.java
+++ src/main/java/baseball/Input/InputAndVerification.java
@@ -1,22 +1,24 @@
 package baseball.Input;
 
+import static baseball.Input.InputNumberVerification.verifyNumber;
+import static baseball.Input.InputNumberVerification.verifyOneOrTwoButton;
+
 public class InputAndVerification {
     InputNumber inputNumber = new InputNumber();
-    InputNumberVerification inputNumberVerification = new InputNumberVerification();
 
-    public int receiveAndVerifyNumber(){
+    public int receiveAndVerifyNumber() {
         int receivedNumber = inputNumber.getInputNumber();
 
-        if(!inputNumberVerification.verifyNumber(receivedNumber)){
+        if (!verifyNumber(receivedNumber)) {
             throw new IllegalArgumentException("형식에 맞게 입력하세요");
         }
         return receivedNumber;
     }
 
-    public int receiveAfterGameEndOneOrTwo(){
+    public int receiveAfterGameEndOneOrTwo() {
         int receivedButtonNumber = inputNumber.getRestartAndEndNumber();
 
-        if(inputNumberVerification.verifyOneOrTwoButton(receivedButtonNumber)){
+        if (!verifyOneOrTwoButton(receivedButtonNumber)) {
             throw new IllegalArgumentException("1, 2 둘 중 하나만 골르세요");
         }
         return receivedButtonNumber;
