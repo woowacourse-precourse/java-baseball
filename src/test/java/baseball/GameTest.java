e68706157c35ccf9db4dd55101330d3192f6f91c
Bumsoo <rhqjatn2398@naver.com>
2022-11-07 18:23:45 +0900

    test(GameTest): 사용자의 숫자 입력을 검증하는 기능의 테스트케이스 작성

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,11 +10,29 @@ import org.junit.jupiter.api.Test;
 public class GameTest {
 
     @Test
-    void getNumInput() {
+    void getNumInputCase1() {
         List<Integer> nums = new ArrayList<>();
         Game game = new Game();
         game.getNumInput(nums, "413");
 
         assertThat(nums.size()).isEqualTo(3);
+    }
+
+    @Test
+    void getNumInputCase2() {
+        List<Integer> nums = new ArrayList<>();
+        Game game = new Game();
+
+        assertThatThrownBy(() -> game.getNumInput(nums, "112"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void getNumInputCase3() {
+        List<Integer> nums = new ArrayList<>();
+        Game game = new Game();
+
+        assertThatThrownBy(() -> game.getNumInput(nums, "120"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
