aa13587b090f48fb48d61279762f91b24ad8770d
kimdoha <kkhhjj3393@gmail.com>
2022-11-08 01:14:22 +0900

    feat(Game): 게임에 필요한 각 플레이어, 스코어, flag 변수 작성

---
+++ src/main/java/baseball/game/GameSetting.java
@@ -0,0 +1,12 @@
+//package baseball.game;
+//
+//import baseball.player.Computer;
+//import baseball.player.User;
+//import baseball.score.Score;
+//
+//public class GameSetting {
+//  public Computer computerPlayer;
+//  public User userPlayer;
+//  public Score score;
+//  public Boolean flag = true;
+//}
