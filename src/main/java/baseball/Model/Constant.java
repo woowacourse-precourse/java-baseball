c60b324b08f0e210be9498940d8ed520cc0828ef
Jihyeon-Park <qkrwlgus20020@gmail.com>
2022-11-08 06:31:34 +0900

    refactor: 상수를 따로 모은 클래스 생성

---
+++ src/main/java/baseball/Model/Constant.java
@@ -0,0 +1,7 @@
+package baseball.Model;
+
+public class Constant {
+    public static final int MAX_NUM_RANGE = 9;
+    public static final int MIN_NUM_RANGE = 1;
+    public static final int MAX_NUM_SIZE = 3;
+}
