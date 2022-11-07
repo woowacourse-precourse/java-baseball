26053e7f7ca596e4f3958ac816c4c922965eb388
Jaeyeop-Jung <jjy5349@naver.com>
2022-11-07 19:09:02 +0900

    feat: 게임 힌트를 사용자에게 출력하는 기능 추가

--- src/main/java/baseball/view/UserOutputView.java
+++ src/main/java/baseball/view/UserOutputView.java
@@ -5,9 +5,23 @@ import baseball.domain.BaseballGameResult;
 public class UserOutputView {
 
 	public static void printResult(BaseballGameResult result) {
+		if (result.getNumberOfBall() == 0 && result.getNumberOfStrike() == 0) {
+			System.out.println("낫싱");
+			return;
+		}
+
+		StringBuilder output = new StringBuilder();
+		if (result.getNumberOfBall() != 0) {
+			output.append(result.getNumberOfBall() + "볼 ");
+		}
+		if (result.getNumberOfStrike() != 0) {
+			output.append(result.getNumberOfStrike() + "스트라이크");
+		}
+		System.out.println(output);
 	}
 
 	public static void printFinishPhrase() {
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 	}
 
 }
