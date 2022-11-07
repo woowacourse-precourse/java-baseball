2e4cbe2e6c9d2d20d5c3f5449bfd8452d938b314
DongwookKim0823 <dwkim0823@naver.com>
2022-11-08 02:08:54 +0900

    refactor: 시작 시 "숫자 야구 게임을 시작합니다." 문구 출력하게 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -11,6 +11,7 @@ public class Game {
     private static final String EXIT = "2";
 
     public void restart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
             play();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
