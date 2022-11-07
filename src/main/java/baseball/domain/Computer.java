ec30d1b518ff5b938bd096e9eee6c1420d704e87
ffolabear <ffolabear@gmail.com>
2022-11-07 19:48:29 +0900

    refactor(Computer): 코드 리팩토링

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -9,38 +9,33 @@ public class Computer {
 
     final int START_NUMBER = 1;
     final int END_NUMBER = 9;
-    final int COMPUTER_NUMBER_SIZE = 3;
-    public List<String> computerAnswer;
+    final static int COMPUTER_NUMBER_SIZE = 3;
+    List<String> computerAnswer;
 
     public Computer() {
-        this.computerAnswer = generateComputerAnswer();
     }
 
-    public List<String> generateComputerAnswer() {
-        List<Integer> numberList = new ArrayList<>();
-        while (numberList.size() < COMPUTER_NUMBER_SIZE) {
-            int currentNumber = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
-            if (isUniqueNumber(numberList, currentNumber)) {
-                numberList.add(currentNumber);
-            }
-        }
-
-        return toStringList(numberList);
+    public List<String> getComputerAnswer() {
+        generateComputerAnswer();
+        return computerAnswer;
     }
 
-    private List<String> toStringList(List<Integer> generatedNumbers) {
-        List<String> parsedList = new ArrayList<>();
-        for (Integer i : generatedNumbers) {
-            parsedList.add(String.valueOf(i));
+    public void generateComputerAnswer() {
+        computerAnswer = new ArrayList<>();
+        while (computerAnswer.size() < COMPUTER_NUMBER_SIZE) {
+            addNumberToList(computerAnswer);
         }
-        return parsedList;
     }
 
-    private boolean isUniqueNumber(List<Integer> numberList, Integer currentNumber) {
-        if (numberList.contains(currentNumber)) {
-            return false;
+    private void addNumberToList(List<String> computerAnswer) {
+        String currentNumber = String.valueOf(Randoms.pickNumberInRange(START_NUMBER, END_NUMBER));
+        if (isUniqueNumber(computerAnswer, currentNumber)) {
+            computerAnswer.add(currentNumber);
         }
-        return true;
+    }
+
+    private boolean isUniqueNumber(List<String> computerAnswer, String currentNumber) {
+        return !computerAnswer.contains(currentNumber);
     }
 
 }
