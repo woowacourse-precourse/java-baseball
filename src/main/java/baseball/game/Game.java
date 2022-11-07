a852a0aa1a529e067f91ae873ad34149050ed38f
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-08 06:11:17 +0900

    feat(game/Game.java): 3개 숫자를 모두 맞추면 게임 종료 후 재시작 또는 종료를 입력받는 기능 구현

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -10,6 +10,7 @@ public class Game {
     private boolean exit = false;
     private boolean play_exit;
     private static final String START_STRING = "숫자 야구 게임을 시작합니다.";
+    private static final String EXIT_STRING = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void start() {
         System.out.println(START_STRING);
@@ -29,6 +30,19 @@ public class Game {
             Hint hint = new Hint(computer.getComputerNum(), player.getInputNum());
             hint.printResult();
             play_exit = hint.checkWin();
+        }
+    }
+
+    public boolean restartOrEnd() {
+        System.out.println(EXIT_STRING);
+        String input = Console.readLine();
+
+        if (input.equals("1")){
+            return false;
+        } else if (input.equals("2")) {
+            return true;
+        } else {
+            throw new IllegalArgumentException("1,2 중 하나만 입력하세요.");
         }
     }
 }
