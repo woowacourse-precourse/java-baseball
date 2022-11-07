ea43679bf77afb3c26a80ed03e258ffefceaabd9
ParkJunYeong1205 <junpreme1205@gmail.com>
2022-11-07 20:44:40 +0900

    feat: 사용자가 맞혀야 할 1부터 9까지 서로 다른 수로 이루어진 3자리의 수 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+    static final int RANDOM_NUMBER_LENGTH = 3;
+    static final int RANDOM_NUMBER_FIRST_RANGE = 1;
+    static final int RANDOM_NUMBER_LAST_RANGE = 9;
+
     static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
     static public void startGame() {
+        String randomNumber = makeRandomNumber();
+    }
 
+    static public String makeRandomNumber() {
+        String randomNumber = "";
+
+        while (randomNumber.length() < RANDOM_NUMBER_LENGTH) {
+            int randomDigit = Randoms.pickNumberInRange(RANDOM_NUMBER_FIRST_RANGE, RANDOM_NUMBER_LAST_RANGE);
+            String randomWord = String.valueOf(randomDigit);
+            if (!randomNumber.contains(randomWord)) {
+                randomNumber += randomWord;
+            }
+        }
+
+        return randomNumber;
     }
 
     public static void main(String[] args) {
