015d29fe082296582c5300ad5b4230cb08eac03b
mgOh <issac_oh@naver.com>
2022-11-08 04:08:03 +0900

    fix: 게임 시작후 랜덤으로 생성된 정수를 List로 저장하도록 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -9,31 +9,15 @@ import java.util.logging.Logger;
 
 public class Game {
     private final static Logger LOG = Logger.getGlobal();
-    int num1, num2, num3;
+    List<Integer> numberList = new ArrayList<>();
     Randoms Ran;
 
     public Game() {
-        List<Integer> numberList = new ArrayList<>();
         while (numberList.size() < 3){
             int term = Ran.pickNumberInRange(1, 9);
             if (!numberList.contains(term)){
                 numberList.add(term);
             }
         }
-        this.num1 = numberList.get(0);
-        this.num2 = numberList.get(1);
-        this.num3 = numberList.get(2);
-    }
-
-    public int getNum1() {
-        return num1;
-    }
-
-    public int getNum2() {
-        return num2;
-    }
-
-    public int getNum3() {
-        return num3;
     }
 }
