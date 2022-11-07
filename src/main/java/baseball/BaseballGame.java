3fc634d9c2e09ae69b500441f69f417dfdbf0044
ji-mango <susie000301@naver.com>
2022-11-07 23:52:26 +0900

    feat: 랜덤 숫자 생성

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,32 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+public class BaseballGame {
+    private static final int START_INCLUSIVE = 1;
+    private static final int END_INCLUSIVE = 9;
+
+    List<Integer> randomList = new ArrayList<>();
+
+    public BaseballGame() {
+        createNumber();
+    }
+
+    private void createNumber() {
+        while (randomList.size() < 3) {
+            int randomNumber = pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+            addRandomNumber(randomNumber);
+        }
+        System.out.println(randomList);
+    }
+
+    private void addRandomNumber(int randomNumber) {
+        if (!randomList.contains(randomNumber)) {
+            randomList.add(randomNumber);
+        }
+    }
+
+}
