73557d1aa51f33f72f68de4ff85b94f249188824
yoonjuho <dbswn990@naver.com>
2022-11-08 06:35:45 +0900

    refactor($Computer) : 변수,메서드명 변경

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -9,60 +9,63 @@ public class Computer {
     private int ballCount = 0;
     private Number numFactory;
     private List<Integer> computerNumber;
+
     public Computer() {
         numFactory = new ComputerNum();
         computerNumber = numFactory.createNumber();
         numFactory.verifyNumber(computerNumber);
     }
 
-    public void calculateCount(int i, List<Integer> playerNumber) {
-        if (computerNumber.contains(playerNumber.get(i))) {
-            if (computerNumber.indexOf(playerNumber.get(i)) == i)
-                strikeCount++;
-            else
-                ballCount++;
-        }
-    }
-
     public String getResult(List<Integer> playerNumber) {
         resetCount();
         for (int i = 0; i < 3; i++) {
-            calculateCount(i, playerNumber);
+            count(i, playerNumber);
         }
-
         if (strikeCount == 3) {
             System.out.println(GameComment.THREE_STRIKE);
             System.out.println(GameComment.GAME_END_MSG);
-            return closeGame();
+            return terminateGame();
         } else if (ballCount < 1 && strikeCount < 1) {
             System.out.println(GameComment.NOTHING);
-        }else if (strikeCount < 1){
+        } else if (strikeCount < 1) {
             System.out.println(ballCount + GameComment.BALL);
-        }else if (ballCount < 1) {
+        } else if (ballCount < 1) {
             System.out.println(strikeCount + GameComment.STRIKE);
-        }else {
+        } else {
             System.out.println(ballCount + GameComment.BALL + " " + strikeCount + GameComment.STRIKE);
         }
         return GameComment.NOT_GAME_END;
     }
 
-    public String closeGame() {
+    private void count(int i, List<Integer> playerNumber) {
+        if (computerNumber.contains(playerNumber.get(i))) {
+            if (computerNumber.indexOf(playerNumber.get(i)) == i) {
+                strikeCount++;
+            } else {
+                ballCount++;
+            }
+        }
+    }
+
+
+    private String terminateGame() {
         System.out.print(GameComment.RESTART_MSG);
         String input = Console.readLine();
         return getStatusByInput(input);
     }
 
-    public String getStatusByInput(String input) {
+    private String getStatusByInput(String input) {
         if (input.equals("1")) {
             return GameComment.RESTART;
-        } else if (input.equals("2"))
+        } else if (input.equals("2")) {
             return GameComment.TERMINATE;
-        else
+        } else {
             throw new IllegalArgumentException();
+        }
     }
 
 
-    public void resetCount() {
+    private void resetCount() {
         strikeCount = 0;
         ballCount = 0;
     }
