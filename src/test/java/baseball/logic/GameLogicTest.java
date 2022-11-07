ac7a46dfabbbd42485083515e0f3aa827993404b
Jzakka <mouse4786@gmail.com>
2022-11-07 17:07:15 +0900

    test: Result에 의존적인 코드 수정

--- src/test/java/baseball/logic/GameLogicTest.java
+++ src/test/java/baseball/logic/GameLogicTest.java
@@ -1,12 +1,10 @@
 package baseball.logic;
 
 import baseball.domain.Result;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class GameLogicTest {
 
@@ -19,26 +17,25 @@ class GameLogicTest {
 
     @Test
     void 결과_검증() {
-        Result result = new Result(-1, -1);
-        gameLogic.process("369", result);
+        gameLogic.process("369");
 
-        assertResult(result, 3, 0);
+        assertResult( 3, 0);
 
-        gameLogic.process("396", result);
+        gameLogic.process("396");
 
-        assertResult(result, 1, 2);
+        assertResult( 1, 2);
 
-        gameLogic.process("123", result);
+        gameLogic.process("123");
 
-        assertResult(result, 0, 1);
+        assertResult( 0, 1);
 
-        gameLogic.process("248", result);
+        gameLogic.process("248");
 
-        assertResult(result, 0, 0);
+        assertResult( 0, 0);
     }
 
-    private void assertResult(Result result, Integer strike, Integer ball) {
-        assertThat(result.getStrike()).isEqualTo(strike);
-        assertThat(result.getBall()).isEqualTo(ball);
+    private void assertResult( Integer strike, Integer ball) {
+        assertThat(Result.getStrike()).isEqualTo(strike);
+        assertThat(Result.getBall()).isEqualTo(ball);
     }
 }
