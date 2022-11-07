334e3cf64d205cbd894dcd039c8bf74620bdac22
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-11-07 12:43:50 +0900

    feat(Play): define non-instantiable class named Play

---
+++ src/main/java/baseball/Play.java
@@ -0,0 +1,10 @@
+package baseball;
+
+class Play {
+    /**
+     * Enforce class non-instantiability with private constructor.
+     */
+    private Play() {
+        throw new AssertionError();
+    }
+}
