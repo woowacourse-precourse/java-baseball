53c90a952beb284bc66c5d93c606005441e12b58
seung-hun2 <jungsh2328@naver.com>
2022-11-07 18:59:32 +0900

    #inputNumber - 사용자가 3자리의 수를 입력할 수 있게 하는 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,6 +9,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         int answer = answerNum();
+        int input = inputNumber();
 
     }
     // 서로 다른 임의의 수 3개 선택
@@ -30,5 +31,22 @@ public class Application {
         }
         int answer = Integer.parseInt(str);
         return answer;
+    }
+    public static int inputNumber() throws NullPointerException {
+        System.out.print("숫자를 입력해주세요 : ");
+        int input = 0;
+
+        try{
+            input = Integer.parseInt(Console.readLine());
+            String str = String.valueOf(input);
+            if(input<100 || input>999){
+                throw new IllegalArgumentException();
+            }
+        }
+        catch (NullPointerException e){
+
+        }
+        return input;
+
     }
 }
