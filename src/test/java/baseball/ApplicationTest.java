149678055ea6d8759c0a349ff653189869398f85
GwansuShin <happysun7080@naver.com>
2022-11-07 16:56:13 +0900

    test(Application): 메시지 출력 테스트
    게임 진행에 따른 메시지 출력 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,6 +21,21 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 메시지_출력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "724", "2");
+                    assertThat(output()).contains(
+                            "숫자 야구 게임을 시작합니다.",
+                            "숫자를 입력해주세요 : ",
+                            "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+                            "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                },
+                7, 2, 4
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
