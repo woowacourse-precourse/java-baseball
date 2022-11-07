92a2cc4414c8dab1ee0089d128229813de4d1f08
abbd2 <ajkety00@gmail.com>
2022-11-07 14:02:05 +0900

    refactor(RandomNumber): RandomNumber 리펙토링 진행

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -7,11 +7,18 @@ import java.util.List;
 
 public class RandomNumber {
 
-    public List<Integer> createRandomNumber(){
+    public static List<Integer> gameRandomNumber(){
+        List<Integer> randomNumberList = createRandomNumber(); // 난수 생성
+        System.out.println("난수:" + randomNumberList + ":");
+        return randomNumberList;
+    }
+
+    public static List<Integer> createRandomNumber(){
 
         List<Integer> computer = new ArrayList<>();
+        int randomNumber = 0;
         while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
