7a2b5271ae0aba240744f158f4f9bc5955247cb2
nswon <swnam0729@gmail.com>
2022-11-07 22:12:50 +0900

    feat: Computer 객체 구현

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+import java.util.Set;
+
+public class Computer {
+    private Set<Integer> randomNumbers;
+
+    public Set<Integer> getRandomNumbers() {
+        return this.randomNumbers;
+    }
+}
