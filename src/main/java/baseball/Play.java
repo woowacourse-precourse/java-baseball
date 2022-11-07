fcf52b551ab2f0109cc7ab6e1a18a4847012a385
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-11-07 12:48:08 +0900

    feat(Play): define getRandomNumber method
    getRandomNumber returns and ArrayList of COUNT random and unique nonzero digits.

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -1,10 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 class Play {
+    private static final int COUNT = 3;
     /**
      * Enforce class non-instantiability with private constructor.
      */
     private Play() {
         throw new AssertionError();
+    }
+
+    static List<Integer> getRandomNumber() {
+        List<Integer> randomNumber = new ArrayList<>();
+        while (randomNumber.size() < COUNT) {
+            int digit = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumber.contains(digit)) {
+                randomNumber.add(digit);
+            }
+        }
+        return randomNumber;
     }
 }
