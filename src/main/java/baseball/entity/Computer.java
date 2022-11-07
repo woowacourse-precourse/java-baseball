7dd8f002e3851b3ff59a40e9b961cf5dcb85f6d6
민병욱 <woook27@naver.com>
2022-11-08 03:33:41 +0900

    feat (Computer): Randoms API를 이용해 랜덤한 숫자를 생성하고 저장하는 클래스 구현

---
+++ src/main/java/baseball/entity/Computer.java
@@ -0,0 +1,24 @@
+package baseball.entity;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+
+public class Computer {
+    private final int numberLength = 3;
+    String answer;
+
+    public Computer() {
+        answer = "";
+    }
+
+    public void setAnswer() {
+        HashSet<Integer> set = new HashSet<>();
+        while (set.size() != numberLength) {
+            set.add(Randoms.pickNumberInRange(1, 9));
+        }
+        for (int i : set) {
+            answer += i;
+        }
+    }
+}
