206425190b3f67a131e5c71fb80547c58906518d
박승민 <tmddudals369@naver.com>
2022-11-07 21:50:03 +0900

    test: 사용자의 값 입력, 게임 재시작 옵션 입력 테스트 케이스 추가

--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -9,7 +9,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 
@@ -30,115 +29,107 @@ public class BaseballGameTest extends NsTest {
         );
     }
 
-    @Nested
-    class 상대방의_수_생성 {
-        @Test
-        void 상대방의_수_범위() {
-            Baseball baseball = new Baseball();
-            List<Integer> computerNumbers = baseball.createComputerNumbers();
-            for (Integer computerNumber : computerNumbers) {
-                assertThat(computerNumber)
-                        .isGreaterThanOrEqualTo(1)
-                        .isLessThanOrEqualTo(9);
-            }
+    @Test
+    void 상대방의_수_범위() {
+        Baseball baseball = new Baseball();
+        List<Integer> computerNumbers = baseball.createComputerNumbers();
+        for (Integer computerNumber : computerNumbers) {
+            assertThat(computerNumber)
+                    .isGreaterThanOrEqualTo(1)
+                    .isLessThanOrEqualTo(9);
         }
-
-        @Test
-        void 상대방의_서로_다른_임의의_수() {
-            Baseball baseball = new Baseball();
-            List<Integer> computerNumbers = baseball.createComputerNumbers();
-            ;
-            Integer firstNum = computerNumbers.get(0);
-            Integer secondNum = computerNumbers.get(1);
-            Integer thirdNum = computerNumbers.get(2);
-            assertThat(firstNum).isNotEqualTo(secondNum);
-            assertThat(firstNum).isNotEqualTo(thirdNum);
-            assertThat(secondNum).isNotEqualTo(thirdNum);
-        }
-
     }
 
-    @Nested
-    class 힌트_출력 {
-        @Test
-        @DisplayName("1볼 1스트라이크")
-        void case1() {
-            Baseball baseball = new Baseball();
-            List<Integer> computerNumbers = List.of(7, 1, 3);
-            List<Integer> userNumbers = List.of(1, 2, 3);
-            String hint = baseball.getHint(computerNumbers, userNumbers);
-            assertThat(hint).isEqualTo("1볼 1스트라이크");
-        }
+    @Test
+    void 상대방의_서로_다른_임의의_수() {
+        Baseball baseball = new Baseball();
+        List<Integer> computerNumbers = baseball.createComputerNumbers();
+        Integer firstNum = computerNumbers.get(0);
+        Integer secondNum = computerNumbers.get(1);
+        Integer thirdNum = computerNumbers.get(2);
+        assertThat(firstNum).isNotEqualTo(secondNum);
+        assertThat(firstNum).isNotEqualTo(thirdNum);
+        assertThat(secondNum).isNotEqualTo(thirdNum);
+    }
 
-        @Test
-        @DisplayName("1볼")
-        void case2() {
-            Baseball baseball = new Baseball();
-            List<Integer> computerNumbers = List.of(7, 1, 3);
-            List<Integer> userNumbers = List.of(1, 4, 5);
-            String hint = baseball.getHint(computerNumbers, userNumbers);
-            assertThat(hint).isEqualTo("1볼");
-        }
+    @Test
+    @DisplayName("1볼 1스트라이크")
+    void case1() {
+        Baseball baseball = new Baseball();
+        List<Integer> computerNumbers = List.of(7, 1, 3);
+        List<Integer> userNumbers = List.of(1, 2, 3);
+        String hint = baseball.getHint(computerNumbers, userNumbers);
+        assertThat(hint).isEqualTo("1볼 1스트라이크");
+    }
 
-        @Test
-        @DisplayName("2볼")
-        void case3() {
-            Baseball baseball = new Baseball();
-            List<Integer> computerNumbers = List.of(7, 1, 3);
-            List<Integer> userNumbers = List.of(6, 7, 1);
-            String hint = baseball.getHint(computerNumbers, userNumbers);
-            assertThat(hint).isEqualTo("2볼");
-        }
+    @Test
+    @DisplayName("1볼")
+    void oneBall() {
+        Baseball baseball = new Baseball();
+        List<Integer> computerNumbers = List.of(7, 1, 3);
+        List<Integer> userNumbers = List.of(1, 4, 5);
+        String hint = baseball.getHint(computerNumbers, userNumbers);
+        assertThat(hint).isEqualTo("1볼");
+    }
 
-        @Test
-        @DisplayName("1스트라이크")
-        void case4() {
-            Baseball baseball = new Baseball();
-            List<Integer> computerNumbers = List.of(7, 1, 3);
-            List<Integer> userNumbers = List.of(2, 1, 6);
-            String hint = baseball.getHint(computerNumbers, userNumbers);
-            assertThat(hint).isEqualTo("1스트라이크");
-        }
+    @Test
+    @DisplayName("2볼")
+    void twoBall() {
+        Baseball baseball = new Baseball();
+        List<Integer> computerNumbers = List.of(7, 1, 3);
+        List<Integer> userNumbers = List.of(6, 7, 1);
+        String hint = baseball.getHint(computerNumbers, userNumbers);
+        assertThat(hint).isEqualTo("2볼");
+    }
 
-        @Test
-        @DisplayName("3스트라이크")
-        void case5() {
-            Baseball baseball = new Baseball();
-            List<Integer> computerNumbers = List.of(7, 1, 3);
-            List<Integer> userNumbers = List.of(7, 1, 3);
-            String hint = baseball.getHint(computerNumbers, userNumbers);
-            assertThat(hint).isEqualTo("3스트라이크");
-        }
+    @Test
+    @DisplayName("1스트라이크")
+    void oneStrike() {
+        Baseball baseball = new Baseball();
+        List<Integer> computerNumbers = List.of(7, 1, 3);
+        List<Integer> userNumbers = List.of(2, 1, 6);
+        String hint = baseball.getHint(computerNumbers, userNumbers);
+        assertThat(hint).isEqualTo("1스트라이크");
+    }
 
-        @Test
-        @DisplayName("낫싱")
-        void case6() {
-            Baseball baseball = new Baseball();
-            List<Integer> computerNumbers = List.of(7, 1, 3);
-            List<Integer> userNumbers = List.of(2, 4, 5);
-            String hint = baseball.getHint(computerNumbers, userNumbers);
-            assertThat(hint).isEqualTo("낫싱");
-        }
+    @Test
+    @DisplayName("3스트라이크")
+    void threeStrike() {
+        Baseball baseball = new Baseball();
+        List<Integer> computerNumbers = List.of(7, 1, 3);
+        List<Integer> userNumbers = List.of(7, 1, 3);
+        String hint = baseball.getHint(computerNumbers, userNumbers);
+        assertThat(hint).isEqualTo("3스트라이크");
+    }
 
-        @Test
-        @DisplayName("2볼 1스트라이크")
-        void case7() {
-            Baseball baseball = new Baseball();
-            List<Integer> computerNumbers = List.of(7, 1, 3);
-            List<Integer> userNumbers = List.of(7, 3, 1);
-            String hint = baseball.getHint(computerNumbers, userNumbers);
-            assertThat(hint).isEqualTo("2볼 1스트라이크");
-        }
+    @Test
+    @DisplayName("낫싱")
+    void nothing() {
+        Baseball baseball = new Baseball();
+        List<Integer> computerNumbers = List.of(7, 1, 3);
+        List<Integer> userNumbers = List.of(2, 4, 5);
+        String hint = baseball.getHint(computerNumbers, userNumbers);
+        assertThat(hint).isEqualTo("낫싱");
+    }
 
-        @Test
-        @DisplayName("2스트라이크")
-        void case8() {
-            Baseball baseball = new Baseball();
-            List<Integer> computerNumbers = List.of(7, 1, 3);
-            List<Integer> userNumbers = List.of(7, 1, 5);
-            String hint = baseball.getHint(computerNumbers, userNumbers);
-            assertThat(hint).isEqualTo("2스트라이크");
-        }
+    @Test
+    @DisplayName("2볼 1스트라이크")
+    void twoBallAndStrike() {
+        Baseball baseball = new Baseball();
+        List<Integer> computerNumbers = List.of(7, 1, 3);
+        List<Integer> userNumbers = List.of(7, 3, 1);
+        String hint = baseball.getHint(computerNumbers, userNumbers);
+        assertThat(hint).isEqualTo("2볼 1스트라이크");
+    }
+
+    @Test
+    @DisplayName("2스트라이크")
+    void twoStrike() {
+        Baseball baseball = new Baseball();
+        List<Integer> computerNumbers = List.of(7, 1, 3);
+        List<Integer> userNumbers = List.of(7, 1, 5);
+        String hint = baseball.getHint(computerNumbers, userNumbers);
+        assertThat(hint).isEqualTo("2스트라이크");
     }
 
     @Test
@@ -153,173 +144,193 @@ public class BaseballGameTest extends NsTest {
         );
     }
 
-    @Nested
-    class 예외_처리 {
-        String inputUserValue;
-        String inputGameOption;
+    @Test
+    @DisplayName("1보다 작은 수가 포함")
+    void inZeroException() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("012"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case1() {
-            inputUserValue = "012";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputUserValue))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("1자리의 수 입력")
+    void OneLengthNumber() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case2() {
-            inputUserValue = "1";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputUserValue))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("2자리의 수 입력")
+    void twoLengthNumber() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case3() {
-            inputUserValue = "12";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputUserValue))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("4자리의 수 입력")
+    void fourLengthNumber() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case4() {
-            inputUserValue = "1234";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputUserValue))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("1보다 작은 중복된 수")
+    void lessThanOneAndDuplicationNumber() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("000"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case5() {
-            inputUserValue = "000";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputUserValue))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("중복된 수1")
+    void DuplicationNumber1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("112"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case6() {
-            inputUserValue = "112";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputUserValue))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("중복된 수2")
+    void DuplicationNumber2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("121"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case7() {
-            inputUserValue = "121";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputUserValue))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("중복된 수3")
+    void DuplicationNumber3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("211"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case8() {
-            inputUserValue = "211";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputUserValue))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("중복된 수4")
+    void DuplicationNumber4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("111"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case9() {
-            inputUserValue = "111";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputUserValue))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("소수 입력")
+    void InputFloatNumber() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1.11"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case10() {
-            inputUserValue = "1.11";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputUserValue))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("0 입력")
+    void inputZero() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("0"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case11() {
-            inputGameOption = "0";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputGameOption))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("음수 입력")
+    void InputNegativeNumber() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("-1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case12() {
-            inputGameOption = "-1";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputGameOption))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("한글 입력")
+    void inputKorean() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("ㅂ"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case13() {
-            inputGameOption = "ㅂ";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputGameOption))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("특수문자 입력")
+    void inputEscape() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("@"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case14() {
-            inputGameOption = "@";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputGameOption))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("공백 입력")
+    void inputSpace() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" "))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
-        @Test
-        void case15() {
-            inputGameOption = "02";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputGameOption))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("게임 옵션 범위 밖 숫자 입력1")
+    void OutRangeGameOption() {
+        assertThatThrownBy(() ->
+                assertRandomNumberInRangeTest(() -> run("123", "3"),
+                        1, 2, 3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
-        @Test
-        void case16() {
-            inputUserValue = " ";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputUserValue))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("게임 옵션 범위 밖 숫자 입력2")
+    void OutRangeGameOption2() {
+        assertThatThrownBy(() ->
+                assertRandomNumberInRangeTest(() -> run("123", "-1"),
+                        1, 2, 3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
-        @Test
-        void case17() {
-            inputGameOption = " ";
-            assertSimpleTest(() ->
-                    assertThatThrownBy(() -> runException(inputGameOption))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
+    @Test
+    @DisplayName("게임 옵션 범위 밖 숫자 입력3")
+    void OutRangeGameOption3() {
+        assertThatThrownBy(() ->
+                assertRandomNumberInRangeTest(() -> run("123", "10"),
+                        1, 2, 3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
-        @Test
-        void case18() {
-            assertThatThrownBy(() ->
-                    assertRandomNumberInRangeTest(() -> {
-                                run("123", "3");
-                            },
-                            1, 2, 3))
-                    .isInstanceOf(IllegalArgumentException.class);
-        }
+    @Test
+    @DisplayName("게임 옵션 범위 영어 입력")
+    void GameOptionInputEnglish() {
+        assertThatThrownBy(() ->
+                assertRandomNumberInRangeTest(() -> run("123", "q"),
+                        1, 2, 3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("게임 옵션 범위 한글 입력")
+    void GameOptionInputKorean() {
+        assertThatThrownBy(() ->
+                assertRandomNumberInRangeTest(() -> run("123", "ㅂ"),
+                        1, 2, 3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("게임 옵션 특수문자 입력")
+    void GameOptionInputEscape() {
+        assertThatThrownBy(() ->
+                assertRandomNumberInRangeTest(() -> run("123", "#"),
+                        1, 2, 3))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Override
