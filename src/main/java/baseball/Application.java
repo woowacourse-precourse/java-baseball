0f42de1b907b4773fb9434e47c785bc4f15f1f34
GeonTae Park <rssr96@naver.com>
2022-11-07 17:30:51 +0900

    [feat]: 게임 플레이어가 3자리 수 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,14 +9,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
+    public static int threeStrike = 0;
+
     public static void main(String[] args) {
         List<Integer> computer = new ArrayList<>();
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (computer.size() < 3) {
             int randomNumber = pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
+        threeStrike = 0;
+        while (threeStrike == 0) {
+            System.out.print("숫자를 입력해 주세요 : ");
+            String user = Console.readLine();
+        }
     }
 }
+
+
 
