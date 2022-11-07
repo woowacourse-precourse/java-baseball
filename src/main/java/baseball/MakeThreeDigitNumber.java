b52b6348d907bd77d115e5eb9b07e2d91030c639
nahyeon cho <whskgus12@nate.com>
2022-11-07 16:27:27 +0900

    feat : 플레이어는 임의의 수를 입력한다
    플레이어가 수를 입력하는 함수입니다. 요구사항에 따라 사용자의 입력 값을 받을 때camp.nextstep.edu.missionutils.Console의 readLine 함수를 사용하였습니다.

--- src/main/java/baseball/MakeThreeDigitNumber.java
+++ src/main/java/baseball/MakeThreeDigitNumber.java
@@ -1,8 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class MakeThreeDigitNumber {
 
@@ -19,6 +21,13 @@ public class MakeThreeDigitNumber {
         }
 
         return randomSelectedNumbers;
+    }
+
+    public ArrayList<String> userSelectedNumber() {
+        System.out.print("숫자를 입력하세요 : ");
+        String inputNumbers = Console.readLine();
+        String[] inputList = inputNumbers.split("");
+        return new ArrayList<>(Arrays.asList(inputList));
     }
 
 }
