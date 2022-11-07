69e8c3d5660b9688fe861e343155ba41179d8754
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-11-07 21:33:27 +0900

    Feat : 예외 상황) 사용자가 입력을 긴 시간 동안 하지 않은 경우 Timer라이브러리를 통한 예외처리

--- src/main/java/baseball/BaseballGameManager.java
+++ src/main/java/baseball/BaseballGameManager.java
@@ -4,9 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BaseballGameManager {
     private static final String BASEBALL_GAME_GET_USER_INPUT_MESSAGE = "숫자를 입력하세요.";
+    private static final String BASEBALL_GAME_FORCE_EXIT_MESSAGE = "※ 30초 이상 입력이 없어 게임을 자동 종료합니다. ※";
+    private static final String BASEBALL_GAME_ASK_FOR_DIVIDED_NUMBER_MESSAGE = "※ 서로 다른 숫자 %d개를 입력해주세요. ※\n";
 
     private List<Integer> baseballNumberList;
     private ElementListMaker<Integer> elementListMaker;
@@ -38,8 +42,18 @@ public class BaseballGameManager {
     }
 
     private String getValidInput() {
-        String userInput = Console.readLine();;
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println(BASEBALL_GAME_FORCE_EXIT_MESSAGE);
+                System.exit(0);
+            }
+        };
 
+        timer.schedule(task, 30 * 1000);
+        String userInput = Console.readLine();
+        timer.cancel();
         try {
             // userInput이 Integer로 타입 변환이 가능한지 (숫자가 아닌 값 입력시 예외처리)
             Integer.parseInt(userInput);
@@ -55,7 +69,7 @@ public class BaseballGameManager {
             }
 
         } catch (NumberFormatException numberFormatException) {
-            System.out.printf("※서로 다른 숫자 %d개를 입력해주세요.※\n", numberCount);
+            System.out.printf(BASEBALL_GAME_ASK_FOR_DIVIDED_NUMBER_MESSAGE, numberCount);
             getValidInput();
         }
 
--- src/main/java/baseball/BaseballGameRunner.java
+++ src/main/java/baseball/BaseballGameRunner.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class BaseballGameRunner {
     private static final int NUMBER_LIST_LENGTH = 3;
@@ -12,6 +14,7 @@ public class BaseballGameRunner {
     private static final String BASEBALL_GAME_STARTING_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String BASEBALL_GAME_RESTART_CHECK_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String BASEBALL_GAME_EXIT_MESSAGE = "게임 종료";
+    private static final String BASEBALL_GAME_FORCE_EXIT_MESSAGE = "※ 30초 이상 입력이 없어 게임을 자동 종료합니다. ※";
 
 
     private ElementListMaker<Integer> numberElementListMaker;
@@ -47,7 +50,17 @@ public class BaseballGameRunner {
         System.out.println(BASEBALL_GAME_RESTART_CHECK_MESSAGE);
 
         try {
+            Timer timer = new Timer();
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    System.out.println(BASEBALL_GAME_FORCE_EXIT_MESSAGE);
+                    System.exit(0);
+                }
+            };
+            timer.schedule(task, 30 * 1000);
             int restartCheck = Integer.parseInt(Console.readLine());
+            timer.cancel();
 
             if (restartCheck == BASEBALL_GAME_RESTART) {
                 return true;
