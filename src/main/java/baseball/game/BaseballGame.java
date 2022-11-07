77e0b1d23ff86be94b78bfc4424116f719831fd0
chkim <cndghks0213@gmail.com>
2022-11-07 18:16:01 +0900

    feat(game): 숫자 야구 게임 실행을 담당하는 BaseballGame 구현

---
+++ src/main/java/baseball/game/BaseballGame.java
@@ -0,0 +1,56 @@
+package baseball.game;
+
+import baseball.numbers.BallTypes;
+import baseball.numbers.Numbers;
+import baseball.numbers.NumbersGenerationStrategy;
+import baseball.ui.InputView;
+import baseball.ui.Result;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static baseball.ui.OutputView.*;
+
+public class BaseballGame {
+    private static final int GAME_OVER_STRIKE_COUNT = 3;
+
+    private final NumbersGenerationStrategy strategy;
+
+    public BaseballGame(NumbersGenerationStrategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void start() {
+        printGameStartMessage();
+        Numbers answer = new Numbers(strategy);
+        Map<BallTypes, Integer> result = new HashMap<>();
+
+        game(answer, result);
+        decideRestartOrEnd();
+    }
+
+    private void game(Numbers answer, Map<BallTypes, Integer> result) {
+        while (inProgress(result)) {
+            Numbers numbers = new Numbers(InputView.numbers());
+            result = answer.compare(numbers);
+            printCompareResult(new Result(result));
+        }
+    }
+
+    private boolean inProgress(Map<BallTypes, Integer> result) {
+        int strike = result.getOrDefault(BallTypes.STRIKE, 0);
+        return strike != GAME_OVER_STRIKE_COUNT;
+    }
+
+    private void decideRestartOrEnd() {
+        if (isGameOver()) {
+            printGameOverMessage();
+            return;
+        }
+        start();
+    }
+
+    private boolean isGameOver() {
+        return InputView.decision() == Decision.END;
+    }
+}
