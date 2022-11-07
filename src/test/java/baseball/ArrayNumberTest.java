e1f2ecc5a779b2daea48c1c537b2b4014cba4007
이규승 <rpg8988@naver.com>
2022-11-08 00:07:50 +0900

    feat: 직접 만든 예외 및 테스트 케이스

--- src/test/java/baseball/ArrayNumberTest.java
+++ src/test/java/baseball/ArrayNumberTest.java
@@ -21,4 +21,73 @@ public class ArrayNumberTest {
     private void createNumber() {
         arraynbr = new ArrayNumber();
     }
+
+    @Test
+    void computer_nbr_verify() {
+        arraynbr.setRandomNbr();
+        assertTrue(checkVaild());
+    }
+
+    @Test
+    void Right_input() {
+        arraynbr.inputAnswer();
+        assertTrue(checkVaild());
+    }
+
+    @Test
+    void str_input() {
+        assertThrows( IllegalArgumentException.class,
+                () -> arraynbr.inputAnswer()//abc
+        );
+    }
+
+    @Test
+    void not_three() {
+        assertThrows( IllegalArgumentException.class,
+                () -> arraynbr.inputAnswer()//1234
+        );
+    }
+
+    @Test
+    void not_range() {
+        assertThrows( IllegalArgumentException.class,
+                () -> arraynbr.inputAnswer()//102
+        );
+    }
+
+    @Test
+    void duplicate_input() {
+        assertThrows( IllegalArgumentException.class,
+                () -> arraynbr.inputAnswer()//112
+        );
+    }
+
+    private boolean checkVaild() {
+        return checkRange()
+                && checkCount()
+                && checkDuplicate();
+    }
+
+    private boolean checkRange() {
+        for(int digit: arraynbr.getDigits()) {
+            if(digit < min_nbr || digit > max_nbr) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkCount() {
+        return cnt_nbr == arraynbr.getDigits().length;
+    }
+
+    private boolean checkDuplicate() {
+        int[] digits = arraynbr.getDigits();
+        Set<Integer> digitSet = convertArrayToSet(digits);
+        return digits.length == digitSet.size();
+    }
+
+    private Set<Integer> convertArrayToSet(int[] arr) {
+        return Arrays.stream(arr).boxed().collect(Collectors.toSet());
+    }
 }
