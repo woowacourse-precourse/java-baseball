c25fe93a1a3a09066cd3d93432a31736757bf175
tkdtn4657 <tkdtn101226@gmail.com>
2022-11-08 06:27:12 +0900

    Console메서드 미사용상태 test1통과

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,22 +1,19 @@
 package baseball;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import camp.nextstep.edu.missionutils.*;
 
-/*camp.nextstep.edu.missionutils에서 제공하는 Randoms 
- * 및 Console API를 사용하여 구현해야 한다.*/
-
 class baseball {
 	
-	int user_num;
 	Scanner number = new Scanner(System.in);
-	Scanner repeat_Yes = new Scanner(System.in);
-	int repeat;
 	int result_num;
+	//List<Integer> playerNum = new ArrayList<>();
+	int playerNum;
 	List<Integer> tmp_List = new ArrayList<>(3);
 	List<Integer> correct = new ArrayList<>(3);
+	//Input_Num n = new Input_Num();
+	int inputnum;
 	
 	baseball(){
 		System.out.println("숫자 야구 게임을 시작합니다.");
@@ -30,26 +27,37 @@ class baseball {
 		        correct.add(randomNumber);
 		    }
 		}
+		System.out.println(correct);
 	}
-
+	
+	void getInputNum() {
+		
+		inputnum = Integer.valueOf(Console.readLine());
+	}
 	
 	int repeat_game(){
 		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-		return repeat_Yes.nextInt();
+		//getInputNum();
+		int n = number.nextInt();
+		return n;//inputnum;
 	}
 	
 	void get_user_input_num() {
-		System.out.println("숫자를 입력해주세요 : "); //get_user_input_num 구현완료
-		user_num = number.nextInt();
+	    
+		tmp_List.clear();
+		System.out.println("숫자를 입력해주세요 : ");
+		//String playerNumString = Console.readLine();
+		//String inputnum = Console.readLine();
 		
+		//getInputNum();
+		int n = number.nextInt();
+		int user_num = n;//inputnum;
+
 		int one = user_num / 100;
 		int two = user_num / 10 % 10;
 		int three = user_num % 10;
-
 		
-		int cipher;
-		
-		for(cipher = 0; cipher < 3; cipher++) {
+		for(int cipher = 0; cipher < 3; cipher++) {
 			if(cipher == 0) {
 				tmp_List.add(cipher, one);
 			}
@@ -61,34 +69,31 @@ class baseball {
 			}
 		}
 	}
-	
+
 	void game() {
 		do {
 		getrandomNum();
 		get_user_input_num();
-			do {
-				compare();
+		compare();
+		while(!result()){
 				get_user_input_num();
-			}while(!result());
+				compare();
+			}
 		}while(repeat_game() == 1);
 	}
 	
 	boolean result() {
-		//random사용해서 정답과 비교수를 compare로 넘김
-		int cipher;
 		int cnt = 0;
 		
-		for(cipher = 0; cipher < 3; cipher++) {
+		for(int cipher = 0; cipher < 3; cipher++) {
 			if(tmp_List.get(cipher) == correct.get(cipher)) {
 				cnt++;
 			}
 		}
 		if(cnt == 3) {
 			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
 			tmp_List.clear();
 			correct.clear();
-
 			return true;
 		}
 		return false;
@@ -102,14 +107,10 @@ class baseball {
 			if(tmp_List.get(cipher) == correct.get(cipher)) {
 				strike++;
 			}
-			
 			else if(tmp_List.contains(correct.get(cipher))) {
 				ball++;
 			}
-			
 		}
-		System.out.println("result : " + correct);//test용 문구
-		System.out.println(tmp_List);//test용 문구
 		if(ball > 0 && strike > 0) {
 			System.out.println(ball + "볼 " + strike + "스트라이크");
 		}else if(ball > 0 && strike < 1) {
@@ -120,17 +121,11 @@ class baseball {
 			System.out.println("낫싱");
 		}
 	}
+	
 }
-
-
-
-
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    	//야구게임
     baseball baseball_game = new baseball();
-
+    
     }
 }
-
