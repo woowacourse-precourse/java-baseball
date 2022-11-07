e5c50f19ee8f3645f3bcce63cb858eb9547d81ae
taehyun <didls1643@gmail.com>
2022-11-08 03:36:58 +0900

    feat: input userNum and playing game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -56,6 +56,32 @@ public class Application {
         }
     }
     public static void main(String[] args) {
-
+        Scanner scan = new Scanner(System.in);
+        int flag = 1;
+        String input;
+        boolean gameOver = false;
+        while (flag != 2) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            List<Integer> computerNum = randomNumGenerate();
+            while(!gameOver) {
+                List<Integer> result = new ArrayList<>();
+                System.out.println("숫자를 입력해주세요 : ");
+                input = Console.readLine();
+                int userNum = Integer.parseInt(input);
+                List<Integer> user = new ArrayList<>();
+                user.add(userNum / 100);
+                userNum %= 100;
+                user.add(userNum / 10);
+                userNum %= 10;
+                user.add(userNum);
+                errorHandle(user);
+                gameOver = ballStrikeNumCalculate(user, computerNum);
+            }
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            input = Console.readLine();
+            flag = Integer.parseInt(input);
+            gameOver = false;
+        }
+        System.out.println("종료되었습니다");
     }
 }
