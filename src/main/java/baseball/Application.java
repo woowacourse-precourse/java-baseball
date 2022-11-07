6ec25476397a7510a8643acc1cd7fa0cfcb557ae
KIMSEI1124 <workju1124@gmail.com>
2022-11-07 17:26:02 +0900

    feat: 내가 만든 Exception 확장
    - Game 객체 주석 해제 하여 코드가 실행되게 변경
    - Exception을 IllegalArgumentException으로 확장

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,10 +3,10 @@ package baseball;
 import baseball.domain.Game;
 
 public class Application {
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-//        Game game = new Game();
-//        game.initGame();
+
+        Game game = new Game();
+        game.initGame();
     }
 }
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -9,7 +9,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
