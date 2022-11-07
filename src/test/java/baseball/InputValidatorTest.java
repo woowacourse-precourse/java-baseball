88b65eb2714b57ee044a2c89a3065564a661ac0d
saewoo1 <wooozooo1123@gmail.com>
2022-11-07 20:02:25 +0900

    test(InputvalidatorTest): test code for InputValidatorTest

--- src/test/java/baseball/InputValidatorTest.java
+++ src/test/java/baseball/InputValidatorTest.java
@@ -4,25 +4,32 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
-
+// 올바르지 않은 입력값이면 throw를 하고 있는지
 class InputValidatorTest {
-  // 올바르지 않은 길이면 throw를 하고 있는가
+  //길이에 대한 검사
   @Test
   void isRightLength() {
     assertThatThrownBy(() -> InputValidator.isRightLength("1234"))
             .isInstanceOf(IllegalArgumentException.class);
   }
 
+// 중복 검사
   @Test
   void isDuplicated() {
+    assertThatThrownBy(() -> InputValidator.isDuplicated("122"))
+            .isInstanceOf(IllegalArgumentException.class);
   }
 
+  //숫자만 들어있는지
   @Test
   void isNumber() {
+    assertThatThrownBy(() -> InputValidator.isNumber("a24"))
+            .isInstanceOf(IllegalArgumentException.class);
   }
 
   @Test
   void isValidNumber() {
+
   }
 
   @Test
