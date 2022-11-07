2502bb447495d81d8b242feba71b0d605e62fca6
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-07 21:51:13 +0900

    feat(table): 중복 허용하지 않도록 예외 추가

--- src/main/java/custom/table/Table.java
+++ src/main/java/custom/table/Table.java
@@ -1,10 +1,18 @@
 package custom.table;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Table {
 
     private String number;
 
+    public Table() {
+
+    }
+
     private Table(String number) {
+        validateDuplicateNumber(number);
         this.number = number;
     }
 
@@ -13,10 +21,12 @@ public class Table {
     }
 
     public char get(int index) {
+        validateInit();
         return number.charAt(index);
     }
 
     public boolean contains(char character) {
+        validateInit();
         for (char elem : number.toCharArray()) {
             if (elem == character) {
                 return true;
@@ -26,6 +36,23 @@ public class Table {
     }
 
     public void setNumber(String number) {
+        validateDuplicateNumber(number);
         this.number = number;
+    }
+
+    private void validateDuplicateNumber(String number) {
+        List<Character> list = new ArrayList<>();
+        for (int i = 0; i < 3; ++i) {
+            if (list.contains(number.charAt(i))) {
+                throw new IllegalStateException("중복된 숫자로 데이터를 초기화 할 수 없습니다");
+            }
+            list.add(number.charAt(i));
+        }
+    }
+
+    private void validateInit() {
+        if (this.number == null) {
+            throw new RuntimeException("숫자를 먼저 초기화해주세요");
+        }
     }
 }
