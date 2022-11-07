ed58f47493d97c4536fe2be29487203f04a2d506
miffyKing <whqudghk28@gmail.com>
2022-11-07 11:12:19 +0900

    test: Add testcase to check appropriate ending message returns

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -60,4 +60,11 @@ class ComputerTest {
         Assertions.assertThat("낫싱").isEqualTo(outContent.toString().trim());
     }
 
+    @Test
+    @Order(4)
+    void computerPrintCalculatorEnding() {
+        Computer.testComputerPrint(3, "");
+        Assertions.assertThat("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.").isEqualTo(outContent.toString().trim());
+    }
+
 }
