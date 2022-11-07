c7dac8822d4d98da5e2012ed62b49a01c69e1d24
이건회 <rg970604@naver.com>
2022-11-08 06:25:30 +0900

    refactor (Numbers.java) : 검증 메소드 클래스 Validator로 변경

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -1,32 +1,15 @@
 package baseball.domain;
 
+import baseball.util.Validator;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Numbers {
-
-    private static final int REGULAR_NUMBERS_SIZE = 3;
     public List<Integer> numbers = new ArrayList<>();
 
     public Numbers(List<Integer> numbers) {
-        validateNumbersDuplicate(numbers);
-        validateNumbersSize(numbers);
+        Validator.validateNumbersDuplicate(numbers);
+        Validator.validateNumbersSize(numbers);
         this.numbers = numbers;
-    }
-
-    private static void validateNumbersDuplicate(List<Integer> numbers) {
-        List<Integer> cachedNumbers = new ArrayList<>();
-        for (Integer number : numbers) {
-            if (cachedNumbers.contains(number)) {
-                throw new IllegalArgumentException("숫자는 중복될 수 없습니다");
-            }
-            cachedNumbers.add(number);
-        }
-    }
-
-    private static void validateNumbersSize(List<Integer> numbers) {
-        if (numbers.size() < REGULAR_NUMBERS_SIZE || numbers.size() > REGULAR_NUMBERS_SIZE) {
-            throw new IllegalArgumentException("숫자는 3개만 입력 가능합니다");
-        }
     }
 }
