72d6bd67d7b7dd5fd7faf845a7b190494a792597
5uhwann <5uhwann27@gmail.com>
2022-11-07 22:27:47 +0900

    refactor(출력): 사용자 숫자 입력 요구 메시지 줄바꿈 수정

--- src/main/java/baseball/view/output/OutputImpl.java
+++ src/main/java/baseball/view/output/OutputImpl.java
@@ -1,10 +1,16 @@
 package baseball.view.output;
 
 
+import baseball.utils.message.OutputMessage;
+
 public class OutputImpl implements Output {
 
     @Override
     public void printMessage(String resultMessage) {
+        if(resultMessage.equals(OutputMessage.NUMBER_REQUEST_MESSAGE)){
+            System.out.print(resultMessage);
+            return;
+        }
         System.out.println(resultMessage);
     }
 }
