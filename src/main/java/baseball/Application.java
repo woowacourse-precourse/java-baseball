cd8b510d3415dd22492f51f9c966f96c6a0f4adb
desfox <desfox99@naver.com>
2022-11-07 21:58:30 +0900

    feat: 사용자가 1 입력 시 재시작, 2 입력 시 종료하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -91,4 +91,10 @@ public class Application {
         return false;
     }
 
+    private static int userInputRestartOrFinish() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userInputCommand = Console.readLine();
+        return Integer.parseInt(userInputCommand);
+    }
+
 }
