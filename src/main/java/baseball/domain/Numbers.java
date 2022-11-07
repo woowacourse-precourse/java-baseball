c8dba119d8c5125e8a2e1a5572602e18d885522f
이건회 <rg970604@naver.com>
2022-11-07 18:53:32 +0900

    feat (Numbers.java) : 중복된 숫자 발견시 예외 처리

--- src/main/java/baseball/domain/Numbers.java
+++ src/main/java/baseball/domain/Numbers.java
@@ -6,4 +6,19 @@ import java.util.List;
 public class Numbers {
 
     public List<Integer> numbers = new ArrayList<>();
+
+    public Numbers(List<Integer> numbers) {
+        validateNumbersDuplicate(numbers);
+        this.numbers = numbers;
+    }
+
+    private static void validateNumbersDuplicate(List<Integer> numbers) {
+        List<Integer> cachedNumbers = new ArrayList<>();
+        for (Integer number : numbers) {
+            if (cachedNumbers.contains(number)) {
+                throw new IllegalArgumentException("숫자는 중복될 수 없습니다");
+            }
+            cachedNumbers.add(number);
+        }
+    }
 }
