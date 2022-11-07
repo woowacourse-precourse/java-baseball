a2c4e21aa5d56ba31b9f51ef0ab0bdea35e81ebb
taehyun <didls1643@gmail.com>
2022-11-08 03:00:39 +0900

    feat: userNum compare with computerNum

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,6 +16,34 @@ public class Application {
         }
         return computer;
     }
+
+    public static boolean ballStrikeNumCalculate(List<Integer> userNum, List<Integer> computerNum) {
+        // 유저의 입력 값을 받아 볼과 스트라이크의 개수를 계산하는 함수
+        Boolean result = false;
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < 3; i++) {
+            if (userNum.get(i) == computerNum.get(i)) {
+                strike++;
+            } else if (computerNum.contains(userNum.get(i))) {
+                ball++;
+            }
+        }
+        if (ball == 0 || strike == 0) {
+            System.out.println("낫싱");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+            if (strike == 3) {
+                result = true;
+            }
+        } else {
+            System.out.println(ball + "볼");
+            System.out.println(strike + "스트라이크");
+        }
+        return result;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
