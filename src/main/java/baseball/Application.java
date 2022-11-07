0a0339b76afa1a3f960c5d6774c03d8219d3fc19
이성준 <75744297+castlejun-2@users.noreply.github.com>
2022-11-07 20:15:26 +0900

    refactor: 결과 출력문 통일

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -78,8 +78,7 @@ public class Application {
     }
     private static void strikeMessage (int strike, int ball) {
         if (strike == 3) {
-            System.out.println("3스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료 \n"+"게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            System.out.println("3스트라이크\n"+"3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"+"게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         } else if (strike + ball == 0) {
             System.out.println("낫싱");
         } else if (ball == 0) {
