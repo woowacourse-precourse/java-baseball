e137b8544b8a670427a6fcf552ede3a3d719d7ad
hodomaroo <enghemdwkd@gmail.com>
2022-11-08 00:57:48 +0900

    [Feat] : NumberBaseballGameApplication 구현 완료
    전체 게임 주기를 관리하는 NumberBaseballGameApplication을 구현하였으며,
    최종적으로 Application 클래스의 구현을 완료하였습니다.
    
    ApplicationTest클래스를 활용해 최종 검증을 완료하였으며, 추후 전체적인
    검토 및 코드 스타일 정비, 리팩토링 등을 진행할 예정입니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import java.util.Scanner;
+import org.junit.jupiter.api.Test;
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        NumberBaseballGameApplication application = new NumberBaseballGameApplication();
+        application.execute();
     }
 }
