8a9c3f0409cc14ea2512c5262b438fa7acc2bdb1
chkim <cndghks0213@gmail.com>
2022-11-08 08:07:47 +0900

    refactor(ui): 불필요한 field 제거

--- src/main/java/baseball/ui/Result.java
+++ src/main/java/baseball/ui/Result.java
@@ -9,21 +9,18 @@ public class Result {
     private static final String STRIKE_MESSAGE = "스트라이크";
     private static final String NOTHING_MESSAGE = "낫싱";
     private static final String SPACE = "";
-    private static final int MAX_COUNT = 3;
 
     private final int ball;
     private final int strike;
-    private final int miss;
 
     public Result(Map<BallTypes, Integer> result) {
         this.ball = result.getOrDefault(BallTypes.BALL, 0);
         this.strike = result.getOrDefault(BallTypes.STRIKE, 0);
-        this.miss = result.getOrDefault(BallTypes.MISS, 0);
     }
 
     @Override
     public String toString() {
-        if (miss == MAX_COUNT) {
+        if (ball + strike == 0) {
             return NOTHING_MESSAGE;
         }
         return (ballResult(ball) + strikeResult(strike)).trim();
