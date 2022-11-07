f19c0e5bf5216ca4430ee0fd59b03fff81111822
ingpyo <juninpyo72@gmail.com>
2022-11-07 11:48:36 +0900

    test:사용자 숫자 생성 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,35 +1,54 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import baseball.count.Game;
+import baseball.player.User;
+import baseball.service.Exception;
+import baseball.service.GameService;
+import baseball.view.SystemMessage;
+import camp.nextstep.edu.missionutils.test.NsTest;
 
 class ApplicationTest extends NsTest {
-    @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
-    }
+	Game game = new Game();
+	Exception exception = new Exception();
+	GameService service = new GameService();
+	User user = new User();
+	SystemMessage systemMessage = new SystemMessage();
+	Controller controller = new Controller();
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
+	@Test
+	void 게임종료_후_재시작() {
+		assertRandomNumberInRangeTest(
+			() -> {
+				run("246", "135", "1", "597", "589", "2");
+				assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+			},
+			1, 3, 5, 5, 8, 9
+		);
+	}
 
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
+	@Test
+	void 예외_테스트() {
+		assertSimpleTest(() ->
+			assertThatThrownBy(() -> runException("1234"))
+				.isInstanceOf(IllegalArgumentException.class)
+		);
+	}
+
+	@Override
+	public void runMain() {
+		Application.main(new String[] {});
+	}
+
+	@Test
+	void 사용자_숫자_생성() {
+		int[] userNumber = new int[] {1, 2, 3};
+		user.setUserNumber(userNumber);
+		assertThat(3).isEqualTo(user.userSize());
+	}
+
 }
