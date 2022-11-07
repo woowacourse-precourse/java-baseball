ffaf4bbc37fcb120f66652d30b286f8c0e5223bc
limecats <limecats0331@gmail.com>
2022-11-07 10:42:54 +0900

    feat(Game): add setComputersRandomAnswer
    add method
    
    change docs why method name change

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,14 +2,29 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Game {
+
+    public List<Integer> setComputersRandomAnswer() {
+        List<Integer> answer = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int addNumber = getRandomIntegerExcludeInList(answer);
+            answer.add(addNumber);
+        }
+
+        return answer;
+    }
+
     private int getRandomIntegerExcludeInList(List<Integer> answer) {
         int number;
+
         do {
             number = Randoms.pickNumberInRange(1, 9);
         } while (answer.contains(number));
+
         return number;
     }
 }
