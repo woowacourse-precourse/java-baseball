55a2cf9d89b34b9a363b27fc47e6ce641eeed06b
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-07 15:10:20 +0900

    readUser() : 주어진 readLine()으로 입력을 받는다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import java.util.*;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
+
+    static int readUser() {
+        int result = Integer.parseInt(Console.readLine());
+        return result;
+    }
+
+
 }
