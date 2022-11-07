78d4dcfd971966b7c5904e21eb0461fa3bcf3f75
Lee Min Woo <lmw7414@naver.com>
2022-11-07 15:29:15 +0900

    feat : 입력 받는 메서드 추가
    사용자로 부터 String을 입력 받아 문자열에 오류가 없다면 List로 변환하여 저장한다.

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,11 +1,44 @@
 package baseball;
 
+import baseball.inputException.ContainingZeroException;
+import baseball.inputException.DuplicatedNumberException;
+import baseball.inputException.IllegalDigitsException;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
-    String input;
+    List<Integer> playerNumber;
 
     public void receiveInput() {
-        this.input = Console.readLine();
+        this.playerNumber = getUserInputValue();
+    }
+
+    List<Integer> getUserInputValue() {
+
+        List<Integer> userInput = new ArrayList<>();
+        String input = Console.readLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            userInput.add(input.charAt(i) - '0');
+        }
+
+        if (userInput.size() != 3)
+            throw new IllegalDigitsException();
+        if (!confirmDuplicationNumber(userInput))
+            throw new DuplicatedNumberException();
+        if (isContainingZero(userInput))
+            throw new ContainingZeroException();
+
+        return userInput;
+    }
+
+    boolean confirmDuplicationNumber(List<Integer> number) {
+        return (number.get(0) != number.get(1)) && (number.get(1) != number.get(2)) && (number.get(0) != number.get(2));
+    }
+
+    boolean isContainingZero(List<Integer> number) {
+        return (number.contains(0));
     }
 }
