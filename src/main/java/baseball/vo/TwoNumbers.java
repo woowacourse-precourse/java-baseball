dc6a7506cfe6c7b38e68f710b93fd23ed759d4a0
Laeng <Laeng@users.noreply.github.com>
2022-11-07 17:27:07 +0900

    refactor(TwoNumbers): MVC 패턴 적용을 위한 파일 이동

---
+++ src/main/java/baseball/vo/TwoNumbers.java
@@ -0,0 +1,47 @@
+package baseball.vo;
+
+import baseball.OneDigitNumbers;
+import baseball.Scoreboard;
+
+public class TwoNumbers {
+    private final baseball.OneDigitNumbers oneDigitComputerNumbers;
+    private final baseball.OneDigitNumbers oneDigitPlayerNumbers;
+
+    public TwoNumbers(int computer, int player) {
+        this.oneDigitComputerNumbers = new OneDigitNumbers(computer);
+        this.oneDigitPlayerNumbers = new OneDigitNumbers(player);
+    }
+
+    public Scoreboard compare() {
+        if (this.isThreeStrikes()) {
+            return new Scoreboard(0, 3); // 3스트라이크
+        }
+
+        Scoreboard scoreboard = new Scoreboard();
+        OneDigitNumbers retainOneDigitNumbers = this.oneDigitComputerNumbers.retainAll(oneDigitPlayerNumbers);
+
+        for (int oneDigitNumber : retainOneDigitNumbers) {
+            scoreboard.addAllPoint(this.compareOneDigitNumber(oneDigitNumber));
+        }
+
+        return scoreboard;
+    }
+
+    private boolean isThreeStrikes() {
+        return this.oneDigitComputerNumbers.equals(this.oneDigitPlayerNumbers);
+    }
+
+    private Scoreboard compareOneDigitNumber(int oneDigit) {
+        Scoreboard scoreboard = new Scoreboard();
+
+        if (this.oneDigitComputerNumbers.indexOf(oneDigit) == this.oneDigitPlayerNumbers.indexOf(oneDigit)) {
+            scoreboard.addStrikePoint();
+
+            return scoreboard;
+        }
+
+        scoreboard.addBallPoint();
+
+        return scoreboard;
+    }
+}
