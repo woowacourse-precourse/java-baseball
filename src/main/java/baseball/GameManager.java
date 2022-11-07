4c7e0628841aa793b8112f377a2cd9e56e42fad5
AhnJoonSung <tjdtna01@naver.com>
2022-11-08 02:13:45 +0900

    add create new computer value function

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameManager {
     static void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -7,6 +12,18 @@ public class GameManager {
     }
 
     static void onGame() {
-    
+        List<Integer> computer = createComputerValue();
+
+    }
+
+    static List<Integer> createComputerValue() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
