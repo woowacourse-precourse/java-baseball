75bb10d89d29d6b337d1a6866e1dd378879944f5
HaODay <ycis5013@naver.com>
2022-11-08 00:03:19 +0900

    feat(printResult): 같은 수가 있으나 모두 맞히지 못한 경우의 결과 출력
    - 같은 수가 같은 자리에 있으면 '스트라이크' 출력
    - 같은 수가 다른 자리에 있으며 '볼' 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -77,6 +77,12 @@ public class Application {
             return;
         } else if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
+        }else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
         }
     }
 }
