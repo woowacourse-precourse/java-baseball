8e16154fc0082119531edef34dc3852d6fd907a4
ParkJunYeong1205 <junpreme1205@gmail.com>
2022-11-07 20:37:46 +0900

    feat: 애플리케이션 실행 시 게임 시작 안내 메시지를 출력하고 게임을 시작하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
 public class Application {
+    static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    static public void startGame() {
+
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(GAME_START_MESSAGE);
+        startGame();
     }
 }
