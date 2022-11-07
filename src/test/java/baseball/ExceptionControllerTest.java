b98c3c977613ccf65b987387faa7d9f61319d748
권민정 <22alswjd@naver.com>
2022-11-08 03:14:36 +0900

    test: 숫자가 아닌 값이 섞여있는 입력 예외처리 테스트
    - 입력에 숫자가 아닌 값이 섞여있을 때 IllegalArgumentException를 발생시키는지 테스트하는 테스트 코드 작성

--- src/test/java/baseball/ExceptionControllerTest.java
+++ src/test/java/baseball/ExceptionControllerTest.java
@@ -24,4 +24,31 @@ class ExceptionControllerTest {
                     .hasMessageContaining("세 자리 수를 입력해야 합니다.");
         }
     }
+
+    @Nested
+    class 숫자가_아닌_값이_섞여있을_때_예외발생_테스트 {
+        @Test
+        void 공백이_포함된_경우() {
+            String input = "1 2";
+            assertThatThrownBy(() -> ExceptionController.handleInvalidThreeRandomNumber(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("숫자만 입력해야 합니다.");
+        }
+
+        @Test
+        void 영문자가_포함된_경우() {
+            String input = "12a";
+            assertThatThrownBy(() -> ExceptionController.handleInvalidThreeRandomNumber(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("숫자만 입력해야 합니다.");
+        }
+
+        @Test
+        void 특수문자가_포함된_경우() {
+            String input = "12*";
+            assertThatThrownBy(() -> ExceptionController.handleInvalidThreeRandomNumber(input))
+                    .isInstanceOf(IllegalArgumentException.class)
+                    .hasMessageContaining("숫자만 입력해야 합니다.");
+        }
+    }
 }
