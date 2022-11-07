d7c64d9be564425e60efcca1f32b2b410b80d3e6
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-07 18:46:14 +0900

    feat:static value and default constructor

---
+++ src/main/java/baseball/model/SystemComputerModel.java
@@ -0,0 +1,22 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.model.SystemScoreModel.STRIKE;
+import static baseball.model.SystemScoreModel.BALL;
+import static baseball.model.SystemScoreModel.NOTHING;
+import static baseball.model.SystemScoreModel.ZERO;
+
+
+public class SystemComputerModel {
+    private static final int DIGIT_COUNT = 3;
+    private static final String EXIT_CODE = "2";
+
+    private List<Integer> digits;
+
+
+    public SystemComputerModel() {
+    }
+}
