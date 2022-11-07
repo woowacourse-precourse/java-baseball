7a7e13fed6170d2792003ba45d9558a4395df27a
w0ntae <bitcampcoding@gmail.com>
2022-11-07 16:22:13 +0900

    feat(generator): Random number generator
    Added new random generator

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+    
+    public static String random(){
+        boolean check = true;
+        ArrayList<Integer> num = new ArrayList<>();
+        while(check){
+            int random = Randoms.pickNumberInRange(1,9);
+            if(!num.contains(random)){
+                num.add(random);
+            }
+            if(num.size() == 3){
+                check = false;
+            }
+        }
+        return num.toString();
     }
 }
