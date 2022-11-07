8d044661e9e9ed9319bb38c2877534db471cc922
JUNBEOM_LEE <bom1215@naver.com>
2022-11-07 15:03:03 +0900

    feat(Class Application): User Input
    Add Input method to Class Application
    
    Breaking Change: Make Input method to input user numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -24,6 +25,32 @@ public class Application {
             if (!Answer_Numbers.contains(randomNumber)) {
                 Answer_Numbers.add(randomNumber);
             }
+        }
+    }
+
+    public static void Input() throws IllegalArgumentException {
+        System.out.print("숫자를 입력해주세요 : ");
+        try {
+            int Input_Number = Integer.parseInt(Console.readLine());
+
+            int hundreds = Input_Number % 1000 / 100;
+            int tens = Input_Number % 100 / 10;
+            int units = Input_Number % 10;
+            if (Input_Number < 111 || Input_Number > 999) {
+                throw new IllegalArgumentException();
+            }
+            if (hundreds == tens || hundreds == units || tens == units) {
+                throw new IllegalArgumentException();
+            }
+            List<Integer> Input_numbers = new ArrayList<>();
+            Input_numbers.add(0, hundreds);
+            Input_numbers.add(1, tens);
+            Input_numbers.add(2, units);
+
+            Final_Judge(Strike_or_Ball_Judge(Input_numbers, Answer_Numbers));
+
+        } catch (Exception e) {
+            throw new IllegalArgumentException();
         }
     }
     public static void main(String[] args) {
