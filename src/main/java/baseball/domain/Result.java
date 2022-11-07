ab6e2a660e64d49cfa0baa407e95907da82a1265
kimtaehoonDev <taehoon_dev@naver.com>
2022-11-07 20:37:31 +0900

    refactor(Result): 매직넘버 상수화

--- src/main/java/baseball/domain/Result.java
+++ src/main/java/baseball/domain/Result.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import static baseball.Constant.NUMBER_CNT;
+
 import java.util.Objects;
 
 public class Result {
@@ -7,10 +9,10 @@ public class Result {
     private Integer ballCnt;
 
     public Result(Integer strikeCnt, Integer ballCnt) {
-        if (strikeCnt < 0 || strikeCnt > 3 || ballCnt < 0 || ballCnt > 3) {
+        if (strikeCnt < 0 || strikeCnt > NUMBER_CNT || ballCnt < 0 || ballCnt > NUMBER_CNT) {
             throw new IllegalStateException("strike와 ball은 3개까지만 가능합니다");
         }
-        if (strikeCnt + ballCnt > 3) {
+        if (strikeCnt + ballCnt > NUMBER_CNT) {
             throw new IllegalStateException("strike와 ball을 합친 숫자가 자릿수보다 클 수는 없습니다");
         }
         this.strikeCnt = strikeCnt;
