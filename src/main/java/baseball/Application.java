96ccc2e8b193d2c7d4fe26d1ab2b6bff2e600744
kylew1004 <kylew1004@gmail.com>
2022-11-07 13:23:40 +0900

    feat: 세자리 중복되지 않은 랜덤 상대숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
+    public static int opponent1;
+    public static int opponent2;
+    public static int opponent3;
+
+    public static void initOpponentNumber(){
+        // num1 ~ num3까지 랜덤한 숫자를 넣는다.
+        opponent1 = Randoms.pickNumberInRange(1, 9);
+        while (true){
+            opponent2 = Randoms.pickNumberInRange(1, 9);
+            if (opponent1 != opponent2)
+                break;
+        }
+        while (true){
+            opponent3 = Randoms.pickNumberInRange(1, 9);
+            if (opponent2 != opponent3 && opponent1 != opponent3)
+                break;
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        initOpponentNumber();
     }
 }
