b3ed37ea022f0bc98682c937f917858626f66f6a
seonghoo1217 <seonghoo1217@naver.com>
2022-11-07 18:36:33 +0900

    fix : modify random number must be diffrent
    I understood just random number of three was enough
    
    - So I modify random number create feature
    
    Fix(commit_id) : #cb3eb5a

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -9,7 +9,10 @@ public class RandomNumber {
     public static List<Integer> createRandomNumber() {
         List<Integer> random_number_list=new ArrayList<>();
         for (int i=0;i<3;i++){
-            random_number_list.add(Randoms.pickNumberInRange(1, 9));
+            int random_number = Randoms.pickNumberInRange(1, 9);
+            if (!random_number_list.contains(random_number)){
+                random_number_list.add(random_number);
+            }
         }
         return random_number_list;
     }
