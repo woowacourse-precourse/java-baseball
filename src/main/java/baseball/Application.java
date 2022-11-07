3978b8ea523f94e3013441d13d1f3be121671db8
gusu9 <suhan.ku9@gmail.com>
2022-11-07 16:10:32 +0900

    updated comparison function output

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -96,8 +96,17 @@ public class Application {
                 System.out.println("중복전체: " + strikeNball);
                 System.out.println("스트라이크: " + strike);
                 System.out.println("볼: " + ball);
-
-                System.out.printf("%d 스트라이크 %d 볼\n", strike, ball);
+                if (ball ==0 && 0 < strike && strike<3) {
+                    System.out.printf("%d스트라이크\n", strike);
+                } else if(ball !=0&&strike == 0) {
+                    System.out.printf("%d볼\n", ball);
+                } else if(strike == 0 && ball == 0 ){
+                        System.out.println("낫싱");
+                } else if(strike == 3){
+                    System.out.println("삼진아웃");
+                } else{
+                    System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+                }
 
             } while (strike < 3);
 
