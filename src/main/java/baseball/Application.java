d02304d126f575ed154d9802807df3595f4ae4e2
Han Jihye <haneh0714@gmail.com>
2022-11-08 02:36:31 +0900

    FEAT : 랜던 숫자 초기화 기능 추가
    1~9 사이의 서로 다른 숫자 3개를 랜덤으로 뽑아 리스트로 반환한다.
    테스트 코드는 중복 없이 숫자 3개인지, 1~9 사이의 숫자인지 검사한다.
    사용자 입력 리스트 반환 메서드 수정 : 숫자에 0 포함되지 않도록 수정하였다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -11,17 +12,26 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        getUserAnswer(getInputString());
+        List<Integer> userAnswer = getUserAnswerList(getInputString());
 
 
     }
 
+    /**
+     * 사용자 입력을 받아 반환
+     */
     private static String getInputString() {
         System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
-    protected static List<Integer> getUserAnswer(String userAnswer) throws IllegalArgumentException {
+    /**
+     * 사용자 입력으로부터 숫자 3개짜리 리스트 반환
+     * @param userAnswer 입력 문자열
+     * @return 숫자 3개짜리 리스트
+     * @throws IllegalArgumentException 잘못된 입력에 대한 예외
+     */
+    protected static List<Integer> getUserAnswerList(String userAnswer) throws IllegalArgumentException {
         if (userAnswer.length() != 3) {
             throw new IllegalArgumentException("숫자 3개를 입력해야 함");
         }
@@ -32,12 +42,25 @@ public class Application {
 
         return userAnswer.chars()
                 .map(c -> {
-                    if (c < '0' || c > '9') {
-                        throw new IllegalArgumentException("숫자가 아닌 문자");
+                    if (c < '1' || c > '9') {
+                        throw new IllegalArgumentException("1~9 사이의 숫자가 아님");
                     }
                     return c-'0';
                 })
                 .boxed()
                 .collect(Collectors.toList());
+    }
+
+    /**
+     * 서로 다른 랜덤 숫자 3개 리스트 반환
+     */
+    protected static List<Integer> getRandomNumber() {
+        Set<Integer> numbers = new LinkedHashSet<>();
+
+        while (numbers.size() < 3) {
+            numbers.add(Randoms.pickNumberInRange(1, 9));
+        }
+
+        return new ArrayList<>(numbers);
     }
 }
--- src/test/java/baseball/FeatureTest.java
+++ src/test/java/baseball/FeatureTest.java
@@ -7,24 +7,45 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.assertj.core.api.Assertions.assertThat;
+
 
 public class FeatureTest {
 
     @Test
-    @DisplayName("다른 숫자 3개가 들어온 경우에만 성공")
+    @DisplayName("다른 숫자 3개가 들어온 경우에만 예외를 발생하지 않는다.")
     void 사용자_입력_테스트() {
         assertThrows(IllegalArgumentException.class,
-                () -> Application.getUserAnswer("122"));
+                () -> Application.getUserAnswerList("122"));
         ;
 
         assertThrows(IllegalArgumentException.class,
-                () -> Application.getUserAnswer("1234"));
+                () -> Application.getUserAnswerList("1234"));
         ;
 
         assertThrows(IllegalArgumentException.class,
-                () -> Application.getUserAnswer("1d2"));
+                () -> Application.getUserAnswerList("1d2"));
         ;
 
-        assertEquals(Application.getUserAnswer("123"), List.of(1,2,3));
+        assertThrows(IllegalArgumentException.class,
+                () -> Application.getUserAnswerList("012"));
+        ;
+
+        assertEquals(Application.getUserAnswerList("123"), List.of(1,2,3));
+    }
+
+    @Test
+    @DisplayName("서로 다른 숫자 3개를 반환해야 한다.")
+    void 랜덤_숫자_초기화_테스트() {
+        // when
+        List<Integer> randomNumber = Application.getRandomNumber();
+
+        // then
+        assertThat(randomNumber.size()).isEqualTo(3);
+        assertThat(randomNumber.stream().distinct().count()).isEqualTo(3);
+
+        for (Integer num : randomNumber) {
+            assertThat(num).isGreaterThanOrEqualTo(1).isLessThanOrEqualTo(9);
+        }
     }
 }
