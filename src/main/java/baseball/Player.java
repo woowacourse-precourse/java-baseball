325748f5857ce39670b4cff4fbac703d5bf8377c
LeeHaiLim <hailim1997@gmail.com>
2022-11-08 04:47:04 +0900

    feat(Player) : implement method is valid format
    사용자 입력이 1에서 9사이의 숫자 3자리인지 검증하는 기능 구현
    -정규식 NUMBER_FORMAT 상수 선언
    -isValidFormat() 메소드 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -8,12 +8,18 @@ public class Player {
     private static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String QUIT_OR_RESTART_INPUT_MESSAGE =
             String.format("게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.", RESTART_GAME_CODE, QUIT_GAME_CODE);
+    private static final String NUMBER_FORMAT = String.format("[%d-%d]{%d}",
+            Constants.NUMBER_RANGE_START, Constants.NUMBER_RANGE_END, Constants.NUMBER_COUNT);
 
     public String getNumberInput() {
         System.out.print(NUMBER_INPUT_MESSAGE);
         String input = Console.readLine();
 
         return input;
+    }
+
+    private boolean isValidFormat(String input) {
+        return input.matches(NUMBER_FORMAT);
     }
 
     public boolean getRestartOrQuitInput() {
