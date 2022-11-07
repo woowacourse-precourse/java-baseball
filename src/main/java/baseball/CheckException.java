2e319ffa841fdfd4b82729cdff3f0961ce8b9209
YongGwan <32181771@dankook.ac.kr>
2022-11-07 13:52:19 +0900

    에러 발생 부분을 따로 함수화해서 처리

--- src/main/java/baseball/CheckException.java
+++ src/main/java/baseball/CheckException.java
@@ -2,12 +2,16 @@ package baseball;
 
 public class CheckException {
 
+    private static void occurException() {
+        throw new IllegalArgumentException();
+    }
+
     public static void restartStatusValue(String restartStatus) {
         try {
             if(!(restartStatus.equals("1") || restartStatus.equals("2")))
-                throw new IllegalArgumentException();
+                occurException();
         } catch (IllegalArgumentException e) {
-            System.out.println(e +" 에러발성생 재시작 관련 값을 잘못 입력하셨습니다. 게임을 종료합니다.");
+            System.out.println(e +" 재시작 관련 값을 잘못 입력하셨습니다. 게임을 종료합니다.");
         }
     }
 }
