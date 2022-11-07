73195e8a422447f677a5c92ce62f7a7ca9a0b2a6
Frog-Slayer <Frog-Slayer@github.com>
2022-11-08 00:21:09 +0900

    feat : 서로 다른 세 숫자 뽑기

---
+++ src/main/java/baseball/model/Computer.java
@@ -0,0 +1,23 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Integer> numeralList = new ArrayList<>();
+    public Computer() {
+        generateComputerNumber();
+    }
+
+    private void generateComputerNumber(){
+        while (numeralList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numeralList.contains(randomNumber)) {
+                numeralList.add(randomNumber);
+            }
+        }
+    }
+
+}
