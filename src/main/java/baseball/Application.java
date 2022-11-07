9530f36f382d49647a4cb0c66199f29c34b4b1ee
Hong-yeonggwang <chamsk0808@naver.com>
2022-11-07 20:52:56 +0900

    게임 과정과 게임 마무리 작업.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,8 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
@@ -20,9 +22,6 @@ public class Application {
     //int형으로 입려받는 userInput을 List형으로 변환함
     public static List<Integer> convertUserInputToList (int userinput){
         List<Integer> returnData = new ArrayList<Integer>();
-        if(userinput >= 1000){
-            return List.of(-1);
-        }
         returnData.add(userinput/100);
         returnData.add(userinput/10 - returnData.get(0)*10);
         returnData.add(userinput % 10);
@@ -49,14 +48,40 @@ public class Application {
         if(hint[0] == 0 && hint[1] == 0){
             System.out.println("낫싱");
         }
-        for(int i = 0; i < hint.length; i++){
-            if(i == 0 ){
-                System.out.print(hint[1]+"볼 ");
-            } else if (i == 1) {
-                System.out.print(hint[0]+"스트라이크");
+        else{
+            System.out.println(hint[0]+"볼 "+hint[1]+"스트라이크");
+        }
+    }
+    public static void runGame(){
+        Scanner sc = new Scanner(System.in);
+        List<Integer> computer = setComputer();
+        System.out.println(computer);
+        boolean state = true;
+        while(state) {
+            System.out.print("숫자를 입력해주세요 : ");
+            int userinput = sc.nextInt();
+            List<Integer> userInput_List = convertUserInputToList(userinput);
+            int[] hint = inspectUserInput(userInput_List, computer);
+            printHint(hint);
+            if (hint[1] == 3) {
+                break;
             }
+        }
+        endGame();
+    }
+    public static void endGame(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        Scanner sc = new Scanner(System.in);
+        int userinput = sc.nextInt();
+        if(userinput == 1){
+            runGame();
+        }
+        else if(userinput == 2){
+
         }
     }
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        runGame();
     }
 }
