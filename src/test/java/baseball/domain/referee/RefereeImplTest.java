353af59e6e9b4cd14563d072ee41c468b87ff37d
5uhwann <5uhwann27@gmail.com>
2022-11-08 06:03:27 +0900

    test(결과판별): Referee 세트게임 진행 상태 관리 추가에 따른 테스트 수정

--- src/test/java/baseball/domain/referee/RefereeImplTest.java
+++ src/test/java/baseball/domain/referee/RefereeImplTest.java
@@ -2,27 +2,49 @@ package baseball.domain.referee;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class RefereeImplTest {
+    @Nested
+    class updateEachSetContextTest {
+        @Test
+        @DisplayName("결과가 3스트라이크일 경우 레프리는 게임 진행을 멈춘다.")
+        void Case1() {
+            //given
+            Referee referee = new RefereeImpl();
+            boolean beforeEachSetContext = referee.isEachSetContext();
 
-    @Test
-    void isEndGameSetTest() {
-        //given
-        Referee referee = new RefereeImpl();
+            String resultMessage = "3스트라이크";
 
-        String resultMessage1 = "3스트라이크";
-        String resultMessage2 = "1볼 2스트라이크";
+            //when
+            referee.updateEachSetContext(resultMessage);
 
-        //when
-        boolean endGameSetResult1 = referee.isEndGameSet(resultMessage1);
-        boolean endGameSetResult2 = referee.isEndGameSet(resultMessage2);
+            //then
+            boolean afterEachSetContext = referee.isEachSetContext();
+            assertThat(beforeEachSetContext).isEqualTo(true);
+            assertThat(afterEachSetContext).isEqualTo(false);
 
-        //then
-        assertThat(endGameSetResult1).isEqualTo(true);
-        assertThat(endGameSetResult2).isEqualTo(false);
+        }
 
+        @Test
+        @DisplayName("결과가 3스트라이크가 아닐 경우 레프리는 게임을 계속 진행한다.")
+        void Case2() {
+            //given
+            Referee referee = new RefereeImpl();
+            boolean beforeEachSetContext = referee.isEachSetContext();
+
+            String resultMessage = "1볼 2스타라이크";
+
+            //when
+            referee.updateEachSetContext(resultMessage);
+
+            //then
+            boolean afterEachSetContext = referee.isEachSetContext();
+            assertThat(beforeEachSetContext).isEqualTo(true);
+            assertThat(afterEachSetContext).isEqualTo(true);
+
+        }
     }
-
-
 }
