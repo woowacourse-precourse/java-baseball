dc201faaeb1026fe76e527db5049022c93dbda86
skfkgla <narahim.lee@gmail.com>
2022-11-07 23:26:03 +0900

    feat: 문자열 리스트에 포함되어 있지 않은 문자열이면 추가하는 메서드 구현
    Service 내부에 중복되지 않은 난수 생성 메서드에서 호출해 사용하기 위해 구현
    내부 메서드를 위해 구현했기 때문에 접근권한은 private

--- src/main/java/baseball/Service.java
+++ src/main/java/baseball/Service.java
@@ -8,4 +8,11 @@ public class Service {
     public List<String> stringToList(String string) {
         return Arrays.asList(string.split(""));
     }
+
+    private List<String> AddToListExcludeContainsString(List<String> list, String string){
+        if(!list.contains(string)) {
+            list.add(string);
+        }
+        return list;
+    }
 }
