d390ac202d349a2e02604d4297debed8c5c6eceb
yback1223 <yoyob1223@gmail.com>
2022-11-07 22:32:54 +0900

    refactor: finish Input class

--- src/main/java/baseball/io/Input.java
+++ src/main/java/baseball/io/Input.java
@@ -25,7 +25,19 @@ public class Input extends EndOrRe{
         return super.makeOneOrTwoInputList();
     }
 
-    List<Character> stringToList(String inputString) {
+    /**
+     * 테스트를 위한 메소드
+     * 문자열을 입력 받았다고 생각하고 클래스의 메소드들을 테스트한다.
+     *
+     * @param inputString Console.readLine()으로 입력받은 문자열이라고 가정한 파라미터
+     * @return 입력받은 문자열을 리스트로 만들어 반환
+     */
+    public List<Character> testReadAndMakeInputList(String inputString) {
+        validStringLength(inputString);
+        return stringToList(inputString);
+    }
+
+    private List<Character> stringToList(String inputString) {
         List<Character> list = new ArrayList<>();
         for (int i = 0; i < inputString.length(); i++) {
             list.add(inputString.charAt(i));
@@ -35,7 +47,7 @@ public class Input extends EndOrRe{
         return list;
     }
 
-    void validStringLength(String inputString) {
+    private void validStringLength(String inputString) {
         if (inputString.length() != listSize) {
             throw new IllegalArgumentException();
         }
