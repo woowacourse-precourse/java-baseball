9a717ccef09df0668d4ecb5546a920e7752111a1
0112leesy <0112leesy@naver.com>
2022-11-07 22:34:04 +0900

    feat(Application): baseballGame 함수 생성
    - User 객체는 본 함수 외부에서 옵션 값도 입력받아야 하므로 static 객체로 선언
    - 게임을 새로 할 때마다 Computer 클래스의 생성자로 새로운 answer 값을 가져야 하므로 함수 내부에서 Computer 객체 생성
    - 사용자가 정답을 정확히 맞힐 때까지 입력을 받고 그에 따른 결과가 출력함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
 public class Application {
+
+    static User user;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    static void baseballGame() {
+        Computer computer = new Computer();
+        boolean GameOver = false;
+
+        while (!GameOver) {
+            user.setInputForAnswer();
+            user.checkInputForAnswer();
+            String result = computer.getResult(user.input);
+            System.out.println(result);
+            if (computer.isGameOver(user.input)) {
+                GameOver = true;
+            }
+        }
     }
 }
