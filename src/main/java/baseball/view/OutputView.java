7dc4729bd8d4ddcca66687f61eacdf5779c29ca8
eom-tae-in <eti0728@daum.net>
2022-11-07 19:03:57 +0900

    feat(OutputView): printStartBaseballGame() -> 숫자야구 게임 시작을 알려주는 문구 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -8,4 +8,9 @@ public class OutputView {
 
         System.out.println(Constant.SELECT_RESTART_OR_END_MESSAGE);
     }
+
+    public static void printStartBaseballGame() {
+
+        System.out.println(Constant.START_BASEBALL_NUMBER_GAME_MESSAGE);
+    }
 }
