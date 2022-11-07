e28501e8575f79d07fd220bf57ac57a9fbd1957b
이원기 <log19951@gmail.com>
2022-11-08 00:50:00 +0900

    feat: 문자열을 출력해주는 Output 클래스 구현

---
+++ src/main/java/baseball/io/Output.java
@@ -0,0 +1,12 @@
+package baseball.io;
+
+public class Output {
+
+    public void printMessageWithEnter(String message) {
+        System.out.println(message);
+    }
+
+    public void printMessageWithoutEnter(String message) {
+        System.out.print(message);
+    }
+}
