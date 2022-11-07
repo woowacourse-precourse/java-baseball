fef18c93d0132329bc5b230484021a121aa4ba5b
delaylee <delaylee219@gmail.com>
2022-11-07 22:10:00 +0900

    test: 재시작 입력 유효성 검증 기능에 대한 테스트 추가
    - 빈 값 입력에 대해 IllegalArgumentException이 발생하는지 확인
    - 입력이 1, 2가 아닌 경우 IllegalArgumentException이 발생하는지 확인

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -29,12 +29,32 @@ class PlayerTest {
 
     @Test
     public void 사용자_입력이_빈값_경우_예외처리() {
-        String input = "123456";
+        String input = "";
         Player player = new Player();
 
         assertThatThrownBy(() -> player.inputValidationCheck(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("3자리만 입력 가능합니다");
+                .hasMessageContaining("입력을 해야합니다");
+    }
+
+    @Test
+    public void 재시작_입력이_1또는_2가_아닌_경우_예외처리() {
+        String input = "3";
+        Player player = new Player();
+
+        assertThatThrownBy(() -> player.replayValidationCheck(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1 또는 2만 입력 가능합니다");
+    }
+
+    @Test
+    public void 재시작_입력이_빈값_경우_예외처리() {
+        String input = "";
+        Player player = new Player();
+
+        assertThatThrownBy(() -> player.replayValidationCheck(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("입력을 해야합니다");
     }
 
 }
