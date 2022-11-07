7a2fb25aa3edae3462c1fbca371d26a51bf1a66f
권민정 <22alswjd@naver.com>
2022-11-07 18:17:25 +0900

    feat: 숫자 입력을 요청
    Computer 클래스에 다음 내용 추가:
    - 플레이어에게 숫자 입력을 요구하는 메세지 출력

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class Computer {
     public void gameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        askForThreeNumber();
+    }
+
+    List<Integer> askForThreeNumber() {
+        System.out.print("숫자를 입력해 주세요 : ");
+
+        return null;
     }
 }
