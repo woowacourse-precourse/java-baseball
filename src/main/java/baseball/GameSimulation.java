1e83f27165dfd7774086c55e595bc53560bc4491
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-07 23:59:33 +0900

    feat: 유저 숫자 받기

--- src/main/java/baseball/GameSimulation.java
+++ src/main/java/baseball/GameSimulation.java
@@ -1,4 +1,31 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class GameSimulation {
+    private Computer computer;
+    private int strike;
+    private int ball;
+
+    public GameSimulation() {
+    }
+
+    public void playGame() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String userInput = readLine();
+        List<Integer> userNumber = makeList(userInput);
+    }
+
+    public List<Integer> makeList(String userInput) {
+        String[] bitInput = userInput.split("");
+        return Arrays
+                .stream(bitInput)
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+    }
 }
