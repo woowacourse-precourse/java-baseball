590d22a770a7947b79d04f74fc0e83e971e06578
BaeDongHyeon <sasd730@gmail.com>
2022-11-07 19:46:22 +0900

    feat: 사용자 입력 검사 기능 추가
    - 1~9인 정수 여부 확인
    - 3자리 수 확인
    - 서로 다른지 확인

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -3,7 +3,9 @@ package baseball.game;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Game {
@@ -17,8 +19,22 @@ public class Game {
     private List<Integer> getInputUserNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         String userInput = Console.readLine();
+        inputCheck(userInput);
         return Arrays.asList(userInput.split("")).stream()
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
+    }
+
+    private void inputCheck(String input) {
+        if (!input.matches("^[1-9]*$")) {
+            throw new IllegalArgumentException("1~9까지의 숫자만 입력이 가능합니다.");
+        }
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3자리 입력만 가능합니다.");
+        }
+        Set<String> distinCheck = new HashSet<>(Arrays.asList(input.split("")));
+        if (distinCheck.size() != 3) {
+            throw new IllegalArgumentException("서로 다른 수만 입력이 가능합니다.");
+        }
     }
 }
