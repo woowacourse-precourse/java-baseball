59dc0005de5593d7a94ddeccbd3d24f952e5ed37
peterysh <52229539+peterysh@users.noreply.github.com>
2022-11-07 18:19:43 +0900

    fix: append GAME_END message

--- src/main/java/baseball/ScreenString.java
+++ src/main/java/baseball/ScreenString.java
@@ -7,7 +7,7 @@ public enum ScreenString {
     OUTPUT_BALL("볼"),
     OUTPUT_STRIKE("스트라이크"),
     NOTHING("낫싱"),
-    GAME_END("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    GAME_END("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
     private final String message;
 
