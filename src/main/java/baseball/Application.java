6d6ec4307766690eedb465f3cfe4bc51e7810fd8
KimSangHyeok <tkdgur0906@naver.com>
2022-11-07 19:07:23 +0900

    Feat: 야구 게임 시작하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,9 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        NumberBaseballGame numberBaseballGame = new NumberBaseballGame();
+        while(numberBaseballGame.checkGameEnd()){
+            numberBaseballGame.gameStart();
+        }
     }
 }
