e02d14bbb35b0223ec3cc3f11796d159b99d98ec
homecoder98 <homecoder98@gmail.com>
2022-11-07 20:51:34 +0900

    feat : 결과 출력하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,13 +8,29 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        chooseResetOrExit();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        startGame();
     }
 
     static void startGame() {
+        String randomThree = getRandomThree();
+        System.out.println(randomThree);
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String s = Console.readLine();
+
+            printResult(s,randomThree);
+
+            if (s.equals(randomThree)) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임 종료");
+                break;
+            }
+        }
+        chooseResetOrExit();
     }
 
-    static List<Integer> getRandomThree() {
+    static String getRandomThree() {
         List<Integer> randomList = new ArrayList<>();
         while (randomList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -22,11 +38,38 @@ public class Application {
                 randomList.add(randomNumber);
             }
         }
-        return randomList;
+        StringBuilder builder = new StringBuilder();
+        for (int i=0;i<3;i++)
+            builder.append(randomList.get(i));
+        return builder.toString();
+    }
+
+    static void printResult(String input, String randomThree) {
+        int strike = 0;
+        int ball = 0;
+        for (int i=0;i<3;i++) {
+            for (int j=0;j<3;j++) {
+                if (input.charAt(i) == randomThree.charAt(j)) {
+                    if (i == j) {
+                        strike++;
+                        continue;
+                    }
+                    ball++;
+                }
+            }
+        }
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        } else if (strike > 0 && ball == 0) {
+            System.out.println(strike+"스트라이크");
+        } else if (strike == 0 && ball > 0) {
+            System.out.println(ball+"볼");
+        } else {
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
     }
 
     static void chooseResetOrExit() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String s = Console.readLine();
 
