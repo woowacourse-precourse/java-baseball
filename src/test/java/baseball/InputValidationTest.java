e18f0a79066778914859c85f182eaddf3c5f61e5
anneu <b.imnuj@gmail.com>
2022-11-07 23:21:16 +0900

    refactor: 접근제어자를 private으로 변경함에 따른 테스트 변경

--- src/test/java/baseball/InputValidationTest.java
+++ src/test/java/baseball/InputValidationTest.java
@@ -19,31 +19,31 @@ class InputValidationTest {
     @DisplayName("세글자가 아닐 경우 예외 발생 확인")
     @CsvSource(value = {"1", "12", "1234", "12345"})
     void validateLength(String input) {
-        assertThatThrownBy(() -> inputValidation.validateLength(input))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> inputValidation.validatePlayerNumbersInput(input))
+                .hasMessage("3자리 수를 입력해야 합니다.");
     }
 
     @ParameterizedTest
     @DisplayName("1부터 9까지 수가 아닐 경우 예외 발생 확인")
-    @CsvSource(value = {"a", "abc", "가나다"})
+    @CsvSource(value = {"abc", "가나다", "a1b", "12가", "102", "1 2"})
     void validateNumberRange(String input) {
-        assertThatThrownBy(() -> inputValidation.validateNumberRange(input))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> inputValidation.validatePlayerNumbersInput(input))
+                .hasMessage("1부터 9까지의 수를 입력해야 합니다.");
     }
 
     @ParameterizedTest
     @DisplayName("중복이 있을 경우 예외 발생 확인")
-    @CsvSource(value = {"11", "aa", "121", "aba"})
+    @CsvSource(value = {"112", "121", "333"})
     void validateDuplication(String input) {
-        assertThatThrownBy(() -> inputValidation.validateDuplication(input))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> inputValidation.validatePlayerNumbersInput(input))
+                .hasMessage("각 자리의 수가 서로 달라야 합니다.");
     }
 
     @ParameterizedTest
     @DisplayName("1 또는 2가 아닐 경우 예외 발생 확인")
     @CsvSource(value = {"0", "3", "4", "a"})
     void validateChoice(String input) {
-        assertThatThrownBy(() -> inputValidation.validateChoice(input))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> inputValidation.validateRestartInput(input))
+                .hasMessage("1 또는 2를 입력해야 합니다.");
     }
 }
