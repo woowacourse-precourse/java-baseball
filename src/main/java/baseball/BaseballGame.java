4d9beca7e1d94be35def4d14a7ce0a2482d3adfd
yujeongkwon <yoojeongkwon@naver.com>
2022-11-08 03:28:28 +0900

    feat(BaseballGame) : 사용자 입력 검증

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,12 +2,11 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class BaseballGame {
 
-    public static String ComputerNumGenerator(){
+    public static List<Integer> ComputerNumGenerator(){
         List<Integer> num = new ArrayList<>();
         while (num.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -15,7 +14,38 @@ public class BaseballGame {
                 num.add(randomNumber);
             }
         }
-        return num.toString();
+        return num;
     }
 
+    public static List<Integer> userInputValidator(String input) throws IllegalArgumentException{
+        if(isThreelen(input) && isNotDuplicated(input) && isOneNineInt(input)){
+            List<Integer> num = new ArrayList<>();
+            for(int i=0; i<3; i++){
+                num.add(input.charAt(i)-'0');
+            }
+            return num;
+        }
+        else
+            throw new IllegalArgumentException();
+    }
+
+
+    public static boolean isThreelen(String input){
+        return input.length() == 3;
+    }
+
+    public static boolean isNotDuplicated(String input){
+        ArrayList<String> listInput = new ArrayList<>(Arrays.asList(input.split("")));
+        Set<String> inputSet = new HashSet<>(listInput);
+        return inputSet.size() == listInput.size();
+    }
+
+    public static boolean isOneNineInt(String input){
+        for(int i=0; i < input.length(); i++){
+            char n = input.charAt(i);
+            if(n == '0' || !Character.isDigit(n))
+                return false;
+        }
+        return true;
+    }
 }
