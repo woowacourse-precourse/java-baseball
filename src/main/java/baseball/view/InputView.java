9ec7c5c2877825eff98b357503cab076aeb9e6e6
dclxxi <dclxxi@kakao.com>
2022-11-07 17:53:53 +0900

    fix: InputView의 상수의 enum을 SHUTDOWN에서 RESTART으로 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
     private static final String INPUT_PLAYER_NUMBER = "숫자를 입력해주세요 : ";
-    private static final String INPUT_STATE_NUMBER = "게임을 새로 시작하려면 " + State.SHUTDOWN.getNumber()
+    private static final String INPUT_STATE_NUMBER = "게임을 새로 시작하려면 " + State.RESTART.getNumber()
             + ", 종료하려면 " + State.SHUTDOWN.getNumber() + "를 입력하세요.";
 
     public static String inputPlayerNumber() {
