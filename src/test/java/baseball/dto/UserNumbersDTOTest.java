c0520d6035c148c66c7caa56fd8fea634b186ebf
JunH <tjdtls690@gmail.com>
2022-11-07 15:37:15 +0900

    :white_check_mark: test(Dto : UserNumbersDTOTest) : null 또는 empty 가 아니면 올바른 유저 번호 입력 값인지 확인하는 테스트 추가

--- src/test/java/baseball/dto/UserNumbersDTOTest.java
+++ src/test/java/baseball/dto/UserNumbersDTOTest.java
@@ -1,12 +1,22 @@
 package baseball.dto;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 class UserNumbersDTOTest {
+    @Test
+    @DisplayName("null 또는 empty 가 아니면 올바른 유저 번호 입력 값인지 확인")
+    void correct_not_null_or_empty() {
+        assertThatNoException()
+                .isThrownBy(() -> new UserNumbersDTO("123"));
+    }
+    
     @DisplayName("예외 처리 : null 또는 empty 입력 시")
     @ParameterizedTest(name = "{displayName} => {0}")
     @NullAndEmptySource
