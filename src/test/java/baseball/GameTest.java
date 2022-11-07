3d008cf021f6a5d027386af84f71d086b8385e97
leeyongjun <leeyongjun0310@naver.com>
2022-11-07 14:54:03 +0900

    test(GameTest): restart 종료를 선택할때 테스트 코드 추가

---
+++ src/test/java/baseball/GameTest.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class GameTest {
+
+    @Test
+    void restart_종료를_선택할때() {
+        String input = "2";
+        Game game = new Game();
+        assertThat(game.restart(input)).isFalse();
+    }
+}
