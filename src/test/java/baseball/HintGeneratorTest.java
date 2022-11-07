58fb5f2d9ae37440f16143ffacd035246eb174ba
viyott-lover <jackk0616@gmail.com>
2022-11-07 18:14:48 +0900

    test: hintGenerator 힌트 생성 테스트

--- src/test/java/baseball/HintGeneratorTest.java
+++ src/test/java/baseball/HintGeneratorTest.java
@@ -15,17 +15,23 @@ class HintGeneratorTest {
     Pitcher p;
     HintGenerator hintGenerator;
 
-    @BeforeAll
+    @BeforeEach
     public void init() {
         c = new Catcher();
         c.forTest_setter(); // 테스트를 위해 catcher에 임의로 713 저장
     }
 
     @Test
-    void 힌트생성_1볼_1스트라이크() {
-        p = new Pitcher("123");
-        hintGenerator = new HintGenerator(p, c);
-        String hint = hintGenerator.generateHint();
-        assertThat(hint).isEqualTo("1볼 1스트라이크");
+    void 힌트생성() {
+        String[] cases = {"854", "714", "137", "721", "713"};
+        String[] answers = {"낫싱", "2스트라이크", "3볼", "1볼 1스트라이크", "3스트라이크"};
+
+        for (int i = 0; i < cases.length; i++) {
+            p = new Pitcher(cases[i]);
+            hintGenerator = new HintGenerator(p, c);
+            String hint = hintGenerator.generateHint();
+            assertThat(hint).isEqualTo(answers[i]);
+        }
+
     }
 }
