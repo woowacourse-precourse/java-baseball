fb67c43a8166e2059991c149e4ef2a02bc11a8a5
DevJay1024 <devjay1024@gmail.com>
2022-11-07 21:21:48 +0900

    feat: 어플리케이션(시스템) 동작 기능 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,6 @@ package baseball;
 public class Application {
 
     public static void main(String[] args) {
-        // 숫자 야구 게임 객체 생성 및 본 게임 호출
+        new NumberBaseballGameServer().run();
     }
 }
