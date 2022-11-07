24502843fd5c722ac65ec6fdc64c0a531a364865
jw0293 <k38836919@gmail.com>
2022-11-07 16:09:50 +0900

    feature : Count Ball and Strike Pair [볼/스트라이크 counting 클래스]

--- src/main/java/baseball/HintPair.java
+++ src/main/java/baseball/HintPair.java
@@ -1,2 +1,27 @@
-package baseball;public class HintPair {
+package baseball;
+
+public class HintPair {
+    private int countBall;
+    private int countStrike;
+
+    HintPair() {
+        this.countBall = 0;
+        this.countStrike = 0;
+    }
+
+    public void increaseBall() {
+        countBall++;
+    }
+
+    public void increaseStrike() {
+        countStrike++;
+    }
+
+    public int getCountBall() {
+        return countBall;
+    }
+
+    public int getCountStrike() {
+        return countStrike;
+    }
 }
