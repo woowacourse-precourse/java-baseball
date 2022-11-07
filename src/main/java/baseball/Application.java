16d057bbadcb0e46c9d613c1e2f83ebac1a4aa87
KimYongJun <rladydwns129@naver.com>
2022-11-07 23:15:20 +0900

    feat: Application 클래스 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,7 @@ package baseball;
 public class Application {
     private static final int NUMBER_LENGTH = 3;
     public static void main(String[] args) {
-
+        Game.runGame();
     }
 
     public static int NUMBER_LENGTH() {
