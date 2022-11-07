3f70ce3dbb523d1b9a69e5948a63bf68ca805336
wjh2012 <wjh2012@naver.com>
2022-11-07 20:33:17 +0900

    test(Game): add 게임종료 기능 false 테스트

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -6,13 +6,23 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class GameTest {
+
     @Test
-    void 결과로_게임_종료를_결정하는_기능_종료(){
+    void 결과로_게임_종료를_결정하는_기능_false() {
         ScoreBoard scoreBoard = new ScoreBoard();
         scoreBoard.addStrike();
         scoreBoard.addStrike();
         scoreBoard.addStrike();
 
         Assertions.assertThat(Game.checkGameOver(scoreBoard)).isEqualTo(false);
+    }
+
+    @Test
+    void 결과로_게임_종료를_결정하는_기능_true() {
+        ScoreBoard scoreBoard = new ScoreBoard();
+        scoreBoard.addStrike();
+        scoreBoard.addStrike();
+
+        Assertions.assertThat(Game.checkGameOver(scoreBoard)).isEqualTo(true);
     }
 }
