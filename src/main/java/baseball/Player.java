c626f8ed8653d73ccdf935d1c03fd3d7a93e87a5
dain-lee <dain9934@gmail.com>
2022-11-07 20:25:23 +0900

    feat(player): 사용자에게 숫자를 입력받는 메서드 구현

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,10 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+    public String getPlayerInputNumbersString() {
+        String input = Console.readLine();
+        return input;
+    }
+}
