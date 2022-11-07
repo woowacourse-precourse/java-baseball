9c8a16f2d2bdfeb45815257526ee3a1937b06f18
이건회 <rg970604@naver.com>
2022-11-07 18:58:02 +0900

    feat (BallCount.java) : 0~3 이외의 카운트에 대해 예외 처리

--- src/main/java/baseball/domain/BallCount.java
+++ src/main/java/baseball/domain/BallCount.java
@@ -2,6 +2,10 @@ package baseball.domain;
 
 public class BallCount {
 
+    private final int MIN_STRIKE_COUNT = 0;
+    private final int MAX_STRIKE_COUNT = 3;
+    private final int MIN_BALL_COUNT = 0;
+    private final int MAX_BALL_COUNT = 3;
     private int Strikes;
     private int Balls;
 
@@ -21,5 +25,17 @@ public class BallCount {
             return String.valueOf(Strikes) + "스트라이크";
         }
         return String.valueOf(Balls) + "볼 " + String.valueOf(Strikes) + "스트라이크";
+    }
+
+    private void validateStrikeCount(int strikes) {
+        if (strikes < MIN_STRIKE_COUNT || strikes > MAX_STRIKE_COUNT) {
+            throw new IllegalArgumentException("스트라이크는 0에서 3 사이 숫자만 집계합니다");
+        }
+    }
+
+    private void validateBallCount(int balls) {
+        if (balls < MIN_BALL_COUNT || balls > MAX_BALL_COUNT) {
+            throw new IllegalArgumentException("볼은 0에서 3 사이 숫자만 집계합니다");
+        }
     }
 }
