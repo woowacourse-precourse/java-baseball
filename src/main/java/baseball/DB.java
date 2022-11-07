3b4e5983ded56767e3a224151ada2693ff6ab988
kdkdhoho <hkim4410@naver.com>
2022-11-07 17:57:03 +0900

    refactor(DB): 사용하지 않는 메소드 제거

--- src/main/java/baseball/DB.java
+++ src/main/java/baseball/DB.java
@@ -25,25 +25,4 @@ public class DB {
             System.out.println(database.get(index));
         }
     }
-
-    public Data getData(Data data) {
-        if(database.contains(data)) {
-            return database.get(database.indexOf(data));
-        }
-        return null;
-    }
-
-    public void modifyData(Data existData, Data newData) {
-        if(database.contains(existData)) {
-            Data data = database.get(database.indexOf(existData));
-            data.modifyTryData(newData.getTryDate());
-            data.modifyTryCount(newData.getTryCount());
-        }
-    }
-
-    public void removeData(Data data) {
-        if (database.contains(data)) {
-            database.remove(data);
-        }
-    }
 }
