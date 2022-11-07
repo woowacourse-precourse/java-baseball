f66192759f2595e2fcf8edcce26e2dc0a0dd8ccf
twoosky <lhjae3@naver.com>
2022-11-08 04:21:39 +0900

    refactor(baseball): 랜덤값 범위 상수 처리

--- src/main/java/utils/RandomNumberGenerator.java
+++ src/main/java/utils/RandomNumberGenerator.java
@@ -6,13 +6,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RandomNumberGenerator {
+    private final static int RANDOM_NUMBER_LENGTH = 3;
+    private static final int RANDOM_NUMBER_RANGE_MIN = 1;
+    private static final int RANDOM_NUMBER_RANGE_MAX = 9;
 
     private RandomNumberGenerator() {}
 
     public static List<Integer> generate() {
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (computer.size() < RANDOM_NUMBER_LENGTH) {
+            int randomNumber = Randoms.pickNumberInRange(RANDOM_NUMBER_RANGE_MIN, RANDOM_NUMBER_RANGE_MAX);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
