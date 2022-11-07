54a45f95dc0edec2d1b7d60fd77c1cedd29c2c20
leesangjin <tkdwls0628@gmail.com>
2022-11-07 23:29:01 +0900

    feat(Player): 사용자 입력 받기 기능 구현

---
+++ src/main/java/baseball/domain/Player.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Player {
+
+    public String enterPlayerInput() {
+        return Console.readLine();
+    }
+
+    public String enterRestartInput() {
+        return Console.readLine();
+    }
+}
