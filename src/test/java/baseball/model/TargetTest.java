476ce1c109e96499f9cd6ff1d0ab5eef4f481f17
Woojin Lee <dhkep03@gmail.com>
2022-11-07 18:57:26 +0900

    test(Target): BeforeEach 어노테이션을 활용하여, 테스트 메서드 별 초기화 적용

--- src/test/java/baseball/model/TargetTest.java
+++ src/test/java/baseball/model/TargetTest.java
@@ -4,9 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TargetTest {
+    @BeforeEach
+    void initTarget() {
+        Target.setTargetIntoZeroList();
+    }
+
     @Test
     void reset() {
         Target.reset();
@@ -17,8 +23,17 @@ public class TargetTest {
 
     @Test
     void get() {
+        Target.reset();
         Target.setTargetIntoZeroList();
         List<Integer> result = Arrays.asList(0, 0, 0);
         assertThat(Target.get()).isEqualTo(result);
+    }
+
+    @Test
+    void setOneNumber() {
+        Target.setOneNumber(1);
+        List<Integer> targetNumberList = Target.get();
+        assertThat(targetNumberList.get(1)).isNotEqualTo(0);
+        assertThat(targetNumberList.get(0)).isEqualTo(0);
     }
 }
