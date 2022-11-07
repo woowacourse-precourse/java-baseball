bc4ab6955e2ddb911b8d76903ff2de122aa88bf6
minju412 <ln8847@ajou.ac.kr>
2022-11-07 23:03:24 +0900

    refactor: convert magic number to constant
    - 1 -> BALL_MINIMUM_VALUE
    - 9 -> BALL_MAXIMUM_VALUE

--- src/main/java/baseball/util/BallGenerator.java
+++ src/main/java/baseball/util/BallGenerator.java
@@ -1,5 +1,7 @@
 package baseball.util;
 
+import static baseball.util.InputValidator.BALL_MAXIMUM_VALUE;
+import static baseball.util.InputValidator.BALL_MINIMUM_VALUE;
 import static baseball.util.InputValidator.COMPUTER_BALLS_SIZE;
 
 import camp.nextstep.edu.missionutils.Console;
@@ -14,7 +16,7 @@ public class BallGenerator {
     public List<Integer> createComputerBalls() {
         List<Integer> computerBalls = new ArrayList<>();
         while (computerBalls.size() < COMPUTER_BALLS_SIZE) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            int randomNumber = Randoms.pickNumberInRange(BALL_MINIMUM_VALUE, BALL_MAXIMUM_VALUE);
             if (!computerBalls.contains(randomNumber)) {
                 computerBalls.add(randomNumber);
             }
