85655ff1788f565b3758772008a5e909bb8923e7
jihwan <sgh002400@naver.com>
2022-11-08 01:34:41 +0900

    feat(InputView): 숫자 입력 메시지 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class InputView {
+	private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
+	public static void displayInputNumberMessage() {
+		System.out.print(INPUT_NUMBER_MESSAGE);
+	}
+}
