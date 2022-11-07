d9098965fe1ff6687e89808b3cd8e65612677ae1
This2sho <zx00018@naver.com>
2022-11-08 04:11:23 +0900

    test(RandomGenerator): 서로 다른 3개의 난수 생성 기능 검증

--- src/test/java/baseball/domain/RandomGeneratorTest.java
+++ src/test/java/baseball/domain/RandomGeneratorTest.java
@@ -2,6 +2,7 @@ package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.RepeatedTest;
 
@@ -15,13 +16,26 @@ class RandomGeneratorTest {
 	}
 
 	@RepeatedTest(10)
-	public void 난수_1_9_생성_테스트() {
+	public void 난수_1_9_생성_검증() {
 		//given
 		int randomNumber = randomGenerator.generateNumber();
-		System.out.println(randomNumber);
 		//when
 		boolean validResult = randomNumber >= 1 && randomNumber <= 9;
 		//then
 		assertThat(validResult).isTrue();
+	}
+
+	@RepeatedTest(10)
+	public void 서로_다른_3개_난수_생성_검증() {
+		//given
+		List<Integer> randomNumbers = randomGenerator.generateNumbers();
+		//when
+		int num1 = randomNumbers.get(0);
+		int num2 = randomNumbers.get(1);
+		int num3 = randomNumbers.get(2);
+		boolean actual = num1 != num2 && num1 != num3 && num2 != num3;
+		//then
+		assertThat(randomNumbers.size()).isEqualTo(3);
+		assertThat(actual).isTrue();
 	}
 }
