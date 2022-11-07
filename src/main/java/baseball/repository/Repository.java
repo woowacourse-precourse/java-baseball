e7cd401625bfcfe125b2b9a316a19247cb1cc6bc
소대훈 <theogns0681@naver.com>
2022-11-07 10:51:43 +0900

    [refactor] 사용하지 않는 import 삭제와 setter 추가

--- src/main/java/baseball/repository/Repository.java
+++ src/main/java/baseball/repository/Repository.java
@@ -1,15 +1,14 @@
 package baseball.repository;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
-
 public class Repository {
     private int score;
 
     public int getScore() {
         return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
     }
 
     public Repository(int score) {
