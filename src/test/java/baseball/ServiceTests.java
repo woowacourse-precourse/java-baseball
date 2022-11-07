786d2f1c304b4a14ca522844c4cd62258ad5fc34
skfkgla <narahim.lee@gmail.com>
2022-11-08 00:49:39 +0900

    test: 최소값 이상 최대값 미만을 원하는 크기만큼 수가 중복되지 않도록 난수를 반환하는 메서드 테스트 코드 작성
    예상대로 동작하는지 확인하기 위한 단위 테스트 작성

--- src/test/java/baseball/ServiceTests.java
+++ src/test/java/baseball/ServiceTests.java
@@ -3,10 +3,13 @@ package baseball;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -51,5 +54,19 @@ public class ServiceTests {
         assertNotEquals(addDuplicatedString, addUnduplicatedString);
         assertEquals(2, addDuplicatedString.size());
         assertEquals(3, addUnduplicatedString.size());
+    }
+
+    @DisplayName("리스트에 이미 문자열이 있다면 추가하지 않는 메서드")
+    @Test
+    void generateRandomNumberTest() {
+        //given
+        Service service = new Service();
+        for(int loopCount = 0; 1000 > loopCount; loopCount++){
+            //when
+            List<String> randomNumbers = service.generateRandomNumber(1, 9, 3);
+            Set<String> deduplicateNumbers = new HashSet<>(randomNumbers);
+            //then
+            assertEquals(deduplicateNumbers.size(), 3);
+        }
     }
 }
