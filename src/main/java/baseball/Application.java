56907bdfd22a8ab6e4fd8e46982609441014650b
Jongha Park <jongha2788@u.sogang.ac.kr>
2022-11-07 10:50:57 +0900

    feat: 사용자 입력 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,14 +2,38 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
 
     private static final int finalRound = 3;
     private static final int numberLength = 3;
     private static int currentRound = 0;
+    private static String StartSymbol = "숫자 야구 게임을 시작합니다.";
+    private static String InputSymbol = "숫자를 입력해주세요 : ";
+    private static String EndSymbol = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"
+        + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+
     private static ArrayList<ArrayList<Integer>> number = new ArrayList<ArrayList<Integer>>(3);
 
+    public static int getInput() {
+        System.out.println(StartSymbol);
+        System.out.print(InputSymbol);
+        String userInput = Console.readLine();
+        if (!inputValidation(userInput))
+            throw new IllegalArgumentException();
+        return Integer.parseInt(userInput);
+    }
+
+    public static boolean inputValidation(String num){
+        try {
+
+        }
+        catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
 
     public static int getRandomNumber() {
         return Randoms.pickNumberInRange(1, 9);
@@ -33,6 +57,7 @@ public class Application {
             getRandomDifferentNumber();
             currentRound = currentRound + 1;
         }
+        getInput();
     }
 
     public static boolean numberDuplication(int newNum, int numberIndex,
@@ -48,10 +73,10 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        // gameInit();
-       //System.out.println(number.get(0));
-       // System.out.println(number.get(1));
-       // System.out.println(number.get(2));
+        gameInit();
+        //System.out.println(number.get(0));
+        // System.out.println(number.get(1));
+        // System.out.println(number.get(2));
 
     }
 }
