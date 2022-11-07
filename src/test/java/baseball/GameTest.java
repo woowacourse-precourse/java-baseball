58a4c7ee2aae4d73dabcb66f27ca1810e975875a
leeyongjun <leeyongjun0310@naver.com>
2022-11-07 15:38:02 +0900

    refactor(GameTest): Game 객체를 멤버 변수에서 한번만 선언하도록 수정

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -9,24 +10,23 @@ import static org.assertj.core.api.Assertions.*;
 
 public class GameTest {
 
+    private final Game game = new Game();
+
     @Test
     void restart_종료를_선택할때() {
         String input = "2";
-        Game game = new Game();
         assertThat(game.restart(input)).isFalse();
     }
 
     @Test
     void restart_재시작을_선택할때() {
         String input = "1";
-        Game game = new Game();
         assertThat(game.restart(input)).isTrue();
     }
 
     @Test
     void restart_여러개의_값을_입력할때() {
         String input = "123";
-        Game game = new Game();
         assertThatThrownBy(() -> game.restart(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -34,7 +34,6 @@ public class GameTest {
     @Test
     void restart_문자를_입력할때() {
         String input = "a";
-        Game game = new Game();
         assertThatThrownBy(() -> game.restart(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
@@ -43,7 +42,6 @@ public class GameTest {
     void getGameResult_볼이_없을때_게임_결과_반환_확인() {
         int ball = 0;
         int strike = 3;
-        Game game = new Game();
 
         assertThat(game.getGameResult(ball, strike)).isEqualTo("3스트라이크");
     }
@@ -52,7 +50,6 @@ public class GameTest {
     void getGameResult_스트라이크가_없을때_게임_결과_반환_확인() {
         int ball = 2;
         int strike = 0;
-        Game game = new Game();
 
         assertThat(game.getGameResult(ball, strike)).isEqualTo("2볼 ");
     }
@@ -61,7 +58,6 @@ public class GameTest {
     void getGameResult_스트라이크_볼_있을때_게임_결과_반환_확인() {
         int ball = 1;
         int strike = 2;
-        Game game = new Game();
 
         assertThat(game.getGameResult(ball, strike)).isEqualTo("1볼 2스트라이크");
     }
@@ -69,7 +65,6 @@ public class GameTest {
     @Test
     void isWin_졌을때_스트라이크가_목표에_달성했는지_확인() {
         int strike = 2;
-        Game game = new Game();
 
         assertThat(game.isWin(strike)).isFalse();
     }
@@ -77,7 +72,6 @@ public class GameTest {
     @Test
     void isWin_이겼을때_스트라이크가_목표에_달성했는지_확인() {
         int strike = 3;
-        Game game = new Game();
 
         assertThat(game.isWin(strike)).isTrue();
     }
@@ -88,7 +82,6 @@ public class GameTest {
         List<Integer> userNumber = Arrays.asList(1, 2, 3);
         List<Integer> computerNumber = Arrays.asList(1, 2, 3);
 
-        Game game = new Game();
         game.play(score, userNumber, computerNumber);
 
         assertThat(score.getBall()).isEqualTo(0);
@@ -101,7 +94,6 @@ public class GameTest {
         List<Integer> userNumber = Arrays.asList(2, 1, 3);
         List<Integer> computerNumber = Arrays.asList(1, 2, 3);
 
-        Game game = new Game();
         game.play(score, userNumber, computerNumber);
 
         assertThat(score.getBall()).isEqualTo(2);
