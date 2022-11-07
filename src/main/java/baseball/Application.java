7c8fcd5a7753a8d82b25074fbf9d408b53f41a57
odo27 <odo27@naver.com>
2022-11-07 22:43:31 +0900

    refactor(Application): add constructor

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,12 @@
 package baseball;
 
 public class Application {
+    private Player player;
+
+    public Application() {
+        player = new Player();
+    }
+
     public static void main(String[] args) {
         Game game = new Game();
         game.restartLoop();
