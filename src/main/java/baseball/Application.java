fd187a48db62f8ca3b25546fdb347cffe23ae400
BaeDongHyeon <sasd730@gmail.com>
2022-11-07 10:14:33 +0900

    refactor: 게임시작 문구 Game클래스로 이동

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,6 @@ import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
 
         List<Integer> computerNumber = getComputerNumber();
 
