e352ba26df025aa0330e10ef9993a7c77a4d245b
doehy <dbehxks02@naver.com>
2022-11-07 23:22:57 +0900

    feat : 볼의 개수를 세주는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,40 @@
 package baseball;
 
+import static baseball.Constant.*;
+
 public class Application {
+    public Computer computer;
+    public Gamer gamer;
+
+    public Application(Computer computer, Gamer gamer) {
+        this.computer = computer;
+        this.gamer = gamer;
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Computer computer = new Computer();
+        Gamer gamer = new Gamer();
+        Application application = new Application(computer, gamer);
+        System.out.print("숫자 야구 게임을 시작합니다.");
+        computer.makeRandomNumber();
+        while (true) {
+            application.gamer.inputNumber();
+            int ball_num = application.countBall();
+        }
+    }
+    public int countBall() {
+        int ball = 0;
+        if (computer.randomNumber.get(FIRST) == gamer.gamerNumber.get(SECOND) ||
+                computer.randomNumber.get(FIRST) == gamer.gamerNumber.get(THIRD)) {
+            ball++;
+        }
+        if (computer.randomNumber.get(SECOND) == gamer.gamerNumber.get(FIRST) ||
+                computer.randomNumber.get(SECOND) == gamer.gamerNumber.get(THIRD)) {
+            ball++;
+        }
+        if (computer.randomNumber.get(THIRD) == gamer.gamerNumber.get(FIRST) ||
+                computer.randomNumber.get(THIRD) == gamer.gamerNumber.get(SECOND)) {
+            ball++;
+        }
+        return ball;
     }
 }
--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -4,5 +4,8 @@ public class Constant{
     public static final int GAMEPLAY_SIZE = 3;
     public static final int START_NUM = 1;
     public static final int END_NUM = 9;
+    public static final int FIRST = 0;
+    public static final int SECOND = 1;
+    public static final int THIRD = 2;
 }
 
