0d5a7df0a1cd0d8cdd2c65a370930040bf728ed0
Kim Mi Ju <76895039+miju0515@users.noreply.github.com>
2022-11-07 22:46:11 +0900

    feat: 값 비교하기
    입력한 값과 컴퓨터 값을 비교해서 strike와 ball 카운트하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,22 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static List<Integer> computernumber = new ArrayList<>();
+    public static void checkstrikeball(List<Integer> personnumber){
+        int strikecount=0;
+        int ballcount=0;
+        for(int i=0;i<3;i++){
+            if(personnumber.get(i)==computernumber.get(i)){
+                strikecount++;
+            }else if(computernumber.contains(personnumber.get(i))){
+                ballcount++;
+            }
+        }
+        
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
