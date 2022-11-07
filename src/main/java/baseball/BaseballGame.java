8045615d483c04c24f172825f8e5d4a779c491b9
yujeongkwon <yoojeongkwon@naver.com>
2022-11-08 05:22:15 +0900

    feat(BaseballGame) : HintProvider() 힌트 제공

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,6 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class BaseballGame {
+    public static final int NUM_LENGTH = 3;
 
     public static List<Integer> ComputerNumGenerator(){
         List<Integer> num = new ArrayList<>();
@@ -17,10 +18,11 @@ public class BaseballGame {
         return num;
     }
 
+
     public static List<Integer> userInputValidator(String input) throws IllegalArgumentException{
         if(isThreelen(input) && isNotDuplicated(input) && isOneNineInt(input)){
             List<Integer> num = new ArrayList<>();
-            for(int i=0; i<3; i++){
+            for(int i=0; i<NUM_LENGTH; i++){
                 num.add(input.charAt(i)-'0');
             }
             return num;
@@ -40,6 +42,7 @@ public class BaseballGame {
         return inputSet.size() == listInput.size();
     }
 
+
     public static boolean isOneNineInt(String input){
         for(int i=0; i < input.length(); i++){
             char n = input.charAt(i);
@@ -48,4 +51,37 @@ public class BaseballGame {
         }
         return true;
     }
+
+
+    public static List<Integer> HintProvider(List<Integer> computerNum, List<Integer> userNum){
+        List<Integer> result = new ArrayList<>();
+        result.add(ballCount(computerNum,userNum));
+        result.add(strikeCount(computerNum,userNum));
+        return result;
+    }
+
+
+    public static int ballCount(List<Integer> computerNum, List<Integer> userNum) {
+        int count = 0;
+
+        for(int i=0; i<NUM_LENGTH; i++){
+            if (computerNum.get(i) != userNum.get(i) && computerNum.contains(userNum.get(i)))
+                count++;
+        }
+
+        return count;
+    }
+
+
+    public static int strikeCount(List<Integer> computerNum, List<Integer> userNum) {
+        int count = 0;
+
+        for(int i=0; i<NUM_LENGTH; i++){
+            if (computerNum.get(i) == userNum.get(i))
+                count++;
+        }
+
+        return count;
+    }
+
 }
