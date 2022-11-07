dfb80056aca04b412e949d45d6e0628eaf203bfe
devMuscle <hongsbien@naver.com>
2022-11-08 05:31:13 +0900

    refactor(generateNumberList): 사용자 입력 예외 처리

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -5,12 +5,26 @@ import java.util.List;
 
 public class User {
 
-    public List<Integer> generateNumberList(String numberString) {
+    public List<Integer> generateNumberList(String numberString) throws IllegalArgumentException {
+        if(numberString.length()!=3) {
+            throw new IllegalArgumentException("3자리를 입력하세요");
+        }
+
         List<Integer> numberList = new ArrayList<>();
         String[] numberStringArray = numberString.split("");
 
         for(String number : numberStringArray) {
-            numberList.add(Integer.parseInt(number));
+            try {
+                numberList.add(Integer.parseInt(number));
+            }catch (IllegalArgumentException illegalArgumentException) {
+                throw illegalArgumentException;
+            }
+        }
+
+        int nonDupplicateElementCnt = Long.valueOf(numberList.stream().distinct().count()).intValue();
+
+        if(nonDupplicateElementCnt!=numberList.size()) {
+            throw new IllegalArgumentException("중복된 원소 입력");
         }
 
         return numberList;
