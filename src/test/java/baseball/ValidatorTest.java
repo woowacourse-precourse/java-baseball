472a63509aae5731770576620fcd4cbd8cf904c4
dain-lee <dain9934@gmail.com>
2022-11-07 21:19:06 +0900

    test(validadtor): 입력한 수 검증 메서드 테스트 코드 추가

---
+++ src/test/java/baseball/ValidatorTest.java
@@ -0,0 +1,49 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidatorTest {
+
+    @Test
+    void 길이가_3_이상일_때_예외_발생() {
+        String input = "12";
+        assertThatThrownBy(() -> Validator.validateLength(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Validator.LENGTH_EXCEPTION);
+    }
+
+    @Test
+    void 길이가_3_이하일_때_예외_발생() {
+        String input = "1234";
+        assertThatThrownBy(() -> Validator.validateLength(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Validator.LENGTH_EXCEPTION);
+    }
+
+    @Test
+    void 숫자_1부터_9_이외의_숫자가_포함됐을_때_예외_발생() {
+        String input = "012";
+        assertThatThrownBy(() -> Validator.validateDigit(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Validator.DIGIT_EXCEPTION);
+    }
+
+    @Test
+    void 문자가_포함됐을_때_예외_발생() {
+        String input = "abc";
+        assertThatThrownBy(() -> Validator.validateDigit(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Validator.DIGIT_EXCEPTION);
+    }
+
+    @Test
+    void 중복_숫자가_포함됐을_때_예외_발생() {
+        String input = "112";
+        assertThatThrownBy(() -> Validator.validateNoDuplication(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Validator.DUPLICATION_EXCEPTION);
+    }
+}
