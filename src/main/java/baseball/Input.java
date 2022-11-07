0dbc39965902c2138100cb3d1612999a550452eb
Fizioo0102 <a27313797@gmail.com>
2022-11-07 23:31:55 +0900

    feat: computerRandomNumber()함수 생성
    - Randoms.pickNumberInRange()를 활용하여 1 - 9 범위의 랜덤 숫자 3개를 생성하는 함수 작성
    - contains()를 사용하여 이미 포함된 숫자는 제외하도록 구현

---
+++ src/main/java/baseball/Input.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Input {
+    static List<Integer> computerRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int random = Randoms.pickNumberInRange(1, 9);
+
+            if (!computer.contains(random)) {
+                computer.add(random);
+            }
+        }
+        return computer;
+    }
+
+}
