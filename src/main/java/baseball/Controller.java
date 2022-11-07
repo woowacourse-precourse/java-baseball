1391802f2fded6e9a31d5d45876b5fadfbb41c5a
YSEO <1park4170@gmail.com>
2022-11-07 16:56:00 +0900

    refactor<Controller> : declare new Game instance at the start of the while statement
    My intention is to clear instances after the one cycle of the game. But before, it's impossible because Controller holds Game instance in static area, so that Game instance never disappear until program end. So I forcefully added new instance to game variable, so that instance can get replace.
    
    Before
    : no game = new Game(); at the start of while
    After:
    game = new Game();

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,16 +1,13 @@
 package baseball;
 
 public class Controller {
-    private Game game;
-
-    public Controller() {
-        this.game = new Game();
-    }
+    private static Game game;
 
     public void play() {
         int retryChoice = 0;
 
         while (retryChoice != 2) {
+            game = new Game();
             game.startGame();
             retryChoice = game.askRegame();
         }
