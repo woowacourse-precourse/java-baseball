eceab2a0aef2269eea50a5418dbe0cc476781504
ensk26 <tjensk26@gmail.com>
2022-11-07 19:22:54 +0900

    feat(Application): 랜덤 번호를 생성하는 메서드
    새로운 메서드 추가 to Application
    - 랜덤 번호를 중복없이 생성해서 리스트로 반환해주는 메서드 추가
    - 랜덤 번호 3개를 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,9 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class Application {
@@ -45,5 +49,17 @@ public class Application {
 
     static boolean isDistinct(String number) {
         return number.length() != Stream.of(number.split("")).distinct().count();
+    }
+
+    static List<Integer> getRandomNumbers() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() != 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
     }
 }
