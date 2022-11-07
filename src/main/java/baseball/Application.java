19910b066d01f6bc4e69bc8e486ca8f4961b0c13
이건회 <rg970604@naver.com>
2022-11-08 03:16:11 +0900

    style(Application.java) : 필요없는 주석 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,6 @@ import baseball.strategy.RandomNumberProducingStrategy;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         NumberProducingStrategy numberProducingStrategy = new RandomNumberProducingStrategy();
         GamePlay.play(numberProducingStrategy);
     }
