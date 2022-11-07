452bd9f36060293f6b97ef26a8dfdb482320ea14
eyeofsol <kingjihong@naver.com>
2022-11-07 14:27:19 +0900

    test(InputCheck): hasSameNumber 테스트 작성
    모두 다른 수 인지 판별하는 메서드를 테스트하는 코드를 작성했습니다.

--- src/test/java/baseball/InputCheckTest.java
+++ src/test/java/baseball/InputCheckTest.java
@@ -18,4 +18,15 @@ class InputCheckTest {
         assertThat(InputCheck.hasNonDigit(str3)).isEqualTo(false);
 
     }
+
+    @Test
+    void hasSameNumber_모두_다른수인지_판별() {
+        String str1 = "123";
+        String str2 = "242";
+        String str3 = "999";
+
+        assertThat(InputCheck.hasNonDigit(str1)).isEqualTo(false);
+        assertThat(InputCheck.hasNonDigit(str2)).isEqualTo(true);
+        assertThat(InputCheck.hasNonDigit(str3)).isEqualTo(true);
+    }
 }
