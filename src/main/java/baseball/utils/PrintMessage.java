efc66bc5a7fd27866d9c31b56dfd444145160929
simba <shim9597@gmail.com>
2022-11-07 23:11:47 +0900

    feat: 게임 재시작 문구/ 게임 종료 문구 출력 기능 구현

--- src/main/java/baseball/utils/PrintMessage.java
+++ src/main/java/baseball/utils/PrintMessage.java
@@ -2,6 +2,13 @@ package baseball.utils;
 
 public class PrintMessage {
 
+    public void gameRestartOrNot() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+    public void gameFinished() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
     public void gameStartNotice() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
@@ -9,7 +16,7 @@ public class PrintMessage {
         System.out.println("숫자를 입력해 주세요 : ");
     }
 
-    public void Status(int ball, int strike) {
+    public void status(int ball, int strike) {
         if (ball != 0 && strike != 0) {
             System.out.println(ball + "볼 " + strike + "스트라이크");
         }
