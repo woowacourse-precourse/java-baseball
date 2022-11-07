203e1a7513531b776b8a3f92f90dff2aaa700b53
kim <kajinori@gachon.ac.kr>
2022-11-08 08:19:46 +0900

    rename(Constants) : 클래스명 GameConstants -> Constants 변경

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -1,6 +1,6 @@
 package baseball;
 
-public class GameConstants {
+public class Constants {
     public static final int NUMBER_LENGTH = 3;
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 9;
