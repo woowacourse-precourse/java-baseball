8e959f02f93a4296871c7e1916e4a6c3d6c08299
JeongHunHui <deca3248@gmail.com>
2022-11-08 01:10:50 +0900

    feat: 무작위 야구게임숫자를 얻는 메소드 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Computer {
+  private BaseBallGameNumber gameNumber;
+
+  public void setAnswerNumber() {
+    List<Integer> numList = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+    gameNumber.setGameNumberByList(numList);
+  }
+
+  public BaseBallGameNumber getAnswerNumber() {
+    return gameNumber;
+  }
+}
