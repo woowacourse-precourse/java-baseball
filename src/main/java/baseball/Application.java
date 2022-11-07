16c5d66b03c687e89d3766ff4ee9968db8612731
hoin <apicad0307@gmail.com>
2022-11-07 22:44:15 +0900

    feat: 출력문 함수로 추출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,42 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
         Randoms.pickNumberInRange(1, 9);
+
+        startGamePrint();
+        //while
+        while(true){
+            //컴퓨터 수 생성
+
+            getNumberPrint();
+            gameOverPrint();
+            gameContinuePrint();
+
+
+
+        }
+
     }
+
+    private static void getNumberPrint() {
+        System.out.print("숫자를 입력해주세요. : ");
+    }
+
+    private static void gameOverPrint() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+    }
+
+    private static void gameContinuePrint() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    private static void startGamePrint() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
 }
