41a96ece3a9bb8cef8d3f314cb82769cc777876b
Fizioo0102 <a27313797@gmail.com>
2022-11-07 23:40:00 +0900

    feat: userNumberErrorCheck() 함수 생성
    - 앞서 만든 userNumberLengthCheck(),userNumberRepeatCheck(),userNumberRangeCheck() 에서 false 를 반환 한다면, IllegalArgumentException 에러를 뱉어주는 함수 생성

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -52,5 +52,17 @@ public class Input {
         return true;
     }
 
+    static void userNumberErrorCheck(String input) {
+        if (!userNumberLengthCheck(input)) {
+            throw new IllegalArgumentException("입력된 수의 길이가 올바르지 않습니다");
+        }
+        if (!userNumberRepeatCheck(input)) {
+            throw new IllegalArgumentException("중복된 수가 입력되었습니다");
+        }
+        if (!userNumberRangeCheck(input)) {
+            throw new IllegalArgumentException("올바른 수의 범위 입력이 아닙니다");
+        }
+    }
+
 
 }
