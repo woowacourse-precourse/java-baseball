77a7f439c13b7342482ba12f44242e1f0bdedc70
jeongjihun <wlgns2234@naver.com>
2022-11-07 23:37:14 +0900

    update: Game클래스 메소드 접근제어자 변경
    Game 클래스 내부 restartGame, endGame 의 접근제어자를 public 에서 private로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,7 +12,7 @@ class Game {
     public final int RESTART = 1;
     public final int END = 2;
 
-    public void restartGame(int userInputNum) {
+    private void restartGame(int userInputNum) {
         if (userInputNum == RESTART)
             System.out.println("게임시작함수");
 
@@ -20,14 +20,12 @@ class Game {
             System.out.println("게임종료함수");
     }
 
+    private void endGame() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 
     public void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
-    }
-
-
-    public void endGame() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
 }
@@ -88,7 +86,6 @@ public class Application {
     public static void main(String[] args) {
         Game game = new Game();
         game.startGame();
-
 
 
         //game.end();
