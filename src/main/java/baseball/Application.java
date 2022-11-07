1cd2bc1f20cbeda0108d8670e9b1fca8adecfbc0
hdsung98 <hdsung98@gmail.com>
2022-11-08 05:07:55 +0900

    fix: fix console input poisition error
    숫자를 입력해주세요 부분을 println을 사용하는 바람에 입력커서가 해당문장 아래에 위치하는 문제가 있었다.
    print로 수정하여 해결했음.
    자동정렬도 수행함.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,26 +2,28 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashSet;
 
 public class Application {
     static void runLoop() {
-        RandomNumberGenerator answer= new RandomNumberGenerator();
+        RandomNumberGenerator answer = new RandomNumberGenerator();
         Guess myGuess;
         BallStrikeChecker checker;
         ResultBoard board;
 
-        do{
-            System.out.println("숫자를 입력해주세요 : ");
-            myGuess= new Guess(Console.readLine());
-            checker = new BallStrikeChecker(answer,myGuess);
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            myGuess = new Guess(Console.readLine());
+            checker = new BallStrikeChecker(answer, myGuess);
             board = new ResultBoard(checker.judgement);
             board.showResult();
-        }while(!checker.threeStrike);
+        } while (!checker.threeStrike);
     }
-    static void endOrRestart()  {
+
+    static void endOrRestart() {
         NextBehavior next;
 
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
@@ -36,10 +38,12 @@ public class Application {
             return;
         }
     }
+
     static void initializeAndStartGame() {
         runLoop();
         endOrRestart();
     }
+
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         initializeAndStartGame();
