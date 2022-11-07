a69c12c08818c489340374c009b5139d2fcb00b1
Dasol Jung <greteljds0251@gmail.com>
2022-11-07 13:18:04 +0900

    feat: Pick 3 random numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+}
+
+class Baseball {
+    private List<Integer> computerRandom = new ArrayList<>();
+    private List<Integer> playerNumber = new ArrayList<>();
+    private List<Integer> baseballCount = new ArrayList<>();
+
+    public void pickRandomNumber() {
+        while (computerRandom.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerRandom.contains(randomNumber)) {
+                computerRandom.add(randomNumber);
+            }
+        }
     }
 }
