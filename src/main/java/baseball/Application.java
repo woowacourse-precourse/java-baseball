480f217d183af576ad0fbfb2b20c62716e8b2886
jsc1014 <wotlr5209@gmail.com>
2022-11-07 17:15:57 +0900

    feat : 컴퓨터의 무작위 숫자 생성하기 1~9 사이의 서로 다른 숫자 3개

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
     private static void gameStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+    private static List<Integer> makeRandomAnswer(){
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+
+        }
+
+        return computer;
     }
 }
