cf3b6f581c28bb0a3d26dff18457fe370b876fe1
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-11-07 17:29:34 +0900

    Test(DividedRandomNumberElementListMakerTest) : NumberListMaker -> ElementListMaker로 클래스명 변경에 따라 내부 클래스명 변경

--- src/test/java/baseball/DividedRandomNumberElementListMakerTest.java
+++ src/test/java/baseball/DividedRandomNumberElementListMakerTest.java
@@ -7,8 +7,8 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
-class DividedRandomNumberListMakerTest {
-    DividedRandomNumberListMaker listMaker = new DividedRandomNumberListMaker();
+class DividedRandomNumberElementListMakerTest {
+    DividedRandomNumberElementListMaker listMaker = new DividedRandomNumberElementListMaker();
 
     @Test
     public void isNumberStrangerOnNumberList_메서드로_숫자가_numberList에_존재하는지_구분() throws Exception {
@@ -26,12 +26,24 @@ class DividedRandomNumberListMakerTest {
     @Test
     public void makeNumberList로_만들어진_리스트에_중복된_숫자가_있는지_확인() throws Exception {
         //given
-        List<Integer> numberList = listMaker.makeNumberList(3);
+        List<Integer> numberList = listMaker.makeElementList(3);
 
         //when
         HashSet<Integer> numberSet = new HashSet<>(numberList);
 
         //then
         assertThat(numberSet.size()).isEqualTo(numberList.size());
+    }
+
+    @Test
+    public void convertToElementList_메서드로_String타입의_숫자를_각_자리별_숫자리스트로_변환() throws Exception {
+        //given
+        String stringNumber = "123";
+
+        //when
+        List<Integer> elementList = listMaker.convertToElementList(stringNumber);
+
+        //then
+        assertThat(elementList).isEqualTo(List.of(1, 2, 3));
     }
 }
