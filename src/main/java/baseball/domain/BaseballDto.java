c554875f1ac2e9e4baf1aee4772d7ac3eeedc6fa
kariskan <zkfzpf56@naver.com>
2022-11-07 15:44:41 +0900

    feat: ball과 strike를 관리하는 BaseballDto 클래스 생성

---
+++ src/main/java/baseball/domain/BaseballDto.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+public class BaseballDto {
+
+    private int ball;
+    private int strike;
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+}
