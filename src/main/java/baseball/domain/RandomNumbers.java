c6caf9c624afd2e6ad56a090ad7185cd2441d014
parkjungyoon <tbr06057@naver.com>
2022-11-07 22:38:19 +0900

    refactor: 랜덤 3자리 숫자를 만드는 메서드를 클래스로 분리

---
+++ src/main/java/baseball/domain/RandomNumbers.java
@@ -0,0 +1,32 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumbers {
+    private final List<Integer> numbers;
+    public RandomNumbers() {
+        this.numbers = generateComputerRandomNumbers();
+    }
+
+    private List<Integer> generateComputerRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+    public int get(int i) {
+        return numbers.get(i);
+    }
+
+    public int indexOf(int value) {
+        return numbers.indexOf(value);
+    }
+}
