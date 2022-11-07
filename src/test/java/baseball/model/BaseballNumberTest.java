842200224359dd632a799dc9c021f4236cb92e32
whdcks420 <whdcks420@gmail.com>
2022-11-07 23:48:31 +0900

    test: 베이스볼 생성 테스트

--- src/test/java/baseball/model/BaseballNumberTest.java
+++ src/test/java/baseball/model/BaseballNumberTest.java
@@ -1,4 +1,25 @@
+package baseball.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class BaseballNumberTest {
-  
+
+    @Test
+    @DisplayName("베이스볼 넘버 생성")
+    void createBaseballNumber() throws NoSuchFieldException, IllegalAccessException {
+        BaseballNumber baseballNumber = new BaseballNumber();
+
+        Field answer = BaseballNumber.class.getDeclaredField("answer");
+        answer.setAccessible(true);
+        String answerTest = (String) answer.get(baseballNumber);
+        assertAll(
+                () -> assertNotNull(baseballNumber),
+                () -> assertEquals(3, answerTest.length())
+        );
+    }
 }
