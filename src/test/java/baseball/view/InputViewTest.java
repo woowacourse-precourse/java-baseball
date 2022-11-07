788f3ce19841ee484c888b1d01e798a1d31221c1
iamjooon2 <iamjooon2@gmail.com>
2022-11-08 02:19:19 +0900

    fix: InputViewTest의 메서드에 출력을 확인하는 케이스 추가

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -1,11 +1,14 @@
 package baseball.view;
 
 import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.io.PrintStream;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class InputViewTest {
+    private static ByteArrayOutputStream outputMessage;
 
     @Test
     void 숫자_입력_묻기() {
@@ -14,9 +17,14 @@ public class InputViewTest {
         InputStream in = new ByteArrayInputStream(expectedInputNumber.getBytes());
         System.setIn(in);
 
+        outputMessage = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputMessage));
+
         String actualInputNumber = InputView.askNumberMessage();
 
+        Assertions.assertThat(outputMessage.toString()).isEqualTo("숫자를 입력해주세요: ");
         Assertions.assertThat(actualInputNumber).isEqualTo(expectedInputNumber);
+
     }
 
 
@@ -27,8 +35,12 @@ public class InputViewTest {
         InputStream in = new ByteArrayInputStream(expectedInputNumber.getBytes());
         System.setIn(in);
 
+        outputMessage = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputMessage));
+
         String actualInputNumber = InputView.askRestartMessage();
 
+        Assertions.assertThat(outputMessage.toString()).isEqualTo("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
         Assertions.assertThat(actualInputNumber).isEqualTo(expectedInputNumber);
     }
 }
