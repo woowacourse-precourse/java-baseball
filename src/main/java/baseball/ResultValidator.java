a0afec628f41d7fe1feae432cf56dc64193a6af4
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-07 11:14:41 +0900

    feat(ResultValidator) : countStrike 메서드 추가
    strike의 개수를 센다

--- src/main/java/baseball/ResultValidator.java
+++ src/main/java/baseball/ResultValidator.java
@@ -6,7 +6,18 @@ import java.util.List;
 public class ResultValidator {
     public List<Integer> validate(String input, String createdNumber) {
         List<Integer> result = new ArrayList<>();
+        int strike = countStrike(input, createdNumber);
 
         return result;
+    }
+
+    private int countStrike(String input, String createdNumber) {
+        int strike = 0;
+        for (int i = 0; i < 3; i++) {
+            if (input.charAt(i) == createdNumber.charAt(i)) {
+                strike = strike + 1;
+            }
+        }
+        return strike;
     }
 }
