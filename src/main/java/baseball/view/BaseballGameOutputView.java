28d1874c54c9aec52911a7a21de918801830e045
Qulip <ilkwon.ryu@gmail.com>
2022-11-08 05:41:42 +0900

    feat: BaseballGameOutputView 볼카운트 출력 추가

--- src/main/java/baseball/view/BaseballGameOutputView.java
+++ src/main/java/baseball/view/BaseballGameOutputView.java
@@ -2,9 +2,15 @@ package baseball.view;
 
 import static baseball.view.BaseballGameConstant.START_GAME_MESSAGE;
 
+import baseball.domain.BallCount;
+
 public class BaseballGameOutputView {
 
     public static void gameStart() {
         System.out.println(START_GAME_MESSAGE);
+    }
+
+    public static void printBallCount(BallCount ballCount) {
+        System.out.println(ballCount.toString());
     }
 }
