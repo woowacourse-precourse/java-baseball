3ef5ed3c1d7ee3fba0156587a0b5bbb2a809aa4c
limzzum <chfhddl1234@naver.com>
2022-11-08 01:28:53 +0900

    test : runMain try/catch 제거

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,129 +1,122 @@
-//package baseball;
-//
-//import camp.nextstep.edu.missionutils.Randoms;
-//import camp.nextstep.edu.missionutils.test.NsTest;
-//import org.assertj.core.api.Assertions;
-//import org.assertj.core.api.ThrowableAssert;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.DisplayName;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.params.provider.ValueSource;
-//
-//import java.io.BufferedInputStream;
-//import java.io.ByteArrayInputStream;
-//import java.io.InputStream;
-//import java.io.PrintStream;
-//import java.lang.reflect.Field;
-//import java.lang.reflect.InvocationTargetException;
-//import java.lang.reflect.Method;
-//import java.nio.charset.StandardCharsets;
-//import java.util.*;
-//
-//import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-//import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-//import static org.assertj.core.api.Assertions.*;
-//
-//class ApplicationTest extends NsTest {
-//
-//    @Test
-//    @DisplayName("랜덤숫자가 서로다른 숫자를 가지고 있는지 확인")
-//    void createRandomTargetNum(){
-//        Application.createRandomTargetNum();
-//        Set<Integer> set = new HashSet<>(Application.targetNum);
-//
-//        Assertions.assertThat(set.size()).isEqualTo(Application.targetNum.size());
-//    }
-//
-//    @Test
-//    @DisplayName("랜덤숫자가 3개 선택되었는지 확인")
-//    void RandomNumSize(){
-//        Application.createRandomTargetNum();
-//
-//        Assertions.assertThat(Application.targetNum.size()).isEqualTo(3);
-//    }
-//
-//    @Test
-//    @DisplayName("사용자 잘못된 값 입력시 오류 발생")
-//    void badInputException(){
-//        Assertions.assertThatThrownBy(()->
-//                Application.inputValidate(List.of("1234")))
-//                        .isInstanceOf(IllegalArgumentException.class);
-//
-//        Assertions.assertThatThrownBy(()->
-//                        Application.inputValidate(List.of("112")))
-//                .isInstanceOf(IllegalArgumentException.class);
-//
-//        Assertions.assertThatThrownBy(()->
-//                        Application.inputValidate(List.of("103")))
-//                .isInstanceOf(IllegalArgumentException.class);
-//
-//
-//    }
-//
-//    @Test
-//    @DisplayName("1을 입력하면 게임 재시작 , 2는 종료")
-//    void restartOrEnd() throws Exception {
-//        String data = String.valueOf(Randoms.pickNumberInList(List.of(1,2)));
-//        InputStream in = new ByteArrayInputStream(data.getBytes());
-//        System.setIn(in);
-//
-//        Application app= new Application();
-//        Method endGame = app.getClass().getDeclaredMethod("endGame");
-//        endGame.setAccessible(true);
-//
-//        if(data.equals("1")){
-//            Assertions.assertThat(endGame.invoke(app)).isEqualTo(false);
-//        }
-//        if(data.equals("2")){
-//            Assertions.assertThat(endGame.invoke(app)).isEqualTo(true);
-//        }
-//
-//    }
-//
-//    @Test
-//    @DisplayName("게임 재시작 혹은 종료시 입력값 검증")
-//    void optionValidate() throws Exception {
-//        Application app = new Application();
-//        Method optionValidate = app.getClass().getDeclaredMethod("optionValidate", int.class);
-//        optionValidate.setAccessible(true);
-//
-//        Assertions.assertThatThrownBy(()->
-//                 optionValidate.invoke(app, 0))
-//                        .isInstanceOf(InvocationTargetException.class)
-//                                .getRootCause().isInstanceOf(IllegalArgumentException.class);
-//    }
-//
-//
-//    @Test
-//    void 게임종료_후_재시작() {
-//        assertRandomNumberInRangeTest(
-//                () -> {
-//                    run("246", "135", "1", "597", "589", "2");
-//                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-//                },
-//                1,  3, 5, 5, 8, 9
-//        );
-//    }
-//
-//    @Test
-//    @Disabled
-//    void 예외_테스트() {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.ThrowableAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.*;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.*;
+
+class ApplicationTest extends NsTest {
+
+    @Test
+    @DisplayName("랜덤숫자가 서로다른 숫자를 가지고 있는지 확인")
+    void createRandomTargetNum(){
+        Application.createRandomTargetNum();
+        Set<Integer> set = new HashSet<>(Application.targetNum);
+
+        Assertions.assertThat(set.size()).isEqualTo(Application.targetNum.size());
+    }
+
+    @Test
+    @DisplayName("랜덤숫자가 3개 선택되었는지 확인")
+    void RandomNumSize(){
+        Application.createRandomTargetNum();
+
+        Assertions.assertThat(Application.targetNum.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("사용자 잘못된 값 입력시 오류 발생")
+    void badInputException(){
+        Assertions.assertThatThrownBy(()->
+                Application.inputValidate(List.of("1234")))
+                        .isInstanceOf(IllegalArgumentException.class);
+
+        Assertions.assertThatThrownBy(()->
+                        Application.inputValidate(List.of("112")))
+                .isInstanceOf(IllegalArgumentException.class);
+
+        Assertions.assertThatThrownBy(()->
+                        Application.inputValidate(List.of("103")))
+                .isInstanceOf(IllegalArgumentException.class);
+
+
+    }
+
+    @Test
+    @DisplayName("1을 입력하면 게임 재시작 , 2는 종료")
+    void restartOrEnd() throws Exception {
+        String data = String.valueOf(Randoms.pickNumberInList(List.of(1,2)));
+        InputStream in = new ByteArrayInputStream(data.getBytes());
+        System.setIn(in);
+
+        Application app= new Application();
+        Method endGame = app.getClass().getDeclaredMethod("endGame");
+        endGame.setAccessible(true);
+
+        if(data.equals("1")){
+            Assertions.assertThat(endGame.invoke(app)).isEqualTo(false);
+        }
+        if(data.equals("2")){
+            Assertions.assertThat(endGame.invoke(app)).isEqualTo(true);
+        }
+
+    }
+
+    @Test
+    @DisplayName("게임 재시작 혹은 종료시 입력값 검증")
+    void optionValidate() throws Exception {
+        Application app = new Application();
+        Method optionValidate = app.getClass().getDeclaredMethod("optionValidate", int.class);
+        optionValidate.setAccessible(true);
+
+        Assertions.assertThatThrownBy(()->
+                 optionValidate.invoke(app, 0))
+                        .isInstanceOf(InvocationTargetException.class)
+                                .getRootCause().isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @Test
+    void 게임종료_후_재시작() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "1", "597", "589", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                1,  3, 5, 5, 8, 9
+        );
+    }
+
+    @Test
+    void 예외_테스트() {
 //        assertSimpleTest(() ->
 //                assertThatThrownBy((ThrowableAssert.ThrowingCallable) catchThrowable(() -> runException("1234")))
 //                        .isInstanceOf(Exception.class)
 //        );
-//
-//
-//    }
-//
-//    @Override
-//    public void runMain() {
-//        try {
-//            Application.main(new String[]{});
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//    }
-//}
+        Assertions.assertThatThrownBy(()-> runException("1234"))
+                .isInstanceOf(Exception.class);
+
+
+    }
+
+    @Override
+    public void runMain(){
+        Application.main(new String[]{});
+
+    }
+}
