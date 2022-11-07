1f5d195de1a27dc1bddd22772b58146d6af69d93
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-07 18:49:55 +0900

    feat:static value and default constructor

---
+++ src/main/java/baseball/Exception/SystemException.java
@@ -0,0 +1,12 @@
+package baseball.Exception;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SystemException {
+    private static final String Restart_OR_Exit = "[1-2]";
+
+    public SystemException() {
+    }
+}
