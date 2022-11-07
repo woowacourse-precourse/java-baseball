cb5d5e1001791baabd83c31fabeb0adff6934bef
devMuscle <hongsbien@naver.com>
2022-11-08 03:59:14 +0900

    refactor: printResult method 변경

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -13,19 +13,24 @@ public class Result {
         this.ball = ballCnt;
     }
 
+    public Integer getStrike() {
+        return strike;
+    }
+
     public void printResult() {
         if(ball>0) {
             System.out.print(ball + "볼 ");
         }
         if(strike>0) {
-            System.out.println(strike + "스트라이크");
-
-            if(strike==3) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            }
+            System.out.print(strike + "스트라이크");
         }
         if(ball==0 && strike==0) {
-            System.out.println("낫싱");
+            System.out.print("낫싱");
+        }
+        System.out.println();
+
+        if(strike==3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
     }
 }
