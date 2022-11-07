1b28bfa276b922fddae55b6867eff91c1c122859
Jeangyu <gjwlsrb1700@naver.com>
2022-11-07 17:45:45 +0900

    [REFACTOR] 랜덤 함수 Constants화 작업 진행

--- src/main/java/baseball/random/NumberGenerator.java
+++ src/main/java/baseball/random/NumberGenerator.java
@@ -5,17 +5,17 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashSet;
 import java.util.Set;
 
+import static baseball.utils.BaseballConstants.*;
+
 public class NumberGenerator {
 
-    public static final int NUMBER_SIZE = 3;
-    public static final int MIN_NUMBER = 1;
-    public static final int MAX_NUMBER = 9;
+
 
     public static String generateAnswerNumber(){
         String result = "";
         Set<Integer> nums = new HashSet<>();
 
-        while (nums.size() < NUMBER_SIZE){
+        while (nums.size() < BALL_NUMBER){
             int rand_num = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             nums.add(rand_num);
         }
--- src/main/java/baseball/utils/BaseballConstants.java
+++ src/main/java/baseball/utils/BaseballConstants.java
@@ -5,4 +5,6 @@ public class BaseballConstants {
     public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
     public static final String PLAY_AGAIN = "1";
     public static final int BALL_NUMBER = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
 }
