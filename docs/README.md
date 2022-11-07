8c2b3232da23ec303b884c68e2a875d224956b4c
impati <impati0716@icloud.com>
2022-11-07 21:19:08 +0900

    docs : 기능 목록 문서 작성

---
+++ docs/README.md
@@ -0,0 +1,9 @@
+# 기능 목록
+### 1.executionGame : 하나의 게임 단위 함수
+### 2.continueGame :  게임을 새로 시작할지 종료할지 결정하는 함수
+### 3.decision : 서로 다른 3개의 랜덤 수 저장 함수
+### 4.validationAndConvert : 입력에 대한 검증과 List<Integer>로 반환 함수
+### 5.calculationBall : 입력에 대한 볼 계산 함수
+### 6.calculationStrike : 입력에 대한 스트라이크 계산
+### 7.currentSituation : 입력에 대한 결과를 문자열 형태로 반환
+### 8.isCorrect : 정답을 맞추었는지 결과 반환 함수 
--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,17 +10,7 @@ import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
 
-/**
- * 기능 목록
- *  - executionGame : 하나의 게임 단위
- *  - continueGame :  게임을 새로 시작할지 종료할지 결정하는 함수
- *  - decision : 서로 다른 3개의 랜덤 수 저장
- *  - validationAndConvert : 입력에 대한 검증과 List<Integer>로 반환
- *  - calculationBall : 볼 계산
- *  - calculationStrike : 스트라이크 계산
- *  - currentSituation : 입력에 대한 결과를 문자열 형태로 반환
- *  - isCorrect : 정답을 맞추었는지
- */
+
 public class Application {
     public static void main(String[] args) {
 
