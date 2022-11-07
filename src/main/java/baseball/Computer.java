3b10f83c11162041fe79105b2d7187f879b50eab
bnbnac <bnbnac@naver.com>
2022-11-07 19:15:04 +0900

    feat: 컴퓨터 객체가 플레이어의 숫자를 입력받아, 저장된 숫자와 비교하여 힌트 반환

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -27,9 +27,45 @@ public class Computer {
         }
     }
 
-    public List<String> giveHint (List<String> playerAnswer) {
+    public List<String> giveHint(List<String> playerAnswer) {
         List<String> ballAndStrike = new ArrayList<>();
 
+        Integer ball = getBall(playerAnswer);
+        Integer strike = getStrike(playerAnswer);
+
+        ballAndStrike.add(Integer.toString((ball - strike)));
+        ballAndStrike.add(Integer.toString(strike));
+
         return ballAndStrike;
+    }
+
+    public Integer getBall(List<String> playerAnswer) {
+        Integer ball = 0;
+        for (String digit : playerAnswer) {
+            ball += contained(digit);
+        }
+        return ball;
+    }
+
+    public Integer contained(String digit) {
+        if(this.computerAnswer.contains(digit)) {
+            return 1;
+        }
+        return 0;
+    }
+
+    public Integer getStrike(List<String> playerAnswer) {
+        Integer strike = 0;
+        for (int i = 0; i < 3; i++) {
+            strike += correspond(playerAnswer, i);
+        }
+        return strike;
+    }
+
+    public Integer correspond(List<String> playerAnswer, int i) {
+        if (playerAnswer.get(i).equals(this.computerAnswer.get(i))) {
+            return 1;
+        }
+        return 0;
     }
 }
