ee146c450bab7bdf2214598895f219daf78b2213
이건회 <rg970604@naver.com>
2022-11-08 04:36:39 +0900

    style(BallCount.java) : 포맷팅

--- src/main/java/baseball/domain/BallCount.java
+++ src/main/java/baseball/domain/BallCount.java
@@ -6,8 +6,7 @@ public class BallCount {
     private final int MAX_STRIKE_COUNT = 3;
     private final int MIN_BALL_COUNT = 0;
     private final int MAX_BALL_COUNT = 3;
-
-    private final int NO_COUNT = 0 ;
+    private final int NO_COUNT = 0;
     private int Strikes;
     private int Balls;
 
