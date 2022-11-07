63aa60d245da66f8f0913bfb65027925d44b43aa
qdtjj4 <76ylo1@gmail.com>
2022-11-08 02:44:11 +0900

    feat: 컴퓨터의 숫자를 생성하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         startGame();
+        List<Integer> computerNumber = generateComputerNumber();
     }
 
     static void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    static List<Integer> generateComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+        return computerNumber;
     }
 }
