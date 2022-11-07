4349d37747fda9f6150cea6f2d33fcc3ee64333b
seojinvely1004 <seojin0124@naver.com>
2022-11-07 18:59:54 +0900

    feat: create progressBaseball()method

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -19,6 +19,36 @@ public class Baseball {
 			}
 		}
 	}
+	private void progressBaseball() {
+		int[] computerNumbers = createComputerNumbers();
+
+		while (true) {
+			int strike = 0;
+			int ball = 0;
+
+			System.out.print("숫자를 입력해주세요 : ");
+
+			int[] userInput = getThreeDigitInput();
+
+			for (int i = 0; i < 3; i++) {
+				String result = checkNumber(computerNumbers, userInput[i], i);
+
+				if (result.equals("스트라이크")) {
+					strike++;
+				} else if (result.equals("볼")) {
+					ball++;
+				}
+			}
+
+			System.out.println(makeMessage(strike, ball));
+
+			if (strike == 3) {
+				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+				break;
+			}
+		}
+
+	}
 
 	
 }
