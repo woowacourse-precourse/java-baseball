03b83f4c0006feb0e9e9ac24748d541a6023cf01
baekeunsun <eunsun5460@gmail.com>
2022-11-07 19:06:58 +0900

    fix(view/OutputView): if 조건 명확하게 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -13,16 +13,15 @@ public class OutputView {
         if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
         }
-        if (strike == 3) {
-            System.out.println("3스트라이크");
-        }
         if (strike == 0) {
             System.out.println(ball+"볼");
         }
         if (ball == 0) {
             System.out.println(strike+"스트라이크");
         }
-        System.out.println(ball+"볼 "+strike+"스트라이크");
+        if (strike != 0 && ball != 0) {
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
     }
 
 }
