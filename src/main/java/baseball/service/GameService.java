530da0fb6d3db59d69074c1d47021382710abd53
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-07 16:49:11 +0900

    221107 Refactor: 기능3 - 매직넘버를 상수로 선언

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -8,19 +8,23 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class GameService {
 
+    private final int MAX_COUNT = 3;
+    private final int MAX_VALUE = 9;
+    private final int MIN_VALUE = 1;
+
     public void validate(Game game){
         List<Integer> numbers = game.getNumbers();
 
-        if(numbers.size() < 3) {
+        if(numbers.size() < MAX_COUNT) {
             throw new IllegalArgumentException("3개 미만의 숫자를 입력할 수 없습니다.");
         }
 
-        if(numbers.size() > 3) {
+        if(numbers.size() > MAX_COUNT) {
             throw new IllegalArgumentException("3개 초과의 숫자를 입력할 수 없습니다.");
         }
 
         for(int num : game.getNumbers()){
-            if (num < 1 || num > 9) {
+            if (num < MIN_VALUE || num > MAX_VALUE) {
                 throw new IllegalArgumentException("1에서 9 사이의 숫자만 입력할 수 있습니다.");
             }
         }
@@ -46,5 +50,10 @@ public class GameService {
         game.setNumbers(list);
 
         return game;
+    }
+
+    public boolean compareAnswer(Game answer, Game userInput) {
+
+        return true;
     }
 }
