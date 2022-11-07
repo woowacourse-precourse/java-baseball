217206042048904d87c300c880155dc4cbe8807f
jaehyeon <jhjh0022@naver.com>
2022-11-08 02:14:38 +0900

    refactor: 검증클래스 검증 메서드들 void 수정으로 인한 수정

--- src/main/java/baseball/Input/InputAndVerification.java
+++ src/main/java/baseball/Input/InputAndVerification.java
@@ -1,26 +1,22 @@
 package baseball.Input;
 
-import static baseball.Input.InputNumberVerification.verifyNumber;
-import static baseball.Input.InputNumberVerification.verifyOneOrTwoButton;
-
 public class InputAndVerification {
     InputNumber inputNumber = new InputNumber();
+    InputNumberVerification inputNumberVerification = new InputNumberVerification();
 
     public int receiveAndVerifyNumber() {
         int receivedNumber = inputNumber.getInputNumber();
 
-        if (!verifyNumber(receivedNumber)) {
-            throw new IllegalArgumentException("형식에 맞게 입력하세요");
-        }
+        inputNumberVerification.verifyNumber(receivedNumber);
+
         return receivedNumber;
     }
 
     public int receiveAfterGameEndOneOrTwo() {
         int receivedButtonNumber = inputNumber.getRestartAndEndNumber();
 
-        if (!verifyOneOrTwoButton(receivedButtonNumber)) {
-            throw new IllegalArgumentException("1, 2 둘 중 하나만 골르세요");
-        }
+        inputNumberVerification.verifyOneOrTwoButton(receivedButtonNumber);
+
         return receivedButtonNumber;
     }
 }
