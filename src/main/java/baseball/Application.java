e862c804d652ff7cae714409e9089a212c97d569
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-11-07 12:36:29 +0900

    feat(Application): define playBall method and set up dummy loops

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
 public class Application {
+    private static boolean userWantsToPlay = true;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: greet user
+        while(userWantsToPlay) {
+            playBall();
+        }
+    }
+
+    private static void playBall() {
+        // TODO: getRandomNumber
+        boolean strikeOut = false;
+        while (!strikeOut) {
+            // TODO: promptForNumber
+            // TODO: getNumberFrom
+            // TODO: getScore
+            // TODO: printScore
+            // TODO: update strikeOut
+            strikeOut = true;
+        }
+        // TODO: congratulate user
+        // TODO: update user willingness
+        userWantsToPlay = false;
     }
 }
