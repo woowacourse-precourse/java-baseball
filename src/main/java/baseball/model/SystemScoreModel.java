9844e2b89f203a1a0a50a442c1422c6d788fe780
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-07 18:44:53 +0900

    feat: value and constructor

---
+++ src/main/java/baseball/model/SystemScoreModel.java
@@ -0,0 +1,14 @@
+package baseball.model;
+
+public enum SystemScoreModel {
+    STRIKE("스트라이크"),
+    BALL("볼"),
+    NOTHING("낫싱"),
+    ZERO("");
+
+    private final String score;
+
+    SystemScoreModel(final String score) {
+        this.score = score;
+    }
+}
