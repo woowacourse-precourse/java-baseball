098fdc00d62443594189259fa853ce785a15248f
eyeofsol <kingjihong@naver.com>
2022-11-07 14:58:41 +0900

    chore(Application): flag 설정과 안쓰는 input 변수 삭제
    flag를 continueOrEnd 메서드로 초기화해 입력값에 따라 재시작을 결정하게 하였고, 안쓰는 변수를 삭제했습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,12 +4,12 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String input;
         int flag = 1;
 
         while (flag == 1) {
             doGame();
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            flag = continueOrEnd();
         }
     }
 
