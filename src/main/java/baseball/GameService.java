1337bee3e13cc955d29b47fc9cc37d0757b98ae8
Sehoon Kim <shggm2000@gmail.com>
2022-11-07 23:25:41 +0900

    유저의 정보를 요청하고 입력 받는 기능 구현 컴퓨터의 랜덤값을 형성하는 기능 구현, 정보를 입력 받거나 형성할때 문자열로 받은 값을 parsing하는 기능 구현

--- src/main/java/baseball/GameService.java
+++ src/main/java/baseball/GameService.java
@@ -1,14 +1,43 @@
 package baseball;
 
 import baseball.domain.Game;
+import baseball.domain.User;
+import baseball.utils.Parser;
 import baseball.utils.RandomUtils;
+import baseball.view.SystemMessage;
+import baseball.view.RequestInput;
+import camp.nextstep.edu.missionutils.Console;
 
 public class GameService {
     int size;
     Game game;
+    User user = new User();
+    SystemMessage systemMessage = new SystemMessage();
 
     public void setGame(int size, int start, int end) {
         this.size = size;
         game = new Game(RandomUtils.getRandomNumbers(size, start, end));
+    }
+
+    private int[] getUserNumber() {
+        RequestInput.requestInputData();
+        String input = Console.readLine();
+        return Parser.parseUserInput(input, size);
+    }
+
+    private void computeScore() {
+        for (int i = 0; i < size; i++) {
+            compute(game.getGameNumbers(), user.getUserNumbers(), i);
+        }
+    }
+
+    private void compute(int[] gameNumber, int[] userNumber, int index) {
+        int temp = -1;
+        for (int i = 0; i < gameNumber.length; i++) {
+            if (gameNumber[i] == userNumber[index]) {
+                temp = i;
+                break;
+            }
+        }
     }
 }
---
+++ src/main/java/view/RequestInput.java
@@ -0,0 +1,8 @@
+package baseball.view;
+
+public class RequestInput {
+
+    public static void requestInputData() {
+        System.out.print("숫자를 입력해 주세요 : ");
+    }
+}
