80b7009e5c4942d933d138f707b60885e86e30c0
yg0585 <yg0585@naver.com>
2022-11-08 01:26:51 +0900

    feat: 사용자 중복 입력 검사 추가

--- src/main/java/baseball/UserInput.java
+++ src/main/java/baseball/UserInput.java
@@ -17,7 +17,7 @@ public class UserInput {
             char c = input.charAt(idx);
             int num = Character.getNumericValue(c);
 
-            if (num <= 0) {
+            if (num <= 0 || inputList.contains(num)) {
                 throw new IllegalArgumentException();
             }
 
