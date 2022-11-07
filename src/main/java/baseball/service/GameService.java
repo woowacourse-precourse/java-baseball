9873e65088eaed397aa7f56ef0b378c50a99c860
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-07 18:58:46 +0900

    feat : 상수 및 GameService에서 사용할 객체 생성

---
+++ src/main/java/baseball/service/GameService.java
@@ -0,0 +1,21 @@
+package baseball.service;
+
+import static baseball.domain.User.getUserInputNumberList;
+
+import baseball.domain.Computer;
+import baseball.domain.Constant;
+import baseball.domain.User;
+import baseball.view.OutputView;
+import java.util.List;
+
+public class GameService {
+    private int strike;
+    private int ball;
+
+    User user = new User();
+    Computer computer = new Computer();
+    OutputView outputView = new OutputView();
+
+
+}
+
