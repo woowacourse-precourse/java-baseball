3ac787f741923e8a1e16e62ac4ca7e177b7271bc
hhiyeon <hhyyeon0214@gmail.com>
2022-11-07 20:22:16 +0900

    feat: number-init computer number

--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -1,16 +1,13 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 class Number {
     private static List<Integer> userNumbers;
     private static List<Integer> computers;
-
-    private void addRandomNumber(int randomNumber, List<Integer> computer) {
-        if (!computer.contains(randomNumber)) {
-            computer.add(randomNumber);
-        }
-    }
 
     public static List<Integer> getComputers() {
         return computers;
@@ -18,5 +15,20 @@ class Number {
 
     public static List<Integer> getUserNumbers() {
         return userNumbers;
+    }
+
+    private List<Integer> initComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            addRandomNumber(randomNumber, computer);
+        }
+        return computer;
+    }
+
+    private void addRandomNumber(int randomNumber, List<Integer> computer) {
+        if (!computer.contains(randomNumber)) {
+            computer.add(randomNumber);
+        }
     }
 }
