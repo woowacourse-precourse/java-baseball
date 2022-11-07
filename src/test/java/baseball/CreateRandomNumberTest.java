a581c7c77e1a3daee9a077f29c407c24c21583a8
saewoo1 <wooozooo1123@gmail.com>
2022-11-07 21:12:19 +0900

    test(CreateRandomNumberTest): remove import static

--- src/test/java/baseball/CreateRandomNumberTest.java
+++ src/test/java/baseball/CreateRandomNumberTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.Set;
-import static org.assertj.core.api.Assertions.assertThat;
+import org.assertj.core.api.Assertions;
 
 class CreateRandomNumberTest {
 
@@ -13,11 +13,11 @@ class CreateRandomNumberTest {
     String testNumber = CreateRandomNumber.numberMaker();
 
     // 길이 검사
-    assertThat(testNumber.length()).isEqualTo(3);
+    Assertions.assertThat(testNumber.length()).isEqualTo(3);
 
     // 1 ~ 9 숫자 검사
     for (int i = 0; i < testNumber.length(); i++) {
-      assertThat(testNumber.charAt(i)).isBetween('1', '9');
+      Assertions.assertThat(testNumber.charAt(i)).isBetween('1', '9');
     }
 
     // 중복 검사
@@ -25,6 +25,6 @@ class CreateRandomNumberTest {
     for (int i = 0; i < testNumber.length(); i++) {
       set.add(testNumber.charAt(i));
     }
-    assertThat(testNumber.length()).isEqualTo(set.size());
+    Assertions.assertThat(testNumber.length()).isEqualTo(set.size());
   }
 }
