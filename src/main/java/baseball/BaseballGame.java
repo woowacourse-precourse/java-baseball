041bae49102b1d45daca1839983c424f7852302c
Joonsoo Kim <kjsoo1901@gmail.com>
2022-11-07 20:59:58 +0900

    feat: Add SetStrikeNumber in BaseballGame

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import java.util.ArrayList;
+
 public class BaseballGame {
     private int strikeNumber = 0;
     private int ballNumber = 0;
+
+    public void SetStrikeNumber(BaseballNumber oppositeNumber, BaseballNumber myNumber){
+        ArrayList<Integer> oppositeList = oppositeNumber.getNumbers();
+        ArrayList<Integer> myList = myNumber.getNumbers();
+
+        strikeNumber = 0;
+        for(int i = 0; i < 3; i++){
+            if(oppositeList.get(i) == myList.get(i))
+                strikeNumber++;
+        }
+    }
 
 }
