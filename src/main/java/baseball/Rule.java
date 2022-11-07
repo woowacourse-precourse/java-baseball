fc96cee82d0b8a6e024a8832a8fb75701912d0b8
이경범 <tkdwk567@naver.com>
2022-11-07 12:28:54 +0900

    feat : Rule 클래스 생성
    컴퓨터에서 받은 난수를 기준으로 타겟 리스트 즉, 플레이어 리스트의 원소를 검사하여 스트라이크, 볼, 낫싱의 개수를 판단하여 GameMessage 에게 넘겨준다.

---
+++ src/main/java/baseball/Rule.java
@@ -0,0 +1,49 @@
+package baseball;
+
+import java.util.List;
+
+public class Rule {
+
+    private List<Integer> selectedNumbers;
+    private int strikeCount;
+    private int ballCount;
+
+    public void selectedNumber(List<Integer> computerNumbers) {
+        this.selectedNumbers = computerNumbers;
+    }
+
+    public GameMessage answerNumber(List<Integer> target) {
+        for (int index = 0; index < target.size(); index++) {
+            Integer value = target.get(index);
+            contains(index, value);
+        }
+
+        GameMessage gameMessage = new GameMessage(strikeCount, ballCount);
+        clear();
+        return gameMessage;
+    }
+
+    private void clear() {
+        this.strikeCount = 0;
+        this.ballCount = 0;
+    }
+
+    private void contains(int index, Integer value) {
+        if (containsExactly(value, index)) {
+            strikeCount += 1;
+            return;
+        }
+
+        if (contains(value)) {
+            ballCount += 1;
+        }
+    }
+
+    private boolean containsExactly(Integer value, int index) {
+        return selectedNumbers.contains(value) && selectedNumbers.get(index).equals(value);
+    }
+
+    private boolean contains(Integer value) {
+        return selectedNumbers.contains(value);
+    }
+}
