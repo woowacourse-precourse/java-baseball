71c6554d93141751ebbfbae71ef4d28ceb0b64e3
kariskan <zkfzpf56@naver.com>
2022-11-07 15:55:34 +0900

    feat: 출력을 담당하는 ConsoleUtil 클래스 작성

---
+++ src/main/java/baseball/utils/ConsoleUtil.java
@@ -0,0 +1,12 @@
+package baseball.utils;
+
+public class ConsoleUtil {
+
+    public static void consoleOutput(String output) {
+        System.out.print(output);
+    }
+
+    public static void consoleOutputLine(String output) {
+        System.out.println(output);
+    }
+}
