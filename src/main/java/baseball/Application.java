1ba52c84cb086053304bc408dbe69468160af7d9
kkkwp <chxrryda@gmail.com>
2022-11-08 01:32:18 +0900

    feat(gameOver): 게임 종료 후 다시 할지 묻는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,6 +13,21 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    private static boolean gameOver() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
+        String input = Console.readLine();
+        if (input.equals("1")) {
+            return true;
+        }
+        if (input.equals("2")) {
+            return false;
+        }
+        else {
+            throw new IllegalArgumentException("오류가 발생했습니다.");
+        }
+    }
+
     private static List<Integer> inputNumber() {
         System.out.print("숫자를 입력해주세요 : ");
 
@@ -23,7 +38,7 @@ public class Application {
         }
         if (user.size() != 3)
             throw new IllegalArgumentException("오류가 발생했습니다.");
-        if (user.stream().noneMatch(x -> x % 10 > 0))
+        if (user.stream().noneMatch(x -> x % 10 > 0)) // 1 ~ 9
             throw new IllegalArgumentException("오류가 발생했습니다.");
         return user;
     }
