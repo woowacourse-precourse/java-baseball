2d081db99125c27716d13432576a65b1bb3cb74d
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-07 23:53:12 +0900

    랜덤 숫자 생성

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,4 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
+    private List<Integer> randomNumber;
+
+    public void makeRandomList() {
+        randomNumber = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumber.contains(number)) {
+                randomNumber.add(number);
+            }
+        }
+    }
 }
