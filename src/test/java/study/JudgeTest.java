456bb1d53d04368ce2f5144fe2fc71016d17d15a
devMuscle <hongsbien@naver.com>
2022-11-08 06:20:56 +0900

    feat: JudgeTest Class 생성

--- src/test/java/study/JudgeTest.java
+++ src/test/java/study/JudgeTest.java
@@ -1,2 +1,46 @@
-package study;public class JudgeTest {
+package study;
+
+import baseball.Judge;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+public class JudgeTest {
+
+    @Test
+    void countStrike_메서드로_스트라이크_갯수를_카운트() {
+        String input = "1,2";
+        String[] result = input.split(",");
+
+        List<Integer> randomNumberList = Arrays.asList(1,2,3);
+        List<Integer> userNumberList = Arrays.asList(1,2,3);
+
+        Judge judge = new Judge();
+
+        Integer strikeCnt = judge.countStrike(randomNumberList, userNumberList);
+
+        Assertions.assertEquals(3, strikeCnt);
+    }
+
+    @Test
+    void countBall_메서드로_볼_갯수를_카운트() {
+        String input = "1,2";
+        String[] result = input.split(",");
+
+        List<Integer> randomNumberList = Arrays.asList(7,1,3);
+        List<Integer> userNumberList = Arrays.asList(2,1,6);
+
+        Judge judge = new Judge();
+
+        Integer strikeCnt = judge.countStrike(randomNumberList, userNumberList);
+        Integer ballCnt = judge.countBall(randomNumberList, userNumberList, strikeCnt);
+
+        System.out.println(strikeCnt);
+        System.out.println(ballCnt);
+
+        Assertions.assertEquals(0, ballCnt);
+    }
 }
