36944a5142c4cfbce4dd58eeea4af50b5a8bf714
hodomaroo <enghemdwkd@gmail.com>
2022-11-08 00:09:20 +0900

    [Feat] : RandomManager 클래스 구현
    camp.nextstep.edu.missionutils.Randoms 클래스의 pickNumberInRange
    메서드를 사용한, 숫자 범위와 개수를 입력받아 주어진 숫자 범위 내에서
    겹치지 않는 개수 길이의 숫자 배열을 반환하는 getDistinctIntegersInRange메서드를 구현하였습니다.

--- src/main/java/baseball/RandomManager.java
+++ src/main/java/baseball/RandomManager.java
@@ -1,2 +1,32 @@
-package baseball;public class RandomManager {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.Set;
+
+
+/**
+ * camp.nextstep.edu.missionutils.Randoms 클래스의 메서드를 활용한 각종 기능을 제공하는 클래스
+ */
+public class RandomManager {
+
+    public static Integer[] getDistinctIntegersInRange(int rangeStart, int rangeEnd, int count) {
+        try {
+            Set<Integer> integerSet = new HashSet<>();
+            Integer[] integers = new Integer[count];
+
+            for (int i = 0; i < count; i++) {
+                Integer randomInt = null;
+                while (i == integerSet.size()) {
+                    randomInt = Randoms.pickNumberInRange(1, 9);
+                    integerSet.add(randomInt);
+                }
+                integers[i] = randomInt;
+            }
+            return integers;
+        }catch (Exception e){
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+
+    }
 }
