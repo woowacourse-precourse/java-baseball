45d760e5c6b251ae5f1741da4a8b1ff4ff6a3449
ryanha <ryanha@handong.ac.kr>
2022-11-07 17:50:49 +0900

    refactor(baseball): 가독성을 위한 Comments 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,10 +13,11 @@ public class Application {
         Referee referee = new Referee();
 
         while(!isFinished) {
-            if(isFirstGame) {
+            if(isFirstGame) { // 첫번째 게임인 경우에만 시작 메시지를 출력함
                 System.out.println("숫자 야구 게임을 시작합니다.");
             }
             String answer = generator.getNumber(); // 정답 생성
+            // 사용자가 정답을 입력할 때 까지 다음을 반복함
             boolean isCollect = false; // 올바른 정답인 가?
             while(!isCollect) {
                 String expected = IOProcessor.getUserInput("숫자를 입력해주세요 :", " ");
@@ -24,6 +25,7 @@ public class Application {
                 isCollect = referee.decision(expected, answer);
                 System.out.println(referee.buildDecisionString());
             }
+            // 게임을 새로 시작할 지, 종료할 지 입력받음
             String status = IOProcessor.getUserInput("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.", "\n");
             if(status.equals("2")) {
                 isFinished = true;
