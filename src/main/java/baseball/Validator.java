2585a1f1f2a7a7000111ff3db0f9468854562534
dain-lee <dain9934@gmail.com>
2022-11-07 20:59:50 +0900

    feat(validator): 입력한 수의 중복 검증 메서드 구현

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,13 +1,17 @@
 package baseball;
 
+import java.util.HashSet;
+
 public class Validator {
 
     public static final String LENGTH_EXCEPTION = "3자리 숫자를 입력해야 합니다.";
-    public static final String DIGIT_EXCEPTION = "각 자리는 숫자 1~9로 구성되어야 합니다."
+    public static final String DIGIT_EXCEPTION = "각 자리는 숫자 1~9로 구성되어야 합니다.";
+    public static final String DUPLICATION_EXCEPTION = "서로 다른 3개의 숫자로 구성되어야 합니다.";
 
     public static void validatePlayerInputNumbersString(String input) {
         validateLength(input);
         validateDigit(input);
+        validateNoDuplication(input);
     }
 
     public static void validateLength(String input) {
@@ -21,6 +25,16 @@ public class Validator {
             if (c < '1' || c > '9') {
                 throw new IllegalArgumentException(DIGIT_EXCEPTION);
             }
+        }
+    }
+
+    public static void validateNoDuplication(String input) {
+        HashSet<Character> inputSet = new HashSet<>();
+        for (char c : input.toCharArray()) {
+            inputSet.add(c);
+        }
+        if (inputSet.size() != input.length()) {
+            throw new IllegalArgumentException(DUPLICATION_EXCEPTION);
         }
     }
 }
