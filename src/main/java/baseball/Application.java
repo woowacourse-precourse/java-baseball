225f6d743a74f26e83ac1909da012488adce7289
Dasol Jung <greteljds0251@gmail.com>
2022-11-07 22:29:05 +0900

    feat: Comparison player numbers and computer numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,22 +10,24 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Baseball baseball = new Baseball();
+        do {
+            baseball.pickRandomNumber();
 
+        } while (true);
     }
 }
 
 class Baseball {
-    private List<Integer> computerRandom = List.of(0, 0, 0);
+    private List<Integer> computerNumber = List.of(0, 0, 0);
     private List<Integer> playerNumber = List.of(0, 0, 0);
     private List<Integer> baseballCount = List.of(0, 0);
 
-
     public void pickRandomNumber() {
-        int i = 0;
-        while (computerRandom.size() < 3) {
+        for (int i = 0; i < 3;) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computerRandom.contains(randomNumber)) {
-                computerRandom.set(i, randomNumber);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.set(i, randomNumber);
                 i++;
             }
         }
@@ -36,6 +38,7 @@ class Baseball {
         try {
             InputStreamReader ir = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(ir);
+            System.out.print("숫자를 입력해주세요 :");
             getPlayerNumber = br.readLine();
         } catch (Exception e) { }
 
@@ -43,6 +46,27 @@ class Baseball {
         for (int i = 0; i < 3; i++) {
             playerNumber.set(i, (num % 10));
             num /= 10;
+        }
+    }
+
+    public void comparisonPlayerComputer() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                comparisonNumber(playerNumber.get(i), computerNumber.get(j), i, j);
+            }
+        }
+    }
+
+    public void comparisonNumber(int playerNumber, int computerNumber, int i, int j) {
+        int count;
+        if (playerNumber == computerNumber) {
+            if (i == j) {
+                count = baseballCount.get(0) + 1;
+                baseballCount.set(0, count);
+            } else {
+                count = baseballCount.get(1) + 1;
+                baseballCount.set(1, count);
+            }
         }
     }
 }
