715526e3573d6055e9782694128e77f428935573
YusunPark <solarpy.kor@gmail.com>
2022-11-07 22:48:47 +0900

    feat: Create computer number
    #1. 1~9 범위의 서로 다른 임의의 수 3개를 생성한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
 
     static void startMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    static List<Integer> makeNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 
     static void getUserNumber() {
@@ -13,6 +28,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         startMessage();
+        List<Integer> computer = makeNumber();
         getUserNumber();
     }
 }
