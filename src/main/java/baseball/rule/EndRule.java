460ea78c905fa900674e257dac7a71072f4f7c90
이건회 <rg970604@naver.com>
2022-11-07 19:12:32 +0900

    feat (EndRule.java) : 게임 종료 후 1 입력시 새로 시작, 2 입력시 프로그램 종료를 판정

--- src/main/java/baseball/rule/EndRule.java
+++ src/main/java/baseball/rule/EndRule.java
@@ -4,8 +4,15 @@ import baseball.domain.BallCount;
 
 public class EndRule {
 
+    private static final String RESTART_NUMBER = "1";
+    private static final String END_NUMBER = "2";
+
     public static boolean isGameEnd(BallCount ballCount) {
         return ballCount.ballCountResult().equals("3스트라이크");
+    }
+
+    public static boolean isProgramEnd(String userInput) {
+        return userInput.equals(END_NUMBER);
     }
 
 }
