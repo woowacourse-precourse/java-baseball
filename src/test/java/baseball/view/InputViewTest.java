f8296112eda25f7e9b29906b4d4f8ba34c991625
iamjooon2 <iamjooon2@gmail.com>
2022-11-07 23:14:10 +0900

    test: 사용자에게 숫자 입력을 묻는 askNumberMessage 메서드 테스트 코드 작성

---
+++ src/test/java/baseball/view/InputViewTest.java
@@ -0,0 +1,22 @@
+package baseball.view;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InputViewTest {
+
+    @Test
+    void 숫자_입력_묻기() {
+        String expectedInputNumber = "123";
+
+        InputStream in = new ByteArrayInputStream(expectedInputNumber.getBytes());
+        System.setIn(in);
+
+        String actualInputNumber = InputView.askNumberMessage();
+
+        Assertions.assertThat(actualInputNumber).isEqualTo(expectedInputNumber);
+    }
+
+}
