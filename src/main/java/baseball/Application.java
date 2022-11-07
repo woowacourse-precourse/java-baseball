ecc9c19b0e04139c33541bcf11afb185fe11e4c2
KangSungBum <peter9706@naver.com>
2022-11-07 12:42:06 +0900

    feat(Application.java) : add createRandomNumber method
    Added new method to Application.java
    - create random 3 numbers by using Randoms.pickNumberInRange() API
    - returns List type of 3 digits random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static List<Integer> createRandomNumber() {
+        List<Integer> createdNumber = new ArrayList<>();
+        while (createdNumber.size() != 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!createdNumber.contains(randomNumber)) {
+                createdNumber.add(randomNumber);
+            }
+        }
+
+        return createdNumber;
     }
 }
