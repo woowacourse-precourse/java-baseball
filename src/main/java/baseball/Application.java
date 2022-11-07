0b0f649485bba3a29881d0f71ab20306fb993fc5
chobobdev <chobobdev@naver.com>
2022-11-08 01:39:13 +0900

    feat : use api to generate random number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,21 @@
 package baseball;
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
+    public static List<Integer> RandomNum() {
+       List <Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
