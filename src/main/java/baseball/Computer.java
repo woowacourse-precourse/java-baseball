859d96efdb24fa2d1b66fb9e2f9347a5f6e71f55
윤서영 <yoonseoyoung@yunseoyeong-ui-MacBookPro.local>
2022-11-07 21:55:06 +0900

    refactor : 구조 변경(Computer, User 클래스 생성)

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,2 +1,11 @@
-package baseball;public class Computer {
+package baseball;
+
+public class Computer {
+    private String answer;
+    private int ball;
+    private int strike;
+
+    public Computer(String answer) {
+        this.answer = answer;
+    }
 }
--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,44 +1,39 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-
-import java.util.ArrayList;
-import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Controller {
     final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
-    Service service = new Service();
 
-    public void startGame() {
+    private Computer computer;
+    private User user = new User();
+
+    public void game() {
+        setAnswer();
+        while(true) {
+            inputUserNumber();
+        }
+    }
+
+    private void setAnswer() {
         System.out.println("START_MESSAGE = " + START_MESSAGE);
-        List<Integer> answerNum = service.setAnswerNum();
-        String inputString = getInputString();
-        List<Integer> inputNum = changeInputFormat(inputString);
-
-    }
-
-    private String getInputString() throws IllegalArgumentException {
-        System.out.print(INPUT_MESSAGE);
-        String inputString = Console.readLine();
-        return inputString;
-    }
-
-    private List<Integer> changeInputFormat(String inputString) {
-        List<Integer> inputNum = new ArrayList<>();
-
-        if(inputString.length() != 3)
-            throw new IllegalArgumentException();
-
-        inputString.chars().forEach(c -> {
-            Integer temp = Character.getNumericValue(c);
-            if(temp < 1 || temp > 9)
-                throw new IllegalArgumentException();
-            if(inputNum.contains(temp)) {
-                throw new IllegalArgumentException();
+        String answer = "";
+        while(answer.length() < 3) {
+            int num = Randoms.pickNumberInRange(1, 9);
+            if(!answer.contains(String.valueOf(num))) {
+                answer += num;
             }
-            inputNum.add(temp);
-        });
-        return inputNum;
+        }
+        computer = new Computer(answer);
     }
+
+    private void inputUserNumber() throws IllegalArgumentException{
+        System.out.print(INPUT_MESSAGE);
+        String input = Console.readLine();
+        user.checkInputFormat(input);
+        user.setInputNumber(input);
+    }
+
 }
