6f5a227d7428b42701739c4fac66b664d6ec2dd3
chaeyeong222 <iky1505@naver.com>
2022-11-07 10:18:31 +0900

    refactor : 사용자입력값 중복체크 기능 오타수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,29 +1,26 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static baseball.Number.inputNumber;
 
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다.");
+
         int randomNumber = GetRandomNumber();
         System.out.println(randomNumber);
+        System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.println("숫자를 입력해주세요 : ");
-        Number.rangeCheck();
-        System.out.println(inputNumber);
-
-
-
+        int inputNumber = Number.rangeCheck();
+        System.out.println("입력값 : "+ inputNumber);
 
 
     }
+
 
     private static int GetRandomNumber() {
         List<Integer> computer = new ArrayList<>();
--- src/main/java/baseball/Number.java
+++ src/main/java/baseball/Number.java
@@ -3,29 +3,32 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Number {
-    public static String inputNumber = Console.readLine();
-    public static String rangeCheck() {
+    public static int rangeCheck() {
+        String beforeCheck = Console.readLine();
+        System.out.println("작성 : " +beforeCheck);
 
-        if(inputNumber.length()>3){
+        if(beforeCheck.length()>3){
             throw new IllegalArgumentException("입력값은 3자리 숫자여야 합니다.");
         }
 
         try{
-            int input = Integer.parseInt(inputNumber);
+            int input = Integer.parseInt(beforeCheck);
         }catch (Exception ex){
             throw new IllegalArgumentException("숫자가 아닌 값을 포함하고 있습니다. 1-9까지의 숫자만 입력하세요.");
             //ex.printStackTrace();
         }
 
-        for (int i = 0; i < 3; i++) {
-            int [] duplicate = new int[3];
-            duplicate[i] = inputNumber.charAt(i);
-            if(duplicate[0]==duplicate[1] || duplicate[1]==duplicate[2] || duplicate[0]==duplicate[2]){
-                throw  new IllegalArgumentException("같은 숫자를 중복해서 입력할 수 없습니다.");
-            }
+        int afterCheck = Integer.parseInt(beforeCheck);
+        int [] duplicate = new int[3];
+        duplicate[0] = afterCheck/100;
+        afterCheck %=100;
+        duplicate[1] = afterCheck/10;
+        afterCheck %=10;
+        duplicate[2] = afterCheck;
+        if(duplicate[0]==duplicate[1] || duplicate[1]==duplicate[2] || duplicate[0]==duplicate[2]){
+                throw new IllegalArgumentException("같은 숫자를 중복해서 입력할 수 없습니다.");
         }
-
-        return inputNumber;
+        return afterCheck;
     }
 
 
