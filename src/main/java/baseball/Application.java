0e5511dabadf3c0c72a43509f79e9a31b5f58f92
broden-horizon <rlghd1698@naver.com>
2022-11-07 18:08:45 +0900

    feat. 게임 시작 정의

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import baseball.gameComponents.GameManager;
+import baseball.gameComponents.MainFunctions;
+import baseball.gameComponents.SubUtils;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        MainFunctions mainFunctions = new MainFunctions();
+        SubUtils subUtils = new SubUtils();
+        GameManager gameManager = new GameManager(mainFunctions, subUtils);
+
+        gameManager.gameStart();
     }
 }
