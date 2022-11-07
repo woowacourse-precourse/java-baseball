1153de88a64c6ebb077ec3ffbf5e6563e2c104d4
hingjae <ljh23270@naver.com>
2022-11-07 17:10:55 +0900

    컴퓨터 난수 생성

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,23 +1,23 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Computer {
-    List<Integer> randomNumber = new ArrayList<>();
-    Map<String, Integer> ballAndStrike = new HashMap<>();
+    List<Integer> randomNumList = new ArrayList<>();
 
-    // 세자리 난수 설정
-    public List<Integer> setRandomNumber() {
-        while(randomNumber.size() < 3){
-            int pickedNumber = Randoms.pickNumberInRange(1, 9);
-            if(!randomNumber.contains(pickedNumber)){
-                randomNumber.add(pickedNumber);
+    public Computer() {
+        setRandomNumList();
+    }
+
+    public void setRandomNumList (){
+        while(randomNumList.size() < 3){
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if(!randomNumList.contains(randomNum)) {
+                randomNumList.add(randomNum);
             }
         }
-        return randomNumber;
     }
 }
