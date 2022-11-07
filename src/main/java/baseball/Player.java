7ca5387f35492fd452da9bce26d07fd3716c3a72
LeeMyeongJin <=>
2022-11-07 22:24:29 +0900

    Feat: 3글자 아니면 예외처리 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -6,6 +6,10 @@ public class Player {
     public static String getNumber(){
         System.out.print("숫자를 입력해주세요 : ");
         String userNumber = Console.readLine();
+        if (userNumber.length() != 3){
+            throw new IllegalArgumentException();
+        }
         return userNumber;
     }
+
 }
