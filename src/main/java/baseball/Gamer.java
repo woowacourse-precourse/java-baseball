93d07a4b9696dd83d0c34a4b53fb9eb3fe0377e2
doehy <dbehxks02@naver.com>
2022-11-07 23:05:24 +0900

    feat : 입력받은 숫자의 예외를 한 곳에 모집

--- src/main/java/baseball/Gamer.java
+++ src/main/java/baseball/Gamer.java
@@ -1,12 +1,19 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import static baseball.Util.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class Gamer {
     public List<Integer> gamerNumber = new ArrayList<>();
+
+    public void checkException(String input_Number){
+        isWeirdSize(input_Number);
+        isNumber(input_Number);
+        isSameEachNumber(input_Number);
+    }
 
     public void inputNumber(){
         System.out.print("\n숫자를 입력해주세요 : ");
