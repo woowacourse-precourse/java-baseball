4a8e4c16c4f7639939433220e6ff47232f21f9ec
swonny <wonny921@gmail.com>
2022-11-07 14:54:04 +0900

    test: Game, Computer, Exception 클래스에 대한 테스트 코드 분리 및 메소드명 수정

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -1,2 +1,48 @@
-package baseball;public class ComputerTest {
+package baseball;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ComputerTest extends NsTest {
+    static Computer computer = new Computer();
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+
+    @Test
+    void 컴퓨터_숫자_길이_테스트() {
+        String randomNumbers = computer.getRandomNumbers();
+
+        assertThat(randomNumbers.length()).isEqualTo(3);
+    }
+
+    @Test
+    void 컴퓨터_숫자_길이_테스트_2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 컴퓨터_숫자_중복_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("122"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 컴퓨터_숫자_문자_포함_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("a12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 }
--- src/test/java/baseball/ExceptionTest.java
+++ src/test/java/baseball/ExceptionTest.java
@@ -1,2 +1,98 @@
-package baseball;public class ExceptionTest {
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ExceptionTest {
+    @Test
+    void 문자_포함_예외_처리_테스트() {
+        String input = " 1234";
+
+        assertThatThrownBy(() -> Exception.hasCharacters(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 기능요구사항4_문자_포함_예외_테스트() {
+        String input = " 1";
+
+        assertThatThrownBy(() -> ExceptionFor4.hasCharacters(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 기능요구사항4_입력_숫자_테스트() {
+        String input = "3";
+
+        assertThatThrownBy(() -> ExceptionFor4.checkYesOrNo(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 기능요구사항4_입력_숫자_길이_테스트() {
+        String input = "31";
+
+        assertThatThrownBy(() -> ExceptionFor4.checkInputLength(input))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 게임_플레이어_입력_숫자_길이_테스트() {
+        String gamePlayerInput = "1234";
+
+        assertThatThrownBy(() -> playGame().checkGamePlayerNumberInput(gamePlayerInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자_중복_테스트_1() {
+        String gamePlayerInput = "112";
+
+        assertThatThrownBy(() -> makeGameInstance().checkGamePlayerNumberInput(gamePlayerInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자_중복_테스트_2() {
+        String gamePlayerInput = "122";
+
+        assertThatThrownBy(() -> makeGameInstance().checkGamePlayerNumberInput(gamePlayerInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 숫자_중복_테스트_3() {
+        String gamePlayerInput = "222";
+
+        assertThatThrownBy(() -> makeGameInstance().checkGamePlayerNumberInput(gamePlayerInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 문자_포함_테스트() {
+        String gamePlayerInput = "a12";
+
+        assertThatThrownBy(() -> makeGameInstance().checkGamePlayerNumberInput(gamePlayerInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 게임_플레이어_입력_숫자_0포함_테스트() {
+        String gamePlayerInput = "012";
+
+        assertThatThrownBy(() -> makeGameInstance().checkGamePlayerNumberInput(gamePlayerInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    public Game makeGameInstance() {
+        return new Game();
+    }
+
+    public Game playGame() {
+        Game game = new Game();
+        game.play();
+
+        return game;
+    }
 }
