82a7879ef4188a6ff77833ffab5500c0eacfe709
woogi <dnrlwo11@gmail.com>
2022-11-07 23:21:24 +0900

    [FIX] message constants 수정

--- src/main/java/baseball/constants/Message.java
+++ src/main/java/baseball/constants/Message.java
@@ -5,6 +5,8 @@ public class Message {
     public static final String BALL = "볼";
     public static final String NOTHING = "낫싱";
     public static final String PLAY = "숫자를 입력해주세요 : ";
-    public static final String END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static final String MATCH = "3개의 숫자를 모두 맞히셨습니다!";
     public static final String REPLAY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String END = "게임 종료";
 }
