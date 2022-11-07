733ea9ba55d0c6f5b5a222ffc9a77be2d83b933d
Woojin Lee <dhkep03@gmail.com>
2022-11-07 19:12:30 +0900

    test(Target): static 키워드 삭제를 반영하여, 테스트용 객체를 새로 생성

--- src/test/java/baseball/model/TargetTest.java
+++ src/test/java/baseball/model/TargetTest.java
@@ -8,31 +8,33 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class TargetTest {
+    private Target target = new Target();
+
     @BeforeEach
     void initTarget() {
-        Target.setTargetIntoZeroList();
+        target.setTargetIntoZeroList();
     }
 
     @Test
     void reset() {
-        Target.reset();
-        assertThat(Target.get())
+        target.reset();
+        assertThat(target.get())
                 .hasSize(3)
                 .doesNotContain(0);
     }
 
     @Test
     void get() {
-        Target.reset();
-        Target.setTargetIntoZeroList();
+        target.reset();
+        target.setTargetIntoZeroList();
         List<Integer> result = Arrays.asList(0, 0, 0);
-        assertThat(Target.get()).isEqualTo(result);
+        assertThat(target.get()).isEqualTo(result);
     }
 
     @Test
     void setOneNumber() {
-        Target.setOneNumber(1);
-        List<Integer> targetNumberList = Target.get();
+        target.setOneNumber(1);
+        List<Integer> targetNumberList = target.get();
         assertThat(targetNumberList.get(1)).isNotEqualTo(0);
         assertThat(targetNumberList.get(0)).isEqualTo(0);
     }
