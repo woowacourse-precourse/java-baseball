49fb9002f23a5971f003490fb8b64a947b13d191
yubin <dbqls2821@gmail.com>
2022-11-08 03:31:09 +0900

    refactor: 중복되는 테스트 코드를 @ParameterizedTest, @CsvResource로 통합

--- src/test/java/baseball/InputValidationTest.java
+++ src/test/java/baseball/InputValidationTest.java
@@ -3,106 +3,46 @@ package baseball;
 import baseball.constant.ExceptionMessages;
 import baseball.io.InputValidator;
 import baseball.io.PlayingInputValidator;
+import org.assertj.core.util.Streams;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
 
 public class InputValidationTest {
 
-  @Test
-  void 입력값에_중복되는_수가_들어있는_경우_예외를_던지는가() {
-    try {
-      InputValidator validator = new PlayingInputValidator();
-      String userInput = "112";
-      validator.validate(userInput);
-    } catch (IllegalArgumentException e) {
-      assertEquals(ExceptionMessages.INPUT_IS_NOT_UNIQUE.getExceptionMessage(), e.getMessage());
-    }
-  }
-
-  @Test
-  void 입력값의_길이가_3이_아닐때_예외를_던지는가_1() {
-    try {
-      InputValidator validator = new PlayingInputValidator();
-      String userInput = "12";
-      validator.validate(userInput);
-    } catch (IllegalArgumentException e) {
-      assertEquals(ExceptionMessages.INPUT_LENGTH_IS_NOT_THREE.getExceptionMessage(), e.getMessage());
-    }
-  }
-
-  @Test
-  void 입력값의_길이가_3이_아닐때_예외를_던지는가_2() {
-    try {
-      InputValidator validator = new PlayingInputValidator();
-      String userInput = "1234";
-      validator.validate(userInput);
-    } catch (IllegalArgumentException e) {
-      assertEquals(ExceptionMessages.INPUT_LENGTH_IS_NOT_THREE.getExceptionMessage(), e.getMessage());
-    }
-  }
-
-  @Test
-  void 입력값에_0이_포함되어_있으면_예외를_던지는가() {
-    try {
-      InputValidator validator = new PlayingInputValidator();
-      String userInput = "120";
-      validator.validate(userInput);
-    } catch (IllegalArgumentException e) {
-      assertEquals(ExceptionMessages.INPUT_IS_NOT_DIGIT.getExceptionMessage(), e.getMessage());
-    }
-  }
-
-  @Test
-  void 입력값이_숫자가_아닌_경우_예외를_던지는가() {
-    try {
-      InputValidator validator = new PlayingInputValidator();
-      String userInput = "가나다";
-      validator.validate(userInput);
-    } catch (IllegalArgumentException e) {
-      assertEquals(ExceptionMessages.INPUT_IS_NOT_DIGIT.getExceptionMessage(), e.getMessage());
-    }
-  }
-
-  private InputStream readUserInput(String input) {
-    return new ByteArrayInputStream(input.getBytes());
-  }
-
-  @Test
-  void 유저가_게임_재시작을_위해_1을_입력한_경우_정상적으로_동작하는가() {
+  @ParameterizedTest
+  @CsvSource(value = {"1,true","2,false"})
+  void 유저가_게임_재시작_및_종료를_위해_1_또는_2를_입력한_경우_정상적으로_동작하는가(String userInput, boolean expectedBoolean) {
 
     // given
     GameController controller = new GameController();
-    InputStream inputStream = readUserInput("1");
+    InputStream inputStream = readUserInput(userInput);
     System.setIn(inputStream);
 
     // when
     boolean isItRestartInput = controller.checkRestart();
 
     // then
-    assertEquals(true, isItRestartInput);
-
+    assertEquals(expectedBoolean, isItRestartInput);
   }
 
-  @Test
-  void 유저가_게임_종료를_위해_2를_입력한_경우_정상적으로_동작하는가() {
-
-    // given
-    GameController controller = new GameController();
-    InputStream inputStream = readUserInput("2");
-    System.setIn(inputStream);
-
-    // when
-    boolean isntItEndInput = controller.checkRestart();
-
-    // then
-    assertEquals(false, isntItEndInput);
-
+  private InputStream readUserInput(String input) {
+    return new ByteArrayInputStream(input.getBytes());
   }
-
 
 }
