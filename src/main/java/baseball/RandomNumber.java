38c6249e4f9e8c34ba5e4b220a6eb8bbc44117fb
seonghoo1217 <seonghoo1217@naver.com>
2022-11-07 21:54:44 +0900

    fix : modify random number list have only two elements issue
    * Why error?
    - In the past my code, if list has exist element this loop ignore
    - So then There are cases at only two elements in the list
    
    Fix(commit_id) : #b3ed37e

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -10,10 +10,24 @@ public class RandomNumber {
         List<Integer> random_number_list=new ArrayList<>();
         for (int i=0;i<3;i++){
             int random_number = Randoms.pickNumberInRange(1, 9);
-            if (!random_number_list.contains(random_number)){
+            if (random_number_list.contains(random_number)){
+                int sub_random = nonPermissionExistNumber(random_number_list, random_number);
+                random_number_list.add(sub_random);
+            }else {
                 random_number_list.add(random_number);
             }
         }
         return random_number_list;
+    }
+
+    private static int nonPermissionExistNumber(List<Integer>random_number_list,int random_number){
+        int sub_random;
+        while (true){
+            sub_random= Randoms.pickNumberInRange(1, 9);
+            if (sub_random!=random_number){
+                break;
+            }
+        }
+        return sub_random;
     }
 }
