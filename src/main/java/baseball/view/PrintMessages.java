bbf0d18a7555d16add205bea782a3abe4967a6a5
chanminmaxscaler <jcm04@naver.com>
2022-11-07 15:59:08 +0900

    refactor: Messages 클래스를 리팩토링한다
    Messages 클래스의 이름을 PrintMessages 로 변경한다. 코드의 가독성을 높이기 위함이다.
    
    getMessageForIntegerFormatting 메소드를 추가한다. 해당 메소드는 호출될 시 Constant.INTEGER_FORMAT_SPECIFIER를 상수값에 합쳐서 반환한다. 문자열 포맷팅을 위해 만든 메소드이다.
    
    BALL("%d볼") 등의 상수값을 BALL("볼")로 수정한다. "%d"와 같은 specifier가 필요할 경우 메소드를 이용하도록 한다.

--- src/main/java/baseball/view/PrintMessages.java
+++ src/main/java/baseball/view/PrintMessages.java
@@ -1,19 +1,25 @@
 package baseball.view;
 
-public enum Messages {
+import baseball.constant.Constant;
+
+public enum PrintMessages {
     START("게임을 시작합니다.\n"),
     INPUT("숫자를 입력하세요. : "),
     ANSWER("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n"),
     RESTART_OR_END("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"),
-    BALL("%d볼"),
-    STRIKE("%d스트라이크"),
+    BALL("볼"),
+    STRIKE("스트라이크"),
     NOTHING("낫싱");
 
     private final String message;
-    Messages(String message) {
+    PrintMessages(String message) {
         this.message = message;
     }
     public String getMessage() {
         return message;
+    }
+
+    public String getMessageForIntegerFormatting() {
+        return Constant.INTEGER_FORMAT_SPECIFIER + message;
     }
 }
