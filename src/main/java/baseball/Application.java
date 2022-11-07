d5583abf547cff61bc4782eabb6c23ebc36c82fc
Ji-Un-Gil <wldns2577@gmail.com>
2022-11-07 16:47:23 +0900

    feat(Application.java): rand_num_decision function
    Added new function to Application.java:
    - rand_num_decision 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+
+    // 컴퓨터가 서로 다른 랜덤의 3자리 수를 정하는 함수
+    public static void rand_num_decision(List<Integer> randnum){
+        while (randnum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randnum.contains(randomNumber)) {
+                randnum.add(randomNumber);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        List<Integer> computer = new ArrayList<>();
+        rand_num_decision(computer);
+
     }
 }
