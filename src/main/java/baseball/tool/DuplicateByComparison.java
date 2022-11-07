e8c952381534528feec467320f20d9eadb6f1770
jaehyeon <jhjh0022@naver.com>
2022-11-07 21:52:08 +0900

    style: 줄 간격 맞추기

--- src/main/java/baseball/tool/DuplicateByComparison.java
+++ src/main/java/baseball/tool/DuplicateByComparison.java
@@ -2,15 +2,16 @@ package baseball.tool;
 
 import java.util.ArrayList;
 import java.util.List;
-import static baseball.tool.BeforeVerificationSetting.makeNumberSplitList;
+
+import static baseball.tool.NumberSplitTool.makeNumberSplitList;
 
 public class DuplicateByComparison {
 
-    public static List<Integer> findOverlapNumber(List<Integer> answerNumber , int receivedNumber){
+    public static List<Integer> findOverlapNumber(List<Integer> answerNumber, int receivedNumber) {
         List<Integer> overlapNumber = new ArrayList<>();
 
-        for(int number : answerNumber){
-            if(makeNumberSplitList(receivedNumber).contains(number)){
+        for (int number : answerNumber) {
+            if (makeNumberSplitList(receivedNumber).contains(number)) {
                 overlapNumber.add(number);
             }
         }
