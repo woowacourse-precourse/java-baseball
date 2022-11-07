8724ac36c3fdea2b9d1f4c8e949972ab08e7dd85
sooyoungh <sooyoung.h8@gmail.com>
2022-11-07 15:25:11 +0900

    feat(Judge): 점수 계산하는 기능 구현
    리스트의 해당 인덱스의 볼/스트라이크 판별, 전체 점수 계산하는 기능 구현

--- src/main/java/baseball/model/Judge.java
+++ src/main/java/baseball/model/Judge.java
@@ -2,10 +2,16 @@ package baseball.model;
 
 import java.util.List;
 
+import static baseball.model.Judge.Result.*;
+
 public class Judge {
 
     private List<Integer> playerNumbers;
     private List<Integer> answer;
+
+    enum Result {
+        BALL, STRIKE, NOTHING;
+    }
 
     public Judge(Computer computer, Player player) {
         this.playerNumbers = player.getPlayerNumbers();
@@ -14,5 +20,24 @@ public class Judge {
 
     public Boolean isGameSuccess() {
         return  (playerNumbers.equals(answer));
+    }
+
+    public Score countScoreOfStrikeAndBall() {
+        Score score = new Score();
+        for (int i = 0; i< answer.size(); i++ ) {
+            Result result = isBallOrStrike(i);
+            if (result.equals(BALL)) score.countBall();
+            if (result.equals(STRIKE)) score.countStrike();
+        }
+        return score;
+    }
+
+    public Result isBallOrStrike(int index) {
+        Integer target = playerNumbers.get(index);
+
+        int findIndex = answer.indexOf(target);
+        if (findIndex == index) return STRIKE;
+        if (findIndex != -1) return BALL;
+        return NOTHING;
     }
 }
