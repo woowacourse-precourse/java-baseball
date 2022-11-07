95f880aa0cc6c796274a561e568490913f49d345
Hyun-Seo Oh <ohseo8813@gmail.com>
2022-11-07 15:53:43 +0900

    refactor: BaseballGameLauncher - 상수명 수정

--- src/main/java/baseball/game/BaseballGameLauncher.java
+++ src/main/java/baseball/game/BaseballGameLauncher.java
@@ -2,17 +2,17 @@ package baseball.game;
 
 public class BaseballGameLauncher {
 
-    public final static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public final static String GAME_LAUNCHER_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public final static String GAME_LAUNCHER_CLOSE_MESSAGE = "게임을 완전히 종료합니다.";
     public final static String GAME_RESTART_SELECTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public final static String GAME_CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    public final static String GAME_CLOSE_MESSAGE = "게임을 완전히 종료합니다.";
 
     private BaseballGame baseballGame = new BaseballGame();
 
     public void run() {
-        System.out.println(GAME_START_MESSAGE);
+        System.out.println(GAME_LAUNCHER_START_MESSAGE);
         startGame();
-        System.out.println(GAME_CLOSE_MESSAGE);
+        System.out.println(GAME_LAUNCHER_CLOSE_MESSAGE);
     }
 
     private void startGame() {
