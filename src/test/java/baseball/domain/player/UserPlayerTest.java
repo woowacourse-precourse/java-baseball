bed8103b73bebb06a33e9574b8dad310110f48e6
seoyoung <sy9723@korea.ac.kr>
2022-11-08 00:01:38 +0900

    test: check each digit of the input and handle exception

--- src/test/java/baseball/domain/player/UserPlayerTest.java
+++ src/test/java/baseball/domain/player/UserPlayerTest.java
@@ -14,4 +14,11 @@ class UserPlayerTest {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    public void 입력받은_값의_각_자리_수가_서로_다른지_확인하고_아니면_예외_발생() {
+        int[] arrInput = {1, 2, 2};
+        assertThatThrownBy(() -> userPlayer.checkDuplicationOfInput(arrInput))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 }
