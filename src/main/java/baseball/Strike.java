55ca81f8953b276762029d4cfe2a2cf01f172ead
twoosky <lhjae3@naver.com>
2022-11-08 00:02:51 +0900

    feat(baseball): strike 개수 계산 메소드 구현

--- src/main/java/baseball/Strike.java
+++ src/main/java/baseball/Strike.java
@@ -5,10 +5,21 @@ import java.util.List;
 public class Strike {
     private final List<Integer> player;
     private final List<Integer> computer;
+    private int count;
 
     public Strike(List<Integer> player, List<Integer> computer) {
         this.player = player;
         this.computer = computer;
+        this.count = 0;
+    }
+
+    public int getCount() {
+        for (int index = 0; index < 3; index++) {
+            if (isStrike(index)) {
+                count += 1;
+            }
+        }
+        return count;
     }
 
     private boolean isStrike(int index) {
