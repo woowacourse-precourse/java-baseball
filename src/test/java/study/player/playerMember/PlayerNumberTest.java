07bf6075c5a25efea0d4716079afbdbefdbeddef
bellljun3395 <195850@jnu.ac.kr>
2022-11-07 22:51:48 +0900

    test(PlayerNumberTest): 다른 수를 가진 두개의 볼이 같은지 비교

--- src/test/java/study/player/playerMember/PlayerNumberTest.java
+++ src/test/java/study/player/playerMember/PlayerNumberTest.java
@@ -18,4 +18,15 @@ class PlayerNumberTest {
                     .isTrue();
     }
 
+    @Test
+    void 다른_수를_가진_두개의_볼이_같은지_비교() {
+        PlayerNumber playerNumber1 = new PlayerNumber(123);
+        PlayerNumber playerNumber2 = new PlayerNumber(456);
+
+        boolean equals = playerNumber2.equals(playerNumber1);
+
+        Assertions.assertThat(equals)
+                .isFalse();
+    }
+
 }
