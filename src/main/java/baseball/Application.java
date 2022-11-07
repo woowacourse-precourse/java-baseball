c3bba988ac09cc7f16ef30562a4c4d741b678632
seung-hun2 <jungsh2328@naver.com>
2022-11-07 18:55:39 +0900

    #answerNum - 서로 다른 임의의 수 3개 선택하는 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int answer = answerNum();
+
+    }
+    // 서로 다른 임의의 수 3개 선택
+    public static int answerNum(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<String> ansL = new ArrayList<>();
+
+        int count=0;
+        while(count!=3){
+            int randpick = Randoms.pickNumberInRange(1,9);
+            if(!ansL.contains(String.valueOf(randpick))) {
+                ansL.add(String.valueOf(randpick));
+                count++;
+            }
+        }
+        String str = "";
+        for(int i=0;i<ansL.size();i++){
+            str += ansL.get(i);
+        }
+        int answer = Integer.parseInt(str);
+        return answer;
     }
 }
