98f0fb005514188e6a82c849df191742e57b52a5
cuttlefish55 <funade7@gmail.com>
2022-11-07 16:29:04 +0900

    feat: Add "BaseballData.java" with constant values
    상수값을 담는 클래스 생성

---
+++ src/main/java/baseball/data/BaseballData.java
@@ -0,0 +1,17 @@
+package baseball.data;
+
+public final class BaseballData {
+    public static final int SIZE = 3;
+    public static final int TARGETSIZE = 0;
+    public static final int DIVIDEND = 100;
+    public static final int DIVIDER = 10;
+    public static final int MINIMUMLENGTH = 0;
+
+    public static final int MINIMUMNUMBER = 1;
+    public static final int MAXIMUMNUMBER = 9;
+
+    public static final String RESTART = "1";
+    public static final String GAMEOVER = "2";
+
+    private BaseballData() { }
+}
