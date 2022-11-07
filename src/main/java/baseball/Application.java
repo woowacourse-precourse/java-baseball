23b8d2c597bebb4124f452070f6694a88cdf9b82
baekeunsun <eunsun5460@gmail.com>
2022-11-07 17:13:27 +0900

    refactor(model/UserGenerate): MVC 패턴 적용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 import java.util.*;
 
+import baseball.model.UserGenerate;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -25,65 +26,6 @@ public class Application {
             computer.add(randomNumber);
         }
         return computer;
-    }
-
-    private static List<Integer> UserGenerate() {
-        System.out.print("숫자를 입력해주세요 : ");
-        String input = Console.readLine();
-        if (CheckInput(input)) {
-            List<Integer> user = StringToList(input);
-            return user;
-        }
-        throw new IllegalArgumentException("잘못된 입력");
-    }
-
-    private static List<Integer> StringToList(String input) {
-        List<Integer> user = new ArrayList<>();
-        for (int i=0; i<input.length(); i++) {
-            user.add(input.charAt(i) - '0');
-        }
-        return user;
-    }
-
-    private static boolean CheckInput(String input) {
-        if (isSize3(input) && isNotDuplicate(input) && isNumber(input) && isNotContain0(input)) {
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean isSize3(String input) {
-        if (input.length() == 3) {
-            return true;
-        }
-        return false;
-    }
-
-    private static boolean isNotDuplicate(String input) {
-        String checkinput = "";
-        for (char charinput : input.toCharArray()) {
-            if (checkinput.contains(String.valueOf(charinput))) {
-                return false;
-            }
-            checkinput += String.valueOf(charinput);
-        }
-        return true;
-    }
-
-    private static boolean isNumber(String input) {
-        for (char charinput : input.toCharArray()) {
-            if (Character.isDigit(charinput) == false) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private static boolean isNotContain0(String input) {
-        if (input.contains("0")) {
-            return false;
-        }
-        return true;
     }
 
     private static String Hint(List<Integer> computer, List<Integer> user) {
@@ -127,11 +69,11 @@ public class Application {
 
     private static void GameStart() {
         List<Integer> computer = ComputerGenerate();
-        List<Integer> user = new ArrayList<>();
+        List<Integer> user = new UserGenerate().user;
         String result = "";
 
         while (!result.equals("3스트라이크")) {
-            user = UserGenerate();
+            user = new UserGenerate().user;
             result = Hint(computer,user);
             System.out.println(result);
         }
