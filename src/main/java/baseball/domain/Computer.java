fd1aaf62d5bff65b546b342dd4ce49537de868c0
kariskan <zkfzpf56@naver.com>
2022-11-07 15:34:42 +0900

    feat: 컴퓨터 숫자 3자리를 생성하는 로직 구현

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,10 +1,41 @@
 package baseball.domain;
 
+import baseball.status.Constants;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
     private List<Integer> computerNumbers;
+
+    public Computer() {
+        computerInit();
+    }
+
+    public void computerInit() {
+        computerNumbers = new ArrayList<>();
+        while (computerNumbers.size() < Constants.NUMBER_OF_DIGITS.getNumber()) {
+            addRandomNumber(getRandomNumber());
+        }
+    }
+
+    private void addRandomNumber(int randomNumber) {
+        if (isNotDuplicateNumber(randomNumber)) {
+            computerNumbers.add(randomNumber);
+        }
+    }
+
+    private boolean isNotDuplicateNumber(int randomNumber) {
+        return !computerNumbers.contains(randomNumber);
+    }
+
+    public int getRandomNumber() {
+        return Randoms.pickNumberInRange(
+                Constants.MIN_RANGE_OF_DIGITS.getNumber(),
+                Constants.MAX_RANGE_OF_DIGITS.getNumber()
+        );
+    }
 
     public List<Integer> getComputerNumbers() {
         return computerNumbers;
