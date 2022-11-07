bf79a4f30e10a062b29dfcd15734a62c4dab2e16
kang hyeon <hyeonkang.dev@gmail.com>
2022-11-07 19:20:13 +0900

    test countStrike and countBall

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,12 +3,48 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.Application.countBall;
+import static baseball.Application.countStrike;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 스트라이크_개수_반환(){
+        List<Integer> computer = new ArrayList<>();
+        computer.add(1);
+        computer.add(2);
+        computer.add(3);
+
+        List<Integer> user = new ArrayList<>();
+        user.add(1);
+        user.add(5);
+        user.add(3);
+
+        assertThat(countStrike(computer, user)).isEqualTo(2);
+    }
+
+    @Test
+    void 볼_개수_반환(){
+        List<Integer> computer = new ArrayList<>();
+        computer.add(1);
+        computer.add(2);
+        computer.add(3);
+
+        List<Integer> user = new ArrayList<>();
+        user.add(1);
+        user.add(5);
+        user.add(3);
+
+        assertThat(countBall(computer, user)).isEqualTo(0);
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
