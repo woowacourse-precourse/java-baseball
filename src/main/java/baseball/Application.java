30be10c45fab31759bfdcdb80bae6f503f9302d7
Junho Choi <ajchoi0928@gmail.com>
2022-11-07 23:15:03 +0900

    feat($main): 게임 진행 제어 함수
    1.3 사용자의 입력을 받아 진행을 제어하는 함수
    GameStatus.java에 정의된 제어 상수에 따라 게임 진행을 제어하도록 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,10 +12,48 @@ public class Application {
      * 게임 진행을 담당하는 함수
      */
     public void initGame() {
+        GameStatus status = GameStatus.CONTINUE;
         do {
+            if (isStopStatus(status)) {
+                break;
+            }
+            if (isExceptionStatus(status)) {
+                throw new IllegalArgumentException();
+            }
             GameFormatter.gameStartMessage();
             GameFormatter.gameInputMessage();
             GameFormatter.gameEndMessage();
+            status = setGameStatus(Console.readLine());
         } while (true);
+    }
+
+    /**
+     * 게임 진행 상황을 제어하는 함수
+     *
+     * @param input
+     * @return GameStatus
+     */
+    public GameStatus setGameStatus(String input) {
+        GameStatus status;
+        switch (input) {
+            case "1":
+                status = GameStatus.CONTINUE;
+                break;
+            case "2":
+                status = GameStatus.STOP;
+                break;
+            default:
+                status = GameStatus.EXCEPTION;
+                break;
+        }
+        return status;
+    }
+
+    public boolean isStopStatus(GameStatus status) {
+        return status.equals(GameStatus.STOP);
+    }
+
+    public boolean isExceptionStatus(GameStatus status) {
+        return status.equals(GameStatus.EXCEPTION);
     }
 }
