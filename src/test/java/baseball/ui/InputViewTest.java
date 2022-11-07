d9bdc63f9f7090282784f66fc73bc53fb2bad9be
chkim <cndghks0213@gmail.com>
2022-11-07 13:42:43 +0900

    test(ui): InputView 테스트 작성

---
+++ src/test/java/baseball/ui/InputViewTest.java
@@ -0,0 +1,25 @@
+package baseball.ui;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class InputViewTest {
+    @ValueSource(strings = {"123", "111", "222", "333"})
+    @ParameterizedTest
+    void 세_자리_숫자를_입력받는다(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertThat(InputView.numbers()).isEqualTo(input);
+    }
+
+    @ValueSource(strings = {"1", "12", "1234", "abc", "가나다", "1a가", "!@#"})
+    @ParameterizedTest
+    void 세_자리_숫자가_아닌_입력을_받으면_예외가_발생한다(String input) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        assertThatThrownBy(InputView::numbers).isInstanceOf(IllegalArgumentException.class);
+    }
+}
