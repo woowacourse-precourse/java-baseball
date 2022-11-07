02dc34a76b5a7f9092ba97761170b3ed2cba80ea
CHAYEONIL <jeay123@naver.com>
2022-11-08 02:40:17 +0900

    Refactor : Integer -> BaseBallNumber로 수정

--- src/main/java/baseball/utill/Random.java
+++ src/main/java/baseball/utill/Random.java
@@ -6,17 +6,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-public class random {
-    private static List<Integer> randomNumbers;
+public class Random {
+    private static List<BaseBallNumber> randomNumbers;
     
-    public void computerNumber() {
+    public Random() {
         this.randomNumbers = makeRandomNumber();
     }
 
-    private List<Integer> makeRandomNumber() {
-        List<Integer> randomNumbers = new ArrayList<>();
+    private List<BaseBallNumber> makeRandomNumber() {
+        List<BaseBallNumber> randomNumbers = new ArrayList<>();
         while (randomNumbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            BaseBallNumber randomNumber = new BaseBallNumber(Randoms.pickNumberInRange(1, 9));
             if (!randomNumbers.contains(randomNumber)) {
                 randomNumbers.add(randomNumber);
             }
@@ -24,7 +24,7 @@ public class random {
         return randomNumbers;
     }
 
-    public List<Integer> randomNumbers() {
+    public List<BaseBallNumber> randomNumbers() {
         return Collections.unmodifiableList(randomNumbers);
     }
 }
