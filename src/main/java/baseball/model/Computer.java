795d0d4c33b9695d342d99335f249c7e652a8e74
KimJaeSung <behappy0916@naver.com>
2022-11-07 15:02:45 +0900

    feat: 랜덤 숫자 생성 기능 추가

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,32 +1,30 @@
 package baseball.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
     private static final int CNT_NUMBER = 3;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
-    private List<Integer> numbers;
-    private int strikeCount;
-    private int ballCount;
 
-    public Computer(){
-        while (numbers.size() < CNT_NUMBER) {
+    private List<Integer> computerNumber;
+
+    public Computer() {
+        this.computerNumber = new ArrayList<>();
+    }
+
+    public List<Integer> addRandomNumbers() {
+        computerNumber = new ArrayList<>();
+        while (computerNumber.size() < CNT_NUMBER) {
             int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
-            if (!numbers.contains(randomNumber)) {
-                numbers.add(randomNumber);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
             }
         }
-    }
-    public List<Integer> getNumbers(){
-        return this.numbers;
-    }
-    public int getStrikeCount(){
-        return strikeCount;
-    }
-    public int getBallCount(){
-        return ballCount;
+        return computerNumber;
     }
 
 }
