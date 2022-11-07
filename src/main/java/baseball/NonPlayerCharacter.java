21ee7c2afb21b84061b252005a864e6967c05e97
yujaemin <woawoa0513@gmail.com>
2022-11-07 14:18:47 +0900

    feat: 게임 종료 안내문 출력 함수 구현
    3개의 숫자를 모두 맞히면 게임 종료/재시작 여부를 묻는다.

--- src/main/java/baseball/NonPlayerCharacter.java
+++ src/main/java/baseball/NonPlayerCharacter.java
@@ -9,4 +9,9 @@ public class NonPlayerCharacter {
     public void printNumberInputNotice() {
         System.out.print("숫자를 입력해주세요 : ");
     }
+
+    public void printGameEndNotice() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                            "게임 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
