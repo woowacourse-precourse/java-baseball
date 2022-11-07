319107a7576bf160be0e026bed8c2f3308522bfa
koo9b9h <90121024+koo9b9h@users.noreply.github.com>
2022-11-07 15:19:58 +0900

    GameStartSetting 메소드명 변경
    1. 게임 시작문구 출력과 랜덤 숫자 전달(start)
    
    2. 랜덤 숫자 3자리 설정(computerNumberPut)
    -고유기능으로 판단하고 분리 기능상 게임시작시 1번 작동하므로 main과 함께 작동하도록 관리
    
    3. 사용자 입력값 받기(userNumberPut)
    -요구사항에 의해 사용자의 잘못된 값발생 시 예외 발생

--- src/main/java/baseball/GameStartSetting.java
+++ src/main/java/baseball/GameStartSetting.java
@@ -4,13 +4,13 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class GameStartSetting {
-	public String main() {
+	public String start() {
 		System.out.println("숫자 야구 게임을 시작합니다.");
 		
-		return computerNumber();
+		return computerNumberPut();
 	}//main
 	
-	private String computerNumber() {
+	private String computerNumberPut() {
 		String cumputerNumber = "";
 		for(;cumputerNumber.length()<3;) {
 		cumputerNumber = cumputerNumber+Randoms.pickNumberInRange(0, 9);
@@ -18,7 +18,7 @@ public class GameStartSetting {
 		return cumputerNumber;
 	}//computerNumber
 	
-	public String userInputNumber() {
+	public String userNumberPut() {
 		String userInputNumber = "";
 		System.out.print("숫자를 입력해주세요 :");
 		userInputNumber = Console.readLine();
