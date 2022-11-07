48361e706db3b19da8f448c8834374fdf9376cbd
kimdeokyoung <dudejrss@naver.com>
2022-11-07 19:02:51 +0900

    chore (src.test.java.baseball.CustomTest) : State 상태전이가 제대로 동작하고 있는지 확인하는 테스트코드 작성

--- src/test/java/baseball/CustomTest.java
+++ src/test/java/baseball/CustomTest.java
@@ -19,19 +19,60 @@ public class CustomTest {
 
         @Test
         @DisplayName("생성된 숫자가 없거나 비어있는 리스트인지 확인")
-        void generatedNumberisEmptyOrNull() {
+        void generatedNumberIsEmptyOrNull() {
             assertThat(bg.answerNumber).isNotNull().isNotEmpty();
         }
 
         @Test
         @DisplayName("생성된 숫자가 valid 한지 확인")
-        void genreatedNumberisValid() {
+        void generatedNumberIsValid() {
             List<Integer> an = bg.answerNumber;
-            for(int i : an){
-                assertThat(an.parallelStream().filter(v->v==i).count() == 1).isTrue();
+            for (int i : an) {
+                assertThat(an.parallelStream().filter(v -> v == i).count() == 1).isTrue();
             }
 
         }
+    }
+
+    @Nested
+    public class CustomStateRulesTest {
+
+        @Test
+        @DisplayName("상태전이 규칙이 제대로 적용되었는지 확인 : ON_GAME --> EXIT_GAME")
+        void onGameWithInvalidUserInput() {
+            assertThat(BaseBallGame.rules.get(List.of(BaseBallGame.State.ON_GAME.ordinal(), BaseBallGame.Trigger.INVALID_USER_INPUT.ordinal())))
+                    .isEqualTo(BaseBallGame.State.EXIT_GAME);
+        }
+
+        @Test
+        @DisplayName("상태전이 규칙이 제대로 적용되었는지 확인 : ON_GAME --> FINISH_GAME")
+        void onGameWithCorrectAnswer() {
+            assertThat(BaseBallGame.rules.get(List.of(BaseBallGame.State.ON_GAME.ordinal(), BaseBallGame.Trigger.CORRECT_ANSWER.ordinal())))
+                    .isEqualTo(BaseBallGame.State.FINISH_GAME);
+        }
+
+        @Test
+        @DisplayName("상태전이 규칙이 제대로 적용되었는지 확인 : ON_GAME --> ON_GAME")
+        void onGameWithIncorrectAnswer() {
+            assertThat(BaseBallGame.rules.get(List.of(BaseBallGame.State.ON_GAME.ordinal(), BaseBallGame.Trigger.INCORRECT_ANSWER.ordinal())))
+                    .isEqualTo(BaseBallGame.State.ON_GAME);
+        }
+
+        @Test
+        @DisplayName("상태전이 규칙이 제대로 적용되었는지 확인 : FINISH_GAME --> EXIT_GAME")
+        void finshGameWithExit() {
+            assertThat(BaseBallGame.rules.get(List.of(BaseBallGame.State.FINISH_GAME.ordinal(), BaseBallGame.Trigger.EXIT.ordinal())))
+                    .isEqualTo(BaseBallGame.State.EXIT_GAME);
+        }
+
+        @Test
+        @DisplayName("상태전이 규칙이 제대로 적용되었는지 확인 : FINISH_GAME --> START_GAME")
+        void finshGameWithRegame() {
+            assertThat(BaseBallGame.rules.get(List.of(BaseBallGame.State.FINISH_GAME.ordinal(), BaseBallGame.Trigger.RE_GAME.ordinal())))
+                    .isEqualTo(BaseBallGame.State.START_GAME);
+        }
+
+
     }
 
 }
