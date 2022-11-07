881838e891ed678affae0e3ba911ab0c32ec000a
eom-tae-in <eti0728@daum.net>
2022-11-07 19:14:05 +0900

    feat(BaseballGameController): start() -> 게임을 시작하고 플레이어의 입력 값에 대한 스트라이크, 볼, 낫싱의 정보를 알려주는 메서드

---
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -0,0 +1,32 @@
+package baseball.controller;
+
+import baseball.model.AnswerNumber;
+import baseball.model.PlayerNumber;
+import baseball.service.HintService;
+import baseball.view.OutputView;
+
+public class BaseballGameController {
+
+    private final HintService hintService;
+    private final AnswerNumber answerNumber;
+    private final PlayerNumber playerNumber;
+
+    public BaseballGameController() {
+
+        hintService = new HintService();
+        answerNumber = new AnswerNumber();
+        playerNumber = new PlayerNumber();
+    }
+
+    public void start() {
+
+        answerNumber.getNewAnswer();
+        do {
+            receiveInputNumberFromPlayer();
+            OutputView.printHintMessage(hintService.getHint(answerNumber.getAnswer(), playerNumber.getPlayerNumber()));
+            System.out.println(answerNumber.getAnswer());
+        } while (!hintService.isThreeStrike());
+        OutputView.printPlayerCorrectAnswerMessage();
+    }
+
+}
