9ded3db8378cc9865d464b15cf762bce24e85970
YooSuYeon <usuyn000@gmail.com>
2022-11-08 02:10:43 +0900

    feat: 플레이어 입력 숫자 안 중복 검사

--- src/main/java/inputoutput/Validation.java
+++ src/main/java/inputoutput/Validation.java
@@ -2,11 +2,15 @@ package inputoutput;
 
 import constants.Format;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class Validation {
     public boolean validatePlayerNumber(String playerNumber) {
-        if (validateFormatPlayerNumber(playerNumber)) {
+        if (validateFormatPlayerNumber(playerNumber) && validateAllDifferentDigit(playerNumber)) {
             return true;
         }
 
@@ -16,11 +20,15 @@ public class Validation {
     private boolean validateFormatPlayerNumber(String playerNumber) {
         Format format = Format.PLAYER_NUMBER;
 
-        if (!Pattern.matches(format.getRegex(), playerNumber)) {
-            throw new IllegalArgumentException();
-        }
+        return Pattern.matches(format.getRegex(), playerNumber);
+    }
 
-        return true;
+    private boolean validateAllDifferentDigit(String playerNumber) {
+        List<String> number = new ArrayList<>(Arrays.asList(playerNumber.split("")));
+
+        return number.stream()
+                .allMatch(num ->
+                        Collections.frequency(number, num) == 1);
     }
 
 }
