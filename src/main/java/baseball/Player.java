6e7e474e95866212b6ce350932cec711ad687feb
kssumin <201566@jnu.ac.kr>
2022-11-07 20:18:36 +0900

    feat : 사용자로부터 입력받은 수 1 ~ 9 사이의 수인지 validation

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,18 +1,30 @@
 package baseball;
 
+import baseball.ball.Ball;
 import baseball.ball.Balls;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Player {
     private Balls playerBalls;
 
-    private List<String> inputPlayerNumber(){
+    public Player() {
+        playerBalls = new Balls();
+    }
+
+    private List<Ball> createBallByInput(List<String> playNumber) {
+        return playNumber.stream()
+                .map(ballNumber -> new Ball(Integer.parseInt(ballNumber)))
+                .collect(Collectors.toList());
+    }
+
+    private List<String> inputPlayerNumber() {
         OutputView.requestInputNumber();
-        String playerCandidateNumber=InputView.inputPlayerNumber();
+        String playerCandidateNumber = InputView.inputPlayerNumber();
         return Arrays.asList(playerCandidateNumber.split(""));
     }
 }
