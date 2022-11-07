d395a837315751663fd054d562832ff4d840912e
LeeMyeongJin <=>
2022-11-08 02:37:20 +0900

    Feat: getNumber static 제거

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -1,17 +1,17 @@
 package baseball;
 
 public class Judge {
-    public static String GameStart(String computer, String user){
+    public String GameStart(String computer, String user) {
         Rule rule = new Rule();
         int strike = rule.strike(computer, user);
         int total = rule.contain(computer, user);
         int ball = total - strike;
 
-        if (total == 0){
+        if (total == 0) {
             return "낫싱";
-        }else if(strike == 0){
+        } else if (strike == 0) {
             return ball + "볼";
-        }else if(ball == 0){
+        } else if (ball == 0) {
             return strike + "스트라이크";
         }
         return ball + "볼 " + strike + "스트라이크";
