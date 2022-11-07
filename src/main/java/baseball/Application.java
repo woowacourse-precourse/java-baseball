7a8a2ffcfa28ee49334a4e989425df1032a31ebd
DoooongDong <rhkswls98@kakao.com>
2022-11-08 00:03:03 +0900

    <Refactor> create 패키지 생성 후 정답 숫자와 유저 숫자 클래스를 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,24 +4,16 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Application {
-    //랜덤 값을 출력하는 함수 작성
-    public static ArrayList<Integer> createRandomNumber(){
-        ArrayList<Integer> randomNumbers = new ArrayList<>();
-        while (randomNumbers.size() < 3){
-            int randomNumber = Randoms.pickNumberInRange(1,9);
-            if(!randomNumbers.contains(randomNumber)){
-                randomNumbers.add(randomNumber);
-            }
-        }
-        return randomNumbers;
-    }
+
+
+    //user 값을 생성
+
     public static void main(String[] args) {
-        //맞춰야 할 컴퓨터 숫자 목록을 할당
-        List<Integer> computer = createRandomNumber();
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        
+
     }
 }
