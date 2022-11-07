d5bb25fd9f70521760630854133a81e1e34224db
sunghyuki <sunghyuk1609@gmail.com>
2022-11-07 19:31:51 +0900

    feat : 랜덤수를 만들고, 게임 플레이어의 입력을 받고, 컴퓨터와 게임 플레이어의 수를 비교해서 야구 규칙을 출력하는 게임의 흐름을 진행시키는 클래스 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,12 +1,34 @@
 package baseball;
 
+import java.util.ArrayList;
+
 public class Game {
-    private BaseballScore baseballScore;
+    private Computer computer;
+    private Referee referee;
 
     public Game() {
-        this.baseballScore = new BaseballScore();
+        computer = new Computer(new ArrayList<>());
+        computer.generateRandomNumbers();
+        referee = new Referee(computer);
     }
 
     public void play() {
+        do {
+            referee.compareComputerWith(Translator.translateStringToIntegerList(InputView.inputNumbers()));
+            printHint(referee.getBaseballScore());
+        } while(referee.getBaseballScore().getStrike() != 3);
+        OutputView.showVictoryMessage();
+    }
+
+    private void printHint(BaseballScore baseballScore) {
+        if(baseballScore.getBall() == 4) {
+            OutputView.showFourBallResult();
+        } else if (baseballScore.getStrike() == 0 && baseballScore.getBall() > 0) {
+            OutputView.showOnlyBallResult(baseballScore);
+        } else if(baseballScore.getStrike() > 0 && baseballScore.getBall() == 0) {
+            OutputView.showOnlyStrikeResult(baseballScore);
+        } else if(baseballScore.getStrike() > 0 && baseballScore.getBall() > 0) {
+            OutputView.showStrikeAndBallResult(baseballScore);
+        }
     }
 }
