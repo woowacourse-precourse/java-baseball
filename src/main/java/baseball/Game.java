d4a6356e7fa198121f37d4af2ffec3d27272362a
sunghyuki <sunghyuk1609@gmail.com>
2022-11-07 16:09:34 +0900

    feat : 볼, 스트라이크에 대한 야구 룰을 관리하는 클래스 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,6 +1,11 @@
 package baseball;
 
 public class Game {
+    private BaseballScore baseballScore;
+
+    public Game() {
+        this.baseballScore = new BaseballScore();
+    }
 
     public void play() {
     }
