00a55059396914ab1ba1c95ead4e2229803a035c
KimSunU <kgw6147@naver.com>
2022-11-08 02:55:08 +0900

    [feat]BaseballMachine - computerSet 구현

---
+++ src/main/java/baseball/BaseballMachine.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BaseballMachine {
+
+    /**
+     * 컴퓨터가 가지는 숫자
+     * 랜덤으로 중복되는거 없게 지급
+     */
+    public static List<Integer> computerSet(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+}
