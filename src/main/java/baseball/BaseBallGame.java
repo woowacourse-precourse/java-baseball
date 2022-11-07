e7cea61259e17114136abfd3b9a55c43acf2a5d2
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-07 11:06:35 +0900

    feat(BaseBallGame): BaseBallGame 클래스의 멤버면수와 생성자 작성(기능목록2-1)

---
+++ src/main/java/baseball/BaseBallGame.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class BaseBallGame {
+    private String userNumber;
+    private Computer computer;
+    private int ballCount;
+    private int strikeCount;
+    private String restartToken;
+    private Scanner scanner;
+
+    public BaseBallGame() {
+        computer = new Computer();
+        ballCount = 0;
+        strikeCount= 0;
+        restartToken = "";
+        scanner = new Scanner(System.in);
+    }
+}
