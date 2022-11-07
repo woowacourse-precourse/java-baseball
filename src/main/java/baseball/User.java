f7795e07727a3803c3f2739b9dba6708bfef79d9
hyeonseog-won <smreo75@gmail.com>
2022-11-07 19:48:43 +0900

    refactor: 입력 받는 로직과 중복 체크 로직 분리

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -13,24 +15,31 @@ public class User {
     public void inputNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        if (verifyInput(input)) {
-            duplicateCheck(input);
+        validateInput(input);
+        numbers = getNumbers(input);
+        validateDuplicate(numbers);
+    }
+
+    private void validateInput (String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("입력한 수가 3자리가 아닙니다.");
         }
-        throw new IllegalArgumentException("숫자는 1부터 9까지로 이루어져야 합니다.");
+        if (!input.matches("^[1-9]*$")) {
+            throw new IllegalArgumentException("입력한 수가 1-9가 아닙니다.");
+        }
     }
 
-    public boolean verifyInput(String input) {
-        return Pattern.matches("^[1-9]*$", input);
-    }
-
-    public List<Integer> duplicateCheck(String input) {
-        numbers = Arrays.stream(input.split(""))
+    public List<Integer> getNumbers(String input) {
+        return numbers = Arrays.stream(input.split(""))
                 .map(Integer::parseInt)
                 .distinct()
                 .collect(Collectors.toList());
-        if (numbers.size() != 3) {
+    }
+
+    private void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> duplicateCheck = new HashSet<>(numbers);
+        if (duplicateCheck.size() != 3) {
             throw new IllegalArgumentException("서로 다른 수로 이루어진 3자리의 수를 입력해야 합니다.");
         }
-        return numbers;
     }
 }
