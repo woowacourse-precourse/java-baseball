cf5195f2019934128327fd57f5886d9c39c217b0
hdsung98 <hdsung98@gmail.com>
2022-11-07 15:20:48 +0900

    style : modify variable and method name according to convention
    1. 지역변수 선언을 메소드 선두에 배치
    2. 변수명 메소드명의 언더바를 제거하고 컨벤션에 맞게 수정(필요에 따라 메소드명 자체도 수정)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,10 +9,10 @@ import java.util.HashSet;
 
 
 public class Application {
-    static int three_strike = 0;
-    static int first_game = 1;
+    static int threeStrike = 0;
+    static int firstGame = 1;
 
-    static void random_number_generate(ArrayList<Integer> answer) {
+    static void generateRandomNumber(ArrayList<Integer> answer) {
         while (answer.size() != 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!answer.contains(randomNumber)) {
@@ -21,63 +21,69 @@ public class Application {
         }
     }
 
-    static ArrayList<Integer> parse_string_into_integer(String guess) {
-        int guess_int_format = Integer.parseInt(guess);
+    static ArrayList<Integer> parseStringIntoInteger(String guess) {
+        int guessIntFormat = Integer.parseInt(guess);
         ArrayList<Integer> parsed = new ArrayList<>();
 
-        for (int divisor = 100; divisor != 0; guess_int_format %= divisor, divisor /= 10) {
-            parsed.add(guess_int_format / divisor);
+        for (int divisor = 100; divisor != 0; guessIntFormat %= divisor, divisor /= 10) {
+            parsed.add(guessIntFormat / divisor);
         }
         return parsed;
     }
 
-    static boolean guess_validity_check(String guess) throws IllegalArgumentException {
-        Integer guess_int_format = Integer.parseInt(guess);
-        if (guess_int_format < 123 || guess_int_format > 987) {
+    static boolean checkValidityOfGuess(String guess) throws IllegalArgumentException {
+        Integer guessIntFormat = Integer.parseInt(guess);
+        ArrayList<Integer> guessParsed = parseStringIntoInteger(guess);;
+        Set<Integer> uniqueElementOnly;
+
+        if (guessIntFormat < 123 || guessIntFormat > 987) {
             throw new IllegalArgumentException("범위를 준수하지 않았습니다.");
         }
 
-        ArrayList<Integer> guess_parsed = parse_string_into_integer(guess);
-        for (Integer toCheck : guess_parsed) {
+        for (Integer toCheck : guessParsed) {
             if (toCheck == 0) {
                 throw new IllegalArgumentException("1부터 9까지의 수로만 이루어져야 합니다.");
             }
         }
 
-        Set<Integer> unique_element_only = new HashSet<>(guess_parsed);
-        if (unique_element_only.size() != guess_parsed.size()) {
+        uniqueElementOnly = new HashSet<>(guessParsed);
+        if (uniqueElementOnly.size() != guessParsed.size()) {
             throw new IllegalArgumentException("숫자의 중복이 있습니다.");
         }
+
         return true;
     }
 
-    static ArrayList<Integer> count_ball_and_strike(ArrayList<Integer> guessList, ArrayList<Integer> answers) {
-        ArrayList<Integer> ball_strike_cnt = new ArrayList<>() {{
+    static ArrayList<Integer> countBallAndStrike(ArrayList<Integer> guessList, ArrayList<Integer> answers) {
+        ArrayList<Integer> ballStrikeCnt = new ArrayList<>() {{
             add(0);
             add(0);
         }};
+        Integer strikeCnt;
+        Integer ballCnt;
+
         for (int idx = 0; idx < guessList.size(); idx++) {
             int guess = guessList.get(idx);
             if (guess == answers.get(idx)) {
-                Integer strike_cnt = ball_strike_cnt.get(1);
-                ball_strike_cnt.set(1, ++strike_cnt);
+                strikeCnt = ballStrikeCnt.get(1);
+                ballStrikeCnt.set(1, ++strikeCnt);
             } else if (answers.contains(guess)) {
-                Integer ball_cnt = ball_strike_cnt.get(0);
-                ball_strike_cnt.set(0, ++ball_cnt);
+                ballCnt = ballStrikeCnt.get(0);
+                ballStrikeCnt.set(0, ++ballCnt);
             }
         }
 
-        return ball_strike_cnt;
+        return ballStrikeCnt;
     }
 
-    static void show_result(ArrayList<Integer> ball_strike_cnt) {
-        Integer ball = ball_strike_cnt.get(0);
-        Integer strike = ball_strike_cnt.get(1);
+    static void showResult(ArrayList<Integer> ballStrikeCnt) {
+        Integer ball = ballStrikeCnt.get(0);
+        Integer strike = ballStrikeCnt.get(1);
 
         if (ball == 0 && strike == 0) {
             System.out.println("낫싱");
         } else if (strike == 3) {
-            three_strike = 1;
+            threeStrike = 1;
             System.out.println("3스트라이크");
         } else if (ball == 0) {
             System.out.printf("%d스트라이크\n", strike);
@@ -88,32 +94,40 @@ public class Application {
         }
     }
 
-    static void judge_guess(String guess, ArrayList<Integer> answer) {
-        ArrayList<Integer> guess_parsed = parse_string_into_integer(guess);
-        ArrayList<Integer> ball_strike_cnt = count_ball_and_strike(guess_parsed, answer);
-        show_result(ball_strike_cnt);
+    static void judgeGuess(String guess, ArrayList<Integer> answer) {
+        ArrayList<Integer> guessParsed;
+        ArrayList<Integer> ballStrikeCnt;
+
+        guessParsed = parseStringIntoInteger(guess);
+        ballStrikeCnt = countBallAndStrike(guessParsed, answer);
+        showResult(ballStrikeCnt);
     }
 
-    static void main_loop() {
+    static void runLoop() {
         ArrayList<Integer> answer = new ArrayList<>();
-        random_number_generate(answer);
+        String guess;
+
+        generateRandomNumber(answer);
         do {
             System.out.print("숫자를 입력해주세요 : ");
-            String guess = Console.readLine();
-            if (guess_validity_check(guess)) {
-                judge_guess(guess, answer);
+            guess = Console.readLine();
+            if (checkValidityOfGuess(guess)) {
+                judgeGuess(guess, answer);
             }
-        } while (three_strike != 1);
+        } while (threeStrike != 1);
     }
 
-    static void check_to_restart() throws IllegalArgumentException {
+    static void endOrRestart() throws IllegalArgumentException {
+        String decisionString;
+        int decision;
+
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         do {
-            String decision_string = Console.readLine();
-            int decision = Integer.parseInt(decision_string);
+            decisionString = Console.readLine();
+            decision = Integer.parseInt(decisionString);
             if (decision == 1) {
-                init_and_start_game();
+                initializeAndStartGame();
                 return;
             } else if (decision == 2) {
                 return;
@@ -123,18 +137,18 @@ public class Application {
         } while (true);
     }
 
-    static void init_and_start_game() {
-        three_strike = 0;
+    static void initializeAndStartGame() {
+        threeStrike = 0;
 
-        if (first_game == 1) {
+        if (firstGame == 1) {
             System.out.println("숫자 야구 게임을 시작합니다.");
-            first_game = 0;
+            firstGame = 0;
         }
-        main_loop();
-        check_to_restart();
+        runLoop();
+        endOrRestart();
     }
 
     public static void main(String[] args) {
-        init_and_start_game();
+        initializeAndStartGame();
     }
 }
