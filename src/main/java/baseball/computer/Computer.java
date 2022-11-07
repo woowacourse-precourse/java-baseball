21171d8f654f2ace9bd88540c48dd60ee6b257dc
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-08 07:55:18 +0900

    fix(computer/Computer.java): 램덤함수 수정

--- src/main/java/baseball/computer/Computer.java
+++ src/main/java/baseball/computer/Computer.java
@@ -2,26 +2,30 @@ package baseball.computer;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    private List<Integer> computerNum;
+    private List<Integer> computerNum = new ArrayList<>();;
     private static final int RANDOM_MIN = 1;
     private static final int RANDOM_MAX = 9;
+    private static final int RANDOM_NUM_LENGTH = 9;
 
     public Computer(){
-        setComputerNum();
+        createRandomNum();
     }
 
     public List<Integer> getComputerNum(){
         return computerNum;
     }
 
-    public void setComputerNum(){
-        computerNum = randomNum();
+    public void createRandomNum(){
+        while(computerNum.size() != RANDOM_NUM_LENGTH){
+            int number = Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
+            if (!computerNum.contains(number)){
+                computerNum.add(number);
+            }
+        }
     }
 
-    public List<Integer> randomNum(){
-        return Randoms.pickUniqueNumbersInRange(RANDOM_MIN, RANDOM_MAX, 3);
-    }
 }
