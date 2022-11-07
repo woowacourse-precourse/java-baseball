2689b2d7c21b1992686664edb9817d820c643cb6
김태용 <amico741@naver.com>
2022-11-08 01:18:14 +0900

    refactor: 숫자 비교 결과 Result 클래스로 변경
    strike, ball  결과를 동시 반환하기 위해 클래스 생성

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -6,17 +6,19 @@ public class Judge {
   private Computer computer;
   private Player player;
   private static final int MAX_SIZE = 3;
+
   public Judge(){
     this.player = new Player();
     this.computer = new Computer();
     System.out.println("숫자 야구 게임을 시작합니다.");
   }
 
-  public int compareBalls(){
+  public Result compareBalls(){
     ArrayList<Integer> playerNumbers = this.player.getNumbers();
     ArrayList<Integer> computerNumbers = this.computer.getNumbers();
     int strike = 0;
     int ball = 0;
+
     for(int i = 0; i < MAX_SIZE; i++){
       if(playerNumbers.get(i).equals(computerNumbers.get(i))) {
         strike ++;
@@ -24,6 +26,6 @@ public class Judge {
         ball++;
       }
     }
-    return strike;
+    return new Result(strike, ball);
   }
 }
