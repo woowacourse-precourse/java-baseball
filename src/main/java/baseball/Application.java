0a39e887ab278f4d4e90786f5427ca07e835f351
yoonjuho <dbswn990@naver.com>
2022-11-08 06:34:52 +0900

    refactor($Application) : 변수명 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,20 +4,19 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        Number numFactory = new PlayerNum();
+        Number playerNumFactory = new PlayerNum();
         Computer computer = new Computer();
         List<Integer> playerNumber;
-
-        do{
-            playerNumber = numFactory.createNumber();
-            numFactory.verifyNumber(playerNumber);
+        do {
+            playerNumber = playerNumFactory.createNumber();
+            playerNumFactory.verifyNumber(playerNumber);
             String gameStatus = computer.getResult(playerNumber);
-            if(gameStatus == GameComment.RESTART){
+            if (gameStatus == GameComment.RESTART) {
                 computer = new Computer();
-            }else if (gameStatus == GameComment.TERMINATE) {
+            } else if (gameStatus == GameComment.TERMINATE) {
                 break;
             }
-            }while (true);
-        }
+        } while (true);
+    }
 
 }
