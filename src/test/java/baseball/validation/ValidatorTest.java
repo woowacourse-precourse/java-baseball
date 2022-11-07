7685a5218720cb565ccc5fe3240ff4963c424502
jyj1143 <jyj1143@gmail.com>
2022-11-08 00:03:46 +0900

    test(ValidatorTest): 게임 플레이어가 입력한 값이 3자리 자연수가 아닌 경우 예외처리 테스트

---
+++ src/test/java/baseball/validation/ValidatorTest.java
@@ -0,0 +1,19 @@
+package baseball.validation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ValidatorTest {
+
+    private static final int MAX_VALUE = 9;
+    private static final int MIN_VALUE = 1;
+    private static final int COUNT_VALUE = 3;
+
+    @Test
+    void validateInputLength() {
+        assertThatThrownBy(() -> {Validator.validatePlayerInput("1234");})
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[IllegalArgumentException]: " + COUNT_VALUE + "자리 자리자연수가 아닙니다.\n");
+    }
+}
