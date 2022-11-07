e0f6820f7cedeeef32f91f5fb7f4320a66d51cd5
dhyyp1001 <dhyyp1001@gmail.com>
2022-11-07 15:28:57 +0900

    showProblem() : 게임 생성 / 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,6 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-
     }
 
     static int readUser() {
@@ -40,5 +39,29 @@ public class Application {
         return result;
     }
 
+    static void showProblem() {
+        List<Integer> computerList = writeComputer();
+        boolean bool = true;
 
+        while (bool) {
+            System.out.print("\n숫자를 입력해주세요 : ");
+            List<Integer> userList = writeUser();
+            if (userList.size() > 3){
+                throw new IllegalArgumentException();
+            }
+
+            List<Integer> answer = check(userList, computerList);
+            if (answer.get(0) != 0)
+                System.out.print(answer.get(0) + "볼 ");
+            if (answer.get(1) != 0)
+                System.out.print(answer.get(1) + "스트라이크");
+
+            if (answer.get(1) == 3) {
+                System.out.println("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                bool = false;
+                lastMessage();
+            } else if (answer.get(2) == 3)
+                System.out.print("낫싱");
+        }
+    }
 }
