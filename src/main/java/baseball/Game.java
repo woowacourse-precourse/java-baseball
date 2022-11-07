c5831f579044d1f3f2346069a53a7d1d0ad1295c
limecats <limecats0331@gmail.com>
2022-11-07 16:44:29 +0900

    feat(Game): add gameStart
    show gameStart message

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -9,6 +9,10 @@ public class Game {
 
     private final int END_STRIKE_COUNT = 3;
 
+    public void gameStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     public List<Integer> setComputersRandomAnswer() {
         List<Integer> answer = new ArrayList<>();
 
