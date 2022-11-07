015089694b07006b2750e743f3b48ddc7919621a
jongmyeongbak <withpleasure@gmail.com>
2022-11-07 14:20:17 +0900

    feat: FEATURE #1 - generate computer numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,27 +1,47 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
 
-    private static boolean keepGaming = true;
+    static boolean keepGaming = true;
 
-    public static void main(String[] args) {
+    static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         do {
             playSmallGame();
             isKeepGaming();
         } while (keepGaming);
     }
 
-    private boolean isKeepGaming() {
+    static boolean isKeepGaming() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String endGame = Console.readLine();
-        if (endGame == "1") {
+        if (endGame.equals("1")) {
             keepGaming = true;
-        } else if (endGame == "2") {
+        } else if (endGame.equals("2")) {
             keepGaming = false;
         } else {
             throw new IllegalArgumentException("잘못된 값을 입력하여 프로그램을 종료합니다.");
         }
         return keepGaming;
+    }
+
+    static void playSmallGame() {
+        generateNumber();
+    }
+
+    static List<Integer> generateNumber() {
+        List<Integer> comNumbers = new ArrayList<>();
+        do {
+            int comNum = Randoms.pickNumberInRange(1, 9);
+            if (!comNumbers.contains(comNum)) {
+                comNumbers.add(comNum);
+            }
+        } while (comNumbers.size() < 3);
+        return comNumbers;
     }
 }
