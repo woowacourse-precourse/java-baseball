bb923addce2123399d3220e6a6307fae292e7358
Ilchul Kim <huj0159@gmail.com>
2022-11-08 07:22:56 +0900

    test: add test is exception thrown when wrong value is given by user

--- src/test/java/baseball/BaseBallGameTest.java
+++ src/test/java/baseball/BaseBallGameTest.java
@@ -1,16 +1,18 @@
 package baseball;
 
 import baseball.BaseBallGame;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BaseBallGameTest {
     @Test
     void DTO를_입력받아_결과값을_출력() {
         //given
         GameController gc = new GameController();
-        BaseballGameResultDto testDto = new BaseballGameResultDto(1, 2, false);
+        BaseballGameResultDto testDto = new BaseballGameResultDto(1, 2);
 
         //when
         String result = gc.printResult(testDto);
@@ -19,6 +21,13 @@ public class BaseBallGameTest {
         assertThat(result).isEqualTo("2볼 1스트라이크 ");
     }
 
+    @Test
+    void 잘못된_사용자_입력시_예외_발생() {
+        BaseBallGame bg = new BaseBallGame(3);
+        assertThatThrownBy(() -> {
+            bg.submitAnswer("예외발생");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     void 사용자의_정답을_입력받아_결과값_출력() {
@@ -33,9 +42,9 @@ public class BaseBallGameTest {
         firstGame.useCustomAnswer(testAnswer);
 
         //then
-        assertThat(gc.printResult(firstGame.submitAnswer("713"))).isEqualTo("3볼 ");
-        assertThat(gc.printResult(firstGame.submitAnswer("731"))).isEqualTo("2볼 1스트라이크 ");
-        assertThat(gc.printResult(firstGame.submitAnswer("986"))).isEqualTo("낫싱");
-        assertThat(gc.printResult(firstGame.submitAnswer("137"))).isEqualTo("3스트라이크 ");
+        assertThat(gc.printResult(firstGame.matchAnswer("713"))).isEqualTo("3볼 ");
+        assertThat(gc.printResult(firstGame.matchAnswer("731"))).isEqualTo("2볼 1스트라이크 ");
+        assertThat(gc.printResult(firstGame.matchAnswer("986"))).isEqualTo("낫싱");
+        assertThat(gc.printResult(firstGame.matchAnswer("137"))).isEqualTo("3스트라이크 ");
     }
 }
