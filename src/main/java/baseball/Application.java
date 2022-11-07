77e8dbc3315cae33d21bae07e3a5da3bd3fe6c63
Min-ji99 <61926751+Min-ji99@users.noreply.github.com>
2022-11-07 09:04:07 +0900

    Refactor : Application 기능3 추가 입력값의 결과 출력 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,8 +6,9 @@ public class Application {
     public static void main(String[] args) throws IllegalArgumentException{
         RandomNumberGenerator randomNumberGenerator=new RandomNumberGenerator();
         InputChecker inputChecker=new InputChecker();
-
+        AnswerResultJudgement answerResultJudgement=new AnswerResultJudgement(randomNumberGenerator.getRandomNumber());
         String answer=Console.readLine();
         inputChecker.checkInput(answer);
+        System.out.println(answerResultJudgement.getResult(answer));
     }
 }
