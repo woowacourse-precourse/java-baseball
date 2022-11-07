032ecfab4e467d63419fe9b6d0e33e300fb423de
kariskan <zkfzpf56@naver.com>
2022-11-07 15:00:57 +0900

    feat: 컴퓨터 숫자 생성을 위한 Computer 클래스 및 멤버 변수 작성

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,12 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Computer {
+
+    private List<Integer> computerNumbers;
+
+    public List<Integer> getComputerNumbers() {
+        return computerNumbers;
+    }
+}
