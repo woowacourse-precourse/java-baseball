63a66e9f4527823b92d992a4bfa212f5b712b096
lullulu__ <75472116+earthyoung@users.noreply.github.com>
2022-11-07 17:14:36 +0900

    feat: validate user input length, its format(number or not) and its uniqueness

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -36,7 +36,10 @@ public class Application {
     }
 
     public static void checkValidInput(String input){
-
+        System.out.println();
+        checkCorrectLength(input);  // input 길이가 3인지 확인
+        checkNumber(input); // input 전부 숫자인지 확인
+        checkUniqueNumber();    // input 각 숫자가 서로 다른지 확인
     }
 
     public static boolean checkResult(){
@@ -45,6 +48,28 @@ public class Application {
 
     public static boolean checkExitGame(){
         return false;
+    }
+
+    public static void checkNumber(String input){
+        try{
+            for(int i=0; i<input.length(); i++){
+                int value = Integer.parseInt(input.substring(i, i+1));
+                current.add(value);
+            }
+        }catch(NumberFormatException nfe){throw new IllegalArgumentException();}
+    }
+
+    public static void checkCorrectLength(String input){
+        if(input.length() != 3){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void checkUniqueNumber(){
+        for(int i=0; i<current.size(); i++){
+            int value = current.get(i);
+            if(current.indexOf(value) != current.lastIndexOf(value)) throw new IllegalArgumentException();  // 리스트에 값이 여러 개 존재한다는 의미이므로 IllegalArgumentException 발생
+        }
     }
 
 }
