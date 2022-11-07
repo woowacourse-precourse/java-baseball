cd2d2052b93bb68fccfe64694a09ede3dba06f58
yback1223 <yoyob1223@gmail.com>
2022-11-08 01:18:57 +0900

    style: apply google java conv.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,6 +6,7 @@ import baseball.io.IOTopClass;
 public class Application {
 
     private static final int LIST_SIZE = 3;
+
     public static void main(String[] args) {
         IOTopClass ioTopClass = new IOTopClass(LIST_SIZE);
         Game game = new Game(LIST_SIZE);
