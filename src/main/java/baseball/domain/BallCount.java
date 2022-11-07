72786bc52cd53cb23914270c68ea745b22a8f149
이건회 <rg970604@naver.com>
2022-11-07 18:56:09 +0900

    feat (BallCount.java) : 볼-스트라이크 판단이 완료된 결과 저장

---
+++ src/main/java/baseball/domain/BallCount.java
@@ -0,0 +1,11 @@
+package baseball.domain;
+
+public class BallCount {
+    private int Strikes;
+    private int Balls;
+
+    public BallCount(int strikes, int balls) {
+        Strikes = strikes;
+        Balls = balls;
+    }
+}
