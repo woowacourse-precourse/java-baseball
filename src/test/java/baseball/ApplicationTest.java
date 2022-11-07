057b36fe88d1d91c2402c82752cd40e73f2bf4e6
Haechan Lee <dlgockssla@gmail.com>
2022-11-08 03:19:07 +0900

    test: extract exceptionTest from ApplicationTest

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,8 +1,6 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -21,47 +19,10 @@ class ApplicationTest extends NsTest {
                 1, 3, 5, 5, 8, 9);
     }
 
-    @Nested
-    class IllegalArgumentException_Test {
-        @Test
-        void 사용자의_입력이_3자리가_아닐때() {
-            assertSimpleTest(() -> assertThatThrownBy(() -> runException("1234"))
-                    .isInstanceOf(IllegalArgumentException.class));
-            assertSimpleTest(() -> assertThatThrownBy(() -> runException("12"))
-                    .isInstanceOf(IllegalArgumentException.class));
-        }
-
-        @Test
-        void 사용자의_입력이_1에서_9사이의_숫자가_아닐때() {
-            assertSimpleTest(() -> assertThatThrownBy(() -> runException("ABC"))
-                    .isInstanceOf(IllegalArgumentException.class));
-            assertSimpleTest(() -> assertThatThrownBy(() -> runException("12A"))
-                    .isInstanceOf(IllegalArgumentException.class));
-            assertSimpleTest(() -> assertThatThrownBy(() -> runException("120"))
-                    .isInstanceOf(IllegalArgumentException.class));
-        }
-
-        @Test
-        void 사용자의_입력에_중복된_수가_있을때() {
-            assertSimpleTest(() -> assertThatThrownBy(() -> runException("122"))
-                    .isInstanceOf(IllegalArgumentException.class));
-            assertSimpleTest(() -> assertThatThrownBy(() -> runException("333"))
-                    .isInstanceOf(IllegalArgumentException.class));
-        }
-
-        @Test
-        void 게임종료_후_1_또는_2를_입력하지_않았을때() {
-            assertRandomNumberInRangeTest(
-                    () -> assertThatThrownBy(() -> runException("135", "3"))
-                            .isInstanceOf(IllegalArgumentException.class),
-                    1, 3, 5);
-
-            assertRandomNumberInRangeTest(
-                    () -> assertThatThrownBy(() -> runException("135", "RESTART"))
-                            .isInstanceOf(IllegalArgumentException.class),
-                    1, 3, 5);
-        }
-
+    @Test
+    void 예외_테스트() {
+        assertSimpleTest(() -> assertThatThrownBy(() -> runException("1234"))
+                .isInstanceOf(IllegalArgumentException.class));
     }
 
     @Override
