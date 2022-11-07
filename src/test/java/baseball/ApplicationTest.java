d981fc8a13636008139b6fac7647d91c71c98d88
josolha <josolha@nate.com>
2022-11-08 01:29:58 +0900

    test : unit tests for inputNum method

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,21 +1,16 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import camp.nextstep.edu.missionutils.test.NsTest;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import org.assertj.core.api.AbstractObjectArrayAssert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
-
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.*;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Nested
@@ -44,33 +39,59 @@ class ApplicationTest extends NsTest {
 
     @Nested
     class inputNumTest {
+        @Test
+        void 입력_숫자_중복_예외() {
+            assertThatThrownBy(() -> Application.inputNum("699"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
+        @Test
+        void 입력_숫자_숫자_이외값_예외() {
+            assertThatThrownBy(() -> Application.inputNum("다"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 입력_숫자_범위_예외() {
+            assertThatThrownBy(() -> Application.inputNum("1234"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void 입력_숫자_0숫자_예외() {
+            assertThatThrownBy(() -> Application.inputNum("071"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
+
     @Nested
-    class checkNumbersTest{
+    class checkNumbersTest {
         @Test
         void 체크_넘버즈_스트라이크_테스트() {
             List<Integer> computerTest = Arrays.asList(1, 2, 3);
             List<Integer> userTest = Arrays.asList(1, 2, 3);
-            Assertions.assertThat(Application.checkNumbers(computerTest,userTest)).isEqualTo("3스트라이크");
+            Assertions.assertThat(Application.checkNumbers(computerTest, userTest)).isEqualTo("3스트라이크");
         }
+
         @Test
         void 체크_넘버즈_볼_테스트() {
             List<Integer> computerTest = Arrays.asList(1, 2, 3);
             List<Integer> userTest = Arrays.asList(2, 3, 1);
-            Assertions.assertThat(Application.checkNumbers(computerTest,userTest)).isEqualTo("3볼");
+            Assertions.assertThat(Application.checkNumbers(computerTest, userTest)).isEqualTo("3볼");
         }
+
         @Test
         void 체크_넘버즈_볼_스트라이크_테스트() {
             List<Integer> computerTest = Arrays.asList(1, 2, 3);
             List<Integer> userTest = Arrays.asList(2, 1, 3);
-            Assertions.assertThat(Application.checkNumbers(computerTest,userTest)).isEqualTo("2볼 1스트라이크");
+            Assertions.assertThat(Application.checkNumbers(computerTest, userTest)).isEqualTo("2볼 1스트라이크");
         }
+
         @Test
         void 체크_넘버즈_낫싱_테스트() {
             List<Integer> computerTest = Arrays.asList(1, 2, 3);
             List<Integer> userTest = Arrays.asList(4, 5, 6);
-            Assertions.assertThat(Application.checkNumbers(computerTest,userTest)).isEqualTo("낫싱");
+            Assertions.assertThat(Application.checkNumbers(computerTest, userTest)).isEqualTo("낫싱");
         }
 
 
