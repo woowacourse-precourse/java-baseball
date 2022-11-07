7425d139cbb977359efcf56d0f3da561645dbaf3
5nam <ohnam90@gmail.com>
2022-11-07 17:18:52 +0900

    refactor: 상수화된 것을 모아두는 클래스 생성

---
+++ src/main/java/baseball/Constant.java
@@ -0,0 +1,7 @@
+package baseball;
+
+public class Constant {
+    public static final String STRIKE = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final int MAX = 3;
+    public static final int OVER = 2;
+}
