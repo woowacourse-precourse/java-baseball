38acc3682d7b482796ba3d20c10d190c48ae60d2
miffyKing <whqudghk28@gmail.com>
2022-11-08 00:29:18 +0900

    refactor: Remove unnecessary user, game, referee class generating.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,40 +1,38 @@
 package baseball;
 
+import IOController.Screen;
+import IOController.User;
+
 public class Game {
 
-    private static final int ENDGAME = 2;
-    private static final int FIRST_START_OFGAME = 0;
-    private static final int NOT_FIRST_START_OFGAME = 1;
+    private static final int ENDGAME = 1;
     private static final int PLAYAGAINMODE = 2;
 
-    public static void start(int firstCheck) {
-        User user = new User();
-        Screen screen = new Screen();
-        Referee referee = new Referee();
-        if (firstCheck == FIRST_START_OFGAME) {
-            screen.printGameStart();
-            progress(user, screen, referee);
-        } else if (firstCheck == NOT_FIRST_START_OFGAME) {
-            progress(user, screen, referee);
-        }
+    public Game() {
+        Screen.printGameStart();
     }
 
-    private static void progress(User user, Screen screen, Referee referee) {
+    public int startAndPlayAgain() {
+        Referee.generateRandom();
+        progress();
+        return checkContinueMode();
+    }
+
+    private static void progress() {
         while (true) {
-            screen.printInputNumber();
-            String RefereeResult = screen.printCalculateResult(
-                referee.calculateAnswer(user.inputValue(1)));
-            if (RefereeResult.equals("3스트라이크") && (checkContinueMode(user, screen) == ENDGAME)) {
+            Screen.printInputNumber();
+            String RefereeResult = Screen.printCalculateResult(
+                Referee.calculateAnswer(User.inputValue(1)));
+            if (RefereeResult.equals("3스트라이크")) {
                 break;
             }
         }
     }
 
-    private static int checkContinueMode(User user, Screen screen) {
-        screen.printEndingMessage();
-        if (user.inputValue(PLAYAGAINMODE).equals("1")) {
-            Game game = new Game();
-            game.start(NOT_FIRST_START_OFGAME);
+    private static int checkContinueMode() {
+        Screen.printEndingMessage();
+        if (User.inputValue(PLAYAGAINMODE).equals("1")) {
+            return PLAYAGAINMODE;
         }
         return ENDGAME;
     }
