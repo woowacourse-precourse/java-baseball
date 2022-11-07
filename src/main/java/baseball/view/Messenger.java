ed1876d570bfeccd7b2f2c59b74de87e73fdd24a
chanminmaxscaler <jcm04@naver.com>
2022-11-07 13:32:20 +0900

    refactor: Messenger 클래스에서 Messages enum 클래스를 사용하도록 리팩토링한다
    하드코딩 되어있는 코드들을 Messages enum 클래스를 활용하여 리팩토링한다. 다음과 같이 바꿀 수 있다.
    
    변경 전 :
    System.out.print("게임을 시작합니다.\n");
    
    변경 후 :
    System.out.print(Messages.START.getMessage());
    
    이러한 변경은 모든 하드 코딩이 포함된 메소드에 적용한다. printStartMessage, printInputMessage, printAnswerMessage, printRestartOrEndMessage, printResultMessage 메소드를 리팩토링한다.

--- src/main/java/baseball/view/Messenger.java
+++ src/main/java/baseball/view/Messenger.java
@@ -6,34 +6,34 @@ import java.util.List;
 public class Messenger {
 
     public void printStartMessage() {
-        System.out.print("게임을 시작합니다.\n");
+        System.out.print(Messages.START.getMessage());
     }
 
     public void printInputMessage() {
-        System.out.print("숫자를 입력하세요. : ");
+        System.out.print(Messages.INPUT.getMessage());
     }
 
     public void printAnswerMessage() {
-        System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+        System.out.print(Messages.ANSWER.getMessage());
     }
 
     public void printRestartOrEndMessage() {
-        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+        System.out.print(Messages.RESTART_OR_END.getMessage());
     }
 
     public void printResultMessage(int strike, int ball) {
         List<String> decisions = new ArrayList<>();
         String countMessage;
         if (!isZero(ball)) {
-            countMessage = String.format("%d볼", ball);
+            countMessage = String.format("%d" + Messages.BALL.getMessage(), ball);
             decisions.add(countMessage);
         }
         if (!isZero(strike)) {
-            countMessage = String.format("%d스트라이크", strike);
+            countMessage = String.format("%d" + Messages.STRIKE.getMessage(), strike);
             decisions.add(countMessage);
         }
         if (isZero(ball) && isZero(strike)) {
-            countMessage = "낫싱";
+            countMessage = Messages.NOTHING.getMessage();
             decisions.add(countMessage);
         }
         System.out.print(String.join(" ", decisions) + "\n");
