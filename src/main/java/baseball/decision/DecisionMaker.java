7bb885423f84eda45f72646fa5cd3a5715634671
Guga <103228463+BGuga@users.noreply.github.com>
2022-11-07 21:45:57 +0900

    chore(DecisionMaker): informatinMessage를 private 변수로 변경

--- src/main/java/baseball/decision/DecisionMaker.java
+++ src/main/java/baseball/decision/DecisionMaker.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class DecisionMaker {
 
-    final String informationMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private final String informationMessage = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public Decision getDecision() {
         return acceptOpinion();
