68a75451e9384377f03f09b96c9ba6300513bfb3
Woogie95 <choisw950928@gmail.com>
2022-11-07 16:13:27 +0900

    refactor: 입력할 때 출력 메시지 Input 으로 이동

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -6,10 +6,6 @@ public class Output {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static void printNumberInputMessage() {
-        System.out.print("숫자를 입력해주세요 : ");
-    }
-
     public static void printStrikeMessage(int strike) {
         System.out.println(strike + " 스트라이크");
     }
@@ -28,10 +24,6 @@ public class Output {
 
     public static void printGameOverMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    }
-
-    public static void RestartGameMessage(int number) {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
 }
