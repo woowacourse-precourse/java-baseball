8e908794f9110ccb35dd0471f80143b9c1b7e290
Kong-hana01 <gksqlsl11@khu.ac.kr>
2022-11-08 02:33:42 +0900

    test(checkInputPlaygame): implement test code
    test contents
    - 1이 주어졌을 때 true 반환
    - empty string이 주어졌을 때 false 반환

---
+++ src/test/java/baseball/GameTest.java
@@ -0,0 +1,34 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class GameTest {
+    Game game = new Game();
+
+    @Test
+    void checkInputPlayGame_테스트_true() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        Method checkInputPlayGameMethod = Game.class.getDeclaredMethod("checkInputPlayGame", String.class);
+        checkInputPlayGameMethod.setAccessible(true);
+
+        boolean isPlay = (boolean) checkInputPlayGameMethod.invoke(game, "1");
+
+        assertThat(isPlay).isEqualTo(true);
+    }
+
+    @Test
+    void checkInputPlayGame_테스트2() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        Method checkInputPlayGameMethod = Game.class.getDeclaredMethod("checkInputPlayGame", String.class);
+        checkInputPlayGameMethod.setAccessible(true);
+
+        boolean isPlay = (boolean) checkInputPlayGameMethod.invoke(game, "");
+
+        assertThat(isPlay).isEqualTo(false);
+    }
+}
