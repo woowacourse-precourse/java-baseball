d5d2b32dbff65ec6d97a71eb42c2ab7d5129d7f8
Insuk Lee <66675919+insukL@users.noreply.github.com>
2022-11-07 23:51:21 +0900

    feat: add main logic
    - 야구 게임을 실행하는 메소드 추가
    - 입출력과 로직 실행 호출 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,61 @@
 package baseball;
 
+import baseball.game.CalcGame;
+import baseball.game.EndGame;
+import baseball.game.StrikeNumberGenerator;
+import baseball.object.GameResult;
+import baseball.object.GameStatus;
+import baseball.view.GameView;
+
 public class Application {
+    private static GameStatus status;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        baseball();
+    }
+
+    private static void baseball() {
+
+        GameView.printStartView();
+        status = GameStatus.RESTART;
+
+        while (status == GameStatus.RESTART) {
+            playGame();
+
+            GameView.printGameRestart();
+            Integer input = getNumber(GameView.getInputNumber());
+
+            if (!EndGame.isRestart(input)) {
+                status = GameStatus.END;
+            }
+        }
+
+    }
+
+    private static void playGame() {
+
+        Boolean isPlay = true;
+        CalcGame.setStrikeNumber(StrikeNumberGenerator.getStrikeNumber());
+
+        while (isPlay) {
+            GameView.printInputView();
+            Integer input = getNumber(GameView.getInputNumber());
+            System.out.println(input);
+            GameResult result = CalcGame.calcGameResult(input);
+            GameView.printGameResult(result);
+
+            if (result.isGameEnd()) {
+                GameView.printGameEnd();
+                isPlay = false;
+            }
+        }
+    }
+
+    private static Integer getNumber(String number) {
+        try {
+            return Integer.valueOf(number);
+        } catch (RuntimeException e) {
+            throw new IllegalArgumentException("Invalid Argument");
+        }
     }
 }
