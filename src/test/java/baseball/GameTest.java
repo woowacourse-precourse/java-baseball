47ed491be95c8cebb479e4c28380e1f69604e310
leeyongjun <leeyongjun0310@naver.com>
2022-11-07 15:32:49 +0900

    test(GameTest): 볼이2, 스트라이크가1 일때 개수를 잘 세는지 확인하는 테스트 코드 추가

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -83,15 +83,28 @@ public class GameTest {
     }
 
     @Test
-    void play_볼과_스트라이크의_개수를_세는지_확인() {
+    void play_볼과_스트라이크의_개수를_세는지_확인1() {
         Score score = new Score();
-        List<Integer> userNumber = Arrays.asList(1,2,3);
-        List<Integer> computerNumber = Arrays.asList(1,2,3);
+        List<Integer> userNumber = Arrays.asList(1, 2, 3);
+        List<Integer> computerNumber = Arrays.asList(1, 2, 3);
 
         Game game = new Game();
         game.play(score, userNumber, computerNumber);
 
         assertThat(score.getBall()).isEqualTo(0);
         assertThat(score.getStrike()).isEqualTo(3);
+    }
+
+    @Test
+    void play_볼과_스트라이크의_개수를_세는지_확인2() {
+        Score score = new Score();
+        List<Integer> userNumber = Arrays.asList(2, 1, 3);
+        List<Integer> computerNumber = Arrays.asList(1, 2, 3);
+
+        Game game = new Game();
+        game.play(score, userNumber, computerNumber);
+
+        assertThat(score.getBall()).isEqualTo(2);
+        assertThat(score.getStrike()).isEqualTo(1);
     }
 }
