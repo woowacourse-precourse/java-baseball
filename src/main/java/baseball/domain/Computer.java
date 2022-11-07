fb7491c9cb4e359b11fa3799545b01b7c320e47f
nswon <swnam0729@gmail.com>
2022-11-07 22:37:49 +0900

    feat: 랜덤한 3자리 숫자 생성

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,9 +1,20 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
 import java.util.Set;
 
 public class Computer {
-    private Set<Integer> randomNumbers;
+    private final Set<Integer> randomNumbers;
+
+    public Computer() {
+        randomNumbers = new HashSet<>();
+        while (randomNumbers.size() != 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            randomNumbers.add(randomNumber);
+        }
+    }
 
     public Set<Integer> getRandomNumbers() {
         return this.randomNumbers;
