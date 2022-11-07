4ee4a53875b29b918d059591afa25f6d33cce074
sooyoungh <sooyoung.h8@gmail.com>
2022-11-07 13:40:53 +0900

    test(Computer): 컴퓨터 랜덤 숫자 생성 테스트 작성

---
+++ src/test/java/baseball/model/ComputerTest.java
@@ -0,0 +1,12 @@
+package baseball.model;
+
+import org.junit.jupiter.api.Test;
+
+class ComputerTest {
+
+    @Test
+    void 컴퓨터_랜덤숫자_생성() {
+        Computer computer = new Computer();
+        System.out.println(computer.setRandomNumber());
+    }
+}
