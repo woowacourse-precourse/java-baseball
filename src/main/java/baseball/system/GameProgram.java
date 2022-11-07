37379a33d9f325a11b4d11b25ff47b2ca410b1d0
sora5421 <21800804@handong.edu>
2022-11-08 04:29:11 +0900

    fix : change  public method to private

--- src/main/java/baseball/system/GameProgram.java
+++ src/main/java/baseball/system/GameProgram.java
@@ -61,7 +61,7 @@ public class GameProgram {
         }
     }
 
-    public void scoreCounter (int result) {
+    private void scoreCounter (int result) {
         if (result == 1){
             strike++;
         } else if (result == 2) {
@@ -69,7 +69,7 @@ public class GameProgram {
         }
     }
 
-    public void gameContinue() {
+    private void gameContinue() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         endError = false;
 
