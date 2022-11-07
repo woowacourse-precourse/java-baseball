1991835d200508ef10420b8b6b058ae6f26db26b
jhsseonn <hosu0125@gmail.com>
2022-11-07 14:19:35 +0900

    fix: 랜덤한 숫자 3개 뽑기 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,10 +14,10 @@ public class Application {
 
         //랜덤한 숫자 3개 뽑기
 
-        List<Integer> computerNumber = new ArrayList<Integer>();
+        List<String> computerNumber = new ArrayList<>();
 
         for(int i=0; i<3; i++) {
-            computerNumber.add(Randoms.pickNumberInRange(1, 9));
+            computerNumber.add(Integer.toString(Randoms.pickNumberInRange(1, 9)));
         }
 
     }
