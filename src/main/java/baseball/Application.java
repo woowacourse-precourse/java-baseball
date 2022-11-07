bcc458ee9d13f0059e955f068bed4a4179cb2aca
Junmin <junmin1623@naver.com>
2022-11-07 12:59:29 +0900

    feat(application):setting computer num add

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,4 +1,9 @@
 package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
 /*
 *
 2. 컴퓨터(상대방)의 숫자를 설정하는 함수
@@ -6,12 +11,21 @@ package baseball;
 4. 재시작or종료 함수. 재시작이면 continue, 아니면 break!!
 * */
 public class Application {
-    
+    static List<Integer> set_Cpnum(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다");
         // DO_LIST2: 숫자 설정
-
+        List<Integer> computer = set_Cpnum();
         while(true){
 
         }
