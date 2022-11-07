0cba354adfde00c91f7043c1d814ddee47d92071
josolha <josolha@nate.com>
2022-11-08 00:07:41 +0900

    test : unit tests for checkNumbers method

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,9 +1,12 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.assertj.core.api.AbstractObjectArrayAssert;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -15,8 +18,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-        @Nested
-    class generateTest {
+    @Nested
+    class generateNumTest {
         @Test
         void 랜덤_숫자_테스트1_3자리_확인() {
             List<Integer> random = Application.generateNum();
@@ -30,20 +33,47 @@ class ApplicationTest extends NsTest {
                 assertThat(random.get(i)).isBetween(0, 9);
             }
         }
+
         @Test
-        void 랜덤_숫자_테스트2_중복_숫자() {
+        void 랜덤_숫자_테스트3_중복_숫자_확인() {
             List<Integer> random = Application.generateNum();
             Set<Integer> randomNumSet = new HashSet<>(random);
             Assertions.assertThat(random.size()).isSameAs(randomNumSet.size());
         }
     }
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+    @Nested
+    class inputNumTest {
+
+    }
+    @Nested
+    class checkNumbersTest{
+        @Test
+        void 체크_넘버즈_스트라이크_테스트() {
+            List<Integer> computerTest = Arrays.asList(1, 2, 3);
+            List<Integer> userTest = Arrays.asList(1, 2, 3);
+            Assertions.assertThat(Application.checkNumbers(computerTest,userTest)).isEqualTo("3스트라이크");
+        }
+        @Test
+        void 체크_넘버즈_볼_테스트() {
+            List<Integer> computerTest = Arrays.asList(1, 2, 3);
+            List<Integer> userTest = Arrays.asList(2, 3, 1);
+            Assertions.assertThat(Application.checkNumbers(computerTest,userTest)).isEqualTo("3볼");
+        }
+        @Test
+        void 체크_넘버즈_볼_스트라이크_테스트() {
+            List<Integer> computerTest = Arrays.asList(1, 2, 3);
+            List<Integer> userTest = Arrays.asList(2, 1, 3);
+            Assertions.assertThat(Application.checkNumbers(computerTest,userTest)).isEqualTo("2볼 1스트라이크");
+        }
+        @Test
+        void 체크_넘버즈_낫싱_테스트() {
+            List<Integer> computerTest = Arrays.asList(1, 2, 3);
+            List<Integer> userTest = Arrays.asList(4, 5, 6);
+            Assertions.assertThat(Application.checkNumbers(computerTest,userTest)).isEqualTo("낫싱");
+        }
+
+
     }
 
     @Override
