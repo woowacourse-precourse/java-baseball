28494b95e4bcc5f27ad68bdea5ed635ed9f6c1c0
meunrud <y3a0522@gmail.com>
2022-11-08 02:50:55 +0900

    :art: userInputNumber 생성
    유저에게 숫자를 입력받는다.

--- src/main/java/baseball/InputNumber.java
+++ src/main/java/baseball/InputNumber.java
@@ -1,4 +1,18 @@
 package baseball;
 
 public class InputNumber {
+
+    private String userNumber;
+
+    public InputNumber(){
+        this.userNumber = "" ;
+    }
+
+    public String userInputNumber(){
+        System.out.println("숫자를 입력해주세요.");
+        userNumber = Console.readLine();
+        return userNumber;
+    }
+
+
 }
