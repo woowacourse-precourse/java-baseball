64ec2658ad02bf37bbac8287c7f7d4d8ea3a9218
Kayoung <rkdkdudjd@gmail.com>
2022-11-07 19:15:14 +0900

    fix(GamePrinter): 3스트라이크 시 볼 개수 붙이지 않기
    조건을 더해주었습니다.

--- src/main/java/baseball/game/GamePrinter.java
+++ src/main/java/baseball/game/GamePrinter.java
@@ -9,9 +9,10 @@ public class GamePrinter {
     private final String END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private final int BALL_IDX = 0;
     private final int STRIKE_IDX = 1;
-    private final String BALL = "볼";
-    private final String STRIKE = "스트라이크";
-    private final String NOTHING = "낫싱";
+    private final String BALL_STRING = "볼";
+    private final String STRIKE_STRING = "스트라이크";
+    private final String NOTHING_STRING = "낫싱";
+    private final String BLANK = " ";
 
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
@@ -33,16 +34,17 @@ public class GamePrinter {
         int ballCount = result.get(BALL_IDX);
         int strikeCount = result.get(STRIKE_IDX);
         StringBuilder resultStr = new StringBuilder();
-        if (ballCount != 0) {
+        if (ballCount != 0 && strikeCount != Number.FULL_SIZE) {
             resultStr.append(ballCount);
-            resultStr.append(BALL + " ");
+            resultStr.append(BALL_STRING);
+            resultStr.append(BLANK);
         }
         if (strikeCount != 0) {
             resultStr.append(strikeCount);
-            resultStr.append(STRIKE);
+            resultStr.append(STRIKE_STRING);
         }
         if (ballCount == 0 && strikeCount == 0) {
-            resultStr.append(NOTHING);
+            resultStr.append(NOTHING_STRING);
         }
         System.out.println(resultStr);
     }
