973303ee825b6b2c79f3a9d768c9ad8cd4e74f89
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-08 06:06:11 +0900

    feat(hint/Hint.java): 힌트 문구 생성하는 기능 구현

--- src/main/java/baseball/hint/Hint.java
+++ src/main/java/baseball/hint/Hint.java
@@ -5,6 +5,9 @@ import java.util.List;
 public class Hint {
     private int ball = 0;
     private int strike = 0;
+    private static final String NOTHING = "낫싱";
+    private static final String BALL = "볼 "; //뒤에 스트라이크가 붙을때 공백을 추가하기 위해 볼뒤에 공백까지 붙여서 저장함
+    private static final String STRIKE = "스트라이크";
 
     public Hint(List<Integer> computerNum, List<Integer> userNum){
         ball = 0;
@@ -22,5 +25,22 @@ public class Hint {
             }
         }
     }
+
+    public String getHint(){
+        StringBuilder strConnect = new StringBuilder();
+
+        if (ball == 0 && strike == 0){
+            return NOTHING;
+        }
+        if (ball > 0){
+            strConnect.append(ball).append(BALL);
+        }
+        if (strike > 0){
+            strConnect.append(strike).append(STRIKE);
+        }
+
+        return strConnect.toString();
+    }
+
 }
 
