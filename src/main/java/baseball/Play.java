8927941572b2f2b801f01bb060b0dbdfa0bf5174
jin8073 <jin8073@gmail.com>
2022-11-07 14:37:26 +0900

    feat(Play): 사용자가 입력한 값을 받아와서 저장

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -1,12 +1,16 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Play {
 
     public static void playStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         int number = createThreeNumber();
+
+        System.out.print("숫자를 입력해주세요 : ");
+        int playerNumber = Integer.parseInt(Console.readLine());
     }
     public static int createThreeNumber() {
         int size = 0;
