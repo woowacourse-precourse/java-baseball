1963185529e61de4cbdb4aab65daf52b65d5d36e
Eun-chan Cho <eunchan1001@gmail.com>
2022-11-07 20:40:29 +0900

    test(GameTest): add test for getting random computer number

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,9 +1,27 @@
 package baseball;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.*;
 
 public class GameTest {
+
+    @DisplayName("3자리 수 생성")
+    @Test
+    void createRandomNumber() {
+        Game game = new Game();
+        List<Integer> testComputerNumber = new ArrayList<>();
+        testComputerNumber.add(1);
+        testComputerNumber.add(2);
+        testComputerNumber.add(3);
+        assertRandomNumberInRangeTest(() -> {
+            game.getComputerNumber();
+            assertThat(game.getComputerNumber()).isEqualTo(testComputerNumber);
+        }, 1, 2, 3);
+    }
 
 }
