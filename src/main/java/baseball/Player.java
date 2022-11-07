a8330d13201ef62dfaee41e54660ed2f89430bc6
김태용 <amico741@naver.com>
2022-11-07 21:37:08 +0900

    refactor: 숫자로만 이루어졌는지 확인하는 메서드 분리
    숫자를 결정하는 메서드를 함수별로 분리

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -9,17 +9,11 @@ public class Player {
   private static final int MAX_SIZE = 3;
   public Player(){};
 
-  public ArrayList<Integer> decideNumbers(){
+  public ArrayList<Integer> decideNumbers(String input){
     System.out.print("숫자를 입력해주세요 : ");
-    String input = Console.readLine();
-    String pattern = "^[0-9]*$";
-    boolean result = Pattern.matches(pattern, input);
-
-    if(!result) {
-      throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
-    }
+    //String input = "122";
+    validateConsistsOfNumber(input);
     addNumber(input);
-    return this.numbers;
   }
 
   public void addNumber(String input){
@@ -34,7 +28,16 @@ public class Player {
 
     if(this.numbers.size() != MAX_SIZE){
       System.out.println(this.numbers);
-      throw new IllegalArgumentException("서로 다른 3개의 숫자를 입력해야 합니다.");
+      throw new IllegalArgumentException("중복된 숫자가 있으면 안됩니다.");
+    }
+  }
+
+  public void validateConsistsOfNumber(String input){
+    String pattern = "^[0-9]*$";
+    boolean result = Pattern.matches(pattern, input);
+
+    if(!result) {
+      throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
     }
   }
 }
