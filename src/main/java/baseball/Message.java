2666235759e34413ddab1d668123920eb4050682
hwiyeon0831 <aab555586@gmail.com>
2022-11-08 08:13:57 +0900

    feat() : 입/출력 메시지를 담은 객체 구현
    Message 객체 추가 및 기능 구현
    - 게임 진행 중 사용되는 메시지를 상수화

---
+++ src/main/java/baseball/Message.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class Message {
+	public static final String INVALID_ARGUMENT_EXCEPTION_MESSAGE = "잘못된 입력입니다. 어플리케이션이 종료됩니다.";
+	public static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+	public static final String NOTHING_MESSAGE = "낫싱";
+	public static final String BALL_MESSAGE = "볼";
+	public static final String STRIKE_MESAGE = "스트라이크";
+	public static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	public static final String INPUT_NEXT_STEP_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	public static final String END_MESSAGE = "게임이 종료됩니다.";
+}
