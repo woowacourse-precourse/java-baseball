b426f319ffffa448ad294a4a752ca502f6bcbbeb
kytmt <tlsrka649@naver.com>
2022-11-08 07:18:13 +0900

    feat(게임 출력 파트 구현): 게임 출력 파트 구현
    View 클래스로 통합하여 게임에서 출력되는 모든 출력문은 View 클래스에서 나옴.

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,11 +1,14 @@
 package baseball;
 
-public class Lobby {
+public class View {
   public static void start() {
     System.out.println("숫자 야구 게임을 시작합니다.");
   }
-  public static void onGame() {
-    System.out.print("숫자를 입력해주세요 : ");
+  public static void waitInput() {
+    System.out.println("숫자를 입력해주세요 : ");
+  }
+  public static void result(String userResult) {
+    System.out.println(userResult);
   }
   public static void finish() {
     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
