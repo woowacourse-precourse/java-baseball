7a9deaf27557a9a9dade999666dc545bbc882058
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-08 00:03:37 +0900

    feat: 비교한 결과 값 출력

--- src/main/java/baseball/GameSimulation.java
+++ src/main/java/baseball/GameSimulation.java
@@ -23,6 +23,8 @@ public class GameSimulation {
 
         ball = computer.addBallScore(userNumber);
         strike = computer.addStrikeScore(userNumber);
+
+        System.out.println(checkResult());
     }
 
     public List<Integer> makeList(String userInput) {
@@ -39,8 +41,22 @@ public class GameSimulation {
             if (Valid.validNumberList(userNumber) == INVALID) {
                 throw new IllegalArgumentException("올바른 숫자가 아닙니다. 다시 입력해주세요.");
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public String checkResult() {
+        if (ball != 0 && strike != 0) {
+            return (ball + "볼" + " " + strike + "스트라이크");
+        }
+        if (ball == 0 && strike != 0) {
+            return (strike + "스트라이크");
+        }
+        if (ball != 0 && strike == 0) {
+            return (ball + "볼");
+        }
+
+        return ("낫싱");
     }
 }
