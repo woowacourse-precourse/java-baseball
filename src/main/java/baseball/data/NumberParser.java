ddfee1c3c37bcc455dc633da0af5ff82dd38a0cc
cuttlefish55 <funade7@gmail.com>
2022-11-07 18:41:40 +0900

    feat: Add "NumberParser.java" to parsing player Numbers
    문자열로 받은 숫자를 리스트형으로 변환해서 전달.

---
+++ src/main/java/baseball/data/NumberParser.java
@@ -0,0 +1,71 @@
+package baseball.data;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+import net.bytebuddy.pool.TypePool.Resolution.Illegal;
+
+/**
+ * 문자열로 입력받은 숫자들을 리스트에 옮겨담는다.
+ */
+public class NumberParser {
+    private static final int NUMBER_LENGTH = 3;
+    private static final int DIVIDE_INITIATE = 100; // 나눌 초기값
+    private static final int DIVIDE_TARGET = 10; // 단계별로 줄어드는 자릿수
+
+
+    /**
+     * 문자열로 입력받은 플레이어의 숫자를 리스트로 변환해서 전달.
+     * @param numbers 아직 문자열로 저장되어있는 숫자들
+     * @return 리스트로 입력받은 숫자를 변환
+     */
+    public List<Integer> parsePlayerNumber(String numbers) {
+        List<Integer> parsedNumbers;
+        int tempNumbers;
+        try{
+            tempNumbers = Integer.parseInt(numbers);
+            parsedNumbers = divideNumbers(tempNumbers);
+            checkExceptions(parsedNumbers);
+        }
+        catch(Exception e){
+            throw new IllegalArgumentException("잘못된 입력입니다.");
+        }
+
+        return parsedNumbers;
+    }
+
+    /**
+     * 숫자 입력에서 발생할 수 있는 오류들 체크<br>
+     * 1. 중복된 숫자가 있음.<br>
+     * 2. 입력받은 숫자가 세 자리 수가 아님.<br>
+     * @param parsedNumbers 플레이어가 입력한 숫자 리스트
+     * @throws IllegalArgumentException
+     */
+    public void checkExceptions(List<Integer> parsedNumbers) throws IllegalArgumentException {
+        Set<Integer> tempSet = new HashSet<Integer>(parsedNumbers);
+        if(tempSet.size() != parsedNumbers.size()){
+            throw new IllegalArgumentException("중복된 숫자가 있습니다.");
+        } else if(parsedNumbers.size() != NUMBER_LENGTH){
+            throw new IllegalArgumentException("3개의 숫자만 입력해주세요");
+        }
+    }
+
+    /**
+     * Integer형을 자릿수끼리 분리해서 리스트에 저장
+     * @param dividend 피제수
+     * @return 플레이어가 입력한 숫자들을 리스트로 전달
+     */
+    public List<Integer> divideNumbers(int dividend){
+        List<Integer> tempNumbers = new ArrayList<>();
+        int divider = DIVIDE_INITIATE;
+        int tempAnswer = 0;
+        while(divider > 0){
+            tempAnswer = dividend / divider; // 자랏수 임시 저장
+            tempNumbers.add(tempAnswer); // 리스트에 자릿수 저장
+            dividend = dividend - tempAnswer * divider; // 가장 왼쪽의 자릿값을 빼기
+            divider = divider / DIVIDE_TARGET; // 자릿수 한칸 이동
+        }
+        return tempNumbers;
+    }
+}
