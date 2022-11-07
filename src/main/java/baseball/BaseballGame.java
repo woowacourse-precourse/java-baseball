8132de2c9dc3d46d8c41579447aa0676f4c7a73b
Yerim lee <103623292+lyrWinterCat@users.noreply.github.com>
2022-11-07 22:42:42 +0900

    Feat : BaseballGame 생성
    1. 게임 시작 메서드 구현
    - 게임 처음 시작 시 안내 메세지 출력
    - play() 실행

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
+public class BaseballGame {
+
+    InputValidation inputValidation = new InputValidation();
+
+    public void start() {
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        play();
+    }
+
+    public void play(){}
+
+}
