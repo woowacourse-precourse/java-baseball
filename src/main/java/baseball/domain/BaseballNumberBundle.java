c68b979f7a94f174c5bac79883f97998d0df69b8
Basak <chgy2131@naver.com>
2022-11-08 00:46:01 +0900

    feat(BaseballNumberBundle): 야구 게임에 사용되는 숫자 묶음 클래스 작성
    - 야구 게임에 사용되는 숫자 묶음 (서로다른 3개의 수)를 기억하는 클래스

--- src/main/java/baseball/domain/BaseballNumberBundle.java
+++ src/main/java/baseball/domain/BaseballNumberBundle.java
@@ -1,4 +1,29 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballNumberBundle {
+    private List<Integer> list = new ArrayList<Integer>();
+
+    BaseballNumberBundle() {
+    }
+    BaseballNumberBundle(int number1, int number2, int number3) {
+        addNumber(number1);
+        addNumber(number2);
+        addNumber(number3);
+    }
+    public List<Integer> getList() {
+        return list;
+    }
+    public List<Integer> getReverseList() {
+        List<Integer> reverseList = new ArrayList<Integer>();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            reverseList.add(list.get(i));
+        }
+        return reverseList;
+    }
+    public void addNumber(Integer number) {
+        list.add(number);
+    }
 }
---
+++ src/test/java/baseball/domain/BaseballNumberBundleTest.java
@@ -0,0 +1,46 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BaseballNumberBundleTest {
+
+    @Test
+    void getList() {
+        BaseballNumberBundle numbers = new BaseballNumberBundle(1,2,3);
+
+        List<Integer> target = new ArrayList<Integer>();
+        target.add(1);
+        target.add(2);
+        target.add(3);
+        assertThat(numbers.getList()).isEqualTo(target);
+    }
+
+    @Test
+    void getReverseList() {
+        BaseballNumberBundle numbers = new BaseballNumberBundle(1,2,3);
+
+        List<Integer> target = new ArrayList<Integer>();
+        target.add(3);
+        target.add(2);
+        target.add(1);
+        assertThat(numbers.getReverseList()).isEqualTo(target);
+    }
+
+    @Test
+    void addNumber() {
+        BaseballNumberBundle numbers = new BaseballNumberBundle();
+        numbers.addNumber(1);
+        numbers.addNumber(3);
+        numbers.addNumber(2);
+
+        List<Integer> target = new ArrayList<Integer>();
+        target.add(1);
+        target.add(3);
+        target.add(2);
+        assertThat(numbers.getList()).isEqualTo(target);
+    }
+}
