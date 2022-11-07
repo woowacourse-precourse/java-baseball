9bee217855f079a6246ac85642d7972d9f2c88c6
kjg1514 <kjg1514@naver.com>
2022-11-07 18:47:45 +0900

    feat : 게임 시작 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,9 +3,15 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
 
-        Game game = new Game();
-        while (game.restartOrEnd()) {
-            game.playGame();
+        boolean firstStartGame = true;
+
+        while (firstStartGame) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            Game game = new Game();
+            while (game.restartOrEnd()) {
+                game.playGame();
+            }
+            firstStartGame = false;
         }
     }
 }
