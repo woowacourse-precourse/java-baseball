dbb4c1912b5c0adaec6e451abe05eef8c9416a4e
JaeHwanCho <makefuture@kakao.com>
2022-11-08 01:51:50 +0900

    feat(function): get User's Numbers
    Add new function:
    - Gets the three numbers entered by the user
    - Add a 3-letter condition exception

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,6 +13,34 @@ public class Application {
         List<Integer> computer = getComputerNumber();
         System.out.println("computer = " + computer);
 
+        // 게임 시작
+        boolean game = true;
+        while (game) {
+            int strike = 0;
+            int ball = 0;
+
+            System.out.println("숫자를 입력해주세요 : ");
+            String user = getNums();
+
+            // 결과 계산 로직
+
+            if (ball > 0) {
+                System.out.print(ball + "볼 ");
+            }
+            if (strike > 0) {
+                System.out.print(strike + "스트라이크");
+            }
+            if (ball == 0 && strike == 0) {
+                System.out.print("낫싱");
+            }
+            System.out.println();
+            if (strike == 3) {
+                // 게임 종료 로직
+            }
+            if (strike == 3 && game == true) {
+                computer = getComputerNumber();
+            }
+        }
     }
 
     // 컴퓨터 숫자 얻기
@@ -25,5 +53,19 @@ public class Application {
             }
         }
         return computer;
+    }
+
+    // 사용자 숫자 얻기
+    private static String getNums() {
+        // 예상 숫자 입력
+        String str = Console.readLine();
+        System.out.println("str = " + str);
+
+        // 3자리가 아닌 경우 예외처리
+        if (str.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        return str;
     }
 }
