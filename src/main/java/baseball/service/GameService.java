71922f8c10b0385e25e97ec8d82a086af3024c0d
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-07 16:05:12 +0900

    221107 Feat: 기능3 - 입력값을 검증하는 메소드 추가

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -8,7 +8,23 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class GameService {
 
+    public void validate(Game game){
+        List<Integer> numbers = game.getNumbers();
 
+        if(numbers.size() < 3) {
+            throw new IllegalArgumentException("3개 미만의 숫자를 입력할 수 없습니다.");
+        }
+
+        if(numbers.size() > 3) {
+            throw new IllegalArgumentException("3개 초과의 숫자를 입력할 수 없습니다.");
+        }
+
+        for(int num : game.getNumbers()){
+            if (num < 1 || num > 9) {
+                throw new IllegalArgumentException("1에서 9 사이의 숫자만 입력할 수 있습니다.");
+            }
+        }
+    }
 
     public Game initAnswer(Game game) {
         game.setNumbers(Randoms.pickUniqueNumbersInRange(1, 9, 3));
