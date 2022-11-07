b93a6a0445ef4c6d7f56c5c3ec7ac0a048e75d49
이원기 <log19951@gmail.com>
2022-11-08 01:24:04 +0900

    feat: BaseballController 클래스 생성 및 Application 클래스 수정

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,51 @@
+package baseball.controller;
+
+import baseball.io.Input;
+import baseball.io.Output;
+import baseball.message.OutPutMessage;
+import baseball.service.BaseballService;
+
+import java.util.List;
+
+public class BaseballController {
+
+    private final static Input input = new Input();
+    private final static Output output = new Output();
+    private final BaseballService baseballservice;
+
+    public BaseballController() {
+        this.baseballservice = new BaseballService();
+    }
+
+    public void run() {
+        do {
+            baseballservice.initBaseballGame(); // 컴퓨터의 랜덤 수 생성
+            output.printMessageWithEnter(OutPutMessage.START_GAME);
+            startBaseballGame();
+        }while(decideReGameOrEndGame());
+    }
+
+    private void startBaseballGame() {
+        do {
+            output.printMessageWithoutEnter(OutPutMessage.ENTER_USER_NUM);
+            List<Integer> userNumber = input.inputUserNumber();
+            String result = baseballservice.playBaseballGame(userNumber);
+            output.printMessageWithEnter(result);
+        }while(isNotGameClear());
+    }
+
+    private boolean decideReGameOrEndGame() {
+        output.printMessageWithEnter(OutPutMessage.ASK_TRY_AGAIN);
+        boolean result = input.inputReGameOrEndGame();
+        return result;
+    }
+
+    private boolean isNotGameClear() {
+        if(baseballservice.isGameClear()) {
+            output.printMessageWithEnter(OutPutMessage.GAME_CLEAR);
+            return false;
+        }
+        return true;
+    }
+
+}
