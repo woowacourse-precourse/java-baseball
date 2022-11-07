01b85b779bb483c26001986502d3e3ce4ee234d0
김선동 <mok03127@gmail.com>
2022-11-07 16:06:16 +0900

    fix: 게임 최초 시작 시에만 메시지 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,11 @@
 package baseball;
 
 import baseball.controller.BaseballGameController;
+import baseball.view.SystemMessage;
 
 public class Application {
     public static void main(String[] args) {
+        SystemMessage.printGameStartMessage();
         BaseballGameController baseballGameController = new BaseballGameController();
         baseballGameController.run();
     }
--- src/main/java/baseball/view/SystemMessage.java
+++ src/main/java/baseball/view/SystemMessage.java
@@ -13,7 +13,7 @@ public class SystemMessage {
     private static final String GAME_WIN_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void printGameStartMessage() {
-        System.out.print(GAME_START_MESSAGE);
+        System.out.println(GAME_START_MESSAGE);
     }
 
     public static void requestInputNumberMessage() {
