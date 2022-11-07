d51b98ba2c135fad85a9c15bdaa94e3e5c074ac0
devMuscle <hongsbien@naver.com>
2022-11-08 06:24:04 +0900

    feat(validateNumber): 유저 입력값 체크

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,30 +1,40 @@
 package baseball;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class User {
 
-    public List<Integer> generateNumberList(String numberString) throws IllegalArgumentException {
+    public void validateNumber(String numberString) {
         if(numberString.length()!=3) {
             throw new IllegalArgumentException("3자리를 입력하세요");
         }
+
+        String pattern = "^[1-9]*$";
+
+        if(!Pattern.matches(pattern, numberString)) {
+            throw new IllegalArgumentException("1~9만 입력하세요");
+        }
+
+        String[] numberStringArray = numberString.split("");
+
+        int nonDupplicateElementCnt = Arrays.stream(numberStringArray).distinct().toArray(String[]::new).length;
+
+        if(nonDupplicateElementCnt!=numberStringArray.length) {
+            throw new IllegalArgumentException("중복된 원소가 있습니다");
+        }
+
+    }
+
+    public List<Integer> generateNumberList(String numberString) {
 
         List<Integer> numberList = new ArrayList<>();
         String[] numberStringArray = numberString.split("");
 
         for(String number : numberStringArray) {
-            try {
-                numberList.add(Integer.parseInt(number));
-            }catch (IllegalArgumentException illegalArgumentException) {
-                throw illegalArgumentException;
-            }
-        }
-
-        int nonDupplicateElementCnt = Long.valueOf(numberList.stream().distinct().count()).intValue();
-
-        if(nonDupplicateElementCnt!=numberList.size()) {
-            throw new IllegalArgumentException("중복된 원소 입력");
+            numberList.add(Integer.parseInt(number));
         }
 
         return numberList;
