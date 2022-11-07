7ed89ce2bfffab4cc338d62cc805c25bdf07102e
kariskan <zkfzpf56@naver.com>
2022-11-07 21:32:09 +0900

    test:  작성

---
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -0,0 +1,29 @@
+package baseball.domain;
+
+
+import baseball.utils.NumberValidator;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ComputerTest {
+
+    Computer computer;
+
+    @BeforeEach
+    void init() {
+        computer = new Computer();
+    }
+
+    @Test
+    void 컴퓨터_숫자_통과() {
+        List<Integer> computerNumbers = computer.getComputerNumbers();
+        String result = computerNumbers.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining());
+        Assertions.assertThatNoException().isThrownBy(() -> NumberValidator.validate(result));
+    }
+
+}
