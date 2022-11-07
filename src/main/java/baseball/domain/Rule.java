0bbd6ff9fc650ee89c2ac87af06a0ff5e426a13c
nswon <swnam0729@gmail.com>
2022-11-07 22:19:54 +0900

    feat: Rule 객체 구현

---
+++ src/main/java/baseball/domain/Rule.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+import java.util.Set;
+
+public class Rule {
+    public String getHint(Set<Integer> numbers) {
+        return "";
+    }
+
+    public String gameOver() {
+        return "";
+    }
+}
