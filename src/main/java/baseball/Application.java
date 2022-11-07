33a02129ae8792b772a37aad558ba45a280e5153
Torres-09 <dla612@naver.com>
2022-11-07 21:05:30 +0900

    feat : 새 게임 시작 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,10 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Game game = new Game();
+        while (true) {
+            Game game = new Game();
+            if(!game.isReStart())
+                break;
+        }
     }
 }
