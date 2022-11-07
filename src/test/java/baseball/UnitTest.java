dcb396b4075caae05e2324a352bcaec7873d6bb8
zeunala <zeunala@naver.com>
2022-11-07 19:49:17 +0900

    test: 숫자 입력받는 기능 테스트 추가

--- src/test/java/baseball/UnitTest.java
+++ src/test/java/baseball/UnitTest.java
@@ -4,10 +4,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class UnitTest {
     @Nested
@@ -32,6 +35,74 @@ public class UnitTest {
 
                 usedNumber.add(ch);
             }
+        }
+    }
+
+    @Nested
+    class GetGuessNumberInputTest {
+        @Test
+        @DisplayName("기본흐름: 플레이어가 중복되지 않는 1~9 세자리 수 입력하는 경우 입력값 그대로 반환")
+        void getGuessNumberInput_ValidInput_Success() {
+            String guessNumberInput = "123";
+            InputStream is = new ByteArrayInputStream(guessNumberInput.getBytes());
+
+            System.setIn(is);
+            assertThat(Application.getGuessNumberInput()).isEqualTo(guessNumberInput);
+        }
+
+        @Test
+        @DisplayName("예외흐름: 플레이어가 2자리를 입력하는 경우")
+        void getGuessNumberInput_InvalidLength2_IllegalArgumentExceptionThrown() {
+            String guessNumberInput = "12";
+            InputStream is = new ByteArrayInputStream(guessNumberInput.getBytes());
+
+            System.setIn(is);
+            assertThatThrownBy(() -> Application.getGuessNumberInput())
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("예외흐름: 플레이어가 4자리를 입력하는 경우")
+        void getGuessNumberInput_InvalidLength4_IllegalArgumentExceptionThrown() {
+            String guessNumberInput = "1234";
+            InputStream is = new ByteArrayInputStream(guessNumberInput.getBytes());
+
+            System.setIn(is);
+            assertThatThrownBy(() -> Application.getGuessNumberInput())
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("예외흐름: 플레이어가 1~9 숫자가 아닌 값을 입력하는 경우")
+        void getGuessNumberInput_InvalidCharacter_IllegalArgumentExceptionThrown() {
+            String guessNumberInput = "12a";
+            InputStream is = new ByteArrayInputStream(guessNumberInput.getBytes());
+
+            System.setIn(is);
+            assertThatThrownBy(() -> Application.getGuessNumberInput())
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("예외흐름: 플레이어가 0이 들어간 값 입력하는 경우")
+        void getGuessNumberInput_InvalidNumber_IllegalArgumentExceptionThrown() {
+            String guessNumberInput = "104";
+            InputStream is = new ByteArrayInputStream(guessNumberInput.getBytes());
+
+            System.setIn(is);
+            assertThatThrownBy(() -> Application.getGuessNumberInput())
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("예외흐름: 플레이어가 중복된 값을 입력하는 경우")
+        void getGuessNumberInput_DuplicatedNumber_IllegalArgumentExceptionThrown() {
+            String guessNumberInput = "141";
+            InputStream is = new ByteArrayInputStream(guessNumberInput.getBytes());
+
+            System.setIn(is);
+            assertThatThrownBy(() -> Application.getGuessNumberInput())
+                    .isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
