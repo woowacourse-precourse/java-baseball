dfa5f1cec34aa2cc0c3791eddbe405c0482df8b9
gayeonP <pagaye@naver.com>
2022-11-07 21:00:27 +0900

    random number 만드는 기능 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private static final int START_RANGE = 1;
+    private static final int END_RANGE = 9;
+    private static final int LIST_SIZE = 3;
+
+    List<Integer> random_num_list = new ArrayList<>();
+    RandomNumGenerator randomNumGenerator = new RandomNumGenerator();
+
+    public void computer_random_num() {
+
+    }
+}
--- src/main/java/baseball/RandomNumGenerator.java
+++ src/main/java/baseball/RandomNumGenerator.java
@@ -2,8 +2,18 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class RandomNumGenerator {
-    private static final int START_RANGE = 1;
-    private static final int END_RANGE = 9;
+import java.util.ArrayList;
+import java.util.List;
 
+public class RandomNumGenerator {
+    public List<Integer> randomNumGenerator(int size, int start_range, int end_range) {
+        List<Integer> random_list = new ArrayList<>();
+        while (random_list.size() < size) {
+            int randomNumber = Randoms.pickNumberInRange(start_range, end_range);
+            if (!random_list.contains(randomNumber)) {
+                random_list.add(randomNumber);
+            }
+        }
+        return random_list;
+    }
 }
