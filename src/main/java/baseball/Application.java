1292d576d71ac3d15d23f1de00516106292e9c52
taejeong <sontaewjd@nate.com>
2022-11-08 07:22:47 +0900

    fix: 사용자 입력 예외 수정
    사용자 입력으로는 0이 들어와도 됨

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -142,12 +142,6 @@ public class Application {
 
     static boolean isValidInputNumber(String str) {
         for (int i = 0; i < str.length(); i++) {
-            if (!(str.charAt(i) >= '1' && str.charAt(i) <= '9')) {
-                return false;
-            }
-        }
-
-        for (int i = 0; i < str.length(); i++) {
             if (isDuplicate(str.charAt(i), str)) {
                 return false;
             }
