b24e74ee6bcff4b934fe07dec77bdf541de1ce0d
taekwon-dev <taekwon_dev@naver.com>
2022-11-07 23:57:48 +0900

    refactor: 숫자야구 게임 숫자 문자열 길이 변수처리
    - 추후 숫자야구 게임 난이도 상승 처리 관련 확장 대비 (문자열 길이 증가 시 더 어려운 게임 진행 가능)

--- src/main/java/baseball/Catcher.java
+++ src/main/java/baseball/Catcher.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Catcher {
     public List<Integer> generateRandomNumbers() {
         List<Integer> randomNumbers = new ArrayList<>();
-        while (randomNumbers.size() < 3) {
+        while (randomNumbers.size() < Game.GAME_LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!randomNumbers.contains(randomNumber)) {
                 randomNumbers.add(randomNumber);
