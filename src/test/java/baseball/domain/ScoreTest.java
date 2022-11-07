bccc2bb37fecf96317712b6cdd9827990a7ac4e5
Be-student <presentsong@naver.com>
2022-11-07 13:29:27 +0900

    test(Score): 점수 계산 검증 테스트

---
+++ src/test/java/baseball/domain/ScoreTest.java
@@ -0,0 +1,25 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ScoreTest {
+    @DisplayName("점수 계산이 이루어진다")
+    @Test
+    void 점수_계산() {
+        Digits player = Digits.create();
+        player.append(Digit.ONE);
+        player.append(Digit.TWO);
+        player.append(Digit.THREE);
+        Digits computer = Digits.create();
+        computer.append(Digit.ONE);
+        computer.append(Digit.THREE);
+        computer.append(Digit.TWO);
+        Score score = Score.calculate(player, computer);
+
+        assertThat(score.isOneStrike()).isEqualTo(true);
+        assertThat(score.isTwoBall()).isEqualTo(true);
+    }
+}
