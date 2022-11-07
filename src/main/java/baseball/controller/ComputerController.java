e20700a29665a60fbce65a9178618e23630e5595
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-08 01:17:46 +0900

    feat(ComputerController): 스트라이크, 볼 카운터 변수 0으로 초기화 기능 추가
    docs: 스트라이크, 볼 카운터 변수 0으로 초기화 완료 체크

--- src/main/java/baseball/controller/ComputerController.java
+++ src/main/java/baseball/controller/ComputerController.java
@@ -10,6 +10,7 @@ import java.util.Arrays;
 public class ComputerController {
     private static final int BASEBALL_GAME_NUMBER_LENGTH = 3;
     private static final int BASEBALL_SUCCESS_STRIKE_COUNT = 3;
+    private static final int INIT_ZERO = 0;
     private static final String STRIKE_MESSAGE = "스트라이크";
     private static final String BALL_MESSAGE = "볼";
     private static final String NOTHING_MESSAGE = "낫싱";
@@ -23,10 +24,16 @@ public class ComputerController {
     public void compareNumbers(GameNumber gameNumber) {
         int[] playerNumbers = gameNumber.getPlayerNumbers();
         int[] computerNumbers = gameNumber.getComputerNumbers();
+        initCount();
         for (int i = 0; i < BASEBALL_GAME_NUMBER_LENGTH; i++) {
             countStrike(computerNumbers, playerNumbers[i], i);
             countBall(computerNumbers, playerNumbers[i], i);
         }
+    }
+
+    private void initCount() {
+        strikeCount = INIT_ZERO;
+        ballCount = INIT_ZERO;
     }
 
     private void countStrike(int[] computerNumbers, int playerNumber, int numberIndex) {
