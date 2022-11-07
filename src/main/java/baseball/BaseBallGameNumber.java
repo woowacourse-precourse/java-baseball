c89c79301ad34ebb92eee2a0f4c60b3319dcd98f
JeongHunHui <deca3248@gmail.com>
2022-11-08 00:28:33 +0900

    feat: 입력받은 숫자가 유효한 숫자인지 확인하는 메소드 작성

--- src/main/java/baseball/BaseBallGameNumber.java
+++ src/main/java/baseball/BaseBallGameNumber.java
@@ -11,6 +11,7 @@ public class BaseBallGameNumber {
     if(inputString.length() != 3) throw new IllegalArgumentException("세자리가 아닙니다!");
     int gameNumber = stringToInt(inputString);
     setBaseBallGameNum(gameNumber);
+    checkNumbersValid();
   }
 
   private int stringToInt(String inputString) {
@@ -25,5 +26,22 @@ public class BaseBallGameNumber {
     gameNumberList.add(0, gameNumber / 100);
     gameNumberList.add(1, gameNumber % 100 / 10);
     gameNumberList.add(2, gameNumber % 10);
+  }
+
+  private void checkNumbersValid() {
+    checkDifferentNumbers();
+    checkIncludeZero();
+  }
+
+  private void checkDifferentNumbers() {
+    for (int i = 1; i < gameNumberList.size(); i++) {
+      if (gameNumberList.get(i) == gameNumberList.get(i - 1))
+        throw new IllegalArgumentException("중복되는 숫자가 있습니다.");
+    }
+  }
+
+  private void checkIncludeZero() {
+    if (gameNumberList.get(0) * gameNumberList.get(1) * gameNumberList.get(2) == 0)
+      throw new IllegalArgumentException("0이 포함되어 있습니다.");
   }
 }
