2bd4c8b99397833a04848ca6303a2ea08ffc4cfd
hyeonseog-won <smreo75@gmail.com>
2022-11-07 14:02:14 +0900

    test: 중복 검증 테스트 추가

--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -13,4 +13,13 @@ public class UserTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("서로 다른 수로 이루어진 3자리의 수를 입력해야 합니다.");
     }
+
+    @Test
+    void 사용자_입력에_중복이_있을_경우_예외_발생() {
+        User user = new User();
+        assertThatThrownBy(() -> user.duplicateCheck("111"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("서로 다른 수로 이루어진 3자리의 수를 입력해야 합니다.");
+    }
+
 }
