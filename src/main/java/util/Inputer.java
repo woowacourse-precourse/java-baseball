b00af7f0c991abda7220db16f5b0c943b434134b
Identityless <rlandwnd555@naver.com>
2022-11-08 02:05:42 +0900

    feat : 게임 시작 종료 플래그 입력 받을 stopOrGo 메서드 추가

--- src/main/java/util/Inputer.java
+++ src/main/java/util/Inputer.java
@@ -13,6 +13,24 @@ public class Inputer {
         this.flag = false;
     }
 
+    public List<Integer> getInput() throws IllegalArgumentException{
+        String rareinput = Console.readLine();
+        if(!flag) {
+            return stopOrGo(rareinput);
+        }
 
+    }
+
+    private List<Integer> stopOrGo(String input){
+        if(Integer.parseInt(input) == 1) {
+            return List.of(1);
+        }
+        else if(Integer.parseInt(input) == 2) {
+            return List.of(2);
+        }
+        else {
+            throw new IllegalArgumentException("Please input right arguments!");
+        }
+    }
 
 }
