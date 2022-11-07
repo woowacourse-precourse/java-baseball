3bd428bd2afc8f8329ecc786e1b786c789f29d38
이경범 <tkdwk567@naver.com>
2022-11-07 16:46:28 +0900

    refactor : GameMessage 열거 원소 추가

--- src/main/java/baseball/GameMessage.java
+++ src/main/java/baseball/GameMessage.java
@@ -4,6 +4,7 @@ public enum GameMessage {
 
     START("숫자 야구 게임을 시작합니다."),
     END("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+    RESTART_OR_END("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
     NOTHING("낫싱");
 
     private final String message;
