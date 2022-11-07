9cd3e0724ff342472991698bf8c1a510b596f4ec
임원정 <LWJ@MacBookPro.local>
2022-11-07 19:28:57 +0900

    feat : 숫자 위치를 확인하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,16 +3,16 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 class Computer {
+    static List<Integer> numberList = new ArrayList<>();
+
     static void getThreeRandomNumber() {
-        List<Integer> numberList = new ArrayList<>();
         while (numberList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!numberList.contains(randomNumber)) {
@@ -32,7 +32,11 @@ class Player {
     }
 }
 
+// todo readLine()이 널일때
 class Umpire {
+    static int strike = 0;
+    static int ball = 0;
+    static HashSet<Integer> strikeSet = new HashSet<>();
 
     static void gameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -52,9 +56,6 @@ class Umpire {
             Player.numberList.add(0, Player.number % 10);
             Player.number /= 10;
         }
-
-
-
     }
 
     private static void checkInput() {
@@ -68,6 +69,40 @@ class Umpire {
             throw new IllegalArgumentException("wrong size");
     }
 
+
+    static void getScore() {
+        checkStrike();
+        checkBall();
+    }
+
+    static void checkStrike() {
+
+        for (int i = 0; i < 3; i++) {
+            if (Computer.numberList.get(i) == Player.numberList.get(i)) {
+                strike++;
+                strikeSet.add(i);
+            }
+        }
+    }
+
+    static void checkBall() {
+        for (int i = 0; i < 3; i++) {
+            if (strikeSet.contains(i))
+                continue;
+            addBall(i);
+        }
+    }
+
+    static void addBall(int i){
+        for (int j = 0; j < 3; j++) {
+            if (Computer.numberList.get(i) == Player.numberList.get(j)) {
+                ball++;
+                break;
+            }
+        }
+    }
+
+
 }
 
 public class Application {
@@ -77,7 +112,14 @@ public class Application {
         Computer.getThreeRandomNumber();
         while (!getAnswer) {
             Umpire.getNumber();
+
+            System.out.println(Computer.numberList);
             System.out.println(Player.numberList);
+
+            Umpire.getScore();
+
+            System.out.println(Umpire.ball + " " + Umpire.strike);
+
             return;
         }
 
