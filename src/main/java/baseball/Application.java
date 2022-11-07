6fefc97adbafb0061cf394f809f59836dbc61f79
kkkwp <chxrryda@gmail.com>
2022-11-08 01:40:23 +0900

    feat(main): 게임 실행 흐름 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,8 +9,16 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        do {
+            List<Integer> computer = makeAnswer();
+            String result = "";
+            while (!result.equals("3스트라이크")) {
+                // result = computer vs user
+                System.out.println(result);
+            }
+        } while (gameOver());
     }
 
     private static boolean gameOver() {
