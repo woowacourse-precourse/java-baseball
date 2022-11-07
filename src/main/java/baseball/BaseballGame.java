9f341c0c1a49ad416acce8a5ab7ec423438c9329
송민진 <m_6595@naver.com>
2022-11-07 19:35:24 +0900

    feat(baseball): check if the number is invalid
    숫자가 서로 다른 세 자리 수인지 유효성 검사하는 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,9 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.Console.*;
 
 public class BaseballGame {
@@ -20,9 +24,33 @@ public class BaseballGame {
         getUserInput();
     }
 
-    int getUserInput() {
+    private void getUserInput() {
+
+        // 숫자 입력받기
         System.out.println("숫자를 입력해주세요 : ");
         int userInput = Integer.parseInt(readLine());
-        return userInput;
+
+        // 입력받은 숫자 유효성 검사
+        if (!isInvalidNumber(userInput)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean isInvalidNumber(int num) {
+        boolean invalidTF = true;
+
+        // 한자리씩 배열에 넣기
+        List<Integer> checkArr = new ArrayList<>();
+        while (num > 0) {
+            checkArr.add(num % 10);
+            num /= 10;
+        }
+        HashSet<Integer> hashSet = new HashSet<>(checkArr);
+
+        // 입력받은 숫자가 세자리가 아니거나, 세자리 수여도 서로 다른 숫자들이 아닐 경우
+        if (checkArr.size() != 3 || hashSet.size() != 3) {
+            invalidTF = false;
+        }
+        return invalidTF;
     }
 }
