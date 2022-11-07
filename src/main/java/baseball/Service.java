04237ce6c99e543d44e86676e9f6f726b8906bd1
skfkgla <narahim.lee@gmail.com>
2022-11-08 04:39:06 +0900

    style: 매개변수명 변경

--- src/main/java/baseball/Service.java
+++ src/main/java/baseball/Service.java
@@ -104,9 +104,9 @@ public class Service {
         }
     }
 
-    private boolean askEndingCondition(int endingCondition) {
+    private boolean askEndingCondition(int gameClearCondition) {
         boolean maintain = false;
-        System.out.println(endingCondition + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(gameClearCondition + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String askingFinish = Console.readLine();
 
