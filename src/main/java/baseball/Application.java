ba56ccd627da940096d13da3447490b4e01cc5d1
Woong99 <dndry1072@naver.com>
2022-11-07 14:17:45 +0900

    feat : 컴퓨터가 임의의 수 3개를 선택하는 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        List<Integer> ls = getRandomThreeNumbers();
+        System.out.println(ls);
+    }
+
+    public static List<Integer> getRandomThreeNumbers(){
+        List<Integer> numberList = new ArrayList<>();
+        while(numberList.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!numberList.contains(randomNumber)){
+                numberList.add(randomNumber);
+            }
+        }
+        return numberList;
     }
 }
