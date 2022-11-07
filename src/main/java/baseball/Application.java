d2ae8b1f33b817fb696eb4b3005fd9d22ffea7be
hayeon <qorgkdus12@naver.com>
2022-11-07 15:15:31 +0900

    feat(main): create 3 random numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,5 +8,14 @@ public class Application {
     public static void main(String[] args) {
         // 1. 시작 멘트
         System.out.println("숫자 야구 게임을 시작합니다.");
+
+        // 2. 숫자 3개 랜덤 추출
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() <3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if (!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
     }
 }
