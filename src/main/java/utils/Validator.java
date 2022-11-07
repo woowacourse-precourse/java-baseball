21e43c5a43d66f0228f3e6c905472e3e0bf5a6cf
twoosky <lhjae3@naver.com>
2022-11-08 01:32:47 +0900

    feat(input): 예외 처리 객체 생성

---
+++ src/main/java/utils/Validator.java
@@ -0,0 +1,11 @@
+package utils;
+
+import java.util.*;
+
+public class Validator {
+    private static String input;
+
+    public static void validate(String inputNumber) {
+        input = inputNumber;
+    }
+}
