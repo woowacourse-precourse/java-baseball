3083b99f728c9a39792183a0b64843ae14465ff1
hyun6ik <fkanwm@naver.com>
2022-11-07 18:06:52 +0900

    feat(23) : 숫자 야구 게임 로직 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import baseball.global.config.AppConfig;
+import baseball.presentation.BaseBallGameController;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        final AppConfig appConfig = new AppConfig();
+        final BaseBallGameController gameController = appConfig.gameController();
+
+        boolean isContinue;
+        do {
+            isContinue = gameController.baseballGame();
+        } while (isContinue);
     }
 }
