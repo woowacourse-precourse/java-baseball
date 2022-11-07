8a453b23294ef54875ba34954743312f20862b1f
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-11-07 17:14:57 +0900

    feat: assemble application
    enum 클래스(Announcement)
    - GAME_START,  GAME_END 를 추가한다
    
    Implement application
    - program scenario에 맞춰 구성한다 (./docs/README.md)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import baseball.enums.Announcement;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Announcement.GAME_START.printlnAnnouncement();
+
+        Gamer gamer = new Gamer();
+        GameMachine gameMachine = new GameMachine();
+
+        boolean playFlag = true;
+        while (playFlag) {
+            Game game = new Game();
+            List<Integer> integerList = gameMachine.generateThreeDiffDigit();
+            game.setThreeDiffDigit(integerList);
+
+            while (game.getNumberOfStrike() != 3) {
+                game.initializeBallCount();
+
+                List<Integer> threeDiffDigitOfGamer = gameMachine.askInputThreeDiffDigit(gamer);
+                gameMachine.updateBallCount(threeDiffDigitOfGamer, game);
+
+                System.out.println(game.getBallCount());
+            }
+            Announcement.GAME_END.printlnAnnouncement();
+
+            playFlag = gameMachine.askReplay(gamer);
+        }
     }
 }
