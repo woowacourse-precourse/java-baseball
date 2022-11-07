97d945590c01860320ba8805e822634997cb51e6
JasmineAir <gofoxfox@naver.com>
2022-11-07 20:31:11 +0900

    1. 세자리 난수 입력 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,21 @@
 package baseball;
 
+import java.util.List;
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        System.out.println("숫자 야구게임을 시작합니다.");
+        System.out.println("숫자를 입력해주세요 : ");
+
     }
 }
