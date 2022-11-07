78c267ecae1db6797eef45768ee44a80f06a1f78
whdcks420 <whdcks420@gmail.com>
2022-11-07 23:53:18 +0900

    test: 야구게임 출력문구 test

--- src/test/java/baseball/view/BaseballViewerTest.java
+++ src/test/java/baseball/view/BaseballViewerTest.java
@@ -6,21 +6,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class BaseballViewTest {
-    private final BaseballView baseBallView;
-    private final ByteArrayOutputStream output = new ByteArrayOutputStream();
-    private final PrintStream standardOut = System.out;
-
-    BaseballViewTest() {
-        this.baseBallView = new BaseballView();
-    }
+class BaseballViewerTest {
+    private final BaseballViewer baseballViewer = new BaseballViewer();
+    private PrintStream standardOut;
+    private OutputStream output;
 
     @BeforeEach
     public void setUp() {
+        standardOut = System.out;
+        output = new ByteArrayOutputStream();
         System.setOut(new PrintStream(output));
     }
 
@@ -31,43 +30,47 @@ class BaseballViewTest {
 
     @Test
     void 게임_시작_문구_출력() {
-        baseBallView.showStart();
+        baseballViewer.showStartText();
         assertEquals("숫자 야구 게임을 시작합니다.\n", output.toString());
     }
 
     @Test
     void 게임_시작_문구_출력_실패() {
-        baseBallView.showStart();
+        baseballViewer.showStartText();
         assertNotEquals("숫자 축구 게임을 시작합니다.\n", output.toString());
     }
 
     @Test
-    void 숫자_입력_문구_출력() {
-        baseBallView.showInput();
+    void 숫자_입력_문구() {
+        baseballViewer.showInputText();
         assertEquals("숫자를 입력해주세요 : ", output.toString());
     }
 
     @Test
     void 숫자_입력_문구_출력_실패() {
-        baseBallView.showInput();
+        baseballViewer.showInputText();
         assertNotEquals("문자를 입력해주세요 : ", output.toString());
     }
 
     @Test
     void 점수_출력_테스트() {
-        baseBallView.showScore(new Score(1, 2));
+        Score score = new Score(1, 2);
+        baseballViewer.showScore(score);
         assertAll(
                 () -> assertEquals("1볼 2스트라이크\n", output.toString()),
                 () -> assertNotEquals("2볼 1스트라이크\n", output.toString())
         );
-
     }
 
     @Test
     void 종료_출력_테스트() {
-        baseBallView.showContinueOrStop();
-        assertEquals("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", output.toString());
+        baseballViewer.showWinText();
+        assertEquals("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n", output.toString());
     }
 
+    @Test
+    void 재시작_출력_테스트() {
+        baseballViewer.showRestartGameText();
+        assertEquals("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n", output.toString());
+    }
 }
