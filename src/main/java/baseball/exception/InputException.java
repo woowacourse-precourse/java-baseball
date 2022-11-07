de98a52afd1b3ed00bb175dff1f568c9022f940d
TaeHwan <kimta2hwan@gmail.com>
2022-11-07 11:54:50 +0900

    feat(inputexception): 사용자가 입력한 3자리의 정수에 0이 있는 경우 예외 처리 기능 구현

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -4,6 +4,7 @@ public class InputException {
 
     private static final String NOT_INTEGER_INPUT = "정수를 입력해 주세요.";
     private static final String NOT_THREE_LENGTH_INTEGER_INPUT = "3자리의 정수를 입력해 주세요.";
+    private static final String INCLUDE_ZERO_INPUT = "0이 포함되지 않아야 합니다.";
 
     public static void throwNotIntegerInputError() {
         throw new IllegalArgumentException(NOT_INTEGER_INPUT);
@@ -11,5 +12,9 @@ public class InputException {
 
     public static void throwNotThreeLengthIntegerInputError() {
         throw new IllegalArgumentException(NOT_THREE_LENGTH_INTEGER_INPUT);
+    }
+
+    public static void throwIncludeZeroInputError() {
+        throw new IllegalArgumentException(INCLUDE_ZERO_INPUT);
     }
 }
