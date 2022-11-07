a636956db7ab5d79cd92e57e736470d12cfe5862
JunH <tjdtls690@gmail.com>
2022-11-07 15:30:13 +0900

    :white_check_mark: test(Domain : BaseBallGameTest) : 길이가 3인 Balls 입력 값인지 확인하는 테스트 추가

--- src/test/java/baseball/domain/BaseBallGameTest.java
+++ src/test/java/baseball/domain/BaseBallGameTest.java
@@ -1,18 +1,33 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.assertThatNoException;
 
 class BaseBallGameTest {
+    private BaseBallGame baseBallGame;
+    
+    @BeforeEach
+    void setUp() {
+        baseBallGame = new BaseBallGame();
+    }
+    
+    @DisplayName("길이가 3인 Balls 입력 값인지 확인")
+    @ParameterizedTest(name = "{displayName} => {0}")
+    @ValueSource(strings = {"123", "456", "789"})
+    void correct_balls(String balls) {
+        assertThatNoException()
+                .isThrownBy(() -> baseBallGame.play(balls));
+    }
+    
     @DisplayName("예외 처리 : 입력된 숫자의 길이가 3이 아닌 경우")
     @ParameterizedTest(name = "{displayName} => {0}")
     @ValueSource(strings = {"12", "1234"})
     void exceed_range_exception(String numbers) {
-        BaseBallGame baseBallGame = new BaseBallGame();
-        
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> baseBallGame.play(numbers))
                 .withMessage("숫자 3개만 입력할 수 있습니다.");
