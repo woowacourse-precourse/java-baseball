e6b9fde77029dcf2cf5ccb664c88cb02adfb0c78
devMuscle <hongsbien@naver.com>
2022-11-08 02:14:47 +0900

    feat: 경기 결과 저장 클래스 생성

---
+++ src/main/java/baseball/Result.java
@@ -0,0 +1,15 @@
+package baseball;
+
+public class Result {
+
+    private Integer strike;
+    private Integer ball;
+
+    public void setStrike(Integer strike) {
+        this.strike = strike;
+    }
+
+    public void setBall(Integer ball) {
+        this.ball = ball;
+    }
+}
