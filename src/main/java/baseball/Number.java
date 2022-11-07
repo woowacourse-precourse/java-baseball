2f11a7456b66678809645dc0b5f4b013a3674a06
hhiyeon <hhyyeon0214@gmail.com>
2022-11-07 20:19:22 +0900

    feat: number-get user number

---
+++ src/main/java/baseball/Number.java
@@ -0,0 +1,11 @@
+package baseball;
+
+import java.util.List;
+
+class Number {
+    private static List<Integer> userNumbers;
+
+    public static List<Integer> getUserNumbers() {
+        return userNumbers;
+    }
+}
