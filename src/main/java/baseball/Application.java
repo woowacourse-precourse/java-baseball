da9081c8abe10b11a2803a3dfdbe9fdbde1d8b6a
wnsgml7267 <dlwns7267@naver.com>
2022-11-08 04:15:23 +0900

    feat(controller/BaseballSystem): 게임 시작, 재시작, 종료 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.controller.BaseballSystem;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballSystem system = new BaseballSystem();
+        system.runGame();
     }
 }
