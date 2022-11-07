b87576a6dbeb32de40fcabaaa6bdf11f71f4ef16
cuttlefish55 <funade7@gmail.com>
2022-11-07 17:38:39 +0900

    feat: Add "NumberReferee.java" to judge ball and strike

---
+++ src/main/java/baseball/data/NumberReferee.java
@@ -0,0 +1,32 @@
+package baseball.data;
+
+import java.util.List;
+
+/**
+ * 플레이어와 컴퓨터의 숫자를 비교해서 볼과 스트라이크를 판단.
+ */
+public class NumberReferee {
+
+    public int checkStrike(List<Integer> player, List<Integer> computer){
+        int strike = 0;
+
+        for(int number = 0; number < player.size(); number ++ ){
+            if(player.get(number).equals(computer.get(number))){
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    public int checkBall(List<Integer> player, List<Integer> computer){
+        int strike = checkStrike(player, computer);
+        int ball = 0;
+
+        for(Integer number : player){
+            if(computer.contains(number)){
+                ball++;
+            }
+        }
+        return ball - strike;
+    }
+}
