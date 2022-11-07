86f6db33e180b6c681ef98d27bf0e5b572889a2a
kssumin <201566@jnu.ac.kr>
2022-11-07 15:37:52 +0900

    feat : 1 ~ 9 사이의 수인지 검증

---
+++ src/main/java/baseball/ball/Ball.java
@@ -0,0 +1,23 @@
+package baseball.ball;
+
+public class Ball {
+    private int ballNumber;
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final String NOT_ALLOWED_UNDER_MIN_NUMBER = "볼은 최소 1 입니다.";
+    private static final String NOT_ALLOWED_OVER_MAX_NUMBER = "볼은 최대 9 입니다.";
+
+    public Ball(int ballNumber) {
+        validateRange(ballNumber);
+        this.ballNumber = ballNumber;
+    }
+
+    private void validateRange(int ballNumber) {
+        if (ballNumber < MIN_NUMBER) {
+            throw new IllegalStateException(NOT_ALLOWED_UNDER_MIN_NUMBER);
+        }
+        if (ballNumber > MAX_NUMBER) {
+            throw new IllegalStateException(NOT_ALLOWED_OVER_MAX_NUMBER);
+        }
+    }
+}
