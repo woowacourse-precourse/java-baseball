c820a2749e0673575303988882fea772a481c9e3
Your Name <dbsdbxkrzz@naver.com>
2022-11-07 16:43:16 +0900

    feat: validate 클래스에 메서드를 추가합니다
    입력받은 값이 세자리인지 확인하는 isNumberLengthLegal 메서드를 추가합니다
    checkCorrectInput
    메서드에서 올바르지 않은 입력에 대해 IllegalArgumentException과 상황에 맞는 문구를 출력하도록 합니다

--- src/main/java/baseball/Validate.java
+++ src/main/java/baseball/Validate.java
@@ -1,8 +1,19 @@
 package baseball;
 
 public class Validate {
-	public void checkCorrectInput(String number){
-
+	public static void checkCorrectInput(String number){
+		if (!isNumeric(number)){
+			throw new IllegalArgumentException(
+				Message.USER_INPUT_NOT_NUMBER.getMsg() +
+				Message.INVALID_INPUT_END_GAME.getMsg()
+			);
+		}
+		if (!isNumberLengthLegal(number)){
+			throw new IllegalArgumentException(
+				Message.USER_INPUT_LENGTH_INVALID.getMsg() +
+					Message.INVALID_INPUT_END_GAME.getMsg()
+			);
+		}
 	}
 
 	public static boolean isNumeric(String number){
@@ -12,6 +23,10 @@ public class Validate {
 		}catch (NumberFormatException e){
 			return false;
 		}
+	}
+
+	public static boolean isNumberLengthLegal(String number){
+		return number.length() == Constant.NUMBER_LENGTH.getNumber();
 	}
 
 }
