28ec1d94a0442a3ce4b3702dd4cbd2f80bd4e5c9
sora5421 <21800804@handong.edu>
2022-11-07 19:39:34 +0900

    feat : make the computer random numbers

--- src/main/java/baseball/datamodel/ComputerNumber.java
+++ src/main/java/baseball/datamodel/ComputerNumber.java
@@ -1,5 +1,20 @@
 package baseball.datamodel;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComputerNumber {
+    private final List<Integer> numbers = new ArrayList<>();
     public ComputerNumber() {}
+    public void makeNumber() {
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+    }
 }
