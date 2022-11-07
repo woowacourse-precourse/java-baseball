e6b308776b750cf56302fa25949f84b728d3b38f
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-07 18:46:38 +0900

    feat : User 생성자 추가

---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,16 @@
+package baseball.domain;
+
+import baseball.exception.UserException;
+import java.util.ArrayList;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+
+public class User {
+    public User() {
+        userInputNumberList = new ArrayList<>();
+    }
+
+
+
+
+}
