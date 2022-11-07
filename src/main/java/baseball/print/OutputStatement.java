a9e5d1d4d835aa53cb5a72771c899c9b6bade726
jaehyeon <jhjh0022@naver.com>
2022-11-07 21:50:37 +0900

    refactor: 메서드 이름 명확하게 하기 -> informResultAnswerAndFinish

--- src/main/java/baseball/print/OutputStatement.java
+++ src/main/java/baseball/print/OutputStatement.java
@@ -1,15 +1,15 @@
 package baseball.print;
 
 public class OutputStatement {
-    public static void informStartNumberBaseballGame(){
+    public static void informStartNumberBaseballGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public static void requestNumberInput(){
+    public static void requestNumberInput() {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public static void printNotSing(){
+    public static void printNotSing() {
         System.out.println("낫싱");
     }
 
@@ -25,23 +25,23 @@ public class OutputStatement {
         }
     }
 
-    public static void informResultAnswer(){
+    public static void informResultAnswerAndFinish() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
-    public static void askGameRestartOrFinish(){
+    public static void askGameRestartOrFinish() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
-    private static void printBallCntNumber(int ball){
-        System.out.println(ball+"볼");
+    private static void printBallCntNumber(int ball) {
+        System.out.println(ball + "볼");
     }
 
-    private static void printStrikeCntNumber(int strike){
+    private static void printStrikeCntNumber(int strike) {
         System.out.println(strike + "스트라이크");
     }
 
-    private static void printBallAndStrikeCntNumber(int ball, int strike){
+    private static void printBallAndStrikeCntNumber(int ball, int strike) {
         System.out.println(ball + "볼" + " " + strike + "스트라이크");
     }
 
