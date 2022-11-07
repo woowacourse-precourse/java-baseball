b6aec3784562a57d6b9386b0f14952b4132ee5df
drunkenhw <drunkenhw@gmail.com>
2022-11-07 23:54:41 +0900

    test(GameStatusTest) : 1,2가 아닌 숫자는 예외를 발생시킨다

---
+++ src/test/java/baseball/domain/GameStatusTest.java
@@ -0,0 +1,28 @@
+package baseball.domain;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+public class GameStatusTest {
+
+    @Test
+    void isRestartTest() {
+        GameStatus gameStatus = GameStatus.getGameStatus(1);
+
+        assertThat(gameStatus).isEqualTo(GameStatus.RESTART);
+    }
+
+    @Test
+    void isEndTest() {
+        GameStatus gameStatus = GameStatus.getGameStatus(2);
+
+        assertThat(gameStatus).isEqualTo(GameStatus.END);
+    }
+
+    @Test
+    void isInvalidNumberTest() {
+        assertThatThrownBy(() -> GameStatus.getGameStatus(3))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
