0537ef4311480837a80629eb7243233d4dd5566c
ChanghyunKim <variety82p@gamil.com>
2022-11-07 20:45:38 +0900

    test : 볼_테스트
    1볼, 2볼, 3볼 테스트 후 게임종료 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -9,46 +9,58 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+//    @Test
+//    void 게임종료_후_재시작() {
+//        assertRandomNumberInRangeTest(
+//                () -> {
+//                    run("246", "135", "1", "597", "589", "2");
+//                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+//                },
+//                1, 3, 5, 5, 8, 9
+//        );
+//    }
+//
+//    @Test
+//    void 예외_테스트() {
+//        assertSimpleTest(() ->
+//                assertThatThrownBy(() -> runException("1234"))
+//                        .isInstanceOf(IllegalArgumentException.class)
+//        );
+//    }
+//
+//    @Test
+//    void 공_범위_예외_테스트() {
+//        assertSimpleTest(() ->
+//                assertThatThrownBy(() -> runException("101"))
+//                        .isInstanceOf(IllegalArgumentException.class)
+//        );
+//    }
+//
+//    @Test
+//    void 공_사이즈_예외_테스트() {
+//        assertSimpleTest(() ->
+//                assertThatThrownBy(() -> runException("11"))
+//                        .isInstanceOf(IllegalArgumentException.class)
+//        );
+//    }
+//    @Test
+//    void 중복_예외_테스트(){
+//        assertSimpleTest(() ->
+//                assertThatThrownBy(() -> runException("121"))
+//                        .isInstanceOf(IllegalArgumentException.class));
+//    }
+
     @Test
-    void 게임종료_후_재시작() {
+    void 볼_테스트(){
         assertRandomNumberInRangeTest(
                 () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                    run("127", "872", "897", "789", "2");
+                    assertThat(output()).contains("1볼", "2볼", "3볼", "3스트라이크", "게임 종료");
                 },
-                1, 3, 5, 5, 8, 9
+                7, 8, 9
         );
     }
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 공_범위_예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("101"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Test
-    void 공_사이즈_예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("11"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-    @Test
-    void 중복_예외_테스트(){
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("121"))
-                        .isInstanceOf(IllegalArgumentException.class));
-    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
