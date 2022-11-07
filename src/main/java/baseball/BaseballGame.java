4a75ba5fc3df9069139fc93bc7dfe3bed5aa205d
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-08 04:13:53 +0900

    feat: 타겟 숫자(targetNumber) 생성 기능 추가 camp.nextstep.edu.missionutils에서 제공하는 Randoms API를 활용하여, 서로 다른 난수 3개로 구성된 세 자리 타겟 숫자를 생성합니다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,6 +1,20 @@
 package baseball;
 
+import java.util.List;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class BaseballGame {
+    List<Integer> targetNumber;
+
+    public void generateTargetNumber(){
+        while(targetNumber.size() < 3) {
+            int randomNumber=Randoms.pickNumberInRange(1, 9);
+            if (!targetNumber.contains(randomNumber)) {
+                targetNumber.add(randomNumber);
+            }
+        }
+    }
+
     public void startGame(){
         // TODO: 타겟 넘버 생성, 사용자 입력 실행 등 새 게임 시작
     }
