5ee313f1d727e4b943702b2d4a2506357d9983db
doongu <must1080@naver.com>
2022-11-07 13:16:22 +0900

    feat(computer): 숫자를 조회하는 기능 구현

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+
+public class Computer {
+    private String answer;
+
+    public String getAnswer(){
+        return answer;
+    }
+}
