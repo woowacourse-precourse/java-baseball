de3d60a0387769fe46a2d6a879bf397ccfddb39a
jojaeng2 <ds4ouj@naver.com>
2022-11-07 13:22:43 +0900

    feat : 사용자에게 입력받은 숫자가 올바른지 확인하는 기능구현

---
+++ src/main/java/baseball/util/InputChecker.java
@@ -0,0 +1,44 @@
+package baseball.util;
+
+import java.util.Arrays;
+
+import static java.util.stream.Collectors.joining;
+
+public final class InputChecker {
+
+    private static final int LIMITED_INPUT_SIZE = 3;
+
+
+    private InputChecker() {
+    }
+
+    public static void isValid(String input) {
+        isSizeEqualToLimitedSize(input);
+        isNotExistRepeatedNumber(input);
+    }
+
+    private static void isNotExistRepeatedNumber(String input) {
+        String compactInput = Arrays.stream(input.split(""))
+                .filter(InputChecker::isDigit)
+                .distinct()
+                .collect(joining());
+        isSizeEqualToLimitedSize(compactInput);
+    }
+
+    private static void isSizeEqualToLimitedSize(String input) {
+        if (input.length() != LIMITED_INPUT_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static boolean isDigit(String init) {
+        if (isNotDigit(init)) {
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
+
+    private static boolean isNotDigit(String init) {
+        return Character.isDigit(init.charAt(0));
+    }
+}
