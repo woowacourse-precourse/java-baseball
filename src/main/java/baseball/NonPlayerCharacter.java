3bbe6854b500310c9ca145c34993cf0e90f4bce5
yujaemin <woawoa0513@gmail.com>
2022-11-07 22:50:29 +0900

    feat: 난수와의 비교 결과 출력 함수 구현
    세 자리의 수를 난수와 비교하여 나온 볼,스트라이크 개수(또는 낫싱)을 출력한다.

--- src/main/java/baseball/NonPlayerCharacter.java
+++ src/main/java/baseball/NonPlayerCharacter.java
@@ -18,9 +18,26 @@ public class NonPlayerCharacter {
         System.out.print("숫자를 입력해주세요 : ");
     }
 
+    public void printComparisonResult() {
+        System.out.println(makeResultString());
+    }
+
     public void printGameEndNotice() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
                 "게임 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public String makeResultString() {
+        if (numberOfBallAndStrike[BALL] == 0 && numberOfBallAndStrike[STRIKE] == 0) {
+            return "낫싱";
+        }
+        if (numberOfBallAndStrike[BALL] == 0) {
+            return "3스트라이크";
+        }
+        if (numberOfBallAndStrike[STRIKE] == 0) {
+            return "3볼";
+        }
+        return numberOfBallAndStrike[BALL] + "볼 " + numberOfBallAndStrike[STRIKE] + "스트라이크";
     }
 
     public void countNumberOfBallAndStrike(int input) {
