45541a55be1c36becf7f7d70307ab2042b90b9ed
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-08 06:13:05 +0900

    feat(Application.java): main 함수 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,12 @@
 package baseball;
 
+import baseball.game.Game;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game game = new Game();
+        game.start();
     }
 }
