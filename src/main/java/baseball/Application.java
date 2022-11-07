9e02204752b85a83c31463c6c5a91f55f5e24a92
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-08 00:45:33 +0900

    feat(Application): add game setting

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,6 +5,7 @@ import baseball.controller.GameController;
 public class Application {
     public static void main(String[] args) {
         GameController controller = new GameController();
+        controller.setGame();
         controller.run();
     }
 }
