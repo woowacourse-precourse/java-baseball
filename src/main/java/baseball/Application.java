68a9ae222d3e296b79915501bda656da322cfde6
qdtjj4 <76ylo1@gmail.com>
2022-11-08 02:29:02 +0900

    feat: 게임의 시작을 알리는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,10 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        startGame();
+    }
+
+    static void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
