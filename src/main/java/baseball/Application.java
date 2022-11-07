a0fc6d724dbf7925ab6b2cf5a18b299e2b94d10a
phj1120 <hyeonj1998@naver.com>
2022-11-07 22:15:47 +0900

    feat: 출력 요구사항 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,8 +12,10 @@ public class Application {
         List<Integer> computerNumber = Utils.pickComputerNumber();
         boolean applicationStatus = true;
         boolean gameStatus = true;
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (applicationStatus) {
             while (gameStatus) {
+                System.out.print("숫자를 입력해주세요 : ");
                 String line = Console.readLine();
                 List<Integer> userNumber = new ArrayList<>();
                 for (int i = 0; i < line.length(); i++) {
@@ -23,11 +25,9 @@ public class Application {
                 int ball = 0;
                 int strike = 0;
                 for (int i = 0; i < computerNumber.size(); i++) {
-                    // strike 확인
                     if (computerNumber.get(i) == userNumber.get(i)) {
                         strike++;
                     } else if (computerNumber.contains(userNumber.get(i))) {
-                        // ball 확인
                         ball++;
                     }
                 }
@@ -47,9 +47,11 @@ public class Application {
                     gameStatus = false;
                 }
             }
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String line = Console.readLine();
             if ("2".equals(line)) {
                 applicationStatus = false;
+                System.out.println("게임 종료");
             }
         }
     }
