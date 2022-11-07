2c6c6f61b336dd339a6fb793a7f3a9b343bfc763
Qulip <ilkwon.ryu@gmail.com>
2022-11-08 06:54:09 +0900

    refactor: BaseballGameInputValidatorTest 리팩터링

--- src/test/java/baseball/view/BaseballGameInputValidatorTest.java
+++ src/test/java/baseball/view/BaseballGameInputValidatorTest.java
@@ -4,59 +4,65 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("입력 유효성 확인")
 class BaseballGameInputValidatorTest {
-    @DisplayName("길이가 3이 아닌 경우")
-    @Test
-    public void inputLengthError() {
-        // given
-        String input = "12";
 
-        // when
-        final boolean result = BaseballGameInputValidator.validatePlayerNumber(input);
+    @Nested
+    @DisplayName("사용자 숫자 입력 유효성 검증")
+    class NumberInputValidation {
+        @DisplayName("길이가 3이 아닌 경우")
+        @Test
+        public void inputLengthError() {
+            // given
+            String input = "12";
 
-        // then
-        assertFalse(result);
-    }
+            // when
+            final boolean result = BaseballGameInputValidator.validatePlayerNumber(input);
 
-    @DisplayName("같은 값이 있는 경우")
-    @Test
-    public void sameNumberError() {
-        // given
-        String input = "112";
+            // then
+            assertFalse(result);
+        }
 
-        // when
-        final boolean result = BaseballGameInputValidator.validatePlayerNumber(input);
+        @DisplayName("같은 값이 있는 경우")
+        @Test
+        public void sameNumberError() {
+            // given
+            String input = "112";
 
-        // then
-        assertFalse(result);
-    }
+            // when
+            final boolean result = BaseballGameInputValidator.validatePlayerNumber(input);
 
-    @DisplayName("숫자가 아닌 값이 있는 경우")
-    @Test
-    public void notNumberError() {
-        // given
-        String input = "1a2";
+            // then
+            assertFalse(result);
+        }
 
-        // when
-        final boolean result = BaseballGameInputValidator.validatePlayerNumber(input);
+        @DisplayName("숫자가 아닌 값이 있는 경우")
+        @Test
+        public void notNumberError() {
+            // given
+            String input = "1a2";
 
-        // then
-        assertFalse(result);
-    }
+            // when
+            final boolean result = BaseballGameInputValidator.validatePlayerNumber(input);
 
-    @DisplayName("올바른 값인 경우")
-    @Test
-    public void success() {
-        // given
-        String input = "123";
+            // then
+            assertFalse(result);
+        }
 
-        // when
-        final boolean result = BaseballGameInputValidator.validatePlayerNumber(input);
+        @DisplayName("올바른 값인 경우")
+        @Test
+        public void success() {
+            // given
+            String input = "123";
 
-        // then
-        assertTrue(result);
+            // when
+            final boolean result = BaseballGameInputValidator.validatePlayerNumber(input);
+
+            // then
+            assertTrue(result);
+        }
     }
 }
