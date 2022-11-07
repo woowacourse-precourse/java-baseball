b4be3b321e45e351ca14f5222e16e02b1054b740
gudals <kimtuti@naver.com>
2022-11-07 18:13:50 +0900

    feat: add game play features
    gameStart() - 입력을 받아 Int 배열로 저장
    gameStartConditional(int[]) - 입력 배열의 크기와 중복제거한 입력 배열의 3인 경우에만 진행 그 외 상황 IllegalException
    removeDuplicates(int[]) - 배열의 중복값 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,9 +3,15 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
 
     public static Boolean gameBreak(){
@@ -22,4 +28,31 @@ public class Application {
             throw new IllegalArgumentException();
         }
     }
+
+    private static void gameStart(){
+        int[] input_arr = Stream.of(Console.readLine().split("")).mapToInt(Integer::parseInt).toArray();
+        gameStartConditional(input_arr);
+
+
+
+    }
+
+    private static void gameStartConditional(int[] input_arr) {
+        if (getGameStartConditional(input_arr)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static Boolean getGameStartConditional(int[] input_arr) {
+        if (input_arr.length == 3 && removeDuplicates(input_arr).length == 3){
+            return false;
+        }
+        return true;
+    }
+    public static int[] removeDuplicates(int[] arr) {
+        Set<Integer> set = Arrays.stream(arr).boxed().collect(Collectors.toSet());
+        return set.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+
 }
