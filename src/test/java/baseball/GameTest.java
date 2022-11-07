3b2dbea05a19d799702b135dcbe3e80f66055de8
choiejae <ejae8319@gmail.com>
2022-11-07 14:53:59 +0900

    refactor: 하나의 메소드 안에서 여러개의 테스트를 돌릴 수 있도록 @Test -> @ParameterizedTest 변경

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,10 +1,14 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -13,6 +17,9 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GameTest {
@@ -23,21 +30,19 @@ public class GameTest {
 
   @BeforeEach
   public void setUp(){
-    outContent = new ByteArrayOutputStream();
-    originalOut = System.out;
-    System.setOut(new PrintStream(outContent));
     game = new Game("1");
   }
 
-  @AfterEach
-  public void restoreStreams() {
-    System.setOut(originalOut);
+  //    String 상태로 내가 넣어준 테스트용 input을 바이트 코드로 바꾸어 준다.
+  public static InputStream generateUserInput(String input) {
+    return new ByteArrayInputStream(input.getBytes());
+  }
+  public void generateNumberForTest(String input) {
+    if (game.validateChecker(input)) {
+      throw new IllegalArgumentException("1, 또는 2를 입력해야 합니다.");
+    }
   }
 
-  public String inputNumberTest(String input) {
-    game.validityChecker(input);
-    return input;
-  }
 
   @Test
   @Order(1)
@@ -48,43 +53,44 @@ public class GameTest {
     }).isInstanceOf(IllegalArgumentException.class);
   }
 
-  @Test
+
   @Order(2)
   @DisplayName("유효성 검사 2. 유효한 값 1 확인")
-  public void generateNumberValidateIsStringOne() {
-    game.generateNumberTest("1");
-    assertEquals("1", outContent.toString());
+  @ParameterizedTest(name ="{displayName}) {index} = {0} ")
+  @ValueSource(strings = {"1", "2"})
+  public void generateNumberValidateIsStringOne(String inputString) {
+    InputStream in = generateUserInput(inputString);
+    System.setIn(in);
+    Scanner scanner = new Scanner(System.in);
+    assertThatThrownBy(() -> generateNumberForTest(scanner.toString()))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("1, 또는 2를 입력해야 합니다.");
   }
 
-  @Test
   @Order(3)
-  @DisplayName("유효성 검사 3. 유효한 값 2 확인")
-  public void generateNumberValidateIsStringTwo() {
-    game.generateNumberTest("2");
-    assertEquals("2", outContent.toString());
+  @DisplayName("유효성 검사 3. 유효하지 않은 값 확인")
+  @ParameterizedTest(name ="{displayName}) {index} = {0} ")
+  @ValueSource(strings = {"3", "4", "0"})
+  public void generateNumberValidateIsStringUnavailable(String inputString) {
+    InputStream in = generateUserInput(inputString);
+    System.setIn(in);
+    Scanner scanner = new Scanner(System.in);
+    assertThatThrownBy(() -> generateNumberForTest(scanner.toString()))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("1, 또는 2를 입력해야 합니다.");
   }
 
-  @Test
   @Order(4)
-  @DisplayName("유효성 검사 4. 유효하지 않은 값 확인")
-  public void generateNumberValidateIsStringUnavailable() {
-    Assertions.assertThatThrownBy(() -> {
-      game.generateNumberTest("3");
-    }).isInstanceOf(IllegalArgumentException.class);
-  }
+  @DisplayName("유효성 검사 4. 기타 값 확인")
+  @ParameterizedTest(name ="{displayName}) {index} = {0} ")
+  @ValueSource(strings = {"a", "ㄱ" , "5", "6", "50", "hello"})
+  public void generateNumberValidateRandomTest(String inputString) {
+    InputStream in = generateUserInput(inputString);
+    System.setIn(in);
+    Scanner scanner = new Scanner(System.in);
+    assertThatThrownBy(() -> generateNumberForTest(scanner.toString()))
+        .isInstanceOf(IllegalArgumentException.class)
+        .hasMessage("1, 또는 2를 입력해야 합니다.");
 
-  @Test
-  @Order(5)
-  @DisplayName("유효성 검사 5. 랜덤 값 확인")
-  public void generateNumberValidateRandomTest() {
-    String number = Integer.toString(Randoms.pickNumberInRange(0, 9));
-    if (number.equals("1") || number.equals("2")) {
-      game.generateNumberTest(number);
-      assertEquals(number, outContent.toString());
-    } else {
-      Assertions.assertThatThrownBy(() -> {
-        game.generateNumberTest(number);
-      }).isInstanceOf(IllegalArgumentException.class);
-    }
   }
 }
