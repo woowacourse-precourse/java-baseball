4a2bf1467d6d31d3a91d86f4e63fa77d639db497
Identityless <rlandwnd555@naver.com>
2022-11-08 02:04:32 +0900

    feat : 입력 기능의 Inputer 클래스 추가

---
+++ src/main/java/util/Inputer.java
@@ -0,0 +1,18 @@
+package util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inputer {
+
+    private boolean flag;
+
+    public Inputer() {
+        this.flag = false;
+    }
+
+
+
+}
