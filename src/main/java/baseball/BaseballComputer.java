cf9056bff6ee5c599de9cc3b5c9d0f482901eb7b
jijongkwon <help3451@naver.com>
2022-11-07 18:03:02 +0900

    feat : 사용자 입력 기능 추가

--- src/main/java/baseball/BaseballComputer.java
+++ src/main/java/baseball/BaseballComputer.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -7,6 +8,7 @@ import java.util.List;
 
 public class BaseballComputer {
     private List<Integer> computerNumber = new ArrayList<>(); // 3자리수
+    private List<Integer> userNumber = new ArrayList<>();
 
     public void startGameMessage(){
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -14,6 +16,14 @@ public class BaseballComputer {
 
     public void endGameMessage(){
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+    }
+
+    public void inputNumberToComputerMessage(){
+        System.out.print("숫자를 입력해 주세요: ");
+    }
+
+    public String userInputNumberInGame(){
+        return Console.readLine();
     }
 
 
