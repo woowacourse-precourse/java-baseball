b9d9ceebec4ee90834bc45f602498c41a63fa59b
MIN-04 <kimji_0424@naver.com>
2022-11-07 16:32:47 +0900

    style: intent depth를 2로 변경
    추가된 요구사항에 있는 내용에 맞게 설정 변경
    (indent(인덴트, 들여쓰기) depth를 3이 넘지 않도록 구현한다. 2까지만 허용한다.)
    - Tab size : 4 -> 2
    - intent : 4 -> 2
    - Continuation indent : 8 -> 4

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,91 +10,92 @@ import java.util.stream.Collectors;
 
 public class Application {
 
-    static List<Integer> computer;
-    public static void main(String[] args) {
+  static List<Integer> computer;
 
+  public static void main(String[] args) {
+
+    computer = createComputerNumber();
+
+    System.out.println("숫자 야구 게임을 시작합니다.");
+
+    int gameFlag = 0; // gameFlag = 0: 처음, 1: 재시작, 2: 종료
+    while (gameFlag != 2) {
+      int strike = 0;
+      int ball = 0;
+
+      System.out.print("숫자를 입력해주세요 : ");
+      String inputNumber = Console.readLine();
+      validateInputNumber(inputNumber);
+
+      List<String> inputNumberList = Arrays.asList(inputNumber.split(""));
+      List<Integer> player = inputNumberList.stream()
+          .map(Integer::parseInt)
+          .collect(Collectors.toList());
+
+      for (int i = 0; i < 3; i++) {
+        if (player.get(i).equals(computer.get(i))) {
+          strike += 1;
+        }
+        if (computer.contains(player.get(i)) && !player.get(i).equals(computer.get(i))) {
+          ball += 1;
+        }
+      }
+
+      boolean correctFlag = checkPlayerResult(ball, strike);
+      gameFlag = checkRerunGame(correctFlag, gameFlag);
+    }
+  }
+
+  private static List<Integer> createComputerNumber() {
+    List<Integer> computer = new ArrayList<>();
+    while (computer.size() < 3) {
+      int randomNumber = Randoms.pickNumberInRange(1, 9);
+      if (!computer.contains(randomNumber)) {
+        computer.add(randomNumber);
+      }
+    }
+    return computer;
+  }
+
+  private static void validateInputNumber(String inputNumber) {
+    if (inputNumber.length() != 3 || !inputNumber.matches("^[1-9]+$")) {
+      throw new IllegalArgumentException();
+    }
+  }
+
+  private static boolean checkPlayerResult(int ball, int strike) {
+    boolean correctFlag = false;
+    if (ball != 0 && strike != 0) {
+      System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+    } else if (ball != 0) {
+      System.out.printf("%d볼\n", ball);
+    } else if (strike != 0 && strike != 3) {
+      System.out.printf("%d스트라이크\n", strike);
+    } else if (strike == 3) {
+      System.out.printf("%d스트라이크\n", strike);
+      System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+      correctFlag = true;
+    } else {
+      System.out.println("낫싱");
+    }
+    return correctFlag;
+  }
+
+  private static int checkRerunGame(boolean correctFlag, int gameFlag) {
+    if (correctFlag) {
+      System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+      String inputNumber = Console.readLine();
+
+      if (inputNumber.equals("1")) {
         computer = createComputerNumber();
-
-        System.out.println("숫자 야구 게임을 시작합니다.");
-
-        int gameFlag = 0; // gameFlag = 0: 처음, 1: 재시작, 2: 종료
-        while (gameFlag != 2) {
-            int strike = 0;
-            int ball = 0;
-
-            System.out.print("숫자를 입력해주세요 : ");
-            String inputNumber = Console.readLine();
-            validateInputNumber(inputNumber);
-
-            List<String> inputNumberList = Arrays.asList(inputNumber.split(""));
-            List<Integer> player = inputNumberList.stream()
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
-
-            for (int i = 0; i < 3; i++) {
-                if (player.get(i).equals(computer.get(i))) {
-                    strike += 1;
-                }
-                if (computer.contains(player.get(i)) && !player.get(i).equals(computer.get(i))) {
-                    ball += 1;
-                }
-            }
-
-            boolean correctFlag = checkPlayerResult(ball, strike);
-            gameFlag = checkRerunGame(correctFlag, gameFlag);
-        }
+        gameFlag = 1;
+      } else if (inputNumber.equals("2")) {
+        gameFlag = 2;
+      } else {
+        throw new IllegalArgumentException();
+      }
     }
-
-    private static List<Integer> createComputerNumber() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-        return computer;
-    }
-
-    private static void validateInputNumber(String inputNumber) {
-        if (inputNumber.length() != 3 || !inputNumber.matches("^[1-9]+$")) {
-            throw new IllegalArgumentException();
-        }
-    }
-
-    private static boolean checkPlayerResult(int ball, int strike) {
-        boolean correctFlag = false;
-        if (ball != 0 && strike != 0) {
-            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
-        } else if (ball != 0) {
-            System.out.printf("%d볼\n", ball);
-        } else if (strike != 0 && strike != 3) {
-            System.out.printf("%d스트라이크\n", strike);
-        } else if (strike == 3) {
-            System.out.printf("%d스트라이크\n", strike);
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            correctFlag = true;
-        } else {
-            System.out.println("낫싱");
-        }
-        return correctFlag;
-    }
-
-    private static int checkRerunGame(boolean correctFlag, int gameFlag) {
-        if (correctFlag) {
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String inputNumber = Console.readLine();
-
-            if (inputNumber.equals("1")) {
-                computer = createComputerNumber();
-                gameFlag = 1;
-            } else if (inputNumber.equals("2")) {
-                gameFlag = 2;
-            } else {
-                throw new IllegalArgumentException();
-            }
-        }
-        return gameFlag;
-    }
+    return gameFlag;
+  }
 
 }
