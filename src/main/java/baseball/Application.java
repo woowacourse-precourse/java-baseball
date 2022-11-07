23285b4daf5cc6b16ebe9dca4c0a8b34477bd715
poter0501 <poter0501@gmail.com>
2022-11-07 19:03:46 +0900

    feat: 서로 다른 세개의 정수를 랜덤으로 생성하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-//        printStartText("숫자 야구 게임을 시작합니다.");
     }
     private static void printStartText(String txt){
         System.out.println(txt);
+    }
+    private static List<Integer> createRandomNumber(){
+        List<Integer> computer = new ArrayList<>();ß
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
