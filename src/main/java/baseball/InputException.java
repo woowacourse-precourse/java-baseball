f18468b2d431bfc538b7aef8f07ebce05d10d3cc
KTH-96 <taehyun960@gmail.com>
2022-11-08 00:30:03 +0900

    feat(InputException): 유저가 입력형식 잘못시  예외발생 기능구현

---
+++ src/main/java/baseball/InputException.java
@@ -0,0 +1,39 @@
+package baseball;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+public class InputException {
+
+    public static String validUserNumberException(String userNumber) {
+        if (isThreeLength(userNumber) || isInteger(userNumber)
+                || isIncludeZero(userNumber) || isDuplicationNumber(userNumber)) {
+            throw new IllegalArgumentException();
+        }
+        return userNumber;
+    }
+
+    private static boolean isDuplicationNumber(String userNumber) {
+        Set<Character> set = new HashSet<>();
+        IntStream.range(0, 3)
+                .forEach(i -> set.add(userNumber.charAt(i)));
+        return 3 != set.size();
+    }
+
+    private static boolean isIncludeZero(String userNumber) {
+        return 0 < (int) IntStream.range(0, 3)
+                .filter(i -> userNumber.charAt(i) == '0')
+                .count();
+    }
+
+    private static boolean isInteger(String userNumber) {
+        return 0 < (int) IntStream.range(0, 3)
+                .filter(i -> !Character.isDigit(userNumber.charAt(i)))
+                .count();
+    }
+
+    private static boolean isThreeLength(String userNumber) {
+        return userNumber.length() != 3;
+    }
+}
--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -6,7 +6,7 @@ public class InputView {
 
     public static String selectUserNumberInput() {
         System.out.print("숫자를 입력해주세요: ");
-        return Console.readLine();
+        return InputException.validUserNumberException(Console.readLine());
     }
 
     public static boolean selectEndOrRestartInput() {
