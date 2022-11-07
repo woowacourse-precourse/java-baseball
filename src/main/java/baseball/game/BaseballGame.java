7ef66a02cb61048a5add24d30bde31cb7a8f5a1f
inhyeok <ss112d86@gmail.com>
2022-11-08 02:03:37 +0900

    feat: ball 카운팅 메소드 구현

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -12,22 +12,32 @@ public class BaseballGame {
   }
 
 
-  public GameResult start(GameInputNumber gameInputNumber){
+  public GameResult start(GameInputNumber gameInputNumber) {
 
     List<Integer> userNumbers = gameInputNumber.getNumbers();
-    int strikeCount =  countStrike(userNumbers);
-
-    return new GameResult(strikeCount, 0);
+    int strikeCount = countStrike(userNumbers);
+    int ballCount = countBall(userNumbers, strikeCount);
+    return new GameResult(strikeCount, ballCount);
   }
 
-  private int countStrike(List<Integer> userNumbers){
-   int count = 0 ;
-   for(int i = 0; i < 3; i++){
-     if(computerNumbers.get(i).equals(userNumbers.get(i))){
-       count++;
-     }
-   }
-   return count;
+  private int countStrike(List<Integer> userNumbers) {
+    int count = 0;
+    for (int i = 0; i < computerNumbers.size(); i++) {
+      if (computerNumbers.get(i).equals(userNumbers.get(i))) {
+        count++;
+      }
+    }
+    return count;
   }
 
+  private int countBall(List<Integer> userNumbers, int strikeNumber) {
+    int count = 0;
+    for (int i = 0; i < computerNumbers.size(); i++) {
+      int userNumber = userNumbers.get(i);
+      if (computerNumbers.contains(userNumber)) {
+        count++;
+      }
+    }
+    return count - strikeNumber;
+  }
 }
--- src/test/java/baseball/game/BaseballGameTest.java
+++ src/test/java/baseball/game/BaseballGameTest.java
@@ -32,4 +32,16 @@ class BaseballGameTest {
     GameResult result = baseballGame.start(userInput);
     assertThat(result.getStrike()).isEqualTo(0);
   }
+
+  @Test
+  void 스트라이크_볼_결과_확인(){
+    List<Integer> computerNumber = List.of(1,2,3);
+    GameInputNumber userInput = new GameInputNumber("321");
+
+    BaseballGame baseballGame = new BaseballGame(computerNumber);
+    GameResult result = baseballGame.start(userInput);
+
+    assertThat(result.getStrike()).isEqualTo(1);
+    assertThat(result.getBall()).isEqualTo(2);
+  }
 }
