ece64dc81842640dc57a1988cb074807acfad5b2
leesangjin <tkdwls0628@gmail.com>
2022-11-08 01:02:21 +0900

    feat(BaseballController): 힌트 출력 기능 구현

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,11 +1,22 @@
 package baseball.controller;
 
 import baseball.domain.Computer;
+import baseball.view.OutputView;
+import java.util.List;
 
 public class BaseballController {
     private Computer computer = new Computer();
 
     private boolean isRightAnswer() {
         return computer.isAnswer();
+    }
+
+    private void printResultOf(List<Integer> randomNumbers, List<Integer> playerInputs) {
+        String hintResult = hintResult(randomNumbers, playerInputs);
+        OutputView.printHintResult(hintResult);
+    }
+
+    private String hintResult(List<Integer> randomNumbers, List<Integer> playerInputs) {
+        return computer.resultOfGame(playerInputs, randomNumbers);
     }
 }
