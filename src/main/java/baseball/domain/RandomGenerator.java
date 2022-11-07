ea4a0dc34b764127c8feaf3fb3dc9566111d3ece
This2sho <zx00018@naver.com>
2022-11-08 04:12:09 +0900

    feat(RandomGenerator): 서로 다른 3개의 난수 생성 기능 구현

--- src/main/java/baseball/domain/RandomGenerator.java
+++ src/main/java/baseball/domain/RandomGenerator.java
@@ -1,13 +1,32 @@
 package baseball.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.LinkedList;
+import java.util.List;
 
 public class RandomGenerator {
 
 	private static final int MAX_NUMBER = 9;
 	private static final int MIN_NUMBER = 1;
+	public static final int NUMBER_SIZE = 3;
 
-	public int generateNumber() {
+	public List<Integer> generateNumbers() {
+		List<Integer> numbers = new LinkedList<>();
+		return generateNumbers(numbers);
+	}
+
+	private List<Integer> generateNumbers(List<Integer> numbers) {
+		if (numbers.size() == NUMBER_SIZE) {
+			return numbers;
+		}
+		int number = generateNumber();
+		if (!numbers.contains(number)) {
+			numbers.add(number);
+		}
+		return generateNumbers(numbers);
+	}
+
+	int generateNumber() {
 		return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
 	}
 }
