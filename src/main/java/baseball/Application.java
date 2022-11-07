8ee0f271dc1794e19c6381c363a0c1852add667b
Woogie95 <choisw950928@gmail.com>
2022-11-07 16:08:25 +0900

    feat: 예외 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        try {
+            BaseballManagement baseballManagement = new BaseballManagement();
+            baseballManagement.proceedBaseball();
+        } catch (IllegalArgumentException exceptionMessage) {
+            System.out.println(exceptionMessage.getMessage());
+        } catch (Exception exceptionMessage) {
+            System.out.println("예기치 못한 에러가 발생하였습니다.");
+        }
     }
+
 }
