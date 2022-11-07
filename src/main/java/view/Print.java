d9d6ac86089dadb2a7f9a6219b264ba45d090271
wonwoo6y <wonwoo6y@gmail.com>
2022-11-07 18:20:59 +0900

    feat: 7. 비교 결과 문구 출력 기능 추가

--- src/main/java/view/Print.java
+++ src/main/java/view/Print.java
@@ -11,4 +11,36 @@ public class Print {
     public static void inputNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
     }
+
+    public static boolean comparisonResult(HashMap<String, Integer> result) {
+        Integer ballCount = result.get("ball");
+        Integer strikeCount = result.get("strike");
+        if (ballCount == 0 && strikeCount == 0) {
+            System.out.println("낫싱\n");
+            return false;
+        }
+
+        String outputStatement = createOutputStatement(ballCount, strikeCount);
+        System.out.println(outputStatement);
+        if (outputStatement.equals("3스트라이크")) {
+            return true;
+        }
+        return false;
+    }
+
+    private static String createOutputStatement(Integer ballCount, Integer strikeCount) {
+        String outputStatement = "";
+        if (ballCount > 0) {
+            outputStatement += String.valueOf(ballCount);
+            outputStatement += "볼";
+        }
+        if (ballCount > 0 && strikeCount > 0) {
+            outputStatement += " ";
+        }
+        if (strikeCount > 0) {
+            outputStatement += String.valueOf(strikeCount);
+            outputStatement += "스트라이크";
+        }
+        return outputStatement;
+    }
 }
