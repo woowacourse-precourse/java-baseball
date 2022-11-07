8a3511eecf1dccfa0a28935b3668a7f7bc063853
jaehyeon <jhjh0022@naver.com>
2022-11-07 21:46:11 +0900

    refactor: 메서드 이름 명확하게 하기 -> verifyNumberBetweenOneAndNine

--- src/main/java/baseball/Input/InputNumberVerification.java
+++ src/main/java/baseball/Input/InputNumberVerification.java
@@ -1,48 +1,46 @@
 package baseball.Input;
 
-import baseball.tool.BeforeVerificationSetting;
-
-import static baseball.tool.BeforeVerificationSetting.*;
+import static baseball.tool.NumberSplitTool.*;
 
 public class InputNumberVerification {
 
-    public static boolean verifyNumber (int receivedNumber){
+    public static boolean verifyNumber(int receivedNumber) {
 
-        if(!verifyNumberOverlap(receivedNumber)){
+        if (!verifyNumberOverlap(receivedNumber)) {
             throw new IllegalArgumentException("각자 다른 숫자를 입력해주세요");
         }
 
-        if(!verifyNumberOneToNine(receivedNumber)){
+        if (!verifyNumberBetweenOneAndNine(receivedNumber)) {
             throw new IllegalArgumentException("1~9 사이의 숫자만 입력해주세요");
         }
 
-        if(!verifyNumberThreeDigit(receivedNumber)){
+        if (!verifyNumberThreeDigit(receivedNumber)) {
             throw new IllegalArgumentException("3개의 숫자만 입력해주세요");
         }
 
         return true;
     }
 
-    private static boolean verifyNumberOverlap(int receivedNumber){
+    private static boolean verifyNumberOverlap(int receivedNumber) {
         return makeNumberSplitList(receivedNumber).size()
                 == makeNumberSplitSet(receivedNumber).size();
     }
 
-    private static boolean verifyNumberOneToNine(int receivedNumber){
-        int cnt = 0;
-        for(int oneNumber : makeNumberSplitList(receivedNumber)){
-            if(!(1 <= oneNumber  && oneNumber <=9)){
+    private static boolean verifyNumberBetweenOneAndNine(int receivedNumber) {
+
+        for (int oneNumber : makeNumberSplitList(receivedNumber)) {
+            if (!(1 <= oneNumber && oneNumber <= 9)) {
                 return false;
             }
         }
         return true;
     }
 
-    private static boolean verifyNumberThreeDigit(int receivedNumber){
+    private static boolean verifyNumberThreeDigit(int receivedNumber) {
         return makeNumberSplitList(receivedNumber).size() == 3;
     }
 
-    public static boolean verifyOneOrTwoButton(int receivedButtonNumber){
+    public static boolean verifyOneOrTwoButton(int receivedButtonNumber) {
         return receivedButtonNumber == 1 || receivedButtonNumber == 2;
     }
 }
