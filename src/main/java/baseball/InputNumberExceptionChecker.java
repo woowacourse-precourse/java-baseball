72d1a75f441763d2e4cc95e1724a80676f30fac1
dener8 <8dener@gmail.com>
2022-11-08 04:36:11 +0900

    feat: 잘못된 입력이 들어왔을 때의 예외처리를 맡는 InputNumberExceptionChecker 클래스 구현
    구현해야하는 메서드의 기능만 주석으로 적어놓은 상태
    하나씩 구현할 계획

---
+++ src/main/java/baseball/InputNumberExceptionChecker.java
@@ -0,0 +1,11 @@
+package baseball;
+
+public class InputNumberExceptionChecker {
+    // 3글자가 아닐 때
+
+    // 모두 다른 수가 아닐 때
+
+    // 재시작 여부를 물었을 때, 1과 2가 아닌 다른 숫자를 입력했을 때
+
+    // 위의 세 개 중 하나라도 만족하지 않음을 확인하는 메서드 (총평?)
+}
