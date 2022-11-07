69ae86a2ca52dcbbb3c0f942114bef1f4f34a014
nayooniverse <kangna99@naver.com>
2022-11-08 08:53:58 +0900

    feat: Game 클래스 및 생성자 추가

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import static baseball.Constant.*;
+
+public class Game {
+    Computer computer;
+    Player player;
+
+    public Game() {
+        computer = new Computer();
+        player = new Player();
+    }
+}
