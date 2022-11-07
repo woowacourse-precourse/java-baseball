4862d583171401fc9d885a799369c2fc8d98c5b4
LeeHaiLim <hailim1997@gmail.com>
2022-11-08 04:36:24 +0900

    feat(Game) : implement method play
    게임을 작동시키는 기능 구현
    - 게임 시작 메시지 GAME_START_MESSAGE 상수 선언
    - 게임 종료 메시지 GAME_END_MESSAGE 상수 선언
    - play() 메소드 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,25 @@
 package baseball;
 
 public class Game {
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_END_MESSAGE =
+            String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 종료", Constants.NUMBER_COUNT);
+
+    public void play() {
+        Computer computer = new Computer();
+        Player player = new Player();
+
+        int score;
+
+        System.out.println(GAME_START_MESSAGE);
+        do {
+            computer.setAnswer();
+
+            do {
+                score = computer.computeResult(player.getNumberInput());
+            } while (score < Constants.NUMBER_COUNT);
+
+            System.out.println(GAME_END_MESSAGE);
+        } while (player.getRestartOrQuitInput());
+    }
 }
