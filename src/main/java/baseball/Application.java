1fa79a996177f53b645524c892a48bbc8000ef00
JUNBEOM_LEE <bom1215@naver.com>
2022-11-07 15:01:20 +0900

    feat(Class Application): Game Start
    Add Start method to Class Application
    
    Breaking Change: Make Start method to start the game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
 public class Application {
+
+    public static void Start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        Generate_Answer();
+        Input();
+    }
     public static void main(String[] args) {
+
         // TODO: 프로그램 구현
     }
 }
