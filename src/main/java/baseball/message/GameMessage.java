82c765dad9118aa19ed897faee1a8fcafb057372
HiiWee <wpdlzhf159@gmail.com>
2022-11-08 03:07:06 +0900

    Style: 숫자 입력 메시지 띄어쓰기 추가

--- src/main/java/baseball/message/GameMessage.java
+++ src/main/java/baseball/message/GameMessage.java
@@ -2,7 +2,7 @@ package baseball.message;
 
 public interface GameMessage {
     String START_GAME = "숫자 야구 게임을 시작합니다.";
-    String NUMBER_INPUT = "숫자를 입력해주세요 :";
+    String NUMBER_INPUT = "숫자를 입력해주세요 : ";
     String WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     String NEW_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     String STRIKE = "스트라이크";
