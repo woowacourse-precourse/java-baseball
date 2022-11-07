0634d1264e21e3b61130e3f17cae3bddd60d5bb7
anso33 <hyeon131812@gmail.com>
2022-11-07 15:20:04 +0900

    [FIX](Application): fix utils Scanner to readLine()

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import java.util.Scanner;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            Game game = new Game();
+            if (game.routine()) {
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                int num = Integer.parseInt(readLine());
+                if (num == 1)
+                    continue;
+                else if (num == 2)
+                    break;
+            }
+        }
     }
 }
