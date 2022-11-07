77fe9b3cb669aa4c7c143ef78ccf59d8ff25b084
yoonjuho <dbswn990@naver.com>
2022-11-08 06:38:33 +0900

    test($TestCase) : 접근 제한자 수정으로 테스트 삭제

--- src/test/java/study/TestCase.java
+++ src/test/java/study/TestCase.java
@@ -1,12 +1,10 @@
 package study;
 
-import baseball.Computer;
 import baseball.ComputerNum;
 import baseball.Number;
 import baseball.PlayerNum;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
 import java.util.Arrays;
 import java.util.List;
 
@@ -63,20 +61,21 @@ public class TestCase {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void 플레이어의_입력이_숫자로만_구성되어_있지_않다면_예외_발생() {
-        PlayerNum playerNumber = new PlayerNum();
-        assertThatThrownBy(() -> playerNumber.verifyInput("안녕3"))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+    /*
+        @Test
+        void 플레이어의_입력이_숫자로만_구성되어_있지_않다면_예외_발생() {
+            PlayerNum playerNumber = new PlayerNum();
+            assertThatThrownBy(() -> playerNumber.verifyInput("안녕3"))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @Test
-    void 플레이어의_입력_문자열을_Integer형_리스트로_변환() {
-        List<Integer> num = Arrays.asList(1, 2, 3);
-        PlayerNum playerNumber = new PlayerNum();
-        assertThat(num).isEqualTo(playerNumber.transformInput("123"));
-    }
-
+        @Test
+        void 플레이어의_입력_문자열을_Integer형_리스트로_변환() {
+            List<Integer> num = Arrays.asList(1, 2, 3);
+            PlayerNum playerNumber = new PlayerNum();
+            assertThat(num).isEqualTo(playerNumber.convertInput("123"));
+        }
+    */
     @Test
     void 플레이어의_숫자가_1부터_9까지가_아닌_숫자가_들어왔을때_예외_발생() {
         List<Integer> num = Arrays.asList(10, 2, 5);
@@ -101,7 +100,7 @@ public class TestCase {
         assertThatThrownBy(() -> playerNumber.verifyNumber(num))
                 .isInstanceOf(IllegalArgumentException.class);
     }
-
+/*
     @Test
     void 재시작이나_게임종료에_해당하지_않는_입력이_들어왔을때_예외_발생() {
         String input = "3";
@@ -110,7 +109,8 @@ public class TestCase {
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-/*
+
+
     @Test
     void 컴퓨터가_플레이어에게_받은_수와_자신의_수를_비교_결과출력() {
         String result = "3스트라이크";
@@ -122,13 +122,6 @@ public class TestCase {
         assertThat(result).isEqualTo(computer.getResult(playerNumber,computerNumber));
     }
 */
-
-
-
-
-
-
-
 
 
 }
