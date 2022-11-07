f8b58fb5146cd8ff3d25495b001bcebe69c1b0fc
이건회 <rg970604@naver.com>
2022-11-08 04:59:41 +0900

    style(EndRule.java) : 매직리터럴 상수선언

--- src/main/java/baseball/rule/EndRule.java
+++ src/main/java/baseball/rule/EndRule.java
@@ -4,11 +4,14 @@ import baseball.domain.BallCount;
 
 public class EndRule {
 
+    private static final String THREE_STRIKE = "3스트라이크";
+    private static final String ONLY_INPUT_1_OR_2 = "1과 2만 입력 가능합니다";
+
     private static final String RESTART_NUMBER = "1";
     private static final String END_NUMBER = "2";
 
     public static boolean isGameEnd(BallCount ballCount) {
-        return ballCount.ballCountResult().equals("3스트라이크");
+        return ballCount.ballCountResult().equals(THREE_STRIKE);
     }
 
     public static boolean isProgramEnd(String userInput) {
@@ -18,7 +21,7 @@ public class EndRule {
 
     private static void validateWrongNum(String userInput) {
         if (!userInput.equals(RESTART_NUMBER) && !userInput.equals(END_NUMBER)) {
-            throw new IllegalArgumentException("1과 2만 입력 가능합니다");
+            throw new IllegalArgumentException(ONLY_INPUT_1_OR_2);
         }
     }
 
