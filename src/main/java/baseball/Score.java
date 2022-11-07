a9e57574eb8543847d835200436bd637d089a621
Euihyun Lee <104973468+euihyunlee@users.noreply.github.com>
2022-11-08 03:46:05 +0900

    feat(Score): define print method

--- src/main/java/baseball/Score.java
+++ src/main/java/baseball/Score.java
@@ -30,4 +30,21 @@ enum Score {
             ball++;
         }
     }
+
+    void print() {
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+        if (ball != 0) {
+            System.out.print(ball + "볼");
+            if (strike != 0) {
+                System.out.print(" ");
+            }
+        }
+        if (strike != 0) {
+            System.out.print(strike + "스트라이크");
+        }
+        System.out.println();
+    }
 }
