a400498250c96a2ef9697041e5d8e9b9472d4b32
sonsoowon <yuwoo1018@gmail.com>
2022-11-08 05:46:46 +0900

    feat(Player): 입력과 게임 정답의 자리수 비교
    - Player 클래스 추가
    - isInputSizeEqualToAnswerSize 메서드 추가

---
+++ src/main/java/baseball/Player.java
@@ -0,0 +1,8 @@
+package baseball;
+
+public class Player {
+    static boolean isInputSizeEqualToAnswerSize(String playerInput){
+        return playerInput.length() == Game.ANSWER_SIZE;
+    }
+
+}
