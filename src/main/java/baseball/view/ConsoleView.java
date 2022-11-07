f491c1b92a9bd03888592283a13004d91805efed
Kloong1 <dkdlsmsm98@gmail.com>
2022-11-07 19:39:51 +0900

    feat: 게임 안내 메시지 상수 & 해당 메시지를 출력하는 메소드 추가
    게임 시작, 입력, 정답, 재시작 메시지 상수를 추가함.
    그리고 각 메시지를 콘솔에 출력하는 4개의 메소드를 추가함.

--- src/main/java/baseball/view/ConsoleView.java
+++ src/main/java/baseball/view/ConsoleView.java
@@ -1,4 +1,24 @@
 package baseball.view;
 
 public class ConsoleView {
+
+    private final static String WELCOME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private final static String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private final static String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private final static String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void printWelcomeMessage() {
+        System.out.println(WELCOME_MESSAGE);
+    }
+    public void printInputMessage() {
+        System.out.print(INPUT_MESSAGE);
+    }
+
+    public void printSuccessMessage() {
+        System.out.print(SUCCESS_MESSAGE);
+    }
+
+    public void printRestartMessage() {
+        System.out.println(RESTART_MESSAGE);
+    }
 }
