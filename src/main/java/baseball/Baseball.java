c2f5237cf27d4888bec7a29997b344f9984c7d8c
zaysverse <zeze@jbnu.ac.kr>
2022-11-08 00:19:48 +0900

    Fix : println -> print
    printResult()함수에서 볼과 스트라이크가 나뉘어서 출력하는 버그 수정

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -50,11 +50,12 @@ public class Baseball {
 
     private void printResult() {
         if (ball > 0)
-            System.out.println(ball + "볼 ");
+            System.out.print(ball + "볼 ");
         if (strike > 0)
-            System.out.println(strike + "스트라이크 ");
+            System.out.print(strike + "스트라이크 ");
         if (ball == 0 && strike == 0)
-            System.out.println("낫싱");
+            System.out.print("낫싱");
+        System.out.println();
         if (strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
