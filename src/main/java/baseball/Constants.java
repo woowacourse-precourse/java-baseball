816679d7413a9330f67214e8a134d11f47cdeeb9
SEONGJOON KIM <jobcho6320@naver.com>
2022-11-08 07:05:25 +0900

    feat: 시스템 출력 메시지 클래스 분리
    메세지 출력 메소드 구현

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -6,10 +6,6 @@ public class Constants {
     public static final int MAX_NUMBER = 9;
     public static final int ZERO = 0;
     public static final int INT_TO_CHAR_CONVERSION_CONSTANT = 48;
-    public static final String GAME_START_MSG = "숫자 야구 게임을 시작합니다.";
-    public static final String NUMBER_INPUT_MSG = "숫자를 입력해 주세요 : ";
-    public static final String GAME_END_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    public static final String REGAME_SUGGESTION_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
     public static final String ILLEGAL_ERR_MSG = "ILLEGAL_INPUT";
     public static final String RANGE_ERR_MSG = "OUT_OF_RANGE";
     public static final String CONTAIN_ZERO_ERR_MSG = "ZERO_IS_INCLUDED";
