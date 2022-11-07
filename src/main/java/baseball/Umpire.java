c0171327e78f28acfc2fd86a3b2827ecd94c8e14
YooSuYeon <usuyn000@gmail.com>
2022-11-08 06:08:20 +0900

    feat: 힌트 저장 hashmap 생성 및 리셋

--- src/main/java/baseball/Umpire.java
+++ src/main/java/baseball/Umpire.java
@@ -1,11 +1,19 @@
 package baseball;
 
 import constants.Rule;
+import constants.Text;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Umpire {
     private final Rule hint = Rule.HINT;
+    private Map<Text, Integer> hintRecord;
+
+    public Umpire() {
+        hintRecord = new HashMap<>();
+    }
 
     private boolean isNothing(List<Integer> computerNumber, List<Integer> playerNumber) {
         return ((int) playerNumber.stream()
@@ -13,4 +21,8 @@ public class Umpire {
                 .count()) == hint.getMin();
     }
 
+    private void resetHintRecord() {
+        hintRecord.put(Text.BALL, hint.getMin());
+        hintRecord.put(Text.STRIKE, hint.getMin());
+    }
 }
