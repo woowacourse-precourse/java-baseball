8e24041ea66621281659a46d9d5dae7061af439e
GraphTree <graphnodesed@gmail.com>
2022-11-07 16:31:31 +0900

    test : 정답과 유저 인풋 비교 테스트 방식 변경

--- src/test/java/baseball/functionTest.java
+++ src/test/java/baseball/functionTest.java
@@ -55,13 +55,13 @@ public class functionTest {
         List<Integer> input6 = List.of(0, 2, 4);// 1스트라이크
         List<Integer> input7 = List.of(3, 1, 2);// 3볼
 
-        assertThat(Application.compareAnswerToInput(answer, input1)).isEqualTo("3스트라이크");
-        assertThat(Application.compareAnswerToInput(answer, input2)).isEqualTo("낫싱");
-        assertThat(Application.compareAnswerToInput(answer, input3)).isEqualTo("2볼 1스트라이크");
-        assertThat(Application.compareAnswerToInput(answer, input4)).isEqualTo("1볼 1스트라이크");
-        assertThat(Application.compareAnswerToInput(answer, input5)).isEqualTo("2볼");
-        assertThat(Application.compareAnswerToInput(answer, input6)).isEqualTo("1스트라이크");
-        assertThat(Application.compareAnswerToInput(answer, input7)).isEqualTo("3볼");
+        assertThat(Application.compareAnswerToInput(answer, input1)).containsExactly(0,3);
+        assertThat(Application.compareAnswerToInput(answer, input2)).containsExactly(0,0);
+        assertThat(Application.compareAnswerToInput(answer, input3)).containsExactly(2,1);
+        assertThat(Application.compareAnswerToInput(answer, input4)).containsExactly(1,1);
+        assertThat(Application.compareAnswerToInput(answer, input5)).containsExactly(2);
+        assertThat(Application.compareAnswerToInput(answer, input6)).containsExactly(0,1);
+        assertThat(Application.compareAnswerToInput(answer, input7)).containsExactly(3,0);
 
 
     }
