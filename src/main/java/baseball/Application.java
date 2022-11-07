6ff10ab76ebec69ae59507202af9586c6654248f
MJ Choi <1621139@gmail.com>
2022-11-07 17:23:43 +0900

    feat main

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,21 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean is3Strike = false;
+        boolean isDone = false;
+
+        ComputerBallNumber computerBallNumber = new ComputerBallNumber();
+        computerBallNumber.getComputerBalls();
+        ResultView.printStartGame();
+        while (!is3Strike && !isDone) {
+            String inputBallNumber = InputView.inputBallNumber();
+            Balls balls = new Balls(inputBallNumber);
+            UserBallNumber userBallNumber = new UserBallNumber(balls.numbers);
+            CompareBallNumber compareBallNumber = new CompareBallNumber();
+            ResultView.printResult(compareBallNumber.checkMatchBalls(userBallNumber.getUserBallNumber(), computerBallNumber.numbers));
+            if (ResultView.determineStopOrNot(computerBallNumber)) {
+                break;
+            }
+        }
     }
 }
