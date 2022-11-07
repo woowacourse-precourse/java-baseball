25df777a4a974f95b1055c3dd2ca433c6112db25
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2022-11-07 19:22:15 +0900

    refactor: Computer - setRandomNumber 메서드 수정
    메서드를 간단하게 만듦

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,25 +1,18 @@
 package baseball.domain;
 
 
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
-    private static List<Integer> Number;
+    private static List<Integer> Number = new ArrayList<>();
 
     public List<Integer> getRandomNumber() {
         return Number;
     }
 
-    public void setRandomNumber() {
-        Number = new ArrayList<>();
-        while (Number.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!Number.contains(randomNumber)) {
-                Number.add(randomNumber);
-            }
-        }
+    public void setRandomNumber(List<Integer> randomNumber) {
+        this.Number = randomNumber;
     }
 }
