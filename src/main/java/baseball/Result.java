873a6b0bbc576c7b2f145364b631b925a4e6e763
devMuscle <hongsbien@naver.com>
2022-11-08 03:37:47 +0900

    feat: 게임 결과 출력

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -5,11 +5,27 @@ public class Result {
     private Integer strike;
     private Integer ball;
 
-    public void setStrike(Integer strike) {
-        this.strike = strike;
+    public void setStrike(Integer strikeCnt) {
+        this.strike = strikeCnt;
     }
 
-    public void setBall(Integer ball) {
-        this.ball = ball;
+    public void setBall(Integer ballCnt) {
+        this.ball = ballCnt;
+    }
+
+    public void printResult() {
+        if(ball>0) {
+            System.out.print(ball + "볼 ");
+        }
+        if(strike>0) {
+            System.out.println(strike + "스트라이크");
+
+            if(strike==3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            }
+        }
+        if(ball==0 && strike==0) {
+            System.out.println("낫싱");
+        }
     }
 }
