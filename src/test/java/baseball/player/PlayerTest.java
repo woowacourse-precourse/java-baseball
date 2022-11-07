f78121a88c60af84860a0f8e8dcde361c0b1c6cc
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-08 06:20:43 +0900

    test(test/PlayerTest.java): 사용자 입력이 리스트로 변환되는지확인하는 Test 구현

--- src/test/java/baseball/player/PlayerTest.java
+++ src/test/java/baseball/player/PlayerTest.java
@@ -3,6 +3,9 @@ package baseball.player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 public class PlayerTest {
@@ -18,5 +21,13 @@ public class PlayerTest {
         String inputString = "sdf";
 
         assertThatThrownBy(() -> player.isInt(inputString)).isInstanceOf(IllegalArgumentException.class).hasMessageContaining("숫자만 입력해주세요.");
+    }
+
+    @Test
+    void 사용자_입력이_리스트로_변환되는지(){
+        String inputString = "123";
+        List<Integer> list = List.of(1,2,3);
+
+        assertThat(player.changeInputToList(inputString)).isEqualTo(list);
     }
 }
