1c761d406342e58f5cf00f45c17d9a638b3f8607
hyeonseog-won <smreo75@gmail.com>
2022-11-08 03:22:51 +0900

    feat: 재시작 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,8 +2,6 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
-
 public class Game {
     private Computer computer;
     private User user;
@@ -40,8 +38,29 @@ public class Game {
         Score score = Score.getResult(computer.getNumbers(), user.getNumbers());
         if (score.checkThreeStrike()) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            return true;
+            return restart();
         }
         return false;
+    }
+
+    private boolean restart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String userChoice = Console.readLine();
+        checkError(userChoice);
+
+        if (userChoice.equals("1")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private void checkError(String restartInput) {
+        if (restartInput.length() != 1) {
+            throw new IllegalArgumentException("1 또는 2 중 하나만 선택하세요.");
+        }
+        if (!restartInput.matches("^[1-2]*$")) {
+            throw new IllegalArgumentException("1 또는 2 중 하나만 선택하세요.");
+        }
     }
 }
