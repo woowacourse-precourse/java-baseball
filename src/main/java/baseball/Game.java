486479a254d16845f90372a652a6db7aabaa9329
SeongukBaek <bsu6235@gmail.com>
2022-11-07 22:26:12 +0900

    refactor: add class Calculator
    Moved methods to class Calculator

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,7 +1,5 @@
 package baseball;
 
-import java.util.Map;
-
 public class Game {
     private static final String START_GAME = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
@@ -66,7 +64,7 @@ public class Game {
                 System.out.print(INPUT_MESSAGE);
                 User.setGameNumber();
 
-                result = Result.getResultByCode(calculateScore(computer, user));
+                result = Result.getResultByCode(Calculator.calculateScore(computer, user));
                 System.out.println(result.getMessage());
             }
         } catch (IllegalArgumentException exception) {
@@ -86,39 +84,5 @@ public class Game {
         } catch (IllegalArgumentException exception) {
             throw new IllegalArgumentException();
         }
-    }
-
-    /*
-     * 컴퓨터와 사용자의 정보를 받아 점수를 계산한다.
-     * 스트라이크 10점, 볼 1점, 낫싱 0점
-     *
-     * @return int
-     * */
-    private static int calculateScore(Computer computer, User user) {
-        Map<Character, Integer> numberIndexMap = computer.getNumberIndexMap();
-        String userGameNumber = user.getGameNumber();
-        int score = 0;
-
-        for (int index = 0; index < userGameNumber.length(); index++) {
-            char currentUserNumber = userGameNumber.charAt(index);
-            if (numberIndexMap.containsKey(currentUserNumber)) {
-                score += computeScoreIndex(numberIndexMap.get(currentUserNumber), index);
-            }
-        }
-
-        return score;
-    }
-
-    /*
-     * 컴퓨터 숫자별 인덱스 정보 Map에서 가져온 numberIndex와 사용자의 현재 숫자를 받아, 점수를 반환
-     * 같다면 10점, 다르다면 1점
-     *
-     * @return int
-     * */
-    private static int computeScoreIndex(int numberIndex, int index) {
-        if (numberIndex == index) {
-            return 10;
-        }
-        return 1;
     }
 }
