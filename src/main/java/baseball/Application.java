b508cc1e935311d2f4052e1e287d592a52d37eab
Fizioo0102 <a27313797@gmail.com>
2022-11-08 00:01:53 +0900

    feat: process() 함수 생성
    - playGame() 함수가 true를 반환하면 사용자에게 재시작 혹은 종료 여부를 묻고 입력을 받는다.
    - replayCheck()가 false 를 반환한다면 return을 통하여 그대로 게임을 종료시킨다.
    - replayCheck()가 true 를 반환한다면 다시 process()를 호출하는 재귀형태로 게임을 재시작한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+    static void process() {
+        if (BaseballGame.playGame()) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int replay = Integer.parseInt(Console.readLine());
+
+            if (!BaseballGame.replayCheck(replay)) {
+                System.out.println("게임이 종료되었습니다.");
+                return;
+            }
+            if (BaseballGame.replayCheck(replay)) {
+                process();
+            }
+        }
+    }
 }
+
