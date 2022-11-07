801baff9174d8f0b689019f0fcc77ccaebccdd5c
Qulip <ilkwon.ryu@gmail.com>
2022-11-08 06:57:36 +0900

    refactor: BaseballGameInputViewTest 리팩토링

--- src/test/java/baseball/view/BaseballGameInputViewTest.java
+++ src/test/java/baseball/view/BaseballGameInputViewTest.java
@@ -11,9 +11,10 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-@DisplayName("사용자 입력 숫자 확인")
+@DisplayName("사용자 입력 확인")
 class BaseballGameInputViewTest {
 
     OutputStream out;
@@ -24,37 +25,42 @@ class BaseballGameInputViewTest {
         out = new ByteArrayOutputStream();
     }
 
-    @Test
-    @DisplayName("올바르지 않은 숫자 입력")
-    public void isNotValidInput() {
-        // given
-        final String input = "111";
-        System.setOut(new PrintStream(out));
-        in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        // when
+    @Nested
+    @DisplayName("사용자 숫자 입력 검증")
+    class PlayerNumberValidation {
+        @Test
+        @DisplayName("올바르지 않은 숫자 입력")
+        public void isNotValidInput() {
+            // given
+            final String input = "111";
+            System.setOut(new PrintStream(out));
+            in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+            // when
 
-        // then
-        assertThrows(IllegalArgumentException.class, BaseballGameInputView::getPlayerNumber);
+            // then
+            assertThrows(IllegalArgumentException.class, BaseballGameInputView::getPlayerNumber);
+        }
+
+        @Test
+        @DisplayName("올바른 숫자 입력")
+        public void isValidInput() {
+            // given
+            final String input = "123";
+            System.setOut(new PrintStream(out));
+            in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+            final BaseballNumber baseballNumber = new BaseballNumber("123");
+
+            // when
+            final BaseballNumber result = BaseballGameInputView.getPlayerNumber();
+
+            // then
+            assertEquals(baseballNumber.getBaseballNumber().size(), result.getBaseballNumber().size());
+            assertEquals(baseballNumber.getBaseballNumber().get(0), result.getBaseballNumber().get(0));
+            assertEquals(baseballNumber.getBaseballNumber().get(1), result.getBaseballNumber().get(1));
+            assertEquals(baseballNumber.getBaseballNumber().get(2), result.getBaseballNumber().get(2));
+        }
     }
 
-    @Test
-    @DisplayName("올바른 숫자 입력")
-    public void isValidInput() {
-        // given
-        final String input = "123";
-        System.setOut(new PrintStream(out));
-        in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        final BaseballNumber baseballNumber = new BaseballNumber("123");
-
-        // when
-        final BaseballNumber result = BaseballGameInputView.getPlayerNumber();
-
-        // then
-        assertEquals(baseballNumber.getBaseballNumber().size(), result.getBaseballNumber().size());
-        assertEquals(baseballNumber.getBaseballNumber().get(0), result.getBaseballNumber().get(0));
-        assertEquals(baseballNumber.getBaseballNumber().get(1), result.getBaseballNumber().get(1));
-        assertEquals(baseballNumber.getBaseballNumber().get(2), result.getBaseballNumber().get(2));
-    }
 }
