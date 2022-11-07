93e3819bffe9a3e8537eadbcd974cd7d2ff504a3
anneu <b.imnuj@gmail.com>
2022-11-07 17:07:44 +0900

    refactor: 입력 값을 검증하는 기능 전면 수정
    - 검증 실패 시 boolean 값을 반환하는 것이 아닌 예외를 발생시키도록 입력 값 검증 로직을 전면 수정합니다.

--- src/main/java/baseball/InputValidation.java
+++ src/main/java/baseball/InputValidation.java
@@ -4,37 +4,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class InputValidation {
-    public boolean isNotThreeLetters(String input) {
-        return input.length() != 3;
+    public void validateLength(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3자리 수를 입력해야 합니다.");
+        }
     }
 
-    public boolean isNot1To9(String input) {
+    public void validateNumberRange(String input) {
         char[] chars = input.toCharArray();
 
         for (char letter : chars) {
             if (!('1' <= letter && letter <= '9')) {
-                return true;
+                throw new IllegalArgumentException("1부터 9까지의 수를 입력해야 합니다.");
             }
         }
-
-        return false;
     }
 
-    public boolean hasDuplicatedValues(String input) {
+    public void validateDuplication(String input) {
         Map<String, Integer> map = new HashMap<>();
 
         for (String letter : input.split("")) {
             map.put(letter, map.getOrDefault(letter, 0) + 1);
 
             if (map.get(letter) >= 2) {
-                return true;
+                throw new IllegalArgumentException("각 자리의 수가 서로 달라야 합니다.");
             }
         }
-
-        return false;
     }
 
-    public boolean isNotCorrectChoice(String input) {
-        return !(input.equals("1") || input.equals("2"));
+    public void validateChoice(String input) {
+        if (!(input.equals("1") || input.equals("2"))) {
+            throw new IllegalArgumentException("1 또는 2를 입력해야 합니다.");
+        }
     }
 }
