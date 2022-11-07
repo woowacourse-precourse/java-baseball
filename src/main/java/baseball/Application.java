b3cde6e51a116ef602c5c72166b454af7ce3ae96
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-07 17:13:04 +0900

    docs : 기능목록 재정의

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,9 @@
 package baseball;
 
+import static baseball.config.Config.playGameController;
+
 public class Application {
     public static void main(String[] args) {
-
+        playGameController().run();
     }
 }
