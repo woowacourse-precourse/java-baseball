5e8656ae15fb753baab0dc38ef8673a163ee305c
noyes5 <noyes5@naver.com>
2022-11-07 15:37:42 +0900

    test: user test 수정

--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -1,20 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.test.NsTest;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class UserTest {
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class UserTest extends NsTest {
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+
     @Test
+    @DisplayName("유저 입력값 테스트")
     void 유저_입력값_테스트() {
         User user = new User();
-        String input = "1234";
-        user.validate(input);
-        String input2 = "012";
-        user.validate(input2);
-        String input3 = "abc";
-        user.validate(input3);
-        String input4 = "12";
-        user.validate(input4);
-        String input5 = "123";
-        user.validate(input5);
+
+        assertThatThrownBy(() -> runException("2345"))
+                        .isInstanceOf(IllegalArgumentException.class);
+
+        assertThatThrownBy(() -> runException("102"))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
