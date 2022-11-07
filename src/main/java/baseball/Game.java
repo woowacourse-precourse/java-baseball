0b6a85a4335d11c53f6934c450a3b1541b21bc33
Seungyong Choi <ppyooy336@naver.com>
2022-11-08 00:51:14 +0900

    feat: 게임플레이어에게 서로 다른 3개의 숫자를 입력받는다.

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
 
     private Computer computer;
@@ -10,5 +12,17 @@ public class Game {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         this.computer = computer;
+
+        String inputNumber = input();
+
+
     }
+
+    private String input(){
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        return input;
+    }
+
+
 }
