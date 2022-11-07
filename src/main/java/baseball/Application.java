32f686e58cf2bb9b071f84ddb93ca0ad5545dff4
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-07 15:12:09 +0900

    style(printResult method) : follow printout format

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -114,6 +114,7 @@ class Output {
         }
         if (strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         }
     }
 }
