e35b9d33e066b347695b050390def45597fb7fa7
song-wooseok <sws981202@gmail.com>
2022-11-07 11:24:12 +0900

    feat: 임의의 서로 다른 숫자 3개를 리스트에 담는 기능 (정답생성)

---
+++ src/main/java/baseball/utils/Constants.java
@@ -0,0 +1,9 @@
+package baseball.utils;
+
+public class Constants {
+    public static final int BASEBALL_GAME_NUMBER_LENGTH = 3;
+
+    public static final int BASEBALL_GAME_NUMBER_RANGE_START = 1;
+
+    public static final int BASEBALL_GAME_NUMBER_RANGE_END = 9;
+}
