418889148f3a03c6b5f06c354e6185f9d4232592
jindaram-stu <jinsb1999@naver.com>
2022-11-08 01:27:36 +0900

    feat(UserNumber): 유저 숫자 입력 시 검증 로직 추가
    3자리가 아닌 경우, 숫자가 아닌 경우, 중복된 숫자가 있는 경우에 대해 검증 기능 추가

--- src/main/java/baseball/game/service/UserNumber.java
+++ src/main/java/baseball/game/service/UserNumber.java
@@ -2,15 +2,49 @@ package baseball.game.service;
 
 import java.util.*;
 import java.util.stream.Collectors;
-
+import java.util.stream.IntStream;
 
 public class UserNumber {
 
     public static List<Integer> generate(String userInput) {
+        if (!isThreeLength(userInput)) {
+            throw new IllegalArgumentException();
+        }
+
+        if (!isNumber(userInput)) {
+            throw new IllegalArgumentException();
+        }
+
+        if(!isDuplicateNumber(userInput)) {
+            throw new IllegalArgumentException();
+        }
+
         return Arrays.stream(userInput.split(""))
                 .map(s -> Integer.parseInt(s))
                 .collect(Collectors.toList());
 
+    }
+
+    private static boolean isThreeLength(String userInput) {
+        return userInput.length() == 3;
+    }
+
+    private static boolean isNumber(String userInput) {
+
+        return IntStream.range(0,userInput.length())
+                .filter(i -> !Character.isDigit(userInput.charAt(i)))
+                .findAny()
+                .isEmpty();
+    }
+
+    private static boolean isDuplicateNumber(String userInput) {
+        Set<String> numberSet = new HashSet<>();
+
+        Arrays.stream(userInput.split(""))
+                .map(s -> numberSet.add(s))
+                .collect(Collectors.toSet());
+
+        return numberSet.size() == 3;
     }
 
 }
