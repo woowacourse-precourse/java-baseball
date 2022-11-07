0790e7f4df456c7e31aa72e8fbead90343774a72
hhiyeon <hhyyeon0214@gmail.com>
2022-11-07 20:17:01 +0900

    feat: response-print stop program

--- src/main/java/baseball/ResponseMessage.java
+++ src/main/java/baseball/ResponseMessage.java
@@ -12,4 +12,8 @@ class ResponseMessage {
     static void allStrike() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+
+    static void stopProgram() {
+        System.out.println("프로그램이 종료되었습니다.");
+    }
 }
