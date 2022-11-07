23876dc3a82099db11d1c18c06137510d15555b7
Woojin Lee <dhkep03@gmail.com>
2022-11-07 14:18:29 +0900

    feat(Game): 유저가 종료할 때까지 게임을 반복

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -1,4 +1,23 @@
 package baseball.controller;
 
+import baseball.type.ResultGame;
+import baseball.view.OutputView;
+
 public class Game {
+    private static ResultGame resultGame = ResultGame.Repeat;
+
+    static void repeatToEnd() {
+        while (checkResultIsRepeat()) {
+            playOneTime();
+        }
+    }
+
+    static void playOneTime() {
+        Turn.repeatToEnd();
+        checkIsUserRepeatGame();
+    }
+
+    static boolean checkResultIsRepeat() {
+        return resultGame == ResultGame.Repeat;
+    }
 }
