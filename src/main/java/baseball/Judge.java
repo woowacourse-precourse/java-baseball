ee2593ed298c5ab938b993b34f9399dc28fafb6f
YUSEONJUN <seonjun0906@gmail.com>
2022-11-07 15:00:22 +0900

    feat : 결과를 최종 결정해주는 함수 구현

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -5,6 +5,13 @@ import java.util.List;
 public class Judge {
 	Compare compare = new Compare();
 
+	public String judgement(List<Integer> computer, List<Integer> player) {
+		int total = compare.howMany(computer, player);
+		int strike = compare.countStrike(computer, player);
+		int ball = compare.countBall(computer, player);
+		return message(total, strike, ball);
+	}
+
 	public String message(int total, int strike, int ball) {
 		if (total == 0) {
 			return "낫싱";
