98883a3408340c952d79251f35c67a1092aaa3ef
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-11-08 03:18:32 +0900

    feat(PlayNumberBaseballGame): add isNewGame feature

--- src/main/java/baseball/PlayNumberBaseballGame.java
+++ src/main/java/baseball/PlayNumberBaseballGame.java
@@ -90,4 +90,11 @@ public class PlayNumberBaseballGame {
     public String outputAskPlayNewGameStatement(){
         return "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     }
+
+    public boolean isNewGame(int playerAnswer){
+        if(playerAnswer==1) {
+            return true;
+        }
+        return false;
+    }
 }
