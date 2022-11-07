7d59cc451d8285856470cbe6063395c3f2b5f8ff
hhiyeon <hhyyeon0214@gmail.com>
2022-11-07 21:39:49 +0900

    feat: app-input 2 and stop program

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
 public class Application {
+    private static final int RESTART_GAME = 1;
+    private static final int STOP_PROGRAM = 2;
+    private static final int STOP_GAME = 3;
+    private static int gameExecution = 0;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static void stopProgram() {
+        if (gameExecution == STOP_PROGRAM) {
+            ResponseMessage.stopProgram();
+        }
     }
 }
