f229867275813c3ddedad00b3d6e5a6845543ead
baksohyeon <bori12370@gmail.com>
2022-11-07 22:54:27 +0900

    feat: 게임을 재시작할지 말지 정하는 메서드 추가

--- src/main/java/baseball/ExecuteGame.java
+++ src/main/java/baseball/ExecuteGame.java
@@ -10,4 +10,22 @@ public class ExecuteGame {
         String input = scanner.next();
         return input;
     }
+
+    // 게임을 재시작할지 말지 묻는 메서드
+    private boolean canRepeatGame () {
+        while (true) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String input = saveInput();
+
+            if (input.equals("1")) {
+                return true;
+            } else if (input.equals("2")) {
+                return false;
+            } else {
+                System.out.println("1 또는 2를 입력하세요.");
+            }
+        }
+    }
+
+
 }
