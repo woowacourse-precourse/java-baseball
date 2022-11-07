80af9bab34d2362c9796f809adb43b487c56920a
jaehyeon <jhjh0022@naver.com>
2022-11-07 23:25:58 +0900

    refactor: 테스트 케이스 이름 명확하게 수정

--- src/test/java/baseball/tool/DuplicateByComparisonTest.java
+++ src/test/java/baseball/tool/DuplicateByComparisonTest.java
@@ -11,40 +11,40 @@ import static org.junit.jupiter.api.Assertions.*;
 class DuplicateByComparisonTest {
 
     @Nested
-    class findOverlapNumber{
+    class findOverlapNumber {
 
         @Test
-        void case1(){
+        void 두개의_리스트_비교하여_중복확인_성공_중복_2개인_경우() {
             int testNumber = 123;
-            List<Integer> testNumber2 = List.of(1,2,4);
-            List<Integer> answerNumber = List.of(1,2);
+            List<Integer> testNumber2 = List.of(1, 2, 4);
+            List<Integer> answerNumber = List.of(1, 2);
             List<Integer> testResult = DuplicateByComparison.findOverlapNumber(testNumber2, testNumber);
             assertEquals(answerNumber, testResult);
         }
 
         @Test
-        void case2(){
+        void 두개의_리스트_비교하여_중복확인_성공_중복_1개인_경우() {
             int testNumber = 753;
-            List<Integer> testNumber2 = List.of(5,4,2);
+            List<Integer> testNumber2 = List.of(5, 4, 2);
             List<Integer> answerNumber = List.of(5);
             List<Integer> testResult = DuplicateByComparison.findOverlapNumber(testNumber2, testNumber);
             assertEquals(answerNumber, testResult);
         }
 
         @Test
-        void case3(){
+        void 두개의_리스트_비교하여_중복확인_성공_중복_0개인_경우() {
             int testNumber = 975;
-            List<Integer> testNumber2 = List.of(1,2,3);
+            List<Integer> testNumber2 = List.of(1, 2, 3);
             List<Integer> answerNumber = List.of();
             List<Integer> testResult = DuplicateByComparison.findOverlapNumber(testNumber2, testNumber);
             assertEquals(answerNumber, testResult);
         }
 
         @Test
-        void case4(){
+        void 두개의_리스트_비교하여_중복확인_성공_중복_3개인_경우() {
             int testNumber = 321;
-            List<Integer> testNumber2 = List.of(1,2,3);
-            List<Integer> answerNumber = List.of(1,2,3);
+            List<Integer> testNumber2 = List.of(1, 2, 3);
+            List<Integer> answerNumber = List.of(1, 2, 3);
             List<Integer> testResult = DuplicateByComparison.findOverlapNumber(testNumber2, testNumber);
             assertEquals(answerNumber, testResult);
         }
