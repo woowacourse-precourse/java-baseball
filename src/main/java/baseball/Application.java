ee50c456318b45d71e30918242801b6c2d06f2f9
doehy <dbehxks02@naver.com>
2022-11-07 23:31:16 +0900

    feat : 게임 재시작 여부 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -76,4 +76,9 @@ public class Application {
         }
         else {return false;}
     }
+    public boolean choiceRestartGame(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return gamer.inputRestartNumber();
+    }
 }
