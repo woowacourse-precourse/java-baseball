8baa9b0e6190550d863b1aa23f04d0c3461a56c6
dclxxi <dclxxi@kakao.com>
2022-11-07 17:55:28 +0900

    fix: 누락됐던 게임을 완전히 종료하는 문구를 출력하는 기능 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,7 +4,8 @@ import baseball.Config;
 
 public class OutputView {
     public static final String START = "숫자 야구 게임을 시작합니다.";
-    public static final String GAME_OVER = Config.MAX_STRIKE + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String SHUTDOWN = "게임 종료";
+    public static final String GAME_OVER = Config.MAX_STRIKE + "개의 숫자를 모두 맞히셨습니다! " + SHUTDOWN;
 
     public static void printStart() {
         System.out.println(START);
@@ -16,5 +17,9 @@ public class OutputView {
 
     public static void printGameOver() {
         System.out.println(GAME_OVER);
+    }
+
+    public static void printShutdown() {
+        System.out.println(SHUTDOWN);
     }
 }
