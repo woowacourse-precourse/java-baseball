f2a430cad3b2a36c8ca173dae4bd03c964182420
ingpyo <juninpyo72@gmail.com>
2022-11-07 11:18:37 +0900

    feat:볼 카운트를 비교하여 "볼","스트라이크","낫싱"출력 기능 구현

--- src/main/java/baseball/view/SystemMessage.java
+++ src/main/java/baseball/view/SystemMessage.java
@@ -5,4 +5,20 @@ public class SystemMessage {
 		return "숫자를 입력해주세요 : ";
 	}
 
+	public String PrintCount(int strike, int ball) {
+		if (strike > 0 && ball > 0) {
+			return ball + "볼 " + strike + "스트라이크";
+		}
+		if (strike > 0 && ball == 0) {
+			return strike + "스트라이크";
+		}
+		if (ball > 0 && strike == 0) {
+			return ball + "볼";
+		}
+		if (strike == 0 && ball == 0) {
+			return "낫싱";
+		}
+		return "";
+	}
+
 }
