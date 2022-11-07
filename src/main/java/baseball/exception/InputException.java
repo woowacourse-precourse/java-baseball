ea53e441500e77398b02d19bc2b203ce7e528f3b
nswon <swnam0729@gmail.com>
2022-11-07 23:25:30 +0900

    style: 매직넘버를 상수로 변경

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -5,17 +5,19 @@ import java.util.regex.Pattern;
 import static baseball.constant.Constant.*;
 
 public class InputException {
+    private static final String NOT_NUMBER = "숫자가 아닙니다.";
+    private static final String NOT_NUMBER_LENGTH = "숫자의 길이가 맞지 않습니다.";
     private InputException() {}
 
     public static void isNumber(String inputNumbers) {
         if(!Pattern.matches("^[0-9]*$", inputNumbers)) {
-            throw new IllegalArgumentException("숫자가 아닙니다.");
+            throw new IllegalArgumentException(NOT_NUMBER);
         }
     }
 
     public static void isLength(String inputNumbers) {
         if(inputNumbers.length() != RANDOM_NUMBERS_LENGTH) {
-            throw new IllegalArgumentException("숫자의 길이가 맞지 않습니다.");
+            throw new IllegalArgumentException(NOT_NUMBER_LENGTH);
         }
     }
 }
