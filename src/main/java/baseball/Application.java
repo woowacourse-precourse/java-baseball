10d4abba3a39938231e349ed37ec15e6c0f00e9b
jhsseonn <hosu0125@gmail.com>
2022-11-07 12:27:46 +0900

    feat: 1~9까지 서로 다른 숫자 3개 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        //랜덤한 숫자 3개 뽑기
+
+        List<Integer> computerNumber = new ArrayList<Integer>();
+
+        for(int i=0; i<3; i++) {
+            computerNumber.add(Randoms.pickNumberInRange(1, 9));
+        }
+
     }
 }
