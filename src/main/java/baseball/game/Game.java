8e649f4bc7d97530ca22ca8e103f199d3e835943
BaeDongHyeon <sasd730@gmail.com>
2022-11-07 19:45:16 +0900

    refactor: 메소드 반환 형태 변경
    - String을 List<Integer>로 변경

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -2,6 +2,10 @@ package baseball.game;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Game {
     private Game() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -10,7 +14,11 @@ public class Game {
         Game game = new Game();
     }
 
-    private String getInputUserNumber() {
-        return Console.readLine();
+    private List<Integer> getInputUserNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String userInput = Console.readLine();
+        return Arrays.asList(userInput.split("")).stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
