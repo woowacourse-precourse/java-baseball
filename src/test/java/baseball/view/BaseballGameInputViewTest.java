b78790eb830ac8b67fd8b944114d901932e427ee
Qulip <ilkwon.ryu@gmail.com>
2022-11-08 07:00:38 +0900

    test: BaseballGameInputViewTest 재시작 입력 유효성 검증 테스트 추가

--- src/test/java/baseball/view/BaseballGameInputViewTest.java
+++ src/test/java/baseball/view/BaseballGameInputViewTest.java
@@ -1,7 +1,9 @@
 package baseball.view;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import baseball.domain.BaseballNumber;
 import java.io.ByteArrayInputStream;
@@ -60,6 +62,56 @@ class BaseballGameInputViewTest {
             assertEquals(baseballNumber.getBaseballNumber().get(0), result.getBaseballNumber().get(0));
             assertEquals(baseballNumber.getBaseballNumber().get(1), result.getBaseballNumber().get(1));
             assertEquals(baseballNumber.getBaseballNumber().get(2), result.getBaseballNumber().get(2));
+        }
+    }
+
+    @Nested
+    @DisplayName("재시작 입력 유효성 검증")
+    class ReplayInputValidation {
+        @Test
+        @DisplayName("올바르지 않은 입력")
+        public void isNotValidInput() {
+            // given
+            final String input = "z";
+            System.setOut(new PrintStream(out));
+            in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+            // when
+
+            // then
+            assertThrows(IllegalArgumentException.class, BaseballGameInputView::selectReplay);
+        }
+
+        @Test
+        @DisplayName("올바른 입력(재시작)")
+        public void isReplay() {
+            // given
+            final String input = "1";
+            System.setOut(new PrintStream(out));
+            in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+
+            // when
+            final boolean result = BaseballGameInputView.selectReplay();
+
+            // then
+            assertTrue(result);
+        }
+
+        @Test
+        @DisplayName("올바른 입력(종료)")
+        public void isEnd() {
+            // given
+            final String input = "2";
+            System.setOut(new PrintStream(out));
+            in = new ByteArrayInputStream(input.getBytes());
+            System.setIn(in);
+
+            // when
+            final boolean result = BaseballGameInputView.selectReplay();
+
+            // then
+            assertFalse(result);
         }
     }
 
