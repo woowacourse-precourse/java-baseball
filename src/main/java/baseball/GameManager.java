5e5b59cb5a0d58ab6bad74a9826fb8c4f97c8d50
AhnJoonSung <tjdtna01@naver.com>
2022-11-08 02:11:39 +0900

    add game start function

---
+++ src/main/java/baseball/GameManager.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class GameManager {
+    static void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        onGame();
+    }
+
+    static void onGame() {
+    
+    }
+}
