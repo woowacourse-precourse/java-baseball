34979c9a11c8fc5cf03b9240ab0f819bfecc6f9a
hayeon <qorgkdus12@naver.com>
2022-11-07 15:33:24 +0900

    feat(CheckNumber function): check strike or ball in 3 numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,8 +5,61 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.*;
 
 public class Application {
+    // userInput의 볼, 스트라이크를 검사하는 함수
+    static int CheckNumber(List<Integer> computer, String userInput){
+        // 스트라이크, 볼 변수 선언
+        int strike=0;
+        int ball=0;
+
+        // 숫자 3개로 분리하기
+        int firstNum=userInput.indexOf(0);
+        int secondNum=userInput.indexOf(1);
+        int thirdNum=userInput.indexOf(2);
+
+        if (computer.indexOf(0) == firstNum){
+            strike++;
+        }
+        else if (computer.indexOf(0) == secondNum || computer.indexOf(0)==thirdNum){
+            ball++;
+        }
+
+        if (computer.indexOf(1) == secondNum){
+            strike++;
+        }
+        else if (computer.indexOf(1)==firstNum||computer.indexOf(1)==thirdNum){
+            ball++;
+        }
+
+        if (computer.indexOf(2) == thirdNum){
+            strike++;
+        }
+        else if (computer.indexOf(2)==firstNum || computer.indexOf(2)==secondNum){
+            ball++;
+        }
+
+        if (strike==3){
+            System.out.println(strike+"스트라이크");
+            System.out.println(strike+"개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return 1;
+        }
+
+        if(ball!=0 || strike!=0){
+            if (ball==0) { //strike만 존재
+                System.out.println(strike + "스트라이크");
+            }
+            else if (strike==0){ //ball만 존재
+                System.out.println(ball+"볼");
+            }
+            else{   //strike, ball 둘 다 존재
+                System.out.println(ball+"볼"+strike + "스트라이크");
+            }
+        }else{
+            System.out.println("낫싱");
+        }
+        return 0;
+    }
     public static void main(String[] args) {
-        String userInput;
+        String userInput = null;
         int status=1;
 
         // 1. 시작 멘트
@@ -25,6 +78,10 @@ public class Application {
             Scanner sc1 = new Scanner(System.in);
             System.out.println("숫자를 입력해주세요 : ");
             userInput = sc1.next();
+        }
+        // 4. 숫자 검사
+        if (CheckNumber(computer, userInput)==1){
+            return;
         }
     }
 }
