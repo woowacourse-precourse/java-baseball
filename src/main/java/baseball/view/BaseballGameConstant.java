753fac90afaa9229f6fe3de5618a430938d71816
Qulip <ilkwon.ryu@gmail.com>
2022-11-08 04:23:01 +0900

    feat: BaseballGameConstant 메세지 상수화

---
+++ src/main/java/baseball/view/BaseballGameConstant.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+public class BaseballGameConstant {
+    public static String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static String BALL_COUNT = "볼 ";
+    public static String STRIKE_COUNT = "스트라이크";
+    public static String NOTHING_MATCHED = "낫싱";
+    public static String ALL_MATCHED_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static String GAME_FINISHED_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
