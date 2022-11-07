df56401020f788e603c9640568d43e0f7efeeed8
Jeonwoojae <dnwo0719@icloud.com>
2022-11-07 17:59:08 +0900

    주어진 라이브러리(pickNum, readLine) 테스트

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    class BaseBallGame{
+
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        //기능 테스트
+        int randNum = Randoms.pickNumberInRange(0,999);
+        System.out.println("RandomNum : " + randNum);
+        String inputData = Console.readLine();
+        System.out.println("InputData : " + inputData);
     }
 }
