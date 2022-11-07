f31bae7d1439f9af4e89626b9731a3354d584cf0
kim1387 <kevink0@naver.com>
2022-11-07 23:02:58 +0900

    feat: Computer가 생성한 숫자를 관리하는 class 구현

---
+++ src/main/java/computer/Computer.java
@@ -0,0 +1,14 @@
+package computer;
+
+public class Computer {
+
+    private final Integer targetNumber;
+
+    public Computer(Integer targetNumber) {
+        this.targetNumber = targetNumber;
+    }
+
+    public Integer getTargetNumber() {
+        return targetNumber;
+    }
+}
