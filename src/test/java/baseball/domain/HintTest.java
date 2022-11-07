92e6c0cbe3b66eb17b78d73d49c906454587fd60
JEONG-JIWHAN <jjw981210@gmail.com>
2022-11-08 05:58:29 +0900

    test: 힌트 제공 기능 테스트 작성

---
+++ src/test/java/baseball/domain/HintTest.java
@@ -0,0 +1,23 @@
+package baseball.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class HintTest {
+
+  @Test
+  @DisplayName("힌트 제공 기능 테스트")
+  void 힌트_스트라이크_볼(){
+    Balls balls1 = new Balls(new ArrayList<>(Arrays.asList(1,4,5)));
+    Balls balls2 = new Balls(new ArrayList<>(Arrays.asList(1,9,4)));
+    Hint hint = new Hint();
+    hint.countStrikeAndBall(balls1, balls2);
+    assertEquals(hint.toString(), "1볼 1스트라이크");
+  }
+  
+}
