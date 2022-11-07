2047954b8d7d39069b5d04ecddba8e26410b4de8
kch4731 <kch4731@naver.com>
2022-11-08 01:14:08 +0900

    Comment : 테스트 함수 주석 해제 및 주석 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -13,17 +13,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-//    @Test
-//    void 인풋_테스트_하는법() throws Exception {
-//        //given
-//        String input = "123"; // 이것이 인풋이 들어가
-//        InputStream in = new ByteArrayInputStream(input.getBytes());
-//        System.setIn(in);
-//        // when
-//        String result = Application.getNumberOfUserString();
-//        // then
-//        AssertionsForClassTypes.assertThat(result).contains("123");
-//    }
+    @Test
+    void 인풋_테스트_하는법() throws Exception {
+        // 1. given
+        String input = "123"; // 이것이 인풋이 들어가
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        // 2. when
+        String result = Application.getNumberOfUserString();
+        // 3. then
+        AssertionsForClassTypes.assertThat(result).contains("123");
+    }
 
     @Test
     void 게임종료_후_재시작() {
