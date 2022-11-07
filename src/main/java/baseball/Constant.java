eb2766cb87b6397b19b5c526b4790fc64f2801f1
nayooniverse <kangna99@naver.com>
2022-11-08 08:43:38 +0900

    feat: 상수 분리

--- src/main/java/baseball/Constant.java
+++ src/main/java/baseball/Constant.java
@@ -5,4 +5,5 @@ public class Constant {
     public static final String GAME_GUESS = "숫자를 입력해주세요 : ";
     public static final String GAME_SUCCESS = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String GAME_RETRY = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final Integer MAX_LENGTH = 3;
 }
