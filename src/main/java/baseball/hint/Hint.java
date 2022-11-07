bdea200cbacd8ae79f39bffda779af8a347fc1c3
yujiyea <54650556+yujiyea@users.noreply.github.com>
2022-11-08 06:07:14 +0900

    feat(hint/Hint.java): 3개 숫자를 모두 맞혔는지 확인하는 기능 구현

--- src/main/java/baseball/hint/Hint.java
+++ src/main/java/baseball/hint/Hint.java
@@ -8,6 +8,8 @@ public class Hint {
     private static final String NOTHING = "낫싱";
     private static final String BALL = "볼 "; //뒤에 스트라이크가 붙을때 공백을 추가하기 위해 볼뒤에 공백까지 붙여서 저장함
     private static final String STRIKE = "스트라이크";
+    private static final int WIN_COUNT = 3;
+    private static final String WIN_STRING = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public Hint(List<Integer> computerNum, List<Integer> userNum){
         ball = 0;
@@ -46,5 +48,12 @@ public class Hint {
         System.out.println(getHint());
     }
 
+    public boolean checkWin(){
+        if (strike == WIN_COUNT){
+            System.out.println(WIN_STRING);
+            return true;
+        }
+        return false;
+    }
 }
 
