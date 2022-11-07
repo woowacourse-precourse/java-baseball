a2abe24160ee95825cb1c91bba1202af0d4666b2
abbd2 <ajkety00@gmail.com>
2022-11-07 15:13:57 +0900

    refactor(InputController): refactoring

--- src/main/java/baseball/InputController.java
+++ src/main/java/baseball/InputController.java
@@ -8,21 +8,14 @@ import java.util.stream.Stream;
 
 public class InputController {
 
-    public List<Integer> gameUserInput(){
-        List<Integer> userNumberList = inputConvert(); // 사용자 입력
-        return userNumberList;
-    }
-
-    public List<Integer> inputConvert() {
-        String userNumber = getInput();
-        List<Integer> userNumberList = userNumberConvertToList(userNumber); // 자릿수로 나눈 리스트로 변환
+    public List<Integer> inputConvert(String inputNumber) {
+        List<Integer> userNumberList = userNumberConvertToList(inputNumber); // 자릿수로 나눈 리스트로 변환
         return userNumberList;
     }
 
     public String getInput(){
-        ShowMessage.showInputNumber(); // 숫자 입력 메세지 출력
-        String userNumber = Console.readLine(); // 입력 받기
-        return userNumber;
+        String inputNumber = Console.readLine(); // 입력 받기
+        return inputNumber;
     }
 
     // 숫자 하나씩 나누어서 리스트에 보관
@@ -30,13 +23,5 @@ public class InputController {
         List<Integer> userNumberList = Stream.of(userNumber.split("")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
         return userNumberList;
     }
-
-//    public String inputException(String userNumber){
-//        Exception exception = new Exception();
-//        userNumber = getInput(); // 입력 받기
-//        exception.numberException(userNumber);
-//        System.out.println("inputException");
-//        return userNumber;
-//    }
 
 }
