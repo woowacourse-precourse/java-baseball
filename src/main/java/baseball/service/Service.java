4a8909e61133be0fc483e3cac302a391005751c8
소대훈 <theogns0681@naver.com>
2022-11-07 11:26:03 +0900

    [refactor] 조건문 Objects사용으로 변경

--- src/main/java/baseball/service/Service.java
+++ src/main/java/baseball/service/Service.java
@@ -4,21 +4,22 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class Service {
 
     public int findBall(int score, int input) {
         int count = 0;
-        if (score / 100 == input / 10 % 10 || score / 100 == input % 10) count++;
-        if (score / 10 % 10 == input / 100 || score / 10 % 10 == input % 10) count++;
-        if (score % 10 == input / 100 || score % 10 == input / 10 % 10) count++;
+        if (Objects.equals(score / 100, input / 10 % 10) || Objects.equals(score / 100, input % 10)) count++;
+        if (Objects.equals(score / 10 % 10, input / 100) || Objects.equals(score / 10 % 10, input % 10)) count++;
+        if (Objects.equals(score % 10, input / 100) || Objects.equals(score % 10, input / 10 % 10)) count++;
         return count;
     }
 
     public int findStrike(int score, int input) {
         int count = 0;
         for (int i = 0; i < 3; i++) {
-            if (score % 10 == input % 10) count++;
+            if (Objects.equals(score % 10, input % 10)) count++;
             score /= 10;
             input /= 10;
         }
