e57f88cf98ec39003e2056215ef837daf91db466
Anthologia <pyoungjy@gmail.com>
2022-11-08 01:29:59 +0900

    feat: 컴퓨터 역할의 임의의 수 3개를 고르는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,10 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
 
@@ -9,6 +13,8 @@ public class Application {
 
         System.out.print("숫자를 입력해주세요 : ");
         String userNumber = Console.readLine();
+
+        List<Integer> computerNumber = Randoms.pickUniqueNumbersInRange(1, 9, 3);
     }
 
     private static void printGameStartMessage() {
