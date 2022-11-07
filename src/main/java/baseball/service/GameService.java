71a77779294821b118d2355ce6e5df91a48276e9
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-08 03:38:50 +0900

    feat : 3스트라이크 이후 게임진행 여부 묻는 함수 구현

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -26,6 +26,15 @@ public class GameService {
 
             findRandomBall();
         }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        selectContinue();
+    }
+    private static void selectContinue() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String selectNumber = Console.readLine();
+
+        if(Integer.valueOf(selectNumber)==1) inputOrder();
+        if(Integer.valueOf(selectNumber)==2) System.out.println("게임 종료");
     }
     public static void checkInput() {
         System.out.print("숫자를 입력해주세요 : ");
