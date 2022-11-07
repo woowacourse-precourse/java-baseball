3c0f6b201358d9dee881c524ac68ff667f8a7fd8
PgmJun <84304802+PgmJun@users.noreply.github.com>
2022-11-07 10:34:00 +0900

    feature : add First Class Collection

---
+++ src/main/java/baseball/utils/Baseball.java
@@ -0,0 +1,13 @@
+package baseball.utils;
+
+import java.util.LinkedHashSet;
+
+public class Baseball {
+
+    private final LinkedHashSet<Integer> baseballNumber;
+
+    public Baseball(LinkedHashSet<Integer> baseballNumber) {
+        this.baseballNumber = baseballNumber;
+    }
+
+}
