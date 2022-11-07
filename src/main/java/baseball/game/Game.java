05c31bd8277a055a86bc3753f3d5225dfe06a2da
minju412 <ln8847@ajou.ac.kr>
2022-11-07 14:19:02 +0900

    refactor: reduce the role of the Game class
    - Game class only plays the role of running the game
      in collaboration with computer and player

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -1,89 +1,45 @@
 package baseball.game;
 
-import static baseball.util.InputValidator.COMPUTER_BALLS_SIZE;
+import static baseball.game.Computer.BALL_COUNT;
+import static baseball.game.Computer.STRIKE_COUNT;
 import static baseball.util.InputValidator.CONTINUE_GAME;
 
 import baseball.message.SystemMessage;
-import baseball.util.InputValidator;
-import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 
 public class Game {
 
-    public static final int STRIKE_COUNT = 0;
-    public static final int BALL_COUNT = 1;
+    private final Computer computer;
+    private final Player player;
+
+    public Game() {
+        this.computer = new Computer();
+        this.player = new Player();
+    }
 
     public void startGame() {
         System.out.println(SystemMessage.START_NUMBER_BASEBALL_GAME);
         do {
             playGame();
-        } while (Integer.parseInt(askRestartGameOrEnd()) == CONTINUE_GAME);
-    }
-
-    public String askRestartGameOrEnd() {
-        System.out.println(SystemMessage.RESTART_GAME_OR_END);
-        String answer = Console.readLine();
-        if (!InputValidator.checkProgressAnswer(answer)) {
-            throw new IllegalArgumentException();
-        }
-        return answer;
+            System.out.println(SystemMessage.RESTART_GAME_OR_END);
+        } while (Integer.parseInt(player.getPlayerAnswer()) == CONTINUE_GAME);
     }
 
     public void playGame() {
-        Ball ball = new Ball();
-        List<Integer> computerBalls = ball.getComputerBalls();
+        List<Integer> computerBalls = computer.getComputerBalls();
         List<Integer> strikeAndBallCounts = new ArrayList<>(Arrays.asList(0, 0));
         String resultMessage;
         System.out.println(computerBalls.toString()); // TODO: 삭제하기
         do {
-            List<Integer> playerBalls = ball.getPlayerBalls();
-            calculateStrikeAndBallCounts(computerBalls, playerBalls, strikeAndBallCounts);
-            resultMessage = getResultMessage(strikeAndBallCounts.get(STRIKE_COUNT),
+            System.out.print(SystemMessage.ENTER_NUMBER);
+            List<Integer> playerBalls = player.getPlayerBalls();
+            computer.calculateStrikeAndBallCounts(computerBalls, playerBalls, strikeAndBallCounts);
+            resultMessage = computer.getResultMessage(strikeAndBallCounts.get(STRIKE_COUNT),
                     strikeAndBallCounts.get(BALL_COUNT));
             System.out.println(resultMessage);
-            updateStrikeAndBallCounts(strikeAndBallCounts, 0, 0);
+            computer.updateStrikeAndBallCounts(strikeAndBallCounts, 0, 0);
         } while (!resultMessage.equals(SystemMessage.THREE_NUMBERS_RIGHT_GAME_OVER));
     }
-
-    private void calculateStrikeAndBallCounts(List<Integer> computerBalls, List<Integer> playerBalls, List<Integer> strikeAndBallCounts) {
-        playerBalls.stream()
-                .filter(ball -> computerBalls.stream().anyMatch(Predicate.isEqual(ball)))
-                .forEach(ball -> updateStrikeAndBallCounts(strikeAndBallCounts,
-                        getCount(playerBalls.indexOf(ball) == computerBalls.indexOf(ball),
-                                strikeAndBallCounts.get(STRIKE_COUNT)),
-                        getCount(playerBalls.indexOf(ball) != computerBalls.indexOf(ball),
-                                strikeAndBallCounts.get(BALL_COUNT))));
-    }
-
-    private void updateStrikeAndBallCounts(List<Integer> strikeAndBallCounts, int strikeCount, int ballCount) {
-        strikeAndBallCounts.set(STRIKE_COUNT, strikeCount);
-        strikeAndBallCounts.set(BALL_COUNT, ballCount);
-    }
-
-    private int getCount(boolean condition, int count) {
-        if (condition) {
-            count++;
-        }
-        return count;
-    }
-
-    private String getResultMessage(int strikeCnt, int ballCnt) {
-        if (strikeCnt == COMPUTER_BALLS_SIZE) {
-            return SystemMessage.THREE_NUMBERS_RIGHT_GAME_OVER;
-        }
-        if (strikeCnt > 0 && ballCnt > 0) {
-            return ballCnt + SystemMessage.BALL + " " + strikeCnt + SystemMessage.STRIKE;
-        }
-        if (strikeCnt == 0 && ballCnt > 0) {
-            return ballCnt + SystemMessage.BALL;
-        }
-        if (strikeCnt > 0 && ballCnt == 0) {
-            return strikeCnt + SystemMessage.STRIKE;
-        }
-        return SystemMessage.NOTHING;
-    }
-
 }
