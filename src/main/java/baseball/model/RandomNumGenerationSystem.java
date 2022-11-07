595a5d177c5e03a668644e1d57e58f9dfc81c2bb
wnsgml7267 <dlwns7267@naver.com>
2022-11-08 04:00:06 +0900

    feat(model/Constant): URandomNumGenerationSystem´ë컴퓨터 난수 생성 시스템 구현

---
+++ src/main/java/baseball/model/RandomNumGenerationSystem.java
@@ -0,0 +1,22 @@
+package baseball.model;
+
+import java.util.Arrays;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class RandomNumGenerationSystem {
+    public final int[] NUMS;
+
+    public RandomNumGenerationSystem() {
+        int[] nums;
+        do {
+            nums = pickComputerNums();
+        } while (!Util.isNotDuplicate(nums));
+        NUMS = nums;
+    }
+
+    private static int[] pickComputerNums() {
+        return Arrays.stream(new int[Constant.NUMS_LENGTH])
+                .map(num -> Randoms.pickNumberInRange(1, 9))
+                .toArray();
+    }
+}
