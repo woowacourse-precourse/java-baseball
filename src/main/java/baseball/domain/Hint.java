5bba741e49248e2c4bd0f06b7c9713ab300fdf7b
JEONG-JIWHAN <jjw981210@gmail.com>
2022-11-08 05:43:12 +0900

    feat: 사용자와 컴퓨터의 숫자를 비교해서 힌트를 제공하는 기능 구현

--- src/main/java/baseball/domain/Hint.java
+++ src/main/java/baseball/domain/Hint.java
@@ -35,4 +35,18 @@ public class Hint{
     return strike;
   }
 
+  @Override
+  public String toString() {
+    if (ball!= 0 && strike!= 0) {
+      return ball + "볼" + " " + strike + "스트라이크";
+    }
+    if (ball != 0) {
+      return ball + "볼";
+    }
+    if (strike != 0) {
+      return strike + "스트라이크";
+    }
+    return "낫싱";
+  }
+
 }
