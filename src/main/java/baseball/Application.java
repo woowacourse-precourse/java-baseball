0ca961cb03bfc5004c557ab064c9c77338ca1488
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-07 22:04:53 +0900

    feat: 사용자 입력을 입력값(List)으로 변환하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,12 +3,15 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         List<Integer> computer = getRandomNumberList();
         String userInput = getUserInput();
+        List<Integer> userAnswer = convertStringToIntegerList(userInput);
     }
 
     private static List<Integer> getRandomNumberList() {
@@ -19,6 +22,18 @@ public class Application {
     private static String getUserInput() {
         String userInput = Console.readLine();
         return userInput;
+    }
+
+    private static List<Integer> convertStringToIntegerList(String userInput) {
+        List<String> userInputStringList = Arrays.asList(userInput.split(""));
+        List<Integer> userAnswer = new ArrayList<>();
+
+        for (String str : userInputStringList) {
+            int converted = Integer.parseInt(str);
+            userAnswer.add(converted);
+        }
+
+        return userAnswer;
     }
 
 }
