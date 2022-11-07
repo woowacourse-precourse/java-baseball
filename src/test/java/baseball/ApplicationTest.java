1dc507536561be4555d895c328a80f7fbcedb14b
TaemHam <tk4003950@gmail.com>
2022-11-07 17:49:17 +0900

    Fix: Nested 관련 코드 롤백

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -4,7 +4,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import java.util.List;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
@@ -19,31 +18,25 @@ class ApplicationTest extends NsTest {
         return stringBuilder.toString();
     }
 
-    @Nested
-    class 종료_테스트 {
-        @Test
-        void 게임종료_후_재시작() {
-            Game baseballGame = new Game();
-            String answer = getAnswer(baseballGame);
-            baseballGame.operate(answer);
-            baseballGame.operate("1");
-            assertThat(output()).doesNotContain("게임종료");
-        }
+    @Test
+    void 게임종료_후_재시작() {
+        Game baseballGame = new Game();
+        String answer = getAnswer(baseballGame);
+        baseballGame.operate(answer);
+        baseballGame.operate("1");
+        assertThat(output()).doesNotContain("게임종료");
     }
 
-    @Nested
-    class 예외_테스트 {
-        @Test
-        void 입력_예외_테스트() {
-            boolean exceptionCaught = false;
-            Game baseballGame = new Game();
-            try {
-                baseballGame.operate("1234");
-            } catch (IllegalArgumentException e) {
-                exceptionCaught = true;
-            }
-            assertThat(exceptionCaught).isTrue();
+    @Test
+    void 입력_예외_테스트() {
+        boolean exceptionCaught = false;
+        Game baseballGame = new Game();
+        try {
+            baseballGame.operate("1234");
+        } catch (IllegalArgumentException e) {
+            exceptionCaught = true;
         }
+        assertThat(exceptionCaught).isTrue();
     }
 
     @Override
