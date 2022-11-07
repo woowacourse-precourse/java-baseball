f50bbcacacdd662ea3a9bdfe85d4ea7e0493abbb
LeeMyeongJin <=>
2022-11-07 22:30:42 +0900

    Feat: 입력한 수에 0이 포함될 경우 예외처리 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -3,11 +3,14 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
-    public static String getNumber(){
+    public static String getNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         String userNumber = Console.readLine();
-        if (userNumber.length() != 3){
-            throw new IllegalArgumentException();
+        if (userNumber.length() != 3) {
+            throw new IllegalArgumentException("입력한 수가 3글자가 아님");
+        }
+        if (userNumber.contains("0")){
+            throw new IllegalArgumentException("입력한 수에 0이 포함");
         }
         return userNumber;
     }
