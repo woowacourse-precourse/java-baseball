18dd24db5440b928aa46d023b7fed0661fc911fb
JEONG-JIWHAN <jjw981210@gmail.com>
2022-11-07 22:17:16 +0900

    test: 1~9자리의 숫자가 아닌 경우 예외 처리 테스트

---
+++ src/test/java/baseball/view/InputViewTest.java
@@ -0,0 +1,26 @@
+package baseball.view;
+
+import static baseball.message.ExceptionMessage.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+  InputView inputView = new InputView();
+  @Test
+  @DisplayName("입력값이 숫자인지 검증 테스트")
+  void 입력값_숫자_검증_테스트(){
+    Exception exception = assertThrows(IllegalArgumentException.class,()-> inputView.checkDigit("a"));
+    assertEquals(exception.getMessage(), INCORRECT_INPUT.getErrorMessage());
+  }
+
+  @Test
+  @DisplayName("입력값 1~9까지의 숫자 여부 테스트")
+  void 숫자_범위_테스트(){
+    Exception exception = assertThrows(IllegalArgumentException.class,()-> inputView.checkDigit("102"));
+    assertEquals(exception.getMessage(), INCORRECT_INPUT.getErrorMessage());
+  }
+
+
+}
