d23b141ab9e4df451c5e38d1725965a841696c01
Jeeseob <jeeseob5761@gmail.com>
2022-11-07 17:01:56 +0900

    Add: 메인함수 구현부 - 게임 재시작 관련 콘솔 출력 Comment 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,8 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     private static final int RESTART_NUMBER = 1;
+    private static final int STOP_NUMBER = 2;
+    private static final String RESTART_COMMENT = "게임을 새로 시작하려면 "+ RESTART_NUMBER +", 종료하려면 "+ STOP_NUMBER +"를 입력하세요.";
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         // TODO: 출력 Comment 추가
@@ -15,6 +17,7 @@ public class Application {
 
     // TODO: 게임 재시작 여부
     private static boolean isRestart() {
+        System.out.println(RESTART_COMMENT);
         int restartCommand = Integer.parseInt(Console.readLine());
         return restartCommand == RESTART_NUMBER;
     }
