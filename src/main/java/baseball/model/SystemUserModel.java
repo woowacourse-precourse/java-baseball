92a2936cf3a0935a071de46d592b8547423f6eed
username459712 <94822739+username459712@users.noreply.github.com>
2022-11-07 18:42:16 +0900

    feat: static value and default constructor

---
+++ src/main/java/baseball/model/SystemUserModel.java
@@ -0,0 +1,12 @@
+package baseball.model;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class SystemUserModel {
+    private List<Integer> digits;
+
+    public SystemUserModel() {
+    }
+}
