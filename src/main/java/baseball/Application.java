01f6201211c2b38e9aec09ae66fae517c1bcc6d8
WonSubSub <boxer1995@naver.com>
2022-11-07 10:12:33 +0900

    출력í함수 작성 및 기능목록 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,16 @@
 package baseball;
 public class Application {
+    public static void game_start_message(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void input_number_message(){
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    public static void three_strike_message(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         //First commit
