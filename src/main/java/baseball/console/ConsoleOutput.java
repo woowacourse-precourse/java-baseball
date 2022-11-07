e9cc3c58b62cf52e22ccc0dde15676f0fe92ee25
inhyeok <ss112d86@gmail.com>
2022-11-08 03:05:07 +0900

    feat: 결과 출력 메소드 구현

--- src/main/java/baseball/console/ConsoleOutput.java
+++ src/main/java/baseball/console/ConsoleOutput.java
@@ -1,5 +1,7 @@
 package baseball.console;
 
+import baseball.game.GameResult;
+
 public class ConsoleOutput {
 
   public void printStartMessage() {
@@ -10,5 +12,47 @@ public class ConsoleOutput {
   public void printEndMessage() {
     final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     System.out.println(END_MESSAGE);
+  }
+
+  public void printResult(GameResult result) {
+    StringBuilder sb = new StringBuilder();
+    final String STRIKE_MESSAGE = "스트라이크";
+    final String BALL_MESSAGE = "볼";
+    final String NOTING_MESSAGE = "낫싱";
+
+    if (isOnlyStrike(result)) {
+      sb.append(result.getStrike()).append(STRIKE_MESSAGE);
+    }
+
+    if (isOnlyBall(result)) {
+      sb.append(result.getBall()).append(BALL_MESSAGE);
+    }
+
+    if (isNoting(result)) {
+      sb.append(NOTING_MESSAGE);
+    }
+
+    if (isAllMatching(result)) {
+      sb.append(result.getBall()).append(BALL_MESSAGE);
+      sb.append(" ");
+      sb.append(result.getStrike()).append(STRIKE_MESSAGE);
+    }
+    System.out.println(sb);
+  }
+
+  private boolean isOnlyStrike(GameResult result) {
+    return !result.getStrike().equals(0) && result.getBall().equals(0);
+  }
+
+  private boolean isOnlyBall(GameResult result) {
+    return result.getStrike().equals(0) && !result.getBall().equals(0);
+  }
+
+  public boolean isNoting(GameResult result) {
+    return result.getStrike().equals(0) && result.getBall().equals(0);
+  }
+
+  public boolean isAllMatching(GameResult result) {
+    return !result.getStrike().equals(0) && !result.getBall().equals(0);
   }
 }
