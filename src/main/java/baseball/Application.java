6f6d4ce7f80c2e64c6bc2801e36e8efb7e640e65
jaehyeon <jhjh0022@naver.com>
2022-11-07 22:42:00 +0900

    feat: main 메서드 게임 실행구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.play.GameProgressFlow;
+
+import static baseball.print.OutputStatement.informStartNumberBaseballGame;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameProgressFlow gameProgressFlow = new GameProgressFlow();
+
+        informStartNumberBaseballGame();
+        gameProgressFlow.startGame();
     }
 }
