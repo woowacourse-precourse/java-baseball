ae9acb29ef3baa3afd3befb99a88d85d113a8b7b
drunkenhw <drunkenhw@gmail.com>
2022-11-07 18:15:00 +0900

    feat(OutputView) : 스트라이크 문구를 출력한다

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,4 +5,8 @@ public class OutputView {
     public static void startGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public static void printStrike(int strike) {
+        System.out.printf("%d 스트라이크", strike);
+    }
 }
