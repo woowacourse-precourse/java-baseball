c8a6588529e1c37878a8832456d0a574ec7c5d8c
김형기 <khk211113@naver.com>
2022-11-07 15:47:00 +0900

    feat: 랜덤이 아닌 직접 지정한 수를 생성할 수 있도록 추가

--- src/main/java/baseball/BaseballNum.java
+++ src/main/java/baseball/BaseballNum.java
@@ -8,6 +8,9 @@ public class BaseballNum {
 
     private final int[] nums = new int[3];
 
+    /**
+     *  중복 없는 3자리 수를 랜덤으로 생성한다.
+     */
     public BaseballNum() {
         int index = 1;
         while (index <= 3) {
@@ -16,6 +19,24 @@ public class BaseballNum {
                 nums[index - 1] = nextNum;
                 indexes[nextNum] = index++;
             }
+        }
+    }
+
+    /**
+     * 랜덤이 아닌 지정된 수를 이용해 BaseballNum을 생성
+     * @param num  3자리 수
+     * @throws IllegalArgumentException 입력된 수가 3자리가 아니거나, 중복이 있을 경우 예외를 발생시킨다.
+     */
+    public BaseballNum(int num) throws IllegalArgumentException {
+        String numStr = Integer.toString(num);
+        if (numStr.length() != 3) throw new IllegalArgumentException();
+
+        for (int i = 0; i < numStr.length(); i++) {
+            int digit = numStr.charAt(i) - '0';
+            if (indexes[digit] != 0) throw new IllegalArgumentException();
+
+            indexes[digit] = i + 1;
+            nums[i] = digit;
         }
     }
 
@@ -31,6 +52,11 @@ public class BaseballNum {
         return nums[2];
     }
 
+    /**
+     * 입력된 숫자가 3자리 수에서 위치한 곳을 반환한다.
+     * @param num 위치를 얻고 싶은 숫자를 입력
+     * @return 존재한다면 1 ~ 3 중 해당하는 위치를 반환. 없다면 0을 반환
+     */
     public int getIndex(int num) {
         try {
             return indexes[num];
