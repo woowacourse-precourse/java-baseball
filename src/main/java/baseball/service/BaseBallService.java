0b45202f37af57989283fb16c98b81ae56c41e1d
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-08 02:30:08 +0900

    feat(BaseBallService): 스트라이크 수와 볼의 수를 세어주는 서비스 메서드 작성

--- src/main/java/baseball/service/BaseBallService.java
+++ src/main/java/baseball/service/BaseBallService.java
@@ -4,6 +4,8 @@ import baseball.model.BaseBall;
 import baseball.model.Computer;
 import baseball.model.User;
 
+import java.util.List;
+
 public class BaseBallService {
     private BaseBall baseBall = new BaseBall();
     private User user = new User();
@@ -25,4 +27,21 @@ public class BaseBallService {
         user.setUserNumber(userNumber);
     }
 
+    public void checkResult() {
+        List<Integer> computerNumberList = computer.getComputerNumberList();
+        List<Integer> userNumberList = user.getUserNumberList();
+
+        int strike = 0, ball = 0;
+
+        for (int i = 0; i < computerNumberList.size(); i++) {
+            if (computerNumberList.get(i) == userNumberList.get(i)) {
+                strike++;
+            } else if (computerNumberList.contains(userNumberList.get(i))) {
+                ball++;
+            }
+        }
+
+        baseBall.setStrike(strike);
+        baseBall.setBall(ball);
+    }
 }
