470c32f79d6c5554b2923bc914908004dfde9b7d
eom-tae-in <eti0728@daum.net>
2022-11-07 18:37:42 +0900

    feat: Constant class -> 클래스를 구현할 때 사용하는 리터럴 값을 상수로 만들어 저장해놓은 클래스

---
+++ src/main/java/baseball/constant/Constant.java
@@ -0,0 +1,21 @@
+package baseball.constant;
+
+public class Constant {
+
+    public static final int INITIAL_NUMBER = 0;
+    public static final int STRIKE_MAX_NUMBER = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final int NUMBER_SIZE = 3;
+
+    public static final String START_BASEBALL_NUMBER_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_USER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public static final String PLAYER_CORRECT_ANSWER_NUMBER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static final String SELECT_RESTART_OR_END_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
+
+    public static final String END_BASEBALL_GAME_MESSAGE = "게임 종료";
+
+
+}
