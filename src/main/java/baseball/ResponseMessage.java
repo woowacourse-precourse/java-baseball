b4ccbff02849b8555498db4dc806551d341a8c88
hhiyeon <hhyyeon0214@gmail.com>
2022-11-07 20:16:21 +0900

    feat: response-print all strike and select game execution

--- src/main/java/baseball/ResponseMessage.java
+++ src/main/java/baseball/ResponseMessage.java
@@ -8,4 +8,8 @@ class ResponseMessage {
     static void inputNumber() {
         System.out.print("숫자를 입력해주세요 : ");
     }
+
+    static void allStrike() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
