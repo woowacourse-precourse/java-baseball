4a7855d8035712b6a075357c91c09b49c26a7393
jeomxon <jeomxon@gmail.com>
2022-11-07 14:56:06 +0900

    feat: 볼과 스트라이크를 계산하는 기능 구현

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,9 +1,12 @@
 package game;
 
+import java.util.List;
+
 import camp.nextstep.edu.missionutils.Console;
 
 public class Game {
-
+    private static final int STARTING_INDEX = 0;
+    private static final int MAX_LENGTH_OF_NUMBER = 3;
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -25,5 +28,21 @@ public class Game {
 
         throw new IllegalArgumentException("잘못된 값을 입력하였습니다. 게임 종료");
     }
+
+    public void checkBallAndStrike(List<Integer> computerNumbers, List<Integer> userNumbers) {
+
+        for (int i = STARTING_INDEX; i < MAX_LENGTH_OF_NUMBER; i++) {
+
+            if (computerNumbers.get(i).equals(userNumbers.get(i))) {
+                hint.incrementStrike();
+                continue;
+            }
+
+            if (computerNumbers.contains(userNumbers.get(i))) {
+                hint.incrementBall();
+            }
+        }
+    }
+
 
 }
---
+++ src/main/java/game/Hint.java
@@ -0,0 +1,22 @@
+package game;
+
+public class Hint {
+    private static int ball = 0;
+    private static int strike = 0;
+
+    public int getBall() {
+        return ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public void incrementBall() {
+        ball++;
+    }
+
+    public void incrementStrike() {
+        strike++;
+    }
+}
