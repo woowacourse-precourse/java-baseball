7a3c3beac4cade9246ed09b1cbc5c64660bc3fa0
xortmd <tmd2166@naver.com>
2022-11-07 23:51:35 +0900

    컴퓨터 숫자 생성 메소드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    private static final int NUM_SIZE = 3;
+
+    private static List<Integer> makeComNum() {
+        List<Integer> comNum = new ArrayList<>();
+        for(int i = 0; i < NUM_SIZE; i++) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!comNum.contains(randomNumber)) {
+                comNum.add(randomNumber);
+            }
+        }
+        return comNum;
+    }
+
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
