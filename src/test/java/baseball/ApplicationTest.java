8ab37fea135198ad4fb9bed96be8f916600eb16f
dongjun0128 <70315475+dongjun0128@users.noreply.github.com>
2022-11-07 16:11:34 +0900

    refactor : 불필요한 변수 제거 private static Number number; 제거

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -10,8 +10,6 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ApplicationTest extends NsTest {
-    private static Number number;
-
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
