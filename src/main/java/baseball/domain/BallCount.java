b8b291c6e459dc2fd88525a96ca1259238c1ce24
Qulip <ilkwon.ryu@gmail.com>
2022-11-08 05:44:09 +0900

    feat: BallCount 3스트라이크 경우 추가

--- src/main/java/baseball/domain/BallCount.java
+++ src/main/java/baseball/domain/BallCount.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import static baseball.view.BaseballGameConstant.ALL_MATCHED_MESSAGE;
 import static baseball.view.BaseballGameConstant.BALL_COUNT;
 import static baseball.view.BaseballGameConstant.NOTHING_MATCHED;
 import static baseball.view.BaseballGameConstant.STRIKE_COUNT;
@@ -27,6 +28,9 @@ public class BallCount {
             return NOTHING_MATCHED;
         }
         StringBuilder stringBuilder = new StringBuilder();
+        if (getStrike() == 3) {
+            stringBuilder.append(getStrike()).append(STRIKE_COUNT).append("\n").append(ALL_MATCHED_MESSAGE);
+        }
         if (getBall() > 0) {
             stringBuilder.append(getBall()).append(BALL_COUNT);
         }
