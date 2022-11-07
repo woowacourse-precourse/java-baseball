5d24dd5d27184713f4bd3fd359c9971ace29677c
josolha <josolha@nate.com>
2022-11-07 22:00:09 +0900

    feat : add inputNum method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,6 +10,7 @@ public class Application {
     public static void main(String[] args) {
 
     }
+
     static List<Integer> generateNum() {
         List<Integer> computerNumber = new ArrayList<>();
         while (computerNumber.size() < 3) {
@@ -19,6 +20,18 @@ public class Application {
             }
         }
         return computerNumber;
+    }
+
+    static List<Integer> inputNum() {
+        System.out.print("숫자를 입력해주세요 : ");
+        List<Integer> userNumber = new ArrayList<>();
+        for (String number : Console.readLine().split("")) {
+            userNumber.add(Integer.parseInt(number));
+            if (userNumber.size() > 3 || userNumber.size() != userNumber.stream().distinct().count()) {
+                throw new IllegalArgumentException();
+            }
+        }
+        return userNumber;
     }
 }
 
