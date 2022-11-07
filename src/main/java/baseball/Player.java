ded6ce9980154ad01bbeefb73f036e9ea042a341
LeeHaiLim <hailim1997@gmail.com>
2022-11-08 04:01:50 +0900

    feat(Player) : implement method get restart or quit input
    사용자로부터 재시작/종료 의사 입력을 받는 기능 구현
    - 재시작 코드 RESTART_GAME_CODE 상수 선언
    - 종료 코드 RESTART_GAME_CODE 상수 선언
    - 입력 메시지 QUIT_OR_RESTART_INPUT_MESSAGE 상수 선언
    - getRestartOrQuitInput() 메소드 구현

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -3,12 +3,27 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Player {
+    private static final String RESTART_GAME_CODE = "1";
+    private static final String QUIT_GAME_CODE = "2";
     private static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String QUIT_OR_RESTART_INPUT_MESSAGE =
+            String.format("게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요.", RESTART_GAME_CODE, QUIT_GAME_CODE);
 
     public String getNumberInput() {
         System.out.print(NUMBER_INPUT_MESSAGE);
         String input = Console.readLine();
 
         return input;
+    }
+
+    public boolean getRestartOrQuitInput() {
+        System.out.println(QUIT_OR_RESTART_INPUT_MESSAGE);
+        String input = Console.readLine();
+
+        if (input.equals(QUIT_GAME_CODE)) {
+            return false;
+        }
+
+        return input.equals(RESTART_GAME_CODE);
     }
 }
