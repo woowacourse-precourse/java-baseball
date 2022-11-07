e74dd2c71ab2730c727085a46acf29bb60bfe924
JaeWoong Kim <kimje1109@gmail.com>
2022-11-07 23:00:59 +0900

    refactor: Change startGame to print new line

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -59,7 +59,7 @@ public class Application {
     }
 
     public static void startGame() {
-        System.out.print("숫자 야구 게임을 시작합니다.");
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public static List<Integer> getUserInput() {
