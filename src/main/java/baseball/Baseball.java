9e15972e3428ee970e917127964ea2518cd80290
rosesua318 <rosesua@duksung.ac.kr>
2022-11-07 23:51:37 +0900

    feat: 세자리 난수 생성하기 기능 추가

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,6 +1,11 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Baseball {
     private String userNumber;
@@ -23,5 +28,37 @@ public class Baseball {
         return randomNumber;
     }
 
+    public void initialize() {
+        randomNumber = createRandomNumber(3);
+    }
 
+    private String createRandomNumber(int number) {
+        Set<Character> randomNumbers = new HashSet<>();
+        String result = "";
+        int count = 0;
+
+        while (number > count) {
+            char c = Character.forDigit(Randoms.pickNumberInRange(1, 9), 10);
+
+            if(randomNumbers.contains(c)) {
+                continue;
+            }
+
+            randomNumbers.add(c);
+            result += c;
+            count++;
+        }
+
+        return result;
+    }
+
+
+    public void play() {
+        System.out.println(Message.START);
+        String result = "";
+        do {
+            System.out.print(Message.INPUT);
+
+        } while (!result.equals(Message.CLEAR));
+    }
 }
