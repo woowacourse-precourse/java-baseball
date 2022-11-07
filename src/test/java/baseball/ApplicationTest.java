370a1dfed58d9d63e6620a828f6871eec7044ddc
TaemHam <tk4003950@gmail.com>
2022-11-07 17:57:28 +0900

    Test: 게임 진행 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -17,6 +17,42 @@ class ApplicationTest extends NsTest {
                 .forEach(stringBuilder::append);
         return stringBuilder.toString();
     }
+    @Test
+    void 스트라이크_테스트() {
+        Game baseballGame = new Game();
+        String answer = getAnswer(baseballGame);
+        String strike1 = answer.substring(0, 1) + "00";
+        String strike2 = answer.substring(0, 2) + "0";
+        String strike3 = answer;
+        baseballGame.operate(strike1);
+        assertThat(output()).contains("1스트라이크");
+        baseballGame.operate(strike2);
+        assertThat(output()).contains("2스트라이크");
+        baseballGame.operate(strike3);
+        assertThat(output()).contains("3스트라이크");
+    }
+
+    @Test
+    void 볼_테스트() {
+        Game baseballGame = new Game();
+        String answer = getAnswer(baseballGame);
+        String ball1 = answer.substring(1, 2) + "00";
+        String ball2 = answer.substring(1, 3) + "0";
+        String ball3 = answer.substring(1, 3) + answer.charAt(0);
+        baseballGame.operate(ball1);
+        assertThat(output()).contains("1볼");
+        baseballGame.operate(ball2);
+        assertThat(output()).contains("2볼");
+        baseballGame.operate(ball3);
+        assertThat(output()).contains("3볼");
+    }
+
+    @Test
+    void 낫싱_테스트() {
+        Game baseballGame = new Game();
+        baseballGame.operate("000");
+        assertThat(output()).contains("낫싱");
+    }
 
     @Test
     void 게임종료_후_재시작() {
