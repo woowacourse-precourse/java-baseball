e177f69ff81c462150d9863aabcdfa0f053d93c4
c1ce <chalicenuclear.gmail.com>
2022-11-07 13:54:13 +0900

    feat : Strike, Ball 카운팅값을 유저에게 결과화면 표시

---
+++ src/main/java/baseball/GameResult.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import java.util.Map;
+
+public class GameResult {
+    public static boolean resultCase(Map<String,Integer> result) {
+
+        if(result.get("strike")==0){
+            System.out.println(result.get("ball")+ "볼");
+            return false;
+        } else if (result.get("strike")==3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }else if (result.get("ball")==0) {
+            System.out.println(result.get("strike")+"스트라이크");
+            return false;
+        }
+        else{
+            System.out.println(result.get("strike")+"스트라이크 "+result.get("ball")+ "볼");
+            return false;
+        }
+
+    }
+}
