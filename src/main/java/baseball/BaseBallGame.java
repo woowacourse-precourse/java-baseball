7be0314606a24eb3c92e452ae314d3dd8dfd934f
Jeeseob <jeeseob5761@gmail.com>
2022-11-07 16:42:55 +0900

    Add: 게임 구현부 - 게임 실행 함수 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,17 +1,25 @@
 package baseball;
 
+import java.util.List;
+
 /**
  * @Author : Jeeseob
  * @CreateAt : 2022/11/07
  */
 public class BaseBallGame {
     // TODO: 게임 실행부 작성
+    private static final int LENGTH_OF_NUMBER = 3;
     public void start() {
         // TODO: 게임 진행시 처음에만 출력되는 Comment 출력
-        while (isFinished()) {
+        List<Integer> answer = BaseBallGameUtil.createAnswerByLength(LENGTH_OF_NUMBER);
+        List<Integer> userAnswer;
+        do {
             // TODO: 게임 로직 작성
+            userAnswer = BaseBallGameUtil.inputUserAnswer();
+            BallCount ballCount = new BallCount(answer, userAnswer);
+            System.out.println(ballCount.toString());
             // TODO: 게임 진행 중 콘솔로 출력되는 Comment 출력
-        }
+        } while(isFinished());
     }
 
     // TODO: 게임 종료 여부 판단
