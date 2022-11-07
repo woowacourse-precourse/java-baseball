accd793d5b9e7bf2a2e1d1aa8e3dd83ccce04cdf
wnsgml7267 <dlwns7267@naver.com>
2022-11-08 03:54:22 +0900

    feat(model/Constant): 상수를 저장할 클래스 생성

---
+++ src/main/java/baseball/model/Constant.java
@@ -0,0 +1,7 @@
+package baseball.model;
+
+public final class Constant {
+    public static final String RESTART = "1";
+    public static final String QUIT = "2";
+    public static final int NUMS_LENGTH = 3;
+}
