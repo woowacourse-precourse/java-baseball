a24be5b553533a77774b30c3a9644b1238acd79a
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-08 03:54:03 +0900

    test: 게임 리셋 시 컴퓨터 난수 생성이 제대로 다르게 나왔는지 확인하는 테스트 코드 작성

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -4,6 +4,8 @@ import baseball.model.Computer;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ComputerTest {
@@ -16,5 +18,19 @@ public class ComputerTest {
         final int COUNT = 3;
         computer.randomGenerate();
         assertThat(computer.getComputerNumberList().size()).isEqualTo(COUNT);
+    }
+
+    @Test
+    @DisplayName("게임 초기화시 난수 다르게 생성 됐는지 테스트")
+    void resetGame() {
+        computer.randomGenerate();
+        List<Integer> beforeReset = computer.getComputerNumberList();
+        String beforeResetString = String.join("", beforeReset.toString());
+
+        computer.resetGame();
+        List<Integer> afterReset = computer.getComputerNumberList();
+        String afterResetString =  String.join("", afterReset.toString());
+
+        assertThat(beforeResetString).isNotEqualTo(afterResetString);
     }
 }
