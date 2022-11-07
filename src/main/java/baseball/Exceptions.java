dfe2b4694fd780667d293e28a4b2bd8909df5e54
KimYongJun <rladydwns129@naver.com>
2022-11-07 21:49:16 +0900

    feat: 유저 입력을 받아 비교 가능한 형태로 변환하는 기능 추가

--- src/main/java/baseball/Exceptions.java
+++ src/main/java/baseball/Exceptions.java
@@ -3,25 +3,25 @@ package baseball;
 import java.util.HashSet;
 
 public class Exceptions {
-    public void checkLength(String s) {
+    public static void checkLength(String s) {
         if (s.length() != Application.NUMBER_LENGTH()) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("정해진 글자 수가 아닙니다.");
         }
     }
 
-    public void checkType(String s) {
+    public static void checkType(String s) {
         try {
             int stringToInteger = Integer.parseInt(s);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("정수 형태의 입력이 아닙니다.");
         }
     }
 
-    public void checkDuplication(String s) {
+    public static void checkDuplication(String s) {
         HashSet<Character> WORD_SET = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
             if (WORD_SET.contains(s.charAt(i))) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("숫자가 중복되었습니다.");
             }
             WORD_SET.add(s.charAt(i));
         }
