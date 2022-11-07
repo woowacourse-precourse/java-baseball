13386a804b083d3697381af315e7ceb05ed8abd3
Kim Seung Beom <88074556+daily1313@users.noreply.github.com>
2022-11-07 19:12:14 +0900

    feat : printFirstNumberBaseballGameMessage 메서드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -10,6 +10,9 @@ public class InputView {
         System.out.println("게임을 다시 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
+    public static void printFirstNumberBaseballGameMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
 
 
 }
