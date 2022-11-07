4253a49719b0531de5a399d470b38800db97f708
BuildTools <unconfigured@null.spigotmc.org>
2022-11-08 04:58:15 +0900

    test: 추가 ApplicationTest 테스트케이스

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -21,11 +22,75 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 다양한_경우의_볼_스트라이크() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("618", "675", "163", "315", "138", "135", "2");
+                    assertThat(output()).contains("1볼", "1스트라이크", "1볼 1스트라이크",
+                            "2볼 1스트라이크", "2스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 예외_숫자야구게임중_잘못된_범위의_입력값() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 예외_숫자야구게임중_중복된_입력값() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("333"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_숫자야구게임중_숫자가아닌_입력값() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("뮤"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_게임종료_선택지중_잘못된_범위의_입력값_1() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                assertRandomNumberInRangeTest(
+                        () -> run("135", "3"),
+                        1, 3, 5
+                ));
+
+    }
+
+    @Test
+    void 예외_게임종료_선택지중_잘못된_범위의_입력값_2() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                assertRandomNumberInRangeTest(
+                        () -> run("135", "321"),
+                        1, 3, 5
+                ));
+    }
+
+    @Test
+    void 예외_게임종료_선택지중_숫자가아닌_입력값() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                assertRandomNumberInRangeTest(
+                        () -> run("135", "뮤"),
+                        1, 3, 5
+                ));
     }
 
     @Override
