5be6783ae4fc8f402dcd2670b736eea77b8d9e7b
Sangkyun Seo <tkdrbs0802@naver.com>
2022-11-07 20:17:53 +0900

    Change the 3strike output message

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -165,7 +165,7 @@ public class Application {
     	int coin = 0;
     	System.out.println("3스트라이크");
 		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		System.out.println("게임을 새로 시작하려면  1, 종료하려면 2를 입력하세요.");
 		coin = Integer.parseInt(Console.readLine());
 		return coin;
     }
