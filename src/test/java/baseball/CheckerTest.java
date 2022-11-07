e88f825fd591c8cac9a2ee5098ad5d8888b3ff5c
Dongyeol <eastheat10@gmail.com>
2022-11-07 16:19:04 +0900

    test: Checker 테스트 코드 추가

---
+++ src/test/java/baseball/CheckerTest.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CheckerTest {
+
+    Checker checker = new Checker();
+
+    @Test
+    @DisplayName("사용자가 맞췄는지 확인")
+    void isUserGuessRight() {
+        String counterpartInput = "123";
+        String userInput = "123";
+
+        assertThat(checker.isUserGuessRight(userInput, counterpartInput)).isTrue();
+    }
+
+    @Test
+    @DisplayName("사용자가 틀렸는지 확인")
+    void isUserGuessWrong() {
+        String counterpartInput = "123";
+        String userInput = "456";
+
+        assertThat(checker.isUserGuessRight(userInput, counterpartInput)).isFalse();
+    }
+
+}
