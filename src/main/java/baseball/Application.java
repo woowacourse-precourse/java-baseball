1fda4c0462e0534a9318823de9653cf724c5f962
ParkRootSeok <g_e_un@naver.com>
2022-11-07 23:08:25 +0900

    feat : 게임 재시작 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Application {
 
@@ -13,11 +16,26 @@ public class Application {
         return true;
     }
 
+    public static boolean restartGame() {
+
+        int answer;
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        answer = Integer.parseInt(Console.readLine());
+
+        if (answer == 1) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
     public static void main(String[] args) {
 
         boolean status = playGame();
 
-        while (status) {
+        while (status != false) {
 
         }
 
