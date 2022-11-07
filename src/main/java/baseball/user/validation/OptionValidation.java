6c54d6ab84e560752aab4d0a5247b801ac1a0248
kimminsu <xjfcnfw3@naver.com>
2022-11-07 09:49:11 +0900

    feat: [예외 처리추가] 옵션은 반드시 1(재시작)과 2(게임 종료)여야 한다.

--- src/main/java/baseball/user/validation/OptionValidation.java
+++ src/main/java/baseball/user/validation/OptionValidation.java
@@ -4,11 +4,19 @@ public class OptionValidation {
     private static final int OPTION_LENGTH = 1;
     public static void validate(String userInput) {
         validateLength(userInput);
+        validateOption(userInput);
     }
 
     public static void validateLength(String userInput) {
         if (userInput.length() != OPTION_LENGTH) {
             throw new IllegalArgumentException(ExceptionMessage.OPTION_LENGTH);
+        }
+    }
+
+    public static void validateOption(String userInput) {
+        int option = Integer.parseInt(userInput);
+        if ((option != 1) && (option != 2)) {
+            throw new IllegalArgumentException(ExceptionMessage.OPTION_TYPE);
         }
     }
 }
