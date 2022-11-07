25fcab0516930c19dacfa2efc24e4a1ff44a51bd
Cyma-s <ssol0319@naver.com>
2022-11-07 23:31:15 +0900

    feat(Constant): make Constant class that collects multiple constant values

---
+++ src/main/java/baseball/model/Constant.java
@@ -0,0 +1,36 @@
+package baseball.model;
+
+public class Constant {
+    public static final String INPUT_SIZE_EXCEPTION = "입력하는 수는 3자리여야 합니다";
+    public static final String DUPLICATE_NUMBER_EXCEPTION = "중복되지 않는 수를 입력해야 합니다";
+
+    public static final String INPUT_IS_NOT_NUMBER_EXCEPTION = "입력은 숫자로 주어져야 합니다";
+    public static final String OUT_OF_RANGE_NUMBER_EXCEPTION = "범위를 벗어난 숫자입니다";
+
+    public static final int START_RANDOM_NUMBER_RANGE = 1;
+    public static final int END_RANDOM_NUMBER_RANGE = 9;
+    public static final int RANDOM_NUMBERS_SIZE = 3;
+
+    public static final String START_BASEBALL_GAME = "숫자 야구 게임을 시작합니다.";
+    public static final String GAME_USER_INPUT = "숫자를 입력해주세요 : ";
+    public static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String END_INPUT_EMPTY_STRING = "빈 문자열을 입력하셨습니다";
+    public static final String END_INPUT_NOT_NUMBER = "숫자를 입력해주세요";
+    public static final String END_INPUT_INVALID_NUMBER_SIZE = "두 자리 이상 숫자를 입력하셨습니다";
+    public static final String END_INPUT_INVALID_END_TYPE = "1, 2 이외의 숫자를 입력하셨습니다";
+    public static final String END_INPUT_TYPE_RESTART = "1";
+    public static final String END_INPUT_TYPE_END = "2";
+
+    public static final int END_CODE_RESTART = 1;
+    public static final int END_CODE_END = 2;
+
+    public static final String BALL_STRING = "볼";
+    public static final String STRIKE_STRING = "스트라이크";
+    public static final String NOTHING_STRING = "낫싱";
+    public static final String SPACE_STRING = " ";
+    public static final String NULL_STRING = "";
+
+
+}
