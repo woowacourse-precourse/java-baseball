42484b232fb0f88611c7f4bd6da998b599851644
jsc1014 <wotlr5209@gmail.com>
2022-11-07 17:12:02 +0900

    feat : 게임 시작문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,4 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+    private static void gameStart(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
 }
