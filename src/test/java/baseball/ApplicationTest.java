2386e5d90aac9bf78ad36489c9577ec71e97dd1c
HaODay <ycis5013@naver.com>
2022-11-08 00:48:41 +0900

    test(예외_테스트2): 플레이어의 입력이 숫자 3개가 아닌 경우 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -39,6 +39,14 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 예외_테스트2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
