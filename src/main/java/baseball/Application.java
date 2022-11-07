78c454a5b26a34350b5943bd4f8a21ebaf6b14b3
gwj0421 <gwj0421@gmail.com>
2022-11-07 09:54:58 +0900

    feat: 기능 요구사항 3번(사용자의 수 입력) 기능 추가
    사용자의 수를 입력받는데 아래와 같은 예외상황일 경우 IllegalArgumentException 발생시킴
    - 입력받은 수가 없을 경우
    - 입력받은 수가 정수가 아닐 경우
    - 입력받은 수의 각각의 자리수가 겹칠 경우

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 public class Application {
@@ -9,11 +12,11 @@ public class Application {
         // 기능 요구사항 1번
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
-    private List<Integer> make_RandomNum(int maxLength) {
+    private List<String> make_RandomNum(int maxLength) {
         // 기능 요구사항 2번
-        List<Integer> computerNum = new ArrayList<>();
+        List<String> computerNum = new ArrayList<>();
         while (computerNum.size() < maxLength) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            String randomNumber = Integer.toString(Randoms.pickNumberInRange(1, 9));
             if (!computerNum.contains(randomNumber)) {
                 computerNum.add(randomNumber);
             }
@@ -21,8 +24,23 @@ public class Application {
         return computerNum;
     }
 
-    private void input_PlayerNum() {
+    private List<String> input_PlayerNum(int maxLength) {
         // 기능 요구사항 3번
+        String playerNum_string = Console.readLine();
+        if (check_PlayerNum(playerNum_string, maxLength)) {
+            String[] playerNum_array = playerNum_string.split("");
+            return new ArrayList<>(Arrays.asList(playerNum_array));
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private boolean check_PlayerNum(String num,int maxLength) {
+        String[] check_array = num.split("");
+        HashSet<String> check_set = new HashSet<>(Arrays.asList(check_array));
+        return !num.isEmpty() && num.matches("^[1-9]*$") && num.length() == maxLength
+                && check_set.size() == num.length();
+
     }
 
     private void get_BallAndStrikeCount() {
@@ -39,19 +57,20 @@ public class Application {
 
     private void play() {
         start_game();
+        while (true) {
+            int maxLength = 3;
+            List<String> computerNum = make_RandomNum(maxLength);
 
-        int maxLength = 3;
-        List<Integer> computerNum = make_RandomNum(maxLength);
+            List<String> playerNum = input_PlayerNum(maxLength);
 
-        input_PlayerNum();
-        get_BallAndStrikeCount();
-        progress_GameRule();
-        close_game();
+            get_BallAndStrikeCount();
+            progress_GameRule();
+            close_game();
+        }
     }
 
     public static void main(String[] args) {
         Application application = new Application();
         application.play();
-
     }
 }
