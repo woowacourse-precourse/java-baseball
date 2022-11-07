ab407fa7aa85d30184ca2ea80f6e2e95c75a26d1
jungle <minuet1215@jbnu.ac.kr>
2022-11-07 17:09:14 +0900

    Feat(NumberValidator): 입력 값 검증 메서드 추가

---
+++ src/main/java/baseball/NumberValidator.java
@@ -0,0 +1,60 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NumberValidator {
+    private static final String IS_EMPTY_MESSAGE = "numbers cannot be empty.";
+    private static final String IS_OUT_OF_RANGE_MESSAGE = "You must enter a number between 1 and 9.";
+    private static final String IS_OVER_LENGTH_MESSAGE = "enter a three-digit number.";
+    private static final String IS_DUPLICATED_MESSAGE = "The number is duplicated.";
+    private static final int BASEBALL_COUNT = 3;
+
+    public void validateInput(String userNumbers) {
+        validateIsEmptyNumbers(userNumbers);
+        validateInputCount(userNumbers);
+        validateInputRanges(userNumbers);
+        validateIsDuplicate(userNumbers);
+    }
+
+    public void validateIsEmptyNumbers(final String numbers) {
+        if (numbers.isEmpty()) {
+            throw new IllegalArgumentException(IS_EMPTY_MESSAGE);
+        }
+    }
+
+    public void validateInputRanges(final String numbers) {
+        for (char number : numbers.toCharArray()) {
+            if (!isNumber(number) || isZeroNumber(number)) {
+                throw new IllegalArgumentException(IS_OUT_OF_RANGE_MESSAGE);
+            }
+        }
+    }
+
+    private boolean isNumber(final char number) {
+        return Character.isDigit(number);
+    }
+
+    private boolean isZeroNumber(final char number) {
+        return number == '0';
+    }
+
+
+    public void validateInputCount(final String count) {
+        if (count.length() != BASEBALL_COUNT) {
+            throw new IllegalArgumentException(IS_OVER_LENGTH_MESSAGE);
+        }
+    }
+
+    public void validateIsDuplicate(final String numbers) {
+        List<Integer> inputNumbers = new Converter().convertStringToListInteger(numbers);
+        List<Integer> duplicateCheckNumberList = new ArrayList<>();
+
+        for (int number : inputNumbers) {
+            if (duplicateCheckNumberList.contains(number)) {
+                throw new IllegalArgumentException(IS_DUPLICATED_MESSAGE);
+            }
+            duplicateCheckNumberList.add(number);
+        }
+    }
+}
