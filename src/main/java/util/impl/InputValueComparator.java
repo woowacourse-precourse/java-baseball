265e49db8a7057d50b7feb59d1654e3cb3da6326
Identityless <rlandwnd555@naver.com>
2022-11-08 01:10:47 +0900

    feat : Comparator 인터페이스 구현체 InputValueComparator 추가

---
+++ src/main/java/util/impl/InputValueComparator.java
@@ -0,0 +1,16 @@
+package util.impl;
+
+import util.Comparator;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputValueComparator implements Comparator {
+    @Override
+    public List<Integer> doIntCompare(List<Integer> standard, List<Integer> target) {
+        List<Integer> result = new ArrayList<>();
+
+        return null;
+    }
+
+}
