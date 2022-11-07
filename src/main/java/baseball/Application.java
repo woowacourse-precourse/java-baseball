e63b47181b863eb86b1d9c3614166c075ffdb7f8
KimYongJun <rladydwns129@naver.com>
2022-11-07 20:31:55 +0900

    feat: BaseBallNumber 클래스 작성 및 원시값 포장

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Application {
@@ -32,6 +33,5 @@ public class Application {
         }
         System.out.println("correct");
     }
-
 
 }
