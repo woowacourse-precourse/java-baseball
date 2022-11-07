a70b7ee5b3dac371af34ba6a14cef8f06b4d9a19
nahyeon cho <whskgus12@nate.com>
2022-11-07 20:05:46 +0900

    feat : 입력한 숫자에 대해 힌트 띄우기
    GameHintList 클래스의 printGameHintList를 통해 스트라이크, 볼 개수를 출력합니다.

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -4,18 +4,29 @@ import java.util.ArrayList;
 
 public class GameController {
     static int gameCount = 0; // "숫자 야구 게임을 시작합니다"는 한번만 출력
-    MakeThreeDigitNumber makeThreeDigitNumber = new MakeThreeDigitNumber();
 
+    MakeThreeDigitNumber makeThreeDigitNumber = new MakeThreeDigitNumber();
     ArrayList<String> computerSelected = makeThreeDigitNumber.computerSelectedNumber();
     ArrayList<String> userSelected;
 
     public void startGame() {
         if (gameCount == 0) System.out.println("숫자 야구 게임을 시작합니다.");
+
         do {
             userSelected = makeThreeDigitNumber.userSelectedNumber();
             PlayerInputValueExceptions.allInputValueException(userSelected);
 
-        } while (GameHintList.countOfStrike() != 3);
+            GameHintList gameHintList = new GameHintList();
+            GameHintList.computerSelectedNumbers = computerSelected;
+            GameHintList.userSelectedNumbers = userSelected;
 
+            if (gameHintList.isNothingSame()) {
+                System.out.println("낫싱");
+            } else {
+                gameHintList.printGameHintList();
+            }
+
+        } while (GameHintList.countOfStrike() != 3);
+        gameCount++;
     }
 }
