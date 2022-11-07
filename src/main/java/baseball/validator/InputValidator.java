b777f16d8618c668265ba7729b28293d43b32490
TaeHwan <kimta2hwan@gmail.com>
2022-11-07 13:29:59 +0900

    feat(inputvalidator): 사용자가 입력한 3자리의 정수에 0이 없는지 검증하는 기능 구현

--- src/main/java/baseball/validator/InputValidator.java
+++ src/main/java/baseball/validator/InputValidator.java
@@ -5,22 +5,33 @@ import baseball.exception.InputException;
 public class InputValidator {
 
     private static final String INTEGER_REGULAR_EXPRESSION = "^[0-9]*$";
+    private static final String ZERO = "0";
     private static final int CORRECT_INPUT_LENGTH = 3;
+    private static String guessedAnswerInput;
 
     public static void validateInputForm(String guessedAnswer) {
-        validateNotIntegerInput(guessedAnswer);
-        validateNotThreeLengthIntegerInput(guessedAnswer);
+        guessedAnswerInput = guessedAnswer;
+
+        validateNotIntegerInput();
+        validateNotThreeLengthIntegerInput();
+        validateIncludeZeroInput();
     }
 
-    public static void validateNotIntegerInput(String guessedAnswer) {
-        if (!guessedAnswer.matches(INTEGER_REGULAR_EXPRESSION)) {
+    public static void validateNotIntegerInput() {
+        if (!guessedAnswerInput.matches(INTEGER_REGULAR_EXPRESSION)) {
             InputException.throwNotIntegerInputError();
         }
     }
 
-    public static void validateNotThreeLengthIntegerInput(String guessedAnswer) {
-        if (guessedAnswer.length() != CORRECT_INPUT_LENGTH) {
+    public static void validateNotThreeLengthIntegerInput() {
+        if (guessedAnswerInput.length() != CORRECT_INPUT_LENGTH) {
             InputException.throwNotThreeLengthIntegerInputError();
+        }
+    }
+
+    public static void validateIncludeZeroInput() {
+        if (guessedAnswerInput.contains(ZERO)) {
+            InputException.throwIncludeZeroInputError();
         }
     }
 }
