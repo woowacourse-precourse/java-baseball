5d597929cfa307cb713865890d89e2a12defba06
SproutMJ <mjk54565@gmail.com>
2022-11-07 14:50:38 +0900

    feat: 사용자로부터 게임을 진행하며 서로다른 숫자 3개를 입력받는 기능, 게임 종료 후 다음 게임 진행 여부를 입력받는 기능 docs: 기능 목록 1 수정, 6 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,37 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static List<Integer> readGameStep(){
+        String nextStep = Console.readLine();
+        if(nextStep.length() != 3) throw new IllegalArgumentException();
+
+        List<Integer> human = new ArrayList<>();
+        for (int i = 0; i < 3; i++){
+            int val = (int)(nextStep.charAt(i)-'0');
+            if(!human.contains(val)){
+                human.add(val);
+            }else{
+                throw new IllegalArgumentException("게임 진행 입력 오류");
+            }
+        }
+
+        return human;
+    }
+
+    private static int readEndStep(){
+        String endStep =  Console.readLine();
+        int selectedNum = (int)(endStep.charAt(0) - '0');
+
+        if(selectedNum == 1 || selectedNum == 2) return selectedNum;
+        else throw new IllegalArgumentException("끝내기 입력 오류");
     }
 }
