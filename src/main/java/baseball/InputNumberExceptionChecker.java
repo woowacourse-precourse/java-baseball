2761e13e8aac68c7e0c9217c52423ff59f11c849
dener8 <8dener@gmail.com>
2022-11-08 04:38:44 +0900

    feat: 사용자가 입력한 숫자의 길이가 3이 맞는지 판단하는 isValidLength() 구현

--- src/main/java/baseball/InputNumberExceptionChecker.java
+++ src/main/java/baseball/InputNumberExceptionChecker.java
@@ -2,7 +2,13 @@ package baseball;
 
 public class InputNumberExceptionChecker {
     // 3글자가 아닐 때
-
+    public boolean isValidLength(String number) {
+        if (number.length() != 3) {
+            // 여기서 안내 메세지(GameMessage)도 출력해주면 좋을 듯?!
+            throw new IllegalArgumentException();
+        }
+        return true;
+    }
     // 모두 다른 수가 아닐 때
 
     // 재시작 여부를 물었을 때, 1과 2가 아닌 다른 숫자를 입력했을 때
