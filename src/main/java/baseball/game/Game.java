a78891908f89d22ed2cf97ef3ba547762c0a1d6b
jihwan <sgh002400@naver.com>
2022-11-08 01:58:22 +0900

    feat(Game): Game 객체 및 상태 초기화

---
+++ src/main/java/baseball/game/Game.java
@@ -0,0 +1,18 @@
+package baseball.game;
+
+public class Game {
+	private static final int MAX_STRIKE_COUNT = 3;
+	private final int strikeCount;
+	private final int ballCount;
+	private GameStatus gameStatus = GameStatus.GAME_CONTINUE;
+
+	private Game(int strikeCount, int ballCount) {
+		this.strikeCount = strikeCount;
+		this.ballCount = ballCount;
+	}
+
+	public static Game from(int strikeCount, int ballCount) {
+		return new Game(strikeCount, ballCount);
+	}
+
+}
