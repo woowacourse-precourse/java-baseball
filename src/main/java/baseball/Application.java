9bf81250cb7616823a64f8d9f394c145db5ee69d
Anthologia <pyoungjy@gmail.com>
2022-11-08 00:58:03 +0900

    feat: 사용자로부터 야구 숫자를 문자열로 입력받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,14 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+
     public static void main(String[] args) {
-            printGameStartMessage();
+        printGameStartMessage();
+
+        System.out.print("숫자를 입력해주세요 : ");
+        String userNumber = Console.readLine();
     }
 
     private static void printGameStartMessage() {
