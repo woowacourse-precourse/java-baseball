e69adce2c473230909e313ee691e7feea0a24c06
Your Name <dbsdbxkrzz@naver.com>
2022-11-07 16:03:11 +0900

    docs: Message 클래스에 문구를 추가합니다
    예외 상황에 대한 안내문구를 추가합니다

--- src/main/java/baseball/Message.java
+++ src/main/java/baseball/Message.java
@@ -8,7 +8,10 @@ public enum Message {
 	BALL("볼"),
 	STRIKE("스트라이크"),
 	NOTHING("낫싱"),
-	INVALID_INPUT("올바르지 않은 입력입니다. 게임을 종료합니다.");
+	USER_INPUT_NOT_NUMBER("숫자만 입력 가능합니다. "),
+	USER_INPUT_LENGTH_INVALID("3자리만 입력 가능합니다. "),
+	USER_INPUT_DUPLICATED("중복된 숫자가 존재합니다. "),
+	INVALID_INPUT_END_GAME("올바르지 않은 입력입니다. 게임을 종료합니다.");
 
 	private final String msg;
 	Message(String msg){
