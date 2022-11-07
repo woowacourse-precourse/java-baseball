fdf533cc79e2e24352a946b26a9bc19c614ab7aa
gumgu <bienlee@naver.com>
2022-11-07 18:07:40 +0900

    Feat: 재시작 여부 확인
    - 재시작 여부 확인 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -28,6 +28,16 @@ public class Application {
 
         }
 
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String choice = Console.readLine();
+        if(choice.equals("1")) {
+            playBaseball();
+        } else {
+            System.out.println("게임을 종료합니다.");
+        }
+
 
     }
 
