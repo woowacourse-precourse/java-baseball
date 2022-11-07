0ba43478ade9adc984d67a02cee6f0b2fa0aac3d
Park shinyeong <syngh503@gmail.com>
2022-11-07 18:33:14 +0900

    Feat(Game): (기능5) 게임 재시작 여부 확인 기능 구현
    게임 재시작 여부를 확인하는 isGameRestart 함수를 구현하였습니다.
    입력값이 1인 경우 게임 시작, 2인 경우 게임을 종료합니다.
    그 외의 값인 경우 1과 2중 하나만 입력하세요 라는 메세지와 함께 예외 처리됩니다.
    
    Relate to: #8

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,21 +1,42 @@
 package baseball;
 
+import java.util.InputMismatchException;
+
 public class Game {
-    private Integer gameStatus;
+    private boolean gameStatus;
+    private final String ERRORMESSAGE = "1과 2중 하나만 입력하세요";
 
-    public Integer getGameStatus() {
-        return gameStatus;
+    public boolean getGameStatus() {
+        return this.gameStatus;
     }
 
-    public void setGameStatus(Integer gameStatus) {
-        this.gameStatus = gameStatus;
+    public void isGameRestart(String playerInput) {
+        try {
+            int inputNum = Integer.parseInt(playerInput);
+            if (inputNum == 1) {
+                this.startGame();
+                return;
+            }
+            if(inputNum == 2) {
+                endGame();
+                return;
+            }
+            throw new InputMismatchException(ERRORMESSAGE);
+
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException(ERRORMESSAGE);
+        }
     }
 
-    public void isGameRestart(Integer playerInput) {
+    public void startGame() {
+        this.gameStatus = true;
+    }
 
+    public void endGame() {
+        this.gameStatus = false;
     }
 
     public boolean isValidInput(String playerInput) {
-        return true;
+
     }
 }
