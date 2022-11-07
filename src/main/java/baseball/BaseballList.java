5bb519242be661cb6ace9d0445290a182fd66986
kwondaehwi <kwondh8561@ajou.ac.kr>
2022-11-07 16:58:05 +0900

    Refactor: BaseballList 생성자 List<Integer>를 받도록 수정

--- src/main/java/baseball/BaseballList.java
+++ src/main/java/baseball/BaseballList.java
@@ -1,15 +1,18 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class BaseballList {
     private final List<Baseball> baseballList;
 
-    public BaseballList(List<Baseball> baseballList) {
-        this.baseballList = baseballList;
+    public BaseballList(List<Integer> baseballListInput) {
+        this.baseballList = baseballListInput.stream()
+                .map(baseball -> new Baseball(baseball))
+                .collect(Collectors.toList());
     }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
