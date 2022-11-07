e3cf7ba677ec11758432814f33c332189bc0fd3d
miffyKing <whqudghk28@gmail.com>
2022-11-07 14:35:39 +0900

    refactor: Change order of if - else if to be more easier to see.

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -19,14 +19,13 @@ public class Referee {
         if (strike == 0 && ball == 0) {
             return "낫싱";
         }
-        if (ball != 0 && strike != 0) {
-            return (ball + "볼 " + strike + "스트라이크");
+
+        if (ball == 0 && strike != 0) {
+            return (strike + "스트라이크");
         } else if (ball != 0 && strike == 0) {
             return (ball + "볼");
-        } else if (ball == 0 && strike != 0) {
-            return (strike + "스트라이크");
         }
-        return "";
+        return (ball + "볼 " + strike + "스트라이크");
     }
 
     private static int calculateBall(String userInput, RandomNumber randNum) {
@@ -42,8 +41,8 @@ public class Referee {
         return strikeCount;
     }
 
-    public static String testReferee(String input, RandomNumber randomNum) {
-        randNum = randomNum;
+    public static String testReferee(String input, RandomNumber randNum) {
+        randomNum = randNum;
         return calculateAnswer(input);
     }
 }
