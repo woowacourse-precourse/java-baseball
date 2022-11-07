df73764a97b49a2537bc491ce3d18917462790ce
김태용 <amico741@naver.com>
2022-11-08 00:56:18 +0900

    test: 컴퓨터 랜덤으로 뽑은 숫자 테스트 코드 추가
    길이가 3이 아니라면 테스트 실패 발생

---
+++ src/test/java/baseball/ComputerTest.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ComputerTest {
+  Computer computer;
+  static final int MAX_SIZE = 3;
+
+  @BeforeEach
+  void setUp(){
+    computer = new Computer();
+  }
+
+  @Test
+  @DisplayName("랜덤으로 뽑은 숫자 길이 확인하는 테스트")
+  void testWithComputerSourceLength(){
+    assertThat(computer.getNumbers().size())
+            .as("숫자 길이가 3이 아닙니다.")
+            .isEqualTo(MAX_SIZE);
+  }
+}
