6da76fa1af3e518e5a20250eb8ae73fdb7988395
seoyoung <sy9723@korea.ac.kr>
2022-11-08 00:28:10 +0900

    test: calculate the number of strikes

--- src/test/java/baseball/service/GameServiceTest.java
+++ src/test/java/baseball/service/GameServiceTest.java
@@ -1,5 +1,15 @@
 package baseball.service;
 
-class GameServiceTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+class GameServiceTest {
+    @Test()
+    public void 스트라이크의_개수를_계산한다() {
+        int computerNumber = 234;
+        int userNumber = 124;
+        int strike = GameService.getStrikeNumber(userNumber, computerNumber);
+        assertEquals(1, strike);
+    }
 }
