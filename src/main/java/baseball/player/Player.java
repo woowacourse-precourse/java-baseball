a555956328875855b9dc984f5eb7f1390ac95120
SeokHwan An <asok369258@naver.com>
2022-11-07 11:47:02 +0900

    feat : 플레이어가 게임을 재시작할지 종료할지 응답하는 기능 추가

--- src/main/java/baseball/player/Player.java
+++ src/main/java/baseball/player/Player.java
@@ -12,4 +12,10 @@ public class Player {
         return Console.readLine();
     }
 
+    /**
+     * 플레이어가 재시작할지 종료할지 응답하는 기능
+     */
+    public String restartOrExit() {
+        return Console.readLine();
+    }
 }
