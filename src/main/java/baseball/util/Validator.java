946c532174f7fd7a3e701ef6ada7bb3fdd7358c0
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-11-07 13:35:09 +0900

    ADD : 유저가 입력한 숫자의 길이가 3이 맞는지 확인 및 예외발생

--- src/main/java/baseball/util/Validator.java
+++ src/main/java/baseball/util/Validator.java
@@ -5,5 +5,18 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Validator {
+    private static final String END_SELECT = "2";
+    private static final String RESTART_SELECT = "1";
+    private static final int NUMBER_LENGTH = 3;
+    private static final int MIN_NUMER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final String SPLIT_STRING = "";
+    private static final String NUMBER_PATTERN = "^[" +MIN_NUMER + "-" + MAX_NUMBER + "]*$";
+    private static final String WRONG_NUMBER = "[ERROR} 잘못된 입력입니다.";
+
+    public void isRightLength(String numbers){
+        if(numbers.length() != NUMBER_LENGTH)
+            throw new IllegalArgumentException(WRONG_NUMBER);
+    }
 
 }
