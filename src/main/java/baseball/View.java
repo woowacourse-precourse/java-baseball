5a49e227f219be51f6f6bb1f760b0eb63aaea5bb
yoon <beatus96@naver.com>
2022-11-08 04:32:48 +0900

    feat(view) : 추가 "입력값이 null인 경우 예외 발생하기"
    + 자바 컨벤션에 따라 if문의 명령 블록 줄바꿈하기

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -4,20 +4,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class View {
     private static final String START ="숫자 야구 게임을 시작합니다.";
-    private static final String INPUT ="숫자를 입력해주세요 : ";
+    private static final String ASK_INPUT ="숫자를 입력해주세요 : ";
     private static final String RESULT_CORRECT ="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String ASK_KEEP_PLAYING ="게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    public static String getUserInput() throws IllegalArgumentException {
+    public static void printStart(){
         System.out.println(START);
-        System.out.print(INPUT);
+    }
+
+    public static String getUserInput() throws IllegalArgumentException {
+        System.out.print(ASK_INPUT);
         String input = Console.readLine().trim();
-        if (!Validator.isNumber(input)) {
+        if (!Validator.isNumber(input) || Validator.isNull(input)) {
             throw new IllegalArgumentException();
         }
         return input;
     }
-
 
     public static void printHint(String hint) {
         System.out.println(hint);
@@ -26,8 +28,11 @@ public class View {
     public static String getExitInput() throws IllegalArgumentException {
         System.out.println(RESULT_CORRECT);
         System.out.println(ASK_KEEP_PLAYING);
+
         String input = Console.readLine().trim();
-        if (!Validator.isValidCommandNumber(input)) throw new IllegalArgumentException();
+        if (!Validator.isValidCommandNumber(input)) {
+            throw new IllegalArgumentException();
+        }
         return input;
     }
 }
