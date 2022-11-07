f350f62c114f9828d74b7b938f63d249792cc400
Woojin Lee <dhkep03@gmail.com>
2022-11-07 17:11:36 +0900

    feat(OutputView): 게임 시작 안내 멘트 출력

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -1,11 +1,13 @@
 package baseball.controller;
 
 import baseball.type.ResultGame;
+import baseball.view.OutputView;
 
 public class Game {
     private static ResultGame resultGame = ResultGame.Repeat;
 
     static void repeatToEnd() {
+        OutputView.informStartGame();
         while (checkCanRepeat()) {
             playOneTime();
         }
