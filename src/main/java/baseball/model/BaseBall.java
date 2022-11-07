4306d33361aedde21f72de138c48021d21bf2688
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-07 23:48:19 +0900

    feat(BaseBall): strike, ball 정보를 가지고 있는 모델 작성

---
+++ src/main/java/baseball/model/BaseBall.java
@@ -0,0 +1,11 @@
+package baseball.model;
+
+public class BaseBall {
+    private int strike;
+    private int ball;
+
+    public BaseBall() {
+        this.strike = 0;
+        this.ball = 0;
+    }
+}
