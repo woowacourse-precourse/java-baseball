a9677909ddebc033924a63efb1a21f3b3d7e30a6
Yejin Shin <shinyejin0212@gmail.com>
2022-11-07 21:38:30 +0900

    feat($SystemMessage): add system massage

---
+++ src/main/java/baseball/SystemMessage.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class SystemMessage {
+    public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+    public static final String REQUEST_INPUT_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String STRIKE_AND_GAME_OVER = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String REQUEST_RESTART_OR_GAME_OVER = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static final String INVALID_LENGTH = "세 자리를 입력해야 합니다.";
+    public static final String INVALID_NUMBER = "1-9 사이의 숫자를 입력해야 합니다.";
+    public static final String DUPLICATE_NUMBER = "서로 다른 3가지 숫자를 입력해야 합니다.";
+    public static final String INVALID_STATEMENT = "1 또는 2를 입력해야 합니다.";
+
+}
