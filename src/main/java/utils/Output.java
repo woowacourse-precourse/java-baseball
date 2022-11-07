af4635f47b9493667aef83241194b70d915ddc81
twoosky <lhjae3@naver.com>
2022-11-08 01:21:01 +0900

    feat(output): 완전한 게임 종료 시 게임 종료 문구 출력 구현

--- src/main/java/utils/Output.java
+++ src/main/java/utils/Output.java
@@ -44,4 +44,8 @@ public class Output {
     public static void restart() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+
+    public static void endGame() {
+        System.out.println("게임 종료");
+    }
 }
