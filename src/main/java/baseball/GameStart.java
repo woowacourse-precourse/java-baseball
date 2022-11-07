ff2a72abba614e4f09be7a6fd2e5ffc2951a0d2e
Torres-09 <dla612@naver.com>
2022-11-07 19:50:06 +0900

    feat : 랜덤 넘버 생성 기능

---
+++ src/main/java/baseball/GameStart.java
@@ -0,0 +1,26 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameStart {
+
+    public static List<Integer> makeRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
+    }
+
+    public GameStart() {
+        List<Integer> computer = makeRandomNumber();
+    }
+}
