0f15dea624cc5548e2e038b287208c4c4e5a31ce
JUNBEOM_LEE <bom1215@naver.com>
2022-11-07 15:02:18 +0900

    feat(Class Application): Make Answer Numbers
    Add Generate_Answer method to Class Application
    
    Breaking Change: Make Generate_Answer method to make answer numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    static List<Integer> Answer_Numbers = new ArrayList<>();
 
     public static void Start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         Generate_Answer();
         Input();
+    }
+
+    public static void Generate_Answer() {
+        if (Answer_Numbers.size() != 0) {
+            Answer_Numbers.clear();
+        }
+        while (Answer_Numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!Answer_Numbers.contains(randomNumber)) {
+                Answer_Numbers.add(randomNumber);
+            }
+        }
     }
     public static void main(String[] args) {
 
