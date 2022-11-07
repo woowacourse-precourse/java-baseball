94ed97961234958333f9d0000247511ace20bf32
yujaemin <woawoa0513@gmail.com>
2022-11-07 14:15:02 +0900

    feat: 난수 맞히기 - 숫자 입력 안내문 출력 함수 구현

--- src/main/java/baseball/NonPlayerCharacter.java
+++ src/main/java/baseball/NonPlayerCharacter.java
@@ -5,4 +5,8 @@ public class NonPlayerCharacter {
     public void printGameStartNotice() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void printNumberInputNotice() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
 }
