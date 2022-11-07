563418239ebd5fc248060be2c2d11e5101ec3417
이건회 <rg970604@naver.com>
2022-11-08 04:05:32 +0900

    refactor(UserInput.java) : 문자열을 정수형 리스트로 반환하는 기능 이동

--- src/main/java/baseball/view/UserInput.java
+++ src/main/java/baseball/view/UserInput.java
@@ -3,16 +3,28 @@ package baseball.view;
 import static java.lang.System.out;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class UserInput {
 
-    public static String InputUserNumbers() {
+    public static List<Integer> InputUserNumbers() {
         out.println("숫자를 입력해주세요 : ");
-        return Console.readLine();
+        String userNumbersInput = Console.readLine();
+        return toIntegerList(userNumbersInput);
+    }
+
+    private static List<Integer> toIntegerList(String userNumbersInput) {
+        String[] userNumbers = userNumbersInput.split("");
+        return Arrays.stream(userNumbers)
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
     }
 
     public static String SelectRestartOrExit() {
         out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
+
 }
