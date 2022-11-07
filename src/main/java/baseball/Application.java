543e01c7d52ff3a8a71a307b619c2d0a896cf0a8
jeongyeonKimm <wjddus9132@gmail.com>
2022-11-07 20:46:05 +0900

    feat: add createRandomNumber

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     private static void playGame() {
+        String answer = createRandomNumber();
+        boolean isAnswer = false;
+
         System.out.println("숫자 야구 게임을 시작합니다.");
-        // TODO: 게임 진행
+
+        while (!isAnswer) {
+            // TODO: 게임 진행
+        }
+    }
+
+    private static String createRandomNumber() {
+        String answer = new String();
+
+        while (answer.length() < 3) {
+            String randomNumber = String.valueOf(Randoms.pickNumberInRange(1, 9));
+            if (!answer.contains(randomNumber)) {
+                answer += randomNumber;
+            }
+        }
+
+        return answer;
     }
 
     public static void main(String[] args) {
