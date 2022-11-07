4faf9a208aa9a30288671e71bd85b6a32476037c
patronum98 <zkrhklkl98@gmail.com>
2022-11-08 06:41:21 +0900

    feat(apply): 숫자 입력받음

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
@@ -19,6 +20,12 @@ public class Application {
         }
 
         System.out.println(computer);
+
+        System.out.print("숫자를 입력해 주세요 : ");
+        Scanner sc = new Scanner(System.in);
+
+        int input = sc.nextInt();
+        System.out.println(input);
 
     }
 }
