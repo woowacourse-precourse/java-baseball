9ca0716b0b91a58fd69940867a19284d6cfd4bbb
ingpyo <juninpyo72@gmail.com>
2022-11-07 11:47:13 +0900

    feat:게임 실행 함수를 불러오는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("숫자 야구 게임을 시작합니다.");
-
+		
+		Controller controller = new Controller();
+		controller.run();
 	}
 }
