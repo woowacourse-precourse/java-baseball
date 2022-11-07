8b025a48841661edf37efdfdd07e6abbb34ff0b7
0chil <0@chll.it>
2022-11-07 21:46:52 +0900

    fix(view): 게임 결과 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,12 +2,33 @@ package baseball.view;
 
 public class OutputView {
 
+    private static final String SINGLE_BLANK = " ";
+
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+
     public void printGameBegin() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
     public void printResult(Long balls, Long strikes) {
-        System.out.println(balls + "볼 " + strikes + "스트라이크");
+        System.out.println(getResultString(balls, strikes));
+    }
+
+    private String getResultString(Long ballCount, Long strikeCount) {
+        String ball = ballCount + BALL;
+        String strike = strikeCount + STRIKE;
+        if (ballCount > 0 && strikeCount > 0) {
+            return ball + SINGLE_BLANK + strike;
+        }
+        if (ballCount > 0) {
+            return ball;
+        }
+        if (strikeCount > 0) {
+            return strike;
+        }
+        return NOTHING;
     }
 
     public void printGameFinish() {
