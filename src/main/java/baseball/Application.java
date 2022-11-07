77e6cd5dc585379a5e27f058bd751db0fdd2000e
JunTaeINC <juntea3523@naver.com>
2022-11-07 15:38:10 +0900

    feat : 야구게임 재시작,종료 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        GameConsole game = new GameConsole();
-        game.gameStart();
+        GameConsole.gameStart();
     }
 }
