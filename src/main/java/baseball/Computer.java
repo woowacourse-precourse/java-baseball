3148972e5671452abbf25264da854870d464fd36
doehy <dbehxks02@naver.com>
2022-11-07 22:46:20 +0900

    feat : Computer 생성자에서 랜덤숫자를 담을 리스트 구현

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    public List<Integer> randomNumber;
+
+    public Computer(){
+        randomNumber = new ArrayList<>();
+    }
+}
