a3e335b31dfd84d961aa19a985f83da9929b48ae
eom-tae-in <eti0728@daum.net>
2022-11-07 18:58:00 +0900

    feat(AnswerNumber): getNewAnswer() -> 게임이 시작될 때마다 새로운 랜덤 정답 숫자를 생성하는 메서드

--- src/main/java/baseball/model/AnswerNumber.java
+++ src/main/java/baseball/model/AnswerNumber.java
@@ -1,5 +1,8 @@
 package baseball.model;
 
+import baseball.constant.Constant;
+import java.util.LinkedHashSet;
+
 public class AnswerNumber {
     private String answer;
 
@@ -9,5 +12,14 @@ public class AnswerNumber {
     public String getAnswer() {
 
         return this.answer;
+    }
+
+    public void getNewAnswer() {
+
+        LinkedHashSet<String> createAnswerNumber = new LinkedHashSet<>();
+        while (createAnswerNumber.size() < Constant.NUMBER_SIZE) {
+            createAnswerNumber.add(getRandomNumber());
+        }
+        this.answer = String.join("", createAnswerNumber);
     }
 }
