00254d777298c290fa03fb2b1131865343f39b1f
kkkwp <chxrryda@gmail.com>
2022-11-08 02:25:04 +0900

    chore(): TODO 문장 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,7 +12,6 @@ public class Application {
     public static final int GAMEOVER = 2;
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         do {
