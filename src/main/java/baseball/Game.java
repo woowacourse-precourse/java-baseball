7a74308ac4d497fb60c146b7c72839a58a104257
limecats <limecats0331@gmail.com>
2022-11-07 15:51:35 +0900

    feat(Game): add getGameResultOfJudge

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,6 +7,8 @@ import java.util.List;
 
 public class Game {
 
+    private final int END_STRIKE_COUNT = 3;
+
     public List<Integer> setComputersRandomAnswer() {
         List<Integer> answer = new ArrayList<>();
 
@@ -16,6 +18,31 @@ public class Game {
         }
 
         return answer;
+    }
+
+    public boolean getGameResultOfJudge(List<Integer> result) {
+        int ballCount = result.get(0);
+        int strikeCount = result.get(1);
+
+        if (strikeCount == END_STRIKE_COUNT) {
+            System.out.println("게임 종료");
+            return true;
+        }
+
+        if( ballCount == 0 && strikeCount == 0){
+            System.out.println("낫싱");
+            return false;
+        }
+
+        if(ballCount != 0){
+            System.out.print(ballCount + " 볼 ");
+        }
+        if(strikeCount != 0){
+            System.out.print(strikeCount + " 스트라이크");
+        }
+        System.out.println();
+
+        return false;
     }
 
     private int getRandomIntegerExcludeInList(List<Integer> answer) {
