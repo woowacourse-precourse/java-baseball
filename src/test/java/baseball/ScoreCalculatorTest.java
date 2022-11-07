6d1c11eddbaf231a451b4b97528371e8c408cf5b
Bumsoo <rhqjatn2398@naver.com>
2022-11-08 00:16:45 +0900

    test(ScoreCalculatorTest): 사용자의 숫자에 따른 결과를 반환하는 calScore() 메소드의 테스트 케이스 작성

--- src/test/java/baseball/ScoreCalculatorTest.java
+++ src/test/java/baseball/ScoreCalculatorTest.java
@@ -1,4 +1,45 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
 class ScoreCalculatorTest {
-  
+
+    @Test
+    void calScoreCase1() {
+        List<Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(1, 3, 2));
+
+        assertThat(score).isEqualTo(List.of(2, 1));
+    }
+
+    @Test
+    void calScoreCase2() {
+        List<Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(1, 2, 4));
+
+        assertThat(score).isEqualTo(List.of(0, 2));
+    }
+
+    @Test
+    void calScoreCase3() {
+        List<Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(3, 1, 2));
+
+        assertThat(score).isEqualTo(List.of(3, 0));
+    }
+
+    @Test
+    void calScoreCase4() {
+        List<Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(1, 2, 3));
+
+        assertThat(score).isEqualTo(List.of(0, 3));
+    }
+
+    @Test
+    void calScoreCase5() {
+        List<Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(4, 5, 6));
+
+        assertThat(score).isEqualTo(List.of(0, 0));
+    }
+
 }
