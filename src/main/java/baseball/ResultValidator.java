00869ca64799fd0db5d76129dfe62ee924b91cd5
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-07 11:13:34 +0900

    feat : ResultValidator 클래스 추가

---
+++ src/main/java/baseball/ResultValidator.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ResultValidator {
+    public List<Integer> validate(String input, String createdNumber) {
+        List<Integer> result = new ArrayList<>();
+
+        return result;
+    }
+}
