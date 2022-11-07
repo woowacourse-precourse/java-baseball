c001ef164af0752d73833f81dab19bb1c42b6c80
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-11-07 11:49:01 +0900

    refactor: GameStatusView 테스트 코드 입력문 출력문으로 반환하게 처리

--- src/test/java/baseball/view/GameStatusViewTest.java
+++ src/test/java/baseball/view/GameStatusViewTest.java
@@ -15,17 +15,6 @@ public class GameStatusViewTest {
     }
 
     @Test
-    void 입력이_1이나_2가_아니면_재요청(){
-        String input = "a";
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
-        gameStatusView.inquireOpinion();
-        OutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
-        assertThat(out.toString()).isEqualTo("잘못된 입력입니다.\n");
-    }
-
-    @Test
     void 입력시에_문자열로_저장해_문자열로_반환(){
         String input = "1";
         OutputStream out = new ByteArrayOutputStream();
