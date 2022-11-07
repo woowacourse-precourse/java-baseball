c8894013d627c53285ce5406e340f4c9a821d653
grand7070 <grand7070@naver.com>
2022-11-08 03:26:52 +0900

    refactor ; 3자리 글자수 상수화

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,9 +5,11 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class Computer {
+    public static final int LENGTH = 3;
+
     public List<Integer> randoms() {
         List<Integer> randoms = new ArrayList<>();
-        while (randoms.size() < 3) {
+        while (randoms.size() < LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!randoms.contains(randomNumber)) {
                 randoms.add(randomNumber);
@@ -19,24 +21,23 @@ public class Computer {
     public List<Integer> stringToInteger(String input) {
         if(!checkNum(input)) throw new IllegalArgumentException(); // 숫자 유효성 체크
         List<Integer> nums = new ArrayList<>();
-        for (int i = 0; i < input.length(); i++) {
+        for (int i = 0; i < LENGTH; i++) {
             nums.add(input.charAt(i) - '0');
         }
         return nums;
     }
 
     public boolean checkNum(String input) {
-        // 1. 숫자인지 확인
-        for (int i = 0; i < input.length(); i++) {
+        // 1. 세자리인지 확인
+        if(input.length() != LENGTH) return false;
+
+        // 2. 숫자인지 확인
+        for (int i = 0; i < LENGTH; i++) {
             if(!('0' <= input.charAt(i) && input.charAt(i) <= '9')) return false;
         }
 
-        // 2. 세자리 수인지 확인
-        if(input.length() != 3) return false;
-
-        // 3. 중복되는 수가 있는지 확인
-        List<String> duplicate = Arrays.asList(input.split(""));
-        if(new HashSet<>(duplicate).size() != duplicate.size()) return false;
+        // 3. 중복되는 수가 있는지 확인 -> 중복 값 허용하지 않는 Set 이용
+        if(new HashSet<>(Arrays.asList(input.split(""))).size() != LENGTH) return false;
 
         return true;
     }
@@ -44,8 +45,8 @@ public class Computer {
     public boolean baseballGame(List<Integer> randoms, List<Integer> nums) {
         Integer ball = 0;
         Integer strike = 0;
-        for(int i = 0; i < nums.size(); i++) {
-            for (int j = 0; j < randoms.size(); j++) {
+        for(int i = 0; i < LENGTH; i++) {
+            for (int j = 0; j < LENGTH; j++) {
                 if(nums.get(i).equals(randoms.get(j))) {
                     if(i == j) strike++;
                     else ball++;
--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -11,10 +11,13 @@ import java.util.stream.Collectors;
 import static org.assertj.core.api.Assertions.*;
 
 public class ComputerTest {
+
+
     @Test
     void 랜덤숫자_반환() {
         Computer computer = new Computer();
-        assertThat(new HashSet<>(computer.randoms()).size()).isEqualTo(computer.randoms().size()); // 숫자 중복 여부
+        assertThat(computer.randoms().size()).isEqualTo(Computer.LENGTH); // 자릿수 확인
+        assertThat(new HashSet<>(computer.randoms()).size()).isEqualTo(Computer.LENGTH); // 숫자 중복 여부
         assertThat(Integer.parseInt(computer.randoms().stream().map(String::valueOf).collect(Collectors.joining())))
                 .isPositive().isBetween(123, 987); // 숫자가 양수이며 세자리 숫자인지
     }
