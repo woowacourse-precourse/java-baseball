8d911edf972375bc45c5c591f6af195fdbeb4f88
bellljun3395 <195850@jnu.ac.kr>
2022-11-07 22:52:25 +0900

    test(PlayerNumbersTest): 쓰리 스트라이크

---
+++ src/test/java/study/player/playerMember/PlayerNumbersTest.java
@@ -0,0 +1,23 @@
+package study.player.playerMember;
+
+import baseball.player.playerMember.PlayerNumbers;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+
+class PlayerNumbersTest {
+
+    @Test
+    void 쓰리_스트라이크() {
+        PlayerNumbers baseNumber = new PlayerNumbers(123);
+        PlayerNumbers compareNumber = new PlayerNumbers(123);
+
+        List<Integer> hints = baseNumber.makeHints(compareNumber);
+
+        Assertions.assertThat(hints)
+                    .containsExactly(0, 3);
+    }
+
+}
