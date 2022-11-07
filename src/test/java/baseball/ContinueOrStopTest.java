ba31cdadeb497151549b0ffa86a32a57420a45be
saewoo1 <wooozooo1123@gmail.com>
2022-11-07 20:52:27 +0900

    test(ContiueOrStopTest): test code structure for choice

--- src/test/java/baseball/ContinueOrStopTest.java
+++ src/test/java/baseball/ContinueOrStopTest.java
@@ -1,12 +1,13 @@
 package baseball;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class ContinueOrStopTest {
 
   @Test
   void choice() {
+    assertThat(ContinueOrStop.choice("1")).isEqualTo(true);
+    assertThat(ContinueOrStop.choice("2")).isEqualTo(false);
   }
 }
