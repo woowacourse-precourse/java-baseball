71b8508dd06de9a01608c7b0595645ef846ab1ca
zeunala <zeunala@naver.com>
2022-11-07 19:04:53 +0900

    feat: 플레이어로부터 숫자 입력받는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -24,9 +25,42 @@ public class Application {
         return generatedNumber.toString();
     }
 
-    public static void main(String[] args) {
+    /**
+     * 플레이어로부터 서로 다른 3개의 숫자를 입력받아 반환
+     *
+     * @return 플레이어가 입력한 3자리 숫자로 된 String
+     * @throws IllegalArgumentException 3자리가 아닌 경우, 1~9까지의 숫자가 아닌 경우, 중복되는 숫자가 존재하는 경우
+     */
+    public static String getGuessNumberInput() throws IllegalArgumentException {
+        String inputNumber;
+        List<Character> usedNumber = new ArrayList<>();
+
+        System.out.print("숫자를 입력해주세요 : ");
+        inputNumber = Console.readLine();
+        if (inputNumber.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+
+        for (int i = 0; i < inputNumber.length(); i++) {
+            char ch = inputNumber.charAt(i);
+
+            if (ch < '1' || ch > '9') {
+                throw new IllegalArgumentException();
+            }
+            if (usedNumber.contains(ch)) {
+                throw new IllegalArgumentException();
+            }
+
+            usedNumber.add(ch);
+        }
+
+        return inputNumber;
+    }
+
+    public static void main(String[] args) throws IllegalArgumentException {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
         String answer = generateRandomNumberString();
+        String guessNumberString = getGuessNumberInput();
     }
 }
