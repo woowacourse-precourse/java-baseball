5ec7e4d1ec04309342a9bab1a77b42c09bdfac81
Be-student <presentsong@naver.com>
2022-11-07 12:51:13 +0900

    test(RandomDigits): 랜덤 생성 Digits 테스트

---
+++ src/test/java/baseball/domain/RandomDigitsTest.java
@@ -0,0 +1,22 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomDigitsTest {
+    @DisplayName("생성시 랜덤값으로 초기화됨")
+    @Test
+    void 랜덤_생성() {
+        RandomDigits randomDigits = RandomDigits.createRandom();
+        assertThat(randomDigits.size()).isEqualTo(3);
+        Digit first = randomDigits.getDigitInIndex(0);
+        Digit second = randomDigits.getDigitInIndex(1);
+        Digit third = randomDigits.getDigitInIndex(2);
+
+        assertThat(first).isNotEqualTo(second);
+        assertThat(second).isNotEqualTo(third);
+        assertThat(third).isNotEqualTo(first);
+    }
+}
