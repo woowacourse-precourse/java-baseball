0acf6c6c34a269123e72f048850c34a1bef371c3
DongWooE <ehddn977@naver.com>
2022-11-07 15:05:59 +0900

    [fix] printEndGame 테스트 코드 수정 중복 문구 삭제에 따른 변경

--- src/test/java/baseball/UnitTest.java
+++ src/test/java/baseball/UnitTest.java
@@ -466,7 +466,7 @@ class UnitTest {
     @Test
     public void printEndGame_state_true_일때_문구를_출력하는지_확인(){
         //given
-        final String content = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+        final String content = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
         //when
         application.printEndGame(true);
 
