857753c3df8a2a7d66c82906f73d407b39bb1d68
jindaram-stu <jinsb1999@naver.com>
2022-11-08 01:36:00 +0900

    feat(BaseballManager): 게임 시작 출력 추가
    게임 시작 시 사용자에게 안내하는 문자열 출력

--- src/main/java/baseball/game/service/BaseballManager.java
+++ src/main/java/baseball/game/service/BaseballManager.java
@@ -1,10 +1,13 @@
 package baseball.game.service;
 
+import baseball.game.io.BaseballGameOutput;
+
 import java.util.List;
 
 public class BaseballManager {
 
     public static void startGame() {
+        BaseballGameOutput.printGameStart();
         while (true) {
             List<Integer> computerNumber = ComputerNumber.generate();
             InningManager.play(computerNumber);
