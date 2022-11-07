990be62269d5d974affc31e4b8f7d206b44dbf53
sun007021 <sun007021@gmail.com>
2022-11-08 08:59:32 +0900

    feat(Domain : BaseballNumber) : 1 ~ 9 이외의 숫자인 경우 예외 발생

--- src/main/java/baseball/domain/BaseballNumber.java
+++ src/main/java/baseball/domain/BaseballNumber.java
@@ -3,8 +3,16 @@ package baseball.domain;
 public class BaseballNumber {
     public static final int MIN_NUMBER = 1;
     public static final int MAX_NUMBER = 9;
+    private static final String INPUT_NUMBER_RANGE_EXCEPTION_MESSAGE = "1~9 사이으 숫자를 입력해야 합니다.";
     private final int ballNumber;
-    BaseballNumber(int ballNumber){
+
+    public BaseballNumber(int ballNumber) {
         this.ballNumber = ballNumber;
+    }
+
+    private void validateBallNumber(int ballNumber) {
+        if (ballNumber < MIN_NUMBER || ballNumber > MAX_NUMBER) {
+            throw new IllegalArgumentException(INPUT_NUMBER_RANGE_EXCEPTION_MESSAGE);
+        }
     }
 }
