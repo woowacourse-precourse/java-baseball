65d9c6db0e9d8cf3acff01114093cf359a846613
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-08 04:45:26 +0900

    test: 유저 검증 메서드 테스트 코드 작성

--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -29,4 +29,25 @@ public class UserTest {
         String userInput = "446";
         assertThat(user.checkDuplicateNumber(userInput)).isTrue();
     }
+
+    @Test
+    @DisplayName("유저 입력에 숫자가 아닌것이 있는지 테스트")
+    void checkNumber() {
+        String userInput = "a12";
+        assertThat(user.checkNumber(userInput)).isTrue();
+    }
+
+    @Test
+    @DisplayName("유저 입력의 숫자가 3개가 아닌지 테스트")
+    void checkNumberSize() {
+        String userInput = "6254";
+        assertThat(user.checkNumberSize(userInput)).isTrue();
+    }
+
+    @Test
+    @DisplayName("유저 입력에 공백이 있는지 테스트")
+    void checkSpace() {
+        String userInput = "5 2";
+        assertThat(user.checkSpace(userInput)).isTrue();
+    }
 }
