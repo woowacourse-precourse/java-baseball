470882095a17f7f73d23f2e98353a545f42da346
Sangwoo Shin <sangwoonoel@naver.com>
2022-11-07 22:57:05 +0900

    feat(BaseBallGame): 안내 메세지 출력 코드 추가(2-8)

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -98,6 +98,7 @@ public class BaseBallGame {
     }
 
     public void setRestartToken() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         restartToken = Console.readLine();
     }
 
