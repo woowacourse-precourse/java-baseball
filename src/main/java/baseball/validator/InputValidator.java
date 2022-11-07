b8a319c7e6b5f517feab04264a3dd165307a3148
TaeHwan <kimta2hwan@gmail.com>
2022-11-07 13:19:40 +0900

    feat(inputvalidator): 사용자가 입력한 정수가 3자리의 수인지 검증하는 기능 구현

--- src/main/java/baseball/validator/InputValidator.java
+++ src/main/java/baseball/validator/InputValidator.java
@@ -5,14 +5,22 @@ import baseball.exception.InputException;
 public class InputValidator {
 
     private static final String INTEGER_REGULAR_EXPRESSION = "^[0-9]*$";
+    private static final int CORRECT_INPUT_LENGTH = 3;
 
     public static void validateInputForm(String guessedAnswer) {
         validateNotIntegerInput(guessedAnswer);
+        validateNotThreeLengthIntegerInput(guessedAnswer);
     }
 
     public static void validateNotIntegerInput(String guessedAnswer) {
         if (!guessedAnswer.matches(INTEGER_REGULAR_EXPRESSION)) {
             InputException.throwNotIntegerInputError();
+        }
+    }
+
+    public static void validateNotThreeLengthIntegerInput(String guessedAnswer) {
+        if (guessedAnswer.length() != CORRECT_INPUT_LENGTH) {
+            InputException.throwNotThreeLengthIntegerInputError();
         }
     }
 }
