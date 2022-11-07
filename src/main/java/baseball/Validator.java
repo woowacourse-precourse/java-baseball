ab1f6e24e54d3cee44b5928c0d753d5ec75c0d2f
kim <kajinori@gachon.ac.kr>
2022-11-07 19:46:59 +0900

    feat(Validator) : 사용자 입력 범위 예외처리 클래스 구현

---
+++ src/main/java/baseball/Validator.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import java.util.List;
+
+import static baseball.GameConstants.*;
+
+public class Validator {
+    public static boolean checkUserNumException(List<Integer> inputNumberList) {
+        return checkLength(inputNumberList)
+                && checkNumberRange(inputNumberList)
+                && checkDuplicateNumber(inputNumberList);
+    }
+
+    public static boolean checkLength(List<Integer> inputNumberList) {
+        return inputNumberList.size() == NUMBER_LENGTH;
+    }
+
+    public static boolean checkNumberRange(List<Integer> inputNumberList) {
+        return inputNumberList.stream()
+                .filter(number -> MIN_NUMBER <= number && number <= MAX_NUMBER)
+                .count() == NUMBER_LENGTH;
+    }
+
+    public static boolean checkDuplicateNumber(List<Integer> inputNumberList) {
+        return inputNumberList.stream()
+                .distinct()
+                .count() == NUMBER_LENGTH;
+    }
+}
