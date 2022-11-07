4eefbfb601a2a1a27c2109e5905dc9fe8a478351
rosesua318 <rosesua@duksung.ac.kr>
2022-11-07 23:53:27 +0900

    feat: main 함수에서 난수 생성과 게임 시작 가능하게 세팅

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,12 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Baseball baseball = new Baseball();
+
+        baseball.initialize();
+        baseball.play();
+
+
+        System.out.println("게임 종료");
     }
 }
