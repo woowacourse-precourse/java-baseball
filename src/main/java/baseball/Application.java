52b4fa75c411046b7867d2608686b1b6d68729d7
권태현 <kygkth2011@gmail.com>
2022-11-08 04:29:18 +0900

    feat(application): add constructor and printStartGame

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
 public class Application {
+    public Computer computer;
+
+    public Application(Computer computer) {
+        this.computer = computer;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Computer computer = new Computer();
+        Application application = new Application(computer);
+        application.computer.pickRandomNumber();
+    }
+
+    public void printStartGame(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
