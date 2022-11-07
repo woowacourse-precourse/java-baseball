c82b89542d9d3a74d2e76ccb8470f16d7fa80587
Songyi Kim <ksl2950@naver.com>
2022-11-07 21:33:50 +0900

    fix: 0~9가 아닌 1~9까지의 수 검증으로 변경

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,4 +1,5 @@
 package baseball;
+
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.Arrays;
@@ -7,18 +8,17 @@ import java.util.stream.Collectors;
 
 public class Player {
 
-    public List<Integer> enterNumber(){
+    public List<Integer> enterNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         String playerInput = Console.readLine();
 
-        if(!validateIsThreeNumber(playerInput)|| !validateIsUniqueNumber(playerInput)){
-            throw new IllegalArgumentException("잘못된 입력값입니다.");
-        }
+        validateIsThreeNumber(playerInput);
+        validateIsUniqueNumber(playerInput);
 
         return convertToIntegerList(playerInput);
     }
 
-    public boolean wantToRestart(){
+    public boolean wantToRestart() {
         int RESTART_NUMBER = 1;
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int playerInput = enterRestartNumber();
@@ -26,28 +26,31 @@ public class Player {
     }
 
 
-    public int enterRestartNumber(){
+    public int enterRestartNumber() {
         String playerInput = Console.readLine();
-        if (!validateIsRestartNumber(playerInput)){
-            throw new IllegalArgumentException("1 또는 2를 입력해주세요.");
-        }
+        validateIsRestartNumber(playerInput);
         return Integer.parseInt(playerInput);
     }
 
-    private boolean validateIsThreeNumber(String playerInput) {
-        return playerInput.matches("^[0-9]{3}$");
+    private void validateIsThreeNumber(String playerInput) {
+        if (!playerInput.matches("^[1-9]{3}$")) {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
     }
 
-    private boolean validateIsUniqueNumber(String playerInput) {
+    private void validateIsUniqueNumber(String playerInput) {
         int COMPARE_LENGTH = 3;
         long inputLength = Arrays.stream(playerInput.split("")).distinct().count();
-        return COMPARE_LENGTH == inputLength;
+        if (COMPARE_LENGTH != inputLength) {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
     }
 
-    private boolean validateIsRestartNumber(String playerInput) {
-        return playerInput.matches("^[1-2]$");
+    private void validateIsRestartNumber(String playerInput) {
+        if (!playerInput.matches("^[1-2]$")) {
+            throw new IllegalArgumentException("잘못된 입력값입니다.");
+        }
     }
-
 
     public static List<Integer> convertToIntegerList(String playerInput) {
         return Arrays.stream(playerInput.split("")).map(Integer::parseInt).collect(Collectors.toList());
