76963679c34f227df2885e59ce3dd522e82bc361
이건회 <rg970604@naver.com>
2022-11-08 03:16:42 +0900

    style(PrintResult.java) : static import 추가

--- src/main/java/baseball/view/PrintResult.java
+++ src/main/java/baseball/view/PrintResult.java
@@ -1,19 +1,20 @@
 package baseball.view;
 
+import static java.lang.System.out;
 import baseball.domain.BallCount;
 
 public class PrintResult {
 
     public static void printStartMent(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        out.println("숫자 야구 게임을 시작합니다.");
     }
     public static void printBallCount(BallCount ballCount) {
-        System.out.println(ballCount.ballCountResult());
+        out.println(ballCount.ballCountResult());
     }
 
     public static void gameClearMent(boolean isgameEnd) {
         if (isgameEnd) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
     }
 
