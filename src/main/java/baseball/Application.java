9e6b7f6404d64a72c2c971eedcbf6071463b2040
noh eul <papooa5140@gmail.com>
2022-11-08 01:48:01 +0900

    feat : ball 기능 수정, strike, printResult 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -38,13 +38,12 @@ public class Application {
         return myThreeNum;
     }
 
-    // <상대 컴퓨터의 3자리수 랜덤숫자뽑기 메서드>
-    public static final List<Integer> computerThreeNum() {
-        // 1~9로 이루어진 서로 다른 3자리 수 List<Integer> [0,0,0]
-        // 1~9까지 수의 랜덤숫자 뽑기로 자리 하나씩 채우기
-        // Random값 추출 -> camp.nextstep.edu.missionutile.Randoms의 pickNumberInRange() 활용
-        // 랜덤숫자 1~9
-
+    /**
+     * 상대 컴퓨터의 3자리수 랜덤숫자뽑기 메서드
+     * 
+     * @return
+     */
+    public static List<Integer> computerThreeNum() {
         List<Integer> computerThreeNum = new ArrayList<Integer>();
         while (computerThreeNum.size() < 3) {
             int computerNum = Randoms.pickNumberInRange(1, 9);
@@ -54,17 +53,18 @@ public class Application {
             }
 
         }
-        System.out.println(computerThreeNum);
+
         return computerThreeNum;
     }
 
-    // <게임진행 메서드>
+    /**
+     * 게임진행 메서드
+     * 
+     * @return
+     */
     public static void startBaseball() {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        // myThreeNum 호출
-        // checkValidation 호출
-        // computerThreeNum 호출
         List<Integer> computerThreeNum = computerThreeNum();
         while (true) {
             List<Integer> myThreeNum = myThreeNum();
@@ -73,16 +73,24 @@ public class Application {
             if (equalList == false) {
                 int strike = strike(computerThreeNum, myThreeNum);
                 int ball = ball(computerThreeNum, myThreeNum);
+                printResult(strike, ball);
                 continue;
             }
-
+            break;
         }
     }
 
+    /**
+     * List값 비교하기 3개 모두 맞으면 3스트라이크!
+     * 
+     * @param computerThreeNum
+     * @param myThreeNum
+     * @return
+     */
     public static boolean equalList(List<Integer> computerThreeNum, List<Integer> myThreeNum) {
-        // list A.containsAll(list B) 리스트 내용 비교
+
         boolean equalList = false;
-        // index, 내용 같으면 true ------> 3스트라이크!
+
         if (myThreeNum.equals(computerThreeNum)) {
             System.out.println("3 스트라이크!");
             System.out.println("3개의 숫자를 모두 맞히셨습니다!");
@@ -92,11 +100,15 @@ public class Application {
         return equalList;
     }
 
+    /**
+     * 게임종료 여부확인
+     * 
+     * @return
+     */
     public static boolean endBaseball() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String endGame = Console.readLine();
 
-        // 게임종료 or 재시작 여부
         if (endGame.equals("1")) {
             System.out.println("게임을 다시 시작합니다.");
             return false;
@@ -110,43 +122,87 @@ public class Application {
 
     }
 
+    /**
+     * strike count 확인
+     * 
+     * @param computerThreeNum
+     * @param myThreeNum
+     * @return
+     */
     public static int strike(List<Integer> computerThreeNum, List<Integer> myThreeNum) {
-        // FALSE ------------>
-        // 반복------>숫자 3개 맞을때까지!!!
-        // strike count ++
-        return 0;
+
+        int strikeCount = 0;
+
+        for (int i = 0; i < computerThreeNum.size(); i++) {
+            if (computerThreeNum.get(i) == myThreeNum.get(i)) {
+                strikeCount++;
+            }
+        }
+
+        return strikeCount;
     }
 
+    /**
+     * ball count 확인
+     * 
+     * @param computerThreeNum
+     * @param myThreeNum
+     * @return
+     */
     public static int ball(List<Integer> computerThreeNum, List<Integer> myThreeNum) {
-        // FALSE ------------>
+
         int ballCount = 0;
 
-        // ball count ++
         for (int i = 0; i < computerThreeNum.size(); i++) {
-            Set<Integer> ball = new HashSet<>(computerThreeNum);
-            if (ball.retainAll(myThreeNum)) {
+            int computerNum = computerThreeNum.get(i);
+            int index = myThreeNum.indexOf(computerNum);
+            if (index == -1 || computerThreeNum.get(i) == myThreeNum.get(i)) {
+                continue;
+            }
+            if (index <= 2) {
                 ballCount++;
             }
         }
-        System.out.println(ballCount + "볼");
 
         return ballCount;
     }
 
+    /**
+     * 예외처리
+     * 
+     * @param myThreeNum
+     */
     public static void checkValidation(List<Integer> myThreeNum) {
-        // myThreeNum 4자리 이상 입력되면 "예외처리" 후 게임 종료
+
         if (myThreeNum.size() != 3) {
             throw new IllegalArgumentException("입력오류. 게임을 종료합니다.");
         }
 
         Set<Integer> numList = new HashSet<>(myThreeNum);
         if (numList.size() != myThreeNum.size()) {
-            throw new IllegalArgumentException("입력오류. 게임을 종료합니다.");
+            throw new IllegalArgumentException("중복입력. 게임을 종료합니다.");
 
         }
     }
-}
 
-// 같은 인덱스번호 == 같은 내용 -----> 1스트라이크 카운트++
-// 다른 인덱스번호, list내에 같은 내용 -------> 1볼 카운트 ++
-// 전혀 다른 내용 --------> 낫싱
+    /**
+     * 조건 충족 시 strike count, ball count, 낫싱 출력
+     * 
+     * @param strike
+     * @param ball
+     */
+    public static void printResult(int strike, int ball) {
+        String messege = "";
+
+        if (ball > 0) {
+            messege = messege + ball + "볼 ";
+        }
+        if (strike > 0) {
+            messege = messege + strike + "스트라이크";
+        }
+        if (strike == 0 && ball == 0) {
+            messege = messege + "낫싱";
+        }
+        System.out.println(messege.trim());
+    }
+}
