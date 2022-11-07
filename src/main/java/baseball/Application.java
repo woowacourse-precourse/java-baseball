db24c2f4dcbf45f28491c718a6cee0d821e5685a
0417taehyun <0417taehyun@gmail.com>
2022-11-08 07:28:51 +0900

    docs (documentation): 세부 구현을 위한 각 클래스 별 TODO 관리
    구현해야 할 클래스 및 인터페이스 목록:
    - Number (Interface) : User 및 Answer 클래스에서의 상속을 위한 인터페이스
    - User (Class) : 사용자의 입력값 관리 및 유효성 검증을 위한 클래스
    - Answer (Class) : 정답 관리 및 사용자 입력값과의 비교를 위한 클래스
    - Game (Class) : 게임 진행 및 숫자 결과 비교에 따른 출력을 위한 클래스

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,29 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        /*
+        TODO: 프로그램 구현
+
+        - Number 인터페이스
+            - MAX_SIZE, START_NUMBER, END_NUMBER 상수
+            - create, get 메서드
+
+        - Answer 클래스
+            - Number 인터페이스 상속
+            - compare 메서드
+            - isStrike 메서드
+            - isBall 메서드
+
+        - User 클래스
+            - Number 인터페이스 상속
+            - isNumberOverMaxSize 메서드
+            - isNumberInValidRange 메서드
+
+        - Game 클래스
+            - printStart 메서드
+            - printEnd 메서드
+            - printRestart 메서드
+            - printResult 메서드
+         */
     }
 }
