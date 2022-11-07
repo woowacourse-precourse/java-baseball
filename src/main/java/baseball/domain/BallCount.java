ec69070319818ba5c168e2fcbfa007cc5d7952ed
이건회 <rg970604@naver.com>
2022-11-08 04:57:18 +0900

    style(BallCount.java) : 문자열 포맷팅, 매직넘버, 매직리터럴 상수선언

--- src/main/java/baseball/domain/BallCount.java
+++ src/main/java/baseball/domain/BallCount.java
@@ -2,13 +2,19 @@ package baseball.domain;
 
 public class BallCount {
 
-    private final int MIN_STRIKE_COUNT = 0;
-    private final int MAX_STRIKE_COUNT = 3;
-    private final int MIN_BALL_COUNT = 0;
-    private final int MAX_BALL_COUNT = 3;
-    private final int NO_COUNT = 0;
-    private int Strikes;
-    private int Balls;
+    private static final int MIN_STRIKE_COUNT = 0;
+    private static final int MAX_STRIKE_COUNT = 3;
+    private static final int MIN_BALL_COUNT = 0;
+    private static final int MAX_BALL_COUNT = 3;
+    private static final int NO_COUNT = 0;
+    private static final String NOTHING = "낫싱";
+    private static final String BALL_FORMAT = "%s볼";
+    private static final String STRIKE_FORMAT = "%s스트라이크";
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String BALLCOUNT_FORMAT = "%s는 0에서 3 사이 숫자만 집계합니다";
+    private final int Strikes;
+    private final int Balls;
 
     public BallCount(int strikes, int balls) {
         validateStrikeCount(strikes);
@@ -19,26 +25,26 @@ public class BallCount {
 
     public String ballCountResult() {
         if (Balls == NO_COUNT && Strikes == NO_COUNT) {
-            return "낫싱";
+            return NOTHING;
         }
         if (Strikes == NO_COUNT) {
-            return String.valueOf(Balls) + "볼";
+            return String.format(BALL_FORMAT, Balls);
         }
         if (Balls == NO_COUNT) {
-            return String.valueOf(Strikes) + "스트라이크";
+            return String.format(STRIKE_FORMAT, Strikes);
         }
-        return String.valueOf(Balls) + "볼 " + String.valueOf(Strikes) + "스트라이크";
+        return String.format(BALL_FORMAT, Balls) + " " + String.format(STRIKE_FORMAT, Strikes);
     }
 
     private void validateStrikeCount(int strikes) {
         if (strikes < MIN_STRIKE_COUNT || strikes > MAX_STRIKE_COUNT) {
-            throw new IllegalArgumentException("스트라이크는 0에서 3 사이 숫자만 집계합니다");
+            throw new IllegalArgumentException(String.format(BALLCOUNT_FORMAT, STRIKE));
         }
     }
 
     private void validateBallCount(int balls) {
         if (balls < MIN_BALL_COUNT || balls > MAX_BALL_COUNT) {
-            throw new IllegalArgumentException("볼은 0에서 3 사이 숫자만 집계합니다");
+            throw new IllegalArgumentException(String.format(BALLCOUNT_FORMAT, BALL));
         }
     }
 }
