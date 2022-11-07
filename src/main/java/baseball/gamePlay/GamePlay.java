8de0300bfcfdda47a884c43f0101b3182dc86fb4
이건회 <rg970604@naver.com>
2022-11-07 19:33:04 +0900

    feat (GamePlay.java) : 사용자가 정답을 맞출 때 까지 게임 진행

--- src/main/java/baseball/gamePlay/GamePlay.java
+++ src/main/java/baseball/gamePlay/GamePlay.java
@@ -1,10 +1,20 @@
 package baseball.gamePlay;
 
 import baseball.domain.BallCount;
+import baseball.domain.Numbers;
 import baseball.rule.EndRule;
+import baseball.rule.JudgementRule;
 import baseball.view.PrintResult;
 
 public class GamePlay {
+
+    private static boolean continuegameUntilGetAnswer(Numbers computerNumbers, boolean isGameEnd,
+        Numbers userNumbers) {
+        BallCount ballCount = JudgementRule.makeBallCounts(computerNumbers, userNumbers);
+        PrintResult.printBallCount(ballCount);
+        return checkGetAnswer(isGameEnd, ballCount);
+    }
+
     private static boolean checkGetAnswer(boolean isGameEnd, BallCount ballCount) {
         if (EndRule.isGameEnd(ballCount)) {
             PrintResult.gameClearMent(EndRule.isGameEnd(ballCount));
