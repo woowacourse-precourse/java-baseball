c47157d19deb6e5a910816eedc6dfbcbf73ab22e
seeunla <107592967+seeunla@users.noreply.github.com>
2022-11-08 00:01:26 +0900

    feat: 유저 숫자 유효성 검증

--- src/main/java/baseball/GameSimulation.java
+++ src/main/java/baseball/GameSimulation.java
@@ -10,6 +10,7 @@ public class GameSimulation {
     private Computer computer;
     private int strike;
     private int ball;
+    private static final boolean INVALID = false;
 
     public GameSimulation() {
     }
@@ -18,6 +19,7 @@ public class GameSimulation {
         System.out.print("숫자를 입력해주세요 : ");
         String userInput = readLine();
         List<Integer> userNumber = makeList(userInput);
+        validateUserNumber(userNumber);
     }
 
     public List<Integer> makeList(String userInput) {
@@ -27,5 +29,15 @@ public class GameSimulation {
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
+    }
+
+    public void validateUserNumber(List<Integer> userNumber) {
+        try {
+            if (Valid.validNumberList(userNumber) == INVALID) {
+                throw new IllegalArgumentException("올바른 숫자가 아닙니다. 다시 입력해주세요.");
+            }
+        } catch (Exception e){
+            throw new IllegalArgumentException();
+        }
     }
 }
