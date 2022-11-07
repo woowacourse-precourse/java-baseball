70560d6654aceecda0e43a8a8b4d0501c10d7f9a
Woojin Lee <dhkep03@gmail.com>
2022-11-08 01:56:46 +0900

    feat(ExceptionHandler): 사용자 숫자 길이 제한

--- src/main/java/baseball/controller/ExceptionHandler.java
+++ src/main/java/baseball/controller/ExceptionHandler.java
@@ -2,7 +2,9 @@ package baseball.controller;
 
 public class ExceptionHandler {
     static void validateUserNumberOfString(String userNumberOfString) throws IllegalArgumentException{
-
+        if (userNumberOfString.length() != 3) {
+            throw new IllegalArgumentException("서로 다른 세 자리의 숫자를 입력해야 합니다.");
+        }
     }
 
     static void validateOneIfGameRepeatOrTwo(String oneIfGameRepeatOrTwo) throws IllegalArgumentException{
