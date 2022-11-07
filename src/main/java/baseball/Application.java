3596911198d8073eb469f70d8adfa5cfe1cd4cac
nayooniverse <kangna99@naver.com>
2022-11-08 08:56:41 +0900

    feat: 게임 실행 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import static baseball.Constant.GAME_START;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println(GAME_START);
+        Controller controller = new Controller();
+        controller.run();
     }
 }
