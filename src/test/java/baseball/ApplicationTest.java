021b12173e433012869cc53eb40966656169e319
ShinKwang <sklee0206@naver.com>
2022-11-07 16:59:51 +0900

    test: Make test for validate duplication of number
    유저가 중복된 숫자를 검색했는지 확인하기 위한 테스트를 작성합니다. 숫자는 총 3개가 들어오니 112, 122, 151과 같은
    2글자 중복과 111, 222와 같은 3글자 중복을 검증합니다. README.md를 확인하며, 기능을 구현하고 있지만 기능을
    구현할 때에도 우선순위가 있습니다. 위 테스트의 경우 앞서 숫자가 3개만 들어오는지, 숫자만 들어오는지를 반드시 검증해야만
    합니다.

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,7 +1,13 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -9,6 +15,35 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 중복된_숫자_2개_입력() {
+        //given
+        String firstMiddleNumber = "112";
+        String middleEndNumber = "233";
+        String firstEndNumber = "191";
+
+        //when, then
+        Assertions.assertThatThrownBy(() -> Application.validateUserNumber(firstMiddleNumber))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("UserNumber has duplicate number");
+        Assertions.assertThatThrownBy(() -> Application.validateUserNumber(middleEndNumber))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("UserNumber has duplicate number");
+        Assertions.assertThatThrownBy(() -> Application.validateUserNumber(firstEndNumber))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("UserNumber has duplicate number");
+    }
+    @Test
+    void 중복된_숫자_3개_입력() {
+        //given
+        String givenNumber = "111";
+        String givenNumber2 = "222";
+
+        //when, then
+        Assertions.assertThatThrownBy(() -> Application.validateUserNumber(givenNumber))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("UserNumber has duplicate number");
+        Assertions.assertThatThrownBy(() -> Application.validateUserNumber(givenNumber2))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining("UserNumber has duplicate number");
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
