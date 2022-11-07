6814bf19f45636a8d5911efa08bad3d727db6ee8
0112leesy <0112leesy@naver.com>
2022-11-07 17:07:30 +0900

    feat(User): User 클래스 및 setInput 함수 정의
    유저 클래스 생성:
    - String input 필드: 사용자의 입력값을 저장하는 문자열
    - setInput 함수: readLine 함수로 입력 받은 값을 input에 저장함

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,13 @@
+package baseball;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class User {
+
+    public String input;
+
+    public void setInput() {
+        this.input = readLine();
+    }
+
+}
