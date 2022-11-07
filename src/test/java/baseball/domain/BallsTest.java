8734bc937e08f808941eb28f2290b60c3bad0d1b
JunH <tjdtls690@gmail.com>
2022-11-07 15:27:00 +0900

    :white_check_mark: test(Domain : BallsTest) : 같은 번호가 존재하지 않는 Balls 입력 값인지 확인하는 테스트 추가

--- src/test/java/baseball/domain/BallsTest.java
+++ src/test/java/baseball/domain/BallsTest.java
@@ -2,14 +2,23 @@ package baseball.domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 class BallsTest {
+    @DisplayName("같은 숫자가 존재하지 않는 Balls 입력 값인지 확인")
+    @ParameterizedTest(name = "{displayName} => {0}")
+    @ValueSource(strings = {"123", "456", "789"})
+    void correct_balls(String balls) {
+        assertThatNoException()
+                .isThrownBy(() -> new Balls(balls));
+    }
+    
     @Test
     @DisplayName("3개의 각 공이 strike, ball, nothing 중 어디에 속하는지 계산")
     void compare_balls() {
