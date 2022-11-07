104f58045142e207571a6d7f0eeebd987e74d92a
desfox <desfox99@naver.com>
2022-11-07 21:43:38 +0900

    feat: 컴퓨터가 임의의 3자리 숫자를 선택하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static final int NUM_OF_DIGIT = 3;
+    public static final int DIGIT_MIN = 1;
+    public static final int DIGIT_MAX = 9;
+
+    public static List<Integer> computerNumList = new ArrayList<>();
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    private static void computerRandomThreeDigitNum() {
+        while (computerNumList.size() < NUM_OF_DIGIT) {
+            int randomDigit = Randoms.pickNumberInRange(DIGIT_MIN, DIGIT_MAX);
+
+            if (computerNumList.contains(randomDigit) == false) {
+                computerNumList.add(randomDigit);
+            }
+        }
+    }
+
 }
