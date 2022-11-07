2b8ab40a4c60749d4d90c7399e1250acfa30cee1
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-11-07 21:34:22 +0900

    Test(BaseballGameManagerTest) : 예외상황에 대한 테스트케이스작성

--- src/test/java/baseball/BaseballGameManagerTest.java
+++ src/test/java/baseball/BaseballGameManagerTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -8,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -69,5 +72,65 @@ class BaseballGameManagerTest {
         //then
         assertEquals("낫싱", outputStreamCaptor.toString().trim());
     }
+    
+    @Test
+    public void _3자리숫자_입력_예외처리_3자리가_넘는_숫자_입력시_NumberFormatException() throws Exception {
+        //given
+        String input = "1234";
+        
+        //when
+        try {
+            if (input.length() != 3) {
+                throw new NumberFormatException();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("3자리가 넘음");
+        }
 
+        //then
+        assertEquals("3자리가 넘음", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void _3자리숫자_입력_예외처리_3자리가_안되는_숫자_입력시_NumberFormatException() throws Exception {
+        //given
+        String input = "12";
+
+        //when
+        try {
+            if (input.length() != 3) {
+                throw new NumberFormatException();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("3자리가 안됨");
+        }
+
+        //then
+        assertEquals("3자리가 안됨", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void _3자리숫자_입력_예외처리_빈문자_입력시_NumberFormatException() throws Exception {
+        //given
+        String input = "";
+
+        //when
+        try {
+            if (input.length() == 0) {
+                throw new NumberFormatException();
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("빈 문자 입력됨");
+        }
+
+        //then
+        assertEquals("빈 문자 입력됨", outputStreamCaptor.toString().trim());
+    }
+
+    @Test
+    public void _3자리숫자_입력_예외처리_숫자가_아닌값_입력시_NumberFormatException() throws Exception {
+        String input = "ans31vnsladn";
+
+        assertThatThrownBy(() -> Integer.parseInt(input)).isInstanceOf(NumberFormatException.class);
+    }
 }
