7a8913891dd7573e87b0eae2677c28fd3ce40979
cpot5620 <=cpot5620@naver.com>
2022-11-07 16:32:49 +0900

    refactor(RandomNumber): change parameter name in createNumbers method

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -9,10 +9,10 @@ public class RandomNumber {
     private static final int START_RANGE = 1;
     private static final int END_RANGE = 9;
 
-    public List<Integer> createNumbers(int length) {
+    public List<Integer> createNumbers(int size) {
         List<Integer> randomNumbers = new ArrayList<>();
 
-        while (randomNumbers.size() < length) {
+        while (randomNumbers.size() < size) {
             int randomNumber = Randoms.pickNumberInRange(START_RANGE, END_RANGE);
 
             if (!randomNumbers.contains(randomNumber))
