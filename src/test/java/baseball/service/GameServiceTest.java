f0955f3373e7e47ccbac1d7dfbfad20df4143c45
seoyoung <sy9723@korea.ac.kr>
2022-11-08 00:55:54 +0900

    test: print game score

--- src/test/java/baseball/service/GameServiceTest.java
+++ src/test/java/baseball/service/GameServiceTest.java
@@ -1,5 +1,6 @@
 package baseball.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -19,5 +20,41 @@ class GameServiceTest {
         int userNumber = 124;
         int ball = GameService.getBallNumber(userNumber, computerNumber);
         assertEquals(1, ball);
+    }
+
+    @Test()
+    public void 계산한_결과를_출력한다_1() {
+        int strike = 1;
+        int ball = 1;
+        String result = GameService.printGameScore(strike, ball);
+        String expected = "1볼 1스트라이크";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test()
+    public void 계산한_결과를_출력한다_2() {
+        int strike = 0;
+        int ball = 0;
+        String result = GameService.printGameScore(strike, ball);
+        String expected = "낫싱";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test()
+    public void 계산한_결과를_출력한다_3() {
+        int strike = 0;
+        int ball = 2;
+        String result = GameService.printGameScore(strike, ball);
+        String expected = "2볼";
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test()
+    public void 계산한_결과를_출력한다_4() {
+        int strike = 2;
+        int ball = 0;
+        String result = GameService.printGameScore(strike, ball);
+        String expected = "2스트라이크";
+        assertThat(result).isEqualTo(expected);
     }
 }
