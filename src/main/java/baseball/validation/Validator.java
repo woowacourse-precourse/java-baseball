6c4584d1015caa3eb010a0e021a13d3a8f7f3408
jyj1143 <jyj1143@gmail.com>
2022-11-08 00:05:43 +0900

    feat(Validator): 게임 플레이어가 입력한 값이 정수가 아닌 다른 타입일 경우 예외처리

--- src/main/java/baseball/validation/Validator.java
+++ src/main/java/baseball/validation/Validator.java
@@ -10,11 +10,20 @@ public class Validator {
 
     public static void validatePlayerInput(String input) {
         validateInputLength(input);
+        validateInputInteger(input);
     }
 
     public static void validateInputLength(String input) {
         if (input.length() != COUNT_VALUE) {
             throw new IllegalArgumentException("[IllegalArgumentException]: " + COUNT_VALUE + "자리 자리자연수가 아닙니다.\n");
+        }
+    }
+
+    public static void validateInputInteger(String input) {
+        for (int i = 0; i < COUNT_VALUE; i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                throw new IllegalArgumentException("[IllegalArgumentException]: 정수가 아닙니다.\n");
+            }
         }
     }
 
