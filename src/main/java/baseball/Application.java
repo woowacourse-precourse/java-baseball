65498816d363a62b099d556df8c16d9995687cec
seongmin15 <tjdalsdl19@gmail.com>
2022-11-07 15:24:51 +0900

    feat: Application에 숫자야구 구현
    Game function 개체를 만든 후 Game 객체의 play가 1이면 계속 play,
    0이면 Game 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,10 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game_function Game = new Game_function();
+        while(Game.play == 1){
+            Game.play();
+        }
+        Game.end();
     }
 }
