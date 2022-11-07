a48992e8c0d2dfb74fea75dc900c7cfed03854ca
narin111 <jungnr11@naver.com>
2022-11-08 00:32:18 +0900

    feat: 사용자 입력 비교 결과 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -24,7 +24,6 @@ public class Application {
     }
 
     public static List<Integer> compare_answer(List<Integer> computer, char[] user_answer){
-        // List<Integer> current_score = new ArrayList<>();
         List<Integer> current_score = Arrays.asList(0, 0, 0);
         int strike = 0;
         int ball = 0;
@@ -33,21 +32,17 @@ public class Application {
             int each_number = Character.getNumericValue(user_answer[i]);
             if(computer.get(i)==each_number){
                 strike++;
-                // current_score.set(0, current_score.get(0)+1);
             }
             else if(computer.contains(each_number)){
                 ball++;
-                // current_score.set(1, current_score.get(1)+1);
             }
             else{
                 nothing++;
-                // current_score.set(2, current_score.get(2)+1);
             }
         }
         current_score.set(0, strike);
         current_score.set(1, ball);
         current_score.set(2, nothing);
-        // System.out.println(current_score.get(0));
         return current_score;
     }
     public static void main(String[] args) {
@@ -68,9 +63,30 @@ public class Application {
                 // 숫자 랜덤
                 String user_answer = readLine();
                 char[] user_input = user_answer.toCharArray();
+
                 List<Integer> compare_result = compare_answer(computer_number, user_input);
+                int strike_result = compare_result.get(0);
+                int ball_result = compare_result.get(1);
+                int nothing_result = compare_result.get(2);
+
+                if(nothing_result==3){
+                    System.out.println("낫싱");
+                }
+                else if(strike_result==3){
+                    System.out.println(strike_result + "스트라이크");
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+                    String stay = readLine();
+
+                    // System.out.println(stay);
+                }
+                else{
+                    System.out.println(ball_result + "볼" + strike_result + "스트라이크");
+                }
+
                 break;
-            }
+                }
             break;
         }
     }
