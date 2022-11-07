c63b92f8c35cfc2c1fae4716d357e570484aa560
CHAYEONIL <jeay123@naver.com>
2022-11-08 02:41:56 +0900

    Refactor : Exception - private -> public 수정

--- src/main/java/baseball/exception/WrongNumberException.java
+++ src/main/java/baseball/exception/WrongNumberException.java
@@ -1,7 +1,7 @@
 package baseball.exception;
 
 public class WrongNumberException extends  IllegalArgumentException{
-    private static final String INVALID_BASEBALL_NUMBER_RANGE = "1 ~ 9 사이의 값을 입력해야 합니다.";
+    public static final String INVALID_BASEBALL_NUMBER_RANGE = "1 ~ 9 사이의 값을 입력해야 합니다.";
 
     public WrongNumberException() {
         super(INVALID_BASEBALL_NUMBER_RANGE);
