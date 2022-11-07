fa7688e7facd7f61e5bc4c692c213959cc2be77e
jiggyjiggy <jiggyjiggy0323@gmail.com>
2022-11-07 16:47:50 +0900

    test: create askInputThreeDiffDigit test of GameMachine

--- src/test/java/baseball/GameMachineTest.java
+++ src/test/java/baseball/GameMachineTest.java
@@ -10,6 +10,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class GameMachineTest {
     GameMachine gameMachine = new GameMachine();
 
+    private void command(final String... args) {
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    }
+
     @Test
     void 중복되는_숫자가_없는_길이가_3인_integer_list_생성한다() {
         // When
@@ -21,6 +26,21 @@ public class GameMachineTest {
         // Then
         assertThat(size).isEqualTo(3);
         assertThat(distinctIntegerListSize).isEqualTo(3);
+    }
+
+    @Test
+    void gamer에게_입력받은_문자열을_integer_list_로_반환한다() {
+        // Given
+        Gamer gamer = new Gamer();
+        String input = "123";
+        command(input);
+
+        // When
+        List<Integer> threeDiffDigit = gameMachine.askInputThreeDiffDigit(gamer);
+
+        // Then
+        List<Integer> integerList = List.of(1, 2, 3);
+        assertThat(threeDiffDigit).isEqualTo(integerList);
     }
 
 }
