64cda9b8108ec65bea9f3d084e2ecf17cd4bfe3d
Kijung Park <kjpark4321@naver.com>
2022-11-08 00:36:07 +0900

    feat : 게임 한 단위를 실행하는 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,17 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
+    
+    private static final Scanner stdinScanner = new Scanner(System.in);
+
+    private static boolean eachQuestion(){
+        System.out.print("숫자를 입력해주세요 : ");
+        int inputNumber = stdinScanner.nextInt();
+
+        return false;
+    }
 
     private static void baseballGameRun(){
         System.out.println("숫자 야구 게임을 시작합니다.");
