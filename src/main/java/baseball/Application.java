e60755baa2a454126a4df9e6ee32a6414279097a
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-07 10:15:10 +0900

    <feat>Application : 게임 종료 후 새로운 게임 생성 혹은 완전한 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,24 +1,41 @@
 package baseball;
 
 import java.sql.SQLOutput;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-
-        String numberComputer = Input.computer();
-        System.out.println(numberComputer);
-        boolean notThreeStrikes = true;
+        boolean restart = true;
+        String reply;
 
         do {
-            String numberPlayer = Input.player();
-            System.out.println(numberPlayer);
 
-            PlayingBaseball round = new PlayingBaseball(numberComputer, numberPlayer);
-            System.out.println(round.countStrikes());
-            System.out.println(round.countBalls());
-            round.printResult();
-            notThreeStrikes = !round.has3Strikes();
-        } while (notThreeStrikes);
 
-        }
+            String numberComputer = Input.computer();
+            System.out.println(numberComputer);
+            boolean notThreeStrikes = true;
+
+            do {
+                String numberPlayer = Input.player();
+                System.out.println(numberPlayer);
+
+                PlayingBaseball round = new PlayingBaseball(numberComputer, numberPlayer);
+                System.out.println(round.countStrikes());
+                System.out.println(round.countBalls());
+                round.printResult();
+                notThreeStrikes = !round.has3Strikes();
+            } while (notThreeStrikes);
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            reply = Console.readLine();
+
+            if (reply.equals("2")) {
+                restart = false;
+            }
+            System.out.println(reply);
+            System.out.println(restart);
+
+
+        } while (restart);
+    }
     }
