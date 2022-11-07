22a9613f4a7f57c515d992151852ac0a65867395
Woong99 <dndry1072@naver.com>
2022-11-07 16:50:20 +0900

    refactor : 랜덤 숫자를 얻는 메소드 위치 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,9 +12,9 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         int startSwitch = 1;
-        List<Integer> computerNumbersList = getRandomThreeNumbers();
-        System.out.println(computerNumbersList);
         while (startSwitch == 1) {
+            List<Integer> computerNumbersList = getRandomThreeNumbers();
+            System.out.println(computerNumbersList);
             System.out.print("숫자를 입력해주세요 : ");
             int userNumber = Integer.parseInt(Console.readLine());
             List<Integer> userNumbersList = getUserNumberList(userNumber);
