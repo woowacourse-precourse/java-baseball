07dc01cc23a4ab5a64e6cba032ec2f04e54f35b3
saewoo1 <wooozooo1123@gmail.com>
2022-11-07 19:11:50 +0900

    test(CreateRandomNumberTest): make duplication test code, range in (1, 9)

--- src/test/java/baseball/CreateRandomNumberTest.java
+++ src/test/java/baseball/CreateRandomNumberTest.java
@@ -2,6 +2,9 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
 
@@ -9,13 +12,21 @@ class CreateRandomNumberTest {
 
   @Test
   void numberMaker() {
-    // 길이 검사
     String testNumber = CreateRandomNumber.numberMaker();
+
+    // 길이 검사
     assertThat(testNumber.length()).isEqualTo(3);
 
-    // 중복 검사
+    // 1 ~ 9 숫자 검사
     for (int i = 0; i < testNumber.length(); i++) {
       assertThat(testNumber.charAt(i)).isBetween('1', '9');
     }
+
+    // 중복 검사
+    Set<Character> set = new HashSet<>();
+    for (int i = 0; i < testNumber.length(); i++) {
+      set.add(testNumber.charAt(i));
+    }
+    assertThat(testNumber.length()).isEqualTo(set.size());
   }
 }
