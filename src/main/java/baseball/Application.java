27027a5c6f5ab03f3678141b833d52e79e7bca8f
hoiae <donald819@naver.com>
2022-11-07 20:12:31 +0900

    난수ë° 발생 메소드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,9 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
@@ -9,16 +13,14 @@ public class Application {
         do{
             //1 재시작, 2 종료
             int judgeNum = JudgeNum();
-            if(judgeNum == 2) {
+            if(judgeNum == 2)
                 break;
-            }
         }
         while(true);
 
         /*재시작 종료 판별 메소드 구현*/
         /* 사용자 입력값이 1 재시작, 2 종료 메소드 구현 */
         /* 사용자 입력 메소드 구현*/
-        /* 난수 발생 메소드 구현 */
         /* 스트라이크,볼,낫싱 판단 메소드 구현*/
         /*입력 예외 처리, 예외 발생시 IllegalArgumentException 발생 시키기.*/
 
@@ -35,5 +37,17 @@ public class Application {
                 return 2;
             System.out.println("잘못된 값을 입력하셨습니다.");
         }
+    }
+    /* 난수 발생 메소드 구현 */
+    public static List<Integer> makeRandomNum(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        System.out.println(computer);
+        return computer;
     }
 }
