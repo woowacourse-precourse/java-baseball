41692385bbc34b39c5f114b1a271b9fda99a5af5
MuSeung <peter0757@nvaer.com>
2022-11-07 21:40:35 +0900

    feat: Application의 main 구현
    Application
        - 시작 시 처음 한번만 시작메세지 출력
        - 반복문으로 computer와 NumberBaseBallGamer객체 생성 반복
        - computer에 NonDuplicateRandomNumber를 이용해 랜덤 숫자 담고 NumberBaseBallGamer 생성자의 인수로 사용
        - 게임 이후 재시작 여부를 묻는 메세지 출력
        - 종료를 원하는 입력이 주어지면 반복문을 break로 탈출하며 main 종료

--- src/main/java/baseball/ConstantAndMessage.java
+++ src/main/java/baseball/ConstantAndMessage.java
@@ -21,4 +21,9 @@ public class ConstantAndMessage {
     public static final String NOTHING_MESSAGE = "낫싱";
     public static final String GETTING_USER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String GAME_END_MESSAGE = NUMBER_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final int RESTART_NUMBER = 1;
+    public static final int TERMINATION_NUMBER = 2;
+    public static final String RESTART_MESSAGE = "게임을 새로 시작하려면 " + RESTART_NUMBER
+            + ", 종료하려면 "+ TERMINATION_NUMBER+"를 입력하세요.";
 }
