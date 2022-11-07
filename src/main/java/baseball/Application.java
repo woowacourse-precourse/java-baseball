6f75d4c1ce3e77b9491e94e97ab6bd3808d290cd
dev-green-flamingo <devgflamingo@gmail.com>
2022-11-08 02:20:19 +0900

    feat(game): 게임 진행
    스트라이크 3회가 충족되면 게임을 종료한다.
    프로젝트 초기에 작성된 테스트 유닛으로도 판별 가능하므로 별도의 테스트 코드는 만들지
    않았다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,7 +8,41 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
+        String line = "1";
+        while (!line.equals("2")) {
+            game(createBaseBall());
+            line = Console.readLine();
+            if (!line.matches("^[12]$")) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 
+    public static void game(List<Integer> computer) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String line = Console.readLine();
+            List<Integer> input = convertBaseBallByLine(line);
+            int strike = countStrike(input, computer);
+            int ball = countBall(input, computer);
+            if (ball == 0 && strike == 0) {
+                System.out.println("낫싱");
+                continue;
+            }
+            String output = "";
+            if (ball > 0) {
+                output += String.format("%d볼 ", ball);
+            }
+            if (strike > 0) {
+                output += String.format("%d스트라이크", strike);
+            }
+            System.out.println(output);
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return;
+            }
+        }
     }
 
     public static int countStrike(List<Integer> input, List<Integer> computer) {
