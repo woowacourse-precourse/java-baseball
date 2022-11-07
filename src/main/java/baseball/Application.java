72873311535160034060f5ddf2d82b5094004ccf
ddogga <hayeonlee60327@gmail.com>
2022-11-07 22:47:49 +0900

    정답을 맞췄을 경우 출력멘트와 재시작,종료 선택지 제시 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,18 +11,30 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        String answer;
+        String quitOrContinue;
+
         List<Integer> computer_num = Randoms.pickUniqueNumbersInRange(1,9,3);
         System.out.println("computer_num = " + computer_num);
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.println("숫자를 입력해주세요 : ");
-        String answer = Console.readLine();
+        answer = Console.readLine();
 
         int strike = checkStrike(computer_num,answer);
 
         int ball = checkBall(computer_num,answer);
 
-        printResult(strike,ball);
+        if (strike == 3) {
+            quitOrContinue = askContinue();
+
+
+        } else {
+            printResult(strike,ball);
+        }
+
+
+
 
     }
 
@@ -63,7 +75,16 @@ public class Application {
             if (strike > 0) {
                 System.out.print(strike+"스트라이크 ");
             }
+
         }
+    }
+
+    public static String askContinue() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String answer = Console.readLine();
+
+        return answer;
     }
 
 
