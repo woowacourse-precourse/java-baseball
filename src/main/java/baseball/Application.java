40cf92c113fb91fddd0eedc3dcc325a04fb01e58
jhsseonn <hosu0125@gmail.com>
2022-11-08 02:16:39 +0900

    refactor: 기존 application 파일에 작성했던 기능 구현 모두 클래스 단위로 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,16 +11,5 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        if (strike==3) {
-            System.out.println(strike+"스트라이크");
-            //프로그램 종료
-        } else {
-            if (matchCount==0) {
-                System.out.println("낫싱");
-            } else {
-                int ball = matchCount - strike;
-                System.out.println(ball+"볼 "+strike+"스트라이크");
-            }
-        }
     }
 }
