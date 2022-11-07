ba8cf24c51b5c1607979b4ba611cdad119c2ba45
doehy <dbehxks02@naver.com>
2022-11-07 23:08:48 +0900

    feat : 게이머 숫자 입력값의 길이가 3이랑 맞는지 검사

--- src/main/java/baseball/Util.java
+++ src/main/java/baseball/Util.java
@@ -1,4 +1,11 @@
 package baseball;
 
+import static baseball.Constant.GAMEPLAY_SIZE;
+
 public class Util {
+    public static void isWeirdSize(String input_Number){
+        if(input_Number.length() != GAMEPLAY_SIZE){
+            throw new IllegalArgumentException();
+        }
+    }
 }
