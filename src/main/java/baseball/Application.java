215aa2929157c983b5a9063e7c424261b7859d5e
yxnxx <jh4rgh0@gmail.com>
2022-11-08 00:46:27 +0900

    feat: add making game flow

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import baseball.model.RandomNumGenerator;
+import baseball.model.UserInputValidation;
+import baseball.model.CompareInputNumber;
+import baseball.view.UserInput;
+import baseball.view.Output;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
+        RandomNumGenerator randomNum = new RandomNumGenerator();
+        UserInputValidation validateUserInput = new UserInputValidation();
+        Output outputResult = new Output();
 
+        final String ANSWER_OUTPUT = "3스트라이크";
+
+        List<Integer> random_num;
+        List<Integer> user_num;
+        String result;
+        boolean restart = true;
+
+        while(!restart) {
+            result = "";
+            random_num = randomNum.getRandomNum();
+            while(!result.equals(ANSWER_OUTPUT)) {
+                user_num = validateUserInput.getUserInput(UserInput.getUserNumber());
+                result = outputResult.outputString(random_num, user_num);
+                System.out.println(result);
+            }
+        }
     }
 }
