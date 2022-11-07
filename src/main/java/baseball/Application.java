23ef9838436f195f3b45b9d62ecd18e9749bec0b
sunhokim1 <srrsr14@gmail.com>
2022-11-07 18:25:48 +0900

    refactor : array관련 메서드 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,4 @@
 package baseball;
-import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -27,13 +26,8 @@ public class Application {
             System.out.println(countBallStrike.get(1) + "스트라이크");
         return end;
     }
-    public static List<Integer> createCountArray(int ball, int strike) {
-        List<Integer> countBallStrike = new ArrayList<>();
-        countBallStrike.add(ball);
-        countBallStrike.add(strike);
-        return countBallStrike;
-    }
     public static boolean compareNumber(List<Integer> playerNumber, List<Integer> randomNumber) {
+        ArrayUtil arrayUtil = new ArrayUtil();
         int ball = 0;
         int strike = 0;
         for (int i=0;i<3;++i) {
@@ -42,28 +36,14 @@ public class Application {
             else
                 ball += countBall(playerNumber.get(i), randomNumber, i);
         }
-        return printBallStrike(createCountArray(ball, strike));
-    }
-    public static List<Integer> arrayPlayerNumber(String number) {
-        List<Integer> playerNumber = new ArrayList<>();
-        for (int i=0;i<number.length();++i)
-            playerNumber.add((int)number.charAt(i) - 48);
-        return playerNumber;
-    }
-    public static List<Integer> createRandomNumber() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber))
-                computer.add(randomNumber);
-        }
-        return (computer);
+        return printBallStrike(arrayUtil.createCountArray(ball, strike));
     }
     public static void inputGame(boolean end, List<Integer> randomNumber) {
+        ArrayUtil arrayUtil = new ArrayUtil();
         InputUtil inputUtil = new InputUtil();
         while (!end) {
             System.out.print("숫자를 입력해주세요 : ");
-            end = compareNumber(arrayPlayerNumber(inputUtil.inputPlayerNumber()), randomNumber);
+            end = compareNumber(arrayUtil.arrayPlayerNumber(inputUtil.inputPlayerNumber()), randomNumber);
         }
     }
     public static void restartGame(boolean restart) {
@@ -71,8 +51,9 @@ public class Application {
             playGame();
     }
     public static void playGame() {
+        ArrayUtil arrayUtil = new ArrayUtil();
         InputUtil inputUtil = new InputUtil();
-        List<Integer> randomNumber = createRandomNumber();
+        List<Integer> randomNumber = arrayUtil.createRandomNumber();
         boolean end = false;
         inputGame(end, randomNumber);
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
---
+++ src/main/java/baseball/ArrayUtil.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayUtil {
+    public static List<Integer> createCountArray(int ball, int strike) {
+        List<Integer> countBallStrike = new ArrayList<>();
+        countBallStrike.add(ball);
+        countBallStrike.add(strike);
+        return countBallStrike;
+    }
+    public List<Integer> arrayPlayerNumber(String number) {
+        List<Integer> playerNumber = new ArrayList<>();
+        for (int i=0;i<number.length();++i)
+            playerNumber.add((int)number.charAt(i) - 48);
+        return playerNumber;
+    }
+    public List<Integer> createRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber))
+                computer.add(randomNumber);
+        }
+        return (computer);
+    }
+    }
