cb3dea0d8ef2f2087a392c92159380ffe7e1f2b2
jijongkwon <help3451@naver.com>
2022-11-07 16:31:59 +0900

    [Feat] baseBallComputer 클래스 생성

---
+++ src/main/java/baseball/BaseBallComputer.java
@@ -0,0 +1,25 @@
+package baseball;
+
+public class BaseBallComputer {
+    private int randomNumber = 0;
+
+    public void createRandomNumber() {
+
+    }
+
+    public void checkHitNumber() {
+
+    }
+
+    public int getRandomNumber() {
+        return this.randomNumber;
+    }
+
+    public int restartOrEnd(){
+        return 0;
+    }
+
+    public void printHitStatus(){
+
+    }
+}
