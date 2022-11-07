ad8573a2d67d693fc1bed912265f594c46a366f8
ShinKwang <sklee0206@naver.com>
2022-11-07 14:20:23 +0900

    feat: feat: Make ComputerNumberList
    숫자야구에서 상대가 사용할 숫자 리스트를 만든다. 이 리스트의 사이즈는 3이며, 서로 다른 숫자로 구성되어있다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static List<Integer> createComputerNumberList() {
+        List<Integer> computerNumberList = new ArrayList<>();
+        while (computerNumberList.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNumberList.contains(randomNumber)) {
+                computerNumberList.add(randomNumber);
+            }
+        }
+        return computerNumberList;
     }
 }
