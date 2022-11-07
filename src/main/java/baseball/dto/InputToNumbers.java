3c542339287ed7fc1f327507ea6dc74e6d2d7cac
이건회 <rg970604@naver.com>
2022-11-07 19:03:50 +0900

    feat (InputToNumbers.java) : 숫자가 아닌 문자열을 예외 처리

--- src/main/java/baseball/dto/InputToNumbers.java
+++ src/main/java/baseball/dto/InputToNumbers.java
@@ -2,9 +2,12 @@ package baseball.dto;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class InputToNumbers {
+
+    private static final String NUMBER_PATTERN = "^[1-999]*$";
 
     public static List<Integer> toNumbers(String userNumbersInput) {
         String[] userNumbers = userNumbersInput.split("");
@@ -12,6 +15,12 @@ public class InputToNumbers {
             .stream()
             .map(Integer::parseInt)
             .collect(Collectors.toList());
+    }
+
+    private static void validateUserNumberInputType(String userNumberInput) {
+        if (!Pattern.matches(NUMBER_PATTERN, userNumberInput)) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
     }
 
 }
