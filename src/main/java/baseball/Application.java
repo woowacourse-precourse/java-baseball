8de7ab2859a1d590b9b3db8966ff4249a42c8cb3
GilHun <suleemon1@naver.com>
2022-11-07 20:02:08 +0900

    refactor : createNum(), checkingNum() 메서드 생성
    프로그래밍 요구사항을 준수하기 위해 main(), startGame() 메서드에 있었던 코드 리팩토링
    createNum() : 임의의 서로 다른 세가지 수 생성하는 메서드
    checkingNum() : 입력값과 결과값 비교하여 결과 반환하는 메서드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,36 +1,31 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.*;
+
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Application {
-    public static void main(String[] args) {
-        List<Integer> computer = new ArrayList<>();
+    public static void main(String[] args) throws IllegalArgumentException{
         StrikeBall result = new StrikeBall();
-
-        while(computer.size()<3){
-            int randomNumber = Randoms.pickNumberInRange(1,9);
-            if(!computer.contains(randomNumber))
-                computer.add(randomNumber);
-        }
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         try {
             do{
+                List<Integer> computer = createNum();
                 startGame(computer,result);
             }while(endGame());
         }catch (IllegalArgumentException iae){
-            return;
+            throw iae;
         }
     }
 
-    public static boolean startGame(List<Integer> computer, StrikeBall result) throws IllegalArgumentException{
+    public static void startGame(List<Integer> computer, StrikeBall result) throws IllegalArgumentException{
         while(true){
-            int strike = 0;
-            int ball = 0;
+            int[] ballStrike = new int[2];
 
             System.out.print("숫자를 입력해주세요 : ");
-            String input = Console.readLine();
+            String input = Console.readLine().trim();
             try{
                 // 3자리 맞는지, 숫자 맞는지
                 isRightNum(input);
@@ -38,18 +33,14 @@ public class Application {
                 throw e;
             }
 
-            for(int index=0;index<input.length();index++){
-                if(isContain(computer,input.charAt(index))){
-                    if(isCorrectPosition(computer,input.charAt(index),index)) strike++;
-                    else ball++;
-                }
-            }
-            result.setBall(ball);
-            result.setStrike(strike);
-            if(strike==3){
+            checkingNum(computer,input,ballStrike);
+            result.setBall(ballStrike[0]);
+            result.setStrike(ballStrike[1]);
+
+            if(result.getStrike()==3){
                 System.out.println(result.toString());
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
-                return true;
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                return;
             }else{
                 System.out.println(result.toString());
                 continue;
@@ -57,14 +48,31 @@ public class Application {
         }
     }
 
+    public static ArrayList<Integer> createNum(){
+        ArrayList<Integer> computer = new ArrayList<>();
+        while(computer.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computer.contains(randomNumber))
+                computer.add(randomNumber);
+        }
+        return computer;
+    }
+
     public static void isRightNum(String input) throws IllegalArgumentException {
         try{
-            if(input.length()!=3)
-                throw new IllegalArgumentException();
-            int inputNum = Integer.parseInt(input);
+            if(input.length()!=3) throw new IllegalArgumentException();
+            Integer.parseInt(input);
         }catch (NumberFormatException nfe){
             throw nfe;
         }
+    }
+
+    public static int[] checkingNum(List<Integer> computer, String input, int[] ballStrike){
+        for(int index=0;index<input.length();index++){
+            if(isContain(computer,input.charAt(index)) && isCorrectPosition(computer,input.charAt(index),index)) ballStrike[1]++;
+            else if(isContain(computer,input.charAt(index))) ballStrike[0]++;
+        }
+        return ballStrike;
     }
 
     public static boolean isContain(List<Integer> computer, char inputElement){
@@ -80,7 +88,7 @@ public class Application {
     }
 
     public static boolean endGame(){
-        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요 : ");
         String input = Console.readLine();
         if(input.equals("1")) return true;
         else return false;
