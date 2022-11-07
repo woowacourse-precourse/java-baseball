7f429d12c91cc06d9275f17d35774225a29f58de
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-11-08 01:57:41 +0900

    feat(PlayNumberBaseballGame): add createRandomNumberOfComputer feature

--- src/main/java/baseball/PlayNumberBaseballGame.java
+++ src/main/java/baseball/PlayNumberBaseballGame.java
@@ -1,4 +1,20 @@
 package baseball;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class PlayNumberBaseballGame {
+
+    public static List<Integer> createRandomNumberOfComputer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
