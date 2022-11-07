24c90685f7b0305293dc925e59c8223bf697d8de
limecats <limecats0331@gmail.com>
2022-11-07 16:12:34 +0900

    test(Game): test getGameResultOfJudge
    test method
    - game is end
    - nothing
    - only ball
    - only strike
    - mixed
    count

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -1,9 +1,9 @@
 package baseball;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.lang.reflect.Method;
 import java.util.List;
 
@@ -14,7 +14,7 @@ class GameTest {
 
     @Nested
     @DisplayName("중복이 아닌 숫자를 찾는 메소드")
-    class getRandomIntegerExcludeInList {
+    class GetRandomIntegerExcludeInList {
 
 
         @RepeatedTest(1000)
@@ -48,6 +48,94 @@ class GameTest {
 
             //Then
             assertThat(number).isNotEqualTo(1);
+        }
+    }
+
+    @Nested
+    @DisplayName("예측한 답에 대한 결과 출력")
+    class GetGameResultOfJudge {
+
+        private final PrintStream standardOut = System.out;
+        private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+        @BeforeEach
+        void setUp() {
+            System.setOut(new PrintStream(outputStreamCaptor));
+        }
+
+        @Test
+        @DisplayName("낫싱일때")
+        void nothing() throws Exception {
+            //Given
+            Game game = new Game();
+            List<Integer> judge = List.of(0, 0);
+
+            //When
+            boolean gameResultOfJudge = game.getGameResultOfJudge(judge);
+
+            //Then
+            assertThat(outputStreamCaptor.toString().trim()).isEqualTo("낫싱");
+            assertThat(gameResultOfJudge).isFalse();
+        }
+
+        @Test
+        @DisplayName("게임이 종료될 때")
+        void gameEnd() throws Exception {
+            //Given
+            Game game = new Game();
+            List<Integer> judge = List.of(0, 3);
+
+            //When
+            boolean gameResultOfJudge = game.getGameResultOfJudge(judge);
+
+            //Then
+            assertThat(outputStreamCaptor.toString().trim()).isEqualTo("3스트라이크");
+            assertThat(gameResultOfJudge).isTrue();
+        }
+
+        @Test
+        @DisplayName("볼만 있을 때")
+        void onlyBall() throws Exception {
+            //Given
+            Game game = new Game();
+            List<Integer> judge = List.of(2, 0);
+
+            //When
+            boolean gameResultOfJudge = game.getGameResultOfJudge(judge);
+
+            //Then
+            assertThat(outputStreamCaptor.toString().trim()).isEqualTo("2볼");
+            assertThat(gameResultOfJudge).isFalse();
+        }
+
+        @Test
+        @DisplayName("스트라이크만 있을 때")
+        void onlyStrike() throws Exception {
+            //Given
+            Game game = new Game();
+            List<Integer> judge = List.of(0, 2);
+
+            //When
+            boolean gameResultOfJudge = game.getGameResultOfJudge(judge);
+
+            //Then
+            assertThat(outputStreamCaptor.toString().trim()).isEqualTo("2스트라이크");
+            assertThat(gameResultOfJudge).isFalse();
+        }
+
+        @Test
+        @DisplayName("둘다 있을 때")
+        void mixed() throws Exception {
+            //Given
+            Game game = new Game();
+            List<Integer> judge = List.of(2, 1);
+
+            //When
+            boolean gameResultOfJudge = game.getGameResultOfJudge(judge);
+
+            //Then
+            assertThat(outputStreamCaptor.toString().trim()).isEqualTo("2볼 1스트라이크");
+            assertThat(gameResultOfJudge).isFalse();
         }
     }
 
