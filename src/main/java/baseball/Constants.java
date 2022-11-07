2e9cb859a2b2d705415a22e03679a5d89e3392b6
tdmdgh <tdmdgh@gmail.com>
2022-11-07 18:06:42 +0800

    fix: edit  END_MESSAGE

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public final static String END = "2";
     public final static String BEGIN_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public final static String INPUT_NUMBER = "숫자를 입력해주세요 : ";
-    public final static String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료.";
+    public final static String END_MESSAGE = Max_size+"개의 숫자를 모두 맞히셨습니다! 게임 종료.";
     public final static String CONT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public final static String INPUT_EXCEPTION = "잘못된 입력입니다. 프로그램이 종료됩니다.";
 }
