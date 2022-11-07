75ca70a19322cce96422ca095bca434159dc693f
MinjeongKong <kong1301@ajou.ac.kr>
2022-11-08 00:41:20 +0900

    feat(Game): add method

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -9,12 +9,34 @@ public class Game {
     private int startRange;
     private int endRange;
 
+    public Game(int digit, int startRange, int endRange) {
+        this.computer = null;
+        this.user = null;
+        this.digit = digit;
+        this.startRange = startRange;
+        this.endRange = endRange;
+    }
+
+    public void pickComputerNum() {
+        this.computer = new Computer(digit, startRange, endRange);
+    }
+
+    public void initTurn() {
+        this.user = new User();
+        this.strike = 0;
+        this.ball = 0;
+    }
+
     public Computer getComputer() {
         return computer;
     }
 
     public User getUser() {
         return user;
+    }
+
+    public int getDigit() {
+        return digit;
     }
 
     public int getStrike() {
@@ -25,4 +47,11 @@ public class Game {
         return ball;
     }
 
+    public void plusStrike() {
+        this.strike++;
+    }
+
+    public void plusBall() {
+        this.ball++;
+    }
 }
