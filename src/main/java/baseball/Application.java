6059910c64d2a32f050357f976e547952fb6151d
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-07 21:54:43 +0900

    feat: 사용자 값을 입력받는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
@@ -7,11 +8,17 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         List<Integer> computer = getRandomNumberList();
+        String userInput = getUserInput();
     }
 
     private static List<Integer> getRandomNumberList() {
         List computer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
         return computer;
+    }
+
+    private static String getUserInput() {
+        String userInput = Console.readLine();
+        return userInput;
     }
 
 }
