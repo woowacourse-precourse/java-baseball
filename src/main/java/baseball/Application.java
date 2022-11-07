076a7c0e1a83ec3bb4ab666fe32a9e1ee27ccaf3
정현수 <96504592+hyunsb@users.noreply.github.com>
2022-11-07 21:10:27 +0900

    feat: 게임시작, 재시작 문구를 출력하는 기능 추가 printStartSentence: 게임시작 문구 출력 printReStartSentence: 게임 재시작 문구 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -107,4 +107,14 @@ public class Application {
         }
         return ball;
     }
+
+    // TODO : 게임 시작 문구를 출력하는 함수
+    public void printStartSentence(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    // TODO : 게임을 재시작 선택 문구를 출력하는 함수
+    public void printReStartSentence() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
