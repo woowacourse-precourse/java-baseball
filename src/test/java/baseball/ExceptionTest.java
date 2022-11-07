dbe6ed0314840109c9db5447677013a8dfcb9ea4
Mun Kyoung-min <mun9659@naver.com>
2022-11-08 01:28:15 +0900

    [Test] Exception Test Add, Refactoring

--- src/test/java/baseball/ExceptionTest.java
+++ src/test/java/baseball/ExceptionTest.java
@@ -3,45 +3,47 @@ package baseball;
 import baseball.domain.User;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class InputTest {
+public class ExceptionTest {
 
     @Test
     void 예외_테스트_길이() {
         String answer = "1234";
-        User user = new User();
 
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> user.userNumberLength(answer))
+                assertThatThrownBy(() -> User.userNumberLength(answer))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
+    
+    @Test
+    void 예외_테스트_범위밖() {
+    	String answer = "108";
+    	
+    	assertSimpleTest(() ->
+    			assertThatThrownBy(() -> User.userNumberRange(answer))
+    					.isInstanceOf(IllegalArgumentException.class)
+    	);
+    }
+    
     @Test
     void 예외_테스트_중복() {
         String answer = "222";
-        User user = new User();
-        Integer[] userArr = {1, 2, 3};
-        ArrayList<Integer> userList = new ArrayList<>(Arrays.asList(userArr));
-        user.userNumber = userList;
 
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> user.userNumberSameCheck(answer))
+                assertThatThrownBy(() -> User.userNumberSameCheck(answer))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
     @Test
     void 예외_테스트_재시작_숫자() {
-        User user = new User();
         assertSimpleTest(() ->
-                assertThatThrownBy(() -> user.finishNumber("3"))
+                assertThatThrownBy(() -> User.finishNumber("3"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
