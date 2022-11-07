b1c6511d4fad06953f5df36a7e76ef3f334087e6
김태용 <amico741@naver.com>
2022-11-07 18:23:19 +0900

    refactor: ArrayList 초기화 위치 변경
    생성자에서만 초기화하면 배열 크기가 유지되어 가는 문제점 발생

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -3,16 +3,16 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Player {
-  private final ArrayList<Integer> numbers;
+  private ArrayList<Integer> numbers;
   private static final int MAX_SIZE = 3;
-  public Player(){
-    this.numbers = new ArrayList<>();
-  };
+  public Player(){};
 
   public ArrayList<Integer> decideNumbers(){
+    System.out.print("숫자를 입력해주세요 : ");
     String input = Console.readLine();
     String pattern = "^[0-9]*$";
     boolean result = Pattern.matches(pattern, input);
@@ -22,12 +22,14 @@ public class Player {
     }
     addNumber(input);
     if(this.numbers.size() != MAX_SIZE){
+      System.out.println(this.numbers);
       throw new IllegalArgumentException("서로 다른 3개의 숫자를 입력해야 합니다.");
     }
     return this.numbers;
   }
 
   public void addNumber(String input){
+    this.numbers = new ArrayList<>();
     for(int i = 0; i < input.length(); i++){
       int number = Integer.parseInt(String.valueOf(input.charAt(i)));
       if(!this.numbers.contains(number)){
