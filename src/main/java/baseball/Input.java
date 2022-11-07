962cf14518632ab9e3795aa5be6f977876153303
Fizioo0102 <a27313797@gmail.com>
2022-11-07 23:42:03 +0900

    feat: cutNumber() 함수 생성
    - 사용자의 입력이 올바른 입력이라면 문자열을 한개씩 쪼개 List<>에 저장해주는 함수 생성

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -30,7 +30,6 @@ public class Input {
         }
         return true;
     }
-
     static boolean userNumberRepeatCheck(String input) {
         List<Integer> equal_number = new ArrayList<>();
 
@@ -42,7 +41,6 @@ public class Input {
         }
         return true;
     }
-
     static boolean userNumberRangeCheck(String input) {
         for (int i = 0; i < input.length(); i++) {
             if (input.charAt(i)-'0' <= 0) {
@@ -51,7 +49,6 @@ public class Input {
         }
         return true;
     }
-
     static void userNumberErrorCheck(String input) {
         if (!userNumberLengthCheck(input)) {
             throw new IllegalArgumentException("입력된 수의 길이가 올바르지 않습니다");
@@ -63,6 +60,12 @@ public class Input {
             throw new IllegalArgumentException("올바른 수의 범위 입력이 아닙니다");
         }
     }
+    static List<Integer> cutNumber(String input) {
+        List<Integer> cut = new ArrayList<>();
 
-
+        for (int i = 0; i < input.length(); i++) {
+            cut.add(input.charAt(i) - '0');
+        }
+        return cut;
+    }
 }
