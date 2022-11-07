51f6e8c0a2f3da883ae9821cd7b2a8afbbdacbb5
Fizioo0102 <a27313797@gmail.com>
2022-11-07 23:52:27 +0900

    feat: endCheck() 함수 생성
    - strike가 3이라면 정답을 맞춘것이다.
    - "3스트라이크"
    - "3개의 숫자를 모두 맞히셨습니다! 게임 종료" 메시지와 함께 종료의 의미로 true를 반환해준다.
    - 스트라이크가 3개가 아니라면 아직 정답이 아니기 때문에 false를 반환 해준다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -33,4 +33,12 @@ public class BaseballGame {
         return false;
     }
 
+    static boolean endCheck(int strike) {
+        if (strike == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
+    }
 }
