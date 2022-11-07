07dee5e30384a6a5d02e74432d3e9b61ccf6f76f
CHAYEONIL <jeay123@naver.com>
2022-11-08 02:38:13 +0900

    feature: 볼,스트라이크,낫싱 판별 구현

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,8 +1,10 @@
 package baseball.controller;
 
 import baseball.exception.WrongContinueException;
+import baseball.utill.StartGame;
 import baseball.utill.Random;
 import baseball.view.Input;
+import baseball.view.Output;
 
 public class BaseballController {
     public static void run() {
@@ -16,19 +18,24 @@ public class BaseballController {
 
         Random random = new Random();
         while(strikeCount != 3) {
-
+            StartGame startGame = new StartGame(Input.startMessage());
+            ballCount = startGame.calculateBallCount(random.randomNumbers());
+            strikeCount = startGame.calculateStrikeCount(random.randomNumbers());
+            Output.printResult(ballCount, strikeCount);
         }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     private static void checkCountinue() {
         String continueMessage = Input.askNextGame();
 
-        if (continueMessage.equals(1)){
+        if (continueMessage.equals("1")){
             runBaseBall();
             return;
         }
 
-        if (continueMessage.equals(2)){
+        if (continueMessage.equals("2")){
+            System.out.println("게임 종료");
             return;
         }
 
