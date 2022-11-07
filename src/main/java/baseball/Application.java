39e406ea095cd8dbf6bc783f92b0b4efc3203651
jeongseungchan <jschan0911@naver.com>
2022-11-07 17:48:35 +0900

    Feat: 랜덤으로 수를 생성 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> randomNumList = makeRandomNum();
     }
+    public static List<Integer> makeRandomNum() {
+        List<Integer> randomNumList = new ArrayList<>();
+        while(randomNumList.size() < 3) {
+            int newRandomNum = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumList.contains(newRandomNum)) {
+                randomNumList.add(newRandomNum);
+            }
+        }
+        return randomNumList;
+    }
+
 }
