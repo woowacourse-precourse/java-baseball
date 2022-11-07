ad516b15aeba23eecc4ad1427b71bd56d042c01f
E1psycongr00 <39326175+E1psycongr00@users.noreply.github.com>
2022-11-07 19:30:23 +0900

    style(controller): 클래스 첫 줄 띄어쓰기 추가

--- src/main/java/custom/controller/BaseBallController.java
+++ src/main/java/custom/controller/BaseBallController.java
@@ -7,6 +7,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class BaseBallController {
+
     private final BaseBallService baseBallService;
 
     public BaseBallController(BaseBallService baseBallService) {
