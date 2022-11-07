019a03c036a6c0022af4b3b218065e7e1aa2a001
phj1120 <hyeonj1998@naver.com>
2022-11-07 22:04:06 +0900

    feat: 1-5 요구사항 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,37 +11,41 @@ public class Application {
     public static void main(String[] args) {
         List<Integer> computerNumber = Utils.pickComputerNumber();
 
-        String line = Console.readLine();
-        List<Integer> userNumber = new ArrayList<>();
-        for (int i = 0; i < line.length(); i++) {
-            userNumber.add(Character.getNumericValue(line.charAt(i)));
-        }
-
-        int ball = 0;
-        int strike = 0;
-        for (int i = 0; i < computerNumber.size(); i++) {
-            // strike 확인
-            if (computerNumber.get(i) == userNumber.get(i)) {
-                strike++;
-            } else if (computerNumber.contains(userNumber.get(i))) {
-                // ball 확인
-                ball++;
+        boolean gameStatus = true;
+        while (gameStatus) {
+            String line = Console.readLine();
+            List<Integer> userNumber = new ArrayList<>();
+            for (int i = 0; i < line.length(); i++) {
+                userNumber.add(Character.getNumericValue(line.charAt(i)));
             }
-        }
 
-        if (ball == 0 && strike == 0) {
-            System.out.println("낫싱");
-        } else if (ball != 0 && strike != 0) {
-            System.out.println(ball + "볼 " + strike + "스트라이크");
-        } else if (ball != 0) {
-            System.out.println(ball + "볼");
-        } else if (strike != 0) {
-            System.out.println(strike + "스트라이크");
-        }
+            int ball = 0;
+            int strike = 0;
+            for (int i = 0; i < computerNumber.size(); i++) {
+                // strike 확인
+                if (computerNumber.get(i) == userNumber.get(i)) {
+                    strike++;
+                } else if (computerNumber.contains(userNumber.get(i))) {
+                    // ball 확인
+                    ball++;
+                }
+            }
 
-        if (strike == 3) {
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        }
+            if (ball == 0 && strike == 0) {
+                System.out.println("낫싱");
+            } else if (ball != 0 && strike != 0) {
+                System.out.println(ball + "볼 " + strike + "스트라이크");
+            } else if (ball != 0) {
+                System.out.println(ball + "볼");
+            } else if (strike != 0) {
+                System.out.println(strike + "스트라이크");
+            }
 
+            if (strike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                gameStatus = false;
+            }
+
+        }
     }
 }
