235c82ab177195d006d89a20f54827ebaa37557a
이경범 <tkdwk567@naver.com>
2022-11-07 12:33:40 +0900

    feat : Game 클래스 생성
    여러 객체들을 합하여 게임을 시작하는 기능이다.
    
    아직은 절차지향적으로 프로그램이 미완성 단계이다.
    리팩터링 하면서 수정할 예정이다.

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,40 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Game {
+
+    public void start() {
+        Computer computer = new Computer(new NumberGenerator());
+        Referee referee = new Referee(new Rule());
+        Player player = new Player();
+
+        System.out.println(computer.getNumbers());
+
+        computer.giveNumbersToReferee(referee);
+        View.start();
+
+        boolean helper = false;
+        do {
+            System.out.print("숫자를 입력하세요 : ");
+            String readLine = Console.readLine();
+            player.sayNumbers(readLine);
+            player.giveNumbersToReferee(referee);
+
+            GameMessage gameMessage = referee.answerNumber();
+            helper = gameMessage.check();
+        } while (!helper);
+        {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String reStartNumber = Console.readLine();
+
+            if (Integer.parseInt(reStartNumber) == 1) {
+                restart();
+            }
+        }
+    }
+
+    private void restart() {
+        start();
+    }
+}
