6090b7316fce7bc3937566f7d632d8c4b4146f5e
Choi-JJunho <junho5336@gmail.com>
2022-11-08 01:10:07 +0900

    refactor: separate feature UserInputValidation
    UserInputValidation class
    - add validateUserInput method
    - add validateRandomAnswer method
    - add validateLength method
    - add validateIsNumber method
    - add validateDuplicate method

--- src/main/java/baseball/validation/UserInputValidation.java
+++ src/main/java/baseball/validation/UserInputValidation.java
@@ -1,2 +1,50 @@
-package baseball.validation;public class UserInputValidation {
+package baseball.validation;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static baseball.util.GameCode.*;
+import static baseball.util.RegexCode.*;
+
+public class UserInputValidation {
+
+    public UserInputValidation() {
+
+    }
+
+    public void validateUserInput(String input) {
+        validateIsNumber(input);
+        validateLength(input);
+        validateDuplicate(input);
+    }
+
+    public void validateRandomAnswer(List<Integer> input) {
+        StringBuilder builder = new StringBuilder();
+        input.forEach(builder::append);
+        String result = builder.toString();
+
+        validateLength(result);
+        validateDuplicate(result);
+    }
+
+    public void validateLength(String input) {
+        if (input.length() != MAX_NUMBER_LENGTH.getCode()) {
+            throw new IllegalArgumentException("number length must be 3");
+        }
+    }
+
+    public void validateIsNumber(String input) {
+        if (input.matches(REGEX_NOT_NUMBER.getRegex())) {
+            throw new IllegalArgumentException("input value must be number");
+        }
+    }
+
+    public void validateDuplicate(String input) {
+        Pattern pattern = Pattern.compile(REGEX_HAS_DUPLICATE_VALUE.getRegex());
+        Matcher matcher = pattern.matcher(input);
+        if (matcher.find()) {
+            throw new IllegalArgumentException("number must not be duplicated");
+        }
+    }
 }
