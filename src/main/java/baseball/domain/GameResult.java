10bce9f0f15d0d33e14c1e4960c6ea2fd3837203
Basak <chgy2131@naver.com>
2022-11-08 01:02:59 +0900

    feat(GameResult): 야구 게임 결과를 저장하는 클래스
    - 변수(기본값)
      - ball(int=0)
      - strike(int=0)
      -
    result(GameState=GameState.NOTHING)

--- src/main/java/baseball/domain/GameResult.java
+++ src/main/java/baseball/domain/GameResult.java
@@ -1,4 +1,37 @@
 package baseball.domain;
 
 public class GameResult {
+    private static final int TARGET_STRIKE = 3;
+    private int ball = 0;
+    private int strike = 0;
+    private GameState state = GameState.NOTHING;
+
+    public int getBall() {
+        return ball;
+    }
+    public int getStrike() {
+        return strike;
+    }
+    public GameState getState() {
+        return state;
+    }
+    public void addStrike() {
+        strike++;
+        if(isCorrect()) {
+            state = GameState.CORRECT;
+        } else {
+            numberAtLeastOnce();
+        }
+    }
+    public void addBall() {
+        ball++;
+        numberAtLeastOnce();
+    }
+    // addStrike 또는 addBall이 한번이라도 실행되었으면 NOTHING에서 GOOD으로 상태를 변환하는 함수
+    private void numberAtLeastOnce() {
+        state = GameState.GOOD;
+    }
+    private boolean isCorrect() {
+        return strike == TARGET_STRIKE;
+    }
 }
---
+++ src/test/java/baseball/domain/GameResultTest.java
@@ -0,0 +1,66 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameResultTest {
+
+    @Test
+    void addStrike() {
+        GameResult result = new GameResult();
+        result.addStrike();
+        assertThat(result.getStrike()).isEqualTo(1);
+    }
+
+    @Test
+    void addBall() {
+        GameResult result = new GameResult();
+        result.addBall();
+        assertThat(result.getBall()).isEqualTo(1);
+    }
+
+    @Test
+    void twoStrikeOneBall() {
+        GameResult result = new GameResult();
+        result.addStrike();
+        result.addBall();
+        result.addStrike();
+        assertThat(result.getStrike()).isEqualTo(2);
+        assertThat(result.getBall()).isEqualTo(1);
+    }
+
+    @Test
+    void stateNothing() {
+        GameResult result = new GameResult();
+        assertThat(result.getState()).isEqualTo(GameState.NOTHING);
+    }
+
+    @Test
+    void stateGoodStrike() {
+        GameResult result = new GameResult();
+        result.addStrike();
+        assertThat(result.getState()).isEqualTo(GameState.GOOD);
+    }
+    @Test
+    void stateGoodBall() {
+        GameResult result = new GameResult();
+        result.addBall();
+        assertThat(result.getState()).isEqualTo(GameState.GOOD);
+    }
+    @Test
+    void stateGoodStrikeBall() {
+        GameResult result = new GameResult();
+        result.addBall();
+        result.addStrike();
+        assertThat(result.getState()).isEqualTo(GameState.GOOD);
+    }
+    @Test
+    void stateCorrect() {
+        GameResult result = new GameResult();
+        result.addStrike();
+        result.addStrike();
+        result.addStrike();
+        assertThat(result.getState()).isEqualTo(GameState.CORRECT);
+    }
+}
