8d49ec9d4da0c973d2593e365c33aa7025078b6b
YuHayeon <youha0930@gmail.com>
2022-11-07 12:00:24 +0900

    chore: BaseballGame, Hint, Player, Computer 를 domain 패키지로 이동

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,4 +1,4 @@
-package baseball;
+package baseball.domain;
 
 import baseball.utils.RandomUtils;
 import java.util.ArrayList;
--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,4 +1,4 @@
-package baseball;
+package baseball.domain;
 
 import baseball.utils.BaseballCount;
 import baseball.utils.InputValidator;
