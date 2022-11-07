e28efb00b50427174b9ec156257280b84811ce83
OhRetry <OhRetry@users.noreply.github.com>
2022-11-07 19:40:46 +0900

    feat: add feature 'main loop'
    기능 요구 사항의 'main loop'를 main함수에 구현했다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,8 +7,21 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalArgumentException {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            startGame();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String cmd = Console.readLine();
+            if (cmd.equals("1")) {
+                continue;
+            } else if (cmd.equals("2")) {
+                break;
+            } else {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     public static List<Integer> getRandomNum() {
