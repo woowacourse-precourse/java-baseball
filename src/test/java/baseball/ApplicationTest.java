5887f06fdf875850f1106d0ca7fb8e7383865f0d
jungle <minuet1215@jbnu.ac.kr>
2022-11-07 18:22:20 +0900

    Test: NumberValidate 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -28,6 +28,38 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 빈_배열_입력() {
+        assertThatThrownBy(() -> {
+            new NumberValidator().validateIsEmptyNumbers("");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("numbers cannot be empty.");
+    }
+
+    @Test
+    void 중복_숫자_입력() {
+        assertThatThrownBy(() -> {
+            new NumberValidator().validateIsDuplicate("122");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("The number is duplicated.");
+    }
+
+    @Test
+    void 숫자가_아닌_값_입력() {
+        assertThatThrownBy(() -> {
+            new NumberValidator().validateInputRanges("abc");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("You must enter a number between 1 and 9.");
+    }
+
+    @Test
+    void 사용자_숫자_0_입력() {
+        assertThatThrownBy(() -> {
+            new NumberValidator().validateInputRanges("109");
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("You must enter a number between 1 and 9.");
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
