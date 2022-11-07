12d0738a317ece0a56f7f90f1ebfbf4bf6adad23
Wonkyun2 <kimwon1031@hanmail.net>
2022-11-08 08:33:08 +0900

    fix(messege): 테스트 실행 후 빠진 출력메시지를 발견하여 추가해줌

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -126,6 +126,7 @@ class entireGame extends playGame {
             List<String> playerNumber = Arrays.asList(playerNumberString.split(""));
             
             if(playerNumber.equals(computerNumber)) {
+                System.out.println("3스트라이크");
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 break;
