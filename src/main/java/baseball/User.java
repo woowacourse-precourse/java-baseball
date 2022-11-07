0800cc216c4a20655b45e859d0790fa3ac5fdeb1
0417taehyun <0417taehyun@gmail.com>
2022-11-08 07:48:04 +0900

    feat (User): User 클래스 추상적 구현
    User 클래스 세부 내용:
    - 클래스 변수 number
    - 인터페이스에서 상속 받은 메서드 create
    - 인터페이스에서 상속 받은 메서드 get
    - 클래스 메서드 isNumberOverMaxSize
    - 캘릇 메서드 isNumberInValidRange

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,25 @@
+package baseball;
+
+import java.util.List;
+
+public class User implements Number {
+    List<Integer> number;
+
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public List<Integer> get() {
+
+    }
+
+    public static boolean isNumberOverMaxSize(List<Integer> inputNumber) {
+
+    }
+
+    public static boolean isNumberInValidRange(List<Integer> inputNumber) {
+
+    }
+}
