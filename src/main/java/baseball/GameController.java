502d67c388da4546e4c33800ead1feab5be83b62
Yejin Shin <shinyejin0212@gmail.com>
2022-11-08 01:25:33 +0900

    feat(GameController): add restartGame, inputStatementNumber method
    add 2 method about restart
    - 재시작 선택시 true를 반환하는 restart method
    - player에게 재시작 or 종료 번호를 입력받는 inputStatementNumber 메소드

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -1,6 +1,47 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import static baseball.SystemMessage.REQUEST_RESTART_OR_GAME_OVER;
+import static baseball.SystemMessage.STRIKE_AND_GAME_OVER;
+
 public class GameController {
-    Opponent opponent = new Opponent();
-    Player player = new Player();
+    public static final String RESTART_STATEMENT = "1";
+    public static final String END_STATEMENT = "2";
+    private final Player player;
+    private final Opponent opponent;
+    private final Referee referee;
+
+    public GameController() {
+        player = new Player();
+        opponent = new Opponent();
+        referee = new Referee();
+    }
+    public void run() {
+        do {
+            readyForGame();
+            startGame();
+        } while (restartGame());
+    }
+    private void readyForGame() {
+        referee.saveOpponentAnswer( opponent.createAnswer() );
+    }
+    private void startGame() {
+        do {
+            referee.CaculateResult( player.getAnswer() );
+            System.out.println(referee.getResult() );
+        } while ( !referee.isAllStrike() );
+    }
+
+    private String inputStatementNumber() {
+        System.out.println( STRIKE_AND_GAME_OVER );
+        System.out.println( REQUEST_RESTART_OR_GAME_OVER );
+        return Console.readLine();
+    }
+
+    private boolean restartGame() {
+        String inputCommand = inputStatementNumber();
+        return inputCommand.equals(RESTART_STATEMENT);
+    }
+
 }
