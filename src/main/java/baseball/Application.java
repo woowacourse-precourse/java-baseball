01bc02ad9bb46a6d9e2f7c3b5e1add4540e726cd
zaysverse <zeze@jbnu.ac.kr>
2022-11-08 00:17:54 +0900

    Feat : Main함수에서 게임흐름 제어
    while문 안에서 게임을 종료하기 전까진 게임 무한 반복
    답을 맞추기 전까지 baseball.run()을 무한 반복

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,12 +10,16 @@ package baseball;
 - 게임이 끝난 경우 1:재시작, 2:종료
  */
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         while (true) {
             Baseball baseball = new Baseball();
             while (!baseball.run()) ;
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            if (Integer.parseInt(Console.readLine()) == 2) break;
         }
     }
 }
