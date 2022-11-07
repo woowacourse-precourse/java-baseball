00308e6b7a13168bf0d0607341663765bf56719a
janu01ary <janu01ary@gmail.com>
2022-11-07 19:06:30 +0900

    feat: make 3 random numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,12 +2,28 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    // 컴퓨터의 랜덤한 숫자를 생성해서 반환
+    private static List<Integer> getComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+        int randomNumber = 0;
+        while (computer.size() < 3) {
+            randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     private static void playGame() {
+        List<Integer> computer = getComputerNumber();
         while (true) {
             System.out.print("숫자를 입력해주세요 : ");
             String num = Console.readLine();
-            System.out.println(num);
         }
     }
 
