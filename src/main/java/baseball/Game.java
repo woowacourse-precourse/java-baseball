4cee03a0b6a418a66cd2d423731e7ac29d46d719
taekwon-dev <taekwon_dev@naver.com>
2022-11-07 23:52:49 +0900

    feat: 게임 진행 안내 메시지 출력 메소드
    - 게임 시작 안내
    - 숫자 입력 안내
    - 새로운 게임 시작 안내 (게임 중 정답 맞춘 뒤 종료하지 않고 새로운 게임 시작할 때)
    - 게임 종료 안내

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -19,4 +19,20 @@ public class Game {
         this.catcher = catcher;
         this.pitcher = pitcher;
     }
+
+    private void notifyGameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    private void notifyPitchBall() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    private void notifyGameRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    private void notifyGameEnd() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
