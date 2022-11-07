d11346c15528567f11c3323e552ef2cf97423f4b
LeeJejune <leejejune0625@gmail.com>
2022-11-07 14:41:17 +0900

    feat: 게임에 필요한 문자열을 출력해주는 메서드 구현

---
+++ src/main/java/baseball/game/Baseball.java
@@ -0,0 +1,15 @@
+    private void printStartMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    private void printEndMessage() {
+        System.out.print("3개의 숫자를 모두 맞히셨습니다! 게임을 종료합니다. \n");
+    }
+
+    private void prinGameEndMessage() {
+        System.out.print("게임 종료");
+    }
+
+    private void printRestartOrEndMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
