3ab8d21abc6cbddfa29d56e31ada376b61052a88
KimYongJun <rladydwns129@naver.com>
2022-11-07 20:46:54 +0900

    refactor: 난수 생성 기능 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,28 +10,6 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
 
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
-            }
-        }
-
-        boolean allCorrect = false;
-
-        while (!allCorrect) {
-            String userAnswer = Console.readLine();
-            for (int i = 0; i < 3; i++) {
-                if (userAnswer.charAt(i) - '0' != computer.get(i)) {
-                    allCorrect = false;
-                    System.out.println("not correct");
-                    break;
-                }
-                allCorrect = true;
-            }
-        }
-        System.out.println("correct");
     }
 
 }
---
+++ src/main/java/baseball/RandomNumberList.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumberList {
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 9;
+    private static final int NUMBER_LENGTH = 3;
+
+    private static List<BaseBallNumber> randomNumberList;
+
+    public RandomNumberList() {
+        this.randomNumberList = makeRandomNumberList();
+    }
+
+    private List<BaseBallNumber> makeRandomNumberList() {
+        List<BaseBallNumber> randomNumberList = new ArrayList<>();
+
+        while (randomNumberList.size() < NUMBER_LENGTH) {
+            BaseBallNumber randomNumber = new BaseBallNumber(Randoms.pickNumberInRange(MINIMUM_NUMBER, MAXIMUM_NUMBER));
+            if (!randomNumberList.contains(randomNumber)) {
+                randomNumberList.add(randomNumber);
+            }
+        }
+        return randomNumberList;
+    }
+}
