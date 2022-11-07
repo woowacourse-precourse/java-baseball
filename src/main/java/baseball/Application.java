4af532404d7f0498ba6b1ec5718146df375856a5
kimJaeHyeon <kmr2644@gmail.com>
2022-11-08 03:03:53 +0900

    fix: 시작전 문자열 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BaseballGame baseballGame = new BaseballGame(new View(), new BaseballService(new ComputerBaseball()));
+        System.out.println("숫자 야구 게임을 시작합니다");
         baseballGame.start();
     }
 }
