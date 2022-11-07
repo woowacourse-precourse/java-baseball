9ec255e60e063b3d8dc938fd3bed011c8987375a
miffyKing <whqudghk28@gmail.com>
2022-11-07 12:12:46 +0900

    test: Make tester that receives result from referee and compare to all possible outputs

--- src/test/java/baseball/RefereeTest.java
+++ src/test/java/baseball/RefereeTest.java
@@ -1,11 +1,15 @@
 package baseball;
 
-
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
@@ -27,70 +31,24 @@ class RefereeTest {
         System.setOut(originalOut);
         System.setErr(originalErr);
     }
-//
-//    @Test
-//    @Order(1)
-//    void ReferenceTestOneStrike() {
-//        Referee referee = new Referee();
-//
-//
-//        Referee.testReferee("156", "123");
-//        Assertions.assertThat("1스트라이크").isEqualTo(outContent.toString().trim());
-//    }
 
-//    @Test
-//    @Order(2)
-//    void ReferenceTestTwoStrike() {
-//        Referee.testReferee("126", "123");
-//        Assertions.assertThat("2스트라이크").isEqualTo(outContent.toString().trim());
-//    }
-//
-//    @Test
-//    @Order(3)
-//    void ReferenceTestThreeStrike() {
-//        Referee.testReferee("123", "123");
-//        Assertions.assertThat("3스트라이크").isEqualTo(outContent.toString().trim());
-//    }
-//
-//    @Test
-//    @Order(4)
-//    void ReferenceTestOneBall() {
-//        Referee.testReferee("417", "123");
-//        Assertions.assertThat("1볼").isEqualTo(outContent.toString().trim());
-//    }
-//
-//    @Test
-//    @Order(5)
-//    void ReferenceTestOneBallOneStrike() {
-//        Referee.testReferee("162", "123");
-//        Assertions.assertThat("1볼 1스트라이크").isEqualTo(outContent.toString().trim());
-//    }
-//
-//    @Test
-//    @Order(6)
-//    void ReferenceTestTwoBall() {
-//        Referee.testReferee("632", "123");
-//        Assertions.assertThat("2볼").isEqualTo(outContent.toString().trim());
-//    }
-//
-//    @Test
-//    @Order(7)
-//    void ReferenceTestTwoBallOneStrike() {
-//        Referee.testReferee("132", "123");
-//        Assertions.assertThat("2볼 1스트라이크").isEqualTo(outContent.toString().trim());
-//    }
-//
-//    @Test
-//    @Order(8)
-//    void ReferenceTestThreeBall() {
-//        Referee.testReferee("312", "123");
-//        Assertions.assertThat("3볼").isEqualTo(outContent.toString().trim());
-//    }
-//
-//    @Test
-//    @Order(9)
-//    void ReferenceTestNothing() {
-//        Referee.testReferee("567", "123");
-//        Assertions.assertThat("낫싱").isEqualTo(outContent.toString().trim());
-//    }
+    @Test
+    @Order(2)
+    void ReferenceTestDoesNotReturnNull() {
+        RandomNumber randomnumber = RandomGenerator.generateRandom(new StringBuilder(), new ArrayList<Integer>());
+        //Referee.testReferee("126", randomnumber);
+        Assertions.assertThat(Referee.testReferee("126", randomnumber)).isNotEqualTo("");
+    }
+
+    @Test
+    @Order(3)
+    void ReferenceTestDoesNotReturnSomethingElse100Times() {
+        for (int i = 0; i < 100; i++) {
+            RandomNumber randomnumber = RandomGenerator.generateRandom(new StringBuilder(), new ArrayList<Integer>());
+            String output = Referee.testReferee("126", randomnumber);
+            ArrayList<String> lineUps = new ArrayList<>(
+                Arrays.asList("낫싱", "1볼", "2볼", "3볼", "1볼 1스트라이크", "2볼 1스트라이크", "1스트라이크", "2스트라이크", "3스트라이크"));
+            Assertions.assertThat(lineUps.contains(output)).isNotEqualTo(false);
+        }
+    }
 }
