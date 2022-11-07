99706825e438fb6de86506a2c4e41bab0914b911
ckn1070 <ckn1070@naver.com>
2022-11-08 03:46:21 +0900

    feat(runBaseballGame): add a method of running one time of baseball game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,6 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Scanner;
 
 public class Application {
     public static List<Integer> generateRandomNumber() {
@@ -57,6 +56,46 @@ public class Application {
         }
         Collections.reverse(listInput);
         return listInput;
+    }
+
+    public static void runBaseballGame (){
+        List<Integer> standardNumber = new ArrayList<>(generateRandomNumber());
+        int strikeCount = 0;
+        int ballCount;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while(strikeCount != 3){
+            System.out.print("숫자를 입력해 주세요 : ");
+
+            String userString = Console.readLine();
+            List<Integer> userNumber =stringToList(userString);
+            List<Integer> strikeIndex =checkStrike(standardNumber, userNumber);
+            strikeCount = strikeIndex.size();
+            ballCount =checkBall(standardNumber, userNumber, strikeIndex);
+
+            if((strikeCount == 0) && (ballCount == 0)){
+                System.out.println("낫싱");
+                continue;
+            }
+
+            if(strikeCount == 3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하시려면 2를 입력하세요.");
+                break;
+            }
+
+            String ballCountString = "";
+            if(ballCount > 0){
+                ballCountString = ballCount + "볼 ";
+            }
+
+            String strikeCountString = "";
+            if(strikeCount > 0){
+                strikeCountString = strikeCount + "스트라이크";
+            }
+
+            System.out.println(ballCountString + strikeCountString);
+        }
     }
 
     public static void main(String[] args) {
