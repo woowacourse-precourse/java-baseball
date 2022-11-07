f31485d0cf04d6f5bb9762a2c6d780bf05b90f15
plusjob70 <plusjob70@gmail.com>
2022-11-07 17:07:35 +0900

    feat: 컴퓨터의 3자리 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,9 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
         printStartGameSentence();
@@ -7,5 +11,17 @@ public class Application {
 
     private static void printStartGameSentence() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    private static List<Integer> getComputerPick() {
+        List<Integer> numbers = new ArrayList<>();
+        int number;
+        while (numbers.size() < 3) {
+            number = Randoms.pickNumberInRange(1, 9);
+            if (!numbers.contains(number)) {
+                numbers.add(number);
+            }
+        }
+        return numbers;
     }
 }
