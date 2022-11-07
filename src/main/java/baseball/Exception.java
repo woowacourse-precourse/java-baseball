be5be1845efacd879607196d9fedb6ea86abf2a4
abbd2 <ajkety00@gmail.com>
2022-11-07 14:00:46 +0900

    feat(Exception): 예외 조건 수정

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -2,9 +2,9 @@ package baseball;
 
 public class Exception {
 
-    public void numberException(String number){
+    public void inpuutNumberException(String number){
         System.out.println("예외 처리 진입");
-        if(!number.matches("[1-9]]")){
+        if(number.length() < 3 || number.contains("0")){
             System.out.println("Exception");
             throw new IllegalArgumentException();
         }
