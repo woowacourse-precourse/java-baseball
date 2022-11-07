12b4205da4b49177e60c9cd44ed72c8e4eaf4f30
swonny <wonny921@gmail.com>
2022-11-07 16:04:07 +0900

    refactor(Application): ExceptionFor4 인스턴스 선언 제거, GameRepeatMessageException로 변경
    Exception 검사하는 메소드는 static 메소드이므로 인스턴스를 생성하지 않고 클래스명으로 메소드를 추가하도록 변경했다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     final static String QUIT_GAME_CONDITION = "2";
-    final static ExceptionFor4 exceptionFor4 = new ExceptionFor4();
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -13,7 +12,7 @@ public class Application {
             game.play();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             String repeat = Console.readLine();
-            exceptionFor4.checkException(repeat);
+            GameRepeatMessageException.verifyGameRepeatMessage(repeat);
             if (repeat.equals(QUIT_GAME_CONDITION)) {
                 break;
             }
