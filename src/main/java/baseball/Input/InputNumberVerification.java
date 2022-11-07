df28e5e047ab70e8f84b28baa78a106dd9f75866
jaehyeon <jhjh0022@naver.com>
2022-11-08 02:11:12 +0900

    refactor: 2중 예외 처리로 인해 각 메서드 void 로 수정

--- src/main/java/baseball/Input/InputNumberVerification.java
+++ src/main/java/baseball/Input/InputNumberVerification.java
@@ -4,43 +4,37 @@ import static baseball.tool.NumberSplitTool.*;
 
 public class InputNumberVerification {
 
-    public static boolean verifyNumber(int receivedNumber) {
+    public void verifyNumber(int receivedNumber) {
 
-        if (!verifyNumberOverlap(receivedNumber)) {
+        verifyNumberOverlap(receivedNumber);
+        verifyNumberBetweenOneAndNine(receivedNumber);
+        verifyNumberThreeDigit(receivedNumber);
+    }
+
+    private void verifyNumberOverlap(int receivedNumber) {
+        if(!(makeNumberSplitList(receivedNumber).size() == makeNumberSplitSet(receivedNumber).size())){
             throw new IllegalArgumentException("각자 다른 숫자를 입력해주세요");
         }
-
-        if (!verifyNumberBetweenOneAndNine(receivedNumber)) {
-            throw new IllegalArgumentException("1~9 사이의 숫자만 입력해주세요");
-        }
-
-        if (!verifyNumberThreeDigit(receivedNumber)) {
-            throw new IllegalArgumentException("3개의 숫자만 입력해주세요");
-        }
-
-        return true;
     }
 
-    private static boolean verifyNumberOverlap(int receivedNumber) {
-        return makeNumberSplitList(receivedNumber).size()
-                == makeNumberSplitSet(receivedNumber).size();
-    }
-
-    private static boolean verifyNumberBetweenOneAndNine(int receivedNumber) {
+    private void verifyNumberBetweenOneAndNine(int receivedNumber) {
 
         for (int oneNumber : makeNumberSplitList(receivedNumber)) {
             if (!(1 <= oneNumber && oneNumber <= 9)) {
-                return false;
+                throw new IllegalArgumentException("1~9 사이의 숫자만 입력해주세요");
             }
         }
-        return true;
     }
 
-    private static boolean verifyNumberThreeDigit(int receivedNumber) {
-        return makeNumberSplitList(receivedNumber).size() == 3;
+    private void verifyNumberThreeDigit(int receivedNumber) {
+        if (!(makeNumberSplitList(receivedNumber).size() == 3)){
+            throw new IllegalArgumentException("3개의 숫자만 입력해주세요");
+        }
     }
 
-    public static boolean verifyOneOrTwoButton(int receivedButtonNumber) {
-        return receivedButtonNumber == 1 || receivedButtonNumber == 2;
+    public void verifyOneOrTwoButton(int receivedButtonNumber) {
+        if(!(receivedButtonNumber == 1 || receivedButtonNumber == 2)){
+            throw new IllegalArgumentException("1, 2 둘 중 하나만 고르세요");
+        }
     }
 }
