3e7a4e3490d343c7f940386c202860f6a284be4d
GraphTree <graphnodesed@gmail.com>
2022-11-07 14:27:23 +0900

    feat : 맞춰야 하는 3자리 수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static List<Integer> createAnswer() {
+        List<Integer> answerNumbers = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            int number = Randoms.pickNumberInRange(0, 9);
+            if (!answerNumbers.contains(number)) {
+                answerNumbers.add(number);
+            }
+        }
+        return answerNumbers;
+
     }
 }
