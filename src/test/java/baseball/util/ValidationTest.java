53b3a87e729e0d345d1c123621d727b67c791828
GwansuShin <happysun7080@naver.com>
2022-11-07 16:35:44 +0900

    test: 검증 테스트
    기존 검증 테스트의 메소드명, import 수정
    Number DTO를 사용했던 이전 코드를 List를 사용하도록 수정

--- src/test/java/baseball/util/ValidationTest.java
+++ src/test/java/baseball/util/ValidationTest.java
@@ -1,23 +1,22 @@
 package baseball.util;
 
-import baseball.model.number.NumberDto;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 
-import static baseball.model.Validation.validateNumberNotRepeated;
+import static baseball.util.Validation.validateNumberNotRepeated;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ValidationTest {
     @Test
-    void 정상_숫자_검증() {
-        assertThat(validateNumberNotRepeated(new NumberDto(Arrays.asList(1, 2, 3))))
+    void 정상_숫자() {
+        assertThat(validateNumberNotRepeated(Arrays.asList(1, 2, 3), 3))
                 .isTrue();
     }
 
     @Test
-    void 중복_숫자_검증() {
-        assertThat(validateNumberNotRepeated(new NumberDto(Arrays.asList(1, 2, 2))))
+    void 중복_숫자() {
+        assertThat(validateNumberNotRepeated(Arrays.asList(1, 2, 2), 3))
                 .isFalse();
 
     }
