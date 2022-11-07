1137395923ac6632a6d2777ff6a2a0a61e5bfbf0
eom-tae-in <eti0728@daum.net>
2022-11-07 19:02:50 +0900

    feat(OutputView): printRestartOrEndGame() -> 정답을 맞춘 후에 재시작(1) 또는 종료(2)에 대해 물어보는 문구 출력

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,11 @@
+package baseball.view;
+
+import baseball.constant.Constant;
+
+public class OutputView {
+
+    public static void printRestartOrEndGame() {
+
+        System.out.println(Constant.SELECT_RESTART_OR_END_MESSAGE);
+    }
+}
