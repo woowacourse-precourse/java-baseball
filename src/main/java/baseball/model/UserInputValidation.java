a0b8db5825d15316a30e1285c182b492bde56c37
yxnxx <jh4rgh0@gmail.com>
2022-11-07 22:39:31 +0900

    feat: add changing input number to int

--- src/main/java/baseball/model/UserInputValidation.java
+++ src/main/java/baseball/model/UserInputValidation.java
@@ -2,16 +2,20 @@ package baseball.model;
 
 import baseball.view.UserInput;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInputValidation {
     private final static int USER_NUMBER_LENGTH = 3;
 
     //private String user_number;
 
-    public String getUserInput(String user_number) {
+    public List<Integer> getUserInput(String user_number) {
         isInputLengthValid(user_number);
         isInputDigit(user_number);
         isInputValueValid(user_number);
-        return UserInput.getUserNumber();
+
+        return changeInputtoInt(user_number);
     }
 
     public boolean isInputLengthValid(String user_number) throws IllegalArgumentException {
@@ -30,5 +34,13 @@ public class UserInputValidation {
     public boolean isInputValueValid(String user_number) throws IllegalArgumentException {
         if(user_number.contains("0")) throw new IllegalArgumentException();
         return true;
+    }
+
+    public List<Integer> changeInputtoInt(String user_number) {
+        List<Integer> input_list = new ArrayList<>();
+        for(int i = 0; i < USER_NUMBER_LENGTH; i++) {
+            input_list.add(Character.getNumericValue(user_number.charAt(i)));
+        }
+        return input_list;
     }
 }
