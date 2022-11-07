8567c599b925aaed72c5a0c59a621804feff6d79
Kloong1 <dkdlsmsm98@gmail.com>
2022-11-08 03:21:23 +0900

    test: compareInputAndAnswer() 메소드 테스트 코드 삭제
    compareInputAndAnswer() 메소드의 파라미터 타입이 변경되어서 기존의
    방식으로는 테스트가 불가능해짐. 따라서 해당 코드를 삭제함.

--- src/test/java/baseball/service/NumberBaseballServiceTest.java
+++ src/test/java/baseball/service/NumberBaseballServiceTest.java
@@ -25,48 +25,4 @@ class NumberBaseballServiceTest {
 
         Assertions.assertThat(answer).allMatch(num -> num >= 1 && num <= 9);
     }
-
-    @Test
-    void compareInputAndAnswer() {
-        //given
-        NumberBaseballService numberBaseballService = new NumberBaseballService();
-        List<Integer> answer = numberBaseballService.getAnswer();
-
-        //when
-        int digit0 = answer.get(0);
-        int digit1 = answer.get(1);
-        int digit2 = answer.get(2);
-
-        List<Integer> input1 = Arrays.asList(digit1, digit2, 10);
-        List<Integer> input2 = Arrays.asList(digit1, digit2, digit0);
-        List<Integer> input3 = Arrays.asList(10, 10, 10);
-        List<Integer> input4 = Arrays.asList(digit2, digit1, 10);
-
-        //then
-        //input == answer
-        StrikeBallCountDto result = numberBaseballService.compareInputAndAnswer(answer);
-        Assertions.assertThat(result.getStrikeCount()).isEqualTo(3);
-        Assertions.assertThat(result.getBallCount()).isEqualTo(0);
-
-        //input1 - 2 ball
-        result = numberBaseballService.compareInputAndAnswer(input1);
-        Assertions.assertThat(result.getStrikeCount()).isEqualTo(0);
-        Assertions.assertThat(result.getBallCount()).isEqualTo(2);
-
-        //input2 - 3 ball
-        result = numberBaseballService.compareInputAndAnswer(input2);
-        Assertions.assertThat(result.getStrikeCount()).isEqualTo(0);
-        Assertions.assertThat(result.getBallCount()).isEqualTo(3);
-
-        //input3 - nothing
-        result = numberBaseballService.compareInputAndAnswer(input3);
-        Assertions.assertThat(result.getStrikeCount()).isEqualTo(0);
-        Assertions.assertThat(result.getBallCount()).isEqualTo(0);
-
-        //input4 - 1 strike 1 ball
-        result = numberBaseballService.compareInputAndAnswer(input4);
-        Assertions.assertThat(result.getStrikeCount()).isEqualTo(1);
-        Assertions.assertThat(result.getBallCount()).isEqualTo(1);
-    }
-
 }
