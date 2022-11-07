06589c352eb1ef1206106979000bfa6943cbd8ab
민병욱 <woook27@naver.com>
2022-11-08 06:48:43 +0900

    refactor (Game): countBall, countStrike 함수 구현

--- src/main/java/baseball/service/Game.java
+++ src/main/java/baseball/service/Game.java
@@ -28,30 +28,49 @@ public class Game {
         if (restart.equals("1")) {
             start();
         }
+        System.out.println("게임 종료");
     }
 
     private boolean isCorrect() {
+        int strike = countStrike();
+        int ball = countBall();
+        Output.print(ball, strike);
+        if (strike == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    private int countBall() {
+        String number = user.getNumber();
+        String answer = computer.getAnswer();
+
+        HashSet<Character> set = new HashSet<>();
+        for (char c : answer.toCharArray()) {
+            set.add(c);
+        }
+        int ballCount = 0;
+        for (int index = 0; index < 3; index++) {
+            if (number.charAt(index) != answer.charAt(index) && set.contains(number.charAt(index))) {
+                ballCount++;
+            }
+        }
+        return ballCount;
+    }
+
+    private int countStrike() {
         String number = user.getNumber();
         String answer = computer.getAnswer();
         if (number.equals(answer)) {
-            Output.printAnswerMessage();
-            return true;
+            return 3;
         }
-        int strike = 0;
-        int ball = 0;
-        HashSet<Character> set = new HashSet<>();
-        for (int i = 0; i < number.length(); i++) {
-            if (number.charAt(i) == answer.charAt(i)) {
-                strike++;
-            }
-            set.add(answer.charAt(i));
-        }
-        for (int i = 0; i < number.length(); i++) {
-            if (number.charAt(i) != answer.charAt(i) && set.contains(number.charAt(i))) {
-                ball++;
+
+        int strikeCount = 0;
+        for (int index = 0; index < 3; index++) {
+            if (number.charAt(index) == answer.charAt(index)) {
+                strikeCount++;
             }
         }
-        Output.print(ball, strike);
-        return false;
+        return strikeCount;
     }
 }
