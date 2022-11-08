be4ba9c87c25ea11f165ca1320664747cea70395
ddukddaki <rlfrkdms1@naver.com>
2022-11-08 14:55:18 +0900

    feat(Constant): 게임에 필요한 상수 선언

---
+++ src/main/java/baseball/Constant.java
@@ -0,0 +1,22 @@
+package baseball;
+
+public class Constant {
+
+    public static final int LENGTH_OF_NUMBER = 3;
+    public static final int NUM_MIN_RANGE = 1;
+    public static final int NUM_MAX_RANGE = 9;
+
+    public static final String STRIKE = "스트라이크";
+    public static final String BALL = "볼";
+    public static final String NOTHING = "낫싱";
+
+    public static final String ASK_RESTART_MSG = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String ERR_MSG = "잘못된 입력입니다. 게임을 종료합니다.";
+    public static final String GAME_START_MSG = "숫자 야구 게임을 시작합니다.";
+    public static final String INPUT_NUMBER_MSG = "숫자를 입력해주세요 : ";
+    public static final String WIN_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static final String RESTART = "1";
+    public static final String EXIT = "2";
+
+}
