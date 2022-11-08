6514a7f2f3c42c25f5c9e81f05b41b786bff9a29
show2891 <show2891@gmail.com>
2022-11-08 21:57:28 +0900

    사용자 입력 요구_구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,20 @@
 package baseball;
+import java.util.Scanner;
 
 public class Application {
-
+    static String inputThreeNumberByUser() {
+        System.out.print("숫자를 입력해주세요 : ");
+        Scanner input = new Scanner(System.in);
+        String userInput = input.nextLine();
+        return userInput;
+    }
 
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
             while (true) {
-                System.out.print("숫자 야구 게임을 시작합니다.");
-
+                System.out.print("숫자 야구 게임을 시작합니다.\n");
+                String userThreeNumber = inputThreeNumberByUser();
 
 
 
