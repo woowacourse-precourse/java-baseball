132cbee9ba4122d97c8403e7dfb5b69921cfaab2
inwoo <korean5078@gmail.com>
2022-11-08 18:10:22 +0900

    feat(src/main/java/baseball/Application/compareNumber) : 컴퓨터와 유저 숫자 비교 함수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,9 +10,18 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         String menu = "";
+        String input = "";
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         List<Integer> computer = makeRandomNumber();
+        for (int i : computer) {
+            System.out.print(i);
+        }
+        input = getNumber();
+        List<Integer> answer = compareNumber(input, computer);
+        for (int i : answer) {
+            System.out.println(i);
+        }
     }
 
     public static List<Integer> makeRandomNumber() {
@@ -36,5 +45,21 @@ public class Application {
         }
 
         return input;
+    }
+
+    public static List<Integer> compareNumber(String input, List<Integer> computer) {
+        int strikeCnt = 0;
+        int ballCnt = 0;
+
+        for (int i = 0; i < computer.size(); i++) {
+            String str = Integer.toString(computer.get(i));
+
+            if (input.contains(str) && input.indexOf(str) == i) {
+                strikeCnt++;
+            } else if (input.contains(str)) {
+                ballCnt++;
+            }
+        }
+        return List.of(strikeCnt, ballCnt);
     }
 }
