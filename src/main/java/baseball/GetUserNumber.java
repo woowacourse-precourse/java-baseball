a57650b4653527e96f2c509d6ca95157e11f6d3f
yewon <yewon.ux@gmail.com>
2022-11-08 20:58:32 +0900

    feat: 예외 케이스 처리

--- src/main/java/baseball/GetUserNumber.java
+++ src/main/java/baseball/GetUserNumber.java
@@ -7,8 +7,17 @@ public class GetUserNumber {
     public String inputUserNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         String userRandNumber = Console.readLine();
-        System.out.println(userRandNumber);
-        return userRandNumber;
+        if (userRandNumber.charAt(0) == userRandNumber.charAt(1)) {
+            throw new IllegalArgumentException();
+        } else if (userRandNumber.charAt(1) == userRandNumber.charAt(2)) {
+            throw new IllegalArgumentException();
+        } else if (userRandNumber.charAt(0) == userRandNumber.charAt(2)) {
+            throw new IllegalArgumentException();
+        } else if (userRandNumber.length() != 3) {
+            throw new IllegalArgumentException();
+        } else {
+            return userRandNumber;
+        }
     }
 
 }
