4bbc5da08d3f9ccaaa0ab5e501db0d81f911b093
Minji Kim <minji1289@naver.com>
2022-11-08 23:55:24 +0900

    랜덤 넘버 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import java.util.ArrayList;
+
+import static camp.nextstep.edu.missionutils.Randoms;
+
+
 public class Application {
+
+    public static ArrayList<Integer> randomNum() {
+        ArrayList<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        ArrayList A = randomNum();
     }
 }
