9f95e393ad70c3663d404c5c0e634cc27ee4c1a6
geonho <soo_1992@naver.com>
2022-11-08 23:42:01 +0900

    feat: 입력 숫자 유효성 검사

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Game {
     List<Integer> numbersPickedByComputer;
@@ -17,5 +19,45 @@ public class Game {
             }
         }
     }
+
+    public void validateInputNumber(String numberString) throws IllegalArgumentException {
+        if (!validateLengthOfInputString((numberString)) ||
+                !validateTypeOfInputString(numberString) ||
+                !validateUniquenessOfEachCharacter(numberString)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean validateLengthOfInputString(String numberString) {
+        if (numberString.length() == 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean validateTypeOfInputString(String numberString) {
+        for (int i = 0; i < numberString.length(); i++) {
+            char numberCharacter = numberString.charAt(i);
+            if (numberCharacter < '1' || numberCharacter > '9') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean validateUniquenessOfEachCharacter(String numberString) {
+        Set<Character> set = new HashSet<>();
+        for (int i = 0; i < numberString.length(); i++) {
+            char numberCharacter = numberString.charAt(i);
+            if (set.contains(numberCharacter)) {
+                return false;
+            } else {
+                set.add(numberCharacter);
+            }
+        }
+        return true;
+    }
+
 
 }
