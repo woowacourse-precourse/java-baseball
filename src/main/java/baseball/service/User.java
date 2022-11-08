bf964041e3db6598a48986b991c276327e1dc57e
유지웅 <poujis0@gmail.com>
2022-11-08 15:51:14 +0900

    docs(📝) - User 메소드 주석 추가

--- src/main/java/baseball/service/User.java
+++ src/main/java/baseball/service/User.java
@@ -7,9 +7,19 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class User {
+    /**
+     * 유저의 Consle 입력을 받아오는 메소드
+     * @return User의 콘솔 입력
+     */
     public String userInput(){
         return Console.readLine();
     }
+
+    /**
+     * User가 입력한 번호의 각 자리수를 List에 담아 반환하는 메소드
+     * @param userNumber User가 입력한 번호
+     * @return 입력한 번호의 각 자리수를 담은 List
+     */
     public List<Integer> getUserNumber(String userNumber){
         String[] split = userNumber.split("");
 
