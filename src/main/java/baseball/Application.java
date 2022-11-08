285c1ee41089e5149222a8b24dd20da99e9ab282
bjm7701 <bjm7701@naver.com>
2022-11-08 16:42:04 +0900

    refactor : start editing main fuction

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -63,6 +63,9 @@ public class Application {
         return 0;
     }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(true) {
+
+        }
     }
 }
