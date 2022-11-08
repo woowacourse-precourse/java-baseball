b62ace2bc76f2402df081ba9c6f4ed139b1b11b7
ParkJoungWoo <jug78@naver.com>
2022-11-08 16:44:42 +0900

    feat($baseball) : Applicatoin add restartOrEndGame
    Added new featrue to $baseball
    - get user's choice
    - valid user's choice
    - end the game if choice is end(2)
    - restart the game if choice is restart(1)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static final int RESTART = 1;
+    public static final int END = 2;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    }
+
+    public static void restartOrEndGame() {
+        int userChoiceRestartOrEnd = 0;
+        Game game;
+        while (true) {
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            userChoiceRestartOrEnd = Integer.parseInt(Console.readLine());
+            ValidateGameComponent.validUserRestartOrEndGame(userChoiceRestartOrEnd);
+            if (userChoiceRestartOrEnd == END) {
+                break;
+            } else if (userChoiceRestartOrEnd == RESTART) {
+                game = new Game();
+                game.playGame();
+            }
+        }
     }
 }
+
