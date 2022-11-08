abf2467c1e6c2e958933d75016019e5391abfdc7
HASEUNGHEEE <ariul123@gmail.com>
2022-11-08 16:22:55 +0900

    feat: 재시작 여부 확인 문구를 출력하는 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -32,4 +32,12 @@ public class Game {
     private void printGameOverMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public boolean askRetry() {
+        printAskRetryMessage();
+    }
+
+    private void printAskRetryMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
 }
