7629200d4aa08259841aa64a659ca854e55b634c
Sojeong <k5k5s5s5j5j5@naver.com>
2022-11-08 11:08:40 +0900

    test(src/test): 테스트할 상황 목록

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,12 +21,130 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void inGameTest_예외_네_개의_숫자_입력() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void inGameTest_예외_숫자가_아닌_입력(){
+
+    }
+
+    @Test
+    void inGameTest_예외_0이_포함된_입력(){
+
+    }
+
+    @Test
+    void inGameTest_예외_중복된_숫자_입력(){
+
+    }
+
+    @Test
+    void endGameTest_예외_1이나_2가_아닌_숫자(){
+
+    }
+
+    @Test
+    void endGameTest_예외_숫자가_아닌_입력(){
+
+    }
+
+    @Test
+    void inGameTest_볼_갯수_세기(){
+
+    }
+
+    @Test
+    void inGameTest_스트라이크_갯수_세기(){
+
+    }
+
+    @Test
+    void inGameTest_매치되지_않음(){
+
+    }
+
+    @Test
+    void inGameTest_정답_입력(){
+
+    }
+
+    @Test
+    void inGameTest_생성된_숫자가_중복되지_않는_세자리_숫자(){
+
+    }
+
+    @Test
+    void printTest_게임_시작_문구(){
+
+    }
+
+    @Test
+    void printTest_정답_입력시_문구(){
+
+    }
+
+    @Test
+    void printTest_볼_only_문구(){
+
+    }
+
+    @Test
+    void printTest_스트라이크_only_문구(){
+
+    }
+
+    @Test
+    void printTest_볼_and_스트라이크_문구(){
+
+    }
+
+    @Test
+    void printTest_낫싱_문구(){
+
+    }
+
+    @Test
+    void printTest_게임_재시작_문구(){
+
+    }
+
+    @Test
+    void printTest_숫자_입력_요구_문구(){
+
+    }
+
+
+/*
+
+    @Test
+    void inGame_
+
+
+    @Test
+    void
+
+
+    @Test
+    void
+
+
+    @Test
+    void
+
+
+    @Test
+    void
+
+
+
+
+*/
+
 
     @Override
     public void runMain() {
