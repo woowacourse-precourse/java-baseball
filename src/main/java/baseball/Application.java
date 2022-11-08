f3ef8e785d7137b9cc4852612803d5973380cec0
Juuuic <sn990908a@gmail.com>
2022-11-08 19:18:32 +0900

    fix: ball과 strike 오류 해결

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -17,17 +17,42 @@ public class Application {
         startGame();
     }
 
+    //5. 컴퓨터 숫자 설정하고 게임 진행 method
+    public static void startGame(){
+        computerNum = getComputerNum();
+        Game();
+    }
+
+    //5. 2번부터 4번까지 반복할 method
+    public static void Game(){
+        List<Integer> userNum = getUserNum();
+
+        int strike = getStrike(computerNum, userNum);
+        int ball = getBall(computerNum, userNum);
+        printResult(strike, ball);
+
+        if(strike == 3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            ContinueOrEnd();
+        }
+        else{
+            userNum.clear();
+            Game();
+        }
+        //isRightAnswerAndContinue(strike, userNum);
+    }
+
 
     //1. 컴퓨터 3자리 랜덤 숫자 생성
     public static List<Integer> getComputerNum() {
-        List<Integer> computerNum = new ArrayList<>();
-        while (computerNum.size() < len) {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < len) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computerNum.contains(randomNumber)) {
-                computerNum.add(randomNumber);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
             }
         }
-        return computerNum;
+        return computer;
     }
 
     //2번부터 4번까지 반복
@@ -48,14 +73,24 @@ public class Application {
 
     //2-1. 사용자가 3자리 숫자 입력 예외상황
     public static void userException(List<String> scope, String userInput, String[] inputArray, List<Integer> userNum){
+        //문자를 입력했을 때
+        for(int i=0; i<inputArray.length; i++){
+            if(!scope.contains(inputArray[i])){
+                throw new IllegalArgumentException();
+            }
+            else{
+                userNum.add(Integer.valueOf(inputArray[i]));
+            }
+        }
+
         //0 입력
         if(userInput.contains("0")){
-            throw new IllegalArgumentException("다시 입력해주세요.");
+            throw new IllegalArgumentException();
         }
 
         //3자리의 수가 아닐 때
         if (userInput.length()!=len){
-            throw new IllegalArgumentException("다시 입력해주세요.");
+            throw new IllegalArgumentException();
         }
 
         //숫자 중복되었을 때
@@ -63,8 +98,8 @@ public class Application {
         int left = 0;
         int right = 1;
         while(cnt < len){
-            if(inputArray[left].equals(inputArray[right])){
-                throw new IllegalArgumentException("다시 입력해주세요.");
+            if(userNum.get(left).equals(userNum.get(right))){
+                throw new IllegalArgumentException();
             }
             else if (right != len-1){
                 right++;
@@ -75,15 +110,6 @@ public class Application {
             cnt++;
         }
 
-        //문자를 입력했을 때
-        for(int i=0; i<inputArray.length; i++){
-            if(!scope.contains(inputArray[i])){
-                throw new IllegalArgumentException("다시 입력해주세요.");
-            }
-            else{
-                userNum.add(Integer.valueOf(inputArray[i]));
-            }
-        }
     }
 
     //3. 사용자가 입력한 수와 컴퓨터 수 비교
@@ -135,35 +161,20 @@ public class Application {
     }
 
 
-
-    //5. 2번부터 4번까지 반복할 method
-    public static void Game(){
-        List<Integer> userNum = getUserNum();
-
-        int ball = getBall(computerNum, userNum);
-        int strike = getStrike(computerNum, userNum);
-
-        printResult(ball, strike);
-        isRightAnswerAndContinue(computerNum, userNum);
-    }
-    //5. 컴퓨터 숫자 설정하고 게임 진행 method
-    public static void startGame(){
-        computerNum = getComputerNum();
-        Game();
-    }
-
-
     //6. 정답인지 아닌지 판별한 다음 게임 재시작할지 종료할지 결정
-    public static void isRightAnswerAndContinue(List<Integer> computerNum, List<Integer> userNum){
-        if(computerNum.equals(userNum)){
+    /*
+    public static void isRightAnswerAndContinue(List<Integer> user){
+        if(strike == 3){
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             ContinueOrEnd();
         }
         else{
-            userNum.clear();
+            user.clear();
             Game();
         }
     }
+
+     */
 
     public static void ContinueOrEnd(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
