fcbc1b97ff66b667d83d5bc545b4365a54af33f3
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-08 18:26:42 +0900

    feat: 입출력 전담 클래스의 게임 상황에 필요한 문구를 출력하는 기능
    문자열 편집이 필요할 경우 이 클래스에서 편집 용이하 도록 게임에 필요한 문자열을 한 곳에 모아 두었다.

---
+++ src/main/java/baseball/OutputSentense.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class OutputSentense {
+    public String start = "숫자 야구 게임을 시작합니다.";
+    public String inputNumber = "숫자를 입력해 주세요 : ";
+    public String restart = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public String ending = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public String reQuestion = "1,2가 아닙니다 다시 입력해 주세요";
+    public String nothing = "낫싱";
+    public String strike =  "스트라이크";
+    public String ball = "볼";
+
+
+}
