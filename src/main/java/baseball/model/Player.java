54a6d356672efa04e26a6d32fbd87289f1340429
mihye126 <mihea12@gmail.com>
2022-11-08 21:57:19 +0900

    attack input(parsing number)

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,10 +1,14 @@
 package baseball.model;
 
+import static baseball.util.InputUtility.parsingNumber;
+
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Player {
+
     public List<Baseball> defend() {
         LinkedHashSet<Baseball> baseballSet = new LinkedHashSet<>();
         while (baseballSet.size() < 3) {
@@ -13,4 +17,7 @@ public class Player {
         return new ArrayList<>(baseballSet);
     }
 
+    public List<Baseball> attack(String input) {
+        return parsingNumber(input).stream().map(Baseball::new).collect(Collectors.toList());
+    }
 }
