9f9283da13701a73b3546fdccef48e71e3390ea9
chaos0103 <lyt1228@naver.com>
2022-11-08 18:46:07 +0900

    style: 변수명 변경

--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -8,41 +8,41 @@ class UserTest {
 
     @Test
     void 숫자_입력_테스트() {
-        User userNumber = new User();
-        userNumber.inputNumber("123");
+        User user = new User();
+        user.inputNumber("123");
 
-        assertThat(userNumber.getNumbers().size()).isEqualTo(3);
+        assertThat(user.getNumbers().size()).isEqualTo(3);
     }
 
     @Test
     void 숫자_이외의_문자_입력_예외() {
         assertThatThrownBy(() -> {
-            User userNumber = new User();
-            userNumber.inputNumber("1*3");
+            User user = new User();
+            user.inputNumber("1*3");
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 숫자_자리수_예외() {
         assertThatThrownBy(() -> {
-            User userNumber = new User();
-            userNumber.inputNumber("13");
+            User user = new User();
+            user.inputNumber("13");
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 숫자_0_입력_예외() {
         assertThatThrownBy(() -> {
-            User userNumber = new User();
-            userNumber.inputNumber("401");
+            User user = new User();
+            user.inputNumber("401");
         }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 숫자_중복_입력_예외() {
         assertThatThrownBy(() -> {
-            User userNumber = new User();
-            userNumber.inputNumber("121");
+            User user = new User();
+            user.inputNumber("121");
         }).isInstanceOf(IllegalArgumentException.class);
     }
 }
