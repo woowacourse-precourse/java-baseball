ad68134b31e901e8eee6a5b1f3d7fa30c67b4c29
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-08 19:57:52 +0900

    refactor(Application): User 객체를 처음 한 번만 생성하도록 변경
    현재는 한명의 유저가 게임을 계속 진행하는 것이므로, User 객체를 처음 앱이 시작할 때 한번만 생성하도록 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,9 +2,9 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
+        User user = new User();
         boolean restart = true;
         while (restart) {
-            User user = new User();
             Game game = Game.start(user);
             restart = game.run();
         }
