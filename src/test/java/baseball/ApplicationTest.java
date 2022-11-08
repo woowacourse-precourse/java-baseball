dfaf28ba2ba6690d9b4714b4546602d47a03e569
geonho <soo_1992@naver.com>
2022-11-08 23:40:48 +0900

    test: computerSelectThreeNumbers_서로_다른_3개의_정수가_생성되는지_확인

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -9,24 +9,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
-    }
 
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+    void computerSelectThreeNumbers_서로_다른_3개의_정수가_생성되는지_확인() {
+        Game game = new Game();
+        game.computerSelectThreeNumbers();
+        assertThat(game.numbersPickedByComputer).containsAnyOf(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        assertThat(game.numbersPickedByComputer.size()).isEqualTo(3);
+        assertThat(game.numbersPickedByComputer.get(0)).isNotEqualTo(game.numbersPickedByComputer.get(1));
+        assertThat(game.numbersPickedByComputer.get(0)).isNotEqualTo(game.numbersPickedByComputer.get(2));
+        assertThat(game.numbersPickedByComputer.get(1)).isNotEqualTo(game.numbersPickedByComputer.get(2));
     }
+
 
     @Override
     public void runMain() {
