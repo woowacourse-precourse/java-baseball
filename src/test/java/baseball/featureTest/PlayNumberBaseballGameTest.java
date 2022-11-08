98b0432f202a691a7d76acd024cd4df09af98b12
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-11-08 23:30:01 +0900

    test(PlayNumberBaseballGameTest): add arrayList_setRandomNumberOfComputer test

--- src/test/java/baseball/featureTest/PlayNumberBaseballGameTest.java
+++ src/test/java/baseball/featureTest/PlayNumberBaseballGameTest.java
@@ -1,8 +1,30 @@
 package baseball.featureTest;
 
+import baseball.PlayNumberBaseballGame;
 import org.junit.jupiter.api.Test;
 
-public class PlayNumberBaseballGameTest {
-    @Test
-}
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.*;
+
+
+public class PlayNumberBaseballGameTest {
+    PlayNumberBaseballGame playNumberBaseballGame = new PlayNumberBaseballGame();
+
+    @Test
+    void arrayList_setRandomNumberOfComputer() {
+        List<Integer> randNum = playNumberBaseballGame.setRandomNumberOfComputer();
+        assertThat(randNum)
+                .isNotEmpty()
+                .hasSize(3);
+        assertThat(randNum.get(0))
+                .isNotEqualTo(randNum.get(1))
+                .isNotEqualTo(randNum.get(2));
+        assertThat(randNum.get(1))
+                .isNotEqualTo(randNum.get(2));
+    }
+
+
+}
