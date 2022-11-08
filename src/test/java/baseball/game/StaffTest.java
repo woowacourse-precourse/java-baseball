5ab244be22e80d8b5dd1f949e1699493630636f2
eora21 <eora21@naver.com>
2022-11-08 16:58:11 +0900

    test: 💍 변경 Enum 선언으로 인한 테스트코드 수정
    Enum에 맞게 테스트코드가 동작되도록 수정했다

--- src/test/java/baseball/game/StaffTest.java
+++ src/test/java/baseball/game/StaffTest.java
@@ -1,18 +1,16 @@
 package baseball.game;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-import static baseball.constant.Rules.END_NUMBER;
-import static baseball.constant.Rules.FINISH;
-import static baseball.constant.Rules.MORE;
-import static baseball.constant.Rules.PICK_COUNT;
-import static baseball.constant.Rules.START_NUMBER;
+import static baseball.constant.Rule.END;
+import static baseball.constant.Rule.PICK;
+import static baseball.constant.Rule.START;
+import static baseball.constant.Want.FINISH;
+import static baseball.constant.Want.MORE;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,27 +20,27 @@ class StaffTest extends NsTest {
     @Test
     void 목표값_생성() {
         List<Integer> targetNumbers = staff.getTargetNumbers();
-        assertThat(targetNumbers.size()).isEqualTo(PICK_COUNT);
-        assertThat(new HashSet<>(targetNumbers).size()).isEqualTo(PICK_COUNT);
+        assertThat(targetNumbers.size()).isEqualTo(PICK.getValue());
+        assertThat(new HashSet<>(targetNumbers).size()).isEqualTo(PICK.getValue());
     }
 
     @Test
     void 유저_입력값_생성() {
         List<Integer> userNumbers = staff.getUserNumbers("123");
-        assertThat(userNumbers.size()).isEqualTo(PICK_COUNT);
-        assertThat(new HashSet<>(userNumbers).size()).isEqualTo(PICK_COUNT);
+        assertThat(userNumbers.size()).isEqualTo(PICK.getValue());
+        assertThat(new HashSet<>(userNumbers).size()).isEqualTo(PICK.getValue());
     }
 
     @Test
     void 유저_입력값_길이미만() {
         Throwable exception = assertThrows(RuntimeException.class, () -> staff.getUserNumbers("12"));
-        assertEquals("입력값이 " + PICK_COUNT + "자리가 아닙니다.", exception.getMessage());
+        assertEquals("입력값이 " + PICK + "자리가 아닙니다.", exception.getMessage());
     }
 
     @Test
     void 유저_입력값_길이초과() {
         Throwable exception = assertThrows(RuntimeException.class, () -> staff.getUserNumbers("1234"));
-        assertEquals("입력값이 " + PICK_COUNT + "자리가 아닙니다.", exception.getMessage());
+        assertEquals("입력값이 " + PICK + "자리가 아닙니다.", exception.getMessage());
     }
 
     @Test
@@ -55,16 +53,16 @@ class StaffTest extends NsTest {
     void 유저_입력값_0입력() {
         Throwable exception = assertThrows(RuntimeException.class, () -> staff.getUserNumbers("012"));
         assertEquals("허용되지 않은 문자가 포함되어 있습니다. "
-                + START_NUMBER + "부터 " + END_NUMBER + "까지의 값을 "
-                + PICK_COUNT + "번 사용해주세요", exception.getMessage());
+                + START + "부터 " + END + "까지의 값을 "
+                + PICK + "번 사용해주세요", exception.getMessage());
     }
 
     @Test
     void 유저_입력값_문자열() {
         Throwable exception = assertThrows(RuntimeException.class, () -> staff.getUserNumbers("-12"));
         assertEquals("허용되지 않은 문자가 포함되어 있습니다. "
-                + START_NUMBER + "부터 " + END_NUMBER + "까지의 값을 "
-                + PICK_COUNT + "번 사용해주세요", exception.getMessage());
+                + START + "부터 " + END + "까지의 값을 "
+                + PICK + "번 사용해주세요", exception.getMessage());
     }
 
     @Test
