53d7fe115793c00b59bcd16f10748582a29c2c16
Sieun Park <psieun0606@gmail.com>
2022-11-08 16:57:13 +0900

    feat: 스트라이크 개수를 세서 추가하는 기능 추가

---
+++ src/main/java/baseball/controller/CompareNumbers.java
@@ -0,0 +1,22 @@
+package baseball.controller;
+
+import baseball.model.UserNumber;
+
+import java.util.ArrayList;
+
+public class CompareNumbers {
+
+    UserNumber userNumber = new UserNumber();
+
+    public CompareNumbers(ArrayList<Integer> userNumberList, ArrayList<Integer> computerNumberList) {
+        countStrike(userNumberList, computerNumberList);
+    }
+
+    public void countStrike(ArrayList<Integer> userNumberList, ArrayList<Integer> computerNumberList) {
+        for (int i = 0; i < 3; i++) {
+            if (userNumberList.get(i) == computerNumberList.get(i)) {
+                userNumber.addStrike();
+            }
+        }
+    }
+}
