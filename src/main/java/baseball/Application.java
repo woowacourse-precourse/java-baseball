65810cf1f8abc5d863e9777c08670c3dc399ee9a
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-08 15:20:09 +0900

    refactor(checkAnswerAndGiveHint) : functionalization the compare system
    functionalization the part where compare the answser with computer number, count strike, ball, nothing
    change the output value to more detail
    
    BREAKING CHANGE: readability increases and refactoring becomes easier

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,24 +9,12 @@ import java.util.Objects;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO : 힌트 출력 함수화
+        // TODO : 힌트가 잘 출력되는지 테스트
         List<Integer> computer = makeRandomNumberList();
-        List<String> playerAnswer = inputPlayerAnswerList();
+        List<String> player = inputPlayerAnswerList();
+        String hint = checkAnswerAndGiveHint(computer, player);
+        System.out.println(hint);
 
-        int strike = 0;
-        int ball = 0;
-        for(int i = 0; i < 3; i++){
-            if (Objects.equals(computer.get(i), Integer.valueOf(playerAnswer.get(i)))){
-                strike++;
-            }
-            if (!Objects.equals(computer.get(i), Integer.valueOf(playerAnswer.get(i))) && computer.contains(Integer.valueOf(playerAnswer.get(i)))){
-                ball++;
-            }
-        }
-        int nothing = (3 - strike - ball);
-        System.out.println(strike);
-        System.out.println(ball);
-        System.out.println(nothing);
     }
 
 
@@ -41,13 +29,34 @@ public class Application {
         return computer;
     }
 
-    public static List<String> inputPlayerAnswerList(){
+    private static List<String> inputPlayerAnswerList(){
         System.out.println("숫자를 입력해주세요 : ");
         String myAnswer = Console.readLine();
 
         String[] splitAnswer = myAnswer.split("");
         ArrayList<String> answerList = new ArrayList<>(Arrays.asList(splitAnswer));
         return answerList;
+    }
+
+    public static String checkAnswerAndGiveHint(List<Integer> computer, List<String> player){
+        int strike = 0;
+        int ball = 0;
+        for(int i = 0; i < 3; i++){
+            if (Objects.equals(computer.get(i), Integer.valueOf(player.get(i)))){
+                strike++;
+            }
+            if (!Objects.equals(computer.get(i), Integer.valueOf(player.get(i))) && computer.contains(Integer.valueOf(player.get(i)))){
+                ball++;
+            }
+        }
+        int nothing = (3 - strike - ball);
+
+        if (nothing != 3){
+            return (ball + "볼 " + strike + "스트라이크");
+        }
+        else {
+           return ("낫싱");
+        }
     }
 
 }
