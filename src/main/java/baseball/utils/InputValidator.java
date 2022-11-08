457bd12474740bc55b7851b82ee765fa16a6ca7f
조병우 <whquddn55@gmail.com>
2022-11-08 11:02:32 +0900

    feat(InputValidator): implement validating input weather user re-game
    This method validate input of weather user wants re-game the game. This method will throw IllegalArgumntException  when input string is not valid.
    
    + public void validateWeather(String)

--- src/main/java/baseball/utils/InputValidator.java
+++ src/main/java/baseball/utils/InputValidator.java
@@ -10,6 +10,12 @@ public class InputValidator {
         validateInputDistinct(input);
     }
 
+    public void validateWhether(String input) throws IllegalArgumentException {
+        if (input.chars().anyMatch(number -> number != '1' && number != '2')) {
+            throw new IllegalArgumentException("1 또는 2을 입력해야합니다.");
+        }
+    }
+
     private void validateIntegerRange(String input) throws IllegalArgumentException {
         if (input.chars().anyMatch(number -> number < '0' || number > '9')) {
             throw new IllegalArgumentException("입력이 [0, 9]사이의 숫자 이외의 문자를 포함합니다.");
