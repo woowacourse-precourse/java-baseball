64b3b989d6d310103b57efabbffe82ee94942dba
JeongHunHui <deca3248@gmail.com>
2022-11-08 14:38:41 +0900

    feat: 게임이 종료되기 전 까지 계속해서 입력을 받게하는 기능 구현 및 메세지 추가

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -10,8 +10,12 @@ public class BaseBallGame {
     System.out.println(GameMessage.GAME_START_MESSAGE.toString());
     String inputData;
     BaseBallGameNumber gameNumber;
-    inputData = getUserNumberInput();
-    gameNumber = createBaseBallGameNumber(inputData);
+    boolean isGameOver = false;
+    while (!isGameOver) {
+      inputData = getUserNumberInput();
+      gameNumber = createBaseBallGameNumber(inputData);
+      if (gameNumber == null) isGameOver = true;
+    }
   }
 
   private void gameInit() {
@@ -37,7 +41,10 @@ public class BaseBallGame {
 
   private enum GameMessage {
     GAME_START_MESSAGE("숫자 야구 게임을 시작합니다."),
-    START_INPUT_MESSAGE("숫자를 입력해주세요: ");
+    START_INPUT_MESSAGE("숫자를 입력해주세요: "),
+    GAME_FINISH_MESSAGE("3개의 숫자를 모두 맞히셨습니다! 게임종료"),
+    CHECK_RESTART_MESSAGE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+    IS_INVALID_VALUE("잘못된 값을 입력하셨습니다. 다시 입력해주세요.");
     private final String message;
 
     GameMessage(String message) {
