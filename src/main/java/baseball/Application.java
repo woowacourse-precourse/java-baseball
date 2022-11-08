25037a8a33ac9c47952ac4f2cc236800d4477239
lqsml <dvlprsml@gmail.com>
2022-11-08 23:58:54 +0900

    feat: 2.게임진행 3) targetNumber 같은 값 존재 여부 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -23,6 +23,29 @@ public class Application {
 		System.out.print("숫자를 입력해주세요 : ");
 		String guessNum = readLine();
 		
+		if(guessNum.length() != 3) {
+			assertThatIllegalArgumentException();
+			
+		} else {
+			
+			if(!(guessNum.equals(targetNumStr))) 
+				return;
+			System.out.print("숫자를 입력해주세요 : ");
+			guessNum = readLine();
+			
+			List<String> guessNumList = Collections.emptyList();
+			for(int i=0; i<3; i++) {
+				guessNumList.add(guessNum.substring(i));
+			}
+	
+			if (targetNum.contains(guessNumList)){
+				
+			} else {
+				System.out.println("낫싱");
+			}
+			
+		}
+		
 	}
 	
 }
