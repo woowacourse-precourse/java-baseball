ab217f9410ee5403c633768e26dff1e1a6413a22
i960107 <i960107@naver.com>
2022-11-08 19:43:54 +0900

    test(Application): add test cases

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,9 +21,45 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 게임종료() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "426", "326", "623", "123", "2");
+                    assertThat(output()).contains("1볼", "1스트라이크", "1볼 1스트라이크", "2스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 2, 3, 4, 5, 6
+        );
+    }
+
+
+    @Test
+    void 숫자가_아닐때_예외_발생() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12a"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자가_많을때_예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자가_적을때_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자가_중복될때_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("111"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
