ea7f29e9330a21e07b81dc0e06c44c6c92d7fddc
yoonjoonseok <yjsygm@naver.com>
2022-11-08 15:54:36 +0900

    사용자가 숫자를 맞출때까지 반복, 맞출 시 게임 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,14 +10,21 @@ public class Application{
     public static void main(String[] args) {
     	String answer = makeRandomNumber();
     	String input;
+    	int strikeCount = 0;
     	
     	System.out.println("숫자 야구 게임을 시작합니다.");
     	
-    	System.out.print("숫자를 입력해주세요 : ");
-    	input = Console.readLine();
-    	
+    	while(strikeCount!=3) {
+        	System.out.print("숫자를 입력해주세요 : ");
+        	input = Console.readLine();
+        	
+        	strikeCount = comparison(answer,input);
+        	
+        	if(strikeCount==3) 
+        		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    	}
+ 
     	return;
-        // TODO: 프로그램 구현
     }
     
     public static String makeRandomNumber() {
