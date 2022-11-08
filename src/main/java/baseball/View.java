325edf4e1ca6e9b1fc5bf195ed60edd3bf0528ca
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-08 17:42:46 +0900

    Feat : View.print 구현(개행 없는 출력을 위함)

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -7,6 +7,14 @@ public class View {
 
     public void printLine(String s) {
         try {
+            System.out.println(s);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e);
+        }
+    }
+
+    public void print(String s) {
+        try {
             System.out.print(s);
         } catch (IllegalArgumentException e) {
             System.out.println(e);
