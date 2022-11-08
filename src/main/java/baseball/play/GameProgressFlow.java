6cd96cc14693ce71fcf03a99e2404ea2ebd05c25
jaehyeon <jhjh0022@naver.com>
2022-11-08 18:39:36 +0900

    refactor: 특정 메서드 static 제거로 인한 수정

--- src/main/java/baseball/play/GameProgressFlow.java
+++ src/main/java/baseball/play/GameProgressFlow.java
@@ -1,20 +1,20 @@
 package baseball.play;
 
 import baseball.Input.InputAndVerification;
+import baseball.answer.AnswerRandomNumber;
+import baseball.count.CountStrikeOrBall;
 
 import java.util.List;
 
-import static baseball.answer.AnswerRandomNumber.makeAnswerRandomNumber;
-import static baseball.count.CountStrikeOrBall.getBallNumber;
-import static baseball.count.CountStrikeOrBall.getStrikeNumber;
 import static baseball.print.OutputStatement.*;
 
 public class GameProgressFlow {
     InputAndVerification inputAndVerification = new InputAndVerification();
+    AnswerRandomNumber answerRandomNumber = new AnswerRandomNumber();
 
     public void startGame() {
 
-        List<Integer> answerNumberList = makeAnswerRandomNumber();
+        List<Integer> answerNumberList = answerRandomNumber.makeAnswerRandomNumber();
         RepeatTheBaseballGame(answerNumberList);
 
         if (askBaseballGameRestartAndFinish() == 1) {
@@ -24,6 +24,7 @@ public class GameProgressFlow {
     }
 
     private void RepeatTheBaseballGame(List<Integer> answerNumberList) {
+        CountStrikeOrBall countStrikeOrBall = new CountStrikeOrBall();
 
         int strikeCnt = 0;
         int ballCnt = 0;
@@ -31,8 +32,8 @@ public class GameProgressFlow {
         requestNumberInput();
         int receivedNumber = inputAndVerification.receiveAndVerifyNumber();
 
-        strikeCnt += getStrikeNumber(answerNumberList, receivedNumber);
-        ballCnt += getBallNumber(answerNumberList, receivedNumber);
+        strikeCnt += countStrikeOrBall.getStrikeNumber(answerNumberList, receivedNumber);
+        ballCnt += countStrikeOrBall.getBallNumber(answerNumberList, receivedNumber);
 
         printHint(ballCnt, strikeCnt);
         if (strikeCnt != 3) {
