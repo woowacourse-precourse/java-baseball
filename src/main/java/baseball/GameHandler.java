d9c607e79a20ba6ec6f45d6af975c95eba211ff4
seonghbae <fullbloomh3ck@gmail.com>
2022-11-08 21:15:24 +0900

    feat(GameHandler) : 클래스 구성

---
+++ src/main/java/baseball/GameHandler.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GameHandler {
+    public static List<Integer> computerNumber;
+
+    public void init() {
+        computerNumber = new ArrayList<>();
+    }
+}
