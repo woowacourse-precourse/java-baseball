c0fcc2e05d7f22136493e6aaf2b0342cbd31ad83
p <gustjr2332@naver.com>
2022-11-08 13:39:50 +0900

    refactor(CheckInputNumberException) : 사용자에게 입력받는 숫자 예외추가

--- src/main/java/baseball/Number/CheckInputNumberException.java
+++ src/main/java/baseball/Number/CheckInputNumberException.java
@@ -2,26 +2,35 @@ package baseball.Number;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class CheckInputNumberException {
+
 
     public void checkException(String number) {
         try {
             lengthException(number);
             duplicateException(number);
+            notNumberException(number);
         }
         catch (Exception e) {
             throw new IllegalArgumentException();
         }
     }
 
-    private static void lengthException(String number) throws Exception {
+    private void notNumberException(String number) throws Exception{
+        if (!Pattern.matches("^[0-9]$", number)) {
+            throw new Exception("숫자를 입력해주세요");
+        }
+    }
+
+    private void lengthException(String number) throws Exception {
         if (number.length() > 3 || number.length() < 3) {
             throw new Exception("3자리 숫자를 입력해주세요");
         }
     }
 
-    private static void duplicateException(String number) throws Exception{
+    private void duplicateException(String number) throws Exception{
         List<Character> tmpNumbers = new ArrayList<>();
 
         for (int digit = 0; digit < number.length(); digit++) {
