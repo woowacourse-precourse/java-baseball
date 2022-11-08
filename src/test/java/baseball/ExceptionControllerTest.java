fc30a9ea82c607ceae165a9771be455acb34f2c7
권민정 <22alswjd@naver.com>
2022-11-08 15:02:26 +0900

    fix: 예외처리 테스트코드 수정
    - IllegalArgumentException 발생 후 나타나는 메세지는 상황에 따라 달라질 수 있기 때문에, 메세지까지 일치하는지 확인하지 않고 IllegalArgumentException가 발생하는지만 확인함.

--- src/test/java/baseball/ExceptionControllerTest.java
+++ src/test/java/baseball/ExceptionControllerTest.java
@@ -12,16 +12,14 @@ class ExceptionControllerTest {
         void 네자리_수가_주어진_경우() {
             String input = "1234";
             assertThatThrownBy(() -> ExceptionController.handleInvalidThreeRandomNumber(input))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("세 자리 수를 입력해야 합니다.");
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
         void 두자리_수가_주어진_경우() {
             String input = "12";
             assertThatThrownBy(() -> ExceptionController.handleInvalidThreeRandomNumber(input))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("세 자리 수를 입력해야 합니다.");
+                    .isInstanceOf(IllegalArgumentException.class);
         }
     }
 
@@ -31,24 +29,21 @@ class ExceptionControllerTest {
         void 공백이_포함된_경우() {
             String input = "1 2";
             assertThatThrownBy(() -> ExceptionController.handleInvalidThreeRandomNumber(input))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("숫자만 입력해야 합니다.");
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
         void 영문자가_포함된_경우() {
             String input = "12a";
             assertThatThrownBy(() -> ExceptionController.handleInvalidThreeRandomNumber(input))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("숫자만 입력해야 합니다.");
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
         void 특수문자가_포함된_경우() {
             String input = "12*";
             assertThatThrownBy(() -> ExceptionController.handleInvalidThreeRandomNumber(input))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("숫자만 입력해야 합니다.");
+                    .isInstanceOf(IllegalArgumentException.class);
         }
     }
 
@@ -69,16 +64,14 @@ class ExceptionControllerTest {
         void 유효하지_않은_숫자가_입력된_경우_1() {
             String input = "3";
             assertThatThrownBy(() -> ExceptionController.handleInvalidFinishCode(input))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("1 또는 2를 입력해야 합니다.");
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
         @Test
         void 유효하지_않은_숫자가_입력된_경우_2() {
             String input = "*1";
             assertThatThrownBy(() -> ExceptionController.handleInvalidFinishCode(input))
-                    .isInstanceOf(IllegalArgumentException.class)
-                    .hasMessageContaining("1 또는 2를 입력해야 합니다.");
+                    .isInstanceOf(IllegalArgumentException.class);
         }
     }
 }
