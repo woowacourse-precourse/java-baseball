d21d060e42be0d0c7c6ed92b7ba284bf8eff65b0
Noah <shgustmd6865@naver.com>
2022-11-08 20:36:26 +0900

    Refacor(ComputerTest) : 컴퓨터 테스트 과정에서 중복 제거
    - 모든 테스트마다 중복해서 인스턴스를 생성하는데 해당 코드를 @BeforeAll을 통해서 중복을 제거합니다

--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,25 +9,31 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("상대방(컴퓨터)의 랜덤한 숫자 생성하기")
 class ComputerTest {
 
+    private Computer computer;
+
+    @BeforeEach
+    @DisplayName("테스트에 사용할 Computer 인스턴스 생성")
+    public void makeComputerInstance() {
+        this.computer = new Computer();
+        assertThat(computer).isInstanceOf(Computer.class);
+    }
+
     @Test
     @DisplayName("숫자의 크기는 3으로 고정")
     public void validNumberSize() {
-        Computer computer = new Computer();
-        assertThat(computer.getRandomNumbers().size()).isEqualTo(3);
+        assertThat(this.computer.getRandomNumbers().size()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("각 자리수는 1~9까지의 숫자만 사용")
     public void randomNumberHasOnlyOneToNine() {
-        Computer computer = new Computer();
-        assertThat(computer.getRandomNumbers()).isNotIn(0).isNotIn(10);
+        assertThat(this.computer.getRandomNumbers()).isNotIn(0).isNotIn(10);
     }
 
     @Test
     @DisplayName("각 자리수는 각기 다른 숫자가 사용되어야 함")
     public void randomNumberElementsAllUnique() {
-        Computer computer = new Computer();
-        assertThat(computer.getRandomNumbers()).doesNotHaveDuplicates();
+        assertThat(this.computer.getRandomNumbers()).doesNotHaveDuplicates();
     }
 
 
