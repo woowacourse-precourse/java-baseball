70a825d8317c7ed8d9496e7fef6752a52f77cb12
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-08 19:12:45 +0900

    feat: 10. 재시작 여부를 1 또는 2로 입력받는 기능 추가(getRestartNumber)

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -106,4 +106,12 @@ public class NumberBaseballGame {
     public void printRestartMessage() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+
+    public int getRestartNumber() {
+        String inputNumber = Console.readLine();
+        if (isRestartNumberException(inputNumber)) {
+            throw new IllegalArgumentException();
+        }
+        return Integer.parseInt(inputNumber);
+    }
 }
