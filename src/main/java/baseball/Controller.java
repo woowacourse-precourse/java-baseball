c3fdebc25c51390956c3a476e5dd05982cbd5488
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-08 16:42:57 +0900

    Computer, User 숫자설정 method 구현

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,12 +1,19 @@
 package baseball;
 
 public class Controller {
-    Model computer;
-    Model user;
+    private Model computer;
+    private Model user;
 
     public Controller(Model computer, Model user) {
         this.computer = computer;
         this.user = user;
     }
-    
+
+    public void setComputerNumber(int number){
+        computer.setNumber(number);
+    }
+
+    public void setUserNumber(int number){
+        user.setNumber(number);
+    }
 }
