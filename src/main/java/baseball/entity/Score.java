4a14a2be0687a45970c90440f81e87258ef5d340
이지영 <jioung93@naver.com>
2022-11-08 23:05:06 +0900

    [Score] feat: 볼 및 스트라이크 개수 증가 기능 구현

--- src/main/java/baseball/entity/Score.java
+++ src/main/java/baseball/entity/Score.java
@@ -1,8 +1,16 @@
 package baseball.entity;
 
 public class Score {
-    // int ball
-    // int strike
+
+    private int ball = 0;
+    private int strike = 0;
+
+    public void increaseBall(int ballCnt) {
+        ball += ballCnt;
+    }
+    public void increaseStrike(int strikeCnt) {
+        strike += strikeCnt;
+    }
 
     // increaseBall(int n)
     // increaseStrike(int n)
