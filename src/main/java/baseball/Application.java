066bc4114a044109329b28f9dda6f2046cfd2992
SONGmINjIJI <songminki18@gmail.com>
2022-11-08 22:23:14 +0900

    feat: 컴퓨터의 숫자 중복 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,13 @@ public class Application {
         com_num.add(Randoms.pickNumberInRange(1, 9));
         while (com_num.size() < 3) {
             int random_num = Randoms.pickNumberInRange(1, 9);
+            com_num = duplicate_inpection(com_num, random_num);
+        }
+        return com_num;
+    }
+    public ArrayList<Integer> duplicate_inpection(ArrayList<Integer> com_num, int random_num){
+        if (!com_num.contains(random_num)) {
+            com_num.add(random_num);
         }
         return com_num;
     }
