971172936a9e2519417f9322e83d6916b3903b97
Moon-Tree <ellaya1012@gmail.com>
2022-11-09 00:11:20 +0900

    Feat : 숫자 야구 게임 기능 목록 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,73 +1,10 @@
 package baseball;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class Application {
 
-    // 기능 목록 1 : 숫자 야구 게임 시작 문구 생성
-    static List<Integer> startMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        return makeThreeNumber();
-    }
-
-    // 기능 목록 2 : 1부터 9까지의 랜덤한 값 생성
-    static int randomValue() {
-        Random random = new Random();
-        return random.nextInt(9) + 1;
-    }
-
-    // 기능 목록 3 : 1부터 9까지 서로 다른 3자리 수 생성
-    static List<Integer> makeThreeNumber() {
-        List<Integer> computerNumber = new ArrayList<>();
-
-        while (computerNumber.size() < 3) {
-            int randomNumbers = randomValue();
-            if (computerNumber.contains(randomNumbers)) {
-                continue;
-            } else {
-                computerNumber.add(randomNumbers);
-            }
-        }
-        return computerNumber;
-    }
-
-    // 기능 목록 4 : 같은 수가 같은 자리에 있을 경우, 스트라이크 확인
-    static int countStrike (List < Integer > computer, List < Integer > player){
-        int strike = 0;
-
-        for (int i = 0; i < player.size(); i++) {
-            if (computer.get(i) == player.get(i)) {
-                strike += 1;
-            }
-        }
-        return strike;
-    }
-
-    // 기능 목록 5 : 같은 수가 다른 자리에 있을 경우, 볼 확인
-    static int countBall(List<Integer> computer, List<Integer> player) {
-        int Ball = 0;
-
-        for (int i = 0; i < player.size(); i++) {
-            if (computer.get(i) == player.get(i)) {
-                continue;
-            } else if (computer.contains(player.get(i))) {
-                Ball += 1;
-            }
-        }
-        return Ball;
-    }
-
-
-
-
-
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-        startMessage();
-        List<Integer> computerNumber = makeThreeNumber();
-
 
     }
 }
