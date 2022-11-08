d4729b47ec7a876ffc5c2f039fac3f62e8a1719a
Yebin <lyb2325@gmail.com>
2022-11-08 21:35:52 +0900

    feat: 볼과 스트라이크 개수 구하기 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -12,17 +12,18 @@ public class BaseballGame {
     private static final String GAME_ENDS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String GET_USER_NEXT_COMMAND_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    private static final Integer NUMBERS_OF_ARRAY = 3;
     private static final String INTEGER_NUMBER_REGEX = "^[1-9]*$";
 
     private static final RandomNumberGenerator computer = new RandomNumberGenerator();
 
-    BaseballGame(){
+    BaseballGame() {
         System.out.print(GAME_STARTS_MESSAGE); // 게임 시작 문구
         computer.generate();
         startGame();
     }
 
-    public static void startGame(){
+    public static void startGame() {
         UserScore user = new UserScore();
 
         // 숫자 입력 받고 숫자 맞히기
@@ -30,22 +31,45 @@ public class BaseballGame {
         // 다음 command
 
     }
-    private static void proceedGame(UserScore user){
+
+    private static void proceedGame(UserScore user) {
         String userInput;
 
         // 숫자 입력 받으며 숫자 맞히기 게임 진행
-        while(true){
+        while (true) {
             user.init();
             System.out.print(GET_USER_INPUT_MESSAGE);
             userInput = Console.readLine();
-            
+
             // 사용자 입력에 따른 에러 처리
             ArrayList<Integer> userInputArray = handleErrorForUserInput(userInput);
 
             // 볼, 스트라이크 개수 맞추기
-            
+            countBallsAndStrikes(user, userInputArray);
+
             // 답에 대한 결과 보여주기
         }
+    }
+
+    private static void countStrikes(UserScore user, ArrayList<Integer> inputArray) {
+        for(int i=0;i<NUMBERS_OF_ARRAY;i++){
+            if(computer.getRandomNumberAt(i) == inputArray.get(i)){
+                user.addStrike();
+            }
+        }
+    }
+
+    private static void countBalls(UserScore user, ArrayList<Integer> inputArray) {
+        for (int number : inputArray) {
+            if (computer.randomNumberList.contains(number)) {
+                user.addBall();
+            }
+        }
+    }
+
+    private static void countBallsAndStrikes(UserScore user, ArrayList<Integer> inputArray) {
+        countBalls(user, inputArray);
+        countStrikes(user, inputArray);
     }
 
     private static boolean isIntegerInRange(String value){
