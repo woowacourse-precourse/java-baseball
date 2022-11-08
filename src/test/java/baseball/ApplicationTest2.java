cb711a706016d643763347bc319946e412acf992
Lemon Soju <yckk980320@naver.com>
2022-11-08 17:32:29 +0900

    test(Application2): inputException() test

--- src/test/java/baseball/ApplicationTest2.java
+++ src/test/java/baseball/ApplicationTest2.java
@@ -11,6 +11,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ApplicationTest2 extends NsTest {
 
+    @Test
+    void 예외_테스트_3개의_숫자가_아닌_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("1"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_서로_다른_3개의_숫자가_아닌_경우() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("111"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
