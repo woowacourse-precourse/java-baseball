886fc10d0fc00baf938f50871266074284030d36
seonghbae <fullbloomh3ck@gmail.com>
2022-11-08 21:17:27 +0900

    feat(init) : GameHandler 클래스 변수 초기화

--- src/main/java/baseball/GameHandler.java
+++ src/main/java/baseball/GameHandler.java
@@ -5,8 +5,10 @@ import java.util.List;
 
 public class GameHandler {
     public static List<Integer> computerNumber;
+    public static List<Integer> compareResult;
 
     public void init() {
         computerNumber = new ArrayList<>();
+        compareResult = new ArrayList<>();
     }
 }
