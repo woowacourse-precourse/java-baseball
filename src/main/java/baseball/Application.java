f882c683562e178ac29cfea52da180aece6d9fba
함궈 <heoja0123@naver.com>
2022-11-08 21:02:09 +0900

    refactor : Application에서 역할별로 메서드 분리
    게임 시작 이후 정답을 맞힐 때 까지의 로직을 담당하는 메서드,
    문자열 출력을 담당하는 메서드로 나누었음.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,6 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.List;
 import java.util.Map;
 
 public class Application {
@@ -12,57 +9,71 @@ public class Application {
     public static final String STRIKE = "스트라이크";
     public static final String NOTHING = "낫싱";
 
-    public static Integer answer;
-    public static String result;
+    public static final int RESTART = 1;
+    public static final int FINISH = 2;
+
+    public static final int LENGTH = 3;
 
     public static void main(String[] args) {
 
         Computer computer = new Computer();
-        answer = 1;
 
-        while(answer != 2){
+        while(true){
             computer.selectUniqueNumbers();
             Player player = new Player();
 
-            StartGame(computer, player);
+            int isContinue = StartGame(computer, player);
+            if(isContinue == RESTART){
+                continue;
+            }
+
+            if(isContinue == FINISH){
+                break;
+            }
         }
     }
 
-    public static void StartGame(Computer computer, Player player){
-        result = "";
-        while(true) {
+    public static Integer StartGame(Computer computer, Player player) {
+        while (true) {
             String s = Console.readLine();
             player.input(s);
 
             Map<String, Integer> scoreMap = Controller.calcScore(computer, player);
 
-            if (scoreMap.containsKey(NOTHING)) {
-                System.out.println(NOTHING);
-            } else {
+            printScore(scoreMap);
 
-                if (scoreMap.containsKey(BALL)) {
-                    result += scoreMap.get(BALL) + "볼";
-                }
+            if (scoreMap.containsKey(STRIKE) && scoreMap.get(STRIKE) == LENGTH) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-                if (scoreMap.containsKey(STRIKE)) {
-                    if (result.length() == 0) {
-                        result += scoreMap.get(STRIKE) + "스트라이크";
-                    } else {
-                        result += " " + scoreMap.get(STRIKE) + "스트라이크";
-                    }
+                int answer = Integer.parseInt(Console.readLine());
+                // 이후 검증 추가할 것
 
-                    if (scoreMap.get(STRIKE) == 3) {
-                        System.out.println(result);
-                        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-                        answer = Integer.valueOf(Console.readLine());
-                        break;
-                    }
-                }
-
-                System.out.println(result);
+                return answer;
             }
         }
+    }
+
+    public static void printScore(Map<String, Integer> scoreMap){
+        String result = "";
+
+        if (scoreMap.containsKey(NOTHING)) {
+            System.out.println(NOTHING);
+            return;
+        }
+
+        if (scoreMap.containsKey(BALL)) {
+            result += scoreMap.get(BALL) + "볼";
+        }
+
+        if (scoreMap.containsKey(STRIKE)) {
+            if (result.length() == 0) {
+                result += scoreMap.get(STRIKE) + "스트라이크";
+            } else {
+                result += " " + scoreMap.get(STRIKE) + "스트라이크";
+            }
+        }
+
+        System.out.println(result);
     }
 }
