304a1d61fbb0b52f903166da8813923a6fb982c7
jin8073 <jin8073@gmail.com>
2022-11-08 15:20:44 +0900

    test(PlayTest): 테스트 케이스 추가

--- src/test/java/baseball/PlayTest.java
+++ src/test/java/baseball/PlayTest.java
@@ -9,12 +9,19 @@ public class PlayTest {
     void 중복_없는_세자리_난수_생성() {
         assertThat(Exception.isRedundancy(Play.createThreeNumber())).isFalse();
     }
+
     @Test
     void 볼_개수_확인() {
         assertThat(Play.countBall("123", "345")).isEqualTo(1);
     }
+
     @Test
     void 스트라이크_개수_확인() {
         assertThat(Play.countStrike("123", "132")).isEqualTo(1);
+    }
+
+    @Test
+    void 스트라이크_개수_확인_3스트라이크() {
+        assertThat(Play.countStrike("123", "123")).isEqualTo(3);
     }
 }
