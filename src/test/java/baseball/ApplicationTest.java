406c23ac58dadf5d89835da2c5b7803c9f6baf7b
Lee Min Woo <lmw7414@naver.com>
2022-11-08 21:02:02 +0900

    test : 테스트 케이스 추가
    사용자 입력 도중 발생할 수 있는 예외에 대해 테스트 해보았다.

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -20,14 +21,75 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @DisplayName("정상 종료 후 재시작 안할 경우")
     @Test
-    void 게임종료_후_재시작_안함() {
+    void case1() {
         assertRandomNumberInRangeTest(
                 () -> {
                     run("246", "215", "134", "135", "2");
-                    assertThat(output()).contains("낫싱", "1볼 1스트라이크" ,"2스트라이크", "3스트라이크", "게임 종료");
+                    assertThat(output()).contains("낫싱", "1볼 1스트라이크", "2스트라이크", "3스트라이크", "게임 종료");
                 },
                 1, 3, 5, 5, 8, 9
+        );
+    }
+
+    @DisplayName("게임 종료 후 재시작 입력 과정에서 올바르지 않은 값 입력할 경우")
+    @Test
+    void case2() {
+        assertRandomNumberInRangeTest(() ->
+                        assertThatThrownBy(() ->
+                                run("369", "3"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                                .hasMessageContaining("Only 1 and 2 can be entered."),
+                3, 6, 9
+        );
+    }
+
+    @DisplayName("게임 도중 사용자의 잘못된 입력으로 예외 발생(0 포함한 경우)")
+    @Test
+    void case3() {
+        assertRandomNumberInRangeTest(() ->
+                        assertThatThrownBy(() ->
+                                run("123", "456", "420"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                                .hasMessageContaining("number can not contain zero."),
+                3, 6, 9
+        );
+    }
+
+    @DisplayName("게임 도중 사용자의 잘못된 입력으로 예외 발생(중복된 숫자를 입력한 경우)")
+    @Test
+    void case4() {
+        assertRandomNumberInRangeTest(() ->
+                        assertThatThrownBy(() ->
+                                run("123", "456", "422"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                                .hasMessageContaining("number can not have duplicated digit."),
+                3, 6, 9
+        );
+    }
+
+    @DisplayName("게임 도중 사용자의 잘못된 입력으로 예외 발생(숫자가 아닌 값 입력)")
+    @Test
+    void case5() {
+        assertRandomNumberInRangeTest(() ->
+                        assertThatThrownBy(() ->
+                                run("123", "l45"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                                .hasMessageContaining("Number can not have non-numeric value."),
+                3, 6, 9
+        );
+    }
+
+    @DisplayName("게임 도중 사용자의 잘못된 입력으로 예외 발생(3자리 숫자가 아닌 경우)")
+    @Test
+    void case6() {
+        assertRandomNumberInRangeTest(() ->
+                        assertThatThrownBy(() ->
+                                run("123", "45", "425", "369", "2"))
+                                .isInstanceOf(IllegalArgumentException.class)
+                                .hasMessageContaining("digit is invalid."),
+                3, 6, 9
         );
     }
 
@@ -38,6 +100,7 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
     @Test
     void 예외_테스트2() {
         assertSimpleTest(() ->
