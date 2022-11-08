461a0569faa653c77ebd67bee7f4cd274e8459d0
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-08 14:53:04 +0900

    chore(User): Refactor Listgenerator -> myList

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -1,6 +1,6 @@
 package baseball.domain;
 
-import baseball.util.ListGenerator;
+import baseball.util.myList;
 import baseball.validator.InputValidator;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -17,7 +17,7 @@ public class User {
     public List<Integer> inputUserAnswer() throws IllegalArgumentException {
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine().trim();
-        List<Integer> userInputs = ListGenerator.stringToIntegerList(input);
+        List<Integer> userInputs = myList.stringToIntegerList(input);
         if (! InputValidator.validateInPlaying(userInputs))
             throw new IllegalArgumentException("서로 다른 3자리 수를 입력해주세요.");
         return userInputs;
