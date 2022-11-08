3d7e209d34f8c307a10c1f3ffd31cb6d43d0182d
Young37 <20171470@g.dongseo.ac.kr>
2022-11-08 15:58:25 +0900

    feat(print): add custom print method for faster speed

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    }
+    
+    private static void print(String text) {
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        try {
+            bufferedWriter.write(text);
+            bufferedWriter.flush();
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+        
     }
 }
