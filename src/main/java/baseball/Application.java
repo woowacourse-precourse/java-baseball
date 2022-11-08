55852fd00caded7c49eb0af0f0cde857d7a27cb5
jisoolee11 <jisu6414@likelion.org>
2022-11-08 23:47:16 +0900

    refactor: 정답 출력 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -27,7 +27,6 @@ public class Application {
         String userNumber;
 
         System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println(correctNumber);
 
         while (userSelectContinue == 1) {
             System.out.print("숫자를 입력해주세요: ");
