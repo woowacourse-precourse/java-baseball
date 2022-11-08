327adf89ea844b284ca8da6aafe27726164adea1
KongDuHwan <kong_du@naver.com>
2022-11-08 18:15:04 +0900

    2. 게임 시작 메세지를 출력할 gameStart 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -26,14 +26,16 @@ public class Application {
         }
     }
 
+    static void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
     static void playGame() {
     }
 
     static boolean playNewGame() {
     }
 
-    static void gameStart() {
-    }
 
     static void setComputer() {
     }
