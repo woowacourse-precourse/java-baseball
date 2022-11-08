1db4292b9d29ea956c7f44f48de768fa8b90e2ac
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-08 18:22:45 +0900

    feat(feature): 스트라이크 여부 판단 기능 구현
    ResultEvaluator의 isStrikeOrNot() 메서드를 구현
    - 사용자 번호의 특정 색인(index)에 위치한 숫자가, 컴퓨터 번호의 같은 색인에 위치할 경우, 스트라이크라는 의미로 1을 반환함.
    - 그렇지 않을 경우, 0을 반환함.

--- src/main/java/baseball/utility/ResultEvaluator.java
+++ src/main/java/baseball/utility/ResultEvaluator.java
@@ -5,4 +5,11 @@ public class ResultEvaluator {
     private ResultEvaluator() {
 
     }
+
+    private static int isStrikeOrNot(String computerNumber, String playerNumber, int index) {
+        if (computerNumber.charAt(index) == playerNumber.charAt(index)) {
+            return 1;
+        }
+        return 0;
+    }
 }
