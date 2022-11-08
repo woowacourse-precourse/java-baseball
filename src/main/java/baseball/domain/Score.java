6ea217b22068978395c6d029b9b4d5e0ab70d369
yuntaeLee <leeyt1201@gmail.com>
2022-11-08 13:44:44 +0900

    feat: Strike, Ball 개수 증가 메서드 구현

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -8,4 +8,20 @@ public class Score {
         this.strike = strike;
         this.ball = ball;
     }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public void increaseStrike() {
+        this.strike += 1;
+    }
+
+    public void increaseBall() {
+        this.ball += 1;
+    }
 }
