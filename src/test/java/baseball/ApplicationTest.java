5beca52a3c6b6fb7555697a655020fdccd3b9123
Jeeseob <jeeseob5761@gmail.com>
2022-11-08 15:51:09 +0900

    test: application test 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -28,6 +28,53 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void 게임종료_후_종료() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 예외_테스트_길이가_짧을_떄() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ArgumentExceptionMessage.INPUT_UNCONFORMABLE_LENGTH.getMessage())
+        );
+    }
+
+    @Test
+    void 예외_테스트_문자열() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("abc"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ArgumentExceptionMessage.INPUT_NOT_INTEGER.getMessage())
+        );
+    }
+
+    @Test
+    void 예외_테스트_일부_문자열() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12c"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ArgumentExceptionMessage.INPUT_NOT_INTEGER.getMessage())
+        );
+    }
+
+    @Test
+    void 예외_테스트_중복() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("122"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(ArgumentExceptionMessage.INPUT_OVERLAP.getMessage())
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
