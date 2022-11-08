2b37fd0180e96942d95e86fc7014211929892235
JungYoonshin <sju08227@naver.com>
2022-11-08 19:44:47 +0900

    feat: 사용자로부터 숫자를 입력받고 저장하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -7,10 +8,17 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
+        List<Integer> randomNumberByComputer;
+        List<Integer> randomNumberByUser;
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
+
         while(true){
+            randomNumberByComputer = generateRandomNumber();
+            System.out.print("숫자를 입력해주세요 : ");
+            randomNumberByUser = inputNumberByUser();
+
 
         }
 
@@ -27,5 +35,15 @@ public class Application {
         }
 
         return computer;
+    }
+
+    private static List<Integer> inputNumberByUser(){
+        List<Integer> inputNumber = new ArrayList<>();
+        String num = Console.readLine();
+
+        for(int i = 0; i <num.length(); i++){
+            inputNumber.add((int) num.charAt(i));
+        }
+        return inputNumber;
     }
 }
