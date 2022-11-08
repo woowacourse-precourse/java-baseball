6bb2e4fb8a1a3b128b0c932037229617e484f0d9
Lee-SungMin <saevers26@gmail.com>
2022-11-08 10:51:20 +0900

    refactor: 변수 이름 및 들여쓰기 수정 - 컴퓨터 수 저장하는 변수명 수정 - 들여쓰기 수정

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -6,22 +6,22 @@ import java.util.ArrayList;
 
 public class Computer {
     protected int answer;
-    ArrayList<Integer> computer = new ArrayList<>();
+    ArrayList<Integer> numberList = new ArrayList<>();
 
     public int randomNumber() {
         return Randoms.pickNumberInRange(1, 9);
     }
 
     public boolean checkNumber(Integer number) {
-        return computer.contains(number);
+        return numberList.contains(number);
     }
 
     public void addNumber(Integer number) {
-        computer.add(number);
+        numberList.add(number);
     }
 
     public void madeNumber() {
-        while (computer.size() < 3) {
+        while (numberList.size() < 3) {
             int randomNumber = randomNumber();
             if (!checkNumber(randomNumber)) {
                 addNumber(randomNumber);
@@ -30,7 +30,7 @@ public class Computer {
     }
 
     public void setNumber() {
-        this.answer = computer.get(0) * 100 + computer.get(1) * 10 + computer.get(2);
+        this.answer = numberList.get(0) * 100 + numberList.get(1) * 10 + numberList.get(2);
     }
 
     public void madeAnswer() {
