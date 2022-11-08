e19277b56951a0fb632e58d606664c75ee49b69c
kimJaeHyeon <kmr2644@gmail.com>
2022-11-08 21:23:37 +0900

    fix: input 시 출력되는 문자열 변경

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console;
 public class View {
 
     public String inputBaseballNumber() {
-        System.out.print("숫자를 입력해주세요 :");
+        System.out.print("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
 
