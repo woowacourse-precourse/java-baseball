ae704414f9eed6cdc33d1949d652781710451127
AhnJoonSung <tjdtna01@naver.com>
2022-11-08 17:11:48 +0900

    Add isValidRange function in Exception class

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -1,7 +1,22 @@
 package baseball;
 
-public class InputException {
-    public static boolean isValid(String input) {
-        return false;
+public class Exception {
+    public static boolean isValidLength(String input) {
+        if (input.length() != Computer.NUMBER_LENGTH) {
+            return false;
+        }
+        return true;
+    }
+
+    public static boolean isValidRange(String input) {
+        char startRange = Character.forDigit(Computer.START_RANGE, 10);
+        char endRange = Character.forDigit(Computer.END_RANGE, 10);
+        for (int i = 0; i < input.length(); i++) {
+            char cur = input.charAt(i);
+            if (cur < startRange || cur > endRange) {
+                return false;
+            }
+        }
+        return true;
     }
 }
