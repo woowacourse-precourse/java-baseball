740354576c8d48e54f0cd73709c85caeba9acab8
limzzum <chfhddl1234@naver.com>
2022-11-08 16:23:51 +0900

    refactor : 함수 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -40,15 +40,16 @@ public class Application {
             List<String> inputNum = List.of(input.split(""));
             inputValidate(inputNum);
             getResult(inputNum);
+            printResult();
 
-            if(ball==0 && strike==0){
-                System.out.println("낫싱");
-            }else{
-                System.out.println(ball+"볼 "+strike+"스트라이크");
-            }
-            if(strike==3){
-                break;
-            }
+        }
+    }
+
+    private static void printResult() {
+        if(ball==0 && strike==0){
+            System.out.println("낫싱");
+        }else{
+            System.out.println(ball+"볼 "+strike+"스트라이크");
         }
     }
 
@@ -57,7 +58,6 @@ public class Application {
         ball = 0;
         for(int i=0; i<inputNum.size();i++){
             int toIntNum= Integer.parseInt(inputNum.get(i));
-
             if(targetNum.contains(toIntNum)){
                 ball+=1;
             }
@@ -81,8 +81,7 @@ public class Application {
     }
 
     private static boolean endGame() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        endMessage();
         int option = Integer.parseInt(Console.readLine());
         optionValidate(option);
 
@@ -91,6 +90,11 @@ public class Application {
             return false;
         }
         return true;
+    }
+
+    private static void endMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
     private static void init() {
