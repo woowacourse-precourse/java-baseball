9ed7ffa719d6934b47d6661acb2b13acbc1cc4fd
kimJaeHyeon <kmr2644@gmail.com>
2022-11-08 10:34:57 +0900

    fix: inputExitOrRestart() 출력 문자열 줄바꿈 삭제

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -11,7 +11,7 @@ public class View {
 
     public String inputExitOrRestart() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         return Console.readLine();
     }
 
