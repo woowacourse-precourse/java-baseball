fa4be43884da6d1d909a0c58c02afe70974f0db3
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-08 11:14:23 +0900

    test(서로_다른_수_3자리를_1부터_9까지_랜덤으로_생성) : create the test
    create two kinds of test
    1. verify that the list has 3 numbers
    2. verify that the list contains different numbers

--- src/test/java/Implement/FunctionTest.java
+++ src/test/java/Implement/FunctionTest.java
@@ -1,51 +1,28 @@
-package function;
+package Implement;
+
+import baseball.Application;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-public class FunctionTest {
+
+public class FunctionTest{
 
     @Test
-    void split_메서드로_주어진_값을_구분() {
-        String input = "1,2";
-        String[] result = input.split(",");
-
-        assertThat(result).contains("2", "1");
-        assertThat(result).containsExactly("1", "2");
+    void 리스트에_3자리_생성() {
+        List<Integer> randomNumber = Application.makeRandomNumberList();
+        assertThat(randomNumber.size() == 3);
     }
 
     @Test
-    void split_메서드_사용시_구분자가_포함되지_않은_경우_값을_그대로_반환() {
-        String input = "1";
-        String[] result = input.split(",");
-
-        assertThat(result).contains("1");
+    void 서로_다른_수_3자리를_1부터_9까지_랜덤으로_생성() {
+        List<Integer> randomNumber = Application.makeRandomNumberList();
+        assertNotEquals(randomNumber.get(0), randomNumber.get(1));
+        assertNotEquals(randomNumber.get(0), randomNumber.get(2));
+        assertNotEquals(randomNumber.get(1), randomNumber.get(2));
     }
-
-    @Test
-    void substring_메서드로_특정_구간_값을_반환() {
-        String input = "(1,2)";
-        String result = input.substring(1, 4);
-
-        assertThat(result).isEqualTo("1,2");
-    }
-
-    @Test
-    void charAt_메서드로_특정_위치의_문자_찾기() {
-        String input = "abc";
-        char charAtElement = input.charAt(0);
-        assertThat(charAtElement).isEqualTo('a');
-    }
-
-    @Test
-    void charAt_메서드_사용시_문자열의_길이보다_큰_숫자_위치의_문자를_찾을_때_예외_발생() {
-        String input = "abc";
-
-        assertThatThrownBy(() -> input.charAt(5))
-                .isInstanceOf(StringIndexOutOfBoundsException.class)
-                .hasMessageContaining("String index out of range: 5");
-    }
-
 }
