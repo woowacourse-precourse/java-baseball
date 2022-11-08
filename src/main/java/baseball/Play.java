5d4b432fce7e85ccdcc2a8b0351b4cb0078a7611
ddukddaki <rlfrkdms1@naver.com>
2022-11-08 15:14:48 +0900

    feat(Play): 재시작 여부를 확인하는 기능

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import static baseball.Constant.ASK_RESTART_MSG;
+import static baseball.Constant.ERR_MSG;
+import static baseball.Constant.EXIT;
+import static baseball.Constant.RESTART;
+
 import baseball.player.Computer;
 import baseball.player.User;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Play {
@@ -23,5 +29,17 @@ public class Play {
             hint.giveHint(computerNumber, userNumber);
             System.out.println(hint.printHint());
         } while (!hint.isThreeStrike());
+    }
+
+    public boolean isRestart() {
+        System.out.println(ASK_RESTART_MSG);
+        String answer = Console.readLine();
+        if (answer.equals(RESTART)) {
+            return true;
+        } else if (answer.equals(EXIT)) {
+            return false;
+        } else {
+            throw new IllegalArgumentException(ERR_MSG);
+        }
     }
 }
