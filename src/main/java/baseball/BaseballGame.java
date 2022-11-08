1c1e3a2776fa9f9b94035ce433d415a7a1a7374e
silver <silver36c@gmail.com>
2022-11-08 22:21:04 +0900

    refactor: Computer 클래스 분리

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -30,61 +30,7 @@ public class BaseballGame {
 
     }
 
-    class Computer {
-        List<Integer> answer = new ArrayList<>();
-        boolean isGameEnd = false;
 
-        void setAnswer() {
-            answer.clear();
-            while (answer.size() < 3) {
-                int randomNumber = Randoms.pickNumberInRange(1, 9);
-                if (!answer.contains(randomNumber)) {
-                    answer.add(randomNumber);
-                }
-            }
-        }
-
-        void giveResult(List<Integer> suggestion) {
-            final int MAX_STRIKE = 3;
-            int strike = checkStrike(suggestion);
-            int ball = checkBall(suggestion);
-            if (ball != 0) System.out.print(ball + "볼 ");
-            if (strike != 0) System.out.print(strike + "스트라이크");
-            if (ball == 0 && strike == 0) System.out.print("낫싱");
-            System.out.println("");
-            if (strike == MAX_STRIKE) {
-                setIsGameEnd(true);
-            }
-        }
-
-        int checkBall(List<Integer> suggestion) {
-            int ball = 0;
-            for (int i = 0; i < answer.size(); i++) {
-                if (answer.get(i) != suggestion.get(i) && suggestion.contains(answer.get(i))) {
-                    ball++;
-                }
-            }
-            return ball;
-        }
-
-        int checkStrike(List<Integer> suggestion) {
-            int strike = 0;
-            for (int i = 0; i < answer.size(); i++) {
-                if (answer.get(i) == suggestion.get(i)) {
-                    strike++;
-                }
-            }
-            return strike;
-        }
-
-        void setIsGameEnd(boolean toSet) {
-            isGameEnd = toSet;
-        }
-
-        public boolean getIsGameEnd() {
-            return isGameEnd;
-        }
-    }
 }
 
 class Player {
