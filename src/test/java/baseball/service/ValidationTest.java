c3f6afb2dfd654d97fa893607686591fb3695124
footdev <ekclstkfka5@gmail.com>
2022-11-08 21:32:44 +0900

    test: 테스트 코드 작성
    입력이 유효하지 않을 때 예외가 발생하는지 테스트하는 코드 작성함.

---
+++ src/test/java/baseball/service/ValidationTest.java
@@ -0,0 +1,22 @@
+package baseball.service;
+
+import org.junit.jupiter.api.Test;
+
+import static baseball.service.Validation.isValidNumber;
+import static org.assertj.core.api.Assertions.*;
+
+class ValidationTest {
+
+    @Test
+    void 유효하지않은_3자리_숫자가_들어왔을_때_예외_발생() {
+        String[] inputs = {"삼사d", "1 2", "343", "1234", "12삼", "", " ", "360"};
+        for (String number : inputs) {
+            assertThatThrownBy(() -> isValidNumber(number))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Test
+    void isValidReStart() {
+    }
+}
