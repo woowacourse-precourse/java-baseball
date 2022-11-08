9005f8172ca86d674199bb4c10fc0111da293ca3
유미 <yumi@yumis-MacBook-Air.local>
2022-11-08 18:19:06 +0900

    기능2-1 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,13 +1,26 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGame {
+    private List<Integer> number;
+    private Boolean gameRestart = false;
 
     public void run(){
+        // TODO 1. 랜덤 숫자 생성
+        generateNumber();
+//        System.out.println(number);
 
-        // TODO LIST 1. 랜덤 숫자 생성
+        // TODO 2-1. 사용자 입력 받기
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 : ");
+        String newInput = Console.readLine();
+        System.out.println(newInput);
+    }
+
+    private void generateNumber(){
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -15,7 +28,6 @@ public class BaseballGame {
                 computer.add(randomNumber);
             }
         }
-        System.out.println(computer);
-
+        this.number = computer;
     }
 }
