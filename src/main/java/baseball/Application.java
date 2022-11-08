531b0792ab246989e6fa601ea93b25317d9c3b60
lhyeonj120 <isk04387@gmail.com>
2022-11-08 16:34:29 +0900

    feat: 플레이어 입력 숫자 검증 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,10 +5,13 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
 	private static List<Integer> computerNumber;
+	private static List<Integer> userNumber;
+	private static String regex = "^[1-9]{3}$";
 
 	public static void main(String[] args) {
 
@@ -23,5 +26,16 @@ public class Application {
 			}
 		}
 		computerNumber = new ArrayList<>(randomNumberSet);
+	}
+
+	private static void checkUserInput() {
+		String userInput = Console.readLine();
+		if (!userInput.matches(regex)) {
+			throw new IllegalArgumentException();
+		}
+		userNumber = new ArrayList<>();
+		for (int i = 0; i < 3; i++) {
+			userNumber.add(userInput.charAt(i) - '0');
+		}
 	}
 }
