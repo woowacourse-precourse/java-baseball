23880f1b61f48951c56a01514ac0b0d5eed65f69
jooys130 <wndustn130@gmail.com>
2022-11-08 22:48:35 +0900

    refactor: update print statement related constants

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -5,11 +5,11 @@ import static baseball.Constants.*;
 public class View {
 
     public static void printStartMsg() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(START_MSG);
     }
 
     public static void printInputMsg() {
-        System.out.println("숫자를 입력해주세요 : ");
+        System.out.println(INPUT_MSG);
     }
 
     public static void printHintMsg(int[] results) {
@@ -27,10 +27,10 @@ public class View {
     }
 
     public static void printEndMsg() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(END_MSG);
     }
 
     public static void printRestartOrEndMsg() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println(RESTART_OR_END_MSG);
     }
 }
