c694b3f6cf16adc33806c5dc00a6fcee56d1ab31
saehim <saeilee.vxv@gmail.com>
2022-11-08 22:20:29 +0900

    Feat: 메인함수 게임 시작

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import java.util.List;
+
+import static baseball.controller.Computer.createRandomNums;
+import static baseball.controller.Game.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        startGame();
     }
 }
