ba1b2b362f1c536fa03c2f7742b801e1d8f1382f
taekwon-dev <taekwon_dev@naver.com>
2022-11-08 15:37:11 +0900

    feat: 게임 종료 뒤 재시작 없이 종료 케이스 테스트

---
+++ src/test/java/baseball/GameTest.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class GameTest extends NsTest {
+    @DisplayName("게임 종료 뒤 재시작 없이 종료")
+    @Test
+    void endGameRightAfterCorrect() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "135", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
