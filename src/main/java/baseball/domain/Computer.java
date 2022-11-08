031ff473777c868b87bad0ba3109f9dbca32d164
mikekang47 <gyeong0308@gmail.com>
2022-11-08 22:31:15 +0900

    Feat: 컴퓨터 난수 생성 기능 추가

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,5 +1,18 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Computer {
     private Computer() {}
+
+    public static String getRandomNumberString() {
+        List<Integer> randomNumbers = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+
+        return randomNumbers.stream()
+                .map(Object::toString)
+                .collect(Collectors.joining());
+    }
 }
