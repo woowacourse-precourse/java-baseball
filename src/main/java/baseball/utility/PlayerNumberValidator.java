5e1ab396951aeddef3060ebf863db02cc5be750a
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-08 18:06:29 +0900

    feat(feature): 사용자 입력 유효성 확인기능 구현
    PlayerNumberValidator의 isPlayerNumberValid 메서드를 구현
    - 본 메서드는 사용자가 입력한 번호가 유효한 형식인지를 확인함.
    - 이를 위해 내부적으로 아래의 도우미 함수 세 개를 호출함.
      - isPlayerNumberLengthValid
      - isNumericLiteralBetweenOneAndNine
      - isDuplicateNumericLiteral

--- src/main/java/baseball/utility/PlayerNumberValidator.java
+++ src/main/java/baseball/utility/PlayerNumberValidator.java
@@ -8,6 +8,31 @@ public class PlayerNumberValidator {
 
     }
 
+    private static boolean isPlayerNumberValid(String playerNumberOrNull) {
+        if (playerNumberOrNull == null) {
+            return false;
+        }
+
+        String playerNumber = playerNumberOrNull;
+
+        if (!isPlayerNumberLengthValid(playerNumber)) {
+            return false;
+        }
+
+        boolean[] checkArray = new boolean[9];
+        int index = 0;
+
+        while (index < GameConstant.VALID_NUMBER_LENGTH.getValue()
+                && isNumericLiteralBetweenOneAndNine(playerNumber.charAt(index))
+                && !isDuplicateNumericLiteral(playerNumber.charAt(index), checkArray)
+        ) {
+            checkArray[playerNumber.charAt(index) - '1'] = true;
+            index++;
+        }
+
+        return index == GameConstant.VALID_NUMBER_LENGTH.getValue();
+    }
+
     private static boolean isPlayerNumberLengthValid(String playerNumber) {
         return playerNumber.length() == GameConstant.VALID_NUMBER_LENGTH.getValue();
     }
