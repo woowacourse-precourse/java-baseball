a1f7c2fd2a8a1a17a7f2eb0d8ad9255ebb88db71
kksks3192 <kksks3192@gmail.com>
2022-11-08 22:42:53 +0900

    refactor(baseball): 사용자 입력 예외처리 기능 추가

--- src/main/java/baseball/service/User.java
+++ src/main/java/baseball/service/User.java
@@ -1,6 +1,6 @@
 package baseball.service;
 
-import baseball.exception.InputLengthException;
+import baseball.exception.CheckException;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.io.IOException;
@@ -8,14 +8,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
+
     public static List<Integer> inputGuess() throws IOException {
         List<Integer> guess = new ArrayList<>();
+        CheckException checkException = new CheckException();
+
         String[] inputArray = Console.readLine().split("");
-        InputLengthException.isInputLengthThree(inputArray);
+        checkException.validateInputLength(inputArray);
+        checkException.validateInputNum(inputArray);
+        checkException.validateInputDistinct(inputArray);
 
         for(String i : inputArray){
             guess.add(Integer.parseInt(i));
         }
         return guess;
     }
+
 }
