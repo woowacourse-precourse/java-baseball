5ac1f17541d6ae0404b4b06c9089c7e2d07dbde5
kwang <shinkwang@yonsei.ac.kr>
2022-11-08 22:35:40 +0900

    refactor: Separate Methods from Application class
    Application class의 do while문에 담겨있는 메소드들을 분리한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,31 +10,45 @@ public class Application {
         // TODO: 프로그램 구현
         PrintLine.startGamePrint();
 
-        String restartNumber;
-
         do {
             List<Integer> computerNumberList = Game.createComputerNumberList();
-            while (true) {
-                Map<String, Integer> scoreTable = Game.createScoreTable();
-                PrintLine.putUserNumberPrint();
-                String userNumber = Console.readLine();
-                Validation.validateUserNumber(userNumber);
-                List<Integer> userNumberList = stringToIntegerList(userNumber);
+            tryOnceForAnswer(computerNumberList);
 
-                Game.matchLists(userNumberList, computerNumberList, scoreTable);
-                PrintLine.resultPrint(scoreTable);
-
-                if (scoreTable.get("스트라이크") == 3) {
-                    PrintLine.afterAnswerPrint();
-                    break;
-                }
-            }
-
-            PrintLine.putRestartNumberPrint();
-            restartNumber = Console.readLine();
-            Validation.validateRestartNumber(restartNumber);
-        } while (restartNumber.equals("1"));
+        } while (tryAgain());
         PrintLine.endGame();
+    }
+
+    private static void tryOnceForAnswer(List<Integer> computerNumberList) {
+        while (true) {
+            Map<String, Integer> scoreTable = Game.createScoreTable();
+            List<Integer> userNumberList = inputUserNumber();
+
+            Game.matchLists(userNumberList, computerNumberList, scoreTable);
+            PrintLine.resultPrint(scoreTable);
+
+            if (scoreTable.get("스트라이크") == 3) {
+                PrintLine.afterAnswerPrint();
+                break;
+            }
+        }
+    }
+
+    private static boolean tryAgain() {
+        PrintLine.putRestartNumberPrint();
+        String restartNumber = Console.readLine();
+        Validation.validateRestartNumber(restartNumber);
+
+        if (restartNumber.equals("1")) {
+            return true;
+        }
+        return false;
+    }
+
+    private static List<Integer> inputUserNumber() {
+        PrintLine.putUserNumberPrint();
+        String userNumber = Console.readLine();
+        Validation.validateUserNumber(userNumber);
+        return stringToIntegerList(userNumber);
     }
 
     private static List<Integer> stringToIntegerList(String numbers) {
