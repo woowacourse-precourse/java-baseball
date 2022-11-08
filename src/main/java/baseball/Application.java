2a3fb7e789405ac6bdd5a4571ce5add029c82275
Jeonsoyeon <itso22@naver.com>
2022-11-08 14:28:31 +0900

    fix(Application): 객체를 생성하지 않고 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import domain.Game;
 
 public class Application {
     public static void main(String[] args) {
-        Game game = new Game();
-        game.startGame();
+        Game.startGame();
     }
 }
