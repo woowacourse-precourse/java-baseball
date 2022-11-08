8c433244099571df9ca47f44e3fc43f25fc26531
dvlpr0 <rladjswkd@naver.com>
2022-11-08 21:40:07 +0900

    feat: 안내 문구를 출력하는 기능

---
+++ src/main/java/baseball/IOHandler.java
@@ -0,0 +1,19 @@
+package baseball;
+
+public class IOHandler {
+	public static void writeLaunchMessage() {
+		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
+
+	public static void writeInputMessage() {
+		System.out.print("숫자를 입력해주세요 : ");
+	}
+
+	public static void writeCompleteMessage() {
+		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+	}
+
+	public static void writeStatusMessage() {
+		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+	}
+}
