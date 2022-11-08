9ad3d9054e4f574186be1b72509f48c2059815ee
Jenn Kim <grdd0419@gmail.com>
2022-11-08 18:18:09 +0900

    feat(Constants): add a constant for IllegalArgumentException

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -7,4 +7,5 @@ public final class Constants {
     public static final Integer CONTINUE_GAME = 1;
     public static final Integer EXIT_GAME = 2;
 
+    public static final String INPUT_ERROR_MESSAGE = "입력 형식이 잘못되었습니다.";
 }
