f8145a70987a7dc0b68c16c5407e44b09f850c2c
Heesu Jeong <huissppea@gmail.com>
2022-11-08 23:46:09 +0900

    test: 0이 포함된 경우, 숫자 3자리 제한 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -24,6 +25,24 @@ class ApplicationTest extends NsTest {
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    @DisplayName("3자리_숫자_이상인_경우_테스트")
+    void 숫자3자리_이상인_경우_테스트() {
+        String input = "abc";
+
+        assertThatThrownBy(() -> input.charAt(5))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 5");
+    }
+
+    @Test
+    void 영이_포함되어있는지_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("012"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
