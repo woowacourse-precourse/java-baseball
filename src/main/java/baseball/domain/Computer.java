cdfa85c5c8b3666accc6a35b2d585370627c0ec6
ljh <dusk5588@naver.com>
2022-11-08 12:15:43 +0900

    refactor : 컴퓨터 난수 생성 내용 Computer클래스로 이동

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,4 +1,20 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Computer {
+    List<Integer> numbers = new ArrayList<>();
+
+    public void setRandomNumber(){
+        this.numbers.clear();
+        while (numbers.size() < 3) {
+            int randomNumber = pickNumberInRange(1, 9);
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+    }
 }
