38ffc4183f6f1f7f960016a056d299308d6fb0c0
yuntaeLee <leeyt1201@gmail.com>
2022-11-08 14:52:47 +0900

    test: parsePlayerNumbers 메서드 테스트 코드 작성

---
+++ src/test/java/baseball/utils/ParserTest.java
@@ -0,0 +1,23 @@
+package baseball.utils;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ParserTest {
+
+    Parser parser = new Parser();
+
+    @Test
+    void parsePlayerNumbersTest() {
+        String input = "123";
+        List<Integer> result = Arrays.asList(1, 2, 3);
+        assertThat(parser.parsePlayerNumbers(input)).isEqualTo(result);
+    }
+
+}
