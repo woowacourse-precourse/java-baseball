86eef49968fc561b9814d794f9759526991727d0
CupRaccoon <41440969+CupRaccoon@users.noreply.github.com>
2022-11-08 22:41:35 +0900

    test: Add InputReader Test

---
+++ src/test/java/baseball/InputReaderTest.java
@@ -0,0 +1,52 @@
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+
+class InputReaderTest {
+
+    @Test
+    void readGameStatusTest() throws IOException {
+        InputStream in = new ByteArrayInputStream("1".getBytes());
+        System.setIn(in);
+        Assertions.assertThat(InputReader.readGameStatus()).isEqualTo(true);
+
+        in = new ByteArrayInputStream("2".getBytes());
+        System.setIn(in);
+        Assertions.assertThat(InputReader.readGameStatus()).isEqualTo(false);
+
+        in = new ByteArrayInputStream("3".getBytes());
+        System.setIn(in);
+        Assertions.assertThatThrownBy(InputReader::readGameStatus)
+                .isInstanceOf(IllegalArgumentException.class);
+
+
+    }
+
+    @Test
+    void readNumber() {
+        InputStream in = new ByteArrayInputStream("123".getBytes());
+        System.setIn(in);
+        Assertions.assertThat(InputReader.readNumber()).isEqualTo("123");
+
+        in = new ByteArrayInputStream("abc".getBytes());
+        System.setIn(in);
+        Assertions.assertThatThrownBy(InputReader::readNumber)
+                .isInstanceOf(IllegalArgumentException.class);
+
+        in = new ByteArrayInputStream("1234".getBytes());
+        System.setIn(in);
+        Assertions.assertThatThrownBy(InputReader::readNumber)
+                .isInstanceOf(IllegalArgumentException.class);
+
+        in = new ByteArrayInputStream("111".getBytes());
+        System.setIn(in);
+        Assertions.assertThatThrownBy(InputReader::readNumber)
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+}
