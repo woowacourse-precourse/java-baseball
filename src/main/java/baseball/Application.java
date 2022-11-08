3d2052c3fc6b03a6e43141d6cd9ce596e9859eb9
inwoo <korean5078@gmail.com>
2022-11-08 17:57:09 +0900

    feat(src/main/java/baseball/Application/makeRandomNumber) : 랜덤 숫자 생성 함수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String menu = "";
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        List<Integer> computer = makeRandomNumber();
+    }
+
+    public static List<Integer> makeRandomNumber() {
+        List<Integer> number = new ArrayList<>();
+
+        while (number.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if (!number.contains(randomNumber)) {
+                number.add(randomNumber);
+            }
+        }
+        return number;
     }
 }
