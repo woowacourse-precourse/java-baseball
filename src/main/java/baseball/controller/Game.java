8968da4dce2e76e1e6652ffa4f6c4335c2f96efd
saehim <saeilee.vxv@gmail.com>
2022-11-08 22:16:25 +0900

    Feat: 플레이어 숫자 입력

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -7,4 +7,14 @@ public class Game {
     public static int getShutDownCode() {
         return shutDownCode;
     }
+    private static int getInputNums() {
+        showRequestInput();
+        int nums = 0;
+        try {
+            nums = Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            // 0인채로 리턴
+        }
+        return nums;
+    }
 }
