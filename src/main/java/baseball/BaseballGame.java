2b5b2ee7106f4308188cf31768b25c96a23730bb
유미 <yumi@yumis-MacBook-Air.local>
2022-11-08 19:15:21 +0900

    기능2-2 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -17,7 +17,8 @@ public class BaseballGame {
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
         String newInput = Console.readLine();
-        System.out.println(newInput);
+        // TODO 2-2. 예외 처리하기
+        checkInputFormat(newInput);
     }
 
     private void generateNumber(){
@@ -29,5 +30,18 @@ public class BaseballGame {
             }
         }
         this.number = computer;
+    }
+
+    private void checkInputFormat(String newInput){
+        if(newInput.length() != 3){
+            throw new IllegalArgumentException("userInput is index out of range: 3");
+        }
+        for (int i = 0; i < 3; i++){
+            if( newInput.charAt(i) < '1' || newInput.charAt(i) > '9') {
+                throw new IllegalArgumentException("userInput is not configured from number : 1 o 9");
+            }else if(newInput.charAt(i) == newInput.charAt((i+1)%3)) {
+                throw new IllegalArgumentException("duplicate number found in use Input");
+            }
+       }
     }
 }
