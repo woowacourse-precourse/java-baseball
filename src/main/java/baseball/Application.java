ef50766e83e7c51242bcbb0d09700f28b7abacd3
sungjun kim <dd8873@naver.com>
2022-11-08 19:33:09 +0900

    chore : try-catch 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,10 @@
 package baseball;
 
 public class Application {
+
     public static void main(String[] args) {
-        try {
-            ApplicationManager applicationManager = new ApplicationManager();
-            applicationManager.play();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        } catch (Exception e) {
-            System.out.println("알수없는 예외가 발생했습니다.");
-        }
+        ApplicationManager applicationManager = new ApplicationManager();
+        applicationManager.play();
     }
 
 }
