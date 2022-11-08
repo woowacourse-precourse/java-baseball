60c27d3bac82c0f107b45f2ab7a7449494134f58
Brilly-Bohyun <bohyun564@swu.ac.kr>
2022-11-08 12:19:21 +0900

    Feat: 랜덤 숫자 생성 기능 구현
    random함수를 이용하여 1-9까지의 숫자를 랜덤하게 만들고 3자리 숫자가 중복되지 않게 만듬

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        CreateRandomNum();
+
+
+
+    }
+
+    public static List<Integer> CreateRandomNum(){
+        List<Integer> computerNum = new ArrayList<>();
+        while(computerNum.size() < 3){
+            int randomNum = pickNumberInRange(1, 9);
+            if(!computerNum.contains(randomNum)){
+                computerNum.add(randomNum);
+            }
+        }
+        return computerNum;
     }
 }
