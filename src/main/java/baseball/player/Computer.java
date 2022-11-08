819848aceac6caabb54d1856de788f997b95ae38
ckyeon <qsky8520@gmail.com>
2022-11-08 16:02:40 +0900

    feat(Computer): implement 공을 고르는 기능 (override)

--- src/main/java/baseball/player/Computer.java
+++ src/main/java/baseball/player/Computer.java
@@ -1,9 +1,16 @@
 package baseball.player;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Computer extends Player {
 
     @Override
     public void selectBalls() {
-
+        while (getBallsNumber() < 3) {
+            String ball = String.valueOf(Randoms.pickNumberInRange(1, 9));
+            if (!isSelectBall(ball)) {
+                selectBall(ball);
+            }
+        }
     }
 }
