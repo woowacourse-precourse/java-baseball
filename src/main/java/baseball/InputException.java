3ea16f05777c4530775191b0c0bdbcab49635b9a
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-11-08 19:59:24 +0900

    feat(InputExecption): add inputEmptyException, inputNotThreeNumberException, inputNotOneOrTwoException feature

--- src/main/java/baseball/InputException.java
+++ src/main/java/baseball/InputException.java
@@ -1,5 +1,35 @@
 package baseball;
 
 public class InputException {
+    public void inputEmptyException(String input) throws IllegalArgumentException{
+        if (input.isEmpty()) {
+            throw new IllegalArgumentException("inputEmptyException");
+        }
+    }
 
+    public void inputNotThreeNumberException(String input) throws IllegalArgumentException{
+        inputEmptyException(input);
+
+        if(input.length()!=3){
+            throw new IllegalArgumentException("inputNotThreeNumberException: input length is not 3");
+        }
+        
+        for(int location=0; location<3; location++){
+            if(input.charAt(location)<'0' || '9'<input.charAt(location)){
+                throw new IllegalArgumentException("inputNotThreeNumberException: input is not number");
+            }
+        }
+    }
+
+    public void inputNotOneOrTwoException(String input) throws IllegalArgumentException{
+        inputEmptyException(input);
+
+        if(input.length()!=1){
+            throw new IllegalArgumentException("inputNotOneOrTwoException: input length is not 1");
+        }
+
+        if(input.charAt(0)!='1' && input.charAt(0)!='2'){
+            throw new IllegalArgumentException("inputNotOneOrTwoException: input is not 1 or 2");
+        }
+    }
 }
