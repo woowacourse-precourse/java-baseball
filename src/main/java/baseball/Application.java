b3a0792ab76b8ca955704afe0ecf64eefe15ff87
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2022-11-08 15:29:31 +0900

    기본 구조 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,12 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        do{
+            while(true){//strickCount!=3
+                //input
+                //count strike and ball
+            }
+            //end game
+        }while(true);//replay or exit
     }
 }
