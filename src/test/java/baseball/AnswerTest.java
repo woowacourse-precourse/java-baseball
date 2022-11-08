3520d8c710c09eda36bb9568282820bfe2c2f0bb
unknown <dragontaek98@naver.com>
2022-11-08 19:26:15 +0900

    fix: random 세자리 수 생성 기능 test file 분리

--- src/test/java/baseball/AnswerTest.java
+++ src/test/java/baseball/AnswerTest.java
@@ -2,19 +2,14 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestClassOrder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class MyTest extends NsTest {
+class AnswerTest extends NsTest {
 
     @Test
     void 랜덤_정답생성_데이터형_테스트(){
