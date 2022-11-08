e2ee7971bd2ffba7580f534bb4b45a182d8622a3
Dabeen Jeong <hello70825@gmail.com>
2022-11-08 21:14:27 +0900

    feat: User에 입력값을 검증하는 코드를 추가하다.

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,14 +1,21 @@
 package baseball;
 
+import baseball.resource.AskRestartValue;
 import baseball.resource.GameMessage;
+import baseball.validation.AskValidation;
 import baseball.validation.GameValidation;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
     private String userBaseballNumber = "";
+    private String userRestartNumber = "";
 
     public void setUserBaseballNumber(String userBaseballNumber) {
         this.userBaseballNumber = userBaseballNumber;
+    }
+
+    public void setUserRestartNumber(String userRestartNumber) {
+        this.userRestartNumber = userRestartNumber;
     }
 
     public boolean inputBaseballNumber() {
@@ -28,5 +35,34 @@ public class User {
         gameValidation.setUserInput(this.userBaseballNumber);
 
         return gameValidation.isAllCheck();
+    }
+
+    public boolean inputRestartNumber() {
+        String userInput;
+
+        System.out.print(GameMessage.ASK_RESTART_GAME);
+        userInput = Console.readLine();
+
+        setUserRestartNumber(userBaseballNumber);
+
+        return isCheckRestart(userInput);
+    }
+
+    private boolean isCheckRestart(String userInput) {
+        AskValidation askValidation = new AskValidation();
+
+        return askValidation.isCheckRestartInput(userInput);
+    }
+
+    public boolean getRestartStatus() {
+        if (this.userRestartNumber.equals(AskRestartValue.RESTART_GAME)) {
+            return true;
+        }
+
+        if (this.userRestartNumber.equals(AskRestartValue.END_GAME)) {
+            return false;
+        }
+
+        return false;
     }
 }
