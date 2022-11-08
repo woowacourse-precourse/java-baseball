aa0cb3ecf0f50ea8ffdfbe6b608bce40706937c5
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-08 18:39:48 +0900

    test(User): 중복되는 번호가 존재하는지 확인

--- src/test/java/baseball/UserTest.java
+++ src/test/java/baseball/UserTest.java
@@ -17,4 +17,13 @@ class UserTest {
 
         assertThat(new HashSet<>(nums)).hasSize(3);
     }
+
+    @Test
+    void 중복된_번호_존재하면_에러() {
+        User user = new User();
+        List<Integer> nums = List.of(1,2,2);
+        assertThatThrownBy(() -> user.createNums(nums, 3))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 번호가 있습니다.");
+    }
 }
