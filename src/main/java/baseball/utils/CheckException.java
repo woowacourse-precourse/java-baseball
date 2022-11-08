b7886e324363b2ab6fa60424308a254219386304
Go-Jaecheol <gojaech@naver.com>
2022-11-08 21:58:02 +0900

    refactor: 사용자 입력 숫자 중 중복된 경우 예외 처리

--- src/main/java/baseball/utils/CheckException.java
+++ src/main/java/baseball/utils/CheckException.java
@@ -10,20 +10,30 @@ public class CheckException {
         checkInputSize(input);
         for (int i = 0; i < input.length(); i++) {
             checkInputBoundary(input.charAt(i));
-            returnInput.add(Character.getNumericValue(input.charAt(i)));
+            int convertedNumber = Character.getNumericValue(input.charAt(i));
+            if (!checkDuplicate(returnInput, convertedNumber)) {
+                returnInput.add(convertedNumber);
+            }
         }
         return returnInput;
     }
 
-    private static void checkInputSize(String input) {
+    private static void checkInputSize(String input) throws IllegalArgumentException {
         if (input.length() != 3) {
             throw new IllegalArgumentException();
         }
     }
 
-    private static void checkInputBoundary(char c) {
-        if (c < '0' || c > '9') {
+    private static void checkInputBoundary(char c) throws IllegalArgumentException {
+        if (c < '1' || c > '9') {
             throw new IllegalArgumentException();
         }
+    }
+
+    private static boolean checkDuplicate(List<Integer> list, int num) throws IllegalArgumentException {
+        if (list.contains(num)) {
+            throw new IllegalArgumentException();
+        }
+        return false;
     }
 }
