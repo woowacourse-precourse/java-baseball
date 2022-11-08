c45d92725324c29711514ea07eac64fdc5144c10
moonn6pence <shin421179@gmail.com>
2022-11-08 13:21:43 +0900

    feat : 메시지 상수화
    - Message 클래스에 각종 상수 선언
    - Application의 디폴트 주석 제거

---
+++ src/main/java/baseball/Message.java
@@ -0,0 +1,16 @@
+package baseball;
+
+public class Message {
+    public static final String REPLAY_INPUT = "1";
+    public static final String END_INPUT = "2";
+
+    public static final int INPUT_SIZE = 3;
+
+    public static final String HINT_BALL = "볼";
+    public static final String HINT_STRIKE = "스트라이크";
+    public static final String HINT_NOTHING = "낫싱";
+
+    public static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    public static final String GAME_WIN = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String GAME_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+}
