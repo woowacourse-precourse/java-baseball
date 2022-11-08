b97b5372efe4772e9c608fa0014060a4f0f042b6
HangSub999 <gkdtjq999@gmail.com>
2022-11-08 20:23:53 +0900

    refactor: 예외처리 제거

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,14 +2,11 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        try {
-            BaseBallGameManager baseBallGameManager = new BaseBallGameManager();
-            baseBallGameManager.startGame();
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        } catch (Exception e) {
-            System.out.println("예기치 못한 오류 발생");
-        }
+
+        BaseBallGameManager baseBallGameManager = new BaseBallGameManager();
+        baseBallGameManager.startGame();
+
     }
 }
+
 
