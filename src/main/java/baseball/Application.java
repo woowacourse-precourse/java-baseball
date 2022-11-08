e7cecf3b946225d7df082250f77c8d512aa938e9
junbeom-Son <89973303+junbeom-Son@users.noreply.github.com>
2022-11-08 22:39:40 +0900

    tyle : vertical space
    add vertical spaces
    to comply with the java code convention

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.List;
 import java.util.ArrayList;
 
@@ -18,16 +17,21 @@ public class Application {
     private static final String BALL_STRING = "볼";
     private static final String STRIKE_STRING = "스트라이크";
     private static final String THREE_STRIKE_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
     private static final int INPUT_BASEBALL_LENGTH = 3;
     private static final int INPUT_REPLAY_LENGTH = 1;
     private static final int MAX_STRIKE = 3;
+
     private static final char REPLAY = '1';
     private static final char END = '2';
+
     public static void main(String[] args) {
         boolean playGame = true;
+
         while (playGame) {
             playGame = false;
             gameStart();
+
             if(willReplay()) {
                 playGame = true;
             }
@@ -37,15 +41,20 @@ public class Application {
 
     static void gameStart() throws IllegalArgumentException {
         System.out.println(START_MESSAGE);
+
         int[] computerNumber = initializeComputerNumber();
         boolean playBaseball = true;
+
         while (playBaseball) {
             String stringUserInput = getUserInput(InputType.BASEBALL_NUMBER);
             isValidInput(stringUserInput, InputType.BASEBALL_NUMBER);
+
             int[] userInput = stringToIntegerArray(stringUserInput);
             int ball = countBalls(userInput, computerNumber);
             int strike = countStrikes(userInput, computerNumber);
+
             printResult(ball, strike);
+
             if (isThreeStrike(strike)) {
                 System.out.println(THREE_STRIKE_MESSAGE);
                 playBaseball = false;
@@ -55,12 +64,15 @@ public class Application {
 
     static int[] initializeComputerNumber() {
         List<Integer> computerNumberList = new ArrayList<>();
+
         while (computerNumberList.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
+
             if (!computerNumberList.contains(randomNumber)) {
                 computerNumberList.add(randomNumber);
             }
         }
+
         int[] computerNumberArray = computerNumberList.stream().mapToInt(i -> i).toArray();
 
         return computerNumberArray;
@@ -69,9 +81,12 @@ public class Application {
     static String getUserInput(InputType inputType) {
         if (inputType == InputType.BASEBALL_NUMBER) {
             System.out.print(GAME_INPUT_MESSAGE);
+
         } else if (inputType == InputType.WILL_REPLAY) {
+
             System.out.println(REPLAY_MESSAGE);
         }
+
         String userInput = Console.readLine();
 
         return userInput;
@@ -81,19 +96,24 @@ public class Application {
         if (inputType == InputType.BASEBALL_NUMBER) {
             if (userInput.length() != INPUT_BASEBALL_LENGTH) {
                 throw new IllegalArgumentException(INPUT_LENGTH_ERROR_MESSAGE);
+
             }
             if (!isNumeric(userInput)) {
                 throw new IllegalArgumentException(INPUT_NOT_NUMERIC_ERROR_MESSAGE);
+
             }
             if (hasDuplicatedNumber(userInput)) {
                 throw new IllegalArgumentException(INPUT_DUPLICATED_ERROR_MESSAGE);
+
             }
         } else if (inputType == InputType.WILL_REPLAY) {
             if (userInput.length() != INPUT_REPLAY_LENGTH) {
                 throw new IllegalArgumentException(INPUT_REPLAY_ERROR_MESSAGE);
+
             }
             if (!(userInput.charAt(0) == REPLAY || userInput.charAt(0) == END)) {
                 throw new IllegalArgumentException(INPUT_REPLAY_ERROR_MESSAGE);
+
             }
         }
     }
@@ -104,6 +124,7 @@ public class Application {
             if (!(userInput.charAt(i) >= '1' && userInput.charAt(i) <= '9')) {
                 result = false;
                 break;
+
             }
         }
 
@@ -112,15 +133,20 @@ public class Application {
 
     static boolean hasDuplicatedNumber(String userInput) {
         boolean isDuplicated = false;
+
         char firstLetter = userInput.charAt(0);
         char secondLetter = userInput.charAt(1);
         char thirdLetter = userInput.charAt(2);
+
         if (firstLetter == secondLetter) {
             isDuplicated = true;
+
         } else if (firstLetter == thirdLetter) {
             isDuplicated = true;
+
         } else if (secondLetter == thirdLetter) {
             isDuplicated = true;
+
         }
 
         return isDuplicated;
@@ -128,8 +154,10 @@ public class Application {
 
     static int[] stringToIntegerArray(String stringUserInput) {
         int[] input = new int[3];
+
         for (int i = 0; i < INPUT_BASEBALL_LENGTH; ++i) {
             input[i] = Integer.parseInt(String.valueOf(stringUserInput.charAt(i)));
+
         }
 
         return input;
@@ -137,9 +165,11 @@ public class Application {
 
     static int countBalls(int[] userInput, int[] computerNumber) {
         int count = 0;
+
         for (int i = 0; i < INPUT_BASEBALL_LENGTH; ++i) {
             if (isBall(userInput[i], i, computerNumber)) {
                 count += 1;
+
             }
         }
 
@@ -150,9 +180,11 @@ public class Application {
         for (int i = 0; i < INPUT_BASEBALL_LENGTH; ++i) {
             if (i == index) {
                 continue;
+
             }
             if (userInput == computerNumber[i]) {
                 return true;
+
             }
         }
 
@@ -161,9 +193,11 @@ public class Application {
 
     static int countStrikes(int[] userInput, int[] computerNumber) {
         int count = 0;
+
         for (int i = 0; i < INPUT_BASEBALL_LENGTH; ++i) {
             if (userInput[i] == computerNumber[i]) {
                 count += 1;
+
             }
         }
 
@@ -173,22 +207,29 @@ public class Application {
     static void printResult(int ball, int strike) {
         boolean hasBall = false;
         boolean hasStrike = false;
+
         StringBuilder result = new StringBuilder();
+
         if (ball > 0) {
             result.append(ball + BALL_STRING);
             hasBall = true;
+
         }
         if (strike > 0) {
             if (hasBall) {
                 result.append(" ");
+
             }
             result.append(strike + STRIKE_STRING);
             hasStrike = true;
+
         }
         if (!(hasBall || hasStrike)) {
             System.out.println(RESULT_NOTHING_MESSAGE);
+
         } else if (hasBall || hasStrike) {
             System.out.println(result);
+
         }
     }
 
@@ -199,12 +240,14 @@ public class Application {
     static boolean willReplay() {
         boolean replay = false;
         String playOrNot = "";
+
         playOrNot = getUserInput(InputType.WILL_REPLAY);
         isValidInput(playOrNot, InputType.WILL_REPLAY);
+
         if (playOrNot.charAt(0) == REPLAY) {
             replay = true;
-        }
 
+        }
         return replay;
     }
 }
