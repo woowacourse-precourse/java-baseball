3866e05e23843df0fea932c8c9060a62ffaa71e8
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-08 21:02:15 +0900

    feat : 입력값 검증 메서드 추가

--- src/main/java/baseball/InputView.java
+++ src/main/java/baseball/InputView.java
@@ -3,10 +3,26 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
-    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static String inputBallNumber() {
+        System.out.print(MessageUtils.INPUT_NUMBER_MESSAGE);
+        return validateBallNumberSize(Console.readLine());
+    }
 
-    public static String inputNumber() {
-        System.out.print(INPUT_NUMBER_MESSAGE);
-        return Console.readLine();
+    private static String validateBallNumberSize(String ballNumber) {
+        if (ballNumber.length() != MessageUtils.BALL_SET_SIZE) {
+            throw new IllegalArgumentException(MessageUtils.NUMBER_SIZE_OUT_OF_RANGE_MESSAGE);
+        }
+        return ballNumber;
+    }
+
+    public static String inputPlayNumber() {
+        return validateIsProperNumber(Console.readLine());
+    }
+
+    private static String validateIsProperNumber(String playNumber) {
+        if (!playNumber.equals(MessageUtils.START_TEXT) && !playNumber.equals(MessageUtils.FINISH_TEXT)) {
+            throw new IllegalArgumentException(MessageUtils.NOT_PROPER_INPUT_MESSAGE);
+        }
+        return playNumber;
     }
 }
