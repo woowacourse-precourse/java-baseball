bdd217e68b6cfb284bf810cc50acb2da19b6bb75
Jeeseob <jeeseob5761@gmail.com>
2022-11-08 15:16:06 +0900

    feat: ball count를 문자열로 반환하는 함수 추가

--- src/main/java/baseball/BallCount.java
+++ src/main/java/baseball/BallCount.java
@@ -32,4 +32,15 @@ public class BallCount {
         }
     }
     // TODO: ballcount comment 반환 override toString
+    @Override
+    public String toString() {
+        if (ball == 0 && strike == 0) {
+            return "낫싱";
+        } else if (ball == 0) {
+            return strike  + "스트라이크";
+        } else if (strike == 0) {
+            return ball + "볼";
+        }
+        return ball + "볼 " + strike + "스트라이크";
+    }
 }
