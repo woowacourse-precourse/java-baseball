36ec843a546af8522052b1b3d93300d0a375943e
davinc71998 <davinc71998@gmail.com>
2022-11-08 19:21:08 +0900

    FEAT: 추가 main 함수
    숫자 야구 시작 알림 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -90,6 +90,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.\n");
         int gameContinue = GAME_RESTART;
         while (gameContinue != GAME_END) {
             List<Integer> computerCreateNumber = createRandomNumber();
