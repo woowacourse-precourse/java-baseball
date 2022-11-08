dbaf9b25f4f49f48d72f73e095566c54864be0ac
p <gustjr2332@naver.com>
2022-11-08 13:57:29 +0900

    feat(CheckRestartNumberException) : 정답을 맞힌 후 1과 2 이외의 입력 예외처리

---
+++ src/main/java/baseball/game/CheckRestartNumberException.java
@@ -0,0 +1,18 @@
+package baseball.game;
+
+public class CheckRestartNumberException {
+
+    public void checkException(String number) {
+        try {
+            wrongNumberException(number);
+        }
+        catch (Exception e) {
+            throw new IllegalArgumentException();
+        }
+    }
+    private static void wrongNumberException(String number) throws Exception {
+        if (!number.equals(1) || !number.equals(2)) {
+            throw new Exception("1과 2가 아닌 다른 번호가 입력되었습니다.");
+        }
+    }
+}
