27cf30b89ae5b6ede91d177035d1785d9b5b04fb
JeonSangYong <tkddyd420@naver.com>
2022-11-08 10:41:25 +0900

    feat : 게임 시작

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,21 @@
 package baseball;
 
 public class Game {
+    private static final int MAX_STRIKE = 3;
+    private static final Computer computer = new Computer();
+    private static final Player player = new Player();
+    private boolean isGameEnd = false;
+    private int strike = 0;
+    private int ball = 0;
+    public void start() {
+        while(!isGameEnd) {
+            // 게임 초기화
+            while(strike != MAX_STRIKE) {
+                // 플레이어가 수 입력
+                // 컴퓨터와 플레이어 수 비교
+            }
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            // 게임 다시 시작 or 완전 종료 선택
+        }
+    }
 }
