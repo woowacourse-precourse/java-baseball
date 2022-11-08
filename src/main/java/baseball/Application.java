70e19ddd90e701ee3ac1193e4e7db7a1b7d87fc5
lms0806 <godmlzkf1@naver.com>
2022-11-08 12:01:45 +0900

    style:시작, 종료 문구 스타일 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,6 @@ import model.Computer;
 public class Application {
 	public static void main(String[] args) {
 		// TODO: 프로그램 구현
-		System.out.println("숫자 야구 게임을 시작합니다.");
 		boolean isgame = true;
 		while (isgame) {
 			Computer computer = new Computer();
--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -4,7 +4,11 @@ import camp.nextstep.edu.missionutils.Console;
 import model.User;
 
 public class Play {
+	public static final String start_print = "숫자 야구 게임을 시작합니다.";
+	public static final String end_print = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+	
 	public void playgame(int[] guessnumber) {
+		System.out.println(start_print);
 		int strike = 0, ball = 0;
 		int[] answer = null;
 
@@ -39,7 +43,7 @@ public class Play {
 	}
 
 	public boolean replay_game() {
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		System.out.println(end_print);
 
 		try {
 			int number = Integer.parseInt(Console.readLine());
