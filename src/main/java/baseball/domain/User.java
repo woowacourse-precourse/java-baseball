fac62717808d6421efa3dd2c9f11b4d1a3abe30c
Jung <anes53027@gmail.com>
2022-11-08 22:22:28 +0900

    feat(Computer) : User 클래스 및 생성자 정의

---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private final List<Integer> user;
+
+    public User() {
+        user = new ArrayList<>();
+    }
+
+}
