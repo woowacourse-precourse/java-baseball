5a43a66ae99451c17d14183c41b745bb72fec8a2
yoon <beatus96@naver.com>
2022-11-08 12:46:49 +0900

    test(controllerTest) : 볼,스트라이크,낫싱 판별 기능 테스트
    - 올바른 결과값 Map 타입으로 반환하는지 테스트
    - Map에 있는 결과를 문자열로 잘 변환하는지 테스트

---
+++ src/test/java/baseball/ControllerTest.java
@@ -0,0 +1,65 @@
+
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ControllerTest {
+
+    @Test
+    void 컴퓨터수_플레이어수_비교하기_테스트() {
+        List<Integer> computerNumber = List.of(8, 9, 3);
+        List<Integer> playerNumber = List.of(1, 2, 3);
+        Map<String, Integer> resultMap = Map.of("strike", 1, "nothing", 2);
+        GameController controller = new GameController();
+        assertThat(controller.checkAnswer(computerNumber, playerNumber)).isEqualTo(resultMap);
+
+    }
+
+    @Test
+    void 컴퓨터수_플레이어수_비교하기_테스트2() {
+        List<Integer> computerNumber = List.of(7, 9, 3);
+        List<Integer> playerNumber = List.of(1, 7, 3);
+        Map<String, Integer> resultMap = Map.of("strike", 1, "ball", 1, "nothing", 1);
+        GameController controller = new GameController();
+        assertThat(controller.checkAnswer(computerNumber, playerNumber)).isEqualTo(resultMap);
+    }
+
+    @Test
+    void 결과값_문자열_테스트1() {
+        Map<String, Integer> resultMap = Map.of("strike", 1, "ball", 1, "nothing", 1);
+        String hint = "1볼 1스트라이크";
+        GameController controller = new GameController();
+        String result = controller.createHint(resultMap);
+        System.out.println(result);
+
+        assertThat(result).isEqualTo(hint);
+    }
+
+    @Test
+    void 결과값_문자열_테스트2() {
+        Map<String, Integer> resultMap = Map.of("nothing", 3);
+        String hint = "낫싱";
+        GameController controller = new GameController();
+        String result = controller.createHint(resultMap);
+        System.out.println(result);
+
+        assertThat(result).isEqualTo(hint);
+    }
+
+    @Test
+    void 결과값_문자열_테스트3() {
+        Map<String, Integer> resultMap = Map.of("strike", 2, "nothing", 1);
+        String hint = "2스트라이크";
+        GameController controller = new GameController();
+        String result = controller.createHint(resultMap);
+        System.out.println(result);
+
+        assertThat(result).isEqualTo(hint);
+    }
+}
+
