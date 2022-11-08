96ae911ca339aecdbf7f1076a4bd84d0d2710fba
Junho Choi <ajchoi0928@gmail.com>
2022-11-08 10:45:50 +0900

    refactor($baseball): 컴퓨터 설정과 관련된 코드 리팩토링
    상대방(컴퓨터)가 갖는 정답 초기화 메소드 관련부를 별도의 클래스로 추출
    - Targetable.java : 다양한 상대 컴포넌트를 갖을 수 있도록 추상화
    - Application.java : 메소드 initGame에 Targetable 의존성 주입

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,23 +1,21 @@
 package baseball;
 
+import baseball.component.Computer;
+import baseball.component.Targetable;
 import baseball.gameRule.GameRule;
 import baseball.gameRule.GameRuleDetail;
 import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         Application app = new Application();
-        app.initGame(new GameRuleDetail());
+        app.initGame(new Computer(), new GameRuleDetail());
     }
 
     /**
      * 게임 진행을 담당하는 함수
      */
-    public void initGame(GameRule gameRule) {
+    public void initGame(Targetable target, GameRule gameRule) {
         GameStatus status = GameStatus.CONTINUE;
         do {
             if (GameStatusUtil.isStopStatus(status)) {
@@ -25,36 +23,10 @@ public class Application {
             }
             GameStatusUtil.validateExceptionStatus(status);
             GameFormatter.gameStartMessage();
-            String answer = initComputerAnswer();
+            String answer = target.initAnswer();
             gameRule.playByGameRule(answer);
             GameFormatter.gameEndMessage();
             status = GameStatusUtil.setGameStatus(Console.readLine());
         } while (true);
     }
-
-    /**
-     * 상대방(컴퓨터)가 1부터 9까지 셔플한 결과 중 3개를 뽑아 문자열로 반환
-     *
-     * @return answer
-     */
-    String initComputerAnswer() {
-        StringBuffer answer = new StringBuffer();
-        // pickNumberInRange를 이용하는 함수를 호출해 문자열로 변환 후 반환
-        for(Integer num : getRandomNumberList()){
-            answer.append(num);
-        }
-        return answer.toString();
-    }
-
-    List<Integer> getRandomNumberList(){
-        List<Integer> list = new ArrayList<>();
-        while(list.size() < 3){
-            int randNum = Randoms.pickNumberInRange(1, 9);
-            if(!list.contains(randNum)){
-                list.add(randNum);
-            }
-        }
-        return list;
-    }
-
 }
