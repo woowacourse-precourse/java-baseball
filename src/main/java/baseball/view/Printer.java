7314a2900cda73aa3c85151d1dfc46f7496acfdf
junyong <jymoon1121@gmail.com>
2022-11-08 13:59:45 +0900

    feat(Printer): Create Printer that print console message

--- src/main/java/baseball/view/Printer.java
+++ src/main/java/baseball/view/Printer.java
@@ -1,5 +1,23 @@
 package baseball.view;
 
 public class Printer {
+    public static void start() {
+        String message = "숫자 야구 게임을 시작합니다.";
+        System.out.println(message);
+    }
 
+    public static void inputNumber() {
+        String message = "숫자를 입력해주세요.";
+        System.out.println(message);
+    }
+
+    public static void correctAll() {
+        String message = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+        System.out.println(message);
+    }
+
+    public static void askRestartOrExit() {
+        String message = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+        System.out.println(message);
+    }
 }
