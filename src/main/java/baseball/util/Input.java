320db7a098e6efef93a20d1379529ec45ad9fa8f
hum02 <ehdgma02@gmail.com>
2022-11-08 19:25:36 +0900

    feat($validation): 사용자가 입력한 예상숫자가 서로 다른 수인지 검증 메소드 추가

--- src/main/java/baseball/util/Input.java
+++ src/main/java/baseball/util/Input.java
@@ -2,6 +2,9 @@ package baseball.util;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static baseball.type.GameFlow.GAME_RESTART;
 import static baseball.type.GameFlow.GAME_END;
 import static baseball.type.BaseballBound.*;
@@ -25,16 +28,35 @@ public class Input {
         return playerNumber;
     }
 
-    private static void validateRestartFlag(String restartFlag) {
+    private static void validateRestartFlag(String restartFlag) throws IllegalArgumentException {
         if (!((restartFlag.equals(RESTART)) || (restartFlag.equals(END)))) {
             throw new IllegalArgumentException("잘못된 입력값입니다. 1이나 2를 입력해주세요");
         }
     }
 
-    private static void validatePlayerNumber(String playerNumber) {
+    private static void validatePlayerNumber(String playerNumber) throws IllegalArgumentException {
+        validateNumberSize(playerNumber);
+        validateDuplicatedNumber(playerNumber);
+        validateZeroNumber(playerNumber);
+    }
+
+    private static void validateNumberSize(String playerNumber) throws IllegalArgumentException {
         if (playerNumber.length() != BASEBALL_MAX_SIZE.getValue()) {
             throw new IllegalArgumentException("잘못된 입력값입니다. 3자리의 수를 입력해주세요");
         }
+    }
+
+    private static void validateDuplicatedNumber(String playerNumber) throws IllegalArgumentException {
+        List<Character> inputNumbers = new ArrayList<>();
+        for(int i=0; i<playerNumber.length(); i++) {
+            if (inputNumbers.contains(playerNumber.charAt(i))) {
+                throw new IllegalArgumentException("서로다른 수를 입력해야 합니다");
+            }
+            inputNumbers.add(playerNumber.charAt(i));
+        }
+    }
+
+    private static void validateZeroNumber(String playerNumber) throws IllegalArgumentException {
         for(int i=0; i<playerNumber.length(); i++) {
             if (playerNumber.charAt(i) == '0') {
                 throw new IllegalArgumentException("잘못된 입력값입니다. 1-9로 이뤄진 수를 입력해주세요");
