a916a977f5ff954c13856e7b0ed2937ff9e3e014
xxeol2 <jhj01177@naver.com>
2022-11-08 22:28:03 +0900

    refactor: 게임 안내 문구 상수추가

--- src/main/java/baseball/constant/Message.java
+++ src/main/java/baseball/constant/Message.java
@@ -1,9 +1,11 @@
 package baseball.constant;
 
+import static baseball.constant.Rules.NUMBER_LENGTH;
+
 public class Message {
 
     public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_NUMBER = "숫자를 입력해주세요 : ";
-    public static final String FINISH_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String FINISH_GAME = NUMBER_LENGTH + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public static final String RESTART_GAME = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 }
