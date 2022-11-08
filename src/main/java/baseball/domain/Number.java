f4dd64c96ef62b88132a36a7c27f159ad2f526db
jeonyuneo <piq2255@gmail.com>
2022-11-08 23:06:49 +0900

    feat: 사용자가 수를 입력한다.

--- src/main/java/baseball/domain/Number.java
+++ src/main/java/baseball/domain/Number.java
@@ -6,4 +6,19 @@ public class Number {
     public Number(int value) {
         this.value = value;
     }
+
+    public Number(char value) {
+        this.value = toInt(value);
+    }
+
+    private int toInt(char value) {
+        value -= 48;
+        if (value == 0) {
+            throw new IllegalArgumentException("범위(1~9)를 벗어나는 수가 포함되었습니다.");
+        }
+        if (value < 1 || value > 9) {
+            throw new IllegalArgumentException("숫자가 아닌 값이 포함되었습니다.");
+        }
+        return value;
+    }
 }
--- src/main/java/baseball/service/Game.java
+++ src/main/java/baseball/service/Game.java
@@ -2,6 +2,8 @@ package baseball.service;
 
 import baseball.domain.Number;
 import baseball.domain.Numbers;
+import baseball.view.View;
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
@@ -9,12 +11,16 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class Game {
+    private static final StringBuilder RESULT = new StringBuilder();
+    private static final String BLANK = "";
+    private static final int INITIALIZE = 0;
 
     private Game() {
     }
 
     public static void start() {
         Numbers computer = new Numbers(getComputerInput());
+        play();
     }
 
     private static List<Number> getComputerInput() {
@@ -22,5 +28,25 @@ public class Game {
                 .map(n -> Randoms.pickNumberInRange(1, 9))
                 .mapToObj(Number::new)
                 .collect(Collectors.toList());
+    }
+
+    private static void play() {
+        RESULT.setLength(INITIALIZE);
+        View.printInputRequest();
+        Numbers player = new Numbers(getPlayerInput());
+    }
+
+    private static List<Number> getPlayerInput() {
+        String input = Console.readLine();
+        checkBlank(input);
+        return IntStream.range(0, input.length())
+                .mapToObj(n -> new Number(input.charAt(n)))
+                .collect(Collectors.toList());
+    }
+
+    private static void checkBlank(String input) {
+        if (input.equals(BLANK)) {
+            throw new IllegalArgumentException("값이 입력되지 않았습니다.");
+        }
     }
 }
