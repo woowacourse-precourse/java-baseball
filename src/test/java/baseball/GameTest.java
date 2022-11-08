993bbf3120dd2238416850a4e3490c9f2d9b36d9
star-sil <kimse9811@naver.com>
2022-11-08 22:54:18 +0900

    TEST 유저로부터 3자리수 입력받기 기능 테스트

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -7,6 +7,7 @@ import java.util.HashSet;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class GameTest {
 
@@ -30,6 +31,25 @@ public class GameTest {
         }
 
         assertThat(set.size()).isEqualTo(3);
+    }
+
+    @Test
+    void 유저의_입력값_알맞은_값인지_확인하기() {
+
+        String tooLongInput = "12345";
+        assertThatThrownBy(() -> baseballGame.checkUserInput(tooLongInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("3자리 이상을 입력하였습니다.");
+
+        String wrongInput = "1a2";
+        assertThatThrownBy(() -> baseballGame.checkUserInput(wrongInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("숫자가 아닌 다른 문자를 입력하였습니다.");
+
+        String duplicateInput = "122";
+        assertThatThrownBy(() -> baseballGame.checkUserInput(duplicateInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 숫자를 입력하였습니다.");
     }
 
 }
