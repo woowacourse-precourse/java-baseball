842d626d500e9a62743828782681d844d2473f85
CJY-kor <vivo_reprise@naver.com>
2022-11-08 21:12:47 +0900

    feat : StartBaseballGame, main 구현
    야구 게임 실행을 구현한 StartBaseballGame 기능을 구현하였고, 이를 이용하여 main 함수를 구현하였다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,10 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application agent = new Application();
+
+        while(agent.StartBaseballGame()){
+        }
     }
 
     public boolean isEnd(String number){
@@ -75,6 +78,42 @@ public class Application {
     }
 
     public String MakeRandomNum(){
-        return Integer.toString(Randoms.pickNumberInRange(100, 999));
+        ArrayList<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return Integer.toString(computer.get(0)) + Integer.toString(computer.get(1)) + Integer.toString(computer.get(2));
+    }
+
+    public boolean StartBaseballGame(){
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        String computerNum = MakeRandomNum();
+
+        while (true){
+            System.out.print("숫자를 입력해주세요 : ");
+            String userNum = Console.readLine();
+
+            CheckInput(userNum);
+            ArrayList<Integer> tempHint = GetHint(computerNum, userNum);
+            System.out.println(PrintHint(tempHint));
+
+            if (tempHint.get(1) == 3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String tempEndNum = Console.readLine();
+                if (tempEndNum.equals("1")){
+                    return true;
+                } else if (tempEndNum.equals("2")) {
+                    return false;
+                }else {
+                    throw new IllegalArgumentException();
+                }
+            }
+        }
     }
 }
