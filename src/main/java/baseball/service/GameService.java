6941880be52c7cac93e90930d0cfbc8fd241cc9e
myungjunlee <dlaudwns789@gmail.com>
2022-11-08 16:46:20 +0900

    feat(service): add function to prepare for game start

---
+++ src/main/java/baseball/service/GameService.java
@@ -0,0 +1,16 @@
+package baseball.service;
+
+import baseball.vo.Master;
+import baseball.utils.NumberGenerator;
+
+public class GameService {
+
+    int digit;
+    Master master;
+
+    public void setGame(int digit, int minNumber, int maxNumber) {
+        this.digit = digit;
+        master = new Master(NumberGenerator.createRandomNumbers(digit, minNumber, maxNumber));
+    }
+
+}
