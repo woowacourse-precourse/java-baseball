cc7dfb65c4fce3aa750566eb357f2e3218be0a49
smc2315 <choeyongtae085@gmail.com>
2022-11-08 21:33:32 +0900

    feat: readme

--- docs/README.md
+++ docs/README.md
@@ -1,3 +1,8 @@
 ## main class
-## computer class
-## 
+게임을 실행
+## Computer class
+숫자를 고르는 컴퓨터
+## Game class
+게임 시작, 중지, 반복, 메시지 출력
+## Player class
+숫자 고르기
--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -7,8 +7,6 @@ public class Game {
     private List<Integer> ComputerNumbers;
     public Game(){
         System.out.println("숫자 야구 게임을 시작합니다.");
-    }
-    public void playGame(){
         getComputerNumbers();
     }
     public void getComputerNumbers(){
