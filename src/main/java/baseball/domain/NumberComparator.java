e662234367259d07bc0158133e808db5ffafa5fa
semi-cloud <sksmsrkdtpal@naver.com>
2022-11-08 13:16:07 +0900

    refactor(Game): 스트라이크와 볼 개수를 세는 기능에 스트림 적용
    for문 -> IntStream 으로 변경

--- src/main/java/baseball/domain/NumberComparator.java
+++ src/main/java/baseball/domain/NumberComparator.java
@@ -3,6 +3,8 @@ package baseball.domain;
 import baseball.common.Constant;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 
 public final class NumberComparator {
@@ -10,22 +12,15 @@ public final class NumberComparator {
     private NumberComparator() {}
 
     public static int getStrikeCount(List<Integer> computer, List<Integer> player) {
-        int strike = 0;
-        for (int i = 0; i < Constant.MAX_STRIKE_SIZE; i++) {
-            if (computer.get(i).equals(player.get(i))) {
-                strike++;
-            }
-        }
-        return strike;
+        return (int) IntStream.range(0, Constant.MAX_STRIKE_SIZE)
+                .filter(i -> Objects.equals(computer.get(i), player.get(i)))
+                .count();
     }
 
     public static int getBallCount(List<Integer> computer, List<Integer> player) {
-        int ball = 0;
-        for (int i = 0; i < Constant.MAX_STRIKE_SIZE; i++) {
-            if (!computer.get(i).equals(player.get(i)) && player.contains(computer.get(i))) {
-                ball++;
-            }
-        }
-        return ball;
+        return (int) IntStream.range(0, Constant.MAX_STRIKE_SIZE)
+                .filter(i -> player.contains(computer.get(i)))
+                .filter(i -> !Objects.equals(computer.get(i), player.get(i)))
+                .count();
     }
 }
