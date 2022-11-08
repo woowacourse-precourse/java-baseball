e1ade391e5790f9c9474eca46f89b58cc96c8e2f
DavinAn <blueapple1030@gmail.com>
2022-11-08 21:17:28 +0900

    결과 출력 메서드, printResult 추가
    스트라이크와 볼이 둘다 없을 경우 낫싱 출력.
    볼만 있을 경우 볼의 갯수 출력.
    스트라이크만 있을 경우 스트라이크의 갯수 출력.
    스트라이크와 볼이 둘다 있을 경우 각자의 갯수 출력.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -46,7 +46,20 @@ public class Application {
                 ball++;
             }
         }
+        printResult(strike, ball);
         return (strike == 3);
+    }
+
+    public static void printResult(int strike, int ball) {
+        if ((strike == 0) && (ball == 0)) {
+            System.out.println("낫싱");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
     }
 
 
