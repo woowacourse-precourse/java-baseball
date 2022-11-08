768ce2862d8cdaa309651c5ba31ec514c10c85a2
zjadofl0408 <zjadofl0408@gmail.com>
2022-11-08 21:40:24 +0900

    fix(all): 불필요한 로직 삭제 및 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -56,7 +56,7 @@ public class Application {
         ball = scoreMap.get("볼");
 
         if ((strike > 0) && (ball > 0)) {
-            System.out.printf("%d볼%d스트라이크%n",ball,strike);
+            System.out.printf("%d볼 %d스트라이크%n",ball,strike);
         } else if ((strike > 0) && (ball == 0)) {
             System.out.printf("%d스트라이크%n", strike);
             if (strike == 3) {
@@ -70,7 +70,7 @@ public class Application {
         return allStrike;
     }
 
-    public static boolean setGameSetting() {
+    public static boolean setGameSetting() throws IllegalArgumentException {
         boolean gameSetting;
         String gameChoice = Console.readLine();
         if (gameChoice.equals("1")) {
@@ -78,34 +78,61 @@ public class Application {
         } else if (gameChoice.equals("2")) {
             gameSetting = false;
         } else {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("1 또는 2만 입력해주세요.");
         }
         return gameSetting;
     }
 
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void startGame(List<Integer> computerNumber) throws IllegalArgumentException {
         String guessNumber;
-        List<Integer> splitNumberArray = new ArrayList<>();
-        List<Integer> computerNumber = pickComputerNumber();
+        boolean stopPoint = true;
+        boolean gamePoint;
+        List<Integer> splitUserNumber;
         Map<String, Integer> scoreMap;
-        boolean stopPoint, gamePoint;
-        stopPoint = true;
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        while (stopPoint) {
+        System.out.println(computerNumber);
+        while(stopPoint) {
             System.out.print("숫자를 입력해주세요 : ");
             guessNumber = Console.readLine();
             isInputFormat(guessNumber);
-
-            scoreMap = countScore(splitNumberArray, computerNumber);
-            gamePoint = printScore(scoreMap);
+            splitUserNumber = covertStringIntoInteger(guessNumber);
+            scoreMap = countScore(splitUserNumber, computerNumber);
+            gamePoint = printScore(scoreMap); //false
             if (gamePoint) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                stopPoint = setGameSetting();
-                computerNumber = pickComputerNumber();
+                computerNumber = stopOrRestart();
+            }
+            if (computerNumber.isEmpty()) {
+                stopPoint = false;
             }
         }
+    }
+
+    public static List<Integer> stopOrRestart() throws IllegalArgumentException {
+        List<Integer> computerNumberArray = new ArrayList<>();
+        boolean setPoint;
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        setPoint = setGameSetting();
+        if (setPoint) {
+            computerNumberArray = pickComputerNumber();
+            System.out.println(computerNumberArray);
+        }
+        return computerNumberArray;
+    }
+
+    public static List<Integer> covertStringIntoInteger(String input) {
+        List<Integer> splitNumberArray = Arrays.stream(input.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        return splitNumberArray;
+    }
+
+    public static void main(String[] args) {
+        // TODO: 프로그램 구현
+        List<Integer> computerNumber = pickComputerNumber();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        startGame(computerNumber);
+
     }
 }
