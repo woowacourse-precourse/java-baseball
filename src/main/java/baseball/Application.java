5c68e5efd81d911ac091cff834402b74a1783215
DavinAn <blueapple1030@gmail.com>
2022-11-08 21:09:41 +0900

    사용자 입력 읽기 메서드, playerNumber 추가
    사용자가 콘솔에 입력한 숫자를 읽고 List<Integer> 구조로 변환한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,14 +13,26 @@ public class Application {
         return computer;
     }
 
+    public static List<Integer> playerNumber(String input) {
+        List<Integer> player = new ArrayList<>();
+        int num;
+        for (char c : input.toCharArray()) {
+            num = Character.getNumericValue(c);
+            player.add(num);
+        }
+        return player;
+    }
+
 
     public static void main(String[] args) {
         String input;
         List<Integer> computer;
+        List<Integer> player;
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         computer = randomNumber();
         System.out.print("숫자를 입력해주세요 : ");
-
+        input = Console.readLine();
+        player = playerNumber(input);
     }
 }
