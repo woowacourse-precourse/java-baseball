2d4f84bdaf3ab69cd6306d46a3f96524117b6910
chanwonlee <clw8679@naver.com>
2022-11-08 12:14:57 +0900

    Test: 스트라이크, 볼 판별 테스트 추가

--- src/test/java/baseball/BaseballNumberTest.java
+++ src/test/java/baseball/BaseballNumberTest.java
@@ -1,10 +1,11 @@
 package baseball;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Nested;;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class BaseballNumberTest {
 
@@ -35,4 +36,68 @@ public class BaseballNumberTest {
 
         assertThat(hasSameNumber).isFalse();
     }
+
+    @Nested
+    class 스트라이크_개수_확인 {
+        @Test
+        void 스트라이크_X() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> inputNumber = List.of(3, 1, 2);
+            int count = BaseballNumber.countStrike(baseballNumber, inputNumber);
+            assertThat(count).isEqualTo(0);
+        }
+        @Test
+        void 스트라이크_1개() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> inputNumber = List.of(3, 2, 1);
+            int count = BaseballNumber.countStrike(baseballNumber, inputNumber);
+            assertThat(count).isEqualTo(1);
+        }
+        @Test
+        void 스트라이크_2개() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> inputNumber = List.of(1, 2, 4);
+            int count = BaseballNumber.countStrike(baseballNumber, inputNumber);
+            assertThat(count).isEqualTo(2);
+        }
+        @Test
+        void 스트라이크_3개() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> inputNumber = List.of(1, 2, 3);
+            int count = BaseballNumber.countStrike(baseballNumber, inputNumber);
+            assertThat(count).isEqualTo(3);
+        }
+    }
+    @Nested
+    class 볼_개수_확인 {
+        @Test
+        void 볼_X() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> inputNumber = List.of(4, 5, 6);
+            int count = BaseballNumber.countBall(baseballNumber, inputNumber);
+            assertThat(count).isEqualTo(0);
+        }
+        @Test
+        void 볼_1개() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> inputNumber = List.of(1, 3, 5);
+            int count = BaseballNumber.countBall(baseballNumber, inputNumber);
+            assertThat(count).isEqualTo(1);
+        }
+        @Test
+        void 볼_2개() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> inputNumber = List.of(2, 1, 3);
+            int count = BaseballNumber.countBall(baseballNumber, inputNumber);
+            assertThat(count).isEqualTo(2);
+        }
+        @Test
+        void 볼_3개() {
+            List<Integer> baseballNumber = List.of(1, 2, 3);
+            List<Integer> inputNumber = List.of(3, 1, 2);
+            int count = BaseballNumber.countBall(baseballNumber, inputNumber);
+            assertThat(count).isEqualTo(3);
+        }
+    }
 }
+
