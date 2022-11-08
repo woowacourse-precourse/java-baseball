bdce2b25a925d0ffe5af93c23b131bced0abca6c
yewon <yewon.ux@gmail.com>
2022-11-08 12:14:44 +0900

    feat: 게임 진행 while loop

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,40 +9,44 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        GetUserNumber userInput = new GetUserNumber();
-        String userRandNumber = userInput.inputUserNumber();
 
         GenerateRandNumber generateNumber = new GenerateRandNumber();
         List<Integer> correctRandNumber = generateNumber.arrayNum();
 
-        // count STRIKE
-        int countStrike = 0;
-        for(int i=0; i<correctRandNumber.size(); i++) {
-            int eachUserNumber = userRandNumber.charAt(i) - '0';
-            if (eachUserNumber == correctRandNumber.get(i)) {
-                countStrike ++;
+        boolean gameStatus = true;
+        while (gameStatus) {
+
+            GetUserNumber userInput = new GetUserNumber();
+            String userRandNumber = userInput.inputUserNumber();
+
+            // count STRIKE
+            int countStrike = 0;
+            for (int i = 0; i < correctRandNumber.size(); i++) {
+                int eachUserNumber = userRandNumber.charAt(i) - '0';
+                if (eachUserNumber == correctRandNumber.get(i)) {
+                    countStrike++;
+                }
             }
-        }
 
-        //count BALL
-        int countBall = 0;
-        for(int i=0; i<correctRandNumber.size(); i++) {
-            int eachUserNumber = userRandNumber.charAt(i) - '0';
-            if (eachUserNumber == correctRandNumber.get(i)) {
-                countBall = countBall;
-            } else if (correctRandNumber.contains(eachUserNumber)) {
-                countBall ++;
+            //count BALL
+            int countBall = 0;
+            for (int i = 0; i < correctRandNumber.size(); i++) {
+                int eachUserNumber = userRandNumber.charAt(i) - '0';
+                if (eachUserNumber == correctRandNumber.get(i)) {
+                    countBall = countBall;
+                } else if (correctRandNumber.contains(eachUserNumber)) {
+                    countBall++;
+                }
             }
-        }
 
-        //결과 출력
-        System.out.println(countBall + "볼 " + countStrike + "스트라이크");
+            //결과 출력
+            System.out.println(countBall + "볼 " + countStrike + "스트라이크");
 
-        //게임 진행 판단
-        int gameStatus = 1;
-        if (countStrike == 3) {
-            System.out.println("3개의 숫자를 모두 맞히였습니다! 게임 종료");
-            gameStatus = 0;
+            //게임 진행 판단
+            if (countStrike == 3) {
+                System.out.println("3개의 숫자를 모두 맞히였습니다! 게임 종료");
+                gameStatus = false;
+            }
         }
     }
 }
