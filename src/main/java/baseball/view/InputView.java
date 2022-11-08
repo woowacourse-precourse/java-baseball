3096a34991ece1942b06846c4c6c9a247a4b7230
gomljo <gomljo0307@naver.com>
2022-11-08 19:33:14 +0900

    feat(InputViewTest.java): 게임 시작 후 플레이어(사용자)에게 게임을 재시작할 것인지 종료할 것인지 입력하라는 메시지를 출력하는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,12 +3,16 @@ package baseball.view;
 public class InputView {
     private static final String GAME_START_MESSAGE="숫자 야구 게임을 시작합니다.";
     private static final String REQUEST_ENTER_INPUT="숫자를 입력해주세요 : ";
+    private static final String REQUEST_SELECT_NEW_GAME_OR_END="게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     public void noticeGameStart(){
         System.out.println(GAME_START_MESSAGE);
     }
     public void requestEnteringNumbers(){
         System.out.print(REQUEST_ENTER_INPUT);
+    }
+    public void requestMoreGame(){
+        System.out.println(REQUEST_SELECT_NEW_GAME_OR_END);
     }
 
 }
