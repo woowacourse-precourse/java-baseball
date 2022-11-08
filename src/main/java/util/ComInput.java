80fb983c79a6169fb6f57f2beee523eb7a1b47ad
WonSeok-dd <cluejws@gmail.com>
2022-11-08 19:31:19 +0900

    fix(ComInput): 올바른 3자리수 생성인지 판단 기능 - 서로 다른 숫자?

--- src/main/java/util/ComInput.java
+++ src/main/java/util/ComInput.java
@@ -5,19 +5,31 @@ import java.util.List;
 public class ComInput {
     public static boolean isValidComInput(List<Integer> numbers) {
 
-        return isDiffer(numbers);
+        return isNotDuplicate(numbers);
     }
 
-    private static boolean isDiffer(List<Integer> numbers) {
+    private static boolean isNotDuplicate(List<Integer> numbers) {
 
-        for (int i=0; i<numbers.size()-1; i++){
-            int curValue = numbers.get(i);
-            int nextValue = numbers.get(i+1);
-            if (curValue == nextValue){
+        for (int i= 0; i<numbers.size(); i++){
+            if (isSame(i, numbers)){
                 return false;
             }
         }
 
         return true;
     }
+
+    private static boolean isSame(int curIndex, List<Integer> numbers) {
+
+        for (int i=0; i<numbers.size(); i++){
+            int curValue = numbers.get(curIndex);
+            int nextValue = numbers.get(i);
+            if (i != curIndex && curValue == nextValue){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 }
