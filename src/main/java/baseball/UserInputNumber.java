99bcd72215e00e2ec98e8610013d0a597e92df2d
sujin0607 <goweg0607@gmail.com>
2022-11-08 14:28:53 +0900

    feat: 사용자 입력 클래스

--- src/main/java/baseball/UserInputNumber.java
+++ src/main/java/baseball/UserInputNumber.java
@@ -1,2 +1,37 @@
-package baseball;public class UserInputNumber {
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserInputNumber {
+    private List<Integer> user;
+
+    public List<Integer> inputNumbers(String userNum) {
+        user = new ArrayList<>();
+        String[] nums = userNum.split("");
+        lengthInCorrectException(nums.length);
+
+        for (String num : nums) {
+            int numInt = isNumberic(num);
+            isContainNumberException(user, numInt);
+            user.add(numInt);
+        }
+        return user;
+    }
+
+    public void lengthInCorrectException(int length) {
+        if (length != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void isContainNumberException(List<Integer> nums, int num) {
+        if (nums.contains(num)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int isNumberic(String num) throws IllegalArgumentException {
+        return Integer.parseInt(num);
+    }
 }
