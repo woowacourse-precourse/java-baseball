b0d027ef04b379d7339325c83da88b207120b754
sangminee <lsmlee99@gmail.com>
2022-11-08 18:12:46 +0900

    Feat : 사용자 숫자 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,15 +3,37 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
 
-    static List<Integer> computer;
+    static BufferedReader br;
+    static boolean result;
 
-    public static void main(String[] args) {
+    static List<Integer> computer;
+    static List<Integer> numbers;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
         selectNumber();
+        while(!result){
+            inputNumber(br);
+        }
+    }
+
+    private static void inputNumber(BufferedReader br) throws IOException {
+        System.out.print("숫자를 입력해 주세요 : ");
+        String[] temp = br.readLine().split("");
+        if(temp.length >= 4){
+            throw new IllegalArgumentException();
+        }
+        numbers = new ArrayList<>();
+        for(int i=0; i<3;i++){
+            numbers.add(Integer.parseInt(temp[i]));
+        }
     }
 
     private static void selectNumber() {
