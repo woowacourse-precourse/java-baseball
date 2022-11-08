910a364c5096165118da2133dc3a64ca74edbe27
Kim-AYoung <onionlily123@naver.com>
2022-11-08 21:54:43 +0900

    refactor(OutputImpl): 낫싱 출력시, 2번 개행을 1번으로 수정

--- src/main/java/baseball/io/OutputImpl.java
+++ src/main/java/baseball/io/OutputImpl.java
@@ -10,10 +10,18 @@ public class OutputImpl implements Output {
 
     @Override
     public void ballCount(BallCount bc) {
-        if (bc.getBall() == 0 && bc.getStrike() == 0) System.out.println("낫싱");
-        if (bc.getBall() > 0) System.out.print(bc.getBall() + "볼 ");
-        if (bc.getStrike() > 0) System.out.println(bc.getStrike() + "스트라이크");
-        else System.out.print("\n");
+        if (bc.getBall() == 0 && bc.getStrike() == 0) {
+            System.out.println("낫싱");
+        } else {
+            if (bc.getBall() > 0) {
+                System.out.print(bc.getBall() + "볼 ");
+            }
+            if (bc.getStrike() > 0) {
+                System.out.println(bc.getStrike() + "스트라이크");
+            } else {
+                System.out.print("\n");
+            }
+        }
     }
 
     @Override
