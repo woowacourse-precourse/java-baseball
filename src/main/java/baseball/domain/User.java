e7417bb9b5c6eda53e7f05db867f1b971395bb6b
ys200209 <lys200209@naver.com>
2022-11-08 17:44:18 +0900

    feat(User.java): 입력한 수를 검증하라고 명령하기
    UserValidator 검증 객체에게 내부 값을 검증하라고 명령함

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -1,5 +1,6 @@
 package baseball.domain;
 
+import baseball.validate.UserValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
@@ -7,9 +8,14 @@ import camp.nextstep.edu.missionutils.Console;
  */
 public class User {
     private String number;
+    private UserValidator validator = new UserValidator();
 
     public void inputNumber() {
         this.number = Console.readLine();
+    }
+
+    public void validateNumber() {
+        validator.validateNumber(number);
     }
 
 }
