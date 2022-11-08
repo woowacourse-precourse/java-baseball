3c27d23e2ff2bccc9169cc96b2abd40930ce2196
sschan99 <seungchan1219@gmail.com>
2022-11-08 22:35:09 +0900

    test(ApplicationTest): write the test code for 'isValid' method
    Added Tests:
    isValid_유효한_문자열이_입력
    isValid_숫자가_3개가_아님
    isValid_숫자가_아닌_문자가_문자열에_포함
    isValid_숫자에_0이_포함
    isValid_중복되는_숫자가_존재

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -28,6 +28,44 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void isValid_유효한_문자열이_입력() {
+        String input = "792";
+        boolean result = Application.isValid(input);
+        assertThat(result).isEqualTo(true);
+    }
+
+    @Test
+    void isValid_숫자가_3개가_아님() {
+        String input1 = "1234";
+        String input2 = "67";
+        boolean result1 = Application.isValid(input1);
+        boolean result2 = Application.isValid(input2);
+        assertThat(result1).isEqualTo(false);
+        assertThat(result2).isEqualTo(false);
+    }
+
+    @Test
+    void isValid_숫자가_아닌_문자가_문자열에_포함() {
+        String input = "1e3";
+        boolean result = Application.isValid(input);
+        assertThat(result).isEqualTo(false);
+    }
+
+    @Test
+    void isValid_숫자에_0이_포함() {
+        String input = "830";
+        boolean result = Application.isValid(input);
+        assertThat(result).isEqualTo(false);
+    }
+
+    @Test
+    void isValid_중복되는_숫자가_존재() {
+        String input = "363";
+        boolean result = Application.isValid(input);
+        assertThat(result).isEqualTo(false);
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
