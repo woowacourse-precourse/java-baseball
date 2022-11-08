bcbdf4df99c0d43e395d3a2a426e8168cfadcd65
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-08 19:53:06 +0900

    feat(feature): 어플리케이션 시작 기능 구현
    프로그램의 시작점(main 함수)에 GameManager의 runGame() 메서드를 호출하는 코드를 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,9 @@
 package baseball;
 
+import baseball.utility.GameManager;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameManager.getGameManager().runGame();
     }
 }
