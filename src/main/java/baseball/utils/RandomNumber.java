3f53a9beca901737047c288ca0163ac2433b257e
simba <shim9597@gmail.com>
2022-11-08 18:36:53 +0900

    refactor: randomNumber 생성 코드 수정
    기존 코드가 테스트에서 10초 이상 걸려 실패함. while 문을 쓰던 코드에서 for 문을 사용하는 코드로 변경. 오류 내용을 보니 Randoms.pickNumberInRange() 에서 근본 문제 발생.
    해당 기능이 while 문 안에서 동작 할 경우 테스트에서 오류를 발생시키는 것으로 추정.

--- src/main/java/baseball/utils/RandomNumber.java
+++ src/main/java/baseball/utils/RandomNumber.java
@@ -3,27 +3,17 @@ package baseball.utils;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class RandomNumber {
 
     public int[] getRandomNumber() {
-        int[] numbers = new int[3];
-
-        for (int i =0; i < 3; i ++) {
-            numbers[i] = newRandomNumber().get(i);
-        }
-        return numbers;
-    }
-
-    private List<Integer> newRandomNumber() {
-        List<Integer> computerNumber = new ArrayList<>();
-        while (computerNumber.size() < 3) {
-            int randomNumber = pickNumberInRange(1, 9);
-            if (!computerNumber.contains(randomNumber)) {
-                computerNumber.add(randomNumber);
+        int[] computerNumber = new int[3];
+        for (int i = 0; i < 3; i ++) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!Arrays.asList(computerNumber).contains(computerNumber[i])) {
+                computerNumber[i] = randomNumber;
             }
         }
         return computerNumber;
