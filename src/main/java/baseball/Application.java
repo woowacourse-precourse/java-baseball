c5e1ae155a0ca08af07d4a6350cbfd8d1bba1399
show2891 <show2891@gmail.com>
2022-11-08 23:16:55 +0900

    게임 재시작 입력값에 대한 검증_구현 완료, 게임 재시작 입력값 기준으로 반복/종료_수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,24 +9,43 @@ public class Application {
         return userInput;
     }
 
-    static Integer getRetryNumberByUser() {
-        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+    static String getRetryNumberByUser() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         Scanner input = new Scanner(System.in);
-        Integer userInput = input.nextInt();
+        String userInput = input.nextLine();
         return userInput;
     }
+
+    static String verifyRetryNumber(String retryNumber) {
+        String numberToVerify = retryNumber;
+
+        try {
+            if (!numberToVerify.equals("1") && !numberToVerify.equals("2")) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getClass().getName());
+        }
+
+        return numberToVerify;
+    }
+
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
             while (true) {
-                System.out.print("숫자 야구 게임을 시작합니다.\n");
+                System.out.println("숫자 야구 게임을 시작합니다.");
                 String userThreeNumber = getThreeNumberByUser();
 
 
-                Integer userRetryNumber = getRetryNumberByUser();
-                if (userRetryNumber == 1) {
+                String userRetryNumber = getRetryNumberByUser();
+                userRetryNumber = verifyRetryNumber(userRetryNumber);
+
+                if (userRetryNumber.equals("1")) {
                     continue;
-                } else if (userRetryNumber == 2) {
+                } else if (userRetryNumber.equals("2")){
+                    break;
+                } else {
                     break;
                 }
 
