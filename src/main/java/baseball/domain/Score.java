0ca9f361bfad50e7ebc34bf1b743aae80cdff850
yuntaeLee <leeyt1201@gmail.com>
2022-11-08 13:47:25 +0900

    feat: Override method of Score toString

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -1,5 +1,7 @@
 package baseball.domain;
 
+import static baseball.vo.GameInterfaceMsg.*;
+
 public class Score {
     private int strike;
     private int ball;
@@ -23,5 +25,23 @@ public class Score {
 
     public void increaseBall() {
         this.ball += 1;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        if (getBall() != 0) {
+            sb.append(getBall()).append(BALL.getMessage()).append(' ');
+        }
+
+        if (getStrike() != 0) {
+            sb.append(getStrike()).append(STRIKE.getMessage());
+        }
+
+        if (getStrike() == 0 && getBall() == 0) {
+            sb.append(NOTHING.getMessage());
+        }
+
+        return sb.toString();
     }
 }
