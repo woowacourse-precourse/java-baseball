62fe40d8f9abcfeddcad92140ab70b87b1b88f24
olrlobt <317tmdgjs@naver.com>
2022-11-08 19:41:44 +0900

    test: 상대방 숫자에 올바른 값이 들어가는지 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,9 @@
 package baseball;
 
+import baseball.model.Goal;
 import camp.nextstep.edu.missionutils.test.NsTest;
+import java.util.Collections;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -50,6 +53,29 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("333"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 상대방_숫자_길이() {
+        Goal goal = new Goal();
+        goal.setGoalNumber();
+        List<Integer> goalNumber = goal.getGoalNumber();
+
+        assertThat(goalNumber.size()).isEqualTo(3);
+    }
+
+    @Test
+    void 상대방_숫자_중복없는지() {
+        Goal goal = new Goal();
+        goal.setGoalNumber();
+        List<Integer> goalNumber = goal.getGoalNumber();
+
+        int i=0;
+        for (Integer goalNumberUnit : goalNumber) {
+            i += Collections.frequency(goalNumber,goalNumberUnit);
+        }
+
+        assertThat(goalNumber.size()).isEqualTo(3);
     }
 
     @Override
