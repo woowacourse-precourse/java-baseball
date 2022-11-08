89ac119dd43841c48abfa52033d998b2ffa3cfa0
dooboocookie <bkh3030@gmail.com>
2022-11-08 16:31:59 +0900

    feat: 공백 입력 예외 처리 추가

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -4,14 +4,24 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Input {
 
+    private static final String ERROR_MESSAGE_BLANK = "공백은 입력할 수 없습니다.";
+
     public static String readString() {
         String inputString;
         inputString = Console.readLine();
-        return inputString;
+        inputString = inputString.trim();
+        validateBlank(inputString);
+        return inputString.trim();
     }
 
     public static int readInt() {
         return Integer.parseInt(readString());
+    }
+
+    private static void validateBlank(String input) {
+        if (input.equals("")) {
+            throw new IllegalArgumentException(ERROR_MESSAGE_BLANK);
+        }
     }
 
 }
