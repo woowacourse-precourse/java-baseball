ac67f1c7cc09a60813c62fb9928bb6a29347bb8c
Gibeom Nam <rlqja0523@naver.com>
2022-11-08 22:56:27 +0900

    feat: GameService 생성

---
+++ src/main/java/baseball/game/GameService.java
@@ -0,0 +1,11 @@
+package baseball.game;
+
+import baseball.entity.Computer;
+import baseball.entity.User;
+import baseball.number.NumberJudgment;
+
+public class GameService {
+    private Computer computer = new Computer();
+    private User user = new User();
+    private NumberJudgment numberJudgment = new NumberJudgment();
+}
