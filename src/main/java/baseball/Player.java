34aa55ef622a70bc6d2f99da9b726e3e4eb3b29a
sangho-jeon <jeonsangho1587@gmail.com>
2022-11-08 21:00:18 +0900

    fix       : fix ball count algorithm

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -6,6 +6,7 @@ import baseball.Util.RandomNumberGenerator;
 import baseball.Util.InputHandler;
 
 import java.util.List;
+import java.util.Objects;
 
 public class Player {
     private List<Integer> computerNumber;
@@ -15,7 +16,7 @@ public class Player {
 
     public Player() {
         this.computerNumber = RandomNumberGenerator.generateNumbers();
-        System.out.println(computerNumber);
+//        System.out.println(computerNumber);
     }
 
     public List<Integer> getInput() {
@@ -26,33 +27,19 @@ public class Player {
         GameResultHandler gameResultHandler = new GameResultHandler();
         System.out.println(inputIndicator);
         List<Integer> userThrow = getInput();
-        GameState state = gameResultHandler.out(getResult(userThrow, computerNumber));
-        return state;
+        return gameResultHandler.out(getResult(userThrow, computerNumber));
     }
 
     private CountResult getResult(List<Integer> userNumber, List<Integer> computerNumber) {
-        CountResult count = new CountResult(strikeCount(userNumber, computerNumber), ballCount(userNumber, computerNumber));
-        return count;
-    }
-
-    private int ballCount(List<Integer> userNumber, List<Integer> computerNumber) {
-        int count = 0;
-        for (int balls :
-                userNumber) {
-            if (computerNumber.contains(balls))
-                count++;
-        }
-        return count;
-    }
-
-    private int strikeCount(List<Integer> userNumber, List<Integer> computerNumber) {
-        int count = 0;
+        int ballCount = 0;
+        int strikeCount = 0;
         for (int i = 0; i < userNumber.size(); i++) {
-            if(userNumber.get(i) == computerNumber.get(i))
-                count++;
+            if (Objects.equals(userNumber.get(i), computerNumber.get(i))) {
+                strikeCount++;
+            } else if (computerNumber.contains(userNumber.get(i))) {
+                ballCount++;
+            }
         }
-        return count;
+        return new CountResult(strikeCount, ballCount);
     }
-
-
 }
--- src/test/java/FunctionTest/UserInputTest.java
+++ src/test/java/FunctionTest/UserInputTest.java
@@ -2,12 +2,14 @@ package FunctionTest;
 
 import baseball.Util.InputHandler;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 
 public class UserInputTest {
     @Test
-    void 인풋테스트(){
+    void 인풋테스트() {
         assertThat(InputHandler.getUserInput()).isEqualTo("r");
-    }
 
+    }
 }
