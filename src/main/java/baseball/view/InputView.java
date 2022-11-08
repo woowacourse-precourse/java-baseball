ed50563f6337ffe63acaf814520e5d62f57fe158
baekeunsun <eunsun5460@gmail.com>
2022-11-08 17:25:24 +0900

    chore(view/InputView): 사용하지 않는 import 삭제

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,8 +2,6 @@ package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
-
 public class InputView {
     public static String startInput() {
         System.out.print("숫자를 입력해주세요 : ");
