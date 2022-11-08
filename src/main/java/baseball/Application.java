2a7dbd70fd996b0d43f7344d8795199d656c9d6b
yoon <beatus96@naver.com>
2022-11-08 10:57:33 +0900

    feat(application) : 컴퓨터 수 생성 기능 추가
    필요한 자릿수와 임의의 수 범위를 상수화하고,
    상수를 이용하여, 1-9 사이의 / 서로 다른 수를 가진 / 3자리 수의 컴퓨터 수를 생성했다.
    여기서 컴퓨터 수는 플레이어가 맞춰야 할 숫자이다. 컴퓨터 수는 List<Integer> 타입으로 반환한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
-}
+
+        //1. 컴퓨터 수 생성하기
+        private static final int DIGITS = 3;
+        private static final int RANDOM_MIN = 1;
+        private static final int RANDOM_MAX = 9;
+
+        public static List<Integer> createComputerNumber () {
+            List<Integer> computerNumber = new ArrayList<>();
+            while (computerNumber.size() < DIGITS) {
+                int randomNumber = Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
+                if (!computerNumber.contains(randomNumber)) {
+                    computerNumber.add(randomNumber);
+                }
+            }
+            return computerNumber;
+        }
+
+    }
