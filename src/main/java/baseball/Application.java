428dc6df7139002bbfa1e9058b23bfd495ceb091
kimJaeHyeon <kmr2644@gmail.com>
2022-11-08 16:00:21 +0900

    fix: 게임시작 문자열 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         BaseballGame baseballGame = new BaseballGame(new View(), new BaseballService(new ComputerBaseball()));
-        System.out.println("숫자 야구 게임을 시작합니다");
+        System.out.println("숫자 야구 게임을 시작합니다.");
         baseballGame.start();
     }
 }
