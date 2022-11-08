a39eaa7c3da6d5cdacf456c8e0d1691640c23670
gyumin159 <gyumin159@naver.com>
2022-11-08 11:28:09 +0900

    feat : 플레이어가 올바른 입력을 했는지 확인하는 메소드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,8 +3,8 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.regex.Pattern;
 
 public class Application {
     public static List<Integer> generateThreeDifferentNum() {
@@ -16,6 +16,22 @@ public class Application {
             }
         }
         return computer;
+    }
+
+    /**
+     *  플레이어의 입력값이 1~9사이 서로다른 3가지 숫자인지 확인
+     *
+     * @param playerInput Console.readLine()에서 받은 String 값
+     * */
+    public static void isDifferentThreeNum(String playerInput){
+        List<String> playerInputArray = Arrays.asList(playerInput.split(""));
+        Set<String> delSameNum = new HashSet<String>(playerInputArray);
+        boolean isOneToNineAndSizeThree = Pattern.matches("^[1-9]{3}$", playerInput);
+        boolean isNoOverlap = playerInputArray.size() == delSameNum.size();
+
+        if (!(isOneToNineAndSizeThree && isNoOverlap)) {
+            throw new IllegalArgumentException();
+        }
     }
 
 
