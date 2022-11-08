3462f49f3392c8d5f2739dde65b626825ea76b2f
juno-junho <ssmm0205@naver.com>
2022-11-08 18:37:20 +0900

    feat(model): create method in StatusCode enum
    StatusCode enum에 생성자와 get 메서드 생성

--- src/main/java/baseball/model/StatusCode.java
+++ src/main/java/baseball/model/StatusCode.java
@@ -1,4 +1,16 @@
 package baseball.model;
 
 public enum StatusCode {
+    RESTART("1"),
+    QUIT("2");
+
+    private final String code;
+
+    StatusCode(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
 }
