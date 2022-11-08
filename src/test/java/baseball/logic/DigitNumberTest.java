7895beeb97f02c6f6d48063fca31461f48bfb700
DaWoon Lee <qkgk61@gmail.com>
2022-11-08 22:14:22 +0900

    Test : DigitNumber 클래스의 convertNumberToMap 테스트

--- src/test/java/baseball/logic/DigitNumberTest.java
+++ src/test/java/baseball/logic/DigitNumberTest.java
@@ -2,6 +2,7 @@ package baseball.logic;
 
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +22,19 @@ public class DigitNumberTest {
             }
 
             assert (checker.size() == 3);
+        }
+    }
+
+    @Test
+    void convertNumberToMap_인풋값을_맵으로_변환() {
+        DigitNumber digitNumber = DigitNumber.getInstance(3);
+
+        String userInput = String.valueOf((int) (Math.random() * 10_000) + 1);
+        Map<Integer, Integer> userNumber = digitNumber.convertNumberToMap(userInput);
+
+        for (int len = userInput.length(), i = 0; i < len; i++) {
+            int current = userInput.charAt(i) - '0';
+            assert (userNumber.get(current) == i);
         }
     }
 
