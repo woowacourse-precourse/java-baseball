e9c39669cd6855cf2d89438e5fedd6f85b8af8db
MinSungKim <timkim01@naver.com>
2022-11-08 10:33:08 +0900

    test: 사용자 입력값이 숫자인지 판단 테스트 작성

--- src/test/java/baseball/utils/UserInputTest.java
+++ src/test/java/baseball/utils/UserInputTest.java
@@ -1,4 +1,24 @@
+package baseball.utils;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserInputTest {
-  
+
+    @Test
+    void 사용자_입력값이_숫자인지_여부_판단(){
+        String userInput = "abc";
+        InputStream in = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(in);
+
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            UserInput.setUserInput();
+        });
+
+        assertEquals("숫자만 입력해주세요.", exception.getMessage());
+    }
 }
