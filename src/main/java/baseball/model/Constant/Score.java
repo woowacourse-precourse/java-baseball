2ff163da9f95a6370c7b46c15fbf8ce72e1e05eb
hwanee__ <fkfkthth@naver.com>
2022-11-08 20:32:09 +0900

    feat: 점수에 대한 상수 생성
    스트라이크/볼/낫싱에 대한 상수

---
+++ src/main/java/baseball/model/Constant/Score.java
@@ -0,0 +1,17 @@
+package baseball.model.Constant;
+
+public enum Score {
+    STRIKE("스트라이크"),
+    BALL("볼"),
+    NOTHING("낫싱");
+
+    private final String value;
+
+    Score(final String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return this.value;
+    }
+}
