ac0175641f971e22a6f8d77428411b5b7dcaa7ee
20mini <dlqslek10@gmail.com>
2022-11-08 21:56:34 +0900

    feat(Computer.java): 플레이어가 입력한 숫자에 대한 결과 출력 기능 추가
    플레이어가 입력한 숫자에 대한 결과 출력 입력 기능 추가
    -Computer class에 strikeCount 추가
    -Computer class에 ballCount 추가
    -Computer class에 countStrike() 생성
    -Computer class에 countBall() 생성
    -Computer class에 setStrikeCount() 생성
    -Computer class에 setBallCount() 생성
    -Computer class에 printResult() 생성

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 
 public class Computer {
     private ArrayList<Integer> computerNumberList; // 컴퓨터 숫자(서로 다른 임의의 숫자 세 개)를 담고 있는 Arraylist
+    private int strikeCount; // 플레이어가 입력한 숫자의 대한 결과 중 스트라이크 개수
+    private int ballCount; // 플레이어가 입력한 숫자의 대한 결과 중 볼 개수
 
     public Computer() // 객체 생성 시, 자동으로 컴퓨터 숫자가 정해짐
     {
@@ -21,5 +23,52 @@ public class Computer {
             }
         }
         this.computerNumberList = numberList;
+    }
+
+    public int countStrike(ArrayList<Integer> input) // 스트라이크 개수 파악
+    {
+        int cnt = 0;
+        for (int i = 0; i < 3; i++) {
+            if (input.get(i) == computerNumberList.get(i)) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public int countBall(ArrayList<Integer> input) // 볼 개수 파악
+    {
+        int cnt = 0;
+        for (int i = 0; i < 3; i++) {
+            if (input.contains(computerNumberList.get(i)) && input.get(i) != computerNumberList.get(i)) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+    public void setStrikeCount(int strikeCount) // 스트라이크 개수 설정
+    {
+        this.strikeCount = strikeCount;
+    }
+
+    public void setBallCount(int ballCount) // 볼 개수 설정
+    {
+        this.ballCount = ballCount;
+    }
+
+    public void printResult() // 플레이어가 입력한 숫자에 대한 결과 출력
+    {
+        String result="";
+        if(ballCount !=0){
+            result+=Integer.toString(ballCount)+"볼 ";
+        }
+        if(strikeCount!=0){
+            result+=Integer.toString(strikeCount)+"스트라이크";
+        }
+        else if(ballCount ==0 && strikeCount==0){
+            result+="낫싱";
+        }
+        System.out.println(result);
     }
 }
