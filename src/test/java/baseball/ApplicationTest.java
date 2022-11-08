9af8c993f6a254d20b3e48a14a0cb535963328e8
coldbucks <met.hsw@gmail.com>
2022-11-08 20:50:01 +0900

    feat: 기능 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -14,28 +14,39 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class ApplicationTest extends NsTest {
 
     @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
+    public void 랜덤숫자테스트() throws Exception {
+        //given
+        RandomNumber randomNumber = new RandomNumber();
+        //when
+        List<Integer> randomTest = randomNumber.create();
+        //then
+        assertThat(3).isEqualTo(randomTest.size());
     }
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+        @Test
+        void 게임종료_후_재시작 () {
+            assertRandomNumberInRangeTest(
+                    () -> {
+                        run("246", "135", "1", "597", "589", "2");
+                        assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                    },
+                    1, 3, 5, 5, 8, 9
+            );
+        }
+
+        @Test
+        void 예외_테스트 () {
+            assertSimpleTest(() ->
+                    assertThatThrownBy(() -> runException("1234"))
+                            .isInstanceOf(IllegalArgumentException.class)
+            );
+        }
+
+        @Override
+        public void runMain () {
+            Application.main(new String[]{});
+        }
     }
 
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
-}
 
 
