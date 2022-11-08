d5f8bc2cc24fe31ee7b53632393fa6410da08f1f
DDing77 <devseo2021@gmail.com>
2022-11-08 21:52:06 +0900

    refactor($message) : 중복 메세지 삭제

--- src/main/java/baseball/common/GameMessage.java
+++ src/main/java/baseball/common/GameMessage.java
@@ -8,11 +8,7 @@ public enum GameMessage {
     NOTING("낫싱"),
 
     GAME_OVER("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
-    QUESTION_CONTINUE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
-    EXCEPTION_NOT_INPUT3("입력한 수의 갯수가 옳지 않습니다."),
-    EXCEPTION_SAME_INPUT("중복된 수가 존재합니다."),
-    EXCEPTION_NOT_SCOPE_INPUT("수의 범위가 알맞지 않습니다."),
-    EXCEPTION_NOT_RESTART_INPUT("1 혹은 2를 입력해야합니다.");
+    QUESTION_CONTINUE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
     private final String value;
 
