156c5b991fd90d577db2a6222aa9127b709dd488
jay <jaewhan232@naver.com>
2022-11-08 20:52:01 +0900

    Implement : getAnswerNumber() 메소드 구현 -> Randoms 사용하여 임의의 정답 숫자열 생성

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class BaseballGame {
 
     private static BaseballGameInputFilter inputFilter = BaseballGameInputFilter.getInstance();
@@ -7,6 +12,18 @@ public class BaseballGame {
 
     public String getInputNumbers(){
         return inputFilter.readNumbers();
+    private String getAnswerNumber() {
+        List<Integer> answerNumbers = new ArrayList<>();
+        String answerString = "";
+
+        while (answerNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if (!answerNumbers.contains(randomNumber)) {
+                answerString += String.valueOf(randomNumber);
+                answerNumbers.add(randomNumber);
+            }
+        }
+        return answerString;
     }
 
 }
