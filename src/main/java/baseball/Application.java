01d06964b6117d0de740dd2f46e1a600f2f5961a
hongjinwon <herenever@naver.com>
2022-11-08 20:42:30 +0900

    feat(Application): Application main 구현
    Application 의 main() 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import baseball.controller.BaseballController;
+import baseball.model.Computer;
+import baseball.model.Flow;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballController baseballController = new BaseballController(new InputView(), new OutputView(), new Computer(), new Flow());
+        baseballController.runGame();
     }
 }
