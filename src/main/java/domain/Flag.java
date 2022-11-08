4d505c8b10b611d5022cb3d72c4c59c579cbebc9
1eejisoo <jisu991012@gmail.com>
2022-11-08 17:49:08 +0900

    Feat: 종료 값 유효성 검증 기능 추가 - 1 또는 2가 되어야 한다.

---
+++ src/main/java/domain/Flag.java
@@ -0,0 +1,16 @@
+package domain;
+
+public class Flag {
+    private final String flag;
+
+    public Flag(String flag) {
+        this.flag = flag;
+        validateCorrectFlag(flag);
+    }
+
+    public void validateCorrectFlag(String flag) {
+        if (!(flag.equals("1") || flag.equals("2"))) {
+            throw new IllegalArgumentException("재시작 / 종료 값은 1 또는 2가 되어야 합니다.");
+        }
+    }
+}
