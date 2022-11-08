431810550f116c418353592282ada93ab038dfb8
baektmdduq <97cowtop@naver.com>
2022-11-08 14:24:03 +0900

    feat: 컴퓨터가 임의의 서로 다른 숫자 3개를 정하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,35 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
-    public static void main(String[] args) {
-        Application app = new Application();
-        app.printStartGame();
+  static final int digit = 3;
+  static final int startInclusive = 1;
+  static final int endInclusive = 9;
+
+  public static void main(String[] args) {
+    Application numberBaseBallGame = new Application();
+    numberBaseBallGame.startGame();
+  }
+
+  public void startGame() {
+    System.out.println("숫자 야구 게임을 시작합니다.");
+    List<Integer> computer = initComputer();
+  }
+
+  public List<Integer> initComputer() {
+    List<Integer> computer = new ArrayList<>();
+
+    while (computer.size() < digit) {
+      int randomNumber = Randoms.pickNumberInRange(startInclusive, endInclusive);
+      if (!computer.contains(randomNumber)) {
+        computer.add(randomNumber);
+      }
     }
 
-    public void printStartGame(){
-        System.out.println("숫자 야구 게임을 시작합니다.");
-    }
+    return computer;
+  }
 }
