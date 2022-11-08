45d5b5dbc7ae5dbdd774f0bd944c71715d78a6f9
footdev <ekclstkfka5@gmail.com>
2022-11-08 17:55:06 +0900

    test: ThreeNumberTest 테스트 코드 수정
    약간의 코드 수정

--- src/test/java/baseball/service/ThreeNumberTest.java
+++ src/test/java/baseball/service/ThreeNumberTest.java
@@ -3,13 +3,10 @@ package baseball.service;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class ThreeNumberTest {
 
