7f2bc4bfa0c8011d66cccf2a6cadf4f80fbf5f1e
Frog-Slayer <Frog-Slayer@github.com>
2022-11-08 21:29:00 +0900

    test : 스트라이크 카운트 테스트

---
+++ src/test/java/baseball/model/GameTest.java
@@ -0,0 +1,24 @@
+package baseball.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    public void 스트라이크_카운트_테스트(){
+        Game game = new Game();
+        List<Integer> computerNumeralList = Arrays.asList(1, 2, 4);
+        List<Integer> userNumeralList = Arrays.asList(1, 2, 3);
+        game.countStrike(computerNumeralList, userNumeralList);
+        assertThat(game.getStrikeCount()).isEqualTo(2);
+
+    }
+
+}
