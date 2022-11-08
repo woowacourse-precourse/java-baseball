161120a863b4cdc5881b07d3bea94492bf447c8b
shoeone96 <dlwnddnjs96@naver.com>
2022-11-08 17:43:45 +0900

    test(GameTest): checkingBall  테스트 성공

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -2,6 +2,8 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
@@ -28,6 +30,15 @@ class GameTest {
 
     @Test
     void checkingBall() {
+        Game game = new Game();
+        ArrayList<Integer> randomNum = new ArrayList<>();
+        randomNum.add(1);
+        randomNum.add(2);
+        randomNum.add(3);
+        String inputNum = "312";
+        int ballCount = 0;
+        int answer = game.checkingBall(randomNum, inputNum, ballCount);
+        assertEquals(3, answer);
     }
 
     @Test
