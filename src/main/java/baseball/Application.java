e21c50933b1433b00049642d7c4bf91a0714ced0
choiejae <ejae8319@gmail.com>
2022-11-08 12:44:14 +0900

    refactor: Modify instance value to final value

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,10 +6,11 @@ import ui.Input;
 import ui.Output;
 
 public class Application {
+    private static final int BALL_MAX_COUNT = 3;
     public static void main(String[] args) {
         Input input = new ConsoleInput();
         Output output = new ConsoleOutput();
-        Game game = new Game(3);
+        Game game = new Game(BALL_MAX_COUNT);
         game.run(input, output);
     }
 }
