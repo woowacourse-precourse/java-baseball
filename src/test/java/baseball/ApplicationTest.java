6f0eef9d63e051ea03b98f9882418e8fd8b7daac
YusunPark <solarpy.kor@gmail.com>
2022-11-08 21:44:34 +0900

    test: add exception check test
    사용자 입력 오류시 발생하는 exception 테스트 제작
    
    Closes #4, #7-3

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -9,6 +9,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -21,10 +22,57 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 개수_예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 중복_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("112"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 범위_예외_테스트_0포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("035"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 범위_예외_테스트_문자포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("ㄱㅇ3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 범위_예외_테스트_특수문자포함() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("!=3"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 재시작_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> assertRandomNumberInRangeTest(
+                        () -> {
+                            run("246", "135", "5");
+                            assertThat(output()).contains("낫싱", "3스트라이크", "3개의 숫자를 모두 맞히셨습니다! 게임 종료",
+                                    "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+                        },
+                        1, 3, 5
+                )).isInstanceOf(IllegalArgumentException.class)
         );
     }
 
