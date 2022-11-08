fe52ce206fcd393b7f561381c0872c1cfcef1d40
great-park <cksgh1735@naver.com>
2022-11-08 19:18:57 +0900

    feat: 스트라이크, 볼, 아웃 개수 판단

--- src/main/java/baseball/game/number/Calculator.java
+++ src/main/java/baseball/game/number/Calculator.java
@@ -14,4 +14,40 @@ public class Calculator {
 
         return userScore;
     }
+
+    private static int countStrike(List<Integer> answer, List<Integer> userInput) {
+        int strike = 0;
+        for (int index = 0; index < TOTAL_COUNT; index++) {
+            strike = checkStrike(answer.get(index), userInput.get(index), strike);
+        }
+        return strike;
+    }
+
+    private static int checkStrike(int answerNumber, int userNumber, int strike) {
+        if (answerNumber == userNumber) {
+            strike++;
+        }
+        return strike;
+    }
+
+    private static int countBall(List<Integer> answer, List<Integer> userInput) {
+        int ball = 0;
+        for (int userNumber : userInput) {
+            ball = checkBall(answer, userNumber, ball);
+        }
+        return ball;
+    }
+
+    private static int countOut(int strike, int ball) {
+        return TOTAL_COUNT - strike - ball;
+    }
+
+    public static int checkBall(List<Integer> answer, int userNumber, int ball) {
+        for (int answerNumber : answer) {
+            if (userNumber == answerNumber) {
+                ball++;
+            }
+        }
+        return ball;
+    }
 }
