e546cb8f1874f27e091e0e0b3cbfa143d77ffc3d
유미 <yumi@yumis-MacBook-Air.local>
2022-11-08 19:30:36 +0900

    기능2-3 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -5,28 +5,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGame {
-    private List<Integer> number;
+    private List<Character> number;
     private Boolean gameRestart = false;
 
     public void run(){
         // TODO 1. 랜덤 숫자 생성
         generateNumber();
-//        System.out.println(number);
+        System.out.println(number);
 
         // TODO 2-1. 사용자 입력 받기
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
         String newInput = Console.readLine();
-        // TODO 2-2. 예외 처리하기
+        // TODO 2-2. 예외 처리하
         checkInputFormat(newInput);
+        // TODO 2-3. 스트라이크, 볼 갯수 계산
+        int strikeCount = 0;
+        int ballCount = 0;
+        for (int i = 0; i < 3; i++){
+            if(number.get(i) == newInput.charAt(i)){
+                strikeCount +=1;
+            }else if(number.contains(newInput.charAt(i))){
+                ballCount += 1;
+            }
+        }
     }
 
     private void generateNumber(){
-        List<Integer> computer = new ArrayList<>();
+        List<Character> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+                computer.add((char)(randomNumber + 48));
             }
         }
         this.number = computer;
