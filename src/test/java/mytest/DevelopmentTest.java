1d49bf049a248bc789e1e706defc789eb8d9bb73
Yebin <lyb2325@gmail.com>
2022-11-08 22:44:05 +0900

    refactor: 테스트코드 메서드 명 변경 및 DisplayName을 통한 테스트 이름 구체화

--- src/test/java/mytest/DevelopmentTest.java
+++ src/test/java/mytest/DevelopmentTest.java
@@ -2,9 +2,9 @@ package mytest;
 
 import baseball.BaseballGame;
 import baseball.RandomNumberGenerator;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static baseball.BaseballGame.*;
@@ -17,6 +17,7 @@ public class DevelopmentTest {
      * 임의의 난수 3개 확인하기
      */
     @Test
+    @DisplayName("컴퓨터에서 생성한 정수 난수 배열의 크기와 범위 검증 테스트")
     void assertRandomNumberTest(){
         RandomNumberGenerator rng = new RandomNumberGenerator();
         rng.generate();
@@ -31,25 +32,29 @@ public class DevelopmentTest {
      * 사용자의 입력값 에러 처리
      */
     @Test
-    void userInput_정확한_숫자를_입력하는_경우(){
+    @DisplayName("사용자가 정수 3자리 수를 올바르게 입력하는 경우 검증 테스트")
+    void appropriateUserInputTest(){
         assertThat(BaseballGame.handleErrorForUserInput("352"))
                 .isEqualTo(List.of(3,5,2));
     }
 
     @Test
-    void userInput_0을_포함한_경우(){
+    @DisplayName("사용자 입력 값이 0을 포함하는 경우 오류 처리 검증 테스트")
+    void userInputContainsZeroTest(){
         assertThatThrownBy(()->handleErrorForUserInput("240"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void userInput_정수가_아닌_경우(){
+    @DisplayName("사용자 입력 값이 정수가 아닌 경우 오류 처리 검증 테스트")
+    void userInputNotIntegerTest(){
         assertThatThrownBy(()->handleErrorForUserInput("hello"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void userInput_세자리_정수가_아닌_경우(){
+    @DisplayName("사용자 입력 값이 세자리 정수가 아닌 경우 오류 처리 검증 테스트")
+    void userInputNotThreeIntegerDigit(){
         assertThatThrownBy(()->handleErrorForUserInput("2421"))
                 .isInstanceOf(IllegalArgumentException.class);
         assertThatThrownBy(()->handleErrorForUserInput("12"))
@@ -57,7 +62,8 @@ public class DevelopmentTest {
     }
 
     @Test
-    void userInput_동일한_숫자를_포함하는_경우(){
+    @DisplayName("사용자 입력 값이 서로 다른 세 정수가 아닌 경우 오류 처리 검증 테스트")
+    void userInputNotThreeDistinctiveInteger(){
         assertThatThrownBy(()->handleErrorForUserInput("343"))
                 .isInstanceOf(IllegalArgumentException.class);
     }
