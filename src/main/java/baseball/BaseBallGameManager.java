6125b1c254a50392b6793f308692b0f863ad58ca
HangSub999 <gkdtjq999@gmail.com>
2022-11-08 16:21:34 +0900

    feat: 상수 추가

--- src/main/java/baseball/BaseBallGameManager.java
+++ src/main/java/baseball/BaseBallGameManager.java
@@ -1,4 +1,8 @@
 package baseball;
 
 public class BaseBallGameManager {
+
+    public static final int THREE_STRIKE_NUMBER = 3;
+    public static final int GAME_RESET_NUMBER_ONE = 1;
+    public static final int RESTART_NUMBER_THREE = 3;
 }
