ee625344fc1ac1c0165a825eae9a9488cc02527d
ys200209 <lys200209@naver.com>
2022-11-08 20:57:21 +0900

    feat(Hint.java): 힌트 메시지를 제공함
    추가된 메서드
    - getHintMessage(): 자신의 상태값을 이용해 힌트 메시지를 생성하여 반환합니다.
    - isExistsBall(): ball이 존재하는지 판별한다.
    - isExistsStrike(): strike가 존재하는지 판별한다.

--- src/main/java/baseball/domain/Hint.java
+++ src/main/java/baseball/domain/Hint.java
@@ -20,4 +20,44 @@ public class Hint {
         this.strike += 1;
     }
 
+    public String getHintMessage() {
+        String hintMessage;
+
+        if (isExistsBall() && isExistsStrike()) {
+            hintMessage = getBallMessage() + " " + getStrikeMessage();
+        } else if (isExistsBall()) {
+            hintMessage = getBallMessage();
+        } else if (isExistsStrike()) {
+            hintMessage = getStrikeMessage();
+        } else {
+            hintMessage = "낫싱";
+        }
+
+        return hintMessage;
+    }
+
+    public boolean isExistsBall() {
+        if (this.ball > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isExistsStrike() {
+        if (this.strike > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getBallMessage() {
+        return ball + "볼";
+    }
+
+    public String getStrikeMessage() {
+        return strike + "스트라이크";
+    }
+
 }
