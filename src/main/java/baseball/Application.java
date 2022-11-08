e475b060bd5e1727a325b3d0d23515c5569b9a08
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-08 19:49:30 +0900

    feat : 메인 기능 클래스의 경우에 게임타입넘버를 변경하여 게임 자릿 수를 변경하는 기능
    만약 사용자가 3자리 수가 아닌 다른 수의 야구게임을 즐기고 싶다면 게임타입넘버를 변경하여 설정하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,32 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
+
 public class Application {
+
+
+    public static int escapeGame(int a) {
+        OutputSentense out = new OutputSentense();
+
+        while (!(a == 1 || a == 2)) {
+
+            out.showMessage(out.reQuestion);
+            a = out.escape(out.Question(out.restart, 1));
+        }
+        return a;
+    }
+
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        //camp.nextstep.edu.missionutils.Randoms
+        OutputSentense output = new OutputSentense();
+        List<Integer> computer = new ArrayList<>();
+        final int gameTypeNumber = 3;
+
     }
 }
