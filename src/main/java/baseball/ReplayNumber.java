edb66cb8309668c828571e7c6060a59e2a0d782e
dooboocookie <bkh3030@gmail.com>
2022-11-08 17:34:40 +0900

    refactor: 재시작 여부 확인하는 int 정적 변수로 명시적으로 표현

--- src/main/java/baseball/ReplayNumber.java
+++ src/main/java/baseball/ReplayNumber.java
@@ -2,6 +2,9 @@ package baseball;
 
 public class ReplayNumber {
 
+    private static final int REPLAY_YES = 1;
+    private static final int REPLAY_NO = 2;
+
     private int replayNumber;
 
     public ReplayNumber(int input) {
@@ -10,13 +13,13 @@ public class ReplayNumber {
     }
 
     private void validateRange(int input) {
-        if ((input != 1) && (input != 2)) {
+        if ((input != REPLAY_YES) && (input != REPLAY_NO)) {
             throw new IllegalArgumentException("0 혹은 1만 입력할 수 있습니다.");
         }
     }
 
     public boolean isReplay() {
-        return (replayNumber == 1);
+        return replayNumber == REPLAY_YES;
     }
 
 }
