7d3bf8861b8043c076fb420e099d636b447f2549
sonsoowon <yuwoo1018@gmail.com>
2022-11-08 21:28:04 +0900

    test(ApplicationTest): Player 관련 테스트 메서드 삭제

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -22,65 +22,6 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void isInputSizeEqualToAnswerSize_입력의_자리수가_게임정답의_자리수와_동일한_경우_참(){
-
-        List<String> playerInput = List.of("123", "12");
-        List<Boolean> expectedOutput = List.of(true, false);
-
-        List<Boolean> realOutput = playerInput.stream()
-                .map(input -> Player.isInputSizeEqualToAnswerSize(input))
-                .collect(Collectors.toList());
-
-        assertThat(expectedOutput).isEqualTo(realOutput);
-    }
-
-    @Test
-    void doesNotHaveDuplicateInputElement_입력에_중복된_문자가_없는_경우_참(){
-        List<String> playerInput = List.of("123", "112");
-        List<Boolean> expectedOutput = List.of(true, false);
-
-        List<Boolean> realOutput = playerInput.stream()
-                .map(input -> Player.doesNotHaveDuplicateInputElement(input))
-                .collect(Collectors.toList());
-
-        assertThat(expectedOutput).isEqualTo(realOutput);
-    }
-
-    @Test
-    void hasOnlyNaturalNumberAsInput_입력이_자연수로만_구성된_경우_참(){
-        List<String> playerInput = List.of("123", "012", "1a3");
-        List<Boolean> expectedOutput = List.of(true, false, false);
-
-        List<Boolean> realOutput = playerInput.stream()
-                .map(input -> Player.hasOnlyNaturalNumberAsInput(input))
-                .collect(Collectors.toList());
-
-        assertThat(expectedOutput).isEqualTo(realOutput);
-    }
-
-    @Test
-    void isValidInput_입력이_유효한_경우_참(){
-        List<String> playerInput = List.of("012", "112", "12", "1234", "", "123");
-        List<Boolean> expectedOutput = List.of(false, false, false, false, false, true);
-
-        List<Boolean> realOutput = playerInput.stream()
-                .map(input -> Player.isValidInput(input))
-                .collect(Collectors.toList());
-
-        assertThat(realOutput).isEqualTo(expectedOutput);
-    }
-
-    @Test
-    void convertStringToIntegerList_문자열_타입의_입력을_정수형_리스트로_변환(){
-        String playerInput = "123";
-        List<Integer> expectedOutput = List.of(1, 2, 3);
-
-        List<Integer> realOutput = Player.convertStringToIntegerList(playerInput);
-
-        assertThat(expectedOutput).isEqualTo(realOutput);
-    }
-
-    @Test
     void countNumbersContainedInAnswer_게임정답에_포함된_입력의_개수_반환(){
         Game game = new Game(List.of(1, 2, 3));
         List<List<Integer>> playerInput = List.of(
