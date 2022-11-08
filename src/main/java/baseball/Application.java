20d58c81087fbc0fc3731cb909f0dddedf8af2f2
rjsah09 <83952174+rjsah09@users.noreply.github.com>
2022-11-08 22:18:37 +0900

    refactor: getRandomNumberList 메서드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.sql.Array;
 import java.util.*;
 
 public class Application {
@@ -16,7 +17,13 @@ public class Application {
     }
 
     public static List<Integer> getRandomNumberList() {
-        List computer = Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        List computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
         return computer;
     }
 
