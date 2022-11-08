3ebff1a50bd1f55aaa40efcf6ca281f0ca42e3f0
Identityless <rlandwnd555@naver.com>
2022-11-08 15:58:59 +0900

    fix : Inputer 의존 객체 추가

--- src/main/java/controller/impl/BaseballGameController.java
+++ src/main/java/controller/impl/BaseballGameController.java
@@ -3,6 +3,7 @@ package controller.impl;
 import controller.GameController;
 import domain.RandNumVO;
 import util.Comparator;
+import util.Inputer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,12 +12,15 @@ public class BaseballGameController implements GameController {
     private int conut = 0;
     private RandNumVO randnumVO;
     private Comparator comparator;
+    private List<Integer> input;
     private List<Integer> sig;
+    private Inputer inputer;
 
     public BaseballGameController(RandNumVO randnumVO, Comparator comparator) {
         this.randnumVO = randnumVO;
         this.comparator = comparator;
         this.sig = new ArrayList<Integer>(List.of(0,0,0));
+        this.inputer = new Inputer();
     }
 
     @Override
