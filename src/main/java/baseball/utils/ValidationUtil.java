4980b54daa50253611cb941cd7b0365fe1eab3cf
HubCreator <chocomilk8604@gmail.com>
2022-11-08 10:19:57 +0900

    refactor : 숫자가 아닌 값 validation 추가

--- src/main/java/baseball/utils/ValidationUtil.java
+++ src/main/java/baseball/utils/ValidationUtil.java
@@ -9,7 +9,9 @@ import static baseball.domain.ConstVariable.*;
 public class ValidationUtil {
 
     public static boolean isValidRange(List<Integer> list) {
-        return list.stream().filter(m -> m >= MIN && m <= MAX).count() == list.size();
+        return list.stream()
+                .filter(m -> m >= MIN && m <= MAX)
+                .count() == list.size();
     }
 
     public static boolean isValidCount(List<Integer> list) {
@@ -17,8 +19,7 @@ public class ValidationUtil {
     }
 
     public static boolean haveNoDuplicatedNum(List<Integer> list) {
-        HashSet<Integer> set = new HashSet<>(list);
-        return list.size() == set.size();
+        return list.size() ==  new HashSet<>(list).size();
     }
 
     public static void isValid(List<Integer> list) {
@@ -34,11 +35,22 @@ public class ValidationUtil {
     }
 
     public static List<Integer> mapStringToList(String str) {
+        if (!isAlphabetic(str)) {
+            throw new IllegalArgumentException("숫자가 아닌 값이 포함되었습니다.");
+        }
         List<Integer> result = new ArrayList<>();
         for (int i = 0; i < str.length(); i++) {
             result.add(Integer.parseInt(String.valueOf(str.charAt(i))));
         }
         isValid(result);
         return result;
+    }
+
+    private static boolean isAlphabetic(String str) {
+        int i = 0;
+        while (i < str.length() && Character.isDigit(str.charAt(i))) {
+            i++;
+        }
+        return str.length() == i;
     }
 }
