6db5dba68e83fac8ca489a447b2538295a9a7ab4
masiljangajji <hjk172262@gmail.com>
2022-11-08 22:45:53 +0900

    feat: 재시작 여부 입력

--- src/main/java/baseball/InputNumber.java
+++ src/main/java/baseball/InputNumber.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public class InputNumber {
     String userBaseBallNumber;
+    String reStartNumber;
     List<Integer> computerBaseBallNumber = new ArrayList<>();
 
 
@@ -24,5 +25,8 @@ public class InputNumber {
         userBaseBallNumber = Console.readLine();
     }
 
+    public void inputReStartNumber() {
+        reStartNumber = Console.readLine();
+    }
 
 }
--- src/main/java/baseball/PlayBaseBallGame.java
+++ src/main/java/baseball/PlayBaseBallGame.java
@@ -6,19 +6,19 @@ public class PlayBaseBallGame {
         InputNumber number = new InputNumber();
         BaseBallCount countBaseBall = new BaseBallCount();
 
-
         message.printStartBaseballGame();
         number.inputComputerBaseballNumber();
 
         while (true) {
             message.printInputBaseballNumber();
             number.inputUserBaseballNumber();
-
             countBaseBall.countBallNumber(number.computerBaseBallNumber, number.userBaseBallNumber);
             countBaseBall.countStrikeNumber(number.computerBaseBallNumber, number.userBaseBallNumber);
             message.printHint(countBaseBall.numberOfStrik, countBaseBall.numberOfBall);
 
             if (countBaseBall.numberOfStrik == 3) {
+                message.printCorrectAnswer();
+                message.printReGame();
                 break;
             }
         }
