abf965bf2b222d95e261dbdcc4253155bdf454f8
Identityless <rlandwnd555@naver.com>
2022-11-08 20:42:24 +0900

    fix : 1을 입력해도 게임이 재시작하지 않는 버그 수정

--- src/main/java/controller/impl/BaseballGameController.java
+++ src/main/java/controller/impl/BaseballGameController.java
@@ -39,11 +39,12 @@ public class BaseballGameController implements GameController {
             return;
         } finally {
             inputer.initInputer();
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             if(inputer.getInput().get(0) == 1)
             {
                 initGame();
+                doGame();
             }
             else {
                 return;
