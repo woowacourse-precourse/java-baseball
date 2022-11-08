1fe2690c85e7738f431d3ba087565c9ca805c40b
math0917 <math0917@naver.com>
2022-11-08 22:10:58 +0900

    fix: 수정 BallStrikeConstant indent depth 2이하

--- src/main/java/baseball/constant/BallStrikeConstant.java
+++ src/main/java/baseball/constant/BallStrikeConstant.java
@@ -7,7 +7,6 @@ public enum BallStrikeConstant {
     THREE_STRIKE("3스트라이크", true),
     ONE_BALL("1볼", false),
     ONE_BALL_ONE_STRIKE("1볼 1스트라이크", false),
-    ONE_BALL_TWO_STRIKE("1볼 2스트라이크", false),
     TWO_BALL("2볼", false),
     TWO_BALL_ONE_STRIKE("2볼 1스트라이크", false),
     THREE_BALL("3볼", false);
@@ -27,48 +26,43 @@ public enum BallStrikeConstant {
 
     public static BallStrikeConstant getResult(int ball, int strike) {
         BallStrikeConstant ballStrikeConstant = null;
-        switch (ball) {
-            case 0:
-                switch (strike) {
-                    case 0:
-                        ballStrikeConstant = NOTHING;
-                        break;
-                    case 1:
-                        ballStrikeConstant = ONE_STRIKE;
-                        break;
-                    case 2:
-                        ballStrikeConstant = TWO_STRIKE;
-                        break;
-                    case 3:
-                        ballStrikeConstant = THREE_STRIKE;
-                        break;
-                }
-                break;
-            case 1:
-                switch (strike) {
-                    case 0:
-                        ballStrikeConstant = ONE_BALL;
-                        break;
-                    case 1:
-                        ballStrikeConstant = ONE_BALL_ONE_STRIKE;
-                        break;
-                }
-                break;
-            case 2:
-                switch (strike) {
-                    case 0:
-                        ballStrikeConstant = TWO_BALL;
-                        break;
-                    case 1:
-                        ballStrikeConstant = TWO_BALL_ONE_STRIKE;
-                        break;
-                }
-                break;
-            case 3:
+
+        if (ballCountEquals(ball, 0)) {
+            if (strikeCountEquals(strike, 0)) {
+                ballStrikeConstant = NOTHING;
+            } else if (strikeCountEquals(strike, 1)) {
+                ballStrikeConstant = ONE_STRIKE;
+            } else if (strikeCountEquals(strike, 2)) {
+                ballStrikeConstant = TWO_STRIKE;
+            } else if (strikeCountEquals(strike, 3)) {
+                ballStrikeConstant = THREE_STRIKE;
+            }
+        } else if (ballCountEquals(ball, 1)) {
+            if (strikeCountEquals(strike, 0)) {
+                ballStrikeConstant = ONE_BALL;
+            } else if (strikeCountEquals(strike, 1)) {
+                ballStrikeConstant = ONE_BALL_ONE_STRIKE;
+            }
+        } else if (ballCountEquals(ball, 2)) {
+            if (strikeCountEquals(strike, 0)) {
+                ballStrikeConstant = TWO_BALL;
+            } else if (strikeCountEquals(strike, 1)) {
+                ballStrikeConstant = TWO_BALL_ONE_STRIKE;
+            }
+        } else if (ballCountEquals(ball, 3)) {
+            if (strikeCountEquals(strike, 0)) {
                 ballStrikeConstant = THREE_BALL;
-                break;
+            }
         }
 
         return ballStrikeConstant;
+    }
+
+    private static boolean strikeCountEquals(int strike, int count) {
+        return count == strike;
+    }
+
+    private static boolean ballCountEquals(int ball, int count) {
+        return count == ball;
     }
 }
