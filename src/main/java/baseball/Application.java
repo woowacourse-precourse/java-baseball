35153d0fa92ea07e7349e3508d0ecbdd378f13fa
Moon-Tree <ellaya1012@gmail.com>
2022-11-08 23:38:28 +0900

    docs : 숫자 야구 게임 기능 목록 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,14 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
+
+
 
 public class Application {
 
-    public static int randomNumber() {
-        Random random = new Random();
-        return random.nextInt(9) + 1;
-    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
