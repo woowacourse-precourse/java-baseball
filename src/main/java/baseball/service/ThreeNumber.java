abf7192e3e8940f3881bbc2c3190a0b51d448b43
footdev <ekclstkfka5@gmail.com>
2022-11-08 17:16:13 +0900

    feat: 상대방이 서로 다른 3자리 수를 생성하는 기능 구현
    ThreeNumber 클래스의 generate 메소드로 해당 기능을 구현함.

--- src/main/java/baseball/service/ThreeNumber.java
+++ src/main/java/baseball/service/ThreeNumber.java
@@ -1,4 +1,23 @@
 package baseball.service;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreeNumber {
+    private final List<Integer> number;
+
+    public ThreeNumber() {
+        this.number = new ArrayList<>();
+    }
+
+    public void generate() {
+        while (number.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!number.contains(randomNumber)) {
+                number.add(randomNumber);
+            }
+        }
+    }
 }
