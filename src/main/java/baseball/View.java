0baec050dee2bbf18b3d482f4ed7ef0855d292f2
yoon <beatus96@naver.com>
2022-11-08 12:14:35 +0900

    refactor(view) : 입출력(UI) 관련 메서드 정리

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,2 +1,32 @@
-package baseball;public class View {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+public class View {
+    private static final String START ="숫자 야구 게임을 시작합니다.";
+    private static final String INPUT ="숫자를 입력해주세요 : ";
+    private static final String RESULT_CORRECT ="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String ASK_KEEP_PLAYING ="게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public static String getUserInput() throws IllegalArgumentException {
+        System.out.println(START);
+        System.out.print(INPUT);
+        String input = Console.readLine().trim();
+        if (!Validator.isNumber(input)) {
+            throw new IllegalArgumentException();
+        }
+        return input;
+    }
+
+
+    public static void printHint(String hint) {
+        System.out.println(hint);
+    }
+
+    public static String getExitInput() throws IllegalArgumentException {
+        System.out.println(RESULT_CORRECT);
+        System.out.println(ASK_KEEP_PLAYING);
+        String input = Console.readLine().trim();
+        if (!Validator.isValidCommandNumber(input)) throw new IllegalArgumentException();
+        return input;
+    }
 }
