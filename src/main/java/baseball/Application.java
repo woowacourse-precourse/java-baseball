8b8ec0e829388798211e4c4de55b8c10481b09b7
meunrud <y3a0522@gmail.com>
2022-11-08 21:11:09 +0900

    :recycle: 구현한 클래스에 맞춰 main에 호출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,16 +4,14 @@ package baseball;
 public class Application {
 
     public static void main(String[] args) {
-        String computerRandomNumber = "";
-        String userInputNumber ="";
+        Game game = new Game();
 
-       computerRandomNumber =  computerRandomNumber();
-        gameStartMessage();
-        userInputNumber = inputUserPickNumber();
-        int strike = howManyStrike(userInputNumber,computerRandomNumber);
-        int ball = howManyBall(userInputNumber,computerRandomNumber);
-        int nothing = nothing(userInputNumber,computerRandomNumber);
-        System.out.println("스트라이크: "+strike + "볼"+ ball);
+        do{
+            game.playGame();
+        }while (game.askResrat());
+
+
+
 
     }
 
