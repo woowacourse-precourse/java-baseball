639f43cac29a5f9aeb1489c6de9338534f499f83
chaos0103 <lyt1228@naver.com>
2022-11-08 17:26:49 +0900

    refactor: 기능과 구현을 분리

--- src/main/java/baseball/BaseballGameService.java
+++ src/main/java/baseball/BaseballGameService.java
@@ -1,54 +1,8 @@
 package baseball;
 
-import java.util.List;
+public interface BaseballGameService {
 
-import static baseball.Message.*;
+    void run(User user);
 
-public class BaseballGameService {
-
-    private final static int MAX_SIZE = 3;
-
-    public void run(User user) {
-        List<Integer> numbers = user.getNumbers();
-        List<Integer> answers = user.getAnswers();
-
-        for (int i = 0; i < MAX_SIZE; i++) {
-            if (isStrike(numbers.get(i), answers.get(i))) {
-                user.addStrike();
-            } else if (isBall(numbers.get(i), answers)) {
-                user.addBall();
-            }
-        }
-    }
-
-    public String getResultMessage(User user) {
-        int ball = user.getBall();
-        int strike = user.getStrike();
-
-        if (isStrikeZero(strike) && isBallZero(ball)) {
-            return NOT_THING;
-        } else if (isStrikeZero(strike)) {
-            return ballMessage(ball);
-        } else if (isBallZero(ball)) {
-            return strikeMessage(strike);
-        } else {
-            return ballAndStrikeMessage(ball, strike);
-        }
-    }
-
-    private boolean isStrike(int number, int answer) {
-        return number == answer;
-    }
-
-    private boolean isBall(int number, List<Integer> answers) {
-        return answers.contains(number);
-    }
-
-    private boolean isStrikeZero(int strike) {
-        return strike == 0;
-    }
-
-    private boolean isBallZero(int ball) {
-        return ball == 0;
-    }
+    String getResultMessage(User user);
 }
