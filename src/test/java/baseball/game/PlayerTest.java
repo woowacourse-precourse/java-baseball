7e8071fde36dba95eeecddc925f7bb4505ffe1b2
minju412 <ln8847@ajou.ac.kr>
2022-11-08 18:32:24 +0900

    test: add PlayerTest

--- src/test/java/baseball/game/PlayerTest.java
+++ src/test/java/baseball/game/PlayerTest.java
@@ -12,13 +12,24 @@ public class PlayerTest {
     private Player player;
 
     @BeforeEach
-    void initial() {
+    void initialize() {
         player = new Player();
     }
 
     @Test
-    void 사용자의_응답이_재시작_혹은_종료_문자가_아니라면_예외가_발생합니다() {
+    void 사용자의_응답이_재시작_혹은_종료_값이_아닌_숫자라면_예외가_발생합니다() {
         String input = "3";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> {
+            player.getPlayerAnswer();
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 사용자의_응답이_재시작_혹은_종료_값이_아닌_문자라면_예외가_발생합니다() {
+        String input = "$";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
