67bd8ee915f5f4d218d3ba213e1aa0254eac1cf7
mihye126 <mihea12@gmail.com>
2022-11-08 21:35:53 +0900

    attack is valid?

--- src/main/java/baseball/util/InputUtility.java
+++ src/main/java/baseball/util/InputUtility.java
@@ -12,6 +12,20 @@ public class InputUtility {
             throw new IllegalArgumentException("유효하지 않은 문자입니다.");
         }
     }
+    private static void isValidAttack(String input) {
+        if (!isNaturalNumber(input)) {
+            throw new IllegalArgumentException("1-9 이외의 문자가 들어있습니다. 1-9까지의 숫자만 입력해주세요");
+        }
+
+        if (isOutOfIndex(input)) {
+            throw new IllegalArgumentException("입력값이 많거나 적습니다. 숫자 3개를 입력해주세요");
+        }
+
+        if (isDuplicated(input)) {
+            throw new IllegalArgumentException("중복된 문자가 들어있습니다.");
+        }
+
+    }
 
     private static boolean isDuplicated(String input) {
         String deduplicatedInput = String.join("", new HashSet<>(List.of(input.split(""))));
