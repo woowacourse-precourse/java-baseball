528e2887a92e674d903126c45ccfeaf9a54a77d8
kkimhaji <kkim.haji@gmail.com>
2022-11-08 17:55:56 +0900

    feat(Hint): init ball, strike count

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -3,53 +3,59 @@ package baseball;
 import java.util.List;
 
 public class Hint {
-    private List<Integer> playerAnswer;
-    private List<Integer> computerAnswer;
     private int strike;
     private int ball;
 
-    public Hint(List<Integer> playerAnswer, List<Integer> computerAnswer, int strike, int ball) {
-        this.playerAnswer = playerAnswer;
-        this.computerAnswer = computerAnswer;
+    public Hint() {
         this.strike = 0;
         this.ball = 0;
     }
 
-    public void compareNumber(List<Integer> playerNum, List<Integer> answer){
-        for(int i=0;i<3;i++){
+    private void initCount(){
+        strike = 0;
+        ball = 0;
+    }
+
+    public String compareNumber(List<Integer> playerNum, List<Integer> answer){
+        initCount();
+        for(int i=0;i<Range.DIGIT;i++){
             countStrike(playerNum.get(i), answer.get(i));
             countBall(i, playerNum.get(i), answer);
         }
+        return makeHint(strike, ball);
     }
 
-    public void countStrike(int playerAnswer, int computerAnswer){
+    public int getStrike() {
+        return strike;
+    }
+
+    private void countStrike(int playerAnswer, int computerAnswer){
         if (playerAnswer == computerAnswer) {
             strike++;
         }
     }
 
-    public void countBall(int index, int playerAnswer, List<Integer> answer){
+    private void countBall(int index, int playerAnswer, List<Integer> answer){
         if(answer.contains(playerAnswer) && answer.indexOf(playerAnswer)!=index){
             ball++;
         }
     }
 
-    public void makeHint(int strike, int ball) {
+    private String makeHint(int strike, int ball) {
         String hint = "";
         if (strike == 0 && ball == 0) {
             hint = Message.NOTHING.getMessage();
         }
-        else if (strike == Range.DIGIT) {
-            hint = Message.END_GAME.getMessage();
-            System.out.println(Message.END_GAME);
-        }
-        else if (ball != 0) {
+        if (ball != 0) {
             hint += ball + Message.BALL.getMessage();
         }
-        else if (strike != 0) {
+        if (strike!=0) {
             hint += strike + Message.STRIKE.getMessage();
         }
-        System.out.println(hint);
+        if (strike == Range.DIGIT) {
+            hint += "\n"+Message.END_GAME.getMessage();
+        }
+        return hint;
     }
 
 
