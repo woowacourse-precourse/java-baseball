7a993d72484fb4bd7f26500ac55ef019c7d59daa
Yh0sun <hy770819@naver.com>
2022-11-08 19:36:38 +0900

    feat : BaseBall 클래스

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,29 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    class Baseball {
+        List<Integer> computerNum;
+        int[] computerNumCheck = new int[10];
+        int[] strikeResult = new int[3];
+
+        public void clear(){}
+        public boolean startGame(){}
+        public List<Integer> makeComputerNum(){}
+        public List<Integer> toInputNum(){}
+        public int strike(){}
+        public int ball(){}
+        public boolean resultPrint(){}
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
 }
