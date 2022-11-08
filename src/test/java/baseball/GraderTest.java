80584b13bea029bb6e865f5cc52d8c9ac6f7b67b
jaehee329 <jaehee329@yonsei.ac.kr>
2022-11-08 23:27:36 +0900

    refactor: non-static import문을 하나로 묶음

--- src/test/java/baseball/GraderTest.java
+++ src/test/java/baseball/GraderTest.java
@@ -3,7 +3,6 @@ package baseball;
 import static org.assertj.core.api.Assertions.*;
 
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 class GraderTest {
--- src/test/java/baseball/OutputHandlerTest.java
+++ src/test/java/baseball/OutputHandlerTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.*;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStream;
 import java.io.PrintStream;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
