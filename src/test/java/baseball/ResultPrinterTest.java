1c639945a9c2eec57774673e169564c056a971e9
aak2075 <35948985+aak2075@users.noreply.github.com>
2022-11-08 20:25:39 +0900

    Test : ResultPrinterTest 추가

---
+++ src/test/java/baseball/ResultPrinterTest.java
@@ -0,0 +1,40 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ResultPrinterTest {
+    @Test
+    void case1() {
+        ResultPrinter resultPrinter = new ResultPrinter();
+        OutputStream captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+        resultPrinter.printResult("123", "124");
+        assertThat(captor.toString().trim()).contains("2스트라이크");
+    }
+
+    @Test
+    void case2() {
+        ResultPrinter resultPrinter = new ResultPrinter();
+        OutputStream captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+        resultPrinter.printResult("589", "985");
+        assertThat(captor.toString().trim()).contains("2볼 1스트라이크");
+    }
+
+    @Test
+    void case3() {
+        ResultPrinter resultPrinter = new ResultPrinter();
+        OutputStream captor = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(captor));
+        resultPrinter.printResult("467", "467");
+        assertThat(captor.toString().trim()).contains("3스트라이크");
+    }
+
+
+}
