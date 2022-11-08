8849d4fb174c688496fd4ed3a0c38f7e877115d6
baeseohyeon <formnarago@naver.com>
2022-11-08 12:48:11 +0900

    feat: 게임모델과 뷰를 이용하여 실행시키는 컨트롤러 클래스 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import baseball.model.BaseballGame;
+import baseball.model.GameBoard;
+import baseball.view.View;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        View view = new View();
+        BaseballGame game = new BaseballGame();
+
+        view.printStartMenu();
+        while (game.isPlaying()) {
+            view.printInputMenu();
+            String selectedBallNumbers = view.getNumberString();
+            GameBoard gameBoard = game.playGame(selectedBallNumbers);
+            view.printGameResult(gameBoard.getResult());
+            if (gameBoard.isThreeStrike()) {
+                view.printEndMenu();
+                String command = view.getNumberString();
+                game.checkContinueCommand(command);
+            }
+        }
     }
 }
