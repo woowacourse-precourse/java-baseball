47167688d941e5f0e74e743fde23aeb5e36b8376
gusu9 <suhan.ku9@gmail.com>
2022-11-08 12:49:56 +0900

    created user enter number file

---
+++ src/main/java/baseball/UserEnter.java
@@ -0,0 +1,79 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+import java.util.HashSet;
+
+public class UserEnter {
+    public static void main(String args[]) {
+        try {
+            userEnterNum();
+        } catch (RedundantException re) {
+            System.out.println("중복되는 숫자를 입력할 수 없습니다.");
+            System.out.println("에러메시지 : " + re.getMessage());
+            re.printStackTrace();
+        } catch (SizeException se) {
+            System.out.println("숫자 세개를 입력해야 합니다.");
+            System.out.println("에러메시지 : " + se.getMessage());
+            se.printStackTrace();
+        } catch (NaturalException ne) {
+            System.out.println("1~9 사이의 숫자를 입력해야 합니다.");
+            System.out.println("에러메시지 : " + ne.getMessage());
+            ne.printStackTrace();
+        }
+    }
+    static void userEnterNum() throws RedundantException, SizeException, NaturalException {
+        int strikeNball = 0;
+        int strike = 0;
+        int ball = 0;
+        int userNumber = 0;
+        int attempt = 0;
+
+        attempt =attempt+1;
+
+        System.out.printf("[%d차시도] 숫자를 입력해주세요: ",attempt);
+        userNumber =Integer.parseInt(Console.readLine());
+
+        int[] digits = Stream.of(String.valueOf(userNumber).split("")).mapToInt(Integer::parseInt).toArray();
+        List<Integer> user = new ArrayList<>();
+
+        for(
+                int order = 0;
+                order<digits.length;order++)
+        {
+            user.add(digits[order]);
+        }
+
+        Set<Integer> userSet = new HashSet<>(user);
+
+        if (userSet.size() != user.size()) {
+            throw new RedundantException("중복되는 숫자를 입력할 수 없습니다.");
+        }
+        if (user.size() != 3) {
+            throw new SizeException("숫자 세개를 입력해야 합니다.");
+        }
+        if (user.contains(0)) {
+            throw new NaturalException("1~9 사이의 숫자를 입력해야 합니다.");
+        }
+    }
+
+    static class RedundantException extends Exception {
+        RedundantException(String redundant) {
+            super(redundant);
+        }
+    }
+    static class SizeException extends Exception {
+        SizeException(String size) {
+            super(size);
+        }
+    }
+    static class NaturalException extends Exception {
+        NaturalException(String natural) {
+            super(natural);
+        }
+    }
+}
