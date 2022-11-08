dd5927662225f045c6438b060d00f5a1ce5cf65c
nn98 <jkllhgb@gmail.com>
2022-11-08 20:10:47 +0900

    Style: 코딩 컨벤션 준수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,7 @@ package baseball;
 public class Application {
 
     public static void main(String[] args) {
-        Game game=new Game();
+        Game game = new Game();
         game.run();
     }
 
