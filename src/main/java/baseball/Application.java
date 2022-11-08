114024988fffec649365d4d819b0c9b0f8a22c9a
yewon <yewon.ux@gmail.com>
2022-11-08 10:27:01 +0900

    feat: user input 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import java.util.Scanner;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        // scan user input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("서로다른 세 숫자를 입력해주세요");
+        int userRandNumber = sc.nextInt();
+
     }
 }
