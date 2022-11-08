9c645b28e48887452fc1ca6e09c42a8e914350c7
gyumin159 <gyumin159@naver.com>
2022-11-08 11:35:56 +0900

    feat : 플레이어와 컴퓨터의 값을 비교하는 메소드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -25,13 +25,39 @@ public class Application {
      * */
     public static void isDifferentThreeNum(String playerInput){
         List<String> playerInputArray = Arrays.asList(playerInput.split(""));
-        Set<String> delSameNum = new HashSet<String>(playerInputArray);
+        Set<String> delSameNum = new HashSet<>(playerInputArray);
         boolean isOneToNineAndSizeThree = Pattern.matches("^[1-9]{3}$", playerInput);
         boolean isNoOverlap = playerInputArray.size() == delSameNum.size();
 
         if (!(isOneToNineAndSizeThree && isNoOverlap)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    // 플레이어와 컴퓨터의 숫자를 비교해 결과를 리스트에 [스트라이크 수, 볼 수, miss 수] 담아 반환
+    /**
+     *  플레이어의 입력과 컴퓨터의 입력을 비교해 [스트라이크 수, 볼 수, miss 수]를 생성
+     *
+     * @param computer 컴퓨터가 생성한 1~9사이 서로다른 3개의 수를 가진 리스트
+     * @param player 플레이어가 생성한 1~9사이 서로다른 3개의 수를 가진 스트링
+     * @return [스트라이크 수, 볼 수, miss 수]의 리스트
+     */
+    public static List<Integer> processScore(List<Integer> computer, String player) {
+        List<Integer> score = new ArrayList<>(Arrays.asList(0, 0, 0));
+        for (int idx = 0; idx < 3; idx++) {
+            char currentPlayerChar = player.charAt(idx);
+            int currentPlayerCharValue = Character.getNumericValue(currentPlayerChar);
+
+            if (currentPlayerCharValue == computer.get(idx)) { // 인덱스와 값 모두 같은 경우 (스트라이크)
+                score.set(0, score.get(0) + 1);
+            } else if (computer.contains(currentPlayerCharValue)) { // 인덱스가 다르나 값이 있는 경우(볼)
+                score.set(1, score.get(1) + 1);
+            }
+            else { // 값이 어디에도 매치하지 않는 경우(Miss)
+                score.set(2, score.get(2) + 1);
+            }
+        }
+        return score;
     }
 
 
