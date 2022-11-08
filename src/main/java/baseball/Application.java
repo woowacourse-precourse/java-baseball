1a81585e07025b78173f90e576de34b58e68a382
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-11-08 18:11:22 +0900

    feat: 숫자 랜덤 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.io.IOException;
+import java.util.*;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IOException {
+        System.out.println("숫자 야구 게임을 시작합니다. 룰은 다음과 같습니다.");
+        System.out.println("숫자 야구 게임은 컴퓨터가 생성한 임의의 1-9사이의 3자리 수를 맞추는 게임입니다.");
+        System.out.println("숫자가 포함되어 있으나 자리가 틀렸을 경우에는 볼, 숫자와 자리가 일치할 경우에는 스트라이크로 표기됩니다.");
+    }
+
+    private static void startGame() throws IOException {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
     }
 }
