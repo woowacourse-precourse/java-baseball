8347c93487e50eec9fb10eef578374944a909a8b
Giho <poqw741@gmail.com>
2022-11-08 21:29:10 +0900

    feat : 최종 구현 및 오류 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,12 +5,11 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
         BaseballGame game = new BaseballGame();
         while(game.gameEnd()){
             game.playGame();
         }
-
-
     }
 }
 
