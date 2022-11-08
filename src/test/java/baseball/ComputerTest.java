a3761ac2102ec7f8681e14116cd4b2238d438dd5
SangWoo Yun <64531982+Awarduuu@users.noreply.github.com>
2022-11-08 19:04:54 +0900

    refactor(ComputerTest): delete duplicate declaration of constructor

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -6,7 +6,8 @@ import static baseball.Computer.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ComputerTest {
-
+    Computer computer = new Computer();
+    List<Integer> list = List.of(2,5,3);
     @Test
     void makeAnswer_컴퓨터_정답_만들기(){
         List<Integer> answer = makeAnswer();
@@ -15,8 +16,7 @@ public class ComputerTest {
     }
 
     @Test
-    void resetHintMap_힌트리스트_초기화() {
-        Computer computer = new Computer();
+    void resetHintMap_힌트리스트_초기화() {;
         computer.resetHintMap();
         assertThat(HintMap.get("스트라이크")).isEqualTo(0);
         assertThat(HintMap.get("볼")).isEqualTo(0);
@@ -24,10 +24,8 @@ public class ComputerTest {
 
     @Test
     void isStrikeOrBall_스트라이크인지_볼인지_판별(){
-        Computer computer = new Computer();
         computer.resetHintMap();
         computer.Answer = List.of(1,2,3);
-        List<Integer> list = List.of(2,1,3);
         for (int i =0; i<list.size(); i++){
             computer.isStrikeOrBall(Answer.get(i), list.get(i));
         }
@@ -39,10 +37,8 @@ public class ComputerTest {
 
     @Test
     void checkPlayerAnswer_플레이어의_정답을_비교(){
-        Computer computer = new Computer();
         computer.Answer = List.of(1,2,3);
         computer.resetHintMap();
-        List<Integer> list = List.of(2,5,3);
         for (int i =0; i<list.size(); i++){
             computer.checkPlayerAnswer(Answer.get(i), list.get(i));
         }
@@ -52,20 +48,17 @@ public class ComputerTest {
 
     @Test
     void makeHintMap_플레이어의_답안에_맞춘_HintList_갱신(){
-        Computer computer = new Computer();
         computer.Answer = List.of(1,2,3);
         computer.resetHintMap();
-        List<Integer> list = List.of(2,5,3);        computer.makeHintMap(list);
+        computer.makeHintMap(list);
         assertThat(HintMap.get("스트라이크")).isEqualTo(1);
         assertThat(HintMap.get("볼")).isEqualTo(1);
     }
 
     @Test
     void giveResult_플레이어에게_힌트_출력(){
-        Computer computer = new Computer();
         computer.Answer = List.of(1,2,3);
         computer.resetHintMap();
-        List<Integer> list = List.of(2,5,3);
         computer.makeHintMap(list);
         computer.giveHint();
         assertThat(computer.result).isEqualTo("1볼 1스트라이크");
@@ -73,11 +66,10 @@ public class ComputerTest {
 
     @Test
     void is3Strike_스트라이크가_3개_인지_판별(){
-        Computer computer = new Computer();
         computer.Answer = List.of(1,2,3);
         computer.resetHintMap();
-        List<Integer> list = List.of(1,2,3);
-        computer.makeHintMap(list);
+        List<Integer> list1 = List.of(1,2,3);
+        computer.makeHintMap(list1);
         boolean result = computer.is3Strike();
         assertThat(result).isEqualTo(true);
     }
