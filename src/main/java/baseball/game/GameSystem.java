6dbe096e7e7a941c084721cd50ff72de98d3cd95
Basak <chgy2131@naver.com>
2022-11-08 17:05:11 +0900

    feat(GameSystem): 게임을 진행하는 클래스 생성
    - 게임에 정답으로 사용될 숫자를 초기화하는 메소드
    - 사용자에게 추측 숫자를 입력받아 결과를 반환하는 메소드
    - 추측 결과가
    정답인지 확인하는 메소드
    - 추측 결과를 정해진 포맷으로 출력하는 메소드
    - 정답이후에 계속 진행할지 사용자에게 입력을 받는
    메소드

--- src/main/java/baseball/game/GameSystem.java
+++ src/main/java/baseball/game/GameSystem.java
@@ -1,4 +1,65 @@
 package baseball.game;
 
+import baseball.domain.BaseballNumberBundle;
+import baseball.domain.GameProgress;
+import baseball.domain.GameResult;
+import baseball.domain.GameState;
+
 public class GameSystem {
+    Computer computer;
+    Player player;
+
+    public GameSystem(Computer computer, Player player) {
+        this.computer = computer;
+        this.player = player;
+    }
+    public void initialize() {
+        computer.setRandomVictoryNumbers();
+    }
+    public GameResult playGame() {
+        BaseballNumberBundle pickedNumbers = player.inputBaseballNumbers();
+        return computer.checkAnswer(pickedNumbers);
+    }
+    public boolean receivedCorrectAnswer(GameResult result) {
+        GameState state = result.getState();
+        return state == GameState.CORRECT;
+    }
+    public String printResult(GameResult result) {
+        String sentence = "";
+        GameState state = result.getState();
+        switch (state) {
+            case NOTHING:
+                sentence = "낫싱";
+                break;
+            case GOOD:
+                sentence = makeStrikeBallSentence(result.getStrike(), result.getBall());
+                break;
+            case CORRECT:
+                sentence = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+                break;
+        }
+        System.out.println(sentence);
+        return sentence;
+    }
+    private String makeStrikeBallSentence(int strike, int ball) {
+        String sentence = "";
+        if(strike == 0) {
+            sentence = ball + "볼";
+        } else if(ball == 0) {
+            sentence = strike + "스트라이크";
+        } else {
+            sentence = ball + "볼 " + strike + "스트라이크";
+        }
+        return sentence;
+    }
+    public boolean continueGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        GameProgress gameProgress = player.inputGameProgress();
+        if(gameProgress == GameProgress.CONTINUE) {
+            return true;
+        } else if(gameProgress == GameProgress.STOP) {
+            return false;
+        }
+        return false;
+    }
 }
