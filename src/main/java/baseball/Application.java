93a4e8f2f9d896a4189ed779f9408f75e18281c0
Jenn Kim <grdd0419@gmail.com>
2022-11-08 18:30:58 +0900

    refactor(continueOrExit): modify codes to use the value of the constant class -- NUMBER_SIZE = 3

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -122,7 +122,7 @@ public class Application {
 
     public static boolean continueOrExit() throws IllegalArgumentException {
 
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println(Constants.NUMBER_SIZE + "개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
         int userChoice = userStringToInteger(Console.readLine());
