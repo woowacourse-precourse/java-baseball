d106de277e264ecc95b5bd5a100de55b1088f765
HyowonSin <tlsgydnjs@naver.com>
2022-11-08 21:21:10 +0900

    fix($restartGame): change method name restartGame to exitGame
    Change method name:
    - restartGame -> exitGame
    - modify comment content

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -66,15 +66,15 @@ public class Application {
     public static void compareNumbers(int[] computerNumbers, int[] userNumbers) {
        if (computerNumbers[0] == userNumbers[0] && computerNumbers[1] == userNumbers[1] && computerNumbers[2] == userNumbers[2]) {
            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-           restartGame();
+           exitGame();
        }
        else {
            printHint(computerNumbers, userNumbers);
        }
     }
 
-    // 게임 재시작 물어보기
-    public static void restartGame() {
+    // 게임 종료
+    public static void exitGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String userInput = new java.util.Scanner(System.in).nextLine();
         if (userInput.equals("1")) {
@@ -83,7 +83,7 @@ public class Application {
             System.out.println("게임을 종료합니다.");
         } else {
             System.out.println("1 또는 2를 입력해주세요.");
-            restartGame();
+            exitGame();
         }
     }
 
