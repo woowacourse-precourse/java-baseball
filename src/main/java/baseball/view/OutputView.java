8075855fb84b95458bd11860925a24e6bf01595a
Jung <anes53027@gmail.com>
2022-11-08 22:41:10 +0900

    feat(OutputView) : gameRestartOrStop 정답이 나오면 게임을 계속할 것인지 멈출 것인지 결정하는 함수 생성

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,10 @@
 package baseball.view;
 
+import baseball.domain.Game;
+import camp.nextstep.edu.missionutils.Console;
+
+import static baseball.exception.InputException.isValidRestartOrStop;
+
 public class OutputView {
     public static String gameResult(int ball, int strike){
         if(strike + ball == 0){
@@ -15,5 +20,27 @@ public class OutputView {
         }
 
         return ball + "볼 " + strike + "스트라이크";
+    }
+
+    public static boolean gameRestartOrStop(String result){
+        if(!result.equals("3스트라이크")) return false;
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String input = Console.readLine();
+
+        if(!isValidRestartOrStop(input)){
+            throw new IllegalArgumentException();
+        }
+
+        int restartOrStop = Integer.parseInt(input);
+
+        if(restartOrStop == 1){
+            Game baseballGame = new Game();
+            baseballGame.startGame();
+        }
+
+        return true;
     }
 }
