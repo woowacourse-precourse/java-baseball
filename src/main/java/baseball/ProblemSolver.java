7e606d5ed28491e9612a355b4a69142fcdb61a3c
YE-EUN <musiclove529@naver.com>
2022-11-08 21:26:18 +0900

    refactor(ProblemSolver) : 메서드 수정 및 추가
    - 예외 처리 메서드 따로 만듦

--- src/main/java/baseball/ProblemSolver.java
+++ src/main/java/baseball/ProblemSolver.java
@@ -1,35 +1,43 @@
 package baseball;
 
-
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Collections;
-
-public class ProblemSolver extends Player{
-    public void solveProblem(int sizeOfProblem) throws IllegalArgumentException{
+public class ProblemSolver extends Player {
+    public void solveProblem(int sizeOfNumbers) {
         String answer = Console.readLine();
 
-        if(!answer.matches("^[1-9]{3}$")){
-            throw new IllegalArgumentException();
-        }else if(answer.chars().distinct().count() != Settings.SIZE_OF_NUMBERS){
-            throw new IllegalArgumentException();
-        }
+        checkValidAnswerInput(answer, sizeOfNumbers);
 
-        getNumbers().clear();
-        for(int i = 0;i<answer.length();i++){
+        initialize();
+
+        for (int i = 0; i < sizeOfNumbers; i++) {
             int number = Character.getNumericValue(answer.charAt(i));
             addNumber(number);
         }
-
     }
 
-    public int chooseToRestart() throws IllegalArgumentException{
-        String answer = Console.readLine();
-
-        if(!answer.matches("^[1-2]$")){
+    public void checkValidAnswerInput(String answer, int sizeOfNumbers) throws IllegalArgumentException {
+        if (!answer.matches("^[1-9]{3}$")) {
+            // 1~9 사이의 수로 이루어진 3자리 수가 아닌 경우
+            throw new IllegalArgumentException();
+        } else if (answer.chars().distinct().count() != sizeOfNumbers) {
+            // 각 자리수가 서로 다른 수가 아닌 경우
             throw new IllegalArgumentException();
         }
+    }
 
-        return Integer.parseInt(answer);
+    public String chooseToRestart() {
+        String choice = Console.readLine();
+
+        checkValidRestartChoiceInput(choice);
+
+        return choice;
+    }
+
+    public void checkValidRestartChoiceInput(String choice) throws IllegalArgumentException {
+        if (!choice.matches("^[1-2]$")) {
+            // 1 또는 2가 아닌 경우
+            throw new IllegalArgumentException();
+        }
     }
 }
