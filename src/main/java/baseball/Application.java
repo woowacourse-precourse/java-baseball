748edbe2f023358d3ee4564a442d7fd5681afc14
jisoolee11 <jisu6414@likelion.org>
2022-11-08 23:05:57 +0900

    feat: 사용자 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,15 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class Application {
     private final String correctNumber;
@@ -19,8 +25,28 @@ public class Application {
     }
 
     public void start() {
-        // 숫자야구게임 시작
-        System.out.println(correctNumber);
+        String userNumber;
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        System.out.print("숫자를 입력해주세요: ");
+        Scanner sc = new Scanner(System.in);
+        userNumber = Console.readLine();
+
+        if (validNumber(userNumber)) {
+            System.out.println("통과");
+        } else {
+            System.out.println("오류");
+        }
+    }
+
+    private boolean validNumber(String userNumber) {
+        List<String> userNumberList = new ArrayList<>(Arrays.asList(userNumber.split("")));
+        if (userNumber.length() == 3 && userNumberList.size() == userNumberList.stream().distinct().count()) {
+            return true;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
     public static void main(String[] args) {
