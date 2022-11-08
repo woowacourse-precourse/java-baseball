177472e6be5b63227929354610e9c9535d09a69b
ljh <dusk5588@naver.com>
2022-11-08 12:18:24 +0900

    refactor : 사용자 입력 저장하는 내용 User클래스로 이동

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -1,4 +1,15 @@
 package baseball.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+    List<Integer> numbers = new ArrayList<>();
+
+    void saveUserInput(String input){
+        numbers.clear();
+        for(int i=0; i<input.length(); i++){
+            this.numbers.add(Integer.parseInt(input.substring(i, i+1)));
+        }
+    }
 }
