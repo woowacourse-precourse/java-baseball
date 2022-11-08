68befdfce7557bb4a9127ec848084b9ad6a16373
geonho <soo_1992@naver.com>
2022-11-08 23:35:54 +0900

    feat: 컴퓨터의 숫자 생성 기능

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    List<Integer> numbersPickedByComputer;
+
+    public void computerSelectThreeNumbers() {
+        numbersPickedByComputer = new ArrayList<>();
+        while (numbersPickedByComputer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!numbersPickedByComputer.contains(randomNumber)) {
+                numbersPickedByComputer.add(randomNumber);
+            }
+        }
+    }
+}
