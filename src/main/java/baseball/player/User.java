5d4a1fe358f442eddb511829b0468180dc1ee378
ddukddaki <rlfrkdms1@naver.com>
2022-11-08 15:04:19 +0900

    feat(User): 숫자(String) 입력받아 List로 변환하는 기능

--- src/main/java/baseball/player/User.java
+++ src/main/java/baseball/player/User.java
@@ -1,8 +1,13 @@
 package baseball.player;
 
+import static baseball.Constant.ERR_MSG;
+
 import baseball.Validation;
+import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class User {
 
@@ -12,5 +17,16 @@ public class User {
     public User(Validation validation) {
         this.validation = validation;
     }
-    
+
+    public List<Integer> makeStringNumberToList() {
+        try {
+            number = Arrays.stream(Console.readLine().split(""))
+                    .mapToInt(Integer::parseInt)
+                    .boxed().collect(Collectors.toList());
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ERR_MSG);
+        }
+        return number;
+    }
+
 }
