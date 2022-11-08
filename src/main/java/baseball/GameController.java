b217216813e07989567d51122b55d55561749848
yoon <beatus96@naver.com>
2022-11-08 12:54:53 +0900

    refactor(controller) : 상수 수정 + static멤버를 인스턴스 멤버로 변경
    수정 이유: 인스턴스 여러개 생성 시, 공유하는 값이 아니기 때문이다
    수정 이후 :
     - static 변수에서 인스턴스 변수로 수정
     - 관련 함수인 restartOrEnd() 메서드도 static이 아닌 인스턴스 변수로 바꾸었다.
    * 나머지 변수를 static으로 둔 이유 : 모든 인스턴스에서 공유하는 값이기 때문이다.

--- src/main/java/baseball/GameController.java
+++ src/main/java/baseball/GameController.java
@@ -5,10 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static baseball.GameRule.DIGITS;
+import static baseball.GameRule.COMMAND_RESTART;
+import static baseball.GameRule.COMMAND_END;
 public class GameController {
+    private List<Integer> computerNumber;
+    private boolean isPlaying = true;
 
-    private static List<Integer> computerNumber;
-    private static boolean isPlaying = true;
     private static final String STRIKE ="strike";
     private static final String STRIKE_KOR ="스트라이크";
     private static final String BALL ="ball";
@@ -16,46 +19,36 @@ public class GameController {
     private static final String NOTHING ="nothing";
     private static final String NOTHING_KOR ="낫싱";
 
-    private static final int DIGITS = GameRule.DIGITS.getValue();
-    private static final String COMMAND_RESTART = GameRule.COMMAND_RESTART.toString();
-    private static final String COMMAND_END = GameRule.COMMAND_END.toString();
-
     public void generate() {
-        //1. 컴퓨터 수 생성
+        View.printStart();
         computerNumber = GameModel.createComputerNumber();
         do {
-            //2.플레이어수 생성(예외)
-            List<Integer> playerNumber = GameModel.createPlayerNumber();
-            //3. 플레이어 수 비교하기
-            Map<String, Integer> resultMap = checkAnswer(computerNumber, playerNumber);
-            //4.힌트 출력하기
+            List<Integer> playerNumber = GameModel.createPlayerNumber();           //유효하지 않으면 예외발생
+            Map<String, Integer>resultMap = checkAnswer(computerNumber, playerNumber);
             View.printHint(createHint(resultMap));
-            //5.정답확인하기 (예외)
             if (isCorrect(resultMap)) {
-                String exitInput = View.getExitInput();
+                String exitInput = View.getExitInput(); //예외발생
                 restartOrEnd(exitInput);
             }
         }while(isPlaying);
     }
 
-    public static Map<String, Integer> checkAnswer(List<Integer> computerNumber, List<Integer> playerNumber) {
+    public Map<String, Integer> checkAnswer(List<Integer> computerNumber, List<Integer> playerNumber) {
         Map<String, Integer> resultMap = new HashMap<>();
-
-        for (int i = 0; i < computerNumber.size(); i++) { //세자리 수 비교
-            if (computerNumber.contains(playerNumber.get(i))) { //포함하는지
+        for (int i = 0; i < DIGITS ; i++) {
+            if (computerNumber.contains(playerNumber.get(i))) {
                 checkBallOrStrike(computerNumber.get(i), playerNumber.get(i), resultMap);
                 continue;
             }
             int oldValue = resultMap.getOrDefault(NOTHING, 0);
             resultMap.put(NOTHING, oldValue + 1);
-
         }
         return resultMap;
     }
 
-    //4-1. 볼인지 스트라이크인지 확인하기
-    public static void checkBallOrStrike(Integer computerNum, Integer playerNum, Map<String, Integer> resultMap) {
-        if (computerNum.equals(playerNum)) { //일치하는지
+    //볼인지 스트라이크인지 확인하기
+    public void checkBallOrStrike(Integer computerNum, Integer playerNum, Map<String, Integer> resultMap) {
+        if (computerNum.equals(playerNum)) {
             int oldValue = resultMap.getOrDefault(STRIKE, 0);
             resultMap.put(STRIKE, oldValue + 1);
             return;
@@ -64,7 +57,7 @@ public class GameController {
         resultMap.put(BALL, oldValue + 1);
     }
 
-    public static String createHint(Map<String, Integer> resultMap) {
+    public String createHint(Map<String, Integer> resultMap) {
         List<String> answer = new ArrayList<>();
         if (resultMap.containsKey(BALL)) {
             answer.add(String.format("%d%s", resultMap.get(BALL), BALL_KOR));
@@ -73,18 +66,18 @@ public class GameController {
             answer.add(String.format("%d%s", resultMap.get(STRIKE), STRIKE_KOR));
         }
         if (resultMap.containsKey(NOTHING)
-                && resultMap.get(NOTHING).equals(DIGITS)) {
+                && resultMap.get(NOTHING) == DIGITS) {
             answer.add(NOTHING_KOR);
         }
 
         return String.join(" ", answer).trim();
     }
 
-    public static boolean isCorrect(Map<String, Integer> resultMap) {
+    public boolean isCorrect(Map<String, Integer> resultMap) {
         return resultMap.containsKey(STRIKE) && resultMap.get(STRIKE) == DIGITS;
     }
 
-    public static void restartOrEnd(String input) {
+    public void restartOrEnd(String input) {
         if (input.equals(COMMAND_RESTART)) {
             computerNumber = GameModel.createComputerNumber();
         }
