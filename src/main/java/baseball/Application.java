e6318a3b0059b7ccd8f4987ed82ff887d191d5db
박정현 <ros28081@naver.com>
2022-11-08 19:57:02 +0900

    [FEAT] 기능구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,17 @@
 package baseball;
 
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game game = new Game();
+        while (true) {
+            game.startGame();
+            game.playGame();
+            if(game.checkFinish()) {
+                break;
+            }
+        }
     }
 }
