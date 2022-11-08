364a6c106d111e0867848a8e03d392c8e57c634b
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-08 19:52:52 +0900

    refactor(Game) : create class Game
    create a class called a game
    
    BREAKING CHANGE: readability increases and refactoring becomes easier
    memory can be reduced and generated repeatedly

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,72 +9,72 @@ import java.util.Objects;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO : 정답이면 잘 종료되는지 테스트
-        List<Integer> computer = makeRandomNumberList();
-        repeatGame(computer);
+        List<Integer> computer = Game.makeRandomNumberList();
+        Game.repeatGame(computer);
     }
 
-
-    public static List<Integer> makeRandomNumberList(){
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+    public static class Game {
+        public static List<Integer> makeRandomNumberList(){
+            List<Integer> computer = new ArrayList<>();
+            while (computer.size() < 3) {
+                int randomNumber = Randoms.pickNumberInRange(1, 9);
+                if (!computer.contains(randomNumber)) {
+                    computer.add(randomNumber);
+                }
             }
+            return computer;
         }
-        return computer;
-    }
 
-    private static List<String> inputPlayerAnswerList(){
-        System.out.println("숫자를 입력해주세요 : ");
-        String myAnswer = Console.readLine();
+        private static List<String> inputPlayerAnswerList(){
+            System.out.println("숫자를 입력해주세요 : ");
+            String myAnswer = Console.readLine();
 
-        String[] splitAnswer = myAnswer.split("");
-        ArrayList<String> answerList = new ArrayList<>(Arrays.asList(splitAnswer));
-        return answerList;
-    }
-
-    public static String checkAnswerAndGiveHint(List<Integer> computer, List<String> player){
-        int strike = 0;
-        int ball = 0;
-        for(int i = 0; i < 3; i++){
-            if (Objects.equals(computer.get(i), Integer.valueOf(player.get(i)))){
-                strike++;
-            }
-            if (!Objects.equals(computer.get(i), Integer.valueOf(player.get(i))) && computer.contains(Integer.valueOf(player.get(i)))){
-                ball++;
-            }
+            String[] splitAnswer = myAnswer.split("");
+            ArrayList<String> answerList = new ArrayList<>(Arrays.asList(splitAnswer));
+            return answerList;
         }
-        int nothing = (3 - strike - ball);
 
-        if (nothing != 3){
-            if (ball == 0) {
-                return (strike + "스트라이크");
+        public static String checkAnswerAndGiveHint(List<Integer> computer, List<String> player){
+            int strike = 0;
+            int ball = 0;
+            for(int i = 0; i < 3; i++){
+                if (Objects.equals(computer.get(i), Integer.valueOf(player.get(i)))){
+                    strike++;
+                }
+                if (!Objects.equals(computer.get(i), Integer.valueOf(player.get(i))) && computer.contains(Integer.valueOf(player.get(i)))){
+                    ball++;
+                }
             }
-            else if (strike == 0) {
-                return (ball + "볼 ");
+            int nothing = (3 - strike - ball);
+
+            if (nothing != 3){
+                if (ball == 0) {
+                    return (strike + "스트라이크");
+                }
+                else if (strike == 0) {
+                    return (ball + "볼 ");
+                }
+                else {
+                    return (ball + "볼 " + strike + "스트라이크");
+                }
             }
             else {
-            return (ball + "볼 " + strike + "스트라이크");
+                return ("낫싱");
             }
         }
-        else {
-           return ("낫싱");
-        }
-    }
 
-    public static String repeatGame(List<Integer> computer) {
-        boolean flag = true;
-        while (flag){
-            List<String> player = inputPlayerAnswerList();
-            String hint = checkAnswerAndGiveHint(computer, player);
-            System.out.println(hint);
-            if (hint.contains("3스트라이크")){
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                flag = false;
+        public static String repeatGame(List<Integer> computer) {
+            boolean flag = true;
+            while (flag){
+                List<String> player = inputPlayerAnswerList();
+                String hint = checkAnswerAndGiveHint(computer, player);
+                System.out.println(hint);
+                if (hint.contains("3스트라이크")){
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    flag = false;
+                }
             }
+            return "종료";
         }
-        return "종료";
     }
 }
--- src/test/java/Implement/FunctionTest.java
+++ src/test/java/Implement/FunctionTest.java
@@ -16,13 +16,13 @@ public class FunctionTest extends NsTest {
 
     @Test
     void 리스트에_3자리_생성() {
-        List<Integer> randomNumber = Application.makeRandomNumberList();
+        List<Integer> randomNumber = Application.Game.makeRandomNumberList();
         assertThat(randomNumber.size() == 3);
     }
 
     @Test
     void 서로_다른_수_3자리를_1부터_9까지_랜덤으로_생성() {
-        List<Integer> randomNumber = Application.makeRandomNumberList();
+        List<Integer> randomNumber = Application.Game.makeRandomNumberList();
         assertNotEquals(randomNumber.get(0), randomNumber.get(1));
         assertNotEquals(randomNumber.get(0), randomNumber.get(2));
         assertNotEquals(randomNumber.get(1), randomNumber.get(2));
@@ -32,7 +32,7 @@ public class FunctionTest extends NsTest {
     void 힌트_출력_여부_낫싱_제외() {
         List<Integer> computer = List.of(1, 2, 3);
         List<String> player = List.of("1", "3", "4");
-        String giveHint = Application.checkAnswerAndGiveHint(computer,player);
+        String giveHint = Application.Game.checkAnswerAndGiveHint(computer,player);
         assertThat(giveHint).isEqualTo("1볼 1스트라이크");
     }
 
@@ -40,7 +40,7 @@ public class FunctionTest extends NsTest {
     void 힌트_출력_여부_낫싱() {
         List<Integer> computer = List.of(1, 2, 3);
         List<String> player = List.of("4", "5", "6");
-        String giveHint = Application.checkAnswerAndGiveHint(computer,player);
+        String giveHint = Application.Game.checkAnswerAndGiveHint(computer,player);
         assertThat(giveHint).isEqualTo("낫싱");
     }
 
