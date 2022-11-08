5420ede312158615532b9d9fb0d40a33e43b922b
Dongdong351 <dolong014@gmail.com>
2022-11-08 22:16:46 +0900

    feat: pickRandomNumber

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Application {
+    private List<Integer> computer;
+
+    public Application() {
+        this.computer = new ArrayList<>();
+    }
+
+    public void pickRandomNumber() {
+        while (this.computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!this.computer.contains(randomNumber)) {
+                this.computer.add(randomNumber);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application app = new Application();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+
+
+
     }
 }
