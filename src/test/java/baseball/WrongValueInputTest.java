0bd7bc86acef2f4af7b8c35262ae13fd5dbd6e32
JeongHunHui <deca3248@gmail.com>
2022-11-08 17:58:22 +0900

    rename: 클래스 이름 및 메소드 명 변경

--- src/test/java/baseball/WrongValueInputTest.java
+++ src/test/java/baseball/WrongValueInputTest.java
@@ -4,31 +4,31 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class NumberInputValidCheckTest {
+public class WrongValueInputTest {
     @Test
     void 문자열입력시_예외발생(){
-        wrongNumberInputTest("가나다");
-        wrongNumberInputTest("   ");
+        checkWrongValueInput("가나다");
+        checkWrongValueInput("   ");
     }
     @Test
     void 잘못된_자리수_입력시_예외발생(){
-        wrongNumberInputTest("1234");
-        wrongNumberInputTest("1");
+        checkWrongValueInput("1234");
+        checkWrongValueInput("1");
     }
     @Test
     void 범위를_벗어나는_수_입력시_예외발생(){
-        wrongNumberInputTest("102");
-        wrongNumberInputTest("013");
-        wrongNumberInputTest("120");
+        checkWrongValueInput("102");
+        checkWrongValueInput("013");
+        checkWrongValueInput("120");
     }
     @Test
     void 중복되는_숫자_입력시_예외발생(){
-        wrongNumberInputTest("121");
-        wrongNumberInputTest("211");
-        wrongNumberInputTest("112");
+        checkWrongValueInput("121");
+        checkWrongValueInput("211");
+        checkWrongValueInput("112");
     }
 
-    private void wrongNumberInputTest(String inputString){
+    private void checkWrongValueInput(String inputString){
         GameNumber gameNumber = new GameNumber();
         assertThrows(IllegalArgumentException.class, () -> {
             gameNumber.stringToGameNumber(inputString);
