f1f5cddb5a7d674eaa55747b589f27f47ddade7a
sun007021 <sun007021@gmail.com>
2022-11-08 14:21:48 +0900

    feat(Domain : User) : 숫자 입력받고 저장

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -1,8 +1,13 @@
 package baseball.domain;
 
+import baseball.view.InputView;
+
 public class User {
     private BaseballNumbers baseballNumbers; // user 입력값
     private int restartOrStop;
 
+    public void inputBaseballNumbers(){
+        baseballNumbers = InputView.inputBaseballNumbers();
+    }
 
 }
