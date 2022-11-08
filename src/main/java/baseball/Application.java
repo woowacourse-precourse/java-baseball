d8bd973a88b4f7952191355721c980e0c4138b53
HangSub999 <gkdtjq999@gmail.com>
2022-11-08 16:38:40 +0900

    feat: 예외처리,메인 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,14 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            BaseBallGameManager baseBallGameManager = new BaseBallGameManager();
+            baseBallGameManager.startGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("예기치 못한 오류 발생");
+        }
     }
 }
+
