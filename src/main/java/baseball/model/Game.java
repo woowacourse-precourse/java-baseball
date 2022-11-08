56ce51ca47377bf6ed7c81c36725e3c3e2765d75
mihye126 <mihea12@gmail.com>
2022-11-08 22:18:00 +0900

    game application

--- src/main/java/baseball/model/Game.java
+++ src/main/java/baseball/model/Game.java
@@ -25,10 +25,30 @@ public class Game {
     private void attack() {
         judge.setAttack(player.attack(askAttackNumber()));
     }
+
     private String askAttackNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         return readLine(GameCode.ATTACK.getValue());
     }
+
+    private void finish() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        askGameContinue();
+    }
+
+    private void askGameContinue() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. : ");
+        status = Integer.parseInt(readLine(GameCode.CONTINUE.getValue()));
+
+        if (status == GameCode.PLAY.getValue()) {
+            updateGame();
+        }
+    }
+
+    private void updateGame() {
+        this.judge.setDefend(player.defend());
+    }
+
 
     public void play() {
 
@@ -37,6 +57,14 @@ public class Game {
         String gameResult = judge.judge();
         System.out.println(gameResult);
 
+        if (gameResult.equals("3스트라이크")) {
+            finish();
+        }
     }
+
+    public int getStatus() {
+        return status;
+    }
+
 
 }
