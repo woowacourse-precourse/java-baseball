6d34ead900712df8c9d0bb524796be53d72d7e6d
younghyun-na <younghyunna@naver.com>
2022-11-08 22:25:05 +0900

    :white_check_mark: test: 사용자의 입력 값 검증 테스트 통과

--- src/test/java/study/MyTest.java
+++ src/test/java/study/MyTest.java
@@ -1,6 +1,7 @@
 package study;
 
 import baseball.Hint;
+import baseball.Player;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -12,12 +13,13 @@ public class MyTest {
 
   @Test
   void 힌트_출력() {
+    // given
     List<Integer> players1 = Arrays.asList(1,5,3);
     List<Integer> players2 = Arrays.asList(5,1,3);
     List<Integer> players3 = Arrays.asList(2,4,6);
-
     List<Integer> computers = Arrays.asList(1,3,5);
 
+    // when
     Hint hint1 = new Hint(players1, computers);
     Hint hint2 = new Hint(players2, computers);
     Hint hint3 = new Hint(players3, computers);
@@ -25,5 +27,19 @@ public class MyTest {
     assertThat(hint1.getHint()).isEqualTo("2볼 1스트라이크");
     assertThat(hint2.getHint()).isEqualTo("3볼");
     assertThat(hint3.getHint()).isEqualTo("낫싱");
+  }
+
+ @Test
+ void 입력값_검증_및_출력() {
+   // given
+   String userInput ="468";
+   List<Integer> players = Arrays.asList(4,6,8);
+
+   // when
+   Player player = new Player();
+   player.setNumbers(userInput);
+
+   // then
+   assertThat(player.getNumbers()).isEqualTo(players);
   }
 }
