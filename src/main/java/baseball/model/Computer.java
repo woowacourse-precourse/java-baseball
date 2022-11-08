8cb52f51a51b1314b374e62f31ee3c4086b782f0
hongjinwon <herenever@naver.com>
2022-11-08 16:45:39 +0900

    feat(model): Computer
    model/Computer에 난수 생성 기능 구현

---
+++ src/main/java/baseball/model/Computer.java
@@ -0,0 +1,20 @@
+package baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    public List<Integer> generateRandomNumbers(){
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
+    }
+}
