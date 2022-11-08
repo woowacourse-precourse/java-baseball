0f29f6c3c27ee80db5c697fa3443ca616127ad7d
can019 <jys01012@gmail.com>
2022-11-08 09:15:07 +0900

    refactor(failTest): Separate incorrect input test case
    For readability.

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -20,33 +20,49 @@ class ApplicationTest extends NsTest {
         );
     }
 
+
     @Test
-    void 예외_테스트() {
+    void 예외_테스트_입력이_3자리가_아닌_경우_1() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
 
+    @Test
+    void 예외_테스트_입력을_int로_변환이_불가능한_경우_1() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("a12"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
 
+    @Test
+    void 예외_테스트_입력을_int로_변환이_불가능한_경우_2() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("abc"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
 
+    @Test
+    void 예외_테스트_입력에_0이_포함된_경우_1() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("012"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
 
+    @Test
+    void 예외_테스트_입력에_0이_포함된_경우_2() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("102"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
 
+    @Test
+    void 예외_테스트_입력에_0이_포함된_경우_3() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("120"))
                         .isInstanceOf(IllegalArgumentException.class)
