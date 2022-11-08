b3bbe6e5e16cc2ee2c87ec8a06b76c8fe8c949f0
Basak <chgy2131@naver.com>
2022-11-08 18:28:31 +0900

    feat(Application): 숫자 야구 게임 실행문 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,43 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
+import baseball.domain.GameResult;
+import baseball.game.Computer;
+import baseball.game.GameSystem;
+import baseball.game.Player;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Computer computer = new Computer();
+        Player player = new Player();
+        GameSystem game = new GameSystem(computer, player);
+
+        while(true) {
+            game.initialize();
+            choosing(game);  // 야구 숫자맞추기를 진행
+//            boolean correct = false;
+//            while(!correct) {  // 정답을 맞출때까지 계속 진행
+//                GameResult result = game.playGame();
+//                game.printResult(result);
+//                if(game.receivedCorrectAnswer(result)) {
+//                    // 정답인 경우
+//                    correct = true;
+//                }
+//            }
+            if (!game.continueGame()) {  // 정답을 맞춘 후 한판 더 할건지 확인
+                break;
+            }
+        }
+    }
+
+    private static void choosing(GameSystem game) {
+        boolean correct = false;
+        while(!correct) {  // 정답을 맞출때까지 계속 진행
+            GameResult result = game.playGame();
+            game.printResult(result);
+            if(game.receivedCorrectAnswer(result)) {
+                // 정답인 경우
+                correct = true;
+            }
+        }
     }
 }
