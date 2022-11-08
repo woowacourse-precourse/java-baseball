a63bb51c221fd99c19b136fad48995f2ba3e46af
조병우 <whquddn55@gmail.com>
2022-11-08 16:09:59 +0900

    feature(BaseBallGame): implement running whole game method.
    This method run game with whole logic. Read user's number, generate computer's number, check score, read whether re-game in sequence and looped.
    
    + public BaseballGame(Player, Computer, OutputGenerator, BallNumbercomparer)
    + public void run()
    + private void initPhase()
    + private Score runOneRount()
    + private boolean runEndPhase()
    + private isGameEnd(Score)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import baseball.domain.Computer;
+import baseball.domain.Player;
+import baseball.utils.BallNumberComparer;
+import baseball.utils.InputValidator;
+import baseball.utils.OutputGenerator;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Player player = new Player(new InputValidator());
+        Computer computer = new Computer();
+        OutputGenerator outputGenerator = new OutputGenerator();
+        BallNumberComparer ballNumberComparer = new BallNumberComparer();
+        BaseballGame baseballGame = new BaseballGame(player, computer, outputGenerator, ballNumberComparer);
+        baseballGame.run();
     }
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,65 @@
 package baseball;
 
+import baseball.domain.Computer;
+import baseball.domain.Player;
+import baseball.domain.Score;
+import baseball.utils.BallNumberComparer;
+import baseball.utils.OutputGenerator;
+
 public class BaseballGame {
     public static final int INPUT_SIZE = 3;
+    private final Player player;
+    private final Computer computer;
+    private final OutputGenerator outputGenerator;
+    private final BallNumberComparer ballNumberComparer;
+
+    public BaseballGame(Player player, Computer computer, OutputGenerator outputGenerator, BallNumberComparer ballNumberComparer) {
+        this.player = player;
+        this.computer = computer;
+        this.outputGenerator = outputGenerator;
+        this.ballNumberComparer = ballNumberComparer;
+    }
+
+    public void run() throws IllegalArgumentException {
+        runInitPhase();
+        while (true) {
+            Score score = runOneRound();
+            if (isGameEnd(score)) {
+                break;
+            }
+        }
+    }
+
+    private void runInitPhase() {
+        outputGenerator.printWelcome();
+        computer.generateComputerNumber();
+    }
+
+    private Score runOneRound() {
+        outputGenerator.printRequestBallNumber();
+        player.readBallNumber();
+
+        Score score = ballNumberComparer.calculateScore(player.getBallNumber(), computer.getBallNumber());
+        outputGenerator.printScore(score);
+        return score;
+    }
+
+    private boolean runEndPhase() {
+        outputGenerator.printGameEnd();
+        boolean ret = false;
+        if (player.readWeatherReGame()) {
+            computer.generateComputerNumber();
+        } else {
+            ret = true;
+        }
+        return ret;
+    }
+
+    private boolean isGameEnd(Score score) {
+        boolean ret = false;
+        if (score.getStrike() == INPUT_SIZE) {
+            ret = runEndPhase();
+        }
+        return ret;
+    }
 }
