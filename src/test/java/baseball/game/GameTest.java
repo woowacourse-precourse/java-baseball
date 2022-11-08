810da7ae6b08bd51e070fc2d94b6426d3e802d77
gomljo <gomljo0307@naver.com>
2022-11-08 17:19:10 +0900

    test(GameTest.java): 스트라이크를 카운트하는 기능 검증
    숫자 리스트 내 스트라이크 조건에 만족하는 숫자들의 갯수를 집계하는 기능 검증

--- src/test/java/baseball/game/GameTest.java
+++ src/test/java/baseball/game/GameTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,5 +29,86 @@ public class GameTest {
         System.setIn(in);
         player.createNumbers();
         assertThat(player.getDigits()).isNotEmpty();
+    }
+    @Test
+    public void count_One_Strike_Test(){
+        int strikeCount=0;
+        int oneStrike=1;
+        List<Integer> computerNumbers;
+        Player player = new Player();
+        Player computer = new Player();
+
+        String computerPick="456";
+        InputStream inComputer = generateUserInput(computerPick);
+        System.setIn(inComputer);
+        computer.createNumbers();
+        computerNumbers=computer.getDigits();
+
+        String userInput="465";
+        InputStream inPlayer = generateUserInput(userInput);
+        System.setIn(inPlayer);
+        player.createNumbers();
+
+        for(int computerNumber:computerNumbers){
+            if(player.getNumberPosition(computerNumber)==computer.getNumberPosition(computerNumber)){
+                strikeCount+=1;
+            }
+        }
+
+        assertThat(strikeCount).isEqualTo(oneStrike);
+    }
+    @Test
+    public void count_Two_Strike_Test(){
+        int strikeCount=0;
+        int twoStrike=2;
+        List<Integer> computerNumbers;
+        Player player = new Player();
+        Player computer = new Player();
+
+        String computerPick="456";
+        InputStream inComputer = generateUserInput(computerPick);
+        System.setIn(inComputer);
+        computer.createNumbers();
+        computerNumbers=computer.getDigits();
+
+        String userInput="457";
+        InputStream inPlayer = generateUserInput(userInput);
+        System.setIn(inPlayer);
+        player.createNumbers();
+
+        for(int computerNumber:computerNumbers){
+            if(player.getNumberPosition(computerNumber)==computer.getNumberPosition(computerNumber)){
+                strikeCount+=1;
+            }
+        }
+
+        assertThat(strikeCount).isEqualTo(twoStrike);
+    }
+    @Test
+    public void count_Three_Strike_Test(){
+        int strikeCount=0;
+        int threeStrike=3;
+        List<Integer> computerNumbers;
+        Player player = new Player();
+        Player computer = new Player();
+
+        String computerPick="456";
+        InputStream inComputer = generateUserInput(computerPick);
+        System.setIn(inComputer);
+        computer.createNumbers();
+        computerNumbers=computer.getDigits();
+
+        String userInput="456";
+        InputStream inPlayer = generateUserInput(userInput);
+        System.setIn(inPlayer);
+        player.createNumbers();
+
+        for(int computerNumber:computerNumbers){
+            if(player.getNumberPosition(computerNumber)==computer.getNumberPosition(computerNumber)){
+                strikeCount+=1;
+            }
+        }
+
+        assertThat(strikeCount).isEqualTo(threeStrike);
     }
 }
