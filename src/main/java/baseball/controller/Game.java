79c418c8c22668429d73579af6a5506792953bc9
saehim <saeilee.vxv@gmail.com>
2022-11-08 22:35:43 +0900

    Feat: 클래스 분리(Game -> Number) 입력 숫자 관리 메서드

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -2,12 +2,11 @@ package baseball.controller;
 
 import baseball.domain.Baseball;
 import baseball.view.Printer;
-import camp.nextstep.edu.missionutils.Console;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static baseball.controller.Computer.createRandomNums;
+import static baseball.controller.Number.*;
 import static baseball.exception.BaseballException.*;
 import static baseball.view.Printer.*;
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -49,51 +48,6 @@ public class Game {
         } else {
             startRound(answer);
         }
-    }
-
-    private static int getInputNums() {
-        showRequestInput();
-        int nums = 0;
-        try {
-            nums = Integer.parseInt(readLine());
-        } catch (NumberFormatException e) {
-            // 0인채로 리턴
-        }
-        return nums;
-    }
-
-    /** 사용자 입력값을 정수 ArrayList 반환 */
-    private static List<Integer> getNumsArr(int num) {
-        List<Integer> numsArr = Arrays.asList(0,0,0);
-
-        vaildNum(num);
-
-        for (int i=0; i<numsArr.size(); i++){
-            numsArr.set(2 - i, num % 10);
-            num = num/10;
-        }
-        return numsArr;
-    }
-
-    private static Baseball checkAnswer(List<Integer> answer, List<Integer> input) {
-        Baseball result = new Baseball();
-        // strike 판별
-        for (int i=0; i<answer.size(); i++) {
-            if(answer.get(i) == input.get(i)) {
-                result.strike();
-            } else if (answer.contains(input.get(i))) {
-                result.ball();
-            }
-        }
-        return result;
-    }
-
-    // 모두 맞춘 경우
-    public static boolean clear(Baseball game) {
-        if (game.getStrike()==3) {
-            return true;
-        }
-        return false;
     }
 
     public static boolean getShutDownCheck() {
---
+++ src/main/java/baseball/controller/Number.java
@@ -0,0 +1,59 @@
+package baseball.controller;
+
+import baseball.domain.Baseball;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static baseball.exception.BaseballException.vaildNum;
+import static baseball.view.Printer.showRequestInput;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class Input {
+
+    /** 사용자로부터 숫자를 받는 함수 */
+    public static int getInputNums() {
+        showRequestInput();
+        int nums = 0;
+        try {
+            nums = Integer.parseInt(readLine());
+        } catch (NumberFormatException e) {
+            // 0인채로 리턴
+        }
+        return nums;
+    }
+
+    /** 사용자 입력값을 정수 ArrayList 반환 */
+    static List<Integer> getNumsArr(int num) {
+        List<Integer> numsArr = Arrays.asList(0,0,0);
+
+        vaildNum(num);
+
+        for (int i=0; i<numsArr.size(); i++){
+            numsArr.set(2 - i, num % 10);
+            num = num/10;
+        }
+        return numsArr;
+    }
+
+    static Baseball checkAnswer(List<Integer> answer, List<Integer> input) {
+        Baseball result = new Baseball();
+        // strike 판별
+        for (int i=0; i<answer.size(); i++) {
+            if(answer.get(i) == input.get(i)) {
+                result.strike();
+            } else if (answer.contains(input.get(i))) {
+                result.ball();
+            }
+        }
+        return result;
+    }
+
+    // 모두 맞춘 경우
+    public static boolean clear(Baseball game) {
+        if (game.getStrike()==3) {
+            return true;
+        }
+        return false;
+    }
+}
