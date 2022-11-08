5520bf19c3dce7bd418159f725b4668c991712e7
kimdoha <kkhhjj3393@gmail.com>
2022-11-08 15:29:11 +0900

    feat(Number): 테스트 요구사항 확인을 위한 Number 생성자와 검증 메소드 추가

--- src/main/java/baseball/domain/number/Number.java
+++ src/main/java/baseball/domain/number/Number.java
@@ -3,13 +3,30 @@ package baseball.domain.number;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Number {
+  public static final String NUMBER_ERROR_MESSAGE = "각 자리 숫자는 1에서 9사이어야 합니다.";
   private static final Integer START_INCLUSIVE = 1;
   private static final Integer END_INCLUSIVE = 9;
 
   private Integer number;
 
   public Number() {
-    this.number = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+    int randomNumber = Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+    validateNumber(randomNumber);
+    this.number = randomNumber;
+  }
+
+  /*
+  * [TEST] 1 ~ 9 사이의 숫자가 아닐 때 에러 발생 확인 생성자
+  */
+  public Number(Integer number) {
+    validateNumber(number);
+    this.number = number;
+  }
+
+  private void validateNumber(int number) {
+    if(number < 1 || number > 9){
+      throw new IllegalArgumentException(NUMBER_ERROR_MESSAGE);
+    }
   }
 
   public Integer getNumber(){
