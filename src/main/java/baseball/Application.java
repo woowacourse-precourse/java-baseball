7fe6e018a844ade1726d7d55bb4e81e64c1b240b
can019 <jys01012@gmail.com>
2022-11-08 09:43:51 +0900

    feat(validateInput): Implement 3 digit input validate

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static final int RANDOM_NUMBER_DIGIT_SIZE = 3;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input = getInput();
+        validateInput(input);
+
+    }
+
+    public static void validateInput(String input) {
+        if (input.length() != 3)
+            throw new IllegalArgumentException();
+        if (!isNumeric(input))
+            throw new IllegalArgumentException();
+        if (input.contains("0"))
+            throw new IllegalArgumentException();
+    }
+
+    private static boolean isNumeric(String str) {
+        return str != null && str.matches("[0-9.]+");
+    }
+
+    private static String getInput() {
+        return Console.readLine();
     }
 }
