27e887c6b45ae60e80d0d407550c738b19d29dd4
moonn6pence <shin421179@gmail.com>
2022-11-08 16:16:28 +0900

    fix : Game 클래스 수정
    - 의존관계 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -32,8 +32,8 @@ public class Game {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        replayNumber.generateSecretDigits();
-        secretDigits = replayNumber.getSecretDigits();
+        secretNumber.generateSecretDigits();
+        secretDigits = secretNumber.getSecretDigits();
 
         while (isGameFinished()) {
             System.out.print("숫자를 입력해주세요 : ");
