8bf58d97020392ca8e08b6990506ee2e0dbcab7e
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-08 15:38:08 +0900

    Model 추가

---
+++ src/main/java/baseball/Model.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class Model {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+}
