02ee29430f79a3f03b55e95d241abcab430d93e3
Minsoo Choi <lucas0209@yonsei.ac.kr>
2022-11-08 16:51:45 +0900

    Fix($User): Fix access modifier of the features
    Features modified:
    - Changed private -> public to test each features

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -29,7 +29,7 @@ public class User {
     }
 
     // String -> List 변환
-    private List<Integer> convertStrToList(String input) {
+    public List<Integer> convertStrToList(String input) {
         List<Integer> result = new ArrayList<>();
         for (int i = 0; i < LEN_NUM; i++) {
             result.add((Integer.valueOf(input.substring(i, i + 1))));
@@ -38,7 +38,7 @@ public class User {
     }
 
     // 각 자리 숫자 1-9 이외의 잘못된 입력 처리
-    private boolean checkInvalid(String input) {
+    public boolean checkInvalid(String input) {
         if (input.length() != LEN_NUM) {
             return true;
         }
@@ -53,7 +53,7 @@ public class User {
         return false;
     }
 
-    private boolean isDuplicate(String input) {
+    public boolean isDuplicate(String input) {
         String first = input.substring(0, 1);
         String sec = input.substring(1, 2);
         String third = input.substring(2, 3);
@@ -65,7 +65,7 @@ public class User {
     }
 
     // 각 자리 수가 숫자인지 판별
-    private boolean isDigitNumber(String input) {
+    public boolean isDigitNumber(String input) {
         for (int i = 0; i < input.length(); i++) {
             String digit = input.substring(i,i+1);
             if (!digit.matches("-?\\d+(\\.\\d+)?")) {
