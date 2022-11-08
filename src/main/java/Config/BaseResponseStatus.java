63c8090c19496150c898a174a5e5997901b110d7
benjaminuj <chosj1526@gmail.com>
2022-11-08 20:52:14 +0900

    feat: 정답 생성하는 기능
    정답은 세자리수이다.
    각 자리의 숫자는 1부터 9까지의 수이다.
    각 자리의 숫자는 서로 다른수로 이루어져있다.
    정답 생성 조건을 만족하지 않는 경우, 만족할때까지 정답을 재생성한다.

---
+++ src/main/java/Config/BaseResponseStatus.java
@@ -0,0 +1,13 @@
+package Config;
+
+public class BaseResponseStatus {
+    public static final String NUMERIC_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String RESULT_BALL = "볼";
+    public static final String RESULT_STRIKE = "스트라이크";
+    public static final String RESULT_NOTHING = "낫싱";
+    public static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_OR_TERMINATE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final int NUMBER_LENGTH = 3;
+    public static final int START_OF_NUMBER = 1;
+    public static final int END_OF_NUMBER = 9;
+}
---
+++ src/main/java/baseball/ComputerOfGame.java
@@ -0,0 +1,35 @@
+package baseball;
+import static Config.BaseResponseStatus .*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Randoms;
+public class ComputerOfGame {
+    private List<Integer> answer;
+
+    public ComputerOfGame() {
+        this.answer = new ArrayList<>(NUMBER_LENGTH);
+    }
+    public void makeAnswer() {
+        List<Integer> randomNumber;
+        do {
+            randomNumber = new ArrayList<>();
+            for (int i = 0; i < NUMBER_LENGTH; i++) {
+                randomNumber.add(Randoms.pickNumberInRange(START_OF_NUMBER, END_OF_NUMBER));
+            }
+        } while (!isUnique(randomNumber) || !isRightLength(randomNumber));
+
+        answer = new ArrayList<>(randomNumber);
+    }
+    private boolean isUnique(List<Integer> randomNumber) {
+        Set<Integer> randomDigits = new HashSet<>(randomNumber);
+        return randomDigits.size() == 3;
+    }
+    private boolean isRightLength(List<Integer> randomNumber) {
+        return randomNumber.size() == 3;
+    }
+
+}
