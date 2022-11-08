6e10950bd55dea5b51bc77c8615a1aa7bbf465b9
Beginner <dkstmdwn17@naver.com>
2022-11-08 21:28:17 +0900

    feat : 3번 기능과 4번 기능을 통합해서 Ball이라는 객체에 구현
    Ball 이라는 객체는 생성자로 `컴퓨터가 선택한 3자리 수` 와 `사용자가 선택한 3자리 수` 를 매개 변수로 가져온다.
    
    이후에 judgeWhichBallItIs() 메서드를 통해 Strike, Ball 의 개수를 산정한다.
    
    이걸 수행하는 메서드 2개가 `checkIfItIsAStrike()`, `checkIfItIsABall()`
    이다.
    
    `determineWhetherTheBallIsAStrikeOrABall()` 은 메세지를 보여주며, 3 스트라이크일 경우 true를 반환하고, 아닌 경우 false를 반환해서 이후에 BaseballGame의 반복문 안에서 계속 될 수 있게 도움을 주는 메서드이다.

--- src/main/java/baseball/domain/Ball.java
+++ src/main/java/baseball/domain/Ball.java
@@ -1,4 +1,70 @@
 package baseball.domain;
 
+import baseball.message.PromptMessage;
+
+import java.util.ArrayList;
+
 public class Ball {
+    private ArrayList<Integer> numbersChosenByTheComputer;
+    private ArrayList<Integer> numberSelectedByTheUser;
+
+    private static int strike;
+    private static int ball;
+
+    public Ball(ArrayList<Integer> numbersChosenByTheComputer, ArrayList<Integer> numbersChosenByUser) {
+        this.numbersChosenByTheComputer = numbersChosenByTheComputer;
+        this.numberSelectedByTheUser = numbersChosenByUser;
+    }
+
+    public void judgeWhichBallItIs(){
+        strike = 0;
+        ball = 0;
+        for(int i=0;i<3;i++){
+            if(!checkIfItIsAStrike(numbersChosenByTheComputer.get(i), numberSelectedByTheUser.get(i))){
+                checkIfItIsABall(numberSelectedByTheUser.get(i));
+            }
+        }
+    }
+
+    private boolean checkIfItIsAStrike(int chosenByTheComputer, int selectedByTheUser) {
+        if(chosenByTheComputer == selectedByTheUser){
+            strike++;
+            return true;
+        }
+        return false;
+    }
+
+    private void checkIfItIsABall(int selectedByTheUser) {
+        if(numbersChosenByTheComputer.contains(selectedByTheUser)){
+            ball++;
+        }
+    }
+
+    public boolean determineWhetherTheBallIsAStrikeOrABall(){
+        if(strike > 0 && ball > 0){
+            System.out.println(ball + PromptMessage.BALL + " " + strike + PromptMessage.STRIKE);
+        }
+        else if(strike < 3 && strike > 0 && ball == 0){
+            System.out.println(strike + PromptMessage.STRIKE);
+        }
+        else if(strike == 0 && ball > 0){
+            System.out.println(ball + PromptMessage.BALL);
+        }
+        else if(strike == 3){
+            System.out.println(strike + PromptMessage.STRIKE);
+            return true;
+        }
+        else{
+            System.out.println(PromptMessage.NOTHING);
+        }
+        return false;
+    }
+
+    public static int getStrike() {
+        return strike;
+    }
+
+    public static int getBall() {
+        return ball;
+    }
 }
