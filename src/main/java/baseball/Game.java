5ddb817f54394ca3892a08c4b90dee1b5741456c
Amenable-C <duswo0624@naver.com>
2022-11-08 13:08:19 +0900

    feat : 게임이 시작된 후 하나의 플레이(상대방의 숫자를 맞히는 일련의 절차)가 재시작 또는 종료될 수 있는 게임 진행 방식

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -8,6 +8,14 @@ public class Game {
     private static final Computer computer = new Computer();
     private static final Player player = new Player();
 
+    public static void start() {
+        do {
+            System.out.println(Message.START_BASEBALL_GAME);
+            play();
+            System.out.println(Message.END_BASEBALL_GAME);
+        } while (player.restart());
+    }
+
     public static void play() {
         computer.createAnswerNumber();
 
