6e8081cf4ebdc7b4780214933d03488cb9d65554
great-park <cksgh1735@naver.com>
2022-11-08 19:11:21 +0900

    feat: main method에서 게임 start

---
+++ src/main/java/baseball/game/BaseballGame.java
@@ -0,0 +1,11 @@
+package baseball.game;
+
+public class BaseballGame {
+
+    public void start() {
+        do {
+            play();
+            askPlayAgain();
+        } while (isPlayAgain);
+    }
+}
