711d7abd9e2ad9baad90ba5748f86540deaa986d
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-08 14:50:34 +0900

    feat(myList): Calculate strike, ball logic

--- src/main/java/baseball/util/myList.java
+++ src/main/java/baseball/util/myList.java
@@ -2,8 +2,9 @@ package baseball.util;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
-public class ListGenerator {
+public class myList {
     public static List<Integer> stringToIntegerList(String input){
         List<Integer> list = new ArrayList<>();
         for ( int i = 0 ; i < input.length() ; i ++){
@@ -11,4 +12,20 @@ public class ListGenerator {
         }
         return list;
     }
+    public static int countDiffLocationValues(List<Integer> l1, List<Integer> l2){
+        int answer = 0;
+        for ( int i = 0 ; i < l1.size() ; i ++ ){
+            for ( int j = 0 ; j < l2.size() ; j ++ ){
+                if ( i == j ) continue;
+                if ( l1.get(i) == l2.get(j) ) answer++;
+            }
+        }
+        return answer;
+    }
+    public static int countSameLocationValues(List<Integer> l1, List<Integer> l2){
+        return (int) IntStream.range(0, 3)
+                .filter(index -> l1.get(index) == l2.get(index))
+                .count();
+    }
 }
+
