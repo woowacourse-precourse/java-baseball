1beb5ee78af65be60e681139a7082421ba5d23b3
ohhonggi <5ghdrl@naver.com>
2022-11-08 17:03:31 +0900

    feat($User): Implement receiveInput function
    This function receives a number from the user.
    
    Added new event to $User
    - Store results by receiveInput function
     in numbers variable
    
    Closes #feat_2

--- src/main/java/baseball/model/User.java
+++ src/main/java/baseball/model/User.java
@@ -1,9 +1,28 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static baseball.service.Validator.validInput;
 
 public class User {
 
     private static List<Integer> numbers;
+
+    public User() {
+        numbers = receiveInput();
+    }
+
+    public static List<Integer> receiveInput() throws IllegalArgumentException {
+        List<Integer> user = new ArrayList<>();
+        String number = Console.readLine();
+        validInput(number);
+        for (String digitNumber : number.split("")) {
+            user.add(Integer.parseInt(digitNumber));
+        }
+        return user;
+    }
 
 }
