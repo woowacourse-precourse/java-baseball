340f9dc8eb1f0cbb7b951f2767e10644dadde110
park-Kumin <rbals97@naver.com>
2022-11-08 20:21:54 +0900

    feat: 랜덤한 숫자 반아온 후 컴퓨터 리스트에 값 추가 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    static final int numberOfDigits = 3;
+    static final int minNumber = 1;
+    static final int maxNumber = 9;
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 랜덤한 숫자 받아온 후 컴퓨터 리스트에 값 추가
+        List<Integer> computer = addToRandomNumberList();
+    }
 
+    // 랜덤한 숫자 받아온 후 컴퓨터 리스트에 값 추가
+    public static List<Integer> addToRandomNumberList() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < numberOfDigits) {
+            int randomNumber = Randoms.pickNumberInRange(minNumber, maxNumber);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 }
