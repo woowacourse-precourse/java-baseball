95c1ed392addc5fdb55151762480f6e8b349e7ef
doxxx <doxxx93@gmail.com>
2022-11-08 20:37:09 +0900

    Test(Command): validate command input

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,12 +1,12 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
@@ -25,6 +25,14 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 재시작_문구_예외_테스트() {
+        assertRandomNumberInRangeTest(
+                () -> assertThatThrownBy(() -> run("246", "135", "3"))
+                        .isInstanceOf(IllegalArgumentException.class), 1, 3, 5
         );
     }
 
