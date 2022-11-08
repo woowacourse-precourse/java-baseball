81b3929b67480e4bebe840d72b5089051dfa8c85
dlrghks2090 <dlrghks2090@gmail.com>
2022-11-08 19:08:05 +0900

    feat: 상대방의 서로 다른 3개의 숫자 생성 메서드

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,8 +1,15 @@
 package baseball;
 
+import java.util.List;
+
 public class BaseballGame {
 
+    private List<Integer>
     public void gameStart(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+        do{
+            Computer computer = new Computer();
+
+        }while(gameResult())
     }
 }
---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,27 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    List<Integer> threeDifferntNumber;
+
+    public Computer() {
+        this.threeDifferntNumber = getThreeDifferntNumber();
+    }
+
+    private List<Integer> getThreeDifferntNumber(){
+        List<Integer> randomDifferentNumber = new ArrayList<>();
+
+        while (randomDifferentNumber.size() < 3) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            if (!randomDifferentNumber.contains(number)) {
+                randomDifferentNumber.add(number);
+            }
+        }
+
+        return randomDifferentNumber;
+    }
+}
