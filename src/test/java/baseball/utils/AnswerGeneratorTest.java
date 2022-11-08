c12c2e9506746820ea8bf1951448431020e84299
Jin409 <sualng123@gmail.com>
2022-11-08 15:49:31 +0900

    test(InputValidatorTest, AnswerGeneratorTest): hasValidInput 메서드 반환값 void 변경에 따른 테스트 코드 수정

--- src/test/java/baseball/utils/AnswerGeneratorTest.java
+++ src/test/java/baseball/utils/AnswerGeneratorTest.java
@@ -1,22 +1,17 @@
 package baseball.utils;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class AnswerGeneratorTest {
     @Test
     void 랜덤_숫자_생성_테스트() {
-        //given
-        AnswerGenerator answerGenerator = new AnswerGenerator();
-        InputValidator inputValidator = new InputValidator();
-
-        //when
-        List<Integer> tempNums = answerGenerator.generateAnswer();
-        boolean result = inputValidator.hasValidInput(tempNums);
+        //given, when
+        List<Integer> tempNums = AnswerGenerator.generateAnswer();
 
         //then
-        assertThat(result).isEqualTo(true);
+        Assertions.assertThatCode(() ->InputValidator.hasValidInput(tempNums))
+                .doesNotThrowAnyException();
     }
 }
--- src/test/java/baseball/utils/InputValidatorTest.java
+++ src/test/java/baseball/utils/InputValidatorTest.java
@@ -1,18 +1,18 @@
 package baseball.utils;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class InputValidatorTest {
     @Test
     void 유효성_검사_테스트() {
         List<Integer> validNum = new ArrayList<>(List.of(1, 2, 3));
-        boolean validNumResult = InputValidator.hasValidInput(validNum);
-        assertThat(validNumResult).isEqualTo(true);
+        Assertions.assertThatCode(() -> InputValidator.hasValidInput(validNum))
+                .doesNotThrowAnyException();
 
         String rangeErrorMessage = "각 자리 수는 최소 1 이상, 최대 9 이하의 정수여야 합니다.";
         String lengthErrorMessage = "숫자는 세 자리 수여야 합니다.";
@@ -24,12 +24,12 @@ public class InputValidatorTest {
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(rangeErrorMessage);
 
-        List<Integer> invalidLengthNum = new ArrayList<>(List.of(1,2));
+        List<Integer> invalidLengthNum = new ArrayList<>(List.of(1, 2));
         assertThatThrownBy(() -> InputValidator.hasValidInput(invalidLengthNum))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(lengthErrorMessage);
 
-        List<Integer> duplicateNum = new ArrayList<>(List.of(1,2,2));
+        List<Integer> duplicateNum = new ArrayList<>(List.of(1, 2, 2));
         assertThatThrownBy(() -> InputValidator.hasValidInput(duplicateNum))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(duplicateErrorMessage);
