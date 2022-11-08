ff1aa71e2579660692a98e42647034b2f676f348
ys200209 <lys200209@naver.com>
2022-11-08 21:20:28 +0900

    feat(User.java): 게임을 재시작할건지에 대해 구현하기
    추가한 메서드
    - validateRestart(): 재시작 입력으로 들어온 값을 검증하라고 명령합니다.
    - isStop(): 사용자의 입력에 따라 게임을 중단할 것인지 계속할 것인지 판별합니다.

--- src/main/java/baseball/domain/User.java
+++ src/main/java/baseball/domain/User.java
@@ -7,6 +7,8 @@ import camp.nextstep.edu.missionutils.Console;
  * 숫자를 맞추는 유저 클래스
  */
 public class User {
+    private static final int RESTART = 1;
+    private static final int FINISH = 2;
     private String number;
     private UserValidator validator = new UserValidator();
 
@@ -16,6 +18,22 @@ public class User {
 
     public void validateNumber() {
         validator.validateNumber(number);
+    }
+
+    public void validateRestart() {
+        validator.validateRestart(number);
+    }
+
+    public boolean isStop() {
+        int restartChoice = Integer.parseInt(number);
+
+        if (restartChoice == FINISH) {
+            return true;
+        } else if (restartChoice == RESTART) {
+            return false;
+        }
+
+        return true;
     }
 
     public String getNumber() {
