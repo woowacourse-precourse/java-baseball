8e00fca9b7fc79f0abd8f7a3262dde733c427f53
Amenable-C <duswo0624@naver.com>
2022-11-08 11:44:19 +0900

    refactor : 플레이어 코드 정렬

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -6,36 +6,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    public List<Integer> createGuessNumber(){
+
+    public List<Integer> createGuessNumber() {
         String guessNumberString = readLine();
 
         exceptionHandler(guessNumberString);
 
         List<Integer> guessNumber = new ArrayList<>();
-        for(int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             guessNumber.add(guessNumberString.charAt(i) - '0');
         }
 
         return guessNumber;
     }
 
-    private void exceptionHandler(String number){
+    private void exceptionHandler(String number) {
         IllegalArgumentException illegalArgumentException = new IllegalArgumentException();
 
-        if(!isValidSize(number)
+        if (!isValidSize(number)
                 || !isValidScope(number)
-                || !isDifferentNumber(number)){
+                || !isDifferentNumber(number)) {
             throw illegalArgumentException;
         }
     }
 
-    private boolean isValidSize(String number){
+    private boolean isValidSize(String number) {
         return number.length() == 3;
     }
 
-    private boolean isValidScope(String number){
-        for(int i = 0; i < 3; i++){
-            if(!(number.charAt(i) >= '1' && number.charAt(i) <= '9')){
+    private boolean isValidScope(String number) {
+        for (int i = 0; i < 3; i++) {
+            if (!(number.charAt(i) >= '1' && number.charAt(i) <= '9')) {
                 return false;
             }
         }
@@ -43,7 +44,7 @@ public class Player {
         return true;
     }
 
-    private boolean isDifferentNumber(String number){
+    private boolean isDifferentNumber(String number) {
         return number.charAt(0) != number.charAt(1)
                 && number.charAt(0) != number.charAt(2)
                 && number.charAt(1) != number.charAt(2);
