501041a197cd2b41a529bdbd4d5fbedf9bf28e1d
Ryokuman <ryokuman@naver.com>
2022-11-08 17:43:04 +0900

    fix ( test ) : devide illegal check fix ( Application ) : change Field in_progress to value
    devide test code : 예외처리 to 너무_긴_값, 너무_짧은_값, 중복된_값 , 문자열
    change Integer Field is_in_progress to hashmap value of main method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,21 +2,24 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
-public class Application {
-    protected static int is_in_progress = 1;
+import java.util.HashMap;
 
+
+public class Application {
     public static void main(String[] args) {
+        HashMap<String, Integer> is_in_progress = new HashMap<>();
+        is_in_progress.put("state", 1);
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while (is_in_progress == 1) {
+        while (is_in_progress.get("state") == 1) {
             Game.start();
-            set_is_in_progress();
+            set_is_in_progress(is_in_progress);
         }
     }
 
-    public static void set_is_in_progress() {
+    public static void set_is_in_progress(HashMap<String, Integer> is_in_progress) {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         try {
-            is_in_progress = Integer.parseInt(Console.readLine());
+            is_in_progress.put("state", Integer.parseInt(Console.readLine()));
         } catch (Exception e) {
             throw new IllegalArgumentException();
         }
