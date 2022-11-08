346db35a86dea7fa7542f6cbc85668c109aa13aa
star-sil <kimse9811@naver.com>
2022-11-08 22:53:52 +0900

    FEAT 유저로부터 3자리수 입력받기 기능 추가
    유저로부터 서로 다른 임의의 3자리 수를 입력 받도록 기능을 구현하였다. 비정상적인 입력을 할경우 IllegalArgumentException 예외가 발생하도록 하였다.

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -11,8 +12,18 @@ public class BaseballGame {
         System.out.println("숫자 야구 게임을 시작합니다.");
         List<Integer> computerNumber = makeRandomNum();
 
-    }
+        boolean gameContinue = true;
 
+        while (gameContinue) {
+
+            System.out.println("숫자를 입력해주세요 : ");
+            String userInput = Console.readLine();
+
+            checkUserInput(userInput);
+            List<Integer> userNumber = convertInput(userInput);
+        }
+
+    }
 
     // 랜덤 숫자 만들기
     public List<Integer> makeRandomNum() {
@@ -29,6 +40,67 @@ public class BaseballGame {
         }
 
         return computer;
+    }
+
+    public void checkUserInput(String user) {
+
+        if (user.length() != 3) throw new IllegalArgumentException("3자리 이상을 입력하였습니다.");
+
+        if (isNotNumber(user)) throw new IllegalArgumentException("숫자가 아닌 다른 문자를 입력하였습니다.");
+
+        if (isDuplicateNum(user)) throw new IllegalArgumentException("중복된 숫자를 입력하였습니다.");
+
+    }
+
+    public boolean isNotNumber(String input) {
+
+        String numberString = "123456789";
+
+        for (int i = 0; i < 3; i++) {
+
+            if (numberString.contains(input.substring(i,i+1))) continue;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean isDuplicateNum(String input) {
+
+        char[] numberArray = {'1','2','3','4','5','6','7','8','9'};
+
+        for (char num : numberArray) {
+
+            if (countNum(input, num) > 1) return true;
+        }
+
+        return false;
+    }
+
+    public int countNum(String input, char num) {
+
+        int count = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+
+            if (input.charAt(i) == num) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    public List<Integer> convertInput(String userInput) {
+
+        List<Integer> newInput = new ArrayList<>();
+
+        for (int i = 0; i < userInput.length(); i++) {
+            newInput.add(userInput.charAt(i) - '0');
+        }
+
+        return newInput;
     }
 
 }
