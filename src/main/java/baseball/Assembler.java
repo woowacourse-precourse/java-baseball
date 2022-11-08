648c249ef300d8f39cde78bf21ee11d6be5b2365
xxeol2 <jhj01177@naver.com>
2022-11-08 15:53:23 +0900

    feat: 조립기 도입

---
+++ src/main/java/baseball/Assembler.java
@@ -0,0 +1,8 @@
+package baseball;
+
+public class Assembler {
+
+    NumberGenerator numberGenerator = new RandomNumberGenerator();
+    InputController inputController = new InputControllerImp();
+    HintCalculator hintCalculator = new HintCalculatorImp();
+}
