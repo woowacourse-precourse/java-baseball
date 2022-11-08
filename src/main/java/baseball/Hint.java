499842d83d94821bd4502e26a7c87aa2fef7a912
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-08 19:01:17 +0900

    feat(Hint): 힌트 출력 (스트라이크, 볼, 낫싱)

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -10,6 +10,7 @@ public class Hint {
     public Hint(List<Integer> userNums, List<Integer> computerNums, int count) {
         strike = countStrike(userNums, computerNums, count);
         ball = countBall(userNums, computerNums, count);
+        printHint(count);
     }
 
     public int getStrike() {
@@ -31,5 +32,17 @@ public class Hint {
                 .filter(i -> userNums.get(i) != computerNums.get(i)
                         && userNums.contains(computerNums.get(i)))
                 .count();
+    }
+
+    private void printHint(int count) {
+        if (strike == count) {
+            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return;
+        }
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+        System.out.printf("%d볼 %d스트라이크\n", ball, strike);
     }
 }
