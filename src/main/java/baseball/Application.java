47521653ace3b0b26278a39a90a97174fda06f39
Hongsugyeong <olivia258@naver.com>
2022-11-08 11:56:48 +0900

    (Comment) : 필요없는 주석 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -41,7 +41,6 @@ class Game {
 
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해 주세요 : ";
     private static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    //private static final String NEW_GAME_CHECK_MESSAGE = "게임을 새로 시작하려면 " + Game.NEW_GAME + ", 종료하려면 " + Game.QUIT_GAME + "를 입력하세요.";
     private static final String NEW_GAME_CHECK_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
     int[] number = new int[3];
