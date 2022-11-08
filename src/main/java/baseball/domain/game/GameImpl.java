718fda849c1119f42ebb2516ded4bf4b6288c0b6
kimdoha <kkhhjj3393@gmail.com>
2022-11-08 21:46:42 +0900

    refactor(Game, User): 유저 넘버 리스트 반환 메소드 변경 반영

--- src/main/java/baseball/domain/game/GameImpl.java
+++ src/main/java/baseball/domain/game/GameImpl.java
@@ -33,7 +33,7 @@ public class GameImpl implements Game{
   @Override
   public void GameScore() {
     score = new Score();
-    score.compareTwoNumber(computerPlayer.getNumberList(), userPlayer.getUserNumberList());
+    score.compareTwoNumber(computerPlayer.getNumberList(), userPlayer.getNumberList());
   }
 
   @Override
