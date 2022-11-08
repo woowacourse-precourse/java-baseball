dde0f6c2450f3adc0ff9f17b2165bff7d509b6ed
YOON <seoung59@gmail.com>
2022-11-08 19:25:55 +0900

    Feat: 게임 플레이어가 잘못된 값을 입력할 경우 IllegalArgumentException을 발생시킨 후 애플리케이션 종료

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -7,7 +7,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Player {
-
+    private final int NUMBER_LENGTH = 3;
     private List<Integer> number;
 
     public Player() {
@@ -16,11 +16,35 @@ public class Player {
     public void input(){
         System.out.print("숫자를 입력해주세요 : ");
         String input = Console.readLine();
-        List<Integer> numberList = Stream.of(input.split(""))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-        this.number = numberList;
-        System.out.println("numberList = " + numberList);
+
+        try{
+            List<Integer> numberList = Stream.of(input.split(""))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            checkNumber(numberList);
+
+            this.number = numberList;
+        } catch (Exception e) {
+            throw new IllegalArgumentException(e);
+        }
     }
+
+    private void checkNumber(List<Integer> number){
+        if(checkSize(number) && checkDuplicate(number)){
+            return;
+        }
+        throw new IllegalArgumentException();
+    }
+    private boolean checkSize(List<Integer> number){
+        return number.size() == NUMBER_LENGTH;
+    }
+
+    private boolean checkDuplicate(List<Integer> number){
+        long count = number.stream()
+                .distinct()
+                .count();
+        return count == NUMBER_LENGTH;
+    }
+
 
 }
