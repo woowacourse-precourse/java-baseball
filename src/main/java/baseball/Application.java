d116be1f01613b4ac1f7ad6c956efaea356f7aea
wormjoo <wormjoo@naver.com>
2022-11-08 18:24:37 +0900

    feat: 게임 시작 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
 public class Application {
+    public static void baseballGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        String restartNumber = "";
+        do {
+            // TODO: 랜덤 숫자 생성
+            restartNumber = startGame();
+        } while (!restartNumber.equals("2"));
+    }
+
+    public static String startGame() {
+        // TODO: 숫자 입력받기
+        // TODO: 입력한 수에 대한 힌트
+        return "";
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        baseballGame();
     }
 }
