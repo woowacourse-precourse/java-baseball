fa689f646f589ff3e6811b9b70971b16399f7b88
baksohyeon <bori12370@gmail.com>
2022-11-08 10:48:37 +0900

    feat: 검증된 세자리 숫자를 받아서 출력하는 메서드 추가

--- src/main/java/baseball/ExecuteGame.java
+++ src/main/java/baseball/ExecuteGame.java
@@ -3,7 +3,7 @@ package baseball;
 import java.io.*;
 import java.util.Scanner;
 
-public class ExecuteGame {
+public class ExecuteGame extends IllegalArgumentException{
 
     // 입력한 값을 String 형태로 반환
 //    private String saveInput () {
@@ -31,4 +31,24 @@ public class ExecuteGame {
         ps.println("1 또는 2를 입력해주세요");
         return canRepeatGame();
     }
+
+    // 게임 중 값을 입력 받아 출력하는 함수
+    // 잘못된 값을 입력했을 경우 에러 메시지 출력 후 `IllegalArgumentException`을 발생
+    public boolean getUserValidInput() {
+        try {
+            while (true) {
+                ps.println("숫자를 입력해주세요: ");
+                String userInput = scanner.nextLine();
+                if (InputValidator.isValidInput(userInput)) {
+                    ps.println("잘못된 값을 입력했습니다!\n0을 제외한 서로 다른 3개의 숫자를 연속하여 입력해주세요.");
+                    break;
+                }
+            } throw new IllegalArgumentException();
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+
+    }
+
+
 }
