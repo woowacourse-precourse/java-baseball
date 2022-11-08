b456307c3e451ba0054f6f1c744daeca2f2f3958
yeada <yeadalm@gmail.com>
2022-11-08 10:06:50 +0900

    test : Game.java test code 오류 수정

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -7,17 +7,30 @@ import java.util.List;
 
 public class GameTest {
     @Test
-    public void strike(){
+    public void strikeCase1(){
         List<Integer> input = List.of(2, 3, 4);
         List<Integer> computer = List.of(2, 1, 5);
         assertThat(Game.strike(input, computer)).isEqualTo(1);
     }
 
     @Test
-    public void ball(){
+    public void strikeCase2(){
+        List<Integer> input = List.of(1, 3, 4);
+        List<Integer> computer = List.of(1, 3, 4);
+        assertThat(Game.strike(input, computer)).isEqualTo(3);
+    }
+    @Test
+    public void ballCase1(){
         List<Integer> input = List.of(3, 2, 4);
         List<Integer> computer = List.of(1, 5, 4);
-        assertThat(Game.strike(input, computer)).isEqualTo(1);
+        assertThat(Game.ball(input, computer)).isEqualTo(0);
+    }
+
+    @Test
+    public void ballCase2(){
+        List<Integer> input = List.of(1, 2, 3);
+        List<Integer> computer = List.of(2, 1, 3);
+        assertThat(Game.ball(input, computer)).isEqualTo(2);
     }
 
     @Test
