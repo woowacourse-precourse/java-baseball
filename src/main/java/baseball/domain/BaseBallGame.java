9213e55e48bca15f18a84718e7bee3ecea1fd5e4
JunH <tjdtls690@gmail.com>
2022-11-08 17:54:10 +0900

    :art: refactor(Domain : BaseBallGame) : 판결된 플레이 결과를 반환하는 로직을 따로 메서드로 분리

--- src/main/java/baseball/domain/BaseBallGame.java
+++ src/main/java/baseball/domain/BaseBallGame.java
@@ -32,14 +32,17 @@ public class BaseBallGame {
     
     private Referee playResult(final String userNumbers) {
         final List<BallStatus> comparisonResult = compareBalls(userNumbers);
-        final Referee referee = new Referee();
-        referee.decide(comparisonResult);
-        
-        return referee;
+        return decidedPlayResult(comparisonResult);
     }
     
     private List<BallStatus> compareBalls(final String userNumbers) {
         return computerBalls.compareBalls(new Balls(userNumbers));
+    }
+    
+    private Referee decidedPlayResult(final List<BallStatus> comparisonResult) {
+        final Referee referee = new Referee();
+        referee.decide(comparisonResult);
+        return referee;
     }
     
     @Override
