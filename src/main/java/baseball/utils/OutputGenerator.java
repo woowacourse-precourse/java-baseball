6c27c78904a96ea78e20228d7a74eabcd0c35ec3
조병우 <whquddn55@gmail.com>
2022-11-08 10:26:59 +0900

    feat(OutputGenerator): implement print score method
    This method just print string of Score instance to console.
    
    + public void printScore()

--- src/main/java/baseball/utils/OutputGenerator.java
+++ src/main/java/baseball/utils/OutputGenerator.java
@@ -2,6 +2,8 @@ package baseball.utils;
 
 import static baseball.BaseballGame.INPUT_SIZE;
 
+import baseball.domain.Score;
+
 public class OutputGenerator {
     public void printWelcome() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -14,5 +16,9 @@ public class OutputGenerator {
     public void printGameEnd() {
         System.out.printf("%d개의 숫자를 모두 맞히셨습니다! 게임 종료\n", INPUT_SIZE);
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public void printScore(Score score) {
+        System.out.println(score);
     }
 }
