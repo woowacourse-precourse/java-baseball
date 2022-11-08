56286b5eeeafa9cec9ea1dfacef541657a28f993
HangSub999 <gkdtjq999@gmail.com>
2022-11-08 16:09:33 +0900

    feat: 게임종료 출력

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -21,4 +21,8 @@ public class Output {
     public static void printFourBall() {
         System.out.println("낫싱");
     }
+
+    public static void printCorrectAnswer() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 }
