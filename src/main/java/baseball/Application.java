ba6d6c5fc900b65c5246959c944f5e065a2afae2
es <mascoeskim@gmail.com>
2022-11-08 15:33:12 +0900

    style: 줄바꿈 및 공백 일괄수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,153 +7,149 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        int gameControllerNumber;
-        int errNumber = 0;
-        int exitNumber = 2;
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+		int gameControllerNumber;
+		int errNumber = 0;
+		int exitNumber = 2;
 
-        do {
-            System.out.println("숫자 야구 게임을 시작합니다!!!");
-            ArrayList<Integer> generateAnswer = generateAnswer();
-            // System.out.println("정답은" + generateAnswer);
-            gameControllerNumber = gameController(generateAnswer);
-        } while (gameControllerNumber != exitNumber && gameControllerNumber != errNumber);
-        System.out.println("게임 종료");
-        if (gameControllerNumber == errNumber) {
-            throw new IllegalArgumentException();
-        }
-    }
+		do {
+			System.out.println("숫자 야구 게임을 시작합니다!!!");
+			ArrayList<Integer> generateAnswer = generateAnswer();
+			// System.out.println("정답은" + generateAnswer);
+			gameControllerNumber = gameController(generateAnswer);
+		} while (gameControllerNumber != exitNumber && gameControllerNumber != errNumber);
+		System.out.println("게임 종료");
+		if (gameControllerNumber == errNumber) {
+			throw new IllegalArgumentException();
+		}
+	}
 
-    public static int gameController(ArrayList<Integer> generateAnswer) {
-        
-        int gameControllerNumber;
-        int errNumber = 0;
-        ArrayList<Integer> inputUserValue;
+	public static int gameController(ArrayList<Integer> generateAnswer) {
 
-        while (true) {
-            inputUserValue = inputUserValue();
-            if (inputUserValue.get(0) == errNumber) {
-                gameControllerNumber = errNumber;
-                break;
-            }
-            String compareAnswerAndValue = compareAnswerAndValue(generateAnswer, inputUserValue);
-            System.out.println(compareAnswerAndValue);
-            if (compareAnswerAndValue.equals("3스트라이크")) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                gameControllerNumber = restart();
-                break;
-            }
-        }
-        return gameControllerNumber;
-    }
+		int gameControllerNumber;
+		int errNumber = 0;
+		ArrayList<Integer> inputUserValue;
 
-    public static ArrayList<Integer> generateAnswer() {
-        ArrayList<Integer> answerNumber = new ArrayList<>();
+		while (true) {
+			inputUserValue = inputUserValue();
+			if (inputUserValue.get(0) == errNumber) {
+				gameControllerNumber = errNumber;
+				break;
+			}
+			String compareAnswerAndValue = compareAnswerAndValue(generateAnswer, inputUserValue);
+			System.out.println(compareAnswerAndValue);
+			if (compareAnswerAndValue.equals("3스트라이크")) {
+				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+				gameControllerNumber = restart();
+				break;
+			}
+		}
+		return gameControllerNumber;
+	}
 
-        while (answerNumber.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!answerNumber.contains(randomNumber)) {
-                answerNumber.add(randomNumber);
-            }
-        }
-        return answerNumber;
-    }
+	public static ArrayList<Integer> generateAnswer() {
+		ArrayList<Integer> answerNumber = new ArrayList<>();
 
-    public static ArrayList<Integer> inputUserValue() {
-        ArrayList<Integer> inputNumberArray = new ArrayList<>();
-        String inputNumber = Console.readLine();
-        int firstBall = 0;
-        int secondBall = 1;
-        int thirdBall = 2;
-        int errNumber = 0;
+		while (answerNumber.size() < 3) {
+			int randomNumber = Randoms.pickNumberInRange(1, 9);
+			if (!answerNumber.contains(randomNumber)) {
+				answerNumber.add(randomNumber);
+			}
+		}
+		return answerNumber;
+	}
 
-        if (inputNumber.charAt(firstBall) == inputNumber.charAt(secondBall)
-            || inputNumber.charAt(firstBall) == inputNumber.charAt(thirdBall)
-            || inputNumber.charAt(secondBall) == inputNumber.charAt(thirdBall)
-            || inputNumber.length() != 3
-            || !Character.isDigit(inputNumber.charAt(firstBall))
-            || !Character.isDigit(inputNumber.charAt(secondBall))
-            || !Character.isDigit(inputNumber.charAt(thirdBall))
-            || inputNumber.charAt(firstBall) == 0
-            || inputNumber.charAt(secondBall) == 0
-            || inputNumber.charAt(thirdBall) == 0 ) {
-            inputNumberArray.add(errNumber);
+	public static ArrayList<Integer> inputUserValue() {
+		ArrayList<Integer> inputNumberArray = new ArrayList<>();
+		String inputNumber = Console.readLine();
+		int firstBall = 0;
+		int secondBall = 1;
+		int thirdBall = 2;
+		int errNumber = 0;
 
-            return inputNumberArray;
-        }
+		if (inputNumber.charAt(firstBall) == inputNumber.charAt(secondBall)
+			|| inputNumber.charAt(firstBall) == inputNumber.charAt(thirdBall)
+			|| inputNumber.charAt(secondBall) == inputNumber.charAt(thirdBall) || inputNumber.length() != 3
+			|| !Character.isDigit(inputNumber.charAt(firstBall)) || !Character.isDigit(inputNumber.charAt(secondBall))
+			|| !Character.isDigit(inputNumber.charAt(thirdBall)) || inputNumber.charAt(firstBall) == 0
+			|| inputNumber.charAt(secondBall) == 0 || inputNumber.charAt(thirdBall) == 0) {
+			inputNumberArray.add(errNumber);
 
-        inputNumberArray.add(Character.getNumericValue(inputNumber.charAt(firstBall)));
-        inputNumberArray.add(Character.getNumericValue(inputNumber.charAt(secondBall)));
-        inputNumberArray.add(Character.getNumericValue(inputNumber.charAt(thirdBall)));
+			return inputNumberArray;
+		}
 
-        return inputNumberArray;
-    }
+		inputNumberArray.add(Character.getNumericValue(inputNumber.charAt(firstBall)));
+		inputNumberArray.add(Character.getNumericValue(inputNumber.charAt(secondBall)));
+		inputNumberArray.add(Character.getNumericValue(inputNumber.charAt(thirdBall)));
 
-    public static String compareAnswerAndValue(ArrayList<Integer> generateAnswer, ArrayList<Integer> inputUserValue) {
+		return inputNumberArray;
+	}
 
-        String outputInText = "";
-        int strikeCount = 0;
-        int ballCount = 0;
-        int firstBall = 0;
-        int secondBall = 1;
-        int thirdBall = 2;
+	public static String compareAnswerAndValue(ArrayList<Integer> generateAnswer, ArrayList<Integer> inputUserValue) {
 
-        for (int j = 0; j < 3; j++) {
-            if (Objects.equals((generateAnswer.get(firstBall)), inputUserValue.get(j)) && j == firstBall) {
-                strikeCount++;
-            }
-            if (Objects.equals((generateAnswer.get(firstBall)), inputUserValue.get(j)) && j != firstBall) {
-                ballCount++;
-            }
-        }
-        
-        for (int j = 0; j < 3; j++) {
-            if (Objects.equals((generateAnswer.get(secondBall)), inputUserValue.get(j)) && j == secondBall) {
-                strikeCount++;
-            }
-            if (Objects.equals((generateAnswer.get(secondBall)), inputUserValue.get(j)) && j != secondBall) {
-                ballCount++;
-            }
-        }
-        
-        for (int j = 0; j < 3; j++) {
-            if (Objects.equals((generateAnswer.get(thirdBall)), inputUserValue.get(j)) && j == thirdBall) {
-                strikeCount++;
-            }
-            if (Objects.equals((generateAnswer.get(thirdBall)), inputUserValue.get(j)) && j != thirdBall) {
-                ballCount++;
-            }
-        }
+		String outputInText = "";
+		int strikeCount = 0;
+		int ballCount = 0;
+		int firstBall = 0;
+		int secondBall = 1;
+		int thirdBall = 2;
 
-        outputInText = outputInText(outputInText, strikeCount, ballCount);
+		for (int j = 0; j < 3; j++) {
+			if (Objects.equals((generateAnswer.get(firstBall)), inputUserValue.get(j)) && j == firstBall) {
+				strikeCount++;
+			}
+			if (Objects.equals((generateAnswer.get(firstBall)), inputUserValue.get(j)) && j != firstBall) {
+				ballCount++;
+			}
+		}
 
-        return outputInText;
-    }
+		for (int j = 0; j < 3; j++) {
+			if (Objects.equals((generateAnswer.get(secondBall)), inputUserValue.get(j)) && j == secondBall) {
+				strikeCount++;
+			}
+			if (Objects.equals((generateAnswer.get(secondBall)), inputUserValue.get(j)) && j != secondBall) {
+				ballCount++;
+			}
+		}
 
-    public static String outputInText(String outputInText, int strikeCount, int ballCount) {
-        if (strikeCount == 0 && ballCount == 0) {
-            outputInText += "낫싱";
-        } else if (strikeCount == 0) {
-            outputInText += (ballCount + "볼");
-        } else if (ballCount == 0) {
-            outputInText += (strikeCount + "스트라이크");
-        } else {
-            outputInText += (ballCount + "볼") + " " + (strikeCount + "스트라이크");
-        }
-        return outputInText;
-    }
+		for (int j = 0; j < 3; j++) {
+			if (Objects.equals((generateAnswer.get(thirdBall)), inputUserValue.get(j)) && j == thirdBall) {
+				strikeCount++;
+			}
+			if (Objects.equals((generateAnswer.get(thirdBall)), inputUserValue.get(j)) && j != thirdBall) {
+				ballCount++;
+			}
+		}
 
-    public static int restart() {
-        System.out.println("계속하시겠습니까?1=계속,2=끝");
-        int toBeContinue = Integer.parseInt(Console.readLine());
-        int continueNumber = 1;
-        int exitNumber = 2;
+		outputInText = outputInText(outputInText, strikeCount, ballCount);
 
-        if (toBeContinue != continueNumber && toBeContinue != exitNumber) {
-            toBeContinue = exitNumber;
-        }
-        return toBeContinue;
-    }
+		return outputInText;
+	}
+
+	public static String outputInText(String outputInText, int strikeCount, int ballCount) {
+		if (strikeCount == 0 && ballCount == 0) {
+			outputInText += "낫싱";
+		} else if (strikeCount == 0) {
+			outputInText += (ballCount + "볼");
+		} else if (ballCount == 0) {
+			outputInText += (strikeCount + "스트라이크");
+		} else {
+			outputInText += (ballCount + "볼") + " " + (strikeCount + "스트라이크");
+		}
+		return outputInText;
+	}
+
+	public static int restart() {
+		System.out.println("계속하시겠습니까?1=계속,2=끝");
+		int toBeContinue = Integer.parseInt(Console.readLine());
+		int continueNumber = 1;
+		int exitNumber = 2;
+
+		if (toBeContinue != continueNumber && toBeContinue != exitNumber) {
+			toBeContinue = exitNumber;
+		}
+		return toBeContinue;
+	}
 }
 
--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -14,68 +14,62 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
 class ApplicationTest extends NsTest {
-    @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
-    }
+	@Test
+	void 게임종료_후_재시작() {
+		assertRandomNumberInRangeTest(() -> {
+			run("246", "135", "1", "597", "589", "2");
+			assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+		}, 1, 3, 5, 5, 8, 9);
+	}
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
+	@Test
+	void 예외_테스트() {
+		assertSimpleTest(
+			() -> assertThatThrownBy(() -> runException("1234")).isInstanceOf(IllegalArgumentException.class));
+	}
 
-    @Test
-    void 예외_테스트2() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("asd"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
+	@Test
+	void 예외_테스트2() {
+		assertSimpleTest(
+			() -> assertThatThrownBy(() -> runException("asd")).isInstanceOf(IllegalArgumentException.class));
+	}
 
-    @Test
-    void generateAnswer_테스트() {
-        ArrayList<Integer> result = generateAnswer();
-        System.out.println(result);
-        assertEquals(result.size(), 3);
-    }
+	@Test
+	void generateAnswer_테스트() {
+		ArrayList<Integer> result = generateAnswer();
+		System.out.println(result);
+		assertEquals(result.size(), 3);
+	}
 
-    @Test
-    void outputInText_테스트 () {
-        String outputInText = "";
-        int strikeCount = 0;
-        int ballCount = 0;
-        String result = outputInText(outputInText, strikeCount, ballCount);
-        assertThat(result).isEqualTo("낫싱");
-    }
+	@Test
+	void outputInText_테스트() {
+		String outputInText = "";
+		int strikeCount = 0;
+		int ballCount = 0;
+		String result = outputInText(outputInText, strikeCount, ballCount);
+		assertThat(result).isEqualTo("낫싱");
+	}
 
-    @Test
-    void outputInText_테스트2 () {
-        String outputInText = "";
-        int strikeCount = 3;
-        int ballCount = 0;
-        String result = outputInText(outputInText, strikeCount, ballCount);
-        assertThat(result).isEqualTo("3스트라이크");
-    }
+	@Test
+	void outputInText_테스트2() {
+		String outputInText = "";
+		int strikeCount = 3;
+		int ballCount = 0;
+		String result = outputInText(outputInText, strikeCount, ballCount);
+		assertThat(result).isEqualTo("3스트라이크");
+	}
 
-    @Test
-    void outputInText_테스트3 () {
-        String outputInText = "";
-        int strikeCount = 2;
-        int ballCount = 2;
-        String result = outputInText(outputInText, strikeCount, ballCount);
-        assertThat(result).isEqualTo("2볼 2스트라이크");
-    }
-    @Override
-    public void runMain() {
-        Application.main(new String[]{});
-    }
+	@Test
+	void outputInText_테스트3() {
+		String outputInText = "";
+		int strikeCount = 2;
+		int ballCount = 2;
+		String result = outputInText(outputInText, strikeCount, ballCount);
+		assertThat(result).isEqualTo("2볼 2스트라이크");
+	}
+
+	@Override
+	public void runMain() {
+		Application.main(new String[] {});
+	}
 }
