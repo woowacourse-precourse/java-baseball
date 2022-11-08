c114f8d97ab752f0f02481862a7d96d6e366f1b1
alstjr706 <alstjr706@naver.com>
2022-11-08 14:52:23 +0900

    refactor : (숫자 야구) 숫자 랜덤 생성클래스 디펜던시 방식으로 리펙토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,11 +2,12 @@ package baseball;
 
 import baseball.constants.SystemConsole;
 import baseball.domain.GameResultScore;
+import baseball.domain.RandomGenerator;
 
 public class Application {
 
     public static void main(String[] args) {
-        BaseballGame baseballGame = new BaseballGame(new SystemConsole(), new GameResultScore());
+        BaseballGame baseballGame = new BaseballGame(new RandomGenerator(), new SystemConsole(), new GameResultScore());
         baseballGame.start();
     }
 }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -12,21 +12,22 @@ public class BaseballGame {
     private static final String RESTART = "1";
     private static final String GAME_WIN_CONDITIONS = "3스트라이크";
 
+    private final RandomGenerator randomGenerator;
     private final SystemConsole systemConsole;
     private final GameResultScore gameResultScore;
 
-    public BaseballGame(SystemConsole systemConsole, GameResultScore gameResultScore) {
+    public BaseballGame(RandomGenerator randomGenerator, SystemConsole systemConsole, GameResultScore gameResultScore) {
+        this.randomGenerator = randomGenerator;
         this.systemConsole = systemConsole;
         this.gameResultScore = gameResultScore;
     }
 
     public void start() {
-        RandomGenerator randomGenerator = new RandomGenerator();
         systemConsole.baseballStartMessage();
-        conditionsBaseballGame(randomGenerator);
+        conditionsBaseballGame();
     }
 
-    public void conditionsBaseballGame(RandomGenerator randomGenerator) {
+    public void conditionsBaseballGame() {
         String restartNumber = "1";
 
         while (restartNumber.equals(RESTART)) {
