b2100565faa689a4d7a02ab4c40d78dd32674639
Laeng <Laeng@users.noreply.github.com>
2022-11-08 17:41:44 +0900

    test(GameController): validateInput 의 IllegalArgumentException 발생을 테스트 하도록 변경

--- src/test/java/baseball/controller/GameControllerTest.java
+++ src/test/java/baseball/controller/GameControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @DisplayName("GameNumbers 테스트")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -15,33 +17,27 @@ public class GameControllerTest {
         @ParameterizedTest(name = "한 자리 숫자 입력 실패 => {0}")
         @ValueSource(strings = {"a", "ㄱ", "가", "0", "11"})
         void 한_자리_숫자_입력_실패(String input) {
-            boolean result = GameController.validateInput("^[1-2]{1}$", input);
-
-            assertThat(result).isFalse();
+            assertThatThrownBy(() -> GameController.validateInput("^[1-2]{1}$", input))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
         @ParameterizedTest(name = "한 자리 숫자 입력 성공 => {0}")
         @ValueSource(strings = {"1", "2"})
         void 한_자리_숫자_입력_성공(String input) {
-            boolean result = GameController.validateInput("^[1-2]{1}$", input);
-
-            assertThat(result).isTrue();
+            assertDoesNotThrow(() -> GameController.validateInput("^[1-2]{1}$", input));
         }
 
-        @ParameterizedTest(name = "세 자리 수 입력 실패")
+        @ParameterizedTest(name = "세 자리 수 입력 실패 => {0}")
         @ValueSource(strings = {"a", "aaa", "ㄱㄱㄱ", "가가가", "1", "22", "000"})
         void 세_자리_숫자_입력_실패(String input) {
-            boolean result = GameController.validateInput("^[1-9]{3}$", input);
-
-            assertThat(result).isFalse();
+            assertThatThrownBy(() -> GameController.validateInput("^[1-9]{3}$", input))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
-        @ParameterizedTest(name = "세 자리 수 입력 성공")
+        @ParameterizedTest(name = "세 자리 수 입력 성공 => {0}")
         @ValueSource(strings = {"111", "234"})
         void 세_자리_숫자_입력_성공(String input) {
-            boolean result = GameController.validateInput("^[1-9]{3}$", input);
-
-            assertThat(result).isTrue();
+            assertDoesNotThrow(() -> GameController.validateInput("^[1-9]{3}$", input));
         }
     }
 }
