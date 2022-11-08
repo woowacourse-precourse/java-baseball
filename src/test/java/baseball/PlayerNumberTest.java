e62b133af0d5fff560510295a56b612bb37a6e7c
yoon <beatus96@naver.com>
2022-11-08 12:44:34 +0900

    test(playernumberTest) : 플레이어수 유효성 체크 기능 테스트 수정
    - Model 클래스 사용해 플레이어수 생성하고, 유효한 입력값이 아닌 경우 예외 발생
    - Vaildator 클래스 사용해 플레이어 수가 게임 룰에 유효한 값인지 boolean값 반환

--- src/test/java/baseball/PlayerNumberTest.java
+++ src/test/java/baseball/PlayerNumberTest.java
@@ -18,11 +18,20 @@ public class PlayerNumberTest {
 //        assertThatThrownBy(() -> Application.getPlayerNumber())
 //                .isInstanceOf(IllegalArgumentException.class);
 //    }
+    @Test
+    void 플레이어수_숫자아닌경우_예외발생_테스트(){
+        //given
+        String input = "1123"; //"가나다" ,"ABC" ,"012", "1234", "120" 오류
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
 
+        assertThatThrownBy(() -> GameModel.createPlayerNumber())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
     @Test
     void 입력값_유효성_테스트() {
         List<Integer> input = List.of(1, 1, 2, 3);
-        assertThat(Application.isValidNumber(input)).isFalse();
+        assertThat(Validator.isValidNumber(input)).isFalse();
     }
 
     @Test
@@ -32,13 +41,13 @@ public class PlayerNumberTest {
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
         List<Integer> output = List.of(1,2,3);
-        assertThat(Application.getPlayerNumber()).isEqualTo(output);
+        assertThat(GameModel.createPlayerNumber()).isEqualTo(output);
     }
 
     @Test
     void 문자열_숫자인지_체크_테스트(){
         String input = "가나다";
-        assertThat(Application.isNumber(input)).isFalse();
+        assertThat(Validator.isNumber(input)).isFalse();
     }
 
 }
