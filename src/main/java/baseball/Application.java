8ffa9209013444947cc8198eb655e23fcde881d1
Moon-Tree <ellaya1012@gmail.com>
2022-11-09 00:23:48 +0900

    Feat : 숫자 야구 게임 시작 문구 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,20 @@
 package baseball;
 
 
+import java.util.List;
+
 public class Application {
+
+    // 기능 목록 1 : 숫자 야구 게임 시작 문구 생성
+    static List<Integer> startMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        return makeThreeNumber();
+    }
+
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
+        startMessage();
     }
 }
