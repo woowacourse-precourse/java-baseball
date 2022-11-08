9a944c3816c0a5b915c2da5ca148cef3f3e3890c
kim1387 <kevink0@naver.com>
2022-11-08 21:56:39 +0900

    refactor: 받은 입력값은 모두 List<Digit>으로 변환하여 비지니스로직에서 변환 과정을 거치지 않도록 수정

--- src/main/java/iosystem/InputSystem.java
+++ src/main/java/iosystem/InputSystem.java
@@ -1,34 +1,50 @@
 package iosystem;
 
+import baseball.Digit;
 import baseball.GameMessage;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class InputSystem {
 
     private InputSystem(){
     }
 
-    public static List<Integer> inputTryNumber(){
-        System.out.print(GameMessage.TRY_CORRECT);
+    public static List<Digit> inputTryNumber(){
+        System.out.print(GameMessage.TRY_CORRECT.getMessage());
         String input = Console.readLine();
         TrialValidator.validate(input);
-        List<String> characterList = Arrays.asList(input.split(""));
-        return characterList.stream().map(Integer::valueOf).collect(Collectors.toList());
+        return convertDigit(input);
     }
 
-    public static boolean inputRetryNumber(){
-        System.out.print(GameMessage.RETRY_GAME);
+    public static List<Digit> convertDigit(String input){
+        List<String> characterList = Arrays.asList(input.split(""));
+        List<Digit> newDigitList = new ArrayList<>(3);
+        for(int i = 0; i < characterList.size(); i++) {
+            newDigitList.add(new Digit(i, Integer.valueOf(characterList.get(i))));
+        }
+        return newDigitList;
+    }
+    public static List<Digit> convertDigit(List<Integer> inputIntegerList){
+        List<Digit> newDigitList = new ArrayList<>();
+        for(int i = 0; i < 3; i++) {
+            newDigitList.add(new Digit(i, inputIntegerList.get(i)));
+        }
+        return newDigitList;
+    }
+
+    public static Boolean inputRetryNumber(){
+        System.out.print(GameMessage.RETRY_GAME.getMessage());
         String input = Console.readLine();
         RetryValidator.validate(input);
         int isRetrySignalNumber = Integer.parseInt(input);
         return isRetrySignal(isRetrySignalNumber);
     }
 
-    private static boolean isRetrySignal(int isRetrySignal) {
+    private static Boolean isRetrySignal(int isRetrySignal) {
         return isRetrySignal == RetrySignal.RETRY_TRUE.getSignal();
     }
     private enum RetrySignal {
