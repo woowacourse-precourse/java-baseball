c908ea6583b84c61e4085a1a306a8dbb43fb77bf
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-11-08 12:04:51 +0900

    feat: Player 객체를 만들어라

---
+++ src/main/java/baseball/vo/Player.java
@@ -0,0 +1,11 @@
+package baseball.vo;
+
+import java.util.List;
+
+public class Player {
+    private final List<Integer> userSelectNumbers;
+
+    public Player(List<Integer> userSelectNumbers) {
+        this.userSelectNumbers = userSelectNumbers;
+    }
+}
