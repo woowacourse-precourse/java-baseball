43fb72d6ebb38ad465db4225ecc96231e478efe0
Sumin Kim <42088290+Eeap@users.noreply.github.com>
2022-11-08 09:19:28 +0900

    Test: create computerValue & check repeat

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,6 +3,11 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,7 +24,15 @@ class ApplicationTest extends NsTest {
                 1, 3, 5, 5, 8, 9
         );
     }
-
+    @Test
+    void 컴퓨터_수_생성_및_중복_검사() {
+        List<Integer> computer = Application.inputComputer();
+        for (int item : computer) {
+            assertThat(item).isBetween(1,9);
+        }
+        Set<Integer> numSet = new HashSet<>(computer);
+        assertThat(numSet.size()).isEqualTo(computer.size());
+    }
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
@@ -27,7 +40,6 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
-
     @Override
     public void runMain() {
         Application.main(new String[]{});
