92b5caaa33468760a18bb8bcab654dfa5e5f75e5
jongmyeongbak <withpleasure@gmail.com>
2022-11-08 12:30:59 +0900

    chore: not to change line

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,7 +12,7 @@ public class Application {
     static int strike, ball;
 
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자 야구 게임을 시작합니다.");
         do {
             playSmallGame();
             isKeepGaming();
