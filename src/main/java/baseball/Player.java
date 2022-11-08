688053fa210c9ec31f867c117633330eea841453
Amenable-C <duswo0624@naver.com>
2022-11-08 11:41:19 +0900

    feat : 플레이어가 입력한 수의 예외 처리

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -9,11 +9,43 @@ public class Player {
     public List<Integer> createGuessNumber(){
         String guessNumberString = readLine();
 
+        exceptionHandler(guessNumberString);
+
         List<Integer> guessNumber = new ArrayList<>();
         for(int i = 0; i < 3; i++){
             guessNumber.add(guessNumberString.charAt(i) - '0');
         }
 
         return guessNumber;
+    }
+
+    private void exceptionHandler(String number){
+        IllegalArgumentException illegalArgumentException = new IllegalArgumentException();
+
+        if(!isValidSize(number)
+                || !isValidScope(number)
+                || !isDifferentNumber(number)){
+            throw illegalArgumentException;
+        }
+    }
+
+    private boolean isValidSize(String number){
+        return number.length() == 3;
+    }
+
+    private boolean isValidScope(String number){
+        for(int i = 0; i < 3; i++){
+            if(!(number.charAt(i) >= '1' && number.charAt(i) <= '9')){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean isDifferentNumber(String number){
+        return number.charAt(0) != number.charAt(1)
+                && number.charAt(0) != number.charAt(2)
+                && number.charAt(1) != number.charAt(2);
     }
 }
