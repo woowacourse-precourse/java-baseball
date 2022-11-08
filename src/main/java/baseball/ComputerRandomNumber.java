b6b304d3089bd8b0ae891ef16c0271096a348d51
sujin0607 <goweg0607@gmail.com>
2022-11-08 14:35:11 +0900

    feat: 난수 생성 로직 클래스화

--- src/main/java/baseball/ComputerRandomNumber.java
+++ src/main/java/baseball/ComputerRandomNumber.java
@@ -1,2 +1,30 @@
-package baseball;public class ComputerRandomNumber {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerRandomNumber {
+    private final int LENGTH = 3;
+    private final int STARTRANGE = 1;
+    private final int FINISHRANGE = 9;
+    private List<Integer> computer;
+
+    public List<Integer> randomNumbers() {
+        computer = new ArrayList<>();
+        while (computer.size() < LENGTH) {
+            int randomNum = Randoms.pickNumberInRange(STARTRANGE, FINISHRANGE);
+            computer = isContainNumber(computer, randomNum);
+        }
+        return computer;
+    }
+
+    public List<Integer> isContainNumber(List<Integer> computer, int randomNum) {
+        if (!computer.contains(randomNum)) {
+            computer.add(randomNum);
+        }
+        return computer;
+    }
+
 }
