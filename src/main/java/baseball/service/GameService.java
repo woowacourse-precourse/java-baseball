9eee1e159c710cd0aeb50bdffbb579983f86521e
Laeng <Laeng@users.noreply.github.com>
2022-11-08 20:57:22 +0900

    feat(GameService): pickNumber 메서드 추가

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -3,11 +3,15 @@ package baseball.service;
 import baseball.dao.ComputerNumber;
 import baseball.dto.Number;
 import baseball.vo.*;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class GameService {
+    public Boolean addComputerNumber() {
+        return addComputerNumber();
+    }
     public Boolean addComputerNumber(int number) {
         Number dto = Number.getInstance();
 
@@ -27,5 +31,30 @@ public class GameService {
         TwoNumbers numbers = new TwoNumbers(computerNumber.getNumber(), number);
 
         return numbers.compare();
+    }
+
+    public static int pickNumber(int numberDigit) {
+        int number = pickOneDigitNumber();
+
+        for (int length = 1; length < numberDigit; length++) {
+            number = appendRandomOneDigit(number);
+        }
+
+        return number;
+    }
+
+    private static int pickOneDigitNumber() {
+        return Randoms.pickNumberInRange(1,9);
+    }
+
+    private static int appendRandomOneDigit(int number) {
+        OneDigitNumbers oneDigitNumbers = new OneDigitNumbers(number);
+        int randomOneDigitNumber = pickOneDigitNumber();
+
+        if (oneDigitNumbers.contains(randomOneDigitNumber)) {
+            return appendRandomOneDigit(number);
+        }
+
+        return number + (randomOneDigitNumber * (int) Math.pow(10, oneDigitNumbers.size()));
     }
 }
