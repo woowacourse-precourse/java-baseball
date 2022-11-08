5bb7d3d13ce13898bb72ad00bbaf91adeaedcd14
jindaram-stu <jinsb1999@naver.com>
2022-11-08 14:08:54 +0900

    refactor(ComputerNumber): 컴퓨터 개별 숫자 추가 로직 분리
    들여쓰기를 1로 유지하기 위해 메소드로 로직 분리

--- src/main/java/baseball/game/service/ComputerNumber.java
+++ src/main/java/baseball/game/service/ComputerNumber.java
@@ -1,24 +1,29 @@
 package baseball.game.service;
 
+import baseball.game.constants.Common;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class ComputerNumber {
 
     public static List<Integer> generate() {
         List<Integer> computerNumber = new ArrayList<>();
 
-        while (computerNumber.size() < 3) {
-            int key = Randoms.pickNumberInRange(1, 9);
-            if (!computerNumber.contains(key)) {
-                computerNumber.add(key);
-            }
+        while (computerNumber.size() < Common.ESSENTIAL_LENGTH) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            addComputerNumberDigit(computerNumber,number);
         }
 
         return computerNumber;
     }
 
+    private static void addComputerNumberDigit(List<Integer> computerNumber, int number) {
+        if (!computerNumber.contains(number)) {
+            computerNumber.add(number);
+        }
+    }
 }
-;
