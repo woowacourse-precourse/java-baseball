31cea2a788afa24e3989c055480c415dcaa5ec79
mikekang47 <gyeong0308@gmail.com>
2022-11-08 23:16:45 +0900

    Feat:  컨트롤러 생성과 동시에 게임 시작을 알리는 기능 완료

--- src/main/java/baseball/controllers/GameController.java
+++ src/main/java/baseball/controllers/GameController.java
@@ -1,12 +1,15 @@
 package baseball.controllers;
 
 import baseball.applications.GameService;
+import baseball.views.Output;
 
 public class GameController {
     private volatile static GameController INSTANCE;
     private final GameService gameService;
 
     private GameController() {
+        Output.start();
+
         this.gameService = GameService.getInstance();
     }
 
---
+++ src/main/java/baseball/views/Output.java
@@ -0,0 +1,10 @@
+package baseball.views;
+
+public class Output {
+    private Output() {
+    }
+
+    public static void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+}
