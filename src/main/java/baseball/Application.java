d1e1e3a841408e7cbd69d3255e0311bdba51ae5e
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-08 17:19:28 +0900

    chore: 메인 함수 주석 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,6 @@ import baseball.controller.BaseballController;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         BaseballController baseballController = new BaseballController();
         baseballController.gameStart();
     }
