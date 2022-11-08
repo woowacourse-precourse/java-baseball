13f4019dff8220dadef0b1b4094c8f1ce1432857
hum02 <ehdgma02@gmail.com>
2022-11-08 19:34:32 +0900

    refactor($validation): 검증 메소드들을 InputValidator클래스로 분리
    InputValidator의 메소드
    validateRestartFlag()로 게임재시작 input검증
    validatePlayerNumber()에서 플레이어의 예상 숫자가
    3자리인지, 서로다른 수인지, 1-9의 수인지 검증

--- src/main/java/baseball/util/Input.java
+++ src/main/java/baseball/util/Input.java
@@ -2,17 +2,10 @@ package baseball.util;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static baseball.type.GameFlow.GAME_RESTART;
-import static baseball.type.GameFlow.GAME_END;
-import static baseball.type.BaseballBound.*;
-
+import static baseball.util.InputValidator.validatePlayerNumber;
+import static baseball.util.InputValidator.validateRestartFlag;
 
 public class Input {
-    private static final String RESTART = Integer.toString(GAME_RESTART.getValue());
-    private static final String END = Integer.toString(GAME_END.getValue());
 
     public static String inputRestartFlag() throws IllegalArgumentException {
         String restartFlag = Console.readLine();
@@ -26,41 +19,5 @@ public class Input {
         validatePlayerNumber(playerNumber);
 
         return playerNumber;
-    }
-
-    private static void validateRestartFlag(String restartFlag) throws IllegalArgumentException {
-        if (!((restartFlag.equals(RESTART)) || (restartFlag.equals(END)))) {
-            throw new IllegalArgumentException("잘못된 입력값입니다. 1이나 2를 입력해주세요");
-        }
-    }
-
-    private static void validatePlayerNumber(String playerNumber) throws IllegalArgumentException {
-        validateNumberSize(playerNumber);
-        validateDuplicatedNumber(playerNumber);
-        validateZeroNumber(playerNumber);
-    }
-
-    private static void validateNumberSize(String playerNumber) throws IllegalArgumentException {
-        if (playerNumber.length() != BASEBALL_MAX_SIZE.getValue()) {
-            throw new IllegalArgumentException("잘못된 입력값입니다. 3자리의 수를 입력해주세요");
-        }
-    }
-
-    private static void validateDuplicatedNumber(String playerNumber) throws IllegalArgumentException {
-        List<Character> inputNumbers = new ArrayList<>();
-        for(int i=0; i<playerNumber.length(); i++) {
-            if (inputNumbers.contains(playerNumber.charAt(i))) {
-                throw new IllegalArgumentException("서로다른 수를 입력해야 합니다");
-            }
-            inputNumbers.add(playerNumber.charAt(i));
-        }
-    }
-
-    private static void validateZeroNumber(String playerNumber) throws IllegalArgumentException {
-        for(int i=0; i<playerNumber.length(); i++) {
-            if (playerNumber.charAt(i) == '0') {
-                throw new IllegalArgumentException("잘못된 입력값입니다. 1-9로 이뤄진 수를 입력해주세요");
-            }
-        }
     }
 }
