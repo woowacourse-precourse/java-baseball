f480352e58f2c69209611890956fc3c9c51b2535
HASEUNGHEEE <ariul123@gmail.com>
2022-11-08 12:35:13 +0900

    test: 컴퓨터가 선택한 숫자와 플레이어가 입력한 숫자를 비교하는 기능 테스트

---
+++ src/test/java/baseball/JudgeTest.java
@@ -0,0 +1,45 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.LinkedHashMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JudgeTest {
+    Judge judge;
+    LinkedHashMap<Integer, Integer> computerNumbers = new LinkedHashMap<>();
+    LinkedHashMap<Integer, Integer> playerNumbers = new LinkedHashMap<>();
+
+    @BeforeEach
+    public void setUp() {
+        judge = new Judge();
+
+        // 컴퓨터 숫자 246
+        computerNumbers.put(0, 2);
+        computerNumbers.put(1, 4);
+        computerNumbers.put(2, 6);
+
+        // 플레이어 숫자 267
+        playerNumbers.put(0, 2);
+        playerNumbers.put(1, 6);
+        playerNumbers.put(2, 7);
+    }
+
+    @Test
+    @DisplayName("스트라이크 개수 구하는 기능 테스트")
+    public void countStrike() {
+        int strikeCount;
+        strikeCount = judge.countStrike(computerNumbers, playerNumbers);
+        assertThat(strikeCount).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("깉은 값의 개수를 자리에 관계없이 모두 구하는 기능 테스트")
+    public void countSameNumber() {
+        int sameNumberCount;
+        sameNumberCount = judge.countSameNumber(computerNumbers, playerNumbers);
+        assertThat(sameNumberCount).isEqualTo(2);
+    }
+}
