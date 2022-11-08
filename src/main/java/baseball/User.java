b37cc445e06f65ada5278503ae8197fe9ea9bc32
Giho <poqw741@gmail.com>
2022-11-08 18:29:43 +0900

    feat : 유저 입력 받는 메서드 생성 및 예외 생성

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -6,8 +6,9 @@ import java.util.*;
 
 
 public class User {
+    public static List<Integer> userNumbers = new ArrayList<>();
+
     public static List<Integer> inputGameNumber(){
-        List<Integer> userNumbers = new ArrayList<>();
 
         String StringUserInput = Console.readLine();
 
@@ -16,7 +17,28 @@ public class User {
         for(int i=0; i<StringUserInput.length();i++){
             userNumbers.add(Integer.parseInt(arrayInput[i]));
         }
+
         return userNumbers;
+    }
+
+    public static void validateUserNum(){
+
+        if(userNumbers.size() != 3){
+            throw new IllegalArgumentException("올바른 숫자가 아닙니다.");
+        }
+        // 사이즈가 3이 아닌 경우
+
+        if(userNumbers.contains(0)){
+            throw new IllegalArgumentException("올바른 숫자가 아닙니다.");
+        }
+        // 0을 포함한 경우
+
+        for(int i =0; i< userNumbers.size(); i++){
+            if(Collections.frequency(userNumbers, userNumbers.get(i)) != 1){
+                throw  new IllegalArgumentException("올바른 숫자가 아닙니다. 같은 숫자가 존재합니다.");
+            }
+        }
+        // 중복 숫자를 입력한 경우
     }
 }
 
