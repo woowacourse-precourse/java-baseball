9dd8b844c67c5f6c76401a72531f349909240d0e
0417taehyun <0417taehyun@gmail.com>
2022-11-08 11:54:34 +0900

    test (User): User 클래스 사용자 입력값 관련 메서드 테스트케이스 추가
    사용자 입력값 관련 메서드 테스트케이스 추가:
    - isNumberValidRange 메서드를 활용한 숫자가 아닌 입력값에 대한 테스트
    - isNumberValidRange 메서드를 활용한 유효하지 않은 범위에 대한 테스트
    - isNumberValidMaxSize 메서드를 활용한 길이를 초과한 입력값에 대한 테스트
    - convertInputAsNumber 메서드를 활용한 사용자 입력값 숫자 배열 변환에 대한 테스트

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,29 +3,41 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
     @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
+    void 숫자가_아닌_값_입력() {
+        String input = "1a2";
+        boolean result = false;
+        assertThat(User.isNumberValidRange(input)).isEqualTo(result);
     }
 
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
+    void 길이가_초과한_값_입력() {
+        String input = "1234";
+        boolean result = false;
+        assertThat(User.isNumberValidMaxSize(input)).isEqualTo(result);
+    }
+
+    @Test
+    void 유효하지_않은_범위_내의_값_입력() {
+        String input = "102";
+        boolean result = false;
+        assertThat(User.isNumberValidRange(input)).isEqualTo(result);
+    }
+
+    @Test
+    void 입력값_숫자_변환_테스트() {
+        String input = "123";
+        List<Integer> result = List.of(1, 2, 3);
+        assertThat(User.convertInputAsNumber(input)).isEqualTo(result);
     }
 
     @Override
