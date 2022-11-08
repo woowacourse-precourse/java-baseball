39918b90755f15e14f964958e40898d9561a70fe
can019 <jys01012@gmail.com>
2022-11-08 17:44:23 +0900

    docs(javaDocs): Add java docs to all methods

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,9 @@ public class Application {
         startGame();
     }
 
+    /**
+     * 전체 프로그램을 실행하는 method
+     */
     public static void startGame() {
         int condition = 1;
         while (condition == 1) {
@@ -28,12 +31,20 @@ public class Application {
         System.out.println("게임 종료");
     }
 
+    /**
+     * 게임 종료 혹은 재시작에 필요한 input을 validate
+     * @param input
+     */
     public static void validateProgramConditionInput(String input) {
         if (!input.equals("1") && !input.equals("2")) {
             throw new IllegalArgumentException();
         }
     }
 
+    /**
+     * Baseball game에 사용되는 input을 validate
+     * @param input
+     */
     public static void validateBaseBallInput(String input) {
         if (input.length() != RANDOM_NUMBER_DIGIT_SIZE)
             throw new IllegalArgumentException();
@@ -43,14 +54,30 @@ public class Application {
             throw new IllegalArgumentException();
     }
 
+    /**
+     * String을 int로 변환할 수 있는지 검사
+     * @param str
+     * @return
+     */
     private static boolean isNumeric(String str) {
         return str != null && str.matches("[0-9.]+");
     }
 
+    /**
+     * 사용자의 input을 받음<br/>
+     * camp.nextstep.edu.missionutils.Console의 readLine()을 wrapping한 method
+     *
+     * @return
+     */
     private static String getInput() {
         return Console.readLine();
     }
 
+    /**
+     * String을 tokenize
+     * @param input
+     * @return
+     */
     public static List<Character> tokenizeString(String input) {
         List<Character> tokenizedResult = new ArrayList<>();
 
@@ -61,6 +88,11 @@ public class Application {
         return tokenizedResult;
     }
 
+    /**
+     * Character list를 Integer list로 변환
+     * @param charList
+     * @return
+     */
     public static List<Integer> convertCharListToIntList(List<Character> charList) {
         List<Integer> parseIntResultList = new ArrayList<>();
 
@@ -70,10 +102,21 @@ public class Application {
         return parseIntResultList;
     }
 
+
+    /**
+     * 사용자의 baseball input을 calculateBaseBallResult()의 input 유형으로 변환
+     * @param input
+     * @return
+     */
     public static List<Integer> convertInputToBaseBallInput(String input) {
         return (convertCharListToIntList(tokenizeString(input)));
     }
 
+    /**
+     * Baseball 게임에 사용되는 난수를 생성
+     * @param digitSize 만들고 싶은 난수의 자리수
+     * @return
+     */
     public static List<Integer> generateRandomNumber(int digitSize) {
         List<Integer> generatedRandomNumber = new ArrayList<>();
 
@@ -82,11 +125,16 @@ public class Application {
 
             if (!generatedRandomNumber.contains(randomNumber))
                 generatedRandomNumber.add(randomNumber);
-
         }
         return generatedRandomNumber;
     }
 
+    /**
+     * 게임 초기에 생성된 random number와 사용자 input을 비교해 스트라이크와 볼을 계산
+     * @param randomNumber
+     * @param input
+     * @return index 0 : strike count, index 1 : ball count>
+     */
     public static List<Integer> calculateBaseBallResult(List<Integer> randomNumber, List<Integer> input) {
         int strikeCount = 0;
         int ballCount = 0;
@@ -104,6 +152,11 @@ public class Application {
         return List.of(strikeCount, ballCount);
     }
 
+    /**
+     * Strike count와 ball count를 통해 출력문을 만듬
+     * @param baseballResult index 0 : strike count, index 1 : ball count
+     * @return
+     */
     public static String convertBaseBallResultToConsoleOutputString(List<Integer> baseballResult) {
         int strikeCount = baseballResult.get(0);
         int ballCount = baseballResult.get(1);
@@ -118,6 +171,9 @@ public class Application {
             return ballCount + "볼" + " " + strikeCount + "스트라이크";
     }
 
+    /**
+     * Baseball game을 진행
+     */
     public static void baseBallStart() {
         List<Integer> randomNumber = generateRandomNumber(RANDOM_NUMBER_DIGIT_SIZE);
         List<Integer> currentBaseBallResult;
