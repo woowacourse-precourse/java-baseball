83c3e34887f728ffccdc5beba49015f8138a30cf
metorg <metrog950@gmail.com>
2022-11-08 10:45:06 +0900

    feat: 플레이어 3자리 숫자 입력 값 예외처리 메서드 생성

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -3,16 +3,37 @@ package baseball;
 import java.util.Scanner;
 
 public class View {
+
     private final Scanner scanner;
 
     public View(Scanner scanner) {
         this.scanner = scanner;
     }
 
+
     public String userRequestNumber(){
         System.out.println("숫자를 입력해주세요 : ");
         String input = scanner.nextLine();
 
+        if(userInputException(input)){
+            return input;
+        }
+        else {
+            throw new IllegalArgumentException("입력값 오류로 인하여 프로그램을 종료합니다");
+        }
+    }
+
+    private boolean userInputException(String input){
+
+        if(input.length()!=3)return false;
+
+        for(int i=0;i<input.length();i++){
+            char number = input.charAt(i);
+
+            if(number<'0'||number>'9') return false;
+
+        }
+        return true;
     }
 
 }
