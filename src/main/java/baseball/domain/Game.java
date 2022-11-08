e6b26e1bed53ed21476941a05b58a2c07ce564d2
ys200209 <lys200209@naver.com>
2022-11-08 16:41:23 +0900

    feat(Game.java): 랜덤수 생성 명령하기
    Computer 객체에게 랜덤한 수를 생성하도록 메시지를 보냄

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -4,9 +4,15 @@ package baseball.domain;
  * 숫자야구 게임을 진행하는 클래스
  */
 public class Game {
+    private static Computer computer;
+
+    public Game(Computer computer) {
+        this.computer = computer;
+    }
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        computer.createRandomNumber();
     }
 
 }
