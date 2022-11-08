efb9df36f66121062a5558b465ba35ccc0f2a051
Brilly-Bohyun <bohyun564@swu.ac.kr>
2022-11-08 12:36:30 +0900

    Test: 랜덤 숫자 생성 테스트
    랜덤 숫자가 3개가 생성되는 것이 맞는지 체크함

---
+++ src/test/java/study/RandomNumTest.java
@@ -0,0 +1,18 @@
+package study;
+
+import baseball.Application;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RandomNumTest {
+    @Test
+    @DisplayName("랜덤 숫자 생성 테스트")
+    void RandomTest(){
+        List<Integer> testNum = Application.CreateRandomNum();
+        assertThat(3).isEqualTo(testNum.size());
+    }
+}
