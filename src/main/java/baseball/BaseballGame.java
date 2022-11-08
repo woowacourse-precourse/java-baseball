26c668967036b6875d4dc11a494838711f4fc0c3
jeongwle <dldl4568@naver.com>
2022-11-08 12:13:59 +0900

    refactor(baseballgame) : 기능 분리 및 enum 적용
    enum Command를 적용하고 startGame에 있던 기능을 최대한 분리
    User와 Command, Computer를 멤버변수화, onGame을 지역변수화

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,42 +1,78 @@
 package baseball;
 
+import static baseball.Command.RESTART;
+import static baseball.Command.EXIT;
+
 import java.util.List;
 
 public class BaseballGame {
 
-    private boolean onGame;
+    private final Computer computer;
+    private final User user;
+    private Command command;
+
     public BaseballGame() {
-        onGame = true;
+        computer = new Computer();
+        user = new User();
+        command = null;
     }
 
     public void startGame() {
-        System.out.println(Message.GAME_START_MESSAGE);
-
-        Computer computer = new Computer();
-        User user = new User();
-
-        boolean restart = false;
+        printStartMessage();
+        boolean onGame = true;
 
         while (onGame) {
-            user.receiveThreeNumber();
-            List<Integer> userNumbers = user.getInputNumber();
-
-            computer.calculateScore(userNumbers);
-            computer.printResult();
-
-            user.resetUserNumber();
-            if (computer.isCorrectAnswer()) {
-                user.receiveOneNumber();
-                List<Integer> exitStatus = user.getInputNumber();
-                restart = computer.isRestart(exitStatus);
-                user.resetUserNumber();
-                onGame = restart;
+            runComputer(getUserInput());
+            if (isCorrectAnswer()) {
+                command = getUserCommand();
             }
-            if (restart) {
-                restart = false;
+            if (isRestart()) {
                 computer.setRandomNumber();
+                command = null;
             }
-            computer.clearScore();
+            if (isExit()) {
+                onGame = false;
+            }
+            clearScore();
         }
+    }
+
+    private List<Integer> getUserInput() {
+        user.receiveThreeNumber();
+        List<Integer> userInput = user.getInputNumber();
+        user.resetUserNumber();
+        return userInput;
+    }
+
+    private void runComputer(List<Integer> userInput) {
+        computer.calculateScore(userInput);
+        computer.printResult();
+    }
+
+    private Command getUserCommand() {
+        user.receiveOneNumber();
+        List<Integer> userCommand = user.getInputNumber();
+        user.resetUserNumber();
+        return computer.getCommand(userCommand);
+    }
+
+    private void clearScore() {
+        computer.clearScore();
+    }
+
+    private void printStartMessage() {
+        System.out.println(Message.GAME_START_MESSAGE);
+    }
+
+    private boolean isCorrectAnswer() {
+        return computer.isCorrectAnswer();
+    }
+
+    private boolean isRestart() {
+        return command != null && command.equals(RESTART);
+    }
+
+    private boolean isExit() {
+        return command != null && command.equals(EXIT);
     }
 }
