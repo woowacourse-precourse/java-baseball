9db648b4a9af0a406a329980dfb53ff4acd3ef07
Woogie95 <choisw950928@gmail.com>
2022-11-08 22:31:16 +0900

    refactor: 클래스 명, 메소드 명 수정

--- src/main/java/baseball/MatchScore.java
+++ src/main/java/baseball/MatchScore.java
@@ -1,35 +1,35 @@
 package baseball;
 
-public class ScoreConfirmation {
+public class MatchScore {
 
     private static final int NUMBER_COUNT_NOTHING = 0;
 
-    public static void confirm(NumberCheck numberCheck) {
-        confirmBallStrikeNothingCase(numberCheck);
-        confirmBallCase(numberCheck);
-        confirmStrikeCase(numberCheck);
-        confirmBallStrikeCase(numberCheck);
+    public static void compareMatchScore(NumberCheck numberCheck) {
+        compareBallStrikeNothingCase(numberCheck);
+        compareBallCase(numberCheck);
+        compareStrikeCase(numberCheck);
+        compareBallStrikeCase(numberCheck);
     }
 
-    private static void confirmBallStrikeNothingCase(NumberCheck numberCheck) {
+    private static void compareBallStrikeNothingCase(NumberCheck numberCheck) {
         if (numberCheck.getBall() == NUMBER_COUNT_NOTHING && numberCheck.getStrike() == NUMBER_COUNT_NOTHING) {
             Output.printNothingMessage();
         }
     }
 
-    private static void confirmBallCase(NumberCheck numberCheck) {
+    private static void compareBallCase(NumberCheck numberCheck) {
         if (numberCheck.getBall() > NUMBER_COUNT_NOTHING && numberCheck.getStrike() == NUMBER_COUNT_NOTHING) {
             Output.printBallMessage(numberCheck.getBall());
         }
     }
 
-    private static void confirmStrikeCase(NumberCheck numberCheck) {
+    private static void compareStrikeCase(NumberCheck numberCheck) {
         if (numberCheck.getBall() == NUMBER_COUNT_NOTHING && numberCheck.getStrike() > NUMBER_COUNT_NOTHING) {
             Output.printStrikeMessage(numberCheck.getStrike());
         }
     }
 
-    private static void confirmBallStrikeCase(NumberCheck numberCheck) {
+    private static void compareBallStrikeCase(NumberCheck numberCheck) {
         if (numberCheck.getBall() > NUMBER_COUNT_NOTHING && numberCheck.getStrike() > NUMBER_COUNT_NOTHING) {
             Output.printBallStrikeMessage(numberCheck.getBall(), numberCheck.getStrike());
         }
