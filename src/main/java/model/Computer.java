955e8b564f4fcd132a767da96c7e71d530c2a185
lms0806 <godmlzkf1@naver.com>
2022-11-08 11:41:24 +0900

    feat:컴퓨터가 랜덤 숫자 출력

---
+++ src/main/java/model/Computer.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.util.Arrays;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Computer {
+	private int[] guessnumber;
+
+	public Computer() {
+		this.guessnumber = new int[3];
+	}
+
+	public void setnumber() {
+		int[] guessnumber = new int[3];
+		boolean[] visited = new boolean[10];
+		int count = 0;
+		while (count < 3) {
+			int num = Randoms.pickNumberInRange(1, 9);
+			if (!visited[num]) {
+				visited[num] = true;
+				guessnumber[count] = num;
+				count++;
+			}
+		} // 서로 다른 맞춰야 하는 수
+		System.out.println(Arrays.toString(guessnumber));
+
+		this.guessnumber = guessnumber;
+	}
+
+	public int[] getnumber() {
+		return this.guessnumber;
+	}
+}
