4287cadf987c8e690cb42adc2661eaefdd1166f6
Dabeen Jeong <hello70825@gmail.com>
2022-11-08 21:57:21 +0900

    feat: ball과 strike 수를 저장하는 객체를 추가하다.

---
+++ src/main/java/baseball/domain/BaseballResult.java
@@ -0,0 +1,24 @@
+package baseball.domain;
+
+import baseball.resource.GameMessage;
+
+public class BaseballResult {
+    private int strike;
+    private int ball;
+
+    public int getStrike() {
+        return this.strike;
+    }
+
+    public int getBall() {
+        return this.ball;
+    }
+
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+}
