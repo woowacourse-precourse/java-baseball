2a8470dbb5055ce2f1d4e22dec1c73de8b661a0b
Laeng <Laeng@users.noreply.github.com>
2022-11-08 17:06:03 +0900

    fix(GameController): showPickThreeDigitNumber, showSelectPlayGameAgain 메서드 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,22 +1,41 @@
 package baseball.controller;
 
-import baseball.view.ControlView;
-import baseball.view.IntroView;
-import baseball.view.PlayView;
-import baseball.view.view;
+import baseball.view.PickThreeDigitNumberView;
+import baseball.view.SelectPlayGameAgain;
+import baseball.view.TutorialView;
+import baseball.view.View;
+
+import java.util.regex.Pattern;
 
 public class GameController {
-    public view intro() {
-        return new IntroView();
+    public void showTutorial() {
+        View view = new TutorialView();
+        view.show();
     }
 
-    public view play() {
-        return new PlayView();
+    public String showPickThreeDigitNumber() {
+        View view = new PickThreeDigitNumberView();
+        view.show();
+
+        return view.getResponse();
     }
 
-    public view control() {
-        return new ControlView();
+    public boolean showSelectNumberResult(String input) {
+        return false;
     }
 
+    public String showSelectPlayGameAgain() {
+        View view = new SelectPlayGameAgain();
+        view.show();
 
+        return view.getResponse();
+    }
+
+    public boolean getPlayGameAgainResult(String input) {
+        return true;
+    }
+
+    public static boolean validateInput(String regex, String input) {
+        return (Pattern.matches(regex, input));
+    }
 }
