75d09e00b870098eb34353f5a08d4c368f2bec30
yuntaeLee <leeyt1201@gmail.com>
2022-11-08 14:01:29 +0900

    feat: Player 객체 생성 메서드 구현

--- src/main/java/baseball/service/BaseballGameService.java
+++ src/main/java/baseball/service/BaseballGameService.java
@@ -1,6 +1,9 @@
 package baseball.service;
 
 import baseball.domain.Computer;
+import baseball.domain.Player;
+import baseball.utils.Parser;
+import camp.nextstep.edu.missionutils.Console;
 
 import static baseball.vo.GameInterfaceMsg.GAME_START;
 
@@ -10,9 +13,16 @@ public class BaseballGameService {
     private static final int RESULT_COUNT = 3;
 
     Computer computer;
+    Player player;
+    Parser parser = new Parser();
 
     public void setComputer() {
         System.out.println(GAME_START.getMessage());
         computer = new Computer(START_INCLUSIVE, END_INCLUSIVE, RESULT_COUNT);
+    }
+
+    private void setPlayer() {
+        String playerInput = Console.readLine();
+        player = new Player(parser.parsePlayerNumbers(playerInput));
     }
 }
