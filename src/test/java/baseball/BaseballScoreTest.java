53badf4baf37d21324afc6b42f9b5b1dbe721837
i960107 <i960107@naver.com>
2022-11-08 15:15:29 +0900

    feat(BaseballScore): add unit test of constructor validation

--- src/test/java/baseball/BaseballScoreTest.java
+++ src/test/java/baseball/BaseballScoreTest.java
@@ -1,10 +1,29 @@
 package baseball;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
 public class BaseballScoreTest {
+    @Test
+    void 스트라이크_수가_유효하지_않으면_예외_발생() {
+        assertThatThrownBy(() -> new BaseballScore(-1, 2))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 볼_수가_유효하지_않으면_예외_발생() {
+        assertThatThrownBy(() -> new BaseballScore(0, -1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 스트라이크와_볼의_합이_전체_숫자의_수보다_크면_예외_발생() {
+        assertThatThrownBy(() -> new BaseballScore(BaseballNumber.NUMBER_COUNT, 1))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     @Test
     void 야구_게임_점수를_출력한다_올_스트라이크() {
         BaseballScore score = new BaseballScore(BaseballNumber.NUMBER_COUNT, 0);
