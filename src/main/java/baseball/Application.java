c654736d36688c2f7c2f06e6135ca08f8512d143
tunkcalb <wjdgusdndi@gmail.com>
2022-11-08 16:17:33 +0900

    check wrong number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,13 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        make_number();
+        boolean game = true;
+        start();
+        while(game) {
+            List<Integer> computer = make_number();
+            List<Integer> player = input_number();
+            wrong_number(player);
+        }
     }
     public static void start(){
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -21,11 +27,19 @@ public class Application {
         }
         return computer;
     }
-    public static int input_number(){
-
+    public static List<Integer> input_number(){
+        System.out.println("숫자를 입력해주세요 : ");
+        String player_number_string = Console.readLine();
+        List<Integer> player_number = new ArrayList<>();
+        for(String number : player_number_string.split("")){
+            player_number.add(Integer.parseInt(number));
+        }
+        return player_number;
     }
-    public static int wrong_number(){
-
+    public static void wrong_number(List<Integer> player_number){
+        if (player_number.size() != 3) {
+            throw new IllegalArgumentException("잘못된 값을 입력하셨습니다. 종료합니다.");
+        }
     }
     public static int check_strike(){
 
