6082bee152372b5717e8991e4838e16052d832c3
wwwndus <simock22@gmail.com>
2022-11-08 22:31:00 +0900

    feat: 1부터 9까지의 범위에서 수를 중복되지 안헤 뽑아서 세자리수 만들기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다. ");
+
+        while (true) {
+            List<Integer> computer = createRandomNumbers();
+
+        }
+
+    }
+
+
+
+    public static List<Integer> createRandomNumbers() {
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (randomNumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomNumbers.contains(randomNumber)) {
+                randomNumbers.add(randomNumber);
+            }
+        }
+        return randomNumbers;
     }
 }
