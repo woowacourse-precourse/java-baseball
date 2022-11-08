35f8ee97efb9b10d52bf1a2de6fccfd6640539f5
aran <aran0317@naver.com>
2022-11-08 16:20:27 +0900

    Feat: 야구게임 랜덤 구현
    랜덤 구현 및 input 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,19 +1,38 @@
 package baseball;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
+    static List<Integer> input(){
+        List<Integer> player = new ArrayList<>();
+        return player;
+    }
+
+    static List<Integer> random(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     static void game(){
+        List<Integer> computer = new ArrayList<>();
+        List<Integer> player = new ArrayList<>();
+        computer = random();
+        player = input();
 
     }
     static boolean finish(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         try {
-            InputStreamReader inputStreamReader = new InputStreamReader(System.in);
-            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
-            int restart = Integer.parseInt(bufferedReader.readLine());
+            int restart = Integer.parseInt(Console.readLine());
             if(restart == 1) return false;
             else if(restart == 2) return true;
         }catch(Exception e) {}
