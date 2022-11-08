eab00a7b09839e1bc3cd16e57c3281163950ba30
Heesu Jeong <huissppea@gmail.com>
2022-11-08 23:29:11 +0900

    feat: input 예외 처리(0이 포함된 경우)

--- src/main/java/baseball/InputException.java
+++ src/main/java/baseball/InputException.java
@@ -6,17 +6,29 @@ public class InputException {
 
     public void checkInput(String input){
         BaseballGame baseballGame = new BaseballGame();
-        if(checkSize(input)){
-            throw new IllegalArgumentException("3자리 숫자만 입력 가능");
+        checkSize(input);
+        checkZero(input);
+    }
+
+    public void checkSize(String input){
+        boolean check = false;
+        if (input.length() != 3){
+            check = true;
+            throw new IllegalArgumentException("3자리 숫자만 입력 가능합니다.");
         }
     }
 
-    public boolean checkSize(String input){
-        boolean check = false;
-        if (input.length() != 3){
+    public void checkZero(String input){
 
-            check = true;
+        BaseballGame baseballGame = new BaseballGame();
+        List<Integer> inputNumber = baseballGame.StringToIntList(input);
+
+
+        for(int i = 0; i < inputNumber.size(); i++){
+            System.out.println(inputNumber.get(i));
+            if(inputNumber.get(i).equals(0)){
+                throw new IllegalArgumentException("0은 입력이 불가능합니다.");
+            }
         }
-        return check;
     }
 }
