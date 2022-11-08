577157800014cc6fe051acb6a03e4a72580468ce
kim1387 <kevink0@naver.com>
2022-11-08 21:59:37 +0900

    feat: 점수관련 도메인을 위한 Score 생성
    출력 메시지 만드는 기능
    출력 메시지 프린트하는 기능
    점수를 계산하는 기능

---
+++ src/main/java/baseball/Score.java
@@ -0,0 +1,58 @@
+package baseball;
+
+import java.util.List;
+
+public class Score {
+
+    private Integer ball;
+
+    private Integer strike;
+
+    public Score() {
+        this.ball = 0;
+        this.strike = 0;
+    }
+
+    public Integer getBall() {
+        return ball;
+    }
+
+    public Integer getStrike() {
+        return strike;
+    }
+
+    public void clear() {
+        this.ball = 0;
+        this.strike = 0;
+    }
+
+    public void countTotalScore(final List<ScoreStatus> scoreStatusList){
+
+        for (ScoreStatus scoreStatus : scoreStatusList) {
+            if (scoreStatus.equals(ScoreStatus.STRIKE)){
+                strike++;
+            }
+            if (scoreStatus.equals(ScoreStatus.BALL)){
+                ball ++;
+            }
+        }
+    }
+
+    public String printMessage(){
+        String resultString ="";
+        if (strike == 0 && ball == 0){
+            return ScoreStatus.NOTHING.getMessage();
+        }
+        if (ball !=0){
+            resultString += resultString + ball +ScoreStatus.BALL.getMessage();
+        }
+        if (strike !=0){
+            resultString += resultString + strike +ScoreStatus.STRIKE.getMessage();
+        }
+
+        return resultString;
+    }
+    public void printScore(){
+        System.out.println(printMessage());
+    }
+}
