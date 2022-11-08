182c91b9733d6eb159f5b1fdeb6762b479209417
DavinAn <blueapple1030@gmail.com>
2022-11-08 21:06:55 +0900

    임의의 숫자 발생 메서드, randomNumber 추가
    1부터 9까지 서로 다른 수로 이루어진 3자리의 수를 만든다. 이때 List<Integer> 구조를 사용한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,19 @@
 package baseball;
 
 public class Application {
+
+    public static List<Integer> randomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
+
     public static void main(String[] args) {
         String input;
         List<Integer> computer;
