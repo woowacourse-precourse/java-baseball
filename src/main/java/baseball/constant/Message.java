e623663675eeb2a5050bdf25709afd9657c98636
Amenable-C <duswo0624@naver.com>
2022-11-08 11:31:07 +0900

    feat : 게임 진행에 필요한 메시지 목록 작성

---
+++ src/main/java/baseball/constant/Message.java
@@ -0,0 +1,13 @@
+package baseball.constant;
+
+public class Message {
+    public static final String START_BASEBALL_GAME = "숫자 야구 게임을 시작합니다.";
+    public static final String ENTER_NUMBER = "숫자를 입력해주세요 : ";
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+    public static final String END_BASEBALL_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String RESTART_OR_END = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String RESTART = "1";
+    public static final String END = "2";
+}
