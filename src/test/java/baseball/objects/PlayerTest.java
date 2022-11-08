d68ef079e2817860f61a90d2202110191ed492f8
이경범 <tkdwk567@naver.com>
2022-11-08 19:18:56 +0900

    refactor : Player class test

--- src/test/java/baseball/objects/PlayerTest.java
+++ src/test/java/baseball/objects/PlayerTest.java
@@ -6,32 +6,18 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerTest {
-
-    @Test
-    @DisplayName("데이터가 숫자가 아닌 경우와 3자리가 아닌 경우 IllegalArgumentException 예외 발생")
-    void validation() {
-        // given
-        Player player = new Player();
-        String pushNumber = "a";
-
-        // when && then
-        assertThatThrownBy(() -> player.sayNumbers(pushNumber))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("Number is Not Valid Exception!!");
-    }
 
     @Test
     @DisplayName("플레이어는 숫자 3개를 입력받아 데이터에 대한 유효성 검사와 리스트로 정제하여 저장한다.")
     void sayNumbers() {
         // given
         Player player = new Player();
-        String pushNumber = "123";
+        List<Integer> pushNumbers = List.of(1, 2, 3);
 
         // when
-        player.sayNumbers(pushNumber);
+        player.sayNumbers(pushNumbers);
 
         // then
         assertThat(player.getNumbers()).isEqualTo(List.of(1, 2, 3));
@@ -45,7 +31,7 @@ class PlayerTest {
         Referee referee = new Referee(new Rule());
 
         Player player = new Player();
-        player.sayNumbers("123");
+        player.sayNumbers(List.of(1, 2, 3));
 
         // when
         player.giveNumbersToReferee(referee);
