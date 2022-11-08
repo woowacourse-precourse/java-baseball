fb11072bc3cf74f417410c2b8241f2cffb85946b
dldbdud314 <dldbdud314@naver.com>
2022-11-08 17:23:35 +0900

    fix(InputConverter): 빠진 제한 조건 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,13 +9,12 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
         while (gameStatus) {
-            // 숫자 야구 게임 시작
-            System.out.println("숫자 야구 게임을 시작합니다.");
             // 컴퓨터 난수 생성
             final int computerNumber = generateRandomNumber();
 
-            // 결과 비교
             JudgeResult judgeResult = new JudgeResult(0, 0);
             boolean strikeFlag = false;
             while (!strikeFlag) {
@@ -30,7 +29,6 @@ public class Application {
             }
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 
-            // 게임 계속하겠니?
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
             String continueInput = readLine();
--- src/main/java/baseball/util/InputConverter.java
+++ src/main/java/baseball/util/InputConverter.java
@@ -3,9 +3,11 @@ package baseball.util;
 public class InputConverter {
 
     public static int stringToInteger(String inputString) throws IllegalArgumentException {
-        if (inputString.length() != 3) {
-            throw new IllegalArgumentException();
+        if (inputString.matches("^[1-9]{3}$") &&
+                inputString.length() == inputString.chars().distinct().count()) {
+            return Integer.parseInt(inputString);
         }
-        return Integer.parseInt(inputString);
+
+        throw new IllegalArgumentException();
     }
 }
