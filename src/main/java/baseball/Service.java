686fdba4f5b0f817602c410ceeb580f9da532170
정석준 <tjrwns1010@naver.com>
2022-11-08 23:04:45 +0900

    feat: 상대방(컴퓨터)의 수를 반환하는 메서드 구현

--- src/main/java/baseball/Service.java
+++ src/main/java/baseball/Service.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class Service {
     private int strike, ball;
+
+    public Service() {}
+
+    public String getComNumInput() {
+        List<String> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            String convertedNum = String.valueOf(randomNumber);
+            if (!computer.contains(convertedNum)) {
+                computer.add(convertedNum);
+            }
+        }
+        return String.join("", computer);
+    }
 
     public void compareIdxAndIncreaseScore(int comIdx, int userIdx) {
         if (userIdx == comIdx) {
