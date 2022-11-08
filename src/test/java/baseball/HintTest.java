0df23c6d76f202af0894580d32b105d93a024dd3
saewoo1 <wooozooo1123@gmail.com>
2022-11-08 10:47:26 +0900

    test(HintTest): 카운트 계산 테스트 코드

--- src/test/java/baseball/HintTest.java
+++ src/test/java/baseball/HintTest.java
@@ -2,19 +2,36 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.assertj.core.api.Assertions;
 
 class HintTest {
 
   @Test
-  void ballCount() {
+  void ballCount_계산() {
+    String userNumber = "123";
+    String computerNumber = "123";
+
+    int ballScore = Hint.ballCount(computerNumber, userNumber);
+    Assertions.assertThat(ballScore).isEqualTo(3);
   }
 
   @Test
-  void strikeCount() {
+  void strikeCount_계산() {
+    String userNumber = "123";
+    String computerNumber = "123";
+
+    int strikeCount = Hint.strikeCount(computerNumber, userNumber);
+    Assertions.assertThat(strikeCount).isEqualTo(3);
   }
 
   @Test
-  void ballAndStrike() {
+  void ballAndStrike_카운트_계산() {
+    String userNumber ="123";
+    String computerNumber ="123";
+    int ballScore = Hint.ballCount(computerNumber, userNumber);
+    int strikeScore = Hint.strikeCount(computerNumber, userNumber);
+
+    Assertions.assertThat(Hint.ballAndStrike(computerNumber, userNumber))
+            .isEqualTo(new int[]{ballScore - strikeScore, strikeScore});
   }
 }
