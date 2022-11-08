47b2efe145821cc2615e58b001d5f06ab36aa29c
young2866 <62212610+young2866@users.noreply.github.com>
2022-11-08 15:14:13 +0900

    refactor(Compute): 코드 스타일 적용

--- src/main/java/baseball/Compute.java
+++ src/main/java/baseball/Compute.java
@@ -3,6 +3,7 @@ package baseball;
 import java.util.List;
 
 public class Compute {
+
 	private static final int ZERO = 0;
 	private static final int DIGIT_LENGTH = 3;
 	private static int strike;
@@ -42,13 +43,13 @@ public class Compute {
 			System.out.println("낫싱");
 		}
 		if (ball == 0 && strike > 0) {
-			System.out.println(strike+"스트라이크");
+			System.out.println(strike + "스트라이크");
 		}
 		if (strike == 0 && ball > 0) {
 			System.out.println(ball + "볼");
 		}
 		if (strike > 0 && ball > 0) {
-			System.out.println(ball+ "볼 " + strike +"스트라이크");
+			System.out.println(ball + "볼 " + strike + "스트라이크");
 		}
 	}
 }
