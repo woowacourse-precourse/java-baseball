61dbe521883727cf25b56683900b6b23d54ed766
YE-EUN <musiclove529@naver.com>
2022-11-08 20:42:46 +0900

    refactor(ProblemMaker) : makeProblem 수정 및 코드 정렬
    - numbers 초기화 부분 initialize() 사용하도록 변경
    - 코드 정렬

--- src/main/java/baseball/ProblemMaker.java
+++ src/main/java/baseball/ProblemMaker.java
@@ -2,23 +2,24 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-public class ProblemMaker extends Player{
-    public void makeProblem(int sizeOfNumbers){
-        getNumbers().clear();
-        while(getNumbers().size() < sizeOfNumbers){
+public class ProblemMaker extends Player {
+    public void makeProblem(int sizeOfNumbers) {
+        initialize();
+
+        while (getNumbers().size() < sizeOfNumbers) {
             int randomNum = Randoms.pickNumberInRange(1, 9);
-            if(!getNumbers().contains(randomNum)){
+
+            if (!getNumbers().contains(randomNum)) {
                 addNumber(randomNum);
             }
         }
-
     }
 
-    public boolean hasNumber(int number){
+    public boolean hasNumber(int number) {
         return getNumbers().contains(number);
     }
 
-    public boolean isSameAtGivenLocation(int number, int index){
+    public boolean isSameAtGivenLocation(int number, int index) {
         return getNumbers().get(index).equals(number);
     }
 }
