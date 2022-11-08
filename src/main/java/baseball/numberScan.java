59b896b00498085488ee9d96fde8a4a882d3667b
jasonoh22 <jasonoh22@naver.com>
2022-11-08 22:24:01 +0900

    RETYPE : Continue 삭제 후 nuberScan에 병합

--- src/main/java/baseball/numberScan.java
+++ src/main/java/baseball/numberScan.java
@@ -25,4 +25,17 @@ public class numberScan {
         }
         return Game;
     }
+
+    public boolean decideContinue(){
+        System.out.println("다시 하시겠습니까? 재시작 : 1 , 종료 :2");
+        String input = Console.readLine();
+        validation.startOrEndValidation(input);
+        char inputChar = input.charAt(0);
+        if(inputChar == '1'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
