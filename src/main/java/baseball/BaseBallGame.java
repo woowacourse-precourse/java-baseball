31f2e2737a70464588e42c2a4c84fa21d48d08f1
khj <kim37171@cuk.edu>
2022-11-08 23:48:00 +0900

    feat: getUserNumber 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -7,6 +8,7 @@ import java.util.List;
 
 public class BaseBallGame {
     private static List<Integer> computerNumber;
+    private static List<Integer> userNumber;
     private static boolean isPlay = true;
 
     public void start() {
@@ -18,6 +20,9 @@ public class BaseBallGame {
 
     private void play() {
         computerNumber = makeNumber();
+        while (true) {
+            userNumber = getUserNumber();
+        }
     }
 
     public static List<Integer> makeNumber() {
@@ -29,5 +34,25 @@ public class BaseBallGame {
             }
         }
         return computer;
+    }
+
+    public static List<Integer> getUserNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        List<Integer> user = new ArrayList<>();
+        String input = Console.readLine();
+        System.out.println(input);
+        for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            int n = charToNumber(c);
+            if (input.length() != 3 || c < '1' || c > '9' || user.contains(n)) {
+                throw new IllegalArgumentException("숫자 입력 오류");
+            }
+            user.add(n);
+        }
+        return user;
+    }
+
+    private static int charToNumber(char c) {
+        return c - '0';
     }
 }
