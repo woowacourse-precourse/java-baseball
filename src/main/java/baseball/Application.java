0c87df6c538ed09178fab7f47b4b78a9f9acef4b
Young37 <20171470@g.dongseo.ac.kr>
2022-11-08 16:45:24 +0900

    feat(print): greet at the beginning

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,6 +10,7 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        print("숫자 야구 게임을 시작합니다.\n");
     }
     
     static void print(String text) {
