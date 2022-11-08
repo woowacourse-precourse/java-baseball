71aed2e54ea4d91aef7e86c6b25e34e59e3cd756
James <rlagurwo6209@naver.com>
2022-11-08 19:03:26 +0900

    [docs]:기능 목록 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        startNumberBaseballGame();
+    }
+
+    static List<Integer> answerRandomNumber = new ArrayList<>();
+    static void startNumberBaseballGame(){
+
     }
 }
