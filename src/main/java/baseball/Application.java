2c510e9b89a3065706e547fc5730afb8033ee9a6
권찬영 <kwonchan-yeong@gwonchan-yeong-ui-MacBookPro.local>
2022-11-08 20:40:15 +0900

    feat: import pickNumberInRange, import readLine

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,9 @@ package baseball;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -15,4 +18,5 @@ public class Application {
         }
         return true;
     }
+
 }
