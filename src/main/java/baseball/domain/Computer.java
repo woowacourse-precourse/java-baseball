b6f6458874d8fe94792e39714bffa985bf994409
Jung <anes53027@gmail.com>
2022-11-08 22:19:08 +0900

    feat(Computer) : Computer 클래스 및 생성자 정의

---
+++ src/main/java/baseball/domain/Computer.java
@@ -0,0 +1,16 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+    private List<Integer> computer;
+
+    public Computer() {
+        computer = new ArrayList<>();
+    }
+
+
+}
