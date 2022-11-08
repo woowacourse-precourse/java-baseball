5ee259af8d126c6ae60c1deef295afdbadcbc9d3
kmw2378 <rlarla677@gmail.com>
2022-11-08 21:39:06 +0900

    test (BullsAndCowsResult) - 테스트 클래스도 main 패키지 구조에 맞게 분

---
+++ src/test/java/baseball/BullsAndCowsResultTest.java
@@ -0,0 +1,36 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BullsAndCowsResultTest {
+
+    BullsAndCowsResult bullsAndCowsResult = new BullsAndCowsResult();
+
+    @Test
+    void 게임_결과_객체_초기화() {
+        bullsAndCowsResult.init();
+
+        assertThat(bullsAndCowsResult.getBallCount()).isEqualTo(0);
+        assertThat(bullsAndCowsResult.getStrikeCount()).isEqualTo(0);
+    }
+
+    @Test
+    void 볼_스트라이크_갯수_추가_테스트() {
+        bullsAndCowsResult.init();
+        bullsAndCowsResult.addCount(-1, 1);
+        assertThat(bullsAndCowsResult.getBallCount()).isEqualTo(0);
+        assertThat(bullsAndCowsResult.getStrikeCount()).isEqualTo(0);
+
+        bullsAndCowsResult.init();
+        bullsAndCowsResult.addCount(1, 1);
+        assertThat(bullsAndCowsResult.getBallCount()).isEqualTo(0);
+        assertThat(bullsAndCowsResult.getStrikeCount()).isEqualTo(1);
+
+        bullsAndCowsResult.init();
+        bullsAndCowsResult.addCount(1, 21);
+        assertThat(bullsAndCowsResult.getBallCount()).isEqualTo(1);
+        assertThat(bullsAndCowsResult.getStrikeCount()).isEqualTo(0);
+    }
+}
