f37819084caafb4c225db8272381638ec86d603e
moonn6pence <shin421179@gmail.com>
2022-11-08 17:30:25 +0900

    feat : ReplayNumber 클래스
     validate() : 재시작 input validation 검증 후 boolean 반환

--- src/main/java/baseball/ReplayNumber.java
+++ src/main/java/baseball/ReplayNumber.java
@@ -1,4 +1,16 @@
 package baseball;
 
+import static baseball.Message.END_INPUT;
+import static baseball.Message.REPLAY_INPUT;
+
 public class ReplayNumber {
+
+    public boolean validate(String input) {
+
+        if (input.equals(REPLAY_INPUT) || input.equals(END_INPUT)) {
+            return input.equals(REPLAY_INPUT);
+        }
+
+        throw new IllegalArgumentException();
+    }
 }
