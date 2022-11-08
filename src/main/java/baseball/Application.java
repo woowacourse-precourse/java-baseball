c81387107445925a541f4671715aeff3f7336438
youswim <youswim96@gmail.com>
2022-11-08 22:24:24 +0900

    feat: 각 자릿값이 중복되지 않는, 무작위로 얻어낸 세자리 수 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static List<Integer> getRandomIntList() {
+        List<Integer> randomInt = new ArrayList<>();
+        while (randomInt.size() < 3) {
+            int tempRan = Randoms.pickNumberInRange(1, 9);
+            if (!randomInt.contains(tempRan)) {
+                randomInt.add(tempRan);
+            }
+        }
+        return randomInt;
     }
 }
