f93712698ff8a665ab39beb01be7c96095d95fb0
flown222 <jangu3384@gmail.com>
2022-11-08 21:32:48 +0900

    refactor(ResultMakerTest) : ParameterizedTest를 활용해 코드 축소

--- src/test/java/baseball/model/ResultMakerTest.java
+++ src/test/java/baseball/model/ResultMakerTest.java
@@ -3,18 +3,29 @@ package baseball.model;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class ResultMakerTest {
     private ResultMaker resultMaker;
+    private List<Integer> testList = Arrays.asList(3, 6, 9);
 
     @BeforeEach
     void setUp() {
         resultMaker = new ResultMaker();
     }
 
-    @Test
+    @ParameterizedTest
+    @CsvSource({"3,7,8,1스트라이크", "3,6,5,2스트라이크", "3,6,9,3스트라이크"
+            , "4,8,6,1볼", "9,5,6,2볼", "9,3,6,3볼", "9,6,5,1볼 1스트라이크", "3,9,6,2볼 1스트라이크"
+            , "5,7,1,낫싱"})
+    public void compare(int num1, int num2, int num3, String expected) {
+        String result = resultMaker.compare(testList, Arrays.asList(num1, num2, num3));
+        assertThat(result).isEqualTo(expected);
+    }
+    /*@Test
     void 스트라이크원() {
         String result = resultMaker.compare(Arrays.asList(1,2,3),Arrays.asList(1,4,5));
         assertThat(result).isEqualTo("1스트라이크");
@@ -49,5 +60,5 @@ class ResultMakerTest {
     void 낫싱() {
         String result = resultMaker.compare(Arrays.asList(1,7,8),Arrays.asList(2,3,4));
         assertThat(result).isEqualTo("낫싱");
-    }
+    }*/
 }
