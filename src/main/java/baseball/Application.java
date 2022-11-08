cd2478a5ee8fea628738d30f1d85cfa11cd70a17
yujaemin <woawoa0513@gmail.com>
2022-11-08 23:07:20 +0900

    feat: 메인 함수 구현
    야구 게임 프로그램 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,38 @@
 package baseball;
 
 public class Application {
+    private static final int START_GAME = 1;
+    private static final int EXIT = 2;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        NonPlayerCharacter npc;
+        Player player;
+        RandomNumber randomNumber;
+
+        int menu = START_GAME;
+
+        while (menu == START_GAME) {
+            npc = new NonPlayerCharacter();
+            player = new Player();
+            randomNumber = RandomNumber.getInstance();
+            System.out.println("randomNumber = " + randomNumber.fullNumber);
+            npc.printGameStartNotice();
+
+            while (true) {
+                npc.printNumberInputNotice();
+                int playerInput = player.guessRandomNumber();
+                npc.countNumberOfBallAndStrike(playerInput);
+                npc.printComparisonResult();
+
+                if (playerInput == randomNumber.fullNumber) {
+                    break;
+                }
+            }
+
+            npc.printGameEndNotice();
+            menu = player.selectExitMenu();
+
+            randomNumber.initialize();
+        }
     }
 }
