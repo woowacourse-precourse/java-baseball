668284d969a762168c176eafdfeb26c7352cbb54
3juhwan <13selfesteem91@naver.com>
2022-11-08 19:15:07 +0900

    feat: implement application

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
 public class Application {
+    private static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static Game game;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        boolean isContinue;
+        do {
+            game = new Game();
+            game.run();
+            System.out.println(GAME_RESTART_MESSAGE);
+            isContinue = game.toBeContinue();
+        }while(isContinue);
     }
 }
