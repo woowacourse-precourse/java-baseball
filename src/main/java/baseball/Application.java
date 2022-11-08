8c46065c9694644c0a066e460f878eb3426b3848
show2891 <show2891@gmail.com>
2022-11-08 22:11:55 +0900

    게임 재시작 입력값 기준으로 반복/종료_구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,24 +2,34 @@ package baseball;
 import java.util.Scanner;
 
 public class Application {
-    static String inputThreeNumberByUser() {
+    static String getThreeNumberByUser() {
         System.out.print("숫자를 입력해주세요 : ");
         Scanner input = new Scanner(System.in);
         String userInput = input.nextLine();
         return userInput;
     }
 
+    static Integer getRetryNumberByUser() {
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+        Scanner input = new Scanner(System.in);
+        Integer userInput = input.nextInt();
+        return userInput;
+    }
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
             while (true) {
                 System.out.print("숫자 야구 게임을 시작합니다.\n");
-                String userThreeNumber = inputThreeNumberByUser();
+                String userThreeNumber = getThreeNumberByUser();
 
 
+                Integer userRetryNumber = getRetryNumberByUser();
+                if (userRetryNumber == 1) {
+                    continue;
+                } else if (userRetryNumber == 2) {
+                    break;
+                }
 
-
-                break;
         }
     }
 }
