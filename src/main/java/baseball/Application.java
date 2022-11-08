3188b73879d5aaea436036588e155ffdc0f31c35
wogus4048 <wogus4048@naver.com>
2022-11-08 22:48:28 +0900

    feat : 각 자릿수를 비교해서 Strike인지 Ball인지 판단하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 import java.util.List;
 import camp.nextstep.edu.missionutils.*;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Application {
@@ -30,5 +31,23 @@ public class Application {
             char eachDigit = input.charAt(i);
             usersNumber.add((Character.getNumericValue(eachDigit)));
         }
+    }
+
+    //digitPosition -> 0,1,2 인덱스를 이용하여 백의자리, 십의자리 일의자리를 접근한다.
+    //해당 자리의 값끼리 비교해서 Strike를 체크하고 아니라면 포함여부를 확인하여 Ball인지 체크한다.
+    public static void checkEachDigit(List<Integer> computersNumber, List<Integer> usersNumber,
+        Map<String, Integer> strikeBallCount, int digitPosition) {
+        if (digitPosition == 3) {
+            return;
+        }
+        int computersCurrentPositionDigit = computersNumber.get(digitPosition);
+        int usersCurrentPositionDigit = usersNumber.get(digitPosition);
+        if (computersCurrentPositionDigit == usersCurrentPositionDigit) { // 숫자가 같다면 Strike값 증가
+            strikeBallCount.put("Strike", strikeBallCount.get("Strike") + 1);
+        } else if (computersNumber.contains(
+            usersCurrentPositionDigit)) { //숫자가 같지않다면 수에 해당 숫자가 존재하는지 체크 , 있다면 Ball값 증가
+            strikeBallCount.put("Ball", strikeBallCount.get("Ball") + 1);
+        }
+        checkEachDigit(computersNumber, usersNumber, strikeBallCount, digitPosition + 1);
     }
 }
