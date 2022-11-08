2ebbf5ee1ba2a13c40c96d6f4cd8a29adc1bceab
anneu <b.imnuj@gmail.com>
2022-11-08 16:55:56 +0900

    refactor: 공통으로 사용되는 상수를 분리
    - 여러 곳에서 재사용될 수 있는 상수를 분리해 Constants 클래스에서 관리합니다.

---
+++ src/main/java/baseball/Constants.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public final class Constants {
+    public static final int NUMBER_LENGTH = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+    public static final String RESTART = "1";
+    public static final String STOP = "2";
+
+    private Constants() {
+    }
+}
