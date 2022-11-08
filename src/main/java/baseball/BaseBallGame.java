24d88e53455654ba10068d6621c663aef15e38a8
sujin0607 <goweg0607@gmail.com>
2022-11-08 21:55:28 +0900

    refactor: 파라미터값 수정 및 코드순서 수정

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -6,34 +6,40 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class BaseBallGame {
-    private List<Integer> computer;
-    private List<Integer> user;
-    private String answer;
-
+    private final String REGAME = "1";
+    private final String FINISHGAME = "2";
 
     public void reStart(String userNum) {
-        if (userNum.equals("1")) run();
-        else if (userNum.equals("2")) return;
-        else throw new IllegalArgumentException();
+        if (userNum.equals(REGAME)) {
+            run();
+        } else if (userNum.equals(FINISHGAME)) {
+            return;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 
-    public void run() {
-        Computer computer = new Computer();
-        List<Integer> computerNumber = computer.randomNumbers();
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        Game game = new Game(computerNumber);
-        User user = new User();
 
+    public void run() {
+        //사용할 클래스 생성
+        Computer computer = new Computer();
+        User user = new User();
+        CompareNumber compareNumber = new CompareNumber();
+
+        //난수 생성
+        List<Integer> computerNumber = computer.randomNumbers();
+        compareNumber.setComputer(computerNumber);
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
             System.out.printf("숫자를 입력해주세요 : ");
             String userNum = Console.readLine();
-
             List<Integer> userNumber = user.inputNumbers(userNum);
-            String answer = game.gameRun(userNumber);
-
-            if (answer.equals("게임 종료")) break;
-            else System.out.println(answer);
+            if (compareNumber.gameRun(userNumber)) {
+                break;
+            }
         }
+
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String userNum = Console.readLine();
         reStart(userNum);
