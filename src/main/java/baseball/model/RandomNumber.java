5cddc9fee1032a631ea80a0b14d737ac09f881dc
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-08 16:39:52 +0900

    refactor(RandomNumber): change from default to static method

--- src/main/java/baseball/model/RandomNumber.java
+++ src/main/java/baseball/model/RandomNumber.java
@@ -6,18 +6,18 @@ import java.util.List;
 
 public class RandomNumber {
 
-    public List<Integer> setRandomNumber() {
+    public static List<Integer> setRandomNumber() {
         final int NUMBER_LIST_SIZE = 3;
         List<Integer> randomNumberList = new ArrayList<>();
 
         while (randomNumberList.size() < NUMBER_LIST_SIZE) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            addUniqueNumber(randomNumberList, randomNumber);
+            RandomNumber.addUniqueNumber(randomNumberList, randomNumber);
         }
         return randomNumberList;
     }
 
-    void addUniqueNumber(List<Integer> numberList, int number) {
+    private static void addUniqueNumber(List<Integer> numberList, int number) {
         if (!numberList.contains(number)) {
             numberList.add(number);
         }
