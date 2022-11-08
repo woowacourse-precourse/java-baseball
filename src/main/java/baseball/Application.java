0d52c409051984807bb0f9b78ae7951297268052
LEE JIEUN <jieun3595@soongsil.ac.kr>
2022-11-08 18:56:54 +0900

    feat: 컴퓨터 기능 함수 구현
    컴퓨터의 랜덤으로 뽑는 3개의 값을 list에 넣게 함수를 구현.
    중복되지 않게 uniqueNumber 함수를 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,47 @@
 package baseball;
 
+import java.util.List;
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application main = new Application();
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        boolean again = true;
+        boolean inGame = true;
+
+        List<Integer> computerRanNumList = null;
+
+        while (again) {
+            if (inGame) {
+                inGame = false;
+                computerRanNumList = main.computerNumberList();
+//                System.out.println("computer" + computerRanNumList);
+            }
+        }
     }
+    public int randomPickNumber() {
+        return Randoms.pickNumberInRange(1, 9);
+    }
+    public boolean uniqueNumber(int computerRanNum, List<Integer> computerRanNumList) {
+        if (computerRanNumList.contains((computerRanNum))) {
+            return false;
+        }
+        return true;
+    }
+    public List<Integer> computerNumberList() {
+        List<Integer> computerRanNumList = new ArrayList<>();
+        while (computerRanNumList.size() < 3) {
+            checkToInput(randomPickNumber(), computerRanNumList);
+        }
+        return computerRanNumList;
+    }
+    private void checkToInput(int computerRanNum, List<Integer> computerRanNumList) {
+        if (uniqueNumber(computerRanNum, computerRanNumList)) {
+            computerRanNumList.add(computerRanNum);
+        }
+    }
+
 }
