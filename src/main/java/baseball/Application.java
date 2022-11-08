e89f65d86e6d474e87375d8908d99e07109961d1
DavinAn <blueapple1030@gmail.com>
2022-11-08 21:04:43 +0900

    게임 시작메시지 추가
    게임을 시작하며 사용자에게 숫자를 요구하는 메시지 출력.
    이는 새 게임을 시작할때마다 새로 출력된다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,12 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String input;
+        List<Integer> computer;
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        computer = randomNumber();
+        System.out.print("숫자를 입력해주세요 : ");
+
     }
 }
