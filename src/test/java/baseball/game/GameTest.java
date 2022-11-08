000fc76150e84e86ec201f07e06d52b95d0233ce
BaeDongHyeon <sasd730@gmail.com>
2022-11-08 12:57:51 +0900

    refactor: 불필요 코드 삭제

--- src/test/java/baseball/game/GameTest.java
+++ src/test/java/baseball/game/GameTest.java
@@ -5,11 +5,9 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
 import java.util.Arrays;
 import java.util.Map;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.*;
 
