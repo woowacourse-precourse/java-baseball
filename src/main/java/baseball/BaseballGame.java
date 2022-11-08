50841147face70f85941abffa218662754b5ec7b
Giho <poqw741@gmail.com>
2022-11-08 19:26:48 +0900

    feat : 컴퓨터 숫자와 입력받은 숫자 비교하기

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,2 +1,28 @@
-package baseball;public class BaseballGame {
+package baseball;
+
+import java.util.List;
+
+public class BaseballGame {
+    public static int strike;
+    public static int ball;
+    public static List<Integer> comNum;
+    public static List<Integer> userNumers;
+
+
+
+    public static void ballCount(){
+        strike = 0;
+        ball = 0;
+        for(int i = 0; i < userNumers.size(); i++) {
+            //스트라이크 카운트
+            if (comNum.indexOf(userNumers.get(i)) == i) {
+                strike++;
+                continue;
+            }
+            //볼 카운트
+            if (comNum.contains(userNumers.get(i))) {
+                ball++;
+            }
+        }
+    }
 }
