7c3289180640cc3a105efab1861ab6c6b47ce132
dclxxi <dclxxi@kakao.com>
2022-11-08 13:56:11 +0900

    test: HintTest 클래스의 힌트 메시지 테스트 추가

--- src/test/java/baseball/HintTest.java
+++ src/test/java/baseball/HintTest.java
@@ -20,14 +20,47 @@ public class HintTest {
     }
 
     @Test
-    void 입력받은_수를_판별하여_올바른_힌트문구를_반환한다() {
+    void 입력받은_수를_판별하여_올바른_힌트문구를_반환한다_스트라이크() {
         List<Integer> computerNumbers = new ArrayList<>(Arrays.asList(4, 2, 5));
 
         hint.setCount(computerNumbers, Arrays.asList(1, 2, 3));
         assertEquals(hint.getMessage(), "1스트라이크");
 
-        hint.setCount(computerNumbers, Arrays.asList(4, 5, 6));
+        hint.setCount(computerNumbers, Arrays.asList(1, 2, 5));
+        assertEquals(hint.getMessage(), "2스트라이크");
+
+        hint.setCount(computerNumbers, Arrays.asList(4, 2, 5));
+        assertEquals(hint.getMessage(), "3스트라이크");
+    }
+
+    @Test
+    void 입력받은_수를_판별하여_올바른_힌트문구를_반환한다_볼() {
+        List<Integer> computerNumbers = new ArrayList<>(Arrays.asList(4, 2, 5));
+
+        hint.setCount(computerNumbers, Arrays.asList(1, 5, 3));
+        assertEquals(hint.getMessage(), "1볼");
+
+        hint.setCount(computerNumbers, Arrays.asList(2, 4, 9));
+        assertEquals(hint.getMessage(), "2볼");
+
+        hint.setCount(computerNumbers, Arrays.asList(5, 4, 2));
+        assertEquals(hint.getMessage(), "3볼");
+    }
+
+    @Test
+    void 입력받은_수를_판별하여_올바른_힌트문구를_반환한다_볼_스트라이크() {
+        List<Integer> computerNumbers = new ArrayList<>(Arrays.asList(4, 2, 5));
+
+        hint.setCount(computerNumbers, Arrays.asList(1, 2, 4));
         assertEquals(hint.getMessage(), "1볼 1스트라이크");
+
+        hint.setCount(computerNumbers, Arrays.asList(5, 2, 4));
+        assertEquals(hint.getMessage(), "2볼 1스트라이크");
+    }
+
+    @Test
+    void 입력받은_수를_판별하여_올바른_힌트문구를_반환한다_낫싱() {
+        List<Integer> computerNumbers = new ArrayList<>(Arrays.asList(4, 2, 5));
 
         hint.setCount(computerNumbers, Arrays.asList(7, 8, 9));
         assertEquals(hint.getMessage(), "낫싱");
