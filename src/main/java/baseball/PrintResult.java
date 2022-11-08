b2e395f4a06b3795faf71bd1026b7ed5450dd0e8
iamsseaa <homingsea@gmail.com>
2022-11-08 16:59:28 +0900

    feat: 결과를 출력하는 메소드

--- src/main/java/baseball/PrintResult.java
+++ src/main/java/baseball/PrintResult.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.Arrays;
+
 public class PrintResult {
 
     public static boolean strike(int target, int userNum) {
@@ -33,5 +35,28 @@ public class PrintResult {
         int[] result = {strike, ball};
 
         return result;
+    }
+
+    public static boolean printResult(int[] target, int[] userNum) {
+        int[] result;
+        result = countResult(target, userNum);
+
+        if(result[0] == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        } else {
+            if (result[0] == 0 && result[1] == 0) {
+                System.out.println("낫싱");
+            } else if (result[0] == 0 && result[1] != 0) {
+                System.out.println(result[1] + "볼");
+            } else if (result[0] != 0 && result[1] == 0) {
+                System.out.println(result[0] + "스트라이크");
+            } else {
+                System.out.println(result[1] + "볼 " + result[0] + "스트라이크");
+            }
+
+            return false;
+        }
     }
 }
