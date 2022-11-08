fd0ffc379a4b5d6ce13c56cb0c50f324be84e69a
Gentlee <ekdma3259@gmail.com>
2022-11-08 18:22:28 +0900

    게임 시작을 알리는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
+import java.util.ArrayList;
+import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 public class Application {
+	
+	public static void gameStart() {
+		System.out.println("숫자 야구 게임을 시작합니다.");
+	}
+	
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	
     }
 }
