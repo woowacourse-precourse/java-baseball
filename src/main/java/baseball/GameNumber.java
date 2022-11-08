99bf5f5d664d2a07f626c4df3c16b607bddc4c1b
JeongHunHui <deca3248@gmail.com>
2022-11-08 19:07:58 +0900

    refactor: 게임숫자 유효성 검증 부분을 새로운 클래스를 만들어서 분리

--- src/main/java/baseball/GameNumber.java
+++ src/main/java/baseball/GameNumber.java
@@ -1,9 +1,7 @@
 package baseball;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class GameNumber {
   public static final int NUMBER_DIGIT = 3;
@@ -11,51 +9,18 @@ public class GameNumber {
 
   public void stringToGameNumber(String inputString) {
     List<Integer> numbers = new ArrayList<>();
-    for (char s : inputString.toCharArray()) numbers.add(charToInt(s));
+    for (char s : inputString.toCharArray()) numbers.add(GameNumberValidator.checkIsInt(s));
     setGameNumberByList(numbers);
   }
 
   public void setGameNumberByList(List<Integer> numberList) {
     for (int i : numberList) gameNumberList.add(i);
-    checkNumbersValid();
+    GameNumberValidator.checkNumbersValid(gameNumberList);
   }
 
-  private int charToInt(char inputChar) {
-    int gameNumber = 0;
-    try {
-      gameNumber = Integer.parseInt(String.valueOf(inputChar));
-    } catch (NumberFormatException e) {
-      throwIllegalArgumentExceptionWithMessage("숫자가 아닙니다!");
-    }
-    return gameNumber;
-  }
 
-  private void checkNumbersValid() {
-    if (gameNumberList.size() != NUMBER_DIGIT)
-      throwIllegalArgumentExceptionWithMessage("자리수가 잘못되었습니다!");
-    checkDifferentNumbers();
-    checkIncludeZero();
-  }
-
-  private void checkDifferentNumbers() {
-    Set<Integer> gameNumberSet = new HashSet<>(gameNumberList);
-    if (gameNumberSet.size() != gameNumberList.size())
-      throwIllegalArgumentExceptionWithMessage("중복되는 숫자가 있습니다!");
-  }
-
-  private void checkIncludeZero() {
-    for (int i : gameNumberList) {
-      if (i == 0) throwIllegalArgumentExceptionWithMessage("0이 포함되어 있습니다!");
-    }
-  }
 
   public List<Integer> getGameNumberList() {
     return this.gameNumberList;
-  }
-
-  private void throwIllegalArgumentExceptionWithMessage(String message)
-      throws IllegalArgumentException {
-    System.out.println(message);
-    throw new IllegalArgumentException(message);
   }
 }
