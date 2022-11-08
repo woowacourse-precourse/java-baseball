ead2fc6ae1d285f005a3bdbbaf799b31881055e8
Juuuic <sn990908a@gmail.com>
2022-11-08 16:45:58 +0900

    feat: 컴퓨터 3자리 숫자 생성 method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,5 +8,26 @@ import java.util.List;
 
 public class Application {
 
+    public static final int len = 3; //항상 3자리 수로 고정이므로 len변수에 값 저장
+    private static List<Integer> computerNum; //컴퓨터 숫자
+
+    public static void main(String[] args) {
+
+    }
+
+
+    //1. 컴퓨터 3자리 랜덤 숫자 생성
+    public static List<Integer> getComputerNum() {
+        List<Integer> computerNum = new ArrayList<>();
+        while (computerNum.size() < len) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerNum.contains(randomNumber)) {
+                computerNum.add(randomNumber);
+            }
+        }
+        return computerNum;
+    }
+
+
 
 }
