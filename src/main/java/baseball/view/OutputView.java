e176db2ba8378e106528ef75343fe321e79335ff
Sanha Ko <55884834+headF1rst@users.noreply.github.com>
2022-11-08 19:02:28 +0900

    feat: 결과값을 반환하여 출력하는 객체 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,28 @@
+package baseball.view;
+
+import baseball.domain.GameResult;
+
+public class OutputView {
+    private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String BALL_MESSAGE = "%d볼";
+    private static final String STRIKE_MESSAGE = "%d스트라이크";
+    private static final String WHOLE_COUNT_MESSAGE = "%d스트라이크 %d볼";
+
+    public static void writeGameResult(GameResult gameResult) {
+        String message = getResultMessage(gameResult);
+        System.out.println(message);
+    }
+
+    private static String getResultMessage(GameResult gameResult) {
+        if (gameResult.isNothing()) {
+            return NOTHING_MESSAGE;
+        }
+        if (gameResult.isOnlyBall()) {
+            return String.format(BALL_MESSAGE, gameResult.getBallCounts());
+        }
+        if (gameResult.isOnlyStrike()) {
+            return String.format(STRIKE_MESSAGE, gameResult.getStrikeCounts());
+        }
+        return String.format(WHOLE_COUNT_MESSAGE, gameResult.getStrikeCounts(), gameResult.getBallCounts());
+    }
+}
