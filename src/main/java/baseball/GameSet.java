d86e4df4e7835984b9dfd9f9b8e46cba9f8ff6bc
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-08 20:29:28 +0900

    fix: GameSet class 수정
    문법 오류를 수정, field 초기화 메소드 추가, 상위 메소드에서 예외를 받게 throws 추가, 재실행 메소드 분리, final 제거

--- src/main/java/baseball/GameSet.java
+++ src/main/java/baseball/GameSet.java
@@ -4,29 +4,41 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class GameSet {
 
-    private final Answer answer;
-    private final Input input;
-    private final GameResult gameResult;
+    private Answer answer;
+    private Input input;
+    private GameResult gameResult;
 
-    public GameSet(Answer answer, Input input, GameResult gameResult) {
-        this.answer = answer;
-        this.input = input;
-        this.gameResult = gameResult;
+    public GameSet() {
+        this.answer = new Answer();
+        this.input = new Input();
+        this.gameResult = new GameResult();
     }
 
-    public void startGame() {
+    public void startGame() throws IllegalArgumentException {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         answer.makeAnswers();
-        while (true) {
-            input.makeInputs();
-            gameResult.setGameResult(answer.getAnswers(), input.getInputs());
-            if (gameResult.getResult()) {
-                break;
-            }
+
+        while (!gameResult.isGameEnd()) {
             gameResult.resetGameResult();
+            input.makeInputs();
+            gameResult.setGameResult(input.getInputs(), answer.getAnswers());
+            gameResult.getResult();
+            input.resetInputs();
         }
+        if (gameResult.isThreeStrike()) {
+            reGame();
+
+        }
+
+    }
+
+    private void reGame() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int choice = Integer.parseInt(readLine());
         if (choice == 1) {
+            answer.resetAnswers();
+            input.resetInputs();
+            gameResult.resetGameResult();
             startGame();
         }
     }
