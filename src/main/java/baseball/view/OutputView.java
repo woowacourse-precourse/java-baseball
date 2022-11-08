d9eb2e56f84e32dd86f01eae3ab9bfeba1c0f56f
Sieun Park <psieun0606@gmail.com>
2022-11-08 17:44:40 +0900

    refactor: 성공 메세지 출력 기능 위치 변경

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,10 @@
+package baseball.view;
+
+public class OutputView {
+    private String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public void printGameEnd() {
+        System.out.println(GAME_END);
+    }
+
+}
