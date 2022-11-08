1b6b42b68c1a4f632809fc81c04118547c1cad49
SeungEon <js112099@gmail.com>
2022-11-08 20:18:01 +0900

    service 클래스 구현

--- src/main/java/service/ThreeNumberService.java
+++ src/main/java/service/ThreeNumberService.java
@@ -6,25 +6,62 @@ import java.util.ArrayList;
 
 public class ThreeNumberService {
     private static ThreeNumber computer;
+    private static int ball;
+    private static int strike;
+    private static boolean nothing;
 
     public ThreeNumberService() {
         computer = new ThreeNumber();
     }
 
-    public static void init() {
+    public void reset() {
+        strike = 0;
+        ball = 0;
+        nothing = true;
+    }
+
+    public void init() {
         computer.setMyList();
+
+        reset();
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         System.out.print("숫자를 입력해주세요 : ");
     }
 
-    public int judge(ArrayList<Integer> user) {
-        int sum = 0;
-        for(int i = 0; i < 3; i++) {
-
+    /**
+     * true: strike
+     * false: ball
+     * */
+    public void setStrike(int num, int index) {
+        if(computer.isSameIndex(num, index)) {
+            strike += 1;
         }
-        return 1;
+        else
+        {
+            ball += 1;
+        }
+    }
+
+    public void calculateStrikeOrBall(int num, int index) {
+        if(computer.contains(num)) {
+            setStrike(num, index);
+        }
+    }
+
+    public void judgeResult(ArrayList<Integer> user) {
+        for(int i = 0; i < 3; i++) {
+            calculateStrikeOrBall(user.get(i), i);
+        }
+
+        if(strike != 0 || ball != 0) {
+            nothing = false;
+        }
+        else
+        {
+            nothing = true;
+        }
     }
 
     /**
@@ -37,6 +74,33 @@ public class ThreeNumberService {
         else
         {
             return 0;
+        }
+    }
+
+    public void printStrike() {
+        if(strike == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+        else if(0 < strike && strike < 3)
+        {
+            System.out.println(strike + "스트라이크");
+        }
+    }
+
+    public void printBall() {
+        if(ball != 0) {
+            System.out.println(ball + "볼 ");
+        }
+    }
+
+    public void printResult() {
+        if(nothing) {
+            System.out.println("낫싱");
+        }
+        else
+        {
+
         }
     }
 }
