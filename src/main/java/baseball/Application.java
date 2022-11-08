c8febe07b58cbf3db3d6c57fcf4fdf720c2e0c09
miffyKing <whqudghk28@gmail.com>
2022-11-08 16:52:59 +0900

    feat: Make game work when there's more than 3 numbers of input

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,20 +4,20 @@ import IOController.InputOutputController;
 
 public class Application {
 
-    private static final int PLAYAGAINMODE = 2;
-    private static final int INPUT_CONSOLEMODE = 1;
-    private static final int OUTPUT_CONSOLEMODE = 1;
+    private static final int PLAY_AGAIN_MODE = 2;
+    private static final int INPUT_CONSOLE_MODE = 1;
+    private static final int OUTPUT_CONSOLE_MODE = 1;
     //private static final int OUTPUT_TXTMODE = 2; -> 출력을 텍스트파일에다 하겠다
     //private static final int INPUT_TXTMODE = 2 -> 입력을 텍스트파일로 넣어서 한줄씩 읽겠다.
-    private static final int INPUTLENGTH = 3;
+    private static final int INPUTLENGTH = 4;
 
     public static void main(String[] args) {
         Game game = new Game();
-        InputOutputController ioController = new InputOutputController(INPUT_CONSOLEMODE, OUTPUT_CONSOLEMODE, INPUTLENGTH);
+        InputOutputController ioController = new InputOutputController(INPUT_CONSOLE_MODE, OUTPUT_CONSOLE_MODE, INPUTLENGTH);
 
         int playAgainReturn;
         do {
-            playAgainReturn = game.startAndPlayAgain(ioController);
-        } while (playAgainReturn == PLAYAGAINMODE);
+            playAgainReturn = game.startAndPlayAgain(ioController, INPUTLENGTH);
+        } while (playAgainReturn == PLAY_AGAIN_MODE);
     }
 }
