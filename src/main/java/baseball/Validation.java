111283bae026cfedc559e9605e6451f30f20164a
Dabeen Jeong <hello70825@gmail.com>
2022-11-08 19:57:42 +0900

    feat: Validation에 유저가 입력한 값을 저장해주다.

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -3,9 +3,15 @@ package baseball;
 import baseball.resource.AskRestartGame;
 
 public final class Validation {
+    private String userInput;
 
-    public boolean isCheckRestartInput(String userInput) {
-        if (userInput.equals(AskRestartGame.RESTART_GAME) || userInput.equals(AskRestartGame.END_GAME)) {
+    public void setUserInput(String userInput) {
+        this.userInput = userInput;
+    }
+
+    public boolean isCheckRestartInput() {
+        if (this.userInput.equals(AskRestartGame.RESTART_GAME)
+                || this.userInput.equals(AskRestartGame.END_GAME)) {
             return true;
         }
 
