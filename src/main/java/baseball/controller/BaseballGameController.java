9af9b5a713ad0ffd6515d838adf69a2ecb4dfa11
yuntaeLee <leeyt1201@gmail.com>
2022-11-08 14:22:06 +0900

    Baseball Game Controller 구현

---
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -0,0 +1,25 @@
+package baseball.controller;
+
+import baseball.service.BaseballGameService;
+
+public class BaseballGameController {
+
+    BaseballGameService baseballGameService = new BaseballGameService();
+
+    public void run() {
+        boolean isRun = true;
+
+        while (isRun) {
+            baseballGameService.setComputer();
+            baseballGameService.playBaseballGame();
+
+            if (!runnable()) {
+                isRun = false;
+            }
+        }
+    }
+
+    public boolean runnable() {
+        return baseballGameService.isRestart();
+    }
+}
