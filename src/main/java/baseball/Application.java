d54e9867f76b5877e48e95b0cdaf3ca699df1780
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-08 14:10:24 +0900

    feat(inputExeption) : 예외처리 메서드 중 세부 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
     public static void main(String[] args) {
@@ -16,6 +19,47 @@ public class Application {
         List<Integer> user = inputExeption();
         System.out.println(user);
         return user;
+    }
+    // 입력 예외 처리
+    public static List<Integer> inputExeption() {
+        List<Integer> userInputNum;
+        String num = Console.readLine();
+
+        // 3개가 아닐때
+        if (num.length() != 3) {
+            throw new IllegalArgumentException("숫자를 잘못입력하셨습니다. 게임을 종료합니다.");
+        }
+
+        // 숫자가 아닐때
+        if (!inputExeptionIsDigit(num)) {
+            throw new IllegalArgumentException("숫자를 잘못입력하셨습니다. 게임을 종료합니다.");
+        }
+
+        // 서로 다른게 아닐 때
+        if (!inputExeptionIsDifferent(num)) {
+            throw new IllegalArgumentException("숫자를 잘못입력하셨습니다. 게임을 종료합니다.");
+        }
+
+        // inputNumList 생성
+        userInputNum = stringToListInteger(num);
+        return userInputNum;
+    }
+    public static boolean inputExeptionIsDifferent(String num) {
+        Set strSet = new HashSet();
+        strSet.add(num.charAt(0));
+        strSet.add(num.charAt(1));
+        strSet.add(num.charAt(2));
+
+        if (strSet.size() == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean inputExeptionIsDigit(String num) {
+        boolean isDigit;
+        isDigit = Character.isDigit(num.charAt(0)) && Character.isDigit(num.charAt(1)) && Character.isDigit(num.charAt(2));
+        return isDigit;
     }
     // 난수 발생기
     public static List<Integer> makeRandomInteger() {
