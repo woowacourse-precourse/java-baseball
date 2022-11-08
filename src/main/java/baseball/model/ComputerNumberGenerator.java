5e25d0b98fa38b02aaf0f29c4c5f4d21897d0349
juno-junho <ssmm0205@naver.com>
2022-11-08 18:50:58 +0900

    feat(model): create ComputerNumberGenerator class
    model 패키지 안, 컴퓨터의 임의의 수 생성을 위한 ComputerNumberGenerator 클래스 생성

---
+++ src/main/java/baseball/model/ComputerNumberGenerator.java
@@ -0,0 +1,13 @@
+package baseball.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerNumberGenerator {
+
+    public final List<Integer> numbers;
+
+    public ComputerNumberGenerator() {
+        numbers = new ArrayList<>();
+    }
+}
