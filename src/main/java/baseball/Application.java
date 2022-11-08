b3227f7ebefd340769c6248900a4a6e3eddc4138
권찬영 <kwonchan-yeong@gwonchan-yeong-ui-MacBookPro.local>
2022-11-08 20:34:21 +0900

    feat: create checkPickNumbInMakingNum Method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,4 +4,13 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
+
+    public static boolean checkPickNumbInMakingNum(List<Integer> nowMakingNumber, int pickRandomNum) {
+        for (int i : nowMakingNumber) {
+            if (i == pickRandomNum) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
