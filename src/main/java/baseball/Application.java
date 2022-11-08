f5393e1e0abb8a8f854c871305ec80f2b731134e
wogus4048 <wogus4048@naver.com>
2022-11-08 22:47:52 +0900

    feat : 유저에게 입력값 받아오기 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,8 +2,10 @@ package baseball;
 
 import java.util.List;
 import camp.nextstep.edu.missionutils.*;
+import java.util.Scanner;
 
 public class Application {
+    static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
@@ -14,6 +16,19 @@ public class Application {
             if (!computersNumber.contains(randomNumber)) {
                 computersNumber.add(randomNumber);
             }
+        }
+    }
+
+    public static void getUsersInputNumber(List<Integer> usersNumber) {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = sc.next();
+        if (input.length() > 3) {
+            throw new IllegalArgumentException();
+        }
+        //각 자리수를 옮겨담는다.
+        for (int i = 0; i < 3; i++) {
+            char eachDigit = input.charAt(i);
+            usersNumber.add((Character.getNumericValue(eachDigit)));
         }
     }
 }
