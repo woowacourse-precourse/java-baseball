65d3af4f9939e591c0b4345bb217168ad38acb2a
bando99 <bando991225@gmail.com>
2022-11-08 22:11:19 +0900

    게임 규칙에 관련된 기능

--- src/main/java/baseball/rulls/ball.java
+++ src/main/java/baseball/rulls/ball.java
@@ -3,6 +3,7 @@ package baseball.rulls;
 import java.util.List;
 
 public class ball {
+
     public static boolean isBall(List<Integer> computerList , int userNumber){
         if(computerList.contains(userNumber)){
             return true;
--- src/main/java/baseball/rulls/strike.java
+++ src/main/java/baseball/rulls/strike.java
@@ -1,6 +1,7 @@
 package baseball.rulls;
 
 public class strike {
+
     public static boolean isStrike(int computerNumber ,int userNumber){
         if(computerNumber == userNumber) {
             return true;
