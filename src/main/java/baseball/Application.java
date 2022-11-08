a49e288d0704b9ec8643770887533c20b53b734f
anso33 <hyeon131812@gmail.com>
2022-11-08 19:15:01 +0900

    [FIX] delete unused import

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,5 @@
 package baseball;
 
-import java.util.Scanner;
-
-import static camp.nextstep.edu.missionutils.Console.readLine;
-
 public class Application {
 
   public static void main(String[] args) {
