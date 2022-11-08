9b7cca7fdc093e2f64e34a3ef6791f07982cde95
yhlee9098 <yhlee9098@gmail.com>
2022-11-08 22:46:44 +0900

    Baseball Game v0.1 - 게임 시작 및 진행 기능 - 각 자릿수의 숫자가 서로 다른 3자리 랜덤 숫자를 생성하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,46 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Application {
+    final static int NUMBER_MIN = 1;
+    final static int NUMBER_MAX = 9;
+
+    final static int GAME_PROGRESS = 0;
+    final static int GAME_RESTART = 1;
+    final static int GAME_END = 2;
+
+    public static ArrayList<Integer> SelectNumbers() {
+        ArrayList<Integer> numbers = new ArrayList<>();  // numbers chosen by computer
+
+        while ( numbers.size() < 3 ) {
+            Integer number = Randoms.pickNumberInRange(NUMBER_MIN, NUMBER_MAX);
+
+            if ( !numbers.contains(number) ) {
+                numbers.add(number);
+            }
+        }
+
+        return numbers;
+    }
+
+    public static void BaseballGame() {
+        int gameStatus = GAME_PROGRESS;
+
+        ArrayList<Integer> computerNumbers = SelectNumbers();
+
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 아구 게임을 시작합니다.");
+
+        BaseballGame();
+
+        System.out.println("게임 종료");
     }
 }
