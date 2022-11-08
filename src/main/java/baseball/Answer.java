0ef4d704da276d2c6a26c0981e87d7b62ffdecca
0417taehyun <0417taehyun@gmail.com>
2022-11-08 16:32:06 +0900

    feat (Answer): pickNumberInRange 메서드를 활용한 create 메서드 상세 구현

--- src/main/java/baseball/Answer.java
+++ src/main/java/baseball/Answer.java
@@ -1,13 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class Answer {
+public class Answer implements Number {
     List<Integer> number;
 
     @Override
     public void create() {
-
+        List<Integer> newAnswer = new ArrayList<>();
+        while (newAnswer.size() < MAX_SIZE) {
+            int number = Randoms.pickNumberInRange(START_NUMBER, END_NUMBER);
+            if (!newAnswer.contains(number)) {
+                newAnswer.add(number);
+            }
+        }
+        number = newAnswer;
     }
 
     @Override
