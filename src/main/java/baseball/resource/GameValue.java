bc99f2a112e15d2aba447e7203987f79327ad43f
Dabeen Jeong <hello70825@gmail.com>
2022-11-08 20:29:43 +0900

    feat: 숫자를 상수화하다.

--- src/main/java/baseball/resource/GameValue.java
+++ src/main/java/baseball/resource/GameValue.java
@@ -3,4 +3,6 @@ package baseball.resource;
 public class GameValue {
     public static final Character START_NUMBER = '1';
     public static final Character END_NUMBER = '9';
+
+    public static final Integer CORRECT_LENGTH = 3;
 }
