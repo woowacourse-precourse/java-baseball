b5b1cda2f5e08ee5becbe6e8d714df63ad4c54e4
lqsml <dvlprsml@gmail.com>
2022-11-08 23:50:52 +0900

    2.게임진행 1)1부터 9까지 서로 다른 수로 이뤄진 3자리의 랜덤 숫자 생성 후 저장

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,24 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.*;
+import static camp.nextstep.edu.missionutils.Randoms.*;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+
 public class Application {
 	public static <E> void main(String[] args) {
 		System.out.println("숫자 야구 게임을 시작합니다.");
+		
+		HashSet<Integer> sourceNum = new HashSet<Integer>();
+		for (int i = 0; i < 3; i++) {
+			sourceNum.add(pickNumberInRange(1, 9));
+		}
+		List<Integer> targetNum = new ArrayList<Integer>(sourceNum);
+		String targetNumStr = targetNum.toString();
 		
 	}
 	
