2a76a9cf99f530470b68727506a5a5764fbfb5c0
Young37 <20171470@g.dongseo.ac.kr>
2022-11-08 16:17:20 +0900

    feat(answer): implement the random answer generation method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,12 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -18,5 +22,27 @@ public class Application {
             error.printStackTrace();
         }
         
+    }
+    
+    private static Integer getNewAnswer() {
+        List<Integer> numbers = new ArrayList<>();
+        
+        while (numbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+            
+        }
+        
+        int result = 0;
+        
+        for (int index = numbers.size() - 1, digit = 1; 0 <= index; index--) {
+            result += numbers.get(index) * digit;
+            digit *= 10;
+        }
+        
+        return result;
     }
 }
