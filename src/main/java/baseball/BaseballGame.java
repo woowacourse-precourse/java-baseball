df7d4df8c4f596e09d559890fb2ea8a44f9ca5f6
buddy <jang_taehwan@naver.com>
2022-11-08 11:22:20 +0900

    feat(BaseballGame): 게임 시작, 재실행, 종료

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,16 +1,52 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BaseballGame {
+
+    private Computer computer;
+
+    public BaseballGame() {
+        this.computer = new Computer();
+    }
+
+    public void run() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+
+            List<Integer> answer = inputAnswer();
+            Result result = computer.compareAnswer(answer);
+
+            System.out.println(result);
+
+            if (result.isEnd()) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
+                int command = inputCommand();
+
+                if (command == 1) {
+                    computer.reset();
+                } else if (command == 2) {
+                    break;
+                }
+            }
+        }
+    }
 
     public List<Integer> inputAnswer() {
         String line = Console.readLine();
 
         validateAnswer(line);
 
-        return List.of(Integer.parseInt(line));
+        return Arrays
+            .stream(line.split(""))
+            .map(Integer::parseInt)
+            .collect(Collectors.toList());
     }
 
     public int inputCommand() {
@@ -30,27 +66,8 @@ public class BaseballGame {
     }
 
     private void validateCommand(String command) {
-       if (!command.equals("1") && !command.equals("2")) {
-           throw new IllegalArgumentException("명령은 1 또는 2만 입력 가능합니다.");
-       }
+        if (!command.equals("1") && !command.equals("2")) {
+            throw new IllegalArgumentException("명령은 1 또는 2만 입력 가능합니다.");
+        }
     }
-
-//    public void run() {
-//        /**
-//         * new Computer();
-//         * 게임을 시작합니다.
-//         * (최초 실행 시 바로 문제 맞추기)
-//         * while() {
-//         *  readline();
-//         *  result = computer.inputValue()
-//         *  if(result.equals("3스트라이크") {
-//         *      "정답을 맞췄습니다. 게임 더? 종료?"
-//         *      readline()
-//         *      if (input.eqauls(2)) {
-//         *          "끝~";
-//         *          break;
-//         *      }
-//         * }
-//         * */
-//    }
 }
