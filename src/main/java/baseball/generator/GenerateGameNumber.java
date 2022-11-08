8d78779a8630ba44b9b528da49d766a36160c6fc
ch200203 <ch200205@naver.com>
2022-11-08 13:28:51 +0900

    fix : Randoms.pickNumberInRange 함수 사용

--- src/main/java/baseball/generator/GenerateGameNumber.java
+++ src/main/java/baseball/generator/GenerateGameNumber.java
@@ -1,5 +1,6 @@
 package baseball.generator;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -13,17 +14,18 @@ public class GenerateGameNumber {
     private static final int GAME_MIN_NUM = 0;
     private static final int GAME_MAX_NUM = 3;
 
-    private static List<Integer> numberList = new ArrayList<>();
+    private static List<Integer> computerNumber = new ArrayList<>();
 
     public void GenerateGameNumber() {
     }
 
     public List<Integer> getGenerateNumerList() {
-        this.numberList = IntStream.rangeClosed(RANGE_MIN_NUM, RNAGE_MAX_NUM)
-                .boxed()
-                .collect(Collectors.toList());
-        Collections.shuffle(this.numberList);
-
-        return this.numberList.subList(GAME_MIN_NUM, GAME_MAX_NUM);
+        while(computerNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(RANGE_MIN_NUM, RNAGE_MAX_NUM);
+            if(!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+        return computerNumber;
     }
 }
