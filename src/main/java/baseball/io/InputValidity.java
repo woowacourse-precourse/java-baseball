4eaaf8c26150c4bdcf6580b593d0375aff1ef310
yback1223 <yoyob1223@gmail.com>
2022-11-08 14:05:08 +0900

    refactor: make all the methods non-static

--- src/main/java/baseball/io/InputValidity.java
+++ src/main/java/baseball/io/InputValidity.java
@@ -7,29 +7,28 @@ public class InputValidity {
     private static final char MINIMUM_CHAR = '1';
     private static final char MAXIMUM_CHAR = '9';
 
-    public static void validStringLength(String inputString, int listSize) {
+    public void validStringLength(String inputString, int listSize) {
         if (inputString.length() != listSize) {
             throw new IllegalArgumentException();
         }
     }
 
-    public static void validForList(List<Character> list) {
+    public void validForList(List<Character> list) {
         validNumElementAndNoZero(list);
         validOnlyOneNumInList(list);
     }
 
-    private static void validOnlyOneNumInList(List<Character> list) {
+    private void validOnlyOneNumInList(List<Character> list) {
         if (list.size() != list.stream().distinct().count()) {
             throw new IllegalArgumentException();
         }
     }
 
-    private static void validNumElementAndNoZero(List<Character> list) {
+    private void validNumElementAndNoZero(List<Character> list) {
         for (Character character : list) {
             if (character < MINIMUM_CHAR || MAXIMUM_CHAR < character) {
                 throw new IllegalArgumentException();
             }
         }
     }
-
 }
