a7ead5f113e094f77a290f2e5ade5f8b682e6607
hwanee__ <fkfkthth@naver.com>
2022-11-08 18:37:33 +0900

    feat: 유저 입력 값에대한 유효성 검사
    입력 받은 값이 유효한지 검사

--- src/main/java/baseball/view/UserView.java
+++ src/main/java/baseball/view/UserView.java
@@ -1,18 +1,26 @@
 package baseball.view;
 
+import baseball.model.Validation;
 import camp.nextstep.edu.missionutils.Console;
 
 public class UserView {
 
+    private static final Validation validation = new Validation();
+
     public UserView(){ }
 
     public String UserAnswer(){
-
         System.out.print("숫자를 입력해주세요 : ");
 
         String input = Console.readLine();
 
-        if(숫자 유효성 검사){}
+        if(!validation.CheckString(input)){
+            throw new IllegalArgumentException();
+        }
 
+        return input;
     }
+
+
+
 }
