da9f26c21903d34fba0b087d061e876669974f83
jindaram-stu <jinsb1999@naver.com>
2022-11-08 14:26:49 +0900

    test(ApplicationTest): 여러 상황에 따른 통합테스트 추가
    여러 예외적인 상황 및 정상 루트에 따른 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -9,6 +10,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @DisplayName("게임이 재시작 되는 경우")
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -20,11 +23,42 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @DisplayName("사용자가 입력한 숫자가 3자리가 아닌 경우")
     @Test
-    void 예외_테스트() {
+    void 입력_예외_테스트_1() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("사용자가 입력한 값에 숫자가 아닌 문자가 포함된 경우")
+    @Test
+    void 입력_예외_테스트_2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("가12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("사용자가 입력한 값에 중복된 숫자가 있는 경우")
+    @Test
+    void 입력_예외_테스트_3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("122"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @DisplayName("게임 재시작 여부 입력에 1과 가 아닌 다른 값이 들어간 경우")
+    @Test
+    void 입력_예외_테스트_4() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    assertThatThrownBy(() -> run("135","a"))
+                            .isInstanceOf(IllegalArgumentException.class);
+                },
+                1, 3, 5
         );
     }
 
