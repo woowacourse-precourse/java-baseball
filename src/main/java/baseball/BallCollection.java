d1b7a635fd3918fdd055bbe9a40c41b8c0e0e15d
JunginPark4364 <wjddlsee@gmail.com>
2022-11-08 16:28:12 +0900

    feat(BallCollection): create computer numbers
    Create computer numbers.(3 random number, not duplicate)
    - add BallCollection
    - add constructor
    - add createComputerNumbers method

---
+++ src/main/java/baseball/BallCollection.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BallCollection {
+    public static final int BALL_COUNT = 3;
+
+    private List<Integer> balls;
+
+    public BallCollection(List<Integer> balls) {
+        this.balls = balls;
+    }
+
+    public static BallCollection createComputerNumbers() {
+        List<Integer> balls = new ArrayList<>();
+
+        while (balls.size() < BALL_COUNT) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!balls.contains(randomNumber)) {
+                balls.add(randomNumber);
+            }
+        }
+
+        return new BallCollection(balls);
+    }
+}
