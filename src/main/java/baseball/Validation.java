bdf0b7321039f3a2227d88bdd740bdea82e6b9b5
ddukddaki <rlfrkdms1@naver.com>
2022-11-08 15:00:28 +0900

    feat(Validation): number가 서로다른 수로 이루어져 있는지 확인하는 기능

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -4,6 +4,7 @@ import static baseball.Constant.ERR_MSG;
 import static baseball.Constant.LENGTH_OF_NUMBER;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Validation {
 
@@ -13,4 +14,11 @@ public class Validation {
         }
     }
 
+    public void validateUnlike(List<Integer> number) {
+        number = number.stream().distinct().collect(Collectors.toList());
+        if (number.size() != LENGTH_OF_NUMBER) {
+            throw new IllegalArgumentException(ERR_MSG);
+        }
+    }
+    
 }
