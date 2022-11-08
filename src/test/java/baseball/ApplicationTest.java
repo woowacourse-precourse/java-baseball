f64522909f3eeb285241174421a9db5d8fa423e6
alivejuicy <kkddhh1998@gmail.com>
2022-11-08 16:34:32 +0900

    test : 상대방 랜덤 숫자 생성 테스트 임의의 숫자를 리스트에 할당하고 pickNumberInRange() 메소드에 맞게 로직 수정 고민해보기

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,12 +3,25 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+    private static final int MIN_NUM = 1;
+    private static final int MAX_NUM = 9;
+    private static final int RANDOM_NUM_LENGTH = 3;
+
+    @Test
+    void 랜덤_숫자_생성() {
+        List<Integer> randomNumList = List.of(1, 2, 3);
+        assertThat(randomNumList.size()).isEqualTo(RANDOM_NUM_LENGTH);
+        assertThat(randomNumList.stream().allMatch(num -> num >= MIN_NUM && num <= MAX_NUM)).isTrue();
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
