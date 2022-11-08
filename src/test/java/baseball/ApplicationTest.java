b66badcaa0a9988b687f8b78550859ebea437691
WonSeok-dd <cluejws@gmail.com>
2022-11-08 19:42:49 +0900

    test(ApplicationTest): 게임종료 테스트 케이스 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,6 +21,18 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 게임종료(){
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("153", "135", "2");
+                    assertThat(output()).contains("2볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
