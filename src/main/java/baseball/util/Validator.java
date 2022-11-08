78d3bc02a27980e9a8ff55797c325bf06ef6bb48
seungjun123 <seungjun123@github.com>
2022-11-08 15:09:43 +0900

    feat : 플레이어에게 입력받은 값에 중복된 숫자가 있는지 확인하는 메서드 추가

--- src/main/java/baseball/util/Validator.java
+++ src/main/java/baseball/util/Validator.java
@@ -1,5 +1,8 @@
 package baseball.util;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Validator {
     public void validateIsDigit(Character number) {
         if (!Character.isDigit(number)) {
@@ -15,6 +18,16 @@ public class Validator {
 
     public void validateLength(String numbers, int length) {
         if (numbers.length() != length) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public void validateDuplication(String numbers) {
+        Set<Character> uniqueNumbers = new HashSet<>();
+        for (char number : numbers.toCharArray()) {
+            uniqueNumbers.add(number);
+        }
+        if (uniqueNumbers.size() != Constant.LENGTH) {
             throw new IllegalArgumentException();
         }
     }
