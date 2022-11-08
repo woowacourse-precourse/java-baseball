1fceed9ee9654867dfbaf1129a60edf1258cf52c
geonho <soo_1992@naver.com>
2022-11-08 23:47:43 +0900

    test: 사용자 입력값 유효성 검사

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,6 +21,58 @@ class ApplicationTest extends NsTest {
         assertThat(game.numbersPickedByComputer.get(1)).isNotEqualTo(game.numbersPickedByComputer.get(2));
     }
 
+    @Test
+    void validateLengthOfInputString_입력_문자열의_길이가_3인지_확인() {
+        Game game = new Game();
+        assertThat(game.validateLengthOfInputString("123")).isEqualTo(true);
+        assertThat(game.validateLengthOfInputString("489")).isEqualTo(true);
+        assertThat(game.validateLengthOfInputString("23")).isEqualTo(false);
+        assertThat(game.validateLengthOfInputString("12367")).isEqualTo(false);
+        assertThat(game.validateLengthOfInputString("-asdf")).isEqualTo(false);
+        assertThat(game.validateLengthOfInputString("-as")).isEqualTo(true);
+
+    }
+
+    @Test
+    void validateTypeOfInputString_입력_문자열이_1에서_9_사이의_정수로_이루어졌는지_확인() {
+        Game game = new Game();
+        assertThat(game.validateTypeOfInputString("123")).isEqualTo(true);
+        assertThat(game.validateTypeOfInputString("489")).isEqualTo(true);
+        assertThat(game.validateTypeOfInputString("23")).isEqualTo(true);
+        assertThat(game.validateTypeOfInputString("123670")).isEqualTo(false);
+        assertThat(game.validateTypeOfInputString("-123")).isEqualTo(false);
+        assertThat(game.validateTypeOfInputString("abc")).isEqualTo(false);
+
+    }
+
+    @Test
+    void validateUniquenessOfEachCharacter_입력_문자열이_서로_다른_값으로_이루어져있는지_확인() {
+        Game game = new Game();
+        assertThat(game.validateUniquenessOfEachCharacter("123")).isEqualTo(true);
+        assertThat(game.validateUniquenessOfEachCharacter("484")).isEqualTo(false);
+        assertThat(game.validateUniquenessOfEachCharacter("235")).isEqualTo(true);
+        assertThat(game.validateUniquenessOfEachCharacter("122")).isEqualTo(false);
+        assertThat(game.validateUniquenessOfEachCharacter("6713")).isEqualTo(true);
+        assertThat(game.validateUniquenessOfEachCharacter("abcc")).isEqualTo(false);
+
+    }
+
+    @Test
+    void validateInputNumber_사용자가_선택한_3개_숫자의_유효성_테스트() {
+        Game game = new Game();
+
+        assertThatThrownBy(() -> game.validateInputNumber("1244"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> game.validateInputNumber("144"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> game.validateInputNumber("024"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> game.validateInputNumber("asb144"))
+                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> game.validateInputNumber("abc"))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
 
     @Override
     public void runMain() {
