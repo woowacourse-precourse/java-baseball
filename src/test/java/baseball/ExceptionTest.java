2f7b88eeafedf67841859af2c2ff46a1370d50c3
kksks3192 <kksks3192@gmail.com>
2022-11-08 17:49:17 +0900

    test(exceptiontest): 입력값 길이 예외처리 테스트코드 추가

---
+++ src/test/java/baseball/ExceptionTest.java
@@ -0,0 +1,19 @@
+package baseball;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ExceptionTest {
+
+    @DisplayName("입력값 길이 예외처리")
+    @Test
+    void isInputLengthThree() {
+        Assertions.assertThrows(RuntimeException.class, () -> {
+            String[] inputArray = {"1","2","3","4"};
+            Application.isInputLengthThree(inputArray);
+        });
+    }
+
+}
