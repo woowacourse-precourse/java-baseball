1933e3c7a930f840961596f41bc9b092c5de8e12
jjmin9797 <jjmin9797@naver.com>
2022-11-08 16:47:18 +0900

    feat(GameService): 추가 예외 사항 기능 구현
    feat (feature)
    fix (bug fix)
    docs (documentation)
    style (formatting, missing semi colons, …)
    refactor
    test (when adding missing tests)
    chore (maintain)

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,11 +1,47 @@
 package baseball.service;
 
+import java.util.ArrayList;
+
 public class GameService {
 
 
     public void checkInputPlayerNumber(String inputPlayerNumbers){
-        if (inputPlayerNumbers.length() != 3){
-            throw new IllegalArgumentException();
+        checkForCharacterExistence(inputPlayerNumbers);
+        checkTheLength(inputPlayerNumbers);
+        checkForDuplicateNumber(inputPlayerNumbers);
+        checkThePresenceOfZeros(inputPlayerNumbers);
+    }
+
+    public void checkTheLength(String inputPlayerNumbers){
+        if (inputPlayerNumbers.length() != 3) {
+            throw  new IllegalArgumentException();
         }
     }
+
+    public void checkForDuplicateNumber(String inputPlayerNumbers){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i=0;i<3;i++) {
+            if (numbers.contains(Character.getNumericValue(inputPlayerNumbers.charAt(i)))){
+                throw  new IllegalArgumentException();
+            }
+            numbers.add(Character.getNumericValue(inputPlayerNumbers.charAt(i)));
+        }
+    }
+
+    public void checkThePresenceOfZeros(String inputPlayerNumbers){
+        if (inputPlayerNumbers.contains("0")){
+            throw  new IllegalArgumentException();
+        }
+    }
+
+    public void checkForCharacterExistence(String inputPlayerNumbers){
+        for (int i=0;i<3;i++) {
+            try{
+                Integer.parseInt(inputPlayerNumbers);
+            }catch (IllegalArgumentException e){
+                throw  new IllegalArgumentException();
+            }
+        }
+    }
+
 }
