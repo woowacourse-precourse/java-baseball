303cfce6467f67cb9c3cc64db662bb0148b57a71
wcyang8 <88764490+wcyang8@users.noreply.github.com>
2022-11-08 23:48:51 +0900

    1st test

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,25 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int input;
+        System.out.println("숫자 야구 게임을 시작합니다 \n");
+        while(True) {
+            input = sc.nextint();
+            if(input == 1){
+                continue;
+            }
+            if(input == 2) {
+                return;
+            }
+            if(input < 100 || input >= 1000){
+                return;
+            }
+            check(input);
+
+        }
+    }
+    static void check(int in) {
+
+        return;
     }
 }
