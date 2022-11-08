07faa30ca91eb5f7a534b7723c24a59ba3b78830
ji-junhyuk <takeoutmoneywallet@naver.com>
2022-11-08 13:56:31 +0900

    feat: 점수판(strike, ball 기록)을 만들어라

---
+++ src/main/java/baseball/vo/ScoreBoard.java
@@ -0,0 +1,20 @@
+package baseball.vo;
+
+public class ScoreBoard {
+    private final int strike;
+    private final int ball;
+
+    public CheckScore(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+}
