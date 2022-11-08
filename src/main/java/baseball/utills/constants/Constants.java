55f1d47076cf9c89e83e8376509c82416b662c13
si hwan <pum005@naver.com>
2022-11-08 21:50:32 +0900

    refactor : 매직 넘버 제거, 예외 메시지 추가, 코드 수정

--- src/main/java/baseball/utills/constants/Constants.java
+++ src/main/java/baseball/utills/constants/Constants.java
@@ -1,6 +1,8 @@
 package baseball.utills.constants;
 
 public class Constants {
+    public static final int INPUT_LENGTH = 3;
+    public static final int RESTART_INPUT_LENGTH = 1;
     public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String INPUT_PALCE_HOLDER = "숫자를 입력하세요 : ";
     public static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
--- src/main/java/baseball/utills/validator/Validator.java
+++ src/main/java/baseball/utills/validator/Validator.java
@@ -1,41 +1,42 @@
 package baseball.utills.validator;
 
+import baseball.utills.constants.Constants;
+
 import java.util.Arrays;
 
 public class Validator {
 
     public static boolean validateUserInput(String userInput) throws IllegalArgumentException{
-        if (isThreelength(userInput) && isUnique(userInput) && isOnlyNumber(userInput)) return true;
-        else throw new IllegalArgumentException();
+        if (!(isThreelength(userInput) && isUnique(userInput) && isOnlyNumber(userInput))) {
+            throw new IllegalArgumentException("입력은 반드시 1~9의 3개의 중복없는 숫자여야 합니다.");
+        }
+        return true;
     }
 
     public static boolean isThreelength(String userInput) {
-        if (userInput.length() == 3) return true;
-        else return false;
+        return userInput.length() == Constants.INPUT_LENGTH;
     }
 
     public static boolean isUnique(String userInput) {
-        if (Arrays.stream(userInput.split("")).distinct().count() == 3) return true;
-        else return false;
+        return Arrays.stream(userInput.split("")).distinct().count() == Constants.INPUT_LENGTH;
     }
 
     public static boolean isOnlyNumber(String userInput) {
-        if ( Arrays.stream(userInput.split("")).filter(v -> v.charAt(0) < '1' || v.charAt(0) > '9').count() == 0) return true;
-        else return false;
+        return Arrays.stream(userInput.split("")).filter(v -> v.charAt(0) < '1' || v.charAt(0) > '9').count() == 0;
     }
 
     public static boolean validateOneOrTwo(String userInput) {
-        if (isOneLength(userInput) && isOneOrTwo(userInput)) return true;
-        else throw new IllegalArgumentException();
+        if (!(isOneLength(userInput) && isOneOrTwo(userInput))) {
+            throw new IllegalArgumentException("입력 숫자는 반드시 1 또는 2 여야 합니다.");
+        }
+        return true;
     }
 
     public static boolean isOneLength(String userInput) {
-        if (userInput.length() == 1) return true;
-        else return false;
+        return userInput.length() == Constants.RESTART_INPUT_LENGTH;
     }
 
     public static boolean isOneOrTwo(String userInput) {
-        if (userInput.charAt(0) == '1' || userInput.charAt(0) == '2') return true;
-        else return false;
+        return userInput.charAt(0) == '1' || userInput.charAt(0) == '2';
     }
 }
