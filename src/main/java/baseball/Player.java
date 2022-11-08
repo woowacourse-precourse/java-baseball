d6b9e1eec08bdc61239256ec5f15dee9be1fa187
younghyun-na <younghyunna@naver.com>
2022-11-08 22:23:09 +0900

    :sparkles: feat: 사용자의 입력 값 유효성 검증 기능 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
+
   private final List<Integer> numbers;
+  private final Validator validator;
 
   public Player() {
     this.numbers = new ArrayList<>(3);
+    this.validator = new Validator();
   }
 
   public void setNumbers(String input) {
     for (String number : input.split("")) {
       numbers.add(Integer.parseInt(number));
     }
+    validator.validate(numbers);
   }
 
   public List<Integer> getNumbers() {
---
+++ src/main/java/baseball/constants/Exception.java
@@ -0,0 +1,9 @@
+package baseball.constants;
+
+public class Exception {
+
+  public static final String INVALID_INPUT_LENGTH_ERROR = "입력 값은 3자리 수여야 합니다.";
+  public static final String OUT_OF_RANGE_INPUT_ERROR = "1~9의 범위 내의 숫자만 입력되어야 합니다.";
+  public static final String DUPLICATED_INPUT_ERROR = "중복되는 숫자가 없어야 합니다.";
+
+}
