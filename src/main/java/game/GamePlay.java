13727e6834070735331e286a68a3682d9d3af3d5
kite707 <kite707707@gmail.com>
2022-11-08 15:01:11 +0900

    refactor(game): 정답 길이(현재는 3) 선언

--- src/main/java/game/GamePlay.java
+++ src/main/java/game/GamePlay.java
@@ -1,20 +1,20 @@
 package game;
 
 import camp.nextstep.edu.missionutils.Randoms;
-
 import java.util.ArrayList;
 import java.util.List;
 
 public class GamePlay {
     private static final int ANSWER_START_NUMBER = 1;
     public static final int ANSWER_END_NUMBER = 9;
+    public static final int ANSWER_LENGTH = 3;
 
     public static final int JUDGEMENT_IS_STRIKE = 1;
     public static final int JUDGEMENT_IS_BALL = 2;
     public static final int JUDGEMENT_IS_NOTHING = 3;
     public static List<Integer> getAnswer(){
         List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
+        while (computer.size() < ANSWER_LENGTH) {
             int randomNumber = Randoms.pickNumberInRange(ANSWER_START_NUMBER, ANSWER_END_NUMBER);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
@@ -35,12 +35,15 @@ public class GamePlay {
 
     public static void printHintMessage(int ball, int strike){
         if(ball==0&&strike==0){
-            System.out.println("낫싱");
-        } else if(ball!=0){
-            System.out.println(ball+"볼 "+strike+"스트라이크");
-        }else{
-            System.out.println(strike+"스트라이크");
+            System.out.print("낫싱");
         }
+        if(ball!=0){
+            System.out.print(ball+"볼 ");
+        }
+        if(strike!=0){
+            System.out.print(strike+"스트라이크");
+        }
+        System.out.println();
 
     }
     public static boolean provideHintMessage(List<Integer> answer, List<Integer> userInput){
@@ -58,7 +61,7 @@ public class GamePlay {
             }
         }
         printHintMessage(ball,strike);
-        if(strike==3){
+        if(strike==ANSWER_LENGTH){
             isAnswer = true;
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
