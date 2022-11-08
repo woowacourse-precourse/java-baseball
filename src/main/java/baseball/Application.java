f9030e8fce4982e1bef2f5478fde82d805a80855
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-08 21:37:30 +0900

    Update Application.java

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -132,7 +132,7 @@ public class Application {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int coin = Integer.parseInt(Console.readLine());
         if (coin == 1) {
-        	runMain();
+            runMain();
         } else if (coin == 2) {
         } else {
               예외_테스트();
