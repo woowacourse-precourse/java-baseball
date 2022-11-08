2f7c3b8f8613538387b343a3cf4ae14a61181c8f
LEE-sh1673 <lsh901673@gmail.com>
2022-11-08 12:38:05 +0900

    fix(outputViewTest): 잘못된 테스트 코드 수정
    - static 메서드 호출을 인스턴스 메서드 형식으로 호출하는 코드를 수정

--- src/test/java/baseball/view/OutputViewTest.java
+++ src/test/java/baseball/view/OutputViewTest.java
@@ -22,20 +22,18 @@ public class OutputViewTest {
 	@DisplayName("결과 화면 출력 테스트 - 3스트라이크")
 	@Test
 	void display_결과_화면_출력_3스트라이크() {
-		OutputView outputView = new OutputView();
 		Balls com = new Balls(List.of(4, 2, 5));
 		Balls player = new Balls(List.of(4, 2, 5));
 		CompareResult result = com.compareTo(player);
-		outputView.display(result);
+		OutputView.display(result);
 	}
 
 	@DisplayName("결과 화면 출력 테스트 - 낫싱")
 	@Test
 	void display_결과_화면_출력_낫싱() {
-		OutputView outputView = new OutputView();
 		Balls com = new Balls(List.of(4, 2, 5));
 		Balls player = new Balls(List.of(7, 8, 9));
 		CompareResult result = com.compareTo(player);
-		outputView.display(result);
+		OutputView.display(result);
 	}
 }
