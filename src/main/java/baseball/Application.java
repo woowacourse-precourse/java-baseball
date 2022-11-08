3f8719a2e6a92b74015a54397e3e982270aa747e
sangminee <lsmlee99@gmail.com>
2022-11-08 18:09:01 +0900

    Feat : 상대방(컴퓨터)의 수 랜덤으로 선택

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.io.BufferedReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    static List<Integer> computer;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        selectNumber();
+    }
+
+    private static void selectNumber() {
+        computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+//        System.out.println(computer);
     }
 }
