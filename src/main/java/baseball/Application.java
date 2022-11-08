cc2d75fb33e570f59677775ac5bc36d21bda68cd
김태용 <amico741@naver.com>
2022-11-08 12:41:56 +0900

    refactor: 게임 진행 로직 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,12 +3,16 @@ package baseball;
 
 public class Application {
   public static final int STRIKE_OUT = 3;
+  public static final int EXIT = 2;
   public static void main(String[] args) {
-      System.out.println("숫자 야구 게임을 시작합니다.");
-      while(true){
-          Judge judge = new Judge();
-          decideResult(judge);
+    System.out.println("숫자 야구 게임을 시작합니다.");
+    while(true){
+      Judge judge = new Judge();
+      decideResult(judge);
+      if(judge.decideGameContinue() == EXIT){
+        break;
       }
+    }
   }
 
   public static void printMessage(int strike, int ball){
