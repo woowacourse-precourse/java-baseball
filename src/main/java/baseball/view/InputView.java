bb1d00c7feb59a4e628f8a32efa13c9454fc7afb
footdev <ekclstkfka5@gmail.com>
2022-11-08 21:48:58 +0900

    refactor: 입력 기능 추가
    입력을 책임지는 클래스 InputView 에서 주어진 메소드를 활요해 구현함.

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,10 @@
 package baseball.view;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class InputView {
+
+    public static String read() {
+        return readLine();
+    }
 }
