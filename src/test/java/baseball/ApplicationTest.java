371d349de8b3c47e750ef3e57018aa796a770762
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-08 20:33:54 +0900

    test: ApplicationTest 테스트 코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import baseball.dto.Score;
+import baseball.utility.PlayerNumberValidator;
+import baseball.utility.ResultEvaluator;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
@@ -21,6 +24,18 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void globalTest() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("957", "1", "312", "132", "123", "1", "123", "456", "987", "794", "795", "2");
+                    assertThat(output()).contains("3스트라이크", "3볼", "2볼 1스트라이크", "3스트라이크",
+                            "낫싱", "2볼", "2스트라이크", "3스트라이크", "게임 종료");
+                },
+                9, 5, 7, 1, 2, 3, 7, 9, 5
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
@@ -28,8 +43,84 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void exceptionTest1() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("let it crash"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void exceptionTest2() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("112"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void exceptionTest3() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("989"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void exceptionTest4() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("98 "))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
+    }
+
+    @Test
+    void isPlayerNumberValidTest() {
+        assertThat(PlayerNumberValidator.isPlayerNumberValid("333")).isEqualTo(false);
+        assertThat(PlayerNumberValidator.isPlayerNumberValid("121")).isEqualTo(false);
+        assertThat(PlayerNumberValidator.isPlayerNumberValid("123")).isEqualTo(true);
+        assertThat(PlayerNumberValidator.isPlayerNumberValid("1234")).isEqualTo(false);
+        assertThat(PlayerNumberValidator.isPlayerNumberValid("12a")).isEqualTo(false);
+        assertThat(PlayerNumberValidator.isPlayerNumberValid("12가")).isEqualTo(false);
+        assertThat(PlayerNumberValidator.isPlayerNumberValid("129")).isEqualTo(true);
+        assertThat(PlayerNumberValidator.isPlayerNumberValid("1 9")).isEqualTo(false);
+        assertThat(PlayerNumberValidator.isPlayerNumberValid(null)).isEqualTo(false);
+        assertThat(PlayerNumberValidator.isPlayerNumberValid("")).isEqualTo(false);
+    }
+
+    @Test
+    void isNumericLiteralBetweenOneAndNine() {
+        assertThat(PlayerNumberValidator.isNumericLiteralBetweenOneAndNine('0')).isEqualTo(false);
+        assertThat(PlayerNumberValidator.isNumericLiteralBetweenOneAndNine('1')).isEqualTo(true);
+        assertThat(PlayerNumberValidator.isNumericLiteralBetweenOneAndNine('2')).isEqualTo(true);
+        assertThat(PlayerNumberValidator.isNumericLiteralBetweenOneAndNine('3')).isEqualTo(true);
+        assertThat(PlayerNumberValidator.isNumericLiteralBetweenOneAndNine(' ')).isEqualTo(false);
+        assertThat(PlayerNumberValidator.isNumericLiteralBetweenOneAndNine('\b')).isEqualTo(false);
+    }
+
+    @Test
+    void translateScoreToHintTest() {
+        assertThat(ResultEvaluator.translateScoreToHint(new Score(1, 2))).isEqualTo("1볼 2스트라이크");
+        assertThat(ResultEvaluator.translateScoreToHint(new Score(0, 3))).isEqualTo("3스트라이크");
+        assertThat(ResultEvaluator.translateScoreToHint(new Score(0, 0))).isEqualTo("낫싱");
+
+    }
+
+    @Test
+    void getHintTest() {
+        String computerNumber = "987";
+
+        assertThat(ResultEvaluator.getHint(computerNumber, "912")).isEqualTo("1스트라이크");
+        assertThat(ResultEvaluator.getHint(computerNumber, "987")).isEqualTo("3스트라이크");
+        assertThat(ResultEvaluator.getHint(computerNumber, "187")).isEqualTo("2스트라이크");
+        assertThat(ResultEvaluator.getHint(computerNumber, "937")).isEqualTo("2스트라이크");
+        assertThat(ResultEvaluator.getHint(computerNumber, "798")).isEqualTo("3볼");
+        assertThat(ResultEvaluator.getHint(computerNumber, "718")).isEqualTo("2볼");
     }
 }
