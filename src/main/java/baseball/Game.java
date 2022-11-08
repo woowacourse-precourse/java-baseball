25b7998fff1044991abb47620309821747c44b9c
sonsoowon <yuwoo1018@gmail.com>
2022-11-08 23:13:36 +0900

    refactor(Game): 두 메서드 삭제
    - getResultOfPlayerGuess
    - isAllStrike

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -37,23 +37,6 @@ public class Game {
         return List.of(ball, strike);
     }
 
-    public static String getResultOfPlayerGuess(int ball, int strike){
-        String result = "낫싱";
-        if((strike > 0) && (ball > 0)){
-            result = ball + "볼 " + strike + "스트라이크";
-        } else if(ball > 0){
-            result = ball + "볼";
-        } else if(strike > 0){
-            result = strike + "스트라이크";
-        }
-
-        return result;
-    }
-
-    public static boolean isAllStrike(int strike){
-        return strike == ANSWER_SIZE;
-    }
-
     private int countNumbersContainedInAnswer(List<Integer> playerGuess){
         return (int) playerGuess.stream()
                 .filter(input -> answer.contains(input))
