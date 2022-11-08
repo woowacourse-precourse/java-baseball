1e98fbe3ae53b4a6e83876bd9534552ed6f62bd9
gomljo <gomljo0307@naver.com>
2022-11-08 17:05:12 +0900

    test(GameTest.java): 플레이어(사용자)의 숫자 리스트 초기화 기능 검증
    게임을 시작하기 전 상대방(컴퓨터)의 숫자 리스트를 맞추기 위한 플레이어(사용자)의 숫자 리스트 초기화 기능 검증

--- src/test/java/baseball/game/GameTest.java
+++ src/test/java/baseball/game/GameTest.java
@@ -1,16 +1,32 @@
 package baseball.game;
 
 import baseball.computer.Computer;
+import baseball.player.Player;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 public class GameTest {
+    public static InputStream generateUserInput(String userInput){
+        return new ByteArrayInputStream(userInput.getBytes());
+    }
     @Test
     public void prepare_Computer_Test(){
         Computer computer = new Computer();
         computer.createRandomNumbers();
         assertThat(computer.getDigits()).isNotEmpty();
+    }
+    @Test
+    public void prepare_Player_Test(){
+        Player player = new Player();
+        String userInput="456";
+        InputStream in = generateUserInput(userInput);
+        System.setIn(in);
+        player.createNumbers();
+        assertThat(player.getDigits()).isNotEmpty();
     }
 }
