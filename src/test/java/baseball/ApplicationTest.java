4f436194aae3115f651edcbf5b75159827a4de78
DDing77 <devseo2021@gmail.com>
2022-11-08 21:50:56 +0900

    test($Computer) : 게임 결과 테스트 케이스 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
@@ -19,10 +17,6 @@ class ApplicationTest extends NsTest {
                 },
                 1, 3, 5, 5, 8, 9
         );
-    void 랜덤숫자_길이확인(){
-    void 랜덤숫자_길이확인() {
-        Util util = new Util();
-        assertThat(util.createAnswerString().length()).isEqualTo(3);
     }
 
     @Test
@@ -31,35 +25,94 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
-    void 랜덤숫자_랜덤확인() {  
+    }
+
+    @Test
+    void 랜덤숫자_길이확인() {
+        Computer computer = new Computer();
+        assertThat(computer.createAnswerString().length()).isEqualTo(3);
+    }
+
+    @Test
     void 랜덤숫자_랜덤확인() {
-        Util util = new Util();
-        assertThat(util.createAnswerString()).isNotEqualTo(util.createAnswerString());
+        Computer computer = new Computer();
+        assertThat(computer.createAnswerString()).isNotEqualTo(computer.createAnswerString());
     }
 
     @Test
-    void 볼갯수확인() {
-        Computer computer = new Computer("123");
-        assertThat(computer.countBall("452")).isEqualTo(1);
-        assertThat(computer.countBall("412")).isEqualTo(2);
-        assertThat(computer.countBall("312")).isEqualTo(3);
+    void 낫싱_확인() {
+        Computer result = new Computer();
+
+        assertThat(result.getStatus("000"))
+                .isEqualTo("낫싱");
+
     }
 
     @Test
-    void 스트라이크갯수확인() {
-        Computer computer = new Computer("123");
-        assertThat(computer.countStrike("425")).isEqualTo(1);
-        assertThat(computer.countStrike("143")).isEqualTo(2);
-        assertThat(computer.countStrike("123")).isEqualTo(3);
+    void 볼_갯수_3개_확인() {
+        Computer result = new Computer();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(result.getAnswer().charAt(2))
+                .append(result.getAnswer().charAt(0))
+                .append(result.getAnswer().charAt(1));
+
+        assertThat(result.countBall(sb.toString()))
+                .isEqualTo(3);
+
     }
 
     @Test
-    void 게임결과확인() {
-        Computer computer = new Computer("123");
-        assertThat(computer.getResult("425")).isEqualTo("1 스트라이크");
-        assertThat(computer.getResult("123")).isEqualTo("3 스트라이크");
-        assertThat(computer.getResult("321")).isEqualTo("2 볼 1 스트라이크");
-        assertThat(computer.getResult("312")).isEqualTo("3 볼");
+    void 스트라이크_점수확인_3점() {
+        Computer result = new Computer();
+
+        assertThat(result.countStrike(result.getAnswer()))
+                .isEqualTo(3);
+    }
+
+    @Test
+    void 스트라이크3_3개_메세지_확인() {
+        Computer result = new Computer();
+
+        assertThat(result.getStatus(result.getAnswer()))
+                .isEqualTo("3스트라이크");
+
+    }
+
+    @Test
+    void 스트라이크_점수확인_0점() {
+        Computer result = new Computer();
+
+        assertThat(result.countStrike("000")).isEqualTo(0);
+
+    }
+
+    @Test
+    void 볼_3개_메세지_확인() {
+        Computer result = new Computer();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(result.getAnswer().charAt(2))
+                .append(result.getAnswer().charAt(0))
+                .append(result.getAnswer().charAt(1));
+
+        assertThat(result.getStatus(sb.toString()))
+                .isEqualTo("3볼");
+
+    }
+
+    @Test
+    void 점수_확인_2볼_1스트라이크() {
+        Computer result = new Computer();
+        StringBuilder sb = new StringBuilder();
+
+        sb.append(result.getAnswer().charAt(2))
+                .append(result.getAnswer().charAt(1))
+                .append(result.getAnswer().charAt(0));
+
+        assertThat(result.getStatus(sb.toString()))
+                .isEqualTo("2볼 1스트라이크");
+
     }
 
     @Override
