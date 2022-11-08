5f8a2454c6be40271a97f0691b15b11f893fbbd6
wonho-seo <sjdlrnen0507@naver.com>
2022-11-08 19:18:22 +0900

    fix(baseball.InputMapping.java): fix InputMapping.getConsoleInt, getRestartFlag
    기존 int를 public을 통해 제공하던것하여 밖에서 flag로 조절하려는것을 내부에서 처리하도록 변경
    
    기존 getConsoleInt()는 private화 되었으며 getRestartFlag()을 통해 flag값을 제공한다.

--- src/main/java/baseball/InputMapping.java
+++ src/main/java/baseball/InputMapping.java
@@ -6,36 +6,46 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class InputMapping {
-    public List<Integer> getGameNumberList(){
+    public List<Integer> getGameNumberList() {
         int inputNumber = getConsoleInt();
         if (nonValidateGameNumber(inputNumber))
             throw new IllegalArgumentException("nonValidateInputNumber");
         List<Integer> getInputNumberList = new ArrayList<>();
-        while (inputNumber > 0){
+        while (inputNumber > 0) {
             getInputNumberList.add(inputNumber % 10);
             inputNumber /= 10;
         }
-        return getInputNumberList;    }
+        return getInputNumberList;
+    }
 
-    public int getConsoleInt(){
+    public int getRestartFlag(){
+        int restartFlag = getConsoleInt();
+        if (restartFlag != 1 && restartFlag != 2){
+            throw new IllegalArgumentException();
+        }
+        return restartFlag;
+    }
+
+    private int getConsoleInt() {
         try {
             String line = Console.readLine();
             if (line.isBlank() || !line.matches("\\d+"))
                 throw new IllegalArgumentException();
             return Integer.parseInt(line);
-        } catch (NumberFormatException e){
+        } catch (NumberFormatException e) {
             throw new IllegalArgumentException("out of Int");
         }
     }
 
-    private boolean nonValidateGameNumber(int inputNumber){
+    private boolean nonValidateGameNumber(int inputNumber) {
         if (inputNumber < 111 || inputNumber > 999)
             return true;
-        while (inputNumber > 0){
+        while (inputNumber > 0) {
             if (inputNumber % 10 == 0) {
                 return true;
             }
             inputNumber /= 10;
         }
-        return false;    }
+        return false;
+    }
 }
