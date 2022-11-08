af2827e77125a27acc4abb89a0cd57bd8f26893d
seongbeom <84623098+hi12123123@users.noreply.github.com>
2022-11-08 15:11:15 +0900

    feat : Apllication 부분에서 실행되도록 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,14 @@
 package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         PlayBaseballGame play = new PlayBaseballGame();
-        System.out.println(PlayBaseballGame);
+        play.playGame();
     }
 }
--- src/main/java/baseball/PlayBaseballGame.java
+++ src/main/java/baseball/PlayBaseballGame.java
@@ -6,15 +6,23 @@ import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 
 public class PlayBaseballGame {
-    private Integer total;
-    private Integer strike;
-    private Integer ball;
-    private List<Integer> RandomNum;
-    private List<Integer> UserNum;
+     Integer total;
+     Integer strike;
+     Integer ball;
+     List<Integer> RandomNum;
+     List<Integer> UserNum;
 
-    String userInput = Console.readLine();
-    UserNum = getUserNum(userInput);
-
+    public void playGame(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.print("숫자를 입력해주세요 :");
+        String userInput = Console.readLine();
+        this.UserNum = getUserNum(userInput);
+        countStrike();
+        countTotal();
+        countBall();
+        compareNumber();
+        endGame(strike);
+    }
     public void RandomNum(){
         while(RandomNum.size() < 3){
             int num = Randoms.pickNumberInRange(1,9);
@@ -24,22 +32,22 @@ public class PlayBaseballGame {
         }
     }
 
-    public void checkUserInput(String userInput){
-        if (userInput.length() != 3){
-            new throw IllegalArgumentException();
-        }
-
-        for (int i = 0; i < userInput.length(); i++){
-            if(userInput.charAt(i) == 0){
-                new throw IllegalArgumentException();
-            }
-        }
-
-        // 각 자리 숫자 비교해서 달라야함
-
-    }
+//    public void checkUserInput(String userInput){
+//        if (userInput.length() != 3){
+//            new throw IllegalArgumentException();
+//        }
+//
+//        for (int i = 0; i < userInput.length(); i++){
+//            if(userInput.charAt(i) == 0){
+//                new throw IllegalArgumentException();
+//            }
+//        }
+//
+//        // 각 자리 숫자 비교해서 달라야함
+//    }
 
     public void countTotal(){
+        this.total = 0;
         for (int i = 0; i < RandomNum.size(); i++){
             if (RandomNum.contains(UserNum.get(i))){
                 total++;
@@ -48,6 +56,7 @@ public class PlayBaseballGame {
     }
 
     public void countStrike(){
+        this.strike = 0;
         for (int i = 0; i < RandomNum.size(); i++){
             if (UserNum.get(i) == RandomNum.get(i)){
                 strike++;
@@ -56,6 +65,7 @@ public class PlayBaseballGame {
     }
 
     public void countBall(){
+        this.ball = 0;
         ball = total - strike;
     }
 
@@ -80,6 +90,22 @@ public class PlayBaseballGame {
 
         if (ball == 0 && strike == 0){
             System.out.println("낫싱");
+        }
+    }
+
+    public void endGame(Integer strike){
+        if (strike == 3){
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String answer = Console.readLine();
+
+            // 재시작
+            if (answer.equals("1")){
+            }
+            // 종료
+            if (answer.equals("2")){
+            }
+            throw new IllegalArgumentException("올바른 숫자가 아닙니다. 1 또는 2를 입력하세요.");
         }
     }
 }
