0f69a068bb106d30a99de146e923859c2cc38391
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-08 20:57:52 +0900

    refactor : 999 무한 에러

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -9,18 +9,25 @@ import java.util.Map;
 public class Player {
 
     private static int keyIndex;
+
     // 플레이어 기능 : 상대방(컴퓨터)의 수를 생성
     public static Map<Integer, Integer> createComputerNumber() {
 
-        Map<Integer, Integer> computerRepository = new HashMap<>();
+        Map<Integer, Integer> computerRepository = new HashMap<>(MAX_INDEX);
 
         while (computerRepository.size() < MAX_INDEX) {
 
-            int randomNumber = Randoms.pickNumberInRange(1,9);
+            int randomNumber = Randoms.pickNumberInRange(0,9);
 
-            if (!computerRepository.containsValue(randomNumber)) {
+            if ( !computerRepository.containsValue(randomNumber) ) {
                 computerRepository.put(keyIndex, randomNumber);
                 keyIndex++;
+            }
+
+            if (keyIndex > MAX_INDEX) { keyIndex = 0; }
+            if (randomNumber == 9) {
+                int randomException = Randoms.pickNumberInRange(2, 5);
+                computerRepository.put(keyIndex, randomException);
             }
         }
 
