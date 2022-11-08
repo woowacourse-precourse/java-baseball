0c52c8d67fecb3bfb806690780012a3922bd878b
yeada <yeadalm@gmail.com>
2022-11-08 22:19:59 +0900

    feat : 컴퓨터 숫자 생성 기능 추가

---
+++ src/main/java/baseball/Settings.java
@@ -0,0 +1,22 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Settings {
+    public static List<Integer> makeNumber(){
+        List<Integer> computer = new ArrayList<>();
+
+        while(computer.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!computer.contains(randomNumber)){
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+}
