0110d2b13ba4990b62a29b016919eef01a55422d
kksks3192 <kksks3192@gmail.com>
2022-11-08 18:54:09 +0900

    test(baseballtest): 기능별 테스트코드 분리

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -5,29 +5,13 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-
 import java.io.IOException;
 import java.util.List;
 
-import static baseball.Application.*;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
-import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static baseball.Application.getAnswerNum;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class ApplicationTest extends NsTest {
-
-    @Test
-    void 게임종료_후_재시작() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("246", "135", "1", "597", "589", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
-                },
-                1, 3, 5, 5, 8, 9
-        );
-    }
-
+class BaseballTest {
 
     @DisplayName("기능 요구사항 1")
     @Test
@@ -95,20 +79,4 @@ class ApplicationTest extends NsTest {
     void endGame_메서드_게임종료() {
     }
 
-    @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-                assertThatThrownBy(() -> runException("1234"))
-                        .isInstanceOf(IllegalArgumentException.class)
-        );
-    }
-
-    @Override
-    public void runMain() {
-        try {
-            Application.main(new String[]{});
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
