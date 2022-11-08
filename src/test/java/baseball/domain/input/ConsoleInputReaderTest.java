f3d9565b25970a75681c182918742c7b0e2dfec5
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-08 14:14:45 +0900

    test: "사용자 입력 검증 (다시시작 | 종료)"
    - '1' 일경우 GameFlag.START 반환
    - '2' 일경우 GameFlag.END 반환
    - 입력받은 값이 1이나 2가 아닐 경우 (IllegalArgumentException)

--- src/test/java/baseball/domain/input/ConsoleInputReaderTest.java
+++ src/test/java/baseball/domain/input/ConsoleInputReaderTest.java
@@ -1,9 +1,12 @@
 package baseball.domain.input;
 
+import baseball.controller.GameFlag;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -16,8 +19,13 @@ class ConsoleInputReaderTest {
         @DisplayName("각 자리수가 char 에서 Integer 형으로 변환된다")
         @Test
         void charToInteger() {
+            //given
             char numChar = '1';
+
+            //when
             Integer integer = ConsoleInputReader.convertCharToInteger(numChar);
+
+            //then
             assertThat(integer).isInstanceOf(Integer.class);
             assertThat(integer).isEqualTo(1);
         }
@@ -25,10 +33,15 @@ class ConsoleInputReaderTest {
         @DisplayName("각 자리수를 나누어 List<Integer> 형태로 반환한다.")
         @Test
         void getUserBaseballNumber() {
+            //given
             String number1 = "142";
             String number2 = "827";
+
+            //when
             List<Integer> numberIntegerList1 = ConsoleInputReader.convertToIntegerList(number1);
             List<Integer> numberIntegerList2 = ConsoleInputReader.convertToIntegerList(number2);
+
+            //then
             assertThat(numberIntegerList1.get(0)).isEqualTo(1);
             assertThat(numberIntegerList1.get(1)).isEqualTo(4);
             assertThat(numberIntegerList1.get(2)).isEqualTo(2);
@@ -64,4 +77,61 @@ class ConsoleInputReaderTest {
         }
     }
 
+    @DisplayName("사용자 입력 검증 (다시시작 | 종료)")
+    @Nested
+    class flagTest {
+        @DisplayName("'1'을 입력하면 START 가 반환된다")
+        @Test
+        void start() {
+            //given
+            String userFlag = "1";
+            InputStream in = new ByteArrayInputStream(userFlag.getBytes());
+            System.setIn(in);
+
+            //when
+            GameFlag gameFlag = ConsoleInputReader.getUserOpinion();
+
+            //then
+            assertThat(gameFlag).isEqualTo(GameFlag.START);
+        }
+
+        @DisplayName("'2'을 입력하면 START 가 반환된다")
+        @Test
+        void end() {
+            //given
+            String userFlag = "2";
+            InputStream in = new ByteArrayInputStream(userFlag.getBytes());
+            System.setIn(in);
+
+            //when
+            GameFlag gameFlag = ConsoleInputReader.getUserOpinion();
+
+            //then
+            assertThat(gameFlag).isEqualTo(GameFlag.END);
+        }
+
+        @DisplayName("'1', '2' 이외의 값을 입력할 경우 예외가 발생한다")
+        @Test
+        void exception1() {
+            //given
+            String userFlag = "d";
+            InputStream in = new ByteArrayInputStream(userFlag.getBytes());
+            System.setIn(in);
+
+            //then
+            assertThrows(IllegalArgumentException.class, ConsoleInputReader::getUserOpinion);
+        }
+
+        @DisplayName("'1', '2' 이외의 값을 입력할 경우 예외가 발생한다")
+        @Test
+        void exception2() {
+            //given
+            String userFlag = "3";
+            InputStream in = new ByteArrayInputStream(userFlag.getBytes());
+            System.setIn(in);
+
+            //then
+            assertThrows(IllegalArgumentException.class, ConsoleInputReader::getUserOpinion);
+        }
+    }
 }
