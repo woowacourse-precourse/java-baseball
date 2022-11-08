24bac07c55eca5fcda87feb53048455fc156dc05
leesangjin <tkdwls0628@gmail.com>
2022-11-08 17:35:46 +0900

    test(ApplicationTest): 게임 진행 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
@@ -17,6 +18,39 @@ class ApplicationTest extends NsTest {
                     assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
                 },
                 1, 3, 5, 5, 8, 9
+        );
+    }
+
+    @Test
+    void 게임진행중_3볼_2볼1스트라이크_추가() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("456", "312", "132", "123", "2");
+                    assertThat(output()).contains("낫싱", "3볼", "2볼 1스트라이크" ,"3스트라이크");
+                },
+                1,2,3
+        );
+    }
+
+    @Test
+    void 게임종료_후_2번의_재시작() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("123", "1", "456", "1", "789", "2");
+                    assertThat(output()).contains("3스트라이크", "3스트라이크", "3스트라이크", "게임 종료");
+                },
+                1,2,3,4,5,6,7,8,9
+        );
+    }
+
+    @Test
+    void 정답이후_바로_게임종료() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("456", "123", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "게임 종료");
+                },
+                1,2,3
         );
     }
 
