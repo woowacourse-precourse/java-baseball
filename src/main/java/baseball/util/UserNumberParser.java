f86c86ec794bbb0ffd8354b67da0d5574e2dc67f
kiarakim <kiarakim413@gmail.com>
2022-11-08 22:24:23 +0900

    feat : check each range of input 'string' number

--- src/main/java/baseball/util/UserNumberParser.java
+++ src/main/java/baseball/util/UserNumberParser.java
@@ -6,6 +6,9 @@ public class UserNumberParser {
         int[] numbers = new int[size];
 
         for (int i = 0; i < size; i++) {
+            if (!checkRange(input, i)) {
+                throw new IllegalArgumentException();
+            }
             numbers[i] = toInt(input, i);
         }
         return numbers;
@@ -15,6 +18,10 @@ public class UserNumberParser {
         if (input.length() != size) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private Boolean checkRange(String input, int index) {
+        return '0' <= input.charAt(index) && input.charAt(index) <= '9';
     }
 
     private int toInt(String input, int index) {
