f53c43cbc656f5cbedf94427e580a773b989a1d3
xxeol2 <jhj01177@naver.com>
2022-11-08 17:31:59 +0900

    refactor: 불필요한 public 제거

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -5,11 +5,11 @@ import java.util.List;
 
 public interface View {
 
-    public List<Integer> inputIntegerList();
+    List<Integer> inputIntegerList();
 
-    public Finish inputFinishCode();
+    Finish inputFinishCode();
 
-    public void printHint(Hint hint);
+    void printHint(Hint hint);
 
-    public void printMessage(String message);
+    void printMessage(String message);
 }
