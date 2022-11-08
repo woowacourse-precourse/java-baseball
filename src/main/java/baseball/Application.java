a48d2de3fef0a942eb7002b783857aca39ccab7e
hyejuc <chjjudy@naver.com>
2022-11-08 16:57:28 +0900

    feat: 게임 실행 기능 추가
    playGame: 게임 실행 부분 메서드로 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,46 +1,56 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        playGame();
+
+    }
+    public static void playGame() {
+
         System.out.println("숫자 야구 게임을 시작합니다.");
         do {
             System.out.print("숫자를 입력해주세요 : ");
             int userNumber = Integer.parseInt(Console.readLine());
-            if (!checkException(userNumber)){
+            if (!checkException(userNumber)) {
                 throw new IllegalArgumentException();
             }
             //낫싱, 볼, 스트라이크
         }
-//        while (3 스트라이크){
-//            //게임 종료
-//        }
+        while () {
+            //게임 종료
+        }
     }
-    public static boolean checkException(int num){
+
+    public static boolean checkException(int num) {
         String numString = Integer.toString(num);
-        if (numString.length() != 3){
+        if (numString.length() != 3) {
             return false;
         }
-        if (numString.contains("0")){
+        if (numString.contains("0")) {
             return false;
         }
-        if (!containSameNumber(numString)){
+        if (!containSameNumber(numString)) {
             return false;
         }
         return true;
     }
-    public static boolean containSameNumber(String numString){
+    public static boolean containSameNumber(String numString) {
         HashMap<Character, Integer> checkSameNumber = new HashMap<>();
         for (int i = 0; i < numString.length(); i++) {
-            if (checkSameNumber.containsKey(numString.charAt(i))){
+            if (checkSameNumber.containsKey(numString.charAt(i))) {
                 return false;
             }
             checkSameNumber.put(numString.charAt(i), i);
         }
         return true;
     }
+
 }
