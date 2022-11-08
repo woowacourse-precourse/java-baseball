89205d9ba07ab971d1bf7e9bb3049585ea718143
jwjin95 <jwjin95@yonsei.ac.kr>
2022-11-08 19:18:51 +0900

    feat: 정답 generation 메서드 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,6 +1,11 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BaseballGame {
 
@@ -15,6 +20,21 @@ public class BaseballGame {
 
     public void printInputMessage() {
         System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public String generateAnswer() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        StringBuilder answer = new StringBuilder();
+        for (int i = 0; i < 3; i++) {
+            answer.append(computer.get(i));
+        }
+        return answer.toString();
     }
 
     public String getUserInput() {
