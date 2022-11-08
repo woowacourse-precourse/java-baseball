810ab3324a407a1de00ac2fb474aed47826b2c2f
masiljangajji <hjk172262@gmail.com>
2022-11-08 22:22:56 +0900

    feat: 정답 난수 생성

--- src/main/java/baseball/InputNumber.java
+++ src/main/java/baseball/InputNumber.java
@@ -1,2 +1,25 @@
-package baseball;public class InputNumber {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputNumber {
+    List<Integer> computerBaseBallNumber = new ArrayList<>();
+
+
+    public void inputComputerBaseballNumber() {
+        while (computerBaseBallNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerBaseBallNumber.contains(randomNumber)) {
+                computerBaseBallNumber.add(randomNumber);
+            }
+        }
+    }
+
+
+
+
 }
