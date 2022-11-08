998c3125fb17ddeb863f701c90db0421d8a695d9
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-08 20:31:31 +0900

    fix: GameResult class 수정
    결과 출력 메소드의 기능을 분리하면서 flag추가, constructor 문법오류 수정, 초기화 메서드 추가

--- src/main/java/baseball/GameResult.java
+++ src/main/java/baseball/GameResult.java
@@ -7,18 +7,24 @@ public class GameResult {
     private int strike;
     private int ball;
 
-    public GameResult(int strike, int ball) {
-        this.strike = strike;
-        this.ball = ball;
+    private boolean isGameEnd;
+
+    public GameResult() {
+        this.strike = 0;
+        this.ball = 0;
+        this.isGameEnd = false;
+
     }
 
-    public void setGameResult(List<Integer> answers, List<Integer> inputs) {
-        for (int i = 0; i < 3; i++) {
+    public void setGameResult(List<Integer> inputs, List<Integer> answers) {
+        int i = 0;
+        while (i < answers.size()) {
             if (answers.get(i).equals(inputs.get(i))) {
                 strike++;
             } else if (answers.contains(inputs.get(i))) {
                 ball++;
             }
+            i++;
         }
     }
 
@@ -30,23 +36,33 @@ public class GameResult {
         return strike == 3;
     }
 
-    public  boolean getResult() {
+    public void getResult() {
         if (isNothing()) {
             System.out.println("낫싱");
         }
         if (isThreeStrike()) {
-            System.out.println("3스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            return true;
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            isGameEnd = true;
         }
-        if (strike == 0) {
+        if (strike > 0 && ball > 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }else if (strike > 0) {
+            System.out.println(strike + "스트라이크");
+        }else if (ball > 0) {
             System.out.println(ball + "볼");
         }
-        if (ball == 0) {
-            System.out.println(strike + "스트라이크");
-        }
-        if (strike != 0 && ball != 0) {
-            System.out.println(strike + "스트라이크 " + ball + "볼");
-        }
-        return false;
+
+    }
+
+    public boolean isGameEnd() {
+        return isGameEnd;
+    }
+
+    public void resetGameResult() {
+        strike = 0;
+        ball = 0;
+        isGameEnd = false;
+
     }
 }
