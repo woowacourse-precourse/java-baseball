8863988af7a9ac0ec2f328a1fb5f23a0f911590f
yback1223 <yoyob1223@gmail.com>
2022-11-08 14:05:33 +0900

    refactor: make inputvalidity variable

--- src/main/java/baseball/io/Input.java
+++ src/main/java/baseball/io/Input.java
@@ -4,17 +4,19 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Input extends EndOrRe{
+public class Input extends EndOrRe {
 
     private final int listSize;
+    private final InputValidity inputValidity;
 
     public Input(int listSize) {
         this.listSize = listSize;
+        this.inputValidity = new InputValidity();
     }
 
     public List<Character> readAndMakeInputList() {
         String inputString = Console.readLine();
-        InputValidity.validStringLength(inputString, listSize);
+        inputValidity.validStringLength(inputString, listSize);
         return stringToList(inputString);
     }
 
@@ -23,14 +25,13 @@ public class Input extends EndOrRe{
     }
 
     /**
-     * 테스트를 위한 메소드
-     * 문자열을 입력 받았다고 생각하고 클래스의 메소드들을 테스트한다.
+     * 테스트를 위한 메소드 문자열을 입력 받았다고 생각하고 클래스의 메소드들을 테스트한다.
      *
      * @param inputString Console.readLine()으로 입력받은 문자열이라고 가정한 파라미터
      * @return 입력받은 문자열을 리스트로 만들어 반환
      */
     public List<Character> testReadAndMakeInputList(String inputString) {
-        InputValidity.validStringLength(inputString, listSize);
+        inputValidity.validStringLength(inputString, listSize);
         return stringToList(inputString);
     }
 
@@ -39,7 +40,7 @@ public class Input extends EndOrRe{
         for (int i = 0; i < inputString.length(); i++) {
             list.add(inputString.charAt(i));
         }
-        InputValidity.validForList(list);
+        inputValidity.validForList(list);
         return list;
     }
 }
