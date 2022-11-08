93aa41a2bf0a65da1156e2486285084c9ed4fa9c
Laeng <Laeng@users.noreply.github.com>
2022-11-08 11:36:21 +0900

    test(Number): Number DTO 테스트 추가

---
+++ src/test/java/baseball/dto/NumberTest.java
@@ -0,0 +1,26 @@
+package baseball.dto;
+
+import baseball.dao.ComputerNumber;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DisplayName("Number 테스트")
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+public class NumberTest {
+    @Test
+    void 데이터_삽입_삭제_조회() {
+        Number dto = Number.getInstance();
+
+        assertThat(dto.insert(new ComputerNumber(123))).isTrue();
+
+        ComputerNumber dao = dto.latest();
+
+        assertThat(dao.getNumber()).isEqualTo(123);
+        assertThat(dto.getAll().size()).isEqualTo(1);
+        assertThat(dto.remove(dao)).isTrue();
+    }
+}
