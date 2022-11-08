9bd5ef04ee4911463d13db2027b7a46179f84b4a
kki4504 <rhkddlf9876@gmail.com>
2022-11-08 11:04:26 +0900

    fix : getNumberValidate 중복 숫자 체크 기능 추가

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,23 +1,39 @@
 package baseball;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.regex.Pattern;
+
 
 public class Validation {
     boolean getNumberValidate(String numberString) {
         if (getNumberOnlyNumber(numberString) && getNumberLengthThree(numberString)) {
-            return true;
+            if (getNumberDuplicateCheck(numberString)) {
+                return true;
+            }
         }
         throw new IllegalArgumentException();
     }
 
-    private boolean getNumberOnlyNumber(String numberString) {
+    private boolean getNumberOnlyNumber(String readLineNumber) {
         final String REGEX = "^[1-9]*$";
-        return Pattern.matches(REGEX, numberString);
+        return Pattern.matches(REGEX, readLineNumber);
     }
 
-    private boolean getNumberLengthThree(String numberString) {
-        if (numberString.length() == 3) {
+    private boolean getNumberLengthThree(String readLineNumber) {
+        if (readLineNumber.length() == 3) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean getNumberDuplicateCheck(String readLineNumber) {
+        String[] numbers = readLineNumber.split("");
+        Set<Integer> duplicateCheckSet = new HashSet<Integer>();
+        for (String number : numbers) {
+            duplicateCheckSet.add(Integer.parseInt(number));
+        }
+        if (duplicateCheckSet.size() == 3) {
             return true;
         }
         return false;
