d16221ba2d5a51e491c44a4b0dd71e05cd5069ef
starwook <star.wookk@gmail.com>
2022-11-08 18:49:11 +0900

    test(UserTest.java): Add UserTest.java and refactor ApplicationTest.java
    UserTest.java를 만들어라
    
    void 유저_입력_숫자_예외_테스트()
    void
    String_to_IntegerList_테스트()
    void
    String_to_IntegerList_예외처리포함_테스트()
    ApplicationTest.java에 있던 위
    파일들을
    UserTest.java로 옮기어라
    
    UserTest.java에
    Application.method()를
    User user
    =new User() 후 user.method()로 바꾸어라
    
    BREAKING CHANGE: Before:
    
    ApplicationTest.java
    {
    void
    유저_입력_숫자_예외_테스트()
    void String_to_IntegerList_테스트()
    void
    String_to_IntegerList_예외처리포함_테스트()
    }
    
    After:
    
    UserTest.java
    {
    void
    유저_입력_숫자_예외_테스트()
    void String_to_IntegerList_테스트()
    void
    String_to_IntegerList_예외처리포함_테스트()
    }
    
    Before:
    
    Application.method()
    
    After:
    User
    user = new User()
    user.method()

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -47,30 +47,6 @@ class ApplicationTest extends NsTest {
         assertTrue(Application.checkTwoNumbersAreSame(1,1));
         assertFalse(Application.checkTwoNumbersAreSame(1,3));
     }
-    @Test
-    void 유저_입력_숫자_예외_테스트(){
-        List<String> testStringList = Arrays.asList("044","abc","1234");
-        for(int i=0;i<testStringList.size();i++){
-            int finalI = i;
-            assertSimpleTest(()->
-                    assertThatThrownBy(()-> Application.checkUserInput(testStringList.get(finalI)))
-                            .isInstanceOf(IllegalArgumentException.class)
-            );
-        }
-    }
-
-    @Test
-    void String_to_IntegerList_테스트(){
-        List<Integer> integerList = Arrays.asList(1,2,3);
-        Assertions.assertThat(Application.StringToIntegerList("123")).isEqualTo(integerList);
-    }
-
-    @Test
-    void String_to_IntegerList_예외처리포함_테스트(){
-        List<Integer> integerList = Arrays.asList(1,2,3);
-        Assertions.assertThat(Application.getUserIntegerList("123")).isEqualTo(integerList);
-
-    }
 
 
     @Override
