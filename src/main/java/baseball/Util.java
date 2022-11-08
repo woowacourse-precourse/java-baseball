15ef683419cb51642c194f424e8ccc380a064fcf
DDing77 <devseo2021@gmail.com>
2022-11-08 17:02:03 +0900

    feat($Util) : 정답랜덤숫자 생성

---
+++ src/main/java/baseball/Util.java
@@ -0,0 +1,30 @@
+package baseball;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+
+public class Util {
+
+    public String createAnswerString() {
+        Set<Integer> set = new HashSet<>();
+
+        while(set.size() < 3) {
+            set.add(Randoms.pickNumberInRange(1, 9));
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        Iterator<Integer> iter = set.iterator();
+
+        while(iter.hasNext()) {
+            sb.append(iter.next());
+        }
+
+        return sb.toString();
+    }
+}
