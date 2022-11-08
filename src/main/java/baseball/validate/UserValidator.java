530b0dbee5415933db2b552d14709d480276dc57
ys200209 <lys200209@naver.com>
2022-11-08 17:53:58 +0900

    feat(UserValidator.java): 입력한 수를 검증하기
    유저가 숫자야구 게임에서 입력한 수를 검증함
    - 공백이 들어왔는지 검증
    - 숫자가 아닌 문자가 입력되었는지 검증
    - 입력의 길이가 3보다 작은지 검증
    - 입력의 길이가 3보다 큰지 검증
    - 서로 다른 숫자가 아닌, 같은 숫자가 중복되었는지 검증

---
+++ src/main/java/baseball/validate/UserValidator.java
@@ -0,0 +1,57 @@
+package baseball.validate;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class UserValidator {
+
+    /**
+     * 야구게임 숫자를 맞출 때 입력한 값을 검증합니다.
+     */
+    public void validateNumber(String number) {
+        if (Pattern.matches(".*\\s.*", number)) {
+            throw new IllegalArgumentException("공백은 입력할 수 없습니다.");
+        }
+
+        if (Pattern.matches(".*\\D.*", number)) {
+            throw new IllegalArgumentException("숫자만 입력할 수 있습니다.");
+        }
+
+        if (number.length() < 3) {
+            throw new IllegalArgumentException("입력의 길이가 3보다 작습니다.");
+        }
+
+        if (number.length() > 3) {
+            throw new IllegalArgumentException("입력의 길이가 3보다 큽니다.");
+        }
+
+        if (isDuplicateNumber(number)) {
+            throw new IllegalArgumentException("서로 다른 숫자를 입력해야 합니다.");
+        }
+    }
+
+    /**
+     * 입력한 숫자에 중복이 있으면 true를 리턴하고, 그렇지 않으면 false를 리턴합니다.
+     */
+    private boolean isDuplicateNumber(String number) {
+        if (getDistinctCount(number) == 3) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * 중복을 제거한 문자의 개수를 리턴합니다.
+     */
+    private int getDistinctCount(String number) {
+        List<String> numberList = new ArrayList<>(Arrays.asList(number.split("")));
+
+        return (int)numberList.stream()
+                .distinct()
+                .count();
+    }
+
+}
