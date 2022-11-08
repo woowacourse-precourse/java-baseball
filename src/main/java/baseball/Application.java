1f30258b2dafef65d995fea38ee9f013e9278c15
Brilly-Bohyun <bohyun564@swu.ac.kr>
2022-11-08 13:13:02 +0900

    Feat: 게임 재시작 여부를 판별한 기능 구현
    boolean형을 이용하여 재시작과 종료를 구분함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -65,7 +65,19 @@ public class Application {
             return ball + "볼";
         } else if (ball == 0) {
             return strike + "스트라이크";
+        } else if (strike == 3) {
+            return "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
         }
         return ball + "볼 " + strike + "스트라이크";
+    }
+
+    public static boolean playAgainOrNot(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        char decision = readLine().charAt(0);
+
+        if(decision == '1'){
+            return true;
+        }
+        return false;
     }
 }
