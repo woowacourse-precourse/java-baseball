ddc35faf10ea600281a609d565423d875dc3f527
wogus4048 <wogus4048@naver.com>
2022-11-08 22:47:19 +0900

    feat : 랜덤 숫자 만들기 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import java.util.List;
+import camp.nextstep.edu.missionutils.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static void getComputersRandomNumber(List<Integer> computersNumber) {
+        while (computersNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computersNumber.contains(randomNumber)) {
+                computersNumber.add(randomNumber);
+            }
+        }
     }
 }
