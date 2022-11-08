bf1360bcdf1e35849973f74f5903fba9ed842d30
choiejae <ejae8319@gmail.com>
2022-11-08 11:16:20 +0900

    style: Google java convention

--- src/main/java/baseball/ValidityChecker.java
+++ src/main/java/baseball/ValidityChecker.java
@@ -5,27 +5,32 @@ import java.util.Objects;
 import java.util.Set;
 
 public class ValidityChecker {
+
     private final int MAX_LEN;
+
     ValidityChecker(int maxLen) {
         this.MAX_LEN = maxLen;
-
     }
+
     public void validateNumberInput(String input) {
         validLength(input);
         validElement(input);
         validContinuity(input);
     }
+
     public void validLength(String input) {
         if (input.length() != MAX_LEN) {
             throw new IllegalArgumentException("input의 길이가 유효하지 않습니다");
         }
     }
+
     public void validElement(String input) {
         final String REGEX = "[1-9]+";
         if (!input.matches(REGEX)) {
             throw new IllegalArgumentException("input의 구성요소가 0을 제외한 숫자로만 이루어지지 않습니다");
         }
     }
+
     public void validContinuity(String input) {
         Set<Character> numSet = new HashSet<>();
         for (int i = 0; i < input.length(); i++) {
