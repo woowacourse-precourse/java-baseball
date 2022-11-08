2b2c4e85cb5fd6802f4cc4b747d23e6843920f3a
K-Diger <kitten.diger@gmail.com>
2022-11-08 16:12:42 +0900

    refactor : 기존 클라이언트 코드에 있던 기능 BaseballGameStarter 클래스에 위임 - 게임 종료 후 계속할 것인지, 그만둘 것인지 입력받는 기능을 operateRefereeJudgement() 메서드에 추가하여 기능 통합

--- src/main/java/baseball/starter/BaseballGameStarter.java
+++ src/main/java/baseball/starter/BaseballGameStarter.java
@@ -3,19 +3,18 @@ package baseball.starter;
 import baseball.computer.Computer;
 import baseball.referee.Referee;
 import baseball.user.User;
+import baseball.validator.UserInputValueValidator;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class BaseballGameStarter {
-
-    public List<List<String>> operate() {
+    public List<List<String>> initializeNewGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.print("숫자를 입력해주세요 : ");
 
-        List<String> userInputValue = new User().stringConvertToStringList(
-                Console.readLine());
+        List<String> userInputValue = new User().stringConvertToStringList(Console.readLine());
         List<String> computerGeneratedValue = new Computer().generateNumber();
         return new ArrayList<>() {{
             add(userInputValue);
@@ -23,15 +22,30 @@ public class BaseballGameStarter {
         }};
     }
 
-    public boolean operateRefereeJudgement(List<String> userInputValue, List<String> computerGeneratedValue) {
+    public void operateRefereeJudgement(List<String> userInputValue, List<String> computerGeneratedValue) {
         Referee referee = new Referee();
+        String option = "";
 
         while (referee.judgement(computerGeneratedValue, userInputValue).get("strikeCount") != 3) {
             referee.sayJudgement(referee.judgement(computerGeneratedValue, userInputValue));
             System.out.print("숫자를 입력해주세요 : ");
             userInputValue = new User().stringConvertToStringList(
                     Console.readLine());
+
+            if (referee.judgement(computerGeneratedValue, userInputValue).get("strikeCount") == 3) {
+                referee.sayGameEnded();
+                option = Console.readLine();
+                UserInputValueValidator.getInstance().validate(List.of(option));
+            }
         }
-        return true;
+
+        if (Integer.parseInt(option) == 1) {
+            List<List<String>> initializedGame = initializeNewGame();
+            userInputValue = initializedGame.get(0);
+            computerGeneratedValue = initializedGame.get(1);
+            operateRefereeJudgement(userInputValue, computerGeneratedValue);
+        } else if (Integer.parseInt(option) != 2) {
+            throw new IllegalArgumentException();
+        }
     }
 }
