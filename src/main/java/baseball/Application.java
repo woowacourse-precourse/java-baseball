a8a5e0f3d1f3e5a4b5400afa9a10171c0b054e2e
권찬영 <kwonchan-yeong@gwonchan-yeong-ui-MacBookPro.local>
2022-11-08 20:59:04 +0900

    fix: Change print statements in playGame Method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -136,13 +136,13 @@ public class Application {
     }
 
     public static void playGame() {
-        int endValue = 0;
+        boolean endValue = true;
 
         System.out.println("숫자 야구 게임을 시작합니다.");
         String randomNum = makeRandomNumber();
 
-        while (endValue == 0) {
-            System.out.println("숫자를 입력해주세요 : ");
+        while (endValue) {
+            System.out.print("숫자를 입력해주세요 : ");
 
             String inputNum = readLine();
             checkInput(inputNum);
@@ -153,6 +153,9 @@ public class Application {
 
             endValue = printBallStrike(printList);
         }
+
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
     public static boolean playRestart() {
