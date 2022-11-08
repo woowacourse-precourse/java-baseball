ce44383ec597a06d4a8d54638bf5bef4a32c0ac9
hgo641 <hgo641@gmail.com>
2022-11-08 20:07:58 +0900

    test: Command 테스트 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 
 import controller.GameController;
+import domain.RoundResult;
 
 public class Application {
     public static void main(String[] args) {
--- src/test/java/baseball/domain/CommandTest.java
+++ src/test/java/baseball/domain/CommandTest.java
@@ -9,7 +9,7 @@ public class CommandTest {
     @Test
     void FailTovalidateInputIf(){
         String invalidInput = "3";
-        assertThatThrownBy(() -> new Command(invalidInput))
+        assertThatThrownBy(() -> Command.findByValue(invalidInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("1또는 2를 입력해야합니다.");
     }
@@ -17,12 +17,12 @@ public class CommandTest {
     @Test
     void IsNotRestart(){
         String exitInput = "2";
-        assertThat(new Command(exitInput).isRestart()).isEqualTo(false);
+        assertThat(Command.findByValue(exitInput).equals(Command.RESTART)).isEqualTo(false);
     }
 
     @Test
     void IsRestart(){
         String exitInput = "1";
-        assertThat(new Command(exitInput).isRestart()).isEqualTo(true);
+        assertThat(Command.findByValue(exitInput).equals(Command.RESTART)).isEqualTo(true);
     }
 }
