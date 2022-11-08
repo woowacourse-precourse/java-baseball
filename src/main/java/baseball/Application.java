1b980fb526f02cf58e79c6b5e2b24d05e2aef716
mng990 <ryoo0504@pusan.ac.kr>
2022-11-08 15:39:20 +0900

    feat(baseball.Application): game.play 호출 추가
    main:
    + game.play()
    
    [skip ci]

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,8 +3,8 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        Game game = new Game(0);
+        Game game = new Game();
+        game.play();
     }
-
 }
 
