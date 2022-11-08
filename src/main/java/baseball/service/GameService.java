c63fcf7aec2d31339c67311b0e45cbc58c1b6a25
ygreenb <nhj7911@gmail.com>
2022-11-08 19:43:52 +0900

    feat: Service 로직 구현

---
+++ src/main/java/baseball/service/GameService.java
@@ -0,0 +1,33 @@
+package baseball.service;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.Constant.NUMBER_LIMIT;
+
+public class GameService {
+
+    public static List<Integer> makeComputerNumber() {
+        List<Integer> computer = new ArrayList<>();
+
+        return computer;
+    }
+
+    public List<Integer> makeUserNumber(String input) {
+        List<Integer> list = new ArrayList<>();
+
+        return list;
+    }
+
+    public boolean compareAnswer(List<Integer> computer, List<Integer> user) {
+
+        return false;
+    }
+
+    public void printHint(int strikeCount, int ballCount) {
+
+    }
+
+}
