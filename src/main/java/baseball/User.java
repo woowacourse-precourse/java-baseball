47957bfb1662e4ccb97a1759df4c582bbb1c966b
Giho <poqw741@gmail.com>
2022-11-08 18:04:52 +0900

    feat : 유저 입력 받는 메서드 생성

---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,22 @@
+package baseball;
+
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.*;
+
+
+public class User {
+    public static List<Integer> inputGameNumber(){
+        List<Integer> userNumbers = new ArrayList<>();
+
+        String StringUserInput = Console.readLine();
+
+        String[] arrayInput = StringUserInput.split("");
+
+        for(int i=0; i<StringUserInput.length();i++){
+            userNumbers.add(Integer.parseInt(arrayInput[i]));
+        }
+        return userNumbers;
+    }
+}
+
