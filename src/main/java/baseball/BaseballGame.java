35c28c35ac454990041a75bc97a0736e5b6d3c0e
silver <silver36c@gmail.com>
2022-11-08 23:05:12 +0900

    feat: 숫자 야구 게임 진행

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -9,7 +9,14 @@ public class BaseballGame {
         this.player = player;
     }
 
-    public void start() {
+    public void preceed() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        do {
+            play();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        } while (player.continueGame());
+        finishEntireGame();
+    }
 
     }
 
