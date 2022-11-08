324112df527a61f08f9a438158eeb0374e2c8328
minji0426 <minjee0426@naver.com>
2022-11-08 23:21:32 +0900

    feat: 사용자에게 숫자 입력받는 기능

---
+++ src/main/java/baseball/domain/PlayerNum.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class PlayerNum {
+    private List<String> number;
+
+    public List<String> getNumber() {
+        return number;
+    }
+
+    public void setNumber(List<String> number) {
+        this.number = number;
+    }
+}
