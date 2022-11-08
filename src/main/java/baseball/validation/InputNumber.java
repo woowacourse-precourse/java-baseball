fdb5246dbf5ea81b9e0d4954b09e908c1632446a
kki4504 <rhkddlf9876@gmail.com>
2022-11-08 11:38:13 +0900

    refactor : rename GetNumber class -> InputNumber

--- src/main/java/baseball/validation/InputNumber.java
+++ src/main/java/baseball/validation/InputNumber.java
@@ -5,29 +5,29 @@ import java.util.Set;
 import java.util.regex.Pattern;
 
 
-public class GetNumber {
-    public boolean getNumberValidate(String numberString) {
-        if (getNumberOnlyNumber(numberString) && getNumberLengthThree(numberString)) {
-            if (getNumberDuplicateCheck(numberString)) {
+public class InputNumber {
+    public boolean inputNumberValidate(String numberString) {
+        if (inputNumberOnlyNumber(numberString) && inputNumberLengthThree(numberString)) {
+            if (inputNumberDuplicateCheck(numberString)) {
                 return true;
             }
         }
         throw new IllegalArgumentException();
     }
 
-    private boolean getNumberOnlyNumber(String readLineNumber) {
+    private boolean inputNumberOnlyNumber(String readLineNumber) {
         final String REGEX = "^[1-9]*$";
         return Pattern.matches(REGEX, readLineNumber);
     }
 
-    private boolean getNumberLengthThree(String readLineNumber) {
+    private boolean inputNumberLengthThree(String readLineNumber) {
         if (readLineNumber.length() == 3) {
             return true;
         }
         return false;
     }
 
-    private boolean getNumberDuplicateCheck(String readLineNumber) {
+    private boolean inputNumberDuplicateCheck(String readLineNumber) {
         String[] numbers = readLineNumber.split("");
         Set<Integer> duplicateCheckSet = new HashSet<Integer>();
         for (String number : numbers) {
