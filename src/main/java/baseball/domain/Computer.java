187eb0457fdd3af8b2b10a5d821231a96aeefcf3
Jung <anes53027@gmail.com>
2022-11-08 22:20:11 +0900

    feat(Computer) : Computer 랜덤 숫자 생성 함수 생성

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -12,5 +12,14 @@ public class Computer {
         computer = new ArrayList<>();
     }
 
+    public void createThreeNumbers(){
+        computer = new ArrayList<>();
 
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
 }
