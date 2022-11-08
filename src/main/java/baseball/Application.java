b20565d2bcf588a180d166fe81cd7c076bddab9a
CJY <48767111+CJY-kor@users.noreply.github.com>
2022-11-08 22:41:19 +0900

    Update Application.java
    fix : 홈페이지에서 실행이 안되는 문제를 해결하던 중 이전 커밋들이 날라가 다시 재 커밋
    
    오류를 해결하기 위해 코드만 붙여놓고 다시 재 커밋하였다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,53 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application agent = new Application();
+        while(agent.StartBaseballGame()){
+        }
     }
-}
+
+    public boolean isEnd(String number){
+
+        int tempNum = Integer.parseInt(number);
+
+        if (tempNum == 1){
+            return true;
+        } else if (tempNum == 2) {
+            return false;
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    public boolean CheckInput(String number){
+
+        for (int i = 0; i < number.length(); i++){  // 서로 다른 숫자인지 확인하는 부분
+            if (number.indexOf(number.charAt(i)) != i) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        int tempNum = Integer.parseInt(number);
+
+        if (100 <= tempNum && tempNum < 1000){
+            return true;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public ArrayList<Integer> GetHint(String number, String input){
+
+        ArrayList<String> tempNumber = new ArrayList<>();
+        ArrayList<String> tempInput = new ArrayList<>();
+        ArrayList<Integer> tempReturn = new ArrayList<>(Arrays.asList(0, 0));
+
+        for (int i = 0; i < number.length(); i++){
+            tempNumber.add(number.substring(i, i+1));
+            tempInput.add(input.substring(i, i+1));
+        }
