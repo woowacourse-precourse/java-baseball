716d1dd5bf85b1ba1c533f42ecedcb8d5c3e5458
SangMinLeeAI <sangmin.lee.ai@gmail.com>
2022-11-08 20:23:48 +0900

    fix: Input class 수정
    문법 오류를 수정, 입력과 예외처리를 한 메소드로 합쳐서 상위 메소드에서 예외를 받게 하였다.

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -2,54 +2,40 @@ package baseball;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Input {
 
-    private final List<Integer> inputs;
+    private List<Integer> inputs;
 
-    public Input(List<Integer> inputs) {
-        this.inputs = inputs;
+    public Input() {
+        this.inputs = new ArrayList<>();
     }
 
-    public void makeInputs() {
-        try {
+    public void makeInputs() throws IllegalArgumentException {
+
             String input = readLine();
             for (int i = 0; i < input.length(); i++) {
-                this.inputs.add(Integer.parseInt(input.substring(i, i + 1)));
+                this.inputs.add(Integer.parseInt(input.substring(i,i+1)));
             }
-            if (!isCorrectInput(this.inputs)) {
-                throw new IllegalArgumentException();
-            }
-        } catch (Exception e) {
-            System.out.println("올바른 값을 입력해주세요.");
-        }
-    }
-
-    public boolean isCorrectInput(List<Integer> inputs) {
-        try {
             if (inputs.size() != 3) {
-                throw new IllegalArgumentException();
+                throw new IllegalArgumentException("3자리의 숫자를 입력해주세요.");
             }
+            if (inputs.get(0).equals(inputs.get(1)) || inputs.get(0).equals(inputs.get(2)) || inputs.get(1).equals(inputs.get(2))) {
+                throw new IllegalArgumentException("중복된 숫자를 입력할 수 없습니다.");
+            }
+        }
 
-        } catch (IllegalArgumentException e) {
-            System.out.println("3자리의 숫자를 입력해주세요.");
-            return false;
-        }
-        try {
-            if (inputs.get(0).equals(inputs.get(1)) || inputs.get(0).equals(inputs.get(2)) || inputs.get(1)
-                    .equals(inputs.get(2))) {
-                throw new IllegalArgumentException();
-            }
-            return true;
-        } catch (IllegalArgumentException e) {
-            System.out.println("서로 다른 숫자를 입력해주세요.");
-            return false;
-        }
-    }
+
 
     public List<Integer> getInputs() {
 
         return inputs;
     }
+
+    public void resetInputs() {
+        this.inputs.clear();
+    }
+
 }
