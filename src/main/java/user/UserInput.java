db1e948cc3a669ae68154ac25b19520b606af113
kite707 <kite707707@gmail.com>
2022-11-08 09:52:59 +0900

    refactor(user): 유저 입력 List로 바꾸는 메서드 분리

--- src/main/java/user/UserInput.java
+++ src/main/java/user/UserInput.java
@@ -7,18 +7,26 @@ import java.util.List;
 public class UserInput {
     public static final int USER_INPUT_LENGTH = 3;
 
-    public static List<Integer> getUser(){
-        List<Integer> userInputList = new ArrayList<>();
-        String userInput = Console.readLine();
 
-        if(userInput.length()!=USER_INPUT_LENGTH){
-            System.out.println("3개의 숫자를 입력해주세요!");
-            throw new IllegalArgumentException();
-        }else{
-            for(int i=0; i<USER_INPUT_LENGTH;i++){
-                int number = userInput.charAt(i) - '0';
-                userInputList.add(number);
+    public static List<Integer> getUserInput(){
+        List<Integer> userInputList = new ArrayList<>();
+        try {
+            String userInput = Console.readLine();
+
+            if(userInput.length()!=USER_INPUT_LENGTH){
+                System.out.println("3개의 숫자를 입력해주세요!");
+                throw new IllegalArgumentException();
             }
+            userInputList = changeUserInputToList(userInput);
+        }catch (Exception e){};
+        return userInputList;
+    }
+
+    public static List<Integer> changeUserInputToList(String userInput){
+        List<Integer> userInputList = new ArrayList<>();
+        for(int i=0; i<USER_INPUT_LENGTH;i++){
+            int number = userInput.charAt(i) - '0';
+            userInputList.add(number);
         }
         return userInputList;
     }
