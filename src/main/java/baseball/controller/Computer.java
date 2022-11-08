8b87dd4eda082599a28ca0fd67ad9068795543ee
saehim <saeilee.vxv@gmail.com>
2022-11-08 21:31:00 +0900

    Feat: 컴퓨터 숫자 랜덤 생성 기능

--- src/main/java/baseball/controller/Computer.java
+++ src/main/java/baseball/controller/Computer.java
@@ -1,2 +1,20 @@
-package baseball.controller;public class Computer {
+package baseball.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+public class Computer {
+    /** 중복되지 않은 1~9까지 숫자 3개 List 반환 */
+    public static List<Integer> createRandomNums(){
+        List<Integer> correctNums = new ArrayList<>();
+        while (correctNums.size()<3) {
+            int temp = pickNumberInRange(1,9);
+            if (!correctNums.contains(temp)) {
+                correctNums.add(temp);
+            }
+        }
+        return correctNums;
+    }
 }
