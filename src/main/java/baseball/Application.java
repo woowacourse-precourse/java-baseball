364144a5b81c2bfe56888888f8e504381215c8d6
jihun-24k <gi020477@gmail.com>
2022-11-08 15:47:25 +0900

    Refactor: isOverlap 간소화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,5 @@
 package baseball;
 
-import util.Util;
-import camp.nextstep.edu.missionutils.Console;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.regex.Pattern;
-
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/baseball/InputException.java
+++ src/main/java/baseball/InputException.java
@@ -1,10 +1,12 @@
 package baseball;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Pattern;
 
 public class InputException {
+
     public static void validInput(String input) {
         if (input.isEmpty()) {
             throw new IllegalArgumentException("input cannot be empty.");
@@ -19,11 +21,9 @@ public class InputException {
             throw new IllegalArgumentException("input cannot be overlap.");
         }
     }
+
     private static boolean isOverlap(String input) {
-        Set<String> inputSet = new HashSet<>();
-        for (String number : input.split("")) {
-            inputSet.add(number);
-        }
+        Set<String> inputSet = new HashSet<>(Arrays.asList(input.split("")));
         return input.length() != inputSet.size();
     }
 
