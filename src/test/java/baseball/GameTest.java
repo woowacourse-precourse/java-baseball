cf290b946c4183ddf5b3d9e97f73aa377ebe25ad
Choi Yoon Chan <cyc0227@gachon.ac.kr>
2022-11-08 20:12:58 +0900

    Test(Hint): 추가 유저 인풋별 테스트
    스트라이크, 볼, 스트라이크 & 볼, 낫싱 이렇게 4가지 경우에 대한 테스트 작성

---
+++ src/test/java/baseball/GameTest.java
@@ -0,0 +1,18 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void 예외_테스트() {
+        //given
+
+        //when
+        //then
+    }
+}
