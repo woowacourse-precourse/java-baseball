5856bc3258bd0ec47fc92cb050d600ad03515c16
Jung <anes53027@gmail.com>
2022-11-08 22:34:23 +0900

    feat(Referee) : countStrike 스트라이크 개수 구하는 함수 생성

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -1,5 +1,8 @@
 package baseball.domain;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Referee {
 
     private int ball;
@@ -9,5 +12,16 @@ public class Referee {
     public void initBallAndStrike(){
         ball = RESET_TO_ZERO;
         strike = RESET_TO_ZERO;
+    }
+
+    public Integer countStrike(List<Integer> computerList, List<Integer> myList){
+
+        for (int i = 0; i < 3; i++) {
+            if(Objects.equals(computerList.get(i), myList.get(i))){
+                strike++;
+            }
+        }
+
+        return strike;
     }
 }
