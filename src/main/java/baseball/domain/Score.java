b125852e99497b7c5b5f15458ad76539d8493468
조병우 <whquddn55@gmail.com>
2022-11-08 09:40:22 +0900

    feat(Score): implement Score class
    This class represents the score of player.
    
    + public Score(int, int)
    + getter
    + toString

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -1,7 +1,38 @@
 package baseball.domain;
 
-/**
- * TODO: implement the class
- */
 public class Score {
+    private int strike;
+    private int ball;
+
+    public Score(int strikeScore, int ballScore) {
+        this.strike = strikeScore;
+        this.ball = ballScore;
+    }
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder ret = new StringBuilder();
+        if (strike == 0 && ball == 0) {
+            ret.append("낫싱");
+        }
+        else {
+            if (ball != 0) {
+                ret.append(ball);
+                ret.append("볼 ");
+            }
+            if (strike != 0) {
+                ret.append(strike);
+                ret.append("스트라이크");
+            }
+        }
+        return ret.toString();
+    }
 }
