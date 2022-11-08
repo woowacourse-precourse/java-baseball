3e578a1219b74d58ed7a351a4e561a953071cfbb
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-11-08 20:32:22 +0900

    style: 테스트 명칭 수정

--- src/test/java/baseball/VerifyUserNumberTest.java
+++ src/test/java/baseball/VerifyUserNumberTest.java
@@ -7,19 +7,19 @@ import static org.assertj.core.api.Assertions.*;
 public class VerifyUserNumberTest {
     private final Application application = new Application();
 
-    @Test void verifyUserNumber_사용자가_입력한_숫자가_3자리가_아닐_경우() {
+    @Test void 사용자가_입력한_숫자가_3자리가_아닐_경우() {
         String input = "12345";
         assertThatThrownBy(() -> Application.verifyUserNumber(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test void verifyUserNumber_사용자가_입력한_숫자에_0이_포함된_경우() {
+    @Test void 사용자가_입력한_숫자에_0이_포함된_경우() {
         String input = "120";
         assertThatThrownBy(() -> Application.verifyUserNumber(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test void verifyUserNumber_사용자가_올바른_숫자를_입력한_경우() {
+    @Test void 사용자가_올바른_숫자를_입력한_경우() {
         String input = "123";
         assertThat(Application.verifyUserNumber(input)).isEqualTo("123");
     }
