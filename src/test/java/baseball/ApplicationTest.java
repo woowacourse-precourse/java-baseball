4f8cdbf042e55acdc732118d0acd04e3aa90d80f
Myungyulee <102199101+Myungyulee@users.noreply.github.com>
2022-11-08 12:21:43 +0900

    test: 빌드실패 확인을 위한 커밋 Nest제거

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,7 +1,6 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -13,120 +12,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
-    @Nested
-    class 메시지_만들기 {
-        @Test
-        void test1(){
-            int ballCount = 0;
-            int strike = 0;
-            String message = Application.makeCountMessage(ballCount, strike);
-            String result = "낫싱";
-            assertThat(message).isEqualTo(result);
-        }
-        @Test
-        void test2(){
-            int ballCount = 1;
-            int strike = 0;
-            String message = Application.makeCountMessage(ballCount, strike);
-            String result = "1볼";
-            assertThat(message).isEqualTo(result);
-        }
-        @Test
-        void test3(){
-            int ballCount = 0;
-            int strike = 1;
-            String message = Application.makeCountMessage(ballCount, strike);
-            String result = "1스트라이크";
-            assertThat(message).isEqualTo(result);
-        }
-        @Test
-        void test4(){
-            int ballCount = 2;
-            int strike = 1;
-            String message = Application.makeCountMessage(ballCount, strike);
-            String result = "2볼 1스트라이크";
-            assertThat(message).isEqualTo(result);
-        }
-    }
-
-    @Nested
-    class 숫자비교_3스트라이크인지_확인 {
-        @Test
-        void test1(){
-            boolean[] isBall = new boolean[10];
-            Arrays.fill(isBall, false);
-            String computer="123";
-            Application.setIsBall(isBall, computer);
-            String player="123";
-            boolean result = true;
-
-            boolean output = Application.compareNumbers(isBall, computer, player);
-            assertThat(output).isEqualTo(result);
-        }
-        @Test
-        void test2(){
-            boolean[] isBall = new boolean[10];
-            Arrays.fill(isBall, false);
-            String computer="723";
-            Application.setIsBall(isBall, computer);
-            String player="123";
-            boolean result = false;
-
-            boolean output = Application.compareNumbers(isBall, computer, player);
-            assertThat(output).isEqualTo(result);
-        }
-        @Test
-        void test3(){
-            boolean[] isBall = new boolean[10];
-            Arrays.fill(isBall, false);
-            String computer="678";
-            Application.setIsBall(isBall, computer);
-            String player="123";
-            boolean result = false;
-
-            boolean output = Application.compareNumbers(isBall, computer, player);
-            assertThat(output).isEqualTo(result);
-        }
-    }
-
-    @Nested
-    class 플레이어_숫자_검증{
-        @Test
-        void test1() {
-            String number = "123";
-            boolean check = Application.checkPlayerNumber(number);
-            boolean result = true;
-            assertThat(check).isEqualTo(result);
-        }
-        @Test
-        void test2() {
-            String number = "102";
-            boolean check = Application.checkPlayerNumber(number);
-            boolean result = false;
-            assertThat(check).isEqualTo(result);
-        }
-        @Test
-        void test3() {
-            String number = "112";
-            boolean check = Application.checkPlayerNumber(number);
-            boolean result = false;
-            assertThat(check).isEqualTo(result);
-        }
-        @Test
-        void test4() {
-            String number = "3412";
-            boolean check = Application.checkPlayerNumber(number);
-            boolean result = false;
-            assertThat(check).isEqualTo(result);
-        }
-        @Test
-        void test5() {
-            String number = "";
-            boolean check = Application.checkPlayerNumber(number);
-            boolean result = false;
-            assertThat(check).isEqualTo(result);
-        }
-    }
 
     @Test
     void 숫자리스트_문자열로_변환메소드() {
