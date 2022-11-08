7d145a373b33f3aae979044788ee43dafbda54ab
dydckd <toofriend09@naver.com>
2022-11-08 22:01:30 +0900

    사용되는 메세지 필드

---
+++ src/main/java/baseball/Message.java
@@ -0,0 +1,12 @@
+package baseball;
+
+public class Message {
+    final String startSentence = "숫자 야구 게임을 시작합니다.";
+    final String whileGameSentence="숫자를 입력해주세요 : ";
+    final String endGameSentence = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    final String strikeSentence = "스트라이크 ";
+    final String ballSentence = "볼 ";
+    final String nothing = "낫싱";
+    final String restart = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+}
