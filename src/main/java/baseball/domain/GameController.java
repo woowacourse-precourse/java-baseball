68ae6b8066412e3c8eaf7145bb1e248edd1c0514
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-08 14:56:08 +0900

    chore(GameController): Add gameover notice

--- src/main/java/baseball/domain/GameController.java
+++ src/main/java/baseball/domain/GameController.java
@@ -39,8 +39,10 @@ public class GameController {
         if ( ball == 0 && strike == 0 ) System.out.println("낫싱");
         if ( ball != 0 ) System.out.print(ball + "볼 ");
         if ( strike != 0 ) System.out.println(strike + "스트라이크");
-        if ( strike == 3) return true;
+        if ( strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
         return false;
     }
-
 }
