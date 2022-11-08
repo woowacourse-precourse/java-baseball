8bff77d3749274ca47ca3ec80d2926b8b8d45a9e
ys200209 <lys200209@naver.com>
2022-11-08 16:58:11 +0900

    feat(Game.java): 숫자를 입력하도록 명령하기
    유저에게 숫자를 입력하라고 명령함

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -5,14 +5,19 @@ package baseball.domain;
  */
 public class Game {
     private static Computer computer;
+    private static User user;
 
-    public Game(Computer computer) {
+    public Game(Computer computer, User user) {
         this.computer = computer;
+        this.user = user;
     }
 
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         computer.createRandomNumber();
+
+        System.out.print("숫자를 입력해주세요 : ");
+        user.inputNumber();
     }
 
 }
