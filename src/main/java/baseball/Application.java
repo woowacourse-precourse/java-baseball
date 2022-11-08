90c538c3bc3f17a199fefcb6a5cc639b7851a416
Gentlee <ekdma3259@gmail.com>
2022-11-08 18:58:57 +0900

    비교하여 나온 볼과 스트라이크를 대입하여 결과 문구를 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -77,6 +77,23 @@ public class Application {
 		return ball;
 	}
 	
+	public static String gameResult(int strike, int ball) {
+		String result="";
+			if(strike == 3) {
+				result="3스트라이크\n";
+				result+="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+			}else if(strike == 0 && ball == 0) {
+				result="낫싱";
+			}else if(strike == 0 && ball != 0) {
+				result=ball+"볼";
+			}else if(ball == 0 && strike != 0) {
+				result=strike+"스트라이크";
+			}else if (ball != 0 && strike != 0){
+				result=ball+"볼 "+strike+"스트라이크";
+			}
+		return result;
+	}
+	
     public static void main(String[] args) {
     	
     }
