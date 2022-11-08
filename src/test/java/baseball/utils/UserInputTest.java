dc8ef0ac90e21f17517a3f52e9a8e19eaf4e1be8
MinSungKim <timkim01@naver.com>
2022-11-08 10:47:42 +0900

    test: 사용자 입력값에 중복이 있는지 판단, 매 테스트마다 객체 초기화 코드 작성

--- src/test/java/baseball/utils/UserInputTest.java
+++ src/test/java/baseball/utils/UserInputTest.java
@@ -1,5 +1,7 @@
 package baseball.utils;
 
+import baseball.model.Game;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -8,6 +10,12 @@ import java.io.InputStream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserInputTest {
+    private static UserInput userInput;
+    @BeforeAll
+    static void init(){
+        Game game = new Game();
+        userInput = new UserInput(game, 3);
+    }
 
     @Test
     void 사용자_입력값이_숫자인지_여부_판단(){
@@ -15,7 +23,7 @@ class UserInputTest {
         InputStream in = new ByteArrayInputStream(userInput.getBytes());
         System.setIn(in);
 
-        Throwable exception = assertThrows(RuntimeException.class, () -> {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
             UserInput.setUserInput();
         });
 
@@ -24,14 +32,27 @@ class UserInputTest {
 
     @Test
     void 사용자_입력값이_3자리인지_여부_판단(){
-        String userInput = "011";
+        String userInput = "012";
         InputStream in = new ByteArrayInputStream(userInput.getBytes());
         System.setIn(in);
 
-        Throwable exception = assertThrows(RuntimeException.class, () -> {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
             UserInput.setUserInput();
         });
 
         assertEquals("3자리 숫자를 입력해주세요.", exception.getMessage());
+    }
+
+    @Test
+    void 사용자_입력값에_중복이_있는지_여부_판단(){
+        String input = "122";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            UserInput.setUserInput();
+        });
+
+        assertEquals("중복되지 않는 3자리 숫자를 입력해주세요.", exception.getMessage());
     }
 }
