860ffcaae769b4ba0d7640a067df7628628adeee
sangminee <lsmlee99@gmail.com>
2022-11-08 20:11:49 +0900

    Refactor : 결과 판단 함수 return 방식으로 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -23,14 +23,15 @@ public class Application {
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         start(br);
-        while(!restart){
+        while (!restart) {
             restart(br);
         }
     }
+
     public static void start(BufferedReader br) throws IOException {
         System.out.println("숫자 야구 게임을 시작합니다.");
         selectNumber();
-        while(!result){
+        while (!result) {
             inputNumber(br);
             checkNumber(numbers);
         }
@@ -39,11 +40,11 @@ public class Application {
     public static void restart(BufferedReader br) throws IOException {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String[] temp = br.readLine().split("");
-        if(temp.length >= 2){
+        if (temp.length >= 2) {
             throw new IllegalArgumentException();
         }
         int choice = Integer.parseInt(temp[0]);
-        if(choice == 1){
+        if (choice == 1) {
             result = false;
             start(br);
         } else restart = true;
@@ -52,41 +53,48 @@ public class Application {
     public static void checkNumber(List<Integer> numbers) {
         strick = 0;
         ball = 0;
-        for(int i=0; i<3; i++){
-            for(int j=0; j<3; j++){
-                if(computer.get(i) == numbers.get(j)){
-                    if(i == j) strick++;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (computer.get(i) == numbers.get(j)) {
+                    if (i == j) strick++;
                     else ball++;
                 }
             }
         }
-        outputResult();
+       outputResult();
     }
 
-    public static void outputResult() {
-        if(strick == 0 && ball == 0){
+    public static String outputResult() {
+        if (strick == 0 && ball == 0) {
             System.out.println("낫싱");
-        }else if(strick == 3){
+            return "낫싱";
+        } else if (strick == 3) {
             System.out.println("3스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             result = true;
-        }else if(ball != 0 && strick != 0){
-            System.out.println(ball+"볼 "+strick+"스트라이크");
-        }else if(ball == 0 && strick != 0){
-            System.out.println(strick+"스트라이크");
-        }else if(ball != 0 && strick == 0){
-            System.out.println(ball+"볼");
+            return "3스트라이크 \n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+        } else if (ball != 0 && strick != 0) {
+            System.out.println(ball + "볼 " + strick + "스트라이크");
+            return ball + "볼 " + strick + "스트라이크";
+        } else if (ball == 0 && strick != 0) {
+            System.out.println(strick + "스트라이크");
+            return strick + "스트라이크";
+        } else if (ball != 0 && strick == 0) {
+            System.out.println(ball + "볼");
+            return ball + "볼";
         }
+        return null;
     }
+
 
     public static void inputNumber(BufferedReader br) throws IOException {
         System.out.print("숫자를 입력해 주세요 : ");
         String[] temp = br.readLine().split("");
-        if(temp.length >= 4){
+        if (temp.length >= 4) {
             throw new IllegalArgumentException();
         }
         numbers = new ArrayList<>();
-        for(int i=0; i<3;i++){
+        for (int i = 0; i < 3; i++) {
             numbers.add(Integer.parseInt(temp[i]));
         }
     }
