19c65b6d08041962debfd172052f6a4cbba1a2ab
yoon <beatus96@naver.com>
2022-11-08 11:34:39 +0900

    test(playernumberTest) : 입력 값 유효성 체크 기능 테스트 수정
    수정 이유:  플레이어 수 입력받기 기능과 플레이어 수 유효성 체크하기 기능이 분화 됨.
    
    수정 이후 : 예외 발생 여부가 아닌, 옳은 boolean값 반환하는 지 테스트한다.

--- src/test/java/baseball/PlayerNumberTest.java
+++ src/test/java/baseball/PlayerNumberTest.java
@@ -1,20 +1,28 @@
 package baseball;
 
 import java.io.*;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class PlayerNumberTest {
-    @Test
-    void 유효하지않은_플레이어수_숫자아닌경우_예외발생_테스트() {
-        String input = "120"; //"가나다" ,"ABC" ,"012", "1234"
-        InputStream in = new ByteArrayInputStream(input.getBytes());
-        System.setIn(in);
+//    @Test
+//    void 플레이어수_범위내의수_예외발생_테스트() {
+//        String input = "120"; //"가나다" ,"ABC" ,"012", "1234"
+//        InputStream in = new ByteArrayInputStream(input.getBytes());
+//        System.setIn(in);
+//
+//        assertThatThrownBy(() -> Application.getPlayerNumber())
+//                .isInstanceOf(IllegalArgumentException.class);
+//    }
 
-        assertThatThrownBy(() -> Application.getPlayerNumber())
-                .isInstanceOf(IllegalArgumentException.class);
+    @Test
+    void 입력값_유효성_테스트() {
+        List<Integer> input = List.of(1, 1, 2, 3);
+        assertThat(Application.isValidNumber(input)).isFalse();
     }
 
 }
