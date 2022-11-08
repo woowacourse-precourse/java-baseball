a6cdb3780c71060e6fdae2d0bf59bab4ce513e13
yoon <beatus96@naver.com>
2022-11-08 12:38:59 +0900

    test(study) : junit 테스트 코드 작성 연습

---
+++ src/test/java/study/JunitPractice.java
@@ -0,0 +1,27 @@
+package study;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class JunitPractice {
+    public List<Integer> operate(final int money) {
+        if (!isValidMoney(money)) {  //money가 천원이 아니면,
+            throw new RuntimeException("올바른 금액이 아닙니다.");
+        }
+        return generate();
+    }
+
+    private boolean isValidMoney(final int money) {
+        return money == 1000;
+    }
+
+    private List<Integer> generate() {
+        return new Random()
+                .ints(1, 45 + 1)
+                .distinct()
+                .limit(6)
+                .boxed()
+                .collect(Collectors.toList());
+    }
+}
