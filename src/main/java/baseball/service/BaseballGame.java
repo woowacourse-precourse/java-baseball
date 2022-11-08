5f0ed8c7113fbe7cf11f50ca0ab29b50eb2af51f
ohhonggi <5ghdrl@naver.com>
2022-11-08 19:27:57 +0900

    feat($BaseballGame): Implement startNewGame function
    This function repeats the number baseball game in progress until
    user enters the correct answer.
    
    This function uses the feat_3 function
    - compareInputAndRandomNumber
    
    Closes feat_5

--- src/main/java/baseball/service/BaseballGame.java
+++ src/main/java/baseball/service/BaseballGame.java
@@ -1,5 +1,21 @@
 package baseball.service;
 
+import baseball.model.Computer;
+import baseball.model.User;
+
+import static baseball.service.Comparator.compareInputAndRandomNumber;
+
 public class BaseballGame {
+
+    public static void startNewGame() {
+        boolean isCorrect = false;
+        Computer computer = new Computer();
+        while (!isCorrect) {
+            System.out.print("숫자를 입력해주세요 : ");
+            User user = new User();
+            isCorrect = compareInputAndRandomNumber(computer, user);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 
 }
