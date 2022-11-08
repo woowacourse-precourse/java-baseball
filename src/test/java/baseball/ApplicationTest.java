f6b3517863a6a3a34399abdfbb3a0501d269aadf
yeada <yeadalm@gmail.com>
2022-11-08 22:43:03 +0900

    test : 게임 진행 test case 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -27,6 +27,29 @@ class ApplicationTest extends NsTest {
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+    @Test
+    void case1() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("246", "783", "159", "951", "3", "2");
+                    assertThat(output()).contains("낫싱", "낫싱", "2볼 1스트라이크", "3스트라이크", "1 또는 2", "종료");
+                },
+                9, 5, 1
+        );
+    }
+    @Test
+
+    void case2() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("123", "145", "671", "216", "713", "1", "123", "456", "789", "641", "642", "246", "2");
+                    assertThat(output()).contains("시작합니다", "1볼 1스트라이크", "1볼", "2볼",
+                            "1스트라이크", "3스트라이크", "게임 종료", "1볼","1볼 1스트라이크", "낫싱",
+                            "1볼 1스트라이크", "2볼 1스트라이크", "3스트라이크", "게임 종료", "프로그램이 종료");
+                },
+                7, 1, 3, 2, 4, 6
+        );
+    }
 
     @Override
     public void runMain() {
