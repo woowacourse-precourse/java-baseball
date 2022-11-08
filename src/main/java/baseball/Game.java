35cc773e7c8790ca34571793347df1f8c1b1d7ac
kki4504 <rhkddlf9876@gmail.com>
2022-11-08 11:33:54 +0900

    refactor : rename methods

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,22 +1,36 @@
 package baseball;
 
+import baseball.validation.GetNumber;
+import baseball.validation.Restart;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Game {
-    SystemMessage systemMessage = new SystemMessage();
-    List<Integer> resultNumbers;
+    private SystemMessage systemMessage = new SystemMessage();
+    private GetNumber getNumber = new GetNumber();
+    private Restart restart = new Restart();
+    private List<Integer> resultNumberList;
+
     void start() {
+        int restartController = 1;
         System.out.println(systemMessage.START_GAME_MESSAGE);
-        resultNumbers = setThreeLengthNumberWithoutDuplication();
+        while (restartController == 1) {
+            resultNumberList = setResultNumberList();
+            while (true) {
+                List<Integer> number = inputNumber();
+                if (strikeBallCheck(number) == 3) {
+                    System.out.println(systemMessage.END_GAME_MESSAGE);
+                    break;
+                }
+            }
+            restartController = SetRestartController();
+        }
     }
 
-    List<Integer> setThreeLengthNumberWithoutDuplication() {
+    private List<Integer> setResultNumberList() {
         List<Integer> resultNumbers = new ArrayList<>();
         while (resultNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -27,22 +41,53 @@ public class Game {
         return resultNumbers;
     }
 
-    List<Integer> getNumber() {
+    private List<Integer> inputNumber() {
         System.out.print(systemMessage.INSERT_NUMBER_MESSAGE);
-        List<Integer> numberList = Arrays.stream(Console.readLine().split(""))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
+        List<Integer> numberList = new ArrayList<>();
+        String numberString = Console.readLine();
+
+        if (getNumber.getNumberValidate(numberString)) {
+            numberList = Arrays.stream(numberString.split(""))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+        }
         return numberList;
     }
 
-    char gameRestartController() {
+    private int strikeBallCheck(List<Integer> userNumberList) {
+        int strikeCount = 0, ballCount = 0;
+        for (int index = 0; index < userNumberList.size(); index++) {
+            int resultNumber = resultNumberList.get(index);
+            int userNumber = userNumberList.get(index);
+
+            if (resultNumber == userNumber) {
+                strikeCount += 1;
+            } else if (resultNumberList.contains(userNumber)) {
+                ballCount += 1;
+            }
+        }
+        printStrikeBallResult(strikeCount, ballCount);
+        return strikeCount;
+    }
+
+    private void printStrikeBallResult(int strikeCount, int ballCount) {
+        if (strikeCount > 0 && ballCount > 0) {
+            System.out.println(ballCount + systemMessage.BALL + " " + strikeCount + systemMessage.STRIKE);
+        } else if (ballCount > 0) {
+            System.out.println(ballCount + systemMessage.BALL);
+        } else if (strikeCount > 0) {
+            System.out.println(strikeCount + systemMessage.STRIKE);
+        } else {
+            System.out.println(systemMessage.NOTHING);
+        }
+    }
+
+    private int SetRestartController() {
         System.out.println(systemMessage.RESTART_GAME_MESSAGE);
         String inputRestartValue = Console.readLine();
-
-        if (inputRestartValue.length() == 1) {
-            return inputRestartValue.charAt(0);
-        } else {
-            throw new IllegalArgumentException();
+        if (restart.validate(inputRestartValue)) {
+            return Integer.parseInt(inputRestartValue);
         }
+        throw new IllegalArgumentException();
     }
 }
