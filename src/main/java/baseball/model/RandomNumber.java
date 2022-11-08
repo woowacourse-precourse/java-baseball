956e81f7a0aa990151da2a104e82287649d5a8fa
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-08 22:18:11 +0900

    chore(RandomNumber): add Magic Number

--- src/main/java/baseball/model/RandomNumber.java
+++ src/main/java/baseball/model/RandomNumber.java
@@ -8,10 +8,12 @@ public class RandomNumber {
 
     public static List<Integer> setRandomNumber() {
         final int NUMBER_LIST_SIZE = 3;
+        final int MIN_NUMBER = 1;
+        final int MAX_NUMBER = 9;
         List<Integer> randomNumberList = new ArrayList<>();
 
         while (randomNumberList.size() < NUMBER_LIST_SIZE) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             RandomNumber.addUniqueNumber(randomNumberList, randomNumber);
         }
         return randomNumberList;
