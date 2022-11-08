3a97881ad76e955b180473ba6e7e7d884b9c9547
sschan99 <seungchan1219@gmail.com>
2022-11-08 22:01:56 +0900

    fix(Application): import IllegalArgumentException instead of creating
    Delete code defining IllegalArgumentException
    and replace it with import

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,14 +2,12 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+import java.lang.IllegalArgumentException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 
 public class Application {
-    public static class IllegalArgumentException extends RuntimeException {
-    }
-
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
