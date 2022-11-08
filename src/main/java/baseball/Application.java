36148fb8213df1dccb59dd3e2a88db9d5eabcc00
HCH <clgusdl2@naver.com>
2022-11-08 16:23:05 +0900

    Revert "FEAT : 야구게임 재시작 메소드 구현"
    This reverts commit de9b883d0ff6f9cc888e4360600a4ce4808294c6.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -110,17 +110,7 @@ public class Application {
             throw new IllegalArgumentException();
         }
     }
-    static boolean reTry(){
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
-        String input;
-        input = Console.readLine();
-        if (input.equals("1")){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
+
 
 
 
