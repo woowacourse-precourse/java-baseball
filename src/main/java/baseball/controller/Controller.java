045e832174d25db16797ef7ae5746aa8ce775876
kiarakim <kiarakim413@gmail.com>
2022-11-08 22:58:22 +0900

    feat : set, play, end and ask game

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -1,8 +1,14 @@
 package baseball.controller;
 
 import baseball.service.GameService;
+import baseball.view.Requester;
+import baseball.view.Respondent;
 
 public class Controller {
+
+    final int SIZE = 3;
+    final int START_NUM = 1;
+    final int END_NUM = 9;
 
     GameService service = new GameService();
 
@@ -14,18 +20,18 @@ public class Controller {
     }
 
     private void setGame() {
-
+        service.setGame(SIZE, START_NUM, END_NUM);
     }
 
     private void playGame() {
-
+        service.playGame();
     }
 
     private void endGame() {
-
+        Respondent.respondGameOver();
     }
 
     private void askRetry() throws IllegalArgumentException {
-
+        Requester.requestRetryOrFinish();
     }
 }
