5fc0c510166ad56787e0a6098f0b42040517233a
hyejuc <chjjudy@naver.com>
2022-11-08 16:02:29 +0900

    feat: 사용자의 입력값에 대한 예외 확인 기능 추가
    checkException 추가:
    - 입력 가능한 값 : 1~9로 이루어진 서로 다른 3자리의 수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,4 +8,16 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
 
     }
+    public static void checkException(int num){
+        String numString = Integer.toString(num);
+        if (numString.length() != 3){
+            throw new IllegalArgumentException();
+        }
+        if (numString.contains("0")){
+            throw new IllegalArgumentException();
+        }
+        if (!containSameNumber(numString)){
+            throw new IllegalArgumentException();
+        }
+    }
 }
