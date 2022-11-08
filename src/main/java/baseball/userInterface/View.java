301bb102895cee376c51cbac67aaf7ec15d855b8
great-park <cksgh1735@naver.com>
2022-11-08 20:28:40 +0900

    fix: 시작 메시지 수정

--- src/main/java/baseball/userInterface/View.java
+++ src/main/java/baseball/userInterface/View.java
@@ -3,7 +3,7 @@ package baseball.userInterface;
 public class View {
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String ASK_PLAY_AGAIN_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작하겠습니다.";
+    private static final String START_GAME_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String END_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void ViewInputNumber() {
