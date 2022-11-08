a971b7631fd24bbd3a081cac89430a5a3caba62d
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-08 17:55:49 +0900

    feat: 1. 시작 메시지를 출력하는 기능 추가(printStartMessage)

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -32,4 +32,8 @@ public class NumberBaseballGame {
             start = judgeRestart(restartNumber);
         }
     }
+
+    private void printStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
 }
