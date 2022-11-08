5a03c001c212a9b1483e3fcc41316651c19af40e
조병우 <whquddn55@gmail.com>
2022-11-08 10:22:15 +0900

    feat(OutputGenerator): implement print welcome method
    This method just print welcome message to console.
    
    + public  void printWelcome()

--- src/main/java/baseball/utils/OutputGenerator.java
+++ src/main/java/baseball/utils/OutputGenerator.java
@@ -4,4 +4,7 @@ package baseball.utils;
  * TODO: implement the class
  */
 public class OutputGenerator {
+    public void printWelcome() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
 }
