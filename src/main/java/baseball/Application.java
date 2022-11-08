80d503b17e4b73d1341c04d3d20946a4aed0771c
hyunsung0917 <sagiri0917@gmail.com>
2022-11-08 11:46:37 +0900

    게임 종료 시의 이벤트 구현, 새로 시작할 지 종료할 지

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,9 +7,11 @@ class Game{
     public int game_clear=0;
     int strike_num=0;
     int ball_num=0;
+    int continue_game;
     int[] target_check;
 //    public static int game_restart_flag;
     public Game(){
+        this.continue_game=1;
         this.strike_num=0;
         this.ball_num=0;
         this.game_clear=0;
@@ -39,7 +41,7 @@ class Game{
     }
     public String Input(){
         String user_num="";
-        System.out.println("숫자를 입력해 주세요 : ");
+        System.out.print("숫자를 입력해 주세요 : ");
         user_num=camp.nextstep.edu.missionutils.Console.readLine();
         return user_num;
     }
@@ -64,6 +66,10 @@ class Game{
         }else{
             System.out.println(to_print);
         }
+        if(strike==3){
+            game_clear=1;
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
 
     }
     public void check(){
@@ -85,21 +91,30 @@ class Game{
                 ball_num+=isBall(ball_check[i]);
             }
         }
+        ball_and_strike_print(ball_num,strike_num);
+        if(game_clear==1){
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int game_flag=Integer.parseInt(camp.nextstep.edu.missionutils.Console.readLine());
+            if(game_flag==2){
+                continue_game=0;
+            }
 
-//        System.out.println(targetNumber);
-//        System.out.println(strike_num);
-//        System.out.println(ball_num);
+        }
+        System.out.println(targetNumber);
+        System.out.println(strike_num);
+        System.out.println(ball_num);
 
 
     }
-//    public void game_processing(){
-//        while(true){
-//            if(game_clear==1){
-//                break;
-//            }
-//            check();
-//        }
-//    }
+    public void game_processing(){
+        while(true){
+            check();
+            if(game_clear==1){
+                break;
+            }
+
+        }
+    }
 
 
 
@@ -112,8 +127,10 @@ public class Application {
 //        System.out.println(randomNumber);
         while(true){
             Game g=new Game();
-            g.check();
-            break;
+            g.game_processing();
+            if(g.continue_game==0){
+                break;
+            }
 //            if(g.game_restart_flag==2){
 //                break;
 //            }
