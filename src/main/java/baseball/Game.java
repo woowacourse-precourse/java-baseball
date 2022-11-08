294163abab1366d828d57876316bd2f654ab2ffd
kim1387 <kevink0@naver.com>
2022-11-08 22:02:34 +0900

    feat: 게임 시작, 재시작등을 관리하는 로직 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,16 +1,68 @@
 package baseball;
 
 import computer.Computer;
+import iosystem.InputSystem;
 import user.User;
+
+import java.util.List;
+
+import static baseball.GameMessage.*;
+import static baseball.GameMessage.START_GAME;
 
 public class Game {
 
     private User user;
     private Computer computer;
+    private Score score;
 
-    public Game(User user, Computer computer) {
-        this.user = user;
-        this.computer = computer;
+    public Game() {
+        this.user = new User();
+        this.computer = new Computer();
+        this.score = new Score();
+    }
+
+    public void init() {
+        this.user = new User();
+        this.computer = new Computer();
+        this.score = new Score();
+    }
+    public boolean isEndGame(){
+        return score.getStrike() == 3;
+    }
+
+    public void start(){
+        System.out.println(START_GAME.getMessage());
+        boolean inputRetryNumber = true;
+        boolean correctAnswer = false;
+        while (inputRetryNumber){
+            init();
+            while (!correctAnswer){
+                correctAnswer = play();
+            }
+            inputRetryNumber = checkRestart();
+            correctAnswer = false;
+        }
+    }
+
+    public boolean play(){
+        List<Digit> inputDigitNumber = InputSystem.inputTryNumber();
+        List<Integer> targetNumber = computer.getTargetNumber();
+        Digits digitTragetList = new Digits(InputSystem.convertDigit(targetNumber));
+        System.out.println("user"+inputDigitNumber);
+        System.out.println("com"+digitTragetList.getDigitList());
+        List<ScoreStatus> scoreResultList = digitTragetList.checkAllDigits(new Digits(inputDigitNumber));
+        score.countTotalScore(scoreResultList);
+        score.printScore();
+        if (isEndGame()){
+            System.out.println(END_GAME.getMessage());
+            return true;
+        }
+        score.clear();
+        return false;
+    }
+
+    public boolean checkRestart(){
+        return InputSystem.inputRetryNumber();
     }
 
 
