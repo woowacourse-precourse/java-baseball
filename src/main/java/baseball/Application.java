bca74d8246d6cc20d25958c01486d76f5cbbe8a1
AhnJoonSung <tjdtna01@naver.com>
2022-11-08 15:06:07 +0900

    Class Rename : Game -> Baseball

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,7 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        Game baseball = new Game();
+        Baseball baseball = new Baseball();
         baseball.start();
     }
 }
--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -4,7 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
 
-public class Game {
+public class Baseball {
     public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         running();
