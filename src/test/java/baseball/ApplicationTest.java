00876f704c58f5a9717fe2b69dacfddf4261476d
Hong Jin-Seop <jinsub1999@khu.ac.kr>
2022-11-08 11:21:21 +0900

    test: add test
    중복숫자 및 다른문자 입력, 숫자 결과에 대한 테스트를 추가하였습니다

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -7,8 +7,28 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static baseball.Application.getResult;
 
 class ApplicationTest extends NsTest {
+    @Test
+    void 숫자_결과_테스트() {
+        String answer = "723";
+        String case1 = "723";
+        String case2 = "327";
+        String case3 = "192";
+        String case4 = "946";
+
+        String resultCase1 = getResult(answer, case1);
+        String resultCase2 = getResult(answer, case2);
+        String resultCase3 = getResult(answer, case3);
+        String resultCase4 = getResult(answer, case4);
+
+        assertThat(resultCase1).isEqualTo("3스트라이크");
+        assertThat(resultCase2).isEqualTo("2볼 1스트라이크");
+        assertThat(resultCase3).isEqualTo("1볼");
+        assertThat(resultCase4).isEqualTo("낫싱");
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -24,6 +44,22 @@ class ApplicationTest extends NsTest {
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_중복숫자_입력() {
+        assertSimpleTest(
+                () -> assertThatThrownBy(() -> runException("133"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 예외_테스트_다른문자_입력() {
+        assertSimpleTest(
+                () -> assertThatThrownBy(() -> runException("abc"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
