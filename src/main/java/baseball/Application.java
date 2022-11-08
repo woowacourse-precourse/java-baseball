f3ec21b89e7df60e58cea8f05c496a51363c52f0
chaos0103 <lyt1228@naver.com>
2022-11-08 15:01:54 +0900

    feat: 게임이 끝난 경우 재시작/종료 여부를 입력받는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,15 +7,38 @@ import java.util.List;
 public class Application {
 
     private final static ComputerNumber computerNumber = new ComputerNumber();
+    private final static BaseballGameService baseballGameService = new BaseballGameService();
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        List<Integer> answer = computerNumber.createComputerNumber();
 
-        while (true) {
-            System.out.println("숫자를 입력해주세요 : ");
-            String inputNumber = Console.readLine();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        User user = new User();
+
+        while (!user.isEnd()) {
+            if (user.getAnswers().isEmpty()) {
+                List<Integer> answer = computerNumber.createComputerNumber();
+                user.insertAnswer(answer);
+            }
+
+            System.out.print("숫자를 입력해주세요 : ");
+            String number = Console.readLine();
+            user.inputNumber(number);
+
+            baseballGameService.run(user);
+            String resultMessage = baseballGameService.getResultMessage(user);
+            System.out.println(resultMessage);
+
+            if (resultMessage.equals("3스트라이크")) {
+                user.correctAnswer();
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String request = Console.readLine();
+                if (request.equals("1")) {
+                    user.reset();
+                } else if (!request.equals("2")) {
+                    throw new IllegalArgumentException();
+                }
+            }
         }
 
     }
