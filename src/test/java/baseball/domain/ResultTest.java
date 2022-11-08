a8bdb109bb40189692d132239d600ed3f4641284
DaWoon Lee <qkgk61@gmail.com>
2022-11-08 22:20:44 +0900

    Test : Result의 get과 add 테스트

---
+++ src/test/java/baseball/domain/ResultTest.java
@@ -0,0 +1,20 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+
+public class ResultTest {
+
+    @Test
+    void get_add_맵에_결과를_반영() {
+        Result result = new Result();
+
+        result.add(ResultType.BALL);
+        result.add(ResultType.STRIKE);
+        result.add(ResultType.STRIKE);
+
+        assert (result.get(ResultType.BALL) == 1);
+        assert (result.get(ResultType.STRIKE) == 2);
+        assert (result.get(ResultType.NOTHING) == 0);
+    }
+
+}
