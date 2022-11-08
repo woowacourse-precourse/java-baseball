a7c3b178c51ea49c2f57352cc16d4c2eadae1d15
jooys130 <wndustn130@gmail.com>
2022-11-08 13:36:54 +0900

    feat: add hint and end print message

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import static baseball.Constants.*;
+
 public class View {
     public static void printStartMsg() {
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -9,6 +11,21 @@ public class View {
         System.out.println("숫자를 입력해주세요 : ");
     }
 
-    public static void printOutputMsg(){
+    public static void printHintMsg(int[] results){
+        int strikeCnt = results[STRIKE_IDX];
+        int ballCnt = results[BALL_IDX];
+        if(strikeCnt == 0 && ballCnt == 0){
+            System.out.println("낫싱");
+        } else if (strikeCnt == 0 && ballCnt > 0){
+            System.out.println(ballCnt + "볼");
+        } else if (ballCnt == 0 && strikeCnt > 0){
+            System.out.println(strikeCnt + "스트라이크");
+        } else if (strikeCnt > 0 && ballCnt > 0) {
+            System.out.println(ballCnt + "볼 " + strikeCnt + "스트라이크");
+        }
+    }
+
+    public static void printEndMsg(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
