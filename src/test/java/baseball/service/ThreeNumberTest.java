9afa773bcc78198fe6e455ab7a6eba398ec4bcfc
footdev <ekclstkfka5@gmail.com>
2022-11-08 17:19:48 +0900

    test: ThreeNumberTest 생성
    테스트 파일 생성

---
+++ src/test/java/baseball/service/ThreeNumberTest.java
@@ -0,0 +1,12 @@
+package baseball.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ThreeNumberTest {
+
+    @Test
+    void generate() {
+    }
+}
