710b2557e0815557ba82af079a1c605c30c10563
sungjun kim <dd8873@naver.com>
2022-11-08 20:01:43 +0900

    test : 테스트 코드 추가 - 랜덤 숫자 생성 확인, 몇개의 숫자가 같은지 확인, 스트라이크 수 확인

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,7 +1,12 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -9,6 +14,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    @DisplayName("랜덤 숫자 생성 확인")
+    void randomTest(){
+        List<Integer> test = ComputerRandomNumbersFactory.createComputerRandomNumber();
+        assertThat(3).isEqualTo(test.size());
+    }
+
+    @Test
+    @DisplayName("몇개의 숫자가 같은지 확인")
+    void count(){
+        assertThat(3).isEqualTo(MatchCounter.countAllMatched(new UserNumber("123"), Arrays.asList(3,1,2)));
+    }
+
+    @Test
+    @DisplayName("스트라이크 수 확인")
+    void strike(){
+        assertThat(1).isEqualTo(MatchCounter.countStrike(new UserNumber("324"),Arrays.asList(3,1,2)));
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
