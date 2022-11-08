4b2a9bc2513db8e86b1f6ab01c8231bbb2b575b2
JuSeong1130 <jsmoon1130@naver.com>
2022-11-08 19:23:24 +0900

    feat(BaseballGameRule): 야구 Rule에 따른 점수 얻는 메서드
    같은 자리 Strike 다른자리에 포함 ball

--- src/main/java/baseball/rule/BaseballGameRule.java
+++ src/main/java/baseball/rule/BaseballGameRule.java
@@ -2,32 +2,34 @@ package baseball.rule;
 
 import baseball.player.Computer;
 import baseball.player.Player;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class BaseballGameRule {
 
 	private Player player;
 	private Computer computer;
-
 	private String playerNumber;
-
 	private String computerNumber;
-
+	private List<Integer> scoreList;
+	private static final int STRIKE_INDEX = 0;
+	private static final int BALL_INDEX = 1;
 	private static final String NUMBER_START = "1";
 	private static final String NUMBER_END = "9";
-
 	private static final int NUMBER_LENGTH = 3;
 
 	public BaseballGameRule(Player player, Computer computer) {
 		this.player = player;
 		this.computer = computer;
 		computerNumber = computer.getNumber();
+		scoreList = new ArrayList<>(Arrays.asList(0, 0));
 	}
 
 	public void isPlayerNumberValidate() {
 		playerNumber = player.getNumber();
-		if (!isLength3() || !isNumber() ||!isDifferentNumbersEach()) {
+		if (!isLength3() || !isNumber() || !isDifferentNumbersEach()) {
 			throw new IllegalArgumentException("올바른 값을 입력하세요(서로 다른 숫자 3개");
 		}
 	}
@@ -47,6 +49,47 @@ public class BaseballGameRule {
 	private String removeDistinctNumber() {
 		return Arrays.stream(playerNumber.split("")).distinct()
 				.collect(Collectors.joining());
+	}
+
+	public String getScore() {
+		scoreList = new ArrayList<>(Arrays.asList(0, 0));
+		countStrike();
+		countBall();
+		return convertListToString(scoreList);
+	}
+
+	private String convertListToString(List<Integer> scoreList) {
+		return scoreList.stream().map(String::valueOf).collect(Collectors.joining());
+	}
+
+	private void countStrike() {
+		if (playerNumber.equals(computerNumber)) {
+			scoreList.set(STRIKE_INDEX, 3);
+		}
+		int count = 0;
+		for (int i = 0; i < NUMBER_LENGTH; i++) {
+			if (isCharEquals(i)) {
+				count++;
+			}
+		}
+		scoreList.set(STRIKE_INDEX, count);
+	}
+
+	private void countBall() {
+		int count = 0;
+		for (int i = 0; i < NUMBER_LENGTH; i++) {
+			if (!isCharEquals(i) && isPlayerNumberContains(i)) {
+				count++;
+			}
+		}
+		scoreList.set(BALL_INDEX, count);
+
+	}
+	private boolean isCharEquals(int index){
+		return playerNumber.charAt(index) == computerNumber.charAt(index);
+	}
+	private boolean isPlayerNumberContains(int index){
+		return computerNumber.indexOf(playerNumber.charAt(index)) != -1;
 	}
 
 
