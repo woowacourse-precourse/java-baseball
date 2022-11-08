b360951bf570c2164230729e2b5b039ac0ffa817
Laeng <Laeng@users.noreply.github.com>
2022-11-08 12:40:17 +0900

    test(Controller): 기존 테스트 케이스로 GameController 에 대한 테스트 케이스로 변경

--- src/test/java/baseball/controller/GameControllerTest.java
+++ src/test/java/baseball/controller/GameControllerTest.java
@@ -8,14 +8,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("GameNumbers 테스트")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-public class ControllerTest {
+public class GameControllerTest {
 
     @Nested
     class 입력_검증 {
         @ParameterizedTest(name = "한 자리 숫자 입력 실패 => {0}")
         @ValueSource(strings = {"a", "ㄱ", "가", "0", "11"})
         void 한_자리_숫자_입력_실패(String input) {
-            Controller controller = new Controller();
+            GameController controller = new GameController();
             boolean result = controller.validateInput("^[1-2]{1}$", input);
 
             assertThat(result).isFalse();
@@ -24,7 +24,7 @@ public class ControllerTest {
         @ParameterizedTest(name = "한 자리 숫자 입력 성공 => {0}")
         @ValueSource(strings = {"1", "2"})
         void 한_자리_숫자_입력_성공(String input) {
-            Controller controller = new Controller();
+            GameController controller = new GameController();
             boolean result = controller.validateInput("^[1-2]{1}$", input);
 
             assertThat(result).isTrue();
@@ -33,7 +33,7 @@ public class ControllerTest {
         @ParameterizedTest(name = "세 자리 수 입력 실패")
         @ValueSource(strings = {"a", "aaa", "ㄱㄱㄱ", "가가가", "1", "22", "000"})
         void 세_자리_숫자_입력_실패(String input) {
-            Controller controller = new Controller();
+            GameController controller = new GameController();
             boolean result = controller.validateInput("^[1-9]{3}$", input);
 
             assertThat(result).isFalse();
@@ -42,7 +42,7 @@ public class ControllerTest {
         @ParameterizedTest(name = "세 자리 수 입력 성공")
         @ValueSource(strings = {"111", "234"})
         void 세_자리_숫자_입력_성공(String input) {
-            Controller controller = new Controller();
+            GameController controller = new GameController();
             boolean result = controller.validateInput("^[1-9]{3}$", input);
 
             assertThat(result).isTrue();
