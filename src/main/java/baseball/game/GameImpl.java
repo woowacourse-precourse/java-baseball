01df167146587bfad6a8b637d54720f168e696ca
kimdoha <kkhhjj3393@gmail.com>
2022-11-08 13:49:30 +0900

    feat(BaseballGame): Game 인터페이스 기능 목록의 구현체 GameImpl 클래스 생성

---
+++ src/main/java/baseball/game/GameImpl.java
@@ -0,0 +1,10 @@
+package baseball.game;
+
+import baseball.player.Computer;
+import baseball.player.User;
+import baseball.score.Result;
+import baseball.score.Score;
+
+public class GameImpl implements Game{
+
+}
