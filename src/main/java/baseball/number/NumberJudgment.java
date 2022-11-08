b58c8909a562b23f67f76ffdcc2048b7fcb8803f
Gibeom Nam <rlqja0523@naver.com>
2022-11-08 22:35:49 +0900

    feat: 두 리스트의 같은 숫자 개수를 반환

---
+++ src/main/java/baseball/number/NumberJudgment.java
@@ -0,0 +1,21 @@
+package baseball.number;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class NumberJudgment {
+    private Map<Integer, Integer> computerSameNumberIndex = new HashMap<>();
+
+    public int getSameNumberCount(List<Integer> computerNumberList, List<Integer> userNumberList) { // 같은 숫자가 있는가?
+        int sameNumberCount = 0;
+
+        for (int userNumber : userNumberList) {
+            if (computerNumberList.contains(userNumber)) {
+                computerSameNumberIndex.put(userNumber, computerNumberList.indexOf(userNumber));
+                sameNumberCount++;
+            }
+        }
+        return sameNumberCount;
+    }
+}
