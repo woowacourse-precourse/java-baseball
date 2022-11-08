96646543207e9cbb3e1e6a49eac26c527574d015
조병우 <whquddn55@gmail.com>
2022-11-08 11:16:04 +0900

    feat(Player): implement reading user's ball number method
    This method read user's ball number in standart input. And by using inputValidator that is validating user's inputs, validating input and will throw IllegalArgumentException when input is not valid.
    
    + public Player(InputValidtor)
    + getters
    + public void readBallNumber()

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,7 +1,26 @@
 package baseball.domain;
 
-/**
- * TODO: implement the class
- */
+import baseball.utils.InputValidator;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Player {
+    private final static String YES = "1";
+    private final InputValidator inputValidator;
+
+    private BallNumber ballNumber;
+
+    public Player(InputValidator inputValidator) {
+        this.inputValidator = inputValidator;
+    }
+
+    public BallNumber getBallNumber() {
+        return ballNumber;
+    }
+
+    public void readBallNumber() throws IllegalArgumentException {
+        String ballNumberString = Console.readLine();
+        inputValidator.validateBallNumberString(ballNumberString);
+
+        ballNumber = BallNumber.fromString(ballNumberString);
+    }
 }
