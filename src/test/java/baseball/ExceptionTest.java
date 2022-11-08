12e4bf5e3f11bff25a688ba79452d37a6c59d672
Sieun Park <psieun0606@gmail.com>
2022-11-08 12:52:02 +0900

    test: 1~9 이외 값 입력 예외 처리 기능 테스트 추가

---
+++ src/test/java/baseball/ExceptionTest.java
@@ -0,0 +1,28 @@
+package baseball;
+
+import baseball.controller.HandleException;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ExceptionTest extends NsTest {
+    @DisplayName("예외 처리 테스트1 - 사용자가 1~9 이외의 숫자를 입력했을 경우")
+    @Test
+    void userInputExceptionTest() {
+        // given
+        String userInput = "1:2";
+        HandleException handleException = new HandleException();
+
+        // when, then
+        assertThatThrownBy(() -> handleException.handleUserInputNumberRangeException(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 값을 입력하였습니다. 게임을 종료합니다.");
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
