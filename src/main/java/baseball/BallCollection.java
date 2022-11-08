fce7d173f0df9b4aa924f63e8a97c307990b6bec
JunginPark4364 <wjddlsee@gmail.com>
2022-11-08 17:11:56 +0900

    feat(BaseballGame, BallCollection): input expected value
    Input expected value. Validate input range.
    Change string to BallCollection.
    - add createPlayerNumbers method
    - add requestPlayerInputNumbers method
    - add validateInputRange method
    - add stringToBall method

--- src/main/java/baseball/BallCollection.java
+++ src/main/java/baseball/BallCollection.java
@@ -2,10 +2,8 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class BallCollection {
     public static final int BALL_COUNT = 3;
@@ -36,6 +34,14 @@ public class BallCollection {
         if (uniqueNumbers.size() != BALL_COUNT) {
             throw new IllegalArgumentException("올바른 입력이 아닙니다.");
         }
+    }
+
+    public static BallCollection stringToBall(String numbers) {
+        List<Integer> balls = Arrays.stream(numbers.split(""))
+                .map(number -> Integer.parseInt(number))
+                .collect(Collectors.toList());
+
+        return new BallCollection(balls);
     }
 
     public int size() {
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,6 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class BaseballGame {
+    private static final char BEGIN_RANGE = '1';
+    private static final char END_RANGE = '9';
+
+    private BallCollection createPlayerNumbers() {
+        String playerInput = requestPlayerInputNumbers();
+
+        validateInputRange(playerInput);
+        return BallCollection.stringToBall(playerInput);
+    }
+
+    private String requestPlayerInputNumbers() {
+        String playerInput = Console.readLine();
+        return playerInput;
+    }
+
+    private void validateInputRange(String playerInput) {
+        char input;
+
+        for (int index = 0; index < playerInput.length(); index++) {
+            input = playerInput.charAt(index);
+
+            if (input < BEGIN_RANGE || input > END_RANGE) {
+                throw new IllegalArgumentException("올바른 입력이 아닙니다.");
+            }
+        }
+    }
 
     private void printStartMessage() {
         System.out.println("숫자 야구 게임을 시작합니다.");
