624dcd682c31d8dc8cfa88aea2447774cd80f9cc
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-08 10:23:56 +0900

    feat(InputView): 게임 재시작, 종료 커맨드 상수 필드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,6 +5,8 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String CONTROLL_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String RESTART_GAME_INPUT = "1";
+    public static final String EXIT_GAME_INPUT = "2";
 
     public static String inputNumber() {
         System.out.print(INPUT_NUMBER_MESSAGE);
