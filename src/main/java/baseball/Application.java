f47056101ee42f0e2de3780cc0a09ddc9f402bec
roasted-penguin <swkwon25@hanyang.ac.kr>
2022-11-08 19:03:27 +0900

    feat main

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    public static boolean game_over = false;
+
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        List<Integer> computer = new ArrayList<>();
+        List<Integer> user = new ArrayList<>();
+        while(true){
+            makeGame(computer);
+            playgame(computer,user);
+            wouldYouExitGame();
+            if(game_over) break;
+        }
     }
 }
