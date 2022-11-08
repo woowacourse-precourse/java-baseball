76d1a694209c8acab3422a29466634e6d12102ba
cutthebutter <42573282+cutthebutter@users.noreply.github.com>
2022-11-08 15:23:30 +0900

    selectComputerNumber 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -5,7 +5,22 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.*;
 
 public class Application {
+    private static final int NUMBERS_SIZE = 3;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+    }
+
+    public static List<Integer> selectComputerNumber(){
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < NUMBERS_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        return computer;
     }
 }
