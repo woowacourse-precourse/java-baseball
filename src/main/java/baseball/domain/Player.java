dcada70ab0fc54273eb9d20f153588d3927c1739
yuntaeLee <leeyt1201@gmail.com>
2022-11-08 13:21:25 +0900

    feat: Player 숫자 범위 검증 메서드 구현

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -3,9 +3,24 @@ package baseball.domain;
 import java.util.List;
 
 public class Player {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    
     private final List<Integer> playerNumbers;
 
     public Player(List<Integer> playerNumbers) {
         this.playerNumbers = playerNumbers;
+    }
+
+    public List<Integer> getPlayerNumbers() {
+        return playerNumbers;
+    }
+
+    private boolean validateIsLessThanMinNumber(int number) {
+        return number < MIN_NUMBER;
+    }
+
+    private boolean validateIsMoreThanMaxNumber(int number) {
+        return number > MAX_NUMBER;
     }
 }
