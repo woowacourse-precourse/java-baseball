6ae583789621efd8e323f28b606c9b779bc39b0c
성익의 MacBook Pro <maegbugpeulo16@seong-ig-ui-MacBookPro.local>
2022-11-08 19:56:08 +0900

    한성익 과제 제출합니다

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -21,6 +21,50 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 스트라이크_확인() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("195", "238", "135", "2");
+                    assertThat(output()).contains("2스트라이크", "1스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 볼_확인() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("521", "891", "135", "2");
+                    assertThat(output()).contains("2볼", "1볼", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 낫싱_확인() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("892", "246", "135", "2");
+                    assertThat(output()).contains("낫싱", "낫싱", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
+    void 사용자_처음답이_정답인_경우() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("135", "2");
+                    assertThat(output()).contains("3스트라이크", "게임 종료");
+                },
+                1, 3, 5
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
