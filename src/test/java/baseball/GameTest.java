3f580e63c01c4e0a1568b3d5b37698067048a8b5
eyeofsol <kingjihong@naver.com>
2022-11-08 21:26:47 +0900

    chore(Game): import 제거
    사용하지 않는 import를 제거했습니다.

--- src/test/java/baseball/GameTest.java
+++ src/test/java/baseball/GameTest.java
@@ -8,7 +8,6 @@ import java.io.PrintStream;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class GameTest {
 
