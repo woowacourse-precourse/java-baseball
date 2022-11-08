f4ec8163c5daac4c4434b2237ef953255cdab717
masiljangajji <hjk172262@gmail.com>
2022-11-08 22:38:52 +0900

    feat: 실직적인 게임 진행과 정답여부 확인

--- src/main/java/baseball/PlayBaseBallGame.java
+++ src/main/java/baseball/PlayBaseBallGame.java
@@ -1,2 +1,27 @@
-package baseball;public class PlayBaseBallGame {
+package baseball;
+
+public class PlayBaseBallGame {
+    public void playGame() {
+        PrintMessage message = new PrintMessage();
+        InputNumber number = new InputNumber();
+        BaseBallCount countBaseBall = new BaseBallCount();
+
+
+        message.printStartBaseballGame();
+        number.inputComputerBaseballNumber();
+
+        while (true) {
+            message.printInputBaseballNumber();
+            number.inputUserBaseballNumber();
+
+            countBaseBall.countBallNumber(number.computerBaseBallNumber, number.userBaseBallNumber);
+            countBaseBall.countStrikeNumber(number.computerBaseBallNumber, number.userBaseBallNumber);
+            message.printHint(countBaseBall.numberOfStrik, countBaseBall.numberOfBall);
+
+            if (countBaseBall.numberOfStrik == 3) {
+                break;
+            }
+        }
+    }
+
 }
