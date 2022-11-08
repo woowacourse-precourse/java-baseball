cbd4017d770603e3edacebbf6a5c2aaf94d10f1a
Jongha Park <jongha2788@u.sogang.ac.kr>
2022-11-08 21:26:50 +0900

    test: IllegalArgumentException 테스트

--- src/test/java/baseball/ExceptionTest.java
+++ src/test/java/baseball/ExceptionTest.java
@@ -4,58 +4,59 @@ import net.bytebuddy.description.modifier.SynchronizationState;
 import net.bytebuddy.utility.FileSystem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ExceptionTest {
+
     private static User user;
     private static Game game;
-    private static Game game;
+
     @BeforeEach
-    public void beforeEach(){
+    public void beforeEach() {
         game = new Game();
         user = new User();
     }
 
     @Test
-    void 숫자_중복_테스트(){
+    void 숫자_중복_테스트() {
         String Usernumber = "112";
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
             () -> user.inputValidation(Usernumber));
+        assertThat(e.getMessage()).isEqualTo("잘못된 숫자 입력입니다.");
     }
 
     @Test
-    void 숫자_길이_테스트(){
+    void 숫자_길이_테스트() {
         String Usernumber = "1234";
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
             () -> user.inputValidation(Usernumber));
+        assertThat(e.getMessage()).isEqualTo("잘못된 숫자 입력입니다.");
     }
 
     @Test
-    void 숫자_길이_테스트2(){
+    void 숫자_길이_테스트2() {
         String Usernumber = "12";
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
             () -> user.inputValidation(Usernumber));
+        assertThat(e.getMessage()).isEqualTo("잘못된 숫자 입력입니다.");
+
     }
 
     @Test
-    void 종료_여부_입력_테스트(){
+    void 종료_여부_입력_테스트() {
         int StartFlag = 3;
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
             () -> game.checkEndGame(StartFlag));
+        assertThat(e.getMessage()).isEqualTo("잘못된 숫자 입력입니다.");
     }
 
     @Test
-    void 정답_입력_테스트(){
-        int  UserInput = 3;
+    void 정답_입력_테스트() {
+        int UserInput = 3;
         IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
             () -> game.checkEndGame(UserInput));
+        assertThat(e.getMessage()).isEqualTo("잘못된 숫자 입력입니다.");
     }
-    @Test
-    void 정답_입력_테스트(){
-        int  UserInput = 3;
-        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
-            () -> game.checkEndGame(UserInput));
-    }
-
 }
