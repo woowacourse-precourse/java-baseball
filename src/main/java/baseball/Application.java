72b373c790ed8a47c1168f3b05e6fa26072a03ff
yhkee0404 <yhkee0404@gmail.com>
2022-11-08 23:15:02 +0900

    feat: start

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    public static void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
     }
 }
