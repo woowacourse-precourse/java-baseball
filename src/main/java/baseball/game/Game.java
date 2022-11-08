8edaa034f3df8ca83d0e74e16b7fc7b63e996c36
gomljo <gomljo0307@naver.com>
2022-11-08 17:35:02 +0900

    feat(Game.java): 스트라이크 개수 집계 기능 구현
    상대방(컴퓨터)의 숫자 리스트와 플레이어(사용자)의 숫자 리스트를 비교하여 스트라이크의 수를 집계하는 기능 구현

--- src/main/java/baseball/game/Game.java
+++ src/main/java/baseball/game/Game.java
@@ -6,6 +6,12 @@ import baseball.player.Player;
 import java.util.List;
 
 public class Game {
+    private static final int NOT_FOUND=-1;
+    private static final int ANSWER=3;
+    private static final int NO_COUNT=0;
+    private static final int COUNT=1;
+    private static int strikeCount=0;
+    private static int ballCount=0;
     public Computer prepareComputer(Computer computer){
         computer.createRandomNumbers();
         return computer;
@@ -13,5 +19,11 @@ public class Game {
     public Player preparePlayer(Player player){
         player.createNumbers();
         return player;
+    }
+    private void countStrike(int playerNumber, int opponentNumber){
+
+        if(playerNumber==opponentNumber){
+            strikeCount+=COUNT;
+        }
     }
 }
