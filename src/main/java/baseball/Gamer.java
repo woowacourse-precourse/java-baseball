623ece71b190bead6670e94caaa3ce87929c6bcc
doehy <dbehxks02@naver.com>
2022-11-08 16:34:31 +0900

    refacotr : 카멜표기법을 따르지 않았던 변수 카멜표기법 적용

--- src/main/java/baseball/Gamer.java
+++ src/main/java/baseball/Gamer.java
@@ -11,29 +11,29 @@ import java.util.List;
 public class Gamer {
     public List<Integer> gamerNumber = new ArrayList<>();
 
-    public void checkException(String input_Number){
-        isWeirdSize(input_Number);
-        isNumber(input_Number);
-        isSameEachNumber(input_Number);
+    public void checkException(String inputNumber){
+        isWeirdSize(inputNumber);
+        isNumber(inputNumber);
+        isSameEachNumber(inputNumber);
     }
-    public void putInGamerNumber(int guess_number){
+    public void putInGamerNumber(int guessNumber){
         gamerNumber.clear();
-        gamerNumber.add(guess_number / 100);
-        gamerNumber.add((guess_number % 100)/10);
-        gamerNumber.add(guess_number % 10);
+        gamerNumber.add(guessNumber / 100);
+        gamerNumber.add((guessNumber % 100)/10);
+        gamerNumber.add(guessNumber % 10);
     }
     public void inputNumber(){
         System.out.print(INPUT_NUMBER);
-        String input_number = Console.readLine();
-        checkException(input_number);
-        int guess_number = Integer.parseInt(input_number);
-        putInGamerNumber(guess_number);
+        String inputNumber = Console.readLine();
+        checkException(inputNumber);
+        int guessNumber = Integer.parseInt(inputNumber);
+        putInGamerNumber(guessNumber);
     }
     public boolean inputRestartNumber(){
-        String restart_number = Console.readLine();
-        if(restart_number.equals(RESTART_BUTTON)){
+        String restartNumber = Console.readLine();
+        if(restartNumber.equals(RESTART_BUTTON)){
             return false;
-        }else if(restart_number.equals(FINISH_BUTTON)){
+        }else if(restartNumber.equals(FINISH_BUTTON)){
             return true;
         }else{
             throw new IllegalArgumentException();
--- src/main/java/baseball/Util.java
+++ src/main/java/baseball/Util.java
@@ -3,24 +3,24 @@ package baseball;
 import static baseball.Constant.*;
 
 public class Util {
-    public static void isWeirdSize(String input_Number){
-        if(input_Number.length() != GAMEPLAY_SIZE){
+    public static void isWeirdSize(String inputNumber){
+        if(inputNumber.length() != GAMEPLAY_SIZE){
             throw new IllegalArgumentException();
         }
     }
-    public static void isNumber(String input_Number){
-        for(int i=0; i<input_Number.length();i++){
-            char number = input_Number.charAt(i);
-            char start_num = (char)(MIN_NUM+'0');
-            char end_num = (char)(MAX_NUM+'0');
-            if(number >= start_num && number <= end_num);
+    public static void isNumber(String inputNumber){
+        for(int i=0; i<inputNumber.length();i++){
+            char number = inputNumber.charAt(i);
+            char startNum = (char)(MIN_NUM+'0');
+            char endNum = (char)(MAX_NUM+'0');
+            if(number >= startNum && number <= endNum);
             else{throw new IllegalArgumentException();}
         }
     }
-    public static void isSameEachNumber(String input_Number){
-        for(int i=0; i<input_Number.length()-1;i++){
-            String each_number = input_Number.substring(i+1);
-            if(each_number.contains(Character.toString(input_Number.charAt(i)))){
+    public static void isSameEachNumber(String inputNumber){
+        for(int i=0; i<inputNumber.length()-1;i++){
+            String eachNumber = inputNumber.substring(i+1);
+            if(eachNumber.contains(Character.toString(inputNumber.charAt(i)))){
                 throw new IllegalArgumentException();
             }
         }
