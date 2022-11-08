b8a2d1bd52065c94c31832d6ec8f51618594b130
buddy <jang_taehwan@naver.com>
2022-11-08 12:46:03 +0900

    refactor(Computer): 매직넘버 상수화

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -11,6 +11,7 @@ public class Computer {
 
     private static final Integer MIN_VALUE = 1;
     private static final Integer MAX_VALUE = 9;
+    private static final Integer MAX_LENGTH = 3;
 
     private List<Integer> answer;
 
@@ -44,12 +45,12 @@ public class Computer {
     }
 
     private void validateAnswer(List<Integer> answer) {
-        if (answer.size() != 3) {
+        if (answer.size() != MAX_LENGTH) {
             throw new IllegalArgumentException("3자리 숫자만 입력 가능합니다.");
         }
 
         Set<Integer> answerSet = new HashSet<>(answer);
-        if (answerSet.size() != 3) {
+        if (answerSet.size() != MAX_LENGTH) {
             throw new IllegalArgumentException("중복되지 않은 숫자만 입력 가능합니다.");
         }
 
