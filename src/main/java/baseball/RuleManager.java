e66287a7d1bb8e74945c1ca58c778b4059237ccc
HangSub999 <gkdtjq999@gmail.com>
2022-11-08 16:11:20 +0900

    feat: 스트라이크 카운트 기능

--- src/main/java/baseball/RuleManager.java
+++ src/main/java/baseball/RuleManager.java
@@ -1,4 +1,17 @@
 package baseball;
 
+import java.util.List;
+
 public class RuleManager {
+
+    public static int countStrike(User user, List<Long> computerNumbers) {
+        int strikeCount = 0;
+        int userNumberIndex = 0;
+        for (Long comNumber : computerNumbers) {
+            if (comNumber.equals(user.getUserNumbers().get(userNumberIndex++))) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
 }
