54c5589bf3e796e226c1b29481d644c4ea545711
이경범 <tkdwk567@naver.com>
2022-11-08 19:16:34 +0900

    refactor : Player class refactor
    List로 변환하는 toIntegerList() 기능을 클라이언트 쪽에서 정제해주는 것이 알맞다고 생각이 바뀜

--- src/main/java/baseball/objects/Player.java
+++ src/main/java/baseball/objects/Player.java
@@ -1,22 +1,13 @@
 package baseball.objects;
 
-import baseball.valid.Validation;
-
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Player {
 
     private List<Integer> numbers;
 
-    public void sayNumbers(String readLine) {
-        Validation validation = new Validation(readLine);
-        this.numbers = toIntegerList(readLine);
-    }
-
-    private  List<Integer> toIntegerList(String readLine) {
-        return Arrays.stream(readLine.split("")).map(Integer::valueOf).collect(Collectors.toList());
+    public void sayNumbers(List<Integer> numbers) {
+        this.numbers = numbers;
     }
 
     public void giveNumbersToReferee(Referee referee) {
