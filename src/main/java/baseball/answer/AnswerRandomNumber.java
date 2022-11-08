ed341267838ad89cc6cb9b7b195036b6b518df69
jaehyeon <jhjh0022@naver.com>
2022-11-08 18:30:26 +0900

    style: 줄간격 맞추기

--- src/main/java/baseball/answer/AnswerRandomNumber.java
+++ src/main/java/baseball/answer/AnswerRandomNumber.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AnswerRandomNumber {
-    public static List<Integer> makeAnswerRandomNumber() {
+    public List<Integer> makeAnswerRandomNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
