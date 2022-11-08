18df03306440861e2138d487f2877faf89b1e416
ddukddaki <rlfrkdms1@naver.com>
2022-11-08 15:02:46 +0900

    feat(User): 필요한 필드(number, validation) 선언

---
+++ src/main/java/baseball/player/User.java
@@ -0,0 +1,12 @@
+package baseball.player;
+
+import baseball.Validation;
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private List<Integer> number = new ArrayList<>();
+    private Validation validation;
+
+}
