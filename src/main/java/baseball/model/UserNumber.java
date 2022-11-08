27fe2ade5d1ca36d69c937d8f57b561a0d9802ef
Jeongthingamajig <netpetet@naver.com>
2022-11-08 22:19:17 +0900

    Refactor : 메서드 로직 변경
    유저가 입력한 값을 거꾸로 반환하고 있는 것을 바로 잡음

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -1,5 +1,7 @@
 package baseball.model;
 
+import baseball.util.NumberNaming;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -11,10 +13,14 @@ public class UserNumber {
 
     public static List<Integer> getUserNumber(String userNumber) {
         int number = Integer.parseInt(userNumber);
+        List<Integer> userReverseNumberList = new ArrayList<>(Collections.emptyList());
         List<Integer> userNumberList = new ArrayList<>(Collections.emptyList());
         while (number > 0){
-            userNumberList.add(number % 10);
+            userReverseNumberList.add(number % 10);
             number /= 10;
+        }
+        for(int i = 0; i < userReverseNumberList.size(); i++){
+            userNumberList.add(userReverseNumberList.get((NumberNaming.MAX_NUMBER_SIZE - 1) - i ));
         }
         return userNumberList;
     }
