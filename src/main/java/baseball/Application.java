3cba96c9aeceef9276fd343e16278f9c1259c0d8
Lemon Soju <yckk980320@naver.com>
2022-11-08 17:06:09 +0900

    feat(Application): game() method
    The game() method manage the overall flow of the baseball game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,52 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        int newGame = 1;
+
+        while (newGame == 1) {
+            newGame = game();
+        }
     }
+
+    public static int game() throws IllegalArgumentException {
+
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        String input;
+        boolean endGame = true;
+
+        while (endGame) {
+            System.out.print("숫자를 입력해주세요 : ");
+            input = Console.readLine();
+//            inputException(input);
+            endGame = scoreCalculation(input, computer);
+        }
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int newGame = Integer.parseInt(Console.readLine());
+        return newGame;
+    }
+
+    public static boolean scoreCalculation(String input, List<Integer> computer) {
+        return false;
+    }
+
+//    public static void inputException(String input)
+
 }
