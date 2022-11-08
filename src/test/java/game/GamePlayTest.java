6c36544981deffa8435e4eda0e1073b585b61b55
kite707 <kite707707@gmail.com>
2022-11-08 11:33:12 +0900

    test(user,game): 테스트 생성

---
+++ src/test/java/game/GamePlayTest.java
@@ -0,0 +1,28 @@
+package game;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GamePlayTest {
+
+    @Test
+    void getAnswer() {
+    }
+
+    @Test
+    void testGetAnswer() {
+    }
+
+    @Test
+    void judgeBallOrStrike() {
+    }
+
+    @Test
+    void printHintMessage() {
+    }
+
+    @Test
+    void provideHintMessage() {
+    }
+}
---
+++ src/test/java/user/UserInputTest.java
@@ -0,0 +1,16 @@
+package user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserInputTest {
+
+    @Test
+    void getUserInput() {
+    }
+
+    @Test
+    void changeUserInputToList() {
+    }
+}
