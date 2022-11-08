575b26151bc7a5ba27921f5bc7f1a6cb0cf1f6ee
gomljo <gomljo0307@naver.com>
2022-11-08 16:06:19 +0900

    test(InputExceptionTest.java): 입력 문자열에 대한 모든 예외 처리 검증
    기능 목록에 정의한 예외 4종에 대한 검증 코드 테스트

--- src/test/java/baseball/exception/InputExceptionTest.java
+++ src/test/java/baseball/exception/InputExceptionTest.java
@@ -6,43 +6,68 @@ import static org.assertj.core.api.Assertions.*;
 public class InputExceptionTest {
     @Test
     @DisplayName("check user's input if input string contains alphabet")
-    void isProperLetter_Alphabet_Test() {
+    void isNotProperLetter_Alphabet_Test() {
         String wrongUserInputAlphabet = "Z56";
-        InputException inputException = new InputException();
-
-        assertThat(inputException.isProperLetter(wrongUserInputAlphabet)).isFalse();
+        assertThat(InputException.isNotProperLetter(wrongUserInputAlphabet)).isTrue();
     }
 
     @Test
     @DisplayName("Check user's input if input string contains special symbols")
-    public void isProperLetter_Symbol_Test(){
+    public void isNotProperLetter_Symbol_Test(){
         String wrongUserInputSymbol = "[56";
-        InputException inputException = new InputException();
-
-        assertThat(inputException.isProperLetter(wrongUserInputSymbol)).isFalse();
+        assertThat(InputException.isNotProperLetter(wrongUserInputSymbol)).isTrue();
     }
     @Test
     @DisplayName("Check for exception when length of input string is not 3")
-    public void isProperLength_Test(){
+    public void isNotProperLength_Test(){
         String wrongLengthUserInput="67";
-        InputException inputException = new InputException();
-
-        assertThat(inputException.isProperLength(wrongLengthUserInput)).isFalse();
+        assertThat(InputException.isNotProperLength(wrongLengthUserInput)).isTrue();
     }
     @Test
     @DisplayName("Check for exception when input string contains zero")
     public void containZero_Test(){
         String userInputContainZero="054";
-        InputException inputException = new InputException();
-
-        assertThat(inputException.containZero(userInputContainZero)).isTrue();
+        assertThat(InputException.containZero(userInputContainZero)).isTrue();
     }
     @Test
     @DisplayName("Check for excpetion when input string contains redundant number")
     public void isDuplicate_Test(){
         String duplicateInput="656";
-        InputException inputException = new InputException();
+        assertThat(InputException.isDuplicate(duplicateInput)).isTrue();
+    }
+    @Test
+    public void isWrongUserInput_Alphabet_Test(){
+        String wrongUserInputAlphabet="05A";
 
-        assertThat(inputException.isDuplicate(duplicateInput)).isTrue();
+        if(InputException.isNotProperLetter(wrongUserInputAlphabet)){
+            assertThatThrownBy(() -> { throw new IllegalArgumentException(); }).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+    @Test
+    public void isWrongUserInput_Symbol_Test(){
+        String wrongUserInputSymbol="05[";
+        if(InputException.isNotProperLetter(wrongUserInputSymbol)){
+            assertThatThrownBy(() -> { throw new IllegalArgumentException(); }).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+    @Test
+    public void isWrongUserInput_Length_Over_Three_Test(){
+        String wrongLengthUserInput="0545";
+        if(InputException.isNotProperLetter(wrongLengthUserInput)){
+            assertThatThrownBy(() -> { throw new IllegalArgumentException(); }).isInstanceOf(IllegalArgumentException.class);
+        }
+    }@Test
+    public void isWrongUserInput_Length_Under_Three_Test(){
+        String wrongLengthUserInput="05";
+        if(InputException.isNotProperLetter(wrongLengthUserInput)){
+            assertThatThrownBy(() -> { throw new IllegalArgumentException(); }).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+    @Test
+    public void isWrongUserInput_Duplicate_Test(){
+        String duplicateInput="455";
+        if(InputException.isNotProperLetter(duplicateInput)){
+            assertThatThrownBy(() -> { throw new IllegalArgumentException(); }).isInstanceOf(IllegalArgumentException.class);
+        }
     }
 }
