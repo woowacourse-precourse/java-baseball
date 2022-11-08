a3bd449f74bc3155ec31ad7ff0a24d181dad9492
JuSeong1130 <jsmoon1130@naver.com>
2022-11-08 19:18:28 +0900

    test(BaseballGameRule): Player 입력 값에 대한 유효성 테스트(서로다른 숫자,통과 테스트)
    서로다른 숫자 들어왔을떄 Exception 테스트, 유효성 통과 테스트

--- src/test/java/baseball/rule/BaseballGameRuleTest.java
+++ src/test/java/baseball/rule/BaseballGameRuleTest.java
@@ -1,6 +1,6 @@
 package baseball.rule;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import baseball.player.Computer;
 import baseball.player.Player;
@@ -13,37 +13,39 @@ class BaseballGameRuleTest {
 		Player player = new Player();
 		player.insertNumber("1");
 		Computer computer = Computer.createComputerForTest("123");
-		BaseballGameRule baseballGameRule = new BaseballGameRule(player,computer);
+		BaseballGameRule baseballGameRule = new BaseballGameRule(player, computer);
 		assertThrows(
 				IllegalArgumentException.class, baseballGameRule::isPlayerNumberValidate);
 	}
+
 	@Test
 	void 플레이어_숫자_유효성_1부터_9까지_Exception() {
 		Player player = new Player();
 		player.insertNumber("0");
 		Computer computer = Computer.createComputerForTest("123");
-		BaseballGameRule baseballGameRule = new BaseballGameRule(player,computer);
+		BaseballGameRule baseballGameRule = new BaseballGameRule(player, computer);
 		assertThrows(
 				IllegalArgumentException.class, baseballGameRule::isPlayerNumberValidate);
 	}
+
 	@Test
 	void 플레이어_숫자_유효성_서도다른숫자_Exception() {
 		Player player = new Player();
 		player.insertNumber("112");
 		Computer computer = Computer.createComputerForTest("123");
-		BaseballGameRule baseballGameRule = new BaseballGameRule(player,computer);
+		BaseballGameRule baseballGameRule = new BaseballGameRule(player, computer);
 		assertThrows(
 				IllegalArgumentException.class, baseballGameRule::isPlayerNumberValidate);
 	}
+
 	@Test
 	void 플레이어_숫자_유효성_체크_통과() {
 		Player player = new Player();
 		player.insertNumber("123");
 		Computer computer = Computer.createComputerForTest("123");
-		BaseballGameRule baseballGameRule = new BaseballGameRule(player,computer);
+		BaseballGameRule baseballGameRule = new BaseballGameRule(player, computer);
 		baseballGameRule.isPlayerNumberValidate();
 	}
-
 
 
 }
