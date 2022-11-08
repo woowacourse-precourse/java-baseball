f92b0b44f9aeb3e3503351fbde98169519de3a22
hongjinwon <herenever@naver.com>
2022-11-08 20:21:38 +0900

    refactor(controller): BaseballController 수정
    게임의 흐름을 관리하는 FlowController를 적용하는 코드 수정

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -9,38 +9,42 @@ import java.util.List;
 import static baseball.utils.GameMessage.*;
 
 public class BaseballController {
-    private final static String ANSWER_RESULT = "3스트라이크\n";
     private final InputView inputView;
     private final OutputView outputView;
     private final Computer computer;
+    private final FlowController flowController;
 
-    public BaseballController(InputView inputView, OutputView outputView, Computer computer) {
+    public BaseballController(InputView inputView, OutputView outputView, Computer computer,FlowController flowController) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.computer = computer;
+        this.flowController = flowController;
     }
 
     public void runGame() {
         outputView.showGameMessage(START);
-        boolean flag = true;
-        while (flag) {
+        while (flowController.getGameFlag()) {
+
             List<Integer> randomNumbers = computer.generateRandomNumbers();
+
             runSubGame(randomNumbers);
+
             outputView.showGameMessage(QUESTION);
-            if (!inputView.questionInput()) {
-                flag = false;
-            }
+
+            Boolean isAgain = inputView.questionInput();
+            flowController.setGameFlag(isAgain);
         }
     }
 
     public void runSubGame(List<Integer> randomNumbers) {
-        String countResult = "";
-        while (!countResult.equals(ANSWER_RESULT)) {
+        while (flowController.getSubGameFlag()) {
+
             outputView.showGameMessage(REQUEST);
+
             List<Integer> userNumbers = inputView.requestInput();
 
-            countResult = computer.generateCountResult(randomNumbers, userNumbers);
-            outputView.showGameMessage(countResult);
+            String countResult = computer.generateCountResult(randomNumbers, userNumbers);
+            flowController.setSubGameFlag(countResult);
         }
         outputView.showGameMessage(ANSWER);
     }
