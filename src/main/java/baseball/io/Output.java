cbfd713d6fc25ab4613666a8197745a384a5d8c8
yback1223 <yoyob1223@gmail.com>
2022-11-08 13:54:27 +0900

    feat: use GameGuide class, delete final variables

--- src/main/java/baseball/io/Output.java
+++ src/main/java/baseball/io/Output.java
@@ -4,15 +4,10 @@ import java.util.List;
 
 public class Output {
 
-    private static final String NOTHING = "낫싱";
-    private static final String BALL = "볼";
-    private static final String BALL_SPACE = "볼 ";
-    private static final String STRIKE = "스트라이크";
-
     public void printStrikeBall(List<Integer> strikeBall) {
         StringBuilder sb = new StringBuilder();
         if (strikeBall.get(0) == 0 && strikeBall.get(1) == 0) {
-            sb.append(NOTHING);
+            sb.append(OutputMessage.NOTHING_MESSAGE.getMessage());
         }
         exceptNothing(strikeBall, sb);
         System.out.println(sb);
@@ -20,11 +15,12 @@ public class Output {
 
     private static void exceptNothing(List<Integer> strikeBall, StringBuilder sb) {
         if (strikeBall.get(1) != 0 && strikeBall.get(0) == 0) {
-            sb.append(strikeBall.get(1)).append(BALL);
+            sb.append(strikeBall.get(1)).append(OutputMessage.BALL_MESSAGE.getMessage());
         } else if (strikeBall.get(1) == 0 && strikeBall.get(0) != 0) {
-            sb.append(strikeBall.get(0)).append(STRIKE);
+            sb.append(strikeBall.get(0)).append(OutputMessage.STRIKE_MESSAGE.getMessage());
         } else if (strikeBall.get(1) != 0 && strikeBall.get(0) != 0) {
-            sb.append(strikeBall.get(1)).append(BALL_SPACE).append(strikeBall.get(0)).append(STRIKE);
+            sb.append(strikeBall.get(1)).append(OutputMessage.BALL_SPACE_MESSAGE.getMessage()).append(strikeBall.get(0))
+                    .append(OutputMessage.STRIKE_MESSAGE.getMessage());
         }
     }
 }
