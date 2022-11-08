43644413e1ef9cff7ca2e6f788245b80f9b1d7da
ddingmin <ddingminyt@gmail.com>
2022-11-08 18:30:51 +0900

    refactor(InputException): IllegalArgumentException 호출 함수화

--- src/main/java/baseball/InputException.java
+++ src/main/java/baseball/InputException.java
@@ -3,28 +3,36 @@ package baseball;
 public class InputException {
     public static void checkThreeNumberException(String input) {
         if (input.length() != 3) {
-            throw new IllegalArgumentException("야구 게임의 입력은 이어진 3자리여야 합니다. [ex) 123]");
+            exceptionThreeNumberInput();
         }
         for (int i = 0; i < input.length(); i++) {
             if (!checkRange(input.charAt(i))){
-                throw new IllegalArgumentException("야구 게임의 입력은 1 ~ 9의 옳바른 범위여야 합니다. [ex) 123]");
+                exceptionThreeNumberInput();
             }
         }
         if (input.charAt(0) == input.charAt(1) || input.charAt(0) == input.charAt(2) || input.charAt(1) == input.charAt(2)) {
-            throw new IllegalArgumentException("야구 게임의 입력은 중복 없이 3자리여야 합니다. [ex) 123]");
+            exceptionThreeNumberInput();
         }
     }
 
     public static void checkRestartNumberException(String input) {
         if (input.length() != 1){
-            throw new IllegalArgumentException("1 또는 2를 입력해야 합니다.");
+            exceptionRestartNumberInput();
         }
         if (!(input.charAt(0) == '1' || input.charAt(0) == '2')){
-            throw new IllegalArgumentException("1 또는 2를 입력해야 합니다.");
+            exceptionRestartNumberInput();
         }
     }
 
     private static Boolean checkRange(char c){
         return ('1' <= c && c <= '9');
+    }
+
+    private static void exceptionThreeNumberInput(){
+        throw new IllegalArgumentException("옳바른 숫자를 입력해주세요. ex) 123");
+    }
+
+    private static void exceptionRestartNumberInput(){
+        throw new IllegalArgumentException("옳바른 숫자를 입력해주세요. ex) 1 or 2");
     }
 }
