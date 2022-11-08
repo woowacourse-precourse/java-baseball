dd22abad0a343813e6c8c435d4d74b9519dec074
“rotomoo” <quddlf410@naver.com>
2022-11-08 16:13:47 +0900

    feat(Player) : 숫자가 아닌 값이 들어올 경우 검사 추가

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,13 +1,26 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
 public class Player {
 
-    private List<Integer> inputNumbers() {
-        return new ArrayList<>();
+    public List<Integer> inputNumbers() {
+
+        List<Integer> playerInputList = new ArrayList<>();
+
+        String input = Console.readLine();
+
+        if (checkWrongInput(input)) throw new IllegalStateException("잘못된 입력입니다.");
+
+        for (char inputOfDigit : input.toCharArray()) {
+            playerInputList.add((int)inputOfDigit);
+        }
+
+        return playerInputList;
     }
 
     public boolean checkWrongInput(String input) {
@@ -15,6 +28,8 @@ public class Player {
 
         HashSet<String> duplicateCheckSet = new HashSet<>(List.of(input.split("")));
         if (duplicateCheckSet.size() != 3) return true;
+
+        for (char inputOfDigit : input.toCharArray()) if (!Character.isDigit(inputOfDigit)) return true;
 
         return false;
     }
