6c2cb1791bc3af7e037f9469391217311d902272
i960107 <i960107@naver.com>
2022-11-08 15:26:43 +0900

    feat(BaseballResult): add feature of computing game result by comparing two number list

--- src/main/java/baseball/BaseballResult.java
+++ src/main/java/baseball/BaseballResult.java
@@ -1,4 +1,30 @@
 package baseball;
 
 public class BaseballResult {
+    public BaseballScore getBaseballScore(BaseballNumber computerNumbers, BaseballNumber userNumbers) {
+        int strikes = 0;
+        int balls = 0;
+
+        for (int index = 0; index < BaseballNumber.NUMBER_COUNT; index++) {
+            int number = userNumbers.get(index);
+
+            if (is_strike(computerNumbers, index, number)) {
+                strikes += 1;
+                continue;
+            }
+
+            if (is_ball(computerNumbers, number)) {
+                balls += 1;
+            }
+        }
+        return new BaseballScore(strikes, balls);
+    }
+
+    private boolean is_strike(BaseballNumber computerNumbers, int index, int number) {
+        return computerNumbers.is_number_in_index_equal_to(index, number);
+    }
+
+    private boolean is_ball(BaseballNumber computerNumbers, int number) {
+        return computerNumbers.contains(number);
+    }
 }
