874db9f85171c5f711aa4a3e8d924b5cc511fc64
chosunghyun18 <krismon@naver.com>
2022-11-08 23:58:02 +0900

    feat(player): add tje player test class

---
+++ src/test/java/baseball/PlayerTest.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+    @Test
+    void getPlayerNumbersList() {
+    }
+
+    @Test
+    void inputNumbers() {
+    }
+}
