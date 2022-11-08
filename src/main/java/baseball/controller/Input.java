28ac0221adb50bae9eb7536139741fe7422dcc08
iamminho <minho100227@kakao.com>
2022-11-08 11:51:58 +0900

    refactor(input): String을 List<Integer>로 변환하는 기능 제거

--- src/main/java/baseball/controller/Input.java
+++ src/main/java/baseball/controller/Input.java
@@ -4,32 +4,22 @@ import baseball.exception.PlayerNumberException;
 import baseball.exception.RestartNumberException;
 import baseball.view.InputView;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class Input {
-	public static List<Integer> getPlayerNumbers() {
-		String inputPlayerNumber = getInput();
-		PlayerNumberException.validatePlayerNumber(inputPlayerNumber);
-		return stringToList(inputPlayerNumber);
+	private static String getInput() {
+		InputView.printInputNumber();
+		return Console.readLine();
 	}
 
-	private static List<Integer> stringToList(String inputPlayerNumber) {
-		String[] inputPlayerNumberArray = inputPlayerNumber.split("");
-		return Arrays.stream(inputPlayerNumberArray)
-			.map(playerNumber -> Integer.parseInt(playerNumber))
-			.collect(Collectors.toList());
+	public static String getPlayerNumber() {
+		String inputPlayerNumber = getInput();
+		PlayerNumberException.validatePlayerNumber(inputPlayerNumber);
+		return inputPlayerNumber;
 	}
 
 	public static int getRestartNumber(int restartNumber, int exitNumber) {
 		String inputRestartNumber = getInput();
 		RestartNumberException.validateRestartNumber(inputRestartNumber, restartNumber, exitNumber);
 		return Integer.parseInt(inputRestartNumber);
-	}
-
-	private static String getInput() {
-		InputView.printInputNumber();
-		return Console.readLine();
 	}
 }
