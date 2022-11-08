2278648550f2620659f8a6a275b5ef66416d674e
hwanee__ <fkfkthth@naver.com>
2022-11-08 18:42:46 +0900

    feat: 게임 재시작에 대한 멘트 & 입력 값 유효성 검사
    재시작 입력 값이 유효한지 검사

--- src/main/java/baseball/view/UserView.java
+++ src/main/java/baseball/view/UserView.java
@@ -21,6 +21,18 @@ public class UserView {
         return input;
     }
 
+    public String RestartAnswer(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String input = Console.readLine();
+
+        if(!validation.RestartAnswer(input)){
+            throw new IllegalArgumentException();
+        }
+
+        return input;
+    }
+
 
 
 }
