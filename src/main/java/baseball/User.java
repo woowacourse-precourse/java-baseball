ad7399d6e51cd54e2f9793b4fd0676c38035a760
kimsinhyun <ksh415622@naver.com>
2022-11-08 18:16:57 +0900

    fix: (User.java) valid 함수 수정
    for loop 안의 checkDuplicate.add(tempInput.charAt(i))
    부분의 true false 체크가 반대로 되어 있는 버그 수정
    이전 버전: checkDuplicate.add(tempInput.charAt(i))
    수정 버전: !checkDuplicate.add(tempInput.charAt(i))

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,9 +1,12 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
+
 import java.util.HashSet;
+
 import java.util.List;
+import java.util.Arrays;
+
 import java.util.stream.Stream;
 
 public class User {
@@ -13,24 +16,32 @@ public class User {
     protected int[] userInput;
     private static HashSet checkDuplicate;
 
-
     public void getUserInput() {
+
         String tempInput = Console.readLine();
+
+        // 유저가 입력한 값이 유효한지 확인
         if (valid(tempInput)) {
+            // 유효한 경우 Stream을 활용하여 String -> ArrayList<Integer>로 mapping
             userInput = Stream.of(
                     tempInput.split(""))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
+                .mapToInt(Integer::parseInt)
+                .toArray();
         } else {
             throw new IllegalArgumentException();
         }
     }
 
     public boolean valid(String tempInput) {
+
         checkDuplicate = new HashSet();
-        for (int i = 0; i < 3; ++i) {
+
+        if (tempInput.length() != NUM_LEN){
+            return false;
+        }
+        for (int i = 0; i < NUM_LEN; ++i) {
             if (!Character.isDigit(tempInput.charAt(i))
-                || checkDuplicate.add(tempInput.charAt(i))
+                || !checkDuplicate.add(tempInput.charAt(i))
                 || tempInput.charAt(i) == '0') {
                 return false;
             }
