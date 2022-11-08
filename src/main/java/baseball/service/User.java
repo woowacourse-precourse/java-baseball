c94b969e71c487a93da3f70337a51113877f9779
kksks3192 <kksks3192@gmail.com>
2022-11-08 23:20:50 +0900

    refactor(baseball): 사용자 입력 예외 발생 시 처리 기능 추가

--- src/main/java/baseball/service/User.java
+++ src/main/java/baseball/service/User.java
@@ -12,15 +12,24 @@ public class User {
     public static List<Integer> inputGuess() throws IOException {
         List<Integer> guess = new ArrayList<>();
         CheckException checkException = new CheckException();
-
         String[] inputArray = Console.readLine().split("");
-        checkException.validateInputLength(inputArray);
-        checkException.validateInputNum(inputArray);
-        checkException.validateInputDistinct(inputArray);
+        while(true) {
+            try {
+                checkException.validateInputLength(inputArray);
+                checkException.validateInputNum(inputArray);
+                checkException.validateInputDistinct(inputArray);
+
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println("숫자를 입력해주세요 : ");
+                inputArray = Console.readLine().split("");
+            }
+        }
 
         for(String i : inputArray){
             guess.add(Integer.parseInt(i));
         }
+
         return guess;
     }
 
