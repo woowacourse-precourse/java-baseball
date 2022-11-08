e8657c20203522f8680c063b4becdc0ecbb42640
Yebin <lyb2325@gmail.com>
2022-11-08 21:07:45 +0900

    feat: 사용자에게 숫자 입력 받기 기능 추가

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -8,24 +8,36 @@ public class BaseballGame {
     private static final String GAME_ENDS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String GET_USER_NEXT_COMMAND_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    private static final RandomNumberGenerator computer = new RandomNumberGenerator();
+
     BaseballGame(){
+        System.out.print(GAME_STARTS_MESSAGE); // 게임 시작 문구
+        computer.generate();
         startGame();
     }
 
     public static void startGame(){
-        System.out.print(GAME_STARTS_MESSAGE); // 게임 시작 문구
+        UserScore user = new UserScore();
 
-        // 랜덤 정수 배열 생성하기
-        RandomNumberGenerator randomNumbers = new RandomNumberGenerator();
-
-        // 숫자 입력 받기
-        System.out.print(GET_USER_INPUT_MESSAGE);
-        String userInput = Console.readLine();
-
-
-        // 숫자 맞히기
+        // 숫자 입력 받고 숫자 맞히기
 
         // 다음 command
 
+    }
+    private static void proceedGame(UserScore user){
+        String userInput;
+
+        // 숫자 입력 받으며 숫자 맞히기 게임 진행
+        while(true){
+            user.init();
+            System.out.print(GET_USER_INPUT_MESSAGE);
+            userInput = Console.readLine();
+            
+            // 사용자 입력에 따른 에러 처리
+            
+            // 볼, 스트라이크 개수 맞추기
+            
+            // 답에 대한 결과 보여주기
+        }
     }
 }
