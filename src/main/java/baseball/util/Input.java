712af18877d3a978fd0ae4d7b71fe00e56a86891
hum02 <ehdgma02@gmail.com>
2022-11-08 12:15:41 +0900

    feat($validation): 재시작/종료 시의 입력값 검증

--- src/main/java/baseball/util/Input.java
+++ src/main/java/baseball/util/Input.java
@@ -2,10 +2,24 @@ package baseball.util;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import static baseball.type.GameFlow.GAME_RESTART;
+import static baseball.type.GameFlow.GAME_END;
+
+
 public class Input {
-    public static String inputRestartFlag() {
-        return Console.readLine();
+    private static final String RESTART = Integer.toString(GAME_RESTART.getValue());
+    private static final String END = Integer.toString(GAME_END.getValue());
+
+    public static String inputRestartFlag() throws IllegalArgumentException {
+        String restartFlag = Console.readLine();
+        //validataion
+        if (!((restartFlag.equals(RESTART)) || (restartFlag.equals(END)))) {
+            throw new IllegalArgumentException("잘못된 입력값입니다. 1이나 2를 입력해주세요");
+        }
+
+        return restartFlag;
     }
+
     public static String inputPlayerNumber() {
         return Console.readLine();
     }
