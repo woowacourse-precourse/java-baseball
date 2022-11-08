2559235e4b219520808d554a76bf4b746b73e556
shin5774 <babo5774@naver.com>
2022-11-08 17:32:56 +0900

    feat(User): 사용자 입력 오류확인 추가기능
    사용자 입력 오류확인
    -입력값이 1~9까지의 범위인지 확인
    -중복체크를 Set을 이용하여 효율적으로 재구성

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,8 +1,13 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.Set;
+import java.util.HashSet;
 
 public class User {
+    static final char INPUT_LOWER_BOUND='1';
+    static final char INPUT_UPPER_BOUND='9';
+
     String value;
 
     User(){
@@ -14,20 +19,30 @@ public class User {
         checkInputException();
     }
 
-    boolean checkOverlap(){//set을 이용 && 1~9 check
+    boolean checkOverlapAndRange(){
+        Set<Character> overlap=new HashSet<Character>();
 
-        char first_num= value.charAt(0);
-        char second_num= value.charAt(1);
-        char third_num= value.charAt(2);
+        for(int idx=0;idx<3;idx++){
+            char digit=this.value.charAt(idx);
+            if(digit<INPUT_LOWER_BOUND||digit>INPUT_UPPER_BOUND){
+                return true;
+            }
+            else {
+                overlap.add(this.value.charAt(idx));
+            }
+        }
 
-        if(first_num==second_num||first_num==third_num||second_num==third_num){
+        if(overlap.size()<3){
             return true;
         }
-        return false;
+        else{
+            return false;
+        }
+
     }
 
     void checkInputException(){
-        if(value.length()!=3||checkOverlap()){
+        if(value.length()!=3|| checkOverlapAndRange()){
             IllegalArgumentException e=new IllegalArgumentException("입력 오류");
             throw e;
         }
