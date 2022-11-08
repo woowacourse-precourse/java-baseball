7d4437cc8b12d53758811adf74a964c6544bb21a
juno-junho <ssmm0205@naver.com>
2022-11-08 18:46:07 +0900

    feat(controller): create methods in BaseballGame
    BaseballGame 클래스 안 playTheGame, startGame, exit 메서드 생성

--- src/main/java/baseball/controller/BaseballGame.java
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -1,6 +1,18 @@
 package baseball.controller;
 
 public class BaseballGame {
+
     public void playTheGame() {
+        do {
+            startGame();
+        } while (!exit());
+    }
+
+    private static void startGame() {
+
+    }
+
+    private static boolean exit() {
+        return true;
     }
 }
