69ebd6f78246651b6b1cc1eb550615d894b7a4fe
juno-junho <ssmm0205@naver.com>
2022-11-08 18:42:45 +0900

    feat(controller): create BaseballGame class
    BaseballGame 클래스를 만들어 Application 클래스에서는 게임 실행만 하는 것로 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,10 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-import camp.nextstep.edu.missionutils.Randoms;
+import baseball.controller.BaseballGame;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        BaseballGame game = new BaseballGame();
+        game.playTheGame();
     }
 }
