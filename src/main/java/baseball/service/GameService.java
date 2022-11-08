1734a9cf0dcafe38269810b849692becb230f171
kiarakim <kiarakim413@gmail.com>
2022-11-08 22:38:17 +0900

    feat : keep printing game result until SUCCESS

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -2,13 +2,27 @@ package baseball.service;
 
 import baseball.domain.GameDomain;
 import baseball.util.RandomNumberMaker;
+import baseball.view.Respondent;
 
 public class GameService {
+    final int SUCCESS = 3;
     int size;
     GameDomain game;
+    Respondent respond = new Respondent();
 
     public void setGame(int size, int start, int end) {
         this.size = size;
         game = new GameDomain(RandomNumberMaker.getRandomNumbers(size, start, end));
     }
+
+    public void playGame() {
+        int strike = 0;
+        while (strike != SUCCESS) {
+            play();
+            respond.respondScoreResult(game.getBallScore(), game.getStrikeScore());
+            strike = game.getStrikeScore();
+        }
+    }
+
+    private void play() {}
 }
