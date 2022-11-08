efc25c772b2d0e506dd06e791f59f64549208557
Gibeom Nam <rlqja0523@naver.com>
2022-11-08 23:00:17 +0900

    feat: 게임 종료

--- src/main/java/baseball/game/GameService.java
+++ src/main/java/baseball/game/GameService.java
@@ -57,6 +57,11 @@ public class GameService {
         }
     }
 
+    public void over() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
     public enum Hint {
         STRIKE("스트라이크"),
         BALL("볼"),
