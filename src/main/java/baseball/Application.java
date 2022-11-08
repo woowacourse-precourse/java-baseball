afad2a9a42d637368780c9d5977d730719fa7240
Hongsugyeong <olivia258@naver.com>
2022-11-08 09:54:55 +0900

    (Feat) :  컴퓨터 랜덤 3자리 수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -29,5 +31,23 @@ class Array {
             }
         }
         return -1;
+    }
+}
+
+class Game {
+
+    int[] number = new int[3];
+
+    Game() {
+        int randomNum;
+        int numberIndex = 0;
+
+        while (numberIndex < 3) {
+            randomNum = Randoms.pickNumberInRange(1, 9);
+
+            if (!Array.isArrayContains(number, randomNum)) {
+                number[numberIndex++] = randomNum;
+            }
+        }
     }
 }
