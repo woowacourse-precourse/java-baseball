08304cfa94ea04798bd1649e7ecf9fe417518f42
Seoyoung <ryupark1006@gmail.com>
2022-11-08 16:08:14 +0900

    feat: 사용자 입력값 유효성 확인 기능 구현

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,5 +25,23 @@ public class Game {
     private void initResult() {
         strike = 0;
         ball = 0;
+    }
+
+    private void playGame() {
+        List<Integer> input = getInput();
+    }
+
+    private List<Integer> getInput() {
+        List<Integer> input = new ArrayList<>();
+        String inputString = Console.readLine();
+
+        for (char c : inputString.toCharArray()) {
+            input.add(Character.getNumericValue(c));
+        }
+
+        if (!Validator.validateInput(input)) {
+            throw new IllegalArgumentException();
+        }
+        return input;
     }
 }
