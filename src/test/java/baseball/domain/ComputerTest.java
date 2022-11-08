ddfbe7dfd61f4d0acc7ba5518a9920e86a8e7a99
Noah <shgustmd6865@naver.com>
2022-11-08 20:27:58 +0900

    Refacor(ComputerTest) : ComputerTest 테스트 코드 수정
    - computer.generateRandomNumber()를 통해 랜덤한 숫자를 생성해야 했으나, 생성자 호출을 통해 랜덤한 숫자를 유지하는 과정으로 변경되어
      Computer 인스턴스를 생성하고 가져다가 사용하는 것으로 변경

--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -3,8 +3,6 @@ package baseball.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("상대방(컴퓨터)의 랜덤한 숫자 생성하기")
@@ -13,28 +11,22 @@ class ComputerTest {
     @Test
     @DisplayName("숫자의 크기는 3으로 고정")
     public void validNumberSize() {
-//        Computer computer = new Computer();
-//        List<Integer> randomNumber = computer.generateRandomNumber();
-//
-//        assertThat(randomNumber.size()).isEqualTo(3);
+        Computer computer = new Computer();
+        assertThat(computer.getRandomNumbers().size()).isEqualTo(3);
     }
 
     @Test
     @DisplayName("각 자리수는 1~9까지의 숫자만 사용")
     public void randomNumberHasOnlyOneToNine() {
-//        Computer computer = new Computer();
-//        List<Integer> randomNumber = computer.generateRandomNumber();
-//
-//        assertThat(randomNumber).isNotIn(0).isNotIn(10);
+        Computer computer = new Computer();
+        assertThat(computer.getRandomNumbers()).isNotIn(0).isNotIn(10);
     }
 
     @Test
     @DisplayName("각 자리수는 각기 다른 숫자가 사용되어야 함")
     public void randomNumberElementsAllUnique() {
-//        Computer computer = new Computer();
-//        List<Integer> randomNumber = computer.getnerateRandomNumber();
-//
-//        assertThat(randomNumber).doesNotHaveDuplicates();
+        Computer computer = new Computer();
+        assertThat(computer.getRandomNumbers()).doesNotHaveDuplicates();
     }
 
 
