4e1841e30ad0d584e01b598273d92bcfb1516ef3
ohhonggi <5ghdrl@naver.com>
2022-11-08 16:37:51 +0900

    feat($Computer): Implement generateRandomNumber function
    This function randomly generates three natural numbers
     with different digits
    
    Added new event to $Computer
    - Store results generated by generateRandomNumber function
     in numbers variable
    
    Closes #feat_1

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,8 +1,25 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    private List<Integer> numbers;
+    private static List<Integer> numbers;
 
+    public Computer() {
+        numbers = generateRandomNumber();
+    }
+
+    public static List<Integer> generateRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
