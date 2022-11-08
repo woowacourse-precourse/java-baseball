755a32cdeb4d0ed1e5fcbd767d0256ed7773b437
sonsoowon <yuwoo1018@gmail.com>
2022-11-08 21:38:46 +0900

    test(PlayerTest): isValidInput을 구성하는 세가지 메서드에 대한 테스트 메서드 삭제
    - isInputSizeEqualToAnswerSize 테스트
    - doesNotHaveDuplicateInputElement 테스트
    - hasOnlyNaturalNumberAsInput 테스트

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -8,43 +8,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PlayerTest {
     @Test
-    void isInputSizeEqualToAnswerSize_입력의_자리수가_게임정답의_자리수와_동일한_경우_참() {
-
-        List<String> playerInput = List.of("123", "12");
-        List<Boolean> expectedOutput = List.of(true, false);
-
-        List<Boolean> realOutput = playerInput.stream()
-                .map(input -> Player.isInputSizeEqualToAnswerSize(input))
-                .collect(Collectors.toList());
-
-        assertThat(expectedOutput).isEqualTo(realOutput);
-    }
-
-    @Test
-    void doesNotHaveDuplicateInputElement_입력에_중복된_문자가_없는_경우_참() {
-        List<String> playerInput = List.of("123", "112");
-        List<Boolean> expectedOutput = List.of(true, false);
-
-        List<Boolean> realOutput = playerInput.stream()
-                .map(input -> Player.doesNotHaveDuplicateInputElement(input))
-                .collect(Collectors.toList());
-
-        assertThat(expectedOutput).isEqualTo(realOutput);
-    }
-
-    @Test
-    void hasOnlyNaturalNumberAsInput_입력이_자연수로만_구성된_경우_참() {
-        List<String> playerInput = List.of("123", "012", "1a3");
-        List<Boolean> expectedOutput = List.of(true, false, false);
-
-        List<Boolean> realOutput = playerInput.stream()
-                .map(input -> Player.hasOnlyNaturalNumberAsInput(input))
-                .collect(Collectors.toList());
-
-        assertThat(expectedOutput).isEqualTo(realOutput);
-    }
-
-    @Test
     void isValidInput_입력이_유효한_경우_참() {
         List<String> playerInput = List.of("012", "112", "12", "1234", "", "123");
         List<Boolean> expectedOutput = List.of(false, false, false, false, false, true);
