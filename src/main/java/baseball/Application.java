3533bef9488b4fe9060db4b5fef293cadbe09b51
core <jinjs6@gmail.com>
2022-11-08 16:42:54 +0900

    style: 가독성을 위해 코드 스타일 수정
    1차 공통 피드백에 따라 가독성 향상을 위해 코드 수정
    1. 메서드와 메서드 사이 줄바꿈 설정
    2. for, if, while등의 사이에 줄바꿈 공백 설정
    3. 변수 선언한 줄 다음에 줄바꿈 공백 추가
    
    전반적인 변경 내용
    변경 전: 메서드 및 분기문, 반복문 사이에 대한 줄바꿈 공백 부재
    변경 후: 메서드 및 분기문, 반복문 사이에 대한 줄바꿈 공백 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,99 +11,129 @@ public class Application {
     private static void noticeStartGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
     private static List<Integer> createComputerNumbers() {
         List<Integer> computerNumbers = new ArrayList<>();
+
         while (computerNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
+
             if (!computerNumbers.contains(randomNumber)) {
                 computerNumbers.add(randomNumber);
             }
         }
         return computerNumbers;
     }
+
     private static boolean isValidUserNumbers(String userNumbers) {
         if (userNumbers.length() != 3) {
             return false;
         }
+
         if (!('1' <= userNumbers.charAt(0) && userNumbers.charAt(0) <= '9') ||
                 !('1' <= userNumbers.charAt(1) && userNumbers.charAt(1) <= '9') ||
                 !('1' <= userNumbers.charAt(2) && userNumbers.charAt(2) <= '9')) {
             return false;
         }
+
         return userNumbers.charAt(0) != userNumbers.charAt(1) && userNumbers.charAt(0) != userNumbers.charAt(2) &&
                 userNumbers.charAt(1) != userNumbers.charAt(2);
     }
+
     private static String inputUserNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         String inputUserNumbers = Console.readLine();
+
         if (!isValidUserNumbers(inputUserNumbers)) {
             throw new IllegalArgumentException();
         }
+
         return inputUserNumbers;
     }
+
     private static int getBallNumber(List<Integer> computerNumbers, String userNumbers) {
         int ballNumber = 0;
+
         for (int index = 0; index < userNumbers.toCharArray().length; ++index) {
             if (userNumbers.toCharArray()[index] - '0' != computerNumbers.get(index) &&
                     computerNumbers.contains(userNumbers.toCharArray()[index] - '0')) {
                 ++ballNumber;
             }
         }
+
         return ballNumber;
     }
+
     private static int getStrikeNumber(List<Integer> computerNumbers, String userNumbers) {
         int strikeNumber = 0;
+
         for (int index = 0; index < userNumbers.length(); ++index) {
             if (computerNumbers.get(index) == userNumbers.toCharArray()[index] - '0') {
                 ++strikeNumber;
             }
         }
+
         return strikeNumber;
     }
+
     private static boolean printBallStrikeResult(int ballNumber, int strikeNumber) {
         if (strikeNumber == 3) {
             System.out.println(strikeNumber + "스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return false;
         }
+
         if (strikeNumber == 0 && ballNumber == 0) {
             System.out.println("낫싱");
             return true;
         }
+
         if (ballNumber == 0) {
             System.out.println(strikeNumber + "스트라이크");
             return true;
         }
+
         if (strikeNumber == 0) {
             System.out.println(ballNumber + "볼");
             return true;
         }
+
         System.out.println(ballNumber + "볼 " + strikeNumber + "스트라이크");
         return true;
     }
+
     private static boolean wantRestart() throws IllegalArgumentException {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int input = Integer.parseInt(Console.readLine());
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
         if (!(input == 1 || input == 2)) {
             throw new IllegalArgumentException();
         }
+
         if (input == 2) {
             return false;
         }
+
         return true;
     }
+
     public static void main(String[] args) {
-        noticeStartGame();
         boolean gameStart = true;
+
+        noticeStartGame();
+
         while (gameStart) {
             List<Integer> computerNumbers = createComputerNumbers();
             boolean isNotMatchComputerUser = true;
+
             while (isNotMatchComputerUser) {
                 String userNumbers = inputUserNumbers();
                 int ballNumber = getBallNumber(computerNumbers, userNumbers);
                 int strikeNumber = getStrikeNumber(computerNumbers, userNumbers);
                 isNotMatchComputerUser = printBallStrikeResult(ballNumber, strikeNumber);
             }
+
             gameStart = wantRestart();
         }
     }
