2633b0ec0358cec6945b73a49f52ee076fb7ba7f
kki4504 <rhkddlf9876@gmail.com>
2022-11-08 19:04:48 +0900

    test : 재시작 기능 테스트 코드 추가

--- src/test/java/baseball/validation/RestartTest.java
+++ src/test/java/baseball/validation/RestartTest.java
@@ -1,4 +1,30 @@
+package baseball.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class RestartTest {
-  
+    Restart restart = new Restart();
+    @Test
+    void 길이가_1초과() {
+        String input = "12";
+        Assertions.assertThat(restart.validate(input))
+                .isEqualTo(false);
+    }
+
+    @Test
+    void 길이가_1미만() {
+        String input = "";
+        Assertions.assertThat(restart.validate(input))
+                .isEqualTo(false);
+    }
+
+    @Test
+    void 값이_1또는_2가_아닌_경우() {
+        String input = "0";
+        Assertions.assertThat(restart.validate(input))
+                .isEqualTo(false);
+    }
 }
