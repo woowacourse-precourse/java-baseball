deec2fd19458bb5ba997c359ec5f748b9e5801ff
asas6978 <asas6978@naver.com>
2022-11-08 19:43:39 +0900

    refactor: 코드 위치 재정렬

--- src/main/java/baseball/CompareNumber.java
+++ src/main/java/baseball/CompareNumber.java
@@ -9,8 +9,7 @@ import static baseball.Score.STRIKE;
 public class CompareNumber {
     public static void compare(List<String> computerNumber, List<String> userInput, Map<Score, Integer> score) {
         for (int i = 0; i < userInput.size(); i++) {
-            if (userInput.get(i)
-                    .equals(computerNumber.get(i))) {
+            if (userInput.get(i).equals(computerNumber.get(i))) {
                 score.put(STRIKE, score.get(STRIKE) + 1);
             } else if (computerNumber.contains(userInput.get(i))) {
                 score.put(BALL, score.get(BALL) + 1);
