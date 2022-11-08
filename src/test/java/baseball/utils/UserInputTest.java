addae40bf80b9689cbc2e33d03d1e0c4d9b27f63
MinSungKim <timkim01@naver.com>
2022-11-08 10:35:12 +0900

    test: 사용자 입력값이 3자리인지 여부 판단 테스트 작성

--- src/test/java/baseball/utils/UserInputTest.java
+++ src/test/java/baseball/utils/UserInputTest.java
@@ -21,4 +21,17 @@ class UserInputTest {
 
         assertEquals("숫자만 입력해주세요.", exception.getMessage());
     }
+
+    @Test
+    void 사용자_입력값이_3자리인지_여부_판단(){
+        String userInput = "011";
+        InputStream in = new ByteArrayInputStream(userInput.getBytes());
+        System.setIn(in);
+
+        Throwable exception = assertThrows(RuntimeException.class, () -> {
+            UserInput.setUserInput();
+        });
+
+        assertEquals("3자리 숫자를 입력해주세요.", exception.getMessage());
+    }
 }
