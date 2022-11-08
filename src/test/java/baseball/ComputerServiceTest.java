1fd10aa5f7535f3e89cd7780d649592e1dc0f2f2
kang-seongbeom <56289258+kang-seongbeom@users.noreply.github.com>
2022-11-08 11:13:03 +0900

    test : 컴퓨터 난수 테스트
    컴퓨터의 난수가 올바른지, 올바르지 않는 경우에 예상되는 예외가 발생하는지 확인한다.;

---
+++ src/test/java/baseball/ComputerServiceTest.java
@@ -0,0 +1,101 @@
+package baseball;
+
+import baseball.serviceImpl.ComputerDigitGeneratorService;
+import baseball.serviceImpl.GameService;
+import baseball.util.Digits;
+import baseball.util.GameMessage;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class ComputerServiceTest extends NsTest {
+    private static GameService gameService;
+
+    @BeforeAll
+    public static void beforeAll(){
+        gameService = new GameService();
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+
+    @Test
+    public void 시작_메시지_테스트(){
+        String input = GameMessage.START_MASSAGE.getMassage();
+        OutputStream out = checkMessage(input);
+        gameService.sendMassage(GameMessage.START_MASSAGE);
+        assertThat(input).isEqualTo(out.toString());
+    }
+
+    @Test
+    public void 시작_메시지_예외_테스트(){
+        String input = GameMessage.START_MASSAGE.getMassage() + "exception";
+        OutputStream out = checkMessage(input);
+        gameService.sendMassage(GameMessage.START_MASSAGE);
+        Assertions.assertThrows(org.opentest4j.AssertionFailedError.class, ()->{
+            assertThat(input).isEqualTo(out.toString());
+        });
+    }
+
+    @Test
+    public void 컴퓨터_세자리_난수_테스트(){
+        List<Integer> computer = gameService.getComputerDigits(new ComputerDigitGeneratorService());
+        int size = computer.size();
+        assertThat(size).isEqualTo(3);
+    }
+
+    @Test
+    public void 컴퓨터_자리수_범위_예외_테스트(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            gameService.getComputerDigits(() -> "1234");
+        });
+    }
+
+    @Test
+    public void 컴퓨터_난수_중복_없음_테스트(){
+        List<Integer> computer = gameService.getComputerDigits(new ComputerDigitGeneratorService());
+        boolean isDuplicated = isDuplicateDigit(computer);
+        assertThat(isDuplicated).isEqualTo(false);
+    }
+
+    @Test
+    public void 컴퓨터_난수_중복_예외_테스트(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            gameService.getComputerDigits(() -> "113");
+        });
+    }
+
+    @Test
+    public void 허용되지_않는_숫자_예외_테스트(){
+        Assertions.assertThrows(IllegalArgumentException.class, ()->{
+            gameService.getComputerDigits(() -> "017");
+        });
+    }
+
+    private boolean isDuplicateDigit(List<Integer> digits){
+        int[] visited = new int[Digits.LAST.getDigit()+1];
+        for (Integer digit : digits) {
+            if(visited[digit] == 1) return true;
+            visited[digit]++;
+        }
+        return false;
+    }
+
+    private OutputStream checkMessage(String input){
+        input = GameMessage.START_MASSAGE.getMassage();
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        return out;
+    }
+}
