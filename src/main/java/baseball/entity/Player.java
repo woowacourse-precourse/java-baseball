5e83141fb85d75444901f6354dc894a77f8ff159
이지영 <jioung93@naver.com>
2022-11-08 22:45:54 +0900

    [Player] feat: 입력받은 문자열을 숫자 리스트로 변환하는 기능 구현

--- src/main/java/baseball/entity/Player.java
+++ src/main/java/baseball/entity/Player.java
@@ -6,14 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
-    // List<Integer> playerNumbers: 사용자의 숫자 저장
     private static List<Integer> playerNumbers = new ArrayList<>();
 
     public void enterInput() {
         String input = Console.readLine();
         // changeInputToNumbers(input);
     }
-    // changeInputToNumber(): 입력된 숫자를 리스트로 변환
+
+    private void changeInputToNumber(String input) {
+        for (int i=0; i<input.length(); i++) {
+            playerNumbers.add(input.charAt(i)-'0');
+        }
+    }
+
     // validateNumber(): 입력받은 숫자에 대한 유효성 검증
     // validateAnswer(): 입력받은 재시작 여부 답변에 대한 유효성 검증
 }
