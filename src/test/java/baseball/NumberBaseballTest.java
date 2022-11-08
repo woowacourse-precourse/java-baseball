74cc9f45a3495f2641391efa2a84b5695078572b
iphuck22 <eldeshue@naver.com>
2022-11-08 19:37:55 +0900

    test($NumberBaseballTest.java): add a new test for 'checkInput' method

--- src/test/java/baseball/NumberBaseballTest.java
+++ src/test/java/baseball/NumberBaseballTest.java
@@ -8,11 +8,17 @@ import static org.assertj.core.api.Assertions.*;
 class NumberBaseballTest {
 
     @Test
-    public void checkAnswerIsNotSameNumber(){
+    public void testAnswerIsNotSameNumber(){
         NumberBaseball test1 = new NumberBaseball();
         assertThat(test1.getAnswer().get(0))
                 .isNotEqualTo(test1.getAnswer().get(1))
                 .isNotEqualTo(test1.getAnswer().get(2));
+    }
+
+    @Test
+    public void testCheckInput(){
+        NumberBaseball test2 = new NumberBaseball();
+        assertThat(test2.checkInput("359")).isEqualTo(true);
     }
 
 }
