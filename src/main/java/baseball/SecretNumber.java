b3abf7f93a2353f08c34ed21fa935408b7ac0760
moonn6pence <shin421179@gmail.com>
2022-11-08 16:19:51 +0900

    feat : SecretNumber 클래스
    - generateSecretDigits() : 세자리 난수 생성
    - getSecretDigits() : 생성한 난수 List 반환

--- src/main/java/baseball/SecretNumber.java
+++ src/main/java/baseball/SecretNumber.java
@@ -1,4 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class SecretNumber {
+    private List<Integer> secretDigits;
+
+    public SecretNumber(){
+        secretDigits = new ArrayList<>();
+    }
+
+    public void generateSecretDigits() {
+        while (secretDigits.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!secretDigits.contains(randomNumber)) {
+                secretDigits.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getSecretDigits() {
+        return secretDigits;
+    }
 }
