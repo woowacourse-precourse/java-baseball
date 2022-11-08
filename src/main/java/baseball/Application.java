cd997163a9fe013cb37911b894fcae8d049b3379
Anthologia <pyoungjy@gmail.com>
2022-11-08 23:42:54 +0900

    refactor: DecimalNumber, BaseballNumber 분리에 따른 main 메서드 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -155,40 +155,34 @@ class DecimalNumber {
 
 public class Application {
 
-    private static final int MAX_DIGIT = 3;
     public static void main(String[] args) {
         printGameStartMessage();
 
-        while (true) {
-            List<Integer> computerNumber = getComputerNumber();
-            System.out.println(computerNumber);
-
-            while (true) {
-                System.out.print("숫자를 입력해주세요 : ");
-                String userBaseballNumberInput = Console.readLine();
-                System.out.println(userBaseballNumberInput);
-                validateBaseballNumber(userBaseballNumberInput);
-
-                List<Integer> userNumber = splitStrNumberToList(userBaseballNumberInput);
-
-
-                HashMap<BaseballResult, Integer> gameResultMap = new HashMap<>();
-                for (int i = 0; i < userNumber.size(); i++) {
-                    BaseballResult currentIdxGameResult = getCurrentIdxGameResult(i, userNumber, computerNumber);
-                    gameResultMap.put(currentIdxGameResult, gameResultMap.getOrDefault(currentIdxGameResult, 0) + 1);
-                }
-                System.out.println(getGameResult(gameResultMap));
-
-                if (gameResultMap.getOrDefault(BaseballResult.STRIKE, 0) == MAX_DIGIT) {
-                    System.out.printf("%d개의 숫자를 모두 맞히셨습니다! 게임 종료\n", MAX_DIGIT);
-                    break;
-                }
-            }
+        int gameRestartUserSelection;
+        do {
+            runBaseballGame();
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            int gameRestartUserSelection = Integer.parseInt(Console.readLine());
+            gameRestartUserSelection = Integer.parseInt(Console.readLine());
+        } while (isRestartGame(gameRestartUserSelection));
+    }
 
-            if (!isRestartGame(gameRestartUserSelection)) {
+    private static void runBaseballGame() {
+        BaseballNumber computerNumber = BaseballNumber.makeRandomBaseballNumber();
+
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
+            BaseballNumber userNumber = new BaseballNumber(Console.readLine());
+
+            HashMap<BaseballResult, Integer> gameResultMap = new HashMap<>();
+            for (int i = 0; i < BaseballNumber.MAX_DIGIT_CRITERIA; i++) {
+                BaseballResult currentIdxGameResult = userNumber.getIndexOfGameResult(i, computerNumber);
+                gameResultMap.put(currentIdxGameResult, gameResultMap.getOrDefault(currentIdxGameResult, 0) + 1);
+            }
+            System.out.println(getBaseballGameResult(gameResultMap));
+
+            if (gameResultMap.getOrDefault(BaseballResult.STRIKE, 0) == BaseballNumber.MAX_DIGIT_CRITERIA) {
+                System.out.printf("%d개의 숫자를 모두 맞히셨습니다! 게임 종료\n", BaseballNumber.MAX_DIGIT_CRITERIA);
                 break;
             }
         }
@@ -198,48 +192,7 @@ public class Application {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    private static List<Integer> getComputerNumber() {
-        List<Integer> computerNumber = new ArrayList<>();
-
-        while (computerNumber.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-
-            if (!computerNumber.contains(randomNumber)) {
-                computerNumber.add(randomNumber);
-            }
-        }
-        return computerNumber;
-    }
-
-    public static List<Integer> splitStrNumberToList(String strNumber) {
-        List<Integer> userNumberList = new ArrayList<>();
-
-        userNumberList.add(Character.getNumericValue(strNumber.charAt(0)));
-        userNumberList.add(Character.getNumericValue(strNumber.charAt(1)));
-        userNumberList.add(Character.getNumericValue(strNumber.charAt(2)));
-
-        return userNumberList;
-    }
-
-    public static BaseballResult getCurrentIdxGameResult(int currentIdx, List<Integer> userNumber, List<Integer> computerNumber) {
-        if (isEqualInts(userNumber.get(currentIdx), computerNumber.get(currentIdx))) {
-            return BaseballResult.STRIKE;
-        }
-
-        for (int i = 0; i < userNumber.size(); i++) {
-            if (isEqualInts(userNumber.get(currentIdx), computerNumber.get(i))) {
-                return BaseballResult.BALL;
-            }
-        }
-
-        return BaseballResult.NOTHING;
-    }
-
-    private static boolean isEqualInts(int number, int comparingNumber) {
-        return number == comparingNumber;
-    }
-
-    private static String getGameResult(HashMap<BaseballResult, Integer> gameResult) {
+    private static String getBaseballGameResult(HashMap<BaseballResult, Integer> gameResult) {
         StringBuilder sb = new StringBuilder();
 
         Optional<Integer> nothingCnt = Optional.ofNullable(gameResult.get(BaseballResult.NOTHING));
@@ -260,22 +213,5 @@ public class Application {
     private static boolean isRestartGame(int userSelect) {
         return userSelect == 1;
     }
-
-    private static void validateBaseballNumber(String baseballNumber) throws IllegalArgumentException {
-        if (baseballNumber.length() != 3) {
-            throw new IllegalArgumentException("올바른 3자리의 수를 입력해주세요.");
-        }
-        validateDuplicateBaseballNumber(baseballNumber);
-    }
-
-    public static void validateDuplicateBaseballNumber(String baseballNumber) throws IllegalArgumentException {
-        int[] duplicateNumbers = new int[10];
-        for (int i = 0; i < MAX_DIGIT; i++) {
-            int number = baseballNumber.charAt(i) - '0';
-            if (duplicateNumbers[number] >= 1) {
-                throw new IllegalArgumentException("중복되지 않은 3자리의 수를 입력해주세요.");
-            }
-            ++duplicateNumbers[number];
-        }
-    }
 }
+
