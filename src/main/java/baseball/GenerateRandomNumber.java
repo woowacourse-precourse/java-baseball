94c602f7278f465bf5b5456bb64a3a9723fd476e
Nugur1i <duckhee092@naver.com>
2022-11-08 21:29:08 +0900

    refactor(GenerateRandomNumber): refactoring generatng method

--- src/main/java/baseball/GenerateRandomNumber.java
+++ src/main/java/baseball/GenerateRandomNumber.java
@@ -1,13 +1,21 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class GenerateRandomNumber {
+    private static final int NUMBER = 3;
+    private static final int MAXNumber = 9;
+    private static final int MINNumber = 1;
+    private int[] eachdigit;
+    public int[] getDigits() {
+        return this.eachdigit;
+    }
 
-    public List<Integer> NumberCreate(){
+    public void setRandomNumber() {
         List<Integer> computerNumber = new ArrayList<>();
         while (computerNumber.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -15,7 +23,18 @@ public class GenerateRandomNumber {
                 computerNumber.add(randomNumber);
             }
         }
-        return computerNumber;
+
+        this.eachdigit = convertListToIntArray(computerNumber);
     }
 
+    private int[] convertListToIntArray (List<Integer> list) {
+        return list.stream()
+                .mapToInt(i -> i)
+                .toArray();
+    }
+
+
+
 }
+
+
