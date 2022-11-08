3c4178fb4545d53bac63b88c5e0fda2fa9adf379
sun007021 <sun007021@gmail.com>
2022-11-08 12:31:36 +0900

    feat(Domain : BaseballNumbers) : 3자리수 아니면 예외처리

--- src/main/java/baseball/domain/BaseballNumbers.java
+++ src/main/java/baseball/domain/BaseballNumbers.java
@@ -8,18 +8,26 @@ import java.util.Set;
 public class BaseballNumbers {
     public static final int BASEBALL_NUMBERS_LENGTH = 3;
     private static final String INPUT_BASEBALL_NUMBER_DUPLICATION_EXCEPTION_MESSAGE = "중복되지 않는 숫자를 입력해주세요.";
-    private static final String INPUT_BASEBALL_NUMBERS_LENGTH_EXCEPTION_MESSAGE = BASEBALL_NUMBERS_LENGTH+"자리의 숫자만 입력할 수 있습니다.";
+    private static final String INPUT_BASEBALL_NUMBERS_LENGTH_EXCEPTION_MESSAGE = BASEBALL_NUMBERS_LENGTH + "자리의 숫자만 입력할 수 있습니다.";
 
     private final List<BaseballNumber> baseballNumberList;
 
-    public BaseballNumbers(List<BaseballNumber> baseballNumberList){
+    public BaseballNumbers(List<BaseballNumber> baseballNumberList) {
+        validateBaseballNumberDuplication(baseballNumberList);
+        validateBaseballNumbersLength(baseballNumberList);
         this.baseballNumberList = new ArrayList<>(baseballNumberList);
     }
 
-    private void validateBaseballNumberDuplication(List<BaseballNumber> baseballNumberList){
+    private void validateBaseballNumberDuplication(List<BaseballNumber> baseballNumberList) {
         Set<BaseballNumber> baseballNumberSet = new HashSet<>(baseballNumberList);
-        if(baseballNumberSet.size() < BASEBALL_NUMBERS_LENGTH){// 중복이 존재하는 경우
+        if (baseballNumberSet.size() < BASEBALL_NUMBERS_LENGTH) {// 중복이 존재하는 경우
             throw new IllegalArgumentException(INPUT_BASEBALL_NUMBER_DUPLICATION_EXCEPTION_MESSAGE);
+        }
+    }
+
+    private void validateBaseballNumbersLength(List<BaseballNumber> baseballNumberList) {
+        if (baseballNumberList.size() != BASEBALL_NUMBERS_LENGTH) {
+            throw new IllegalArgumentException(INPUT_BASEBALL_NUMBERS_LENGTH_EXCEPTION_MESSAGE);
         }
     }
 }
