e4b1e8adef265010040e0bbb614cf175583ff4b7
himjjong <jjijji5728@tukorea.ac.kr>
2022-11-08 20:28:52 +0900

    fix : 3스트라이크 후 재시작시 게임 시작을 알리는 문구가 출력되는 문제 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,6 @@ import baseball.controller.PlayGameController;
 
 public class Application {
     public static void main(String[] args) {
-        PlayGameController.run();
+        PlayGameController.startGame();
     }
 }
--- src/main/java/baseball/controller/PlayGameController.java
+++ src/main/java/baseball/controller/PlayGameController.java
@@ -4,8 +4,12 @@ import baseball.service.GameService;
 import baseball.service.NeedForGameService;
 
 public class PlayGameController {
-    public static void run() {
+    public static void startGame() {
         NeedForGameService.callStartOrder();
+        run();
+    }
+
+    public static void run() {
         GameService.makeRandomBall();
         GameService.catchRandomBall();
     }
