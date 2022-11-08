76aecc242285b3bff4e1d90d8f8c95f538c087f0
footdev <ekclstkfka5@gmail.com>
2022-11-08 17:54:42 +0900

    test: ThreeNumberTest 테스트 코드 추가
    서로 다른 세자리수 인지 테스트하는 케이스 추가

--- src/test/java/baseball/service/ThreeNumberTest.java
+++ src/test/java/baseball/service/ThreeNumberTest.java
@@ -1,12 +1,30 @@
 package baseball.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ThreeNumberTest {
 
+    @DisplayName("서로 다른 세자리 수인지 테스트")
     @Test
     void generate() {
+        //given
+        ThreeNumber number = new ThreeNumber();
+        Set<Integer> dup = new HashSet<>();
+        //when
+        number.generate();
+        //then
+        for (int num : number.getNumber()) {
+            assertThat(dup.contains(num)).isFalse();
+            dup.add(num);
+        }
     }
 }
