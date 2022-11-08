576c96636d5921f8183fd080b47db7e882179fa8
choiejae <ejae8319@gmail.com>
2022-11-08 19:51:12 +0900

    test: Java 파일 수정으로 인한 test 수정

--- src/test/java/baseball/ConsoleOutputTest.java
+++ src/test/java/baseball/ConsoleOutputTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class GameResultPrinterTest {
+class ConsoleOutputTest {
     private GameResultConverter converter;
 
     @BeforeEach
@@ -31,25 +31,25 @@ class GameResultPrinterTest {
     @DisplayName("Ball 확인")
     @Test
     void BallResultPrinter() {
-        assertEquals("1 볼", converter.convertGameResult(new ArrayList<>(Arrays.asList(0, 1, 0))));
-        assertEquals("2 볼", converter.convertGameResult(new ArrayList<>(Arrays.asList(0, 2, 0))));
-        assertEquals("3 볼", converter.convertGameResult(new ArrayList<>(Arrays.asList(0, 3, 0))));
+        assertEquals("1볼", converter.convertGameResult(new ArrayList<>(Arrays.asList(0, 1, 0))));
+        assertEquals("2볼", converter.convertGameResult(new ArrayList<>(Arrays.asList(0, 2, 0))));
+        assertEquals("3볼", converter.convertGameResult(new ArrayList<>(Arrays.asList(0, 3, 0))));
     }
 
     @Order(3)
     @DisplayName("Strike 확인")
     @Test
     void StrikeResultPrinter() {
-        assertEquals("1 스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(1, 0, 0))));
-        assertEquals("2 스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(2, 0, 0))));
-        assertEquals("3 스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(3, 0, 0))));
+        assertEquals("1스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(1, 0, 0))));
+        assertEquals("2스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(2, 0, 0))));
+        assertEquals("3스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(3, 0, 0))));
     }
 
     @Order(4)
     @DisplayName("Ball + Strike 확인")
     @Test
     void BallStrikeResultPrinter() {
-        assertEquals("1 볼 1 스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(1, 1, 0))));
-        assertEquals("2 볼 1 스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(1, 2, 0))));
+        assertEquals("1볼 1스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(1, 1, 0))));
+        assertEquals("2볼 1스트라이크", converter.convertGameResult(new ArrayList<>(Arrays.asList(1, 2, 0))));
     }
 }
