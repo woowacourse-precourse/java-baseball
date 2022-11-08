eb798acd77f35805825bd162538a440dce0bc79f
ddukddaki <rlfrkdms1@naver.com>
2022-11-08 17:49:50 +0900

    feat(PlayTest): 재시작 여부를 물어볼 때 1,2외의 답변으로 인한 예외 확인 테스트

---
+++ src/test/java/baseball/PlayTest.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+import baseball.player.Computer;
+import baseball.player.User;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+class PlayTest extends NsTest {
+
+    Play play = new Play(new User(new Validation()), new Computer(), new Hint());
+
+    @Test
+    void 재시작_예외_확인() {
+        //given
+        run("3");
+        //when
+        Throwable throwable = catchThrowable(() -> play.isRestart());
+        //then
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
