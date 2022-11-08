52a8c34689a2f6a294fcaf0e24fc30cbf8db09c1
No BongJun <sewd1224@gmail.com>
2022-11-08 20:12:32 +0900

    [Feat] <Application> Management Number Baseball Game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,11 +1,31 @@
 package baseball;
 
 import java.io.IOException;
+import java.util.List;
+
 public class Application {
 
     public static void main(String[] args) throws IOException, IllegalArgumentException{
         // TODO: 프로그램 구현
 
+        ComputerNum computer = new ComputerNum();
+        Init_game init = new Init_game();
+        Compare compare = new Compare();
+        Retry retry = new Retry();
 
+        boolean again = true;
+
+        while(again) {
+            List<Integer> com_num = computer.ComputerNumber();
+
+            String result = "";
+
+            while(!result.equals("3스트라이크")) {
+                result = compare.Classification(com_num, init.player_num());
+                System.out.println(result);
+            }
+
+            again = retry.retry();
+        }
     }
 }
