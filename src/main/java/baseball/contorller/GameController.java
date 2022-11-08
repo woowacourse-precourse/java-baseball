cb388ce5c1374412a22ce181d5e3dc98405b255b
Daeeun Ko <7273322@naver.com>
2022-11-08 22:18:44 +0900

    feat: get user input

--- src/main/java/baseball/contorller/GameController.java
+++ src/main/java/baseball/contorller/GameController.java
@@ -2,7 +2,6 @@ package baseball.contorller;
 
 import baseball.domain.Ball;
 import baseball.domain.Config;
-import baseball.domain.Flag;
 import baseball.service.GameService;
 import baseball.view.Views;
 import camp.nextstep.edu.missionutils.Console;
@@ -12,6 +11,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class GameController {
+
+    private static final int NUMBER_TYPE = 9;
 
     private final GameService service;
 
@@ -23,11 +24,31 @@ public class GameController {
         service.startGame();
         Views.INITIAL_VIEW.getView().view();
 
-            checkHint();
+        checkHint();
     }
 
     public void checkHint() {
         Views.INPUT_VIEW.getView().view();
+        String input = Console.readLine();
+        validateInput(input);
+
+        List<Ball> balls = Arrays.stream(input.split(""))
+                .mapToInt(Integer::parseInt)
+                .mapToObj(Ball::fromNumber)
+                .collect(Collectors.toList());
+    }
+
+    private void validateInput(String input) {
+        if (!isNumber(input) || isOutOfBounds(input)) throw new IllegalArgumentException();
+    }
+
+    private boolean isNumber(String string) {
+        return string.chars()
+                .allMatch(c -> Character.getType(c) == NUMBER_TYPE);
+    }
+
+    private boolean isOutOfBounds(String string) {
+        return string.length() != Config.MAX_BALLS_SIZE;
     }
     }
 }
