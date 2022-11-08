2cebca7ca5671d0983dc69122dd8fb3e9cebb609
dlgosla <dlgoslaek1@gmail.com>
2022-11-08 18:22:57 +0900

    feat: 두 리스트를 비교해 전체 스코어를 계산하는 메소드 추가

--- src/main/java/baseball/Score.java
+++ src/main/java/baseball/Score.java
@@ -19,7 +19,66 @@ public class Score {
         this.maxScore = maxScore;
     }
 
-    
+    private matchingTypes decideMatchingTypeBetween(int idxInUser, int idxInComputer) {
+        if (idxInComputer == -1) {
+            return matchingTypes.FAIL;
+        } else {
+            if (idxInComputer == idxInUser) {
+                return matchingTypes.STRIKE;
+            } else {
+                return matchingTypes.BALL;
+            }
+        }
+    }
+
+    private void clearAllScore() {
+        this.strike = 0;
+        this.fail = 0;
+        this.ball = 0;
+    }
+
+    public void updateTotalScore(List<Integer> computer, List<Integer> user) {
+        System.out.println(computer);
+        System.out.println(user);
+
+        clearAllScore();
+
+        for (int i = 0; i < user.size(); i++) {
+            int idxInComputer = computer.indexOf(user.get(i));
+
+            // strike, fail, ball 중에 해당하는 유형을 찾아 해당 유형의 점수를 1 증가
+            matchingTypes matchingType = decideMatchingTypeBetween(i, idxInComputer);
+            increase(matchingType);
+        }
+    }
+
+    public void print() {
+        if (isAllStrike()) {
+            System.out.printf("%d스트라이크\n", strike);
+        } else if (isAllFail()) {
+            System.out.println("낫싱");
+        } else {
+            System.out.printf("%d볼 %d스트라이크\n", this.ball, this.strike);
+        }
+    }
+
+    public boolean isAllStrike() {
+        if (this.strike == this.maxScore) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isAllFail() {
+        if (this.fail == this.maxScore) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
     private void increase(matchingTypes type) {
         if (type == matchingTypes.FAIL) {
             increaseFail();
