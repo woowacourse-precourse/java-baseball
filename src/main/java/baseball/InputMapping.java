c6e453dece026b82df86a51d07eb366dacd85503
wonho-seo <sjdlrnen0507@naver.com>
2022-11-08 16:54:14 +0900

    refactor(baseball.InputMapping.java): refactor InputMapping
    내부적으로 readLine을 진행하여 List<Integer> 반환으로 변경
    
    이 클래스 내부에서 readLine을 진행, 공백 이나 숫자가 아니며 숫자이나 int형 범위를 넘어간 경우 IllegalArgumentException을 던짐 기존 nonValidateGameNumber 함수를 유지.

--- src/main/java/baseball/InputMapping.java
+++ src/main/java/baseball/InputMapping.java
@@ -1,28 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class InputMapping {
-    public List<Integer> get(int inputNumber){
-        if (nonValidateInput(inputNumber))
-            throw new IllegalArgumentException("nonValidateInputNumber");
-        List<Integer> getInputNumberList = new ArrayList<>();
-        while (inputNumber > 0){
-            getInputNumberList.add(inputNumber % 10);
-            inputNumber /= 10;
-        }
-        return getInputNumberList;
+    public List<Integer> getGameNumberList(){
+        return new ArrayList<>();
     }
 
-    private boolean nonValidateInput(int inputNumber){
+    public int getConsoleInt(){
+        try {
+            String line = Console.readLine();
+            if (line.isBlank() || !line.matches("\\d+"))
+                throw new IllegalArgumentException();
+            return Integer.parseInt(line);
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("out of Int");
+        }
+    }
+
+    private boolean nonValidateGameNumber(int inputNumber){
         if (inputNumber < 111 || inputNumber > 999)
             return true;
         while (inputNumber > 0){
-            if (inputNumber % 10 == 0)
+            if (inputNumber % 10 == 0) {
                 return true;
+            }
             inputNumber /= 10;
         }
-        return false;
-    }
+        return false;    }
 }
