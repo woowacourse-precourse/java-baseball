75f3279e60dd94b75305aa65ad0943a3652f47ec
ddingmin <ddingminyt@gmail.com>
2022-11-08 17:12:19 +0900

    fix(InputException): 3자리 입력에서 숫자의 범위 예외처리 [1 ~ 9만 입력받도록] 구현

--- src/main/java/baseball/InputException.java
+++ src/main/java/baseball/InputException.java
@@ -5,6 +5,11 @@ public class InputException {
         if (input.length() != 3) {
             throw new IllegalArgumentException("야구 게임의 입력은 이어진 3자리여야 합니다. [ex) 123]");
         }
+        for (int i = 0; i < input.length(); i++) {
+            if (!checkRange(input.charAt(i))){
+                throw new IllegalArgumentException("야구 게임의 입력은 1 ~ 9의 옳바른 범위여야 합니다. [ex) 123]");
+            }
+        }
         if (input.charAt(0) == input.charAt(1) || input.charAt(0) == input.charAt(2) || input.charAt(1) == input.charAt(2)) {
             throw new IllegalArgumentException("야구 게임의 입력은 중복 없이 3자리여야 합니다. [ex) 123]");
         }
@@ -17,5 +22,9 @@ public class InputException {
         if (!(input.charAt(0) == '1' || input.charAt(0) == '2')){
             throw new IllegalArgumentException("1 또는 2를 입력해야 합니다.");
         }
+    }
+
+    private static Boolean checkRange(char c){
+        return ('1' <= c && c <= '9');
     }
 }
