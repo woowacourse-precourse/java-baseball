afa8c517ab94b2bf00e2f8106acf1f0f3cea6e3b
yoon <beatus96@naver.com>
2022-11-08 09:59:58 +0900

    feat(model) : reload

--- src/main/java/baseball/GameModel.java
+++ src/main/java/baseball/GameModel.java
@@ -1,2 +1,40 @@
-package baseball;public class GameModel {
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static baseball.GameRule.DIGITS;
+import static baseball.GameRule.RANDOM_MIN;
+import static baseball.GameRule.RANDOM_MAX;
+
+public class GameModel {
+    public static List<Integer> createComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < DIGITS) {
+            int randomNumber = Randoms.pickNumberInRange(RANDOM_MIN, RANDOM_MAX);
+            if (!computerNumber.contains(randomNumber)) {
+                computerNumber.add(randomNumber);
+            }
+        }
+        return computerNumber;
+    }
+
+    public static List<Integer> createPlayerNumber() throws IllegalArgumentException {
+        String userInput = View.getUserInput(); // 숫자가 아니면 예외 발생
+        List<Integer> playerNumber = stringToList(userInput);
+        if(!Validator.isValidNumber(playerNumber)){
+            throw new IllegalArgumentException();
+        }
+        return playerNumber;
+    }
+
+    public static List<Integer> stringToList(String input) {
+        return input.chars()
+                .map(i -> i - '0')
+                .boxed()
+                .collect(Collectors.toList());
+    }
 }
