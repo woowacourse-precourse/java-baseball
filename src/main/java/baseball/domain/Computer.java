30735d0602473cc6a49d19b6324d2792cb5c7e2a
“rotomoo” <quddlf410@naver.com>
2022-11-08 18:31:46 +0900

    feat(Computer) : 게임 종료 시 기능 수정

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,11 +1,29 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
-    private void continueOrQuit() {
+    public void continueOrQuit() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String checkContinue = Console.readLine();
+        if (checkContinue == "1") {
+            playGame();
+        }
+        else if (checkContinue == "2") {
+            System.out.println("게임 종료");
+        }
+        else {
+            System.out.println("잘못된 입력값입니다.");
+        }
+    }
+
+    public void playGame() {
+
     }
 
     public List<Integer> selectNumbers() {
@@ -22,7 +40,15 @@ public class Computer {
     }
 
     public String printResult(List<Integer> strikeBallList) {
-        return strikeBallList.get(0) + "볼 " + strikeBallList.get(1) + "스트라이크";
+        String result = "";
+        Player player = new Player();
+        if (strikeBallList.get(0) != 0) {
+            result += strikeBallList.get(0) + "볼 ";
+        }
+        if (strikeBallList.get(1) != 0) {
+            result += strikeBallList.get(1) + "스트라이크";
+        }
+        return result;
     }
 
     public List<Integer> getBallStrikeList(int sameNumbersCnt, int numberOfDigitCnt) {
