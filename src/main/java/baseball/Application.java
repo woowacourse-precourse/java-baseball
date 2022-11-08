a74b4abbfc023c9e4434efbc6bece204faf96380
heohyeonjink <hyeonjin4870@naver.com>
2022-11-08 20:58:29 +0900

    feat(getUserRestartEndNumber) : 재시작/종료 구분하는 값 입력받기
    - 1 입력하면 재시작 (true)
    - 2 입력하면 종료 (false)
    - 나머지는 IllegalArgumentException

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -78,4 +78,16 @@ public class Application {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
+
+    public static boolean getUserRestartEndNumber(int num){
+
+        if(num==1){
+            return true;
+        }
+        else if(num==2)
+            return false;
+        else{
+            throw new IllegalArgumentException();
+        }
+    }
 }
