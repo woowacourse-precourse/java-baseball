19114d33cd3de146fb3e52412d655f9c8da4f81e
kimdoha <kkhhjj3393@gmail.com>
2022-11-08 13:41:50 +0900

    feat(BaseballGame): 게임 지속 여부를 결정하는 클래스 생성

---
+++ src/main/java/baseball/game/GameContinuity.java
@@ -0,0 +1,13 @@
+package baseball.game;
+
+import baseball.utils.Input;
+
+public class GameContinuity {
+
+  public final Integer RESTART = 1;
+  public final Integer CLOSE = 2;
+
+  private Boolean continuity = false;
+
+
+}
