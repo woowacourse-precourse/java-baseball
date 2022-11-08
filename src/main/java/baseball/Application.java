140a1643cdc7cb619c296d4ae2f602146c8a45b5
KongDuHwan <kong_du@naver.com>
2022-11-08 18:12:40 +0900

    구현 할 함수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,60 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    public static List<Integer> computer = new ArrayList<>();
+    public static List<Integer> user = new ArrayList<>();
+    public static boolean continueGame;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        baseballGame();
     }
+
+    static void baseballGame() {
+    }
+
+    static void playGame() {
+    }
+
+    static boolean playNewGame() {
+    }
+
+    static void gameStart() {
+    }
+
+    static void setComputer() {
+    }
+
+    static void readUser() {
+    }
+
+    static String[] getUser() {
+    }
+
+    static void setUser(String str[]) throws IllegalArgumentException {
+    }
+
+    static boolean compareBall() {
+    }
+
+    static boolean isAllStrike() {
+    }
+
+    static boolean isNothing() {
+    }
+
+    static int getBall() {
+    }
+
+    static int getStrike() {
+    }
+
+    static void printBallStrike(int ball, int strike) {
+    }
+
 }
