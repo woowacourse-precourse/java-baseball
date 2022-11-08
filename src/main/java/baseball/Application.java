2e2c4059d27899ac6eb62a759e4cf38ba35d7fe7
aran <aran0317@naver.com>
2022-11-08 13:28:21 +0900

    Feat: 야구게임 로직 구현
    반복문 구현 및 finish() 함수 구현 완료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class Application {
+    static void game(){
+
+    }
+    static boolean finish(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        try {
+            InputStreamReader inputStreamReader = new InputStreamReader(System.in);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+            int restart = Integer.parseInt(bufferedReader.readLine());
+            if(restart == 1) return false;
+            else if(restart == 2) return true;
+        }catch(Exception e) {}
+        return true;
+    }
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        do{
+            game();
+        }while(!finish());
     }
 }
