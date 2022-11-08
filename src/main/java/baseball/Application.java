41837263aa6dd29977b7098c080082f6f9fec89f
pasly0920 <inwoo920621@gmail.com>
2022-11-08 15:00:58 +0900

    feat: game 함수 main에 반영

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import static baseball.Game.*;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        gameStart();
     }
 }
