e945de76e87b212f4a473cece412f6efadb4f2a5
jijongkwon <help3451@naver.com>
2022-11-08 17:02:18 +0900

    sytle : 예외 출력 문장 수정

--- src/main/java/baseball/BaseballComputer.java
+++ src/main/java/baseball/BaseballComputer.java
@@ -39,13 +39,13 @@ public class BaseballComputer {
     }
     private void checkOutOfInputNumber(String answer){
         if(answer.length() != 3){
-            throw new IllegalArgumentException("out of input number");
+            throw new IllegalArgumentException("OutOfBoundsException. you input 3 number");
         }
     }
 
     private int checkOutOfChoiceNumber(int choice){
         if(1 > choice || choice > 2){
-            throw new IllegalArgumentException("out of choice number");
+            throw new IllegalArgumentException("OutOfBoundsException. you input 1 or 2");
         }
         return choice;
     }
