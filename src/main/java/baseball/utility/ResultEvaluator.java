37c501e9a35a3b0cf51e0dfa3f1a089b470604b8
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-08 18:27:19 +0900

    feat(feature): 점수 문자열 변환 기능 구현
    ResultEvaluator의 translateScoreToHint() 메서드를 구현
    - Score 타입의 사용자 점수를 인자로 받아서 이를 일정 형식의 힌트 문자열로 변환함.

--- src/main/java/baseball/utility/ResultEvaluator.java
+++ src/main/java/baseball/utility/ResultEvaluator.java
@@ -3,6 +3,9 @@ package baseball.utility;
 import baseball.dto.Score;
 
 public class ResultEvaluator {
+    private static final String TEXT_FOR_NO_SCORE = "낫싱";
+    private static final String TEXT_FOR_BALL = "볼";
+    private static final String TEXT_FOR_STRIKE = "스트라이크";
 
     private ResultEvaluator() {
 
@@ -42,5 +45,28 @@ public class ResultEvaluator {
             return 0;
         }
         return 1;
+    }
+
+    public static String translateScoreToHint(Score score) {
+        int numBalls = score.getBallCount();
+        int numStrikes = score.getStrikeCount();
+
+        if (numBalls == 0 && numStrikes == 0) {
+            return TEXT_FOR_NO_SCORE;
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if (numBalls != 0) {
+            String ballText = String.format("%d%s ", numBalls, TEXT_FOR_BALL);
+            stringBuilder.append(ballText);
+        }
+
+        if (numStrikes != 0) {
+            String strikeText = String.format("%d%s ", numStrikes, TEXT_FOR_STRIKE);
+            stringBuilder.append(strikeText);
+        }
+
+        return stringBuilder.toString().trim();
     }
 }
