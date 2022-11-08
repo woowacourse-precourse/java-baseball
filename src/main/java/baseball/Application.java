d93b3844889948434ddd611d32a657b66c4c3a7a
kwang <shinkwang@yonsei.ac.kr>
2022-11-08 22:56:12 +0900

    refactor: Move methods about game in Application class to Game class
    Application이 가지고 있는 game 관련 메소드들을 Game 클래스로 옮긴다. game과 관련된 메소드들은 Game 클래스가 담당함으로써 응집도를 높힌다. 뿐만 아니라 Application의 코드는 매우 간결해진다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,6 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,47 +9,10 @@ public class Application {
 
         do {
             List<Integer> computerNumberList = Game.createComputerNumberList();
-            tryOnceForAnswer(computerNumberList);
-
-        } while (tryAgainGame());
+            Game.tryOnceForAnswer(computerNumberList);
+        } while (Game.tryAgainGame());
         PrintLine.endGame();
     }
 
-    private static void tryOnceForAnswer(List<Integer> computerNumberList) {
-        while (true) {
-            Map<String, Integer> scoreTable = Game.createScoreTable();
-            List<Integer> userNumberList = inputUserNumber();
 
-            Game.matchLists(userNumberList, computerNumberList, scoreTable);
-            PrintLine.resultPrint(scoreTable);
-
-            if (scoreTable.get("스트라이크") == 3) {
-                PrintLine.afterAnswerPrint();
-                break;
-            }
-        }
-    }
-
-    private static boolean tryAgainGame() {
-        PrintLine.putRestartNumberPrint();
-        String restartNumber = Console.readLine();
-        Validation.validateRestartNumber(restartNumber);
-
-        if (restartNumber.equals("1")) {
-            return true;
-        }
-        return false;
-    }
-
-    private static List<Integer> inputUserNumber() {
-        PrintLine.putUserNumberPrint();
-        String userNumber = Console.readLine();
-        Validation.validateUserNumber(userNumber);
-        return stringToIntegerList(userNumber);
-    }
-
-    private static List<Integer> stringToIntegerList(String numbers) {
-        String[] arr = numbers.split("");
-        return Arrays.stream(arr).map(Integer::parseInt).collect(Collectors.toList());
-    }
 }
