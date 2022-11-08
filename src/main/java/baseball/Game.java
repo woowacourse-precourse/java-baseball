07c53c8fd59c7132da6d79b165b812e1d040b5ae
geonho <soo_1992@naver.com>
2022-11-08 23:44:04 +0900

    feat: 맞힌 숫자 계산 결과값 출력

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -84,5 +84,25 @@ public class Game {
         }
     }
 
+    public boolean printResultOfGame() {
+        String result = "";
+        if (ball == 0 && strike == 0) {
+            result = "낫싱";
+        } else if(ball !=0 && strike !=0) {
+            result = ball + "볼 " + strike + "스트라이크";
+        } else if (ball == 0) {
+            result = strike + "스트라이크";
+        } else {
+            result = ball + "볼";
+        }
+        System.out.println(result);
+
+        if (strike == 3) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 
 }
