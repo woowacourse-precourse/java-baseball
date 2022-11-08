f8614ed9d815c0c429ff299ccc1708dd0fc93ab0
dingding-21 <phj8221@naver.com>
2022-11-08 16:23:58 +0900

    feat: 게임 중 사용자 입력 받은 후 검증하는 기능 추가

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,10 +1,16 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Player {
+    private static final int FIXED_NUMBER_LENGTH = 3;
+
     public List<Integer> getComputerRandomNumber() {
         List<Integer> computer = new ArrayList<>();
 
@@ -20,5 +26,31 @@ public class Player {
         if (!computer.contains(randomNumber)) {
             computer.add(randomNumber);
         }
+    }
+
+    public List<Integer> enterNumber() {
+        String input = Console.readLine();
+        checkNumberValidation(input);
+
+        return toInts(input);
+    }
+
+    private void checkNumberValidation(String number) {
+        List<Integer> numbers = toInts(number);
+
+        InputValidation.checkNumberLength(number, FIXED_NUMBER_LENGTH);
+
+        for (Integer num : numbers) {
+            InputValidation.checkNumberRange(num);
+            InputValidation.checkDuplicateNumber(numbers, num);
+        }
+    }
+
+    private List<Integer> toInts(String number) {
+        List<String> numbers = Arrays.asList(number.split(""));
+
+        return numbers.stream()
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
     }
 }
