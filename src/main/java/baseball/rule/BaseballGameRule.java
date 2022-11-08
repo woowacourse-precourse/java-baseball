bd91d6baf594931789b0176a39db750ff021de77
JuSeong1130 <jsmoon1130@naver.com>
2022-11-08 19:04:54 +0900

    feat(BaseballGameRule): Player 입력 값에 대한 Rule(유효성) 체크
    Validate 길이 3 체크

---
+++ src/main/java/baseball/rule/BaseballGameRule.java
@@ -0,0 +1,36 @@
+package baseball.rule;
+
+import baseball.player.Computer;
+import baseball.player.Player;
+
+public class BaseballGameRule {
+
+	private Player player;
+	private Computer computer;
+
+	private String playerNumber;
+
+	private String computerNumber;
+
+
+	private static final int NUMBER_LENGTH = 3;
+
+	public BaseballGameRule(Player player, Computer computer) {
+		this.player = player;
+		this.computer = computer;
+		computerNumber = computer.getNumber();
+	}
+
+	public void isPlayerNumberValidate() {
+		playerNumber = player.getNumber();
+		if (!isLength3()) {
+			throw new IllegalArgumentException("올바른 값을 입력하세요(서로 다른 숫자 3개");
+		}
+	}
+
+	private boolean isLength3() {
+		return playerNumber.length() == NUMBER_LENGTH;
+	}
+
+
+}
