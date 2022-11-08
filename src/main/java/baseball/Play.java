b34c58775149ddde701250e2d169a20911f33acd
yeada <yeadalm@gmail.com>
2022-11-08 10:08:51 +0900

    refactor : 볼, 스트라이크, 낫싱 판단 메서드 나누기

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -1,10 +1,9 @@
 package baseball;
-
+import java.util.List;
 import java.util.ArrayList;
 
 public class Play {
-
-    public static void playGame(ArrayList<Integer> computer){
+    public static void playGame(List<Integer> computer){
         int strikeCounter = 0;
 
         do{
@@ -13,22 +12,24 @@ public class Play {
             int ballCounter = Game.ball(user, computer);
             boolean nothingTrue = Game.nothing(strikeCounter, ballCounter);
 
-            if(ballCounter!=0 && strikeCounter!=0) {
-                System.out.println(ballCounter+"볼 "+ strikeCounter+"스트라이크");
-            }
-
-            if(ballCounter==0 && strikeCounter!=0) {
-                System.out.println(strikeCounter+"스트라이크");
-            }
-
-            if(ballCounter!=0 && strikeCounter==0) {
-                System.out.println(ballCounter+"볼");
-            }
-
-            if(nothingTrue) System.out.println("낫싱");
+            printResult(ballCounter, strikeCounter, nothingTrue);
 
         } while(strikeCounter != 3);
 
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public static void printResult(int ballCounter, int strikeCounter, boolean nothingTrue){
+        if(ballCounter!=0 && strikeCounter!=0) {
+            System.out.println(ballCounter+"볼 "+ strikeCounter+"스트라이크");
+        }
+        if(ballCounter==0 && strikeCounter!=0) {
+            System.out.println(strikeCounter+"스트라이크");
+        }
+        if(ballCounter!=0 && strikeCounter==0) {
+            System.out.println(ballCounter+"볼");
+        }
+        if(nothingTrue) System.out.println("낫싱");
+    }
+
 }
