f6f0880cad4a483c70c4463ccf410b4a27eb0c11
xxeol2 <jhj01177@naver.com>
2022-11-08 17:29:57 +0900

    test: view 테스트 개선

--- src/test/java/baseball/ViewTest.java
+++ src/test/java/baseball/ViewTest.java
@@ -1,6 +1,10 @@
 package baseball;
 
+import static baseball.constant.Finish.EXIT;
+import static baseball.constant.Finish.RESTART;
+import static baseball.constant.Finish.getFinishByCode;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
@@ -13,7 +17,7 @@ public class ViewTest {
 
     @BeforeEach
     public void beforeEach() {
-        view = new View(new InputControllerImp());
+        view = new ConsoleView(new InputControllerImp());
     }
 
     @Test
@@ -47,6 +51,30 @@ public class ViewTest {
 
         assertHint(hint1, "1볼 1스트라이크");
         assertHint(hint2, "2볼 1스트라이크");
+    }
+
+
+    @Test
+    void 재시작여부입력은_1혹은_2여야함() {
+        // given
+        String userInput1 = "1";
+        String userInput2 = "2";
+        String userInput3 = "3";
+        String userInput4 = "12";
+        String userInput5 = "0";
+        String userInput6 = "01";
+
+        assertThat(getFinishByCode(userInput1)).isEqualTo(RESTART);
+        assertThat(getFinishByCode(userInput2)).isEqualTo(EXIT);
+        assertException(userInput3);
+        assertException(userInput4);
+        assertException(userInput5);
+        assertException(userInput6);
+    }
+
+    private void assertException(String userInput) {
+        assertThatThrownBy(() -> getFinishByCode(userInput))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     private Method testConvertHint() {
