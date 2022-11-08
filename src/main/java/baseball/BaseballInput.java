3c7c5adf4e455182328d759cd0c1667e9f7c6027
i960107 <i960107@naver.com>
2022-11-08 15:23:39 +0900

    feat(BaseballInput): add feature of getting user number and getting user play code

--- src/main/java/baseball/BaseballInput.java
+++ src/main/java/baseball/BaseballInput.java
@@ -4,8 +4,25 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class BaseballInput {
 
-    public String getUserInput() {
-        return Console.readLine();
+    public static BaseballNumber getUserNumber() {
+        BaseballOutput.printInputNumberMessage();
+        BaseballNumber baseballNumber = new BaseballNumber(Console.readLine());
+        return baseballNumber;
+    }
+
+    public static Integer getPlayCode() throws IllegalArgumentException {
+        BaseballOutput.printInputPlayCodeMessage();
+        Integer playCode = null;
+        try {
+            while (playCode == null
+                    || !(playCode == Application.PLAY_AGAIN_CODE
+                    || playCode == Application.EXIT_CODE)) {
+                playCode = Integer.parseInt(Console.readLine());
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+        return playCode;
     }
 
 
