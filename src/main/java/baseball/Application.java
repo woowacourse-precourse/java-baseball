959b03240c8e0cf8516ec095e8c0d35c64cc1aa3
sypark <soun997@gmail.com>
2022-11-08 16:58:06 +0900

    refactor: remove unused or duplicated variables, clear lines and blanks, add comment

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Pattern;
 
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
@@ -16,10 +15,10 @@ public class Application {
 
         List<Integer> computer = new ArrayList<>();
 
-        for (int i = 0; i < 3; i++){
-            
+        for (int i = 0; i < 3; i++) {
+
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)){
+            if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
@@ -30,7 +29,7 @@ public class Application {
     /*
      * 사용자가 세자리의 숫자를 입력, 각 자리값을 리스트에 담아 반환
      */
-    public static List<Integer> usersChoice(){
+    public static List<Integer> usersChoice() {
 
         String input = Console.readLine();
         System.out.println(input);
@@ -38,7 +37,7 @@ public class Application {
 
         List<Integer> user = new ArrayList<>();
 
-        for (int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
             user.add(Character.getNumericValue(input.charAt(i)));
         }
 
@@ -48,8 +47,9 @@ public class Application {
     /*
      * 사용자 입력 예외를 처리함
      */
-    public static void inputExceptionHandler(String input){
-        if (input.length() > 3){
+    public static void inputExceptionHandler(String input) {
+
+        if (input.length() > 3) {
             throw new IllegalArgumentException("you can't enter more than three digits");
         }
     }
@@ -57,41 +57,36 @@ public class Application {
     /*
      * 컴퓨터가 고른 숫자와 사용자가 입력한 숫자를 비교하여 결과를 반환
      */
-    public static String getGameResult(List<Integer> computer, List<Integer> user){
+    public static String getGameResult(List<Integer> computer, List<Integer> user) {
 
         int ball = 0;
         int strike = 0;
-        boolean isBall = false;
 
-        for(int i = 0; i < 3; i++){
+        for (int i = 0; i < 3; i++) {
 
             // 볼 혹은 스트라이크일 경우
-            if (computer.contains(user.get(i))){
-                isBall = true;
+            if (computer.contains(user.get(i))) {
                 ball++;
             }
-
             // 스트라이크일 경우
-            if (isBall && computer.get(i).equals(user.get(i))){
+            if (computer.get(i).equals(user.get(i))) {
                 ball--;
                 strike++;
             }
-
-            isBall = false;
         }
 
         String result = "";
-        
-        if (ball == 0 && strike == 0){
+
+        if (ball == 0 && strike == 0) {
             result += "낫싱";
             return result;
         }
 
-        if (ball > 0){
+        if (ball > 0) {
             result += ball + "볼 ";
         }
 
-        if (strike > 0){
+        if (strike > 0) {
             result += strike + "스트라이크";
         }
 
@@ -99,49 +94,45 @@ public class Application {
     }
 
     /*
-     * 게임 종료 후 숫자 야구 게임 재시작 여부 반환
+     * 게임 종료 후 숫자 야구 게임 종료 여부 반환
      */
-    public static boolean restartOrQuitGame(){
+    public static boolean restartOrQuitGame() {
 
         String input = Console.readLine();
         System.out.println(input);
 
-        if (Integer.valueOf(input).equals(1)){
-            return false;
-        }
-        else if (Integer.valueOf(input).equals(2)){
-            return true;
-        }
-        else{
-            throw new IllegalArgumentException("you must choose between 1 and 2");
+        if (Integer.valueOf(input).equals(1)) {
+            return false;   // 게임 재시작
+        } else if (Integer.valueOf(input).equals(2)) {
+            return true;    // 게임 종료
+        } else {
+            throw new IllegalArgumentException("you must choose between 1 and 2");  // 예외
         }
     }
 
     public static void main(String[] args) {
 
-        List<Integer> computer = new ArrayList<>();
+        List<Integer> computer;
+        List<Integer> user;
         List<String> results = new ArrayList<>();
-        List<Integer> user = new ArrayList<>();
         boolean quit = false;
 
         computer = computersChoice();
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-        while(true){
+        while (!quit) {
 
             System.out.print("숫자를 입력해주세요 : ");
             user = usersChoice();
             results.add(getGameResult(computer, user));
             System.out.println(results.get(results.size() - 1));    // 숫자 판별 결과 출력
 
-            if (results.get(results.size() - 1).equals("3스트라이크")){
+            // 결과가 3스트라이크라면 게임종료, 재시작 혹은 애플리케이션 종료 중 하나 선택
+            if (results.get(results.size() - 1).equals("3스트라이크")) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 computer = computersChoice();
                 quit = restartOrQuitGame();    // 게임을 재시작할지 종료할지 여부 결정
-            }
-            if (quit){
-                break;
             }
         }
     }
