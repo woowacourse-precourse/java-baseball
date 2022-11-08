60c4aa35440da54cb97cde07d1662b32786a8e45
kwondaehwi <kwondh8561@ajou.ac.kr>
2022-11-08 16:04:06 +0900

    feat: User 클래스 isValidInput 메서드 구현 isThreeDigitNumber() - 정규표현식을 통해 1~9 3자리 숫자인지 확인 isDistinctInput() - 중복 숫자 없는지 확인 isValidInput() - 1~9 사이 3개의 중복 없는 숫자인지 확인

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -5,16 +5,20 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class User {
+    private static final Integer MAX_BASEBALLLIST_SIZE = 3;
     private BaseballList baseballList;
 
     public void setBaseballList() {
-        String temp = Console.readLine();
+        String input = Console.readLine();
+        // 유저 input validation - 3자리인지, 숫자인지, 중복된 수 없는지
+        isValidInput(input);
         this.baseballList = new BaseballList(
-                Arrays.stream(temp.split(""))
+                Arrays.stream(input.split(""))
                         .map(Integer::parseInt)
                         .collect(Collectors.toList())
         );
@@ -25,5 +29,20 @@ public class User {
 
     public BaseballList getUserBaseballList() {
         return this.baseballList;
+    }
+
+    private void isValidInput(String input) {
+        isThreeDigitNumber(input);
+        isDistinctInput(input);
+    }
+    private void isDistinctInput(String input) {
+        if(Arrays.stream(input.split("")).distinct().count() != MAX_BASEBALLLIST_SIZE)
+            throw new IllegalArgumentException();
+    }
+
+    private void isThreeDigitNumber(String input) {
+        String pattern = "[1-9]{3}";
+        if(!Pattern.matches(pattern, input))
+            throw new IllegalArgumentException();
     }
 }
