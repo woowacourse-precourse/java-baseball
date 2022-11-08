4e26c5c4890192d600c5af8c7f117b3ddf14a7e9
yoon <beatus96@naver.com>
2022-11-08 10:00:22 +0900

    feat(validator) : reload

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,2 +1,41 @@
-package baseball;public class Validator {
+package baseball;
+
+import java.util.List;
+import java.util.Set;
+
+import static baseball.GameRule.DIGITS;
+import static baseball.GameRule.RANDOM_MIN;
+import static baseball.GameRule.RANDOM_MAX;
+import static baseball.GameRule.COMMAND_RESTART;
+import static baseball.GameRule.COMMAND_END;
+public class Validator {
+    public static boolean isNumber(String input) {
+        return input.chars()
+                .allMatch(Character::isDigit);
+    }
+
+    public static boolean isValidNumber(List<Integer> playerNumber) {
+        return isCorrectSize(playerNumber) &&
+                isInRange(playerNumber) &&
+                isNotDuplicated(playerNumber);
+    }
+    //i. 자릿수 체크
+    public static boolean isCorrectSize(List<Integer> playerNumber) {
+        return playerNumber.size() == DIGITS;
+    }
+    //ii. 숫자 범위 체크
+    public static boolean isInRange(List<Integer> playerNumber) {
+        return playerNumber.stream()
+                .allMatch(v ->
+                        RANDOM_MIN<= v && v <= RANDOM_MAX);
+    }
+    // iii. 숫자 중복 체크
+    public static boolean isNotDuplicated(List<Integer> playerNumber) {
+        return Set.copyOf(playerNumber).size() == DIGITS;
+    }
+
+
+    public static boolean isValidCommandNumber(String input) {
+        return input.equals(COMMAND_RESTART) || input.trim().equals(COMMAND_END);
+    }
 }
