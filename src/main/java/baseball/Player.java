0118e57b4e4c8a6a7cea82f5e5140ca0c2ab57fa
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-08 20:40:20 +0900

    refactor : 상대방 랜덤 숫자 중복 제거, key값 제거하기

--- src/main/java/baseball/Player.java
+++ src/main/java/baseball/Player.java
@@ -1,32 +1,27 @@
 package baseball;
 
 import static baseball.Constant.MAX_INDEX;
-import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Player {
 
+    private static int keyIndex;
     // 플레이어 기능 : 상대방(컴퓨터)의 수를 생성
     public static Map<Integer, Integer> createComputerNumber() {
 
-        Map<Integer, Integer> computerRepository = new HashMap<>(MAX_INDEX);
+        Map<Integer, Integer> computerRepository = new HashMap<>();
 
+        while (computerRepository.size() < MAX_INDEX) {
 
-        for ( int keyIndex = 0; keyIndex < MAX_INDEX; keyIndex++ ) {
-            int randomNumber = pickNumberInRange(0, 9);
+            int randomNumber = Randoms.pickNumberInRange(1,9);
 
-            if ( !computerRepository.containsValue(randomNumber) ) {
-                int randomRecursion = pickNumberInRange(0, 9);
-                computerRepository.put(keyIndex, randomRecursion);
+            if (!computerRepository.containsValue(randomNumber)) {
+                computerRepository.put(keyIndex, randomNumber);
+                keyIndex++;
             }
-
-            if ( computerRepository.containsValue(randomNumber) ) {
-                int randomRecursion = pickNumberInRange(0, 9);
-                computerRepository.put(keyIndex, randomRecursion);
-            }
-
         }
 
         return computerRepository;
