83aefe0510433eaaeaea543f699ff9aca911f8c2
saehim <saeilee.vxv@gmail.com>
2022-11-08 22:15:15 +0900

    Feat: 게임 컨트롤 코드 변수

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -1,2 +1,10 @@
-package baseball.controller;public class Game {
+public class Game {
+    private static int newGameCode = 1;
+    private static int shutDownCode = 2;
+    public static int getNewGameCode() {
+        return newGameCode;
+    }
+    public static int getShutDownCode() {
+        return shutDownCode;
+    }
 }
