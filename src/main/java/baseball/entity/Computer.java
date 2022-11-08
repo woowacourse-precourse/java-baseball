90c885f3dc178a22e87fd99bce040e2d9369036f
Gibeom Nam <rlqja0523@naver.com>
2022-11-08 22:41:13 +0900

    feat: computer entity 생성

---
+++ src/main/java/baseball/entity/Computer.java
@@ -0,0 +1,9 @@
+package baseball.entity;
+
+import baseball.number.NumberHandler;
+
+import java.util.List;
+
+public class Computer {
+    private final List<Integer> computerNumberList = NumberHandler.createRandomNumberList();
+}
