7ced8f296f3d73080019902056aacc7f190918b4
chj <bluedog129@gmail.com>
2022-11-08 22:18:03 +0900

    feat : 유저의 현재 점수를 반환하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -72,4 +72,10 @@ public class Application {
         }
         return strike;
     }
+    public static String currentSituation(Integer ball , Integer strike){
+        if(ball == 0 && strike == 0) return "낫싱";
+        else if(ball == 0) return strike + "스트라이크";
+        else if(strike == 0) return ball + "볼";
+        else return ball + "볼" + " " + strike + "스트라이크";
+    }
 }
