8e99da2e8ad774c5cbc7c106643f08cf1ccae6be
This2sho <zx00018@naver.com>
2022-11-08 18:43:06 +0900

    feat(Constants): 스트라이크, 볼, 낫싱을 나타내는 상수 추가

--- src/main/java/baseball/domain/Constants.java
+++ src/main/java/baseball/domain/Constants.java
@@ -1,7 +1,12 @@
 package baseball.domain;
 
 public class Constants {
+
 	public static final int MAX_NUMBER = 9;
 	public static final int MIN_NUMBER = 1;
 	public static final int NUMBER_SIZE = 3;
+
+	public static final String MSG_STRIKE = "스트라이크";
+	public static final String MSG_BALL = "볼";
+	public static final String MSG_NOTHING = "낫싱";
 }
