043dda970816b9ab97a72c07c581a055c9d7e9bf
ys200209 <lys200209@naver.com>
2022-11-08 21:33:38 +0900

    feat(Application.java): 게임 애플리케이션을 실행함
    1. 컴퓨터와 사용자를 파라미터로 받는 게임 객체를 생성함
    2. 게임을 시작함
    3. 게임을 종료함
    4. 재시작을 원하면 게임 객체를 새로 생성하여 반복하고, 그렇지 않으면 종료함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import baseball.domain.Computer;
+import baseball.domain.Game;
+import baseball.domain.User;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Computer computer = new Computer();
+        User user = new User();
+
+        while (true) {
+            Game game = new Game(computer, user);
+            game.start();
+
+            game.finish();
+
+            if (game.isStop()) {
+                break;
+            }
+        }
     }
+
 }
