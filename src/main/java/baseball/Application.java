0b1343188a94b80f770c9489cb956f55c134b3a7
mingyunLEE <leemingyun@naver.com>
2022-11-08 20:13:48 +0900

    Add Function [게임 종료 후 새 게임/완전 종료 입력]

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -91,4 +91,11 @@ public class Application {
         }
         return false;
     }
+
+    private static int reGame(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+        int endGame = Integer.parseInt(Console.readLine());
+
+        return endGame;
+    }
 }
