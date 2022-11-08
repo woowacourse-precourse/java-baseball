9a6badff66fb72b7496090440bacb28d31fa8782
kksks3192 <kksks3192@gmail.com>
2022-11-08 22:44:24 +0900

    refactor(baseball): 코드 포맷 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,8 @@
 package baseball;
 
 import baseball.service.*;
-import camp.nextstep.edu.missionutils.Console;
 
 import java.io.IOException;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) throws IOException {
