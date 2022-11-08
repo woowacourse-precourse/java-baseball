4a5fd67273b93ceab3b6fa4a2136d91d86e51951
Bumsoo <rhqjatn2398@naver.com>
2022-11-08 18:16:08 +0900

    test(ScoreCalculatorTest): 볼 개수와 스트라이크 개수를 담은 자료구조 변경에 따라 테스트 케이스 수정

--- src/test/java/baseball/ScoreCalculatorTest.java
+++ src/test/java/baseball/ScoreCalculatorTest.java
@@ -2,44 +2,52 @@ package baseball;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import baseball.ScoreCalculator.BallOrStrike;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
 
 class ScoreCalculatorTest {
 
     @Test
     void calScoreCase1() {
-        List<Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(1, 3, 2));
+        EnumMap<BallOrStrike, Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(1, 3, 2));
+        EnumMap<BallOrStrike, Integer> expected = new EnumMap<>(Map.of(BallOrStrike.BALL, 2, BallOrStrike.STRIKE, 1));
 
-        assertThat(score).isEqualTo(List.of(2, 1));
+        assertThat(score).isEqualTo(expected);
     }
 
     @Test
     void calScoreCase2() {
-        List<Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(1, 2, 4));
+        EnumMap<BallOrStrike, Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(1, 2, 4));
+        EnumMap<BallOrStrike, Integer> expected = new EnumMap<>(Map.of(BallOrStrike.BALL, 0, BallOrStrike.STRIKE, 2));
 
-        assertThat(score).isEqualTo(List.of(0, 2));
+        assertThat(score).isEqualTo(expected);
     }
 
     @Test
     void calScoreCase3() {
-        List<Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(3, 1, 2));
+        EnumMap<BallOrStrike, Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(3, 1, 2));
+        EnumMap<BallOrStrike, Integer> expected = new EnumMap<>(Map.of(BallOrStrike.BALL, 3, BallOrStrike.STRIKE, 0));
 
-        assertThat(score).isEqualTo(List.of(3, 0));
+        assertThat(score).isEqualTo(expected);
     }
 
     @Test
     void calScoreCase4() {
-        List<Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(1, 2, 3));
+        EnumMap<BallOrStrike, Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(1, 2, 3));
+        EnumMap<BallOrStrike, Integer> expected = new EnumMap<>(Map.of(BallOrStrike.BALL, 0, BallOrStrike.STRIKE, 3));
 
-        assertThat(score).isEqualTo(List.of(0, 3));
+        assertThat(score).isEqualTo(expected);
     }
 
     @Test
     void calScoreCase5() {
-        List<Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(4, 5, 6));
+        EnumMap<BallOrStrike, Integer> score = ScoreCalculator.calScore(List.of(1, 2, 3), List.of(4, 5, 6));
+        EnumMap<BallOrStrike, Integer> expected = new EnumMap<>(Map.of(BallOrStrike.BALL, 0, BallOrStrike.STRIKE, 0));
 
-        assertThat(score).isEqualTo(List.of(0, 0));
+        assertThat(score).isEqualTo(expected);
     }
 
 }
