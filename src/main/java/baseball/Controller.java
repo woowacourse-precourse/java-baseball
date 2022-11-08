3714e33e34a87af3069be8b32f86efeff6d952e1
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-08 17:33:41 +0900

    Feat : Controller.compareNumber

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -11,18 +11,31 @@ public class Controller {
         this.user = user;
     }
 
-    public void setNumber(Model model, String number){
+    public void setNumber(Model model, String number) {
         model.setNumber(number);
     }
 
-    public String getNumber(Model model){
+    public String getNumber(Model model) {
         return model.getNumber();
     }
 
-    public String[] splitNumber(String number){
+    public String[] splitNumber(String number) {
         String[] numbers = number.split("");
         return numbers;
     }
 
-    
+    public int[] compareNumber(String computerNumber, String userNumber) {
+        int ball = 0;
+        int strike = 0;
+        String[] numbers = splitNumber(userNumber);
+
+        for (int i = 0; i < computerNumber.length(); i++) {
+            if (computerNumber.indexOf(numbers[i]) == i) {
+                strike++;
+            } else if (computerNumber.contains(numbers[i])) {
+                ball++;
+            }
+        }
+        return new int[]{ball,strike};
+    }
 }
