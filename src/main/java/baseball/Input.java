0dd5f58e31588670f92b985ac33c5ca87cabdf95
HangSub999 <gkdtjq999@gmail.com>
2022-11-08 16:24:32 +0900

    refactor: 문자열 에서 정수형으로 형변환

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -9,8 +9,8 @@ public class Input {
         return Console.readLine();
     }
 
-    public static String inputRestartNumber(){
+    public static int inputRestartNumber() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        return Console.readLine();
+        return Integer.parseInt(Console.readLine());
     }
 }
