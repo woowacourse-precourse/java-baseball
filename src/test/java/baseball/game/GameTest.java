ede3a62e3535481df935b1989fd639586606e39a
gomljo <gomljo0307@naver.com>
2022-11-08 18:03:00 +0900

    test(GameTest.java): 스트라이크와 볼을 집계하는 기능 검증
    상대방(컴퓨터)와 플레이어(사용자)의 숫자 리스트를 비교하여 주어진 시나리오에 맞게 집계되는지 검증

--- src/test/java/baseball/game/GameTest.java
+++ src/test/java/baseball/game/GameTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -141,5 +142,185 @@ public class GameTest {
 
         assertThat(ballCount).isEqualTo(twoBall);
     }
+    @Test
+    public void count_One_Ball_No_Strike_Test(){
+        int ballCount=0;
+        int strikeCount=0;
+        int oneBall=1;
+        int noStrike=0;
+        List<Integer> answer = List.of(noStrike, oneBall);
+        List<Integer> expectedAnswer=new ArrayList<>();
+        int notFound=-1;
+        List<Integer> computerNumbers;
+        String computerPick="456";
+        preparePlayer(opponent, computerPick);
+        computerNumbers=opponent.getDigits();
 
+        String userInput="637";
+        preparePlayer(player, userInput);
+
+        for(int computerNumber:computerNumbers){
+            if(player.getNumberPosition(computerNumber)!=opponent.getNumberPosition(computerNumber)&&player.getNumberPosition(computerNumber)!=notFound){
+                ballCount+=1;
+            }
+            if(player.getNumberPosition(computerNumber)==opponent.getNumberPosition(computerNumber)){
+                strikeCount+=1;
+            }
+
+        }
+        expectedAnswer.add(strikeCount);
+        expectedAnswer.add(ballCount);
+        assertThat(expectedAnswer).isEqualTo(answer);
+    }
+    @Test
+    public void count_One_Ball_One_Strike_Test(){
+        int ballCount=0;
+        int strikeCount=0;
+        int Ball=1;
+        int Strike=1;
+        List<Integer> answer = List.of(Strike, Ball);
+        List<Integer> expectedAnswer=new ArrayList<>();
+        int notFound=-1;
+        List<Integer> computerNumbers;
+        String computerPick="456";
+        preparePlayer(opponent, computerPick);
+        computerNumbers=opponent.getDigits();
+
+        String userInput="657";
+        preparePlayer(player, userInput);
+
+        for(int computerNumber:computerNumbers){
+            if(player.getNumberPosition(computerNumber)!=opponent.getNumberPosition(computerNumber)&&player.getNumberPosition(computerNumber)!=notFound){
+                ballCount+=1;
+            }
+            if(player.getNumberPosition(computerNumber)==opponent.getNumberPosition(computerNumber)){
+                strikeCount+=1;
+            }
+
+        }
+        expectedAnswer.add(strikeCount);
+        expectedAnswer.add(ballCount);
+        assertThat(expectedAnswer).isEqualTo(answer);
+    }
+
+    @Test
+    public void count_Two_Ball_One_Strike_Test(){
+        int ballCount=0;
+        int strikeCount=0;
+        int Ball=2;
+        int Strike=1;
+        List<Integer> answer = List.of(Strike, Ball);
+        List<Integer> expectedAnswer=new ArrayList<>();
+        int notFound=-1;
+        List<Integer> computerNumbers;
+        String computerPick="456";
+        preparePlayer(opponent, computerPick);
+        computerNumbers=opponent.getDigits();
+
+        String userInput="654";
+        preparePlayer(player, userInput);
+
+        for(int computerNumber:computerNumbers){
+            if(player.getNumberPosition(computerNumber)!=opponent.getNumberPosition(computerNumber)&&player.getNumberPosition(computerNumber)!=notFound){
+                ballCount+=1;
+            }
+            if(player.getNumberPosition(computerNumber)==opponent.getNumberPosition(computerNumber)){
+                strikeCount+=1;
+            }
+
+        }
+        expectedAnswer.add(strikeCount);
+        expectedAnswer.add(ballCount);
+        assertThat(expectedAnswer).isEqualTo(answer);
+    }
+    @Test
+    public void count_No_Ball_Two_Strike_Test(){
+        int ballCount=0;
+        int strikeCount=0;
+        int Ball=0;
+        int Strike=2;
+        List<Integer> answer = List.of(Strike, Ball);
+        List<Integer> expectedAnswer=new ArrayList<>();
+        int notFound=-1;
+        List<Integer> computerNumbers;
+        String computerPick="456";
+        preparePlayer(opponent, computerPick);
+        computerNumbers=opponent.getDigits();
+
+        String userInput="457";
+        preparePlayer(player, userInput);
+
+        for(int computerNumber:computerNumbers){
+            if(player.getNumberPosition(computerNumber)!=opponent.getNumberPosition(computerNumber)&&player.getNumberPosition(computerNumber)!=notFound){
+                ballCount+=1;
+            }
+            if(player.getNumberPosition(computerNumber)==opponent.getNumberPosition(computerNumber)){
+                strikeCount+=1;
+            }
+
+        }
+        expectedAnswer.add(strikeCount);
+        expectedAnswer.add(ballCount);
+        assertThat(expectedAnswer).isEqualTo(answer);
+    }
+    @Test
+    public void count_No_Ball_Three_Strike_Test(){
+        int ballCount=0;
+        int strikeCount=0;
+        int Ball=0;
+        int Strike=3;
+        List<Integer> answer = List.of(Strike, Ball);
+        List<Integer> expectedAnswer=new ArrayList<>();
+        int notFound=-1;
+        List<Integer> computerNumbers;
+        String computerPick="456";
+        preparePlayer(opponent, computerPick);
+        computerNumbers=opponent.getDigits();
+
+        String userInput="456";
+        preparePlayer(player, userInput);
+
+        for(int computerNumber:computerNumbers){
+            if(player.getNumberPosition(computerNumber)!=opponent.getNumberPosition(computerNumber)&&player.getNumberPosition(computerNumber)!=notFound){
+                ballCount+=1;
+            }
+            if(player.getNumberPosition(computerNumber)==opponent.getNumberPosition(computerNumber)){
+                strikeCount+=1;
+            }
+
+        }
+        expectedAnswer.add(strikeCount);
+        expectedAnswer.add(ballCount);
+        assertThat(expectedAnswer).isEqualTo(answer);
+    }
+    @Test
+    public void count_No_Ball_No_Strike_Test(){
+        int ballCount=0;
+        int strikeCount=0;
+        int Ball=0;
+        int Strike=0;
+        List<Integer> answer = List.of(Strike, Ball);
+        List<Integer> expectedAnswer=new ArrayList<>();
+        int notFound=-1;
+        List<Integer> computerNumbers;
+        String computerPick="637";
+        preparePlayer(opponent, computerPick);
+        computerNumbers=opponent.getDigits();
+
+        String userInput="458";
+        preparePlayer(player, userInput);
+
+        for(int computerNumber:computerNumbers){
+            if(player.getNumberPosition(computerNumber)!=opponent.getNumberPosition(computerNumber)&&player.getNumberPosition(computerNumber)!=notFound){
+                ballCount+=1;
+            }
+            if(player.getNumberPosition(computerNumber)==opponent.getNumberPosition(computerNumber)){
+                strikeCount+=1;
+            }
+
+        }
+        expectedAnswer.add(strikeCount);
+        expectedAnswer.add(ballCount);
+        assertThat(expectedAnswer).isEqualTo(answer);
+    }
 }
