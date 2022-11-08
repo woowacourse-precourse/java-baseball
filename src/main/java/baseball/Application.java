1187d4ce0e1a7fdf0e2fddbc92abed91a3db1042
JUNBEOM_LEE <bom1215@naver.com>
2022-11-08 14:05:00 +0900

    Refactor(Class Application): delete static
    delete each static of every method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,13 +11,12 @@ public class Application {
 
     static List<Integer> Answer_Numbers = new ArrayList<>();
 
-    public static void Start() {
+    public void Start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         Generate_Answer();
         Input();
     }
-
-    public static void Generate_Answer() {
+    public void Generate_Answer() {
         if (Answer_Numbers.size() != 0) {
             Answer_Numbers.clear();
         }
@@ -29,7 +28,7 @@ public class Application {
         }
     }
 
-    public static void Input() throws IllegalArgumentException {
+    public void Input() throws IllegalArgumentException {
         System.out.print("숫자를 입력해주세요 : ");
         try {
             int Input_Number = Integer.parseInt(Console.readLine());
@@ -55,7 +54,7 @@ public class Application {
         }
     }
 
-    public static HashMap Strike_or_Ball_Judge(List<Integer> Input, List<Integer> Answer) {
+    public HashMap Strike_or_Ball_Judge(List<Integer> Input, List<Integer> Answer) {
         HashMap<String, Integer> Strike_or_Ball = new HashMap<>();
         Strike_or_Ball.put("스트라이크", 0);
         Strike_or_Ball.put("볼", 0);
@@ -69,7 +68,7 @@ public class Application {
         return Strike_or_Ball;
     }
 
-    public static void Final_Judge(HashMap<String, Integer> Strike_or_Ball) {
+    public void Final_Judge(HashMap<String, Integer> Strike_or_Ball) {
         Integer Strike_Count = Strike_or_Ball.get("스트라이크");
         Integer Ball_Count = Strike_or_Ball.get("볼");
 
@@ -94,7 +93,7 @@ public class Application {
         }
     }
 
-    public static void End() throws IllegalArgumentException {
+    public void End() throws IllegalArgumentException {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         int New_Game_or_Finish = Integer.parseInt(Console.readLine());
@@ -108,6 +107,7 @@ public class Application {
     }
 
     public static void main(String[] args) {
-        Start();
+        Practice p = new Practice();
+        p.Start();
     }
 }
