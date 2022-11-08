a9b23cda0d6ddaa8b7568fc54a7e58bc6c0d91b1
Juuuic <sn990908a@gmail.com>
2022-11-08 17:56:33 +0900

    feat: 사용자 숫자 입력 method

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -28,6 +28,64 @@ public class Application {
         return computerNum;
     }
 
+    //2번부터 4번까지 반복
+    //2. 사용자가 3자리 숫자 입력
+    public static List<Integer> getUserNum(){
+        List<String> scope = new ArrayList<>(List.of("1","2","3","4","5","6","7","8","9"));
+
+        System.out.print("숫자를 입력해주세요: ");
+        String input = Console.readLine();
+        System.out.println(input);
+
+        String[] inputArray = input.split(""); //사용자가 입력한 숫자 쪼개기
+        List<Integer> userNum = new ArrayList<>(); //사용자가 입력한 최종 숫자
+        userException(scope, input, inputArray, userNum); //예외상황 판별
+
+        return userNum;
+    }
+
+    //2-1. 사용자가 3자리 숫자 입력 예외상황
+    public static void userException(List<String> scope, String userInput, String[] inputArray, List<Integer> userNum){
+        //0 입력
+        if(userInput.contains("0")){
+            throw new IllegalArgumentException("다시 입력해주세요.");
+        }
+
+        //3자리의 수가 아닐 때
+        if (userInput.length()!=len){
+            throw new IllegalArgumentException("다시 입력해주세요.");
+        }
+
+        //숫자 중복되었을 때
+        int cnt = 0;
+        int left = 0;
+        int right = 1;
+        while(cnt < len){
+            if(inputArray[left].equals(inputArray[right])){
+                throw new IllegalArgumentException("다시 입력해주세요.");
+            }
+            else if (right != len-1){
+                right++;
+            }
+            else {
+                left++;
+            }
+            cnt++;
+        }
+
+        //문자를 입력했을 때
+        for(int i=0; i<inputArray.length; i++){
+            if(!scope.contains(inputArray[i])){
+                throw new IllegalArgumentException("다시 입력해주세요.");
+            }
+            else{
+                userNum.add(Integer.valueOf(inputArray[i]));
+            }
+        }
+    }
+
+
+
 
 
 }
