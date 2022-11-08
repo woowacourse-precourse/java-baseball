7986de59b1b60072ff0d6997a409667c618002b8
“rotomoo” <quddlf410@naver.com>
2022-11-08 16:45:45 +0900

    test(Player) : 입력값 리스트 변환 테스트

--- src/test/java/baseball/domain/PlayerTest.java
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -2,6 +2,8 @@ package baseball.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 class PlayerTest {
@@ -12,15 +14,27 @@ class PlayerTest {
         Player player = new Player();
         String input = "122";
         String input2 = "1245";
-        String input3 = "124";
+        String input3 = "가나다";
+        String input4 = "124";
 
         boolean trueFlag = player.checkWrongInput(input);
         boolean trueFlag2 = player.checkWrongInput(input2);
-        boolean falseFlag = player.checkWrongInput(input3);
+        boolean trueFlag3 = player.checkWrongInput(input3);
+        boolean falseFlag = player.checkWrongInput(input4);
 
         assertThat(trueFlag).isEqualTo(true);
         assertThat(trueFlag2).isEqualTo(true);
+        assertThat(trueFlag3).isEqualTo(true);
         assertThat(falseFlag).isEqualTo(false);
+    }
+
+    @Test
+    void 입력값_리스트_변환_테스트() {
+
+        Player player = new Player();
+
+        List<Integer> inputList = player.StringToList("123");
+        assertThat(inputList).isEqualTo(List.of(1, 2, 3));
     }
 
 }
