00731d4f393444c74061708452a9834e97df031b
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-11-08 23:10:40 +0900

    feat(Application): 게임을 진행하는 main 메소드 구현 추가
    Computer 객체 저장 추가
    게임 진행 상태 저장 추가
    사용자 입력을 검증하는 기능(길이 제한, 상태에 맞는 입력 길이 검증) 추가
    사용자 입력에 따라 게임 진행 상태를 변환하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,101 @@
 package baseball;
 
+import baseball.pitch.PitchInput;
+import baseball.pitch.PitchResult;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static final Computer computer = new Computer();
+    public static GameState state;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        state = GameState.START;
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (state != GameState.FINISH) {
+            playGame();
+        }
+    }
+
+    private static void playGame() {
+        if (state == GameState.START) {
+            start();
+        }
+
+        if (state == GameState.INCORRECT) {
+            keepPlaying();
+        }
+
+        if (state == GameState.CORRECT) {
+            restartOrFinish();
+        }
+    }
+
+    private static void restartOrFinish() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String input = Console.readLine();
+        validateRestartAndFinishInput(input);
+
+        int value = Integer.parseInt(input);
+        changeStateToRestartOrFinish(value);
+    }
+
+    private static void keepPlaying() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        validateInput(input);
+
+        PitchInput pitch = new PitchInput(input);
+        PitchResult result = computer.compareToInputNumber(pitch.getNumber());
+
+        if (result.getPitchResultString().equals("3스트라이크")) {
+            state = GameState.CORRECT;
+        }
+
+        System.out.println(result.getPitchResultString());
+    }
+
+    private static void start() {
+        computer.createComputerNumber();
+        state = GameState.INCORRECT;
+    }
+
+    private static void changeStateToRestartOrFinish(int value) {
+        if (value == 1) {
+            state = GameState.START;
+            return;
+        }
+
+        if (value == 2) {
+            state = GameState.FINISH;
+            return;
+        }
+
+        throw new IllegalArgumentException("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    private static void validateInput(String input) {
+        validateInputLength(input);
+
+        input = input.replace(" ", "");
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("3자리 숫자를 입력해주세요.");
+        }
+    }
+
+    private static void validateRestartAndFinishInput(String input) {
+        validateInputLength(input);
+
+        input = input.replace(" ", "");
+        if (input.length() != 1) {
+            throw new IllegalArgumentException("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+    }
+
+    private static void validateInputLength(String input) {
+        if (input.length() >= 20) {
+            throw new IllegalArgumentException("입력 가능한 길이를 초과하였습니다.");
+        }
     }
 }
