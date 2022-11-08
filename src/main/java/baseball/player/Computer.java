2de101bb7cbe0bf0a657d8acb0a3f690c9facae4
JuSeong1130 <jsmoon1130@naver.com>
2022-11-08 18:41:04 +0900

    feat(Computer): Computer 랜덤 숫자 얻기 메서드 구현
    Computer 클래스 선언. 생성자를 통해 생성할때 자동적으로 랜덤 숫자가 들어가도록 설정

---
+++ src/main/java/baseball/player/Computer.java
@@ -0,0 +1,28 @@
+package baseball.player;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Computer {
+
+	String number;
+
+	Computer() {
+		number = getRandomNumber();
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	private String getRandomNumber() {
+		StringBuilder stringBuilder = new StringBuilder();
+		while (stringBuilder.length() < 3) {
+			String randomNumber = Randoms.pickNumberInRange(1, 9) + "";
+			if (stringBuilder.indexOf(randomNumber) == -1) {
+				stringBuilder.append(randomNumber);
+			}
+		}
+		return stringBuilder.toString();
+	}
+
+}
