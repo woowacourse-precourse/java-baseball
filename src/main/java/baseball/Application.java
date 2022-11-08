1af403e183d2817241d04ccf2353789962240e2b
thousandcows <milcamboo@gmail.com>
2022-11-08 11:17:59 +0900

    feat(feature): create function #3
    Function definition:
    - check the random number is valid
    - return 3 different numbers

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -15,5 +15,27 @@ public class Application {
         return "숫자 야구 게임을 시작합니다.";
     }
 
+    /*
+     3. Get random number
+     - get random number after making sure all digits are different
+     */
+    public static String getRandomNumber() {
+        List<String> numberList = new ArrayList<>();
+
+        while (numberList.size() < 3) {
+            String digit = String.valueOf(Randoms.pickNumberInRange(1, 9));
+
+            numberList = updateNumberList(digit, numberList);
+        }
+
+        return String.join("", numberList);
+    }
+
+    private static List<String> updateNumberList(String digit, List<String> numberList) {
+        if (!numberList.contains(digit)) {
+            numberList.add(digit);
+        }
+        return numberList;
+    }
 
 }
