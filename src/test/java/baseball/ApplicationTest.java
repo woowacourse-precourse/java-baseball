3d36b9f9b1aaed34d3e12b73226a1a34eaf7d807
Basak <chgy2131@naver.com>
2022-11-08 19:15:59 +0900

    test(ApplicationTest): 메인  로직 테스트 케이스 추가
    - 바로 정답을 맞추는 케이스
    - 입력에 구분자가 혼합되어있는 케이스
      - 사용자들이 입력시 구분자를 사용할 수 있으나, 이는
    게임에 영향을 주는 잘못된 입력이 아니라고 판단하여 정상으로 처리하도록 작성함

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -19,7 +19,36 @@ class ApplicationTest extends NsTest {
                 1, 3, 5, 5, 8, 9
         );
     }
-
+    @Test
+    void 게임종료_후_재시작2() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("123", "2");
+                    assertThat(output()).contains("3스트라이크", "게임 종료");
+                },
+                1,2,3,2
+        );
+    }
+    @Test
+    void 구분자입력() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("2,4,6", "1,3,5", "1", "5.9.7", "5/8/9", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5, 5, 8, 9
+        );
+    }
+    @Test
+    void 구분자입력2() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("2 4 6", "1\t3\t5", "1", "597", "5         8          9", "2");
+                    assertThat(output()).contains("낫싱", "3스트라이크", "1볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 3, 5, 5, 8, 9
+        );
+    }
     @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
