b70e33e04a75d00046307ba5d9226ab5b0ce2c4b
yoon <beatus96@naver.com>
2022-11-08 11:11:58 +0900

    test(playernumberTest) : 입력한 값 유효성 체크 기능 테스트
    1) 숫자 아닌경우  예) "가나다", ''abc"
    2) 숫자이지만, 세자리 아니거나 / 1-9사이 숫자 아니거나 / 서로다른 수가 아닌 경우
    => IllegalArgumentException 예외 발생하는지 테스트
    
    #1. 120을 입력했을 때, 1-9사이 숫자가 아님에도, 예외 발생하지 않음

---
+++ src/test/java/baseball/PlayerNumberTest.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import java.io.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayerNumberTest {
+    @Test
+    void 유효하지않은_플레이어수_숫자아닌경우_예외발생_테스트() {
+        String input = "120"; //"가나다" ,"ABC" ,"012", "1234"
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> Application.getPlayerNumber())
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+    
+}
