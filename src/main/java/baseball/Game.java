6dbdd247f71ac418686d9b261b3af2faa1bbd38d
Isu Shin <isu010614@naver.com>
2022-11-08 14:08:08 +0900

    refactor: Separate the class that asks to restart the game

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,15 +1,9 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.List;
 
 public class Game {
     private static final String ENTER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-    private static final String END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String RESTART = "1";
-    private static final String GAME_OVER = "2";
 
     public static void run() {
         int strike = -1;
@@ -24,7 +18,7 @@ public class Game {
             strike = Count.Strike(opponent, user);
             printHint(strike, ball);
         }
-        askRestart();
+        EndGame.askRestart();
     }
 
     public static void printUserNumber(List<Integer> user) {
@@ -72,26 +66,5 @@ public class Game {
         System.out.print(Hint.BALL.message());
         System.out.print(" " + strike);
         System.out.println(Hint.STRIKE.message());
-    }
-
-    public static void askRestart() {
-        System.out.println(END_MESSAGE);
-        System.out.println(RESTART_MESSAGE);
-        String action = Console.readLine();
-        validateAction(action);
-    }
-
-    public static void validateAction(String action) {
-        if (action.equals(RESTART)) {
-            System.out.println(RESTART);
-            Game.run();
-        }
-        if (action.equals(GAME_OVER)) {
-            // 게임 종료
-            System.out.println(GAME_OVER);
-        }
-        if (!action.equals(RESTART) && !action.equals(GAME_OVER)) {
-            throw new IllegalArgumentException("Enter 1 to re-start, or 2 to quit.");
-        }
     }
 }
