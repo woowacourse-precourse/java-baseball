a29b81bfe54cf9aceb610d266c140211612264fd
kiarakim <kiarakim413@gmail.com>
2022-11-08 22:22:51 +0900

    feat : check whether user's input is same with the size

--- src/main/java/baseball/util/UserNumberParser.java
+++ src/main/java/baseball/util/UserNumberParser.java
@@ -2,12 +2,19 @@ package baseball.util;
 
 public class UserNumberParser {
     public int[] parseUserNumber(String input, int size) throws IllegalArgumentException {
+        checkSize(input, size);
         int[] numbers = new int[size];
 
         for (int i = 0; i < size; i++) {
             numbers[i] = toInt(input, i);
         }
         return numbers;
+    }
+
+    private void checkSize(String input, int size) throws IllegalArgumentException {
+        if (input.length() != size) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private int toInt(String input, int index) {
