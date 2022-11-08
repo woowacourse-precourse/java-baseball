d75d3cbb9f48e79bf178257725b4e8d05d262ad1
sangminee <lsmlee99@gmail.com>
2022-11-08 19:54:11 +0900

    Feat : 상대방과 사용자 결과값 판단 유무 테스트 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -4,15 +4,29 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
-import static baseball.Application.computer;
-import static baseball.Application.selectNumber;
+import static baseball.Application.*;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+
+    @Test
+    void 상대방_사용자_결과값_판단(){
+        selectNumber();
+
+        List<Integer> numbers1 = new ArrayList<>();
+        numbers1.add(3);
+        numbers1.add(5);
+        numbers1.add(2);
+        checkNumber(numbers1);
+
+        assertThat(outputResult().isEmpty()).isFalse();
+    }
 
     @Test
     void 상대방_숫자_개수_확인(){
