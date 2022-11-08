6e446a0ef67facfd09ce5c5addcb0056e83ccb6e
jhsseonn <hosu0125@gmail.com>
2022-11-08 17:23:29 +0900

    refactor: strike, ball 변수 사용 도메인 필드를 사용한 코드로 리팩토링

--- src/main/java/baseball/Controller/BaseballController.java
+++ src/main/java/baseball/Controller/BaseballController.java
@@ -1,5 +1,6 @@
 package baseball.Controller;
 
+import baseball.Domain.Baseball;
 import baseball.Service.BaseballService;
 import camp.nextstep.edu.missionutils.Console;
 
@@ -10,20 +11,40 @@ import java.util.List;
 public class BaseballController {
 
     private BaseballService baseballService;
-//    private int matchCount=0;
+    public Baseball baseball;
+
     public static List<String> randomList = new ArrayList<>();
     private List<String> inputList = new ArrayList<>();
+    private int strike = 0;
+    private int ball = 0;
 
     //게임 시작
     public void startBaseballGame() {
-        String strikeAndBallMessage;
+
+        baseball = new Baseball(randomList, inputList, strike, ball);
+
+        printStartMessage();
+
         randomList = baseballService.createRandomNumber();
-        inputList = readInputNumber();
-        strikeAndBallMessage = countStrikeOrBall(randomList, inputList);
+        baseball.setComputerNumber(randomList);
+
+        while(baseball.getStrike()!=3) {
+            inputList = readInputNumber();
+            baseball.setInputNumber(inputList);
+
+            countStrikeOrBall(randomList, inputList);
+
+            result(baseball.getStrike(), baseball.getBall());
+        }
+
+        result(baseball.getStrike(), baseball.getBall());
+
+        gameExit(Integer.parseInt(Console.readLine()));
     }
 
     //유저가 입력한 숫자 받기
     public List<String> readInputNumber() {
+        printInputMessage();
         String inputNumber = Console.readLine();
 
         String[] numberToArray = inputNumber.split("");
@@ -33,18 +54,15 @@ public class BaseballController {
     }
 
     //스트라이크 또는 볼 판정
-    public String countStrikeOrBall(List<String> randomNumber, List<String> inputNumber) {
-        int strike=0;
-        int ball=0;
-        String outputMessage;
+    public void countStrikeOrBall(List<String> randomNumber, List<String> inputNumber) {
 
         for(String number: inputNumber) {
-            strike = countStrike(randomNumber, inputNumber, number, strike);
-            ball = countBall(randomNumber, inputNumber, number, ball);
+            strike = countStrike(randomNumber, inputNumber, number, baseball.getStrike());
+            ball = countBall(randomNumber, inputNumber, number, baseball.getBall());
         }
-        outputMessage = result(strike, ball);
 
-        return outputMessage;
+        baseball.setStrike(strike);
+        baseball.setBall(ball);
     }
 
 
@@ -72,18 +90,45 @@ public class BaseballController {
     }
 
     //스트라이크 볼 출력
-    public String result(int strike, int ball) {
-        String outputMessage="";
+    public void result(int strike, int ball) {
         if (strike==3) {
-            outputMessage = "3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             //게임이 종료되었다는 문구+게임 이대로 종료할지 재시작할지 입력받기
+            printEndMessage();
         } else if (strike==0 && ball==0) {
-            outputMessage = "낫싱";
+            System.out.println("낫싱");
         } else {
-            outputMessage = ball+"볼 "+strike+"스트라이크";
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+
+        }
+    }
+
+    public void printStartMessage() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printInputMessage() {
+        System.out.println("숫자를 입력해주세요 : ");
+    }
+
+    public void printEndMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+    }
+
+    //프로그램 재시작 or 완전히 종료
+    public void gameExit(int number) {
+
+        if (number!=1 && number!=2) {
+            throw new IllegalArgumentException();
         }
 
-        return outputMessage;
+        if (number==1) {
+            startBaseballGame();
+        }
+
+        if (number==2) {
+
+        }
     }
 
 }
