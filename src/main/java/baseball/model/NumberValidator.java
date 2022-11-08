860e5bbe3be70b716aeda167a0e44bcbc9bb3510
juno-junho <ssmm0205@naver.com>
2022-11-08 18:54:55 +0900

    feat(model): create checkIfIsDuplicateNum method
    NumberValidator 클래스 안에서 중복된 수가 있는지 판별하는 메서드 생성

--- src/main/java/baseball/model/NumberValidator.java
+++ src/main/java/baseball/model/NumberValidator.java
@@ -1,4 +1,10 @@
 package baseball.model;
 
+import java.util.List;
+
 public class NumberValidator {
+
+    public static boolean checkIfIsDuplicateNum(List<Integer> numbers) {
+        return numbers.size() == numbers.stream().distinct().count();
+    }
 }
