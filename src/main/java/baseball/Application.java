b8992d58b7aea877c5b8d91095884dd89d699ba2
Hongsugyeong <olivia258@naver.com>
2022-11-08 09:52:21 +0900

    (Feat) : 정답값과 입력값 문자열 비교 (포함여부)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+}
+
+/**
+ * 정답값과 입력값 문자열 비교
+ * 1. 포함여부
+ */
+class Array {
+    static boolean isArrayContains(final int[] ar, final int num) {
+        for (int value : ar) {
+            if (value == num) {
+                return true;
+            }
+        }
+        return false;
     }
 }
