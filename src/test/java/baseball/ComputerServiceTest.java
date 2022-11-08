066c469822423b2c590c1d6f6131fb9229d9615e
chaos0103 <lyt1228@naver.com>
2022-11-08 18:45:40 +0900

    refactor: ComputerService 기능과 구현 분리에 따른 수정

--- src/test/java/baseball/ComputerServiceTest.java
+++ src/test/java/baseball/ComputerServiceTest.java
@@ -8,18 +8,18 @@ import static org.assertj.core.api.Assertions.*;
 
 class ComputerServiceTest {
 
-    ComputerService computer = new ComputerService();
+    ComputerService computerService = new ComputerServiceImpl();
 
     @Test
     void 랜덤_숫자_자리수_테스트() {
-        List<Integer> computerNumber = computer.createComputerNumber();
+        List<Integer> computerNumber = computerService.createComputerNumber();
 
         assertThat(computerNumber.size()).isEqualTo(3);
     }
 
     @Test
     void 랜덤_숫자_범위_테스트() {
-        List<Integer> computerNumber = computer.createComputerNumber();
+        List<Integer> computerNumber = computerService.createComputerNumber();
 
         computerNumber
                 .forEach(number -> assertThat(number).isBetween(1, 9));
