e6991b8f4969771ec63fc2b19b7aa34c993c72eb
오승윤 <>
2022-11-08 18:34:57 +0900

    test (PlayerInputTest) : 게임 새로시작 및 종료를 선택하는 입력 값 이외의 값이 들어오는 경우 예외 테스트 추가

--- src/test/java/baseball/player/PlayerInputTest.java
+++ src/test/java/baseball/player/PlayerInputTest.java
@@ -1,12 +1,16 @@
 package baseball.player;
 
+import baseball.Application;
 import baseball.utils.appConfig.AppConfig;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class PlayerInputTest {
+class PlayerInputTest extends NsTest {
 
     PlayerInput playerInput = AppConfig.createPlayerInput();
 
@@ -41,6 +45,39 @@ class PlayerInputTest {
     void 사용자_3자리_이상_입력시_예외() {
         assertThatThrownBy(() -> playerInput.setNumbers("1234"))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 게임종료_후_새로시작_종료가_아닌_잘못된_값_문자_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() ->
+                        assertRandomNumberInRangeTest(() -> {
+                                    run("246", "135", "a");
+                                    assertThat(output()).contains("낫싱", "3스트라이크");
+                                },
+                                1, 3, 5
+                        ))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 게임종료_후_새로시작_종료가_아닌_잘못된_숫자_입력_예외() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() ->
+                        assertRandomNumberInRangeTest(() -> {
+                                    run("246", "135", "4");
+                                    assertThat(output()).contains("낫싱", "3스트라이크");
+                                },
+                                1, 3, 5
+                        ))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
     }
 
 }
