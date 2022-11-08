8c76c0bbf29f1a702b36325ad9e6d1d1108a150f
Nitroblack1 <109022435+Nitroblack1@users.noreply.github.com>
2022-11-08 11:25:10 +0900

    test(ImplementTest) : add test_code for generating answer method

--- src/test/java/baseball/ImplementTest.java
+++ src/test/java/baseball/ImplementTest.java
@@ -4,7 +4,9 @@ import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
@@ -12,6 +14,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ImplementTest extends NsTest{
 
+    @Test
+    void 정답_생성_테스트() {
+        List<Integer> answer = GameManager.createAnswer();
+        assertThat(answer.size()).isEqualTo(3);
+        for(int element : answer) {
+            assertThat(element)
+                .isGreaterThan(0)
+                .isLessThan(10);
+        }
+    }
     @Override
     public void runMain() {
         Application.main(new String[]{});
