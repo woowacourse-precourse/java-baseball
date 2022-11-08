9adab16f67026e287b36ee5bb5f9f4ccbaaee5df
sypark <soun997@gmail.com>
2022-11-08 16:30:45 +0900

    fix: modify function 'computersChoice'
    Infinity loop issue occur, change while loop to for loop that repeat 3 times

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -16,7 +16,7 @@ public class Application {
 
         List<Integer> computer = new ArrayList<>();
 
-        while (computer.size() < 3){
+        for (int i = 0; i < 3; i++){
             
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)){
