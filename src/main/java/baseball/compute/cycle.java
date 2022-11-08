32fa6865366324935864634e6a95db8be91457fd
kyokugirl <kjeaeun97@gmail.com>
2022-11-08 19:10:49 +0900

    기능 : 종료

---
+++ src/main/java/baseball/compute/cycle.java
@@ -0,0 +1,12 @@
+package baseball.compute;
+
+public class cycle {
+    public void restart(int sign) {
+        if (sign == 1) {
+            //다시 시작
+        }
+        if (sign == 2) {
+            //완전종료
+        }
+    }
+}
