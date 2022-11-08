2e05504b22690995955e05e0f7afd76f5de367f0
TeddyYeoni <po99027@naver.com>
2022-11-08 22:33:33 +0900

    사용자에게 3자리 수를 입력 받고 잘못된 값을 입력하면 예외 발생하는 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,23 +3,41 @@ package baseball;
 import java.util.ArrayList;
 import java.util.List;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
-	
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    	
-    }
-    
-    public static List<Integer> computer() {
-    	List<Integer> computer = new ArrayList<Integer>();
-    	while(computer.size() < 3) {
-    		int RandomNumber = Randoms.pickNumberInRange(1, 9);
-    		if(!computer.contains(RandomNumber)) {
-    			computer.add(RandomNumber);
-    		}
-    	}
+
+	public static void main(String[] args) {
+		// TODO: 프로그램 구현
+
+	}
+
+	public static List<Integer> computer() {
+		List<Integer> computer = new ArrayList<Integer>();
+		while (computer.size() < 3) {
+			int RandomNumber = Randoms.pickNumberInRange(1, 9);
+			if (!computer.contains(RandomNumber)) {
+				computer.add(RandomNumber);
+			}
+		}
 		return computer;
+	}
+
+	public static List<Integer> user() {
+    	String userInput = Console.readLine(); 
+    	String[] userInputArray = userInput.split("");
+    	List<Integer> user = new ArrayList<Integer>();
+    	for (int index = 0; index < userInputArray.length; index++) {
+    		int digitNumber = Integer.parseInt(userInputArray[index]);
+    		user.add(digitNumber);
+    		if(!user.contains(digitNumber)) {
+    			throw new IllegalArgumentException();
+    		}
+		}
+    	if(user.size() != 3) {
+    		throw new IllegalArgumentException();
+    	}
+		return user;
 	}
 }
