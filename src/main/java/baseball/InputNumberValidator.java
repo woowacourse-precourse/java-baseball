f134d909a773d742b98de68bd0f6d896a28e0a59
meunrud <y3a0522@gmail.com>
2022-11-08 21:39:18 +0900

    :art: 유저 입력 숫자 자료형 String으로 되돌림

--- src/main/java/baseball/InputNumberValidator.java
+++ src/main/java/baseball/InputNumberValidator.java
@@ -5,47 +5,46 @@ import java.util.List;
 
 public class InputNumberValidator {
 
-    public static boolean inputNumberTotalCheck(int inputNumber){
+   public static boolean inputNumberTotalCheck(String  inputNumber){
         return StringIndexOutOfRange(inputNumber)
                 && rangeCheck(inputNumber)
                 && repeatNumberCheck(inputNumber);
     }
-    public static boolean inputNumberCorrection(int inputNumber){
+    public static boolean inputNumberCorrection(String inputNumber){
         return thereIsSpace(inputNumber)
                 && thereIsComma(inputNumber);
     }
-    public static boolean restartOrEndNumberCheck(int inputNumber){
-        return inputNumber == Game.RESTART || inputNumber == Game.GAMEEND;
+    public static boolean restartOrEndNumberCheck(String inputNumber){
+        return inputNumber.equals(Game.RESTART) || inputNumber.equals(Game.GAMEEND);
     }
-    public static boolean StringIndexOutOfRange(int inputNumber){
-        String input = String.valueOf(inputNumber);
-        return inputNumber == Game.BALLSIZE;
+    public static boolean StringIndexOutOfRange(String inputNumber){
+        return inputNumber.length() ==  Game.BALLSIZE;
     }
 
-    public static boolean rangeCheck(int inputNumber){
+    public static boolean rangeCheck(String inputNumber){
+        int input = Integer.parseInt(inputNumber);
         List<Integer> inputNumbers = new ArrayList<>();
-        inputNumbers.add(inputNumber);
+        inputNumbers.add(input);
         return inputNumbers.stream()
                 .filter(number -> 1 <= number && number <= 9)
                 .count() == Game.BALLSIZE;
     }
 
-    public static boolean repeatNumberCheck(int inputNumber) {
+    public static boolean repeatNumberCheck(String inputNumber) {
+        int input = Integer.parseInt(inputNumber);
         List<Integer> inputNumbers = new ArrayList<>();
-        inputNumbers.add(inputNumber);
+        inputNumbers.add(input);
         return inputNumbers.stream()
                 .distinct()
                 .count() == Game.BALLSIZE;
     }
 
-    public static boolean thereIsComma(int inputNumber){
-        String input = String.valueOf(inputNumber);
-        return input.contains(",");
+    public static boolean thereIsComma(String  inputNumber){
+        return inputNumber.contains(",");
     }
 
-    public static boolean thereIsSpace(int inputNumber){
-        String input = String.valueOf(inputNumber);
-        return input.contains(" ");
+    public static boolean thereIsSpace(String  inputNumber){
+        return inputNumber.contains(" ");
     }
 
 
