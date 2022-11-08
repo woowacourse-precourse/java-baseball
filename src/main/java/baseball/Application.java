7a931d9d8be671e4343d24db1a22e138c1dfaec6
JONGMIN SHIN <jongmin373@gmail.com>
2022-11-08 14:06:23 +0900

    feat(makeRandomInteger) : 임의의 난수 발생 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-
+        List<Integer> computer = makeRandomInteger();
+    }
+    // 난수 발생기
+    public static List<Integer> makeRandomInteger() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
 
 }
