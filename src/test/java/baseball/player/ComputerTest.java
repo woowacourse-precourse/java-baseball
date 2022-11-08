ac419e37c724353292d53bb8f44a8ff9addfea76
ckyeon <qsky8520@gmail.com>
2022-11-08 20:48:38 +0900

    refactor(ComputerTest): use List<String> instead of String type

--- src/test/java/baseball/player/ComputerTest.java
+++ src/test/java/baseball/player/ComputerTest.java
@@ -3,6 +3,7 @@ package baseball.player;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import baseball.Valid;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -18,9 +19,9 @@ class ComputerTest {
     @Test
     void selectBalls_1_과_9_사이의_랜덤한_공을_3개_선택() {
         computer.selectBalls();
-        String selectBalls = computer.getSelectBalls().toString();
 
-        assertThat(selectBalls.length()).isEqualTo(3);
-        assertThat(selectBalls).doesNotMatch(Valid.NOT_INPUT_NUMBER.getPattern());
+        List<String> selectBalls = computer.getSelectBalls();
+        assertThat(selectBalls.size()).isEqualTo(3);
+        assertThat(selectBalls.toString()).doesNotMatch(Valid.NOT_INPUT_NUMBER.getPattern());
     }
 }
