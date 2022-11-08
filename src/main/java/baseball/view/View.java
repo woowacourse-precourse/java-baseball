d1784ab17ac29c3392ef1bb813fbed36d464c632
Frog-Slayer <Frog-Slayer@github.com>
2022-11-08 20:11:27 +0900

    refactor : View에서 MESSAGE, FORMAT을 같은 자리에 모아둠

--- src/main/java/baseball/view/View.java
+++ src/main/java/baseball/view/View.java
@@ -4,13 +4,13 @@ import baseball.model.Game;
 
 public class View {
     private static final String NOTHING_MESSAGE = "낫싱\n";
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
+    private static final String REPLAY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
     private static final String STRIKE_MESSAGE_FORMAT = "%d스트라이크\n";
     private static final String BALL_MESSAGE_FORMAT = "%d볼\n";
     private static final String BALL_AND_STRIKE_MESSAGE_FORMAT = "%d볼 %d스트라이크\n";
-
-    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
     private static final String GAME_CLEAR_MESSAGE_FORMAT = "%d개의 숫자를 모두 맞히셨습니다! 게임 종료\n";
-    private static final String REPLAY_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n";
+
 
     public void printNothing() {
         System.out.print(NOTHING_MESSAGE);
@@ -27,7 +27,6 @@ public class View {
     public void printBall(int ballCount) {
         System.out.printf(BALL_MESSAGE_FORMAT, ballCount);
     }
-
 
     public void printGameClear(int answerLength) {
         System.out.printf(GAME_CLEAR_MESSAGE_FORMAT, answerLength);
