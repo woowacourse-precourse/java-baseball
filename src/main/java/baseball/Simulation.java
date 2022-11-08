2f396041776331a79f86544daa93f2df629f422f
gudals113 <gudals113@gmail.com>
2022-11-08 16:02:19 +0900

    feat: input user number
    feat: input user number
    
    3자리 숫자 입력 후 서로 다른 숫자인지, 모두 숫자인지 유효성 검증
    유효성 검증 실패 시 IllegalArgumentException

--- src/main/java/baseball/Simulation.java
+++ src/main/java/baseball/Simulation.java
@@ -6,9 +6,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Simulation {
-
+    static final String START_MSG = "숫자 야구 게임을 시작합니다.";
+    static final String INPUT_REQ = "숫자를 입력해주세요 : ";
     public void simulate(){
         List<Integer> randomNumList = createNumber();
+        while (true){
+            List<Integer> inputNumList = inputNumber();
+//            checkStrikeBallOut();
+//            returnMiddleResult();
+            break;
+        }
     }
 
     public List<Integer> createNumber() {
@@ -20,6 +27,36 @@ public class Simulation {
             }
         }
         return numberList;
+    }
+
+    public List<Integer> inputNumber() {
+        List<Integer> numberList = new ArrayList<>();
+        System.out.print(INPUT_REQ);
+        String inputStr = Console.readLine();
+        checkInputValidation(inputStr);
+        for (int i = 0; i < 3; i++){
+            numberList.add(inputStr.charAt(i)-'0');
+        }
+        return numberList;
+    }
+
+    public void checkInputValidation(String inputStr) {
+        if (inputStr.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < 3; i++){
+            int s = inputStr.charAt(i) - '0';
+            if (s<1 || s>9) {
+                throw new IllegalArgumentException();
+            }
+        }
+        char firstNum = inputStr.charAt(0);
+        char secondNum = inputStr.charAt(1);
+        char thirdNum = inputStr.charAt(2);
+        if (firstNum==secondNum || secondNum==thirdNum || thirdNum==firstNum){
+            throw new IllegalArgumentException();
+        }
+
     }
 
 
