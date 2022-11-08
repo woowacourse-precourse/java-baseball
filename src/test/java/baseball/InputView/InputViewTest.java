44d2b58eaa586132a392fa5818c3a359bb255393
gomljo <gomljo0307@naver.com>
2022-11-08 19:28:32 +0900

    test(InputViewTest.java): 게임 시작 후 플레이어(사용자)에게 게임을 재시작할 것인지 종료할 것인지 입력하라는 메시지를 출력하는 기능 검증

--- src/test/java/baseball/InputView/InputViewTest.java
+++ src/test/java/baseball/InputView/InputViewTest.java
@@ -10,6 +10,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class InputViewTest {
     private static final String GAME_START_MESSAGE="숫자 야구 게임을 시작합니다.";
     private static final String REQUEST_ENTER_INPUT="숫자를 입력해주세요 : ";
+    private static final String REQUEST_SELECT_NEW_GAME_OR_END="게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
 
     @Test
     public void notice_Start_Message_Test(){
@@ -26,5 +28,13 @@ public class InputViewTest {
         System.setOut(new PrintStream(out));
         System.out.print(REQUEST_ENTER_INPUT);
         Assertions.assertThat(REQUEST_ENTER_INPUT).isEqualTo(out.toString());
+    }
+    @Test
+    public void request_Entering_Input_Want_More_Game_OR_End_Message_Test(){
+
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        System.out.print(REQUEST_SELECT_NEW_GAME_OR_END);
+        Assertions.assertThat(REQUEST_SELECT_NEW_GAME_OR_END).isEqualTo(out.toString());
     }
 }
