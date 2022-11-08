2b4c486f90564e822308700c919e1e487dacd4b6
jisoolee11 <jisu6414@likelion.org>
2022-11-08 21:58:59 +0900

    feat: 숫자야구게임 시작  메소드

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
 public class Application {
+
+    public void start() {
+        // 숫자야구게임 시작
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application numberBaseballGame = new Application();
+        numberBaseballGame.start();
     }
 }
