03a705f568538bdaa85b8ec47e1eb430baea71af
HASEUNGHEEE <ariul123@gmail.com>
2022-11-08 12:59:56 +0900

    feat: 힌트를 출력하는 기능 구현
    - 힌트로 "3스트라이크"가 출력될 때까지 게임이 계속 진행되기 때문에 boolean을 리턴하는 printGameOver() 메서드를 만들었습니다.
    - countStrike() 메서드와 countSameNumber() 메서드를 호출한 결과를 조합하여 strikeCount와 ballCount를 구합니다.
    - printHint() 메서드를 통해 힌트를 출력합니다.

--- src/main/java/baseball/GameConstant.java
+++ src/main/java/baseball/GameConstant.java
@@ -4,4 +4,7 @@ public final class GameConstant {
     public static final int MIN_VALUE = 1;
     public static final int MAX_VALUE = 9;
     public static final int NUMBER_COUNT = 3;
+    public static final String BALL = "볼 ";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
 }
--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -3,6 +3,15 @@ package baseball;
 import java.util.LinkedHashMap;
 
 public class Judge {
+    public boolean printGameOver(LinkedHashMap<Integer, Integer> computerNumbers,
+                                 LinkedHashMap<Integer, Integer> playerNumbers) {
+        int sameNumberCount = countSameNumber(computerNumbers, playerNumbers);
+        int strikeCount = countStrike(computerNumbers, playerNumbers);
+        int ballCount = sameNumberCount - strikeCount;
+        printHint(strikeCount, ballCount);
+        return (strikeCount == GameConstant.NUMBER_COUNT);
+    }
+
     public int countStrike(LinkedHashMap<Integer, Integer> computerNumbers,
                            LinkedHashMap<Integer, Integer> playerNumbers) {
         int strikeCount = 0;
@@ -23,5 +32,19 @@ public class Judge {
             }
         }
         return sameNumberCount;
+    }
+
+    public void printHint(int strikeCount, int ballCount) {
+        String hint = "";
+        if (ballCount != 0) {
+            hint += (ballCount + GameConstant.BALL);
+        }
+        if (strikeCount != 0) {
+            hint += (strikeCount + GameConstant.STRIKE);
+        }
+        if (hint.length() == 0) {
+            hint += GameConstant.NOTHING;
+        }
+        System.out.println(hint);
     }
 }
