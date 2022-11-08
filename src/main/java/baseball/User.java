4be2cc454809e2562a05c4e6e178c1c6f008c5d8
Dabeen Jeong <hello70825@gmail.com>
2022-11-08 20:58:17 +0900

    refactor: 입력 메소드와 검증 메소드를 분리하다.

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -5,16 +5,28 @@ import baseball.validation.GameValidation;
 import camp.nextstep.edu.missionutils.Console;
 
 public class User {
+    private String userBaseballNumber = "";
 
-    public String inputUserBaseballNumber() {
-        GameValidation gameValidation = new GameValidation();
+    public void setUserBaseballNumber(String userBaseballNumber) {
+        this.userBaseballNumber = userBaseballNumber;
+    }
+
+    public boolean inputUserNumber() {
+        String userInput;
 
         System.out.print(GameMessage.INPUT_NUMBER_MESSAGE);
-        String userInput = Console.readLine();
+        userInput = Console.readLine();
 
-        gameValidation.setUserInput(userInput);
-        gameValidation.isAllCheck();
+        setUserBaseballNumber(userInput);
 
-        return userInput;
+        return isValidInputUserBaseballNumber();
+    }
+
+    private boolean isValidInputUserBaseballNumber() {
+        GameValidation gameValidation = new GameValidation();
+
+        gameValidation.setUserInput(this.userBaseballNumber);
+
+        return gameValidation.isAllCheck();
     }
 }
