1979bb7cbb1a87a78b48513d74df7d8f76d1ec7c
saewoo1 <wooozooo1123@gmail.com>
2022-11-08 20:02:10 +0900

    delete(CreateRandomNumberTest): 주석 제거

--- src/test/java/baseball/CreateRandomNumberTest.java
+++ src/test/java/baseball/CreateRandomNumberTest.java
@@ -9,10 +9,6 @@ import org.assertj.core.api.Assertions;
 
 class CreateRandomNumberTest {
 
-//  @BeforeEach //얘가 모든 테스트 전에 한번씩 쓰고싶은데 어케 끌어오지...
-//  void setNumber() {
-//    String testNumber = CreateRandomNumber.numberMaker();
-//  }
   @Test
   void numberMaker_길이_테스트() {
     String testNumber = CreateRandomNumber.numberMaker();
