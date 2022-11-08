adff70fcf93a6741d2532a5314691b8b02c22336
Identityless <rlandwnd555@naver.com>
2022-11-08 17:05:20 +0900

    fix : 각 자릿수가 같은 수가 나올 수 있는 버그 수정

--- src/main/java/domain/RandNumVO.java
+++ src/main/java/domain/RandNumVO.java
@@ -3,15 +3,24 @@ package domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RandNumVO {
     private final List<Integer> randNum;
 
     public RandNumVO() throws IllegalArgumentException{
-        this.randNum = List.of(Randoms.pickNumberInRange(1, 9)
-                , Randoms.pickNumberInRange(1, 9)
-                , Randoms.pickNumberInRange(1, 9));
+        int nums[] = new int[3];
+        nums[0] = Randoms.pickNumberInRange(1, 9);
+        nums[1] = Randoms.pickNumberInRange(1, 9);
+        while(nums[0] != nums[1]) {
+            nums[1] = Randoms.pickNumberInRange(1, 9);
+        }
+        nums[2] = Randoms.pickNumberInRange(1, 9);
+        while(nums[2] != nums[1] && nums[2] != nums[0]) {
+            nums[2] = Randoms.pickNumberInRange(1, 9);
+        }
+        this.randNum = List.of(nums[0], nums[1], nums[2]);
     }
 
     public List<Integer> getRandNum(){
