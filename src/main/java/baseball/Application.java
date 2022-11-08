55bf3e47983b48b818391c30675dd0f103d4ae7c
SONGmINjIJI <songminki18@gmail.com>
2022-11-08 22:15:10 +0900

    feat: 컴퓨터의 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,18 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public ArrayList<Integer> make_computer_number() {
+        ArrayList<Integer> com_num = new ArrayList<>();
+        com_num.add(Randoms.pickNumberInRange(1, 9));
+        while (com_num.size() < 3) {
+            int random_num = Randoms.pickNumberInRange(1, 9);
+        }
+        return com_num;
     }
 }
