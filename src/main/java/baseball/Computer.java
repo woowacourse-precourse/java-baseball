3cdb4106b043b156bd4c4879601a142ec9c60048
Dabeen Jeong <hello70825@gmail.com>
2022-11-08 22:16:37 +0900

    feat: 정답값을 저장하는 변수를 초기화하다.

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -4,12 +4,15 @@ import baseball.domain.BaseballResult;
 import baseball.resource.GameValue;
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
     private List<Character> computerAnswer;
 
     public void createAnswer() {
+        computerAnswer = new ArrayList<>();
+
         while (computerAnswer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             Character randomNumberChar = (char)(randomNumber + '0');
