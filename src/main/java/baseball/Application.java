b1ae7cd5ae28da2f8e82a7dd36b1f7a0acad1351
thousandcows <milcamboo@gmail.com>
2022-11-08 11:30:19 +0900

    feat(feature): create function #10
    Function definition:
    - if 3 strike, return message to guide the player

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -121,4 +121,11 @@ public class Application {
         return message;
     }
 
+    // 10. Message of game continue or exit
+    public static void askContinueOrExitGame() {
+        System.out.println("3스트라이크\n" + "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+
 }
