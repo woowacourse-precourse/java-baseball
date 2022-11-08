f22cd3b5b0e0685d8710714c61f7ff3b1c05d2a0
gardenee <snnjjang@naver.com>
2022-11-08 22:03:45 +0900

    feat: 사용할 메소드 껍데기 생성
    기능목록 1~10번 메소드 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,78 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+
+    public static void baseballGame() {
+
+    }
+
+
+    public static List<Integer> newAnswer() {
+        List<Integer> answerNumber = new ArrayList<>();
+
+        return answerNumber;
+    }
+
+
+    public static boolean tryGuess(List<Integer> answerNumber) {
+        boolean result = false;
+
+        return result;
+    }
+
+
+    public static List<Integer> checkBallCount(List<Integer> answerNumber, List<Integer> guessNumber) {
+        List<Integer> ballCount = new ArrayList<>();
+
+        return ballCount;
+    }
+
+
+    public static boolean checkStrike(int i, List<Integer> answerNumber, List<Integer> guessNumber) {
+        boolean isStrike = false;
+
+        return isStrike;
+    }
+
+
+    public static boolean checkBall(int targetNumber, List<Integer> answerNumber) {
+        boolean isBall = false;
+
+        return isBall;
+    }
+
+
+    public static List<Integer> userInput() {
+        List<Integer> userGuess = new ArrayList<>();
+
+        return userGuess;
+    }
+
+
+    public static List<Integer> guessStringToList(String userGuessString) {
+        List<Integer> userGuess = new ArrayList<>();
+
+        return userGuess;
+    }
+
+
+    public static boolean gameInputException(String userGuessString) {
+        boolean error = false;
+
+        return error;
+    }
+
+
+    public static boolean askNewGame() {
+        boolean newGame = false;
+
+        return newGame;
     }
 }
