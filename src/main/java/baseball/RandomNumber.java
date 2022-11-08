82897c3eca121d36a4c81a58f0a586b962146464
seonghoo1217 <seonghoo1217@naver.com>
2022-11-08 13:31:26 +0900

    refactor : modify to upgrade time effective
    It's for purpose time effective upgrade
    
    Fix(commit_id) : #4cbe2al

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -11,7 +11,7 @@ public class RandomNumber {
         for (int i=0;i<3;i++){
             int random_number = Randoms.pickNumberInRange(1, 9);
             if (random_number_list.contains(random_number)){
-                int sub_random = nonPermissionExistNumber(random_number,random_number_list);
+                int sub_random = nonPermissionExistNumber(random_number);
                 random_number_list.add(sub_random);
             }else {
                 random_number_list.add(random_number);
@@ -20,12 +20,14 @@ public class RandomNumber {
         return random_number_list;
     }
 
-    private static int nonPermissionExistNumber(int random_number,List<Integer> random_number_list){
+    private static int nonPermissionExistNumber(int random_number){
         int sub_random;
-        while (random_number_list.contains(random_number)){
-            random_number=Randoms.pickNumberInRange(1, 9);
+        if (random_number - 1>0){
+            sub_random=Randoms.pickNumberInRange(1,random_number-1);
+            return sub_random;
+        }else {
+            sub_random=Randoms.pickNumberInRange(random_number+1,9);
+            return sub_random;
         }
-        sub_random=random_number;
-        return sub_random;
     }
 }
