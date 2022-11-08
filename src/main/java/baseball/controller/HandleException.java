12e08d98edb2208609bd3386b4b4ded829a18658
Sieun Park <psieun0606@gmail.com>
2022-11-08 12:57:39 +0900

    bug: try-catch 오류 수정

--- src/main/java/baseball/controller/HandleException.java
+++ src/main/java/baseball/controller/HandleException.java
@@ -5,36 +5,24 @@ import java.io.IOException;
 public class HandleException {
 
     public void handleUserInputNumberRangeException(String userInput) {
-        try {
-            for (int i = 0; i < userInput.length(); i++) {
-                if (userInput.charAt(i) < 48 || userInput.charAt(i) > 57) {
-                    throw new IllegalArgumentException("잘못된 값을 입력하였습니다. 게임을 종료합니다.");
-                }
+        for (int i = 0; i < userInput.length(); i++) {
+            if (userInput.charAt(i) < 48 || userInput.charAt(i) > 57) {
+                throw new IllegalArgumentException("잘못된 값을 입력하였습니다. 게임을 종료합니다.");
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 값을 입력하였습니다. 게임을 종료합니다.");
         }
     }
 
     static void handleUserInputSizeException(String userInput) {
-        try {
-            for (int i = 0; i < userInput.length(); i++) {
-                if (i != userInput.indexOf(userInput.charAt(i))) {
-                    throw new IllegalArgumentException();
-                }
+        for (int i = 0; i < userInput.length(); i++) {
+            if (i != userInput.indexOf(userInput.charAt(i))) {
+                throw new IllegalArgumentException("중복된 값을 입력하였습니다. 게임을 종료합니다.");
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println("중복된 값을 입력하였습니다. 게임을 종료합니다.");
         }
     }
 
     public void handleRestartOrExitNumberException(int restartORExit) {
-        try {
-            if (1 > restartORExit || restartORExit > 2) {
-                throw new IllegalArgumentException();
-            }
-        } catch (IllegalArgumentException e) {
-            System.out.println("잘못된 값을 입력하였습니다. 게임을 종료합니다.");
+        if (1 > restartORExit || restartORExit > 2) {
+            throw new IllegalArgumentException("잘못된 값을 입력하였습니다. 게임을 종료합니다.");
         }
     }
 }
