4b1796df901df3ce375cbe4ecc71a8a8cdfa3582
김수민(Sumin Kim) <sumin.kim.develop@gmail.com>
2022-11-08 20:08:16 +0900

    test(Computer): 생성 확인 테스트 코드 수정 및 status 테스트 코드 추가
    생성 확인 테스트 코드를 한번에 100개를 체크하도록 변경
    private 메소드인 strike, ball 을 테스트하는 대신 status 만 테스트하도록 변경
    status 테스트 코드를 추가 작성

--- src/test/java/baseball/ComputerTest.java
+++ src/test/java/baseball/ComputerTest.java
@@ -1,72 +1,51 @@
 package baseball;
 
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
-
-import java.lang.reflect.Constructor;
-import java.lang.reflect.Field;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ComputerTest {
 
-    @RepeatedTest(value = 100, name = "생성_확인() #{currentRepetition}")
-    void generateAnswer_올바른_생성_확인() {
-        Computer computer = Computer.generateAnswer();
-        try {
-            Field field = computer.getClass().getDeclaredField("answer");
-            field.setAccessible(true);
-            Numbers answer = (Numbers) field.get(computer);
-            boolean result = true;
-            assertThat(result).isEqualTo(answer.validation());
-        } catch (Exception e) {
-            e.printStackTrace();
+    @Test
+    void 올바른_생성_확인() {
+        for (int i = 0; i < 100; i++) {
+            Computer.create();
         }
     }
 
     @Test
-    void strike_확인() {
-        try {
-            Constructor<Computer> constructor = Computer.class.getDeclaredConstructor(Numbers.class);
-            constructor.setAccessible(true);
-            Computer test = constructor.newInstance(new Numbers(List.of(1, 2, 3)));
-            User user = new User();
-            user.guess = new Numbers(List.of(1, 2, 4));
-            int result = 2;
-            assertThat(result).isEqualTo(test.strike(user));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    void status_확인_1() {
+        Computer test = Computer.of(1, 2, 3);
+        User user = new User();
+        user.guess = Numbers.of(1, 4, 2);
+        String result = "1볼 1스트라이크";
+        assertThat(result).isEqualTo(test.status(user).toString());
     }
 
     @Test
-    void ball_확인() {
-        try {
-            Constructor<Computer> constructor = Computer.class.getDeclaredConstructor(Numbers.class);
-            constructor.setAccessible(true);
-            Computer test = constructor.newInstance(new Numbers(List.of(1, 2, 3)));
-            User user = new User();
-            user.guess = new Numbers(List.of(2, 1, 3));
-            int result = 2;
-            assertThat(result).isEqualTo(test.ball(user));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    void status_확인_2() {
+        Computer test = Computer.of(1, 2, 3);
+        User user = new User();
+        user.guess = Numbers.of(4, 5, 6);
+        String result = "낫싱";
+        assertThat(result).isEqualTo(test.status(user).toString());
     }
-    
+
     @Test
-    void status_확인() {
-        try {
-            Constructor<Computer> constructor = Computer.class.getDeclaredConstructor(Numbers.class);
-            constructor.setAccessible(true);
-            Computer test = constructor.newInstance(new Numbers(List.of(1, 2, 3)));
-            User user = new User();
-            user.guess = new Numbers(List.of(1, 4, 2));
-            String result = "1볼 1스트라이크";
-            assertThat(result).isEqualTo(test.status(user).toString());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    void status_확인_3() {
+        Computer test = Computer.of(1, 2, 3);
+        User user = new User();
+        user.guess = Numbers.of(1, 5, 6);
+        String result = "1스트라이크";
+        assertThat(result).isEqualTo(test.status(user).toString());
+    }
+
+    @Test
+    void status_확인_4() {
+        Computer test = Computer.of(1, 2, 3);
+        User user = new User();
+        user.guess = Numbers.of(2, 3, 1);
+        String result = "3볼";
+        assertThat(result).isEqualTo(test.status(user).toString());
     }
 }
