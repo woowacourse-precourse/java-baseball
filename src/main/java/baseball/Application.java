b028c6ed30a011a5084987daddfd021cad3bebc5
Umin <adorecamus@gmail.com>
2022-11-08 23:24:54 +0900

    feat: 게임 시작, 컴퓨터가 임의의 수 선택, 게임 플레이어가 입력한 값 잘못된 경우 예외 발생, 게임 플레이어가 입력한 수 저장

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,25 +8,69 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-
         System.out.println("숫자 야구 게임을 시작합니다.");
-
-        computer();
-
-        System.out.println("숫자를 입력해주세요 : ");
-
+        start();
     }
 
-    public static List<Integer> computer() {
+    public static void start() {
+        List<Integer> computerNumber = randomNumber();
+
+        boolean run = true;
+
+        while (run) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String numbers = Console.readLine();
+
+            isThree(numbers);
+            hasDuplicate(numbers);
+
+            List<Integer> playerNumber = player(numbers);
+
+        }
+    }
+
+    public static List<Integer> randomNumber() {
         List<Integer> computer = new ArrayList<>();
+
         while (computer.size() < 3){
             int randomNumber = Randoms.pickNumberInRange(1,9);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
         }
+
         return computer;
+    }
+
+    public static void isThree(String numbers) throws IllegalArgumentException {
+        if(numbers.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void hasDuplicate(String numbers) throws IllegalArgumentException {
+        if (numbers.charAt(0) == numbers.charAt(1)
+                || numbers.charAt(0) == numbers.charAt(2)
+                || numbers.charAt(1) == numbers.charAt(2)) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static List<Integer> player(String numbers) throws IllegalArgumentException {
+        String[] split = numbers.split("");
+
+        List<Integer> player = new ArrayList<>();
+
+        for(int i=0; i<numbers.length(); i++) {
+            try {
+                Integer number = Integer.parseInt(split[i]);
+                player.add(number);
+            } catch (Exception e) {
+                throw new IllegalArgumentException();
+            }
+        }
+
+        return player;
     }
 
 }
