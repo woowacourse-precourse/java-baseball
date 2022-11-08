610f5a7f5400cc44da1127faa788d43cb8b1e266
saewoo1 <wooozooo1123@gmail.com>
2022-11-08 09:55:40 +0900

    refactor(ContinueOrStopTest): 케이스 분류

--- src/test/java/baseball/ContinueOrStopTest.java
+++ src/test/java/baseball/ContinueOrStopTest.java
@@ -6,25 +6,43 @@ import org.assertj.core.api.Assertions;
 class ContinueOrStopTest {
 
   @Test
-  void choice() {
-    String testCase1 = "a";
-    String testCase2 = "12";
-    String testCase3 = "11";
-    String testCase4 = "22";
+  void choice_throw_test_짧은_숫자가_들어온_경우() {
     String testCase5 = "3";
-    Assertions.assertThat(ContinueOrStop.choice("1")).isEqualTo(true);
-    Assertions.assertThat(ContinueOrStop.choice("2")).isEqualTo(false);
-    Assertions.assertThatThrownBy(() -> ContinueOrStop.choice(testCase1))
-            .isInstanceOf(IllegalArgumentException.class);
-    Assertions.assertThatThrownBy(() -> ContinueOrStop.choice(testCase2))
-            .isInstanceOf(IllegalArgumentException.class);
-    Assertions.assertThatThrownBy(() -> ContinueOrStop.choice(testCase3))
-            .isInstanceOf(IllegalArgumentException.class);
-    Assertions.assertThatThrownBy(() -> ContinueOrStop.choice(testCase4))
-            .isInstanceOf(IllegalArgumentException.class);
+
     Assertions.assertThatThrownBy(() -> ContinueOrStop.choice(testCase5))
             .isInstanceOf(IllegalArgumentException.class);
   }
 
+  @Test
+  void choice_throw_test_긴_숫자가_들어온_경우() {
+    String testCase2 = "12";
 
+    Assertions.assertThatThrownBy(() -> ContinueOrStop.choice(testCase2))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void choice_throw_test_중복된_숫자가_들어온_경우() {
+    String testCase3 = "11";
+    String testCase4 = "22";
+
+    Assertions.assertThatThrownBy(() -> ContinueOrStop.choice(testCase3))
+            .isInstanceOf(IllegalArgumentException.class);
+    Assertions.assertThatThrownBy(() -> ContinueOrStop.choice(testCase4))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void choice_throw_test_숫자가_아닌_경우() {
+    String testCase1 = "a";
+
+    Assertions.assertThatThrownBy(() -> ContinueOrStop.choice(testCase1))
+            .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  @Test
+  void choice_true_false_판별() {
+    Assertions.assertThat(ContinueOrStop.choice("1")).isEqualTo(true);
+    Assertions.assertThat(ContinueOrStop.choice("2")).isEqualTo(false);
+  }
 }
