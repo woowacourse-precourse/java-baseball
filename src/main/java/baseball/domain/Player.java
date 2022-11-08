80f40a5dd77ca3558db8c1b71fca0849621305b7
yuntaeLee <leeyt1201@gmail.com>
2022-11-08 13:20:04 +0900

    feat: Player 생성자 구현

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -1,4 +1,11 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class Player {
+    private final List<Integer> playerNumbers;
+
+    public Player(List<Integer> playerNumbers) {
+        this.playerNumbers = playerNumbers;
+    }
 }
