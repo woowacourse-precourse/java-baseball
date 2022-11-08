4963f158ac6e49517b86543fb8d3217cdd395c4b
KJE <12181501@inha.edu>
2022-11-08 15:50:16 +0900

    feat: changeInput 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -32,4 +32,21 @@ public class Application {
         }
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public static List<Integer> changeInput(String userInput) {
+        if (userInput.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+        for (int i = 0; i < 3; i++) {
+            if (userInput.charAt(i) < '0' || userInput.charAt(i) > '9') {
+                throw new IllegalArgumentException();
+            }
+        }
+        List<Integer> userInputList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            userInputList.add(userInput.charAt(i) - '0');
+        }
+        return userInputList;
+    }
+
 }
