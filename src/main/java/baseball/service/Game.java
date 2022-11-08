467fda17fbe795fe2a9d84ad5ed53223f8d0a34d
footdev <ekclstkfka5@gmail.com>
2022-11-08 15:44:44 +0900

    feat: 전체적인 게임 흐름 구현
    전체적인 게임의 흐름을 책임지는 Game 클래스의 구현 완료

--- src/main/java/baseball/service/Game.java
+++ src/main/java/baseball/service/Game.java
@@ -1,33 +1,69 @@
 package baseball.service;
 
 import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.List;
 
 public class Game {
+    private static final int NOTHING = 0;
     private static final int RESTART = 1;
     private static final int EXIT = 2;
+    private static final int SUCCESS = 3;
 
-    private Rule rule;
+    private final Rule rule;
+    private boolean isEnd;
 
     public Game() {
         this.rule = new Rule();
+        this.isEnd = false;
     }
 
     public void run() {
         InputView.printStartMessage();
-        init();
-        process();
-        reStrart();
+        while (true) {
+            isEnd = process();
+            if(isEnd) {
+                return;
+            }
+        }
+
     }
 
     private void init() {
         rule.generateRandomNumber();
     }
 
-    private boolean process() {
-
+    private List<Integer> getResult() {
+        rule.getNumber();
+        return rule.decideStrikeOrBall();
     }
 
-    private int reStrart() {
+    private boolean process() {
+        init();
+        List<Integer> result = getResult();
+        //다 맞았다면
+        if (result.get(0) == SUCCESS) {
+            int reStartFlag = processSucess();
+            if (reStartFlag == EXIT) {
+                return true;
+            }
+            if (reStartFlag == RESTART) {
+                return false;
+            }
+        }
 
+        if(result.get(0) == NOTHING && result.get(1) == NOTHING) {
+            OutputView.printNothingMessage();
+            return false;
+        }
+
+        OutputView.printHintMessage();
+        return false;
+    }
+
+    private int processSucess() {
+        OutputView.printSuccessMessage();
+        return InputView.reStart();
     }
 }
