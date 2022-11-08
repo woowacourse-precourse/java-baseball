caabd07e528b2c00d2f1f5174eed376807480bf7
2021110806 <2021110806@dgu.ac.kr>
2022-11-08 21:48:36 +0900

    style($Application):add horizontal blank

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -32,12 +32,14 @@ public class Application {
 
     public static List<Integer> makeAnswer() {
         List<Integer> answer = new ArrayList<>();
+
         while (answer.size()<3) {
             int pickedNumber = pickNumberInRange(1,9);
             if(!answer.contains(pickedNumber)) {
                 answer.add(pickedNumber);
             }
         }
+
         return answer;
     }
 
@@ -45,31 +47,39 @@ public class Application {
         System.out.println("숫자를 입력해주세요 : ");
         String userInputString = readLine();
         List<Integer> userInput = new ArrayList<>();
+
         checkInputLength(userInputString);
+
         for (int i=0; i<userInputString.length(); i++) {
             userInput.add(Character.getNumericValue(userInputString.charAt(i)));
         }
+
         checkInputValue(userInput);
+
         return userInput;
     }
 
     public static int checkBall (List<Integer> answer, List<Integer> userInput) {
         int howManyBall = 0;
+
         for (int i=0; i<3; i++) {
             if (answer.contains(userInput.get(i))) {
                 howManyBall++;
             }
         }
+
         return howManyBall;
     }
 
     public static int checkStrike (List<Integer> answer, List<Integer> userInput) {
         int howManyStrike = 0;
+
         for (int i=0; i<userInput.size(); i++) {
             if (answer.get(i).equals(userInput.get(i))) {
                 howManyStrike++;
             }
         }
+
         return howManyStrike;
     }
 
@@ -91,20 +101,26 @@ public class Application {
     public static boolean returnAnswer (List<Integer> answer, List<Integer> userInput) {
         int ball = checkBall(answer,userInput);
         int strike = checkStrike(answer,userInput);
+
         ball = ball-strike;
         printStrikeAndBall(ball,strike);
+
         return strike==3;
     }
 
     public static boolean endGame() {
         boolean quit = false;
+
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String continueOrQuit = readLine();
+
         checkEndGameInput(continueOrQuit);
+
         if (continueOrQuit.equals("2")) {
             quit = true;
         }
+
         return quit;
     }
 
@@ -112,7 +128,9 @@ public class Application {
         List<Integer> answer = makeAnswer();
         boolean isCorrect;
         boolean quitGame;
+
         System.out.println("숫자 야구 게임을 시작합니다.");
+
         while(true) {
             List<Integer> userInput = getUserInput();
             isCorrect = returnAnswer(answer,userInput);
@@ -121,6 +139,7 @@ public class Application {
                 break;
             }
         }
+
         return quitGame;
     }
 
