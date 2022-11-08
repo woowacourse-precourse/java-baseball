4ad848686f09250bffc2872e1cd757a779a50408
neppiness <0414kjh@naver.com>
2022-11-08 12:34:25 +0900

    test: modify 게임_후_입력_예외_테스트 (src/test/java/baseball)
    Modify 게임_후_입력_예외_테스트 correspond to refactoring
    - execution code class is used
    - validate method replace validateCode method

--- src/test/java/baseball/ValidateMethodTest.java
+++ src/test/java/baseball/ValidateMethodTest.java
@@ -4,27 +4,30 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-class ValidateCodeMethodTest {
+class ValidateMethodTest {
     @Test
     void 게임_후_입력_예외_테스트1() {
-        String executionCode = "3";
-        assertThatThrownBy(() -> Application.validateCode(executionCode))
+        Application.ExecutionCode executionCode = new Application.ExecutionCode();
+        executionCode.code = "3";
+        assertThatThrownBy(() -> executionCode.validate())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Execution code must be 1 or 2");
     }
 
     @Test
     void 게임_후_입력_예외_테스트2() {
-        String executionCode = "neppiness";
-        assertThatThrownBy(() -> Application.validateCode(executionCode))
+        Application.ExecutionCode executionCode = new Application.ExecutionCode();
+        executionCode.code = "neppiness";
+        assertThatThrownBy(() -> executionCode.validate())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Execution code must be 1 or 2");
     }
 
     @Test
     void 게임_후_입력_예외_테스트3() {
-        String executionCode = "0";
-        assertThatThrownBy(() -> Application.validateCode(executionCode))
+        Application.ExecutionCode executionCode = new Application.ExecutionCode();
+        executionCode.code = "0";
+        assertThatThrownBy(() -> executionCode.validate())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("Execution code must be 1 or 2");
     }
