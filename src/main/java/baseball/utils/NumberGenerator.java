1497218244b59351656c80f9f8a0e48394e4e764
LEE-sh1673 <lsh901673@gmail.com>
2022-11-08 20:53:25 +0900

    refactor(numberGenerator): 원시값을 상수로 포장

--- src/main/java/baseball/utils/NumberGenerator.java
+++ src/main/java/baseball/utils/NumberGenerator.java
@@ -7,11 +7,17 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberGenerator {
 
+    private static final int MAX_NUMBERS_SIZE = 3;
+
+    private static final int MIN_NUMBER = 1;
+
+    private static final int MAX_NUMBER = 9;
+
     public static List<Integer> createNonDuplicateNumbers() {
         List<Integer> numbers = new ArrayList<>();
 
-        while (numbers.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
+        while (numbers.size() < MAX_NUMBERS_SIZE) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
             if (!numbers.contains(randomNumber)) {
                 numbers.add(randomNumber);
             }
