d2400b8d3404e1ad1edd93edd04d1f9d0b509893
3juhwan <13selfesteem91@naver.com>
2022-11-08 18:45:51 +0900

    feat: add system out words

---
+++ src/main/java/baseball/Game.java
@@ -0,0 +1,17 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Game {
+    private static User user;
+    private static Computer computer;
+
+    private final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private final String USER_NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private final String BALL_MESSAGE = "볼";
+    private final String STRIKE_MESSAGE = "스트라이크";
+    private final String NOTHING_MESSAGE = "낫싱";
+    private final String END_CONDITION = "3스트라이크";
+    
+}
