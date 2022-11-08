b5eb7888026c530877b81990fd62e036ed57c0a1
knh4437 <knh4437@naver.com>
2022-11-08 16:38:39 +0900

    feat: add decide function about branch and termination based by doc\README
    Add branch function that compare result
    - result means that quit the game if there are 3 strikes
    
    Add termination function that continue with new game
    - String 1 means restart the game, String 2 means completely quit the game
    
    fix: wrong formula (getCompareResult)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,18 +13,30 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 숫자 비교, 조건 비교, 종료 비교
+        String newgame = "1";
         System.out.println("숫자 야구 게임을 시작합니다.");
-        game();
+        while (newgame.equals("1")) {
+            List<Integer> computer = setComputerNumber();
+            game(computer);
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            newgame = Console.readLine();
+            if (newgame.equals("2"))
+                System.out.println("게임 종료");
+        }
     }
 
-    public static void game() {
-        List<Integer> computer = new ArrayList<>();
-        List<Integer> user = new ArrayList<>();
-        HashMap<String, Integer> resultMap = new HashMap<>();
-        computer = setComputerNumber();
-        System.out.println("숫자를 입력해주세요 : ");
-        user = setUserNumber();
-        resultMap = getCompareResult(user, computer);
+    public static void game(List<Integer> computer) {
+        while (true) {
+            List<Integer> user;
+            HashMap<String, Integer> resultMap;
+            System.out.println("숫자를 입력해주세요 : ");
+            user = setUserNumber();
+            resultMap = getCompareResult(user, computer);
+            printResult(resultMap);
+            if (isAllCorrect(resultMap)) {
+                break;
+            }
+        }
     }
 
     public static List<Integer> setComputerNumber() {
@@ -53,25 +65,45 @@ public class Application {
 
     public static HashMap<String, Integer> setResultMap() {
         HashMap<String, Integer> initalMap = new HashMap<>();
-        initalMap.put("스트라이크", 0);
-        initalMap.put("볼", 0);
-        initalMap.put("낫싱", 0);
+        initalMap.put("strike", 0);
+        initalMap.put("ball", 0);
+        initalMap.put("nothing", 0);
         return initalMap;
     }
 
     public static HashMap<String, Integer> getCompareResult(List<Integer> user, List<Integer> computer) {
         HashMap<String, Integer> resultMap = setResultMap();
         for (int userNumber : user) {
-            if (computer.contains(userNumber)) {
-                int userIndex = user.indexOf(userNumber);
-                if (computer.indexOf(userNumber) == userIndex)
-                    resultMap.put("스트라이크", resultMap.get("스트라이크") + 1);
-                else
-                    resultMap.put("볼", resultMap.get("볼") + 1);
-                resultMap.put("낫싱", 1);
-            }
+            int userIndex = user.indexOf(userNumber);
+            if (computer.contains(userNumber) && computer.indexOf(userNumber) == userIndex)
+                resultMap.put("strike", resultMap.get("strike") + 1);
+            else if (computer.contains(userNumber) && computer.indexOf(userNumber) != userIndex)
+                resultMap.put("ball", resultMap.get("ball") + 1);
+            else resultMap.put("nothing", resultMap.get("nothing") + 1);
         }
         return resultMap;
+    }
+
+    public static void printResult(HashMap<String, Integer> resultMap) {
+        if (resultMap.get("nothing") == 3)
+            System.out.print("낫싱");
+        else {
+            if (resultMap.get("ball") > 0) {
+                System.out.print(resultMap.get("ball") + "볼 ");
+            }
+            if (resultMap.get("strike") > 0) {
+                System.out.print(resultMap.get("strike") + "스트라이크");
+            }
+        }
+        System.out.println();
+    }
+
+    public static boolean isAllCorrect(HashMap<String, Integer> resultMap) {
+        if (resultMap.get("strike") == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
     }
 
 }
