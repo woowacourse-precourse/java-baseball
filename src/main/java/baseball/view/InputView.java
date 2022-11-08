615d3afe11300e515ccd17f25924a3f7dec5a164
Sieun Park <psieun0606@gmail.com>
2022-11-08 17:35:38 +0900

    refactor: 출력 문구 추가 및 상수로 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,10 +1,15 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
     private int RESTART_NUM = 1;
     private int EXIT_NUM = 2;
     private String GAMESTART = "숫자 야구 게임을 시작합니다.";
     private String INPUT_USERNUM = "숫자를 입력해주세요 : ";
+    private String restartOrExit = String.format("게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.",
+            RESTART_NUM, EXIT_NUM);
+    private String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public void GameStart() {
         System.out.println(GAMESTART);
@@ -14,9 +19,12 @@ public class InputView {
         System.out.println(INPUT_USERNUM);
     }
 
-    public void RestartOrExit() {
-        String restartOrExit = String.format("게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.", RESTART_NUM, EXIT_NUM);
+    public String RestartOrExit() {
+        System.out.println(GAME_END);
         System.out.println(restartOrExit);
+        String userInputNum = Console.readLine();
+
+        return userInputNum;
     }
 
 }
