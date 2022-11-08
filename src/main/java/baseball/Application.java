41c0ed1cbc6cc435c11ac7c69ce8b0df659e5a6b
ShinKwang <sklee0206@naver.com>
2022-11-08 15:36:58 +0900

    refactor: Change location of computerNumberList
    computerNumberList의 위치는 게임 시작 이후 do while문 내부로 옮긴다. 게임 시작 이후 게임이 재시작하면 값이 변해야하기 때문이다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,7 +10,6 @@ import java.util.stream.Collectors;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        List<Integer> computerNumberList = createComputerNumberList();
         System.out.println("숫자 야구 게임을 시작합니다.");
 
         String restartNumber;
