d98a1e546140fe25b0de0c684971dc889218421d
YongNyeo <won05121@naver.com>
2022-11-08 22:24:48 +0900

    feat: 상대방(컴퓨터) 값 랜덤 생성 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -39,8 +39,16 @@ public class Application {
     }
 
     public static List<Integer> computerNumber() {
-        return computerNum;
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
     }
+
     public static List<Integer> myNumber() {
         return myNum;
     }
