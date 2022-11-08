d2285fbe68ba2ee4a05582544759df645075bdec
Noah <shgustmd6865@naver.com>
2022-11-08 21:41:11 +0900

    Feat(UserTest) : 사용자 입력에 대한 테스트 - 입력에 대해서 자릿수별로 각기 다른 숫자가 입력되었는지 확인

--- src/test/java/baseball/domain/UserTest.java
+++ src/test/java/baseball/domain/UserTest.java
@@ -17,4 +17,16 @@ class UserTest {
         this.user = new User();
         assertThat(user).isInstanceOf(User.class);
     }
+
+    @Test
+    @DisplayName("입력에 대해서 자릿수별로 각기 다른 숫자가 입력되었는지 확인")
+    public void isInputElementsUnique() {
+        String input = "111";
+
+        assertThatThrownBy(() -> {
+            user.isValidInput(input);
+        }).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자릿수별로 각기 다른 숫자를 입력해야 합니다.");
+    }
+
 }
