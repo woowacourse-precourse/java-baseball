a9fb56f40ebab11602fa19cfc1a32d54a02d8fcb
myungjunlee <dlaudwns789@gmail.com>
2022-11-08 18:39:22 +0900

    feat(service): add function to get user numbers

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -1,17 +1,29 @@
 package baseball.service;
 
 import baseball.vo.Master;
+import baseball.vo.User;
+import baseball.view.RequestMessage;
 import baseball.utils.NumberGenerator;
+import baseball.utils.Validator;
+
+import camp.nextstep.edu.missionutils.Console;
 
 public class GameService {
 
-    int digit;
     Master master;
+    User user;
+    Validator validator = new Validator();
+    RequestMessage requestMessage = new RequestMessage();
 
     public void setGame(int digit, int minNumber, int maxNumber) {
-        this.digit = digit;
         master = new Master(NumberGenerator.createRandomNumbers(digit, minNumber, maxNumber));
         master.initCount();
+    }
+
+    public void getUserNumbers() {
+        requestMessage.requestInputNumbers();
+        String userInput = Console.readLine();
+        user = new User(validator.validateUserInput(userInput));
     }
 
 }
