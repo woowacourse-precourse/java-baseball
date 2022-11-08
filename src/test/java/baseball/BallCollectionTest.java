c54804f00e8f9dff54a6f0d77bcc56119a9c67e7
JunginPark4364 <wjddlsee@gmail.com>
2022-11-08 16:56:08 +0900

    test(BallCollectionTest): add test that validate balls
    Add test that validate balls.
    - fail to create ball collection if size is not BALL_COUNT
    - fail to create ball collection if there are duplicate numbers
    - success in create ball collection

--- src/test/java/baseball/BallCollectionTest.java
+++ src/test/java/baseball/BallCollectionTest.java
@@ -3,7 +3,11 @@ package baseball;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class BallCollectionTest {
     @Test
@@ -11,5 +15,33 @@ public class BallCollectionTest {
     public void SuccessInCreateComputerNumbers() {
         BallCollection computerNumbers = BallCollection.createComputerNumbers();
         assertThat(computerNumbers.size()).isEqualTo(BallCollection.BALL_COUNT);
+    }
+
+    @Test
+    @DisplayName("세자리 숫자가 아닌경우 예외처리")
+    public void FailToCreateBallCollectionIfSizeIsNotBALL_COUNT() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+
+        assertThatThrownBy(() -> new BallCollection(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("중복되는 숫자가 있는 경우 예외처리")
+    public void FailToCreateBallCollectionIfThereAreDuplicateNumbers() {
+        List<Integer> numbers = Arrays.asList(2, 1, 2);
+
+        assertThatThrownBy(() -> new BallCollection(numbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("중복없는 3자리 숫자인 정상입력")
+    public void SuccessInCreateBallCollection() {
+        List<Integer> numbers = Arrays.asList(1, 2, 3);
+        BallCollection balls = new BallCollection(numbers);
+
+        assertThat(balls).isInstanceOf(BallCollection.class);
+        assertThat(balls.size()).isEqualTo(BallCollection.BALL_COUNT);
     }
 }
