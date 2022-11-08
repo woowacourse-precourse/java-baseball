92f20c00d30a9421ce3a7075441c8394f8924316
RadSparkle <wwwartg1@naver.com>
2022-11-08 21:57:29 +0900

    FEAT : 겹치지않는 랜덤한숫사 3개를 추출하는 기능구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,6 +14,25 @@ public class Application {
 
         String numbers = Console.readLine();
 
+        List result = RandomNum();
+        System.out.println(result);
+
+
+    }
+
+    //랜덤 세자리 수 생성 함수
+    public static List RandomNum(){
+        List numList = new ArrayList<>();
+        for(int i=0; i<3; i++) {
+            int num = Randoms.pickNumberInRange(1, 9);
+                if (numList.contains(num)) {
+                    i--;
+                } else if (!numList.contains(num)) {
+                    numList.add(num);
+                }
+
+        }
+        return numList;
     }
 
     public static boolean checkNumber (int test) {
