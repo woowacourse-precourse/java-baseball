25dd9b32599702fe9e0438bad919667fcded5345
hum02 <ehdgma02@gmail.com>
2022-11-08 12:49:29 +0900

    feat($validation): 컴퓨터 숫자 예상시의 입력값 검증

--- src/main/java/baseball/util/Input.java
+++ src/main/java/baseball/util/Input.java
@@ -4,6 +4,7 @@ import camp.nextstep.edu.missionutils.Console;
 
 import static baseball.type.GameFlow.GAME_RESTART;
 import static baseball.type.GameFlow.GAME_END;
+import static baseball.type.BaseballBound.*;
 
 
 public class Input {
@@ -12,6 +13,7 @@ public class Input {
 
     public static String inputRestartFlag() throws IllegalArgumentException {
         String restartFlag = Console.readLine();
+
         //validataion
         if (!((restartFlag.equals(RESTART)) || (restartFlag.equals(END)))) {
             throw new IllegalArgumentException("잘못된 입력값입니다. 1이나 2를 입력해주세요");
@@ -20,7 +22,19 @@ public class Input {
         return restartFlag;
     }
 
-    public static String inputPlayerNumber() {
-        return Console.readLine();
+    public static String inputPlayerNumber() throws IllegalArgumentException {
+        String playerNumber = Console.readLine();
+
+        //validation
+        if (playerNumber.length() != BASEBALL_MAX_SIZE.getValue()) {
+            throw new IllegalArgumentException("잘못된 입력값입니다. 3자리의 수를 입력해주세요");
+        }
+        for(int i=0; i<playerNumber.length(); i++) {
+            if (!(('1' <= playerNumber.charAt(i)) && (playerNumber.charAt(i) <= '9'))) {
+                throw new IllegalArgumentException("잘못된 입력값입니다. 1-9로 이뤄진 수를 입력해주세요");
+            }
+        }
+
+        return playerNumber;
     }
 }
