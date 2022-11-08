c3dff78df6f0d91db64e17cba676233b69d7072c
baksohyeon <bori12370@gmail.com>
2022-11-08 15:07:10 +0900

    test: 게임 재시작을 정하는 메서드 테스트 코드 추가

--- src/test/java/baseball/ExecuteGameTest.java
+++ src/test/java/baseball/ExecuteGameTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
@@ -8,38 +9,64 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class ExecuteGameTest {
     @Test
-    void repeatTest() {
-        ByteArrayInputStream inputStream = new ByteArrayInputStream("3".getBytes());
+    void insert_other_number_and_finish() {
+        String INPUT = "3\n2\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(INPUT.getBytes());
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(outputStream);
         ExecuteGame executeGame = new ExecuteGame(inputStream, ps);
-        executeGame.canRepeatGame();
+
+        boolean result = executeGame.canRepeatGame();
         String output = outputStream.toString();
-        System.out.println(output);
-//        assertThat(output).isEqualTo("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n" +
-//                "1 또는 2를 입력하세요.\n");
+        assertThat(output).isEqualTo("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n" +
+                "1 또는 2를 입력해주세요\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." +
+                "\n");
+        assertThat(result).isFalse();
     }
 
     @Test
-    void repeatTest2() {
-        ByteArrayInputStream inputStream = new ByteArrayInputStream("2".getBytes());
+    void insert_other_number_and_repeat() {
+        String INPUT = "3\n1\n";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(INPUT.getBytes());
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(outputStream);
         ExecuteGame executeGame = new ExecuteGame(inputStream, ps);
-        executeGame.canRepeatGame();
+
+        boolean result = executeGame.canRepeatGame();
         String output = outputStream.toString();
-        assertThat(executeGame.canRepeatGame()).isFalse();
+        assertThat(output).isEqualTo("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n" +
+                "1 또는 2를 입력해주세요\n" +
+                "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." +
+                "\n");
+        assertThat(result).isTrue();
     }
 
     @Test
-    void repeatTest3() {
-        ByteArrayInputStream inputStream = new ByteArrayInputStream("1".getBytes());
+    void finish_game() {
+        String FINISH_GAME = "2";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(FINISH_GAME.getBytes());
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         PrintStream ps = new PrintStream(outputStream);
         ExecuteGame executeGame = new ExecuteGame(inputStream, ps);
-        executeGame.canRepeatGame();
-        System.out.println(executeGame.canRepeatGame());
-        assertThat(executeGame.canRepeatGame()).isTrue();
+        boolean result = executeGame.canRepeatGame();
+        String output = outputStream.toString();
+        assertThat(output).isEqualTo("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void repeat_game() {
+        String REPEAT_VALUE = "1";
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(REPEAT_VALUE.getBytes());
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream ps = new PrintStream(outputStream);
+
+        ExecuteGame executeGame = new ExecuteGame(inputStream, ps);
+        boolean result = executeGame.canRepeatGame();
+        String output = outputStream.toString();
+        assertThat(output).isEqualTo("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+        assertThat(result).isTrue();
     }
 }
 
