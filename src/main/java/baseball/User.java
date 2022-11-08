3db4071adbb740ee0f2edfe2bb02673e30f6cabe
hanuel <ezzeezz@naver.com>
2022-11-08 18:52:56 +0900

    feat: Add getting user's numlist part

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,11 +1,36 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class User {
-    public void createNumList() {
+
+    public static final String INPUT_NUM_MENT = "숫자를 입력해주세요 : ";
+    private List<Integer> numList;
+
+    public User() {
+    }
+
+    public void createNumList() throws IllegalArgumentException {
+        System.out.print(INPUT_NUM_MENT);
+        String str = Console.readLine();
+        if (exceptionCheck(str)) {
+            throw new IllegalArgumentException();
+        }
+
+        List<String> strings = Arrays.asList(str.split(""));
+        List<Integer> inputNumList = strings.stream().map(Integer::parseInt).collect(Collectors.toList());
+
+        this.numList = inputNumList;
+    }
+
+    private boolean exceptionCheck(String str) {
     }
 
     public List<Integer> getNumList() {
+        return numList;
     }
 }
