8fa21f3032188a32d25b985f68b5d32591ea042a
youngkyunIm <yee0230@naver.com>
2022-11-08 15:25:43 +0900

    style(Users) : change

--- src/main/java/baseball/Users.java
+++ src/main/java/baseball/Users.java
@@ -17,11 +17,11 @@ public class Users {
     }
 
     public List<Integer> getInputList() {
-        List<Integer> list = new ArrayList<>();
+        List<Integer> inputList = new ArrayList<>();
         for (int i = 0; i < this.input.length(); i++) {
-            list.add(Integer.valueOf(String.valueOf(this.input.charAt(i))));
+            inputList.add(Integer.valueOf(String.valueOf(this.input.charAt(i))));
         }
-        return list;
+        return inputList;
     }
 
     private void CheckRange(String input) {
