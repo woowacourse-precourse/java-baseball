eaab179872667cd96be2df85d0bc3872174d87e6
TeddyYeoni <po99027@naver.com>
2022-11-08 22:03:50 +0900

    임의로 서로 다른 3자리 수를 선택하는 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+	
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    	
     }
+    
+    public static List<Integer> computer() {
+    	List<Integer> computer = new ArrayList<Integer>();
+    	while(computer.size() < 3) {
+    		int RandomNumber = Randoms.pickNumberInRange(1, 9);
+    		if(!computer.contains(RandomNumber)) {
+    			computer.add(RandomNumber);
+    		}
+    	}
+		return computer;
+	}
 }
