59e8b3a145e3a80be362461a5cc81f78731599c2
Laeng <Laeng@users.noreply.github.com>
2022-11-08 21:24:00 +0900

    refactor(SelectNumberResultView): View 역할에 충실하도록 변경

--- src/main/java/baseball/view/SelectNumberResultView.java
+++ src/main/java/baseball/view/SelectNumberResultView.java
@@ -4,27 +4,14 @@ import baseball.service.GameService;
 import baseball.vo.Scoreboard;
 
 public class SelectNumberResultView extends View {
-    private final int number;
-    private Boolean response = false;
+    private final Scoreboard scoreboard;
 
-    public SelectNumberResultView(int number) {
-        this.number = number;
+    public SelectNumberResultView(Scoreboard scoreboard) {
+        this.scoreboard = scoreboard;
     }
 
     @Override
     public void show() {
-        GameService service = new GameService();
-        Scoreboard scoreboard = service.compareComputerNumberWith(this.number);
-
-        if (scoreboard.getStrikePoint() == 3 && scoreboard.getBallPoint() == 0) {
-            this.response = service.deleteComputerNumber(number);
-        }
-
-        printLine(scoreboard.toString());
-    }
-
-    @Override
-    public boolean getResponseToBoolean() {
-        return this.response;
+        printLine(this.scoreboard.toString());
     }
 }
