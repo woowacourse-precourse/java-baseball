a0ae125b97970e668f795a35336f04eb0db04710
yuntaeLee <leeyt1201@gmail.com>
2022-11-08 14:00:47 +0900

    feat: Player Input String을 Integer 타입의 List로 변환 기능 구현

---
+++ src/main/java/baseball/utils/Parser.java
@@ -0,0 +1,20 @@
+package baseball.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Parser {
+
+    public List<Integer> parsePlayerNumbers(String input) {
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            answer.add(convertCharToInt(input, i));
+        }
+
+        return answer;
+    }
+
+    private int convertCharToInt(String input, int index) {
+        return input.charAt(index) - '0';
+    }
+}
