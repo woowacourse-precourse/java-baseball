b1c4f4caba0fe3bca489150b12ffcb52f3fd0c36
gilyeon00 <12venus08@naver.com>
2022-11-08 16:33:47 +0900

    refactor: getRandomNum 메서드 pubic static으로 변경

--- src/main/java/baseball/Game/Number.java
+++ src/main/java/baseball/Game/Number.java
@@ -3,13 +3,9 @@ package baseball.Game;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Number {
-    final int NUM_LEN = 3;
+    final static int NUM_LEN = 3;
 
-    public Number() {
-
-    }
-
-    public int [] getRandomNum(final int len, final int MIN_NUM, final int MAX_NUM) {
+    public static int [] getRandomNum(final int len, final int MIN_NUM, final int MAX_NUM) {
         int[] num = new int[NUM_LEN];
 
         for (int i = 0; i < len; i++){
