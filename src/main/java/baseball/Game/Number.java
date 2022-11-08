405840beaff642cae3811872eb3e690cea7c2471
gilyeon00 <12venus08@naver.com>
2022-11-08 16:16:36 +0900

    refactor: 입력받은 숫자 int[]로 저장 및 메서드 이름 변경

--- src/main/java/baseball/Game/Number.java
+++ src/main/java/baseball/Game/Number.java
@@ -3,15 +3,17 @@ package baseball.Game;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Number {
+    final int NUM_LEN = 3;
+
     public Number() {
 
     }
-    public int getRandomNum(final int len, final int MIN_NUM, final int MAX_NUM) {
-        int num = 0;
+
+    public int [] getRandomNum(final int len, final int MIN_NUM, final int MAX_NUM) {
+        int[] num = new int[NUM_LEN];
 
         for (int i = 0; i < len; i++){
-            num *= 10;
-            num += Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
+            num[i] = Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
         }
         return num;
     }
--- src/main/java/baseball/Game/Vaildate.java
+++ src/main/java/baseball/Game/Vaildate.java
@@ -3,9 +3,9 @@ package baseball.Game;
 public class Vaildate {
     final int NUM_LEN = 3;
 
-    public int userInputVaildate(String input) {
+    public int [] userInputVaildate(String input) {
         checkLen(input, NUM_LEN);
-        int targetInt = castToInt(input);
+        int targetInt [] = vailInput(input);
 
         return targetInt;
     }
@@ -17,16 +17,17 @@ public class Vaildate {
         }
     }
 
-    /* 입력받은 숫자 String to int */
-    public int castToInt(String input) {
-        int targetInt = 0;
+    /* 입력받은 숫자 검증 */
+    public int [] vailInput(String input) {
+        int targetInt[] = new int[NUM_LEN];
+
         for (int i = 0; i < input.length(); i++) {
             if('0' > input.charAt(i) || input.charAt(i) > '9') {
                 throw new IllegalArgumentException();
             }
-            targetInt *= 10;
-            targetInt += Integer.parseInt(input, i);
+            targetInt[i] = input.charAt(i) - '0';
         }
+
         return targetInt;
     }
 
