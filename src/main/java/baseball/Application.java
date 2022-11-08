763a04e5000fd92bd5a599ba4638c7cc6cabf6be
jaewoo <wjdwodn0722@naver.com>
2022-11-08 18:07:29 +0900

    feat : add input number function
    - 숫자를 받는 메소드 추가
    - 숫자의 길이가 유효한지를 판단하는 메소드 추가
    - 숫자가 중복된 값이 있는지 판단하는 메소드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,47 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
     }
+}
+
+class Game {
+
+    public void startGame() {
+
+    }
+
+    public void endGame() {
+
+    }
+
+    public static void makeRandomNumber(Integer[] solutionNumber) {
+
+    }
+
+    public static char[] inputNumber() {
+        String inputString = Console.readLine();
+        isValidLength(inputString);
+        isValidNumber(inputString);
+        return inputString.toCharArray();
+    }
+
+    public static void isValidLength(String input) {
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("입력된 값이 세자리 수가 아닙니다.");
+        }
+    }
+
+    public static void isValidNumber(String input) {
+        char[] inputCharList = input.toCharArray();
+        if (inputCharList[0] == inputCharList[1] ||
+                inputCharList[0] == inputCharList[2] || inputCharList[1] == inputCharList[2]) {
+            throw new IllegalArgumentException("입력된 값에 중복된 숫자가 있습니다.");
+        }
+    }
+
 }
