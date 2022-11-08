9a1514a500437bb2ef4e91d5f713ca813fcd4529
jinju9553 <69393506+jinju9553@users.noreply.github.com>
2022-11-08 13:06:10 +0900

    221108 Refactor: 기능3 - Validator 클래스 분리

---
+++ src/main/java/baseball/service/Validator.java
@@ -0,0 +1,39 @@
+package baseball.service;
+
+import baseball.model.Game;
+import baseball.model.Range;
+
+import java.util.List;
+
+public class Validator {
+
+    public void validate(Game game) {
+        List<Integer> numbers = game.getNumbers();
+
+        int maxCount = Range.valueOf("MAX_COUNT").getValue();
+        int minValue = Range.valueOf("MIN_VALUE").getValue();
+        int maxValue = Range.valueOf("MAX_VALUE").getValue();
+
+        if(numbers.size() < maxCount) {
+            throw new IllegalArgumentException("3개 미만의 숫자를 입력할 수 없습니다.");
+        }
+
+        if(numbers.size() > maxCount) {
+            throw new IllegalArgumentException("3개 초과의 숫자를 입력할 수 없습니다.");
+        }
+
+        for(int num : game.getNumbers()){
+            if (num < minValue || num > maxValue) {
+                throw new IllegalArgumentException("1에서 9 사이의 숫자만 입력할 수 있습니다.");
+            }
+        }
+
+        boolean isDuplicated = numbers.stream()
+                .distinct()
+                .count() != numbers.size();
+
+        if(isDuplicated) {
+            throw new IllegalArgumentException("중복되는 숫자는 입력할 수 없습니다.");
+        }
+    }
+}
