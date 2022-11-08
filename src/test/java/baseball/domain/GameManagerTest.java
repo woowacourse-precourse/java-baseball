8a7d340341dc687f9b2bd173c869623dc8e53e07
hum02 <ehdgma02@gmail.com>
2022-11-08 14:11:43 +0900

    test($validation): 야구숫자 1-9 검증 테스트

--- src/test/java/baseball/domain/GameManagerTest.java
+++ src/test/java/baseball/domain/GameManagerTest.java
@@ -4,8 +4,9 @@ import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class GameManagerTest extends NsTest {
 
@@ -17,6 +18,14 @@ class GameManagerTest extends NsTest {
                     assertThat(output()).contains("게임 종료", "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 },
                 1,4,6,7,8,9
+        );
+    }
+
+    @Test
+    void 예외_1부터9_숫자_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("105"))
+                        .isInstanceOf(IllegalArgumentException.class)
         );
     }
 
