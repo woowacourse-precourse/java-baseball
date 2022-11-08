c2a31b33da3a5941394a48c7197ba97feb9b2fc4
ye1ne <yewon7036@naver.com>
2022-11-08 14:32:17 +0900

    feature : makeStringNumberToList method
    makeStringNumberToList which make String Number to Integer Number List
    return List<Integer> type.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -10,8 +11,34 @@ public class Application {
         // TODO: 프로그램 구현
 
         List<Integer> resultNumber = makeRandomNumber();
+        String inputStringNumber = Console.readLine();
+        calculationNumber(resultNumber, inputStringNumber);
 
+    }
 
+    public static int calculationNumber(List<Integer> resultNumber ,String inputStringNumber){
+        List<Integer> inputNumber = makeStringNumberToList(inputStringNumber);
+        int strike = calculationStrike();
+        int ball = calculationStrike();
+        printResult(strike, ball);
+        return strike;
+    }
+
+    private static List<Integer> makeStringNumberToList(String inputStringNumber) {
+        List<Integer> inputNumber = new ArrayList<>();
+        for(int i=0; i<inputStringNumber.length(); i++){
+            int number = inputStringNumber.charAt(i) - '0';
+            inputNumber.add(number);
+        }
+        return inputNumber;
+    }
+
+    private static void printResult(int strike, int ball) {
+
+    }
+
+    private static int calculationStrike() {
+        return 0;
     }
 
     public static List<Integer> makeRandomNumber() {
