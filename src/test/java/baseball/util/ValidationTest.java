e95f968a650a0e4b4388f775e86fc552885bfb6a
Miseong Kim <miseongk@naver.com>
2022-11-08 18:55:23 +0900

    refactor(ValidationTest): move validation test

---
+++ src/test/java/baseball/util/ValidationTest.java
@@ -0,0 +1,51 @@
+package baseball.util;
+
+import baseball.view.InputView;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ValidationTest {
+
+    void inputFromUser(String input) {
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+    }
+
+    @Test
+    void 문자입력_예외테스트() {
+        inputFromUser("abc");
+        Exception exception = assertThrows(IllegalArgumentException.class, InputView::getNumber);
+        assertThat(exception.getClass()).isEqualTo(IllegalArgumentException.class);
+        assertThat(exception.getMessage()).isEqualTo("숫자를 입력해주세요.");
+    }
+
+    @Test
+    void 숫자_3자리_예외테스트() {
+        inputFromUser("1234");
+        Exception exception = assertThrows(IllegalArgumentException.class, InputView::getNumber);
+        assertThat(exception.getClass()).isEqualTo(IllegalArgumentException.class);
+        assertThat(exception.getMessage()).isEqualTo("숫자 3자리를 입력해주세요.");
+    }
+
+    @Test
+    void 서로다른숫자_예외테스트() {
+        inputFromUser("122");
+        Exception exception = assertThrows(IllegalArgumentException.class, InputView::getNumber);
+        assertThat(exception.getClass()).isEqualTo(IllegalArgumentException.class);
+        assertThat(exception.getMessage()).isEqualTo("서로 다른 숫자 3자리를 입력해주세요.");
+    }
+
+    @Test
+    void 게임종료시_입력_예외테스트() {
+        inputFromUser("3");
+        Exception exception = assertThrows(IllegalArgumentException.class, InputView::isReplay);
+        assertThat(exception.getClass()).isEqualTo(IllegalArgumentException.class);
+        assertThat(exception.getMessage()).isEqualTo("1 또는 2를 입력해주세요.");
+    }
+
+}
--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -13,36 +13,4 @@ class InputViewTest {
         System.setIn(in);
     }
 
-    @Test
-    void 문자입력_예외테스트() {
-        inputFromUser("abc");
-        Exception exception = assertThrows(IllegalArgumentException.class, InputView::getNumber);
-        assertThat(exception.getClass()).isEqualTo(IllegalArgumentException.class);
-        assertThat(exception.getMessage()).isEqualTo("숫자를 입력해주세요.");
-    }
-
-    @Test
-    void 숫자_3자리_예외테스트() {
-        inputFromUser("1234");
-        Exception exception = assertThrows(IllegalArgumentException.class, InputView::getNumber);
-        assertThat(exception.getClass()).isEqualTo(IllegalArgumentException.class);
-        assertThat(exception.getMessage()).isEqualTo("숫자 3자리를 입력해주세요.");
-    }
-
-    @Test
-    void 서로다른숫자_예외테스트() {
-        inputFromUser("122");
-        Exception exception = assertThrows(IllegalArgumentException.class, InputView::getNumber);
-        assertThat(exception.getClass()).isEqualTo(IllegalArgumentException.class);
-        assertThat(exception.getMessage()).isEqualTo("서로 다른 숫자 3자리를 입력해주세요.");
-    }
-
-    @Test
-    void 게임종료시_입력_예외테스트() {
-        inputFromUser("3");
-        Exception exception = assertThrows(IllegalArgumentException.class, InputView::isReplay);
-        assertThat(exception.getClass()).isEqualTo(IllegalArgumentException.class);
-        assertThat(exception.getMessage()).isEqualTo("1 또는 2를 입력해주세요.");
-    }
-
 }
