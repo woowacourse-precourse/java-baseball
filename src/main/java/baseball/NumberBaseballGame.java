5841efe40a44c49f0e2081cdf73cb8f0231b4885
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-08 19:10:28 +0900

    feat: 7. 입력한 숫자에 대한 결과를 출력하는 기능 추가(printResultMessage)

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -86,4 +86,16 @@ public class NumberBaseballGame {
         result.add(ball);
         return result;
     }
+
+    public void printResultMessage(int strike, int ball) {
+        if (strike==0 && ball==0) {
+            System.out.println("낫싱");
+        } else if (strike==0 && ball!=0) {
+            System.out.printf("%d볼\n", ball);
+        } else if (strike!=0 && ball==0) {
+            System.out.printf("%d스트라이크\n", strike);
+        } else {
+            System.out.printf("%d볼 %d스트라이크\n", ball, strike);
+        }
+    }
 }
