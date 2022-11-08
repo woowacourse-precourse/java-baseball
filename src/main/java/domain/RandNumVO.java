df9f99c14449a00029d07970dcfb34ccf41ef302
Identityless <rlandwnd555@naver.com>
2022-11-08 20:19:01 +0900

    fix : 랜덤 수 발생 로직 재정의

--- src/main/java/domain/RandNumVO.java
+++ src/main/java/domain/RandNumVO.java
@@ -10,17 +10,13 @@ public class RandNumVO {
     private final List<Integer> randNum;
 
     public RandNumVO() throws IllegalArgumentException{
-        int nums[] = new int[3];
-        nums[0] = Randoms.pickNumberInRange(1, 9);
-        nums[1] = Randoms.pickNumberInRange(1, 9);
-        while(nums[0] == nums[1]) {
-            nums[1] = Randoms.pickNumberInRange(1, 9);
+        randNum = new ArrayList<>();
+        while (randNum.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randNum.contains(randomNumber)) {
+                randNum.add(randomNumber);
+            }
         }
-        nums[2] = Randoms.pickNumberInRange(1, 9);
-        while(nums[2] == nums[1] || nums[2] == nums[0]) {
-            nums[2] = Randoms.pickNumberInRange(1, 9);
-        }
-        this.randNum = List.of(nums[0], nums[1], nums[2]);
     }
 
     public List<Integer> getRandNum(){
