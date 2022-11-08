75f650eeba1135c033d1a408c65b4e2e468b3047
조병우 <whquddn55@gmail.com>
2022-11-08 10:23:21 +0900

    feat(OutputGenerator): implement print requesting ball number method
    This method just print requesting ball number message to console.
    
    + public void printRequestBallNumber()

--- src/main/java/baseball/utils/OutputGenerator.java
+++ src/main/java/baseball/utils/OutputGenerator.java
@@ -7,4 +7,8 @@ public class OutputGenerator {
     public void printWelcome() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public void printRequestBallNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
 }
