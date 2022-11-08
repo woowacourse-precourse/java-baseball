ba91d209696948f6b2ab17875846d5111457cbcb
ch200203 <ch200205@naver.com>
2022-11-08 22:53:02 +0900

    fix : 종료메시지 추가 및 필요없는 메시지 삭제

--- src/main/java/message/OutputMessage.java
+++ src/main/java/message/OutputMessage.java
@@ -9,11 +9,11 @@ public class OutputMessage {
     private static final String START_MESSAGE = "숫자 야구게임을 시작합니다";
     private static final String USER_INPUT_MESSAGE = "숫자를 입력해 주세요 : ";
     private static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞추셨습니다. 게임 종료";
+    private static final String WIN_MESSAGE = "3개의 숫자를 모두 맞추셨습니다! 게임 종료";
+    private static final String GAME_OUT = "프로그램을 종료합니다.";
 
     private static final String GAME_BALL = "볼";
     private static final String GAME_STRIKE = "스트라이크";
-    private static final String GAME_OUT = "아웃";
     private static final String GAME_NOTHING = "낫싱";
     private StringBuilder sb;
 
@@ -46,7 +46,7 @@ public class OutputMessage {
             System.out.println(GAME_NOTHING);
         } else if(baseBall.getBall() > 0 && baseBall.getStrike() > 0) {
             System.out.println(baseBall.getBall() + GAME_BALL + " " + baseBall.getStrike() + GAME_STRIKE);
-        } else if(baseBall.getBall() == 0 && baseBall.getStrike() > 0 ) {
+        } else if(baseBall.getBall() == 0 && baseBall.getStrike() > 0 && baseBall.getStrike() < 3) {
             System.out.println(baseBall.getStrike() + GAME_STRIKE);
         } else if(baseBall.getBall() > 0 && baseBall.getStrike() == 0 ) {
             System.out.println(baseBall.getBall() + GAME_BALL);
@@ -54,5 +54,13 @@ public class OutputMessage {
             System.out.println(baseBall.getStrike() + GAME_STRIKE);
             System.out.println(WIN_MESSAGE);
         }
+    }
+
+    public void gameRestartInputMessage(int restartControlNum) {
+        System.out.println(restartControlNum);
+    }
+
+    public void gameExitMessage() {
+        System.out.println(GAME_OUT);
     }
 }
