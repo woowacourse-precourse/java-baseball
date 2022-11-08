e83679931900291ef11e57e47406f48478ec91e3
yhkee0404 <yhkee0404@gmail.com>
2022-11-08 23:28:11 +0900

    fix: make functions private

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,11 +13,11 @@ public class Application {
         // TODO: 프로그램 구현
     }
 
-    public static void start() {
+    private static void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public List<Integer> rand() {
+    private List<Integer> rand() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -28,7 +28,7 @@ public class Application {
         return computer;
     }
 
-    public boolean retry() {
+    private boolean retry() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         final int check = in.nextInt();
         if (check == 1) {
@@ -36,4 +36,6 @@ public class Application {
         }
         return false;
     }
+
+
 }
