00bdeecc6d1be92c10f72fae6eb1a8e17180f606
khj1 <74335027+khj1@users.noreply.github.com>
2022-11-08 20:30:31 +0900

    refactor: 게임 안내 메시지 상수 처리

--- src/main/java/baseball/GameMessage.java
+++ src/main/java/baseball/GameMessage.java
@@ -1,20 +1,25 @@
 package baseball;
 
 public class GameMessage {
+	public static final String START_GAME = "숫자 야구 게임을 시작합니다.";
+	public static final String ENTER_NUMBER = "숫자를 입력해주세요 : ";
+	public static final String END_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	public static final String QUIT_OR_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
 	public static void startGame() {
-		System.out.println("숫자 야구 게임을 시작합니다.");
+		System.out.println(START_GAME);
 	}
 
 	public static void enterNumber() {
-		System.out.print("숫자를 입력해주세요 : ");
+		System.out.print(ENTER_NUMBER);
 	}
 
 	public static void endGame() {
-		System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+		System.out.println(END_GAME);
 	}
 
 	public static void quitOrRestart() {
-		System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+		System.out.println(QUIT_OR_RESTART);
 	}
 
 	public static void informResult(GameResult gameResult) {
