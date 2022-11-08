492eed7654ceebcd85c9ac458aee4e25113d9c9e
Noah <shgustmd6865@naver.com>
2022-11-08 20:01:31 +0900

    Feat(ComputerTest) : 상대방(컴퓨터)의 랜덤한 숫자 생성하기 - 각 자리수는 1~9까지의 숫자만 사용
    컴퓨터가 랜덤한 숫자를 생성할 때, 숫자의 각 자리수는 0과 10을 포함하지 않아야 한다는 테스트 코드 입니다

--- src/test/java/baseball/domain/ComputerTest.java
+++ src/test/java/baseball/domain/ComputerTest.java
@@ -14,8 +14,19 @@ class ComputerTest {
     @DisplayName("숫자의 크기는 3으로 고정")
     public void validNumberSize() {
         Computer computer = new Computer();
-        List<Integer> randomNumber = computer.getnerateRandomNumber();
+        List<Integer> randomNumber = computer.generateRandomNumber();
 
         assertThat(randomNumber.size()).isEqualTo(3);
     }
+
+    @Test
+    @DisplayName("각 자리수는 1~9까지의 숫자만 사용")
+    public void randomNumberHasOnlyOneToNine() {
+        Computer computer = new Computer();
+        List<Integer> randomNumber = computer.generateRandomNumber();
+
+        assertThat(randomNumber).isNotIn(0).isNotIn(10);
+    }
+
+
 }
