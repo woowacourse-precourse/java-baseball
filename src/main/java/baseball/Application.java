73423eebd725f9969d9f88a514056f5ad7d15b28
Park Sungchul <try615@naver.com>
2022-11-08 19:22:35 +0900

    feat(Application.java): 오프닝 문구 출력 메소드 추가
    야구 게임의 시작에서 오프닝 문구 출력 역할.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -11,6 +11,10 @@ public class Application {
         private void gameInit() {
             printOpeningStatus();
         }
+
+        private void printOpeningStatus() {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+        }
     }
 
     public static void main(String[] args) {
