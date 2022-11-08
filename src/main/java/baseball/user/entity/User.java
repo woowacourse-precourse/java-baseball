baa84b35f8ce86488248147c45da864f1db194c3
euncheol-kim <klmeuncheol@kakao.com>
2022-11-08 20:18:53 +0900

    feat : User Entity 및 UserService 구현

--- src/main/java/baseball/user/entity/User.java
+++ src/main/java/baseball/user/entity/User.java
@@ -2,19 +2,12 @@ package baseball.user.entity;
 
 public class User {
     private final int NUMBER_LENGTH = 3;
-    private final Integer[] userAnswer;
+
+    public String inputUserNumber;
+    public Integer[] userNumbersArr;
 
     public User(){
-        this.userAnswer = new Integer[NUMBER_LENGTH];
+        this.userNumbersArr = new Integer[NUMBER_LENGTH];
     }
 
-    public void setUserAnswer(String number) {
-        for(int i = 0; i < NUMBER_LENGTH; i++) {
-            userAnswer[i] = Character.getNumericValue(number.charAt(i));
-        }
-    }
-
-    public Integer[] getUserAnswer(){
-        return userAnswer;
-    }
 }
