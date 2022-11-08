b3727acf758a1e245402be8ffc092dcd345ef2f6
JunginPark4364 <wjddlsee@gmail.com>
2022-11-08 16:45:38 +0900

    feat(BallCollection): validate balls
    Validate balls. Have 3 numbers.
    Don't have duplicate numbers.

--- src/main/java/baseball/BallCollection.java
+++ src/main/java/baseball/BallCollection.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class BallCollection {
     public static final int BALL_COUNT = 3;
@@ -11,6 +13,7 @@ public class BallCollection {
     private List<Integer> balls;
 
     public BallCollection(List<Integer> balls) {
+        validateBalls(balls);
         this.balls = balls;
     }
 
@@ -25,6 +28,14 @@ public class BallCollection {
         }
 
         return new BallCollection(balls);
+    }
+
+    private void validateBalls(List<Integer> balls) {
+        Set<Integer> uniqueNumbers = new HashSet<>(balls);
+
+        if (uniqueNumbers.size() != BALL_COUNT) {
+            throw new IllegalArgumentException("올바른 입력이 아닙니다.");
+        }
     }
 
     public int size() {
