b226358018c9d6020b7c4f69ebb64f50366c14b9
youswim <youswim96@gmail.com>
2022-11-08 22:26:09 +0900

    feat: 사용자로부터 입력 받고 검증하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -20,4 +20,34 @@ public class Application {
         }
         return randomInt;
     }
+
+    public static int getInputInt(String input) {
+        validateInput(input);
+        return Integer.parseInt(input);
+    }
+
+    public static void validateInput(String input) {
+
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("잘못된 입력!");
+        }
+
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) < '1' || '9' < input.charAt(i)) {
+                throw new IllegalArgumentException("잘못된 입력!");
+            }
+        }
+
+        for (int i = 0; i < input.length() - 1; i++) {
+            for (int j = i + 1; j < input.length(); j++) {
+                checkCharSame(input.charAt(i), input.charAt(j));
+            }
+        }
+    }
+
+    public static void checkCharSame(char a, char b) {
+        if (a == b) {
+            throw new IllegalArgumentException("잘못된 입력!");
+        }
+    }
 }
