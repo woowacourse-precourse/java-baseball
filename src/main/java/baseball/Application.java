a6aaa601a32b724770ea5ca35f6aa0fb511acbde
yewon <yewon.ux@gmail.com>
2022-11-08 10:41:20 +0900

    feat: random number 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,10 @@
 package baseball;
 
-import java.util.Scanner;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
@@ -12,6 +14,16 @@ public class Application {
         System.out.println("서로다른 세 숫자를 입력하세요");
         String userRandNumber = Console.readLine();
         System.out.println(userRandNumber);
+
+        // make random number
+        List<Integer> correct = new ArrayList<>();
+
+        while (correct.size() < 3) {
+            int randNumber = Randoms.pickNumberInRange(1, 9);
+            if (!correct.contains(randNumber)) {
+                correct.add(randNumber);
+            }
+        }
 
     }
 }
