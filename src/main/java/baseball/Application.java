9fb3d0c4f69d3c3e742a077b44ee99f7b6152928
roasted-penguin <swkwon25@hanyang.ac.kr>
2022-11-08 19:04:30 +0900

    feat makeGame

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,6 +10,17 @@ public class Application {
 
     public static boolean game_over = false;
 
+    public static void makeGame(List<Integer> computer){
+        computer.clear();
+        while (computer.size()<3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        System.out.print("숫자 야구 게임을 시작합니다.");
+        System.out.println();
+    }
 
     public static void main(String[] args) {
         List<Integer> computer = new ArrayList<>();
