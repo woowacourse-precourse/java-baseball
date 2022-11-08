994285c37db1a49f5614c36fda77091902d3c6b2
Hiluxy <sunset.lhj@gmail.com>
2022-11-08 20:51:10 +0900

    Feat 랜덤 숫자 생성기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,26 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Application {
+    static List<Integer> target;
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+    static void setTarget() {
+        Set<Integer> randNumSet = new LinkedHashSet<>();
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (randNumSet.size() < 3) {
+            int pickNum = Randoms.pickNumberInRange(1, 9);
+            if (!randNumSet.contains(pickNum)) {
+                randNumSet.add(pickNum);
+            }
+        }
+        ArrayList<Integer> target = new ArrayList<>(randNumSet);
     }
 }
