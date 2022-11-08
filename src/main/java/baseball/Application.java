343e6698559319a0fa54ba039580fd7f7de3297d
yeada <yeadalm@gmail.com>
2022-11-08 22:32:59 +0900

    feat : 게임 진행 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,6 +8,15 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
+        int game = 1;
+        do{
+            List<Integer> computer = Settings.makeNumber();
+            Play.playGame(computer);
+            System.out.println("게임을 새로 시작하려면 1, 게임을 종료하려면 2를 입력하세요. ");
+            game = Integer.parseInt(Console.readLine());
+            game = Settings.restartException(game);
+        } while(game==1);
 
+        Settings.exitMessage(game);
     }
 }
