b0e8136dc85f9b393f83c0371576886b8efc6a4d
ygreenb <nhj7911@gmail.com>
2022-11-08 19:40:43 +0900

    feat: Constant definition

---
+++ src/main/java/baseball/Constant.java
@@ -0,0 +1,14 @@
+package baseball;
+
+public class Constant {
+    public static final int NUMBER_LIMIT = 3;
+    public static final int RESTART_GAME = 1;
+    public static final int QUIT_GAME = 2;
+
+    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String GAME_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    public static final String USER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static final String USER_INPUT_ERROR = "서로 다른 3자리의 수를 입력하세요.";
+    public static final String GAME_OVER_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    public static final String USER_SELECT_ERROR = "1(재시작) 또는 2(종료)만 입력할 수 있습니다.";
+}
