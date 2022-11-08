c7d05900073f4dcb31d1eeee53474f51ed1b0334
JunH <tjdtls690@gmail.com>
2022-11-08 17:01:05 +0900

    :white_check_mark: test(Domain : BallStatusTest) : 각 BallStatus의 반환 객체가 올바른지 확인하는 테스트 완료

--- src/test/java/baseball/domain/BallStatusTest.java
+++ src/test/java/baseball/domain/BallStatusTest.java
@@ -1,5 +1,8 @@
 package baseball.domain;
 
+import baseball.domain.score.BallScore;
+import baseball.domain.score.Nothing;
+import baseball.domain.score.StrikeScore;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +17,16 @@ class BallStatusTest {
                 () -> assertThat(BallStatus.STRIKE.isNothing()).isFalse(),
                 () -> assertThat(BallStatus.BALL.isNothing()).isFalse(),
                 () -> assertThat(BallStatus.NOTHING.isNothing()).isTrue()
+        );
+    }
+    
+    @Test
+    @DisplayName("반환 객체가 올바른지 확인")
+    void is_correct_score() {
+        assertAll(
+                () -> assertThat(BallStatus.STRIKE.score()).isExactlyInstanceOf(StrikeScore.class),
+                () -> assertThat(BallStatus.BALL.score()).isExactlyInstanceOf(BallScore.class),
+                () -> assertThat(BallStatus.NOTHING.score()).isExactlyInstanceOf(Nothing.class)
         );
     }
 }
