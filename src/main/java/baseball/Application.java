4fb24f790fa35ac815b198ba86f2877cedab1809
knh4437 <knh4437@naver.com>
2022-11-08 15:27:50 +0900

    refactor: function that compare between computer and user
    기존 commit한 기능 재구성
    - 기능별 메소드 분할
    - getCompareResult : 조건식 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.io.BufferedReader;
@@ -12,60 +13,65 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 숫자 비교, 조건 비교, 종료 비교
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        game();
+    }
 
+    public static void game() {
+        List<Integer> computer = new ArrayList<>();
+        List<Integer> user = new ArrayList<>();
+        HashMap<String, Integer> resultMap = new HashMap<>();
+        computer = setComputerNumber();
+        System.out.println("숫자를 입력해주세요 : ");
+        user = setUserNumber();
+        resultMap = getCompareResult(user, computer);
+    }
+
+    public static List<Integer> setComputerNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber))
+            if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
+            }
         }
-
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println("숫자를 입력해주세요 : ");
-
-        List<Integer> user;
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            String answer = br.readLine();
-            int userNumber = Integer.parseInt(answer);
-            user = seperateNumber(userNumber);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        HashMap<String, Integer> resultMap = compareNumber(user, computer);
-
+        return computer;
     }
 
-    private static List<Integer> seperateNumber(int number) {
-        List<Integer> numberList = new ArrayList<>();
-        int hundred = 100;
-        while (hundred > 0) {
-            numberList.add(number/hundred);
-            number %= hundred;
-            hundred /= 10;
+    public static List<Integer> setUserNumber() {
+        List<Integer> user = new ArrayList<>();
+        int number;
+        String answer = Console.readLine();
+        for (String s : answer.split("")) {
+            number = Integer.parseInt(s);
+            if (user.contains(number) || user.size()>3)
+                throw new IllegalArgumentException();
+            user.add(number);
         }
-        return numberList;
+        return user;
     }
 
-    private static HashMap<String, Integer> setHashMap() {
-        HashMap<String, Integer> hashmap = new HashMap<>();
-        hashmap.put("Ball", 0);
-        hashmap.put("Strike", 0);
-        return hashmap;
+    public static HashMap<String, Integer> setResultMap() {
+        HashMap<String, Integer> initalMap = new HashMap<>();
+        initalMap.put("스트라이크", 0);
+        initalMap.put("볼", 0);
+        initalMap.put("낫싱", 0);
+        return initalMap;
     }
 
-    private static HashMap<String, Integer> compareNumber(List<Integer> user, List<Integer> computer) {
-        HashMap<String, Integer> resultMap = setHashMap();
-        for(int userNumber : user) {
-            int userIndex = user.indexOf(userNumber);
-            if (computer.contains(userNumber) && (computer.indexOf(userNumber)==userIndex))
-                resultMap.put("Strike", resultMap.get("Strike") + 1);
-            else if (computer.contains(userNumber))
-                resultMap.put("Ball", resultMap.get("Ball") + 1);
+    public static HashMap<String, Integer> getCompareResult(List<Integer> user, List<Integer> computer) {
+        HashMap<String, Integer> resultMap = setResultMap();
+        for (int userNumber : user) {
+            if (computer.contains(userNumber)) {
+                int userIndex = user.indexOf(userNumber);
+                if (computer.indexOf(userNumber) == userIndex)
+                    resultMap.put("스트라이크", resultMap.get("스트라이크") + 1);
+                else
+                    resultMap.put("볼", resultMap.get("볼") + 1);
+                resultMap.put("낫싱", 1);
+            }
         }
         return resultMap;
     }
-
 
 }
