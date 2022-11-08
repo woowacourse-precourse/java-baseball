7dcfbd38db17b00bbc1870121d6de3d0b254cd66
juno-junho <ssmm0205@naver.com>
2022-11-08 20:16:35 +0900

    feat(model): add method in NumberValidator
    string을 list로 변경해주는 convertStringToList 메서드 생성

--- src/main/java/baseball/model/NumberValidator.java
+++ src/main/java/baseball/model/NumberValidator.java
@@ -1,10 +1,22 @@
 package baseball.model;
 
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static baseball.model.ValidateNumber.*;
 
 public class NumberValidator {
 
     public static boolean checkIfIsNotDuplicatedNum(List<Integer> numbers) {
         return numbers.size() == numbers.stream().distinct().count();
     }
+
+    public static List<Integer> convertStringToList(String startInput) {
+        return startInput
+                .chars()
+                .map(i -> Integer.parseInt(String.valueOf(i)))
+                .boxed()
+                .collect(Collectors.toList());
     }
+
+}
