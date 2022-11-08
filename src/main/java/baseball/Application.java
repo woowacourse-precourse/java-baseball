7e8f149561d492efd3de0c33dccea451d25cdad9
이지선 <jiisuniui@ijiseon-ui-MacBookAir.local>
2022-11-08 20:15:18 +0900

    feat(Application): add main content

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,11 @@
 package baseball;
 
+import baseball.PlayNumberBaseballGame;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        PlayNumberBaseballGame playNumberBaseballGame = new PlayNumberBaseballGame();
+        playNumberBaseballGame.playNumberBaseballOneGame();
     }
 }
