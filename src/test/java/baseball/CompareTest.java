cdea23c0d4a2e00480a3d592a08ed5de918100ce
Brilly-Bohyun <bohyun564@swu.ac.kr>
2022-11-08 21:42:35 +0900

    Refactor: 테스트 코드의 위치를 수정하였습니다.

--- src/test/java/baseball/CompareTest.java
+++ src/test/java/baseball/CompareTest.java
@@ -1,12 +1,11 @@
 package study;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import baseball.Application;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
 import java.util.Arrays;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class CompareTest {
     @Test
