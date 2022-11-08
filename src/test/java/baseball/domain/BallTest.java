c905ed686dd7e651107edfe47ccece44f967b9d7
chanminmaxscaler <jcm04@naver.com>
2022-11-08 19:37:49 +0900

    chore: BallTest 클래스의 공백을 수정한다
    공백을 지운다.

--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-
 class BallTest {
 
     @Nested
