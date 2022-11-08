61890dea87faf342dd7646c990442aac8b5a3838
jonghun <jonghuncu@gmail.com>
2022-11-08 16:22:34 +0900

    Chore : update AnswerNumber 제시된 library 사용하기
    camp.nextstep.edu.missionutils에서 제공하는 Randoms 및 Console API를 사용하여 구현

--- src/main/java/baseball/answerNumber.java
+++ src/main/java/baseball/answerNumber.java
@@ -1,22 +1,34 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class answerNumber {
     public static List<Integer> makeAnswerNumber() {
-        List<Integer> arrLst = new ArrayList<>();
-        List<Integer> answerLst = new ArrayList<>();
+//        List<Integer> arrLst = new ArrayList<>();
+//        List<Integer> answerLst = new ArrayList<>();
 
-        for(int i=1; i<10; i++) {
-            arrLst.add(i);
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
         }
-        Collections.shuffle(arrLst);
-        for(int i=0; i<3; i++) {
-            answerLst.add(i, arrLst.get(i));
-        }
-//        System.out.println(answerLst);
-        return answerLst;
+//        System.out.println(computer);
+
+
+
+//        for(int i=1; i<10; i++) {
+//            arrLst.add(i);
+//        }
+//        Collections.shuffle(arrLst);
+//        for(int i=0; i<3; i++) {
+//            answerLst.add(i, arrLst.get(i));
+//        }
+        return computer;
     }
 }
