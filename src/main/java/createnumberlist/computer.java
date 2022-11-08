0ff471958376588710cc881da7f2844500c08a5b
kkm5291 <kkm23125291@gmail.com>
2022-11-08 11:26:47 +0900

    Feat : createnumberlist 패키지 user,computer 클래스 추가

---
+++ src/main/java/createnumberlist/computer.java
@@ -0,0 +1,19 @@
+package createnumberlist;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class computer {
+    public static List<Integer> computerNumberList() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNum = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNum)) {
+                computer.add(randomNum);
+            }
+        }
+        return computer;
+    }
+}
---
+++ src/main/java/createnumberlist/user.java
@@ -0,0 +1,30 @@
+package createnumberlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static validate.check.checkSize;
+import static validate.check.isNumberValidate;
+
+
+public class user {
+    public static List<Integer> createUserNumberList(String number) {
+        List<Integer> user = new ArrayList<>();
+
+        if (isNumberValidate(number) == false) {
+            throw new IllegalArgumentException("숫자만 입력 가능합니다.");
+        }
+
+        for (int i = 0; i < number.length(); i++) {
+            int numberToInt = Integer.parseInt(String.valueOf(number.charAt(i)));
+            if (user.contains(numberToInt)) {
+                throw new IllegalArgumentException("중복된 숫자는 잘못된 입력값입니다.");
+            }
+            user.add(numberToInt);
+        }
+
+        checkSize(user);
+
+        return user;
+    }
+}
