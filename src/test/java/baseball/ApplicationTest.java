9318940d573efb326107f6050f22d43e91254644
ezzanzzan <cjsdmswjd821@naver.com>
2022-11-08 22:07:20 +0900

    Test: Add should_PrintResult_When_OnlyStrike

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -42,6 +42,17 @@ class ApplicationTest extends NsTest {
         );
     }
 
+    @Test
+    void should_PrintResult_When_OnlyStrike() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("145", "128", "123", "2");
+                    assertThat(output()).contains("1스트라이크", "2스트라이크", "3스트라이크", "게임 종료");
+                },
+                1, 2, 3
+        );
+    }
+
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
