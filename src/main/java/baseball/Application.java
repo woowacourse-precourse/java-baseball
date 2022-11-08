6ae0721111ead7a3de331c6c7c28c1d83611ee67
i960107 <i960107@naver.com>
2022-11-08 15:58:56 +0900

    feat(Application): add feature of playing again or exit

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,19 @@
 package baseball;
 
 public class Application {
+    public static int EXIT_CODE = 2;
+    public static int PLAY_AGAIN_CODE = 1;
+
     public static void main(String[] args) {
+        BaseballOutput.printGameStart();
         Baseball baseball = new Baseball();
-        baseball.play();
+        do {
+            baseball.play();
+        } while (does_user_want_play_again());
+    }
+
+    public static boolean does_user_want_play_again() {
+        Integer play_code = BaseballInput.getPlayCode();
+        return play_code.equals(PLAY_AGAIN_CODE);
     }
 }
