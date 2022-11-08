58a76258c33a8df790c8912376e6c3d8646e189b
JuSeong1130 <jsmoon1130@naver.com>
2022-11-08 18:51:35 +0900

    feat(Player): Player 숫자 입력, 입력한 숫자 get 메서드 구현
    숫자 입력 및 숫자 얻기 메서드

---
+++ src/main/java/baseball/player/Player.java
@@ -0,0 +1,14 @@
+package baseball.player;
+
+public class Player  {
+
+	String number;
+
+	public void insertNumber(String number) {
+		this.number = number;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+}
