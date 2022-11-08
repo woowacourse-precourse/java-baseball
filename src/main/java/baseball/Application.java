3a4441a1077d1129e33d4ab62daf4b4e046277be
HiiWee <wpdlzhf159@gmail.com>
2022-11-08 14:47:02 +0900

    Refact: 애플리케이션 시작 방식 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,6 +4,7 @@ import baseball.controller.NumberBaseballController;
 
 public class Application {
     public static void main(String[] args) {
-        new NumberBaseballController().startGame();
+        NumberBaseballController numberBaseballController = new NumberBaseballController();
+        numberBaseballController.startGame();
     }
 }
