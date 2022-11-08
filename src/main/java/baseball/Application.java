e070e30f244326b049fbd9dbe4ff8cbb7c0738da
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-08 13:13:00 +0900

    feat(split) : split the input answer
    split the input answer and put it in the list

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,17 +3,22 @@ package baseball;
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
         List<Integer> computer = makeRandomNumberList();
 
-        // TODO : 입력 받은 숫자를 분리하여 리스트에 넣기
+        // TODO : 숫자를 입력받아 분리한 다음 리스트에 넣는 부분을 함수화
 
         System.out.println("숫자를 입력해주세요 : ");
         String myAnswer = Console.readLine();
-        System.out.println("myAnswer : " + myAnswer);
+
+        String[] splitAnswer = myAnswer.split("");
+        ArrayList<String> AnswerList = new ArrayList<>(Arrays.asList(splitAnswer));
+        System.out.println("Answer : " + AnswerList);
+
         System.out.println("computer : " + computer);
     }
 
