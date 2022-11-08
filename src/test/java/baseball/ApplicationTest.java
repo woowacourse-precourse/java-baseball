09334fbac6c0b900de4fa798d1dcc6bcf77fe386
gmu1026 <gmu1026@gmail.com>
2022-11-08 22:41:27 +0900

    test: 입력값에 대한 테스트케이스 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,14 +1,22 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.in;
 
 class ApplicationTest extends NsTest {
+    BaseBallGame baseBallGame;
+    @BeforeEach
+    void setUp() {
+        baseBallGame = new BaseBallGame();
+    }
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -21,11 +29,43 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
-    void 예외_테스트() {
+    void 입력제한보다_긴_값_입력() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 중복된_숫자_입력() {
+        String input = "122";
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 숫자가_아닌_값_입력() {
+        String input = "테스트";
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 공백_입력() {
+        String input = " ";
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class));
+    }
+
+    @Test
+    void 입력제한보다_짧은_값_입력() {
+        String input = "3";
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(input))
+                        .isInstanceOf(IllegalArgumentException.class));
     }
 
     @Override
