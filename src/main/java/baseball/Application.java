c6d29c1e2c37f58c21faaf49930b30b8fc6dd1dd
KangSungBum <peter9706@naver.com>
2022-11-08 18:03:11 +0900

    refactor(Application.java) : Refactor main method structure
    Reconstruct main method codes
    - reconstructed main method by devide into two methods
    - gameProcess() : single baseball game process
    - decideExit() : gets user input of exit decision and returns exit signal

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,34 +12,39 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다");
         while (true) {
-            List<Integer> randomNumber = createRandomNumber();
-            while(true){
-                System.out.print("숫자를 입력해주세요 : ");
-                String inputNumber = Console.readLine();
-                checkInputCondition(inputNumber);
-                System.out.println("randomNumber = " + randomNumber);
+            gameProcess();
+            if (decideExit()) break;
+        }
+    }
 
-                List<Integer> listedInputNumber = convertIntegerIntoList(inputNumber);
+    private static void gameProcess() {
+        List<Integer> randomNumber = createRandomNumber();
+        while(true){
+            System.out.print("숫자를 입력해주세요 : ");
+            String inputNumber = Console.readLine();
+            checkInputCondition(inputNumber);
 
-                int[] countResult = countBallsAndStrikes(randomNumber, listedInputNumber);
-                String resultMessage = getResultMessage(countResult);
-                System.out.println(resultMessage);
+            List<Integer> listedInputNumber = convertIntegerIntoList(inputNumber);
 
-                if (resultMessage.equals("3스트라이크")) {
-                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                    break;
-                }
-            }
+            int[] countResult = countBallsAndStrikes(randomNumber, listedInputNumber);
+            String resultMessage = getResultMessage(countResult);
+            System.out.println(resultMessage);
 
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-            int exitCode = Integer.parseInt(Console.readLine());
-            checkExitCode(exitCode);
-
-            if (exitCode == 2) {
+            if (resultMessage.equals("3스트라이크")) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 break;
             }
         }
+    }
+
+    private static boolean decideExit(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int exitCode = Integer.parseInt(Console.readLine());
+        checkExitCode(exitCode);
+
+        if (exitCode == 2) {
+            return true;
+        }else return false;
     }
 
     private static List<Integer> createRandomNumber() {
