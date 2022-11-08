88ebca84ee9c276651b378950338dca481bf78c6
hwanee__ <fkfkthth@naver.com>
2022-11-08 21:46:34 +0900

    feat: Baseball Game 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,10 +1,12 @@
 package baseball;
 
+import baseball.controller.BaseballGame;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
-
+        BaseballGame baseballgame = new BaseballGame();
+        baseballgame.StartGame();
 
     }
 }
