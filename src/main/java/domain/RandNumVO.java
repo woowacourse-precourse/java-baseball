7d268bd70e406334bca790fff6b999516547bfd8
Identityless <rlandwnd555@naver.com>
2022-11-08 19:52:27 +0900

    fix : 3개의 숫자가 모두 같은 숫자가 나오는 버그 수정

--- src/main/java/domain/RandNumVO.java
+++ src/main/java/domain/RandNumVO.java
@@ -13,11 +13,11 @@ public class RandNumVO {
         int nums[] = new int[3];
         nums[0] = Randoms.pickNumberInRange(1, 9);
         nums[1] = Randoms.pickNumberInRange(1, 9);
-        while(nums[0] != nums[1]) {
+        while(nums[0] == nums[1]) {
             nums[1] = Randoms.pickNumberInRange(1, 9);
         }
         nums[2] = Randoms.pickNumberInRange(1, 9);
-        while(nums[2] != nums[1] && nums[2] != nums[0]) {
+        while(nums[2] == nums[1] && nums[2] == nums[0]) {
             nums[2] = Randoms.pickNumberInRange(1, 9);
         }
         this.randNum = List.of(nums[0], nums[1], nums[2]);
