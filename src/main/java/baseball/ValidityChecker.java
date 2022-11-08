3eefec532f0e54637cf1da9562b78de08936f010
choiejae <ejae8319@gmail.com>
2022-11-08 12:52:08 +0900

    style: 들여쓰기 지속은 최소 +8 스페이스로 수정

--- src/main/java/baseball/ValidityChecker.java
+++ src/main/java/baseball/ValidityChecker.java
@@ -20,30 +20,30 @@ public class ValidityChecker {
 
     public void validLength(String input) {
         if (input.length() != MAX_LEN) {
-            throw new IllegalArgumentException("input의 길이가 유효하지 않습니다");
+                throw new IllegalArgumentException("input의 길이가 유효하지 않습니다");
         }
     }
 
     public void validElement(String input) {
         final String REGEX = "[1-9]+";
         if (!input.matches(REGEX)) {
-            throw new IllegalArgumentException("input의 구성요소가 0을 제외한 숫자로만 이루어지지 않습니다");
+                throw new IllegalArgumentException("input의 구성요소가 0을 제외한 숫자로만 이루어지지 않습니다");
         }
     }
 
     public void validContinuity(String input) {
         Set<Character> numSet = new HashSet<>();
         for (int i = 0; i < input.length(); i++) {
-            numSet.add(input.charAt(i));
+                numSet.add(input.charAt(i));
         }
         if (numSet.size() != MAX_LEN) {
-            throw new IllegalArgumentException("input의 구성요소가 중복됩니다");
+                throw new IllegalArgumentException("input의 구성요소가 중복됩니다");
         }
     }
 
-    public void validateRestartGameInput(String input) {
+    public void validateRepalyGameInput(String input) {
         if (!Objects.equals(input, "1") && !Objects.equals(input, "2")) {
-            throw new IllegalArgumentException("1, 또는 2를 입력해야 합니다.");
+                throw new IllegalArgumentException("1, 또는 2를 입력해야 합니다.");
         }
     }
 }
