39f71ab47bd92fc916fa301f3c5476240429941a
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-08 17:51:24 +0900

    Feat : Controller.runGame 구현

--- src/main/java/baseball/Controller.java
+++ src/main/java/baseball/Controller.java
@@ -1,8 +1,12 @@
 package baseball;
 
-import net.bytebuddy.matcher.StringMatcher;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
 
 public class Controller {
+    private View view;
+
     private Model computer;
     private Model user;
 
@@ -36,6 +40,33 @@ public class Controller {
                 ball++;
             }
         }
-        return new int[]{ball,strike};
+        return new int[]{ball, strike};
+    }
+
+    public void runGame() {
+        String randomNumber = String.valueOf(Randoms.pickNumberInRange(111, 999));
+        computer = new Model(randomNumber);
+
+        view.printLine("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            view.print("숫자를 입력해주세요 : ");
+            String userNumber = view.inputInt();
+            user = new Model(userNumber);
+
+            int[] results = compareNumber(computer.getNumber(), user.getNumber());
+
+            view.printResult(results[0], results[1]);
+
+            if (results[1] == 3) {
+                view.printLine("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                view.printLine("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                String checkNumber = view.inputInt();
+                if (checkNumber.equals("1")) {
+                    continue;
+                } else if (checkNumber.equals("2")) {
+                    break;
+                }
+            }
+        }
     }
 }
