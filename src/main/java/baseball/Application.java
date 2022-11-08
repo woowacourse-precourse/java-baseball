779d90bdb64865a61c11825d09c33f37f8e03464
TeddyYeoni <po99027@naver.com>
2022-11-08 22:49:28 +0900

    슷자를 비교하여 힌트를 반환하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -25,19 +25,70 @@ public class Application {
 	}
 
 	public static List<Integer> user() {
-    	String userInput = Console.readLine(); 
-    	String[] userInputArray = userInput.split("");
-    	List<Integer> user = new ArrayList<Integer>();
-    	for (int index = 0; index < userInputArray.length; index++) {
-    		int digitNumber = Integer.parseInt(userInputArray[index]);
-    		user.add(digitNumber);
-    		if(!user.contains(digitNumber)) {
-    			throw new IllegalArgumentException();
-    		}
+		String userInput = Console.readLine();
+		String[] userInputArray = userInput.split("");
+		List<Integer> user = new ArrayList<Integer>();
+		for (int index = 0; index < userInputArray.length; index++) {
+			int digitNumber = Integer.parseInt(userInputArray[index]);
+			user.add(digitNumber);
+			if (!user.contains(digitNumber)) {
+				throw new IllegalArgumentException();
+			}
 		}
-    	if(user.size() != 3) {
-    		throw new IllegalArgumentException();
-    	}
+		if (user.size() != 3) {
+			throw new IllegalArgumentException();
+		}
 		return user;
 	}
+
+	public static int ball(List<String> computer, List<String> user) {
+		int ball = 0;
+		for (int userIndex = 0; userIndex < user.size(); userIndex++) {
+			int userNumber = user.indexOf(userIndex);
+			if (computer.contains(userNumber)) {
+				ball++;
+			}
+		}
+		return ball;
+	}
+
+	public static String compareResult(List<String> computer, List<String> user) {
+		String result = "";
+		
+		int ball = 0;
+		for (int userIndex = 0; userIndex < user.size(); userIndex++) {
+			int userNumber = user.indexOf(userIndex);
+			if (computer.contains(userNumber)) {
+				ball++;
+			}
+		}
+		
+		int strike = 0;
+		for (int index = 0; index < 3; index++) {
+			int userNumber = user.indexOf(index);
+			int computerNumber = computer.indexOf(index);
+			if (userNumber == computerNumber) {
+				strike++;
+			}
+		}
+		
+		if (ball != 0 && strike !=0) {
+			result = ball + "볼" + " " +  strike + "스트라이크";
+		}
+		
+		if(ball != 0 && strike == 0) {
+			result = ball + "볼";
+		}
+		
+		if(ball == 0 && strike != 0) {
+			result = strike + "스트라이크";
+		}
+		
+		if(ball == 0 && strike == 0) {
+			result = "낫싱";
+		}
+		
+		return result;
+	}
+
 }
