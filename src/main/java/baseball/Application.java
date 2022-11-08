d36f22a4517f0739bca8728318ed72ed67c1809e
Hongsugyeong <olivia258@naver.com>
2022-11-08 11:58:12 +0900

    (Feat) : main 메서드에 게임 시작 코드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,8 @@ import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Game answer = new Game();
+        Game.init(answer);
     }
 }
 
