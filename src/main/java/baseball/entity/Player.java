126e63487fc3ac7f3cecc08c23461a3dbc1c7cf4
이지영 <jioung93@naver.com>
2022-11-08 22:41:30 +0900

    [Player] feat: 플레이어에게 숫자 입력받기 기능 구현

--- src/main/java/baseball/entity/Player.java
+++ src/main/java/baseball/entity/Player.java
@@ -1,8 +1,18 @@
 package baseball.entity;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
     // List<Integer> playerNumbers: 사용자의 숫자 저장
-    // enterInput(): 입력 받는 함수
+    private static List<Integer> playerNumbers = new ArrayList<>();
+
+    public void enterInput() {
+        String input = Console.readLine();
+        // changeInputToNumbers(input);
+    }
     // changeInputToNumber(): 입력된 숫자를 리스트로 변환
     // validateNumber(): 입력받은 숫자에 대한 유효성 검증
     // validateAnswer(): 입력받은 재시작 여부 답변에 대한 유효성 검증
