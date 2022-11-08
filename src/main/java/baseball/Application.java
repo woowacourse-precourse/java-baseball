f451abc18d645fd7f982e1141dbe988f0d4607b0
hyejuc <chjjudy@naver.com>
2022-11-08 16:20:43 +0900

    feat: 사용자 입력 구현, checkException 수정
    checkException: return 타입 boolean으로 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,23 +2,36 @@ package baseball;
 
 import java.util.HashMap;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-
+        do {
+            System.out.print("숫자를 입력해주세요 : ");
+            int userNumber = Integer.parseInt(Console.readLine());
+            if (!checkException(userNumber)){
+                throw new IllegalArgumentException();
+            }
+            //낫싱, 볼, 스트라이크
+        }
+//        while (3 스트라이크){
+//            //게임 종료
+//        }
     }
-    public static void checkException(int num){
+    public static boolean checkException(int num){
         String numString = Integer.toString(num);
         if (numString.length() != 3){
-            throw new IllegalArgumentException();
+            return false;
         }
         if (numString.contains("0")){
-            throw new IllegalArgumentException();
+            return false;
         }
         if (!containSameNumber(numString)){
-            throw new IllegalArgumentException();
+            return false;
         }
+        return true;
     }
     public static boolean containSameNumber(String numString){
         HashMap<Character, Integer> checkSameNumber = new HashMap<>();
