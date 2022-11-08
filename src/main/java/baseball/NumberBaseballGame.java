9db0fb61cf0296f875d8ba023e0167e5d736a283
idoburnish <doyun7433@naver.com>
2022-11-08 16:23:19 +0900

    feat(NumberBaseballGame): 야구 게임 시작
    야구 게임을 최초로 실행하고, 실행 시 나오는 문구 출력

---
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -0,0 +1,13 @@
+package baseball;
+
+public class NumberBaseballGame {
+    private static final String START_TEXT = "숫자 야구 게임을 시작합니다.";
+    private static final String INPUT_TEXT = "숫자를 입력해주세요 : ";
+    private static final String RESULT_TEXT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String RESTART_TEXT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+
+    public void startGame() {
+        System.out.println(START_TEXT);
+        
+    }
+}
