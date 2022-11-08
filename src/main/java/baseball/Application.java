1f5a0fc8a748a5fc3a223ceeed94f35cb139e3a6
heohyeonjink <hyeonjin4870@naver.com>
2022-11-08 20:27:17 +0900

    feat(getRandomNumber) : 3자리 랜덤 숫자 생성
    3자리 랜덤 숫자 문자열로 반환

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String computer = getRandomNumber();
+    }
+
+    public static String getRandomNumber(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        for(Integer x : computer){
+            sb.append(x);
+        }
+        return sb.toString();
     }
 }
