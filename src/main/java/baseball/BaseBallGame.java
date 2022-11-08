5d57109a1ff1668eb3901a4a8aa6012625b39929
tgoddessana <twicegoddessana1229@gmail.com>
2022-11-08 20:42:22 +0900

    FEAT : 무한 반복하는 게임 흐름 구현

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
@@ -27,4 +29,81 @@ public class BaseBallGame implements GameInterface {
         return emptyList.toString();
     }
 
+    /**
+     * 게임의 시작 메시지를 출력합니다.
+     */
+    @Override
+    public void start() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    /**
+     * 유저로부터 숫자를 입력받습니다.
+     */
+    @Override
+    public String getUserInput() {
+        System.out.print("숫자를 입력하세요 : ");
+        return readLine();
+    }
+
+    /**
+     * 게임의 재시작 여부에 따라, 게임을 끝내거나 재시작합니다.
+     */
+    @Override
+    public void end() {
+        boolean flag = askRestart();
+        if (flag) {
+
+        } else {
+
+        }
+    }
+
+    /**
+     * 게임 재시작 여부를 물어보고, 그렇다면 true, 아니면 false 를 반환합니다.
+     */
+    @Override
+    public boolean askRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = readLine();
+        if (input.equals("1")) {
+            return true;
+        } else if (input.equals("2")) {
+            return false;
+        }
+        return false;
+    }
+
+    /**
+     * 데이터를 검증합니다. 들어온 자릿수가 3자릿수가 맞는지를 검사합니다.
+     */
+    @Override
+    public boolean validateInput(String input) {
+        return input.length() == 3;
+    }
+
+    /**
+     * 데이터 검증 결과에 따라서, 게임을 계속 진행할지 혹은 예외를 발생시킬지를 처리합니다.
+     */
+    @Override
+    public void exceptionHandler(boolean flag) {
+        if (!flag) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+    /**
+     * 게임을 반복해서 수행합니다.
+     */
+    @Override
+    public void run() {
+        while (true) {
+            start();
+            String input = getUserInput();
+            exceptionHandler(validateInput(input));
+
+        }
+
+    }
 }
