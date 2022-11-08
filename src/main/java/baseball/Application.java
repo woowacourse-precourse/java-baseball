a9690ecbc8a01fa4f9bda60fd63e8ea0e738af20
SEUNGHEE96 <dnffktmdgml@gmail.com>
2022-11-08 21:26:36 +0900

    refactor : 주석 최소화

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class Application {
 
-    //[컴퓨터] 임의의 3자리 숫자 저장
     public static List<Integer> makeNumber(List<Integer> computer) {
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -19,11 +18,9 @@ public class Application {
         }
         return computer;
     }
-    //[플레이어] 서로 다른 3개의 숫자 입력
     public static List<Integer> input(){
         List<Integer> inputNumber = new ArrayList<>();
         String input = Console.readLine();
-        //[예외처리]
         //세자리 숫자 이외의 입력 시
         if (input.length() > 3) { //3자리 이상 숫자 입력 시
             System.out.println("3자리 이상의 숫자를 입력하셨습니다.");
@@ -44,7 +41,6 @@ public class Application {
         }
         return inputNumber;
     }
-    //[기능] 스트라이크, 볼 갯수 판별
     public static HashMap<String, Integer> caseCheck(List<Integer> computer, List<Integer>inputNumber) {
         HashMap<String, Integer> gameResult = new HashMap<>();
         int strike = 0;
@@ -57,7 +53,6 @@ public class Application {
         gameResult.put("ball", ball);
         return gameResult;
     }
-    //[출력] 결과문 출력
     public static void output(int strike, int ball) {
         if ( ball == 0 && strike == 0) {
             System.out.println("낫싱");
@@ -80,27 +75,21 @@ public class Application {
 
         while (true) {
 
-            //변수 초기 설정
             List<Integer> inputNumber;
             HashMap<String, Integer> gameResult;
             int strike = 0;
             int ball = 0;
 
-            //컴퓨터가 3자리 숫자 생성
             makeNumber(computer);
 
-            //플레이어가 3자리 숫자 입력
             System.out.print("숫자를 입력해주세요 : ");
             inputNumber = input();
-            
-            //야구게임 결과 (ex 2스트라이크 1볼) 저장
+
             gameResult = caseCheck(computer, inputNumber);
+            output(strike, ball);
+
             strike = gameResult.get("strike");
             ball = gameResult.get("ball");
-            
-            //결과 출력
-            output(strike, ball);
-            
             //정답 맞췄을 시, 게임 종료 혹은 반복 가능
             if ( strike == 3) {
                 String repeat = Console.readLine();
