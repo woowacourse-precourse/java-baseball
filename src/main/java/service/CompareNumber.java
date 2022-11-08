bcbbcf4abb4004f9a0ee10157e8db25182e70d91
JunHyeon <dlwnsgus07@naver.com>
2022-11-08 14:30:09 +0900

    refactor(상수 지정): 조건에 사용될 상수 변수화

--- src/main/java/service/CompareNumber.java
+++ src/main/java/service/CompareNumber.java
@@ -5,6 +5,9 @@ import domain.Number;
 import java.util.List;
 
 public class CompareNumber {
+    private static final int CORRECT_ANSWER = 3;
+    private static final int NO_COUNT = 0;
+
     private List<Number> userNumber;
     private List<Number> computerNumber;
 
@@ -16,7 +19,7 @@ public class CompareNumber {
         int strikeCount = 0;
         int ballCount = 0;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < CORRECT_ANSWER; i++) {
             if (this.userNumber.get(i).equals(this.computerNumber.get(i))) {
                 strikeCount++;
             } else if (userNumber.contains(computerNumber.get(i))) {
@@ -27,16 +30,16 @@ public class CompareNumber {
     }
 
     private boolean printHint(int strikeCount, int ballCount) {
-        if (strikeCount != 0 && ballCount != 0) {
+        if (strikeCount != NO_COUNT && ballCount != NO_COUNT) {
             System.out.println(ballCount + "볼" + " " + strikeCount + "스트라이크");
-        } else if (strikeCount != 0) {
-            if (strikeCount == 3) {
+        } else if (strikeCount != NO_COUNT) {
+            if (strikeCount == CORRECT_ANSWER) {
                 System.out.println(strikeCount + "스트라이크");
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 return true;
             }
             System.out.println(strikeCount + "스트라이크");
-        } else if (ballCount != 0) {
+        } else if (ballCount != NO_COUNT) {
             System.out.println(ballCount + "볼");
         } else {
             System.out.println("낫싱");
