58ce0431be1c1036baa5a3cd7043e5545a6065fb
gomljo <gomljo0307@naver.com>
2022-11-08 19:25:39 +0900

    test(InputViewTest.java): 게임 시작 후 플레이어(사용자)에게 숫자를 입력하라는 메시지를 출력하는 기능 검증

--- src/test/java/baseball/InputView/InputViewTest.java
+++ src/test/java/baseball/InputView/InputViewTest.java
@@ -9,12 +9,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class InputViewTest {
     private static final String GAME_START_MESSAGE="숫자 야구 게임을 시작합니다.";
+    private static final String REQUEST_ENTER_INPUT="숫자를 입력해주세요 : ";
+
     @Test
-    public void notice_Start_Message(){
+    public void notice_Start_Message_Test(){
 
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
         System.out.print(GAME_START_MESSAGE);
         Assertions.assertThat(GAME_START_MESSAGE).isEqualTo(out.toString());
+    }
+    @Test
+    public void request_Entering_Input_Message_Test(){
+
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        System.out.print(REQUEST_ENTER_INPUT);
+        Assertions.assertThat(REQUEST_ENTER_INPUT).isEqualTo(out.toString());
     }
 }
