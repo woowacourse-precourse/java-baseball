4737f01d75d0d1e674bcf5120f5840b8b18c8040
Vector1331 <89854207+Vector1331@users.noreply.github.com>
2022-11-08 18:57:13 +0900

    [Feat] player : 상대방의 수라고 생각하는 (서로 다른 숫자) 3개 입력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -6,7 +6,11 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-
-        List<Integer> computerNumberList = Computer.generateRandomNum();
+        boolean gamingMode = true;
+        NumberBaseBall Game = new NumberBaseBall();
+        do {
+            List<Integer> computerNumberList = Computer.generateRandomNum();
+            gamingMode = Game.GameStart(computerNumberList);
+        } while (gamingMode);
     }
 }
---
+++ src/main/java/baseball/NumberBaseBall.java
@@ -0,0 +1,16 @@
+package baseball;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.*;
+public class NumberBaseBall {
+    boolean GameStart(List<Integer> computerNumList) {
+        while (true) {
+            /**
+             * player 숫자 입력받음 */
+            int playerNum = Integer.parseInt(Console.readLine());
+
+
+
+        }
+    }
+}
