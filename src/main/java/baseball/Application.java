655856dd2edc3a2fe0b981810c73c423ac1e05f0
aud19 <aud19980518@gmail.com>
2022-11-08 21:34:45 +0900

    쓸모없는 코드 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,6 @@ package baseball;
 import baseball.controller.StartController;
 
 public class Application {
-    private static final int EXIT_CODE = 2;
     public static void main(String[] args) {
         StartController startController = new StartController();
         startController.run();
