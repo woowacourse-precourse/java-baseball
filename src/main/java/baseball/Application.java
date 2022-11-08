1bc43913540dc4cae505da68490fbadf7a85c60d
kmw2378 <rlarla677@gmail.com>
2022-11-08 23:25:33 +0900

    refactor (Application) - 출력문을 저장한 Message 클래스를 사용하여 출력문 수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,11 @@
 package baseball;
 
+import static baseball.message.Message.START_MESSAGE;
+
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(START_MESSAGE);
 
         while (true) {
             BullsAndCows bullsAndCows = new BullsAndCows(new BullsAndCowsResult());
