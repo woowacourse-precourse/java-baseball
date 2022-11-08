54ccdea82935538c6a07ffa4a52d2ee7deda819d
syhan7516 <syhan7516@naver.com>
2022-11-08 23:37:27 +0900

    feat (main/getCheckRestart) : 결과 처리 야구 게임 종료 후 사용자의 결정에 따라 재시작을 확인하는 함수 & 결과에 따라 수행 코드 - 재시작 여부 확인 문장 출력 후 옵션 선택 - 1은 재시작 - 2는 게임 종료 - 재시작을 위한 while 코드 작성 - while 코드에 따른 코드 재배치

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Application {
 
     // 1.상대 숫자 생성
-    static  void getSettingComputerNumber(List<Integer> computerNumber) {
+    static void getSettingComputerNumber(List<Integer> computerNumber) {
 
         while (computerNumber.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -27,18 +27,18 @@ public class Application {
 
         System.out.println(strNumber);
 
-        for(int idx=0; idx<strNumber.length(); idx++) {
+        for (int idx = 0; idx < strNumber.length(); idx++) {
             int intNumber = Character.getNumericValue(strNumber.charAt(idx));
             guessingNumber.add(intNumber);
         }
     }
 
     // 3.낫싱 판단
-    static void checkNoting(List<Integer>computerNumber,List<Integer>guessingNumber,List<Integer>numberCheckCount) {
+    static void checkNoting(List<Integer> computerNumber, List<Integer> guessingNumber, List<Integer> numberCheckCount) {
 
-        for(int idx=0; idx<3; idx++) {
+        for (int idx = 0; idx < 3; idx++) {
             int checkNumber = guessingNumber.get(idx);
-            if(computerNumber.contains(checkNumber)) {
+            if (computerNumber.contains(checkNumber)) {
                 numberCheckCount.add(0);
                 return;
             }
@@ -48,14 +48,14 @@ public class Application {
     }
 
     // 4.볼 판단
-    static void checkBall(List<Integer>computerNumber,List<Integer>guessingNumber,List<Integer>numberCheckCount) {
+    static void checkBall(List<Integer> computerNumber, List<Integer> guessingNumber, List<Integer> numberCheckCount) {
 
         int ballCount = 0;
 
-        for(int idx=0; idx<3; idx++) {
+        for (int idx = 0; idx < 3; idx++) {
             int checkNumber = guessingNumber.get(idx);
 
-            if(computerNumber.contains(checkNumber) && checkNumber != computerNumber.get(idx)) {
+            if (computerNumber.contains(checkNumber) && checkNumber != computerNumber.get(idx)) {
                 ballCount += 1;
             }
         }
@@ -64,13 +64,13 @@ public class Application {
     }
 
     // 5.스트라이크 판단
-    static void checkStrike(List<Integer>computerNumber,List<Integer>guessingNumber,List<Integer>numberCheckCount) {
+    static void checkStrike(List<Integer> computerNumber, List<Integer> guessingNumber, List<Integer> numberCheckCount) {
 
         int strikeCount = 0;
 
-        for(int idx=0; idx<3; idx++) {
+        for (int idx = 0; idx < 3; idx++) {
             int checkNumber = guessingNumber.get(idx);
-            if(computerNumber.contains(checkNumber) && checkNumber == computerNumber.get(idx)) {
+            if (computerNumber.contains(checkNumber) && checkNumber == computerNumber.get(idx)) {
                 strikeCount += 1;
             }
         }
@@ -79,7 +79,7 @@ public class Application {
     }
 
     // 6.숫자 판단 결과
-    static int getNoticeResult(List<Integer>numberCheckCount) {
+    static int getNoticeResult(List<Integer> numberCheckCount) {
 
         // 각 결과 수
         int nCount = numberCheckCount.get(0);
@@ -89,22 +89,22 @@ public class Application {
         String noticeResult = "";
 
         // 낫싱인 경우
-        if(nCount == 1) {
+        if (nCount == 1) {
             System.out.println("낫싱");
             return -1;
         }
 
         // 볼이 있는 경우
-        if(bCount > 0) {
+        if (bCount > 0) {
             noticeResult = bCount + "볼" + " ";
         }
 
         // 스트라이크가 있는 경우
-        if(sCount > 0) {
+        if (sCount > 0) {
             noticeResult = noticeResult + sCount + "스트라이크";
 
             // 모두 맞힌 경우
-            if(sCount == 3) {
+            if (sCount == 3) {
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 return 1;
             }
@@ -114,36 +114,63 @@ public class Application {
         return -1;
     }
 
+    // 8.결과 처리
+    static String getCheckRestart() {
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        String selectNumber = Console.readLine();
+
+        System.out.println(selectNumber);
+
+        return selectNumber;
+    }
+
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
 
-        List<Integer> computerNumber = new ArrayList<>();
+        String restart = "";
 
-        int gameResult = 0;
+        while (true) {
 
-        // 상대 숫자 생성 함수 호출
-        getSettingComputerNumber(computerNumber);
+            // 종료 조건
+            if(restart.equals("2")) {
+                System.out.println("게임 종료");
+                return;
+            }
 
-        // 7.게임 진행 반복
-        while(gameResult != 1) {
+            System.out.println("숫자 야구 게임을 시작합니다.");
 
-            List<Integer> guessingNumber = new ArrayList<>();
-            List<Integer> numberCheckCount = new ArrayList<>();
+            List<Integer> computerNumber = new ArrayList<>();
 
-            // 숫자 입력 함수 호출
-            getSettingGuessingNumber(guessingNumber);
+            int gameResult = 0;
 
-            // 낫싱 판단 함수 호출
-            checkNoting(computerNumber,guessingNumber,numberCheckCount);
+            // 상대 숫자 생성 함수 호출
+            getSettingComputerNumber(computerNumber);
 
-            // 볼 판단 함수 호출주석 제거
-            checkBall(computerNumber,guessingNumber,numberCheckCount);
+            // 7.게임 진행 반복
+            while (gameResult != 1) {
 
-            // 스트라이크 판단 함수 호출
-            checkStrike(computerNumber,guessingNumber,numberCheckCount);
+                List<Integer> guessingNumber = new ArrayList<>();
+                List<Integer> numberCheckCount = new ArrayList<>();
 
-            // 숫자 판단 결과 함수 호출
-            gameResult = getNoticeResult(numberCheckCount);
+                // 숫자 입력 함수 호출
+                getSettingGuessingNumber(guessingNumber);
+
+                // 낫싱 판단 함수 호출
+                checkNoting(computerNumber, guessingNumber, numberCheckCount);
+
+                // 볼 판단 함수 호출주석 제거
+                checkBall(computerNumber, guessingNumber, numberCheckCount);
+
+                // 스트라이크 판단 함수 호출
+                checkStrike(computerNumber, guessingNumber, numberCheckCount);
+
+                // 숫자 판단 결과 함수 호출
+                gameResult = getNoticeResult(numberCheckCount);
+            }
+
+            // 결과 처리 함수 호출
+            restart = getCheckRestart();
         }
     }
 }
