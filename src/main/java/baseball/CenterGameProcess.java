7bf9fd26bded63c85bf1dfd8753111502b7801f7
DESKTOP-AUSH0GT\Power <sgh0718@naver.com>
2022-11-08 19:31:30 +0900

    feat : 숫자야구게임을 진행하는 기능 클래스의 사용자가 입력한 3자리 숫자를 호출하는 기능
    입출력 전담 클래스에서 입력 메서드를 호출하여 3자리 수를 문자열 형태로 입력하는 기능

---
+++ src/main/java/baseball/CenterGameProcess.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import java.util.List;
+
+public class CenterGameProcess {
+    OutputSentense output = new OutputSentense();
+    GameReferee count = new GameReferee();
+
+    public CenterGameProcess(List<Integer> computer, int gameTypeNumber) {
+
+        String myNumber = output.Question(output.inputNumber, gameTypeNumber);
+
+    }
+}
