d59dec27f7a7a2147ea2ba5f5d548a7e1831d5ed
kkm5291 <kkm23125291@gmail.com>
2022-11-08 11:32:51 +0900

    Refactor : computerNumberList to computerNumber

--- src/main/java/createList/computer.java
+++ src/main/java/createList/computer.java
@@ -1,4 +1,4 @@
-package createnumberlist;
+package createList;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class computer {
-    public static List<Integer> computerNumberList() {
+    public static List<Integer> computerNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNum = Randoms.pickNumberInRange(1, 9);
