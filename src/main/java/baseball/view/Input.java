5f387d0f7c329e466e63133b368479ff095d87a4
Yunsik-Choi <choiys0311@gmail.com>
2022-11-08 20:29:53 +0900

    ♻️ refactor : Input 클래스 하드코딩 상수화

--- src/main/java/baseball/view/Input.java
+++ src/main/java/baseball/view/Input.java
@@ -9,13 +9,20 @@ public class Input {
     private static final int BASEBALL_NUMBER_LENGTH = 3;
     private static final String RESTART = "1";
     private static final String END = "2";
+    private static final String INPUT_INSTANTIATION_EXCEPTION = "Input 클래스는 생성할 수 없습니다.";
+    private static final String INPUT_BASEBALL_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String VALIDATE_ALL_MATCH_NUMBER_EXCEPTION = "숫자가 아닌 값이 있습니다.";
+    private static final String BASEBALL_NUMBER_LENGTH_EXCEPTION = BASEBALL_NUMBER_LENGTH + "자리가 아닙니다.";
+    private static final String OVERLAP_EXCEPTION = "중복된 값이 있습니다.";
+    private static final String INPUT_RESTART = "게임을 새로 시작하려면 " + RESTART + ", 종료하려면 " + END + "를 입력하세요.";
+    private static final String RESTART_INPUT_EXCEPTION = "재시작/종료를 구분하는 " + RESTART + "과 " + END + "중 하나의 수를 입력해야 합니다.";
 
     private Input() throws InstantiationException {
-        throw new InstantiationException("Input 클래스는 생성할 수 없습니다.");
+        throw new InstantiationException(INPUT_INSTANTIATION_EXCEPTION);
     }
 
     public static List<Integer> inputBaseballNumber() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_BASEBALL_NUMBER);
         String text = Console.readLine().trim();
         validateInputBaseballNumber(text);
         return textToIntList(text);
@@ -29,13 +36,13 @@ public class Input {
 
     private static void validateAllMatchNumber(String text) {
         if (!text.chars().allMatch(Character::isDigit)) {
-            throw new IllegalArgumentException("숫자가 아닌 값이 있습니다.");
+            throw new IllegalArgumentException(VALIDATE_ALL_MATCH_NUMBER_EXCEPTION);
         }
     }
 
     private static void validateBaseballNumberLength(String text) {
         if (text.length() != BASEBALL_NUMBER_LENGTH) {
-            throw new IllegalArgumentException(BASEBALL_NUMBER_LENGTH + "자리가 아닙니다.");
+            throw new IllegalArgumentException(BASEBALL_NUMBER_LENGTH_EXCEPTION);
         }
     }
 
@@ -46,7 +53,7 @@ public class Input {
                 .count();
 
         if (count != text.length()) {
-            throw new IllegalArgumentException("중복된 값이 있습니다.");
+            throw new IllegalArgumentException(OVERLAP_EXCEPTION);
         }
     }
 
@@ -58,7 +65,7 @@ public class Input {
     }
 
     public static int inputRestart() {
-        System.out.println("게임을 새로 시작하려면 " + RESTART + ", 종료하려면 " + END + "를 입력하세요.");
+        System.out.println(INPUT_RESTART);
         String text = Console.readLine().trim();
         validateInputRestart(text);
         return Integer.parseInt(text);
@@ -66,7 +73,7 @@ public class Input {
 
     public static void validateInputRestart(String text) {
         if (!text.equals(RESTART) && !text.equals(END)) {
-            throw new IllegalArgumentException("재시작/종료를 구분하는 " + RESTART + "과 " + END + "중 하나의 수를 입력해야 합니다.");
+            throw new IllegalArgumentException(RESTART_INPUT_EXCEPTION);
         }
     }
 }
