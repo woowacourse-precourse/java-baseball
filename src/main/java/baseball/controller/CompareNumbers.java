613b624e5f67e11dbbb1c4773f54cebd8a52b319
Sieun Park <psieun0606@gmail.com>
2022-11-08 18:51:12 +0900

    feat: 스트라이크가 3이 아닐 때 힌트 제공 기능 추가

--- src/main/java/baseball/controller/CompareNumbers.java
+++ src/main/java/baseball/controller/CompareNumbers.java
@@ -1,6 +1,7 @@
 package baseball.controller;
 
 import baseball.model.User;
+import baseball.view.OutputView;
 
 import java.util.ArrayList;
 
@@ -8,11 +9,20 @@ public class CompareNumbers {
 
     User user = new User();
 
-    public CompareNumbers(ArrayList<Integer> userNumberList, ArrayList<Integer> computerNumberList) {
+    public int compareNumbers(ArrayList<Integer> userNumberList, ArrayList<Integer> computerNumberList) {
         for (int i = 0; i < 3; i++) {
             countStrike(userNumberList.get(i), computerNumberList.get(i));
             countBall(userNumberList.get(i), computerNumberList.get(i), computerNumberList);
         }
+
+        int strike = user.getStrike();
+        int ball = user.getBall();
+
+        if (strike != 3) {
+            OutputView.printHint(ball, strike);
+        }
+
+        return strike;
     }
 
     public void countStrike(int userNumber, int computerNumber) {
