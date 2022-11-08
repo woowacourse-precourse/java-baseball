41781720d761240cb0a018b1f2c2a93b08d12a53
조병우 <whquddn55@gmail.com>
2022-11-08 09:31:35 +0900

    feat(InputValidator): implement validating inptut method
    Implement validateBallNumberString method that is validating user's input string. This method will throw IllegalArgumntException when string is not valid.
    
    + public void validateBallNumberString(String)
    + private void validateInputRange(String)
    + private void validateInputLength(String)
    + private void validateInputDistinct(String)

--- src/main/java/baseball/utils/InputValidator.java
+++ src/main/java/baseball/utils/InputValidator.java
@@ -1,7 +1,30 @@
 package baseball.utils;
 
-/**
- * TODO: implement the class
- */
+import static baseball.BaseballGame.INPUT_SIZE;
+
 public class InputValidator {
+
+    public void validateBallNumberString(String input) throws IllegalArgumentException {
+        validateIntegerRange(input);
+        validateInputLength(input);
+        validateInputDistinct(input);
+    }
+
+    private void validateIntegerRange(String input) throws IllegalArgumentException {
+        if (input.chars().anyMatch(number -> number < '0' || number > '9')) {
+            throw new IllegalArgumentException("입력이 [0, 9]사이의 숫자 이외의 문자를 포함합니다.");
+        }
+    }
+
+    private void validateInputLength(String input) throws IllegalArgumentException {
+        if (input.length() != INPUT_SIZE) {
+            throw new IllegalArgumentException(String.format("입력한 숫자의 길이가 %d이 아닙니다.", INPUT_SIZE));
+        }
+    }
+
+    private void validateInputDistinct(String input) throws IllegalArgumentException {
+        if (input.chars().distinct().count() != INPUT_SIZE) {
+            throw new IllegalArgumentException("입력한 숫자에 중복이 발생했습니다.");
+        }
+    }
 }
