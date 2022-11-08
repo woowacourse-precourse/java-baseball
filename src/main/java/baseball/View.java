03bb9a194c902697795af2303e9120d545427f18
jwPark6 <67006945+jwPark6@users.noreply.github.com>
2022-11-08 16:05:52 +0900

    숫자 입력 Method 구현

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -1,11 +1,24 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class View {
     private String s;
 
     public void printLine(String s){
         try {
             System.out.printf(s);
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e);
+        }
+    }
+
+    public void inputInt(){
+        try{
+            int number;
+            System.out.print("숫자를 입력해주세요 : ");
+            number = Integer.parseInt(Console.readLine());
         }
         catch (IllegalArgumentException e){
             System.out.println(e);
