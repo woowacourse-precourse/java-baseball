dd703f59de707fcc569abd7a252bf2ce2b520f3c
Jenn Kim <grdd0419@gmail.com>
2022-11-08 18:13:09 +0900

    feat(Constants): add two constants for code readability -- CONTINUE_GAME = 1, EXIT_GAME = 2

--- src/main/java/baseball/Constants.java
+++ src/main/java/baseball/Constants.java
@@ -1,5 +1,10 @@
 package baseball;
 
 public final class Constants {
+
     public static final Integer	NUMBER_SIZE = 3;
+
+    public static final Integer CONTINUE_GAME = 1;
+    public static final Integer EXIT_GAME = 2;
+
 }
