204a948953e873a8feb6518ec3295c85b124a3b8
Yh0sun <hy770819@naver.com>
2022-11-08 19:46:04 +0900

    feat : resultPrint method add

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,10 +14,13 @@ public class Application {
         int[] computerNumCheck = new int[10];
         int[] strikeResult = new int[3];
 
-        public void clear(){}
+        public void clear() {
+        }
+
         public boolean startGame() throws IOException {
             computerNum = makeComputerNum();
         }
+
         public List<Integer> makeComputerNum() {
             List<Integer> computerNum = new ArrayList<>();
 
@@ -30,34 +33,53 @@ public class Application {
             }
             return computerNum;
         }
-        public List<Integer> toInputNum(String input){
-            List<Integer> inputNum=new ArrayList<>();
-            String[] inputStr=input.split("");
 
-            for(int i=0;i<3;i++){
+        public List<Integer> toInputNum(String input) {
+            List<Integer> inputNum = new ArrayList<>();
+            String[] inputStr = input.split("");
+
+            for (int i = 0; i < 3; i++) {
                 inputNum.add(Integer.parseInt(inputStr[i]));
             }
             return inputNum;
         }
+
         public int strike(List<Integer> inputNum) {
-            int strikeCnt=0;
-            for(int i=0;i<3;i++){
-                if(Integer.compare(computerNum.get(i),inputNum.get(i))==0){
+            int strikeCnt = 0;
+            for (int i = 0; i < 3; i++) {
+                if (Integer.compare(computerNum.get(i), inputNum.get(i)) == 0) {
                     strikeCnt++;
-                    strikeResult[i]=1;
+                    strikeResult[i] = 1;
                 }
             }
             return strikeCnt;
         }
-        public int ball(List<Integer> inputNum){
-            int ballCnt=0;
-            for(int i=0;i<3;i++){
-                if(strikeResult[i]!=1&&computerNumCheck[inputNum.get(i)]==1) ballCnt++;
+
+        public int ball(List<Integer> inputNum) {
+            int ballCnt = 0;
+            for (int i = 0; i < 3; i++) {
+                if (strikeResult[i] != 1 && computerNumCheck[inputNum.get(i)] == 1) ballCnt++;
             }
             return ballCnt;
         }
-        public boolean resultPrint(){}
+
+        public boolean resultPrint(int ballCnt, int strikeCnt) {
+            if (strikeCnt == 3) {
+                System.out.println("3스트라이크");
+                System.out.print("3개의 숫자를 모두 맞히셨습니다!");
+                return true;
+            } else if (strikeCnt == 0 && ballCnt == 0) {
+                System.out.println("낫싱");
+                return false;
+            } else {
+                if (ballCnt != 0) System.out.print(String.format("%d볼 ", ballCnt));
+                if (strikeCnt != 0) System.out.print(String.format("%d스트라이크 ", strikeCnt));
+                System.out.println("");
+                return false;
+            }
+        }
     }
+
 
     public static void main(String[] args) {
 
