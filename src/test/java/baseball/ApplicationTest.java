c1e87b08db7f910c0006632b979044fafd85fada
sujin0607 <goweg0607@gmail.com>
2022-11-08 21:56:32 +0900

    Test: 클래스명, 메소드명 수정

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -12,11 +12,9 @@ import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberI
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationTest extends NsTest {
-    private BaseBallGame baseBallGame = new BaseBallGame();
 
     @Test
     void 게임종료_후_재시작() {
@@ -35,7 +33,7 @@ class ApplicationTest extends NsTest {
     class ComputerTest {
         Computer computer = new Computer();
         @Test
-        void 난수_생성시_중복여부_확인() {
+        void 난수_생성시_중복여부_검사() {
             List<Integer> randomNums = computer.randomNumbers();
             Set<Integer> setRandomNums = new HashSet<>(randomNums);
 
@@ -43,7 +41,7 @@ class ApplicationTest extends NsTest {
         }
 
         @Test
-        void 난수_범위가_1_9_인지_확인() {
+        void 난수_범위가_1_9_인지_검사() {
             List<Integer> randomNums = computer.randomNumbers();
             assertThat(randomNums.stream().allMatch(randomNum -> randomNum >= 1 && randomNum <= 9)).isTrue();
 
@@ -80,29 +78,29 @@ class ApplicationTest extends NsTest {
     }
 
     @Nested
-    class GameTest {
-        Game game = new Game();
+    class CompareNumberTest {
+        CompareNumber compareNumber = new CompareNumber();
         @Test
-        void find_Number함수_볼_스트라이크_결과_반환() {
+        void 숫자를_못_맞출_시_false_반환() {
             List<Integer> computerNumber = List.of(1, 2, 3);
-            game.setComputer(computerNumber);
+            compareNumber.setComputer(computerNumber);
 
-            assertEquals("1볼 1스트라이크", game.gameRun(List.of(1, 3, 0)));
-            assertEquals("1스트라이크", game.gameRun(List.of(1, 4, 5)));
-            assertEquals("1볼", game.gameRun(List.of(4, 5, 1)));
-
+            assertFalse(compareNumber.gameRun(List.of(1, 3, 0)));
+            assertFalse(compareNumber.gameRun(List.of(1, 4, 5)));
+            assertFalse(compareNumber.gameRun(List.of(4, 5, 1)));
         }
 
         @Test
-        void _3스트라이크인_경우_게임_종료_반환() {
+        void 모든_숫자를_맞출_시_true_반환() {
             List<Integer> computerNumber = List.of(1, 2, 3);
-            game.setComputer(computerNumber);
-            assertEquals("게임 종료", game.gameRun(List.of(1, 2, 3)));
+            compareNumber.setComputer(computerNumber);
+            assertTrue(compareNumber.gameRun(List.of(1, 2, 3)));
         }
     }
 
     @Nested
-    class reStartTset {
+    class RestartTest {
+        BaseBallGame baseBallGame = new BaseBallGame();
         @Test
         void 게임_재시작시_입력값이_1_2가_아닐_경우_예외_발생() {
             String userInput = "5";
@@ -111,7 +109,7 @@ class ApplicationTest extends NsTest {
             });
         }
     }
-    
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
