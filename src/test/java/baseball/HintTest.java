ac043d9408a7a327e18365ab6e3e5eded87665f6
saewoo1 <wooozooo1123@gmail.com>
2022-11-08 10:50:11 +0900

    refector(HintTest): 테스트 코드 수치 다양화

--- src/test/java/baseball/HintTest.java
+++ src/test/java/baseball/HintTest.java
@@ -8,30 +8,30 @@ class HintTest {
 
   @Test
   void ballCount_계산() {
-    String userNumber = "123";
-    String computerNumber = "123";
+    String userNumber = "928";
+    String computerNumber = "482";
 
     int ballScore = Hint.ballCount(computerNumber, userNumber);
-    Assertions.assertThat(ballScore).isEqualTo(3);
+    Assertions.assertThat(ballScore).isEqualTo(2);
   }
 
   @Test
   void strikeCount_계산() {
-    String userNumber = "123";
-    String computerNumber = "123";
+    String userNumber = "529";
+    String computerNumber = "941";
 
     int strikeCount = Hint.strikeCount(computerNumber, userNumber);
-    Assertions.assertThat(strikeCount).isEqualTo(3);
+    Assertions.assertThat(strikeCount).isEqualTo(0);
   }
 
   @Test
   void ballAndStrike_카운트_계산() {
-    String userNumber ="123";
-    String computerNumber ="123";
+    String userNumber ="925";
+    String computerNumber ="952";
     int ballScore = Hint.ballCount(computerNumber, userNumber);
     int strikeScore = Hint.strikeCount(computerNumber, userNumber);
 
     Assertions.assertThat(Hint.ballAndStrike(computerNumber, userNumber))
-            .isEqualTo(new int[]{ballScore - strikeScore, strikeScore});
+            .isEqualTo(new int[]{2, 1});
   }
 }
