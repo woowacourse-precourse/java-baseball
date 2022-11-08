50883e3fa8a3f89c1481ce623a8865bbbdf6f4ff
moonstal1506 <moonstal1506@gmail.com>
2022-11-08 11:32:51 +0900

    feat: 게임을 종료한 후 게임을 다시 시작하거나 완전히 종료

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -6,16 +6,37 @@ import baseball.model.Result;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
+import java.util.List;
+
 public class BaseBallController {
+
+    private static final int CONTINUE = 1;
 
     public void run() {
         OutputView.startMessage();
-        playGame();
+        playGames();
+    }
+
+    private void playGames() {
+        int command = CONTINUE;
+
+        while (isContinueGame(command)) {
+            playGame();
+            command = InputView.inputContinueGame();
+        }
+    }
+
+    private static boolean isContinueGame(int command) {
+        return command == CONTINUE;
     }
 
     private void playGame() {
         boolean isGameEnd = false;
-        Balls computerBalls = new Balls(NumberGenerator.generate());
+        List<Integer> generate = NumberGenerator.generate();
+        for (Integer integer : generate) {
+            System.out.print(integer);
+        }
+        Balls computerBalls = new Balls(generate);
 
         while (!isGameEnd) {
             Result result = matchBalls(computerBalls);
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,12 +5,31 @@ import java.util.Scanner;
 public class InputView {
 
     private static final Scanner scanner = new Scanner(System.in);
+    private static final int CONTINUE = 1;
+    private static final int STOP = 2;
 
     private InputView() {
     }
 
     public static String inputNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
-        return scanner.nextLine();
+        return scanner.next();
+    }
+
+    public static int inputContinueGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int command = scanner.nextInt();
+        validate(command);
+        return command;
+    }
+
+    private static void validate(int command) {
+        if(!isValidCommand(command)){
+            throw new IllegalArgumentException("1과 2 중에서 선택해주세요.");
+        }
+    }
+
+    private static boolean isValidCommand(int command) {
+        return command == CONTINUE || command == STOP;
     }
 }
