54fe1e8f803af771a494f8199fd36de6a9e98031
ygreenb <nhj7911@gmail.com>
2022-11-08 20:16:20 +0900

    feat: 컨트롤러 로직 생성

---
+++ src/main/java/baseball/controller/GameController.java
@@ -0,0 +1,24 @@
+package baseball.controller;
+
+import baseball.service.GameService;
+
+import static baseball.Constant.*;
+
+public class GameController {
+    private GameService gameService = new GameService();
+
+    public void start() {
+
+    }
+
+    public void proceed() {
+
+    }
+
+    public int checkRestartorExit() {
+        int selection = 0;
+
+
+        return selection;
+    }
+}
