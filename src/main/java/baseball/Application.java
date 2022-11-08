76b9b327416020be1b38be965ce308cf8579d063
wogus4048 <wogus4048@naver.com>
2022-11-08 22:51:32 +0900

    feat : 한게임이 끝날때까지 사이클을 도는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -89,4 +89,15 @@ public class Application {
         checkEachDigit(computersNumber, usersNumber, strikeBallCount, 0);
         return checkStrikeBallCount(strikeBallCount);
     }
+
+    public static int playOneGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        List<Integer> computersNumber = new ArrayList<>();
+        getComputersRandomNumber(computersNumber);
+        int oneCycleResult;
+        do {
+            oneCycleResult = playOneCycle(computersNumber);
+        } while (oneCycleResult == 0);
+        return oneCycleResult;
+    }
 }
