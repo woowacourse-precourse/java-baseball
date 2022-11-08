e6ae1ce915b8b61b096e13bd6724acadd805d0b4
This2sho <zx00018@naver.com>
2022-11-08 13:50:53 +0900

    refactor(BallTest): 변수명 변경
    isError -> hasError

--- src/test/java/baseball/domain/BallTest.java
+++ src/test/java/baseball/domain/BallTest.java
@@ -13,12 +13,12 @@ class BallTest {
 		"9, 0, false", "9, 2, false",
 		"0, 0, true", "1, 3, true",
 		"10, 0, true", "10, 3, true"})
-	public void 야구공_유효성_검증(int ballNumber, int ballIndex, boolean isError) {
-		if (!isError) {
+	public void 야구공_유효성_검증(int ballNumber, int ballIndex, boolean hasError) {
+		if (!hasError) {
 			Ball ball = new Ball(ballNumber, ballIndex);
 			assertThat(ball).isNotNull();
 		}
-		if (isError) {
+		if (hasError) {
 			assertThatThrownBy(() -> new Ball(ballNumber, ballIndex))
 				.isInstanceOf(IllegalArgumentException.class);
 		}
