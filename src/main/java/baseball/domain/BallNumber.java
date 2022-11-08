5374b606bf146c21c6af610f212bc6a8b131d279
조병우 <whquddn55@gmail.com>
2022-11-08 10:17:38 +0900

    feat(BallNumber): implement BallNumber class and factory method.
    Implemented BallNumber class that is represents number of ball. And this class can generate instance by factory method named fromIntegerList.
    
    + private BallNumber(List<Integer>)
    + static public BallNumber frontIntegerList(List<Integer>)
    + getters

--- src/main/java/baseball/domain/BallNumber.java
+++ src/main/java/baseball/domain/BallNumber.java
@@ -1,7 +1,20 @@
 package baseball.domain;
 
-/**
- * TODO: implement the class
- */
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class BallNumber {
+    List<Integer> numberList;
+
+    private BallNumber(List<Integer> numberList) {
+        this.numberList = numberList;
+    }
+
+    static public BallNumber fromIntegerList(List<Integer> ballNumberList) {
+        return new BallNumber(ballNumberList);
+    }
+
+    public Integer get(int i) {
+        return numberList.get(i);
+    }
 }
