2287c2446a71698a81ce86f57217afe6aae60396
iphuck22 <eldeshue@naver.com>
2022-11-08 15:10:26 +0900

    feature($Application.java): add front end of the project at main method.
    add front end feature at the main method in the Application.java
    - send game start message
    - get command '1' or '2' and decide to replay or not
    - throw IllegalArgumentException error if the command is neither 1 nor 2

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(true){
+            NumberBaseball Game = new NumberBaseball();
+            Game.playBall();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            int replay = Console.readLine().charAt(0) - 48; // value of '0' = 48
+            if( replay == 1){
+                continue;
+            }
+            else if( replay == 2){
+                break;
+            }
+            else{ // wrong input
+                throw new IllegalArgumentException();
+            }
+        }
     }
 }
