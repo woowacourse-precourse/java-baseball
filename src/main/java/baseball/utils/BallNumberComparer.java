df1e72fe29234f83cbf32bbdd0b9ca8446764f52
조병우 <whquddn55@gmail.com>
2022-11-08 15:49:35 +0900

    feat(BallNumberComparer): implement calculating score method
    This method calculatiing stirke, ball count and return Score instnace.
    
    + public Score calculateScore(BallNumber, BallNumer)
    + private int calculateStrikeScore(BallNumber, BallNumer)
    + private int calcuilateBallScore(BallNumber, BallNumer)

--- src/main/java/baseball/utils/BallNumberComparer.java
+++ src/main/java/baseball/utils/BallNumberComparer.java
@@ -1,7 +1,43 @@
 package baseball.utils;
 
-/**
- * TODO: implement the class
- */
+import static baseball.BaseballGame.INPUT_SIZE;
+
+import baseball.domain.BallNumber;
+import baseball.domain.Score;
+
 public class BallNumberComparer {
+    public Score calculateScore(BallNumber playerBallNumber, BallNumber computerBallNumber) {
+        int strikeScore = calculateStrikeScore(playerBallNumber, computerBallNumber);
+        int ballScore = calculateBallScore(playerBallNumber, computerBallNumber);
+        return new Score(strikeScore, ballScore);
+    }
+
+    private int calculateStrikeScore(BallNumber playerBallNumber, BallNumber computerBallNumber) {
+        int strikeScore = 0;
+        for (int i = 0; i < INPUT_SIZE; ++i) {
+            if (playerBallNumber.get(i).equals(computerBallNumber.get(i))) {
+                ++strikeScore;
+            }
+        }
+        return strikeScore;
+    }
+
+    /**
+     * TODO: 2중 for문 refactoring 필요함.
+     */
+    private int calculateBallScore(BallNumber playerBallNumber, BallNumber computerBallNumber) {
+        int ballScore = 0;
+        for (int i = 0; i < INPUT_SIZE; ++i) {
+            for (int j = 0; j < INPUT_SIZE; ++j) {
+                if (i == j) {
+                    continue;
+                }
+
+                if (playerBallNumber.get(i).equals(computerBallNumber.get(j))) {
+                    ++ballScore;
+                }
+            }
+        }
+        return ballScore;
+    }
 }
