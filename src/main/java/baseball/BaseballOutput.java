dbe2bdb89bd4c3acd23c6a1871c880a3076a72b8
i960107 <i960107@naver.com>
2022-11-08 15:24:45 +0900

    refactor(BaseballOutput): make all methods static

--- src/main/java/baseball/BaseballOutput.java
+++ src/main/java/baseball/BaseballOutput.java
@@ -6,15 +6,15 @@ public class BaseballOutput {
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String INPUT_PLAY_CODE_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
-    public void printInputPlayCodeMessage() {
+    public static void printInputPlayCodeMessage() {
         System.out.println(INPUT_PLAY_CODE_MESSAGE);
     }
 
-    public void printInputNumberMessage() {
+    public static void printInputNumberMessage() {
         System.out.print(INPUT_NUMBER_MESSAGE);
     }
 
-    public void printGameStart() {
+    public static void printGameStart() {
         System.out.println(GAME_START_MESSAGE);
     }
 }
