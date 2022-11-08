7dcd00e026b90d679218492e2fd273657d0fbe0d
kimdoha <kkhhjj3393@gmail.com>
2022-11-08 13:44:14 +0900

    feat(BaseballGame): Game Continuity 메세지 출력 함수 구현

--- src/main/java/baseball/game/GameContinuity.java
+++ src/main/java/baseball/game/GameContinuity.java
@@ -13,5 +13,10 @@ public class GameContinuity {
     printGameContinuityMessage();
     continuityInput();
   }
-  
+
+  public void printGameContinuityMessage(){
+    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    System.out.println("게임을 새로 시작하려면 1, 종료하면 2를 입력하세요.");
+  }
+
 }
