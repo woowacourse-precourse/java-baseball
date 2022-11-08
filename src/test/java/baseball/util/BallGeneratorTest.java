11af5b9984295634ce80cb65395c0a852f0440e2
minju412 <ln8847@ajou.ac.kr>
2022-11-08 21:00:59 +0900

    test: add BallGeneratorTest

--- src/test/java/baseball/util/BallGeneratorTest.java
+++ src/test/java/baseball/util/BallGeneratorTest.java
@@ -43,8 +43,19 @@ public class BallGeneratorTest {
     }
 
     @Test
-    void 사용자_공이_3개가_아니라면_예외가_발생합니다() {
+    void 사용자_공이_4개라면_예외가_발생합니다() {
         String input = "1234";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        assertThatThrownBy(() -> {
+            ballGenerator.createPlayerBalls();
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 사용자_공이_2개라면_예외가_발생합니다() {
+        String input = "12";
         InputStream in = new ByteArrayInputStream(input.getBytes());
         System.setIn(in);
 
