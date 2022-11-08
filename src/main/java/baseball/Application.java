9507cd1f0ebf44a1ded81e545a6e2c8862f0ceba
sjh910805@gmail.com <sjh910805@gmail.com>
2022-11-08 18:41:54 +0900

    fix: 빌드 오류 수정중 8

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Application {
-    public static void main(String[] args) throws IllegalArgumentException {
+    public static void main(String[] args) {
         try {
             Operator op = new Operator();
             op.playGameReady();
