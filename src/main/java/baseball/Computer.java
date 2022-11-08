6fd2bebee20ce9a8e7f263772bd3f312333bc5ae
Cho-El <syzzang21c@gmail.com>
2022-11-08 11:23:31 +0900

    feat(Computer): 랜덤으로 3가지 숫자를 뽑아서 맞출 수 생성
    1. Computer 클래스 생성
    2. 랜덤 타겟 넘버 리스트로 담아주는 함수 구현
    3. 타겟 넘버 리스트 Getter

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,35 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Computer {
+    private static final int MIN_NUMBER = 1;
+    private static final int MAX_NUMBER = 9;
+    private static final int NUMBER_LENGTH = 3;
+    private List<Integer> targetNumList = new ArrayList<>();
+
+    public Computer() {
+    }
+
+    public void TargetNumberGenerator() {
+        Set<Integer> targetSet = new HashSet<>();
+
+        while (targetSet.size() < NUMBER_LENGTH) {
+            targetSet.add(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
+        }
+        this.targetNumList = SetToList(targetSet);
+    }
+
+    public List<Integer> SetToList(Set<Integer> target) {
+        return new ArrayList<>(target);
+    }
+
+    public List<Integer> getTargetNumber() {
+        return targetNumList;
+    }
+}
