7ef91c2579f837e1f058f5d9a0efc778261385c9
Jeeseob <jeeseob5761@gmail.com>
2022-11-08 15:27:03 +0900

    feat: 게임 진행중 출력되는 comment 추가

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -8,19 +8,21 @@ import java.util.List;
  */
 public class BaseBallGame {
     // TODO: 게임 실행부 작성
-    private static final int LENGTH_OF_NUMBER = 3;
     public void start() {
         // TODO: 게임 진행시 처음에만 출력되는 Comment 출력
-        List<Integer> answer = BaseBallGameUtil.createAnswerByLength(LENGTH_OF_NUMBER);
+        System.out.println(Constant.START_MESSAGE);
+        List<Integer> answer = BaseBallGameUtil.createAnswerByLength(Constant.LENGTH_OF_NUMBER);
         List<Integer> userAnswer;
         do {
             // TODO: 게임 로직 작성
+            System.out.println(Constant.INPUT_MESSAGE);
             userAnswer = BaseBallGameUtil.inputUserAnswer();
             BallCount ballCount = new BallCount();
             ballCount.calc(answer, userAnswer);
             System.out.println(ballCount.toString());
             // TODO: 게임 진행 중 콘솔로 출력되는 Comment 출력
         } while(!isFinished(answer, userAnswer));
+        System.out.println(Constant.FINISH_MESSAGE);
     }
 
     // TODO: 게임 종료 여부 판단
---
+++ src/main/java/baseball/Constant.java
@@ -0,0 +1,12 @@
+package baseball;
+
+/**
+ * @Author : Jeeseob
+ * @CreateAt : 2022/11/08
+ */
+public class Constant {
+    public static int LENGTH_OF_NUMBER = 3;
+    public static String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    public static String FINISH_MESSAGE = LENGTH_OF_NUMBER + "개의 숫자를 모두 맞히셨습니다! 게임 종료";
+}
