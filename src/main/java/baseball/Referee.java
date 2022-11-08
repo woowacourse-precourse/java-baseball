43cc536a0668340665de991d917e219d5b614573
Cho-El <syzzang21c@gmail.com>
2022-11-08 12:54:24 +0900

    feat(Referee): 숫자 평가 기능
    1. 스트라이크, 볼, 낫싱 판정 기능 구현
    2. 상황에 맞는 메시지 리턴해주는 기능 구현

--- src/main/java/baseball/Referee.java
+++ src/main/java/baseball/Referee.java
@@ -1,11 +1,43 @@
 package baseball;
 
+import java.util.List;
+
 public class Referee {
 
-    private int ball;
-    private int strike;
+    private int ball = 0;
+    private int strike = 0;
+    private static final String RESULT_NOTHING = "낫싱";
+    private static final String RESULT_STRIKE = "스트라이크";
+    private static final String RESULT_BALL = "볼";
+    
+    private void setBallandStrike(List<Integer> targetNum, List<Integer> userInput) {
+        initBallAndStrike();
+        for (int i = 0; i < targetNum.size(); i++) {
+            if (targetNum.get(i).equals(userInput.get(i))) {
+                strike++;
+            }
+            else {
+                ball++;
+            }
+        }
+    }
 
-    private void setBallandStrike() {
-        
+    private void initBallAndStrike() {
+        this.ball = 0;
+        this.strike = 0;
+    }
+
+    private String resultMessage() {
+        String result = "";
+        if (ball == 0 && strike == 0) {
+            result += RESULT_NOTHING;
+        } else if (ball > 0 && strike > 0) {
+            result += ball + RESULT_BALL + " " + strike + RESULT_STRIKE;
+        } else if (ball > 0) {
+            result += ball + RESULT_BALL;
+        } else if (strike > 0) {
+            result += strike + RESULT_STRIKE;
+        }
+        return result;
     }
 }
