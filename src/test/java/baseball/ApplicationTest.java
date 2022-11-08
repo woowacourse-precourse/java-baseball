7f02137e2b84037ba0684586b53772c4442a75ce
bfinecpa1@naver.com <bfinecpa1@naver.com>
2022-11-08 21:16:16 +0900

    test: 테스트 추가
    - validateContinueEnd 테스트 코드 추가
    - printPoint 테스트 코드 추가
    - findPoint 테스트 코드 추가
    - continueOrEnd 테스트 코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,8 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.io.ByteArrayInputStream;
+import java.util.HashMap;
+import java.util.List;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
@@ -42,6 +43,51 @@ class ApplicationTest extends NsTest {
         assertThatThrownBy(()-> application.validateThreeDifferentDigits("103")).isInstanceOf(IllegalArgumentException.class);
     }
 
+    @Test
+    void validateContinueEndTest(){
+        Application application = new Application();
+        assertThatThrownBy(()-> application.validateContinueEnd(3)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void printPointTest(){
+        Application application = new Application();
+        HashMap<String, Integer> ballStrikeNum = new HashMap<>();
+        ballStrikeNum.put("ballNum", 1);
+        ballStrikeNum.put("strikeNum", 1);
+        application.printPoint(ballStrikeNum);
+        ballStrikeNum.put("ballNum" , 0);
+        ballStrikeNum.put("strikeNum", 0);
+        application.printPoint(ballStrikeNum);
+        ballStrikeNum.put("ballNum" , 0);
+        ballStrikeNum.put("strikeNum", 3);
+        application.printPoint(ballStrikeNum);
+        assertThat(output()).contains("1볼 1스트라이크", "낫싱","3스트라이크");
+    }
+
+    @Test
+    void findPointTest(){
+        Application application = new Application();
+        HashMap<String, Integer> ballStrikeNum = application.initializeBallStrikePoint();
+        application.findPoint(List.of(1, 3, 5), "145",ballStrikeNum);
+        assertThat(ballStrikeNum.get("strikeNum")).isEqualTo(2);
+        assertThat(ballStrikeNum.get("ballNum")).isEqualTo(0);
+    }
+
+    @Test
+    void continueOrEndTest(){
+        Application application = new Application();
+        command("2");
+        assertThat(application.continueOrEnd()).isFalse();
+        command("1");
+        assertThat(application.continueOrEnd()).isTrue();
+    }
+
+    private void command(final String... args) {
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    }
+    
 
     @Override
     public void runMain() {
