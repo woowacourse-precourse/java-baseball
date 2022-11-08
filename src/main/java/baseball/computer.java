23e8b052a8c44e4d4ac112e7c05459d0a000bab8
nomushroomplz <aaron97@knou.ac.kr>
2022-11-08 19:25:22 +0900

    [feat] 컴퓨터 3자리  랜덤숫자 생성

--- src/main/java/baseball/computer.java
+++ src/main/java/baseball/computer.java
@@ -3,5 +3,14 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 public class computer {
-
+    public static List<Integer> computerNumber(){
+        List<Integer> computerBaseballNumber = new ArrayList<>();
+        while (computerBaseballNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computerBaseballNumber.contains(randomNumber)){
+                computerBaseballNumber.add(randomNumber);
+            }
+        }
+        return computerBaseballNumber;
+    }
 }
