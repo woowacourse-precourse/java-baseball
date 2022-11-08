24a0d4848a7343e63853d20b987acf9664f93d4f
kimdoha <kkhhjj3393@gmail.com>
2022-11-08 17:52:31 +0900

    test(Computer): 테스트 요구사항 확인을 위한 Computer 생성자와 검증 메소드 추가
    - 컴퓨터가 생성하는 숫자가 세 자리가 아닐 때 오류 발생
    - 중복 제거 고려

--- src/main/java/baseball/domain/player/Computer.java
+++ src/main/java/baseball/domain/player/Computer.java
@@ -7,12 +7,25 @@ import java.util.Set;
 
 
 public class Computer {
-  private static final Integer NUMBER_SIZE = 3;
+  public static final Integer COMPUTER_NUMBER_SIZE = 3;
+  public static final String COMPUTER_NUMBER_SIZE_ERROR_MESSAGE = "컴퓨터가 생성하는 숫자는 세 자리이어야 합니다.";
+  public static final String COMPUTER_DUPLICATE_NUMBER_ERROR_MESSAGE = "컴퓨터가 생성하는 각각의 숫자는 서로 중복되지 않습니다.";
+
   private Set<Integer> computer = new LinkedHashSet<>();
 
   public Computer() {
     init();
     createRandomThreeDigitNumber();
+    validateComputerNumber();
+  }
+
+  /*
+   * [TEST] 컴퓨터의 숫자가 세 자리 미만이거나 초과일 때 오류 발생
+   */
+  public Computer(List<Integer> numList) {
+    init();
+    computer.addAll(numList);
+    validateComputerNumber();
   }
 
   private void init(){
@@ -20,17 +33,23 @@ public class Computer {
   }
 
   private void createRandomThreeDigitNumber() {
-    while (getSetSize() < NUMBER_SIZE) {
+    while (getSize() < COMPUTER_NUMBER_SIZE) {
       Number randomNumber = new Number();
       this.computer.add(randomNumber.getNumber());
     }
   }
 
-  private Integer getSetSize(){
+  private void validateComputerNumber() {
+    if(!getSize().equals(COMPUTER_NUMBER_SIZE)){
+      throw new IllegalArgumentException(COMPUTER_NUMBER_SIZE_ERROR_MESSAGE);
+    }
+  }
+
+  private Integer getSize(){
     return this.computer.size();
   }
 
-  public List<Integer> getComputerNumberList() {
+  public List<Integer> getNumberList() {
     return List.copyOf(this.computer);
   }
 }
