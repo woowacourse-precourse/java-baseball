4a41f2458ddfbf3e06a9454c221153565c1ef245
youswim <youswim96@gmail.com>
2022-11-08 22:39:08 +0900

    test: 테스트 코드 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,12 +3,16 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+    
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -26,6 +30,139 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void getRandomIntList_사이즈3_반환확인() {
+        List<Integer> list = Application.getRandomIntList();
+        assertThat(list.size()).isEqualTo(3);
+    }
+
+    @Test
+    void getInputInt_정상입력() {
+        assertThat(Application.getInputInt("123")).isEqualTo(123);
+    }
+    
+    @Test
+    void validateInput_길이_3미만() {
+        assertThatThrownBy(() -> Application.validateInput("12"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 입력!");
+    }
+
+    @Test
+    void validateInput_길이_3초과() {
+        assertThatThrownBy(() -> Application.validateInput("1234"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 입력!");
+    }
+
+    @Test
+    void validateInput_입력범위_초과() {
+        assertThatThrownBy(() -> Application.validateInput("Abc"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 입력!");
+    }
+
+    @Test
+    void validateInput_중복숫자_입력() {
+        assertThatThrownBy(() -> Application.validateInput("112"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 입력!");
+    }
+    
+    
+
+    @Test
+    void grading_낫싱() {
+        List<Integer> randomInt = new ArrayList<>();
+        randomInt.add(1);
+        randomInt.add(2);
+        randomInt.add(3);
+        Grade grade = new Grade(0, 0);
+        Grade output = Application.grading(randomInt, 456);
+        assertThat(grade).isEqualTo(output);
+    }
+
+    @Test
+    void grading_3볼() {
+        List<Integer> randomInt = new ArrayList<>();
+        randomInt.add(1);
+        randomInt.add(2);
+        randomInt.add(3);
+        Grade grade = new Grade(3, 0);
+        Grade output = Application.grading(randomInt, 312);
+        assertThat(grade).isEqualTo(output);
+    }
+
+    @Test
+    void grading_1볼_1스트라이크() {
+        List<Integer> randomInt = new ArrayList<>();
+        randomInt.add(1);
+        randomInt.add(2);
+        randomInt.add(3);
+        Grade grade = new Grade(1, 1);
+        Grade output = Application.grading(randomInt, 134);
+        assertThat(grade).isEqualTo(output);
+    }
+
+    @Test
+    void grading_3스트라이크() {
+        List<Integer> randomInt = new ArrayList<>();
+        randomInt.add(1);
+        randomInt.add(2);
+        randomInt.add(3);
+        Grade grade = new Grade(0, 3);
+        Grade output = Application.grading(randomInt, 123);
+        assertThat(grade).isEqualTo(output);
+    }
+
+    @Test
+    void makeGradeMessage_0볼_0스트라이크() {
+        Grade grade = new Grade(0, 0);
+        String message = Application.makeGradeMessage(grade);
+        assertThat(message).isEqualTo("낫싱");
+    }
+
+    @Test
+    void makeGradeMessage_1볼_0스트라이크() {
+        Grade grade = new Grade(1, 0);
+        String message = Application.makeGradeMessage(grade);
+        assertThat(message).isEqualTo("1볼");
+    }
+
+    @Test
+    void makeGradeMessage_0볼_1스트라이크() {
+        Grade grade = new Grade(0, 1);
+        String message = Application.makeGradeMessage(grade);
+        assertThat(message).isEqualTo("1스트라이크");
+    }
+
+    @Test
+    void makeGradeMessage_1볼_1스트라이크() {
+        Grade grade = new Grade(1, 1);
+        String message = Application.makeGradeMessage(grade);
+        assertThat(message).isEqualTo("1볼 1스트라이크");
+    }
+
+    @Test
+    void getChoice_1입력() {
+        int choice = Application.getChoice("1");
+        assertThat(choice).isEqualTo(1);
+    }
+
+    @Test
+    void getChoice_2입력() {
+        int choice = Application.getChoice("2");
+        assertThat(choice).isEqualTo(2);
+    }
+
+    @Test
+    void getChoice_예외입력() {
+
+        assertThatThrownBy(() -> Application.getChoice("예외"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("잘못된 입력!");
     }
 
     @Override
