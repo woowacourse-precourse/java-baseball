afaa6208be5ce267c8bf6ccaf70e269c38aec762
kki4504 <rhkddlf9876@gmail.com>
2022-11-08 11:19:35 +0900

    feat : 재시작 값 유효성 검사 기능 구현

--- src/main/java/baseball/validation/Restart.java
+++ src/main/java/baseball/validation/Restart.java
@@ -1,4 +1,25 @@
 package baseball.validation;
 
+import java.util.regex.Pattern;
+
 public class Restart {
+    boolean validate(String restartValue) {
+        if (lengthCheck(restartValue)) {
+            return valueCheck(restartValue);
+        } else {
+            return false;
+        }
+    }
+
+    private boolean lengthCheck(String restartValue) {
+        if (restartValue.length() == 1) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean valueCheck(String restartValue) {
+        final String REGEX = "^[1-2]*$";
+        return Pattern.matches(REGEX, restartValue);
+    }
 }
