8e5c00a69144bc0df0e052228f062531eb3ab2fd
mingyunLEE <leemingyun@naver.com>
2022-11-08 20:10:25 +0900

    Add Function [3스트라이크 출력]

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -83,4 +83,12 @@ public class Application {
         }
         return hint;
     }
+
+    private static  boolean threeStrike(int strike){
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
+    }
 }
