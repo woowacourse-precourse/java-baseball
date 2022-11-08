962a951712f781dbd34add702558131ec6227949
davinc71998 <davinc71998@gmail.com>
2022-11-08 15:42:31 +0900

    FEAT: 고침 createRandomNumber 함수
    매개변수로 값을 넣어줄 리스트를 받을 필요 없이 안에서 생성해서 리턴하도록 변경

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,7 +14,8 @@ public class Application {
     static final int COUNT_INITALIZE = 0;
     static final int GAME_END_STRIKE_COUNT = 3;
 
-    public static List<Integer> createRandomNumber(List<Integer> computer) {
+    public static List<Integer> createRandomNumber() {
+        List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
@@ -75,7 +76,7 @@ public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        List<Integer> computerCreateNumber = new ArrayList<>();
+        List<Integer> computerCreateNumber = createRandomNumber();
         List<Integer> userInputNumber = new ArrayList<>();
         int gameContinue = gameRestart;
         while(gameContinue != gameEnd) {
