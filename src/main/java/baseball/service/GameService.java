372ac0e827eff05e4c01f8813ab70219339bdfe3
ygreenb <nhj7911@gmail.com>
2022-11-08 19:46:17 +0900

    feat: 컴퓨터 임의의 수/사용자 입력 리스트 반환

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -12,11 +12,25 @@ public class GameService {
     public static List<Integer> makeComputerNumber() {
         List<Integer> computer = new ArrayList<>();
 
+        while (computer.size() < NUMBER_LIMIT) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
         return computer;
     }
 
     public List<Integer> makeUserNumber(String input) {
         List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            int nextNumber = (input.charAt(i)) - '0';
+
+            if (nextNumber != ' ' - '0') {
+                list.add(nextNumber);
+            }
+        }
 
         return list;
     }
