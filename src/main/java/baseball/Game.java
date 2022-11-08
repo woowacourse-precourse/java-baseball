330fff47e0799c4925ac87fae603f70f11b5992d
meunrud <y3a0522@gmail.com>
2022-11-08 21:35:17 +0900

    :art: 오타 수정

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -84,7 +84,7 @@ public class Game {
         return strike == BALLSIZE;
     }
 
-    public boolean askResrat(){
+    public boolean askRestart(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         inputNumber.userChoiceRestartOrEnd();
         String yesOrNo = inputNumber.getUserNumber();
