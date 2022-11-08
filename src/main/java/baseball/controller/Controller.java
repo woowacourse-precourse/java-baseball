740cc41502899fb569ccb7c0bfeef88e1a355edc
eun-byeol <62530884+eun-byeol@users.noreply.github.com>
2022-11-08 16:46:21 +0900

    feat(Application): implement the function to run baseball game

--- src/main/java/baseball/controller/Controller.java
+++ src/main/java/baseball/controller/Controller.java
@@ -1,2 +1,62 @@
-package baseball.controller;public class Controller {
+package baseball.controller;
+
+import baseball.model.GameJudge;
+import baseball.model.InputNumber;
+import baseball.model.GameMessage;
+import baseball.model.RandomNumber;
+import baseball.view.GameResult;
+
+import java.util.List;
+
+public class Controller {
+    private static boolean isGameOver;
+
+    public static void runBaseballGame() {
+        isGameOver = false;
+        GameMessage.printGameStartMessage();
+        while (!isGameOver) {
+            try {
+                playOneGame();
+                askRestartGame();
+            } catch (IllegalArgumentException error) {
+                System.out.println("message: " + error.getMessage());
+                isGameOver = true;
+                throw error;
+            }
+        }
+    }
+
+    private static void playOneGame() {
+        List<Integer> computer = RandomNumber.setRandomNumber();
+        System.out.println("컴퓨터: " + computer);
+        while (!isGameOver) {
+            GameMessage.printInputNumberMessage();
+            List<Integer> player = InputNumber.getValidNumberList();
+            //System.out.println("플레이어: " + player);
+
+            String result = GameJudge.judgeGameResult(player, computer);
+            GameResult.printGameResultMessage(result);
+
+            if (GameJudge.allStrike) {
+                GameMessage.printGameOverMessage();
+                break;
+            }
+        }
+    }
+
+    private static void askRestartGame() {
+        final String RESTART_CHOICE = "1";
+        final String END_CHOICE = "2";
+
+        GameMessage.printRestartGameQuestionMessage();
+        String restartGameInput = InputNumber.getValidChoice();
+        //System.out.println("restart?" + restartGameInput);
+
+        if (restartGameInput.equals(RESTART_CHOICE)) {
+            isGameOver = false;
+        }
+        if (restartGameInput.equals(END_CHOICE)) {
+            isGameOver = true;
+        }
+    }
 }
--- src/main/java/baseball/model/GameJudge.java
+++ src/main/java/baseball/model/GameJudge.java
@@ -1,34 +1,35 @@
 package baseball.model;
 
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class GameJudge {
-    public String judgeGameResult(List<Integer> playerNumber, List<Integer> computerNumber) {
+    public static boolean allStrike = false;
+
+    public static String judgeGameResult(List<Integer> playerNumber, List<Integer> computerNumber) {
         int strike = 0;
         int ball = 0;
 
-        for (int i = 0; i< playerNumber.size(); i++){
-            if (isStrike(playerNumber.get(i), computerNumber.get(i))){
+        for (int i = 0; i < playerNumber.size(); i++) {
+            if (isStrike(playerNumber.get(i), computerNumber.get(i))) {
                 strike++;
                 continue;
             }
-            if (isBall(playerNumber.get(i), computerNumber)){
+            if (isBall(playerNumber.get(i), computerNumber)) {
                 ball++;
             }
         }
+        isAllStrike(strike);
         return changeResultMessage(strike, ball);
     }
 
-    boolean isStrike(int player, int computer){
-        if (player == computer){
+    private static boolean isStrike(int player, int computer) {
+        if (player == computer) {
             return true;
         }
         return false;
     }
 
-    boolean isBall(int player, List<Integer> computerNumberList){
+    private static boolean isBall(int player, List<Integer> computerNumberList) {
         for (Integer number : computerNumberList) {
             if (player == number) {
                 return true;
@@ -37,13 +38,21 @@ public class GameJudge {
         return false;
     }
 
-    String changeResultMessage(int strike, int ball) {
+    private static void isAllStrike(int strike) {
+        if (strike == 3) {
+            allStrike = true;
+        } else {
+            allStrike = false;
+        }
+    }
+
+    private static String changeResultMessage(int strike, int ball) {
         String result = "";
         if (ball > 0) {
             result = ball + "볼";
         }
         if (strike > 0) {
-            if (!result.isEmpty()){
+            if (!result.isEmpty()) {
                 result += " ";
             }
             result += strike + "스트라이크";
@@ -53,18 +62,4 @@ public class GameJudge {
         }
         return result;
     }
-
-//    public static void main(String[] args) {
-//        ArrayList<Integer> player = new ArrayList<Integer>(List.of(1, 2, 3));
-//        ArrayList<Integer> computer = new ArrayList<Integer>(List.of(7, 4, 8));
-//        GameJudge judge = new GameJudge();
-//        System.out.println(judge.judgeGameResult(player, computer));
-//    }
 }
-/*
-[_, _, _]
-같은 자리, 같은 숫자 = S
-같은 숫자 = B
-모두 아님 = N
-
- */
