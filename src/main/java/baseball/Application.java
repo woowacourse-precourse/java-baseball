f90cf4541a3a0a14a240af43a3adc233567671db
jeonyuneo <piq2255@gmail.com>
2022-11-08 23:01:45 +0900

    feat: 게임 시작 문구를 출력한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,9 @@
 package baseball;
 
+import baseball.view.View;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        View.printStart();
     }
 }
