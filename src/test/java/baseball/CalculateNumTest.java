ea8bdeb3e09a73de7d240764f79ec43fe795e141
Jongha Park <jongha2788@u.sogang.ac.kr>
2022-11-08 21:52:39 +0900

    test(CalculaterNumTest) : testcase 추가

---
+++ src/test/java/baseball/CalculateNumTest.java
@@ -0,0 +1,37 @@
+package baseball;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class CalculateNumTest {
+
+    private static Calculator calculator;
+
+    @BeforeEach
+    public void beforeEach() {
+        calculator = new Calculator();
+    }
+
+
+    @Test
+    void 볼_스트라이크_계산(){
+        calculator.calculateNum("123","234");
+        assertThat(calculator.getBallNum()).isEqualTo(2);
+        assertThat(calculator.getStrikeNum()).isEqualTo(0);
+    }
+
+    @Test
+    void 볼_증가(){
+        int ballnum =  calculator.getBallNum();
+        calculator.addBall();
+        assertThat(calculator.getBallNum()).isEqualTo(ballnum+1);
+    }
+
+    @Test
+    void 스트라이크_증가(){
+        int strikeNum =  calculator.getStrikeNum();
+        calculator.addStrike();
+        assertThat(calculator.getStrikeNum()).isEqualTo(strikeNum+1);
+    }
+}
