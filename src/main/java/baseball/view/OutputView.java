722d562af82aa5be44106152ad785c39d333933a
Sieun Park <psieun0606@gmail.com>
2022-11-08 17:56:12 +0900

    feat: 힌트를 출력해주는 기능 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,7 +1,29 @@
 package baseball.view;
 
+import baseball.model.User;
+
 public class OutputView {
     private String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    User user = new User();
+
+    public void printHint() {
+        int strike = user.getStrike();
+        int ball = user.getBall();
+
+        if (strike == 0 && ball == 0) {
+            System.out.println("낫싱");
+        }
+        else if (strike == 0) {
+            System.out.println(String.format("%d볼", ball));
+        }
+        else if (ball == 0) {
+            System.out.println(String.format("%d스트라이크", strike));
+        }
+        else {
+            System.out.println(String.format("%d볼 %d스트라이크",ball, strike));
+        }
+    }
 
     public void printGameEnd() {
         System.out.println(GAME_END);
