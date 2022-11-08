8ae5df82f3508543e600b1392d8357c90b3ebd59
juno-junho <ssmm0205@naver.com>
2022-11-08 18:28:03 +0900

    feat(view): create game start input method
    게임 시작 시 숫자를 입력 받는 gameStartInput 메서드 생성

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,10 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public static String gameStartInput() {
+        System.out.print("숫자를 입력해주세요 : ");
+        return Console.readLine();
+    }
 }
