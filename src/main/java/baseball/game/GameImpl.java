89eed5dd5eac92b602f5fa71b0d859006bbcf307
kimdoha <kkhhjj3393@gmail.com>
2022-11-08 13:51:50 +0900

    feat(BaseballGame): Game Init 메소드 구현
    - 게임이 시작되면, 정해진 게임 시작 문구 출력

--- src/main/java/baseball/game/GameImpl.java
+++ src/main/java/baseball/game/GameImpl.java
@@ -11,4 +11,10 @@ public class GameImpl implements Game{
   public Score score;
   public Boolean flag = true;
 
+  @Override
+  public void GameInit() {
+    final String INIT_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    System.out.println(INIT_MESSAGE);
+  }
+
 }
