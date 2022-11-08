e0abe0d17c382d3e21eb12f906d9c3519d0e764b
dldmsdn99 <dldmsdn99@naver.com>
2022-11-08 16:58:55 +0900

    컴퓨터(상대방) 랜덤 숫자 생성기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
+import camp.nextstep.edu.missionutils.*;
+import java.util.*;
+
+
+
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
+
+     static ArrayList<Integer> randomComputer(ArrayList<Integer> computer){
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        } return computer;
+    }
+
 }
