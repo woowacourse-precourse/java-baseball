02c7580869f80a31a82d827ce59095d039d05db7
kimdoha <kkhhjj3393@gmail.com>
2022-11-08 20:39:46 +0900

    refactor(User): 테스트 요구사항 확인을 위한 User 생성자와 입력 값 중복 검증 메소드 추가
    - 유저 생성자 추가
    - 유저 입력 값 중복 검증 메소드 구현
    - 유저 입력 값 검증 메소드 리팩터링

--- src/main/java/baseball/domain/player/User.java
+++ src/main/java/baseball/domain/player/User.java
@@ -2,11 +2,31 @@ package baseball.domain.player;
 
 import baseball.utils.Input;
 import baseball.utils.Regex;
-import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 public class User {
-  private List<Integer> user = new ArrayList<>();
+  public static final String USER_INPUT_ERROR_MESSAGE = "유저가 입력하는 숫자는 한 자리 이상 세 자리 이하, 각각은 1에서 9사이의 숫자여야 합니다.";
+  public static final String USER_DUPLICATE_INPUT_ERROR_MESSAGE = "유저가 입력하는 각각의 숫자는 서로 중복되지 않습니다.";
+
+  private Set<Integer> user = new LinkedHashSet<>();
+
+  public User() {
+    init();;
+  }
+
+  /*
+   * [TEST]
+   * 1. 유저가 입력하는 숫자가 1 ~ 9 사이의 숫자 스트링이 아닐 때 오류 발생
+   * 2. 유저가 입력하는 숫자가 한 자리 이상 세 자리 이하가 아닐 때 오류 발생
+   * 3. 숫자가 아닌 스트링일 때 오류 발생
+   * 4. 유저가 입력한 숫자 중 중복된 값이 있을 때 오류 발생
+   */
+  public User(String input){
+    init();
+    validateInputValue(input);
+  }
 
   private void init(){
     user.clear();
@@ -15,24 +35,34 @@ public class User {
   public void createUserInput(){
     init();
     Input input = new Input();
-    isValidInputValue(input.getInput());
     putInputValueInList(input.toIntList());
+    validateInputValue(input.getInput());
   }
 
-  private void isValidInputValue(String input) {
-    if(!Regex.getRegexMatcherResult("^[1-9]{1,3}$", input)){
-      throw new IllegalArgumentException("invalid user input");
+  public void validateInputValue(String input) {
+    if(!isMatcherResult(input)){
+      throw new IllegalArgumentException(USER_INPUT_ERROR_MESSAGE);
+    }
+
+    if(!isDistinctInputValue(input)){
+      throw new IllegalArgumentException(USER_DUPLICATE_INPUT_ERROR_MESSAGE);
     }
   }
 
-  private void putInputValueInList(int[] numList){
-    for(int num : numList) {
-      user.add(num);
-    }
+  private boolean isMatcherResult(String input) {
+    return Regex.getRegexMatcherResult( "^[1-9]{1,3}$", input );
   }
 
-  public List<Integer> getUserNumberList(){
-    return this.user;
-  };
+  private boolean isDistinctInputValue(String input){
+   return this.user.size() == input.length();
+  }
+
+  private void putInputValueInList(List<Integer> numList){
+    user.addAll(numList);
+  }
+
+  public List<Integer> getNumberList(){
+    return List.copyOf(this.user);
+  }
 
 }
