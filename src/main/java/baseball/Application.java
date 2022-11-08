24c8146240bd2b189450155ddbc172ef9ca6b350
EUNSH1 <tkdgus968@naver.com>
2022-11-08 11:30:12 +0900

    feat : 숫자 야구 게임 프로그램 실행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,13 @@
 package baseball;
 
+import baseball.controller.BaseballGame;
+import baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        OutputView.printGameStart();
+        BaseballGame baseballGame = new BaseballGame();
+        baseballGame.start();
     }
 }
