f0936e480cd9b74ab431494990a552dc1b54d747
ckyeon <qsky8520@gmail.com>
2022-11-08 15:23:35 +0900

    feat(Player): add Player abstract class

---
+++ src/main/java/baseball/player/Player.java
@@ -0,0 +1,9 @@
+package baseball.player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Player {
+
+    private final List<String> selectBalls = new ArrayList<>();
+}
