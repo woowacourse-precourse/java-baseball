2525d1113940a01dde5e72c9d657449d6d3ec30d
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-08 23:47:17 +0900

    Update ApplicationTest.java

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -2,16 +2,11 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-
 
 class ApplicationTest extends NsTest {
     @Test
@@ -36,60 +31,5 @@ class ApplicationTest extends NsTest {
     @Override
     public void runMain() {
         Application.main(new String[]{});
-    }
-    
-    @Test
-    void 숫자2_입력시_게임종료() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("123", "456", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "게임 종료");
-                },
-                4, 5, 6
-        );
-    }
-
-    @Test
-    void 숫자1_입력시_() {
-        assertRandomNumberInRangeTest(
-                () -> {
-                    run("123", "456", "1", "213", "123", "2");
-                    assertThat(output()).contains("낫싱", "3스트라이크", "2볼 1스트라이크", "게임 종료");
-                },
-                4, 5, 6, 1, 2, 3
-        );
-    }
-    
-    @ParameterizedTest
-    @ValueSource(strings = {"111", "222", "333"})
-    void 중복된_수_(String no) {
-        // given
-        // when
-        // then
-        assertThatThrownBy(() -> new BaseballNumber(no))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageStartingWith("잘못된 수를 입력하셨습니다.");
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"1", "22", "4444", "125125134", " "})
-    void 범위_오류(String no) {
-        // given
-        // when
-        // then
-        assertThatThrownBy(() -> new BaseballNumber(no))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageStartingWith("잘못된 수를 입력하셨습니다.");
-    }
-    
-    @ParameterizedTest
-    @ValueSource(strings = {"안녕", "asd2", "vfs"})
-    void 수가_아닌_입력(String no) {
-        // given
-        // when
-        // then
-        assertThatThrownBy(() -> new BaseballNumber(no))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageStartingWith("잘못된 수를 입력하셨습니다.");
     }
 }
