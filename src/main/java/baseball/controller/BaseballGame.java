ff73e3180a70db742a979bddc80ae11a33bb46ba
hwanee__ <fkfkthth@naver.com>
2022-11-08 20:26:34 +0900

    feat: 게임 시작 구현
    랜덤 숫자 생성 후 유저에게 번호 입력 받음

---
+++ src/main/java/baseball/controller/BaseballGame.java
@@ -0,0 +1,38 @@
+package baseball.controller;
+
+import baseball.model.ComputerNumber;
+import baseball.model.UserNumber;
+import baseball.view.UserView;
+
+public class BaseballGame {
+    private static final int CNT_NUMBER = 3;
+    private static final UserView userView = new UserView();
+
+    private int strike = 0;
+    private int ball = 0;
+    private boolean restart = false;
+
+    public BaseballGame(){}
+
+    public void StartGame(){
+        ComputerNumber computerNumber = new ComputerNumber();
+        UserNumber userNumber = new UserNumber();
+
+        computerNumber.setRandom();
+
+        while(!restart){
+            String input = userView.UserAnswer();
+            userNumber.setDigits(input);
+
+             strike = 0;
+             ball = 0;
+
+             for(int i=0;i<CNT_NUMBER;i++){
+                 //점수 계산 함수 먼저 구현
+             }
+
+        }
+
+    }
+
+}
