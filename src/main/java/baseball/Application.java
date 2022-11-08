9b10dfc04b380510b1335c82b7a72b04e5d20a79
0417taehyun <0417taehyun@gmail.com>
2022-11-08 20:19:56 +0900

    feat (Application): Application 클래스에서의 절차에 따른 숫자 야구 게임 최종 구축

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,35 +2,22 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        /*
-        TODO: 프로그램 구현
+        User user = new User();
+        Answer answer = new Answer();
 
-        v Number 인터페이스
-            v MAX_SIZE, START_NUMBER, END_NUMBER 상수
-            v create, get 메서드
-
-        - Answer 클래스
-            - Number 인터페이스 상속
-
-        v User 클래스
-            v Number 인터페이스 상속
-            v validateNumber 메서드
-            v isNumberValidMaxSize 메서드
-            v isNumberValidRange 메서드
-            v convertInputAsNumber
-            v isNumberValidUnique 메서드
-
-        - Comparison 클래스
-            v countBallNumbers
-            v countStrikeNumbers
-            v isBall
-            v isStrike
-
-        - Game 클래스
-            - printStart 메서드
-            - printEnd 메서드
-            - printRestart 메서드
-            - printResult 메서드
-         */
+        Game.start();
+        answer.create();
+        while (Game.isRepeat) {
+            if (Game.isRestart) {
+                answer.create();
+            }
+            System.out.println(answer.get());
+            user.create();
+            System.out.println(user.get());
+            if (Comparison.isAnswered(user.get(), answer.get())) {
+                Game.printCongratsMessage();
+                user.createRepeatInput();
+            }
+        }
     }
 }
