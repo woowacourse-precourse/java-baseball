2d98b06a84b01bd985dab26f3697e0a94182ebd3
Jinseo Song <82093280+rxb8k@users.noreply.github.com>
2022-11-08 21:07:44 +0900

    feat: 숫자가 아닌 입력이 들어왔을 경우의 예외 처리 기능 추가

--- src/main/java/baseball/Input.java
+++ src/main/java/baseball/Input.java
@@ -6,6 +6,7 @@ public class Input {
     public String getUserNumber() {
         System.out.print("숫자를 입력해주세요 : ");
         String inputStr = Console.readLine();
+        checkNumberOnly(inputStr);
         checkLength(inputStr);
         checkNumber(inputStr);
         return inputStr;
@@ -23,6 +24,15 @@ public class Input {
                 || (inputStr.charAt(0) == inputStr.charAt(2))
         ) { // 각 자리수 중 서로 같은 수가 있다면
             throw new IllegalArgumentException("서로 다른 3자리의 수를 입력하세요.");
+        }
+    }
+
+    public void checkNumberOnly(String inputStr){
+        for(int index=0; index<inputStr.length(); index++){
+            char inputDigit=inputStr.charAt(index);
+            if (Character.isDigit(inputDigit) == false){
+                throw new IllegalArgumentException("숫자를 입력하세요.");
+            }
         }
     }
 }
