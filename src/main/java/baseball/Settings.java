e26bea5af7e9d0475f23dfa02e7665055cd33ded
YE-EUN <musiclove529@naver.com>
2022-11-08 21:29:32 +0900

    refactor(Settings) : START_STATUS와 TERMINATE_STATUS 수정
    - ProblemSolver의 chooseToRestart 함수의 변경으로 START_STATUS와 TERMINATE_STATUS String 형으로 변경

--- src/main/java/baseball/Settings.java
+++ src/main/java/baseball/Settings.java
@@ -2,8 +2,8 @@ package baseball;
 
 public class Settings {
     public static final int SIZE_OF_NUMBERS = 3;
-    public static final int START_STATUS = 1;
-    public static final int TERMINATE_STATUS = 2;
+    public static final String START_STATUS = "1";
+    public static final String TERMINATE_STATUS = "2";
     public static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     public static final String ANSWER_REQUEST_MESSAGE = "숫자를 입력해주세요 : ";
     public static final String SUCCESS_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
