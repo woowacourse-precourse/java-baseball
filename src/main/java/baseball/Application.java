92f6cef9e7922aa6c7b7c69fad2c1bced50991b1
ckyeon <qsky8520@gmail.com>
2022-11-08 19:17:26 +0900

    feat(Application): add game play

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,15 @@
 package baseball;
 
+import baseball.player.Computer;
+import baseball.player.User;
+
 public class Application {
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        User user = new User();
+        Computer computer = new Computer();
+
+        NumberBaseBallGame numberBaseBallGame = new NumberBaseBallGame(user, computer);
+        numberBaseBallGame.play();
     }
 }
