fa140a588349b52ad4df0bdfcf6b9d019c2ab416
moonn6pence <shin421179@gmail.com>
2022-11-08 13:58:17 +0900

    feat : Game 클래스
    - start() : 난수 생성, 게임 진행, 공 입력 및 validate, judge
    - isGameFinishied() : 스트라이크 카운트해서 게임이 종료되었는지 확인
    - replay() : 리플레이 여부 validate 및 결과를 main으로 반환

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,4 +1,59 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+import static baseball.Message.INPUT_SIZE;
+import static baseball.Message.REPLAY_INPUT;
+
 public class Game {
+
+    private final SecretNumber secretNumber;
+    private final ReplayNumber replayNumber;
+    private final UserNumber userNumber;
+
+    private int ballCount;
+    private int strikeCount;
+
+    private List<Integer> inputDigits;
+    private List<Integer> secretDigits;
+
+    public Game() {
+        secretNumber = new SecretNumber();
+        replayNumber = new ReplayNumber();
+        userNumber = new UserNumber();
+
+        ballCount = 0;
+        strikeCount = 0;
+    }
+
+    public void start() {
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        replayNumber.generateSecretDigits();
+        secretDigits = replayNumber.getSecretDigits();
+
+        while (isGameFinished()) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
+            inputDigits = userNumber.validate(input);
+            userNumber.judge(inputDigits,secretDigits);
+        }
+    }
+
+    private boolean isGameFinished(){
+        if (strikeCount == INPUT_SIZE) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean replay() {
+
+        String input = Console.readLine();
+
+        return replayNumber.validate(input);
+    }
 }
