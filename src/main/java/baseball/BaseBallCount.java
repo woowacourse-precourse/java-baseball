0328153e413c2922153f97774f05078c71e0c506
masiljangajji <hjk172262@gmail.com>
2022-11-08 22:26:21 +0900

    feat: 볼과 스트라이크 개수 확인

--- src/main/java/baseball/BaseBallCount.java
+++ src/main/java/baseball/BaseBallCount.java
@@ -7,19 +7,10 @@ public class BaseBallCount {
     int numberOfBall;
     int numberOfStrik;
 
-    public  void countStrikNumber(List<Integer> computerBaseBallNumber, String userBaseBallNumber) {
-        int count = 0;
-        for (int i = 0; i < computerBaseBallNumber.size(); i++) {
-            if (computerBaseBallNumber.get(i).equals(userBaseBallNumber.charAt(i) - '0') ) {
-                count++;
-            }
-        }
-        numberOfStrik= count;
-    }
 
     public static int countBall(int eachComputerBaseBallNumber, String userBaseBallNumber, int Index) {
         for (int i = 0; i < userBaseBallNumber.length(); i++) {
-            if (eachComputerBaseBallNumber==userBaseBallNumber.charAt(i) - '0' && i != Index) {
+            if (eachComputerBaseBallNumber == userBaseBallNumber.charAt(i) - '0' && i != Index) {
                 return 1;
             }
         }
@@ -31,7 +22,7 @@ public class BaseBallCount {
         for (int i = 0; i < computerBaseBallNumber.size(); i++) {
             countBall += countBall(computerBaseBallNumber.get(i), userBaseBallNumber, i);
         }
-        numberOfBall=countBall;
+        numberOfBall = countBall;
     }
 
 
