181a69ff02d0692dbc02f7668404d4f5dfaf5db0
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-08 18:23:43 +0900

    feat(feature): 볼 여부 판단 기능 구현
    ResultEvaluator의 isBallOrNot() 메서드를 구현
    - 사용자 번호의 특정 색인(index)에 위치한 숫자가, 컴퓨터 번호의 특정 색인(index)을 제외한 나머지 색인 어딘가에 존재할 경우, 볼이라는 의미로 1을 반환함.
    - 그렇지 않을 경우, 0을 반환함.

--- src/main/java/baseball/utility/ResultEvaluator.java
+++ src/main/java/baseball/utility/ResultEvaluator.java
@@ -12,4 +12,20 @@ public class ResultEvaluator {
         }
         return 0;
     }
+
+    private static int isBallOrNot(String computerNumber, String playerNumber, int index) {
+        int numberLength = playerNumber.length();
+        int iteratingIndex = (index + 1) % numberLength;
+        int count = 0;
+
+        while (count < numberLength - 1 && (playerNumber.charAt(index) != computerNumber.charAt(iteratingIndex))) {
+            iteratingIndex = (iteratingIndex + 1) % numberLength;
+            count++;
+        }
+
+        if (count == numberLength - 1) {
+            return 0;
+        }
+        return 1;
+    }
 }
