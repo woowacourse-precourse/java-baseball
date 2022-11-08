3cc17753be7b71a58192bbeabb28f9e4807a321d
James <rlagurwo6209@naver.com>
2022-11-08 19:12:06 +0900

    [feat]:2번기능(랜덤 번호 생성) 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,7 +12,19 @@ public class Application {
     static void startNumberBaseballGame(){
 
     }
-
+    static List<Integer> getAnswerRandomNumber(){
+        // 랜덤 3자리 숫자 생성
+        while(answerRandomNumber.size()<3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            if(!answerRandomNumber.contains(randomNumber)){
+                answerRandomNumber.add(randomNumber);
+            }
+            else{
+                System.out.println("랜덤숫자 중복됨. 다시 부여.");
+            }
+        }
+        return answerRandomNumber;
+    }
     static Integer stringToInteger(String input){
         try{
             Integer answer = Integer.parseInt(input);
