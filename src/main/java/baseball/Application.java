e3c16f64be7c9049ee13d1b25575faf13cf57b4c
naaaaag <115922748+naaaaag@users.noreply.github.com>
2022-11-08 14:35:18 +0900

    <feat>README : 1과 2를 제외한 다른 값을 입력할 때 재입력 받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,5 @@
 package baseball;
 
-import java.sql.SQLOutput;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
@@ -12,12 +11,10 @@ public class Application {
 
 
             String numberComputer = Input.computer();
-            System.out.println(numberComputer);
             boolean notThreeStrikes = true;
 
             do {
                 String numberPlayer = Input.player();
-                System.out.println(numberPlayer);
                 Input.checkError(numberPlayer);
 
 
@@ -29,16 +26,21 @@ public class Application {
                 notThreeStrikes = !round.has3Strikes();
             } while (notThreeStrikes);
 
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            reply = Console.readLine();
+            do {
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                reply = Console.readLine();
 
-            if (reply.equals("2")) {
-                restart = false;
-            }
-            System.out.println(reply);
-            System.out.println(restart);
-
+                if (reply.equals("1")) {
+                    restart = true;
+                    continue;
+                }
+                if (reply.equals("2")) {
+                    restart = false;
+                    continue;
+                }
+                System.out.println("입력값이 유효하지 않습니다. 다시 입력해주십시오");
+            } while (!reply.equals("1") && !reply.equals("2"));
 
         } while (restart);
     }
-    }
+}
