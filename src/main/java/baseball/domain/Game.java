4cc6ba0158bcc4d449285c7be86548d66e9c42db
ys200209 <lys200209@naver.com>
2022-11-08 21:14:00 +0900

    feat(Game.java): 게임을 재시작할건지 물어보기
    추가한 메서드
    - isStop(): 게임을 재시작할건지 메시지를 출력하고 사용자에게 입력을 요청함.
    사용자가 입력한 답에 대한 검증을 요청하고 최종적으로 사용자의 대답을 반환함.

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -35,4 +35,13 @@ public class Game {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
+    public boolean isStop() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        user.inputNumber();
+
+        user.validateRestart();
+
+        return user.isStop();
+    }
+
 }
