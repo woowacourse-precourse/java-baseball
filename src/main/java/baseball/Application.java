70beb6109d81077a618548b15a5b9d6a60fa7154
Dabeen Jeong <hello70825@gmail.com>
2022-11-08 20:43:06 +0900

    feat: 유저가 모든 입력값을 입력해볼 수 있도록 한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,15 @@
 package baseball;
 
+import baseball.resource.GameMessage;
+
 public class Application {
     public static void main(String[] args) {
 
         BaseballGame baseballGame = new BaseballGame();
 
+        System.out.println(GameMessage.START_GAME_MESSAGE);
+        do {
+            baseballGame.playGame();
+        } while(baseballGame.askNewGame());
     }
 }
