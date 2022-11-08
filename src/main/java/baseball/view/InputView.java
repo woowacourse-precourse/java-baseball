e0499e23d0c65dc8900c4ec1b24233611c19847a
EUNSH1 <tkdgus968@naver.com>
2022-11-08 11:33:58 +0900

    feat : 게임 재시작 입력 뷰 생성

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -8,5 +8,10 @@ public class InputView {
         System.out.println("숫자를 입력해주세요 : ");
         return Console.readLine();
     }
-    
+
+    public static String setRetryNumber(){
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        return Console.readLine();
+    }
 }
