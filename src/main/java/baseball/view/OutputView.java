b4ccd00e8516e286b98ebb74be572069499b98c7
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-11-08 10:32:54 +0900

    refactor(OutputView): OutputView 상수 필드 변수명 + MESSAGE로 의미 명확하게 수정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,32 +2,32 @@ package baseball.view;
 
 public class OutputView {
     private static final String INIT_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final String NOTTING = "낫싱";
-    private static final String STRIKE = "스트라이크";
-    private static final String BALL = "볼";
-    private static final String WIN_GAME = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String NOTTING_MESSAGE = "낫싱";
+    private static final String STRIKE_MESSAGE = "스트라이크";
+    private static final String BALL_MESSAGE = "볼";
+    private static final String WIN_GAME_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     public static void printInitGameMessage() {
         System.out.println(INIT_MESSAGE);
     }
 
     public void printNothing() {
-        System.out.println(NOTTING);
+        System.out.println(NOTTING_MESSAGE);
     }
 
     public void printResult(int strike, int ball) {
         String result = "";
         if (ball > 0) {
-            result += ball + BALL + " ";
+            result += ball + BALL_MESSAGE + " ";
         }
         if (strike > 0) {
-            result += strike + STRIKE;
+            result += strike + STRIKE_MESSAGE;
         }
         System.out.println(result);
     }
 
     public void printWinGame(int strike) {
-        System.out.println(strike + STRIKE);
-        System.out.println(WIN_GAME);
+        System.out.println(strike + STRIKE_MESSAGE);
+        System.out.println(WIN_GAME_MESSAGE);
     }
 }
