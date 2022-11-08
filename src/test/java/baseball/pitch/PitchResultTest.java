7b90990d68db1bea50814160212a5b2ccaedf2ba
inhwanK <79566221+inhwanK@users.noreply.github.com>
2022-11-08 23:59:09 +0900

    feat(PitchResultTest): 1볼 테스트 추가 및 낫싱 테스트 메소드 이름 변경

--- src/test/java/baseball/pitch/PitchResultTest.java
+++ src/test/java/baseball/pitch/PitchResultTest.java
@@ -5,8 +5,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PitchResultTest {
     @Test
-    void 낫싱() {
+    void ball과_strike가_둘다_0일_때_낫싱() {
         PitchResult pitchResult = new PitchResult(0, 0);
         assertThat(pitchResult.getPitchResultString()).isEqualTo("낫싱");
+    }
+
+    @Test
+    void ball이_1이고_stike가_0일_때_1볼() {
+        PitchResult pitchResult = new PitchResult(1, 0);
+        assertThat(pitchResult.getPitchResultString()).isEqualTo("1볼");
     }
 }
