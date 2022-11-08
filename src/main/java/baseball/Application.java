b8783f4e81e2d6c0e24afaa5865c6fd6d554a073
Young37 <20171470@g.dongseo.ac.kr>
2022-11-08 17:38:09 +0900

    feat(result): print out the results of the game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -124,4 +124,26 @@ public class Application {
         
         return List.of(ball, strike);
     }
+    
+    static void printGameResult(int ball, int strike) {
+        
+        if (ball != 0) {
+            print(ball+"볼 ");
+        }
+        
+        if (strike != 0) {
+            print(strike+"스트라이크");
+        }
+        
+        if (strike == 0 && ball == 0) {
+            print("낫싱");
+        }
+        
+        if (strike == 3) {
+            print("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
+            print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+        
+        print("\n");
+    }
 }
