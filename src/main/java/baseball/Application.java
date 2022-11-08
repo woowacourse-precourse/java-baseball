2b55f105d74f92f3094b34eb3a455a63477672cf
Woogie95 <choisw950928@gmail.com>
2022-11-08 19:36:19 +0900

    feat: Application 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,15 +3,8 @@ package baseball;
 public class Application {
 
     public static void main(String[] args) {
-
-        try {
-            BaseballManagement baseballManagement = new BaseballManagement();
-            baseballManagement.proceedBaseball();
-        } catch (IllegalArgumentException exceptionMessage) {
-            System.out.println(exceptionMessage.getMessage());
-        } catch (Exception exceptionMessage) {
-            System.out.println("예기치 못한 에러가 발생하였습니다.");
-        }
+        BaseballManagement baseballManagement = new BaseballManagement();
+        baseballManagement.runBaseballGame();
     }
 
 }
