a520e80cfadeb966208b68c43b20846b57a2356c
lacram <chemin9898@gmail.com>
2022-11-08 19:12:13 +0900

    feat: 비교한 결과에 따라 볼, 스트라이크 개수를 출력하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -96,4 +96,24 @@ public class Application {
         }
         return result;
     }
+
+    // 비교한 결과에 따라 볼, 스트라이크 개수를 출력하는 기능
+    public static void printResult(List<Integer> result) {
+        int ball = result.get(0);
+        int strike = result.get(1);
+
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        } else if (ball == 0){
+            System.out.println(strike + "스트라이크");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        }
+        if (strike == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+    }
 }
