a848dcfd713caa3e0937228a2856dfd9e2fc9e34
chj <bluedog129@gmail.com>
2022-11-08 21:29:52 +0900

    feat : 한번의 게인 단위당 시퀀스 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,19 +10,29 @@ import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
 
-/**
- * 기능 목록
- *  - executionGame : 하나의 게임 단위
- *  - continueGame :  게임을 새로 시작할지 종료할지 결정하는 함수
- *  - decision : 서로 다른 3개의 랜덤 수 저장
- *  - validationAndConvert : 입력에 대한 검증과 List<Integer>로 반환
- *  - calculationBall : 볼 계산
- *  - calculationStrike : 스트라이크 계산
- *  - currentSituation : 입력에 대한 결과를 문자열 형태로 반환
- *  - isCorrect : 정답을 맞추었는지
- */
 public class Application {
     public static void main(String[] args) {
+        int run = 1;
         System.out.println("숫자 야구 게임을 시작합니다.");
+        while(run != 0){
+            executionGame();
+        }
+    }
+
+    public static void executionGame(){
+        List<Integer> computerOutput = decision();
+
+        while(true) {
+            System.out.print("숫자를 입력해주세요 :");
+
+            List<Integer> user = validation(Console.readLine());
+
+            Integer ball =calculationBall(computerOutput, user);
+
+            Integer strike =calculationStrike(computerOutput, user);
+
+            String result =currentSituation(ball, strike);
+            System.out.println(result);
+        }
     }
 }
