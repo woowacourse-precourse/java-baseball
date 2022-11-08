871ccd055a3688ad835dd9f1f7e0968efc24791c
saehim <saeilee.vxv@gmail.com>
2022-11-08 22:36:41 +0900

    Feat: 사용자 입력 숫자 중 각 자리가 같을 경우 예외 처리

--- src/main/java/baseball/exception/BaseballException.java
+++ src/main/java/baseball/exception/BaseballException.java
@@ -2,7 +2,18 @@ package baseball.exception;
 
 import baseball.controller.Game;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class BaseballException {
+
+    public static void vaildNumArr(List<Integer> arrNums) {
+        Set<Integer> setNums = new HashSet<>(arrNums);
+        if (arrNums.size() != setNums.size()) {
+            throw new IllegalArgumentException("각각 다른 숫자를 입력해 주십시오.");
+        }
+    }
     public static void vaildNum(int num){
         if (num < 100 || num > 999) {
             throw new IllegalArgumentException("3개의 숫자를 입력해 주십시오.");
@@ -11,7 +22,6 @@ public class BaseballException {
 
     public static void vaildShutDownInput(int num){
         if (num==Game.getNewGameCode() || num==Game.getShutDownCode()) {
-
         } else {
             throw new IllegalArgumentException("잘못된 입력입니다.");
         }
