d2b3a0a25ca58689b491b0cf44b67809772ff54e
mgOh <issac_oh@naver.com>
2022-11-08 17:20:58 +0900

    fix: 입력값에 대한 출력값을 계산할 JudgePitchingResult 생성

--- src/main/java/baseball/JudgePitchingResult.java
+++ src/main/java/baseball/JudgePitchingResult.java
@@ -1,5 +1,27 @@
 package baseball;
 
-public class PitchingResult {
-    PitchingResult
+import java.util.ArrayList;
+import java.util.List;
+
+public class JudgePitchingResult {
+    int ball;
+    int strike;
+
+    public JudgePitchingResult(){
+        this.ball = 0;
+        this.strike = 0;
+    }
+
+    // 입력 정수값을 각 자리로 나누는 함수
+    private List<Integer> changeStringToIntegerList(String inputString) {
+        List<Integer> integerList = new ArrayList<>();
+
+        String[] digits = inputString.split("(?<=.)");
+
+        for (int i = 0; i < 3; i++) {
+            integerList.add(Integer.valueOf(digits[i]));
+        }
+        return integerList;
+    }
+
 }
