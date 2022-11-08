8063357b1f6534ba80b436ed6a039813e14ae3f2
yoon <beatus96@naver.com>
2022-11-08 12:51:38 +0900

    refactor(rule) : enum에서 일반 클래스로 수정
    수정이유 : 효율성을 보완하기 위해서
    상수값 쓸 때마다 getValue()나 toString() 메서드 불러와야 했기 때문에, 효율성이 떨어졌다.

--- src/main/java/baseball/GameRule.java
+++ src/main/java/baseball/GameRule.java
@@ -1,20 +1,10 @@
 package baseball;
 
-public enum GameRule {
-    DIGITS(3), RANDOM_MIN(1), RANDOM_MAX(9), COMMAND_RESTART(1),COMMAND_END(2);
+public class GameRule {
+    public static final int DIGITS = 3;
+    public static final int RANDOM_MIN =1;
+    public static final int RANDOM_MAX =9;
+    public static final String COMMAND_RESTART ="1";
+    public static final String COMMAND_END ="2";
 
-    private final int value;
-
-    GameRule(int value) {
-        this.value = value;
-    }
-
-    public int getValue(){
-        return this.value;
-    }
-
-    @Override
-    public String toString(){
-        return String.valueOf(this.value);
-    }
 }
