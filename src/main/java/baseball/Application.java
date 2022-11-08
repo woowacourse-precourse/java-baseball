4a5d1ba3860eb643aff6e5c90eb5df056824a576
aran <aran0317@naver.com>
2022-11-08 17:30:50 +0900

    Feat: 야구게임 점수 판단 출력 구현
    print 함수로 값 출력 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -26,7 +26,7 @@ public class Application {
 
     static void verifyPlayer(List<Integer> player){
         if(player.size() != 3 || (player.contains(0))) throw new IllegalArgumentException();
-        if(player.get(0) == player.get(1) || player.get(0) == player.get(1) || player.get(1) == player.get(2)) throw new IllegalArgumentException();
+        if(player.get(0) == player.get(1) || player.get(0) == player.get(2) || player.get(1) == player.get(2)) throw new IllegalArgumentException();
         return;
     }
 
@@ -61,9 +61,26 @@ public class Application {
         return strikeScore;
     }
 
+    static void print(int ball, int strike){
+        if(strike == 3){
+            System.out.println(strike+"스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+        else if(ball > 0 && strike > 0){
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }else if(ball > 0){
+            System.out.println(ball+"볼");
+        }else if(strike>0){
+            System.out.println(strike+"스트라이크");
+        }else{
+            System.out.println("낫싱");
+        }
+    }
+
     static boolean compare(List<Integer> computer, List<Integer> player){
         int ball = getBallScore(computer,player);
         int strike = getStrikeScore(computer,player);
+        print(ball,strike);
         if(strike == 3) return true;
         else return false;
     }
