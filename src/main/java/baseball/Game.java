5f7b8cb24cb7ebe3909774b9d5a313dbc096f848
young2866 <62212610+young2866@users.noreply.github.com>
2022-11-08 16:15:05 +0900

    refactor(Game): 하드 코딩 제거

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -9,17 +9,19 @@ public class Game {
 
 	private final static String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요";
 	private final static String GAME_END_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+	private final static String RESTART = "1";
 	private Opponent opponent = new Opponent();
 	private User user = new User();
 	private Compute compute = new Compute();
 
 	public void start() {
 		opponent.generateAnswerNumber();
+
 		do {
 			user.inputNumber();
 		} while (compute.computeResult(user, opponent));
-
 		System.out.println(GAME_END_MESSAGE);
+
 		if (restart()) {
 			start();
 		}
@@ -29,7 +31,7 @@ public class Game {
 		System.out.println(RESTART_MESSAGE);
 		String restart = Valid.validRestartNumber(readLine());
 
-		if (restart.equals("1")) {
+		if (restart.equals(RESTART)) {
 			return true;
 		}
 		return false;
