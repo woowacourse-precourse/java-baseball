d8d23220aae8d98c42934801e7e87c3cc3e95bf5
dingding-21 <phj8221@naver.com>
2022-11-08 15:54:26 +0900

    feat: 입력값이 서로 다른 3자리 수인지 검증하는 기능 추가

---
+++ src/main/java/baseball/InputValidation.java
@@ -0,0 +1,32 @@
+package baseball;
+
+import java.util.Collections;
+import java.util.List;
+
+public class InputValidation {
+    private static final Integer START_NUMBER = 1;
+    private static final Integer END_NUMBER = 9;
+
+    private static final String INPUT_LENGTH_ERROR = "입력값은 %s자리 수가 아닙니다.";
+    private static final String INPUT_RANGE_ERROR = "입력값은 %s ~ %s 범위의 수가 아닙니다.";
+    private static final String INPUT_DUPLICATE_ERROR = "입력값은 서로 다른 수가 아닙니다.";
+
+
+    public static void checkNumberLength(String number, Integer maxLength) {
+        if (number.length() != maxLength) {
+            throw new IllegalArgumentException(String.format(INPUT_LENGTH_ERROR, maxLength));
+        }
+    }
+
+    public static void checkNumberRange(Integer number) {
+        if (number < START_NUMBER || number > END_NUMBER) {
+            throw new IllegalArgumentException(String.format(INPUT_RANGE_ERROR, START_NUMBER, END_NUMBER));
+        }
+    }
+
+    public static void checkDuplicateNumber(List<Integer> numbers, Integer targetNumber) {
+        if (Collections.frequency(numbers, targetNumber) != 1) {
+            throw new IllegalArgumentException(INPUT_DUPLICATE_ERROR);
+        }
+    }
+}
