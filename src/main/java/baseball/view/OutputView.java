a32339f1632f4847a9f374661c2acc1784cedc79
gomljo <gomljo0307@naver.com>
2022-11-08 19:40:16 +0900

    feat(OutputView.java): 사용자의 답안 메시지를 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,6 +3,9 @@ package baseball.view;
 public class OutputView {
     private static final String SUCCESS_MESSAGE="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
+    public void showMessage(String message){
+        System.out.println(message);
+    }
     public void showSuccessMessage(){
         System.out.println(SUCCESS_MESSAGE);
     }
