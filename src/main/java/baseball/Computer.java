278e1c1d6a67721b5702b62e1fcc8f09cd91d244
goldcollar95 <yjs7298@naver.com>
2022-11-08 16:07:55 +0900

    refactor(Computer): 메소드에 static제거 후에 import static

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
-    public static List<Integer> computerNum() {
+    public List<Integer> computerNum() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
