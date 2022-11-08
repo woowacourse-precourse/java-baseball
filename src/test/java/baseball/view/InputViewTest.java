dd954b941ab16c0d046d871ede269ffbc18de567
Miseong Kim <miseongk@naver.com>
2022-11-08 19:39:55 +0900

    test(ValidationTest): input and convert string to integer

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -13,4 +13,18 @@ class InputViewTest {
         System.setIn(in);
     }
 
+    @Test
+    void 숫자입력_테스트() {
+        inputFromUser("123");
+        int inputNumber = InputView.getNumber();
+        assertThat(inputNumber).isEqualTo(123);
+    }
+
+    @Test
+    void 게임재시작여부입력_테스트() {
+        inputFromUser("1");
+        int inputNumber = InputView.isReplay();
+        assertThat(inputNumber).isEqualTo(1);
+    }
+
 }
