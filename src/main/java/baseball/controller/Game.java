23ccc999bc371e33062a9c2afac7b3ae06b6d46f
si hwan <pum005@naver.com>
2022-11-08 21:03:29 +0900

    feat : Game 클래스 생성, init, play 메소드 생성

---
+++ src/main/java/baseball/controller/Game.java
@@ -0,0 +1,39 @@
+package baseball.controller;
+
+import baseball.model.strikeandball.StrikeAndBall;
+import baseball.service.GameService;
+import baseball.view.Input;
+import baseball.view.Output;
+import baseball.model.numbergenerator.NumberGenerator;
+import baseball.model.numbers.Numbers;
+
+public class Game {
+    private final NumberGenerator numberGenerator;
+    private final Input input;
+    private final Output output;
+    private final GameService gameService;
+
+    public Game(NumberGenerator numberGenerator, Input input, Output output, GameService gameService) {
+        this.numberGenerator = numberGenerator;
+        this.input = input;
+        this.output = output;
+        this.gameService = gameService;
+    }
+
+    public void init() {
+        output.printInit();
+    }
+
+    public void play() {
+        Numbers answerNumbers = numberGenerator.generateRandomNumber(3);
+
+        StrikeAndBall result;
+        do {
+            Numbers inputNumbers = input.getNumber();
+
+            result = gameService.compareAnswerNumberAndInputNumber(answerNumbers, inputNumbers);
+            output.printStrikeandBall(result);
+
+        } while (!result.isThreeStrike());
+    }
+}
