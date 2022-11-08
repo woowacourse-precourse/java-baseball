f1c87218901abe00769321f2d736e591284ea5ce
code-tree <dltjdqls1200@naver.com>
2022-11-08 16:06:20 +0900

    refactor: Set을 사용해서 서로 다른 랜덤 숫자 뽑기

--- src/main/java/baseball/service/NumberGenerator.java
+++ src/main/java/baseball/service/NumberGenerator.java
@@ -3,32 +3,19 @@ package baseball.service;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class NumberGenerator {
-    private static final int START_NUM = 1;
-    private static final int END_NUM = 9;
-    private static final int PICK_MAX_COUNT = 3;
+    private static final int PICK_COUNT = 3;
 
     public List<Integer> createRandomNumbers() {
-        List<Integer> randomNumbers = new ArrayList<>(PICK_MAX_COUNT);
-        int pickCount = 0;
-
-        while (pickCount < PICK_MAX_COUNT) {
-            randomNumbers.add(pickUniqueNumber(randomNumbers));
-            pickCount++;
+        Set<Integer> randomNumbers = new HashSet<>();
+        while (randomNumbers.size() < PICK_COUNT) {
+            randomNumbers.add(Randoms.pickNumberInRange(1, 9));
         }
 
-        return randomNumbers;
-    }
-
-    private int pickUniqueNumber(List<Integer> randomNumbers) {
-        int pickNumber = Randoms.pickNumberInRange(START_NUM, END_NUM);
-
-        while (randomNumbers.contains(pickNumber)) {
-            pickNumber = Randoms.pickNumberInRange(START_NUM, END_NUM);
-        }
-
-        return pickNumber;
+        return new ArrayList<>(randomNumbers);
     }
 }
