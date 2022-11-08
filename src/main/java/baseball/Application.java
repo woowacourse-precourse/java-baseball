28ba67614327da9bed630125158984e3740f7b7e
Konahyeon <koo@inha.edu>
2022-11-08 21:32:24 +0900

    style: restart 메서드 라인 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -30,11 +30,10 @@ public class Application {
 
     public static void restart(Computer computer) {
         System.out.println("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
-        String input = " ";
-
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        input = Console.readLine();
+
+        String input = Console.readLine();
+
         if (input.equals("1")) {
             RESTART = true;
         }
