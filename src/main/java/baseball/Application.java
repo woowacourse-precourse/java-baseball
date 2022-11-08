c8abb2d09563d92a0288cf330a4975d40bef9291
HikiJun97 <sgn04088@gmail.com>
2022-11-08 20:21:37 +0900

    feat: implement feature of user input
    Add user input method. It reads the input from user as String and parses it as Integer with single space.
    Then it saves the Integers in a List and returns it.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,45 @@
 package baseball;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+import java.lang.IllegalArgumentException;
+
 public class Application {
+
+    static List<Integer> inputAnswer(){
+        List<Integer> inputList;
+        Integer[] inputArray = new Integer[3];
+        String inputString = Console.readLine();
+        String[] parsedString = inputString.split(" ");
+
+        //for debugging
+        System.out.println(inputString);
+        for (String str : parsedString) {
+            System.out.println(str);
+        }
+        System.out.println(parsedString.length);
+        //
+
+        for (int i = 0; i < 3; i++) {
+            inputArray[i] = Integer.parseInt(parsedString[i]);
+        }
+        inputList = Arrays.asList(inputArray);
+
+        return inputList;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+        List<Integer> isStrike = inputAnswer();
+
+        //for debugging
+        for (Integer i : isStrike) {
+            System.out.println("(" + i + ")");
+        }
+        //
     }
 }
