9f5cb332e4842535de44499428c8ec6db2e80127
Jeeseob <jeeseob5761@gmail.com>
2022-11-08 15:49:51 +0900

    feat: 예외처리 게임 중 입력받는 데이터

--- src/main/java/baseball/BaseBallGameUtil.java
+++ src/main/java/baseball/BaseBallGameUtil.java
@@ -24,12 +24,22 @@ public class BaseBallGameUtil {
 
     // TODO: 데이터 입력받아, 랜덤 숫자와 같은 형태로 형태변환
     public static List<Integer> inputUserAnswer() {
-        return toIntegerList(Console.readLine());
+        String input = Console.readLine();
+        if (input.length() != Constant.LENGTH_OF_NUMBER) {
+            throw new IllegalArgumentException();   // 입력받은 값의 길이가 요구사항과 다른 경우
+        }
+        return toIntegerList(input);
     }
 
     private static List<Integer> toIntegerList(String input) {
         List<Integer> userAnswer = new ArrayList<>();
         for (String digit : input.split("")) {
+            if (!Character.isDigit(digit.charAt(0))) {
+                throw new IllegalArgumentException();   // 입력받은 값이 숫자가 아닌 경우
+            }
+            if(userAnswer.contains(Integer.parseInt(digit))) {
+                throw new IllegalArgumentException();   // 입력받은 값에 중복되는 값이 존재하는 경우
+            }
             userAnswer.add(Integer.parseInt(digit));
         }
         return userAnswer;
