7a1bd0dd1b6ecf15354adf7f1aff276216dc3e11
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-08 18:56:53 +0900

    feat(Hint): 볼(같은 수, 다른 자리) 개수 세기

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -5,18 +5,31 @@ import java.util.stream.IntStream;
 
 public class Hint {
     private final int strike;
+    private final int ball;
 
     public Hint(List<Integer> userNums, List<Integer> computerNums, int count) {
         strike = countStrike(userNums, computerNums, count);
+        ball = countBall(userNums, computerNums, count);
     }
 
     public int getStrike() {
         return strike;
     }
 
+    public int getBall() {
+        return ball;
+    }
+
     private int countStrike(List<Integer> userNums, List<Integer> computerNums, int count) {
         return (int) IntStream.range(0, count)
                 .filter(i -> userNums.get(i) == computerNums.get(i))
+                .count();
+    }
+
+    private int countBall(List<Integer> userNums, List<Integer> computerNums, int count) {
+        return (int) IntStream.range(0, count)
+                .filter(i -> userNums.get(i) != computerNums.get(i)
+                        && userNums.contains(computerNums.get(i)))
                 .count();
     }
 }
