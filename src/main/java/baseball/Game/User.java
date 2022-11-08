b83c3b185847bd27233e66ec7eb3a5b69b168993
gilyeon00 <12venus08@naver.com>
2022-11-08 23:54:43 +0900

    refactor: edit message

--- src/main/java/baseball/Game/User.java
+++ src/main/java/baseball/Game/User.java
@@ -19,11 +19,11 @@ public class User {
     }
 
     public void correctAnsMsg() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
     public void retryMsg() {
-        System.out.println("게임을 계속 시작하려면 1, 종료하려면 2를 입력하세요.");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 
 }
