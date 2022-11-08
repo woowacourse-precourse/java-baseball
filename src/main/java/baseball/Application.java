25cf6cf4dc2c716f313f8e9cdea768160e28dc00
Ellie <doris0707@naver.com>
2022-11-08 11:24:15 +0900

    feat: write get_random_number method
    write new method
    - return type: List<Integer>,
    - return: random three number,

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,23 @@
 package baseball;
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
 public class Application {
+
+
+    public static List<Integer> get_random_number(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
