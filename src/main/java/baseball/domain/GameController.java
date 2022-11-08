57e064056df9ae55a32c7635b7195ade24393d10
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-08 17:30:06 +0900

    chore: refactoring by deleting singleton

--- src/main/java/baseball/domain/GameController.java
+++ src/main/java/baseball/domain/GameController.java
@@ -5,51 +5,59 @@ import baseball.util.myList;
 import java.util.List;
 
 public class GameController {
-    private static GameController gameController = new GameController();
+    private static GameController instance = null;
     private static Computer computer = null;
     private static User user = null;
     private static boolean keepPlaying;
-    GameController(){
-        this.computer = Computer.getInstance();
-        this.user = User.getInstance();
+
+    GameController() {
+        this.computer = new Computer();
+        this.user = new User();
         this.keepPlaying = true;
     }
-    public static GameController getInstance(){
-        return gameController;
+
+    public static GameController getInstance() {
+        if (instance == null) instance = new GameController();
+        return instance;
     }
-    public void run(){
+
+    public void run() {
         // 종료 플래그가 나올 때까지 계속 실행
-        while(keepPlaying){
+        while (keepPlaying) {
             playGame();
             requestNewGame();
         }
     }
+
     void playGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+        computer.clearRandomNumbers();
         if (!computer.isReady())
             computer.initRandomNumbers();
         computer.getCounts();
-        List<Integer> answer = user.inputUserAnswer();
+        List<Integer> answer = null;
         while (!isClear(answer)) answer = user.inputUserAnswer();
-        computer.clearRandomNumbers();
+
     }
 
-    boolean isClear(List<Integer> input){
+    boolean isClear(List<Integer> input) {
+        if (input == null) return false;
 
         int strike = myList.countSameLocationValues(input, computer.getBallCounts());
         int ball = myList.countDiffLocationValues(input, computer.getBallCounts());
 
-        if ( ball == 0 && strike == 0 ) System.out.println("낫싱");
-        if ( ball != 0 ) System.out.print(ball + "볼 ");
-        if ( strike != 0 ) System.out.println(strike + "스트라이크");
-        if ( strike == 3) {
+        if (ball == 0 && strike == 0) System.out.print("낫싱");
+        if (ball != 0) System.out.print(ball + "볼 ");
+        if (strike != 0) System.out.print(strike + "스트라이크");
+        System.out.println();
+        if (strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             return true;
         }
         return false;
     }
 
-    void requestNewGame(){
-        if ( user.inputKeepPlaying() == 2 ) keepPlaying = false;
+    void requestNewGame() {
+        if (user.inputKeepPlaying() == 2) keepPlaying = false;
     }
 }
