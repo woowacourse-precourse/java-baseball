23772218ac4f5e0c8eab63db6198f362c3ac44e3
iphuck22 <eldeshue@naver.com>
2022-11-08 15:46:43 +0900

    feat($NumberbBaseball.java): add constructor at NumberBaseball class
    add constructor at NumberBaseball class
    - initialize the field 'CorrectAnswer' with 3 random integers
    - the choice of the integers are made by 'Randoms.pickNumberInRange'
    - those three integers are different to each other

--- src/main/java/baseball/NumberBaseball.java
+++ src/main/java/baseball/NumberBaseball.java
@@ -1,6 +1,8 @@
 package baseball;
 
 import java.util.List;
+import java.util.ArrayList;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class NumberBaseball {
     private List<Integer> CorrectAnswer;
@@ -14,5 +16,21 @@ public class NumberBaseball {
         CorrectAnswer.set(1, second);
         CorrectAnswer.set(2, third);
     }
+
+    public NumberBaseball(){ // generate answer by Randoms library
+        CorrectAnswer = new ArrayList<Integer>();
+        CorrectAnswer.add(Randoms.pickNumberInRange(0, 9));
+        for(int i = 0; i < 2; i++){
+            int temp;
+            do{
+                temp = Randoms.pickNumberInRange(0, 9);
+            }
+            while(CorrectAnswer.contains(temp));
+            CorrectAnswer.add(temp);
+        }
+        //CorrectAnswer = Randoms.pickUniqueNumbersInRange(0,9,3);
+    }
+
+
 
 }
