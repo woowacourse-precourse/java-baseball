d4a9be3a41f59b51bd38e2242bf8b9957cdb5322
glaehfdl1654 <glaehfdl1654@naver.com>
2022-11-08 15:07:16 +0900

    feat(Application class): 게임의 시작이나 종료를 결정하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,7 +4,14 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String usersInput = "";
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        do {
+            NumberBaseball.newGame();
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            usersInput = Console.readLine();
+        } while (canRestart(usersInput));
     }
     private static boolean canRestart(String usersInput) {
         if (usersInput.equals("1")) {
