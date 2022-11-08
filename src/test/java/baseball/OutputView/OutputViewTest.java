5dd4eb5438786826ed461ac0950bb693b2bc1217
gomljo <gomljo0307@naver.com>
2022-11-08 19:37:47 +0900

    test(OutputViewTest.java): 사용자의 답안 메시지를 출력하는 기능 검증

--- src/test/java/baseball/OutputView/OutputViewTest.java
+++ src/test/java/baseball/OutputView/OutputViewTest.java
@@ -9,8 +9,16 @@ import java.io.PrintStream;
 
 public class OutputViewTest {
     private static final String SUCCESS_MESSAGE="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String STATE_MESSAGE="1볼 1스트라이크";
     @Test
-    public void notice_Start_Message_Test(){
+    public void show_Success_Message_Test(){
+        OutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        System.out.print(SUCCESS_MESSAGE);
+        Assertions.assertThat(SUCCESS_MESSAGE).isEqualTo(out.toString());
+    }
+    @Test
+    public void Show_State_Message_Test(){
         OutputStream out = new ByteArrayOutputStream();
         System.setOut(new PrintStream(out));
         System.out.print(SUCCESS_MESSAGE);
