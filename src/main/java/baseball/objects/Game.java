0edd7261e421425a991dee29383835c00dd0555b
이경범 <tkdwk567@naver.com>
2022-11-08 19:20:38 +0900

    refactor : Game class refactor
    Validation 인스턴스 추가 및 하나의 메서드에 하나의 기능만 만들려고 노력했음!!!

--- src/main/java/baseball/objects/Game.java
+++ src/main/java/baseball/objects/Game.java
@@ -1,13 +1,20 @@
 package baseball.objects;
 
+import baseball.valid.Validation;
 import baseball.view.View;
 import baseball.view.service.ViewService;
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Game {
-    private final Computer computer = new Computer(new NumberGenerator());
+    private final Computer computer = new Computer();
     private final Referee referee = new Referee(new Rule());
     private final Player player = new Player();
+    private final Validation validation = new Validation();
+
 
     public boolean start() {
         computer.giveNumbersToReferee(referee);
@@ -26,16 +33,29 @@ public class Game {
     }
 
     private void savePlayerNumberToReferee(Player player, Referee referee) {
-        player.sayNumbers(pushNumber());
+        String pushNumber = pushBaseballNumber();
+        player.sayNumbers(stringNumberToList(pushNumber));
         toReferee(player, referee);
+    }
+
+    private String pushBaseballNumber() {
+        String baseBallPushNumber = pushNumber();
+        validation.validNumbers(baseBallPushNumber);
+        return baseBallPushNumber;
+    }
+
+    private boolean restart() {
+        String restartPushNumber = pushNumber();
+        validation.restartNumberValid(restartPushNumber);
+        return Integer.valueOf(restartPushNumber).equals(1);
+    }
+
+    private List<Integer> stringNumberToList(String number) {
+        return Arrays.stream(number.split("")).map(Integer::valueOf).collect(Collectors.toList());
     }
 
     private void toReferee(Player player, Referee referee) {
         player.giveNumbersToReferee(referee);
-    }
-
-    private boolean restart() {
-        return Integer.valueOf(pushNumber()).equals(1);
     }
 
     private String pushNumber() {
