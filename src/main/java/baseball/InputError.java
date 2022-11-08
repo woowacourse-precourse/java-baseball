195fad5c44e5c4a9f9f7e79956d635103854eb0c
ssssudan <kankgm0324@gmail.com>
2022-11-08 22:10:06 +0900

    <refactor>: InputError 메서드들은 return이 필요없어서 수정

--- src/main/java/baseball/InputError.java
+++ src/main/java/baseball/InputError.java
@@ -10,23 +10,14 @@ public class InputError {
 
     public static final String ERROR_MESSAGE = "잘못된 입력값입니다. 게임을 종료하겠습니다.";
 
-    public static String replayOrStopExceptionCheck(String answer) {
+    public static void replayOrStopExceptionCheck(String answer) {
         // 1, 2가 아닌 다른 값인지 검사
 
-        if (answer.equals("1"))
-            return answer;
-        if (answer.equals("2"))
-            return answer;
-        throw new IllegalArgumentException(ERROR_MESSAGE);
+        if (!answer.equals("1") && !answer.equals("2"))
+            throw new IllegalArgumentException(ERROR_MESSAGE);
     }
 
-    public static String userNumberInputExceptionCheck(String answer) {
-        /**
-         * 서로 다른 3자리
-         * 길이가 3이 아닌 경우 예외 발생
-         * 중복된 수가 들어 있을 때 예외 발생
-         * 각 자리가 숫자가 아닌경우 예외발생
-         */
+    public static void userNumberInputExceptionCheck(String answer) {
 
         if (answer.length() != 3)
             throw new IllegalArgumentException(ERROR_MESSAGE);
@@ -45,7 +36,5 @@ public class InputError {
             if (n < '1' || n > '9')
                 throw new IllegalArgumentException(ERROR_MESSAGE);
         }
-
-        return answer;
     }
 }
