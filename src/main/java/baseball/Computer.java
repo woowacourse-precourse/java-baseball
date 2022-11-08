8737c785bfbf435a7b90ade87110d25a9e9a1170
DDing77 <devseo2021@gmail.com>
2022-11-08 17:42:28 +0900

    feat($Computer) : Computer 생성자, 볼 갯수 체크

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,24 @@
+package baseball;
+
+public class Computer {
+
+    private final String answer;
+
+    public Computer(String answer) {
+        this.answer = answer;
+    }
+
+    public int countBall(String input) {
+        int cnt = 0;
+
+        for (int i = 0; i < input.length(); i++) {
+            char curNum = input.charAt(i);
+
+            if (answer.charAt(i) != curNum && answer.contains(String.valueOf(curNum))) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+}
