05e5cf9cdbe1cef081f3d21cf4c3110bb5e4b3d2
SHEOMM <esh0821@snu.ac.kr>
2022-11-08 13:19:09 +0900

    fix(RandomNumberListGenerator): fix generate function to use appropriate Randoms function
    문제 요구 스펙대로 랜덤 함수를 생성하기 위한 generate 함수 수정

--- src/main/java/baseball/service/RandomNumberListGenerator.java
+++ src/main/java/baseball/service/RandomNumberListGenerator.java
@@ -7,6 +7,13 @@ import camp.nextstep.edu.missionutils.Randoms;
 public class RandomNumberListGenerator {
 
     public static List<Integer> generate(){
-        return Randoms.pickUniqueNumbersInRange(1, 9, 3);
+        List<Integer> answer = new LinkedList<>();
+        while(answer.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if(!answer.contains(randomNumber)){
+                answer.add(randomNumber);
+            }
+        }
+        return answer;
     }
 }
