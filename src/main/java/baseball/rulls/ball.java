b423bd823e83dd47d5029fcbefe1d4aee41fdccf
bando99 <bando991225@gmail.com>
2022-11-08 22:09:17 +0900

    게임 규칙에 관련된 기능

---
+++ src/main/java/baseball/rulls/ball.java
@@ -0,0 +1,12 @@
+package baseball.rulls;
+
+import java.util.List;
+
+public class ball {
+    public static boolean isBall(List<Integer> computerList , int userNumber){
+        if(computerList.contains(userNumber)){
+            return true;
+        }
+        return false;
+    }
+}
