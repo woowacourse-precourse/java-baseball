1eded8ef734eda69ed205f372682399a7c008ae1
thousandcows <milcamboo@gmail.com>
2022-11-08 10:41:32 +0900

    feat(feature): create function to print the game start message
    Function definition:
    - execute when the application starts
    - print message that the game begins

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,19 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
     }
+
+    // 2. Print the game start message
+    public static String printTheGameStartMessage() {
+        return "숫자 야구 게임을 시작합니다.";
+    }
+
+
 }
