666b2d9cdf010f780ca79e18e4314e80f827d01f
parkjiyun <pjyun05200@gmail.com>
2022-11-08 17:20:44 +0900

    set functions simply

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,57 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class BaseballGame {
+    Scanner sc = new Scanner(System.in);
+    List<Integer> num_list = new ArrayList<>();
+    List<Integer> my_list = new ArrayList<>();
+    int ballCount;
+    int strikeCount;
+
+    public BaseballGame() {
+        makeNumList();
+        initalizeBall();
+    };
+
+    public void makeNumList() {
+
+    }
+
+    public void initalizeBall() {
+        int ballCount = 0;
+        int strikeCount = 0;
+    }
+
+    public void writeMyNumList() {
+
+    }
+
+    public void strikeBall() {
+
+    }
+
+     public void message() {
+
+     }
+
+}
+
 public class Application {
+
     public static void main(String[] args) {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        loop:
+        while(true) {
+            BaseballGame baseballGame = new BaseballGame();
+            while(true) {
+                baseballGame.writeMyNumList();
+                baseballGame.strikeBall();
+                baseballGame.message();
+            }
+        }
 
     }
 
