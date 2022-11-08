afbaac0d79f544792c2ae1b14539219674050941
hyee0715 <hyee0715@naver.com>
2022-11-08 15:34:05 +0900

    feat: '숫자 야구 게임을 시작합니다.' 출력

---
+++ src/main/java/baseball/Message.java
@@ -0,0 +1,9 @@
+package baseball;
+
+public class Message {
+    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+
+    public static void printInitialGameStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+}
