89da7090c2cb3a8564e5e5190b49a05d45a798ae
idoburnish <doyun7433@naver.com>
2022-11-08 16:30:10 +0900

    feat(NumberBaseballGame): 정답 생성 기능
    컴퓨터가 랜덤으로 생성한 3자리 수의 정답을 생성
    `camp.nextstep.edu.missionutils`에서 제공하는 `Randoms`를 사용

--- src/main/java/baseball/NumberBaseballGame.java
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -1,13 +1,22 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberBaseballGame {
     private static final String START_TEXT = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_TEXT = "숫자를 입력해주세요 : ";
     private static final String RESULT_TEXT = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     private static final String RESTART_TEXT = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
 
+    private final Answer answer = new Answer();
+    private List<Integer> computerNumber = new ArrayList<>();
+
     public void startGame() {
         System.out.println(START_TEXT);
-        
+
+        while (true) {
+            computerNumber = answer.generateAnswer();
+        }
     }
 }
