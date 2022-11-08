30dc04d02ec101f2ac717b945519574bb14491a0
wwwndus <simock22@gmail.com>
2022-11-08 22:39:24 +0900

    feat: 게임이 시작되면 세자리 수 값을 입력받고 배열로 만듦

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,8 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +18,27 @@ public class Application {
 
     }
 
+    private static void startGame(List<Integer> computer) {
+        while (true) {
+            System.out.print("숫자를 입력해주세요 : ");
 
+            int[] inputNumbers = getThreeInts();
+
+        }
+    }
+
+    private static int[] getThreeInts() {
+        String input = Console.readLine();
+
+        int inputNumber = Integer.parseInt(input);
+        int[] inputNumbers = new int[3];
+
+        inputNumbers[0] = inputNumber / 100;
+        inputNumbers[1] = (inputNumber / 10) % 10;
+        inputNumbers[2] = inputNumber % 10;
+        
+        return inputNumbers;
+    }
 
     public static List<Integer> createRandomNumbers() {
         List<Integer> randomNumbers = new ArrayList<>();
