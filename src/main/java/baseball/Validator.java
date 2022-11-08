3cd3b917800d7a2020de027d62bec170b0f6469d
Seoyoung <ryupark1006@gmail.com>
2022-11-08 15:54:47 +0900

    feat: 숫자 중복 확인 기능 구현

--- src/main/java/baseball/Validator.java
+++ src/main/java/baseball/Validator.java
@@ -1,10 +1,17 @@
 package baseball;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Validator {
 
     private static boolean checkLength(List<Integer> input) {
         return input.size() == 3;
+    }
+
+    private static boolean checkDuplication(List<Integer> input) {
+        Set<Integer> set = new HashSet<>(input);
+        return input.size() == set.size();
     }
 }
