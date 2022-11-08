a86b51ffa31d48535d1721da15b8c1cb5413c53c
yujaemin <woawoa0513@gmail.com>
2022-11-08 23:17:12 +0900

    refactor: 난수 맞히기 함수 생성
    난수 맞히는 과정 함수로 추출

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,35 +4,40 @@ public class Application {
     private static final int START_GAME = 1;
     private static final int EXIT = 2;
 
-    public static void main(String[] args) {
-        NonPlayerCharacter npc;
-        Player player;
-        RandomNumber randomNumber;
+    static NonPlayerCharacter npc;
+    static Player player;
+    static RandomNumber randomNumber;
 
+    public static void main(String[] args) {
         int menu = START_GAME;
 
+        player = new Player();
+        npc = new NonPlayerCharacter();
+
         while (menu == START_GAME) {
-            npc = new NonPlayerCharacter();
-            player = new Player();
             randomNumber = RandomNumber.getInstance();
-            System.out.println("randomNumber = " + randomNumber.fullNumber);
+
             npc.printGameStartNotice();
 
-            while (true) {
-                npc.printNumberInputNotice();
-                int playerInput = player.guessRandomNumber();
-                npc.countNumberOfBallAndStrike(playerInput);
-                npc.printComparisonResult();
-
-                if (playerInput == randomNumber.fullNumber) {
-                    break;
-                }
-            }
+            guessRandomNumber();
 
             npc.printGameEndNotice();
             menu = player.selectExitMenu();
 
             randomNumber.initialize();
+        }
+    }
+
+    public static void guessRandomNumber() {
+        while (true) {
+            npc.printNumberInputNotice();
+            int playerInput = player.guessRandomNumber();
+            npc.countNumberOfBallAndStrike(playerInput);
+            npc.printComparisonResult();
+
+            if (playerInput == randomNumber.fullNumber) {
+                return;
+            }
         }
     }
 }
