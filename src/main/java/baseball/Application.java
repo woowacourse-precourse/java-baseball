482c69c8cb2180b65137b43cf08f2bf375f6c779
함궈 <heoja0123@naver.com>
2022-11-08 20:34:29 +0900

    refactor : Application 숫자 야구 게임의 진행 로직을 분리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,59 +7,61 @@ import java.util.List;
 import java.util.Map;
 
 public class Application {
-    static final int FIXED_NUMBERS_LENGTH = 3;
-    static final int RESTART = 1;
-    static final int FINISH = 2;
-    
+
+    public static final String BALL = "볼";
+    public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
+
+    public static Integer answer;
+    public static String result;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
-        Integer answer = 1;
+        Computer computer = new Computer();
+        answer = 1;
 
-
-        while(answer == 1){
-
-            Computer computer = new Computer();
-            computer.init();
-            System.out.println("computer = " + computer);
-
+        while(answer != 2){
+            computer.selectUniqueNumbers();
             Player player = new Player();
 
-            while(true) {
-                String s = Console.readLine();
-                System.out.println(computer +  ", " +s);
-                player.input(s);
+            StartGame(computer, player);
+        }
+    }
 
-                Map<String, Integer> scoreMap = computer.calcScore(player);
+    public static void StartGame(Computer computer, Player player){
+        result = "";
+        while(true) {
+            String s = Console.readLine();
+            player.input(s);
 
-                if (scoreMap.containsKey("낫싱")) {
-                    System.out.println("낫싱");
-                } else {
-                    String result = "";
-                    if (scoreMap.containsKey("볼")) {
-                        result += scoreMap.get("볼") + "볼";
-                    }
+            Map<String, Integer> scoreMap = Controller.calcScore(computer, player);
 
-                    if (scoreMap.containsKey("스트라이크")) {
-                        if (result.length() == 0) {
-                            result += scoreMap.get("스트라이크") + "스트라이크";
-                        } else {
-                            result += " " + scoreMap.get("스트라이크") + "스트라이크";
-                        }
+            if (scoreMap.containsKey(NOTHING)) {
+                System.out.println(NOTHING);
+            } else {
 
-                        if (scoreMap.get("스트라이크") == 3) {
-                            System.out.println(result);
-                            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-
-                            answer = Integer.valueOf(Console.readLine());
-                            break;
-                        }
-                    }
-
-                    System.out.println(result);
-
+                if (scoreMap.containsKey(BALL)) {
+                    result += scoreMap.get(BALL) + "볼";
                 }
+
+                if (scoreMap.containsKey(STRIKE)) {
+                    if (result.length() == 0) {
+                        result += scoreMap.get(STRIKE) + "스트라이크";
+                    } else {
+                        result += " " + scoreMap.get(STRIKE) + "스트라이크";
+                    }
+
+                    if (scoreMap.get(STRIKE) == 3) {
+                        System.out.println(result);
+                        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+                        answer = Integer.valueOf(Console.readLine());
+                        break;
+                    }
+                }
+
+                System.out.println(result);
             }
         }
     }
