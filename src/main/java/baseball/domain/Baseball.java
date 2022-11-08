77803a0dde72b99b84588e09cf0eb2a6c9dfd4d4
saehim <saeilee.vxv@gmail.com>
2022-11-08 21:28:36 +0900

    Feat: Baseball 클래스 생성
    - ball, strike

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -1,2 +1,32 @@
-package baseball.domain;public class Baseball {
+package baseball.domain;
+
+public class Baseball {
+    int ball;
+    int strike;
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+    public Baseball() {
+        this.ball = 0;
+        this.strike = 0;
+    }
+
+    public Baseball(int ball, int strike) {
+        this.ball = ball;
+        this.strike = strike;
+    }
+
+    public void strike() {
+        this.strike += 1;
+    }
+
+    public void ball() {
+        this.ball += 1;
+    }
 }
