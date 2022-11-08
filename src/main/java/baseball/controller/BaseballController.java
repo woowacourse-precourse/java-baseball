33bfc89a7da9f83d74785bc6a573ac32c5800bf0
LeeKiWoo <99247279+KAispread@users.noreply.github.com>
2022-11-08 13:45:52 +0900

    feat: "BaseballController"
    - 전체 게임을 담당하는 BaseballController 생성
    - startGame 으로 게임이 시작되어, PlayGround 에서 진행.
    - 이후 게임을 다시시작할 때 새로운 PlayGround 생성

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,17 +1,23 @@
 package baseball.controller;
 
+import baseball.controller.ground.PlayGround;
+
 import static baseball.controller.GameFlag.START;
 
 public class BaseballController {
     private GameFlag gameFlag;
+    private PlayGround playGround;
 
     public BaseballController() {
         this.gameFlag = START;
     }
 
     public void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (gameFlag == START) {
-
+            playGround = new PlayGround();
+            playGround.playInGround();
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         }
     }
 }
