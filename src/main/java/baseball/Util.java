25a65c371765d865682752c12d2285a0b6b7e684
jhon3242 <rldotnr2@gmail.com>
2022-11-08 13:18:43 +0900

    refactor: 변수 이름 명확하게 수정
    Util
    - chars -> charArray
    - ch -> word
    - 변수명들을 직관적인 이름으로 바꿔주었다

--- src/main/java/baseball/Util.java
+++ src/main/java/baseball/Util.java
@@ -2,11 +2,11 @@ package baseball;
 
 public class Util {
 	public static String getDistinctString(String src) {
-		char[] chars = src.toCharArray();
+		char[] charArray = src.toCharArray();
 		String result = "";
-		for (char ch : chars) {
-			if (!result.contains(String.valueOf(ch))) {
-				result += ch;
+		for (char word : charArray) {
+			if (!result.contains(String.valueOf(word))) {
+				result += word;
 			}
 		}
 		return result;
