bf096a159e263de5573430979e081ac157efd3f2
masiljangajji <hjk172262@gmail.com>
2022-11-08 23:01:02 +0900

    feat: 최종 Application class

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,12 +1,15 @@
 package baseball;
 public class Application {
+
     public static void main(String[] args) {
         PlayBaseBallGame playBaseBall = new PlayBaseBallGame();
         InputNumber number = new InputNumber();
+        Exception exception = new Exception();
 
-        while (true) {
+        while(true) {
             playBaseBall.playGame();
             number.inputReStartNumber();
+            exception.checkReStarException(number.reStartNumber);
             if (number.reStartNumber.equals("2")) {
                 break;
             }
