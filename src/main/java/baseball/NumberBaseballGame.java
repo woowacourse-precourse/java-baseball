bbbf562b8c35af7ea0fbc63ccc7c2ad86f081dae
HaYoung Jung <82437499+summerlight636@users.noreply.github.com>
2022-11-08 17:46:43 +0900

    feat: 게임 진행 기능 추가 (startGame)

---
+++ src/main/java/baseball/NumberBaseballGame.java
@@ -0,0 +1,35 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class NumberBaseballGame {
+    public void startGame() {
+        boolean start = true;
+        while (start) {
+            int strike = 0;
+            int ball = 0;
+            int restartNumber;
+            List<Integer> computerNumber;
+            List<Integer> playerNumber;
+
+            printStartMessage();
+            computerNumber = getComputerNumber();
+            while (strike != 3) {
+                askPlayerToInputNumber();
+                playerNumber = getPlayerNumber();
+                strike = calculateStrikeAndBall(computerNumber, playerNumber).get(0);
+                ball = calculateStrikeAndBall(computerNumber, playerNumber).get(1);
+                printResultMessage(strike, ball);
+            }
+            printEndMessage();
+            printRestartMessage();
+            restartNumber = getRestartNumber();
+            start = judgeRestart(restartNumber);
+        }
+    }
+}
