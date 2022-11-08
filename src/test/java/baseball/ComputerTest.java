e1478ed40e10a5b37fb63c50cf69bf7517461c55
JeongHunHui <deca3248@gmail.com>
2022-11-08 18:53:41 +0900

    test: 컴퓨터가 값을 생성할때 유효한 값인지 테스트하는 코드 작성

---
+++ src/test/java/baseball/ComputerTest.java
@@ -0,0 +1,14 @@
+package baseball;
+
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+public class ComputerTest {
+
+    @Test
+    @RepeatedTest(100)
+    void 컴퓨터가_생성하는_값이_유효한값인가(){
+        Computer computer = new Computer();
+        computer.setAnswerNumber();
+    }
+}
