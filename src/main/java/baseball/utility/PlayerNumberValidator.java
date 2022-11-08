555bcdba61d73285f8966fcc1a841aaf82c6eb2a
yongseungLeeDec <yonson.dev@gmail.com>
2022-11-08 17:52:49 +0900

    feat(feature): 사용자 번호 길이 유효 확인 기능 구현
    사용자가 입력한 번호가 게임에서 허용하는 유효한 길이인지 확인하는
    기능을PlayerNumberValidator의 isPlayerNumberLengthValid() 메서드로 구현
    - 본 메서드는 정적 메서드임.

--- src/main/java/baseball/utility/PlayerNumberValidator.java
+++ src/main/java/baseball/utility/PlayerNumberValidator.java
@@ -1,8 +1,14 @@
 package baseball.utility;
 
+import baseball.constant.GameConstant;
+
 public class PlayerNumberValidator {
 
     private PlayerNumberValidator() {
 
+    }
+
+    private static boolean isPlayerNumberLengthValid(String playerNumber) {
+        return playerNumber.length() == GameConstant.VALID_NUMBER_LENGTH.getValue();
     }
 }
