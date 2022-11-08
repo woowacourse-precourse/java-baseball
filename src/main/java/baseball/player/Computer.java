fca784fdc21bccb5868ab7473c190d166b1e93e0
ddukddaki <rlfrkdms1@naver.com>
2022-11-08 14:57:40 +0900

    feat(Computer): 조건에 맞게 랜덤값을 생성하는 기능

--- src/main/java/baseball/player/Computer.java
+++ src/main/java/baseball/player/Computer.java
@@ -1,10 +1,25 @@
 package baseball.player;
 
+import static baseball.Constant.LENGTH_OF_NUMBER;
+import static baseball.Constant.NUM_MAX_RANGE;
+import static baseball.Constant.NUM_MIN_RANGE;
+
+import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Computer {
 
     private List<Integer> number = new ArrayList<>();
-    
+
+    public List<Integer> createNumber() {
+        while (number.size() < LENGTH_OF_NUMBER) {
+            int pickNumberInRange = Randoms.pickNumberInRange(NUM_MIN_RANGE, NUM_MAX_RANGE);
+            if (!number.contains(pickNumberInRange)) {
+                number.add(pickNumberInRange);
+            }
+        }
+        return number;
+    }
+
 }
