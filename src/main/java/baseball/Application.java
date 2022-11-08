d423a86d2f18bc698b2da21ba6dd6920fbdb4bf5
KongDuHwan <kong_du@naver.com>
2022-11-08 18:24:00 +0900

    9. 결과를 출력할 printBallStrike 함수

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -130,11 +130,20 @@ public class Application {
         }
     }
 
+    static void printBallStrike(int ball, int strike) {
+        if (ball != 0 && strike != 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        } else if (ball == 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (strike == 0) {
+            System.out.println(ball + "볼");
+        } else {
+            System.out.println("printBallStrikeError!");
+        }
+    }
+
     static boolean playNewGame() {
     }
 
-
-    static void printBallStrike(int ball, int strike) {
-    }
 
 }
