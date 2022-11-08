6b80d19b647ae93c9f58746ee7fa8b494745fcae
SONGmINjIJI <songminki18@gmail.com>
2022-11-08 22:25:51 +0900

    feat: 플레이어 숫자 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,6 +7,20 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class Application {
+
+    public ArrayList<Integer> input_my_num() {
+        ArrayList<Integer> my_num = new ArrayList<>();
+        String num_str = "";
+        System.out.print("숫자를 입력해주세요 : ");
+        num_str = Console.readLine();
+        int num = Integer.parseInt(num_str);
+        while (num > 0) {
+            my_num.add(num % 10);
+            num/=10;
+        }Collections.reverse(my_num);
+        
+        return my_num;
+    }
     public ArrayList<Integer> make_computer_number() {
         ArrayList<Integer> com_num = new ArrayList<>();
         com_num.add(Randoms.pickNumberInRange(1, 9));
