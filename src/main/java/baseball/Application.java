242b2eeac02f43520915539b7b3a6936ae2d52b0
Baejw0111 <bjw0111@naver.com>
2022-11-08 21:20:28 +0900

    [feat] pickRandomNumber 구현
    opponentNumber를 랜덤으로 설정하는 메서드인 pickRandomNumber 구현, 프로그램에 필요한 기본적인 변수 선언

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
 
 public class Application {
+    public static int strike=0,ball=0;
+    public static int gameSwitch=1;
+    public static ArrayList<Integer> opponentNumber=new ArrayList<Integer>();
+    public static ArrayList<Integer> playerNumber=new ArrayList<Integer>();
+
+    public static void pickRandomNumber(){
+        opponentNumber.clear();
+        while (opponentNumber.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!opponentNumber.contains(randomNumber)) {
+                opponentNumber.add(randomNumber);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
     }
 }
