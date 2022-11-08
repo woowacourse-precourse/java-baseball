48cd34dea0415357f238545ba9933e6983a46df6
moonchaewon <codnjs8989@gmail.com>
2022-11-08 22:29:12 +0900

    test(hint): add test code to check hint output

---
+++ src/test/java/baseball/HintTest.java
@@ -0,0 +1,82 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HintTest extends NsTest {
+
+    final List<Integer> randomNumber = new ArrayList<>(List.of(3, 6, 9));
+
+    @Test
+    void _1볼() {
+        List<Integer> playerNumber = new ArrayList<>(List.of(2, 4, 6));
+        HintCalculator.calculateHint(randomNumber, playerNumber);
+        assertThat(output()).isEqualTo("1볼");
+    }
+
+    @Test
+    void _2볼() {
+        List<Integer> playerNumber = new ArrayList<>(List.of(2, 3, 6));
+        HintCalculator.calculateHint(randomNumber, playerNumber);
+        assertThat(output()).isEqualTo("2볼");
+    }
+
+    @Test
+    void _3볼() {
+        List<Integer> playerNumber = new ArrayList<>(List.of(9, 3, 6));
+        HintCalculator.calculateHint(randomNumber, playerNumber);
+        assertThat(output()).isEqualTo("3볼");
+    }
+
+    @Test
+    void _1스트라이크() {
+        List<Integer> playerNumber = new ArrayList<>(List.of(3, 4, 5));
+        HintCalculator.calculateHint(randomNumber, playerNumber);
+        assertThat(output()).isEqualTo("1스트라이크");
+    }
+
+    @Test
+    void _2스트라이크() {
+        List<Integer> playerNumber = new ArrayList<>(List.of(2, 6, 9));
+        HintCalculator.calculateHint(randomNumber, playerNumber);
+        assertThat(output()).isEqualTo("2스트라이크");
+    }
+
+    @Test
+    void _3스트라이크() {
+        List<Integer> playerNumber = new ArrayList<>(List.of(3, 6, 9));
+        HintCalculator.calculateHint(randomNumber, playerNumber);
+        assertThat(output()).isEqualTo("3스트라이크");
+    }
+
+    @Test
+    void _1볼_1스트라이크() {
+        List<Integer> playerNumber = new ArrayList<>(List.of(3, 4, 6));
+        HintCalculator.calculateHint(randomNumber, playerNumber);
+        assertThat(output()).isEqualTo("1볼 1스트라이크");
+    }
+
+    @Test
+    void _2볼_1스트라이크() {
+        List<Integer> playerNumber = new ArrayList<>(List.of(3, 9, 6));
+        HintCalculator.calculateHint(randomNumber, playerNumber);
+        assertThat(output()).isEqualTo("2볼 1스트라이크");
+    }
+
+    @Test
+    void 낫싱() {
+        List<Integer> playerNumber = new ArrayList<>(List.of(1, 4, 7));
+        HintCalculator.calculateHint(randomNumber, playerNumber);
+        assertThat(output()).isEqualTo("낫싱");
+    }
+
+    @Override
+    public void runMain() {
+        Application.main(new String[]{});
+    }
+}
