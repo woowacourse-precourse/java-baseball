52b1e60f877138cd70c4912ecc0cbc80a48c2c99
gomljo <gomljo0307@naver.com>
2022-11-08 19:26:36 +0900

    feat(InputView.java): 게임 시작 후 플레이어(사용자)에게 숫자를 입력하라는 메시지를 출력하는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -2,8 +2,13 @@ package baseball.view;
 
 public class InputView {
     private static final String GAME_START_MESSAGE="숫자 야구 게임을 시작합니다.";
-   
+    private static final String REQUEST_ENTER_INPUT="숫자를 입력해주세요 : ";
+
     public void noticeGameStart(){
         System.out.println(GAME_START_MESSAGE);
     }
+    public void requestEnteringNumbers(){
+        System.out.print(REQUEST_ENTER_INPUT);
+    }
+
 }
