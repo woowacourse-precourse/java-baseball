2e7c2f273d27891646cbda35896a434cdba71b3a
HikiJun97 <sgn04088@gmail.com>
2022-11-08 20:39:42 +0900

    feat: implement exception handling method
    Add the method handling exception from invalid user input.
    It handles exception such as too many args, too long arg, 0 input.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,6 +10,45 @@ import java.lang.IllegalArgumentException;
 
 public class Application {
 
+    static void handleLineException(String[] parsedString){
+        if (parsedString.length != 3)
+            throw new IllegalArgumentException("Too many arguments");
+    }
+
+    static void handleWordException(String word){
+        if (word.length() != 1)
+            throw new IllegalArgumentException("Wrong argument length");
+        if (!Character.isDigit(word.charAt(0)))
+            throw new IllegalArgumentException("Argument is not a number");
+    }
+
+    static void handleZeroException(Integer num){
+        if (num == 0)
+            throw new IllegalArgumentException("Invalid range of input");
+    }
+
+    static Integer castToInteger(String str){
+        Integer num = 0;
+        try {
+            num = Integer.parseInt(str);
+            handleZeroException(num);
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return num;
+    }
+
+    static void handleException(String[] parsedString){
+        try {
+            handleLineException(parsedString);
+            for (String word : parsedString){
+                handleWordException(word);
+            }
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+
     static List<Integer> inputAnswer(){
         List<Integer> inputList;
         Integer[] inputArray = new Integer[3];
@@ -24,8 +63,10 @@ public class Application {
         System.out.println(parsedString.length);
         //
 
+        handleException(parsedString);
+
         for (int i = 0; i < 3; i++) {
-            inputArray[i] = Integer.parseInt(parsedString[i]);
+            inputArray[i] = castToInteger(parsedString[i]);
         }
         inputList = Arrays.asList(inputArray);
 
