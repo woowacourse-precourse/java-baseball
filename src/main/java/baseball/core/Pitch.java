56bf047717a15b36e0c0784c54af474f3d492867
hyungkya <hyungkya@uci.edu>
2022-11-08 13:43:44 +0900

    feat: add Pitch class with constructor

---
+++ src/main/java/baseball/core/Pitch.java
@@ -0,0 +1,16 @@
+package baseball.core;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static baseball.util.Random.generateIntegers;
+
+public class Pitch {
+    private List<Integer> pitches;
+
+    public Pitch() {
+        this.pitches = List.of(1, 2, 3);
+    }
+}
