afdd78ad4cb94cfa6b52a8350dcdd79d805c5819
sun007021 <sun007021@gmail.com>
2022-11-08 20:18:27 +0900

    feat(Domain : Computer) : 정답 3자리수 저장

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,9 +1,23 @@
 package baseball.domain;
 
+import baseball.util.RandomNumbers;
+import baseball.view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
     private BaseballNumbers correctBaseballNumbers; // 정답값
 
-    public void setRandomCorrectBaseballNumbers(){
+    public void setCorrectBaseballNumbersRandom(){
+        List<Integer> randomNumberList = new ArrayList<>();
+        randomNumberList = RandomNumbers.createRandomIntListWithoutDuplication(1, 9);
+        List<BaseballNumber> randomBaseballNumberList = new ArrayList<>();
+        for(int i=0; i<randomNumberList.size();i++){// BaseballNumberList 형태로 변환
+            BaseballNumber baseballNumber = new BaseballNumber(randomNumberList.indexOf(i));
+            randomBaseballNumberList.add(baseballNumber);
+        }
 
+        correctBaseballNumbers = new BaseballNumbers(randomBaseballNumberList);
     }
 }
