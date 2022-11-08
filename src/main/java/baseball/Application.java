bdb04542057c36bb18a80662ea430b645201a27b
yeop0740 <ssucgoo@gmail.com>
2022-11-08 09:37:28 +0900

    feat(Application): main 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,6 +9,9 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        playGame();
+
+        System.out.println("게임을 종료합니다.");
     }
 
     public static List<Integer> selectNumbers() {
