8c6016b30229830f68b4e3b44a61289163c0a552
dener8 <8dener@gmail.com>
2022-11-08 22:08:31 +0900

    feat: 게임을 한 판 더할 때, 새로운 랜덤값을 만드는 makeNewRandomNumber() 구현
    game()내의 depth 3을 피하기 위해서 불가피하게 구현했다.
    NumberMaker 클래스에서 숫자 만드는 모든 작업을 처리할 수 있게 만들어야 가장 좋은 코드가 아닐까 싶다..

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,7 +7,6 @@ import static baseball.InputNumberExceptionChecker.hasException;
 
 public class Application {
     public static void main(String[] args) {
-
         NumberMaker numberMaker = new NumberMaker();
         game(numberMaker);
     }
@@ -19,7 +18,7 @@ public class Application {
         String randomNumber;
         String playerNumber;
         boolean isContinuousGame = true;
-        randomNumber = numberMaker.makeRandomNumber();
+        randomNumber = numberMaker.makeInitRandomNumber();
         System.out.println(GAME_START_MESSAGE);
 
         while (isContinuousGame) {
@@ -33,23 +32,41 @@ public class Application {
 
             // 완전히 동일한 경우
             if (randomNumber.equals(playerNumber)) {
-                System.out.println(THREE_STRIKE_MESSAGE);
-                System.out.println(RESTART_OR_NOT_MESSAGE);
-                restartSign = Integer.parseInt(Console.readLine());
-                hasException(restartSign);
-                isContinuousGame = restart(restartSign);
-
+                isContinuousGame = isEqual(randomNumber, playerNumber);
+                randomNumber = makeNewRandomNumber(isContinuousGame);
             }
         }
     }
 
-    private static boolean restart(int restartSign) {
-        if (restartSign == 1) {
-            return true;
+    public static String makeNewRandomNumber(boolean isContinuousGame) {
+        NumberMaker numberMaker = new NumberMaker();
+        String newString;
+        if (isContinuousGame) {
+            newString = numberMaker.makeInitRandomNumber();
+            return newString;
         } else {
-            return false;
+            return null;
         }
     }
+
+    public static boolean isEqual(String randomNumber, String playerNumber) {
+        int restartSign;
+        boolean isContinuousGame;
+        System.out.println(THREE_STRIKE_MESSAGE);
+        System.out.println(RESTART_OR_NOT_MESSAGE);
+        restartSign = Integer.parseInt(Console.readLine());
+        hasException(restartSign);
+        if (restartSign == 1) {
+            isContinuousGame = true;
+        } else {
+            isContinuousGame = false;
+        }
+        return isContinuousGame;
+    }
+
+
+
+
 
 
 }
