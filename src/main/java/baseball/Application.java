f1b0ab1e685c074fc77555196287d95a6554d184
이지영 <jioung93@naver.com>
2022-11-08 23:33:27 +0900

    feat: 메인 로직 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,20 @@
 package baseball;
 
+import baseball.constant.Message;
+import baseball.entity.Game;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-//        새 게임 생성: Game game = new Game();
+        Game game = new Game();
 
-//        초기 문구 출력
+        System.out.println(Message.START_MESSAGE);
 
-//        game.init();
-//        game.start();
+        while(game.checkStopGame()) {
+            game.init();
+            game.start();
+        }
 
     }
 }
