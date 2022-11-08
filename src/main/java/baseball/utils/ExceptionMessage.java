42b95bde5dd590b3af960d7bca4b4615a32051b6
hongjinwon <herenever@naver.com>
2022-11-08 15:47:26 +0900

    feat(utils): 출력 메세지 작성
    1. 메세지
    - 사용자의 input이 유효하지 않을 경우 각각의 경우에 맞는 message 작성
    - 게임 진행시 어플리케이션이
    출력해야할 message 작성

---
+++ src/main/java/baseball/utils/ExceptionMessage.java
@@ -0,0 +1,8 @@
+package baseball.utils;
+
+public class ExceptionMessage {
+    public static final String USER_INPUT_NON_NUMBER_EXCEPTION = "숫자만 입력 가능합니다.";
+    public static final String USER_INPUT_OUT_OF_RANGE_EXCEPTION = "3자리 이내의 숫자만 입력해주세요.";
+    public static final String USER_INPUT_DUPLICATED_NUMBER_EXCEPTION = "중복되지 않은 3자리 숫자를 입력해주세요";
+    public static final String USER_INPUT_END_EXCEPTION = "시작하려면 1, 종료하려면 2를 입력해주세요.";
+}
