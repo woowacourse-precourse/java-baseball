f2e7cbde36da767843720bee6a6e0723a472cb2f
Identityless <rlandwnd555@naver.com>
2022-11-08 21:07:35 +0900

    fix : 숫자름 입력해 주세요 문구 출력 삭제

--- src/main/java/util/Inputer.java
+++ src/main/java/util/Inputer.java
@@ -14,7 +14,6 @@ public class Inputer {
     }
 
     public List<Integer> getInput() throws IllegalArgumentException{
-        System.out.print("숫자를 입력해주세요 : ");
         String rareinput = Console.readLine();
         List<Integer> result = null;
         if(!flag) {
