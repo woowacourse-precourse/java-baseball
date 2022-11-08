23c9c1830ab02b0ee71c9d037b36ea8a0116e820
sypark <soun997@gmail.com>
2022-11-08 20:49:23 +0900

    fix: modify class 'BaseballGame's 'computersChoice' method
    If the computer picks up a number that already exists in the list, three-repeated for-loop can end before picking up three numbers, so use while-loop and define field to avoid infinity-loop issue

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -9,6 +9,8 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 public class BaseballGame {
 
+    private List<Integer> computer;
+
     /*
      * 컴퓨터가 세자리의 랜덤 숫자를 선택, 각 자리값을 리스트에 담아 반환
      */
@@ -16,7 +18,7 @@ public class BaseballGame {
 
         List<Integer> computer = new ArrayList<>();
 
-        for (int i = 0; i < 3; i++) {
+        while(computer.size() < 3) {
 
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computer.contains(randomNumber)) {
@@ -99,10 +101,13 @@ public class BaseballGame {
      */
     private boolean restartOrQuitGame() {
 
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
         System.out.println(input);
 
         if (Integer.valueOf(input).equals(1)) {
+            computer = computersChoice();
             return false;   // 게임 재시작
         } else if (Integer.valueOf(input).equals(2)) {
             return true;    // 게임 종료
@@ -113,8 +118,6 @@ public class BaseballGame {
 
     public void run(){
 
-        List<Integer> computer;
-        List<Integer> user;
         List<String> results = new ArrayList<>();
         boolean quit = false;
 
@@ -124,15 +127,12 @@ public class BaseballGame {
         while (!quit) {
 
             System.out.print("숫자를 입력해주세요 : ");
-            user = usersChoice();
+            List<Integer> user = usersChoice();
             results.add(getGameResult(computer, user));
             System.out.println(results.get(results.size() - 1));    // 숫자 판별 결과 출력
 
             // 결과가 3스트라이크라면 게임종료, 재시작 혹은 애플리케이션 종료 중 하나 선택
             if (results.get(results.size() - 1).equals("3스트라이크")) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                computer = computersChoice();
                 quit = restartOrQuitGame();    // 게임을 재시작할지 종료할지 여부 결정
             }
         }
