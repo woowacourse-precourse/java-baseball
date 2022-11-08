1257ca0d9d0a957c34dc1493077760d6316b27e7
Yunsik-Choi <choiys0311@gmail.com>
2022-11-08 20:42:18 +0900

    ✨style : Number 엔터 정리

--- src/main/java/baseball/domain/Number.java
+++ src/main/java/baseball/domain/Number.java
@@ -8,7 +8,6 @@ public class Number {
     private static final int NUMBER_MIN = 1;
     private static final int NUMBER_MAX = 9;
     private static final String BOUND_EXCEPTION = "숫자의 범위는 " + NUMBER_MIN + "부터 " + NUMBER_MAX + "까지 입니다.";
-    
     private static final Map<Integer, Number> numbers = new HashMap<>();
     private final int number;
 
