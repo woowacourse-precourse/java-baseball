e9175e62163ec107f909581d351915f945d402b0
mikekang47 <gyeong0308@gmail.com>
2022-11-08 23:26:46 +0900

    Feat: 사용자 입력과 컴퓨터 난수를 비교하는 기능

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -12,4 +12,18 @@ public class Game {
         return ball;
     }
 
+    public void compare(String userInput, String computerProducedRandomNumber) {
+        strike = 0;
+        ball = 0;
+
+        for (String s : userInput.split("")) {
+            if (computerProducedRandomNumber.contains(s)) {
+                if (userInput.indexOf(s) == computerProducedRandomNumber.indexOf(s)) {
+                    this.strike += 1;
+                } else {
+                    this.ball += 1;
+                }
+            }
+        }
+    }
 }
