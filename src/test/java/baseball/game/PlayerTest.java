567590792e679ec0c9f61da3c2b3e74e560e504c
Basak <chgy2131@naver.com>
2022-11-08 19:22:49 +0900

    test(PlayerTest): 각 사용자입력에 대한 검증 함수 테스트 코드 추가

--- src/test/java/baseball/game/PlayerTest.java
+++ src/test/java/baseball/game/PlayerTest.java
@@ -1,13 +1,13 @@
 package baseball.game;
 
-import baseball.domain.BaseballNumberBundle;
-import baseball.domain.GameProgress;
-import baseball.domain.GameResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PlayerTest {
     Player player;
@@ -16,23 +16,6 @@ class PlayerTest {
     void beforeEach() {
         player = new Player();
     }
-
-    /*
-    @Test
-    void inputBaseballNumbers() {
-        BaseballNumberBundle b = player.inputBaseballNumbers();
-        for(int n:b.getList()) {
-            System.out.println(n);
-        }
-    }
-
-    @Test
-    void inputGameProgress() {
-        GameProgress gp = player.inputGameProgress();
-        System.out.println(gp.getProgress());
-    }
-
-     */
 
     @Test
     void removeSeparator() {
@@ -43,16 +26,34 @@ class PlayerTest {
         s = player.removeSeparator(s);
         e = "hello";
         assertThat(s).isEqualTo(e);
+    }
+
+    @Test
+    void removeSeparator2() {
+        String s;
+        String e;
 
         s = "h  e  l  l  o";
         s = player.removeSeparator(s);
         e = "hello";
         assertThat(s).isEqualTo(e);
+    }
+
+    @Test
+    void removeSeparator3() {
+        String s;
+        String e;
 
         s = "  h\nello\n";
         s = player.removeSeparator(s);
         e = "hello";
         assertThat(s).isEqualTo(e);
+    }
+
+    @Test
+    void removeSeparator4() {
+        String s;
+        String e;
 
         s = "/h 1 e 2 l. 3, l4.  o5,\n";
         s = player.removeSeparator(s);
@@ -67,37 +68,76 @@ class PlayerTest {
     }
     @Test
     void numberValid2() {
+        boolean flag = false;
         String s = "1a2345";
         try {
             player.numberValid(s);
         } catch (IllegalArgumentException e) {
             System.out.println(e.toString());
+            flag = true;
         }
+        assertTrue(flag);
     }
     @Test
     void numberValid3() {
+        boolean flag = false;
         String s = "       b";
         try {
             player.numberValid(s);
         } catch (IllegalArgumentException e) {
             System.out.println(e.toString());
+            flag = true;
         }
+        assertTrue(flag);
     }
     @Test
     void numberValid4() {
+        boolean flag = false;
         String s = "       1";
         try {
             player.numberValid(s);
         } catch (IllegalArgumentException e) {
             System.out.println(e.toString());
+            flag = true;
         }
+        assertTrue(flag);
     }
 
     @Test
     void baseballVaild() {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        player.baseballValid(list);
+
+        boolean flag = false;
+        list.add(4);
+        try {
+            player.baseballValid(list);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.toString());
+            flag = true;
+        }
+        assertTrue(flag);
     }
 
     @Test
-    void progressVaild() {
+    void progressVaild1() {
+        player.progressValid(1);
+    }
+    @Test
+    void progressVaild2() {
+        player.progressValid(2);
+    }
+    @Test
+    void progressVaild3() {
+        boolean flag = false;
+        try {
+            player.progressValid(3);
+        } catch (IllegalArgumentException e) {
+            flag = true;
+        }
+        assertTrue(flag);
     }
 }
