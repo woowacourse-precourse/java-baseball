230a162b54f2a6660312c18232fc9980fa671d26
Jung <anes53027@gmail.com>
2022-11-08 22:30:10 +0900

    feat(InputException) : isInteger 입력한 자릿값이 다 숫자인지 확인하는 함수 생성

--- src/main/java/baseball/exception/InputException.java
+++ src/main/java/baseball/exception/InputException.java
@@ -2,8 +2,18 @@ package baseball.exception;
 
 public class InputException {
 
+    private static final int LENGTH = 3;
+
     private static boolean isThreeLength(String input){
-        return input.length() == 3;
+        return input.length() == LENGTH;
     }
 
+    private static boolean isInteger(String input) {
+        for (int i = 0; i < LENGTH; i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
