184c8b906fd9c530ba7630000b8f8a1667264294
park-Kumin <rbals97@naver.com>
2022-11-08 20:22:45 +0900

    feat: 사용자에게 숫자 입력받기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.ArrayList;
@@ -14,6 +15,12 @@ public class Application {
         // TODO: 프로그램 구현
         // 랜덤한 숫자 받아온 후 컴퓨터 리스트에 값 추가
         List<Integer> computer = addToRandomNumberList();
+
+        while (true) {
+            List<Integer> user = new ArrayList<>();
+            // 사용자에게 숫자 입력받기
+            String user_number = getTheUserToInputNumber();
+        }
     }
 
     // 랜덤한 숫자 받아온 후 컴퓨터 리스트에 값 추가
@@ -26,5 +33,10 @@ public class Application {
             }
         }
         return computer;
+    }
+    // 사용자에게 숫자 입력받기
+    public static String getTheUserToInputNumber() {
+        System.out.println("숫자를 입력해 주세요");
+        return Console.readLine();
     }
 }
