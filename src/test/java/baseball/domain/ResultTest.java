adff68dd791abd72906c119b8cefa35a9a556db0
DaWoon Lee <qkgk61@gmail.com>
2022-11-08 22:28:07 +0900

    Test : Result 클래스의 isCorrect 테스트

--- src/test/java/baseball/domain/ResultTest.java
+++ src/test/java/baseball/domain/ResultTest.java
@@ -26,4 +26,35 @@ public class ResultTest {
         assert (!result.isNothing());
     }
 
+    @Test
+    void isCorrect_비어있는_경우_오답_판단() {
+        Result result = new Result();
+        assert (!result.isCorrect(3));
+    }
+
+    @Test
+    void isCorrect_볼만_경우_오답_판단() {
+        Result result = new Result();
+        result.add(ResultType.BALL);
+        result.add(ResultType.BALL);
+        assert (!result.isCorrect(3));
+    }
+
+    @Test
+    void isCorrect_스트라이크가_부족한_경우_오답_판단() {
+        Result result = new Result();
+        result.add(ResultType.STRIKE);
+        result.add(ResultType.STRIKE);
+        assert (!result.isCorrect(3));
+    }
+
+    @Test
+    void isCorrect_모두_스트라이크인_경우_정답_판단() {
+        Result result = new Result();
+        result.add(ResultType.STRIKE);
+        result.add(ResultType.STRIKE);
+        result.add(ResultType.STRIKE);
+        assert (result.isCorrect(3));
+    }
+
 }
