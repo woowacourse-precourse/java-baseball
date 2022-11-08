48ca5388fcb65201fd92dc0a6b97df811692ad03
yoon <beatus96@naver.com>
2022-11-08 10:00:11 +0900

    feat(rule) : reload

--- src/main/java/baseball/GameRule.java
+++ src/main/java/baseball/GameRule.java
@@ -1,2 +1,10 @@
-package baseball;public class GameRule {
+package baseball;
+
+public class GameRule {
+    public static final int DIGITS = 3;
+    public static final int RANDOM_MIN =1;
+    public static final int RANDOM_MAX =9;
+    public static final String COMMAND_RESTART ="1";
+    public static final String COMMAND_END ="2";
+
 }
