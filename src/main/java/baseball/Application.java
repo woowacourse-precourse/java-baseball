03488bb14c4b8196c13e61e273f2722947362df2
kite707 <kite707707@gmail.com>
2022-11-08 14:54:52 +0900

    feat(baseball): 메인 함수에 메서드 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,31 @@
 package baseball;
 
+import static game.GamePlay.getAnswer;
+import static game.GamePlay.provideHintMessage;
+import static user.UserInput.getUserInput;
+import static user.UserInput.isRetry;
+
+import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        final List<Integer> answer = getAnswer();
+        System.out.println(answer);
+
+        System.out.println("숫자 야구 게임을 시작합니다.");
+
+        boolean isContinued = true;
+
+        while (isContinued) {
+            boolean isCorrect = provideHintMessage(answer, getUserInput());
+            if (isCorrect == true) {
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                final String retryUserInput = Console.readLine();
+                isContinued = isRetry(retryUserInput);
+            }
+        }
+
     }
 }
