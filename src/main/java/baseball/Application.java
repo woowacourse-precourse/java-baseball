33de8ea30e7c4533d3837aedd6177ce35795a7fc
dener8 <8dener@gmail.com>
2022-11-08 20:23:42 +0900

    refactor: restartOrNot으로 정의한 변수를 restartSign으로 변경
    restartSign이 조금 더 직관적이라고 생각함.
    1과 2가 아닌 다른 값이 들어왔을 때도 생각해서 지은 이름

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,20 +7,22 @@ import static baseball.InputNumberExceptionChecker.hasException;
 
 public class Application {
     public static void main(String[] args) {
-        boolean continuousGame = true;
+
         NumberMaker numberMaker = new NumberMaker();
         game(numberMaker);
     }
 
     private static void game(NumberMaker numberMaker) {
-        int restartOrNot;
+        int restartSign;
+        int ball;
         int strike;
         String randomNumber;
         String playerNumber;
-        int ball;
+        boolean isContinuousGame = true;
         randomNumber = numberMaker.makeRandomNumber();
         System.out.println(GAME_START_MESSAGE);
-        while (true) {
+
+        while (isContinuousGame) {
             System.out.printf(INPUT_NUMBER_MESSAGE);
             playerNumber = numberMaker.makePlayerNumber();
             hasException(playerNumber);
@@ -28,21 +30,26 @@ public class Application {
             ball = ballStrikeChecker.countBall(randomNumber, playerNumber);
             strike = ballStrikeChecker.countStrike(randomNumber, playerNumber);
             ballStrikeChecker.printHint(ball, strike);
+
             // 완전히 동일한 경우
             if (randomNumber.equals(playerNumber)) {
                 System.out.println(THREE_STRIKE_MESSAGE);
                 System.out.println(RESTART_OR_NOT_MESSAGE);
-                restartOrNot = Integer.parseInt(Console.readLine());
-                hasException(restartOrNot);
-                if (restartOrNot == 1) {
-                    randomNumber = numberMaker.makeRandomNumber();
-                    continue;
-                } else {
-                    break;
-                }
+                restartSign = Integer.parseInt(Console.readLine());
+                hasException(restartSign);
+                isContinuousGame = restart(restartSign);
+
             }
         }
     }
 
-    private static
+    private static boolean restart(int restartSign) {
+        if (restartSign == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
