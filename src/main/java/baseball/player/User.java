2756a3dcafb57de06d9a9de6417f06145aad5e16
ckyeon <qsky8520@gmail.com>
2022-11-08 15:59:23 +0900

    feat(User): implement 중복된 공이 있는지 검증하는 기능

--- src/main/java/baseball/player/User.java
+++ src/main/java/baseball/player/User.java
@@ -1,6 +1,8 @@
 package baseball.player;
 
 import baseball.Valid;
+import java.util.Collections;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class User extends Player {
@@ -17,5 +19,21 @@ public class User extends Player {
     private boolean hasNotInputNumber(String userInput) {
         Pattern notInputNumberPattern = Valid.NOT_INPUT_NUMBER.getPattern();
         return notInputNumberPattern.matcher(userInput).find();
+    }
+
+    private boolean hasDuplicateBall(String userInput) {
+        List<String> selectBalls = List.of(userInput.split(""));
+
+        for (String selectBall : selectBalls) {
+            if (isDuplicateBall(selectBalls, selectBall)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isDuplicateBall(List<String> selectBalls, String selectBall) {
+        int frequency = Collections.frequency(selectBalls, selectBall);
+        return frequency > 1;
     }
 }
