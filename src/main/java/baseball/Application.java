3d7766303ffb038ec3768ce2651aa123f145961f
KSB-skyBlueLover <gogotkdqja@yonsei.ac.kr>
2022-11-08 13:45:24 +0900

    feat: add function that generates computer number

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
@@ -7,6 +12,16 @@ public class Application {
 
     public static void welcomeMessage(){
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static List<Integer> generateComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
+        while (computerNumber.size() < 3) {
+            int randomNumber = pickNumberInRange(1, 9);
+            if (!computerNumber.contains(randomNumber))
+                computerNumber.add(randomNumber);
+        }
+        return computerNumber;
     }
 
 
