61111ade9d2632d3af5f573ae1aa873a8bb5feed
leekyukin <leekujin14@gmail.com>
2022-11-08 20:24:57 +0900

    fix: error message

--- src/main/java/baseball/exception/IllegalInputException.java
+++ src/main/java/baseball/exception/IllegalInputException.java
@@ -5,6 +5,6 @@ public class IllegalInputException extends IllegalArgumentException {
     public final static IllegalInputException EXCEPTION = new IllegalInputException();
 
     private IllegalInputException() {
-        super("존재하지 않는 선택지 입니다!");
+        super("입력값이 올바르지 않습니다!");
     }
 }
