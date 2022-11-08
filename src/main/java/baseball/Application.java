d67239b2b0382cad86fb493339d52ec3d2518119
kite707 <kite707707@gmail.com>
2022-11-08 16:15:22 +0900

    fix(baseball): 출력문구 뒤 공백 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,16 +12,16 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         List<Integer> answer = getAnswer();
-        System.out.println(answer);
+        //System.out.println(answer);
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println("숫자 야구 게임을 시작합니다. ");
 
         boolean isContinued = true;
 
         while (isContinued) {
             boolean isCorrect = provideHintMessage(answer, getUserInput());
             if (isCorrect == true) {
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
                 final String retryUserInput = Console.readLine();
                 isContinued = isRetry(retryUserInput);
                 answer = getAnswer();
