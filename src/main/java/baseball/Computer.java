a7d3a6aea50db229148bde40098229f71297793e
Amenable-C <duswo0624@naver.com>
2022-11-08 11:47:40 +0900

    feat : 컴퓨터(상대방)의 수(난수) 생성

---
+++ src/main/java/baseball/Computer.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Computer {
+
+    private List<Integer> answerNumber = new ArrayList<>();
+
+    public void createAnswerNumber() {
+        List<Integer> answer = new ArrayList<>();
+
+        while (answer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!answer.contains(randomNumber)) {
+                answer.add(randomNumber);
+            }
+        }
+
+        this.answerNumber = answer;
+    }
+
+}
