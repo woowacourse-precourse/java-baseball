22f1626e3f96d683188908b16f2fa6c8232d60c4
이지영 <wldudwldhd@naver.com>
2022-11-08 15:55:12 +0900

    docs:ãê기능목록 정리

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,13 @@
 package baseball;
-
+/* 기능목록정리
+      1. 숫자야구게임 시작문구 출력
+      2. 정답인 랜덤 숫자 3개 지정
+      3. 입력값 받기(Integer[]로 형변환
+      4. 스트라이크, 볼, 낫띵인지 판단하는 함수
+      5. 재시작(1), 종료(2) 구현
+      6. 예외처리 / IllegalArgumentException 처리
+      7. 테스트 코드 작성 후 검증
+* */
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
