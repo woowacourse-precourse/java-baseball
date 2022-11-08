f8cc6b6e2deac3268571efca082973f66faa4cc4
Beginner <dkstmdwn17@naver.com>
2022-11-08 21:53:25 +0900

    5번 기능 다 맞춘 후 재시작 혹은 종료 할 수 있는 부분과 게임의 시작 부분을 완성
    start()를 통해 게임을 시작하고 while 문을 통해 반복문이 실행된다.
    
    notFountTheRightAnswer가 참일 경우 컴퓨터는 새로운 숫자를 생성한다.
    
    guestTheRightAnswer가 거짓일 경우 컴퓨터에게 새로운 숫자를 생성하지 않게 만들기 위해 notFountTheRightAnswer를 거짓으로 만들어 준다.
    
    참일 경우 게임을 다시 시작해도 될지 입력을 받는 restartOrQuitTheGame()을 실행시킨다. 거짓이면 반복문을 종료한다.
    
    restartOrQuitTheGame가 참인 경우 computer 객체를 초기화해주고 다시 숫자를 생성해줄 수 있게 notFoundTheRightAnswer를 참으로 설정해준다.

--- src/main/java/baseball/domain/BaseballGame.java
+++ src/main/java/baseball/domain/BaseballGame.java
@@ -1,6 +1,43 @@
 package baseball.domain;
 
+import baseball.message.PromptMessage;
+import baseball.validation.Validation;
+import camp.nextstep.edu.missionutils.Console;
+
 public class BaseballGame {
+    static boolean notFoundTheRightAnswer;
+
     public void start() {
+        Computer computer = new Computer();
+        User user;
+        System.out.println(PromptMessage.WELCOME_MESSAGE);
+        notFoundTheRightAnswer = true;
+        while (true) {
+            if (notFoundTheRightAnswer) computer.chooseThreeNumbers();
+            System.out.print(PromptMessage.ENTER_NUMBERS);
+            user = new User();
+            user.pickANumber();
+            Ball ball = new Ball(computer.getNumbersChosenByTheComputer(), user.getUser_selectedNumbers());
+            ball.judgeWhichBallItIs();
+            boolean guessTheRightAnswer = ball.determineWhetherTheBallIsAStrikeOrABall();
+            if (!guessTheRightAnswer) {
+                notFoundTheRightAnswer = false;
+            } else if (!restartOrQuitTheGame()) {
+                break;
+            } else {
+                computer = new Computer();
+                notFoundTheRightAnswer = true;
+            }
+
+        }
+    }
+
+    private boolean restartOrQuitTheGame() {
+        System.out.println(PromptMessage.endOfGameMessage);
+        System.out.println(PromptMessage.START_OR_END);
+        String answer = Console.readLine();
+        Validation validation = new Validation();
+        int certifiedNumbers = validation.validationRestartOrShutDown(answer);
+        return certifiedNumbers != 2;
     }
 }
