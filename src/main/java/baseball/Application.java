1f5bf17fcd445692da232c28e4d0e697966861e9
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-11-08 13:03:07 +0900

    feat: Add restart logic to main function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,9 +7,13 @@ import java.util.List;
 public class Application {
 
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         System.out.println("숫자 야구 게임을 시작합니다.");
-        playGame();
+        boolean restart = true;
+
+        while (restart) {
+            playGame();
+            restart = Input.restart();
+        }
     }
 
     /**
