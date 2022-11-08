80a1fe952991d3916b191a730983bfb7351b2ea3
d0ri123 <lsh81550@gmail.com>
2022-11-08 19:47:15 +0900

    Fix: 숫자 검증 메서드 분리

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -2,8 +2,11 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
+import java.util.Arrays;
+
 public class Baseball {
     int user_answer;
+    boolean run = true;
 
     public void start(){
         System.out.println("숫자 야구 게임을 시작합니다.");
@@ -13,25 +16,33 @@ public class Baseball {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         user_answer = Integer.parseInt(Console.readLine());
+
+        if(user_answer==2) run = false;
     }
 
-    public boolean possibleNum(String response){
-        boolean result = false;
-        for(int i=0; i<response.length()-1; i++){
-            if((response.charAt(i)!=response.charAt(i+1)) || (response.charAt(i)!=response.charAt(2-i)))
-                result = true; //숫자 3개가 모두 다른 숫자인지 확인
-        }
-
+    public boolean isNum(String response){
+        boolean result = true;
         for(int i=0; i<response.length(); i++){
-            if(response.charAt(i)>=49 || response.charAt(i)<=57)
-                result = true; //숫자인지 확인
+            if(response.charAt(i)>=49 && response.charAt(i)<=57)
+                continue;//숫자인지 확인
+            else {
+                result = false;
+                break;
+            }
         }
         return result;
     }
 
-    public void generateEx(int response){
-        String str_response = String.valueOf(response);
-        if(!(str_response.length()==3 && possibleNum(str_response)))
+    public boolean isDifferent(String response){
+        return Arrays.asList(response.split(""))
+                .stream()
+                .distinct()
+                .count()==3;
+    }
+
+    public boolean generateEx(String response){
+        if(!(isNum(response) && isDifferent(response)))
            throw new IllegalArgumentException();
+        return true;
     }
 }
