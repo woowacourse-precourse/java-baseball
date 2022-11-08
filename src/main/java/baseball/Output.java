f0c196e8f6801853094cf24075832e22cfdac96e
HangSub999 <gkdtjq999@gmail.com>
2022-11-08 16:03:31 +0900

    feat: 스트라이크,볼 출력

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -5,4 +5,9 @@ public class Output {
     public static void printGameStart() {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
+
+    public static void printStrikeBall(int strike, int ball) {
+        System.out.println(strike + " 스트라이크" + ball + " 볼");
+    }
+
 }
