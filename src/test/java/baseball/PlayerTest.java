838d2a08f063bf175f00e4ffc3e770530450a808
Yejin Shin <shinyejin0212@gmail.com>
2022-11-08 18:55:16 +0900

    test(PlayerTest): add getAnswer test
    getAnswer()로 player가 입력한 3자리 수 저장

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -2,6 +2,8 @@ package baseball;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.List;
 
 import static baseball.Player.*;
@@ -13,6 +15,18 @@ public class PlayerTest {
         String number = "123";
         List<Integer> list = splitAnswer(number);
         List<Integer> result = List.of(1,2,3);
+        assertThat(list).isEqualTo(result);
+    }
+
+    @Test
+    void getAnswer_메소드로_Player_숫자_입력값_List_저장(){
+        String input = "123";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+
+        List<Integer> list = getAnswer();
+        List<Integer> result = List.of(1,2,3);
+
         assertThat(list).isEqualTo(result);
     }
 }
