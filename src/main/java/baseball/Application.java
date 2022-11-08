17b03dffbf9b93e4172f398f2f899df7de8d8dfe
mgOh <issac_oh@naver.com>
2022-11-08 17:32:00 +0900

    feat: 볼, 스트라이크 개수와 게임종료를 출력하는 기능 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,6 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class Application {
@@ -10,8 +9,8 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         boolean gameRestartWhether = true;
+        System.out.println("숫자 야구 게임을 시작합니다.");
         while (gameRestartWhether){
-            System.out.println("숫자 야구 게임을 시작합니다.");
             startGame(new Game());
             gameRestartWhether = getGameRestartWhether();
         }
@@ -24,12 +23,14 @@ public class Application {
             System.out.print("숫자를 입력해주세요 : ");
             final String input = Console.readLine();
             if (userInputException.checkAllCondition(input)){
-
+                List<Integer> result = new JudgePitchingResult().returnPitchingResult(input, game.getNumberList());
+                gameWin = outputOfPitchingResult(result);
             }
             else {
                 System.out.println("입력값이 잘못되었습니다.");
             }
         }
+
     }
 
     static boolean getGameRestartWhether(){
@@ -50,15 +51,25 @@ public class Application {
         }
     }
 
-    // 입력 정수값을 각 자리로 나누는 함수
-    static List<Integer> changeStringToIntegerList(String inputString) {
-        List<Integer> integerList = new ArrayList<>();
-
-        for (int i = 0; i < 3; i++) {
-            integerList.add((int) inputString.charAt(i));
+    static boolean outputOfPitchingResult(List<Integer> result){
+        if (!(result.get(1) == 3)){
+            if (result.get(0) == 0 && result.get(1) == 0 )
+            System.out.println("낫싱");
+            else if(result.get(0) == 0 && result.get(1) != 0){
+                System.out.println(result.get(1) + "스트라이크");
+            }
+            else if(result.get(0) != 0 && result.get(1) == 0){
+                System.out.println(result.get(0) + "볼");
+            }
+            else{
+                System.out.println(result.get(0) + "볼 " + result.get(1) + "스트라이크");
+            }
         }
-
-        return integerList;
+        else{
+            System.out.println(result.get(1) + "스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return true;
+        }
+        return false;
     }
-
 }
