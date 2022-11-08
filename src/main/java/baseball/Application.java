c57819e8b8900929eb45ea6894892471fdffa6e5
HikiJun97 <sgn04088@gmail.com>
2022-11-08 21:23:12 +0900

    refactor: get rid of useless method, separate method
    Get rid of useless exception handling method created due to misunderstanding
    of objectives. Separate input method and convert-str-to-integer method.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -9,46 +9,6 @@ import camp.nextstep.edu.missionutils.Console;
 import java.lang.IllegalArgumentException;
 
 public class Application {
-
-    static void handleLineException(String[] parsedString){
-        if (parsedString.length != 3)
-            throw new IllegalArgumentException("Too many arguments");
-    }
-
-    static void handleWordException(String word){
-        if (word.length() != 1)
-            throw new IllegalArgumentException("Wrong argument length");
-        if (!Character.isDigit(word.charAt(0)))
-            throw new IllegalArgumentException("Argument is not a number");
-    }
-
-    static void handleZeroException(Integer num){
-        if (num == 0)
-            throw new IllegalArgumentException("Invalid range of input");
-    }
-
-    static Integer castToInteger(String str){
-        Integer num = 0;
-        try {
-            num = Integer.parseInt(str);
-            handleZeroException(num);
-        } catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-        return num;
-    }
-
-    static void handleException(String[] parsedString){
-        try {
-            handleLineException(parsedString);
-            for (String word : parsedString){
-                handleWordException(word);
-            }
-        } catch(Exception e){
-            System.out.println(e.getMessage());
-        }
-    }
-
     static List<Integer> createAnswer(){
         List<Integer> answerList = new ArrayList<>(3); //allocate exact capacity to list
         int randomNum = 0;
@@ -59,47 +19,62 @@ public class Application {
             }
         }
 
-        //for debugging
-        for (Integer i : answerList) {
-            System.out.print(i);
-        }
-        System.out.println();
-        //
-
         return answerList;
     }
 
-    static List<Integer> inputAnswer(){
-        List<Integer> inputList = new ArrayList<>(3);
+    static void checkValidInput(String input){
+        int len = input.length();
+        if (len != 3){
+            throw new IllegalArgumentException("Invalid input");
+        }
+
+        char check;
+        for (int i = 0; i < len; i++) {
+            check = input.charAt(i);
+            if (check < '1' || check > '9') {
+                throw new IllegalArgumentException("Invalid input");
+            }
+        }
+    }
+
+    static String inputAnswer(){
         String inputString = Console.readLine();
-        String[] parsedString = inputString.split(" ");
 
-        //for debugging
-        System.out.println(inputString);
-        for (String str : parsedString) {
-            System.out.println(str);
-        }
-        System.out.println(parsedString.length);
-        //
-
-        handleException(parsedString);
-
-        for (int i = 0; i < 3; i++) {
-            inputList.add(castToInteger(parsedString[i])); //handle 0 input exception here
+        try {
+            checkValidInput(inputString);
+        } catch(Exception e){
+            e.printStackTrace();
         }
 
+        return inputString;
+    }
+
+    static List<Integer> castToInteger(String str){
+        List<Integer> inputList = new ArrayList<>(3);
+        int len = str.length();
+        for (int i = 0; i < len; i++) {
+            inputList.add(Character.getNumericValue(str.charAt(i)));
+        }
         return inputList;
     }
 
     public static void main(String[] args) {
 
-        List<Integer> isStrike = inputAnswer();
+        String inputString = inputAnswer();
+        List<Integer> isStrike = castToInteger(inputString);
         List<Integer> Answer = createAnswer();
 
         //for debugging
+        System.out.print("Input: ");
         for (Integer i : isStrike) {
-            System.out.println("(" + i + ")");
+            System.out.print(i);
         }
+        System.out.println();
+        System.out.print("Answer: ");
+        for (Integer i : Answer) {
+            System.out.print(i);
+        }
+        System.out.println();
         //
     }
 }
