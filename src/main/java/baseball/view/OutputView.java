490d419772942984cd3504b8f41c849026ae4ed3
gomljo <gomljo0307@naver.com>
2022-11-08 19:39:13 +0900

    feat(OutputView.java): 정답 메시지를 출력하는 기능 구현

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,9 @@
+package baseball.view;
+
+public class OutputView {
+    private static final String SUCCESS_MESSAGE="3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public void showSuccessMessage(){
+        System.out.println(SUCCESS_MESSAGE);
+    }
+}
