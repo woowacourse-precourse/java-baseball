001669df069868b3422df0f1ed3644f1aa91ac37
Gentlee <ekdma3259@gmail.com>
2022-11-08 18:23:44 +0900

    임의의 3가지 숫자를 List에 저장하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,6 +10,17 @@ public class Application {
 		System.out.println("숫자 야구 게임을 시작합니다.");
 	}
 	
+	public static List<Integer> computerNumber(){
+		List<Integer> computer = new ArrayList<>();
+		while (computer.size() < 3) {
+		    int randomNumber = Randoms.pickNumberInRange(1, 9);
+		    if (!computer.contains(randomNumber)) {
+		        computer.add(randomNumber);
+		    }
+		}
+		return computer;
+	}
+	
     public static void main(String[] args) {
     	
     }
