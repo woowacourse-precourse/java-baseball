b6888591b05e1102cc124fa97f3161967034176b
WonSeok-dd <cluejws@gmail.com>
2022-11-08 18:24:45 +0900

    feat(UserInput): 올바른 3자리수 입력인지 판단 기능

--- src/main/java/util/UserInput.java
+++ src/main/java/util/UserInput.java
@@ -3,6 +3,51 @@ package util;
 public class UserInput {
 
     public static boolean isValidUserInput(String userInput) {
-        return false;
+
+        return hasNotCharacter(userInput) && isRightLength(userInput) && isRightRange(userInput) && isDiffer(userInput);
+    }
+
+    private static boolean hasNotCharacter(String userInput){
+
+        final String REGEX = "[0-9]+";
+        if (!userInput.matches(REGEX)){
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isRightLength(String userInput){
+
+        if (!(userInput.length() == Constants.NUMBER_LENGTH)){
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean isRightRange(String userInput){
+
+        for (int i=0; i<userInput.length(); i++){
+            int value = Character.getNumericValue(userInput.charAt(i));
+            if (Constants.MIN_NUMBER > value || value > Constants.MAX_NUMBER){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static boolean isDiffer(String userInput){
+
+        for (int i=0; i<userInput.length()-1; i++){
+            int curValue = Character.getNumericValue(userInput.charAt(i));
+            int nextValue = Character.getNumericValue(userInput.charAt(i+1));
+            if (curValue == nextValue){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
