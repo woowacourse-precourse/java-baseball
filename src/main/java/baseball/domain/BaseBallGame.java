bf673538f995d02027e98a37fe21e282ef2ea683
Yohan <sprae114@naver.com>
2022-11-08 17:45:23 +0900

    feat : 야구 게임 매커니즘 구현

---
+++ src/main/java/baseball/domain/BaseBallGame.java
@@ -0,0 +1,29 @@
+package baseball.domain;
+
+import baseball.view.InputView;
+
+public class BaseBallGame {
+    private final UserBall userBall;
+    private final ComputerBall computerBall;
+    private final ThrowBall throwBall;
+
+    public BaseBallGame() {
+        userBall = new UserBall();
+        computerBall = new ComputerBall();
+        throwBall = new ThrowBall();
+    }
+
+    public void playGame(){
+        computerBall.generateComputerNum();
+        userBall.generateUserBall(InputView.inputUserBall());
+
+        while (throwBall.decideBall(computerBall.computerNumber, userBall.userNumber)){
+            userBall.generateUserBall(InputView.inputUserBall());
+        }
+
+        String coin = InputView.printGameStartOrStop();
+        if (coin.equals("1")){
+            playGame();
+        }
+    }
+}
