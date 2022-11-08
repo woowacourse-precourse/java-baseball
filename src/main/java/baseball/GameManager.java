0e8aaf11f474d01135c84e4a2f7a37b6e25d9a0d
고동우 <shp05179@gmail.com>
2022-11-08 19:53:46 +0900

    style: if문 중괄호 추가, 함수 사이에 줄바꿈
    java 컨벤션에 맞추어 줄바꿈과 중괄호 추가

--- src/main/java/baseball/GameManager.java
+++ src/main/java/baseball/GameManager.java
@@ -15,12 +15,14 @@ public class GameManager {
         randomNumberHash = Computer.generateRandomNumber();
     }
 
-    static void setResultHash(HashMap<Character, Integer> newResultHash){
+    static void setResultHash(HashMap<Character, Integer> newResultHash) {
         resultHash = newResultHash;
     }
-    static boolean getIsContinue(){
+
+    static boolean getIsContinue() {
         return isContinue;
     }
+
     static HashMap<Integer, Integer> scanNumber() {
         inputNumberHash.clear();
         System.out.print("숫자를 입력해주세요 : ");
@@ -39,8 +41,11 @@ public class GameManager {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String numberString = Console.readLine();
         ValidCheck.checkExceptionIsContinue(numberString);
-        if (numberString.equals("1")) isContinue = true;
-        else isContinue = false;
+        if (numberString.equals("1")) {
+            isContinue = true;
+        } else {
+            isContinue = false;
+        }
     }
 
     static HashMap<Character, Integer> judgeResult() {
@@ -73,9 +78,15 @@ public class GameManager {
             return resultString;
         }
         for (char SBO : resultKeys) {
-            if (resultHash.get(SBO) == 0) continue;
-            if (SBO == 'B') resultString = resultString + resultHash.get(SBO) + "볼 ";
-            if (SBO == 'S') resultString = resultString + resultHash.get(SBO) + "스트라이크 ";
+            if (resultHash.get(SBO) == 0) {
+                continue;
+            }
+            if (SBO == 'B') {
+                resultString = resultString + resultHash.get(SBO) + "볼 ";
+            }
+            if (SBO == 'S') {
+                resultString = resultString + resultHash.get(SBO) + "스트라이크 ";
+            }
         }
         resultString = resultString.substring(0, resultString.length() - 1);
         System.out.println(resultString);
