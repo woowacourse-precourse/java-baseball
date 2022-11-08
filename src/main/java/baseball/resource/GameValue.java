0c17426c83069aad8e17709e0b09dcbc048a7611
Dabeen Jeong <hello70825@gmail.com>
2022-11-08 23:02:11 +0900

    feat: 상수를 추가하다.

--- src/main/java/baseball/resource/GameValue.java
+++ src/main/java/baseball/resource/GameValue.java
@@ -5,4 +5,7 @@ public class GameValue {
     public static final Character END_NUMBER = '9';
 
     public static final Integer CORRECT_LENGTH = 3;
+
+    public static final Integer ZERO = 0;
+    public static final Integer THREE_STRIKE = 3;
 }
