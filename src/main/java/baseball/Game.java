955a740c0a0f9bef626894dc8a30311d2341fe9b
dingding-21 <phj8221@naver.com>
2022-11-08 20:03:08 +0900

    feat: 3스트라이크까지 게임을 반복하는 기능 추가

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -3,6 +3,7 @@ package baseball;
 import java.util.List;
 
 public class Game {
+    private static final String THREE_STRIKE = "3스트라이크";
     private List<Integer> computerNumber;
 
     private void start(Player computer) {
@@ -19,5 +20,27 @@ public class Game {
         score = GameScore.getGameScore(playerNumber, this.computerNumber);
 
         return GameScore.getGameResult(score.get(0), score.get(1));
+    }
+
+    public void playGameAgain() {
+        String result;
+
+        Player computer = new Player();
+        Player player = new Player();
+
+        start(computer);
+        result = playGame(player);
+        Print.printResult(result);
+
+        while (!checkThreeStrike(result)) {
+            result = playGame(player);
+            Print.printResult(result);
+        }
+
+        Print.checkGameEnd();
+    }
+
+    private boolean checkThreeStrike(String result) {
+        return result.equals(THREE_STRIKE);
     }
 }
