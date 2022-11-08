6242c81442ef56471f122ed547b58ab912e6638e
silver <silver36c@gmail.com>
2022-11-08 22:49:24 +0900

    refactor: BaseballGame 필요없는 코드 삭제

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,13 +1,5 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-import camp.nextstep.edu.missionutils.Console;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class BaseballGame {
 
     BaseballGame() {
