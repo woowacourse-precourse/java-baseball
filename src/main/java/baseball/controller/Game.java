973beebe56e4af6c3e97935fd0c86f0b359ae4cf
baekeunsun <eunsun5460@gmail.com>
2022-11-08 12:52:58 +0900

    fix(controller/Game): 재시작 input을 받으면 게임 반복

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -7,14 +7,21 @@ import baseball.view.OutputView;
 import java.util.ArrayList;
 import java.util.List;
 
+import static baseball.model.Constant.RESTART;
 import static baseball.view.InputView.restartInput;
 
 public class Game {
-    List<Integer> computer = new ComputerGenerate().computer;
+    List<Integer> computer = new ArrayList<>();
     List<Integer> user = new ArrayList<>();
-    int result = 0;
 
     public Game() {
+        do {
+            RunGame(0);
+        } while (ReGame());
+    }
+
+    private void RunGame(int result) {
+        computer = new ComputerGenerate().computer;
         while (result != 3) {
             user = new UserGenerate().user;
             System.out.println(computer);
@@ -22,9 +29,15 @@ public class Game {
             hint.ResultHint();
             result = hint.strike;
         }
-        OutputView.EndGameOutput();
-        restartInput();
     }
 
+
+    private boolean ReGame() {
+        OutputView.EndGameOutput();
+        if (restartInput() == RESTART) {
+            return true;
+        }
+        return false;
+    }
 
 }
--- src/main/java/baseball/model/Constant.java
+++ src/main/java/baseball/model/Constant.java
@@ -2,4 +2,5 @@ package baseball.model;
 
 public final class Constant {
     public static final int NUM_SIZE = 3;
+    public static final int RESTART = 1;
 }
