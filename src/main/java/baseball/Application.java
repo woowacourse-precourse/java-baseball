4d0904372adf8a4d0f9c6dea9d90f1f032b2172e
bfinecpa1@naver.com <bfinecpa1@naver.com>
2022-11-08 16:42:11 +0900

    feat: 게임 시작 기능 구현
    시작시 "숫자 야구 게임을 시작합니다." 출력
    다음에 나올 내용 반복 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.*;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        gameStart();
+    }
+
+    private static void gameStart() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        boolean continueGame = true;
+        while (continueGame) {
+
+        }
     }
 }
