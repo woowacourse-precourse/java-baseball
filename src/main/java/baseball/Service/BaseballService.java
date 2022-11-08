b9c83332d85afefd2d160353595de513db1820a7
jhsseonn <hosu0125@gmail.com>
2022-11-08 20:33:16 +0900

    feat: 입력받은 숫자의 리스트 크기가 3이 아닐 경우 예외처리

--- src/main/java/baseball/Service/BaseballService.java
+++ src/main/java/baseball/Service/BaseballService.java
@@ -8,6 +8,10 @@ import java.util.List;
 public class BaseballService {
 
     private static final int NUMBER_SIZE=3;
+    private static final int RANGE_START=1;
+    private static final int RANGE_END=9;
+    private static final int RESTART=1;
+    private static final int END=2;
 
     //랜덤 숫자 생성
     public List<String> createRandomNumber(){
@@ -15,11 +19,19 @@ public class BaseballService {
         List<String> computerNumber = new ArrayList<>();
 
         for(int i=0; i<NUMBER_SIZE; i++) {
-            computerNumber.add(Integer.toString(Randoms.pickNumberInRange(1, 9)));
+            String number = Integer.toString(Randoms.pickNumberInRange(1, 9));
+            isDuplicate(computerNumber, number);
+            computerNumber.add(number);
         }
 
         return computerNumber;
     }
 
+    //입력받은 숫자의 리스트 크기가 3이 아닐 경우 예외처리
+    public void isValidInputSize(List<String> inputNumber) {
+        if (inputNumber.size()!=NUMBER_SIZE) {
+            throw new IllegalArgumentException();
+        }
+    }
 
 }
