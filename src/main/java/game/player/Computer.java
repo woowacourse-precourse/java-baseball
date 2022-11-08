51a1ded90895c64b6422685fe31630612910379a
ho2n <jungho2n@gmail.com>
2022-11-08 17:52:39 +0900

    refactor: 플레이어 관련 클래스 정의

--- src/main/java/game/player/Computer.java
+++ src/main/java/game/player/Computer.java
@@ -1,80 +1,23 @@
 package game.player;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
-import camp.nextstep.edu.missionutils.Randoms;
+import game.number.ComputerRandomNumbers;
 
 public class Computer {
-	private final List<Integer> computersDifferentThreeNumbers = new ArrayList<>();
-	private int strike;
-	private int ball;
+    ComputerRandomNumbers threeNumbers;
 
-	public Computer() {
-		createThreeNumbers();
-	}
+    public Computer() {
+        createThreeNumbers();
+    }
 
-	private void createThreeNumbers() {
-		for (int i = 0; i < 3; ++i) {
-			addNumber();
-		}
-	}
+    private void createThreeNumbers() {
+        threeNumbers = new ComputerRandomNumbers();
+    }
 
-	private void addNumber() {
-		int randomNumber = Randoms.pickNumberInRange(1, 9);
-		if (!computersDifferentThreeNumbers.contains(randomNumber)) {
-			computersDifferentThreeNumbers.add(randomNumber);
-		}
-	}
+    public boolean contains(int number) {
+        return threeNumbers.contains(number);
+    }
 
-	public boolean computeAndPrintResult(String userNumbers) {
-		compute(userNumbers);
-		printResult();
-		return strike == 3;
-	}
-
-	private void compute(String userNumbers) {
-		String[] userNumbersArr = userNumbers.split("");
-		List<String> userNumbersList = Arrays.asList(userNumbersArr);
-
-		for (int user_index = 0; user_index < 3; ++user_index) {
-			int number = Integer.parseInt(userNumbersList.get(user_index));
-			countBall(number);
-			countStrike(number, user_index);
-		}
-	}
-
-	private void countBall(int number) {
-		if (computersDifferentThreeNumbers.contains(number)) {
-			ball += 1;
-		}
-	}
-
-	public void countStrike(int number, int index) {
-		if (computersDifferentThreeNumbers.contains(number)
-				&& index == computersDifferentThreeNumbers.indexOf(number)) {
-			strike += 1;
-			ball -= 1;
-		}
-	}
-
-	public void resetBallAndStrike() {
-		this.ball = 0;
-		this.strike = 0;
-	}
-
-	private void printResult() {
-		String str = "";
-		if (ball != 0) {
-			str += ball + "볼 ";
-		}
-		if (strike != 0) {
-			str += strike + "스트라이크";
-		}
-		else if (ball == 0) {
-			str += "낫싱";
-		}
-		System.out.println(str);
-	}
+    public int getIndex(int number) {
+        return threeNumbers.getIndex(number);
+    }
 }
---
+++ src/main/java/game/player/User.java
@@ -0,0 +1,14 @@
+package game.player;
+
+import game.number.InputNumbers;
+
+public class User {
+    InputNumbers threeNumbers;
+    public User() {
+        threeNumbers = new InputNumbers();
+    }
+
+    public int getNumber(int index) {
+        return threeNumbers.getNumber(index);
+    }
+}
