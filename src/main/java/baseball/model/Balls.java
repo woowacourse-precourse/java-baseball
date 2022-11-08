906b0873aed250786728b9231bb69eecc996302f
moonstal1506 <moonstal1506@gmail.com>
2022-11-08 19:16:30 +0900

    refactor: 중복코드 제거, 숫자 유효성 검사 추가

--- src/main/java/baseball/model/Balls.java
+++ src/main/java/baseball/model/Balls.java
@@ -1,52 +1,67 @@
 package baseball.model;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Balls {
 
+    private static final String NUMBER_PATTERN = "\\d+";
     public static final int BALLS_SIZE = 3;
 
-    private List<Ball> balls = new ArrayList<>();
+    private final List<Ball> balls;
 
     public Balls(List<Integer> balls) {
         if (balls == null) {
             throw new IllegalArgumentException("볼이 생성되지 않았습니다.");
         }
+        validateSize(balls);
         this.balls = mapToBalls(balls);
     }
 
     private List<Ball> mapToBalls(List<Integer> inputBalls) {
         List<Ball> balls = new ArrayList<>();
         for (int index = 0; index < inputBalls.size(); index++) {
-            balls.add(new Ball(index, inputBalls.get(index)));
+            Ball ball = new Ball(index, inputBalls.get(index));
+            balls.add(ball);
         }
         return balls;
     }
 
-    public Balls(String inputBalls) {
-        String[] splitBalls = inputBalls.split("");
-        validateSize(splitBalls);
-        addBalls(splitBalls);
-    }
-
-    private void validateSize(String[] splitBalls) {
-        if (isValidSize(splitBalls)) {
+    private void validateSize(List<Integer> balls) {
+        if (isValidSize(balls)) {
             throw new IllegalArgumentException("서로 다른 3자리의 수를 입력해주세요.");
         }
     }
 
-    private boolean isValidSize(String[] splitBalls) {
-        return Arrays.stream(splitBalls).collect(Collectors.toSet()).size() != BALLS_SIZE;
+    private boolean isValidSize(List<Integer> balls) {
+        return balls.size() != BALLS_SIZE;
     }
 
-    private void addBalls(String[] splitBalls) {
-        for (int index = 0; index < splitBalls.length; index++) {
-            balls.add(new Ball(index, Integer.parseInt(splitBalls[index])));
+    public Balls(String inputBalls) {
+        this(parseInt(inputBalls));
+    }
+
+    private static List<Integer> parseInt(String inputBalls) {
+        validateNumber(inputBalls);
+        List<Integer> balls = new ArrayList<>();
+        for (String ball : inputBalls.split("")) {
+            addBalls(balls, Integer.parseInt(ball));
         }
+        return balls;
+    }
+
+    private static void validateNumber(String balls) {
+        if (!balls.matches(NUMBER_PATTERN)) {
+            throw new IllegalArgumentException("숫자만 입력해주세요.");
+        }
+    }
+
+    private static void addBalls(List<Integer> balls, int ball) {
+        if (balls.contains(ball)) {
+            throw new IllegalArgumentException("중복된 숫자입니다.");
+        }
+        balls.add(ball);
     }
 
     public Result match(Balls balls) {
@@ -58,7 +73,7 @@ public class Balls {
     }
 
     private BallStatus match(Ball inputBall) {
-        return this.balls.stream()
+        return balls.stream()
                 .map(ball -> ball.match(inputBall))
                 .filter(status -> !status.isNothing())
                 .findFirst()
