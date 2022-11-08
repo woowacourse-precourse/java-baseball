12e9c3a496db417d84513f0b82c3f7036d93dc83
lms0806 <godmlzkf1@naver.com>
2022-11-08 11:44:40 +0900

    feat:사용자 입력 및 예외처리

--- src/main/java/baseball/Play.java
+++ src/main/java/baseball/Play.java
@@ -1,4 +1,16 @@
 package baseball;
 
+import model.User;
+
 public class Play {
+	public void playgame(int[] guessnumber) {
+		int strike = 0, ball = 0;
+		int[] answer = null;
+
+		while (strike != 3) {
+			User user = new User();
+			user.setnumber();
+			answer = user.getnumber();
+		}
+	}
 }
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,36 @@
 package model;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class User {
+	private int[] answer;
+
+	public User() {
+		this.answer = new int[3];
+	}
+
+	public void setnumber() {
+		System.out.print("숫자를 입력해주세요 : ");
+		
+		String input;
+		try {
+			input = Console.readLine();
+			int number = Integer.parseInt(input);
+			
+			answer[0] = number / 100;
+			number %= 100;
+			answer[1] = number / 10;
+			answer[2] = number % 10;
+			
+			if (answer[0] == answer[1] || answer[1] == answer[2] || answer[0] == answer[2]) {
+				throw new IllegalArgumentException();
+			} // 같은수 입력할 시
+		} catch (Exception exception) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	public int[] getnumber() {
+		return this.answer;
+	}
 }
