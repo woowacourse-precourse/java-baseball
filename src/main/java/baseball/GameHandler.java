959f63b769e4289e4f79b847d5cb87445440d68d
seonghbae <fullbloomh3ck@gmail.com>
2022-11-08 21:43:51 +0900

    feat(getResult) : getResult 메서드 구현

--- src/main/java/baseball/GameHandler.java
+++ src/main/java/baseball/GameHandler.java
@@ -5,6 +5,13 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 
 public class GameHandler {
+    private static final String GAME_START = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_END = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_RESTART = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String INSERT_NUMBER = "숫자를 입력해주세요 : ";
+    private static final String NOTHING = "낫싱";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
     public static List<Integer> computerNumber;
     public static List<Integer> inputNumber;
     public static List<Integer> compareResult;
@@ -35,5 +42,23 @@ public class GameHandler {
         }
         compareResult.add(strike);
         compareResult.add(ball);
+    }
+
+    public String getResult() {
+        StringBuilder sb = new StringBuilder();
+        int strike = compareResult.get(0), ball = compareResult.get(1);
+        if(strike == 0 && ball == 0) {
+            sb.append(NOTHING);
+        } else if(strike > 0 && ball > 0) {
+            sb.append(String.format("%d%s %d%s", ball, BALL, strike, STRIKE));
+        } else if(ball > 0) {
+            sb.append(String.format("%d%s", ball, BALL));
+        } else if(strike > 0) {
+            sb.append(String.format("%d%s", strike, STRIKE));
+        }
+        if(strike == 3) {
+            sb.append(String.format("\n%s\n%s\n", GAME_END, GAME_RESTART));
+        }
+        return sb.toString();
     }
 }
