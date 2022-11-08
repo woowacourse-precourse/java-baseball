146663178abd8f16b769248b6aa5245bf5b7e5c7
kssumin <201566@jnu.ac.kr>
2022-11-08 20:16:24 +0900

    refactor : 불필요한 import 정리

--- src/main/java/baseball/Judge.java
+++ src/main/java/baseball/Judge.java
@@ -1,39 +1,36 @@
 package baseball;
 
-import baseball.Player;
-import baseball.Result;
 import baseball.ball.Ball;
-import baseball.ball.Balls;
 import baseball.view.OutputView;
 
 import java.util.List;
 
 public class Judge {
     private List<Ball> computerBalls;
-    private Result result;
+    private Result result=new Result();
 
     public Judge(List<Ball> computerBalls) {
         this.computerBalls = computerBalls;
-        this.result=new Result();
     }
 
     public void compareComputerAndPlayer(List<Ball> playerBalls) {
         result.initBallAndStrike();
         for (int i = 0; i < playerBalls.size(); i++) {
-            computerBalls.contains(playerBalls.get(i));
-            calculateStrikeAndBall(computerBalls, playerBalls, i);
+            if(computerBalls.contains(playerBalls.get(i))) {
+                calculateStrikeAndBall(computerBalls, playerBalls, i);
+            }
         }
-        printGameResult();
+        printGameResult(result);
     }
 
     private void calculateStrikeAndBall(List<Ball> computerBalls, List<Ball> playBalls, int i) {
-        if (computerBalls.get(i) == playBalls.get(i)) {
+        if (computerBalls.get(i).equals(playBalls.get(i))) {
             result.plusStrike();
         }
         result.plusBall();
     }
 
-    private void printGameResult() {
+    private void printGameResult(Result result) {
         OutputView.gameResult(result);
     }
 
