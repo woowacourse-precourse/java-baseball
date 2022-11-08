a3549564b0474c72a96fc0f050da485ea0bc765d
oyoungsun <plusmate@naver.com>
2022-11-08 19:42:17 +0900

    fix : output if 조건 error수정

--- src/main/java/baseball/OutputHandler.java
+++ src/main/java/baseball/OutputHandler.java
@@ -14,17 +14,16 @@ public class OutputHandler {
         if((ball+strike)==0){
             System.out.println("낫싱");
         }
-        //아래에는 모두 0보다 큰 경우다.
-        if(ball==0&&strike<=3){
+        else if(ball==0&&strike<=3){
             System.out.println(strike+"스트라이크");
             if(strike==3){
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             }
         }
-        if(strike==0&&ball<3){
+        else if(strike==0&&ball<3){
             System.out.println(ball+"볼");
         }
-        if(ball>0&&strike>0&&(strike+ball)<=3){
+        else if(ball>0&&strike>0&&(strike+ball)<=3){
             System.out.println(ball+"볼 "+strike+"스트라이크");
         }
     }
