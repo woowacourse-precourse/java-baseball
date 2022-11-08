d407135751c33794e234d4f10ed003fb32f42464
JunHyeon <dlwnsgus07@naver.com>
2022-11-08 19:11:11 +0900

    style: NumberTest 공백, 및 줄 정렬

--- src/test/java/baseball/NumberTest.java
+++ src/test/java/baseball/NumberTest.java
@@ -20,15 +20,16 @@ public class NumberTest {
     UserInputNumberList userInputNumberList;
 
     @BeforeEach
-    void createList(){
+    void createList() {
         computerNumberList = new ComputerNumberList();
         userInputNumberList = new UserInputNumberList();
     }
+
     /**
      * 컴퓨터 랜덤숫자 검증
      */
     @Test
-    void 컴퓨터_숫자가_3자리인지_검증(){
+    void 컴퓨터_숫자가_3자리인지_검증() {
         //given
         computerNumberList.generateNewComputerRandomNumber();
         List<Number> computerNumber = computerNumberList.getComputerNumber();
@@ -39,7 +40,7 @@ public class NumberTest {
     }
 
     @Test
-    void 컴퓨터의_숫자중_중복이_있는지_검증(){
+    void 컴퓨터의_숫자중_중복이_있는지_검증() {
         //given
         computerNumberList.generateNewComputerRandomNumber();
         List<Number> computerNumber = computerNumberList.getComputerNumber();
@@ -55,7 +56,7 @@ public class NumberTest {
      * 사용자 입력 검증
      */
     @Test
-    void 사용자가_정상적으로_값을_입력했을떄_정상적으로_주입(){
+    void 사용자가_정상적으로_값을_입력했을떄_정상적으로_주입() {
         //given
         String userInput;
         //when
@@ -69,46 +70,49 @@ public class NumberTest {
     }
 
     @Test
-    void 사용자가_적절하지_않은_길이의_값을_입력했을떄_예외발생(){
+    void 사용자가_적절하지_않은_길이의_값을_입력했을떄_예외발생() {
         //given
         String userInput;
         //when
         userInput = "1234";
         //then
-        assertThatThrownBy(()->userInputNumberList.setUserInputNumberList(userInput))
+        assertThatThrownBy(() -> userInputNumberList.setUserInputNumberList(userInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("3자리 숫자를 입력해야 합니다.");
     }
+
     @Test
-    void 사용자_중복된_숫자를_입력했을때_예외발생(){
+    void 사용자_중복된_숫자를_입력했을때_예외발생() {
         //given
         String userInput;
         //when
         userInput = "133";
         //then
-        assertThatThrownBy(()->userInputNumberList.setUserInputNumberList(userInput))
+        assertThatThrownBy(() -> userInputNumberList.setUserInputNumberList(userInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("중복된 숫자가 입력되었습니다.");
     }
+
     @Test
-    void 사용자가_유효범위를_벗어난_수를_입력하였을떄_예외발생(){
+    void 사용자가_유효범위를_벗어난_수를_입력하였을떄_예외발생() {
         //given
         String userInput;
         //when
         userInput = "023";
         //then
-        assertThatThrownBy(()->userInputNumberList.setUserInputNumberList(userInput))
+        assertThatThrownBy(() -> userInputNumberList.setUserInputNumberList(userInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("숫자의 범위가 1 미만입니다.");
     }
+
     @Test
-    void 사용자가_숫자가_아닌_문자를_입력하였을떄_예외발생(){
+    void 사용자가_숫자가_아닌_문자를_입력하였을떄_예외발생() {
         //given
         String userInput;
         //when
         userInput = "w12";
         //then
-        assertThatThrownBy(()->userInputNumberList.setUserInputNumberList(userInput))
+        assertThatThrownBy(() -> userInputNumberList.setUserInputNumberList(userInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("입력하신 값이 숫자가 아닙니다.");
     }
