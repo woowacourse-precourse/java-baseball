55639b85b97e41b3d2da640935a9404e33ceb3e8
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-08 15:58:23 +0900

    Update Application.java
    컴퓨터의 랜덤 숫자 생성 함수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,10 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현   
@@ -7,6 +12,19 @@ public class Application {
     }
     
     public static void gameStart() {
+    	List<Integer> computerPickNums = computerPick();
     	
     }
+    
+    public static List<Integer> computerPick() {
+    	List<Integer> computerPickNums = new ArrayList<>();
+    	while (computerPickNums.size() < 3) {
+    	    int randomNumber = Randoms.pickNumberInRange(1, 9);
+    	    if (!computerPickNums.contains(randomNumber)) {
+    	    	computerPickNums.add(randomNumber);
+    	    }
+    	}
+    	return computerPickNums;
+    }
+    
 }
