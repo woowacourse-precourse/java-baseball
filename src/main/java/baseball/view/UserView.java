31d2196c264cf15544f6c8dce8bc4ba9006d3299
hwanee__ <fkfkthth@naver.com>
2022-11-08 17:50:01 +0900

    feat: User뷰 구현 & 숫자 입력 받기
    숫자 입력 멘트 출력과 입력 받기 구현

---
+++ src/main/java/baseball/view/UserView.java
@@ -0,0 +1,18 @@
+package baseball.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class UserView {
+
+    public UserView(){ }
+
+    public String UserAnswer(){
+
+        System.out.print("숫자를 입력해주세요 : ");
+
+        String input = Console.readLine();
+
+        if(숫자 유효성 검사){}
+
+    }
+}
