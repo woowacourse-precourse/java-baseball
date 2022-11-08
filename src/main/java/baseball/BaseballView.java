dcf4900867965156f025f2f507861b13a912e559
jinuk <devuks0820@gmail.com>
2022-11-08 10:32:44 +0900

    refactor: 출력 주석처리 코드 수정
    출력부분 주석처리된 코드 수정

--- src/main/java/baseball/BaseballView.java
+++ src/main/java/baseball/BaseballView.java
@@ -69,7 +69,7 @@ public class BaseballView {
     }
 
     public int continueGame() {
-        //System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
+        System.out.print("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ");
         String str = Console.readLine();
 
         continueValidCheck(str);
