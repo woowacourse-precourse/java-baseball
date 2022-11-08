65577ad1ddd59adc6fddac7925ebc629b98cc6a5
3juhwan <13selfesteem91@naver.com>
2022-11-08 14:13:06 +0900

    test: add verifying user input

---
+++ src/test/java/baseball/UserTest.java
@@ -0,0 +1,31 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+    private static User user;
+
+    @BeforeEach
+    private void createUser() {
+        user = new User();
+    }
+
+    @Test
+    void test() {
+        String input = "123";
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        assertThat(user.getInputNumbers()).isEqualTo(Arrays.asList(1, 2, 3));
+    }
+}
