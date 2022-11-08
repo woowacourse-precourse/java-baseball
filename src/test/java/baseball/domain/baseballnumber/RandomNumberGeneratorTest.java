d074d23b2c2d8b56dae2c4a4dfd71abd2e16cb4d
SeolYoungKim <nasur4da@gmail.com>
2022-11-08 17:52:15 +0900

    test: @Nested 삭제. 우테코 테스트 try

--- src/test/java/baseball/domain/baseballnumber/RandomNumberGeneratorTest.java
+++ src/test/java/baseball/domain/baseballnumber/RandomNumberGeneratorTest.java
@@ -4,31 +4,23 @@ import static baseball.domain.baseballnumber.RandomNumberGenerator.randomNumbers
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
-import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 
 class RandomNumberGeneratorTest {
 
-    @Nested
-    class 정해진_개수의_숫자가_생성된다 {
-        @RepeatedTest(3)
-        void 정해진_개수의_숫자가_생성된다() {
-            List<Integer> baseballNumbers = randomNumbers();
-            assertThat(baseballNumbers.size()).isEqualTo(3);
-        }
+    @RepeatedTest(3)
+    void 정해진_개수의_숫자가_생성된다() {
+        List<Integer> baseballNumbers = randomNumbers();
+        assertThat(baseballNumbers.size()).isEqualTo(3);
     }
 
-    @Nested
-    class 추출한_숫자는_서로_다르다 {
-        @RepeatedTest(3)
-        void 추출한_숫자는_서로_다르다() {
-            List<Integer> baseballNumbers = randomNumbers();
-            long count = baseballNumbers.stream()
-                    .distinct()
-                    .count();
+    @RepeatedTest(3)
+    void 추출한_숫자는_서로_다르다() {
+        List<Integer> baseballNumbers = randomNumbers();
+        long count = baseballNumbers.stream()
+                .distinct()
+                .count();
 
-            assertThat(count).isEqualTo(3);
-        }
+        assertThat(count).isEqualTo(3);
     }
-
 }
