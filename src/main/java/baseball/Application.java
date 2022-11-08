f542cc003b71a08d3ccac760159f2fe49c0fa29d
chaeyeong222 <iky1505@naver.com>
2022-11-08 13:33:30 +0900

    feat : 메인 게임 진행 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,38 +1,74 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
-import java.util.List;
-
-
-
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        int randomNumber = ready();
+        int inputNumber = set();
+        start(randomNumber, inputNumber);
+        int continueCheck = Answer.retry();//재시작 여부
 
-        int randomNumber = GetRandomNumber();
-        System.out.println(randomNumber);
-        System.out.println("숫자 야구 게임을 시작합니다.");
-        System.out.println("숫자를 입력해주세요 : ");
-        int inputNumber = Number.rangeCheck();
-        System.out.println("입력값 : "+ inputNumber);
-
-
-    }
-
-
-    private static int GetRandomNumber() {
-        List<Integer> computer = new ArrayList<>();
-        while (computer.size() < 3) {
-            int randomNumber = Randoms.pickNumberInRange(1, 9);
-            if (!computer.contains(randomNumber)) {
-                computer.add(randomNumber);
+        while (continueCheck ==1){
+            randomNumber = ready();
+            inputNumber = set();
+            start(randomNumber, inputNumber);
+            continueCheck = Answer.retry();
+            if(continueCheck==2){
+                break;
             }
         }
-        int randomNumber = computer.get(0)*100 + computer.get(1)*10 + computer.get(2);
+        //System.out.println("게임이 완전히 종료되었습니다.");
+    }
 
+    private static int start(int randomNumber, int inputNumber) {
+        int strike = 0;
+        while (strike != 3){
+            int [] result = Game.getScore(randomNumber, inputNumber);
+            getScoreMessage(result);
+            strike = result[1];
+            if(strike==3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                break;
+            }
+            inputNumber = keepGoing();
+        }
+        return strike;
+    }
+
+    private static int keepGoing() {
+        System.out.print("숫자를 입력해주세요 : ");
+        int inputNumber = Number.rangeCheck(); //입력한 숫자 예외 확인
+        return inputNumber;
+    }
+
+    private static void getScoreMessage(int[] result) {
+        if(result[0]!=0 && result[1]==0){
+            System.out.println(result[0] + "볼");
+        }
+        if(result[0]!=0 && result[1]!=0){
+            System.out.println(result[0] + "볼 "+result[1]+"스트라이크");
+        }
+        if(result[0]==0 && result[1]!=0){
+            System.out.println(result[1] + "스트라이크");
+        }
+        if(result[0]==0 && result[1]==0){
+            System.out.println("낫싱");
+        }
+    }
+
+    private static int ready() {
+        int randomNumber = GameSetting.getRandomNumber();
+        System.out.println(randomNumber);
+        System.out.println("숫자 야구 게임을 시작합니다.");
         return randomNumber;
     }
+
+    private static int set(){
+        System.out.print("숫자를 입력해주세요 : ");
+        int inputNumber = Number.rangeCheck(); //입력한 숫자 예외 확인
+        return inputNumber;
+    }
+
 
 }
