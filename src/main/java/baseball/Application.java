a4ea39ef352ec346aa38a52cc2ba8e51b47a65ef
sschan99 <seungchan1219@gmail.com>
2022-11-08 17:49:32 +0900

    feat(Application): control the start and end of a game
    1. Start the game when the application is running.
    2. At the end of the game, the player receives either 1 or 2.
    3. If the entered value is 1, restart the game.
    4. If the entered value is 2, exit the application.
    5. If neither, generate 'IllegalArgumentException'.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+    public static class IllegalArgumentException extends RuntimeException {
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while (true) {
+            playGame();
+
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            String userInput = Console.readLine();
+            if (userInput.equals("1")) {
+                continue;
+            }
+            if (userInput.equals("2")) {
+                break;
+            }
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public static void playGame() {
     }
 }
