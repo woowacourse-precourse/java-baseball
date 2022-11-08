93347427dfca8387e6397e8a5ccf1bb931d91df1
meunrud <y3a0522@gmail.com>
2022-11-08 13:26:37 +0900

    :art: 유저 입력 숫자 리스트 초기화

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -37,6 +37,7 @@ public class Game {
             ball = 0;
             getScore();
             getHintMessage();
+            inputNumberList.clear();
         }while(!getWin());
         System.out.println("3개의 숫자를 모두 맞히셨습니다!. 게임종료");
     }
