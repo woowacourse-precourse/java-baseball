be873184fa305627bd490f00f0e880d89f57c7df
Jung <anes53027@gmail.com>
2022-11-08 22:43:24 +0900

    feat(Game) : startGame 게임을 시작하고 진행하는 함수 생성

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,5 +1,12 @@
 package baseball.domain;
 
+import baseball.view.InputView;
+
+import java.util.List;
+
+import static baseball.view.OutputView.gameRestartOrStop;
+import static baseball.view.OutputView.gameResult;
+
 public class Game {
     private final Computer computer;
     private final User user;
@@ -9,5 +16,27 @@ public class Game {
         user = new User();
         computer = new Computer();
         referee = new Referee();
+    }
+
+    public void startGame(){
+        computer.createThreeNumbers();
+        while (true){
+            List<Integer> computerList = computer.getComputer();
+            int userNumber = user.stringToInteger(InputView.getInput());
+
+            List<Integer> userList = user.reverseListOrder(user.numbersToList(userNumber));
+
+            referee.initBallAndStrike();
+
+            int strike = referee.countStrike(computerList, userList);
+            int ball = referee.countBall(computerList, userList);
+
+            String gameResult = gameResult(ball, strike);
+            System.out.println(gameResult);
+
+            if(gameRestartOrStop(gameResult)){
+                break;
+            }
+        }
     }
 }
