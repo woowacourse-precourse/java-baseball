4cbe2a1d333e81ebc01dd40f29bf33e96c7f62ca
seonghoo1217 <seonghoo1217@naver.com>
2022-11-08 12:24:25 +0900

    fix : modify check duplication number method
    It's wrong logical service feature and I fix this never duplicate
    
    Fix(commit_id) : #c53dcfc

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -11,7 +11,7 @@ public class RandomNumber {
         for (int i=0;i<3;i++){
             int random_number = Randoms.pickNumberInRange(1, 9);
             if (random_number_list.contains(random_number)){
-                int sub_random = nonPermissionExistNumber(random_number);
+                int sub_random = nonPermissionExistNumber(random_number,random_number_list);
                 random_number_list.add(sub_random);
             }else {
                 random_number_list.add(random_number);
@@ -20,12 +20,12 @@ public class RandomNumber {
         return random_number_list;
     }
 
-    private static int nonPermissionExistNumber(int random_number){
-        int sub_random=Randoms.pickNumberInRange(1,9);
-        while (sub_random!=random_number){
-            sub_random= Randoms.pickNumberInRange(1, 9);
-
+    private static int nonPermissionExistNumber(int random_number,List<Integer> random_number_list){
+        int sub_random;
+        while (random_number_list.contains(random_number)){
+            random_number=Randoms.pickNumberInRange(1, 9);
         }
+        sub_random=random_number;
         return sub_random;
     }
 }
