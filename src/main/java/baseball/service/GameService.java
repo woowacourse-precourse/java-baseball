d9852bf9ef9ed57f30c44906603603a2bdbe81c5
권도현 <tobeykwon@gmail.com>
2022-11-08 21:38:49 +0900

    feat : 사용자 숫자 입력 유효성 검증 기능 추가
    - 입력한 숫자 범위 1~9 체크
    - 입력한 숫자가 3개인지 체크
    - 입력한 숫자 중복 체크

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -2,11 +2,26 @@ package baseball.service;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class GameService {
 
     public List<Integer> makeRandomNumbers() {
         return Randoms.pickUniqueNumbersInRange(1, 9, 3);
+    }
+
+    public String validateUserInput(String userInput) {
+        userInput = userInput.trim();
+
+        boolean matches = userInput.matches("[1-9]{3}");
+        Set<String> collect = Arrays.asList(userInput.split("")).stream().collect(Collectors.toSet());
+
+        if(!matches || collect.size() != 3)
+            throw new IllegalArgumentException();
+        else
+            return userInput;
     }
 }
