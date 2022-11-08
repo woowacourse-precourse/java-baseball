445d45938e76d3fdf1d433c76d47d947338c778b
ShinYeaChan <88381563+YeaChan05@users.noreply.github.com>
2022-11-08 15:30:30 +0900

    valueToList 함수를 통해 입력값을 List로 변경하도록 함수 작성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,30 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         do{
             while(true){//strickCount!=3
                 //input
+                List<Integer> input = valueToList(Integer.parseInt(Console.readLine()));
                 //count strike and ball
             }
             //end game
         }while(true);//replay or exit
+    }
+
+    public static List<Integer> valueToList(int value){
+        List<Integer> list = new ArrayList<>();
+
+        while (value / 10 != 0) {
+            list.add(0, value % 10);
+            value /= 10;
+        }
+        list.add(0, value % 10);
+        return list;
     }
 }
