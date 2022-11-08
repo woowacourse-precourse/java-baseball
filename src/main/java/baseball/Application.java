b2b4dcee98d2a245fc7c82dc08ae9c6272bce41e
seonghbae <fullbloomh3ck@gmail.com>
2022-11-08 22:21:30 +0900

    feat(main) : main 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,15 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GameHandler gameHandler = new GameHandler();
+        gameHandler.init();
+        while(true) {
+            if(gameHandler.isGameEnd && gameHandler.askEnd()) {
+                break;
+            }
+            gameHandler.inputHandler();
+            gameHandler.compareNumber();
+            gameHandler.getResult();
+        }
     }
 }
