b9e82fb96186e379beaf12d50e1c60118f38e929
yoonjoonseok <yjsygm@naver,com>
2022-11-08 16:04:48 +0900

    숫자 확인용 출력 문자열 줄바꿈 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -63,6 +63,7 @@ public class Application{
     		System.out.print(ballCount+"볼 ");
     	if(strikeCount>0)
     		System.out.print(strikeCount+"스트라이크 ");
+    	System.out.println();
     	
     	return strikeCount;	
     }
