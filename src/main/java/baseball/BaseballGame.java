aae9f708e254554870c18f69fe92301554553089
silver <silver36c@gmail.com>
2022-11-08 23:10:24 +0900

    refactor: 게임 진행 관련 비슷한 함수 이름 수정
    preceed, play 함수 이름으로 차이를 구분하기 어려워 좀 더 직관적인 이름으로 수정
    
    BREAKING CHANGE: preceed는  preceedEntireGame로, play는 playOneGame로 이름 수정

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -9,16 +9,16 @@ public class BaseballGame {
         this.player = player;
     }
 
-    public void preceed() {
+    public void preceedEntireGame() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         do {
-            play();
+            playOneGame();
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         } while (player.continueGame());
         finishEntireGame();
     }
 
-    void play() {
+    void playOneGame() {
         computer.ready();
         while (!computer.isGameEnd) {
             System.out.print("숫자를 입력해주세요 : ");
