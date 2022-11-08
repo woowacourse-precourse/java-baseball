c2069da6fb474e37f7ce7f1bab1be0d505822661
ohhonggi <5ghdrl@naver.com>
2022-11-08 20:08:50 +0900

    feat($BaseballGame): Implement gameStart function
    This function starts the number baseball game and
    starts the new number baseball game if the user wants
    to start the new number baseball game after the number baseball game
    
    This function uses two functions within the same class
    - startNewGame
    - restart
    
    Closes feat_6

--- src/main/java/baseball/service/BaseballGame.java
+++ src/main/java/baseball/service/BaseballGame.java
@@ -32,4 +32,11 @@ public class BaseballGame {
             return false;
         }
     }
+
+    public static void gameStart() {
+        do {
+            System.out.println("숫자 야구 게임을 시작합니다.");
+            startNewGame();
+        } while (restart());
+    }
 }
