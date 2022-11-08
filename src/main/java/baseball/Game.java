8075249f9ed4d8f0b70b909475a7002656e633eb
dlgosla <dlgoslaek1@gmail.com>
2022-11-08 18:05:15 +0900

    feat: 컴퓨터 숫자 생성

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,9 +1,22 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Game {
 
     int MAX_SCORE = 3;
 
 
+    private List<Integer> getRandomNumbers() {
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < MAX_SCORE) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
 }
