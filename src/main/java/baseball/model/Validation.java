cc0c4be1a2144196c1cd452c99bc7e967e869a16
hwanee__ <fkfkthth@naver.com>
2022-11-08 18:11:29 +0900

    feat: 숫자 1~9 유효성 체크
    숫자의 범위 유효성 체크

--- src/main/java/baseball/model/Validation.java
+++ src/main/java/baseball/model/Validation.java
@@ -1,13 +1,20 @@
 package baseball.model;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class Validation {
     private static final int CNT_NUMBER = 3;
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
+    private static final String Number_RegEx = "^[" + MIN_NUMBER + "-" + MAX_NUMBER + "]*$";
 
     public Validation(){}
+
+    private boolean CheckMinToMax(String input){
+        Pattern numberPattern = Pattern.compile(Number_RegEx);
+        return numberPattern.matcher(input).matches();
+    }
 
     private boolean CheckDuplicate(String input){
         return Arrays.stream(input.split(""))
