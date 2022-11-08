98113834af841c41d1eb60d82989e59d0d8d181c
kyokugirl <kjeaeun97@gmail.com>
2022-11-08 12:52:42 +0900

    기능 : 정답 생성

---
+++ src/main/java/baseball/compute/computer.java
@@ -0,0 +1,20 @@
+package baseball.compute;
+
+import java.util.*;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+
+public class computer {
+    private List<Integer> makeRandom() {
+        List<Integer> randomnumbers = new ArrayList<>();
+        while (randomnumbers.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!randomnumbers.contains(randomNumber)) {
+                randomnumbers.add(randomNumber);
+            }
+        }
+        return randomnumbers;
+    }
+    
+}
