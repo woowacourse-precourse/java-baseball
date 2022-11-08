c5d51da7def72723ba7c15ce2656b7006260d529
masiljangajji <hjk172262@gmail.com>
2022-11-08 22:52:44 +0900

    feat: 사용자 입력에 예외처리

--- src/main/java/baseball/Exception.java
+++ src/main/java/baseball/Exception.java
@@ -1,2 +1,18 @@
-package baseball;public class Exception {
+package baseball;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Exception {
+
+
+
+    public void checkLength(String userBaseBallNumber) throws IllegalArgumentException {
+        if (userBaseBallNumber.length() != 3) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+
 }
