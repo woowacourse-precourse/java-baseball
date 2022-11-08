7fcf2a72292424866963419968fde2925bd4543f
ddukddaki <rlfrkdms1@naver.com>
2022-11-08 17:30:47 +0900

    feat(HintTest): 볼인지 확인하는 isBall 확인 테스트

--- src/test/java/baseball/HintTest.java
+++ src/test/java/baseball/HintTest.java
@@ -24,4 +24,18 @@ class HintTest {
         assertThat(strike_false).isFalse();
     }
 
+    @Test
+    public void 볼_확인() throws Exception {
+        //given
+        List<Integer> computerNumber = List.of(1, 3, 5);
+        int userNumber_true = 3;
+        int userNumber_false = 2;
+        //when
+        boolean ball_true = hint.isBall(computerNumber, userNumber_true);
+        boolean ball_false = hint.isBall(computerNumber, userNumber_false);
+        //then
+        assertThat(ball_true).isTrue();
+        assertThat(ball_false).isFalse();
+    }
+
 }
