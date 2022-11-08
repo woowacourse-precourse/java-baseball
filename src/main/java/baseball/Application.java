47315439e56b30d34d6652ee2e40d0cae8b92f02
ye1ne <yewon7036@naver.com>
2022-11-08 15:01:06 +0900

    feature : checkUserFinishGame method
    check user want to continue game,
    if NOT want, finish game

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,12 +10,30 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
-        List<Integer> resultNumber = makeRandomNumber();
-        String inputStringNumber = Console.readLine();
-        List<Integer> inputNumber = makeStringNumberToList(inputStringNumber);
-        calculationNumber(resultNumber, inputNumber);
+        while (true) {
+            List<Integer> resultNumber = makeRandomNumber();
+            while (true) {
+                String inputStringNumber = Console.readLine();
+                List<Integer> inputNumber = makeStringNumberToList(inputStringNumber);
+                boolean findNumber = calculationNumber(resultNumber, inputNumber);
+                if (findNumber) {
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
+                }
+            }
+            boolean isFinish = checkUserFinishGame();
+            if (isFinish) break;
+        }
+    }
 
-
+    private static boolean checkUserFinishGame() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String inputString= Console.readLine();
+        if(inputString.equals("1")) {
+            System.out.println("게임을 새로 시작하겠습니다.");
+            return false;
+        }
+        return true;
     }
 
     public static boolean calculationNumber(List<Integer> resultNumber ,List<Integer> inputNumber) {
@@ -23,9 +41,9 @@ public class Application {
         int ball = countBall(resultNumber, inputNumber);
         printResult(strike, ball);
         if(strike == 3){
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 
     private static int countBall(List<Integer> resultNumber, List<Integer> inputNumber) {
@@ -56,7 +74,7 @@ public class Application {
         }else if(ball == 0){
             System.out.println(strike + "스트라이크");
         }else{
-            System.out.println(strike + "스트라이크 " + ball + "볼");
+            System.out.println(ball + "볼 " + strike + "스트라이크 ");
         }
     }
 
