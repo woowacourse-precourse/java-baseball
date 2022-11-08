1cbce47d11a9304542901dde3eb1e702c8e87d71
kbm6609 <kbm6609@nvaer.com>
2022-11-08 22:32:45 +0900

    feat : getRandomValue() 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+
+    }
+
+    static int getRandomValue() {
+        Set<Integer> set=new HashSet<>();
+        int result = 0;
+        for (int i = 0; i < 3; i++) {
+            int random = Randoms.pickNumberInRange(1, 9);
+            if (set.contains(random)) {
+                i--;
+            }else{
+                result = result * 10 + random;
+                set.add(random);
+            }
+        }
+        return result;
     }
 }
