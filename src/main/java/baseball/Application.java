f2a38a77c9f28bbb038df6c46201464fc25102b8
jaewoo <wjdwodn0722@naver.com>
2022-11-08 19:17:15 +0900

    feat : add measure number and print result function
    - 결과 출력 메서드 추가
    - 스트라이크 개수 판별 메서드 추가
    - 볼 개수 판별 메서드 추가
    - 볼 개수를 세기 위한 다른 위치의 숫자 비교 메서드 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -13,38 +13,45 @@ public class Application {
 
 class Game {
 
-    public void startGame() {
-        char[] radomNumber = makeRandomNumbers();
-        char[] inputNubmer = inputNumber();
-        System.out.println(radomNumber);
-        System.out.println(inputNubmer);
+    public static void startGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        char[] randomNumbers = makeRandomNumbers();
+        char[] inputNumbers = inputNumber();
+        System.out.println(inputNumbers);
+        System.out.println(randomNumbers);
+        printResult(inputNumbers, randomNumbers);
     }
 
-    public void endGame() {
+    public static void endGame() {
 
     }
 
     public static char[] makeRandomNumbers() {
         char[] randomNumber = new char[3];
         int[] numbers = new int[3];
+
         for (int i = 0; i < 3; i++) {
             numbers[i] = makeARandomNumber(numbers);
         }
+
         for (int i = 0; i < 3; i++) {
-            randomNumber[i] = (char)('0' + numbers[i]);
+            randomNumber[i] = (char) ('0' + numbers[i]);
         }
+
         return randomNumber;
     }
 
     // 중복을 확인해 숫자 한개 생성
     public static int makeARandomNumber(int[] numbers) {
         int num = 0;
+
         while (true) {
             num = Randoms.pickNumberInRange(1, 9);
             if (!isInNumbers(numbers, num)) {
                 break;
             }
         }
+
         return num;
     }
 
@@ -57,9 +64,12 @@ class Game {
     }
 
     public static char[] inputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
         String inputString = Console.readLine();
+
         isValidLength(inputString);
         isValidNumber(inputString);
+
         return inputString.toCharArray();
     }
 
@@ -77,4 +87,54 @@ class Game {
         }
     }
 
+    public static void printResult(char[] inputNumber, char[] randomNumber) {
+        int ballNumber = countBall(inputNumber, randomNumber);
+        int strikeNumber = countStrike(inputNumber, randomNumber);
+
+        if (ballNumber + strikeNumber == 0) {
+            System.out.println("낫싱");
+        } else if (strikeNumber == 3) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            endGame();
+        } else {
+            if (ballNumber > 0)
+                System.out.printf("%d볼", ballNumber);
+            if (strikeNumber > 0)
+                System.out.printf("%d스트라이크", strikeNumber);
+            System.out.println("");
+        }
+    }
+
+    public static int countStrike(char[] inputNumber, char[] randomNumber) {
+        int strikeNumber = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (inputNumber[i] == randomNumber[i])
+                strikeNumber++;
+        }
+
+        return strikeNumber;
+    }
+
+    public static int countBall(char[] inputNumber, char[] randomNumber) {
+        int ballNumber = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (compareOtherPositionNumber(inputNumber, randomNumber, i))
+                ballNumber++;
+        }
+
+        return ballNumber;
+    }
+
+    public static boolean compareOtherPositionNumber(char[] inputNumber, char[] randomNumber, int index) {
+        for (int i = 0; i < 3; i++) {
+            if (i == index)
+                continue;
+            if (inputNumber[index] == randomNumber[i])
+                return true;
+        }
+
+        return false;
+    }
 }
