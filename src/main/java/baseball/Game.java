b29d13203a63f8da6c1f1093c76c9647516598e4
smc2315 <choeyongtae085@gmail.com>
2022-11-08 22:16:30 +0900

    feat: Game

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -1,16 +1,62 @@
 package baseball;
 
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 import java.util.List;
 
 public class Game {
-    Computer computer = new Computer();
-    private List<Integer> ComputerNumbers;
+    private Computer computer;
+    private List<Integer> computerNumbers;
     public Game(){
+        computer = new Computer();
+    }
+    public void start(){
         System.out.println("숫자 야구 게임을 시작합니다.");
         getComputerNumbers();
+        guessNumbers();
     }
+
     public void getComputerNumbers(){
         computer.selectNumbers();
-        ComputerNumbers = computer.getSelectedNum();
+        computerNumbers = computer.getSelectedNum();
     }
+
+    public void guessNumbers(){
+        String userNumber;
+        do{
+            player.selectNumber();
+            userNumber = player.getSelectedNumber();
+            Validate.validateInputNumber();
+        }
+        while(judge(userNumber));
+    }
+
+    public boolean judge(String num){
+        int cntBall=0;
+        int cntStrike = 0;
+        for(int i=0;i<3;i++){
+            int cur = num.charAt(i)-'0';
+            if(cur == computerNumbers.get(i)){
+                cntStrike++;
+            }
+            else if(computerNumbers.contains(cur)){
+                cntBall++;
+            }
+        }
+        printJudgeResult(cntBall,cntStrike);
+        return(cntStrike!=3);
+
+    }
+    public void printJudgeResult(int cntBall, int cntStrike){
+        if(cntBall+cntStrike==0){
+            System.out.println("낫싱");
+        }
+        else if(cntBall==0){
+            System.out.println(cntStrike+"스트라이크");
+        }
+        else{
+            System.out.println(cntBall+"볼 "+cntStrike+"스트라이크");
+        }
+    }
+
 }
