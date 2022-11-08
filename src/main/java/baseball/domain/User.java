b144d0e84af00402d9ec868887633278f3f13f7b
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-08 13:42:42 +0900

    feat(User): Singleton & read user input

---
+++ src/main/java/baseball/domain/User.java
@@ -0,0 +1,25 @@
+package baseball.domain;
+
+import baseball.util.ListGenerator;
+import baseball.validator.InputValidator;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
+public class User {
+    private static User user = new User();
+    User(){
+
+    }
+    public static User getInstance(){
+        return user;
+    }
+    public List<Integer> inputUserAnswer() throws IllegalArgumentException {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine().trim();
+        List<Integer> userInputs = ListGenerator.stringToIntegerList(input);
+        if (! InputValidator.validateInPlaying(userInputs))
+            throw new IllegalArgumentException("서로 다른 3자리 수를 입력해주세요.");
+        return userInputs;
+    }
+}
