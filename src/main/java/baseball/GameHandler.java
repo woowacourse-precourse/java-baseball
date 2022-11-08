9078f885dbb1fcbf6d5783a7e54ad7b4e54c671a
seonghbae <fullbloomh3ck@gmail.com>
2022-11-08 21:21:08 +0900

    feat(inputHandler) : inputHandler 메서드 구현

--- src/main/java/baseball/GameHandler.java
+++ src/main/java/baseball/GameHandler.java
@@ -2,13 +2,24 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 public class GameHandler {
     public static List<Integer> computerNumber;
+    public static List<Integer> inputNumber;
     public static List<Integer> compareResult;
 
     public void init() {
         computerNumber = new ArrayList<>();
+        inputNumber = new ArrayList<>();
         compareResult = new ArrayList<>();
+    }
+
+    public void inputHandler() {
+        String input = Console.readLine();
+        // validation check
+        for(char c : input.toCharArray()) {
+            inputNumber.add(c - '0');
+        }
     }
 }
