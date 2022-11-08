d9cd481061e40987ed9a1676b6df9f2e6e25a14c
yujeongkwon <yoojeongkwon@naver.com>
2022-11-08 21:31:03 +0900

    refactor(Output) : 모든 함수 private에서 public으로

--- src/main/java/baseball/view/Output.java
+++ src/main/java/baseball/view/Output.java
@@ -10,25 +10,25 @@ public class Output {
         Noting(hintReq);
     }
 
-    private static void OnlyBall(HintReq hintReq) {
+    public static void OnlyBall(HintReq hintReq) {
         if (hintReq.getBallCount() != 0 && hintReq.getStrikeCount() == 0) {
             System.out.println(hintReq.getBallCount() + "볼");
         }
     }
 
-    private static void OnlyStrike(HintReq hintReq) {
+    public static void OnlyStrike(HintReq hintReq) {
         if (hintReq.getBallCount() == 0 && hintReq.getStrikeCount() != 0) {
             System.out.println(hintReq.getStrikeCount() + "스트라이크");
         }
     }
 
-    private static void BallAndStrike(HintReq hintReq) {
+    public static void BallAndStrike(HintReq hintReq) {
         if (hintReq.getBallCount() != 0 && hintReq.getStrikeCount() != 0) {
             System.out.println(hintReq.getBallCount() + "볼 " + hintReq.getStrikeCount() + "스트라이크");
         }
     }
 
-    private static void Noting(HintReq hintReq) {
+    public static void Noting(HintReq hintReq) {
         if (hintReq.getBallCount() == 0 && hintReq.getStrikeCount() == 0) {
             System.out.println("낫싱");
         }
