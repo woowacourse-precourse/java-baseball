a588204e55bf7ca0e5eb09012c4b082c19f06977
nn98 <jkllhgb@gmail.com>
2022-11-08 20:21:48 +0900

    Feat: 게임 종료 상황에서 오류 발생 시 출력할 메시지 추가

--- src/main/java/constant/Texts.java
+++ src/main/java/constant/Texts.java
@@ -13,7 +13,8 @@ public class Texts {
     public static final String USER_INPUT_ILLEGAL_DIGIT = "입력된 수가 3자리가 아닙니다. ";
     public static final String USER_INPUT_ILLEGAL_DUPLICATED = "입력된 수에 중복값이 존재합니다. ";
     public static final String USER_INPUT_ILLEGAL_ZERO = "입력된 수에 0이 존재합니다. ";
-    public static final String USER_INPUT_ILLEGAL_NOTICE = "1에서 9까지의 서로 다른 임의의 수 3개를 입력해주세요.";
+    public static final String USER_INPUT_ILLEGAL_NOTICE = "1에서 9까지의 서로 다른 임의의 수 3개를 입력해주세요. ";
+    public static final String USER_INPUT_ILLEGAL_NOT_1_OR_2 = "입력된 수가 1 또는 2가 아닙니다. ";
 
     public static final String CHECKER_RESULT_BALL = "볼";
     public static final String CHECKER_RESULT_STRIKE = "스트라이크";
