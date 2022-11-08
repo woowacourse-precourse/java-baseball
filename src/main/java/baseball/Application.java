465f00e385cea35f377673394edf68ebeed7dbe8
JungYoonshin <sju08227@naver.com>
2022-11-08 20:11:13 +0900

    feat: 사용자로부터 입력받는 숫자가 유효한 숫자인지 확인하는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,7 +3,9 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Application {
 
@@ -13,18 +15,20 @@ public class Application {
 
         System.out.println("숫자 야구 게임을 시작합니다.");
 
-
-        while(true){
+        while (true) {
             randomNumberByComputer = generateRandomNumber();
             System.out.print("숫자를 입력해주세요 : ");
             randomNumberByUser = inputNumberByUser();
 
+            if (checkException(randomNumberByUser)) {
+                exitGame();
+            }
 
         }
 
     }
 
-    private static List<Integer> generateRandomNumber(){
+    private static List<Integer> generateRandomNumber() {
         List<Integer> computer = new ArrayList<>();
 
         while (computer.size() < 3) {
@@ -37,13 +41,34 @@ public class Application {
         return computer;
     }
 
-    private static List<Integer> inputNumberByUser(){
+    private static List<Integer> inputNumberByUser() {
         List<Integer> inputNumber = new ArrayList<>();
         String num = Console.readLine();
 
-        for(int i = 0; i <num.length(); i++){
+        for (int i = 0; i < num.length(); i++) {
             inputNumber.add((int) num.charAt(i));
         }
         return inputNumber;
     }
+
+    private static Boolean checkException(List<Integer> inputNum) {
+        if (inputNum.size() != 3) {
+            return true;
+        }
+        Set<Integer> numSet = new HashSet<>(inputNum);
+        if (inputNum.size() != numSet.size()) {
+            return true;
+        }
+        for (int n : inputNum) {
+            if (n < 1 || n > 9) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static void exitGame(){
+        throw new IllegalArgumentException();
+    }
+
 }
