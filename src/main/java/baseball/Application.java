2042fddb2b85b33afdf91b9e2735e7eef7c4adc7
poter0501 <poter0501@gmail.com>
2022-11-08 15:20:33 +0900

    feat: 스트라이크와 볼의 개수를 입력 받아 결과를 출력해주는 기능 구현
    함수의 이름은 printComparisonResult이며, 스트라이크와 볼의 개수를 받아 비교 결과를 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.*;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -10,10 +11,12 @@ public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
     }
-    private static void printStartText(String txt){
+
+    private static void printStartText(String txt) {
         System.out.println(txt);
     }
-    private static List<Integer> createRandomNumber(){
+
+    private static List<Integer> createRandomNumber() {
         List<Integer> computer = new ArrayList<>();
         while (computer.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
@@ -23,36 +26,58 @@ public class Application {
         }
         return computer;
     }
-    private static void printAskInputText(String txt){
+
+    private static void printAskInputText(String txt) {
         System.out.printf(txt);
     }
-    private static String getInput(){
-        String input="";
+
+    private static String getInput() {
+        String input = "";
         input = Console.readLine();
         return input;
     }
-    private static void ableToParseInt(String input){
+
+    private static void ableToParseInt(String input) {
         try {
-            int temp=0;
-            temp=Integer.parseInt(input);
-        }catch (Exception e){
+            int temp = 0;
+            temp = Integer.parseInt(input);
+        } catch (Exception e) {
             IllegalArgumentException exception = new IllegalArgumentException();
             throw exception;
         }
     }
-    private static void checkInputThreeDifferentNumber(List<Integer> numbers){
+
+    private static void checkInputThreeDifferentNumber(List<Integer> numbers) {
         Set<Integer> numberSet = new HashSet<>(numbers);
-        if (numbers.size()!=3 || numberSet.size()!=3){
+        if (numbers.size() != 3 || numberSet.size() != 3) {
             IllegalArgumentException exception = new IllegalArgumentException();
             throw exception;
         }
     }
-    private static void checkInputOneOrTwo(String input){
-        int inputCode=0;
+
+    private static void checkInputOneOrTwo(String input) {
+        int inputCode = 0;
         ableToParseInt(input);
         inputCode = Integer.parseInt(input);
-        if (!(inputCode==1 || inputCode==2) ){
+        if (!(inputCode == 1 || inputCode == 2)) {
             throw new IllegalArgumentException();
         }
+    }
+
+    private static void printComparisonResult(int strikeCount, int ballCount) {
+        String printText = "";
+        String ballCountText = "";
+        String strikeCountText = "";
+        if (strikeCount == 0 && ballCount == 0) {
+            printText = "낫싱";
+        }
+        if (ballCount != 0) {
+            ballCountText = Integer.toString(ballCount) + "볼";
+        }
+        if (strikeCount != 0 && strikeCount != 3) {
+            strikeCountText = Integer.toString(ballCount) + "스트라이크";
+        }
+        printText = printText + ballCountText + " " + strikeCountText;
+        System.out.println(printText);
     }
 }
