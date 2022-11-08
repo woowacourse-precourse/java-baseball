77915b3aeed5b262f88e92872cae97f03c4eb0bc
romanticker <romanticker54@naver.com>
2022-11-08 20:54:12 +0900

    2번째 랜덤숫자뽑기 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,20 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        GenerateRandomNum randomNum = new GenerateRandomNum(); //랜덤숫자뽑기
+        Input input = new Input();//
+        Judge judge = new Judge();
+        Playagain playagain = new Playagain();
+        boolean again = true;
+
+        while (again){
+            List<Integer> computer = randomNum.create();
+            String result = "";
+            while (!result.equals("3스트라이크")){
+                result = judge.judgement(computer, input.playerNumber());
+                System.out.println(result);
+            }
+            again = playagain.playagain();
+        }
     }
 }
