448c78c16f260361f23d1a57259727e78f50071e
유미 <yumi@yumis-MacBook-Air.local>
2022-11-08 21:46:25 +0900

    코드 개선

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,10 +3,10 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        while(true){
+        while (true) {
             BaseballGame game = new BaseballGame();
             game.run();
-            if(!game.run()){
+            if (!game.run()) {
                 break;
             }
         }
--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,42 +1,45 @@
 package baseball;
+
 import camp.nextstep.edu.missionutils.Randoms;
 import camp.nextstep.edu.missionutils.Console;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.*;
 
 public class BaseballGame {
-    static private List<Character> number;
-    static public Boolean gameRestart = false;
+    private List<Character> number;
+    public Boolean gameRestart = false;
 
-    public boolean run(){
+    public boolean run() {
         // TODO 1. 랜덤 숫자 생성
         generateNumber();
 
         boolean threeStrike = false;
-        while(!threeStrike){
+        while (!threeStrike) {
             // TODO 2-1. 사용자 입력 받기
             System.out.print("숫자를 입력해주세요 : ");
-            String newInput = Console.readLine();
+
+            String newInput = new String(Console.readLine());
             // TODO 2-2. 예외 처리하기
             checkInputFormat(newInput);
             // TODO 2-3. 스트라이크, 볼 갯수 계산
-            // TODO 2-4. 결과 출력
-            if(strikeAndBall(newInput)){
+            if (strikeAndBall(newInput)) {
                 threeStrike = true;
                 System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                 System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
                 // TODO 3. 게임을 종료한 후 게임을 다시 시작할건지 종료할건지 선택하는 부분
-                gameRestart= checkRestartGame();
+                gameRestart = checkRestartGame();
             }
         }
-
+        System.out.println(gameRestart);
         return gameRestart;
     }
 
-    public void generateNumber(){
+    private void generateNumber() {
         List<Character> computer = new ArrayList<>();
         while (computer.size() < 3) {
-            char randomNumber = (char) (Randoms.pickNumberInRange(1, 9)+48);
+            char randomNumber = (char) (Randoms.pickNumberInRange(1, 9) + 48);
             if (!computer.contains(randomNumber)) {
                 computer.add(randomNumber);
             }
@@ -44,20 +47,20 @@ public class BaseballGame {
         this.number = computer;
     }
 
-    public void checkInputFormat(String newInput){
-        if(newInput.length() != 3){
+    private void checkInputFormat(String newInput) {
+        if (newInput.length() != 3) {
             throw new IllegalArgumentException("userInput is index out of range: 3");
         }
-        for (int i = 0; i < 3; i++){
-            if( newInput.charAt(i) < '1' || newInput.charAt(i) > '9') {
+        for (int i = 0; i < 3; i++) {
+            if (newInput.charAt(i) < '1' || newInput.charAt(i) > '9') {
                 throw new IllegalArgumentException("userInput is not configured from number : 1 o 9");
-            }else if(newInput.charAt(i) == newInput.charAt((i+1)%3)) {
+            } else if (newInput.charAt(i) == newInput.charAt((i + 1) % 3)) {
                 throw new IllegalArgumentException("duplicate number found in use Input");
             }
-       }
+        }
     }
 
-    public boolean strikeAndBall(String newInput){
+    private boolean strikeAndBall(String newInput) {
         int strikeCount = 0;
         int ballCount = 0;
         for (int i = 0; i < 3; i++) {
@@ -67,7 +70,13 @@ public class BaseballGame {
                 ballCount += 1;
             }
         }
+        // TODO 2-4. 볼과 스트라이크 결과 출력
+        printStrikeAndBall(ballCount, strikeCount);
 
+        return (strikeCount == 3);
+    }
+
+    private void printStrikeAndBall(int ballCount, int strikeCount) {
         if (ballCount == 0 && strikeCount == 0) {
             System.out.println("낫싱");
         } else if (ballCount != 0 && strikeCount != 0) {
@@ -77,19 +86,17 @@ public class BaseballGame {
         } else {
             System.out.println(strikeCount + "스트라이크");
         }
-
-        return (strikeCount==3);
     }
 
-    public boolean checkRestartGame() {
+    private boolean checkRestartGame() {
         String restartInput = Console.readLine();
         if (restartInput.length() != 1) {
             throw new IllegalArgumentException();
         }
-        if (restartInput.charAt(1) != '2' || restartInput.charAt(1) != '1') {
+        if (restartInput.charAt(0) != '2' & restartInput.charAt(0) != '1') {
             throw new IllegalArgumentException();
         }
-        return restartInput.charAt(1) == '1';
+        return restartInput.charAt(0) == '1';
     }
 
 
