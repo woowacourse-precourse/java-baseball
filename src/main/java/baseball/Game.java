16cdbb1ed4d3adc454827b9068996b8f6e51f508
JIHYEISWISDOM <wlgp8958@naver.com>
2022-11-08 19:30:39 +0900

    feat(Game): 사용자 값 입력 받기

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -8,9 +8,11 @@ import java.util.List;
 public class Game {
     private static int MAX_BALL_COUNT;
     private final Computer computer;
+    private final User user;
 
     public Game(Computer computer, User user, int count) {
         this.computer = computer;
+        this.user = user;
         MAX_BALL_COUNT = count;
     }
 
@@ -18,6 +20,13 @@ public class Game {
         System.out.println("숫자 야구 게임을 시작합니다.");
         while (true) {
             List<Integer> computerNums = computer.createNums(MAX_BALL_COUNT);
+            playStage(computerNums, MAX_BALL_COUNT);
+        }
+    }
+
+    public void playStage(List<Integer> computerNums, int count) {
+        while (true) {
+            List<Integer> userNums = user.createNums(user.getInputs(), MAX_BALL_COUNT);
         }
     }
 }
