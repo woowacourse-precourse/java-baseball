2a3ab2934cf674ac482dd88209466e58b5424cf7
juhwang <juhong4930@naver.com>
2022-11-08 11:10:24 +0900

    test(GameBoardTest): check ball count

--- src/test/java/baseball/GameBoardTest.java
+++ src/test/java/baseball/GameBoardTest.java
@@ -1,13 +1,14 @@
 package baseball;
 
-import org.junit.jupiter.api.Nested;
+import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Field;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-class GameBoardTest {
+class GameBoardTest extends NsTest {
 
     GameBoard gameBoard = new GameBoard();
 
@@ -16,11 +17,22 @@ class GameBoardTest {
         try {
             Field result = gameBoard.getClass().getDeclaredField("randomNumbers");
             result.setAccessible(true);
-            assertThat(gameBoard.createRandomNumbers())
-                    .isEqualTo(result.get(gameBoard));
+            assertThat(gameBoard.createRandomNumbers()).isEqualTo(result.get(gameBoard));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
 
+    @Test
+    void case2_비교_후_볼카운트_출력_확인() {
+        assertRandomNumberInRangeTest(() -> {
+            run("213", "123", "2");
+            assertThat(output()).contains("2볼 1스트라이크", "3스트라이크", "게임 종료");
+        }, 1, 2, 3);
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
 }
