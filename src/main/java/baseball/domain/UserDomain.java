34d48e73f82bde038cd655e48d59028672da9e33
kiarakim <kiarakim413@gmail.com>
2022-11-08 22:33:22 +0900

    feat : add getter and setter of user numbers

---
+++ src/main/java/baseball/domain/UserDomain.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+public class UserDomain {
+    int[] userNumbers;
+
+    public int[] getUserNumbers(){
+        return userNumbers;
+    }
+
+    public void setUserNumbers(int[] numbers){
+        userNumbers = numbers;
+    }
+}
