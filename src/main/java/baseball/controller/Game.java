ec1b530fbcd9a20fb02f70a8a03dc9c15a9fe8c3
yunjeong <uu0078@naver.com>
2022-11-08 15:52:55 +0900

    rename: Console -> CLI
    nextstep의 Console과 혼동 방지

--- src/main/java/baseball/controller/Game.java
+++ src/main/java/baseball/controller/Game.java
@@ -1,10 +1,10 @@
 package baseball.controller;
 
-import baseball.view.Console;
+import baseball.view.CLI;
 import baseball.view.View;
 
 public class Game {
-    View view = new Console();
+    View view = new CLI();
     Round round = new Round();
 
     public void play() {
