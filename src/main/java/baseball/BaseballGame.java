b82a9fef94785c14cdbd6c8ac1c3e42a32ebf92b
rt3310 <btac3310@gmail.com>
2022-11-08 23:49:29 +0900

    refactor: 사용자 입력 및 검증 로직 in BaseballGame -> Hitter 객체로 이동

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,15 +1,11 @@
 package baseball;
 
 import camp.nextstep.edu.missionutils.Console;
-import java.util.Arrays;
-import java.util.regex.Pattern;
-import java.util.stream.IntStream;
 
 public class BaseballGame {
     private static final int NUMBER_OF_BALLS = 3;
     private static final String REPLAY = "1";
     private static final String OVER = "2";
-    private static final String INVALID_NUMBER_RANGE_REGEX = "[^1-9]";
 
     private final Hitter hitter;
     private final Pitcher pitcher;
@@ -27,7 +23,7 @@ public class BaseballGame {
     private boolean playInning() {
         Balls pitcherBalls = pitcher.throwRandomBalls(NUMBER_OF_BALLS);
         while (true) {
-            Balls hitterBalls = hitter.hitBalls(inputNumber());
+            Balls hitterBalls = hitter.hitBalls(hitter.inputNumber());
             int strikeCount = pitcherBalls.getStrikeCount(hitterBalls);
             int ballCount = pitcherBalls.getBallCount(hitterBalls);
             printResult(strikeCount, ballCount);
@@ -37,14 +33,6 @@ public class BaseballGame {
             }
         }
     }
-
-    public String inputNumber() {
-        System.out.print(BaseballMessage.INPUT_NUMBER_MESSAGE.getMessage());
-        String number = Console.readLine();
-        validate(number);
-        return number;
-    }
-
 
     private boolean isGameOver(int strikeCount) {
         if (strikeCount == NUMBER_OF_BALLS) {
@@ -92,40 +80,5 @@ public class BaseballGame {
         if (ballCount == 0 && strikeCount == 0) {
             System.out.print(BaseballMessage.NOTHING_MESSAGE.getMessage());
         }
-    }
-
-    private void validate(String number) {
-        validateNumberLength(number);
-        validateNumberRange(number);
-        validateNumberDuplication(number);
-    }
-
-    private void validateNumberLength(String number) {
-        if (number.length() != NUMBER_OF_BALLS) {
-            throw new IllegalArgumentException(NUMBER_OF_BALLS + BaseballMessage.INVALID_NUMBER_LENGTH_MESSAGE.getMessage());
-        }
-    }
-
-    private void validateNumberRange(String number) {
-        if (Pattern.compile(INVALID_NUMBER_RANGE_REGEX).matcher(number).find()) {
-            throw new IllegalArgumentException(BaseballMessage.INVALID_NUMBER_RANGE_MESSAGE.getMessage());
-        }
-    }
-
-    private void validateNumberDuplication(String number) {
-        if (IntStream.range(0, 10)
-                .anyMatch(digit -> isDuplicated(number, digit))) {
-            throw new IllegalArgumentException(BaseballMessage.DUPLICATED_NUMBER_MESSAGE.getMessage());
-        }
-    }
-
-    private boolean isDuplicated(String number, int digit) {
-        return getDigitCount(number, digit) >= 2;
-    }
-
-    private long getDigitCount(String number, int digit) {
-        return Arrays.stream(number.split(""))
-                .filter(digitString -> digitString.equals(String.valueOf(digit)))
-                .count();
     }
 }
