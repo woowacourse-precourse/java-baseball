dd260c7d0098cd7bb2f85e873b7c03b0237d5bc4
chanwonlee <clw8679@naver.com>
2022-11-08 12:08:03 +0900

    feat: 플레이어에게 숫자를 입력받는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,28 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+    }
+
+    private static List<Integer> inputNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String inputData = Console.readLine();
+        validateNumber(inputData);
+
+        List<Integer> inputNumber = new ArrayList<>();
+        for (char digit : inputData.toCharArray()) {
+            inputNumber.add(digit - 48);
+        }
+
+        return inputNumber;
+    }
+
+    private static void validateNumber(String inputData) {
     }
 }
