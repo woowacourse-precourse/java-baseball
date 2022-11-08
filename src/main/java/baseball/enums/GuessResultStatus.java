bb9dd930d3397efe9369d1855340494154aba782
HiiWee <wpdlzhf159@gmail.com>
2022-11-08 16:08:13 +0900

    Reefact: Enum에서 사용하지 않는 값, 메소드 제거

--- src/main/java/baseball/enums/GuessResultStatus.java
+++ src/main/java/baseball/enums/GuessResultStatus.java
@@ -1,15 +1,5 @@
 package baseball.enums;
 
 public enum GuessResultStatus {
-    WIN(1), NOT_WIN(0);
-
-    private final int winType;
-
-    GuessResultStatus(int winType) {
-        this.winType = winType;
-    }
-
-    public int getWinType() {
-        return winType;
-    }
+    WIN, NOT_WIN;
 }
