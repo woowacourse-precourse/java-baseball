291661c1b6a1bf9927303c432056ef26ea38947e
K-Diger <kitten.diger@gmail.com>
2022-11-08 16:13:32 +0900

    refactor : baseballGameStarter 클래스의 메서드 네이밍 변경 및 기능 위임에 대한 변경사항을 클라이언트 코드에 반영 - 게임 종료 후 계속할 것인지, 그만둘 것인지 입력받는 기능을 operateRefereeJudgement() 메서드에 추가하여 기능 통합

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,6 @@
 package baseball;
 
 import baseball.starter.BaseballGameStarter;
-import camp.nextstep.edu.missionutils.Console;
 
 import java.util.List;
 
@@ -9,27 +8,9 @@ public class Application {
 
     public static void main(String[] args) {
         BaseballGameStarter baseballGameStarter = new BaseballGameStarter();
-        List<List<String>> operatedGame = baseballGameStarter.operate();
-        List<String> userInputValue = operatedGame.get(0);
-        List<String> computerGeneratedValue = operatedGame.get(1);
-        int option = 0;
-
-        while (true) {
-            if (baseballGameStarter.operateRefereeJudgement(userInputValue, computerGeneratedValue)) {
-                System.out.println("3스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                option = Integer.parseInt(Console.readLine());
-            }
-            if (option == 1) {
-                operatedGame = baseballGameStarter.operate();
-                userInputValue = operatedGame.get(0);
-                computerGeneratedValue = operatedGame.get(1);
-            } else if (option == 2) {
-                break;
-            } else if (option != 1 || option != 2) {
-                throw new IllegalArgumentException();
-            }
-        }
+        List<List<String>> initializedGame = baseballGameStarter.initializeNewGame();
+        List<String> userInputValue = initializedGame.get(0);
+        List<String> computerGeneratedValue = initializedGame.get(1);
+        baseballGameStarter.operateRefereeJudgement(userInputValue, computerGeneratedValue);
     }
 }
