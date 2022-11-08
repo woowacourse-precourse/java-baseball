70fd7f3a5654c901d2b458edbe6d111a6c5017d5
jhsseonn <hosu0125@gmail.com>
2022-11-08 15:24:48 +0900

    refactor: 스트라이크 또는 볼 판정 함수 리팩토링 수정

--- src/main/java/baseball/Controller/BaseballController.java
+++ src/main/java/baseball/Controller/BaseballController.java
@@ -10,18 +10,16 @@ import java.util.List;
 public class BaseballController {
 
     private BaseballService baseballService;
-
-    private int strike=0;
-    private int ball=0;
 //    private int matchCount=0;
     public static List<String> randomList = new ArrayList<>();
     private List<String> inputList = new ArrayList<>();
 
     //게임 시작
     public void startBaseballGame() {
+        String strikeAndBallMessage;
         randomList = baseballService.createRandomNumber();
         inputList = readInputNumber();
-        countStrikeOrBall(randomList, inputList);
+        strikeAndBallMessage = countStrikeOrBall(randomList, inputList);
     }
 
     //유저가 입력한 숫자 받기
@@ -35,31 +33,46 @@ public class BaseballController {
     }
 
     //스트라이크 또는 볼 판정
-    public void countStrikeOrBall(List<String> randomNumber, List<String> inputNumber) {
+    public String countStrikeOrBall(List<String> randomNumber, List<String> inputNumber) {
+        int strike=0;
+        int ball=0;
+        String outputMessage="낫싱";
         for(String number: inputNumber) {
-            isNumberStrike(randomNumber, inputNumber, number);
-            isNumberBall(randomNumber, inputNumber, number);
+            strike = countStrike(randomNumber, inputNumber, number, strike);
+            ball = countBall(randomNumber, inputNumber, number, ball);
         }
+
+        return outputMessage;
     }
 
 
     //스트라이크 판정 및 카운트
-    public void isNumberStrike(List<String> randomNumber, List<String> inputNumber, String number) {
+    public int countStrike(List<String> randomNumber, List<String> inputNumber, String number, int strike) {
+        int strikeCount=0;
         if (randomNumber.contains(number) && randomNumber.indexOf(number) == inputNumber.indexOf(number)) {
-            strike = countStrikeOrBall(strike);
+            strikeCount = countStrikeOrBall(strike);
         }
+        return strikeCount;
     }
 
     //볼 판정 및 카운트
-    public void isNumberBall(List<String> randomNumber, List<String> inputNumber, String number) {
+    public int countBall(List<String> randomNumber, List<String> inputNumber, String number, int ball) {
+        int ballCount=0;
         if(randomNumber.contains(number) && randomNumber.indexOf(number)!=inputNumber.indexOf(number)) {
-            ball = countStrikeOrBall(ball);
+            ballCount = countStrikeOrBall(ball);
         }
+        return ballCount;
     }
 
     //스트라이크 또는 볼 카운트 함수
     public int countStrikeOrBall(int StrikeOrBall) {
         return StrikeOrBall+1 ;
     }
+
+    //낫싱인지 판정
+
+
+    //스트라이크 볼 출력
+
 
 }
