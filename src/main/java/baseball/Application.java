f9b26aae139ca3cf3b48c8b1802cbf1a18112280
yw824 <yongwoo25@gmail.com>
2022-11-08 19:44:50 +0900

    feat: [큰 틀] Game 클래스 생성 및 멤버변수, 생성자 설정
    - Game 클래스 : 게임의 전체 과정을 위한 큰 틀 생성
    - Game 클래스 멤버변수
    	- BALLS : 숫자 야구 게임 후 볼 탐색 위한 인덱스
    	- STRIKES : 숫자 야구 게임 후 스트라이크 탐색 위한 인덱스
    	- List computer : 컴퓨터의 정답을 담게 될 리스트
    	- List user : 사용자의 정답을 담게 될 리스트
    	- Game 생성자 : 리스트 멤버변수의 생성
    		- 매개변수 없음
    		- 게임 작동을 위한 추가 기능 X
    	- void init() :
    		- 게임의 큰 틀, 혹은 흐름을 가지고 있을 함수
    		- 매개변수 없음
    		- 내부 함수의 호출만을 담당 예정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,33 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        // 큰 틀 : 게임 클래스를 생성
+        Game numberBaseball = new Game();
+        // 게임 클래스 내에서 게임을 실행
+        numberBaseball.init();
+    }
+}
+
+class Game
+{
+    static int BALLS = 0; // 숫자 야구 게임 실행 후 볼 결과 탐색 위한 인덱스
+    static int STRIKES = 1; // 숫자 야구 게임 실행 후 스트라이크 결과 탐색 위한 인덱스
+    static int maxLen = 3; // 사용자 입력 및 컴퓨터 정답 배열이 가져야 할 항목 수
+    List<Integer> computer; // 컴퓨터의 정답을 담게 될 리스트
+    List<Integer> user; // 사용자의 입력값을 담게 될 리스트
+    Game() { // 리스트 멤버변수의 생성자, 게임 작동을 위한 추가 기능 없음
+        computer = new ArrayList<>();
+        user = new ArrayList<>();
+    }
+
+    // 게임의 큰 틀, 혹은 흐름을 가지고 있는 함수
+    void init()
+    {
+
     }
 }
