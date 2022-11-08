7a340e26ec7795b1cd9e863ff16111fba4cb8094
JJongmen <pjy90123@gmail.com>
2022-11-08 21:56:43 +0900

    refactor: 숫자 야구 게임을 시작하는 기능의 메서드들 더 작게 분리

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -9,24 +9,33 @@ public class BaseballGame {
 
     private static void playGameUntilFinish() {
         while (true) {
-            BaseballGameNumber computerNumber = RandomNumberGenerator.generateBaseballGameNumber();
-            guessNumberUntilCorrect(computerNumber);
-            Message.GAME_OVER.println();
-            Message.ASK_RESTART.println();
+            playGame();
             if (Input.readGameStatus() == GameStatus.GAME_OVER) {
                 return;
             }
         }
     }
 
+    private static void playGame() {
+        BaseballGameNumber computerNumber = RandomNumberGenerator.generateBaseballGameNumber();
+        guessNumberUntilCorrect(computerNumber);
+        Message.GAME_OVER.println();
+        Message.ASK_RESTART.println();
+    }
+
     private static void guessNumberUntilCorrect(BaseballGameNumber computerNumber) {
         while (true) {
-            Message.INPUT_NUMBER.print();
-            BaseballGameNumber expectedNumber = Input.readNumber();
-            Hint.show(Counter.calculate(computerNumber, expectedNumber));
+            BaseballGameNumber expectedNumber = guessNumber(computerNumber);
             if (BaseballGameNumber.isSame(computerNumber, expectedNumber)) {
                 return;
             }
         }
+    }
+
+    private static BaseballGameNumber guessNumber(BaseballGameNumber computerNumber) {
+        Message.INPUT_NUMBER.print();
+        BaseballGameNumber expectedNumber = Input.readNumber();
+        Hint.show(Counter.calculate(computerNumber, expectedNumber));
+        return expectedNumber;
     }
 }
