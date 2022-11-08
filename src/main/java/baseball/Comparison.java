8cb5e1daf67547a66d7f57d91396fe68733d0f82
0417taehyun <0417taehyun@gmail.com>
2022-11-08 17:25:52 +0900

    feat (Comparison): Comparison 클래스 세부 구현 이전 틀 구축

---
+++ src/main/java/baseball/Comparison.java
@@ -0,0 +1,23 @@
+package baseball;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.IntStream;
+
+public class Comparison {
+    public static int countBallNumbers(List<Integer> userNumber, List<Integer> answerNumber) {
+
+    }
+
+    public static int countStrikeNumbers(List<Integer> userNumber, List<Integer> answerNumber) {
+
+    }
+
+    public static boolean isBall(List<Integer> answerNumber, int number, int index) {
+
+    }
+
+    public static boolean isStrike(List<Integer> userNumber, List<Integer> answerNumber, int index) {
+
+    }
+}
