1051ab136d4f81d6ee3c43631e556f5b04b28617
mingyunLEE <leemingyun@naver.com>
2022-11-08 18:39:22 +0900

    Add Function [스트라이크 확인 테스트]

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -3,6 +3,11 @@ package baseball;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -26,6 +31,28 @@ class ApplicationTest extends NsTest {
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
+    }
+
+    @Test
+    void 스트라이크_확인_테스트() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Application application = new Application();
+        List<Integer> computer = new ArrayList<>();
+        List<Integer> user = new ArrayList<>();
+
+        computer.add(1);
+        computer.add(2);
+        computer.add(3);
+
+        user.add(1);
+        user.add(2);
+        user.add(4);
+
+        Method strike = application.getClass().getDeclaredMethod("strike", List.class, List.class);
+        strike.setAccessible(true);
+
+        int strikeCheck = (int)strike.invoke(application, computer, user);
+
+        assertThat(strike).isEqualTo(2);
     }
 
     @Override
