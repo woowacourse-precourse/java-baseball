7ecee9022dca379fb81a46eb9797f9a0693b054c
seonghbae <fullbloomh3ck@gmail.com>
2022-11-08 21:30:14 +0900

    feat(compareNumber) : compareNumber 메서드 구현

--- src/main/java/baseball/GameHandler.java
+++ src/main/java/baseball/GameHandler.java
@@ -10,9 +10,9 @@ public class GameHandler {
     public static List<Integer> compareResult;
 
     public void init() {
-        computerNumber = new ArrayList<>();
-        inputNumber = new ArrayList<>();
-        compareResult = new ArrayList<>();
+        computerNumber = new ArrayList<>(3);
+        inputNumber = new ArrayList<>(3);
+        compareResult = new ArrayList<>(2);
     }
 
     public void inputHandler() {
@@ -21,5 +21,19 @@ public class GameHandler {
         for(char c : input.toCharArray()) {
             inputNumber.add(c - '0');
         }
+    }
+
+    public void compareNumber() {
+        int strike = 0, ball = 0, number;
+        for(int i = 0; i < 3; i++) {
+            number = inputNumber.get(i);
+            if(number == computerNumber.get(i)) {
+                strike++;
+            } else if (computerNumber.contains(number)) {
+                ball++;
+            }
+        }
+        compareResult.add(strike);
+        compareResult.add(ball);
     }
 }
