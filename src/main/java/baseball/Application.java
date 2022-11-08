481bae2cadcf6aee5508730639dbe9e84bc9b2bb
kathyleesh <90851865+kathyleesh@users.noreply.github.com>
2022-11-08 13:54:44 +0900

    refactor(inputPlayerAnswerList) : functionalization the list
    functionalization the part where you enter the player's answer and divide the numbers into lists
    
    BREAKING CHANGE: readability increases and refactoring becomes easier

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,17 +8,11 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+        // TODO : 같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 낫싱이란 힌트를 출력
         List<Integer> computer = makeRandomNumberList();
+        List<String> playerAnswer = inputPlayerAnswerList();
 
-        // TODO : 숫자를 입력받아 분리한 다음 리스트에 넣는 부분을 함수화
-
-        System.out.println("숫자를 입력해주세요 : ");
-        String myAnswer = Console.readLine();
-
-        String[] splitAnswer = myAnswer.split("");
-        ArrayList<String> AnswerList = new ArrayList<>(Arrays.asList(splitAnswer));
-        System.out.println("Answer : " + AnswerList);
-
+        System.out.println("playerAnswer : " + playerAnswer);
         System.out.println("computer : " + computer);
     }
 
@@ -32,4 +26,14 @@ public class Application {
         }
         return computer;
     }
+
+    public static List<String> inputPlayerAnswerList(){
+        System.out.println("숫자를 입력해주세요 : ");
+        String myAnswer = Console.readLine();
+
+        String[] splitAnswer = myAnswer.split("");
+        ArrayList<String> answerList = new ArrayList<>(Arrays.asList(splitAnswer));
+        return answerList;
+    }
+
 }
