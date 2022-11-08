a9efa42f5e70ddb5b849645018c71a4a96033d1e
jijongkwon <help3451@naver.com>
2022-11-08 17:36:17 +0900

    feat : 야구게임 시작을 위한 메서드 추가 - BaseballComputer 클래스와 GameMessage 클래수 생성 - GameMessage 클래스의 startGameMessage 메서드 사용 - BaseballComputer 클래스의 start 메서드 사용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballComputer baseballComputer = new BaseballComputer();
+        GameMessage gameMessage = new GameMessage();
+        gameMessage.startGameMessage();
+        baseballComputer.start();
     }
 }
