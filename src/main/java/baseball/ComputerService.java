1989cdaffc7e7eef20c957b5fe0b935c30f2f4b0
chaos0103 <lyt1228@naver.com>
2022-11-08 18:45:21 +0900

    refactor: ComputerService 기능과 구현 분리

--- src/main/java/baseball/ComputerService.java
+++ src/main/java/baseball/ComputerService.java
@@ -1,41 +1,8 @@
 package baseball;
 
-import camp.nextstep.edu.missionutils.Randoms;
-
-import java.util.ArrayList;
 import java.util.List;
 
-public class ComputerService {
+public interface ComputerService {
 
-    private final static int MIN_RANGE = 1;
-    private final static int MAX_RANGE = 9;
-    private final static int MAX_SIZE = 3;
-
-    List<Integer> numbers = new ArrayList<>();
-
-    public List<Integer> createComputerNumber() {
-        clearNumbers();
-        while (isMaxSize()) {
-            addComputerNumber(Randoms.pickNumberInRange(MIN_RANGE, MAX_RANGE));
-        }
-        return numbers;
-    }
-
-    private void clearNumbers() {
-        numbers = new ArrayList<>();
-    }
-
-    private boolean isMaxSize() {
-        return numbers.size() < MAX_SIZE;
-    }
-
-    private void addComputerNumber(int number) {
-        if (isNotContains(number)) {
-            numbers.add(number);
-        }
-    }
-
-    private boolean isNotContains(int number) {
-        return !numbers.contains(number);
-    }
+    List<Integer> createComputerNumber();
 }
