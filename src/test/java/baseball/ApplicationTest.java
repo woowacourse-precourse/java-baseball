843f0ffcb00e3760224d04f51f9abec7009bba3f
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-08 09:39:35 +0900

    test<ApplicationTest>: test case 추가
    - 세자리 미만 예외 발생 test case 추가
    - 중복된 숫자가 있을 경우 예외 발생 test case 추가
    - 1~9 범위 밖 숫자 예외 발생 test case 추가
    - 문자열 입력값 예외 발생 test case 추가
    - 공백 문자열 입력값 예외 발생 test case 추가
    - 빈 문자열 입력값 예외 test case 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import baseball.util.GameExceptionMessage;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
@@ -21,12 +22,79 @@ class ApplicationTest extends NsTest {
     }
 
     @Test
+    void 입력값_일치_후_게임종료() {
+        assertRandomNumberInRangeTest(
+                () -> {
+                    run("123", "456", "789", "157", "248", "247", "742", "2");
+                    assertThat(output()).contains(
+                            "1볼", "1볼", "1스트라이크", "1볼", "1볼 1스트라이크", "2볼 1스트라이크", "3스트라이크", "게임 종료");
+                },
+                7, 4, 2
+        );
+    }
+
+    @Test
     void 예외_테스트() {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
         );
     }
+
+    @Test
+    void 세자리_미만_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("12"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(GameExceptionMessage.ENTER_THREE_DIFFERENT_NUMBER)
+        );
+    }
+
+    @Test
+    void 중복_숫자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("122"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(GameExceptionMessage.ENTER_THREE_DIFFERENT_NUMBER)
+        );
+    }
+
+    @Test
+    void 범위_밖_숫자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("120"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(GameExceptionMessage.ENTER_NUMBER_FROM_ONE_TO_NINE)
+        );
+    }
+
+    @Test
+    void 문자열_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("abc"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(GameExceptionMessage.ENTER_THREE_DIFFERENT_NUMBER)
+        );
+    }
+
+    @Test
+    void 공백_문자열_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException(" "))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(GameExceptionMessage.ENTER_THREE_DIFFERENT_NUMBER)
+        );
+    }
+
+    @Test
+    void 빈문자열_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("", "\n"))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessage(GameExceptionMessage.ENTER_THREE_DIFFERENT_NUMBER)
+        );
+    }
+
 
     @Override
     public void runMain() {
