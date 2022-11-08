cf86e20f6a7a5942a5abb9e30b7bbd2ab9b4133e
juno-junho <ssmm0205@naver.com>
2022-11-08 20:20:04 +0900

    feat(model): add generateUserNum method
    UserNumberGenerator 클래스 안에서 user의 number를 생성하는 generateUserNum 메서드 생성

--- src/main/java/baseball/model/UserNumberGenerator.java
+++ src/main/java/baseball/model/UserNumberGenerator.java
@@ -12,5 +12,12 @@ public class UserNumberGenerator {
         this.VALIDATED_NUM = generateUserNum();
     }
 
+    private static List<Integer> generateUserNum() throws IllegalArgumentException {
+        String startInput = InputView.gameEndInput();
+        List<Integer> numbers = NumberValidator.convertStringToList(startInput);
+        if (isValid(numbers))
+            return numbers;
+        throw new IllegalArgumentException();
 
+    }
 }
