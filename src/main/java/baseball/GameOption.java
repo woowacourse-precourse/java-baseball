6aaf3e5f7fa6e94cad70d0f7144dbb92803d0858
kimJaeHyeon <kmr2644@gmail.com>
2022-11-08 11:17:00 +0900

    fix: GameOption value 삭제

--- src/main/java/baseball/GameOption.java
+++ src/main/java/baseball/GameOption.java
@@ -1,14 +1,8 @@
 package baseball;
 
 public enum GameOption {
-    RESTART("1"),
-    EXIT("2");
-
-    private final String input;
-
-    GameOption(String input) {
-        this.input = input;
-    }
+    RESTART,
+    EXIT;
 
     public static GameOption of(String input) {
         if (input.equals("1")) {
