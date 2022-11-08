dc232855e2a29ab8f8204466342dde89a5f22ec3
MinSungKim <timkim01@naver.com>
2022-11-08 10:12:42 +0900

    test: Comparison 숫자를 리스트로 변환하는 메서드 테스트 작성

--- src/test/java/baseball/process/ComparisonTest.java
+++ src/test/java/baseball/process/ComparisonTest.java
@@ -1,4 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball.process;
+
+import baseball.model.Game;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 class ComparisonTest {
-  
+    Comparison comparison;
+
+    public ComparisonTest(){
+        Game game = new Game();
+        game.setUserInputNumber(123);
+        game.setRandomNumber(456);
+
+        this.comparison = new Comparison(game, 3);
+    }
+
+    @Test
+    void 숫자를_리스트로(){
+        int number = 789;
+        List<Integer> resultList = List.of(7, 8, 9);
+
+        Assertions.assertThat(comparison.numberToList(number)).isEqualTo(resultList);
+    }
 }
