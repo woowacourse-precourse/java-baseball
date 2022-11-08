af86afc2cd90fabfc5de90987570d8ce52a055d7
Grooomit <tmd5972@gmail.com>
2022-11-08 21:57:28 +0900

    [Feat] 랜덤숫자 생성 구현
    - createRandomNumber()
    랜덤 API 를 이용해 서로 다른 3자리의 수를 생성한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,40 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    private static List<Integer> numberList;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        System.out.println("숫자 아규 게임을 시작합니다.");
+
+        baseball();
     }
+
+    private static void baseball() {
+        String answer = createRandomNumber();
+
+
+    }
+
+
+    private static String createRandomNumber() {
+        StringBuilder randomNum = new StringBuilder();
+
+        for (int i = 0; i < 3; i++) {
+            int random = Randoms.pickNumberInRange(1, 9);
+
+            while (randomNum.toString().contains(Integer.toString(random))) {
+                random = Randoms.pickNumberInRange(1, 9);
+            }
+            randomNum.append(random);
+        }
+        return randomNum.toString();
+    }
+
+
 }
