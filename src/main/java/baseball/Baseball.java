14431bfe4ee256013b2655f615b377fe418522dd
i960107 <i960107@naver.com>
2022-11-08 15:22:15 +0900

    refactor(Baseball): move feature of playing again or exiting to Application.class

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -1,49 +1,22 @@
 package baseball;
 
 public class Baseball {
-    private BaseballInput baseballInput;
-    private BaseballOutput baseballOutput;
-    private BaseballResult baseballResult;
-    private BaseballScore baseballScore;
-
-    public static int EXIT_CODE = 2;
-    public static int PLAY_AGAIN_CODE = 1;
-
+    private static BaseballResult baseballResult;
 
     Baseball() {
-        baseballInput = new BaseballInput();
-        baseballOutput = new BaseballOutput();
         baseballResult = new BaseballResult();
-        baseballScore = new BaseballScore();
     }
 
     public void play() {
-        baseballOutput.printGameStart();
+        BaseballNumber computerNumbers = BaseballNumber.getComputerPickedNumbers();
+        boolean is_all_strike;
         do {
-
-        } while (does_user_want_play_again());
-
+            BaseballNumber userNumber = BaseballInput.getUserNumber();
+            BaseballScore roundScore = baseballResult.getBaseballScore(computerNumbers, userNumber);
+            System.out.println(roundScore.toString());
+            is_all_strike = roundScore.is_all_strike();
+        } while (!is_all_strike);
     }
 
-    private boolean does_user_want_play_again() {
-        Integer play_code = get_user_input_play_code();
-        return play_code.equals(PLAY_AGAIN_CODE);
-    }
 
-    private Integer get_user_input_play_code() {
-
-        Integer play_code = null;
-
-        while (play_code == null || !(play_code == PLAY_AGAIN_CODE || play_code == EXIT_CODE)) {
-
-            try {
-                baseballOutput.printInputPlayCodeMessage();
-                play_code = Integer.parseInt(baseballInput.getUserInput());
-
-            } catch (NumberFormatException e) {
-
-            }
-        }
-        return play_code;
-    }
 }
