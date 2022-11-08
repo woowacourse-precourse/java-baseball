be03ac0296037d1e407a915110b91b75d0400d22
kimdoha <kkhhjj3393@gmail.com>
2022-11-08 21:45:36 +0900

    test(Result): Result 테스트 요구사항 구현

--- src/test/java/baseball/domain/score/BaseballGameResultTest.java
+++ src/test/java/baseball/domain/score/BaseballGameResultTest.java
@@ -2,6 +2,45 @@ package baseball.domain.score;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 class BaseballGameResultTest {
 
+  private Result board;
+
+  @Test
+  @DisplayName("점수 결과가 낫싱일 경우")
+  void print_nothing(){
+    board = new Result(0,0);
+    assertEquals("낫싱", board.getResults());
+  }
+
+  @Test
+  @DisplayName("점수 결과가 3스트라이크일 경우")
+  void print_3_strike(){
+    board = new Result(3,0);
+    assertEquals("3스트라이크", board.getResults());
+  }
+
+  @Test
+  @DisplayName("점수 결과가 1볼 2스트라이크일 경우")
+  void print_1_ball_2_strike(){
+    board = new Result(2,1);
+    assertEquals("1볼 2스트라이크", board.getResults());
+  }
+
+  @Test
+  @DisplayName("점수 결과가 2볼 2스트라이크일 경우")
+  void print_2_ball_2_strike(){
+    board = new Result(2,2);
+    assertEquals("2볼 2스트라이크", board.getResults());
+  }
+
+  @Test
+  @DisplayName("점수 결과가 3볼 2스트라이크일 경우")
+  void print_3_ball_2_strike(){
+    board = new Result(2,3);
+    assertEquals("3볼 2스트라이크", board.getResults());
+  }
 }
