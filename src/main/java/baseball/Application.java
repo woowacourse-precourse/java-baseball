ed13b36c5a038e622f823eb66812c04d63841794
yewon <yewon.ux@gmail.com>
2022-11-08 10:35:12 +0900

    refactor: user input API 활용

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,15 +1,17 @@
 package baseball;
 
 import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
 
         // scan user input
-        Scanner sc = new Scanner(System.in);
-        System.out.println("서로다른 세 숫자를 입력해주세요");
-        int userRandNumber = sc.nextInt();
+        System.out.println("서로다른 세 숫자를 입력하세요");
+        String userRandNumber = Console.readLine();
+        System.out.println(userRandNumber);
 
     }
 }
