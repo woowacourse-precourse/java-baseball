3f980274b2062e7af8e3e9a3b0ef9ce12093f9b7
juy4556 <kws05050@naver.com>
2022-11-08 22:40:00 +0900

    feat: 예외처리에 대한 테스트코드 작성

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import baseball.exception.Exception;
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
 
@@ -9,6 +10,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+    Exception exception = new Exception();
+
     @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
@@ -25,6 +28,84 @@ class ApplicationTest extends NsTest {
         assertSimpleTest(() ->
                 assertThatThrownBy(() -> runException("1234"))
                         .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 입력값_길이_작을_때_예외_테스트() {
+        String input = "1";
+        int rightLength = 3;
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> exception.validateInputLength(input, rightLength))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("길이가 " + rightLength + "인 수를 입력해주세요.")
+        );
+    }
+
+    @Test
+    void 입력값_길이_길_때_예외_테스트() {
+        String input = "1234";
+        int rightLength = 3;
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> exception.validateInputLength(input, rightLength))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("길이가 " + rightLength + "인 수를 입력해주세요.")
+        );
+    }
+
+    @Test
+    void 입력값_중복_테스트() {
+        String input = "112";
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> exception.validateIsDuplicate(input))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("중복된 숫자를 입력하지 말아주세요.")
+        );
+    }
+
+    @Test
+    void 입력값_특정값_사이_테스트() {
+        String input = "012";
+        char startNumber = '1';
+        char endNumber = '9';
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> exception.validateIsNumeric(input, startNumber, endNumber))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(startNumber + "~" + endNumber + "사이의 숫자만 입력해주세요.")
+        );
+    }
+
+    @Test
+    void 재시작_입력값_길이_짧을_때_테스트() {
+        String input = "";
+        int rightLength = 1;
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> exception.validateInputLength(input, rightLength))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("길이가 " + rightLength + "인 수를 입력해주세요.")
+        );
+    }
+
+    @Test
+    void 재시작_입력값_길이_길_때_테스트() {
+        String input = "123456";
+        int rightLength = 1;
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> exception.validateInputLength(input, rightLength))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining("길이가 " + rightLength + "인 수를 입력해주세요.")
+        );
+    }
+
+    @Test
+    void 재시작_입력값_특정값_사이_테스트() {
+        String input = "7";
+        char startNumber = '1';
+        char endNumber = '2';
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> exception.validateIsNumeric(input, startNumber, endNumber))
+                        .isInstanceOf(IllegalArgumentException.class)
+                        .hasMessageContaining(startNumber + "~" + endNumber + "사이의 숫자만 입력해주세요.")
         );
     }
 
