a5dd2c538c039b0d06f314d892f6e8e5ac1e68de
RadSparkle <wwwartg1@naver.com>
2022-11-08 21:57:03 +0900

    FEAT : 사용자가 입력하는값을 받는 기능 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,13 +1,18 @@
 package baseball;
-
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Application {
     private static final int threeDigits = 999;
 
     public static void main(String[] args) throws IOException {
+
+        String numbers = Console.readLine();
 
     }
 
