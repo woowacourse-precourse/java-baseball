7b1c4181ea2087fbe7070ff6b04ccd2763894eff
Jin409 <sualng123@gmail.com>
2022-11-08 14:26:54 +0900

    test(ScoreTest): Score 클래스에 대한 테스트 코드 추가
    - 점수에 따른 올바른 상태 반환 테스트
    - 상태에 따른 올바른 횟수 반환 테스트

---
+++ src/test/java/baseball/domain/ScoreTest.java
@@ -0,0 +1,35 @@
+package baseball.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class ScoreTest {
+    @Test
+    void 점수에_따른_올바른_상태를_반환() {
+        //given
+        Score testScore = new Score(1, 2);
+
+        //when
+        CountStatus countStatus = testScore.getCountStatus();
+
+        //then
+        assertThat(countStatus).isEqualTo(CountStatus.BALL_AND_STRIKE);
+    }
+
+    @Test
+    void 상태에_따른_올바른_횟수를_반환() {
+        //given
+        int strikeCount = 1;
+        int ballCount = 2;
+        Score testScore = new Score(ballCount, strikeCount);
+        CountStatus countStatus = testScore.getCountStatus();
+
+        //when
+        List<Integer> strikeAndBallCount = testScore.getStrikeAndBallCount(countStatus);
+
+        //then
+        assertThat(strikeAndBallCount).isEqualTo(List.of(strikeCount, ballCount));
+    }
+}
