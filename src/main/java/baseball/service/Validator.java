31a00e8fb39fc6b07f49d091ff3a2968159884d3
ohhonggi <5ghdrl@naver.com>
2022-11-08 20:22:03 +0900

    fix($Validator): Fix validInput function error
    This change does not include zero in the number entered by the user.

--- src/main/java/baseball/service/Validator.java
+++ src/main/java/baseball/service/Validator.java
@@ -5,7 +5,7 @@ public class Validator {
     public static void validInput(String text) {
         if (text.length() != 3) {
             throw new IllegalArgumentException("숫자는 3자리 수이어야 합니다.");
-        } else if (!text.matches("^\\d+$")) {
+        } else if (!text.matches("^[1-9]+$")) {
             throw new IllegalArgumentException("입력은 숫자로만 이루어져야 합니다.");
         }
     }
