c062b40b2167c6cbc5022cfadf248f3715a3432a
si hwan <pum005@naver.com>
2022-11-08 21:05:04 +0900

    feat : Main 함수에서 게임 시작하는 기능

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,24 @@
 package baseball;
 
+import baseball.controller.Game;
+import baseball.service.GameService;
+import baseball.view.ConsoleInput;
+import baseball.view.ConsoleOutput;
+import baseball.view.Input;
+import baseball.view.Output;
+import baseball.model.numbergenerator.NextStepNumberGenerator;
+import baseball.model.numbergenerator.NumberGenerator;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        NumberGenerator randomNumberGenerator = new NextStepNumberGenerator();
+        Input input = new ConsoleInput();
+        Output output = new ConsoleOutput();
+        GameService gameService = new GameService();
+
+        Game game = new Game(randomNumberGenerator, input, output, gameService);
+        game.init();
+        game.play();
     }
 }
