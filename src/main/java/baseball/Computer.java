b39c6b89eb09f69f094fdbb7f3f6eea75e85a058
Eunkeee <eunkeeeee@gmail.com>
2022-11-08 21:24:40 +0900

    refactor: 메소드 분리
    getThreeDifferentRandomDigits, joinDigitsTogether, getRandomDigit

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,31 +2,43 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 public class Computer {
 
-    static final int NUMBER_LENGTH = 3;
-
+    private static final int NUMBER_LENGTH = 3;
+    private static final int DIGIT_MIN = 1;
+    private static final int DIGIT_MAX = 9;
     int randomNumber;
 
     public Computer() {
     }
 
     public void setRandomNumber() {
-        List<String> digitsList = new ArrayList<>();
-        while(digitsList.size() < NUMBER_LENGTH) {
-            String digit = String.valueOf(Randoms.pickNumberInRange(1,9));
-            if(!digitsList.contains(digit)) {
-                digitsList.add(digit);
-            }
-        }
-        randomNumber = Integer.parseInt(String.join("", digitsList));
+        List<String> digitsList = getThreeDifferentRandomDigits();
+        randomNumber = joinDigitsTogether(digitsList);
     }
 
     public int getRandomNumber() {
         return randomNumber;
+    }
+
+    private static List<String> getThreeDifferentRandomDigits() {
+        List<String> digitsList = new ArrayList<>();
+        while(digitsList.size() < NUMBER_LENGTH) {
+            String digit = getRandomDigit();
+            if(!digitsList.contains(digit)) {
+                digitsList.add(digit);
+            }
+        }
+        return digitsList;
+    }
+
+    private static int joinDigitsTogether(List<String> digitsList) {
+        return Integer.parseInt(String.join("", digitsList));
+    }
+
+    private static String getRandomDigit() {
+        return String.valueOf(Randoms.pickNumberInRange(DIGIT_MIN, DIGIT_MAX));
     }
 }
