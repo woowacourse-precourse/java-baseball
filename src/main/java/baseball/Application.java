aa5eb72d429d945479dd85a27c88c9ea2ccd2188
bjm7701 <bjm7701@naver.com>
2022-11-08 16:56:54 +0900

    refactor : replace main function with moduled function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -82,6 +82,8 @@ public class Application {
     }
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다.");
-        
+        while(true) {
+            if(baseball_event() == 0) return;
+        }
     }
 }
