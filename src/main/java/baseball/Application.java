e980c055e24a9372bc90eca494442f5da7d4ef3c
baektmdduq <97cowtop@naver.com>
2022-11-08 15:11:45 +0900

    feat: 사용자가 서로 다른 숫자 3개를 입력하는 기능 추가
    숫자가 3개가 아닐 시 게임 종료
    중복된 숫자가 있을 시 게임 종료
    숫자가 아닌 입력일 시 게임 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -18,6 +18,12 @@ public class Application {
   public void startGame() {
     System.out.println("숫자 야구 게임을 시작합니다.");
     List<Integer> computer = initComputer();
+
+    while (true) {
+      String userInput = enterNumber();
+      checkCorrectInput(userInput);
+
+    }
   }
 
   public List<Integer> initComputer() {
@@ -31,5 +37,34 @@ public class Application {
     }
 
     return computer;
+  }
+
+  public String enterNumber() {
+    System.out.print("숫자를 입력해주세요 : ");
+    String userInput = Console.readLine();
+    return userInput;
+  }
+
+  public void checkCorrectInput(String userInput) {
+    if (userInput.length() != digit) {
+      throw new IllegalArgumentException("입력 길이가 초과되어 종료합니다.");
+    }
+
+    List<Integer> userInputList = new ArrayList<>();
+    String[] numbers = userInput.split("");
+
+    for (int i = 0; i < digit; i++) {
+      try {
+        int number = Integer.parseInt(numbers[i]);
+        if (!((number < 49) || (number > 57))) {
+          throw new IllegalArgumentException("숫자가 아닌 입력이므로 종료합니다.");
+        } else if (userInputList.contains(number)) {
+          throw new IllegalArgumentException("중복된 숫자가 존재하므로 종료합니다.");
+        }
+        userInputList.add(number);
+      } catch (NumberFormatException e) {
+        throw new IllegalArgumentException("숫자가 아닌 입력이므로 종료합니다.");
+      }
+    }
   }
 }
