78b08878bcabc1fcccd52f0af876262807fd191e
mikekang47 <gyeong0308@gmail.com>
2022-11-08 23:39:35 +0900

    Feat: 결과를 받아 출력하는 메서드 완성

--- src/main/java/baseball/views/Output.java
+++ src/main/java/baseball/views/Output.java
@@ -1,10 +1,17 @@
 package baseball.views;
 
+import baseball.utils.ResultRenderer;
+
 public class Output {
     private Output() {
     }
 
     public static void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void result(int strike, int ball) {
+        String result = ResultRenderer.getResult(strike, ball);
+        System.out.println(result);
     }
 }
