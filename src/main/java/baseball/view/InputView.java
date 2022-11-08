f4f9f220ac375f67ab4258868b3c256fee259280
dhkim <akdlzmf1123@gmail.com>
2022-11-08 16:09:08 +0900

    refactor(InputView): 외부에서 접근하지 않는 상수 private 접근자로 변경 & 사용자 입력 패턴 상수로 추출

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -10,12 +10,13 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class InputView {
-    public static final int REMATCH_NUMBER = 1;
-    public static final int NO_REMATCH_NUMBER = 2;
-    public static final String INPUT_SIGN = "숫자를 입력해주세요 : ";
-    public static final String REMATCH_SIGN = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    public static final String ERR_REMATCH_INPUT = "게임 시작하려면 1만 입력하고 종료하려면 2만 입력해야 합니다.";
-    public static final String ERR_GAME_INPUT = "입력 숫자는 1 ~ 9의 자릿수를 가진 3 자리 숫자입니다.";
+    private static final int REMATCH_NUMBER = 1;
+    private static final int NO_REMATCH_NUMBER = 2;
+    private static final String INPUT_SIGN = "숫자를 입력해주세요 : ";
+    private static final String REMATCH_SIGN = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String ERR_REMATCH_INPUT = "게임 시작하려면 1만 입력하고 종료하려면 2만 입력해야 합니다.";
+    private static final String ERR_GAME_INPUT = "입력 숫자는 1 ~ 9의 자릿수를 가진 3 자리 숫자입니다.";
+    private static final String USER_INPUT_PATTERN = "^[1-9]{3}$";
 
     public static Balls input() {
         System.out.print(INPUT_SIGN);
@@ -23,7 +24,7 @@ public class InputView {
     }
 
     public static Balls input(String input) {
-        Pattern pattern = Pattern.compile("^[1-9]{3}$");
+        Pattern pattern = Pattern.compile(USER_INPUT_PATTERN);
         Matcher matcher = pattern.matcher(input);
         if (!matcher.matches()) {
             throw new IllegalArgumentException(ERR_GAME_INPUT);
