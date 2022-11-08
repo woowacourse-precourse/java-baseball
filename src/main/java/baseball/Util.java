1a31c5f8d609784c7f6c1923c617e85c749d8483
DDing77 <devseo2021@gmail.com>
2022-11-08 17:21:19 +0900

    feat($Util) : 유저숫자3개 입력

--- src/main/java/baseball/Util.java
+++ src/main/java/baseball/Util.java
@@ -13,7 +13,7 @@ public class Util {
     public String createAnswerString() {
         Set<Integer> set = new HashSet<>();
 
-        while(set.size() < 3) {
+        while (set.size() < 3) {
             set.add(Randoms.pickNumberInRange(1, 9));
         }
 
@@ -21,10 +21,19 @@ public class Util {
 
         Iterator<Integer> iter = set.iterator();
 
-        while(iter.hasNext()) {
+        while (iter.hasNext()) {
             sb.append(iter.next());
         }
 
         return sb.toString();
+    }
+
+    public String createUserAnswerString() {
+        String input = Console.readLine();
+
+        Exception exception = new Exception();
+        exception.createNumberInputException(input);
+
+        return input;
     }
 }
