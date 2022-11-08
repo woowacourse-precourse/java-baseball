cf79d0557a5151c1b5485b823a497f9339445abb
kswhj <108874006+kswhj@users.noreply.github.com>
2022-11-08 15:57:55 +0900

    Update Application.java
    게임을 진행하는 메인 함수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,6 +2,11 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        // TODO: 프로그램 구현   
+        gameStart();
+    }
+    
+    public static void gameStart() {
+    	
     }
 }
