a67f24350f078b37f39fd68aae527d0009eeec0c
moonstal1506 <moonstal1506@gmail.com>
2022-11-08 11:50:58 +0900

    chore: 정답 출력 코드 삭제

--- src/main/java/baseball/controller/BaseBallController.java
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -6,8 +6,6 @@ import baseball.model.Result;
 import baseball.view.InputView;
 import baseball.view.OutputView;
 
-import java.util.List;
-
 public class BaseBallController {
 
     private static final int CONTINUE = 1;
@@ -32,11 +30,7 @@ public class BaseBallController {
 
     private void playGame() {
         boolean isGameEnd = false;
-        List<Integer> generate = NumberGenerator.generate();
-        for (Integer integer : generate) {
-            System.out.print(integer);
-        }
-        Balls computerBalls = new Balls(generate);
+        Balls computerBalls = new Balls(NumberGenerator.generate());
 
         while (!isGameEnd) {
             Result result = matchBalls(computerBalls);
