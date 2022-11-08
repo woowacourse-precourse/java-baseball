8462ee80ac9b738f0e2863ccd506949f1d85571d
mywnajsldkf <mywnajsldkf@gmail.com>
2022-11-08 20:54:25 +0900

    refactor : messageUtil 사용

--- src/main/java/baseball/PlayerNumber.java
+++ src/main/java/baseball/PlayerNumber.java
@@ -3,8 +3,6 @@ package baseball;
 import java.util.List;
 
 public class PlayerNumber {
-    private static final int BALL_NUMBER_SET_SIZE = 3;
-    private static final String BALL_NUMBER_SET_OUT_OF_RANGE = "야구공 숫자는 3개까지 입력할 수 있습니다.";
     private final List<BallNumber> playerNumberSet;
 
     public PlayerNumber(List<BallNumber> ballNumberSet) {
