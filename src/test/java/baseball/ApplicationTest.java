a57dc5a0bbc860212995e2286c9c56aeed2cbd43
sodlfmag <sodlfmag@khu.ac.kr>
2022-11-08 21:20:05 +0900

    test: make exception test3 for isInputValid()

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -123,6 +123,16 @@ class ApplicationTest extends NsTest {
 
         assertThat(exception.getCause().getMessage()).isEqualTo("정수가 아닌 값을 입력했습니다.");
     }
+    @Test
+    void 입력값_유효성_테스트4_except() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        Method method = Application.class.getDeclaredMethod("isInputValid", String.class);
+        method.setAccessible(true);
+
+        InvocationTargetException exception = assertThrows(InvocationTargetException.class,
+                ()-> method.invoke(application, "104"));
+
+        assertThat(exception.getCause().getMessage()).isEqualTo("0 또는 중복되는 수가 입력되었습니다.");
+    }
 
     @Override
     public void runMain() {
