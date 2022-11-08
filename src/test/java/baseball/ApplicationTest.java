1cb062d9648df805b7a95a524af5fdcd4ca1986a
KangSungBum <peter9706@naver.com>
2022-11-08 15:38:16 +0900

    feat(ApplicationTest.java) : add getResultTest method
    Added new test method to ApplicationTest.java
    - checks if the method correctly retrieves the result message

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -134,6 +134,25 @@ class ApplicationTest extends NsTest {
         assertArrayEquals(expectedResult, actualResult);
     }
 
+    @Test
+    public void getResultTest() throws Exception{
+        //given
+        int[] countResult1 = new int[]{1, 2};
+        String expectedOneBallTwoStrike = "1볼 2스트라이크";
+
+        int[] countResult2 = new int[]{1, 1};
+        String expectedOneBallOneStrike = "1볼 1스트라이크";
+
+
+        //when
+        String actualOneBallTwoStrike = Application.getResultMessage(countResult1);
+        String actualOneBallOneStrike = Application.getResultMessage(countResult2);
+
+        //then
+        assertEquals(expectedOneBallTwoStrike, actualOneBallTwoStrike);
+        assertEquals(expectedOneBallOneStrike, actualOneBallOneStrike);
+    }
+
     @Override
     public void runMain() {
         Application.main(new String[]{});
