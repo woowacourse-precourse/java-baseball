72c06401ba50e5718ddc5d350da9cc69d3986ce5
ShinKwang <sklee0206@naver.com>
2022-11-08 17:16:15 +0900

    refactor: Change class of validation call
    검증 관련 메소드를 Validation 클래스로 나누었기 때문에, Application의 검증 관련 코드는 Validation을 통해서 호출하게끔 바꾼다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,8 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.*;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
@@ -20,7 +18,7 @@ public class Application {
                 Map<String, Integer> scoreTable = createScoreTable();
                 System.out.print("숫자를 입력해주세요 : ");
                 String userNumber = Console.readLine();
-                List<Integer> userNumberList = validateUserNumber(userNumber);
+                List<Integer> userNumberList = Validation.validateUserNumber(userNumber);
 
 
                 matchLists(userNumberList, computerNumberList, scoreTable);
@@ -33,7 +31,7 @@ public class Application {
 
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             restartNumber = Console.readLine();
-            validateRestartNumber(restartNumber);
+            Validation.validateRestartNumber(restartNumber);
         } while (restartNumber.equals("1"));
     }
 
@@ -78,64 +76,6 @@ public class Application {
         scoreTable.put("볼", 0);
 
         return scoreTable;
-    }
-
-    public static void validateRestartNumber(String restartNumber) {
-        validateDigit(restartNumber);
-        validateSize(restartNumber, 1);
-        validateRange(restartNumber, "^[1-2]*$");
-    }
-
-    public static List<Integer> validateUserNumber(String userNumbers) {
-        validateDigit(userNumbers);
-        validateSize(userNumbers, 3);
-        validateRange(userNumbers, "^[1-9]*$");
-        validateDuplication(userNumbers);
-
-        return stringToIntegerList(userNumbers);
-    }
-
-    private static void validateRange(String numbers, String regex) {
-
-        if (!Pattern.matches(regex, numbers)) {
-            throw new IllegalArgumentException("Number is out of range");
-        }
-    }
-
-    private static void validateDigit(String numbers) {
-        for (char number : numbers.toCharArray()) {
-            checkDigit(number);
-        }
-    }
-
-    private static void checkDigit(char number) {
-        if (!Character.isDigit(number)) {
-            throw new IllegalArgumentException("Number must be digit");
-        }
-    }
-
-
-    private static void validateSize(String numbers, int digitSize) {
-        if (numbers.length() != digitSize) {
-            throw new IllegalArgumentException("Number must be correct-digit size: " + digitSize);
-        }
-    }
-
-    private static void validateDuplication(String numbers) {
-        HashMap<Character, Integer> checkMap = new HashMap<>();
-
-        for (char number : numbers.toCharArray()) {
-            checkMap.put(number, checkMap.getOrDefault(number, 0) + 1);
-        }
-
-        if (checkMap.containsValue(2) || checkMap.containsValue(3)) {
-            throw new IllegalArgumentException("UserNumber has duplicate number");
-        }
-    }
-
-    private static List<Integer> stringToIntegerList(String numbers) {
-        String[] arr = numbers.split("");
-        return Arrays.stream(arr).map(Integer::parseInt).collect(Collectors.toList());
     }
 
     public static List<Integer> createComputerNumberList() {
