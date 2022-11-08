8a428463b527bdabcf0e383d3e2bac31b89bdf66
Laeng <Laeng@users.noreply.github.com>
2022-11-08 18:15:27 +0900

    feat(SelectNumberResult): 3스트라이크 여부를 반환할 수 있도록 메서드 구현

---
+++ src/main/java/baseball/view/SelectNumberResult.java
@@ -0,0 +1,27 @@
+package baseball.view;
+
+import baseball.service.GameService;
+import baseball.vo.Scoreboard;
+
+public class SelectNumberResult extends View {
+    private final int number;
+    private Boolean response = false;
+
+    public SelectNumberResult(int number) {
+        this.number = number;
+    }
+
+    @Override
+    public void show() {
+        GameService service = new GameService();
+        Scoreboard scoreboard = service.compareComputerNumberWith(this.number);
+        this.response = (scoreboard.getStrikePoint() == 3 && scoreboard.getBallPoint() == 0);
+
+        printLine(scoreboard.toString());
+    }
+
+    @Override
+    public boolean getResponseToBoolean() {
+        return this.response;
+    }
+}
