73451fd8c2766786df173b73fcaa25cb59a2af0f
JJ503 <63184334+JJ503@users.noreply.github.com>
2022-11-08 20:28:45 +0900

    refactor: 필요없는 빈 줄 제거

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -6,7 +6,6 @@ public class InputView {
     private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String ASK_RESET_GAME_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요. ";
 
-
     public String enterGameNumber() {
         System.out.print(INPUT_NUMBER_MESSAGE);
         return Console.readLine();
