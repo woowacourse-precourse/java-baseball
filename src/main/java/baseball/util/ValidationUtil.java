b583f07efadb5dfd0adbfb1ef56b5f2d79e258c1
the9kim <96895686+the9kim@users.noreply.github.com>
2022-11-08 13:53:19 +0900

    fix: 사용자 입력 값 검증 메서드 내 문자열 비  내용  수정
    - String 객체를 비교 연산자를 이용해 비교하는 오류 발견
    - char 타입으로 수정

--- src/main/java/baseball/util/ValidationUtil.java
+++ src/main/java/baseball/util/ValidationUtil.java
@@ -5,11 +5,11 @@ import baseball.controller.GameController;
 public class ValidationUtil {
 
     public static boolean validateInputNumber(String input) {
-        if (input.length()==3 && !input.contains("0")
+        if (input.length()==3 && (!input.contains("0"))
                 && validateIsDigit(input) && validateDuplicateNumber(input)) {
             return true;
         }
-        return false;
+        throw new IllegalArgumentException();
     }
 
     private static boolean validateIsDigit(String input) {
@@ -23,9 +23,9 @@ public class ValidationUtil {
     }
 
     private static boolean validateDuplicateNumber(String input){
-        String[] strArray = input.split("");
-        if (strArray[0].equals(strArray[1]) || strArray[0].equals(strArray[2])
-                || strArray[1].equals(strArray[2])){
+        char[] charArray = input.toCharArray();
+        if (charArray[0] == charArray[1] || charArray[0] == charArray[2]
+                || charArray[1] == charArray[2]){
             return false;
         }
         return true;
