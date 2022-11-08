61c1c609b187ca4e84bc4b80ffd068e2ea55b7f9
dingding-21 <phj8221@naver.com>
2022-11-08 16:26:22 +0900

    feat: strike, ball 개수로 게임 결과 반환하는 기능 추가

--- src/main/java/baseball/GameScore.java
+++ src/main/java/baseball/GameScore.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 import java.util.List;
 
 public class GameScore {
+    private static final String STRIKE = "스트라이크";
+    private static final String BALL = "볼";
+    private static final String NOTHING = "낫싱";
+
     public static List<Integer> getGameScore(List<Integer> numbers, List<Integer> computers) {
         int strike = 0;
         int ball = 0;
@@ -17,5 +21,16 @@ public class GameScore {
         }
 
         return Arrays.asList(ball, strike);
+    }
+
+    public static String getGameResult(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            return NOTHING;
+        } else if (ball != 0 && strike == 0) {
+            return ball + BALL;
+        } else if (ball == 0) {
+            return strike + STRIKE;
+        }
+        return ball + BALL + " " + strike + STRIKE;
     }
 }
