7a5e3216139cca951389b300a24341d8c66318cd
dlgosla <dlgoslaek1@gmail.com>
2022-11-08 18:21:56 +0900

    feat: 매칭 타입에 따라 해당 스코어를 1 증가 시키는 메소드 추가

--- src/main/java/baseball/Score.java
+++ src/main/java/baseball/Score.java
@@ -1,7 +1,11 @@
 package baseball;
 
 
+import java.util.List;
+
 public class Score {
+
+    enum matchingTypes {FAIL, STRIKE, BALL}
 
     private int maxScore;
     private int strike;
@@ -13,6 +17,29 @@ public class Score {
         this.ball = 0;
         this.fail = 0;
         this.maxScore = maxScore;
+    }
+
+    
+    private void increase(matchingTypes type) {
+        if (type == matchingTypes.FAIL) {
+            increaseFail();
+        } else if (type == matchingTypes.BALL) {
+            increaseBall();
+        } else if (type == matchingTypes.STRIKE) {
+            increaseStrike();
+        }
+    }
+
+    private void increaseStrike() {
+        this.strike++;
+    }
+
+    private void increaseBall() {
+        this.ball++;
+    }
+
+    private void increaseFail() {
+        this.fail++;
     }
 
 
