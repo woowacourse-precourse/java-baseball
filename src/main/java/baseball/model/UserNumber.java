06f409324d8897a4284d5f4171f2f68acd094677
Jeongthingamajig <netpetet@naver.com>
2022-11-08 20:47:16 +0900

    ReFactor : 메서드 변경
    userNumberList를 emptyList로 선언해서 nullpointerexception을 예방하려고 함

--- src/main/java/baseball/model/UserNumber.java
+++ src/main/java/baseball/model/UserNumber.java
@@ -1,20 +1,17 @@
 package baseball.model;
 
-import camp.nextstep.edu.missionutils.Console;
-
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-
-import static baseball.util.Message.SET_USER_INPUT_NUMBER_MESSAGE;
 
 /*
 유저가 입력한 값 받아서 유효성 검사 하고 반환 해주는 곳
  */
 public class UserNumber {
 
-    public List<Integer>  getUserNumber(String userNumber) {
+    public static List<Integer> getUserNumber(String userNumber) {
         int number = Integer.parseInt(userNumber);
-        List<Integer> userNumberList = new ArrayList<>();
+        List<Integer> userNumberList = new ArrayList<>(Collections.emptyList());
         while (number > 0){
             userNumberList.add(number % 10);
             number /= 10;
@@ -22,15 +19,10 @@ public class UserNumber {
         return userNumberList;
     }
 
-    public UserNumber() {
-        String userNumber = Console.readLine();
-        NumberCheckList.isIntegerCheck(userNumber);
-        NumberCheckList.isSizeCheck(userNumber);
-        NumberCheckList.isOverLapCheck(userNumber);
+    public UserNumber(String userInput) {
+        NumberCheckList.isIntegerCheck(userInput);
+        NumberCheckList.isSizeCheck(userInput);
+        NumberCheckList.isOverLapCheck(userInput);
     }
-    public static String getUserInput(){
-        System.out.println(SET_USER_INPUT_NUMBER_MESSAGE);
 
-        return Console.readLine();
-    }
 }
