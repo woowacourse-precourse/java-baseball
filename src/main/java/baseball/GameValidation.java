03e93472fa0821bb8c3d3ae7deee4a73eeaac4c4
Dabeen Jeong <hello70825@gmail.com>
2022-11-08 20:12:13 +0900

    feat: 입력값을 검증할 때, 입력값을 List로 변환하는 기능을 추가하다.

--- src/main/java/baseball/GameValidation.java
+++ src/main/java/baseball/GameValidation.java
@@ -1,12 +1,22 @@
 package baseball;
 
-import baseball.resource.AskRestartGame;
+import java.util.ArrayList;
+import java.util.List;
 
 public final class GameValidation {
     private String userInput;
+    private List<Character> userInputList;
 
     public void setUserInput(String userInput) {
         this.userInput = userInput;
+        ConvertUserInputToList();
     }
 
+    public void ConvertUserInputToList() {
+        userInputList = new ArrayList<>();
+
+        for(int i = 0; i < userInput.length(); i++) {
+            userInputList.add(userInput.charAt(i));
+        }
+    }
 }
