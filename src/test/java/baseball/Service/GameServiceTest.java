b70e76b8e7d3e7429192d999bc2e106e8e8e30e5
sjkim <seoungjoo01@gmail.com>
2022-11-08 09:17:23 +0900

    test(GameServiceTest): 게임서비스 힌트 관련 단위테스트 추가

--- src/test/java/baseball/Service/GameServiceTest.java
+++ src/test/java/baseball/Service/GameServiceTest.java
@@ -5,12 +5,14 @@ import baseball.Domain.Hint;
 import baseball.Domain.NumberGenerator;
 import baseball.Domain.Player;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GameServiceTest {
 
@@ -19,10 +21,12 @@ public class GameServiceTest {
     Hint hint = new Hint();
 
     GameService gameService = new GameService();
-
+    @BeforeEach
+    public void setUp(){
+        gameService.gameStart(com, player, hint);
+    }
     @Test
     public void 게임서비스_게임_시작_테스트() {
-        gameService.gameStart(com, player, hint);
         assertThat(gameService.getComputer().getClass()).isEqualTo(Computer.class);
         assertThat(gameService.getPlayer().getClass()).isEqualTo(Player.class);
         assertThat(gameService.getHint().getClass()).isEqualTo(Hint.class);
@@ -31,19 +35,32 @@ public class GameServiceTest {
 
     @Test
     public void 게임서비스_컴퓨터숫자_생성_테스트() {
-        gameService.gameStart(com, player, hint);
         gameService.makeComputerNum();
         assertEquals(gameService.getComputer().getGameNumbers().size(), NumberGenerator.GAME_NUM_LENGTH);
     }
 
     @Test
-    public void 게임서비스_정답확인_테스트() {
-        gameService.gameStart(com, player, hint);
-
+    public void 게임서비스_힌트확인_테스트() {
         gameService.getComputer().setGameNumbers(List.of(1, 7, 8));
         gameService.getPlayer().setPlayerGameNumbers(List.of(2, 7, 8));
         gameService.countResult();
         assertThat(gameService.getHint().getGameResult()).isEqualTo(List.of(2, 0));
+
+    }
+
+    @Test
+    public void 게임서비스_힌트확인_테스트2() {
+        gameService.getComputer().setGameNumbers(List.of(1, 2, 3));
+        gameService.getPlayer().setPlayerGameNumbers(List.of(3, 7, 8));
+        gameService.countResult();
+        assertThat(gameService.getHint().getGameResult()).isEqualTo(List.of(0,1));
+
+    }
+
+    @Test
+    public void 게임서비스_정답여부_테스트(){
+        gameService.getHint().setGameResult(List.of(3,0));
+        assertTrue(gameService.checkIsAnswer());
 
     }
 }
