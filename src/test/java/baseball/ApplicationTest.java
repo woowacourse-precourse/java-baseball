655d6a44210872d5991d47af0a4a5738f8e2a277
SONGmINjIJI <songminki18@gmail.com>
2022-11-08 22:41:02 +0900

    fix: 추가한 테스트 코드의 부족한 내용 추가

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -10,6 +10,19 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
     @Test
+    void 랜덤_난수_생성(){
+        // 개수, 각 수 1~9인지, 중복 여부 확인
+        // given
+        final Application te = new Application();
+        // when
+        final ArrayList<Integer> random_num = te.make_computer_number();
+        final HashSet<Integer> random_num_hashset = new HashSet<Integer>(random_num);
+        // then
+        assertThat(random_num.size()==3);
+        assertThat(random_num_hashset.size()==3);
+        assertThat(!random_num.contains(0));
+    }
+    @Test
     void 게임종료_후_재시작() {
         assertRandomNumberInRangeTest(
                 () -> {
