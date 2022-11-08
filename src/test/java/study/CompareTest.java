3fc6abfc4fbb980ccc2bd2c0571b68b31bff4302
Brilly-Bohyun <bohyun564@swu.ac.kr>
2022-11-08 13:05:27 +0900

    Test: howManyStrike 기능의 테스트 코드

--- src/test/java/study/CompareTest.java
+++ src/test/java/study/CompareTest.java
@@ -14,4 +14,10 @@ public class CompareTest {
     void count(){
         assertThat(3).isEqualTo(Application.howManySame(Arrays.asList(1,2,3), Arrays.asList(3,1,2)));
     }
+
+    @Test
+    @DisplayName("스트라이크 수 확인")
+    void countStrike(){
+        assertThat(2).isEqualTo(Application.howManyStrike(Arrays.asList(3, 1, 4), Arrays.asList(3, 2, 4)));
+    }
 }
