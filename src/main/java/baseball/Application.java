ce1baa65554e0c7e53edb7155950aa4e10dd847c
lvalentine6 <ysr1227@naver.com>
2022-11-08 21:10:02 +0900

    style : 코드 정렬

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -156,7 +156,7 @@ public class Application {
         } else {
             System.out.println(ball + "볼 " + strike + "스트라이크");
         }
-        if(strike == 3) {
+        if (strike == 3) {
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
             return true;
