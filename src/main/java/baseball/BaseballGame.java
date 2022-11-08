08311c8cff41ae11098031656563d2b3a23ec70b
iamsseaa <homingsea@gmail.com>
2022-11-08 17:00:41 +0900

    feat: 게임 진행 코드 구현

---
+++ src/main/java/baseball/BaseballGame.java
@@ -0,0 +1,15 @@
+package baseball;
+
+public class BaseballGame {
+
+    public static void Game(int[] target) {
+        boolean result = false;
+
+        do {
+            System.out.print("숫자를 입력해주세요: ");
+            int[] userNumber = User.inputNumber();
+
+            result = PrintResult.printResult(target, userNumber);
+        } while(result == false);
+    }
+}
