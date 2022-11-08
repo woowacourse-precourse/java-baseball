cb5ca1f27be18e1c9eaf2d2653b2cf5afb5f8d2b
jfavy <guitiar1296@gmail.com>
2022-11-08 20:50:43 +0900

    feat: add the ability to specify random values
    Add new import.
    Add a static method(computer) for specifying three random numbers.
    New static List computerNUM.
    
    BREAKING CHANGE: add new importcamp.nextstep.edu.missionutils.Randoms
    and camp.nextstep.edu.missionutils.Console

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
+	
+	static List<Integer> computerNUM = new ArrayList<>();
+	
+	
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    	
+    }
+    //랜덤 값 지정 camp.nextstep.edu.missionutils.Randoms 사용 
+    static int computer(int digit){
+    	int randomNUM = 0;
+    	while(computerNUM.size()<3) {
+    		randomNUM = pickNumberInRange(1,9);
+    		computerNUM.add(randomNUM);
+    	}
+    	return computerNUM.get(digit);
+    	//computer(0);
     }
 }
