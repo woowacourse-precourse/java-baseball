6de5ab058248bdee21d57165eba01a5e27e59393
이지영 <jioung93@naver.com>
2022-11-08 23:12:32 +0900

    [Message] feat: 낫싱 메세지 추가

--- src/main/java/baseball/constant/Message.java
+++ src/main/java/baseball/constant/Message.java
@@ -8,5 +8,6 @@ public final class Message {
     public static final String RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public static final String BALL = "볼";
     public static final String STRIKE = "스트라이크";
+    public static final String NOTHING = "낫싱";
 
 }
