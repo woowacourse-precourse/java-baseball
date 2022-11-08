a4b7572df1ed2a71f808ad0c534cddd0450ddf35
AhnJoonSung <tjdtna01@naver.com>
2022-11-08 14:37:22 +0900

    Edit Access Modifier in Game Class : static -> public

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -5,12 +5,12 @@ import camp.nextstep.edu.missionutils.Console;
 import java.util.List;
 
 public class Game {
-    public static void start() {
+    public void start() {
         System.out.println("숫자 야구 게임을 시작합니다.");
         running();
     }
 
-    static void running() {
+    public void running() {
         List<Integer> computer = Computer.createNewValue();
 
         while (true) {
@@ -30,7 +30,7 @@ public class Game {
         afterRound();
     }
 
-    static void afterRound() {
+    public void afterRound() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
         if (input.equals("1")) {
