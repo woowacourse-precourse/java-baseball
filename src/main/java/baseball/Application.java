35065312c8d65a3f849cfd2a193edd8e8afb9799
Umin <adorecamus@gmail.com>
2022-11-08 23:27:32 +0900

    feat: 스트라이크 갯수, 볼 갯수, 컴퓨터의 수와 게임 플레이어의 수 비교해 힌트 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -26,6 +26,11 @@ public class Application {
 
             List<Integer> playerNumber = player(numbers);
 
+            int strike = strike(computerNumber, playerNumber);
+
+            int ball = ball(computerNumber, playerNumber);
+
+            hint(strike, ball);
         }
     }
 
@@ -71,6 +76,43 @@ public class Application {
         }
 
         return player;
+    }
+
+    public static int strike(List<Integer> computerNumber, List<Integer> playerNumber) {
+        int strike = 0;
+
+        for (int i=0; i<3; i++) {
+            if (computerNumber.get(i) == playerNumber.get(i)) {
+                strike++;
+            }
+        }
+
+        return strike;
+    }
+
+    public static int ball(List<Integer> computerNumber, List<Integer> playerNumber) {
+        int ball = 0;
+
+        for (int i=0; i<3; i++) {
+            int index = computerNumber.indexOf(playerNumber.get(i));
+            if (index != i && index >= 0) {
+                ball++;
+            }
+        }
+
+        return ball;
+    }
+
+    public static void hint(int strike, int ball) {
+        if (strike > 0 && ball > 0) {
+            System.out.println(ball + "볼 " + strike + "스트라이크");
+        } else if (strike > 0) {
+            System.out.println(strike + "스트라이크");
+        } else if (ball > 0) {
+            System.out.println(ball + "볼");
+        } else {
+            System.out.println("낫싱");
+        }
     }
 
 }
