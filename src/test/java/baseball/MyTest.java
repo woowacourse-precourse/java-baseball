07c6ca8478839e56b35c722474838879303fadf8
unknown <dragontaek98@naver.com>
2022-11-08 19:20:20 +0900

    fix: random 세자리 수 생성 기능 test code 분리

--- src/test/java/baseball/MyTest.java
+++ src/test/java/baseball/MyTest.java
@@ -2,6 +2,7 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestClassOrder;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,23 +17,28 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class MyTest extends NsTest {
 
     @Test
-    void 랜덤_정답생성_테스트(){
+    void 랜덤_정답생성_데이터형_테스트(){
         Answer answer = new Answer();
         List<Integer> myAnswer = answer.getAnswer();
 
-        //check if it is number
         boolean isOnlyInteger = false;
         if (myAnswer.toString().matches(".*[0-9].*")){
             isOnlyInteger = true;
         }
 
-        //check if it is duplicated
+        assertThat(isOnlyInteger).isEqualTo(true);
+    }
+
+    @Test
+    void 랜덤_정답생성_중복_테스트(){
+        Answer answer = new Answer();
+        List<Integer> myAnswer = answer.getAnswer();
+
         int filteredSize = myAnswer.stream()
                 .distinct()
                 .collect(Collectors.toList())
                 .size();
 
-        assertThat(isOnlyInteger).isEqualTo(true);
         assertThat(filteredSize).isEqualTo(3);
     }
 
