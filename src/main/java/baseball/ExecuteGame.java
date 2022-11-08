eb46b0808475b054fffb0298646ac4ed3ee320d1
baksohyeon <bori12370@gmail.com>
2022-11-08 10:25:08 +0900

    feat: 게임 재시작을 결정하는 메서드에 input constructor 생성

--- src/main/java/baseball/ExecuteGame.java
+++ src/main/java/baseball/ExecuteGame.java
@@ -1,31 +1,34 @@
 package baseball;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class ExecuteGame {
 
     // 입력한 값을 String 형태로 반환
-    private String saveInput () {
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.next();
-        return input;
+//    private String saveInput () {
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.next();
+//        return input;
+//    }
+    private Scanner scanner;
+    private PrintStream ps;
+
+    public ExecuteGame(InputStream inputStream, PrintStream ps) {
+        this.scanner = new Scanner(inputStream);
+        this.ps = ps;
     }
 
     // 게임을 재시작할지 말지 묻는 메서드
-    private boolean canRepeatGame () {
-        while (true) {
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            String input = saveInput();
-
-            if (input.equals("1")) {
-                return true;
-            } else if (input.equals("2")) {
-                return false;
-            } else {
-                System.out.println("1 또는 2를 입력하세요.");
-            }
+    public boolean canRepeatGame() {
+        ps.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = scanner.nextLine();
+        if (input.equals("1")) {
+            return true;
+        } else if (input.equals("2")) {
+            return false;
         }
+        ps.println("1 또는 2를 입력해주세요");
+        return canRepeatGame();
     }
-
-
 }
