874f965e2d7dbd78d0628ebee5aabe643aeb1288
JJIaa <110366681+JJIaa@users.noreply.github.com>
2022-11-08 18:23:22 +0900

    refactor: 게임 재시작 혹은 완전 종료 코드 리팩토링

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -33,6 +33,10 @@ public class Application {
         CntBallAndStrike(computer, userNumberList);
 
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        gameRestartOrOver();
+    }
+
+    private static void gameRestartOrOver() throws IOException {
         Scanner options = new Scanner(System.in);
         int inputOption = options.nextInt();
         if (inputOption == 1) {
