91f2d5a51697e5463d8ddd3eae0f69674ffdbf92
hanuel <ezzeezz@naver.com>
2022-11-08 18:33:26 +0900

    feat: Add start game part

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -1,10 +1,16 @@
 package baseball;
 
 public class BaseBallGame {
+    public static final String GAME_START_MENT = "숫자 야구 게임을 시작합니다";
+
     public BaseBallGame() {
     }
 
     public void logic() throws IllegalArgumentException {
+        Computer computer = new Computer();
+        User user = new User();
 
+        System.out.println(GAME_START_MENT);
+        computer.createNumList();
     }
 }
