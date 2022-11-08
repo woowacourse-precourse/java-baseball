976b4395edf621c11dbf721203efbde6bfb079b4
masiljangajji <hjk172262@gmail.com>
2022-11-08 22:21:42 +0900

    feat: 게임 시작 메시지 출력

--- src/main/java/baseball/PrintMessage.java
+++ src/main/java/baseball/PrintMessage.java
@@ -1,2 +1,13 @@
-package baseball;public class PrintMessage {
+package baseball;
+
+public class PrintMessage {
+
+    public static void printStartBaseballGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public static void printInputBaseballNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
 }
