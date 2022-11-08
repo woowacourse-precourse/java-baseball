7ad1366551c12d13a3f4e8d3c83b7b82fcba01d6
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-08 19:20:37 +0900

    chore: clean up code

--- src/test/java/baseball/util/ListUtilTest.java
+++ src/test/java/baseball/util/ListUtilTest.java
@@ -8,26 +8,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ListUtilTest {
     ListUtil listUtil = new ListUtil();
+
     @Test
     void stringToIntegerList() {
         String input = "123";
         List<Integer> list = listUtil.stringToIntegerList(input);
 
-        assertEquals(list,List.of(1,2,3));
+        assertEquals(list, List.of(1, 2, 3));
     }
 
     @Test
     void countDiffLocationValues() {
-        List<Integer> userNumbers = List.of(1,2,3);
-        List<Integer> computerNumbers = List.of(3,2,1);
+        List<Integer> userNumbers = List.of(1, 2, 3);
+        List<Integer> computerNumbers = List.of(3, 2, 1);
 
         assertEquals(listUtil.countDiffLocationValues(userNumbers, computerNumbers), 2);
     }
 
     @Test
     void countSameLocationValues() {
-        List<Integer> userNumbers = List.of(1,2,3);
-        List<Integer> computerNumbers = List.of(3,2,1);
+        List<Integer> userNumbers = List.of(1, 2, 3);
+        List<Integer> computerNumbers = List.of(3, 2, 1);
 
         assertEquals(listUtil.countSameLocationValues(userNumbers, computerNumbers), 1);
     }
