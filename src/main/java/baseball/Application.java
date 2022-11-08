515a897c4fd5bce63db1d213c6d93d64ce4a1fb4
heohyeonjink <hyeonjin4870@naver.com>
2022-11-08 20:55:02 +0900

    feat(printEndMsg) : 숫자를 맞혔을 때 종료 메시지 출력

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -12,6 +12,7 @@ public class Application {
         printStartMsg();
         printInputMsg();
         String userInput = getInputNumber();
+
     }
 
     public static String getRandomNumber(){
@@ -71,5 +72,10 @@ public class Application {
             return true;
         }
         return false;
+    }
+
+    public static void printEndMsg(){
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
