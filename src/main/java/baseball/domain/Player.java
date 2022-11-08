3dc724222a4c924dc4d40d0a6991b74b6c5ef53f
yuntaeLee <leeyt1201@gmail.com>
2022-11-08 13:27:27 +0900

    feat: Player 숫자 범위 검증메서드 통합 구현

--- src/main/java/baseball/domain/Player.java
+++ src/main/java/baseball/domain/Player.java
@@ -2,10 +2,12 @@ package baseball.domain;
 
 import java.util.List;
 
+import static baseball.vo.ValidationMsg.NUMBER_RANGE_EXCEPTION;
+
 public class Player {
     private static final int MIN_NUMBER = 1;
     private static final int MAX_NUMBER = 9;
-    
+
     private final List<Integer> playerNumbers;
 
     public Player(List<Integer> playerNumbers) {
@@ -14,6 +16,12 @@ public class Player {
 
     public List<Integer> getPlayerNumbers() {
         return playerNumbers;
+    }
+
+    private void validateNumberRange(int number) {
+        if (validateIsLessThanMinNumber(number) || validateIsMoreThanMaxNumber(number)) {
+            throw new IllegalArgumentException(NUMBER_RANGE_EXCEPTION.getMessage());
+        }
     }
 
     private boolean validateIsLessThanMinNumber(int number) {
