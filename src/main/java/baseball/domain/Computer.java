d9260e7970facd9264e7c28f686fc6751a08dc5d
Beginner <dkstmdwn17@naver.com>
2022-11-08 20:08:41 +0900

    feat : 컴퓨터가 서로 다른 3개의 숫자를 뽑는 기능을 구현
    컴퓨터가 3개의 서로 다른 숫자를 뽑는 것을 chooseThreeNumbers()로 구현하였다.
    
    identifyDuplicateNumbers()는 랜덤인 숫자가 중복일 경우를 판단하여 중복인 경우 저장하지 않는다.
    
    이 외에 toString(), equals(), hashCode()를 작성하였다.

--- src/main/java/baseball/domain/Computer.java
+++ src/main/java/baseball/domain/Computer.java
@@ -1,4 +1,48 @@
 package baseball.domain;
+import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.Objects;
 public class Computer {
+    public Computer() {}
+
+    private final ArrayList<Integer> numbersChosenByTheComputer = new ArrayList<>();
+
+    public void chooseThreeNumbers(){
+        while(numbersChosenByTheComputer.size() < 3){
+            int randomNumber = Randoms.pickNumberInRange(1,9);
+            identifyDuplicateNumbers(randomNumber);
+        }
+    }
+
+    private void identifyDuplicateNumbers(int randomNumber) {
+        if(!numbersChosenByTheComputer.contains(randomNumber)){
+            numbersChosenByTheComputer.add(randomNumber);
+        }
+    }
+
+    public ArrayList<Integer> getNumbersChosenByTheComputer() {
+        return numbersChosenByTheComputer;
+    }
+
+    @Override
+    public String toString() {
+        return "컴퓨터가 고른 3개의 숫자 :"
+                + " [" + numbersChosenByTheComputer.get(0) + "]"
+                + " [" + numbersChosenByTheComputer.get(1) + "]"
+                + " [" + numbersChosenByTheComputer.get(2) + "]";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Computer computer = (Computer) o;
+        return Objects.equals(getNumbersChosenByTheComputer(), computer.getNumbersChosenByTheComputer());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNumbersChosenByTheComputer());
+    }
 }
