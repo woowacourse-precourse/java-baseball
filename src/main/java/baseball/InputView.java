d812e8bbb94babcc246595ab76c5e9e2ec961167
Cho-El <syzzang21c@gmail.com>
2022-11-08 11:32:00 +0900

    feat(InputView): 플레이어로부터 입력 받기
    1. 사용자 숫자 입력 받기 View
    2. 세자리 수 입력 받기
    3. 입력 받은 세자리 수 유효성 검사
    4. 재시작 입력 받기 View
    5. 게임 재시작 조건 입력 받기
    6. 재시작 조건 유효성 검사

---
+++ src/main/java/baseball/InputView.java
@@ -0,0 +1,91 @@
+package baseball;
+
+import java.util.*;
+
+public class InputView {
+    private static final String INPUT_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String SELECT_RESTART_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final int NUMBER_LENGTH = 3;
+
+    public static List<Integer> getUserInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(INPUT_NUMBER_MESSAGE);
+        String userInput = scanner.nextLine();
+        userInputValidationCheck(userInput);
+        return stringToList(userInput);
+    }
+
+    public static String restartGame(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(SELECT_RESTART_MESSAGE);
+        String userInput = scanner.nextLine();
+        userInputRestartValidationCheck(userInput);
+        return userInput;
+    }
+
+    public static void userInputRestartValidationCheck(String userInput) {
+        if (onlyOneOrTwo(userInput)) {
+            return;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public static void userInputValidationCheck(String userInput) {
+        if (overThreeLength(userInput) && isNumber(userInput) && haveNotZero(userInput) && notDuplicatedNumber(userInput)){
+            return ;
+        }
+        throw new IllegalArgumentException();
+    }
+
+    private static boolean onlyOneOrTwo(String userInput) {
+        for (int i = 0; i < userInput.length(); i++) {
+            if (userInput.charAt(i) == '1' || userInput.charAt(i) == '2'){
+                return true;
+            }
+        }
+        return false;
+    }
+    private static boolean overThreeLength(String userInput){ // 세자리 수가 넘는 경우
+        if (userInput.length() > 3){
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isNumber(String userInput){ // 숫자가 아닌 값이 있는 경우
+        for (int i = 0; i < userInput.length(); i++){
+            if (!Character.isDigit(userInput.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean notDuplicatedNumber(String userInput){ // 숫자가 중복이 되는 경우
+        Set<Character> inputSet = new HashSet<>();
+        for (int i = 0; i < userInput.length(); i++){
+            inputSet.add(userInput.charAt(i));
+        }
+        if (inputSet.size() == userInput.length()){
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean haveNotZero(String userInput){ // 문자 중 0이 있는 경우
+        for (int i = 0; i < userInput.length(); i++){
+            if ('0' == userInput.charAt(i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static List<Integer> stringToList(String target){
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < target.length(); i++){
+            result.add(Character.getNumericValue(target.charAt(i)));
+        }
+        return result;
+    }
+}
