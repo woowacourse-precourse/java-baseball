f766e9521ae749296c431005e595dc65b753ff78
idoburnish <doyun7433@naver.com>
2022-11-08 17:20:12 +0900

    feat(Score): 사용자의 점수를 관리하는 클래스
    사용자의 입력 값에 따른 스트라이크, 볼의 개수를 관리하는 클래스 선언

---
+++ src/main/java/baseball/Score.java
@@ -0,0 +1,22 @@
+package baseball;
+
+public class Score {
+    private int strike;
+    private int ball;
+
+    public int getStrike() {
+        return this.strike;
+    }
+
+    public int getBall() {
+        return this.ball;
+    }
+
+    public void setStrike(int strike) {
+        this.strike = strike;
+    }
+
+    public void setBall(int ball) {
+        this.ball = ball;
+    }
+}
