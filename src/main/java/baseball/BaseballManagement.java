6158d2eddea111dcf6d82742f9ff5e06374539e2
Woogie95 <choisw950928@gmail.com>
2022-11-08 19:37:23 +0900

    feat: Baseball 관리자 기능, 3스트라이크 확인 기능, 게임 재시작 기능 구현

---
+++ src/main/java/baseball/BaseballManagement.java
@@ -0,0 +1,35 @@
+package baseball;
+
+import java.util.List;
+
+public class BaseballManagement {
+
+    private final static int MAX_STRIKE_VALUE = 3;
+    private final static String REPLAY_GAME_NUMBER_VALUE = "1";
+
+    public void runBaseballGame() {
+        Output.printGameStartMessage();
+        boolean isRestart = true;
+        while (isRestart) {
+            List<Integer> computerRandomNumber = ComputerNumber.generateRandomNumber();
+            boolean isThreeStrike = true;
+            while (isThreeStrike) {
+                NumberCheck numberCheck = new NumberCheck();
+                UserNumber userNumber = new UserNumber(Input.inputNumber());
+                numberCheck.checkLocationNumbers(userNumber, computerRandomNumber);
+                ScoreConfirmation.confirm(numberCheck);
+                isThreeStrike = isMatchThreeStrike(numberCheck);
+            }
+            isRestart = isRestartGame();
+        }
+    }
+
+    private boolean isMatchThreeStrike(NumberCheck numberCheck) {
+        return numberCheck.getStrike() != MAX_STRIKE_VALUE;
+    }
+
+    private boolean isRestartGame() {
+        return Input.inputRestartNumber().equals(REPLAY_GAME_NUMBER_VALUE);
+    }
+
+}
