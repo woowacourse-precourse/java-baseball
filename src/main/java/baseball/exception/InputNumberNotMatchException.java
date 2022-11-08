e427def566d58702c1872e93e6bd53ca3acda991
we4sley <galaxy138742@gmail.com>
2022-11-08 22:20:02 +0900

    Revert "feat: 입력 값이 숫자가 아닌 경우에 대한 예외 구현"
    This reverts commit 908ce1186b56ae231e96fa3d1bcf3444e75ba824.

--- src/main/java/baseball/exception/InputNumberNotMatchException.java
+++
@@ -1,9 +0,0 @@
-package baseball.exception;
-
-public class InputNumberNotMatchException extends IllegalArgumentException {
-    private static final String MESSAGE = "입력 값의 자릿수가 일치하지 않습니다.";
-
-    public InputNumberNotMatchException() {
-        super(MESSAGE);
-    }
-}
