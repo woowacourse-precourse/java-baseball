8ec6d1e9af6537144a30f54c3943e396d648e29f
JuSeong1130 <jsmoon1130@naver.com>
2022-11-08 22:59:52 +0900

    feat(BaseballGame): 게임 종료 후 입력 값에 대한 유효성 검사 메서드 구현
    1과 2가 아닌 값이 입력되면 Exception이 발생되게 하였다.

--- src/main/java/baseball/game/BaseballGame.java
+++ src/main/java/baseball/game/BaseballGame.java
@@ -13,12 +13,14 @@ public class BaseballGame {
 	BaseballGameRule baseballGameRule;
 	boolean isGameRun = true;
 	private static final String GAME_WIN = Result.THREE_STRIKE_ZERO_BALL.getMessage();
+
+	private static final String GAME_RESTART = "1";
 	private static final String GAME_OVER = "2";
 
 	public BaseballGame() {
 		this.player = new Player();
 		this.computer = new Computer();
-		this.baseballGameRule = new BaseballGameRule(player,computer);
+		this.baseballGameRule = new BaseballGameRule(player, computer);
 	}
 
 	public void start() {
@@ -33,19 +35,28 @@ public class BaseballGame {
 			isEnd(result);
 		}
 	}
-	private void isEnd(String result){
-		if(result.equals(GAME_WIN)){
+
+	private void isEnd(String result) {
+		if (result.equals(GAME_WIN)) {
 			System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 			System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 			askRestart();
 		}
 	}
-	private void askRestart(){
+
+	private void askRestart() {
 		String askNumber = Input.insertNumber();
-		if(askNumber.equals(GAME_OVER)){
+		isRestartCode(askNumber);
+		if (askNumber.equals(GAME_OVER)) {
 			isGameRun = false;
 			return;
 		}
-		this.baseballGameRule = new BaseballGameRule(player,new Computer());
+		this.baseballGameRule = new BaseballGameRule(player, new Computer());
+	}
+
+	private void isRestartCode(String askNumber) {
+		if (!(askNumber.equals(GAME_OVER) || askNumber.equals(GAME_RESTART))) {
+			throw new IllegalArgumentException("올바른 값을 입력하세요(재시작 1, 종료 2)");
+		}
 	}
 }
