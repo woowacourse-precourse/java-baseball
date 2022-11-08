539087b2ae864cc9e9f25022bf2fa5c7866aea3b
iphuck22 <eldeshue@naver.com>
2022-11-08 15:40:03 +0900

    feat($Numberbaseball.java): add getAnswer, setAnswer method
    add 'getAnswer' and 'setAnswer' method to the Numberbaseball class
    - 'getAnswer' is to know the answer to test the features
    - 'setAnswer' is to modify the correct answer made by 'Randoms' library

--- src/main/java/baseball/NumberBaseball.java
+++ src/main/java/baseball/NumberBaseball.java
@@ -4,4 +4,15 @@ import java.util.List;
 
 public class NumberBaseball {
     private List<Integer> CorrectAnswer;
+
+    public List<Integer> getAnswer(){
+        return CorrectAnswer;
+    }
+
+    public void setAnswer(int first, int second, int third){
+        CorrectAnswer.set(0, first);
+        CorrectAnswer.set(1, second);
+        CorrectAnswer.set(2, third);
+    }
+
 }
