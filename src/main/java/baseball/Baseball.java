c3534333710384eba2e323b1bcf087cb46e3cbe3
박승민 <tmddudals369@naver.com>
2022-11-08 10:23:07 +0900

    refactor: Baseball 인스턴스 변수 클래스 변수로 변경
    인스턴스 변수가 가지고 있는 값들이 인스턴스를 생성할 때 마다 인스턴스에 할당할 필요가 없다

--- src/main/java/baseball/Baseball.java
+++ src/main/java/baseball/Baseball.java
@@ -9,26 +9,26 @@ import java.util.stream.Collectors;
 
 public class Baseball {
 
-    private final String ANSWER = "3스트라이크";
-    private final String BALL = "%d볼";
-    private final String BALL_AND_STRIKE = "%d볼 %d스트라이크";
-    private final String NOTHING = "낫싱";
-    private final String STRIKE = "%d스트라이크";
+    private static final String ANSWER = "3스트라이크";
+    private static final String BALL = "%d볼";
+    private static final String BALL_AND_STRIKE = "%d볼 %d스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final String STRIKE = "%d스트라이크";
 
-    private final String CLOSE_MESSAGE = "애플리케이션을 종료합니다.";
-    private final String CONGRATULATION_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private final String GAME_OPTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-    private final String GAME_OPTION_ERROR_MESSAGE = "1 또는 2를 입력하세요.";
-    private final String INPUT_USER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
-    private final String INPUT_USER_NUMBER_ERROR_MESSAGE = "1부터 9까지의 서로 다른 3자리의 양의 정수를 입력하세요.";
+    private static final String CLOSE_MESSAGE = "애플리케이션을 종료합니다.";
+    private static final String CONGRATULATION_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String GAME_OPTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String GAME_OPTION_ERROR_MESSAGE = "1 또는 2를 입력하세요.";
+    private static final String INPUT_USER_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String INPUT_USER_NUMBER_ERROR_MESSAGE = "1부터 9까지의 서로 다른 3자리의 양의 정수를 입력하세요.";
 
-    private final String RESTART_GAME = "1";
-    private final String GAME_OVER = "2";
+    private static final String RESTART_GAME = "1";
+    private static final String GAME_OVER = "2";
 
-    private final int START_VALUE = 1;
-    private final int END_VALUE = 9;
-    private final int NUMBER_COUNT = 3;
+    private static final int START_VALUE = 1;
+    private static final int END_VALUE = 9;
+    private static final int NUMBER_COUNT = 3;
 
     public void startGame() throws IllegalArgumentException {
         List<Integer> computerNumbers = createComputerNumbers();
