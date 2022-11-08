6779f06abf2038785886eb5df1c47f95d11473c3
yuntaeLee <leeyt1201@gmail.com>
2022-11-08 13:43:30 +0900

    feat: Score 생성자 구현

--- src/main/java/baseball/domain/Score.java
+++ src/main/java/baseball/domain/Score.java
@@ -1,4 +1,11 @@
 package baseball.domain;
 
 public class Score {
+    private int strike;
+    private int ball;
+
+    public Score(int strike, int ball) {
+        this.strike = strike;
+        this.ball = ball;
+    }
 }
