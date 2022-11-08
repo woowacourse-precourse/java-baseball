fef4ec0070b862e3590f8dcdc4b7de49d3659459
정석준 <tjrwns1010@naver.com>
2022-11-08 23:00:12 +0900

    feat: View 클래스 메시지 출력용 메서드 구현(스트라이크/볼 출력 메서드 제외)

--- src/main/java/baseball/View.java
+++ src/main/java/baseball/View.java
@@ -4,6 +4,26 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class View {
 
+    public void printInitMsg() {
+        String msg = "숫자 야구 게임을 시작합니다.";
+        System.out.println(msg);
+    }
+
+    public void printInputMsg() {
+        String msg = "숫자를 입력해주세요 : ";
+        System.out.print(msg);
+    }
+
+    public void printSuccessMsg() {
+        String msg = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+        System.out.println(msg);
+    }
+
+    public void printRestartMsg() {
+        String msg = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+        System.out.println(msg);
+    }
+
     private boolean validateUserResponse(String number) {
         if (number == null) {
             return false;
