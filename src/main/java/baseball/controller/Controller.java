7d9f345ea0b283f90c5b3114aeeb241d92f1f325
SHEOMM <esh0821@snu.ac.kr>
2022-11-08 13:20:32 +0900

    feat(Controller): implement Controller that control entire game
    전체 게임 플로우를 통제하는 Controller 클래스 구현

---
+++ src/main/java/baseball/controller/Controller.java
@@ -0,0 +1,67 @@
+package baseball.controller;
+
+import baseball.UI.HintView;
+import baseball.UI.InputView;
+import baseball.UI.ResultView;
+import baseball.model.Result;
+import baseball.model.State;
+import baseball.service.Service;
+
+public class Controller {
+    InputView inputView;
+    ResultView resultView;
+    HintView hintView;
+    Service service;
+    State state;
+
+    public Controller(){
+        state = State.PROCEEDING;
+        service = new Service();
+        inputView = new InputView();
+        resultView = new ResultView();
+        hintView = new HintView();
+    }
+    public void playGame(){
+        System.out.println("숫자 야구 게임을 시작합니다.");
+        while(isRestart()){
+            playGameOneCycle();
+        }
+    }
+
+    private void playGameOneCycle(){
+        sendInputToService();
+        sendToHintView();
+        if(state == State.FINISH){
+            getResult();
+            restartGame();
+        }
+
+    }
+
+    private void sendInputToService(){
+        inputView.getBaseballInput();
+        hintView.setHint(service.generateHint(inputView.getInput()));
+    }
+    private void sendToHintView(){
+        hintView.printHint();
+        if(hintView.checkWinningCondition()){
+            state = State.FINISH;
+        }
+    }
+    private void getResult(){
+        resultView.printResult(state);
+    }
+
+    private boolean isRestart(){
+        Result result = resultView.getResult();
+        return result == Result.RESTART;
+    }
+
+    private void restartGame(){
+        if(isRestart()){
+            state = State.PROCEEDING;
+            service.resetRandomAnswer();
+        }
+    }
+
+}
