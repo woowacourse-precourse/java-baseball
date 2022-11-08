72324d43cd6d15e75f80c7603539aa0d210838bd
Choi Seongwon <vlflalektzja@naver.com>
2022-11-08 13:41:59 +0900

    feat: 게임 재시작 무한루프 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,14 @@
 package baseball;
 
+
 public class Application {
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseballGame game = new BaseballGame();
+        while (true) {
+            if (game.run() == 2) break; // 1: 게임 재시작, 2: 게임 종료
+        }
+
     }
 }
