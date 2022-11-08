aed8ce2cbebadade0857d099a8b22e3cea24ad23
footdev <ekclstkfka5@gmail.com>
2022-11-08 15:59:27 +0900

    feat: generateRandomNumber 구현
    ThreeNumberGenerator 에게 조건에 맞는 3자리 정수를 생성하도록 요청하는 함수 작성

--- src/main/java/baseball/service/Rule.java
+++ src/main/java/baseball/service/Rule.java
@@ -1,4 +1,24 @@
 package baseball.service;
 
+import java.util.List;
+
 public class Rule {
+    private static final String STRIKE = "STRIKE";
+    private static final String BALL = "BALL";
+
+    private Player player;
+    private Computer computer;
+
+    public Rule() {
+        this.player = new Player();
+        this.computer = new Computer();
+    }
+
+    public void generateRandomNumber() {
+        computer.number = ThreeNumberGenerator.generate();
+    }
+
+    public List<Integer> decideStrikeOrBall() {
+
+    }
 }
