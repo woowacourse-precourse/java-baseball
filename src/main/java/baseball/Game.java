d93afc4d9b6e8628f5004dd26278b5426d128320
yoonsj970407 <yoonsj970407@gmail.com>
2022-11-08 22:19:18 +0900

    ✨Feat: init()과 play() 추가
    Game 클래스 안에 숫자 야구를 시작하기 위한 초기 값을 셋팅해주는 init()과 게임을 진행하는 play()를 추가하였음. 그리고 start() 삭제하였음

--- src/main/java/baseball/Game.java
+++ src/main/java/baseball/Game.java
@@ -2,18 +2,58 @@ package baseball;
 
 import java.util.List;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
 class Game {
-    List<Integer> computerNumber, userNumber;
+    List<Integer> userNumber, computerNumber;
+    User user = new User();
+    Computer computer = new Computer();
 
-    void start() {
-        Computer computer = new Computer();
-        this.computerNumber = computer.computerNumber();
-        User user = new User();
-
+    void init() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
         System.out.println("숫자를 입력해주세요 : ");
 
-        user.input(); user.exception();
-        this.userNumber = user.userNumber();
+        computerNumber = computer.computerNumber();
+
+        play();
+    }
+
+    void play() {
+        int restart = 1;
+
+        while(restart == 1) {
+            user.input();
+            userNumber = user.userNumber();
+            int strikeCount = strike(), ballCount = ball();
+
+            if(nothing()) {
+                System.out.println("낫싱");
+                System.out.println("숫자를 입력해주세요 : ");
+                continue;
+            }
+
+            if(strikeCount == 3) {
+                System.out.println(strikeCount + "스트라이크");
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+                restart = Integer.parseInt(readLine());
+            } else {
+                System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+                userNumber.clear();
+            }
+
+            if(restart != 1 && restart != 2) {
+                throw new IllegalArgumentException();
+            }
+
+            if(strikeCount == 3 && restart == 1) {
+                System.out.println("숫자를 입력해주세요 : ");
+
+                computerNumber.clear();
+                computerNumber = computer.computerNumber();
+            }
+        }
     }
 
     int strike() {
@@ -31,11 +71,14 @@ class Game {
     int ball() {
         int cnt = 0;
 
+
         if(computerNumber.get(0).equals(userNumber.get(1)) || computerNumber.get(0).equals(userNumber.get(2))) {
             cnt++;
-        } else if(computerNumber.get(1).equals(userNumber.get(0)) || computerNumber.get(1).equals(userNumber.get(2))) {
+        }
+        if(computerNumber.get(1).equals(userNumber.get(0)) || computerNumber.get(1).equals(userNumber.get(2))) {
             cnt++;
-        } else if(computerNumber.get(2).equals(userNumber.get(0)) || computerNumber.get(2).equals(userNumber.get(1))) {
+        }
+        if(computerNumber.get(2).equals(userNumber.get(0)) || computerNumber.get(2).equals(userNumber.get(1))) {
             cnt++;
         }
 
