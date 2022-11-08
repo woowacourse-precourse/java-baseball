ffbec7a9dc871d956fa01a23996ab031499a58e4
권도현 <tobeykwon@gmail.com>
2022-11-08 22:56:44 +0900

    fix : GameOutput 종료 메시지 수정

--- src/main/java/baseball/output/GameOutput.java
+++ src/main/java/baseball/output/GameOutput.java
@@ -6,7 +6,7 @@ public abstract class GameOutput {
 
     private static String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static String INPUT_USER_NUMBER_MESSAGE = "숫자를 입력해 주세요 : ";
-    private static String GAME_CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임종료";
+    private static String GAME_CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다!\n게임 종료";
     private static String RESTART_QUESTION_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static String NOTHING = "낫싱";
 
