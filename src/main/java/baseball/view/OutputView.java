a4be789c415aa2ac628d2691908e4e13f0217d3e
juno-junho <ssmm0205@naver.com>
2022-11-08 20:28:56 +0900

    feat(view): create printGameResult method
    게임의 결과를 출력해주는 printGameResult 메서드 생성

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,7 +3,19 @@ package baseball.view;
 import baseball.controller.BaseballGame;
 
 public class OutputView {
-    public OutputView(BaseballGame game) {
 
+    private final int STRIKE_COUNT;
+    private final int BALL_COUNT;
+
+    public OutputView(BaseballGame game) {
+        this.STRIKE_COUNT = game.STRIKE_COUNT;
+        this.BALL_COUNT = game.BALL_COUNT;
+    }
+
+    public void printGameResult() {
+        printBallCount();
+        printStrikeCount();
+        printBallAndStrike();
+        printNothing();
     }
 }
