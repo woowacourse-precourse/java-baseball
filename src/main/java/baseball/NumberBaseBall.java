7cf271edb08ea1ed7add7217fe35fd0e7d9480af
Vector1331 <89854207+Vector1331@users.noreply.github.com>
2022-11-08 18:58:42 +0900

    [Feat] 입력받은 숫자 자릿수 나눠서 예외 로직 체크 (IllegalArgumentException)

--- src/main/java/baseball/NumberBaseBall.java
+++ src/main/java/baseball/NumberBaseBall.java
@@ -9,8 +9,38 @@ public class NumberBaseBall {
              * player 숫자 입력받음 */
             int playerNum = Integer.parseInt(Console.readLine());
 
-
+            /**
+             * 자릿수 나누기 */
+            List<Integer> playerNumList = numberDigits(playerNum);
+            /**
+             * player 숫자가 로직에 벗어나는지체크
+             * */
+            NumberLogicCheck(playerNumList);
+            
 
         }
+    }
+    // 숫자 로직 체크
+    void NumberLogicCheck(List<Integer> list){
+        if (list.size() != 3) {
+            System.out.println("입력한 숫자의 자릿수를 확인하세요.");
+            throw new IllegalArgumentException();
+        }
+        if (list.size() != list.stream().distinct().count()){
+            System.out.println("중복되는 숫자를 확인하세요");
+            throw new IllegalArgumentException();
+        }
+    }
+
+    // 자릿수 나누기
+    List<Integer> numberDigits(int number) {
+        List<Integer> list = new ArrayList<>();
+        while (number > 0) {
+            int div = number % 10;
+            list.add(div);
+            number = number / 10;
+        }
+        Collections.reverse((list));
+        return list;
     }
 }
