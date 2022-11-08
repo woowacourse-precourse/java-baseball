ef8715cfb87dc326acf58a3e24757a1d34d4315e
Laeng <Laeng@users.noreply.github.com>
2022-11-08 18:45:12 +0900

    feat(GameService): deleteComputerNumber 메서드 파라미터 타입 변경

--- src/main/java/baseball/service/GameService.java
+++ src/main/java/baseball/service/GameService.java
@@ -4,6 +4,9 @@ import baseball.dao.ComputerNumber;
 import baseball.dto.Number;
 import baseball.vo.*;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class GameService {
     public Boolean addComputerNumber(int number) {
         Number dto = Number.getInstance();
@@ -11,15 +14,16 @@ public class GameService {
         return dto.insert(new ComputerNumber(number));
     }
 
-    public Boolean deleteComputerNumber(ComputerNumber computerNumber) {
+    public Boolean deleteComputerNumber(int number) {
         Number dto = Number.getInstance();
+        List<ComputerNumber> computerNumbers = dto.findByNumber(number);
 
-        return dto.delete(computerNumber);
+        return dto.deleteAll(computerNumbers);
     }
 
     public Scoreboard compareComputerNumberWith(int number) {
         Number dto = Number.getInstance();
-        ComputerNumber computerNumber = dto.first();
+        ComputerNumber computerNumber = dto.getOldest();
         TwoNumbers numbers = new TwoNumbers(computerNumber.getNumber(), number);
 
         return numbers.compare();
