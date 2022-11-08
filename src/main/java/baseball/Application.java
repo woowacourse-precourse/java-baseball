895af9b8f648432ddf7a7fda4ee4aa18d0b3a0e3
lhyeonj120 <isk04387@gmail.com>
2022-11-08 16:26:02 +0900

    feat: 랜덤 숫자 생성 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	private static List<Integer> computerNumber;
+
+	public static void main(String[] args) {
+
+	}
+
+	private static void createRandomNumber() {
+		Set<Integer> randomNumberSet = new LinkedHashSet<>();
+		while (randomNumberSet.size() < 3) {
+			int pickNumber = Randoms.pickNumberInRange(1, 9);
+			if (!randomNumberSet.contains(pickNumber)) {
+				randomNumberSet.add(pickNumber);
+			}
+		}
+		computerNumber = new ArrayList<>(randomNumberSet);
+	}
 }
