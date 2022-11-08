22376c51cc9bdffe5069b7a195651c420659a45a
oyoungsun <plusmate@naver.com>
2022-11-08 19:37:59 +0900

    fix : output 문장 고침

--- src/main/java/baseball/OutputHandler.java
+++ src/main/java/baseball/OutputHandler.java
@@ -7,19 +7,25 @@ public class OutputHandler {
     public void printGetUserNumber(){
         System.out.print("숫자를 입력해주세요 : ");
     }
-    public void Strike_andBall_result_print(int strike, int ball){
-        int sum = strike+ball;
-        if(strike==3 && ball==0){
-            System.out.println("3스트라이크\n" +
-                    "3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-        }
-        else if((sum) ==0){
-            System.out.println("낫싱");
-        }else if(sum <=3 && sum >0 )
-        System.out.println(ball+"볼 "+strike+"스트라이크");
-    }
     public void printRestart_orExit(){
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
-
+    public void Strike_andBall_result_print(int strike, int ball) {
+        if((ball+strike)==0){
+            System.out.println("낫싱");
+        }
+        //아래에는 모두 0보다 큰 경우다.
+        if(ball==0&&strike<=3){
+            System.out.println(strike+"스트라이크");
+            if(strike==3){
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            }
+        }
+        if(strike==0&&ball<3){
+            System.out.println(ball+"볼");
+        }
+        if(ball>0&&strike>0&&(strike+ball)<=3){
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
+    }
 }
