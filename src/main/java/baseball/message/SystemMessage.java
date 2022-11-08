897c2fe0ae18558d2d408f24b5c9d28fd62b1303
cocomozila <joseyun94@naver.com>
2022-11-08 14:03:19 +0900

    feat(SystemMessage): 시스템 재시작, 종료 원시값 포장

--- src/main/java/baseball/message/SystemMessage.java
+++ src/main/java/baseball/message/SystemMessage.java
@@ -8,5 +8,6 @@ public final class SystemMessage {
     public final static String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     public final static String GAME_CLEAR_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
     public final static String TRY_AGAIN_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
-
+    public final static String SYSTEM_RESTART = "1";
+    public final static String SYSTEM_SHUTDOWN = "2";
 }
