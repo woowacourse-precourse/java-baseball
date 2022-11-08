4f7dbb83915966a576e2dd5499806f07065a6594
Laeng <Laeng@users.noreply.github.com>
2022-11-08 23:42:25 +0900

    revert: 리버트를 위한 삭제

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,27 +4,10 @@ import baseball.controller.GameController;
 
 public class Application {
     public static void main(String[] args) {
-        boolean isPlayAgain = true;
-        GameController controller = new GameController();
 
-        //controller.showTutorial();
-        controller.addComputerNumbersForTest();
-
-        while(isPlayAgain) {
-            controller.addComputerNumber();
-            executeGame(controller);
-
-            String response = controller.showSelectPlayGameAgain();
-            isPlayAgain = controller.getPlayGameAgainResult(response);
-        }
     }
 
     private static void executeGame(GameController controller) {
-        boolean gameResult = false;
 
-        while (!gameResult) {
-            String gameNumber = controller.showPickThreeDigitNumber();
-            gameResult = controller.showSelectNumberResult(gameNumber);
-        }
     }
 }
