ffd6119ecc8864cc03bd7e2c891605923cc8c3dc
jihun-24k <gi020477@gmail.com>
2022-11-08 16:49:20 +0900

    Test: 게임 종료 후 사용자 입력값 테스트 코드 추가

--- src/test/java/baseball/InputExceptionTest.java
+++ src/test/java/baseball/InputExceptionTest.java
@@ -9,18 +9,26 @@ import org.junit.jupiter.params.provider.CsvSource;
 
 public class InputExceptionTest {
 
-    @DisplayName("사용자 입력값이 형식에 맞지않으면 예외가 발생한다.")
+    @DisplayName("게임 중 사용자 입력값이 형식에 맞지않으면 예외가 발생한다.")
     @CsvSource({"1234", "12", "가나", "sdg", "1가3"})
     @ParameterizedTest
-    void validInput(String input) {
+    void validGameInput(String input) {
         assertThatThrownBy(() -> InputException.validGameInput(input))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("사용자 입력값이 비어있으면 예외가 발생한다.")
+    @DisplayName("게임 중 사용자 입력값이 비어있으면 예외가 발생한다.")
     @Test
-    void validInput() {
+    void validGameInput() {
         assertThatThrownBy(() -> InputException.validGameInput(" "))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("게임 종료 후 사용자 입력값이 형식에 맞지않으면 예외가 발생한다.")
+    @CsvSource({"12", "가나", "sdg"})
+    @ParameterizedTest
+    void validRestartInput(String input) {
+        assertThatThrownBy(() -> InputException.validRestartInput(input))
             .isInstanceOf(IllegalArgumentException.class);
     }
 }
