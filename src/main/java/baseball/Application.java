0da032bafa841630c39afced7d6fc2c99b018ddd
YEJIN <yejin9858@sookmyung.ac.kr>
2022-11-08 17:00:55 +0900

    [FEAT] 컴퓨터가 정답 숫자를 뽑음

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+
+    private static List<Integer> computer= new ArrayList<>();
+    private static List<Integer> user = new ArrayList<>();
+    public static List<Integer> chooseNumByComputer(){
+        List<Integer> computer = new ArrayList<>();
+        while (computer.size() < 3) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+        return computer;
+    }
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        computer = chooseNumByComputer();
     }
 }
