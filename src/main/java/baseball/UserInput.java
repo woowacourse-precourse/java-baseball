a83ab2ee98d6f5b9e1c37c30321c185ff8e57d15
isieun0908 <supiya0908@naver.com>
2022-11-08 14:12:04 +0900

    Feat: 사용자 입력값 검사

--- src/main/java/baseball/UserInput.java
+++ src/main/java/baseball/UserInput.java
@@ -14,9 +14,33 @@ public class UserInput {
         Scanner sc = new Scanner(System.in);
         String inputString = sc.next();
 
+        if (check_input(inputString) == false) {
+            throw new IllegalArgumentException();
+        }
+
         for (String str : inputString.split("")) {
             this.number.add(Integer.parseInt(str));
         }
+    }
+
+    public boolean check_input(String input) {
+
+        // 서로 다른 세 자리 수인지 확인
+        if (input.length() != 3) {
+            return false;
+        }
+
+        if (input.charAt(0) == input.charAt(1) || input.charAt(0) == input.charAt(2) || input.charAt(1) == input.charAt(2)) {
+            return false;
+        }
+
+        for (int i=0; i<input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
     public List<Integer> getNumber() {
