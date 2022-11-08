db7e00107e4f17c7df1f4b5444e6059cb92d8c17
Gibeom Nam <rlqja0523@naver.com>
2022-11-08 22:57:09 +0900

    feat: Hint enum 생성

--- src/main/java/baseball/game/GameService.java
+++ src/main/java/baseball/game/GameService.java
@@ -8,4 +8,19 @@ public class GameService {
     private Computer computer = new Computer();
     private User user = new User();
     private NumberJudgment numberJudgment = new NumberJudgment();
+
+    public enum Hint {
+        STRIKE("스트라이크"),
+        BALL("볼"),
+        NOTHING("낫싱");
+
+        private String name;
+        Hint(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
 }
