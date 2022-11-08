649d33b6c2fde096b2dd3fcd1d99b59e7d6fa7d0
ddukddaki <rlfrkdms1@naver.com>
2022-11-08 15:08:02 +0900

    feat(Hint): 스트라이크인지 확인하는 기능

--- src/main/java/baseball/Hint.java
+++ src/main/java/baseball/Hint.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import java.util.List;
+
 public class Hint {
 
     private int strikeCount;
@@ -9,5 +11,12 @@ public class Hint {
         strikeCount = 0;
         ballCount = 0;
     }
-    
+
+    public boolean isStrike(List<Integer> computerNumber, int userNumber, int index) {
+        if (computerNumber.get(index) == userNumber) {
+            return true;
+        }
+        return false;
+    }
+
 }
