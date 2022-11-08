27bb443467bd839384eda6278001fa19de8076e7
moonn6pence <shin421179@gmail.com>
2022-11-08 13:13:04 +0900

    feat : 게임 진행
     Application의 main에서 do-while문으로 Game 클래스의 start, replay 진행

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,10 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Game game = new Game();
+
+        do {
+            game.start();
+        } while (game.replay());
     }
 }
