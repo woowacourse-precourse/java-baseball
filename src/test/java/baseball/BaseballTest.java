963e653cbe716ab657850f5b6f5a42512726d71f
James <rlagurwo6209@naver.com>
2022-11-08 20:55:55 +0900

    [test]:기능리스트3_테스트케이스구현

--- src/test/java/baseball/BaseballTest.java
+++ src/test/java/baseball/BaseballTest.java
@@ -1,16 +1,50 @@
 package baseball;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 
 public class BaseballTest {
-
-    @Test
-    void 기능리스트_2_랜덤숫자_생성시_3자리확인(){
-        Integer RandomNumberLength = Application.getAnswerRandomNumber().size();
-        assertThat(RandomNumberLength).isEqualTo(3);
+    @Nested
+    class FunctionList2{
+        @Test
+        void 랜덤숫자_생성시_3자리확인(){
+            Integer RandomNumberLength = Application.getAnswerRandomNumber().size();
+            assertThat(RandomNumberLength).isEqualTo(3);
+        }
+    }
+    @Nested
+    class FunctionList3{
+        @Test
+        void 숫자입력_테스트_정상1(){
+            String inputString_Case1 = "123"; // 정상입력
+            List<Integer> inputStringList = Application.getListByInputNumber(inputString_Case1);
+            List<Integer> exNumberList = List.of(1,2,3);
+            assertThat(exNumberList).isEqualTo(inputStringList);
+        }
+        @Test
+        void 숫자입력_테스트_비정상1(){
+            String inputString_Case2 = "abc"; // 숫자가 아닌 자료형 입력
+            assertThatThrownBy(() -> Application.getListByInputNumber(inputString_Case2))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+        @Test
+        void 숫자입력_테스트_비정상2(){
+            String inputString_Case3 = "4567"; // 숫자이지만 3자리가 아닌 입력
+            assertThatThrownBy(() -> Application.getListByInputNumber(inputString_Case3))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+        @Test
+        void 숫자입력_테스트_비정상3(){
+            String inputString_Case4 = "111"; // 숫자이고 3자리이지만 중복숫자 입력
+            assertThatThrownBy(() -> Application.getListByInputNumber(inputString_Case4))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
 }
