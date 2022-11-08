47964dae45d50b3c01637deb91d3091a21ff38f4
parkjiyun <pjyun05200@gmail.com>
2022-11-08 17:26:37 +0900

    update makeNumList() function

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,5 +1,7 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,10 +18,16 @@ class BaseballGame {
         initalizeBall();
     };
 
+    // 서로 다른 3자리의 수 생성
     public void makeNumList() {
-
+        while(num_list.size() < 3) {
+            int num = Randoms.pickNumberInRange(1, 9);
+            if(!num_list.contains(num))
+                num_list.add(num);
+        }
     }
 
+    // ball과 strike count를 0으로 초기화
     public void initalizeBall() {
         int ballCount = 0;
         int strikeCount = 0;
