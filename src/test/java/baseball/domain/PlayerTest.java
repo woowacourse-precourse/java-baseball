0bd47488ec8b0d5c2d1ea4321caced103365cbd1
yuntaeLee <leeyt1201@gmail.com>
2022-11-08 13:39:19 +0900

    test: Player 숫자 범위 테스트

---
+++ src/test/java/baseball/domain/PlayerTest.java
@@ -0,0 +1,21 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    @Test
+    void validateNumberRangeTest() {
+        List<Integer> playerNumbers = Arrays.asList(10, 1, 2);
+        assertThatThrownBy(() -> new Player(playerNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+}
