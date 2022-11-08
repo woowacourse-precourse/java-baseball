5d307997b936990b221f08cfe5906a4016dfe407
hdsung98 <hdsung98@gmail.com>
2022-11-08 23:24:46 +0900

    fix: fix output format error
    볼 스트라이크 둘 중 하나만 존재하는 경우에 대한 출력은 개행을 하지 못하는 문제 해결.
    조건 분기를 메소드가 아닌 constructor 내부로 집어 넣고 조건을 추가했다.

--- src/main/java/baseball/ResultBoard.java
+++ src/main/java/baseball/ResultBoard.java
@@ -2,6 +2,7 @@ package baseball;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class ResultBoard {
 
@@ -25,6 +26,13 @@ public class ResultBoard {
             strikeResult = strikeResult.concat("스트라이크");
             totalResult.add(strikeResult);
         }
+        if(ball!=0&& strike!=0){
+            List<String> space = List.of(" ");
+            totalResult.addAll(1,space);
+        }
+        if(ball!=0 || strike!=0) {
+            totalResult.add("\n");
+        }
     }
 
     public void showResult() {
@@ -32,15 +40,8 @@ public class ResultBoard {
             System.out.println("낫싱");
             return;
         }
-
         for (String result : totalResult) {
             System.out.print(result);
-            if (totalResult.indexOf(result) == 0) {
-                System.out.print(" ");
-            }
-            if (totalResult.indexOf(result) == 1) {
-                System.out.print("\n");
-            }
         }
     }
 }
