8186bf78b0f14a5c458e9ecfc8bc570f1642d6ee
Noah <shgustmd6865@naver.com>
2022-11-08 23:01:52 +0900

    Refactor(Game) : 기존 인스턴스를 가진 것을 수정하여 서비스로 분리

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,21 +1,43 @@
 package baseball.domain;
 
+import baseball.dto.MessageDTO;
+import baseball.dto.ScoreDTO;
+import baseball.service.GameService;
+import baseball.util.MessageMaker;
+import camp.nextstep.edu.missionutils.Console;
+
 public class Game {
 
-    private Computer computer;
-    private User user;
+    private Boolean isEnd;
+    private Boolean isNew;
+    private final GameService gameService;
+    private final MessageMaker messageMaker;
 
     public Game() {
-        this.computer = new Computer();
-        this.user = new User();
+        this.isEnd = false;
+        this.isNew = false;
+        this.gameService = new GameService();
+        this.messageMaker = new MessageMaker();
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
-    public Computer getComputer() {
-        return this.computer;
-    }
-
-    public User getUser() {
-        return this.user;
+    public void play() {
+        while(!isEnd) {
+            System.out.print("숫자를 입력해주세요 : ");
+            String input = Console.readLine();
+            ScoreDTO scoreDTO = gameService.calculate(input, isNew);
+            MessageDTO messageDTO = messageMaker.makeMessage(scoreDTO);
+            System.out.println(messageDTO.getMessage());
+            if (messageDTO.getIsOver()) {
+                System.out.println(messageMaker.makeEndMessage());
+                String endInput = Console.readLine();
+                if (endInput.equals("2"))
+                    isEnd = true;
+                else
+                    isNew = true;
+            }
+            else
+                isNew = false;
+        }
     }
 }
