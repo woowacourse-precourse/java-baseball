9534c9112c060af3506be776b797c9431d316301
Jeeseob <jeeseob5761@gmail.com>
2022-11-08 15:29:33 +0900

    feat: 메인 함수에 게임 실행부 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,5 +3,15 @@ package baseball;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        BaseBallGame baseBallGame = new BaseBallGame();
+        do {
+            // TODO: 게임 시작
+            baseBallGame.start();
+        } while (isRestart());
+    }
+
+    private static boolean isRestart() {
+        // TODO: 게임 재시작 여부 판단
+        return false;
     }
 }
