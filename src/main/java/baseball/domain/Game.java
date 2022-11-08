ec84150466cd18ffdcfb7c5fa042077befd22714
mikekang47 <gyeong0308@gmail.com>
2022-11-08 22:23:11 +0900

    Feat: 게임 도메인 생성

---
+++ src/main/java/baseball/domain/Game.java
@@ -0,0 +1,15 @@
+package baseball.domain;
+
+public class Game {
+    private int strike;
+    private int ball;
+
+    public int getStrike() {
+        return strike;
+    }
+
+    public int getBall() {
+        return ball;
+    }
+
+}
