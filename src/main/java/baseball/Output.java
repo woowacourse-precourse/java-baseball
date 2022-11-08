14df19f713fe31276faa1d775bd3e5169eb97b5d
noparamin <noparamin@naver.com>
2022-11-08 19:51:20 +0900

    style(Output): 코드 포매팅 정렬

--- src/main/java/baseball/Output.java
+++ src/main/java/baseball/Output.java
@@ -13,25 +13,25 @@ public class Output {
     }
 
     public void printStrikeOnly(int strike, int ball) {
-        if(strike > 0 && ball == 0) {
+        if (strike > 0 && ball == 0) {
             System.out.println(String.format("%d스트라이크", strike));
         }
     }
 
     public void printBallOnly(int strike, int ball) {
-        if(strike == 0 && ball > 0) {
+        if (strike == 0 && ball > 0) {
             System.out.println(String.format("%d볼", ball));
         }
     }
 
     public void printBallAndStrike(int strike, int ball) {
-        if(strike > 0 && ball > 0) {
+        if (strike > 0 && ball > 0) {
             System.out.println(String.format("%d볼 %d스트라이크", ball, strike));
         }
     }
 
     public void printNothing(int strike, int ball) {
-        if(strike == 0 && ball == 0) {
+        if (strike == 0 && ball == 0) {
             System.out.println("낫싱");
         }
     }
