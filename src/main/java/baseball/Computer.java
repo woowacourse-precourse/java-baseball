21a809c03e83576b1062711a142f987eefe82198
2do1 <dowon3036@naver.com>
2022-11-08 22:50:32 +0900

    feat: 사용자 서로 다른 3자리의 수 입력 기능 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -1,23 +1,24 @@
-package baseball;
+package computer;
 
 import java.util.ArrayList;
 import java.util.List;
+
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Computer {
 
-    private List<Integer> randomNumbers;
+    static List<Integer> randomNumbers;
 
     static final int MAX_SIZE = 3;
     static final int MIN_NUM = 1;
     static final int MAX_NUM = 9;
 
-    public void resetRandomNumbers() {
+    public static void resetRandomNumbers() {
         randomNumbers = new ArrayList<>();
     }
 
-    public List<Integer> pickRandomNumbers() {
-
+    public static List<Integer> pickRandomNumbers() {
+        resetRandomNumbers();
         while (randomNumbers.size() < MAX_SIZE) {
             int randomNum = Randoms.pickNumberInRange(MIN_NUM, MAX_NUM);
             if (!randomNumbers.contains(randomNum)) {
---
+++ src/main/java/baseball/User.java
@@ -0,0 +1,36 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class User {
+
+    static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+
+    private String inputNum;
+    private List<Integer> userNumbers;
+
+    public List<Integer> inputNumbers() {
+        System.out.println(INPUT_MESSAGE);
+        resetUserNumbers();
+        inputNum = Console.readLine();
+        toList(inputNum);
+
+        return userNumbers;
+    }
+
+    public void resetUserNumbers() {
+        userNumbers = new ArrayList<>();
+    }
+
+    public List<Integer> toList(String inputNum) {
+        for (int index = 0; index < inputNum.length(); index++) {
+            userNumbers.add(inputNum.charAt(index) - '0');
+        }
+        return userNumbers;
+    }
+}
