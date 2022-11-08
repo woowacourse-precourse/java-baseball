dd5fc451271d3e2cb634c5cf13c5c11008de8b06
shoeone96 <dlwnddnjs96@naver.com>
2022-11-08 16:10:48 +0900

    chore(test): test 메서드 생성

---
+++ src/test/java/baseball/ErrorCheckingTest.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ErrorCheckingTest {
+
+    @Test
+    void errorChecking() {
+    }
+
+    @Test
+    void checkingInputNum() {
+    }
+
+    @Test
+    void checkingDigit() {
+    }
+
+    @Test
+    void checkingContaingZero() {
+    }
+}
---
+++ src/test/java/baseball/GameTest.java
@@ -0,0 +1,36 @@
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameTest {
+
+    @Test
+    void gameInit() {
+    }
+
+    @Test
+    void makingRandomNum() {
+    }
+
+    @Test
+    void inputNum() {
+    }
+
+    @Test
+    void comparingNum() {
+    }
+
+    @Test
+    void checkingStrike() {
+    }
+
+    @Test
+    void checkingBall() {
+    }
+
+    @Test
+    void keepGoing() {
+    }
+}
