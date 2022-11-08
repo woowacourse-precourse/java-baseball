f57652f192b93379ada85abb6dd788805083ff0a
MJoon-Jung <gjgjajaj31@gmail.com>
2022-11-08 11:33:56 +0900

    fix<GameEndInput>: 검증 함수 수정
    - validate 함수 가독성을 높이기 위해 try catch 삭제
    - hasNumber로 분리하여 string integer로 바꿨을 때 NumberFormatException이 터지면 false 반환
    - isBlank를 추가하고 INPUT_LENGTH 상수를 만들어 길이 비교

--- src/main/java/baseball/util/input/GameEndInput.java
+++ src/main/java/baseball/util/input/GameEndInput.java
@@ -6,23 +6,34 @@ public class GameEndInput {
     private final int gameEndInput;
     private final int CONTINUE_GAME = 1;
     private final int EXIT_GAME = 2;
+    private final int INPUT_LENGTH = 1;
 
     public GameEndInput(String gameEndInput) {
         validate(gameEndInput);
         this.gameEndInput = Integer.parseInt(gameEndInput);
     }
 
-    public void validate(String continueInput) {
-        if (continueInput.length() > 1) {
-            throw new IllegalArgumentException(GameExceptionMessage.ENTER_CONTINUE_NUMBER_OR_EXIT_NUMBER);
+    public void validate(String gameEndInput) {
+        if (supportsInputLength(gameEndInput) &&
+                hasNumber(gameEndInput) &&
+                continueOrExit(Integer.parseInt(gameEndInput))
+        ) {
+            return;
         }
+        throw new IllegalArgumentException(GameExceptionMessage.ENTER_CONTINUE_NUMBER_OR_EXIT_NUMBER);
+    }
+
+    private boolean hasNumber(String gameEndInput) {
         try {
-            if (!continueOrExit(Integer.parseInt(continueInput))) {
-                throw new IllegalArgumentException(GameExceptionMessage.ENTER_CONTINUE_NUMBER_OR_EXIT_NUMBER);
-            }
+            Integer.parseInt(gameEndInput);
         } catch (NumberFormatException e) {
-            throw new IllegalArgumentException(GameExceptionMessage.ENTER_CONTINUE_NUMBER_OR_EXIT_NUMBER);
+            return false;
         }
+        return true;
+    }
+
+    private boolean supportsInputLength(String gameEndInput) {
+        return !gameEndInput.isBlank() && gameEndInput.length() == INPUT_LENGTH;
     }
 
     private boolean continueOrExit(int gameEndInput) {
