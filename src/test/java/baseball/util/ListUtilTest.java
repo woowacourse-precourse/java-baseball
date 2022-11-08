7d0dd9d1bf3a5c9815a4bd9eb5433d05157f3c61
hyunkoes <201702083@o.cnu.ac.kr>
2022-11-08 18:32:43 +0900

    test: ListUtil test success

---
+++ src/test/java/baseball/util/ListUtilTest.java
@@ -0,0 +1,34 @@
+package baseball.util;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListUtilTest {
+    ListUtil listUtil = new ListUtil();
+    @Test
+    void stringToIntegerList() {
+        String input = "123";
+        List<Integer> list = listUtil.stringToIntegerList(input);
+
+        assertEquals(list,List.of(1,2,3));
+    }
+
+    @Test
+    void countDiffLocationValues() {
+        List<Integer> userNumbers = List.of(1,2,3);
+        List<Integer> computerNumbers = List.of(3,2,1);
+
+        assertEquals(listUtil.countDiffLocationValues(userNumbers, computerNumbers), 2);
+    }
+
+    @Test
+    void countSameLocationValues() {
+        List<Integer> userNumbers = List.of(1,2,3);
+        List<Integer> computerNumbers = List.of(3,2,1);
+
+        assertEquals(listUtil.countSameLocationValues(userNumbers, computerNumbers), 1);
+    }
+}
