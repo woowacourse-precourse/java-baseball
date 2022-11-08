bf36971ddf04dd1e7c91209f6e0d063fb4fda9c2
ch200203 <ch200205@naver.com>
2022-11-08 21:42:30 +0900

    fix : google 코드 컨벤션 적용

--- src/main/java/baseball/domain/UserBaseBall.java
+++ src/main/java/baseball/domain/UserBaseBall.java
@@ -15,31 +15,37 @@ public class UserBaseBall {
     }
 
     public void setUserBallNumber(String userBallNumber) {
+
         validateNumber(userBallNumber);
         this.userBallNumber = userBallNumber;
     }
 
 
     public void validateNumber(String userBallNumber) {
+
         if (numberRange(userBallNumber)) {
             throw new IllegalArgumentException("숫자의 입력범위를 초과하였습니다.");
         } else if (numberLength(userBallNumber)) {
-            throw new IllegalArgumentException("야구공 입력 개수를 초과하였습니다.");
+            throw new IllegalArgumentException("야구공 입력 개수를 만족하지 못 하였습니다.");
         } else if (numberDuplicate(userBallNumber)) {
             throw new IllegalArgumentException("중복된 숫자는 입력 할 수 없습니다.");
-        } else if(numberIsDigit(userBallNumber)) {
+        } else if (numberIsDigit(userBallNumber)) {
             throw new IllegalArgumentException("숫자만 입력 하실 수 있습니다.");
         }
     }
 
     private boolean numberIsDigit(String numbers) {
-        for (Character number : numbers.toCharArray() ) {
-            if(!Character.isDigit(number)) return true;
+
+        for (Character number : numbers.toCharArray()) {
+            if (!Character.isDigit(number)) {
+                return true;
+            }
         }
         return false;
     }
 
     private boolean numberRange(String numbers) {
+
         for (int i = 0; i < numbers.length(); i++) {
             int number = Character.getNumericValue(numbers.charAt(i));
             if (number < MIN_NUM || number > MAX_NUM) {
@@ -66,7 +72,7 @@ public class UserBaseBall {
     }
 
     private boolean numberLength(String numbers) {
-        if (numbers.length() > 3) {
+        if (numbers.length() != 3) {
             return true;
         } else {
             return false;
