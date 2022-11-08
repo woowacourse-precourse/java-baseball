ca12e50c9f1dabcff187cc717808a2229aee453f
ohhonggi <5ghdrl@naver.com>
2022-11-08 19:57:19 +0900

    feat($BaseballGame): Implement restart function
    This function starts the new number baseball game if
    user wants to restart the number baseball game.
    
    This function uses a function from the Validator class
    - validRestartInput

--- src/main/java/baseball/service/BaseballGame.java
+++ src/main/java/baseball/service/BaseballGame.java
@@ -2,8 +2,10 @@ package baseball.service;
 
 import baseball.model.Computer;
 import baseball.model.User;
+import camp.nextstep.edu.missionutils.Console;
 
 import static baseball.service.Comparator.compareInputAndRandomNumber;
+import static baseball.service.Validator.validRestartInput;
 
 public class BaseballGame {
 
@@ -18,4 +20,16 @@ public class BaseballGame {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 
+    public static boolean restart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        validRestartInput(input);
+
+        int play = Integer.parseInt(input);
+        if (play == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
