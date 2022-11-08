00dbff57b6ec5d7b94addf090357e56981f7c037
Songyi Kim <ksl2950@naver.com>
2022-11-08 10:32:13 +0900

    fix: Player Test 대상 메서드 변경 및 command 메서드 추가
    - 인자로 값을 받는 private method 테스트에서 input을 직접 받는 public method 테스트로 변경 
    - input을 위한 command 메서드 추가

--- src/test/java/baseball/PlayerTest.java
+++ src/test/java/baseball/PlayerTest.java
@@ -3,10 +3,13 @@ package baseball;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class PlayerTest {
 
@@ -18,79 +21,72 @@ class PlayerTest {
     }
 
     @Test
-    void validateIsThreeNumber_메서드_사용시_서로다른_3자리숫자_입력한_경우_정상종료() throws Exception {
+    void enterNumber_메서드로_서로다른_3자리숫자_입력한_경우_정상리턴() {
         // given
         String playerInput = "123";
-        Method validateMethod1 = player.getClass().getDeclaredMethod("validateIsThreeNumber", String.class);
-        Method validateMethod2 = player.getClass().getDeclaredMethod("validateIsUniqueNumber", String.class);
-
-        validateMethod1.setAccessible(true);
-        validateMethod2.setAccessible(true);
-
-        // when, then
-        validateMethod1.invoke(player, playerInput);
-        validateMethod2.invoke(player, playerInput);
+        List<Integer> expectedList = Arrays.stream(playerInput.split(""))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        // when
+        command(playerInput);
+        List<Integer> integerList = player.enterNumber();
+        // then
+        assertThat(integerList).isEqualTo(expectedList);
     }
 
     @Test
-    void validateIsThreeNumber_메서드_사용시_숫자가_아닌값을_입력한경우_예외발생() throws Exception {
+    void enterNumber_메서드로_숫자가_아닌값을_입력한경우_예외발생() {
         // given
         String playerInput = "가나다";
-        Method method = player.getClass().getDeclaredMethod("validateIsThreeNumber", String.class);
-        method.setAccessible(true);
         // when, then
-        assertThrows(InvocationTargetException.class, () -> method.invoke(player, playerInput));
+        command(playerInput);
+        assertThatThrownBy(player::enterNumber).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void validateIsThreeNumber_메서드_사용시_세자리_숫자를_입력하지_않은_경우_예외발생() throws Exception {
-        // given
-        String playerInput = "12";
-        Method method = player.getClass().getDeclaredMethod("validateIsThreeNumber", String.class);
-        method.setAccessible(true);
-        // when, then
-        assertThrows(InvocationTargetException.class, () -> method.invoke(player, playerInput));
-    }
-
-    @Test
-    void validateIsUniqueNumber_메서드_사용시_중복된_숫자를_입력한경우_예외발생() throws Exception {
+    void enterNumber_메서드로_중복된_숫자를_입력한경우_예외발생() {
         // given
         String playerInput = "112";
-        Method method = player.getClass().getDeclaredMethod("validateIsUniqueNumber", String.class);
-        method.setAccessible(true);
         // when, then
-        assertThrows(InvocationTargetException.class, () -> method.invoke(player, playerInput));
+        command(playerInput);
+        assertThatThrownBy(player::enterNumber).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void validateIsRestartNumber_메서드_사용시_입력값이_1인경우_정상_종료() throws Exception {
+    void enterRestartNumber_메서드로_1을_입력한경우_정상리턴() {
         // given
         String playerInput = "1";
-
-        Method method = player.getClass().getDeclaredMethod("validateIsRestartNumber", String.class);
-        method.setAccessible(true);
-        // when, then
-        method.invoke(player, playerInput);
+        // when
+        command(playerInput);
+        int inputNumber = player.enterRestartNumber();
+        // then
+        assertThat(inputNumber).isEqualTo(Integer.parseInt(playerInput));
     }
 
     @Test
-    void validateIsRestartNumber_메서드_사용시_입력값이_2인경우_정상_종료() throws Exception {
+    void enterRestartNumber_메서드로_2을_입력한경우_정상리턴() {
         // given
         String playerInput = "2";
-        Method method = player.getClass().getDeclaredMethod("validateIsRestartNumber", String.class);
-        method.setAccessible(true);
-        // when, then
-        method.invoke(player, playerInput);
+        // when
+        command(playerInput);
+        int inputNumber = player.enterRestartNumber();
+        // then
+        assertThat(inputNumber).isEqualTo(Integer.parseInt(playerInput));
     }
 
+
     @Test
-    void validateIsRestartNumber_메서드_사용시_1과_2가_아닌값을_입력한_경우_예외발생() throws Exception {
+    void enterRestartNumber_메서드로_1과_2가_아닌값을_입력한_경우_예외발생() throws Exception {
         // given
         String playerInput = "3";
-        Method method = player.getClass().getDeclaredMethod("validateIsRestartNumber", String.class);
-        method.setAccessible(true);
         // when, then
-        assertThrows(InvocationTargetException.class, () -> method.invoke(player, playerInput));
+        command(playerInput);
+        assertThatThrownBy(player::enterRestartNumber).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void command(final String... args) {
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
     }
 
 }
