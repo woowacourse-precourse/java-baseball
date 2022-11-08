3f7031ecf86f40e4b44234299796fc0196d521f8
jemin <jemin03120111@gmail.com>
2022-11-08 20:16:22 +0900

    feat: 사용자가 입력한 숫자의 결과 도출하기

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,23 +3,38 @@ package baseball;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
+
 
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("숫자 야구 게임을 시작합니다.");
-
-        List<Integer> randomNumber = getRandomNumber();
 
         while (true)
         {
-            List<Integer> inputNumber = makeUserNumber();
+            System.out.println("숫자 야구 게임을 시작합니다.");
+
+            List<Integer> randomNumber = getRandomNumber();
+            System.out.println(randomNumber.toString());
+
+            while (true)
+            {
+                System.out.print("숫자를 입력해주세요 : ");
+
+                List<Integer> inputNumber = makeUserNumber();
+                List<Integer> results = playGame(randomNumber, inputNumber);
+
+                if(results.get(0) == 3)
+                {
+                    System.out.println("3스트라이크");
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
+                }
+                else {
+                    gameOutput(results);
+                }
+            }
 
         }
 
@@ -45,7 +60,7 @@ public class Application {
         List<Integer> inputNumber = new ArrayList<>();
         String consoleInputNumber = Console.readLine();
 
-        for(int i =0 ; i < 3; i++)
+        for(int i =0 ; i < consoleInputNumber.length(); i++)
         {
             Integer number = Integer.parseInt(String.valueOf(consoleInputNumber.charAt(i)));
             // 같은 숫자를 입력한 예외 케이스 체크
@@ -58,6 +73,51 @@ public class Application {
         }
 
         return inputNumber;
+
+    }
+
+    private static List<Integer> playGame(List<Integer> computerNumber, List<Integer> userNumber)
+    {
+        int strike = 0;
+        int ball = 0;
+        for (int i = 0; i < userNumber.size(); i++)
+        {
+            // 만약 유저가 입력한 숫자와 컴퓨터 임의의 숫자가 같다면 strike를 하나 추가
+            if(userNumber.get(i).equals(computerNumber.get(i)))
+            {
+                strike+=1;
+            }
+            // 만약 유저가 입력한 숫자가 컴퓨터의 임의의 숫자에 포함된다면 ball을 하나 추가
+            else if(computerNumber.contains(userNumber.get(i)))
+            {
+                ball+=1;
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        result.add(strike);
+        result.add(ball);
+
+        return result;
+
+    }
+
+    private static void gameOutput(List<Integer> results)
+    {
+        int strike = 0;
+        int ball = 0;
+
+        strike = results.get(0);
+        ball = results.get(1);
+
+        if ( ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        } else if ( ball == 0 ){
+            System.out.println(strike+"스트라이크");
+        } else if ( strike == 0 ){
+            System.out.println(ball+"볼");
+        } else {
+            System.out.println(ball+"볼 "+strike+"스트라이크");
+        }
 
     }
 }
