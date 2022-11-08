3b0b3bc3f303978f383859c9f8d6c76ae55e9315
ki-met-hoon <101192772+ki-met-hoon@users.noreply.github.com>
2022-11-08 09:29:54 +0900

    오류 해결 및 테스트 코드 작성

--- src/test/java/baseball/ApplicationTest.java
+++ src/test/java/baseball/ApplicationTest.java
@@ -29,20 +29,21 @@ class ApplicationTest extends NsTest {
         );
     }
 
-//    @Test
-//    void 스트라이크_볼_판단_테스트() {
-//        int[] gameNumber = {4,5,6};
-//        int[] userNumber = {4,6,8};
-//
-//        for(int i = 0; i < 3; i++){
-//            baseball.service.GameService.compute(gameNumber,userNumber,i);
-//        }
-//
-//        int [] result = {Game.strikeCount,Game.ballCount};
-//        int [] testNum = {1, 1};
-//
-//        assertThat(result).isEqualTo(testNum);
-//    }
+    @Test
+    void 적은_숫자_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("14"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
+
+    @Test
+    void 숫자_제외_예외_테스트() {
+        assertSimpleTest(() ->
+                assertThatThrownBy(() -> runException("aaa"))
+                        .isInstanceOf(IllegalArgumentException.class)
+        );
+    }
 
     @Override
     public void runMain() {
