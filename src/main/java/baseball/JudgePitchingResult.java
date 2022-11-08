ffec62c0c3afe4749b67739d2b05393b4af7edaf
mgOh <issac_oh@naver.com>
2022-11-08 17:23:49 +0900

    feat: 입력값에 따른 볼, 스트라이크 수를 전달하는 기능 생성

--- src/main/java/baseball/JudgePitchingResult.java
+++ src/main/java/baseball/JudgePitchingResult.java
@@ -12,6 +12,26 @@ public class JudgePitchingResult {
         this.strike = 0;
     }
 
+    public List<Integer> returnPitchingResult(String input, List<Integer> computerNumberList){
+        List<Integer> integerList = changeStringToIntegerList(input);
+        for (int i =0; i<3; i++){
+            int term = integerList.get(i);
+            for (int j =0; j<3; j++){
+                if(term == computerNumberList.get(j)){
+                    if(i == j){
+                        this.strike++;
+                    }
+                    else{
+                        this.ball++;
+                    }
+                }
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        result.add(ball);
+        result.add(strike);
+        return result;
+    }
 
     // 입력 정수값을 각 자리로 나누는 함수
     private List<Integer> changeStringToIntegerList(String inputString) {
@@ -24,4 +44,6 @@ public class JudgePitchingResult {
         }
         return integerList;
     }
+
+
 }
