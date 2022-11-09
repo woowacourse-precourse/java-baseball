493c9e3e06ea2afbb6b6ee8e3a531b86504508f4
ffolabear <ffolabear@gmail.com>
2022-11-09 23:01:31 +0900

    refactor(Set): 일관성있게 메서드 변경 및 생성

--- src/main/java/baseball/domain/Set.java
+++ src/main/java/baseball/domain/Set.java
@@ -7,23 +7,19 @@ public class Set {
     List<String> computerAnswer;
     List<String> userAnswer;
     Calculator calculator;
-    int ball = 0;
-    int strike = 0;
 
     public Set(List<String> computerAnswer, List<String> userAnswer) {
         calculator = new Calculator(computerAnswer, userAnswer);
         setComputerAnswer(computerAnswer);
         setUserAnswer(userAnswer);
-        countBall();
-        countStrike();
     }
 
     public int getBall() {
-        return ball;
+        return calculator.getBallResult();
     }
 
     public int getStrike() {
-        return strike;
+        return calculator.getStrikeResult();
     }
 
     private void setUserAnswer(List<String> userInput) {
@@ -32,14 +28,6 @@ public class Set {
 
     private void setComputerAnswer(List<String> computerNumber) {
         this.computerAnswer = computerNumber;
-    }
-
-    private void countBall() {
-        this.ball = calculator.ballResult;
-    }
-
-    private void countStrike() {
-        this.strike = calculator.strikeResult;
     }
 
 }
