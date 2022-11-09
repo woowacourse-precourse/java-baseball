114c5c064cc7c87b409ee6de0751c6f7a5f9c357
JeongHunHui <deca3248@gmail.com>
2022-11-09 18:44:10 +0900

    refactor: 필요없는 메소드를 삭제하고, 직접 예외를 던지도록 수정

--- src/main/java/baseball/GameNumberValidator.java
+++ src/main/java/baseball/GameNumberValidator.java
@@ -10,14 +10,14 @@ public class GameNumberValidator {
     try {
       gameNumber = Integer.parseInt(String.valueOf(inputChar));
     } catch (NumberFormatException e) {
-      throwIllegalArgumentExceptionWithMessage("숫자가 아닙니다!");
+      throw new IllegalArgumentException("숫자가 아닙니다!");
     }
     return gameNumber;
   }
 
   public static void checkNumbersValid(List<Integer> gameNumberList) {
     if (gameNumberList.size() != GameNumber.NUMBER_DIGIT)
-      throwIllegalArgumentExceptionWithMessage("자리수가 잘못되었습니다!");
+      throw new IllegalArgumentException("자리수가 잘못되었습니다!");
     checkDifferentNumbers(gameNumberList);
     checkIncludeZero(gameNumberList);
   }
@@ -25,18 +25,12 @@ public class GameNumberValidator {
   private static void checkDifferentNumbers(List<Integer> gameNumberList) {
     Set<Integer> gameNumberSet = new HashSet<>(gameNumberList);
     if (gameNumberSet.size() != gameNumberList.size())
-      throwIllegalArgumentExceptionWithMessage("중복되는 숫자가 있습니다!");
+      throw new IllegalArgumentException("중복되는 숫자가 있습니다!");
   }
 
   private static void checkIncludeZero(List<Integer> gameNumberList) {
     for (int i : gameNumberList) {
-      if (i == 0) throwIllegalArgumentExceptionWithMessage("0이 포함되어 있습니다!");
+      if (i == 0) throw new IllegalArgumentException("0이 포함되어 있습니다!");
     }
-  }
-
-  private static void throwIllegalArgumentExceptionWithMessage(String message)
-      throws IllegalArgumentException {
-    System.out.println(message);
-    throw new IllegalArgumentException(message);
   }
 }
