8a32e646de6affdaf7939c8f2d6b8fc91c4de8a8
JeongHunHui <deca3248@gmail.com>
2022-11-09 14:53:42 +0900

    refactor: 게임메세지 Enum을 분리

--- src/main/java/baseball/BaseBallGame.java
+++ src/main/java/baseball/BaseBallGame.java
@@ -60,21 +60,4 @@ public class BaseBallGame {
     gameNumber.setGameNumberByString(inputData);
     return gameNumber;
   }
-
-  private enum GameMessage {
-    GAME_START_MESSAGE("숫자 야구 게임을 시작합니다."),
-    START_INPUT_MESSAGE("숫자를 입력해주세요: "),
-    GAME_FINISH_MESSAGE("개의 숫자를 모두 맞히셨습니다! 게임 종료"),
-    CHECK_RESTART_MESSAGE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
-    IS_INVALID_VALUE("잘못된 값을 입력하셨습니다. 다시 입력해주세요.");
-    private final String message;
-
-    GameMessage(String message) {
-      this.message = message;
-    }
-
-    public String getMessage() {
-      return message;
-    }
-  }
 }
