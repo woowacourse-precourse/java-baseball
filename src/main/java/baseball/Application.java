c488498079dcea6fd3186f4b7fc6d927fb131c06
core <jinjs6@gmail.com>
2022-11-10 14:30:17 +0900

    fix(wantRestart): wantRestart에서 재시작 여부 출력 순서 뒤바낌 오류 수정
    게임 재시작 여부 출력을 먼저 진행 후 숫자를 입력 받도록 설정하였습니다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -103,9 +103,9 @@ public class Application {
     }
 
     private static boolean wantRestart() throws IllegalArgumentException {
-        int input = Integer.parseInt(Console.readLine());
-
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+
+        int input = Integer.parseInt(Console.readLine());
 
         if (!(input == 1 || input == 2)) {
             throw new IllegalArgumentException();
