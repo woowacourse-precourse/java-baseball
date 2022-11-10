7bdbd0d7087061f7b8163aca541f0d3a14fece17
CAEL0 <kjongmin26@gmail.com>
2022-11-10 20:38:05 +0900

    style(Application): 공백 라인 추가

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -14,14 +14,15 @@ public class Application {
     private static final int QUIT = 2;
     private static final int MIN_DIGIT = 1;
     private static final int MAX_DIGIT = 9;
+
     private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
     private static final String INPUT_MESSAGE = "숫자를 입력해주세요 : ";
     private static final String GAME_OVER_MESSAGE = String.format("%d개의 숫자를 모두 맞히셨습니다! 게임 종료", DIGITS);
     private static final String RESTART_MESSAGE = String.format("게임을 새로 시작하려면 %d, 종료하려면 %d를 입력하세요.", RESTART, QUIT);
+
     private static final String NOTHING_MESSAGE = "낫싱";
     private static final String STRIKE_MESSAGE = "스트라이크";
     private static final String BALL_MESSAGE = "볼";
-
 
     private static final String GUESSED_NUMBER_REGEX = String.format("[%d-%d]{%d}", MIN_DIGIT, MAX_DIGIT, DIGITS);
     private static final String RESTART_NUMBER_REGEX = String.format("[%d%d]", RESTART, QUIT);
