0aba2e1d17d3bdd3efb45af449bb48f4e34df418
sunjinb <tjswls8062@skuniv.ac.kr>
2022-11-10 10:05:06 +0900

    feat(baseball) : 서로 다른 임의의 수 3개를 생성하는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,25 @@
 package baseball;
 
+import baseball.domain.NumberGenerator;
+import java.util.List;
+
+public class Application{
+    public static void main(String[] args) {
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> numbers = generator.createRandomNumbers();
+    }
+}
+
+
+
+
+
+/*
+package baseball;
+
 import java.util.Random;
 import java.util.Scanner;
+
 
 public class Application {
     static Scanner sc;
@@ -102,3 +120,4 @@ public class Application {
         }
     }
 }
+*/
---
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -0,0 +1,21 @@
+package baseball.domain;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class NumberGenerator {
+    public List<Integer> createRandomNumbers(){
+        List<Integer> numbers = new ArrayList<>();
+
+        while(numbers.size() < 3){
+            int number = new Random().nextInt(9) + 1;
+            if(numbers.contains(number)){
+                continue;
+            }
+            else{
+                numbers.add(number);
+            }
+        }
+        return numbers;
+    }
+}
