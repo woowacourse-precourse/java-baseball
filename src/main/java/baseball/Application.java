98aec8030aeceafdde700d24fe329eb14c6cb8c0
TSTP <86605720+seokjin0404@users.noreply.github.com>
2022-11-10 14:18:53 +0900

    refactor : MESSAGE 클래스 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 import static camp.nextstep.edu.missionutils.Console.readLine;
 
 import baseball.domain.Balls;
+import baseball.domain.MESSAGE;
 import baseball.domain.Result;
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
@@ -10,27 +11,27 @@ import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(MESSAGE.GAME_START);
         boolean keepGame = true;
         while (keepGame){
-            startGame();
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            inGame();
+            System.out.println(MESSAGE.GAME_RESTART);
             keepGame = restartOrNot();
         }
 
     }
-    private static void startGame() {
+    private static void inGame() {
         Balls computerBalls = new Balls(makeRandomNumbers());
         Balls playerBalls;
         Result result;
         boolean keepReading = true;
         while (keepReading) {
-            System.out.print("숫자를 입력해주세요 : ");
+            System.out.print(MESSAGE.READ_NUMBER);
             playerBalls = new Balls(readLine());
             result = computerBalls.play(playerBalls);
             System.out.println(result);
             if (result.equals(new Result(0,3))) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println(MESSAGE.GAME_END);
                 keepReading = false;
             }
         }
