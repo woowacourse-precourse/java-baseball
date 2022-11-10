d74162bc466115110a8a61bae22608612b11febc
sunjinb <tjswls8062@skuniv.ac.kr>
2022-11-10 10:13:53 +0900

    feat(baseball) : 몇 개의 숫자가 같은지 알 수 있는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,10 @@
 package baseball;
 
 import baseball.domain.NumberGenerator;
+
 import java.util.List;
 
-public class Application{
+public class Application {
     public static void main(String[] args) {
         NumberGenerator generator = new NumberGenerator();
         List<Integer> numbers = generator.createRandomNumbers();
--- src/main/java/baseball/domain/Judgement.java
+++ src/main/java/baseball/domain/Judgement.java
@@ -1,4 +1,20 @@
 package baseball.domain;
 
+import java.util.List;
+
 public class Judgement {
+    public int correctCount(List<Integer> computer, List<Integer> player){
+        int result = 0;
+        for(int i = 0; i < player.size(); i++){
+            int playerNumber = player.get(i);
+            if(computer.contains(playerNumber)){
+                result++;
+            }
+        }
+        return 0;
+    }
+
+    public boolean hasPlace(List<Integer> computer, int placeNumber, int number){
+        return false;
+    }
 }
