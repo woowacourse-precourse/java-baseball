88911f9fc10a4a7b5991534250ba8d131ac6734b
Nugur1i <duckhee092@naver.com>
2022-11-11 16:03:18 +0900

    all

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class Application {
     public static void main(String[] args) {
-        StrikeBallNothingResultGenerator game = new StrikeBallNothingResultGenerator();
+        CompareNumber game = new CompareNumber();
         game.startGame();
     }
 }
