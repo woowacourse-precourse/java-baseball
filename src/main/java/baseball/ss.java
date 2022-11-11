1d71870b9a60a7c5b8cf75aab7dd136b90f93f24
Nugur1i <duckhee092@naver.com>
2022-11-11 14:39:49 +0900

    test(ss): add test code

--- src/main/java/baseball/ss.java
+++ src/main/java/baseball/ss.java
@@ -1,8 +1,9 @@
 package baseball;
 
 
-import camp.nextstep.edu.missionutils.Console;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -20,27 +21,17 @@ public class ss {
 
     public void startGames() {
         GenerateRandomNumber computerNumber = new GenerateRandomNumber();
-
         computerNumber.setRandomNumber();
         computer = computerNumber.getDigits();
 
-
-
-
-
         InputUserNumber userNumber = new InputUserNumber();
-
         userNumber.setDigits();
         player = userNumber.getDigits();
-
-
-
 
         resetScore();
         count();
 
     }
-
 
 
     private void resetScore() {
@@ -70,6 +61,31 @@ public class ss {
         }
 
     }
+    private List<Integer> countTest(List<Integer> computerLists, List<Integer> playerLists) {
 
+        for (int position = 0; position < 3; position++) {
+
+            if (computerLists.contains(playerLists.get(position))) {
+
+                if (computerLists.indexOf(playerLists.get(position)) == position) {
+
+                    strike++;
+                    //strike 상황
+                } else if (computerLists.indexOf(playerLists.get(position)) != position) {
+
+                    ball++;
+                    //ball 상황
+                }
+
+            }
+
+        }
+
+        List<Integer> check = Arrays.asList(strike, ball);
+
+        return check;
+
+
+    }
 
 }
