78025d3ea8b5ccd87349015a72cb8ffb2bdacef9
phj1120 <hyeonj1998@naver.com>
2022-11-13 19:42:32 +0900

    feat(baseball):컴퓨터의 수와 플레이어의 수를 비교하는 기능 구현

--- src/main/java/bestpractice/baseball/Application.java
+++ src/main/java/bestpractice/baseball/Application.java
@@ -2,12 +2,18 @@ package bestpractice.baseball;
 
 import bestpractice.baseball.domain.Judgment;
 import bestpractice.baseball.domain.NumberGenerator;
+import bestpractice.baseball.domain.Referee;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
+
+        Referee referee = new Referee();
+        String result = referee.compare(Arrays.asList(6, 5, 4), Arrays.asList(1, 2, 3));
+        System.out.println(result);
+
         NumberGenerator generator = new NumberGenerator();
         List<Integer> randomNumbers = generator.createRandomNumbers();
         System.out.println(randomNumbers);
