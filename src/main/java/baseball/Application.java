29be390d6c200b29bfdc1f85605451bbafbb3a1b
junyong <jymoon1121@gmail.com>
2022-11-13 22:17:32 +0900

    bug(baseball): 예외 처리 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -3,6 +3,7 @@ package baseball;
 import baseball.domain.Judgement;
 import baseball.domain.NumberGenerator;
 import baseball.domain.Referee;
+import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,8 +17,11 @@ import java.util.Scanner;
  * 3. 각 결과를 종합한다.
  */
 public class Application {
-    public static final int ZERO = 0;
-    public static final int BASEBALL_LENGTH = 3;
+    private static final int ZERO = 0;
+    private static final int BASEBALL_LENGTH = 3;
+    private static final int RESTART =1;
+    private static final int EXIT = 2;
+    private static boolean flag = true;
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         NumberGenerator generator = new NumberGenerator();
@@ -26,13 +30,17 @@ public class Application {
         Referee referee = new Referee();
 
         String result ="";
-        while(!result.equals("3 스트라이크")) {
+        while(flag) {
             result = referee.compare(computer, askNumbers());
             System.out.println(result);
+            if(result.equals("3스트라이크")) {
+                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                int restartAnswer = askRestart();
+                processRestartAnswer(restartAnswer);
+            }
         }
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-
     }
+
     public static List<Integer> askNumbers() {
         System.out.print("숫자를 입력해주세요 : ");
         Scanner scanner = new Scanner(System.in);
@@ -41,6 +49,9 @@ public class Application {
         List<Integer> numbers = new ArrayList<>();
         for(String number: input.split("")) {
             numbers.add(Integer.valueOf(number));
+        }
+        if(numbers.size() != numbers.stream().distinct().count()) {
+            throw new IllegalArgumentException("중복된 숫자가 있습니다. 다시 입력해주세요");
         }
         if(!isCorrectForm(numbers)) {
             throw new IllegalArgumentException(ZERO +"가 아닌 " + BASEBALL_LENGTH + "자리 숫자를 입력해주세요.");
@@ -52,5 +63,25 @@ public class Application {
             return true;
         }
         return false;
+    }
+    public static int askRestart() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        int restart = getRestart();
+        return restart;
+    }
+    public static int getRestart() {
+        String s = Console.readLine();
+        int restart = Integer.parseInt(s);
+        return restart;
+    }
+    private static void processRestartAnswer(int restartAnswer) {
+        if(restartAnswer == RESTART) {
+            flag = true;
+        }
+        else if(restartAnswer != EXIT) {
+            throw new IllegalArgumentException("1 또는 2의 숫자를 입력해주세요.");
+        } else {
+            flag = false;
+        }
     }
 }
