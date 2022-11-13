bc5bf62a971a1854f2e18c4f234ae840dd95f1fa
phj1120 <hyeonj1998@naver.com>
2022-11-13 18:07:36 +0900

    feat(baseball) : 랜덤 숫자 생성 구현

--- src/main/java/bestpractice/baseball/Application.java
+++ src/main/java/bestpractice/baseball/Application.java
@@ -1,8 +1,13 @@
 package bestpractice.baseball;
 
+import bestpractice.baseball.domain.NumberGenerator;
+
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        final int num = 10;
-
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> randomNumbers = generator.createRandomNumbers();
+        System.out.println(randomNumbers);
     }
 }
