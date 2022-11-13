1f4c82c3f4d678027413588f1a5a479ea8b6c66c
junyong <jymoon1121@gmail.com>
2022-11-13 18:45:41 +0900

    feat(baseball): 서로 다른 임의의 수 세개를 생성하는 기능 구현

--- src/main/java/baseball/domain/NumberGenerator.java
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -1,9 +1,24 @@
 package baseball.domain;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NumberGenerator {
     public List<Integer> createRandomNumber() {
-        return null;
+        List<Integer> numbers = new ArrayList<>();
+
+        // 3개의 숫자가 담길 때까지
+        // 만약 이미 존재하는 숫자가 있는 경우 담지 않는다.
+        // 만약 존재하는 숫자가 없는 경우 담는다.
+        while(numbers.size() < 3) {
+            int number = new Random().nextInt(9) + 1;
+            if(numbers.contains(number)) {
+                continue;
+            } else {
+                numbers.add(number);
+            }
+        }
+        return numbers;
     }
 }
