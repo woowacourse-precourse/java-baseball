8223840f35de2c52ac55a0126b14a8e4485608fa
junyong <jymoon1121@gmail.com>
2022-11-13 21:24:34 +0900

    refactor(baseball): 자동 정렬

--- src/test/java/baseball/domain/RefereeTest.java
+++ src/test/java/baseball/domain/RefereeTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 class RefereeTest {
-    private static final List<Integer> ANSWER = Arrays.asList(1,2,3);
+    private static final List<Integer> ANSWER = Arrays.asList(1, 2, 3);
     private Referee referee;
 
     @BeforeEach
@@ -20,19 +20,19 @@ class RefereeTest {
 
     @Test
     void 스트라이크3() {
-        String result = referee.compare(ANSWER,ANSWER);
+        String result = referee.compare(ANSWER, ANSWER);
         assertThat(result).isEqualTo("3스트라이크");
     }
 
     @Test
     void 낫싱() {
-        String result = referee.compare(Arrays.asList(7,8,9),ANSWER);
+        String result = referee.compare(Arrays.asList(7, 8, 9), ANSWER);
         assertThat(result).isEqualTo("낫싱");
     }
 
     @Test
     void 볼() {
-        String result = referee.compare(Arrays.asList(1,3,2),ANSWER);
+        String result = referee.compare(Arrays.asList(1, 3, 2), ANSWER);
         assertThat(result).isEqualTo("2볼 1스트라이크");
     }
 }
