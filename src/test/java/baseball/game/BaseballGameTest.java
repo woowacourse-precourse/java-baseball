624f13b0d04e2bf304efc6917d299a7b4aa18cb3
Kayoung <rkdkdudjd@gmail.com>
2022-11-16 01:44:00 +0900

    refactor(BaseballGameTest): 중복되는 초기 로직을 before() 메서드로 분리

--- src/test/java/baseball/game/BaseballGameTest.java
+++ src/test/java/baseball/game/BaseballGameTest.java
@@ -7,30 +7,31 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 public class BaseballGameTest {
     @Nested
     @DisplayName("게임 시작 시")
     class WhenGameStarts {
-        BaseballGame game;
+        BaseballGame game = new BaseballGame();
+        Digit first;
+        Digit second;
+        Digit third;
 
         @BeforeEach
         public void before() {
-            game = new BaseballGame();
+            game.initialize();
+
+            first = game.answer.first;
+            second = game.answer.second;
+            third = game.answer.third;
         }
 
         @Test
         @DisplayName("3자리수가 생성된다.")
         public void generateThreeDigitNumber() {
-            // when
-            game.initialize();
-
-            Digit first = game.answer.first;
-            Digit second = game.answer.second;
-            Digit third = game.answer.third;
-
-            // then
             assertThat(first).isNotNull();
             assertThat(second).isNotNull();
             assertThat(third).isNotNull();
@@ -40,14 +41,6 @@ public class BaseballGameTest {
         @Test
         @DisplayName("각 자릿수가 다른 3자리수가 생성된다.")
         public void generateUniqueDigits() {
-            // when
-            game.initialize();
-
-            Digit first = game.answer.first;
-            Digit second = game.answer.second;
-            Digit third = game.answer.third;
-
-            // then
             assertThat(first).isNotEqualTo(second);
             assertThat(second).isNotEqualTo(third);
             assertThat(third).isNotEqualTo(first);
