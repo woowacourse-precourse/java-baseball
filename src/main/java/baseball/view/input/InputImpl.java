c1fac1fdab0cbac14f9604257626227bc05fdc27
5uhwann <5uhwann27@gmail.com>
2022-11-24 15:30:21 +0900

    refactor(검증): 검증 메서드 축약

--- src/main/java/baseball/view/input/InputImpl.java
+++ src/main/java/baseball/view/input/InputImpl.java
@@ -10,52 +10,36 @@ import java.util.stream.Collectors;
 
 public class InputImpl implements Input {
 
-    private final String REGEX = "^[1-9]{3}$";
-    private final int NEW_GAGE = 1;
-    private final int QUIT = 2;
+    private static final String REGEX_BASEBALL = "^[1-9]{3}$";
+    private static final String REGEX_END = "[12]{1}";
 
     @Override
     public String baseballInput() {
         String baseballInput = Console.readLine();
-        if (isAllowedBaseballInput(baseballInput) && isEachNumberDifferent(baseballInput)) {
-            return baseballInput;
-        }
-        return null;
+        validateBaseballInput(baseballInput);
+        return baseballInput;
     }
 
     @Override
     public String endInput() {
         String endInput = Console.readLine();
-        if (isAllowedEndInput(endInput)) {
-            return endInput;
-        }
-        return null;
+        validateEndInput(endInput);
+        return endInput;
     }
 
-    private boolean isAllowedEndInput(String endInput) {
-        int endInputNumber = Integer.parseInt(endInput);
-        if (endInputNumber == NEW_GAGE || endInputNumber == QUIT) {
-            return true;
-        }
-        throw new IllegalArgumentException(END_INPUT_EXCEPTION_MESSAGE);
-    }
-
-    private boolean isAllowedBaseballInput(String baseballInput) throws IllegalArgumentException {
-        if (baseballInput.matches(REGEX)) {
-            return true;
-        }
-        throw new IllegalArgumentException(BASEBALL_INPUT_EXCEPTION_MESSAGE);
-    }
-
-    private boolean isEachNumberDifferent(String baseballInput) throws IllegalArgumentException{
+    private void validateBaseballInput(String baseballInput) throws IllegalArgumentException {
         String[] baseballNumber = baseballInput.split("");
         List<String> distinctNumber = Arrays.stream(baseballNumber)
                 .distinct()
                 .collect(Collectors.toList());
-        if (distinctNumber.size() == 3) {
-            return true;
+        if (!baseballInput.matches(REGEX_BASEBALL) || distinctNumber.size() != 3) {
+            throw new IllegalArgumentException(BASEBALL_INPUT_EXCEPTION_MESSAGE);
         }
-        throw new IllegalArgumentException(BASEBALL_INPUT_EXCEPTION_MESSAGE);
     }
 
+    private void validateEndInput(String endInput) {
+        if (!endInput.matches(REGEX_END)) {
+            throw new IllegalArgumentException(END_INPUT_EXCEPTION_MESSAGE);
+        }
+    }
 }
