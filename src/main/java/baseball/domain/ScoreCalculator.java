3c1bba673de6a2c557b5766363739848947600d8
woosung1223 <dntjd991223@naver.com>
2022-11-24 20:42:25 +0900

    refactor(ScoreCalculator): 드러낼 필요가 없는 메소드 접근제어자 변경

--- src/main/java/baseball/domain/ScoreCalculator.java
+++ src/main/java/baseball/domain/ScoreCalculator.java
@@ -9,7 +9,14 @@ public final class ScoreCalculator {
     private ScoreCalculator() {
     }
 
-    public static int calculateBallCount(BaseballNumber baseballNum1, BaseballNumber baseballNum2) {
+    public static Score calculateUserFinalScore(BaseballNumber baseballNum1, BaseballNumber baseballNum2) {
+        return new Score(
+                calculateBallCount(baseballNum1, baseballNum2),
+                calculateStrikeCount(baseballNum1, baseballNum2)
+        );
+    }
+
+    private static int calculateBallCount(BaseballNumber baseballNum1, BaseballNumber baseballNum2) {
         return (int) Stream.iterate(0, i -> i + 1)
                 .limit(SystemConstant.GAME_DIGIT)
                 .filter(i -> baseballNum2.contains(baseballNum1.getNumberAt(i)))
@@ -17,17 +24,10 @@ public final class ScoreCalculator {
                 .count();
     }
 
-    public static int calculateStrikeCount(BaseballNumber baseballNum1, BaseballNumber baseballNum2) {
+    private static int calculateStrikeCount(BaseballNumber baseballNum1, BaseballNumber baseballNum2) {
         return (int) Stream.iterate(0, i -> i + 1)
                 .limit(SystemConstant.GAME_DIGIT)
                 .filter(i -> baseballNum1.getNumberAt(i) == baseballNum2.getNumberAt(i))
                 .count();
-    }
-
-    public static Score calculateUserFinalScore(BaseballNumber baseballNum1, BaseballNumber baseballNum2) {
-        return new Score(
-                calculateBallCount(baseballNum1, baseballNum2),
-                calculateStrikeCount(baseballNum1, baseballNum2)
-        );
     }
 }
--- src/test/java/baseball/domain/ScoreCalculatorTest.java
+++ src/test/java/baseball/domain/ScoreCalculatorTest.java
@@ -15,32 +15,6 @@ import java.util.List;
 import java.util.stream.Stream;
 
 class ScoreCalculatorTest {
-    @Nested
-    @DisplayName("각 자리수마다 서로 다른 자리수가 있는 경우, ballCount는 1 증가하는지 테스트한다")
-    class Ball {
-        @ParameterizedTest
-        @CsvSource(value = {"123, 123, 0", "123, 392, 2", "567, 345, 1"})
-        void calculateBallCountTest(int number1, int number2, int expected) {
-            BaseballNumber baseballNum1 = new BaseballNumber(number1);
-            BaseballNumber baseballNum2 = new BaseballNumber(number2);
-            assertThat(ScoreCalculator.calculateBallCount(baseballNum1, baseballNum2))
-                    .isEqualTo(expected);
-        }
-    }
-
-    @Nested
-    @DisplayName("각 자리수마다 서로 같은 자리수가 있는 경우, strikeCount는 1 증가한다")
-    class Strike {
-        @ParameterizedTest
-        @CsvSource(value = {"123, 123, 3", "123, 392, 0", "465, 345, 1"})
-        void calculateStrikeCountTest(int number1, int number2, int expected) {
-            BaseballNumber baseballNum1 = new BaseballNumber(number1);
-            BaseballNumber baseballNum2 = new BaseballNumber(number2);
-            assertThat(ScoreCalculator.calculateStrikeCount(baseballNum1, baseballNum2))
-                    .isEqualTo(expected);
-        }
-    }
-
     @TestInstance(TestInstance.Lifecycle.PER_CLASS)
     @Nested
     @DisplayName("Ball과 Strike가 둘 다 있는 경우를 테스트한다")
