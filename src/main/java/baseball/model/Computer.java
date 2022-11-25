9023cd15237a3940c222e8d20824b1c7e0b7bfda
Eunkeee <eunkeeeee@gmail.com>
2022-11-25 15:04:51 +0900

    feat(Computer): 컴퓨터의 수에 중복 예외 처리

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -4,21 +4,44 @@ import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Computer {
-    private List<Integer> computerNumber = new ArrayList<>();
+    private List<Integer> computerNumber;
 
     public Computer() {
-        createComputerNumbers();
+        this.computerNumber = createComputerNumber();
     }
 
-    private void createComputerNumbers() {
+    private List<Integer> createComputerNumber() {
+        validateComputerNumber(generateComputerNumber());
+        return computerNumber;
+    }
+
+    private static List<Integer> generateComputerNumber() {
+        List<Integer> computerNumber = new ArrayList<>();
         while (computerNumber.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!computerNumber.contains(randomNumber)) {
                 computerNumber.add(randomNumber);
             }
         }
+        return computerNumber;
+    }
+
+
+    private void validateComputerNumber(List<Integer> computerNumber) {
+        if (hasDuplicates(computerNumber)) {
+            throw new IllegalArgumentException("컴퓨터의 수에 중복된 숫자가 존재합니다.");
+        }
+    }
+
+    private boolean hasDuplicates(List<Integer> computerNumber) {
+        return sizeOfDuplicatesRemoved(computerNumber) != computerNumber.size();
+    }
+
+    private int sizeOfDuplicatesRemoved(List<Integer> computerNumber) {
+        return computerNumber.stream().collect(Collectors.toSet()).size();
     }
 
     public List<Integer> getComputerNumber() {
