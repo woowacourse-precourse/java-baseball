f4051e151458f5b9c7052b33883551bf4980d6a0
woosung1223 <dntjd991223@naver.com>
2022-11-25 18:45:48 +0900

    refactor(OutputView): 중복 메소드 제거

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,26 +1,16 @@
 package baseball.view;
 
 import baseball.domain.dto.Score;
-import baseball.system.SystemConstant;
 
 public final class OutputView {
     private final String BALL = "볼";
     private final String STRIKE = "스트라이크";
     private final String NOTHING = "낫싱";
-    private final String GAME_END_AND_RESTART_MESSAGE =
-            SystemConstant.GAME_DIGIT + "개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-            "게임을 새로 시작하려면 " + SystemConstant.GAME_RESTART_CODE +
-            ", 종료하려면 " + SystemConstant.GAME_EXIT_CODE + "를 입력하세요.\n";
-
-    public void printFinalResult(Score userScore) {
-        printProgress(userScore);
-        System.out.println(GAME_END_AND_RESTART_MESSAGE);
-    }
 
     public void printProgress(Score userScore) {
         printMessage(userScore);
     }
-    
+
 
     private void printMessage(Score score) {
         if (score.getBall() == 0 && score.getStrike() == 0) {
