0bb235d7ad5006c16f6f430026a25b57de0b87b1
Eunkeee <eunkeeeee@gmail.com>
2022-11-25 14:45:09 +0900

    test(Computer): 컴퓨터의 수 생성 테스트 작성

--- src/test/java/baseball/model/ComputerTest.java
+++ src/test/java/baseball/model/ComputerTest.java
@@ -1,4 +1,15 @@
+package baseball.model;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class ComputerTest {
-  
+
+    @Test
+    void 컴퓨터_숫자_생성() {
+        Computer computer = new Computer();
+        Assertions.assertEquals(3, computer.getComputerNumber().size());
+    }
 }
