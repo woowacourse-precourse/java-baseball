df122ca261ca3c787cb14410afd323147bdf836a
Ridealist <cjsn92@gmail.com>
2022-11-27 15:54:14 +0900

    feat(Baseball): 값 유효성 검증 및 스트라이크, 볼 카운트 기능 구현
    List값 올바른 범위 확인, 중복 숫자 없는지 유효성 검증
    Baseball 객체를 받아 스트라이크 개수 반환
    Baseball 객체를 받아 볼 개수 반환

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -1,20 +1,59 @@
 package baseball.domain;
 
+import baseball.utils.Constants;
+import java.util.HashSet;
 import java.util.List;
 
 public class Baseball {
 
-    private List<Integer> numbers;
+    private final List<Integer> numbers;
 
     public Baseball(List<Integer> numbers) {
         validate(numbers);
+        this.numbers = numbers;
     }
 
-    public boolean countSameNumber(Baseball baseball) {
-        return true;
+    public List<Integer> getNumbers() {
+        return numbers;
     }
 
-    private void validate(List<Integer> numbers) {
-        throw new IllegalArgumentException();
+    public int countStrike(Baseball baseball) {
+        int strikeCount = 0;
+        for (int i = 0; i < Constants.NUMBERS_SIZE; i++) {
+            if (baseball.getNumbers().get(i) == this.numbers.get(i)) {
+                strikeCount++;
+            }
+        }
+        return strikeCount;
+    }
+
+    public int countBall(Baseball baseball) {
+        int sameNumberCount = 0;
+        for (int number : baseball.getNumbers()) {
+            if (this.numbers.contains(number)) {
+                sameNumberCount++;
+            }
+        }
+        return sameNumberCount - countStrike(baseball);
+    }
+
+    private void validate(List<Integer> numbers) throws IllegalArgumentException {
+        isInCorrectRange(numbers);
+        isAllDifferent(numbers);
+    }
+
+    private void isInCorrectRange(List<Integer> numbers) {
+        for (int number : numbers) {
+            if (number < Constants.NUMBERS_LOWER_BOUND || number > Constants.NUMBERS_UPPER_BOUND) {
+                // TODO exception 메세지 추가
+                throw new IllegalArgumentException();
+            }
+        }
+    }
+
+    private void isAllDifferent(List<Integer> numbers) {
+        if ((new HashSet<>(numbers)).size() != Constants.NUMBERS_SIZE) {
+            throw new IllegalArgumentException();
+        }
     }
 }
