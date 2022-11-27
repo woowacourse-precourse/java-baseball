5a0eed8fb1c55cc6f9944d162fa93be2d3008674
woosung1223 <dntjd991223@naver.com>
2022-11-28 01:28:03 +0900

    refactor(GameManager): 로직을 이해하기 쉽도록 수정

--- src/main/java/baseball/controller/GameManager.java
+++ src/main/java/baseball/controller/GameManager.java
@@ -12,42 +12,34 @@ public class GameManager {
     private final BaseballGame baseballGame = new BaseballGame();
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.\n";
-    private final String GAME_END_AND_RESTART_MESSAGE =
-            SystemConstant.GAME_DIGIT + "개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
-                    "게임을 새로 시작하려면 " + SystemConstant.GAME_RESTART_CODE +
-                    ", 종료하려면 " + SystemConstant.GAME_EXIT_CODE + "를 입력하세요.\n";
 
     public void run() {
         System.out.println(GAME_START_MESSAGE);
-        baseballGame.initComputerNumber();
-        startFirstGame();
-        startRepeatedGame();
+        startGameUntilUserQuit();
     }
 
-    private void startFirstGame() {
-        startLoop();
-    }
-
-    private void startRepeatedGame() {
-        int flag = inputView.readGameControlInput();
-        if (flag == SystemConstant.GAME_EXIT_CODE) {
-            return;
-        }
-        baseballGame.initComputerNumber();
-        startLoop();
-        startRepeatedGame();
-    }
-
-    private void startLoop() {
-        Score userScore;
+    private void startGameUntilUserQuit() {
+        int control;
         do {
-            userScore = baseballGame.computeUserScore(inputView.readGameInput());
-            outputView.printResult(userScore);
-        } while (!isGameOver(userScore));
-        System.out.println(GAME_END_AND_RESTART_MESSAGE);
+            baseballGame.initComputerNumber();
+            doGameLoopUntilGameOver();
+            control = inputView.readGameControlInput();
+        } while(control != SystemConstant.GAME_EXIT_CODE);
+    }
+
+    private void doGameLoopUntilGameOver() {
+       Score userScore;
+       do {
+           int userNumber = inputView.readGameInput();
+           userScore = baseballGame.computeUserScore(userNumber);
+           outputView.printResult(userScore);
+       } while(!isGameOver(userScore));
     }
 
     private boolean isGameOver(Score userScore) {
-        return userScore.isGameOver();
+        if (userScore.isGameOver()) {
+            return true;
+        }
+        return false;
     }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,6 @@
 package baseball.view;
 
+import baseball.system.SystemConstant;
 import baseball.verifier.BaseballNumVerifier;
 import baseball.verifier.ControlNumberVerifier;
 import baseball.verifier.Verifier;
@@ -7,6 +8,10 @@ import camp.nextstep.edu.missionutils.Console;
 
 public final class InputView {
     private final String GAME_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private final String GAME_END_AND_RESTART_MESSAGE =
+            SystemConstant.GAME_DIGIT + "개의 숫자를 모두 맞히셨습니다! 게임 종료\n" +
+                    "게임을 새로 시작하려면 " + SystemConstant.GAME_RESTART_CODE +
+                    ", 종료하려면 " + SystemConstant.GAME_EXIT_CODE + "를 입력하세요.\n";
 
     public int readGameInput() {
         System.out.print(GAME_INPUT_MESSAGE);
@@ -18,6 +23,7 @@ public final class InputView {
     }
 
     public int readGameControlInput() {
+        System.out.println(GAME_END_AND_RESTART_MESSAGE);
         String line = Console.readLine();
 
         Verifier controlVerifier = new ControlNumberVerifier();
