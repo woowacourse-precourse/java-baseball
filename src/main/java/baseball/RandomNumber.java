f26804396844b4321959d6fba305982e0ecabb00
dohyun <ldh19738@gmail.com>
2022-11-27 10:36:14 +0900

    feat(RandomNumber): RandomNumber 객체 반환하는 기능 추가

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -3,9 +3,15 @@ package baseball;
 public class RandomNumber {
     private Integer randomNumber;
 
-    public RandomNumber(Integer randomNumber) {
+    private RandomNumber(Integer randomNumber) {
         Exception.randomNumberRangeException(randomNumber);
         this.randomNumber = randomNumber;
+    }
+
+    public static RandomNumber valueOf(Integer randomNumber) {
+        Exception.randomNumberRangeException(randomNumber);
+
+        return new RandomNumber(randomNumber);
     }
 
     @Override
