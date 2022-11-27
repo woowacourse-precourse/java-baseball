ae755b79c02f9b6c3411947763e0fc9a60ea2d06
Ridealist <cjsn92@gmail.com>
2022-11-27 23:53:08 +0900

    feat(InputView): 사용자 입력 요청 및 유효성 검증 후 데이터 넘겨주는 클래스 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,15 +1,32 @@
 package baseball.view;
 
-import java.util.Arrays;
+import baseball.utils.Validator;
+import java.util.ArrayList;
 import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
-    public List<Integer> readUserNumbers() {
-        return Arrays.asList(0);
+    public static List<Integer> readUserNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        Validator.isLengthThree(input);
+        Validator.isAllNumber(input);
+        return stringToList(input);
     }
 
-    public int readGameCommand() {
-        return 1;
+    public static int readGameCommand() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        Validator.isOneOrTwo(input);
+        return Integer.parseInt(input);
+    }
+
+    private static List<Integer> stringToList(String input) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int i = 0; i < input.length(); i++) {
+            numbers.add((int) input.charAt(i) - '0');
+        }
+        return numbers;
     }
 }
