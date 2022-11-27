980ae4eaa6f277c0afe82b55a15b11af0b9d2d5f
Ridealist <cjsn92@gmail.com>
2022-11-27 16:58:30 +0900

    feat(Baseball): 스트라이크, 볼 카운트, 게임 진행 여부 알려주는 클래스 구현
    Numbers 객체에서 받은 List<Integer>끼리 비교하여 결과 반환

--- src/main/java/baseball/domain/Baseball.java
+++ src/main/java/baseball/domain/Baseball.java
@@ -1,59 +1,39 @@
 package baseball.domain;
 
+import baseball.utils.BaseballNumbersGenerator;
 import baseball.utils.Constants;
-import java.util.HashSet;
 import java.util.List;
 
 public class Baseball {
 
-    private final List<Integer> numbers;
+    private final List<Integer> computerNumbers;
 
-    public Baseball(List<Integer> numbers) {
-        validate(numbers);
-        this.numbers = numbers;
+    public Baseball(BaseballNumbersGenerator numbersGenerator) {
+        Numbers numbers = new Numbers(numbersGenerator.generate());
+        this.computerNumbers = numbers.getNumbers();
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
+    public boolean isGameContinue(Numbers user) {
+        return !(countStrike(user) == Constants.NUMBERS_SIZE);
     }
 
-    public int countStrike(Baseball baseball) {
+    public int countStrike(Numbers user) {
         int strikeCount = 0;
         for (int i = 0; i < Constants.NUMBERS_SIZE; i++) {
-            if (baseball.getNumbers().get(i) == this.numbers.get(i)) {
+            if (user.getNumbers().get(i) == computerNumbers.get(i)) {
                 strikeCount++;
             }
         }
         return strikeCount;
     }
 
-    public int countBall(Baseball baseball) {
+    public int countBall(Numbers numbers) {
         int sameNumberCount = 0;
-        for (int number : baseball.getNumbers()) {
-            if (this.numbers.contains(number)) {
+        for (int number : numbers.getNumbers()) {
+            if (computerNumbers.contains(number)) {
                 sameNumberCount++;
             }
         }
-        return sameNumberCount - countStrike(baseball);
-    }
-
-    private void validate(List<Integer> numbers) throws IllegalArgumentException {
-        isInCorrectRange(numbers);
-        isAllDifferent(numbers);
-    }
-
-    private void isInCorrectRange(List<Integer> numbers) {
-        for (int number : numbers) {
-            if (number < Constants.NUMBERS_LOWER_BOUND || number > Constants.NUMBERS_UPPER_BOUND) {
-                // TODO exception 메세지 추가
-                throw new IllegalArgumentException();
-            }
-        }
-    }
-
-    private void isAllDifferent(List<Integer> numbers) {
-        if ((new HashSet<>(numbers)).size() != Constants.NUMBERS_SIZE) {
-            throw new IllegalArgumentException();
-        }
+        return sameNumberCount - countStrike(numbers);
     }
 }
