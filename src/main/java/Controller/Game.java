6f3b4bb64d12f7f0b422961e23b83080a3537d1b
glass <glass7312@gmail.com>
2022-11-27 18:19:42 +0900

    refactor: 기능 분리

--- src/main/java/Controller/Game.java
+++ src/main/java/Controller/Game.java
@@ -1,70 +1,27 @@
 package Controller;
 
+import View.InputView;
+import View.OutputView;
 import camp.nextstep.edu.missionutils.Console;
 import domain.Computer;
 import domain.User;
 
 public class Game {
-
     public static String EXITSTATUS = "playing";
+    private InputView inputView=new InputView();
+    private OutputView outputView=new OutputView();
+    private User user;
 
-    public static void start() {
+
+    public void start() {
         System.out.print("숫자를 입력해주세요 : ");
-        Game.UserInput_Set(Game.readLine());
-        Game.print_Message();
-    }
-
-    public static void print_Message() {
-        Game.ball_Message();
-        Game.strike_Message();
-        Game.nothing_Message();
-    }
-
-    public static void strike_Message() {
-        if (User.strike_count() != 0) {
-            Game.startOrexit();
-
-            System.out.print(User.strike_count() + "스트라이크" + " ");
-        }
-    }
-
-    public static void ball_Message() {
-        if (User.ball_count() != 0) {
-            System.out.print(User.ball_count() + "볼 ");
-        }
-    }
-
-    public static void nothing_Message() {
-        if (User.strike_count() == 0 && User.ball_count() == 0) {
-            System.out.print("낫싱" + "\n");
-        }
-    }
-
-    public static String readLine() {
-        String input = Console.readLine();
-
-        validation.check_input(input);
-        return input;
-
-    }
-
-    public static void startOrexit() {
-        if (User.strike_count() == 3) {
-            whenCorrect_Message();
-
-            set_EXITSTATUS(Console.readLine());
-        }
-    }
-
-    public static void whenCorrect_Message() {
-
-        System.out.print(User.strike_count() + "스트라이크" + "\n");
-        System.out.print("숫자를 모두 맞히셨습니다! 게임 종료" + "\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." + "\n");
+        this.user=new User(inputView.readnumber());
+        outputView.print_Message(user);
     }
 
     public static void set_EXITSTATUS(String input) {
         if (input.equals("1")) {
-            Computer.setNumber();
+            Computer.setNumber(validation.get_ThreeRandomNumbers());
 
             return;
         }
@@ -73,9 +30,5 @@ public class Game {
 
             EXITSTATUS = "exit";
         }
-    }
-
-    public static void UserInput_Set(String input) {
-        User.setNumber(input);
     }
 }
--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -1,4 +1,59 @@
 package View;
 
+import Controller.Game;
+import Controller.validation;
+import camp.nextstep.edu.missionutils.Console;
+import domain.Computer;
+import domain.User;
+
 public class OutputView {
+
+    public void print_Message(User user) {
+        ball_Message(user);
+        strike_Message(user);
+        nothing_Message(user);
+    }
+
+    public void strike_Message(User user) {
+        if (user.strike_count() != 0) {
+            if (user.strike_count() == 3) {
+                whenCorrect_Message(user);
+
+                set_EXITSTATUS(Console.readLine());
+            }
+
+            System.out.print(user.strike_count() + "스트라이크" + " ");
+        }
+    }
+
+    public void ball_Message(User user) {
+        if (user.ball_count() != 0) {
+            System.out.print(user.ball_count() + "볼 ");
+        }
+    }
+
+
+    public void nothing_Message(User user) {
+        if (user.strike_count() == 0 && user.ball_count() == 0) {
+            System.out.print("낫싱" + "\n");
+        }
+    }
+    public void whenCorrect_Message(User user) {
+
+        System.out.print(user.strike_count() + "스트라이크" + "\n");
+        System.out.print("숫자를 모두 맞히셨습니다! 게임 종료" + "\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." + "\n");
+    }
+
+    public static void set_EXITSTATUS(String input) {
+        if (input.equals("1")) {
+            Computer.setNumber(validation.get_ThreeRandomNumbers());
+            return;
+        }
+        if (input.equals("2")) {
+            System.out.print("게임 종료");
+
+            Game.EXITSTATUS = "exit";
+        }
+    }
+
 }
