1ae3bfd08037e6c3dc0c2327188ca1be42ea230e
Ridealist <cjsn92@gmail.com>
2022-11-27 15:20:35 +0900

    feat(Constants): 게임 전체 사용되는 상수 관리 클래스 구현

---
+++ src/main/java/baseball/utils/Constants.java
@@ -0,0 +1,7 @@
+package baseball.utils;
+
+public class Constants {
+    public static final int NUMBERS_SIZE = 3;
+    public static final int NUMBERS_LOWER_BOUND = 1;
+    public static final int NUMBERS_UPPER_BOUND = 9;
+}
