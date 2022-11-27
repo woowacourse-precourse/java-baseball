1d7b99598e04b20ddde343f5312ba8843a3dcb52
glass <glass7312@gmail.com>
2022-11-28 02:37:59 +0900

    refactor: 함수명 변경

--- src/main/java/View/OutputView.java
+++ src/main/java/View/OutputView.java
@@ -19,7 +19,7 @@ public class OutputView {
             if (user.strike_count() == 3) {
                 whenCorrect_Message(user);
 
-                set_EXITSTATUS(Console.readLine());
+                set_exitstatus(Console.readLine());
             }
 
             System.out.print(user.strike_count() + "스트라이크" + " ");
@@ -38,13 +38,14 @@ public class OutputView {
             System.out.print("낫싱" + "\n");
         }
     }
+
     public void whenCorrect_Message(User user) {
 
         System.out.print(user.strike_count() + "스트라이크" + "\n");
         System.out.print("숫자를 모두 맞히셨습니다! 게임 종료" + "\n" + "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." + "\n");
     }
 
-    public static void set_EXITSTATUS(String input) {
+    public static void set_exitstatus(String input) {
         if (input.equals("1")) {
             Computer.setNumber(validation.get_ThreeRandomNumbers());
             return;
