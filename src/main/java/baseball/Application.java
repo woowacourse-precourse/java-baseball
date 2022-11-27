6808b9debe17a3d09af685a09075d77a381ae37e
glass <glass7312@gmail.com>
2022-11-28 02:36:54 +0900

    refactor: 코드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,15 +10,12 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("숫자 야구 게임을 시작합니다");
 
-        Game game= new Game();
+        Game game = new Game();
         Computer.setNumber(validation.get_ThreeRandomNumbers());
         System.out.println(Computer.getNumber());
 
-        while (true) {
+        do {
             game.start();
-            if (Game.EXITSTATUS.equals("exit")) break;
-
-            else Game.EXITSTATUS="playing";
-        }
+        } while (!Game.EXITSTATUS.equals("exit"));
     }
 }
