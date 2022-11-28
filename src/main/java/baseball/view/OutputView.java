6844f38f0f58ebee3cfe1bc8001de9564eb20615
HaODay <ycis5013@naver.com>
2022-11-28 15:57:07 +0900

    feat(OutputView): 게임 시작 메시지 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,4 +1,9 @@
 package baseball.view;
 
+import static baseball.view.Message.START_MESSAGE;
+
 public class OutputView {
+    public static void printStartMessage() {
+        System.out.println(START_MESSAGE);
+    }
 }
