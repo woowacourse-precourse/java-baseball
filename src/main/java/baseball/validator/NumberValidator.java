9441d6db8d325d8504469f94f713e107ef3d68cb
parkjungyoon <tbr06057@naver.com>
2022-11-28 19:55:49 +0900

    refactor: 3가지 숫자 각각이 올바른 범위인지 판단하도록 코드 수정

--- src/main/java/baseball/validator/NumberValidator.java
+++ src/main/java/baseball/validator/NumberValidator.java
@@ -1,16 +1,8 @@
 package baseball.validator;
 
 public class NumberValidator {
-    private final static int MIN_RANGE_NUM = 1;
-    private final static int MAX_RANGE_NUM = 9;
-    private final static String RANGE_ERROR_MESSAGE = "1부터 9 사이의 자연수를 입력해주세요.";
     private final static String RESTART_ERROR_MESSAGE = "1또는 2를 입력해주세요.";
 
-    public static void validateRange(int input) {
-        if (input < MIN_RANGE_NUM || input > MAX_RANGE_NUM) {
-            throw new IllegalArgumentException(RANGE_ERROR_MESSAGE);
-        }
-    }
 
     public static void validateRestartNumber(int input) {
         if (input == 1) {
