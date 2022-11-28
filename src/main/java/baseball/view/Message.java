2d2b5eb0c25a1ca91f7552332d7e875ea3052089
HaODay <ycis5013@naver.com>
2022-11-28 15:56:43 +0900

    feat(Message): 게임 시작 시 출력하는 열거상수 추가

--- src/main/java/baseball/view/Message.java
+++ src/main/java/baseball/view/Message.java
@@ -1,5 +1,11 @@
 package baseball.view;
 
 public enum Message {
+    START_MESSAGE("숫자 야구 게임을 시작합니다.");
 
+    private final String message;
+
+    Message(String message) {
+        this.message=message;
+    }
 }
