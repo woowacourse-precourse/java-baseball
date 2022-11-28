a1bfe311772e657602e5322c322b70a5880a81cb
HaODay <ycis5013@naver.com>
2022-11-28 17:00:59 +0900

    feat(OutputView): 플레이어 값 요청 메시지 출력하는 기능 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,9 +1,15 @@
 package baseball.view;
 
+import static baseball.view.Message.INPUT_PLAYER_NUM_MESSAGE;
 import static baseball.view.Message.START_MESSAGE;
 
 public class OutputView {
     public static void printStartMessage() {
         System.out.println(START_MESSAGE);
     }
+
+    public static void printInputPlayerNumMessage() {
+        System.out.println(INPUT_PLAYER_NUM_MESSAGE);
+    }
+
 }
