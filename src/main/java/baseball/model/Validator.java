b25f85f55ab82e52d9c5fcc277f7df83bfbe83a4
HaODay <ycis5013@naver.com>
2022-11-28 17:00:24 +0900

    feat(Validator): 플레이어의 값 검증하는 기능 구현

--- src/main/java/baseball/model/Validator.java
+++ src/main/java/baseball/model/Validator.java
@@ -1,4 +1,35 @@
 package baseball.model;
 
+import static baseball.model.NumberProperty.SIZE;
+
 public class Validator {
+    public static void validateNum(String num) throws IllegalArgumentException {
+        checkSize(num);
+        checkDuplicate(num);
+        checkNumScope(num);
+    }
+
+    private static void checkSize(String num) throws IllegalArgumentException {
+        if (num.length() != SIZE.get()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void checkDuplicate(String num) throws IllegalArgumentException {
+        StringBuilder tmp = new StringBuilder();
+        for (char x : num.toCharArray()) {
+            if (tmp.toString().contains(String.valueOf(x))) {
+                throw new IllegalArgumentException();
+            }
+            tmp.append(x);
+        }
+    }
+
+    private static void checkNumScope(String num) {
+        for (char x : num.toCharArray()) {
+            if (!('0' < x && x <= '9')) {
+                throw new IllegalArgumentException();
+            }
+        }
+    }
 }
