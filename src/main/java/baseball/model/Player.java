a3278c157df0c8fe9e3c6c373da693cffa9568f2
HaODay <ycis5013@naver.com>
2022-11-28 16:59:59 +0900

    feat(Player): 플레이어의 값 Integer List로 변환하는 기능 구현

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,4 +1,27 @@
 package baseball.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.model.NumberProperty.SIZE;
+
 public class Player {
+
+    private final List<Integer> player;
+
+    public Player(String readPlayerNum) {
+        player = new ArrayList<Integer>();
+        Validator.validateNum(readPlayerNum);
+        convertToIntList(readPlayerNum);
+    }
+
+    private void convertToIntList(String readPlayerNum) {
+        for (int i = 0; i < SIZE.get(); i++) {
+            player.add(readPlayerNum.charAt(i) - '0');
+        }
+    }
+
+    public List<Integer> get() {
+        return player;
+    }
 }
