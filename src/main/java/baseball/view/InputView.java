4cbc4ca2554a90a290673ec364d8c38b961a58ff
sonsoowon <yuwoo1018@gmail.com>
2022-11-29 02:58:05 +0900

    refactor(InputView): 입력 유효성 검증 코드 중복 제거

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,6 +1,7 @@
 package baseball.view;
 
 import baseball.util.InputViewMessage;
+import baseball.valid.GameCommandValidator;
 import baseball.valid.InputValidator;
 import baseball.valid.PlayerNumberValidator;
 import camp.nextstep.edu.missionutils.Console;
@@ -12,12 +13,22 @@ public enum InputView {
     INSTANCE;
 
     public static String readPlayerNumber() {
+        InputValidator inputValidator = new PlayerNumberValidator();
         System.out.print(InputViewMessage.READ_PLAYER_NUMBER_MESSAGE);
-        return Console.readLine();
+
+        return validInput(inputValidator);
     }
 
     public static String readGameCommand() {
+        InputValidator inputValidator = new GameCommandValidator();
         System.out.println(InputViewMessage.READ_GAME_COMMAND_MESSAGE);
-        return Console.readLine();
+
+        return validInput(inputValidator);
+    }
+
+    private static String validInput(InputValidator inputValidator) {
+        String input = Console.readLine();
+        inputValidator.validate(input);
+        return input;
     }
 }
