3a7dbbb3ace49e175574cd421be1afd58e164969
HaODay <ycis5013@naver.com>
2022-11-28 16:18:06 +0900

    feat(Computer): 상대방(컴퓨터)의 값 생성하는 기능 구현

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,4 +1,27 @@
 package baseball.model;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static baseball.model.NumberProperty.*;
+
 public class Computer {
+
+    private final List<Integer> computer;
+
+    Computer() {
+        computer = new ArrayList<Integer>();
+        while (computer.size() < SIZE.get()) {
+            int randomNumber = Randoms.pickNumberInRange(MIN_NUM.get(), MAX_NUM.get());
+            if (!computer.contains(randomNumber)) {
+                computer.add(randomNumber);
+            }
+        }
+    }
+
+    public List<Integer> getComputer() {
+        return computer;
+    }
 }
