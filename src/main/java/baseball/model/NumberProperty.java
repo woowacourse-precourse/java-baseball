3dfd639cda075bf506bb3d678c9df26cb7a597a3
HaODay <ycis5013@naver.com>
2022-11-28 16:17:05 +0900

    feat(NumberProperty): 숫자 개수(3), 최소값(1), 최댓값(9)을 열거상수로 선언

--- src/main/java/baseball/model/NumberProperty.java
+++ src/main/java/baseball/model/NumberProperty.java
@@ -1,4 +1,17 @@
 package baseball.model;
 
 public enum NumberProperty {
+    SIZE(3),
+    MIN_NUM(1),
+    MAX_NUM(9);
+
+    private final int property;
+
+    NumberProperty(int property) {
+        this.property=property;
+    }
+
+    public int get(){
+        return property;
+    }
 }
