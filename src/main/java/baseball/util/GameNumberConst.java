c137f509cce3db0ff53f3d37e42e897f79b96a62
sonsoowon <yuwoo1018@gmail.com>
2022-11-28 19:30:23 +0900

    refactor(GameNumberConst): 게임 숫자관련 상수 유틸리티 클래스로 분리

---
+++ src/main/java/baseball/util/GameNumberConst.java
@@ -0,0 +1,10 @@
+package baseball.util;
+
+public class GameNumberConst {
+    private GameNumberConst() {
+    }
+
+    public static final int NUMBER_SIZE = 3;
+    public static final int MIN_NUMBER = 1;
+    public static final int MAX_NUMBER = 9;
+}
