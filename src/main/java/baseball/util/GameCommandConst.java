74edc65f8ad18f43a73ea2eec6b15d17ab50aa34
sonsoowon <yuwoo1018@gmail.com>
2022-11-29 00:26:11 +0900

    refactor(GameCommandConst): 게임 재시작/종료관련 상수를 유틸리티 클래스로 분리

---
+++ src/main/java/baseball/util/GameCommandConst.java
@@ -0,0 +1,9 @@
+package baseball.util;
+
+public class GameCommandConst {
+    public static final String RESTART = "1";
+    public static final String QUIT = "2";
+
+    private GameCommandConst() {
+    }
+}
