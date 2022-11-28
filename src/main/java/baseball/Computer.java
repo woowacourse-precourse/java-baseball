0642ad847be5968268a2671d2d16564ea4f23b48
dohyun <ldh19738@gmail.com>
2022-11-28 10:08:20 +0900

    refactor(Computer): 랜덤 넘버 구하는 기능 구현

--- src/main/java/baseball/Computer.java
+++ src/main/java/baseball/Computer.java
@@ -2,21 +2,37 @@ package baseball;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import static baseball.Exception.DIGIT_NUMBER;
+import static baseball.Exception.MAX_NUMBER;
+import static baseball.Exception.MIN_NUMBER;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Computer {
-    private static final int DIGIT_NUMBER = 3;
-    private static final int MIN_NUMBER = 1;
-    private static final int MAX_NUMBER = 9;
-    public static String number() {
-        String computerNumber = "";
+    public RandomNumbers generate() {
+        List<Integer> randomNumbers = new ArrayList<>();
 
-        do {
-            String randomNumber = String.valueOf(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER));
+        while (randomNumbers.size() < DIGIT_NUMBER) {
+            addRandomNumber(randomNumbers);
+        }
 
-            if (!computerNumber.contains(randomNumber)) {
-                computerNumber += randomNumber;
-            }
-        } while (computerNumber.length() != DIGIT_NUMBER);
+        return toRandomNumbers(randomNumbers);
+    }
 
-        return computerNumber;
+    private void addRandomNumber(List<Integer> randomNumbers) {
+        int randomNumber = Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
+
+        if (randomNumbers.size() != DIGIT_NUMBER) {
+            randomNumbers.add((randomNumber));
+        }
+    }
+
+    private RandomNumbers toRandomNumbers(List<Integer> randomNumbers) {
+        return randomNumbers.stream()
+                .map(RandomNumber :: valueOf)
+                .collect(collectingAndThen(toList(), RandomNumbers :: new));
     }
 }
