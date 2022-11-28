0a097ae7f59c2ce36d61e03ed334ce6be5de119d
HaODay <ycis5013@naver.com>
2022-11-29 00:08:15 +0900

    feat(Validator): 재시작/종료 여부 입력값 검증하는 기능 구현

--- src/main/java/baseball/model/Validator.java
+++ src/main/java/baseball/model/Validator.java
@@ -1,12 +1,21 @@
 package baseball.model;
 
 import static baseball.model.NumberProperty.SIZE;
+import static baseball.model.GameCommand.END;
+import static baseball.model.GameCommand.REPLAY;
 
 public class Validator {
+
     public static void validateNum(String num) throws IllegalArgumentException {
         checkSize(num);
         checkDuplicate(num);
         checkNumScope(num);
+    }
+
+    public static void validateGameCommand(String gameCommand) throws IllegalArgumentException {
+        if (!(gameCommand.equals(REPLAY.get()) || gameCommand.equals(END.get()))) {
+            throw new IllegalArgumentException();
+        }
     }
 
     private static void checkSize(String num) throws IllegalArgumentException {
