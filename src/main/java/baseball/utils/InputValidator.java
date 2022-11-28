0e29b8c42b091b438f383751186aa075192c6c56
Ridealist <cjsn92@gmail.com>
2022-11-29 00:01:13 +0900

    refactor(InputValidator): 객체 생성 차단 처리 및 Exception 메세지 내부 상수화
    생성자 접근제한자 private 설정
    외부 상수 클래스 내부 필드화
    숫자 확인 로직 정규식 적용

--- src/main/java/baseball/utils/InputValidator.java
+++ src/main/java/baseball/utils/InputValidator.java
@@ -1,24 +1,33 @@
 package baseball.utils;
 
-public class Validator {
-    
+import static baseball.utils.Constants.*;
+
+public final class InputValidator {
+    public static final String INVALID_LENGTH_EXCEPTION = "띄어쓰기 없이 연속해서 숫자 3개를 입력해주세요.";
+    public static final String NOT_NUMBER_EXCEPTION = "1에서 9까지의 숫자 3개를 입력해주세요.";
+    public static final String NOT_ONE_OR_TWO_EXCEPTION = "숫자 1또는 2 중에 하나를 입력해주세요.";
+
+    private InputValidator() {
+
+    }
+
     public static void isLengthThree(String input) {
-        if (input.length() != Constants.NUMBERS_SIZE) {
-            throw new IllegalArgumentException(ExceptionMessage.INVALID_LENGTH_EXCEPTION);
+        if (input.length() != NUMBERS_SIZE) {
+            throw new IllegalArgumentException(INVALID_LENGTH_EXCEPTION);
         }
     }
 
     public static void isAllNumber(String input) {
-        for (int i = 0; i < Constants.NUMBERS_SIZE; i++) {
-            if (!Character.isDigit(input.charAt(i))) {
-                throw new IllegalArgumentException(ExceptionMessage.NOT_NUMBER_EXCEPTION);
+        for (int i = 0; i < NUMBERS_SIZE; i++) {
+            if (!input.matches("[0-9]+")) {
+                throw new IllegalArgumentException(NOT_NUMBER_EXCEPTION);
             }
         }
     }
 
     public static void isOneOrTwo(String input) {
-        if (!(input.equals("1") || input.equals("2"))) {
-            throw new IllegalArgumentException(ExceptionMessage.NOT_ONE_OR_TWO_EXCEPTION);
+        if (!(input.equals(GAME_RESTART) || input.equals(GAME_QUIT))) {
+            throw new IllegalArgumentException(NOT_ONE_OR_TWO_EXCEPTION);
         }
     }
 }
