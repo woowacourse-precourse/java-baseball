15681c269fc8ac945a4f54407c020ae0cd7be3be
HaODay <ycis5013@naver.com>
2022-11-28 16:58:14 +0900

    feat(Message): 플레이어 값 요청 메시지 출력하는 열거상수 추가

--- src/main/java/baseball/view/Message.java
+++ src/main/java/baseball/view/Message.java
@@ -1,7 +1,8 @@
 package baseball.view;
 
 public enum Message {
-    START_MESSAGE("숫자 야구 게임을 시작합니다.");
+    START_MESSAGE("숫자 야구 게임을 시작합니다."),
+    INPUT_PLAYER_NUM_MESSAGE("숫자를 입력해주세요 : ");
 
     private final String message;
 
