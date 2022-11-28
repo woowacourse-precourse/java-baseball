de58bcddc489da456a48d18c423bf3156926c8ab
HaODay <ycis5013@naver.com>
2022-11-28 16:58:47 +0900

    feat(InputView): 플레이어의 값 입력 받는 기능 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,13 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.List;
+
 public class InputView {
+
+    public static String readPlayerNum() {
+        return Console.readLine();
+    }
+
 }
