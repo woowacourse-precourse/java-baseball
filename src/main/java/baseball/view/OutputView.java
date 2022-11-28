297a6a7b9c90b023772c4363d2f0c70dbf0fa4ac
Ridealist <cjsn92@gmail.com>
2022-11-29 00:08:02 +0900

    refactor(OutputView): 내부 enum 클래스 구현 및 중복 로직 메소드 통합
    정적 메세지 랜더링하는 로직을 Enum 클래스로 묶어서 관리
    Enum 클래스 toString 메소드 오버라이딩
    정적 메세지 출력 로직 한 메스드로 통합

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,17 +2,34 @@ package baseball.view;
 
 import baseball.domain.Result;
 
-public class OutputView {
+public final class OutputView {
+    public enum Message {
+        REQUEST_NUMBER("숫자를 입력해주세요 : "),
+        REQUEST_RESTART_OR_QUIT("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.%n"),
+        START_GAME("숫자 야구 게임을 시작합니다.%n"),
+        GAME_OVER("3개의 숫자를 모두 맞히셨습니다! 게임 종료%n");
 
-    public static void printStartMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        private final String message;
+
+        Message(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public String toString() {
+            return message;
+        }
+    }
+
+    private OutputView() {
+
+    }
+
+    public static void printMessage(Message message) {
+        System.out.printf(message.toString());
     }
 
     public static void printGameStatus(Result result) {
         System.out.println(result.makeMessage());
-    }
-
-    public static void printResult() {
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
 }
