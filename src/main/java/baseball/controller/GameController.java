4956de2ed5d2ef5079488bd888745e7b569b3f4a
HaODay <ycis5013@naver.com>
2022-11-29 00:13:08 +0900

    feat(GameController): 1개의 라운드 진행하는 기능 구현

--- src/main/java/baseball/controller/GameController.java
+++ src/main/java/baseball/controller/GameController.java
@@ -1,4 +1,28 @@
 package baseball.controller;
 
+import baseball.model.Computer;
+import baseball.model.Judge;
+import baseball.model.Player;
+import baseball.model.Validator;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import static baseball.model.GameCommand.REPLAY;
+
 public class GameController {
+
+    private Judge judge = new Judge();
+    private String gameCommand = "";
+
+    public void startGame() {
+        Computer computer = new Computer();
+        while (!judge.gameSucceeded()) {
+            judge = new Judge();
+            guess(computer, judge);
+        }
+        OutputView.printInputGameCommandMessage();
+        gameCommand = InputView.readGameCommand();
+        Validator.validateGameCommand(gameCommand);
+    }
+    
 }
