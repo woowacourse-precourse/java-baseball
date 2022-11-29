c858297f9cab4e3e4f944c295a4673bfddd40fb1
ffolabear <ffolabear@gmail.com>
2022-11-29 16:41:41 +0900

    refactor(Game): Game 클래스 리팩토링

--- src/main/java/baseball/service/Game.java
+++ src/main/java/baseball/service/Game.java
@@ -1,24 +1,24 @@
-package baseball.domain;
+package baseball.service;
 
 import baseball.util.Message;
 
 import java.util.List;
+import java.util.Map;
 
 public class Game {
 
     Message message = new Message();
-    User user;
+    Set set;
     Computer computer;
     List<String> computerAnswer;
 
     public Game() {
-        this.user = new User();
         this.computer = new Computer();
         message.initialGameMessage();
     }
 
     public void startGame() {
-        computerAnswer = computer.getComputerAnswer();
+        computerAnswer = computer.generateComputerAnswer();
         boolean isCorrectSet = false;
         while (!isCorrectSet) {
             isCorrectSet = startSet();
@@ -30,19 +30,19 @@ public class Game {
 
     private boolean startSet() {
         message.askPlayerInputAnswer();
+        set = new Set(computerAnswer);
+        set.inputUserAnswer();
+        Map<String, Integer> setResult = set.calculate();
 
-        List<String> userAnswer = user.getUserAnswer();
-        Set set = new Set(computerAnswer, userAnswer);
-        int ball = set.getBall();
-        int strike = set.getStrike();
+        int ball = setResult.get(ResultKeys.BALL.getKey());
+        int strike = setResult.get(ResultKeys.STRIKE.getKey());
 
         message.gameResultMessage(ball, strike);
-        return (strike == 3);
+        return strike == GameSetting.GAME_END.getAttribute();
     }
 
     public boolean replay() {
         message.askPlayerReplayMessage();
-        String replayAnswer = user.getUserReplayAnswer();
-        return replayAnswer.equals("1");
+        return set.inputReplayAnswer();
     }
 }
