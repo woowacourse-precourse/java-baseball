37460eddaa04aad828b5bf7fa8e3ed159cff8c08
sooyoungh <sooyoung.h8@gmail.com>
2022-11-29 14:40:10 +0900

    ♻ refactor(Hint, OutputView): 메시지 출력 기능 Enum에 오버라이딩
    Hint별로 다른 메시지 출력하는 기능 Enum에 위임하여, Enum에 관련 기능 모아둠

--- src/main/java/baseball/model/Hint.java
+++ src/main/java/baseball/model/Hint.java
@@ -1,5 +1,31 @@
 package baseball.model;
 
 public enum Hint {
-    NOTHING, BALL_AND_STRIKE, ONLY_BALL, ONLY_STRIKE;
+
+    NOTHING() {
+        @Override
+        public String printHintMessage(int ballCnt, int strikeCnt) {
+            return "낫싱";
+        }
+    },
+    BALL_AND_STRIKE() {
+        @Override
+        public String printHintMessage(int ballCnt, int strikeCnt) {
+            return ballCnt + "볼 " + strikeCnt + "스트라이크\n";
+        }
+    },
+    ONLY_BALL(){
+        @Override
+        public String printHintMessage(int ballCnt, int strikeCnt) {
+            return ballCnt + "볼\n";
+        }
+    },
+    ONLY_STRIKE(){
+        @Override
+        public String printHintMessage(int ballCnt, int strikeCnt) {
+            return strikeCnt + "스트라이크\n";
+        }
+    };
+
+    abstract public String printHintMessage(int ballCnt, int strikeCnt) ;
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,29 +6,9 @@ import baseball.model.Judge;
 public class OutputView {
     private static final String START_GAME= "숫자 야구 게임을 시작합니다.";
     private static final String GAME_CLEAR= "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
-    private static final String BALL= "볼";
-    private static final String STRIKE= "스트라이크";
-    private static final String NOTHING= "낫싱";
 
     public static void printStart() {
         System.out.println(START_GAME);
-    }
-
-    public static void printBall (int ballCnt) {
-        System.out.printf("%d" +BALL+"\n", ballCnt);
-    }
-
-    public static void printStrike (int strikeCnt) {
-        System.out.printf("%d" +STRIKE+ "\n", strikeCnt);
-    }
-
-    public static void printBallAndStrike (int ballCnt, int strikeCnt) {
-        System.out.printf("%d" +BALL, ballCnt);
-        System.out.printf(" %d" +STRIKE+ "\n", strikeCnt);
-    }
-
-    public static void printNothing () {
-        System.out.println(NOTHING);
     }
 
     public static void printGameClear() {
@@ -38,9 +18,9 @@ public class OutputView {
     public static void printHint(Judge judge) {
         Hint hint = judge.getHint();
 
-        if (hint.equals(Hint.NOTHING)) OutputView.printNothing();
-        if (hint.equals(Hint.BALL_AND_STRIKE)) OutputView.printBallAndStrike(judge.getBall(), judge.getStrike());
-        if (hint.equals(Hint.ONLY_BALL)) OutputView.printBall(judge.getBall());
-        if (hint.equals(Hint.ONLY_STRIKE)) OutputView.printStrike(judge.getStrike());
+        int ballCnt = judge.getBall();
+        int strikeCnt = judge.getStrike();
+        String message = hint.printHintMessage(ballCnt, strikeCnt);
+        System.out.println(message);
     }
 }
