5a10e77aa21bd462a4d6275db6c22710a6776f1d
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-29 23:54:49 +0900

    refactor(Rendering) : print() -> render(), printLine -> message로 변경

--- src/main/java/baseball/Rendering.java
+++ src/main/java/baseball/Rendering.java
@@ -3,20 +3,22 @@ package baseball;
 public enum Rendering {
 
     START("숫자 야구 게임을 시작합니다."),
+    ERROR("[ERROR] 서로 다른 세 자리 수만 입력 가능합니다."),
     PUT("숫자를 입력해주세요 : "),
-    HINT_BOTH("%d볼 %d스트라이크"),
-    HINT_BALL("%d볼"),
-    HINT_STRIKE("%d스트라이크"),
+    BALL_STRIKE("%d볼 %d스트라이크"),
+    BALL("%d볼"),
+    STRIKE("%d스트라이크"),
+    NOTHING("낫싱"),
     VICTORY("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
     CHOOSE("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
 
-    private String printLine;
+    private final String message;
 
-    Rendering(String printLine) {
-        this.printLine = printLine;
+    Rendering(String message) {
+        this.message = message;
     }
 
-    public String print() {
-        return printLine;
+    public String render() {
+        return message;
     }
 }
