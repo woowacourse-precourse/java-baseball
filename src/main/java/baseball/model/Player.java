ae805b286ce0be06c02f1bf5fc9540299ce5016d
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-29 20:14:12 +0900

    feat(Player) : 사용자 입력자를 통해 랜덤수를 생성하는 기능 추가

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -1,2 +1,30 @@
-package baseball.model;public class Player {
+package baseball.model;
+
+import static baseball.model.Number.*;
+import static java.util.stream.Collector.*;
+import static java.util.stream.Collectors.*;
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player {
+
+    public List<Integer> generateNumber(String readLine) {
+
+        return (List<Integer>) separateReadLine(readLine)
+                .stream()
+                .map(Character::getNumericValue)
+                .collect(toList());
+    }
+
+    private static List<Character> separateReadLine(String readLine) {
+        List<Character> separate = new ArrayList<>();
+        for (int index = 0; index < LENGTH.getNumber(); index++) {
+            char eachNumber = readLine.charAt(index);
+            separate.add(eachNumber);
+        }
+        return separate;
+    }
+
 }
