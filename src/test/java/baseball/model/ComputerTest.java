af7f5b0a10b27a57840c6d3e7bde365428c2bc90
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-30 19:37:47 +0900

    refactor(ComputerTest) : 불필요한 코드 제거, 랜덤수를 설정할 방법 못 찾음

--- src/test/java/baseball/model/ComputerTest.java
+++ src/test/java/baseball/model/ComputerTest.java
@@ -1,56 +1,46 @@
 package baseball.model;
 
+import static baseball.model.Computer.COMPUTER;
 import static baseball.model.Number.LENGTH;
+import static baseball.model.Player.PLAYER;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 import camp.nextstep.edu.missionutils.Randoms;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 @DisplayName("Number 클래스")
 @DisplayNameGeneration(ReplaceUnderscores.class)
-class NumberTest {
+class ComputerTest {
 
     List<Integer> computer;
 
     @BeforeEach
     void setUp() {
-        computer = Number.generateNumber();
+        computer = COMPUTER.number();
     }
-    public static List<Integer> generateExpectList() {
-        List<Integer> expectList = new ArrayList<>();
-
-        while (expectList.size() < LENGTH.getNumber()){
-            expectList.add(Randoms.pickNumberInRange(1,9));
-        }
-        return expectList;
-    }
-
-
 
     @Test
     public void 상대방_랜덤수_생성(){
         //given
-
         int expect = 3;
+
+        //when
         int actual = computer.size();
 
         //then
         assertEquals(expect, actual);
     }
 
-    @Test
-    public void 랜덤수_비교() throws Exception{
-        //given
-        List<Integer> expect = generateExpectList();
-
-        //then
-        assertEquals(expect, computer);
-    }
 
 }
