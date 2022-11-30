e53e21ee0eb0cbdb4cd9ba5babd21d6803c147e3
joohyeongKim <wnguddl96@knou.ac.kr>
2022-11-30 19:29:33 +0900

    feat(Computer) : 상대방 수 생성 기능 / Number enum과 분리

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -1,2 +1,25 @@
-package baseball.model;public enum Computer {
+package baseball.model;
+
+import static baseball.model.Number.LENGTH;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public enum Computer {
+
+    COMPUTER;
+
+    public List<Integer> number() {
+        return generateNumber();
+    }
+
+    private static List<Integer> generateNumber() {
+        List<Integer> computer = new ArrayList<>();
+
+        while (computer.size() < LENGTH.getNumber()){
+            computer.add(Randoms.pickNumberInRange(1,9));
+        }
+        return computer;
+    }
 }
