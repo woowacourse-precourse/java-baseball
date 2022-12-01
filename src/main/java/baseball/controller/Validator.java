9e9fe62d8a046e31b39453ac640c0bdf5935a55a
joohyeongKim <wnguddl96@knou.ac.kr>
2022-12-01 17:45:26 +0900

    faet(Validator) : 생성할 때마다 입력자의 유효성을 검사하는 기능 추가

--- src/main/java/baseball/controller/Validator.java
+++ src/main/java/baseball/controller/Validator.java
@@ -1,2 +1,37 @@
-package baseball.controller;public enum Validator {
+package baseball.controller;
+
+import static baseball.Rendering.ERROR;
+import static baseball.controller.Separator.SEPARATOR;
+import static baseball.model.Number.LENGTH;
+
+import java.util.stream.Stream;
+
+public class Validator {
+
+    public Validator(String readLine) {
+        checkRange(readLine);
+        checkNumeric(readLine);
+        checkDistinct(readLine);
+    }
+
+    private void checkRange(String readLine) {
+        if (readLine.length() != LENGTH.getNumber()) {
+            throw new IllegalArgumentException(ERROR.render());
+        }
+    }
+
+    private void checkNumeric(String readLine) {
+        Stream.of(readLine)
+                .filter(element -> element.matches("[0-9]*$"))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ERROR.render()));
+    }
+
+    private void checkDistinct(String readLine) {
+        readLine.chars()
+                .mapToObj(element -> (char) element)
+                .distinct()
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(ERROR.render()));
+    }
 }
