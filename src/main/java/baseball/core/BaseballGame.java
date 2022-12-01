1b91bb884cf8850ba9f81d5b51f99ea5444f590c
jinhyeon <vmflwu123@gmail.com>
2022-12-01 12:51:16 +0900

    refactor(BaseballGame): 기능 위임으로 인한 기존 기능 삭제
    BaseballNumber로 기능을 이전하면서
    필요없는 기능을 삭제

--- src/main/java/baseball/core/BaseballGame.java
+++ src/main/java/baseball/core/BaseballGame.java
@@ -10,47 +10,8 @@ public class BaseballGame {
         this.currentStatus = currentStatus;
     }
 
-    // 볼의 개수 확인
-    public Integer countTheNumberOfBall(String targetNumber, String inputNumber) {
+    public void checkSuccessOrFail() {
 
-        Integer numberOfBall = 0;
-        for (int indexOfInput = 0; indexOfInput < inputNumber.length(); indexOfInput++) {
-            char digitOfInput = inputNumber.charAt(indexOfInput);
-            if (isBall(targetNumber, indexOfInput, digitOfInput)) numberOfBall++;
-        }
-
-        return numberOfBall;
-    }
-
-    // 스트라이크의 개수 확인
-    public Integer countTheNumberOfStrike(String targetNumber, String inputNumber) {
-
-        Integer numberOfStrike = 0;
-        for (int indexOfInput = 0; indexOfInput < inputNumber.length(); indexOfInput++) {
-            char digitOfInput = inputNumber.charAt(indexOfInput);
-            if (isStrike(targetNumber, indexOfInput, digitOfInput)) numberOfStrike++;
-        }
-
-        return numberOfStrike;
-    }
-
-    //== 입력 숫자에서 각 자릿수마다 볼인지 스트라인지 확인 ==//
-    // 해당 자릿수가 볼이면 true, 볼이 아니면 false를 반환
-    private Boolean isBall(String targetNumber, int indexOfInput, char digitOfInput) {
-        for (int indexOfTarget = 0; indexOfTarget < targetNumber.length(); indexOfTarget++) {
-            char digitOfTarget = targetNumber.charAt(indexOfTarget);
-            if (indexOfTarget == indexOfInput) continue;
-            if (digitOfTarget == digitOfInput) return true;
-        }
-        return false;
-    }
-
-    // 해당 자릿수가 스트라이크면 true, 스트라이크가 아니면 false를 반환
-    private Boolean isStrike(String targetNumber, int indexOfInput, char digitOfInput) {
-        int indexOfTarget = indexOfInput;
-        char digitOfTarget = targetNumber.charAt(indexOfTarget);
-        if (digitOfTarget == digitOfInput) return true;
-        return false;
     }
 
     //== Getter & Setter ==//
