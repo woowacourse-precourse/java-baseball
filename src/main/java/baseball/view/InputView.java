878af2ef4435358228d4fade3d05ae68d2f43bb1
joohyeongKim <wnguddl96@knou.ac.kr>
2022-12-01 17:46:07 +0900

    refactor(InputView) : 유효 검사 함수 분리

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,38 +1,23 @@
 package baseball.view;
 
-import static baseball.Rendering.ERROR;
 import static baseball.Rendering.PUT;
-import static baseball.view.Number.LENGTH;
+
+import baseball.controller.Validator;
 import camp.nextstep.edu.missionutils.Console;
-import java.util.List;
 
 public class InputView {
 
+    private String readLine;
+
+    public InputView() {
+        this.readLine = Console.readLine();
+    }
+
     public String putReadLine() {
-        System.out.println(PUT.print());
+        System.out.println(PUT.render());
+        new Validator(readLine);
         return Console.readLine();
     }
 
-    public void validate(String readLine) {
-        readLine = putReadLine();
-        validateNumeric(readLine);
-        distinct(readLine);
-
-        if (readLine.length() != LENGTH.getNumber()) {    throw new IllegalArgumentException(ERROR.print());    }
-    }
-
-    public String distinct(String readLine) {
-        return List.of(readLine).stream()
-                .distinct()
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(ERROR.print()));
-    }
-
-    private static String validateNumeric(String readLine) {
-        return List.of(readLine).stream()
-                .filter(character -> character.matches("[0-9]"))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException(ERROR.print()));
-    }
 
 }
