5ac75aa8d65fae5f1c8303c1cef0578cb7909b5a
hingjae <ljh23270@naver.com>
2022-12-05 16:23:28 +0900

    컨트롤러

--- src/main/java/baseball/controller/BaseballController.java
+++ src/main/java/baseball/controller/BaseballController.java
@@ -1,2 +1,50 @@
-package baseball.controller;public class BaseballController {
+package baseball.controller;
+
+import baseball.Computer;
+import baseball.Converter;
+import baseball.RandomNumberGenerator;
+import baseball.User;
+import baseball.inputchecker.Checker;
+import baseball.inputview.InputNumber;
+import baseball.inputview.InputReplay;
+import baseball.outputview.ResultMessage;
+import baseball.outputview.StartMessage;
+
+public class BaseballController {
+    InputNumber inputNumber = new InputNumber();
+    RandomNumberGenerator randomNumberGenerator;
+    Computer computer;
+    ResultMessage resultMessage = new ResultMessage();
+
+    public void run() {
+        int replay = 1;
+        while(replay == 1) {
+            startGame();
+            playGame();
+            replay = replay();
+        }
+    }
+
+    public void startGame() {
+        StartMessage.print();
+        randomNumberGenerator = new RandomNumberGenerator();
+        randomNumberGenerator.generate();
+        computer = new Computer(randomNumberGenerator.getRandomNumberList());
+    }
+
+    public void playGame() {
+        boolean retry = true;
+        while (retry) {
+            String input = inputNumber.run();
+            Checker.isValid(input);
+            User user = new User(Converter.stringToList(input));
+            retry = computer.compareAndResult(user.getNumberList());
+            resultMessage.print(computer.getResult().toString());
+        }
+    }
+
+    public int replay() {
+        InputReplay inputReplay = new InputReplay();
+        return inputReplay.run();
+    }
 }
