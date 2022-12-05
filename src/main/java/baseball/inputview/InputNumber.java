3acbed2e47dad6a32bd13d8dc4b9eab29aba99d9
hingjae <ljh23270@naver.com>
2022-12-05 16:20:14 +0900

    유저 : 세자리수 입력 및 예외 처리

--- src/main/java/baseball/inputview/InputNumber.java
+++ src/main/java/baseball/inputview/InputNumber.java
@@ -1,2 +1,18 @@
-package baseball.inputview;public class InputNumber {
+package baseball.inputview;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputNumber {
+
+    public String run() {
+        printMessage();
+        return input();
+    }
+
+    public void printMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+    public String input() {
+        return Console.readLine();
+    }
 }
--- src/test/java/baseball/inputchecker/CheckerTest.java
+++ src/test/java/baseball/inputchecker/CheckerTest.java
@@ -1,4 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball.inputchecker;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class CheckerTest {
-  
+    @Test
+    void 정상값입력(){
+        Checker.isValid("123");
+        Checker.isValid("139");
+        Checker.isValid("327");
+    }
+    @Test
+    void 예외값입력() {
+        assertThatThrownBy(() -> Checker.isValid("a1234")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Checker.isValid("a123")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Checker.isValid("1223")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Checker.isValid("122")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Checker.isValid("992")).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> Checker.isValid("92")).isInstanceOf(IllegalArgumentException.class);
+    }
 }
