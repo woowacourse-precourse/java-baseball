cbba580b30c3943c6760bbbe8f3e283cb97c94f8
hingjae <ljh23270@naver.com>
2022-12-05 16:20:53 +0900

    유저 : 입력 값 리스트로 변환

--- src/test/java/baseball/ConverterTest.java
+++ src/test/java/baseball/ConverterTest.java
@@ -1,4 +1,24 @@
+package baseball;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ConverterTest {
-  
+    @Test
+    void 문자열을_정수형리스트로_변환() {
+        List temp;
+        temp = Converter.stringToList("327");
+        System.out.println("temp = " + temp);
+    }
+
+    @Test
+    void 정수형리스트_여부() {
+        List temp;
+        temp = Converter.stringToList("143");
+        Assertions.assertThat(temp).isInstanceOf(List.class);
+    }
 }
