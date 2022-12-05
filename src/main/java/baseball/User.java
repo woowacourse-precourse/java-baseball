2baccc3e335a2ac9da9a08849eab5f6a1d7dffb9
hingjae <ljh23270@naver.com>
2022-12-05 16:19:04 +0900

    유저

--- src/main/java/baseball/User.java
+++ src/main/java/baseball/User.java
@@ -1,2 +1,15 @@
-package baseball;public class User {
+package baseball;
+
+import java.util.List;
+
+public class User {
+    private final List numberList;
+
+    public User(List numberList) {
+        this.numberList = numberList;
+    }
+
+    public List getNumberList() {
+        return numberList;
+    }
 }
