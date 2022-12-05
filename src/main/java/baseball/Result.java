47ec9169bbca8cf3aa64b3b3728ae30e6ae43a4f
hingjae <ljh23270@naver.com>
2022-12-05 16:17:58 +0900

    게임 결과 : 볼 스트라이크 결과 값 생성

--- src/main/java/baseball/Result.java
+++ src/main/java/baseball/Result.java
@@ -1,2 +1,39 @@
-package baseball;public class Result {
+package baseball;
+
+public class Result {
+    StringBuilder sb;
+
+    public void convertToString(int ball, int strike) {
+        sb = new StringBuilder();
+        if (ball > 0) {
+            sbAppend(ball, "볼");
+        }
+        if (strike > 0) {
+            sbAppend(strike, "스트라이크");
+            isThree(strike);
+        }
+        isNothing(ball, strike);
+    }
+
+    public void sbAppend(int count, String text) {
+        sb.append(count);
+        sb.append(text + " ");
+    }
+
+    public void isThree(int strike) {
+        if (strike == 3) {
+            sb.append("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+    }
+
+    public void isNothing(int ball, int strike) {
+        if(ball == 0 && strike == 0) {
+            sb.append("낫싱");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return sb.toString();
+    }
 }
--- src/test/java/baseball/ResultTest.java
+++ src/test/java/baseball/ResultTest.java
@@ -1,4 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
 class ResultTest {
-  
+    @Test
+    void 볼_스트라이크_문자열() {
+        Result result = new Result();
+        result.convertToString(3, 0);
+        System.out.println(result.toString());
+        result.convertToString(0, 3);
+        System.out.println(result.toString());
+        result.convertToString(1, 2);
+        System.out.println(result.toString());
+    }
 }
