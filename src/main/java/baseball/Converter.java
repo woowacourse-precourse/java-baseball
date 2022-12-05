e669763e8a9fa08cc62429f63ed893c78a5981ca
hingjae <ljh23270@naver.com>
2022-12-05 14:30:53 +0900

    입력 값 리스트로 변환

--- src/main/java/baseball/Converter.java
+++ src/main/java/baseball/Converter.java
@@ -1,2 +1,14 @@
-package baseball;public class Converter {
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Converter {
+    public static List stringToList(String input) {
+        List numberList = new ArrayList();
+        for(char c : input.toCharArray()) {
+            numberList.add(Character.getNumericValue(c));
+        }
+        return numberList;
+    }
 }
