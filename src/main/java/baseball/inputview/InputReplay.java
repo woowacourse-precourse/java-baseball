ea05f7f0f318e03da39a5d05c8c833ca5fb3da7c
hingjae <ljh23270@naver.com>
2022-12-05 16:22:54 +0900

    반복 여부 결정

--- src/main/java/baseball/inputview/InputReplay.java
+++ src/main/java/baseball/inputview/InputReplay.java
@@ -1,2 +1,20 @@
-package baseball.inputview;public class InputReplay {
+package baseball.inputview;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputReplay {
+
+    public int run() {
+        printMessage();
+        return input();
+    }
+
+    public void printMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
+    public int input() {
+        String input = Console.readLine();
+        return Integer.parseInt(input);
+    }
 }
