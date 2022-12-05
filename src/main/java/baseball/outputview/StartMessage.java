c38e4ef2be0ec8cb3bc60356c6f4382b1f700692
hingjae <ljh23270@naver.com>
2022-12-05 10:26:40 +0900

    출력 값 : 게임시작

--- src/main/java/baseball/outputview/StartMessage.java
+++ src/main/java/baseball/outputview/StartMessage.java
@@ -1,2 +1,7 @@
-package baseball.outputview;public class StartMessage {
+package baseball.outputview;
+
+public class StartMessage {
+    public static void print () {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
 }
