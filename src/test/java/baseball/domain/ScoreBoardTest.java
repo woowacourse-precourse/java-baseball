adc7ce0e5b17bb0a30faad750954b2c056c279c8
Choi-JJunho <junho5336@gmail.com>
2022-12-08 11:49:57 +0900

    test(ScoreBoardTest): add ScoreBoard Test

---
+++ src/test/java/baseball/domain/ScoreBoardTest.java
@@ -0,0 +1,61 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class ScoreBoardTest {
+
+    @Nested
+    class ScoreBoard의_인자값에_대해 {
+
+        @Test
+        void ball이_0이상_4이하가_아니면_예외() {
+            assertThatThrownBy(() -> {
+                new ScoreBoard(5, 0);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        void strike가_0이상_3이하가_아니면_예외() {
+            assertThatThrownBy(() -> {
+                new ScoreBoard(0, 4);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
+    }
+
+    @Nested
+    class toString_메소드는_메시지_형태의_문자열을_반환한다 {
+        @ParameterizedTest(name = "{0}볼 {1}스트라이크인 경우")
+        @CsvSource(value = {"1:1", "2:1", "3:2", "0:1"}, delimiter = ':')
+        void parameterized_toString_test(int ball, int strike) {
+            ScoreBoard scoreBoard = new ScoreBoard(ball, strike);
+
+            List<String> resultMessage = new ArrayList<>();
+            if (ball != 0) {
+                resultMessage.add(ball + "볼");
+            }
+            if (strike != 0) {
+                resultMessage.add(strike + "스트라이크");
+            }
+            
+            assertThat(scoreBoard.toString()).contains(resultMessage);
+        }
+
+        @Test
+        void 점수가_0볼_0스트라이크인_경우_낫싱을_반환한다() {
+            ScoreBoard scoreBoard = new ScoreBoard(0, 0);
+            assertThat(scoreBoard.toString()).isEqualTo("낫싱");
+        }
+    }
+}
