8368e6f2e9abd9825673aa455f68fe847b1abae1
Choi-JJunho <junho5336@gmail.com>
2022-12-08 14:07:17 +0900

    feat(ErrorMessage): add ErrorMessage

--- src/main/java/baseball/utils/ErrorMessage.java
+++ src/main/java/baseball/utils/ErrorMessage.java
@@ -1,8 +1,12 @@
 package baseball.utils;
 
+import static baseball.utils.BaseBallStatus.BASEBALL_DEFAULT_SIZE;
+
 public enum ErrorMessage {
     INVALID_STRIKE_NUMBER("스트라이크의 범위는 0 ~ 3 입니다."),
-    INVALID_BALL_NUMBER("볼의 범위는 0 ~ 3 입니다.");
+    INVALID_BALL_NUMBER("볼의 범위는 0 ~ 3 입니다."),
+    INVALID_BASEBALL_SIZE("숫자야구의 자리수는 " + BASEBALL_DEFAULT_SIZE.getCode() + "자리 입니다."),
+    INVALID_ARGUMENT("잘못된 값입니다.");
 
     private final String message;
 
