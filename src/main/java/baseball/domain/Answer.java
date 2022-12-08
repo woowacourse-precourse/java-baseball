69c80a933dcc2a58a5cde1a791eed8372b14d672
Choi-JJunho <junho5336@gmail.com>
2022-12-08 14:07:05 +0900

    feat(Answer): add validate method

--- src/main/java/baseball/domain/Answer.java
+++ src/main/java/baseball/domain/Answer.java
@@ -1,13 +1,24 @@
 package baseball.domain;
 
+import baseball.utils.ErrorMessage;
+
 import java.util.List;
+
+import static baseball.utils.BaseBallStatus.BASEBALL_DEFAULT_SIZE;
 
 public class Answer {
 
     private final List<Integer> numbers;
 
     public Answer(List<Integer> numbers) {
+        validateAnswer(numbers);
         this.numbers = numbers;
+    }
+
+    private void validateAnswer(List<Integer> numbers) {
+        if (numbers.size() != BASEBALL_DEFAULT_SIZE.getCode()) {
+            throw new IllegalArgumentException(ErrorMessage.INVALID_BASEBALL_SIZE.getMessage());
+        }
     }
 
     public int get(int index) {
@@ -18,7 +29,7 @@ public class Answer {
         return numbers.size();
     }
 
-    public boolean contains(Integer number) {
+    public boolean contains(int number) {
         return numbers.contains(number);
     }
 
