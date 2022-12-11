fd2d15e1e72e1726901a2f6f966a36819a45aff7
fakerdeft <whakswp123@naver.com>
2022-12-11 23:26:58 +0900

    feat: 메세지들 추가

---
+++ src/main/java/Utils/Messages.java
@@ -0,0 +1,7 @@
+package Utils;
+
+public class Messages {
+    public static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public static final String NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요: ";
+    public static final String INPUT_ERROR_MESSAGE = "1~9까지 서로 다른 3자리 수 이외의 값을 입력했습니다. 시스템을 종료합니다.";
+}
