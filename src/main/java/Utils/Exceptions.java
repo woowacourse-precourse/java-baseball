b2bda7ec50f2af4550d4a7743e0a70d567645f4a
fakerdeft <whakswp123@naver.com>
2022-12-13 01:12:39 +0900

    feat: 입력 예외처리 수정

--- src/main/java/Utils/Exceptions.java
+++ src/main/java/Utils/Exceptions.java
@@ -8,8 +8,20 @@ public class Exceptions {
     }
 
     public String ValidateUserInputNumber(String readLine) {
-        if (!readLine.matches("^[1-9]*?") && readLine.length() != 3) {
-            throw new IllegalArgumentException(Messages.INPUT_ERROR_MESSAGE);
+        for (int i = 0; i < readLine.length(); i++) {
+            if (!Character.isDigit(readLine.charAt(i)) || readLine.charAt(i) == '0' || readLine.length() != 3) {
+                throw new IllegalArgumentException(Messages.INPUT_ERROR_MESSAGE);
+            }
+        }
+        if (readLine.charAt(0) == readLine.charAt(1) || readLine.charAt(1) == readLine.charAt(2) || readLine.charAt(0) == readLine.charAt(2)) {
+            throw new IllegalArgumentException(Messages.DUPLICATION_ERROR_MESSAGE);
+        }
+        return readLine;
+    }
+
+    public String ValidateOneOrTwoNumber(String readLine) {
+        if (!readLine.matches("^[1-9]*?") && readLine.length() != 1) {
+            throw new IllegalArgumentException(Messages.ONE_OR_TWO_ERROR_MESSAGE);
         }
         return readLine;
     }
