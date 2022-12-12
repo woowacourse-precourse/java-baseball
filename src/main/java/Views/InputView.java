07c049bdd6cdfbed2ab81eed75bf892607b3c3c6
fakerdeft <whakswp123@naver.com>
2022-12-12 22:49:59 +0900

    feat: 유저 입력 자료형 수정

--- src/main/java/Views/InputView.java
+++ src/main/java/Views/InputView.java
@@ -4,7 +4,7 @@ import static Utils.Exceptions.EXCEPTIONS;
 
 public class InputView {
     public static final InputView INPUT_VIEW = new InputView();
-    
+
     public String InputUserNumber(String readLine) {
         return EXCEPTIONS.ValidateUserInputNumber(readLine);
     }
