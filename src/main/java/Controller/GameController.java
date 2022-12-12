b74687c091bac7db702f3aa57d6e9ee9ac7af72a
fakerdeft <whakswp123@naver.com>
2022-12-13 01:12:27 +0900

    feat: 게임 기능들 추가

--- src/main/java/Controller/GameController.java
+++ src/main/java/Controller/GameController.java
@@ -8,14 +8,18 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static Utils.Exceptions.EXCEPTIONS;
 import static Views.InputView.INPUT_VIEW;
+import static Views.OutputView.OUTPUT_VIEW;
 
 public class GameController {
     public static final GameController GAME_CONTROLLER = new GameController();
 
+
     public List<Integer> GetUserNumber() {
         List<Integer> user = new ArrayList<>();
         int i = 0;
+        OUTPUT_VIEW.PrintInputNumber();
         String userRandomNumber = INPUT_VIEW.InputUserNumber(Console.readLine());
         while (user.size() < 3) {
             if (!user.contains(userRandomNumber.substring(i, i + 1))) {
@@ -52,4 +56,55 @@ public class GameController {
         result.put("ball", ball);
         return result;
     }
+
+    public int CompareUserWithEnemy(Map<String, Integer> result) {
+        if (result.get("ball") != 0) {
+            OUTPUT_VIEW.PrintBall(result);
+        }
+        if (result.get("strike") != 0) {
+            OUTPUT_VIEW.PrintStrike(result);
+            if (result.get("strike") == 3) {
+                OUTPUT_VIEW.PrintRestartGame();
+                return Integer.parseInt(EXCEPTIONS.ValidateOneOrTwoNumber(Console.readLine()));
+            }
+        }
+        if (result.get("ball") == 0 && result.get("strike") == 0) {
+            OUTPUT_VIEW.PrintNothing();
+        }
+        return 0;
+    }
+
+    public void SelectGoOrStopGame() {
+        int endNum = 0;
+        List<Integer> enemyNumber = GetEnemyNumber();
+        while (endNum != 2) {
+            endNum = CompareUserWithEnemy(GetStrikeAndBall(GetUserNumber(), enemyNumber));
+            if (endNum == 1) {
+                enemyNumber = GetEnemyNumber();
+            }
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
