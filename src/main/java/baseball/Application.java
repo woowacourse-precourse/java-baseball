0386de1d3f89401c51d07198d7bcfb3faba8ed99
fakerdeft <whakswp123@naver.com>
2022-12-13 22:37:46 +0900

    feat: 실행 메서드 수정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -8,7 +8,7 @@ public class Application {
     public static void main(String[] args) {
         OUTPUT_VIEW.PrintGameStartMessage();
         GAME_CONTROLLER.SelectGoOrStopGame();
-    }//public static void main
+    }
 
 
 }
